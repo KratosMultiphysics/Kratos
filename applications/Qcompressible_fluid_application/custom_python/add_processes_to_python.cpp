67ca9699a18feed5bfc7ec8968815e2472ceef6a
jmarti <jmarti@cimne.upc.edu>
2009-02-04 13:15:21 +0000

    aplicacion de Julio Marti

---
+++ applications/Qcompressible_fluid_application/custom_python/add_processes_to_python.cpp
@@ -0,0 +1,80 @@
+/*
+==============================================================================
+KratosR1ULFApplication 
+A library based on:
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi, Pawel Ryzhakov
+pooyan@cimne.upc.edu 
+rrossi@cimne.upc.edu
+- CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+ 
+//   
+//   Project Name:        Kratos       
+//   Last modified by:    $Author: jmarti $
+//   Date:                $Date: 2009-01-23 14:34:00 $
+//   Revision:            $Revision: 1.1 $
+//
+//
+ 
+
+// System includes 
+
+// External includes 
+#include <boost/python.hpp>
+
+ 
+// Project includes
+#include "includes/define.h"
+#include "processes/process.h"
+#include "custom_python/add_processes_to_python.h"
+
+#include "includes/node.h"
+
+namespace Kratos
+{
+	
+namespace Python
+{
+  void  AddProcessesToPython()
+  {
+	using namespace boost::python;
+
+	
+  }
+	
+}  // namespace Python.
+
+} // Namespace Kratos
+
