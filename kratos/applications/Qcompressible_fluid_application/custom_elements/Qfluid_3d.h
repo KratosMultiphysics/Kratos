5fa5753dc1529262decfcc86d643e61879c6a5c7
jmarti <jmarti@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-02-04 13:13:29 +0000

    aplicacion de Julio Marti

---
+++ kratos/applications/Qcompressible_fluid_application/custom_elements/Qfluid_3d.h
@@ -0,0 +1,334 @@
+/*
+==============================================================================
+KratosR1IncompressibleFluidApplication 
+A library based on:
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu 
+rrossi@cimne.upc.edu
+- CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author: jmarti $
+//   Date:                $Date: 2009-01-23 14:33:59 $
+//   Revision:            $Revision: 1.1 $
+//
+//
+
+
+#if !defined(KRATOS_FLUID_3D_COUPLED_H_INCLUDED )
+#define  KRATOS_FLUID_3D_COUPLED_H_INCLUDED
+
+
+
+// System includes 
+
+
+// External includes 
+#include "boost/smart_ptr.hpp"
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/element.h"
+#include "includes/ublas_interface.h"
+#include "includes/variables.h"
+
+
+namespace Kratos
+{
+
+  ///@name Kratos Globals
+  ///@{ 
+  
+  ///@} 
+  ///@name Type Definitions
+  ///@{ 
+  
+  ///@} 
+  ///@name  Enum's
+  ///@{
+      
+  ///@}
+  ///@name  Functions 
+  ///@{
+      
+  ///@}
+  ///@name Kratos Classes
+  ///@{
+  
+  /// Short class definition.
+  /** Detail class definition.
+  */
+  class QFluid3D
+	  : public Element
+    {
+    public:
+      ///@name Type Definitions
+      ///@{
+      
+      /// Counted pointer of Fluid3DCoupled
+      KRATOS_CLASS_POINTER_DEFINITION(QFluid3D);
+ 
+      ///@}
+      ///@name Life Cycle 
+      ///@{ 
+      
+      /// Default constructor.
+      QFluid3D(IndexType NewId, GeometryType::Pointer pGeometry);
+      QFluid3D(IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties);
+
+      /// Destructor.
+      virtual ~QFluid3D();
+      
+
+      ///@}
+      ///@name Operators 
+      ///@{
+      
+      
+      ///@}
+      ///@name Operations
+      ///@{
+
+      Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes,  PropertiesType::Pointer pProperties) const;
+
+      void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+      
+      void CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+      //virtual void CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix, ProcessInfo& rCurrentProcessInfo);
+      
+      void EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& rCurrentProcessInfo);
+
+	  void GetDofList(DofsVectorType& ElementalDofList,ProcessInfo& CurrentProcessInfo);
+
+	  void InitializeSolutionStep(ProcessInfo& CurrentProcessInfo);
+
+      ///@}
+      ///@name Access
+      ///@{ 
+      
+      
+      ///@}
+      ///@name Inquiry
+      ///@{
+      
+      
+      ///@}      
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+//      virtual String Info() const;
+      
+      /// Print information about this object.
+//      virtual void PrintInfo(std::ostream& rOStream) const;
+
+      /// Print object's data.
+//      virtual void PrintData(std::ostream& rOStream) const;
+      
+            
+      ///@}      
+      ///@name Friends
+      ///@{
+
+            
+      ///@}
+      
+    protected:
+      ///@name Protected static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected  Access 
+      ///@{ 
+        
+        
+      ///@}      
+      ///@name Protected Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Protected LifeCycle 
+      ///@{ 
+      
+            
+      ///@}
+      
+    private:
+      ///@name Static Member Variables 
+      ///@{ 
+		static boost::numeric::ublas::bounded_matrix<double,4,4> msaux_matrix;
+		static boost::numeric::ublas::bounded_matrix<double,4,4> msMassFactors;
+		static boost::numeric::ublas::bounded_matrix<double,4,4> msMassC;
+		static boost::numeric::ublas::bounded_matrix<double,4,3> msDN_DX; //cartesian coords
+		static boost::numeric::ublas::bounded_matrix<double,3,3> msF;
+  		static array_1d<double,4> msN; //dimension = number of nodes
+		static array_1d<double,3> ms_aux; //dimesion coincides with space dimension
+		static array_1d<double,3> ms_vel_gauss; //dimesion coincides with space dimension
+  		static array_1d<double,4> ms_temp_vec_np; //dimension = number of nodes
+		static array_1d<double,4> ms_u_DN;
+		static array_1d<double,4> ms_press; //dimension = number of nodes
+		std::vector< Matrix > mInvJ;
+	        Vector mDetJ;
+
+		static void InitializeAuxiliaries()
+		{
+		
+		  msMassFactors(0,0) = 0.25;msMassFactors(0,1) = 0.0;msMassFactors(0,2) = 0.0;msMassFactors(0,3) = 0.0;
+		  msMassFactors(1,0) = 0.0;msMassFactors(1,1) = 0.25;msMassFactors(1,2) = 0.0;msMassFactors(1,3) = 0.0;
+		  msMassFactors(2,0) = 0.0;msMassFactors(2,1) = 0.0;msMassFactors(2,2) = 0.25;msMassFactors(2,3) = 0.0;
+		  msMassFactors(3,0) = 0.0;msMassFactors(3,1) = 0.0;msMassFactors(3,2) = 0.25;msMassFactors(3,3) = 0.0;
+		  
+		  
+			msMassC = ZeroMatrix(4,4);
+			msMassC(0,0) = 1/10 ;
+			msMassC(0,1) = 1/20;
+			msMassC(0,2) = 1/20;
+			msMassC(0,3) = 1/20;
+			msMassC(1,0) = 1/20;
+			msMassC(1,1) = 1/10;
+			msMassC(1,2) = 1/20;
+			msMassC(1,3) = 1/20;
+			msMassC(2,0) = 1/20;
+			msMassC(2,1) = 1/20;
+			msMassC(2,2) = 1/10;
+			msMassC(2,3) = 1/20;
+			msMassC(3,0) = 1/20;
+			msMassC(3,1) = 1/20;
+			msMassC(3,2) = 1/20;
+			msMassC(3,3) = 1/10;
+
+
+
+		}
+        
+      ///@} 
+      ///@name Member Variables 
+      ///@{ 
+		
+        
+        
+      ///@} 
+      ///@name Private Operators
+      ///@{ 
+		void Stage1(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+		void Stage2(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+		
+		inline double CalculateH(double Volume);
+		
+		
+		void CalculateViscousMatrix(MatrixType& K, const boost::numeric::ublas::bounded_matrix<double,4,3>& DN_DX, const double& nu, const double& k, const double& density);    
+	  
+	  inline void  ExpandAndAddReducedMatrix(MatrixType& Destination,	boost::numeric::ublas::bounded_matrix<double,4,4>& ReducedMatrix, const unsigned int dimension);        
+	  ///@} 
+	  ///@name Private Operations
+	  ///@{ 
+	  
+	  
+	  ///@} 
+      ///@name Private  Access 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Private Inquiry 
+      ///@{ 
+        
+         
+      ///@}    
+      ///@name Un accessible methods 
+      ///@{ 
+      
+      /// Assignment operator.
+      //Fluid3DCoupled& operator=(const Fluid3DCoupled& rOther);
+
+      /// Copy constructor.
+      //Fluid3DCoupled(const Fluid3DCoupled& rOther);
+
+        
+      ///@}    
+        
+    }; // Class Fluid3DCoupled 
+
+  ///@} 
+  
+  ///@name Type Definitions       
+  ///@{ 
+  
+  
+  ///@} 
+  ///@name Input and output 
+  ///@{ 
+        
+ 
+  /// input stream function
+/*  inline std::istream& operator >> (std::istream& rIStream, 
+				    Fluid3DCoupled& rThis);
+*/
+  /// output stream function
+/*  inline std::ostream& operator << (std::ostream& rOStream, 
+				    const Fluid3DCoupled& rThis)
+    {
+      rThis.PrintInfo(rOStream);
+      rOStream << std::endl;
+      rThis.PrintData(rOStream);
+
+      return rOStream;
+    }*/
+  ///@} 
+
+}  // namespace Kratos.
+
+#endif // KRATOS_QFLUID_3D_COUPLED_H_INCLUDED  defined 
+
+
