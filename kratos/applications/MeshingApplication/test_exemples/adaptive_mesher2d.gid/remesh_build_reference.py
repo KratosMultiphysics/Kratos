12b3e29164ecbe2f64c65ccb2e184a0c4365c354
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-03-09 16:31:46 +0000

    benchmarking

--- kratos/applications/MeshingApplication/test_exemples/adaptive_mesher2d.gid/remesh_build_reference.py
+++ kratos/applications/MeshingApplication/test_exemples/adaptive_mesher2d.gid/remesh_build_reference.py
@@ -4,4 +4,4 @@ sys.path.append(kratos_benchmarking_path)
 import benchmarking
 
 print "Building reference data for remesh.py..."
-benchmarking.BuildReferenceData("remesh.py", "adapt_remesh2d_ref.txt")
+benchmarking.BuildReferenceData("remesh.py", "adaptive_mesher2d_ref.txt")
