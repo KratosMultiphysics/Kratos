9439e92e87ba04b90471d4b7a687badb2b96ea84
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-03-09 16:31:52 +0000

    benchmarking

--- kratos/applications/MeshingApplication/test_exemples/adaptive_mesher2d.gid/remesh_benchmark.py
+++ kratos/applications/MeshingApplication/test_exemples/adaptive_mesher2d.gid/remesh_benchmark.py
@@ -7,5 +7,5 @@ import benchmarking
 
 def Run():        
 	print "Running remesh.py..."
-	return benchmarking.RunBenchmark("remesh.py", "adapt_remesh2d_ref.txt")
+	return benchmarking.RunBenchmark("remesh.py", "adaptive_mesher2d_ref.txt")
 
