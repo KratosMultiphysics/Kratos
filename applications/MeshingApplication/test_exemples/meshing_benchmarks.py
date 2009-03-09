f18c38f7625a45f670cb556bf1d3236e9fab8602
rrossi <rrossi@cimne.upc.edu>
2009-03-09 16:31:09 +0000

    benchmarking

---
+++ applications/MeshingApplication/test_exemples/meshing_benchmarks.py
@@ -0,0 +1,63 @@
+import os
+import sys
+
+kratos_benchmarking_path = '../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
+def Run():
+	Msg = ""
+	Text = "========== Meshing Aplication ==========\n"
+
+        ################################################################
+
+	Text += "adaptive_mesher2d: "
+	os.chdir("adaptive_mesher2d.gid")
+	sys.path.append(os.getcwd())
+
+	print "Running Adaptive Mesher 2d benchmark..."
+	Msg = benchmarking.RunBenchmark("remesh.py", "adaptive_mesher2d_ref.txt")	
+	
+	if (Msg == True):
+		Text += "OK\n"
+		print "Adaptive_mesher2d benchmarking example succesful"
+	else:
+		Text += "FAILED\n"
+		Text += Msg
+		Text += "\n\n"
+		print "Adaptive_mesher2d benchmarking example FAILED"
+
+	os.chdir("..")
+
+        ################################################################
+
+	Text += "adaptive_mesher3d: "
+	os.chdir("adaptive_mesher3d.gid")
+	sys.path.append(os.getcwd())
+
+	print "Running Adaptive Mesher 3d benchmark..."
+	Msg = benchmarking.RunBenchmark("remesh.py", "adaptive_mesher3d_ref.txt")	
+	
+	if (Msg == True):
+		Text += "OK\n"
+		print "Adaptive_mesher2d benchmarking example succesful"
+	else:
+		Text += "FAILED\n"
+		Text += Msg
+		Text += "\n\n"
+		print "Adaptive_mesher2d benchmarking example FAILED"
+
+	os.chdir("..")
+	
+
+        ################################################################
+
+        ################################################################
+
+
+
+        print "resume of all of the examples for the meshing application :"
+        print Text
+	return Text
+
+Run();
