76268771e098f09d94eb2a00f1799ee43586a216
Pooyan Dadvand <pooyan@cimne.upc.edu>
2009-03-25 15:21:47 +0000

    changing run_all_benchmarks.py

--- benchmarking/run_all_benchmarks.py
+++ benchmarking/run_all_benchmarks.py
@@ -11,14 +11,20 @@ ApplicationsRoot = KratosRoot + "applications/"
 Text = "Status of Kratos examples:\n\n"
 os.chdir(ApplicationsRoot)
 
-# PFEM                                                                                                                                                                           
-os.chdir(ApplicationsRoot + "PFEMapplication/test_exemples")
+# Structural application                                                                                                                                                                           
+os.chdir(ApplicationsRoot + "structural_application/test_examples")
+import structural_benchmarks
+Text += structural_benchmarks.Run()
 
-import pfem_benchmarks
-Text += pfem_benchmarks.Run()
+
+# Incompressible fluid application
+os.chdir(ApplicationsRoot + "incompressible_fluid_application/test_examples")
+import incompressible_benchmarks
+Text += incompressible_benchmarks.Run()
+
 
 # Add other directories here
 
 print Text
 
-benchmarking.NotifyViaEmail("Status of Kratos examples", Text, ["pooyan@cimne.upc.edu", "rrossi@cimne.upc.edu", "mossaiby@yahoo.com"])
+#benchmarking.NotifyViaEmail("Status of Kratos examples", Text, ["pooyan@cimne.upc.edu", "rrossi@cimne.upc.edu", "mossaiby@yahoo.com"])
