8ea42ac10c75d3a4a538b465ca7c6ba7510072e3
janosch <janosch@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-03-31 11:59:29 +0000

    made test example executable without other applications

--- kratos/applications/structural_application/test_examples/balken.gid/balken.py
+++ kratos/applications/structural_application/test_examples/balken.gid/balken.py
@@ -14,28 +14,17 @@ domain_size = 3
 ## ATTENTION: the following lines have to be adapted to      #####
 ##            match your acrtual configuration               #####
 ##################################################################
-kratos_libs_path = '/home/hurga/kratos_local/kratos/libs/' ##kratos_root/libs
-#kratos_libs_path = '/home/hurga/kratosR1/libs/' ##kratos_root/libs
-kratos_applications_path = '/home/hurga/kratos_local/kratos/applications/' ##kratos_root/applications
-#kratos_applications_path = '/home/hurga/kratosR1/applications/' ##kratos_root/applications
+## kratos test examples:
+kratos_root_path = '../../../..'
+##setting up paths
+kratos_libs_path = kratos_root_path+'/libs' ##kratos_root/libs
+kratos_applications_path = kratos_root_path+'/applications' ##kratos_root/applications
 ##################################################################
 ##################################################################
 import sys
 sys.path.append(kratos_libs_path)
 sys.path.append(kratos_applications_path)
 
-#importing Kratos main library
-#from Kratos import *
-#kernel = Kernel()   #defining kernel
-
-#importing applications
-#import applications_interface
-#applications_interface.Import_StructuralApplication = True
-#applications_interface.Import_EkateAuxiliaryApplication = True
-#applications_interface.ImportApplications(kernel, kratos_applications_path)
-#from KratosStructuralApplication import *
-#from KratosExternalSolversApplication import *
-#from KratosEkateAuxiliaryApplication import *
 ##################################################################
 ##################################################################
 sys.path.append('./balken.gid')
