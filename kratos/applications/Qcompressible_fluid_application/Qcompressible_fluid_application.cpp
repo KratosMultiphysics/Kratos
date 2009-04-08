2d0087f953a8ad9dd2dea9b5dc934a163b6debd7
jmarti <jmarti@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-04-08 10:31:59 +0000

    aplicacion de Julio Marti

--- kratos/applications/Qcompressible_fluid_application/Qcompressible_fluid_application.cpp
+++ kratos/applications/Qcompressible_fluid_application/Qcompressible_fluid_application.cpp
@@ -22,7 +22,6 @@
 #include "Qcompressible_fluid_application.h"
 #include "includes/variables.h"
 
-
 namespace Kratos
 {
 
@@ -32,9 +31,15 @@ namespace Kratos
 	KRATOS_CREATE_VARIABLE( double, MACH_NUMBER )
 	KRATOS_CREATE_VARIABLE( double, PRESSURE_COEFFICIENT )
 	KRATOS_CREATE_VARIABLE( double, PRESSURE_OLD_IT )
+	KRATOS_CREATE_VARIABLE( double, PRESSUREAUX_OLD_IT )
 	KRATOS_CREATE_VARIABLE( Vector, BDF_COEFFICIENTS )
 	KRATOS_CREATE_VARIABLE(double, NODAL_MASS)
+	KRATOS_CREATE_VARIABLE(double, NODAL_MASSAUX)
+	KRATOS_CREATE_VARIABLE(double, MASS)
+	//KRATOS_CREATE_VARIABLE(double, NODAL_MAUX)
+	//KRATOS_CREATE_VARIABLE(double, NODAL_PAUX)
 	KRATOS_CREATE_VARIABLE(double, NODAL_PRESS)
+	KRATOS_CREATE_VARIABLE(double, NODAL_PRESSAUX)
 	KRATOS_CREATE_VARIABLE(int, AUX_INDEX)
 	KRATOS_CREATE_VARIABLE(double, EXTERNAL_PRESSURE)
 	KRATOS_CREATE_VARIABLE(double, DIAMETER)
@@ -44,24 +49,30 @@ namespace Kratos
 	KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(AUX_VECTOR)
 
 
-	KratosR1QcompressibleFluidApplication::KratosR1QcompressibleFluidApplication():	
+	KratosQcompressibleFluidApplication::KratosQcompressibleFluidApplication():	
 mQFluid3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element::GeometryType::PointsArrayType(4, Node<3>())))),
 		mQFluid2D(0, Element::GeometryType::Pointer(new Triangle2D3<Node<3> >(Element::GeometryType::PointsArrayType(3, Node<3>()))))
 	{}
 
-	void KratosR1QcompressibleFluidApplication::Register()
+	void KratosQcompressibleFluidApplication::Register()
 	{
-		std::cout << "Initializing KratosR1QcompressibleFluidApplication... " << std::endl;
+		std::cout << "Initializing KratosQcompressibleFluidApplication... " << std::endl;
 		// calling base class register to register Kratos components
 		KratosApplication::Register();
-		std::cout << "Initializing KratosR1QcompressibleFluidApplication...Register completed " << std::endl;
+		std::cout << "Initializing KratosQcompressibleFluidApplication...Register completed " << std::endl;
 		
 		KRATOS_REGISTER_VARIABLE(  MACH_NUMBER )
 		KRATOS_REGISTER_VARIABLE(  PRESSURE_COEFFICIENT )
 		KRATOS_REGISTER_VARIABLE( PRESSURE_OLD_IT )
+		KRATOS_REGISTER_VARIABLE( PRESSUREAUX_OLD_IT )
 		KRATOS_REGISTER_VARIABLE( BDF_COEFFICIENTS )
 		KRATOS_REGISTER_VARIABLE( NODAL_MASS)
+		KRATOS_REGISTER_VARIABLE( NODAL_MASSAUX)
 		KRATOS_REGISTER_VARIABLE( NODAL_PRESS)
+		KRATOS_REGISTER_VARIABLE( NODAL_PRESSAUX)
+		KRATOS_REGISTER_VARIABLE( MASS)
+		//KRATOS_REGISTER_VARIABLE( NODAL_PAUX)
+		//KRATOS_REGISTER_VARIABLE( NODAL_MAUX)
 		KRATOS_REGISTER_VARIABLE( AUX_INDEX)
 		KRATOS_REGISTER_VARIABLE( EXTERNAL_PRESSURE)
 		KRATOS_REGISTER_VARIABLE( DIAMETER)
@@ -76,7 +87,7 @@ mQFluid3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element:
 
 	
 
-		std::cout << "Initializing KratosR1QcompressibleFluidApplication...variables succesfully registered " << std::endl;
+		std::cout << "Initializing KratosQcompressibleFluidApplication...variables succesfully registered " << std::endl;
 
 		// Registering elements and conditions here
 		KRATOS_REGISTER_ELEMENT("QFluid3D", mQFluid3D);
@@ -84,7 +95,7 @@ mQFluid3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element:
 
 		
 		
-		std::cout << "Initializing KratosR1QcompressibleFluidApplication...elements succesfully registered " << std::endl;
+		std::cout << "Initializing KratosQcompressibleFluidApplication...elements succesfully registered " << std::endl;
 	
 	}
 
@@ -94,5 +105,6 @@ mQFluid3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element:
 
 
 }  // namespace Kratos.
+
 
 
