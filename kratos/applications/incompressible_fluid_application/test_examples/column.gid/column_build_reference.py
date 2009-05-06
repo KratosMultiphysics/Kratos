5b335ecf082dcbbc4edf2dff731f8e5de00681bd
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-05-06 19:18:33 +0000

    incompressible_monolithicr.h

--- kratos/applications/incompressible_fluid_application/test_examples/column.gid/column_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for dam2d.py..."
-benchmarking.BuildReferenceData("column.py", "column_ref.txt")
