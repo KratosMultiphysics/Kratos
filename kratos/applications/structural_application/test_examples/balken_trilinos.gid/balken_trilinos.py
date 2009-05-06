37ebe79c5234a186b0dc6f3c303d1cfa22d7ac97
janosch <janosch@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-05-06 14:05:28 +0000

    test example for contact with trilinos application

---
+++ kratos/applications/structural_application/test_examples/balken_trilinos.gid/balken_trilinos.py
@@ -0,0 +1,61 @@
+##################################################################
+##### ekate - Enhanced KRATOS for Advanced Tunnel Enineering #####
+##### copyright by CIMNE, Barcelona, Spain                   #####
+#####          and Janosch Stascheit for TUNCONSTRUCT        #####
+##### all rights reserved                                    #####
+##################################################################
+#setting the domain size for the problem to be solved
+domain_size = 3
+
+#importing MPI ... for this boost 1.35 or superior is needed
+import mpi
+print "i am ",mpi.rank , " of ",mpi.size
+
+
+##################################################################
+##################################################################
+## ATTENTION: here the order is important                    #####
+##################################################################
+## including kratos path                                     #####
+## ATTENTION: the following lines have to be adapted to      #####
+##            match your acrtual configuration               #####
+##################################################################
+## calypso:
+#kratos_root_path = '/home/hurga/kratos_bcn/kratos'
+#kratos_root_path = '/home/hurga/kratos_merged/kratos'
+## alderaan:
+#kratos_root_path = '/localsw/kratos/kratos'
+## r2d2
+#kratos_root_path = '/home/stasch/kratos_bcn/kratos'
+## astra
+kratos_root_path = '/home/stasch/kratos_bcn/kratos'
+##setting up paths
+kratos_libs_path = kratos_root_path+'/libs' ##kratos_root/libs
+kratos_applications_path = kratos_root_path+'/applications' ##kratos_root/applications
+##################################################################
+##################################################################
+import sys
+sys.path.append(kratos_libs_path)
+sys.path.append(kratos_applications_path)
+
+##################################################################
+##################################################################
+sys.path.append('./balken_trilinos.gid')
+import balken_trilinos_include
+from balken_trilinos_include import *
+# calculate insitu-stress for geology_virgin.gid
+model = balken_trilinos_include.Model('balken_trilinos','./')
+model.InitializeModel()
+
+##################################################################
+###  SIMULATION  #################################################
+##################################################################
+time = 0.0
+model.SetCalculateInSituStress( False )
+print( mpi.rank )
+print( model.model_part )
+model.Solve( time, 0, 0, 0, 0 )
+print( model.model_part )
+model.WriteOutput( time )
+print "Calculation done"
+sys.exit(0)
