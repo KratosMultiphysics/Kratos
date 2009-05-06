2497757a42875fb3add867d5bc60edb8714a737f
kazem <kazem@cimne.upc.edu>
2009-05-06 16:39:32 +0000

    incompressible_monolithicr.h

---
+++ applications/incompressible_fluid_application/python_scripts/monolithic_solver_eulerian.py
@@ -0,0 +1,105 @@
+#importing the Kratos Library
+from Kratos import *
+from KratosIncompressibleFluidApplication import *
+from KratosExternalSolversApplication import *
+#from KratosStructuralApplication import *
+
+
+def AddVariables(model_part):
+    model_part.AddNodalSolutionStepVariable(VELOCITY);
+    model_part.AddNodalSolutionStepVariable(ACCELERATION);
+    model_part.AddNodalSolutionStepVariable(MESH_VELOCITY);
+    model_part.AddNodalSolutionStepVariable(PRESSURE);
+    model_part.AddNodalSolutionStepVariable(AIR_PRESSURE);
+    model_part.AddNodalSolutionStepVariable(IS_FLUID);
+    model_part.AddNodalSolutionStepVariable(IS_POROUS);
+    model_part.AddNodalSolutionStepVariable(IS_STRUCTURE);
+    model_part.AddNodalSolutionStepVariable(IS_FREE_SURFACE);
+    model_part.AddNodalSolutionStepVariable(IS_INTERFACE);
+    model_part.AddNodalSolutionStepVariable(IS_BOUNDARY);
+    model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
+    model_part.AddNodalSolutionStepVariable(VISCOSITY);
+    model_part.AddNodalSolutionStepVariable(DENSITY);
+    model_part.AddNodalSolutionStepVariable(DENSITY_AIR);
+    model_part.AddNodalSolutionStepVariable(AIR_SOUND_VELOCITY);
+    model_part.AddNodalSolutionStepVariable(SOUND_VELOCITY);
+    model_part.AddNodalSolutionStepVariable(BODY_FORCE);
+    model_part.AddNodalSolutionStepVariable(NODAL_AREA);
+    model_part.AddNodalSolutionStepVariable(NODAL_H);
+    model_part.AddNodalSolutionStepVariable(ADVPROJ);
+    model_part.AddNodalSolutionStepVariable(DIVPROJ);
+    model_part.AddNodalSolutionStepVariable(THAWONE);
+    model_part.AddNodalSolutionStepVariable(THAWTWO); 
+    model_part.AddNodalSolutionStepVariable(REACTION); 
+    model_part.AddNodalSolutionStepVariable(REACTION_WATER_PRESSURE);
+    model_part.AddNodalSolutionStepVariable(EXTERNAL_PRESSURE);
+    model_part.AddNodalSolutionStepVariable(ARRHENIUS); 
+
+
+    print "variables for the dynamic structural solution added correctly"
+        
+def AddDofs(model_part):
+    for node in model_part.Nodes:
+        #adding dofs
+        node.AddDof(VELOCITY_X,REACTION_X);
+        node.AddDof(VELOCITY_Y,REACTION_Y);
+        node.AddDof(VELOCITY_Z,REACTION_Z);
+        node.AddDof(PRESSURE,REACTION_WATER_PRESSURE);
+	node.AddDof(AIR_PRESSURE,REACTION_AIR_PRESSURE);
+        
+    print "dofs for the monolithic solver added correctly"
+
+class MonolithicSolver:
+    #######################################################################
+    def __init__(self,model_part,domain_size):
+
+        self.model_part = model_part
+
+        self.alpha = -0.3
+        self.move_mesh_strategy = 0
+	self.time_scheme = ResidualBasedPredictorCorrectorVelocityBossakScheme( self.alpha,self.move_mesh_strategy )
+        #definition of the solvers
+        #self.linear_solver =  SkylineLUFactorizationSolver()
+        self.linear_solver =SuperLUSolver()
+        
+        #definition of the convergence criteria
+        self.conv_criteria = UPCriteria(1e-7,1e-9,1e-7,1e-9)
+       # self.conv_criteria = UPCriteria(1e-12,1e-14,1e-15,1e-17)
+
+        self.max_iter = 20
+                            
+        #default settings
+        self.echo_level = 1
+        self.CalculateReactionFlag = False
+        self.ReformDofSetAtEachStep = True
+        self.CalculateNormDxFlag = True
+        self.MoveMeshFlag = False
+    
+
+        
+    #######################################################################
+    def Initialize(self):
+        #creating the solution strategy
+        
+        self.solver = ResidualBasedNewtonRaphsonStrategy(self.model_part,self.time_scheme,self.linear_solver,self.conv_criteria,self.max_iter,self.CalculateReactionFlag, self.ReformDofSetAtEachStep,self.MoveMeshFlag)   
+        (self.solver).SetEchoLevel(self.echo_level)
+
+	                     
+    #######################################################################   
+    def Solve(self):
+
+        (self.solver).Solve()
+
+       
+
+    #######################################################################   
+    def SetEchoLevel(self,level):
+        (self.solver).SetEchoLevel(level)
+    
+    ########################################################################
+
+        
+
+
+
+
