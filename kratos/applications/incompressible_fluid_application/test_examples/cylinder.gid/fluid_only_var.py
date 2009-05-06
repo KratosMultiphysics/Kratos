c65994567d3fb8f269aa71a532b6f6395901de7f
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-05-06 19:13:45 +0000

    incompressible_monolithicr.h

---
+++ kratos/applications/incompressible_fluid_application/test_examples/cylinder.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 2
+
+Dt = 1
+max_time = 10
+SolverType = "monolithic_solver_eulerian"
+laplacian_form = 3
+output_step = 1
+predictor_corrector = True
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 1
+# Declare Python Variables
+
+problem_name="cylinder"
+problem_path="/home/kazem/kratos/applications/incompressible_fluid_application/test_examples/cylinder.gid"
+kratos_path="/home/kazem/kratos"
