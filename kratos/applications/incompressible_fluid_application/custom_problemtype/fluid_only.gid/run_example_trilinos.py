e27bfa385976779ee134d1e5ed1a2059015b89a9
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-05-12 08:38:06 +0000

    correction for automatic generation of parallel run_example_trilinos.py

--- kratos/applications/incompressible_fluid_application/custom_problemtype/fluid_only.gid/run_example_trilinos.py
+++ kratos/applications/incompressible_fluid_application/custom_problemtype/fluid_only.gid/run_example_trilinos.py
@@ -1,4 +1,6 @@
+import mpi
 import fluid_only_var
+
 
 ##################################################################
 ##################################################################
@@ -12,9 +14,13 @@ domain_size = fluid_only_var.domain_size
 #including kratos path
 kratos_libs_path            = fluid_only_var.kratos_path + '/libs' ##kratos_root/libs
 kratos_applications_path    = fluid_only_var.kratos_path + '/applications' ##kratos_root/applications
+kratos_benchmarking_path = fluid_only_var.kratos_path +'/benchmarking' ##kratos_root/benchmarking
 import sys
 sys.path.append(kratos_libs_path)
 sys.path.append(kratos_applications_path)
+sys.path.append(kratos_benchmarking_path)
+
+import benchmarking
 
 #importing Kratos main library
 from Kratos import *
@@ -23,16 +29,29 @@ kernel = Kernel()   #defining kernel
 #importing applications
 import applications_interface
 applications_interface.Import_IncompressibleFluidApplication = True
-applications_interface.Import_ExternalSolversApplication = True
-applications_interface.Import_TrilinosApplication = True
+applications_interface.Import_KratosTrilinosApplication = True
+applications_interface.Import_KratosMetisApplication = True
 applications_interface.ImportApplications(kernel, kratos_applications_path)
 
 ## from now on the order is not anymore crucial
 ##################################################################
 ##################################################################
 from KratosIncompressibleFluidApplication import *
-from KratosExternalSolversApplication import *
 from KratosTrilinosApplication import *
+from KratosMetisApplication import *
+
+##################################################################
+##################################################################
+##def NodeFinder(node_list,X,Y,Z):
+##   for node in node_list:
+##	if((node.X-X)**2 + (node.Y-Y)**2 + (node.Z-Z)**2 < .000001):
+##		return node
+##
+##def BenchmarkCheck(time, node1):
+##    benchmarking.Output(time, "Time")
+##    benchmarking.Output(node1.GetSolutionStepValue(PRESSURE), "Node 1 pressure", 1.0)
+##    benchmarking.Output(node1.GetSolutionStepValue(VELOCITY_Y), "Node 2 velocity_y", 1.0)
+
 
 #defining a model part for the fluid and one for the structure
 fluid_model_part = ModelPart("FluidPart");  
@@ -43,9 +62,9 @@ SolverType = fluid_only_var.SolverType
 if(SolverType == "FractionalStep"):
     import incompressible_fluid_solver
     incompressible_fluid_solver.AddVariables(fluid_model_part)
-elif(SolverType == "Monolithic"):
-    import monolithic_solver_ALE
-    monolithic_solver_ALE.AddVariables(fluid_model_part)
+elif(SolverType == "monolithic_solver_eulerian"):
+    import trilinos_monolithic_solver_eulerian
+    trilinos_monolithic_solver_eulerian.AddVariables(fluid_model_part)
 else:
     raise "solver type not supported: options are FractionalStep - Monolithic"
 
@@ -59,16 +78,43 @@ deformed_mesh_flag = WriteDeformedMeshFlag.WriteUndeformed
 write_conditions = WriteConditionsFlag.WriteElementsOnly
 gid_io = GidIO(input_file_name,gid_mode,multifile,deformed_mesh_flag, write_conditions)
 model_part_io_fluid = ModelPartIO(input_file_name)
-model_part_io_fluid.ReadModelPart(fluid_model_part)
+#model_part_io_fluid.ReadModelPart(fluid_model_part)
+
+
+
+mpi.world.barrier
+print "AAAPAPAPAPAPAAPAPAPAPAPAPAPAPAPAPAPAP"
+
+number_of_partitions = mpi.size #we set it equal to the number of processors
+print "number_of_partitions", number_of_partitions
+partitioner = MetisPartitioningProcess(fluid_model_part, model_part_io_fluid, number_of_partitions, domain_size);
+partitioner.Execute()
+print "GetRank()",GetRank()
+
+#mesh to be printed
+mesh_name = mpi.rank
+gid_io.InitializeMesh( mesh_name)
+gid_io.WriteMesh( (fluid_model_part).GetMesh() )
+gid_io.FinalizeMesh()
+gid_io.Flush()
+
+##mpi.world.barrier()
+##prova
+
+
+##for node in fluid_model_part.Nodes:
+##   print node.X
+##   print node.Y
+##
 
 #setting up the buffer size: SHOULD BE DONE AFTER READING!!!
-fluid_model_part.SetBufferSize(3)
+fluid_model_part.SetBufferSize(2)
 
 ##adding dofs
 if(SolverType == "FractionalStep"):
     incompressible_fluid_solver.AddDofs(fluid_model_part)
-elif(SolverType == "Monolithic"):
-    monolithic_solver_ALE.AddDofs(fluid_model_part)
+elif(SolverType == "monolithic_solver_eulerian"):
+    trilinos_monolithic_solver_eulerian.AddDofs(fluid_model_part)
 
 
 #########select here the laplacian form!!!!!!!!!!!!!!!!!
@@ -84,7 +130,7 @@ for node in fluid_model_part.Nodes:
         raise 'node with zero density found'
     if(node.GetSolutionStepValue(VISCOSITY) == 0.0):
         print "node ",node.Id," has zero viscosity!"
-        raise 'node with zero density found'
+        raise 'node with zero VISCOSITY found'
 
 #creating the solvers
 #fluid solver
@@ -94,10 +140,37 @@ if(SolverType == "FractionalStep"):
     fluid_solver.predictor_corrector = fluid_only_var.predictor_corrector
     fluid_solver.max_press_its = fluid_only_var.max_press_its
     fluid_solver.Initialize()
-elif(SolverType == "Monolithic"):
-    fluid_solver = monolithic_solver_ALE.MonolithicSolver(fluid_model_part,domain_size)
-    fluid_solver.linear_solver = SuperLUSolver()
-    fluid_solver.max_iter = 20   
+elif(SolverType == "monolithic_solver_eulerian"): 
+    fluid_solver = trilinos_monolithic_solver_eulerian.MonolithicSolver(fluid_model_part,domain_size)
+    oss_swith = fluid_only_var.use_oss
+    dynamic_tau = fluid_only_var.dynamic_tau
+    fluid_model_part.ProcessInfo.SetValue(OSS_SWITCH, oss_swith);				
+    fluid_model_part.ProcessInfo.SetValue(DYNAMIC_TAU, dynamic_tau);
+
+
+
+##    ########################################################
+##    #defining the linear solver
+##    aztec_parameters = ParameterList()
+##    aztec_parameters.set("AZ_solver","AZ_gmres");
+##    aztec_parameters.set("AZ_kspace",100);
+##    aztec_parameters.set("AZ_output",32);
+##
+##    preconditioner_type = "Amesos"
+##    preconditioner_parameters = ParameterList()
+##    preconditioner_parameters.set("amesos: solver type", "Amesos_Klu");
+##
+##    ##preconditioner_type = "ILU"
+##    ##preconditioner_parameters = ParameterList()
+##
+##    overlap_level = 3
+##    nit_max = 300
+##    tol = 1e-6
+##
+##    fluid_solver.linear_solver =  AztecSolver(aztec_parameters,preconditioner_type,preconditioner_parameters,tol,nit_max,overlap_level);
+##    ##############################################################
+##    fluid_solver.buildertype = "standard"
+    
     fluid_solver.Initialize()
 
 
@@ -113,13 +186,22 @@ output_step = fluid_only_var.output_step
 out = 0
 
 
-#mesh to be printed
-mesh_name = 0.0
-gid_io.InitializeMesh( mesh_name)
-gid_io.WriteMesh( fluid_model_part.GetMesh() )
-gid_io.FinalizeMesh()
+
+
 
 gid_io.InitializeResults(mesh_name,(fluid_model_part).GetMesh())
+
+###
+##for node in fluid_model_part.Nodes:
+##   print node.X
+##   print node.Y
+##aaa
+
+##############################################################
+##back_node = NodeFinder(fluid_model_part.Nodes , 6.199, 4.0, 0.0)
+##print back_node
+##
+#################################################################
 
 
 time = 0.0
@@ -137,15 +219,39 @@ while(time < final_time):
     if(step >= 3):
         fluid_solver.Solve()
 
+##        gid_io.WriteNodalResults(PRESSURE,fluid_model_part.Nodes,time,0)
+##        gid_io.WriteNodalResults(VELOCITY,fluid_model_part.Nodes,time,0)
+##        BenchmarkCheck(time, back_node)
+
+        
+
     if(out == output_step):
-        gid_io.WriteNodalResults(VELOCITY,fluid_model_part.Nodes,time,0)
         gid_io.WriteNodalResults(PRESSURE,fluid_model_part.Nodes,time,0)
+	gid_io.WriteNodalResults(AIR_PRESSURE,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(VELOCITY,fluid_model_part.Nodes,time,0)
+        #gid_io.WriteNodalResults(DISPLACEMENT,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(MESH_VELOCITY,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(IS_STRUCTURE,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(IS_BOUNDARY,fluid_model_part.Nodes,time,0)
+	gid_io.WriteNodalResults(IS_POROUS,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(IS_FREE_SURFACE,fluid_model_part.Nodes,time,0)
+        #gid_io.PrintOnGaussPoints(THAWONE,fluid_model_part,time)
+        #gid_io.PrintOnGaussPoints(THAWTWO,fluid_model_part,time)
+        gid_io.WriteNodalResults(ADVPROJ,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(DIVPROJ,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(DENSITY,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(DENSITY_AIR,fluid_model_part.Nodes,time,0)
+       ## gid_io.WriteNodalResults(NODAL_H,fluid_model_part.Nodes,time,0)
+        gid_io.WriteNodalResults(VISCOSITY,fluid_model_part.Nodes,time,0)
+        gid_io.Flush()
+
+        mpi.world.barrier
 
         out = 0
 
     out = out + 1
     step = step + 1
-      
+
 gid_io.FinalizeResults()
           
         
