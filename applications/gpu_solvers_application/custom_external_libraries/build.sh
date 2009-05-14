2dfd7108fb3484d7aec89c1f2bd9466e036b3dd0
mossaiby <mossaiby@cimne.upc.edu>
2009-05-14 17:23:06 +0000

    Adding GPU solvers to Kratos

---
+++ applications/gpu_solvers_application/custom_external_libraries/build.sh
@@ -0,0 +1,9 @@
+# Compilation options:
+#
+#   -DUSE_UMUL24: Use __umul24 in GlobalIdx() instead of simple multiplication
+#   -DUSE_TEXTURE_CACHING: Use texture caching in matrix vector multiplication kernels
+#
+# To run, simply issue: ./build.sh
+#
+nvcc gpu_sparse.cu -o gpu_sparse.o -c -O3 -Xcompiler -fPIC -DNDEBUG -DUSE_UMUL24 -DUSE_TEXTURE_CACHING -arch compute_13 -code compute_13 -lcublas
+ar cr libgpu_sparse.a gpu_sparse.o
