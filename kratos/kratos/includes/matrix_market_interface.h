d27fd718c608796769af2dfd0ff9901c10a51d5e
mossaiby <mossaiby@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-06-28 22:51:47 +0000

    Adding matrix_interface_application to Kratos for supporting import / export of various matrix formats, currently MatrixMarket

---
+++ kratos/kratos/includes/matrix_market_interface.h
@@ -0,0 +1,380 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi, Farshid Mossaiby
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+mossaiby@yahoo.com
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+
+
+#if !defined(KRATOS_MATRIX_MARKET_INTERFACE_H_INCLUDED )
+#define  KRATOS_MATRIX_MARKET_INTERFACE_H_INCLUDED
+
+
+// System includes
+#include <stdio.h>
+
+
+// External includes
+
+extern "C" { 
+	#include "mmio.h"
+}
+
+#include <boost/numeric/ublas/matrix_sparse.hpp>
+
+
+// Project includes
+#include "includes/ublas_interface.h"
+
+
+namespace Kratos {
+
+	bool ReadMatrixMarket(char *FileName, CompressedMatrix *m)
+	{
+		// Open MM file for reading
+		FILE *f = fopen(FileName, "r");
+	
+		if (f == NULL)
+		{
+			printf("ReadMatrixMarket(): unable to open %s.\n", FileName);
+			return false;
+		}
+
+		// Process MM file header
+		MM_typecode mm_code;
+	
+		if (mm_read_banner(f, &mm_code) != 0)
+		{
+			printf("ReadMatrixMarket(): unable to read MatrixMarket banner.\n");
+			return false;
+		}
+	
+		if (!mm_is_valid(mm_code))
+		{
+			printf("ReadMatrixMarket(): invalid MatrixMarket banner.\n");
+			return false;
+		}
+	
+		// Check for supported types of MM file
+		if (!((mm_is_real(mm_code) || mm_is_integer(mm_code) || mm_is_pattern(mm_code)) && mm_is_coordinate(mm_code) && mm_is_sparse(mm_code)))
+		{
+			printf("ReadMatrixMarket(): invalid MatrixMarket type, \"%s\".\n",  mm_typecode_to_str(mm_code));
+			return false;
+		}
+	
+		// Read MM dimensions and NNZ
+		int size1, size2, nnz;
+	
+		if (mm_read_mtx_crd_size(f, &size1, &size2, &nnz) != 0)
+		{
+			printf("ReadMatrixMarket(): cannot read dimensions and NNZ.\n");
+			return false;
+		}
+	
+		// Allocate temporary arrays
+		int *I = new int[nnz];
+		int *J = new int[nnz];
+		double *V = new double[nnz];
+	
+		// Read MM file
+	
+		// Pattern file, only non-zero structure
+		if (mm_is_pattern(mm_code))
+			for (int i = 0; i < nnz; i++)
+			{
+				if (fscanf(f, "%d %d", &I[i], &J[i]) != 2)
+				{
+					printf("ReadMatrixMarket(): invalid data.\n");
+				
+					delete[] I;
+					delete[] J;
+					delete[] V;
+				
+					return false;
+				}
+			
+				// Adjust to 0-based
+				I[i]--;
+				J[i]--;
+			
+				// Set all values to 1.00
+				V[i] = 1.00;
+			}
+		else		
+			for (int i = 0; i < nnz; i++)
+			{
+				if (fscanf(f, "%d %d %lg", &I[i], &J[i], &V[i]) != 3)
+				{
+					printf("ReadMatrixMarket(): invalid data.\n");
+				
+					delete[] I;
+					delete[] J;
+					delete[] V;
+				
+					return false;
+				}
+			
+				// Adjust to 0-based
+				I[i]--;
+				J[i]--;
+			}
+	
+		fclose(f);
+	
+		// Second stage
+		int nz[size1];
+	
+		for (int i = 0; i < size1; i++)
+			nz[i] = 0;
+		
+		// Count non-zeros on each line
+		if (mm_is_symmetric(mm_code))
+			for (int i = 0; i < nnz; i++)
+			{
+				if (I[i] == J[i])
+					nz[I[i]]++;
+				else
+				{
+					nz[I[i]]++;
+					nz[J[i]]++;
+				}
+			}
+		else
+			for (int i = 0; i < nnz; i++)
+				nz[I[i]]++;
+
+		// Find out total number of non-zeros			
+		int nnz2;
+	
+		if (mm_is_symmetric(mm_code))
+		{
+			int diagonals = 0;
+		
+			for (int i = 0; i < nnz; i++)
+				if (I[i] == J[i])
+					diagonals++;
+				
+			nnz2 = diagonals + 2 * (nnz - diagonals);
+		}
+		else
+			nnz2 = nnz;
+
+		// Fill in an almost-CSR data structure
+		int *filled = new int[size1];
+		int *indices = new int[size1];
+		int *columns = new int[nnz2];
+		double *values = new double[nnz2];
+	
+		indices[0] = 0;
+		for (int i = 1; i < size1; i++)
+			indices[i] = indices[i - 1] + nz[i - 1];
+		
+		for (int i = 0; i < size1; i++)
+			filled[i] = 0;
+
+		if (mm_is_symmetric(mm_code))
+			for (int i = 0; i < nnz; i++)
+				if (I[i] == J[i])
+				{
+					int index;
+				
+					index = indices[I[i]] + filled[I[i]];
+					columns[index] = J[i];
+					values[index] = V[i];
+					filled[I[i]]++;
+				}
+				else
+				{
+					int index;
+				
+					index = indices[I[i]] + filled[I[i]];
+					columns[index] = J[i];
+					values[index] = V[i];
+					filled[I[i]]++;
+				
+					index = indices[J[i]] + filled[J[i]];
+					columns[index] = I[i];
+					values[index] = V[i];
+					filled[J[i]]++;
+				}
+		else
+			for (int i = 0; i < nnz; i++)
+				{
+					int index;
+				
+					index = indices[I[i]] + filled[I[i]];
+					columns[index] = J[i];
+					values[index] = V[i];
+					filled[I[i]]++;
+				}
+	
+		// Create the matrix
+		m = new CompressedMatrix(size1, size2, nnz2);
+	
+		int k = 0;
+	
+		for (int i = 0; i < size1; i++)
+			for (int j = 0; j < nz[i]; j++)
+				(*m)(i, columns[indices[i] + j]) = values[k++];
+	
+		delete[] I;
+		delete[] J;
+		delete[] V;
+
+		delete[] filled;
+		delete[] indices;
+		delete[] columns;
+		delete[] values;
+
+		return true;
+	}
+
+	bool WriteMatrixMarket(char *FileName, CompressedMatrix &m, bool Symmetric)
+	{
+		// Open MM file for writing
+		FILE *f = fopen(FileName, "w");
+	
+		if (f == NULL)
+		{
+			printf("WriteMatrixMarket(): unable to open %s.\n", FileName);
+			return false;
+		}
+
+		// Write MM file header
+		MM_typecode mm_code;
+	
+		mm_initialize_typecode(&mm_code);	
+	
+		mm_set_matrix(&mm_code);
+		mm_set_coordinate(&mm_code);
+		mm_set_real(&mm_code);
+	
+		if (Symmetric)
+			mm_set_symmetric(&mm_code);
+		else
+			mm_set_general(&mm_code);
+
+		mm_write_banner(f, mm_code);
+		
+		// Find out the actual number of non-zeros in case of a symmetric matrix
+		int nnz;
+		
+		if (Symmetric)
+		{
+			nnz = 0;
+			
+			CompressedMatrix::iterator1 a_iterator = m.begin1();
+
+			for (unsigned int i = 0; i < m.size1(); i++)
+			{
+				#ifndef BOOST_UBLAS_NO_NESTED_CLASS_RELATION
+				for (CompressedMatrix::iterator2 row_iterator = a_iterator.begin(); row_iterator != a_iterator.end(); ++row_iterator) 
+				#else
+				for (CompressedMatrix::iterator2 row_iterator = begin(a_iterator, iterator1_tag()); row_iterator != end(a_iterator, iterator1_tag()); ++row_iterator)
+				#endif
+				{
+					if (a_iterator.index1() >= row_iterator.index2())
+						nnz++;
+				}
+
+			   a_iterator++;
+			}
+		}
+		else
+			nnz = m.nnz();
+		
+		// Write MM file sizes
+		mm_write_mtx_crd_size(f, m.size1(), m.size2(), nnz);
+
+		if (Symmetric)
+		{
+			CompressedMatrix::iterator1 a_iterator = m.begin1();
+
+			for (unsigned int i = 0; i < m.size1(); i++)
+			{
+				#ifndef BOOST_UBLAS_NO_NESTED_CLASS_RELATION
+				for (CompressedMatrix::iterator2 row_iterator = a_iterator.begin(); row_iterator != a_iterator.end(); ++row_iterator) 
+				#else
+				for (CompressedMatrix::iterator2 row_iterator = begin(a_iterator, iterator1_tag()); row_iterator != end(a_iterator, iterator1_tag()); ++row_iterator)
+				#endif
+				{
+					int I = a_iterator.index1(), J = row_iterator.index2();
+				
+					if (I >= J)
+						if (fprintf(f, "%d %d %g\n", I + 1, J + 1, *row_iterator) < 0)
+						{
+							printf("WriteMatrixMarket(): unable to write data.\n");
+							fclose(f);
+							return false;
+						}
+				}
+
+			   a_iterator++;
+			}
+		}
+		else
+		{
+			CompressedMatrix::iterator1 a_iterator = m.begin1();
+
+			for (unsigned int i = 0; i < m.size1(); i++)
+			{
+				#ifndef BOOST_UBLAS_NO_NESTED_CLASS_RELATION
+				for (CompressedMatrix::iterator2 row_iterator = a_iterator.begin(); row_iterator != a_iterator.end(); ++row_iterator) 
+				#else
+				for (CompressedMatrix::iterator2 row_iterator = begin(a_iterator, iterator1_tag()); row_iterator != end(a_iterator, iterator1_tag()); ++row_iterator)
+				#endif
+				{
+					int I = a_iterator.index1(), J = row_iterator.index2();
+				
+					if (fprintf(f, "%d %d %g\n", I + 1, J + 1, *row_iterator) < 0)
+					{
+						printf("WriteMatrixMarket(): unable to write data.\n");
+						fclose(f);
+						return false;
+					}
+				}
+
+			   a_iterator++;
+			}
+		}
+	
+		fclose(f);
+	
+		return true;
+	}
+
+} // namespace Kratos
+
+#endif // KRATOS_MATRIX_MARKET_INTERFACE_H_INCLUDED  defined 
