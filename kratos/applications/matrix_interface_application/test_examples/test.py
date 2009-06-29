7e8dba9f8c52393a5a6368f5c0632fd462fa7d76
mossaiby <mossaiby@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-06-29 06:39:24 +0000

    Updating test example of matrix_interface_application

--- kratos/applications/matrix_interface_application/test_examples/test.py
+++ kratos/applications/matrix_interface_application/test_examples/test.py
@@ -9,17 +9,8 @@ import sys
 sys.path.append(kratos_libs_path)
 sys.path.append(kratos_applications_path)
 
-print "aaa"
-#importing Kratos main library
 from Kratos import *
-print "bbbb"
 kernel = Kernel()   #defining kernel
-print kernel
-print "ccc"
-
-#importing applications
-import applications_interface
-applications_interface.ImportApplications(kernel, kratos_applications_path)
 
 ## from now on the order is not anymore crucial
 ##################################################################
@@ -27,11 +18,42 @@ applications_interface.ImportApplications(kernel, kratos_applications_path)
 
 from KratosMatrixInterfaceApplication import *
 
-# There is a problem here...
-
-# Of course, this is not correct, as we want a new object created...
+# Create an empty CompressedMatrix
 m = CompressedMatrix()
 
-# Also, it seems that m is not touched, despite successful reading of the matrix
-print ReadMatrixMarket("test-general.mtx", m)
+# General case
+
+print "==== Testing general case ========================================="
+
+# Read a test matrix
+if (ReadMatrixMarket("test-general.mtx", m)):
+	print "Matrix read successfully!"
+else:
+	print "Error reading matrix!"
+
+# Print the contents of m
 print m
+
+if (WriteMatrixMarket("test-general-out.mtx", m, False)):
+	print "Matrix written successfully!"
+else:
+	print "Error writing matrix!"
+
+# Symmetric case
+
+print "==== Testing symmetric case ======================================="
+
+# Read a test matrix
+if (ReadMatrixMarket("test-symmetric.mtx", m)):
+	print "Matrix read successfully!"
+else:
+	print "Error reading matrix!"
+
+# Print the contents of m
+print m
+
+if (WriteMatrixMarket("test-symmetric-out.mtx", m, True)):
+	print "Matrix written successfully!"
+else:
+	print "Error writing matrix!"
+
