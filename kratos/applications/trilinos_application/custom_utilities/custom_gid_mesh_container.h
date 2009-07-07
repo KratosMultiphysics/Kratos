27dde5104f9d854e1f4d543f2f52145cbab80025
janosch <janosch@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-07-07 07:58:38 +0000

    deactivation for trilinos application

---
+++ kratos/applications/trilinos_application/custom_utilities/custom_gid_mesh_container.h
@@ -0,0 +1,225 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author: janosch $
+//   Date:                $Date: 2009-01-13 16:51:36 $
+//   Revision:            $Revision: 1.3 $
+//
+//
+
+#if !defined(KRATOS_TRILINOS_GID_MESH_CONTAINER_H_INCLUDED)
+#define  KRATOS_TRILINOS_GID_MESH_CONTAINER_H_INCLUDED
+
+// System includes
+#include <string>
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <cstddef>
+
+// External includes 
+#include "gidpost/source/gidpost.h"
+
+// Project includes
+#include "includes/define.h"
+#include "includes/gid_io.h"
+#include "includes/gid_mesh_container.h"
+#include "geometries/geometry_data.h"
+#include "trilinos_application.h"
+
+namespace Kratos
+{
+    /**
+     * Type definitions
+     */
+    typedef ModelPart::ElementsContainerType ElementsArrayType;
+    typedef ModelPart::NodesContainerType NodesArrayType;
+    typedef ModelPart::ConditionsContainerType ConditionsArrayType;
+    typedef GeometryData::IntegrationMethod IntegrationMethodType;
+    typedef GeometryData::KratosGeometryFamily KratosGeometryFamily;
+    
+    /**
+     * Auxiliary class to store meshes of different element types and to
+     * write these meshes to an output file
+     */
+    class TrilinosGidMeshContainer : public GidMeshContainer
+    {
+        public:
+            ///Constructor
+            TrilinosGidMeshContainer( GeometryData::KratosGeometryType geometryType, 
+                              GiD_ElementType elementType, const char* mesh_title )
+            : GidMeshContainer( geometryType, elementType, mesh_title ){}
+            
+            void WriteMesh(bool deformed)
+            {
+                KRATOS_TRY
+                if( mMeshElements.size() != 0 )
+                {
+                    if( mMeshElements.begin()->GetGeometry().WorkingSpaceDimension() == 2 )
+                    {
+                        std::cout << "writing a 2D mesh" << std::endl;
+                        GiD_BeginMesh( mMeshTitle, GiD_2D, mGidElementType,
+                                       mMeshElements.begin()->GetGeometry().size() );
+                    }
+                    else if( mMeshElements.begin()->GetGeometry().WorkingSpaceDimension() == 3 )
+                    {
+                        std::cout << "writing a 3D mesh" << std::endl;
+                        GiD_BeginMesh( mMeshTitle, GiD_3D, mGidElementType,
+                                       mMeshElements.begin()->GetGeometry().size() );
+                    }
+                    else
+                        KRATOS_ERROR(std::logic_error,"check working space dimension of model","");
+                    //printing nodes
+                    GiD_BeginCoordinates();
+                    for( ModelPart::NodesContainerType::iterator it = mMeshNodes.begin(); 
+                         it != mMeshNodes.end(); ++it )
+                    {
+                        if( deformed )
+                            GiD_WriteCoordinates( (it)->Id(), (it)->X(),
+                                (it)->Y(), (it)->Z());
+                        else
+                            GiD_WriteCoordinates( (it)->Id(), (it)->X0(),
+                                (it)->Y0(), (it)->Z0());
+                    }
+                    GiD_EndCoordinates();
+                    //printing elements
+                    GiD_BeginElements();
+                    int* nodes_id = new int[mMeshElements.begin()->GetGeometry().size()+1];
+                    for( ModelPart::ElementsContainerType::iterator it = mMeshElements.begin(); 
+                         it != mMeshElements.end(); ++it )
+                    {
+                        if( ! it->GetValue( IS_INACTIVE ) )
+                        {
+                            for( unsigned int i=0; i<(it)->GetGeometry().size(); i++ )
+                                nodes_id[i] = (it)->GetGeometry()[i].Id();
+                            //workaround: reordering node ids for Hexahedra20 elements
+                            if( mGeometryType == GeometryData::Kratos_Hexahedra3D20 )
+                            {
+                                nodes_id[12] = (it)->GetGeometry()[16].Id();
+                                nodes_id[13] = (it)->GetGeometry()[17].Id();
+                                nodes_id[14] = (it)->GetGeometry()[18].Id();
+                                nodes_id[15] = (it)->GetGeometry()[19].Id();
+                                nodes_id[16] = (it)->GetGeometry()[12].Id();
+                                nodes_id[17] = (it)->GetGeometry()[13].Id();
+                                nodes_id[18] = (it)->GetGeometry()[14].Id();
+                                nodes_id[19] = (it)->GetGeometry()[15].Id();
+                            }
+                            if( mGeometryType == GeometryData::Kratos_Line2D3 
+                                || mGeometryType == GeometryData::Kratos_Line3D3 )
+                            {
+                                nodes_id[0] = (it)->GetGeometry()[0].Id();
+                                nodes_id[1] = (it)->GetGeometry()[2].Id();
+                                nodes_id[2] = (it)->GetGeometry()[1].Id();
+                            }
+                            nodes_id[(it)->GetGeometry().size()]= (it)->GetProperties().Id();
+                            GiD_WriteElementMat((it)->Id(), nodes_id);
+                        }
+                    }
+                    delete [] nodes_id;
+                    GiD_EndElements();
+                    GiD_EndMesh();
+                }
+                
+                if( mMeshConditions.size() != 0 )
+                {
+                    if( mMeshConditions.begin()->GetGeometry().WorkingSpaceDimension() == 2 )
+                    {
+                        std::cout << "writing a 2D mesh" << std::endl;
+                        GiD_BeginMesh( mMeshTitle, GiD_2D, mGidElementType,
+                                       mMeshConditions.begin()->GetGeometry().size() );
+                    }
+                    else if( mMeshConditions.begin()->GetGeometry().WorkingSpaceDimension() == 3 )
+                    {
+                        std::cout << "writing a 3D mesh" << std::endl;
+                        GiD_BeginMesh( mMeshTitle, GiD_3D, mGidElementType,
+                                       mMeshConditions.begin()->GetGeometry().size() );
+                    }
+                    else
+                        KRATOS_ERROR(std::logic_error,"check working space dimension of model","");
+                    //printing nodes
+                    GiD_BeginCoordinates();
+                    for( ModelPart::NodesContainerType::iterator it = mMeshNodes.begin(); 
+                         it != mMeshNodes.end(); ++it )
+                    {
+                        if( deformed )
+                            GiD_WriteCoordinates( (it)->Id(), (it)->X(),
+                                (it)->Y(), (it)->Z());
+                        else
+                            GiD_WriteCoordinates( (it)->Id(), (it)->X0(),
+                                (it)->Y0(), (it)->Z0());
+                    }
+                    GiD_EndCoordinates();
+                    //printing elements
+                    GiD_BeginElements();
+                    int* nodes_id = new int[mMeshConditions.begin()->GetGeometry().size()+1];
+                    for( ModelPart::ConditionsContainerType::iterator it = mMeshConditions.begin(	); 
+                         it != mMeshConditions.end(); ++it )
+                    {
+                        if( ! it->GetValue( IS_INACTIVE ) )
+                        {
+                            for( unsigned int i=0; i<(it)->GetGeometry().size(); i++ )
+                                nodes_id[i] = (it)->GetGeometry()[i].Id();
+                            //workaround: reordering node ids for Hexahedra20 elements
+                            if( mGeometryType == GeometryData::Kratos_Hexahedra3D20 )
+                            {
+                                nodes_id[12] = (it)->GetGeometry()[16].Id();
+                                nodes_id[13] = (it)->GetGeometry()[17].Id();
+                                nodes_id[14] = (it)->GetGeometry()[18].Id();
+                                nodes_id[15] = (it)->GetGeometry()[19].Id();
+                                nodes_id[16] = (it)->GetGeometry()[12].Id();
+                                nodes_id[17] = (it)->GetGeometry()[13].Id();
+                                nodes_id[18] = (it)->GetGeometry()[14].Id();
+                                nodes_id[19] = (it)->GetGeometry()[15].Id();
+                            }
+                            nodes_id[(it)->GetGeometry().size()]= (it)->GetProperties().Id();
+                            GiD_WriteElementMat((it)->Id(), nodes_id);
+                        }
+                    }
+                    delete [] nodes_id;
+                    GiD_EndElements();
+                    GiD_EndMesh();
+                }
+                KRATOS_CATCH("")
+            }
+    };//class TrilinosGidMeshContainer
+
+}// namespace Kratos.
+
+#endif // KRATOS_TRILINOS_GID_MESH_CONTAINER_H_INCLUDED defined 
---
+++ kratos/applications/trilinos_application/custom_utilities/trilinos_deactivation_utility.h
@@ -0,0 +1,313 @@
+/*
+==============================================================================
+KratosStructuralApplication 
+A library based on:
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi, Janosch Stascheit, Felix Nagel 
+pooyan@cimne.upc.edu 
+rrossi@cimne.upc.edu
+janosch.stascheit@rub.de
+nagel@sd.rub.de
+- CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+- Ruhr-University Bochum, Institute for Structural Mechanics, Germany
+
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author: hurga $
+//   Date:                $Date: 2009-02-26 13:49:42 $
+//   Revision:            $Revision: 1.7 $
+//
+//
+
+
+#if !defined(KRATOS_TRILINOS_DEACTIVATION_UTILITY_INCLUDED )
+#define  KRATOS_TRILINOS_DEACTIVATION_UTILITY_INCLUDED
+
+// System includes
+#include <string>
+#include <iostream> 
+#include <algorithm>
+#include <fstream>
+#include <cmath>
+
+#if !defined(isnan)
+#define isnan(x) ((x)!=(x))
+#endif
+// External includes 
+
+// Project includes
+#include "includes/define.h"
+#include "processes/process.h"
+#include "includes/node.h"
+#include "includes/element.h"
+#include "includes/model_part.h"
+#include "includes/variables.h"
+
+
+namespace Kratos
+{
+    /**
+     * Deactivation and reactivation of elements
+     * This process handles deactivation and reactivation
+     * of elements and associated conditions.
+     * In order to use this process, a variable calles
+     * ACTIVATION_LEVEL of type int has to be defined.
+     * The following values can be assigned to ACTIVATION_LEVEL:
+     * ACTIVATION_LEVEL == 0: element is always active
+     * ACTIVATION_LEVEL > 0: element will be deactivated on demand
+     * ACTIVATION_LEVEL < 0: element is initially deactivated and
+     *                       will be reactivated on demand
+     */
+    class TrilinosDeactivationUtility
+    {
+        public:
+            
+            typedef PointerVectorSet<Element> ElementsArrayType;
+            typedef PointerVectorSet<Condition> ConditionsArrayType;
+            
+            /**
+             * class pointer definition
+             */
+            KRATOS_CLASS_POINTER_DEFINITION( TrilinosDeactivationUtility );
+            
+            /**
+             * Constructor.
+             * The constructor takes the current model_part as argument.
+             * Please note that reactivation of elements does only work
+             * as long as the process that deactivated the elements before
+             * is living.
+                         */
+            TrilinosDeactivationUtility()
+            {
+            }
+            
+            /**
+             * Destructor.
+             */
+            virtual ~TrilinosDeactivationUtility()
+            {
+            }
+            
+            /**
+             * Initializes all elements before performing any calculation.
+             * This is done even for those elements that are deactivated
+             * in the beginning of the simulation
+             */
+            void Initialize( ModelPart& model_part )
+            {
+                std::cout << "initializing deactivation utility" << std::endl;
+                //initializing elements
+                for ( ElementsArrayType::ptr_iterator it=model_part.Elements().ptr_begin();
+                      it!=model_part.Elements().ptr_end(); ++it)
+                {
+                    if( (*it)->GetValue(ACTIVATION_LEVEL) < 0 ) (*it)->SetValue(IS_INACTIVE, true);
+                    else (*it)->SetValue(IS_INACTIVE, false);
+                    (*it)->Initialize();
+                }
+                for ( ConditionsArrayType::ptr_iterator it=model_part.Conditions().ptr_begin();
+                      it != model_part.Conditions().ptr_end(); ++it)
+                {
+                    if( (*it)->GetValue(IS_CONTACT_MASTER) || (*it)->GetValue(IS_CONTACT_SLAVE) )
+                    {
+                        (*it)->SetValue(ACTIVATION_LEVEL, 0 );
+                    }
+                    if( (*it)->GetValue(ACTIVATION_LEVEL) < 0 ) (*it)->SetValue(IS_INACTIVE, true);
+                    else (*it)->SetValue(IS_INACTIVE, false);
+                    (*it)->Initialize();
+                }
+                std::cout << "deactivation utility initialized" << std::endl;
+            }
+            
+            /**
+             * Deactivates all elements and conditions marked with an
+             * activation level in range (from_level, to_level).
+             * Deactivated entities are stored in an intermediate
+             * container and can be restored by calling Reactivate() or
+             * ReactivateAll()
+             */
+            void Deactivate( ModelPart& model_part, int from_level, int to_level )
+            {
+                KRATOS_TRY;
+                
+                //first step: reactivate all elements and conditions
+                ReactivateAll( model_part );
+                //second step: deactivate elements and conditions to be deactivated currently
+                // identify elements to be deactivated
+                for ( ElementsArrayType::ptr_iterator it=model_part.Elements().ptr_begin();
+                      it!=model_part.Elements().ptr_end(); ++it)
+                {
+                    //if element is to be deactivated
+                    if( ( (*it)->GetValue( ACTIVATION_LEVEL ) >= from_level 
+                            && (*it)->GetValue( ACTIVATION_LEVEL ) <= to_level
+                            && (*it)->GetValue( ACTIVATION_LEVEL ) != 0 ) 
+                            || ( (*it)->GetValue( ACTIVATION_LEVEL ) < 0 )
+                      )
+                    {
+			(*it)->GetValue( IS_INACTIVE ) = true;
+                    }
+                }
+                for( ConditionsArrayType::ptr_iterator it = model_part.Conditions().ptr_begin();
+                     it != model_part.Conditions().ptr_end(); ++it )
+                {
+//                     std::cout << "condition: " << (*it)->Id() <<  "has activation level: " << (*it)->GetValue( ACTIVATION_LEVEL ) << std::endl;
+                    if( ( (*it)->GetValue( ACTIVATION_LEVEL ) >= from_level
+                            && (*it)->GetValue( ACTIVATION_LEVEL ) <= to_level
+                            && (*it)->GetValue( ACTIVATION_LEVEL ) != 0 )
+                            || ( (*it)->GetValue( ACTIVATION_LEVEL ) < 0 )
+                      )
+                    {
+                        if( !( (*it)->GetValue( IS_CONTACT_MASTER ) || (*it)->GetValue( IS_CONTACT_SLAVE ) ) )
+                        {
+				(*it)->GetValue( IS_INACTIVE ) = true;
+                        }
+                    }
+                }
+				
+                KRATOS_CATCH("")
+            }
+            
+            /**
+             * Reactivates all elements and conditions stored in the
+             * intermediate containers
+             */
+            void ReactivateAll( ModelPart& model_part )
+            {
+                for ( ElementsArrayType::ptr_iterator it=model_part.Elements().ptr_begin();
+                      it!=model_part.Elements().ptr_end(); ++it)
+			(*it)->GetValue( IS_INACTIVE ) = false;
+                for( ConditionsArrayType::ptr_iterator it = model_part.Conditions().ptr_begin();
+                     it != model_part.Conditions().ptr_end(); ++it )
+			(*it)->GetValue( IS_INACTIVE ) = false;
+            }
+            
+            /**
+             * reactivate all elements with activation level in range
+             * (from_level, to_level)
+             */
+            void Reactivate( ModelPart& model_part, int from_level, int to_level )
+            {
+                KRATOS_TRY;
+                for ( ElementsArrayType::ptr_iterator it=model_part.Elements().ptr_begin();
+                      it!=model_part.Elements().ptr_end(); ++it)
+                {
+                    //if element is to be reactivated
+                    if( (*it)->GetValue( ACTIVATION_LEVEL ) >= from_level 
+                          && (*it)->GetValue( ACTIVATION_LEVEL ) <= to_level)
+                    {
+                        (*it)->SetValue(ACTIVATION_LEVEL, 0 );
+                    }
+                }
+                for( ConditionsArrayType::ptr_iterator it = model_part.Conditions().ptr_begin();
+                     it != model_part.Conditions().ptr_end(); ++it )
+                {
+                    if( ! ( (*it)->GetValue( IS_CONTACT_MASTER ) || (*it)->GetValue(IS_CONTACT_SLAVE) ) )
+                    {
+                        if( (*it)->GetValue( ACTIVATION_LEVEL ) >= from_level
+                              && (*it)->GetValue(ACTIVATION_LEVEL) <= to_level)
+                        {
+                            (*it)->SetValue(ACTIVATION_LEVEL, 0);
+                        }
+                    }
+                }
+                KRATOS_CATCH("");
+            }
+            
+            /**
+             * reactivate all elements with activation level in range
+             * (from_level, to_level)
+             */
+            void ReactivateStressFree( ModelPart& model_part, int from_level, int to_level )
+            {
+                KRATOS_TRY;
+                
+                for ( ElementsArrayType::ptr_iterator it=model_part.Elements().ptr_begin();
+                      it!=model_part.Elements().ptr_end(); ++it)
+                {
+                    //if element is to be reactivated
+                    if( (*it)->GetValue( ACTIVATION_LEVEL ) >= from_level 
+                          && (*it)->GetValue( ACTIVATION_LEVEL ) <= to_level)
+                    {
+                        (*it)->Initialize();
+                        (*it)->SetValue(ACTIVATION_LEVEL, 0 );
+                    }
+                }
+                KRATOS_CATCH("");
+            }
+            
+            /**
+             * Turn back information as a string.
+             */
+            virtual std::string Info() const
+            {
+                return "TrilinosDeactivationUtility";
+            }
+            
+            /**
+             * Print information about this object.
+             */
+            virtual void PrintInfo(std::ostream& rOStream) const
+            {
+                rOStream << "TrilinosDeactivationUtility";
+            }
+            
+            /**
+             * Print object's data.
+             */
+            virtual void PrintData(std::ostream& rOStream) const
+            {
+            }
+		
+        private:
+            
+            /**
+             * Containers for deactivated elements and conditions
+             */
+            PointerVector<Element> mDeactivatedElements;
+            PointerVector<Condition> mDeactivatedConditions;
+            
+            /**
+             * Assignment operator
+             */
+            //TrilinosDeactivationUtility& operator=(TrilinosDeactivationUtility const& rOther);
+            
+            /**
+             * Copy constructor
+             */
+            //TrilinosDeactivationUtility(TrilinosDeactivationUtility const& rOther);
+    
+    };//class TrilinosDeactivationUtility
+
+}  // namespace Kratos.
+
+#endif // KRATOS_TRILINOS_DEACTIVATION_UTILITY_INCLUDED  defined 
