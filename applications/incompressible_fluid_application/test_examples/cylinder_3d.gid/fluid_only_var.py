cbe3bac26bed59cfff00a6e1dec73f950535a31f
kazem <kazem@cimne.upc.edu>
2009-07-28 14:06:44 +0000

    cylinder_3d

---
+++ applications/incompressible_fluid_application/test_examples/cylinder_3d.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 2
+
+Dt = 0.01
+max_time = 1
+SolverType = "monolithic_solver_eulerian"
+laplacian_form = 1
+output_step = 1
+predictor_corrector = True
+max_press_its = 2
+use_oss = 0
+dynamic_tau = 1
+# Declare Python Variables
+
+problem_name="cylinder_3d"
+problem_path="/home/kazem/kratos/applications/incompressible_fluid_application/test_examples/cylinder_3d.gid"
+kratos_path="/home/kazem/kratos"
