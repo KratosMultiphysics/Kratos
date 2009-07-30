ae0f22db63fbc5bad6b75daa4ac62a410c37262f
kazem <kazem@cimne.upc.edu>
2009-07-30 10:15:00 +0000

    variable

--- applications/incompressible_fluid_application/test_examples/dam2d.gid/pfem_var.py
+++ applications/incompressible_fluid_application/test_examples/dam2d.gid/pfem_var.py
@@ -2,18 +2,18 @@ domain_size = 2
 
 max_time = 1.0
 max_x = 2.5
-max_y = 3.0
+max_y = 2.0
 max_z = 1.0
-min_x = -1.0
-min_y = -1.0
-min_z = -1.0
+min_x = 0.0
+min_y = 0.0
+min_z = 0.0
 use_oss = 0
 dynamic_tau = 1
-nsteps = 50
+nsteps = 5000
 output_Dt = 0.01
 min_dt = 0.05
 max_dt = 0.1
-safety_factor = 0.1
+safety_factor = .1
 SolverType = "monolithic_solver_lagrangian"
 laplacian_form = 3
 # Declare Python Variables
