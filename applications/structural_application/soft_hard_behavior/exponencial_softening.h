e6aca68e1b0f805f39368337383f7f593a7cc0d5
Pooyan Dadvand <pooyan@cimne.upc.edu>
2009-07-30 10:17:26 +0000

    New files for softening hardening behavior Nelson

---
+++ applications/structural_application/soft_hard_behavior/exponencial_softening.h
@@ -0,0 +1,84 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+/* *********************************************************
+*
+*   Last Modified by:    $Author: Nelson Lafontaine $
+*   Date:                $Date: 26-06-2009 $
+*   Revision:            $Revision: 1.0 $
+*
+* ***********************************************************/
+
+#if !defined( KRATOS_EXPONENCIAL_SOFTENING_CRITERIA)
+#define KRATOS_EXPONENCIAL_SOFTENING_CRITERIA
+
+/* System includes */
+
+/* External includes */
+#include "boost/smart_ptr.hpp"
+
+#include "custom_utilities/tensor_utils.h"
+#include "includes/ublas_interface.h"
+#include "includes/properties.h"
+#include "soft_hard_behavior/softening_hardening_criteria.h"
+#include <cmath>
+#include <string>
+#include <iostream>
+
+
+
+namespace Kratos
+{
+
+	  class Exponential_Softening: public SofteningHardeningCriteria
+	    {
+	    
+		public: 
+		   Exponential_Softening();
+		  ~Exponential_Softening();
+                   KRATOS_CLASS_POINTER_DEFINITION(Exponential_Softening);
+                   double FunctionSofteningHardeningBehavior(const double& A, const double& r_o, const double& r);
+           };    
+    
+
+    /**
+     * definition of CONSTITUTIVE_LAW variable
+     */
+}  /* namespace Kratos.*/
+#endif /* FLUENCY_CRITERIA defined */
+
---
+++ applications/structural_application/soft_hard_behavior/lineal_softening.cpp
@@ -0,0 +1,87 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+/* *********************************************************
+*
+*   Last Modified by:    $Author: Nelson Lafontaine $
+*   Date:                $Date: 26-06-2009 $
+*   Revision:            $Revision: 1.0 $
+*
+* ***********************************************************/
+
+
+/* System includes */
+
+/* External includes */
+#include "boost/smart_ptr.hpp"
+
+#include "custom_utilities/tensor_utils.h"
+#include "includes/ublas_interface.h"
+#include "includes/properties.h"
+#include "soft_hard_behavior/lineal_softening.h"
+#include <cmath>
+#include <string>
+#include <iostream>
+
+
+namespace Kratos
+{
+	    
+		   Lineal_Softening::Lineal_Softening():SofteningHardeningCriteria() {}
+		   Lineal_Softening::~Lineal_Softening(){}
+               
+		   //Nota : Se require hablar con  Oller para verificar funcionalidad de funcion.q
+
+                   double  Lineal_Softening::FunctionSofteningHardeningBehavior(const double& A, const double& r_o, const double& r)
+		   {
+		      double q_r  =  r_o/(A+1.00);
+                      double a    =  1.00 - q_r/r;
+		      if (a < 0.00) 
+		      {
+		      a = fabs(a);
+		      }
+                      return a;
+		      
+		   }   
+    
+    /**
+     * definition of CONSTITUTIVE_LAW variable
+     */
+}  /* namespace Kratos.*/
+
+
