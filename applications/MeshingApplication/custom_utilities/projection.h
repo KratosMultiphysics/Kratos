ab4952017d3d86f817a325158f1ddc55142e37db
antonia <antonia@cimne.upc.edu>
2009-10-26 14:42:22 +0000

    3d interpolation

--- applications/MeshingApplication/custom_utilities/projection.h
+++ applications/MeshingApplication/custom_utilities/projection.h
@@ -111,7 +111,7 @@ namespace Kratos
 		//If you want to pass the whole model part
 		//**********************************************************************
 		//**********************************************************************
-		void Direct_Interpolation(
+		void DirectInterpolation(
 			ModelPart& rOrigin_ModelPart , 
 			ModelPart& rDestination_ModelPart 
 			)
@@ -185,7 +185,7 @@ namespace Kratos
 			unsigned int MaximumNumberOfResults = 10000;
 			PointVector Results(MaximumNumberOfResults);
 			DistanceVector ResultsDistances(MaximumNumberOfResults);
-			array_1d<double,3> N; //Shape functions vector//
+			array_1d<double,TDim+1> N; //Shape functions vector//
 			int step_data_size = rDestination_ModelPart.GetNodalSolutionStepDataSize();
 
 			for(ModelPart::NodesContainerType::iterator node_it = rDestination_ModelPart.NodesBegin();
@@ -202,33 +202,38 @@ namespace Kratos
 				Geometry<Node<3> >&geom = el_it->GetGeometry();
 				
 				//find the center and "radius" of the element
-				double xc,  yc, radius;	
-				CalculateCenterAndSearchRadius( geom[0].X(), geom[0].Y(),
-								geom[1].X(), geom[1].Y(),
-								geom[2].X(), geom[2].Y(),
-								xc,yc,radius);
-								
+// 				double xc,  yc,  radius;	
+// 				if(TDim == 2)
+// 				{
+// 					 CalculateCenterAndSearchRadius( geom[0].X(), geom[0].Y(),
+// 								geom[1].X(), geom[1].Y(),
+// 								geom[2].X(), geom[2].Y(),
+// 								xc,yc,radius);
+// 	 				work_point.X() = xc; work_point.Y() = yc;
+// 
+// 				}
+// 				else
+// 				{
+// 					 double zc;
+// 					 CalculateCenterAndSearchRadius( geom[0].X(), geom[0].Y(),geom[0].Z(),
+// 								geom[1].X(), geom[1].Y(),geom[1].Z(),
+// 								geom[2].X(), geom[2].Y(),geom[2].Z(),
+// 								geom[3].X(), geom[3].Y(),geom[3].Z(),
+// 								xc,yc,zc, radius);   
+// 					 work_point.X() = xc; work_point.Y() = yc; work_point.Z() = zc;
+// 
+// 				}
+				double xc, yc, zc,  radius;	
+				CalculateCenterAndSearchRadius( geom, xc,yc,zc, radius,N);   
+				work_point.X() = xc; work_point.Y() = yc; work_point.Z() = zc;
+
 				//find all of the new nodes within the radius
 				int number_of_points_in_radius;
-				work_point.X() = xc; work_point.Y() = yc;
 
 				//look between the new nodes which of them is inside the radius of the circumscribed cyrcle
 				number_of_points_in_radius = nodes_tree.SearchInRadius(work_point, radius, Results.begin(),
  						ResultsDistances.begin(),  MaximumNumberOfResults);
-// 				if((el_it) -> Id()==245)
-// 				{
-// 				KRATOS_WATCH("Entra en el elemento 245");	
-// 				}
-// 				if((el_it) -> Id()==307)
-// 				{
-// 				KRATOS_WATCH("Entra en el elemento 307!!!!!!!!!!!!!");	
-// 						KRATOS_WATCH(geom[0].X());
-// 						KRATOS_WATCH(geom[0].Y());
-// 						KRATOS_WATCH(geom[1].X());
-// 						KRATOS_WATCH(geom[1].Y());
-// 					std::cout << "velocity correction time " << vel_time.elapsed() << std::endl;	KRATOS_WATCH(geom[2].X());
-// 						KRATOS_WATCH(geom[2].Y());
-// 				}
+
 
 				//check if inside 
 				for( PointIterator it_found = Results.begin(); it_found != Results.begin() + number_of_points_in_radius; it_found++)
@@ -236,32 +241,31 @@ namespace Kratos
  				
 					bool is_inside = false;
 					//once we are sure the node in inside the circle we have to see if it is inside the triangle i.e. if all the Origin element shape functions are >1
-					is_inside = CalculatePosition(geom[0].X(), geom[0].Y(),
-									geom[1].X(), geom[1].Y(),
-									geom[2].X(), geom[2].Y(),
-									(*it_found)->X(),(*it_found)->Y(),N);
-				
-// 					
-// 					if((*it_found)->Id() == 20163)
-// 					{
-// 						KRATOS_WATCH("*********************************  GOOOOOD");
-// 						KRATOS_WATCH("NODE 20163");
-// 						KRATOS_WATCH(is_inside);
-//  						KRATOS_WATCH(geom[0].X());
-// 						KRATOS_WATCH(geom[0].Y());
-// 						KRATOS_WATCH(geom[1].X());
-// 						KRATOS_WATCH(geom[1].Y());
-// 						KRATOS_WATCH(geom[2].X());
-// 						KRATOS_WATCH(geom[2].Y());
-// 						KRATOS_WATCH(N);
-// 					}
-					//if the node falls inside the element interpolate
-					if(is_inside == true)
-					{
-						//Interpolating all the rVariables of the rOrigin_ModelPart to get their nodal value in the rDestination_ModelPart
-						Interpolate(  el_it,  N, step_data_size, *it_found );
-						
-					}
+// 				        if(TDim == 2)
+// 				        {
+// 					       is_inside = CalculatePosition(geom[0].X(), geom[0].Y(),
+// 									       geom[1].X(), geom[1].Y(),
+// 									       geom[2].X(), geom[2].Y(),
+// 									       (*it_found)->X(),(*it_found)->Y(),N);
+// 				        
+// 				        }
+// 				        else
+// 				        {
+// 					       is_inside = CalculatePosition(geom[0].X(), geom[0].Y(), geom[0].Z(),
+// 									       geom[1].X(), geom[1].Y(), geom[1].Z(),
+// 									       geom[2].X(), geom[2].Y(), geom[2].Z(),
+// 									       geom[3].X(), geom[3].Y(), geom[3].Z(),
+// 									       (*it_found)->X(),(*it_found)->Y(),(*it_found)->Z(),N);
+// 				        
+// 				        }
+				        is_inside = CalculatePosition(geom, (*it_found)->X(),(*it_found)->Y(),(*it_found)->Z(),N);
+				        //if the node falls inside the element interpolate
+				        if(is_inside == true)
+				        {
+						        //Interpolating all the rVariables of the rOrigin_ModelPart to get their nodal value in the rDestination_ModelPart
+						        Interpolate(  el_it,  N, step_data_size, *it_found );
+						        
+				        }
 				}
  			}		
 			
@@ -273,7 +277,7 @@ namespace Kratos
 		//**********************************************************************
 		//**********************************************************************
 		template<class TDataType>
-		void Direct_Variable_Interpolation(
+		void DirectVariableInterpolation(
 			ModelPart& rOrigin_ModelPart , 
 			ModelPart& rDestination_ModelPart,
 			Variable<TDataType>& rVariable 
@@ -349,7 +353,7 @@ namespace Kratos
 			unsigned int MaximumNumberOfResults = 10000;
 			PointVector Results(MaximumNumberOfResults);
 			DistanceVector ResultsDistances(MaximumNumberOfResults);
-			array_1d<double,3> N; //Shape functions vector//
+			array_1d<double,TDim+1> N; //Shape functions vector//
 			//int step_data_size = rDestination_ModelPart.GetNodalSolutionStepDataSize();
 			//unsigned int TDim = 3;
 
@@ -360,15 +364,33 @@ namespace Kratos
 				Geometry<Node<3> >&geom = el_it->GetGeometry();
 				
 				//find the center and "radius" of the element
-				double xc,  yc, radius;	
-				CalculateCenterAndSearchRadius( geom[0].X(), geom[0].Y(),
-								geom[1].X(), geom[1].Y(),
-								geom[2].X(), geom[2].Y(),
-								xc,yc,radius);
-								
+// 				double xc,  yc,  radius;	
+// 				if(TDim == 2)
+// 				{
+// 					 CalculateCenterAndSearchRadius( geom[0].X(), geom[0].Y(),
+// 								geom[1].X(), geom[1].Y(),
+// 								geom[2].X(), geom[2].Y(),
+// 								xc,yc,radius);
+// 	 				 work_point.X() = xc; work_point.Y() = yc;
+// 
+// 				}
+// 				else
+// 				{
+// 					 double zc;
+// 					 CalculateCenterAndSearchRadius( geom[0].X(), geom[0].Y(),geom[0].Z(),
+// 								geom[1].X(), geom[1].Y(),geom[1].Z(),
+// 								geom[2].X(), geom[2].Y(),geom[2].Z(),
+// 								geom[3].X(), geom[3].Y(),geom[3].Z(),
+// 								xc,yc,zc, radius);   
+// 					 work_point.X() = xc; work_point.Y() = yc; work_point.Z() = zc;
+// 
+// 				}
+				double xc, yc, zc,  radius;	
+				CalculateCenterAndSearchRadius( geom, xc,yc,zc, radius, N);   
+				work_point.X() = xc; work_point.Y() = yc; work_point.Z() = zc; 
+ 
 				//find all of the new nodes within the radius
 				int number_of_points_in_radius;
-				work_point.X() = xc; work_point.Y() = yc;
 
 				//look between the new nodes which of them is inside the radius of the circumscribed cyrcle
 				number_of_points_in_radius = nodes_tree.SearchInRadius(work_point, radius, Results.begin(),
@@ -380,11 +402,25 @@ namespace Kratos
  				
 					bool is_inside = false;
 					//once we are sure the node in inside the circle we have to see if it is inside the triangle i.e. if all the Origin element shape functions are >1
-					is_inside = CalculatePosition(geom[0].X(), geom[0].Y(),
-									geom[1].X(), geom[1].Y(),
-									geom[2].X(), geom[2].Y(),
-									(*it_found)->X(),(*it_found)->Y(),N);
-				
+// 					 if(TDim == 2)
+// 					 {
+// 						is_inside = CalculatePosition(geom[0].X(), geom[0].Y(),
+// 										geom[1].X(), geom[1].Y(),
+// 										geom[2].X(), geom[2].Y(),
+// 										(*it_found)->X(),(*it_found)->Y(),N);
+// 					 
+// 					 }
+// 					 else
+// 					 {
+// 						is_inside = CalculatePosition(geom[0].X(), geom[0].Y(), geom[0].Z(),
+// 										geom[1].X(), geom[1].Y(), geom[1].Z(),
+// 										geom[2].X(), geom[2].Y(), geom[2].Z(),
+// 										geom[3].X(), geom[3].Y(), geom[3].Z(),
+// 										(*it_found)->X(),(*it_found)->Y(),(*it_found)->Z(),N);
+// 					 
+// 					 }
+				        is_inside = CalculatePosition(geom, (*it_found)->X(),(*it_found)->Y(),(*it_found)->Z(),N);
+
 					//if the node falls inside the element interpolate
 					if(is_inside == true)
 					{//CANCELLA insert the variable TDim
@@ -477,15 +513,19 @@ namespace Kratos
 		///@} 
 		///@name Member rVariables 
 		///@{ 
-		inline void CalculateCenterAndSearchRadius(const double x0, const double y0,
-						const double x1, const double y1,
-    						const double x2, const double y2,
-	  					double& xc, double& yc, double& R
+		inline void CalculateCenterAndSearchRadius(Geometry<Node<3> >&geom,
+	  					double& xc, double& yc, double& zc, double& R, array_1d<double,3>& N		
 					       )
 		{
+			 double x0 = geom[0].X();double  y0 = geom[0].Y(); 
+			 double x1 = geom[1].X();double  y1 = geom[1].Y(); 
+			 double x2 = geom[2].X();double  y2 = geom[2].Y(); 
+			 
+
 			xc = 0.3333333333333333333*(x0+x1+x2);
 			yc = 0.3333333333333333333*(y0+y1+y2);
-			 
+			zc = 0.0;
+
 			double R1 = (xc-x0)*(xc-x0) + (yc-y0)*(yc-y0);
 			double R2 = (xc-x1)*(xc-x1) + (yc-y1)*(yc-y1);
 			double R3 = (xc-x2)*(xc-x2) + (yc-y2)*(yc-y2);
@@ -496,7 +536,37 @@ namespace Kratos
 			
 			R = 1.01 * sqrt(R);
 		}
-		
+		//***************************************
+		//***************************************
+		inline void CalculateCenterAndSearchRadius(Geometry<Node<3> >&geom,
+	  					double& xc, double& yc, double& zc, double& R, array_1d<double,4>& N		
+
+					       )
+		{
+			 double x0 = geom[0].X();double  y0 = geom[0].Y();double  z0 = geom[0].Z();
+			 double x1 = geom[1].X();double  y1 = geom[1].Y();double  z1 = geom[1].Z();
+			 double x2 = geom[2].X();double  y2 = geom[2].Y();double  z2 = geom[2].Z();
+			 double x3 = geom[3].X();double  y3 = geom[3].Y();double  z3 = geom[3].Z();	
+
+
+			xc = 0.25*(x0+x1+x2+x3);
+			yc = 0.25*(y0+y1+y2+y3);
+			zc = 0.25*(z0+z1+z2+z3);			 
+
+			double R1 = (xc-x0)*(xc-x0) + (yc-y0)*(yc-y0) + (zc-z0)*(zc-z0);
+			double R2 = (xc-x1)*(xc-x1) + (yc-y1)*(yc-y1) + (zc-z1)*(zc-z1);
+			double R3 = (xc-x2)*(xc-x2) + (yc-y2)*(yc-y2) + (zc-z2)*(zc-z2);
+			double R4 = (xc-x3)*(xc-x3) + (yc-y3)*(yc-y3) + (zc-z3)*(zc-z3);
+			
+			R = R1;
+			if(R2 > R) R = R2;
+			if(R3 > R) R = R3;
+			if(R4 > R) R = R4;
+			 
+			R = sqrt(R);
+		}
+		//***************************************
+		//***************************************
 		inline double CalculateVol(	const double x0, const double y0,
 						const double x1, const double y1,
     						const double x2, const double y2
@@ -504,14 +574,42 @@ namespace Kratos
 		{
 			return 0.5*( (x1-x0)*(y2-y0)- (y1-y0)*(x2-x0) );
 		}
-		
-		inline bool CalculatePosition(	const double x0, const double y0,
-						const double x1, const double y1,
-   						const double x2, const double y2,
-						const double xc, const double yc,
+		//***************************************
+		//***************************************
+		inline double CalculateVol(	const double x0, const double y0, const double z0,
+						const double x1, const double y1, const double z1,
+    						const double x2, const double y2, const double z2,
+    						const double x3, const double y3, const double z3
+					  )
+		{
+			double x10 = x1 - x0;
+			double y10 = y1 - y0;
+			double z10 = z1 - z0;
+
+			double x20 = x2 - x0;
+			double y20 = y2 - y0;
+			double z20 = z2 - z0;
+
+			double x30 = x3 - x0;
+			double y30 = y3 - y0;
+			double z30 = z3 - z0;
+
+			double detJ = x10 * y20 * z30 - x10 * y30 * z20 + y10 * z20 * x30 - y10 * x20 * z30 + z10 * x20 * y30 - z10 * y20 * x30;
+			return  detJ*0.1666666666666666666667;
+			
+			//return 0.5*( (x1-x0)*(y2-y0)- (y1-y0)*(x2-x0) );
+		}
+		//***************************************
+		//***************************************
+		inline bool CalculatePosition(	Geometry<Node<3> >&geom,
+						const double xc, const double yc, const double zc,
 						array_1d<double,3>& N		
 					  )
 		{
+			 double x0 = geom[0].X();double  y0 = geom[0].Y(); 
+			 double x1 = geom[1].X();double  y1 = geom[1].Y(); 
+			 double x2 = geom[2].X();double  y2 = geom[2].Y(); 
+			 
 			double area = CalculateVol(x0,y0,x1,y1,x2,y2);
 			double inv_area = 0.0;
 			if(area == 0.0)
@@ -535,12 +633,51 @@ namespace Kratos
 			return false;
 		}	
 		
+		//***************************************
+		//***************************************
 
+		inline bool CalculatePosition(	Geometry<Node<3> >&geom,
+						const double xc, const double yc, const double zc,
+						array_1d<double,4>& N		
+					  )
+		{ 
+			
+			 double x0 = geom[0].X();double  y0 = geom[0].Y();double  z0 = geom[0].Z();
+			 double x1 = geom[1].X();double  y1 = geom[1].Y();double  z1 = geom[1].Z();
+			 double x2 = geom[2].X();double  y2 = geom[2].Y();double  z2 = geom[2].Z();
+			 double x3 = geom[3].X();double  y3 = geom[3].Y();double  z3 = geom[3].Z();	
+	
+			double vol = CalculateVol(x0,y0,z0,x1,y1,z1,x2,y2,z2,x3,y3,z3);
+
+			double inv_vol = 0.0;
+			if(vol < 0.0000000000001)
+			  {
+				KRATOS_ERROR(std::logic_error,"element with zero vol found","");
+			  }
+			else
+			  {
+				inv_vol = 1.0 / vol;
+			  }
+			
+			  N[0] = CalculateVol(x1,y1,z1,x3,y3,z3,x2,y2,z2,xc,yc,zc) * inv_vol;
+			  N[1] = CalculateVol(x0,y0,z0,x1,y1,z1,x2,y2,z2,xc,yc,zc) * inv_vol;
+			  N[2] = CalculateVol(x3,y3,z3,x1,y1,z1,x0,y0,z0,xc,yc,zc) * inv_vol;
+	   		  N[3] = CalculateVol(x3,y3,z3,x0,y0,z0,x2,y2,z2,xc,yc,zc) * inv_vol;
+
+						
+			if(N[0] >= 0.0 && N[1] >= 0.0 && N[2] >= 0.0 && N[3] >=0.0 &&
+			   N[0] <= 1.0 && N[1] <= 1.0 && N[2] <= 1.0 && N[3] <=1.0)
+			//if the xc yc zc is inside the tetrahedron return true
+				return true;
+			
+			return false;
+		}	
 //el_it		     	Element iterator
 //N			Shape functions
 //step_data_size	
 //pnode			pointer to the node
-		//total model part
+
+		//projecting total model part 2Dversion
 		void Interpolate( 
 				ModelPart::ElementsContainerType::iterator el_it, 
 				const array_1d<double,3>& N, 
@@ -568,10 +705,38 @@ namespace Kratos
 				}						
 			}				
 		}
-		
+		//projecting total model part 3Dversion
+		void Interpolate( 
+				ModelPart::ElementsContainerType::iterator el_it, 
+				const array_1d<double,4>& N, 
+				int step_data_size,
+      				Node<3>::Pointer pnode)//dimension or number of nodes???
+		{
+			//Geometry element of the rOrigin_ModelPart
+			Geometry< Node<3> >& geom = el_it->GetGeometry();
+			
+			unsigned int buffer_size = pnode->GetBufferSize();
+			
+			for(unsigned int step = 0; step<buffer_size; step++)
+			{
+				//getting the data of the solution step
+				double* step_data = (pnode)->SolutionStepData().Data(step);
+				
+				double* node0_data = geom[0].SolutionStepData().Data(step);
+				double* node1_data = geom[1].SolutionStepData().Data(step);
+				double* node2_data = geom[2].SolutionStepData().Data(step);
+				double* node3_data = geom[3].SolutionStepData().Data(step);
+
+				//copying this data in the position of the vector we are interested in
+				for(int j= 0; j< step_data_size; j++)
+				{
+					step_data[j] = N[0]*node0_data[j] + N[1]*node1_data[j] + N[2]*node2_data[j] + N[3]*node3_data[j];
+				}						
+			}				
+		}
 
 
-		//array1D
+		//projecting an array1D 2Dversion
 		 void Interpolate( 
 				ModelPart::ElementsContainerType::iterator el_it, 
 				const array_1d<double,3>& N, 
@@ -600,7 +765,36 @@ namespace Kratos
 			}				
 		}
 
-		//scalar
+		//projecting an array1D 3Dversion
+		 void Interpolate( 
+				ModelPart::ElementsContainerType::iterator el_it, 
+				const array_1d<double,4>& N, 
+      				Node<3>::Pointer pnode,
+				Variable<array_1d<double,3> >& rVariable)
+		{
+			//Geometry element of the rOrigin_ModelPart
+			Geometry< Node<3> >& geom = el_it->GetGeometry();
+			
+			unsigned int buffer_size = pnode->GetBufferSize();
+
+			for(unsigned int step = 0; step<buffer_size; step++)
+			{
+				//getting the data of the solution step
+				array_1d<double,3>& step_data = (pnode)->FastGetSolutionStepValue(rVariable , step);
+				//Reference or no reference???//CANCELLA
+				array_1d<double,3>& node0_data = geom[0].FastGetSolutionStepValue(rVariable , step);
+				array_1d<double,3>& node1_data = geom[1].FastGetSolutionStepValue(rVariable , step);
+				array_1d<double,3>& node2_data = geom[2].FastGetSolutionStepValue(rVariable , step);
+				array_1d<double,3>& node3_data = geom[3].FastGetSolutionStepValue(rVariable , step);
+
+				//copying this data in the position of the vector we are interested in
+				for(unsigned int j= 0; j< TDim; j++)
+				{
+					step_data[j] = N[0]*node0_data[j] + N[1]*node1_data[j] + N[2]*node2_data[j] + N[3]*node3_data[j];
+				}						
+			}				
+		}
+		//projecting a scalar 2Dversion
 		void Interpolate( 
 				ModelPart::ElementsContainerType::iterator el_it, 
 				const array_1d<double,3>& N, 
@@ -627,7 +821,34 @@ namespace Kratos
 										
 			}				
 		}
+		//projecting a scalar 3Dversion
+		void Interpolate( 
+				ModelPart::ElementsContainerType::iterator el_it, 
+				const array_1d<double,4>& N, 
+      				Node<3>::Pointer pnode,
+				Variable<double>& rVariable)
+		{
+			//Geometry element of the rOrigin_ModelPart
+			Geometry< Node<3> >& geom = el_it->GetGeometry();
+			
+			unsigned int buffer_size = pnode->GetBufferSize();
+		//facendo un loop sugli step temporali step_data come salva i dati al passo anteriore? Cioś dove passiamo l'informazione ai nodi???
+			for(unsigned int step = 0; step<buffer_size; step++)
+			{
+				//getting the data of the solution step
+				double& step_data = (pnode)->FastGetSolutionStepValue(rVariable , step);
+				//Reference or no reference???//CANCELLA
+				double& node0_data = geom[0].FastGetSolutionStepValue(rVariable , step);
+				double& node1_data = geom[1].FastGetSolutionStepValue(rVariable , step);
+				double& node2_data = geom[2].FastGetSolutionStepValue(rVariable , step);
+				double& node3_data = geom[3].FastGetSolutionStepValue(rVariable , step);
 
+				//copying this data in the position of the vector we are interested in
+				
+				step_data = N[0]*node0_data + N[1]*node1_data + N[2]*node2_data + N[3]*node3_data; 
+										
+			}				
+		}
 		inline void Clear(ModelPart::NodesContainerType::iterator node_it,  int step_data_size )	
 		{
 			unsigned int buffer_size = node_it->GetBufferSize();
