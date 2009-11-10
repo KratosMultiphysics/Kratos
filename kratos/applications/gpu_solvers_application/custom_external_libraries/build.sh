353860ef85ac8d289f23d16733c2ec4d39ef27cd
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-11-10 11:44:04 +0000

    merging Isaac's and Farshid's version.

--- kratos/applications/gpu_solvers_application/custom_external_libraries/build.sh
+++
@@ -1,9 +0,0 @@
-# Compilation options:
-#
-#   -DUSE_UMUL24: Use __umul24 in GlobalIdx() instead of simple multiplication
-#   -DUSE_TEXTURE_CACHING: Use texture caching in matrix vector multiplication kernels
-#
-# To run, simply issue: ./build.sh
-#
-nvcc gpu_sparse.cu -o gpu_sparse.o -c -g -Xcompiler -fPIC -DNDEBUG -DUSE_UMUL24 -DUSE_TEXTURE_CACHING -arch compute_13 -code compute_13 -lcublas -I/usr/local/lapackpp/include/lapackpp
-ar cr libgpu_sparse.a gpu_sparse.o
