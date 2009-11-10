74b38cb47ec4eddcdf6a8bb4f84663e42b69f2b4
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-11-10 11:38:31 +0000

    adding the custom_linear_solvers dir to gpu app

---
+++ kratos/applications/gpu_solvers_application/custom_linear_solvers/gpu_cg_solver.h
@@ -0,0 +1,583 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author: rrossi $
+//   Date:                $Date: 2007-03-06 10:30:33 $
+//   Revision:            $Revision: 1.3 $
+//
+//
+
+
+#if !defined(KRATOS_GPU_CG_SOLVER_H_INCLUDED )
+#define  KRATOS_GPU_CG_SOLVER_H_INCLUDED
+
+
+
+// System includes
+#include <string>
+#include <iostream> 
+#include <ctime>
+using namespace std;
+// External includes 
+
+
+// Project includes
+#include "includes/define.h"
+#include "linear_solvers/iterative_solver.h"
+#include "linear_solvers.h"
+
+
+//using namespace Kratos::GPUSparse;
+
+
+namespace Kratos
+{
+
+  ///@name Kratos Globals
+  ///@{ 
+  
+  ///@} 
+  ///@name Type Definitions
+  ///@{ 
+  
+  ///@} 
+  ///@name  Enum's
+  ///@{
+      
+  ///@}
+  ///@name  Functions 
+  ///@{
+      
+  ///@}
+  ///@name Kratos Classes
+  ///@{
+  
+  /// Short class definition.
+  /** Detail class definition.
+  */
+ /* template<class TSparseSpaceType, class TDenseSpaceType, 
+    class TPreconditionerType = Preconditioner<TSparseSpaceType, TDenseSpaceType>, 
+    class TReordererType = Reorderer<TSparseSpaceType, TDenseSpaceType> >
+    class CGSolver : public IterativeSolver<TSparseSpaceType, TDenseSpaceType, TPreconditionerType, TReordererType>*/
+    template<class TSparseSpaceType, class TDenseSpaceType, 
+    class TPreconditionerType = Preconditioner<TSparseSpaceType, TDenseSpaceType>, 
+    class TReordererType = Reorderer<TSparseSpaceType, TDenseSpaceType> >
+    class GPUCGSolver : public IterativeSolver<TSparseSpaceType, TDenseSpaceType, TPreconditionerType, TReordererType>
+    {
+      public:
+      ///@name Type Definitions
+      ///@{
+      
+      /// Pointer definition of CGSolver
+      KRATOS_CLASS_POINTER_DEFINITION(GPUCGSolver);
+
+      typedef IterativeSolver<TSparseSpaceType, TDenseSpaceType, TPreconditionerType, TReordererType> BaseType; 
+  
+      typedef typename TSparseSpaceType::MatrixType SparseMatrixType;
+  
+      typedef typename TSparseSpaceType::VectorType VectorType;
+  
+      typedef typename TDenseSpaceType::MatrixType DenseMatrixType;
+  
+      ///@}
+      ///@name Life Cycle 
+      ///@{ 
+      
+      /// Default constructor.
+      GPUCGSolver(){}
+
+      GPUCGSolver(double NewMaxTolerance) : BaseType(NewMaxTolerance)
+	{
+		havePreconditioner = false;
+		maxIter = 5000;		
+		tol = NewMaxTolerance;
+		this->preconditioner = 0;
+	}
+
+      GPUCGSolver(double NewMaxTolerance, unsigned int NewMaxIterationsNumber) : BaseType(NewMaxTolerance, NewMaxIterationsNumber){
+		havePreconditioner = false;
+		maxIter = NewMaxIterationsNumber;		
+		tol = NewMaxTolerance;
+		this->preconditioner = 0;
+      }
+	//GPUCG constructor with new preconditioner
+      GPUCGSolver(double NewMaxTolerance, unsigned int NewMaxIterationsNumber, GPUPreconditioner& _preconditioner) : BaseType(NewMaxTolerance, NewMaxIterationsNumber)
+	{
+		havePreconditioner = true;
+		this->preconditioner = &_preconditioner;
+		maxIter = NewMaxIterationsNumber;
+		tol = NewMaxTolerance;
+      }
+
+      GPUCGSolver(double NewMaxTolerance, unsigned int NewMaxIterationsNumber, typename TPreconditionerType::Pointer pNewPreconditioner) : 
+      BaseType(NewMaxTolerance, NewMaxIterationsNumber, pNewPreconditioner){}
+
+      /// Copy constructor.
+      GPUCGSolver(const GPUCGSolver& Other) : BaseType(Other) {}
+
+
+      /// Destructor.
+      virtual ~GPUCGSolver(){  }
+      
+
+      ///@}
+      ///@name Operators 
+      ///@{
+      
+      /// Assignment operator.
+      GPUCGSolver& operator=(const GPUCGSolver& Other)
+      {
+        BaseType::operator=(Other);
+	return *this;
+      }
+      
+      ///@}
+      ///@name Operations
+      ///@{
+      
+      /** Normal solve method.
+	  Solves the linear system Ax=b and puts the result on SystemVector& rX. 
+	  rX is also th initial guess for iterative methods.
+	  @param rA. System matrix
+	  @param rX. Solution vector. it's also the initial 
+	  guess for iterative linear solvers.
+ 	  @param rB. Right hand side vector.
+      */
+      bool Solve(SparseMatrixType& rA, VectorType& rX, VectorType& rB)
+	{
+	  if(IsNotConsistent(rA, rX, rB))
+	  {
+	    KRATOS_WATCH(rA.size1());
+	    KRATOS_WATCH(rA.size2());
+	    KRATOS_WATCH(rX.size());
+	    KRATOS_WATCH(rB.size());
+	    std::cout << "system is not consistent" << std::endl;
+	    return false;
+	  }
+	  
+// 	  GetTimeTable()->Start(Info());
+
+	  BaseType::GetPreconditioner()->Initialize(rA,rX,rB);
+ 	  BaseType::GetPreconditioner()->ApplyInverseRight(rX);
+	  BaseType::GetPreconditioner()->ApplyLeft(rB);
+
+	  bool is_solved = IterativeSolve(rA,rX,rB);
+
+ 	  BaseType::GetPreconditioner()->Finalize(rX);
+
+// 	  GetTimeTable()->Stop(Info());
+
+	  return is_solved;
+	}
+      
+      
+      /** Multi solve method for solving a set of linear systems with same coefficient matrix.
+	  Solves the linear system Ax=b and puts the result on SystemVector& rX. 
+	  rX is also th initial guess for iterative methods.
+	  @param rA. System matrix
+	  @param rX. Solution vector. it's also the initial 
+	  guess for iterative linear solvers.
+ 	  @param rB. Right hand side vector.
+      */
+      bool Solve(SparseMatrixType& rA, DenseMatrixType& rX, DenseMatrixType& rB)
+	{
+// 	  GetTimeTable()->Start(Info());
+
+   	  BaseType::GetPreconditioner()->Initialize(rA,rX,rB);
+
+ 	  bool is_solved = true;
+	  VectorType x(TDenseSpaceType::Size1(rX));
+	  VectorType b(TDenseSpaceType::Size1(rB));
+	  for(unsigned int i = 0 ; i < TDenseSpaceType::Size2(rX) ; i++)
+	    {
+	      TDenseSpaceType::GetColumn(i,rX, x);
+	      TDenseSpaceType::GetColumn(i,rB, b);
+	      
+	      BaseType::GetPreconditioner()->ApplyInverseRight(x);
+	      BaseType::GetPreconditioner()->ApplyLeft(b);
+
+	      is_solved &= IterativeSolve(rA,x,b);
+
+	      BaseType::GetPreconditioner()->Finalize(x);
+	    }
+
+// 	  GetTimeTable()->Stop(Info());
+
+	  return is_solved;
+	}
+      
+      ///@}
+      ///@name Access
+      ///@{ 
+      
+      
+      ///@}
+      ///@name Inquiry
+      ///@{
+      
+      
+      ///@}      
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+      virtual std::string Info() const
+	{
+	  std::stringstream buffer;
+	  buffer << "Conjugate gradient linear solver with " << BaseType::GetPreconditioner()->Info();
+	  return  buffer.str();
+	}
+      
+      /// Print information about this object.
+      virtual void PrintInfo(std::ostream& rOStream) const
+	{
+	  rOStream << Info();
+	}
+
+      /// Print object's data.
+      virtual void PrintData(std::ostream& rOStream) const
+	{
+	  BaseType::PrintData(rOStream);
+	}
+      
+            
+      ///@}      
+      ///@name Friends
+      ///@{
+      
+            
+      ///@}
+      
+    protected:
+      ///@name Protected static Member Variables 
+      ///@{ 
+        //This var control preconditioner 
+	bool havePreconditioner;
+	GPUPreconditioner *preconditioner;
+        size_t maxIter;
+	double tol;
+      ///@} 
+      ///@name Protected member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected  Access 
+      ///@{ 
+        
+        
+      ///@}      
+      ///@name Protected Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Protected LifeCycle 
+      ///@{ 
+      
+            
+      ///@}
+      
+    private:
+      ///@name Static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private Operations
+      ///@{ 
+
+      bool IterativeSolve(SparseMatrixType& rA, VectorType& rX, VectorType& rB)
+      {
+
+	//if(havePreconditioner)
+		CG_GPU(rA.size1(), rA.size2(), rA.nnz(), &(rA.value_data() [0]), &(rA.index2_data() [0]), &(rA.index1_data() [0]), rB.size(), &(rX[0]), &(rB[0]), 
+			tol, maxIter, BaseType::mBNorm, BaseType::mResidualNorm, BaseType::mIterationsNumber, *(this->preconditioner));
+	//else
+	//	CG_GPU(rA.size1(), rA.size2(), rA.nnz(), &(rA.value_data() [0]), &(rA.index2_data() [0]), &(rA.index1_data() [0]), rB.size(), &(rX[0]), &(rB[0]), 
+	//		tol, maxIter, BaseType::mBNorm, BaseType::mResidualNorm, BaseType::mIterationsNumber, 0);
+      
+/*	const int size = rX.size();
+
+	//Allocating matrix A
+	GPUCSRMatrix gpuA(rA.nnz(), rA.size1(), rA.size2(), &(rA.index2_data() [0]), &(rA.index1_data() [0]), &(rA.value_data() [0]));
+	KRATOS_GPU_CHECK(gpuA.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuA.Copy(CPU_GPU, false));
+
+	//Allocating vector b
+	GPUVector gpuB(rB.size(), &(rB[0]));
+	KRATOS_GPU_CHECK(gpuB.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuB.Copy(CPU_GPU));
+
+	//Allocating vector x
+	GPUVector gpuX(rX.size(), &(rX[0]));
+	KRATOS_GPU_CHECK(gpuX.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuX.Copy(CPU_GPU));
+
+	if(havePreconditioner){
+		double alpha, beta, rho, rho_1 = 1.0;
+
+		//Ini preconditioner
+		clock_t s1 = clock();
+		this->preconditioner->initialize(gpuA.CPU_RowIndices, gpuA.CPU_Columns, gpuA.CPU_Values,
+				gpuA.GPU_RowIndices, gpuA.GPU_Columns, gpuA.GPU_Values,
+				gpuA.Size1, gpuA.Size2, gpuA.NNZ, true, true);
+		clock_t s2 = clock();
+		std::cout << "Time to create hierarchy" << double(s2-s1) / CLOCKS_PER_SEC << "s" << std::endl;
+
+
+		//Norm(b)
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuB, BaseType::mBNorm));
+
+		//r = b - A*x
+		GPUVector gpuR(size);
+		KRATOS_GPU_CHECK(gpuR.GPU_Allocate());
+		KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gpuA, gpuX, gpuR));
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gpuB, -1.00, gpuR));
+
+		//std::cout << "BNORM " << BaseType::mBNorm << std::endl;
+		if(BaseType::mBNorm == 0.0)
+			BaseType::mBNorm = 1.0;
+
+		//Norm(r)
+		double normr, resid;
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuR, normr));
+		if((resid = normr/BaseType::mBNorm) <= 1.0e-30){
+			std::cout << "Out from outer return" << std::endl;
+			return false;
+		}
+		GPUVector gpuP(size), gpuZ(size), gpuQ(size);
+		KRATOS_GPU_CHECK(gpuP.GPU_Allocate());
+		KRATOS_GPU_CHECK(gpuZ.GPU_Allocate());
+		KRATOS_GPU_CHECK(gpuQ.GPU_Allocate());
+		
+		BaseType::mIterationsNumber = 0;
+
+		clock_t s3 = 0.0;
+		size_t i = 1;
+		do{
+
+			GPU_fillWithZeros(size, gpuZ.GPU_Values);
+			s1 = clock();
+			this->preconditioner->singleStep(gpuR.GPU_Values, gpuZ.GPU_Values);
+			s2 = clock();
+			s3 += s2-s1;
+			rho = GPU_dotProduct(gpuR.Size, gpuR.GPU_Values, 1, gpuZ.GPU_Values, 1);
+
+			if(i == 1)
+				//gpuP = gpuZ
+				gpuP.CopyGPUValuesFrom(gpuZ);
+			else{
+				beta = rho / rho_1; 
+				KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gpuZ, beta, gpuP));
+			}
+
+			KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gpuA, gpuP, gpuQ));
+			alpha = rho / GPU_dotProduct(gpuP.Size, gpuP.GPU_Values, 1, gpuQ.GPU_Values, 1);
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(alpha, gpuP, 1.00, gpuX));
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(-alpha, gpuQ, 1.00, gpuR));
+		
+			KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuR, normr));
+			BaseType::mResidualNorm = normr;
+			if((resid = normr/BaseType::mBNorm) <= 1.0e-30){
+				break;
+			}
+			rho_1 = rho;
+			BaseType::mIterationsNumber++;			
+			i++;
+			KRATOS_WATCH("end iteration");
+			KRATOS_WATCH(BaseType::mResidualNorm);
+			KRATOS_WATCH(BaseType::mBNorm);
+			KRATOS_WATCH(BaseType::mResidualNorm/BaseType::mBNorm);
+		}while(BaseType::IterationNeeded());
+		std::cout << "Average time for single step" << (double(s3)/(i-1)) / CLOCKS_PER_SEC << "s" << std::endl;
+
+		KRATOS_GPU_CHECK(gpuX.Copy(GPU_CPU));
+		this->preconditioner->cleanPreconditioner();
+	}else{     		
+		double alpha, beta, rho, rho_1 = 1.0;
+
+		//Norm(b)
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuB, BaseType::mBNorm));
+
+		//r = b - A*x
+		GPUVector gpuR(size);
+		KRATOS_GPU_CHECK(gpuR.GPU_Allocate());
+		KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gpuA, gpuX, gpuR));
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gpuB, -1.00, gpuR));
+
+		//std::cout << "BNORM " << BaseType::mBNorm << std::endl
+		if(BaseType::mBNorm == 0.0)
+			BaseType::mBNorm = 1.0;
+
+		//Norm(r)
+		double normr, resid;
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuR, normr));
+		if((resid = normr/BaseType::mBNorm) <= 1.0e-30){
+			std::cout << "Out from outer return" << std::endl;
+			return false;
+		}
+		GPUVector gpuP(size), gpuQ(size);
+		KRATOS_GPU_CHECK(gpuP.GPU_Allocate());
+		KRATOS_GPU_CHECK(gpuQ.GPU_Allocate());
+		
+		BaseType::mIterationsNumber = 0;
+
+		size_t i = 1;
+		do{
+
+			rho = GPU_dotProduct(gpuR.Size, gpuR.GPU_Values, 1, gpuR.GPU_Values, 1);
+
+			if(i == 1)
+				//gpuP = gpuZ
+				gpuP.CopyGPUValuesFrom(gpuR);
+			else{
+				beta = rho / rho_1; 
+				KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gpuR, beta, gpuP));
+			}
+
+			KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gpuA, gpuP, gpuQ));
+			alpha = rho / GPU_dotProduct(gpuP.Size, gpuP.GPU_Values, 1, gpuQ.GPU_Values, 1);
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(alpha, gpuP, 1.00, gpuX));
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(-alpha, gpuQ, 1.00, gpuR));
+		
+			KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuR, normr));
+			BaseType::mResidualNorm = normr;
+			if((resid = normr/BaseType::mBNorm) <= 1.0e-30){
+				break;
+			}
+			rho_1 = rho;
+			BaseType::mIterationsNumber++;			
+			i++;
+			KRATOS_WATCH("end iteration");
+			KRATOS_WATCH(BaseType::mResidualNorm);
+			KRATOS_WATCH(BaseType::mBNorm);
+			KRATOS_WATCH(BaseType::mResidualNorm/BaseType::mBNorm);
+		}while(BaseType::IterationNeeded());
+
+		KRATOS_GPU_CHECK(gpuX.Copy(GPU_CPU));
+
+	}*/
+	//std::cout << "Got return with value of convergence " << BaseType::IsConverged() << ", and finalIterations " << BaseType::mIterationsNumber << std::endl;	return BaseType::IsConverged();
+      }
+        
+      ///@} 
+      ///@name Private  Access 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Private Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Un accessible methods 
+      ///@{ 
+      
+        
+      ///@}    
+        
+    }; // Class CGSolver 
+
+  ///@} 
+  
+  ///@name Type Definitions       
+  ///@{ 
+  
+  
+  ///@} 
+  ///@name Input and output 
+  ///@{ 
+        
+ 
+  /// input stream function
+  template<class TSparseSpaceType, class TDenseSpaceType, 
+    class TPreconditionerType, 
+    class TReordererType>
+  inline std::istream& operator >> (std::istream& IStream, 
+				      GPUCGSolver<TSparseSpaceType, TDenseSpaceType, 
+				      TPreconditionerType, TReordererType>& rThis)
+    {
+		return IStream;
+    }
+
+  /// output stream function
+  template<class TSparseSpaceType, class TDenseSpaceType, 
+    class TPreconditionerType, 
+    class TReordererType>
+  inline std::ostream& operator << (std::ostream& OStream, 
+				    const GPUCGSolver<TSparseSpaceType, TDenseSpaceType, 
+				      TPreconditionerType, TReordererType>& rThis)
+    {
+      rThis.PrintInfo(OStream);
+      OStream << std::endl;
+      rThis.PrintData(OStream);
+
+      return OStream;
+    }
+  ///@} 
+  
+  
+}  // namespace Kratos.
+
+#endif // KRATOS_GPU_CG_SOLVER_H_INCLUDED  defined 
+
+
---
+++ kratos/applications/gpu_solvers_application/custom_linear_solvers/linear_solvers.h
@@ -0,0 +1,298 @@
+#if !defined(LINEAR_GPU_SOLVERS_H_INCLUDED )
+#define  LINEAR_GPU_SOLVERS_H_INCLUDED
+
+
+#include <cmath>
+#include <iostream>
+#include "gpu_sparse.h"
+#include "GPUPreconditioner.h"
+
+#define KRATOS_GPU_CHECK(call)		if (!(call)) printf(#call " in file %s line %d failed.\n", __FILE__, __LINE__);
+using namespace Kratos::GPUSparse;
+
+void BICGSTAB_GPU(size_t A_Size1, size_t A_size2, size_t A_NNZ, double *A_values, size_t *A_indices, size_t *A_ptr, size_t vectorSize, double *X_values, double *B_values, 
+	double tol, size_t maxIterations, double &mBNorm, double &mResidualNorm, size_t &mIterationsNumber, GPUPreconditioner &preconditioner)
+      {
+	bool havePreconditioner = (&preconditioner != NULL);
+
+	// Inputs
+	GPUCSRMatrix gA(A_NNZ, A_Size1, A_size2, A_indices, A_ptr, A_values);
+	KRATOS_GPU_CHECK(gA.GPU_Allocate());
+	KRATOS_GPU_CHECK(gA.Copy(CPU_GPU, false));
+	
+	GPUVector gX(vectorSize, X_values);
+	KRATOS_GPU_CHECK(gX.GPU_Allocate());
+	KRATOS_GPU_CHECK(gX.Copy(CPU_GPU));
+	
+	GPUVector gB(vectorSize, B_values);
+	KRATOS_GPU_CHECK(gB.GPU_Allocate());
+	KRATOS_GPU_CHECK(gB.Copy(CPU_GPU));
+
+	if(havePreconditioner){
+		preconditioner.initialize(gA.CPU_RowIndices, gA.CPU_Columns, gA.CPU_Values,
+			gA.GPU_RowIndices, gA.GPU_Columns, gA.GPU_Values,
+			gA.Size1, gA.Size2, gA.NNZ, true, true);
+	}
+
+	double resid;
+	double roh_1 = 0.0, roh_2 = 0.0, alpha = 0.0, beta = 0.0, omega = 0.0;
+
+
+	const int size = vectorSize;
+	mBNorm = 0.0;
+	double norms = 0.0;
+	mResidualNorm = 0.0;
+
+	GPUVector p(size);
+	KRATOS_GPU_CHECK(p.GPU_Allocate());
+	GPUVector phat(size);
+	KRATOS_GPU_CHECK(phat.GPU_Allocate());
+	GPUVector s(size);
+	KRATOS_GPU_CHECK(s.GPU_Allocate());
+	GPUVector shat(size);
+	KRATOS_GPU_CHECK(shat.GPU_Allocate());
+	GPUVector t(size);
+	KRATOS_GPU_CHECK(t.GPU_Allocate());
+	GPUVector v(size);
+	KRATOS_GPU_CHECK(v.GPU_Allocate());
+
+
+		// Real normb = norm(b);
+	KRATOS_GPU_CHECK(GPU_VectorNorm2(gB, mBNorm));
+
+		//  Vector r = b - A * x;
+	//VectorType r(size);
+	GPUVector r(size);
+	KRATOS_GPU_CHECK(r.GPU_Allocate());
+	//SparseSpaceType::Mult(rA,rX,r); // r = rA*rX
+	KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gA, gX, r));
+
+	//SparseSpaceType::ScaleAndAdd(1.00, rB, -1.00, r); // r = rB - r
+	KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gB, -1.00, r));
+		// rtilde
+	//VectorType rtilde(size);
+	GPUVector rtilde(size);
+	KRATOS_GPU_CHECK(rtilde.GPU_Allocate());
+	KRATOS_GPU_CHECK(rtilde.CopyFromGPU(r));
+	
+	if(mBNorm == 0.0)
+		mBNorm = 1.0;
+
+	mIterationsNumber = 0;
+	KRATOS_GPU_CHECK(GPU_VectorNorm2(r, mResidualNorm));
+	if((resid = mResidualNorm/mBNorm) <= 1.0e-30){
+		if(havePreconditioner)
+			preconditioner.cleanPreconditioner();
+		return;
+	}
+	size_t i = 1;
+
+	do{
+		//roh_1 = dotProduct(rtilde, r);
+		KRATOS_GPU_CHECK(GPU_VectorVectorMultiply(rtilde, r, roh_1));
+
+		if(roh_1 == 0){
+			KRATOS_GPU_CHECK(GPU_VectorNorm2(r, mResidualNorm));
+			break;
+		}
+
+		if(i == 1){
+			//p = r;
+			KRATOS_GPU_CHECK(p.CopyFromGPU(r));
+		}else{
+			beta = (roh_1/roh_2) * (alpha/omega);
+      			//p = r + beta * (p - omega * v);
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(-omega, v, 1.0, p));
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.0, r, beta, p));
+		}
+		if(havePreconditioner){
+			//phat = M.solve(p);
+			//GPU_fillWithZeros(size, phat.GPU_Values);
+			KRATOS_GPU_CHECK(phat.CopyFromGPU(p));
+			preconditioner.singleStep(p.GPU_Values, phat.GPU_Values);
+		}else{
+			KRATOS_GPU_CHECK(phat.CopyFromGPU(p));
+		}
+		
+		//v = A * phat;
+		KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gA, phat, v));
+		//dot(rtilde, v)
+		double dotRtildeV = 0.0;
+		KRATOS_GPU_CHECK(GPU_VectorVectorMultiply(rtilde, v, dotRtildeV));
+
+		alpha = roh_1 / dotRtildeV;
+
+		//s = r - alpha(0) * v;
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.0, r, -alpha, v, s));
+
+		//norm(s)
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(s, norms));
+
+		if ((resid = norms/mBNorm) < tol) {
+			//x += alpha(0) * phat;
+			//tol = resid;
+			//return 0;
+			break;
+		}
+		if(havePreconditioner){
+			//shat = M.solve(s);
+			//GPU_fillWithZeros(size, shat.GPU_Values);
+			KRATOS_GPU_CHECK(shat.CopyFromGPU(s));
+			preconditioner.singleStep(s.GPU_Values, shat.GPU_Values);
+		}else{
+			KRATOS_GPU_CHECK(shat.CopyFromGPU(s));
+		}
+
+		//t = A * shat;
+		KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gA, shat, t));
+
+		//omega = dot(t,s) / dot(t,t);
+		double dotTS = 0.0, dotTT = 0.0;
+		KRATOS_GPU_CHECK(GPU_VectorVectorMultiply(t, s, dotTS));
+		KRATOS_GPU_CHECK(GPU_VectorVectorMultiply(t, t, dotTT));
+		omega = dotTS / dotTT;
+		
+		//x += alpha(0) * phat + omega(0) * shat;
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd_addingVersion(alpha, phat, omega, shat, gX));
+		//r = s - omega(0) * t;
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.0, s, -omega, t, r));
+
+		roh_2 = roh_1;
+		//norm(r)
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(r, mResidualNorm));
+		mIterationsNumber++;
+		if ((resid = mResidualNorm / mBNorm) < tol) {
+			//tol = resid;
+			//max_iter = i;
+			//return 0;
+			break;
+		}
+		if (omega == 0) {
+			//tol = norm(r) / normb;
+			//return 3;
+			break;
+    		}
+		i++;
+//		std::cout << "end iteration" << std::endl;
+//		std::cout << mResidualNorm << std::endl;
+//		std::cout << mBNorm << std::endl;
+//		std::cout << mResidualNorm/mBNorm << std::endl;
+	}while((mIterationsNumber < maxIterations) && (mResidualNorm > tol * mBNorm));
+	KRATOS_GPU_CHECK(gX.Copy(GPU_CPU));
+	if(havePreconditioner)
+		preconditioner.cleanPreconditioner();
+}
+
+void CG_GPU(size_t A_Size1, size_t A_size2, size_t A_NNZ, double *A_values, size_t *A_indices, size_t *A_ptr, size_t vectorSize, double *X_values, double *B_values, 
+	double tol, size_t maxIterations, double &mBNorm, double &mResidualNorm, size_t &mIterationsNumber, GPUPreconditioner& preconditioner)
+      {
+
+	bool havePreconditioner = (&preconditioner != 0);
+	const int size = vectorSize;
+
+	//Allocating matrix A
+	GPUCSRMatrix gpuA(A_NNZ, A_Size1, A_size2, A_indices, A_ptr, A_values, true);
+	KRATOS_GPU_CHECK(gpuA.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuA.Copy(CPU_GPU, false));
+
+	//Allocating vector b
+	GPUVector gpuB(vectorSize, B_values);
+	KRATOS_GPU_CHECK(gpuB.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuB.Copy(CPU_GPU));
+
+	//Allocating vector x
+	GPUVector gpuX(vectorSize, X_values);
+	KRATOS_GPU_CHECK(gpuX.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuX.Copy(CPU_GPU));
+
+	double alpha, beta, roh, roh_1 = 1.0;
+		
+	//Ini preconditioner
+	//clock_t s1 = clock();
+	if(havePreconditioner){
+		preconditioner.initialize(gpuA.CPU_RowIndices, gpuA.CPU_Columns, gpuA.CPU_Values,
+				gpuA.GPU_RowIndices, gpuA.GPU_Columns, gpuA.GPU_Values,
+				gpuA.Size1, gpuA.Size2, gpuA.NNZ, true, true);
+	}
+	//clock_t s2 = clock();
+	//std::cout << "Time to create hierarchy" << double(s2-s1) / CLOCKS_PER_SEC << "s" << std::endl;
+
+	//Norm(b)
+	KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuB, mBNorm));
+
+	//r = b - A*x
+	GPUVector gpuR(size);
+	KRATOS_GPU_CHECK(gpuR.GPU_Allocate());
+	KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gpuA, gpuX, gpuR));
+	KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gpuB, -1.00, gpuR));
+
+	if(mBNorm == 0.0)
+		mBNorm = 1.0;
+
+	mIterationsNumber = 0;
+	//Norm(r)
+	double resid;
+	KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuR, mResidualNorm));
+	if((resid = mResidualNorm/mBNorm) <= 1.0e-30){
+		if(havePreconditioner)
+			preconditioner.cleanPreconditioner();
+		std::cout << "leaving 1" << std::endl;
+		return;
+	}
+	GPUVector gpuP(size), gpuZ(size), gpuQ(size);
+	KRATOS_GPU_CHECK(gpuP.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuZ.GPU_Allocate());
+	KRATOS_GPU_CHECK(gpuQ.GPU_Allocate());
+	
+	//clock_t s3 = 0.0;
+	size_t i = 1;
+	do{
+		//s1 = clock();
+		if(havePreconditioner){
+			GPU_fillWithZeros(size, gpuZ.GPU_Values);
+			//gpuZ.CopyFromGPU(gpuR);		
+			preconditioner.singleStep(gpuR.GPU_Values, gpuZ.GPU_Values);	
+		}else{
+			gpuZ.CopyFromGPU(gpuR);
+		}
+		//s2 = clock();
+		//s3 += s2-s1;
+		//roh = GPU_dotProduct(gpuR.Size, gpuR.GPU_Values, 1, gpuZ.GPU_Values, 1);
+		KRATOS_GPU_CHECK(GPU_VectorVectorMultiply(gpuR, gpuZ, roh));
+
+		if(i == 1)
+			//gpuP = gpuZ
+			gpuP.CopyFromGPU(gpuZ);
+		else{
+			beta = roh / roh_1; 
+			KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(1.00, gpuZ, beta, gpuP));
+		}
+
+		KRATOS_GPU_CHECK(GPU_MatrixVectorMultiply(gpuA, gpuP, gpuQ));
+		//roh_1 = dotProduct(p, q);
+		double dotPQ;
+		KRATOS_GPU_CHECK(GPU_VectorVectorMultiply(gpuP, gpuQ, dotPQ));
+		alpha = roh / dotPQ;
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(alpha, gpuP, 1.00, gpuX));
+		KRATOS_GPU_CHECK(GPU_VectorScaleAndAdd(-alpha, gpuQ, 1.00, gpuR));
+	
+		KRATOS_GPU_CHECK(GPU_VectorNorm2(gpuR, mResidualNorm));
+
+		if((resid = mResidualNorm/mBNorm) <= 1.0e-30){
+			break;
+					std::cout << "leaving 2" << std::endl;
+		}
+		roh_1 = roh;
+		mIterationsNumber++;			
+		i++;
+	}while((mIterationsNumber < maxIterations) && (mResidualNorm > tol * mBNorm));
+	//std::cout << "Average time for single step" << (double(s3)/(i-1)) / CLOCKS_PER_SEC << "s" << std::endl;
+			std::cout << "leaving 3" << std::endl;
+	KRATOS_GPU_CHECK(gpuX.Copy(GPU_CPU));
+	if(havePreconditioner)
+		preconditioner.cleanPreconditioner();
+}
+
+//to add in gpu_sparse.cu
+
+#endif
