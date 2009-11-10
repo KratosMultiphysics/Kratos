0a6cc63ec593ea2a66c6684505f0258856f04bef
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-11-10 11:43:45 +0000

    merging Isaac's and Farshid's version.

--- kratos/applications/gpu_solvers_application/custom_external_libraries/build.sh
+++ kratos/applications/gpu_solvers_application/custom_external_libraries/build.sh
@@ -5,5 +5,5 @@
 #
 # To run, simply issue: ./build.sh
 #
-nvcc gpu_sparse.cu -o gpu_sparse.o -c -O3 -Xcompiler -fPIC -DNDEBUG -DUSE_UMUL24 -DUSE_TEXTURE_CACHING -arch compute_13 -code compute_13 -lcublas
+nvcc gpu_sparse.cu -o gpu_sparse.o -c -g -Xcompiler -fPIC -DNDEBUG -DUSE_UMUL24 -DUSE_TEXTURE_CACHING -arch compute_13 -code compute_13 -lcublas -I/usr/local/lapackpp/include/lapackpp
 ar cr libgpu_sparse.a gpu_sparse.o
