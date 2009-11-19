f59ab01543e7e9b195d63d7f3b33838fdee1f19c
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2009-11-19 07:56:38 +0000

    Relase Candidate 2 of the GPU solver application

--- kratos/applications/gpu_solvers_application/test_examples/LinearSolversTesting/tests.py
+++ kratos/applications/gpu_solvers_application/test_examples/LinearSolversTesting/tests.py
@@ -31,13 +31,13 @@ b = (pb).GetReference()
 
 
 
-matrix_filename = "mat8.mm"
-#matrix_filename = "A_0.01005_.mm"
+#matrix_filename = "mat8.mm"
+matrix_filename = "A_0.01005_.mm"
 ReadMatrixMarketMatrix(matrix_filename,A)
 print "finished reading matrix"
 
-vector_filename = "vecb8.mm"
-#vector_filename = "b_0.01005_.mm"
+#vector_filename = "vecb8.mm"
+vector_filename = "b_0.01005_.mm"
 ReadMatrixMarketVector(vector_filename,b)
 print "finished reading vector"
 
@@ -57,20 +57,49 @@ W = 4.0/3.0
 
 #TESTING IF CG AND BICGSTAB CAN ITERATE INDEFINITIVELY
 x = Vector(len(b))
-for i in range(1, 2):
-	print "----- iteration ", i, " ------"
-	space_utils.SetToZeroVector(x)	
-	
-	t1 = time.time()
-	#precond = KratosAMGPreconditioner(W, 2, True, 5, 1000, preSweeps, postSweeps, True)
-	#precond = DiagonalpreconditionerType()
-	linear_solver = GPUCGSolver(1e-9, 5000)#, precond)
-	linear_solver.Solve(A, x, b)
-	print linear_solver
-	print "\n\n"
-	t2 = time.time()
-	print 'Solve time: %0.3f ms' % ((t2-t1)*1000.0)
-	linear_solver=0
+
+
+space_utils.SetToZeroVector(x)	
+
+t1 = time.time()
+precond1 = KratosAMGPreconditioner(W, 2, True, 5, 1000, preSweeps, postSweeps, True)
+#precond = GPUDiagonalPreconditioner()
+linear_solver1 = GPUCGSolver(1e-9, 5000, precond1)
+linear_solver1.Solve(A, x, b)
+print linear_solver1
+print "\n\n"
+t2 = time.time()
+print 'Solve time: %0.3f ms' % ((t2-t1)*1000.0)
+#linear_solver=0
+
+exit(0)
+space_utils.SetToZeroVector(x)	
+
+t1 = time.time()
+#precond = KratosAMGPreconditioner(W, 2, True, 5, 100, preSweeps, postSweeps, True)
+precond2 = GPUDiagonalPreconditioner()
+linear_solver2 = GPUCGSolver(1e-9, 5000, precond2)
+linear_solver2.Solve(A, x, b)
+print linear_solver2
+print "\n\n"
+t2 = time.time()
+print 'Solve time: %0.3f ms' % ((t2-t1)*1000.0)
+#linear_solver=0
+
+
+space_utils.SetToZeroVector(x)	
+
+t1 = time.time()
+#precond = KratosAMGPreconditioner(W, 2, True, 5, 100, preSweeps, postSweeps, True)
+#precond = GPUDiagonalPreconditioner()
+precond3 = DiagonalPreconditioner()
+linear_solver3 = CGSolver(1e-9, 5000, precond3)
+linear_solver3.Solve(A, x, b)
+print linear_solver3
+print "\n\n"
+t2 = time.time()
+print 'Solve time: %0.3f ms' % ((t2-t1)*1000.0)
+#linear_solver3=0
 	
 
 exit(0)
