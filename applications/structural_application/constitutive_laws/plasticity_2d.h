82d67250e35e6e183ffc253204346fafb3105baf
nelson <nelson@cimne.upc.edu>
2010-01-04 17:03:58 +0000

    compostie material comented

---
+++ applications/structural_application/constitutive_laws/plasticity_2d.h
@@ -0,0 +1,221 @@
+/*
+==============================================================================
+KratosStructuralApplication 
+A library based on:
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi, Janosch Stascheit, Felix Nagel 
+pooyan@cimne.upc.edu 
+rrossi@cimne.upc.edu
+janosch.stascheit@rub.de
+nagel@sd.rub.de
+- CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+- Ruhr-University Bochum, Institute for Structural Mechanics, Germany
+
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+/* *********************************************************   
+*          
+*   Last Modified by:    $Author: nelson $
+*   Date:                $Date: 2008-09-03 
+*   Revision:            $Revision: 1.1 $
+*
+* ***********************************************************/
+
+#if !defined(KRATOS_PLASTICITY_2D_H_INCLUDED )
+#define  KRATOS_PLASTICITY_2D_H_INCLUDED
+
+// System includes 
+
+// External includes 
+#include "boost/smart_ptr.hpp"
+
+// Project includes
+#include "includes/define.h"
+#include "includes/variables.h"
+#include "includes/constitutive_law.h"
+#include "fluency_criteria/fluency_criteria.h"
+#include "soft_hard_behavior/softening_hardening_criteria.h"
+
+
+namespace Kratos
+{
+	/**
+	 * Defines a linear elastic isotropic constitutive law in 3D space.
+	 * This material law is defined by the parameters E (Young's modulus) 
+	 * and NU (Poisson ratio)
+	 * As there are no further parameters the functionality is limited 
+	 * to linear elasticity.
+	 */
+	class Plasticity2D : public ConstitutiveLaw<Node<3> >
+	{
+		public:
+
+		      
+		        ///@name Type Definitions
+		        typedef ConstitutiveLaw<Node<3> > BaseType;
+			/**
+			 * Counted pointer of Plasticity2D
+			 */
+			typedef boost::shared_ptr<Plasticity2D> Pointer;
+
+			typedef FluencyCriteria::Pointer FluencyCriteriaPointer;  
+
+                        typedef SofteningHardeningCriteria::Pointer SofteningHardeningCriteriaPointer;   
+                        
+                        typedef Properties::Pointer PropertiesPointer;
+
+		        //const ProcessInfo  *mCurrentProcessInfo;
+			/**
+			 * Life Cycle 
+			 */
+			/**
+			 * Default constructor.
+			 */
+			Plasticity2D();
+			
+                        virtual boost::shared_ptr<ConstitutiveLaw<Node<3> > > Clone() const
+			{
+				boost::shared_ptr<ConstitutiveLaw<Node<3> > > p_clone(new Plasticity2D(mpFluencyCriteria->Clone(),mpSofteningBehavior, mpProperties));
+				return p_clone;
+			}
+			
+
+                        Plasticity2D(FluencyCriteriaPointer FluencyCriteria, SofteningHardeningCriteriaPointer SofteningBehavior, PropertiesPointer Property);
+			/**
+			 * Destructor.
+			 */
+			virtual ~Plasticity2D();
+			
+			/**
+			 * Operators 
+			 */
+			/**
+			 * Operations
+			 */
+		  bool Has( const Variable<double>& rThisVariable );
+		  bool Has( const Variable<Vector>& rThisVariable );
+		  bool Has( const Variable<Matrix>& rThisVariable );
+
+		  double GetValue( const Variable<double>& rThisVariable );
+		  Vector GetValue( const Variable<Vector>& rThisVariable );
+		  Matrix GetValue( const Variable<Matrix>& rThisVariable );
+
+		  void SetValue( const Variable<double>& rVariable, 
+		  const double& Value, 
+		  const ProcessInfo& rCurrentProcessInfo );
+		  void SetValue( const Variable<Vector>& rThisVariable, 
+		  const Vector& rValue, 
+		  const ProcessInfo& rCurrentProcessInfo );
+		  void SetValue( const Variable<Matrix>& rThisVariable, 
+		  const Matrix& rValue, 
+		  const ProcessInfo& rCurrentProcessInfo );
+
+
+		  void InitializeMaterial( const Properties& props,
+		  const GeometryType& geom,
+		  const Vector& ShapeFunctionsValues );
+
+		  void CalculateConstitutiveMatrix(const Vector& StrainVector, Matrix& rResult);
+
+		  void CalculateStress( const Vector& StrainVector, 
+		  Vector& StressVector);
+
+		  void CalculateCauchyStresses( Vector& Cauchy_StressVector,
+		  const Matrix& F,
+		  const Vector& PK2_StressVector,
+		  const Vector& GreenLagrangeStrainVector);
+
+
+		  void Calculate(const Variable<Matrix >& rVariable, Matrix& rResult, 
+		  const ProcessInfo& rCurrentProcessInfo);
+
+		  void InitializeSolutionStep( const Properties& props,
+		  const GeometryType& geom,
+		  const Vector& ShapeFunctionsValues ,
+		  const ProcessInfo& CurrentProcessInfo);
+
+		  void FinalizeSolutionStep( const Properties& props,
+		  const GeometryType& geom, 
+		  const Vector& ShapeFunctionsValues ,
+		  const ProcessInfo& CurrentProcessInfo);
+
+		  void CalculateStressAndTangentMatrix( Vector& StressVector,
+		  const Vector& StrainVector,
+		  Matrix& algorithmicTangent);
+
+		  void Calculate( const Variable<double>& rVariable, 
+		  double& Output, 
+		  const ProcessInfo& rCurrentProcessInfo);
+
+                  void UpdateMaterial( const Vector& StrainVector,
+                                     const Properties& props,
+                                     const GeometryType& geom,
+                                     const Vector& ShapeFunctionsValues,
+                                     const ProcessInfo& CurrentProcessInfo);
+
+
+ 
+    private:
+
+    protected:
+   
+    // Atributos  Privados
+
+    bool   ComputeTangentMatrix;
+    double mNU;
+    double mE;
+    double mDE;
+    double mlamda;
+    double mefective_plastic_strain;
+    double mcurrent_efective_plastic_strain; 
+
+    array_1d<double,4>  mplastic_strain;
+    array_1d<double,4>  mcurrent_plastic_strain;
+    array_1d<double,4>  mDerivate_Fluency;
+    
+    FluencyCriteriaPointer mpFluencyCriteria;
+    SofteningHardeningCriteriaPointer mpSofteningBehavior;
+    PropertiesPointer mpProperties;
+
+
+
+   void CalculateElasticMatrix(Matrix& C, const double E, const double NU);
+   void CalculateElasticStress(const Vector& StrainVector, array_1d<double,4>& StressVector);
+   void Comprobate_State_Vector(Vector& Result);
+   void ComputeCondentationMatrix(const Matrix& TangentMatrix, Matrix& Result);
+
+
+
+   
+
+	}; // Class Plasticity2D 
+}  // namespace Kratos.
+#endif // KRATOS_ISOTROPIC_2D_H_INCLUDED  defined 
