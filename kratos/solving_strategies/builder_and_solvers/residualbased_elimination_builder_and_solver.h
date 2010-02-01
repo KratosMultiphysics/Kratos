d49ce2322b206fa916ec750f1a20c9cfc225fd12
janosch <janosch@cimne.upc.edu>
2010-02-01 09:50:28 +0000

    bugfix

--- kratos/solving_strategies/builder_and_solvers/residualbased_elimination_builder_and_solver.h
+++ kratos/solving_strategies/builder_and_solvers/residualbased_elimination_builder_and_solver.h
@@ -1051,8 +1051,8 @@ std::cout << "DofTemp before Unique" << Doftemp.size() << std::endl;
                     }
                 }
             }
-            Timer::Stop("MatrixStructure");
 #endif
+            Timer::Stop("MatrixStructure");
         }
 
 		//**************************************************************************
