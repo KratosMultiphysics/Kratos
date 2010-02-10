58aeab0c9332e38b97e443c7f660c556dce9cfb6
rrossi <rrossi@cimne.upc.edu>
2010-02-10 15:19:37 +0000

    committing the eulerian version of the navier stokes solver

---
+++ applications/incompressible_fluid_application/python_scripts/edgebased_eulerian_solver.py
@@ -0,0 +1,124 @@
+#importing the Kratos Library
+from Kratos import *
+from KratosIncompressibleFluidApplication import *
+
+def AddVariables(model_part):
+    model_part.AddNodalSolutionStepVariable(VELOCITY);
+    model_part.AddNodalSolutionStepVariable(PRESSURE);
+    model_part.AddNodalSolutionStepVariable(NORMAL)
+    model_part.AddNodalSolutionStepVariable(AUX_INDEX)
+    model_part.AddNodalSolutionStepVariable(DISTANCE)
+    model_part.AddNodalSolutionStepVariable(PRESS_PROJ)
+    model_part.AddNodalSolutionStepVariable(POROSITY)
+
+#    model_part.AddNodalSolutionStepVariable(ACCELERATION)
+    model_part.AddNodalSolutionStepVariable(DIAMETER)
+
+    print "variables for the edgebased incompressible fluid solver added correctly"
+
+def AddDofs(model_part):
+    for node in model_part.Nodes:
+        node.AddDof(PRESSURE);
+        node.AddDof(VELOCITY_X);
+        node.AddDof(VELOCITY_Y);
+        node.AddDof(VELOCITY_Z);
+
+
+class EdgeBasedLevelSetSolver:
+    
+    def __init__(self,model_part,domain_size,body_force,viscosity,density):
+
+        print "entered in EdgeBasedLevelSetSolver python constructor"
+        #data of the problem
+        self.model_part = model_part
+        self.domain_size = domain_size
+        self.body_force = body_force
+        self.density = density
+        self.viscosity = viscosity
+
+        self.use_mass_correction = True
+
+        self.redistance_frequency = 5;
+        self.step = 0;
+
+        self.extrapolation_layers = 5
+
+        #neighbour search
+        number_of_avg_elems = 10
+        number_of_avg_nodes = 10
+        self.neighbour_search = FindNodalNeighboursProcess(model_part,number_of_avg_elems,number_of_avg_nodes)
+        (self.neighbour_search).Execute()
+
+        pDiagPrecond = DiagonalPreconditioner()
+        self.pressure_linear_solver =  BICGSTABSolver(1e-3, 5000,pDiagPrecond)
+
+        
+
+
+
+    def Initialize(self):
+        print "entered in EdgeBasedLevelSetSolver python constructor"
+        #build the edge data structure
+        if(self.domain_size == 2):
+            self.matrix_container = MatrixContainer2D()
+        else:
+            self.matrix_container = MatrixContainer3D()
+
+        self.matrix_container.ConstructCSRVector(self.model_part)
+        self.matrix_container.BuildCSRData(self.model_part)
+
+        ##for 3D problems we need to evaluate the condition's neighbours
+        if(self.domain_size == 3):
+            self.condition_neighbours_finder = FindConditionsNeighboursProcess(self.model_part,self.domain_size,10)
+            self.condition_neighbours_finder.Execute()
+
+        ##constructing the solver
+        if(self.domain_size == 2):
+            self.fluid_solver = EdgeBasedLevelSet2D(self.matrix_container,self.model_part,self.viscosity,self.density,self.body_force,self.use_mass_correction)
+        else:
+            self.fluid_solver = EdgeBasedLevelSet3D(self.matrix_container,self.model_part,self.viscosity,self.density,self.body_force,self.use_mass_correction)
+
+        
+
+#TO BE DELETED SOON
+##        print "ASSIGNING DIAMETER FROM SOLVER: TO BE REMOVED SOOOOOON!!!!!!!!!!!!!!!!!!!!"
+        for node in self.model_part.Nodes:
+            node.SetSolutionStepValue(DIAMETER,0, 0.01)
+            node.SetSolutionStepValue(POROSITY,0, 1.0)
+            node.SetSolutionStepValue(DISTANCE,0, -1.0)
+
+        self.fluid_solver.Initialize()
+
+
+
+        print "**********************************************"
+        print "finished EdgeBasedLevelSetSolver initialize"
+
+        
+
+    ################################################################
+    ################################################################
+    def Solve(self):
+        (self.fluid_solver).UpdateFixedVelocityValues()
+        (self.fluid_solver).SolveStep1();
+        (self.fluid_solver).SolveStep2(self.pressure_linear_solver);
+        (self.fluid_solver).SolveStep3();
+   
+    ################################################################
+    ################################################################
+    def EstimateTimeStep(self,safety_factor,max_Dt):
+        dt = (self.fluid_solver).ComputeTimeStep(safety_factor,max_Dt);
+
+        if(dt > max_Dt):
+            dt = max_Dt
+
+        print dt
+
+        return dt
+
+
+
+
+        
+        
+
