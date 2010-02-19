680e86735afa2af13b5f62dab50dcbc9dbd17247
Pooyan Dadvand <pooyan@cimne.upc.edu>
2010-02-19 15:19:57 +0000

    Modification of kratos_monitor.py

--- kratos/python_scripts/KratosMonitor.py
+++ kratos/python_scripts/KratosMonitor.py
@@ -4,16 +4,21 @@ import Tkinter
 class KratosMonitor:
 
     def DeleteRoot(self):
-        self.root.destroy()
+        self.toplevel.update()
+        self.toplevel.destroy();
+        self.root.destroy();
+        self.toplevel = 0;
         print "Monitor Closed"
 
     def __init__(self):
         self.root = Tkinter.Tk()
-        self.root.title("Kratos")
-#        self.root.protocol("WM_DELETE_WINDOW", self.DeleteRoot())
-
-        self.frame = Tkinter.Frame(self.root)
-        self.frame.grid()
+        self.root.withdraw();
+        self.toplevel = Tkinter.Toplevel( self.root);
+        self.toplevel.title("Kratos")
+        self.toplevel.protocol("WM_DELETE_WINDOW", self.DeleteRoot)
+        self.frame = Tkinter.Frame( self.toplevel)
+        self.frame.grid();
+        self.toplevel.bind( "<Control-c>", exit);
 
         self.Variables = dict()
         self.Values = dict()
@@ -22,19 +27,30 @@ class KratosMonitor:
         self.DeleteRoot()
 
     def Set(self, VariableName, Value):
-        if(self.root):
+        if(self.toplevel != 0):
+            bg_color="white"
             if(VariableName in self.Variables):
                 self.Values[VariableName].config(text=Value)
             else:
-                new_label = Tkinter.Label(self.frame, text=VariableName, anchor=Tkinter.W, justify=Tkinter.LEFT)
-                new_label.grid(row=len(self.Variables), column=0, padx=8, sticky=Tkinter.W)
-                new_label.config(anchor=Tkinter.W, justify=Tkinter.LEFT)
+                new_label = Tkinter.Label(self.toplevel, text=VariableName, anchor=Tkinter.W, justify=Tkinter.LEFT)
+                new_label.grid(row=len(self.Variables), column=0, padx=8, pady=1, sticky=Tkinter.N+Tkinter.W+Tkinter.E)
+##                if ( len(self.Variables) % 2):
+##                    new_label.config(anchor=Tkinter.W, justify=Tkinter.LEFT)
+##                else:
+##                    new_label.config(anchor=Tkinter.W, justify=Tkinter.LEFT, background=bg_color)
 
                 self.Variables[VariableName] = new_label;
             
-                new_label = Tkinter.Label(self.frame, text=Value, anchor=Tkinter.W, justify=Tkinter.LEFT)
-                new_label.grid(row=len(self.Values), column=1, padx=8, sticky=Tkinter.W)
+                new_label = Tkinter.Label(self.toplevel, text=Value, anchor=Tkinter.W, justify=Tkinter.LEFT)
+                new_label.grid(row=len(self.Values), column=1, padx=8, pady=1, sticky=Tkinter.N+Tkinter.W+Tkinter.E)
+##                if ( len(self.Values) % 2):
+##                    new_label.config(anchor=Tkinter.W, justify=Tkinter.LEFT)
+##                else:
+                new_label.config(anchor=Tkinter.W, justify=Tkinter.LEFT, background=bg_color)
                 self.Values[VariableName] = new_label;
+            self.root.update()
+        else:
+            print "KratosMonitor : ", VariableName, " = ", Value
 
 
  
@@ -42,8 +58,8 @@ class KratosMonitor:
 kratos_monitor = KratosMonitor()
 
 
-#kratos_monitor.root.mainloop()
-
+# kratos_monitor.root.mainloop()
+# kratos_monitor.root.update( )
 
 #######################This part is for testing
 
@@ -53,3 +69,5 @@ kratos_monitor = KratosMonitor()
 #start_time = gmtime()
 #kratos_monitor.Set("Started at", strftime("%a, %d %b %Y %H:%M:%S ", start_time))
 #sleep(4)
+
+#kratos_monitor.root.mainloop()
