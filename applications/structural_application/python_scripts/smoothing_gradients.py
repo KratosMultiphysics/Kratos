12e6cfc786f6528c9faffdeee935f2aa6adc8f71
nelson <nelson@cimne.upc.edu>
2010-06-02 10:05:44 +0000

    Nelson, Some Changes

---
+++ applications/structural_application/python_scripts/smoothing_gradients.py
@@ -0,0 +1,33 @@
+import os, sys
+#importing the Kratos Library
+from Kratos import *
+from KratosStructuralApplication import *
+
+class Smoothing_Gradients:
+    #######################################################################
+   
+    def __init__(self, model_part, domain_size):
+
+        self.model_part    = model_part
+        self.domain_size   = domain_size  
+        
+    #######################################################################
+    def Initialize(self):
+        self.smoothing = SmoothingUtility(self.model_part, self.domain_size)   
+                 
+    #######################################################################   
+    def WeightedRecoveryGradients(self):
+        self.smoothing.WeightedRecoveryGradients()
+
+     #######################################################################   
+    def InterpolatedRecoveryGradients(self):
+        self.smoothing.InterpolatedRecoveryGradients()
+
+    def DoubleWeightedRecoveryGradients(self):
+         self.smoothing.DoubleWeightedRecoveryGradients() 
+
+
+
+    
+  
+
