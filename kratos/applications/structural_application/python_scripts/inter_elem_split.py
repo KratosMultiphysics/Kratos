127205e592870ffbc8ab797e7b9a575c42e78a2d
nelson <nelson@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-06-02 10:05:50 +0000

    Nelson, Some Changes

---
+++ kratos/applications/structural_application/python_scripts/inter_elem_split.py
@@ -0,0 +1,52 @@
+import os, sys
+#importing the Kratos Library
+from Kratos import *
+from KratosStructuralApplication import *
+#from KratosMeshingApplication import *
+
+
+class Nodal_Split_Elem:
+    #######################################################################
+   
+    def __init__(self, model_part, domain_size):
+
+        self.model_part    =  model_part
+        self.domain_size   =  domain_size 
+        self.smoothing     =  SmoothingUtility(self.model_part, self.domain_size)
+ 
+        if(self.domain_size==2):  
+	    self.split         =  InterFractureTriangle(self.model_part, self.domain_size)
+        else:
+	    self.split        =  InterFractureTetrahedra(self.model_part, self.domain_size)
+        
+    #######################################################################
+    def Initialize(self):
+        self.initialize= False  
+       
+    
+
+    def Inter_Fracture(self):
+
+        self.smoothing.SettingNodalValues(self.model_part, self.domain_size)  
+        self.smoothing.DoubleWeightedRecoveryGradients(DAMAGE, self.model_part, self.domain_size)
+        self.smoothing.WeightedRecoveryGradients(GREEN_LAGRANGE_STRAIN_TENSOR, self.model_part, self.domain_size)
+        self.smoothing.Finalize()  
+        
+        self.split.DetectAndSplitElements(self.model_part) 
+	  
+
+	##self.detect.Finalize(self.model_part)
+	#self.smoothing.SettingNodalValues(self.model_part, self.domain_size)
+	#self.smoothing.RecomputeValuesForNewMesh(self.model_part, self.domain_size)
+	#self.Smoothing();
+	#self.smoothing.Finalize()           
+				  
+    #################################################################
+    def Smoothing(self):
+        #self.smoothing.InterpolatedRecoveryGradients(PK2_STRESS_TENSOR, self.model_part, self.domain_size)
+        #self.smoothing.InterpolatedRecoveryGradients(GREEN_LAGRANGE_STRAIN_TENSOR, self.model_part, self.domain_size)  
+        self.smoothing.WeightedRecoveryGradients(GREEN_LAGRANGE_STRAIN_TENSOR, self.model_part, self.domain_size)
+        #self.smoothing.WeightedRecoveryGradients(PK2_STRESS_TENSOR, self.model_part, self.domain_size)
+        self.smoothing.DoubleWeightedRecoveryGradients(DAMAGE, self.model_part, self.domain_size)
+
+
