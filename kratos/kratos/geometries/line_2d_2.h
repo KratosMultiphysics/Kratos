d13c2c9729de15b1d2d8d8bede0c06e1d5f52b50
nelson <nelson@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-07-21 14:05:02 +0000

    bonding box

--- kratos/kratos/geometries/line_2d_2.h
+++ kratos/kratos/geometries/line_2d_2.h
@@ -373,6 +373,18 @@ namespace Kratos
       }
 
 
+ 
+       
+     virtual void Bounding_Box(BoundingBox<TPointType, BaseType>& rResult) const
+             {
+                //rResult.Geometry() = *(this); 
+                BaseType::Bounding_Box(rResult.LowPoint(), rResult.HighPoint());  
+             }
+
+     
+              
+
+
       ///@}      
       ///@name Jacobian 
       ///@{
