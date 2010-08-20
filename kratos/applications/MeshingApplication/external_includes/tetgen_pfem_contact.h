0e4090f596381016e58d5d20d03918ded64adb2c
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-08-20 15:05:37 +0000

    contact_mesher

--- kratos/applications/MeshingApplication/external_includes/tetgen_pfem_contact.h
+++ kratos/applications/MeshingApplication/external_includes/tetgen_pfem_contact.h
@@ -96,12 +96,13 @@ namespace Kratos
 		//*******************************************************************************************
 		void ReGenerateMesh(
 			ModelPart& ThisModelPart ,
-			Element const& rReferenceElement 
+			Element const& rReferenceElement,
+			Condition const& rReferenceBoundaryCondition 
 			)
 		{
 
 			KRATOS_TRY
-
+KRATOS_WATCH(">>>>>>> INSIDE MESHER <<<<<<<<<");
 		     std::vector <int> shell_list;
 		     shell_list.clear();
 		     int shell_num = 0;
@@ -185,9 +186,9 @@ namespace Kratos
 			    in_shell.holelist = NULL;
 
                         }
-
-			//in_shell.save_nodes("shell_mesh_in");
-			//in_shell.save_poly("shell_mesh_in");
+//KRATOS_WATCH("@@@@@@@@@@@@@ before meshing @@@@@@@@@@@@@@@@");
+		//	in_shell.save_nodes("shell_mesh_in");
+		//	in_shell.save_poly("shell_mesh_in");
 			//char tetgen_options[] = "VMYYJ";pA
 			char tetgen_options[] = "pYYJnQ";
 
@@ -195,10 +196,10 @@ namespace Kratos
 
 			tetrahedralize(tetgen_options, &in_shell, &out_shell); //with option to remove slivers
 
-		        //out_shell.save_nodes("shell_mesh_out");
-		        //out_shell.save_elements("shell_mesh_out");
+		  //     out_shell.save_nodes("shell_mesh_out");
+		  //      out_shell.save_elements("shell_mesh_out");
 		        //out_shell.save_faces("shell_mesh_out");
-			//out_shell.save_neighbors("shell_mesh_out");
+		//	out_shell.save_neighbors("shell_mesh_out");
 
 			//deinitialize
 			in_shell.deinitialize();
@@ -206,33 +207,37 @@ namespace Kratos
 
 			//clear elements
 			ThisModelPart.Elements().clear();
-			ThisModelPart.Conditions().clear();
+			//ThisModelPart.Conditions().clear();
 			
 
 			//add contact elements
 			boost::timer adding_elems;
 			Properties::Pointer properties = ThisModelPart.GetMesh().pGetProperties(1);
 			(ThisModelPart.Elements()).reserve(out_shell.numberoftetrahedra);
-		
+	KRATOS_WATCH(out_shell.numberoftetrahedra);
 			for(int iii = 0; iii< out_shell.numberoftetrahedra; iii++)
 			{
 				int id = iii + 1;
 				int base = iii * 4;
-
+	//KRATOS_WATCH("inside 1");
 				Tetrahedra3D4<Node<3> > geom(
 					*( (nodes_begin +  out_shell.tetrahedronlist[base]-1).base() 		), 
 					*( (nodes_begin +  out_shell.tetrahedronlist[base+1]-1).base() 	), 
 					*( (nodes_begin +  out_shell.tetrahedronlist[base+2]-1).base() 	), 
 					*( (nodes_begin +  out_shell.tetrahedronlist[base+3]-1).base() 	) 
 					);
+//KRATOS_WATCH(geom);
+//KRATOS_WATCH("AFTER GEOM");
 				Element::Pointer p_element = rReferenceElement.Create(id, geom, properties);
-				
+				p_element->GetValue(IS_CONTACT_MASTER) = 10;
+					//KRATOS_WATCH("inside 12");
 							      
 				(ThisModelPart.Elements()).push_back(p_element);
 //KRATOS_WATCH(p_element->GetGeometry());
 //KRATOS_WATCH("After get geometry");
 
 			}
+KRATOS_WATCH("After adding elements");
 			ThisModelPart.Elements().Sort();
 			
 			boost::timer adding_neighb;
@@ -253,16 +258,73 @@ namespace Kratos
 					if(index > 0)
 						neighb(i) = *((el_begin + index-1).base());
 					else
-						neighb(i) = Element::WeakPointer();
+						neighb(i) = *(iii.base()); 
 				}
 			}
 			std::cout << "time for adding neigbours" << adding_neighb.elapsed() << std::endl;;
 
-						
+			//***********************************************************************************
+			//***********************************************************************************
+			//mark boundary nodes 
+			//reset the boundary flag
+/*
+			for(ModelPart::NodesContainerType::const_iterator in = ThisModelPart.NodesBegin(); in!=ThisModelPart.NodesEnd(); in++)
+			{
+				in->FastGetSolutionStepValue(IS_BOUNDARY) = 0;
+			}
 
+
+			//***********************************************************************************
+			//***********************************************************************************
+			boost::timer adding_faces;
+KRATOS_WATCH(out_shell.numberoftrifaces);
+			(ThisModelPart.Conditions()).reserve(out_shell.numberoftrifaces   );
+
+			//creating the faces
+			for(ModelPart::ElementsContainerType::const_iterator iii = ThisModelPart.ElementsBegin();
+				iii != ThisModelPart.ElementsEnd(); iii++)
+			{			   
+
+				int base = ( iii->Id() - 1 )*4;
+				
+				//create boundary faces and mark the boundary nodes 
+				//each face is opposite to the corresponding node number so
+				// 0 ----- 1 2 3
+				// 1 ----- 0 3 2
+				// 2 ----- 0 1 3
+				// 3 ----- 0 2 1
+
+				//node 1
+KRATOS_WATCH(rReferenceBoundaryCondition);
+KRATOS_WATCH(properties);
+				//if( neighb(0).expired()  );
+				if( out_shell.neighborlist[base] == -1)
+				{
+					CreateBoundaryFace(1, 2, 3, ThisModelPart,   0, *(iii.base()), properties,rReferenceBoundaryCondition );
+				}
+				//if(neighb(1).expired() );
+				if( out_shell.neighborlist[base+1] == -1)
+				{
+					CreateBoundaryFace(0,3,2, ThisModelPart,   1, *(iii.base()), properties, rReferenceBoundaryCondition );
+				}
+				if( out_shell.neighborlist[base+2] == -1)
+				//if(neighb(2).expired() );
+				{
+					CreateBoundaryFace(0,1,3, ThisModelPart,   2, *(iii.base()), properties, rReferenceBoundaryCondition );
+				}
+				if( out_shell.neighborlist[base+3] == -1)
+				//if(neighb(3).expired() );
+				{
+					CreateBoundaryFace(0,2,1, ThisModelPart,   3, *(iii.base()), properties, rReferenceBoundaryCondition );
+				}
+
+			}
+						
+			std::cout << "time for adding faces" << adding_faces.elapsed() << std::endl;;
+*/
 			out_shell.deinitialize();
 			out_shell.initialize();
-		
+
 			shell_nodes.clear();
 			shell_list.clear();
 
@@ -341,7 +403,46 @@ KRATOS_WATCH(">>>>>>>>>>>>>>>>>>>>< BYE BYE MESHER <<<<<<<<<<<<<<<<<<<<<<<<");
 	private:
 		///@name Static Member Variables 
 		///@{ 
+/*
+	      void CreateBoundaryFace(const int& i1, const int& i2, const int& i3, ModelPart& ThisModelPart, const int& outer_node_id, Element::Pointer origin_element, Properties::Pointer properties,Condition const& rReferenceBoundaryCondition)
+		{
+			KRATOS_TRY
+KRATOS_WATCH("inside create boundary");
+			Geometry<Node<3> >& geom = origin_element->GetGeometry();
+			//mark the nodes as free surface
+			geom[i1].FastGetSolutionStepValue(IS_BOUNDARY) = 1;
+			geom[i2].FastGetSolutionStepValue(IS_BOUNDARY) = 1;
+			geom[i3].FastGetSolutionStepValue(IS_BOUNDARY) = 1;
 
+			//generate a face condition
+			Condition::NodesArrayType temp;
+			temp.reserve(3);
+			temp.push_back(geom(i1)); 
+			temp.push_back(geom(i2));
+			temp.push_back(geom(i3));
+KRATOS_WATCH("face is created 1");
+KRATOS_WATCH(geom);
+KRATOS_WATCH(properties);
+KRATOS_WATCH(rReferenceBoundaryCondition);
+			Geometry< Node<3> >::Pointer cond = Geometry< Node<3> >::Pointer(new Triangle3D3< Node<3> >(temp) );
+			//Geometry< Node<3> >::Pointer cond = Geometry< Node<3> >::Pointer(new Triangle3D< Node<3> >(temp) );
+			int id = (origin_element->Id()-1)*4;
+			//Condition::Pointer p_cond = Condition::Pointer(new Condition(id, cond, properties) );
+			//Condition::Pointer p_cond = rReferenceBoundaryCondition::Pointer(new Condition(id, cond, properties) );
+                        Condition::Pointer p_cond = rReferenceBoundaryCondition.Create(id, temp, properties);
+			//assigning the neighbour node
+KRATOS_WATCH("face is created 2");
+			(p_cond->GetValue(NEIGHBOUR_NODES)).clear();
+			(p_cond->GetValue(NEIGHBOUR_NODES)).push_back( Node<3>::WeakPointer( geom(outer_node_id) ) );
+			(p_cond->GetValue(NEIGHBOUR_ELEMENTS)).clear();
+			(p_cond->GetValue(NEIGHBOUR_ELEMENTS)).push_back( Element::WeakPointer( origin_element ) );
+			ThisModelPart.Conditions().push_back(p_cond);
+			KRATOS_CATCH("")
+KRATOS_WATCH("face is created");
+		}
+*/
+		
+		//////////////////////////////////////////////////////////////////////////////////////
 
 		///@} 
 		///@name Member Variables 
