2f20da6ebf8f358b34b16082f20188f72cde936f
rrossi <rrossi@cimne.upc.edu>
2010-09-07 09:22:26 +0000

    removing the old version of the cavity2d

--- applications/incompressible_fluid_application/test_examples/cavity2d.gid/cavity2d_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for cavity2d.py..."
-benchmarking.BuildReferenceData("cavity2d.py", "cavity_ref.txt")
