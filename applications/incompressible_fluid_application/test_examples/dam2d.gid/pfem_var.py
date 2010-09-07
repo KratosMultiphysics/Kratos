0ed5b8a56b10322af91bb0bde751ca90f0f09ab0
kazem <kazem@cimne.upc.edu>
2010-09-07 10:32:24 +0000

    dam2d

--- applications/incompressible_fluid_application/test_examples/dam2d.gid/pfem_var.py
+++ applications/incompressible_fluid_application/test_examples/dam2d.gid/pfem_var.py
@@ -1,6 +1,6 @@
 domain_size = 2
 
-max_time = 1.0
+max_time = 10.0
 max_x = 2.5
 max_y = 2.0
 max_z = 1.0
@@ -9,8 +9,8 @@ min_y = 0.0
 min_z = 0.0
 use_oss = 0
 dynamic_tau = 1
-nsteps = 100
-output_Dt = 0.01
+nsteps = 150
+output_Dt = .001
 min_dt = 0.05
 max_dt = 0.1
 safety_factor = .1
