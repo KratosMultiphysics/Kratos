c032fb8e98d653c0d9b4f5430095d750cc4e3d51
antonia <antonia@cimne.upc.edu>
2010-09-10 10:55:46 +0000

    Added benchmarks non-newtonian element

--- applications/PFEMapplication/test_exemples/pfem_benchmarks.py
+++ applications/PFEMapplication/test_exemples/pfem_benchmarks.py
@@ -1,26 +1,91 @@
 import os
+import sys
+
+kratos_benchmarking_path = '../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
 
 def Run():
 	Msg = ""
 	Text = "== PFEM ==========\n"
+        
+##	# dam2d
+##
+##	Text += "dam_2d: "
+##	os.chdir("dam2d.gid")	
+##	sys.path.append(os.getcwd())
+##
+##	print "running the pfem dam2d benchmark test..."
+##        Msg = benchmarking.RunBenchmark("dam2d.py","dam2d_ref.txt")
+##	
+##	if (Msg == True):
+##		Text += "OK\n"
+##        	print "pfem dam2d test example succesful"
+##	else:
+##		Text += "FAILED\n"
+##		Text += Msg
+##		Text += "\n\n"
+##        	print "pfem dam2d test example FAILED"
+##
+##
+##	os.chdir("..")       
 
-	# dam2d
 
-	Text += "dam_2d: "
-	os.chdir("dam2d.gid")	
+        ###############################################################################
+	#dam 2D NON-NEWTONIAN
 	
-	import dam2d_benchmark
-	Msg = dam2d_benchmark.Run()
-	
-	if (Msg == True):
-		Text += "OK\n"
-	else:
-		Text += "FAILED\n"
-		Text += Msg
-		Text += "\n\n"
+	Text += "dam_2d_Non-Newtonian: "
+	os.chdir("dam2dNonNewt_Y1500.gid")	
+	sys.path.append(os.getcwd())
+
+	print "running the dam2dNonNewt benchmark test..."
+        Msg = benchmarking.RunBenchmark("dam2dNonNewt.py", "dam2dNonNewt_ref.txt")
+
+        if (Msg == True):
+            Text += "OK\n"
+            print "dam2dNonNewt example succesful"
+        else:
+            Text += "FAILED\n"
+            Text += Msg
+            Text += "\n\n"
+            print "dam2dNonNewt example FAILED"
+
 
 	os.chdir("..")
+        ###############################################################################
+	#slope 2D NON-NEWTONIAN COUPLED
+	
+	Text += "coupled_slope_2d_Non-Newtonian: "
+	os.chdir("Coupled_slope2dNonNewt_Y3000.gid")	
+	sys.path.append(os.getcwd())
+
+	print "running the CoupledNonNewtSlope benchmark test..."
+        Msg = benchmarking.RunBenchmark("CoupledNonNewtSlope.py", "CoupledNonNewtSlope_ref.txt")
+
+        if (Msg == True):
+            Text += "OK\n"
+            print "CoupledNonNewtSlope example succesful"
+        else:
+            Text += "FAILED\n"
+            Text += Msg
+            Text += "\n\n"
+            print "CoupledNonNewtSlope example FAILED"
+
+
+
+
+	os.chdir("..")
+        ###############################################################################
+
 
 	# Add other examples here
 
+
+
+        ################################################################
+        print "resume of all of the examples for the pfem application :"
+        print Text
 	return Text
+
+Run();
