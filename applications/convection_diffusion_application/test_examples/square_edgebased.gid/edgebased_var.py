9a9436f9cea8bda9415b24f1b2224544db4b52f7
antonia <antonia@cimne.upc.edu>
2010-09-10 09:36:05 +0000

    adding a new benchmark for edgebased convection diffusion

---
+++ applications/convection_diffusion_application/test_examples/square_edgebased.gid/edgebased_var.py
@@ -0,0 +1,22 @@
+domain_size = 2
+
+max_time = 7.0
+safety_factor = 0.9
+max_time_step = 0.001
+output_dt = 0.5
+density = 1000.0
+viscosity = 0.000001
+body_force_x = 0.0
+body_force_y = -9.81
+body_force_z = 0.0
+print_layers = True
+use_mass_correction = True
+redistance_frequency = 5
+extrapolation_layers = 5000
+number_of_inital_steps = 10
+initial_time_step = 0.00001
+# Declare Python Variables
+
+problem_name="square_edgebased"
+problem_path="/home/antonia/Desktop/ConvectionTest/square_edgebased.gid"
+kratos_path="/home/antonia/kratos"
