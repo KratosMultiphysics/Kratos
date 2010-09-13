01dca2f05f0476fde184cf4041d70a87240f4a74
antonia <antonia@cimne.upc.edu>
2010-09-13 16:18:39 +0000

    Corrected error in script benchmark PFEM application

--- applications/PFEMapplication/test_exemples/Coupled_slope2dNonNewt_Y3000.gid/CoupledNonNewtSlope.py
+++ applications/PFEMapplication/test_exemples/Coupled_slope2dNonNewt_Y3000.gid/CoupledNonNewtSlope.py
@@ -72,8 +72,8 @@ def NodeFinder(node_list,X,Y,Z):
 def BenchmarkCheck(time, node1):
     benchmarking.Output(time, "Time",1.0,1.0)
 ##    print "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"
-    benchmarking.Output(node1.GetSolutionStepValue(PRESSURE), "Node 1 pressure", 0.1,.0001)
-    benchmarking.Output(node1.GetSolutionStepValue(VELOCITY_Y), "Node 2 velocity_y", 0.1,.0001)
+    benchmarking.Output(node1.GetSolutionStepValue(PRESSURE), "Node 1 pressure", 0.1,0.1)
+    benchmarking.Output(node1.GetSolutionStepValue(VELOCITY_Y), "Node 2 velocity_y", 0.1,0.0001)
 
 #importing the fluid_solver files and adding the variablesv
 edgebased_levelset_solver.AddVariables(fixed_model_part)
