a3d7dc0008ecfac8dff58c9dc3e69fbbe35f21be
gerardo <gerardo@cimne.upc.edu>
2010-09-14 10:42:23 +0000

    Update unix script

--- gid_interface/kratos.gid/kratos.unix.bat
+++ gid_interface/kratos.gid/kratos.unix.bat
@@ -2,9 +2,9 @@
 #    OutputFile: $2/$1.info
 #    ErrorFile: $2/$1.err
 #delete previous result file 
-rm -f  $2/$1.flavia.res 
-rm $2/$1.info
-rm $2/$1.err
-rm $2/$1.flavia.dat
+rm -f $2/$1.post.bin 
+rm -f $2/$1.info
+rm -f $2/$1.err
+rm -f $2/$1.flavia.dat
 
 python KratosOpenMP.py > $2/$1.info 2> $2/$1.err
