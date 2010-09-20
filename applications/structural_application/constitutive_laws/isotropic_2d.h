bab7694aa7a77735c9cd55c900f0448a6788ff99
nelson <nelson@cimne.upc.edu>
2010-09-20 20:12:57 +0000

    split triangle included

--- applications/structural_application/constitutive_laws/isotropic_2d.h
+++ applications/structural_application/constitutive_laws/isotropic_2d.h
@@ -122,7 +122,10 @@ namespace Kratos
             void SetValue( const Variable<Matrix>& rThisVariable, 
                            const Matrix& rValue, 
                            const ProcessInfo& rCurrentProcessInfo );
-			/**
+			   
+	    double& GetValue( const Variable<double>& rThisVariable, double& rValue );
+		
+		/**
 			 * Material parameters are inizialized
 			 */ 
 			void InitializeMaterial( const Properties& props,
@@ -243,7 +246,7 @@ namespace Kratos
 			  */
 			 void CalculateElasticMatrix(Matrix& C, const double E, const double NU);
 
-		         double mE,mNU;
+		         double mE,mNU, mDE ;
 
  	//		 void CalculatePlaneElasticMatrix(Matrix& C, const double E, const double NU);
 
