ebbebe6db6a45561019d3a552810deb056cd61f0
nelson <nelson@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-09-20 20:18:11 +0000

    time corrected

--- kratos/applications/structural_application/soft_hard_behavior/softening_hardening_criteria.h
+++ kratos/applications/structural_application/soft_hard_behavior/softening_hardening_criteria.h
@@ -1,102 +1,271 @@
-/*
-==============================================================================
-Kratos
-A General Purpose Software for Multi-Physics Finite Element Analysis
-Version 1.0 (Released on march 05, 2007).
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author: pooyan $
+//   Date:                $Date: 2006-11-27 16:07:33 $
+//   Revision:            $Revision: 1.1.1.1 $
+//
+//
 
-Copyright 2007
-Pooyan Dadvand, Riccardo Rossi
-pooyan@cimne.upc.edu
-rrossi@cimne.upc.edu
-CIMNE (International Center for Numerical Methods in Engineering),
-Gran Capita' s/n, 08034 Barcelona, Spain
 
-Permission is hereby granted, free  of charge, to any person obtaining
-a  copy  of this  software  and  associated  documentation files  (the
-"Software"), to  deal in  the Software without  restriction, including
-without limitation  the rights to  use, copy, modify,  merge, publish,
-distribute,  sublicense and/or  sell copies  of the  Software,  and to
-permit persons to whom the Software  is furnished to do so, subject to
-the following condition:
+#if !defined(KRATOS_SOFTENING_HARDENING_CRITERIA)
+#define  KRATOS_SOFTENING_HARDENING_CRITERIA
 
-Distribution of this code for  any  commercial purpose  is permissible
-ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
 
-The  above  copyright  notice  and  this permission  notice  shall  be
-included in all copies or substantial portions of the Software.
 
-THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
-EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
-CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
-TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-==============================================================================
-*/
- 
-/* *********************************************************
-*
-*   Last Modified by:    $Author: Nelson Lafontaine $
-*   Date:                $Date: 26-06-2009 $
-*   Revision:            $Revision: 1.0 $
-*
-* ***********************************************************/
-
-#if !defined( KRATOS_SOFTENING_HARDENING_CRITERIA)
-#define KRATOS_SOFTENING_HARDENING_CRITERIA
-
-/* System includes */
-
-/* External includes */
-#include "boost/smart_ptr.hpp"
-
-#include "custom_utilities/tensor_utils.h"
-#include "includes/ublas_interface.h"
-#include "includes/properties.h"
-#include <cmath>
+// System includes
 #include <string>
-#include <iostream>
+#include <iostream> 
 
+
+// External includes 
+
+
+// Project includes
+#include "includes/define.h"
+#include "structural_application.h"
 
 
 namespace Kratos
 {
 
-
-      class SofteningHardeningCriteria
-	  {
-	    public:
-           
-            SofteningHardeningCriteria(){}
-            virtual ~SofteningHardeningCriteria(){}
-            KRATOS_CLASS_POINTER_DEFINITION( SofteningHardeningCriteria );
-	    
-	    virtual double FunctionSofteningHardeningBehavior(const double& A, const double& r_o, const double& r) 
-	      { 
-		    return 0.00;
-	      }
-         
-            // Calcula la funcion de abalandamiento o endurecimiento junto con su derivada respecto kp_punto (0 <= kp_punto <= 1.00) 
-            virtual void FunctionSofteningHardeningBehavior(const double& capap, const double& sigma, double& Result, double& der_Result) 
-	      { 
-		    return;
-	      }
-
-            virtual void Linear_Strain_Softening(Vector& Principal_Stress)
-             {
-               return;
-             } 
+  ///@name Kratos Globals
+  ///@{ 
   
-	    protected:
+  ///@} 
+  ///@name Type Definitions
+  ///@{ 
+  
+  ///@} 
+  ///@name  Enum's
+  ///@{
+      
+  ///@}
+  ///@name  Functions 
+  ///@{
+      
+  ///@}
+  ///@name Kratos Classes
+  ///@{
+  
+  /// Short class definition.
+  /** Detail class definition.
+  */
+  class SofteningHardeningCriteria
+    {
+    public:
+      ///@name Type Definitions
+      ///@{
+      
+      /// Pointer definition of SofteningHardeningCriteria
+      KRATOS_CLASS_POINTER_DEFINITION(SofteningHardeningCriteria);
+        
+      ///@}
+      ///@name Life Cycle 
+      ///@{ 
+      
+      /// Default constructor.
+      SofteningHardeningCriteria(){}
 
-	    private:
-                 
-          };
-    /**
-     * definition of CONSTITUTIVE_LAW variable
-     */
-}  /* namespace Kratos.*/
-#endif /* FLUENCY_CRITERIA defined */
+      /// Destructor.
+      virtual ~SofteningHardeningCriteria(){}
+      
+      
+      virtual boost::shared_ptr<SofteningHardeningCriteria> Clone() const
+      {
+      boost::shared_ptr<SofteningHardeningCriteria> p_clone(new SofteningHardeningCriteria());
+      return p_clone;
+      }
+     
+      /*
+      virtual double FunctionSofteningHardeningBehavior(const double& A, const double& r_o, const double& r) 
+	{ 
+	      return 0.00;
+	}
+      */	
+    
+      // Calcula la funcion de abalandamiento o endurecimiento junto con su derivada respecto kp_punto (0 <= kp_punto <= 1.00) 
+      virtual void FunctionSofteningHardeningBehavior(const double& capap, const double& sigma, double& Result, double& der_Result) 
+	{ 
+	      return;
+	}
 
+      virtual void Linear_Strain_Softening(Vector& Principal_Stress)
+	{
+	  return;
+	} 
+ 
+      virtual void   InitializeMaterial(const Properties& props){mprops = &props;}
+      virtual double Calculate(Vector& Imput_Parameters) {return 0;}
+	
+  
+      ///@}
+      ///@name Operators 
+      ///@{
+      
+      
+      ///@}
+      ///@name Operations
+      ///@{
+      
+      
+      ///@}
+      ///@name Access
+      ///@{ 
+      
+      
+      ///@}
+      ///@name Inquiry
+      ///@{
+      
+      
+      ///@}      
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+      virtual std::string Info() const
+      {
+	return "The Base Class of Softening-Hardening Behavior";
+      }
+      
+      /// Print information about this object.
+      virtual void PrintInfo(std::ostream& rOStream)const
+      {
+	 rOStream << Info();
+      }
+
+      /// Print object's data.
+      virtual void PrintData(std::ostream& rOStream) const
+      {
+	 return;
+      }
+      
+            
+      ///@}      
+      ///@name Friends
+      ///@{
+      
+            
+      ///@}
+      
+    protected:
+      ///@name Protected static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected member Variables 
+      ///@{ 
+      const Properties *mprops;
+      
+        
+      ///@} 
+      ///@name Protected Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected  Access 
+      ///@{ 
+        
+        
+      ///@}      
+      ///@name Protected Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Protected LifeCycle 
+      ///@{ 
+      
+            
+      ///@}
+      
+    private:
+      ///@name Static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private  Access 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Private Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Un accessible methods 
+      ///@{ 
+      
+      /// Assignment operator.
+      SofteningHardeningCriteria& operator=(SofteningHardeningCriteria const& rOther)
+      {
+          mprops= rOther.mprops;
+	  return *this;
+      }
+      
+
+      /// Copy constructor.
+      SofteningHardeningCriteria(SofteningHardeningCriteria const& rOther):
+          mprops(rOther.mprops)
+      {
+      }
+
+        
+      ///@}    
+        
+    }; // Class SofteningHardeningCriteria 
+
+  ///@} 
+  
+  ///@name Type Definitions       
+  ///@{ 
+  
+  
+  ///@} 
+  ///@name Input and output 
+  ///@{ 
+        
+ 
+  /// input stream function
+   inline std::istream& operator >> (std::istream& rIStream, 
+ 				    SofteningHardeningCriteria& rThis);
+ 
+   /// output stream function
+   inline std::ostream& operator << (std::ostream& rOStream, 
+ 				    const SofteningHardeningCriteria& rThis)
+     {
+       rThis.PrintInfo(rOStream);
+       rOStream << std::endl;
+       rThis.PrintData(rOStream);
+       return rOStream;     
+     }
+  ///@} 
+  
+  
+}  // namespace Kratos.
+
+#endif // KRATOS_SOFTENING_HARDENING_CRITERIA defined 
