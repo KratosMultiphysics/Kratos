2bf744fd9b736c60b8ae7aefaa6835a44784a9dc
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-09-23 07:57:42 +0000

    make it to work in mpi

--- kratos/applications/incompressible_fluid_application/custom_utilities/pure_convection_edgebased.h
+++ kratos/applications/incompressible_fluid_application/custom_utilities/pure_convection_edgebased.h
@@ -479,6 +479,9 @@ namespace Kratos
 // 						for (unsigned int k_comp = 0; k_comp < TDim; k_comp++)
 // 							for (unsigned int m_comp = 0; m_comp < TDim; m_comp++)
 // 								aaa += a_i[k_comp] * a_i[m_comp] * edge_ij.LaplacianIJ(k_comp,m_comp);
+
+/*						if(norm_a > 1e-20)
+						  aaa/=(norm_a*norm_a);*/
 // 						aaa/=(norm_a*norm_a);
 // 						double capturing2 = aaa * (phi_j - phi_i);
 // 						
