92f7893c2ee0a700d4433361364eb1f3981d50a7
mossaiby <mossaiby@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-10-02 09:33:26 +0000

    Minor updating of OpenCL interface temporary test file

--- kratos/applications/OpenCLapplication/custom_utilities/test_ocli.cpp
+++ kratos/applications/OpenCLapplication/custom_utilities/test_ocli.cpp
@@ -1,23 +1,28 @@
 #include "opencl_interface.h"
 #include <cmath>
 
-#include <cstdlib>
+#ifdef _OPENMP
+	#include <omp.h>
+#else
+	#include <ctime>
+#endif
 
 #ifdef _OPENMP
-#include <omp.h>
+	#define TIMER()					omp_get_wtime()
 #else
-#include <ctime>
+	#define TIMER()					clock()
 #endif
 
 const cl_uint DataSize = 360000;
 const double Eps = 1e-10;
+const double Offset = 10.00;
 
-double Input[DataSize], Output[DataSize];
+double OCLInput[DataSize], OCLOutput[DataSize], HostInput[DataSize], HostOutput[DataSize];
 
 int main(int argc, char *argv[])
 {
 	std::cout << "Initializing OpenCL device group..." << std::endl;
-	Kratos::OpenCL::DeviceGroup OCLDeviceGroup(CL_DEVICE_TYPE_CPU, false);  // Try to find all available devices
+	Kratos::OpenCL::DeviceGroup OCLDeviceGroup(CL_DEVICE_TYPE_ALL, false);  // Try to find all available devices
 
 	std::cout << "Found " << OCLDeviceGroup.DeviceNo << " device(s)." << std::endl;
 	for (cl_uint i = 0; i < OCLDeviceGroup.DeviceNo; i++)
@@ -26,15 +31,15 @@ int main(int argc, char *argv[])
 	}
 
 	std::cout << "Loading kernel(s) from file test_ocli.cl..." << std::endl;
-	OCLDeviceGroup.BuildProgramFromFile("test_ocli.cl", "-cl-unsafe-math-optimizations");
+	OCLDeviceGroup.BuildProgramFromFile("/home/mossaiby/kratos/applications/OpenCLapplication/custom_utilities/opencl_edge_data.cl");
 
 	std::cout << "Registering kernel Test()..." << std::endl;
 	cl_uint TestKernel = OCLDeviceGroup.RegisterKernel("Test");  // This will return 0, but we do not want to memorize it ourselves!
 
 	for (cl_uint i = 0; i < DataSize; i++)
 	{
-		Input[i] = i;
-		Output[i] = -1.00;
+		OCLInput[i] = i;
+		OCLOutput[i] = -1.00;
 	}
 
 	std::cout << "Creating the buffers..." << std::endl;
@@ -44,57 +49,66 @@ int main(int argc, char *argv[])
 	std::cout << "Copying the data to the device(s)..." << std::endl;
 	for (cl_uint i = 0; i < OCLDeviceGroup.DeviceNo; i++)
 	{
-		OCLDeviceGroup.CopyBuffer(i, InputBuffer, Kratos::OpenCL::HostToDevice, Input + i * DataSize / OCLDeviceGroup.DeviceNo);
+		OCLDeviceGroup.CopyBuffer(i, InputBuffer, Kratos::OpenCL::HostToDevice, OCLInput + i * DataSize / OCLDeviceGroup.DeviceNo);
 	}
 
 	std::cout << "Setting kernel arguments..." << std::endl;
 	OCLDeviceGroup.SetBufferAsKernelArg(TestKernel, 0, InputBuffer);
 	OCLDeviceGroup.SetBufferAsKernelArg(TestKernel, 1, OutputBuffer);
-	OCLDeviceGroup.SetKernelArg(TestKernel, 2, 10.00);
+	OCLDeviceGroup.SetKernelArg(TestKernel, 2, Offset);
+
+	double OCLTimer = TIMER();
 
 	std::cout << "Executing kernel..." << std::endl;
 	OCLDeviceGroup.ExecuteKernel(TestKernel, DataSize / OCLDeviceGroup.DeviceNo);
 
+	OCLTimer = TIMER() - OCLTimer;
+
 	std::cout << "Copying the data from the device(s)..." << std::endl;
 	for (cl_uint i = 0; i < OCLDeviceGroup.DeviceNo; i++)
 	{
-		OCLDeviceGroup.CopyBuffer(i, OutputBuffer, Kratos::OpenCL::DeviceToHost, Output + i * DataSize / OCLDeviceGroup.DeviceNo);
+		OCLDeviceGroup.CopyBuffer(i, OutputBuffer, Kratos::OpenCL::DeviceToHost, OCLOutput + i * DataSize / OCLDeviceGroup.DeviceNo);
 	}
 
 	std::cout << "Verification of results..." << std::endl;
 	for (cl_uint i = 0; i < DataSize; i++)
 	{
-		if (Output[i] - (2.00 * sin(i) * cos(i) + 10.00) > Eps)
+		if (OCLOutput[i] - (2.00 * sin(i) * cos(i) + Offset) > Eps)
 		{
 			std::cout << "Error at position " << i << std::endl;
 		}
 	}
-	std::cout << "Verification finished." << std::endl;
-	
-	#ifndef _OPENMP
-	  double start_time = clock();
-	#else
-	  double start_time = omp_get_wtime();
-	#endif
-	
-	#pragma omp parallel for
-	for(int i =0; i<DataSize;i++)
-	  Output[i] = -1.0;
-	
-	#pragma omp parallel for
-	for(int i =0; i<DataSize;i++)
-	  Output[i] -= (2.00 * sin(i) * cos(i) + 10.00);
 
-	
-	#ifndef _OPENMP
-	  double stop_time = clock();
-	  std::cout << "no openmp timing " <<  (stop_time - start_time)/CLOCKS_PER_SEC <<  std::endl;
-	#else
-	  double stop_time = omp_get_wtime();
-	  std::cout << "with openmp " <<  (stop_time - start_time) <<  std::endl;
-	#endif
-	
-	
+	std::cout << "Verification finished." << std::endl;
+
+	#pragma omp parallel for
+	for(cl_uint i = 0; i < DataSize; i++)
+	{
+		HostInput[i] = i;
+		// HostOutput[i] = -1;
+	}
+
+	double HostTimer = TIMER();
+
+	#pragma omp parallel for
+	for(cl_uint i = 0; i < DataSize; i++)
+		HostOutput[i] = (2.00 * sin(i) * cos(i) + Offset);
+
+	HostTimer = TIMER() - HostTimer;
+
+#ifdef _OPENMP
+		std::cout <<
+			"Timing with OpenMP" << std::endl <<
+			std::endl <<
+			"OpenCL: " << OCLTimer << std::endl <<
+			"Host:   " << HostTimer << std::endl;
+#else
+		std::cout <<
+			"Timing without OpenMP" << std::endl <<
+			std::endl <<
+			"OpenCL: " << OCLTimer / CLOCKS_PER_SEC << std::endl <<
+			"Host:   " << HostTimer / CLOCKS_PER_SEC << std::endl;
+#endif
 
 	return 0;
 }
