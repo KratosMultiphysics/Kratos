e9ef531b825967eee713109d6e386f805746999d
antonia <antonia@cimne.upc.edu>
2010-10-18 17:40:05 +0000

    add PODApplication

---
+++ applications/PODApplication/test_examples/cylinder.gid/fluid_only_var.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+domain_size = 2
+
+Dt = 0.05
+max_time =20
+#20.0
+#0.1
+#100
+#20.0
+SolverType = "monolithic_solver_eulerian"
+laplacian_form = 3
+output_step = 2
+#20
+#2
+predictor_corrector = True
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 0.0
+# Declare Python Variables
+
+problem_name="cylinder"
+problem_path="/home/rrossi/kratos/applications/PODApplication/test_examples/cylinder.gid"
+kratos_path="/home/rrossi/kratos"
