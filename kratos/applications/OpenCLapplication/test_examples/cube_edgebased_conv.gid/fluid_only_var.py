3f12f4ce8b4473924bdc32901707a0e5f912c02b
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-10-21 16:00:49 +0000

    committing the test example

---
+++ kratos/applications/OpenCLapplication/test_examples/cube_edgebased_conv.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 3
+
+Dt = 0.01
+max_time = 1.0
+SolverType = "fractional_step"
+laplacian_form = 1
+output_step = 1
+predictor_corrector = False
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 1.0
+# Declare Python Variables
+
+problem_name="cube_edgebased_conv"
+problem_path="/home/rrossi/esempi/cube_edgebased_conv.gid"
+kratos_path="/home/rrossi/kratos"
