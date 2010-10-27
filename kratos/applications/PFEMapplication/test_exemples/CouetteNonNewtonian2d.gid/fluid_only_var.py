66329f1ac62c32360f31fa07589a09dc4db50143
antonia <antonia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-10-27 18:21:28 +0000

    Couette Non Newtoniana 2d Benchmark added

---
+++ kratos/applications/PFEMapplication/test_exemples/CouetteNonNewtonian2d.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 2
+
+Dt = 0.01
+max_time = 3.5
+SolverType = "monolithic_solver_eulerian"
+laplacian_form = 1
+output_step = 1
+predictor_corrector = False
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 1.0
+# Declare Python Variables
+
+problem_name="CouetteNonNewtonian2d"
+problem_path="/media/data/EXAMPLES/PFEM/NON_NEWTONIAN/COUETTEflow/CouetteNonNewtonian2d.gid"
+kratos_path="/home/antonia/kratos"
