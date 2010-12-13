a33d31f8766aea76014b17dee931e2db50dadc24
nelson <nelson@cimne.upc.edu>
2010-12-13 10:23:21 +0000

    fixed error :88:36: error: utilities/timer_CLabra.h: No such file or directory

--- applications/MeshingApplication/custom_utilities/local_triangle_refine_mesh.h
+++ applications/MeshingApplication/custom_utilities/local_triangle_refine_mesh.h
@@ -1,4 +1,7 @@
-//   
+
+///WARNING
+/// it is needed compute he neirgbourg
+
 //   Project Name:        Kratos       
 //   Last Modified by:    $Author: Nelson $
 //   Date:                $Date: 2010-05-12 $
@@ -14,15 +17,7 @@
 #include <omp.h>
 #endif
 
-// #include "boost/smart_ptr.hpp"
-// #include <boost/timer.hpp>
-// #include <boost/numeric/ublas/matrix.hpp>
-// #include <boost/numeric/ublas/vector.hpp>
-// #include <boost/numeric/ublas/banded.hpp>
-// #include <boost/numeric/ublas/matrix_sparse.hpp>
-// #include <boost/numeric/ublas/triangular.hpp>
-// #include <boost/numeric/ublas/operation.hpp>
-// #include <boost/numeric/ublas/lu.hpp>
+
 
 
 // System includes
@@ -88,13 +83,11 @@ void Local_Refine_Mesh(bool refine_on_reference, bool interpolate_internal_varia
 	 if(!(mr_model_part.NodesBegin()->SolutionStepsDataHas(DISPLACEMENT)) )
 	     KRATOS_ERROR(std::logic_error,"DISPLACEMENT Variable is not in the model part -- needed if refine_on_reference = true","")
 
-      //NodesArrayType& pNodes = mr_model_part.Nodes();
       compressed_matrix<int> Coord;
       boost::numeric::ublas::vector<int> List_New_Nodes;                         ///* the news nodes
       boost::numeric::ublas::vector<array_1d<int, 2 > > Position_Node;           ///* edges where are the news nodes
       boost::numeric::ublas::vector< array_1d<double, 3> > Coordinate_New_Node;  ///* the coordinate of the new nodes
      
-        
       PointerVector< Element > New_Elements;
       if(refine_on_reference==true)
        {
@@ -108,12 +101,10 @@ void Local_Refine_Mesh(bool refine_on_reference, bool interpolate_internal_varia
  	
       CSR_Row_Matrix(mr_model_part, Coord); 
       Search_Edge_To_Be_Refined(mr_model_part, Coord); 
-      Create_List_Of_New_Nodes(mr_model_part, Coord, List_New_Nodes, Position_Node);
+      Create_List_Of_New_Nodes(mr_model_part, Coord, List_New_Nodes, Position_Node);    
       Calculate_Coordinate_And_Insert_New_Nodes(mr_model_part, Position_Node, List_New_Nodes);
-      //Insert_New_Node_In_Mesh(mr_model_part, Coordinate_New_Node, Position_Node, List_New_Nodes);
-      Erase_Old_Element_And_Create_New_Triangle_Element(mr_model_part, Coord, New_Elements, interpolate_internal_variables); 
+      Erase_Old_Element_And_Create_New_Triangle_Element(mr_model_part, Coord, New_Elements, interpolate_internal_variables);    
       Renumering_Elements_And_Nodes(mr_model_part, New_Elements);
-
 
       
   if(refine_on_reference==true)
@@ -126,7 +117,7 @@ void Local_Refine_Mesh(bool refine_on_reference, bool interpolate_internal_varia
  	     it->Z()=it->Z0() + disp[2];
           }
   } 
-     
+
       KRATOS_CATCH("")
 }
 
@@ -157,8 +148,7 @@ void CSR_Row_Matrix(ModelPart& this_model_part, compressed_matrix<int>& Coord )
 		   active++;
 		 }
 	  }
-	  std::sort(aux.begin(), aux.begin()+active);
-
+	  std::sort(aux.begin(), aux.begin()+active);	  
 	  for(unsigned int k=0; k<active; k++)
 	  {
 		Coord.push_back(index_i, aux[k], -1);
@@ -181,8 +171,9 @@ ElementsArrayType::iterator it_begin = rElements.ptr_begin(); //+element_partiti
 ElementsArrayType::iterator it_end   = rElements.ptr_end();     //+element_partition[k+1];
 for (ElementsArrayType::iterator it= it_begin; it!=it_end; ++it)
   {     
-        unsigned int level = it->GetValue(REFINEMENT_LEVEL);
-         if(level==1)
+        
+         //unsigned int level = it->GetValue(REFINEMENT_LEVEL);
+         if (it->GetValue(SPLIT_ELEMENT) == true)
            { 
   	    Element::GeometryType& geom = it->GetGeometry(); // Nodos del elemento        
  	    for (unsigned int i = 0; i <geom.size(); i++)
@@ -194,8 +185,8 @@ for (ElementsArrayType::iterator it= it_begin; it!=it_end; ++it)
                         Coord(index_i, index_j) = -2;
                       }  
                   }
-             }
-        }
+              }
+           }
 
     }
 }
@@ -214,7 +205,7 @@ NodesArrayType& pNodes =  this_model_part.Nodes();
 typedef compressed_matrix<int>::iterator1 i1_t;
 typedef compressed_matrix<int>::iterator2 i2_t;
 
-///*WARNING
+///*WARNING = it is can be optimizase
 for (i1_t i1 = Coord.begin1(); i1 != Coord.end1(); ++i1) {
     for (i2_t i2 = i1.begin(); i2 != i1.end(); ++i2)
       { if( Coord( i2.index1(), i2.index2() )==-2)
@@ -235,7 +226,7 @@ for(unsigned int i = 0; i <number_of_new_nodes; i++ )
 
 
 ///* setting edges -2 to the new id of the new node
-///* WARNING
+///* WARNING = it is can be optimizase
 Position_Node.resize(number_of_new_nodes);
 unsigned int index = 0;
 for (i1_t i1 = Coord.begin1(); i1 != Coord.end1(); ++i1) {
@@ -270,10 +261,11 @@ Coordinate_New_Node.resize(Position_Node.size());
 unsigned int step_data_size = this_model_part.GetNodalSolutionStepDataSize();
 Node<3>::DofsContainerType& reference_dofs = (this_model_part.NodesBegin())->GetDofs(); 
 
+
 for(unsigned int i = 0; i < Position_Node.size(); i++ )
   {
-
      /// calculating the coordinate of the news nodes  
+
      const int& node_i     = Position_Node[i][0];
      const int& node_j     = Position_Node[i][1];
      ModelPart::NodesContainerType::iterator it_node1 =  this_model_part.Nodes().find(node_i);
@@ -283,17 +275,14 @@ for(unsigned int i = 0; i < Position_Node.size(); i++ )
      std::size_t pos2 = it_node2 - this_model_part.NodesBegin();
      noalias(Coord_Node_2) = it_node2->Coordinates(); 
      noalias(Coordinate_New_Node[i]) =  0.50 * (Coord_Node_1 + Coord_Node_2);
-     
      /// inserting the news node in the model part 
+
      Node<3>::Pointer  pnode = this_model_part.CreateNewNode(List_New_Nodes[i],Coordinate_New_Node[i][0], Coordinate_New_Node[i][1], Coordinate_New_Node[i][2] );
      pnode->SetBufferSize(this_model_part.NodesBegin()->GetBufferSize() );
- 
+     
      it_node1 = this_model_part.NodesBegin() + pos1;
      it_node2 = this_model_part.NodesBegin() + pos2;
-//      it_node1 =  this_model_part.Nodes().find(node_i);
-//      it_node2 =  this_model_part.Nodes().find(node_j);
 
-    
      pnode->X0() = 0.5*(it_node1->X0() + it_node2->X0());
      pnode->Y0() = 0.5*(it_node1->Y0() + it_node2->Y0());
      pnode->Z0() = 0.5*(it_node1->Z0() + it_node2->Z0()); 
@@ -301,12 +290,7 @@ for(unsigned int i = 0; i < Position_Node.size(); i++ )
      for(Node<3>::DofsContainerType::iterator iii = reference_dofs.begin();    iii != reference_dofs.end(); iii++)  
       {
        Node<3>::DofType& rDof = *iii;
-       Node<3>::DofType::Pointer p_new_dof = pnode->pAddDof( rDof );
-       //KRATOS_WATCH(it_node1->Id())
-       //KRATOS_WATCH(it_node2->Id())  
-       //KRATOS_WATCH(iii->GetVariable())  
-       //KRATOS_WATCH(it_node1->IsFixed(iii->GetVariable()))   
-       //KRATOS_WATCH(it_node2->IsFixed(iii->GetVariable()))          
+       Node<3>::DofType::Pointer p_new_dof = pnode->pAddDof( rDof );        
        if(it_node1->IsFixed(iii->GetVariable()) == true && it_node2->IsFixed(iii->GetVariable()) == true)
          (p_new_dof)->FixDof();
        else
@@ -334,10 +318,7 @@ for(unsigned int i = 0; i < Position_Node.size(); i++ )
        {          
           if(pnode->IsFixed(iii->GetVariable())==false) 
                  { 
-                    //KRATOS_WATCH("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") 
-                    //KRATOS_WATCH( iii->GetSolutionStepReactionValue());
                     iii->GetSolutionStepReactionValue() = zero;
-                    //KRATOS_WATCH( iii->GetSolutionStepReactionValue());
                   }       
        }          
   }
@@ -442,8 +423,7 @@ void Erase_Old_Element_And_Create_New_Triangle_Element(
        ElementsArrayType::iterator it_end   =  rElements.ptr_end(); 
        Element const rReferenceElement;   
        unsigned int to_be_deleted=0;
-       unsigned int large_id = (rElements.end()-1)->Id() * 7;
-       //array_1d<int,3> triangle_ids; 
+       unsigned int large_id = (rElements.end()-1)->Id() * 10;
        bool create_element = false; 
        int  edge_ids[3];       
        int  t[12];       
@@ -451,89 +431,22 @@ void Erase_Old_Element_And_Create_New_Triangle_Element(
        int  splitted_edges  = 0; 
        int  nint            = 0;
        array_1d<int,6> aux;
-       //PointerVector< Element > New_Elements;  
        
        
        ProcessInfo& rCurrentProcessInfo  = this_model_part.GetProcessInfo();  
        PointerVector< Element > Old_Elements;
 
-
 	unsigned int current_id = (rElements.end()-1)->Id() + 1;
         for (ElementsArrayType::iterator it= it_begin; it!=it_end; ++it)
          { 
-
-             //KRATOS_WATCH(it->Id())
 	     for (unsigned int i = 0; i<12; i++) {t[i]=-1; } 
   	     Element::GeometryType& geom = it->GetGeometry();
-	     Calculate_Edges(geom, Coord, edge_ids, aux);
-	     
-	     /*
-             int index_0 = geom[0].Id()-1;
-             int index_1 = geom[1].Id()-1;
-             int index_2 = geom[2].Id()-1; 
-	     
-   
-             aux[0] = geom[0].Id();
-	     aux[1] = geom[1].Id();
-	     aux[2] = geom[2].Id();
-            //------------------------------------------------------------------------- 
-            if(index_0 > index_1)
-	      aux[3] = Coord(index_1, index_0);
-            else
-              aux[3] = Coord(index_0, index_1); 
-
-  
-            if(index_1 > index_2)
-               aux[4] = Coord(index_2, index_1);
-            else 
-	       aux[4] = Coord(index_1, index_2 );
-
-	    
-            if(index_2 > index_0)
-               aux[5] = Coord(index_0, index_2);
-            else 
-	       aux[5] = Coord(index_2, index_0 );           
-	    //-------------------------------------------------------------------------
-	    
-	    //edge 01
-	    if(aux[3] < 0)
-		if(index_0 > index_1) edge_ids[0] = 0;                           
-		else edge_ids[0] = 1;
-	    else
-		edge_ids[0] = 3;
-
-	    //edge 12
-	    if(aux[4] < 0)
-		if(index_1 > index_2) edge_ids[1] = 1;
-		else edge_ids[1] = 2;
-	    else
-		edge_ids[1] = 4;
-
-	    //edge 20
-	    if(aux[5] < 0)
-		if(index_2 > index_0) edge_ids[2] = 2;
-		else edge_ids[2] = 0;
-	    else
-		edge_ids[2] = 5;
-
-	    /*
-	     if(index_i > index_j) edge_ids[0] = Coord(index_j, index_i );
-	     else edge_ids[0] = Coord(index_i, index_j );
-	    
-	     if(index_j > index_k) edge_ids[1] = Coord(index_k, index_j );
-	     else edge_ids[1] = Coord(index_j, index_k );
-	    
-	     if(index_k > index_i) edge_ids[2] = Coord(index_i, index_k );
-	     else edge_ids[2] = Coord(index_k, index_i ); */
-		  
+	     Calculate_Edges(geom, Coord, edge_ids, aux);		  
 
             ///* crea las nuevas conectividades
-// 	    for (unsigned int i = 0; i<3; i++) {KRATOS_WATCH (edge_ids[i])}  
 	    create_element =  Split_Triangle(edge_ids, t, &nel, &splitted_edges, &nint); 
-	    
-//  	    KRATOS_WATCH(nel)   
-//  	    for (unsigned int i = 0; i<12; i++) {KRATOS_WATCH (t[i])}  
- 	   
+	     
+ 
             ///* crea los nuevos elementos           
             if(create_element==true)
              {
@@ -545,21 +458,8 @@ void Erase_Old_Element_And_Create_New_Triangle_Element(
 		  unsigned int i0   = aux[t[base]];
 		  unsigned int i1   = aux[t[base+1]];
 		  unsigned int i2   = aux[t[base+2]];
-// 		  KRATOS_WATCH(base)
-// 		  KRATOS_WATCH(t[base])
-// 		  KRATOS_WATCH(t[base+1])
-// 		  KRATOS_WATCH(t[base+2])
-// 		  KRATOS_WATCH(aux)
-// 		  KRATOS_WATCH(aux[t[base]]);
-// 		  KRATOS_WATCH(aux[t[base+1]]);
-// 		  KRATOS_WATCH(aux[t[base+2]]);
-// 		
-// 		  KRATOS_WATCH(i0)
-// 		  KRATOS_WATCH(i1)
-// 		  KRATOS_WATCH(i2)
-// 		  KRATOS_WATCH("----------------------------------")
-		  
-	
+
+		  	
    		  Triangle2D3<Node<3> > geom(
 						this_model_part.Nodes()(i0),
 						this_model_part.Nodes()(i1),
@@ -575,10 +475,12 @@ void Erase_Old_Element_And_Create_New_Triangle_Element(
 		       
 		       /// setting the internal variables in the child elem
 		       if(interpolate_internal_variables == true)
-			  InterpolateInteralVariables(nel , *it.base(),p_element,rCurrentProcessInfo);	
+			  InterpolateInteralVariables(nel , *it.base(),p_element,rCurrentProcessInfo);
 		       
-		       //int level =  it->GetValue(REFINEMENT_LEVEL);
-		       p_element->SetValue(REFINEMENT_LEVEL, 1);	       
+		       //const unsigned int& level = it->GetValue(REFINEMENT_LEVEL);
+		       p_element->GetValue(REFINEMENT_LEVEL) = it->GetValue(REFINEMENT_LEVEL);
+                       p_element->GetValue(SPLIT_ELEMENT) = false;
+		       //p_element->SetValue(REFINEMENT_LEVEL, 1);	       
                        New_Elements.push_back(p_element);                         
                        current_id++;    
                                          
@@ -590,17 +492,9 @@ void Erase_Old_Element_And_Create_New_Triangle_Element(
           }         
   
           ///* adding news elements to the model part
-          //Vector temp;
           for(PointerVector< Element >::iterator it_new = New_Elements.begin(); it_new!=New_Elements.end(); it_new++)
-            {      
-               
-               //it_new->Initialize();  
-               //it_new->InitializeSolutionStep(rCurrentProcessInfo);      
-               //it_new->CalculateRightHandSide(temp, rCurrentProcessInfo);
-               //sit_new->FinalizeSolutionStep(rCurrentProcessInfo);           
+            {             
                rElements.push_back(*(it_new.base()));
-	       //const std::string type = it_new->Info();  
-               //KRATOS_WATCH(type)
             } 
              
           ///* all of the elements to be erased are at the end
@@ -682,15 +576,11 @@ PointerVector< Element >& New_Elements)
    NodesArrayType& pNodes =  this_model_part.Nodes();  
    NodesArrayType::iterator i_begin=pNodes.ptr_begin();
    NodesArrayType::iterator i_end=pNodes.ptr_end();
-   //ProcessInfo& rCurrentProcessInfo  = this_model_part.GetProcessInfo();  
 
 
    for(ModelPart::NodeIterator i=i_begin; i!= i_end; ++i)     
 	{
-           if(i->Id()!=id_node)
-              {  //std::cout<< "Setting Id of Node  " << i->Id() << " by " <<  id_node << std::endl; 
-                 i->SetId(id_node);
-              }
+           if(i->Id()!=id_node){i->SetId(id_node);}
             id_node++;     
         }
 
@@ -700,28 +590,9 @@ PointerVector< Element >& New_Elements)
 
   for (ElementsArrayType::iterator it= it_begin; it!=it_end; ++it)
         {
-          if(it->Id()!=id_elem)
-          { 
-            //std::cout<< "Setting Id of elelemnt  " << it->Id() << " by " <<  id_elem << std::endl;  
-            it->SetId(id_elem);
-          }
+          if(it->Id()!=id_elem){it->SetId(id_elem);}
           id_elem++;
         }
-
-
-  ///* adding news elements to the model part
-//  Vector temp;  
-/*
-  for(PointerVector< Element >::iterator it_new = New_Elements.begin(); it_new!=New_Elements.end(); it_new++)
-    {      
-      std::cout<< "************************************************" <<std::endl;
-      KRATOS_WATCH(it_new->Id())  
-      it_new->Initialize();  
-      it_new->InitializeSolutionStep(rCurrentProcessInfo);      
-      it_new->CalculateRightHandSide(temp, rCurrentProcessInfo);
-      it_new->FinalizeSolutionStep(rCurrentProcessInfo);           
-      std::cout<< "************************************************" <<std::endl;
-    }*/ 
 
 }
 
@@ -749,12 +620,13 @@ void InterpolateInteralVariables(const int&  nel,
     {
       std::vector<Vector> values;
       father_elem->GetValueOnIntegrationPoints(INTERNAL_VARIABLES, values, rCurrentProcessInfo); 
-      /// WARNING =  Calculando la longitud ponderada de fractura del elemento. Solo valido para isotropic_damage 
+     /* /// WARNING =  Calculando la longitud ponderada de fractura del elemento. Solo valido para isotropic_damage 
       Element::GeometryType& geom_father = father_elem->GetGeometry();
       Element::GeometryType& geom_child  = child_elem->GetGeometry();
       double area_father = geom_father.Area();  
       double area_child  = geom_child.Area();
       values[0][4]       = (area_child/area_father) * values[0][4]; 
+      */
       child_elem->SetValueOnIntegrationPoints (INTERNAL_VARIABLES, values, rCurrentProcessInfo);
 
     }
