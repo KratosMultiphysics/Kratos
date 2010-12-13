050d8d7770985bce9d77400e0737a4c0ac39197b
janosch <janosch@4358b7d9-91ec-4505-bf62-c3060f61107a>
2010-12-13 08:24:30 +0000

    removed warning

--- kratos/applications/structural_application/custom_utilities/parallel_variable_transfer_utility.h
+++ kratos/applications/structural_application/custom_utilities/parallel_variable_transfer_utility.h
@@ -1892,7 +1892,7 @@ namespace Kratos
                 int partition_size = number_of_rows / number_of_threads;
                 partitions[0] = 0;
                 partitions[number_of_threads] = number_of_rows;
-                for(int i = 1; i<number_of_threads; i++)
+                for(unsigned int i = 1; i<number_of_threads; i++)
                     partitions[i] = partitions[i-1] + partition_size ;
             }
 
