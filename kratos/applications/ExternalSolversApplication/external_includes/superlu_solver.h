7b9c1a6313061bf92160dc8a3f26fd1915d5de32
janosch <janosch@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-01-05 13:31:39 +0000

    external solvers application new version

--- kratos/applications/ExternalSolversApplication/external_includes/superlu_solver.h
+++ kratos/applications/ExternalSolversApplication/external_includes/superlu_solver.h
@@ -45,37 +45,18 @@
 #if !defined(KRATOS_SUPERLU_SOLVER_H_INCLUDED )
 #define  KRATOS_SUPERLU_SOLVER_H_INCLUDED
 
-// #define BOOST_NUMERIC_BINDINGS_SUPERLU_PRINT
-
 // External includes 
 #include "boost/smart_ptr.hpp"
-// #include "utilities/superlu_interface.h"
+
 #include "includes/ublas_interface.h"
-//#include "external_includes/bindings/superlu/superlu.hpp"
-// #include "external_includes/bindings/traits/traits.hpp"
-// #include <boost/numeric/bindings/traits/ublas_sparse.hpp>
-// #include "boost/numeric/bindings/traits/ublas_matrix.hpp"
-// #include "structural_application/custom_utilities/ublas_matrix.hpp"
-// #include "boost/numeric/bindings/traits/ublas_vector2.hpp"
+// #include "external_includes/superlu/superlu.hpp"
 
-// #include <boost/numeric/bindings/traits/sparse_traits.hpp>
-// #include <boost/numeric/bindings/traits/ublas_matrix.hpp>
-// #include <boost/numeric/bindings/traits/ublas_sparse.hpp>
-// #include <boost/numeric/bindings/traits/ublas_vector.hpp>
-
-#include <boost/numeric/bindings/traits/sparse_traits.hpp>
-
-#include "external_includes/superlu/superlu.hpp"
-// #include <boost/numeric/bindings/superlu/superlu.hpp>
-#include <boost/numeric/bindings/traits/ublas_matrix.hpp>
-#include <boost/numeric/bindings/traits/ublas_sparse.hpp>
-#include <boost/numeric/bindings/traits/ublas_vector.hpp>
+#include "SRC/slu_ddefs.h"
 
 // Project includes
 #include "includes/define.h"
 #include "linear_solvers/direct_solver.h"
 
-namespace slu = boost::numeric::bindings::superlu;
 namespace ublas = boost::numeric::ublas;
 
 namespace Kratos
@@ -119,34 +100,116 @@ namespace Kratos
              */
             bool Solve(SparseMatrixType& rA, VectorType& rX, VectorType& rB)
             {
-				std::cout << "matrix size in solver: " << rA.size1() << std::endl;
-				std::cout << "RHS size in solver: " << rB.size() << std::endl;
-                typedef boost::numeric::bindings::traits::sparse_matrix_traits<SparseMatrixType> matraits;
-                typedef typename matraits::value_type val_t;
+		std::cout << "matrix size in solver: " << rA.size1() << std::endl;
+		std::cout << "RHS size in solver: " << rB.size() << std::endl;
                 
-                typedef ublas::compressed_matrix<double, ublas::row_major, 0,
-                ublas::unbounded_array<int>, ublas::unbounded_array<double> > cm_t;
-                typedef ublas::matrix<double, ublas::row_major> m_t;
-                
+//               typedef ublas::compressed_matrix<double, ublas::row_major, 0,
+//                 ublas::unbounded_array<int>, ublas::unbounded_array<double> > cm_t;
+               
                 if(this->IsNotConsistent(rA, rX, rB))
                     return false;
+                               
+                superlu_options_t options;
+		SuperLUStat_t stat;
+			    
+		/* Set the default input options:
+		    options.Fact = DOFACT;
+		    options.Equil = YES;
+		    options.ColPerm = COLAMD;
+		    options.DiagPivotThresh = 1.0;
+		    options.Trans = NOTRANS;
+		    options.IterRefine = NOREFINE;
+		    options.SymmetricMode = NO;
+		    options.PivotGrowth = NO;
+		    options.ConditionNumber = NO;
+		    options.PrintStat = YES;
+		*/
+		set_default_options(&options);
+		options.IterRefine = DOUBLE;
+// 		options.ColPerm = MMD_AT_PLUS_A;
                 
-                //manually create RHS matrix
-                m_t b( rB.size(), 1 );
-                for( unsigned int i=0; i<rB.size(); i++ )
-                {
-//                     if( ! (abs(rB[i])< 1.0e-15) )
-					b(i,0) = rB[i];
-//                     else b[i][0] = 0.0;
-                }
-                //call solver routine
-                slu::gssv (rA, b, slu::atpla_min_degree);
-                
+                //Fill the SuperLU matrices
+                SuperMatrix Aslu, B, L, U;
+      
+		//create a copy of the matrix
+		int *index1_vector = new (std::nothrow) int[rA.index1_data().size()];
+		int *index2_vector = new (std::nothrow) int[rA.index2_data().size()];
+// 		double *values_vector = new (std::nothrow) double[rA.value_data().size()];
+		
+		for( int unsigned i = 0; i < rA.index1_data().size(); i++ )
+		  index1_vector[i] = (int)rA.index1_data()[i];
+
+		for( unsigned int i = 0; i < rA.index2_data().size(); i++ )
+		    index2_vector[i] = (int)rA.index2_data()[i];
+
+/*		for( unsigned int i = 0; i < rA.value_data().size(); i++ )
+		    values_vector[i] = (double)rA.value_data()[i];*/
+		
+		//create a copy of the rhs vector (it will be overwritten with the solution)
+/*		double *b_vector = new (std::nothrow) double[rB.size()];
+		for( unsigned int i = 0; i < rB.size(); i++ )
+		    b_vector[i] = rB[i];*/
+/*
+		dCreate_CompCol_Matrix (&Aslu, rA.size1(), rA.size2(), 
+					       rA.nnz(),
+					      values_vector, 
+					      index2_vector,
+ 					      index1_vector,
+					      SLU_NR, SLU_D, SLU_GE
+					      );*/
+
+		//works also with dCreate_CompCol_Matrix
+		dCreate_CompRow_Matrix (&Aslu, rA.size1(), rA.size2(), 
+				rA.nnz(),
+			      rA.value_data().begin(), 
+			      index2_vector, //can not avoid a copy as ublas uses unsigned int internally
+			      index1_vector, //can not avoid a copy as ublas uses unsigned int internally
+			      SLU_NR, SLU_D, SLU_GE
+			      );
+				
+		dCreate_Dense_Matrix (&B, rB.size(), 1,&rB[0],rB.size(),SLU_DN, SLU_D, SLU_GE);   
+		
+		//allocate memory for permutation arrays
+		int* perm_c;
+		int* perm_r;
+		if ( !(perm_c = intMalloc(rA.size1())) ) ABORT("Malloc fails for perm_c[].");
+		if ( !(perm_r = intMalloc(rA.size2())) ) ABORT("Malloc fails for perm_r[].");
+		
+
+		//initialize container for statistical data
+		StatInit(&stat);
+
+		//call solver routine
+		int info;
+		dgssv(&options, &Aslu, perm_c, perm_r, &L, &U, &B, &stat, &info);
+
+		//print output
+		StatPrint(&stat);
+                    
                 //resubstitution of results
+		#pragma omp parallel for
                 for( unsigned int i=0; i<rB.size(); i++ )
-                {
-					rX[i] = b(i,0);
-                }
+                	rX[i] = rB[i]; // B(i,0);
+                
+		//deallocate memory used
+		StatFree(&stat);
+		SUPERLU_FREE (perm_r);
+		SUPERLU_FREE (perm_c);
+		Destroy_SuperMatrix_Store(&Aslu); //note that by using the "store" function we will take care of deallocation ourselves
+		Destroy_SuperMatrix_Store(&B);
+		Destroy_SuperNode_Matrix(&L);
+		Destroy_CompCol_Matrix(&U);
+		
+		delete [] index1_vector;
+		delete [] index2_vector;
+// 		delete [] b_vector;
+
+		//CHECK WITH VALGRIND IF THIS IS NEEDED ...or if it is done by the lines above
+                //deallocate tempory storage used for the matrix
+//                 if(b_vector!=NULL) delete [] index1_vector;
+// //   		if(b_vector!=NULL) delete [] index2_vector;
+//   		if(b_vector!=NULL) delete [] values_vector;
+// 		if(b_vector!=NULL) delete [] b_vector;
                 
                 return true;
             }
