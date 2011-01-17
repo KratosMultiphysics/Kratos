5fd7be35766062c2771693184c9c51fa95af16e3
antonia <antonia@cimne.upc.edu>
2011-01-17 14:28:16 +0000

    removing benchmark test

--- applications/PFEMapplication/test_exemples/Coupled_slope2dNonNewt_Y3000.gid/CoupledNonNewtSlope_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for CoupledNonNewtSlope.py..."
-benchmarking.BuildReferenceData("CoupledNonNewtSlope.py", "CoupledNonNewtSlope_ref.txt")
