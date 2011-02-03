42336b5bb16106f5a9ec523af4612a0ddfd3d2aa
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-02-03 17:08:03 +0000

    committing a new test example for CFD on GPU

---
+++ kratos/applications/OpenCLapplication/test_examples/cavity3d_edgebased.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 3
+
+Dt = 0.01
+max_time = 1.0
+SolverType = "fractional_step"
+laplacian_form = 1
+output_step = 1
+predictor_corrector = False
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 0.001
+# Declare Python Variables
+
+problem_name="cavity3d_edgebased"
+problem_path="/home/rrossi/esempi/cavity3d_edgebased.gid"
+kratos_path="/home/rrossi/kratos"
