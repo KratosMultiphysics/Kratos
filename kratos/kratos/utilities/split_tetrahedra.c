01b04cd8390a7528a587316c605975b2fd7a4310
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-02-18 07:30:08 +0000

    minor improvements - working towards clean c compilation

--- kratos/kratos/utilities/split_tetrahedra.c
+++ kratos/kratos/utilities/split_tetrahedra.c
@@ -1,21 +1,24 @@
 #if !defined(KRATOS_SPLIT_TETRAHEDRA)
 #define  KRATOS_SPLIT_TETRAHEDRA
 
-// Copyright (C) 2010 Riccardo Rossi, Pooyan Dadvand, Nelson Maireni
-// Email contact: rrossi@cimne.upc.edu
-// The current tetrahedra splitting library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+/*VERSION 1.0 17 Feb 2011*/
 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+/* Copyright (C) 2010 Riccardo Rossi, Pooyan Dadvand, Nelson Maireni
+ Email contact: rrossi@cimne.upc.edu
+ The current tetrahedra splitting library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 /** @file split_tetrahedra.c
  * @brief this class performs the splitting of a tetrahedra.\n
