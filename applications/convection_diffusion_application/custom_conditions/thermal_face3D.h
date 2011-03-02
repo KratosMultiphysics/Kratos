3c305e2d2ca79e9beae14a407d4c9d9fdabcd2a6
paolo <paolo@cimne.upc.edu>
2011-03-02 13:10:08 +0000

    serialization

--- applications/convection_diffusion_application/custom_conditions/thermal_face3D.h
+++ applications/convection_diffusion_application/custom_conditions/thermal_face3D.h
@@ -233,7 +233,7 @@ namespace Kratos
 	{
 	}
 	
-        virtual void save(Serializer& rSerializer)
+        virtual void save(Serializer& rSerializer) const
 	{
 	rSerializer.save("Name", "ThermalFace3D");
 	KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition);
