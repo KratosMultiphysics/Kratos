ceff1dbac1cebfef1d576db4b6035fef11fa2cb9
paolo <paolo@cimne.upc.edu>
2011-03-02 13:10:16 +0000

    serialization

--- applications/convection_diffusion_application/custom_elements/conv_diff_3d.h
+++ applications/convection_diffusion_application/custom_elements/conv_diff_3d.h
@@ -230,7 +230,7 @@ void GetDofList(DofsVectorType& ElementalDofList,ProcessInfo& CurrentProcessInfo
 	{
 	}
 	
-        virtual void save(Serializer& rSerializer)
+        virtual void save(Serializer& rSerializer) const
 	{
 	rSerializer.save("Name", "ConvDiff3D");
 	KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Element);
