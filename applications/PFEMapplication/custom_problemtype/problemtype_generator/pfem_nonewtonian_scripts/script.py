666e85691ba2a73ff02655bb00b373fcd8390efa
antonia <antonia@cimne.upc.edu>
2011-03-25 12:35:55 +0000

    PT cleaning

--- applications/PFEMapplication/custom_problemtype/problemtype_generator/pfem_nonewtonian_scripts/script.py
+++ applications/PFEMapplication/custom_problemtype/problemtype_generator/pfem_nonewtonian_scripts/script.py
@@ -29,7 +29,7 @@ applications_interface.Import_MeshingApplication = True
 applications_interface.ImportApplications(kernel, kratos_applications_path)
 from KratosStructuralApplication import *
 from KratosIncompressibleFluidApplication import *
-## from now on the order is not anymore crucial
+# from now on the order is not anymore crucial
 ##################################################################
 ##################################################################
 
@@ -37,17 +37,8 @@ from KratosIncompressibleFluidApplication import *
 model_part = ModelPart("FluidPart");  
 
 #importing the solver files and adding the variables
-SolverType = pfem_nonewtonian_var.SolverType
-
-if(SolverType == "pfem_solver_ale"):
-    import pfem_solver_ale
-    pfem_solver_ale.AddVariables(model_part)
-elif(SolverType == "monolithic_solver_lagrangian"):
-    import monolithic_solver_lagrangian_nonnewtonian
-    monolithic_solver_lagrangian_nonnewtonian.AddVariables(model_part)
-else:
-    raise "solver type not supported: options are FractionalStep - Monolithic"
-
+import monolithic_solver_lagrangian_nonnewtonian
+monolithic_solver_lagrangian_nonnewtonian.AddVariables(model_part)
 
 #reading a model
 name = pfem_nonewtonian_var.problem_name
@@ -60,22 +51,17 @@ gid_io = GidIO(name,gid_mode,multifile,deformed_mesh_flag, write_conditions)
 model_part_io = ModelPartIO(name)
 model_part_io.ReadModelPart(model_part)
 
-##check to ensure that no node has zero density or pressure
+##assigning density viscosity and gravity acceleration
 for node in model_part.Nodes:
     node.SetSolutionStepValue(DENSITY,0,pfem_nonewtonian_var.Density)
     node.SetSolutionStepValue(VISCOSITY,0,pfem_nonewtonian_var.Viscosity) 
     node.SetSolutionStepValue(BODY_FORCE_X,0,pfem_nonewtonian_var.Gravity_X) 
     node.SetSolutionStepValue(BODY_FORCE_Y,0,pfem_nonewtonian_var.Gravity_Y) 
     node.SetSolutionStepValue(BODY_FORCE_Z,0,pfem_nonewtonian_var.Gravity_Z)
-    node.SetSolutionStepValue(YIELD_STRESS,0,pfem_nonewtonian_var.YieldStress)
-##    if(node.GetSolutionStepValue(DENSITY) == 0.0):
-##        print "node ",node.Id," has zero density!"
-##        raise 'node with zero density found'
-##    if(node.GetSolutionStepValue(VISCOSITY) == 0.0):
-##        print "node ",node.Id," has zero viscosity!"
-##        raise 'node with zero density found'
-
-
+    node.SetSolutionStepValue(INTERNAL_FRICTION_ANGLE,0,pfem_nonewtonian_var.Friction_Angle_Tangent)
+    node.SetSolutionStepValue(YIELD_STRESS,0,pfem_nonewtonian_var.Yield_Stress)
+    
+#initializing the mesh
 mesh_name = 0.0
 gid_io.InitializeMesh( mesh_name );
 gid_io.WriteMesh((model_part).GetMesh());
@@ -99,42 +85,29 @@ output_Dt = pfem_nonewtonian_var.output_Dt
 max_dt = pfem_nonewtonian_var.max_dt
 min_dt = pfem_nonewtonian_var.min_dt
 safety_factor = pfem_nonewtonian_var.safety_factor
-nsteps = pfem_nonewtonian_var.nsteps
+final_time = pfem_nonewtonian_var.max_time
 
 #the buffer size should be set up here after the mesh is read for the first time
 model_part.SetBufferSize(2)
 
-if(SolverType == "pfem_solver_ale"):
-    #adding dofs
-    pfem_solver_ale.AddDofs(model_part)
-    #creating a fluid solver object
-    name = str("dam2d")
-    solver = pfem_solver_ale.PFEMSolver(model_part,name,box_corner1,box_corner2,domain_size)
-    solver.laplacian_form = pfem_nonewtonian_var.laplacian_form
-    solver.echo_level = 0
-    solver.prediction_order = 1
-    solver.predictor_corrector = True
-    solver.smooth = True
-    solver.alpha_shape = 1.2
-    solver.max_press_its = 3;  
-    #initializing the solver
-    initial_dt = 0.001*min_dt
-    solver.Initialize(initial_dt,output_Dt)
-elif(SolverType == "monolithic_solver_lagrangian"):
-    #adding dofs
-    monolithic_solver_lagrangian_nonnewtonian.AddDofs(model_part)
-    solver = monolithic_solver_lagrangian_nonnewtonian.MonolithicSolver(model_part,domain_size,box_corner1,box_corner2)
-    oss_swith = pfem_nonewtonian_var.use_oss
-    dynamic_tau = pfem_nonewtonian_var.dynamic_tau
-    solver.echo_level = 2
-    model_part.ProcessInfo.SetValue(OSS_SWITCH, oss_swith);				
-    model_part.ProcessInfo.SetValue(DYNAMIC_TAU, dynamic_tau);
-    solver.Initialize(output_Dt)
 
+#adding dofs
+monolithic_solver_lagrangian_nonnewtonian.AddDofs(model_part)
+#defining the solver
+solver = monolithic_solver_lagrangian_nonnewtonian.MonolithicSolver(model_part,domain_size,box_corner1,box_corner2)
+#setting solver parameters
+solver.oss_switch = int(pfem_nonewtonian_var.use_oss) #OSS = 1; ASGS = 0;
+solver.dynamic_tau = pfem_nonewtonian_var.dynamic_tau 
+solver.regularization_coef = pfem_nonewtonian_var.m_coef #m regularization coefficient in the exponential law of viscosity
+solver.echo_level = 2
+#Initializing the solver
+solver.Initialize(output_Dt)
 
+step = 0
 time = 0.0
-for step in range(0,nsteps):
-    print "line49"
+##for step in range(0,nsteps):
+while(time < final_time):
+##    print "line49"
 
     new_Dt = solver.EstimateDeltaTime(min_dt,max_dt)
 
@@ -148,6 +121,7 @@ for step in range(0,nsteps):
     if(step > 3):
         solver.Solve(time,gid_io)
 
+    step = step + 1
 
           
         
