7c940d4b82f035497c5bfe3fe17d8afc4aeae694
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-07-26 16:19:57 +0000

    adding the trilinos communicator

---
+++ kratos/applications/trilinos_application/custom_python/add_trilinos_communicator_to_python.cpp
@@ -0,0 +1,76 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+ 
+//   
+//   Project Name:        Kratos       
+//   Last modified by:    $Author: janosch $
+//   Date:                $Date: 2008-04-28 16:19:49 $
+//   Revision:            $Revision: 1.2 $
+//
+//
+
+
+// System includes 
+
+// External includes 
+#include <boost/python.hpp>
+#include "includes/model_part.h"
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/mpi_communicator.h"
+
+
+namespace Kratos
+{
+    namespace Python
+    {
+        using namespace boost::python;
+        
+        void  AddTrilinosCommunicatorToPython()
+        {
+            class_<MPICommunicator, bases<Communicator> >
+                    ("MPICommunicator",
+                     init<>() )
+                    ;
+        }	
+    }  // namespace Python.
+
+} // namespace Kratos
+
