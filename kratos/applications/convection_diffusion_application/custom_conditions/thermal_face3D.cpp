2b4e294d4c970ec21e029802bcb3d82c3c981b43
jmarti <jmarti@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-09-06 17:17:43 +0000

    CONVECTION_COEFFICIENT is defined in the nodes

--- kratos/applications/convection_diffusion_application/custom_conditions/thermal_face3D.cpp
+++ kratos/applications/convection_diffusion_application/custom_conditions/thermal_face3D.cpp
@@ -139,6 +139,11 @@ namespace Kratos
 		const double& q1 = GetGeometry()[1].FastGetSolutionStepValue(FACE_HEAT_FLUX);
 		const double& q2 = GetGeometry()[2].FastGetSolutionStepValue(FACE_HEAT_FLUX);
 
+		const double C0 = GetGeometry()[0].FastGetSolutionStepValue(CONVECTION_COEFFICIENT);
+		const double C1 = GetGeometry()[1].FastGetSolutionStepValue(CONVECTION_COEFFICIENT);
+		const double C2 = GetGeometry()[2].FastGetSolutionStepValue(CONVECTION_COEFFICIENT);
+		double C=0.333333333333*(C0+C1+C2);
+
 		if (CalculateStiffnessMatrixFlag == true) //calculation of the matrix is required
 		{
 			if(rLeftHandSideMatrix.size1() != MatSize )
