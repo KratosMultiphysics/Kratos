004667a6881044d3123355100f84969c0d2d1047
janosch <janosch@cimne.upc.edu>
2011-09-08 17:38:11 +0000

    application for third-party constitutive models

---
+++ applications/constitutive_laws_application/test_examples/beam_3d.gid/beam_3d.py
@@ -0,0 +1,45 @@
+##################################################################
+##### ekate - Enhanced KRATOS for Advanced Tunnel Enineering #####
+##### copyright by CIMNE, Barcelona, Spain                   #####
+#####          and Janosch Stascheit for TUNCONSTRUCT        #####
+##### all rights reserved                                    #####
+##################################################################
+#setting the domain size for the problem to be solved
+domain_size = 3
+##################################################################
+##################################################################
+## ATTENTION: here the order is important                    #####
+##################################################################
+## including kratos path                                     #####
+## ATTENTION: the following lines have to be adapted to      #####
+##            match your acrtual configuration               #####
+##################################################################
+kratos_root_path = '../../../../'
+##setting up paths
+kratos_libs_path = kratos_root_path+'/libs' ##kratos_root/libs
+kratos_applications_path = kratos_root_path+'/applications' ##kratos_root/applications
+##################################################################
+##################################################################
+import sys
+sys.path.append(kratos_libs_path)
+sys.path.append(kratos_applications_path)
+
+##################################################################
+##################################################################
+sys.path.append('./beam_3d.gid')
+import beam_3d_include
+from beam_3d_include import *
+# calculate insitu-stress for geology_virgin.gid
+model = beam_3d_include.Model('beam_3d','./')
+linear_elastic = False
+model.InitializeModel( linear_elastic )
+
+##################################################################
+###  SIMULATION  #################################################
+##################################################################
+time = 0.0
+model.SetCalculateInSituStress( False )
+model.Solve( time, 0, 0, 0, 0 )
+model.WriteOutput( time )
+print "Calculation done"
+sys.exit(0)
