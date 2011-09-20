2e0faa7ba0426222de1431e75b094c6001141066
paolo <paolo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-09-20 14:21:53 +0000

    A clean FSI benchmark example

---
+++ kratos/applications/ULFapplication/test_exemples/dam_2d_FSI.gid/fluid_ulf_var.py
@@ -0,0 +1,26 @@
+domain_size = 2
+
+FSI = 1.00000e+00
+Dt = 1.00000e-02
+max_time = 1.00000e+00
+output_step = 1.00000e-02
+alpha_shape = 1.60000e+00
+erase_nodes = 1.00000e+00
+adaptive_refinement = 0.00000e+00
+delete_nodes_close_to_wall = 1.00000e+00
+lagrangian_nodes_inlet = 0.00000e+00
+bulk_modulus = -1.00000e+03
+density = 1.00000e+03
+bounding_box_corner1_x = -1.00000e+00
+bounding_box_corner1_y = -1.00000e+00
+bounding_box_corner1_z = -1.00000e+00
+bounding_box_corner2_x = 1.00000e+00
+bounding_box_corner2_y = 1.00000e+00
+bounding_box_corner2_z = 1.00000e+00
+SolverType = "Quasi_Inc_Linear_Pressure"
+# Declare Python Variables
+
+problem_name = 'dam_2d_FSI'
+problem_path = '/home/paolo/Kratos_Newest/applications/ULFapplication/test_exemples/dam_2d_FSI.gid'
+kratos_path = '/home/paolo/Kratos_Newest'
+
