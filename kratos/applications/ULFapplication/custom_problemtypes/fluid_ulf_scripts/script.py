681f144d2ccd53ab020c96263c6e79251a0f7358
paolo <paolo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-09-20 16:04:27 +0000

    Adjusting to the new "general" ULF problemtype

--- kratos/applications/ULFapplication/custom_problemtypes/fluid_ulf_scripts/script.py
+++ kratos/applications/ULFapplication/custom_problemtypes/fluid_ulf_scripts/script.py
@@ -81,15 +81,16 @@ model_part_io_origin.ReadModelPart(fluid_model_part)
 #setting up the buffer size: SHOULD BE DONE AFTER READING!!!
 fluid_model_part.SetBufferSize(3)
 
+compute_reactions=fluid_ulf_var.compute_reactions
 ##adding dofs
 if(SolverType == "Incompressible_Modified_FracStep"):
-    ulf_frac.AddDofs(fluid_model_part)
+    ulf_frac.AddDofs(fluid_model_part, compute_reactions)
 elif(SolverType == "FracStep"):
-    ulf_frac.AddDofs(fluid_model_part)
+    ulf_frac.AddDofs(fluid_model_part, compute_reactions)
 elif(SolverType == "Quasi_Inc_Constant_Pressure"):
-    ulf_fsi.AddDofs(fluid_model_part)
+    ulf_fsi.AddDofs(fluid_model_part, compute_reactions)
 elif(SolverType == "Quasi_Inc_Linear_Pressure"):
-    ulf_fsi_inc.AddDofs(fluid_model_part)
+    ulf_fsi_inc.AddDofs(fluid_model_part, compute_reactions)
 
 
 if(SolverType == "Quasi_Inc_Constant_Pressure" or SolverType == "Quasi_Inc_Linear_Pressure"):
@@ -122,7 +123,7 @@ density=fluid_ulf_var.density
 #creating the solvers
 #fluid solver
 if(SolverType == "Incompressible_Modified_FracStep"):    
-    solver = ulf_frac.ULF_FSISolver(outputfile1, fluid_only_model_part, fluid_model_part, structure_model_part, combined_model_part, box_corner1, box_corner2, domain_size, add_nodes, bulk_modulus, density)
+    solver = ulf_frac.ULF_FSISolver(outputfile1, fluid_only_model_part, fluid_model_part, structure_model_part, combined_model_part, compute_reactions, box_corner1, box_corner2, domain_size, add_nodes, bulk_modulus, density)
     solver.alpha_shape = fluid_ulf_var.alpha_shape;
     solver.echo_level = 2;
     
@@ -132,7 +133,7 @@ if(SolverType == "Incompressible_Modified_FracStep"):
     solver.Initialize()
     
 if(SolverType == "FracStep"):    
-    solver = ulf_frac.ULF_FSISolver(outputfile1, fluid_only_model_part, fluid_model_part, structure_model_part, combined_model_part, box_corner1, box_corner2, domain_size, add_nodes, bulk_modulus, density)
+    solver = ulf_frac.ULF_FSISolver(outputfile1, fluid_only_model_part, fluid_model_part, structure_model_part, combined_model_part, compute_reactions, box_corner1, box_corner2, domain_size, add_nodes, bulk_modulus, density)
     solver.alpha_shape = fluid_ulf_var.alpha_shape;
     solver.echo_level = 2;
     for node in fluid_model_part.Nodes:
@@ -141,7 +142,7 @@ if(SolverType == "FracStep"):
     solver.Initialize()
     
 elif(SolverType == "Quasi_Inc_Constant_Pressure"):
-    solver = ulf_fsi.ULF_FSISolver(fluid_model_part, structure_model_part, combined_model_part, box_corner1, box_corner2, domain_size, add_nodes)
+    solver = ulf_fsi.ULF_FSISolver(fluid_model_part, structure_model_part, combined_model_part, compute_reactions, box_corner1, box_corner2, domain_size, add_nodes)
     solver.alpha_shape = fluid_ulf_var.alpha_shape;
     solver.echo_level = 2;
    
@@ -152,7 +153,7 @@ elif(SolverType == "Quasi_Inc_Constant_Pressure"):
     solver.Initialize()
     
 elif(SolverType == "Quasi_Inc_Linear_Pressure"): 
-    solver = ulf_fsi_inc.ULF_FSISolver(outputfile1, fluid_model_part, structure_model_part, combined_model_part, box_corner1, box_corner2, domain_size, add_nodes, bulk_modulus, density)
+    solver = ulf_fsi_inc.ULF_FSISolver(outputfile1, fluid_model_part, structure_model_part, combined_model_part, compute_reactions, box_corner1, box_corner2, domain_size, add_nodes, bulk_modulus, density)
     solver.alpha_shape = fluid_ulf_var.alpha_shape;
     solver.echo_level = 2;
         
@@ -224,12 +225,11 @@ while (time < final_time):
 
             gid_io.InitializeResults(time, (combined_model_part).GetMesh());
     
-            gid_io.WriteNodalResults(DISPLACEMENT, combined_model_part.Nodes, time, 0);
-            gid_io.WriteNodalResults(IS_STRUCTURE, combined_model_part.Nodes, time, 0);           
-            gid_io.WriteNodalResults(IS_FREE_SURFACE, combined_model_part.Nodes, time, 0);
-            gid_io.WriteNodalResults(IS_INTERFACE, combined_model_part.Nodes, time, 0);            
+            gid_io.WriteNodalResults(DISPLACEMENT, combined_model_part.Nodes, time, 0);            
             gid_io.WriteNodalResults(VELOCITY, combined_model_part.Nodes, time, 0);
             gid_io.WriteNodalResults(PRESSURE, (combined_model_part).Nodes, time, 0);
+            if (compute_reactions==1):
+		gid_io.WriteNodalResults(REACTION, (combined_model_part).Nodes, time, 0);
             
             
 
