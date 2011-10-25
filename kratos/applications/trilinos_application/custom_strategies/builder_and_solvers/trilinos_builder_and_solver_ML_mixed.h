f7e36f2704ab7180638fcde987401ea751284cd1
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2011-10-25 10:03:22 +0000

    some improvements

--- kratos/applications/trilinos_application/custom_strategies/builder_and_solvers/trilinos_builder_and_solver_ML_mixed.h
+++ kratos/applications/trilinos_application/custom_strategies/builder_and_solvers/trilinos_builder_and_solver_ML_mixed.h
@@ -395,7 +395,7 @@ namespace Kratos
  
                 solver.SetAztecOption(AZ_solver, AZ_gmres_condnum);
                 solver.SetAztecOption(AZ_output, 32);
-                Solver.SetAztecOption(AZ_kspace, 100);
+                solver.SetAztecOption(AZ_kspace, 100);
                 solver.Iterate(500, 1e-8);
 
                 // delete the preconditioner. Do it BEFORE MPI_Finalize
