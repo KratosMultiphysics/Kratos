9c63d42ca93b52c01af856fc88c9835d304f52b8
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-01-05 14:18:52 +0000

    Add thermomechanical application

---
+++ kratos/applications/ThermoMechanicalApplication/test_examples/duplicate_test.gid/duplicate_test_0.post.res
@@ -0,0 +1,1 @@
+GiD Post Results File 1.0
---
+++ kratos/applications/ThermoMechanicalApplication/test_examples/duplicate_test.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 2
+
+Dt = 0.1
+max_time = 0.1
+SolverType = "monolithic_solver_eulerian"
+laplacian_form = 3
+output_step = 1
+predictor_corrector = True
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 1
+# Declare Python Variables
+
+problem_name="duplicate_test"
+problem_path="/home/kazem/kratos/applications/ThermoMechanicalApplication/test_examples/duplicate_test.gid"
+kratos_path="/home/kazem/kratos"
