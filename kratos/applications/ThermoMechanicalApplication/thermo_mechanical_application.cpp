8d6e5fafed72a740a52c901a88e017c5e78fd4b2
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-01-05 14:16:29 +0000

    Add thermomechanical application

---
+++ kratos/applications/ThermoMechanicalApplication/thermo_mechanical_application.cpp
@@ -0,0 +1,75 @@
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author:  $
+//   Date:                $Date:  $
+//   Revision:            $Revision: 1.3 $
+//
+// 
+
+
+
+// System includes
+
+
+// External includes 
+
+
+// Project includes
+#include "includes/define.h"
+#include "geometries/triangle_2d_3.h"
+#include "geometries/triangle_3d_3.h"
+#include "geometries/tetrahedra_3d_4.h"
+#include "geometries/line_2d.h"
+#include "thermo_mechanical_application.h"
+#include "includes/variables.h"
+
+
+namespace Kratos
+{
+	//Example
+// 	KRATOS_CREATE_VARIABLE(double, AUX_MESH_VAR)
+//	KRATOS_CREATE_VARIABLE(double, IS_INTERFACE);
+//	KRATOS_CREATE_VARIABLE(double, NODAL_AREA);
+//
+	KRATOS_CREATE_VARIABLE(int, NODE_PROPERTY_ID)
+
+	KratosThermoMechanicalApplication::KratosThermoMechanicalApplication():
+// 		mElem2D(0, Element::GeometryType::Pointer(new Triangle2D3<Node<3> >(Element::GeometryType::PointsArrayType(3, Node<3>())))),
+/*		mMonolithic2DNeumann(0, Element::GeometryType::Pointer(new Line2D2<Node<3> >(Element::GeometryType::PointsArrayType(2, Node<3>())))),*/
+		mHeatContact2D(0, Element::GeometryType::Pointer(new Line2D2<Node<3> >(Element::GeometryType::PointsArrayType(2, Node<3>())))),
+		mHeatContact3D(0, Element::GeometryType::Pointer(new Line3D2<Node<3> >(Element::GeometryType::PointsArrayType(2, Node<3>())))),
+		
+		mSUPGConvDiff2D(0, Element::GeometryType::Pointer(new Triangle2D3<Node<3> >(Element::GeometryType::PointsArrayType(3, Node<3>())))),
+		mSUPGConvDiff3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element::GeometryType::PointsArrayType(4, Node<3>())))),
+		mSUPGConv3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element::GeometryType::PointsArrayType(4, Node<3>()))))
+		
+		
+		
+	{}
+// 	
+	void KratosThermoMechanicalApplication::Register()
+	{
+		// calling base class register to register Kratos components
+		KratosApplication::Register();
+		std::cout << "Initializing KratosThermoMechanicalApplication... " << std::endl;
+
+		KRATOS_REGISTER_VARIABLE(NODE_PROPERTY_ID)		
+		
+// 		KRATOS_REGISTER_VARIABLE( AUX_MESH_VAR )
+// 		KRATOS_REGISTER_VARIABLE(IS_INTERFACE);
+// 		KRATOS_REGISTER_VARIABLE(NODAL_AREA);
+// 	
+// 		KRATOS_REGISTER_ELEMENT("Elem2D", mElem2D);
+// 		KRATOS_REGISTER_ELEMENT("Elemt3D", mElem3D);
+		KRATOS_REGISTER_CONDITION("HeatContact2D", mHeatContact2D);
+		KRATOS_REGISTER_CONDITION("HeatContact3D", mHeatContact3D);
+		
+		KRATOS_REGISTER_ELEMENT("SUPGConvDiff2D", mSUPGConvDiff2D);				
+		KRATOS_REGISTER_ELEMENT("SUPGConvDiff3D", mSUPGConvDiff3D);
+		KRATOS_REGISTER_ELEMENT("SUPGConv3D", mSUPGConv3D);
+		
+	}
+
+}  // namespace Kratos.
+
+
