0b9dd601caf3309ae8996303b57aa0218efcc345
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-01-06 15:07:40 +0000

    thermal_face_2d

---
+++ kratos/applications/ThermoMechanicalApplication/custom_elements/thermal_face2d.h
@@ -0,0 +1,311 @@
+/*
+==============================================================================
+KratosConvectionDiffusionApplication 
+A library based on:
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu 
+rrossi@cimne.upc.edu
+- CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/ 
+//   
+//   Project Name:        Kratos       
+//   Last Modified by:    $Author: rrossi $
+//   Date:                $Date: 2007-03-06 10:30:32 $
+//   Revision:            $Revision: 1.2 $
+//
+//
+
+ 
+#if !defined(KRATOS_THERMAL_FACE2D_CONDITION_H_INCLUDED )
+#define  KRATOS_THERMAL_FACE2D_CONDITION_H_INCLUDED 
+
+
+
+// System includes 
+
+
+// External includes 
+#include "boost/smart_ptr.hpp"
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/condition.h"
+#include "includes/ublas_interface.h"
+#include "includes/variables.h"
+
+#include "includes/serializer.h"
+
+namespace Kratos
+{
+
+  ///@name Kratos Globals
+  ///@{ 
+  
+  ///@} 
+  ///@name Type Definitions
+  ///@{ 
+  
+  ///@} 
+  ///@name  Enum's
+  ///@{
+      
+  ///@}
+  ///@name  Functions 
+  ///@{
+      
+  ///@}
+  ///@name Kratos Classes
+  ///@{
+  
+  /// Evaluates the term  \f$ \overline{q_n} = q_n - \varepsilon \sigma (T^4 - T_0^4) - \alpha_c (T - T_0)\f$ .
+  /** Detail class definition.
+  */
+  class ThermalFace2D
+	  : public Condition
+    {
+    public:
+      ///@name Type Definitions
+      ///@{
+      
+      /// Counted pointer of ThermalFace2D
+      KRATOS_CLASS_POINTER_DEFINITION(ThermalFace2D);
+ 
+      ///@}
+      ///@name Life Cycle 
+      ///@{ 
+      
+      /// Default constructor.
+	  ThermalFace2D(IndexType NewId, GeometryType::Pointer pGeometry);
+      ThermalFace2D(IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties);
+
+      /// Destructor.
+      virtual ~ThermalFace2D();
+      
+
+      ///@}
+      ///@name Operators 
+      ///@{
+      
+      
+      ///@}
+      ///@name Operations
+      ///@{
+
+      Condition::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes,  PropertiesType::Pointer pProperties) const;
+
+      void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+      
+      void CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+      //virtual void CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix, ProcessInfo& rCurrentProcessInfo);
+      
+      void EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& rCurrentProcessInfo);
+
+	  void GetDofList(DofsVectorType& ConditionalDofList,ProcessInfo& CurrentProcessInfo);
+
+      ///@}
+      ///@name Access
+      ///@{ 
+      
+      
+      ///@}
+      ///@name Inquiry
+      ///@{
+      
+      
+      ///@}      
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+//      virtual String Info() const;
+      
+      /// Print information about this object.
+//      virtual void PrintInfo(std::ostream& rOStream) const;
+
+      /// Print object's data.
+//      virtual void PrintData(std::ostream& rOStream) const;
+      
+            
+      ///@}      
+      ///@name Friends
+      ///@{
+
+            
+      ///@}
+      
+    protected:
+      ///@name Protected static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected  Access 
+      ///@{ 
+        
+        
+      ///@}      
+      ///@name Protected Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Protected LifeCycle 
+      ///@{ 
+      
+            
+      ///@}
+      
+    private:
+      ///@name Static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Member Variables 
+      ///@{ 
+		
+        
+        
+      ///@} 
+      ///@name Private Operators
+      ///@{ 
+
+///Evaluates  \f$ L h s =  4 \epsilon \sigma T^3 \left\langle W, N \right\rangle + \alpha \left\langle W, N \right\rangle \f$ and \f$ R h s = \left\langle W, q \right\rangle - \epsilon \sigma \left\langle W, T^4 - T_0^4 \right\rangle - \left\langle W, \alpha (T - T_0) \right\rangle \f$
+		void CalculateAll(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, 
+										ProcessInfo& rCurrentProcessInfo,
+										bool CalculateStiffnessMatrixFlag,
+										bool CalculateResidualVectorFlag);
+        
+      
+
+	///@}
+        ///@name Serialization
+        ///@{	
+	friend class Serializer;
+
+        // A private default constructor necessary for serialization
+        ThermalFace2D() : Condition()
+	{
+	}
+	
+        virtual void save(Serializer& rSerializer) const
+	{
+	rSerializer.save("Name", "ThermalFace2D");
+	KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition);
+	}
+	
+	virtual void load(Serializer& rSerializer)
+	{
+	KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Condition);
+	}
+
+
+      ///@} 
+      ///@name Private Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private  Access 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Private Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Un accessible methods 
+      ///@{ 
+      
+      /// Assignment operator.
+      //ThermalFace2D& operator=(const ThermalFace2D& rOther);
+
+      /// Copy constructor.
+      //ThermalFace2D(const ThermalFace2D& rOther);
+
+        
+      ///@}    
+        
+    }; // Class ThermalFace2D 
+
+  ///@} 
+  
+  ///@name Type Definitions       
+  ///@{ 
+  
+  
+  ///@} 
+  ///@name Input and output 
+  ///@{ 
+        
+ 
+  /// input stream function
+/*  inline std::istream& operator >> (std::istream& rIStream, 
+				    ThermalFace2D& rThis);
+*/
+  /// output stream function
+/*  inline std::ostream& operator << (std::ostream& rOStream, 
+				    const ThermalFace2D& rThis)
+    {
+      rThis.PrintInfo(rOStream);
+      rOStream << std::endl;
+      rThis.PrintData(rOStream);
+
+      return rOStream;
+    }*/
+  ///@} 
+
+}  // namespace Kratos.
+
+#endif // KRATOS_THERMAL_FACE2D_CONDITION_H_INCLUDED   defined 
+
+
