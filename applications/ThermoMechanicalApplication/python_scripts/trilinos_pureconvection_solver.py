efaa4fb8c8c6f5659ecdf54a9d3caf5c9dc30d00
rrossi <rrossi@cimne.upc.edu>
2012-01-06 15:56:36 +0000

    some corrections and changing default solvers for trilinos

--- applications/ThermoMechanicalApplication/python_scripts/trilinos_pureconvection_solver.py
+++ applications/ThermoMechanicalApplication/python_scripts/trilinos_pureconvection_solver.py
@@ -43,16 +43,28 @@ class Solver:
 	self.buildertype="standard"
 
         #definition of the solvers
+#        aztec_parameters = ParameterList()
+#        aztec_parameters.set("AZ_solver","AZ_gmres");
+#        aztec_parameters.set("AZ_kspace",100);
+#        aztec_parameters.set("AZ_output","AZ_none");
+#        preconditioner_type = "ILU"
+#        preconditioner_parameters = ParameterList()
+#        overlap_level = 0
+#        nit_max = 1000
+#        linear_tol = 1e-9
+#        self.linear_solver =  AztecSolver(aztec_parameters,preconditioner_type,preconditioner_parameters,linear_tol,nit_max,overlap_level);
+
         aztec_parameters = ParameterList()
         aztec_parameters.set("AZ_solver","AZ_gmres");
-        aztec_parameters.set("AZ_kspace",100);
         aztec_parameters.set("AZ_output","AZ_none");
-        preconditioner_type = "ILU"
-        preconditioner_parameters = ParameterList()
-        overlap_level = 0
+#        aztec_parameters.set("AZ_output",10);
+        MLList = ParameterList()
+        default_settings = EpetraDefaultSetter()
+        default_settings.SetDefaults(MLList,"NSSA");
+#        MLList.set("ML output", "ML_none");
         nit_max = 1000
         linear_tol = 1e-9
-        self.linear_solver =  AztecSolver(aztec_parameters,preconditioner_type,preconditioner_parameters,linear_tol,nit_max,overlap_level);
+        self.linear_solver =  MultiLevelSolver(aztec_parameters,MLList,linear_tol,nit_max);
         
         #definition of the convergence criteria
         self.conv_criteria = TrilinosDisplacementCriteria(1e-6,1e-9,self.Comm)
@@ -61,7 +73,7 @@ class Solver:
         self.ReformDofSetAtEachStep = False
         self.MoveMeshFlag = False
         self.calculate_norm_dx_flag = False
-        self.max_iterations = 2
+        self.max_iterations = 1
 
         if(domain_size == 2):
             self.guess_row_size = 20
@@ -82,6 +94,7 @@ class Solver:
         import trilinos_strategy_python
         self.solver = trilinos_strategy_python.SolvingStrategyPython(self.buildertype,self.model_part,self.time_scheme,self.linear_solver,self.conv_criteria,self.CalculateReactionFlag,self.ReformDofSetAtEachStep,self.MoveMeshFlag,self.Comm,self.guess_row_size)
         mpi.world.barrier()
+        self.solver.max_iter = self.max_iterations
 	                     
     #######################################################################   
     def Solve(self):
