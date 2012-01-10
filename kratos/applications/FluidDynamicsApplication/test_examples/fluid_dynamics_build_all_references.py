cd54f8bee2ec0fba5f49bba8af94a07bc883eaff
jcotela <jcotela@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-01-10 15:14:11 +0000

    Implementing the 2D verision of the monolithic slip condition and wall law, adding a relevant benchmark

--- kratos/applications/FluidDynamicsApplication/test_examples/fluid_dynamics_build_all_references.py
+++ kratos/applications/FluidDynamicsApplication/test_examples/fluid_dynamics_build_all_references.py
@@ -10,6 +10,14 @@ import vms2d_build_reference
 
 os.chdir("..")
 
+# slip condition + wall law
+os.chdir("slip_test")
+sys.path.append(os.getcwd())
+
+import slip_test_build_reference
+
+os.chdir("..")
+
 # Add other examples here
 
 # Note: some test examples are not included in this file, as the results are compared to an exact solution
