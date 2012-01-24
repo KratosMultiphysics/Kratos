bf8f7525b7cbb2caa2679f6399b0b2748f75bb3a
nelson <nelson@cimne.upc.edu>
2012-01-24 11:36:39 +0000

    deleting some debug warning

--- applications/structural_application/python_scripts/structural_solver_dynamic_central_differences.py
+++ applications/structural_application/python_scripts/structural_solver_dynamic_central_differences.py
@@ -4,47 +4,45 @@ from KratosStructuralApplication import *
 
 
 def AddVariables(model_part):
+    model_part.AddNodalSolutionStepVariable(FORCE);
     model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
     model_part.AddNodalSolutionStepVariable(NODAL_MASS);
     model_part.AddNodalSolutionStepVariable(RHS);
-    model_part.AddNodalSolutionStepVariable(DISPLACEMENT_OLD);
-    #model_part.AddNodalSolutionStepVariable(DISPLACEMENT_NULL);
-    #model_part.AddNodalSolutionStepVariable(DISPLACEMENT_EINS);
-    #model_part.AddNodalSolutionStepVariable(DISPLACEMENT_DT);
-    #model_part.AddNodalSolutionStepVariable(DISPLACEMENT_NULL_DT);
-    #model_part.AddNodalSolutionStepVariable(DISPLACEMENT_EINS_DT);
-    #model_part.AddNodalSolutionStepVariable(ACCELERATION_NULL);
-    #model_part.AddNodalSolutionStepVariable(ACCELERATION_EINS);
     model_part.AddNodalSolutionStepVariable(VELOCITY);
     model_part.AddNodalSolutionStepVariable(ACCELERATION);
     model_part.AddNodalSolutionStepVariable(REACTION);
     model_part.AddNodalSolutionStepVariable(NEGATIVE_FACE_PRESSURE);
     model_part.AddNodalSolutionStepVariable(POSITIVE_FACE_PRESSURE);
-    #model_part.AddNodalSolutionStepVariable(INSITU_STRESS);
-    model_part.AddNodalSolutionStepVariable(NODAL_DAMAGE);
-    model_part.AddNodalSolutionStepVariable(NODAL_AREA);
-    model_part.AddNodalSolutionStepVariable(NODAL_VOLUME);
+    model_part.AddNodalSolutionStepVariable(BODY_FORCE);
+    model_part.AddNodalSolutionStepVariable(DAMAGE);
+    model_part.AddNodalSolutionStepVariable(DENSITY);
+    model_part.AddNodalSolutionStepVariable(PRESSURE);
+    model_part.AddNodalSolutionStepVariable(DISPLACEMENT_OLD);
     model_part.AddNodalSolutionStepVariable(NODAL_VALUES);
-    model_part.AddNodalSolutionStepVariable(SPLIT_NODAL);
-    model_part.AddNodalSolutionStepVariable(IS_DUPLICATED);
-    model_part.AddNodalSolutionStepVariable(NODAL_STRAIN);
     model_part.AddNodalSolutionStepVariable(IS_BOUNDARY);
-    model_part.AddNodalSolutionStepVariable(REFINEMENT_LEVEL);
     model_part.AddNodalSolutionStepVariable(IS_CONTACT_SLAVE);
     model_part.AddNodalSolutionStepVariable(IS_CONTACT_MASTER);
+    model_part.AddNodalSolutionStepVariable(NODAL_AREA);
+    model_part.AddNodalSolutionStepVariable(NODAL_VOLUME);
+    model_part.AddNodalSolutionStepVariable(NODAL_DAMAGE);
+    model_part.AddNodalSolutionStepVariable(IS_DUPLICATED);
+    model_part.AddNodalSolutionStepVariable(NODAL_STRAIN);
+    model_part.AddNodalSolutionStepVariable(REFINEMENT_LEVEL);
+    model_part.AddNodalSolutionStepVariable(SPLIT_NODAL);
+    model_part.AddNodalSolutionStepVariable(NORMAL);
+    #model_part.AddNodalSolutionStepVariable(KINETIC_ENERGY);
+    #model_part.AddNodalSolutionStepVariable(NORMAL_CONTACT_STRESS);
+    print "Variables for the dynamic structural solution added correctly"
+    
+    
 
-    print "variables for the dynamic structural solution added correctly"
-        
 def AddDofs(model_part):
     for node in model_part.Nodes:
-        #adding dofs
-##        node.AddDof(DISPLACEMENT_X);
-##        node.AddDof(DISPLACEMENT_Y);
-##        node.AddDof(DISPLACEMENT_Z);
         node.AddDof(DISPLACEMENT_X,REACTION_X);
         node.AddDof(DISPLACEMENT_Y,REACTION_Y);
         node.AddDof(DISPLACEMENT_Z,REACTION_Z);
-    print "dofs for the dynamic structural solution added correctly"
+    print "Dofs for the dynamic structural solution added correctly"
+
 
 
 class DynamicStructuralSolver:
@@ -56,9 +54,10 @@ class DynamicStructuralSolver:
         self.alpha_damp               = 0.00;
         self.max_delta_time           = 0.05;
         self.fraction_delta_time      = 0.90;
-        self.CalculateReactionFlag    = False;
+        self.CalculateReactionFlag    = True;
         self.MoveMeshFlag             = True;
         self.ComputeContactConditions = False;
+        self.CE                       = Constraint_Enforcement.Penalty_Methods; #Constraint_Enforcement.Lagrange_Multiplie_Methods;
 
                
     #######################################################################
@@ -72,7 +71,7 @@ class DynamicStructuralSolver:
     def Initialize(self):
         
         #creating the solution strategy
-        self.solver = ResidualBasedCentralDiferencesStrategy(self.model_part,  self.domain_size,  self.alpha_damp, self.fraction_delta_time, self.max_delta_time, self.CalculateReactionFlag, self.ComputeContactConditions, self.MoveMeshFlag)
+        self.solver = ResidualBasedCentralDiferencesStrategy(self.model_part, self.CE, self.domain_size,  self.alpha_damp, self.fraction_delta_time, self.max_delta_time, self.CalculateReactionFlag, self.ComputeContactConditions, self.MoveMeshFlag)
 
         
     #######################################################################   
@@ -88,6 +87,9 @@ class DynamicStructuralSolver:
       
     def SetConditionsFlag(self, ComputeContactConditions):
       (self.solver).SetConditionsFlag(ComputeContactConditions)
+      
+    def CalculateBoundaryContours(self, ComputeBoundary):
+      (self.solver).CalculateBoundaryContours(ComputeBoundary)
     
     
       
