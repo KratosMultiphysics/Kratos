33ca4bf5fb6d8d3cec3184512762153a136649de
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-01-30 08:26:44 +0000

    removed save from comments in some files

--- kratos/applications/convection_diffusion_application/custom_conditions/thermal_face2D.h
+++ kratos/applications/convection_diffusion_application/custom_conditions/thermal_face2D.h
@@ -237,7 +237,6 @@ namespace Kratos
 	
         virtual void save(Serializer& rSerializer) const
 	{
-	//rSerializer.save("Name", "ThermalFace2D");
 	KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition);
 	}
 	
