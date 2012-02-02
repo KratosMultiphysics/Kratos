0cf6e1e9f3e62e8a9cc170b23c697ebc5479b470
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-02-02 16:50:05 +0000

    serializer

--- kratos/applications/ThermoMechanicalApplication/custom_elements/heat_contact_2d.h
+++ kratos/applications/ThermoMechanicalApplication/custom_elements/heat_contact_2d.h
@@ -229,7 +229,6 @@ namespace Kratos
 
       virtual void save(Serializer& rSerializer) const
       {
-	  rSerializer.save("Name", "HeatContact2D");
 	  KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition);
       }
 
