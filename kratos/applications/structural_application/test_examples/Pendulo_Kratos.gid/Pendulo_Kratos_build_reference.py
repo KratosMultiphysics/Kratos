2ea1a8cc7a077462d1ed428eb562e4be0b440c42
nelson <nelson@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 15:04:54 +0000

    removing old format files

--- kratos/applications/structural_application/test_examples/Pendulo_Kratos.gid/Pendulo_Kratos_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for arc_length_des_benchmarking.py..."
-benchmarking.BuildReferenceData("Pendulo_Kratos_benchmarking.py", "Pendulo_Kratos_benchmarking.txt")
