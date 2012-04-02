fd109f139020ed7975353aab406811b89b72a90f
antonia <antonia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 16:41:51 +0000

    Benchmarking Meshing application

---
+++ kratos/applications/incompressible_fluid_application/test_examples/StillWater_Edgebased.gid/edgebased_levelset_var.py
@@ -0,0 +1,31 @@
+domain_size = 2
+
+max_time = 1.00000e+01
+safety_factor = 8.00000e-01
+max_time_step = 1.00000e-03
+output_dt = 1.00000e+00
+density = 1.00000e+03
+viscosity = 1.00000e-06
+body_force_x = 0.00000e+00
+body_force_y = -9.81000e+00
+body_force_z = 0.00000e+00
+wall_law_y = 0.00000e+00
+print_layers =  True 
+use_mass_correction =  True 
+redistance_frequency = 5.00000e+00
+extrapolation_layers = 5.00000e+00
+number_of_inital_steps = 1.00000e+01
+initial_time_step = 1.00000e-05
+reduction_on_failure = 3.00000e-01
+stabdt_pressure_factor = 1.00000e+00
+stabdt_convection_factor = 1.00000e-02
+tau2_factor = 1.00000e+00
+edge_detection_angle = 4.50000e+01
+assume_constant_pressure =  False 
+compute_porous_resistance_law = 0.00000e+00
+# Declare Python Variables
+
+problem_name = 'StillWater_Edgebased'
+problem_path = '/media/data/Documents_work/EXAMPLES/BENCHMARKING/StillWater_Edgebased.gid'
+kratos_path = '/home/antonia/kratos'
+
