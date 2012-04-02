1b35f151fd425c9c4a85fde527be69a436cc5600
antonia <antonia@cimne.upc.edu>
2012-04-02 16:07:28 +0000

    Benchmarking Meshing application

--- applications/MeshingApplication/test_exemples/Mapping_3d_BinBased.gid/ProjectionTestBinBased_3D_build_reference.py
+++ applications/MeshingApplication/test_exemples/Mapping_3d_BinBased.gid/ProjectionTestBinBased_3D_build_reference.py
@@ -4,4 +4,4 @@ sys.path.append(kratos_benchmarking_path)
 import benchmarking
 
 print "Building reference data for Mapping_3d..."
-benchmarking.BuildReferenceData("ProjectionTestBinBased_3D_benchmarking.py", "ProjectionTestBinBased_3D_benchmarking_ref.txt")
+benchmarking.BuildReferenceData("ProjectionTestBinBased_3D_script.py", "ProjectionTestBinBased_3D_benchmarking_ref.txt")
