85b659a61dc38be725a4306aa3483633e5e59667
jmarti <jmarti@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 14:52:45 +0000

    new format

--- kratos/applications/convection_diffusion_application/test_examples/convection_diffusion_benchmarks.py
+++ kratos/applications/convection_diffusion_application/test_examples/convection_diffusion_benchmarks.py
@@ -12,47 +12,24 @@ def Run():
         ################################################################
 	# rotatingcone_PureConvection
 
-	Text += "rotationgcone: "
-	os.chdir("square.gid")
-	sys.path.append(os.getcwd())
+##	Text += "rotationgcone: "
+##	os.chdir("square.gid")
+##	sys.path.append(os.getcwd())
+##
+##	print "Running rotatingcone_PureConvection.py..."
+##	Msg = benchmarking.RunBenchmark("rotatingcone_PureConvectionBenchmarking.py", "rotatingcone_PureConvection_ref.txt")	
+##	
+##	if (Msg == True):
+##		Text += "OK\n"
+##		print "square example succesful"
+##	else:
+##		Text += "FAILED\n"
+##		Text += Msg
+##		Text += "\n\n"
+##		print "square example FAILED"
+##
+##	os.chdir("..")
 
-	print "Running rotatingcone_PureConvection.py..."
-	Msg = benchmarking.RunBenchmark("rotatingcone_PureConvectionBenchmarking.py", "rotatingcone_PureConvection_ref.txt")	
-	
-	if (Msg == True):
-		Text += "OK\n"
-		print "square example succesful"
-	else:
-		Text += "FAILED\n"
-		Text += Msg
-		Text += "\n\n"
-		print "square example FAILED"
-
-	os.chdir("..")
-
-
-	################################################################
-	# testConvection
-
-	Text += "testConvection: "
-	os.chdir("testConvection.gid")
-	sys.path.append(os.getcwd())
-
-	print "Running test_pureconvectionsolver_benchmarking.py..."
-	Msg = benchmarking.RunBenchmark("test_pureconvectionsolver_benchmarking.py", "test_pureconvectionsolver_benchmarking_ref.txt")
-	
-	if (Msg == True):
-		Text += "OK\n"
-		print "testConvection example succesful"
-	else:
-		Text += "FAILED\n"
-		Text += Msg
-		Text += "\n\n"
-		print "testConvection example FAILED"
-
-	os.chdir("..")
-
-	
 
 	################################################################
 	# testConvection Edgebased
