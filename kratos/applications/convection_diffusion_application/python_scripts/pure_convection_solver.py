0232abe19ed45251aed39d3c9866144f2f887536
jmarti <jmarti@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 14:52:19 +0000

    new format

--- kratos/applications/convection_diffusion_application/python_scripts/pure_convection_solver.py
+++ kratos/applications/convection_diffusion_application/python_scripts/pure_convection_solver.py
@@ -1,6 +1,8 @@
 #importing the Kratos Library
-from Kratos import *
-from KratosConvectionDiffusionApplication import *
+from KratosMultiphysics import *
+from KratosMultiphysics.ConvectionDiffusionApplication import *
+CheckForPreviousImport()
+
 
 def AddVariables(model_part,settings):
     print "user should include the variables as needed over the model_part"
@@ -8,7 +10,7 @@ def AddVariables(model_part,settings):
     model_part.AddNodalSolutionStepVariable(VELOCITY);
     model_part.AddNodalSolutionStepVariable(settings.GetMeshVelocityVariable());
     model_part.AddNodalSolutionStepVariable(settings.GetUnknownVariable());
-    model_part.AddNodalSolutionStepVariable(TEMP_CONV_PROJ);
+    model_part.AddNodalSolutionStepVariable(settings.GetProjectionVariable());
     model_part.AddNodalSolutionStepVariable(NODAL_AREA);
     model_part.AddNodalSolutionStepVariable(SPECIFIC_HEAT);
     model_part.AddNodalSolutionStepVariable(settings.GetVolumeSourceVariable());
@@ -41,17 +43,11 @@ class PureConvectionSolver:
         self.ReformDofAtEachIteration = True; 
         self.reform_convection_matrix = True;
 
-        ##Variable to be convected:
-        ## #1 = TEPERATURE
-        ## #2 = DISTANCE
         self.scalar_var_convected = 1
         
-##        self.echo_level = 0
 
         #definition of the solvers
         pDiagPrecond = DiagonalPreconditioner()
-##        self.linear_solver =  BICGSTABSolver(1e-6, 5000,pDiagPrecond)
-##        pILUPrecond = ILU0Preconditioner()
         self.linear_solver =  BICGSTABSolver(1e-9, 5000,pDiagPrecond)
 
         ##pure convection tool
@@ -68,33 +64,6 @@ class PureConvectionSolver:
         print "Finishing Initialize"  
                 
    
-##    def Solve(self):
-##        print self.scalar_var_convected
-##        if(self.ReformDofAtEachIteration == True):
-##            (self.neighbour_search).Execute()        
-##        if(self.scalar_var_convected == 1):
-##            # construct system -- could be done once if the mesh does not change
-##            self.convection_solver.ConstructSystem(self.model_part,TEMPERATURE,VELOCITY,MESH_VELOCITY);
-##
-##            #calculate projections
-##            self.convection_solver.CalculateProjection(self.model_part,TEMPERATURE,NODAL_AREA,VELOCITY,MESH_VELOCITY,TEMP_CONV_PROJ);
-##                
-##            #perform convection step
-##            self.convection_solver.ConvectScalarVar(self.model_part,self.linear_solver,TEMPERATURE,VELOCITY,MESH_VELOCITY,TEMP_CONV_PROJ,self.time_order);
-##        elif(self.scalar_var_convected == 2):
-##            # construct system -- could be done once if the mesh does not change
-##            self.convection_solver.ConstructSystem(self.model_part,DISTANCE,VELOCITY,MESH_VELOCITY);
-##
-##            #calculate projections
-##            self.convection_solver.CalculateProjection(self.model_part,DISTANCE,NODAL_AREA,VELOCITY,MESH_VELOCITY,TEMP_CONV_PROJ);
-##                
-##            #perform convection step
-##            self.convection_solver.ConvectScalarVar(self.model_part,self.linear_solver, DISTANCE,VELOCITY,MESH_VELOCITY,TEMP_CONV_PROJ,self.time_order);
-##        else:
-##            print "ERROR: choose the correct variable to convect #1:TEMPERATURE #2:DISTANCE"
-##        #free memory
-##        self.convection_solver.ClearSystem() 
-
     def Solve(self, scalar_variable):
 	(self.model_part.ProcessInfo).SetValue(CONVECTION_DIFFUSION_SETTINGS,self.settings)
         if(self.ReformDofAtEachIteration == True or self.first_initialize_performed == False):
