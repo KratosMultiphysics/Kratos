116d0c4ea7785bbf34f0e0c47069440819ac9bac
nelson <nelson@cimne.upc.edu>
2012-04-02 15:04:04 +0000

    removing old format files

--- applications/structural_application/test_examples/cantilever2d.gid/cantilever2dstatic_superlu_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for cantilever2dstatic_superlu__benchmarking.py..."
-benchmarking.BuildReferenceData("cantilever2dstatic_superlu_benchmarking.py", "cantilever2dstatic_superlu.txt")
