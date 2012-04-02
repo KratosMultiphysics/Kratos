70610ab0f78057d3e5d777cb3ccaea671c39c16b
antonia <antonia@cimne.upc.edu>
2012-04-02 16:09:57 +0000

    Benchmarking Meshing application

--- applications/MeshingApplication/test_exemples/Moving_Circle.gid/ProjectionTest_2D_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for Moving_Circle..."
-benchmarking.BuildReferenceData("ProjectionTest_2D_benchmarking.py", "ProjectionTest_2D_benchmarking_ref.txt")
