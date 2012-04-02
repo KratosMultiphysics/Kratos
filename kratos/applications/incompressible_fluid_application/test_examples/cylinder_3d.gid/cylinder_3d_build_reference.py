47c593f49a27a9a043b8f4e52c50bc84c30522af
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 13:42:39 +0000

    delete cylinder_3d

--- kratos/applications/incompressible_fluid_application/test_examples/cylinder_3d.gid/cylinder_3d_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for cylinder_3d.py..."
-benchmarking.BuildReferenceData("run_example.py", "cylinder_3d_ref.txt")
