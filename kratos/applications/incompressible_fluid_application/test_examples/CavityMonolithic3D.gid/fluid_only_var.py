89180a1e1c785399d73e069a63add83e741e8ac9
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 13:49:52 +0000

    add test_example CavityMonolithic3D

---
+++ kratos/applications/incompressible_fluid_application/test_examples/CavityMonolithic3D.gid/fluid_only_var.py
@@ -0,0 +1,16 @@
+domain_size = 2
+
+Dt = 0.05
+max_time = 1.0
+SolverType = "monolithic_solver_eulerian"
+laplacian_form = 1
+output_step = 1
+predictor_corrector = True
+max_press_its = 3
+use_oss = 0
+dynamic_tau = 1
+# Declare Python Variables
+
+problem_name="CavityMonolithic3D"
+problem_path="/home/kazem/kratos/applications/incompressible_fluid_application/test_examples/CavityMonolithic3D.gid"
+kratos_path="/home/kazem/kratos"
