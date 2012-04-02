bc5305a25090c10c45ef842bab081108cfd9850f
antonia <antonia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-02 16:07:05 +0000

    Benchmarking Meshing application

--- kratos/applications/MeshingApplication/test_exemples/Mapping_2d_BinBased.gid/ProjectionTestBinBased_2D_build_reference.py
+++ kratos/applications/MeshingApplication/test_exemples/Mapping_2d_BinBased.gid/ProjectionTestBinBased_2D_build_reference.py
@@ -4,4 +4,4 @@ sys.path.append(kratos_benchmarking_path)
 import benchmarking
 
 print "Building reference data for Mapping 2d_BinBased..."
-benchmarking.BuildReferenceData("ProjectionTestBinBased_2D_benchmarking.py", "ProjectionTestBinBased_2D_benchmarking_ref.txt")
+benchmarking.BuildReferenceData("ProjectionTestBinBased_2D_script.py", "ProjectionTestBinBased_2D_benchmarking_ref.txt")
