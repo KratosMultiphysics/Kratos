ec9161fecb3abadf00674c8a03068641fa8c59ca
antonia <antonia@cimne.upc.edu>
2012-04-03 15:07:27 +0000

    new import kratosMultiphysics

--- applications/PFEMapplication/test_exemples/CouetteNonNewtonian2d.gid/Couette2dNonNewt.py
+++ applications/PFEMapplication/test_exemples/CouetteNonNewtonian2d.gid/Couette2dNonNewt.py
@@ -10,31 +10,23 @@ domain_size = fluid_only_var.domain_size
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path            = '../../../../libs' ##kratos_root/libs
-kratos_applications_path    = '../../../../applications' ##kratos_root/applications
+kratos_path = '../../../../' ##kratos_root
 kratos_benchmarking_path = '../../../../benchmarking' ##kratos_root/benchmarking
 import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
+sys.path.append(kratos_path)
 sys.path.append(kratos_benchmarking_path)
+import benchmarking
 
 #importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
+from KratosMultiphysics import *
 
-#importing applications
-import applications_interface
-applications_interface.Import_IncompressibleFluidApplication = True
-##applications_interface.Import_ExternalSolversApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
 
 ## from now on the order is not anymore crucial
 ##################################################################
 ##################################################################
-from KratosIncompressibleFluidApplication import *
-##from KratosExternalSolversApplication import *
+from KratosMultiphysics.IncompressibleFluidApplication import *
+##from KratosMultiphysics.ExternalSolversApplication import *
 
-import benchmarking
 
 def NodeFinder(node_list,X,Y,Z):
    for node in node_list:
