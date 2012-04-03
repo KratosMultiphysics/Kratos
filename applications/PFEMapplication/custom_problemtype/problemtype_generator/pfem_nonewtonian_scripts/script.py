51630186c12ff4d9bb86de69e14bc371e0e18e2f
antonia <antonia@cimne.upc.edu>
2012-04-03 10:38:38 +0000

    new import kratosMultiphysics

--- applications/PFEMapplication/custom_problemtype/problemtype_generator/pfem_nonewtonian_scripts/script.py
+++ applications/PFEMapplication/custom_problemtype/problemtype_generator/pfem_nonewtonian_scripts/script.py
@@ -11,26 +11,15 @@ domain_size = pfem_nonewtonian_var.domain_size
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path            = pfem_nonewtonian_var.kratos_path + '/libs' ##kratos_root/libs
-kratos_applications_path    = pfem_nonewtonian_var.kratos_path + '/applications' ##kratos_root/applications
 import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
+sys.path.append(pfem_nonewtonian_var.kratos_path)
 
 #importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
+from KratosMultiphysics import *
 
-#importing applications
-import applications_interface
-applications_interface.Import_IncompressibleFluidApplication = True
-applications_interface.Import_PFEMApplication = True
-applications_interface.Import_MeshingApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
-##from KratosStructuralApplication import *
-from KratosIncompressibleFluidApplication import *
-from KratosPFEMApplication import *
-from KratosMeshingApplication import *
+from KratosMultiphysics.IncompressibleFluidApplication import *
+from KratosMultiphysics.PFEMApplication import *
+from KratosMultiphysics.MeshingApplication import *
 # from now on the order is not anymore crucial
 ##################################################################
 ##################################################################
