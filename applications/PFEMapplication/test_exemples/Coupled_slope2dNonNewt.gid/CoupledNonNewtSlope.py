fb78a445ed698de33618fe87beb093c537019da5
antonia <antonia@cimne.upc.edu>
2012-04-03 15:07:34 +0000

    new import kratosMultiphysics

--- applications/PFEMapplication/test_exemples/Coupled_slope2dNonNewt.gid/CoupledNonNewtSlope.py
+++ applications/PFEMapplication/test_exemples/Coupled_slope2dNonNewt.gid/CoupledNonNewtSlope.py
@@ -11,40 +11,29 @@ domain_size = pfem_nonewtonian_coupled_var_benchmark.domain_size
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path = '../../../../libs' ##kratos_root/libs
-kratos_applications_path = '../../../../applications' ##kratos_root/applications
+kratos_path = '../../../../' ##kratos_root
 kratos_benchmarking_path = '../../../../benchmarking' ##kratos_root/benchmarking
-
 import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
+sys.path.append(kratos_path)
 sys.path.append(kratos_benchmarking_path)
-
-import edgebased_levelset_var
-
-#importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
-
-#importing applications
-import applications_interface
-applications_interface.Import_IncompressibleFluidApplication = True
-applications_interface.Import_PFEMApplication = True
-applications_interface.Import_MeshingApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
 import benchmarking
 
-from KratosStructuralApplication import *
-from KratosIncompressibleFluidApplication import *
-from KratosPFEMApplication import *
-from KratosMeshingApplication import *
-from KratosExternalSolversApplication import*
+
+#importing Kratos main library
+from KratosMultiphysics import *
+
+from KratosMultiphysics.StructuralApplication import *
+from KratosMultiphysics.IncompressibleFluidApplication import *
+from KratosMultiphysics.PFEMApplication import *
+from KratosMultiphysics.MeshingApplication import *
+from KratosMultiphysics.ExternalSolversApplication import*
 
 
 ## from now on the order is not anymore crucial
 ##################################################################
 ##################################################################
 import math
+import edgebased_levelset_var
 import edgebased_levelset_solver
 
 
