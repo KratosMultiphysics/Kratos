2f25b5eb86980864eda393a5697b588c10da55f0
antonia <antonia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-03 08:04:13 +0000

    Benchmarking Meshing application

--- kratos/applications/incompressible_fluid_application/test_examples/QuietWater_LevelSet.gid/split_level_set_build_reference.py
+++
@@ -1,7 +0,0 @@
-import sys
-kratos_benchmarking_path = '../../../../benchmarking' 
-sys.path.append(kratos_benchmarking_path)
-import benchmarking
-
-print "Building reference data for QuietWater_LevelSet.py..."
-benchmarking.BuildReferenceData("split_level_set_QuietWater.py", "QuietWater_LS_ref.txt")
