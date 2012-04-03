ec48ee65185235270abfe64919823cafaa0c4ac3
antonia <antonia@cimne.upc.edu>
2012-04-03 15:34:38 +0000

    new import kratosMultiphysics

--- applications/MeshingApplication/test_exemples/adaptive_mesher3d.gid/remesh.py
+++ applications/MeshingApplication/test_exemples/adaptive_mesher3d.gid/remesh.py
@@ -8,26 +8,19 @@ domain_size = 3
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path = '../../../../libs/' ##kratos_root/libs
-kratos_applications_path = '../../../../applications/' ##kratos_root/applications
+kratos_path = '../../../../' ##kratos_root/
 kratos_benchmarking_path = '../../../../benchmarking/' ##kratos_root/benchmarking
 import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
+sys.path.append(kratos_path)
 sys.path.append(kratos_benchmarking_path)
+import benchmarking
 
 #importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
+from KratosMultiphysics import *
 
-#importing applications
-import applications_interface
-
-applications_interface.ImportApplications(kernel, kratos_applications_path)
-import benchmarking
 #loading meshing application
 sys.path.append(kratos_applications_path + 'meshing_application/python_scripts') 
-from KratosMeshingApplication import *
+from KratosMultiphysics.MeshingApplication import *
 meshing_application = KratosMeshingApplication()
 kernel.AddApplication(meshing_application)
 
