3ed45bee093b33bee00539f3fe427b3d3bac9568
antonia <antonia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-03 15:34:46 +0000

    new import kratosMultiphysics

--- kratos/applications/MeshingApplication/test_exemples/dam3d_remesh.gid/remesh_paolo_pfem_refine.py
+++ kratos/applications/MeshingApplication/test_exemples/dam3d_remesh.gid/remesh_paolo_pfem_refine.py
@@ -8,25 +8,18 @@ domain_size = 3
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path = '../../../../libs/' ##kratos_root/libs
-kratos_applications_path = '../../../../applications/' ##kratos_root/applications
+kratos_path = '../../../../' ##kratos_root/
 import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
+sys.path.append(kratos_path)
 
 #importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
+from KratosMultiphysics import *
 
-#importing applications
-import applications_interface
-applications_interface.Import_IncompressibleFluidApplication = True
-applications_interface.Import_MeshingApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
 
 #loading meshing application
 #sys.path.append(kratos_applications_path + 'meshing_application/python_scripts') 
-from KratosMeshingApplication import *
+from KratosMultiphysics.IncompressibleFluidApplication import *
+from KratosMultiphysics.MeshingApplication import *
 #meshing_application = KratosMeshingApplication()
 #kernel.AddApplication(meshing_application)
 
