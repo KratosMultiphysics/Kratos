223af0ae3da3d8546183475a1d02e0a531d5492f
antonia <antonia@cimne.upc.edu>
2012-04-03 15:26:14 +0000

    new import kratosMultiphysics

--- applications/PFEMapplication/test_exemples/dam3d.gid/dam3d.py
+++ applications/PFEMapplication/test_exemples/dam3d.gid/dam3d.py
@@ -8,22 +8,17 @@ domain_size = 3
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path = '../../../../libs/' ##kratos_root/libs
-kratos_applications_path = '../../../../applications/' ##kratos_root/applications
+kratos_path = '../../../../' ##kratos_root/
 import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
+sys.path.append(kratos_path)
 
 #importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
+from KratosMultiphysics import *
 
 #importing applications
-import applications_interface
-applications_interface.Import_IncompressibleFluidApplication = True
-applications_interface.Import_PFEMApplication = True
-applications_interface.Import_MeshingApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
+from KratosMultiphysics.IncompressibleFluidApplicationimport *
+from KratosMultiphysics.PFEMApplication import *
+from KratosMultiphysics.MeshingApplication import *
 
 ## from now on the order is not anymore crucial
 ##################################################################
