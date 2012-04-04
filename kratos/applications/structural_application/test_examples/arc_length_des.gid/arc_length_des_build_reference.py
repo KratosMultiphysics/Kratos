592ee3d26ec8a09e3c4da6ccc36087cc1788c711
nelson <nelson@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-04 11:02:38 +0000

    adding file for benchmarking

---
+++ kratos/applications/structural_application/test_examples/arc_length_des.gid/arc_length_des_build_reference.py
@@ -0,0 +1,7 @@
+import sys
+kratos_benchmarking_path = '../../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
+print "Building reference data for arc_length_des_benchmarking.py..."
+benchmarking.BuildReferenceData("arc_length_des_benchmarking.py", "arc_length_des_benchmarking.txt")
