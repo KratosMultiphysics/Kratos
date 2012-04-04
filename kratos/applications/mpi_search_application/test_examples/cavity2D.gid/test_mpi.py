fe80c79135524015d84ff94fbb07e36a9ff9eab0
roigcarlos <roigcarlos@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-04 11:04:43 +0000

    Rewrtie some code Add support for multiple input search

--- kratos/applications/mpi_search_application/test_examples/cavity2D.gid/test_mpi.py
+++ kratos/applications/mpi_search_application/test_examples/cavity2D.gid/test_mpi.py
@@ -19,7 +19,7 @@ from KratosMultiphysics.MPISearchApplication import *
 
 
 #defining a model part
-model_part = ModelPart("FluidPart");
+model_part   = ModelPart("FluidPart");
 p_model_part = ModelPart("FluidPart");
 
 #providing the variable list to the model part
@@ -64,9 +64,6 @@ model_part_io_fluid.ReadModelPart(model_part)
 ##partitioner = MetisPartitioningProcess(model_part, gid_io, number_of_partitions, domain_size);
 ##partitioner.Execute()
 ##print "GetRank()",GetRank()
-
-
-
 
 print model_part
 
@@ -151,44 +148,25 @@ gid_io.InitializeResults(mesh_name , model_part.GetMesh())
 BoxSize = 1;
 bins_dynamic_mpi = BinsDynamicMpi(model_part,p_model_part,BoxSize)
 
-Count = 0
-for node in model_part.Nodes:
-    if (Count == 7):
-		print "("+str(mpi.rank)+") ("+str(node.Id)+") ("+ str(node.X)+" "+ str(node.Y)+" "+str(node.Z)+")"
-#        bins_dynamic_mpi.MPI_ExistPoint(node,None,0)
-		bins_dynamic_mpi.MPI_SearchInRadius(node,0.1,None,None,5,0)
-    Count = Count + 1;
+print "----------------------------------------------------------"
+bins_dynamic_mpi.MPIMultiSearchInRadiusTest()
+#bins_dynamic_mpi.MPISingleSearchInRadiusTest()
+#bins_dynamic_mpi.MultiSearchInRadiusTest()
+print "----------------------------------------------------------"
 
-catapum_directive
+#Count = 0
+#for node in model_part.Nodes:
+  #if (Count == 7):
+	#print "----------------------------------------------------------"
+	#bins_dynamic_mpi.MPISingleSearchInRadiusTest()
+	#bins_dynamic_mpi.MultiSearchInRadiusTest()
+	#print "----------------------------------------------------------"
+  #Count = Count + 1;
 
-for step in range(0,10):
+#break_here
+
+#for step in range(0,10):
     
-    time = Dt*step
-    model_part.CloneTimeStep(time)
-
-    if(mpi.rank == 0):
-        print time
-    #print model_part.ProcessInfo()[TIME]
-
-    #solving the fluid problem
-    if(step > 4):
-        fluid_solver.Solve()
-
-##        for node in model_part.Nodes:
-##            node.SetSolutionStepValue(PRESS_PROJ,0,zero);
-
-
-    #print the results
-    if(out == output_step):
-        gid_io.WriteNodalResults(PARTITION_INDEX,model_part.Nodes,time,0)
-        gid_io.WriteNodalResults(NORMAL,model_part.Nodes,time,0)
-        gid_io.WriteNodalResults(PRESSURE,model_part.Nodes,time,0)
-        gid_io.WriteNodalResults(VELOCITY,model_part.Nodes,time,0)
-        out = 0
-    out = out + 1
-
-#for step in range(0,nsteps):
-
     #time = Dt*step
     #model_part.CloneTimeStep(time)
 
@@ -212,8 +190,34 @@ for step in range(0,10):
         #gid_io.WriteNodalResults(VELOCITY,model_part.Nodes,time,0)
         #out = 0
     #out = out + 1
+
+##for step in range(0,nsteps):
+
+    ##time = Dt*step
+    ##model_part.CloneTimeStep(time)
+
+    ##if(mpi.rank == 0):
+        ##print time
+    ###print model_part.ProcessInfo()[TIME]
+
+    ###solving the fluid problem
+    ##if(step > 4):
+        ##fluid_solver.Solve()
+
+####        for node in model_part.Nodes:
+####            node.SetSolutionStepValue(PRESS_PROJ,0,zero);
+
+
+    ###print the results
+    ##if(out == output_step):
+        ##gid_io.WriteNodalResults(PARTITION_INDEX,model_part.Nodes,time,0)
+        ##gid_io.WriteNodalResults(NORMAL,model_part.Nodes,time,0)
+        ##gid_io.WriteNodalResults(PRESSURE,model_part.Nodes,time,0)
+        ##gid_io.WriteNodalResults(VELOCITY,model_part.Nodes,time,0)
+        ##out = 0
+    ##out = out + 1
     
-gid_io.FinalizeResults()
+#gid_io.FinalizeResults()
 
           
         
