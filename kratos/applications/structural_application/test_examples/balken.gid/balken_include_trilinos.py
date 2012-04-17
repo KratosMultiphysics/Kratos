832da0cabfa5a1aa32901539217c7de4e259aa56
janosch <janosch@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-17 14:35:33 +0000

    new application import

--- kratos/applications/structural_application/test_examples/balken.gid/balken_include_trilinos.py
+++ kratos/applications/structural_application/test_examples/balken.gid/balken_include_trilinos.py
@@ -16,31 +16,14 @@
 ## ATTENTION: the following lines have to be adapted to      #####
 ##            match your acrtual configuration               #####
 ##################################################################
-## kratos test examples:
-kratos_root_path = '../../../..'
-##setting up paths
-kratos_libs_path = kratos_root_path+'/libs' ##kratos_root/libs
-kratos_applications_path = kratos_root_path+'/applications' ##kratos_root/applications
-##################################################################
-##################################################################
-import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
-
-#importing Kratos main library
-from Kratos import *
+#importing Kratos modules
+from KratosMultiphysics import *
+from KratosMultiphysics.StructuralApplication import *
+from KratosMultiphysics.ExternalSolversApplication import *
+from KratosMultiphysics.MKLSolversApplication import *
+from KratosMultiphysics.TrilinosApplication import *
+from KratosMultiphysice.MetisApplication import *
 kernel = Kernel()   #defining kernel
-
-#importing applications
-import applications_interface
-applications_interface.Import_StructuralApplication = True
-applications_interface.Import_KratosTrilinosApplication = True
-applications_interface.Import_KratosMetisApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
-from KratosStructuralApplication import *
-from KratosExternalSolversApplication import *
-from KratosTrilinosApplication import *
-from KratosMetisApplication import *
 ##################################################################
 ##################################################################
 class Model:
