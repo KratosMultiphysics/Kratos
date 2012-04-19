4a1cb47be0eeafaa9dd664152dfa47afc21fba26
paolo <paolo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-19 15:06:23 +0000

    Changes for the new interface

--- kratos/applications/ULFapplication/test_exemples/dam_2d.gid/script.py
+++ kratos/applications/ULFapplication/test_exemples/dam_2d.gid/script.py
@@ -10,31 +10,15 @@ domain_size = fluid_ulf_var.domain_size
 ## ATTENTION: here the order is important
 
 #including kratos path
-kratos_libs_path            = fluid_ulf_var.kratos_path + '/libs' ##kratos_root/libs
-kratos_applications_path    = fluid_ulf_var.kratos_path + '/applications' ##kratos_root/applications
-import sys
-sys.path.append(kratos_libs_path)
-sys.path.append(kratos_applications_path)
-
 #importing Kratos main library
-from Kratos import *
-kernel = Kernel()   #defining kernel
+import sys
+sys.path.append(fluid_ulf_var.kratos_path)
+from KratosMultiphysics import *
+from KratosMultiphysics.ULFApplication import *
+from KratosMultiphysics.MeshingApplication import *
+from KratosMultiphysics.PFEMApplication import PfemUtils
+from KratosMultiphysics.StructuralApplication import *
 
-#importing applications
-import applications_interface
-applications_interface.Import_ULFApplication = True
-applications_interface.Import_MeshingApplication = True
-applications_interface.Import_PFEMApplication = True
-applications_interface.Import_StructuralApplication = True
-##applications_interface.Import_ExternalSolversApplication = True
-applications_interface.ImportApplications(kernel, kratos_applications_path)
-
-## from now on the order is not anymore crucial
-##################################################################
-##################################################################
-from KratosULFApplication import *
-from KratosMeshingApplication import *
-from KratosStructuralApplication import *
 
 #defining a model part for the fluid and one for the structure
 fluid_model_part = ModelPart("FluidPart");
