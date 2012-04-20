48f9fa6becf04f7aa7c813811aaa8d855ef085ec
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-04-20 09:29:19 +0000

    removed some unmantained applications

--- kratos/applications/ALEapplication/python_scripts/mesh_solver_ballvertex.py
+++ kratos/applications/ALEapplication/python_scripts/mesh_solver_ballvertex.py
@@ -39,7 +39,7 @@ class MeshSolver:
 #        self.linear_solver =  BICGSTABSolver(1e-5, 300,pILUPrecond)
 ##        pDiagPrecond = DiagonalPreconditioner()
 ##        self.linear_solver = CGSolver(1e-3, 300, pDiagPrecond)
-	self.linear_solver = ScalingSolver( DeflatedCGSolver(1e-6, 300, True,1000) , True)
+	self.linear_solver = ScalingSolver( DeflatedCGSolver(1e-6, 3000, True,1000) , True)
 
     def Initialize(self):
         (self.neighbour_search).Execute()
