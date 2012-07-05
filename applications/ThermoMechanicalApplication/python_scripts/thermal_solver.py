ed5d0b6b1bf44e266e18e469a3f7698038c2d270
kazem <kazem@cimne.upc.edu>
2012-07-05 13:28:40 +0000

    minor changes

--- applications/ThermoMechanicalApplication/python_scripts/thermal_solver.py
+++ applications/ThermoMechanicalApplication/python_scripts/thermal_solver.py
@@ -13,14 +13,15 @@ def AddVariables(model_part,settings):
     model_part.AddNodalSolutionStepVariable(settings.GetMeshVelocityVariable());   
     model_part.AddNodalSolutionStepVariable(settings.GetUnknownVariable());    
     model_part.AddNodalSolutionStepVariable(SPECIFIC_HEAT);
-    model_part.AddNodalSolutionStepVariable(settings.GetVolumeSourceVariable());
+    model_part.AddNodalSolutionStepVariable(settings.GetVolumeSourceVariable());    
     model_part.AddNodalSolutionStepVariable(settings.GetDensityVariable());
-    model_part.AddNodalSolutionStepVariable(settings.GetDiffusionVariable());
-    model_part.AddNodalSolutionStepVariable(settings.GetSurfaceSourceVariable());
+    model_part.AddNodalSolutionStepVariable(settings.GetDiffusionVariable());   
+    model_part.AddNodalSolutionStepVariable(settings.GetSurfaceSourceVariable());   
     #model_part.AddNodalSolutionStepVariable(CONVECTION_COEFFICIENT);
     model_part.AddNodalSolutionStepVariable(NORMAL);
-    model_part.AddNodalSolutionStepVariable(IS_BOUNDARY);    
-    model_part.AddNodalSolutionStepVariable(HTC);    
+    model_part.AddNodalSolutionStepVariable(IS_BOUNDARY);       
+    model_part.AddNodalSolutionStepVariable(settings.GetTransferCoefficientVariable()); 
+    #model_part.AddNodalSolutionStepVariable(HTC);    
     
     print "variables for the THERMAL_SOLVER added correctly"
         
@@ -39,14 +40,16 @@ class Solver:
 
         self.time_scheme = ResidualBasedIncrementalUpdateStaticScheme()       
         self.settings = my_settings
+        self.domain_size = domain_size
         #definition of the solvers
-        self.linear_solver =  SkylineLUFactorizationSolver()
+        #self.linear_solver =  SkylineLUFactorizationSolver()
 ##        self.linear_solver =SuperLUSolver()
 ##        self.linear_solver = MKLPardisoSolver()
 
         pPrecond = DiagonalPreconditioner()
 ##        pPrecond = ILU0Preconditioner()
         #self.linear_solver =  BICGSTABSolver(1e-6, 5000,pPrecond)
+        self.linear_solver = BICGSTABSolver(1e-6, 5000)
 
         self.dynamic_tau = 1.0
 
@@ -57,7 +60,11 @@ class Solver:
         self.MoveMeshFlag = False
     
         self.neigh_finder = FindNodalNeighboursProcess(self.model_part,9,18)
-        self.elem_neighbor_finder = FindElementalNeighboursProcess(self.model_part, 2, 20)
+        if (self.domain_size == 2):
+           self.elem_neighbor_finder = FindElementalNeighboursProcess(self.model_part, 2, 20)
+        else:
+           self.elem_neighbor_finder = FindElementalNeighboursProcess(self.model_part, 3, 20)	  
+           
         self.Nmax = len(model_part.Properties)
         self.contact_matix = Matrix()
         
@@ -66,14 +73,17 @@ class Solver:
         
     #######################################################################
     def Initialize(self):
- 
-        self.duplicate_and_create_conditions = DuplicateInterfaceNodesCreateConditionsProcess(self.model_part,"HeatContact2D", self.Nmax, self.contact_matix )        
 
-        (self.neigh_finder).ClearNeighbours();
-        (self.neigh_finder).Execute();
+        if (self.domain_size == 2):
+             self.duplicate_and_create_conditions = DuplicateInterfaceNodesCreateConditionsProcess(self.model_part,"HeatContact2D", self.Nmax, self.contact_matix )        
+        else:
+             print "duplicate_and_create_conditions is not implemented for 3D"        
+	     
+        #(self.neigh_finder).ClearNeighbours();
+        #(self.neigh_finder).Execute();
         
-        (self.elem_neighbor_finder).ClearNeighbours()
-        (self.elem_neighbor_finder).Execute() 
+        #(self.elem_neighbor_finder).ClearNeighbours()
+        #(self.elem_neighbor_finder).Execute() 
         print "INSIDE INITIALIZE"           
  	(self.model_part.ProcessInfo).SetValue(CONVECTION_DIFFUSION_SETTINGS,self.settings)    
  	
