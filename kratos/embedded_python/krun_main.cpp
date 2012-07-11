6fdbae8f39d8e62bfa5fb9fb4c4aba8d1368329f
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-07-11 13:47:32 +0000

    correction for krun to work on windows

--- kratos/embedded_python/krun_main.cpp
+++ kratos/embedded_python/krun_main.cpp
@@ -3,13 +3,27 @@
 
 int main(int argc, char *argv[])
 {
+	if ( argc < 2) {
+		std::cout << "MISSING SCRIPT NAME! Usage: " << argv[ 0] << " filename_in.py " <<std::endl;
+		return 1;
+	}
   Py_Initialize();
+  
   PySys_SetArgv(argc,argv);
 
-  FILE *fp      = fopen (argv[1],   "r+");
+ // FILE *fp      = fopen (argv[1],   "r");
+ // if ( !fp) {
+	 // std::cout << "Error opening file: " << argv[ 1] << "\n";
+	 // return 1;
+  //}
 
-  PyRun_SimpleFile (fp, argv[1]);
+  PyObject* PyFileObject = PyFile_FromString(argv[1], "r");
+  PyRun_SimpleFile(PyFile_AsFile(PyFileObject), argv[1]);
+  //PyRun_SimpleFile (fp, argv[1]);
 
   Py_Finalize();
+  //fclose( fp);
+
+  std::cout << "KRATOS TERMINATED CORRECTLY" << std::endl;
   return 0;
 }
