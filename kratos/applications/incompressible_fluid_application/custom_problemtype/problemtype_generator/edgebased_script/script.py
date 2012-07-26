a87972857df452abe234eca7034c8757fe87fe0b
antoldt <antoldt@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-07-26 10:18:53 +0000

    problem_settings

--- kratos/applications/incompressible_fluid_application/custom_problemtype/problemtype_generator/edgebased_script/script.py
+++ kratos/applications/incompressible_fluid_application/custom_problemtype/problemtype_generator/edgebased_script/script.py
@@ -1,9 +1,9 @@
-import edgebased_levelset_var
+import problem_settings
 
 ##################################################################
 ##################################################################
 #setting the domain size for the problem to be solved
-domain_size = edgebased_levelset_var.domain_size
+domain_size = problem_settings.domain_size
 
 ##################################################################
 ##################################################################
@@ -11,7 +11,7 @@ domain_size = edgebased_levelset_var.domain_size
 
 #including kratos path
 import sys
-sys.path.append(edgebased_levelset_var.kratos_path)
+sys.path.append(problem_settings.kratos_path)
 
 #importing Kratos main library
 from KratosMultiphysics import *
@@ -33,7 +33,7 @@ import edgebased_levelset_solver
 edgebased_levelset_solver.AddVariables(fluid_model_part)
 
 #introducing input file name
-input_file_name = edgebased_levelset_var.problem_name
+input_file_name = problem_settings.problem_name
 
 #reading the fluid part
 gid_mode = GiDPostMode.GiD_PostBinary
@@ -42,7 +42,7 @@ deformed_mesh_flag = WriteDeformedMeshFlag.WriteUndeformed
 write_conditions = WriteConditionsFlag.WriteConditions
 
 ##selecting output format
-if(edgebased_levelset_var.print_layers == True):
+if(problem_settings.print_layers == True):
     gid_io = EdgebasedGidIO(input_file_name,gid_mode,multifile,deformed_mesh_flag, write_conditions)
 else:
     gid_io = GidIO(input_file_name,gid_mode,multifile,deformed_mesh_flag, write_conditions)
@@ -80,32 +80,32 @@ for node in fluid_model_part.Nodes:
         
 #constructing the solver
 body_force = Vector(3)
-body_force[0] = edgebased_levelset_var.body_force_x
-body_force[1] = edgebased_levelset_var.body_force_y
-body_force[2] = edgebased_levelset_var.body_force_z
+body_force[0] = problem_settings.body_force_x
+body_force[1] = problem_settings.body_force_y
+body_force[2] = problem_settings.body_force_z
 if(body_force[0] == 0.0 and body_force[1] == 0.0 and body_force[2] == 0.0):
     raise "ERROR. Body Force cannot be a ZERO VECTOR"
 
-viscosity   = edgebased_levelset_var.viscosity
-density     = edgebased_levelset_var.density
+viscosity   = problem_settings.viscosity
+density     = problem_settings.density
 fluid_solver = edgebased_levelset_solver.EdgeBasedLevelSetSolver(fluid_model_part,domain_size,body_force,viscosity,density)
-fluid_solver.redistance_frequency = edgebased_levelset_var.redistance_frequency
-fluid_solver.extrapolation_layers = int(edgebased_levelset_var.extrapolation_layers)
-fluid_solver.stabdt_pressure_factor = edgebased_levelset_var.stabdt_pressure_factor
-fluid_solver.stabdt_convection_factor = edgebased_levelset_var.stabdt_convection_factor
-fluid_solver.use_mass_correction = edgebased_levelset_var.use_mass_correction
-fluid_solver.tau2_factor = edgebased_levelset_var.tau2_factor
-fluid_solver.edge_detection_angle = edgebased_levelset_var.edge_detection_angle
-fluid_solver.assume_constant_pressure = edgebased_levelset_var.assume_constant_pressure
-fluid_solver.compute_porous_resistance_law = int(edgebased_levelset_var.compute_porous_resistance_law) ##0 = None; 1 = Ergun; 2 = Custom;
+fluid_solver.redistance_frequency = problem_settings.redistance_frequency
+fluid_solver.extrapolation_layers = int(problem_settings.extrapolation_layers)
+fluid_solver.stabdt_pressure_factor = problem_settings.stabdt_pressure_factor
+fluid_solver.stabdt_convection_factor = problem_settings.stabdt_convection_factor
+fluid_solver.use_mass_correction = problem_settings.use_mass_correction
+fluid_solver.tau2_factor = problem_settings.tau2_factor
+fluid_solver.edge_detection_angle = problem_settings.edge_detection_angle
+fluid_solver.assume_constant_pressure = problem_settings.assume_constant_pressure
+fluid_solver.compute_porous_resistance_law = int(problem_settings.compute_porous_resistance_law) ##0 = None; 1 = Ergun; 2 = Custom;
 ##print "compute_porous_resistance_law   ", fluid_solver.compute_porous_resistance_law
 #using MKLPardisosolver ----> it has to be compiled in kratos!!
 ##fluid_solver.pressure_linear_solver = MKLPardisoSolver()
 
 fluid_solver.Initialize()
 
-if(edgebased_levelset_var.wall_law_y > 1e-10):
-    fluid_solver.fluid_solver.ActivateWallResistance(edgebased_levelset_var.wall_law_y);
+if(problem_settings.wall_law_y > 1e-10):
+    fluid_solver.fluid_solver.ActivateWallResistance(problem_settings.wall_law_y);
     
 ####
 
@@ -113,20 +113,20 @@ if(edgebased_levelset_var.wall_law_y > 1e-10):
 print "fluid solver created"
 
 #settings to be changed
-max_Dt = edgebased_levelset_var.max_time_step 
+max_Dt = problem_settings.max_time_step 
 initial_Dt = 0.001 * max_Dt 
-final_time = edgebased_levelset_var.max_time
-output_dt = edgebased_levelset_var.output_dt
-safety_factor = edgebased_levelset_var.safety_factor
+final_time = problem_settings.max_time
+output_dt = problem_settings.output_dt
+safety_factor = problem_settings.safety_factor
 
-number_of_inital_steps = edgebased_levelset_var.number_of_inital_steps
-initial_time_step = edgebased_levelset_var.initial_time_step
+number_of_inital_steps = problem_settings.number_of_inital_steps
+initial_time_step = problem_settings.initial_time_step
 out = 0
 
 original_max_dt = max_Dt
 
 ###mesh to be printed
-if(edgebased_levelset_var.print_layers == False):
+if(problem_settings.print_layers == False):
     mesh_name = 0.0
     gid_io.InitializeMesh( mesh_name)
     gid_io.WriteMesh( fluid_model_part.GetMesh() )
@@ -177,7 +177,7 @@ while(time < final_time):
             #we found a velocity too large! we need to reduce the time step
             fluid_solver.fluid_solver.ReduceTimeStep(fluid_model_part,time) ##this is to set the database to the value at the beginning of the step
 
-            safety_factor *= edgebased_levelset_var.reduction_on_failure
+            safety_factor *= problem_settings.reduction_on_failure
             reduced_dt = fluid_solver.EstimateTimeStep(safety_factor,max_Dt)
             
             print "time before reduction= ",time
@@ -191,7 +191,7 @@ while(time < final_time):
             fluid_solver.Solve()
 
     if(time >= next_output_time):
-        if(edgebased_levelset_var.print_layers == True):
+        if(problem_settings.print_layers == True):
             #writing mesh 
             gid_io.InitializeMesh( time );
             gid_io.WriteMesh((fluid_model_part).GetMesh());
@@ -207,7 +207,7 @@ while(time < final_time):
         gid_io.WriteNodalResults(NONLIN_DARCY_COEF,fluid_model_part.Nodes,time,0)
         gid_io.Flush()
 
-        if(edgebased_levelset_var.print_layers == True):
+        if(problem_settings.print_layers == True):
             gid_io.FinalizeResults()
 
         next_output_time = time + output_dt
@@ -217,7 +217,7 @@ while(time < final_time):
     out = out + 1
     step = step + 1
       
-if(edgebased_levelset_var.print_layers == False):
+if(problem_settings.print_layers == False):
     gid_io.FinalizeResults()    
         
 
