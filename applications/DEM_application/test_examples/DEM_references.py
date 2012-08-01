89cef111f6d0edc9b3d409c9782d4d819ca95640
jirazabal <jirazabal@cimne.upc.edu>
2012-08-01 08:01:45 +0000

    New benchmark

---
+++ applications/DEM_application/test_examples/DEM_references.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# two_balls_no_damp element test
+
+os.chdir("two_balls_no_damp")
+sys.path.append(os.getcwd())
+
+import two_balls_no_damp_build_reference
+
+os.chdir("..")
+
+# Add other examples here
+
+# Note: some test examples are not included in this file, as the results are compared to an exact solution
