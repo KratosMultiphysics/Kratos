2041f461bad50ef0645d762cc25abae1e7076beb
jirazabal <jirazabal@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-08-01 08:01:45 +0000

    New benchmark

---
+++ kratos/applications/DEM_application/test_examples/DEM_benchmarks.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+kratos_benchmarking_path = '../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
+
+def Run():
+	Msg = ""
+	Text = "========== DEM ==========\n"
+
+        ###############################################################################
+	#VMS2D element test
+	
+	Text += "DEM element test: "
+	os.chdir("two_balls_no_damp")	
+	sys.path.append(os.getcwd())
+
+	print "running the two_balls_no_damp benchmark test..."
+        Msg = benchmarking.RunBenchmark("script.py", "two_balls_no_damp_ref.txt")
+
+        if (Msg == True):
+            Text += "OK\n"
+            print "two_balls_no_damp element test succesful"
+        else:
+            Text += "FAILED\n"
+            Text += Msg
+            Text += "\n\n"
+            print "two_balls_no_damp element test FAILED"
+
+
+	os.chdir("..")
+        ###############################################################################
+
+
+	# Add other examples here
+
+
+
+        ################################################################
+        print "resume of all of the examples for the DEM application :"
+        print Text
+	return Text
+
+if __name__=='__main__':
+  Run();
+
+
---
+++ kratos/applications/DEM_application/test_examples/two_balls_no_damp/two_balls_no_damp_build_reference.py
@@ -0,0 +1,7 @@
+import sys
+kratos_benchmarking_path = '../../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
+print "Building reference data for script.py..."
+benchmarking.BuildReferenceData("script.py", "two_balls_no_damp_ref.txt")
