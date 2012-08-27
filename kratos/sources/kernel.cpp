2bdc079f5c0b644df2ea0f066c6488e366fcb02a
Pooyan Dadvand <pooyan@cimne.upc.edu>
2012-08-27 13:12:05 +0000

    Adding kernel.cpp

---
+++ kratos/sources/kernel.cpp
@@ -0,0 +1,88 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+
+//
+//   Project Name:        Kratos
+//   Last Modified by:    $Author: pooyan $
+//   Date:                $Date: 2008-10-23 11:35:13 $
+//   Revision:            $Revision: 1.6 $
+//
+//
+
+#include "includes/kernel.h"
+
+
+namespace Kratos
+{
+    Kernel::Kernel()
+    {
+	std::cout << " |  /           |             " << std::endl;
+	std::cout << " ' /   __| _` | __|  _ \\   __|" << std::endl;
+	std::cout << " . \\  |   (   | |   (   |\\__ \\ " << std::endl;
+	std::cout << "_|\\_\\_|  \\__,_|\\__|\\___/ ____/" << std::endl;
+	std::cout << "           Multi-Physics 3.0.1" << std::endl;
+
+        mKratosApplication.RegisterVariables();
+    }
+
+    std::string Kernel::Info() const
+    {
+        return "kernel";
+    }
+
+    void Kernel::PrintInfo(std::ostream& rOStream) const
+    {
+        rOStream << "kernel";
+    }
+
+    /// Print object's data.
+    void Kernel::PrintData(std::ostream& rOStream) const
+    {
+        rOStream << "Variables:" << std::endl;
+        KratosComponents<VariableData>().PrintData(rOStream);
+        rOStream << std::endl;
+        rOStream << "Elements:" << std::endl;
+        KratosComponents<Element>().PrintData(rOStream);
+        rOStream << std::endl;
+        rOStream << "Conditions:" << std::endl;
+        KratosComponents<Condition>().PrintData(rOStream);
+    }
+
+}
+
+
