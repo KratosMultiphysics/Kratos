a52cbb506a1c1c9d597b6768ba7c0e0747724657
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-09-25 16:16:08 +0000

    changes for MPI interface

--- kratos/gid_interface/kratos.gid/kratos-mpi.unix.bat
+++ kratos/gid_interface/kratos.gid/kratos-mpi.unix.bat
@@ -1,11 +1,11 @@
 #!/bin/bash -i
-echo hola
-echo par1 : $1
-echo "1. param: $1" > /tmp/kk.txt
-echo "2. param: $2" >> /tmp/kk.txt
-echo "3. param: $3" >> /tmp/kk.txt
-echo "4. param: $4" >> /tmp/kk.txt
-echo "5. param: $5" >> /tmp/kk.txt
+#  echo hola
+#  echo par1 : $1
+#  echo "1. param: $1" > /tmp/kk.txt
+#  echo "2. param: $2" >> /tmp/kk.txt
+#  echo "3. param: $3" >> /tmp/kk.txt
+#  echo "4. param: $4" >> /tmp/kk.txt
+#  echo "5. param: $5" >> /tmp/kk.txt
 #    OutputFile: $2/$1.info
 #    ErrorFile: $2/$1.err
 #delete previous result file
@@ -13,5 +13,7 @@ rm -f "$2/$1.post.bin"
 rm -f "$2/$1.info"
 rm -f "$2/$1.err"
 rm -f "$2/$1.flavia.dat"
+
+export OMP_NUM_THREADS=1
 
 mpirun -np $5 /usr/bin/python KratosMPI.py >"$2/$1.info" 2>"$2/$1.err"
