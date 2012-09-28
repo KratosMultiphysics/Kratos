9fea2171a1406b2d5c93731e93f29da5ffa064cb
kazem <kazem@cimne.upc.edu>
2012-09-28 16:46:49 +0000

    DC operator

--- applications/ThermoMechanicalApplication/custom_elements/SUPG_conv_diff_2d.h
+++ applications/ThermoMechanicalApplication/custom_elements/SUPG_conv_diff_2d.h
@@ -148,6 +148,7 @@ public:
     */
     void GetDofList(DofsVectorType& ElementalDofList,ProcessInfo& CurrentProcessInfo);
 
+    void GetValueOnIntegrationPoints(const Variable<double>& rVariable, std::vector<double>& rValues, const ProcessInfo& rCurrentProcessInfo);
 
 
     ///@}
@@ -230,7 +231,12 @@ protected:
      * @return tau: multiplied by Residual of the momentum equation
     */
     virtual void CalculateTau(array_1d<double, 2 >& ms_adv_vel, double& tau,const double& K, const double time, const double area, const ProcessInfo& rCurrentProcessInfo);
-
+    virtual void CalculateArtifitialViscosity(double& art_visc,  
+						      boost::numeric::ublas::bounded_matrix<double, 3, 2 > DN_DX, 
+						      array_1d<double, 2 > ms_vel_gauss,
+						      const Variable<double>& temperature,
+						      const double area,
+						      const double scaled_K);
 private:
     ///@name Static Member Variables
     ///@{
