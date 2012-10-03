988332f2878985b0fa94797a7dab796250a00e39
gerardo <gerardo@cimne.upc.edu>
2012-10-03 23:43:11 +0000

    Update .bat file to correct a bug with folders with spaces in the path

--- gid_interface/kratos.gid/kratos.win.bat
+++ gid_interface/kratos.gid/kratos.win.bat
@@ -4,15 +4,15 @@ REM basename                          = %1
 REM Project directory                 = %2
 REM Problem directory                 = %3
  
-REM OutputFile: %2\%1.info
-REM ErrorFile: %2\%1.err
+REM OutputFile: "%2\%1.info"
+REM ErrorFile: "%2\%1.err"
  
-DEL %2\%1.info
-DEL %2\%1.err
+DEL "%2\%1.info"
+DEL "%2\%1.err"
 
 REM Set the number of threads for OpenMP
 REM export OMP_NUM_THREADS=%5
 set OMP_NUM_THREADS=%5
 
 REM Run Python using the script KratosOpenMP.py
-%3\\Python27\\python.exe KratosOpenMP.py > %2\%1.info 2> %2\%1.err
+%3\\Python27\\python.exe KratosOpenMP.py > "%2\%1.info" 2> "%2\%1.err"
