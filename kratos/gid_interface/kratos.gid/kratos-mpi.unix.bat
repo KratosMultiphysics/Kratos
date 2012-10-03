33440e31802010074c21308709f89fe0868b1b6d
gerardo <gerardo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-10-03 23:43:11 +0000

    Update .bat file to correct a bug with folders with spaces in the path

--- kratos/gid_interface/kratos.gid/kratos-mpi.unix.bat
+++ kratos/gid_interface/kratos.gid/kratos-mpi.unix.bat
@@ -6,8 +6,8 @@
 #  echo "3. param: $3" >> /tmp/kk.txt
 #  echo "4. param: $4" >> /tmp/kk.txt
 #  echo "5. param: $5" >> /tmp/kk.txt
-#    OutputFile: $2/$1.info
-#    ErrorFile: $2/$1.err
+#    OutputFile: "$2/$1.info"
+#    ErrorFile: "$2/$1.err"
 #delete previous result file
 rm -f "$2/$1.post.bin"
 rm -f "$2/$1.info"
--- kratos/gid_interface/kratos.gid/kratos.unix.bat
+++ kratos/gid_interface/kratos.gid/kratos.unix.bat
@@ -1,14 +1,14 @@
 #!/bin/bash -i
-#    OutputFile: $2/$1.info
-#    ErrorFile: $2/$1.err
+# OutputFile: "$2/$1.info"
+# ErrorFile: "$2/$1.err"
 #delete previous result file 
-rm -f $2/$1.post.bin 
-rm -f $2/$1.info
-rm -f $2/$1.err
-rm -f $2/$1.flavia.dat
+rm -f "$2/$1.post.bin" 
+rm -f "$2/$1.info"
+rm -f "$2/$1.err"
+rm -f "$2/$1.flavia.dat"
 
 # Set the number of threads for OpenMP
 export OMP_NUM_THREADS=$5
 
 # Run Python using the script KratosOpenMP.py
-python KratosOpenMP.py > $2/$1.info 2> $2/$1.err
+python KratosOpenMP.py > "$2/$1.info" 2> "$2/$1.err"
