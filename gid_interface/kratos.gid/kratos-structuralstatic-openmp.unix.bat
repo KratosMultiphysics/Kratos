8bf52198d17ad0a7c27793efd81d996247765de2
gerardo <gerardo@cimne.upc.edu>
2012-10-10 09:23:19 +0000

    Add structural analysis MPI and OpenMP bat files to the repository

---
+++ gid_interface/kratos.gid/kratos-structuralstatic-openmp.unix.bat
@@ -0,0 +1,14 @@
+#!/bin/bash -i
+# OutputFile: "$2/$1.info"
+# ErrorFile: "$2/$1.err"
+#delete previous result file 
+rm -f "$2/$1.post.bin" 
+rm -f "$2/$1.info"
+rm -f "$2/$1.err"
+rm -f "$2/$1.flavia.dat"
+
+# Set the number of threads for OpenMP
+export OMP_NUM_THREADS=$5
+
+# Run Python using the script KratosOpenMPStructuralStatic.py
+python KratosOpenMPStructuralStatic.py > "$2/$1.info" 2> "$2/$1.err"
---
+++ gid_interface/kratos.gid/kratos-structuralstatic-openmp.win.bat
@@ -0,0 +1,18 @@
+REM @ECHO OFF
+REM Identification for arguments
+REM basename                          = %1
+REM Project directory                 = %2
+REM Problem directory                 = %3
+ 
+REM OutputFile: "%2\%1.info"
+REM ErrorFile: "%2\%1.err"
+ 
+DEL "%2\%1.info"
+DEL "%2\%1.err"
+
+REM Set the number of threads for OpenMP
+REM export OMP_NUM_THREADS=%5
+set OMP_NUM_THREADS=%5
+
+REM Run Python using the script KratosOpenMPStructuralStatic.py
+%3\\Python27\\python.exe KratosOpenMPStructuralStatic.py > "%2\%1.info" 2> "%2\%1.err"
