fc18ab939b9a4ea00fedcb8a537f1a69600a0d01
fmossaiby <fmossaiby@4358b7d9-91ec-4505-bf62-c3060f61107a>
2012-11-10 22:52:37 +0000

    Updating samples

--- kratos/applications/OpenCLapplication/custom_utilities/opencl_linear_solver/main.cpp
+++ kratos/applications/OpenCLapplication/custom_utilities/opencl_linear_solver/main.cpp
@@ -1,9 +1,6 @@
-#include "../opencl_linear_solver.h"
+#include "opencl_linear_solver.h"
 #include "includes/matrix_market_interface.h"
 #include "includes/ublas_interface.h"
-
-//#include "viennacl/compressed_matrix.hpp"
-//#include "viennacl/linalg/cg.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -20,31 +17,21 @@ int main(int argc, char *argv[])
     Kratos::OpenCL::DeviceGroup DeviceGroup(CL_DEVICE_TYPE_ALL, true);
     DeviceGroup.AddCLSearchPath("..");
 
-    Kratos::CompressedMatrix TA;
-    Kratos::Vector B;
+    boost::numeric::ublas::compressed_matrix <double, boost::numeric::ublas::row_major, 0, boost::numeric::ublas::unbounded_array <cl_uint>, boost::numeric::ublas::unbounded_array <double> > A;
+    boost::numeric::ublas::vector <double> B;
 
     std::cout << "Reading matrix and vector from file..." << std::endl;
 
-    Kratos::ReadMatrixMarketMatrix(argv[1], TA);
+    Kratos::ReadMatrixMarketMatrix(argv[1], A);
     Kratos::ReadMatrixMarketVector(argv[2], B);
 
-    if (TA.size1() != TA.size2() || TA.size1() != B.size())
+    if (A.size1() != A.size2() || A.size1() != B.size())
     {
         std::cout << "Inconsistent matrix / vector sizes." << std::endl;
         return -1;
     }
 
-    cl_uint Size = TA.size1();
-
-    std::cout << "Copying data..." << std::endl;
-
-    boost::numeric::ublas::compressed_matrix <double, boost::numeric::ublas::row_major, 0, boost::numeric::ublas::unbounded_array<cl_uint>, boost::numeric::ublas::unbounded_array<double> > A(TA);
-
-//	viennacl::compressed_matrix <double> VA;
-//	viennacl::vector <double> VB(Size);
-
-//	viennacl::copy(A, VA);
-//	viennacl::copy(B, VB);
+    cl_uint Size = A.size1();
 
     cl_uint A_RowIndices = DeviceGroup.CreateBuffer((Size + 1) * sizeof(cl_uint), CL_MEM_READ_ONLY);
     cl_uint A_ColumnIndices = DeviceGroup.CreateBuffer(A.nnz() * sizeof(cl_uint), CL_MEM_READ_ONLY);
@@ -64,16 +51,8 @@ int main(int argc, char *argv[])
 
     Kratos::OpenCL::LinearSolverOptimizationParameters OptimizationParameters(DeviceGroup, Size);
 
-    std::cout << "Optimizing..." << std::endl;
-
     OptimizationParameters.OptimizeInnerProd(B_Values, X_Values, T_Values);
     OptimizationParameters.OptimizeSpMV(A_RowIndices, A_ColumnIndices, A_Values, B_Values, X_Values);
-
-#ifndef CGTYPE
-
-    #define CGTYPE 1
-
-#endif
 
 #ifndef MAXIT
 
@@ -84,6 +63,12 @@ int main(int argc, char *argv[])
 #ifndef MAXERR
 
     #define MAXERR 1e-10
+
+#endif
+
+#ifndef CGTYPE
+
+    #define CGTYPE 1
 
 #endif
 
@@ -127,19 +112,5 @@ int main(int argc, char *argv[])
 
     DeviceGroup.CopyBuffer(X_Values, Kratos::OpenCL::DeviceToHost, Kratos::OpenCL::VoidPList(1, &X[0]));
 
-    /*	std::cout << "Solving the system using ViennaCL..." << std::endl;
-
-    	viennacl::linalg::cg_tag VCLSolver(1.00e-10, 1000);
-
-    	T0 = Kratos::OpenCL::Timer();
-
-    	viennacl::linalg::solve(VA, VB, VCLSolver);
-
-    	T1 = Kratos::OpenCL::Timer() - T0;
-
-    	std::cout << "Iterations: " << VCLSolver.iters() << ", error: " << VCLSolver.error() << std::endl;
-
-    	std::cout << "Solution took " << double(T1) / 1000000000 << "s." << std::endl;
-    */
     return 0;
 }
