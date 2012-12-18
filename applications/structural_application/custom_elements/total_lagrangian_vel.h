a9e1fea70cff37e1eddb5aeed5258e873ef668a7
rrossi <rrossi@cimne.upc.edu>
2012-12-18 10:58:34 +0000

    minor change to allow correct linkingÇ

--- applications/structural_application/custom_elements/total_lagrangian_vel.h
+++ applications/structural_application/custom_elements/total_lagrangian_vel.h
@@ -110,13 +110,13 @@ public:
         DofsVectorType& ElementalDofList,
         ProcessInfo& rCurrentProcessInfo);
 
-    void Calculate( const Variable<array_1d<double,3> >& rVariable,
-                    array_1d<double,3>& Output,
-                    const ProcessInfo& rCurrentProcessInfo);
-
-    void Calculate( const Variable<double>& rVariable,
-                    double& Output,
-                    const ProcessInfo& rCurrentProcessInfo);
+//     void Calculate( const Variable<array_1d<double,3> >& rVariable,
+//                     array_1d<double,3>& Output,
+//                     const ProcessInfo& rCurrentProcessInfo);
+// 
+//     void Calculate( const Variable<double>& rVariable,
+//                     double& Output,
+//                     const ProcessInfo& rCurrentProcessInfo);
 
 protected:
 
