cfd8720f26d4faad011352899fe96b89de6481d5
jirazabal <jirazabal@cimne.upc.edu>
2013-01-16 10:48:20 +0000

    Add new benchmark edgebased_fixed_press

--- applications/incompressible_fluid_application/test_examples/incompressible_benchmarks.py
+++ applications/incompressible_fluid_application/test_examples/incompressible_benchmarks.py
@@ -374,6 +374,35 @@ def Run():
 		    print "cavity_monolithic_3d example FAILED"
 
 	    os.chdir("..")
+	
+	  ################################################################
+	# edgebased fixed press test for edge based level set solver
+
+	try:
+	    Text += "edgebased fixed press test: "
+	    os.chdir("edgebased_fixed_press.gid")
+	    sys.path.append(os.getcwd())
+	except OSError:
+	    print "edgebased fixed press test: directory does not exist"
+	    Text += "FAILED: directory not found\n"
+	else:
+	    print "running the benchmark for edgebased_fixed_press test..."
+	    Msg = benchmarking.RunBenchmark("run_benchmark.py", "benchmark_reference_solution.txt")
+	    
+	    if (Msg == True):
+		    Text += "OK\n"
+		    print "edgebased_fixed_press test example succesful"
+	    else:
+		    Text += "FAILED\n"
+		    Text += Msg
+		    Text += "\n\n"
+		    print "edgebased_fixed_press test example FAILED"
+
+	    os.chdir("..")
+	
+        # Add other examples here	
+
+##        ################################################################
 
 	# Add other examples here
         ################################################################
