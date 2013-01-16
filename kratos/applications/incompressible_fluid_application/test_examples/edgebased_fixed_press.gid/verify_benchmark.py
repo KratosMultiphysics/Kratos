57683c35674b417b3c219a2b91773d3b3c2b7fd6
jirazabal <jirazabal@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-01-16 10:47:23 +0000

    Benchmark new problemtype that allows to fix variable pressure in GiD

---
+++ kratos/applications/incompressible_fluid_application/test_examples/edgebased_fixed_press.gid/verify_benchmark.py
@@ -0,0 +1,12 @@
+import sys
+kratos_benchmarking_path = '../../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
+print "running the benchmark for edgebased_fixed_press test..."
+Msg = benchmarking.RunBenchmark("run_benchmark.py", "benchmark_reference_solution.txt")
+
+if (Msg == True):
+    print "edgebased_fixed_press test example succesful"
+else:
+    print "edgebased_fixed_press test example FAILED"
