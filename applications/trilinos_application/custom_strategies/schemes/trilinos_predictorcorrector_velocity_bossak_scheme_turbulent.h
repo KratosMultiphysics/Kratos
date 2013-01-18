d769f2bfe625eb85833bbb188e28b6d443bc4dfe
jcotela <jcotela@cimne.upc.edu>
2013-01-18 13:54:04 +0000

    Changing trilinos bossak turbulent scheme to take into account periodic conditions when computing oss projections.

--- applications/trilinos_application/custom_strategies/schemes/trilinos_predictorcorrector_velocity_bossak_scheme_turbulent.h
+++ applications/trilinos_application/custom_strategies/schemes/trilinos_predictorcorrector_velocity_bossak_scheme_turbulent.h
@@ -134,19 +134,31 @@ public:
 
     TrilinosPredictorCorrectorVelocityBossakSchemeTurbulent(double NewAlphaBossak, double MoveMeshStrategy, unsigned int DomainSize):
         ResidualBasedPredictorCorrectorVelocityBossakSchemeTurbulent<TSparseSpace,TDenseSpace>(NewAlphaBossak, MoveMeshStrategy, DomainSize),
-        mImporterIsInitialized(false)
+        mImporterIsInitialized(false),
+        mrPeriodicIdVar(Kratos::Variable<int>::StaticObject())
     {
         std::cout << "using the TRILINOS velocity Bossak Time Integration Scheme (with turbulence model)" << std::endl;
     }
 
     TrilinosPredictorCorrectorVelocityBossakSchemeTurbulent(double NewAlphaBossak, double MoveMeshStrategy, unsigned int DomainSize, Process::Pointer pTurbulenceModel):
         ResidualBasedPredictorCorrectorVelocityBossakSchemeTurbulent<TSparseSpace,TDenseSpace>(NewAlphaBossak, MoveMeshStrategy, DomainSize, pTurbulenceModel),
-        mImporterIsInitialized(false)
+        mImporterIsInitialized(false),
+        mrPeriodicIdVar(Kratos::Variable<int>::StaticObject())
     {
 
         std::cout << "using the TRILINOS velocity Bossak Time Integration Scheme (with turbulence model)" << std::endl;
     }
 
+    TrilinosPredictorCorrectorVelocityBossakSchemeTurbulent(double NewAlphaBossak, double MoveMeshStrategy, unsigned int DomainSize, const Variable<int>& rPeriodicIdVar):
+        ResidualBasedPredictorCorrectorVelocityBossakSchemeTurbulent<TSparseSpace,TDenseSpace>(NewAlphaBossak, MoveMeshStrategy, DomainSize),
+        mImporterIsInitialized(false),
+        mrPeriodicIdVar(rPeriodicIdVar)
+    {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+        if (rank == 0)
+            std::cout << "using the TRILINOS velocity Bossak Time Integration Scheme (with periodic conditions)" << std::endl;
+    }
     /** Destructor.
      */
     virtual ~TrilinosPredictorCorrectorVelocityBossakSchemeTurbulent()
@@ -193,6 +205,61 @@ public:
 
         return 0;
         KRATOS_CATCH("");
+    }
+
+
+    /// Calculate OSS projections, properly taking into account periodic boundaries.
+    virtual void FinalizeNonLinIteration(ModelPart &rModelPart, TSystemMatrixType &A, TSystemVectorType &Dx, TSystemVectorType &b)
+    {
+        ProcessInfo& CurrentProcessInfo = rModelPart.GetProcessInfo();
+
+        //if orthogonal subscales are computed
+        if (CurrentProcessInfo[OSS_SWITCH] == 1.0) {
+            if (rModelPart.GetCommunicator().MyPID() == 0)
+                std::cout << "Computing OSS projections" << std::endl;
+            for (typename ModelPart::NodesContainerType::iterator ind = rModelPart.NodesBegin(); ind != rModelPart.NodesEnd(); ind++) {
+
+                noalias(ind->FastGetSolutionStepValue(ADVPROJ)) = ZeroVector(3);
+
+                ind->FastGetSolutionStepValue(DIVPROJ) = 0.0;
+
+                ind->FastGetSolutionStepValue(NODAL_AREA) = 0.0;
+
+
+            }//end of loop over nodes
+
+            //loop on nodes to compute ADVPROJ   CONVPROJ NODALAREA
+            array_1d<double, 3 > output;
+
+
+            for (typename ModelPart::ElementsContainerType::iterator elem = rModelPart.ElementsBegin(); elem != rModelPart.ElementsEnd(); elem++)
+            {
+                elem->Calculate(ADVPROJ, output, CurrentProcessInfo);
+            }
+
+            rModelPart.GetCommunicator().AssembleCurrentData(NODAL_AREA);
+            rModelPart.GetCommunicator().AssembleCurrentData(DIVPROJ);
+            rModelPart.GetCommunicator().AssembleCurrentData(ADVPROJ);
+
+
+            // Correction for periodic conditions
+            this->PeriodicConditionProjectionCorrection(rModelPart);
+
+
+            for (typename ModelPart::NodesContainerType::iterator ind = rModelPart.NodesBegin(); ind != rModelPart.NodesEnd(); ind++)
+            {
+                if (ind->FastGetSolutionStepValue(NODAL_AREA) == 0.0)
+                {
+                    ind->FastGetSolutionStepValue(NODAL_AREA) = 1.0;
+                    //KRATOS_WATCH("*********ATTENTION: NODAL AREA IS ZERRROOOO************");
+                }
+                const double Area = ind->FastGetSolutionStepValue(NODAL_AREA);
+                ind->FastGetSolutionStepValue(ADVPROJ) /= Area;
+                ind->FastGetSolutionStepValue(DIVPROJ) /= Area;
+            }
+        }
+
+
     }
 
 
@@ -339,6 +406,66 @@ protected:
         mImporterIsInitialized = true;
     }
 
+    /** On periodic boundaries, the nodal area and the values to project need to take into account contributions from elements on
+     * both sides of the boundary. This is done using the conditions and the non-historical nodal data containers as follows:\n
+     * 1- The partition that owns the PeriodicCondition adds the values on both nodes to their non-historical containers.\n
+     * 2- The non-historical containers are added across processes, transmiting the right value from the condition owner to all partitions.\n
+     * 3- The value on all periodic nodes is replaced by the one received in step 2.
+     */
+    void PeriodicConditionProjectionCorrection(ModelPart& rModelPart)
+    {
+        if (mrPeriodicIdVar.Key() != 0)
+        {
+            for (typename ModelPart::ConditionIterator itCond = rModelPart.ConditionsBegin(); itCond != rModelPart.ConditionsEnd(); itCond++ )
+            {
+                ModelPart::ConditionType::GeometryType& rGeom = itCond->GetGeometry();
+                if (rGeom.PointsNumber() == 2)
+                {
+                    Node<3>& rNode0 = rGeom[0];
+                    int Node0Pair = rNode0.FastGetSolutionStepValue(mrPeriodicIdVar);
+
+                    Node<3>& rNode1 = rGeom[1];
+                    int Node1Pair = rNode1.FastGetSolutionStepValue(mrPeriodicIdVar);
+
+                    // If the nodes are marked as a periodic pair (this is to avoid acting on two-noded conditions that are not PeriodicCondition)
+                    if ( ( static_cast<int>(rNode0.Id()) == Node1Pair ) && (static_cast<int>(rNode1.Id()) == Node0Pair ) )
+                    {
+                        double NodalArea = rNode0.FastGetSolutionStepValue(NODAL_AREA) + rNode1.FastGetSolutionStepValue(NODAL_AREA);
+                        array_1d<double,3> AdvProj = rNode0.FastGetSolutionStepValue(ADVPROJ) + rNode1.FastGetSolutionStepValue(ADVPROJ);
+                        double DivProj = rNode0.FastGetSolutionStepValue(DIVPROJ) + rNode1.FastGetSolutionStepValue(DIVPROJ);
+
+                        rNode0.GetValue(NODAL_AREA) = NodalArea;
+                        rNode0.GetValue(ADVPROJ) = AdvProj;
+                        rNode0.GetValue(DIVPROJ) = DivProj;
+
+                        rNode1.GetValue(NODAL_AREA) = NodalArea;
+                        rNode1.GetValue(ADVPROJ) = AdvProj;
+                        rNode1.GetValue(DIVPROJ) = DivProj;
+                    }
+                }
+            }
+
+            rModelPart.GetCommunicator().AssembleNonHistoricalData(NODAL_AREA);
+            rModelPart.GetCommunicator().AssembleNonHistoricalData(ADVPROJ);
+            rModelPart.GetCommunicator().AssembleNonHistoricalData(DIVPROJ);
+
+            for (typename ModelPart::NodeIterator itNode = rModelPart.NodesBegin(); itNode != rModelPart.NodesEnd(); itNode++)
+            {
+                if (itNode->GetValue(NODAL_AREA) != 0.0)
+                {
+                    itNode->FastGetSolutionStepValue(NODAL_AREA) = itNode->GetValue(NODAL_AREA);
+                    itNode->FastGetSolutionStepValue(ADVPROJ) = itNode->GetValue(ADVPROJ);
+                    itNode->FastGetSolutionStepValue(DIVPROJ) = itNode->GetValue(DIVPROJ);
+
+                    // reset for next iteration
+                    itNode->GetValue(NODAL_AREA) = 0.0;
+                    itNode->GetValue(ADVPROJ) = array_1d<double,3>(3,0.0);
+                    itNode->GetValue(DIVPROJ) = 0.0;
+                }
+            }
+        }
+    }
+
     /*@} */
     /**@name Protected  Access */
     /*@{ */
@@ -369,6 +496,8 @@ private:
     bool mImporterIsInitialized;
 
     boost::shared_ptr<Epetra_Import> mpDofImporter;
+
+    const Kratos::Variable<int>& mrPeriodicIdVar;
 
     /*@} */
     /**@name Private Operators*/
