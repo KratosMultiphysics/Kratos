51f848d998f40f4566fe74bbe05f4b3c94bc8bab
pbecker <pbecker@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-01-23 17:14:37 +0000

    reuploaded

---
+++ kratos/applications/incompressible_fluid_application/test_examples/dam2d.gid/pfem_var.py
@@ -0,0 +1,23 @@
+domain_size = 2
+
+max_time = 10.0
+max_x = 2.5
+max_y = 2.0
+max_z = 1.0
+min_x = 0.0
+min_y = 0.0
+min_z = 0.0
+use_oss = 0
+dynamic_tau = 1
+nsteps = 150
+output_Dt = .001
+min_dt = 0.05
+max_dt = 0.1
+safety_factor = .1
+SolverType = "monolithic_solver_lagrangian"
+laplacian_form = 3
+# Declare Python Variables
+
+problem_name="dam2d"
+problem_path="."
+kratos_path="../../../../"
