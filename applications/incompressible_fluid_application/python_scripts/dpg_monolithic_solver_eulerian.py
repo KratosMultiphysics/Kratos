31d4b4b274c5d67fab34bc441086ba3a332bddac
kazem <kazem@cimne.upc.edu>
2013-01-31 17:16:17 +0000

    cleaning

--- applications/incompressible_fluid_application/python_scripts/dpg_monolithic_solver_eulerian.py
+++ applications/incompressible_fluid_application/python_scripts/dpg_monolithic_solver_eulerian.py
@@ -24,9 +24,7 @@ def AddVariables(model_part):
     model_part.AddNodalSolutionStepVariable(ACCELERATION);
     model_part.AddNodalSolutionStepVariable(MESH_VELOCITY);
     model_part.AddNodalSolutionStepVariable(PRESSURE);
-    model_part.AddNodalSolutionStepVariable(AIR_PRESSURE);
     model_part.AddNodalSolutionStepVariable(IS_FLUID);
-    model_part.AddNodalSolutionStepVariable(IS_POROUS);
     model_part.AddNodalSolutionStepVariable(IS_STRUCTURE);
     model_part.AddNodalSolutionStepVariable(IS_FREE_SURFACE);
     model_part.AddNodalSolutionStepVariable(IS_INTERFACE);
@@ -34,27 +32,20 @@ def AddVariables(model_part):
     model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
     model_part.AddNodalSolutionStepVariable(VISCOSITY);
     model_part.AddNodalSolutionStepVariable(DENSITY);
-    model_part.AddNodalSolutionStepVariable(POROSITY);
-    model_part.AddNodalSolutionStepVariable(DENSITY_AIR);
-    model_part.AddNodalSolutionStepVariable(AIR_SOUND_VELOCITY);
-    model_part.AddNodalSolutionStepVariable(SOUND_VELOCITY);
     model_part.AddNodalSolutionStepVariable(BODY_FORCE);
     model_part.AddNodalSolutionStepVariable(NODAL_AREA);
     model_part.AddNodalSolutionStepVariable(NODAL_H);
-    model_part.AddNodalSolutionStepVariable(ADVPROJ);
-    model_part.AddNodalSolutionStepVariable(DIVPROJ);
     model_part.AddNodalSolutionStepVariable(THAWONE);
     model_part.AddNodalSolutionStepVariable(THAWTWO); 
-    model_part.AddNodalSolutionStepVariable(REACTION); 
-    model_part.AddNodalSolutionStepVariable(REACTION_WATER_PRESSURE);
-    model_part.AddNodalSolutionStepVariable(EXTERNAL_PRESSURE);
-    model_part.AddNodalSolutionStepVariable(WATER_PRESSURE);
-    model_part.AddNodalSolutionStepVariable(AIR_PRESSURE_DT);
-    model_part.AddNodalSolutionStepVariable(ARRHENIUS); 
     model_part.AddNodalSolutionStepVariable(FLAG_VARIABLE);
     model_part.AddNodalSolutionStepVariable(NORMAL);
     model_part.AddNodalSolutionStepVariable(DISTANCE);
     model_part.AddNodalSolutionStepVariable(IS_SLIP);
+    model_part.AddNodalSolutionStepVariable(PRESSURES)
+    model_part.AddNodalSolutionStepVariable(VELOCITIES)
+    model_part.AddNodalSolutionStepVariable(MATERIAL)
+    model_part.AddNodalSolutionStepVariable(LAST_AIR)
+    model_part.AddNodalSolutionStepVariable(NODAL_MASS)   
     
     #variables needed for the distance solver
     levelset_solver.AddVariables(model_part,distance_settings)
@@ -64,11 +55,10 @@ def AddVariables(model_part):
 def AddDofs(model_part):
     for node in model_part.Nodes:
         #adding dofs
-        node.AddDof(VELOCITY_X,REACTION_X);
-        node.AddDof(VELOCITY_Y,REACTION_Y);
-        node.AddDof(VELOCITY_Z,REACTION_Z);
-        node.AddDof(PRESSURE,REACTION_WATER_PRESSURE);
-	node.AddDof(AIR_PRESSURE,REACTION_AIR_PRESSURE);
+        node.AddDof(VELOCITY_X);
+        node.AddDof(VELOCITY_Y);
+        node.AddDof(VELOCITY_Z);
+        node.AddDof(PRESSURE);
 	
     levelset_solver.AddDofs(model_part,distance_settings)        
     print "dofs for the monolithic solver added correctly"
@@ -112,9 +102,6 @@ class MonolithicSolver:
         self.abs_vel_tol = 1e-7
         self.rel_pres_tol = 1e-5
         self.abs_pres_tol = 1e-7
-
-       # self.conv_criteria = UPCriteria(1e-12,1e-14,1e-15,1e-17)
-        #self.model_part.ProcessInfo.SetValue(DYNAMIC_TAU, 0.001);
 
         self.dynamic_tau_levelset = 0.01
         self.dynamic_tau_fluid = 1.0        
@@ -204,8 +191,6 @@ class MonolithicSolver:
         #creating the solution strategy
         self.conv_criteria = VelPrCriteria(self.rel_vel_tol,self.abs_vel_tol,\
                                            self.rel_pres_tol,self.abs_pres_tol)
-##        self.conv_criteria = UPCriteria(self.rel_vel_tol,self.abs_vel_tol,
-##                                        self.rel_pres_tol,self.abs_pres_tol)
 	builder_and_solver = ResidualBasedBlockBuilderAndSolver(self.linear_solver)
         #self.solver = ResidualBasedNewtonRaphsonStrategy(self.model_part,self.time_scheme,self.linear_solver,self.conv_criteria,builder_and_solver,self.max_iter,self.CalculateReactionFlag, self.ReformDofSetAtEachStep,self.MoveMeshFlag)   
 
@@ -214,9 +199,8 @@ class MonolithicSolver:
         print ">>>>>>>>>>>>>>>", self.oss_switch
         self.model_part.ProcessInfo.SetValue(DYNAMIC_TAU, self.dynamic_tau_fluid);
         self.model_part.ProcessInfo.SetValue(OSS_SWITCH, self.oss_switch );
-        self.model_part.ProcessInfo.SetValue(M, self.regularization_coef );
 
-##        print "Initialization monolithic solver finished"
+
         # LEvel_set solver initialization
         self.level_set_solver.dynamic_tau =self.dynamic_tau_levelset
         self.redistance_utils.CalculateDistances(self.model_part,DISTANCE,NODAL_AREA,self.max_levels,self.max_distance)
@@ -224,17 +208,6 @@ class MonolithicSolver:
 
         self.ApplyFluidProperties()
         
-        # assigne to 1 density(AUX_INDEX) and specific_heat and to 0 conductivitu for the level_set solver
-        #for node in self.model_part.Nodes:
-            #node.SetSolutionStepValue(AUX_INDEX,0,1.0) 
-            #node.SetSolutionStepValue(ARRHENIUSAUX,0,0.0)            
-            #node.SetSolutionStepValue(SPECIFIC_HEAT,0,1.0)
-            
-        ############### saving inlet nodes
-        #self.inlet_nodes = []
-	#for node in self.model_part.Nodes:
-	  #if(node.IsFixed(DISTANCE)):
-	    #self.inlet_nodes.append(node);
 
         self.next_redistance = self.redistance_frequency
         ###         ###         ###        
@@ -249,14 +222,6 @@ class MonolithicSolver:
 	    (FindConditionsNeighboursProcess(self.model_part, 3, 20)).Execute()	  
             self.normal_util = NormalCalculationUtils()
             self.normal_util.CalculateOnSimplex(self.model_part,self.domain_size,IS_STRUCTURE,0.0,35.0)#,0.0,35.0
-            
-            #for node in self.model_part.Nodes:
-		#if (node.GetSolutionStepValue(IS_SLIP) > 0.0):
-		   #node.SetValue(IS_STRUCTURE,1.0)
-		#else:
-		   #node.SetValue(IS_STRUCTURE,0.0)		  
-		   #node.SetSolutionStepValue(IS_STRUCTURE,0,1.0)
-		   #node.Fix(IS_STRUCTURE)
 
 	############## saving inlet nodes
 	self.inlet_nodes = []
@@ -264,33 +229,19 @@ class MonolithicSolver:
 	  if(cond.GetValue(IS_INLET) > 0):
 	    for node in cond.GetNodes():
 	      self.inlet_nodes.append(node);	      
-		   
-    ################################################################
-    ################################################################
-    def WettenNodes(self):
-      for node in self.inlet_nodes:
-	if(node.GetSolutionStepValue(DISTANCE) > 0):
-	  node.SetSolutionStepValue(DISTANCE,0,-1.0e-3); 
+
 	  
     #######################################################################   
-    def DoRedistance(self):
-	#self.WettenNodes()
-	
+    def DoRedistance(self):	
 	#redistance if required
-        print "beginning recalculation of distances"
         self.redistance_utils.CalculateDistances(self.model_part,DISTANCE,NODAL_AREA,self.max_levels,self.max_distance)
-
-	print "finished recalculation of distances"    
     
      #######################################################################   
     def ConvectDistance(self):
-        print "beginning convection step for the distance function"
         self.level_set_model_part.ProcessInfo = self.model_part.ProcessInfo
         (self.level_set_model_part.ProcessInfo).SetValue(CONVECTION_DIFFUSION_SETTINGS,distance_settings)
         (self.level_set_model_part.ProcessInfo).SetValue(DYNAMIC_TAU,self.dynamic_tau_levelset)#self.dynamic_tau
-	#self.WettenNodes()
 	(self.level_set_solver).Solve()
-        print "finished convection step for the distance function"     
      #######################################################################                 
       #######################################################################      
     def Solve(self):
@@ -300,12 +251,6 @@ class MonolithicSolver:
 	    node.SetSolutionStepValue(DISTANCE,1,node.GetSolutionStepValue(DISTANCE))
 	  self.divergence_clearance_performed = True    
 
-        ##recompute distance function as needed
-        #if(self.internal_step_counter >= self.next_redistance):
-	  #self.DoRedistance()
-	  #for node in self.model_part.Nodes:	    
-	    #node.SetSolutionStepValue(DISTANCE,1,node.GetSolutionStepValue(DISTANCE))
-	  #self.next_redistance = self.internal_step_counter + self.redistance_frequency
 
 	#convect distance function
         self.ConvectDistance()
@@ -313,17 +258,13 @@ class MonolithicSolver:
         if(self.internal_step_counter >= self.next_redistance):
 	  self.DoRedistance()
 	  self.next_redistance = self.internal_step_counter + self.redistance_frequency	  
-
 	self.ApplyFluidProperties()
         #Recompute normals if necessary
-	if(self.ReformDofSetAtEachStep == True):
-           if self.use_slip_conditions == True:
-	      self.normal_util.CalculateOnSimplex(self.model_part,self.domain_size,IS_STRUCTURE,0.0,35.0)#,0.0,35.0
-        
+##	if(self.ReformDofSetAtEachStep == True):
+##           if self.use_slip_conditions == True:
+##	      self.normal_util.CalculateOnSimplex(self.model_part,self.domain_size,IS_STRUCTURE,0.0,35.0)#,0.0,35.0     
         (self.solver).Solve()
-##        print "solving step monolithic solver finished"
         self.internal_step_counter += 1       
-
     #######################################################################   
     def SetEchoLevel(self,level):
         (self.solver).SetEchoLevel(level)
