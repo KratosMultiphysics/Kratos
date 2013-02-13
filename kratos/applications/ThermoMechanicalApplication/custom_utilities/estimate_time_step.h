6868f25fba0c63051247ed1081e2026c18c32451
kazem <kazem@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-02-13 17:27:36 +0000

    CFL_distance

--- kratos/applications/ThermoMechanicalApplication/custom_utilities/estimate_time_step.h
+++ kratos/applications/ThermoMechanicalApplication/custom_utilities/estimate_time_step.h
@@ -71,13 +71,15 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "thermo_mechanical_application.h"
 // #include "custom_conditions/environment_contact.h"
 //#include "includes/variables.h"
+#include "utilities/openmp_utils.h"
+
 
 
 
 namespace Kratos
 {
  	
-
+	template<unsigned int TDim>
 	class EstimateTimeStep
 	{
 	public:
@@ -85,54 +87,137 @@ namespace Kratos
 		//**********************************************************************************************
 		//**********************************************************************************************
 		//
-		//template<unsigned int TDim>
-		double ComputeDt(ModelPart& ThisModelPart, const double dt_min, const double dt_max  )
+
+		double ComputeDt(ModelPart& ThisModelPart, const double dist_max, const double CFL, const double dt_min ,const double dt_max  )
 		{			
 		  KRATOS_TRY
 
-		  array_1d<double, 3 > dx, dv;
-		  double deltatime = dt_max;
-		  double dvside, lside;
+        const unsigned int NumNodes = TDim +1;
 
-		  for (ModelPart::ElementsContainerType::iterator i = ThisModelPart.ElementsBegin();
-			  i != ThisModelPart.ElementsEnd(); i++)
-		  {
-		      //calculating shape functions values
-		      Geometry< Node < 3 > >& geom = i->GetGeometry();
+        int NumThreads = OpenMPUtils::GetNumThreads();
+        OpenMPUtils::PartitionVector ElementPartition;
+        OpenMPUtils::DivideInPartitions(ThisModelPart.NumberOfElements(),NumThreads,ElementPartition);
 
-		      for (unsigned int i1 = 0; i1 < geom.size() - 1; i1++)
-		      {
-			  for (unsigned int i2 = i1 + 1; i2 < geom.size(); i2++)
-			  {
-			      dx[0] = geom[i2].X() - geom[i1].X();
-			      dx[1] = geom[i2].Y() - geom[i1].Y();
-			      dx[2] = geom[i2].Z() - geom[i1].Z();
+        std::vector<double> MaxProj(NumThreads,0.0);
 
-			      lside = inner_prod(dx, dx);
+        #pragma omp parallel shared(MaxProj)
+        {
+            int k = OpenMPUtils::ThisThread();
+            ModelPart::ElementIterator ElemBegin = ThisModelPart.ElementsBegin() + ElementPartition[k];
+            ModelPart::ElementIterator ElemEnd = ThisModelPart.ElementsBegin() + ElementPartition[k+1];
 
-			      noalias(dv) = geom[i2].FastGetSolutionStepValue(VELOCITY);
-			      noalias(dv) -= geom[i1].FastGetSolutionStepValue(VELOCITY);
+            double& rMaxProj = MaxProj[k];
 
-			      dvside = inner_prod(dx, dv);
+            double Area;
+            array_1d<double, NumNodes> N;
+            boost::numeric::ublas::bounded_matrix<double, NumNodes, TDim> DN_DX;
 
-			      double dt;
-			      if (dvside < 0.0) //otherwise the side is "getting bigger" so the time step has not to be diminished
-			      {
-				  dt = fabs(lside / dvside);
-				  if (dt < deltatime) deltatime = dt;
-			      }
+            for( ModelPart::ElementIterator itElem = ElemBegin; itElem != ElemEnd; ++itElem)
+            {
+                // Get the element's geometric parameters
+                Geometry< Node<3> >& rGeom = itElem->GetGeometry();
+			    double ele_dist = 0.0;
+			    for (unsigned int kk = 0; kk < rGeom.size(); kk++){
+				     double dist = rGeom[kk].FastGetSolutionStepValue(DISTANCE);
+				  ele_dist +=  fabs(dist);
+			    }
 
+			    if(ele_dist <= dist_max){
+                GeometryUtils::CalculateGeometryData(rGeom, DN_DX, N, Area);
+
+                // Elemental Velocity
+                array_1d<double,3> ElementVel = N[0]*itElem->GetGeometry()[0].FastGetSolutionStepValue(VELOCITY);
+                for (unsigned int i = 1; i < NumNodes; ++i)
+                    ElementVel += N[i]*rGeom[i].FastGetSolutionStepValue(VELOCITY);
+
+                // Velocity norm
+                double VelNorm = ElementVel[0]*ElementVel[0];
+                for (unsigned int d = 1; d < TDim; ++d)
+                    VelNorm += ElementVel[d]*ElementVel[d];
+                VelNorm = sqrt(VelNorm);
+
+                // Maximum element size along the direction of velocity
+                for (unsigned int i = 0; i < NumNodes; ++i)
+                {
+                    double Proj = 0.0;
+                    for (unsigned int d = 0; d < TDim; ++d)
+                        Proj += ElementVel[d]*DN_DX(i,d);
+                    Proj = fabs(Proj);
+                    if (Proj > rMaxProj) rMaxProj = Proj;
+                }
 			  }
-		      }
-		  }
+            }
+        }
 
-		  if (deltatime < dt_min)
-		  {
-		      std::cout << "ATTENTION dt_min is being used" << std::endl;
-		      deltatime = dt_min;
-		  }
+        // Obtain the maximum projected element size (compare thread results)
+        double Max = 0.0;
+        for (int k = 0; k < NumThreads; ++k)
+            if (Max < MaxProj[k]) Max = MaxProj[k];
 
-		  return deltatime;
+        // Dt to obtain desired CFL
+        double dt = CFL / Max;
+        if(dt > dt_max)
+            dt = dt_max;
+		else if(dt < dt_min)
+			dt = dt_min;
+
+        //perform mpi sync if needed
+        double global_dt = dt;
+        ThisModelPart.GetCommunicator().MinAll(global_dt);
+        dt = global_dt;
+
+        return dt;
+
+		  //array_1d<double, 3 > dx, dv;
+		  //double deltatime = dt_max;
+		  //double dvside, lside;
+
+		  //for (ModelPart::ElementsContainerType::iterator i = ThisModelPart.ElementsBegin();
+			 // i != ThisModelPart.ElementsEnd(); i++)
+		  //{
+		  //    //calculating shape functions values
+		  //    Geometry< Node < 3 > >& geom = i->GetGeometry();
+			 // double ele_dist = 0.0;
+			 // for (unsigned int kk = 0; kk < geom.size(); kk++){
+				//     double dist = geom[kk].FastGetSolutionStepValue(DISTANCE);
+				//  ele_dist +=  fabs(dist);
+			 // }
+
+			 // if(ele_dist <= dist_max){
+		  //    for (unsigned int i1 = 0; i1 < geom.size() - 1; i1++)
+		  //    {
+			 // for (unsigned int i2 = i1 + 1; i2 < geom.size(); i2++)
+			 // {
+			 //     dx[0] = geom[i2].X() - geom[i1].X();
+			 //     dx[1] = geom[i2].Y() - geom[i1].Y();
+			 //     dx[2] = geom[i2].Z() - geom[i1].Z();
+
+			 //     lside = inner_prod(dx, dx);
+
+			 //     noalias(dv) = geom[i2].FastGetSolutionStepValue(VELOCITY);
+			 //     noalias(dv) -= geom[i1].FastGetSolutionStepValue(VELOCITY);
+
+			 //     dvside = inner_prod(dx, dv);
+
+			 //     double dt;
+			 //     if (dvside < 0.0) //otherwise the side is "getting bigger" so the time step has not to be diminished
+			 //     {
+				//  dt = fabs(lside / dvside);
+				//  if (dt < deltatime) deltatime = dt;
+			 //     }
+
+			 // }
+		  //    }
+			 //}
+		  //}
+
+		  //if (deltatime < dt_min)
+		  //{
+		  //    std::cout << "ATTENTION dt_min is being used" << std::endl;
+		  //    deltatime = dt_min;
+		  //}
+
+		  //return deltatime;
 
 		  KRATOS_CATCH("")	
 		}
