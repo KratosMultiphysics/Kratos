38caf4cff90a9383814942c42da5bd9558a38faa
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-02-13 14:49:26 +0000

    Adding interface preserving distance calculator

--- kratos/kratos/utilities/body_distance_calculation_utils.h
+++ kratos/kratos/utilities/body_distance_calculation_utils.h
@@ -210,7 +210,7 @@ public:
             }
 
         }
-        KRATOS_WATCH(elements_to_solve.size());
+        //KRATOS_WATCH(elements_to_solve.size());
 
         //this is the "total" solution loop
         boost::numeric::ublas::bounded_matrix<double, TDim + 1, TDim> DN_DX;
