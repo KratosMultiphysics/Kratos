a482d77122a23c8b8e07fc9e8b9a4007c99bcf34
kazem <kazem@cimne.upc.edu>
2013-02-27 19:56:19 +0000

    volume correction

--- applications/ThermoMechanicalApplication/custom_utilities/biphasic_filling_utilities.h
+++ applications/ThermoMechanicalApplication/custom_utilities/biphasic_filling_utilities.h
@@ -66,6 +66,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "includes/model_part.h"
 #include "includes/node.h"
 #include "utilities/geometry_utilities.h"
+#include "utilities/enrichment_utilities.h"
 //#include "geometries/tetrahedra_3d_4.h"
 #include "geometries/point.h"
 #include "thermo_mechanical_application.h"
@@ -231,6 +232,70 @@ class BiphasicFillingUtilities
 		  KRATOS_CATCH("")
 		}
 	//**********************************************************************************************
+	//**********************************************************************************************
+    void VolumeCorrection(ModelPart& ThisModelPart, const double Net_volume)
+	    {	
+	      KRATOS_TRY;	
+	      
+	      double wet_volume = 0.0;
+	      double cutted_area = 0.0;
+	      int node_size = ThisModelPart.Nodes().size();	      
+// #pragma omp parallel for firstprivate(node_size) reduction(+:wet_volume,cutted_area )
+// 		for (int ii = 0; ii < node_size; ii++)
+// 		{
+// 		  ModelPart::NodesContainerType::iterator it = ThisModelPart.NodesBegin() + ii;
+// 		  
+// 		  wet_volume += it->FastGetSolutionStepValue(WET_VOLUME);
+// 		  cutted_area += it->FastGetSolutionStepValue(CUTTED_AREA);
+// 		}
+		
+		ComputeWetVolumeAndCuttedArea(ThisModelPart, wet_volume, cutted_area);
+		
+		double volume_difference = fabs(Net_volume) - wet_volume;
+		volume_difference /= cutted_area;
+
+#pragma omp parallel for firstprivate(node_size)		
+		for (int ii = 0; ii < node_size; ii++)
+		{
+		  ModelPart::NodesContainerType::iterator it = ThisModelPart.NodesBegin() + ii;
+		  
+		  it->FastGetSolutionStepValue(DISTANCE) -= volume_difference;
+		  
+		}
+std::cout << "Volume Correction " << " Net volume: "<< fabs(Net_volume) << " wet volume: " << wet_volume << " percent: "<< wet_volume/fabs(Net_volume)<< " Area: "<< cutted_area << std::endl;
+	      KRATOS_CATCH("")
+	    }
+	//**********************************************************************************************
+	//**********************************************************************************************
+    void ComputeNetInletVolume(ModelPart& ThisModelPart)
+	    {	
+	      KRATOS_TRY;
+	      double net_input = 0.0;	      
+	      int node_size = ThisModelPart.Nodes().size();	      
+#pragma omp parallel for firstprivate(node_size) reduction(+:net_input)
+		for (int ii = 0; ii < node_size; ii++)
+		{
+		  ModelPart::NodesContainerType::iterator it = ThisModelPart.NodesBegin() + ii;
+		  double str_flag = it->GetValue(IS_STRUCTURE);
+		  double slip_flag = it->GetSolutionStepValue(IS_SLIP);
+		  double distance = it->GetSolutionStepValue(DISTANCE);
+		  
+		  if ( (str_flag != 0.0 || slip_flag == 0.0) && distance < 0.0 ){
+		  const array_1d<double, 3> vel = it->FastGetSolutionStepValue(VELOCITY);
+		  const array_1d<double, 3> normal = it->FastGetSolutionStepValue(NORMAL);
+		  
+		  net_input += inner_prod(vel,normal);
+		  }
+		}
+		
+               ProcessInfo& CurrentProcessInfo = ThisModelPart.GetProcessInfo();
+	       const double delta_t = CurrentProcessInfo[DELTA_TIME];
+               double& net_volume = CurrentProcessInfo[NET_INPUT_MATERIAL];
+	       net_volume += (net_input*delta_t);
+	      
+	      KRATOS_CATCH("")
+	    }	      
+	//**********************************************************************************************
 	//**********************************************************************************************		  
   private:
  	
@@ -238,7 +303,7 @@ class BiphasicFillingUtilities
 	{
         int elem_size = ThisModelPart.Elements().size();
 
-        #pragma omp parallel for firstprivate(elem_size)
+        #pragma omp parallel for firstprivate(elem_size) 
         for(int ii = 0; ii<elem_size; ii++)
          {
             PointerVector< Element>::iterator iel=ThisModelPart.ElementsBegin()+ii;
@@ -257,6 +322,67 @@ class BiphasicFillingUtilities
 
 		}
 	}
+	//**********************************************************************************************
+	//**********************************************************************************************	
+	void ComputeWetVolumeAndCuttedArea(ModelPart& ThisModelPart, double& wet_volume, double& cutted_area)
+	{
+	   KRATOS_TRY;
+	    int elem_size = ThisModelPart.Elements().size();
+	    double wetvol = 0.0;
+	    double cutare = 0.0;
+
+	    #pragma omp parallel for firstprivate(elem_size) reduction(+:wetvol,cutare)
+	    for(int ii = 0; ii<elem_size; ii++)
+	    {
+	      PointerVector< Element>::iterator iel=ThisModelPart.ElementsBegin()+ii;	  
+
+	      // Calculate this element's geometric parameters
+	      double Area;
+	      array_1d<double, 4> N;
+	      boost::numeric::ublas::bounded_matrix<double, 4, 3> DN_DX;
+	      GeometryUtils::CalculateGeometryData(iel->GetGeometry(), DN_DX, N, Area);
+	      //get position of the cut surface
+	      Vector distances(4);
+	      Matrix Nenriched(6, 1);
+	      Vector volumes(6);
+	      Matrix coords(4, 3);
+	      Matrix Ngauss(6, 4);
+	      Vector signs(6);
+	      std::vector< Matrix > gauss_gradients(6);
+	      //fill coordinates
+	      for (unsigned int i = 0; i < 4; i++)
+	      {
+		  const array_1d<double, 3 > & xyz = iel->GetGeometry()[i].Coordinates();
+		  volumes[i] = 0.0;
+		  distances[i] = iel->GetGeometry()[i].FastGetSolutionStepValue(DISTANCE);
+		  for (unsigned int j = 0; j < 3; j++)
+		      coords(i, j) = xyz[j];
+	      }
+	      for (unsigned int i = 0; i < 6; i++)
+		  gauss_gradients[i].resize(1, 3, false);
+	      unsigned int ndivisions = EnrichmentUtilities::CalculateTetrahedraEnrichedShapeFuncions(coords, DN_DX, distances, volumes, Ngauss, signs, gauss_gradients, Nenriched);      
+	  
+	      if(ndivisions == 1){		
+		if( signs[0] < 0.0)
+		    wetvol += volumes[0];}	      
+	      else{
+		double ele_wet_volume=0.0;
+		for (unsigned int kk = 0; kk < ndivisions; kk++)
+		{
+		  if( signs[kk]<0.0 )
+		    ele_wet_volume += volumes[kk];		  
+		}
+		wetvol += ele_wet_volume;
+	        cutare += 7.205621731 * pow(ele_wet_volume,0.666666666667); // equilateral tetrahedraon is considered
+	      }				
+	    }
+	    
+	    wet_volume = wetvol;
+	    cutted_area = cutare;
+	    
+	   KRATOS_CATCH("")	  
+	}
+
 
  };
 
