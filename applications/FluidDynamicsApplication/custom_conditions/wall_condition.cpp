366ac961f3947f8c39e2cc145857af0ecccc55f4
rrossi <rrossi@cimne.upc.edu>
2013-04-15 16:48:52 +0000

    improved the outler condition for the case of outlet very close to the figure. Corrected an error with the normal wrongly oriented towards the inside of the domain in 2D

--- applications/FluidDynamicsApplication/custom_conditions/wall_condition.cpp
+++ applications/FluidDynamicsApplication/custom_conditions/wall_condition.cpp
@@ -122,4 +122,35 @@ void WallCondition<3,3>::GetDofList(DofsVectorType& rElementalDofList,
     }
 }
 
+template <>
+ void WallCondition<2,2>::CalculateNormal(array_1d<double,3>& An)
+    {
+        Geometry<Node<3> >& pGeometry = this->GetGeometry();
+
+        An[0] =   pGeometry[1].Y() - pGeometry[0].Y();
+        An[1] = - (pGeometry[1].X() - pGeometry[0].X());
+        An[2] =    0.00;
+
+    }
+
+template <>
+void WallCondition<3,3>::CalculateNormal(array_1d<double,3>& An )
+    {
+        Geometry<Node<3> >& pGeometry = this->GetGeometry();
+
+	array_1d<double,3> v1,v2;
+        v1[0] = pGeometry[1].X() - pGeometry[0].X();
+        v1[1] = pGeometry[1].Y() - pGeometry[0].Y();
+        v1[2] = pGeometry[1].Z() - pGeometry[0].Z();
+
+        v2[0] = pGeometry[2].X() - pGeometry[0].X();
+        v2[1] = pGeometry[2].Y() - pGeometry[0].Y();
+        v2[2] = pGeometry[2].Z() - pGeometry[0].Z();
+
+        MathUtils<double>::CrossProduct(An,v1,v2);
+        An *= 0.5;
+
+        // 				noalias((it)->GetValue(NORMAL)) = An;
+    }
+
 } // namespace Kratos
