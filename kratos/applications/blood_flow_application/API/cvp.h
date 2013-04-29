77263a9db3406d3a21de24c55a523a2815280bc8
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-04-29 13:38:57 +0000

    adding -h for cvp

---
+++ kratos/applications/blood_flow_application/API/cvp.h
@@ -0,0 +1,10 @@
+#if !defined(CVP_H_INCLUDED )
+#define  CVP_H_INCLUDED
+
+int solve(
+	std::string kratos_path,
+	std::string model_1d_name, 
+	std::string model_3d_name
+	);
+	
+#endif //CVP_H_INCLUDED  defined 
