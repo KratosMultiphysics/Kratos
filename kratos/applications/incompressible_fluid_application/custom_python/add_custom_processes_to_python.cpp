43abb938ccb901e0c4896684de00eaa8449a0d36
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-05-02 15:37:30 +0000

    Adding AirEntrapmentRecognitionProcess

--- kratos/applications/incompressible_fluid_application/custom_python/add_custom_processes_to_python.cpp
+++ kratos/applications/incompressible_fluid_application/custom_python/add_custom_processes_to_python.cpp
@@ -80,7 +80,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "custom_processes/explicit_dt.h"
 #include "custom_processes/assign_h_by_distance_process.h" 
 #include "custom_processes/copy_to_vulcan_post_variables_process.h"
-#include "custom_processes\air_entrapment_recognition_process.h"
+#include "custom_processes/air_entrapment_recognition_process.h"
 
 #include "includes/node.h"
 
