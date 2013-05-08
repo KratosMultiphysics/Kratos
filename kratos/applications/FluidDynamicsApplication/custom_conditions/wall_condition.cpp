76047aca344055b4493792a8f5a4dc83aa1f1191
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-05-08 14:46:17 +0000

    modification to integrate by parts the mass conservation equation - after this change it REQUIRES the skin to be generated correctly

--- kratos/applications/FluidDynamicsApplication/custom_conditions/wall_condition.cpp
+++ kratos/applications/FluidDynamicsApplication/custom_conditions/wall_condition.cpp
@@ -13,7 +13,8 @@ template <>
 void WallCondition<2,2>::EquationIdVector(EquationIdVectorType& rResult,
                                                     ProcessInfo& rCurrentProcessInfo)
 {
-    if ( rCurrentProcessInfo[FRACTIONAL_STEP] == 1 )
+    int step = rCurrentProcessInfo[FRACTIONAL_STEP];
+    if ( step == 1 )
     {
         const unsigned int NumNodes = 2;
         const unsigned int LocalSize = 4;
@@ -26,6 +27,20 @@ void WallCondition<2,2>::EquationIdVector(EquationIdVectorType& rResult,
         {
             rResult[LocalIndex++] = this->GetGeometry()[iNode].GetDof(VELOCITY_X).EquationId();
             rResult[LocalIndex++] = this->GetGeometry()[iNode].GetDof(VELOCITY_Y).EquationId();
+        }
+    }
+	else if(step == 5)
+    {
+        const SizeType NumNodes = 2;
+        const SizeType LocalSize = 2;
+        unsigned int LocalIndex = 0;
+
+        if (rResult.size() != LocalSize)
+            rResult.resize(LocalSize, false);
+
+        for (unsigned int iNode = 0; iNode < NumNodes; ++iNode)
+        {
+            rResult[LocalIndex++] = this->GetGeometry()[iNode].GetDof(PRESSURE).EquationId();
         }
     }
     else
@@ -41,7 +56,8 @@ template <>
 void WallCondition<3,3>::EquationIdVector(EquationIdVectorType& rResult,
                                                     ProcessInfo& rCurrentProcessInfo)
 {
-    if ( rCurrentProcessInfo[FRACTIONAL_STEP] == 1 )
+    int step = rCurrentProcessInfo[FRACTIONAL_STEP];
+    if ( step == 1 )
     {
         const SizeType NumNodes = 3;
         const SizeType LocalSize = 9;
@@ -57,6 +73,20 @@ void WallCondition<3,3>::EquationIdVector(EquationIdVectorType& rResult,
             rResult[LocalIndex++] = this->GetGeometry()[iNode].GetDof(VELOCITY_Z).EquationId();
         }
     }
+	else if(step == 5)
+    {
+        const SizeType NumNodes = 3;
+        const SizeType LocalSize = 3;
+        unsigned int LocalIndex = 0;
+
+        if (rResult.size() != LocalSize)
+            rResult.resize(LocalSize, false);
+
+        for (unsigned int iNode = 0; iNode < NumNodes; ++iNode)
+        {
+            rResult[LocalIndex++] = this->GetGeometry()[iNode].GetDof(PRESSURE).EquationId();
+        }
+    }
     else
     {
         rResult.resize(0,false);
@@ -70,7 +100,8 @@ template <>
 void WallCondition<2,2>::GetDofList(DofsVectorType& rElementalDofList,
                                               ProcessInfo& rCurrentProcessInfo)
 {
-    if ( rCurrentProcessInfo[FRACTIONAL_STEP] == 1 )
+ 	int step = rCurrentProcessInfo[FRACTIONAL_STEP];
+    if ( step == 1 )
     {
         const SizeType NumNodes = 2;
         const SizeType LocalSize = 4;
@@ -86,6 +117,22 @@ void WallCondition<2,2>::GetDofList(DofsVectorType& rElementalDofList,
             rElementalDofList[LocalIndex++] = this->GetGeometry()[iNode].pGetDof(VELOCITY_Y);
         }
     }
+	else if ( step == 5 )
+    {
+		const SizeType NumNodes = 2;
+        const SizeType LocalSize = 2;
+
+        if (rElementalDofList.size() != LocalSize)
+            rElementalDofList.resize(LocalSize);
+
+        unsigned int LocalIndex = 0;
+
+        for (unsigned int iNode = 0; iNode < NumNodes; ++iNode)
+        {
+            rElementalDofList[LocalIndex++] = this->GetGeometry()[iNode].pGetDof(PRESSURE);
+        }
+	}
+	
     else
     {
         rElementalDofList.resize(0);
@@ -99,7 +146,8 @@ template <>
 void WallCondition<3,3>::GetDofList(DofsVectorType& rElementalDofList,
                                     ProcessInfo& rCurrentProcessInfo)
 {
-    if ( rCurrentProcessInfo[FRACTIONAL_STEP] == 1 )
+	int step = rCurrentProcessInfo[FRACTIONAL_STEP];
+    if ( step == 1 )
     {
         const SizeType NumNodes = 3;
         const SizeType LocalSize = 9;
@@ -116,6 +164,21 @@ void WallCondition<3,3>::GetDofList(DofsVectorType& rElementalDofList,
             rElementalDofList[LocalIndex++] = this->GetGeometry()[iNode].pGetDof(VELOCITY_Z);
         }
     }
+	else if ( step == 5 )
+    {
+		const SizeType NumNodes = 3;
+        const SizeType LocalSize = 3;
+
+        if (rElementalDofList.size() != LocalSize)
+            rElementalDofList.resize(LocalSize);
+
+        unsigned int LocalIndex = 0;
+
+        for (unsigned int iNode = 0; iNode < NumNodes; ++iNode)
+        {
+            rElementalDofList[LocalIndex++] = this->GetGeometry()[iNode].pGetDof(PRESSURE);
+        }
+	}
     else
     {
         rElementalDofList.resize(0);
@@ -148,7 +211,7 @@ void WallCondition<3,3>::CalculateNormal(array_1d<double,3>& An )
         v2[2] = pGeometry[2].Z() - pGeometry[0].Z();
 
         MathUtils<double>::CrossProduct(An,v1,v2);
-        An *= 0.5;
+        An *= -0.5;
 
         // 				noalias((it)->GetValue(NORMAL)) = An;
     }
