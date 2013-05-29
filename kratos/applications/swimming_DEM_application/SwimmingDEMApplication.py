acb321c23001b67239bc31b74fb13f6daa9f4814
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-05-29 15:41:34 +0000

    swimming_application

---
+++ kratos/applications/swimming_DEM_application/SwimmingDEMApplication.py
@@ -0,0 +1,10 @@
+from KratosSwimmingDEMApplication import *
+application = KratosSwimmingDEMApplication()
+application_name = "KratosSwimmingDEMApplication"
+application_folder = "swimming_DEM_application"
+
+# The following lines are common for all applications
+import application_importer
+import inspect
+caller = inspect.stack()[1] # Information about the file that imported this, to check for unexpected imports
+application_importer.ImportApplication(application,application_name,application_folder,caller)
