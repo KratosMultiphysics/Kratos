dc140e57c7eda2425e97cdf3ba8231dd5a9dff25
rrossi <rrossi@cimne.upc.edu>
2013-06-03 11:41:27 +0000

    implementing an element which handles embedded structures

--- applications/FluidDynamicsApplication/custom_elements/fractional_step_discontinuous.cpp
+++ applications/FluidDynamicsApplication/custom_elements/fractional_step_discontinuous.cpp
@@ -4,7 +4,77 @@ namespace Kratos {
 
 
 
+template< unsigned int TDim >
+void FractionalStepDiscontinuous<TDim>::AddMomentumSystemTerms(Matrix& rLHSMatrix,
+                                                  Vector& rRHSVector,
+                                                  const double Density,
+                                                  const Vector& rConvOperator,
+                                                  const array_1d<double,3>& rBodyForce,
+                                                  const double OldPressure,
+                                                  const double TauOne,
+                                                  const double TauTwo,
+                                                  const array_1d<double,3>& rMomentumProjection,
+                                                  const double MassProjection,
+                                                  const ShapeFunctionsType& rN,
+                                                  const ShapeFunctionDerivativesType& rDN_DX,
+                                                  const double Weight)
+{
+    const SizeType NumNodes = this->GetGeometry().PointsNumber();
 
+    SizeType FirstRow = 0;
+    SizeType FirstCol = 0;
+    
+    array_1d<double,TDim+1> OldPressures;
+    for (SizeType i = 0; i < NumNodes; ++i) 
+      OldPressures[i] = this->GetGeometry()[i].FastGetSolutionStepValue(PRESSURE);
+    
+    array_1d<double,TDim> grad_p = prod(trans(rDN_DX),OldPressures);
+
+    for (SizeType i = 0; i < NumNodes; ++i)
+    {
+
+        // Build RHS
+        for (SizeType d = 0; d < TDim; ++d)
+        {
+            // Body force
+            double RHSi = Density * rN[i] * rBodyForce[d];
+            // Pressure gradient (integrated by parts)
+            //RHSi += rDN_DX(i,d) * OldPressure;
+	    RHSi -= rN[i]*grad_p[d];
+	    
+            // Momentum Stabilization
+            RHSi += Density * rConvOperator[i] * TauOne * ( /*rBodyForce[d] + rOldPressureGradient[d]*/ - rMomentumProjection[d] );
+            // Mass Stabilization
+            RHSi -= rDN_DX(i,d) * TauTwo * MassProjection;
+
+            rRHSVector[FirstRow+d] += Weight * RHSi;
+        }
+
+        // Build LHS
+        for (SizeType j = 0; j < NumNodes; ++j)
+        {
+            // Convective term
+            double Kij = Density * rN[i] * rConvOperator[j];
+
+            // Streamline stabilization
+            Kij += Density * rConvOperator[i] * TauOne * Density * rConvOperator[j];
+
+            Kij *= Weight;
+
+            for (SizeType d = 0; d < TDim; ++d)
+                rLHSMatrix(FirstRow + d,FirstCol + d) += Kij;
+
+            // Mass-GLS (TauTwo) stabiliziation term
+            for (SizeType m = 0; m < TDim; ++m)
+                for (SizeType n = 0; n < TDim; ++n)
+                    rLHSMatrix(FirstRow+m,FirstCol+n) += Weight * rDN_DX(i,m) * TauTwo * rDN_DX(j,n);
+
+            FirstCol += TDim;
+        }
+        FirstRow += TDim;
+        FirstCol = 0;
+    }
+}
 
 
 
@@ -77,13 +147,14 @@ void FractionalStepDiscontinuous<TDim>::CalculateLocalPressureSystem(MatrixType&
         this->EvaluateConvVelocity(ConvVel,N);
         double Viscosity = this->EffectiveViscosity(N,rDN_DX,ElemSize,rCurrentProcessInfo);
         this->CalculateTau(TauOne,TauTwo,ElemSize,ConvVel,Density,Viscosity,rCurrentProcessInfo);
+		//TauOne = 0.0;
 
 //        // Evaluate convection operator Velocity * Grad(N)
 //        Vector UGradN(NumNodes);
 //        this->EvaluateConvection(UGradN,ConvVel,mDN_DX);
 
-/*        double DivU;
-        this->EvaluateDivergenceInPoint(DivU,VELOCITY,rDN_DX);*/
+        double DivU;
+        //this->EvaluateDivergenceInPoint(DivU,VELOCITY,rDN_DX);
 		array_1d<double,3> ugauss;
 		this->EvaluateInPoint(ugauss,VELOCITY,N);
 		for (SizeType i = 0; i < NumNodes; ++i)
@@ -130,6 +201,193 @@ void FractionalStepDiscontinuous<TDim>::CalculateLocalPressureSystem(MatrixType&
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template< unsigned int TDim >
+void FractionalStepDiscontinuous<TDim>::Calculate(const Variable<array_1d<double,3> > &rVariable,
+                                     array_1d<double,3> &rOutput,
+                                     const ProcessInfo &rCurrentProcessInfo)
+{
+    if (rVariable == ADVPROJ)
+    {
+        double Tmp = 0.0;
+        this->Calculate(DIVPROJ,Tmp,rCurrentProcessInfo);
+    }
+    else if (rVariable == CONV_PROJ)
+    {
+        const GeometryType& rGeom = this->GetGeometry();
+        const unsigned int NumNodes = rGeom.PointsNumber();
+        const unsigned int LocalSize = TDim * NumNodes;
+
+        // Shape functions and integration points
+        ShapeFunctionDerivativesArrayType DN_DX;
+        Matrix NContainer;
+        VectorType GaussWeights;
+        this->CalculateGeometryData(DN_DX,NContainer,GaussWeights);
+        const unsigned int NumGauss = GaussWeights.size();
+
+        VectorType ConvTerm = ZeroVector(LocalSize);
+        VectorType PresTerm = ZeroVector(LocalSize);
+        VectorType DivTerm = ZeroVector(NumNodes);
+        VectorType NodalArea = ZeroVector(NumNodes);
+
+        // Loop on integration points
+        for (unsigned int g = 0; g < NumGauss; g++)
+        {
+            const ShapeFunctionsType& N = row(NContainer,g);
+            const double GaussWeight = GaussWeights[g];
+
+            for (unsigned int i = 0; i < NumNodes; i++)
+                NodalArea[i] += N[i] * GaussWeight;
+
+            this->CalculateProjectionRHS(ConvTerm,PresTerm,DivTerm,N,DN_DX[g],GaussWeight);
+        }
+
+        // Carefully write results to nodal variables, to avoid parallelism problems
+        unsigned int RowIndex = 0;
+        for (SizeType i = 0; i < NumNodes; ++i)
+        {
+            this->GetGeometry()[i].SetLock(); // So it is safe to write in the node in OpenMP
+            array_1d<double,3>& rConvVal = this->GetGeometry()[i].FastGetSolutionStepValue(CONV_PROJ);
+            array_1d<double,3>& rPresVal = this->GetGeometry()[i].FastGetSolutionStepValue(PRESS_PROJ);
+            for (unsigned int d = 0; d < TDim; ++d)
+            {
+                rConvVal[d] += ConvTerm[RowIndex];
+                rPresVal[d] += PresTerm[RowIndex];
+                ++RowIndex;
+            }
+            this->GetGeometry()[i].FastGetSolutionStepValue(DIVPROJ) += DivTerm[i];
+            this->GetGeometry()[i].FastGetSolutionStepValue(NODAL_AREA) += NodalArea[i];
+            this->GetGeometry()[i].UnSetLock(); // Free the node for other threads
+        }
+    }
+    else if (rVariable == VELOCITY)
+    {
+        GeometryType& rGeom = this->GetGeometry();
+        const SizeType NumNodes = rGeom.PointsNumber();
+        const SizeType LocalSize = TDim * NumNodes;
+
+        // Shape functions and integration points
+        ShapeFunctionDerivativesArrayType DN_DX;
+        Matrix NContainer;
+        VectorType GaussWeights;
+        this->CalculateGeometryData(DN_DX,NContainer,GaussWeights);
+        const unsigned int NumGauss = GaussWeights.size();
+
+        VectorType NodalVelCorrection = ZeroVector(LocalSize);
+	
+	
+
+        // Loop on integration points
+        for (unsigned int g = 0; g < NumGauss; ++g)
+        {
+            const ShapeFunctionsType& N = row(NContainer,g);
+            const ShapeFunctionDerivativesType& rDN_DX = DN_DX[g];
+
+            double Density;
+
+            this->EvaluateInPoint(Density,DENSITY,N);
+
+            const double Coeff = GaussWeights[g] / ( Density * rCurrentProcessInfo[BDF_COEFFICIENTS][0] );
+	    
+	    array_1d<double,TDim> DeltaPressureGradient(TDim,0.0);
+	    this->EvaluateGradientInPoint(DeltaPressureGradient,PRESSURE_OLD_IT,rDN_DX);
+
+            // Calculate contribution to the gradient term (RHS)
+//             double DeltaPressure;
+//             this->EvaluateInPoint(DeltaPressure,PRESSURE_OLD_IT,N);
+
+            SizeType RowIndex = 0;
+
+            for (SizeType i = 0; i < NumNodes; ++i)
+            {
+                for (SizeType d = 0; d < TDim; ++d)
+                {
+//                     NodalVelCorrection[RowIndex++] += Coeff * rDN_DX(i,d) * DeltaPressure;
+		    NodalVelCorrection[RowIndex++] -= Coeff * N(i) * DeltaPressureGradient[d];
+                }
+            }
+        }
+
+        SizeType Index = 0;
+
+        for (SizeType i = 0; i < NumNodes; ++i)
+        {
+            rGeom[i].SetLock(); // So it is safe to write in the node in OpenMP
+            array_1d<double,3>& rTemp = rGeom[i].FastGetSolutionStepValue(FRACT_VEL);
+            for (SizeType d = 0; d < TDim; ++d)
+            {
+                rTemp[d] += NodalVelCorrection[Index++];
+            }
+            rGeom[i].UnSetLock(); // Free the node for other threads
+        }
+
+    }
+}
+
+
+template< unsigned int TDim >
+void FractionalStepDiscontinuous<TDim>::Calculate(const Variable<double> &rVariable,
+                                     double &rOutput,
+                                     const ProcessInfo &rCurrentProcessInfo)
+{
+    if (rVariable == DIVPROJ)
+    {
+        const GeometryType& rGeom = this->GetGeometry();
+        const SizeType NumNodes = rGeom.PointsNumber();
+        const unsigned int LocalSize = TDim * NumNodes;
+
+        // Shape functions and integration points
+        ShapeFunctionDerivativesArrayType DN_DX;
+        Matrix NContainer;
+        VectorType GaussWeights;
+        this->CalculateGeometryData(DN_DX,NContainer,GaussWeights);
+        const unsigned int NumGauss = GaussWeights.size();
+
+        VectorType MomentumRHS = ZeroVector(LocalSize);
+        VectorType MassRHS = ZeroVector(NumNodes);
+        VectorType NodalArea = ZeroVector(NumNodes);
+
+        // Loop on integration points
+        for (unsigned int g = 0; g < NumGauss; g++)
+        {
+            const ShapeFunctionsType& N = row(NContainer,g);
+            const double GaussWeight = GaussWeights[g];
+
+            for (unsigned int i = 0; i < NumNodes; i++)
+                NodalArea[i] += N[i] * GaussWeight;
+
+            this->CalculateProjectionRHS(MomentumRHS,MassRHS,N,DN_DX[g],GaussWeight);
+        }
+
+        // Carefully write results to nodal variables, to avoid parallelism problems
+        unsigned int RowIndex = 0;
+        for (SizeType i = 0; i < NumNodes; ++i)
+        {
+            this->GetGeometry()[i].SetLock(); // So it is safe to write in the node in OpenMP
+            array_1d<double,3>& rMomValue = this->GetGeometry()[i].FastGetSolutionStepValue(ADVPROJ);
+            for (unsigned int d = 0; d < TDim; ++d)
+                rMomValue[d] += MomentumRHS[RowIndex++];
+            this->GetGeometry()[i].FastGetSolutionStepValue(DIVPROJ) += MassRHS[i];
+            this->GetGeometry()[i].FastGetSolutionStepValue(NODAL_AREA) += NodalArea[i];
+            this->GetGeometry()[i].UnSetLock(); // Free the node for other threads
+        }
+    }
+}
+
+
 /*
  * protected FractionalStepDiscontinuous<TDim> functions
  */
@@ -139,16 +397,84 @@ void FractionalStepDiscontinuous<TDim>::CalculateGeometryData(ShapeFunctionDeriv
                                            Matrix& rNContainer,
                                            Vector& rGaussWeights)
 {
-    const GeometryType& rGeom = this->GetGeometry();
-    Vector DetJ;
-    rGeom.ShapeFunctionsIntegrationPointsGradients(rDN_DX,DetJ,GeometryData::GI_GAUSS_2);
-    rNContainer = rGeom.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
-    const GeometryType::IntegrationPointsArrayType& IntegrationPoints = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_2);
+	bool split_element = this->GetValue(SPLIT_ELEMENT);
+/*	KRATOS_WATCH(this->Id())
+	KRATOS_WATCH(this->GetValue(SPLIT_ELEMENT))*/
+	if (split_element == true)
+	{
+		
+		array_1d<double,(TDim+1)> distances = this->GetValue(ELEMENTAL_DISTANCES);
+		unsigned int npos = 0, nneg=0;
+		for(unsigned int i=0; i<TDim+1; i++)
+			if(distances[i] >= 0) npos++;
+			else nneg++;
+		
+		if(nneg !=0 && npos != 0)
+		{
+			boost::numeric::ublas::bounded_matrix<double,3*(TDim-1), (TDim+1)> Nenriched;
+			array_1d<double,(3*(TDim-1))> volumes;
+			boost::numeric::ublas::bounded_matrix<double,(TDim+1), TDim > coords;
+			boost::numeric::ublas::bounded_matrix<double, 3*(TDim-1), (TDim+1) > Ngauss;
+			array_1d<double,(3*(TDim-1))> signs;
+			std::vector< Matrix > gauss_gradients(3*(TDim-1));
 
-    rGaussWeights.resize(rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2),false);
+			boost::numeric::ublas::bounded_matrix<double, (TDim+1), TDim > DN_DX;
+			array_1d<double, (TDim+1) > N;
+				
+			
 
-    for (unsigned int g = 0; g < rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2); g++)
-        rGaussWeights[g] = DetJ[g] * IntegrationPoints[g].Weight();
+			for (unsigned int i = 0; i < TDim+1; i++)
+			{
+				const array_1d<double, 3 > & xyz = this->GetGeometry()[i].Coordinates();
+				volumes[i] = 0.0;
+				for (unsigned int j = 0; j < TDim; j++) coords(i, j) = xyz[j];
+			}
+
+			for (unsigned int i = 0; i < 3*(TDim-1); i++) gauss_gradients[i].resize(TDim+1, TDim, false);  //2 values of the 2 shape functions, and derivates in (xy) direction).
+			unsigned int ndivisions= DiscontinuousShapeFunctionsUtilities::CalculateDiscontinuousShapeFunctions(coords, DN_DX, distances, volumes, Ngauss, signs, gauss_gradients, Nenriched);
+			
+			if(rGaussWeights.size() != ndivisions)
+				rGaussWeights.resize(ndivisions,false);
+			if(rNContainer.size1() != ndivisions || rNContainer.size2() != TDim+1)	
+				rNContainer.resize(ndivisions,TDim+1,false);
+			rDN_DX.resize(ndivisions);
+			
+			rNContainer = Nenriched;
+			rGaussWeights = volumes;
+
+			for (unsigned int g = 0; g < ndivisions; g++)
+			{
+				rDN_DX[g] = gauss_gradients[g];
+			
+			}
+		}
+		else
+		{
+			const GeometryType& rGeom = this->GetGeometry();
+			Vector DetJ;
+			rGeom.ShapeFunctionsIntegrationPointsGradients(rDN_DX,DetJ,GeometryData::GI_GAUSS_2);
+			rNContainer = rGeom.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
+			const GeometryType::IntegrationPointsArrayType& IntegrationPoints = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_2);
+
+			rGaussWeights.resize(rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2),false);
+
+			for (unsigned int g = 0; g < rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2); g++)
+				rGaussWeights[g] = DetJ[g] * IntegrationPoints[g].Weight();
+		}
+	}
+	else
+	{
+		const GeometryType& rGeom = this->GetGeometry();
+		Vector DetJ;
+		rGeom.ShapeFunctionsIntegrationPointsGradients(rDN_DX,DetJ,GeometryData::GI_GAUSS_2);
+		rNContainer = rGeom.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
+		const GeometryType::IntegrationPointsArrayType& IntegrationPoints = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_2);
+
+		rGaussWeights.resize(rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2),false);
+
+		for (unsigned int g = 0; g < rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2); g++)
+			rGaussWeights[g] = DetJ[g] * IntegrationPoints[g].Weight();
+	}
     /*
     const GeometryType& rGeom = this->GetGeometry();
     const SizeType NumNodes = rGeom.PointsNumber();
