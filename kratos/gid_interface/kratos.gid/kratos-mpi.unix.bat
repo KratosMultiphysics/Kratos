6c8c00ac4fc7480ff7b420504a430dfffa6f42a5
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-06-13 13:11:18 +0000

    modifications for correct packaging

--- kratos/gid_interface/kratos.gid/kratos-mpi.unix.bat
+++ kratos/gid_interface/kratos.gid/kratos-mpi.unix.bat
@@ -1,24 +1,26 @@
-#!/bin/bash -i
-#  echo hola
-#  echo par1 : $1
-#  echo "1. param: $1" > /tmp/kk.txt
-#  echo "2. param: $2" >> /tmp/kk.txt
-#  echo "3. param: $3" >> /tmp/kk.txt
-#  echo "4. param: $4" >> /tmp/kk.txt
-#  echo "5. param: $5" >> /tmp/kk.txt
-#    OutputFile: "$2/$1.info"
-#    ErrorFile: "$2/$1.err"
-#delete previous result file
-rm -f "$2/$1.post.bin"
+#!/bin/bash
+# OutputFile: "$2/$1.info"
+# ErrorFile: "$2/$1.err"
+#delete previous result file 
+rm -f "$2/$1.post.bin" 
 rm -f "$2/$1.info"
 rm -f "$2/$1.err"
 rm -f "$2/$1.flavia.dat"
 
+# include .bashrc if it exists
+if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+fi
 
-# gid redefined LD_LIBRARY_PATH to its own libs directory
+# gid redefines LD_LIBRARY_PATH to its own libs directory
 # and maintains OLD_LD_LIBRARY_PATH with previous settings
 # therefore, we use the OLD_LD_LIBRARY_PATH and prepend the path to the kratos libs
-export LD_LIBRARY_PATH="$3/kratos/libs":$OLD_LD_LIBRARY_PATH
+if [ "$OLD_LD_LIBRARY_PATH" != "" ]; then
+    export LD_LIBRARY_PATH="$3/kratos":"$3/kratos/libs":$OLD_LD_LIBRARY_PATH
+else
+    # do not add the ':'
+    export LD_LIBRARY_PATH="$3/kratos":"$3/kratos/libs"
+fi
 
 # Set the number of threads for OpenMP
 export OMP_NUM_THREADS=1
--- kratos/gid_interface/kratos.gid/kratos-structuralstatic-openmp.unix.bat
+++ kratos/gid_interface/kratos.gid/kratos-structuralstatic-openmp.unix.bat
@@ -1,4 +1,4 @@
-#!/bin/bash -i
+#!/bin/bash
 # OutputFile: "$2/$1.info"
 # ErrorFile: "$2/$1.err"
 #delete previous result file 
@@ -6,6 +6,21 @@ rm -f "$2/$1.post.bin"
 rm -f "$2/$1.info"
 rm -f "$2/$1.err"
 rm -f "$2/$1.flavia.dat"
+
+# include .bashrc if it exists
+if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+fi
+
+# gid redefines LD_LIBRARY_PATH to its own libs directory
+# and maintains OLD_LD_LIBRARY_PATH with previous settings
+# therefore, we use the OLD_LD_LIBRARY_PATH and prepend the path to the kratos libs
+if [ "$OLD_LD_LIBRARY_PATH" != "" ]; then
+    export LD_LIBRARY_PATH="$3/kratos":"$3/kratos/libs":$OLD_LD_LIBRARY_PATH
+else
+    # do not add the ':'
+    export LD_LIBRARY_PATH="$3/kratos":"$3/kratos/libs"
+fi
 
 # Set the number of threads for OpenMP
 export OMP_NUM_THREADS=$5
