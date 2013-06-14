07a430bdf45d1832dee3fbf39be6f1cfe20607a1
jirazabal <jirazabal@cimne.upc.edu>
2013-06-14 11:19:28 +0000

    New DEM benchmark (ball rotating over a inclined surface without tangential damping but with rolling friction) added

---
+++ applications/DEM_application/test_examples/rotating_ball_rolling_friction.gid/DEM_explicit_solver_var.py
@@ -0,0 +1,145 @@
+# Model Type
+
+ContinuumOption                  = "OFF"
+RotationOption                   = "ON"
+HomogeneousMaterialOption        = "OFF"
+ElementType = "SphericParticle3D"
+
+# Meshing Settings
+
+CleanIndentationsOption          = "OFF"
+
+# General Settings
+
+final_time                       = 1.00000e+00
+gravity_x                        = 0.00000e+00
+gravity_y                        = -9.81000e+00
+gravity_z                        = 0.00000e+00
+BoundingBoxOption                = "OFF"
+bounding_box_enlargement_factor  = 2.00000e+00
+domain_size                      = 3
+OutputFileType                   = "Binary"
+Multifile                        = "single_file"
+PrintNeighbourLists              = "OFF"
+ModelDataInfo                    = "OFF"
+
+# Special features
+
+VirtualMassOption                = "OFF"
+VirtualMassCoefficient           = 0.00000e+00
+MagicFactor                      = 1.00000e+00
+DeltaOption                      = "OFF"
+search_radius_extension          = 1.00000e-02
+FixVelocities                    = "OFF"
+TimePercentageFixVelocities      = 1.00000e+01
+TrihedronOption                  = "OFF"
+LimitSurfaceOption               = "ON"
+surface_normal_dir_x             = 5.00000e-01
+surface_normal_dir_y             = 5.00000e-01
+surface_normal_dir_z             = 0.00000e+00
+surface_point_coor_x             = 0.00000e+00
+surface_point_coor_y             = 0.00000e+00
+surface_point_coor_z             = 0.00000e+00
+surface_friction_angle           = 45
+
+# Time Discretization Settings
+
+Integration_Scheme               = "forward_euler"
+search_step                      = 1.00000e+00
+AutoReductionOfTimeStepOption    = "ON"
+dt_safety_factor                 = 1.00000e+00
+max_time_step                    = 1.00000e-04
+output_dt                        = 1.00000e-02
+control_time                     = 2.00000e+01
+
+# Material Model
+
+NormalForceCalculation           = "Linear"
+NormalDampId                     = "ViscDamp"
+TangentialDampId                 = "NoDamp"
+FailureCriterionOption           = "Uncoupled"
+TauZero                          = 5.26000e+00
+SigmaMax                         = 3.32000e+01
+SigmaMin                         = 3.32000e+00
+InternalFricc                    = 3.50000e+01
+NonLinearOption                  = "OFF"
+C1                               = 4.00000e-01
+N1                               = 2.00000e+01
+C2                               = 1.50000e+00
+N2                               = 1.50000e+01
+RotationalSpringOption           = "OFF"
+RotaDampId                       = "RollingFric"
+
+# Global Material Parameters
+
+GeneralDensity                   = "1.00000e+02"
+GeneralYoungModulus              = "2.10000e+07"
+GeneralPoissonRatio              = "5.00000e-01"
+GeneralCohesion                  = "4.16000e+06"
+GeneralRollingFriction           = "0.00000e+00"
+GeneralTension                   = "2.01000e+06"
+GeneralRotaDampRatio             = "5.00000e-01"
+GeneralStaticFrictionCoef        = "0.00000e+00"
+GeneralDynamicFrictionCoef       = "0.00000e+00"
+GeneralRestitutionCoef           = "5.00000e-01"
+GeneralColour                    = "1.00000e+00"
+GlobalVariablesOption            = "OFF"
+global_kn                        = 3.00000e+03
+global_kt                        = 1.00000e+03
+global_kr                        = 1.00000e+03
+global_rn                        = 1.00000e+03
+global_rt                        = 1.00000e+03
+global_rr                        = 1.00000e+03
+global_fri_ang                   = 4.00000e+01
+
+# Continuum Options
+
+StressStrainOperations           = "OFF"
+ContactMeshOption                = "OFF"
+ConcreteTestOption               = "OFF"
+RealTimeGraph                    = "OFF"
+TriaxialOption                   = "OFF"
+ConfinementPressure              = 0.00000e+00
+InitialTime                      = 0.00000e+00
+IncreasingTemporaily             = 1.50000e+01
+
+#POSTPROCES
+
+print_velocity                   = "1"
+print_displacement               = "1"
+print_radial_displacement        = "0"
+print_rhs                        = "0"
+print_total_forces               = "0"
+print_damp_forces                = "0"
+print_applied_forces             = "0"
+print_radius                     = "1"
+print_particle_cohesion          = "0"
+print_particle_tension           = "0"
+print_group_id                   = "0"
+print_export_id                  = "1"
+print_export_particle_failure_id = "0"
+print_export_skin_sphere         = "0"
+print_local_contact_force_low    = "0"
+print_local_contact_force_high   = "0"
+print_failure_criterion_state    = "0"
+print_contact_failure            = "0"
+print_contact_tau                = "0"
+print_contact_sigma              = "0"
+print_angular_velocity           = "0"
+print_particle_moment            = "0"
+print_euler_angles               = "0"
+print_representative_volume      = "0"
+print_mean_contact_area          = "0"
+print_stress_tensor              = "0"
+
+#FROM CND:
+
+predefined_skin_option           = "OFF"
+
+mass_elements                    = 4.18879e+00
+
+# Declare Python Variables
+problem_name = 'rotating_ball_rolling_friction'
+problem_path = '/home/cimne/kratos/applications/DEM_application/test_examples/rotating_ball_rolling_friction.gid'
+kratos_path = '/home/cimne/kratos'
+
---
+++ applications/DEM_application/test_examples/rotating_ball_rolling_friction.gid/DEM_procedures.py
@@ -0,0 +1,378 @@
+from KratosMultiphysics import *
+from KratosMultiphysics.DEMApplication import *
+#from KratosMultiphysics.MetisApplication import * #CARLOS
+
+from DEM_explicit_solver_var import *
+from pressure_script import *
+
+from numpy import *
+
+#from KratosMultiphysics.mpi import * #CARLOS
+
+
+# GLOBAL VARIABLES OF THE SCRIPT
+#Defining list of skin particles (For a test tube of height 30 cm and diameter 15 cm)
+    
+sup_layer_fm = list()
+inf_layer_fm = list()
+sup_plate_fm = list()
+inf_plate_fm = list()
+special_selection = list()
+others = list()    
+SKIN = list()  
+LAT = list()
+BOT = list()
+TOP = list()
+XLAT = list()  #only lat, not the corner ones
+XTOP = list()  #only top, not corner ones...
+XBOT = list()
+XTOPCORNER = list()
+XBOTCORNER = list()
+
+   
+def AddMpiVariables(model_part):
+    
+    model_part.AddNodalSolutionStepVariable(PARTITION_INDEX)
+    model_part.AddNodalSolutionStepVariable(INTERNAL_ENERGY)
+    model_part.AddNodalSolutionStepVariable(OSS_SWITCH)
+    
+def PerformInitialPartition(model_part,model_part_io_solid,input_file_name):
+    
+    domain_size = 3
+    
+    print "("+str(mpi.rank)+","+str(mpi.size)+")"+"before performing the division"
+    number_of_partitions = mpi.size #we set it equal to the number of processors
+    if mpi.rank == 0 :
+        print "("+str(mpi.rank)+","+str(mpi.size)+")"+"start partition process"
+        partitioner = MortonDivideInputToPartitionsProcess(model_part_io_solid, number_of_partitions, domain_size);
+        partitioner.Execute()
+
+    print "("+str(mpi.rank)+","+str(mpi.size)+")"+"division performed"
+    mpi.world.barrier()
+
+    MPICommSetup = SetMPICommunicatorProcess(model_part)
+    MPICommSetup.Execute()
+
+    print "("+str(mpi.rank)+","+str(mpi.size)+")"+"Comunicator Set"
+
+    print "("+str(mpi.rank)+","+str(mpi.size)+")"+"Reading: "+input_file_name+"_"+str(mpi.rank)
+
+    my_input_filename = input_file_name+"_"+str(mpi.rank)
+    model_part_io_solid = ModelPartIO(my_input_filename)
+    
+    return model_part_io_solid
+    
+
+def ProcModelData(solid_model_part,solver):
+# Previous Calculations.
+
+   Model_Data = open('Model_Data.txt','w')
+
+#mean radius, and standard deviation:
+
+   i = 0
+   sum_radi = 0
+   sum_squared = 0
+   for node in solid_model_part.Nodes:
+
+     sum_radi += node.GetSolutionStepValue(RADIUS)
+     sum_squared += node.GetSolutionStepValue(RADIUS)**2
+     i+=1
+
+   mean=sum_radi/i
+   var =sum_squared/i-mean**2
+   if(abs(var)<1e-05):
+     var=0
+   std_dev=var**0.5
+
+   Model_Data.write("Radius Mean: "+str(mean)+'\n')
+   Model_Data.write("Std Deviation: "+str(std_dev)+'\n')
+   Model_Data.write('\n')
+
+   Total_Particles     = len(solid_model_part.Nodes)
+   Total_Contacts      = solver.model_part.ProcessInfo.GetValue(TOTAL_CONTACTS)/2
+   Coordination_Number    = 1.0*(Total_Contacts*2)/Total_Particles
+
+   Model_Data.write("Total Number of Particles: "+str(Total_Particles)+'\n')
+   Model_Data.write("Total Number of Contacts: "+str(Total_Contacts)+'\n')
+   Model_Data.write("Coordination Number NC: "+str(Coordination_Number)+'\n')
+   Model_Data.write('\n')
+
+   Model_Data.write("Volume Elements: "+str(mass_elements)+'\n')
+
+   Model_Data.close()
+
+
+def ProcListDefinition(model_part,solver):
+
+# Defining lists (FOR COMPRESSION TESTS)
+
+    for node in model_part.Nodes:
+        if (node.GetSolutionStepValue(GROUP_ID)==1):      #reserved for speciment particles with imposed displacement and strain-stress measurement (superior). Doesn't recive pressure
+            sup_layer_fm.append(node)
+        elif (node.GetSolutionStepValue(GROUP_ID)==2):    #reserved for speciment particles with imposed displacement and strain-stress measurement (superior). Doesn't recive pressure
+            inf_layer_fm.append(node)
+        elif (node.GetSolutionStepValue(GROUP_ID)==3):    #reserved for auxiliar strain-stress measurement plate (superior)
+            sup_plate_fm.append(node)
+        elif (node.GetSolutionStepValue(GROUP_ID)==4):    #reserved for auxiliar strain-stress measurement plate (inferior)
+            inf_plate_fm.append(node)
+        elif (node.GetSolutionStepValue(GROUP_ID)==5):
+            special_selection.append(node)
+        else:
+            others.append(node)
+
+    return (sup_layer_fm, inf_layer_fm, sup_plate_fm, inf_plate_fm)
+
+
+def ProcGiDSolverTransfer(model_part,solver):
+
+    extra_radius = 0.0
+    max_radius = 0.0
+    min_radius = 0.0
+    first_it = True
+
+    #calculation of search radius
+    for node in model_part.Nodes:
+          
+      rad = node.GetSolutionStepValue(RADIUS)
+      if rad > max_radius:  
+          max_radius = rad
+      if first_it == True:
+          min_radius = rad
+          first_it = False
+      if rad < min_radius:  
+          min_radius = rad
+          
+    if(BoundingBoxOption =="ON"):
+      solver.bounding_box_OPTION=1  #xapuza
+      
+    extra_radius = 2.5 * max_radius
+    prox_tol = 0.000001 * min_radius  #currently not in use.
+    m_bounding_box_enlargement_factor = max(1.0 + extra_radius, bounding_box_enlargement_factor)
+
+    solver.enlargement_factor = m_bounding_box_enlargement_factor
+    
+    if ( TriaxialOption =="ON" ):
+       Pressure = ConfinementPressure*1e6 #Mpa
+    else:
+       Pressure = 0.0
+    
+    if(Pressure!=0):
+      
+      solver.external_pressure = 1
+   
+    if(FixVelocities =="ON"):
+        solver.fix_velocities = 1  #xapuza
+    solver.time_step_percentage_fix_velocities = TimePercentageFixVelocities   
+    
+    return Pressure
+    
+def ProcSkinAndPressure(model_part,solver):
+    
+    #SKIN DETERMINATION
+
+    Pressure = ConfinementPressure*1e6 #Mpa
+    total_cross_section = 0.0
+
+    #Cylinder dimensions
+
+    h   = 0.3
+    d   = 0.15
+    eps = 2.0
+
+    surface = 2*(3.141592*d*d*0.25)+(3.141592*d*h)
+
+    top_pressure = 0.0
+    bot_pressure = 0.0
+
+    xlat_area = 0.0
+    xbot_area = 0.0
+    xtop_area = 0.0
+    xbotcorner_area = 0.0
+    xtopcorner_area = 0.0
+    
+    for element in model_part.Elements:
+      
+      element.SetValue(SKIN_SPHERE,0)
+   
+      if ( predefined_skin_option == "OFF" ):
+      
+        node = element.GetNode(0)
+        r = node.GetSolutionStepValue(RADIUS,0)
+        x = node.X
+        y = node.Y
+        z = node.Z
+        node_group = node.GetSolutionStepValue(GROUP_ID,0)
+        cross_section = 3.141592*r*r
+
+        #if( (node_group!=2) and (node_group!=4) ):
+      
+        if ( (x*x+z*z)>=((d/2-eps*r)*(d/2-eps*r)) ): 
+      
+             element.SetValue(SKIN_SPHERE,1)     
+
+             LAT.append(node)
+            
+             if ( (y>eps*r ) and (y<(h-eps*r)) ) :
+        
+               SKIN.append(element)
+        
+               XLAT.append(node)
+          
+               xlat_area = xlat_area + cross_section
+    
+        if ( (y<=eps*r ) or (y>=(h-eps*r)) ): 
+
+               element.SetValue(SKIN_SPHERE,1)
+        
+               SKIN.append(element)
+        
+               if ( y<=eps*r ):
+
+                   BOT.append(node)
+
+               elif ( y>=(h-eps*r) ):
+
+                   TOP.append(node)
+
+               if ( (x*x+z*z) >= ((d/2-eps*r)*(d/2-eps*r) ) ) :
+         
+                   if ( y>h/2 ):
+
+                       XTOPCORNER.append(node)
+                       
+                       xtopcorner_area = xtopcorner_area + cross_section
+                
+                   else:
+
+                       XBOTCORNER.append(node)
+                       xbotcorner_area = xbotcorner_area + cross_section
+               else:
+
+                   if ( y<=eps*r ):
+                
+                       XBOT.append(node)
+                       xbot_area = xbot_area + cross_section
+                
+                   elif ( y>=(h-eps*r) ):
+                    
+                       XTOP.append(node)
+                       xtop_area = xtop_area + cross_section
+
+    print "End CLASSIC TEST SKIN DETERMINATION", "\n"
+              
+    return (xtop_area,xbot_area,xlat_area,xtopcorner_area,xbotcorner_area) 
+     
+def ProcApplyPressure(Pressure,model_part,solver,alpha_top,alpha_bot,alpha_lat):
+     
+    if ( predefined_skin_option == "ON"):
+      print "\n", "Predefined Skin by the user, In this case is not correct to apply pressure yet"  ,"\n" 
+    else:
+      ApplyPressure(Pressure,model_part,solver,SKIN,BOT,TOP,LAT,XLAT,XBOT,XTOP,XBOTCORNER,XTOPCORNER,alpha_top,alpha_bot,alpha_lat)
+
+    
+def ProcMeasureBOT(BOT,solver):
+
+    tol = 2.0
+    y_mean = 0.0
+    counter = 0.0
+    for node in BOT:
+      r = node.GetSolutionStepValue(RADIUS,0)
+      y = node.Y
+      
+      y_mean += (y-r)*r
+      counter += r
+
+    return (y_mean,counter)      
+
+def ProcMeasureTOP(TOP,solver):
+
+    tol = 2.0
+    y_mean = 0.0
+    counter = 0.0
+    
+    for node in TOP:
+      r = node.GetSolutionStepValue(RADIUS,0)
+      y = node.Y
+      
+      y_mean += (y+r)*r
+      counter += r
+
+    return (y_mean,counter)
+    
+def ProcPrintingVariables(gid_io,export_model_part,time):
+  
+    if (print_displacement=="1"):
+      gid_io.WriteNodalResults(DISPLACEMENT, export_model_part.Nodes, time, 0)       
+    if (print_radial_displacement=="1"):
+      gid_io.WriteNodalResults(RADIAL_DISPLACEMENT, export_model_part.Nodes, time, 0)       
+    if (print_velocity=="1"):
+      gid_io.WriteNodalResults(VELOCITY, export_model_part.Nodes, time, 0)
+    if (print_rhs=="1"):
+      gid_io.WriteNodalResults(RHS, export_model_part.Nodes, time, 0)       
+    if (print_applied_forces=="1"):
+      gid_io.WriteNodalResults(APPLIED_FORCE, export_model_part.Nodes, time, 0)       
+    if (print_total_forces=="1"):     
+      gid_io.WriteNodalResults(TOTAL_FORCES, export_model_part.Nodes, time, 0)    
+    if (print_damp_forces=="1"):
+      gid_io.WriteNodalResults(DAMP_FORCES, export_model_part.Nodes, time, 0)        
+    if (print_radius=="1"):
+      gid_io.WriteNodalResults(RADIUS, export_model_part.Nodes, time, 0)       
+    if (print_particle_cohesion=="1"):
+      gid_io.WriteNodalResults(PARTICLE_COHESION, export_model_part.Nodes, time, 0)       
+    if (print_particle_tension=="1"):
+      gid_io.WriteNodalResults(PARTICLE_TENSION, export_model_part.Nodes, time, 0)
+    if (print_group_id=="1"):
+      gid_io.WriteNodalResults(GROUP_ID, export_model_part.Nodes, time, 0)
+    if (print_export_id=="1"):
+      gid_io.WriteNodalResults(EXPORT_ID, export_model_part.Nodes, time, 0)
+    if (print_export_particle_failure_id=="1"):
+      gid_io.WriteNodalResults(EXPORT_PARTICLE_FAILURE_ID, export_model_part.Nodes, time, 0)
+    if (print_export_skin_sphere=="1"):
+      gid_io.WriteNodalResults(EXPORT_SKIN_SPHERE, export_model_part.Nodes, time, 0)
+    if (print_stress_tensor == "1"):
+      gid_io.WriteNodalResults(DEM_STRESS_XX, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_XY, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_XZ, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_YX, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_YY, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_YZ, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_ZX, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_ZY, export_model_part.Nodes, time, 0)
+      gid_io.WriteNodalResults(DEM_STRESS_ZZ, export_model_part.Nodes, time, 0)
+    if (print_representative_volume == "1"):
+      gid_io.WriteNodalResults(REPRESENTATIVE_VOLUME, export_model_part.Nodes, time, 0)
+    
+    #Aixo sempre per que si no hi ha manera de debugar
+    #gid_io.WriteNodalResults(PARTITION_INDEX, export_model_part.Nodes, time, 0)
+    #gid_io.WriteNodalResults(INTERNAL_ENERGY, export_model_part.Nodes, time, 0)
+
+    if (ContactMeshOption == "ON"): ##xapuza
+      if (print_local_contact_force_low=="1"):
+          gid_io.PrintOnGaussPoints(LOCAL_CONTACT_FORCE_LOW,export_model_part,time)
+      if (print_local_contact_force_high=="1"):
+          gid_io.PrintOnGaussPoints(LOCAL_CONTACT_FORCE_HIGH,export_model_part,time)
+      if (print_mean_contact_area=="1"): 
+          gid_io.PrintOnGaussPoints(MEAN_CONTACT_AREA,export_model_part,time)
+      if (print_contact_failure=="1"): 
+          gid_io.PrintOnGaussPoints(CONTACT_FAILURE,export_model_part,time)  
+      if (print_failure_criterion_state=="1"):
+          gid_io.PrintOnGaussPoints(FAILURE_CRITERION_STATE,export_model_part,time)         
+      if (print_contact_tau=="1"):
+          gid_io.PrintOnGaussPoints(CONTACT_TAU,export_model_part,time)
+      if (print_contact_sigma=="1"):
+          gid_io.PrintOnGaussPoints(CONTACT_SIGMA,export_model_part,time)
+          gid_io.PrintOnGaussPoints(LOCAL_CONTACT_AREA_HIGH,export_model_part,time)
+          gid_io.PrintOnGaussPoints(LOCAL_CONTACT_AREA_LOW,export_model_part,time)
+      #gid_io.PrintOnGaussPoints(NON_ELASTIC_STAGE,export_model_part,time)    
+
+    if (RotationOption == "ON"): ##xapuza
+      if (print_angular_velocity=="1"):
+          gid_io.WriteNodalResults(ANGULAR_VELOCITY, export_model_part.Nodes, time, 0)
+      if (print_particle_moment=="1"):
+          gid_io.WriteNodalResults(PARTICLE_MOMENT, export_model_part.Nodes, time, 0)
+      if (print_euler_angles=="1"):
+          gid_io.WriteLocalAxesOnNodes(EULER_ANGLES, export_model_part.Nodes, time, 0)
+
+    gid_io.Flush()
+    sys.stdout.flush()
