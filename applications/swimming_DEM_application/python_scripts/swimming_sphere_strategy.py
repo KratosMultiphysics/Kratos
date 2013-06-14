92a9d5cbc49dc3ee52d436c724e94a6b68e5e4ea
gcasas <gcasas@cimne.upc.edu>
2013-06-14 19:02:54 +0000

    updates

--- applications/swimming_DEM_application/python_scripts/swimming_sphere_strategy.py
+++ applications/swimming_DEM_application/python_scripts/swimming_sphere_strategy.py
@@ -1,4 +1,4 @@
-
+#*elemsconec(1) 0 *operation(sqrt(elemsmatprop(Particle_density,real)))
 from KratosMultiphysics import *
 from KratosMultiphysics.DEMApplication import *
 from DEM_explicit_solver_var import *
@@ -25,16 +25,18 @@ def AddVariables(model_part):
     model_part.AddNodalSolutionStepVariable(TOTAL_FORCES)
     model_part.AddNodalSolutionStepVariable(DAMP_FORCES)
     model_part.AddNodalSolutionStepVariable(PARTICLE_MOMENT)
+    model_part.AddNodalSolutionStepVariable(APPLIED_FORCE)
     model_part.AddNodalSolutionStepVariable(DRAG_FORCE)
     model_part.AddNodalSolutionStepVariable(BUOYANCY)
 
     # BASIC PARTICLE PROPERTIES
     model_part.AddNodalSolutionStepVariable(RADIUS)
     model_part.AddNodalSolutionStepVariable(NODAL_MASS)
+    model_part.AddNodalSolutionStepVariable(SQRT_OF_MASS)
     model_part.AddNodalSolutionStepVariable(PARTICLE_DENSITY)
     model_part.AddNodalSolutionStepVariable(YOUNG_MODULUS)
     model_part.AddNodalSolutionStepVariable(POISSON_RATIO)
-    model_part.AddNodalSolutionStepVariable(RESTITUTION_COEFF)
+    model_part.AddNodalSolutionStepVariable(LN_OF_RESTITUTION_COEFF)
     model_part.AddNodalSolutionStepVariable(PARTICLE_COHESION)
     model_part.AddNodalSolutionStepVariable(PARTICLE_FRICTION)
     model_part.AddNodalSolutionStepVariable(PARTICLE_TENSION)
@@ -49,11 +51,12 @@ def AddVariables(model_part):
     model_part.AddNodalSolutionStepVariable(PARTICLE_MATERIAL)   # Colour defined in GiD
     model_part.AddNodalSolutionStepVariable(PARTICLE_CONTINUUM)  # Continuum group
     model_part.AddNodalSolutionStepVariable(REPRESENTATIVE_VOLUME)
+    model_part.AddNodalSolutionStepVariable(MAX_INDENTATION)
 
     # LOCAL AXIS
     model_part.AddNodalSolutionStepVariable(EULER_ANGLES)
 
-    #SURFACE
+    # BOUNDARY SURFACE
     model_part.AddNodalSolutionStepVariable(PARTICLE_SURFACE_CONTACT_FORCES)
     model_part.AddNodalSolutionStepVariable(PARTICLE_SURFACE_ROTATE_SPRING_MOMENT)
 
@@ -82,6 +85,15 @@ def AddDofs(model_part):
 
     print "DOFs for the DEM solution added correctly"
 
+def Var_Translator(variable):
+
+    if (variable == "OFF" or variable == "0"):
+        variable = 0
+    else:
+        variable = 1
+
+    return variable
+
 class ExplicitStrategy:
 
     def __init__(self, model_part, domain_size):
@@ -89,99 +101,153 @@ class ExplicitStrategy:
         # Initialization of member variables
 
         # SIMULATION FLAGS
-        self.MoveMeshFlag                   = True
-        self.virtual_mass_OPTION            = 0  #its 1/0 xapuza
-        self.critical_time_OPTION           = 0  #its 1/0 xapuza
+        self.virtual_mass_OPTION            = Var_Translator(VirtualMassOption)  #its 1/0 xapuza
+        self.critical_time_OPTION           = Var_Translator(AutoReductionOfTimeStepOption)  #its 1/0 xapuza
         self.case_OPTION                    = 3  #aixo es una xapuza fins que pooyan permeti bools a pyton o tinguis flags.
-        self.trihedron_OPTION               = 0
-        self.rotation_OPTION                = 0  #its 1/0 xapuza
-        self.rotation_spring_OPTION         = 0  #its 1/0 xapuza
-        self.bounding_box_OPTION            = 0  #its 1/0 xapuza
+        self.trihedron_OPTION               = Var_Translator(TrihedronOption)
+        self.rotation_OPTION                = Var_Translator(RotationOption)
+        self.rotation_spring_OPTION         = Var_Translator(RotationalSpringOption)  #its 1/0 xapuza
+        self.bounding_box_OPTION            = Var_Translator(BoundingBoxOption)  #its 1/0 xapuza
         self.activate_search                = 1  #its 1/0 xapuza
-        self.fix_velocities                 = 0
-        self.global_variables_OPTION        = 0  #its 1/0 xapuza
-        self.limit_surface_OPTION           = 0  #its 1/0 xapuza
+        self.fix_velocities                 = Var_Translator(FixVelocities)
+        self.limit_surface_OPTION           = Var_Translator(LimitSurfaceOption)  #its 1/0 xapuza
+        self.clean_init_indentation_OPTION  = Var_Translator(CleanIndentationsOption)
+        self.homogeneous_material_OPTION    = Var_Translator(HomogeneousMaterialOption)
+        self.global_variables_OPTION        = Var_Translator(GlobalVariablesOption)
+        self.Non_Linear_Option              = Var_Translator(NonLinearOption)
+        self.stress_strain_operations       = Var_Translator(StressStrainOperations)
+        self.contact_mesh_OPTION            = Var_Translator(ContactMeshOption)
+        self.concrete_test_OPTION           = Var_Translator(ConcreteTestOption)
+        self.concrete_test_OPTION           = Var_Translator(ConcreteTestOption)
+        self.search_radius_extension        = Var_Translator(DeltaOption)
+        self.MoveMeshFlag                   = True
 
-        if (HomogeneousMaterialOption == "ON"):
-            self.homogeneous_material_OPTION= 1
 
-        else:
-            self.homogeneous_material_OPTION= 0
+        if (self.search_radius_extension and Var_Translator(ContinuumOption)):
+            self.amplified_continuum_search_radius_extension = Var_Translator(ContinuumOption)
 
-        if (GlobalVariablesOption == "ON"):
-            self.global_variables_OPTION    = 1
+        if (Var_Translator(DeltaOption)):
+            self.delta_OPTION               = True
 
-        else:
-            self.global_variables_OPTION    = 0
+        if (Var_Translator(ContinuumOption)):
+            self.continuum_simulating_OPTION = True
 
         # MODEL
         self.model_part                     = model_part
         self.domain_size                    = domain_size
 
-        # BOUNDARY        
+        # BOUNDARY
         self.surface_normal_dir             = Vector(3)
-        self.surface_normal_dir[0]          = 0.0
-        self.surface_normal_dir[1]          = 1.0
-        self.surface_normal_dir[2]          = 0.0
+        self.surface_normal_dir[0]          = surface_normal_dir_x
+        self.surface_normal_dir[1]          = surface_normal_dir_y
+        self.surface_normal_dir[2]          = surface_normal_dir_z
         self.surface_point_coor             = Vector(3)
-        self.surface_point_coor[0]          = 0.0
-        self.surface_point_coor[1]          = 0.0
-        self.surface_point_coor[2]          = 0.0
-        self.surface_friction_angle         = 45
+        self.surface_point_coor[0]          = surface_point_coor_x
+        self.surface_point_coor[1]          = surface_point_coor_y
+        self.surface_point_coor[2]          = surface_point_coor_z
+        self.surface_friction_angle         = surface_friction_angle
+
 
         # GLOBAL PHISICAL ASPECTS
-
-        self.penalty_factor                 = 10.00
         self.gravity                        = Vector(3)
-        self.gravity[0]                     = 0.0
-        self.gravity[1]                     = - 9.81
-        self.gravity[2]                     = 0.0
+        self.gravity[0]                     = gravity_x
+        self.gravity[1]                     = gravity_y
+        self.gravity[2]                     = gravity_z
 
         # GLOBAL MATERIAL PROPERTIES
+        self.nodal_mass_coeff               = VirtualMassCoefficient
+        self.magic_factor                   = Var_Translator(MagicFactor)
 
-        self.damping_ratio                  = 0.00
-        self.nodal_mass_coeff               = 0.0
-        self.magic_factor                   = 1.0
+        if (self.global_variables_OPTION):
+            self.global_kn                  = global_kn
+            self.global_kt                  = global_kt
+            solver.global_kr                = global_kr
+            solver.global_rn                = global_rn
+            solver.global_rt                = global_rt
+            solver.global_rr                = global_rr
+            solver.global_fri_ang           = global_fri_ang
 
-        if (self.homogeneous_material_OPTION == "ON"):
-            self.general_density            = GeneralDensity
-            self.general_young_mod          = GeneralYoungModulus
-            self.general_poisson_ratio      = GeneralPoissonRatio
-            self.general_cohesion           = GeneralCohesion
-            self.general_rota_damp_ratio    = GeneralRollingFriction
-            self.general_rolling_friction   = GeneralRotaDampRatio
-            self.general_stat_friction_coef = GeneralStaticFrictionCoef
-            self.general_dyn_friction_coef  = GeneralDynamicFrictionCoef
-            self.general_restitution_coef   = GeneralRestitutionCoef
+        if (NormalForceCalculation == "Linear"):
+            self.force_calculation_type_id  = 0
+        elif (NormalForceCalculation == "Hertz"):
+            self.force_calculation_type_id  = 1
 
-        if (self.global_variables_OPTION == "ON"):
-            self.global_kn                  = 1000.0
-            self.global_kt                  = 1000.0
+        if (self.Non_Linear_Option):
+            solver.C1                       = C1
+            solver.C2                       = C2
+            solver.N1                       = N1
+            solver.N2                       = N2
+
+        if (NormalDampId == "ViscDamp"):
+
+            if (TangentialDampId == "ViscDamp"):
+                self.damp_id                = 11
+
+            else:
+                self.damp_id                = 10
+        else:
+
+            if (TangentialDampId == "ViscDamp"):
+                self.damp_id                = 1
+
+            else:
+                self.damp_id                = 0
+
+        if (RotaDampId == "LocalDamp"):
+            self.rota_damp_id               = 1
+
+        elif (RotaDampId == "RollingFric"):
+            self.rota_damp_id               = 2
+
+        else:
+            self.rota_damp_id               = 0
+
+        if (FailureCriterionOption == "Mohr-Coulomb"):
+            self.failure_criterion_OPTION   = 1
+
+        elif (FailureCriterionOption == "Uncoupled"):
+            self.failure_criterion_OPTION   = 2
+
+        self.tau_zero                       = TauZero
+        self.sigma_max                      = SigmaMax
+        self.sigma_min                      = SigmaMin
+        self.internal_fricc                 = InternalFricc
+
+        # CONCRETE TEST
+
+        if (Var_Translator(TriaxialOption)):
+          self.initial_pressure_time        = InitialTime
+          self.time_increasing_ratio        = IncreasingTemporaily
 
         # PRINTING VARIABLES
-        self.print_export_id                = 0
-        self.print_export_skin_sphere       = 0
-        self.force_calculation_type_id      = 1
-        self.damp_id                        = 1
-        self.rota_damp_id                   = 0
-        self.dummy_switch                   = 0
+        self.print_export_id                = Var_Translator(print_export_id)
+        self.print_export_skin_sphere       = Var_Translator(print_export_skin_sphere)
+        self.print_radial_displacement      = Var_Translator(print_radial_displacement)
 
         # TIME RELATED PARAMETERS
-        self.delta_time                     = 0.00001
-        self.max_delta_time                 = 0.05
-        self.fraction_delta_time            = 0.90
-        self.final_time                     = 3.0
-        self.time_increasing_ratio          = 15 # Percentage (%)
+        self.delta_time                     = max_time_step
+        self.max_delta_time                 = max_time_step
+        print 'max_delta_time'
+        print self.max_delta_time
+        self.final_time                     = final_time
+        self.time_increasing_ratio          = int(IncreasingTemporaily) # Percentage (%)
 
         # RESOLUTION METHODS AND PARAMETERS
-        self.enlargement_factor             = 1
-        self.n_step_search                  = 1
-        self.safety_factor                  = 1.0 # For critical time step
-        self.create_and_destroy             = particle_destructor_and_constructor();
+        self.n_step_search                  = int(search_step)
+        self.safety_factor                  = dt_safety_factor # For critical time step
+        self.create_and_destroy             = particle_destructor_and_constructor()
 
         # STRATEGIES
-        self.time_scheme                    = ForwardEulerScheme();
-        self.search_strategy                = OMP_DEMSearch();
+        self.search_strategy                = OMP_DEMSearch()
+
+        if (Integration_Scheme == 'forward_euler'):
+            self.time_scheme = ForwardEulerScheme()
+        elif (Integration_Scheme == 'mid_point_rule'):
+            self.time_scheme = MidPointScheme()
+        elif (Integration_Scheme == 'const_average_acc'):
+            self.time_scheme = ConstAverageAccelerationScheme()
+        else:
+            print('scheme not defined')
 
     ######################################################################
 
@@ -202,6 +268,12 @@ class ExplicitStrategy:
         self.model_part.ProcessInfo.SetValue(UNIFORM_MATERIAL_OPTION, self.homogeneous_material_OPTION)
         self.model_part.ProcessInfo.SetValue(NEIGH_INITIALIZED, 0);
         self.model_part.ProcessInfo.SetValue(TOTAL_CONTACTS, 0);
+        self.model_part.ProcessInfo.SetValue(CLEAN_INDENT_OPTION, self.clean_init_indentation_OPTION);
+        self.model_part.ProcessInfo.SetValue(ROTATION_SPRING_OPTION, self.rotation_spring_OPTION);
+
+
+        # TOLERANCES
+        self.model_part.ProcessInfo.SetValue(DISTANCE_TOLERANCE, 0);
 
         # BOUNDARY
         self.model_part.ProcessInfo.SetValue(LIMIT_SURFACE_OPTION, self.limit_surface_OPTION)
@@ -209,22 +281,14 @@ class ExplicitStrategy:
         self.model_part.ProcessInfo.SetValue(SURFACE_POINT_COOR, self.surface_point_coor)
         self.model_part.ProcessInfo.SetValue(SURFACE_FRICC, self.surface_friction_angle)
 
-        # GLOBAL PHISICAL ASPECTS
+        # GLOBAL PHYSICAL ASPECTS
         self.model_part.ProcessInfo.SetValue(GRAVITY, self.gravity)
+        self.model_part.ProcessInfo.SetValue(DEM_MAGIC_FACTOR, self.magic_factor)
 
         # GLOBAL MATERIAL PROPERTIES
 
-        if (self.homogeneous_material_OPTION == "ON"):
+        if(self.homogeneous_material_OPTION == "ON"):
             self.model_part.ProcessInfo.SetValue(NODAL_MASS_COEFF, self.nodal_mass_coeff)
-            self.model_part.ProcessInfo.SetValue(DEM_MAGIC_FACTOR, self.magic_factor)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_DENSITY, self.general_young_mod)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_YOUNG_MODULUS, self.general_poisson_ratio)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_POISSON_RATIO, self.general_cohesion)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_COHESION, self.general_rota_damp_ratio)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_ROLLING_FRICTION, self.general_rolling_friction)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_STATIC_FRICTION, self.general_stat_friction_coef)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_DYNAMIC_FRICTION, self.general_dyn_friction_coef)
-            #self.model_part.ProcessInfo.SetValue(GENERAL_RESTITUTION_COEF, self.general_restitution_coef)
             self.model_part.ProcessInfo.SetValue(NODAL_MASS_COEFF, self.magic_factor)
             self.model_part.ProcessInfo.SetValue(HISTORICAL_MIN_K, self.magic_factor)
 
@@ -248,7 +312,7 @@ class ExplicitStrategy:
         # RESOLUTION METHODS AND PARAMETERS
         # Creating the solution strategy
 
-        self.solver = ExplicitSolverStrategy(self.model_part, self.enlargement_factor, self.fraction_delta_time, self.n_step_search, self.safety_factor,
+        self.solver = ExplicitSolverStrategy(self.model_part, self.enlargement_factor, self.max_delta_time, self.n_step_search, self.safety_factor,
                                              self.MoveMeshFlag, self.time_scheme, self.search_strategy)
 
         self.solver.Initialize() # Calls the solver Initialized function (initializes all elements and performs other necessary tasks before iterating)
