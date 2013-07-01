a717c5620cc337c58aabc1a20c31eaf1ed9a9cae
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-07-01 14:02:50 +0000

    moving projector to swimming procedures

--- kratos/applications/swimming_DEM_application/python_scripts/swimming_DEM_procedures.py
+++ kratos/applications/swimming_DEM_application/python_scripts/swimming_DEM_procedures.py
@@ -19,3 +19,28 @@ def CalculateGlobalPorosity(ParticleModelPart, DomainVolume):
     balls_per_area = DomainVolume / n_balls
     output = [solid_volume, global_porosity, n_balls, balls_per_area]
     return output
+
+class ProjectionModule:
+
+    def __init__(self, FluidModelPart, ParticlesModelPart, DomainSize, NParticlesInDepth):
+
+        self.fluid_model_part     = FluidModelPart
+        self.particles_model_part = ParticlesModelPart
+        self.n_particles_in_depth = NParticlesInDepth
+
+        if (DomainSize == 3):
+            self.projector = BinBasedDEMFluidCoupledMapping3D()
+            self.bin_of_objects_fluid = BinBasedFastPointLocator3D(FluidModelPart)
+
+        else:
+            self.projector = BinBasedDEMFluidCoupledMapping2D()
+            self.bin_of_objects_fluid = BinBasedFastPointLocator2D(FluidModelPart)
+
+    def UpdateDatabase(self, HMin):
+        self.bin_of_objects_fluid.UpdateSearchDatabaseAssignedSize(HMin)
+
+    def ProjectFromFluid(self):
+        self.projector.InterpolationFromFluidMesh(self.fluid_model_part, self.particles_model_part, self.bin_of_objects_fluid)
+
+    def ProjectFromParticles(self):
+        self.projector.InterpolationFromDEMMesh(self.particles_model_part, self.fluid_model_part, self.n_particles_in_depth, self.bin_of_objects_fluid)
