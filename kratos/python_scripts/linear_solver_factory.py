11e877a72c314bd1e03491aaab25d487fcea87b1
rrossi <rrossi@cimne.upc.edu>
2013-07-02 16:07:15 +0000

    modified adding the AMGCL solver and cleaning up stuff

--- kratos/python_scripts/linear_solver_factory.py
+++ kratos/python_scripts/linear_solver_factory.py
@@ -4,10 +4,10 @@ from KratosMultiphysics import *
 #######################################################################################
 #######################################################################################
 def ConstructPreconditioner( configuration ):
-    try:
+    if hasattr(configuration, 'verbosity'):
       preconditioner_type = configuration.preconditioner_type
-    except LogicError:
-      print "missing preconditioner type. Either set the preconditioner type as None or to a given preconditioner"
+    else:
+      return None
       
       
     if(preconditioner_type == "None"):
@@ -27,8 +27,6 @@ def ConstructPreconditioner( configuration ):
 #######################################################################################
 def ConstructSolver( configuration ):
     solver_type = configuration.solver_type
-    max_it = configuration.max_iteration
-    tol    = configuration.tolerance
     scaling = configuration.scaling
     
     linear_solver = None
@@ -36,6 +34,8 @@ def ConstructSolver( configuration ):
     #######################################################################################
     if(solver_type == "Conjugate gradient"): 
         precond = ConstructPreconditioner( configuration )
+        max_it = configuration.max_iteration
+	tol    = configuration.tolerance
         if(precond == None):
             linear_solver = CGSolver(tol, max_it)
         else:
@@ -43,6 +43,8 @@ def ConstructSolver( configuration ):
     #######################################################################################
     elif(solver_type == "BiConjugate gradient stabilized"):  
         precond = ConstructPreconditioner( configuration )
+        max_it = configuration.max_iteration
+	tol    = configuration.tolerance
         if(precond == None):
           linear_solver = BICGSTABSolver(tol, max_it)
         else:
@@ -50,6 +52,8 @@ def ConstructSolver( configuration ):
     #######################################################################################
     elif(solver_type == "GMRES"):     
         precond = ConstructPreconditioner( configuration )
+        max_it = configuration.max_iteration
+	tol    = configuration.tolerance
         if(precond == None):
 	    linear_solver = GMRESSolver(tol, max_it)
         else:
@@ -67,6 +71,48 @@ def ConstructSolver( configuration ):
     elif(solver_type == "Super LU"):  
 	import KratosMultiphysics.ExternalSolversApplication
         linear_solver = KratosMultiphysics.ExternalSolversApplication.SuperLUSolver()
+    #######################################################################################
+    elif(solver_type == "AMGCL"):  
+	import KratosMultiphysics.ExternalSolversApplication
+	if(configuration.preconditioner_type != "None"):
+	    print "WARNING: preconditioner specified in preconditioner_type will not be used as it is not compatible with the AMGCL solver"
+	    
+        max_it = configuration.max_iteration
+	tol    = configuration.tolerance
+	
+	if hasattr(configuration, 'verbosity'):
+	    verbosity = configuration.verbosity
+	else:
+	    verbosity = 0
+	smoother_type  	= configuration.smoother_type #options are DAMPED_JACOBI, ILU0, SPAI
+		
+	if(smoother_type == "ILU0"): 
+	    amgcl_smoother = KratosMultiphysics.ExternalSolversApplication.AMGCLSmoother.ILU0
+	elif(smoother_type == "DAMPED_JACOBI"): 
+	    amgcl_smoother = KratosMultiphysics.ExternalSolversApplication.AMGCLSmoother.DAMPED_JACOBI
+	elif(smoother_type == "SPAI0"): 
+	    amgcl_smoother = KratosMultiphysics.ExternalSolversApplication.AMGCLSmoother.SPAI0
+	else:
+	    print "ERROR: smoother_type shall be one of ILU0, DAMPED_JACOBI, SPAI0"
+	    return None
+
+	krylov_type    	= configuration.krylov_type #options are GMRES, BICGSTAB, CG
+	if(krylov_type == "GMRES"): 
+	    amgcl_krylov_type = KratosMultiphysics.ExternalSolversApplication.AMGCLIterativeSolverType.GMRES
+	elif(krylov_type == "BICGSTAB"): 
+	    amgcl_krylov_type = KratosMultiphysics.ExternalSolversApplication.AMGCLIterativeSolverType.BICGSTAB
+	elif(krylov_type == "CG"): 
+	    amgcl_krylov_type = KratosMultiphysics.ExternalSolversApplication.AMGCLIterativeSolverType.CG
+	else:
+	    print "ERROR: krylov_type shall be one of GMRES, BICGSTAB, CG"
+	    return None
+
+	
+	if hasattr(configuration, 'gmres_krylov_space_dimension'):
+	    m = configuration.gmres_krylov_space_dimension
+	else:
+	    m = max_it
+        linear_solver = KratosMultiphysics.ExternalSolversApplication.AMGCLSolver(amgcl_smoother,amgcl_krylov_type,tol,max_it,verbosity,m) 
     #######################################################################################
     elif (solver_type == "Parallel MKL Pardiso"):  
 	import KratosMultiphysics.MKLSolversApplication
