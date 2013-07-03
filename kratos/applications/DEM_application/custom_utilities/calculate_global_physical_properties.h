3668149f3e3e66ad8d3e0c1023cc32f8942ee76b
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-07-03 09:35:30 +0000

    Commented Typename 's that caused compiling errors.

--- kratos/applications/DEM_application/custom_utilities/calculate_global_physical_properties.h
+++ kratos/applications/DEM_application/custom_utilities/calculate_global_physical_properties.h
@@ -67,8 +67,8 @@ class Spheric_Element_Global_Physics_Calculator
 
           #pragma omp parallel for
           for (int k = 0; k < OpenMPUtils::GetNumThreads(); k++){
-              typename ElementsArrayType::iterator it_begin = pElements.ptr_begin() + this->GetElementPartition()[k];
-              typename ElementsArrayType::iterator it_end   = pElements.ptr_begin() + this->GetElementPartition()[k + 1];
+              ElementsArrayType::iterator it_begin = pElements.ptr_begin() + this->GetElementPartition()[k];
+              ElementsArrayType::iterator it_end   = pElements.ptr_begin() + this->GetElementPartition()[k + 1];
 
               for (ElementsArrayType::iterator it = it_begin; it != it_end; ++it){
                   double particle_mass = (it)->GetGeometry()(0)->FastGetSolutionStepValue(NODAL_MASS);
