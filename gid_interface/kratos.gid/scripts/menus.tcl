fa9e1e73ec1761153c33f53e60de54439e518508
escolano <escolano@cimne.upc.edu>
2013-07-17 21:18:24 +0000

    -cleaned and reordered code -removed menus 'Entities group editor' and 'Function editor' (now kratos menu is moved before 'Help' because are used dev_kit menu funcions instead of a direct access to GiD menus variables!!) -Added initial splash that show the problemtype version (declared in kratos.xml) -Add to menu 'Help' entries for 'About' and 'Go to kratos web'

--- gid_interface/kratos.gid/scripts/menus.tcl
+++ gid_interface/kratos.gid/scripts/menus.tcl
@@ -39,46 +39,7 @@ namespace eval ::kmtb:: {
 
 }
 
-proc ::kmtb::ChangePreprocessMenu {dir} {
-   
-    # Add some menu in preprocess menu
-    ::kmtb::AddMenuToPreprocessMenu $dir
-    
-    # Create the process toolbar
-    ::kmtb::CreatePreprocessModelTBar $dir
-    
-    # Update the menu properties
-    ::GiDMenu::UpdateMenus
-}
-
-proc ::kmtb::AddMenuToPreprocessMenu {dir} {
-    global NumMenus MenuNames MenuEntries MenuCommands MenuAcceler MenuIcons
-    
-    set mname [= "Kratos#C#menu"]
-    
-    # Insert a interface name entry after meshing menu
-    set pos [lsearch -exact $MenuNames [_ "Mesh#C#menu"]]
-    set pos2 [lsearch -exact $MenuNames $mname]
-    ;# WarnWinText "pos:$pos pos2:$pos2"
-    if {($pos!="-1") && ($pos2==-1)} {
-     	set MenuNames [linsert $MenuNames [incr pos] $mname]
-     	set NumMenus [llength $MenuNames ]
-	# WarnWinText "MenuNames:$MenuNames NumMenus:$NumMenus"
-     	# Move all the predefined commands one position
-     	for {set ii $NumMenus} {$ii > $pos} {incr ii -1} {
-	    set jj [expr $ii - 1]
-	    ;# WarnWinText "ii:$ii jj:$jj"
-     	    catch { set MenuEntries($ii)  $MenuEntries($jj)  }
-     	    catch { set MenuCommands($ii) $MenuCommands($jj) }
-     	    catch { set MenuAcceler($ii)  $MenuAcceler($jj)  }
-	    catch { set MenuIcons($ii) $MenuIcons($jj)  }
-     	    set MenuEntries($jj)  ""
-     	    set MenuCommands($jj) ""
-     	    set MenuAcceler($jj)  ""
-	    set MenuIcons($jj) ""
-     	}
-    }
-
+proc ::kmtb::ChangePreprocessMenu {dir} {   
     # Disabled all base options
     GidChangeDataLabel "Data units" ""
     GidChangeDataLabel "Interval" ""
@@ -88,113 +49,19 @@ proc ::kmtb::AddMenuToPreprocessMenu {dir} {
     GidChangeDataLabel "Problem Data" ""
     GidChangeDataLabel "Local axes" ""
     
-    # Create the interface name for workshop menu
-    set MenuEntries($pos) [list [= "Entities group editor#C#menu"]... \
-			       --- \
-			       [= "Model properties#C#menu"] \
-			       [= "Material database#C#menu"] \
-			       --- \
-			       [= "Function editor#C#menu"] \
-			       [= "Project settings#C#menu"]]
-                   
+    GiDMenu::Create [= "Kratos#C#menu"] PRE -1 =
+    GiDMenu::InsertOption [= "Kratos#C#menu"] [list [= "Model properties#C#menu"]] 0 PRE KMProps::StartBaseWindow "" "" replace =
+    GiDMenu::InsertOption [= "Kratos#C#menu"] [list [= "Material database#C#menu"]] 1 PRE {KMProps::StartBaseWindow Materials} "" "" replace =
+    GiDMenu::InsertOption [= "Kratos#C#menu"] "---" 2 PRE "" "" "" replace =
+    GiDMenu::InsertOption [= "Kratos#C#menu"] [list [= "Project settings#C#menu"]] 3 PRE kps::InitSettingWindow "" "" replace =    
     
-     	set MenuCommands($pos) [list [list -np- ::WinUtils::OpenGiDGroupTab] \
-				    "" \
-				    [list -np- ::KMProps::StartBaseWindow] \
-				    [list -np- ::KMProps::StartBaseWindow Materials] \
-				    "" \
-				    "" \
-				    [list  -np- ::kps::InitSettingWindow]]
-   
-    
-    if { [kipt::CurvesModule ] } {
-        set MenuCommands($pos) [list [list -np- $grw window open] \
-            "" \
-            [list -np- ::KMProps::StartBaseWindow] \
-            [list -np- ::KMProps::StartBaseWindow Materials] \
-            "" \
-            [list -np- ::KCurves::PruebaCurvas] \
-            [list  -np- ::kps::InitSettingWindow]]
-    }
-    
-    set MenuAcceler($pos) {"" "" "" "" "" "" ""}
-    set MenuIcons($pos) {"" "" "" "" "" "" ""}
-
+    GiDMenu::InsertOption "Help" [list ---] end PREPOST {} "" "" insertafter
+    GiDMenu::InsertOption "Help" [list [_ "Visit %s web" $::kipt::ProgramName]...] end PREPOST [list VisitWeb $::kipt::Web] "" "" insertafter
+    GiDMenu::InsertOption "Help" [list [concat [_ "About"] " " $::kipt::ProgramName]...] end PREPOST kipt::About "" "" insertafter
+        
+    # Update the menu properties
+    ::GiDMenu::UpdateMenus
 }
-
-proc ::kmtb::DeleteMenu {pos position {prepost "PRE"}} {
-    # For base global  
-    global MenuEntries MenuCommands MenuAcceler MenuIcons
-
-    # Delete the menu entry and command
-    # WarnWinText "MenuEntries:$MenuEntries($pos)\npos:$pos\nposition:$position"
-    # WarnWinText "MenuCommands:$MenuCommands($pos)\npos:$pos\nposition:$position"
-    # WarnWinText "MenuAcceler:$MenuAcceler($pos)\npos:$pos\nposition:$position"
-    # WarnWinText "MenuIcons:$MenuIcons($pos)\npos:$pos\nposition:$position"
-    if {[info exists MenuEntries($pos)]} {
-	set llen [expr [llength $MenuEntries($pos)]-1]
-	# WarnWinText "MenuEntries length $llen"
-	if {$position<=$llen} {
-	    set MenuEntries($pos) [lreplace $MenuEntries($pos) $position $position]
-	}
-    }
-    if {[info exists MenuCommands($pos)]} {
-	set llen [expr [llength $MenuCommands($pos)]-1]
-	# WarnWinText "MenuCommands length $llen"
-	if {$position<=$llen} {
-	    set MenuCommands($pos) [lreplace $MenuCommands($pos) $position $position]
-	}
-    }
-    if {[info exists MenuAcceler($pos)]} {
-	set llen [expr [llength $MenuAcceler($pos)]-1]
-	# WarnWinText "MenuAcceler length $llen"
-	if {$position<=$llen} {
-	    set MenuAcceler($pos) [lreplace $MenuAcceler($pos) $position $position]
-	}
-    }
-    if {[info exists MenuIcons($pos)]} {
-	set llen [expr [llength $MenuIcons($pos)]-1]
-	# WarnWinText "MenuIcons length $llen"
-	if {$position<=$llen} {
-	    set MenuIcons($pos) [lreplace $MenuIcons($pos) $position $position]
-	}
-    }
-    # WarnWinText "Despues MenuEntries:$MenuEntries($pos)\npos:$pos\nposition:$position"
-}
-
-proc ::kmtb::DeleteSubMenu {pos position {prepost "PRE"}} {
-     # For base global  
-     global MenuEntries MenuCommands MenuAcceler MenuIcons
-    
-     if {[info exists MenuEntries($pos,$position)]} {
-	 # Reset submenu
-	 array unset MenuEntries $pos,$position
-     }
-     if {[info exists MenuCommands($pos,$position)]} {
-	 array unset MenuCommands $pos,$position   
-     }
-     if {[info exists MenuAcceler($pos,$position)]} {
-	 array unset MenuAcceler $pos,$position 
-     }
-     if {[info exists MenuIcons($pos,$position)]} {
-	 array unset MenuIcons $pos,$position
-     }
- }
-
-proc ::kmtb::AddToMenu {pos clist {prepost "PRE"}} {
-     # For base global  
-     global MenuEntries MenuCommands MenuAcceler MenuIcons
-
-     set translationfunc =
-
-     # try to add ---
-     if {[lindex $MenuEntries($pos) end] !="---"} {
-	 lappend MenuEntries($pos) "---"
-	 lappend MenuCommands($pos) ""
-	 lappend MenuAcceler($pos) ""
-	 lappend MenuIcons($pos) ""
-     }
- }
 
 proc ::kmtb::CreatePreprocessModelTBar {dir {type "DEFAULT INSIDELEFT"}} {
     global KBitmapsNames KBitmapsCommands KBitmapsHelp 
@@ -205,55 +72,55 @@ proc ::kmtb::CreatePreprocessModelTBar {dir {type "DEFAULT INSIDELEFT"}} {
     
     if { [kipt::CurvesModule ] } {
         set KBitmapsNames(0) "$KPriv(imagesdir)/groups.gif $KPriv(imagesdir)/new_props.gif $KPriv(imagesdir)/maticon.gif $KPriv(imagesdir)/nonlinear.gif \
-				--- $KPriv(imagesdir)/openrunsim.gif $KPriv(imagesdir)/runsimulation.gif $KPriv(imagesdir)/runsiminfo.gif \
-				$KPriv(imagesdir)/stop.gif"
-	
-	    set KBitmapsCommands(0) [list \
-					 [list -np- ::WinUtils::OpenGiDGroupTab Toolbar] \
-					 [list -np- ::KMProps::StartBaseWindow] \
-					 [list -np- ::KMProps::StartBaseWindow Materials] \
-					 [list -np- ::KMProps::StartBaseWindow Curve] \
-					 "" \
-					 [list -np- RunWin] \
-					 {Utilities Calculate} \
-					 [list -np- PWViewOutput] \
-					 {Utilities CancelProcess}]
+                                --- $KPriv(imagesdir)/openrunsim.gif $KPriv(imagesdir)/runsimulation.gif $KPriv(imagesdir)/runsiminfo.gif \
+                                $KPriv(imagesdir)/stop.gif"
+        
+            set KBitmapsCommands(0) [list \
+                                         [list -np- ::WinUtils::OpenGiDGroupTab Toolbar] \
+                                         [list -np- ::KMProps::StartBaseWindow] \
+                                         [list -np- ::KMProps::StartBaseWindow Materials] \
+                                         [list -np- ::KMProps::StartBaseWindow Curve] \
+                                         "" \
+                                         [list -np- RunWin] \
+                                         {Utilities Calculate} \
+                                         [list -np- PWViewOutput] \
+                                         {Utilities CancelProcess}]
 
 
         set KBitmapsHelp(0) [list [= "Define the group properties using the group editor"] \
-				 [= "Define the model properties"] \
-				 [= "Define the material properties"] \
-				 [= "Define the curve properties"] \
-				 "" \
-				 [= "Open the process control window"] \
-				 [= "Run the simulation"] \
-				 [= "View process info"] \
-				 [= "Cancel process"]] 
+                                 [= "Define the model properties"] \
+                                 [= "Define the material properties"] \
+                                 [= "Define the curve properties"] \
+                                 "" \
+                                 [= "Open the process control window"] \
+                                 [= "Run the simulation"] \
+                                 [= "View process info"] \
+                                 [= "Cancel process"]] 
     } else {
 
-	set KBitmapsNames(0) "$KPriv(imagesdir)/groups.gif $KPriv(imagesdir)/new_props.gif $KPriv(imagesdir)/maticon.gif \
-				--- $KPriv(imagesdir)/openrunsim.gif $KPriv(imagesdir)/runsimulation.gif $KPriv(imagesdir)/runsiminfo.gif \
-				$KPriv(imagesdir)/stop.gif"
-	    
-	    set KBitmapsCommands(0) [list \
-					 [list -np- ::WinUtils::OpenGiDGroupTab Toolbar] \
-					 [list -np- ::KMProps::StartBaseWindow] \
-					 [list -np- ::KMProps::StartBaseWindow Materials] \
-					 "" \
-					 [list -np- RunWin] \
-					 {Utilities Calculate} \
-					 [list -np- PWViewOutput] \
-					 {Utilities CancelProcess}]
-	
-	set KBitmapsHelp(0) [list [= "Define the group properties using the group editor"] \
-				 [= "Define the model properties"] \
-				 [= "Define the material properties"] \
-				 "" \
-				 [= "Open the process control window"] \
-				 [= "Run the simulation"] \
-				 [= "View process info"] \
-				 [= "Cancel process"]] 
-	
+        set KBitmapsNames(0) "$KPriv(imagesdir)/groups.gif $KPriv(imagesdir)/new_props.gif $KPriv(imagesdir)/maticon.gif \
+                                --- $KPriv(imagesdir)/openrunsim.gif $KPriv(imagesdir)/runsimulation.gif $KPriv(imagesdir)/runsiminfo.gif \
+                                $KPriv(imagesdir)/stop.gif"
+            
+            set KBitmapsCommands(0) [list \
+                                         [list -np- ::WinUtils::OpenGiDGroupTab Toolbar] \
+                                         [list -np- ::KMProps::StartBaseWindow] \
+                                         [list -np- ::KMProps::StartBaseWindow Materials] \
+                                         "" \
+                                         [list -np- RunWin] \
+                                         {Utilities Calculate} \
+                                         [list -np- PWViewOutput] \
+                                         {Utilities CancelProcess}]
+        
+        set KBitmapsHelp(0) [list [= "Define the group properties using the group editor"] \
+                                 [= "Define the model properties"] \
+                                 [= "Define the material properties"] \
+                                 "" \
+                                 [= "Open the process control window"] \
+                                 [= "Run the simulation"] \
+                                 [= "View process info"] \
+                                 [= "Cancel process"]] 
+        
     }
     
     # prefix values:
@@ -261,29 +128,29 @@ proc ::kmtb::CreatePreprocessModelTBar {dir {type "DEFAULT INSIDELEFT"}} {
     # Post       Only active in the postprocessor
     # PrePost    Active Always
     
-     set prefix "Pre"
-     set name "KPreprocessModel"
-     set winname "::kmtb::CreatePreprocessModelTBar"
+     set prefix Pre
+     set name KPreprocessModel
+     set winname ::kmtb::CreatePreprocessModelTBar
      set KPriv(ToolBars,PreprocessModelTBar) [CreateOtherBitmaps ${name}bar \
-						   [= "Model definition toolbar"] \
-						   KBitmapsNames KBitmapsCommands \
-						   KBitmapsHelp $dir "$winname [list $dir]" \
-						   $type $prefix]
+                                                   [= "Model definition toolbar"] \
+                                                   KBitmapsNames KBitmapsCommands \
+                                                   KBitmapsHelp $dir "$winname [list $dir]" \
+                                                   $type $prefix]
     
     AddNewToolbar "${name}bar" ${prefix}${winname}WindowGeom "$winname [list $dir]" \
-	[= "Model definition toolbar"]
+        [= "Model definition toolbar"]
 }
 
 proc ::kmtb::EndCreatePreprocessTBar {} {
     global KPriv
    
-    set name "KPreprocessModel"
-    set winname "::kmtb::CreatePreprocessModelTBar"
+    set name KPreprocessModel
+    set winname ::kmtb::CreatePreprocessModelTBar
     catch { 
-	ReleaseToolbar "${name}bar"
-	rename $winname ""
-	destroy $KPriv(ToolBars,PreprocessModelTBar)
-	unset KPriv(ToolBars,PreprocessModelTBar)
+        ReleaseToolbar ${name}bar
+        rename $winname ""
+        destroy $KPriv(ToolBars,PreprocessModelTBar)
+        unset KPriv(ToolBars,PreprocessModelTBar)
     }
 }
 
