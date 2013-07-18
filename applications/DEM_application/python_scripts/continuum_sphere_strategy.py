b32748fa139fa585fa275306f8d6f4fbce389fbd
gcasas <gcasas@cimne.upc.edu>
2013-07-18 15:38:35 +0000

    updating some names

--- applications/DEM_application/python_scripts/continuum_sphere_strategy.py
+++ applications/DEM_application/python_scripts/continuum_sphere_strategy.py
@@ -55,7 +55,7 @@ def AddVariables(model_part, Param):
 
     # FLAGS
     model_part.AddNodalSolutionStepVariable(GROUP_ID)            # Differencied groups for plotting, etc..
-
+    model_part.AddNodalSolutionStepVariable(ERASE_FLAG)
     # ONLY VISUALITZATION
 
     if (Var_Translator(Param.PostExportId)):
@@ -245,7 +245,7 @@ class ExplicitStrategy:
 
         self.n_step_search                  = int(Param.TimeStepsPerSearchStep)
         self.safety_factor                  = Param.DeltaTimeSafetyFactor # For critical time step
-        self.create_and_destroy             = particle_destructor_and_constructor()
+        self.create_and_destroy             = ParticleCreatorDestructor()
 
 
         # STRATEGIES
