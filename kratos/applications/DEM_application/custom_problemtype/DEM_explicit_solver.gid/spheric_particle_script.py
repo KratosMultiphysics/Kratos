4d1384efb7fa1c4586d3e7aa2157b3fc34ee0393
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-07-19 09:24:40 +0000

    updated

--- kratos/applications/DEM_application/custom_problemtype/DEM_explicit_solver.gid/spheric_particle_script.py
+++ kratos/applications/DEM_application/custom_problemtype/DEM_explicit_solver.gid/spheric_particle_script.py
@@ -4,11 +4,12 @@ import sys
 import math
 import matplotlib
 from numpy import *
-from pylab import *  
+from pylab import *
 
 from KratosMultiphysics import *
 from KratosMultiphysics.DEMApplication import *
 
+from DEM_explicit_solver_var import *
 import DEM_explicit_solver_var as Param
 import DEM_procedures
 proc = DEM_procedures.Procedures(Param)
@@ -18,11 +19,15 @@ proc = DEM_procedures.Procedures(Param)
 # Defining a model part for the solid part
 
 my_timer = Timer();
-balls_model_part = ModelPart("SolidPart");  
+balls_model_part = ModelPart("SolidPart");
 
 # Importing the strategy object
 
-import sphere_strategy as SolverStrategy
+if (Param.ElementType == "SphericParticle3D" or Param.ElementType == "CylinderParticle2D"):
+    import sphere_strategy as SolverStrategy
+
+elif (Param.ElementType == "SphericContinuumParticle3D"):
+    import continuum_sphere_strategy as SolverStrategy
 
 SolverStrategy.AddVariables(balls_model_part, Param)
 
@@ -30,21 +35,21 @@ SolverStrategy.AddVariables(balls_model_part, Param)
 
 if (Param.OutputFileType == "Binary"):
     gid_mode = GiDPostMode.GiD_PostBinary
-  
+
 else:
     gid_mode = GiDPostMode.GiD_PostAscii
-  
+
 if (Param.Multifile == "multiple_files"):
     multifile = MultiFileFlag.MultipleFiles
-  
+
 else:
     multifile = MultiFileFlag.SingleFile
 
 deformed_mesh_flag = WriteDeformedMeshFlag.WriteDeformed
 write_conditions   = WriteConditionsFlag.WriteConditions
 
-gid_io = GidIO(Param.problem_name, gid_mode, multifile, deformed_mesh_flag, write_conditions)
-model_part_io_solid = ModelPartIO(Param.problem_name)
+gid_io = GidIO(problem_name, gid_mode, multifile, deformed_mesh_flag, write_conditions)
+model_part_io_solid = ModelPartIO(problem_name)
 model_part_io_solid.ReadModelPart(balls_model_part)
 
 # Setting up the buffer size: SHOULD BE DONE AFTER READING!!!
@@ -63,24 +68,24 @@ solver = SolverStrategy.ExplicitStrategy(balls_model_part, Param) #here, solver 
 # Creating necessary directories
 
 main_path        = os.getcwd()
-post_path        = str(main_path) + '/' + str(Param.problem_name) + '_Post_Files'
-list_path        = str(main_path) + '/' + str(Param.problem_name) + '_Post_Lists'
-neigh_list_path  = str(main_path) + '/' + str(Param.problem_name) + '_Neigh_Lists'
-data_and_results = str(main_path) + '/' + str(Param.problem_name) + '_Results_and_Data'
-graphs_path      = str(main_path) + '/' + str(Param.problem_name) + '_Graphs'	
-MPI_results      = str(main_path) + '/' + str(Param.problem_name) + '_MPI_results'	
+post_path        = str(main_path) + '/' + str(problem_name) + '_Post_Files'
+list_path        = str(main_path) + '/' + str(problem_name) + '_Post_Lists'
+neigh_list_path  = str(main_path) + '/' + str(problem_name) + '_Neigh_Lists'
+data_and_results = str(main_path) + '/' + str(problem_name) + '_Results_and_Data'
+graphs_path      = str(main_path) + '/' + str(problem_name) + '_Graphs'
+MPI_results      = str(main_path) + '/' + str(problem_name) + '_MPI_results'
 
 for directory in [post_path, list_path, neigh_list_path, data_and_results, graphs_path, MPI_results]:
 
-    if not os.path.isdir(directory):    
+    if not os.path.isdir(directory):
         os.makedirs(str(directory))
 
 os.chdir(list_path)
 
-multifile       = open(Param.problem_name + '_all' + '.post.lst', 'w')
-multifile_5     = open(Param.problem_name + '_5'   + '.post.lst', 'w')
-multifile_10    = open(Param.problem_name + '_10'  + '.post.lst', 'w')
-multifile_50    = open(Param.problem_name + '_50'  + '.post.lst', 'w')
+multifile       = open(problem_name + '_all' + '.post.lst', 'w')
+multifile_5     = open(problem_name + '_5'   + '.post.lst', 'w')
+multifile_10    = open(problem_name + '_10'  + '.post.lst', 'w')
+multifile_50    = open(problem_name + '_50'  + '.post.lst', 'w')
 
 multifile.write('Multiple\n')
 multifile_5.write('Multiple\n')
@@ -103,33 +108,54 @@ export_model_part = balls_model_part
 
 #------------------------------------------DEM_PROCEDURES FUNCTIONS & INITIALITZATIONS--------------------------------------------------------
 
-Pressure = 0.0  
-Pressure = proc.GiDSolverTransfer(balls_model_part, solver, Param)
-
 if (Param.ModelDataInfo == "ON"):
     os.chdir(data_and_results)
     proc.ModelData(balls_model_part, solver)       # calculates the mean number of neighbours the mean radius, etc..
     os.chdir(main_path)
-       
+
 print 'Initializing Problem....'
 
 solver.Initialize()
 
 # Initialization of physics monitor and of the initial position of the center of mass
 
-physics_calculator = spheric_element_global_physics_calculator(balls_model_part)
+physics_calculator = SphericElementGlobalPhysicsCalculator(balls_model_part)
+
+# Constructing a creator/destructor object
+
+creator_destructor = ParticleCreatorDestructor()
+
+# Porosity Utils
+
+domain_volume = 2.9 * 0.194 * 0.194 * math.pi
+porosity_utils = DEM_procedures.GranulometryUtils(domain_volume, balls_model_part)
+porosity_utils.PrintCurrentData()
+
+# Bounding box
+
+time_of_destruction = 2.0
+marking_for_destruction_not_performed = True
+manual_b_box_low = Array3()
+manual_b_box_high = Array3()
+manual_b_box_low[0]= -1.0;
+manual_b_box_low[1]= -1.0;
+manual_b_box_low[2]= -1.0;
+manual_b_box_high[0]= 1.0;
+manual_b_box_high[1]= 2.0;
+manual_b_box_high[2]= 1.0;
+
 properties_list = []
 
 print 'Initialitzation Complete' + '\n'
 
 dt = balls_model_part.ProcessInfo.GetValue(DELTA_TIME)
 
-step                   = 0 
-time                   = 0.0 
+step                   = 0
+time                   = 0.0
 time_old_print         = 0.0
 initial_pr_time        = timer.clock()
 initial_real_time      = timer.time()
-    
+
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 #-----------------------SINGLE FILE MESH AND RESULTS INITIALITZATION-------------------------------------------------------------------
@@ -142,15 +168,15 @@ if (Param.Multifile == "single_file"):
       gid_io.InitializeMesh(0.0)
       gid_io.WriteMesh(contact_model_part.GetMesh());
       gid_io.FinalizeMesh()
-      gid_io.InitializeResults(0.0, contact_model_part.GetMesh()); 
+      gid_io.InitializeResults(0.0, contact_model_part.GetMesh());
 
   gid_io.InitializeMesh(0.0)
   gid_io.WriteSphereMesh(balls_model_part.GetMesh())
   gid_io.FinalizeMesh()
-  gid_io.InitializeResults(0.0, balls_model_part.GetMesh()); 
+  gid_io.InitializeResults(0.0, balls_model_part.GetMesh());
 
 #------------------------------------------------------------------------------------------
- 
+
 ###########################################################################################
 #                                                                                         #
 #                                    MAIN LOOP                                            #
@@ -160,59 +186,61 @@ os.chdir(main_path)
 
 print ('Main loop starts at instant: ' + str(initial_pr_time) + '\n')
 
-total_steps_expected = int(Param.FinalTime / dt)
+total_steps_expected = int(FinalTime / dt)
 
 print ('Total number of TIME STEPs expected in the calculation are: ' + str(total_steps_expected) + ' if time step is kept ' + '\n' )
 
-while (time < Param.FinalTime):
- 
+while (time < FinalTime):
+
     dt = balls_model_part.ProcessInfo.GetValue(DELTA_TIME) # Possible modifications of DELTA_TIME
     time = time + dt
     balls_model_part.CloneTimeStep(time)
     balls_model_part.ProcessInfo[TIME_STEPS] = step
 
     #########################_SOLVE_#########################################4
-    os.chdir(main_path) 
+    os.chdir(main_path)
     solver.Solve()
     #########################TIME CONTROL######################################4
-   
+
     incremental_time = (timer.time() - initial_real_time) - prev_time
 
-    if (incremental_time > Param.ControlTime):
+    if (time > time_of_destruction and marking_for_destruction_not_performed):
+        creator_destructor.MarkParticlesForErasingGivenBoundingBox(balls_model_part, manual_b_box_low, manual_b_box_high)
+        marking_for_destruction_not_performed = False
+
+    if (incremental_time > ControlTime):
         percentage = 100 * (float(step) / total_steps_expected)
-      
+
         print 'Real time calculation: ' + str(timer.time() - initial_real_time)
         print 'Percentage Completed: '  + str(percentage) + ' %'
         print "TIME STEP = "            + str(step) + '\n'
-        sys.stdout.flush()
-        
+
         prev_time = (timer.time() - initial_real_time)
-  
-    if ((timer.time() - initial_real_time > 60) and first_print == True):    
-        first_print = False    
+
+    if ((timer.time() - initial_real_time > 60) and first_print == True):
+        first_print = False
         estimated_sim_duration = 60 * (total_steps_expected / step) # seconds
-    
+
         print('The calculation total estimated time is ' + str(estimated_sim_duration) + 'seconds' + '\n')
         print('in minutes:'        + str(estimated_sim_duration / 60) + 'min.' + '\n')
         print('in hours:'        + str(estimated_sim_duration / 3600) + 'hrs.' + '\n')
-        print('in days:'        + str(estimated_sim_duration / 86400) + 'days' + '\n') 
-        sys.stdout.flush()
-        
-	if (estimated_sim_duration / 86400 > 2.0):
-	    print('WARNING!!!:       VERY LASTING CALCULATION' + '\n')
-    
+        print('in days:'        + str(estimated_sim_duration / 86400) + 'days' + '\n')
+
+        if (estimated_sim_duration / 86400 > 2.0):
+            print('WARNING!!!:       VERY LASTING CALCULATION' + '\n')
+
     #########################CONCRETE_TEST_STUFF#########################################4
-    
+
     os.chdir(data_and_results)
-    
+
     total_force = 0.0
     force_node  = 0.0
 
-    if (Param.FixVelocitiesOption == 'OFF'):
+    if (FixVelocitiesOption == 'OFF'):
         TimePercentageFixVelocities = 0.0
-      
-    os.chdir(list_path)    
-    multifile.write(Param.problem_name + '_' + str(time) + '.post.bin\n')   
+
+    os.chdir(list_path)
+    multifile.write(problem_name + '_' + str(time) + '.post.bin\n')
     os.chdir(main_path)
 
   #########################___GiD IO____#########################################4
@@ -224,61 +252,61 @@ while (time < Param.FinalTime):
 
         properties_list = proc.MonitorPhysicalProperties(balls_model_part, physics_calculator, properties_list)
 
-        if (index_5 == 5):       
-            multifile_5.write(Param.problem_name + '_' + str(time) + '.post.bin\n')
+        if (index_5 == 5):
+            multifile_5.write(problem_name + '_' + str(time) + '.post.bin\n')
             index_5 = 0
-        
-        if (index_10 == 10):       
-            multifile_10.write(Param.problem_name + '_' + str(time) + '.post.bin\n')
+
+        if (index_10 == 10):
+            multifile_10.write(problem_name + '_' + str(time) + '.post.bin\n')
             index_10 = 0
-        
+
         if (index_50 == 50):
-            multifile_50.write(Param.problem_name + '_' + str(time) + '.post.bin\n')
+            multifile_50.write(problem_name + '_' + str(time) + '.post.bin\n')
             index_50 = 0
-      
+
         index_5  += 1
         index_10 += 1
         index_50 += 1
 
-        if (Param.Multifile == "multiple_files"):
+        if (Multifile == "multiple_files"):
             gid_io.FinalizeResults()
 
         os.chdir(graphs_path)
 
-        if (Param.PrintNeighbourLists == "ON"): # Printing neighbours id's
+        if (PrintNeighbourLists == "ON"): # Printing neighbours id's
             os.chdir(neigh_list_path)
             neighbours_list = open('neigh_list_' + str(time), 'w')
-  
+
             for elem in balls_model_part.Elements:
                 ID = (elem.Id)
                 Neigh_ID = elem.GetValue(NEIGHBOURS_IDS)
-      
+
             for i in range(len(Neigh_ID)):
                 neighbours_list.write(str(ID) + ' ' + str(Neigh_ID[i]) + '\n')
-  
+
             neighbours_list.close()
 
         os.chdir(post_path)
 
-        if (Param.Multifile == "multiple_files"):
+        if (Multifile == "multiple_files"):
             gid_io.InitializeMesh(time)
             gid_io.WriteSphereMesh(balls_model_part.GetMesh())
             gid_io.FinalizeMesh()
-            gid_io.InitializeResults(time, balls_model_part.GetMesh()); 
- 
+            gid_io.InitializeResults(time, balls_model_part.GetMesh());
+
         proc.PrintingVariables(gid_io, export_model_part, time)
-        os.chdir(main_path)     
-              
+        os.chdir(main_path)
+
         time_old_print = time
-         
+
     step += 1
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 
-#-----------------------FINALITZATION OPERATIONS-------------------------------------------------------------------------------------- 
+#-----------------------FINALITZATION OPERATIONS--------------------------------------------------------------------------------------
 proc.PlotPhysicalProperties(properties_list, graphs_path)
 
-if (Param.Multifile == "single_file"):
+if (Multifile == "single_file"):
     gid_io.FinalizeResults()
 
 multifile.close()
@@ -286,6 +314,12 @@ multifile_5.close()
 multifile_10.close()
 multifile_50.close()
 os.chdir(main_path)
+
+# Porosity
+
+domain_volume = 2 * 0.194 * 0.194 * math.pi
+porosity_utils.UpdateData(domain_volume)
+porosity_utils.PrintCurrentData()
 
 elapsed_pr_time 	= timer.clock() - initial_pr_time
 elapsed_real_time 	= timer.time() - initial_real_time
@@ -297,4 +331,4 @@ print 'Elapsed real time: '                           + str(elapsed_real_time)
 
 print (my_timer)
 
-print "ANALYSIS COMPLETED" 
+print "ANALYSIS COMPLETED"
