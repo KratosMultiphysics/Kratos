7780dc87b3774c24284a244124a31339551c8c52
jwolf <jwolf@cimne.upc.edu>
2013-08-21 07:20:15 +0000

    Adaption of Kratos-Kratos-FSI wrapper in order to use Kratos as CSM-solver

--- applications/empire_application/python_scripts/empire_wrapper.py
+++ applications/empire_application/python_scripts/empire_wrapper.py
@@ -7,23 +7,30 @@ CheckForPreviousImport()
 
 class EmpireWrapper:
 
-    def __init__(self,fluid_model_part,libempire_api):
+    # -------------------------------------------------------------------------------------------------
+    def __init__(self,model_part,libempire_api):
 	self.libempire_api        = libempire_api
-	self.fluid_model_part     = fluid_model_part 
+	self.model_part           = model_part 
 	self.interface_model_part = ModelPart("InterfaceModelPart");  
-	self.ale_wrapper_process  = ALEWrapperProcess(self.fluid_model_part,self.interface_model_part)	
+	self.ale_wrapper_process  = ALEWrapperProcess(self.model_part,self.interface_model_part)
+    # -------------------------------------------------------------------------------------------------	
 
+    # -------------------------------------------------------------------------------------------------
     # extract interface nodes and conditions from the fluid model part which have a flag "IS_INTERFACE"
     def extractInterface(self):
 	self.ale_wrapper_process.ExtractInterface()
+    # -------------------------------------------------------------------------------------------------
 
+    # -------------------------------------------------------------------------------------------------
     # convert char* to python-compatible string
     def getUserDefinedText(self,stringText):
 	empireFunc = self.libempire_api.EMPIRE_API_getUserDefinedText
 	empireFunc.restype = c_char_p
 	text = empireFunc(stringText)
 	return text
+    # -------------------------------------------------------------------------------------------------
 
+    # -------------------------------------------------------------------------------------------------
     def recvDisplacement(self): 
 	# initialize vector storing the displacements
 	size = self.interface_model_part.GetNodes().Size() * 3
@@ -39,21 +46,30 @@ class EmpireWrapper:
 		disp[0] = c_displacements[3*i+0]
 		disp[1] = c_displacements[3*i+1]
 		disp[2] = c_displacements[3*i+2]
-		nodes.SetSolutionStepValue(DISPLACEMENT,0,disp);
-		i = i + 1
 
-    def sendForces(self):
-	# extract forces from interface nodes as result of the CFD simulation
-	forces = []
-	self.ale_wrapper_process.ExtractForcesFromModelPart( forces )
+		disp[0] = 0.01
+		disp[1] = 0.01
+		disp[2] = 0.01
+
+		nodes.SetSolutionStepValue(DISPLACEMENT,0,disp)
+		i = i + 1
+    # -------------------------------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------------------------------
+    def sendPressure(self):
+	# extract pressure from interface nodes as result of the CFD simulation
+	pressure = []
+	self.ale_wrapper_process.ExtractPressureFromModelPart( pressure )
 
 	# convert list containg the forces to ctypes
-	c_forces = (c_double * len(forces))(*forces)
-	c_size = len(c_forces)
+	c_pressure = (c_double * len(pressure))(*pressure)
+	c_size = len(c_pressure)
 
-	# send forces to empire
-	self.libempire_api.EMPIRE_API_sendDataField("defaultField", c_size , c_forces)
+	# send pressure to empire
+	self.libempire_api.EMPIRE_API_sendDataField("defaultField", c_size , c_pressure)
+    # -------------------------------------------------------------------------------------------------
 
+    # -------------------------------------------------------------------------------------------------
     def sendMesh(self):
 	numNodes 	= []
 	numElems 	= []
@@ -71,12 +87,51 @@ class EmpireWrapper:
 	c_nodes = (c_double * len(nodes))(*nodes)
 	c_nodeIDs = (c_int * len(nodeIDs))(*nodeIDs)
 	c_numNodesPerElem = (c_int * len(numNodesPerElem))(*numNodesPerElem)
-	c_elems = (c_int * len(elems))(*elems)	
+	c_elems = (c_int * len(elems))(*elems)
 
 	# send mesh information to empire
 	self.libempire_api.EMPIRE_API_sendMesh("defaultMesh",c_numNodes[0], c_numElems[0], c_nodes, c_nodeIDs, c_numNodesPerElem, c_elems)
+    # -------------------------------------------------------------------------------------------------
 
+    # -------------------------------------------------------------------------------------------------
     def recvConvergenceSignal(self):
 	# receive convergence information from empire
 	isConvergent = self.libempire_api.EMPIRE_API_recvConvergenceSignal()
 	return isConvergent
+    # -------------------------------------------------------------------------------------------------
+
+#######################################################################################################
+    # Functions required for Kratos-Kratos-FSI (whereas Kratos solves also the structural side)
+    # -------------------------------------------------------------------------------------------------
+    def recvPressure(self):
+	# initialize pressure variable
+	size = self.interface_model_part.GetNodes().Size() * 1
+	c_pressure = (c_double * size)(0)
+
+	# receive pressure from empire	
+	self.libempire_api.EMPIRE_API_recvDataField("defaultField", size, c_pressure)
+
+	pressure = Vector(1)
+	i = 0
+	# assign pressure to nodes of interface for current time step
+	for nodes in (self.interface_model_part).Nodes:
+		pressure[0] = c_pressure[i]
+
+		nodes.SetSolutionStepValue(POSITIVE_FACE_PRESSURE,0,pressure[0]);
+		i = i + 1
+    # -------------------------------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------------------------------	
+    def sendDisplacements(self):
+	# extract displacements from interface nodes as result of the CFD simulation
+	displacements = []
+	self.ale_wrapper_process.ExtractDisplacementsFromModelPart( displacements )
+
+	# convert list containg the displacements to ctypes
+	c_displacements = (c_double * len(displacements))(*displacements)
+	c_size = len(c_displacements)
+
+	# send displacements to empire
+	self.libempire_api.EMPIRE_API_sendDataField("defaultField", c_size , c_displacements)
+    # -------------------------------------------------------------------------------------------------
+#######################################################################################################
