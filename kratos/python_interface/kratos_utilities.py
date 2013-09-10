a7957cd8fbd3d705489c89b0992233ebeac7cb04
rrossi <rrossi@cimne.upc.edu>
2013-09-10 16:06:49 +0000

    changing to use the function import_solver

--- kratos/python_interface/kratos_utilities.py
+++ kratos/python_interface/kratos_utilities.py
@@ -1,5 +1,5 @@
-def load_solver( SolverSettings ):
+def import_solver( SolverSettings ):
 	"""this function imports a solver named "solver_type" from SolverSettings
-	solver_type is expected to be the FILENAME of the solver to be loaded"""
+	solver_type is expected to be the FILENAME of the solver to be imported"""
 	obj =  __import__(SolverSettings.solver_type)
 	return obj
