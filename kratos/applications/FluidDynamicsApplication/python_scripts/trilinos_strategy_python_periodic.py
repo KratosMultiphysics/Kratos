1dd9c385620cac5b68552562bc5dee3ab8d6b753
jcotela <jcotela@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-09-10 13:03:15 +0000

    Cleaning of prints and indentation.

--- kratos/applications/FluidDynamicsApplication/python_scripts/trilinos_strategy_python_periodic.py
+++ kratos/applications/FluidDynamicsApplication/python_scripts/trilinos_strategy_python_periodic.py
@@ -1,4 +1,4 @@
-#importing the Kratos Library
+# importing the Kratos Library
 from KratosMultiphysics import *
 from KratosMultiphysics.mpi import *
 from KratosMultiphysics.TrilinosApplication import *
@@ -9,10 +9,25 @@ CheckForPreviousImport()
 # import base class
 from trilinos_strategy_python import SolvingStrategyPython
 
+
 class SolvingStrategyPeriodic(SolvingStrategyPython):
-  def __init__(self,domain_size,model_part,time_scheme,linear_solver,convergence_criteria,CalculateReactionsFlag,ReformDofSetAtEachStep,MoveMeshFlag,Comm,guess_row_size,periodic_var):
-    # add my own builder and solver
-    self.builder_and_solver = TrilinosBlockBuilderAndSolverPeriodic(Comm,guess_row_size,linear_solver,periodic_var)
-    #initialize skiping builder and solver
-    SolvingStrategyPython.__init__(self,"NONE",model_part,time_scheme,linear_solver,convergence_criteria,CalculateReactionsFlag,ReformDofSetAtEachStep,MoveMeshFlag,Comm,guess_row_size)
-    
+
+    def __init__(
+        self, domain_size, model_part, time_scheme, linear_solver, convergence_criteria,
+            CalculateReactionsFlag, ReformDofSetAtEachStep, MoveMeshFlag, Comm, guess_row_size, periodic_var):
+        # add my own builder and solver
+        self.builder_and_solver = TrilinosBlockBuilderAndSolverPeriodic(
+            Comm, guess_row_size, linear_solver, periodic_var)
+        # initialize skiping builder and solver
+        SolvingStrategyPython.__init__(
+            self,
+            "NONE",
+            model_part,
+            time_scheme,
+            linear_solver,
+            convergence_criteria,
+            CalculateReactionsFlag,
+            ReformDofSetAtEachStep,
+            MoveMeshFlag,
+            Comm,
+            guess_row_size)
