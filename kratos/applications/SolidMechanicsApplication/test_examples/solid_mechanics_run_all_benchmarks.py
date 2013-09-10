d5d5e84f5b8ce1967adc7aad57b9295c02114cc2
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-09-10 10:23:37 +0000

    adding script to run benchmarks for Solid Mechanics Applicatino

---
+++ kratos/applications/SolidMechanicsApplication/test_examples/solid_mechanics_run_all_benchmarks.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+kratos_benchmarking_path = '../../../benchmarking' 
+sys.path.append(kratos_benchmarking_path)
+import benchmarking
+
+
+def Run():
+        Msg = ""
+        Text = "== Solid Mechanics ==========\n"
+
+        ###############################################################################
+        #VMS2D element test
+        
+        Text += "scordelis low roof test: "
+        os.chdir("scordelis.gid")  
+        sys.path.append(os.getcwd())
+
+        print "running the scordelis low roof benchmark test..."
+        Msg = benchmarking.RunBenchmark("run_test.py", "min_displacements.txt")
+
+        if (Msg == True):
+            Text += "OK\n"
+            print "scordelis low roof test succesful"
+        else:
+            Text += "FAILED\n"
+            Text += Msg
+            Text += "\n\n"
+            print "scordelis low roof test FAILED"
+
+
+        os.chdir("..")
+
+        ################################################################
+        print "resume of all of the examples for the Solid Mechanics application :"
+        print Text
+        return Text
+
+if __name__=='__main__':
+  Run();
+
+
