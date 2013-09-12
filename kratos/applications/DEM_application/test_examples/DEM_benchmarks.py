dc3120313efdd345b4e32c2f871fe34bac8564a9
jirazabal <jirazabal@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-09-12 09:35:55 +0000

    Benchmark automatized

--- kratos/applications/DEM_application/test_examples/DEM_benchmarks.py
+++ kratos/applications/DEM_application/test_examples/DEM_benchmarks.py
@@ -5,7 +5,6 @@ kratos_benchmarking_path = '../../../benchmarking'
 sys.path.append(kratos_benchmarking_path)
 import benchmarking
 
-
 def Run():
 	Msg = ""
 	Text = "========== DEM ==========\n"
@@ -15,6 +14,12 @@ def Run():
 	Text += "DEM element no damp test: "
 	os.chdir("two_balls_no_damp.gid")	
 	sys.path.append(os.getcwd())
+
+	import update_problem
+	print "Problem updated..."
+
+	import update_script
+	print "Python script updated..."
 
 	print "running the DEM two_balls_no_damp test..."
         Msg = benchmarking.RunBenchmark("two_balls_no_damp.py", "two_balls_no_damp_ref.txt")
@@ -32,65 +37,89 @@ def Run():
 	os.chdir("..")
         ###############################################################################
 
-	Text += "DEM element normal damp test: "
-	os.chdir("two_balls_normal_damp.gid")	
-	sys.path.append(os.getcwd())
-
-	print "running the DEM two_balls_normal_damp test..."
-        Msg = benchmarking.RunBenchmark("two_balls_normal_damp.py", "two_balls_normal_damp_ref.txt")
-
-        if (Msg == True):
-            Text += "OK\n"
-            print "two_balls_normal_damp test succesful"
-        else:
-            Text += "FAILED\n"
-            Text += Msg
-            Text += "\n\n"
-            print "two_balls_normal_damp example test FAILED"
-
-
-	os.chdir("..")
-        ###############################################################################
-
-	Text += "DEM element rotation no tangential damp test: "
-	os.chdir("rotating_ball_no_tangent_damp.gid")	
-	sys.path.append(os.getcwd())
-
-	print "running the DEM rotating_ball_no_tangent_damp test..."
-        Msg = benchmarking.RunBenchmark("rotating_ball_no_tangent_damp.py", "rotating_ball_no_tangent_damp_ref.txt")
-
-        if (Msg == True):
-            Text += "OK\n"
-            print "rotating_ball_no_tangent_damp test succesful"
-        else:
-            Text += "FAILED\n"
-            Text += Msg
-            Text += "\n\n"
-            print "rotating_ball_no_tangent_damp test FAILED"
-
-
-	os.chdir("..")
-        ###############################################################################
-
-	Text += "DEM element rotation with rolling friction test: "
-	os.chdir("rotating_ball_rolling_friction.gid")	
-	sys.path.append(os.getcwd())
-
-	print "running the DEM rotating_ball_rolling_friction test..."
-        Msg = benchmarking.RunBenchmark("rotating_ball_rolling_friction.py", "rotating_ball_rolling_friction_ref.txt")
-
-        if (Msg == True):
-            Text += "OK\n"
-            print "rotating_ball_rolling_friction test succesful"
-        else:
-            Text += "FAILED\n"
-            Text += Msg
-            Text += "\n\n"
-            print "rotating_ball_rolling_friction test FAILED"
-
-
-	os.chdir("..")
-        ###############################################################################
+	#~ Text += "DEM element normal damp test: "
+	#~ os.chdir("two_balls_normal_damp.gid")	
+	#~ sys.path.append(os.getcwd())
+#~ 
+	#~ import update_script
+	#~ print "Python script updated..."
+#~ 
+	#~ print "Variables script is updated..."
+#~ 
+	#~ import two_balls_normal_damp_build_reference
+	#~ print "References updated..."
+#~ 
+	#~ print "running the DEM two_balls_normal_damp test..."
+        #~ Msg = benchmarking.RunBenchmark("two_balls_normal_damp_benchmark.py", "two_balls_normal_damp_ref.txt")
+#~ 
+        #~ if (Msg == True):
+            #~ Text += "OK\n"
+            #~ print "two_balls_normal_damp test succesful"
+        #~ else:
+            #~ Text += "FAILED\n"
+            #~ Text += Msg
+            #~ Text += "\n\n"
+            #~ print "two_balls_normal_damp example test FAILED"
+#~ 
+#~ 
+	#~ os.chdir("..")
+        #~ ###############################################################################
+#~ 
+	#~ Text += "DEM element rotation no tangential damp test: "
+	#~ os.chdir("rotating_ball_no_tangent_damp.gid")	
+	#~ sys.path.append(os.getcwd())
+#~ 
+	#~ import update_script
+	#~ print "Python script updated..."
+#~ 
+	#~ print "Variables script is updated..."
+#~ 
+	#~ import rotating_ball_no_tangent_damp_reference
+	#~ print "References updated..."
+#~ 
+	#~ print "running the DEM rotating_ball_no_tangent_damp test..."
+        #~ Msg = benchmarking.RunBenchmark("rotating_ball_no_tangent_damp.py", "rotating_ball_no_tangent_damp_ref.txt")
+#~ 
+        #~ if (Msg == True):
+            #~ Text += "OK\n"
+            #~ print "rotating_ball_no_tangent_damp test succesful"
+        #~ else:
+            #~ Text += "FAILED\n"
+            #~ Text += Msg
+            #~ Text += "\n\n"
+            #~ print "rotating_ball_no_tangent_damp test FAILED"
+#~ 
+#~ 
+	#~ os.chdir("..")
+        #~ ###############################################################################
+#~ 
+	#~ Text += "DEM element rotation with rolling friction test: "
+	#~ os.chdir("rotating_ball_rolling_friction.gid")	
+	#~ sys.path.append(os.getcwd())
+#~ 
+	#~ import update_script
+	#~ print "Python script updated..."
+#~ 
+	#~ print "Variables script is updated..."
+#~ 
+	#~ import rotating_ball_rolling_friction_reference
+	#~ print "References updated..."
+#~ 
+	#~ print "running the DEM rotating_ball_rolling_friction test..."
+        #~ Msg = benchmarking.RunBenchmark("rotating_ball_rolling_friction.py", "rotating_ball_rolling_friction_ref.txt")
+#~ 
+        #~ if (Msg == True):
+            #~ Text += "OK\n"
+            #~ print "rotating_ball_rolling_friction test succesful"
+        #~ else:
+            #~ Text += "FAILED\n"
+            #~ Text += Msg
+            #~ Text += "\n\n"
+            #~ print "rotating_ball_rolling_friction test FAILED"
+#~ 
+#~ 
+	#~ os.chdir("..")
+        #~ ###############################################################################
 
 	# Add other examples here
 
