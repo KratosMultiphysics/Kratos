d3d515134bc7e8c5e586912c8f2b6d2023644961
Josep Maria <josep-m-carbonell@users.noreply.github.com>
2013-09-21 06:51:07 +0000

    serialization was recursive

--- applications/SolidMechanicsApplication/custom_constitutive/hyperelastic_plastic_plane_strain_2D_law.hpp
+++ applications/SolidMechanicsApplication/custom_constitutive/hyperelastic_plastic_plane_strain_2D_law.hpp
@@ -284,12 +284,12 @@ private:
 
     virtual void save(Serializer& rSerializer) const
     {
-        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, HyperElasticPlasticPlaneStrain2DLaw);
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, HyperElasticPlastic3DLaw);
     }
 
     virtual void load(Serializer& rSerializer)
     {
-        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, HyperElasticPlasticPlaneStrain2DLaw);
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, HyperElasticPlastic3DLaw);
     }
 
 
