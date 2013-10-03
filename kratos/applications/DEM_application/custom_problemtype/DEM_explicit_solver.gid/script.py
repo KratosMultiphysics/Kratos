697e0cb3c765bd2db29c6d5feaf85f31735c4f3e
msantasusana <msantasusana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-10-03 15:26:24 +0000

    cylindrical continuum particle introduced

--- kratos/applications/DEM_application/custom_problemtype/DEM_explicit_solver.gid/script.py
+++ kratos/applications/DEM_application/custom_problemtype/DEM_explicit_solver.gid/script.py
@@ -5,7 +5,7 @@ from DEM_explicit_solver_var import *
 
 if (ElementType == "SphericParticle3D" or ElementType == "CylinderParticle2D"):
     import spheric_particle_script
-elif (ElementType == "SphericContinuumParticle3D"):
+elif (ElementType == "SphericContinuumParticle3D" or ElementType == "CylinderContinuumParticle2D"):
     import continuum_spheric_particle_script
   
   
