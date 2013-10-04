ff67a611d74b7bf6809efdc89045f7ae6c9ff282
msantasusana <msantasusana@cimne.upc.edu>
2013-10-04 14:55:32 +0000

    corrected

--- applications/DEM_application/custom_elements/cylinder_particle.h
+++ applications/DEM_application/custom_elements/cylinder_particle.h
@@ -89,18 +89,16 @@ namespace Kratos
       ///@{
       void Initialize();
       void CalculateRightHandSide(VectorType& rRightHandSideVector,ProcessInfo& rCurrentProcessInfo);
-      void EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& rCurrentProcessInfo);
-      void MassMatrix(MatrixType& rMassMatrix, ProcessInfo& rCurrentProcessInfo);
-      void DampMatrix(MatrixType& rDampMatrix, ProcessInfo& rCurrentProcessInfo);
-      void GetDofList( DofsVectorType& ElementalDofList, ProcessInfo& CurrentProcessInfo );
-      void FinalizeSolutionStep(ProcessInfo& rCurrentProcessInfo);
+      
+      
+      
+      
+      
       void Calculate(const Variable<double>& rVariable, double& Output, const ProcessInfo& rCurrentProcessInfo);
       void Calculate(const Variable<array_1d<double, 3 > >& rVariable, array_1d<double, 3 > & Output, const ProcessInfo& rCurrentProcessInfo);
       void Calculate(const Variable<Vector >& rVariable, Vector& Output, const ProcessInfo& rCurrentProcessInfo);
       void Calculate(const Variable<Matrix >& rVariable, Matrix& Output, const ProcessInfo& rCurrentProcessInfo);
 
-      int mContinuumGroup;
-      int* mpFailureId;
 
       ///@}
       ///@name Access
@@ -141,96 +139,13 @@ namespace Kratos
 
       //void SetInitialContacts(int case_opt, ProcessInfo& rCurrentProcessInfo);
 
-      virtual void InitializeSolutionStep(ProcessInfo& rCurrentProcessInfo);
-      virtual void MemberDeclarationFirstStep(ProcessInfo& rCurrentProcessInfo);
-      virtual void ComputeNewNeighboursHistoricalData();
-      void CalculateMaxIndentation(double& rCurrentMaxIndentation, const double& rTolerance);
-      virtual void ComputeBallToBallContactForce(   array_1d<double, 3>& rContactForce, array_1d<double, 3>& rContactMoment, array_1d<double, 3>& InitialRotaMoment, array_1d<double, 3>& MaxRotaMoment, ProcessInfo& rCurrentProcessInfo); 
-      void ComputeBallToSurfaceContactForce(array_1d<double, 3>& rContactForce, array_1d<double, 3>& rContactMoment, array_1d<double, 3>& InitialRotaMoment, array_1d<double, 3>& MaxRotaMoment, ProcessInfo& rCurrentProcessInfo);
+     
+      
+            
+      
       //virtual void ComputeParticleBlockContactForce(const ProcessInfo& rCurrentProcessInfo);
       //virtual void ComputeParticleRotationSpring(   const ProcessInfo& rCurrentProcessInfo);
 
-      virtual void EvaluateDeltaDisplacement(double DeltDisp[3],
-                                double RelVel[3],
-                                double NormalDir[3],
-                                double OldNormalDir[3],
-                                double LocalCoordSystem[3][3],
-                                double OldLocalCoordSystem[3][3],
-                                const array_1d<double, 3> &other_to_me_vect,
-                                const array_1d<double, 3> &vel,
-                                const array_1d<double, 3> &delta_displ,
-                                ParticleWeakIteratorType neighbour_iterator);
-      
-      virtual void NormalForceCalculation(double LocalElasticContactForce[3],double kn, double indentation, int mElasticityType);
-
-      virtual void DisplacementDueToRotation(double DeltDesp[3],
-                                double OldNormalDir[3],
-                                double OldLocalCoordSystem[3][3],
-                                const double &other_radius,
-                                const double &dt,
-                                const array_1d<double, 3> &angl_vel,
-                                ParticleWeakIteratorType neighbour_iterator);
-
-      virtual void ComputeMoments(double LocalElasticContactForce[3],
-                                  double GlobalElasticContactForces[3],
-                                  double InitialRotaMoment[3],
-                                  double MaxRotaMoment[3],
-                                  double LocalCoordSystem[3][3],
-                                  const double &other_radius,
-                                  array_1d<double, 3>& rContactMoment,
-                                  ParticleWeakIteratorType neighbour_iterator);
-
-      virtual void CustomInitialize( );
-      virtual void CustomCalculateRightHandSide(array_1d<double, 3>& contact_force, array_1d<double, 3>& contact_moment);
-
-      virtual void AddUpForcesAndProject(double LocalCoordSystem[3][3],
-                                VectorArray3Double &GlobalContactForceMatrix,
-                                double LocalContactForce[3],
-                                double LocalElasticContactForce[3],
-                                double GlobalContactForce[3],
-                                double GlobalElasticContactForce[3],
-                                double ViscoDampingLocalContactForce[3],
-                                double ViscoDampingGlobalContactForce[3],
-                                array_1d<double, 3> &rContactForce,
-                                const double &i_neighbour_count);
-
-      virtual void CalculateViscoDamping(double LocalRelVel[3],
-                                                  double ViscoDampingLocalContactForce[3],
-                                                  double indentation,
-                                                  double equiv_visco_damp_coeff_normal,
-                                                  double equiv_visco_damp_coeff_tangential,
-                                                  bool sliding);
-
-
-
-      bool mDeltaOption;
-      int mDampType;
-      int mElasticityType;
-      int mRotationOption;
-      int mRotationDampType;
-      int mGlobalVariablesOption;
-      int mCriticalTimeOption;
-      int mUniformMaterialOption;
-      int mInitializedVariablesFlag;
-      double mRadius;
-      double mRealMass;
-      double mSqrtOfRealMass;
-      double mMomentOfInertia;
-      double mRollingFriction;
-      double mYoung;
-      double mPoisson;
-      double mFrictionAngle;
-      double mTgOfFrictionAngle;
-      double mLnOfRestitCoeff;
-      double mMagicFactor;
-      double mGlobalKn;
-      double mGlobalKt;
-      double mGlobalAuxNormToTang;
-      int mLimitSurfaceOption;
-      int mRotationSpringOption;
-      vector<int> mOldNeighbourIds;
-      vector< array_1d<double, 3> > mOldNeighbourContactForces;
-      //ParticleWeakVectorType mrNeighbours;
 
       ///@name Protected static Member Variables
       ///@{
