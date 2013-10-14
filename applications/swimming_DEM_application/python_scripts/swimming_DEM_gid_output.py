63c17885b4bad3d293b7c3b5b54f3716515be5e4
gcasas <gcasas@cimne.upc.edu>
2013-10-14 15:23:28 +0000

    gid output

---
+++ applications/swimming_DEM_application/python_scripts/swimming_DEM_gid_output.py
@@ -0,0 +1,84 @@
+import os
+from KratosMultiphysics import *
+from KratosMultiphysics.SwimmingDEMApplication import *
+import gid_output
+
+class SwimmingDEMGiDOutput(gid_output.GiDOutput):
+
+    def __init__(self,
+                 file_name,
+                 vol_output = True,
+                 post_mode = "Binary",
+                 multifile = "Single",
+                 deformed_mesh = False,
+                 write_conditions = True
+                 ):
+        gid_output.GiDOutput.__init__(self,
+                 file_name,
+                 vol_output,
+                 post_mode,
+                 multifile,
+                 deformed_mesh,
+                 write_conditions)
+       
+    def initialize_swimming_DEM_results(self,DEM_model_part,mixed_model_part):
+        
+        if self.multi_file == MultiFileFlag.SingleFile:
+            mesh_name = 0.0
+            self.io.InitializeMesh(mesh_name)
+            self.io.WriteMesh(mixed_model_part.GetMesh())
+            self.io.WriteSphereMesh( DEM_model_part.GetMesh())
+            self.io.FinalizeMesh(mesh_name)
+            self.io.InitializeResults(mesh_name, mixed_model_part.GetMesh())
+
+        # Initialize list file
+        with open(self.listfilename,"w") as listfile:
+            if self.multi_file == MultiFileFlag.MultipleFiles:
+                listfile.write("Multiple\n")
+            elif self.multi_file == MultiFileFlag.SingleFile:
+                listfile.write("Single\n")
+                
+        if self.multi_file == MultiFileFlag.SingleFile:
+            if self.post_mode == GiDPostMode.GiD_PostBinary:
+                self.write_step_to_list()
+            else:
+                self.write_step_to_list(0)
+               
+    def write_swimming_DEM_results(self,label,fluid_model_part,DEM_model_part,mixed_model_part,fluid_nodal_variables,DEM_nodal_variables,mixed_nodal_variables, fluid_gp_variables):
+       #label = str(label) #it should be a C double
+        # update cut data if necessary
+        out_model_part = self.get_out_model_part(fluid_model_part)
+
+        # update cut data if necessary
+        if not self.volume_output:
+            self.cut_app.UpdateCutData(out_model_part,fluid_model_part)
+
+        if self.multi_file == MultiFileFlag.MultipleFiles:
+            self.io.InitializeMesh( label )
+            self.io.WriteSphereMesh( DEM_model_part.GetMesh() )
+            self.io.WriteMesh( mixed_model_part.GetMesh() )
+            self.io.FinalizeMesh()
+            self.io.InitializeResults(label,mixed_model_part.GetMesh())
+
+        for var in fluid_nodal_variables:
+            kratos_variable = globals()[var]
+            self._write_nodal_results(label,fluid_model_part,kratos_variable)
+            
+        for var in DEM_nodal_variables:
+            kratos_variable = globals()[var]
+            self._write_nodal_results(label,DEM_model_part,kratos_variable)
+            
+        for var in mixed_nodal_variables:
+            kratos_variable = globals()[var]
+            self._write_nodal_results(label,mixed_model_part,kratos_variable) 
+            
+        #for var in fluid_gp_variables:
+         #   kratos_variable = globals()[var]
+         #   self._write_gp_results(label,fluid_model_part,kratos_variable)
+
+        if self.multi_file == MultiFileFlag.MultipleFiles:
+            self._finalize_results()
+
+            with open(self.listfilename,"w") as listfile:
+                self.write_step_to_list(label)
+       
