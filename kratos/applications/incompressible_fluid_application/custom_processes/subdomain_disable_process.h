557e6b41f1bf8134f10641cd159e235e378f9126
pryzhakov <pryzhakov@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-10-28 13:26:06 +0000

    clean version

--- kratos/applications/incompressible_fluid_application/custom_processes/subdomain_disable_process.h
+++ kratos/applications/incompressible_fluid_application/custom_processes/subdomain_disable_process.h
@@ -28,6 +28,7 @@
 #include "includes/condition.h"
 #include "includes/model_part.h"
 #include "utilities/geometry_utilities.h"
+#include "processes/find_nodal_neighbours_process.h"
 #include "incompressible_fluid_application.h"
 
 namespace Kratos
@@ -101,58 +102,106 @@ public:
 
     void SaveReducedPart(ModelPart& full_model_part, ModelPart& reduced_model_part)
     {
-        KRATOS_TRY
-
+        KRATOS_TRY        
+       
+	/////////////////////////////////////////////////////////////////////////
+	//clear reduced_model_part
         reduced_model_part.Conditions().clear();
         reduced_model_part.Elements().clear();
         reduced_model_part.Nodes().clear();
-        reduced_model_part.Conditions().reserve(full_model_part.Conditions().size());
-        reduced_model_part.Elements().reserve(full_model_part.Elements().size());
-        reduced_model_part.Nodes().reserve(full_model_part.Nodes().size());
+      
+	//change the name of the var to another one  - otherwise confusing
+	for(ModelPart::NodesContainerType::iterator in = full_model_part.NodesBegin() ; in != full_model_part.NodesEnd() ; ++in)
+	{	  	
+	in->FastGetSolutionStepValue(DISABLE)=false;
+	}
 
-        int n_int=0;
-        int n_disabled=0;
+	int n_nodes=0;
 
-        for(ModelPart::NodesContainerType::iterator in = full_model_part.NodesBegin() ; in != full_model_part.NodesEnd() ; ++in)
+        for(ModelPart::ElementsContainerType::iterator im = full_model_part.ElementsBegin() ;
+                im != full_model_part.ElementsEnd() ; ++im)
         {
-            in->FastGetSolutionStepValue(DISABLE)=false;
-        }
-        for(ModelPart::ElementsContainerType::iterator im = full_model_part.ElementsBegin() ; im != full_model_part.ElementsEnd() ; ++im)
-        {
-            n_int=im->GetGeometry()[0].FastGetSolutionStepValue(IS_INTERFACE);
-            n_int+=im->GetGeometry()[1].FastGetSolutionStepValue(IS_INTERFACE);
-            n_int+=im->GetGeometry()[2].FastGetSolutionStepValue(IS_INTERFACE);
-            if(n_int==3)
+	    int n_int=0;
+	    int n_nodes=im->GetGeometry().size();
+
+	    for (int i=0; i<n_nodes; i++)
+		n_int+=im->GetGeometry()[i].FastGetSolutionStepValue(IS_INTERFACE);   
+
+	    //KRATOS_WATCH(n_int);
+	    //if the element has at least one non-interface node, save the element and mark all its node with a disable flag (change flag name.. coz in reality its "enabled")
+            if (n_int<n_nodes)
             {
-                im->GetGeometry()[0].FastGetSolutionStepValue(MATERIAL_VARIABLE)=true;
-                im->GetGeometry()[1].FastGetSolutionStepValue(MATERIAL_VARIABLE)=true;
-                im->GetGeometry()[2].FastGetSolutionStepValue(MATERIAL_VARIABLE)=true;
+                reduced_model_part.AddElement(*(im.base())); 
+		for (int i=0;i<n_nodes;i++)
+			{
+			im->GetGeometry()[i].FastGetSolutionStepValue(DISABLE)=true;
+			}               
             }
-            if (n_int<3)
-            {
-                reduced_model_part.Elements().push_back(*(im.base()));
-            }
-            if (n_int>3)
+	    //KRATOS_WATCH(n_int)
+	    //KRATOS_WATCH(n_nodes)
+            if (n_int>n_nodes)
                 KRATOS_ERROR(std::logic_error,  "Number of DISABLE flags cant exceed number of the element nodes.... " , "");
-            if (n_int<3)
-            {
-                for (int i=0; i<3; i++)
-                {
-                    im->GetGeometry()[i].FastGetSolutionStepValue(DISABLE)=true;
-                }
-            }
+
         }
-        for(ModelPart::NodesContainerType::iterator in = full_model_part.NodesBegin() ;
+
+	for(ModelPart::NodesContainerType::iterator in = full_model_part.NodesBegin() ; in != full_model_part.NodesEnd() ; ++in)
+        {
+
+            if (in->FastGetSolutionStepValue(DISABLE)==true)
+            {
+                reduced_model_part.AddNode(*(in.base()));       
+	    }
+	}
+       
+        for(ModelPart::PropertiesContainerType::iterator i_properties = full_model_part.PropertiesBegin() ;
+                i_properties != full_model_part.PropertiesEnd() ; ++i_properties)
+        {
+            reduced_model_part.AddProperties(*(i_properties.base()));
+
+        }
+	
+	//find neighbors within reduced model part
+	if (n_nodes==3)
+		{
+		FindNodalNeighboursProcess N_FINDER=FindNodalNeighboursProcess(reduced_model_part, 9, 20);
+		N_FINDER.Execute();
+		}
+	else if (n_nodes==4)
+		{
+		FindNodalNeighboursProcess N_FINDER=FindNodalNeighboursProcess(reduced_model_part, 20, 30);
+		N_FINDER.Execute();
+		}
+
+
+
+	//THIS PART EXCLUDES FROM THE SYSTEM SOLUTION (FOR AUX_VEL) the NODES WHOSE ALL NEIGHBORS ARE FIXED	
+	for(ModelPart::NodesContainerType::iterator in = full_model_part.NodesBegin() ;
                 in != full_model_part.NodesEnd() ; ++in)
         {
-            n_disabled=in->FastGetSolutionStepValue(DISABLE);
-            if (n_disabled==1.0)
+
+            int n_int=in->FastGetSolutionStepValue(IS_INTERFACE);
+
+            if (n_int==1.0)
             {
-                reduced_model_part.Nodes().push_back(*(in.base()));
-            }
-        }
-
-
+		WeakPointerVector< Node<3> >& neighb_nodes = in->GetValue(NEIGHBOUR_NODES);
+                int n_neighbor_nodes=neighb_nodes.size();
+		int n_fixed=0;
+		array_1d<double,3> av_vel=ZeroVector(3);
+		for( WeakPointerVector< Node<3> >::iterator i =	neighb_nodes.begin(); i != neighb_nodes.end(); i++)
+                {
+                    n_fixed+=	i->IsFixed(AUX_VEL_X);
+		    av_vel+=i->FastGetSolutionStepValue(AUX_VEL);
+                }
+		if (n_neighbor_nodes==n_fixed)
+			{
+			av_vel/=n_neighbor_nodes;
+			in->FastGetSolutionStepValue(AUX_VEL)=av_vel;
+			in->Fix(AUX_VEL_X);
+			in->Fix(AUX_VEL_Y);
+			in->Fix(AUX_VEL_Z);
+			}
+            }   
+	}    
 
 
         KRATOS_CATCH("")
