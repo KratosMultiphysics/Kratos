31fe14d17922118386d9f03b97b82d175613e9e9
msantasusana <msantasusana@cimne.upc.edu>
2013-10-31 10:03:00 +0000

    activate search

--- applications/DEM_application/python_scripts/sphere_strategy.py
+++ applications/DEM_application/python_scripts/sphere_strategy.py
@@ -408,7 +408,8 @@ class ExplicitStrategy:
         self.model_part.ProcessInfo.SetValue(UNIFORM_MATERIAL_OPTION, self.homogeneous_material_option)
         self.model_part.ProcessInfo.SetValue(NEIGH_INITIALIZED, 0);
         self.model_part.ProcessInfo.SetValue(TOTAL_CONTACTS, 0);
-        self.model_part.ProcessInfo.SetValue(CLEAN_INDENT_OPTION, self.clean_init_indentation_option);        
+        self.model_part.ProcessInfo.SetValue(CLEAN_INDENT_OPTION, self.clean_init_indentation_option);
+        self.model_part.ProcessInfo.SetValue(ACTIVATE_SEARCH, 1) #needed in the basic for the continuum.
 
         # TOTAL NUMBER OF INITIALIZED ELEMENTS
         self.model_part.ProcessInfo.SetValue(NUM_PARTICLES_INITIALIZED, 0);
