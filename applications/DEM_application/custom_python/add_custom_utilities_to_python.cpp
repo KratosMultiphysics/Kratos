5213f820f74eecec454639b6e791b888f243b158
maceli <maceli@cimne.upc.edu>
2013-11-12 15:36:44 +0000

    ModelPart additions are called inside DEM_application (output)

--- applications/DEM_application/custom_python/add_custom_utilities_to_python.cpp
+++ applications/DEM_application/custom_python/add_custom_utilities_to_python.cpp
@@ -119,6 +119,7 @@ void  AddCustomUtilitiesToPython(){
      class_<PostUtilities, boost::noncopyable >
         ("PostUtilities", init<>())
         .def("VelocityTrap", &PostUtilities::VelocityTrap)
+        .def("AddModelPartNodesToModelPart", &PostUtilities::AddModelPartNodesToModelPart)
         .def("QuasiStaticAdimensionalNumber", &PostUtilities::QuasiStaticAdimensionalNumber)     
         ;
 
