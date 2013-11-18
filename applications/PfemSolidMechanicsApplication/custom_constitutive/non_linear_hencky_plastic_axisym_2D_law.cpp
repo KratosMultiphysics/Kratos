0b8151ea4837712f2cff36457d6f747f42e02fb0
lmonforte <lmonforte@cimne.upc.edu>
2013-11-18 15:45:35 +0000

    Adding new constitutive laws to PFEM/SMA.

---
+++ applications/PfemSolidMechanicsApplication/custom_constitutive/non_linear_hencky_plastic_axisym_2D_law.cpp
@@ -0,0 +1,99 @@
+// System includes
+#include <iostream>
+
+// External includes
+#include<cmath>
+
+// Project includes
+#include "includes/properties.h"
+#include "custom_constitutive/hyperelastic_plastic_3D_law.hpp"
+
+#include "../PfemSolidMechanicsApplication/custom_constitutive/non_linear_hencky_plastic_axisym_2D_law.hpp"
+
+#include "solid_mechanics_application.h"
+//Molt important, el tema de constructors... etc
+namespace Kratos
+{
+
+
+NonLinearHenckyElasticPlasticAxisym2DLaw::NonLinearHenckyElasticPlasticAxisym2DLaw()
+   : NonLinearHenckyElasticPlastic3DLaw()
+{
+
+}
+
+NonLinearHenckyElasticPlasticAxisym2DLaw::NonLinearHenckyElasticPlasticAxisym2DLaw(FlowRulePointer pFlowRule, YieldCriterionPointer pYieldCriterion, HardeningLawPointer pHardeningLaw)
+   : NonLinearHenckyElasticPlastic3DLaw()
+{
+
+}
+
+
+
+// ************* COPY CONSTRUCTOR ******************
+NonLinearHenckyElasticPlasticAxisym2DLaw::NonLinearHenckyElasticPlasticAxisym2DLaw(const NonLinearHenckyElasticPlasticAxisym2DLaw&  rOther)
+  : NonLinearHenckyElasticPlastic3DLaw(rOther)
+{
+
+}
+
+ 
+NonLinearHenckyElasticPlasticAxisym2DLaw::~NonLinearHenckyElasticPlasticAxisym2DLaw()
+{
+}
+
+//***********************COMPUTE TOTAL STRAIN*****************************************
+//************************************************************************************
+
+void NonLinearHenckyElasticPlasticAxisym2DLaw::CalculateGreenLagrangeStrain( const Matrix & rRightCauchyGreen,
+        Vector& rStrainVector )
+{
+
+    //E= 0.5*(FT*F-1)
+    rStrainVector[0] = 0.5 * ( rRightCauchyGreen( 0, 0 ) - 1.00 );
+    rStrainVector[1] = 0.5 * ( rRightCauchyGreen( 1, 1 ) - 1.00 );
+    rStrainVector[2] = 0.5 * ( rRightCauchyGreen( 2, 2 ) - 1.00 );
+    rStrainVector[3] = rRightCauchyGreen( 0, 1 );
+
+}
+
+
+//***********************COMPUTE TOTAL STRAIN*****************************************
+//************************************************************************************
+
+void NonLinearHenckyElasticPlasticAxisym2DLaw::CalculateAlmansiStrain( const Matrix & rLeftCauchyGreen,
+        Vector& rStrainVector )
+{
+
+    // e= 0.5*(1-invbT*invb)
+    Matrix InverseLeftCauchyGreen ( rLeftCauchyGreen.size1() , rLeftCauchyGreen.size2() );
+    double det_b=0;
+    MathUtils<double>::InvertMatrix( rLeftCauchyGreen, InverseLeftCauchyGreen, det_b);
+
+    rStrainVector.clear();
+    rStrainVector[0] = 0.5 * ( 1.0 - InverseLeftCauchyGreen( 0, 0 ) );
+    rStrainVector[1] = 0.5 * ( 1.0 - InverseLeftCauchyGreen( 1, 1 ) );
+    rStrainVector[2] = 0.5 * ( 1.0 - InverseLeftCauchyGreen( 2, 2 ) );
+    rStrainVector[3] = -InverseLeftCauchyGreen( 0, 1 );
+
+
+}
+
+
+Matrix NonLinearHenckyElasticPlasticAxisym2DLaw::SetConstitutiveMatrixToAppropiateDimension(const Matrix& rElastoPlasticTangentMatrix)
+{
+     Matrix Result = ZeroMatrix(4);
+
+
+     for (unsigned int i = 0; i < 4; ++i)  {
+         for (unsigned int j = 0; j < 4; ++j) {
+             Result( i, j) = rElastoPlasticTangentMatrix( i, j);
+         }
+     }
+
+     return Result;
+
+}
+
+
+} //end namespace kratos
