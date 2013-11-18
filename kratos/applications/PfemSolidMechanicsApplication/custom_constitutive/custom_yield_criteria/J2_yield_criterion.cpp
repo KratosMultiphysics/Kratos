3860ddd4e8c0657ee0ee796a93abb74da390913a
lmonforte <lmonforte@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-11-18 15:50:25 +0000

    Adding new constitutive laws to PFEM/SMA (J2 with hencky explicit...)

---
+++ kratos/applications/PfemSolidMechanicsApplication/custom_constitutive/custom_yield_criteria/J2_yield_criterion.cpp
@@ -0,0 +1,129 @@
+// System includes
+#include <string>
+#include <iostream>
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "solid_mechanics_application.h"
+#include "../PfemSolidMechanicsApplication/custom_constitutive/custom_yield_criteria/J2_yield_criterion.hpp"
+
+namespace Kratos
+{
+
+
+//*******************************CONSTRUCTOR******************************************
+//************************************************************************************
+J2YieldCriterion::J2YieldCriterion()
+	:YieldCriterion()
+{
+   
+}
+
+
+//*******************************ASSIGMENT OPERATOR***********************************
+//************************************************************************************
+
+J2YieldCriterion& J2YieldCriterion::operator=(J2YieldCriterion const& rOther)
+{
+   YieldCriterion::operator=(rOther);
+   return *this;
+}
+
+//*******************************COPY CONSTRUCTOR*************************************
+//************************************************************************************
+
+J2YieldCriterion::J2YieldCriterion(J2YieldCriterion const& rOther)
+	:YieldCriterion(rOther)
+{
+
+}
+
+
+//********************************DESTRUCTOR******************************************
+//************************************************************************************
+
+J2YieldCriterion::~J2YieldCriterion()
+{
+}
+
+
+
+//************************* CALCULATE YIELD FUNCTION  ******************
+//**********************************************************************
+
+double& J2YieldCriterion::CalculateYieldCondition(double& rStateFunction, const Vector& rStressVector, const double& rAlpha)
+{
+ 
+   rStateFunction = 0.0;
+
+   double MeanStress = 0.0;
+  
+   for (unsigned int i = 0; i<3; ++i)
+       MeanStress += rStressVector(i);
+
+   MeanStress /= 3.0;
+
+   for (unsigned int i = 0; i<3; ++i)
+       rStateFunction += pow(rStressVector(i) - MeanStress, 2.0);
+
+   for (unsigned int i = 3; i<6; ++i)
+       rStateFunction += 2.0*pow(rStressVector(i), 2.0);
+
+
+   rStateFunction = pow(3.0/2.0, 1.0/2.0)*pow( rStateFunction , 1.0/2.0);
+   rStateFunction /= 50.0;
+   rStateFunction -= 1.0;
+//   rStateFunction = -1.0;
+   return rStateFunction; 
+}
+
+
+
+
+//************************* YIELD FUNCTION DERIVATIVE ******************
+//**********************************************************************
+void J2YieldCriterion::CalculateYieldFunctionDerivative(const Vector& rStressVector, Vector& rYieldFunctionD, const double& rAlpha)
+{
+
+     double MeanStress = 0.0;
+ 
+     for (unsigned int i = 0; i<3; ++i)
+         MeanStress += rStressVector(i);
+
+     MeanStress /= 3.0;
+
+     double denominador = 0.0;
+     Vector ShearVector = ZeroVector(6);
+
+     for (unsigned int i = 0; i<3; ++i)  {
+         ShearVector(i) = rStressVector(i) -MeanStress; 
+         denominador += pow( ShearVector(i), 2.0);
+     }
+
+    for (unsigned int i = 3; i<6; ++i) {
+         ShearVector(i) = 2.0*rStressVector(i);
+         denominador += 2.0*pow( ShearVector(i), 2.0);
+     }
+
+     denominador = pow(3.0/2.0, 1.0/2.0)*pow(denominador, 1.0/2.0);
+
+     rYieldFunctionD = ShearVector;
+ 
+     rYieldFunctionD *= 3.0/2.0/denominador/50.0;
+
+
+}
+void J2YieldCriterion::save( Serializer& rSerializer ) const
+{
+    KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, YieldCriterion );
+}
+
+void J2YieldCriterion::load( Serializer& rSerializer )
+{
+    KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, YieldCriterion );
+}
+
+
+}  // namespace Kratos.
---
+++ kratos/applications/PfemSolidMechanicsApplication/custom_constitutive/custom_yield_criteria/J2_yield_criterion.hpp
@@ -0,0 +1,237 @@
+//
+//   Project Name:        KratosSolidMechanicsApplication $
+//   Last modified by:    $Author:            JMCarbonell $
+//   Date:                $Date:                July 2013 $
+//   Revision:            $Revision:                  0.0 $
+//
+//
+
+#if !defined(KRATOS_J2_YIELD_CRITERION_H_INCLUDED )
+#define      KRATOS_J2_YIELD_CRITERION_H_INCLUDED
+
+
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "custom_constitutive/custom_yield_criteria/yield_criterion.hpp"
+#include "custom_constitutive/custom_hardening_laws/cam_clay_hardening_law.hpp"
+namespace Kratos
+{
+///@addtogroup ApplicationNameApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/// Short class definition.
+/** Detail class definition.
+*/
+class J2YieldCriterion
+	: public YieldCriterion 
+{
+    public:
+        ///@name Type Definitions
+        ///@{
+
+        /// Pointer definition of MisesHuberYieldCriterion
+        KRATOS_CLASS_POINTER_DEFINITION(J2YieldCriterion);
+
+        ///@}
+        ///@name Life Cycle
+        ///@{
+
+        /// Default constructor.
+        J2YieldCriterion();
+
+        /// Copy constructor.
+        J2YieldCriterion(J2YieldCriterion const& rOther);
+
+        /// Assignment operator.
+        J2YieldCriterion& operator=(J2YieldCriterion const& rOther);
+
+
+        /// Destructor.
+        virtual ~J2YieldCriterion();
+
+
+        ///@}
+        ///@name Operators
+        ///@{
+
+
+        ///@}
+        ///@name Operations
+        ///@{
+
+        double& CalculateYieldCondition(double & rStateFunction, const Vector& rStressVector, const double& rAlpha);
+
+	void CalculateYieldFunctionDerivative(const Vector& rStressVector, Vector& rFirstDerivative, const double& rAlpha);
+        ///@}
+        ///@name Access
+        ///@{
+        
+
+        ///@}
+        ///@name Inquiry
+        ///@{
+
+
+        ///@}
+        ///@name Input and output
+        ///@{
+
+        // /// Turn back information as a string.
+        // virtual std::string Info() const;
+
+        // /// Print information about this object.
+        // virtual void PrintInfo(std::ostream& rOStream) const;
+
+        // /// Print object's data.
+        // virtual void PrintData(std::ostream& rOStream) const;
+
+
+        ///@}
+        ///@name Friends
+        ///@{
+
+
+        ///@}
+
+    protected:
+        ///@name Protected static Member Variables
+        ///@{
+
+
+        ///@}
+        ///@name Protected member Variables
+        ///@{
+	
+	
+        ///@}
+        ///@name Protected Operators
+        ///@{
+
+
+        ///@}
+        ///@name Protected Operations
+        ///@{
+
+
+        ///@}
+        ///@name Protected  Access
+        ///@{
+
+
+        ///@}
+        ///@name Protected Inquiry
+        ///@{
+
+
+        ///@}
+        ///@name Protected LifeCycle
+        ///@{
+
+
+        ///@}
+
+    private:
+        ///@name Static Member Variables
+        ///@{
+
+
+        ///@}
+        ///@name Member Variables
+        ///@{
+
+
+        ///@}
+        ///@name Private Operators
+        ///@{
+
+
+        ///@}
+        ///@name Private Operations
+        ///@{
+
+        ///@}
+        ///@name Private  Access
+        ///@{
+
+	
+	///@}
+	///@name Serialization
+	///@{
+	friend class Serializer;
+
+	// A private default constructor necessary for serialization
+
+	virtual void save(Serializer& rSerializer) const;
+
+	virtual void load(Serializer& rSerializer);
+
+        ///@}
+        ///@name Private Inquiry
+        ///@{
+
+
+        ///@}
+        ///@name Un accessible methods
+        ///@{
+
+        ///@}
+
+    }; // Class MisesHuberYieldCriterion
+
+    ///@}
+
+    ///@name Type Definitions
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+
+    // /// input stream function
+    // inline std::istream& operator >> (std::istream& rIStream,
+    //                                   MisesHuberYieldCriterion& rThis);
+
+    // /// output stream function
+    // inline std::ostream& operator << (std::ostream& rOStream,
+    //                                   const MisesHuberYieldCriterion& rThis)
+    // {
+    //     rThis.PrintInfo(rOStream);
+    //     rOStream << std::endl;
+    //     rThis.PrintData(rOStream);
+
+    //     return rOStream;
+    // }
+    ///@}
+
+    ///@} addtogroup block
+
+}  // namespace Kratos.
+
+#endif // KRATOS_CAM_CLAY_YIELD_CRITERION_H_INCLUDED  defined 
+
+
