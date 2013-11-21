ec71d8bc9e7ef51b431ca07b5e5ccbe7ce238b57
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-11-21 09:59:56 +0000

    Minor

--- kratos/applications/DEM_application/custom_elements/spheric_particle.cpp
+++ kratos/applications/DEM_application/custom_elements/spheric_particle.cpp
@@ -103,7 +103,6 @@ namespace Kratos
           
           ComputeNewNeighboursHistoricalData();		  
             //Cfeng,RigidFace
-          
           ComputeNewRigidFaceNeighboursHistoricalData();		  
           
             
