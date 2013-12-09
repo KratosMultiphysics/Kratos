a04b2f2c256b3b6dfbed472d9f4ab5f8466a2d80
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-12-09 16:25:51 +0000

    uint changed to unsigned int

--- kratos/applications/incompressible_fluid_application/custom_utilities/embedded_utilities.h
+++ kratos/applications/incompressible_fluid_application/custom_utilities/embedded_utilities.h
@@ -235,7 +235,7 @@ public:
 		Vector which_edge=ZeroVector(10);
 		which_edge.resize(IntersectionPoints.size());
 
-                for (uint i=0; i<IntersectionPoints.size(); i++)
+                for (unsigned int i=0; i<IntersectionPoints.size(); i++)
                 {
                     which_edge[i]=100;
                 }
