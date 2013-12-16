af8b82ea95ba8151ee8caabf464546ee96efaccf
jcotela <jcotela@cimne.upc.edu>
2013-12-16 10:48:31 +0000

    Re-implementing fractional step Neumann boundary conditions to use full integration. Note that normals are now assumed to point OUTWARDS.

--- applications/FluidDynamicsApplication/custom_conditions/wall_condition.cpp
+++ applications/FluidDynamicsApplication/custom_conditions/wall_condition.cpp
@@ -206,7 +206,7 @@ void WallCondition<3,3>::CalculateNormal(array_1d<double,3>& An )
     {
         Geometry<Node<3> >& pGeometry = this->GetGeometry();
 
-	array_1d<double,3> v1,v2;
+        array_1d<double,3> v1,v2;
         v1[0] = pGeometry[1].X() - pGeometry[0].X();
         v1[1] = pGeometry[1].Y() - pGeometry[0].Y();
         v1[2] = pGeometry[1].Z() - pGeometry[0].Z();
@@ -216,9 +216,92 @@ void WallCondition<3,3>::CalculateNormal(array_1d<double,3>& An )
         v2[2] = pGeometry[2].Z() - pGeometry[0].Z();
 
         MathUtils<double>::CrossProduct(An,v1,v2);
-        An *= -0.5;
-
-        // 				noalias((it)->GetValue(NORMAL)) = An;
+        An *= 0.5;
     }
+
+template<unsigned int TDim, unsigned int TNumNodes>
+void WallCondition<TDim,TNumNodes>::ApplyNeumannCondition(MatrixType &rLocalMatrix, VectorType &rLocalVector)
+{
+    if (this->GetValue(IS_STRUCTURE) == 0.0)
+    {
+        const unsigned int LocalSize = TNumNodes;
+        const GeometryType& rGeom = this->GetGeometry();
+        const GeometryType::IntegrationPointsArrayType& IntegrationPoints = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_2);
+        const unsigned int NumGauss = IntegrationPoints.size();
+        Vector GaussWeights = ZeroVector(NumGauss);
+
+        MatrixType NContainer = rGeom.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
+
+        array_1d<double,3> Normal;
+        this->CalculateNormal(Normal); //this already contains the area
+        double A = std::sqrt(Normal[0]*Normal[0]+Normal[1]*Normal[1]+Normal[2]*Normal[2]);
+        Normal /= A;
+
+        for (unsigned int g = 0; g < NumGauss; g++)
+            GaussWeights[g] = 2.0*A * IntegrationPoints[g].Weight();
+
+        for (unsigned int g = 0; g < NumGauss; g++)
+        {
+            Vector N = row(NContainer,g);
+            double Weight = GaussWeights[g];
+
+            // Neumann boundary condition
+            for (unsigned int i = 0; i < TNumNodes; i++)
+            {
+                //unsigned int row = i*LocalSize;
+                const NodeType& rConstNode = this->GetGeometry()[i];
+                if ( rConstNode.IsFixed(PRESSURE)==true)
+                {
+                    const double pext = rConstNode.FastGetSolutionStepValue(PRESSURE);
+                    for (unsigned int j = 0; j < TNumNodes; j++)
+                    {
+                        unsigned int row = j*LocalSize;
+                        for (unsigned int d = 0; d < TDim;d++)
+                            rLocalVector[row+d] -= Weight*N[j]*N[i]*pext*Normal[d];
+                    }
+                }
+            }
+
+            // Velocity inflow correction
+            array_1d<double,3> Vel(3,0.0);
+            double Density = 0.0;
+
+            for (unsigned int i = 0; i < TNumNodes; i++)
+            {
+                const NodeType& rConstNode = this->GetGeometry()[i];
+                Vel += N[i]*rConstNode.FastGetSolutionStepValue(VELOCITY);
+                Density += N[i]*rConstNode.FastGetSolutionStepValue(DENSITY);
+            }
+
+            double Proj = Vel[0]*Normal[0] + Vel[1]*Normal[1] + Vel[2]*Normal[2];
+
+            if (Proj < 0)
+            {
+                const double W = Weight*Density*Proj;
+                for (unsigned int i = 0; i < TNumNodes; i++)
+                {
+                    double row = i*LocalSize;
+                    for (unsigned int j = 0; j < TNumNodes; j++)
+                    {
+                        double col = j*LocalSize;
+                        const array_1d<double,3>& rVel = this->GetGeometry()[j].FastGetSolutionStepValue(VELOCITY);
+                        for (unsigned int d = 0; d < TDim; d++)
+                        {
+                            double Tij = W*N[i]*N[j];
+                            rLocalMatrix(row+d,col+d) -= Tij;
+                            rLocalVector[row+d] += Tij*rVel[d];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Class template instantiation
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template class WallCondition<2,2>;
+template class WallCondition<3,3>;
 
 } // namespace Kratos
