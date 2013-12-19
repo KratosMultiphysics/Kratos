7659334d076161c6e3b2b2d362ab5f2a454efd1a
Gerardo <Gerardo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2013-12-19 10:26:53 +0000

    - WriteConvectionDiffusionPropertyAtNodes and WriteConvectionDiffusionProjectParameters - Add the proc CDWriteDefaultProperties,GetPropertyIdForFaceHeatFluxBC, GetPropertyDataFromFaceHeatFluxBC and WriteConvectionDiffusionPrescribedFaceHeatFluxBC

--- kratos/gid_interface/kratos.gid/scripts/libs/wkcf/wkcfconvectiondiffusion.tcl
+++ kratos/gid_interface/kratos.gid/scripts/libs/wkcf/wkcfconvectiondiffusion.tcl
@@ -12,12 +12,125 @@
 #
 #    HISTORY:
 #
+#     0.4- 14/11/13-G. Socorro, modify the proc WriteConvectionDiffusionPropertyAtNodes and WriteConvectionDiffusionProjectParameters
+#     0.3- 10/11/13-G. Socorro, add the proc CDWriteDefaultProperties,GetPropertyIdForFaceHeatFluxBC, GetPropertyDataFromFaceHeatFluxBC and WriteConvectionDiffusionPrescribedFaceHeatFluxBC
 #     0.2- 27/09/13-G. Socorro, create some new proc GetConvectionDiffusionMaterialProperties,WriteConvectionDiffusionPrescribedHeatFluxBC
 #                               and WriteConvectionDiffusionProjectParameters
 #     0.1- 12/09/13-G. Socorro, create a base source code from wkcf.tcl
 #
 ###############################################################################
 
+proc ::wkcf::CDWriteDefaultProperties {AppId} {
+    variable filechannel
+
+    # Kratos key word xpath
+    set kxpath "Applications/$AppId"
+    
+    # Write the base properties   
+    GiD_File fprintf $filechannel "%s" "Begin Properties 0"
+    
+    # Write default values
+    set plist [list "VEmissivity" "EnvironmentTemperature" "VConvectionCoefficient"]
+    set cvalue "0.0"
+    foreach pid $plist {
+	# Get the kratos keyword
+	set ckword [::xmlutils::getKKWord $kxpath $pid]
+	# wa "ckword:$ckword"
+	if {$ckword !=""} {
+	    GiD_File fprintf $filechannel "%s" " $ckword $cvalue"
+	}
+    }
+    
+    GiD_File fprintf $filechannel "%s" "End Properties"
+    GiD_File fprintf $filechannel ""
+}
+
+proc ::wkcf::GetPropertyIdForFaceHeatFluxBC {AppId GroupId} {
+    # ABSTRACT: Get the property identifier from the group identifier
+    variable dprops
+
+    set RPropertyId 1
+    foreach PropertyId $dprops($AppId,GBCKProps,AllPropertyId) {
+	# Get the propertyid-group identifier
+	set PropertyGroupId $dprops($AppId,GBCKProps,$PropertyId,PropertyGroupId)
+	if {$PropertyGroupId eq $GroupId} {
+	    set RPropertyId $PropertyId
+	}
+    }
+    return $RPropertyId
+}
+
+proc ::wkcf::GetPropertyDataFromFaceHeatFluxBC {AppId} {
+    # ABSTRACT: Get the mapping between property and face heat flux boundary condition
+    variable dprops
+   
+    # For debug
+    if {!$::wkcf::pflag} {
+        set inittime [clock seconds]
+    }
+
+    # Init the property to BC counter
+    set bcgroupid -1
+
+    # Solver type
+    set cproperty "dv"
+    set rootid $AppId
+    set cxpath "$rootid//c.AnalysisData//i.AnalysisType"
+    set AnalysisType [::xmlutils::setXml $cxpath $cproperty]
+    if {$AnalysisType eq "Linear"} {
+	set bcgroupid 0
+    } 
+
+    # Init the global boundary condition identifier list
+    set dprops($AppId,GBCKProps,AllPropertyId) [list]
+   
+    # Set the rootid
+    set rootid "$AppId"
+    set cproperty "dv"
+    
+    # Get the values
+    set basexpath "$rootid//c.Conditions//c.FaceHeatFlux"
+    set gproplist [::xmlutils::setXmlContainerIds $basexpath]
+    # wa "gproplist:$gproplist"
+    foreach cgroupid $gproplist {
+	# Get the group properties
+	set cxpath "${basexpath}//c.[list ${cgroupid}]//c.MainProperties"
+	set allgprop [::xmlutils::setXmlContainerPairs $cxpath "" "dv"]
+	# wa "allgprop:$allgprop"
+	if {[llength $allgprop]} {
+	    if {[GiD_EntitiesGroups get $cgroupid nodes -count]} {
+		incr bcgroupid 1
+		# Create the propertyid-group identifier mapping
+		lappend dprops($AppId,GBCKProps,AllPropertyId) $bcgroupid
+	
+		# Set the properties
+		# Modify some properties
+		set newprops [list]
+	       	foreach propid $allgprop {
+		    lassign $propid key value
+		    if {($key eq "VFaceHeatFlux")} {
+			continue
+		    } else {
+			lappend newprops [list $key $value]	
+		    }
+		}
+		# wa "newprops:$newprops"
+		if {[llength $newprops]} {
+		    set dprops($AppId,GBCKProps,$bcgroupid,PropertyList) $newprops
+		    set dprops($AppId,GBCKProps,$bcgroupid,PropertyGroupId) $cgroupid
+		}
+	    }
+	}
+    }
+    
+    # For debug
+    if {!$::wkcf::pflag} {
+	set endtime [clock seconds]
+	set ttime [expr $endtime-$inittime]
+	# WarnWinText "endtime:$endtime ttime:$ttime"
+	WarnWinText "Get the mapping between property and face heat flux boundary condition: [::KUtils::Duration $ttime]"
+    }
+}
 
 proc ::wkcf::GetConvectionDiffusionMaterialProperties {AppId} {
     # ABSTRACT : Return the material properties for the convection diffusion application
@@ -54,6 +167,7 @@ proc ::wkcf::GetConvectionDiffusionMaterialProperties {AppId} {
 		    # Get the current value for this properties
 		    set cvalue [lindex [::KMat::getMaterialProperties "p" "$mpxpath//$xpath//p.[list $pid]"] 0 1]
 		    set Conductivity [GiD_FormatReal "%10.5e" $cvalue]
+		    # wa "Conductivity:$Conductivity"
 		} elseif {$pid =="SpecificHeat"} {
 		    set xpath "c.Thermal//c.Isotropic"
 		    # Get the current value for this properties
@@ -107,26 +221,23 @@ proc ::wkcf::WriteConvectionDiffusionPropertyAtNodes {AppId} {
 		# For all defined group identifier for this element
 		foreach cgroupid $dprops($AppId,KElem,$celemid,AllGroupId) {                    
 		    if {[GiD_EntitiesGroups get $cgroupid nodes -count]} {
+			set strnodes ""
+			foreach node_id [GiD_EntitiesGroups get $cgroupid nodes] {
+			    append  strnodes "[format "%10i%4i" $node_id $cpropid]  [GiD_FormatReal "%10.5e" $MeshVelocity]\n"
+			}
 
-			# Write mesh velocity value for this group
-		        set vkword [::xmlutils::getKKWord $kxpath "MeshVelocity" "kkword"]
-		        GiD_File fprintf $filechannel "%s" "Begin NodalData $vkword \/\/ GUI group identifier: $cgroupid"
-		        foreach node_id [GiD_EntitiesGroups get $cgroupid nodes] {
-		            GiD_File fprintf $filechannel "%10i %4i %4s" $node_id $cpropid $MeshVelocity
-		        }
-		        GiD_File fprintf $filechannel "%s " "End NodalData"
-		        GiD_File fprintf $filechannel ""
-		 
+			# Write mesh velocity and velocity value for this group
+			foreach item [list "MeshVelocity" "Velocity"] {
+			    foreach comp [list "X" "Y" "Z"] {
+				set vkword [::xmlutils::getKKWord $kxpath "${item}_${comp}" "kkword"]
+				GiD_File fprintf $filechannel "%s" "Begin NodalData $vkword \/\/ GUI group identifier: $cgroupid"
+				GiD_File fprintf $filechannel "%s"  "$strnodes"
+				GiD_File fprintf $filechannel "%s " "End NodalData"
+				GiD_File fprintf $filechannel ""
+			    }
+			}
 			
-			# Write velocity value for this group
-			set vkword [::xmlutils::getKKWord $kxpath "Velocity" "kkword"]
-		        GiD_File fprintf $filechannel "%s" "Begin NodalData $vkword \/\/ GUI group identifier: $cgroupid"
-		        foreach node_id [GiD_EntitiesGroups get $cgroupid nodes] {
-		            GiD_File fprintf $filechannel "%10i %4i %4s" $node_id $cpropid $Velocity
-		        }
-		        GiD_File fprintf $filechannel "%s" "End NodalData"
-		        GiD_File fprintf $filechannel "%s" ""
-
+		
 			# Write others material properties
 			foreach kwordid $matkwordlist varid $matvarlist {
 			    set vkword [::xmlutils::getKKWord $kxpath "$kwordid" "kkword"]
@@ -184,7 +295,7 @@ proc ::wkcf::WriteConvectionDiffusionPrescribedTemperatureBC {AppId ccondid kwor
 proc ::wkcf::WriteConvectionDiffusionPrescribedHeatFluxBC {AppId ccondid kwordlist} {
     variable ndime; variable dprops; variable filechannel
 
-    set cpropid "1"
+    set cpropid "0"
  
     # Convection-diffusion heat flux boundary condition
     set xitem [lindex $kwordlist 0]
@@ -207,14 +318,78 @@ proc ::wkcf::WriteConvectionDiffusionPrescribedHeatFluxBC {AppId ccondid kwordli
     }
 }
 
+proc ::wkcf::WriteConvectionDiffusionPrescribedFaceHeatFluxBC {AppId ccondid kwordlist} {
+    variable ndime; variable dprops; variable filechannel
+
+    set cpropid "1"
+ 
+    # Convection-diffusion face heat flux boundary condition
+    set xitem [lindex $kwordlist 0]
+  
+    # For each group in the this condition
+    foreach cgroupid $dprops($AppId,BC,$ccondid,AllGroupId) {
+	# wa "cgroupid:$cgroupid"
+	# Get the condition properties
+	lassign $dprops($AppId,BC,$ccondid,$cgroupid,GProps) faceheatfluxvalue
+	# wa "heatfluxvalue:$heatfluxvalue"
+	# Face heat flux value
+	if { [GiD_EntitiesGroups get $cgroupid nodes -count] } {
+	    # Get the property identifier from the group property mapping
+	    set cpropid [::wkcf::GetPropertyIdForFaceHeatFluxBC $AppId $cgroupid]
+	    GiD_File fprintf $filechannel "%s" "Begin NodalData $xitem \/\/ Convection-Diffusion face heat flux condition GUI group identifier: $cgroupid"
+	    foreach node_id [GiD_EntitiesGroups get $cgroupid nodes] {
+		GiD_File fprintf $filechannel "%10i %8i   %10.5e" $node_id $cpropid $faceheatfluxvalue
+	    }
+	    GiD_File fprintf $filechannel "End NodalData"
+	    GiD_File fprintf $filechannel ""
+	}
+    }
+}
+
+proc ::wkcf::WriteInitialTemperatureIC {AppId ccondid kwordlist} {
+    variable ndime; variable dprops; variable filechannel
+
+    set cpropid "0"
+ 
+    # Convection-diffusion initial temperature initial condition
+    set xitem [lindex $kwordlist 0]
+  
+    # For each group in the this condition
+    foreach cgroupid $dprops($AppId,IC,$ccondid,AllGroupId) {
+	# wa "cgroupid:$cgroupid"
+	# Get the condition properties
+	lassign $dprops($AppId,IC,$ccondid,$cgroupid,GProps) initialtempvalue
+	# wa "initialtempvalue:$initialtempvalue"
+	# Initial temperature value
+	if { [GiD_EntitiesGroups get $cgroupid nodes -count] } {
+	    GiD_File fprintf $filechannel "%s" "Begin NodalData $xitem \/\/ Convection-Diffusion initial temperature condition GUI group identifier: $cgroupid"
+	    foreach node_id [GiD_EntitiesGroups get $cgroupid nodes] {
+		GiD_File fprintf $filechannel "%10i %8i   %10.5e" $node_id $cpropid $initialtempvalue
+	    }
+	    GiD_File fprintf $filechannel "End NodalData"
+	    GiD_File fprintf $filechannel ""
+	}
+    }
+}
+
 proc ::wkcf::WriteConvectionDiffusionProjectParameters {AppId fileid PDir} {
     variable ndime; variable dprops
    
     set rootid "$AppId"
     
+    set trailing_spaces  "    "
+
     # Kratos key word xpath
     set kxpath "Applications/$rootid"
     
+    # Write linear solver method
+    # 0 => Old format
+    # 1 => New format
+    set wfsmethod 1
+
+    # Get others properties
+    set cproperty "dv"
+
     set domain_size 2
     if {$ndime =="2D"} {
 	set domain_size 2
@@ -226,14 +401,28 @@ proc ::wkcf::WriteConvectionDiffusionProjectParameters {AppId fileid PDir} {
     puts $fileid "domain_size = $domain_size"
     puts $fileid ""
     
-    # Get others properties
-    set cproperty "dv"
-    
-    puts $fileid "class SolverSettings:"
+    # Solver type
+    set cxpath "$rootid//c.AnalysisData//i.AnalysisType"
+    set AnalysisType [::xmlutils::setXml $cxpath $cproperty]
+    if {$AnalysisType eq "Linear"} {
+	puts $fileid "Linear = True"
+    } else {
+	puts $fileid "Linear = False"
+    }
+    puts $fileid ""
+
+    puts $fileid "class SolverSettings1:"
     puts $fileid "    solver_type = \"convection_diffusion_solver\""
-    puts $fileid "    domain_size= 2"	
-    puts $fileid "    time_order = 2"
-    puts $fileid "    predictor_corrector = False"
+    puts $fileid "    domain_size= $domain_size"
+    # Time order
+    set cxpath "$rootid//c.SolutionStrategy//c.Advanced//i.TimeOrder"
+    set TimeOrder [::xmlutils::setXml $cxpath $cproperty]
+    puts $fileid "${trailing_spaces}time_order = $TimeOrder"
+    
+    # Predictor corrector
+    set cxpath "$rootid//c.SolutionStrategy//c.Advanced//i.PredictorCorrector"
+    set PredictorCorrector [::xmlutils::setXml $cxpath $cproperty]
+    puts $fileid "${trailing_spaces}predictor_corrector = $PredictorCorrector"	
     puts $fileid "    ReformDofAtEachIteration = False"
     puts $fileid "    echo_level=0"
     
@@ -254,23 +443,41 @@ proc ::wkcf::WriteConvectionDiffusionProjectParameters {AppId fileid PDir} {
     puts $fileid ""
     puts $fileid ""
 
-    puts $fileid "    class linear_solver_config:" 
-    puts $fileid "        solver_type = \"Skyline LU factorization\""
-    puts $fileid "        scaling = False"           
+    # Linear solvers
+    # Temperature
+    ::wkcf::WriteLinearSolvers $rootid $fileid "Temperature" $wfsmethod $trailing_spaces "linear_solver_config"
     
     puts $fileid ""
     puts $fileid ""
     
     puts $fileid "class SolverSettings2:"
     puts $fileid "    solver_type = \"nonlinear_convection_diffusion_solver\""
-    puts $fileid "    domain_size= 2"	
-    puts $fileid "    time_order = 2"
-    puts $fileid "    predictor_corrector = False"
+    puts $fileid "    domain_size= $domain_size"
+    
+    # Time order
+    set cxpath "$rootid//c.SolutionStrategy//c.Advanced//i.TimeOrder"
+    set TimeOrder [::xmlutils::setXml $cxpath $cproperty]
+    puts $fileid "${trailing_spaces}time_order = $TimeOrder"
+    
+    # Predictor corrector
+    set cxpath "$rootid//c.SolutionStrategy//c.Advanced//i.PredictorCorrector"
+    set PredictorCorrector [::xmlutils::setXml $cxpath $cproperty]
+    puts $fileid "${trailing_spaces}predictor_corrector = $PredictorCorrector"	
     puts $fileid "    ReformDofAtEachIteration = False"
     puts $fileid "    echo_level=0"
-    puts $fileid "    max_iter = 15"
-    puts $fileid "    toll = 1e-3"
-
+    
+    # Maximum temperature iterations
+    puts $fileid "$trailing_spaces"
+    puts $fileid "${trailing_spaces}#convergence criteria settings"
+    set cxpath "$rootid//c.SolutionStrategy//c.Advanced//i.MaximumTemperatureIterations"
+    set cvalue [::xmlutils::setXml $cxpath $cproperty]
+    puts $fileid "${trailing_spaces}max_iter = $cvalue"
+    
+    # Relative temperature tolerance
+    set cxpath "$rootid//c.SolutionStrategy//c.Advanced//i.RelativeTemperatureTolerance"
+    set cvalue [::xmlutils::setXml $cxpath $cproperty]
+    puts $fileid "${trailing_spaces}toll = $cvalue"
+    
     puts $fileid ""
     puts $fileid ""
 
@@ -288,27 +495,29 @@ proc ::wkcf::WriteConvectionDiffusionProjectParameters {AppId fileid PDir} {
     puts $fileid ""
     puts $fileid ""
 
-    puts $fileid "    class linear_solver_config:" 
-    puts $fileid "        solver_type = \"Skyline LU factorization\""
-    puts $fileid "        scaling = False"           
-    
+    # Linear solvers
+    # Temperature
+    ::wkcf::WriteLinearSolvers $rootid $fileid "Temperature" $wfsmethod $trailing_spaces "linear_solver_config"
+
     puts $fileid ""
 
     puts $fileid "nodal_results=\[\"TEMPERATURE\"\]"
+    puts $fileid "gauss_points_results=\[\]"
 
     # GiD post mode variables
     ::wkcf::WriteGiDPostMode $AppId $fileid 
 
     puts $fileid ""
 
+    puts $fileid "AutomaticDeltaTime = \"Fixed\""
     # Start time
-    set cxpath "$rootid//c.SolutionStrategy//i.StartTime"
+    set cxpath "$rootid//c.SolutionStrategy//c.ProblemParameters//i.StartTime"
     set StartTime [::xmlutils::setXml $cxpath $cproperty]
     # End time
-    set cxpath "$rootid//c.SolutionStrategy//i.EndTime"
+    set cxpath "$rootid//c.SolutionStrategy//c.ProblemParameters//i.EndTime"
     set EndTime [::xmlutils::setXml $cxpath $cproperty]
     # Delta time
-    set cxpath "$rootid//c.SolutionStrategy//i.DeltaTime"
+    set cxpath "$rootid//c.SolutionStrategy//c.ProblemParameters//i.DeltaTime"
     set DeltaTime [::xmlutils::setXml $cxpath $cproperty]
     
      # WarnWinText "StartTime:$StartTime EndTime:$EndTime DeltaTime:$DeltaTime"
@@ -329,7 +538,15 @@ proc ::wkcf::WriteConvectionDiffusionProjectParameters {AppId fileid PDir} {
     set output_step [expr int($OutputDeltaTime/double($DeltaTime))]
     # WarnWinText "output_step:$output_step"
     puts $fileid "output_step = $output_step"
-    
+    # Volumen Output
+    set cxpath "$rootid//c.Results//i.VolumeOutput"
+    set VolumeOutput [::xmlutils::setXml $cxpath $cproperty]
+    if {$VolumeOutput eq "Yes"} {
+	puts $fileid "VolumeOutput = True"
+    } else {
+	puts $fileid "VolumeOutput = False"
+    }
+
     puts $fileid ""
     set PName [::KUtils::GetPaths "PName"]
     puts $fileid "problem_name=\"${PName}${AppId}\"" 
