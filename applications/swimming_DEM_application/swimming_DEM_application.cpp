ab2a186080ebd1aff71a3f131686310f7484aff7
gcasas <gcasas@cimne.upc.edu>
2013-12-19 17:56:45 +0000

    adding fluid element

--- applications/swimming_DEM_application/swimming_DEM_application.cpp
+++ applications/swimming_DEM_application/swimming_DEM_application.cpp
@@ -16,24 +16,24 @@
 
 // Project includes
 #include "includes/define.h"
-
-
 #include "includes/variables.h"
 #include "swimming_DEM_application.h"
 #include "geometries/point_3d.h"
 #include "geometries/line_3d_2.h"
 #include "../DEM_application/DEM_application.h"
+#include "../FluidDynamicsApplication/fluid_dynamics_application.h"
 
 namespace Kratos
 {
         
-  KRATOS_CREATE_VARIABLE(double, AUX_DOUBLE_VAR) //SALVA
-  //KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(PRESSURE_GRADIENT) //SALVA
+  KRATOS_CREATE_VARIABLE(double, AUX_DOUBLE_VAR)
   KRATOS_CREATE_VARIABLE(double, POWER_LAW_N)
   KRATOS_CREATE_VARIABLE(double, POWER_LAW_K)
   KRATOS_CREATE_VARIABLE(double, GEL_STRENGTH)
   
-KratosSwimmingDEMApplication::KratosSwimmingDEMApplication()
+KratosSwimmingDEMApplication::KratosSwimmingDEMApplication():
+  mMonolithicDEMCoupled2D(0, Element::GeometryType::Pointer(new Triangle2D3<Node<3> >(Element::GeometryType::PointsArrayType(3, Node<3>())))),
+  mMonolithicDEMCoupled3D(0, Element::GeometryType::Pointer(new Tetrahedra3D4<Node<3> >(Element::GeometryType::PointsArrayType(4, Node<3>()))))
 {}
 
 void KratosSwimmingDEMApplication::Register()
@@ -42,22 +42,18 @@ void KratosSwimmingDEMApplication::Register()
   KratosApplication::Register();
   std::cout << "Initializing KratosSwimmingDEMApplication... " << std::endl;
                 
-  KRATOS_REGISTER_VARIABLE(AUX_DOUBLE_VAR) //SALVA
-  
+
+  KRATOS_REGISTER_VARIABLE(AUX_DOUBLE_VAR)
   KRATOS_REGISTER_VARIABLE(POWER_LAW_N)
   KRATOS_REGISTER_VARIABLE(POWER_LAW_K)        
-  KRATOS_REGISTER_VARIABLE(GEL_STRENGTH)        
-  //KRATOS_REGISTER_3D_VARIABLE_WITH_COMPONENTS(PRESSURE_GRADIENT) //SALVA
-          
-  //  KRATOS_REGISTER_VARIABLE ( EXPORT_NEIGHBOUR_LIST )
+  KRATOS_REGISTER_VARIABLE(GEL_STRENGTH)
 
+      /* Define In Global variables.cpp */
 
-		/* Define In Global variables.cpp
-		  */
+  KRATOS_REGISTER_ELEMENT("MonolithicDEMCoupled2D", mMonolithicDEMCoupled2D)
+  KRATOS_REGISTER_ELEMENT("MonolithicDEMCoupled3D", mMonolithicDEMCoupled3D)
 
-	Serializer::Register( "VariablesList", mVariablesList );
-
-	
+  Serializer::Register( "VariablesList", mVariablesList );
  }
 
 }  // namespace Kratos.
