6805babc13178db58e9691669ca3ec6794eb0a60
Josep Maria <josep-m-carbonell@users.noreply.github.com>
2014-01-23 23:56:47 +0000

    correction in the displacement imposition

--- gid_interface/kratos.gid/python/KratosStructuralOpenMP.py
+++ gid_interface/kratos.gid/python/KratosStructuralOpenMP.py
@@ -329,6 +329,8 @@ for step in range(istep, nstep):
                 problem_restart.Save(current_time, current_step, restart_print.operation_id());
                 StopTimeMeasuring(clock_time, "Restart");
 
+        
+        conditions.RestartImposedDisp()
 
 # --FINALIZE--############################
 #
