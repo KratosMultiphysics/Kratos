df37b41b4ecd816f1ecea93e20b1032a0672f1cc
jcotela <jcotela@cimne.upc.edu>
2014-01-23 15:17:10 +0000

    Change for compatibility with Python 3.3

--- external_libraries/mpi_python/mpi.py
+++ external_libraries/mpi_python/mpi.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import sys
-if sys.platform == 'linux2':
+if sys.platform.startswith('linux'):
     import DLFCN as dl
     flags = sys.getdlopenflags()
     sys.setdlopenflags(dl.RTLD_NOW | dl.RTLD_GLOBAL)
