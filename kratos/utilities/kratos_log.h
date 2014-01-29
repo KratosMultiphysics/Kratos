14cdb638002fdf323451621ab63c03f329cd2fde
Carlos Roig <roigcarlo@gmail.com>
2014-01-29 09:52:34 +0000

    Corrected a bunch of mistakes Corrected all compiletion errors Implemented warning severity level Implemented EACH_N macro Implemented IF macro Implemented CHECK macro Redesigned preambles

--- kratos/utilities/kratos_log.h
+++ kratos/utilities/kratos_log.h
@@ -64,13 +64,23 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // Project includes
 #include "includes/define.h"
 
-// Macros
-#define KRATOS_LOG_ERROR_INFO(file,line,function,severity) "[" << #severity << "]:" << (file) << ":" << (line) << ":"
+// Preambles
+#define KRATOS_LOG_ERROR_PREAMBLE KRATOS_LOG_ERROR_INFO(__FILE__,__LINE__,BOOST_CURRENT_FUNCTION,KRATOS_SEVERITY_ERROR)
+#define KRATOS_LOG_ERROR_INFO(file,line,function,severity) "[KRATOS_ERROR]:" << (file) << ":" << (line) << ":"
 
-#define KRATOS_ERROR_LOG        KRATOS_LOG(KRATOS_SEVERITY_ERROR) << KRATOS_LOG_ERROR_INFO(__FILE__,__LINE__,BOOST_CURRENT_FUNCTION,KRATOS_SEVERITY_ERROR)
-#define KRATOS_ERROR_LOG_N      KRATOS_LOG_N(KRATOS_SEVERITY_ERROR)	
-#define KRATOS_ERROR_LOG_IF     KRATOS_LOG_IF(KRATOS_SEVERITY_ERROR)	
-#define KRATOS_ERROR_LOG_CHECK	KRATOS_LOG_CHECK(KRATOS_SEVERITY_ERROR)	
+#define KRATOS_LOG_WARNING_PREAMBLE KRATOS_LOG_WARNING_INFO(__FILE__,__LINE__,BOOST_CURRENT_FUNCTION,KRATOS_SEVERITY_ERROR)
+#define KRATOS_LOG_WARNING_INFO(file,line,function,severity) "[KRATOS_WARNING]:" << (file) << ":" << (line) << ":"
+
+// Check is only defined for KRATOS_SEVERITY_ERROR
+#define KRATOS_CHECK(C)         KRATOS_LOG_IF(KRATOS_SEVERITY_ERROR,C) << KRATOS_LOG_ERROR_PREAMBLE
+
+#define KRATOS_ERROR_LOG          KRATOS_LOG(KRATOS_SEVERITY_ERROR)      << KRATOS_LOG_ERROR_PREAMBLE
+#define KRATOS_ERROR_LOG_N(N)     KRATOS_LOG_N(KRATOS_SEVERITY_ERROR,N)  << KRATOS_LOG_ERROR_PREAMBLE
+#define KRATOS_ERROR_LOG_IF(C)    KRATOS_LOG_IF(KRATOS_SEVERITY_ERROR,C) << KRATOS_LOG_ERROR_PREAMBLE
+
+#define KRATOS_WARNING_LOG        KRATOS_LOG(KRATOS_SEVERITY_WARNING)      << KRATOS_LOG_WARNING_PREAMBLE
+#define KRATOS_WARNING_LOG_N(N)   KRATOS_LOG_N(KRATOS_SEVERITY_WARNING,N)  << KRATOS_LOG_WARNING_PREAMBLE
+#define KRATOS_WARNING_LOG_IF(C)  KRATOS_LOG_IF(KRATOS_SEVERITY_WARNING,C) << KRATOS_LOG_WARNING_PREAMBLE
 
 #ifdef  KRATOS_DEBUG
 #define KRATOS_DEBUG_LOG        KRATOS_LOG(KRATOS_DEBUG)
@@ -80,18 +90,23 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define KRATOS_TRACE_LOG    
 #endif
 
-#define KRATOS_LOG(SEVERITY)                                                           \
-  KratosLog<SEVERITY>::GetInstance()    \
+// Normal log
+#define KRATOS_LOG(SEVERITY) KratosLog<SEVERITY>::GetInstance()
 
-# define KRATOS_LOG_OCCURRENCES(line) kratos_log_loop_counter ## line
+// Each-N block
+#define KRATOS_LOG_OCCURRENCES KRATOS_LOG_OCCURRENCES_LINE(__LINE__)
+#define KRATOS_LOG_OCCURRENCES_LINE(line) kratos_log_loop_counter##line
 
-// Each N
-#define KRATOS_LOG_N(KRATOS_SEVERITY) \
-  {static int KRATOS_LOG_OCCURRENCES(__LINE__) = 0, KRATOS_LOG_OCCURRENCES_MOD_N = 0; \
-  ++LOG_OCCURRENCES; \
-  if (++KRATOS_LOG_OCCURRENCES > 10) KRATOS_LOG_OCCURRENCES -= n; \
+#define KRATOS_LOG_N(KRATOS_SEVERITY,N) \
+  static int KRATOS_LOG_OCCURRENCES = 0;\
+  if (++KRATOS_LOG_OCCURRENCES > N) KRATOS_LOG_OCCURRENCES -= N; \
   if (KRATOS_LOG_OCCURRENCES == 1) \
-    KRATOS_LOG(KRATOS_SEVERITY) << KRATOS_LOG_ERROR_INFO(__FILE__,__LINE__,BOOST_CURRENT_FUNCTION,KRATOS_SEVERITY_ERROR)}
+    KRATOS_LOG(KRATOS_SEVERITY)
+    
+// If-block
+#define KRATOS_LOG_IF(KRATOS_SEVERITY,C) \
+  if (C) \
+    KRATOS_LOG(KRATOS_SEVERITY)
 
 #define KRATOS_TIME_STAMP   "[" << CurrentDateTime() << "]"
 
@@ -119,11 +134,11 @@ namespace Kratos
   
 enum KRATOS_SEVERITY {
   KRATOS_SEVERITY_ERROR,
-  KRATOS_SEVERITY_WAR,
-  KRATOS_SEVERITY_INF,
-  KRATOS_SEVERITY_DET,
-  KRATOS_SEVERITY_DEB,
-  KRATOS_SEVERITY_TRC,
+  KRATOS_SEVERITY_WARNING,
+  KRATOS_SEVERITY_INFO,
+  KRATOS_SEVERITY_DETEAIL,
+  KRATOS_SEVERITY_DEBUG,
+  KRATOS_SEVERITY_TRACE,
   KRATOS_LOG_SOLVER
 };
  
@@ -165,13 +180,43 @@ class KratosLog : public std::ostream
 ///@}
 ///@name Input and output
 ///@{
+  
+// Generic Specialization
+template<KRATOS_SEVERITY S>
+inline KratosLog<S> & operator << (KratosLog<S> &out, std::ostream& (*fn)(std::ostream&))
+{ 
+  if(out.mPrintLogOnScreen)
+    fn(std::cout);
+     
+  fn((*out.mOutputFile));
+  (*out.mOutputFile).flush();
+        
+  return out;
+};
+  
+template<class T, KRATOS_SEVERITY S>
+inline KratosLog<S> & operator << (KratosLog<S> &out, const T& data)
+{
+  if(out.mPrintLogOnScreen)
+  {
+      std::cout << data;
+  }
 
+  (*out.mOutputFile) << data;
+
+  return out;  
+};
+  
+// Error specialization
 inline KratosLog<KRATOS_SEVERITY_ERROR> & operator << (KratosLog<KRATOS_SEVERITY_ERROR> &out, std::ostream& (*fn)(std::ostream&))
 { 
   if(out.mPrintLogOnScreen)
     fn(std::cout);
      
   fn((*out.mOutputFile));
+  (*out.mOutputFile).flush();
+  
+  abort();
         
   return out;
 };
@@ -185,16 +230,10 @@ inline KratosLog<KRATOS_SEVERITY_ERROR> & operator << (KratosLog<KRATOS_SEVERITY
   }
 
   (*out.mOutputFile) << data;
-  (*out.mOutputFile).flush();
-
-//   if(out.mSeverityLevel == getKratosSeverityError()?)
-//   {
-//       (*out.mOutputFile).flush();
-//       abort();
-//   }
 
   return out;  
 };
+
 
 ///@}
 
