fcd2eb86a633c025d3b1a80940d3feec7f6980bb
mpetracca <mpetracca@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-02-10 17:09:31 +0000

    Adding Shell Elements Tests

--- kratos/applications/SolidMechanicsApplication/test_examples/solid_mechanics_run_all_benchmarks.py
+++ kratos/applications/SolidMechanicsApplication/test_examples/solid_mechanics_run_all_benchmarks.py
@@ -33,9 +33,77 @@ def Run():
     os.chdir("..")
 
     #
+    
+    #
+    # SHELL elements tests
+    
+    Text += "Shell Q4 Thick Bending RollUp test: "
+    os.chdir("Shell_Q4_Thick__BendingRollUp.gid")
+    sys.path.append(os.getcwd())
+    print("running the Shell Q4 Thick Bending RollUp benchmark test...")
+    Msg = benchmarking.RunBenchmark("run_test.py", "benchmark_results.txt")
+    if (Msg):
+        Text += "OK\n"
+        print("Shell Q4 Thick Bending RollUp test succesful")
+    else:
+        Text += "FAILED\n"
+        Text += Msg
+        Text += "\n\n"
+        print("Shell Q4 Thick Bending RollUp test FAILED")
+    os.chdir("..")
+    
+    Text += "Shell Q4 Thick Drilling RollUp test: "
+    os.chdir("Shell_Q4_Thick__DrillingRollUp.gid.gid")
+    sys.path.append(os.getcwd())
+    print("running the Shell Q4 Thick Drilling RollUp benchmark test...")
+    Msg = benchmarking.RunBenchmark("run_test.py", "benchmark_results.txt")
+    if (Msg):
+        Text += "OK\n"
+        print("Shell Q4 Thick Drilling RollUp test succesful")
+    else:
+        Text += "FAILED\n"
+        Text += Msg
+        Text += "\n\n"
+        print("Shell Q4 Thick Drilling RollUp test FAILED")
+    os.chdir("..")
+    
+    Text += "Shell T3 Thin Bending RollUp test: "
+    os.chdir("Shell_T3_Thin__BendingRollUp.gid")
+    sys.path.append(os.getcwd())
+    print("running the Shell T3 Thin Bending RollUp benchmark test...")
+    Msg = benchmarking.RunBenchmark("run_test.py", "benchmark_results.txt")
+    if (Msg):
+        Text += "OK\n"
+        print("Shell T3 Thin Bending RollUp test succesful")
+    else:
+        Text += "FAILED\n"
+        Text += Msg
+        Text += "\n\n"
+        print("Shell T3 Thin Bending RollUp test FAILED")
+    os.chdir("..")
+    
+    Text += "Shell T3 Thin Drilling RollUp test: "
+    os.chdir("Shell_T3_Thin__DrillingRollUp.gid.gid")
+    sys.path.append(os.getcwd())
+    print("running the Shell T3 Thin Drilling RollUp benchmark test...")
+    Msg = benchmarking.RunBenchmark("run_test.py", "benchmark_results.txt")
+    if (Msg):
+        Text += "OK\n"
+        print("Shell T3 Thin Drilling RollUp test succesful")
+    else:
+        Text += "FAILED\n"
+        Text += Msg
+        Text += "\n\n"
+        print("Shell T3 Thin Drilling RollUp test FAILED")
+    os.chdir("..")
+    
+    #
+    #
+    
     print("resume of all of the examples for the Solid Mechanics application :")
     print(Text)
     return Text
+    
 
 if __name__ == '__main__':
     Run()
