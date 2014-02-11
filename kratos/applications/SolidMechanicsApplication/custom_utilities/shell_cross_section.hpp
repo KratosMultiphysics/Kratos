975b51590d5e89c44edb454a58b060e05799c4a6
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-02-11 14:07:47 +0000

    some warnings and errors of windows linux compatibility

--- kratos/applications/SolidMechanicsApplication/custom_utilities/shell_cross_section.hpp
+++ kratos/applications/SolidMechanicsApplication/custom_utilities/shell_cross_section.hpp
@@ -1,46 +1,3 @@
-/*
-==============================================================================
-KratosMultiScaleApplication
-A library based on:
-Kratos
-A General Purpose Software for Multi-Physics Finite Element Analysis
-Version 1.0 (Released on march 05, 2007).
-
-Copyright 2007
-Pooyan Dadvand, Riccardo Rossi, Janosch Stascheit, Felix Nagel
-pooyan@cimne.upc.edu
-rrossi@cimne.upc.edu
-janosch.stascheit@rub.de
-nagel@sd.rub.de
-- CIMNE (International Center for Numerical Methods in Engineering),
-Gran Capita' s/n, 08034 Barcelona, Spain
-- Ruhr-University Bochum, Institute for Structural Mechanics, Germany
-
-
-Permission is hereby granted, free  of charge, to any person obtaining
-a  copy  of this  software  and  associated  documentation files  (the
-"Software"), to  deal in  the Software without  restriction, including
-without limitation  the rights to  use, copy, modify,  merge, publish,
-distribute,  sublicense and/or  sell copies  of the  Software,  and to
-permit persons to whom the Software  is furnished to do so, subject to
-the following condition:
-
-Distribution of this code for  any  commercial purpose  is permissible
-ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
-
-The  above  copyright  notice  and  this permission  notice  shall  be
-included in all copies or substantial portions of the Software.
-
-THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
-EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
-CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
-TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-==============================================================================
-*/
 //
 //   Project Name:        Kratos
 //   Last Modified by:    $Author: Massimo Petracca $
@@ -281,7 +238,7 @@ public:
 		IntegrationPoint()
 			: mWeight(0.0)
 			, mLocation(0.0)
-			, mConstitutiveLaw(NULL)
+			, mConstitutiveLaw(ConstitutiveLaw::Pointer())
 		{}
 		
 		IntegrationPoint(double location, double weight, const ConstitutiveLaw::Pointer pMaterial)
@@ -293,14 +250,14 @@ public:
 		IntegrationPoint(const IntegrationPoint& other)
 			: mWeight(other.mWeight)
 			, mLocation(other.mLocation)
-			, mConstitutiveLaw(other.mConstitutiveLaw != NULL ? other.mConstitutiveLaw->Clone() : NULL)
+			, mConstitutiveLaw(other.mConstitutiveLaw != NULL ? other.mConstitutiveLaw->Clone() : ConstitutiveLaw::Pointer())
 		{}
 		
 		IntegrationPoint & operator = (const IntegrationPoint & other) {
 			if(this != &other) {
 				mWeight = other.mWeight;
 				mLocation = other.mLocation;
-				mConstitutiveLaw = other.mConstitutiveLaw != NULL ? other.mConstitutiveLaw->Clone() : NULL;
+				mConstitutiveLaw = other.mConstitutiveLaw != NULL ? other.mConstitutiveLaw->Clone() : ConstitutiveLaw::Pointer();
 			}
 			return *this;
 		}
@@ -357,7 +314,7 @@ public:
 			, mLocation(0.0)
 			, mOrientationAngle(0.0)
 			, mIntegrationPoints()
-			, mpProperties(NULL)
+			, mpProperties(Properties::Pointer())
 		{}
 		
 		Ply(double thickness, double location, double orientationAngle, int numPoints, const Properties::Pointer & pProperties)
@@ -1224,8 +1181,8 @@ private:
 
 public:
 
-	DECLARE_ADD_THIS_TYPE_TO_PROPERTIES;
-	DECLARE_GET_THIS_TYPE_FROM_PROPERTIES;
+	DECLARE_ADD_THIS_TYPE_TO_PROPERTIES
+	DECLARE_GET_THIS_TYPE_FROM_PROPERTIES
 
 };
 
