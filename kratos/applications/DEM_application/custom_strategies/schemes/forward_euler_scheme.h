e4bca2f112ef09542944ff2dbf686b1f1f159c50
msantasusana <msantasusana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-02-19 10:37:03 +0000

    minor

--- kratos/applications/DEM_application/custom_strategies/schemes/forward_euler_scheme.h
+++ kratos/applications/DEM_application/custom_strategies/schemes/forward_euler_scheme.h
@@ -116,7 +116,7 @@ namespace Kratos
                   array_1d<double, 3 > & coor            = i->Coordinates();
                   array_1d<double, 3 > & initial_coor    = i->GetInitialPosition();
                   array_1d<double, 3 > & force           = i->FastGetSolutionStepValue(TOTAL_FORCES);
-                  
+
                   double mass                            = i->FastGetSolutionStepValue(SQRT_OF_MASS);
                   mass                                  *= mass;
                   
