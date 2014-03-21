97fdf49f6136a9b7463b324116438444214c6d7d
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-03-21 10:50:09 +0000

    meshes movement removed from the embedded script

--- kratos/applications/swimming_DEM_application/python_scripts/embedded.py
+++ kratos/applications/swimming_DEM_application/python_scripts/embedded.py
@@ -89,7 +89,7 @@ def ApplyEmbeddedBCsToFluid(model_part):
             node.Free(VELOCITY_X)
             node.Free(VELOCITY_Y)
             node.Free(VELOCITY_Z)
-
+            
     if model_part.NumberOfMeshes() > 1:
 
         for mesh_number in range(2, model_part.NumberOfMeshes()):
@@ -162,53 +162,6 @@ def ApplyEmbeddedBCsToBalls(model_part, DEMParameters):
                 #print("--------------- One particle was erased because it entered an embedded structure -------------------")
                 node.Set(TO_ERASE,True)
 
-def MoveEmbeddedStructure(model_part, time):
-
-    if model_part.NumberOfMeshes() > 1:
-
-        for mesh_number in range(1, model_part.NumberOfMeshes()):
-            mesh_nodes       = model_part.GetMesh(mesh_number).Nodes
-            # print model_part.Properties[mesh_number]
-            linear_velocity  = model_part.GetMesh(mesh_number).Properties[0][VELOCITY]
-            linear_period    = model_part.GetMesh(mesh_number).Properties[0][VELOCITY_PERIOD]
-            angular_velocity = model_part.GetMesh(mesh_number).Properties[0][ANGULAR_VELOCITY]
-            angular_period   = model_part.GetMesh(mesh_number).Properties[0][ANGULAR_VELOCITY_PERIOD]
-            initial_center   = model_part.GetMesh(mesh_number).Properties[0][ROTATION_CENTER]
-
-            if (linear_period > 0.0):
-                linear_omega = 2 * math.pi / linear_period
-                center_position = VectSum(initial_center, VectTimes(linear_velocity, math.sin(linear_omega * time) / linear_omega))
-            else:
-                center_position = initial_center + time * linear_velocity
-
-            if (angular_period > 0.0):
-                angular_omega = 2 * math.pi / angular_period
-                angle = VectTimes(angular_velocity, math.sin(angular_omega * time) / angular_omega)
-            else:
-                angle = VectTimes(angular_velocity, time)
-
-            mod_angular_velocity = Norm(angular_velocity)
-            relative_position = [0.0, 0.0, 0.0]
-
-            if (mod_angular_velocity > 0.0):
-                ang = Norm(angle)
-                rotation_axis = Normalize(angular_velocity)
-                e1 = [1.0, 0, 0]
-                e2 = [0, 1.0, 0]
-                [new_axes1, new_axes2, new_axes3] = RotateRightHandedBasisAroundAxis(e1, e2, rotation_axis, ang)
-
-                for node in mesh_nodes:
-                    local_X = node.X0 - initial_center[0]
-                    local_Y = node.Y0 - initial_center[1]
-                    local_Z = node.Z0 - initial_center[2]
-                    relative_position[0] = new_axes1[0] * local_X + new_axes2[0] * local_Y + new_axes3[0] * local_Z
-                    relative_position[1] = new_axes1[1] * local_X + new_axes2[1] * local_Y + new_axes3[1] * local_Z
-                    relative_position[2] = new_axes1[2] * local_X + new_axes2[2] * local_Y + new_axes3[2] * local_Z
-                    # NEW POSITION
-                    [node.X, node.Y, node.Z] = VectSum(center_position, relative_position)
-                    velocity_due_to_rotation = Cross(relative_position, angular_velocity)
-                    # NEW VELOCITY
-                    node.SetSolutionStepValue(VELOCITY, VectSum(linear_velocity, velocity_due_to_rotation))
 
 class SearchEmbeddedDEMTools:
 
