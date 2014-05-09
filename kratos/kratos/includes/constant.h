553a196680bb318a5bdb1eb881e6e687840b8d03
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-05-09 09:39:22 +0000

    Adding constant.h and some initial definitions

---
+++ kratos/kratos/includes/constant.h
@@ -0,0 +1,57 @@
+/*
+==============================================================================
+Kratos
+A General Purpose Software for Multi-Physics Finite Element Analysis
+Version 1.0 (Released on march 05, 2007).
+
+Copyright 2007
+Pooyan Dadvand, Riccardo Rossi
+pooyan@cimne.upc.edu
+rrossi@cimne.upc.edu
+CIMNE (International Center for Numerical Methods in Engineering),
+Gran Capita' s/n, 08034 Barcelona, Spain
+
+Permission is hereby granted, free  of charge, to any person obtaining
+a  copy  of this  software  and  associated  documentation files  (the
+"Software"), to  deal in  the Software without  restriction, including
+without limitation  the rights to  use, copy, modify,  merge, publish,
+distribute,  sublicense and/or  sell copies  of the  Software,  and to
+permit persons to whom the Software  is furnished to do so, subject to
+the following condition:
+
+Distribution of this code for  any  commercial purpose  is permissible
+ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNER.
+
+The  above  copyright  notice  and  this permission  notice  shall  be
+included in all copies or substantial portions of the Software.
+
+THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
+CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
+TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+*/
+
+//
+//   Project Name:        Kratos
+//   Last Modified by:    $Author: rrossi $
+//   Date:                $Date: 2007-03-06 10:30:33 $
+//   Revision:            $Revision: 1.3 $
+//
+//
+
+#if !defined(KRATOS_CONSTANTS )
+#define  KRATOS_CONSTANTS
+
+const double KRATOS_M_PI 	= 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651f;
+const double KRATOS_M_PI_2 	= 1.57079632679489661923132169163975144209858469968755291048747229615390820314310449931401741267105853399107404326f;
+const double KRATOS_M_PI_3 	= 1.04719755119659774615421446109316762806572313312503527365831486410260546876206966620934494178070568932738269550f;
+
+#endif // KRATOS_CONSTANTS defined 
+
+
+
