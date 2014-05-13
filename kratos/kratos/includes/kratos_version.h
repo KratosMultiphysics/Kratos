c19210017bb33523a904d5a71ffdd5a119310546
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-05-13 09:46:18 +0000

    Added "kratos_version.h" with default Kratos version

---
+++ kratos/kratos/includes/kratos_version.h
@@ -0,0 +1,8 @@
+//defines for the kratos version
+#define KRATOS_MAJOR_VERSION  3
+#define KRATOS_MINOR_VERSION  3
+
+//svn version at configure time
+#define KRATOS_SVN_REVISION  0 
+
+#define KRATOS_VERSION  "3.3.0"
