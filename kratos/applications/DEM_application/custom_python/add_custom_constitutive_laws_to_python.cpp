3b8d000239db1c4cb9c7aca321158fc139b9c028
msantasusana <msantasusana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-05-15 15:33:09 +0000

    DEM constitutive laws

---
+++ kratos/applications/DEM_application/custom_python/add_custom_constitutive_laws_to_python.cpp
@@ -0,0 +1,67 @@
+//
+//   Project Name:        KratosSolidMechanicsApplication $
+//   Last modified by:    $Author:            MSantasusana $
+//   Date:                $Date:                Maig 2014 $
+//   Revision:            $Revision:                  0.0 $
+//
+//
+
+// System includes
+#include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+
+// External includes
+
+
+// Project includes
+#include "includes/define.h"
+
+
+//constitutive laws
+#include "custom_constitutive/DEM_discontinuum_constitutive_law.h"    
+#include "custom_constitutive/DEM_continuum_constitutive_law.h"     
+
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+using namespace boost::python;
+
+
+void  AddCustomConstitutiveLawsToPython()
+{
+    
+
+    //ConstitutiveLaw
+
+    class_< DEMDiscontinuumConstitutiveLaw, DEMDiscontinuumConstitutiveLaw::Pointer, boost::noncopyable >  //bases< ConstitutiveLawBaseType >
+    ( "DEMDiscontinuumConstitutiveLaw",
+      init<>() )
+    .def("Clone",&DEMDiscontinuumConstitutiveLaw::Clone)
+    .def("SetConstitutiveLawInProperties",&DEMDiscontinuumConstitutiveLaw::SetConstitutiveLawInProperties)
+    ;
+   
+    class_<Variable<DEMDiscontinuumConstitutiveLaw::Pointer>, boost::noncopyable >( "DEMDiscontinuumConstitutiveLawPointerVariable", no_init )
+    .def( self_ns::str( self ) )
+    ;
+    
+    
+    
+    class_< DEMContinuumConstitutiveLaw, DEMContinuumConstitutiveLaw::Pointer, boost::noncopyable >  //bases< ConstitutiveLawBaseType >
+    ( "DEMContinuumConstitutiveLaw",
+      init<>() )
+    .def("Clone",&DEMContinuumConstitutiveLaw::Clone)
+    .def("SetConstitutiveLawInProperties",&DEMContinuumConstitutiveLaw::SetConstitutiveLawInProperties)
+    ;
+   
+    class_<Variable<DEMContinuumConstitutiveLaw::Pointer>, boost::noncopyable >( "DEMContinuumConstitutiveLawPointerVariable", no_init )
+    .def( self_ns::str( self ) )
+    ;
+
+}
+
+}  // namespace Python.
+}  // namespace Kratos.
