9b3acf39238871d77d2c44edc78f97b99cf53704
msantasusana <msantasusana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-05-15 15:33:16 +0000

    DEM constitutive laws

---
+++ kratos/applications/DEM_application/custom_python/add_custom_constitutive_laws_to_python.h
@@ -0,0 +1,37 @@
+//
+//   Project Name:        KratosSolidMechanicsApplication $
+//   Last modified by:    $Author:            MSantasusana $
+//   Date:                $Date:                Maig 2014 $
+//   Revision:            $Revision:                  0.0 $
+//
+//
+
+#if !defined(KRATOS_ADD_CUSTOM_DEM_CONSTITUTIVE_LAWS_TO_PYTHON_H_INCLUDED )
+#define  KRATOS_ADD_CUSTOM_DEM_CONSTITUTIVE_LAWS_TO_PYTHON_H_INCLUDED
+
+
+
+
+// System includes
+
+
+// External includes
+#include "boost/smart_ptr.hpp"
+
+
+// Project includes
+#include "includes/define.h"
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+void  AddCustomConstitutiveLawsToPython();
+
+}  // namespace Python.
+
+}  // namespace Kratos.
+
+#endif // KRATOS_ADD_CUSTOM_DEM_CONSTITUTIVE_LAWS_TO_PYTHON_H_INCLUDED  defined 
