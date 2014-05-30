9ec362825adcac90d86b849816c8b8ab9e7fdd9f
Josep Maria <josep-m-carbonell@users.noreply.github.com>
2014-05-30 17:31:23 +0000

    order benchmarks changed... solidmechanics needs an update of the scripts to run correctly

--- benchmarking/run_all_benchmarks.py
+++ benchmarking/run_all_benchmarks.py
@@ -13,22 +13,30 @@ ApplicationsRoot = KratosRoot + "applications/"
 Text = "Status of Kratos examples:\n\n"
 os.chdir(ApplicationsRoot)
 
+# SolidMechanics application
+os.chdir(ApplicationsRoot + "SolidMechanicsApplication/test_examples")
+import solid_mechanics_run_all_benchmarks
+Text += solid_mechanics_run_all_benchmarks.Run()
+
+# Fluid Dynamics application
+os.chdir(ApplicationsRoot + "FluidDynamicsApplication/test_examples")
+import fluid_dynamics_run_all_benchmarks
+Text += fluid_dynamics_run_all_benchmarks.Run()
+
+# DEM application
+os.chdir(ApplicationsRoot + "DEM_application/test_examples")
+import DEM_benchmarks
+Text += DEM_benchmarks.Run()
+
 # Structural application
 os.chdir(ApplicationsRoot + "structural_application/test_examples")
 import structural_benchmarks
 Text += structural_benchmarks.Run()
 
-
 # Incompressible fluid application
 os.chdir(ApplicationsRoot + "incompressible_fluid_application/test_examples")
 import incompressible_benchmarks
 Text += incompressible_benchmarks.Run()
-
-
-# Meshing application
-os.chdir(ApplicationsRoot + "MeshingApplication/test_exemples")
-import meshing_benchmarks
-Text += meshing_benchmarks.Run()
 
 # Convection-Diffusion application
 os.chdir(ApplicationsRoot + "convection_diffusion_application/test_examples")
@@ -45,28 +53,18 @@ Text += FSI_benchmarks.Run()
 # import pfem_benchmarks
 # Text += pfem_benchmarks.Run()
 
-# Fluid Dynamics application
-os.chdir(ApplicationsRoot + "FluidDynamicsApplication/test_examples")
-import fluid_dynamics_run_all_benchmarks
-Text += fluid_dynamics_run_all_benchmarks.Run()
-
-# Fluid Dynamics application
+# Thermo Mechanical application
 os.chdir(ApplicationsRoot + "ThermoMechanicalApplication/test_examples")
 import ThermoMechanical_benchmarks
 Text += ThermoMechanical_benchmarks.Run()
 
-# SolidMechanics application
-os.chdir(ApplicationsRoot + "SolidMechanicsApplication/test_examples")
-import solid_mechanics_run_all_benchmarks
-Text += solid_mechanics_run_all_benchmarks.Run()
+# Meshing application
+os.chdir(ApplicationsRoot + "MeshingApplication/test_exemples")
+import meshing_benchmarks
+Text += meshing_benchmarks.Run()
 
-# DEM application
-os.chdir(ApplicationsRoot + "DEM_application/test_examples")
-import DEM_benchmarks
-Text += DEM_benchmarks.Run()
-
-# Fluid Dynamics application
-os.chdir(ApplicationsRoot + "trilinos_application//test_examples")
+# Trilinos application
+os.chdir(ApplicationsRoot + "trilinos_application/test_examples")
 import trilinos_benchmarks
 Text += trilinos_benchmarks.Run()
 
