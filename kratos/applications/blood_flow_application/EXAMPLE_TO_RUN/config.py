8d7a379ff5ea92ac480efef498f84208620a3c74
esoudah <esoudah@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-06-05 08:55:48 +0000

    ps

--- kratos/applications/blood_flow_application/EXAMPLE_TO_RUN/config.py
+++ kratos/applications/blood_flow_application/EXAMPLE_TO_RUN/config.py
@@ -5,7 +5,8 @@ from __future__ import print_function, absolute_import, division
 relative_path_3D = "/home/esoudah/kratos/applications/blood_flow_application/EXAMPLE_TO_RUN/3D/"
 #relative_path_1D = "/home/biomedical/Desktop/IamWorkingOn_April/Example_TORUN/1D/"
 relative_path_1D = "/home/esoudah/kratos/applications/blood_flow_application/EXAMPLE_TO_RUN/1D/"
-name = "volume_mesh_tube"
+#name = "volume_mesh_tube"
+name = "simple_test"
 artery_type = [3]
 deactivate_list = [40, 41]
 inlets_1d = [[100, 40]]
@@ -14,6 +15,6 @@ outlets_1d = [[1001, 41]]
 systolic_pressure = 12500.0
 diastolic_pressure = 7500.0
 time_period = 0.1
-nro_cardiac_cycles = 2
+nro_cardiac_cycles = 1
 # Config_Hypermia_pressure
 diastolic_hypermia_pressure = 7500.0
