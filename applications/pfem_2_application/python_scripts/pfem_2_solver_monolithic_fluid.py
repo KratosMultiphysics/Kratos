a88b3ebd890db3bcf504e135acbd10957f4e9ff5
pbecker <pablobecker@gmail.com>
2014-07-29 15:09:57 +0000

    minor changes

--- applications/pfem_2_application/python_scripts/pfem_2_solver_monolithic_fluid.py
+++ applications/pfem_2_application/python_scripts/pfem_2_solver_monolithic_fluid.py
@@ -1,25 +1,28 @@
 from KratosMultiphysics import *
 from KratosMultiphysics.PFEM2Application import *
-from KratosMultiphysics.ConvectionDiffusionApplication import *
+#from KratosMultiphysics.ConvectionDiffusionApplication import *
 from KratosMultiphysics.ExternalSolversApplication import *
-from KratosMultiphysics.OpenCLApplication import *
+#from KratosMultiphysics.OpenCLApplication import *
 #from KratosMultiphysics.GPUSolversApplication import *
 
 from math import sqrt
 import time as timer
 #dir(pure_diffusion_application)
 
-def AddVariables(model_part,linea_model_part):
+def AddVariables(model_part):
     model_part.AddNodalSolutionStepVariable(PRESSURE);
     model_part.AddNodalSolutionStepVariable(DISTANCE);
     model_part.AddNodalSolutionStepVariable(VELOCITY);
     model_part.AddNodalSolutionStepVariable(FRACT_VEL);
     model_part.AddNodalSolutionStepVariable(G_VALUE);
     model_part.AddNodalSolutionStepVariable(TEMPERATURE);
+    model_part.AddNodalSolutionStepVariable(PRESS_PROJ);
+    model_part.AddNodalSolutionStepVariable(PRESS_PROJ_NO_RO);
     model_part.AddNodalSolutionStepVariable(YP);
 
     model_part.AddNodalSolutionStepVariable(RHS);
     model_part.AddNodalSolutionStepVariable(MESH_VELOCITY);
+    model_part.AddNodalSolutionStepVariable(ACCELERATION);
     model_part.AddNodalSolutionStepVariable(NORMAL);	
     #model_part.AddNodalSolutionStepVariable(DENSITY);	
 
@@ -114,7 +117,6 @@ class PFEM2Solver:
 		self.calculatewatervolume = CalculateWaterFraction2D(self.model_part)
 	else:	
 		self.calculatewatervolume = CalculateWaterFraction3D(self.model_part)
-	print "hola0"
 	self.moveparticles.MountBinDiff()
         #self.matrix_container.ConstructCSRVector(self.model_part)
         #self.matrix_container.BuildCSRData(self.model_part)
@@ -150,16 +152,6 @@ class PFEM2Solver:
 
 	(self.addBC).AddThem()
 	'''	
-	#RICC TOOLS
-	self.convection_solver = PureConvectionUtilities2D();
-	self.convection_solver.ConstructSystem(self.model_part,DISTANCE,VELOCITY,FORCE);
-	if self.domain_size==2:
-		self.distance_utils = SignedDistanceCalculationUtils2D()
-	else:
-		self.distance_utils = SignedDistanceCalculationUtils3D()
-	self.redistance_step = 0
-
-	print "hola1"
 	import strategy_python #implicit solver
 
 	self.monolitic_solver = strategy_python.SolvingStrategyPython(self.model_part,self.time_scheme,self.monolitic_linear_solver,self.conv_criteria,CalculateReactionFlag,ReformDofSetAtEachStep,MoveMeshFlag)
@@ -179,7 +171,6 @@ class PFEM2Solver:
 	self.nodaltasks=0.0
 	self.thermal=0.0
 	self.implicitviscosity=0.0
-	print "hola2"
         
       
                  
@@ -192,7 +183,6 @@ class PFEM2Solver:
 		add_gravity=False #in the monolitic solver we do not add the gravity, it is added directly in the implicit monolitic system
 		#add_gravity=True #in the monolitic solver we do not add the gravity, it is added directly in the implicit monolitic system
 
-		transfer_pressure=False
 		temperature_implicit_calculation=False;
 		viscosity_streamline_integrate=False; #True means explicit integration! False means we will have to solve the fractional velocity system implicetely later, once information has been passed to mesh
 		#pressure_gradient_integrate=False; #we use the pressure of the previous step. not reccomended for 2 fluid flows. REMOVED FROM THE CODE	
@@ -227,72 +217,33 @@ class PFEM2Solver:
 		self.reseed = self.reseed + t4-t3a
 		self.prereseed = self.prereseed + t4-t3a
 
-		#self.distance_utils.CalculateDistances(self.model_part,DISTANCE,0.1)
 
-		#for node in self.model_part.Nodes:
-		#	node.SetSolutionStepValue(TEMP_CONV_PROJ,node.GetSolutionStepValue(DISTANCE))
-
-		#transfering data from the particles to the mesh:
-		(self.moveparticles).TransferLagrangianToEulerian(transfer_pressure);
-
-		#for node in self.model_part.Nodes:
-		#	node.SetSolutionStepValue(MESH_VELOCITY,node.GetSolutionStepValue(VELOCITY))
-
-		#for node in self.model_part.Nodes:
-		#	node.SetSolutionStepValue(DISTANCE,node.GetSolutionStepValue(TEMP_CONV_PROJ))
-		#self.convection_solver.CalculateProjection(self.model_part,DISTANCE,NODAL_AREA,VELOCITY,FORCE,TEMP_CONV_PROJ);
-		#self.convection_solver.ConvectScalarVar(self.model_part,self.linear_solver,DISTANCE,VELOCITY,FORCE,TEMP_CONV_PROJ,1);
-		
-		print "hola1"
-
-		#value  = elem_size*0.02
-
+		(self.moveparticles).TransferLagrangianToEulerian();
 
 		t5 = timer.time()
 		self.lagrangiantoeulerian = self.lagrangiantoeulerian + t5-t4
-		print "hola2"
-		#Flagging splitted elements (the ones who need special integration)
-		#if combustion_problem:
-		#	(self.moveparticles).FlagSplittedElementsAndTheirNodesForCombustion();
-		#else:
+
+
 		(self.moveparticles).FlagSplittedElementsAndTheirNodes();
-		print "hola4"
-		#if (implicit_velocity_correction==False): #already solved in the streamlines, no need to do it now.
-		#	(self.moveparticles).MoveDataFromMESH_VELOCITYtoFRACT_VEL(); #insted of first step,. when moving through streamlines the first step is already performed
-		#else: #we did not do it. doing it explicitely
-		#	(self.moveparticles).CalculateFirstStep();
-			#self.model_part.ProcessInfo.SetValue(NL_ITERATION_NUMBER, 0) #1= semiimplicit, =0 fully implciit
-			#(self.solver).Solve()
-		
+
 		t6 = timer.time()
 
-		if (temperature_implicit_calculation):
-			self.model_part.ProcessInfo.SetValue(FRACTIONAL_STEP, 5)
-			print "calculating temperature implicitely"	
-			(self.temperature_solver).Solve()
 		t7=timer.time()
-		self.thermal = self.thermal + t7-t6
-		print "hola5"
-		
+
 		(self.VariableUtils).CopyVectorVar(MESH_VELOCITY,VELOCITY,self.model_part.Nodes)
-		#self.CalculateExplicitPressureViscousCorrection()
-		#pressure iterations
-		
 		
 		full_reset=False;
-		#self.CalculateMassMatrix();
-		#(self.moveparticles).ResetBoundaryConditions(full_reset) 
 		
 		#implicit everything
-		print "hola6"
 		full_reset=True;
 		(self.moveparticles).ResetBoundaryConditions(full_reset) 
 
-		(self.monolitic_solver).Solve() #implicit resolution of the pressure system. All the other tasks are explicit
-		print "hola7"
+		(self.monolitic_solver).Solve() #implicit resolution of the system. 
+		self.CalculatePressureProjection() #for the next timestep
+
 		full_reset=True;
 		(self.moveparticles).ResetBoundaryConditions(full_reset) 
-		#delta_velocity= Velocity(final) - MeshVelocity(from the particles), so we add to the particles the correction done in the mesh.
+
 		(self.moveparticles).CalculateDeltaVelocity();		
 		t11 = timer.time()
 		self.navierstokes = self.navierstokes + t11-t6
@@ -311,7 +262,7 @@ class PFEM2Solver:
 			self.water_initial_volume_flag=False
 		print self.water_volume
 		water_fraction= self.water_volume/(self.water_initial_volume+1e-9)
-		self.mass_correction_factor = (1.0 - water_fraction) * 100.0 * 0.2
+		self.mass_correction_factor = (1.0 - water_fraction) * 100.0 * 0.25
 		#if self.mass_correction_factor>0.5:
 		#	 self.mass_correction_factor
 		print "mass correction factor: ", self.mass_correction_factor
@@ -319,12 +270,10 @@ class PFEM2Solver:
 		(self.moveparticles).PostReseed(post_minimum_number_of_particles,self.mass_correction_factor);		
 		t13 = timer.time()
 		self.reseed=self.reseed+ t13-t12
-		#self.nodaltasks = self.nodaltasks + t11-t9
 
-		#print ". erasing  = ", t14-t13
 		self.total = self.total + t13-t1 
 		
-		if combustion_problem==False:
+		if True:
 			print "----------TIMES----------"
 			print "self.calculateinitialdrag  " , self.calculateinitialdrag , "in % = ",  100.0*(self.calculateinitialdrag)/(self.total)
 			print "self.streamlineintegration " ,  self.streamlineintegration , "in % = ", 100.0*(self.streamlineintegration)/(self.total)
@@ -336,38 +285,14 @@ class PFEM2Solver:
 			print "self.prereseed " ,  self.prereseed , "in % = ", 100.0*(self.prereseed)/(self.total)
 			print "TOTAL ----- " ,  self.total
 			print "THIS TIME STEP = " , t13-t1 
-		else:
-			print "----------TIMES----------"
-			print "self.calculateinitialdrag  " , self.calculateinitialdrag , "in % = ",  100.0*(self.calculateinitialdrag)/(self.total)
-			print "self.streamlineintegration " ,  self.streamlineintegration , "in % = ", 100.0*(self.streamlineintegration)/(self.total)
-			print "self.accelerateparticles " ,  self.accelerateparticles , "in % = ", 100.0*(self.accelerateparticles)/(self.total)
-			pressure_iterations=self.navierstokes-self.implicitviscosity-self.thermal
-			print "pressureiterations " , pressure_iterations , "in % = ", (100.0*pressure_iterations)/(self.total)
-			print "self.implicitviscosity " ,  self.implicitviscosity , "in % = ", 100.0*(self.implicitviscosity)/(self.total)
-			print "self.allimplicitsystems " ,  self.navierstokes , "in % = ", 100.0*(self.navierstokes)/(self.total)
-			print "self.thermal " ,  self.thermal , "in % = ", 100.0*(self.thermal)/(self.total)
-			print "self.nodaltasks ", self.nodaltasks , (self.nodaltasks)/(self.total)
-			print "self.lagrangiantoeulerian " ,  self.lagrangiantoeulerian , "in % = ", 100.0*(self.lagrangiantoeulerian)/(self.total)
-			print "self.reseed " ,  self.reseed , "in % = ", 100.0*(self.reseed)/(self.total)
-			print "self.prereseed " ,  self.prereseed , "in % = ", 100.0*(self.prereseed)/(self.total)
-			print "TOTAL ----- " ,  self.total
-			print "THIS TIME STEP = " , t13-t1 
-		
+
 
     #######################################################################   
-    def CalculatePressureIteration(self, iteration_number):
-		self.model_part.ProcessInfo.SetValue(FRACTIONAL_STEP, 2)
-		self.model_part.ProcessInfo.SetValue(NL_ITERATION_NUMBER, iteration_number)
-		
-		(self.pressure_solver).Solve() #implicit resolution of the pressure system. All the other tasks are explicit
-		#if iteration_number==1:
-		#	for node in self.model_part.Nodes:
-		#		press= node.GetSolutionStepValue(PRESSURE)+node.GetSolutionStepValue(PRESSUREAUX)
-		#		node.SetSolutionStepValue(PRESSURE,press)
-		#setting the Fract step number to 3 to calculate the pressure gradient effect on the velocity
-		self.model_part.ProcessInfo.SetValue(FRACTIONAL_STEP, 3)
+    def CalculatePressureProjection(self):
+		self.model_part.ProcessInfo.SetValue(FRACTIONAL_STEP, 3) #explicit contribution by viscosity is defined as fract step = 0
 		(self.ExplicitStrategy).InitializeSolutionStep(self.model_part.ProcessInfo);
 		(self.ExplicitStrategy).AssembleLoop(self.model_part.ProcessInfo);
+		self.model_part.ProcessInfo.SetValue(FRACTIONAL_STEP, 10)
 		(self.ExplicitStrategy).FinalizeSolutionStep(self.model_part.ProcessInfo);
 
 
