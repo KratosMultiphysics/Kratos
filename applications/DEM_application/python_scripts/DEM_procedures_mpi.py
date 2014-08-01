5012c7cbdc513c029efca8cbfc5a30c58315d2db
Carlos Roig <roigcarlo@gmail.com>
2014-08-01 07:21:50 +0000

    Starting to merge DEM scripts, some utilities added

--- applications/DEM_application/python_scripts/DEM_procedures_mpi.py
+++ applications/DEM_application/python_scripts/DEM_procedures_mpi.py
@@ -1,16 +1,17 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
 from KratosMultiphysics import *
 from KratosMultiphysics.DEMApplication import *
 from KratosMultiphysics.MetisApplication import *
-
 from KratosMultiphysics.mpi import *
 
+def KRATOS_MPI_WORLD_BARRIER():
+    mpi.world.barrier()
 
 def AddMpiVariables(model_part):
 
     model_part.AddNodalSolutionStepVariable(PARTITION_INDEX)
     model_part.AddNodalSolutionStepVariable(PARTITION_MASK)
-
 
 def PerformInitialPartition(model_part, model_part_io_solid, input_file_name):
 
@@ -37,3 +38,28 @@ def PerformInitialPartition(model_part, model_part_io_solid, input_file_name):
     model_part_io_solid = ModelPartIO(my_input_filename, True)
 
     return [model_part_io_solid, model_part]
+
+def CreateDirectories(main_path,problem_name):
+
+    KRATOS_MPI_WORLD_BARRIER()
+
+    root             = main_path + '/' + problem_name
+
+    post_path        = root + '_Post_Files'
+    list_path        = root + '_Post_Lists'
+    data_and_results = root + '_Results_and_Data'
+    graphs_path      = root + '_Graphs'
+    MPI_results      = root + '_MPI_results'
+
+    if mpi.rank == 0:
+        for directory in [post_path, list_path, data_and_results, graphs_path, MPI_results]:
+            if not os.path.isdir(directory):
+                os.makedirs(str(directory))
+
+    KRATOS_MPI_WORLD_BARRIER()
+
+    return [post_path,list_path,data_and_results,graphs_path,MPI_results]
+
+def KRATOSprint(message):
+    if (mpi.rank == 0):
+        print(message)    
