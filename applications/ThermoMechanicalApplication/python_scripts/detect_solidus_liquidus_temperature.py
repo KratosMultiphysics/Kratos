428d7a2e32b03627e9a924549c7e4439ba0fcc3a
jrubio <jrubio@cimne.upc.edu>
2014-10-01 10:06:30 +0000

    Script for detecting the solidus and liquidus temperature from the given tables

--- applications/ThermoMechanicalApplication/python_scripts/detect_solidus_liquidus_temperature.py
+++ applications/ThermoMechanicalApplication/python_scripts/detect_solidus_liquidus_temperature.py
@@ -1,24 +1,27 @@
 from KratosMultiphysics import *
 from KratosMultiphysics.ThermoMechanicalApplication import *
 def DetectSolidusLiquidusTemperature(model_part):
-	lowlimit=0.0
-	uplimit=10000.0
-	dtemp=0.01
-	solidustemp=lowlimit
-	liquidustemp=uplimit
-	temp=lowlimit
-	while(temp<liquidustemp):
-		SF=model_part.GetTable(3).GetValue(temp)
-		if SF<=0.0:
-			liquidustemp=temp
-		if SF>=1.0:
-			solidustemp=temp
-		temp+=dtemp
-		print(temp)
-		print(SF)
-	print("SOLIDUS TEMP FOUND :=" +str(solidustemp))
-	print("LIQUIDUS TEMP FOUND :=" +str(liquidustemp))
-	model_part.ProcessInfo.SetValue(FLUID_TEMPERATURE, liquidustemp)#ProjectParameters.FLUID_TEMPERATURE)
-	model_part.ProcessInfo.SetValue(SOLID_TEMPERATURE, solidustemp)#ProjectParameters.SOLID_TEMPERATURE)
+    """ 
+    Finds both the solidus and liquidus temperature that are included in the
+    Solid Fraction table (Table 3) of the ModelPart. It scans from 0 to 10,000 
+    in 0.01 intervals.
+    @param model_part
+    """
+    lowlimit=0.0
+    uplimit=10000.0
+    dtemp=0.01
+    solidustemp=lowlimit
+    liquidustemp=uplimit
+    temp=lowlimit
+    table=model_part.GetTable(3)
+    while(temp<liquidustemp):
+        SF=table.GetValue(temp)
+        if SF<=0.0:
+            liquidustemp=temp
+        if SF>=1.0:
+            solidustemp=temp
+            temp+=dtemp
+    model_part.ProcessInfo.SetValue(FLUID_TEMPERATURE, liquidustemp)#ProjectParameters.FLUID_TEMPERATURE)
+    model_part.ProcessInfo.SetValue(SOLID_TEMPERATURE, solidustemp)#ProjectParameters.SOLID_TEMPERATURE)
 
 
