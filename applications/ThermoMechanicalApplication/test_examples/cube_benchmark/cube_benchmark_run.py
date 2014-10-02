18b8a28b01fa67bbda8468647d6d576b0ed4f7e4
jrubio <jrubio@cimne.upc.edu>
2014-10-02 15:24:15 +0000

    Convection-Diffusion tests. Path problems fixed

--- applications/ThermoMechanicalApplication/test_examples/cube_benchmark/cube_benchmark_run.py
+++ applications/ThermoMechanicalApplication/test_examples/cube_benchmark/cube_benchmark_run.py
@@ -1,10 +1,17 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import sys
+import os
 #Declaring Problem name and path
-kratos_path = "\\KratosMultiphysics"
+#Declaring Problem name and path
+current_directory=os.getcwd()
+problem_path = current_directory
+os.chdir("..\\..\\..\\..\\..")
+kratos_path = os.getcwd()
+os.chdir(current_directory)
+#os.chdir(current_directory)
 
 # including kratos path
-kratos_benchmarking_path=kratos_path+'\\kratos\\benchmarking'
+kratos_benchmarking_path=os.path.join(kratos_path,'kratos\\benchmarking')
 sys.path.append(kratos_benchmarking_path)
 
 import benchmarking
