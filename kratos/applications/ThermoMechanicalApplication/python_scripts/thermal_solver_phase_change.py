5ebb11c03e02293eec99ebc740a62614baeb8381
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-10-03 14:28:38 +0000

    added a new solver for the change of phase

--- kratos/applications/ThermoMechanicalApplication/python_scripts/thermal_solver_phase_change.py
+++ kratos/applications/ThermoMechanicalApplication/python_scripts/thermal_solver_phase_change.py
@@ -3,6 +3,9 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 from KratosMultiphysics import *
 from KratosMultiphysics.ThermoMechanicalApplication import *
 from KratosMultiphysics.IncompressibleFluidApplication import *
+from KratosMultiphysics.StructuralApplication import *
+from KratosMultiphysics.ExternalSolversApplication import *
+
 
 # Check that KratosMultiphysics was imported in the main script
 CheckForPreviousImport()
@@ -25,7 +28,10 @@ def AddVariables(model_part, settings):
     model_part.AddNodalSolutionStepVariable(settings.GetTransferCoefficientVariable());
     # model_part.AddNodalSolutionStepVariable(HTC);
     model_part.AddNodalSolutionStepVariable(SOLID_FRACTION);
+    model_part.AddNodalSolutionStepVariable(DISTANCE);    
     model_part.AddNodalSolutionStepVariable(SOLID_FRACTION_RATE);
+    model_part.AddNodalSolutionStepVariable(NODAL_PAUX);   
+    model_part.AddNodalSolutionStepVariable(IS_SLIP);        
 
     print("variables for the THERMAL_SOLVER added correctly")
 
@@ -54,21 +60,27 @@ class Solver:
 # self.linear_solver =SuperLUSolver()
 # self.linear_solver = MKLPardisoSolver()
 
-        pPrecond = DiagonalPreconditioner()
+        #pPrecond = DiagonalPreconditioner()
 # pPrecond = ILU0Preconditioner()
         # self.linear_solver =  BICGSTABSolver(1e-6, 5000,pPrecond)
-        self.linear_solver = BICGSTABSolver(1e-6, 5000)
+        #self.linear_solver = BICGSTABSolver(1e-6, 5000)
+        
+        gmres_size = 50
+        tol = 1e-6
+        verbosity = 0
+        self.linear_solver = AMGCLSolver(AMGCLSmoother.ILU0, AMGCLIterativeSolverType.GMRES, tol, 200, verbosity, gmres_size)        
 
         self.dynamic_tau = 1.0
 
         self.echo_level = 0
         self.CalculateReactionFlag = False
-        self.ReformDofSetAtEachStep = True
+        self.ReformDofSetAtEachStep = False
         self.CalculateNormDxFlag = True
         self.MoveMeshFlag = False
 
         self.conv_criteria = ResidualCriteria(1e-5, 1e-6)
-        self.max_iter = 5
+        #self.conv_criteria = IncrementalDisplacementCriteria(1e-3, 1e-16)
+        self.max_iter = 15
 
         self.neigh_finder = FindNodalNeighboursProcess(self.model_part, 9, 18)
         if (self.domain_size == 2):
@@ -81,6 +93,20 @@ class Solver:
 
         # calculate normals
         self.normal_tools = BodyNormalCalculationUtils()
+        self.normal_util = NormalCalculationUtils()
+        
+        
+         # linesearch solver
+        # definition of parameters
+        self.MaxLineSearchIterations = 20
+        self.MaxNewtonRapshonIterations = 5
+        self.tolls = 0.8           # energy tolerance factor on LineSearch (0.8 is ok)
+        self.amp = 1.618         # maximum amplification factor
+        self.etmxa = 3.0           # maximum allowed step length
+        self.etmna = 0.1           # minimum allowed step length
+        self.toler = 1.0E-9
+        self.norm = 1.0E-6
+        self.ApplyLineSearches = False       
 
     #
     def Initialize(self):
@@ -101,6 +127,12 @@ class Solver:
         # self.solver = ResidualBasedLinearStrategy(self.model_part,self.time_scheme,self.linear_solver,self.CalculateReactionFlag, self.ReformDofSetAtEachStep,self.CalculateNormDxFlag,self.MoveMeshFlag)
         self.solver = ResidualBasedNewtonRaphsonStrategy(self.model_part, self.time_scheme, self.linear_solver, self.conv_criteria, self.max_iter, self.CalculateReactionFlag, self.ReformDofSetAtEachStep, self.MoveMeshFlag)
 
+        #self.solver = ResidualBasedNewtonRaphsonLineSearchesStrategy(self.model_part, self.time_scheme, self.linear_solver, self.conv_criteria,
+                                                                     #self.MaxNewtonRapshonIterations, self.MaxLineSearchIterations, self.tolls, self.amp, self.etmxa, self.etmna,
+                                                                     #self.CalculateReactionFlag,
+                                                                     #self.ReformDofSetAtEachStep,
+                                                                     #self.MoveMeshFlag,
+                                                                    #self.ApplyLineSearches)
         (self.solver).SetEchoLevel(self.echo_level)
         #(self.solver).SetBuilderAndSolver(ResidualBasedEliminationBuilderAndSolverDeactivation(self.linear_solver))
 
@@ -119,7 +151,10 @@ class Solver:
 
         # ActivationDeactivationConditionsProcess(self.model_part, self.Nmax, a ).Execute()
 
-        self.normal_tools.CalculateBodyNormals(self.model_part, 2);
+        self.normal_tools.CalculateBodyNormals(self.model_part, 3);
+        #(FindConditionsNeighboursProcess(self.model_part, 3, 20)).ClearNeighbours()
+        #(FindConditionsNeighboursProcess(self.model_part, 3, 20)).Execute()        
+        #self.normal_util.CalculateOnSimplex(self.model_part,self.domain_size,IS_STRUCTURE, 0, 35.0)         
 
 # print "Initialization monolithic solver finished"
 
