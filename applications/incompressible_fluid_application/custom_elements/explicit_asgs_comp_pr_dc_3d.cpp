9d22531c30cd4aa242bf3cffce241570740279c0
rrossi <rrossi@cimne.upc.edu>
2014-10-05 12:37:12 +0000

    changing abs to fabs (error correction)

--- applications/incompressible_fluid_application/custom_elements/explicit_asgs_comp_pr_dc_3d.cpp
+++ applications/incompressible_fluid_application/custom_elements/explicit_asgs_comp_pr_dc_3d.cpp
@@ -505,7 +505,7 @@ void ExplicitASGSCOMPPRDC3D::CalculateArtifitialViscosity(double& Vel_art_visc,d
     if( div_vel < 0.0)
     {
         CalculateCharectristicLength(H,DN_DX,norm_grad_p);
-        Vel_art_visc = 1.4* abs(div_vel) * pow(H,2);
+        Vel_art_visc = 1.4* fabs(div_vel) * pow(H,2);
 
         Pr_art_visc = 1.0*sqrt(norm_grad_p/density) * pow(H,1.5);
     }
