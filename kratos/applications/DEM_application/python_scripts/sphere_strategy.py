bd0987e205f9ad5eb5b33e9f32295d79e369881b
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-10-27 18:51:13 +0000

    Clusters implementations.

--- kratos/applications/DEM_application/python_scripts/sphere_strategy.py
+++ kratos/applications/DEM_application/python_scripts/sphere_strategy.py
@@ -94,7 +94,7 @@ def AddClusterVariables(model_part, Param):
 
 class ExplicitStrategy:
 
-    def __init__(self, model_part, fem_model_part, creator_destructor, Param):
+    def __init__(self, model_part, fem_model_part, cluster_model_part, creator_destructor, Param):
 
         # Initialization of member variables
 
@@ -135,6 +135,7 @@ class ExplicitStrategy:
         # MODEL
         self.model_part = model_part
         self.fem_model_part = fem_model_part
+        self.cluster_model_part = cluster_model_part
 
         # BOUNDING_BOX
         self.enlargement_factor = Param.BoundingBoxEnlargementFactor
@@ -274,7 +275,7 @@ class ExplicitStrategy:
         
         cluster_model_part = ModelPart("Dummy")
         
-        self.solver = ExplicitSolverStrategy(self.model_part, self.fem_model_part, cluster_model_part,self.max_delta_time, self.n_step_search, self.safety_factor, self.move_mesh_flag,
+        self.solver = ExplicitSolverStrategy(self.model_part, self.fem_model_part, self.cluster_model_part,self.max_delta_time, self.n_step_search, self.safety_factor, self.move_mesh_flag,
                                              self.delta_option, self.search_tolerance, self.coordination_number, self.creator_destructor, self.time_integration_scheme, self.search_strategy)
 
         self.solver.Initialize()  # Calls the solver Initialize function (initializes all elements and performs other necessary tasks before iterating) (C++)
