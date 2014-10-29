89a500508b701c1b1d8ba8d96234f0e68b62f00d
jrubio <jrubio@4358b7d9-91ec-4505-bf62-c3060f61107a>
2014-10-29 18:15:47 +0000

    added some variables

--- kratos/kratos/includes/kratos_version.h
+++ kratos/kratos/includes/kratos_version.h
@@ -3,6 +3,6 @@
 #define KRATOS_MINOR_VERSION  3
 
 //svn version at configure time
-#define KRATOS_SVN_REVISION  10841 
+#define KRATOS_SVN_REVISION  10859 
 
-#define KRATOS_VERSION  "3.3.10841"
+#define KRATOS_VERSION  "3.3.10859"
