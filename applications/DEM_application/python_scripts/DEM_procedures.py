f7aa608a291648cad0405532ebe8f06e8174d3af
msantasusana <msantasusana@cimne.upc.edu>
2014-11-03 14:19:05 +0000

    If Bounding Box Option is checked outside int the main script

--- applications/DEM_application/python_scripts/DEM_procedures.py
+++ applications/DEM_application/python_scripts/DEM_procedures.py
@@ -434,9 +434,8 @@ class Procedures(object):
                
         creator_destructor.SetLowNode(b_box_low)
         creator_destructor.SetHighNode(b_box_high)
-
-        if (self.bounding_box_OPTION):
-            creator_destructor.CalculateSurroundingBoundingBox(balls_model_part, clusters_model_part, rigid_faces_model_part, self.bounding_box_enlargement_factor, self.automatic_bounding_box_OPTION)
+        
+        creator_destructor.CalculateSurroundingBoundingBox(balls_model_part, clusters_model_part, rigid_faces_model_part, self.bounding_box_enlargement_factor, self.automatic_bounding_box_OPTION)
             
             
     def PreProcessModel(self, DEM_parameters):
