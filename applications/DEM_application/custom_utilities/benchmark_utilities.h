2820d49d83039b28892d9f47a3106200bbff0cc2
gcasas <gcasas@cimne.upc.edu>
2014-11-11 11:02:33 +0000

    solving the error due to a misplaced typename declaration

--- applications/DEM_application/custom_utilities/benchmark_utilities.h
+++ applications/DEM_application/custom_utilities/benchmark_utilities.h
@@ -13,8 +13,8 @@ public:
 
 typedef ModelPart::ElementsContainerType::iterator  ElementIterator;
 typedef ModelPart::NodesContainerType::iterator     NodeIterator;
-typedef PointerVectorSet<Properties, IndexedObject>               PropertiesContainerType;
-typedef typename PropertiesContainerType::iterator                PropertiesIterator;
+typedef PointerVectorSet<Properties, IndexedObject> PropertiesContainerType;
+typedef PropertiesContainerType::iterator           PropertiesIterator;
 
 KRATOS_CLASS_POINTER_DEFINITION(BenchmarkUtils);
 
@@ -29,10 +29,7 @@ virtual ~BenchmarkUtils(){}
 
 void ComputeHydrodynamicForces(ModelPart& r_model_part)
 {
-    mFastProperties.clear();
-    mFastProperties.resize(r_model_part.NumberOfProperties());
-    int properties_counter = 0;
-    AddPropertiesProxiesFromModelPartProperties(r_model_part, properties_counter);
+    CreatePropertiesProxies(mFastProperties, r_model_part);
 
     for (ModelPart::ElementIterator i_elem = r_model_part.ElementsBegin(); i_elem != r_model_part.ElementsEnd(); ++i_elem){
         Kratos::SphericParticle& particle = static_cast<Kratos::SphericParticle&>(*i_elem);
@@ -54,42 +51,7 @@ void ComputeHydrodynamicForces(ModelPart& r_model_part)
 private:
 
 std::vector<PropertiesProxy> mFastProperties;
-
-void AddPropertiesProxiesFromModelPartProperties(ModelPart& rModelPart, int& properties_counter){
-
-    PropertiesProxy aux_props;
-
-    for (PropertiesIterator props_it = rModelPart.GetMesh(0).PropertiesBegin(); props_it!= rModelPart.GetMesh(0).PropertiesEnd();   props_it++ ) {
-
-        aux_props.SetId( props_it->GetId() );
-
-        double* aux_pointer = &( props_it->GetValue(YOUNG_MODULUS) );
-        aux_props.SetYoungFromProperties( aux_pointer );
-
-        aux_pointer = &( props_it->GetValue(POISSON_RATIO) );
-        aux_props.SetPoissonFromProperties(aux_pointer);
-
-        aux_pointer = &( props_it->GetValue(ROLLING_FRICTION) );
-        aux_props.SetRollingFrictionFromProperties(aux_pointer);
-
-        aux_pointer = &( props_it->GetValue(PARTICLE_FRICTION) );
-        aux_props.SetTgOfFrictionAngleFromProperties(aux_pointer);
-
-        aux_pointer = &( props_it->GetValue(LN_OF_RESTITUTION_COEFF) );
-        aux_props.SetLnOfRestitCoeffFromProperties(aux_pointer);
-
-        aux_pointer = &( props_it->GetValue(PARTICLE_DENSITY) );
-        aux_props.SetDensityFromProperties(aux_pointer);
-
-        int* int_aux_pointer = &( props_it->GetValue(PARTICLE_MATERIAL) );
-        aux_props.SetParticleMaterialFromProperties(int_aux_pointer);
-        mFastProperties[properties_counter].SetParticleMaterialFromProperties(int_aux_pointer);
-
-        properties_counter++;
-
-    }
-
-}
 };
+
 }
 #endif // KRATOS_BECHMARK_UTILITIES_H
