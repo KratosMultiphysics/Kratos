be40ad0ad2a6d3ae81e36ba0213f373556328090
Salva Latorre <latorre@cimne.upc.edu>
2015-01-13 11:40:14 +0000

    Making the pill cluster element lighter.

--- applications/DEM_application/custom_elements/pillcluster3D.cpp
+++ applications/DEM_application/custom_elements/pillcluster3D.cpp
@@ -54,29 +54,77 @@ namespace Kratos {
     
     void PillCluster3D::CustomInitialize() {
 
-        int number_of_spheres_in_first_sector = 6;
-        int number_of_passes = 5;
-        int number_of_spheres_per_pass = 4 * number_of_spheres_in_first_sector - 4;
-        mListOfRadii.resize(number_of_spheres_per_pass * number_of_passes);
-        mListOfCoordinates.resize(number_of_spheres_per_pass * number_of_passes);
-        mListOfSphericParticles.resize(number_of_spheres_per_pass * number_of_passes);
+//        int number_of_spheres_in_first_sector = 6;
+//        int number_of_passes = 5;
+//        int number_of_spheres_per_pass = 4 * number_of_spheres_in_first_sector - 4;
+//        mListOfRadii.resize(number_of_spheres_per_pass * number_of_passes);
+//        mListOfCoordinates.resize(number_of_spheres_per_pass * number_of_passes);
+//        mListOfSphericParticles.resize(number_of_spheres_per_pass * number_of_passes);
+        
+        int total_number_of_particles = 20 + 16 + 12 + 6 + 1;
+        
+        mListOfRadii.resize(total_number_of_particles);
+        mListOfCoordinates.resize(total_number_of_particles);
+        mListOfSphericParticles.resize(total_number_of_particles);
         
         double cl = GetGeometry()[0].FastGetSolutionStepValue(CHARACTERISTIC_LENGTH);
         
-        double sinusoidal_factor = 0.5 * KRATOS_M_PI / (number_of_spheres_in_first_sector - 1);
-        
-        for (int j = 0; j < number_of_passes; j++) {
-        
-            for (int i = 0; i < number_of_spheres_per_pass; i++) {
-            
-                mListOfRadii[i + number_of_spheres_per_pass * j]= 0.1 * cl;
-            
-                mListOfCoordinates[i + number_of_spheres_per_pass * j][0] = (0.05 + 0.1 * j) * cl * cos(sinusoidal_factor * i); ////APPROXIMATE COORDINATES, CALCULATE MORE EXACTLY
-                mListOfCoordinates[i + number_of_spheres_per_pass * j][1] = (0.05 + 0.1 * j) * cl * sin(sinusoidal_factor * i);
-                mListOfCoordinates[i + number_of_spheres_per_pass * j][2] = 0.0;
-            
-            }
+        double sinusoidal_factor = 0.5 * KRATOS_M_PI / (20.0/4.0 - 1);
+             
+        for (int i = 0; i < 20; i++) {           
+            mListOfCoordinates[i][0] = 0.45 * cl * cos(sinusoidal_factor * i); ////APPROXIMATE COORDINATES, CALCULATE MORE EXACTLY
+            mListOfCoordinates[i][1] = 0.45 * cl * sin(sinusoidal_factor * i);
+            mListOfCoordinates[i][2] = 0.0;
         }
+        
+        sinusoidal_factor = 0.5 * KRATOS_M_PI / (16.0/4.0 - 1);
+        
+        for (int i = 0; i < 16; i++) {           
+            mListOfCoordinates[20 + i][0] = 0.35 * cl * cos(sinusoidal_factor * i);
+            mListOfCoordinates[20 + i][1] = 0.35 * cl * sin(sinusoidal_factor * i);
+            mListOfCoordinates[20 + i][2] = 0.0;
+        }
+        
+        sinusoidal_factor = 0.5 * KRATOS_M_PI / (12.0/4.0 - 1);
+        
+        for (int i = 0; i < 12; i++) {           
+            mListOfCoordinates[36 + i][0] = 0.25 * cl * cos(sinusoidal_factor * i); 
+            mListOfCoordinates[36 + i][1] = 0.25 * cl * sin(sinusoidal_factor * i);
+            mListOfCoordinates[36 + i][2] = 0.0;
+        }
+        
+        sinusoidal_factor = 0.333333333333333333 * KRATOS_M_PI;
+        
+        for (int i = 0; i <  6; i++) {           
+            mListOfCoordinates[48 + i][0] = 0.15 * cl * cos(sinusoidal_factor * i); 
+            mListOfCoordinates[48 + i][1] = 0.15 * cl * sin(sinusoidal_factor * i);
+            mListOfCoordinates[48 + i][2] = 0.0;
+        }
+        
+        for (int i = 0; i <  1; i++) {           
+            mListOfCoordinates[54 + i][0] = 0.0; 
+            mListOfCoordinates[54 + i][1] = 0.0;
+            mListOfCoordinates[54 + i][2] = 0.0;
+        }
+        
+        for (int i = 0; i < 55; i++) {           
+            mListOfRadii[i]= 0.1 * cl;
+        }        
+                
+//        double sinusoidal_factor = 0.5 * KRATOS_M_PI / (number_of_spheres_in_first_sector - 1);
+//        
+//        for (int j = 0; j < number_of_passes; j++) {
+//        
+//            for (int i = 0; i < number_of_spheres_per_pass; i++) {
+//            
+//                mListOfRadii[i + number_of_spheres_per_pass * j]= 0.1 * cl;
+//            
+//                mListOfCoordinates[i + number_of_spheres_per_pass * j][0] = (0.05 + 0.1 * j) * cl * cos(sinusoidal_factor * i); ////APPROXIMATE COORDINATES, CALCULATE MORE EXACTLY
+//                mListOfCoordinates[i + number_of_spheres_per_pass * j][1] = (0.05 + 0.1 * j) * cl * sin(sinusoidal_factor * i);
+//                mListOfCoordinates[i + number_of_spheres_per_pass * j][2] = 0.0;
+//            
+//            }
+//        }
         
         double particle_density = this->SlowGetDensity(); /////////////////////////////USE FAST
          
@@ -88,7 +136,7 @@ namespace Kratos {
         
         GetGeometry()[0].FastGetSolutionStepValue(PRINCIPAL_MOMENTS_OF_INERTIA)[0] = 0.25 * cluster_mass * 0.5 * 0.5 * cl * cl;
         GetGeometry()[0].FastGetSolutionStepValue(PRINCIPAL_MOMENTS_OF_INERTIA)[1] = 0.25 * cluster_mass * 0.5 * 0.5 * cl * cl;
-        GetGeometry()[0].FastGetSolutionStepValue(PRINCIPAL_MOMENTS_OF_INERTIA)[2] =  0.5 * cluster_mass * 0.5 * 0.5 * cl * cl;
+        GetGeometry()[0].FastGetSolutionStepValue(PRINCIPAL_MOMENTS_OF_INERTIA)[2] = 0.50 * cluster_mass * 0.5 * 0.5 * cl * cl;
          
         array_1d<double, 3> base_principal_moments_of_inertia = GetGeometry()[0].FastGetSolutionStepValue(PRINCIPAL_MOMENTS_OF_INERTIA);     
   
