4d762da0e0d63c919c88d41426d61bdd9dde8467
pbecker <pablobecker@gmail.com>
2015-01-14 16:17:45 +0000

    clean up

--- applications/pfem_2_application/test_examples/rayleigh/example_monolithic.py
+++ applications/pfem_2_application/test_examples/rayleigh/example_monolithic.py
@@ -43,36 +43,36 @@ model_part.ProcessInfo.SetValue(GRAVITY_Y, -10.0);
 pi=3.14159
 
 for node in model_part.Nodes:
-	art=1+1
-	#(p(0),p(1)) -> posicion del nodo
-	g = 10.0;
-	delta0 = 0.1;
-	L = 1.0;
+        art=1+1
+        #(p(0),p(1)) -> posicion del nodo
+        g = 10.0;
+        delta0 = 0.1;
+        L = 1.0;
 
-	delta = -delta0*(cos(2*pi*(node.X-0.5)/L))+2.0;
-	distance_function=delta
-	distance_function=delta-node.Y
-	#if (node.Y>=delta):
-	#	distance_function=-1.0;
-	#else:
-	#	distance_function =1.0;
+        delta = -delta0*(cos(2*pi*(node.X-0.5)/L))+2.0;
+        distance_function=delta
+        distance_function=delta-node.Y
+        #if (node.Y>=delta):
+        #        distance_function=-1.0;
+        #else:
+        #        distance_function =1.0;
 
-	#distance_function=-1.0
-	node.SetSolutionStepValue(DISTANCE,0,distance_function)
-	if node.X>0.999 and node.Y>3.999:
-		node.Fix(PRESSURE)
-	
+        #distance_function=-1.0
+        node.SetSolutionStepValue(DISTANCE,0,distance_function)
+        if node.X>0.999 and node.Y>3.999:
+                node.Fix(PRESSURE)
+        
 
-	if node.X>0.999 or node.X<0.0001:
-		node.Fix(VELOCITY_X)
-		node.Fix(FRACT_VEL_X)
-	if node.Y>3.999 or node.Y<0.0001:
-		node.Fix(FRACT_VEL_Y)
-		node.Fix(VELOCITY_Y)
+        if node.X>0.999 or node.X<0.0001:
+                node.Fix(VELOCITY_X)
+                node.Fix(FRACT_VEL_X)
+        if node.Y>3.999 or node.Y<0.0001:
+                node.Fix(FRACT_VEL_Y)
+                node.Fix(VELOCITY_Y)
 
-	
+        
 
-		
+                
 
 
 
@@ -87,7 +87,7 @@ pfem_2_solver.AddDofs(model_part)
 solver = pfem_2_solver.PFEM2Solver(model_part,domain_size)
 solver.time_order = 1
 solver.echo_level = 3
-solver.Initialize()	
+solver.Initialize()        
   
 mesh_name = 0.0
 gid_io.InitializeMesh( mesh_name );
@@ -115,35 +115,35 @@ import time as timer
 t1 = timer.time()
 for step in range(1,nsteps):
     out=out+1
-    print "new step"
+    print("new step")
     time = Dt*step
     
     model_part.CloneTimeStep(time)
 
     if step>1:
-    	solver.Solve()
+            solver.Solve()
 
     #if step==500:
-	#model_part.ProcessInfo.SetValue(GRAVITY_Y, 0.0);
-	#model_part.ProcessInfo.SetValue(VISCOSITY, 0.0);
+        #model_part.ProcessInfo.SetValue(GRAVITY_Y, 0.0);
+        #model_part.ProcessInfo.SetValue(VISCOSITY, 0.0);
 
     if step==0:
-	for node in model_part.Nodes:
-		node.SetSolutionStepValue(VELOCITY_X,0,0.0) 
-		node.SetSolutionStepValue(VELOCITY_Y,0,0.0)
+        for node in model_part.Nodes:
+                node.SetSolutionStepValue(VELOCITY_X,0,0.0) 
+                node.SetSolutionStepValue(VELOCITY_Y,0,0.0)
     if out==out_step:
-   	  out=0
-	  print "printing a step"		
-   	  
-   	  gid_io.WriteNodalResults(VELOCITY,model_part.Nodes,time,0)
-	  gid_io.WriteNodalResults(PRESSURE,model_part.Nodes,time,0)
-	  gid_io.WriteNodalResults(DISTANCE,model_part.Nodes,time,0)
-	  gid_io.Flush()
+          out=0
+          print("printing a step")
+             
+          gid_io.WriteNodalResults(VELOCITY,model_part.Nodes,time,0)
+          gid_io.WriteNodalResults(PRESSURE,model_part.Nodes,time,0)
+          gid_io.WriteNodalResults(DISTANCE,model_part.Nodes,time,0)
+          gid_io.Flush()
 
     
 t2=timer.time()
 total_time=t2-t1
 
-print "total_time", total_time
+print ("total_time", total_time)
 
 gid_io.FinalizeResults()
