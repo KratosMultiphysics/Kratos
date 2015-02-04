d4decf5c5d04453884c7a34f6ea359420b7bd4c0
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-02-04 15:56:57 +0000

    Checking that the inlet velocities are non-zero.

--- kratos/applications/DEM_application/custom_utilities/inlet.cpp
+++ kratos/applications/DEM_application/custom_utilities/inlet.cpp
@@ -71,6 +71,12 @@ namespace Kratos {
             int mesh_size=mesh_it->NumberOfNodes();
             ModelPart::NodesContainerType::ContainerType all_nodes = mesh_it->NodesArray();
             
+            if (mInletModelPart.GetProperties(mesh_number)[VELOCITY][0] == 0 &&
+                mInletModelPart.GetProperties(mesh_number)[VELOCITY][1] == 0 &&
+                mInletModelPart.GetProperties(mesh_number)[VELOCITY][2] == 0) {
+                KRATOS_ERROR(std::runtime_error, "There is at least one inlet velocity set to", 0);
+            }
+            
             int general_properties_id = mInletModelPart.GetProperties(mesh_number).Id();  
             PropertiesProxy* p_fast_properties = NULL;
             
