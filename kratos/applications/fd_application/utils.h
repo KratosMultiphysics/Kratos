975d66731bd81b5f201b81676f2f410c1510bc4c
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-03-26 09:46:04 +0000

    Separated IO and some code cleanup

--- kratos/applications/fd_application/utils.h
+++ kratos/applications/fd_application/utils.h
@@ -7,7 +7,7 @@ void AllocateGrid(T ** grid,
 }
 
 template <typename T>
-void AllocateGrid(T ** grid,
+void AllocateGrid(T * grid,
     const uint &X, const uint &Y, const uint &Z, const uint &align) {
 
   uint size = (X+BW) * (Y+BW) * (Z+BW);
@@ -18,53 +18,7 @@ void AllocateGrid(T ** grid,
 }
 
 template <typename T>
-void ReleaseGrid(T ** grid) {
+void ReleaseGrid(T * grid) {
 
   free(*grid);
-}
-
-template <typename T>
-void WriteGrid(T * grid, 
-    const uint &X, const uint &Y, const uint &Z,
-    const char * fileName) {
-
-  std::ofstream outputFile(fileName,std::ofstream::app);
-
-  outputFile << std::fixed;
-  outputFile << std::setprecision(2);
-
-  for(uint k = 0; k < Z + BW; k++) {
-    for(uint j = 0; j < Y + BW; j++) {
-      for(uint i = 0; i < X + BW; i++) {
-        outputFile << grid[k*(Y+BW)*(X+BW)+j*(X+BW)+i] << " ";
-      } 
-      outputFile << std::endl;
-    }
-    outputFile << std::endl;
-  }
-  outputFile << "%" << std::endl;
-  
-}
-
-template <typename T>
-void WriteGridWipe(T * grid, 
-    const uint &X, const uint &Y, const uint &Z,
-    const char * fileName) {
-
-  std::ofstream outputFile(fileName);
-
-  outputFile << std::fixed;
-  outputFile << std::setprecision(2);
-
-  for(uint k = 0; k < Z + BW; k++) {
-    for(uint j = 0; j < Y + BW; j++) {
-      for(uint i = 0; i < X + BW; i++) {
-        outputFile << grid[k*(Y+BW)*(X+BW)+j*(X+BW)+i] << " ";
-      } 
-      outputFile << std::endl;
-    }
-    outputFile << std::endl;
-  }
-  outputFile << "%" << std::endl;
-  
 }
