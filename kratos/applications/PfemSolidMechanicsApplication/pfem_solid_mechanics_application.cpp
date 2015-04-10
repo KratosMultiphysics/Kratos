ca180523e63a6baad1b992a5e4a820fcf7e9d144
lmonforte <lmonforte@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-04-10 10:13:07 +0000

    Adding postProcess variables + modifying CMake

--- kratos/applications/PfemSolidMechanicsApplication/pfem_solid_mechanics_application.cpp
+++ kratos/applications/PfemSolidMechanicsApplication/pfem_solid_mechanics_application.cpp
@@ -41,7 +41,7 @@ namespace Kratos
   KRATOS_CREATE_VARIABLE(int, NUMBER_OF_STICK_CONTACTS )
   KRATOS_CREATE_VARIABLE(int, NUMBER_OF_SLIP_CONTACTS )
   
-  KRATOS_CREATE_VARIABLE(double, IMPOSED_WATER_PRESSURE )
+   KRATOS_CREATE_VARIABLE(double, IMPOSED_WATER_PRESSURE )
   //constitutive law	
   KRATOS_CREATE_VARIABLE(double, MEAN_ERROR )
 
@@ -55,10 +55,20 @@ namespace Kratos
   KRATOS_CREATE_VARIABLE(double, SWELLING_SLOPE )
   KRATOS_CREATE_VARIABLE(double, CRITICAL_STATE_LINE )
   KRATOS_CREATE_VARIABLE(double, ALPHA_SHEAR )
+  KRATOS_CREATE_VARIABLE(double, INITIAL_POROSITY )
+  KRATOS_CREATE_VARIABLE(double, COHESION )
+  KRATOS_CREATE_VARIABLE(double, INTERNAL_DILATANCY_ANGLE )
 
   //element
   KRATOS_CREATE_VARIABLE(Vector, DARCY_FLOW )
-  KRATOS_CREATE_VARIABLE(Matrix, TOTAL_CAUCHY_STRESS)
+  KRATOS_CREATE_VARIABLE(Matrix, TOTAL_CAUCHY_STRESS )
+
+  // transfer variables and initial
+  KRATOS_CREATE_VARIABLE(Matrix, ELASTIC_LEFT_CAUCHY_GREEN_TENSOR )
+  KRATOS_CREATE_VARIABLE(Vector, ELASTIC_LEFT_CAUCHY_GREEN_VECTOR )
+
+  KRATOS_CREATE_VARIABLE(Matrix, KIRCHHOFF_STRESS_TENSOR )
+  KRATOS_CREATE_VARIABLE(Vector, ELASTIC_LEFT_CAUCHY_FROM_KIRCHHOFF_STRESS )
 
   //thermal
 
@@ -90,8 +100,33 @@ namespace Kratos
   KRATOS_CREATE_VARIABLE(double, MU_STATIC )
   KRATOS_CREATE_VARIABLE(double, MU_DYNAMIC )
 
+  // contact post process
+  KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS( CONTACT_STRESS )
+  KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS( EFFECTIVE_CONTACT_STRESS )
+  KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS( EFFECTIVE_CONTACT_FORCE )
+  KRATOS_CREATE_VARIABLE(double, CONTACT_ADHESION ) 
+  KRATOS_CREATE_VARIABLE(double, CONTACT_FRICTION_ANGLE )
+
+  // some post process variables + stress invariants
+  KRATOS_CREATE_VARIABLE(double, PRECONSOLIDATION )
+  KRATOS_CREATE_VARIABLE(double, STRESS_INV_P )
+  KRATOS_CREATE_VARIABLE(double, STRESS_INV_J2 )
+  KRATOS_CREATE_VARIABLE(double, STRESS_INV_THETA )
+  KRATOS_CREATE_VARIABLE(double, VOLUMETRIC_PLASTIC )
+  KRATOS_CREATE_VARIABLE(double, INCR_SHEAR_PLASTIC )
+
+  KRATOS_CREATE_VARIABLE(double, M_MODULUS )
+  KRATOS_CREATE_VARIABLE(double, SIMILAR_YOUNG_MODULUS)
+  KRATOS_CREATE_VARIABLE(double, STABILIZATION )
+
   KratosPfemSolidMechanicsApplication::KratosPfemSolidMechanicsApplication():
     mSpatialLagrangianUwPElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
+    mSpatialLagrangianUwPStabElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
+    mSpatialLagrangianUwPFICElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
+    mSpatialLagrangianUwPStabLagElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
+    mSpatialLagrangianUwPSecondElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
+    mAxisymSpatialLagrangianUwPElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
+    mAxisymSpatialLagrangianUwPStabElement2D3N( 0, Element::GeometryType::Pointer( new Triangle2D3 <Node<3> >( Element::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
     mCondition2D( 0, Condition::GeometryType::Pointer( new Line2D2<Node<3> >( Condition::GeometryType::PointsArrayType( 2, Node<3>() ) ) ) ),
     mCondition3D( 0, Condition::GeometryType::Pointer( new Triangle3D3<Node<3> >( Condition::GeometryType::PointsArrayType( 3, Node<3>() ) ) ) ),
     mCompositeCondition2D( 0, Condition::GeometryType::Pointer( new Line2D2<Node<3> >( Condition::GeometryType::PointsArrayType( 2, Node<3>() ) ) ) ),
@@ -119,6 +154,12 @@ namespace Kratos
     
     //Register Elements
     KRATOS_REGISTER_ELEMENT( "SpatialLagrangianUwPElement2D3N", mSpatialLagrangianUwPElement2D3N )
+    KRATOS_REGISTER_ELEMENT( "SpatialLagrangianUwPStabElement2D3N", mSpatialLagrangianUwPStabElement2D3N )
+    KRATOS_REGISTER_ELEMENT( "SpatialLagrangianUwPFICElement2D3N", mSpatialLagrangianUwPFICElement2D3N )
+    KRATOS_REGISTER_ELEMENT( "SpatialLagrangianUwPStabLagElement2D3N", mSpatialLagrangianUwPStabLagElement2D3N )
+    KRATOS_REGISTER_ELEMENT( "SpatialLagrangianUwPSecondElement2D3N", mSpatialLagrangianUwPSecondElement2D3N )
+    KRATOS_REGISTER_ELEMENT( "AxisymSpatialLagrangianUwPElement2D3N", mAxisymSpatialLagrangianUwPElement2D3N )
+    KRATOS_REGISTER_ELEMENT( "AxisymSpatialLagrangianUwPStabElement2D3N", mAxisymSpatialLagrangianUwPStabElement2D3N )
 
     //Register Conditions
 
@@ -148,14 +189,18 @@ namespace Kratos
     Serializer::Register("HenckyJ2PlasticAxisym2DLaw", mHenckyJ2PlasticAxisym2DLaw);
     Serializer::Register("HenckyTrescaPlasticAxisym2DLaw", mHenckyTrescaPlasticAxisym2DLaw);
     Serializer::Register("HenckyTrescaPlasticPlaneStrain2DLaw", mHenckyTrescaPlasticPlaneStrain2DLaw);
+    Serializer::Register("HenckyMohrCoulombPlasticAxisym2DLaw", mHenckyMohrCoulombPlasticAxisym2DLaw);
+    Serializer::Register("HenckyMohrCoulombPlasticPlaneStrain2DLaw", mHenckyMohrCoulombPlasticPlaneStrain2DLaw);
 
     Serializer::Register("HenckyPlasticUPJ2Axisym2DLaw", mHenckyPlasticUPJ2Axisym2DLaw);
+    Serializer::Register("HenckyPlasticUPJ2PlaneStrain2DLaw", mHenckyPlasticUPJ2PlaneStrain2DLaw);
     Serializer::Register("HenckyPlasticUPTrescaAxisym2DLaw", mHenckyPlasticUPTrescaAxisym2DLaw);
     Serializer::Register("HenckyPlasticUPTrescaPlaneStrain2DLaw", mHenckyPlasticUPTrescaPlaneStrain2DLaw);
 
     //Register Flow Rules
     Serializer::Register("TrescaExplicitFlowRule", mTrescaExplicitFlowRule);
     Serializer::Register("J2ExplicitFlowRule", mJ2ExplicitFlowRule);
+    Serializer::Register("MohrCoulombExplicitFlowRule", mMohrCoulombExplicitFlowRule);
     Serializer::Register("CamClayExplicitFlowRule", mCamClayExplicitFlowRule);
     Serializer::Register("LinearCamClayExplicitFlowRule", mLinearCamClayExplicitFlowRule);
     Serializer::Register("BorjaCamClayExplicitFlowRule", mBorjaCamClayExplicitFlowRule);
@@ -163,6 +208,7 @@ namespace Kratos
     //Register Yield Criterion
     Serializer::Register("J2YieldCriterion", mJ2YieldCriterion);
     Serializer::Register("TrescaYieldCriterion", mTrescaYieldCriterion);
+    Serializer::Register("MohrCoulombYieldCriterion", mMohrCoulombYieldCriterion);
     Serializer::Register("CamClayYieldCriterion", mCamClayYieldCriterion);
 
     //Register Hardening Laws
@@ -174,6 +220,8 @@ namespace Kratos
     KRATOS_REGISTER_VARIABLE( NUMBER_OF_ACTIVE_CONTACTS )
     KRATOS_REGISTER_VARIABLE( NUMBER_OF_STICK_CONTACTS )
     KRATOS_REGISTER_VARIABLE( NUMBER_OF_SLIP_CONTACTS )
+
+
     KRATOS_REGISTER_VARIABLE( IMPOSED_WATER_PRESSURE )
     
     //constitutive law	
@@ -189,10 +237,19 @@ namespace Kratos
     KRATOS_REGISTER_VARIABLE( SWELLING_SLOPE )
     KRATOS_REGISTER_VARIABLE( CRITICAL_STATE_LINE )
     KRATOS_REGISTER_VARIABLE( ALPHA_SHEAR )
+    KRATOS_REGISTER_VARIABLE( INITIAL_POROSITY )
+    KRATOS_REGISTER_VARIABLE( COHESION )
+    KRATOS_REGISTER_VARIABLE( INTERNAL_DILATANCY_ANGLE ) 
 
     //element
     KRATOS_REGISTER_VARIABLE( DARCY_FLOW )
-    KRATOS_REGISTER_VARIABLE( TOTAL_CAUCHY_STRESS ) 
+    KRATOS_REGISTER_VARIABLE( TOTAL_CAUCHY_STRESS )
+
+    // transfer and initial
+    KRATOS_REGISTER_VARIABLE( ELASTIC_LEFT_CAUCHY_FROM_KIRCHHOFF_STRESS )
+    KRATOS_REGISTER_VARIABLE( KIRCHHOFF_STRESS_TENSOR )
+    KRATOS_REGISTER_VARIABLE( ELASTIC_LEFT_CAUCHY_GREEN_TENSOR )
+    KRATOS_REGISTER_VARIABLE( ELASTIC_LEFT_CAUCHY_GREEN_VECTOR )
 
     //thermal
 
@@ -223,7 +280,25 @@ namespace Kratos
     KRATOS_REGISTER_VARIABLE( TAU_STAB )
     KRATOS_REGISTER_VARIABLE( MU_STATIC )
     KRATOS_REGISTER_VARIABLE( MU_DYNAMIC )
-  
+
+    // Contact postprocess
+    KRATOS_REGISTER_3D_VARIABLE_WITH_COMPONENTS( CONTACT_STRESS )
+    KRATOS_REGISTER_3D_VARIABLE_WITH_COMPONENTS( EFFECTIVE_CONTACT_STRESS )
+    KRATOS_REGISTER_3D_VARIABLE_WITH_COMPONENTS( EFFECTIVE_CONTACT_FORCE )
+    KRATOS_REGISTER_VARIABLE( CONTACT_ADHESION )
+    KRATOS_REGISTER_VARIABLE( CONTACT_FRICTION_ANGLE )
+    // Material postprocess + invariants
+    KRATOS_REGISTER_VARIABLE( PRECONSOLIDATION )
+    KRATOS_REGISTER_VARIABLE( STRESS_INV_P )
+    KRATOS_REGISTER_VARIABLE( STRESS_INV_J2 )
+    KRATOS_REGISTER_VARIABLE( STRESS_INV_THETA )
+    KRATOS_REGISTER_VARIABLE( VOLUMETRIC_PLASTIC )
+    KRATOS_REGISTER_VARIABLE( INCR_SHEAR_PLASTIC )
+
+    KRATOS_REGISTER_VARIABLE( M_MODULUS )
+    KRATOS_REGISTER_VARIABLE( SIMILAR_YOUNG_MODULUS )
+    KRATOS_REGISTER_VARIABLE( STABILIZATION )
+
   }
   
 }  // namespace Kratos.
