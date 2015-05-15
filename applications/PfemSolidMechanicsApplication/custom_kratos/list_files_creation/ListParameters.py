cfe7a20d75bea57e6b9f882710e09d891f21c4eb
Josep Maria <josep-m-carbonell@users.noreply.github.com>
2015-05-15 11:35:10 +0000

    list creation

---
+++ applications/PfemSolidMechanicsApplication/custom_kratos/list_files_creation/ListParameters.py
@@ -0,0 +1,20 @@
+#List Files Creation
+#####################################
+problem_name = 'string_drill_lm_6'
+problem_path = '/home/jmaria/Desktop/String Drill Tests/string_drill_lm_6.gid'
+
+print_lists = "True"
+output_mode = "Binary" #"Ascii"
+
+file_list = []
+file_list.append(1)
+file_list.append(2)
+file_list.append(4)
+file_list.append(5)
+file_list.append(6)
+file_list.append(7)
+file_list.append(8)
+file_list.append(10)
+file_list.append(15)
+file_list.append(20)
+file_list.append(40)
