6451efb4a62de30f7dfbc82201e5a310cbe90d2d
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-05-28 10:06:04 +0000

    some improvements in parameters for meshing and  refinement

--- kratos/applications/PfemSolidMechanicsApplication/custom_modelers/mesh_modeler.cpp
+++ kratos/applications/PfemSolidMechanicsApplication/custom_modelers/mesh_modeler.cpp
@@ -547,11 +547,19 @@ namespace Kratos
 	    //recover DETERMINANT_F FOR VARIABLES SMOOTHING from nodes
 	    if( mMeshingVariables[MeshId].RemeshInfo.CriticalElements > 0 ){
 	      
-	      double critical_value = mMeshingVariables[MeshId].Refine.CriticalDissipation;
+	      double critical_value = 0;
+
+	      //Smoothing performed only in critical elements (based on Plastic Strain)	      
+	      mDataTransferUtilities.TransferNodalValuesToElements(DETERMINANT_F,PLASTIC_STRAIN,critical_value,rModelPart,MeshId);
+
+	      //Smoothing performed only in critical elements (based on Set Dissipation Variable)	      
+	      //critical_value = mMeshingVariables[MeshId].Refine.CriticalDissipation;
+	      //mDataTransferUtilities.TransferNodalValuesToElements(DETERMINANT_F,mMeshingVariables[MeshId].Refine.GetDissipationVariable(),critical_value,rModelPart,MeshId);
 
 	      //Smoothing performed only in critical elements (based on Plastic Energy Dissipation)
-	      mDataTransferUtilities.TransferNodalValuesToElements(DETERMINANT_F,mMeshingVariables[MeshId].Refine.GetDissipationVariable(),critical_value,rModelPart,MeshId);
+	      //critical_value = mMeshingVariables[MeshId].Refine.CriticalDissipation;
 	      //mDataTransferUtilities.TransferNodalValuesToElements(DETERMINANT_F,PLASTIC_DISSIPATION,critical_value,rModelPart,MeshId);
+
 	    }
 	    else{
 
