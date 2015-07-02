46e7896d87bb5dc9618e2993bc2a956ebbed4318
ipouplana <ipouplana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-07-02 16:36:44 +0000

    It is the hardening law associated to the implemented isotropic damage model

---
+++ kratos/applications/SolidMechanicsApplication/custom_constitutive/custom_hardening_laws/exponential_damage_hardening_law.cpp
@@ -0,0 +1,137 @@
+//
+//   Project Name:        KratosSolidMechanicsApplication $
+//   Last modified by:    $Author:              IPouplana $
+//   Date:                $Date:                July 2015 $
+//   Revision:            $Revision:                  0.0 $
+//
+//
+// System includes
+#include <string>
+#include <iostream>
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/properties.h"
+#include "solid_mechanics_application.h"
+#include "custom_constitutive/custom_hardening_laws/exponential_damage_hardening_law.hpp"
+
+namespace Kratos
+{
+
+//*******************************CONSTRUCTOR******************************************
+//************************************************************************************
+
+ExponentialDamageHardeningLaw::ExponentialDamageHardeningLaw()
+	:HardeningLaw()
+{
+       
+}
+
+
+//*******************************ASSIGMENT OPERATOR***********************************
+//************************************************************************************
+
+ExponentialDamageHardeningLaw& ExponentialDamageHardeningLaw::operator=(ExponentialDamageHardeningLaw const& rOther)
+{
+   HardeningLaw::operator=(rOther);
+   return *this;
+}
+
+//*******************************COPY CONSTRUCTOR*************************************
+//************************************************************************************
+
+ExponentialDamageHardeningLaw::ExponentialDamageHardeningLaw(ExponentialDamageHardeningLaw const& rOther)
+	:HardeningLaw(rOther)
+{
+
+}
+
+
+//********************************CLONE***********************************************
+//************************************************************************************
+
+HardeningLaw::Pointer ExponentialDamageHardeningLaw::Clone() const
+{
+  HardeningLaw::Pointer p_clone(new ExponentialDamageHardeningLaw(*this));
+  return p_clone;
+}
+
+
+//********************************DESTRUCTOR******************************************
+//************************************************************************************
+
+ExponentialDamageHardeningLaw::~ExponentialDamageHardeningLaw()
+{
+}
+
+/// Operations.
+
+//****************************** CALCULATE DAMAGE PARAMETER **************************
+//************************************************************************************
+
+double& ExponentialDamageHardeningLaw::CalculateHardening(double &rHardening, const Parameters& rValues)
+{
+	const Properties& MaterialProperties = this->GetProperties();
+    const double& FractureEnergy  = MaterialProperties[FRACTURE_ENERGY];
+    const double& DamageThreshold = MaterialProperties[DAMAGE_THRESHOLD];
+
+    const double& CharacteristicLength = rValues.GetCharacteristicSize();
+    const double& InternalVariable = rValues.GetDeltaGamma();
+
+    double A = 1.0/(FractureEnergy/(CharacteristicLength*DamageThreshold*DamageThreshold)-0.5);
+
+    if(A < 0.0) A = 0.0;
+    
+    //Compute Damage variable from the internal historical variable
+    rHardening = 1.0-DamageThreshold/InternalVariable*exp(A*(1.0-InternalVariable/DamageThreshold));
+
+    if(rHardening < 0.0) rHardening = 0.0;
+    if(rHardening > 1.0) rHardening = 1.0;
+    
+	return rHardening;
+}
+
+
+//***************************** CALCULATE DAMAGE DERIVATIVE **************************
+//************************************************************************************
+
+double& ExponentialDamageHardeningLaw::CalculateDeltaHardening(double &rDeltaHardening, const Parameters& rValues)
+{
+	const Properties& MaterialProperties = this->GetProperties();
+    const double& FractureEnergy  = MaterialProperties[FRACTURE_ENERGY];
+    const double& DamageThreshold = MaterialProperties[DAMAGE_THRESHOLD];
+
+    const double& CharacteristicLength = rValues.GetCharacteristicSize();
+    const double& InternalVariable = rValues.GetDeltaGamma();
+
+    double A = 1.0/(FractureEnergy/(CharacteristicLength*DamageThreshold*DamageThreshold)-0.5);
+
+    if(A < 0.0) A = 0.0;
+    
+    //Damage derivative with respect to the internal historical variable
+    rDeltaHardening = (DamageThreshold + A*InternalVariable)/(InternalVariable*InternalVariable)*exp(A*(1.0 - InternalVariable/DamageThreshold));
+
+    if(rDeltaHardening < 0.0) rDeltaHardening = 0.0;
+    
+	return rDeltaHardening;
+}
+
+
+//************************************************************************************
+//************************************************************************************
+
+
+void ExponentialDamageHardeningLaw::save( Serializer& rSerializer ) const
+{
+    KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, HardeningLaw )
+}
+
+void ExponentialDamageHardeningLaw::load( Serializer& rSerializer )
+{
+    KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, HardeningLaw )
+}
+
+
+}  // namespace Kratos.
---
+++ kratos/applications/SolidMechanicsApplication/custom_constitutive/custom_hardening_laws/exponential_damage_hardening_law.hpp
@@ -0,0 +1,216 @@
+//
+//   Project Name:        KratosSolidMechanicsApplication $
+//   Last modified by:    $Author:              IPouplana $
+//   Date:                $Date:                July 2015 $
+//   Revision:            $Revision:                  0.0 $
+//
+//
+
+#if !defined(KRATOS_EXPONENTIAL_DAMAGE_HARDENING_LAW_H_INCLUDED )
+#define  KRATOS_EXPONENTIAL_DAMAGE_HARDENING_LAW_H_INCLUDED
+
+
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "custom_constitutive/custom_hardening_laws/hardening_law.hpp"
+
+namespace Kratos
+{
+///@addtogroup ApplicationNameApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/// Short class definition.
+/** Detail class definition.
+*/
+class ExponentialDamageHardeningLaw
+	: public HardeningLaw
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of ExponentialDamageHardeningLaw
+    KRATOS_CLASS_POINTER_DEFINITION( ExponentialDamageHardeningLaw );
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    ExponentialDamageHardeningLaw();
+
+
+    /// Copy constructor.
+    ExponentialDamageHardeningLaw(ExponentialDamageHardeningLaw const& rOther);
+
+    /// Assignment operator.
+    ExponentialDamageHardeningLaw& operator=(ExponentialDamageHardeningLaw const& rOther);
+
+    /// Destructor.
+    ~ExponentialDamageHardeningLaw();
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+   /**
+     * Clone function (has to be implemented by any derived class)
+     * @return a pointer to a new instance of this hardening law
+     */
+    HardeningLaw::Pointer Clone() const;
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    double& CalculateHardening(double &rHardening, const Parameters& rValues);
+
+    double& CalculateDeltaHardening(double &rDeltaHardening, const Parameters& rValues);
+    
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+    ///@}
+    ///@name Protected Operators
+    ///@{
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+    ///@}
+    ///@name Serialization
+    ///@{
+    friend class Serializer;
+
+    // A private default constructor necessary for serialization
+
+    virtual void save(Serializer& rSerializer) const;
+
+    virtual void load(Serializer& rSerializer);
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+
+    ///@}
+
+}; // Class ExponentialDamageHardeningLaw
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+/// // input stream function
+
+///@}
+
+///@} addtogroup block
+
+}  // namespace Kratos.
+
+#endif // KRATOS_EXPONENTIAL_DAMAGE_HARDENING_LAW_H_INCLUDED  defined 
+
+
