3dbe17eaa3bd635b00ac1f44fa8b6e3b6c0f8452
ipouplana <ipouplana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-07-02 16:43:28 +0000

    Super classes of the isotropic damage laws. They derive from hyperelastic_plastic laws.

---
+++ kratos/applications/SolidMechanicsApplication/custom_constitutive/linear_elastic_plastic_plane_stress_2D_law.cpp
@@ -0,0 +1,116 @@
+//
+//   Project Name:        KratosSolidMechanicsApplication $
+//   Last modified by:    $Author:              IPouplana $
+//   Date:                $Date:                July 2015 $
+//   Revision:            $Revision:                  0.0 $
+//
+//
+
+// System includes
+#include <iostream>
+
+// External includes
+#include<cmath>
+
+// Project includes
+#include "includes/properties.h"
+#include "custom_constitutive/linear_elastic_plastic_plane_stress_2D_law.hpp"
+
+#include "solid_mechanics_application.h"
+
+namespace Kratos
+{
+
+//******************************CONSTRUCTOR*******************************************
+//************************************************************************************
+
+LinearElasticPlasticPlaneStress2DLaw::LinearElasticPlasticPlaneStress2DLaw()
+    : LinearElasticPlastic3DLaw()
+{
+
+}
+
+
+//******************************CONSTRUCTOR*******************************************
+//************************************************************************************
+
+LinearElasticPlasticPlaneStress2DLaw::LinearElasticPlasticPlaneStress2DLaw(FlowRulePointer pFlowRule, YieldCriterionPointer pYieldCriterion, HardeningLawPointer pHardeningLaw)
+ : LinearElasticPlastic3DLaw(pFlowRule,pYieldCriterion,pHardeningLaw)
+{
+
+}
+
+//******************************COPY CONSTRUCTOR**************************************
+//************************************************************************************
+
+LinearElasticPlasticPlaneStress2DLaw::LinearElasticPlasticPlaneStress2DLaw(const LinearElasticPlasticPlaneStress2DLaw& rOther)
+    : LinearElasticPlastic3DLaw(rOther)
+{
+
+}
+
+//********************************CLONE***********************************************
+//************************************************************************************
+
+ConstitutiveLaw::Pointer LinearElasticPlasticPlaneStress2DLaw::Clone() const
+{
+    LinearElasticPlasticPlaneStress2DLaw::Pointer p_clone(new LinearElasticPlasticPlaneStress2DLaw(*this));
+    return p_clone;
+}
+
+//*******************************DESTRUCTOR*******************************************
+//************************************************************************************
+
+LinearElasticPlasticPlaneStress2DLaw::~LinearElasticPlasticPlaneStress2DLaw()
+{
+}
+
+
+//************* COMPUTING  METHODS
+//************************************************************************************
+//************************************************************************************
+
+
+//********************* COMPUTE LINEAR ELASTIC CONSTITUTIVE MATRIX *******************
+//************************************************************************************
+
+
+void LinearElasticPlasticPlaneStress2DLaw::CalculateLinearElasticMatrix( Matrix& rConstitutiveMatrix,
+        const double &rYoungModulus,
+        const double &rPoissonCoefficient )
+{
+    rConstitutiveMatrix.clear();
+
+    rConstitutiveMatrix ( 0 , 0 ) = (rYoungModulus)/(1.0-rPoissonCoefficient*rPoissonCoefficient);
+    rConstitutiveMatrix ( 1 , 1 ) = rConstitutiveMatrix ( 0 , 0 );
+
+    rConstitutiveMatrix ( 2 , 2 ) = rConstitutiveMatrix ( 0 , 0 )*(1-rPoissonCoefficient)*0.5;
+
+    rConstitutiveMatrix ( 0 , 1 ) = rConstitutiveMatrix ( 0 , 0 )*rPoissonCoefficient;
+    rConstitutiveMatrix ( 1 , 0 ) = rConstitutiveMatrix ( 0 , 1 );
+}
+
+
+//*************************CONSTITUTIVE LAW GENERAL FEATURES *************************
+//************************************************************************************
+
+void LinearElasticPlasticPlaneStress2DLaw::GetLawFeatures(Features& rFeatures)
+{
+    //Set the type of law
+	rFeatures.mOptions.Set( PLANE_STRESS_LAW );
+	rFeatures.mOptions.Set( INFINITESIMAL_STRAINS );
+	rFeatures.mOptions.Set( ISOTROPIC );
+
+	//Set strain measure required by the consitutive law
+	rFeatures.mStrainMeasures.push_back(StrainMeasure_Infinitesimal);
+	rFeatures.mStrainMeasures.push_back(StrainMeasure_Deformation_Gradient);
+
+	//Set the strain size
+	rFeatures.mStrainSize = GetStrainSize();
+
+	//Set the spacedimension
+	rFeatures.mSpaceDimension = WorkingSpaceDimension();
+
+}
+
+} // Namespace Kratos
