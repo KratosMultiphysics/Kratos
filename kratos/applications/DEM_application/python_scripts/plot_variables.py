4082e944d2bafd5da6c302924f4d6b7d00229d07
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-07-15 09:12:05 +0000

    v1.1 of Chung-Ooi benchmarks.

--- kratos/applications/DEM_application/python_scripts/plot_variables.py
+++ kratos/applications/DEM_application/python_scripts/plot_variables.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 # importing the Kratos Library
 from KratosMultiphysics import *
-
+from KratosMultiphysics.DEMApplication import *
 # check that KratosMultiphysics was imported in the main script
 CheckForPreviousImport()
 
@@ -18,7 +18,11 @@ class variable_plotter:
                 if node.Id == id:
                     self.list_of_nodes.append(node)
                     file_writer = open("variables_for_node_" + str(id) + ".txt", 'w');
-                    file_writer.write("Time  DISPLACEMENT_X  DISPLACEMENT_Y  DISPLACEMENT_Z  VELOCITY_X  VELOCITY_Y  VELOCITY_Z  ACCELERATION_X  ACCELERATION_Y  ACCELERATION_Z  ANGULAR_VELOCITY_X  ANGULAR_VELOCITY_Y  ANGULAR_VELOCITY_Z   ANGULAR_ACCELERATION_X  ANGULAR_ACCELERATION_Y  ANGULAR_ACCELERATION_Z \n")
+                    file_writer.write("#Time  DISPLACEMENT_X  DISPLACEMENT_Y  DISPLACEMENT_Z  ")
+                    file_writer.write("ELASTIC_FORCES_X  ELASTIC_FORCES_Y  ELASTIC_FORCES_Z  ")
+                    file_writer.write("TOTAL_FORCES_X  TOTAL_FORCES_Y  TOTAL_FORCES_Z  ")
+                    file_writer.write("VELOCITY_X  VELOCITY_Y  VELOCITY_Z  ")
+                    file_writer.write("ANGULAR_VELOCITY_X  ANGULAR_VELOCITY_Y  ANGULAR_VELOCITY_Z\n")
                     self.files.append(file_writer)
                     print("The Id "+str(id)+" was found in the model part")
                     break
@@ -29,41 +33,33 @@ class variable_plotter:
                
         self.plot_variables(0.0)
          
-         
-
     def plot_variables(self, time): 
-    
+        
         i = 0
+        
         for file_writer in self.files:
             node = self.list_of_nodes[i]
             string = str(time) \
             + "  " + str(node.GetSolutionStepValue(DISPLACEMENT_X)) \
             + "  " + str(node.GetSolutionStepValue(DISPLACEMENT_Y)) \
             + "  " + str(node.GetSolutionStepValue(DISPLACEMENT_Z)) \
+            + "  " + str(node.GetSolutionStepValue(ELASTIC_FORCES_X)) \
+            + "  " + str(node.GetSolutionStepValue(ELASTIC_FORCES_Y)) \
+            + "  " + str(node.GetSolutionStepValue(ELASTIC_FORCES_Z)) \
+            + "  " + str(node.GetSolutionStepValue(TOTAL_FORCES_X)) \
+            + "  " + str(node.GetSolutionStepValue(TOTAL_FORCES_Y)) \
+            + "  " + str(node.GetSolutionStepValue(TOTAL_FORCES_Z)) \
             + "  " + str(node.GetSolutionStepValue(VELOCITY_X)) \
             + "  " + str(node.GetSolutionStepValue(VELOCITY_Y)) \
             + "  " + str(node.GetSolutionStepValue(VELOCITY_Z)) \
-            + "  " + str(node.GetSolutionStepValue(ACCELERATION_X)) \
-            + "  " + str(node.GetSolutionStepValue(ACCELERATION_Y)) \
-            + "  " + str(node.GetSolutionStepValue(ACCELERATION_Z)) \
             + "  " + str(node.GetSolutionStepValue(ANGULAR_VELOCITY_X)) \
             + "  " + str(node.GetSolutionStepValue(ANGULAR_VELOCITY_Y)) \
             + "  " + str(node.GetSolutionStepValue(ANGULAR_VELOCITY_Z)) \
-            + "  " + str(node.GetSolutionStepValue(ANGULAR_ACCELERATION_X)) \
-            + "  " + str(node.GetSolutionStepValue(ANGULAR_ACCELERATION_Y)) \
-            + "  " + str(node.GetSolutionStepValue(ANGULAR_ACCELERATION_Z)) \
-            + "\n"
-            
+            + '\n'
             file_writer.write(string)
             i = i + 1 
-            
-            
-            
+               
     def close_files(self):
         
         for file_writer in self.files:
             file_writer.close()
-            
-        
-                
-    
