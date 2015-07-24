b19c6a7d757f24f5d5db243ed0fa8f14b559a8dd
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-07-24 10:32:44 +0000

    Hertz and Tsuji are now the same Tsuji is to be removed.

--- kratos/applications/DEM_application/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.h
+++ kratos/applications/DEM_application/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.h
@@ -1,10 +1,15 @@
+//Authors: M.A. Celigueta and S. Latorre (CIMNE)
+//   Date: July 2015
 
 #if !defined(DEM_D_HERTZ_VISCOUS_COULOMB_CL_H_INCLUDED)
 #define  DEM_D_HERTZ_VISCOUS_COULOMB_CL_H_INCLUDED
 
-/* Project includes */
-//#include "../custom_elements/spheric_continuum_particle.h"
+#include <string>
+#include <iostream>
+
+#include "DEM_application.h"
 #include "DEM_discontinuum_constitutive_law.h"
+#include "../custom_elements/spheric_particle.h"
 
 namespace Kratos {
 
@@ -23,87 +28,64 @@ namespace Kratos {
         ~DEM_D_Hertz_viscous_Coulomb() {
         }
 
-        DEMDiscontinuumConstitutiveLaw::Pointer Clone() const;
-
-        void CalculateForces(double LocalElasticContactForce[3],
-                double LocalDeltDisp[3],
-                double kn_el,
-                double kt_el,
-                double indentation,
-                double& failure_criterion_state,
-                bool& sliding,
-                SphericParticle* element1,
-                SphericParticle* element2,
-                int &mNeighbourFailureId_count,
-                double mapping_new_cont);
-              
-        void CalculateNormalForceHertz(double LocalElasticContactForce[3], const double kn_el, const double indentation);
+        DEMDiscontinuumConstitutiveLaw::Pointer Clone() const;      
         
-        void CalculateTangentialForceLinear(double LocalElasticContactForce[3],
-                double LocalDeltDisp[3],
-                const double kt_el,
-                const double indentation,
-                double& failure_criterion_state,
-                bool& sliding,
-                SphericParticle* element1,
-                SphericParticle* element2,
-                int &mNeighbourFailureId_count,
-                double mapping_new_cont);
-
-        void CalculateViscoDamping(double LocalRelVel[3],
-                double ViscoDampingLocalContactForce[3],
-                double indentation,
-                double equiv_visco_damp_coeff_normal,
-                double equiv_visco_damp_coeff_tangential,
-                bool sliding,
-                int mDampType);
+        void InitializeContact(SphericParticle* const element1, SphericParticle* const element2, const double indentation);  
         
-        
-        
-        void InitializeContact(SphericParticle* const element1, SphericParticle* const element2);  
-        
-        void InitializeContactWithFEM(SphericParticle* const element, DEMWall* const wall, const double indentation, const double ini_delta=0.0);
+        void InitializeContactWithFEM(SphericParticle* const element, DEMWall* const wall, const double indentation, const double ini_delta = 0.0);
+        void CalculateForces(const double OldLocalContactForce[3],
+                            double LocalElasticContactForce[3],
+                            double LocalDeltDisp[3],
+                            double LocalRelVel[3],            
+                            double indentation,
+                            double previous_indentation,
+                            double ViscoDampingLocalContactForce[3],
+                            double& cohesive_force,
+                            SphericParticle* element1,
+                            SphericParticle* element2); 
         
         void CalculateForcesWithFEM(const double OldLocalContactForce[3], double LocalElasticContactForce[3],
                                     double LocalDeltDisp[3], double LocalRelVel[3], double indentation,
                                     double previous_indentation, double ViscoDampingLocalContactForce[3], double& cohesive_force,
                                     SphericParticle* const element, DEMWall* const wall);
         
-        double CalculateNormalForce(const double indentation,
-                                    SphericParticle* const element1,
-                                    SphericParticle* const element2);
-        double CalculateNormalForceWithFEM(const double indentation,
-                                    SphericParticle* const element,
-                                    DEMWall* const wall);
+        double CalculateNormalForce(const double indentation);
         
         double CalculateCohesiveNormalForce(SphericParticle* const element1,
                                             SphericParticle* const element2);
         double CalculateCohesiveNormalForceWithFEM(SphericParticle* const element,
                                             DEMWall* const wall);
         
-        void CalculateTangentialForce(const double normal_force,
+        void CalculateTangentialForce(const double normal_contact_force,
+                                    const double OldLocalContactForce[3],
+                                    double LocalElasticContactForce[3],
+                                    //const double ViscoDampingLocalContactForce[3],
+                                    double ViscoDampingLocalContactForce[3],
+                                    const double LocalDeltDisp[3],            
+                                    bool& sliding,
+                                    SphericParticle* const element1,
+                                    SphericParticle* const element2,
+                                    double indentation,
+                                    double previous_indentation);
+        
+        void CalculateTangentialForceWithFEM(const double normal_contact_force,
+                                             const double OldLocalContactForce[3],
                                       double LocalElasticContactForce[3],
+                                      double ViscoDampingLocalContactForce[3],
                                       const double LocalDeltDisp[3],            
                                       bool& sliding,
-                                      SphericParticle* const element1,
-                                      SphericParticle* const element2);
-        void CalculateTangentialForceWithFEM(double LocalRelVel[3],
-                                                    double ViscoDampingLocalContactForce[3],
-                                                    const double OldLocalContactForce[3],
-                                                    double LocalElasticContactForce[3],
-                                                    const double LocalDeltDisp[3],            
-                                                    bool& sliding,
-                                                    SphericParticle* const element,
-                                                    DEMWall* const wall,
-                                                    double indentation,
-                                                    double previous_indentation,
-                                                    double delta_force_normal);
+                                      SphericParticle* const element,
+                                      DEMWall* const wall,
+                                      double indentation,
+                                      double previous_indentation);
         
         void CalculateViscoDampingForce(double LocalRelVel[3],
                                         double ViscoDampingLocalContactForce[3],
                                         bool sliding,
-                                        SphericParticle* const element,
-                                        SphericParticle* const element2);                                        
+                                        SphericParticle* const element1,
+                                        SphericParticle* const element2,
+                                        double indentation);            
+        
         void CalculateViscoDampingForceWithFEM(double LocalRelVel[3],
                                         double ViscoDampingLocalContactForce[3],
                                         bool sliding,
@@ -114,7 +96,7 @@ namespace Kratos {
     private:
 
         friend class Serializer;
-
+        
         virtual void save(Serializer& rSerializer) const {
             KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, DEMDiscontinuumConstitutiveLaw)
                     //rSerializer.save("MyMemberName",myMember);
