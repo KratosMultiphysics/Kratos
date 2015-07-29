b4309aba01d903dbc7297de89579b5cb41ac663f
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-07-29 08:37:05 +0000

    v2.2 of Chung-Ooi benchmarks.

---
+++ kratos/applications/DEM_application/test_examples/DEM_run_all_benchmarks.py
@@ -0,0 +1,64 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+import os
+import sys
+import platform
+
+kratos_benchmarking_path = '../../../benchmarking'
+sys.path.append(kratos_benchmarking_path)
+dem_scripts_path = '../test_examples/basic_benchmarks'
+sys.path.append(dem_scripts_path)
+import benchmarking
+os.chdir(dem_scripts_path)
+
+
+def Run():
+    
+    print("\nStarting DEM Benchmarks..............\n")
+    f = open("errors.txt", "w")
+    f.write("\n========== DEM BENCHMARKS ===========\n")
+    f.write("========== SLIDING REGIME ===========\n\n")
+    f.close()
+    Text=""
+        
+    for benchmark in range(3, 9):
+      
+        print("Running Benchmark " + str(benchmark) + " of 8.............\n")    
+        
+        if platform.system()=="Windows":
+            os.system("python Chung_Ooi_benchmarks.py " + str(benchmark) + " > BenchTemp.txt")
+        else:
+            if sys.version_info >= (3, 0):
+                os.system("python3 Chung_Ooi_benchmarks.py " + str(benchmark) + " > BenchTemp.txt")
+            else:
+                os.system("python -3 Chung_Ooi_benchmarks.py " + str(benchmark) + " > BenchTemp.txt")
+                
+    os.remove("BenchTemp.txt")
+        
+    f = open("errors.txt")
+    file_contents = f.read()
+    f.close()
+    
+    Text += file_contents.rstrip("\n")
+    Text += "\n\n\n"
+        
+    os.remove("errors.txt")
+    
+    return Text
+
+    
+def RunBenchmark(ExamplePath):
+
+    if platform.system()=="Windows":
+        os.system("python " + ExamplePath)
+    else:
+        if sys.version_info >= (3, 0):
+            os.system(
+                "python3 " +
+                ExamplePath)
+        else:
+            os.system(
+                "python -3 " +
+                ExamplePath)
+       
+if __name__ == '__main__':
+    print(Run())
