195a4b747c00c1dd9c080115e4826fe06f75092a
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-08-03 21:27:19 +0000

    aesth.

--- kratos/applications/DEM_application/python_scripts/angle_finder.py
+++ kratos/applications/DEM_application/python_scripts/angle_finder.py
@@ -12,7 +12,6 @@ fraction = [0] * number_of_sectors
 fraction_exp_300 = [0.0, 0.005645342312008972, 0.030788274905921897, 0.2692645408331682, 0.3890479963028982, 0.19824427279329238, 0.06181699346405223, 0.006438106555753553, 0.0002079619726678139, 0.0, 0.0, 0.0]
 fraction_exp_500 = [0.0, 0.00490797546012256, 0.009815950920245342, 0.04539877300613493, 0.40981595092024536, 0.44417177914110423, 0.08036809815950907, 0.00490797546012256, 0.0, 0.0, 0.0, 0.0]
 fraction_exp_650 = [0.0, 0.0, 0.006341436575547199, 0.047897039000100894, 0.47890931765871014, 0.40405553364939345, 0.015909436868987936, 0.0, 0.0, 0.0, 0.0, 0.0]
-print len(fraction_exp_300)
 total_accumulation = 0
 
 for line in in_file.readlines():
@@ -45,10 +44,11 @@ print("Total number of particles was: " + str(total_accumulation))
 in_file.close()
 
 import matplotlib.pyplot as plotter
-plotter.plot(range(0,number_of_sectors*30,30), fraction, label="DEM")
-plotter.plot(range(0,number_of_sectors*30,30), fraction_exp_300,label="experiment, 300 rpm")
-plotter.plot(range(0,number_of_sectors*30,30), fraction_exp_500,label="experiment, 500 rpm")
-plotter.plot(range(0,number_of_sectors*30,30), fraction_exp_650,label="experiment, 650 rpm")
+x_range = range(0,number_of_sectors*30,30)
+plotter.plot(x_range, fraction, label="DEM")
+plotter.plot(x_range, fraction_exp_300,label="experiment, 300 rpm")
+plotter.plot(x_range, fraction_exp_500,label="experiment, 500 rpm")
+plotter.plot(x_range, fraction_exp_650,label="experiment, 650 rpm")
 plotter.xlabel('Compartment angle')
 plotter.ylabel('Mass fraction')
 plotter.legend()
