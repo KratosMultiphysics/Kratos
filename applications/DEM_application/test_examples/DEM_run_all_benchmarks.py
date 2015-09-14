788ce159683b1536b861fd1793b30ad4a64c439c
Salva Latorre <latorre@cimne.upc.edu>
2015-09-14 16:12:56 +0000

    v2.7 of DEM Benchmarking. Encapsulation of Chung-Ooi benchmarks, renamed as Basic Discontinuum Tests.

--- applications/DEM_application/test_examples/DEM_run_all_benchmarks.py
+++ applications/DEM_application/test_examples/DEM_run_all_benchmarks.py
@@ -12,37 +12,45 @@ import benchmarking
 path = os.getcwd()
 path += '/basic_benchmarks'
 os.chdir(path)
-
+                  
+Benchmark_text = ["Running DEM Benchmark 1... Elastic normal contact of two identical spheres\n",\
+                  "Running DEM Benchmark 2... Elastic normal contact of a sphere against a rigid plane\n",\
+                  "Running DEM Benchmark 3... Normal contact of a sphere against a rigid plane with different coefficients of restitution\n",\
+                  "Running DEM Benchmark 4... Oblique impact of a sphere with a rigid plane with constant resultant velocity and variable incident angles\n",\
+                  "Running DEM Benchmark 5... Oblique impact of a sphere with a rigid plane with constant normal velocity and variable angular velocity\n",\
+                  "Running DEM Benchmark 6... Impact of a sphere with a rigid plane with a constant normal velocity and different angular velocities\n",\
+                  "Running DEM Benchmark 7... Impact of two identical spheres with a constant normal velocity and different angular velocities\n",\
+                  "Running DEM Benchmark 8... Impact of two differently sized spheres with a constant normal velocity and different angular velocities\n",\
+                  "Running DEM Benchmark 9... Impact of two identical spheres with a constant normal velocity and different coefficients of restitution\n"]
 
 def Run():
     
-    print("\nStarting DEM Benchmarks..............\n")
+    print("\nStarting DEM Benchmarking..............\n")
     g = open("errors.txt", "w")
-    g.write("\n========== DEM BENCHMARKS ===========\n")
-    g.write("========== SLIDING REGIME ===========\n\n")
+    g.write("\n========== DEM BENCHMARKING RESULTS ==========\n\n")
+    g.write("== BASIC DISCONTINUUM TESTS, SLIDING REGIME ==\n\n")
     g.close()
     Text=""
-    
     f=open("BenchTemp.txt", "w")
-
+    
     for benchmark in range(3, 10):
       
-        print("Running Benchmark " + str(benchmark) + " of 9.............")
-             
+        print(Benchmark_text[benchmark - 1])
+                
         try:
             if platform.system()=="Windows":
-                subprocess.check_call(["python", path + "/Chung_Ooi_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
+                subprocess.check_call(["python", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
                             
             else:
                 if sys.version_info >= (3, 0):
-                    subprocess.check_call(["python3", path + "/Chung_Ooi_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
+                    subprocess.check_call(["python3", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
                     
                 else:
-                    subprocess.check_call(["python", "-3", path + "/Chung_Ooi_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
+                    subprocess.check_call(["python", "-3", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
         except:
-            print("\nA problem was found in Benchmark " + str(benchmark) + "... Resuming...\n")
+            print("\nA problem was found in DEM Benchmark " + str(benchmark) + "... Resuming...\n")
             g = open("errors.txt", "a")
-            g.write("Test " + str(benchmark) + ": KO!........ Test " + str(benchmark) + " FAILED\n")
+            g.write("DEM Benchmark " + str(benchmark) + ": KO!........ Test " + str(benchmark) + " FAILED\n")
             g.close()
     
     print('\n')
