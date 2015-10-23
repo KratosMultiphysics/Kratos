0a42f938d16a7b1b4c940de56c5e749d630f5ab1
Salva Latorre <latorre@cimne.upc.edu>
2015-10-23 16:56:07 +0000

    DEM Benchmarks summary is now sent only when errors are found.

--- applications/DEM_application/test_examples/DEM_run_all_benchmarks.py
+++ applications/DEM_application/test_examples/DEM_run_all_benchmarks.py
@@ -36,13 +36,15 @@ def Run():
     
     print("\nStarting DEM Benchmarking..............\n")
     g = open("errors.txt", "w")
-    g.write("\n\n========== DEM BENCHMARKING RESULTS ==========\n\n")
+    g.write("The complete list of benchmarks are included at the end of this message as a quick reference.\n")
+    g.write("\n========== DEM BENCHMARKING RESULTS ==========\n")
     g.write("\n=========== DEM DISCONTINUUM TESTS ===========\n")
     g.write("\n==== TSUJI PAPER BENCHMARKS. SLIDING REGIME ==\n\n")
     g.close()
     Text = ""
     f = open("BenchTemp.txt", "w")
     failure = False
+    #list_of_failed_tests = []
     
     #Discontinuum Tests. From 1 to 11
     D_DEM_Benchmarks_list = list(range(1,12))
@@ -71,7 +73,8 @@ def Run():
                     subprocess.check_call(["python", "-3", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.txt"], stdout=f, stderr=f)
                 os.system("export OMP_NUM_THREADS=16") # Trying to set a 'default' value
         except:
-            failure = True
+            #failure = True
+            #list_of_failed_tests += [benchmark]
             print("A problem was found in DEM Benchmark " + str(benchmark) + "... Resuming...\n")
             g = open("errors.txt", "a")
             g.write("DEM Benchmark " + str(benchmark) + ": KO!........ Test " + str(benchmark) + " FAILED\n")
@@ -79,8 +82,36 @@ def Run():
             
     print('\n')
     f.close()
-    os.remove("BenchTemp.txt")        
+    os.remove("BenchTemp.txt")
     
+    g = open("errors.txt", 'a')
+    #g.write("\n______________________________________________\n")
+    g.write("\n----------------------------------------------\n")
+    g.write("\nList of Benchmarks:\n")
+    g.write("\nDISCONTINUUM TESTS:\n")
+    g.write("Benchmark 01. Elastic normal impact of two identical spheres\n")
+    g.write("Benchmark 02. Elastic normal impact of a sphere against a rigid plane\n")
+    g.write("Benchmark 03. Impact of a sphere against a rigid plane with different coefficients of restitution\n")
+    g.write("Benchmark 04. Oblique impact of a sphere with a rigid plane with constant velocity module and variable incident angles\n")
+    g.write("Benchmark 05. Oblique impact of a sphere with a rigid plane with constant normal velocity and different angular velocities\n")
+    g.write("Benchmark 06. Impact of a sphere with a rigid plane with a constant normal velocity and variable angular velocities\n")
+    g.write("Benchmark 07. Impact of two identical spheres with a constant normal velocity and different angular velocities\n")
+    g.write("Benchmark 08. Impact of two differently sized spheres with a constant normal velocity and variable angular velocities\n")
+    g.write("Benchmark 09. Impact of two identical spheres with a constant normal velocity and different coefficients of restitution\n")
+    g.write("Benchmark 10. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
+    g.write("Benchmark 11. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
+    g.write("\nCONTINUUM TESTS:\n")
+    g.write("Benchmark 20. Normal compression of two identical spheres\n")
+    g.write("Benchmark 21. Normal compression of two identical indented spheres\n")
+    g.write("Benchmark 22. Tensile test of two identical spheres\n")
+    g.write("Benchmark 23. Tensile test of two identical indented spheres\n")
+    g.write("Benchmark 24. Shear test of two identical spheres by applying rotation\n")
+    g.write("Benchmark 25. Shear test of two identical spheres by applying rotation and radius expansion\n")
+    g.close()
+    
+    if 'FAILED' in open('errors.txt').read():
+        failure = True
+        
     g = open("errors.txt")
     file_contents = g.read()
     g.close()
@@ -90,6 +121,9 @@ def Run():
     Text += "\n\n\n"
     
     # To send an email summary to the DEM Team
+    #list_of_failed_tests = ', '.join(str(e) for e in list_of_failed_tests)
+    #subject = "DEM Benchmarks Results. Problems found in tests number "
+    #subject += list_of_failed_tests
     recipients = ["latorre@cimne.upc.edu"]
     recipients += ["maceli@cimne.upc.edu", "msantasusana@cimne.upc.edu", "gcasas@cimne.upc.edu", "farrufat@cimne.upc.edu", "jirazabal@cimne.upc.edu"]
     subject = "Problems found in DEM Benchmarks"   
