cf81c58d0555f1cd316f1b9924d11d0422517d7f
msantasusana <msantasusana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-10-27 13:05:26 +0000

    new search dem_fem

--- kratos/applications/DEM_application/custom_python/add_custom_utilities_to_python.cpp
+++ kratos/applications/DEM_application/custom_python/add_custom_utilities_to_python.cpp
@@ -62,7 +62,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "custom_utilities/post_utilities.h"
 #include "custom_utilities/search_utilities.h"
 #include "custom_utilities/omp_dem_search.h"
-#include "custom_utilities/dem_fem_utilities.h"
 #include "custom_utilities/benchmark_utilities.h"
 #include "custom_utilities/inlet.h"
 
