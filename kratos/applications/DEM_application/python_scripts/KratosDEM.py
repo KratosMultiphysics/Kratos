ea89408447a52b58b2443a9141ad1fa264cce9c8
msantasusana <msantasusana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-10-27 12:12:56 +0000

    only one constructor

--- kratos/applications/DEM_application/python_scripts/KratosDEM.py
+++ kratos/applications/DEM_application/python_scripts/KratosDEM.py
@@ -74,12 +74,14 @@ contact_model_part    = ""
 #EXTRA ModelPart Operations
 #
 
-# Constructing a creator/destructor object
+# Constructing a utilities objects
 creator_destructor = ParticleCreatorDestructor()
+dem_fem_search = DEM_FEM_Search()
 #
 
 # Creating a solver object and set the search strategy
-solver                 = SolverStrategy.ExplicitStrategy(spheres_model_part, rigid_face_model_part, cluster_model_part, DEM_inlet_model_part, creator_destructor, DEM_parameters)
+
+solver = SolverStrategy.ExplicitStrategy(spheres_model_part, rigid_face_model_part, cluster_model_part, DEM_inlet_model_part, creator_destructor, dem_fem_search, DEM_parameters)
 
 # Add variables
 procedures.AddCommonVariables(spheres_model_part, DEM_parameters)
@@ -357,8 +359,7 @@ while ( time < DEM_parameters.FinalTime):
         if (DEM_parameters.ContactMeshOption == "ON"):
             solver.PrepareContactElementsForPrinting()
         
-        demio.PrintResults(mixed_model_part, spheres_model_part, rigid_face_model_part, cluster_model_part, contact_model_part, mapping_model_part, creator_destructor, time)
-                
+        demio.PrintResults(mixed_model_part, spheres_model_part, rigid_face_model_part, cluster_model_part, contact_model_part, mapping_model_part, creator_destructor, dem_fem_search, time)
         os.chdir(main_path)
 
         time_old_print = time
