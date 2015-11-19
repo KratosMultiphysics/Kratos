6bf0b8d742ad3280e1da5838674973f6ab3c5c81
pbecker <pbecker@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-11-19 17:40:56 +0000

    Reimplementing the ConvDiffApplication. Check PecletTest for usage

---
+++ kratos/applications/convection_diffusion_application/test_examples/PecletTest/ProjectParameters.py
@@ -0,0 +1,9 @@
+class ConvectionSolverSettings:
+    unknown_variable = "TEMPERATURE"
+    diffusion_variable = "CONDUCTIVITY"
+    velocity_variable = "VELOCITY"
+
+Dt = 0.001
+Start_time = 0.0
+max_time = 0.1
+output_time = 0.1
