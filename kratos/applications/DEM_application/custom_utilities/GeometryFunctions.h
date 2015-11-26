4637df1c6222a694ce08932b0e0141fcecb8ea75
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-11-26 19:09:12 +0000

    Dividing the MoveAllMeshes function in dem_fem_utilities.cpp into 3 smaller subfunctions (translation, rotation and update kinematics variables) defined now in GeometryFunctions.h.

--- kratos/applications/DEM_application/custom_utilities/GeometryFunctions.h
+++ kratos/applications/DEM_application/custom_utilities/GeometryFunctions.h
@@ -10,11 +10,39 @@
 
 
 #include <cmath>
+#include "utilities/openmp_utils.h"
 
-namespace Kratos
-{
-    namespace GeometryFunctions
-    {
+namespace Kratos {
+    
+    namespace GeometryFunctions {
+        
+    static void TranslateGridOfNodes(const double time, const double velocity_start_time, const double velocity_stop_time, array_1d<double, 3>& center_position,
+                                     const array_1d<double, 3> initial_center, array_1d<double, 3>& previous_displ, array_1d<double, 3>& linear_velocity_changed,
+                                     const double linear_period, const double dt, const array_1d<double, 3> linear_velocity) {
+
+        if (time < velocity_start_time || time > velocity_stop_time) {
+            center_position[0] = initial_center[0] + previous_displ[0];
+            center_position[1] = initial_center[1] + previous_displ[1];
+            center_position[2] = initial_center[2] + previous_displ[2];
+            linear_velocity_changed = ZeroVector(3);
+        } else {
+            if (linear_period > 0.0) {
+                double linear_omega = 2.0 * KRATOS_M_PI / linear_period;
+                double inv_linear_omega = 1.0 / linear_omega;
+                noalias(center_position) = initial_center + linear_velocity * sin(linear_omega * time) * inv_linear_omega;
+                noalias(linear_velocity_changed) = linear_velocity * cos(linear_omega * time);
+                noalias(previous_displ) = center_position - initial_center;
+            } else {
+                center_position[0] = initial_center[0] + previous_displ[0] + dt * linear_velocity[0];
+                center_position[1] = initial_center[1] + previous_displ[1] + dt * linear_velocity[1];
+                center_position[2] = initial_center[2] + previous_displ[2] + dt * linear_velocity[2];
+                previous_displ[0] += dt * linear_velocity[0];
+                previous_displ[1] += dt * linear_velocity[1];
+                previous_displ[2] += dt * linear_velocity[2];
+                linear_velocity_changed = linear_velocity;
+            }
+        }
+    }
 
     static inline int sign(double a)
     {
@@ -214,11 +242,155 @@ namespace Kratos
         ReturnVector[2] = u[0]*v[1] - u[1]*v[0];
     }
 
+    static inline void RotateRightHandedBasisAroundAxis(const array_1d<double, 3 >& e1,  const array_1d<double, 3 >& e2,  const array_1d<double, 3 >& axis,
+                                                           const double ang, array_1d<double, 3 >& new_axes1, array_1d<double, 3 >& new_axes2,
+                                                           array_1d<double, 3 >& new_axes3) {
+        double cang = cos(ang);
+        double sang = sin(ang);
+            
+        new_axes1[0] = axis[0] * (axis[0] * e1[0] + axis[1] * e1[1] + axis[2] * e1[2]) * (1 - cang) + e1[0] * cang + (- axis[2] * e1[1] + axis[1] * e1[2]) * sang;
+        new_axes1[1] = axis[1] * (axis[0] * e1[0] + axis[1] * e1[1] + axis[2] * e1[2]) * (1 - cang) + e1[1] * cang +   (axis[2] * e1[0] - axis[0] * e1[2]) * sang;
+        new_axes1[2] = axis[2] * (axis[0] * e1[0] + axis[1] * e1[1] + axis[2] * e1[2]) * (1 - cang) + e1[2] * cang + (- axis[1] * e1[0] + axis[0] * e1[1]) * sang;
+          
+        new_axes2[0] = axis[0] * (axis[0] * e2[0] + axis[1] * e2[1] + axis[2] * e2[2]) * (1 - cang) + e2[0] * cang + (- axis[2] * e2[1] + axis[1] * e2[2]) * sang;
+        new_axes2[1] = axis[1] * (axis[0] * e2[0] + axis[1] * e2[1] + axis[2] * e2[2]) * (1 - cang) + e2[1] * cang +   (axis[2] * e2[0] - axis[0] * e2[2]) * sang;
+        new_axes2[2] = axis[2] * (axis[0] * e2[0] + axis[1] * e2[1] + axis[2] * e2[2]) * (1 - cang) + e2[2] * cang + (- axis[1] * e2[0] + axis[0] * e2[1]) * sang;  
+            
+        CrossProduct(new_axes1, new_axes2, new_axes3);
+    }
+    
+    static inline void RotateGridOfNodes(const double time, const double angular_velocity_start_time, const double angular_velocity_stop_time,
+                                         array_1d<double, 3>& angular_velocity_changed, const double angular_period, const double mod_angular_velocity,
+                                         const array_1d<double, 3> angular_velocity, array_1d<double, 3>& new_axes1, array_1d<double, 3>& new_axes2,
+                                         array_1d<double, 3>& new_axes3) {
+        
+        array_1d<double, 3> angle = ZeroVector(3);
+        double sign_angle = 1.0;
+        array_1d<double, 3> final_angle = ZeroVector(3);                       
+        
+        if (time < angular_velocity_start_time) angular_velocity_changed = ZeroVector(3);
+
+        else if (((time - angular_velocity_start_time) > 0.0) && ((time - angular_velocity_stop_time) < 0.0)) {
+
+            if (angular_period > 0.0) {
+                double angular_omega = 2.0 * KRATOS_M_PI / angular_period;
+                double inv_angular_omega = 1.0 / angular_omega;
+                noalias(angle) = angular_velocity * sin(angular_omega * (time - angular_velocity_start_time)) * inv_angular_omega;
+                sign_angle = sin(angular_omega * (time - angular_velocity_start_time)) / fabs(sin(angular_omega * (time - angular_velocity_start_time)));
+                noalias(angular_velocity_changed) = angular_velocity * cos(angular_omega * (time - angular_velocity_start_time));
+                final_angle = angle;
+            } else {
+                noalias(angle) = angular_velocity * (time - angular_velocity_start_time);
+                angular_velocity_changed = angular_velocity;
+            }
+        } else { //if ((time - angular_velocity_stop_time) > 0.0) {
+            angular_velocity_changed = ZeroVector(3);
+
+            if (angular_period > 0.0) {
+                angle = final_angle;
+            } else {
+                noalias(angle) = angular_velocity * (angular_velocity_stop_time - angular_velocity_start_time);
+            }
+        }
+
+        //mod_angular_velocity = MathUtils<double>::Norm3(angular_velocity);
+
+        new_axes1[0] = 1.0;
+        new_axes1[1] = 0.0;
+        new_axes1[2] = 0.0;
+
+        new_axes2[0] = 0.0;
+        new_axes2[1] = 1.0;
+        new_axes2[2] = 0.0;
+
+        new_axes3[0] = 0.0;
+        new_axes3[1] = 0.0;
+        new_axes3[2] = 1.0;
+
+        if (mod_angular_velocity > 0.0) {
+
+            double ang = sign_angle * MathUtils<double>::Norm3(angle);
+            array_1d<double, 3> rotation_axis;
+            noalias(rotation_axis) = angular_velocity / mod_angular_velocity;
+            array_1d<double, 3> e1;
+            e1[0] = 1.0;
+            e1[1] = 0.0;
+            e1[2] = 0.0;
+
+            array_1d<double, 3> e2;
+            e2[0] = 0.0;
+            e2[1] = 1.0;
+            e2[2] = 0.0;
+
+            RotateRightHandedBasisAroundAxis(e1, e2, rotation_axis, ang, new_axes1, new_axes2, new_axes3);
+        }
+    }
+    
+    static inline void UpdateKinematicVariablesOfAGridOfNodes(double mod_angular_velocity, array_1d<double, 3> linear_velocity,
+                                                       const array_1d<double, 3> initial_center, array_1d<double, 3> new_axes1, array_1d<double, 3> new_axes2,
+                                                       array_1d<double, 3> new_axes3, array_1d<double, 3> angular_velocity_changed,
+                                                       array_1d<double, 3> linear_velocity_changed, array_1d<double, 3> center_position,
+                                                       const bool fixed_mesh, const double dt, ModelPart::NodesContainerType& pNodes)
+    {
+        if (mod_angular_velocity > 0.0 || MathUtils<double>::Norm3(linear_velocity) > 0.0) {
+
+            vector<unsigned int> node_partition;
+
+            #ifdef _OPENMP
+            int number_of_threads = omp_get_max_threads();
+            #else
+            int number_of_threads = 1;
+            #endif
+            OpenMPUtils::CreatePartition(number_of_threads, pNodes.size(), node_partition);
+
+            #pragma omp parallel for
+            for (int k = 0; k < number_of_threads; k++) {
+
+                array_1d<double, 3> local_coordinates;
+                local_coordinates[0] = 0.0;
+                local_coordinates[1] = 0.0;
+                local_coordinates[2] = 0.0;
+                array_1d<double, 3> relative_position;
+                relative_position[0] = 0.0;
+                relative_position[1] = 0.0;
+                relative_position[2] = 0.0;
+
+                ModelPart::NodesContainerType::iterator i_begin = pNodes.ptr_begin() + node_partition[k];
+                ModelPart::NodesContainerType::iterator i_end = pNodes.ptr_begin() + node_partition[k + 1];
+
+                for (ModelPart::NodeIterator node = i_begin; node != i_end; ++node) {
+
+                    noalias(local_coordinates) = node->GetInitialPosition().Coordinates() - initial_center;
+                    noalias(relative_position) = new_axes1 * local_coordinates[0] + new_axes2 * local_coordinates[1] + new_axes3 * local_coordinates[2];
+
+                    array_1d<double, 3> displacement;
+                    array_1d<double, 3> old_coordinates;
+                    noalias(old_coordinates) = node->Coordinates();
+                    array_1d<double, 3> velocity_due_to_rotation;
+                    
+                    CrossProduct(angular_velocity_changed, relative_position, velocity_due_to_rotation);
+                               
+                    array_1d<double, 3>& velocity = node->FastGetSolutionStepValue(VELOCITY);
+                    noalias(velocity) = linear_velocity_changed + velocity_due_to_rotation;  
+                    if (!fixed_mesh) {
+                        // NEW POSITION
+                        noalias(node->Coordinates()) = center_position + relative_position;
+                        // DISPLACEMENT
+                        noalias(node->FastGetSolutionStepValue(DISPLACEMENT)) = node->Coordinates() - node->GetInitialPosition().Coordinates();
+                        noalias(node->FastGetSolutionStepValue(DELTA_DISPLACEMENT)) = node->Coordinates() - old_coordinates;
+                    } else {
+                        (node->FastGetSolutionStepValue(DISPLACEMENT)).clear(); //Set values to zero
+                        noalias(node->FastGetSolutionStepValue(DELTA_DISPLACEMENT)) = velocity * dt; //But still there must be some delta_displacement (or motion won't be detected by the spheres!)
+                    }
+                }
+            }
+        }
+    }
    //NOTE:: Modified by M. Santasusana Feb 2013 - simplification (the one proposed by F.Chun was for a more generalized case)
     static inline void ComputeContactLocalCoordSystem(array_1d<double,3> NormalDirection, const double& distance, double LocalCoordSystem[3][3])  //inline: modifies the LocalCoordSystem as it were a reference
     {
 
-        double inv_distance = (distance != 0.0) ?  1.0 / distance : 0.00;
+        double inv_distance = (distance != 0.0) ? 1.0 / distance : 0.0;
         NormalDirection[0] *= inv_distance;
         NormalDirection[1] *= inv_distance;
         NormalDirection[2] *= inv_distance;
@@ -387,8 +559,8 @@ namespace Kratos
     static inline void Compute3DimElementFaceLocalSystem(double FaceCoord1[3], double FaceCoord2[3], double FaceCoord3[3], double ParticleCoord[3], 
                                                          double LocalCoordSystem[3][3], double& normal_flag){
          
-        //NOTE: this function is designed in a way that the normal always points the side where the centre of particle is found. Therefore should only be used in this way if the indentation is less than the radius value.
-                //the fucntion returns a flag with the same value as the dotproduct of the normal of the triangle and the normal pointing to the particle.
+        //NOTE: this function is designed in a way that the normal always points the side where the center of particle is found. Therefore should only be used in this way if the indentation is less than the radius value.
+                //the function returns a flag with the same value as the dot product of the normal of the triangle and the normal pointing to the particle.
          
          double Vector1[3] = {0.0};
          double Vector2[3] = {0.0};
@@ -1199,8 +1371,8 @@ namespace Kratos
 
                 bisection[index] = -bisection[index];
             }
-            dot_product = -dot_product;
 
+            dot_product = -dot_product;
         }
 
         module(a,norm_a);
@@ -1243,15 +1415,15 @@ namespace Kratos
 
     static inline void AreaAndCentroidCircularSegment(double Centre[3], double Radius, double tol_Radius, double V0[3], double V1[3], double Normal[3], double& AreaSegC, double CoMSegC[3], bool& flag)
     {
-        double V0V1[3]            = {0.0};  double V0CC[3]      = {0.0};
+        double V0V1[3]            = {0.0};
+        double V0CC[3]            = {0.0};
         double a[3]               = {0.0};
         double normal_outwards[3] = {0.0};
         double Radius_SQ = 0.0;
         double distance_V0V1      = 0.0;
         double dist_CoM           = 0.0;
+        AreaSegC                  =  0.0 ;
         flag = false;
-
-        AreaSegC = 0.0;
 
         for (unsigned int index = 0; index<3; index++){
 
