14551695079da2fbc4d763f448805e34f604c4aa
jirazabal <jirazabal@cimne.upc.edu>
2015-12-02 09:21:33 +0000

    Inlet error fixed

--- applications/swimming_DEM_application/python_scripts/KratosSwimmingDEM.py
+++ applications/swimming_DEM_application/python_scripts/KratosSwimmingDEM.py
@@ -186,6 +186,15 @@ vars_man.AddNodalVariables(spheres_model_part, pp.dem_vars)
 vars_man.AddNodalVariables(rigid_face_model_part, pp.rigid_faces_vars)
 vars_man.AddNodalVariables(DEM_inlet_model_part, pp.inlet_vars)
 
+# If Lalplacian form = 2, free all pressure Dofs
+# laplacian_form = pp.laplacian_form
+# if(laplacian_form >= 2):
+    # for node in fluid_model_part.Nodes:
+        # node.Free(PRESSURE)
+
+# adding extra process info variables
+vars_man.AddingDEMProcessInfoVariables(pp, spheres_model_part)
+
 # defining a model part for the mixed part
 mixed_model_part = ModelPart("MixedPart")
 
@@ -214,7 +223,6 @@ model_part_io_demInlet.ReadModelPart(DEM_inlet_model_part)
 # Setting up the buffer size
 if pp.swim.virtual_mass_force_type  > 0:
     buffer_size = 2
-
 else:
     buffer_size = 1
 
@@ -222,43 +230,76 @@ spheres_model_part.SetBufferSize(buffer_size)
 cluster_model_part.SetBufferSize(buffer_size)
 DEM_inlet_model_part.SetBufferSize(buffer_size)
 rigid_face_model_part.SetBufferSize(1)
+fluid_model_part.SetBufferSize(3)
 
 # Adding dofs
 solver.AddDofs(spheres_model_part)
 solver.AddDofs(cluster_model_part)
 solver.AddDofs(DEM_inlet_model_part)
-
-# adding extra process info variables
-vars_man.AddingDEMProcessInfoVariables(pp, spheres_model_part)
-
-#_____________________________________________________________________________________________________________________________________
-#
-#                               F L U I D    B L O C K    B E G I N S
-#_____________________________________________________________________________________________________________________________________
-
-
-# setting up the buffer size: SHOULD BE DONE AFTER READING!!!
-fluid_model_part.SetBufferSize(3)
-
-# adding nodal degrees of freedom
 solver_module.AddDofs(fluid_model_part, SolverSettings)
-
-# If Lalplacian form = 2, free all pressure Dofs
-# laplacian_form = pp.laplacian_form
-# if(laplacian_form >= 2):
-    # for node in fluid_model_part.Nodes:
-        # node.Free(PRESSURE)
 
 # copy Y_WALL
 for node in fluid_model_part.Nodes:
     y = node.GetSolutionStepValue(Y_WALL, 0)
     node.SetValue(Y_WALL, y)
 
+# Creating necessary directories
+main_path = os.getcwd()
+[post_path,data_and_results,graphs_path,MPI_results] = procedures.CreateDirectories(str(main_path),str(DEM_parameters.problem_name))
+
+os.chdir(main_path)
+
+KRATOSprint("Initializing Problem....")
+
+# Initialize GiD-IO
+demio.AddGlobalVariables()
+demio.AddSpheresVariables()
+demio.AddFEMBoundaryVariables()
+demio.AddClusterVariables()
+demio.AddContactVariables()
 #
-#
+demio.AddMpiVariables()
+demio.EnableMpiVariables()
+
+demio.Configure(DEM_parameters.problem_name,
+                DEM_parameters.OutputFileType,
+                DEM_parameters.Multifile,
+                DEM_parameters.ContactMeshOption)
+
+demio.SetOutputName(DEM_parameters.problem_name)
+
+os.chdir(post_path)
+
+demio.InitializeMesh(mixed_model_part,
+                     spheres_model_part,
+                     rigid_face_model_part,
+                     cluster_model_part,
+                     contact_model_part,
+                     mapping_model_part)
+
+os.chdir(post_path)
+
+# Perform a partition to balance the problem
+parallelutils.Repart(spheres_model_part)
+parallelutils.CalculateModelNewIds(spheres_model_part)
+
+os.chdir(post_path)
+
+#Setting up the BoundingBox
+bounding_box_time_limits = []
+if (DEM_parameters.BoundingBoxOption == "ON"):
+    procedures.SetBoundingBox(spheres_model_part, cluster_model_part, rigid_face_model_part, creator_destructor)
+    bounding_box_time_limits = [solver.bounding_box_start_time, solver.bounding_box_stop_time]
+
+# Creating a solver object and set the search strategy
+#solver                 = SolverStrategy.ExplicitStrategy(spheres_model_part, rigid_face_model_part, cluster_model_part, DEM_inlet_model_part, creator_destructor, DEM_parameters)
+solver.search_strategy = parallelutils.GetSearchStrategy(solver, spheres_model_part)
+
 # Creating the fluid solver
 fluid_solver = solver_module.CreateSolver(
     fluid_model_part, SolverSettings)
+
+Dt_DEM = DEM_parameters.MaxTimeStep
 
 # activate turbulence model
 if(pp.FluidSolverConfiguration.TurbulenceModel == "Spalart-Allmaras"):
@@ -283,30 +324,23 @@ if(pp.FluidSolverConfiguration.TurbulenceModel == "Spalart-Allmaras"):
             node.SetSolutionStepValue(TURBULENT_VISCOSITY, 0, 0.0)
             node.Fix(TURBULENT_VISCOSITY)
 
+solver.Initialize()    # Possible modifications of DELTA_TIME
 
 fluid_solver.Initialize()
 print("fluid solver created")
 sys.stdout.flush()
 
-# initialize GiD  I/O
-from gid_output import GiDOutput
-gid_io = GiDOutput(input_file_name,
-                   pp.VolumeOutput,
-                   pp.GiDPostMode,
-                   pp.GiDMultiFileFlag,
-                   pp.GiDWriteMeshFlag,
-                   pp.GiDWriteConditionsFlag)
+if (DEM_parameters.ContactMeshOption =="ON"):
+    contact_model_part = solver.contact_model_part
+  
+# constructing a model part for the DEM inlet. it contains the DEM elements to be released during the simulation  
+# Initializing the DEM solver must be done before creating the DEM Inlet, because the Inlet configures itself according to some options of the DEM model part
 
 if not pp.VolumeOutput:
     cut_list = define_output.DefineCutPlanes()
     gid_io.define_cuts(fluid_model_part, cut_list)
 
 # gid_io.initialize_results(fluid_model_part) # MOD.
-
-#_____________________________________________________________________________________________________________________________________
-#
-#                               F L U I D    B L O C K    E N D S
-#_____________________________________________________________________________________________________________________________________
 
 import swimming_DEM_gid_output
 swimming_DEM_gid_io = swimming_DEM_gid_output.SwimmingDEMGiDOutput(input_file_name,
@@ -317,11 +351,6 @@ swimming_DEM_gid_io = swimming_DEM_gid_output.SwimmingDEMGiDOutput(input_file_na
                                                                    pp.GiDWriteConditionsFlag)
 
 swimming_DEM_gid_io.initialize_swimming_DEM_results(spheres_model_part, cluster_model_part, rigid_face_model_part, mixed_model_part)
-
-#_____________________________________________________________________________________________________________________________________
-#
-#                               F L U I D    B L O C K    B E G I N S
-#_____________________________________________________________________________________________________________________________________
 
 # define the drag computation list
 drag_list = define_output.DefineDragList()
@@ -371,11 +400,6 @@ def PrintDrag(drag_list, drag_file_output_list, fluid_model_part, time):
     #fluid_model_part,
     #variables_dictionary,
     #domain_size)
-
-#_____________________________________________________________________________________________________________________________________
-#
-#                               F L U I D    B L O C K    E N D S
-#_____________________________________________________________________________________________________________________________________
 
 # setting fluid's body force to the same as DEM's
 if pp.swim.body_force_on_fluid_option:
@@ -430,20 +454,6 @@ stationarity_tool = swim_proc.StationarityAssessmentTool(pp.swim.max_pressure_va
 # creating a debug tool
 dem_volume_tool = swim_proc.ProjectionDebugUtils(pp.swim.fluid_domain_volume, fluid_model_part, spheres_model_part, custom_functions_tool)
 
-# creating a CreatorDestructor object, responsible for any adding or removing of elements during the simulation
-creator_destructor = ParticleCreatorDestructor()
-dem_fem_search = DEM_FEM_Search()
-
-# setting up a bounding box for the DEM balls (it is used for erasing remote balls)
-procedures.SetBoundingBox(spheres_model_part, cluster_model_part, rigid_face_model_part, creator_destructor)
-
-# creating a Solver object for the DEM part. It contains the sequence of function calls necessary for the evolution of the DEM system at every time step
-dem_solver = SolverStrategy.ExplicitStrategy(spheres_model_part, rigid_face_model_part, cluster_model_part, DEM_inlet_model_part, creator_destructor, dem_fem_search, DEM_parameters)
-
-# Initializing the DEM solver (must be done before creating the DEM Inlet, because the Inlet configures itself according to some options of the DEM model part)
-dem_solver.search_strategy = parallelutils.GetSearchStrategy(dem_solver, spheres_model_part)
-dem_solver.Initialize()
-
 # creating a distance calculation process for the embedded technology
 # (used to calculate elemental distances defining the structure embedded in the fluid mesh)
 if (pp.swim.embedded_option):
@@ -452,51 +462,45 @@ if (pp.swim.embedded_option):
 
 # constructing a model part for the DEM inlet. it contains the DEM elements to be released during the simulation
 
-if pp.swim.inlet_option:
-    
-    vars_man.AddingDEMProcessInfoVariables(pp, DEM_inlet_model_part)
+if (DEM_parameters.dem_inlet_option):    
     max_DEM_node_Id = creator_destructor.FindMaxNodeIdInModelPart(spheres_model_part)
     max_FEM_node_Id = creator_destructor.FindMaxNodeIdInModelPart(rigid_face_model_part)
     max_fluid_node_Id = swim_proc.FindMaxNodeIdInFLuid(fluid_model_part)
     max_node_Id = max(max_DEM_node_Id, max_FEM_node_Id, max_fluid_node_Id)
-    
-    creator_destructor.SetMaxNodeId(max_node_Id)      
 
-    for properties in DEM_inlet_model_part.Properties:
-        DiscontinuumConstitutiveLawString = properties[DEM_DISCONTINUUM_CONSTITUTIVE_LAW_NAME];
-        DiscontinuumConstitutiveLaw = globals().get(DiscontinuumConstitutiveLawString)()
-        DiscontinuumConstitutiveLaw.SetConstitutiveLawInProperties(properties)
-
-    # constructiong the inlet and intializing it (must be done AFTER the spheres_model_part Initialize)
-    DEM_inlet = DEM_Inlet(DEM_inlet_model_part)
+    creator_destructor.SetMaxNodeId(max_node_Id)                            
+        
+    # constructing the inlet and initializing it (must be done AFTER the spheres_model_part Initialize)    
+    DEM_inlet = DEM_Inlet(DEM_inlet_model_part)    
     DEM_inlet.InitializeDEM_Inlet(spheres_model_part, creator_destructor)
 
-# Creating necessary directories
-main_path = os.getcwd()
-[post_path,data_and_results,graphs_path,MPI_results] = procedures.CreateDirectories(str(main_path),str(DEM_parameters.problem_name))
-#_____________________________________________________________________________________________________________________________________
-#
-#                               F L U I D    B L O C K    B E G I N S
-#_____________________________________________________________________________________________________________________________________
+DEMFEMProcedures = DEM_procedures.DEMFEMProcedures(DEM_parameters, graphs_path, spheres_model_part, rigid_face_model_part)
 
-# renumerating IDs if required
 
-# swim_proc.RenumberNodesIdsToAvoidRepeating(fluid_model_part, spheres_model_part, rigid_face_model_part)
+KRATOSprint("Initialization Complete" + "\n")
 
-# Stepping and time settings
+step           = 0
+time           = pp.Start_time
+Dt             = pp.Dt
+out            = Dt
+Nsteps         = pp.nsteps
+final_time     = pp.max_time
+output_time    = pp.output_time
 
-Dt = pp.Dt
-Nsteps = pp.nsteps
-final_time = pp.max_time
-output_time = pp.output_time
+report.Prepare(timer, DEM_parameters.ControlTime)
 
-time = pp.Start_time
-out = Dt
-step = 0
-#_____________________________________________________________________________________________________________________________________
-#
-#                               F L U I D    B L O C K    E N D S
-#_____________________________________________________________________________________________________________________________________
+first_print = True; index_5 = 1; index_10 = 1; index_50 = 1; control = 0.0
+    
+## MODEL DATA #~CHARLIE~#:????
+
+if (DEM_parameters.ModelDataInfo == "ON"):
+    os.chdir(data_and_results)
+    if (DEM_parameters.ContactMeshOption == "ON"):
+        (coordination_number) = procedures.ModelData(spheres_model_part, contact_model_part, solver) # Calculates the mean number of neighbours the mean radius, etc..
+        KRATOSprint ("Coordination Number: " + str(coordination_number) + "\n")
+        os.chdir(main_path)
+    else:
+        KRATOSprint("Activate Contact Mesh for ModelData information")
 
 if pp.swim.flow_in_porous_medium_option:
     fluid_frac_util = swim_proc.FluidFractionFieldUtility(fluid_model_part, pp.swim.min_fluid_fraction )
@@ -551,14 +555,6 @@ particles_results_counter = swim_proc.Counter(pp.swim.print_particles_results_cy
 #meshing_utils.ModifyRadiusesGivenPorosity(model_part, porosity, tol)
 #Z
 
-if (DEM_parameters.ModelDataInfo == "ON"):
-    os.chdir(data_and_results)
-    if (DEM_parameters.ContactMeshOption == "ON"):
-        (coordination_number) = procedures.ModelData(spheres_model_part, contact_model_part, solver) # Calculates the mean number of neighbours the mean radius, etc..
-        KRATOSprint ("Coordination Number: " + str(coordination_number) + "\n")
-        os.chdir(main_path)
-    else:
-        KRATOSprint("Activate Contact Mesh for ModelData information")
 
 ##############################################################################
 #                                                                            #
@@ -584,7 +580,7 @@ post_utils_DEM = DEM_procedures.PostUtils(DEM_parameters, spheres_model_part)
 
 swim_proc.InitializeVariablesWithNonZeroValues(fluid_model_part, spheres_model_part) # all variables are set to 0 by default
 
-while time <= final_time:
+while (time <= final_time):
 
     time = time + Dt
     step += 1
@@ -597,9 +593,6 @@ while time <= final_time:
 
     else:
         time_final_DEM_substepping = time
-
-    # walls movement
-    mesh_motion.MoveAllMeshes(rigid_face_model_part, time, Dt)
 
     # calculating elemental distances defining the structure embedded in the fluid mesh
     if pp.swim.embedded_option:
@@ -659,7 +652,9 @@ while time <= final_time:
 
         DEM_step += 1   # this variable is necessary to get a good random insertion of particles
 
-        spheres_model_part.ProcessInfo[TIME_STEPS] = DEM_step
+        spheres_model_part.ProcessInfo[TIME_STEPS]    = DEM_step
+        rigid_face_model_part.ProcessInfo[TIME_STEPS] = DEM_step
+        cluster_model_part.ProcessInfo[TIME_STEPS]    = DEM_step
 
         # applying fluid-to-DEM coupling if required
 
@@ -673,16 +668,25 @@ while time <= final_time:
 
         # performing the time integration of the DEM part
 
-        spheres_model_part.CloneTimeStep(time_dem)
+        spheres_model_part.ProcessInfo[TIME]    = time_dem
+        rigid_face_model_part.ProcessInfo[TIME] = time_dem
+        cluster_model_part.ProcessInfo[TIME]    = time_dem
 
         if not pp.swim.flow_in_porous_DEM_medium_option: # in porous flow particles remain static
-            dem_solver.Solve()
+            solver.Solve()
 
-    # adding DEM elements by the inlet:
-    if (DEM_parameters.dem_inlet_option):
-        DEM_inlet.CreateElementsFromInletMesh(spheres_model_part, cluster_model_part, creator_destructor)  # After solving, to make sure that neighbours are already set.              
+        # Walls movement:
+        mesh_motion.MoveAllMeshes(rigid_face_model_part, time, Dt)
+        mesh_motion.MoveAllMeshes(spheres_model_part, time, Dt)
+        mesh_motion.MoveAllMeshes(DEM_inlet_model_part, time, Dt)
+
+        #### TIME CONTROL ##################################
     
-        first_dem_iter = False
+        # adding DEM elements by the inlet:
+        if (DEM_parameters.dem_inlet_option):
+            DEM_inlet.CreateElementsFromInletMesh(spheres_model_part, cluster_model_part, creator_destructor)  # After solving, to make sure that neighbours are already set.              
+
+        #first_dem_iter = False
     #### PRINTING GRAPHS ####
     os.chdir(graphs_path)
     # measuring mean velocities in a certain control volume (the 'velocity trap')
@@ -710,8 +714,8 @@ while time <= final_time:
         
         # eliminating remote balls
         
-        if pp.dem.BoundingBoxOption == "ON":
-            creator_destructor.DestroyParticlesOutsideBoundingBox(spheres_model_part)
+        #if pp.dem.BoundingBoxOption == "ON":
+            #creator_destructor.DestroyParticlesOutsideBoundingBox(spheres_model_part)
             
         io_tools.PrintParticlesResults(pp.variables_to_print_in_file, time, spheres_model_part)
         graph_printer.PrintGraphs(time)
