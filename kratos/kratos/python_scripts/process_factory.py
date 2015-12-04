5f126318111cc9c20ae48e8d386ec260bf71357f
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2015-12-04 15:00:48 +0000

    small modifications to allow constructing basic processes to apply variables

--- kratos/kratos/python_scripts/process_factory.py
+++ kratos/kratos/python_scripts/process_factory.py
@@ -1,5 +1,6 @@
 from KratosMultiphysics import *
 
+#################33please do not change the following class
 class KratosProcessFactory(object):
     def __init__(self, Model):
         self.Model = Model #model is a place
@@ -7,21 +8,68 @@ class KratosProcessFactory(object):
     def ConstructListOfProcesses( self, process_list ):
         constructed_processes = []
         for item in process_list:
-            if( "implemented_in_python" in item): #check if implemented in python or in c++
-                if item["implemented_in_python"] == True: #here we treat the case of python implemented processes
-                    module = __import__(item["implemented_in_module"])
-                    interface_file = __import__(item["implemented_in_file"])
-                    p = interface_file.Factory(item, self.Model)
-                    constructed_processes.append( p )
+            module = __import__(item["implemented_in_module"])
+            interface_file = __import__(item["implemented_in_file"])
+            p = interface_file.Factory(item, self.Model)
+            constructed_processes.append( p )
+            #if( "implemented_in_python" in item): #check if implemented in python or in c++
+                #if item["implemented_in_python"] == True: #here we treat the case of python implemented processes
+                    #module = __import__(item["implemented_in_module"])
+                    #interface_file = __import__(item["implemented_in_file"])
+                    #p = interface_file.Factory(item, self.Model)
+                    #constructed_processes.append( p )
                     
-                else: #here we create c++ processes
-                    module = __import__(item["implemented_in_module"])
-                    interface_file = __import__(item["implemented_in_file"])
-                    p = interface_file.Factory(item, self.Model)
-                    constructed_processes.append( p )
+                #else: #here we create c++ processes
+                    #module = __import__(item["implemented_in_module"])
+                    #interface_file = __import__(item["implemented_in_file"])
+                    #p = interface_file.Factory(item, self.Model)
+                    #constructed_processes.append( p )
                     
-            else:
-                raise Exception( "process defined by settings ", item, " does not define if implemented in python or in c++")
+            #else:
+                #raise Exception( "process defined by settings ", item, " does not define if implemented in python or in c++")
             
     
         return constructed_processes
+    
+    
+########## here we generate the common kratos processes --- IMPLEMENTED IN C++ ###################
+def Factory(settings, Model):
+    
+    if(settings["process_name"] == "ApplyConstantScalarValueProcess"):
+        params = settings["parameters"]
+        model_part = Model.get(  params.get( "model_part_name", "not found!!" ) , "model part not found" )
+        mesh_id = params["mesh_id"]
+        variable_name = params["variable_name"] 
+        value = params["value"] 
+        is_fixed = params["is_fixed"]
+    
+        options = Flags()
+        options.Set(ApplyConstantScalarValueProcess.VARIABLE_IS_FIXED,  is_fixed)
+        
+      
+        return ApplyConstantScalarValueProcess(model_part, variable_name, value,mesh_id,options)
+    
+    elif(settings["process_name"] == "ApplyConstantVectorValueProcess"):
+        params = settings["parameters"]
+        model_part = Model.get(  params.get( "model_part_name", "not found!!" ) , "model part not found" )
+        mesh_id = params["mesh_id"]
+        variable_name = params["variable_name"]  
+        value = params["value"]
+        factor = params["factor"]  
+        
+        is_fixed_x = params["is_fixed_x"]
+        is_fixed_y = params["is_fixed_y"]
+        is_fixed_z = params["is_fixed_z"]
+        
+        options = Flags()
+        options.Set(ApplyConstantVectorValueProcess.X_COMPONENT_FIXED,  is_fixed_x)
+        options.Set(ApplyConstantVectorValueProcess.Y_COMPONENT_FIXED,  is_fixed_y)
+        options.Set(ApplyConstantVectorValueProcess.Z_COMPONENT_FIXED,  is_fixed_z)
+        
+        print(variable_name)
+        print("11111")
+        print(mesh_id)
+        new_process = ApplyConstantVectorValueProcess(model_part, variable_name, factor, value,mesh_id, options)
+        print("aaa")
+        
+        return new_process
