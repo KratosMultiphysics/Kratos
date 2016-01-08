1eaae8b6958740f926392c7e1b3681e59a9d234b
Salva Latorre <latorre@cimne.upc.edu>
2016-01-08 10:38:50 +0000

    Added the corresponding virtual version of the ComputeParticleRotationalMoments function to the base class plus some aesthetics.

--- applications/DEM_application/custom_constitutive/DEM_continuum_constitutive_law.cpp
+++ applications/DEM_application/custom_constitutive/DEM_continuum_constitutive_law.cpp
@@ -35,27 +35,34 @@ namespace Kratos {
     }
 
     void DEMContinuumConstitutiveLaw::CalculateViscoDamping(double LocalRelVel[3],
-            double ViscoDampingLocalContactForce[3],
-            double indentation,
-            double equiv_visco_damp_coeff_normal,
-            double equiv_visco_damp_coeff_tangential,
-            bool sliding) {
+                                                            double ViscoDampingLocalContactForce[3],
+                                                            double indentation,
+                                                            double equiv_visco_damp_coeff_normal,
+                                                            double equiv_visco_damp_coeff_tangential,
+                                                            bool sliding) {
 
         //*** component-wise since localContactForce and RelVel have in principle no relationship.
         // The visco force can be higher than the contact force only if they go to the same direction. (in my opinion)
-        // But in oposite direction the visco damping can't overpass the force...
+        // But in opposite direction the visco damping can't overpass the force...
 
         KRATOS_TRY  
 
-                ViscoDampingLocalContactForce[2] = -equiv_visco_damp_coeff_normal * LocalRelVel[2];
+        ViscoDampingLocalContactForce[2] = -equiv_visco_damp_coeff_normal * LocalRelVel[2];
 
-            if (sliding == false) { //only applied when no sliding to help to the regularized friction law or the spring convergence
-                ViscoDampingLocalContactForce[0] = -equiv_visco_damp_coeff_tangential * LocalRelVel[0];
-                ViscoDampingLocalContactForce[1] = -equiv_visco_damp_coeff_tangential * LocalRelVel[1];
-            }
+        if (sliding == false) { //only applied when no sliding to help the regularized friction law or the spring convergence
+            ViscoDampingLocalContactForce[0] = -equiv_visco_damp_coeff_tangential * LocalRelVel[0];
+            ViscoDampingLocalContactForce[1] = -equiv_visco_damp_coeff_tangential * LocalRelVel[1];
+        }
 
-    KRATOS_CATCH("")
-
+        KRATOS_CATCH("")
     }
+    
+    void DEMContinuumConstitutiveLaw::ComputeParticleRotationalMoments(SphericContinuumParticle* element,
+                                                                       SphericContinuumParticle* neighbor,
+                                                                       double equiv_young,
+                                                                       double distance,
+                                                                       double calculation_area,
+                                                                       double LocalCoordSystem[3][3],
+                                                                       array_1d<double, 3>& mContactMoment) {}
 
 } //kratos
