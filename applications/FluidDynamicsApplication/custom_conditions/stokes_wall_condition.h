3cde1f8d710265e95a088b8f7a08cabc59a38e0d
rrossi <rrossi@cimne.upc.edu>
2016-01-11 08:18:46 +0000

    adding stokes wall condition

---
+++ applications/FluidDynamicsApplication/custom_conditions/stokes_wall_condition.h
@@ -0,0 +1,487 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Riccardo Rossi
+//
+
+
+#ifndef KRATOS_STOKES_WALL_CONDITION_H
+#define KRATOS_STOKES_WALL_CONDITION_H
+
+// System includes
+#include <string>
+#include <iostream>
+
+
+// External includes
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/serializer.h"
+#include "includes/condition.h"
+#include "includes/process_info.h"
+
+// Application includes
+#include "fluid_dynamics_application_variables.h"
+#include "includes/deprecated_variables.h"
+#include "includes/cfd_variables.h"
+
+namespace Kratos
+{
+///@addtogroup FluidDynamicsApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/// Implements a wall condition for the monolithic stokes formulation - based on BDF2.
+/**
+  It is intended to be used with the Stokes2D and Stokes3D elements and a BDF2 time integration scheme
+  @see Stokes3D
+ */
+template< unsigned int TDim, unsigned int TNumNodes = TDim >
+class StokesWallCondition : public Condition
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of StokesWallCondition
+    KRATOS_CLASS_POINTER_DEFINITION(StokesWallCondition);
+
+    typedef Node < 3 > NodeType;
+
+    typedef Properties PropertiesType;
+
+    typedef Geometry<NodeType> GeometryType;
+
+    typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
+
+    typedef Vector VectorType;
+
+    typedef Matrix MatrixType;
+
+    typedef std::size_t IndexType;
+
+    typedef std::size_t SizeType;
+
+    typedef std::vector<std::size_t> EquationIdVectorType;
+
+    typedef std::vector< Dof<double>::Pointer > DofsVectorType;
+
+    typedef PointerVectorSet<Dof<double>, IndexedObject> DofsArrayType;
+
+    typedef VectorMap<IndexType, DataValueContainer> SolutionStepsConditionalDataContainerType;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    /** Admits an Id as a parameter.
+      @param NewId Index for the new condition
+      */
+    StokesWallCondition(IndexType NewId = 0):
+        Condition(NewId)
+    {
+    }
+
+    /// Constructor using an array of nodes
+    /**
+     @param NewId Index of the new condition
+     @param ThisNodes An array containing the nodes of the new condition
+     */
+    StokesWallCondition(IndexType NewId,
+                            const NodesArrayType& ThisNodes):
+        Condition(NewId,ThisNodes)
+    {
+    }
+
+    /// Constructor using Geometry
+    /**
+     @param NewId Index of the new condition
+     @param pGeometry Pointer to a geometry object
+     */
+    StokesWallCondition(IndexType NewId,
+                            GeometryType::Pointer pGeometry):
+        Condition(NewId,pGeometry)
+    {
+    }
+
+    /// Constructor using Properties
+    /**
+     @param NewId Index of the new element
+     @param pGeometry Pointer to a geometry object
+     @param pProperties Pointer to the element's properties
+     */
+    StokesWallCondition(IndexType NewId,
+                            GeometryType::Pointer pGeometry,
+                            PropertiesType::Pointer pProperties):
+        Condition(NewId,pGeometry,pProperties)
+    {
+    }
+
+    /// Copy constructor.
+    StokesWallCondition(StokesWallCondition const& rOther):
+        Condition(rOther)
+    {
+    }
+
+    /// Destructor.
+    virtual ~StokesWallCondition() {}
+
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /// Assignment operator
+    StokesWallCondition & operator=(StokesWallCondition const& rOther)
+    {
+        Condition::operator=(rOther);
+
+        return *this;
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /// Create a new StokesWallCondition object.
+    /**
+      @param NewId Index of the new condition
+      @param ThisNodes An array containing the nodes of the new condition
+      @param pProperties Pointer to the element's properties
+      */
+    virtual Condition::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const
+    {
+        return Condition::Pointer(new StokesWallCondition(NewId, GetGeometry().Create(ThisNodes), pProperties));
+    }
+
+
+    /// Return local contributions of the correct size, filled with zeros (for compatibility with time schemes).
+    /** The actual local contributions are computed in the Damping functions
+      @see CalculateLocalVelocityContribution
+      */
+    virtual void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix,
+                                      VectorType& rRightHandSideVector,
+                                      ProcessInfo& rCurrentProcessInfo)
+    {
+        const SizeType BlockSize = TDim + 1;
+        const SizeType LocalSize = BlockSize * TNumNodes;
+
+        if (rLeftHandSideMatrix.size1() != LocalSize)
+            rLeftHandSideMatrix.resize(LocalSize,LocalSize);
+
+        if (rRightHandSideVector.size() != LocalSize)
+            rRightHandSideVector.resize(LocalSize);
+
+        noalias(rLeftHandSideMatrix) = ZeroMatrix(LocalSize,LocalSize);
+        noalias(rRightHandSideVector) = ZeroVector(LocalSize);
+    }
+
+    /// Return a matrix of the correct size, filled with zeros (for compatibility with time schemes).
+    /** The actual local contributions are computed in the Damping functions
+      @see DampingMatrix
+      */
+    virtual void CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix,
+                                       ProcessInfo& rCurrentProcessInfo)
+    {
+        const SizeType BlockSize = TDim + 1;
+        const SizeType LocalSize = BlockSize * TNumNodes;
+
+        if (rLeftHandSideMatrix.size1() != LocalSize)
+            rLeftHandSideMatrix.resize(LocalSize,LocalSize);
+
+        noalias(rLeftHandSideMatrix) = ZeroMatrix(LocalSize,LocalSize);
+    }
+
+    /// Return local right hand side of the correct size, filled with zeros (for compatibility with time schemes).
+    /** The actual local contributions are computed in the Damping functions
+      @see CalculateLocalVelocityContribution
+      */
+    virtual void CalculateRightHandSide(VectorType& rRightHandSideVector,
+                                        ProcessInfo& rCurrentProcessInfo)
+    {
+        const SizeType BlockSize = TDim + 1;
+        const SizeType LocalSize = BlockSize * TNumNodes;
+
+        if (rRightHandSideVector.size() != LocalSize)
+            rRightHandSideVector.resize(LocalSize);
+
+        noalias(rRightHandSideVector) = ZeroVector(LocalSize);
+    }
+
+
+
+    /// Check that all data required by this condition is available and reasonable
+    virtual int Check(const ProcessInfo& rCurrentProcessInfo)
+    {
+        KRATOS_TRY;
+
+        int Check = Condition::Check(rCurrentProcessInfo); // Checks id > 0 and area > 0
+
+        if (Check != 0)
+        {
+            return Check;
+        }
+        else
+        {
+            // Check that all required variables have been registered
+            if(VELOCITY.Key() == 0)
+                KRATOS_THROW_ERROR(std::invalid_argument,"VELOCITY Key is 0. Check if the application was correctly registered.","");
+            if(PRESSURE.Key() == 0)
+                KRATOS_THROW_ERROR(std::invalid_argument,"PRESSURE Key is 0. Check if the application was correctly registered.","");
+            if(DENSITY.Key() == 0)
+                KRATOS_THROW_ERROR(std::invalid_argument,"DENSITY Key is 0. Check if the application was correctly registered.","");
+            if(VISCOSITY.Key() == 0)
+                KRATOS_THROW_ERROR(std::invalid_argument,"VISCOSITY Key is 0. Check if the application was correctly registered.","");
+            if(IS_STRUCTURE.Key() == 0)
+                KRATOS_THROW_ERROR(std::invalid_argument,"IS_STRUCTURE Key is 0. Check if the application was correctly registered.","");
+             if(EXTERNAL_PRESSURE.Key() == 0)
+                KRATOS_THROW_ERROR(std::invalid_argument,"EXTERNAL_PRESSURE Key is 0. Check if the application was correctly registered.","");
+
+
+                // Checks on nodes
+
+                // Check that the element's nodes contain all required SolutionStepData and Degrees of freedom
+                for(unsigned int i=0; i<this->GetGeometry().size(); ++i)
+                {
+                    if(this->GetGeometry()[i].SolutionStepsDataHas(VELOCITY) == false)
+                        KRATOS_THROW_ERROR(std::invalid_argument,"missing VELOCITY variable on solution step data for node ",this->GetGeometry()[i].Id());
+                    if(this->GetGeometry()[i].SolutionStepsDataHas(PRESSURE) == false)
+                        KRATOS_THROW_ERROR(std::invalid_argument,"missing PRESSURE variable on solution step data for node ",this->GetGeometry()[i].Id());
+                    if(this->GetGeometry()[i].SolutionStepsDataHas(EXTERNAL_PRESSURE) == false)
+                        KRATOS_THROW_ERROR(std::invalid_argument,"missing EXTERNAL_PRESSURE variable on solution step data for node ",this->GetGeometry()[i].Id());
+                    if(this->GetGeometry()[i].HasDofFor(VELOCITY_X) == false ||
+                            this->GetGeometry()[i].HasDofFor(VELOCITY_Y) == false ||
+                            this->GetGeometry()[i].HasDofFor(VELOCITY_Z) == false)
+                        KRATOS_THROW_ERROR(std::invalid_argument,"missing VELOCITY component degree of freedom on node ",this->GetGeometry()[i].Id());
+                    if(this->GetGeometry()[i].HasDofFor(PRESSURE) == false)
+                        KRATOS_THROW_ERROR(std::invalid_argument,"missing PRESSURE component degree of freedom on node ",this->GetGeometry()[i].Id());
+                }
+
+            return Check;
+        }
+
+        KRATOS_CATCH("");
+    }
+
+
+    /// Provides the global indices for each one of this element's local rows.
+    /** This determines the elemental equation ID vector for all elemental DOFs
+     * @param rResult A vector containing the global Id of each row
+     * @param rCurrentProcessInfo the current process info object (unused)
+     */
+    virtual void EquationIdVector(EquationIdVectorType& rResult,
+                                  ProcessInfo& rCurrentProcessInfo);
+
+
+    /// Returns a list of the element's Dofs
+    /**
+     * @param ElementalDofList the list of DOFs
+     * @param rCurrentProcessInfo the current process info instance
+     */
+    virtual void GetDofList(DofsVectorType& ConditionDofList,
+                            ProcessInfo& CurrentProcessInfo);
+
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    virtual std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "StokesWallCondition" << TDim << "D";
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    virtual void PrintInfo(std::ostream& rOStream) const
+    {
+        rOStream << "StokesWallCondition";
+    }
+
+    /// Print object's data.
+    virtual void PrintData(std::ostream& rOStream) const {}
+
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    void CalculateNormal(array_1d<double,3>& An );
+
+
+    void ApplyNeumannCondition(MatrixType &rLocalMatrix, VectorType &rLocalVector);
+
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    virtual void save(Serializer& rSerializer) const
+    {
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition );
+    }
+
+    virtual void load(Serializer& rSerializer)
+    {
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Condition );
+    }
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+
+    ///@}
+
+}; // Class StokesWallCondition
+
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+/// input stream function
+template< unsigned int TDim, unsigned int TNumNodes >
+inline std::istream& operator >> (std::istream& rIStream,
+                                  StokesWallCondition<TDim,TNumNodes>& rThis)
+{
+    return rIStream;
+}
+
+/// output stream function
+template< unsigned int TDim, unsigned int TNumNodes >
+inline std::ostream& operator << (std::ostream& rOStream,
+                                  const StokesWallCondition<TDim,TNumNodes>& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+
+///@}
+
+///@} addtogroup block
+
+
+}  // namespace Kratos.
+
+#endif // KRATOS_STOKES_WALL_CONDITION_H
