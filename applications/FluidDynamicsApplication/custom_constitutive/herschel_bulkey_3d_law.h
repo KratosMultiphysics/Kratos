8dfcc04bff08e8a3ee598bc27febcaeadec66114
rrossi <rrossi@cimne.upc.edu>
2016-01-11 13:33:44 +0000

    correcting save and load

--- applications/FluidDynamicsApplication/custom_constitutive/herschel_bulkey_3d_law.h
+++ applications/FluidDynamicsApplication/custom_constitutive/herschel_bulkey_3d_law.h
@@ -166,12 +166,12 @@ private:
 
     virtual void save(Serializer& rSerializer) const
     {
-        KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, HerschelBulkey3DLaw )
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, ConstitutiveLaw )
     }
 
     virtual void load(Serializer& rSerializer)
     {
-        KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, HerschelBulkey3DLaw )
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, ConstitutiveLaw )
     }
 
 
