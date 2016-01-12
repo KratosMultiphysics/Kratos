a42eab69110bfbde3bc73b1cc37de64e96961c0d
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-01-12 13:57:38 +0000

    Changing the identity function for set to SetIdentityFunction in set_identity_function.h

---
+++ kratos/kratos/containers/set_identity_function.h
@@ -0,0 +1,52 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics 
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Pooyan Dadvand
+//                    
+//
+
+
+#if !defined(KRATOS_SET_IDENTITY_FUNCTION_H_INCLUDED )
+#define  KRATOS_SET_IDENTITY_FUNCTION_H_INCLUDED
+
+
+
+// System includes
+#include <functional>
+
+
+
+namespace Kratos
+{
+	///@}
+	///@name Kratos Classes
+	///@{
+
+	/// Identity function is for having the object also as key in sets
+	/**
+	*/
+	template<class TDataType> class SetIdentityFunction : public std::unary_function<TDataType, TDataType>
+	{
+	public:
+		TDataType& operator()(TDataType& data)
+		{
+			return data;
+		}
+		const TDataType& operator()(const TDataType& data) const
+		{
+			return data;
+		}
+	};
+
+	///@}
+
+
+}  // namespace Kratos.
+
+#endif // KRATOS_SET_IDENTITY_FUNCTION_H_INCLUDED  defined 
