c8853be2871c5b1339b6bed8295f71e03e38206a
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2016-01-15 17:40:20 +0000

    Updated with more integration rules

--- kratos/integration/prism_gauss_legendre_integration_points.h
+++ kratos/integration/prism_gauss_legendre_integration_points.h
@@ -20,6 +20,46 @@
 namespace Kratos
 {
 
+//class KRATOS_API(KRATOS_CORE) PrismGaussLegendreIntegrationPoints1
+//{
+//public:
+//    KRATOS_CLASS_POINTER_DEFINITION(PrismGaussLegendreIntegrationPoints1);
+//    typedef std::size_t SizeType;
+
+//    static const unsigned int Dimension = 3;
+
+//    typedef IntegrationPoint<3> IntegrationPointType;
+
+//    typedef boost::array<IntegrationPointType, 1> IntegrationPointsArrayType;
+
+//    typedef IntegrationPointType::PointType PointType;
+
+//    static SizeType IntegrationPointsNumber()
+//    {
+//        return 1;
+//    }
+
+//    static IntegrationPointsArrayType& IntegrationPoints()
+//    {
+
+//        msIntegrationPoints[0] = IntegrationPointType( 0.33333333333333333333  , 0.33333333333333333333 , 0.00 , 0.5 );
+//        return msIntegrationPoints;
+//    }
+
+//    std::string Info() const
+//    {
+//        std::stringstream buffer;
+//        buffer << "Prism Gauss-Legendre quadrature 1 ";
+//        return buffer.str();
+//    }
+//protected:
+
+//private:
+
+//    static IntegrationPointsArrayType msIntegrationPoints;
+
+//}; // Class PrismGaussLegendreIntegrationPoints1
+
 class KRATOS_API(KRATOS_CORE) PrismGaussLegendreIntegrationPoints1
 {
 public:
@@ -30,19 +70,21 @@ public:
 
     typedef IntegrationPoint<3> IntegrationPointType;
 
-    typedef boost::array<IntegrationPointType, 1> IntegrationPointsArrayType;
+    typedef boost::array<IntegrationPointType, 3> IntegrationPointsArrayType;
 
     typedef IntegrationPointType::PointType PointType;
 
     static SizeType IntegrationPointsNumber()
     {
-        return 1;
+        return 3;
     }
 
     static IntegrationPointsArrayType& IntegrationPoints()
     {
 
-        msIntegrationPoints[0] = IntegrationPointType( 0.33333333333333333333  , 0.33333333333333333333 , 0.00 , 0.5 );
+        msIntegrationPoints[0] = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 , 0.00000000000000000000 , 0.16666666666666666667 );
+        msIntegrationPoints[1] = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 , 0.00000000000000000000 , 0.16666666666666666667 );
+        msIntegrationPoints[2] = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 , 0.00000000000000000000 , 0.16666666666666666667 );
         return msIntegrationPoints;
     }
 
@@ -144,7 +186,7 @@ public:
         std::stringstream buffer;
         buffer << "Prism Gauss-Legendre quadrature 3 ";
         return buffer.str();
-    }
+    }  
 protected:
 
 private:
@@ -152,6 +194,113 @@ private:
     static IntegrationPointsArrayType msIntegrationPoints;
 
 }; // Class PrismGaussLegendreIntegrationPoints3
+
+class KRATOS_API(KRATOS_CORE) PrismGaussLegendreIntegrationPoints4
+{
+public:
+    KRATOS_CLASS_POINTER_DEFINITION(PrismGaussLegendreIntegrationPoints4);
+    typedef std::size_t SizeType;
+
+    static const unsigned int Dimension = 3;
+
+    typedef IntegrationPoint<3> IntegrationPointType;
+
+    typedef boost::array<IntegrationPointType, 12> IntegrationPointsArrayType;
+
+    typedef IntegrationPointType::PointType PointType;
+
+    static SizeType IntegrationPointsNumber()
+    {
+        return 12;
+    }
+
+    static IntegrationPointsArrayType& IntegrationPoints()
+    {
+        msIntegrationPoints[0]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 , -0.86113631159405257522 , 0.02898790376145448812 );
+        msIntegrationPoints[1]  = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 , -0.86113631159405257522 , 0.02898790376145448812 );
+        msIntegrationPoints[2]  = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 , -0.86113631159405257522 , 0.02898790376145448812 );
+        msIntegrationPoints[3]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 , -0.33998104358485626480 , 0.05434542957187884522 );
+        msIntegrationPoints[4]  = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 , -0.33998104358485626480 , 0.05434542957187884522 );
+        msIntegrationPoints[5]  = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 , -0.33998104358485626480 , 0.05434542957187884522 );
+        msIntegrationPoints[6]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 ,  0.33998104358485626480 , 0.05434542957187884522 );
+        msIntegrationPoints[7]  = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 ,  0.33998104358485626480 , 0.05434542957187884522 );
+        msIntegrationPoints[8]  = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 ,  0.33998104358485626480 , 0.05434542957187884522 );
+        msIntegrationPoints[9]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 ,  0.86113631159405257522 , 0.02898790376145448812 );
+        msIntegrationPoints[10] = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 ,  0.86113631159405257522 , 0.02898790376145448812 );
+        msIntegrationPoints[11] = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 ,  0.86113631159405257522 , 0.02898790376145448812 );
+
+        return msIntegrationPoints;
+    }
+
+    std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "Prism Gauss-Legendre quadrature 4 ";
+        return buffer.str();
+    }
+protected:
+
+private:
+
+    static IntegrationPointsArrayType msIntegrationPoints;
+
+}; // Class PrismGaussLegendreIntegrationPoints4
+
+class KRATOS_API(KRATOS_CORE) PrismGaussLegendreIntegrationPoints5
+{
+public:
+    KRATOS_CLASS_POINTER_DEFINITION(PrismGaussLegendreIntegrationPoints5);
+    typedef std::size_t SizeType;
+
+    static const unsigned int Dimension = 3;
+
+    typedef IntegrationPoint<3> IntegrationPointType;
+
+    typedef boost::array<IntegrationPointType, 15> IntegrationPointsArrayType;
+
+    typedef IntegrationPointType::PointType PointType;
+
+    static SizeType IntegrationPointsNumber()
+    {
+        return 15;
+    }
+
+    static IntegrationPointsArrayType& IntegrationPoints()
+    {
+        msIntegrationPoints[0]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 , -0.90617984593866399280 , 0.01974390708801575729 );
+        msIntegrationPoints[1]  = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 , -0.90617984593866399280 , 0.01974390708801575729 );
+        msIntegrationPoints[2]  = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 , -0.90617984593866399280 , 0.01974390708801575729 );
+        msIntegrationPoints[3]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 , -0.53846931010568309104 , 0.03988572254161387234 );
+        msIntegrationPoints[4]  = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 , -0.53846931010568309104 , 0.03988572254161387234 );
+        msIntegrationPoints[5]  = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 , -0.53846931010568309104 , 0.03988572254161387234 );
+        msIntegrationPoints[6]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 ,  0.00000000000000000000 , 0.04740740740740740741 );
+        msIntegrationPoints[7]  = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 ,  0.00000000000000000000 , 0.04740740740740740741 );
+        msIntegrationPoints[8]  = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 ,  0.00000000000000000000 , 0.04740740740740740741 );
+        msIntegrationPoints[9]  = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 ,  0.53846931010568309104 , 0.03988572254161387234 );
+        msIntegrationPoints[10] = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 ,  0.53846931010568309104 , 0.03988572254161387234 );
+        msIntegrationPoints[11] = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 ,  0.53846931010568309104 , 0.03988572254161387234 );
+        msIntegrationPoints[12] = IntegrationPointType( 0.66666666666666666667 , 0.16666666666666666667 ,  0.90617984593866399280 , 0.01974390708801575729 );
+        msIntegrationPoints[13] = IntegrationPointType( 0.16666666666666666667 , 0.66666666666666666667 ,  0.90617984593866399280 , 0.01974390708801575729 );
+        msIntegrationPoints[14] = IntegrationPointType( 0.16666666666666666667 , 0.16666666666666666667 ,  0.90617984593866399280 , 0.01974390708801575729 );
+
+        return msIntegrationPoints;
+    }
+
+    std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "Prism Gauss-Legendre quadrature 5 ";
+        return buffer.str();
+    }
+protected:
+
+private:
+
+    static IntegrationPointsArrayType msIntegrationPoints;
+
+}; // Class PrismGaussLegendreIntegrationPoints5
+
+
 
 ///@name Type Definitions
 ///@{
