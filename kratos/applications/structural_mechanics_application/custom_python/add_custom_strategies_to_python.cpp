b6f2424204d27b3145dd0b9471799f16c3368d1b
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-01 12:07:17 +0000

    initial commit for the structural mechanics application. The application is derived from SolidMechanics, hence it will not work until a change is committed to the base application

---
+++ kratos/applications/structural_mechanics_application/custom_python/add_custom_strategies_to_python.cpp
@@ -0,0 +1,66 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:		 BSD License
+//					 license: structural_mechanics_application/license.txt
+//
+//  Main authors:    Riccardo Rossi
+//
+
+
+// System includes
+
+
+// External includes
+#include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <boost/timer.hpp>
+
+
+// Project includes
+#include "includes/define.h"
+#include "custom_python/add_custom_strategies_to_python.h"
+
+#include "spaces/ublas_space.h"
+
+//strategies
+#include "solving_strategies/strategies/solving_strategy.h"
+
+//linear solvers
+#include "linear_solvers/linear_solver.h"
+
+
+
+namespace Kratos
+{
+
+namespace Python
+{
+using namespace boost::python;
+
+void  AddCustomStrategiesToPython()
+{
+    typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
+    typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
+
+    typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
+    typedef SolvingStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > BaseSolvingStrategyType;
+    typedef Scheme< SparseSpaceType, LocalSpaceType > BaseSchemeType;
+
+    //********************************************************************
+    //********************************************************************
+// 			class_< TestStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType >,
+// 					bases< BaseSolvingStrategyType >,  boost::noncopyable >
+// 				("TestStrategy",
+// 				init<ModelPart&, LinearSolverType::Pointer, int, int, bool >() )
+// 				.def("MoveNodes",&TestStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType >::MoveNodes)
+// 				;
+
+}
+
+}  // namespace Python.
+
+} // Namespace Kratos
+
