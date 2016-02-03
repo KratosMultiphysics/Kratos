bffa5aef042f40c9ac12f1618cb71cd1110696dd
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-03 17:13:03 +0000

    newmark method (incomplete)

---
+++ kratos/applications/DEM_application/custom_strategies/schemes/newmark_beta_scheme.cpp
@@ -0,0 +1,131 @@
+
+
+#include "DEM_application.h"
+#include "newmark_beta_scheme.h"
+
+namespace Kratos {
+    void NewmarkBetaScheme::CalculateTranslationalMotion(ModelPart& model_part, NodesArrayType& pNodes) {
+        KRATOS_TRY
+        ProcessInfo& rCurrentProcessInfo = model_part.GetProcessInfo();
+        double delta_t = rCurrentProcessInfo[DELTA_TIME];
+        double virtual_mass_coeff = rCurrentProcessInfo[NODAL_MASS_COEFF];
+        bool if_virtual_mass_option = (bool) rCurrentProcessInfo[VIRTUAL_MASS_OPTION];
+        double force_reduction_factor = 1.0;
+        if (if_virtual_mass_option) {
+            force_reduction_factor = 1.0 - virtual_mass_coeff;
+            if (virtual_mass_coeff < 0.0) KRATOS_THROW_ERROR(std::runtime_error, "The coefficient assigned for virtual mass is larger than one, virtual_mass_coeff= ", virtual_mass_coeff)
+        }
+        vector<unsigned int> node_partition;
+        unsigned int number_of_threads = OpenMPUtils::GetNumThreads();
+        OpenMPUtils::CreatePartition(number_of_threads, pNodes.size(), node_partition);
+
+    #pragma omp parallel for shared(delta_t)
+        for (int k = 0; k < (int) number_of_threads; k++) {
+            NodesArrayType::iterator i_begin = pNodes.ptr_begin() + node_partition[k];
+            NodesArrayType::iterator i_end = pNodes.ptr_begin() + node_partition[k + 1];
+
+            for (ModelPart::NodeIterator i_iterator = i_begin; i_iterator != i_end; ++i_iterator) {
+                Node < 3 > & i = *i_iterator;
+                if (i.Is(DEMFlags::BELONGS_TO_A_CLUSTER)) continue;
+                array_1d<double, 3 >& vel = i.FastGetSolutionStepValue(VELOCITY);
+                array_1d<double, 3 >& displ = i.FastGetSolutionStepValue(DISPLACEMENT);
+                array_1d<double, 3 >& delta_displ = i.FastGetSolutionStepValue(DELTA_DISPLACEMENT);
+                array_1d<double, 3 >& coor = i.Coordinates();
+                array_1d<double, 3 >& initial_coor = i.GetInitialPosition();
+                array_1d<double, 3 >& force = i.FastGetSolutionStepValue(TOTAL_FORCES);
+                array_1d<double, 3 >& old_force = i.FastGetSolutionStepValue(TOTAL_FORCES);
+
+                double mass = i.FastGetSolutionStepValue(NODAL_MASS);
+
+                bool Fix_vel[3] = {false, false, false};
+
+                Fix_vel[0] = i.Is(DEMFlags::FIXED_VEL_X);
+                Fix_vel[1] = i.Is(DEMFlags::FIXED_VEL_Y);
+                Fix_vel[2] = i.Is(DEMFlags::FIXED_VEL_Z);
+
+                UpdateTranslationalVariables(i, coor, displ, delta_displ, vel, initial_coor, force, old_force, force_reduction_factor, mass, delta_t, Fix_vel);
+            } //nodes in the thread
+        } //threads
+        KRATOS_CATCH(" ")
+    }
+
+    void NewmarkBetaScheme::UpdateTranslationalVariables(
+            const Node < 3 > & i,
+            array_1d<double, 3 >& coor,
+            array_1d<double, 3 >& displ,
+            array_1d<double, 3 >& delta_displ,
+            array_1d<double, 3 >& vel,
+            const array_1d<double, 3 >& initial_coor,
+            const array_1d<double, 3 >& force,
+            const array_1d<double, 3 >& old_force,
+            const double force_reduction_factor,
+            const double mass,
+            const double delta_t,
+            const bool Fix_vel[3]) {
+
+        for (int k = 0; k < 3; k++) {
+            if (Fix_vel[k] == false) {
+                double mass_inv = 1.0 / mass;
+                double old_acceleration = mass_inv * old_force[k];
+                double acceleration = force_reduction_factor * mass_inv * force[k];
+                delta_displ[k] = delta_t * vel[k] + delta_t * delta_t * ((0.5 - mBeta) * acceleration + mBeta * old_acceleration);
+                displ[k] += delta_displ[k];
+                coor[k] = initial_coor[k] + displ[k];
+                vel[k] += 0.5 * delta_t * (acceleration + old_acceleration);
+            } else {
+                delta_displ[k] = delta_t * vel[k];
+                displ[k] += delta_displ[k];
+                coor[k] = initial_coor[k] + displ[k];
+            }
+        } // dimensions  
+    }
+
+    void NewmarkBetaScheme::UpdateRotationalVariables(
+                const Node < 3 > & i,
+                array_1d<double, 3 >& rotated_angle,
+                array_1d<double, 3 >& delta_rotation,
+                array_1d<double, 3 >& angular_velocity,
+                array_1d<double, 3 >& angular_acceleration,
+                const double delta_t,
+                const bool Fix_Ang_vel[3]) {
+
+        for (int k = 0; k < 3; k++) {
+            if (Fix_Ang_vel[k] == false) {
+                delta_rotation[k] = angular_velocity[k] * delta_t;
+                rotated_angle[k] += delta_rotation[k];
+                angular_velocity[k] += delta_t * angular_acceleration[k];
+            } else {
+                delta_rotation[k] = angular_velocity[k] * delta_t;
+                rotated_angle[k] += delta_rotation[k];
+            }
+        }
+    }            
+    
+    void NewmarkBetaScheme::CalculateLocalAngularAcceleration(
+                                const Node < 3 > & i,
+                                const double moment_of_inertia,
+                                const array_1d<double, 3 >& torque, 
+                                const double moment_reduction_factor,
+                                array_1d<double, 3 >& angular_acceleration){
+        
+        for (int j = 0; j < 3; j++) {
+            angular_acceleration[j] = moment_reduction_factor * torque[j] / moment_of_inertia;           
+        }
+    }
+    
+    
+    void NewmarkBetaScheme::CalculateLocalAngularAccelerationByEulerEquations(
+                                const Node < 3 > & i,
+                                const array_1d<double, 3 >& local_angular_velocity,
+                                const array_1d<double, 3 >& moments_of_inertia,
+                                const array_1d<double, 3 >& local_torque, 
+                                const double moment_reduction_factor,
+                                array_1d<double, 3 >& local_angular_acceleration){
+        
+        for (int j = 0; j < 3; j++) {
+            //Euler equations in Explicit (Forward Euler) scheme:
+            local_angular_acceleration[j] = (local_torque[j] - (local_angular_velocity[(j + 1) % 3] * moments_of_inertia[(j + 2) % 3] * local_angular_velocity[(j + 2) % 3] - local_angular_velocity[(j + 2) % 3] * moments_of_inertia[(j + 1) % 3] * local_angular_velocity[(j + 1) % 3])) / moments_of_inertia[j];
+            local_angular_acceleration[j] = local_angular_acceleration[j] * moment_reduction_factor;            
+        }
+    }
+} //namespace Kratos
---
+++ kratos/applications/DEM_application/custom_strategies/schemes/newmark_beta_scheme.h
@@ -0,0 +1,140 @@
+//
+// Author: Guillermo Casas gcasas@cimne.upc.edu
+//
+
+#if !defined(KRATOS_NEWMARK_BETA_SCHEME_H_INCLUDED )
+#define  KRATOS_NEWMARK_BETA_SCHEME_H_INCLUDED
+
+// System includes
+#include <string>
+#include <iostream> 
+#include <cfloat>
+
+// Project includes
+#include "dem_integration_scheme.h"
+#include "includes/define.h"
+#include "utilities/openmp_utils.h"
+#include "includes/model_part.h"
+#include "utilities/openmp_utils.h"
+#include "DEM_application.h"
+
+namespace Kratos {
+
+    class NewmarkBetaScheme : public DEMIntegrationScheme {
+    public:
+
+        typedef ModelPart::NodesContainerType NodesArrayType;
+
+        /// Pointer definition of NewmarkBetaScheme
+        KRATOS_CLASS_POINTER_DEFINITION(NewmarkBetaScheme);
+
+        /// Default constructor.
+        NewmarkBetaScheme(const double gamma = 0.5, const double beta = 0.25):
+            mGamma(gamma), mBeta(beta) {}
+
+        /// Destructor.
+        virtual ~NewmarkBetaScheme() {}
+        void CalculateTranslationalMotion(ModelPart& model_part, NodesArrayType& pNodes);
+
+        void UpdateTranslationalVariables(
+            const Node < 3 > & i,
+            array_1d<double, 3 >& coor,
+            array_1d<double, 3 >& displ,
+            array_1d<double, 3 >& delta_displ,
+            array_1d<double, 3 >& vel,
+            const array_1d<double, 3 >& initial_coor,
+            const array_1d<double, 3 >& force,
+            const array_1d<double, 3 >& old_force,
+            const double force_reduction_factor,
+            const double mass,
+            const double delta_t,
+            const bool Fix_vel[3]);
+        
+        void UpdateRotationalVariables(
+                const Node < 3 > & i,
+                array_1d<double, 3 >& rotated_angle,
+                array_1d<double, 3 >& delta_rotation,
+                array_1d<double, 3 >& angular_velocity,
+                array_1d<double, 3 >& angular_acceleration,
+                const double delta_t,
+                const bool Fix_Ang_vel[3]);        
+        
+        void CalculateLocalAngularAcceleration(
+                                const Node < 3 > & i,
+                                const double moment_of_inertia,
+                                const array_1d<double, 3 >& torque, 
+                                const double moment_reduction_factor,
+                                array_1d<double, 3 >& angular_acceleration);
+        
+        void CalculateLocalAngularAccelerationByEulerEquations(
+                                    const Node < 3 > & i,
+                                    const array_1d<double, 3 >& local_angular_velocity,
+                                    const array_1d<double, 3 >& moments_of_inertia,
+                                    const array_1d<double, 3 >& local_torque, 
+                                    const double moment_reduction_factor,
+                                    array_1d<double, 3 >& local_angular_acceleration);
+        
+        /// Turn back information as a string.
+
+        virtual std::string Info() const {
+            std::stringstream buffer;
+            buffer << "NewmarkBetaScheme";
+            return buffer.str();
+        }
+
+        /// Print information about this object.
+
+        virtual void PrintInfo(std::ostream& rOStream) const {
+            rOStream << "NewmarkBetaScheme";
+        }
+
+        /// Print object's data.
+
+        virtual void PrintData(std::ostream& rOStream) const {
+        }
+
+
+    protected:
+
+
+    private:
+
+        double mGamma;
+        double mBeta;
+
+
+        /// Assignment operator.
+
+        NewmarkBetaScheme& operator=(NewmarkBetaScheme const& rOther) {
+            return *this;
+        }
+
+        /// Copy constructor.
+
+        NewmarkBetaScheme(NewmarkBetaScheme const& rOther) {
+            *this = rOther;
+        }
+
+
+        ///@}    
+
+    }; // Class NewmarkBetaScheme
+
+
+    inline std::istream& operator>>(std::istream& rIStream,
+            NewmarkBetaScheme& rThis) {
+        return rIStream;
+    }
+
+    inline std::ostream& operator<<(std::ostream& rOStream,
+            const NewmarkBetaScheme& rThis) {
+        rThis.PrintInfo(rOStream);
+        rOStream << std::endl;
+        rThis.PrintData(rOStream);
+
+        return rOStream;
+    }
+
+} // namespace Kratos.
+
+#endif // KRATOS_NEWMARK_BETA_SCHEME_H_INCLUDED  defined
