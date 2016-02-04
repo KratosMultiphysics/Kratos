abd2321ce642c45ed0baf2c701f59c2c7607e14f
gcasas <gcasas@cimne.upc.edu>
2016-02-04 09:39:08 +0000

    correcting some mistakes for working with normal swimmings

--- applications/swimming_DEM_application/python_scripts/variables_management.py
+++ applications/swimming_DEM_application/python_scripts/variables_management.py
@@ -60,7 +60,7 @@ def ConstructListsOfVariables(pp):
     pp.fluid_vars += pp.coupling_fluid_vars
     pp.fluid_vars += [PRESSURE_GRADIENT]
 
-    if (pp.CFD_DEM.drag_force_type >= 0):
+    if pp.CFD_DEM.drag_force_type >= 0:
         pp.fluid_vars += [POWER_LAW_N]
         pp.fluid_vars += [POWER_LAW_K]
         pp.fluid_vars += [GEL_STRENGTH]
@@ -73,16 +73,16 @@ def ConstructListsOfVariables(pp):
     pp.dem_vars += pp.coupling_dem_vars
     pp.dem_vars += [BUOYANCY]
 
-    if (pp.CFD_DEM.drag_force_type > 0 and  pp.CFD_DEM.add_each_hydro_force_option):
+    if pp.CFD_DEM.drag_force_type > 0 and  pp.CFD_DEM.add_each_hydro_force_option:
         pp.dem_vars += [DRAG_FORCE]
 
-    if (pp.CFD_DEM.drag_force_type > 1):
+    if pp.CFD_DEM.drag_force_type > 1:
         pp.dem_vars += [PARTICLE_SPHERICITY]
 
-    if (pp.CFD_DEM.lift_force_type > 0 and  pp.CFD_DEM.add_each_hydro_force_option):
+    if pp.CFD_DEM.lift_force_type > 0 and  pp.CFD_DEM.add_each_hydro_force_option:
         pp.dem_vars += [LIFT_FORCE]
 
-    if (pp.CFD_DEM.virtual_mass_force_type > 0 and  pp.CFD_DEM.add_each_hydro_force_option):
+    if pp.CFD_DEM.virtual_mass_force_type > 0 and  pp.CFD_DEM.add_each_hydro_force_option:
         pp.dem_vars += [VIRTUAL_MASS_FORCE]
 
     # clusters variables
@@ -91,7 +91,7 @@ def ConstructListsOfVariables(pp):
     # rigid faces variables
     pp.rigid_faces_vars = [VELOCITY, DISPLACEMENT, ELASTIC_FORCES, PRESSURE, TANGENTIAL_ELASTIC_FORCES, SHEAR_STRESS, NODAL_AREA]
 
-    if (pp.CFD_DEM.embedded_option):
+    if pp.CFD_DEM.embedded_option:
         pp.rigid_faces_vars += [FORCE]
         pp.rigid_faces_vars += [POSITIVE_FACE_PRESSURE]
         pp.rigid_faces_vars += [NEGATIVE_FACE_PRESSURE]
@@ -104,64 +104,65 @@ def ConstructListsOfResultsToPrint(pp):
     pp.clusters_nodal_results = []
     pp.rigid_faces_nodal_results = []
 
-    if (DEM_parameters.PostRadius):
+    if DEM_parameters.PostRadius:
         pp.dem_nodal_results += ["RADIUS"]
 
-    if (DEM_parameters.PostAngularVelocity):
+    if DEM_parameters.PostAngularVelocity:
         pp.dem_nodal_results += ["ANGULAR_VELOCITY"]
         
-    if (DEM_parameters.PostElasticForces):
+    if DEM_parameters.PostElasticForces:
         pp.dem_nodal_results += ["ELASTIC_FORCES"]
         
-    if (DEM_parameters.PostContactForces):
+    if DEM_parameters.PostContactForces:
         pp.dem_nodal_results += ["CONTACT_FORCES"]
         
-    if (DEM_parameters.PostTotalForces):
+    if DEM_parameters.PostTotalForces:
         pp.dem_nodal_results += ["TOTAL_FORCES"]
 
-    if (DEM_parameters.PostCationConcentration):
-        pp.dem_nodal_results += ["CATION_CONCENTRATION"]
+    if DEM_parameters.ElementType == "SwimmingNanoParticle":
+        if (DEM_parameters.PostCationConcentration):
+            pp.dem_nodal_results += ["CATION_CONCENTRATION"]
         
-    if (pp.CFD_DEM.coupling_level_type > 0):
+    if pp.CFD_DEM.coupling_level_type > 0:
 
-        if (pp.CFD_DEM.print_REYNOLDS_NUMBER_option):
+        if pp.CFD_DEM.print_REYNOLDS_NUMBER_option:
             pp.dem_nodal_results += ["REYNOLDS_NUMBER"]
 
-        if (pp.CFD_DEM.print_PRESSURE_GRAD_PROJECTED_option):
+        if pp.CFD_DEM.print_PRESSURE_GRAD_PROJECTED_option:
             pp.dem_nodal_results += ["PRESSURE_GRAD_PROJECTED"]
 
-        if (pp.CFD_DEM.print_HYDRODYNAMIC_FORCE_option):
+        if pp.CFD_DEM.print_HYDRODYNAMIC_FORCE_option:
             pp.dem_nodal_results += ["HYDRODYNAMIC_FORCE"]
 
-        if (pp.CFD_DEM.print_HYDRODYNAMIC_MOMENT_option):
+        if pp.CFD_DEM.print_HYDRODYNAMIC_MOMENT_option:
             pp.dem_nodal_results += ["HYDRODYNAMIC_MOMENT"]
 
-        if (pp.CFD_DEM.print_FLUID_VEL_PROJECTED_option):
+        if pp.CFD_DEM.print_FLUID_VEL_PROJECTED_option:
             pp.dem_nodal_results += ["FLUID_VEL_PROJECTED"]
 
-        if (pp.CFD_DEM.print_FLUID_ACCEL_PROJECTED_option):
+        if pp.CFD_DEM.print_FLUID_ACCEL_PROJECTED_option:
             pp.dem_nodal_results += ["FLUID_ACCEL_PROJECTED"]            
 
-        if (pp.CFD_DEM.print_FLUID_FRACTION_PROJECTED_option):
+        if pp.CFD_DEM.print_FLUID_FRACTION_PROJECTED_option:
             pp.dem_nodal_results += ["FLUID_FRACTION_PROJECTED"]
 
-        if (pp.CFD_DEM.print_FLUID_VISCOSITY_PROJECTED_option):
+        if pp.CFD_DEM.print_FLUID_VISCOSITY_PROJECTED_option:
             pp.dem_nodal_results += ["FLUID_VISCOSITY_PROJECTED"]
 
-        if (pp.CFD_DEM.print_BUOYANCY_option):
+        if pp.CFD_DEM.print_BUOYANCY_option:
             pp.dem_nodal_results += ["BUOYANCY"]
 
-    if (pp.CFD_DEM.add_each_hydro_force_option):
+    if pp.CFD_DEM.add_each_hydro_force_option:
 
-        if (pp.CFD_DEM.print_DRAG_FORCE_option):
+        if pp.CFD_DEM.print_DRAG_FORCE_option:
             pp.dem_nodal_results += ["DRAG_FORCE"]
 
-        if (pp.CFD_DEM.print_VIRTUAL_MASS_FORCE_option):
+        if pp.CFD_DEM.print_VIRTUAL_MASS_FORCE_option:
             pp.dem_nodal_results += ["VIRTUAL_MASS_FORCE"]
 
-        if (pp.CFD_DEM.print_LIFT_FORCE_option):
+        if pp.CFD_DEM.print_LIFT_FORCE_option:
             pp.dem_nodal_results += ["LIFT_FORCE"]
-    if (pp.CFD_DEM.embedded_option):
+    if pp.CFD_DEM.embedded_option:
         pp.rigid_faces_nodal_results += ["POSITIVE_FACE_PRESSURE"]
         pp.rigid_faces_nodal_results += ["NEGATIVE_FACE_PRESSURE"]
 
@@ -211,40 +212,40 @@ def ConstructListsOfVariablesForCoupling(pp):
     pp.coupling_fluid_vars = []
     pp.coupling_fluid_vars += [ACCELERATION]
 
-    if (pp.CFD_DEM.embedded_option):
+    if pp.CFD_DEM.embedded_option:
        pp.coupling_fluid_vars += [DISTANCE]
 
     pp.coupling_fluid_vars += [BODY_FORCE]
 
-    if (pp.CFD_DEM.fluid_model_type == 0):
+    if pp.CFD_DEM.fluid_model_type == 0:
         pp.coupling_fluid_vars += [AVERAGED_FLUID_VELOCITY]
 
-    if (pp.CFD_DEM.fluid_model_type == 0 or pp.CFD_DEM.coupling_level_type >= 1 or pp.CFD_DEM.drag_force_type == 4):
+    if pp.CFD_DEM.fluid_model_type == 0 or pp.CFD_DEM.coupling_level_type >= 1 or pp.CFD_DEM.drag_force_type == 4:
         pp.coupling_fluid_vars += [FLUID_FRACTION]
         
-        if (pp.CFD_DEM.print_SOLID_FRACTION_option):
+        if pp.CFD_DEM.print_SOLID_FRACTION_option:
             pp.coupling_fluid_vars += [SOLID_FRACTION]
 
-    if (pp.CFD_DEM.fluid_model_type >= 1):
+    if pp.CFD_DEM.fluid_model_type >= 1:
         pp.coupling_fluid_vars += [FLUID_FRACTION_GRADIENT]
         pp.coupling_fluid_vars += [FLUID_FRACTION_RATE]
 
-    if (pp.CFD_DEM.coupling_level_type >= 1):
+    if pp.CFD_DEM.coupling_level_type >= 1:
         pp.coupling_fluid_vars += [HYDRODYNAMIC_REACTION]
 
-    if (pp.CFD_DEM.drag_force_type >= 2):
+    if pp.CFD_DEM.drag_force_type >= 2:
         pp.coupling_fluid_vars += [POWER_LAW_N]
         pp.coupling_fluid_vars += [POWER_LAW_K]
         pp.coupling_fluid_vars += [GEL_STRENGTH]
         pp.coupling_fluid_vars += [YIELD_STRESS]
 
-    if (pp.viscosity_modification_type):
+    if pp.viscosity_modification_type:
         pp.coupling_fluid_vars += [VISCOSITY]
 
     # dem coupling variables
     pp.coupling_dem_vars = []
 
-    if (pp.CFD_DEM.coupling_level_type > 0):
+    if pp.CFD_DEM.coupling_level_type > 0:
         pp.coupling_dem_vars += [FLUID_VEL_PROJECTED]
         pp.coupling_dem_vars += [FLUID_DENSITY_PROJECTED]
         pp.coupling_dem_vars += [PRESSURE_GRAD_PROJECTED]
@@ -252,60 +253,60 @@ def ConstructListsOfVariablesForCoupling(pp):
         pp.coupling_dem_vars += [HYDRODYNAMIC_FORCE]
         pp.coupling_dem_vars += [HYDRODYNAMIC_MOMENT]
 
-    if (pp.CFD_DEM.coupling_level_type >= 1 or pp.CFD_DEM.fluid_model_type == 0):
+    if pp.CFD_DEM.coupling_level_type >= 1 or pp.CFD_DEM.fluid_model_type == 0:
         pp.coupling_dem_vars += [FLUID_FRACTION_PROJECTED]
 
-    if (pp.CFD_DEM.lift_force_type == 1):
+    if pp.CFD_DEM.lift_force_type == 1:
         pp.coupling_dem_vars += [FLUID_VORTICITY_PROJECTED]
         pp.coupling_dem_vars += [SHEAR_RATE_PROJECTED]
 
-    if (pp.CFD_DEM.virtual_mass_force_type >= 1):
+    if pp.CFD_DEM.virtual_mass_force_type >= 1:
         pp.coupling_dem_vars += [FLUID_ACCEL_PROJECTED]
 
-    if (pp.CFD_DEM.embedded_option):
+    if pp.CFD_DEM.embedded_option:
         pp.coupling_dem_vars += [DISTANCE]
 
-    if (pp.CFD_DEM.drag_force_type >= 2):
+    if pp.CFD_DEM.drag_force_type >= 2:
         pp.coupling_dem_vars += [POWER_LAW_N]
         pp.coupling_dem_vars += [POWER_LAW_K]
         #pp.coupling_dem_vars += [GEL_STRENGTH]
         pp.coupling_dem_vars += [YIELD_STRESS]
 
-    if (pp.CFD_DEM.print_REYNOLDS_NUMBER_option):
+    if pp.CFD_DEM.print_REYNOLDS_NUMBER_option:
         pp.coupling_dem_vars += [REYNOLDS_NUMBER]
 
 def ChangeListOfFluidNodalResultsToPrint(pp):
 
-    if (pp.CFD_DEM.print_FLUID_FRACTION_option):
+    if pp.CFD_DEM.print_FLUID_FRACTION_option:
         pp.nodal_results += ["FLUID_FRACTION"]
 
-    if (pp.CFD_DEM.print_SOLID_FRACTION_option):
+    if pp.CFD_DEM.print_SOLID_FRACTION_option:
         pp.nodal_results += ["SOLID_FRACTION"]
 
-    if (pp.CFD_DEM.fluid_model_type == 0 and pp.CFD_DEM.print_AVERAGED_FLUID_VELOCITY_option):
+    if pp.CFD_DEM.fluid_model_type == 0 and pp.CFD_DEM.print_AVERAGED_FLUID_VELOCITY_option:
         pp.nodal_results += ["AVERAGED_FLUID_VELOCITY"]
 
-    if (pp.CFD_DEM.fluid_model_type == 1 and pp.CFD_DEM.print_FLUID_FRACTION_GRADIENT_option):
+    if pp.CFD_DEM.fluid_model_type == 1 and pp.CFD_DEM.print_FLUID_FRACTION_GRADIENT_option:
         pp.nodal_results += ["FLUID_FRACTION_GRADIENT"]
 
-    if (pp.CFD_DEM.body_force_on_fluid_option and pp.CFD_DEM.print_BODY_FORCE_option):
+    if pp.CFD_DEM.body_force_on_fluid_option and pp.CFD_DEM.print_BODY_FORCE_option:
         pp.nodal_results += ["BODY_FORCE"]
 
-    if (pp.CFD_DEM.print_HYDRODYNAMIC_REACTION_option):
+    if pp.CFD_DEM.print_HYDRODYNAMIC_REACTION_option:
         pp.nodal_results += ["HYDRODYNAMIC_REACTION"]
 
-    if (pp.CFD_DEM.embedded_option):
+    if pp.CFD_DEM.embedded_option:
         pp.nodal_results += ["DISTANCE"]
 
 def ChangeInputDataForConsistency(pp):
     pp.CFD_DEM.project_at_every_substep_option *= (pp.CFD_DEM.coupling_level_type > 0)
     
-    if (DEM_parameters.VelocityTrapOption == "ON"):
+    if DEM_parameters.VelocityTrapOption == "ON":
         pp.velocity_trap_option = 1
     else: 
         pp.velocity_trap_option = 0
 
-    if (pp.CFD_DEM.flow_in_porous_medium_option):
+    if pp.CFD_DEM.flow_in_porous_medium_option:
         pp.coupling_weighing_type = - 1 # the fluid fraction is not projected from DEM (there may not be a DEM part) but is externally imposed
 
     pp.CFD_DEM.time_steps_per_stationarity_step = max(1, int(pp.CFD_DEM.time_steps_per_stationarity_step)) # it should never be smaller than 1!
