b5662b7b9de58563170252ad31760c78b172331e
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-04 21:33:06 +0000

    new tests for the SolidMechanicsApplication

--- kratos/applications/SolidMechanicsApplication/test_examples/IsotropicDamageSimoJuPlaneStress_FourPointShearTest.gid/run_test.py
+++ kratos/applications/SolidMechanicsApplication/test_examples/IsotropicDamageSimoJuPlaneStress_FourPointShearTest.gid/run_test.py
@@ -14,7 +14,7 @@ print(ctime())
 # measure process time
 t0p = clock()
 # measure wall time
-# t0w = time()
+t0w = time()
 
 # ----------------------------------------------------------------#
 # --CONFIGURATIONS START--####################
@@ -28,17 +28,19 @@ domain_size = general_variables.domain_size
 from KratosMultiphysics import *
 
 # including Applications paths
+from KratosMultiphysics.SolidMechanicsApplication  import *
 from KratosMultiphysics.ExternalSolversApplication import *
-from KratosMultiphysics.SolidMechanicsApplication import *
+
 
 # import the python utilities:
-import restart_utility as restart_utils
-import gid_output_utility as gid_utils
+import restart_utility           as restart_utils
+import gid_output_utility        as gid_utils
 
 import conditions_python_utility as condition_utils
 import list_files_python_utility as files_utils
 
-import time_operation_utility as operation_utils
+import time_operation_utility    as operation_utils
+import solving_info_utility      as solving_info_utils
 
 # ------------------------#--FUNCTIONS START--#------------------#
 # ---------------------------------------------------------------#
@@ -48,53 +50,44 @@ def StartTimeMeasuring():
     time_ip = clock()
     return time_ip
 
-
-def StopTimeMeasuring(time_ip, process):
+def StopTimeMeasuring(time_ip, process, report):
     # measure process time
     time_fp = clock()
-    print(" ", process, " [ spent time = ", time_fp - time_ip, "] ")
+    if( report ):
+        used_time = time_fp - time_ip
+        print("::[KSM Simulation]:: [ %.2f" % round(used_time,2),"s", process," ] ")
 # --TIME MONITORING END --###################
 
 # --SET NUMBER OF THREADS --#################
 
-
 def SetParallelSize(num_threads):
     parallel = OpenMPUtils()
-    print("Num Threads = ", num_threads)
     parallel.SetNumThreads(int(num_threads))
+    print("::[KSM Simulation]:: OMP USING",num_threads,"THREADS ]")
+    #parallel.PrintOMPInfo()
+    print(" ")
+
 # --SET NUMBER OF THREADS --#################
 
 # ------------------------#--FUNCTIONS END--#--------------------#
 # ---------------------------------------------------------------#
 
-
-# defining the number of threads:
-num_threads = general_variables.NumberofThreads
-SetParallelSize(num_threads)
-
 # defining the type, the name and the path of the problem:
+echo_level   = general_variables.echo_level
 problem_type = general_variables.ProblemType
 problem_name = general_variables.problem_name
-problem_path = general_variables.problem_path
+
+#problem_path = general_variables.problem_path #fixed path
+problem_path  = os.getcwd() #current path
 
 # defining a model part
-model_part = ModelPart("SolidDomain")
+model_part = ModelPart("Solid Domain")
+
+# defining solver settings
+SolverSettings = general_variables.SolverSettings
 
 # defining the model size to scale
 length_scale = 1.0
-
-# --DEFINE MAIN SOLVER START--################
-
-SolverSettings = general_variables.SolverSettings
-
-# import solver file
-solver_constructor = __import__(SolverSettings.solver_type)
-
-# construct the solver
-main_step_solver = solver_constructor.CreateSolver(model_part, SolverSettings)
-
-# --DEFINE MAIN SOLVER END--##################
-
 
 # --READ AND SET MODEL FILES--###############
 
@@ -115,7 +108,7 @@ list_files.Initialize(general_variables.file_list)
 incr_disp = general_variables.Incremental_Displacement
 incr_load = general_variables.Incremental_Load
 rotation_dofs = SolverSettings.RotationDofs
-conditions = condition_utils.ConditionsUtility(model_part, domain_size, incr_disp, incr_load, rotation_dofs)
+conditions    = condition_utils.ConditionsUtility(model_part, domain_size, incr_disp, incr_load, rotation_dofs)
 
 # --DEFINE CONDITIONS END--###################
 
@@ -125,6 +118,23 @@ conditions = condition_utils.ConditionsUtility(model_part, domain_size, incr_dis
 gid_print = gid_utils.GidOutputUtility(problem_name, general_variables.GidOutputConfiguration)
 
 # --GID OUTPUT OPTIONS END--##################
+
+# start problem initialization:
+print(" ")
+
+# defining the number of threads:
+num_threads = general_variables.NumberofThreads
+SetParallelSize(num_threads)
+
+# --DEFINE MAIN SOLVER START--################
+
+# import solver file
+solver_constructor = __import__(SolverSettings.solver_type)
+
+# construct the solver
+main_step_solver = solver_constructor.CreateSolver(model_part, SolverSettings)
+
+# --DEFINE MAIN SOLVER END--##################
 
 
 # --CONFIGURATIONS END--######################
@@ -147,6 +157,8 @@ solver_constructor.AddVariables(model_part, SolverSettings)
 # --- READ MODEL ------#
 if(load_restart == False):
 
+    print("::[KSM Simulation]:: Reading -START- (MDPA FILE) ")
+
     # remove results, restart, graph and list previous files
     problem_restart.CleanPreviousFiles()
     list_files.RemoveListFiles()
@@ -159,6 +171,8 @@ if(load_restart == False):
     model_part.SetBufferSize(buffer_size)
     # Note: the buffer size should be set once the mesh is read for the first time
 
+    print("::[KSM Simulation]:: Reading -END- ")
+
     # set the degrees of freedom
     solver_constructor.AddDofs(model_part, SolverSettings)
 
@@ -168,14 +182,20 @@ if(load_restart == False):
     constitutive_law = constitutive_law_utils.ConstitutiveLawUtility(model_part, domain_size);
     constitutive_law.Initialize();
 
+
 else:
+
+    print("::[KSM Simulation]:: Reading -RESTART- (RESTART FILE ",restart_step,")- ")
 
     # reading the model from the restart file
     problem_restart.Load(restart_step);
 
+    print("::[KSM Simulation]:: Reading -END- ")
+
     # remove results, restart, graph and list posterior files
     problem_restart.CleanPosteriorFiles(restart_step)
     list_files.ReBuildListFiles()
+
 
 # set mesh searches and modeler
 # modeler.InitializeDomains();
@@ -186,8 +206,11 @@ else:
 
 
 # --- PRINT CONTROL ---#
-print(model_part)
-print(model_part.Properties[1])
+
+if(echo_level>1):
+    print("")
+    print(model_part)
+    print(model_part.Properties[1])
 
 
 # --INITIALIZE--###########################
@@ -223,8 +246,8 @@ else:
 # initialize step operations
 starting_step  = model_part.ProcessInfo[TIME_STEPS]
 starting_time  = model_part.ProcessInfo[TIME]
-ending_step    = general_variables.nsteps
-ending_time    = general_variables.nsteps * time_step
+#ending_step    = general_variables.nsteps
+ending_time    = general_variables.end_time
 
 
 output_print = operation_utils.TimeOperationUtility()
@@ -235,12 +258,16 @@ restart_print = operation_utils.TimeOperationUtility()
 restart_time_frequency = general_variables.RestartFrequency
 restart_print.InitializeTime(starting_time, ending_time, time_step, restart_time_frequency)
 
+solving_print = operation_utils.TimeOperationUtility()
+solving_time_frequency = gid_time_frequency
+solving_print.InitializeTime(starting_time, ending_time, time_step, solving_time_frequency)
+
 
 # --TIME INTEGRATION--#######################
 #
 
 # writing a single file
-gid_print.initialize_results(model_part)
+#gid_print.initialize_results(model_part)
 
 #initialize time integration variables
 current_time = starting_time
@@ -255,8 +282,15 @@ for step in range(0,buffer_size):
 
 # writing a initial state results file
 current_id = 0
-gid_print.write_results(model_part, general_variables.nodal_results, general_variables.gauss_points_results, current_time, current_step, current_id)
-list_files.PrintListFiles(current_id);
+#if(load_restart == False): 
+    #gid_print.write_results(model_part, general_variables.nodal_results, general_variables.gauss_points_results, current_time, current_step, current_id)
+    #list_files.PrintListFiles(current_id);
+
+# set solver info starting parameters
+solving_info = solving_info_utils.SolvingInfoUtility(model_part, SolverSettings)
+
+print(" ")
+print("::[KSM Simulation]:: Analysis -START- ")
 
 # solving the problem
 while(current_time < ending_time):
@@ -271,19 +305,23 @@ while(current_time < ending_time):
   model_part.CloneTimeStep(current_time)
   model_part.ProcessInfo[TIME] = current_time
 
-  print("STEP = ", current_step)
-  print("TIME = ", current_time)
+  # print process information:
+  print_info = solving_print.perform_time_operation(current_time)
+  if(print_info):
+      solving_info.print_step_info(current_time,current_step)
 
-  clock_time = StartTimeMeasuring();
+  # ssolving the solid problem 
+  clock_time = StartTimeMeasuring();     
+
   # solve time step non-linear system
   main_step_solver.Solve()
-  StopTimeMeasuring(clock_time, "Solving");
+  
+  StopTimeMeasuring(clock_time, "Solving", False);
 
   # incremental load
   conditions.SetIncrementalLoad(current_step, time_step);
 
   # print the results at the end of the step
-  #execute_write = output_print.perform_time_operation(current_time)
   execute_write = False
   if(execute_write):
       clock_time = StartTimeMeasuring();
@@ -291,8 +329,9 @@ while(current_time < ending_time):
       # print gid output file
       gid_print.write_results(model_part, general_variables.nodal_results, general_variables.gauss_points_results, current_time, current_step, current_id)
       # print on list files
-      list_files.PrintListFiles(current_id);
-      StopTimeMeasuring(clock_time, "Write Results");
+      list_files.PrintListFiles(current_id)
+      solving_info.set_print_info(execute_write, current_id)    
+      StopTimeMeasuring(clock_time, "Writing Results", False);
 
   # print restart file
   save_restart = False
@@ -300,10 +339,15 @@ while(current_time < ending_time):
       execute_save = restart_print.perform_time_operation(current_time)
       if(execute_save):
           clock_time = StartTimeMeasuring();
-          current_id = output_print.operation_id()
-          problem_restart.Save(current_time, current_step, current_id);
-          StopTimeMeasuring(clock_time, "Restart");
+          current_id = restart_print.operation_id()
+          problem_restart.Save(current_time, current_step, current_id)
+          solving_info.set_restart_info(execute_save,current_id)
+          StopTimeMeasuring(clock_time, "Writing Restart", False);
 
+
+  solving_info.update_solving_info()
+  if(print_info):
+      solving_info.print_solving_info()
         
   conditions.RestartImposedDisp()
 
@@ -311,21 +355,25 @@ while(current_time < ending_time):
 #
 
 # writing a single file
-gid_print.finalize_results()
+#gid_print.finalize_results()
 
-print("Analysis Finalized ")
+print("::[KSM Simulation]:: Analysis -END- ")
+print(" ")
 
 # --END--###############################
 #
 
+# check solving information for any problem
+solving_info.info_check()
+
 # measure process time
 tfp = clock()
 # measure wall time
-# tfw = time()
+tfw = time()
+
+print("::[KSM Simulation]:: [ Computing Time = (%.2f" % (tfp - t0p)," seconds process time) ( %.2f" % (tfw - t0w)," seconds wall time) ]")
 
 print(ctime())
-# print "Analysis Completed  [Process Time = ", tfp - t0p, "seconds, Wall Time = ", tfw - t0w, " ]"
-print("Analysis Completed  [Process Time = ", tfp - t0p, "] ")
 
 # to create a benchmark: add standard benchmark files and decomment next two lines 
 # rename the file to: run_test.py
