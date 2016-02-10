a777aede8a99fe419118c30a0575f7bd9cf91e1b
Pooyan Dadvand <pooyan@cimne.upc.edu>
2016-02-10 12:07:33 +0000

    Adding tests directory to the KratosCore

---
+++ kratos/tests/test_model_part_io.py
@@ -0,0 +1,122 @@
+﻿import unittest
+from KratosMultiphysics import *
+
+class TestModelPartIO(unittest.TestCase):
+
+    def test_model_part_io_read_model_part(self):
+        model_part = ModelPart("Main")
+        model_part.AddNodalSolutionStepVariable(DISPLACEMENT)
+        model_part.AddNodalSolutionStepVariable(VISCOSITY)
+        model_part_io = ModelPartIO("test_model_part_io")
+        model_part_io.ReadModelPart(model_part)
+
+        self.assertEqual(model_part.NumberOfSubModelParts(), 2)
+
+        self.assertEqual(model_part.NumberOfTables(), 1)
+        self.assertEqual(model_part.NumberOfProperties(), 1)
+        self.assertEqual(model_part.NumberOfNodes(), 6)
+        self.assertEqual(model_part.NumberOfProperties(), 1)
+        self.assertEqual(model_part.NumberOfElements(), 4)
+        self.assertEqual(model_part.NumberOfConditions(), 5)
+
+        self.assertTrue(model_part.GetNode(1).IsFixed(DISPLACEMENT_X))
+        self.assertTrue(model_part.GetNode(2).IsFixed(DISPLACEMENT_X))
+        self.assertFalse(model_part.GetNode(3).IsFixed(DISPLACEMENT_X))
+        self.assertFalse(model_part.GetNode(972).IsFixed(DISPLACEMENT_X))
+        self.assertTrue(model_part.GetNode(973).IsFixed(DISPLACEMENT_X))
+        self.assertTrue(model_part.GetNode(974).IsFixed(DISPLACEMENT_X))
+
+        self.assertTrue(model_part.GetNode(1).IsFixed(DISPLACEMENT_Y))
+        self.assertTrue(model_part.GetNode(2).IsFixed(DISPLACEMENT_Y))
+        self.assertFalse(model_part.GetNode(3).IsFixed(DISPLACEMENT_Y))
+        self.assertFalse(model_part.GetNode(972).IsFixed(DISPLACEMENT_Y))
+        self.assertTrue(model_part.GetNode(973).IsFixed(DISPLACEMENT_Y))
+        self.assertTrue(model_part.GetNode(974).IsFixed(DISPLACEMENT_Y))
+
+        self.assertTrue(model_part.GetNode(1).IsFixed(DISPLACEMENT_Z))
+        self.assertTrue(model_part.GetNode(2).IsFixed(DISPLACEMENT_Z))
+        self.assertFalse(model_part.GetNode(3).IsFixed(DISPLACEMENT_Z))
+        self.assertFalse(model_part.GetNode(972).IsFixed(DISPLACEMENT_Z))
+        self.assertTrue(model_part.GetNode(973).IsFixed(DISPLACEMENT_Z))
+        self.assertTrue(model_part.GetNode(974).IsFixed(DISPLACEMENT_Z))
+
+        self.assertEqual(model_part.GetNode(1).GetSolutionStepValue(DISPLACEMENT_X),0.1)
+        self.assertEqual(model_part.GetNode(2).GetSolutionStepValue(DISPLACEMENT_X),0.2)
+        self.assertEqual(model_part.GetNode(3).GetSolutionStepValue(DISPLACEMENT_X),0.0)
+        self.assertEqual(model_part.GetNode(972).GetSolutionStepValue(DISPLACEMENT_X),0.0)
+        self.assertEqual(model_part.GetNode(973).GetSolutionStepValue(DISPLACEMENT_X),0.0)
+        self.assertEqual(model_part.GetNode(974).GetSolutionStepValue(DISPLACEMENT_X),0.0)
+
+        self.assertEqual(model_part.GetNode(1).GetSolutionStepValue(DISPLACEMENT_Y),0.0)
+        self.assertEqual(model_part.GetNode(2).GetSolutionStepValue(DISPLACEMENT_Y),0.0)
+        self.assertEqual(model_part.GetNode(3).GetSolutionStepValue(DISPLACEMENT_Y),0.0)
+        self.assertEqual(model_part.GetNode(972).GetSolutionStepValue(DISPLACEMENT_Y),0.0)
+        self.assertEqual(model_part.GetNode(973).GetSolutionStepValue(DISPLACEMENT_Y),0.000973)
+        self.assertEqual(model_part.GetNode(974).GetSolutionStepValue(DISPLACEMENT_Y),0.000974)
+
+        self.assertEqual(model_part.GetNode(1).GetSolutionStepValue(DISPLACEMENT_Z),0.0)
+        self.assertEqual(model_part.GetNode(2).GetSolutionStepValue(DISPLACEMENT_Z),0.0)
+        self.assertEqual(model_part.GetNode(3).GetSolutionStepValue(DISPLACEMENT_Z),0.0)
+        self.assertEqual(model_part.GetNode(972).GetSolutionStepValue(DISPLACEMENT_Z),0.0)
+        self.assertEqual(model_part.GetNode(973).GetSolutionStepValue(DISPLACEMENT_Z),0.0)
+        self.assertEqual(model_part.GetNode(974).GetSolutionStepValue(DISPLACEMENT_Z),0.0)
+
+        self.assertEqual(model_part.GetNode(1).GetSolutionStepValue(VISCOSITY),0.01)
+        self.assertEqual(model_part.GetNode(2).GetSolutionStepValue(VISCOSITY),0.01)
+        self.assertEqual(model_part.GetNode(3).GetSolutionStepValue(VISCOSITY),0.0)
+        self.assertEqual(model_part.GetNode(972).GetSolutionStepValue(VISCOSITY),0.0)
+        self.assertEqual(model_part.GetNode(973).GetSolutionStepValue(VISCOSITY),0.01)
+        self.assertEqual(model_part.GetNode(974).GetSolutionStepValue(VISCOSITY),0.01)
+
+        self.assertTrue(model_part.HasSubModelPart("Inlets"))
+
+        inlets_model_part = model_part.GetSubModelPart("Inlets")
+
+        self.assertEqual(inlets_model_part.NumberOfTables(), 1)
+        self.assertEqual(inlets_model_part.NumberOfProperties(), 0)
+        self.assertEqual(inlets_model_part.NumberOfNodes(), 3)
+        self.assertEqual(inlets_model_part.NumberOfElements(), 1)
+        self.assertEqual(inlets_model_part.NumberOfConditions(), 3)
+        self.assertEqual(inlets_model_part.NumberOfSubModelParts(), 2)
+        self.assertTrue(inlets_model_part.HasSubModelPart("Inlet1"))
+        self.assertTrue(inlets_model_part.HasSubModelPart("Inlet2"))
+
+        inlet1_model_part = inlets_model_part.GetSubModelPart("Inlet1")
+
+        self.assertEqual(inlet1_model_part.NumberOfTables(), 0)
+        self.assertEqual(inlet1_model_part.NumberOfProperties(), 0)
+        self.assertEqual(inlet1_model_part.NumberOfNodes(), 2)
+        self.assertEqual(inlet1_model_part.NumberOfElements(), 0)
+        self.assertEqual(inlet1_model_part.NumberOfConditions(), 2)
+        self.assertEqual(inlet1_model_part.NumberOfSubModelParts(), 0)
+
+        inlet2_model_part = inlets_model_part.GetSubModelPart("Inlet2")
+
+        self.assertEqual(inlet2_model_part.NumberOfTables(), 0)
+        self.assertEqual(inlet2_model_part.NumberOfProperties(), 0)
+        self.assertEqual(inlet2_model_part.NumberOfNodes(), 0)
+        self.assertEqual(inlet2_model_part.NumberOfElements(), 0)
+        self.assertEqual(inlet2_model_part.NumberOfConditions(), 2)
+        self.assertEqual(inlet2_model_part.NumberOfSubModelParts(), 0)
+
+        self.assertTrue(model_part.HasSubModelPart("Outlet"))
+
+        outlet_model_part = model_part.GetSubModelPart("Outlet")
+
+ 
+        self.assertEqual(outlet_model_part.NumberOfTables(), 0)
+        self.assertEqual(outlet_model_part.NumberOfProperties(), 1)
+        self.assertEqual(outlet_model_part.NumberOfNodes(), 0)
+        self.assertEqual(outlet_model_part.NumberOfElements(), 0)
+        self.assertEqual(outlet_model_part.NumberOfConditions(), 1)
+        self.assertEqual(outlet_model_part.NumberOfSubModelParts(), 0)
+
+
+
+    #def test_model_part_io_properties_block(self):
+    #    model_part = ModelPart("Main")
+    #    model_part_io = ModelPartIO("test_model_part_io")
+    #    model_part_io.ReadProperties(model_part.Properties)
+
+if __name__ == '__main__':
+    unittest.main()
