2394e0b4b7ea944d9cf0baa0007e96b9ebf38102
ipouplana <ipouplana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-11 19:48:28 +0000

    Cleaning bat files.

--- kratos/applications/DamApplication/custom_problemtype/Dam_Application.gid/Dam_Application.unix.bat
+++ kratos/applications/DamApplication/custom_problemtype/Dam_Application.gid/Dam_Application.unix.bat
@@ -1,16 +1,18 @@
 #!/bin/bash
-#    OutputFile: $2/$1.info
-#    ErrorFile: $2/$1.err
+# OutputFile: $2/$1.info
+# ErrorFile: $2/$1.err
 
-#delete previous data and settings 
-rm -f "${2}/${1}.info"
-rm -f "${2}/${1}.err"
-rm -f "${2}/${1}.mdpa"
-rm -f "${2}/ProjectParameters.py"
+# Identification for arguments
+# basename               = ${1}
+# Project directory      = ${2}
+# Problem Type directory = ${3}
+
+
+rm "${2}/${1}.info"
+rm "${2}/${1}.err"
 
 mv "${2}/${1}.dat" "${2}/${1}.mdpa"
 mv "${2}/${1}-1.dat" "${2}/ProjectParameters.py"
-
 cp "${3}/../../python_scripts/thermo_mechanic_script.py" "${2}/"
 
 # Warning: one must properly set the following paths before running this file
@@ -29,6 +31,5 @@ export PATH="/usr/bin/python3:$PATH"
 #Setting PATH for Python 3.5
 #export PATH="/Library/Frameworks/Python.framework/Versions/3.5/bin:${PATH}"
 
-if [ -f "${2}/thermo_mechanic_script.py" ]; then
- python3 "${2}/thermo_mechanic_script.py" > "${2}/${1}.info" 2> "${2}/${1}.err"
-fi
+
+python3 "${2}/thermo_mechanic_script.py" > "${2}/${1}.info" 2> "${2}/${1}.err"
