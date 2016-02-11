c3c542d53304783088bca15b56c45d3b3a80db1f
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-11 11:52:24 +0000

    Added test_KratosCore.py and modified existing tests so they use KratosUnittest instead of Unittest module. Also corrected file location form test_model_part_io.py so it always loads a modelpart from the script directory

---
+++ kratos/kratos/tests/test_KratosCore.py
@@ -0,0 +1,118 @@
+import getopt
+import sys
+
+# import Kratos
+from KratosMultiphysics import *
+
+# Import Kratos "wrapper" for unittests
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+# Import the tests o test_classes to create the suits
+from test_model_part_io import TestModelPartIO as TModelPartIO
+from test_model_part import TestModelPart as TModelPart
+
+
+def Usage():
+    ''' Prints the usage of the script '''
+
+    lines = [
+        'Usage:',
+        '\t python kratos_run_tests [-l level] [-v vervosity]',
+        'Options',
+        '\t -h, --help: Shows this command',
+        '\t -l, --level: Minimum level of detail of the tests: \'all\'(Default) \'(nightly)\' \'(small)\'',  # noqa
+        '\t -v, --verbose: Vervosty level: 0, 1 (Default), 2'
+    ]
+
+    for l in lines:
+        print(l)
+
+
+def AssambleTestSuits():
+    ''' Generates the test suits to run.
+
+    Generates the test suits to run. At least it will return the suits:
+    "small", "nighlty" and "all"
+
+    Return
+    ------
+
+    set of suits:
+        A set of suits in the form of {"suitname": suit}.
+
+    '''
+
+    # Create a test suit with the selected tests (Small tests):
+    SmallSuite = KratosUnittest.TestSuite()
+    SmallSuite.addTest(TModelPartIO('test_model_part_io_read_model_part'))
+    SmallSuite.addTest(TModelPart('test_model_part_properties'))
+
+    # Create a test suit with the selected tests plus all small tests
+    NightSuite = KratosUnittest.TestSuite([SmallSuite])
+    NightSuite.addTest(TModelPart('test_model_part_sub_model_parts'))
+    NightSuite.addTest(TModelPart('test_model_part_nodes'))
+    NightSuite.addTest(TModelPart('test_model_part_tables'))
+
+    # Create a test suit that contains all the tests:
+    AllModelPartIO = KratosUnittest.TestLoader().loadTestsFromTestCase(
+        TModelPartIO)
+    AllModelPart = KratosUnittest.TestLoader().loadTestsFromTestCase(
+        TModelPart)
+
+    AllSuit = KratosUnittest.TestSuite([
+        AllModelPartIO,
+        AllModelPart
+    ])
+
+    return {
+        'small': SmallSuite,
+        'nightly': NightSuite,
+        'all': AllSuit
+    }
+
+if __name__ == '__main__':
+
+    tests = AssambleTestSuits()
+
+    verbose_values = [0, 1, 2]
+    level_values = ['all', 'small', 'nightly']
+
+    verbosity = 1
+    level = 'all'
+
+    # Parse Commandline
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            'hv:l:', [
+                'help',
+                'verbose=',
+                'level='
+            ])
+    except getopt.GetoptError as err:
+        print(str(err))
+        Usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ('-v', '--verbose'):
+            if int(a) in verbose_values:
+                verbosity = int(a)
+            else:
+                print('Error: {} is not a valid verbose level.'.format(a))
+                Usage()
+                sys.exit()
+        elif o in ('-h', '--help'):
+            Usage()
+            sys.exit()
+        elif o in ('-l', '--level'):
+            if a in level_values:
+                level = a
+            else:
+                print('Error: {} is not a valid level.'.format(a))
+                Usage()
+                sys.exit()
+        else:
+            assert False, 'unhandled option'
+
+    KratosUnittest.TextTestRunner(verbosity=verbosity).run(tests[level])
--- kratos/kratos/tests/test_model_part.py
+++ kratos/kratos/tests/test_model_part.py
@@ -1,7 +1,7 @@
-﻿import unittest
+﻿import KratosMultiphysics.KratosUnittest as KratosUnittest
 from KratosMultiphysics import *
 
-class TestModelPart(unittest.TestCase):
+class TestModelPart(KratosUnittest.TestCase):
 
     def test_model_part_sub_model_parts(self):
         model_part = ModelPart("Main")
@@ -24,8 +24,8 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetSubModelPart("Outlet").Name, "Outlet")
 
         sub_model_part_1 = model_part.GetSubModelPart("Inlets")
-        sub_model_part_1.CreateSubModelPart("Inlet1") 
-        sub_model_part_1.CreateSubModelPart("Inlet2") 
+        sub_model_part_1.CreateSubModelPart("Inlet1")
+        sub_model_part_1.CreateSubModelPart("Inlet2")
 
         self.assertEqual(model_part.NumberOfSubModelParts(), 3)
         self.assertEqual(model_part.GetSubModelPart("Inlets").Name, "Inlets")
@@ -74,14 +74,14 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetNode(1,0).X, 0.00)
         self.assertEqual(len(model_part.Nodes), 1)
 
-        model_part.CreateNewNode(2000, 2.00,0.00,0.00) 
+        model_part.CreateNewNode(2000, 2.00,0.00,0.00)
 
         self.assertEqual(model_part.NumberOfNodes(), 2)
         self.assertEqual(model_part.GetNode(1).Id, 1)
         self.assertEqual(model_part.GetNode(2000).Id, 2000)
         self.assertEqual(model_part.GetNode(2000).X, 2.00)
 
-        model_part.CreateNewNode(2, 2.00,0.00,0.00) 
+        model_part.CreateNewNode(2, 2.00,0.00,0.00)
 
         self.assertEqual(model_part.NumberOfNodes(), 3)
         self.assertEqual(model_part.GetNode(1).Id, 1)
@@ -89,7 +89,7 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetNode(1).X, 0.00)
         self.assertEqual(model_part.GetNode(2).X, 2.00)
 
-        model_part.RemoveNode(2000) 
+        model_part.RemoveNode(2000)
 
         self.assertEqual(model_part.NumberOfNodes(), 2)
 
@@ -97,7 +97,7 @@ class TestModelPart(unittest.TestCase):
         model_part.CreateSubModelPart("Temp")
         model_part.CreateSubModelPart("Outlet")
         inlets_model_part = model_part.GetSubModelPart("Inlets")
-        inlets_model_part.CreateNewNode(3, 3.00,0.00,0.00) 
+        inlets_model_part.CreateNewNode(3, 3.00,0.00,0.00)
 
         self.assertEqual(inlets_model_part.NumberOfNodes(), 1)
         self.assertEqual(inlets_model_part.GetNode(3).Id, 3)
@@ -106,10 +106,10 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetNode(3).Id, 3)
         self.assertEqual(model_part.GetNode(3).X, 3.00)
 
-        inlets_model_part.CreateSubModelPart("Inlet1") 
+        inlets_model_part.CreateSubModelPart("Inlet1")
         inlets_model_part.CreateSubModelPart("Inlet2")
         inlet2_model_part = inlets_model_part.GetSubModelPart("Inlet2")
-        inlet2_model_part.CreateNewNode(4, 4.00,0.00,0.00) 
+        inlet2_model_part.CreateNewNode(4, 4.00,0.00,0.00)
 
         self.assertEqual(inlet2_model_part.NumberOfNodes(), 1)
         self.assertEqual(inlet2_model_part.GetNode(4).Id, 4)
@@ -120,16 +120,16 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfNodes(), 4)
         self.assertEqual(model_part.GetNode(4).Id, 4)
 
-        inlets_model_part.CreateNewNode(5, 5.00,0.00,0.00) 
-        inlets_model_part.CreateNewNode(6, 6.00,0.00,0.00) 
-        inlet2_model_part.CreateNewNode(7, 7.00,0.00,0.00) 
-        inlet2_model_part.CreateNewNode(8, 8.00,0.00,0.00) 
- 
+        inlets_model_part.CreateNewNode(5, 5.00,0.00,0.00)
+        inlets_model_part.CreateNewNode(6, 6.00,0.00,0.00)
+        inlet2_model_part.CreateNewNode(7, 7.00,0.00,0.00)
+        inlet2_model_part.CreateNewNode(8, 8.00,0.00,0.00)
+
         self.assertEqual(inlet2_model_part.NumberOfNodes(), 3)
         self.assertEqual(inlets_model_part.NumberOfNodes(), 6)
         self.assertEqual(model_part.NumberOfNodes(), 8)
         self.assertEqual(model_part.GetNode(4).Id, 4)
-        
+
         inlets_model_part.RemoveNode(4)
 
         self.assertEqual(inlet2_model_part.NumberOfNodes(), 2)
@@ -137,11 +137,11 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfNodes(), 8) # the parent model part remains intact
         self.assertEqual(model_part.GetNode(4).Id, 4)
 
-        inlets_model_part.RemoveNodeFromAllLevels(4) # Remove from all levels will delete it from 
+        inlets_model_part.RemoveNodeFromAllLevels(4) # Remove from all levels will delete it from
 
         self.assertEqual(inlet2_model_part.NumberOfNodes(), 2)
         self.assertEqual(inlets_model_part.NumberOfNodes(), 5)
-        self.assertEqual(model_part.NumberOfNodes(), 7) 
+        self.assertEqual(model_part.NumberOfNodes(), 7)
 
     def test_model_part_tables(self):
         model_part = ModelPart("Main")
@@ -190,7 +190,7 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetProperties()[1].Id, 1)
         self.assertEqual(model_part.GetProperties()[2].Id, 2)
 
-        model_part.RemoveProperties(2000) 
+        model_part.RemoveProperties(2000)
 
         self.assertEqual(model_part.NumberOfProperties(), 2)
 
@@ -198,17 +198,17 @@ class TestModelPart(unittest.TestCase):
         model_part.CreateSubModelPart("Temp")
         model_part.CreateSubModelPart("Outlet")
         inlets_model_part = model_part.GetSubModelPart("Inlets")
-        inlets_model_part.AddProperties(Properties(3)) 
+        inlets_model_part.AddProperties(Properties(3))
 
         self.assertEqual(inlets_model_part.NumberOfProperties(), 1)
         self.assertEqual(inlets_model_part.GetProperties()[3].Id, 3)
         self.assertEqual(model_part.NumberOfProperties(), 3)
         self.assertEqual(model_part.GetProperties()[3].Id, 3)
 
-        inlets_model_part.CreateSubModelPart("Inlet1") 
+        inlets_model_part.CreateSubModelPart("Inlet1")
         inlets_model_part.CreateSubModelPart("Inlet2")
         inlet2_model_part = inlets_model_part.GetSubModelPart("Inlet2")
-        inlet2_model_part.AddProperties(Properties(4)) 
+        inlet2_model_part.AddProperties(Properties(4))
 
         self.assertEqual(inlet2_model_part.NumberOfProperties(), 1)
         self.assertEqual(inlet2_model_part.GetProperties()[4].Id, 4)
@@ -218,15 +218,15 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetProperties()[4].Id, 4)
 
         inlets_model_part.AddProperties(Properties(5))
-        inlets_model_part.AddProperties(Properties(6)) 
+        inlets_model_part.AddProperties(Properties(6))
         inlet2_model_part.AddProperties(Properties(7))
-        inlet2_model_part.AddProperties(Properties(8)) 
- 
+        inlet2_model_part.AddProperties(Properties(8))
+
         self.assertEqual(inlet2_model_part.NumberOfProperties(), 3)
         self.assertEqual(inlets_model_part.NumberOfProperties(), 6)
         self.assertEqual(model_part.NumberOfProperties(), 8)
         self.assertEqual(model_part.GetProperties()[4].Id, 4)
-        
+
         inlets_model_part.RemoveProperties(4)
 
         self.assertEqual(inlet2_model_part.NumberOfProperties(), 2)
@@ -234,11 +234,11 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfProperties(), 8) # the parent model part remains intact
         self.assertEqual(model_part.GetProperties()[4].Id, 4)
 
-        inlets_model_part.RemovePropertiesFromAllLevels(4) # Remove from all levels will delete it from 
+        inlets_model_part.RemovePropertiesFromAllLevels(4) # Remove from all levels will delete it from
 
         self.assertEqual(inlet2_model_part.NumberOfProperties(), 2)
         self.assertEqual(inlets_model_part.NumberOfProperties(), 5)
-        self.assertEqual(model_part.NumberOfProperties(), 7) 
+        self.assertEqual(model_part.NumberOfProperties(), 7)
 
     def test_model_part_elements(self):
         model_part = ModelPart("Main")
@@ -275,7 +275,7 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetElement(1).Id, 1)
         self.assertEqual(model_part.GetElement(2).Id, 2)
 
-        model_part.RemoveElement(2000) 
+        model_part.RemoveElement(2000)
 
         self.assertEqual(model_part.NumberOfElements(), 2)
 
@@ -286,20 +286,20 @@ class TestModelPart(unittest.TestCase):
         inlets_model_part.CreateNewNode(4, 0.00,0.00,0.00)
         inlets_model_part.CreateNewNode(5, 1.00,0.00,0.00)
         inlets_model_part.CreateNewNode(6, 1.00,1.00,0.00)
-        inlets_model_part.CreateNewElement("Element2D3N", 3, [4,5,6], model_part.GetProperties()[1]) 
+        inlets_model_part.CreateNewElement("Element2D3N", 3, [4,5,6], model_part.GetProperties()[1])
 
         self.assertEqual(inlets_model_part.NumberOfElements(), 1)
         self.assertEqual(inlets_model_part.GetElement(3).Id, 3)
         self.assertEqual(model_part.NumberOfElements(), 3)
         self.assertEqual(model_part.GetElement(3).Id, 3)
 
-        inlets_model_part.CreateSubModelPart("Inlet1") 
+        inlets_model_part.CreateSubModelPart("Inlet1")
         inlets_model_part.CreateSubModelPart("Inlet2")
         inlet2_model_part = inlets_model_part.GetSubModelPart("Inlet2")
         inlet2_model_part.CreateNewNode(7, 0.00,0.00,0.00)
         inlet2_model_part.CreateNewNode(8, 1.00,0.00,0.00)
         inlet2_model_part.CreateNewNode(9, 1.00,1.00,0.00)
-        inlet2_model_part.CreateNewElement("Element2D3N", 4, [7,8,9], model_part.GetProperties()[1])  
+        inlet2_model_part.CreateNewElement("Element2D3N", 4, [7,8,9], model_part.GetProperties()[1])
 
         self.assertEqual(inlet2_model_part.NumberOfElements(), 1)
         self.assertEqual(inlet2_model_part.GetElement(4).Id, 4)
@@ -308,16 +308,16 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfElements(), 4)
         self.assertEqual(model_part.GetElement(4).Id, 4)
 
-        inlets_model_part.CreateNewElement("Element2D3N", 5, [7,8,9], model_part.GetProperties()[1]) 
-        inlets_model_part.CreateNewElement("Element2D3N", 6, [7,8,9], model_part.GetProperties()[1]) 
+        inlets_model_part.CreateNewElement("Element2D3N", 5, [7,8,9], model_part.GetProperties()[1])
+        inlets_model_part.CreateNewElement("Element2D3N", 6, [7,8,9], model_part.GetProperties()[1])
         inlet2_model_part.CreateNewElement("Element2D3N", 7, [7,8,9], model_part.GetProperties()[1])
         inlet2_model_part.CreateNewElement("Element2D3N", 8, [7,8,9], model_part.GetProperties()[1])
- 
+
         self.assertEqual(inlet2_model_part.NumberOfElements(), 3)
         self.assertEqual(inlets_model_part.NumberOfElements(), 6)
         self.assertEqual(model_part.NumberOfElements(), 8)
         self.assertEqual(model_part.GetElement(4).Id, 4)
-        
+
         inlets_model_part.RemoveElement(4)
 
         self.assertEqual(inlet2_model_part.NumberOfElements(), 2)
@@ -325,12 +325,12 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfElements(), 8) # the parent model part remains intact
         self.assertEqual(model_part.GetElement(4).Id, 4)
 
-        inlets_model_part.RemoveElementFromAllLevels(4) # Remove from all levels will delete it from 
+        inlets_model_part.RemoveElementFromAllLevels(4) # Remove from all levels will delete it from
 
         self.assertEqual(inlet2_model_part.NumberOfElements(), 2)
         self.assertEqual(inlets_model_part.NumberOfElements(), 5)
-        self.assertEqual(model_part.NumberOfElements(), 7) 
-    
+        self.assertEqual(model_part.NumberOfElements(), 7)
+
     def test_model_part_conditions(self):
         model_part = ModelPart("Main")
 
@@ -366,7 +366,7 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.GetCondition(1).Id, 1)
         self.assertEqual(model_part.GetCondition(2).Id, 2)
 
-        model_part.RemoveCondition(2000) 
+        model_part.RemoveCondition(2000)
 
         self.assertEqual(model_part.NumberOfConditions(), 2)
 
@@ -377,20 +377,20 @@ class TestModelPart(unittest.TestCase):
         inlets_model_part.CreateNewNode(4, 0.00,0.00,0.00)
         inlets_model_part.CreateNewNode(5, 1.00,0.00,0.00)
         inlets_model_part.CreateNewNode(6, 1.00,1.00,0.00)
-        inlets_model_part.CreateNewCondition("Condition3D", 3, [4,5,6], model_part.GetProperties()[1]) 
+        inlets_model_part.CreateNewCondition("Condition3D", 3, [4,5,6], model_part.GetProperties()[1])
 
         self.assertEqual(inlets_model_part.NumberOfConditions(), 1)
         self.assertEqual(inlets_model_part.GetCondition(3).Id, 3)
         self.assertEqual(model_part.NumberOfConditions(), 3)
         self.assertEqual(model_part.GetCondition(3).Id, 3)
 
-        inlets_model_part.CreateSubModelPart("Inlet1") 
+        inlets_model_part.CreateSubModelPart("Inlet1")
         inlets_model_part.CreateSubModelPart("Inlet2")
         inlet2_model_part = inlets_model_part.GetSubModelPart("Inlet2")
         inlet2_model_part.CreateNewNode(7, 0.00,0.00,0.00)
         inlet2_model_part.CreateNewNode(8, 1.00,0.00,0.00)
         inlet2_model_part.CreateNewNode(9, 1.00,1.00,0.00)
-        inlet2_model_part.CreateNewCondition("Condition3D", 4, [7,8,9], model_part.GetProperties()[1])  
+        inlet2_model_part.CreateNewCondition("Condition3D", 4, [7,8,9], model_part.GetProperties()[1])
 
         self.assertEqual(inlet2_model_part.NumberOfConditions(), 1)
         self.assertEqual(inlet2_model_part.GetCondition(4).Id, 4)
@@ -399,16 +399,16 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfConditions(), 4)
         self.assertEqual(model_part.GetCondition(4).Id, 4)
 
-        inlets_model_part.CreateNewCondition("Condition3D", 5, [7,8,9], model_part.GetProperties()[1]) 
-        inlets_model_part.CreateNewCondition("Condition3D", 6, [7,8,9], model_part.GetProperties()[1]) 
+        inlets_model_part.CreateNewCondition("Condition3D", 5, [7,8,9], model_part.GetProperties()[1])
+        inlets_model_part.CreateNewCondition("Condition3D", 6, [7,8,9], model_part.GetProperties()[1])
         inlet2_model_part.CreateNewCondition("Condition3D", 7, [7,8,9], model_part.GetProperties()[1])
         inlet2_model_part.CreateNewCondition("Condition3D", 8, [7,8,9], model_part.GetProperties()[1])
- 
+
         self.assertEqual(inlet2_model_part.NumberOfConditions(), 3)
         self.assertEqual(inlets_model_part.NumberOfConditions(), 6)
         self.assertEqual(model_part.NumberOfConditions(), 8)
         self.assertEqual(model_part.GetCondition(4).Id, 4)
-        
+
         inlets_model_part.RemoveCondition(4)
 
         self.assertEqual(inlet2_model_part.NumberOfConditions(), 2)
@@ -416,11 +416,11 @@ class TestModelPart(unittest.TestCase):
         self.assertEqual(model_part.NumberOfConditions(), 8) # the parent model part remains intact
         self.assertEqual(model_part.GetCondition(4).Id, 4)
 
-        inlets_model_part.RemoveConditionFromAllLevels(4) # Remove from all levels will delete it from 
+        inlets_model_part.RemoveConditionFromAllLevels(4) # Remove from all levels will delete it from
 
         self.assertEqual(inlet2_model_part.NumberOfConditions(), 2)
         self.assertEqual(inlets_model_part.NumberOfConditions(), 5)
-        self.assertEqual(model_part.NumberOfConditions(), 7) 
+        self.assertEqual(model_part.NumberOfConditions(), 7)
 
     def test_modelpart_variables_list(self):
         model_part = ModelPart("Main")
@@ -430,11 +430,11 @@ class TestModelPart(unittest.TestCase):
         model_part.CreateNewNode(1, 0.00,0.00,0.00)
         model_part.CreateNewNode(2, 1.00,0.00,0.00)
         model_part.CreateNewNode(3, 1.00,1.00,0.00)
-        
+
         self.assertTrue(model_part.Nodes[1].SolutionStepsDataHas(VELOCITY))
         self.assertTrue(model_part.Nodes[1].SolutionStepsDataHas(VELOCITIES))
 
-    
+
 
 if __name__ == '__main__':
     unittest.main()
