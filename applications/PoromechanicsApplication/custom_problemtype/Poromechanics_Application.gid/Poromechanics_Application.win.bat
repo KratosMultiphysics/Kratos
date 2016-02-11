326cecc3c00a6710272f797f9205d1e2abba2c5a
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2016-02-11 19:49:03 +0000

    Cleaning bat files.

--- applications/PoromechanicsApplication/custom_problemtype/Poromechanics_Application.gid/Poromechanics_Application.win.bat
+++ applications/PoromechanicsApplication/custom_problemtype/Poromechanics_Application.gid/Poromechanics_Application.win.bat
@@ -1,22 +1,20 @@
 REM @ECHO OFF
+
 REM Identification for arguments
-REM basename                          = %1
-REM Project directory                 = %2
-REM Problem directory                 = %3
+REM basename               = %1
+REM Project directory      = %2
+REM Problem Type directory = %3
  
 REM OutputFile: "%2\%1.info"
 REM ErrorFile: "%2\%1.err"
 
+
 del "%2\%1.info"
 del "%2\%1.err"
-del "%2\%1.mdpa"
-del "%2\ProjectParameters.py"
 
 move "%2\%1.dat" "%2\%1.mdpa"
 move "%2\%1-1.dat" "%2\ProjectParameters.py"
 copy "%3\\..\\..\\python_scripts\\main_script.py" "%2\\"
 
-REM Updathe PATH
-set PATH=C:\\KratosMultiphysics\\KratosMultiphysics\\libs;%PATH%
 
-C:\\KratosMultiphysics\\runkratos.exe %2\\main_script.py > "%2\%1.info" 2> "%2\%1.err"
+"C:\\KratosMultiphysics\\runkratos.exe" "%2\\main_script.py" > "%2\%1.info" 2> "%2\%1.err"
