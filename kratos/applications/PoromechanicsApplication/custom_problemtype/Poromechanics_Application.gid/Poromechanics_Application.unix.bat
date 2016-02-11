711c9fd377c1b767b4f87508e69290d640329028
ipouplana <ipouplana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-11 19:49:03 +0000

    Cleaning bat files.

--- kratos/applications/PoromechanicsApplication/custom_problemtype/Poromechanics_Application.gid/Poromechanics_Application.unix.bat
+++ kratos/applications/PoromechanicsApplication/custom_problemtype/Poromechanics_Application.gid/Poromechanics_Application.unix.bat
@@ -1,18 +1,19 @@
 #!/bin/bash
-#    OutputFile: $2/$1.info
-#    ErrorFile: $2/$1.err
+# OutputFile: $2/$1.info
+# ErrorFile: $2/$1.err
 
-#delete previous data and settings 
-rm -f "${2}/${1}.info"
-rm -f "${2}/${1}.err"
-rm -f "${2}/${1}.mdpa"
-rm -f "${2}/ProjectParameters.py"
+# Identification for arguments
+# basename               = ${1}
+# Project directory      = ${2}
+# Problem Type directory = ${3}
+
+
+rm "${2}/${1}.info"
+rm "${2}/${1}.err"
 
 mv "${2}/${1}.dat" "${2}/${1}.mdpa"
 mv "${2}/${1}-1.dat" "${2}/ProjectParameters.py"
-
 cp "${3}/../../python_scripts/main_script.py" "${2}/"
-
 
 # Warning: one must properly set the following paths before running this file
 
@@ -23,7 +24,6 @@ export LD_LIBRARY_PATH="/path/to/kratos/libs:/path/to/boost/stage/lib:$LD_LIBRAR
 #Setting PATH for Python 3
 export PATH="/usr/bin/python3:$PATH"
 
-
 #OS X
 #Setting PATHs for kratos
 #export PYTHONPATH="/path/to/kratos:$PYTHONPATH"
@@ -32,7 +32,4 @@ export PATH="/usr/bin/python3:$PATH"
 #export PATH="/Library/Frameworks/Python.framework/Versions/3.5/bin:${PATH}"
 
 
-# Launch the script
-if [ -f "${2}/main_script.py" ]; then
- python3 "${2}/main_script.py" > "${2}/${1}.info" 2> "${2}/${1}.err"
-fi
+python3 "${2}/main_script.py" > "${2}/${1}.info" 2> "${2}/${1}.err"
