413a088131f2944b5c8de881d0b9de623cf8bc02
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-15 16:20:17 +0000

    Existing tests now use the new api from KratosUnittest

--- kratos/kratos/tests/test_KratosCore.py
+++ kratos/kratos/tests/test_KratosCore.py
@@ -1,8 +1,5 @@
 from __future__ import print_function, absolute_import, division
 
-import getopt
-import sys
-
 # import Kratos
 from KratosMultiphysics import *
 
@@ -14,26 +11,10 @@ from test_model_part_io import TestModelPartIO as TModelPartIO
 from test_model_part import TestModelPart as TModelPart
 
 
-def Usage():
-    ''' Prints the usage of the script '''
-
-    lines = [
-        'Usage:',
-        '\t python kratos_run_tests [-l level] [-v vervosity]',
-        'Options',
-        '\t -h, --help: Shows this command',
-        '\t -l, --level: Minimum level of detail of the tests: \'all\'(Default) \'(nightly)\' \'(small)\'',  # noqa
-        '\t -v, --verbose: Vervosty level: 0, 1 (Default), 2'
-    ]
-
-    for l in lines:
-        print(l)
-
-
 def AssambleTestSuits():
     ''' Generates the test suits to run.
 
-    Generates the test suits to run. At least it will return the suits:
+    Generates the test suits to run. At least, it needs to return the suits:
     "small", "nighlty" and "all"
 
     Return
@@ -73,48 +54,4 @@ def AssambleTestSuits():
     }
 
 if __name__ == '__main__':
-
-    tests = AssambleTestSuits()
-
-    verbose_values = [0, 1, 2]
-    level_values = ['all', 'small', 'nightly']
-
-    verbosity = 1
-    level = 'all'
-
-    # Parse Commandline
-    try:
-        opts, args = getopt.getopt(
-            sys.argv[1:],
-            'hv:l:', [
-                'help',
-                'verbose=',
-                'level='
-            ])
-    except getopt.GetoptError as err:
-        print(str(err))
-        Usage()
-        sys.exit(2)
-
-    for o, a in opts:
-        if o in ('-v', '--verbose'):
-            if int(a) in verbose_values:
-                verbosity = int(a)
-            else:
-                print('Error: {} is not a valid verbose level.'.format(a))
-                Usage()
-                sys.exit()
-        elif o in ('-h', '--help'):
-            Usage()
-            sys.exit()
-        elif o in ('-l', '--level'):
-            if a in level_values:
-                level = a
-            else:
-                print('Error: {} is not a valid level.'.format(a))
-                Usage()
-                sys.exit()
-        else:
-            assert False, 'unhandled option'
-
-    KratosUnittest.TextTestRunner(verbosity=verbosity).run(tests[level])
+    KratosUnittest.runTests(AssambleTestSuits())
