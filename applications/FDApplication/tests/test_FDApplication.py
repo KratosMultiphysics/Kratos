8e6e2036a89e97a4df47a6d8e2675d37cd5c00d2
Carlos Roig <roigcarlo@gmail.com>
2016-02-15 16:20:17 +0000

    Existing tests now use the new api from KratosUnittest

--- applications/FDApplication/tests/test_FDApplication.py
+++ applications/FDApplication/tests/test_FDApplication.py
@@ -1,26 +1,50 @@
-import unittest
-
+# import Kratos
 from KratosMultiphysics import *
 from KratosMultiphysics.FDApplication import *
 
+import sys
 
-class TestFDApplication(unittest.TestCase):
-    def setUp(self):
-        pass
+# Import Kratos "wrapper" for unittests
+import KratosMultiphysics.KratosUnittest as KratosUnittest
 
-    def tearDown(self):
-        pass
+# Import the tests o test_classes to create the suits
+from SmallTests import Bfecc as TBfecc
 
-    def testNormal(self):
-        pass
 
+def AssambleTestSuits():
+    ''' Generates the test suits to run.
+
+    Generates the test suits to run. At least, it needs to return the suits:
+    "small", "nighlty" and "all"
+
+    Return
+    ------
+
+    set of suits:
+        A set of suits in the form of {"suitname": suit}.
+
+    '''
+
+    # Create a test suit with the selected tests (Small tests):
+    SmallSuite = KratosUnittest.TestSuite()
+    SmallSuite.addTest(TBfecc('testCreateSolver'))
+
+    # Create a test suit with the selected tests plus all small tests
+    NightSuite = KratosUnittest.TestSuite([SmallSuite])
+
+    # Create a test suit that contains all the tests:
+    AllBfecc = KratosUnittest.TestLoader().loadTestsFromTestCase(
+        TBfecc)
+
+    AllSuit = KratosUnittest.TestSuite([
+        AllBfecc,
+    ])
+
+    return {
+        'small': SmallSuite,
+        'nightly': NightSuite,
+        'all': AllSuit
+    }
 
 if __name__ == '__main__':
-
-    # Would run everything
-    # unittest.main()
-
-    # would run only test from suit N
-    FDApplicationSuit = unittest.TestLoader().loadTestsFromTestCase(TestFDApplication)
-
-    unittest.TextTestRunner(verbosity=2).run(FDApplicationSuit)
+    KratosUnittest.runTests(AssambleTestSuits())
