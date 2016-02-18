aee9c4b99a9a458aad56eec40de24483e4ae8e09
fjgarate <fjgarate@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-18 16:02:00 +0000

    Fluid Application

---
+++ kratos/interfaces/GiD/kratos.gid/apps/Fluid/xml/GetFromXML.tcl
@@ -0,0 +1,37 @@
+namespace eval Fluid::xml {
+    # Namespace variables declaration
+    variable dir
+}
+
+proc Fluid::xml::Init { } {
+    # Namespace variables inicialization
+    variable dir
+    Model::InitVariables dir $Fluid::dir
+    
+    getSolutionStrategies
+    getElements
+    getConstitutiveLaws
+    #getConditions
+    #getSolvers
+}
+
+
+proc Fluid::xml::getSolutionStrategies { } {
+    Model::InitVariables SolutionStrategyFileName strategydefinition.xml
+    Model::getSolutionStrategies
+}
+
+proc Fluid::xml::getElements { } {
+    Model::InitVariables ElementsFileName Elements.xml
+    Model::getElements
+}
+proc Fluid::xml::getConstitutiveLaws { } {
+    Model::InitVariables ConstitutiveLawsFileName ConstitutiveLaws.xml
+    Model::getConstitutiveLaws
+}
+
+proc Fluid::xml::getUniqueName {name} {
+    return FL$name
+}
+
+Fluid::xml::Init
