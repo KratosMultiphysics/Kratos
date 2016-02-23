221ea61081a172303d010348a1ec08abcafbb458
Salva Latorre <latorre@cimne.upc.edu>
2016-02-23 16:33:53 +0000

    Aesthetics.

--- applications/DEM_application/custom_elements/cylinder_continuum_particle.h
+++ applications/DEM_application/custom_elements/cylinder_continuum_particle.h
@@ -2,7 +2,6 @@
 // Author: Miquel Santasusana msantasusana@cimne.upc.edu
 //
 
-
 #if !defined(KRATOS_CYLINDER_CONTINUUM_PARTICLE_H_INCLUDED )
 #define  KRATOS_CYLINDER_CONTINUUM_PARTICLE_H_INCLUDED
 
@@ -10,15 +9,10 @@
 #include <string>
 #include <iostream>
 
-
-// External includes
-
-
 // Project includes
 #include "includes/define.h"
 #include "discrete_element.h"
 #include "spheric_continuum_particle.h"
-
 
 
 namespace Kratos
