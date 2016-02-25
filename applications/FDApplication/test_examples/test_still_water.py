9e4a3782c02ee5cf3a4915806d807dceb887fd55
Carlos Roig <roigcarlo@gmail.com>
2016-02-25 16:37:49 +0000

    Updated base example using the new python interface options

--- applications/FDApplication/test_examples/test_still_water.py
+++ applications/FDApplication/test_examples/test_still_water.py
@@ -46,7 +46,6 @@ def GenerateGidIO(params):
 
     return gid_io
 
-
 # =========================================================================== #
 #                                                                             #
 # =========================================================================== #
@@ -88,106 +87,19 @@ gid_io_configuration = {
 
 solver = BfeccSolverStrategy(model_part)
 
+solver.Dt = 1.0 / 32.0
+solver.NumCells = [48, 48, 48]
+solver.BorderWidth = [1, 1, 1]
+
 solver.Initialize()
 
-for i in range(0, 500):
-    # print("Convecting iteration {}".format(i))
+# Main loop
+for i in range(0, 100):
+
+    # Solve
     solver.SolveSolutionStep()
 
-# # Stepping and time settings
-# Dt = ProjectParameters.Dt
-# full_Dt = Dt
-# initial_Dt = 0.001 * full_Dt  # 0.05 #0.01
-# Nsteps = ProjectParameters.nsteps
-# final_time = ProjectParameters.max_time
-# output_time = ProjectParameters.output_time
-#
-# time = ProjectParameters.Start_time
-# out = 0
-# step = 0
-#
-# while(time <= final_time):
-#
-#     if(step < 3):
-#         Dt = initial_Dt
-#     else:
-#         Dt = full_Dt
-#
-#     time = time + Dt
-#     step = step + 1
-#     fluid_model_part.CloneTimeStep(time)
-#
-#     print("STEP = ", step)
-#     print("TIME = ", time)
-#
-#     if(step >= 3):
-#         fluid_solver.Solve()
-#
-#         if(step < 4):
-#             for k in range(0, ProjectParameters.divergence_cleareance_step):
-#                 print("DOING DIVERGENCE CLEAREANCE")
-#                 buffer_size = fluid_model_part.GetBufferSize()
-#                 for i in range(0, buffer_size):
-#                     for node in fluid_model_part.Nodes:
-#                         vel = node.GetSolutionStepValue(VELOCITY)
-#                         node.SetSolutionStepValue(VELOCITY, i, vel)
-#                         node.SetSolutionStepValue(PRESSURE, i, 0.0)
-#                     if(SolverType == "monolithic_solver_eulerian"):
-#                         zero_vector = Vector(3)
-#                         zero_vector[0] = 0.0
-#                         zero_vector[1] = 0.0
-#                         zero_vector[2] = 0.0
-#                         for node in fluid_model_part.Nodes:
-#                             node.SetSolutionStepValue(ACCELERATION, i, zero_vector)
-#                     if(ProjectParameters.TurbulenceModel == "Spalart-Allmaras"):
-#                         for node in fluid_model_part.Nodes:
-#                             visc = node.GetSolutionStepValue(VISCOSITY)
-#                             node.SetSolutionStepValue(VISCOSITY, i, visc)
-#
-#                 fluid_solver.Solve()
-#
-#         graph_printer.PrintGraphs(time)
-#         PrintDrag(drag_list, drag_file_output_list, fluid_model_part, time)
-#
-#     if(output_time <= out):
-#         if(ProjectParameters.VolumeOutput):
-#             if Multifile:
-#                 gid_io.InitializeMesh(time)
-#                 gid_io.WriteMesh(fluid_model_part.GetMesh())
-#                 gid_io.FinalizeMesh()
-#
-#                 gid_io.InitializeResults(time, (fluid_model_part).GetMesh())
-#
-#             PrintResults(fluid_model_part)
-#             out = 0
-#         else:
-#             cut_model_part.CloneTimeStep(time)
-#             Cut_App.UpdateCutData(cut_model_part, fluid_model_part)
-#
-#             if Multifile:
-#                 gid_io.InitializeMesh(time)
-#                 gid_io.WriteMesh(cut_model_part.GetMesh())
-#                 gid_io.FinalizeMesh()
-#
-#                 gid_io.InitializeResults(time, (cut_model_part).GetMesh())
-#
-#             PrintResults(cut_model_part)
-#
-#             out = 0
-#
-#         if Multifile:
-#             gid_io.FinalizeResults()
-#             if ProjectParameters.GiDPostMode == "Binary":
-#                 f.write(ProjectParameters.problem_name + '_' + str(time) + '.post.bin\n')
-#             elif ProjectParameters.GiDPostMode == "Ascii":
-#                 f.write(ProjectParameters.problem_name + '_' + str(time) + '.post.msh\n')
-#
-#     out = out + Dt
-#
-# if Multifile:
-#     f.close()
-# else:
-#     gid_io.FinalizeResults()
-#
-# for i in drag_file_output_list:
-#     i.close();
+    # # Print results
+    # if not i % 10:
+    #     print("Printing results for step {}...".format(i))
+    #     solver.WriteResults(i+1)
