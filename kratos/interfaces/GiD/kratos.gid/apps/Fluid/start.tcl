3dfb501bd4dcbed0bcb60bfcfd6515547fd8559f
fjgarate <fjgarate@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-02-29 09:06:24 +0000

    Fluid Write

--- kratos/interfaces/GiD/kratos.gid/apps/Fluid/start.tcl
+++ kratos/interfaces/GiD/kratos.gid/apps/Fluid/start.tcl
@@ -17,4 +17,11 @@ proc ::Fluid::LoadMyFiles { } {
     uplevel #0 [list source [file join $dir write write.tcl]]
 }
 
+proc ::Fluid::DrawAutoGeom { } {
+    # Draw
+    GiD_Process Mescape Geometry Create Object Prism 4 1 1 0 0.0,0.0,1.0 1 3 escape
+    # Groups
+    GiD_Process 'Groups Create Group0 escape 'Groups Edit Rename Group0 Body escape escape escape escape Utilities EntitiesGroups Assign Body Volumes 1 escape Mescape 'Groups Create Group0 escape 'Groups Edit Rename Group0 Inlet escape escape escape escape Utilities EntitiesGroups Assign Inlet Surfaces 6 escape Mescape 'Groups Create Group0 escape 'Groups Edit Rename Group0 Outlet escape escape escape escape Utilities EntitiesGroups Assign Outlet Surfaces 1 escape Mescape 'Groups Create Group0 escape 'Groups Edit Rename Group0 Slip escape escape escape escape Utilities EntitiesGroups Assign Slip Surfaces 2 2 3 5 2 escape Mescape 'Groups Create Group0 escape 'Groups Edit Rename Group0 NoSlip escape escape escape escape Utilities EntitiesGroups Assign NoSlip Surfaces 4 escape Mescape
+}
+
 ::Fluid::Init
