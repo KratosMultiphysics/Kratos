6287f5850aeb89fce4bd372da85d29001bd3db4e
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-03-01 18:27:02 +0000

    python bounding .cpp committed

---
+++ kratos/applications/PfemFluidDynamicsApplication/custom_python/add_custom_bounding_to_python.cpp
@@ -0,0 +1,38 @@
+//
+//   Project Name:        KratosPfemFluidDynamicsApplication $
+//   Created by:          $Author:               JMCarbonell $
+//   Last modified by:    $Co-Author:                        $
+//   Date:                $Date:               February 2016 $
+//   Revision:            $Revision:                     0.0 $
+//
+//
+
+// System includes
+
+// External includes
+#include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+
+// Project includes
+#include "includes/define.h"
+#include "processes/process.h"
+#include "custom_python/add_custom_bounding_to_python.h"
+
+// Meshers
+
+// Bounding Boxes
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+  void  AddCustomBoundingToPython()
+  {
+  }
+
+}  // namespace Python.
+
+} // Namespace Kratos
+
