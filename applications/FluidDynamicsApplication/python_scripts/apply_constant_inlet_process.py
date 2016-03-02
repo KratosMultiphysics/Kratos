46329188400da33484736605c0f1ef2fd3ac0f77
rrossi <rrossi@cimne.upc.edu>
2016-03-02 17:55:23 +0000

    initial commit of the processes needed for the fluid dynamics application

---
+++ applications/FluidDynamicsApplication/python_scripts/apply_constant_inlet_process.py
@@ -0,0 +1,18 @@
+import KratosMultiphysics 
+import KratosMultiphysics.FluidDynamicsApplication 
+
+def Factory(settings, Model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return ApplyConstantInletProcess(Model, settings["Parameters"])
+
+##all the processes python processes should be derived from "python_process"
+class ApplyConstantInletProcess(KratosMultiphysics.ApplyConstantVectorValueProcess):
+    def __init__(self, Model, Parameters ):
+        model_part = Model[Parameters["model_part_name"].GetString()]
+        Parameters["variable_name"].SetString("VELOCITY")
+        Parameters["is_fixed_x"].SetBool(True)
+        Parameters["is_fixed_y"].SetBool(True)
+        Parameters["is_fixed_z"].SetBool(True)
+        KratosMultiphysics.ApplyConstantVectorValueProcess(model_part, Parameters)
+        
