d2bd6c5bbe4860129eeb03208214e7f895dcae9a
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-03-03 14:45:24 +0000

    simplifying the code to just one class parameters

--- kratos/kratos/python/add_kratos_parameters_to_python.cpp
+++ kratos/kratos/python/add_kratos_parameters_to_python.cpp
@@ -32,11 +32,13 @@ void  AddKratosParametersToPython()
 {
     using namespace boost::python;
 
-    class_<Parameters, Parameters::Pointer>("Parameters", no_init) //init<rapidjson::Value& >())
+    class_<Parameters, Parameters::Pointer >("Parameters", init<std::string>()) //init<rapidjson::Value& >())
+        .def(init<Parameters const&>())
         .def("WriteJsonString", &Parameters::WriteJsonString)
         .def("PrettyPrintJsonString", &Parameters::PrettyPrintJsonString)
         .def("Has", &Parameters::Has)
-        .def("GetValue", &Parameters::GetValue)
+        .def("Clone", &Parameters::Clone)
+        //.def("GetValue", &Parameters::GetValue) //Do not export this method. users shall adopt the operator [] syntax
         .def("IsNumber", &Parameters::IsNumber)
         .def("IsDouble", &Parameters::IsDouble)
         .def("IsInt", &Parameters::IsInt)
@@ -53,24 +55,24 @@ void  AddKratosParametersToPython()
         .def("SetBool", &Parameters::SetBool)
         .def("SetString", &Parameters::SetString)
         .def("size", &Parameters::size)
-        .def("GetArrayItem", &Parameters::GetArrayItem)
+        //.def("GetArrayItem", &Parameters::GetArrayItem) //Do not export this method. users shall adopt the operator [] syntax
         .def("__setitem__", &Parameters::SetValue)
         .def("__getitem__", &Parameters::GetValue)
         .def("__setitem__", &Parameters::SetArrayItem)
         .def("__getitem__", &Parameters::GetArrayItem)
         ; 
 
-    class_<KratosParameters, KratosParameters::Pointer>("KratosParameters", init<std::string>())
-        .def(init<KratosParameters const&>())
-        .def("WriteJsonString", &KratosParameters::WriteJsonString)
-        .def("PrettyPrintJsonString", &KratosParameters::PrettyPrintJsonString)
-        .def("GetValue", &KratosParameters::GetValue)
-        .def("Has", &KratosParameters::Has)
-        .def("__setitem__", &KratosParameters::SetValue)
-        .def("__getitem__", &KratosParameters::GetValue)
-        .def("__setitem__", &KratosParameters::SetArrayItem)
-        .def("__getitem__", &KratosParameters::GetArrayItem)        
-        ;   
+//     class_<KratosParameters, KratosParameters::Pointer>("KratosParameters", init<std::string>())
+//         .def(init<KratosParameters const&>())
+//         .def("WriteJsonString", &KratosParameters::WriteJsonString)
+//         .def("PrettyPrintJsonString", &KratosParameters::PrettyPrintJsonString)
+//         .def("GetValue", &KratosParameters::GetValue)
+//         .def("Has", &KratosParameters::Has)
+//         .def("__setitem__", &KratosParameters::SetValue)
+//         .def("__getitem__", &KratosParameters::GetValue)
+//         .def("__setitem__", &KratosParameters::SetArrayItem)
+//         .def("__getitem__", &KratosParameters::GetArrayItem)        
+//         ;   
 
   
 
