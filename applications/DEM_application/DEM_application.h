11c5947b9bc3c13b7babef86cb81639a9b09523a
msantasusana <msantasusana@cimne.upc.edu>
2016-03-18 19:16:48 +0000

    solid condition

--- applications/DEM_application/DEM_application.h
+++ applications/DEM_application/DEM_application.h
@@ -46,6 +46,7 @@
 #include "custom_elements/custom_clusters/ballast5cluster3D.h"
 #include "custom_elements/custom_clusters/ballast6cluster3D.h"
 #include "custom_conditions/mapping_condition.h"
+#include "custom_conditions/SolidFace.h"
 #include "custom_conditions/RigidFace.h"
 #include "custom_conditions/RigidEdge.h"
 #include "custom_constitutive/DEM_Dempack_CL.h"
@@ -108,6 +109,8 @@ private:
     const ThermalSphericParticle mThermalSphericContinuumParticle3D;  
     const ParticleContactElement mParticleContactElement;
     const VariablesList mVariablesList;
+    const SolidFace3D  mSolidFace3D3N;
+    const SolidFace3D  mSolidFace3D4N;
     const RigidFace3D  mRigidFace3D3N;
     const RigidFace3D  mRigidFace3D4N;
     const RigidEdge3D  mRigidEdge3D2N;
