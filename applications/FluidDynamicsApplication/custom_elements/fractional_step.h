3e9f564238bc975f6ff240efb4dbce30242ba6b3
rrossi <rrossi@cimne.upc.edu>
2016-03-21 10:49:24 +0000

    correcting a compilation error

--- applications/FluidDynamicsApplication/custom_elements/fractional_step.h
+++ applications/FluidDynamicsApplication/custom_elements/fractional_step.h
@@ -173,7 +173,7 @@ namespace Kratos
 	Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes,
                             Element::PropertiesType::Pointer pProperties) const
 	{
-	  return boost::make_shared< FractionalStep<TDim> >(NewId, this->GetGeometry().Create(ThisNodes), pProperties));
+	  return boost::make_shared< FractionalStep<TDim> >(NewId, this->GetGeometry().Create(ThisNodes), pProperties);
 	}
 	        /**
          * Returns a pointer to a new FractionalStep element, created using given input
@@ -183,9 +183,9 @@ namespace Kratos
          * @return a Pointer to the new element
          */
 		
-        Element::Pointer Create(IndexType NewId, GeometryType::Pointer pGeom, PropertiesType::Pointer pProperties) const
+        Element::Pointer Create(IndexType NewId, Element::GeometryType::Pointer pGeom, Element::PropertiesType::Pointer pProperties) const
         {
-	  return boost::make_shared< FractionalStep<TDim> >(NewId, pGeom, pProperties));
+	  return boost::make_shared< FractionalStep<TDim> >(NewId, pGeom, pProperties);
         }
 
         virtual void Initialize();
