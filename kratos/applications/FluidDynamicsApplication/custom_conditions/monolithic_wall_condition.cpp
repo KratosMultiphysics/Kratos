43d6e98fbd6e283a2ad87f57172c4628c8a8c632
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-03-21 09:57:59 +0000

    adding a Create method with geometry and changing the header to the new format

--- kratos/applications/FluidDynamicsApplication/custom_conditions/monolithic_wall_condition.cpp
+++ kratos/applications/FluidDynamicsApplication/custom_conditions/monolithic_wall_condition.cpp
@@ -1,3 +1,15 @@
+//    |  /           | 
+//    ' /   __| _` | __|  _ \   __| 
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/ 
+//                   Multi-Physics  
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Jordi Cotela
+//
+
 #include "monolithic_wall_condition.h"
 
 namespace Kratos
