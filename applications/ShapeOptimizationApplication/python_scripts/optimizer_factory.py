3ec9066c6db95e205ec2cb169d15c8026979d0b1
dbaumgaertner <dbaumgaertner@cimne.upc.edu>
2016-03-21 14:58:17 +0000

    Fix of minor commit issues

--- applications/ShapeOptimizationApplication/python_scripts/optimizer_factory.py
+++ applications/ShapeOptimizationApplication/python_scripts/optimizer_factory.py
@@ -45,7 +45,7 @@
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
-# Import
+# Imports
 # ------------------------------------------------------------------------------
 # Making KratosMultiphysics backward compatible with python 2.6 and 2.7
 from __future__ import print_function, absolute_import, division 
