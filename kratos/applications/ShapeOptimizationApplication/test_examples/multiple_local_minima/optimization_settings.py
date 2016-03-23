70765ee50ad9b8c4b43450042bf6a403f1ae70b6
mandre <mandre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-03-23 16:32:33 +0000

    new test example

---
+++ kratos/applications/ShapeOptimizationApplication/test_examples/multiple_local_minima/optimization_settings.py
@@ -0,0 +1,24 @@
+class KratosShapeSettings:
+    design_history_directory = "./history"
+    design_history_file = design_history_directory + "/design_history.csv"
+    objectives = {"1":{"grad": "provided"}}
+    constraints = {}
+    design_control = "vertex_morphing"
+    design_output_mode = "relative"
+    design_surface_name = "multiple_local_minima"
+    domain_size = 3
+    # filter size = 7.0
+    filter_size = 3.0
+    optimization_algorithm = "steepest_descent"
+    max_opt_iterations = 200
+    relative_tolerance_objective = 1e-3
+    normalize_sensitivities = True
+    step_size_0 = 0.01
+    nodal_results=["OBJECTIVE_SENSITIVITY",
+                   "SHAPE_UPDATE",
+                   "SHAPE_CHANGE_ABSOLUTE"]
+    VolumeOutput = True
+    GiDPostMode = "Binary"
+    GiDWriteMeshFlag = True
+    GiDWriteConditionsFlag = True
+    GiDMultiFileFlag = "Single"
---
+++ kratos/applications/ShapeOptimizationApplication/test_examples/multiple_local_minima/run_optimization.py
@@ -0,0 +1,78 @@
+from KratosMultiphysics import *
+from KratosMultiphysics.ShapeOptimizationApplication import *
+import optimization_settings as settings
+from math import pi, sin
+
+# This test example is from M. Hojjat, E. Stavropoulou, 
+# K.-U. Bletzinger, The Vertex Morphing method for node-based
+# shape optimization, Comput. Methods Appl. Mech. Engrg. 268
+# (2014) 494-513.
+#
+# The target curves are illustrated below.
+#
+#                    z=1
+#                **  +++  **    target curve 1 (+)
+#              *  + *   * +  *  target curve 2 (*)
+#             * +    * *    + *
+#  |--> x     +               +     z=0
+#  __________*________*________*__________
+#  |-- 10 ---|-- 10 --|-- 10 --|--- 10 --|
+#
+# 
+
+design_surface = ModelPart("design_surface")
+
+def TargetCurveOne(x):
+    """ Defines target curve 1 as z=TargetCurveOne(x) """
+    if x <= 10.0:
+        return 0.0
+    elif x<= 30.0:
+        return sin(2.0 * pi * (x-10.0) / 40.0)
+    else:
+        return 0.0
+
+def TargetCurveTwo(x):
+    """ Defines target curve 2 as z=TargetCurveTwo(x) """
+    if x <= 10.0:
+        return 0.0
+    elif x<= 30.0:
+        return abs(sin(2.0 * pi * (x-10.0) / 20.0))
+    else:
+        return 0.0
+
+def ObjectiveFunction(X,opt_iter):
+    """ Returns the objective function to be minimized """
+    objective = 0.0
+    for node in design_surface.Nodes:
+        x = node.X
+        z = node.Z
+        objective = objective + abs(TargetCurveOne(x) - z) \
+                    * abs(TargetCurveTwo(x) - z)
+    return objective
+
+def ObjectiveGradient(X,opt_iter):
+    """ Returns the gradient of the objective function """
+    sensitivity = dict()
+    for node in design_surface.Nodes:
+        x = node.X
+        z = node.Z
+        delta_one = z - TargetCurveOne(x)
+        delta_two = z - TargetCurveTwo(x)
+        if abs(delta_one) == 0.0 or abs(delta_two) == 0.0:
+            sz = 0.0
+        else:
+            sz = abs(delta_two) * delta_one / abs(delta_one) \
+                 + abs(delta_one) * delta_two / abs(delta_two)
+        sensitivity[node.Id] = [0.0, 0.0, sz]
+    return sensitivity
+
+def Analyzer(X, controls, iterator, response):
+    """ Performs the analysis in the optimization loop """
+    response["1"]["func"] = ObjectiveFunction(X,iterator)
+    response["1"]["grad"] = ObjectiveGradient(X,iterator)
+
+import optimizer_factory
+
+optimizer = optimizer_factory.CreateOptimizer(design_surface,settings.KratosShapeSettings,Analyzer)
+
+optimizer.optimize()
