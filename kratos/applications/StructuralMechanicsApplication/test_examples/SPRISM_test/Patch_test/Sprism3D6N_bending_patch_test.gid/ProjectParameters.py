6f9f46d3754195c90733fc970ec6f0ab0344de6a
vmataix <vmataix@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-03-28 20:40:56 +0000

    Adding SPRISM patch test

---
+++ kratos/applications/StructuralMechanicsApplication/test_examples/SPRISM_test/Patch_test/Sprism3D6N_bending_patch_test.gid/ProjectParameters.py
@@ -0,0 +1,56 @@
+## General Data ---------------------------------------------------------------------------------------------------------------
+NumberofThreads = 4
+Start_time = 0
+Dt = 1.0
+max_time = 1.0
+output_time = 1.0
+nsteps =  1
+ProblemType = "Mechanical"
+
+## Solver Data ----------------------------------------------------------------------------------------------------------------
+
+linear_solver = "Direct"
+direct_solver = "Super_LU"
+iterative_solver = "AMGCL"
+
+class SolidSolverConfiguration:
+    solver_type = "structural_mechanics_solver"
+    echo_level  = 1
+    domain_size = 3 
+    
+    time_integration_method = "Implicit"
+    explicit_integration_scheme = "CentralDifferences"
+    time_step_prediction_level  = 0
+
+    LineSearch =  False
+    Implex =  False
+    ComputeReactions = False 
+    ComputeContactForces = False 
+    scheme_type = "StaticSolver"
+    convergence_criterion = "Residual_criteria" 
+    displacement_relative_tolerance = 1.0E-4
+    displacement_absolute_tolerance =1.0E-4
+    residual_relative_tolerance =  1.0E-4
+    residual_absolute_tolerance =  1.0E-4
+    max_iteration = 30
+    if linear_solver == "Direct":
+        class linear_solver_config:
+            solver_type = direct_solver
+            scaling = False
+    else:
+        class linear_solver_config:
+            solver_type = iterative_solver
+            max_iteration = 100
+            tolerance = 1.0E-4
+
+## Boundary Conditions Data -----------------------------------------------------------------------------------------------------------
+
+class ConditionsOptions:
+    Imposed_Displacement = "Constant"
+    Imposed_PointLoad = "Constant"
+    
+## Problem Data ---------------------------------------------------------------------------------------------------------------
+kratos_path="~/bin/kratos"
+problem_name = 'patch_test'
+problem_path = '/home/vicente/Descargas/patch_test.gid'
+
