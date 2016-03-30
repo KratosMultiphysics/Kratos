3df937f36904440fb7ea4d01b4628f0ae13d0892
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-03-30 15:54:09 +0000

    Added new functionalities to the application generator

---
+++ kratos/kratos/python_scripts/application_generator/applicationGenerator.py
@@ -0,0 +1,363 @@
+import os
+import sys
+import shutil
+
+from kratosClasses import KratosElementClass
+from kratosClasses import KratosConditionClass
+from kratosClasses import KratosProcessClass
+
+from utils.io import ToUpperFromCamel
+from utils.io import ToLowerFromCamel
+from utils.io import GetApplicationsDirectory
+
+
+class ApplicationGenerator(object):
+
+    def __init__(self, name):
+
+        self._appDir = GetApplicationsDirectory()
+
+        self._nameCamel = name
+        self._nameUpper = ToUpperFromCamel(self._nameCamel)
+        self._nameLower = ToLowerFromCamel(self._nameCamel)
+
+        self._elements = []
+        self._conditions = []
+        self._processes = []
+        self._strategies = []
+        self._variables = []
+
+        self.__isDerived = False
+
+        # What templates files need to be used for every class
+        self._classTemplateFiles = {
+            'Elements': ['element_template.cpp', 'element_template.h'],
+            'Conditions': ['condition_template.h'],
+            'Processes': ['process_template.h']
+        }
+
+        # Where they should be placed
+        self._classTemplatePath = {
+            'Elements': ['classes', 'custom_elements'],
+            'Conditions': ['classes', 'custom_conditions'],
+            'Processes': ['classes', 'custom_process']
+        }
+
+    def GenerateFile(self, src, dst, subsMap):
+        with open(src) as srcFile, open(dst, 'w+') as dstFile:
+            for l in srcFile:
+                for pair in subsMap:
+                    l = l.replace(pair['token'], pair['value'])
+                dstFile.write(l)
+
+    def AddCustomElements(self, elements):
+        self._addGenericClass(elements, self._elements, KratosElementClass)
+
+    def AddCustomConditions(self, conditions):
+        self._addGenericClass(conditions, self._conditions, KratosConditionClass)
+
+    def AddCustomProcesses(self, processes):
+        self._addGenericClass(processes, self._processes, KratosProcessClass)
+
+    def AddCustomStrategies(self, strategies):
+        for strategy in strategies:
+            self._elements.append(strategy)
+
+    def AddCustomVariables(self, variables):
+        for variable in variables:
+            self._elements.append(variable)
+
+    def AddCustomConstitutiveLaws(self, constituiveLaws):
+        for constitutiveLaw in constituiveLaws:
+            self._elements.append(constitutiveLaw)
+
+    def Generate(self):
+        ''' Generates the application by copying it from the template app '''
+
+        root = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+        tpldir = root + "../../templates/" + "template_application"
+        appdir = self._appDir + self._nameCamel + 'Application'
+
+        # TODO: Catch the exception
+        # Create the directory
+        shutil.copytree(tpldir, appdir, symlinks=False, ignore=None)
+
+        # Copy all files from the template application
+        filesToRename = {
+            "template_application.cpp.in":
+                self._nameLower+"_application.cpp.in",
+            "template_application.h.in":
+                self._nameLower+"_application.h.in",
+            "TemplateApplication.py.in":
+                self._nameCamel+"Application.py.in",
+            "tests/test_TemplateApplication.py.in":
+                "tests/test_"+self._nameCamel+"Application.py.in",
+            "custom_python/test_python_application.cpp.in":
+                "custom_python/"+self._nameLower+"_python_application.cpp.in"
+        }
+
+        # Define the substitution rules
+        rules = [
+            {'token': '@{APP_NAME_CAMEL}', 'value': self._nameCamel},
+            {'token': '@{APP_NAME_CAPS}', 'value': self._nameUpper},
+            {'token': '@{APP_NAME_LOW}', 'value': self._nameLower},
+            {'token': '@{APP_DEPEND_LIST}', 'value': ''}
+        ]
+
+        # Rename the files
+        for key, value in filesToRename.items():
+            os.rename(
+                os.path.join(appdir, key),
+                os.path.join(appdir, value)
+            )
+
+        # Replace the tokens in the app files
+        for root, subfolder, files in os.walk(appdir):
+            for f in files:
+                src = os.path.join(root, f)
+                fileName = src.split(".")
+                dst = '.'.join(src.split(".")[0:len(fileName)-1])
+
+                self._applyTemplateRulesToFile(src, dst, rules)
+
+                # Here we iterate over existing files, so we have to remove
+                # the original
+                os.remove(src)
+
+        # Generate the additional classes
+        self._generateClassFrom(
+            entityType='Elements',
+            fromContainer=self._elements)
+        self._generateClassFrom(
+            entityType='Conditions',
+            fromContainer=self._conditions)
+        self._generateClassFrom(
+            entityType='Processes',
+            fromContainer=self._processes)
+
+        # add it to kratos
+        self._addApplicationToCMake()
+        self._addApplicationToAppList()
+
+    # Interal goes here
+    def _applyTemplateRulesToFile(self, src, dst, rules):
+        ''' Creates a 'dst' file by iterating over all pairs of [token,value]
+        in 'rules' and replacing all ocurrences of 'token' by its 'value' in
+        the source file 'src'.
+        '''
+
+        with open(src, 'r') as srcFile, open(dst, 'w+') as dstFile:
+            for line in srcFile:
+                for rule in rules:
+                    line = line.replace(rule['token'], rule['value'])
+                dstFile.write(line)
+
+    def _generateClassFrom(self, entityType, fromContainer):
+        # Generate elements
+        root = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+        srcpath = root + "../../templates/" + self._classTemplatePath[entityType][0]
+        dstpath = self._appDir + self._nameCamel + 'Application/' + self._classTemplatePath[entityType][1]
+
+        if not os.path.exists(dstpath):
+            os.makedirs(dstpath)
+
+        for entity in fromContainer:
+            files = self._classTemplateFiles[entityType]
+            for f in files:
+
+                src = os.path.join(srcpath, f)
+                ext = f.split(".")[1]
+                dst = os.path.join(dstpath, entity.nameLower + "." + ext)
+
+                self._applyTemplateRulesToFile(src, dst, entity.rules)
+
+    def _addGenericClass(self, entities, container, containerType):
+        msg = "{} is not an instance of KratosElementClass"
+
+        for entity in entities:
+            if isinstance(entity, containerType):
+                container.append(entity)
+            else:
+                raise TypeError(msg.format(entity))
+
+    def _addApplicationToCMake(self):
+        # Locate the CMake file
+        srcFile = self._appDir + "CMakeLists.txt"
+        dstFile = self._appDir + "CMakeLists.txt.tmp"
+
+        with open(srcFile, 'r') as src, open(dstFile, 'w+') as dst:
+            for l in src:
+
+                # Add the applciation to the list message
+                if l == 'message(" ")':
+                    newLine = ' '
+
+                    newLine += 'message("' + self._nameUpper
+                    newLine += ('.'*37-len(self._nameUpper))
+                    newLine += '$\{' + self._nameUpper + '\}")\n'
+
+                    dst.write(newLine)
+
+                # TODO: Probably is not a good idea to use the end comment as a
+                # token for adding this, but we cannot use the end of the file
+                # until that same comment is removed
+
+                # Write the add subdirectory clause
+                if l == '# get_property(inc_dirs DIRECTORY PROPERTY INCLUDE_DIRECTORIES)':
+                    dst.write('if(${' + self._nameUpper + '} MATCHES ON)')
+                    dst.write('  add_subdirectory(' + self._nameCamel+')')
+                    dst.write('endif(${' + self._nameUpper + '} MATCHES ON)')
+                    dst.write('\n')
+
+                # Write the old line
+                dst.write(l)
+
+        # Replace the old file with the new one
+        # os.remove(srcFile)
+        # os.rename(dstFile, srcFile)
+
+    def _addApplicationToAppList(self):
+
+        # Locate the applications lists
+        srcFile = self._appDir + "applications_interface.py"
+        dstFile = self._appDir + "applications_interface.py.tmp"
+
+        # For this file is just easier to reconstruct the whole thing
+        fileStruct = {
+            'header': [],
+            'importFalseBlock': [],
+            'printMsgBlock': [],
+            'appDirBlock': [],
+            'importValueBlock': [],
+            'prepareBlock': [],
+            'initializeBlock': [],
+            'footer': [],
+        }
+
+        currentBlock = 'header'
+
+        # First read the file and parse all the info
+        with open(srcFile, 'r') as src:
+            for l in src:
+
+                # Select the block
+                if 'Import_' in l and 'Application = False' in l:
+                    currentBlock = 'importFalseBlock'
+                if 'print("Applications Available:")' in l:
+                    currentBlock = 'printMsgBlock'
+                if 'application_directory = os.path.dirname' in l:
+                    currentBlock = 'appDirBlock'
+                if 'def ImportApplications' in l:
+                    currentBlock = 'importValueBlock'
+                if 'if(Import_SolidMechanicsApplication):' in l:
+                    currentBlock = 'prepareBlock'
+                if 'kernel.Initialize()' in l:
+                    currentBlock = 'initializeBlock'
+                if '# def ImportApplications(kernel  ):' in l:
+                    currentBlock = 'footer'
+
+                # Append the result if its not null
+                if l is not '\n':
+                    fileStruct[currentBlock].append(l)
+
+        # Prepare some blocks
+        prepareBlockContent = [
+            'if(Import_{CAMEL}):\n'
+            'print("importing Kratos{CAMEL}Application ...")\n'
+            'sys.path.append(applications_path + \'/{CAMEL}/python_scripts\')\n'
+            'sys.path.append(applications_path + \'/{CAMEL}/Linux\')\n'
+            'from Kratos{CAMEL}Application import *\n'
+            '{LOWER}_application = Kratos{CAMEL}Application()\n'
+            'kernel.AddApplication({LOWER}_application)\n'
+            'print("Kratos{CAMEL}Application Succesfully imported")\n'
+        ]
+
+        prepareBlockContent = [
+            s.format(
+                CAMEL=self._nameCamel,
+                LOWER=self._nameLower
+            ) for s in prepareBlockContent
+        ]
+
+        # Add our application to the requeired blocks
+        fileStruct['importFalseBlock'].append(
+            'Import_' + self._nameCamel + ' = False\n'
+        )
+
+        fileStruct['printMsgBlock'].append(
+            'print("Import_' + self._nameCamel + ': False")\n'
+        )
+
+        fileStruct['importValueBlock'].append(
+            'print("Import_{CAMEL}: " + str(Import_{CAMEL}))\n'.format(
+                CAMEL=self._nameCamel
+            )
+        )
+
+        fileStruct['prepareBlock'].append(
+            prepareBlockContent
+        )
+
+        # Write the whole thing down
+        with open(dstFile, 'w+') as dst:
+            for l in fileStruct['header']:
+                dst.write(l)
+            dst.write('\n')
+            for l in fileStruct['importFalseBlock']:
+                dst.write(l)
+            dst.write('\n')
+            for l in fileStruct['printMsgBlock']:
+                dst.write(l)
+            dst.write('\n')
+            for l in fileStruct['appDirBlock']:
+                dst.write(l)
+            dst.write('\n')
+            for l in fileStruct['importValueBlock']:
+                dst.write(l)
+            dst.write('\n')
+            for b in fileStruct['prepareBlock']:
+                for l in b:
+                    dst.write(l)
+            dst.write('\n')
+            for l in fileStruct['initializeBlock']:
+                dst.write(l)
+            dst.write('\n')
+            for l in fileStruct['footer']:
+                dst.write(l)
+
+        # os.remove(srcFile)
+        # os.rename(dstFile, srcFile)
+
+
+# TODO: Main sould exists in a separate file
+def main():
+
+    # Read the application name and generate Camel, Caps and Low
+    appCamel = sys.argv[1]
+
+    # Fetch the applications directory
+    debugApp = ApplicationGenerator(appCamel)
+
+    # Add test element
+    debugApp.AddCustomElements([
+        KratosElementClass("CustomTestElement")
+    ])
+
+    debugApp.AddCustomConditions([
+        KratosConditionClass("CustomTestCondition")
+    ])
+
+    debugApp.AddCustomProcesses([
+        KratosProcessClass("CustomTestProcessAlpha"),
+        KratosProcessClass("CustomTestProcessDelta")
+    ])
+
+    debugApp.Generate()
+
+    # Remind the user to change other files
+    print("Your application has been generated in: applications/{}Application".format(appCamel))
+
+if __name__ == "__main__":
+    main()
---
+++ kratos/kratos/python_scripts/application_generator/kratosClasses.py
@@ -0,0 +1,232 @@
+import difflib
+import re
+import os
+import sys
+
+from utils.io import GetApplicationsDirectory
+from utils.io import GetKratosDirectory
+from utils.io import ToUpperFromCamel, ToLowerFromCamel
+from utils.io import bcolors
+
+
+def TestCamel(s):
+    expr = re.compile(r'^(?:[A-Z][a-z]+)+$')
+    return expr.match(s)
+
+
+def formatc(stringList, where):
+
+    string = ""
+
+    for sl in stringList:
+        if where.isatty() and sl['color']:
+            string += sl['color'] + sl['msg'] + bcolors.ENDC
+        else:
+            string += sl['msg']
+
+    return string
+
+
+class KratosClassMember(object):
+    def __init__(self, name, memberType=None, initValue=None):
+        self.name = name
+        self.type = memberType
+        self.initValue = initValue
+
+        self.isRef = '&' in memberType
+
+
+class KratosClassProcedure(object):
+    def __init__(self, name, returnType=None, parameters=None, template=None):
+        self.name = name
+        self.type = returnType
+        self.template = template
+        self.body = None
+
+
+class KratosClass(object):
+    def __init__(
+        self, name, base=None, template=None,
+        members=None, procedures=None, author='KratosAppGenerator'
+    ):
+
+        # Check that name is in camelcase, or at least something parseable
+        if not TestCamel(name):
+            msg = formatc([
+                {'color': bcolors.FAIL, 'msg': '[ERROR]'},
+                {'color': None, 'msg': ' Name: "'},
+                {'color': bcolors.CYAN, 'msg': '{}'.format(name)},
+                {'color': None, 'msg': '" is not camelcase'},
+            ], sys.stderr)
+
+            raise ValueError(msg)
+
+        # Check name, if a class already exists with that name in the context
+        # specified, ask if you want to continue. In case of continuing a
+        # warning message will be shown (in red?) and a backup of the class
+        # will be created.
+
+        self.author = author
+        self.nameCamel = name
+        self.nameUpper = ToUpperFromCamel(name)
+        self.nameLower = ToLowerFromCamel(name)
+        self.base = base
+        self.template = template
+        self.procedures = procedures
+        self.rules = [
+            {'token': '@{KRATOS_APP_AUTHOR}', 'value': self.author},
+            {'token': '@{KRATOS_NAME_CAMEL}', 'value': self.nameCamel},
+            {'token': '@{KRATOS_NAME_UPPER}', 'value': self.nameUpper},
+            {'token': '@{KRATOS_NAME_LOWER}', 'value': self.nameLower},
+            {'token': '@{KRATOS_SYSTEM_INCLUDES}', 'value': ''},
+            {'token': '@{KRATOS_EXTERNAL_INCLUDES}', 'value': ''},
+            {'token': '@{KRATOS_PROJECT_INCLUDES}', 'value': ''},
+            {'token': '@{KRATOS_CLASS_TEMPLATE}', 'value': ''},
+            {'token': '@{KRATOS_DEPEND_LIST}', 'value': ''}
+        ]
+
+        [a, b, c, d] = self.CheckClassNameAvailable(
+            self.nameCamel, "Kratos", True
+        )
+
+        for m in c.values():
+            msg = formatc([
+                {'color': bcolors.FAIL, 'msg': '[ERROR]'},
+                {'color': None, 'msg': ' Class with name: "'},
+                {'color': bcolors.CYAN, 'msg': '{}'.format(name)},
+                {'color': None, 'msg': '" already defined in:'},
+            ], sys.stderr)
+            print(msg, file=sys.stderr)
+            print("  ["+m[0]+"]: \""+m[1].replace("\n", "")+"\""+"\n",
+                  file=sys.stderr)
+
+        for m in d.values():
+            msg = formatc([
+                {'color': bcolors.WARNING, 'msg': '[WARNING]'},
+                {'color': None, 'msg': ' Class with name: "'},
+                {'color': bcolors.CYAN, 'msg': '{}'.format(name)},
+                {'color': None, 'msg': '" probably ({}%) defined in:'.format(
+                    str(m[2]*100))},
+            ], sys.stderr)
+            print(msg, file=sys.stderr)
+            print("  ["+m[0]+"]: \""+m[1].replace("\n", "")+"\""+"\n",
+                  file=sys.stderr)
+
+    def AddClassMember(self, member):
+        self.procedures.append(member)
+
+    def AddClassProcudure(self, procedure):
+        self.procedures.append(procedure)
+
+    def _IsClassDefinition(self, line):
+        # Match class definition and possible decorators
+        expr = re.compile('(^class) ([\w]+) ([\w]*)')
+        r = expr.match(line)
+        if r:
+            return True, r.group(2), r.group(3)
+        else:
+            return False, 0, 0
+
+    def _GenerateCandidateFiles(self, files, name):
+        return [f for f in files if
+                difflib.SequenceMatcher(None, name, f).ratio() > 0.4]
+
+    def _CheckClassNameInDir(
+      self, where, name,
+      appRep, appSim, repFiles, simFiles):
+
+        for root, subfolder, files in os.walk(where):
+            for f in self._GenerateCandidateFiles(files, name):
+                src = os.path.join(root, f)
+                with open(src, 'r', encoding='latin1') as _file:
+                    for l in _file:
+                        [found, m1, m2] = self._IsClassDefinition(l)
+                        if found:
+                            for m in [k.lower() for k in [m1, m2] if k]:
+                                seq = difflib.SequenceMatcher(None, name, m)
+                                if seq.ratio() == 1 and f not in repFiles:
+                                    appRep = True
+                                    repFiles[f] = [f, l, 1]
+                                elif seq.ratio() > 0.75 and f not in simFiles:
+                                    appSim = True
+                                    simFiles[f] = [f, l, round(seq.ratio(), 3)]
+
+        return appRep, appSim, repFiles, simFiles
+
+    def _CheckFileNameInDir(
+      self, where, name,
+      appRep, appSim, repFiles, simFiles):
+
+        for root, subfolder, files in os.walk(where):
+            for f in files:
+                seq = difflib.SequenceMatcher(None, name, f)
+                if seq.ratio() == 1:
+                    pass
+                elif seq.ratio() > 0.50:
+                    pass
+
+    def CheckClassNameAvailable(
+      self, name,
+      context="Application",
+      searchSimilar=False):
+
+        appRep = False
+        appSim = False
+
+        repFiles = {}
+        simFiles = {}
+
+        # Search in the application context
+        [appRep, appSim, repFiles, simFiles] = self._CheckClassNameInDir(
+            GetApplicationsDirectory()+self.nameCamel+"Application",
+            self.nameCamel.lower(),
+            appRep, appSim,
+            repFiles, simFiles
+        )
+
+        if context == "Application":
+            if searchSimilar:
+                return appRep, appSim, repFiles, simFiles
+            else:
+                return appRep, repFiles
+
+        # Search also in the kratos context:
+        [appRep, appSim, repFiles, simFiles] = self._CheckClassNameInDir(
+            GetKratosDirectory(),
+            self.nameCamel.lower(),
+            appRep, appSim,
+            repFiles, simFiles
+        )
+
+        if context == "Kratos":
+            if searchSimilar:
+                return appRep, appSim, repFiles, simFiles
+            else:
+                return appRep, repFiles
+
+        # Search in other apps... "Everywhere":
+        for root, dirs, files in os.walk(GetApplicationsDirectory()):
+            for subapp in dirs:
+                [appRep, appSim, repFiles, simFiles] = self._CheckClassNameInDir(
+                    GetApplicationsDirectory()+subapp,
+                    self.nameCamel.lower(),
+                    appRep, appSim,
+                    repFiles, simFiles
+                )
+
+        if searchSimilar:
+            return appRep, appSim, repFiles, simFiles
+        else:
+            return appRep, repFiles
+
+
+class KratosElementClass(KratosClass):
+    pass
+
+
+class KratosConditionClass(KratosClass):
+    pass
+
+
+class KratosProcessClass(KratosClass):
+    pass
