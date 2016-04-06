9df97cca911b6dffdf7245ee45c0603a708e6725
vmataix <vmataix@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-04-06 15:38:22 +0000

    Adapting the arc_length script to the one used in the solid mechanics

--- kratos/applications/StructuralMechanicsApplication/python_scripts/structural_solver_static_arc_length.py
+++ kratos/applications/StructuralMechanicsApplication/python_scripts/structural_solver_static_arc_length.py
@@ -52,66 +52,59 @@ class StaticArcLengthStructuralSolver:
     #
     def __init__(self, model_part, domain_size):
 
+        # Default settings
+        self.echo_level = 0
         self.model_part = model_part
-        self.time_scheme = ResidualBasedIncrementalUpdateStaticScheme()
+        self.domain_size = domain_size
+        self.buffer_size = 3 #default buffer_size
 
-        # self.time_scheme   = ParallelResidualBasedIncrementalUpdateStaticScheme()
-        # Varibles de Control de Arc Lenght Method
+        # Default varibles de Control de Arc Lenght Method
         self.Ide = 5
         self.factor_delta_lmax = 1.00
-        self.toler = 1.0E-9
-        self.norm = 1.0E-5
-        self.max_iter = 20
-
-         # self.time_scheme = ResidualBasedPredictorCorrectorBossakScheme(self.damp_factor)
-        # definition of the solvers. Super_Lu Default
-        self.structure_linear_solver = SkylineLUFactorizationSolver()
-        # self.structure_linear_solver      =   SuperLUSolver()
-        # self.structure_linear_solver =  MKLPardisoSolver()
-        # pDiagPrecond = ParallelDiagonalPreconditioner()
-        # self.structure_linear_solver     =  ParallelCGSolver(1e-8, 5000,pDiagPrecond)
-        # self.structure_linear_solver    =   Preconditioner()
-        # self.structure_linear_solver    =   IterativeSolver()
-
-        # pDiagPrecond = DiagonalPreconditioner()
-        # LST  = 1E-9
-        # LSMI = 5000
-        # self.structure_linear_solver  =  BICGSTABSolver(LST,LSMI,pDiagPrecond)
-
-        # definition of the convergence criteria
-        Displacement = DisplacementCriteria(1E-6, 1E-9)
-        Residual = ResidualCriteria(1E-3, 1E-6)
-
-        self.conv_criteria = AndCriteria(Residual, Displacement)
-        # self.conv_Residual     = ResidualCriteria(0.000001,1E-9)
-        # self.conv_Displacement = DisplacementCriteria(self.norm,self.toler)
-        # self.conv_criteria     = ResDisCriteria(self.conv_Residual, self.conv_Displacement)
-        # self.conv_criteria     = ResidualDisplacementCriteria(self.norm,self.toler)
-        # self.conv_criteria     = DisplacementCriteria(self.norm,self.toler)
-        # self.conv_criteria    = ParallelDisplacementCriteria(0.000001,1e-9)
-        # self.conv_criteria     = DisplacementCriteria(self.norm,self.toler)
-
-        # definition of the convergence criteria
-
+        self.max_iteration = 20
+        self.toler = 1.0E-10
+        self.norm = 1.0E-7
+        self.MaxLineSearchIterations = 20
+        self.tolls = 0.000001
+        self.amp = 1.618
+        self.etmxa = 5
+        self.etmna = 0.1
         self.CalculateReactionFlag = True
         self.ReformDofSetAtEachStep = True
         self.MoveMeshFlag = True
 
+        # Default linear solver
+        self.linear_solver = SkylineLUFactorizationSolver()
+
+        # definition of the convergence criteria
+        self.rel_disp_tol = 1e-4
+        self.abs_disp_tol = 1e-9
+        self.rel_res_tol = 1e-4
+        self.abs_res_tol = 1e-9
+        
+        Displacement = DisplacementCriteria(self.rel_disp_tol, self.abs_disp_tol)
+        Residual = ResidualCriteria(self.rel_res_tol, self.abs_res_tol)
+
+        self.convergence_criterion_type = "And_criteria"
+        self.mechanical_convergence_criterion = AndCriteria(Residual, Displacement)
+    
+        # Definition of the default builder_and_solver:
+        self.time_scheme = ResidualBasedIncrementalUpdateStaticScheme()
+        self.block_builder = False
+
+        # Definition of the component wise calculation "computation is slower"
+        #(it affects to strategy, builder_and_solver, scheme and convergence_criterion)
+        self.component_wise = False
+    
     #
     def Initialize(self):
 
-        self.solver = ResidualBasedArcLengthStrategy(
-            self.model_part, 
-            self.time_scheme,
-            self.structure_linear_solver, 
-            self.conv_criteria, 
-            self.Ide, 
-            self.max_iter, 
-            self.factor_delta_lmax, 
-            self.CalculateReactionFlag, 
-            self.ReformDofSetAtEachStep, 
-            self.MoveMeshFlag
-            )
+        # Creating the convergence criterion:
+        self.SetConvergenceCriterion()
+
+        # Creating the solver
+        self.solver = ResidualBasedArcLengthStrategy(self.model_part,  self.time_scheme, self.linear_solver, 
+            self.mechanical_convergence_criterion,  self.Ide,  self.max_iteration,  self.factor_delta_lmax,  self.CalculateReactionFlag, self.ReformDofSetAtEachStep,  self.MoveMeshFlag )
     #
     def Solve(self):
         (self.solver).Solve()
@@ -119,7 +112,45 @@ class StaticArcLengthStructuralSolver:
     #
     def SetEchoLevel(self, level):
         (self.solver).SetEchoLevel(level)
+    #
+    def SetConvergenceCriterion(self):
 
+        # Mechanical convergence criteria
+        D_RT = self.rel_disp_tol;
+        D_AT = self.abs_disp_tol;
+        R_RT = self.rel_res_tol;
+        R_AT = self.abs_res_tol;
+
+        if(self.echo_level > 1):
+            print("::[Arc length Solver]:: CONVERGENCE CRITERION : ", self.convergence_criterion_type)
+
+        if(self.convergence_criterion_type == "Displacement_criteria"):
+            self.mechanical_convergence_criterion = DisplacementConvergenceCriterion(D_RT, D_AT)
+        elif(self.convergence_criterion_type == "Residual_criteria"):
+            if(self.component_wise):
+                self.mechanical_convergence_criterion = ComponentWiseResidualConvergenceCriterion(R_RT, R_AT)
+            else:
+                self.mechanical_convergence_criterion = ResidualCriteria(R_RT, R_AT)
+        elif(self.convergence_criterion_type == "And_criteria"):
+            Displacement = DisplacementConvergenceCriterion(D_RT, D_AT)
+            if(self.component_wise):
+                Residual = ComponentWiseResidualConvergenceCriterion(R_RT, R_AT)
+            else:
+                Residual = ResidualCriteria(R_RT, R_AT)
+            self.mechanical_convergence_criterion = AndCriteria(Residual, Displacement)
+        elif(self.convergence_criterion_type == "Or_criteria"):
+            Displacement = DisplacementConvergenceCriterion(D_RT, D_AT)
+            if(self.component_wise):
+                Residual = ComponentWiseResidualConvergenceCriterion(R_RT, R_AT)
+            else:
+                Residual = ResidualCriteria(R_RT, R_AT)
+            self.mechanical_convergence_criterion = OrCriteria(Residual, Displacement)
+        elif(self.convergence_criterion_type == "Mixed_criteria"):
+            Displacement = MixedElementConvergeCriteria(D_RT, D_AT)
+            Residual = ResidualCriteria(R_RT, R_AT)
+            self.mechanical_convergence_criterion = AndCriteria(Residual, Displacement)
+        
+    #
     def ChangeCondition(self, model_part, lamda):
         for cond in model_part.Conditions:
             print(cond)
@@ -127,4 +158,61 @@ class StaticArcLengthStructuralSolver:
 def CreateSolver(model_part, config):
     structural_solver = StaticArcLengthStructuralSolver(model_part, config.domain_size)
     model_part.ProcessInfo[LAMBDA] = 0.00;
+    
+    # Definition of the control variables of the Arc Lenght Method
+    if(hasattr(config, "Ide")): 
+        structural_solver.Ide = config.Ide
+    if(hasattr(config, "factor_delta_lmax")):
+        structural_solver.factor_delta_lmax = config.factor_delta_lmax
+    if(hasattr(config, "max_iteration")):
+        structural_solver.max_iteration = config.max_iteration
+    if(hasattr(config, "toler")):
+        structural_solver.toler = config.toler
+    if(hasattr(config, "norm")):
+        structural_solver.norm = config.norm
+    if(hasattr(config, "MaxLineSearchIterations")):
+        structural_solver.MaxLineSearchIterations = config.MaxLineSearchIterations
+    if(hasattr(config, "tolls")):
+        structural_solver.tolls = config.tolls
+    if(hasattr(config, "amp")):
+        structural_solver.amp = config.amp
+    if(hasattr(config, "etmxa")):
+        structural_solver.etmxa = config.etmxa
+    if(hasattr(config, "etmna")): 
+        structural_solver.etmna = config.etmna
+    if(hasattr(config, "CalculateReactionFlag")): # bool
+        structural_solver.CalculateReactionFlag = config.CalculateReactionFlag
+    if(hasattr(config, "ReformDofSetAtEachStep")): # bool
+        structural_solver.ReformDofSetAtEachStep = config.ReformDofSetAtEachStep
+    if(hasattr(config, "MoveMeshFlag")): # bool
+        structural_solver.MoveMeshFlag = config.MoveMeshFlag
+
+    # Definition of the convergence criteria
+    if(hasattr(config, "convergence_criterion")):
+        structural_solver.convergence_criterion_type = config.convergence_criterion
+    if(hasattr(config, "displacement_relative_tolerance")):
+        structural_solver.rel_disp_tol = config.displacement_relative_tolerance
+    if(hasattr(config, "displacement_absolute_tolerance")):
+        structural_solver.abs_disp_tol = config.displacement_absolute_tolerance
+    if(hasattr(config, "residual_relative_tolerance")):
+        structural_solver.rel_res_tol = config.residual_relative_tolerance
+    if(hasattr(config, "residual_absolute_tolerance")):
+        structural_solver.abs_res_tol = config.residual_absolute_tolerance
+
+    # definition of the echo level
+    if(hasattr(config, "echo_level")):
+        structural_solver.echo_level = config.echo_level
+
+    # definition of the linear solver
+    import linear_solver_factory
+    if(hasattr(config, "linear_solver_config")):
+        if(config.echo_level > 1):
+            print("::[Mechanical Solver]:: LINEAR SOLVER : ", config.linear_solver_config.solver_type)
+        structural_solver.linear_solver = linear_solver_factory.ConstructSolver(config.linear_solver_config)
+
+        if(config.linear_solver_config.solver_type == "AMGCL"):
+            structural_solver.block_builder = True
+        else:
+            structural_solver.block_builder = False
+    
     return structural_solver
