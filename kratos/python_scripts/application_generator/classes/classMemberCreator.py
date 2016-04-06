8c263b164379caa8959505d9a332cbf7cef84159
Carlos Roig <roigcarlo@gmail.com>
2016-04-06 15:45:21 +0000

    Added kratosClass.py functionality into a new submodule 'classes'

---
+++ kratos/python_scripts/application_generator/classes/classMemberCreator.py
@@ -0,0 +1,49 @@
+import sys
+
+from utils.io import bcolors, Formatc
+
+
+class ClassMemberCreator(object):
+    def __init__(self, name, vtype=None, default=None):
+
+        # This wil be extended with valid and invalid types
+        if vtype is None:
+            msg = Formatc([
+                {'color': bcolors.FAIL, 'msg': '[ERROR]'},
+                {'color': None, 'msg': ' while creating variable: "'},
+                {'color': bcolors.CYAN, 'msg': '{}'.format(name)},
+                {'color': None, 'msg': '" :No type defined'},
+            ], sys.stderr)
+
+            raise ValueError(msg)
+
+        self.isRef = '&' in vtype
+        self.isPtr = '*' in vtype
+        self.isStatic = 'static' in vtype
+        self.default = default
+
+        if self.isRef and self.default is None:
+            msg = Formatc([
+                {'color': bcolors.WARNING, 'msg': '[WARNING]'},
+                {'color': None, 'msg': ' while creating variable: "'},
+                {'color': bcolors.CYAN, 'msg': '{}'.format(name)},
+                {'color': None, 'msg': '" : Reference variable without default value.'},
+            ], sys.stderr)
+
+            print(msg, file=sys.stderr)
+
+        if self.isStatic and self.default is not None:
+            msg = Formatc([
+                {'color': bcolors.WARNING, 'msg': '[WARNING]'},
+                {'color': None, 'msg': ' while creating variable: "'},
+                {'color': bcolors.CYAN, 'msg': '{}'.format(name)},
+                {'color': None, 'msg': '" : static variable default values are not currently supported.\n'},
+                {'color': None, 'msg': 'Variable will be left unitialized'},
+            ], sys.stderr)
+
+            print(msg, file=sys.stderr)
+
+            self.default = None
+
+        self.name = name
+        self.type = vtype
---
+++ kratos/python_scripts/application_generator/classes/elementCreator.py
@@ -0,0 +1,147 @@
+from classes.classCreator import ClassCreator
+
+from utils.constants import ctab
+
+
+class ElementCreator(ClassCreator):
+    ''' Extends KratosClassCreator for a Kratos element
+
+        Input
+        -----
+        - name: string
+            name of the class
+
+        - base: string
+            name of the base element, if any. Defaults to 'Element'.
+
+        - template:: instance of KratosTemplate
+            not yet implemented.
+
+        - members: list of KratosClassMember
+            class memebers to be added. TO BE REMOVED!
+
+        - procedures: list of KratosClassProcedures
+            class procedures to be added. TO BE REMOVED!
+
+        Output
+        ------
+        - Instance of a KratosElementCreator
+
+        Rules
+        -----
+        - @{KRATOS_CLASS_LOCAL_FLAGS}:
+            The flag list
+
+        - @{KRATOS_ELEMENT_LIST_DOFS}:
+            The dof list
+
+        - @{KRATOS_ELEMENT_ECUATION_ID_DOFS}:
+            The dof list for the ecuation id's
+    '''
+
+    def __init__(
+        self, name, base='Element', template=None,
+        members=None, procedures=None, author='KratosAppGenerator'
+    ):
+        super(ElementCreator, self).__init__(
+            name, base, template, members,
+            procedures, author
+        )
+
+        # Register additional rules for elements
+        self.rules += [
+            {'token': '@{KRATOS_CLASS_LOCAL_FLAGS}', 'value': ''},
+            {'token': '@{KRATOS_ELEMENT_LIST_DOFS}', 'value': ''},
+            {'token': '@{KRATOS_ELEMENT_ECUATION_ID_DOFS}', 'value': ''}
+        ]
+
+    def AddFlags(self, flagList):
+        ''' Adds a list of flags to the element and initializes
+
+            Input
+            -----
+            - flagList: list of strings
+                the list of falg names to be added
+
+            Output
+            ------
+            - returns itself to allow chaining
+
+            Rules
+            -----
+            - @{KRATOS_CLASS_LOCAL_FLAGS}:
+                The flag list
+        '''
+
+        localFlagsValue = ''
+        localFlagDefinition = ctab + 'KRATOS_DEFINE_LOCAL_FLAG({})\n'
+
+        for flagName in flagList:
+            upperName = flagName.upper()
+            if upperName != flagName:
+                print('Flag variable name appears to be in incorrect format')
+            localFlagsValue += localFlagDefinition.format(upperName)
+
+        # Locate the flag rule and replace the value
+        flagRule = self._GetRule('@{KRATOS_CLASS_LOCAL_FLAGS}')
+        flagRule['value'] += localFlagsValue
+
+        return self
+
+    def AddDofs(self, dofList):
+        ''' Adds a list of dofs to the element and initializes. It is guaranteed that
+            the order of the list will be respected.
+
+            Input
+            -----
+            - dofList: list of strings
+                the list of dof names to be added
+
+            Output
+            ------
+            - returns itself to allow chaining
+
+            Rules
+            -----
+            - @{KRATOS_ELEMENT_LIST_DOFS}:
+                The dof list
+
+            - @{KRATOS_ELEMENT_ECUATION_ID_DOFS}:
+                The dof list for the ecuation id's
+        '''
+
+        # Dof are used to fill a couple of macros. Dofs can only be replaced, not modified (add/remove)
+        # List of dofs
+        localDofListValue = ''
+        localDofListDefinition = [
+            ctab * 2 + 'for (unsigned int i = 0; i < number_of_nodes; i++)\n',
+            ctab * 3 + 'ElementalDofList[i] = GetGeometry()[i].pGetDof({});\n',
+            '\n'
+        ]
+
+        # EcuationID's
+        localDofEidValue = ''
+        localDofEidDefinition = [
+            ctab * 2 + 'for (unsigned int i = 0; i < number_of_nodes; i++)\n',
+            ctab * 3 + 'rResult[i] = GetGeometry()[i].GetDof({}).EquationId();\n',
+            '\n'
+        ]
+
+        # Assamble the code
+        for dofName in dofList:
+            upperName = dofName.upper()
+            if upperName != dofName:
+                print('Dof name appears to be in incorrect format')
+            for l in localDofListDefinition:
+                localDofListValue += l.format(upperName)
+            for l in localDofEidDefinition:
+                localDofEidValue += l.format(upperName)
+
+        # Locate the rules and replace the values
+        dofListRule = self._GetRule('@{KRATOS_ELEMENT_LIST_DOFS}')
+        dofEidRule = self._GetRule('@{KRATOS_ELEMENT_ECUATION_ID_DOFS}')
+
+        dofListRule['value'] = localDofListValue
+        dofEidRule['value'] = localDofEidValue
+
+        return self
