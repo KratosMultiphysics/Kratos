40a229da39d65032aa8e96135121d7ef431a0132
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-04-06 15:55:33 +0000

    Add a small example for the generator

---
+++ kratos/kratos/python_scripts/application_generator/example.py
@@ -0,0 +1,40 @@
+import sys
+
+from classes.elementCreator import ElementCreator
+from classes.conditionCreator import ConditionCreator
+from classes.processCreator import ProcessCreator
+from classes.classMemberCreator import ClassMemberCreator
+
+from applicationGenerator import ApplicationGenerator
+
+# Read the application name and generate Camel, Caps and Low
+appCamel = sys.argv[1]
+
+# Fetch the applications directory
+debugApp = ApplicationGenerator(appCamel)
+
+# Add test element
+debugApp.AddElements([
+    ElementCreator('CustomTestElement')
+    .AddDofs(['DOF_1', 'DOF_2'])
+    .AddFlags(['FLAG_1', 'FLAG_2'])
+    .AddClassMemberVariables([
+        ClassMemberCreator(name='Variable1', vtype='double *', default='nullptr'),
+        ClassMemberCreator(name='Variable2', vtype='int', default='0'),
+        ClassMemberCreator(name='Variable3', vtype='std::string &', default='0'),
+        # ClassMemberCreator(name='Warnvar1', vtype='std::string &',)
+    ])
+])
+
+debugApp.AddConditions([
+    ConditionCreator('CustomTestCondition')
+])
+
+debugApp.AddProcesses([
+    ProcessCreator('CustomTestProcessAlpha'),
+    ProcessCreator('CustomTestProcessDelta')
+])
+
+debugApp.Generate()
+
+print("Your application has been generated in: applications/{}Application".format(appCamel))
