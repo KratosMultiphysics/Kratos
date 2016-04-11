17b4373058f5072d4e66ff3dc8b27cb597d7b38d
lgracia <lgracia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-04-11 12:45:35 +0000

    Erasing Newton Raphson strategy and newmark scheme

--- kratos/applications/DamApplication/custom_python/add_custom_strategies_to_python.cpp
+++ kratos/applications/DamApplication/custom_python/add_custom_strategies_to_python.cpp
@@ -21,12 +21,11 @@
 
 //strategies
 #include "solving_strategies/strategies/solving_strategy.h"
-#include "custom_strategies/newton_raphson_strategy.hpp"
 
 //builders and solvers
 
 //schemes
-#include "custom_strategies/custom_schemes/newmark_scheme.hpp"
+
 
 
 namespace Kratos
@@ -39,21 +38,7 @@ using namespace boost::python;
 
 void  AddCustomStrategiesToPython()
 {
-    typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
-    typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
-    typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
-    typedef SolvingStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > BaseSolvingStrategyType;
-    typedef Scheme< SparseSpaceType, LocalSpaceType > BaseSchemeType;
-    typedef BuilderAndSolver< SparseSpaceType, LocalSpaceType, LinearSolverType > BuilderAndSolverType;
-    typedef NewtonRaphsonStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > NewtonRaphsonStrategyType;
-    typedef NewmarkScheme< SparseSpaceType, LocalSpaceType >  NewmarkSchemeType;  
 
-    // Newmark Scheme
-    class_< NewmarkSchemeType,bases< BaseSchemeType >,  boost::noncopyable >("NewmarkScheme",init< bool >());
-      
-    // Newton-Raphson Strategy
-    class_< NewtonRaphsonStrategyType, bases< BaseSolvingStrategyType >, boost::noncopyable >("NewtonRaphsonStrategy", 
-        init < ModelPart&, BaseSchemeType::Pointer, BuilderAndSolverType::Pointer, double, double, int, bool, bool, bool >());
 }
 
 }  // namespace Python.
