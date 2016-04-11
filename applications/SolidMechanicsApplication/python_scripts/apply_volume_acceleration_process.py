f2771109e74553b88f99fbca5775c583a05e12f1
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-04-11 13:49:39 +0000

    New process to impose volume accelerations from a Json string

---
+++ applications/SolidMechanicsApplication/python_scripts/apply_volume_acceleration_process.py
@@ -0,0 +1,17 @@
+import KratosMultiphysics 
+
+def Factory(settings, Model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return ApplyVolumeAccelerationProcess(Model, settings["parameters"])
+
+##all the processes python processes should be derived from "python_process"
+class ApplyVolumeAccelerationProcess(KratosMultiphysics.ApplyConstantVectorValueProcess):
+    def __init__(self, Model, Parameters ):
+        model_part = Model[Parameters["model_part_name"].GetString()]
+        Parameters.AddEmptyValue("variable_name").SetString("VOLUME_ACCELERATION")
+        Parameters.AddEmptyValue("is_fixed_x").SetBool(False)
+        Parameters.AddEmptyValue("is_fixed_y").SetBool(False)
+        Parameters.AddEmptyValue("is_fixed_z").SetBool(False)
+               
+        KratosMultiphysics.ApplyConstantVectorValueProcess.__init__(self,model_part, Parameters)
