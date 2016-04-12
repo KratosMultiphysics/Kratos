06d7fe45b433c53a5460c3a534056b8ddb1c1aa8
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-04-12 09:40:17 +0000

    Modified to set the constrained displacement directions in the Json file.

--- applications/SolidMechanicsApplication/python_scripts/apply_displacement_process.py
+++ applications/SolidMechanicsApplication/python_scripts/apply_displacement_process.py
@@ -10,8 +10,5 @@ class ApplyDisplacementProcess(KratosMultiphysics.ApplyConstantVectorValueProces
     def __init__(self, Model, Parameters ):
         model_part = Model[Parameters["model_part_name"].GetString()]
         Parameters.AddEmptyValue("variable_name").SetString("DISPLACEMENT")
-        Parameters.AddEmptyValue("is_fixed_x").SetBool(True)
-        Parameters.AddEmptyValue("is_fixed_y").SetBool(True)
-        Parameters.AddEmptyValue("is_fixed_z").SetBool(True)
                
         KratosMultiphysics.ApplyConstantVectorValueProcess.__init__(self,model_part, Parameters)
