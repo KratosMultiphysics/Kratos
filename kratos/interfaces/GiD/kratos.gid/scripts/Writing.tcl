0098c44d6cf9a1cf11452b6abf8e94cc5e54bb5a
fjgarate <fjgarate@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-04-13 11:22:37 +0000

    Adapt to GiD 12.1.11d

--- kratos/interfaces/GiD/kratos.gid/scripts/Writing.tcl
+++ kratos/interfaces/GiD/kratos.gid/scripts/Writing.tcl
@@ -541,37 +541,37 @@ proc write::tcl2json { value } {
     regexp {^value is a (.*?) with a refcount} [::tcl::unsupported::representation $value] -> type
  
     switch $type {
-	string {
-	    return [json::write string $value]
-	}
-	dict {
-	    return [json::write object {*}[
-		dict map {k v} $value {tcl2json $v}]]
-	}
-	list {
-	    return [json::write array {*}[lmap v $value {tcl2json $v}]]
-	}
-	int - double {
-	    return [expr {$value}]
-	}
-	booleanString {
-	    return [expr {$value ? "true" : "false"}]
-	}
-	default {
-	    # Some other type; do some guessing...
-	    if {$value eq "null"} {
-		# Tcl has *no* null value at all; empty strings are semantically
-		# different and absent variables aren't values. So cheat!
-		return $value
-	    } elseif {[string is integer -strict $value]} {
-		return [expr {$value}]
-	    } elseif {[string is double -strict $value]} {
-		return [expr {$value}]
-	    } elseif {[string is boolean -strict $value]} {
-		return [expr {$value ? "true" : "false"}]
-	    }
-	    return [json::write string $value]
-	}
+        string {
+            return [json::write string $value]
+        }
+        dict {
+            return [json::write object {*}[
+            dict map {k v} $value {tcl2json $v}]]
+        }
+        list {
+            return [json::write array {*}[lmap v $value {tcl2json $v}]]
+        }
+        int - double {
+            return [expr {$value}]
+        }
+        booleanString {
+            return [expr {$value ? "true" : "false"}]
+        }
+        default {
+            # Some other type; do some guessing...
+            if {$value eq "null"} {
+            # Tcl has *no* null value at all; empty strings are semantically
+            # different and absent variables aren't values. So cheat!
+            return $value
+            } elseif {[string is integer -strict $value]} {
+            return [expr {$value}]
+            } elseif {[string is double -strict $value]} {
+            return [expr {$value}]
+            } elseif {[string is boolean -strict $value]} {
+            return [expr {$value ? "true" : "false"}]
+            }
+            return [json::write string $value]
+        }
     }
 }
 
