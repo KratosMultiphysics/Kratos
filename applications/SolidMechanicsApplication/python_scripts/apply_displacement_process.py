d2bbb0e30dad8b48a94f5bbe33d849f9ace5e94c
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-04-14 18:47:24 +0000

    Changing "parameters" by "Parameters" to match with the interface tcl files.

--- applications/SolidMechanicsApplication/python_scripts/apply_displacement_process.py
+++ applications/SolidMechanicsApplication/python_scripts/apply_displacement_process.py
@@ -3,7 +3,7 @@ import KratosMultiphysics
 def Factory(settings, Model):
     if(type(settings) != KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
-    return ApplyDisplacementProcess(Model, settings["parameters"])
+    return ApplyDisplacementProcess(Model, settings["Parameters"])
 
 ##all the processes python processes should be derived from "python_process"
 class ApplyDisplacementProcess(KratosMultiphysics.ApplyConstantVectorValueProcess):
