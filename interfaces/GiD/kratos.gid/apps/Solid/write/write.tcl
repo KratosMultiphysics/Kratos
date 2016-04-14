f3264109fe788266f8d97fa1b5735a1ec3943400
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-04-14 17:31:17 +0000

    Misspell in the materials.py

--- interfaces/GiD/kratos.gid/apps/Solid/write/write.tcl
+++ interfaces/GiD/kratos.gid/apps/Solid/write/write.tcl
@@ -96,7 +96,7 @@ proc Solid::write::WriteMaterialsFile { } {
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 # Importing the Kratos Library
 from KratosMultiphysics import *
-from KratosMultiphysics.SolidApplication import *
+from KratosMultiphysics.SolidMechanicsApplication import *
 #from beam_sections_python_utility import SetProperties
 #from beam_sections_python_utility import SetMaterialProperties
 
