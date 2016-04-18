39c5d22efbead69358d808cf05938b4a29c4b8a6
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-04-18 10:17:48 +0000

    New KDEM_2D implementations.

---
+++ kratos/applications/DEM_application/custom_constitutive/DEM_KDEM_2D_CL.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+#include "DEM_application.h"
+#include "DEM_KDEM_2D_CL.h"
+#include "custom_elements/spheric_particle.h"
+
+namespace Kratos {
+
+    DEMContinuumConstitutiveLaw::Pointer DEM_KDEM2D::Clone() const {
+        DEMContinuumConstitutiveLaw::Pointer p_clone(new DEM_KDEM2D(*this));
+        return p_clone;
+    }
+
+    void DEM_KDEM2D::SetConstitutiveLawInProperties(Properties::Pointer pProp) const {
+        std::cout << "Assigning DEM_KDEM2D to properties " << pProp->Id() << std::endl;
+        pProp->SetValue(DEM_CONTINUUM_CONSTITUTIVE_LAW_POINTER, this->Clone());
+    }
+
+    void DEM_KDEM2D::CalculateContactArea(double radius, double other_radius, double& calculation_area) {
+        
+        KRATOS_TRY
+        double radius_sum = radius + other_radius;
+        double equiv_radius = radius * other_radius / radius_sum;
+        calculation_area = 2.0 * equiv_radius;
+        KRATOS_CATCH("")
+    }
+
+} /* namespace Kratos.*/
