760af1d689c1c94be504903f48fe4c2d9f8bf86a
gcasas <gcasas@cimne.upc.edu>
2016-04-22 19:15:52 +0000

    correcting the laplacian

--- applications/swimming_DEM_application/python_scripts/chandelier/gradient_recovery_analyser.py
+++ applications/swimming_DEM_application/python_scripts/chandelier/gradient_recovery_analyser.py
@@ -23,9 +23,33 @@ class sinus_vector_field():
         self.fx = [0, z * math.cos(x*z), y * math.cos(x*y)]
         self.fy = [z * math.cos(y*z), 0, x * math.cos(x*y)]
         self.fz = [y * math.cos(y*z), x * math.cos(x*z), 0]
+        self.fxy = [0, 0, math.cos(x*y) - y * x * math.sin(x*y)]
+        self.fyx = self.fxy
+        self.fxz = [0, math.cos(x*z) - z * x * math.sin(x*z), 0]
+        self.fzx = self.fxz
+        self.fyz = [math.cos(y*z) - z * y * math.sin(y*z), 0, 0]
+        self.fzy = self.fyz
         self.fxx = [0, - z ** 2 * math.sin(x*z), - y ** 2 * math.sin(x*y)]
         self.fyy = [- z ** 2 * math.sin(y*z), 0, - x ** 2 * math.sin(x*y)]
-        self.fzz = [- y ** 2 * math.sin(y*z), - x ** 2 * math.sin(x*z), 0]                
+        self.fzz = [- y ** 2 * math.sin(y*z), - x ** 2 * math.sin(x*z), 0]
+        
+class polynomial_vector_field():
+    def __init__(self):
+        pass
+    def Calculate(self, x, y, z):
+        self.f = [x**2, x*y*z, z]
+        self.fx = [2*x, y*z, 0]
+        self.fy = [0, x*z, 0]
+        self.fz = [0, x*y, 1]
+        self.fxy = [0, z, 0]
+        self.fyx = self.fxy
+        self.fxz = [0,y, 0]
+        self.fzx = self.fxz
+        self.fyz = [0, x, 0]
+        self.fzy = self.fyz
+        self.fxx = [2, 0, 0]
+        self.fyy = [0, 0, 0]
+        self.fzz = [0, 0, 0]
 
 class GradientRecoveryAnalyser:
     def __init__(self, scalar_field, vector_field):
@@ -44,7 +68,7 @@ class GradientRecoveryAnalyser:
         self.v0 = v.f[0]
         self.v1 = v.f[1] 
         self.v2 = v.f[2] 
-        self.lx = v.fxx[0] + v.fyy[0] + v.fzz[0] 
-        self.ly = v.fxx[1] + v.fyy[1] + v.fzz[1] 
-        self.lz = v.fxx[2] + v.fyy[2] + v.fzz[2] 
+        self.lx = v.fxx[0] + v.fxy[0] + v.fxz[0] 
+        self.ly = v.fyy[1] + v.fyx[1] + v.fyz[1] 
+        self.lz = v.fzz[2] + v.fzx[2] + v.fzy[2] 
         
