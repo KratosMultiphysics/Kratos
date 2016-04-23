957d8fe40ba3424c6f1728fd8b7761082f44f1ed
maceli <maceli@cimne.upc.edu>
2016-04-23 12:58:08 +0000

    remove c++03 warninng (the code will be valid in c++11, and we will recover it)

--- applications/DEM_application/custom_strategies/strategies/explicit_solver_strategy.h
+++ applications/DEM_application/custom_strategies/strategies/explicit_solver_strategy.h
@@ -282,7 +282,9 @@ namespace Kratos
           int max_FEM_Id = mpParticleCreatorDestructor->FindMaxNodeIdInModelPart( *mpFem_model_part );
           int max_cluster_Id = mpParticleCreatorDestructor->FindMaxNodeIdInModelPart( *mpCluster_model_part );
 
-          max_Id = std::max({max_Id, max_FEM_Id,max_cluster_Id});
+          //max_Id = std::max({max_Id, max_FEM_Id,max_cluster_Id});
+          max_Id = std::max(max_Id, max_FEM_Id);
+          max_Id = std::max(max_Id, max_cluster_Id;
           mpParticleCreatorDestructor->SetMaxNodeId(max_Id);
 
       }
