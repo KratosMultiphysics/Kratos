01e3a943427b24476f1c147503bdb68e77360546
msantasusana <msantasusana@cimne.upc.edu>
2016-04-25 12:51:49 +0000

    minor

--- applications/DEM_application/custom_strategies/strategies/iterative_solver_strategy.h
+++ applications/DEM_application/custom_strategies/strategies/iterative_solver_strategy.h
@@ -24,27 +24,27 @@ namespace Kratos
       public:
       ///@name Type Definitions
       ///@{
-        
+
       typedef ExplicitSolverStrategy<TSparseSpace,TDenseSpace,TLinearSolver>   BaseType;
-   
+
       typedef typename BaseType::NodesArrayType                             NodesArrayType;
       typedef typename BaseType::ElementsArrayType                          ElementsArrayType;
       typedef typename BaseType::ElementsIterator                           ElementsIterator;
       typedef typename BaseType::ConditionsArrayType                        ConditionsArrayType;
-      
+
       /*  Revisar charlie */
-      
-      typedef WeakPointerVector<Element> ParticleWeakVectorType; 
+
+      typedef WeakPointerVector<Element> ParticleWeakVectorType;
       typedef WeakPointerVector<Element >::iterator ParticleWeakIteratorType;
       typedef ParticleWeakVectorType::ptr_iterator ParticleWeakIteratorType_ptr;
 
-      
+
       /// Pointer definition of ExplicitSolverStrategy
       KRATOS_CLASS_POINTER_DEFINITION(IterativeSolverStrategy);
 
       /// Default constructor.
       IterativeSolverStrategy(){}
-     
+
       IterativeSolverStrategy(
                              ExplicitSolverSettings& settings,
                              const double max_delta_time,
@@ -56,8 +56,8 @@ namespace Kratos
                              typename DEMIntegrationScheme::Pointer pScheme,
                              typename SpatialSearch::Pointer pSpSearch)
       :ExplicitSolverStrategy<TSparseSpace,TDenseSpace,TLinearSolver>(settings, max_delta_time, n_step_search, safety_factor, delta_option, p_creator_destructor, p_dem_fem_search, pScheme, pSpSearch)
-      {                    
-          BaseType::GetParticleCreatorDestructor()   = p_creator_destructor;                            
+      {
+          BaseType::GetParticleCreatorDestructor()   = p_creator_destructor;
       }
 
       /// Destructor.
@@ -65,72 +65,69 @@ namespace Kratos
       {
          Timer::SetOuputFile("TimesPartialRelease");
          Timer::PrintTimingInformation();
-      }      
-      
-    
+      }
+
+
       virtual void Initialize()
       {
-        
+
         KRATOS_TRY
         ModelPart& r_model_part = BaseType::GetModelPart();
         BaseType::Initialize();
         BaseType::InitializeSolutionStep();
         BaseType::ForceOperations(r_model_part);
-        BaseType::FinalizeSolutionStep();  
+        BaseType::FinalizeSolutionStep();
         //SchemeInitialize();
-           
-        KRATOS_CATCH("")        
+
+        KRATOS_CATCH("")
       }// Initialize()
 
-      
+
        void SchemeInitialize()
       {
           BaseType::GetScheme()->Calculate(BaseType::GetModelPart(),0);
-          BaseType::GetScheme()->Calculate(BaseType::GetClusterModelPart(),0); 
+          BaseType::GetScheme()->Calculate(BaseType::GetClusterModelPart(),0);
       }
-      
-      
+
+
       void SchemePredict()
       {
           BaseType::GetScheme()->Calculate(BaseType::GetModelPart(),1);
-          BaseType::GetScheme()->Calculate(BaseType::GetClusterModelPart(),1); 
+          BaseType::GetScheme()->Calculate(BaseType::GetClusterModelPart(),1);
       }
-  
+
       void SchemeCorrect()
       {
-        BaseType::GetScheme()->Calculate(BaseType::GetModelPart(),2); 
-        BaseType::GetScheme()->Calculate(BaseType::GetClusterModelPart(),2); 
+        BaseType::GetScheme()->Calculate(BaseType::GetModelPart(),2);
+        BaseType::GetScheme()->Calculate(BaseType::GetClusterModelPart(),2);
       }
-      
+
       virtual double Solve()
       {
-            
+
         KRATOS_TRY
 
         ModelPart& r_model_part = BaseType::GetModelPart();
-        
+
         BaseType::InitializeSolutionStep();
         SchemePredict();
-        BaseType::SearchOperations(r_model_part);
+        BaseType::SearchDEMOperations(r_model_part);
+        BaseType::SearchFEMOperations(r_model_part);
         BaseType::ForceOperations(r_model_part);
         SchemeCorrect();
-        BaseType::FinalizeSolutionStep();         
+        BaseType::FinalizeSolutionStep();
 
         return 0.00;
 
         KRATOS_CATCH("")
-                  
+
       }//Solve()
 
   };//ClassIterativeSolverStrategy
 
-  
-  
-     
+
+
+
 }  // namespace Kratos.
 
 #endif // KRATOS_FILENAME_H_INCLUDED  defined
-
-
-
-
