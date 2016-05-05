1e3d1fd7672c4978d4f1f5319f469771142171f6
maceli <maceli@cimne.upc.edu>
2016-05-05 13:40:17 +0000

    Initial distance is no longer imposed to 0.0 by the sintering particle

--- applications/DEM_application/custom_elements/sintering_spheric_continuum_particle.h
+++ applications/DEM_application/custom_elements/sintering_spheric_continuum_particle.h
@@ -1,24 +1,16 @@
 //   
 //   Project Name:        ThermalD       
-//   Last Modified by:    $Author: Ferran Arrufat $
+//   Last Modified by:    $Author: Szymon Nosewicz and Miguel Angel Celigueta (maceli@cimne.upc.edu) $
 //   Date:                $Date: 2015-02-01  $
 //   Revision:            $Revision: 1.0.0.0 $
 //
-//
-
 
 #if !defined(KRATOS_SINTERING_SPHERIC_CONTINUUM_PARTICLE_H_INCLUDED )
 #define  KRATOS_SINTERING_SPHERIC_CONTINUUM_PARTICLE_H_INCLUDED
 
-
-
 // System includes
 #include <string>
 #include <iostream> 
-
-
-// External includes 
-
 
 // Project includes
 #include "includes/define.h"
@@ -29,66 +21,62 @@
 #include "containers/vector_component_adaptor.h"
 #include "discrete_element.h"
 
+namespace Kratos {
 
-namespace Kratos
-{
-	class KRATOS_API(DEM_APPLICATION) SinteringSphericContinuumParticle : public ThermalSphericParticle<SphericContinuumParticle>
-	{
-	public:
+    class KRATOS_API(DEM_APPLICATION) SinteringSphericContinuumParticle : public ThermalSphericParticle<SphericContinuumParticle> {
+    public:
 
-		/// Pointer definition of SinteringSphericContinuumParticle
-		KRATOS_CLASS_POINTER_DEFINITION(SinteringSphericContinuumParticle);
+        /// Pointer definition of SinteringSphericContinuumParticle
+        KRATOS_CLASS_POINTER_DEFINITION(SinteringSphericContinuumParticle);
 
-		typedef WeakPointerVector<Element> ParticleWeakVectorType;
-		typedef ParticleWeakVectorType::ptr_iterator ParticleWeakIteratorType_ptr;
-		typedef WeakPointerVector<Element >::iterator ParticleWeakIteratorType;
+        typedef WeakPointerVector<Element> ParticleWeakVectorType;
+        typedef ParticleWeakVectorType::ptr_iterator ParticleWeakIteratorType_ptr;
+        typedef WeakPointerVector<Element >::iterator ParticleWeakIteratorType;
 
-		typedef Node <3> NodeType;
-		typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
-		typedef std::size_t IndexType;
-		typedef Geometry<Node < 3 > > GeometryType;
-		typedef Properties PropertiesType; 
+        typedef Node <3> NodeType;
+        typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
+        typedef std::size_t IndexType;
+        typedef Geometry<Node < 3 > > GeometryType;
+        typedef Properties PropertiesType;
 
-		/// Default constructor. 
-		SinteringSphericContinuumParticle() {};
-		SinteringSphericContinuumParticle(IndexType NewId, GeometryType::Pointer pGeometry):ThermalSphericParticle<SphericContinuumParticle>(NewId, pGeometry){}
-		SinteringSphericContinuumParticle(IndexType NewId, NodesArrayType const& ThisNodes):ThermalSphericParticle<SphericContinuumParticle>(NewId, ThisNodes){}
-		SinteringSphericContinuumParticle(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties):ThermalSphericParticle<SphericContinuumParticle>(NewId, pGeometry, pProperties){}
+        /// Default constructor. 
 
+        SinteringSphericContinuumParticle() {
+        };
 
-		Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const override {
-			return Element::Pointer(new SinteringSphericContinuumParticle(NewId, GetGeometry().Create(ThisNodes), pProperties));
-		}
-		
+        SinteringSphericContinuumParticle(IndexType NewId, GeometryType::Pointer pGeometry) : ThermalSphericParticle<SphericContinuumParticle>(NewId, pGeometry) {
+        }
 
-	/// Destructor.
-	virtual ~SinteringSphericContinuumParticle() {};
+        SinteringSphericContinuumParticle(IndexType NewId, NodesArrayType const& ThisNodes) : ThermalSphericParticle<SphericContinuumParticle>(NewId, ThisNodes) {
+        }
 
-	void Initialize(const ProcessInfo& r_process_info)  override;
+        SinteringSphericContinuumParticle(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties) : ThermalSphericParticle<SphericContinuumParticle>(NewId, pGeometry, pProperties) {
+        }
+
+        Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const override {
+            return Element::Pointer(new SinteringSphericContinuumParticle(NewId, GetGeometry().Create(ThisNodes), pProperties));
+        }
+
+        /// Destructor.
+
+        virtual ~SinteringSphericContinuumParticle() {
+        };
+
+        void Initialize(const ProcessInfo& r_process_info) override;
         void InitializeSolutionStep(ProcessInfo& r_process_info) override;
-
-	void UpdateContinuumNeighboursVector(ProcessInfo& r_process_info);
-
-	void SetInitialSinteringSphereContacts(ProcessInfo& r_process_info);
-
-	void InitializeForceComputation(ProcessInfo& r_process_info)  override;
-
+        void UpdateContinuumNeighboursVector(ProcessInfo& r_process_info);
+        void SetInitialSinteringSphereContacts(ProcessInfo& r_process_info);
+        void InitializeForceComputation(ProcessInfo& r_process_info) override;
         void ComputeOtherBallToBallForces(array_1d<double, 3>& other_ball_to_ball_forces) override;
-        
-        double GetInitialDelta(int index)  override;
+        //double GetInitialDelta(int index)  override;
+        void ComputeContactArea(const double rmin, double indentation, double& calculation_area) override;
 
-		void ComputeContactArea(const double rmin, double indentation, double& calculation_area) override;
-
-
-
-	double mSinteringDisplacement;
-	double mSinteringDrivingForce;
-	std::vector<double> mOldNeighbourSinteringDisplacement; // initialization of a container of sintering displ - old
-	std::vector<double> mActualNeighbourSinteringDisplacement; // initialization of a container of sintering displ - actual
-	};
-
-
-}  // namespace Kratos.
+        double mSinteringDisplacement;
+        double mSinteringDrivingForce;
+        std::vector<double> mOldNeighbourSinteringDisplacement; // initialization of a container of sintering displ - old
+        std::vector<double> mActualNeighbourSinteringDisplacement; // initialization of a container of sintering displ - actual
+    };
+} // namespace Kratos.
 
 #endif // KRATOS_THERMAL_SPHERIC_PARTICLE_H_INCLUDED  defined 
 
