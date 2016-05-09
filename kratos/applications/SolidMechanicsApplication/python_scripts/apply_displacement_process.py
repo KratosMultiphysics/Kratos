c0fd5f9ce384251c07a3f32e87a8345a135526fb
vmataix <vmataix@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-05-09 15:43:49 +0000

    Correcting the obligation of the condition to be fixed (anyway this process is going to be replaced soon for one more generic present in the core)

--- kratos/applications/SolidMechanicsApplication/python_scripts/apply_displacement_process.py
+++ kratos/applications/SolidMechanicsApplication/python_scripts/apply_displacement_process.py
@@ -27,52 +27,42 @@ class ApplyDisplacementProcess(Process):
         
         model_part = Model[settings["model_part_name"].GetString()]
 
-        if settings["is_fixed_x"].GetBool() == True:
-            # Auxiliar x-component parameters creation
-            x_params = Parameters("{}")           
-            x_params.AddValue("model_part_name",settings["model_part_name"])
-            x_params.AddValue("mesh_id",settings["mesh_id"])
-            x_params.AddValue("is_fixed",settings["is_fixed_x"])
-            x_params.AddValue("value",settings["direction"][0])
-            x_params.AddEmptyValue("variable_name").SetString("DISPLACEMENT_X")
-            
-            self.x_component_process = ApplyConstantScalarValueProcess(model_part, x_params)
-            
-        if settings["is_fixed_y"].GetBool() == True:
-            # Auxiliar y-component parameters creation
-            y_params = Parameters("{}")
-            y_params.AddValue("model_part_name",settings["model_part_name"])
-            y_params.AddValue("mesh_id",settings["mesh_id"])
-            y_params.AddValue("is_fixed",settings["is_fixed_y"])
-            y_params.AddValue("value",settings["direction"][1])
-            y_params.AddEmptyValue("variable_name").SetString("DISPLACEMENT_Y")
-            
-            self.y_component_process = ApplyConstantScalarValueProcess(model_part, y_params)
-            
-        if settings["is_fixed_z"].GetBool() == True:
-            # Auxiliar x-component parameters creation
-            z_params = Parameters("{}")
-            z_params.AddValue("model_part_name",settings["model_part_name"])
-            z_params.AddValue("mesh_id",settings["mesh_id"])
-            z_params.AddValue("is_fixed",settings["is_fixed_z"])
-            z_params.AddValue("value",settings["direction"][2])
-            z_params.AddEmptyValue("variable_name").SetString("DISPLACEMENT_Z")
-            
-            self.z_component_process = ApplyConstantScalarValueProcess(model_part, z_params)
+        # Auxiliar x-component parameters creation
+        x_params = Parameters("{}")           
+        x_params.AddValue("model_part_name",settings["model_part_name"])
+        x_params.AddValue("mesh_id",settings["mesh_id"])
+        x_params.AddValue("is_fixed",settings["is_fixed_x"])
+        x_params.AddValue("value",settings["direction"][0])
+        x_params.AddEmptyValue("variable_name").SetString("DISPLACEMENT_X")
         
-        # Auxiliar vector with the fixicity settings    
-        self.fixicity_vec = [settings["is_fixed_x"].GetBool(),
-                             settings["is_fixed_y"].GetBool(),
-                             settings["is_fixed_z"].GetBool()]
+        self.x_component_process = ApplyConstantScalarValueProcess(model_part, x_params)
+        
+        self.model_part = model_part
+        
+        # Auxiliar y-component parameters creation
+        y_params = Parameters("{}")
+        y_params.AddValue("model_part_name",settings["model_part_name"])
+        y_params.AddValue("mesh_id",settings["mesh_id"])
+        y_params.AddValue("is_fixed",settings["is_fixed_y"])
+        y_params.AddValue("value",settings["direction"][1])
+        y_params.AddEmptyValue("variable_name").SetString("DISPLACEMENT_Y")
+        
+        self.y_component_process = ApplyConstantScalarValueProcess(model_part, y_params)
             
+        # Auxiliar x-component parameters creation
+        z_params = Parameters("{}")
+        z_params.AddValue("model_part_name",settings["model_part_name"])
+        z_params.AddValue("mesh_id",settings["mesh_id"])
+        z_params.AddValue("is_fixed",settings["is_fixed_z"])
+        z_params.AddValue("value",settings["direction"][2])
+        z_params.AddEmptyValue("variable_name").SetString("DISPLACEMENT_Z")
+        
+        self.z_component_process = ApplyConstantScalarValueProcess(model_part, z_params)
         
     def ExecuteInitialize(self):
-               
-        if self.fixicity_vec[0] == True:
-            self.x_component_process.ExecuteInitialize()
+        
+        self.x_component_process.ExecuteInitialize()
+        
+        self.y_component_process.ExecuteInitialize()
             
-        if self.fixicity_vec[1] == True:
-            self.y_component_process.ExecuteInitialize()
-            
-        if self.fixicity_vec[2] == True:
-            self.z_component_process.ExecuteInitialize()
+        self.z_component_process.ExecuteInitialize()
