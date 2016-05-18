82e6d5971ec01f929ff065b97d1043a6afe5e3d5
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-05-18 14:28:03 +0000

    PFEM app starts writing something (mdpa)

--- kratos/interfaces/GiD/kratos.gid/apps/Pfem/write/write.tcl
+++ kratos/interfaces/GiD/kratos.gid/apps/Pfem/write/write.tcl
@@ -14,8 +14,22 @@ proc Pfem::write::writeParametersEvent { } {
 # Model Part Blocks
 proc Pfem::write::writeModelPartEvent { } {
     write::initWriteData "SMParts" "SMMaterials"
+    write::WriteString "Begin ModelPartData"
+    write::WriteString "End ModelPartData"
     write::WriteString "Begin Properties 0"
     write::WriteString "End Properties"
+    
+    write::WriteString "Begin Nodes"
+    set dim [write::getValue "PFEM_dimensions"]
+    #if { $dim eq "2D" || $dim eq "2Da"}   
+    customlib::WriteCoordinates "%5d %14.5e %14.5e %14.5e%.0s\n"           
+    write::WriteString "End Nodes"
+    
+    write::WriteString "Begin Elements TwoStepUpdatedLagrangianVPFluidElement2D"
+    set elements_conditions [list "Shells"]
+    set element_formats [list {"%10d" "element" "id"} {"%10d" "element" "connectivities"} {"%10d" "material" "MID"}]
+    customlib::WriteConnectivities $elements_conditions $element_formats 
+    write::WriteString "End Elements"
 }
 
 # Custom files (Copy python scripts, write materials file...)
