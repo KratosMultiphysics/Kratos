17769d011d3764a030402bc303850745d4740624
Carlos Roig <roigcarlo@gmail.com>
2016-05-20 10:20:36 +0000

    Simplified SmallTests.py and made it contextDependent to avoid all problems with relative paths

--- applications/StructuralMechanicsApplication/tests/SmallTests.py
+++ applications/StructuralMechanicsApplication/tests/SmallTests.py
@@ -1,3 +1,5 @@
+import os
+
 # Import Kratos
 from KratosMultiphysics import *
 
@@ -6,185 +8,74 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 import Kratos_Execute_Solid_Test as Execute_Test
 
 
-def GetFilePath(fileName):
-    return os.path.dirname(__file__) + "/" + fileName
+# This utiltiy will control the execution scope in case we need to acces files or we depend
+# on specific relative locations of the files.
+
+# TODO: Should we move this to KratosUnittest?
+class controlledExecutionScope:
+    def __init__(self, scope):
+        self.currentPath = os.getcwd()
+        self.scope = scope
+
+    def __enter__(self):
+        os.chdir(self.scope)
+
+    def __exit__(self, type, value, traceback):
+        os.chdir(self.currentPath)
 
 
-class DynamicBossakTests(KratosUnittest.TestCase):
+class StructrualMechanichsTestFactory(KratosUnittest.TestCase):
 
     def setUp(self):
-        self.file_name = "dynamic_test/dynamic_bossak_test"
-        
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
+        # Within this location context:
+        with controlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            # Initialize GiD  I/O
+            parameter_file = open(self.file_name + "_parameters.json", 'r')
+            ProjectParameters = Parameters(parameter_file.read())
 
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
+            # Creating the model part
+            self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
 
-    def test_Bossak(self):
-        self.test.Solve()
+    def test_execution(self):
+        # Within this location context:
+        with controlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            self.test.Solve()
 
     def tearDown(self):
         pass
 
 
-class DynamicNewmarkTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "dynamic_test/dynamic_newmark_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_Newmark(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class DynamicBossakTests(StructrualMechanichsTestFactory):
+    file_name = "dynamic_test/dynamic_bossak_test"
 
 
-class SprismMembranePatchTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "sprism_test/patch_membrane_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_MembranePatch(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class DynamicNewmarkTests(StructrualMechanichsTestFactory):
+    file_name = "dynamic_test/dynamic_newmark_test"
 
 
-class SprismBendingPatchTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "sprism_test/patch_bending_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_BendingPatch(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class SprismMembranePatchTests(StructrualMechanichsTestFactory):
+    file_name = "sprism_test/patch_membrane_test"
 
 
-class ShellQ4ThickBendingRollUpTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "Shell_Q4_Thick__BendingRollUp_test/Shell_Q4_Thick__BendingRollUp_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_ShellQ4ThickBendingRollUpTests(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class SprismBendingPatchTests(StructrualMechanichsTestFactory):
+    file_name = "sprism_test/patch_bending_test"
 
 
-class ShellQ4ThickDrillingRollUpTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "Shell_Q4_Thick__DrillingRollUp_test/Shell_Q4_Thick__DrillingRollUp_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_ShellQ4ThickDrillingRollUpTests(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class ShellQ4ThickBendingRollUpTests(StructrualMechanichsTestFactory):
+    file_name = "Shell_Q4_Thick__BendingRollUp_test/Shell_Q4_Thick__BendingRollUp_test"
 
 
-class ShellT3IsotropicScordelisTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "Shell_T3_Isotropic_Scordelis_test/Shell_T3_Isotropic_Scordelis_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_ShellT3IsotropicScordelisTests(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class ShellQ4ThickDrillingRollUpTests(StructrualMechanichsTestFactory):
+    file_name = "Shell_Q4_Thick__DrillingRollUp_test/Shell_Q4_Thick__DrillingRollUp_test"
 
 
-class ShellT3ThinBendingRollUpTests(KratosUnittest.TestCase):
-
-    def setUp(self):
-        self.file_name = "Shell_T3_Thin__BendingRollUp_test/Shell_T3_Thin__BendingRollUp_test"
-
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_ShellT3ThinBendingRollUpTests(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class ShellT3IsotropicScordelisTests(StructrualMechanichsTestFactory):
+    file_name = "Shell_T3_Isotropic_Scordelis_test/Shell_T3_Isotropic_Scordelis_test"
 
 
-class ShellT3ThinDrillingRollUpTests(KratosUnittest.TestCase):
+class ShellT3ThinBendingRollUpTests(StructrualMechanichsTestFactory):
+    file_name = "Shell_T3_Thin__BendingRollUp_test/Shell_T3_Thin__BendingRollUp_test"
 
-    def setUp(self):
-        self.file_name = "Shell_T3_Thin__DrillingRollUp_test/Shell_T3_Thin__DrillingRollUp_test"
 
-        # Initialize GiD  I/O
-        input_file_name = GetFilePath(self.file_name)
-        parameter_file = open(input_file_name + "_parameters.json", 'r')
-        ProjectParameters = Parameters(parameter_file.read())
-
-        # Creating the model part
-        self.test = Execute_Test.Kratos_Execute_Test(ProjectParameters)
-
-    def test_ShellT3ThinDrillingRollUpTests(self):
-        self.test.Solve()
-
-    def tearDown(self):
-        pass
+class ShellT3ThinDrillingRollUpTests(StructrualMechanichsTestFactory):
+    file_name = "Shell_T3_Thin__DrillingRollUp_test/Shell_T3_Thin__DrillingRollUp_test"
