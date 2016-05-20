82cbc4776d1d95cc31c524a42f78b91750dc2cbb
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-05-20 14:22:05 +0000

    adding an example for the altair course

---
+++ kratos/kratos/python_scripts/application_generator/laplacian_application_example.py
@@ -0,0 +1,43 @@
+import sys
+
+from classes.elementCreator import ElementCreator
+from classes.conditionCreator import ConditionCreator
+from classes.processCreator import ProcessCreator
+from classes.classMemberCreator import ClassMemberCreator
+from classes.variableCreator import VariableCreator
+
+from applicationGenerator import ApplicationGenerator
+
+# Read the application name and generate Camel, Caps and Low
+appCamel = "MyLaplacian" #DO NOT put "application" in the name, it will be added automatically
+
+# Fetch the applications directory
+debugApp = ApplicationGenerator(appCamel)
+
+# Add KratosVariables
+debugApp.AddVariables([
+    VariableCreator(name='MY_SCALAR', vtype='double'),
+    VariableCreator(name='MY_VECTOR', vtype='int', is3D=True),
+])
+
+# Add test element
+debugApp.AddElements([
+    ElementCreator('MyLaplacianElement')
+    .AddDofs(['TEMPERATURE'])
+    .AddFlags([])
+    .AddClassMemberVariables([])
+])
+
+debugApp.AddConditions([
+    ConditionCreator('MyFaceCondition')
+	#.AddDofs([TEMPERATURE])
+    #.AddFlags([])
+])
+
+debugApp.AddProcesses([
+    ProcessCreator('DoSomethingProcess')
+])
+
+debugApp.Generate()
+
+print("Your application has been generated in: applications/{}Application".format(appCamel))
