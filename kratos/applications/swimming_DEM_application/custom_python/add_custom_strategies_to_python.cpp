2dec94532c7080c80edf9138b09a2e39493f48f2
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-05-23 22:43:02 +0000

    new hybrid scheme

--- kratos/applications/swimming_DEM_application/custom_python/add_custom_strategies_to_python.cpp
+++ kratos/applications/swimming_DEM_application/custom_python/add_custom_strategies_to_python.cpp
@@ -79,6 +79,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "solving_strategies/schemes/scheme.h"
 //#include "../DEM_application/DEM_application.h"
 #include "../DEM_application/custom_strategies/schemes/dem_integration_scheme.h"
+#include "custom_strategies/schemes/hybrid_bashforth_scheme.h"
 #include "custom_strategies/schemes/terminal_velocity_scheme.h"
 
 //parallel strategies
@@ -99,6 +100,9 @@ namespace Kratos
 
 		void  AddCustomStrategiesToPython()
 		{
+            class_< HybridBashforthScheme, bases<DEMIntegrationScheme>,  boost::noncopyable>
+            ("HybridBashforthScheme", init<>());
+
             class_< TerminalVelocityScheme, bases<DEMIntegrationScheme>,  boost::noncopyable>
             ("TerminalVelocityScheme", init<>());
 		  
