2594f33b33cbbd3074fc4805f141caa4e4796917
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-05-24 07:50:05 +0000

    Now no-slip condition is applied by components. Direction (0,0,0) problem is always avoided. (corrected by A. Adam)

--- applications/FluidDynamicsApplication/python_scripts/apply_noslip_process.py
+++ applications/FluidDynamicsApplication/python_scripts/apply_noslip_process.py
@@ -1,19 +1,34 @@
-import KratosMultiphysics 
-import KratosMultiphysics.FluidDynamicsApplication 
+from KratosMultiphysics import * 
 
 def Factory(settings, Model):
-    if(type(settings) != KratosMultiphysics.Parameters):
+    if(type(settings) != Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return ApplyNoSlipProcess(Model, settings["Parameters"])
 
 ##all the processes python processes should be derived from "python_process"
-class ApplyNoSlipProcess(KratosMultiphysics.ApplyConstantVectorValueProcess):
-    def __init__(self, Model, Parameters ):
-        model_part = Model[Parameters["model_part_name"].GetString()]
-        Parameters.AddEmptyValue("variable_name").SetString("VELOCITY")
-        Parameters.AddEmptyValue("is_fixed_x").SetBool(True)
-        Parameters.AddEmptyValue("is_fixed_y").SetBool(True)
-        Parameters.AddEmptyValue("is_fixed_z").SetBool(True)
-        Parameters.AddEmptyValue("factor").SetDouble(0.0)
-        KratosMultiphysics.ApplyConstantVectorValueProcess.__init__(self,model_part, Parameters)
+class ApplyNoSlipProcess(Process):
+    def __init__(self, Model, settings):
         
+        Process.__init__(self)
+        
+        model_part = Model[settings["model_part_name"].GetString()]
+        settings.AddEmptyValue("is_fixed").SetBool(True)
+        settings.AddEmptyValue("value").SetDouble(0)
+        
+        # For X component
+        settings.AddEmptyValue("variable_name").SetString("VELOCITY_X")
+        self.x = ApplyConstantScalarValueProcess(model_part,settings)
+        
+        # For Y component
+        settings.AddEmptyValue("variable_name").SetString("VELOCITY_Y")
+        self.y = ApplyConstantScalarValueProcess(model_part,settings)
+        
+        #For Z component
+        settings.AddEmptyValue("variable_name").SetString("VELOCITY_Z")
+        self.z = ApplyConstantScalarValueProcess(model_part,settings)
+
+    def ExecuteInitialize(self):
+        
+        self.x.ExecuteInitialize()
+        self.y.ExecuteInitialize()
+        self.z.ExecuteInitialize()
