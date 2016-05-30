e30341281cd55c0030bf0a2a44d198ea05c07048
gcasas <gcasas@cimne.upc.edu>
2016-05-30 21:31:39 +0000

    aesth.

--- applications/DEM_application/custom_elements/nanoparticle.cpp
+++ applications/DEM_application/custom_elements/nanoparticle.cpp
@@ -23,17 +23,14 @@ void NanoParticle::ComputeAdditionalForces(array_1d<double, 3>& additionally_app
                              const ProcessInfo& r_current_process_info,
                              const array_1d<double,3>& gravity){
     KRATOS_TRY
-
     array_1d<double, 3> brownian_motion_force; brownian_motion_force.clear();
     array_1d<double, 3> van_der_waals_force; van_der_waals_force.clear();
     array_1d<double, 3> double_layer_force; double_layer_force.clear();
 
     noalias(additionally_applied_force) += brownian_motion_force + van_der_waals_force + double_layer_force;
 
-
     //Now add the contribution of base class function (gravity or other forces added in upper levels):
     SphericParticle::ComputeAdditionalForces(additionally_applied_force, additionally_applied_moment, r_current_process_info, gravity);
-
     KRATOS_CATCH( "" )
 }
 
