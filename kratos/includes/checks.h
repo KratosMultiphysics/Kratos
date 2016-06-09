0f6e7b97ca9706a11f7c624585854b1d9bc85487
Pooyan Dadvand <pooyan@cimne.upc.edu>
2016-06-09 11:15:56 +0000

    Adding checks.h

---
+++ kratos/includes/checks.h
@@ -0,0 +1,32 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics 
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Pooyan Dadvand
+//                    
+//
+
+#include "includes/kratos_exception.h"
+
+#if !defined(KRATOS_CHECKS_H_INCLUDED )
+#define  KRATOS_CHECKS_H_INCLUDED
+
+///@addtogroup KratosCore
+///@{
+
+///@name macros
+///@{ 
+#define KRATOS_CHECK(IsTrue) if(!(IsTrue)) KRATOS_ERROR 
+
+///@}
+
+///@} addtogroup block
+
+#endif // KRATOS_CHECKS_H_INCLUDED  defined 
+
+
