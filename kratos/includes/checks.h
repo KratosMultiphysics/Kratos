cf89e0e04bad573376afcbc1a99d43b08d8dca52
Pooyan Dadvand <pooyan@cimne.upc.edu>
2016-06-13 14:07:22 +0000

    Bug fixes in checks and exceptions

--- kratos/includes/checks.h
+++ kratos/includes/checks.h
@@ -2,16 +2,16 @@
 //    ' /   __| _` | __|  _ \   __|
 //    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics 
+//                   Multi-Physics
 //
-//  License:		 BSD License 
+//  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
 //  Main authors:    Pooyan Dadvand
-//                    
+//
 //
 
-#include "includes/kratos_exception.h"
+#include "includes/exception.h"
 
 #if !defined(KRATOS_CHECKS_H_INCLUDED )
 #define  KRATOS_CHECKS_H_INCLUDED
@@ -19,24 +19,22 @@
 ///@addtogroup KratosCore
 ///@{
 ///@name macros
-///@{ 
-#define KRATOS_CHECK(IsTrue) if(!(IsTrue)) KRATOS_ERROR 
-#define KRATOS_CHECK_IS_FALSE(IsFalse) if(IsFalse) KRATOS_ERROR 
+///@{
+#define KRATOS_CHECK(IsTrue) if(!(IsTrue)) KRATOS_ERROR
+#define KRATOS_CHECK_IS_FALSE(IsFalse) if(IsFalse) KRATOS_ERROR
 
-#define KRATOS_CHECK_EQUAL(a,b) if(!(a == b)) KRATOS_ERROR 
-#define KRATOS_CHECK_NOT_EQUAL(a,b) if(a == b) KRATOS_ERROR 
+#define KRATOS_CHECK_EQUAL(a,b) if(!(a == b)) KRATOS_ERROR
+#define KRATOS_CHECK_NOT_EQUAL(a,b) if(a == b) KRATOS_ERROR
 
-#define KRATOS_CHECK_LESS(a,b) if(!(a < b)) KRATOS_ERROR 
-#define KRATOS_CHECK_LESS_EQUAL(a,b) if(a > b) KRATOS_ERROR 
+#define KRATOS_CHECK_LESS(a,b) if(!(a < b)) KRATOS_ERROR
+#define KRATOS_CHECK_LESS_EQUAL(a,b) if(a > b) KRATOS_ERROR
 
-#define KRATOS_CHECK_GREATER(a,b) if(!(a > b)) KRATOS_ERROR 
-#define KRATOS_CHECK_GREATER_EQUAL(a,b) if(a < b) KRATOS_ERROR 
+#define KRATOS_CHECK_GREATER(a,b) if(!(a > b)) KRATOS_ERROR
+#define KRATOS_CHECK_GREATER_EQUAL(a,b) if(a < b) KRATOS_ERROR
 
 
 ///@}
 
 ///@} addtogroup block
 
-#endif // KRATOS_CHECKS_H_INCLUDED  defined 
-
-
+#endif // KRATOS_CHECKS_H_INCLUDED  defined
