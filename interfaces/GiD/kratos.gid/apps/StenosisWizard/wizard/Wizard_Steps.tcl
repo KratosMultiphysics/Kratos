f9f6efbd4d6a8cd6de86db968b6539e7d03448d8
fjgarate <fjgarate@cimne.upc.edu>
2016-06-13 15:32:46 +0000

    Wizard step 2

--- interfaces/GiD/kratos.gid/apps/StenosisWizard/wizard/Wizard_Steps.tcl
+++ interfaces/GiD/kratos.gid/apps/StenosisWizard/wizard/Wizard_Steps.tcl
@@ -9,9 +9,7 @@ proc StenosisWizard::Wizard::Init { } {
 }
 
 proc StenosisWizard::Wizard::Geometry { win } {
-# Units
-     set mtxt [= "m"]
-     set mmtxt [= "mm"]
+     Wizard::SetWindowSize 650 500
      set entrywidth 8
      
      set properties [Wizard::GetStepProperties Geometry]
@@ -172,9 +170,85 @@ proc ValidateDraw { } {
 }
 
 proc StenosisWizard::Wizard::Material { win } {
+     Wizard::SetWindowSize 250 350
+     set entrywidth 8
+     
+     set properties [Wizard::GetStepProperties Material]
+     #W $properties
+     
+     # Set the widgets
+     #set imgname [Wizard::GetProperty Geometry ImageGeom,value]
+     #set img1 [apps::getImgFrom StenosisWizard $imgname]
+     
+     # Left frame
+     set fr1 [ttk::frame $win.fr1 -borderwidth 10]
+     # Rigth frame
+     #set fr2 [ttk::frame $win.fr2 -borderwidth 10]
 
+     #set labIm [ttk::label $fr2.lIm -image $img1]
+     
+     # Geom frame
+     set labfr1 [ttk::labelframe $fr1.lfr1 -text [= "Define material data"] -padding 10 -width 200 -height 200 ]
+     
+     set i 1
+     set listids [list ]
+     foreach prop $properties {
+        if {$prop ni [list "Active" "Visited" "State"]} {
+            set j [Wizard::GetProperty Material $prop,order]
+            lappend listids $j
+            set txt [= $prop]
+            set lab$j [ttk::label $labfr1.l$j -text "${txt}:"]
+            set ent$j [ttk::entry $labfr1.e$j -textvariable ::Wizard::wprops(Material,$prop,value) -width $entrywidth]
+            wcb::callback $labfr1.e$j before insert wcb::checkEntryForReal
+            #wcb::callback $labfr1.e$j after insert "StenosisWizard::Wizard::callbackCheckGeom"
+            #wcb::callback $labfr1.e$j after delete "StenosisWizard::Wizard::callbackCheckGeom"
+            #set labun$i [ttk::label $labfr1.lu$i -text "$mmtxt"]
+            set txt [= "Enter a value for $txt"]
+            tooltip::tooltip $labfr1.e$j "${txt}."
+            incr i
+        }
+     }
+     set listids [lsort $listids]
+     # Widget geometry     
+     # Grid the frames
+     grid $fr1 -column 1 -row 0 -sticky nw
+     #grid $fr2 -column 2 -row 0 -sticky ne
+     
+     #grid $labIm -column 0 -row 0 -sticky ne -rowspan 3
+         
+     # Label frames
+     grid $labfr1 -column 1 -row 0 -sticky wen -ipadx 2  -columnspan 2
+     
+     # Label frame 1 => Geometrical data frame
+
+     for {set i 0} {$i < [llength $listids]} {incr i} {
+        # Cada uno
+        set j [lindex $listids $i]
+        set lab "lab$j"
+        set ent "ent$j"
+        #set labun "labun$i"
+        #set units "units$i"
+        
+        grid [expr $$lab] -column 1 -row $i -sticky w -pady 2
+        grid [expr $$ent] -column 2 -row $i -sticky w -pady 2
+        #grid [expr $$labun] -column 3 -row $i -sticky w
+        #grid [expr $$units] -column 4 -row $i -sticky w
+          
+     }
+    
+    grid columnconfigure $labfr1 1 -minsize 120
 }
 proc StenosisWizard::Wizard::NextMaterial { } {
+     # Quitar parts existentes
+     # Crear una part con los datos que toquen
+     gid_groups_conds::addF {container[@n='StenosisWizard']/condition[@n='Parts']} group {n Fluid}
+     gid_groups_conds::addF {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n Element pn Element dict {[GetElements]} actualize_tree 1 values FractionalStep3D state hidden v FractionalStep3D}
+     gid_groups_conds::addF {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n ConstitutiveLaw pn {Fluid type} actualize_tree 1 values Newtonian,HerschelBulkley dict {[GetConstitutiveLaws]} state normal v Newtonian}
+     gid_groups_conds::addF -resolve_parametric 1 {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n DENSITY pn Density state {[PartParamState]} unit_magnitude Density help {} v 1000 units kg/m^3}
+     gid_groups_conds::addF -resolve_parametric 1 {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n VISCOSITY pn {Kinematic viscosity} state {[PartParamState]} unit_magnitude L^2/T help {Fluidized viscosity.} v 1e-6 units m^2/s}
+     gid_groups_conds::addF -resolve_parametric 1 {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n YIELD_STRESS pn {Yield stress (?)} state {[PartParamState]} unit_magnitude P help {} v 0 units Pa}
+     gid_groups_conds::addF {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n POWER_LAW_K pn {Consistency index (k)} state {[PartParamState]} unit_magnitude {} help {} v 1}
+     gid_groups_conds::addF {container[@n='StenosisWizard']/condition[@n='Parts']/group[@n='Fluid']} value {n POWER_LAW_N pn {Flow index (n)} state {[PartParamState]} unit_magnitude {} help {} v 1}
 
 }
 
