37226e48c54ca2dfb5b5a0fa5c2c9578939f2b4e
lgracia <lgracia@cimne.upc.edu>
2016-06-13 14:43:36 +0000

    new Dam interface (work in process)

---
+++ interfaces/GiD/kratos.gid/apps/Dam/start.tcl
@@ -0,0 +1,24 @@
+namespace eval ::Dam {
+    # Variable declaration
+    variable dir
+}
+
+proc ::Dam::Init { } {
+    # Variable initialization
+    variable dir
+    
+    set dir [apps::getMyDir "Dam"]
+    set ::Model::ValidSpatialDimensions [list 2D 3D]
+    ::spdAux::CreateDimensionWindow
+    
+}
+
+proc ::Dam::LoadMyFiles { } {
+    variable dir
+    
+    uplevel #0 [list source [file join $dir xml GetFromXML.tcl]]
+    uplevel #0 [list source [file join $dir write write.tcl]]
+    uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
+}
+
+::Dam::Init
