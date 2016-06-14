b74ba58b86d6a35c99b7465da8eaefb77fe808f4
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-06-14 13:12:17 +0000

    Adding new checks to checks.h

--- kratos/kratos/includes/checks.h
+++ kratos/kratos/includes/checks.h
@@ -11,6 +11,7 @@
 //
 //
 
+#include <cstring>
 #include "includes/exception.h"
 
 #if !defined(KRATOS_CHECKS_H_INCLUDED )
@@ -19,18 +20,21 @@
 ///@addtogroup KratosCore
 ///@{
 ///@name macros
-///@{
-#define KRATOS_CHECK(IsTrue) if(!(IsTrue)) KRATOS_ERROR
-#define KRATOS_CHECK_IS_FALSE(IsFalse) if(IsFalse) KRATOS_ERROR
+///@{ 
+#define KRATOS_CHECK(IsTrue) if(!(IsTrue)) KRATOS_ERROR << "Check failed becuase " << #IsTrue << " is not true"
+#define KRATOS_CHECK_IS_FALSE(IsFalse) if(IsFalse) KRATOS_ERROR  << "Check failed becuase " << #IsFalse << " is not false"
 
-#define KRATOS_CHECK_EQUAL(a,b) if(!(a == b)) KRATOS_ERROR
-#define KRATOS_CHECK_NOT_EQUAL(a,b) if(a == b) KRATOS_ERROR
+#define KRATOS_CHECK_EQUAL(a,b) if(!(a == b)) KRATOS_ERROR << "Check failed becuase " << #a << " is not equal to " << #b;
+#define KRATOS_CHECK_NOT_EQUAL(a,b) if(a == b) KRATOS_ERROR << "Check failed becuase " << #a << " is equal to " << #b;
 
-#define KRATOS_CHECK_LESS(a,b) if(!(a < b)) KRATOS_ERROR
-#define KRATOS_CHECK_LESS_EQUAL(a,b) if(a > b) KRATOS_ERROR
+#define KRATOS_CHECK_C_STRING_EQUAL(a,b) if((strcmp(a,b) != 0)) KRATOS_ERROR << "Check failed becuase \"" << a << "\" is not equal to \"" << b << "\"";
+#define KRATOS_CHECK_C_STRING_NOT_EQUAL(a,b) if((strcmp(a,b) == 0)) KRATOS_ERROR << "Check failed becuase \"" << a << "\" is equal to \"" << b << "\"";
 
-#define KRATOS_CHECK_GREATER(a,b) if(!(a > b)) KRATOS_ERROR
-#define KRATOS_CHECK_GREATER_EQUAL(a,b) if(a < b) KRATOS_ERROR
+#define KRATOS_CHECK_LESS(a,b) if(!(a < b)) KRATOS_ERROR << "Check failed becuase " << #a << " is greater than or equal to " << #b;
+#define KRATOS_CHECK_LESS_EQUAL(a,b) if(a > b) KRATOS_ERROR << "Check failed becuase " << #a << " is greater than " << #b; 
+
+#define KRATOS_CHECK_GREATER(a,b) if(!(a > b)) KRATOS_ERROR << "Check failed becuase " << #a << " is less than or equal to " << #b;
+#define KRATOS_CHECK_GREATER_EQUAL(a,b) if(a < b) KRATOS_ERROR  << "Check failed becuase " << #a << " is less than " << #b;
 
 
 ///@}
