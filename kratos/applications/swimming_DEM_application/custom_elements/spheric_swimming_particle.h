35510e4ebe9996ca46169ef39762f8750d76a734
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-06-18 10:20:07 +0000

    fixed first order quadrature with strict substepping. Very good accuracies obtained

--- kratos/applications/swimming_DEM_application/custom_elements/spheric_swimming_particle.h
+++ kratos/applications/swimming_DEM_application/custom_elements/spheric_swimming_particle.h
@@ -170,6 +170,7 @@ namespace Kratos
         double GetDaitcheCoefficient(int order, unsigned int n, unsigned int j, const double last_h_over_h, const int n_steps_per_quad_step);
         void CalculateExplicitFractionalDerivative(NodeType& node, array_1d<double, 3>& fractional_derivative, double& present_coefficient, Vector& historic_integrands, const double last_h_over_h, const int n_steps_per_quad_step);
         void AddHinsbergTailContribution(NodeType& node, array_1d<double, 3>& fractional_derivative_of_slip_vel, const int order, const int n_steps_per_quad_step, const double time, const double delta_time, const double last_h_over_h, vector<double>& historic_integrands);
+        void AddHinsbergTailContributionStrict(NodeType& node, array_1d<double, 3>& fractional_derivative_of_slip_vel, const int order, const int n_steps_per_quad_step, const double time, const double delta_time, const double last_h_over_h, vector<double>& historic_integrands);
         double Phi(const double x);
         void AddFdi(const int order, array_1d<double, 3>& F, const double t_win, const double ti, const double beta, const double last_h_over_h, const double delta_time, const vector<double>& historic_integrands, const array_1d<double, 3>& oldest_integrand);
         void AddFre(array_1d<double, 3>& old_Fi, const double beta, const double delta_time);
