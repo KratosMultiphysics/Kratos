f165409370f6d0800e662e75b3252977d63f14c8
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-06-22 15:52:44 +0000

    Configuration for the nightly builds

--- kratos/scripts/build/nightly_build.sh
+++ kratos/scripts/build/nightly_build.sh
@@ -16,15 +16,21 @@ mkdir -p cmake_gcc
 mkdir -p cmake_clang
 
 ## Copy the already prepared configure files
-cp ${HOME}/nightlyConfig/configure_gcc.sh ${HOME}/Kratos/cmake_gcc/configure.sh
-cp ${HOME}/nightlyConfig/configure_clang.sh ${HOME}/Kratos/cmake_clang/configure.sh
+cp ${HOME}/Kratos/scripts/build/nightly/configure_gcc.sh ${HOME}/Kratos/cmake_gcc/configure.sh
+cp ${HOME}/Kratos/scripts/build/nightly/configure_clang.sh ${HOME}/Kratos/cmake_clang/configure.sh
 
 ## Step2: Gcc
 
+# Build
 cd ${HOME}/Kratos/cmake_gcc
 sh configure.sh > ${LOG_DIR}/configure_gcc.log 2> ${LOG_DIR}/configure_gcc.log
 make install -j2 -k > ${LOG_DIR}/compile_gcc.log 2> ${LOG_DIR}/compile_gcc.log
 
+# UnitTesting
+cd ${HOME}/Kratos/kratos/python_scripts
+python3 run_tests.py > ${LOG_DIR}/unittest_gcc.log 2> ${LOG_DIR}/unittest_gcc.log
+
+# Benchmarking
 cd ${HOME}/Kratos/benchmarking
 python3 run_all_benchmarks.py > ${LOG_DIR}/benchmarking_gcc.log 2> ${LOG_DIR}/benchmarking_gcc.log
 
@@ -38,15 +44,21 @@ echo -n "Compiling Errors    : "  >> ${MAIL_GCC};
 cat ${LOG_DIR}/compile_gcc.log | grep " error:" | wc -l >> ${MAIL_GCC};
 cat ${LOG_DIR}/compile_gcc.log | grep " error:" >> ${MAIL_GCC};
 echo -n "Compiling Warnings  : "  >> ${MAIL_GCC};
-cat ${LOG_DIR}/compile_clang.log | grep " warning:" | wc -l >> ${MAIL_GCC};
-cat ${LOG_DIR}/compile_clang.log | grep " warning:" >> ${MAIL_GCC};
+cat ${LOG_DIR}/compile_gcc.log | grep " warning:" | wc -l >> ${MAIL_GCC};
+cat ${LOG_DIR}/compile_gcc.log | grep " warning:" >> ${MAIL_GCC};
 echo -n "Linking Error       : "  >> ${MAIL_GCC};
 grep -c "cannot find" ${LOG_DIR}/compile_gcc.log >> ${MAIL_GCC};
 grep "cannot find" ${LOG_DIR}/compile_gcc.log >> ${MAIL_GCC};
 echo "\n" >> ${MAIL_GCC}
+echo "UnitTests:    \n" >> ${MAIL_GCC}
+echo "============= \n" >> ${MAIL_GCC}
+cat ${LOG_DIR}/unittest_gcc.log >> ${MAIL_GCC};
+echo "\n" >> ${MAIL_GCC}
+echo "Benchmarking: \n" >> ${MAIL_GCC}
+echo "============= \n" >> ${MAIL_GCC}
 cat ${LOG_DIR}/benchmarking_gcc.log >> ${MAIL_GCC};
 
-tar -zcvf ${LOG_DIR}/logs_gcc.tar.gz ${LOG_DIR}/configure_gcc.log ${LOG_DIR}/compile_gcc.log ${LOG_DIR}/benchmarking_gcc.log
+tar -zcvf ${LOG_DIR}/logs_gcc.tar.gz ${LOG_DIR}/configure_gcc.log ${LOG_DIR}/compile_gcc.log ${LOG_DIR}/benchmarking_gcc.log ${LOG_DIR}/unittest_gcc.log
 sendEmail -s ${KRATOS_MAIL_SERVER} -xu ${KRATOS_MAIL_USER} -xp ${KRATOS_MAIL_PASSWD} -f "Kratos@no-reply.com" -t "${MAIL_TO}" -o message-file=${MAIL_GCC} -u "Kratos Nightly Report GCC" -a ${LOG_DIR}/logs_gcc.tar.gz
 
 ## ------------------------------------------------------ ##
@@ -64,6 +76,11 @@ cd ${HOME}/Kratos/cmake_clang
 sh configure.sh > ${LOG_DIR}/configure_clang.log 2> ${LOG_DIR}/configure_clang.log
 make install -j2 -k > ${LOG_DIR}/compile_clang.log 2> ${LOG_DIR}/compile_clang.log
 
+# UnitTesting
+cd ${HOME}/Kratos/kratos/python_scripts
+python3 run_tests.py > ${LOG_DIR}/unittest_clang.log 2> ${LOG_DIR}/unittest_clang.log
+
+# Benchmarking
 cd ${HOME}/Kratos/benchmarking
 python3 run_all_benchmarks.py > ${LOG_DIR}/benchmarking_clang.log 2> ${LOG_DIR}/benchmarking_clang.log
 
@@ -83,9 +100,15 @@ echo -n "Linking Error       : "  >> ${MAIL_CLANG};
 grep -c "cannot find" ${LOG_DIR}/compile_clang.log >> ${MAIL_CLANG};
 grep "cannot find" ${LOG_DIR}/compile_clang.log >> ${MAIL_CLANG};
 echo "\n" >> ${MAIL_CLANG}
+echo "UnitTests:    \n" >> ${MAIL_CLANG}
+echo "============= \n" >> ${MAIL_CLANG}
+cat ${LOG_DIR}/unittest_clang.log >> ${MAIL_CLANG};
+echo "\n" >> ${MAIL_CLANG}
+echo "Benchmarking: \n" >> ${MAIL_CLANG}
+echo "============= \n" >> ${MAIL_CLANG}
 cat ${LOG_DIR}/benchmarking_clang.log >> ${MAIL_CLANG};
 
-tar -zcvf ${LOG_DIR}/logs_clang.tar.gz ${LOG_DIR}/configure_clang.log ${LOG_DIR}/compile_clang.log ${LOG_DIR}/benchmarking_clang.log
+tar -zcvf ${LOG_DIR}/logs_clang.tar.gz ${LOG_DIR}/configure_clang.log ${LOG_DIR}/compile_clang.log ${LOG_DIR}/benchmarking_clang.log ${LOG_DIR}/unittest_clang.log
 sendEmail -s ${KRATOS_MAIL_SERVER} -xu ${KRATOS_MAIL_USER} -xp ${KRATOS_MAIL_PASSWD} -f "Kratos@no-reply.com" -t "${MAIL_TO}" -o message-file=${MAIL_CLANG} -u "Kratos Nightly Report Clang" -a ${LOG_DIR}/logs_clang.tar.gz
 
 ## Step4: Finish
