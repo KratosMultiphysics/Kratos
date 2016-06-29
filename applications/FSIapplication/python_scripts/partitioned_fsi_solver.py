a1511f110e7f22374a4bf7b08f86c303f1b5081e
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-06-29 17:08:59 +0000

    Advances

--- applications/FSIapplication/python_scripts/partitioned_fsi_solver.py
+++ applications/FSIapplication/python_scripts/partitioned_fsi_solver.py
@@ -2,16 +2,12 @@ from __future__ import print_function, absolute_import, division  # makes Kratos
 
 # Import utilities
 #~ import connectivity_mapper                  # Auxiliary matching meshes communicator
-#~ import residual_definitions                 # Residual definitions
+import residual_definitions                 # Residual definitions
 #~ import mvqn_strategy                        # MultiVector Quasi-Newton method strategy
 #~ import jfnk_strategy                        # Jacobian Free Newton-Krylov method strategy
 #~ import relaxation_strategy                  # Relaxation strategy
 
 # Import libraries
-#~ import numpy
-#~ import scipy
-#~ import scipy.sparse
-#~ import scipy.sparse.linalg
 import time as timemodule                   # Import time library as timemodule (avoid interferences with "time" var)
 import json                                 # Encoding library (for data exchange)
 
@@ -20,6 +16,7 @@ import KratosMultiphysics
 import KratosMultiphysics.FSIApplication as KratosFSI
 import KratosMultiphysics.FluidDynamicsApplication as KratosFluid
 import KratosMultiphysics.SolidMechanicsApplication as KratosSolid
+import KratosMultiphysics.StructuralMechanicsApplication as KratosStructural
 
 # Check that KratosMultiphysics was imported in the main script
 KratosMultiphysics.CheckForPreviousImport()
@@ -136,8 +133,17 @@ class PartitionedFSISolver:
             },
         "coupling_solver_settings":
             {
-            "solver_type": "mvqn_strategy",
-            "max_nl_it": 10
+            "coupling_scheme"   : "DirichletNeumann",
+            "solver_type"       : "partitioned_fsi_solver",
+            "nl_tol"            : 1e-5,
+            "nl_max_it"         : 50,
+            "coupling_strategy" : {
+                "solver_type"       : "relaxation_strategy",
+                "acceleration_type" : "Aitken",
+                "w_0"               : 0.825
+                },
+            "structure_interfaces_list" : [""],
+            "fluid_interfaces_list" : [""]
             }
         }
         """)
@@ -146,24 +152,33 @@ class PartitionedFSISolver:
         self.settings = KratosMultiphysics.Parameters("{}")
         self.settings.AddValue("structure_solver_settings",project_parameters["structure_solver_settings"]["solver_settings"])
         self.settings.AddValue("fluid_solver_settings",project_parameters["fluid_solver_settings"]["solver_settings"])
-        #~ custom_settings.AddValue("coupling_solver_settings",project_parameters["coupling_solver_settings"]["solver_settings"])           # TO BE IMPLEMENTED           
+        self.settings.AddValue("coupling_solver_settings",project_parameters["coupling_solver_settings"]["solver_settings"])
         
         # Overwrite the default settings with user-provided parameters
         self.settings.ValidateAndAssignDefaults(default_settings)
         
+        print("*** Partitioned FSI solver construction starts...")
         # Construct the structure solver
         structure_solver_module = __import__(self.settings["structure_solver_settings"]["solver_type"].GetString())
-        self.structure_solver = structure_solver_module.CreateSolver(self.structure_main_model_part, self.settings["structure_solver_settings"])
+        self.structure_solver = structure_solver_module.CreateSolver(self.structure_main_model_part, 
+                                                                     self.settings["structure_solver_settings"])
+        print("* Structure solver constructed.")
         
         # Construct the fluid solver
         fluid_solver_module = __import__(self.settings["fluid_solver_settings"]["solver_type"].GetString())
-        self.fluid_solver = fluid_solver_module.CreateSolver(self.fluid_main_model_part, self.settings["fluid_solver_settings"])
+        self.fluid_solver = fluid_solver_module.CreateSolver(self.fluid_main_model_part, 
+                                                             self.settings["fluid_solver_settings"])
+        print("* Fluid solver constructed.")
         
-        # Construct the coupling strategy
-        self.max_nl_it = self.settings["coupling_solver_settings"]["max_nl_it"].GetInt()
-        ### TO BE DONE IN A SIMILAR FASHION AS THE ONES ABOVE
-               
-        print("Construction of PartitionedFSISolver finished.")
+        # Construct the coupling partitioned strategy
+        self.max_nl_it = self.settings["coupling_solver_settings"]["nl_max_it"].GetInt()
+        self.nl_tol = self.settings["coupling_solver_settings"]["nl_tol"].GetDouble()
+        
+        coupling_strategy_name = self.settings["coupling_solver_settings"]["coupling_strategy"]["solver_type"].GetString()
+        interface_strategy_module = __import__(coupling_strategy_name)       
+        self.coupling_strategy = interface_strategy_module.CreateStrategy(self.settings["coupling_solver_settings"]["coupling_strategy"])
+        print("* Coupling strategy constructed.")
+        print("*** FSI partitioned solver construction finished.")
         
         
     def GetMinimumBufferSize(self):
@@ -174,23 +189,48 @@ class PartitionedFSISolver:
 
 
     def AddVariables(self):
-        # Add structure variables
+        ## Structure variables addition
+        # Standard CFD variables addition
         self.structure_solver.AddVariables()
-        # Add fluid variables
+        # FSI variables addition
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.IS_INTERFACE)    # TODO: IS_INTERFACE is deprecated. Move to INTERFACE.
+        # Mapper variables addition
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_MAUX)
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.AUX)
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.VAUX)
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NORMAL)
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.SCALAR_PROJECTED)
+        self.structure_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.VECTOR_PROJECTED)
+        
+        ## Fluid variables addition
+        # Standard CFD variables addition
         self.fluid_solver.AddVariables()
-        # Add coupling variables
+        # FSI variables addition
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.FORCE)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISPLACEMENT)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.MESH_VELOCITY)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.IS_INTERFACE)    # TODO: IS_INTERFACE is deprecated. Move to INTERFACE.
+        # Mapper variables addition
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_MAUX) 
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.AUX)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.VAUX)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NORMAL)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.SCALAR_PROJECTED)
+        self.fluid_solver.main_model_part.AddNodalSolutionStepVariable(KratosFSI.VECTOR_PROJECTED)
         
                 
     def ImportModelPart(self):
         # Import structure model part
         self.structure_solver.ImportModelPart()
+        
         # Import fluid model part
         self.fluid_solver.ImportModelPart()
         
         
     def AddDofs(self):
         # Add DOFs structure
-        self.structure_solver.AddDofs()        
+        self.structure_solver.AddDofs()     
+           
         # Add DOFs fluid
         self.fluid_solver.AddDofs()
         
@@ -198,11 +238,44 @@ class PartitionedFSISolver:
     def Initialize(self):
         # Initialize structure solver
         self.structure_solver.Initialize()
+        
         # Initialize fluid solver
         self.fluid_solver.Initialize()
-        # Initialize coupling solver
+
+        # Get interface problem sizes
+        interface_problem_sizes = self._GetInterfaceProblemSizes()
+        self.solid_interface_problem_size = interface_problem_sizes[0]
+        self.fluid_interface_problem_size = interface_problem_sizes[1]
         
+        # Get the domain size
+        self.domain_size = self._GetDomainSize()
+                
+        # Construct the interface mapper
+        ### NOTE: The old mapper in FSI app is used --> Move to mortar mapper asap.
+        ### NOTE2: The flag "INTERFACE" ("IS_INTERFACE" if the old mapper is used) has to be set to the interface nodes before the mapper construction.
+        import NonConformant_OneSideMap
+        self.interface_mapper = NonConformant_OneSideMap.NonConformant_OneSideMap(self.fluid_solver.main_model_part,
+                                                                                  self.structure_solver.main_model_part, 
+                                                                                  1.0, 50, 1e-5)
+        print("Non-conformant one side map constructed.")
+                                                                                  
+                                                                             
+        # Construct the interface residual 
+        self.interface_residual = residual_definitions.CreateResidual(self.settings["coupling_solver_settings"]["coupling_scheme"].GetString(),
+                                                                      self.fluid_solver,  
+                                                                      self.structure_solver, 
+                                                                      self.interface_mapper) 
+        print("Interface residual constructed.")
         
+        # Interface solution initial guess (it might be velocity or fluxes depending on the type of coupling)
+        # Note that the FSI problem is defined in terms of the fluid interface
+        fluid_interface_residual_size = self.fluid_interface_problem_size*self.domain_size
+        self.iteration_guess_value = KratosMultiphysics.Vector(fluid_interface_residual_size) 
+
+        for i in range(0,fluid_interface_residual_size):
+            self.iteration_guess_value[i] = 0.0001
+                                                    
+                
     def GetComputeModelPart(self):
         ##### THIS FUNCTION IS PROBABLY NOT NECESSARY IN THE COMBINED SOLVER
         pass
@@ -222,7 +295,62 @@ class PartitionedFSISolver:
         
     def Solve(self):
         
-        for nl_it in range(1,self.max_nl_it+1)
+        ##### DEVELOPMENT FLAGS --> TO BE REMOVED AS SOON AS THEY ARE IMPLEMENTED IN THE JSON FILE.
+        mesh_prediction = False
+        fluid_prediction = False
+        move_interface =  False
+        
+        # Compute mesh prediction # --> IMPLEMENT CORRECTLY THE MESH PREDICTION
+        #~ if mesh_prediction == True:
+            #~ print("Computing time step ",step," prediction...")
+            #~ # Get the previous step fluid interface nodal fluxes
+            #~ fluid_flux_prev_step = residual_definitions.GetInterfaceNodalResult(self.fluid_solver,"REACTION",1)
+            #~ fluid_flux_prev_step_comm = wet_interface_comm.FluidToStructure_VectorMap(fluid_flux_prev_step,True)
+                
+            #~ # Solve the current step structure problem with the previous step fluid interface nodal fluxes
+            #~ D2_problem.SolveNeumann(fluid_flux_prev_step_comm)
+            #~ solid_interface_disp = residual_definitions.GetInterfaceNodalResult(D2_problem,"DISPLACEMENT",0)
+            #~ solid_interface_disp_comm = wet_interface_comm.StructureToFluid_VectorMap(solid_interface_disp,False)
+                
+            #~ # Solve the fluid mesh problem with the obtained solid interface displacements
+            #~ D1_problem.SolveMesh(solid_interface_disp_comm)
+        
+            #~ if fluid_prediction == True:
+                #~ # Get the obtained solid interface velocity
+                #~ solid_interface_velo = residual_definitions.GetInterfaceNodalResult(D2_problem,"VELOCITY",1)
+                #~ solid_interface_velo_comm = wet_interface_comm.StructureToFluid_VectorMap(solid_interface_velo,False)
+            
+                #~ # Solve the fluid domain with the obtained solid interface velocity and the updated mesh
+                #~ D1_problem.SolveDirichlet(solid_interface_velo_comm)
+            
+            #~ print("Time step ",step," prediction computed.")
+        
+        
+        for nl_it in range(1,self.max_nl_it+1):
+            
+            print("    NL-ITERATION ",nl_it,"STARTS.")
+            
+            print("     Residual computation starts...")
+            vel_residual = self.interface_residual.ComputeResidual(self.iteration_guess_value)            
+            nl_res_norm = space.TwoNorm(vel_residual)
+            print("     Residual computation finished. |res|=",nl_res_norm)      
+            
+            # Move interface nodes
+            if move_interface == True:
+                
+                    solid_interface_disp = self.interface_residual.GetInterfaceNodalResult(self.structure_solver,"DISPLACEMENT",0)
+                    #~ solid_interface_disp = D2_problem.GetInterfaceDisplacement()
+                    solid_interface_disp_comm = wet_interface_comm.StructureToFluid_VectorMap(solid_interface_disp)
+                    D1_problem.MoveInterface(solid_interface_disp_comm)
+                    
+            # Check convergence
+            
+            
+                    
+            
+        
+        
+        ### ESTO DEBE HACERSE DENTRO DEL RESIDUAL
         # Initialize structure solver
         self.structure_solver.Solve()
         # Initialize fluid solver
@@ -248,3 +376,32 @@ class PartitionedFSISolver:
         pass
         # How to do a check of the interface solver?¿?¿ Probably it is not necessary...
         #~ self.mechanical_solver.Check()
+            
+    def _GetInterfaceProblemSizes(self):
+        
+        # Get solid interface problem size
+        structure_interface_pb_size = 0
+        for i in range(self.settings["coupling_solver_settings"]["structure_interfaces_list"].size()):
+            interface_submodelpart_name = self.settings["coupling_solver_settings"]["structure_interfaces_list"][i].GetString()
+            structure_interface_pb_size += len(self.structure_solver.main_model_part.GetSubModelPart(interface_submodelpart_name).Nodes)
+        
+        # Get fluid interface problem size
+        fluid_interface_pb_size = 0
+        for i in range(self.settings["coupling_solver_settings"]["fluid_interfaces_list"].size()):
+            interface_submodelpart_name = self.settings["coupling_solver_settings"]["fluid_interfaces_list"][i].GetString()
+            fluid_interface_pb_size += len(self.fluid_solver.main_model_part.GetSubModelPart(interface_submodelpart_name).Nodes)
+            
+        return (structure_interface_pb_size, fluid_interface_pb_size)
+        
+    def _GetDomainSize(self):
+        
+        fluid_domain_size = self.fluid_solver.main_model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE]
+        structure_domain_size = self.structure_solver.main_model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE]
+        
+        if fluid_domain_size !=structure_domain_size:
+            raise("ERROR: Solid domain size and fluid domain size are not equal!")
+        
+        return fluid_domain_size
+            
+            
+        
