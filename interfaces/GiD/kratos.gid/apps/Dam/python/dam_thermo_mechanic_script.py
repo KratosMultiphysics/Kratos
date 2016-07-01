9b2d7915826970e772bd3acb434cb61efbba2917
lgracia <lgracia@cimne.upc.edu>
2016-07-01 15:26:03 +0000

    Dam interface updates

--- interfaces/GiD/kratos.gid/apps/Dam/python/dam_thermo_mechanic_script.py
+++ interfaces/GiD/kratos.gid/apps/Dam/python/dam_thermo_mechanic_script.py
@@ -37,18 +37,22 @@ solver_module = __import__("dam_new_mechanical_solver")  # Up to now is the uniq
 solver = solver_module.CreateSolver(main_model_part, ProjectParameters)
 
 
-#thermal_solver_module = __import__("eulerian_convection_diffusion_solver")
-#thermal_solver = solver_module.CreateSolver(main_model_part, ProjectParameters)
+thermal_solver_module = __import__("new_eulerian_convection_diffusion_solver")
+thermal_solver = solver_module.CreateSolver(main_model_part, ProjectParameters)
 
 
 # Add variables
 solver.AddVariables()
+thermal_solver.AddVariables()
+
 
 # Import the model part
 solver.ImportModelPart()
 
+
 # Add degrees of freedom
 solver.AddDofs()
+thermal_solver.AddDofs()
 
 ########################## IMPORTING NECESSARY MODULES ########################## 
 
@@ -57,7 +61,7 @@ from gid_output_process import GiDOutputProcess
 import cleaning_utility
 import process_factory
 
-########################## PREVIOUS DEFINITIONS ########################## 
+############################### PREVIOUS DEFINITIONS ############################# 
 
 # Number of threads
 parallel=OpenMPUtils()
@@ -79,6 +83,7 @@ tol = delta_time*1.0e-10
 echo_level = ProjectParameters["mechanical_settings"]["echo_level"].GetInt()
 buffer_size = ProjectParameters["mechanical_settings"]["buffer_size"].GetInt()
 
+
 # Time Units Converter
 if(time_converter=="Months"):               # Factor to pass from months to seconds
     time_unit_converter = 2592000.0
@@ -89,23 +94,39 @@ elif(time_converter=="Hours"):              # Factor to pass from hours to secon
 else:                                       # No changes
     time_unit_converter = 1.0               
 
-#Type of reference temperature
+
+#Thermal Parameters
 if (type_of_problem == "Thermo-Mechanical"):
+    
+    #Type of reference temperature
     reference_temperature = ProjectParameters["diffusion_settings"]["reference_temperature"].GetString()
+    
     if (reference_temperature =="Reservoir Information"):
         main_model_part.ProcessInfo[REFERENCE_TEMPERATURE] = 10.0  #TODO: for working
-#model_part.ProcessInfo[REFERENCE_TEMPERATURE] = model_part.GetTable(18).GetNearestValue(0.0)      To start computations at time = 0  / Table 5 = Reference Temperature Values
+        #model_part.ProcessInfo[REFERENCE_TEMPERATURE] = model_part.GetTable(18).GetNearestValue(0.0)      To start computations at time = 0  / Table 5 = Reference Temperature Values
     else:
         main_model_part.ProcessInfo[REFERENCE_TEMPERATURE] = 10.0 # TODO: Here we have to solve the thermal problem until arrives a stationary    
-
+    
+    #Variable defining the temporal scheme (0: Forward Euler, 1: Backward Euler, 0.5: Crank-Nicolson)
+    thermal_scheme = ProjectParameters["diffusion_settings"]["temporal_scheme"]
+    if(thermal_scheme=="Backward-Euler"):
+        main_model_part.ProcessInfo[THETA] = 1.0
+    elif(thermal_scheme=="Crank-Nicolson"):   
+        main_model_part.ProcessInfo[THETA] = 0.5
+    else:
+        main_model_part.ProcessInfo[THETA] = 0.0
+    
+    
 # Update time variables
 delta_time = delta_time * time_unit_converter
 ending_time = ending_time * time_unit_converter
+
 
 # Build sub_model_parts
 for i in range(ProjectParameters["processes_sub_model_part_list"].size()):
     part_name = ProjectParameters["processes_sub_model_part_list"][i].GetString()
     Model.update({part_name : main_model_part.GetSubModelPart(part_name)})
+
 
 # Print control
 if(echo_level > 1):
@@ -120,90 +141,49 @@ if(echo_level > 1):
 list_of_processes = process_factory.KratosProcessFactory(Model).ConstructListOfProcesses( ProjectParameters["nodal_processes_sub_model_part_list"] )
 list_of_processes += process_factory.KratosProcessFactory(Model).ConstructListOfProcesses( ProjectParameters["load_processes_sub_model_part_list"] )
 
+
 # Print list of constructed processes
 if(echo_level>1):
     for process in list_of_processes:
         print(process)
 
+
 # Initialize processes
 for process in list_of_processes:
     process.ExecuteInitialize()
+
 
 # Set ProcessInfo variables and fill the previous steps of the buffer with the initial conditions
 current_time = -(buffer_size-1)*delta_time
 main_model_part.ProcessInfo[TIME] = current_time #current_time and TIME = 0 after filling the buffer
 
+
 for step in range(buffer_size-1):
     current_time = current_time + delta_time
     main_model_part.CloneTimeStep(current_time)
+
 
 # Initialize GiD I/O
 computing_model_part = solver.GetComputeModelPart()
 gid_output = GiDOutputProcess(computing_model_part,problem_name,ProjectParameters["output_configuration"])
 gid_output.ExecuteInitialize()
 
+
 #Initializa the mechanical solver
 solver.Initialize()
+thermal_solver.Initialize()
+
 
 #Execute before solution
 for process in list_of_processes:
     process.ExecuteBeforeSolutionLoop()
 
+
 # Set the results 
 gid_output.ExecuteBeforeSolutionLoop()
 
 
-## Model part ------------------------------------------------------------------------------------------------
-
-# Definition of model part
-#model_part = ModelPart("SolidDomain")
-
-
-# Setting thermal variables
-#diffusion_solver.AddVariables(model_part,ProjectParameters["diffusion_settings"]) 
-
-# Set mechanical variables
-# Construct the solver (main setting methods are located in the solver_module)
-
-#mechanical_solver.CreateSolver(main_model_part, ProjectParameters["mechanical_settings"])
-
-# Reading model part
-#model_part_io = ModelPartIO(problem_name)
-#model_part_io.ReadModelPart(main_model_part)
-
-# Set buffer size
-#buffer_size = 2
-#main_model_part.SetBufferSize(buffer_size)
-
-# Set thermal degrees of freedom
-# TODO: fix the problems with convection-diffusion solver
-#diffusion_solver.AddDofs(model_part)  
-
-# Set mechanical degrees of freedom
-#mechanical_solver.AddDofs()
-
-## Initialize ------------------------------------------------------------------------------------------------
-
-
-################ OBTAIN THE LIST OF PROCESSES ##################
-
-# Define and initialize the diffusion solver
-# TODO: Problems with the solver, domain_size is not found. 
-#thermal_diffusion_solver = diffusion_solver.CreateSolver(model_part, ProjectParameters)
-
-#Variable defining the temporal scheme (0: Forward Euler, 1: Backward Euler, 0.5: Crank-Nicolson)
-#thermal_scheme = ProjectParameters["diffusion_settings"]["temporal_scheme"]
-#if(thermal_scheme=="Backward-Euler"):
-    #model_part.ProcessInfo[THETA] = 1.0
-#elif(thermal_scheme=="Crank-Nicolson"):   
-    #model_part.ProcessInfo[THETA] = 0.5
-#else:
-    #model_part.ProcessInfo[THETA] = 0.0
-    
-#thermal_diffusion_solver.Initialize()
-#thermal_diffusion_solver.SetEchoLevel(0)
-
-## Temporal loop ---------------------------------------------------------------------------------------------
+################################# TEMPORAL LOOP ####3########################## 
 
 while( (current_time+tol) < ending_time ):
 
@@ -218,12 +198,14 @@ while( (current_time+tol) < ending_time ):
     #conditions_util.UpdateImposedConditions(model_part,current_step)
 
     # Solve thermal step
-    #clock_time = clock()
-    #thermal_diffusion_solver.Solve()
-    #print("Thermal Solving Time = ","%.5f" % (time.clock() - clock_time)," seconds")
+    clock_time = clock()
+    thermal_solver.Solve()
+    print("Thermal Solving Time = ","%.5f" % (clock() - clock_time)," seconds")
+    
     
     for process in list_of_processes:
         process.ExecuteInitializeSolutionStep()
+
 
     gid_output.ExecuteInitializeSolutionStep()
     
@@ -233,32 +215,40 @@ while( (current_time+tol) < ending_time ):
     solver.Solve()
     print("Mechanical Solving Time = ","%.5f" % (clock() - clock_time)," seconds")
 
+
     gid_output.ExecuteFinalizeSolutionStep()
+    
     
     for process in list_of_processes:
         process.ExecuteFinalizeSolutionStep()
     
+    
     for process in list_of_processes:
         process.ExecuteBeforeOutputStep()
+    
     
     # Write GiD results
     if gid_output.IsOutputStep():
         gid_output.PrintOutput()
     
+    
     for process in list_of_processes:
         process.ExecuteAfterOutputStep()
 
 
-## Finalize --------------------------------------------------------------------------------------------------
+############################### FINALIZE ############################### 
 
 # Finalizing output files
 gid_output.ExecuteFinalize()
 
+
 for process in list_of_processes:
     process.ExecuteFinalize()
     
+    
 # Finalizing strategy
 solver.Clear()
+
 
 # Time control
 print (ctime())
