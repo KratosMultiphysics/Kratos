7f55eefb10627f7d1b9796c90d77c1169b935e8d
Pooyan Dadvand <pooyan@cimne.upc.edu>
2016-07-04 17:11:12 +0000

    Adding the testing.h

---
+++ kratos/testing/testing.h
@@ -0,0 +1,20 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Pooyan Dadvand
+//                   
+//
+
+#if !defined(KRATOS_TESTING_H_INCLUDED )
+#define  KRATOS_TESTING_H_INCLUDED
+
+#include "testing/test_suite.h" // This includes the test_case.h which includes tester.h
+#include "includes/checks.h"  // It is almost always necessary. includes the exception
+
+#endif // KRATOS_TEST_SUITE_H_INCLUDED  defined
