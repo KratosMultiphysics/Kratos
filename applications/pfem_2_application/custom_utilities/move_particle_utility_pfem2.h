3c4dc6c638b6b77a6482028cc8777b434ba85255
pbecker <pablobecker@gmail.com>
2016-07-04 14:22:03 +0000

    pfem2 cleanup

--- applications/pfem_2_application/custom_utilities/move_particle_utility_pfem2.h
+++ applications/pfem_2_application/custom_utilities/move_particle_utility_pfem2.h
@@ -275,11 +275,10 @@ namespace Kratos
 			//now we seed: LOOP IN ELEMENTS
 			//using loop index, DO NOT paralelize this! change lines : mparticles_in_elems_pointers((ii*mmaximum_number_of_particles)+mparticles_in_elems_integers(ii)) = pparticle; and the next one
 			
-			PFEM_Particle_Fluid& firstparticle =mparticles_vector[0];
 			for(unsigned int ii=0; ii<mr_model_part.Elements().size(); ii++)
 			{
 				ModelPart::ElementsContainerType::iterator ielem = ielembegin+ii;
-				(ielem->GetValue(FLUID_PARTICLE_POINTERS)) = ParticlePointerVector( mmaximum_number_of_particles*2, &firstparticle );
+				(ielem->GetValue(FLUID_PARTICLE_POINTERS)) = ParticlePointerVector( mmaximum_number_of_particles*2);//, &firstparticle );
 				ParticlePointerVector&  particle_pointers =  (ielem->GetValue(FLUID_PARTICLE_POINTERS));
 				//now we link the mpointers_to_particle_pointers_vectors to the corresponding element
 				mpointers_to_particle_pointers_vectors(ii) = &particle_pointers;
