20d898360b14a4bb0bf8cfafbf4e7f862ccf3954
pbecker <pablobecker@gmail.com>
2016-07-05 14:29:32 +0000

    added rotation of velocity to particles in pfem2

--- applications/pfem_2_application/custom_python/add_custom_utilities_to_python.cpp
+++ applications/pfem_2_application/custom_python/add_custom_utilities_to_python.cpp
@@ -99,6 +99,7 @@ namespace Python
                     .def("PreReseedUsingTopographicDomain", &MoveParticleUtilityPFEM2<2>::PreReseedUsingTopographicDomain)
                     .def("ExecuteParticlesPritingTool", &MoveParticleUtilityPFEM2<2>::ExecuteParticlesPritingTool)
                     .def("ExecuteParticlesPritingToolForDroppletsOnly", &MoveParticleUtilityPFEM2<2>::ExecuteParticlesPritingToolForDroppletsOnly)
+                    .def("RotateParticlesVelocities", &MoveParticleUtilityPFEM2<2>::RotateParticlesVelocities)
                     ;    
 
         class_< MoveParticleUtilityPFEM2<3> > ("MoveParticleUtilityPFEM23D", init<ModelPart& , int >())
@@ -118,6 +119,7 @@ namespace Python
                     .def("ExecuteParticlesPritingTool", &MoveParticleUtilityPFEM2<3>::ExecuteParticlesPritingTool)
                     .def("ExecuteParticlesPritingToolForDroppletsOnly", &MoveParticleUtilityPFEM2<3>::ExecuteParticlesPritingToolForDroppletsOnly)
                     .def("AssignNodalVelocityUsingInletConditions", &MoveParticleUtilityPFEM2<3>::AssignNodalVelocityUsingInletConditions)
+                    .def("RotateParticlesVelocities", &MoveParticleUtilityPFEM2<3>::RotateParticlesVelocities)
                     ;    
                     
       	class_<AddFixedVelocityCondition2D > ("AddFixedVelocityCondition2D", init<ModelPart& >())
