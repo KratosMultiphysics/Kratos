7f23a2dbaaaff46efe9469cd694f2b84152dff58
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-06 15:51:38 +0000

    ScanSum method added to the Communicator. Elemental Getvalue allows heterogenous sizes. To be tested and extended to the GetValue of nodes or other entities...

--- kratos/kratos/includes/mpi_communicator.h
+++ kratos/kratos/includes/mpi_communicator.h
@@ -302,6 +302,18 @@ public:
         MPI_Allreduce(&local_value, &rValue, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
         return true;
     }
+    
+    virtual bool ScanSum(const double& send_partial, double& receive_accumulated)
+    {
+        MPI_Scan(&send_partial, &receive_accumulated, 1, MPI_DOUBLE, MPI_SUM , MPI_COMM_WORLD);
+        return true;
+    }
+    
+    virtual bool ScanSum(const int& send_partial, int& receive_accumulated)
+    {
+        MPI_Scan(&send_partial, &receive_accumulated, 1, MPI_INT, MPI_SUM , MPI_COMM_WORLD);
+        return true;
+    }
 
     virtual bool SynchronizeElementalIds()
     {
@@ -643,10 +655,16 @@ public:
         SynchronizeElementalNonHistoricalVariable<vector<array_1d<double,3> >,double>(ThisVariable);
         return true;
     }
+    
+    virtual bool SynchronizeElementalNonHistoricalVariable(Variable<vector<int> > const& ThisVariable)
+    {
+        SynchronizeHeterogeneousElementalNonHistoricalVariable<vector<int>,int>(ThisVariable);
+        return true;
+    }
 
     virtual bool SynchronizeElementalNonHistoricalVariable(Variable<Vector> const& ThisVariable)
     {
-        SynchronizeElementalNonHistoricalVariable<Vector,double>(ThisVariable);
+        SynchronizeHeterogeneousElementalNonHistoricalVariable<Vector,double>(ThisVariable);
         return true;
     }
 
@@ -1346,8 +1364,8 @@ private:
                     continue; // nothing to transfer!
 
                 unsigned int position = 0;
-                double* send_buffer = new double[send_buffer_size];
-                double* receive_buffer = new double[receive_buffer_size];
+                TSendType* send_buffer = new TSendType[send_buffer_size];
+                TSendType* receive_buffer = new TSendType[receive_buffer_size];
 
                 // Filling the send buffer
                 for (ModelPart::ElementIterator i_element = r_local_elements.begin(); i_element != r_local_elements.end(); ++i_element)
@@ -1367,7 +1385,8 @@ private:
                 position = 0;
                 for (ModelPart::ElementIterator i_element = r_ghost_elements.begin(); i_element != r_ghost_elements.end(); ++i_element)
                 {
-                    i_element->GetValue(ThisVariable) = *reinterpret_cast<TDataType*> (receive_buffer + position);
+                    //i_element->GetValue(ThisVariable) = *reinterpret_cast<TDataType*> (receive_buffer + position);
+                    i_element->SetValue(ThisVariable, *reinterpret_cast<TDataType*> (receive_buffer + position) );
                     position += elemental_data_size;
                 }
 
@@ -1380,6 +1399,136 @@ private:
 
         return true;
     }
+    
+    
+    template< class TDataType, class TSendType >
+    bool SynchronizeHeterogeneousElementalNonHistoricalVariable(Variable<TDataType> const& ThisVariable)
+    {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        int destination = 0;
+
+        NeighbourIndicesContainerType& neighbours_indices = NeighbourIndices();
+        
+        for (unsigned int i_color = 0; i_color < neighbours_indices.size(); i_color++) {
+            if ((destination = neighbours_indices[i_color]) >= 0) {
+                                          
+                ElementsContainerType& r_local_elements = LocalMesh(i_color).Elements();
+                ElementsContainerType& r_ghost_elements = GhostMesh(i_color).Elements();
+                unsigned int local_elements_size = r_local_elements.size();
+                unsigned int ghost_elements_size = r_ghost_elements.size();
+                                
+                if ((local_elements_size == 0) && (ghost_elements_size == 0))
+                    continue; // nothing to transfer!
+                
+                ////////////STEP 1 : SEND SIZES OF VARIABLE OF ALL ELEMENTS    
+                
+                unsigned int send_buffer_size = local_elements_size;
+                unsigned int receive_buffer_size = ghost_elements_size;
+                                
+                int* send_buffer_1 = new int[send_buffer_size];
+                int* receive_buffer_1 = new int[receive_buffer_size];
+                
+                int dummy_int_variable = 0;
+                MPI_Datatype ThisMPI_Datatype = GetMPIDatatype(dummy_int_variable);
+                
+                unsigned int size_of_variable = 0;
+
+                // Filling the send buffer
+                std::vector<int> sent_vector_of_sizes(send_buffer_size, 0);
+                unsigned int position = 0;
+                for (ModelPart::ElementIterator i_element = r_local_elements.begin(); i_element != r_local_elements.end(); ++i_element) {
+                    TDataType& variable = i_element->GetValue(ThisVariable);
+                    size_of_variable = variable.size();
+                    sent_vector_of_sizes[position] = size_of_variable;
+                    send_buffer_1[position] = size_of_variable;
+                    position ++;
+                }
+
+                MPI_Status status1;
+
+                int send_tag = i_color;
+                int receive_tag = i_color;
+
+                MPI_Sendrecv(send_buffer_1, send_buffer_size, ThisMPI_Datatype, destination, send_tag, receive_buffer_1, receive_buffer_size, ThisMPI_Datatype, destination, receive_tag,
+                             MPI_COMM_WORLD, &status1);
+
+                position = 0;
+                std::vector<int> received_vector_of_sizes(receive_buffer_size, 0);
+                
+                for (ModelPart::ElementIterator i_element = r_ghost_elements.begin(); i_element != r_ghost_elements.end(); ++i_element) {
+                    received_vector_of_sizes[position] = receive_buffer_1[position];
+                    position ++;
+                }
+
+                if (position > receive_buffer_size)
+                    std::cout << rank << " First step Error in estimating receive buffer size...." << std::endl;
+
+                delete [] send_buffer_1;
+                delete [] receive_buffer_1;
+                               
+                ////////////STEP 2 : SEND ACTUAL VECTORS                
+                int sent_total_size = 0;
+                for (int i=0; i< (int) sent_vector_of_sizes.size(); i++) {
+                    sent_total_size += sent_vector_of_sizes[i];
+                }
+                
+                unsigned int received_total_size = 0;
+                for (int i=0; i< (int) received_vector_of_sizes.size(); i++) {
+                    received_total_size += received_vector_of_sizes[i];
+                }
+
+                TSendType* send_buffer_2 = new TSendType[sent_total_size];
+                TSendType* receive_buffer_2 = new TSendType[received_total_size];
+
+                // Filling the send buffer
+                int i=0;
+                position = 0;
+                for (ModelPart::ElementIterator i_element = r_local_elements.begin(); i_element != r_local_elements.end(); ++i_element) {
+
+                    int size_of_this_one = sent_vector_of_sizes[i];
+                    TDataType& variable_to_add = i_element->GetValue(ThisVariable);
+                    
+                    for (int j=0; j<size_of_this_one; j++){
+                        send_buffer_2[position + j] = variable_to_add[j];
+                    }
+                    
+                    position += sent_vector_of_sizes[i];
+                    i++;
+                }
+                
+                TSendType Value = TSendType();
+                ThisMPI_Datatype = GetMPIDatatype(Value);
+
+                MPI_Status status2;
+                send_tag = i_color;
+                receive_tag = i_color;
+                MPI_Sendrecv(send_buffer_2, sent_total_size, ThisMPI_Datatype, destination, send_tag, receive_buffer_2, received_total_size, ThisMPI_Datatype, destination, receive_tag,
+                             MPI_COMM_WORLD, &status2);
+
+                position = 0, i=0;
+                for (ModelPart::ElementIterator i_element = r_ghost_elements.begin(); i_element != r_ghost_elements.end(); ++i_element) {                                      
+                    int size_of_this_one = received_vector_of_sizes[i];
+                    TDataType& variable_to_fill = i_element->GetValue(ThisVariable);  
+                    variable_to_fill.resize(size_of_this_one);
+                    for (int j=0; j<size_of_this_one; j++){
+                        variable_to_fill[j] =  receive_buffer_2[position + j];
+                    }
+                    position += size_of_this_one;
+                    i++;
+                }
+
+                if (position > received_total_size)
+                    std::cout << rank << " Second Step Error in estimating receive buffer size...." << std::endl;
+
+                delete [] send_buffer_2;
+                delete [] receive_buffer_2;
+            }
+        }
+        return true;
+    }
+    
 
     template<class TObjectType>
     bool AsyncSendAndReceiveObjects(std::vector<TObjectType>& SendObjects, std::vector<TObjectType>& RecvObjects, Kratos::Serializer& externParticleSerializer)
