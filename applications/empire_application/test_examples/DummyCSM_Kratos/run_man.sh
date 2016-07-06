28179dc955f9c6154fbb3ae274f63ca790c11864
AMini <a.winterstein@tum.de>
2016-07-06 05:53:32 +0000

    Adding wrappers, test examples and mapper to empire_application

--- applications/empire_application/test_examples/DummyCSM_Kratos/run_man.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-xterm  -hold -e mpirun -np 1 Emperor emperorInput.xml & EPID=$!
-sleep 0.4s
-xterm  -hold -e mpirun -np 1 dummyCSM CSM/dummyCSMInput.xml &
-sleep 0.4s 
-xterm  -hold -e mpirun -np 1 python CFD/KratosOpenMP.py  &
-wait $EPID
