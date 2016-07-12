b5faacc0c27c93413f26c654322431d5ab52ad73
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-12 07:34:36 +0000

    Added logger interface

---
+++ kratos/kratos/python/add_logger_to_python.cpp
@@ -0,0 +1,71 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Carlos A. Roig
+//
+//
+
+// External includes
+#include <boost/python.hpp>
+
+// Project includes
+#include "input_output/logger.h"
+using namespace boost::python;
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+/**
+ * Prints the arguments from the python script using the Kratos Logger class
+ * @args: tuple boost::python::object representing the arguments of the function
+ * @kwargs: dictionary of boost::python::objects resenting key-value pairs for name arguments
+ **/
+object print(tuple args, dict kwargs) {
+  std::stringstream buffer;
+  LoggerOutput output(buffer);
+
+  LoggerMessage message;
+
+  list keys = kwargs.keys();
+
+  // Extract the tuple part
+  for(int i = 0; i < len(args); ++i) {
+    object curArg = args[i];
+    if(curArg) {
+      message << extract<const char *>(boost::python::str(args[i])) << std::endl;
+    }
+  }
+
+  // Extract the dict part
+  for(int i = 0; i < len(keys); ++i) {
+    object curArg = kwargs[keys[i]];
+    if(curArg) {
+      message << extract<const char *>(keys[i]) << ": " << extract<const char *>(boost::python::str(kwargs[keys[i]])) << std::endl;
+    }
+  }
+
+  output.WriteMessage(message);
+
+  return object();
+}
+
+void  AddLoggerToPython() {
+	using namespace boost::python;
+
+  class_<Logger, boost::shared_ptr<Logger>, boost::noncopyable>("Logger", no_init)
+  .def("Print", raw_function(print,1))
+  .staticmethod("Print");
+}
+
+}  // namespace Python.
+
+} // Namespace Kratos
---
+++ kratos/kratos/python/add_logger_to_python.h
@@ -0,0 +1,34 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Carlos A. Roig
+//
+//
+
+#if !defined(KRATOS_ADD_LOGGER_TO_PYTHON_H_INCLUDED )
+#define  KRATOS_ADD_LOGGER_TO_PYTHON_H_INCLUDED
+
+
+// Project includes
+#include "includes/define.h"
+
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+void  AddLoggerToPython();
+
+}  // namespace Python.
+
+}  // namespace Kratos.
+
+#endif // KRATOS_ADD_LOGGER_TO_PYTHON_H_INCLUDED  defined
