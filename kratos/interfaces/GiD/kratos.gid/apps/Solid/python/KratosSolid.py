94c2c56314d9cfb45b82deda847525d5750fb175
vmataix <vmataix@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-14 13:46:36 +0000

    Correcting solver. Clear was not correctly defined

--- kratos/interfaces/GiD/kratos.gid/apps/Solid/python/KratosSolid.py
+++ kratos/interfaces/GiD/kratos.gid/apps/Solid/python/KratosSolid.py
@@ -2,22 +2,21 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 
 #### TIME MONITORING START ####
 
-# time control starts
+# Time control starts
 import time as timer
 print(timer.ctime())
-# measure process time
+# Measure process time
 t0p = timer.clock()
-# measure wall time
+# Measure wall time
 t0w = timer.time()
 
-#
 def StartTimeMeasuring():
-    # measure process time
+    # Measure process time
     time_ip = timer.clock()
     return time_ip
 
 def StopTimeMeasuring(time_ip, process, report):
-    # measure process time
+    # Measure process time
     time_fp = timer.clock()
     if( report ):
         used_time = time_fp - time_ip
@@ -25,11 +24,10 @@ def StopTimeMeasuring(time_ip, process, report):
 
 #### TIME MONITORING END ####
 
-#import kratos core and applications
+# Import kratos core and applications
 from KratosMultiphysics import *
 from KratosMultiphysics.SolidMechanicsApplication import *
 from KratosMultiphysics.ExternalSolversApplication import *
-
 
 ######################################################################################
 ######################################################################################
@@ -37,40 +35,38 @@ from KratosMultiphysics.ExternalSolversApplication import *
 
 #### PARSING THE PARAMETERS ####
 
-#import define_output
+# Import define_output
 parameter_file = open("ProjectParameters.json",'r')
 ProjectParameters = Parameters( parameter_file.read())
 
-#set echo level
+# Set echo level
 echo_level = ProjectParameters["problem_data"]["echo_level"].GetInt()
 
-#### model_part settings start ####
+#### Model_part settings start ####
 
-#defining the model_part
+# Defining the model_part
 main_model_part = ModelPart(ProjectParameters["problem_data"]["model_part_name"].GetString())
 main_model_part.ProcessInfo.SetValue(DOMAIN_SIZE, ProjectParameters["problem_data"]["domain_size"].GetInt())
 
-###TODO replace this "model" for real one once available in kratos core
 Model = {ProjectParameters["problem_data"]["model_part_name"].GetString() : main_model_part}
 
 #construct the solver (main setting methods are located in the solver_module)
 solver_module = __import__(ProjectParameters["solver_settings"]["solver_type"].GetString())
 solver = solver_module.CreateSolver(main_model_part, ProjectParameters["solver_settings"])
 
-#add variables (always before importing the model part) (it must be integrated in the ImportModelPart)
-# if we integrate it in the model part we cannot use combined solvers
+# Add variables (always before importing the model part) (it must be integrated in the ImportModelPart)
+# If we integrate it in the model part we cannot use combined solvers
 solver.AddVariables()
 
-#read model_part (note: the buffer_size is set here) (restart can be read here)
+# Read model_part (note: the buffer_size is set here) (restart can be read here)
 solver.ImportModelPart()
 
-#add dofs (always after importing the model part) (it must be integrated in the ImportModelPart)
-# if we integrate it in the model part we cannot use combined solvers
+# Add dofs (always after importing the model part) (it must be integrated in the ImportModelPart)
+# If we integrate it in the model part we cannot use combined solvers
 solver.AddDofs()
 
-#build sub_model_parts or submeshes (rearrange parts for the application of custom processes)
-##TODO: replace MODEL for the Kratos one ASAP
-##get the list of the submodel part in the object Model
+# Build sub_model_parts or submeshes (rearrange parts for the application of custom processes)
+## Get the list of the submodel part in the object Model
 for i in range(ProjectParameters["solver_settings"]["processes_sub_model_part_list"].size()):
     part_name = ProjectParameters["solver_settings"]["processes_sub_model_part_list"][i].GetString()
     Model.update({part_name: main_model_part.GetSubModelPart(part_name)})
@@ -82,10 +78,9 @@ if(echo_level>1):
     for properties in main_model_part.Properties:
         print(properties)
 
-#### model_part settings end ####
+#### Model_part settings end ####
 
-
-#### processes settings start ####
+#### Processes settings start ####
 
 #obtain the list of the processes to be applied
 
@@ -93,6 +88,8 @@ import process_factory
 list_of_processes = process_factory.KratosProcessFactory(Model).ConstructListOfProcesses( ProjectParameters["constraints_process_list"] )
 
 list_of_processes += process_factory.KratosProcessFactory(Model).ConstructListOfProcesses( ProjectParameters["loads_process_list"] )
+
+#list_of_processes += process_factory.KratosProcessFactory(Model).ConstructListOfProcesses(ProjectParameters["list_other_processes"])
 
 #list_of_processes = []
 #process_definition = ProjectParameters["boundary_conditions_process_list"]
@@ -109,7 +106,6 @@ if(echo_level>1):
     for process in list_of_processes:
         print(process)
 
-#TODO: decide which is the correct place to initialize the processes 
 for process in list_of_processes:
     process.ExecuteInitialize()
 
@@ -117,16 +113,14 @@ for process in list_of_processes:
 
 #### START SOLUTION ####
 
-#TODO: think if there is a better way to do this
 computing_model_part = solver.GetComputeModelPart()
 
-
-#### output settings start ####
+#### Output settings start ####
 
 problem_path = os.getcwd()
 problem_name = ProjectParameters["problem_data"]["problem_name"].GetString()
 
-# initialize GiD  I/O (gid outputs, file_lists)
+# Initialize GiD  I/O (gid outputs, file_lists)
 from gid_output_process import GiDOutputProcess
 output_settings = ProjectParameters["output_configuration"]
 gid_output = GiDOutputProcess(computing_model_part,
@@ -135,12 +129,11 @@ gid_output = GiDOutputProcess(computing_model_part,
 
 gid_output.ExecuteInitialize()
 
-# restart write included in gid IO ??
-
-#### output settings end ####
+#### Output settings end ####
 
 ## Sets strategies, builders, linear solvers, schemes and solving info, and fills the buffer
 solver.Initialize()
+solver.SetEchoLevel(echo_level)
 
 print(" ")
 print("::[KSM Simulation]:: Analysis -START- ")
@@ -168,13 +161,13 @@ end_time   = ProjectParameters["problem_data"]["end_time"].GetDouble()
 # writing a initial state results file (if no restart)
 # gid_io.write_results(time, computing_model_part) done in ExecuteBeforeSolutionLoop()
 
-# solving the problem (time integration)
+# Solving the problem (time integration)
 while(time <= end_time):
 
     #TODO: this must be done by a solving_info utility in the solver
-    # store previous time step
+    # Store previous time step
     #~ computing_model_part.ProcessInfo[PREVIOUS_DELTA_TIME] = delta_time
-    # set new time step ( it can change when solve is called )
+    # Set new time step ( it can change when solve is called )
     #~ delta_time = computing_model_part.ProcessInfo[DELTA_TIME]
 
     time = time + delta_time
@@ -182,14 +175,14 @@ while(time <= end_time):
     main_model_part.ProcessInfo[TIME_STEPS] = step
     main_model_part.CloneTimeStep(time)
 
-    # print process info
-    ##
-    
+    # Print process info
+
     for process in list_of_processes:
         process.ExecuteInitializeSolutionStep()
 
     gid_output.ExecuteInitializeSolutionStep()
         
+    solver.Clear()
     solver.Solve()
 
     gid_output.ExecuteFinalizeSolutionStep()
@@ -197,20 +190,18 @@ while(time <= end_time):
     for process in list_of_processes:
         process.ExecuteFinalizeSolutionStep()
 
-    #TODO: decide if it shall be done only when output is processed or not (boundary_conditions_processes ??)
     for process in list_of_processes:
         process.ExecuteBeforeOutputStep()
     
-    # write results and restart files: (frequency writing is controlled internally by the gid_io)
+    # Write results and restart files: (frequency writing is controlled internally by the gid_io)
     if gid_output.IsOutputStep():
         gid_output.PrintOutput()
                       
-    #TODO: decide if it shall be done only when output is processed or not
     for process in list_of_processes:
         process.ExecuteAfterOutputStep()
 
 
-# ending the problem (time integration finished)
+# Ending the problem (time integration finished)
 gid_output.ExecuteFinalize()
 
 for process in list_of_processes:
@@ -219,26 +210,19 @@ for process in list_of_processes:
 print("::[KSM Simulation]:: Analysis -END- ")
 print(" ")
 
-# check solving information for any problem
+# Check solving information for any problem
 #~ solver.InfoCheck() # InfoCheck not implemented yet.
 
 #### END SOLUTION ####
 
-# measure process time
+# Measure process time
 tfp = timer.clock()
-# measure wall time
+# Measure wall time
 tfw = timer.time()
 
 print("::[KSM Simulation]:: [ Computing Time = (%.2f" % (tfp - t0p)," seconds process time) ( %.2f" % (tfw - t0w)," seconds wall time) ]")
 
 print(timer.ctime())
-
-# to create a benchmark: add standard benchmark files and decomment next two lines 
-# rename the file to: run_test.py
-#from run_test_benchmark_results import *
-#WriteBenchmarkResults(model_part)
-
-
 
 
 
