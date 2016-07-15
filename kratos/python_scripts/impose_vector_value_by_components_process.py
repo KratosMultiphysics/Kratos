7c728fd6a7c125247bcf78e9f47187ab7e81078d
Josep Maria <josep-m-carbonell@users.noreply.github.com>
2016-07-15 15:13:38 +0000

    change import KratosMultiphysics

--- kratos/python_scripts/impose_vector_value_by_components_process.py
+++ kratos/python_scripts/impose_vector_value_by_components_process.py
@@ -1,17 +1,17 @@
-from KratosMultiphysics import *
+import KratosMultiphysics
 
 ## This proces sets the value of a vector variable component-by-component.
 ## In this case, the fixicity is given by the user and some of the components may not be fixed.
 
 def Factory(settings, Model):
-    if(type(settings) != Parameters):
+    if(type(settings) != KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return ImposeVectorValueByComponentsProcess(Model, settings["Parameters"])
 
 ## All the processes python processes should be derived from "python_process"
-class ImposeVectorValueByComponentsProcess(Process):
+class ImposeVectorValueByComponentsProcess(KratosMultiphysics.Process):
     def __init__(self, Model, settings ):
-        Process.__init__(self)
+        KratosMultiphysics.Process.__init__(self)
         
         model_part = Model[settings["model_part_name"].GetString()]
         variable_name = settings["variable_name"].GetString()
@@ -23,36 +23,36 @@ class ImposeVectorValueByComponentsProcess(Process):
 
         if settings["is_fixed_x"].GetBool() == True:
             # Auxiliar x-component parameters creation
-            x_params = Parameters("{}")           
+            x_params = KratosMultiphysics.Parameters("{}")           
             x_params.AddValue("model_part_name",settings["model_part_name"])
             x_params.AddValue("mesh_id",settings["mesh_id"])
             x_params.AddValue("is_fixed",settings["is_fixed_x"])
             x_params.AddValue("value",settings["value"][0])
             x_params.AddEmptyValue("variable_name").SetString(variable_name+"_X")
             
-            self.x_component_process = ApplyConstantScalarValueProcess(model_part, x_params)
+            self.x_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, x_params)
             
         if settings["is_fixed_y"].GetBool() == True:
             # Auxiliar y-component parameters creation
-            y_params = Parameters("{}")
+            y_params = KratosMultiphysics.Parameters("{}")
             y_params.AddValue("model_part_name",settings["model_part_name"])
             y_params.AddValue("mesh_id",settings["mesh_id"])
             y_params.AddValue("is_fixed",settings["is_fixed_y"])
             y_params.AddValue("value",settings["value"][1])
             y_params.AddEmptyValue("variable_name").SetString(variable_name+"_Y")
             
-            self.y_component_process = ApplyConstantScalarValueProcess(model_part, y_params)
+            self.y_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, y_params)
             
         if settings["is_fixed_z"].GetBool() == True:
             # Auxiliar x-component parameters creation
-            z_params = Parameters("{}")
+            z_params = KratosMultiphysics.Parameters("{}")
             z_params.AddValue("model_part_name",settings["model_part_name"])
             z_params.AddValue("mesh_id",settings["mesh_id"])
             z_params.AddValue("is_fixed",settings["is_fixed_z"])
             z_params.AddValue("value",settings["value"][2])
             z_params.AddEmptyValue("variable_name").SetString(variable_name+"_Z")
             
-            self.z_component_process = ApplyConstantScalarValueProcess(model_part, z_params)
+            self.z_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, z_params)
                     
         
     def ExecuteInitialize(self):
