cac73da7b2579d3093d90edbda0cec03a3aac6ff
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-15 06:03:15 +0000

    calling the correspondent remove methods of the modelpart, to ensure consistent behaviour and removal from all the submodelparts of the model_part passed as argument

--- kratos/kratos/processes/condition_erase_process.h
+++ kratos/kratos/processes/condition_erase_process.h
@@ -1,50 +1,13 @@
-/*
-==============================================================================
-KratosPFEMApplication
-A library based on:
-Kratos
-A General Purpose Software for Multi-Physics Finite Condition Analysis
-Version 1.0 (Released on march 05, 2007).
-
-Copyright 2007
-Pooyan Dadvand, Riccardo Rossi
-pooyan@cimne.upc.edu
-rrossi@cimne.upc.edu
-- CIMNE (International Center for Numerical Methods in Engineering),
-Gran Capita' s/n, 08034 Barcelona, Spain
-
-
-Permission is hereby granted, free  of charge, to any person obtaining
-a  copy  of this  software  and  associated  documentation files  (the
-"Software"), to  deal in  the Software without  restriction, including
-without limitation  the rights to  use, copy, modify,  merge, publish,
-distribute,  sublicense and/or  sell copies  of the  Software,  and to
-permit persons to whom the Software  is furnished to do so, subject to
-the following condition:
-
-Distribution of this code for  any  commercial purpose  is permissible
-ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
-
-The  above  copyright  notice  and  this permission  notice  shall  be
-included in all copies or substantial portions of the Software.
-
-THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
-EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
-CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
-TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-==============================================================================
-*/
-
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
-//   Project Name:        Kratos
-//   Last Modified by:    $Author: rrossi $
-//   Date:                $Date: 2009-01-22 17:13:57 $
-//   Revision:            $Revision: 1.2 $
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
 //
+//  Main authors:    Riccardo Rossi
 //
 
 
@@ -142,19 +105,8 @@ public:
     virtual void Execute()
     {
         KRATOS_TRY;
-
-        ModelPart::ConditionsContainerType temp_container;
-        temp_container.reserve(mr_model_part.Conditions().size());
-
-        temp_container.swap(mr_model_part.Conditions());
-
-
-
-        for(ModelPart::ConditionsContainerType::iterator i_elem = temp_container.begin() ; i_elem != temp_container.end() ; i_elem++)
-        {
-            if( static_cast<bool>(i_elem->Is(TO_ERASE)) == false)
-                (mr_model_part.Conditions()).push_back(*(i_elem.base()));
-        }
+        
+        mr_model_part.RemoveConditions(TO_ERASE);
 
         KRATOS_CATCH("")
     }
--- kratos/kratos/processes/element_erase_process.h
+++ kratos/kratos/processes/element_erase_process.h
@@ -1,51 +1,15 @@
-/*
-==============================================================================
-KratosPFEMApplication
-A library based on:
-Kratos
-A General Purpose Software for Multi-Physics Finite Element Analysis
-Version 1.0 (Released on march 05, 2007).
-
-Copyright 2007
-Pooyan Dadvand, Riccardo Rossi
-pooyan@cimne.upc.edu
-rrossi@cimne.upc.edu
-- CIMNE (International Center for Numerical Methods in Engineering),
-Gran Capita' s/n, 08034 Barcelona, Spain
-
-
-Permission is hereby granted, free  of charge, to any person obtaining
-a  copy  of this  software  and  associated  documentation files  (the
-"Software"), to  deal in  the Software without  restriction, including
-without limitation  the rights to  use, copy, modify,  merge, publish,
-distribute,  sublicense and/or  sell copies  of the  Software,  and to
-permit persons to whom the Software  is furnished to do so, subject to
-the following condition:
-
-Distribution of this code for  any  commercial purpose  is permissible
-ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
-
-The  above  copyright  notice  and  this permission  notice  shall  be
-included in all copies or substantial portions of the Software.
-
-THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
-EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
-CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
-TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-==============================================================================
-*/
-
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
-//   Project Name:        Kratos
-//   Last Modified by:    $Author: rrossi $
-//   Date:                $Date: 2009-01-22 17:13:57 $
-//   Revision:            $Revision: 1.2 $
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
 //
+//  Main authors:    Riccardo Rossi
 //
+
 
 
 #if !defined(KRATOS_ELEMENT_ERASE_PROCESS_INCLUDED )
@@ -64,7 +28,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // Project includes
 #include "includes/define.h"
 #include "processes/process.h"
-#include "includes/element.h"
 #include "includes/model_part.h"
 
 
@@ -142,18 +105,7 @@ public:
     {
         KRATOS_TRY;
 
-        ModelPart::ElementsContainerType temp_container;
-        temp_container.reserve(mr_model_part.Elements().size());
-
-        temp_container.swap(mr_model_part.Elements());
-
-
-
-        for(ModelPart::ElementsContainerType::iterator i_elem = temp_container.begin() ; i_elem != temp_container.end() ; i_elem++)
-        {
-            if( static_cast<bool>(i_elem->Is(TO_ERASE)) == false)
-                (mr_model_part.Elements()).push_back(*(i_elem.base()));
-        }
+        mr_model_part.RemoveElements(TO_ERASE);
 
         KRATOS_CATCH("")
     }
