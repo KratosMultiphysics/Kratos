c5ab92ab753f993584eb9a07247f346157458f7b
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-15 15:13:38 +0000

    change import KratosMultiphysics

--- kratos/kratos/python_scripts/impose_scalar_value_process.py
+++ kratos/kratos/python_scripts/impose_scalar_value_process.py
@@ -1,23 +1,23 @@
-from KratosMultiphysics import *
+import KratosMultiphysics
 
 ## This proces sets the value of a scalar variable using the ApplyConstantScalarValueProcess.
 ## In this case, the scalar value is automatically fixed.
 
 def Factory(settings, Model):
-    if(type(settings) != Parameters):
+    if(type(settings) != KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return ImposeScalarValueProcess(Model, settings["Parameters"])
 
 ## All the processes python processes should be derived from "python_process"
-class ImposeScalarValueProcess(ApplyConstantVectorValueProcess):
+class ImposeScalarValueProcess(KratosMultiphysics.ApplyConstantVectorValueProcess):
     def __init__(self, Model, settings ):
-        Process.__init__(self)
+        KratosMultiphysics.Process.__init__(self)
         
         model_part = Model[settings["model_part_name"].GetString()]  
         
         settings.AddEmptyValue("is_fixed").SetBool(True)       
         
-        ApplyConstantScalarValueProcess.__init__(self,model_part, settings)
+        KratosMultiphysics.ApplyConstantScalarValueProcess.__init__(self, model_part, settings)
 
 
         
