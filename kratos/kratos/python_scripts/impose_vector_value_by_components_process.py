914e8982ed2b3a96ca15682cfa650f4bdf79c42d
vmataix <vmataix@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-18 11:47:48 +0000

    Changing impose vector, the fixicity is already an argument in the scalar version of the script, which is called, so this is redundant and does not allow to consider a free initial displacement

--- kratos/kratos/python_scripts/impose_vector_value_by_components_process.py
+++ kratos/kratos/python_scripts/impose_vector_value_by_components_process.py
@@ -16,55 +16,42 @@ class ImposeVectorValueByComponentsProcess(KratosMultiphysics.Process):
         model_part = Model[settings["model_part_name"].GetString()]
         variable_name = settings["variable_name"].GetString()
         
-        # Auxiliar vector with the fixicity settings    
-        self.fixicity_vec = [settings["is_fixed_x"].GetBool(),
-                             settings["is_fixed_y"].GetBool(),
-                             settings["is_fixed_z"].GetBool()]
-
-        if settings["is_fixed_x"].GetBool() == True:
-            # Auxiliar x-component parameters creation
-            x_params = KratosMultiphysics.Parameters("{}")           
-            x_params.AddValue("model_part_name",settings["model_part_name"])
-            x_params.AddValue("mesh_id",settings["mesh_id"])
-            x_params.AddValue("is_fixed",settings["is_fixed_x"])
-            x_params.AddValue("value",settings["value"][0])
-            x_params.AddEmptyValue("variable_name").SetString(variable_name+"_X")
+        # Auxiliar x-component parameters creation
+        x_params = KratosMultiphysics.Parameters("{}")           
+        x_params.AddValue("model_part_name",settings["model_part_name"])
+        x_params.AddValue("mesh_id",settings["mesh_id"])
+        x_params.AddValue("is_fixed",settings["is_fixed_x"])
+        x_params.AddValue("value",settings["value"][0])
+        x_params.AddEmptyValue("variable_name").SetString(variable_name+"_X")
+        
+        self.x_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, x_params)
             
-            self.x_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, x_params)
+        # Auxiliar y-component parameters creation
+        y_params = KratosMultiphysics.Parameters("{}")
+        y_params.AddValue("model_part_name",settings["model_part_name"])
+        y_params.AddValue("mesh_id",settings["mesh_id"])
+        y_params.AddValue("is_fixed",settings["is_fixed_y"])
+        y_params.AddValue("value",settings["value"][1])
+        y_params.AddEmptyValue("variable_name").SetString(variable_name+"_Y")
+        
+        self.y_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, y_params)
             
-        if settings["is_fixed_y"].GetBool() == True:
-            # Auxiliar y-component parameters creation
-            y_params = KratosMultiphysics.Parameters("{}")
-            y_params.AddValue("model_part_name",settings["model_part_name"])
-            y_params.AddValue("mesh_id",settings["mesh_id"])
-            y_params.AddValue("is_fixed",settings["is_fixed_y"])
-            y_params.AddValue("value",settings["value"][1])
-            y_params.AddEmptyValue("variable_name").SetString(variable_name+"_Y")
-            
-            self.y_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, y_params)
-            
-        if settings["is_fixed_z"].GetBool() == True:
-            # Auxiliar x-component parameters creation
-            z_params = KratosMultiphysics.Parameters("{}")
-            z_params.AddValue("model_part_name",settings["model_part_name"])
-            z_params.AddValue("mesh_id",settings["mesh_id"])
-            z_params.AddValue("is_fixed",settings["is_fixed_z"])
-            z_params.AddValue("value",settings["value"][2])
-            z_params.AddEmptyValue("variable_name").SetString(variable_name+"_Z")
-            
-            self.z_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, z_params)
+        # Auxiliar x-component parameters creation
+        z_params = KratosMultiphysics.Parameters("{}")
+        z_params.AddValue("model_part_name",settings["model_part_name"])
+        z_params.AddValue("mesh_id",settings["mesh_id"])
+        z_params.AddValue("is_fixed",settings["is_fixed_z"])
+        z_params.AddValue("value",settings["value"][2])
+        z_params.AddEmptyValue("variable_name").SetString(variable_name+"_Z")
+        
+        self.z_component_process = KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, z_params)
                     
         
     def ExecuteInitialize(self):
                
-        if self.fixicity_vec[0] == True:
-            self.x_component_process.ExecuteInitialize()
-            
-        if self.fixicity_vec[1] == True:
-            self.y_component_process.ExecuteInitialize()
-            
-        if self.fixicity_vec[2] == True:
-            self.z_component_process.ExecuteInitialize()
+        self.x_component_process.ExecuteInitialize()
+        self.y_component_process.ExecuteInitialize()    
+        self.z_component_process.ExecuteInitialize()
         
         
 
