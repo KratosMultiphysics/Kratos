e52e10fb5f1a02400da5210e1ea5435e9afaf23c
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-19 20:57:06 +0000

    parametric walls build

---
+++ kratos/applications/ContactMechanicsApplication/custom_elements/rigid_body_element.hpp
@@ -0,0 +1,559 @@
+//
+//   Project Name:        KratosStringDynamicsApplication $
+//   Created by:          $Author:            JMCarbonell $
+//   Last modified by:    $Co-Author:                     $
+//   Date:                $Date:            November 2015 $
+//   Revision:            $Revision:                  0.0 $
+//
+// 
+
+#if !defined(KRATOS_RIGID_BODY_ELEMENT_H_INCLUDED )
+#define  KRATOS_RIGID_BODY_ELEMENT_H_INCLUDED
+
+// System includes
+
+// External includes
+#include "boost/smart_ptr.hpp"
+
+// Project includes
+#include "includes/define.h"
+#include "includes/serializer.h"
+#include "includes/element.h"
+#include "includes/ublas_interface.h"
+#include "includes/variables.h"
+#include "includes/constitutive_law.h"
+#include "custom_utilities/beam_math_utilities.hpp"
+#include "custom_bounding/rigid_body_bounding_box.hpp"
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+///@}
+///@name Type Definitions
+///@{
+///@}
+///@name  Enum's
+///@{
+///@}
+///@name  Functions
+///@{
+///@}
+///@name Kratos Classes
+///@{
+
+/// Rigid Body Element for 3D space dimension
+
+/**
+ * Nodal Variables: DISPLACEMENT, STEP_DISPLACEMENT, VELOCITY, ACCELERATION, ROTATION, STEP_ROTATION, DELTA_ROTATION, ANGULAR_VELOCITY, ANGULAR_ACCELERATION
+ * Nodal Dofs: DISPLACEMENT, ROTATION
+ */
+
+class RigidBodyElement
+    :public Element
+{
+public:
+
+    ///@name Type Definitions
+    ///@{    
+   ///Reference type definition for constitutive laws
+    typedef ConstitutiveLaw                         ConstitutiveLawType;
+    ///Pointer type for constitutive laws
+    typedef ConstitutiveLawType::Pointer     ConstitutiveLawPointerType;
+    ///StressMeasure from constitutive laws
+    typedef ConstitutiveLawType::StressMeasure        StressMeasureType;
+    ///Type definition for integration methods
+    typedef GeometryData::IntegrationMethod           IntegrationMethod;
+    ///Type definition for beam utilities
+    typedef BeamMathUtils<double>                     BeamMathUtilsType;
+    ///Type definition for quaternion 
+    typedef Quaternion<double>                           QuaternionType;
+
+    /// Counted pointer of RigidBodyElement
+    KRATOS_CLASS_POINTER_DEFINITION( RigidBodyElement );
+
+    ///@}
+
+protected:
+
+    /**
+     * Flags related to the element computation
+     */
+    KRATOS_DEFINE_LOCAL_FLAG( COMPUTE_RHS_VECTOR );
+    KRATOS_DEFINE_LOCAL_FLAG( COMPUTE_LHS_MATRIX );
+    KRATOS_DEFINE_LOCAL_FLAG( COMPUTE_RHS_VECTOR_WITH_COMPONENTS );
+    KRATOS_DEFINE_LOCAL_FLAG( COMPUTE_LHS_MATRIX_WITH_COMPONENTS );
+
+    /**
+     * Parameters to be used to store section properties
+     */
+
+    struct RigidBodyProperties
+    {
+      double Mass;                            // Mass of the Rigid Body
+      Matrix InertiaTensor;                   // Global Inertia Tensor
+    };
+
+    /**
+     * Parameters to be used in the Element as they are. Direct interface to Parameters Struct
+     */
+
+    struct GeneralVariables
+    {
+        //section properties
+        RigidBodyProperties RigidBody;
+ 
+        Matrix DeltaPosition;
+    };
+
+
+    /**
+     * This struct is used in the component wise calculation only
+     * is defined here and is used to declare a member variable in the component wise elements
+     * private pointers can only be accessed by means of set and get functions
+     * this allows to set and not copy the local system variables
+     */
+
+    struct LocalSystemComponents
+    {
+    private:
+      
+      //for calculation local system with compacted LHS and RHS 
+      MatrixType *mpLeftHandSideMatrix;
+      VectorType *mpRightHandSideVector;
+
+    public:
+
+      //calculation flags
+      Flags        CalculationFlags;
+
+      /**
+       * sets the value of a specified pointer variable
+       */
+      void SetLeftHandSideMatrix( MatrixType& rLeftHandSideMatrix ) { mpLeftHandSideMatrix = &rLeftHandSideMatrix; };
+
+      void SetRightHandSideVector( VectorType& rRightHandSideVector ) { mpRightHandSideVector = &rRightHandSideVector; };
+
+
+      /**
+       * returns the value of a specified pointer variable
+       */
+      MatrixType& GetLeftHandSideMatrix() { return *mpLeftHandSideMatrix; };
+
+      VectorType& GetRightHandSideVector() { return *mpRightHandSideVector; };
+ 
+    };
+
+
+public:
+
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructors
+    RigidBodyElement(IndexType NewId, GeometryType::Pointer pGeometry);
+
+    RigidBodyElement(IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties);
+
+    RigidBodyElement(IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties, ModelPart::MeshType::Pointer pMesh);
+
+    ///Copy constructor
+    RigidBodyElement(RigidBodyElement const& rOther);
+
+    /// Destructor.
+    virtual ~RigidBodyElement();
+
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+
+   /**
+     * creates a new element pointer
+     * @param NewId: the ID of the new element
+     * @param ThisNodes: the nodes of the new element
+     * @param pProperties: the properties assigned to the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes,  PropertiesType::Pointer pProperties) const;
+
+
+  
+
+    //************* GETTING METHODS
+
+
+    /**
+     * Sets on rElementalDofList the degrees of freedom of the considered element geometry
+     */
+    void GetDofList(DofsVectorType& rElementalDofList,ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * Sets on rResult the ID's of the element degrees of freedom
+     */
+    void EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * Sets on rValues the nodal displacements
+     */
+    void GetValuesVector(Vector& rValues, int Step = 0);
+
+    /**
+     * Sets on rValues the nodal velocities
+     */
+    void GetFirstDerivativesVector(Vector& rValues, int Step = 0);
+
+    /**
+     * Sets on rValues the nodal accelerations
+     */
+    void GetSecondDerivativesVector(Vector& rValues, int Step = 0);
+
+    //on integration points:
+    /**
+     * Access for variables on Integration points.
+     * This gives access to variables stored in the constitutive law on each integration point.
+     * Specialisations of element.h (e.g. the TotalLagrangian) must specify the actual
+     * interface to the constitutive law!
+     * Note, that these functions expect a std::vector of values for the
+     * specified variable type that contains a value for each integration point!
+     * SetValueOnIntegrationPoints: set the values for given Variable.
+     * GetValueOnIntegrationPoints: get the values for given Variable.
+     */
+
+    //GET:
+    /**
+     * Get on rVariable a Vector Value from the Element Constitutive Law
+     */
+    void GetValueOnIntegrationPoints( const Variable< array_1d<double, 3 > >& rVariable,
+				      std::vector< array_1d<double, 3 > >& rValues,
+				      const ProcessInfo& rCurrentProcessInfo );
+
+
+    //************* STARTING - ENDING  METHODS
+
+    /**
+      * Called to initialize the element.
+      * Must be called before any calculation is done
+      */
+    void Initialize();
+  
+      /**
+     * Called at the beginning of each solution step
+     */
+    void InitializeSolutionStep(ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * this is called for non-linear analysis at the beginning of the iteration process
+     */
+    void InitializeNonLinearIteration(ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * this is called for non-linear analysis at the beginning of the iteration process
+     */
+    void FinalizeNonLinearIteration(ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * Called at the end of eahc solution step
+     */
+    void FinalizeSolutionStep(ProcessInfo& rCurrentProcessInfo);
+
+    //************* COMPUTING  METHODS
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate all elemental contributions to the global system
+     * matrix and the right hand side
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rRightHandSideVector: the elemental right hand side
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental right hand side vector only
+     * @param rRightHandSideVector: the elemental right hand side vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental left hand side vector only
+     * @param rLeftHandSideVector: the elemental left hand side vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix, ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the second derivatives contributions for the LHS and RHS 
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rRightHandSideVector: the elemental right hand side
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateSecondDerivativesContributions(MatrixType& rLeftHandSideMatrix,
+						VectorType& rRightHandSideVector,
+						ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental left hand side matrix for the second derivatives constributions
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateSecondDerivativesLHS(MatrixType& rLeftHandSideMatrix,
+				       ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental right hand side vector for the second derivatives constributions
+     * @param rRightHandSideVector: the elemental right hand side vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateSecondDerivativesRHS(VectorType& rRightHandSideVector,
+				       ProcessInfo& rCurrentProcessInfo);
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental mass matrix
+     * @param rMassMatrix: the elemental mass matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateMassMatrix(MatrixType& rMassMatrix, ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * this function is designed to make the element to assemble an rRHS vector
+     * identified by a variable rRHSVariable by assembling it to the nodes on the variable
+     * rDestinationVariable.
+     * The "AddEXplicit" FUNCTIONS THE ONLY FUNCTIONS IN WHICH AN ELEMENT
+     * IS ALLOWED TO WRITE ON ITS NODES.
+     * the caller is expected to ensure thread safety hence
+     * SET/UNSETLOCK MUST BE PERFORMED IN THE STRATEGY BEFORE CALLING THIS FUNCTION
+     * @param rRHSVector: input variable containing the RHS vector to be assembled
+     * @param rRHSVariable: variable describing the type of the RHS vector to be assembled
+     * @param rDestinationVariable: variable in the database to which the rRHSVector will be assembled 
+      * @param rCurrentProcessInfo: the current process info instance
+     */
+    void AddExplicitContribution(const VectorType& rRHSVector, const Variable<VectorType>& rRHSVariable, Variable<array_1d<double,3> >& rDestinationVariable, const ProcessInfo& rCurrentProcessInfo);
+
+    //on integration points:
+    /**
+     * Calculate a double Variable on the Element Constitutive Law
+     */
+    void CalculateOnIntegrationPoints( const Variable< array_1d<double, 3 > >& rVariable,
+                                       std::vector< array_1d<double, 3 > >& Output,
+                                       const ProcessInfo& rCurrentProcessInfo);
+
+
+    //************************************************************************************
+    //************************************************************************************
+    /**
+     * This function provides the place to perform checks on the completeness of the input.
+     * It is designed to be called only once (or anyway, not often) typically at the beginning
+     * of the calculations, so to verify that nothing is missing from the input
+     * or that no common error is found.
+     * @param rCurrentProcessInfo
+     */
+    int Check(const ProcessInfo& rCurrentProcessInfo);
+    ///@}
+    ///@name Access
+    ///@{
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+    /// Turn back information as a string.
+    virtual std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "Rigid Body Element #" << Id();
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    virtual void PrintInfo(std::ostream& rOStream) const
+    {
+        rOStream << "Rigid Body Element #" << Id();
+    }
+
+    /// Print object's data.
+    virtual void PrintData(std::ostream& rOStream) const
+    {
+      GetGeometry().PrintData(rOStream);
+    }
+    ///@}
+    ///@name Friends
+    ///@{
+    ///@}
+
+protected:
+
+    ///@name Protected static Member Variables
+    ///@{
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    /**
+     * Global to Local Quaternion for Global to Local tensor transformation SPATIAL
+     */
+    QuaternionType  mInitialLocalQuaternion;
+
+
+    ModelPart::MeshType::Pointer mpMesh;
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+    RigidBodyElement() {};
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+
+
+    /**
+     * Calculates the elemental dynamic contributions
+      */
+    void CalculateDynamicSystem( LocalSystemComponents& rLocalSystem,
+				 ProcessInfo& rCurrentProcessInfo );
+
+
+    /**
+     * Initialize System Matrices
+     */
+    virtual void InitializeSystemMatrices(MatrixType& rLeftHandSideMatrix,
+                                          VectorType& rRightHandSideVector,
+                                          Flags& rCalculationFlags);
+
+    /**
+     * Transform Vector Variable from Global Frame to the Spatial Local Frame
+     */    
+    Vector& MapToInitialLocalFrame(Vector& rVariable);
+
+
+    /**
+     * Get Current Value, buffer 0 with FastGetSolutionStepValue
+     */    
+    Vector& GetNodalCurrentValue(const Variable<array_1d<double,3> >&rVariable, Vector& rValue, const unsigned int& rNode);
+
+    /**
+     * Get Previous Value, buffer 1 with FastGetSolutionStepValue
+     */    
+    Vector& GetNodalPreviousValue(const Variable<array_1d<double,3> >&rVariable, Vector& rValue, const unsigned int& rNode);
+
+
+    /**
+     * Initialize Element General Variables
+     */
+    virtual void InitializeGeneralVariables(GeneralVariables & rVariables, const ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * Calculation of the Rigid Body Properties
+     */
+    void CalculateRigidBodyProperties(RigidBodyProperties & rRigidBody);
+  
+
+ 
+    /**
+     * Calculation of the External Forces Vector. Fe = N * t + N * b
+     */
+    virtual void CalculateAndAddExternalForces(VectorType& rRightHandSideVector,
+					       GeneralVariables& rVariables,
+					       Vector& rVolumeForce);
+
+
+    /**
+      * Calculation of the Tangent Intertia Matrix
+      */
+    virtual void CalculateAndAddInertiaLHS(MatrixType& rLeftHandSideMatrix,
+					   GeneralVariables& rVariables,
+					   ProcessInfo& rCurrentProcessInfo);
+
+    /**
+      * Calculation of the Inertial Forces Vector
+      */
+    virtual void CalculateAndAddInertiaRHS(VectorType& rRightHandSideVector,
+					   GeneralVariables& rVariables,
+					   ProcessInfo& rCurrentProcessInfo);
+
+
+    /**
+     * Calculation of the Volume Force of the Element
+     */
+    virtual Vector& CalculateVolumeForce(Vector& rVolumeForce);
+
+
+    /**
+     * Calculation Complementary Method : Inertial Matrix Calculation Part 1
+     */
+   virtual void CalculateRotationLinearPartTensor(Vector& rRotationVector, Matrix& rRotationTensor);
+
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+    ///@}
+
+private:
+
+    ///@name Static Member Variables
+    ///@{
+    ///@}
+    ///@name Member Variables
+    ///@{
+    ///@}
+    ///@name Private Operators
+    ///@{
+    ///@}
+    ///@name Private Operations
+    ///@{
+    ///@}
+    ///@name Private  Access
+    ///@{
+    ///@}
+    ///@}
+    ///@name Serialization
+    ///@{
+    friend class Serializer;
+
+
+    // A private default constructor necessary for serialization
+
+
+    virtual void save(Serializer& rSerializer) const;
+  
+    virtual void load(Serializer& rSerializer);
+  
+    ///@name Private Inquiry
+    ///@{
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+    ///@}
+
+
+}; // Class RigidBodyElement
+
+} // namespace Kratos.
+#endif //  KRATOS_RIGID_BODY_ELEMENT_H_INCLUDED defined
+
