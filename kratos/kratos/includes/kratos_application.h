e3ea46b383461b20a3d2c82b59fb7ea389134568
jrubio <jrubio@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-20 13:31:44 +0000

    Added Blank elements

--- kratos/kratos/includes/kratos_application.h
+++ kratos/kratos/includes/kratos_application.h
@@ -370,6 +370,11 @@ protected:
     const Element  mElement3D4N;
     const Element  mElement2D3N;
     const Element  mElement2D2N;
+	
+	const Element mElement3D10N;
+	const Element mElement3D8N;
+	const Element mElement3D6N;
+
 
     KratosComponents<VariableData>::ComponentsContainerType* mpVariableData;
 
