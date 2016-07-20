fa549ff6963ce4ef1515618ff97e79d1e7e14f82
jrubio <jrubio@cimne.upc.edu>
2016-07-20 13:50:50 +0000

    added more precision

--- applications/ThermoMechanicalApplication/python_scripts/detect_solidus_liquidus_temperature.py
+++ applications/ThermoMechanicalApplication/python_scripts/detect_solidus_liquidus_temperature.py
@@ -3,13 +3,13 @@ from KratosMultiphysics.ThermoMechanicalApplication import *
 def DetectSolidusLiquidusTemperature(model_part):
     """ 
     Finds both the solidus and liquidus temperature that are included in the
-    Solid Fraction table (Table 3) of the ModelPart. It scans from 0 to 10,000 
-    in 0.01 intervals.
+    Solid Fraction table (Table 3) of the ModelPart. It scans from 100 to 5,000 
+    in 0.001 intervals.
     @param model_part
     """
-    lowlimit=0.0
-    uplimit=10000.0
-    dtemp=0.01
+    lowlimit=100.0
+    uplimit=5000.0
+    dtemp=0.001
     solidustemp=lowlimit
     liquidustemp=uplimit
     temp=lowlimit
@@ -21,10 +21,12 @@ def DetectSolidusLiquidusTemperature(model_part):
         if SF>=1.0:
             solidustemp=temp
         temp+=dtemp
-    print("solidustemp",solidustemp)
-    print("liquidustemp",liquidustemp)    
+    print("")
+    print("Found Solidus Temperature: ",solidustemp)
+    print("Found Liquidus Temperature: ",liquidustemp)    
     
     model_part.ProcessInfo.SetValue(FLUID_TEMPERATURE, liquidustemp)#ProjectParameters.FLUID_TEMPERATURE)
     model_part.ProcessInfo.SetValue(SOLID_TEMPERATURE, solidustemp)#ProjectParameters.SOLID_TEMPERATURE)
+    return liquidustemp, solidustemp
 
 
