0907a88608b074449661eedcffc5e30dc2b0a15a
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-07-21 06:06:41 +0000

    removing warnings and taking the occasion to change the license header

--- kratos/applications/FluidDynamicsApplication/fluid_dynamics_application.cpp
+++ kratos/applications/FluidDynamicsApplication/fluid_dynamics_application.cpp
@@ -1,11 +1,14 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
-//   Project Name:        Kratos
-//   Last Modified by:    $Author: jcotela $
-//   Date:                $Date: 2010-11-11 $
-//   Revision:            $Revision: 1.0 $
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
 //
+//  Main authors:    Jordi Cotela
 //
-
 
 
 // System includes
