31e0d94a3c68883b7fa9f3267efc27ee525085bf
rrossi <rrossi@cimne.upc.edu>
2016-07-22 08:20:49 +0000

    adding an inteface for the method GetProperties of the model part

--- kratos/python_scripts/read_materials_process.py
+++ kratos/python_scripts/read_materials_process.py
@@ -1,5 +1,6 @@
 from KratosMultiphysics import * 
-from KratosMultiphysics.SolidMechanicsApplication import *
+import importlib
+#from KratosMultiphysics.SolidMechanicsApplication import *
         
 def Factory(settings, Model):
     if(type(settings) != Parameters):
@@ -31,13 +32,31 @@ class ReadMaterialsProcess(Process):
             self._AssignPropertyBlock(prop)
         
         print("finished reading materials")
+        
+    def _GetItemFromModule(self,my_string):
+        splitted = my_string.split(".")
+        if(len(splitted) == 0):
+            raise Exception("something wrong. Trying to split the string "+my_string)
+        if(len(splitted) == 1):
+            return eval(my_string)
+        else:
+            module_name = ""
+            for i in range(len(splitted)-1):
+                module_name += splitted[i] 
+                if i != len(splitted)-2:
+                    module_name += "."
+
+            module = importlib.import_module(module_name)
+            return getattr(module,splitted[-1]) 
+            
             
             
     def _AssignPropertyBlock(self, data):
-        print(data["model_part_name"])
         model_part = self.Model[data["model_part_name"]]
-        prop = model_part.Properties[ data["properties_id"] ]
-                
+        property_id = data["properties_id"]
+        mesh_id = 0
+        prop = model_part.GetProperties(property_id, mesh_id)
+        
         ####################################
         #assign property to the list of elements and conditions in the model part
         for elem in model_part.Elements:
@@ -54,23 +73,28 @@ class ReadMaterialsProcess(Process):
 
         #read constitutive law and assign it to prop
         if "Variables" in mat["constitutive_law"].keys(): #pass the list of variables when constructing the constitutive law
-           constitutive_law = eval( mat["constitutive_law"]["name"])(mat["constitutive_law"]["Variables"])
+           constitutive_law = self._GetItemFromModule( mat["constitutive_law"]["name"])(mat["constitutive_law"]["Variables"])
         else:
-           constitutive_law = eval( mat["constitutive_law"]["name"])()
+           constitutive_law = self._GetItemFromModule( mat["constitutive_law"]["name"])()
            
         prop.SetValue(CONSTITUTIVE_LAW, constitutive_law)
         
         #read variables 
         for key, value in mat["Variables"].items():
-            prop.SetValue(eval(key), value)
-            #print(prop)
+            var = self._GetItemFromModule(key)
+            prop.SetValue( var, value)
 
         #read table
-        #for table in mat["Tables"]:
-            #new_table = PiecewiseLinearTable(eval(table["input_variable"]), eval(table["output_variable"]))
-            #for i in range(table["data"].size()):
-                #new_table.AddRow(new_table["data"][i][0], new_table["data"][i][1])
-            #prop.AddTable(new_table)
+        for key, table in mat["Tables"].items():
+            table_name = key
+
+            input_var = self._GetItemFromModule(table["input_variable"])
+            output_var = self._GetItemFromModule(table["output_variable"])
+
+            new_table = PiecewiseLinearTable()
+            for i in range(len(table["data"])):
+                new_table.AddRow(table["data"][i][0], table["data"][i][1])
+            prop.SetTable(input_var,output_var,new_table)
       
         
                 
