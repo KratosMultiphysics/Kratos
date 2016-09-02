33358858283fdd197609b7eac08ea3f07d2f045f
ipouplana <ipouplana@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-09-02 14:19:21 +0000

    Warning removal.

--- kratos/applications/DamApplication/custom_python/add_custom_strategies_to_python.cpp
+++ kratos/applications/DamApplication/custom_python/add_custom_strategies_to_python.cpp
@@ -40,12 +40,12 @@ using namespace boost::python;
 void  AddCustomStrategiesToPython()
 {
 
-    typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
-    typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
-    typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
-    typedef SolvingStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > BaseSolvingStrategyType;
-    typedef Scheme< SparseSpaceType, LocalSpaceType > BaseSchemeType;
-    typedef BuilderAndSolver< SparseSpaceType, LocalSpaceType, LinearSolverType > BuilderAndSolverType;
+    //typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
+    //typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
+    //typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
+    //typedef SolvingStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > BaseSolvingStrategyType;
+    //typedef Scheme< SparseSpaceType, LocalSpaceType > BaseSchemeType;
+    //typedef BuilderAndSolver< SparseSpaceType, LocalSpaceType, LinearSolverType > BuilderAndSolverType;
     
     //typedef DamNewtonRaphsonStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > DamNewtonRaphsonStrategyType;
 
