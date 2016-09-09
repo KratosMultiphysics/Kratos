b7b09cb58a2c3419665fbd1af35a2a9a043c74cc
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-09-09 15:34:56 +0000

    Changes to work together with custom ALE mesh BCs.

--- applications/FSIapplication/python_scripts/set_interface_process.py
+++ applications/FSIapplication/python_scripts/set_interface_process.py
@@ -12,5 +12,21 @@ class SetInterfaceProcess(Process):
         
         interface_model_part = Model[settings["model_part_name"].GetString()]
         
-        for node in interface_model_part.Nodes:
-            node.Set(INTERFACE, True)
+        if settings["variable_name"].GetString() == "STRUCTURE_INTERFACE":
+            for node in interface_model_part.Nodes:
+                # Set the INTERFACE flag
+                node.Set(INTERFACE, True)
+        
+        elif settings["variable_name"].GetString() == "FLUID_INTERFACE":
+            zero_vect = [0,0,0]
+            
+            for node in interface_model_part.Nodes:
+                # Set the INTERFACE flag
+                node.Set(INTERFACE, True)
+                # Fix the DISPLACEMENT and initialize it to zero at the interface
+                node.Fix(DISPLACEMENT_X)
+                node.Fix(DISPLACEMENT_Y)
+                node.Fix(DISPLACEMENT_Z)
+                node.SetSolutionStepValue(DISPLACEMENT,0,zero_vect)
+                
+        
