9973411ae5927a0334202a3e7d4601404ffdf87e
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2016-09-13 15:43:16 +0000

    Adding possibility of calculate the resultant

--- kratos/python_scripts/json_output_process.py
+++ kratos/python_scripts/json_output_process.py
@@ -21,7 +21,8 @@ class JsonOutputProcess(KratosMultiphysics.Process):
             "output_file_name"      : "",
             "model_part_name"      : "",
             "sub_model_part_name"  : "",
-            "time_frequency"       : 1.00
+            "time_frequency"       : 1.00,
+            "resultant_solution"   : false
         }
         """)
         
@@ -37,6 +38,7 @@ class JsonOutputProcess(KratosMultiphysics.Process):
         self.output_variables = []
         self.frequency = 0.0
         self.time_counter = 0.0
+        self.resultant_solution = False
         
     def ExecuteInitialize(self):
         self.output_file_name = self.params["output_file_name"].GetString()
@@ -44,27 +46,45 @@ class JsonOutputProcess(KratosMultiphysics.Process):
             self.sub_model_part = self.model_part[self.params["model_part_name"].GetString()].GetSubModelPart(self.params["sub_model_part_name"].GetString())
         else:
             self.sub_model_part = self.model_part[self.params["model_part_name"].GetString()]
+            
         self.output_variables = self.__generate_variable_list_from_input(self.params["output_variables"])
         self.frequency = self.params["time_frequency"].GetDouble()
+        self.resultant_solution = self.params["resultant_solution"].GetBool()
         
     def ExecuteBeforeSolutionLoop(self):
-      data = {}
-      data["TIME"] = []
-      for node in self.sub_model_part.Nodes:
-        data["NODE_"+str(node.Id)] = {}
-
-        for i in range(self.params["output_variables"].size()):
-          out = self.params["output_variables"][i]
-          variable = KratosMultiphysics.KratosGlobals.GetVariable( out.GetString() )
-          val = node.GetSolutionStepValue(variable, 0)
-          if isinstance(val,float):
-              data["NODE_"+str(node.Id)][out.GetString() ] = []
-          else: # It is a vector
-              data["NODE_"+str(node.Id)][out.GetString()  + "_X"] = []
-              data["NODE_"+str(node.Id)][out.GetString()  + "_Y"] = []
-              data["NODE_"+str(node.Id)][out.GetString()  + "_Z"] = []
-        write_external_json(self.output_file_name, data)
+        data = {}
+        data["TIME"] = []
+        count = 0
+        for node in self.sub_model_part.Nodes:
+            if (self.resultant_solution == False):
+                data["NODE_"+str(node.Id)] = {}
+            else:
+                data["RESULTANT"] = {}
+                    
+            for i in range(self.params["output_variables"].size()):
+                out = self.params["output_variables"][i]
+                variable = KratosMultiphysics.KratosGlobals.GetVariable( out.GetString() )
+                val = node.GetSolutionStepValue(variable, 0)
+                if isinstance(val,float):
+                    if (self.resultant_solution == False):
+                        data["NODE_"+str(node.Id)][out.GetString() ] = []
+                    else:
+                        if (count == 0):
+                            data["RESULTANT"][out.GetString() ] = []
+                else: # It is a vector
+                    if (self.resultant_solution == False):
+                        data["NODE_"+str(node.Id)][out.GetString()  + "_X"] = []
+                        data["NODE_"+str(node.Id)][out.GetString()  + "_Y"] = []
+                        data["NODE_"+str(node.Id)][out.GetString()  + "_Z"] = []
+                    else:
+                        if (count == 0):
+                            data["RESULTANT"][out.GetString()  + "_X"] = []
+                            data["RESULTANT"][out.GetString()  + "_Y"] = []
+                            data["RESULTANT"][out.GetString()  + "_Z"] = []
+            count += 1
             
+        write_external_json(self.output_file_name, data)
+        
     def ExecuteInitializeSolutionStep(self):
         pass
 
@@ -75,25 +95,39 @@ class JsonOutputProcess(KratosMultiphysics.Process):
         dt = self.sub_model_part.ProcessInfo.GetValue(KratosMultiphysics.DELTA_TIME)
         self.time_counter += dt
         if self.time_counter > self.frequency:
-          self.time_counter = 0.0
-          data["TIME"].append(time)
-          for node in self.sub_model_part.Nodes:
-            for i in range(self.params["output_variables"].size()):
-              out = self.params["output_variables"][i]
-              variable = KratosMultiphysics.KratosGlobals.GetVariable( out.GetString() )
-              val = node.GetSolutionStepValue(variable, 0)
-              if isinstance(val,float):
-                value_scalar = True
-              else:
-                value_scalar = False
+            self.time_counter = 0.0
+            data["TIME"].append(time)
+            count = 0
+            for node in self.sub_model_part.Nodes:
+                for i in range(self.params["output_variables"].size()):
+                    out = self.params["output_variables"][i]
+                    variable = KratosMultiphysics.KratosGlobals.GetVariable( out.GetString() )
+                    val = node.GetSolutionStepValue(variable, 0)
 
-              value = node.GetSolutionStepValue(variable, 0)
-              if value_scalar:
-                data["NODE_"+str(node.Id)][out.GetString() ].append(value)
-              else: # It is a vector
-                data["NODE_"+str(node.Id)][out.GetString()  + "_X"].append(value[0])
-                data["NODE_"+str(node.Id)][out.GetString()  + "_Y"].append(value[1])
-                data["NODE_"+str(node.Id)][out.GetString()  + "_Z"].append(value[2])
+                    value = node.GetSolutionStepValue(variable, 0)
+                    if isinstance(val,float):
+                        if (self.resultant_solution == False):
+                            data["NODE_"+str(node.Id)][out.GetString() ].append(value)
+                        else:
+                            if (count == 0):
+                                data["RESULTANT"][out.GetString() ].append(value)
+                            else:
+                                data["RESULTANT"][out.GetString() ][-1] += value
+                    else: # It is a vector
+                        if (self.resultant_solution == False):
+                            data["NODE_"+str(node.Id)][out.GetString()  + "_X"].append(value[0])
+                            data["NODE_"+str(node.Id)][out.GetString()  + "_Y"].append(value[1])
+                            data["NODE_"+str(node.Id)][out.GetString()  + "_Z"].append(value[2])
+                        else:
+                            if (count == 0):
+                                data["RESULTANT"][out.GetString()  + "_X"].append(value[0])
+                                data["RESULTANT"][out.GetString()  + "_Y"].append(value[1])
+                                data["RESULTANT"][out.GetString()  + "_Z"].append(value[2])
+                            else:
+                                data["RESULTANT"][out.GetString()  + "_X"][-1] += value[0]
+                                data["RESULTANT"][out.GetString()  + "_Y"][-1] += value[1]
+                                data["RESULTANT"][out.GetString()  + "_Z"][-1] += value[2]
+                count += 1
               
         write_external_json(self.output_file_name, data)
 
