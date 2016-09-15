e7e6629f8cfce83a884c614d052e80a7503f8224
fjgarate <fjgarate@cimne.upc.edu>
2016-09-15 17:10:39 +0000

    PFEM element filter by Body type

--- interfaces/GiD/kratos.gid/apps/Pfem/xml/GetFromXML.tcl
+++ interfaces/GiD/kratos.gid/apps/Pfem/xml/GetFromXML.tcl
@@ -42,4 +42,45 @@ proc ::Pfem::xml::::CheckElementOutputState { domNode args } {
      return [::Model::CheckElementOutputState $elemsactive $paramName]
 }
 
+proc Pfem::xml::ProcGetElements {domNode args} {
+     set nodeApp [spdAux::GetAppIdFromNode $domNode]
+     set sol_stratUN [apps::getAppUniqueName $nodeApp SolStrat]
+     set schemeUN [apps::getAppUniqueName $nodeApp Scheme]
+     if {[get_domnode_attribute [$domNode selectNodes [spdAux::getRoute $sol_stratUN]] v] eq ""} {
+         get_domnode_attribute [$domNode selectNodes [spdAux::getRoute $sol_stratUN]] dict
+     }
+     if {[get_domnode_attribute [$domNode selectNodes [spdAux::getRoute $schemeUN]] v] eq ""} {
+          get_domnode_attribute [$domNode selectNodes [spdAux::getRoute $schemeUN]] dict
+     }
+     
+     #W "solStrat $sol_stratUN sch $schemeUN"
+     set solStratName [::write::getValue $sol_stratUN]
+     set schemeName [write::getValue $schemeUN]
+     #W "$solStratName $schemeName"
+     #W "************************************************************************"
+     #W "$nodeApp $solStratName $schemeName"
+     set elems [::Model::GetAvailableElements $solStratName $schemeName]
+     #W "************************************************************************"
+     set names [list ]
+     set pnames [list ]
+     set BodyType [get_domnode_attribute [[[$domNode parent] parent] selectNodes "value\[@n='BodyType'\]"] v]
+     set argums [list ElementType $BodyType]
+     
+     foreach elem $elems {
+         if {[$elem cumple $argums]} {
+             lappend names [$elem getName]
+             lappend pnames [$elem getName] 
+             lappend pnames [$elem getPublicName]
+         }
+     }
+     set diction [join $pnames ","]
+     set values [join $names ","]
+     
+     $domNode setAttribute values $values
+     if {[get_domnode_attribute $domNode v] eq ""} {$domNode setAttribute v [lindex $names 0]}
+     if {[get_domnode_attribute $domNode v] ni $names} {$domNode setAttribute v [lindex $names 0]}
+     #spdAux::RequestRefresh
+     return $diction
+}
+
 Pfem::xml::Init
