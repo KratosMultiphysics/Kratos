851e417af57c93a29bfebc0f3537586e076fcb2c
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-09-16 10:06:26 +0000

    Moved contant pair as a separate class

--- kratos/kratos/utilities/spatial_containers_configure.h
+++ kratos/kratos/utilities/spatial_containers_configure.h
@@ -17,7 +17,8 @@
 #include <iostream>
 #include <cmath>
 
-
+// Project includes
+#include "utilities/contact_pair.h"
 
 namespace Kratos
 {
@@ -44,75 +45,6 @@ namespace Kratos
 /// Short class definition.
 /** Detail class definition.
 */
-
-
-template <class T>
-class ContactPair
-{
-public:
-    T value[2];
-
-    ContactPair() {}
-
-    ContactPair(const T& First,const T& Second)
-    {
-        value[0]  = First;
-        value[1]  = Second;
-    }
-
-    ~ContactPair() {}
-    T& operator[](std::size_t index)
-    {
-        return value[index];
-    }
-
-
-    T const& operator[](std::size_t index) const
-    {
-        return value[index];
-    }
-
-
-    ContactPair& operator = (ContactPair& Pair)
-    {
-        value[0] = Pair[0];
-        value[1] = Pair[1];
-        return *this;
-    }
-
-    ContactPair& operator = (const ContactPair& Pair)
-    {
-        value[0] = Pair[0];
-        value[1] = Pair[1];
-        return *this;
-    }
-
-
-    inline bool operator == (const ContactPair& Pair)
-    {
-        return  (value[0] == Pair[0]) && (value[1] == Pair[1]) ;
-    }
-
-    inline bool  operator == (const ContactPair& Pair) const
-    {
-        return ( (value[0] == Pair[0]) && (value[1] == Pair[1]) ) ;
-    }
-
-    inline std::ostream& operator << (std::ostream& OStream)
-    {
-        OStream << "Object 1 =  "<<  value[0] << "  " << "Object 2 =" << value[1] << std::endl;
-        return OStream;
-    }
-
-    inline std::ostream& operator << (std::ostream& OStream) const
-    {
-        OStream << "Object 1 =  "<<  value[0] << "  " << "Object 2 =" << value[1] << std::endl;
-        return OStream;
-    }
-
-
-};
-
 template <std::size_t TDimension>
 class SpatialContainersConfigure
 {
@@ -365,6 +297,4 @@ inline std::ostream& operator << (std::ostream& rOStream,
 
 }  // namespace Kratos.
 
-#endif // KRATOS_SPATIAL_CONTAINERS_CONFIGURE_INCLUDED  defined 
-
-
+#endif // KRATOS_SPATIAL_CONTAINERS_CONFIGURE_INCLUDED  defined
