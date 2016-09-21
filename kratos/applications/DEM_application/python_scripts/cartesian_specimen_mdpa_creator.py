c6f05ccfedfefad7a2ae07ff65a57486694b9cce
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-09-21 10:55:14 +0000

    Implemented a first simple version of a function that builds the mdpa of a specimen using a cartesian mesh of user-defined size

---
+++ kratos/applications/DEM_application/python_scripts/cartesian_specimen_mdpa_creator.py
@@ -0,0 +1,6 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from KratosMultiphysics import *
+from KratosMultiphysics.DEMApplication import *
+import sys
+
+PreUtilities().CreateCartesianSpecimenMdpa(sys.argv[1])
