b394d5b4bc87e03972f85087553d1c3c8dbbf393
maceli <maceli@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-09-22 09:54:39 +0000

    Help added to the script

--- kratos/applications/DEM_application/python_scripts/cartesian_specimen_mdpa_creator.py
+++ kratos/applications/DEM_application/python_scripts/cartesian_specimen_mdpa_creator.py
@@ -1,6 +1,10 @@
+# To use this script just type in the terminal: python (or python3) cartesian_specimen_mdpa_creator.py name_of_your_case
+# It creates a file called name_of_your_caseDEM.mdpa. 
+# Remember to overwrite the Properties and the boundary conditions if necessary!!
+
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 from KratosMultiphysics import *
-from KratosMultiphysics.DEMApplication import *
+import KratosMultiphysics.DEMApplication as DEMapp
 import sys
 
-PreUtilities().CreateCartesianSpecimenMdpa(sys.argv[1])
+DEMapp.PreUtilities().CreateCartesianSpecimenMdpa(sys.argv[1])
