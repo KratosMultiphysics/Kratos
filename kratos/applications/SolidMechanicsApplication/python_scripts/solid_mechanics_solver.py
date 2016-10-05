739a4630459373597acb725a6648d4d0d45fc611
cpuigbo <cpuigbo@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-05 18:21:45 +0000

    bodies passed as params

--- kratos/applications/SolidMechanicsApplication/python_scripts/solid_mechanics_solver.py
+++ kratos/applications/SolidMechanicsApplication/python_scripts/solid_mechanics_solver.py
@@ -163,6 +163,10 @@ class MechanicalSolver(object):
             params.AddValue("problem_domain_sub_model_part_list",self.settings["problem_domain_sub_model_part_list"])
             params.AddValue("processes_sub_model_part_list",self.settings["processes_sub_model_part_list"])         
 
+            if( self.settings.Has("bodies_list") ):
+                params.AddValue("bodies_list",self.settings["bodies_list"])
+
+            
             # CheckAndPrepareModelProcess creates the solid_computational model part
             import check_and_prepare_model_process_solid
             check_and_prepare_model_process_solid.CheckAndPrepareModelProcess(self.main_model_part, params).Execute()
