9b539a177fc36dabd80e9c655c67fca8e9cccfdb
rrossi <rrossi@cimne.upc.edu>
2016-10-07 09:55:03 +0000

    optimizing the application of a time function

--- kratos/python_scripts/experimental_assign_value_process.py
+++ kratos/python_scripts/experimental_assign_value_process.py
@@ -25,18 +25,24 @@ class AssignValueProcess(KratosMultiphysics.Process):
             {
                 "mesh_id"         : 0,
                 "model_part_name" : "please_specify_model_part_name",
-                "variable_name"   : "TEMPERATURE",
-                "interval"        : [0.0, "end"],
-                "constrained"          : true,
-                "free_outside_of_interval" : false,
-                "value"             : "x+y",
-                "local_axes"               :{}
+                "variable_name"   : "SPECIFY_VARIABLE_NAME",
+                "interval"        : [0.0, 1e30],
+                "constrained"     : true,
+                "value"           : 0.0,
+                "local_axes"      : {}
             }
             """
             )
         
-        settings.ValidateAndAssignDefaults(default_settings)
+        #here i do a trick, since i want to allow "value" to be a string or a double value
+        if(settings.Has("value")):
+            if(settings["value"].IsString()):
+                default_settings["value"].SetString("0.0")
+                
         
+        
+        settings.ValidateAndAssignDefaults(default_settings)
+
         #admissible values for local axes, are "empty" or 
         #"local_axes"               :{
         #    "origin" : [0.0, 0.0, 0.0]
@@ -70,16 +76,21 @@ class AssignValueProcess(KratosMultiphysics.Process):
         self.is_fixed = settings["constrained"].GetBool()
         
         self.value_is_numeric = False
-        self.evaluate_once_for_all_nodes = False
+        self.is_time_function = False
         if settings["value"].IsNumber():
             self.value_is_numeric = True
             self.value = settings["value"].GetDouble()
-            self.evaluate_once_for_all_nodes = True
         else:            
             self.function_string = settings["value"].GetString()
-            self.compiled_function = compile(self.function_string, '', 'eval', optimize=2) #here we precompile the expression so that then it is much faster to execute it
-            self.tmp = aux_object_cpp_callback(self.compiled_function)
-            self.cpp_apply_function_utility = KratosMultiphysics.PythonGenericFunctionUtility(self.mesh.Nodes, self.tmp ) 
+            self.aux_function = aux_object_cpp_callback(compile(self.function_string, '', 'eval', optimize=2))
+            
+            if(self.function_string.find("x") == -1 and 
+               self.function_string.find("y") == -1 and
+               self.function_string.find("z") == -1): #depends on time alone!
+                    self.is_time_function = True
+            else:
+                self.cpp_apply_function_utility = KratosMultiphysics.PythonGenericFunctionUtility(self.mesh.Nodes, self.aux_function ) 
+            
         
         #construct a variable_utils object to speedup fixing
         self.variable_utils = KratosMultiphysics.VariableUtils()
@@ -97,21 +108,20 @@ class AssignValueProcess(KratosMultiphysics.Process):
                 
             if self.value_is_numeric:
                 self.variable_utils.SetScalarVar(self.variable, self.value, self.model_part.Nodes)
-            #if self.is_time_function:
-                #self.function = self.compiled_function(x,y,z,t)
-                #self.variable_utils.SetScalarVar(self.variable, self.value, self.model_part.Nodes)
-            #TODO: here we should read from a table
-            else: #most general case - space varying function (possibly also time varying)
-                if self.non_trivial_local_system == False:
-                    self.cpp_apply_function_utility.ApplyFunction(self.variable, current_time)
-                else: #TODO: OPTIMIZE!!
-                    for node in self.model_part.Nodes:
-                        dx = node - self.x0
-                        x = self.R[0,0]*dx[0] + self.R[0,1]*dx[1] + self.R[0,2]*dx[2]
-                        y = self.R[1,0]*dx[0] + self.R[1,1]*dx[1] + self.R[1,2]*dx[2]
-                        z = self.R[2,0]*dx[0] + self.R[2,1]*dx[1] + self.R[2,2]*dx[2]
-                        t = current_time
-                        node.SetSolutionStepValue(self.variable,0,eval(self.compiled_function))
+            else:
+                if self.is_time_function:
+                    self.value = self.aux_function.f(0.0,0.0,0.0,current_time)
+                    self.variable_utils.SetScalarVar(self.variable, self.value, self.model_part.Nodes)
+                else: #most general case - space varying function (possibly also time varying)
+                    if self.non_trivial_local_system == False:
+                        self.cpp_apply_function_utility.ApplyFunction(self.variable, current_time)
+                    else: #TODO: OPTIMIZE!!
+                        for node in self.model_part.Nodes:
+                            dx = node - self.x0
+                            x = self.R[0,0]*dx[0] + self.R[0,1]*dx[1] + self.R[0,2]*dx[2]
+                            y = self.R[1,0]*dx[0] + self.R[1,1]*dx[1] + self.R[1,2]*dx[2]
+                            z = self.R[2,0]*dx[0] + self.R[2,1]*dx[1] + self.R[2,2]*dx[2]
+                            node.SetSolutionStepValue(self.variable,0,self.aux_function.f(x,y,z,current_time))
                         
             
     def ExecuteFinalizeSolutionStep(self):
@@ -120,4 +130,5 @@ class AssignValueProcess(KratosMultiphysics.Process):
         if(current_time > self.interval[0].GetDouble() and  current_time<self.interval[1].GetDouble()):
             #here we free all of the nodes in the mesh
             if(self.is_fixed):
-                self.variable_utils.ApplyFixity(self.variable, False, self.mesh.Nodes)
+                fixity_status  = False
+                self.variable_utils.ApplyFixity(self.variable, fixity_status, self.mesh.Nodes)
