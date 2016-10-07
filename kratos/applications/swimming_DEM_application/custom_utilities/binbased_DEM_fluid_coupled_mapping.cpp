6ca04ce81bcf1ed0ebecc1c7d7754cb8b60d6580
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-07 07:54:16 +0000

    I had forgot to change one of the ways to project material acceleration

--- kratos/applications/swimming_DEM_application/custom_utilities/binbased_DEM_fluid_coupled_mapping.cpp
+++ kratos/applications/swimming_DEM_application/custom_utilities/binbased_DEM_fluid_coupled_mapping.cpp
@@ -931,7 +931,8 @@ void BinBasedDEMFluidCoupledMapping<TDim, TBaseTypeOfSwimmingParticle>::Project(
     }
 
     else if (*r_destination_variable == FLUID_ACCEL_PROJECTED){
-        InterpolateAcceleration(p_elem, N, p_node, FLUID_ACCEL_PROJECTED);
+        //InterpolateAcceleration(p_elem, N, p_node, FLUID_ACCEL_PROJECTED);
+        Interpolate(p_elem, N, p_node, MATERIAL_ACCELERATION, FLUID_ACCEL_PROJECTED);
     }
 
     else if (*r_destination_variable == SHEAR_RATE_PROJECTED){
