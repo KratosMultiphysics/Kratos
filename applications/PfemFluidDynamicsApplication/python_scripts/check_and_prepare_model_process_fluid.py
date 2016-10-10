8074b7c4e9adbdeabf1dcf16b26a686f3a82fd96
falessandro <falessandro@cimne.upc.edu>
2016-10-10 16:19:47 +0000

    Chec&prepare model  part for PFEMfluid

---
+++ applications/PfemFluidDynamicsApplication/python_scripts/check_and_prepare_model_process_fluid.py
@@ -0,0 +1,135 @@
+#from KratosMultiphysics import *
+import KratosMultiphysics 
+from KratosMultiphysics.ExternalSolversApplication import *
+from KratosMultiphysics.SolidMechanicsApplication import *
+from KratosMultiphysics.PfemBaseApplication import *
+from KratosMultiphysics.PfemFluidDynamicsApplication import *
+from KratosMultiphysics.PfemSolidMechanicsApplication import *
+#from KratosMultiphysics.PfemBaseApplication import *
+#from KratosMultiphysics.PfemFluidDynamicsApplication import *
+
+def Factory(settings, Model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return CheckAndPrepareModelProcess(Model, settings["Parameters"])
+
+##all the processes python processes should be derived from "python_process"
+class CheckAndPrepareModelProcess(KratosMultiphysics.Process):
+    def __init__(self, main_model_part, Parameters ):
+        self.main_model_part = main_model_part
+        
+        self.computing_model_part_name  = Parameters["computing_model_part_name"].GetString()
+        print("Parameters.Has problem_domain_sub_model_part_list ?")
+        if( Parameters.Has("problem_domain_sub_model_part_list") ):
+            print("Parameters.Has processes_sub_model_part_list")       
+            self.sub_model_part_names     = Parameters["problem_domain_sub_model_part_list"]
+        print("Parameters.Has processes_sub_model_part_list ?")
+        if( Parameters.Has("processes_sub_model_part_list") ):
+            print("Parameters.Has processes_sub_model_part_list")
+            self.processes_model_part_names = Parameters["processes_sub_model_part_list"]
+
+        self.bodies_parts_list = []
+        self.bodies_list = False
+        if( Parameters.Has("bodies_list") ):
+            self.bodies_list = True
+            self.bodies_parts_list = Parameters["bodies_list"]
+
+
+    def Execute(self):
+        print("Execute check_and_prepare_model_process_fluid")
+
+        #construct body model parts:
+        fluid_body_model_parts = []
+        rigid_body_model_parts = []
+
+        if( self.bodies_list == True ):
+            for i in range(self.bodies_parts_list.size()):
+                #create body model part
+                body_model_part_name = self.bodies_parts_list[i]["body_name"].GetString()
+                self.main_model_part.CreateSubModelPart(body_model_part_name)
+                body_model_part = self.main_model_part.GetSubModelPart(body_model_part_name)
+ 
+                print("[Model_Prepare]::Body Creation", body_model_part_name)
+                body_model_part.ProcessInfo = self.main_model_part.ProcessInfo
+                body_model_part.Properties  = self.main_model_part.Properties
+
+                #build body from their parts
+                body_parts_name_list = self.bodies_parts_list[i]["parts_list"]
+                body_parts_list = []
+                #for j in range(len(body_parts_name_list)):
+                for j in range(body_parts_name_list.size()):
+
+                    body_parts_list.append(self.main_model_part.GetSubModelPart(body_parts_name_list[j].GetString()))
+                    
+                body_model_part_type = self.bodies_parts_list[i]["body_type"].GetString()
+
+                for part in body_parts_list:
+                    for node in part.Nodes:
+                        body_model_part.AddNode(node,0)
+                        #body_model_part.Nodes.append(node)
+                        if (body_model_part_type=="fluid"):
+                            node.Set(KratosMultiphysics.FLUID)
+                        if (body_model_part_type=="rigid"):
+                            node.Set(KratosMultiphysics.RIGID)
+                            node.Set(KratosMultiphysics.BOUNDARY)
+                    for elem in part.Elements:
+                        body_model_part.AddElement(elem,0)
+                    for cond in part.Conditions:
+                        body_model_part.AddCondition(cond,0) 
+
+                if( body_model_part_type == "fluid" ):
+                    body_model_part.Set(KratosMultiphysics.FLUID)
+                    fluid_body_model_parts.append(self.main_model_part.GetSubModelPart(body_model_part_name))
+                if( body_model_part_type == "rigid" ):
+                    body_model_part.Set(KratosMultiphysics.RIGID)
+                    rigid_body_model_parts.append(self.main_model_part.GetSubModelPart(body_model_part_name))
+
+                print(body_model_part)
+
+            for fluid_part in fluid_body_model_parts:
+                for rigid_part in rigid_body_model_parts:
+                    for node in rigid_part.Nodes:
+                        if( node.IsNot(KratosMultiphysics.FLUID) ):
+                            fluid_part.AddNode(node,0)
+
+
+            
+        #construct the computing model part:        
+        domain_parts = []
+        for i in range(self.sub_model_part_names.size()):
+            domain_parts.append(self.main_model_part.GetSubModelPart(self.sub_model_part_names[i].GetString()))
+        
+        processes_parts = []
+        for i in range(self.processes_model_part_names.size()):
+            processes_parts.append(self.main_model_part.GetSubModelPart(self.processes_model_part_names[i].GetString()))
+        
+        #construct a model part which contains the mesh to compute
+        self.main_model_part.CreateSubModelPart(self.computing_model_part_name)
+        fluid_computing_model_part = self.main_model_part.GetSubModelPart(self.computing_model_part_name)
+        fluid_computing_model_part.ProcessInfo = self.main_model_part.ProcessInfo
+        fluid_computing_model_part.Properties  = self.main_model_part.Properties
+
+        #set flag to identify the fluid model part
+        fluid_computing_model_part.Set(KratosMultiphysics.FLUID)
+        #set flag to identify the computing model part
+        fluid_computing_model_part.Set(KratosMultiphysics.ACTIVE)
+        
+        for node in self.main_model_part.Nodes:
+            fluid_computing_model_part.AddNode(node,0)
+ 
+        for part in domain_parts:
+            #part.Set(KratosMultiphysics.FLUID)
+            for elem in part.Elements:
+                fluid_computing_model_part.AddElement(elem,0)
+            #for node in part.Nodes:
+            #    fluid_computing_model_part.Nodes.append(node)
+            
+        for part in processes_parts:
+            part.Set(KratosMultiphysics.BOUNDARY)
+            for cond in part.Conditions:
+                fluid_computing_model_part.AddCondition(cond,0)  
+                
+        #for part in domain_parts:
+        #    self.main_model_part.RemoveSubModelPart(part.Name)
+
+        print(self.main_model_part)
