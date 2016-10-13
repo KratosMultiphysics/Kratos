122a0cc8d7994873785d1a58b968ab57309293ca
lgracia <lgracia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-13 13:36:27 +0000

    Adding the new strategy

--- kratos/applications/DamApplication/custom_python/add_custom_strategies_to_python.cpp
+++ kratos/applications/DamApplication/custom_python/add_custom_strategies_to_python.cpp
@@ -15,6 +15,7 @@
 #include "containers/flags.h"
 #include "custom_python/add_custom_strategies_to_python.h"
 #include "spaces/ublas_space.h"
+#include "includes/kratos_parameters.h"
 
 //linear solvers
 #include "linear_solvers/linear_solver.h"
@@ -27,6 +28,9 @@
 //schemes
 #include "custom_strategies/schemes/incrementalupdate_static_smoothing_scheme.hpp"
 #include "custom_strategies/schemes/bossak_displacement_smoothing_scheme.hpp"
+
+//strategies
+#include "custom_strategies/strategies/dam_eulerian_convection_diffusion_strategy.hpp"
 
 
 namespace Kratos
@@ -42,24 +46,27 @@ void  AddCustomStrategiesToPython()
     typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
     typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
     typedef Scheme< SparseSpaceType, LocalSpaceType > BaseSchemeType;
-    
+    typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
+    typedef SolvingStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > BaseSolvingStrategyType;
+
+
     //custom scheme types
     typedef IncrementalUpdateStaticSmoothingScheme< SparseSpaceType, LocalSpaceType >  IncrementalUpdateStaticSmoothingSchemeType;     
     typedef BossakDisplacementSmoothingScheme< SparseSpaceType, LocalSpaceType >  BossakDisplacementSmoothingSchemeType;
+    
+    //custom strategies types
+    typedef DamEulerianConvectionDiffusionStrategy< SparseSpaceType, LocalSpaceType, LinearSolverType > DamEulerianConvectionDiffusionStrategy;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    class_< IncrementalUpdateStaticSmoothingSchemeType,
-            bases< BaseSchemeType >,  boost::noncopyable >
-            (
-            "IncrementalUpdateStaticSmoothingScheme", init< >() 
-            );
+    class_< IncrementalUpdateStaticSmoothingSchemeType, bases< BaseSchemeType >, boost::noncopyable >("IncrementalUpdateStaticSmoothingScheme",
+        init< >());
 
-    class_< BossakDisplacementSmoothingSchemeType,
-            bases< BaseSchemeType >,  boost::noncopyable >
-            (
-            "BossakDisplacementSmoothingScheme", init< double >() 
-            );
+    class_< BossakDisplacementSmoothingSchemeType, bases< BaseSchemeType >, boost::noncopyable >("BossakDisplacementSmoothingScheme",
+        init< double >());
+            
+    class_< DamEulerianConvectionDiffusionStrategy, bases< BaseSolvingStrategyType >, boost::noncopyable >("DamEulerianConvectionDiffusionStrategy", 
+        init < ModelPart&, LinearSolverType::Pointer, Parameters&, bool, int >());
 
 }
 
