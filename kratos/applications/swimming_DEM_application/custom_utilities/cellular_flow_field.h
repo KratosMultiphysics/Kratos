ccc09801dc843b80cfef810c113497eb78b384d1
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-17 18:01:11 +0000

    adding overrides to avoid warnings from CLANG

--- kratos/applications/swimming_DEM_application/custom_utilities/cellular_flow_field.h
+++ kratos/applications/swimming_DEM_application/custom_utilities/cellular_flow_field.h
@@ -84,20 +84,20 @@ void UpdateCoordinates(const double time, const vector<double>& coor, const int 
 
 /// Turn back information as a stemplate<class T, std::size_t dim> tring.
 
-virtual std::string Info() const
+virtual std::string Info() const override
 {
     return "";
 }
 
 /// Print information about this object.
 
-virtual void PrintInfo(std::ostream& rOStream) const
+virtual void PrintInfo(std::ostream& rOStream) const override
 {
 }
 
 /// Print object's data.
 
-virtual void PrintData(std::ostream& rOStream) const
+virtual void PrintData(std::ostream& rOStream) const override
 {
 }
 
