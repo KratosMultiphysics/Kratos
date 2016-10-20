decf8d0aeae04ca5283e136e80d764ab64f9fca5
croig <croig@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-20 10:02:18 +0000

    Added CONTACT_STRUCTURAL_MECHANICS_APPLICATION

--- kratos/scripts/build/nightly/configure_gcc.sh
+++ kratos/scripts/build/nightly/configure_gcc.sh
@@ -34,6 +34,7 @@ cmake .. \
 -DSTRUCTURAL_MECHANICS_APPLICATION=ON                                                           \
 -DSWIMMING_DEM_APPLICATION=ON                                                                   \
 -DTHERMO_MECHANICAL_APPLICATION=ON                                                              \
+-DCONTACT_STRUCTURAL_MECHANICS_APPLICATION=ON                                                   \
 -DMKL_SOLVERS_APPLICATION=OFF                                                                   \
 -DMKLSOLVER_INCLUDE_DIR=\"UNSET\"                                                               \
 -DMKLSOLVER_LIB_DIR=\"UNSET\"                                                                   \
