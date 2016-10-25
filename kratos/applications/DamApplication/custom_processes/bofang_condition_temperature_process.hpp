8847f393f747d288cecd00b8d3b5c58559875eb0
lgracia <lgracia@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-25 12:33:12 +0000

    Updating processes for the use of tables

--- kratos/applications/DamApplication/custom_processes/bofang_condition_temperature_process.hpp
+++ kratos/applications/DamApplication/custom_processes/bofang_condition_temperature_process.hpp
@@ -25,6 +25,8 @@ class BofangConditionTemperatureProcess : public Process
 public:
 
     KRATOS_CLASS_POINTER_DEFINITION(BofangConditionTemperatureProcess);
+    
+    typedef Table<double,double> TableType;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -50,8 +52,11 @@ public:
                 "Temperature_Amplitude"                            : 0.0,
                 "Day_Ambient_Temp"                                 : 1,
                 "Water_level"                                      : 0.0,
-                "Outer_temp"                                       : 1.0,
-                "Month"                                            : 1
+                "Water_level_Table"                                : 0,
+                "Outer_temp"                                       : 0.0,
+                "Outer_temp_Table"                                 : 0,
+                "Month"                                            : 1,
+                "Month_Table"                                      : 0
             }  )" );
         
         // Some values need to be mandatorily prescribed since no meaningful default value exist. For this reason try accessing to them
@@ -73,14 +78,25 @@ public:
         mheight = rParameters["Height_Dam"].GetDouble();
         mamplitude = rParameters["Temperature_Amplitude"].GetDouble();
         mday = rParameters["Day_Ambient_Temp"].GetInt();
-        
-        // TODO: PARAMETERS MUST BE GOT FROM THE TABLE
         mwater_level = rParameters["Water_level"].GetDouble();
         mouter_temp = rParameters["Outer_temp"].GetDouble();
         mmonth = rParameters["Month"].GetInt();
         mfreq = 0.52323;
-        
 
+        mtime_unit_converter = mr_model_part.GetProcessInfo()[TIME_UNIT_CONVERTER];
+        mTableIdWater = rParameters["Water_level_Table"].GetInt();
+        mTableIdOuter = rParameters["Outer_temp_Table"].GetInt();
+        mTableIdMonth = rParameters["Month_Table"].GetInt();
+        
+        if(mTableIdWater != 0)
+            mpTableWater = model_part.pGetTable(mTableIdWater);
+            
+        if(mTableIdOuter != 0)
+            mpTableOuter = model_part.pGetTable(mTableIdOuter);
+        
+        if(mTableIdMonth != 0)
+            mpTableMonth = model_part.pGetTable(mTableIdMonth);
+        
         KRATOS_CATCH("");
     }
 
@@ -94,19 +110,84 @@ public:
 
     void Execute()
     {
+        
+        KRATOS_TRY;
+        
+        Variable<double> var = KratosComponents< Variable<double> >::Get(mvariable_name);
+        const int nnodes = mr_model_part.GetMesh(mmesh_id).Nodes().size();
+        int direction;
+        
+        if( mgravity_direction == "X")
+            direction = 1;
+        else if( mgravity_direction == "Y")
+            direction = 2;
+        else if( mgravity_direction == "Z")
+            direction = 3;
+              
+                
+        if(nnodes != 0)
+        {
+            ModelPart::NodesContainerType::iterator it_begin = mr_model_part.GetMesh(mmesh_id).NodesBegin();
+        
+            #pragma omp parallel for
+            for(int i = 0; i<nnodes; i++)
+            {
+                ModelPart::NodesContainerType::iterator it = it_begin + i;
+                
+                if(mis_fixed)
+                {
+                    it->Fix(var);
+                }
+                
+                double aux = (mreference_coordinate + mwater_level) - it->Coordinate(direction);
+                if(aux >= 0.0)
+                {
+                    double aux1 = ((mbottom_temp-(msurface_temp*exp(-0.04*mheight)))/(1-(exp(-0.04*mheight))));
+                    double Temperature = (aux1+((msurface_temp-aux1)*(exp(-0.04*aux)))+(mamplitude*(exp(-0.018*aux))*(cos(mfreq*(mmonth-(mday/30.0)-2.15+(1.30*exp(-0.085*aux)))))));
+                    
+                    it->FastGetSolutionStepValue(var) = Temperature;
+                    
+                }
+                else
+                    it->FastGetSolutionStepValue(var) = mouter_temp;
+            }
+        }
+        
+        KRATOS_CATCH("");
     }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    void ExecuteInitialize()
+    void ExecuteInitializeSolutionStep()
     {
         
         KRATOS_TRY;
         
         Variable<double> var = KratosComponents< Variable<double> >::Get(mvariable_name);
         
-        const int nnodes = mr_model_part.GetMesh(mmesh_id).Nodes().size();
+        // Getting the values of table in case that it exist        
+        if(mTableIdWater != 0)
+        { 
+            double time = mr_model_part.GetProcessInfo()[TIME];
+            time = time/mtime_unit_converter;
+            mwater_level = mpTableWater->GetValue(time);
+        }
         
+        if(mTableIdOuter != 0)
+        { 
+            double time = mr_model_part.GetProcessInfo()[TIME];
+            time = time/mtime_unit_converter;
+            mouter_temp = mpTableOuter->GetValue(time);
+        }
+        
+        if(mTableIdMonth != 0)
+        { 
+            double time = mr_model_part.GetProcessInfo()[TIME];
+            time = time/mtime_unit_converter;
+            mmonth = mpTableMonth->GetValue(time);
+        }
+
+        const int nnodes = mr_model_part.GetMesh(mmesh_id).Nodes().size();
         int direction;
         
         if( mgravity_direction == "X")
@@ -186,7 +267,13 @@ protected:
     double mwater_level;
     double mouter_temp;
     double mfreq;
-
+    double mtime_unit_converter;
+    TableType::Pointer mpTableWater;
+    TableType::Pointer mpTableOuter;
+    TableType::Pointer mpTableMonth;
+    int mTableIdWater;
+    int mTableIdOuter;
+    int mTableIdMonth;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- kratos/applications/DamApplication/custom_processes/dam_uplift_condition_load_process.hpp
+++ kratos/applications/DamApplication/custom_processes/dam_uplift_condition_load_process.hpp
@@ -25,6 +25,8 @@ class DamUpliftConditionLoadProcess : public Process
 public:
 
     KRATOS_CLASS_POINTER_DEFINITION(DamUpliftConditionLoadProcess);
+    
+    typedef Table<double,double> TableType;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -53,7 +55,8 @@ public:
                 "Drains"                                                : false,
                 "Height_drain"                                          : 0.0,
                 "Distance"                                              : 0.0,
-                "Effectiveness"                                         : 0.0
+                "Effectiveness"                                         : 0.0,
+                "table"                                                 : 0
             }  )" );
             
         
@@ -95,9 +98,13 @@ public:
         mheight_drain = rParameters["Height_drain"].GetDouble();
         mdistance_drain = rParameters["Distance"].GetDouble();
         meffectiveness_drain = rParameters["Effectiveness"].GetDouble();
-        
-        // TODO: PARAMETERS MUST BE GOT FROM THE TABLE
         mwater_level = rParameters["Water_level"].GetDouble();
+        
+        mtime_unit_converter = mr_model_part.GetProcessInfo()[TIME_UNIT_CONVERTER];
+        mTableId = rParameters["table"].GetInt();
+        
+        if(mTableId != 0)
+            mpTable = model_part.pGetTable(mTableId);
 
         KRATOS_CATCH("");
     }
@@ -117,6 +124,122 @@ public:
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     void ExecuteInitialize()
+{
+        
+        KRATOS_TRY;
+        
+        //Defining necessary variables
+        Variable<double> var = KratosComponents< Variable<double> >::Get(mvariable_name);
+        const int nnodes = mr_model_part.GetMesh(mmesh_id).Nodes().size();
+        boost::numeric::ublas::bounded_matrix<double,3,3> RotationMatrix;
+                
+        // Computing the rotation matrix accoding with the introduced points by the user
+        this->CalculateRotationMatrix(RotationMatrix);
+        array_1d<double,3> newCoordinate;
+        array_1d<double,3> auxiliar_vector;
+        array_1d<double,3> reference_vector;
+
+        // Gravity direction for computing the hydrostatic pressure
+        int direction;
+        
+        if( mgravity_direction == "X")
+            direction = 1;
+        else if( mgravity_direction == "Y")
+            direction = 2;
+        else if( mgravity_direction == "Z")
+            direction = 3;
+            
+        // Computing the reference vector (coordinates)    
+        reference_vector = prod(RotationMatrix,mx_0);
+                
+        if(nnodes != 0)
+        {
+            ModelPart::NodesContainerType::iterator it_begin = mr_model_part.GetMesh(mmesh_id).NodesBegin();
+            
+            double ref_coord = mreference_coordinate + mwater_level;
+            
+            if( mdrain == true)
+            {
+				double coefficient_effectiveness = 1.0 - meffectiveness_drain;
+				double aux_drain = coefficient_effectiveness *(mwater_level - mheight_drain)* ((mbase_dam-mdistance_drain)/mbase_dam) + mheight_drain;
+
+				#pragma omp parallel for
+				for(int i = 0; i<nnodes; i++)
+				{
+					ModelPart::NodesContainerType::iterator it = it_begin + i;
+                    
+                    auxiliar_vector.resize(3,false);                    
+                    auxiliar_vector[0] = it->Coordinate(1);
+                    auxiliar_vector[1] = it->Coordinate(2);
+                    auxiliar_vector[2] = it->Coordinate(3);
+                    
+                    // Computing the new coordinates                                        
+                    newCoordinate = prod(RotationMatrix,auxiliar_vector);
+
+					if(mis_fixed)
+					{
+						it->Fix(var);
+					}
+		
+                    // We compute the first part of the uplift law 
+                    muplift_pressure = (mspecific*((ref_coord-aux_drain)- (it->Coordinate(direction))))*(1.0-((1.0/(mdistance_drain))*(fabs( (newCoordinate(0)) - reference_vector(0))))) + mspecific*aux_drain;
+                    
+                    // If uplift pressure is greater than the limit we compute the second part and we update the value
+                        if(muplift_pressure <= mspecific*aux_drain)
+                        {
+                            muplift_pressure = (mspecific*((mreference_coordinate+aux_drain)- (it->Coordinate(direction))))*(1.0-((1.0/(mbase_dam - mdistance_drain))*(fabs( (newCoordinate(0)) - (reference_vector(0)+mdistance_drain)))));
+                        }
+                        
+					if(muplift_pressure<0.0)
+					{
+						it->FastGetSolutionStepValue(var)=0.0;
+					}
+					else
+					{
+						it->FastGetSolutionStepValue(var) = muplift_pressure;
+					}
+				}
+					
+			}
+			else
+			{
+				#pragma omp parallel for
+				for(int i = 0; i<nnodes; i++)
+				{
+					ModelPart::NodesContainerType::iterator it = it_begin + i;
+                    
+                    auxiliar_vector.resize(3,false);                    
+                    auxiliar_vector[0] = it->Coordinate(1);
+                    auxiliar_vector[1] = it->Coordinate(2);
+                    auxiliar_vector[2] = it->Coordinate(3);
+                    
+                    newCoordinate = prod(RotationMatrix,auxiliar_vector);
+
+					if(mis_fixed)
+					{
+						it->Fix(var);
+					}
+				
+					muplift_pressure = (mspecific*(ref_coord- (it->Coordinate(direction))))*(1.0-((1.0/mbase_dam)*(fabs(newCoordinate(0)-reference_vector(0)))));
+                    
+					if(muplift_pressure<0.0)
+					{
+						it->FastGetSolutionStepValue(var)=0.0;
+					}
+					else
+					{
+						it->FastGetSolutionStepValue(var) = muplift_pressure;
+					}
+				}
+			}            
+        }
+        
+        KRATOS_CATCH("");
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    void ExecuteInitializeSolutionStep()
     {
         
         KRATOS_TRY;
@@ -125,6 +248,16 @@ public:
         Variable<double> var = KratosComponents< Variable<double> >::Get(mvariable_name);
         const int nnodes = mr_model_part.GetMesh(mmesh_id).Nodes().size();
         boost::numeric::ublas::bounded_matrix<double,3,3> RotationMatrix;
+        
+        // Getting the values of table in case that it exist        
+        if(mTableId != 0)
+        { 
+            double time = mr_model_part.GetProcessInfo()[TIME];
+            time = time/mtime_unit_converter;
+            mwater_level = mpTable->GetValue(time);
+        }
+        
+        // && mr_model_part.GetProcessInfo()[TIME] >= 1.0
         
         // Computing the rotation matrix accoding with the introduced points by the user
         this->CalculateRotationMatrix(RotationMatrix);
@@ -311,7 +444,10 @@ protected:
     double muplift_pressure;
     Vector mx_0;
     Vector mx_1;
-    Vector mx_2;   
+    Vector mx_2;
+    double mtime_unit_converter;
+    TableType::Pointer mpTable;
+    int mTableId;   
     
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
