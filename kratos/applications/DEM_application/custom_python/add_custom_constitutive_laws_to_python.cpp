69c799e710a22e2b8dad57eb9a3a624aac183348
jirazabal <jirazabal@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-10-28 08:30:34 +0000

    Big commit. Now the Clusters orientetion is defined by quaternions, no Euler Angles

--- kratos/applications/DEM_application/custom_python/add_custom_constitutive_laws_to_python.cpp
+++ kratos/applications/DEM_application/custom_python/add_custom_constitutive_laws_to_python.cpp
@@ -118,8 +118,8 @@ namespace Kratos {
             class_<DEM_KDEM, bases< DEMContinuumConstitutiveLaw >, boost::noncopyable >("DEM_KDEM",init<>())
                     ;
 
-			class_<DEM_sintering_continuum, bases< DEMContinuumConstitutiveLaw >, boost::noncopyable >("DEM_sintering_continuum", init<>())
-				    ;
+            class_<DEM_sintering_continuum, bases< DEMContinuumConstitutiveLaw >, boost::noncopyable >("DEM_sintering_continuum", init<>())
+                    ;
             
             class_<DEM_KDEMFabric, bases< DEM_KDEM >, boost::noncopyable >("DEM_KDEMFabric",init<>())
                     ;
