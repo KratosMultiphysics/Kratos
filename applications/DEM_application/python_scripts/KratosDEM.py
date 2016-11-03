5a703fa5c2365374c8ce5d243f6e36c60af8b297
farrufat <farrufat@cimne.upc.edu>
2016-11-03 17:09:12 +0000

    no need to reoder on OMP

--- applications/DEM_application/python_scripts/KratosDEM.py
+++ applications/DEM_application/python_scripts/KratosDEM.py
@@ -31,7 +31,7 @@ else:
     #def model_part_reader(modelpart, a=0, b=0, c=0):
     #    return ModelPartIO(modelpart)
     def model_part_reader(modelpart, nodeid=0, elemid=0, condid=0):
-        return ReorderConsecutiveFromGivenIdsModelPartIO(modelpart, nodeid, elemid, condid)
+        return ModelPartIO(modelpart)
     
 
 # TODO: Ugly fix. Change it. I don't like this to be in the main...
