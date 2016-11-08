b1edb10ca15dda4868851a734094eac224755140
latorre <latorre@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-11-08 15:00:04 +0000

    Ice implementations

--- kratos/applications/DEM_application/custom_elements/ice_continuum_particle.cpp
+++ kratos/applications/DEM_application/custom_elements/ice_continuum_particle.cpp
@@ -1,39 +1,18 @@
 //
-// Author: Salva Latorre    latorre@cimne.upc.edu
+// Author: Salva Latorre, latorre@cimne.upc.edu
 //
 
 // System includes
 #include <string>
 #include <iostream>
 #include <iomanip> // to improve std::cout precision
+#include <cmath> // for the sine in the wave equation
 
 // Project includes
 #include "ice_continuum_particle.h"
 
 namespace Kratos {
-    
-    void IceContinuumParticle::Initialize(const ProcessInfo& r_process_info) {   
-        
-        SphericContinuumParticle::Initialize(r_process_info);
 
-        SetMass(GetDensity() * CalculateVolume());
-    }
-
-    void IceContinuumParticle::MemberDeclarationFirstStep(const ProcessInfo& r_process_info) {
-        SphericContinuumParticle::MemberDeclarationFirstStep(r_process_info);
-    }
-
-    double IceContinuumParticle::CalculateVolume()
-    {
-        return 4.0 * KRATOS_M_PI_3 * mRadius * mRadius * mRadius;
-    }
-
-    void IceContinuumParticle::SetInteractionRadius(const double radius) {}
-
-    double IceContinuumParticle::GetInteractionRadius()
-    {
-        return mSearchRadius;
-    }
     void IceContinuumParticle::ComputeAdditionalForces(array_1d<double, 3>& externally_applied_force,
                                                   array_1d<double, 3>& externally_applied_moment,
                                                   const ProcessInfo& r_process_info,
@@ -46,13 +25,14 @@ namespace Kratos {
                                                          r_process_info,
                                                          gravity);
         
-        ComputeIceForces(this, externally_applied_force, gravity);
+        ComputeIceForces(this, externally_applied_force, r_process_info, gravity);
 
         KRATOS_CATCH("")
     }
     
     void IceContinuumParticle::ComputeIceForces(SphericContinuumParticle* continuum_particle,
                                                 array_1d<double, 3>& externally_applied_force,
+                                                const ProcessInfo& r_process_info,
                                                 const array_1d<double,3>& gravity) {
         KRATOS_TRY
         
@@ -62,8 +42,14 @@ namespace Kratos {
         array_1d<double, 3> velocity = continuum_particle->GetGeometry()[0].FastGetSolutionStepValue(VELOCITY);
         double velocity_module = 1.0; //sqrt(velocity[0] * velocity[0] + velocity[1] * velocity[1] * velocity[2] * velocity[2]); // This would correspond to a quadratic drag law
         
-        double water_level = 0.0; // We will always assume this value 
-        double Cd = 2.0; // 3.0; // We can experiment a little bit with this
+        double Cd = 2.0; // 3.0; // Drag coefficient. We can experiment a little bit with this number
+        double water_level = 0.0; // We will always assume this value
+        /*
+        double amplitude = 1.0;
+        double wave_celerity = 1.0;
+        double wavelength = 8.0;
+        double water_level = amplitude * sin ((KRATOS_M_PI * ((continuum_particle->GetGeometry()[0].Coordinates()[0]) - wave_celerity * r_process_info[TIME])) / wavelength);
+        */
         
         // Adding buoyancy and drag. Now a linear (with the velocity) drag law is implemented
         if (Z_coord < water_level) {
