8dad0abd27e3f36a963853af25decf666cd553e4
rrossi <rrossi@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-11-23 14:38:37 +0000

    adding the option to not improve the mesh, with a default value so not to break old scripts

--- kratos/applications/FluidDynamicsApplication/python_scripts/refine_embedded_object.py
+++ kratos/applications/FluidDynamicsApplication/python_scripts/refine_embedded_object.py
@@ -9,7 +9,7 @@ class MeshAdaptor:
     def __init__(self,model_part):
         self.model_part = model_part
         
-    def DoRefinement(self,target_distance):
+    def DoRefinement(self,target_distance, improve_quality=True):
         
         for node in self.model_part.Nodes:
             node.Set(TO_REFINE,False)
@@ -32,6 +32,7 @@ class MeshAdaptor:
         nodal_neighbour_search = FindNodalNeighboursProcess(self.model_part,20,20)
         nodal_neighbour_search.Execute()
 
+        print("before refinement")
         ###perform the refinement
         Refine = LocalRefineTetrahedraMesh(self.model_part)
         refine_on_reference = False;
@@ -41,28 +42,30 @@ class MeshAdaptor:
         ###recompute the neighbours since they changed due to the creation of new nodes
         nodal_neighbour_search.ClearNeighbours()
         nodal_neighbour_search.Execute()
-        
+        print("after refinement")
         ##imporving mesh quality after refining
         #OptimizeQuality(ModelPart& r_model_part,int simIter, int iterations ,
                          #bool processByNode, bool processByFace, bool processByEdge,
                          #bool saveToFile, bool removeFreeVertexes ,
                          #bool evaluateInParallel , bool reinsertNodes , bool debugMode, int minAngle)
-        reconnector = TetrahedraReconnectUtility(self.model_part)
-        simIter = 2
-        iterations = 2
-        ProcessByNode = False
-        ProcessByFace = True
-        ProcessByEdge = True
-        saveToFile = False
-        removeFreeVertexes = False
-        evaluateInParallel = True
-        reInsertNodes = False
-        debugMode = False
-        minAngle = 5
+                         
+        if improve_quality == True:
+            reconnector = TetrahedraReconnectUtility(self.model_part)
+            simIter = 2
+            iterations = 2
+            ProcessByNode = False
+            ProcessByFace = True
+            ProcessByEdge = True
+            saveToFile = False
+            removeFreeVertexes = False
+            evaluateInParallel = True
+            reInsertNodes = False
+            debugMode = False
+            minAngle = 5
 
 
-        reconnector.setBlockSize(2048)
-        reconnector.OptimizeQuality(self.model_part, simIter, iterations, ProcessByNode, ProcessByFace, ProcessByEdge, saveToFile, removeFreeVertexes, evaluateInParallel, reInsertNodes, debugMode,minAngle)
-        meshIsValid = reconnector.EvaluateQuality()
-        reconnector.FinalizeOptimization(removeFreeVertexes)
+            reconnector.setBlockSize(2048)
+            reconnector.OptimizeQuality(self.model_part, simIter, iterations, ProcessByNode, ProcessByFace, ProcessByEdge, saveToFile, removeFreeVertexes, evaluateInParallel, reInsertNodes, debugMode,minAngle)
+            meshIsValid = reconnector.EvaluateQuality()
+            reconnector.FinalizeOptimization(removeFreeVertexes)
                 
