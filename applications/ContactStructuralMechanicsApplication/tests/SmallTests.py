6e5b8875239c5ed893ccc1b75bd4bd036dda5b73
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2016-11-30 15:07:18 +0000

    Adding test for the double LM

--- applications/ContactStructuralMechanicsApplication/tests/SmallTests.py
+++ applications/ContactStructuralMechanicsApplication/tests/SmallTests.py
@@ -48,6 +48,9 @@ class BasicCATest(StructuralMechanichsTestFactory):
 class SolidCATest(StructuralMechanichsTestFactory):
     file_name = "CA_test/solid_CA_test"
     
+class HyperSimplePatchTestContact(StructuralMechanichsTestFactory):
+    file_name = "contact_test_2D/hyper_simple_patch_test"
+    
 class SimplePatchTestContact(StructuralMechanichsTestFactory):
     file_name = "contact_test_2D/simple_patch_test"
     
