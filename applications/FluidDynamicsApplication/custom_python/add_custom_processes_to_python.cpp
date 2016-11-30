a7c67eac4de80df298e3fed9f2014714ee158f4b
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2016-11-30 13:57:30 +0000

    distance_modification_process added to python.

--- applications/FluidDynamicsApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/FluidDynamicsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -63,6 +63,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "custom_processes/spalart_allmaras_turbulence_model.h"
 #include "custom_processes/Boundary_Windkessel_model.h"
 #include "custom_processes/stokes_initialization_process.h"
+#include "custom_processes/distance_modification_process.h"
 #include "spaces/ublas_space.h"
 
 #include "solving_strategies/strategies/solving_strategy.h"
@@ -84,25 +85,25 @@ void AddCustomProcessesToPython()
 
     typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
 
-    class_<SpalartAllmarasTurbulenceModel< SparseSpaceType, LocalSpaceType, LinearSolverType >, bases<Process>, boost::noncopyable >
+    class_< SpalartAllmarasTurbulenceModel< SparseSpaceType, LocalSpaceType, LinearSolverType >, bases<Process>, boost::noncopyable >
     ("SpalartAllmarasTurbulenceModel", init < ModelPart&, LinearSolverType::Pointer, unsigned int, double, unsigned int, bool, unsigned int>())
     .def("ActivateDES", &SpalartAllmarasTurbulenceModel< SparseSpaceType, LocalSpaceType, LinearSolverType >::ActivateDES)
     .def("AdaptForFractionalStep", &SpalartAllmarasTurbulenceModel< SparseSpaceType, LocalSpaceType, LinearSolverType >::AdaptForFractionalStep)
     ;
 
-    class_< StokesInitializationProcess<SparseSpaceType, LocalSpaceType, LinearSolverType>, bases<Process>, boost::noncopyable >
-            ("StokesInitializationProcess",init<ModelPart::Pointer, LinearSolverType::Pointer, unsigned int, const Kratos::Variable<int>& >())
-            .def("SetConditions",&StokesInitializationProcess<SparseSpaceType, LocalSpaceType, LinearSolverType>::SetConditions);
-
-    class_<WindkesselModel, bases<Process>, boost::noncopyable >
-    ("WindkesselModel", init < ModelPart&>())
+    class_< StokesInitializationProcess< SparseSpaceType, LocalSpaceType, LinearSolverType >, bases<Process>, boost::noncopyable >
+    ("StokesInitializationProcess",init<ModelPart::Pointer, LinearSolverType::Pointer, unsigned int, const Kratos::Variable<int>& >())
+    .def("SetConditions",&StokesInitializationProcess<SparseSpaceType, LocalSpaceType, LinearSolverType>::SetConditions)
     ;
 
+    class_< WindkesselModel, bases<Process>, boost::noncopyable >
+    ("WindkesselModel", init < ModelPart&>())
+    ;
+    
+    class_< DistanceModificationProcess, bases<Process>, boost::noncopyable >
+    ("DistanceModificationProcess",init < ModelPart&, const bool >())
+    ;
 }
-
-
-
-
 
 } // namespace Python.
 
