c33e99c3fbcc3e6467d1af4e45a58ad0144ada13
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-12-03 15:59:40 +0000

    latest update of plot format

--- kratos/applications/swimming_DEM_application/python_scripts/chandelier/plot_errors_decreasing_time_step.py
+++ kratos/applications/swimming_DEM_application/python_scripts/chandelier/plot_errors_decreasing_time_step.py
@@ -85,17 +85,33 @@ dts = [DEM_parameters.MaxTimeStep / 2 ** k for k in range(n_doubling)]
 #E   = [[Radius([positions_table[m][k][0] - x_final, positions_table[m][k][1] - y_final]) / r_final for k in range(n_doubling)] for m in range(len(r_table))]
 index_short = len([value for value in dts if value < 2])
 
+
+#from matplotlib import rc
+#rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+#rc('text', usetex=True)
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+font_size = 40
+ax.tick_params(axis='x', labelsize=0.75 * font_size)
+ax.tick_params(axis='y', labelsize=0.75 * font_size)
+ax.tick_params(axis='both', which='major', pad=15)
+fig.tight_layout()
+
 m = 0
+
 for line in reversed(E):
     if m == n_exponentials - 1:
         plt.plot(dts, line, '--', marker='o', markersize=10, label='Daitche', color='k')
     else:
         plt.plot(dts, line, '-o', markersize=10, label='Hinsberg, m = ' + str(10 - m), linewidth=0.5*(11-m), ms=10)
     m += 1
-plt.xlabel('$\mathrm{d}t$')
-plt.ylabel('$E$')
-plt.legend(loc='lower right')
+
+
+plt.xlabel('$\mathrm{d}t$', fontsize = font_size)
+plt.ylabel('$E$', fontsize = font_size)
+plt.legend(loc='upper left',prop={'size':25})
 plt.semilogx()
 plt.semilogy()
-plt.savefig('EvsDt.pdf')
+plt.savefig('EvsDt.pdf', dpi = 600, bbox_inches = 'tight')
 plt.show()
