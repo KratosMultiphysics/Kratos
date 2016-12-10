2fa4b0265eafd54dc90a6283d5f864d234e75213
gcasas <gcasas@cimne.upc.edu>
2016-12-10 21:45:23 +0000

    adding conditions for Laplacian recovery (now they give a seg fault)

---
+++ applications/swimming_DEM_application/custom_conditions/calculate_laplacian_simplex_condition.h
@@ -0,0 +1,430 @@
+//    |  /           | 
+//    ' /   __| _` | __|  _ \   __| 
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/ 
+//                   Multi-Physics  
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Guillermo Casas gcasas@gmail.com
+//
+
+#ifndef KRATOS_CALCULATE_LAPLACIAN_SIMPLEX_CONDITION_H
+#define KRATOS_CALCULATE_LAPLACIAN_SIMPLEX_CONDITION_H
+
+// System includes
+#include <string>
+#include <iostream>
+
+
+// External includes
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/serializer.h"
+#include "includes/condition.h"
+#include "includes/process_info.h"
+
+// Application includes
+#include "../FluidDynamicsApplication/fluid_dynamics_application_variables.h"
+#include "includes/deprecated_variables.h"
+#include "includes/cfd_variables.h"
+
+//Other Applications includes
+#include "../../applications/FluidDynamicsApplication/fluid_dynamics_application.h"
+#include "../../applications/FluidDynamicsApplication/custom_conditions/monolithic_wall_condition.h"
+
+namespace Kratos
+{
+///@addtogroup FluidDynamicsApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/// Implements a wall condition for the laplacian recovery.
+
+template< unsigned int TDim, unsigned int TNumNodes = TDim >
+class KRATOS_API(SWIMMING_DEM_APPLICATION) ComputeLaplacianSimplexCondition : public Condition
+{
+public:
+    ///@name Type Definitions
+    ///@{
+    KRATOS_CLASS_POINTER_DEFINITION(ComputeLaplacianSimplexCondition);
+
+    typedef Node < 3 > NodeType;
+
+    typedef Properties PropertiesType;
+
+    typedef Geometry<NodeType> GeometryType;
+
+    typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
+
+    typedef Vector VectorType;
+
+    typedef Matrix MatrixType;
+
+    typedef std::size_t IndexType;
+
+    typedef std::size_t SizeType;
+
+    typedef std::vector<std::size_t> EquationIdVectorType;
+
+    typedef std::vector< Dof<double>::Pointer > DofsVectorType;
+
+    typedef PointerVectorSet<Dof<double>, IndexedObject> DofsArrayType;
+
+    typedef VectorMap<IndexType, DataValueContainer> SolutionStepsConditionalDataContainerType;
+    /// Pointer definition of ComputeLaplacianSimplexCondition
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    /** Admits an Id as a parameter.
+      @param NewId Index for the new condition
+      */
+    ComputeLaplacianSimplexCondition(IndexType NewId = 0):
+        Condition(NewId)
+    {
+    }
+
+    /// Constructor using an array of nodes
+    /**
+     @param NewId Index of the new condition
+     @param ThisNodes An array containing the nodes of the new condition
+     */
+    ComputeLaplacianSimplexCondition(IndexType NewId,
+                            const NodesArrayType& ThisNodes):
+        Condition(NewId,ThisNodes)
+    {
+    }
+
+    /// Constructor using Geometry
+    /**
+     @param NewId Index of the new condition
+     @param pGeometry Pointer to a geometry object
+     */
+    ComputeLaplacianSimplexCondition(IndexType NewId,
+                            GeometryType::Pointer pGeometry):
+        Condition(NewId,pGeometry)
+    {
+    }
+
+    /// Constructor using Properties
+    /**
+     @param NewId Index of the new element
+     @param pGeometry Pointer to a geometry object
+     @param pProperties Pointer to the element's properties
+     */
+    ComputeLaplacianSimplexCondition(IndexType NewId,
+                            GeometryType::Pointer pGeometry,
+                            PropertiesType::Pointer pProperties):
+        Condition(NewId,pGeometry,pProperties)
+    {
+    }
+
+    /// Copy constructor.
+    ComputeLaplacianSimplexCondition(ComputeLaplacianSimplexCondition const& rOther):
+        Condition(rOther)
+    {
+    }
+
+    /// Destructor.
+    virtual ~ComputeLaplacianSimplexCondition() {}
+
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /// Assignment operator
+    ComputeLaplacianSimplexCondition & operator=(ComputeLaplacianSimplexCondition const& rOther)
+    {
+        Condition::operator=(rOther);
+
+        return *this;
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /// Create a new ComputeLaplacianSimplexCondition object.
+    /**
+      @param NewId Index of the new condition
+      @param ThisNodes An array containing the nodes of the new condition
+      @param pProperties Pointer to the element's properties
+      */
+    Condition::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const
+    {
+        return Condition::Pointer(new ComputeLaplacianSimplexCondition(NewId, Condition::GetGeometry().Create(ThisNodes), pProperties));
+    }
+    Condition::Pointer Create(IndexType NewId,
+                           GeometryType::Pointer pGeom,
+                           PropertiesType::Pointer pProperties) const
+    {
+        return boost::make_shared< ComputeLaplacianSimplexCondition >(NewId, pGeom, pProperties);
+    }
+
+    /// Return local contributions of the correct size, filled with zeros (for compatibility with time schemes).
+    /** The actual local contributions are computed in the Damping functions
+      @see CalculateLocalVelocityContribution
+      */
+    void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix,
+                                      VectorType& rRightHandSideVector,
+                                      ProcessInfo& rCurrentProcessInfo)
+    {
+        const SizeType BlockSize = TDim;
+        const SizeType LocalSize = BlockSize * TNumNodes;
+
+        if (rLeftHandSideMatrix.size1() != LocalSize)
+            rLeftHandSideMatrix.resize(LocalSize,LocalSize);
+
+        if (rRightHandSideVector.size() != LocalSize)
+            rRightHandSideVector.resize(LocalSize);
+
+        noalias(rLeftHandSideMatrix) = ZeroMatrix(LocalSize,LocalSize);
+        noalias(rRightHandSideVector) = ZeroVector(LocalSize);
+
+    }
+
+    /// Return a matrix of the correct size, filled with zeros (for compatibility with time schemes).
+    /** The actual local contributions are computed in the Damping functions
+      @see DampingMatrix
+      */
+    void CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix,
+                                       ProcessInfo& rCurrentProcessInfo)
+    {
+        const SizeType BlockSize = TDim;
+        const SizeType LocalSize = BlockSize * TNumNodes;
+
+        if (rLeftHandSideMatrix.size1() != LocalSize)
+            rLeftHandSideMatrix.resize(LocalSize,LocalSize);
+
+        noalias(rLeftHandSideMatrix) = ZeroMatrix(LocalSize,LocalSize);
+    }
+
+    /// Return local right hand side of the correct size, filled with zeros (for compatibility with time schemes).
+    /** The actual local contributions are computed in the Damping functions
+      @see CalculateLocalVelocityContribution
+      */
+    void CalculateRightHandSide(VectorType& rRightHandSideVector,
+                                        ProcessInfo& rCurrentProcessInfo)
+    {
+        const unsigned int BlockSize = TDim;
+        const SizeType LocalSize = BlockSize * TNumNodes;
+
+        if (rRightHandSideVector.size() != LocalSize)
+            rRightHandSideVector.resize(LocalSize);
+
+        noalias(rRightHandSideVector) = ZeroVector(LocalSize);
+    }
+
+
+    /// Provides the global indices for each one of this element's local rows.
+    /** This determines the elemental equation ID vector for all elemental DOFs
+     * @param rResult A vector containing the global Id of each row
+     * @param rCurrentProcessInfo the current process info object (unused)
+     */
+    void EquationIdVector(EquationIdVectorType& rResult,
+                                  ProcessInfo& rCurrentProcessInfo);
+
+
+    /// Returns a list of the element's Dofs
+    /**
+     * @param ElementalDofList the list of DOFs
+     * @param rCurrentProcessInfo the current process info instance
+     */
+    void GetDofList(DofsVectorType& ConditionDofList,
+                            ProcessInfo& CurrentProcessInfo);
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    virtual std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "ComputeLaplacianSimplexCondition" << TDim << "D";
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    virtual void PrintInfo(std::ostream& rOStream) const
+    {
+        rOStream << "ComputeLaplacianSimplexCondition";
+    }
+
+    /// Print object's data.
+    virtual void PrintData(std::ostream& rOStream) const {}
+
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    virtual void save(Serializer& rSerializer) const
+    {
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition );
+    }
+
+    virtual void load(Serializer& rSerializer)
+    {
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Condition );
+    }
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+
+    ///@}
+
+}; // Class ComputeLaplacianSimplexCondition
+
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+/// input stream function
+template< unsigned int TDim, unsigned int TNumNodes >
+inline std::istream& operator >> (std::istream& rIStream,
+                                  ComputeLaplacianSimplexCondition<TDim,TNumNodes>& rThis)
+{
+    return rIStream;
+}
+
+/// output stream function
+template< unsigned int TDim, unsigned int TNumNodes >
+inline std::ostream& operator << (std::ostream& rOStream,
+                                  const ComputeLaplacianSimplexCondition<TDim,TNumNodes>& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+
+///@}
+
+///@} addtogroup block
+
+
+}  // namespace Kratos.
+
+#endif // KRATOS_CALCULATE_LAPLACIAN_SIMPLEX_CONDITION_H
