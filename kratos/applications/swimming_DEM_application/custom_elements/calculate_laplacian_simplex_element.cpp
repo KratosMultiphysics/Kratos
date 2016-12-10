6085718ad16c3c4433dd59c5ecb166f85b2da9cb
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-12-10 21:46:25 +0000

    adding element for laplacian recovery (it runs with the fluid condition but gives the wrong answer)

---
+++ kratos/applications/swimming_DEM_application/custom_elements/calculate_laplacian_simplex_element.cpp
@@ -0,0 +1,87 @@
+#include "swimming_DEM_application.h"
+#include "calculate_laplacian_simplex_element.h"
+
+namespace Kratos
+{
+
+template <>
+void ComputeLaplacianSimplex<2,3>::CalculateWeights(ShapeFunctionDerivativesArrayType& rDN_DX,
+        Matrix& rNContainer,
+        Vector& rGaussWeights)
+{
+
+  const GeometryType& rGeom = this->GetGeometry();
+  Vector DetJ;
+  rGeom.ShapeFunctionsIntegrationPointsGradients(rDN_DX, DetJ, GeometryData::GI_GAUSS_2);
+  rNContainer = rGeom.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
+  const GeometryType::IntegrationPointsArrayType& IntegrationPoints = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_2);
+
+  rGaussWeights.resize(rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2), false);
+
+  for (unsigned int g = 0; g < rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2); g++)
+      rGaussWeights[g] = DetJ[g] * IntegrationPoints[g].Weight();
+}
+
+
+template <>
+void ComputeLaplacianSimplex<3,4>::CalculateWeights(ShapeFunctionDerivativesArrayType& rDN_DX,
+        Matrix& rNContainer,
+        Vector& rGaussWeights)
+{
+    const GeometryType& rGeom = this->GetGeometry();
+    Vector DetJ;
+    rGeom.ShapeFunctionsIntegrationPointsGradients(rDN_DX, DetJ, GeometryData::GI_GAUSS_2);
+    rNContainer = rGeom.ShapeFunctionsValues(GeometryData::GI_GAUSS_2);
+    const GeometryType::IntegrationPointsArrayType& IntegrationPoints = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_2);
+
+    rGaussWeights.resize(rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2), false);
+
+    for (unsigned int g = 0; g < rGeom.IntegrationPointsNumber(GeometryData::GI_GAUSS_2); g++)
+        rGaussWeights[g] = DetJ[g] * IntegrationPoints[g].Weight();
+}
+
+template <>
+void ComputeLaplacianSimplex<2,3>::AddRHSLaplacian(VectorType& F,
+                             const boost::numeric::ublas::bounded_matrix<double, 3, 2>& rShapeDeriv,
+                             const double Weight)
+{
+    double Coef = Weight;
+    array_1d<double, 3 > Velocity;
+
+    int LocalIndex = 0;
+    int LocalNodalIndex = 0;
+
+    for (unsigned int iNode = 0; iNode < 3; ++iNode)
+    {
+        Velocity = this->GetGeometry()[iNode].FastGetSolutionStepValue(VELOCITY);
+        for (unsigned int d = 0; d < 2; ++d)
+        {
+            F[LocalIndex++] -= Coef * rShapeDeriv(LocalNodalIndex, d) * Velocity[d] * rShapeDeriv(iNode, d);
+        }
+        LocalNodalIndex++;
+    }
+}
+
+template <>
+void ComputeLaplacianSimplex<3,4>::AddRHSLaplacian(VectorType& F,
+                             const boost::numeric::ublas::bounded_matrix<double, 4, 3>& rShapeDeriv,
+                             const double Weight)
+{
+    double Coef = Weight;
+    array_1d<double, 3 > Velocity;
+
+    int LocalIndex = 0;
+    int LocalNodalIndex = 0;
+
+    for (unsigned int iNode = 0; iNode < 4; ++iNode)
+    {
+        Velocity = this->GetGeometry()[iNode].FastGetSolutionStepValue(VELOCITY);
+        for (unsigned int d = 0; d < 3; ++d)
+        {
+            F[LocalIndex++] -= Coef * rShapeDeriv(LocalNodalIndex, d) * Velocity[d] * rShapeDeriv(iNode, d);
+        }
+        LocalNodalIndex++;
+    }
+}
+
+}
