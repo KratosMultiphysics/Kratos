2eff0b61f99f2d6be29fa1758ea2d96fe1feb78a
jirazabal <jirazabal@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-12-16 11:56:42 +0000

    New ORIENTATION input, quaternions available in .mdpa

--- kratos/applications/DEM_application/custom_elements/cluster3D.h
+++ kratos/applications/DEM_application/custom_elements/cluster3D.h
@@ -57,7 +57,7 @@ namespace Kratos
         virtual void InitializeSolutionStep(ProcessInfo& r_process_info){};
         virtual void FinalizeSolutionStep(ProcessInfo& r_process_info){};
         virtual void CustomInitialize(ProcessInfo& r_process_info);
-        virtual void SetOrientation(const double& OrientationReal, const array_1d<double, 3>& OrientationImag);
+        virtual void SetOrientation(const Quaternion<double> Orientation);
         virtual void CreateParticles(ParticleCreatorDestructor* p_creator_destructor, ModelPart& dem_model_part, PropertiesProxy* p_fast_properties);
         virtual void UpdatePositionOfSpheres();
         virtual void UpdateLinearDisplacementAndVelocityOfSpheres();
