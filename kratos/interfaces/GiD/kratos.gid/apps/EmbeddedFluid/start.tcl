c01c00e192f899e8c81d64171c8d6f4e770548a6
rzorrilla <rzorrilla@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-12-16 19:14:04 +0000

    Temporary solution to allow embedded distance computation.

--- kratos/interfaces/GiD/kratos.gid/apps/EmbeddedFluid/start.tcl
+++ kratos/interfaces/GiD/kratos.gid/apps/EmbeddedFluid/start.tcl
@@ -11,23 +11,23 @@ proc ::EmbeddedFluid::Init { } {
     variable dir
     variable prefix
     variable attributes
-    
+
     apps::LoadAppById "Fluid"
-    
+
     set dir [apps::getMyDir "EmbeddedFluid"]
     set attributes [dict create]
-    
+
     set prefix EMBFL
-    
+
     set ::Model::ValidSpatialDimensions [list 3D]
     spdAux::SetSpatialDimmension "3D"
-    
+
     # Allow to open the tree
     set ::spdAux::TreeVisibility 1
-    
+
     dict set attributes UseIntervals 0
     if {$::Kratos::kratos_private(DevMode) eq "dev"} {dict set attributes UseIntervals 1}
-    
+
     LoadMyFiles
     Kratos::AddRestoreVar "::GidPriv(DuplicateEntities)"
     set ::GidPriv(DuplicateEntities) 1
@@ -37,7 +37,7 @@ proc ::EmbeddedFluid::Init { } {
 
 proc ::EmbeddedFluid::LoadMyFiles { } {
     variable dir
-    
+
     uplevel #0 [list source [file join $dir xml GetFromXML.tcl]]
     uplevel #0 [list source [file join $dir xml ImportWindowController.tcl]]
     uplevel #0 [list source [file join $dir xml BoundingBoxWindowController.tcl]]
@@ -53,15 +53,15 @@ proc ::EmbeddedFluid::GetAttribute {name} {
 }
 
 proc ::EmbeddedFluid::CustomToolbarItems { } {
-    Kratos::ToolbarAddItem "ImportMesh" "Import.png" [list -np- EmbeddedFluid::xml::ImportMeshWindow] [= "Import embedded mesh"]   
-    Kratos::ToolbarAddItem "Move" "move.png" [list -np- CopyMove Move] [= "Move the geometry/mesh"]   
-    Kratos::ToolbarAddItem "Box" "box.png" [list -np- EmbeddedFluid::xml::BoundingBox::CreateWindow] [= "Generate the bounding box"]   
+    Kratos::ToolbarAddItem "ImportMesh" "Import.png" [list -np- EmbeddedFluid::xml::ImportMeshWindow] [= "Import embedded mesh"]
+    Kratos::ToolbarAddItem "Move" "move.png" [list -np- CopyMove Move] [= "Move the geometry/mesh"]
+    Kratos::ToolbarAddItem "Box" "box.png" [list -np- EmbeddedFluid::xml::BoundingBox::CreateWindow] [= "Generate the bounding box"]
 }
 
 proc ::EmbeddedFluid::BeforeMeshGeneration {elementsize} {
     variable oldVolumeMesher
     catch {file delete -force [file join $::write::dir "[file tail [GiD_Info project modelname] ].post.res"]}
-    GiD_Process Escape Escape Utilities Variables EmbeddedMesh Activated 1 escape escape 
+    GiD_Process Escape Escape Utilities Variables EmbeddedMesh Activated 1 escape escape
     # Set Octree
     set oldVolumeMesher [GiD_Set VolumeMesher]
     ::GiD_Set VolumeMesher 3
@@ -69,7 +69,7 @@ proc ::EmbeddedFluid::BeforeMeshGeneration {elementsize} {
 
 proc ::EmbeddedFluid::AfterMeshGeneration {fail} {
     variable oldVolumeMesher
-    GiD_Process Escape Escape Utilities Variables EmbeddedMesh Activated 0 escape escape
+    # GiD_Process Escape Escape Utilities Variables EmbeddedMesh Activated 0 escape escape
     GiD_Set VolumeMesher $oldVolumeMesher
 }
 
