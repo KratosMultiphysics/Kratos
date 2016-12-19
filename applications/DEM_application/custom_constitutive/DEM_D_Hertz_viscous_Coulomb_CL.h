04fc17f048e83a5be8e80ecb7f8fd66493229cef
jirazabal <jirazabal@cimne.upc.edu>
2016-12-19 13:20:24 +0000

    Minor change

--- applications/DEM_application/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.h
+++ applications/DEM_application/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.h
@@ -31,7 +31,7 @@ namespace Kratos {
 
         DEMDiscontinuumConstitutiveLaw::Pointer Clone() const;
 
-        void InitializeContact(SphericParticle* const element1, SphericParticle* const element2, const double indentation);  
+        void InitializeContact(SphericParticle* const element1, SphericParticle* const element2, const double indentation);
         
         void InitializeContactWithFEM(SphericParticle* const element, DEMWall* const wall, const double indentation, const double ini_delta = 0.0);
 
