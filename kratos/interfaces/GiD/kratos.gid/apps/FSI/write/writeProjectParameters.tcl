de1d573475eb99ddf912aaac30b3b5bc0de41ea9
rzorrilla <rzorrilla@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-12-21 17:47:20 +0000

    Now MESH_DISPLACEMENT is print instead of DISPLACEMENT.

--- kratos/interfaces/GiD/kratos.gid/apps/FSI/write/writeProjectParameters.tcl
+++ kratos/interfaces/GiD/kratos.gid/apps/FSI/write/writeProjectParameters.tcl
@@ -1,7 +1,7 @@
 # Project Parameters
 proc FSI::write::getParametersDict { } {
    set projectParametersDict [dict create]
-   
+
    # FSI section
    set FSIParametersDict [dict create]
    # Solver settings
@@ -12,41 +12,41 @@ proc FSI::write::getParametersDict { } {
    dict set solverSettingsDict solver_type $strategy_write_name
    set solverSettingsDict [dict merge $solverSettingsDict [write::getSolutionStrategyParametersDict] ]
    set solverSettingsDict [dict merge $solverSettingsDict [write::getSolversParametersDict FSI] ]
-   
+
    dict set solverSettingsDict mesh_solver [write::getValue FSIALEParams MeshSolver]
    dict set solverSettingsDict mesh_reform_dofs_each_step [write::getValue FSIALEParams ReformDOFs]
-   
+
    set solidInterfacesList [write::GetMeshFromCondition STLoads StructureInterface2D]
    lappend solidInterfacesList {*}[write::GetMeshFromCondition STLoads StructureInterface3D]
    dict set solverSettingsDict structure_interfaces_list $solidInterfacesList
-   
+
    set fluidInterfacesList [write::GetMeshFromCondition FLBC FluidNoSlipInterface2D]
    lappend fluidInterfacesList {*}[write::GetMeshFromCondition FLBC FluidNoSlipInterface3D]
    dict set solverSettingsDict fluid_interfaces_list $fluidInterfacesList
-   
+
    dict set FSIParametersDict solver_settings $solverSettingsDict
-   
+
    # Structural section
    UpdateUniqueNames Structural
    apps::setActiveAppSoft Structural
-   
+
    set StructuralParametersDict [Structural::write::getParametersEvent]
    set current [dict get $StructuralParametersDict solver_settings model_import_settings input_filename]
    dict set StructuralParametersDict solver_settings model_import_settings input_filename "${current}_Structural"
-   
+
    # Fluid section
    UpdateUniqueNames Fluid
    apps::setActiveAppSoft Fluid
-   
+
    set FluidParametersDict [Fluid::write::getParametersDict]
    set current [dict get $FluidParametersDict solver_settings model_import_settings input_filename]
    dict set FluidParametersDict solver_settings model_import_settings input_filename "${current}_Fluid"
    set nodalresults [dict get $FluidParametersDict output_configuration result_file_configuration nodal_results]
-   lappend nodalresults "DISPLACEMENT"
+   lappend nodalresults "MESH_DISPLACEMENT"
    dict set FluidParametersDict output_configuration result_file_configuration nodal_results $nodalresults
    UpdateUniqueNames FSI
    apps::setActiveAppSoft FSI
-   
+
    dict set projectParametersDict structure_solver_settings $StructuralParametersDict
    dict set projectParametersDict fluid_solver_settings $FluidParametersDict
    dict set projectParametersDict coupling_solver_settings $FSIParametersDict
