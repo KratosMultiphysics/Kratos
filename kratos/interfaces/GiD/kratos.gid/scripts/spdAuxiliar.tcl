08a4752883ec6a3a688da353c73d1af1b08344ad
fjgarate <fjgarate@4358b7d9-91ec-4505-bf62-c3060f61107a>
2016-12-21 14:59:13 +0000

    Bug fixed: nodal output on results

--- kratos/interfaces/GiD/kratos.gid/scripts/spdAuxiliar.tcl
+++ kratos/interfaces/GiD/kratos.gid/scripts/spdAuxiliar.tcl
@@ -905,7 +905,7 @@ proc spdAux::injectNodalConditionsOutputs { basenode args} {
         set pn [$nc getPublicName]
         set v [$nc getAttribute v]
         if {$v eq ""} {set v "Yes"}
-        set node "<value n='$n' pn='$pn' v='$v' values='Yes,No' state='\[CheckNodalConditionState\]'/>"
+        set node "<value n='$n' pn='$pn' v='$v' values='Yes,No' state='\[CheckNodalConditionOutputState $n\]'/>"
         catch {$parts appendXML $node}
         foreach {n1 output} [$nc getOutputs] {
             set nout [$output getName]
