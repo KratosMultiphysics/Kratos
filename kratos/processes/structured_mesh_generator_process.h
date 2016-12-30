df4c5099f06efcfb08670727e643c2f6d5f69cb8
Pooyan Dadvand <pooyan@cimne.upc.edu>
2016-12-30 10:50:11 +0000

    Adding the StructuredMeshGeneratorProcess

---
+++ kratos/processes/structured_mesh_generator_process.h
@@ -0,0 +1,202 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Pooyan Dadvand
+//                   
+//
+
+#if !defined(KRATOS_STRUCTURED_MESH_GENERATOR_PROCESS_H_INCLUDED )
+#define  KRATOS_STRUCTURED_MESH_GENERATOR_PROCESS_H_INCLUDED
+
+
+
+// System includes
+#include <string>
+#include <iostream>
+
+
+// External includes
+
+
+// Project includes
+#include "processes/process.h"
+#include "includes/model_part.h"
+#include "includes/kratos_parameters.h"
+
+
+namespace Kratos
+{
+  ///@addtogroup KratosCore
+  ///@{
+
+  ///@name Kratos Classes
+  ///@{
+
+  /// Short class definition.
+  /** Detail class definition.
+  */
+  class StructuredMeshGeneratorProcess : public Process
+    {
+    public:
+		using GeometryType = Geometry<Point<3> >;
+      ///@name Type Definitions
+      ///@{
+
+      /// Pointer definition of StructuredMeshGeneratorProcess
+      KRATOS_CLASS_POINTER_DEFINITION(StructuredMeshGeneratorProcess);
+
+      ///@}
+      ///@name Life Cycle
+      ///@{
+
+      /// Default constructor is deleted.
+      StructuredMeshGeneratorProcess() = delete;
+
+	  /// Constructor to be used. Takes the geometry to be meshed and ModelPart to be filled
+	  StructuredMeshGeneratorProcess(GeometryType& rGeometry, ModelPart& rOutputModelPart, Parameters TheParameters);
+
+	  /// The object is not copyable.
+	  StructuredMeshGeneratorProcess(StructuredMeshGeneratorProcess const& rOther) = delete;
+
+      /// Destructor.
+      virtual ~StructuredMeshGeneratorProcess() ;
+
+      ///@}
+      ///@name Operators
+      ///@{
+
+	  /// It is not assignable.
+	  StructuredMeshGeneratorProcess& operator=(StructuredMeshGeneratorProcess const& rOther) = delete;
+
+      ///@}
+      ///@name Operations
+      ///@{
+
+	  void Execute() override;
+
+
+      ///@}
+      ///@name Access
+      ///@{
+
+
+      ///@}
+      ///@name Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+      virtual std::string Info() const;
+
+      /// Print information about this object.
+      virtual void PrintInfo(std::ostream& rOStream) const;
+
+      /// Print object's data.
+      virtual void PrintData(std::ostream& rOStream) const;
+
+
+      ///@}
+      ///@name Friends
+      ///@{
+
+
+      ///@}
+
+      private:
+      ///@name Static Member Variables
+      ///@{
+
+
+      ///@}
+      ///@name Member Variables
+      ///@{
+		  GeometryType& mrGeometry;
+
+		  std::size_t mNumberOfDivisions;
+		  std::size_t mStartNodeId;
+		  std::size_t mStartElementId;
+		  std::size_t mStartConditionId;
+		  std::size_t mElementPropertiesId;
+		  std::size_t mConditiongPropertiesId;
+		  std::string mElementName;
+		  std::string mConditionName;
+		  bool mCrateSkinSubModelPart;
+		  ModelPart& mrOutputModelPart;
+
+
+      ///@}
+      ///@name Private Operations
+      ///@{
+
+		  void Generate2DMesh();
+
+		  void GenerateNodes2D(Point<3> const& rMinPoint, Point<3> const& rMaxPoint);
+
+		  void GenerateTriangularElements();
+
+		  std::size_t GetNodeId(std::size_t Row, std::size_t Column);
+
+		  void GetLocalCoordinatesRange(Point<3>& rMinPoint, Point<3>& rMaxPoint);
+
+      ///@}
+      ///@name Private  Access
+      ///@{
+
+
+      ///@}
+      ///@name Private Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Un accessible methods
+      ///@{
+
+
+
+      ///@}
+
+    }; // Class StructuredMeshGeneratorProcess
+
+  ///@}
+
+  ///@name Type Definitions
+  ///@{
+
+
+  ///@}
+  ///@name Input and output
+  ///@{
+
+
+  /// input stream function
+  inline std::istream& operator >> (std::istream& rIStream,
+				    StructuredMeshGeneratorProcess& rThis);
+
+  /// output stream function
+  inline std::ostream& operator << (std::ostream& rOStream,
+				    const StructuredMeshGeneratorProcess& rThis)
+    {
+      rThis.PrintInfo(rOStream);
+      rOStream << std::endl;
+      rThis.PrintData(rOStream);
+
+      return rOStream;
+    }
+  ///@}
+
+  ///@} addtogroup block
+
+}  // namespace Kratos.
+
+#endif // KRATOS_STRUCTURED_MESH_GENERATOR_PROCESS_H_INCLUDED  defined
