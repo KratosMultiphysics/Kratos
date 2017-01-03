2dd6e94f7534d975c444515b2a2eeb00be9fbb77
rzorrilla <rzorrilla@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-01-03 08:59:11 +0000

    adding an explcit delete for the solver so to preserve the order of deletion

--- kratos/applications/trilinos_application/tests/test_trilinos_linear_solvers.py
+++ kratos/applications/trilinos_application/tests/test_trilinos_linear_solvers.py
@@ -11,72 +11,72 @@ def GetFilePath(fileName):
 
 
 class TestLinearSolvers(KratosUnittest.TestCase):
-    
+
     def _RunParametrized(self, my_params_string ):
         all_settings = KratosMultiphysics.Parameters( my_params_string )
-        
+
         for i in range(all_settings["test_list"].size()):
             settings = all_settings["test_list"][i]
             self._auxiliary_test_function(settings)
-    
+
     def _auxiliary_test_function(self, settings, matrix_name="A.mm", absolute_norm=False):
         comm = KratosMultiphysics.TrilinosApplication.CreateCommunicator()
         space = KratosMultiphysics.TrilinosApplication.TrilinosSparseSpace()
-        
+
         #read the matrices
         pA = space.CreateEmptyMatrixPointer(comm)
         space.ReadMatrixMarketMatrix(GetFilePath(matrix_name),pA)
         n = space.Size1(pA.GetReference())
-        
-        
+
+
         pAoriginal = space.CreateEmptyMatrixPointer(comm) #create a copy of A
         space.ReadMatrixMarketMatrix(GetFilePath(matrix_name),pAoriginal)
-        
+
         pb  = space.CreateEmptyVectorPointer(comm)
         space.ResizeVector(pb,n)
         space.SetToZeroVector(pb.GetReference())
-        space.SetValue(pb.GetReference(), 0, 1.0)  #pb[1] = 1.0 
+        space.SetValue(pb.GetReference(), 0, 1.0)  #pb[1] = 1.0
         #print("--- ",space.TwoNorm(pb.GetReference()))
-        
+
         px = space.CreateEmptyVectorPointer(comm)
         space.ResizeVector(px,n)
         pboriginal = space.CreateEmptyVectorPointer(comm) #create a copy of b
         space.ResizeVector(pboriginal,n)
         space.SetToZeroVector(pboriginal.GetReference())
         space.UnaliasedAdd(pboriginal.GetReference(), 1.0,pb.GetReference())
-        
+
         space.SetToZeroVector(px.GetReference())
         #space.SetToZeroVector(boriginal)
-        #space.UnaliasedAdd(boriginal, 1.0, b) #boriginal=1*bs          
+        #space.UnaliasedAdd(boriginal, 1.0, b) #boriginal=1*bs
 
         #construct the solver
         import new_trilinos_linear_solver_factory
         linear_solver = new_trilinos_linear_solver_factory.ConstructSolver(settings)
-        
+
         #solve
-        linear_solver.Solve(pA.GetReference(),px.GetReference(),pb.GetReference())    
-        
+        linear_solver.Solve(pA.GetReference(),px.GetReference(),pb.GetReference())
+
         #test the results
         ptmp = space.CreateEmptyVectorPointer(comm)
         space.ResizeVector(ptmp,n)
         space.SetToZeroVector(ptmp.GetReference())
         space.Mult(pAoriginal.GetReference(),px.GetReference(),ptmp.GetReference())
 
-        
+
         pcheck = space.CreateEmptyVectorPointer(comm)
         space.ResizeVector(pcheck,n)
         space.SetToZeroVector(pcheck.GetReference())
         space.UnaliasedAdd(pcheck.GetReference(), 1.0,pboriginal.GetReference())
         space.UnaliasedAdd(pcheck.GetReference(), -1.0,ptmp.GetReference())
-        
-        
+
+
         achieved_norm = space.TwoNorm(pcheck.GetReference())
-         
+
         tolerance = 1e-9
         if(settings.Has("tolerance")):
-            tolerance = settings["tolerance"].GetDouble() 
-        
-        
+            tolerance = settings["tolerance"].GetDouble()
+
+
         nproc = KratosMultiphysics.mpi.mpi.size
         target_norm = tolerance*space.TwoNorm(pboriginal.GetReference())*nproc #multiplying by nproc the target tolerance to give some slack. Not really nice :-(
 
@@ -85,9 +85,12 @@ class TestLinearSolvers(KratosUnittest.TestCase):
             print("achieved_norm :",achieved_norm)
         self.assertTrue(achieved_norm <= target_norm)
 
+        #destroy the preconditioner - this is needed since  the solver should be destroyed before the destructor of the system matrix is called
+        del linear_solver
 
-        
- 
+
+
+
     @KratosUnittest.expectedFailure
     def test_amesos_superludist(self):
         self._RunParametrized("""
@@ -100,7 +103,7 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                 ]
             }
             """)
-        
+
     def test_amesos_klu(self):
         self._RunParametrized("""
             {
@@ -112,7 +115,7 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                 ]
             }
             """)
-        
+
     def test_aztec_cg(self):
         self._RunParametrized("""
             {
@@ -153,7 +156,7 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                 ]
             }
             """)
-        
+
     def test_aztec_gmres(self):
         self._RunParametrized("""
             {
@@ -194,7 +197,7 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                 ]
             }
             """)
-        
+
     def test_ml_symmetric(self):
         self._RunParametrized("""
             {
@@ -213,8 +216,8 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                     }
                 ]
             }
-            """)        
-        
+            """)
+
     def test_ml_nonsymmetric(self):
         self._RunParametrized("""
             {
@@ -233,8 +236,8 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                     }
                 ]
             }
-            """)         
-   
+            """)
+
     def test_amgcl_mpi_solver_cg(self):
         self._RunParametrized("""
             {
@@ -248,8 +251,8 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                     }
                 ]
             }
-            """) 
-        
+            """)
+
     def test_amgcl_mpi_solver_bicgstab(self):
         self._RunParametrized("""
             {
@@ -263,8 +266,8 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                     }
                 ]
             }
-            """) 
-        
+            """)
+
     def test_amgcl_mpi_solver_bicgstabl(self):
         self._RunParametrized("""
             {
@@ -278,7 +281,7 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                     }
                 ]
             }
-            """) 
+            """)
 
     def test_amgcl_mpi_solver_gmres(self):
         self._RunParametrized("""
@@ -293,8 +296,8 @@ class TestLinearSolvers(KratosUnittest.TestCase):
                     }
                 ]
             }
-            """) 
+            """)
 
-        
+
 if __name__ == '__main__':
     KratosUnittest.main()
