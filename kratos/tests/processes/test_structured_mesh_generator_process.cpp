6d0b527a9841629fd66d3c724d45444742f45e4e
Pooyan Dadvand <pooyan@cimne.upc.edu>
2017-01-04 10:03:34 +0000

    Adding test for tetrahedra mesh generator for cubes

--- kratos/tests/processes/test_structured_mesh_generator_process.cpp
+++ kratos/tests/processes/test_structured_mesh_generator_process.cpp
@@ -22,12 +22,57 @@
 #include "includes/kernel.h"
 #include "processes/structured_mesh_generator_process.h"
 #include "geometries/quadrilateral_2d_4.h"
+#include "geometries/hexahedra_3d_8.h"
 
 
 namespace Kratos {
 	namespace Testing {
 
-		KRATOS_TEST_CASE_IN_SUITE(StructuredMeshGeneratorProcess, KratosCoreFastSuite)
+		KRATOS_TEST_CASE_IN_SUITE(StructuredMeshGeneratorProcessHexahedra, KratosCoreFastSuite)
+		{
+			Kernel kernel;
+
+			Point<3>::Pointer p_point1(new Point<3>(0.00, 0.00, 0.00));
+			Point<3>::Pointer p_point2(new Point<3>(10.00, 0.00, 0.00));
+			Point<3>::Pointer p_point3(new Point<3>(10.00, 10.00, 0.00));
+			Point<3>::Pointer p_point4(new Point<3>(0.00, 10.00, 0.00));
+			Point<3>::Pointer p_point5(new Point<3>(0.00, 0.00, 10.00));
+			Point<3>::Pointer p_point6(new Point<3>(10.00, 0.00, 10.00));
+			Point<3>::Pointer p_point7(new Point<3>(10.00, 10.00, 10.00));
+			Point<3>::Pointer p_point8(new Point<3>(0.00, 10.00, 10.00));
+
+			Hexahedra3D8<Point<3> > geometry(p_point1, p_point2, p_point3, p_point4, p_point5, p_point6, p_point7, p_point8);
+
+			ModelPart model_part("Generated");
+
+			Parameters mesher_parameters(R"( 
+            {
+                "number_of_divisions":10,
+                "element_name": "Element3D4N"
+            }  )");
+
+			std::size_t number_of_divisions = mesher_parameters["number_of_divisions"].GetInt();
+
+			StructuredMeshGeneratorProcess(geometry, model_part, mesher_parameters).Execute();
+			std::size_t number_of_nodes = (number_of_divisions + 1) * (number_of_divisions + 1) * (number_of_divisions + 1);
+			std::size_t number_of_elements = number_of_divisions * number_of_divisions * number_of_divisions * 6;
+			KRATOS_CHECK_EQUAL(model_part.NumberOfNodes(), number_of_nodes);
+			KRATOS_CHECK_EQUAL(model_part.NumberOfElements(), number_of_elements) << " Number of elements = " << model_part.NumberOfElements() ;
+
+			double total_volume = 0.00;
+			for (auto i_element = model_part.ElementsBegin(); i_element != model_part.ElementsEnd(); i_element++) {
+				double element_volume = i_element->GetGeometry().Volume();
+				KRATOS_CHECK_GREATER(element_volume, 0.00) << " for element #" << i_element->Id() << " with nodes ["
+					<< i_element->GetGeometry()[0].Id()
+					<< "," << i_element->GetGeometry()[1].Id()
+					<< "," << i_element->GetGeometry()[2].Id()
+					<< "," << i_element->GetGeometry()[3].Id() << "] with volume : " << element_volume << std::endl << *i_element;
+				total_volume += element_volume;
+			}
+			KRATOS_CHECK_NEAR(total_volume, 1000., 1.E-6) << "with total_volume = " << total_volume ;
+		}
+
+		KRATOS_TEST_CASE_IN_SUITE(StructuredMeshGeneratorProcessQuadrilateral, KratosCoreFastSuite)
 		{
 			Kernel kernel;
 
@@ -56,7 +101,7 @@ namespace Kratos {
 			double total_area = 0.00;
 			for (auto i_element = model_part.ElementsBegin(); i_element != model_part.ElementsEnd(); i_element++) {
 				double element_area = i_element->GetGeometry().Area();
-				KRATOS_CHECK_GREATER(element_area, 0.00)  << " for element #" << i_element->Id() << " with nodes [" 
+				KRATOS_CHECK_GREATER(element_area, 0.00) << " for element #" << i_element->Id() << " with nodes ["
 					<< i_element->GetGeometry()[0].Id()
 					<< "," << i_element->GetGeometry()[1].Id()
 					<< "," << i_element->GetGeometry()[2].Id() << "] with area : " << element_area << std::endl << *i_element;
