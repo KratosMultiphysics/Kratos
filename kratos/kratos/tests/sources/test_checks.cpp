10b142005f662da66f9d9bb73129517b96cd5b44
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-01-04 18:31:26 +0000

    Changing debug check macros to avoid message streaming compilation error in release mode

--- kratos/kratos/tests/sources/test_checks.cpp
+++ kratos/kratos/tests/sources/test_checks.cpp
@@ -2,19 +2,19 @@
 //    ' /   __| _` | __|  _ \   __|
 //    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics 
+//                   Multi-Physics
 //
-//  License:		 BSD License 
+//  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
 //  Main authors:    Pooyan Dadvand
-//                   
 //
-	           
+//
+
 // System includes
 
 
-// External includes 
+// External includes
 
 
 // Project includes
@@ -43,46 +43,6 @@ namespace Kratos {
 			KRATOS_CHECK_GREATER_EQUAL(2., 2.);
 
 			KRATOS_CHECK_STRING_CONTAIN_SUB_STRING(std::string("Test"), "es");
-
-#ifdef KRATOS_DEBUG
-			KRATOS_DEBUG_CHECK(true);
-			KRATOS_DEBUG_CHECK_IS_FALSE(false);
-
-			KRATOS_DEBUG_CHECK_EQUAL(1.0, 1.0);
-			KRATOS_DEBUG_CHECK_NOT_EQUAL(1.0, 2.0);
-
-			KRATOS_DEBUG_CHECK_C_STRING_EQUAL("Test", "Test");
-			KRATOS_DEBUG_CHECK_C_STRING_NOT_EQUAL("Test ", "Test");
-
-			KRATOS_DEBUG_CHECK_LESS(1., 2.);
-			KRATOS_DEBUG_CHECK_LESS_EQUAL(1., 1.);
-
-			KRATOS_DEBUG_CHECK_GREATER(2., 1.);
-			KRATOS_DEBUG_CHECK_GREATER_EQUAL(2., 2.);
-
-			KRATOS_DEBUG_CHECK_STRING_CONTAIN_SUB_STRING(std::string("Test"), "es");
-#else
-			// all these checks should fail in debug mode but in release should be ignored
-			KRATOS_DEBUG_CHECK(false); 
-			KRATOS_DEBUG_CHECK_IS_FALSE(true);
-
-			KRATOS_DEBUG_CHECK_EQUAL(1.0, 2.0);
-			KRATOS_DEBUG_CHECK_NOT_EQUAL(1.0, 1.0);
-
-			KRATOS_DEBUG_CHECK_C_STRING_EQUAL("T", "Test");
-			KRATOS_DEBUG_CHECK_C_STRING_NOT_EQUAL("Test", "Test");
-
-			KRATOS_DEBUG_CHECK_LESS(1., 1.);
-			KRATOS_DEBUG_CHECK_LESS_EQUAL(1., 0.);
-
-			KRATOS_DEBUG_CHECK_GREATER(1., 1.);
-			KRATOS_DEBUG_CHECK_GREATER_EQUAL(0., 2.);
-
-			KRATOS_DEBUG_CHECK_STRING_CONTAIN_SUB_STRING(std::string("Test"), "AnotherTEST");
-#endif
-
 		}
 	}
 }  // namespace Kratos.
-
-
