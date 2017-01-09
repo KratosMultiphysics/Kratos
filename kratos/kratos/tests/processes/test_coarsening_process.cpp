6b542ef1619fd1bfa38b2f4e20c948ac70f19efa
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-01-09 15:59:19 +0000

    Adding 3d coarsening test

--- kratos/kratos/tests/processes/test_coarsening_process.cpp
+++ kratos/kratos/tests/processes/test_coarsening_process.cpp
@@ -23,6 +23,7 @@
 #include "processes/structured_mesh_generator_process.h"
 #include "processes/find_nodal_neighbours_process.h"
 #include "geometries/quadrilateral_2d_4.h"
+#include "geometries/hexahedra_3d_8.h"
 #include "includes/gid_io.h"
 
 
@@ -70,8 +71,8 @@ namespace Kratos {
 			// gid_io.FinalizeMesh();
 
 
-			KRATOS_WATCH(model_part.NumberOfNodes());
-			KRATOS_WATCH(model_part.NumberOfElements());
+			//KRATOS_WATCH(model_part.NumberOfNodes());
+			//KRATOS_WATCH(model_part.NumberOfElements());
 			//std::size_t number_of_nodes = (number_of_divisions + 1) * (number_of_divisions + 1);
 			//KRATOS_CHECK_EQUAL(model_part.NumberOfNodes(), number_of_nodes);
 			//KRATOS_CHECK_EQUAL(model_part.NumberOfElements(), number_of_divisions * number_of_divisions * 2);
@@ -218,5 +219,87 @@ namespace Kratos {
 			}
 			KRATOS_CHECK_NEAR(total_area, original_mesh_area, 1e-6);
 		}
+
+		KRATOS_TEST_CASE_IN_SUITE(Structured3DMeshCoarseningProcess, KratosCoreFastSuite)
+		{
+
+			Point<3>::Pointer p_point1(new Point<3>(0.00, 0.00, 0.00));
+			Point<3>::Pointer p_point2(new Point<3>(10.00, 0.00, 0.00));
+			Point<3>::Pointer p_point3(new Point<3>(10.00, 10.00, 0.00));
+			Point<3>::Pointer p_point4(new Point<3>(0.00, 10.00, 0.00));
+			Point<3>::Pointer p_point5(new Point<3>(0.00, 0.00, 10.00));
+			Point<3>::Pointer p_point6(new Point<3>(10.00, 0.00, 10.00));
+			Point<3>::Pointer p_point7(new Point<3>(10.00, 10.00, 10.00));
+			Point<3>::Pointer p_point8(new Point<3>(0.00, 10.00, 10.00));
+
+			Hexahedra3D8<Point<3> > geometry(p_point1, p_point2, p_point3, p_point4, p_point5, p_point6, p_point7, p_point8);
+
+			ModelPart model_part("Test");
+
+			Parameters mesher_parameters(R"(
+            {
+                "number_of_divisions":10,
+                "element_name": "Element3D4N"
+            }  )");
+
+			std::size_t number_of_divisions = mesher_parameters["number_of_divisions"].GetInt();
+
+			StructuredMeshGeneratorProcess(geometry, model_part, mesher_parameters).Execute();
+
+			GidIO<> gid_io("c:/temp/coarsening/coarsening_3d_test", GiD_PostAscii, SingleFile, WriteDeformed, WriteConditions);
+			gid_io.InitializeMesh(0.00);
+			gid_io.WriteMesh(model_part.GetMesh());
+			gid_io.FinalizeMesh();
+
+			ModelPart& r_skin_model_part = model_part.GetSubModelPart("Skin");
+
+			for (auto i_node = r_skin_model_part.NodesBegin(); i_node != r_skin_model_part.NodesEnd(); i_node++)
+				i_node->Set(BOUNDARY);
+
+			FindNodalNeighboursProcess(model_part).Execute();
+
+			MeshCoarseningProcess(model_part).Execute();
+
+			gid_io.InitializeMesh(1.00);
+			gid_io.WriteMesh(model_part.GetMesh());
+			gid_io.FinalizeMesh();
+
+			FindNodalNeighboursProcess(model_part).Execute();
+
+			MeshCoarseningProcess(model_part).Execute();
+
+			gid_io.InitializeMesh(2.00);
+			gid_io.WriteMesh(model_part.GetMesh());
+			gid_io.FinalizeMesh();
+
+			FindNodalNeighboursProcess(model_part).Execute();
+
+			MeshCoarseningProcess(model_part).Execute();
+
+			gid_io.InitializeMesh(3.00);
+			gid_io.WriteMesh(model_part.GetMesh());
+			gid_io.FinalizeMesh();
+
+
+			//KRATOS_WATCH(model_part.NumberOfNodes());
+			//KRATOS_WATCH(model_part.NumberOfElements());
+			KRATOS_CHECK_NOT_EQUAL(model_part.NumberOfNodes(), 0);
+			KRATOS_CHECK_NOT_EQUAL(model_part.NumberOfElements(), 0);
+
+
+			const double cube_volume = 1000.;
+			const double avarage_element_volume = cube_volume / model_part.NumberOfElements();
+			double total_volume = 0.00;
+			for (auto i_element = model_part.ElementsBegin(); i_element != model_part.ElementsEnd(); i_element++) {
+				double element_volume = i_element->GetGeometry().Volume();
+				KRATOS_CHECK_GREATER(element_volume, avarage_element_volume / 1000) << " for element #" << i_element->Id() << " with nodes ["
+					<< i_element->GetGeometry()[0].Id()
+					<< "," << i_element->GetGeometry()[1].Id()
+					<< "," << i_element->GetGeometry()[2].Id() << "] with volume : " << element_volume << std::endl << *i_element;
+				total_volume += element_volume;
+			}
+			KRATOS_CHECK_NEAR(total_volume, cube_volume, 1.E-6) << "with total_volume = " << total_volume;
+		}
+
 	}
 }  // namespace Kratos.
