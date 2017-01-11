f340064049297976bf55eb0b2681ed2e6d3ab086
maceli <maceli@cimne.upc.edu>
2017-01-11 08:44:03 +0000

    Clang warning removed

--- kratos/processes/structured_mesh_generator_process.h
+++ kratos/processes/structured_mesh_generator_process.h
@@ -96,13 +96,13 @@ namespace Kratos
       ///@{
 
       /// Turn back information as a string.
-      virtual std::string Info() const;
+      virtual std::string Info() const override;
 
       /// Print information about this object.
-      virtual void PrintInfo(std::ostream& rOStream) const;
+      virtual void PrintInfo(std::ostream& rOStream) const override;
 
       /// Print object's data.
-      virtual void PrintData(std::ostream& rOStream) const;
+      virtual void PrintData(std::ostream& rOStream) const override;
 
 
       ///@}
