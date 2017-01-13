4e3f6ca1f395e1afe1a5faec2bd95e1085df7701
jcotela <jcotela@cimne.upc.edu>
2017-01-13 17:51:18 +0000

    Compatibility fix for Python >= 3.6

--- external_libraries/mpi_python/mpi.py
+++ external_libraries/mpi_python/mpi.py
@@ -1,9 +1,16 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import sys
 if sys.platform.startswith('linux'):
-    import DLFCN as dl
+    # Note: from Python 3.3 onwards, dll load flags are available from module os
+    # from Python 3.6 onwards, module DLFCN no longer exists
     flags = sys.getdlopenflags()
-    sys.setdlopenflags(dl.RTLD_NOW | dl.RTLD_GLOBAL)
+    if sys.version_info >= (3,3):
+        import os
+        dll_load_flags = os.RTLD_NOW | os.RTLD_GLOBAL
+    else:
+        import DLFCN as dl
+        dll_load_flags = dl.RTLD_NOW | dl.RTLD_GLOBAL
+    sys.setdlopenflags(dll_load_flags)
     import mpipython
     sys.setdlopenflags(flags)
 else:
