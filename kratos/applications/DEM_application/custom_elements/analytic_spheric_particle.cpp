db12dfd52ba204b60c5fe8ac0a01f817eae6b94c
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-01-16 07:02:02 +0000

    dding analytic particle, that will be used to probe statistical info

---
+++ kratos/applications/DEM_application/custom_elements/analytic_spheric_particle.cpp
@@ -0,0 +1,43 @@
+//
+// Author: Guillermo Casas gcasas@cimne.upc.edu
+//
+
+// System includes
+#include <string>
+#include <iostream>
+#include <cmath>
+
+#include <fstream>
+
+// External includes
+
+// Project includes
+#include "analytic_spheric_particle.h"
+
+
+namespace Kratos
+{
+// using namespace GeometryFunctions;
+
+AnalyticSphericParticle::AnalyticSphericParticle()
+    : SphericParticle()
+{}
+
+AnalyticSphericParticle::AnalyticSphericParticle(IndexType NewId, GeometryType::Pointer pGeometry)
+    : SphericParticle(NewId, pGeometry)
+{}
+
+AnalyticSphericParticle::AnalyticSphericParticle(IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties)
+    : SphericParticle(NewId, pGeometry, pProperties)
+{}
+
+AnalyticSphericParticle::AnalyticSphericParticle(IndexType NewId, NodesArrayType const& ThisNodes)
+    : SphericParticle(NewId, ThisNodes)
+{}
+
+Element::Pointer AnalyticSphericParticle::Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const
+{
+    return Element::Pointer(new AnalyticSphericParticle(NewId, GetGeometry().Create(ThisNodes), pProperties));
+}
+
+}  // namespace Kratos.
---
+++ kratos/applications/DEM_application/custom_elements/analytic_spheric_particle.h
@@ -0,0 +1,119 @@
+//
+// Author: Guillermo Casas gcasas@cimne.upc.edu
+//
+
+
+#if !defined(KRATOS_ANALYTIC_SPHERIC_PARTICLE_H_INCLUDED)
+#define  KRATOS_ANALYTIC_SPHERIC_PARTICLE_H_INCLUDED
+
+// System includes
+#include <string>
+#include <iostream>
+#include "spheric_particle.h"
+
+
+namespace Kratos
+{
+
+class KRATOS_API(DEM_APPLICATION) AnalyticSphericParticle : public SphericParticle
+{
+public:
+
+/// Pointer definition of AnalyticSphericParticle
+KRATOS_CLASS_POINTER_DEFINITION(AnalyticSphericParticle);
+
+typedef WeakPointerVector<Condition> ConditionWeakVectorType;
+typedef WeakPointerVector<Condition >::iterator ConditionWeakIteratorType;
+
+typedef WeakPointerVector<Element> ParticleWeakVectorType;
+typedef ParticleWeakVectorType::ptr_iterator ParticleWeakIteratorType_ptr;
+typedef WeakPointerVector<Element >::iterator ParticleWeakIteratorType;
+
+/// Default constructor.
+AnalyticSphericParticle( IndexType NewId, GeometryType::Pointer pGeometry );
+AnalyticSphericParticle( IndexType NewId, NodesArrayType const& ThisNodes);
+AnalyticSphericParticle( IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties );
+
+Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const;
+
+/// Destructor.
+virtual ~AnalyticSphericParticle(){};
+
+/// Turn back information as a string.
+std::string Info() const
+{
+std::stringstream buffer;
+buffer << "AnalyticSphericParticle" ;
+return buffer.str();
+}
+
+/// Print information about this object.
+void PrintInfo(std::ostream& rOStream) const {rOStream << "AnalyticSphericParticle";}
+
+/// Print object's data.
+void PrintData(std::ostream& rOStream) const {}
+
+protected:
+
+AnalyticSphericParticle();
+
+private:
+
+friend class Serializer;
+
+void save(Serializer& rSerializer) const
+{
+    KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, DiscreteElement );
+    rSerializer.save("mRadius",mRadius);
+    rSerializer.save("mSearchRadius", mSearchRadius);
+    rSerializer.save("mSearchRadiusWithFem", mSearchRadiusWithFem);
+    rSerializer.save("mRealMass",mRealMass);
+    rSerializer.save("mClusterId",mClusterId);
+    rSerializer.save("mBoundDeltaDispSq",mBoundDeltaDispSq);
+    rSerializer.save("HasStressTensor", (int)this->Is(DEMFlags::HAS_STRESS_TENSOR));
+    if (this->Is(DEMFlags::HAS_STRESS_TENSOR)){
+        rSerializer.save("mSymmStressTensor", mSymmStressTensor);
+    }
+}
+
+void load(Serializer& rSerializer)
+{
+    KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, DiscreteElement );
+    rSerializer.load("mRadius",mRadius);
+    rSerializer.load("mSearchRadius", mSearchRadius);
+    rSerializer.load("mSearchRadiusWithFem", mSearchRadiusWithFem);
+    rSerializer.load("mRealMass",mRealMass);
+    rSerializer.load("mClusterId",mClusterId);
+    rSerializer.load("mBoundDeltaDispSq",mBoundDeltaDispSq); 
+    int aux_int=0;
+    rSerializer.load("HasStressTensor", aux_int);
+    if(aux_int) this->Set(DEMFlags::HAS_STRESS_TENSOR, true);
+    if (this->Is(DEMFlags::HAS_STRESS_TENSOR)){
+        mStressTensor  = new Matrix(3,3);
+        *mStressTensor = ZeroMatrix(3,3);
+        mSymmStressTensor  = new Matrix(3,3);
+        *mSymmStressTensor = ZeroMatrix(3,3);
+        rSerializer.load("mSymmStressTensor", mSymmStressTensor);
+    }
+}
+
+}; // Class AnalyticSphericParticle
+
+/// input stream function
+inline std::istream& operator >> (std::istream& rIStream,
+            AnalyticSphericParticle& rThis){ return rIStream;}
+
+/// output stream function
+inline std::ostream& operator << (std::ostream& rOStream,
+            const AnalyticSphericParticle& rThis)
+{
+rThis.PrintInfo(rOStream);
+rOStream << std::endl;
+rThis.PrintData(rOStream);
+
+return rOStream;
+}
+
+}  // namespace Kratos.
+
+#endif // KRATOS_ANALYTIC_SPHERIC_PARTICLE_H_INCLUDED  defined
