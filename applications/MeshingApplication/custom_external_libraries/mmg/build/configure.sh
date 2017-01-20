1f0622672d6e095aa6cbc7e7b08154e158225857
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2017-01-20 14:04:03 +0000

    Updating configure

--- applications/MeshingApplication/custom_external_libraries/mmg/build/configure.sh
+++ applications/MeshingApplication/custom_external_libraries/mmg/build/configure.sh
@@ -1,8 +1,13 @@
 cmake .. \
--DCMAKE_BUILD_TYPE=Release      \
--DUSE_SCOTCH=ON      \
+-DCMAKE_BUILD_TYPE=Release                                  \
+-DUSE_SCOTCH=OFF                                            \
 -DSCOTCH_INCLUDE_DIR="/usr/include/scotch/"	            \
 -DSCOTCH_LIBRARY="libscotchmetis-5.1.so"	            \
 -DSCOTCHERR_LIBRARY="libscotcherr-5.1.so"	            \
--DCMAKE_CXX_FLAGS="-O3 -mavx2 -fPIC -fopenmp" \
--DCMAKE_C_FLAGS="-O3 -mavx2 -fPIC -fopenmp"
+-DCMAKE_CXX_FLAGS="-O3 -mavx2 -fPIC -fopenmp"               \
+-DCMAKE_C_FLAGS="-O3 -mavx2 -fPIC -fopenmp"                 \
+
+#decomment this to have it verbose
+# make VERBOSE=1 -j4
+make -j4
+sudo make install
