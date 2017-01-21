a2f03b6d80dfe7d4297f74031195bbad94c3ee7d
rrossi <rrossi@cimne.upc.edu>
2017-01-21 17:58:13 +0000

    adding the possibility of setting constrained status and End as a viable value for the end of interval

--- kratos/python_scripts/experimental_assign_vector_process.py
+++ kratos/python_scripts/experimental_assign_vector_process.py
@@ -19,12 +19,32 @@ class AssignVectorProcess(KratosMultiphysics.Process):
                 "variable_name"        : "SPECIFY_VARIABLE_NAME",
                 "interval"             : [0.0, 1e30],
                 "value"                : [10.0, "3*t", "x+y"],
+                "constrained"          : [true,true,true],
                 "local_axes"           : {}
             }
             """
             )
-        
         #example of admissible values for "value" : [10.0, "3*t", "x+y"]
+        
+        ##trick to ensure that if someone sets constrained as a single bool, it is transformed to a vector
+        if(settings.Has("constrained")):
+            if(settings["constrained"].IsBool()):
+                is_fixed = settings["constrained"].GetBool()
+                print("is_fixed = ",is_fixed)
+                settings["constrained"] = default_settings["constrained"]
+                for i in range(3):
+                    settings["constrained"][i].SetBool(is_fixed)
+        
+                    
+        #detect "End" as a tag and replace it by a large number
+        if(settings.Has("interval")):
+            if(settings["interval"][1].IsString() ):
+                if(settings["interval"][1].GetString() == "End"):
+                    settings["interval"][1].SetDouble(1e30) # = default_settings["interval"][1]
+                else:
+                    raise Exception("the second value of interval can be \"End\" or a number, interval currently:"+settings["interval"].PrettyPrintJsonString())
+        
+        print(settings.PrettyPrintJsonString())
         
         settings.ValidateAndAssignDefaults(default_settings)
         
@@ -39,7 +59,7 @@ class AssignVectorProcess(KratosMultiphysics.Process):
             x_params = KratosMultiphysics.Parameters("{}")           
             x_params.AddValue("model_part_name",settings["model_part_name"])
             x_params.AddValue("mesh_id",settings["mesh_id"])
-            x_params.AddEmptyValue("constrained").SetBool(True)
+            x_params.AddEmptyValue("constrained").SetBool(settings["constrained"][0].GetBool())
             x_params.AddValue("interval",settings["interval"])
             x_params.AddValue("value",settings["value"][0])
             x_params.AddEmptyValue("variable_name").SetString(settings["variable_name"].GetString() + "_X")
@@ -51,7 +71,7 @@ class AssignVectorProcess(KratosMultiphysics.Process):
             y_params = KratosMultiphysics.Parameters("{}")           
             y_params.AddValue("model_part_name",settings["model_part_name"])
             y_params.AddValue("mesh_id",settings["mesh_id"])
-            y_params.AddEmptyValue("constrained").SetBool(True)
+            y_params.AddEmptyValue("constrained").SetBool(settings["constrained"][1].GetBool())
             y_params.AddValue("interval",settings["interval"])
             y_params.AddValue("value",settings["value"][1])
             y_params.AddEmptyValue("variable_name").SetString(settings["variable_name"].GetString() + "_Y")
@@ -63,7 +83,7 @@ class AssignVectorProcess(KratosMultiphysics.Process):
             z_params = KratosMultiphysics.Parameters("{}")           
             z_params.AddValue("model_part_name",settings["model_part_name"])
             z_params.AddValue("mesh_id",settings["mesh_id"])
-            z_params.AddEmptyValue("constrained").SetBool(True)
+            z_params.AddEmptyValue("constrained").SetBool(settings["constrained"][2].GetBool())
             z_params.AddValue("interval",settings["interval"])
             z_params.AddValue("value",settings["value"][2])
             z_params.AddEmptyValue("variable_name").SetString(settings["variable_name"].GetString() + "_Z")
@@ -77,5 +97,7 @@ class AssignVectorProcess(KratosMultiphysics.Process):
             process.ExecuteInitializeSolutionStep()
             
     def ExecuteFinalizeSolutionStep(self):
+        #print("---")
         for process in self.aux_processes:
-            process.ExecuteInitializeSolutionStep()
+            #print("current_time = ", self.model_part.ProcessInfo[KratosMultiphysics.TIME], " interval = ", process.interval)
+            process.ExecuteFinalizeSolutionStep()
