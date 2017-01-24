94857ca798ed2cd3f87b657c5c19aaf067162fc9
gcasas <gcasas@cimne.upc.edu>
2017-01-24 22:36:48 +0000

    adding some member variables

--- applications/DEM_application/custom_elements/analytic_spheric_particle.cpp
+++ applications/DEM_application/custom_elements/analytic_spheric_particle.cpp
@@ -21,19 +21,55 @@ namespace Kratos
 
 AnalyticSphericParticle::AnalyticSphericParticle()
     : SphericParticle()
-{}
+{
+    mNumberOfCollidingSpheres = 0;
+
+    for (unsigned int i = 0; i < 4; ++i){
+        mCollidingIds[i] = 0;
+        mCollidingRadii[i] = 0.0;
+        mCollidingNormalVelocities[i] = 0.0;
+        mCollidingTangentialVelocities[i] = 0.0;
+    }
+}
 
 AnalyticSphericParticle::AnalyticSphericParticle(IndexType NewId, GeometryType::Pointer pGeometry)
     : SphericParticle(NewId, pGeometry)
-{}
+{
+    mNumberOfCollidingSpheres = 0;
+
+    for (unsigned int i = 0; i < 4; ++i){
+        mCollidingIds[i] = 0;
+        mCollidingRadii[i] = 0.0;
+        mCollidingNormalVelocities[i] = 0.0;
+        mCollidingTangentialVelocities[i] = 0.0;
+    }
+}
 
 AnalyticSphericParticle::AnalyticSphericParticle(IndexType NewId, GeometryType::Pointer pGeometry,  PropertiesType::Pointer pProperties)
     : SphericParticle(NewId, pGeometry, pProperties)
-{}
+{
+    mNumberOfCollidingSpheres = 0;
+
+    for (unsigned int i = 0; i < 4; ++i){
+        mCollidingIds[i] = 0;
+        mCollidingRadii[i] = 0.0;
+        mCollidingNormalVelocities[i] = 0.0;
+        mCollidingTangentialVelocities[i] = 0.0;
+    }
+}
 
 AnalyticSphericParticle::AnalyticSphericParticle(IndexType NewId, NodesArrayType const& ThisNodes)
     : SphericParticle(NewId, ThisNodes)
-{}
+{
+    mNumberOfCollidingSpheres = 0;
+
+    for (unsigned int i = 0; i < 4; ++i){
+        mCollidingIds[i] = 0;
+        mCollidingRadii[i] = 0.0;
+        mCollidingNormalVelocities[i] = 0.0;
+        mCollidingTangentialVelocities[i] = 0.0;
+    }
+}
 
 Element::Pointer AnalyticSphericParticle::Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const
 {
