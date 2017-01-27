9b09b4c740d510094c792ed3d680c383efb9bb2f
jcotela <jcotela@cimne.upc.edu>
2017-01-27 14:36:43 +0000

    Removing unneeded dependency on incompressible_fluid_application.

--- applications/FluidDynamicsApplication/python_scripts/trilinos_vms_monolithic_solver.py
+++ applications/FluidDynamicsApplication/python_scripts/trilinos_vms_monolithic_solver.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 # importing the Kratos Library
 from KratosMultiphysics import *
-from KratosMultiphysics.IncompressibleFluidApplication import *
 from KratosMultiphysics.FluidDynamicsApplication import *
 from KratosMultiphysics.mpi import *
 from KratosMultiphysics.MetisApplication import *
