4c6f24aea3bbafdcf27ce0bef8bb2fbb9dcce090
pooyan <pooyan@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-01-27 14:21:09 +0000

    Removing multicomment bug

--- kratos/kratos/processes/measure_mesh_quality_process.h
+++ kratos/kratos/processes/measure_mesh_quality_process.h
@@ -1,16 +1,16 @@
 //    |  /           |
 //    ' /   __| _` | __|  _ \   __|
-//    . \  |   (   | |   (   |\__ \
+//    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics 
+//                   Multi-Physics
 //
-//  License:		 BSD License 
+//  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
 //  Main authors:    Pooyan Dadvand
-//                    
 //
-	           
+//
+
 #if !defined(KRATOS_MEASURE_MESH_QUALITY_PROCESS_H_INCLUDED )
 #define  KRATOS_MEASURE_MESH_QUALITY_PROCESS_H_INCLUDED
 
@@ -18,10 +18,10 @@
 
 // System includes
 #include <string>
-#include <iostream> 
+#include <iostream>
 
 
-// External includes 
+// External includes
 
 
 // Project includes
@@ -37,7 +37,7 @@ namespace Kratos
   ///@{
   ///@name Kratos Classes
   ///@{
-  
+
   /// Short class definition.
   /** Detail class definition.
   */
@@ -46,103 +46,103 @@ namespace Kratos
     public:
       ///@name Type Definitions
       ///@{
-      
+
       /// Pointer definition of MeasureMeshQualityProcess
       KRATOS_CLASS_POINTER_DEFINITION(MeasureMeshQualityProcess);
-  
+
       ///@}
-      ///@name Life Cycle 
-      ///@{ 
-      
+      ///@name Life Cycle
+      ///@{
+
       /// Constructor takes the modelpart to apply smoothing to its mesh 0.
       MeasureMeshQualityProcess(ModelPart& rModelPart, std::size_t Dimension);
 
       /// Destructor.
       virtual ~MeasureMeshQualityProcess();
-      
+
 
       ///@}
-      ///@name Operators 
+      ///@name Operators
       ///@{
-      
-      
+
+
       ///@}
       ///@name Operations
       ///@{
-      
+
 	  virtual void Execute();
-      
+
       ///@}
       ///@name Access
-      ///@{ 
-      
-      
+      ///@{
+
+
       ///@}
       ///@name Inquiry
       ///@{
-      
-      
-      ///@}      
+
+
+      ///@}
       ///@name Input and output
       ///@{
 
       /// Turn back information as a string.
       virtual std::string Info() const;
-      
+
       /// Print information about this object.
       virtual void PrintInfo(std::ostream& rOStream) const;
 
       /// Print object's data.
       virtual void PrintData(std::ostream& rOStream) const;
-      
-            
-      ///@}      
+
+
+      ///@}
       ///@name Friends
       ///@{
-      
-            
+
+
       ///@}
-      
+
     protected:
-      ///@name Protected static Member Variables 
-      ///@{ 
-        
-        
-      ///@} 
-      ///@name Protected member Variables 
-      ///@{ 
-        
-        
-      ///@} 
-      ///@name Protected Operators
-      ///@{ 
-        
-        
-      ///@} 
-      ///@name Protected Operations
-      ///@{ 
-        
-        
-      ///@} 
-      ///@name Protected  Access 
-      ///@{ 
-        
-        
-      ///@}      
-      ///@name Protected Inquiry 
-      ///@{ 
-        
-        
-      ///@}    
-      ///@name Protected LifeCycle 
-      ///@{ 
-      
-            
+      ///@name Protected static Member Variables
+      ///@{
+
+
       ///@}
-      
+      ///@name Protected member Variables
+      ///@{
+
+
+      ///@}
+      ///@name Protected Operators
+      ///@{
+
+
+      ///@}
+      ///@name Protected Operations
+      ///@{
+
+
+      ///@}
+      ///@name Protected  Access
+      ///@{
+
+
+      ///@}
+      ///@name Protected Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Protected LifeCycle
+      ///@{
+
+
+      ///@}
+
     private:
-      ///@name Static Member Variables 
-      ///@{ 
+      ///@name Static Member Variables
+      ///@{
 
 		ModelPart& mrModelPart;
 		std::size_t mDimension;
@@ -153,67 +153,67 @@ namespace Kratos
 		double mMaxArea;
 		double mMinH;
 		double mMinAngle;
-        
-        
-      ///@} 
-      ///@name Member Variables 
-      ///@{ 
-        
-        
-      ///@} 
+
+
+      ///@}
+      ///@name Member Variables
+      ///@{
+
+
+      ///@}
       ///@name Private Operators
-      ///@{ 
-        
-        
-      ///@} 
+      ///@{
+
+
+      ///@}
       ///@name Private Operations
-      ///@{ 
+      ///@{
 
 		void ResetMeasures();
-        
-        
-      ///@} 
-      ///@name Private  Access 
-      ///@{ 
-        
-        
-      ///@}    
-      ///@name Private Inquiry 
-      ///@{ 
-        
-        
-      ///@}    
-      ///@name Un accessible methods 
-      ///@{ 
-      
+
+
+      ///@}
+      ///@name Private  Access
+      ///@{
+
+
+      ///@}
+      ///@name Private Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Un accessible methods
+      ///@{
+
       /// Assignment operator.
       MeasureMeshQualityProcess& operator=(MeasureMeshQualityProcess const& rOther);
 
       /// Copy constructor.
       MeasureMeshQualityProcess(MeasureMeshQualityProcess const& rOther);
 
-        
-      ///@}    
-        
-    }; // Class MeasureMeshQualityProcess 
 
-  ///@} 
-  
-  ///@name Type Definitions       
-  ///@{ 
-  
-  
-  ///@} 
-  ///@name Input and output 
-  ///@{ 
-        
- 
+      ///@}
+
+    }; // Class MeasureMeshQualityProcess
+
+  ///@}
+
+  ///@name Type Definitions
+  ///@{
+
+
+  ///@}
+  ///@name Input and output
+  ///@{
+
+
   /// input stream function
-  inline std::istream& operator >> (std::istream& rIStream, 
+  inline std::istream& operator >> (std::istream& rIStream,
 				    MeasureMeshQualityProcess& rThis);
 
   /// output stream function
-  inline std::ostream& operator << (std::ostream& rOStream, 
+  inline std::ostream& operator << (std::ostream& rOStream,
 				    const MeasureMeshQualityProcess& rThis)
     {
       rThis.PrintInfo(rOStream);
@@ -228,6 +228,4 @@ namespace Kratos
 
 }  // namespace Kratos.
 
-#endif // KRATOS_MEASURE_MESH_QUALITY_PROCESS_H_INCLUDED  defined 
-
-
+#endif // KRATOS_MEASURE_MESH_QUALITY_PROCESS_H_INCLUDED  defined
