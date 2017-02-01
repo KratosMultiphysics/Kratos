50c03b2de50124d6606686f9e06997151c52b62a
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-02-01 11:24:38 +0000

    correcting some mistaken calls

--- kratos/applications/swimming_DEM_application/python_scripts/derivative_recovery/standard_recoverer.py
+++ kratos/applications/swimming_DEM_application/python_scripts/derivative_recovery/standard_recoverer.py
@@ -7,26 +7,21 @@ from . import recoverer
 class StandardGradientRecoverer(recoverer.GradientRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
         recoverer.GradientRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
-
     def RecoverGradientOfScalar(self, scalar_variable, gradient_variable):
-        self.cplusplus_recovery_tool.CalculateGradient(scalar_variable, gradient_variable)
-
+        self.cplusplus_recovery_tool.CalculateGradient(self.model_part, scalar_variable, gradient_variable)
     def RecoverGradientOfVector(self, vector_variable, gradient_variable_x, gradient_variable_y, gradient_variable_z):
-        self.cplusplus_recovery_tool.CalculateGradient(vector_variable, gradient_variable_x, gradient_variable_y, gradient_variable_z)
-
+        self.cplusplus_recovery_tool.CalculateGradient(self.model_part, vector_variable, gradient_variable_x, gradient_variable_y, gradient_variable_z)
     def RecoverGradientOfVelocity(self):
-        self.cplusplus_recovery_tool.CalculateGradient(VELOCITY, VELOCITY_X_GRADIENT, VELOCITY_Y_GRADIENT, VELOCITY_Z_GRADIENT)
+        self.RecoverGradientOfVector(VELOCITY, VELOCITY_X_GRADIENT, VELOCITY_Y_GRADIENT, VELOCITY_Z_GRADIENT)
 
 class StandardMaterialAccelerationRecoverer(recoverer.MaterialAccelerationRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
         recoverer.MaterialAccelerationRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
-
     def RecoverMaterialAcceleration(self):
         self.cplusplus_recovery_tool.CalculateVectorMaterialDerivative(self.model_part, VELOCITY, ACCELERATION, MATERIAL_ACCELERATION)
 
 class StandardLaplacianRecoverer(recoverer.LaplacianRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
         recoverer.LaplacianRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
-
     def RecoverVectorLaplacian(self, vector_variable, laplacian_variable):
         self.cplusplus_recovery_tool.CalculateVectorLaplacian(self.model_part, vector_variable, laplacian_variable)
