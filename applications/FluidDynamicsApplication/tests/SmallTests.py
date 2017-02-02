4cfe85f06920ca0589b3cbc6ef191edd71255160
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2017-02-02 19:38:31 +0000

    Addition of manufactured solution tests.

--- applications/FluidDynamicsApplication/tests/SmallTests.py
+++ applications/FluidDynamicsApplication/tests/SmallTests.py
@@ -6,6 +6,7 @@ from KratosMultiphysics import *
 # Import KratosUnittest
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosExecuteEmbeddedTest as ExecuteEmbeddedTest
+import KratosExecuteManufacturedSolutionTest as ExecuteManufacturedSolutionTest
 
 # This utiltiy will control the execution scope in case we need to acces files or we depend
 # on specific relative locations of the files.
@@ -34,6 +35,27 @@ class EmbeddedTestFactory(KratosUnittest.TestCase):
 
             # Create the test
             self.test = ExecuteEmbeddedTest.KratosExecuteEmbeddedTest(ProjectParameters)
+
+    def test_execution(self):
+        # Within this location context:
+        with controlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            self.test.Solve()
+
+    def tearDown(self):
+        pass
+
+
+class ManufacturedSolutionTestFactory(KratosUnittest.TestCase):
+
+    def setUp(self):
+        # Within this location context:
+        with controlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            # Get the ProjectParameters file
+            parameter_file = open(self.file_name + "_parameters.json", 'r')
+            ProjectParameters = Parameters(parameter_file.read())
+
+            # Create the test
+            self.test = ExecuteManufacturedSolutionTest.KratosExecuteManufacturedSolutionTest(ProjectParameters)
 
     def test_execution(self):
         # Within this location context:
@@ -79,3 +101,7 @@ class EmbeddedCouetteImposedTest(EmbeddedTestFactory):
 
 class EmbeddedReservoirTest(EmbeddedTestFactory):
     file_name = "EmbeddedReservoirTest/EmbeddedReservoirTest"
+
+
+class ManufacturedSolutionTest(ManufacturedSolutionTestFactory):
+    file_name = "ManufacturedSolutionTest/ManufacturedSolutionTest"
