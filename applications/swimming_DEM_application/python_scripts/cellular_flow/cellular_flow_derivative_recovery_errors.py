bdda7812e5502ebff46b028da1e400ea51098096
gcasas <gcasas@cimne.upc.edu>
2017-02-02 22:19:54 +0000

    reading from an h5py file and simplifying

--- applications/swimming_DEM_application/python_scripts/cellular_flow/cellular_flow_derivative_recovery_errors.py
+++ applications/swimming_DEM_application/python_scripts/cellular_flow/cellular_flow_derivative_recovery_errors.py
@@ -1,17 +1,13 @@
 import math
 import matplotlib.pyplot as plt
+import h5py
 
-regular_mesh = True
+regular_mesh = False
 
 show_math_deriv_or_laplacian = 'L' # 'M' or 'L'
 n_divs = [10, 20, 40, 80]
 marker_size = 10
 line_width = 1
-
-if regular_mesh:
-    sizes = [1.0 / n_div for n_div in n_divs]
-else:
-    sizes = sizes = [0.05, 0.025, 0.0125]
 
 mat_deriv_recovery_types = [1, 2, 3, 4, 5, 6, 7]
 laplacian_recovery_types = [1, 3, 4, 6, 7]
@@ -22,41 +18,37 @@ def CalculateLastSlopes(sizes, results):
     slope = abs(Delta_result/Delta_size)
     return slope
 
-def FillVectors(recovery_type, average_errors, max_errors, laplacian_or_mat_deriv):
+def FillVectors(recovery_type, sizes, average_errors, max_errors, laplacian_or_mat_deriv):
+    del sizes[:]
+    del average_errors[:]
+    del max_errors[:]
     if laplacian_or_mat_deriv == 'M':
-        recovery_tag = 'mat_deriv_errors_'
+        recovery_tag = 'material derivative/method = ' + str(recovery_type)
     else:
-        recovery_tag = 'laplacian_errors_'
+        recovery_tag = 'laplacian/method = ' + str(recovery_type)
+    if regular_mesh:
+        mesh_tag = 'regular mesh'
+    else:
+        mesh_tag = 'irregular mesh'
+    recovery_tag += '/' + mesh_tag
 
-    for i in range(len(average_errors)):
-        average_errors[i] = 0.0
-        max_errors[i] = float('-inf')
-
-    for i_size, (n_div, size) in enumerate(zip(n_divs, sizes)):
-        if regular_mesh:
-            mesh_tag = 'ndiv_' + str(n_div)
-        else:
-            mesh_tag = 'h_' + str(size)
-
-        with open('errors_recorded/' + recovery_tag + mesh_tag + '_type_' + str(recovery_type) + '.txt', 'r') as errors_file:
-            for i_line, line in enumerate(errors_file):
-                numbers_str = line.split()
-                error = float(numbers_str[0])
-                average_errors[i_size] += error
-                max_errors[i_size] = max(max_errors[i_size], error)
-                average_errors[i_size] /= i_line + 1
-
-    return average_errors, max_errors
+    with h5py.File('errors_recorded/recovery_errors.hdf5', 'r') as f:
+        for i, size_dset in enumerate(f[recovery_tag].values()):
+            size = float(size_dset.name.split()[-1])
+            if regular_mesh:
+                size = 1. / size
+            sizes.append(size)
+            average_errors.append(size_dset[0])
+            max_errors.append(size_dset[1])
 
 min_error = float("inf")
 max_error = - float("inf")
 
 if show_math_deriv_or_laplacian == 'M':
     marker_type = 'v'
-    mat_deriv_average_errors = [0. for size in sizes]
-    mat_deriv_max_errors = [float('-inf') for size in sizes]
     for method in mat_deriv_recovery_types:
-        FillVectors(method, mat_deriv_average_errors, mat_deriv_max_errors, 'M')
+        FillVectors(method, sizes, mat_deriv_average_errors, mat_deriv_max_errors, 'M')
+        print(sizes)
         if method == 1:
             mat_deriv_type = 'standard'
             color = 'r'
@@ -92,10 +84,11 @@ if show_math_deriv_or_laplacian == 'M':
 
 elif show_math_deriv_or_laplacian == 'L':
     marker_type = '^'
-    laplacian_average_errors = [0. for size in sizes]
-    laplacian_max_errors = [float('-inf') for size in sizes]
+    laplacian_average_errors = []
+    laplacian_max_errors = []
+    sizes = []
     for method in laplacian_recovery_types:
-        FillVectors(method, laplacian_average_errors, laplacian_max_errors, 'L')
+        FillVectors(method, sizes, laplacian_average_errors, laplacian_max_errors, 'L')
         if method == 1:
             laplacian_type = 'standard'
             color = 'r'
@@ -138,7 +131,7 @@ else:
     plot_name = 'derivative_recovery_errors_irregular.pdf'
 
 plt.plot(sizes, slope, linestyle='dashed',  label='slope = ' + str(expected_order))
-plt.ylim((min_error / 10, max_error * 10))
+plt.ylim((min(slope) / 10, max_error * 10))
 plt.xlabel('$h$', fontsize = 20)
 
 if show_math_deriv_or_laplacian == 'M':
