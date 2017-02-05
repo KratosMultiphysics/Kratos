d95425ef9012a0f14697ab3f000adcca67843353
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-02-05 10:57:20 +0000

    changing to corret name

--- kratos/applications/swimming_DEM_application/custom_elements/calculate_gradient_Pouliot_2012.cpp
+++ kratos/applications/swimming_DEM_application/custom_elements/calculate_gradient_Pouliot_2012.cpp
@@ -9,18 +9,21 @@ void ComputeGradientFortin2012<TDim, TNumNodes>::CalculateLocalSystem(MatrixType
                                   ProcessInfo& rCurrentProcessInfo)
 {
     BaseType::CalculateLocalSystem(rLeftHandSideMatrix, rRightHandSideVector, rCurrentProcessInfo);
-    const double h_inv = 1.0 / this->GetGeometry().MinEdgeLength();
-    const double epsilon  = 100 * h_inv * h_inv; // we divide by h^3 to scale the L2 system to the same RHS order of magnitude as the Forting 2012 system; then we multiply by h to make the sum of systems of order 2 (the L2 system is accurate of order 1 only)
+    //const double h_inv = 1.0 / this->GetGeometry().MinEdgeLength();
+    //const double epsilon = 1e-6 * h_inv * h_inv; // we divide by h^3 to scale the L2 system to the same RHS order of magnitude as the Forting 2012 system; then we multiply by h to make the sum of systems of order 2 (the L2 system is accurate of order 1 only)
+    const double epsilon = 1e-6 * this->GetGeometry().MinEdgeLength();
     const unsigned int LocalSize(TDim * TNumNodes);
 
     for (unsigned int i=0; i<LocalSize; ++i){
         for (unsigned int j=0; j<LocalSize; ++j){
-            rLeftHandSideMatrix(i, j) *= epsilon;
+            rLeftHandSideMatrix(i, j) *= 0;
         }
-        rRightHandSideVector(i) *= epsilon;
+        rRightHandSideVector(i) *= 0;
     }
 
     AddFortin2012LHS(rLeftHandSideMatrix, rCurrentProcessInfo);
+
+    AddFortin2012StabilizationLHS(epsilon, rLeftHandSideMatrix, rCurrentProcessInfo);
 
     AddFortin2012RHS(rRightHandSideVector, rCurrentProcessInfo);
 }
@@ -121,6 +124,61 @@ void ComputeGradientFortin2012<TDim, TNumNodes>::AddFortin2012LHS(MatrixType& rL
         edge_lengths_inv[e] = he_inv;
         le *= he_inv;
         AssembleEdgeLHSContribution(edges[e], le, rLeftHandSideMatrix);
+    }
+}
+
+template <unsigned int TDim, unsigned int TNumNodes>
+void ComputeGradientFortin2012<TDim, TNumNodes>::AddFortin2012StabilizationLHS(const double epsilon, MatrixType& rLeftHandSideMatrix, ProcessInfo& rCurrentProcessInfo)
+{
+    const unsigned int NEdges = 3 * TNumNodes - 6; // works in 2D and 3D
+
+    unsigned int edges[NEdges][2];
+
+    unsigned int i_edge = 0;
+    for (unsigned int i = 0; i < TNumNodes - 1; ++i){
+        for (unsigned int j = i + 1; j < TNumNodes; ++j){
+            edges[i_edge][0]   = i;
+            edges[i_edge++][1] = j;
+        }
+    }
+
+    array_1d<array_1d<double, 3>, NEdges> EdgeVectors; // stores the [lx, ly(, lz)] vectors for all edges
+    const GeometryType& rGeom = this->GetGeometry();
+
+    for (unsigned int e = 0; e < NEdges; ++e){
+        array_1d<double, 3>& le = EdgeVectors[e];
+        noalias(le) = rGeom[edges[e][1]].Coordinates() - rGeom[edges[e][0]].Coordinates();
+    }
+
+    for (unsigned int d_k = 0; d_k < TDim; ++d_k){
+
+        const double a = fabs(EdgeVectors[0][d_k]);
+        const double b = fabs(EdgeVectors[1][d_k]);
+        const double c = fabs(EdgeVectors[2][d_k]);
+        const double d = fabs(EdgeVectors[3][d_k]);
+        const double a_i = a > epsilon ? 1.0 / a: 1.0;
+        const double b_i = b > epsilon ? 1.0 / b: 1.0;
+        const double c_i = c > epsilon ? 1.0 / c: 1.0;
+        const double d_i = d > epsilon ? 1.0 / d: 1.0;
+        const double a_plus_b_i = (a + b) > epsilon ? 1.0 / (a + b): 1.0;
+        const double b_plus_c_i = (b + c) > epsilon ? 1.0 / (b + c): 1.0;
+        const double c_plus_d_i = (c + d) > epsilon ? 1.0 / (c + d): 1.0;
+        const double d_plus_a_i = (d + a) > epsilon ? 1.0 / (d + a): 1.0;
+        rLeftHandSideMatrix(d, d)         += epsilon * b_i * a_plus_b_i;
+        rLeftHandSideMatrix(d, d + 1)     -= epsilon * a_i * b_i;
+        rLeftHandSideMatrix(d, d + 2)     += epsilon * a_i * a_plus_b_i;
+
+        rLeftHandSideMatrix(d + 1, d)     += epsilon * c_i * b_plus_c_i;
+        rLeftHandSideMatrix(d + 1, d + 1) -= epsilon * b_i * c_i;
+        rLeftHandSideMatrix(d + 1, d + 2) += epsilon * b_i * b_plus_c_i;
+
+        rLeftHandSideMatrix(d + 2, d + 1) += epsilon * c_i * c_plus_d_i;
+        rLeftHandSideMatrix(d + 2, d + 2) += epsilon * d_i * c_plus_d_i;
+        rLeftHandSideMatrix(d + 2, d + 3) -= epsilon * c_i * d_i;
+
+        rLeftHandSideMatrix(d + 3, d)     -= epsilon * d_i * a_i;
+        rLeftHandSideMatrix(d + 3, d + 1) += epsilon * d_i * d_plus_a_i;
+        rLeftHandSideMatrix(d + 3, d + 3) += epsilon * a_i * d_plus_a_i;
     }
 }
 
