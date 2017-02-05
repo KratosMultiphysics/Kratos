acdda2b39624c46ce827586433880f1b0867c4e4
gcasas <gcasas@cimne.upc.edu>
2017-02-05 11:15:07 +0000

    correcting names Fortin -> Pouliot

--- applications/swimming_DEM_application/python_scripts/derivative_recovery/fortin_2012_recoverer.py
+++ applications/swimming_DEM_application/python_scripts/derivative_recovery/fortin_2012_recoverer.py
@@ -5,25 +5,25 @@ from KratosMultiphysics.SwimmingDEMApplication import *
 from . import recoverer
 from . import L2_projection_recoverer
 
-class Fortin2012GradientRecoverer(L2_projection_recoverer.L2ProjectionGradientRecoverer):
+class Pouliot2012GradientRecoverer(L2_projection_recoverer.L2ProjectionGradientRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
         L2_projection_recoverer.L2ProjectionGradientRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
-        self.element_type = "ComputeGradientFortin20123D"
+        self.element_type = "ComputeGradientPouliot20123D"
         self.condition_type = "ComputeLaplacianSimplexCondition3D"
         self.FillUpModelPart(self.element_type, self.condition_type)
         self.DOFs = (VELOCITY_Z_GRADIENT_X, VELOCITY_Z_GRADIENT_Y, VELOCITY_Z_GRADIENT_Z)
         self.AddDofs(self.DOFs)
         self.calculate_vorticity = self.pp.CFD_DEM.lift_force_type
 
-class Fortin2012MaterialAccelerationRecoverer(Fortin2012GradientRecoverer, L2_projection_recoverer.L2ProjectionMaterialAccelerationRecoverer):
+class Pouliot2012MaterialAccelerationRecoverer(Pouliot2012GradientRecoverer, L2_projection_recoverer.L2ProjectionMaterialAccelerationRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
         L2_projection_recoverer.L2ProjectionMaterialAccelerationRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
-        Fortin2012GradientRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
+        Pouliot2012GradientRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
 
-class Fortin2012LaplacianRecoverer(L2_projection_recoverer.L2ProjectionDerivativesRecoverer, recoverer.LaplacianRecoverer):
+class Pouliot2012LaplacianRecoverer(L2_projection_recoverer.L2ProjectionDerivativesRecoverer, recoverer.LaplacianRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
         recoverer.LaplacianRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
-        Fortin2012DerivativesRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
+        Pouliot2012DerivativesRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
         self.element_type = "ComputeLaplacianSimplex3D"
         self.condition_type = "ComputeLaplacianSimplexCondition3D"
         self.FillUpModelPart(self.element_type, self.condition_type)
