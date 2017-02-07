e028457dd5d8f3690af38c71478a5bc1a3e6e004
gcasas <gcasas@4358b7d9-91ec-4505-bf62-c3060f61107a>
2017-02-07 19:16:15 +0000

    adding option to pre-compute derivataives

--- kratos/applications/swimming_DEM_application/python_scripts/derivative_recovery/pouliot_2012_recoverer.py
+++ kratos/applications/swimming_DEM_application/python_scripts/derivative_recovery/pouliot_2012_recoverer.py
@@ -16,9 +16,27 @@ class Pouliot2012GradientRecoverer(L2_projection_recoverer.L2ProjectionGradientR
         self.calculate_vorticity = self.pp.CFD_DEM.lift_force_type
 
 class Pouliot2012MaterialAccelerationRecoverer(Pouliot2012GradientRecoverer, L2_projection_recoverer.L2ProjectionMaterialAccelerationRecoverer):
-    def __init__(self, pp, model_part, cplusplus_recovery_tool):
+    def __init__(self, pp, model_part, cplusplus_recovery_tool, do_pre_recovery = False):
         L2_projection_recoverer.L2ProjectionMaterialAccelerationRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
         Pouliot2012GradientRecoverer.__init__(self, pp, model_part, cplusplus_recovery_tool)
+        self.do_pre_recovery = do_pre_recovery
+
+        if self.do_pre_recovery:
+            scheme = ResidualBasedIncrementalUpdateStaticScheme()
+            amgcl_smoother = AMGCLSmoother.ILU0
+            amgcl_krylov_type = AMGCLIterativeSolverType.BICGSTAB
+            tolerance = 1e-6
+            max_iterations = 1000
+            verbosity = 0 #0->shows no information, 1->some information, 2->all the information
+            gmres_size = 50
+
+            if self.use_lumped_mass_matrix:
+                linear_solver = CGSolver()
+            else:
+                linear_solver = AMGCLSolver(amgcl_smoother, amgcl_krylov_type, tolerance, max_iterations, verbosity,gmres_size)
+
+            self.aux_recovery_strategy = ResidualBasedDerivativeRecoveryStrategy(self.recovery_model_part, scheme, linear_solver, False, True, False, False)
+            self.aux_recovery_strategy.SetEchoLevel(0)
 
 class Pouliot2012LaplacianRecoverer(L2_projection_recoverer.L2ProjectionDerivativesRecoverer, recoverer.LaplacianRecoverer):
     def __init__(self, pp, model_part, cplusplus_recovery_tool):
@@ -31,4 +49,12 @@ class Pouliot2012LaplacianRecoverer(L2_projection_recoverer.L2ProjectionDerivati
         self.AddDofs(self.DOFs)
     def RecoverVectorLaplacian(self, vector_variable, laplacian_variable):
         self.SetToZero(VELOCITY_LAPLACIAN)
+        self.recovery_strategy.Solve()
+
+    def Solve(self):
+        print("\nSolving for the fluid acceleration...")
+        sys.stdout.flush()
+        self.SetToZero(VELOCITY_Z_GRADIENT)
+        if self.do_pre_recovery:
+            self.aux_recovery_strategy.Solve()
         self.recovery_strategy.Solve()
