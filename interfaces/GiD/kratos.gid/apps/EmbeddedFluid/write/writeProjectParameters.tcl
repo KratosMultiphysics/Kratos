33c793708763271e71fb33afb88cd5f3f549553c
Rubén Zorrilla <rzorrilla@cimne.upc.edu>
2017-02-10 17:32:07 +0000

    Adaptivity mesh settings added (interface and main file modifications)

--- interfaces/GiD/kratos.gid/apps/EmbeddedFluid/write/writeProjectParameters.tcl
+++ interfaces/GiD/kratos.gid/apps/EmbeddedFluid/write/writeProjectParameters.tcl
@@ -2,14 +2,32 @@
 proc ::EmbeddedFluid::write::getParametersDict { } {
       set param_dict [Fluid::write::getParametersDict]
 
-      # Set the auxiliar embedded fluid application processes dictionary list
+      ## Set the meshing adaptivity settings
+      set mesh_adaptivity [write::getValue EMBFLAdaptivitySettings mesh_adaptivity]
+      # Meshing adaptivity switch on/off in problem data dict
+      set new_problem_data [dict get $param_dict problem_data]
+      dict set new_problem_data "mesh_adaptivity" $mesh_adaptivity
+      dict set param_dict problem_data $new_problem_data
+      # Set the meshing adaptivity process list
+      if {$mesh_adaptivity eq "Yes"} {
+          dict set param_dict mesh_adaptivity_process_list [list [getMeshAdaptivityProcessDict]]
+      } else {
+          dict set param_dict mesh_adaptivity_process_list [list]
+      }
+
+      ## Set the auxiliar embedded fluid application processes dictionary list
       dict set param_dict auxiliar_process_list [getAuxiliarProcessList]
 
-      # Set the solver settings dictionary
+      ## Set the solver settings dictionary
       set solverSettingsDict [dict get $param_dict solver_settings]
+      # If drag has to be computed, ensure that "compute_reactions" is set to true
+      set compute_embedded_drag [write::getValue EMBFLEmbeddedDrag compute_embedded_drag]
+      if {$compute_embedded_drag eq "Yes"} {
+          dict set solverSettingsDict "compute_reactions" true
+      }
       set solverSettingsDict [dict merge $solverSettingsDict [write::getSolversParametersDict EmbeddedFluid] ]
 
-      # Set the distance reading settings dictionary
+      ## Set the distance reading settings dictionary
       set dist_settings_dict [dict create]
       set dist_mode [write::getValue EMBFLDistanceSettings ReadingMode]
       dict set dist_settings_dict import_mode $dist_mode
@@ -34,8 +52,12 @@ proc EmbeddedFluid::write::getAuxiliarProcessList {} {
 
     # Append the distance modification process
     lappend auxiliar_process_list [getDistanceModificationDict]
-    # Append the embedded drag process dictionary
-    lappend auxiliar_process_list [getEmbeddedDragProcessDict]
+
+    # If required, append the embedded drag process dictionary
+    set compute_embedded_drag [write::getValue EMBFLEmbeddedDrag compute_embedded_drag]
+    if {$compute_embedded_drag eq "Yes"} {
+        lappend auxiliar_process_list [getEmbeddedDragProcessDict]
+    }
 
     return $auxiliar_process_list
 }
@@ -60,11 +82,34 @@ proc EmbeddedFluid::write::getEmbeddedDragProcessDict {} {
     dict set pdict "process_name" "ComputeEmbeddedDragProcess"
         set params [dict create]
         dict set params "mesh_id" 0
-        dict set params "fluid_model_part" [lindex [write::getPartsMeshId] 0]
+        dict set params "model_part_name" [lindex [write::getPartsMeshId] 0]
         dict set params "write_drag_output_file" [write::getValue EMBFLEmbeddedDrag write_drag_output_file]
         dict set params "print_drag_to_screen" [write::getValue EMBFLEmbeddedDrag print_drag_to_screen]
         dict set params "interval" [write::getInterval [write::getValue EMBFLEmbeddedDrag Interval] ]
     dict set pdict "Parameters" $params
+
+    return $pdict
+}
+
+proc EmbeddedFluid::write::getMeshAdaptivityProcessDict {} {
+    set pdict [dict create]
+    dict set pdict "python_module" "mmg_process"
+    dict set pdict "kratos_module" "KratosMultiphysics.MeshingApplication"
+    dict set pdict "process_name" "MmgProcess"
+        # MmgProcess settings dictionary
+        set params_dict [dict create]
+        dict set params_dict "mesh_id" 0
+        dict set params_dict "model_part_name" "MainModelPart"
+        dict set params_dict "initial_step" [write::getValue EMBFLAdaptivitySettings initial_step]
+        dict set params_dict "step_frequency" [write::getValue EMBFLAdaptivitySettings step_frequency]
+        dict set params_dict "initial_remeshing" [write::getValue EMBFLAdaptivitySettings initial_remeshing]
+            # Set anisotropic parameters section
+            set anisotropy_parameters_dict [dict create]
+            dict set anisotropy_parameters_dict "hmin_over_hmax_anisotropic_ratio" [write::getValue EMBFLAdaptivitySettings hmin_over_hmax_anisotropic_ratio]
+            dict set anisotropy_parameters_dict "boundary_layer_min_size_ratio" [write::getValue EMBFLAdaptivitySettings boundary_layer_min_size_ratio]
+        dict set params_dict "anisotropy_parameters" $anisotropy_parameters_dict
+        dict set params_dict "echo_level" [write::getValue EMBFLAdaptivitySettings echo_level]
+    dict set pdict "Parameters" $params_dict
 
     return $pdict
 }
