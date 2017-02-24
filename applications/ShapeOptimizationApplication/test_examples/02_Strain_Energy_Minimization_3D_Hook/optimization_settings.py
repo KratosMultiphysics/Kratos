1aea109c0d630f241ecd424067760a6414ca74d1
dbaumgaertner <daniel.baumgaertner@tum.de>
2017-02-24 17:01:12 +0100

    Updated hook test example

--- applications/ShapeOptimizationApplication/test_examples/02_Strain_Energy_Minimization_3D_Hook/optimization_settings.py
+++ applications/ShapeOptimizationApplication/test_examples/02_Strain_Energy_Minimization_3D_Hook/optimization_settings.py
@@ -36,25 +36,34 @@ design_output_mode = "relative"
 #          "total"      - X is defined relative to initial design
 #          "absolute"   - X is defined as absolute values (coordinates)
 
+
+# ================================================================================================================
+# Case: design_control = "vertex_morphing"
+# ================================================================================================================
+
+model_input_filename = "hook"
+design_surface_sub_model_name = "design_surface"
 domain_size = 3
 # options: 2 or 3 for 2D or 3D optimization patch
 
-# Case: design_control = "vertex_morphing"
-design_surface_name = "design_surface"
 filter_function = "linear"
 # options: "gaussian"
 #          "linear"
-filter_size = 35
+filter_size = 40
+
 use_mesh_preserving_filter_matrix = False
 # options: True    - surface normal information used in the filter matrix
 #        : False   - complete filter matrix is used
-perform_edge_damping = False
-# options: True    - edge damping is applied with the settings below
-#        : False   - no edge damping is applied, settings below can be ignored
-damped_edges = [ ]
-# damped_edges = [ [edge_sub_model_part_name_1, damp_in_X, damp_in_Y, damp_in_Z, damping_function, damping_radius ],
-#                  [edge_sub_model_part_name_2, damp_in_X, damp_in_Y, damp_in_Z, damping_function, damping_radius ],
-#                  ... ]
+
+perform_damping = True
+# options: True    - damping is applied with the settings below
+#        : False   - no damping is applied, settings below can be ignored
+
+damping_regions = [ ["support", False, False, True, "linear", 50 ],
+                    ["tip_face", True, True, True, "linear", 10 ] ]
+# damping_region = [ [sub_model_part_name_1, damp_in_X, damp_in_Y, damp_in_Z, damping_function, damping_radius ],
+#                    [sub_model_part_name_2, damp_in_X, damp_in_Y, damp_in_Z, damping_function, damping_radius ],
+#                    ... ]
 # options for damping function: "cosine"
 #                               "linear"
 
@@ -87,7 +96,7 @@ lambda_0 = 0.0
 
 # Only constant step-size is implemented yet
 normalize_search_direction = True
-step_size = 1.5 # e.g. 5 for active normalization or 1e7 for inactive normalization
+step_size = 3
 
 # ================================================================================================================
 # For GID output 
