abe65943237e33b996925e3ebe4a52511038e278
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2017-02-24 11:33:00 +0100

    Update configure.sh
    Correcting the configure.sh , incompatible with old processors

--- applications/MeshingApplication/custom_external_libraries/mmg/build/configure.sh
+++ applications/MeshingApplication/custom_external_libraries/mmg/build/configure.sh
@@ -4,8 +4,12 @@ cmake .. \
 -DSCOTCH_INCLUDE_DIR="/usr/include/scotch/"	            \
 -DSCOTCH_LIBRARY="libscotchmetis-5.1.so"	            \
 -DSCOTCHERR_LIBRARY="libscotcherr-5.1.so"	            \
--DCMAKE_CXX_FLAGS="-O3 -mavx2 -fPIC -fopenmp"               \
--DCMAKE_C_FLAGS="-O3 -mavx2 -fPIC -fopenmp"                 \
+-DCMAKE_CXX_FLAGS="-O3 -fPIC -fopenmp"               \
+-DCMAKE_C_FLAGS="-O3 -fPIC -fopenmp"                 \
+
+# If you ha modern processor use this instructions instead
+# -DCMAKE_CXX_FLAGS="-O3 -mavx2 -fPIC -fopenmp"               \
+# -DCMAKE_C_FLAGS="-O3 -mavx2 -fPIC -fopenmp"                 \
 
 #decomment this to have it verbose
 # make VERBOSE=1 -j4
