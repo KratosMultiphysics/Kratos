12924de4eb3c8d4103896ba475a8231dee0e2b22
Riccardo Rossi <rrossi@cimne.upc.edu>
2017-02-28 15:05:28 +0100

    adding test to verify the behaviour of gid_io after the change from IS_INACTIVE to the flag ACTIVE

---
+++ kratos/tests/test_gid_io.py
@@ -0,0 +1,88 @@
+﻿from __future__ import print_function, absolute_import, division
+
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+import KratosMultiphysics
+from gid_output_process import GiDOutputProcess
+import os
+
+def GetFilePath(fileName):
+    
+    return os.path.dirname(os.path.realpath(__file__)) + "/" + fileName
+
+class TestGidIO(KratosUnittest.TestCase):
+
+    def __WriteOutput(self, model_part, output_file):
+        
+        gid_output = GiDOutputProcess(model_part,
+                                    output_file,
+                                    KratosMultiphysics.Parameters("""
+                                        {
+                                            "result_file_configuration": {
+                                                "gidpost_flags": {
+                                                    "GiDPostMode": "GiD_PostAscii",
+                                                    "WriteDeformedMeshFlag": "WriteUndeformed",
+                                                    "WriteConditionsFlag": "WriteConditions",
+                                                    "MultiFileFlag": "SingleFile"
+                                                },
+                                                "file_label": "time",
+                                                "output_control_type": "step",
+                                                "output_frequency": 1.0,
+                                                "body_output": true,
+                                                "node_output": false,
+                                                "skin_output": false,
+                                                "plane_output": [],
+                                                "nodal_results": ["DISPLACEMENT","VISCOSITY"],
+                                                "nodal_nonhistorical_results": [],
+                                                "nodal_flags_results": [],
+                                                "gauss_point_results": [],
+                                                "additional_list_files": []
+                                            }
+                                        }
+                                        """)
+                                    )
+        
+        gid_output.ExecuteInitialize()
+        gid_output.ExecuteBeforeSolutionLoop()
+        gid_output.ExecuteInitializeSolutionStep()
+        gid_output.PrintOutput()
+        gid_output.ExecuteFinalizeSolutionStep()
+        gid_output.ExecuteFinalize()
+        
+    def __InitialRead(self):
+        model_part = KratosMultiphysics.ModelPart("Main")
+        model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISPLACEMENT)
+        model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VISCOSITY)
+        KratosMultiphysics.ModelPartIO(GetFilePath("test_model_part_io_read")).ReadModelPart(model_part)
+        model_part.SetBufferSize(2)
+        return model_part
+    
+    def __Check(self,output_file,reference_file):
+        import filecmp
+        self.assertTrue( filecmp.cmp(os.path.dirname(os.path.realpath(__file__)) + "/" +output_file, os.path.dirname(os.path.realpath(__file__)) + "/" +reference_file))
+        
+    def test_gid_io_all(self):
+        model_part = self.__InitialRead()
+        
+        self.__WriteOutput(model_part,"all_active_out")
+        
+        self.__Check("all_active_out_0.post.msh","all_active_ref.ref")
+        
+    def test_gid_io_deactivation(self):
+        model_part = self.__InitialRead()
+
+        model_part.Elements[3].Set(KratosMultiphysics.ACTIVE,False)
+        model_part.Elements[1796].Set(KratosMultiphysics.ACTIVE,False)
+        
+        model_part.Conditions[1947].Set(KratosMultiphysics.ACTIVE,False)
+        model_part.Conditions[1948].Set(KratosMultiphysics.ACTIVE,False)
+        
+        self.__WriteOutput(model_part,"deactivated_out")
+        
+        self.__Check("deactivated_out.mdpa_0.post.msh","deactivated_ref.ref")
+        
+        
+        
+
+        
+if __name__ == '__main__':
+    KratosUnittest.main()
