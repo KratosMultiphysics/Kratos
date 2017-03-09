fa57eb04148fd1a82fad5067716cae639c2347e9
Unknown <jordi.cotela@gmail.com>
2017-03-09 18:12:21 +0100

    Adding test for issue #133
    Note that it is skipped by default

--- kratos/tests/test_gid_io.py
+++ kratos/tests/test_gid_io.py
@@ -6,13 +6,13 @@ from gid_output_process import GiDOutputProcess
 import os
 
 def GetFilePath(fileName):
-    
+
     return os.path.dirname(os.path.realpath(__file__)) + "/" + fileName
 
 class TestGidIO(KratosUnittest.TestCase):
 
     def __WriteOutput(self, model_part, output_file):
-        
+
         gid_output = GiDOutputProcess(model_part,
                                     output_file,
                                     KratosMultiphysics.Parameters("""
@@ -40,14 +40,14 @@ class TestGidIO(KratosUnittest.TestCase):
                                         }
                                         """)
                                     )
-        
+
         gid_output.ExecuteInitialize()
         gid_output.ExecuteBeforeSolutionLoop()
         gid_output.ExecuteInitializeSolutionStep()
         gid_output.PrintOutput()
         gid_output.ExecuteFinalizeSolutionStep()
         gid_output.ExecuteFinalize()
-        
+
     def __InitialRead(self):
         model_part = KratosMultiphysics.ModelPart("Main")
         model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISPLACEMENT)
@@ -55,34 +55,52 @@ class TestGidIO(KratosUnittest.TestCase):
         KratosMultiphysics.ModelPartIO(GetFilePath("test_model_part_io_read")).ReadModelPart(model_part)
         model_part.SetBufferSize(2)
         return model_part
-    
+
     def __Check(self,output_file,reference_file):
         import filecmp
         self.assertTrue( filecmp.cmp(os.path.dirname(os.path.realpath(__file__)) + "/" +output_file, os.path.dirname(os.path.realpath(__file__)) + "/" +reference_file))
-        
+
     def test_gid_io_all(self):
         model_part = self.__InitialRead()
-        
+
         self.__WriteOutput(model_part,"all_active_out")
-        
+
         self.__Check("all_active_out_0.post.msh","all_active_ref.ref")
-        
+
     def test_gid_io_deactivation(self):
         model_part = self.__InitialRead()
 
         model_part.Elements[3].Set(KratosMultiphysics.ACTIVE,False)
         model_part.Elements[1796].Set(KratosMultiphysics.ACTIVE,False)
-        
+
         model_part.Conditions[1947].Set(KratosMultiphysics.ACTIVE,False)
         model_part.Conditions[1948].Set(KratosMultiphysics.ACTIVE,False)
-        
-        self.__WriteOutput(model_part,"deactivated_out")
-        
-        self.__Check("deactivated_out.mdpa_0.post.msh","deactivated_ref.ref")
-        
-        
-        
 
-        
+        self.__WriteOutput(model_part,"deactivated_out")
+
+        self.__Check("deactivated_out.mdpa_0.post.msh","deactivated_ref.ref")
+
+    @UnitTest.skip("testDoubleFreeError currently produces a SegFault and stops execution.")
+    def testDoubleFreeError(self):
+
+        output_file_1 = "outFile"
+        output_file_2 = "otherFile"
+
+        gid_mode = GiDPostMode.GiD_PostAscii
+        multifile = MultiFileFlag.MultipleFiles
+        deformed_mesh_flag = WriteDeformedMeshFlag.WriteUndeformed
+        write_conditions = WriteConditionsFlag.WriteConditions
+
+        gid_io_1 = GidIO(output_file_1, gid_mode, multifile,
+                         deformed_mesh_flag, write_conditions)
+
+        gid_io_2 = GidIO(output_file_2, gid_mode, multifile,
+                         deformed_mesh_flag, write_conditions)
+
+        gid_io_1 = None
+        gid_io_2 = None
+
+
+
 if __name__ == '__main__':
     KratosUnittest.main()
