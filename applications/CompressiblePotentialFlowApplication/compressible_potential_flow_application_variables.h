e8280dd14141ceccc04d95517de5ab117a5eeb82
Riccardo Rossi <rrossi@cimne.upc.edu>
2017-03-15 22:24:51 +0100

    minor change in the header

--- applications/CompressiblePotentialFlowApplication/compressible_potential_flow_application_variables.h
+++ applications/CompressiblePotentialFlowApplication/compressible_potential_flow_application_variables.h
@@ -7,7 +7,7 @@
 //  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
-//  Main authors:    @{KRATOS_APP_AUTHOR}
+//  Main authors:    Riccardo Rossi
 //
 
 #if !defined(KRATOS_COMPRESSIBLE_POTENTIAL_FLOW_APPLICATION_VARIABLES_H_INCLUDED )
