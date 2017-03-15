b007c6ca0d7d53495ac93d29842aa769b1c76102
loumalouomega <vmataix@cimne.upc.edu>
2017-03-15 12:31:41 +0100

    Adding new test

--- applications/ContactStructuralMechanicsApplication/tests/TestExactIntegration.py
+++ applications/ContactStructuralMechanicsApplication/tests/TestExactIntegration.py
@@ -210,6 +210,89 @@ class TestTriangleExactIntegration2(KratosUnittest.TestCase):
         self.assertAlmostEqual(MatrixSolution[2,0], 0.5)
         self.assertAlmostEqual(MatrixSolution[2,1], 1.0/3.0)
         self.assertAlmostEqual(MatrixSolution[2,2], 1.0/12.0)
+        
+class TestTriangleExactIntegration3(KratosUnittest.TestCase):
+    def test_execution(self):
+        model_part = KratosMultiphysics.ModelPart("Main")
+        model_part.SetBufferSize(3)
+        model_part.AddProperties(KratosMultiphysics.Properties(1))
+        
+        normal = KratosMultiphysics.Vector(3)
+        normal[0] = 0.0
+        normal[1] = 0.0
+        normal[2] = 1.0
+        
+        # Triangle 1
+        model_part.CreateNewNode(1, 0.00,0.00,0.00)
+        model_part.GetNode(1).SetValue(KratosMultiphysics.NORMAL, normal)
+        model_part.CreateNewNode(2, 1.00,0.00,0.00)
+        model_part.GetNode(2).SetValue(KratosMultiphysics.NORMAL, normal)
+        model_part.CreateNewNode(3, 0.00,1.00,0.00)
+        model_part.GetNode(3).SetValue(KratosMultiphysics.NORMAL, normal)
+        model_part.CreateNewNode(4, 1.00,1.00,0.00)
+        model_part.GetNode(3).SetValue(KratosMultiphysics.NORMAL, normal)
+        
+        cond1 = model_part.CreateNewCondition("SurfaceCondition3D3N", 1, [1,2,3], model_part.GetProperties()[1])
+        cond2 = model_part.CreateNewCondition("SurfaceCondition3D3N", 2, [2,3,4], model_part.GetProperties()[1])
+        cond1.SetValue(KratosMultiphysics.NORMAL, normal)
+        cond2.SetValue(KratosMultiphysics.NORMAL, normal)
+        
+        # Creating the utility:
+        ExactIntegration = ContactStructuralMechanicsApplication.ExactMortarIntegrationUtility3D3N(2)
+
+        # Triangle 2
+        normal[2] = -1.0
+        model_part.CreateNewNode(5, 0.00,0.00,0.01)
+        model_part.GetNode(5).SetValue(KratosMultiphysics.NORMAL, normal)
+        model_part.CreateNewNode(6, 1.00,0.00,0.01)
+        model_part.GetNode(6).SetValue(KratosMultiphysics.NORMAL, normal)
+        model_part.CreateNewNode(7, 0.00,1.00,0.01)
+        model_part.GetNode(7).SetValue(KratosMultiphysics.NORMAL, normal)
+        model_part.CreateNewNode(8, 1.00,1.00,0.01)
+        model_part.GetNode(7).SetValue(KratosMultiphysics.NORMAL, normal)
+        
+        cond3 = model_part.CreateNewCondition("SurfaceCondition3D3N", 3, [5,6,7], model_part.GetProperties()[1])
+        cond4 = model_part.CreateNewCondition("SurfaceCondition3D3N", 4, [6,7,8], model_part.GetProperties()[1])
+        cond3.SetValue(KratosMultiphysics.NORMAL, normal)
+        cond4.SetValue(KratosMultiphysics.NORMAL, normal)
+        
+        MatrixSolution = KratosMultiphysics.Matrix()
+        
+        
+        solution = ExactIntegration.TestGetExactIntegration(cond1, cond3, MatrixSolution)
+        
+        # Debug
+        if (solution == True):
+            print("\n\nFirst Integration accomplished", MatrixSolution)
+        
+        solution = ExactIntegration.TestGetExactIntegration(cond1, cond4, MatrixSolution)
+        
+        # Debug
+        if (solution == True):
+            print("\n\nSecond Integration accomplished", MatrixSolution)
+            
+        solution = ExactIntegration.TestGetExactIntegration(cond2, cond3, MatrixSolution)
+        
+        # Debug
+        if (solution == True):
+            print("\n\nThird Integration accomplished", MatrixSolution)
+        
+        solution = ExactIntegration.TestGetExactIntegration(cond2, cond4, MatrixSolution)
+        
+        # Debug
+        if (solution == True):
+            print("\n\nFourth Integration accomplished", MatrixSolution)
+        
+        #self.assertTrue(solution)
+        #self.assertAlmostEqual(MatrixSolution[0,0], 0.25)
+        #self.assertAlmostEqual(MatrixSolution[0,1], 1.0/12.0)
+        #self.assertAlmostEqual(MatrixSolution[0,2], 1.0/12.0)
+        #self.assertAlmostEqual(MatrixSolution[1,0], 0.75)
+        #self.assertAlmostEqual(MatrixSolution[1,1], 1.0/12.0)
+        #self.assertAlmostEqual(MatrixSolution[1,2], 1.0/12.0)
+        #self.assertAlmostEqual(MatrixSolution[2,0], 0.5)
+        #self.assertAlmostEqual(MatrixSolution[2,1], 1.0/3.0)
+        #self.assertAlmostEqual(MatrixSolution[2,2], 1.0/12.0)
 
 # QUADRILATERAL
 class TestQuadrilateralExactIntegration1(KratosUnittest.TestCase):
@@ -353,6 +436,7 @@ if __name__ == '__main__':
    TestLineExactIntegration2().test_execution()
    TestTriangleExactIntegration1().test_execution()
    TestTriangleExactIntegration2().test_execution()
+   TestTriangleExactIntegration3().test_execution()
    TestQuadrilateralExactIntegration1().test_execution()
    TestQuadrilateralExactIntegration2().test_execution()
         
