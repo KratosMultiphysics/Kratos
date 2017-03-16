2fa9138b2ce58f8d8b294c9ca561c5ba2a15ef5a
Andreas <a.winterstein@tum.de>
2017-03-16 10:52:41 +0100

    Modifying mesh solver structural similarity

--- applications/ALEapplication/python_scripts/mesh_solver_structural_similarity.py
+++ applications/ALEapplication/python_scripts/mesh_solver_structural_similarity.py
@@ -19,12 +19,34 @@ class MeshSolverStructuralSimilarity(mesh_solver_base.MeshSolverBase):
         # default settings for structural similarity mesh solver
         default_settings = Parameters("""
         {
-            "time_order"                 : 2,
-            "mesh_reform_dofs_each_step" : false,
-            "mesh_compute_reactions"     : false
+        "solver_type"                  : "mesh_solver_structural_similarity",
+        "model_import_settings"        : {
+            "input_type"     : "mdpa",
+            "input_filename" : "unknown_name"
+        },
+        "ale_linear_solver_settings"        : {
+        "solver_type" : "AMGCL",
+        "smoother_type":"ilu0",
+        "krylov_type": "gmres",
+        "coarsening_type": "aggregation",
+        "max_iteration": 200,
+        "provide_coordinates": false,
+        "gmres_krylov_space_dimension": 100,
+        "verbosity" : 0,
+        "tolerance": 1e-7,
+        "scaling": false,
+        "block_size": 1,
+        "use_block_matrices_if_possible" : true,
+        "coarse_enough" : 5000
+        },
+        "volume_model_part_name"       : "volume_model_part",
+        "time_order"                 : 2,
+        "mesh_reform_dofs_each_step" : false,
+        "mesh_compute_reactions"     : false
         }""")
 
-        custom_settings.ValidateAndAssignDefaults(default_settings)
+        self.settings = custom_settings
+        self.settings.ValidateAndAssignDefaults(default_settings)
 
         # set parameters
         self.model_part = model_part
@@ -38,14 +60,8 @@ class MeshSolverStructuralSimilarity(mesh_solver_base.MeshSolverBase):
         self.neighbour_search = FindNodalNeighboursProcess(model_part, number_of_avg_elems, number_of_avg_nodes)
 
         # definition of the solvers
-        tol = 1e-8
-        max_it = 1000
-        verbosity = 1
-        m = 10
-        #self.linear_solver = AMGCLSolver(AMGCLSmoother.GAUSS_SEIDEL, AMGCLIterativeSolverType.BICGSTAB, tol, max_it, verbosity, m)
-        #pILUPrecond = ILU0Preconditioner()
-        #self.linear_solver =  BICGSTABSolver(1e-9, 300, pILUPrecond)
-        self.linear_solver = SuperLUSolver()
+        import linear_solver_factory
+        self.linear_solver = linear_solver_factory.ConstructSolver(self.settings["ale_linear_solver_settings"])
         print("Construction of MeshSolverStructuralSimilarity finished")
 
     def AddVariables(self):
@@ -76,15 +92,7 @@ class MeshSolverStructuralSimilarity(mesh_solver_base.MeshSolverBase):
             node.SetSolutionStepValue(MESH_RHS,0,zero)
 
         self.solver = StructuralMeshMovingStrategy(self.model_part, self.linear_solver, self.time_order, self.mesh_reform_dofs_each_step, self.mesh_compute_reactions)
-        (self.solver).SetEchoLevel(0)
-
-    # def Solve(self):
-    #     if(self.mesh_reform_dofs_each_step):
-    #         (self.neighbour_search).Execute()
-    #     (self.solver).Solve()
-    #
-    # def MoveNodes(self):
-    #     (self.solver).MoveNodes()
+        (self.solver).SetEchoLevel(2)
 
     def UpdateReferenceMesh(self):
         (self.solver).UpdateReferenceMesh()
