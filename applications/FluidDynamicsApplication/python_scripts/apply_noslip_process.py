e748440c54b7a76ba51247339631dc37e4246ff9
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-03-18 12:46:49 +0100

    Usign experimental_assign_vector_process
    Instead of using three independent experimental_assign_value_process for each component a unique experimental_assign_vector_process is used.

--- applications/FluidDynamicsApplication/python_scripts/apply_noslip_process.py
+++ applications/FluidDynamicsApplication/python_scripts/apply_noslip_process.py
@@ -15,61 +15,25 @@ class ApplyNoSlipProcess(KratosMultiphysics.Process):
             {
                 "mesh_id"              : 0,
                 "model_part_name"      : "please_specify_model_part_name",
-                "interval"             : [0.0, "End"]
+                "variable_name"        : "VELOCITY",
+                "interval"             : [0.0, 1e30],
+                "value"                : [0.0, 0.0, 0.0],
+                "constrained"          : [true,true,true],
+                "local_axes"           : {}
             }
             """
             )
 
-        # Detect "End" as a tag and replace it by a large number
-        if(settings.Has("interval")):
-            if(settings["interval"][1].IsString() ):
-                if(settings["interval"][1].GetString() == "End"):
-                    settings["interval"][1].SetDouble(1e30) # = default_settings["interval"][1]
-                else:
-                    raise Exception("the second value of interval can be \"End\" or a number, interval currently:"+settings["interval"].PrettyPrintJsonString())
-
         settings.ValidateAndAssignDefaults(default_settings)
 
-        self.aux_processes = []
+        import experimental_assign_vector_process
 
-        import experimental_assign_value_process
-
-        # Component X
-        x_params = KratosMultiphysics.Parameters("{}")
-        x_params.AddValue("model_part_name",settings["model_part_name"])
-        x_params.AddValue("mesh_id",settings["mesh_id"])
-        x_params.AddEmptyValue("constrained").SetBool(True)
-        x_params.AddValue("interval",settings["interval"])
-        x_params.AddEmptyValue("value").SetDouble(0.0)
-        x_params.AddEmptyValue("variable_name").SetString("VELOCITY_X")
-        self.aux_processes.append( experimental_assign_value_process.AssignValueProcess(Model, x_params) )
-
-        # Component Y
-        y_params = KratosMultiphysics.Parameters("{}")
-        y_params.AddValue("model_part_name",settings["model_part_name"])
-        y_params.AddValue("mesh_id",settings["mesh_id"])
-        y_params.AddEmptyValue("constrained").SetBool(True)
-        y_params.AddValue("interval",settings["interval"])
-        y_params.AddEmptyValue("value").SetDouble(0.0)
-        y_params.AddEmptyValue("variable_name").SetString("VELOCITY_Y")
-        self.aux_processes.append( experimental_assign_value_process.AssignValueProcess(Model, y_params) )
-
-        # Component Y
-        z_params = KratosMultiphysics.Parameters("{}")
-        z_params.AddValue("model_part_name",settings["model_part_name"])
-        z_params.AddValue("mesh_id",settings["mesh_id"])
-        z_params.AddEmptyValue("constrained").SetBool(True)
-        z_params.AddValue("interval",settings["interval"])
-        z_params.AddEmptyValue("value").SetDouble(0.0)
-        z_params.AddEmptyValue("variable_name").SetString("VELOCITY_Z")
-        self.aux_processes.append( experimental_assign_value_process.AssignValueProcess(Model, z_params) )
+        self.vector_process = experimental_assign_vector_process.AssignVectorProcess(Model, settings)
 
 
     def ExecuteInitializeSolutionStep(self):
-        for process in self.aux_processes:
-            process.ExecuteInitializeSolutionStep()
+        self.vector_process.ExecuteInitializeSolutionStep()
 
 
     def ExecuteFinalizeSolutionStep(self):
-        for process in self.aux_processes:
-            process.ExecuteFinalizeSolutionStep()
+        self.vector_process.ExecuteFinalizeSolutionStep()
