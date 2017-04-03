f20f18a86f883e765235f0a594bacbc9c772c9db
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-04-03 17:21:14 +0200

    Creaction of CHARACTERISTIC_VELOCITY var
    Creation of a new CHARACTERISTIC_VELOCITY variable. This variable is intended to store the characteristic velocity of the model that has to be used in the computation of the outlet inflow correction.

--- applications/FluidDynamicsApplication/custom_python/fluid_dynamics_python_application.cpp
+++ applications/FluidDynamicsApplication/custom_python/fluid_dynamics_python_application.cpp
@@ -1,53 +1,13 @@
-/*
-==============================================================================
-KratosFluidDynamicsApplication
-A library based on:
-Kratos
-A General Purpose Software for Multi-Physics Finite Element Analysis
-Version 1.0 (Released on march 05, 2007).
-
-Copyright 2007
-Pooyan Dadvand, Riccardo Rossi, Janosch Stascheit, Felix Nagel
-pooyan@cimne.upc.edu
-rrossi@cimne.upc.edu
-janosch.stascheit@rub.de
-nagel@sd.rub.de
-- CIMNE (International Center for Numerical Methods in Engineering),
-Gran Capita' s/n, 08034 Barcelona, Spain
-- Ruhr-University Bochum, Institute for Structural Mechanics, Germany
-
-
-Permission is hereby granted, free  of charge, to any person obtaining
-a  copy  of this  software  and  associated  documentation files  (the
-"Software"), to  deal in  the Software without  restriction, including
-without limitation  the rights to  use, copy, modify,  merge, publish,
-distribute,  sublicense and/or  sell copies  of the  Software,  and to
-permit persons to whom the Software  is furnished to do so, subject to
-the following condition:
-
-Distribution of this code for  any  commercial purpose  is permissible
-ONLY BY DIRECT ARRANGEMENT WITH THE COPYRIGHT OWNERS.
-
-The  above  copyright  notice  and  this permission  notice  shall  be
-included in all copies or substantial portions of the Software.
-
-THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
-EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT  SHALL THE AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY
-CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,
-TORT  OR OTHERWISE, ARISING  FROM, OUT  OF OR  IN CONNECTION  WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-==============================================================================
-*/
-
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
-//   Project Name:        Kratos
-//   Last modified by:    $Author: jcotela $
-//   Date:                $Date: 2010-11-11 $
-//   Revision:            $Revision: 1.0 $
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
 //
+//  Main authors:    Jordi Cotela
 //
 
 // System includes
@@ -97,13 +57,14 @@ BOOST_PYTHON_MODULE(KratosFluidDynamicsApplication)
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(Y_WALL);
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(SUBSCALE_PRESSURE);
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(C_DES);
-//        KRATOS_REGISTER_IN_PYTHON_VARIABLE(C_SMAGORINSKY);    
+//        KRATOS_REGISTER_IN_PYTHON_VARIABLE(C_SMAGORINSKY);
+KRATOS_REGISTER_IN_PYTHON_VARIABLE(CHARACTERISTIC_VELOCITY);
 
     // For Non-Newtonian constitutive relations
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(REGULARIZATION_COEFFICIENT);
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(BINGHAM_SMOOTHER)
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(GEL_STRENGTH)
-            
+
     KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS(SUBSCALE_VELOCITY);
     KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS(COARSE_VELOCITY);
 
