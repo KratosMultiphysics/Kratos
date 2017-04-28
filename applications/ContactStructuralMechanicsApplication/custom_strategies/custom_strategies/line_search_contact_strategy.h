fba2f990f248c2cff0dc30da38da065c255b25cb
loumalouomega <vmataix@cimne.upc.edu>
2017-04-28 21:54:52 +0200

    Advances in contact line search

--- applications/ContactStructuralMechanicsApplication/custom_strategies/custom_strategies/line_search_contact_strategy.h
+++ applications/ContactStructuralMechanicsApplication/custom_strategies/custom_strategies/line_search_contact_strategy.h
@@ -75,35 +75,37 @@ public:
     /** Counted pointer of ClassName */
     KRATOS_CLASS_POINTER_DEFINITION( LineSearchContactStrategy );
 
-    typedef SolvingStrategy<TSparseSpace, TDenseSpace, TLinearSolver>            StrategyBaseType;
+    typedef SolvingStrategy<TSparseSpace, TDenseSpace, TLinearSolver>              StrategyBaseType;
     
-    typedef LineSearchStrategy<TSparseSpace, TDenseSpace, TLinearSolver>                 BaseType;
+    typedef ResidualBasedNewtonRaphsonStrategy<TSparseSpace, TDenseSpace, TLinearSolver> NRBaseType;
     
-    typedef typename BaseType::TBuilderAndSolverType                        TBuilderAndSolverType;
-
-    typedef typename BaseType::TDataType                                                TDataType;
-
-    typedef TSparseSpace                                                          SparseSpaceType;
-
-    typedef typename BaseType::TSchemeType                                            TSchemeType;
-
-    typedef typename BaseType::DofsArrayType                                        DofsArrayType;
-
-    typedef typename BaseType::TSystemMatrixType                                TSystemMatrixType;
-
-    typedef typename BaseType::TSystemVectorType                                TSystemVectorType;
-
-    typedef typename BaseType::LocalSystemVectorType                        LocalSystemVectorType;
-
-    typedef typename BaseType::LocalSystemMatrixType                        LocalSystemMatrixType;
-
-    typedef typename BaseType::TSystemMatrixPointerType                  TSystemMatrixPointerType;
+    typedef LineSearchStrategy<TSparseSpace, TDenseSpace, TLinearSolver>                   BaseType;
     
-    typedef typename BaseType::TSystemVectorPointerType                  TSystemVectorPointerType;
+    typedef typename BaseType::TBuilderAndSolverType                          TBuilderAndSolverType;
+ 
+    typedef typename BaseType::TDataType                                                  TDataType;
+
+    typedef TSparseSpace                                                            SparseSpaceType;
+
+    typedef typename BaseType::TSchemeType                                              TSchemeType;
+
+    typedef typename BaseType::DofsArrayType                                          DofsArrayType;
+
+    typedef typename BaseType::TSystemMatrixType                                  TSystemMatrixType;
+
+    typedef typename BaseType::TSystemVectorType                                  TSystemVectorType;
+
+    typedef typename BaseType::LocalSystemVectorType                          LocalSystemVectorType;
+
+    typedef typename BaseType::LocalSystemMatrixType                          LocalSystemMatrixType;
+
+    typedef typename BaseType::TSystemMatrixPointerType                    TSystemMatrixPointerType;
     
-    typedef ModelPart::NodesContainerType                                          NodesArrayType;
+    typedef typename BaseType::TSystemVectorPointerType                    TSystemVectorPointerType;
     
-    typedef ModelPart::ConditionsContainerType                                ConditionsArrayType;
+    typedef ModelPart::NodesContainerType                                            NodesArrayType;
+    
+    typedef ModelPart::ConditionsContainerType                                  ConditionsArrayType;
     
     /**
      * Default constructor 
@@ -312,7 +314,69 @@ protected:
         const bool MoveMesh
     ) override
     {
-        BaseType::UpdateDatabase(A,Dx,b,MoveMesh);
+        typename TSchemeType::Pointer pScheme = this->GetScheme();
+        typename TBuilderAndSolverType::Pointer pBuilderAndSolver = this->GetBuilderAndSolver(); // FIXME: Separate in the parts of LM and displacement
+
+        TSystemVectorType aux(b.size()); //TODO: do it by using the space
+        TSparseSpace::Assign(aux, 0.5, Dx);
+
+        // Compute residual without update
+        TSparseSpace::SetToZero(b);
+        pBuilderAndSolver->BuildRHS(pScheme, BaseType::GetModelPart(), b );
+        const double ro = TSparseSpace::TwoNorm(b);
+
+        // Compute half step residual
+        NRBaseType::UpdateDatabase(A,aux,b,MoveMesh);
+        TSparseSpace::SetToZero(b);
+        pBuilderAndSolver->BuildRHS(pScheme, BaseType::GetModelPart(), b );
+        const double rh = TSparseSpace::TwoNorm(b);
+
+        // Compute full step residual (add another half Dx to the previous half)
+        NRBaseType::UpdateDatabase(A,aux,b,MoveMesh);
+        TSparseSpace::SetToZero(b);
+        pBuilderAndSolver->BuildRHS(pScheme, BaseType::GetModelPart(), b );
+        const double rf = TSparseSpace::TwoNorm(b);
+
+        // Compute optimal (limited to the range 0-1)
+        // Parabola is y = a*x^2 + b*x + c -> min/max for
+        // x=0   --> r=ro
+        // x=1/2 --> r=rh
+        // x=1   --> r =
+        // c= ro,     b= 4*rh -rf -3*ro,  a= 2*rf - 4*rh + 2*ro
+        // max found if a>0 at the position  xmax = (rf/4 - rh)/(rf - 2*rh);
+        
+        const double parabola_a = 2 * rf + 2 * ro - 4 * rh;
+        const double parabola_b = 4 * rh - rf - 3 * ro;
+        double xmin = 1e-3;
+        double xmax = 1.0;
+        
+        if( parabola_a > 0.0) //  If parabola has a local minima
+        {
+            xmax = -0.5 * parabola_b/parabola_a; // -b / 2a
+            if( xmax > 1.0)
+            {
+                xmax = 1.0;
+            }
+            else if(xmax < -1.0)
+            {
+                xmax = -1.0;
+            }
+        }
+        else //parabola degenerates to either a line or to have a local max. best solution on either extreme
+        {
+            if(rf < ro)
+            {
+                xmax = 1.0;
+            }
+            else
+            {
+                xmax = xmin; //should be zero, but otherwise it will stagnate
+            }
+        }
+
+        // Perform final update
+        TSparseSpace::Assign(aux,-(1.0-xmax), Dx);
+        NRBaseType::UpdateDatabase(A,aux,b,MoveMesh);
         
         CalculateContactReactions(b);
     }
@@ -420,6 +484,7 @@ private:
 
     ///@name Static Member Variables
     ///@{
+    
     ///@}
     ///@name Member Variables
     ///@{
