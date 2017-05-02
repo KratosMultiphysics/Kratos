e1d42b2c753cc62acfd69a60463d5e30bfb162e9
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-05-02 18:59:03 +0200

    minor algorithm changing without success

--- kratos/tests/processes/test_calculate_distance_to_skin_process.cpp
+++ kratos/tests/processes/test_calculate_distance_to_skin_process.cpp
@@ -112,7 +112,7 @@ namespace Kratos {
 
 		  Parameters mesher_parameters(R"(
             {
-                "number_of_divisions":   4,
+                "number_of_divisions":   10,
                 "element_name":     "Element3D4N"
             })");
 
