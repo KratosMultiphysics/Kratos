6683a89512ec029babac54e0518cc4eec2aa78cc
jcotela <jcotela@cimne.upc.edu>
2017-05-11 18:21:01 +0200

    Adding MPI version of Aitken to factory.

--- applications/FSIapplication/python_scripts/convergence_accelerator_factory.py
+++ applications/FSIapplication/python_scripts/convergence_accelerator_factory.py
@@ -2,52 +2,73 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 
 import KratosMultiphysics
 import KratosMultiphysics.FSIApplication as KratosFSI
+try:
+    import KratosMultiphysics.TrilinosApplication as KratosTrilinos
+    have_trilinos = True
+except ImportError:
+    have_trilinos = False
+
+_aitken_defaults = KratosMultiphysics.Parameters("""{   "solver_type"       : "Relaxation",
+                                                        "acceleration_type" : "Aitken",
+                                                        "w_0"               : 0.825  }""")
+
+_mvqn_defaults = KratosMultiphysics.Parameters("""{ "solver_type" : "MVQN",
+                                                    "w_0"         : 0.825   }""")
+
+_mvqn_defaults_recursive = KratosMultiphysics.Parameters("""{ "solver_type" : "MVQN_recursive",
+                                                              "w_0"         : 0.825,
+                                                              "buffer_size" : 7           }""")
 
 def CreateConvergenceAccelerator(configuration):
-    
+
     if(type(configuration) != KratosMultiphysics.Parameters):
         raise Exception("Input is expected to be provided as a Kratos Parameters object.")
-    
+
+    convergence_accelerator_type = configuration["solver_type"].GetString()
+
+    if(convergence_accelerator_type == "Relaxation"):
+
+        configuration.ValidateAndAssignDefaults(_aitken_defaults)
+
+        if (configuration["acceleration_type"].GetString() == "Aitken"):
+            convergence_accelerator = KratosFSI.AitkenConvergenceAccelerator(configuration["w_0"].GetDouble())
+
+    elif(convergence_accelerator_type == "MVQN"):
+
+        configuration.ValidateAndAssignDefaults(_mvqn_defaults)
+
+        convergence_accelerator = KratosFSI.MVQNFullJacobianConvergenceAccelerator(configuration["w_0"].GetDouble())
+
+    elif(convergence_accelerator_type == "MVQN_recursive"):
+
+        configuration.ValidateAndAssignDefaults(_mvqn_defaults_recursive)
+
+        convergence_accelerator = KratosFSI.MVQNRecursiveJacobianConvergenceAccelerator(configuration["w_0"].GetDouble(),
+                                                                                        configuration["buffer_size"].GetInt())
+
+    else:
+        raise Exception("Convergence accelerator not found. Asking for : " + convergence_accelerator_type)
+
+    return convergence_accelerator
+
+def CreateTrilinosConvergenceAccelerator(configuration):
+
+    if(type(configuration) != KratosMultiphysics.Parameters):
+        raise Exception("Input is expected to be provided as a Kratos Parameters object.")
+
+    if not have_trilinos:
+        raise Exception("Trying to create a Trilinos convergence accelerator, but TrilinosApplication could not be found.")
+
     convergence_accelerator_type = configuration["solver_type"].GetString()
     
     if(convergence_accelerator_type == "Relaxation"):
-        
-        defaults_relaxation = KratosMultiphysics.Parameters("""{
-                                                                "solver_type"       : "Relaxation",
-                                                                "acceleration_type" : "Aitken",
-                                                                "w_0"               : 0.825
-                                                               }""")
-                                                               
-        configuration.ValidateAndAssignDefaults(defaults_relaxation)        
-        
+
+        configuration.ValidateAndAssignDefaults(_aitken_defaults)
+
         if (configuration["acceleration_type"].GetString() == "Aitken"):
-            convergence_accelerator = KratosFSI.AitkenConvergenceAccelerator(configuration["w_0"].GetDouble())       
-            
-    elif(convergence_accelerator_type == "MVQN"):
-        
-        defaults_MVQN = KratosMultiphysics.Parameters("""{
-                                                          "solver_type" : "MVQN",
-                                                          "w_0"         : 0.825
-                                                         }""")
-                                                               
-        configuration.ValidateAndAssignDefaults(defaults_MVQN)
-        
-        convergence_accelerator = KratosFSI.MVQNFullJacobianConvergenceAccelerator(configuration["w_0"].GetDouble())
-            
-    elif(convergence_accelerator_type == "MVQN_recursive"):
-        
-        defaults_MVQN_recursive = KratosMultiphysics.Parameters("""{
-                                                                    "solver_type" : "MVQN_recursive",
-                                                                    "w_0"         : 0.825,
-                                                                    "buffer_size" : 7
-                                                                   }""")
-                                                               
-        configuration.ValidateAndAssignDefaults(defaults_MVQN_recursive)
-        
-        convergence_accelerator = KratosFSI.MVQNRecursiveJacobianConvergenceAccelerator(configuration["w_0"].GetDouble(),
-                                                                                        configuration["buffer_size"].GetInt())
-        
+            convergence_accelerator = KratosTrilinos.TrilinosAitkenConvergenceAccelerator(configuration["w_0"].GetDouble())
+
     else:
-        raise Exception("Convergence accelerator not found. Asking for : " + convergence_accelerator_type)
+        raise Exception("Trilinos convergence accelerator not found. Asking for : " + convergence_accelerator_type)
 
     return convergence_accelerator
