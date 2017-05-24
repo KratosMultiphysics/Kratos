100c9fefeddb99a0e03f4c09a6167e3b02fba786
msandre <michael.andre@tum.de>
2017-05-24 18:27:37 +0200

    added check for orphaned nodes in partitioner

--- applications/metis_application/custom_processes/metis_divide_heterogeneous_input_process.h
+++ applications/metis_application/custom_processes/metis_divide_heterogeneous_input_process.h
@@ -126,7 +126,13 @@ public:
         IO::ConnectivitiesContainerType KratosFormatNodeConnectivities;
 
         SizeType NumNodes = BaseType::mrIO.ReadNodalGraph(KratosFormatNodeConnectivities);
-       
+
+        SizeType NumNodesInMesh = BaseType::mrIO.ReadNodesNumber();
+        if (NumNodes < NumNodesInMesh)
+            KRATOS_ERROR << "Found " << (NumNodesInMesh - NumNodes) << " orphaned nodes in input mesh." << std::endl;
+        else if (NumNodes > NumNodesInMesh)
+            KRATOS_ERROR << "Found " (NumNodes - NumNodesInMesh) << " missing nodes in input mesh." << std::endl;
+
         // Write connectivity data in CSR format
         idxtype* NodeIndices = 0;
         idxtype* NodeConnectivities = 0;
