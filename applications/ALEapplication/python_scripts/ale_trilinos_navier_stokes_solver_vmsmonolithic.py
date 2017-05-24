f27e6720b56a043b7ee283cfe826a1fafee37987
msandre <michael.andre@tum.de>
2017-05-24 17:52:33 +0200

    add space to error string

--- applications/ALEapplication/python_scripts/ale_trilinos_navier_stokes_solver_vmsmonolithic.py
+++ applications/ALEapplication/python_scripts/ale_trilinos_navier_stokes_solver_vmsmonolithic.py
@@ -19,7 +19,7 @@ class ALETrilinosNavierStokesSolverVMSMonolithic(trilinos_navier_stokes_solver_v
         self.ale_solver_type = custom_settings["ale_solver_type"].GetString()
         valid_ale_solver_types = ["trilinos_mesh_solver_structural_similarity"]
         if self.ale_solver_type not in valid_ale_solver_types:
-            raise Exception("Invalid ale_solver_type:" + self.ale_solver_type)
+            raise Exception("Invalid ale_solver_type: " + self.ale_solver_type)
         # remove the ale solver type from settings so we can reuse the navier stokes constructor
         navier_stokes_settings = custom_settings
         navier_stokes_settings.RemoveValue("ale_solver_type")
