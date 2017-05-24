1c7fdc9e095faf579e050da0e9440c7e3df3e99f
msandre <michael.andre@tum.de>
2017-05-24 18:29:28 +0200

    fix missing ostream operator

--- applications/metis_application/custom_processes/metis_divide_heterogeneous_input_process.h
+++ applications/metis_application/custom_processes/metis_divide_heterogeneous_input_process.h
@@ -131,7 +131,7 @@ public:
         if (NumNodes < NumNodesInMesh)
             KRATOS_ERROR << "Found " << (NumNodesInMesh - NumNodes) << " orphaned nodes in input mesh." << std::endl;
         else if (NumNodes > NumNodesInMesh)
-            KRATOS_ERROR << "Found " (NumNodes - NumNodesInMesh) << " missing nodes in input mesh." << std::endl;
+            KRATOS_ERROR << "Found " << (NumNodes - NumNodesInMesh) << " missing nodes in input mesh." << std::endl;
 
         // Write connectivity data in CSR format
         idxtype* NodeIndices = 0;
