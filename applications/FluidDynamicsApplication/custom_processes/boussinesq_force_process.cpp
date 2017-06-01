eabcb8ae523164d571c8fe5b7952155e50ba2052
jcotela <jcotela@cimne.upc.edu>
2017-06-01 19:04:17 +0200

    Adding process to assign a Boussinesq thermal forcing term.

---
+++ applications/FluidDynamicsApplication/custom_processes/boussinesq_force_process.cpp
@@ -0,0 +1,73 @@
+#include "boussinesq_force_process.h"
+
+namespace Kratos
+{
+    /* Public functions *******************************************************/
+    BoussinesqForceProcess::BoussinesqForceProcess(
+        ModelPart& rModelPart,
+        Parameters& rParameters):
+    mrModelPart(rModelPart),
+    mrParameters(rParameters),
+    mrGravity(array_1d<double,3>(3,0.0))
+    {
+
+    }
+
+    BoussinesqForceProcess::~BoussinesqForceProcess()
+    {
+
+    }
+
+    void BoussinesqForceProcess::Execute()
+    {
+        this->AssignBoussinesqForce();
+    }
+
+    void BoussinesqForceProcess::ExecuteInitializeSolutionStep()
+    {
+        this->AssignBoussinesqForce();
+    }
+
+    std::string BoussinesqForceProcess::Info() const
+    {
+        return "BoussinesqForceProcess";
+    }
+
+    void BoussinesqForceProcess::PrintInfo(std::ostream& rOStream) const
+    {
+        rOStream << "BoussinesqForceProcess";
+    }
+
+    void BoussinesqForceProcess::PrintData(std::ostream& rOStream) const
+    {
+    }
+
+    /* Protected functions ****************************************************/
+
+    void BoussinesqForceProcess::AssignBoussinesqForce()
+    {
+        const double AmbientTemperature = mrModelPart.GetProcessInfo().GetValue(AMBIENT_TEMPERATURE);
+        int NumNodes = mrModelPart.NumberOfNodes();
+        #pragma omp parallel for firstprivate(NumNodes,AmbientTemperature)
+        for (int i = 0; i < NumNodes; ++i)
+        {
+            ModelPart::NodeIterator iNode = mrModelPart.NodesBegin() + i;
+            double Temperature = iNode.FastGetSolutionStepValue(TEMPERATURE);
+
+            iNode.FastGetSolutionStepValue(BODY_FORCE) = (1. - (Temperature-AmbientTemperature)/Temperature)*mrGravity;
+        }
+
+    }
+
+    /* External functions *****************************************************/
+
+    /// output stream function
+    inline std::ostream& operator << (
+        std::ostream& rOStream,
+        const BoussinesqForceProcess& rThis)
+    {
+        rThis.PrintData(rOStream);
+        return rOStream;
+    }
+
+}
---
+++ applications/FluidDynamicsApplication/custom_processes/boussinesq_force_process.h
@@ -0,0 +1,234 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main author:     Jordi Cotela
+//
+
+#if !defined(KRATOS_BOUSSINESQ_FORCE_PROCESS_H_INCLUDED )
+#define  KRATOS_BOUSSINESQ_FORCE_PROCESS_H_INCLUDED
+
+
+
+// System includes
+#include <string>
+#include <iostream>
+
+
+// External includes
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/kratos_parameters.h"
+#include "includes/model_part.h"
+#include "processes/process.h"
+
+namespace Kratos
+{
+  ///@addtogroup FluidDynamicsApplication
+  ///@{
+
+  ///@name Kratos Globals
+  ///@{
+
+  ///@}
+  ///@name Type Definitions
+  ///@{
+
+  ///@}
+  ///@name  Enum's
+  ///@{
+
+  ///@}
+  ///@name  Functions
+  ///@{
+
+  ///@}
+  ///@name Kratos Classes
+  ///@{
+
+  /// Auxiliary process to set Boussinesq forces to variable temperature flows.
+  /** The proces sets the BODY_FORCE variable to (1 - temp_fluctuation/reference_temp)*g,
+      so that the fluid element takes the Boussinesq force into account in solving the flow.
+   */
+  class BoussinesqForceProcess: public Process
+  {
+  public:
+      ///@name Type Definitions
+      ///@{
+
+      /// Pointer definition of BoussinesqForceProcess
+      KRATOS_CLASS_POINTER_DEFINITION(BoussinesqForceProcess);
+
+      ///@}
+      ///@name Life Cycle
+      ///@{
+
+      /// Constructor
+      BoussinesqForceProcess(ModelPart& rModelPart, Parameters& rParameters);
+
+      /// Destructor.
+      virtual ~BoussinesqForceProcess() override;
+
+
+      ///@}
+      ///@name Operators
+      ///@{
+
+
+      ///@}
+      ///@name Operations
+      ///@{
+
+      virtual void Execute() override;
+
+      virtual void ExecuteInitializeSolutionStep() override;
+
+      ///@}
+      ///@name Access
+      ///@{
+
+
+      ///@}
+      ///@name Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+      virtual std::string Info() const;
+
+      /// Print information about this object.
+      virtual void PrintInfo(std::ostream& rOStream) const;
+
+      /// Print object's data.
+      virtual void PrintData(std::ostream& rOStream) const;
+
+
+      ///@}
+      ///@name Friends
+      ///@{
+
+
+      ///@}
+
+    protected:
+      ///@name Protected static Member Variables
+      ///@{
+
+
+      ///@}
+      ///@name Protected member Variables
+      ///@{
+
+
+      ///@}
+      ///@name Protected Operators
+      ///@{
+
+
+      ///@}
+      ///@name Protected Operations
+      ///@{
+
+      void AssignBoussinesqForce();
+
+      ///@}
+      ///@name Protected  Access
+      ///@{
+
+
+      ///@}
+      ///@name Protected Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Protected LifeCycle
+      ///@{
+
+
+      ///@}
+
+    private:
+      ///@name Static Member Variables
+      ///@{
+
+
+      ///@}
+      ///@name Member Variables
+      ///@{
+
+      ModelPart &mrModelPart;
+
+      Parameters &mrParameters;
+
+      const array_1d<double,3> mrGravity;
+
+      ///@}
+      ///@name Private Operators
+      ///@{
+
+
+      ///@}
+      ///@name Private Operations
+      ///@{
+
+
+      ///@}
+      ///@name Private  Access
+      ///@{
+
+
+      ///@}
+      ///@name Private Inquiry
+      ///@{
+
+
+      ///@}
+      ///@name Un accessible methods
+      ///@{
+
+      /// Assignment operator.
+      BoussinesqForceProcess& operator=(BoussinesqForceProcess const& rOther);
+
+      /// Copy constructor.
+      BoussinesqForceProcess(BoussinesqForceProcess const& rOther);
+
+
+      ///@}
+
+    }; // Class BoussinesqForceProcess
+
+  ///@}
+
+  ///@name Type Definitions
+  ///@{
+
+
+  ///@}
+  ///@name Input and output
+  ///@{
+
+
+  /// output stream function
+  inline std::ostream& operator << (
+      std::ostream& rOStream,
+      const BoussinesqForceProcess& rThis);
+
+  ///@}
+
+  ///@} addtogroup block
+
+}  // namespace Kratos.
+
+#endif // KRATOS_BOUSSINESQ_FORCE_PROCESS_H_INCLUDED  defined
