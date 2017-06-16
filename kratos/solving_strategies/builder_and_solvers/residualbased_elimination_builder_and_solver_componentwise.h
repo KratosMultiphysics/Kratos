71e5acc9ff23fecf6a30fcdc3bb9e50d5e8eb8f1
Unknown <falessandro@cimne.upc.edu>
2017-06-16 18:31:36 +0200

    New function for splitting elements
    It creates a new node in the middle of the simplex and it creates new inner elements. The function is called before computing and after remeshing.

--- kratos/solving_strategies/builder_and_solvers/residualbased_elimination_builder_and_solver_componentwise.h
+++ kratos/solving_strategies/builder_and_solvers/residualbased_elimination_builder_and_solver_componentwise.h
@@ -222,25 +222,38 @@ public:
 
             // assemble all elements
             for (typename ElementsArrayType::ptr_iterator it=it_begin; it!=it_end; ++it)
-            {
+	      {  
+		bool element_is_active = true;
+		if ((*it)->IsDefined(ACTIVE))
+		  element_is_active = (*it)->Is(ACTIVE);
 
-                //calculate elemental contribution
-                (*it)->InitializeNonLinearIteration(CurrentProcessInfo);
-                (*it)->CalculateLocalSystem(LHS_Contribution,RHS_Contribution,CurrentProcessInfo);
+		if ((*it)->IsNot(ACTIVE)){
+		  element_is_active = false;
+		}
 
-                Geometry< Node<3> >& geom = (*it)->GetGeometry();
-                if(EquationId.size() != geom.size()) EquationId.resize(geom.size(),false);
+		if (element_is_active)
+		  {
 
-                for(unsigned int i=0; i<geom.size(); i++)
-                    EquationId[i] = geom[i].GetDof(rVar,pos).EquationId();
+		    //calculate elemental contribution
+		    (*it)->InitializeNonLinearIteration(CurrentProcessInfo);
+		    (*it)->CalculateLocalSystem(LHS_Contribution,RHS_Contribution,CurrentProcessInfo);
 
-                //assemble the elemental contribution
+		    Geometry< Node<3> >& geom = (*it)->GetGeometry();
+		    if(EquationId.size() != geom.size()) EquationId.resize(geom.size(),false);
+
+		    for(unsigned int i=0; i<geom.size(); i++)
+		      EquationId[i] = geom[i].GetDof(rVar,pos).EquationId();
+
+		    //assemble the elemental contribution
 #ifdef _OPENMP
-                this->Assemble(A,b,LHS_Contribution,RHS_Contribution,EquationId,lock_array);
+		    this->Assemble(A,b,LHS_Contribution,RHS_Contribution,EquationId,lock_array);
 #else
-                this->Assemble(A,b,LHS_Contribution,RHS_Contribution,EquationId);
+		    this->Assemble(A,b,LHS_Contribution,RHS_Contribution,EquationId);
 #endif
-            }
+
+		  }
+
+	      }
         }
 
         vector<unsigned int> condition_partition;
