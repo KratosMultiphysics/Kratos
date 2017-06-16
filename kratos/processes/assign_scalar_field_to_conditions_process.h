f53d75fa01562458490f73b51e0e914acfccbd2f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2017-06-16 10:47:03 +0200

    Adding missing overrides

--- kratos/processes/assign_scalar_field_to_conditions_process.h
+++ kratos/processes/assign_scalar_field_to_conditions_process.h
@@ -153,19 +153,19 @@ public:
     ///@{
 
     /// Turn back information as a string.
-    virtual std::string Info() const
+    virtual std::string Info() const override
     {
         return "AssignScalarFieldToConditionsProcess";
     }
 
     /// Print information about this object.
-    virtual void PrintInfo(std::ostream& rOStream) const
+    virtual void PrintInfo(std::ostream& rOStream) const override
     {
         rOStream << "AssignScalarFieldToConditionsProcess";
     }
 
     /// Print object's data.
-    virtual void PrintData(std::ostream& rOStream) const
+    virtual void PrintData(std::ostream& rOStream) const override
     {
     }
 
