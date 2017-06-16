e25c58162b263e2380a19e5c1de11f7121a32532
Unknown <mmaso@cimne.upc.edu>
2017-06-16 16:09:01 +0200

    Adding shock capturing term

--- applications/ShallowWaterApplication/shallow_water_application.h
+++ applications/ShallowWaterApplication/shallow_water_application.h
@@ -25,8 +25,9 @@
 #include "includes/kratos_application.h"
 #include "includes/variables.h"
 #include "shallow_water_application_variables.h"
-#include "custom_elements/projected_swe.h" //including the file for the second element
-#include "includes/condition.h"            //we'll also need conditions for the point heat loads
+#include "custom_elements/projected_swe.h"       // Including the file for the element
+#include "custom_elements/non_conservative_dc.h" // Inlcuding the file for the element with discontinuity capturing
+#include "includes/condition.h"                  // We'll also need conditions for the point heat loads
 #include "includes/ublas_interface.h"
 
 
@@ -187,7 +188,8 @@ namespace Kratos
 		///@} 
 		///@name Member Variables 
 		///@{ 
-		const ProjectedSWE mProjectedSWE;  // element
+		const ProjectedSWE mProjectedSWE;            // Element
+		const NonConservativeDC mNonConservativeDC;  // Element with discontinuty capturing
 
 
 		///@} 
