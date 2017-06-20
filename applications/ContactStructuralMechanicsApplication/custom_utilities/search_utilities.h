e520d4cdebb011cc209dfe367ae16e4d0523cdbc
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2017-06-20 13:04:22 +0200

    Adding some functions

--- applications/ContactStructuralMechanicsApplication/custom_utilities/search_utilities.h
+++ applications/ContactStructuralMechanicsApplication/custom_utilities/search_utilities.h
@@ -107,42 +107,6 @@ public:
         GeometryType& geom_1 = pCond1->GetGeometry(); // SLAVE
         GeometryType& geom_2 = pCond2->GetGeometry(); // MASTER
         
-//         // Initialize variables
-//         bool condition_is_active = false;
-//         const unsigned int dimension  = geom_1.WorkingSpaceDimension();
-//         const unsigned int number_of_nodes = geom_1.size();
-//         
-//         if (dimension == 2 && number_of_nodes == 2)
-//         {
-//             ExactMortarIntegrationUtility<2, 2> IntUtil = ExactMortarIntegrationUtility<2, 2>();
-//             std::vector<array_1d<PointType,2>>  ConditionsPointsSlave;
-//             condition_is_active = IntUtil.GetExactIntegration(geom_1, ContactNormal1, geom_2, ContactNormal2, ConditionsPointsSlave);
-//         }
-//         else if (dimension == 3 && number_of_nodes == 3)
-//         {
-//             ExactMortarIntegrationUtility<3, 3> IntUtil = ExactMortarIntegrationUtility<3, 3>();
-//             std::vector<array_1d<PointType,3>>  ConditionsPointsSlave;
-//             condition_is_active = IntUtil.GetExactIntegration(geom_1, ContactNormal1, geom_2, ContactNormal2, ConditionsPointsSlave);
-//         }
-//         else if (dimension == 3 && number_of_nodes == 4)
-//         {
-//             ExactMortarIntegrationUtility<3, 4> IntUtil = ExactMortarIntegrationUtility<3, 4>();
-//             std::vector<array_1d<PointType,3>>  ConditionsPointsSlave;
-//             condition_is_active = IntUtil.GetExactIntegration(geom_1, ContactNormal1, geom_2, ContactNormal2, ConditionsPointsSlave);
-//         }
-//         else
-//         {
-//             KRATOS_ERROR << "INTEGRATION NOT IMPLEMENTED" << std::endl;
-//         }
-//         
-//         if (condition_is_active == true)
-//         {
-//             for (unsigned int i_node = 0; i_node < geom_1.size(); i_node++)
-//             {
-//                 geom_1[i_node].Set(ACTIVE, true);
-//             }
-//         }
-        
         // LEGACY WAY
         // Define the basic information
 //         const double Tolerance = 1.0e-12;
@@ -199,6 +163,60 @@ public:
         {
             ConditionPointers->AddNewCondition(pCond2);
         }
+        
+//         // Initialize variables
+//         bool condition_is_active = false;
+//         const unsigned int dimension  = geom_1.WorkingSpaceDimension();
+//         const unsigned int number_of_nodes = geom_1.size();
+//         
+//         if (dimension == 2 && number_of_nodes == 2)
+//         {
+//             ExactMortarIntegrationUtility<2, 2> IntUtil = ExactMortarIntegrationUtility<2, 2>();
+//             std::vector<array_1d<PointType,2>> conditions_points_slave;
+//             condition_is_active = IntUtil.GetExactIntegration(geom_1, ContactNormal1, geom_2, ContactNormal2, conditions_points_slave);
+//             
+//             if (condition_is_active == true)
+//             {
+//                 condition_is_active = CheckPointsInside<2>(conditions_points_slave, geom_1, geom_2, ContactNormal1, ContactNormal2, ActiveCheckLength, StrictCheck);
+//             }
+//         }
+//         else if (dimension == 3 && number_of_nodes == 3)
+//         {
+//             ExactMortarIntegrationUtility<3, 3> IntUtil = ExactMortarIntegrationUtility<3, 3>();
+//             std::vector<array_1d<PointType,3>> conditions_points_slave;
+//             condition_is_active = IntUtil.GetExactIntegration(geom_1, ContactNormal1, geom_2, ContactNormal2, conditions_points_slave);
+//             
+//             if (condition_is_active == true)
+//             {
+//                 condition_is_active = CheckPointsInside<3>(conditions_points_slave, geom_1, geom_2, ContactNormal1, ContactNormal2, ActiveCheckLength, StrictCheck);
+//             }
+//         }
+//         else if (dimension == 3 && number_of_nodes == 4)
+//         {
+//             ExactMortarIntegrationUtility<3, 4> IntUtil = ExactMortarIntegrationUtility<3, 4>();
+//             std::vector<array_1d<PointType,3>> conditions_points_slave;
+//             condition_is_active = IntUtil.GetExactIntegration(geom_1, ContactNormal1, geom_2, ContactNormal2, conditions_points_slave);
+//             
+//             if (condition_is_active == true)
+//             {
+//                 condition_is_active = CheckPointsInside<3>(conditions_points_slave, geom_1, geom_2, ContactNormal1, ContactNormal2, ActiveCheckLength, StrictCheck);
+//             }
+//         }
+//         else
+//         {
+//             KRATOS_ERROR << "INTEGRATION NOT IMPLEMENTED" << std::endl;
+//         }
+//         
+//         // If condition is active we add
+//         if (condition_is_active == true)
+//         {
+//             for (unsigned int i_node = 0; i_node < geom_1.size(); i_node++)
+//             {
+//                 geom_1[i_node].Set(ACTIVE, true);
+//             }
+//             
+//             ConditionPointers->AddNewCondition(pCond2);
+//         }
     }
     
 protected:
@@ -245,6 +263,53 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
+    
+    template<unsigned int TDim>
+    static inline bool CheckPointsInside(
+        std::vector<array_1d<PointType,TDim>> ConditionsPointsSlave,
+        GeometryType& Geom1, // SLAVE
+        GeometryType& Geom2,  // MASTER
+        const array_1d<double, 3> & ContactNormal1, // SLAVE
+        const array_1d<double, 3> & ContactNormal2, // MASTER
+        const double ActiveCheckLength,
+        const bool StrictCheck = true
+        )
+    {        
+        // Define the basic information
+        Point<3> projected_point;
+        double aux_distance = 0.0;
+        Vector N;
+        
+        for (unsigned int i_geom = 0; i_geom < ConditionsPointsSlave.size(); i_geom++)
+        {
+            for (unsigned int i_node = 0; i_node < TDim; i_node++)
+            {
+                const PointType LocalPoint = ConditionsPointsSlave[i_geom][i_node];
+                PointType GlobalPoint;
+                Geom1.GlobalCoordinates(GlobalPoint, LocalPoint);
+                    
+                Geom1.ShapeFunctionsValues( N, LocalPoint );
+                const array_1d<double, 3> normal = ContactUtilities::GaussPointNormal(N, Geom1);
+                
+                if (norm_2(normal) < std::numeric_limits<double>::epsilon())
+                {
+                    aux_distance = ContactUtilities::FastProjectDirection(Geom2, GlobalPoint, projected_point, ContactNormal2, ContactNormal1);
+                }
+                else
+                {
+                    aux_distance = ContactUtilities::FastProjectDirection(Geom2, GlobalPoint, projected_point, ContactNormal2, normal);
+                }  
+                
+                array_1d<double, 3> result;
+                if (aux_distance <= ActiveCheckLength && (StrictCheck == true ? Geom2.IsInside(projected_point, result, std::numeric_limits<double>::epsilon()) : true)) // NOTE: This can be problematic (It depends the way IsInside() and the LocalPointCoordinates() are implemented)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
 
     ///@}
     ///@name Private  Access
