6c73653a7a0069c6b07301737d61dc301ed8aed4
jcotela <jcotela@cimne.upc.edu>
2017-07-06 10:53:07 +0200

    Implementing CalculateRightHandSide (required for reactions).

--- applications/convection_diffusion_application/custom_elements/eulerian_conv_diff.cpp
+++ applications/convection_diffusion_application/custom_elements/eulerian_conv_diff.cpp
@@ -364,7 +364,8 @@ namespace Kratos
     template< unsigned int TDim, unsigned int TNumNodes >
     void EulerianConvectionDiffusionElement< TDim, TNumNodes >::CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo)
     {
-        KRATOS_THROW_ERROR(std::runtime_error, "CalculateRightHandSide not implemented","");
+        Matrix LeftHandSide;
+        this->CalculateLocalSystem(LeftHandSide,rRightHandSideVector,rCurrentProcessInfo);
     }
 
 //----------------------------------------------------------------------------------------
