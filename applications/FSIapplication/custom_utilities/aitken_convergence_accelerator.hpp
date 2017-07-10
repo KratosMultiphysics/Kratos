35e76584ab0194a722786c89d750125301fdb5cf
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-07-10 19:18:10 +0200

    Minor modification un residual vector update

--- applications/FSIapplication/custom_utilities/aitken_convergence_accelerator.hpp
+++ applications/FSIapplication/custom_utilities/aitken_convergence_accelerator.hpp
@@ -159,7 +159,8 @@ public:
     {
         KRATOS_TRY;
 
-        mpResidualVector_0 = mpResidualVector_1;
+        // mpResidualVector_0 = mpResidualVector_1;
+        std::swap(mpResidualVector_0, mpResidualVector_1);
         mConvergenceAcceleratorIteration += 1;
 
         KRATOS_CATCH( "" );
