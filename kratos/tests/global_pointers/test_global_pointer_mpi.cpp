11c6c81379305ced18e2a8a3c399639cfd46f21b
Carlos Roig <roigcarlo@gmail.com>
2017-07-14 10:33:09 +0200

    Move parallel test to a diferent file

--- kratos/tests/global_pointers/test_global_pointer_mpi.cpp
+++ kratos/tests/global_pointers/test_global_pointer_mpi.cpp
@@ -193,5 +193,53 @@ KRATOS_TEST_CASE_IN_SUITE(GlobalPointerModifyBoostWeakPtr, KratosCoreFastSuit)
   }
 }
 
+/// Parallel tests
+
+#ifdef KRATOS_USING_MPI
+KRATOS_TEST_CASE_IN_SUITE(GlobalPointerGatherRaw, KratosCoreFastSuit)
+{
+  int mpi_size;
+  int mpi_rank;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+
+  int sample_var = 1337 + mpi_rank;
+
+	auto from_raw_origin = GlobalPointer<int>(&sample_var);
+
+  std::size_t gp_size = sizeof(GlobalPointer<int>);
+
+  char * raw_gather_send_buffer = new char[gp_size];
+  char * raw_gather_recv_buffer = new char[gp_size * mpi_size];
+
+  KRATOS_CHECK_NOT_EQUAL(raw_gather_send_buffer, nullptr);
+  KRATOS_CHECK_NOT_EQUAL(raw_gather_recv_buffer, nullptr);
+
+  from_raw_origin.Save(raw_gather_send_buffer);
+
+  MPI_Allgather(
+    raw_gather_send_buffer, gp_size, MPI_CHAR,
+    raw_gather_recv_buffer, gp_size, MPI_CHAR,
+    MPI_COMM_WORLD
+  );
+
+  auto from_raw_remote = GlobalPointer<int>(nullptr);
+
+  for(int i = 0; i < mpi_size; i += 1) {
+    from_raw_remote.Load(&raw_gather_recv_buffer[i * gp_size]);
+    KRATOS_CHECK_EQUAL(from_raw_remote.GetRank(), i);
+    if(mpi_rank == i) {
+      KRATOS_CHECK_EQUAL(*from_raw_remote, sample_var);
+    }
+  }
+
+  delete[] raw_gather_send_buffer;
+  delete[] raw_gather_recv_buffer;
+}
+#endif
+
+// Test shared_ptr<global_ptr>
+
 } // namespace Testing
 } // namespace Kratos
