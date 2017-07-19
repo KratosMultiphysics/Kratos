e98cf66261e3e97e17447f130b2fae244693e6ad
jcotela <jcotela@cimne.upc.edu>
2017-07-19 17:42:30 +0200

    Adding a method to print normals for validation.

--- applications/convection_diffusion_application/custom_conditions/flux_condition.cpp
+++ applications/convection_diffusion_application/custom_conditions/flux_condition.cpp
@@ -158,6 +158,100 @@ GeometryData::IntegrationMethod FluxCondition<TNodeNumber>::GetIntegrationMethod
     return GeometryData::GI_GAUSS_2;
 }
 
+template< unsigned int TNodeNumber >
+void FluxCondition<TNodeNumber>::GetValueOnIntegrationPoints(
+    const Variable<array_1d<double,3> > &rVariable,
+    std::vector<array_1d<double,3> > &rValues,
+    const ProcessInfo &rCurrentProcessInfo)
+{
+    const unsigned int NumGauss = this->GetGeometry().IntegrationPointsNumber(this->GetIntegrationMethod());
+    rValues.resize(NumGauss);
+    if (rVariable == NORMAL)
+    {
+        this->CalculateNormal(rValues[0]);
+    }
+    else
+    {
+        /* The cast is done to avoid modification of the element's data. Data modification
+         * would happen if rVariable is not stored now (would initialize a pointer to &rVariable
+         * with associated value of 0.0). This is catastrophic if the variable referenced
+         * goes out of scope.
+         */
+        const FluxCondition* const_this = static_cast< const FluxCondition* >(this);
+        rValues[0] = const_this->GetValue(rVariable);
+    }
+
+    // Copy the values to the different gauss points
+    for (unsigned int g = 1; g < NumGauss; g++)
+    {
+        noalias(rValues[g]) = rValues[0];
+    }
+}
+
+template< unsigned int TNodeNumber >
+void FluxCondition<TNodeNumber>::GetValueOnIntegrationPoints(
+    const Variable<double>& rVariable,
+    std::vector<double>& rValues,
+    const ProcessInfo& rCurrentProcessInfo)
+{
+    const unsigned int NumGauss = this->GetGeometry().IntegrationPointsNumber(this->GetIntegrationMethod());
+    rValues.resize(NumGauss);
+    const FluxCondition* const_this = static_cast< const FluxCondition* >(this);
+    rValues[0] = const_this->GetValue(rVariable);
+    for (unsigned int g = 1; g < NumGauss; g++)
+    {
+        rValues[g] = rValues[0];
+    }
+}
+
+template< unsigned int TNodeNumber >
+void FluxCondition<TNodeNumber>::GetValueOnIntegrationPoints(
+    const Variable<array_1d<double, 6 > >& rVariable,
+    std::vector<array_1d<double, 6 > >& rValues,
+    const ProcessInfo& rCurrentProcessInfo)
+{
+    const unsigned int NumGauss = this->GetGeometry().IntegrationPointsNumber(this->GetIntegrationMethod());
+    rValues.resize(NumGauss);
+    const FluxCondition* const_this = static_cast< const FluxCondition* >(this);
+    rValues[0] = const_this->GetValue(rVariable);
+    for (unsigned int g = 1; g < NumGauss; g++)
+    {
+        noalias(rValues[g]) = rValues[0];
+    }
+}
+
+template< unsigned int TNodeNumber >
+void FluxCondition<TNodeNumber>::GetValueOnIntegrationPoints(
+    const Variable<Vector>& rVariable,
+    std::vector<Vector>& rValues,
+    const ProcessInfo& rCurrentProcessInfo)
+{
+    const unsigned int NumGauss = this->GetGeometry().IntegrationPointsNumber(this->GetIntegrationMethod());
+    rValues.resize(NumGauss);
+    const FluxCondition* const_this = static_cast< const FluxCondition* >(this);
+    rValues[0] = const_this->GetValue(rVariable);
+    for (unsigned int g = 1; g < NumGauss; g++)
+    {
+        noalias(rValues[g]) = rValues[0];
+    }
+}
+
+template< unsigned int TNodeNumber >
+void FluxCondition<TNodeNumber>::GetValueOnIntegrationPoints(
+    const Variable<Matrix>& rVariable,
+    std::vector<Matrix>& rValues,
+    const ProcessInfo& rCurrentProcessInfo)
+{
+    const unsigned int NumGauss = this->GetGeometry().IntegrationPointsNumber(this->GetIntegrationMethod());
+    rValues.resize(NumGauss);
+    const FluxCondition* const_this = static_cast< const FluxCondition* >(this);
+    rValues[0] = const_this->GetValue(rVariable);
+    for (unsigned int g = 1; g < NumGauss; g++)
+    {
+        noalias(rValues[g]) = rValues[0];
+    }
+}
+
 // Input and Output ///////////////////////////////////////////////////////////
 
 template< unsigned int TNodeNumber >
@@ -196,6 +290,35 @@ void FluxCondition<TNodeNumber>::AddIntegrationPointRHSContribution(
     }
 }
 
+
+template <>
+void FluxCondition<2>::CalculateNormal(array_1d<double,3>& An)
+{
+    Geometry<Node<3> >& pGeometry = this->GetGeometry();
+
+    An[0] =   pGeometry[1].Y() - pGeometry[0].Y();
+    An[1] = - (pGeometry[1].X() - pGeometry[0].X());
+    An[2] =    0.00;
+}
+
+
+template <>
+void FluxCondition<3>::CalculateNormal(array_1d<double,3>& An )
+{
+    Geometry<Node<3> >& pGeometry = this->GetGeometry();
+
+    array_1d<double,3> v1,v2;
+    v1[0] = pGeometry[1].X() - pGeometry[0].X();
+    v1[1] = pGeometry[1].Y() - pGeometry[0].Y();
+    v1[2] = pGeometry[1].Z() - pGeometry[0].Z();
+
+    v2[0] = pGeometry[2].X() - pGeometry[0].X();
+    v2[1] = pGeometry[2].Y() - pGeometry[0].Y();
+    v2[2] = pGeometry[2].Z() - pGeometry[0].Z();
+
+    MathUtils<double>::CrossProduct(An,v1,v2);
+    An *= 0.5;
+}
 
 // Serialization //////////////////////////////////////////////////////////////
 
