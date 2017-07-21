52b2ead72a9f639885015edfce560e273d58e19f
Josep Maria <cpuigbo@cimne.upc.edu>
2017-07-21 22:23:50 +0200

    errors in override
    the modified lines do not have a method to override.....some additional corrections are needed

--- kratos/geometries/quadrilateral_interface_2d_4.h
+++ kratos/geometries/quadrilateral_interface_2d_4.h
@@ -1041,7 +1041,7 @@ public:
      * Calculates the local gradients for all integration points for
      * given integration method
      */
-    virtual ShapeFunctionsGradientsType ShapeFunctionsLocalGradients( IntegrationMethod ThisMethod ) override
+    virtual ShapeFunctionsGradientsType ShapeFunctionsLocalGradients( IntegrationMethod ThisMethod )
     {
         ShapeFunctionsGradientsType localGradients
         = CalculateShapeFunctionsIntegrationPointsLocalGradients( ThisMethod );
@@ -1061,7 +1061,7 @@ public:
      * Calculates the local gradients for all integration points for the
      * default integration method
      */
-    virtual ShapeFunctionsGradientsType ShapeFunctionsLocalGradients() override
+    virtual ShapeFunctionsGradientsType ShapeFunctionsLocalGradients()
     {
         IntegrationMethod ThisMethod = msGeometryData.DefaultIntegrationMethod();
         ShapeFunctionsGradientsType localGradients
@@ -1286,7 +1286,7 @@ private:
      * :KLUDGE: number of points is hard-coded -> be careful if you want to copy and paste!
      */
     static Matrix CalculateShapeFunctionsIntegrationPointsValues(
-        typename BaseType::IntegrationMethod ThisMethod ) override
+        typename BaseType::IntegrationMethod ThisMethod )
     {
         IntegrationPointsContainerType all_integration_points =
             AllIntegrationPoints();
