9f70fb90dce7d70759bf1417608b2f4f7e8f73fe
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2017-07-26 12:40:23 +0200

    Making the qud8 test work

--- applications/StructuralMechanicsApplication/custom_elements/base_solid_element.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/base_solid_element.cpp
@@ -616,9 +616,28 @@ namespace Kratos
         const ProcessInfo& rCurrentProcessInfo 
         )
     {
+        const GeometryType::IntegrationPointsArrayType &integration_points = GetGeometry().IntegrationPoints();
+        
         if ( rOutput.size() != GetGeometry().IntegrationPoints(  ).size() )
         {
             rOutput.resize( GetGeometry().IntegrationPoints(  ).size() );
+        }
+
+        if (rVariable == INTEGRATION_COORDINATES)
+        {
+            const unsigned int number_of_nodes = GetGeometry().size();
+            const unsigned int dimension = GetGeometry().WorkingSpaceDimension();
+            const unsigned int strain_size = mConstitutiveLawVector[0]->GetStrainSize();
+
+            KinematicVariables this_kinematic_variables(strain_size, dimension, number_of_nodes);
+            
+            for (unsigned int point_number = 0; point_number < integration_points.size(); point_number++)
+            {
+                Point<3> global_point;
+                GetGeometry().GlobalCoordinates(global_point, integration_points[point_number]);
+                
+                rOutput[point_number] = global_point.Coordinates();
+            }
         }
     }
 
