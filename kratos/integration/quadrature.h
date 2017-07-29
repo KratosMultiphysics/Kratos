7c8ecc389e3807fbc872eb96af039dd2e1a4c371
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2017-07-29 22:05:35 +0200

    Updating license header

--- kratos/integration/quadrature.h
+++ kratos/integration/quadrature.h
@@ -1,9 +1,14 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics 
 //
-//   Project Name:        Kratos
-//   Last Modified by:    $Author:   JMCarbonell $
-//   Date:                $Date:   December 2015 $
-//   Revision:            $Revision:         1.7 $
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
 //
+//  Main authors:    Josep Maria Carbonell
+//                    
 //
 
 #if !defined(KRATOS_QUADRATURE_H_INCLUDED )
