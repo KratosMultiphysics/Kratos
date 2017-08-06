7e234819b25c1c5e06b447f348cbd332da3c979f
Josep Maria <cpuigbo@cimne.upc.edu>
2017-08-06 13:19:45 +0200

    solve warnings pfem BigText

--- applications/PfemApplication/pfem_application_variables.h
+++ applications/PfemApplication/pfem_application_variables.h
@@ -1,6 +1,8 @@
 //-------------------------------------------------------------
-//
-//  KRATOS PFEM APPLICATION
+//         ___  __          
+//  KRATOS| _ \/ _|___ _ __
+//        |  _/  _/ -_) '  '
+//        |_| |_| \___|_|_|_|APPLICATION
 //                                                                
 //  License:(BSD)    PfemFluidMechanicsApplication/license.txt
 //
