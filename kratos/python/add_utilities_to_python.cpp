0a51ae5fbd19acdf0de186a401d9acf0f43c0a5a
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2017-09-11 19:54:13 +0200

    Adding table stream to the core

--- kratos/python/add_utilities_to_python.cpp
+++ kratos/python/add_utilities_to_python.cpp
@@ -49,6 +49,7 @@
 
 #include "utilities/python_function_callback_utility.h"
 #include "utilities/interval_utility.h"
+#include "utilities/table_stream_utility.h"
 
 namespace Kratos
 {
@@ -314,6 +315,11 @@ void AddUtilitiesToPython()
     .def("GetIntervalBegin", &IntervalUtility::GetIntervalBegin)
     .def("GetIntervalEnd", &IntervalUtility::GetIntervalEnd)
     .def("IsInInterval", &IntervalUtility ::IsInInterval)
+    ;
+    
+    // Adding table from table stream to python
+    class_<TableStreamUtility>("TableStreamUtility", init<>())
+    .def(init< bool >())
     ;
 }
 
