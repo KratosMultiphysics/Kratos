0de6e84357f030b309fcfd89572d6a0d14e71867
Unknown <mmaso@cimne.upc.edu>
2017-09-21 11:10:02 -0500

    adding variables to convect particles

--- applications/ShallowWaterApplication/shallow_water_application.h
+++ applications/ShallowWaterApplication/shallow_water_application.h
@@ -39,219 +39,219 @@
 namespace Kratos
 {
 
-	///@name Kratos Globals
-	///@{ 
+    ///@name Kratos Globals
+    ///@{ 
 
-	///@} 
-	///@name Type Definitions
-	///@{ 
+    ///@} 
+    ///@name Type Definitions
+    ///@{ 
 
-	///@} 
-	///@name  Enum's
-	///@{
+    ///@} 
+    ///@name  Enum's
+    ///@{
 
-	///@}
-	///@name  Functions 
-	///@{
+    ///@}
+    ///@name  Functions 
+    ///@{
 
-	///@}
-	///@name Kratos Classes
-	///@{
+    ///@}
+    ///@name Kratos Classes
+    ///@{
 
-	/// Short class definition.
-	/** Detail class definition.
-	*/
-	class KratosShallowWaterApplication : public KratosApplication
-	{
-	public:
-		///@name Type Definitions
-		///@{
-		
+    /// Short class definition.
+    /** Detail class definition.
+    */
+    class KratosShallowWaterApplication : public KratosApplication
+    {
+    public:
+        ///@name Type Definitions
+        ///@{
+        
 
-		/// Pointer definition of KratosShallowWaterApplication
-		KRATOS_CLASS_POINTER_DEFINITION(KratosShallowWaterApplication);
+        /// Pointer definition of KratosShallowWaterApplication
+        KRATOS_CLASS_POINTER_DEFINITION(KratosShallowWaterApplication);
 
-		///@}
-		///@name Life Cycle 
-		///@{ 
+        ///@}
+        ///@name Life Cycle 
+        ///@{ 
 
-		/// Default constructor.
-		KratosShallowWaterApplication();
+        /// Default constructor.
+        KratosShallowWaterApplication();
 
-		/// Destructor.
-		virtual ~KratosShallowWaterApplication(){}
+        /// Destructor.
+        virtual ~KratosShallowWaterApplication(){}
 
 
-		///@}
-		///@name Operators 
-		///@{
+        ///@}
+        ///@name Operators 
+        ///@{
 
 
-		///@}
-		///@name Operations
-		///@{
+        ///@}
+        ///@name Operations
+        ///@{
 
-		virtual void Register();
+        virtual void Register();
 
 
 
-		///@}
-		///@name Access
-		///@{ 
+        ///@}
+        ///@name Access
+        ///@{ 
 
 
-		///@}
-		///@name Inquiry
-		///@{
+        ///@}
+        ///@name Inquiry
+        ///@{
 
 
-		///@}      
-		///@name Input and output
-		///@{
+        ///@}      
+        ///@name Input and output
+        ///@{
 
-		/// Turn back information as a string.
-		virtual std::string Info() const
-		{
-			return "KratosShallowWaterApplication";
-		}
+        /// Turn back information as a string.
+        virtual std::string Info() const
+        {
+            return "KratosShallowWaterApplication";
+        }
 
-		/// Print information about this object.
-		virtual void PrintInfo(std::ostream& rOStream) const
-		{
-			rOStream << Info();
-			PrintData(rOStream);
-		}
+        /// Print information about this object.
+        virtual void PrintInfo(std::ostream& rOStream) const
+        {
+            rOStream << Info();
+            PrintData(rOStream);
+        }
 
-		///// Print object's data.
-      virtual void PrintData(std::ostream& rOStream) const
-      {
-      	KRATOS_WATCH("in my application");
-      	KRATOS_WATCH(KratosComponents<VariableData>::GetComponents().size() );
-		rOStream << "Variables:" << std::endl;
-		KratosComponents<VariableData>().PrintData(rOStream);
-		rOStream << std::endl;
-		rOStream << "Elements:" << std::endl;
-		KratosComponents<Element>().PrintData(rOStream);
-		rOStream << std::endl;
-		rOStream << "Conditions:" << std::endl;
-		KratosComponents<Condition>().PrintData(rOStream);
-      }
+        ///// Print object's data.
+        virtual void PrintData(std::ostream& rOStream) const
+        {
+            KRATOS_WATCH("in my application");
+            KRATOS_WATCH(KratosComponents<VariableData>::GetComponents().size() );
+            rOStream << "Variables:" << std::endl;
+            KratosComponents<VariableData>().PrintData(rOStream);
+            rOStream << std::endl;
+            rOStream << "Elements:" << std::endl;
+            KratosComponents<Element>().PrintData(rOStream);
+            rOStream << std::endl;
+            rOStream << "Conditions:" << std::endl;
+            KratosComponents<Condition>().PrintData(rOStream);
+        }
 
 
-		///@}      
-		///@name Friends
-		///@{
+        ///@}      
+        ///@name Friends
+        ///@{
 
 
-		///@}
+        ///@}
 
-	protected:
-		///@name Protected static Member Variables 
-		///@{ 
+    protected:
+        ///@name Protected static Member Variables 
+        ///@{ 
 
 
-		///@} 
-		///@name Protected member Variables 
-		///@{ 
+        ///@} 
+        ///@name Protected member Variables 
+        ///@{ 
 
 
-		///@} 
-		///@name Protected Operators
-		///@{ 
+        ///@} 
+        ///@name Protected Operators
+        ///@{ 
 
 
-		///@} 
-		///@name Protected Operations
-		///@{ 
+        ///@} 
+        ///@name Protected Operations
+        ///@{ 
 
 
-		///@} 
-		///@name Protected  Access 
-		///@{ 
+        ///@} 
+        ///@name Protected  Access 
+        ///@{ 
 
 
-		///@}      
-		///@name Protected Inquiry 
-		///@{ 
+        ///@}      
+        ///@name Protected Inquiry 
+        ///@{ 
 
 
-		///@}    
-		///@name Protected LifeCycle 
-		///@{ 
+        ///@}    
+        ///@name Protected LifeCycle 
+        ///@{ 
 
 
-		///@}
+        ///@}
 
-	private:
-		///@name Static Member Variables 
-		///@{ 
+    private:
+        ///@name Static Member Variables 
+        ///@{ 
 
 
-		///@} 
-		///@name Member Variables 
-		///@{
-		// Lagrangian elements 
-		const PrimitiveVarElement<3> mPrimitiveVarElement2D3N;
-		const PrimitiveVarElement<4> mPrimitiveVarElement2D4N;
-		const ConservedVarElement<3> mConservedVarElement2D3N;
-		const ConservedVarElement<4> mConservedVarElement2D4N;
-		// Eulerian elements
-		const EulerPrimVarElement<3> mEulerPrimVarElement2D3N;
-		const EulerPrimVarElement<4> mEulerPrimVarElement2D4N;
-		const EulerConsVarElement<3> mEulerConsVarElement2D3N;
-		const EulerConsVarElement<4> mEulerConsVarElement2D4N;
-		// Conditions
-		const RainCondition<3> mRainCondition2D3N;
-		const RainCondition<3> mRainCondition2D4N;
+        ///@} 
+        ///@name Member Variables 
+        ///@{
+        // Lagrangian elements 
+        const PrimitiveVarElement<3> mPrimitiveVarElement2D3N;
+        const PrimitiveVarElement<4> mPrimitiveVarElement2D4N;
+        const ConservedVarElement<3> mConservedVarElement2D3N;
+        const ConservedVarElement<4> mConservedVarElement2D4N;
+        // Eulerian elements
+        const EulerPrimVarElement<3> mEulerPrimVarElement2D3N;
+        const EulerPrimVarElement<4> mEulerPrimVarElement2D4N;
+        const EulerConsVarElement<3> mEulerConsVarElement2D3N;
+        const EulerConsVarElement<4> mEulerConsVarElement2D4N;
+        // Conditions
+        const RainCondition<3> mRainCondition2D3N;
+        const RainCondition<3> mRainCondition2D4N;
 
 
-		///@} 
-		///@name Private Operators
-		///@{ 
+        ///@} 
+        ///@name Private Operators
+        ///@{ 
 
 
-		///@} 
-		///@name Private Operations
-		///@{ 
+        ///@} 
+        ///@name Private Operations
+        ///@{ 
 
 
-		///@} 
-		///@name Private  Access 
-		///@{ 
+        ///@} 
+        ///@name Private  Access 
+        ///@{ 
 
 
-		///@}    
-		///@name Private Inquiry 
-		///@{ 
+        ///@}    
+        ///@name Private Inquiry 
+        ///@{ 
 
 
-		///@}    
-		///@name Un accessible methods 
-		///@{ 
+        ///@}    
+        ///@name Un accessible methods 
+        ///@{ 
 
-		/// Assignment operator.
-		KratosShallowWaterApplication& operator=(KratosShallowWaterApplication const& rOther);
+        /// Assignment operator.
+        KratosShallowWaterApplication& operator=(KratosShallowWaterApplication const& rOther);
 
-		/// Copy constructor.
-		KratosShallowWaterApplication(KratosShallowWaterApplication const& rOther);
+        /// Copy constructor.
+        KratosShallowWaterApplication(KratosShallowWaterApplication const& rOther);
 
 
-		///@}    
+        ///@}    
 
-	}; // Class KratosShallowWaterApplication 
+    }; // Class KratosShallowWaterApplication 
 
-	///@} 
+    ///@} 
 
 
-	///@name Type Definitions       
-	///@{ 
+    ///@name Type Definitions       
+    ///@{ 
 
 
-	///@} 
-	///@name Input and output 
-	///@{ 
+    ///@} 
+    ///@name Input and output 
+    ///@{ 
 
-	///@} 
+    ///@} 
 
 
 }  // namespace Kratos.
