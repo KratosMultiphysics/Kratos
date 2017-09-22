5aacc4626bca2209a6da837bdd6bd3f7775b7803
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-09-22 13:49:55 +0200

    Removing unused Ublas space declaration

--- applications/FluidDynamicsApplication/tests/KratosExecuteEmbeddedTest.py
+++ applications/FluidDynamicsApplication/tests/KratosExecuteEmbeddedTest.py
@@ -13,8 +13,6 @@ class KratosExecuteEmbeddedTest(KratosUnittest.TestCase):
 
         self.ProjectParameters = ProjectParameters
 
-        self.vector_space = KratosMultiphysics.UblasSparseSpace()
-
         self.main_model_part = KratosMultiphysics.ModelPart(ProjectParameters["problem_data"]["model_part_name"].GetString())
         self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, ProjectParameters["problem_data"]["domain_size"].GetInt())
 
