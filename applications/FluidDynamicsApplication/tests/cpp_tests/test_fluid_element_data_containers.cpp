880edabb5a62ce764ab7882fd1630aab37f8be14
jcotela <jcotela@cimne.upc.edu>
2017-09-28 15:02:10 +0200

    Moving c tests to own folder.

---
+++ applications/FluidDynamicsApplication/tests/cpp_tests/test_fluid_element_data_containers.cpp
@@ -0,0 +1,26 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Jordi Cotela
+//
+//
+
+// Project includes
+#include "testing/testing.h"
+
+namespace Kratos {
+    namespace Testing {
+
+        KRATOS_TEST_CASE_IN_SUITE(TestFluidDynamicsTests, KratosCoreFastSuite)
+        {
+            std::cout << "I am a test!!!" << std::endl;
+        }
+
+    }
+}
