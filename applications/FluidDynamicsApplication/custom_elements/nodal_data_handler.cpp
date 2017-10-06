cf49ae40be916a90a6555e1da4539719b86ccec4
jcotela <jcotela@cimne.upc.edu>
2017-10-06 15:28:03 +0200

    Fixing internal matrix dimensions.

--- applications/FluidDynamicsApplication/custom_elements/nodal_data_handler.cpp
+++ applications/FluidDynamicsApplication/custom_elements/nodal_data_handler.cpp
@@ -6,7 +6,7 @@
 namespace Kratos
 {
 template class NodalDataHandler<double, 3, array_1d<double, 3>>;
-template class NodalDataHandler<array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 2, 3>>;
+template class NodalDataHandler<array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 3, 2>>;
 
 template <class TDataType, unsigned int TNumNodes, class TStorageType>
 NodalDataHandler<TDataType, TNumNodes, TStorageType>::NodalDataHandler(const Variable<TDataType>& rVariable):
@@ -55,31 +55,39 @@ array_1d<double, 3>& NodalDataHandler<double, 3, array_1d<double, 3>>::Get()
 // Variable< array_1d<double,3> > version /////////////////////////////////////
 
 template <>
-void NodalDataHandler< array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 2, 3>>::Initialize(
+void NodalDataHandler< array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 3, 2>>::Initialize(
     const Element& rElement, const ProcessInfo& rProcessInfo)
 {
     const Geometry<Node<3>> r_geometry = rElement.GetGeometry();
     for (unsigned int i = 0; i < 3; i++) // NOTE: TNumNodes
     {
         const array_1d<double,3>& r_nodal_values = r_geometry[i].FastGetSolutionStepValue(this->mrVariable);
-        for (unsigned int j = 0; j < mValues.size1(); j++)
+        KRATOS_WATCH(r_nodal_values)
+        for (unsigned int j = 0; j < mValues.size2(); j++)
         {
             mValues(i,j) = r_nodal_values[j];
         }
     }
+
+    KRATOS_WATCH(mValues)
 }
 
 template <>
-array_1d<double, 3> NodalDataHandler< array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 2, 3>>::Interpolate(boost::numeric::ublas::matrix_row< Matrix >& rN, Element* pElement)
+array_1d<double, 3> NodalDataHandler< array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 3, 2>>::Interpolate(boost::numeric::ublas::matrix_row< Matrix >& rN, Element* pElement)
 {
 	array_1d<double, 3> result(3, 0.0);
-	noalias(result) = boost::numeric::ublas::prod(rN, mValues);
+
+    for (unsigned int i = 0; i < 3; i++) { // NOTE NumNodes
+        for (unsigned int j = 0; j < 2; j++) { // NOTE Dim
+            result[j] += rN[i] * mValues(i,j);
+        }
+    }
 
 	return result;
 }
 
 template <>
-boost::numeric::ublas::bounded_matrix<double, 2, 3>& NodalDataHandler<array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 2, 3>>::Get()
+boost::numeric::ublas::bounded_matrix<double, 3, 2>& NodalDataHandler<array_1d<double, 3>, 3, boost::numeric::ublas::bounded_matrix<double, 3, 2>>::Get()
 {
     return mValues;
 }
