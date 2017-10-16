8f3cd0ca242ba3be459af358f93019a4c7f3e875
Unknown <cgguillem@gmail.com>
2017-10-16 17:12:40 +0200

    Making the fluid time step coincide witht he load time step
    When the fluid was to be calculated one reads it instead of having a separate counter for it. Also some aesthetic changes and comments

--- applications/swimming_DEM_application/python_scripts/pre_calculated_fluid_algorithm.py
+++ applications/swimming_DEM_application/python_scripts/pre_calculated_fluid_algorithm.py
@@ -9,14 +9,14 @@ import h5py
 class Algorithm(BaseAlgorithm):
     def __init__(self, varying_parameters = Parameters("{}")):
         BaseAlgorithm.__init__(self, varying_parameters)
-        self.fluid_loader_counter = self.GetFluidLoaderCounter()
+        # self.fluid_loader_counter = self.GetFluidLoaderCounter()
 
     def SetBetaParameters(self):
         BaseAlgorithm.SetBetaParameters(self)
         self.pp.CFD_DEM.AddEmptyValue("alpha").SetDouble(0.01)
         self.pp.CFD_DEM.AddEmptyValue("fluid_already_calculated").SetBool(True)
         self.pp.CFD_DEM.AddEmptyValue("load_derivatives").SetBool(False)
-        self.pp.CFD_DEM.AddEmptyValue("DEM_steps_per_fluid_load_step").SetInt(1)
+        self.pp.CFD_DEM.AddEmptyValue("DEM_steps_per_fluid_load_step").SetInt(10)
         self.pp.CFD_DEM.AddEmptyValue("store_fluid_in_single_precision").SetBool(True)
         self.pp.CFD_DEM.AddEmptyValue("store_fluid_pressure_option").SetBool(True)
 
@@ -28,13 +28,21 @@ class Algorithm(BaseAlgorithm):
         if not self.pp.CFD_DEM["fluid_already_calculated"].GetBool():
             self.fluid_algorithm.fluid_solver.Solve()
             self.fluid_loader.FillFluidDataStep()
+        else:
+            self.fluid_loader.LoadFluid(time)
 
     def PerformInitialDEMStepOperations(self, time = None):
-        if self.fluid_loader_counter.Tick():
-            if time > 0.07:
-                self.fluid_loader.LoadFluid(time)
+        pass
 
     def GetFluidLoaderCounter(self):
         return SDP.Counter(steps_in_cycle = self.pp.CFD_DEM["DEM_steps_per_fluid_load_step"].GetInt(),
                            beginning_step = 1,
                            is_dead = not self.pp.CFD_DEM["fluid_already_calculated"].GetBool())
+
+    def GetRunCode(self):
+        code = SDP.CreateRunCode(self.pp)
+
+        if self.pp.CFD_DEM["fluid_already_calculated"].GetBool():
+            return code + '_precalculated_fluid'
+        else:
+            return code
