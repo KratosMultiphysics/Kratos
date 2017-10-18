faea3cc3c4c526aefde94951da43279963db31fd
loumalouomega <vmataix@cimne.upc.edu>
2017-10-18 16:41:13 +0200

    Removing duplicated enums

---
+++ kratos/includes/enums.h
@@ -0,0 +1,59 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:   Vicente Mataix Ferrandiz
+//
+
+#if !defined(KRATOS_ENUMS_INCLUDED )
+#define  KRATOS_ENUMS_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+
+namespace Kratos
+{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+enum HistoricalValues {Historical = 0, NonHistorical = 1};
+    
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name Input and output
+///@{
+
+}  // namespace Kratos.
+
+#endif // KRATOS_ENUMS_INCLUDED  defined 
+
+
+ 
