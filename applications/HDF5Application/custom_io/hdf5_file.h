c1da142b733fdb322c734b9b519168da3850161f
msandre <michael.andre@tum.de>
2017-10-19 14:33:51 +0200

    small cleanup.

--- applications/HDF5Application/custom_io/hdf5_file.h
+++ applications/HDF5Application/custom_io/hdf5_file.h
@@ -158,41 +158,44 @@ private:
     template <class T>
     void WriteDataSetImpl(std::string Path, const std::vector<T>& rData)
     {
+        // Check that full path does not exist before trying to write data.
+        KRATOS_ERROR_IF(HasPath(Path)) << "Path already exists: " << Path << std::endl;
+
         // Create any missing subpaths.
         auto pos = Path.find_last_of('/');
         std::string sub_path = Path.substr(0, pos);
         AddPath(sub_path);
 
-        // Check that full path does not exist before trying to write data.
-        KRATOS_ERROR_IF(HasPath(Path)) << "Path already exists: " << Path << std::endl;
-
-        // Create and write data set.
-        int ndims = 1; // Default rank is 1 (scalar data type).
-        hid_t dtype_id;
-        hsize_t dims[2] = {0};
-        dims[0] = rData.size(); // Set first dataspace dimension.
+        // Initialize data space dimensions.
         constexpr bool is_int_type = std::is_same<int, T>::value;
         constexpr bool is_double_type = std::is_same<double, T>::value;
         constexpr bool is_array_1d_type = std::is_same<array_1d<double, 3>, T>::value;
+        constexpr unsigned ndims = (!is_array_1d_type) ? 1 : 2;
+        hsize_t dims[ndims] = {0};
+        dims[0] = rData.size(); // Set first data space dimension.
+        if (is_array_1d_type)
+            dims[1] = 3; // Set second data space dimension (array_1d<double,3>).
+        
+        // Set the data type.
+        hid_t dtype_id;
         if (is_int_type)
             dtype_id = H5T_NATIVE_INT;
         else if (is_double_type)
             dtype_id = H5T_NATIVE_DOUBLE;
         else if (is_array_1d_type)
-        {
             dtype_id = H5T_NATIVE_DOUBLE;
-            ndims = 2; // Increase data space rank to 2 for array_1d data type.
-            dims[1] = 3; // Second dimension is length of array_1d<double, 3>.
-        }
         else
             static_assert(is_int_type || is_double_type || is_array_1d_type,
                           "Unsupported data type.");
 
+        // Create and write the data set.
         hid_t dspace_id = H5Screate_simple(ndims, dims, nullptr);
         hid_t dset_id = H5Dcreate(m_file_id, Path.c_str(), dtype_id, dspace_id,
                                   H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
         KRATOS_ERROR_IF(dset_id < 0) << "H5Dcreate failed." << std::endl;
-        KRATOS_ERROR_IF(H5Dwrite(dset_id, dtype_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, rData.data()) < 0) << "H5Dwrite failed." << std::endl;
+        KRATOS_ERROR_IF(H5Dwrite(dset_id, dtype_id, H5S_ALL, H5S_ALL,
+                                 H5P_DEFAULT, rData.data()) < 0)
+            << "H5Dwrite failed." << std::endl;
         KRATOS_ERROR_IF(H5Dclose(dset_id) < 0) << "H5Dclose failed." << std::endl;
         KRATOS_ERROR_IF(H5Sclose(dspace_id) < 0) << "H5Sclose failed." << std::endl;
     }
@@ -204,49 +207,49 @@ private:
         KRATOS_ERROR_IF_NOT(IsDataSet(Path))
             << "Path is not a data set: " << Path << std::endl;
 
-        // Check data types and dimensions.
-        hid_t dtype_id;
-        std::vector<unsigned> file_space_dims = GetDataDimensions(Path);
-        KRATOS_ERROR_IF(file_space_dims.size() == 0) << "Invalid data set." << std::endl;
-        KRATOS_ERROR_IF(BlockSize > file_space_dims[0])
-            << "BlockSize exceeds data set dimension by: "
-            << BlockSize - file_space_dims[0] << std::endl;
-        if (rData.size() != BlockSize)
-            rData.resize(BlockSize);
-
-        // Define the hyperslab.
-        hsize_t start[] = {0, 0}, stride[] = {1, 1}, count[] = {0, 0};
-        count[0] = BlockSize;
-        std::vector<hsize_t> mem_space_dims(file_space_dims.size());
-        mem_space_dims[0] = BlockSize; // First dimension is BlockSize.
         constexpr bool is_int_type = std::is_same<int, T>::value;
         constexpr bool is_double_type = std::is_same<double, T>::value;
         constexpr bool is_array_1d_type = std::is_same<array_1d<double, 3>, T>::value;
+        constexpr unsigned ndims = (!is_array_1d_type) ? 1 : 2;
+
+        // Check file's data set dimensions.
+        std::vector<unsigned> file_space_dims = GetDataDimensions(Path);
+        KRATOS_ERROR_IF(file_space_dims.size() != ndims) << "Invalid data set dimension." << std::endl;
+        KRATOS_ERROR_IF(BlockSize > file_space_dims[0])
+            << "BlockSize exceeds data set dimension by: "
+            << BlockSize - file_space_dims[0] << std::endl;
+        if (is_array_1d_type)
+            KRATOS_ERROR_IF(file_space_dims[1] != 3)
+                << "Invalid data set dimension." << std::endl;
+        
+        if (rData.size() != BlockSize)
+            rData.resize(BlockSize);
+
+        // Initialize memory space dimensions.
+        hsize_t start[ndims] = {0}, mem_dims[ndims];
+        mem_dims[0] = BlockSize; // Set first dimension.
+        if (is_array_1d_type)
+            mem_dims[1] = 3; // Set second dimension.
+
+        // Set the data type.
+        hid_t dtype_id;
         if (is_int_type)
         {
             KRATOS_ERROR_IF_NOT(HasIntDataType(Path))
                 << "Data type is not int: " << Path << std::endl;
-            KRATOS_ERROR_IF(file_space_dims.size() != 1)
-                << "Invalid data set dimension." << std::endl;
             dtype_id = H5T_NATIVE_INT;
         }
         else if (is_double_type)
         {
             KRATOS_ERROR_IF_NOT(HasFloatDataType(Path))
                 << "Data type is not float: " << Path << std::endl;
-            KRATOS_ERROR_IF(file_space_dims.size() != 1)
-                << "Invalid data set dimension." << std::endl;
             dtype_id = H5T_NATIVE_DOUBLE;
         }
         else if (is_array_1d_type)
         {
             KRATOS_ERROR_IF_NOT(HasFloatDataType(Path))
                 << "Data type is not float: " << Path << std::endl;
-            KRATOS_ERROR_IF(file_space_dims.size() != 2 || file_space_dims[1] != 3)
-                << "Invalid data set dimension." << std::endl;
             dtype_id = H5T_NATIVE_DOUBLE;
-            count[1] = 3; // Second dimension is length of array_1d<double, 3>.
-            mem_space_dims[1] = 3; 
         }
         else
             static_assert(is_int_type || is_double_type || is_array_1d_type,
@@ -256,8 +259,8 @@ private:
         KRATOS_ERROR_IF(dset_id < 0) << "H5Dopen failed." << std::endl;
         hid_t file_space_id = H5Dget_space(dset_id);
         hid_t mem_space_id =
-            H5Screate_simple(mem_space_dims.size(), mem_space_dims.data(), nullptr);
-        KRATOS_ERROR_IF(H5Sselect_hyperslab(file_space_id, H5S_SELECT_SET, start, stride, count, nullptr) < 0)
+            H5Screate_simple(ndims, mem_dims, nullptr);
+        KRATOS_ERROR_IF(H5Sselect_hyperslab(file_space_id, H5S_SELECT_SET, start, nullptr, mem_dims, nullptr) < 0)
             << "H5Sselect_hyperslab failed." << std::endl;
         KRATOS_ERROR_IF(H5Dread(dset_id, dtype_id, mem_space_id, file_space_id, H5P_DEFAULT, rData.data()) < 0)
             << "H5Dread failed." << std::endl;
