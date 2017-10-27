df6bb8c02e79144737dab69b79f9401a0d693408
msandre <michael.andre@tum.de>
2017-10-27 17:55:26 +0200

    added matrix test.

--- applications/HDF5Application/tests/test_hdf5_file.cpp
+++ applications/HDF5Application/tests/test_hdf5_file.cpp
@@ -40,58 +40,81 @@ KRATOS_TEST_CASE_IN_SUITE(HDF5Test1, KratosHDF5TestSuite)
     KRATOS_CHECK(HDF5Utils::IsPath("/foo") == true);
     KRATOS_CHECK(HDF5Utils::IsPath("/foo/") == false);
     KRATOS_CHECK(HDF5Utils::IsPath("/foo/bar") == true);
+
     // Check HasPath().
     KRATOS_CHECK(test_file.HasPath("/foo/bar") == false);
     test_file.CreateGroup("/foo");
     test_file.CreateGroup("/foo/bar");
     KRATOS_CHECK(test_file.HasPath("/foo/bar") == true);
+
     // Check IsGroup().
     KRATOS_CHECK(test_file.IsGroup("/foo") == true);
     KRATOS_CHECK(test_file.IsGroup("/foo/bar") == true);
     KRATOS_CHECK(test_file.IsGroup("/abcdef") == false);
+
     // Check IsDataSet(), WriteDataSet().
-    HDF5File::Vector<int> write_data_1(5);
+    HDF5File::Vector<int> write_data_vec_int(5);
     for (unsigned i = 0; i < 5; ++i)
-        write_data_1[i] = i + 1;
-    test_file.WriteDataSet("/foo/data1", write_data_1);
+        write_data_vec_int[i] = i + 1;
+    test_file.WriteDataSet("/foo/data_vec_int", write_data_vec_int);
     KRATOS_CHECK(test_file.IsDataSet("/foo/bar") == false);
-    KRATOS_CHECK(test_file.IsDataSet("/foo/data1") == true);
+    KRATOS_CHECK(test_file.IsDataSet("/foo/data_vec_int") == true);
     array_1d<double, 3> x1(3, 2.718282);
     array_1d<double, 3> x2(3, 3.141592);
-    HDF5File::Vector<array_1d<double, 3>> write_data_2(2);
-    write_data_2[0] = x1;
-    write_data_2[1] = x2;
-    test_file.WriteDataSet("/foo/bar/data2", write_data_2);
-    KRATOS_CHECK(test_file.IsDataSet("/foo/bar/data2") == true);
+    HDF5File::Vector<array_1d<double, 3>> write_data_array1d(2);
+    write_data_array1d[0] = x1;
+    write_data_array1d[1] = x2;
+    test_file.WriteDataSet("/foo/bar/data_array1d", write_data_array1d);
+    KRATOS_CHECK(test_file.IsDataSet("/foo/bar/data_array1d") == true);
+    HDF5File::Matrix<int> write_data_mat_int(2, 2);
+    for (unsigned i = 0; i < 2; ++i)
+        for (unsigned j = 0; j < 2; ++j)
+            write_data_mat_int(i, j) = 10 * i + j;
+    test_file.WriteDataSet("/foo/data_mat_int", write_data_mat_int);
+
     // Check GetDataDimensions().
-    std::vector<unsigned> dims1 = test_file.GetDataDimensions("/foo/data1");
-    KRATOS_CHECK(dims1.size() == 1);
-    KRATOS_CHECK(dims1[0] == 5);
-    std::vector<unsigned> dims2 = test_file.GetDataDimensions("/foo/bar/data2");
-    KRATOS_CHECK(dims2.size() == 2);
-    KRATOS_CHECK(dims2[0] == 2);
-    KRATOS_CHECK(dims2[1] == 3);
+    std::vector<unsigned> dims_vec_int = test_file.GetDataDimensions("/foo/data_vec_int");
+    KRATOS_CHECK(dims_vec_int.size() == 1);
+    KRATOS_CHECK(dims_vec_int[0] == 5);
+    std::vector<unsigned> dims_array1d = test_file.GetDataDimensions("/foo/bar/data_array1d");
+    KRATOS_CHECK(dims_array1d.size() == 2);
+    KRATOS_CHECK(dims_array1d[0] == 2);
+    KRATOS_CHECK(dims_array1d[1] == 3);
+    std::vector<unsigned> dims_mat_int = test_file.GetDataDimensions("/foo/data_mat_int");
+    KRATOS_CHECK(dims_mat_int.size() == 2);
+    KRATOS_CHECK(dims_mat_int[0] == 2);
+    KRATOS_CHECK(dims_mat_int[1] == 2);
+
     // Check ReadDataSet() reading full data set.
-    HDF5File::Vector<int> read_data_1;
-    HDF5File::Vector<array_1d<double, 3>> read_data_2;
-    test_file.ReadDataSet("/foo/data1", read_data_1, 0, 5);
-    test_file.ReadDataSet("/foo/bar/data2", read_data_2, 0, 2);
-    for (unsigned i = 0; i < write_data_1.size(); ++i)
-        KRATOS_CHECK(write_data_1[i] == read_data_1[i]);
-    for (unsigned i = 0; i < write_data_2.size(); ++i)
-        for (unsigned j = 0; j < write_data_2[i].size(); ++j)
-            KRATOS_CHECK(write_data_2[i][j] == read_data_2[i][j]);
+    HDF5File::Vector<int> read_data_vec_int;
+    HDF5File::Vector<array_1d<double, 3>> read_data_array1d;
+    HDF5File::Matrix<int> read_data_mat_int;
+    test_file.ReadDataSet("/foo/data_vec_int", read_data_vec_int, 0, 5);
+    test_file.ReadDataSet("/foo/bar/data_array1d", read_data_array1d, 0, 2);
+    test_file.ReadDataSet("/foo/data_mat_int", read_data_mat_int, 0, 2);
+    for (unsigned i = 0; i < write_data_vec_int.size(); ++i)
+        KRATOS_CHECK(write_data_vec_int[i] == read_data_vec_int[i]);
+    for (unsigned i = 0; i < write_data_array1d.size(); ++i)
+        for (unsigned j = 0; j < write_data_array1d[i].size(); ++j)
+            KRATOS_CHECK(write_data_array1d[i][j] == read_data_array1d[i][j]);
+    for (unsigned i = 0; i < write_data_mat_int.size1(); ++i)
+        for (unsigned j = 0; j < write_data_mat_int.size2(); ++j)
+            KRATOS_CHECK(write_data_mat_int(i,j) == read_data_mat_int(i,j));
+
     // Check ReadDataSet() reading partial data set.
-    read_data_2.resize(0);
-    test_file.ReadDataSet("/foo/bar/data2", read_data_2, 0, 1);
-    KRATOS_CHECK(read_data_2.size() == 1);
-    for (unsigned j = 0; j < write_data_2[0].size(); ++j)
-        KRATOS_CHECK(write_data_2[0][j] == read_data_2[0][j]);
+    read_data_array1d.resize(0);
+    test_file.ReadDataSet("/foo/bar/data_array1d", read_data_array1d, 0, 1);
+    KRATOS_CHECK(read_data_array1d.size() == 1);
+    for (unsigned j = 0; j < write_data_array1d[0].size(); ++j)
+        KRATOS_CHECK(write_data_array1d[0][j] == read_data_array1d[0][j]);
+
     // Check HasIntDataType().
-    KRATOS_CHECK(test_file.HasIntDataType("/foo/data1") == true);
-    KRATOS_CHECK(test_file.HasFloatDataType("/foo/data1") == false);
-    KRATOS_CHECK(test_file.HasIntDataType("/foo/bar/data2") == false);
-    KRATOS_CHECK(test_file.HasFloatDataType("/foo/bar/data2") == true);
+    KRATOS_CHECK(test_file.HasIntDataType("/foo/data_vec_int") == true);
+    KRATOS_CHECK(test_file.HasFloatDataType("/foo/data_vec_int") == false);
+    KRATOS_CHECK(test_file.HasIntDataType("/foo/bar/data_array1d") == false);
+    KRATOS_CHECK(test_file.HasFloatDataType("/foo/bar/data_array1d") == true);
+    KRATOS_CHECK(test_file.HasIntDataType("/foo/data_mat_int") == true);
+
     // Check GetFileName().
     KRATOS_CHECK(test_file.GetFileName() == "test.h5");
 }
