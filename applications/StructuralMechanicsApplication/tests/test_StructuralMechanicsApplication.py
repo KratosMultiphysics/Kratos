16cfd5efc9a30a67cae6c376bc52a459b1325b6b
philbucher <philipp.bucher@tum.de>
2017-10-27 13:54:41 +0200

    Fixed Test that was broken bcs of new processes

--- applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
+++ applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
@@ -240,7 +240,7 @@ def AssambleTestSuites():
     nightSuite.addTest(TShellQ4ThinLinearStaticTests('test_execution'))
     nightSuite.addTest(TShellQ4ThinNonLinearStaticTests('test_execution'))
     nightSuite.addTest(TShellQ4ThinLinearDynamicTests('test_execution'))
-    # nightSuite.addTest(TShellQ4ThinNonLinearDynamicTests('test_execution')) # FIXME: Needs get up to date
+    nightSuite.addTest(TShellQ4ThinNonLinearDynamicTests('test_execution'))
     # CL tests
     ##nightSuite.addTest(TIsotropicDamageSimoJuPSTest('test_execution'))
 
@@ -305,7 +305,7 @@ def AssambleTestSuites():
         TShellQ4ThinLinearStaticTests,
         TShellQ4ThinNonLinearStaticTests,
         TShellQ4ThinLinearDynamicTests,
-        # TShellQ4ThinNonLinearDynamicTests, # FIXME: Needs get up to date
+        TShellQ4ThinNonLinearDynamicTests,
         T3D2NTrussTest,
         T3D2NTrussLinearTest,
         T3D2NTrussDynamicTest,
