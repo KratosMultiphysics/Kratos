031786ee4b9c9f21c428eeaabbf4f41e94d14487
Riccardo Rossi <rrossi@cimne.upc.edu>
2017-11-02 17:41:26 +0100

    Update define.h

--- kratos/includes/define.h
+++ kratos/includes/define.h
@@ -362,12 +362,12 @@ catch(...) { Block KRATOS_THROW_ERROR(std::runtime_error, "Unknown error", MoreI
     
     
 #ifdef __GNUC__
-#define DEPRECATED __attribute__((deprecated))
+#define KRATOS_DEPRECATED __attribute__((deprecated))
 #elif defined(_MSC_VER)
-#define DEPRECATED __declspec(deprecated)
+#define KRATOS_DEPRECATED __declspec(deprecated)
 #else
 #pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-#define DEPRECATED
+#define KRATOS_DEPRECATED
 #endif
     
 
