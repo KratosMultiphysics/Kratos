6ed179ad69a007bbe4c91820b05febb8c74d6132
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-11-02 12:38:28 +0100

    Using data() method to get a c-array from std::vector

--- kratos/utilities/divide_triangle_2d_3.cpp
+++ kratos/utilities/divide_triangle_2d_3.cpp
@@ -114,18 +114,18 @@ namespace Kratos
 
             // Call the splitting mode computation function
             std::vector<int> edge_ids(3);
-            TriangleSplitMode(&mSplitEdges[0], &edge_ids[0]);
+            TriangleSplitMode(mSplitEdges.data(), edge_ids.data());
 
             // Call the splitting function
             std::vector<int> t(12);     // Ids of the generated subdivisions
             int n_int = 0;              // Number of internal nodes (set to 0 since it is not needed for triangle splitting)
-            Split_Triangle(&edge_ids[0], &t[0], &mDivisionsNumber, &mSplitEdgesNumber, &n_int);
+            Split_Triangle(edge_ids.data(), t.data(), &mDivisionsNumber, &mSplitEdgesNumber, &n_int);
 
             // Fill the subdivisions arrays
             for (int idivision = 0; idivision < mDivisionsNumber; ++idivision) {
                 // Get the subdivision indices
                 int i0, i1, i2;
-                TriangleGetNewConnectivityGID(idivision, &t[0], &mSplitEdges[0], &i0, &i1, &i2);
+                TriangleGetNewConnectivityGID(idivision, t.data(), mSplitEdges.data(), &i0, &i1, &i2);
 
                 // Generate a pointer to an auxiliar triangular geometry made with the subdivision points
                 IndexedPointGeometryPointerType p_aux_partition = boost::make_shared<IndexedPointTriangleType>(mAuxPointsContainer(i0), 
