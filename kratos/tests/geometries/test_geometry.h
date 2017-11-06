fd7af95872d9563941509adb42c8e0dbb211fb6e
Carlos Roig <roigcarlo@gmail.com>
2017-11-06 12:13:20 +0100

    Replacing M_PI with Globals::Pi

--- kratos/tests/geometries/test_geometry.h
+++ kratos/tests/geometries/test_geometry.h
@@ -25,10 +25,6 @@
 #include "testing/testing.h"
 #include "geometries/geometry.h"
 
-#ifndef M_PI
-#define M_PI 3.1415926535897932384626433832795
-#endif
-
 namespace Kratos {
 namespace Testing {
 
