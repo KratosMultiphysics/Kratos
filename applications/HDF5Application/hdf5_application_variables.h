a360c9d00781eb0ac5a13e262c39ccbdf368381d
msandre <michael.andre@tum.de>
2017-11-09 10:33:17 +0100

    updated license.

--- applications/HDF5Application/hdf5_application_variables.h
+++ applications/HDF5Application/hdf5_application_variables.h
@@ -5,7 +5,7 @@
 //                   Multi-Physics
 //
 //  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//					 Kratos default license: HDF5Application/license.txt
 //
 //  Main authors:    Jordi Cotela
 //
