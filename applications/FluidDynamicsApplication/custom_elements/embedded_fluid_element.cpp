f454c50224b9e71d3fa7a9bcc1cf84677c579826
jcotela <jcotela@cimne.upc.edu>
2017-11-28 17:04:50 +0100

    Skeleton structure for the new embedded element.

---
+++ applications/FluidDynamicsApplication/custom_elements/embedded_fluid_element.cpp
@@ -0,0 +1,109 @@
+#include "custom_elements/embedded_fluid_element.h"
+#include "custom_elements/qs_vms.h"
+#include "custom_elements/symbolic_navier_stokes.h"
+
+#include "custom_utilities/time_integrated_qsvms_data.h"
+#include "custom_utilities/symbolic_navier_stokes_data.h"
+
+namespace Kratos {
+
+template class EmbeddedFluidElement< QSVMS< TimeIntegratedQSVMSData<3,4> > >;
+template class EmbeddedFluidElement< SymbolicNavierStokes< SymbolicNavierStokesData<3,4> > >;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Life cycle
+
+template< class TBaseElement >
+EmbeddedFluidElement<TBaseElement>::EmbeddedFluidElement(IndexType NewId):
+    TBaseElement(NewId)
+{}
+
+template< class TBaseElement >
+EmbeddedFluidElement<TBaseElement>::EmbeddedFluidElement(IndexType NewId, const NodesArrayType& ThisNodes):
+    TBaseElement(NewId,ThisNodes)
+{}
+
+
+template< class TBaseElement >
+EmbeddedFluidElement<TBaseElement>::EmbeddedFluidElement(IndexType NewId, GeometryType::Pointer pGeometry):
+    TBaseElement(NewId,pGeometry)
+{}
+
+template< class TBaseElement >
+EmbeddedFluidElement<TBaseElement>::EmbeddedFluidElement(IndexType NewId, GeometryType::Pointer pGeometry, Properties::Pointer pProperties):
+    TBaseElement(NewId,pGeometry,pProperties)
+{}
+
+
+template< class TBaseElement >
+EmbeddedFluidElement<TBaseElement>::~EmbeddedFluidElement()
+{}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Public Operations
+
+template< class TBaseElement >
+Element::Pointer EmbeddedFluidElement<TBaseElement>::Create(IndexType NewId,NodesArrayType const& ThisNodes,Properties::Pointer pProperties) const
+{
+    return Element::Pointer(new EmbeddedFluidElement(NewId, this->GetGeometry().Create(ThisNodes), pProperties));
+}
+
+
+template< class TBaseElement >
+Element::Pointer EmbeddedFluidElement<TBaseElement>::Create(IndexType NewId,GeometryType::Pointer pGeom,Properties::Pointer pProperties) const
+{
+    return Element::Pointer(new EmbeddedFluidElement(NewId, pGeom, pProperties));
+}
+
+template <class TBaseElement>
+void EmbeddedFluidElement<TBaseElement>::CalculateLocalSystem(
+    MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector,
+    ProcessInfo& rCurrentProcessInfo) {}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Inquiry
+
+template <class TBaseElement>
+int EmbeddedFluidElement<TBaseElement>::Check(
+    const ProcessInfo& rCurrentProcessInfo) {
+    return TBaseElement::Check(rCurrentProcessInfo);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Input and output
+
+template <class TBaseElement>
+std::string EmbeddedFluidElement<TBaseElement>::Info() const {
+    std::stringstream buffer;
+    buffer << "EmbeddedFluidElement #" << this->Id();
+    return buffer.str();
+}
+
+template <class TBaseElement>
+void EmbeddedFluidElement<TBaseElement>::PrintInfo(std::ostream& rOStream) const {
+    rOStream << "EmbeddedFluidElement" << Dim << "D" << NumNodes << "N"
+             << std::endl
+             << "on top of ";
+    TBaseElement::PrintInfo(rOStream);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Private functions
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// serializer
+
+template< class TBaseElement >
+void EmbeddedFluidElement<TBaseElement>::save(Serializer& rSerializer) const
+{
+    KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, TBaseElement );
+}
+
+
+template< class TBaseElement >
+void EmbeddedFluidElement<TBaseElement>::load(Serializer& rSerializer)
+{
+    KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, TBaseElement);
+}
+
+}
---
+++ applications/FluidDynamicsApplication/custom_elements/embedded_fluid_element.h
@@ -0,0 +1,331 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Jordi Cotela
+//
+
+#ifndef KRATOS_EMBEDDED_FLUID_ELEMENT_H
+#define KRATOS_EMBEDDED_FLUID_ELEMENT_H
+
+#include "includes/define.h"
+#include "includes/element.h"
+#include "includes/serializer.h"
+#include "geometries/geometry.h"
+
+#include "includes/cfd_variables.h"
+#include "custom_elements/fluid_element.h"
+
+namespace Kratos
+{
+
+///@addtogroup FluidDynamicsApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+template< class TBaseElement >
+class EmbeddedFluidElement : public TBaseElement
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of EmbeddedFluidElement
+    KRATOS_CLASS_POINTER_DEFINITION(EmbeddedFluidElement);
+
+    /// Node type (default is: Node<3>)
+    typedef Node<3> NodeType;
+
+    /// Geometry type (using with given NodeType)
+    typedef Geometry<NodeType> GeometryType;
+
+    /// Definition of nodes container type, redefined from GeometryType
+    typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
+
+    /// Vector type for local contributions to the linear system
+    typedef Vector VectorType;
+
+    /// Matrix type for local contributions to the linear system
+    typedef Matrix MatrixType;
+
+    typedef std::size_t IndexType;
+
+    typedef std::size_t SizeType;
+
+    typedef std::vector<std::size_t> EquationIdVectorType;
+
+    typedef std::vector< Dof<double>::Pointer > DofsVectorType;
+
+    typedef PointerVectorSet<Dof<double>, IndexedObject> DofsArrayType;
+
+    /// Type for shape function values container
+    typedef Kratos::Vector ShapeFunctionsType;
+
+    /// Type for a matrix containing the shape function gradients
+    typedef Kratos::Matrix ShapeFunctionDerivativesType;
+
+    /// Type for an array of shape function gradient matrices
+    typedef GeometryType::ShapeFunctionsGradientsType ShapeFunctionDerivativesArrayType;
+
+    constexpr static unsigned int Dim = TBaseElement::Dim;
+    constexpr static unsigned int NumNodes = TBaseElement::NumNodes;
+    constexpr static unsigned int BlockSize = TBaseElement::BlockSize;
+    constexpr static unsigned int LocalSize = TBaseElement::LocalSize;
+
+    using ElementData = typename TBaseElement::ElementData;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    //Constructors.
+
+    /// Default constuctor.
+    /**
+     * @param NewId Index number of the new element (optional)
+     */
+    EmbeddedFluidElement(IndexType NewId = 0);
+
+    /// Constructor using an array of nodes.
+    /**
+     * @param NewId Index of the new element
+     * @param ThisNodes An array containing the nodes of the new element
+     */
+    EmbeddedFluidElement(IndexType NewId, const NodesArrayType& ThisNodes);
+
+    /// Constructor using a geometry object.
+    /**
+     * @param NewId Index of the new element
+     * @param pGeometry Pointer to a geometry object
+     */
+    EmbeddedFluidElement(IndexType NewId, GeometryType::Pointer pGeometry);
+
+    /// Constuctor using geometry and properties.
+    /**
+     * @param NewId Index of the new element
+     * @param pGeometry Pointer to a geometry object
+     * @param pProperties Pointer to the element's properties
+     */
+    EmbeddedFluidElement(IndexType NewId, GeometryType::Pointer pGeometry, Properties::Pointer pProperties);
+
+    /// Destructor.
+    ~EmbeddedFluidElement() override;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+
+    /// Create a new element of this type
+    /**
+     * Returns a pointer to a new EmbeddedFluidElement element, created using given input
+     * @param NewId: the ID of the new element
+     * @param ThisNodes: the nodes of the new element
+     * @param pProperties: the properties assigned to the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Create(IndexType NewId,
+                            NodesArrayType const& ThisNodes,
+                            Properties::Pointer pProperties) const override;
+
+    /// Create a new element of this type using given geometry
+    /**
+     * Returns a pointer to a new FluidElement element, created using given input
+     * @param NewId: the ID of the new element
+     * @param pGeom: a pointer to the geomerty to be used to create the element
+     * @param pProperties: the properties assigned to the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Create(IndexType NewId,
+                            GeometryType::Pointer pGeom,
+                            Properties::Pointer pProperties) const override;
+
+    void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo) override;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+    int Check(const ProcessInfo &rCurrentProcessInfo) override;
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override;
+
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override;
+
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+protected:
+
+    ///@name Protected static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+    ///@}
+
+private:
+
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    void save(Serializer& rSerializer) const override;
+
+    void load(Serializer& rSerializer) override;
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    /// Assignment operator.
+    EmbeddedFluidElement& operator=(EmbeddedFluidElement const& rOther);
+
+    /// Copy constructor.
+    EmbeddedFluidElement(EmbeddedFluidElement const& rOther);
+
+    ///@}
+
+
+}; // Class EmbeddedFluidElement
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+/// input stream function
+template< class TElementData >
+inline std::istream& operator >>(std::istream& rIStream,
+                                 EmbeddedFluidElement<TElementData>& rThis)
+{
+    return rIStream;
+}
+
+/// output stream function
+template< class TElementData >
+inline std::ostream& operator <<(std::ostream& rOStream,
+                                 const EmbeddedFluidElement<TElementData>& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+///@}
+
+///@} // Fluid Dynamics Application group
+
+} // namespace Kratos.
+
+#endif // KRATOS_EMBEDDED_FLUID_ELEMENT_H
