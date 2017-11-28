076cecf2ff22d81b753a0029b20a0d3ebf63649e
Unknown <cgguillem@gmail.com>
2017-11-28 12:41:19 +0100

    updating to modern style and simplifying a bit

--- applications/swimming_DEM_application/python_scripts/chandelier/run_benchmark_candelier.py
+++ applications/swimming_DEM_application/python_scripts/chandelier/run_benchmark_candelier.py
@@ -1,10 +1,13 @@
+from KratosMultiphysics import *
 import KratosSwimmingDEM as script
+import json
 import os
+import candelier_algorithm
 
 def PrintMessage(run_name, radial_error, tolerance):
         run_name += ': '
         run_name += ': '
-        error_message = 'relative radial error: '
+        error_message = '    relative radial error: '
 
         messages_to_print = [run_name, error_message]
         max_len = max([len(msg) for msg in {run_name, error_message}])
@@ -23,31 +26,35 @@ tolerance = 1e-4
 errors = []
 error_names = []
 varying_parameters = dict()
-varying_parameters['simulation_time'] = 1
-varying_parameters['Nq'] = 1
-varying_parameters['m'] = 10
+varying_parameters['FinalTime'] = 1
+varying_parameters['time_steps_per_quadrature_step'] = 1
+varying_parameters['number_of_exponentials'] = 10
 varying_parameters['number_of_quadrature_steps_in_window'] = 10
 varying_parameters['basset_force_type'] = 0
 
 # No history force benchmark
-import candelier_algorithm
-test = script.Solution(candelier_algorithm.Algorithm(varying_parameters), varying_parameters)
-error_names.append('No history force, Daitche')
-errors.append(test.alg.Run())
+parameters = Parameters(json.dumps(varying_parameters))
+with script.Solution(candelier_algorithm, parameters) as test:
+    error_names.append('No history force, Daitche')
+    errors.append(test.alg.Run())
 
 varying_parameters['basset_force_type'] = 2
 # Second-order accurate Daitche benchmark
-test = script.Solution(candelier_algorithm.Algorithm(varying_parameters), varying_parameters)
-error_names.append('All forces, Daitche')
-errors.append(test.alg.Run())
+parameters = Parameters(json.dumps(varying_parameters))
+with script.Solution(candelier_algorithm, parameters) as test:
+    error_names.append('All forces, Daitche')
+    errors.append(test.alg.Run())
 
 # Output
-print()
-print('-----------------------')
+width_buffer = 5
+width = max([len(name) + len(str(error)) for name, error in zip(error_names, errors)]) + width_buffer
+thick_line = '=' * width
+separator = '-' * width
+print(thick_line)
 print('Candelier tests results')
-print('-----------------------')
+print(thick_line)
 
-for i, e in enumerate(errors):
-    PrintMessage(error_names[i], e, tolerance)
-print('-----------------------')
-print()
+for name, error in zip(error_names, errors):
+    PrintMessage(name, error, tolerance)
+    print(separator)
+print(thick_line)
