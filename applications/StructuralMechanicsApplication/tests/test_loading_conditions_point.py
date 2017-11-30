b5fd870d247f155475453d7dbbe9f877039dd9ab
philbucher <philipp.bucher@tum.de>
2017-11-30 12:29:07 +0100

    Restructured Loading Tests

---
+++ applications/StructuralMechanicsApplication/tests/test_loading_conditions_point.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, absolute_import, division
+import KratosMultiphysics 
+
+import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+import math
+
+
+class TestLoadingConditionsPoint(KratosUnittest.TestCase):
+    def setUp(self):
+        pass
+    
+    
+        
+if __name__ == '__main__':
+    KratosUnittest.main()
