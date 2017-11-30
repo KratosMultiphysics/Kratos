18ba31da4e31d97dd775898c81313c85258a6ae0
Unknown <lmonforte@cimne.upc.edu>
2017-11-30 13:21:42 +0100

    Define CriticalState parameters in mat_variables
    remove them from PfemSolidMec, then the MCC of ConstitutiveModels may get them

--- applications/ConstitutiveModelsApplication/custom_models/plasticity_models/yield_surfaces/modified_cam_clay_yield_surface.hpp
+++ applications/ConstitutiveModelsApplication/custom_models/plasticity_models/yield_surfaces/modified_cam_clay_yield_surface.hpp
@@ -110,15 +110,18 @@ namespace Kratos
     {
       KRATOS_TRY
 
+      const ModelDataType & rModelData = rVariables.GetModelData();
+      const MatrixType    & rStressMatrix = rModelData.GetStressMatrix();
+
       // Material Parameters
-      const double ShearM = 1.0;
+      const Properties& rMaterialProperties = rModelData.GetMaterialProperties();
+      const double& rShearM = rMaterialProperties[CRITICAL_STATE_LINE];
+
 
       // compute something with the hardening rule
       double PreconsolidationStress;
       PreconsolidationStress = this->mHardeningRule.CalculateHardening( rVariables, PreconsolidationStress );
 
-      const ModelDataType & rModelData = rVariables.GetModelData();
-      const MatrixType    & rStressMatrix = rModelData.GetStressMatrix();
 
       double MeanStress, LodeAngle;
       double DeviatoricQ; // == sqrt(3)*J2
@@ -128,7 +131,7 @@ namespace Kratos
       DeviatoricQ *= sqrt(3.0);
 
 
-      rYieldCondition  = pow( DeviatoricQ/ShearM, 2);
+      rYieldCondition  = pow( DeviatoricQ/rShearM, 2);
       rYieldCondition += (MeanStress * (MeanStress - PreconsolidationStress) );
 
 
@@ -145,15 +148,17 @@ namespace Kratos
     {
       KRATOS_TRY
 
+      const ModelDataType & rModelData = rVariables.GetModelData();
+      const MatrixType    & rStressMatrix = rModelData.GetStressMatrix();
+
       // Material Parameters
-      const double ShearM = 1.0;
+      const Properties& rMaterialProperties = rModelData.GetMaterialProperties();
+      const double& rShearM = rMaterialProperties[CRITICAL_STATE_LINE];
 
       // compute something with the hardening rule
       double PreconsolidationStress;
       PreconsolidationStress = this->mHardeningRule.CalculateHardening( rVariables, PreconsolidationStress );
 
-      const ModelDataType & rModelData = rVariables.GetModelData();
-      const MatrixType    & rStressMatrix = rModelData.GetStressMatrix();
 
       double MeanStress, J2, LodeAngle;
      
@@ -162,7 +167,7 @@ namespace Kratos
       StressInvariantsUtilities::CalculateStressInvariants( rStressMatrix, MeanStress, J2, LodeAngle);
       StressInvariantsUtilities::CalculateDerivativeVectors( rStressMatrix, V1, V2);
 
-      rDeltaStressYieldCondition  = ( 2.0*MeanStress - PreconsolidationStress) * V1 + 2.0 * 3.0 * pow( 1.0 / ShearM, 2) * J2 * V2;
+      rDeltaStressYieldCondition  = ( 2.0*MeanStress - PreconsolidationStress) * V1 + 2.0 * 3.0 * pow( 1.0 / rShearM, 2) * J2 * V2;
 
       return rDeltaStressYieldCondition;
 
