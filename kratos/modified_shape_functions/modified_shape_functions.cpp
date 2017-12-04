72a0dccd4b69a2d379373bdb82ab4c8aa0c7f64d
jcotela <jcotela@cimne.upc.edu>
2017-12-04 11:18:25 +0100

    Writing to the correct position in shape function derivatives vector.

--- kratos/modified_shape_functions/modified_shape_functions.cpp
+++ kratos/modified_shape_functions/modified_shape_functions.cpp
@@ -181,7 +181,7 @@ namespace Kratos
                     }
                 }
 
-                rShapeFunctionsGradientsValues[i_gauss] = trans(prod(sh_func_gradients_mat, rPmatrix));
+                rShapeFunctionsGradientsValues[i_subdivision*n_int_pts + i_gauss] = trans(prod(sh_func_gradients_mat, rPmatrix));
             }
         }
     };
@@ -294,7 +294,7 @@ namespace Kratos
                 }
 
                 aux_grad_sh_func_cond = prod(aux_grad_sh_func_exp, rPmatrix);
-                rInterfaceShapeFunctionsGradientsValues[i_gauss] = trans(aux_grad_sh_func_cond);
+                rInterfaceShapeFunctionsGradientsValues[i_interface*n_int_pts + i_gauss] = trans(aux_grad_sh_func_cond);
             }
         }
     };
