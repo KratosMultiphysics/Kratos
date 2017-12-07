5dfe8343ee43aff827f11f77d6a605159602bfe1
Klaus Bernd Sautter <klaus.sautter@tum.de>
2017-12-07 15:26:09 +0100

    Update cr_beam_element_2D2N.cpp
    globalize consistent mass matrix

--- applications/StructuralMechanicsApplication/custom_elements/cr_beam_element_2D2N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/cr_beam_element_2D2N.cpp
@@ -194,6 +194,9 @@ namespace Kratos
 		rMassMatrix(5, 2) = pre_beam * (-3.00) * L * L;
 		rMassMatrix(5, 4) = pre_beam * (-22.00) * L;
 		rMassMatrix(5, 5) = pre_beam * (4.00) * L * L;		
+		
+		this->GlobalizeMatrix(rMassMatrix);
+		
 		KRATOS_CATCH("")
 	}
 
