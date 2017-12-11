83c722d1d79bf4732f4956014577cd459647059b
Ali Nuri Kayis <alinuri.kayis@tum.de>
2017-12-11 15:15:58 +0100

    name is changed for hdf5 to avoid conflict and registered here

--- applications/ShapeOptimizationApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/ShapeOptimizationApplication/custom_python/add_custom_processes_to_python.cpp
@@ -21,8 +21,8 @@
 
 // Project includes
 #include "processes/process.h"
-#include "custom_processes/output_primal_solution_process.h"
-#include "custom_processes/input_primal_solution_process.h"
+#include "custom_processes/output_primal_structural_solution_process.h"
+#include "custom_processes/input_primal_structural_solution_process.h"
 #include "custom_processes/replace_elements_and_conditions_for_adjoint_problem_process.h"
 #include "includes/model_part.h"
 #include "custom_python/add_custom_processes_to_python.h"
@@ -37,12 +37,12 @@ void AddCustomProcessesToPython()
 {
     using namespace boost::python;
 
-    class_< OutputPrimalSolutionProcess, bases<Process> >
-    ("OutputPrimalSolutionProcess", init<ModelPart&, Parameters&>())
+    class_< OutputPrimalStructuralSolutionProcess, bases<Process> >
+    ("OutputPrimalStructuralSolutionProcess", init<ModelPart&, Parameters&>())
     ;
 
-    class_< InputPrimalSolutionProcess, bases<Process> >
-    ("InputPrimalSolutionProcess", init<ModelPart&, Parameters&>())
+    class_< InputPrimalStructuralSolutionProcess, bases<Process> >
+    ("InputPrimalStructuralSolutionProcess", init<ModelPart&, Parameters&>())
     ;
 
     class_<ReplaceElementsAndConditionsForAdjointProblemProcess , bases<Process>, boost::noncopyable >("ReplaceElementsAndConditionsForAdjointProblemProcess",
