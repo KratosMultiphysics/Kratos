85994d5aea08c2e10f31345941c961d5b7e6e99a
philbucher <philipp.bucher@tum.de>
2017-12-15 13:52:07 +0100

    Added global test Script

--- applications/EigenSolversApplication/tests/test_eigen_direct_solver.py
+++ applications/EigenSolversApplication/tests/test_eigen_direct_solver.py
@@ -1,29 +1,22 @@
 
 from __future__ import print_function, absolute_import, division
 import KratosMultiphysics
+
+import KratosMultiphysics.EigenSolversApplication as EigenSolversApplication
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 from new_linear_solver_factory import ConstructSolver
 
 class TestEigenDirectSolver(KratosUnittest.TestCase):
-    def _test_eigen_direct_solver(self, class_name, solver_type):
-        # check if application is available
-        try:
-            import KratosMultiphysics.EigenSolversApplication
-        except:
-            self.skipTest("EigenSolversApplication is not available")
-        
+    def _execute_eigen_direct_solver_test(self, class_name, solver_type):
         # check if solver is available
-        if (not hasattr(KratosMultiphysics.EigenSolversApplication, class_name)):
+        if (not hasattr(EigenSolversApplication, class_name)):
             self.skipTest(class_name + " is not included in the compilation of the EigenSolversApplication")
 
-
         space = KratosMultiphysics.UblasSparseSpace()
-
 
         settings = KratosMultiphysics.Parameters('{ "solver_type" : "' + solver_type + '" }')
         
         solver = ConstructSolver(settings)
-
 
         a = KratosMultiphysics.CompressedMatrix()
         
@@ -37,28 +30,26 @@ class TestEigenDirectSolver(KratosUnittest.TestCase):
             b_exp[i] = i + 1
 
         x = KratosMultiphysics.Vector(dimension)
-        
 
         solver.Solve(a, x, b_exp)
-
 
         b_act = KratosMultiphysics.Vector(dimension)
         space.Mult(a, x, b_act)
 
         for i in range(dimension):
-            self.assertLess(abs(b_act[i] - b_exp[i]), 1e-7)
+            self.assertAlmostEqual(b_act[i], b_exp[i], 7)
 
     def test_eigen_sparse_lu(self):
-        self._test_eigen_direct_solver('SparseLUSolver', 'eigen_sparse_lu')
+        self._execute_eigen_direct_solver_test('SparseLUSolver', 'eigen_sparse_lu')
     
     def test_eigen_pardiso_lu(self):
-        self._test_eigen_direct_solver('PardisoLUSolver', 'eigen_pardiso_lu')
+        self._execute_eigen_direct_solver_test('PardisoLUSolver', 'eigen_pardiso_lu')
 
     def test_eigen_pardiso_ldlt(self):
-        self._test_eigen_direct_solver('PardisoLDLTSolver', 'eigen_pardiso_ldlt')
+        self._execute_eigen_direct_solver_test('PardisoLDLTSolver', 'eigen_pardiso_ldlt')
 
     def test_eigen_pardiso_llt(self):
-        self._test_eigen_direct_solver('PardisoLLTSolver', 'eigen_pardiso_llt')
+        self._execute_eigen_direct_solver_test('PardisoLLTSolver', 'eigen_pardiso_llt')
 
 if __name__ == '__main__':
     KratosUnittest.main()
