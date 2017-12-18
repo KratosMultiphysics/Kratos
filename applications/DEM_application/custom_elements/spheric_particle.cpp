61d5dd2a68d0591883ae8a7c49d3b8e1554a3505
Unknown <jirazabal@cimne.upc.edu>
2017-12-18 12:41:44 +0100

    Minor change

--- applications/DEM_application/custom_elements/spheric_particle.cpp
+++ applications/DEM_application/custom_elements/spheric_particle.cpp
@@ -663,18 +663,16 @@ void SphericParticle::ComputeMoments(double NormalLocalContactForce,
     // ROLLING FRICTION
     if (this->Is(DEMFlags::HAS_ROLLING_FRICTION)) {
 
-        double equiv_rolling_friction_coeff; 
+        double equiv_rolling_friction_coeff = 0.0; 
 
         if (!wall) {
-            const double my_rolling_friction_coeff      = GetRollingFriction() * GetRadius(); 
-            const double other_rolling_friction_coeff   = p_neighbour->GetRollingFriction() * p_neighbour->GetRadius(); 
-            const double rolling_friction_coeff_sum     = my_rolling_friction_coeff + other_rolling_friction_coeff; 
-            const double rolling_friction_coeff_sum_inv = 1.0 / rolling_friction_coeff_sum; 
-            equiv_rolling_friction_coeff                = my_rolling_friction_coeff * other_rolling_friction_coeff * rolling_friction_coeff_sum_inv; 
+            const double my_rolling_friction_coeff    = GetRollingFriction() * GetRadius(); 
+            const double other_rolling_friction_coeff = p_neighbour->GetRollingFriction() * p_neighbour->GetRadius(); 
+            equiv_rolling_friction_coeff = std::min(my_rolling_friction_coeff, other_rolling_friction_coeff);
         }
 
-        if (wall) {
-            equiv_rolling_friction_coeff = GetRollingFriction() * GetRadius(); // Should be wall->GetRollingFriction() * GetRadius(); JIG
+        else if (wall) {
+            equiv_rolling_friction_coeff = GetRollingFriction() * GetRadius();
         }
 
         if (equiv_rolling_friction_coeff != 0.0) {
