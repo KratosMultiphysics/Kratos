672a4127a2a9e183333e114bbb1e16f64196f558
aditya_office_pc <shine.aditya@gmail.com>
2017-12-29 12:05:19 +0100

    Adding first test example

---
+++ applications/CoSimulationApplication/test_examples/simple_test_one/MainKratos.py
@@ -0,0 +1,45 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import KratosMultiphysics
+from KratosMultiphysics import *
+import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
+import KratosMultiphysics.ExternalSolversApplication as ExternalSolversApplication
+import KratosMultiphysics.CoSimulationApplication as CoSimulationApplication
+import os
+
+parameter_file = open("ProjectParameters.json",'r')
+ProjectParameters = KratosMultiphysics.Parameters( parameter_file.read())
+
+convScheme = CoSimulationApplication.CoSimulationBaseConvergenceAccelerationScheme()
+io = CoSimulationApplication.CoSimulationBaseIo(KratosMultiphysics.Parameters("""{ }"""))
+app1 = CoSimulationApplication.CoSimulationBaseApplication( io, KratosMultiphysics.Parameters("""{ }""") )
+app2 = CoSimulationApplication.CoSimulationBaseApplication( io, KratosMultiphysics.Parameters("""{ }""") )
+
+newStrategyModule = __import__(ProjectParameters["co_simulation_solver_settings"]["solver_type"].GetString())
+solver = newStrategyModule.CreateSolver(app1, app2, convScheme)
+solver.Initialize()
+solver.Check()
+solver.Solve()
+
+
+delta_time = ProjectParameters["problem_data"]["time_step"].GetDouble()
+# Start time
+time = ProjectParameters["problem_data"]["start_time"].GetDouble()
+# End time
+end_time = ProjectParameters["problem_data"]["end_time"].GetDouble()
+
+step = 0
+time = 0
+# Solving the problem (time integration)
+while(time <= end_time):
+    time = time + delta_time
+    step = step + 1
+    
+    solver.InitializeSolutionStep()
+    print('############## ')
+    print('Step :: ', step)
+    print('Time :: ', time)
+    
+    solver.SolveSolutionStep()
+    
+    solver.FinalizeSolutionStep()
