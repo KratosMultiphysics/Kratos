19932675455cdbf8af952037ddf70e36e4b25a9f
Unknown <cgguillem@gmail.com>
2018-01-01 16:18:41 +0100

    Making the fluid loading work properly fro the start

--- applications/swimming_DEM_application/python_scripts/PID/run_PID.py
+++ applications/swimming_DEM_application/python_scripts/PID/run_PID.py
@@ -1,8 +1,11 @@
 from KratosMultiphysics import *
 from KratosMultiphysics.DEMApplication import *
+from KratosMultiphysics.FluidDynamicsApplication import *
+from KratosMultiphysics.IncompressibleFluidApplication import *
 from KratosMultiphysics.SwimmingDEMApplication import *
 import pre_calculated_fluid_algorithm
 BaseAlgorithm = pre_calculated_fluid_algorithm.Algorithm
+
 import h5py
 import json
 
@@ -11,23 +14,21 @@ import KratosSwimmingDEM as script
 varying_parameters = dict()
 combinations_that_failed = []
 errors = []
-varying_parameters["fluid_already_calculated"] = False
+varying_parameters["fluid_already_calculated"] = True
 
 parameters = Parameters(json.dumps(varying_parameters))
 
 with script.Solution(pre_calculated_fluid_algorithm, parameters) as test:
     test.Run()
 
-
-print()
-print('****************************************')
+print('\n****************************************')
 
 if len(combinations_that_failed):
-    print('The following combinations produced an error:')
-    print()
+    print('The following combinations produced an error:\n')
+
     for combination, error in zip(combinations_that_failed, errors):
         print(combination)
         print(error)
 else:
     print('All combinations run without errors')
-print('****************************************')
+print('****************************************\n')
