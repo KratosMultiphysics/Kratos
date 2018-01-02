72ce6d4605dd8ce54c2c48b46ecd765aa3fcffb0
Unknown <mmaso@cimne.upc.edu>
2018-01-02 13:58:01 +0100

    updating laplacian example

--- kratos/python_scripts/application_generator/laplacian_application_example.py
+++ kratos/python_scripts/application_generator/laplacian_application_example.py
@@ -8,20 +8,15 @@ from classes.variableCreator import VariableCreator
 
 from applicationGenerator import ApplicationGenerator
 
-if len(sys.argv) != 2:
-    print("Error: Invalid usage.\nPlease call this script with your application name (in camelCase) like this: ")
-    print("python laplacian_application_example.py MyNewApplication")
-    exit()
-
-# Read the application name and generate Camel, Caps and Low
-appNameCamel = sys.argv[1]
+# Set the application name and generate Camel, Caps and Low
+appNameCamel = "PureDiffusion"
 
 # Fetch the applications directory
 debugApp = ApplicationGenerator(appNameCamel)
 
 # Add KratosVariables
 debugApp.AddVariables([
-    VariableCreator(name='MY_SCALAR', vtype='double'),
+    VariableCreator(name='POINT_SOURCE', vtype='double'),
 ])
 
 # Add test element
