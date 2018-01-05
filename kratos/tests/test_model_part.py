c6ffba58ae76e637e00959afdbd019c94c46ca94
Riccardo Rossi <rrossi@cimne.upc.edu>
2018-01-05 19:25:51 +0100

    enriching a little the test

--- kratos/tests/test_model_part.py
+++ kratos/tests/test_model_part.py
@@ -505,9 +505,13 @@ class TestModelPart(KratosUnittest.TestCase):
         
         #this should add node 3 to both sub1 and model_part1, but not to sub2
         sub1.AddNode( model_part2.Nodes[3], 0 )  
-        self.assertTrue( n3.Id in sub1.Nodes )
-        self.assertTrue( n3.Id in model_part1.Nodes )
-        self.assertFalse( n3.Id in sub2.Nodes )        
+        #self.assertTrue( n3.Id in sub1.Nodes )
+        #self.assertTrue( n3.Id in model_part1.Nodes )
+        #self.assertFalse( n3.Id in sub2.Nodes )        
+        self.assertTrue( n3 in sub1.Nodes )
+        self.assertTrue( n3 in model_part1.Nodes )
+        self.assertFalse( n3 in sub2.Nodes )        
+
         
         ##next should throw an exception, since we try to add a node with Id1 which already exists
         with self.assertRaisesRegex(RuntimeError, "Error\: attempting to add pNewNode with Id \:1, unfortunately a \(different\) node with the same Id already exists\n"):
