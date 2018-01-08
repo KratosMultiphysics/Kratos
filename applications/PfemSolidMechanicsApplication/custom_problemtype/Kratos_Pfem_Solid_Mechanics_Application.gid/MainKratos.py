220ef8ff830f25937f6cefc66550d214ba425259
Unknown <lmonforte@cimne.upc.edu>
2018-01-08 17:24:57 +0100

    Add contact. Modify the unix bat

--- applications/PfemSolidMechanicsApplication/custom_problemtype/Kratos_Pfem_Solid_Mechanics_Application.gid/MainKratos.py
+++ applications/PfemSolidMechanicsApplication/custom_problemtype/Kratos_Pfem_Solid_Mechanics_Application.gid/MainKratos.py
@@ -3,6 +3,7 @@ import KratosMultiphysics.ExternalSolversApplication
 import KratosMultiphysics.SolidMechanicsApplication
 import KratosMultiphysics.ConstitutiveModelsApplication
 import KratosMultiphysics.PfemApplication
+import KratosMultiphysics.ContactMechanicsApplication
 import KratosMultiphysics.PfemSolidMechanicsApplication
 import MainSolid
 
