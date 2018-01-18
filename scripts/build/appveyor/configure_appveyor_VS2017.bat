ddfdbf2865081c3962d80a9625ea7d0fb52e5db9
Carlos Roig <roigcarlo@gmail.com>
2018-01-18 10:53:56 +0100

    Updating versions, cleaning config

--- scripts/build/appveyor/configure_appveyor_VS2017.bat
+++ scripts/build/appveyor/configure_appveyor_VS2017.bat
@@ -1,10 +1,11 @@
 cmake .. -G "Visual Studio 15 2017 Win64"
--DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -msse3   " 								                ^
--DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -msse3   " 									                  ^
--DBOOST_ROOT="C:\Libraries\boost_1_59_0"                                        ^
--DPYTHON_INCLUDE_PATH="C:\Python35-x64\include"							                    ^
--DPYTHON_LIBRARY="C:\Python35-x64\lib\python3.dll"					                    ^
--DCMAKE_BUILD_TYPE="Custom"  													                          ^
+-DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -msse3   " 								^
+-DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -msse3   " 									^
+-DBOOST_ROOT="C:\Libraries\boost_1_65_1"                                        ^
+-DBOOST_PYTHON_SUFFIX="3"                                                       ^
+-DPYTHON_INCLUDE_PATH="C:\Python36-x64\include"							        ^
+-DPYTHON_LIBRARY="C:\Python36-x64\lib\python3.dll"					            ^
+-DCMAKE_BUILD_TYPE="Debug"  													^
 -DALE_APPLICATION=ON                                                            ^
 -DADJOINT_FLUID_APPLICATION=OFF                                                 ^
 -DCONVECTION_DIFFUSION_APPLICATION=ON                                           ^
