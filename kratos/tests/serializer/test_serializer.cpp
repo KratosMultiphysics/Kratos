0375c3285383c4b254916b1a43a72f00d1e49535
philbucher <philipp.bucher@tum.de>
2018-01-29 16:43:41 +0100

    Initial commit serializer test

---
+++ kratos/tests/serializer/test_serializer.cpp
@@ -0,0 +1,43 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:     BSD License
+//           Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+//
+
+// Project includes
+#include "testing/testing.h"
+#include "includes/serializer.h"
+
+namespace Kratos {
+    namespace Testing {
+
+        KRATOS_TEST_CASE_IN_SUITE(ZZZMyCustomTest, KratosCoreFastSuite)
+        {
+            Serializer serializer;
+
+            const std::string tag_string("TestString");
+
+            std::map<std::string, int> first;
+
+            std::map<std::string, int> loaded;
+
+            first["www"]=10;
+            first["eee"]=30;
+            first["rrr"]=50;
+            first["ttt"]=70;
+
+            serializer.save(tag_string, first);
+
+            serializer.load(tag_string, loaded);
+
+
+        }
+    } // namespace Testing
+}  // namespace Kratos.
