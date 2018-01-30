47b877dbe6589207c08f60d2434909366d8a6169
philbucher <philipp.bucher@tum.de>
2018-01-30 11:43:23 +0100

    Initial Commit

---
+++ applications/StructuralMechanicsApplication/custom_utilities/static_condensation_utility.cpp
@@ -0,0 +1,32 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics 
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Klauss B Sautter
+//
+
+
+// System includes
+
+
+// External includes 
+
+
+// Project includes
+#include "static_condensation_utility.h"
+
+
+namespace Kratos
+{
+	namespace StaticCondensationUtility
+	{
+		
+
+	}  // namespace StaticCondensationUtility
+  
+}  // namespace Kratos.
---
+++ applications/StructuralMechanicsApplication/custom_utilities/static_condensation_utility.h
@@ -0,0 +1,43 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics 
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Klauss B Sautter
+//
+
+#if !defined(KRATOS_STATIC_CONDENSATION_UTILITY_H_INCLUDED )
+#define  KRATOS_STATIC_CONDENSATION_UTILITY_H_INCLUDED
+
+
+// System includes
+// #include <string>
+// #include <iostream> 
+
+
+// External includes 
+
+
+// Project includes
+#include "includes/define.h"
+// #include "includes/element.h"
+// #include "includes/properties.h"
+
+
+namespace Kratos
+{
+	namespace StaticCondensationUtility
+	{
+		
+
+	}  // namespace StaticCondensationUtility
+  
+}  // namespace Kratos.
+
+#endif // KRATOS_STATIC_CONDENSATION_UTILITY_H_INCLUDED  defined 
+
+
