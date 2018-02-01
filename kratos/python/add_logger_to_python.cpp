a4f899d4b2b19641fb76f8e34e20702ba6876001
Carlos Roig <roigcarlo@gmail.com>
2018-02-01 11:43:56 +0100

    Recoding the python interface so its usable

--- kratos/python/add_logger_to_python.cpp
+++ kratos/python/add_logger_to_python.cpp
@@ -17,6 +17,7 @@
 
 // Project includes
 #include "input_output/logger.h"
+
 using namespace boost::python;
 
 namespace Kratos
@@ -25,38 +26,59 @@ namespace Kratos
 namespace Python
 {
 
+object printImpl(tuple args, dict kwargs, Logger::Severity severity) {
+    std::cout << "?" << std::endl;
+    if(len(args) == 0)
+        std::cout << "ERROR" << std::endl;
+
+    Logger& loggerRef = boost::python::extract<Logger&>(args[0]);
+    
+    std::stringstream buffer;
+
+    // Extract the tuple part
+    std::cout << len(args)-1 << std::endl;
+    for(int i = 1; i < len(args); ++i) {
+        object curArg = args[i];
+        std::cout << "Printing arg " << i << std::endl;
+        std::cout << extract<const char *>(boost::python::str(args[i])) << std::endl;
+        if(curArg) {
+            buffer << extract<const char *>(boost::python::str(args[i])) << std::endl;
+        }
+    }
+
+    loggerRef << KRATOS_CODE_LOCATION << buffer.str() << severity;
+    std::cout << "--?" << std::endl;
+
+    return object();
+}
+
 /**
  * Prints the arguments from the python script using the Kratos Logger class
  * @args: tuple boost::python::object representing the arguments of the function The first argument is the label
  * @kwargs: dictionary of boost::python::objects resenting key-value pairs for
  * name arguments
  **/
-object print(tuple args, dict kwargs) {
-  if(len(args) == 0)
-    return object();
-  
-  std::stringstream buffer;
+object printInfo(tuple args, dict kwargs) {
+    return printImpl(args, kwargs, Logger::Severity::INFO);
+}
 
-  // Extract the tuple part
-  for(int i = 1; i < len(args); ++i) {
-    object curArg = args[i];
-    if(curArg) {
-      buffer << extract<const char *>(boost::python::str(args[i])) << std::endl;
-    }
-  }
-  const char* label = extract<const char *>(boost::python::str(args[0]));
-
-  Logger(std::string(label)) << buffer.str();
-
-  return object();
+/**
+ * Prints the arguments from the python script using the Kratos Logger class
+ * @args: tuple boost::python::object representing the arguments of the function The first argument is the label
+ * @kwargs: dictionary of boost::python::objects resenting key-value pairs for
+ * name arguments
+ **/
+object printWarning(tuple args, dict kwargs) {
+    return printImpl(args, kwargs, Logger::Severity::WARNING);
 }
 
 void  AddLoggerToPython() {
 	using namespace boost::python;
 
-  class_<Logger, boost::shared_ptr<Logger>, boost::noncopyable>("Logger", no_init)
-  .def("Print", raw_function(print,1))
-  .staticmethod("Print");
+    class_<Logger, boost::shared_ptr<Logger>, boost::noncopyable>("Logger", init<std::string const &>())
+    .def("PrintInfo", raw_function(printInfo,1))
+    .def("PrintWarning", raw_function(printWarning,1))
+    ;
 }
 
 }  // namespace Python.
