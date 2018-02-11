678781fec4fc024f81b92a6e0db073703d31dd71
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-02-11 22:37:01 +0100

    Logger mass process

--- applications/StructuralMechanicsApplication/custom_processes/total_structural_mass_process.cpp
+++ applications/StructuralMechanicsApplication/custom_processes/total_structural_mass_process.cpp
@@ -56,7 +56,7 @@ void TotalStructuralMassProcess::Execute()
         }
     }
     
-    std::cout << "The total mass of the system is :" << total_mass/1000.0 << " Tn\n" << "Check variable NODAL_MASS in the process info in order to access to it in any moment" << std::endl;
+    KRATOS_INFO("TOTAL MASS") << "The total mass of the system is :" << total_mass/1000.0 << " Tn\n" << "Check variable NODAL_MASS in the process info in order to access to it in any moment" << std::endl;
     
     mrThisModelPart.GetProcessInfo()[NODAL_MASS] = total_mass;
     
