6a18ddc26f1d6cbbe0da25abbfe5fcba9c12f29c
jcotela <jcotela@cimne.upc.edu>
2018-02-16 11:33:42 +0100

    last MKL in fluid dynamics.

--- applications/FluidDynamicsApplication/python_scripts/monolithic_solver_bdf2.py
+++ applications/FluidDynamicsApplication/python_scripts/monolithic_solver_bdf2.py
@@ -67,11 +67,6 @@ class MonolithicSolver:
 
         self.alpha = -0.3
         self.move_mesh_strategy = 0
-# self.time_scheme = ResidualBasedPredictorCorrectorVelocityBossakScheme( self.alpha,self.move_mesh_strategy )
-        # definition of the solvers
-# self.linear_solver =  SkylineLUFactorizationSolver()
-# self.linear_solver =SuperLUSolver()
-# self.linear_solver = MKLPardisoSolver()
 
         pPrecond = DiagonalPreconditioner()
 # pPrecond = ILU0Preconditioner()
