ab67fe226665851c3e6d071131de77738f756df3
philbucher <philipp.bucher@tum.de>
2018-02-16 09:41:33 +0100

    updating the compilation scripts

--- scripts/build/appveyor/configure_appveyor_VS2017.bat
+++ scripts/build/appveyor/configure_appveyor_VS2017.bat
@@ -10,11 +10,9 @@ cmake .. -G "Visual Studio 15 2017 Win64"                                       
 -DFLUID_DYNAMICS_APPLICATION=ON                                                 ^
 -DSTRUCTURAL_MECHANICS_APPLICATION=ON                                           ^
 -DSWIMMING_DEM_APPLICATION=OFF                                                  ^
--DMKLSOLVER_INCLUDE_DIR="UNSET"                                                 ^
--DMKLSOLVER_LIB_DIR="UNSET"                                                     ^
 -DMETIS_APPLICATION=OFF                                                         ^
 -DPARMETIS_ROOT_DIR="UNSET"                                                     ^
 -DTRILINOS_APPLICATION=OFF                                                      ^
 -DTRILINOS_ROOT="UNSET"                                                         ^
 -DINSTALL_EMBEDDED_PYTHON=ON                                                    ^
--DINCLUDE_FEAST=OFF                                                              
+-DINCLUDE_FEAST=OFF
