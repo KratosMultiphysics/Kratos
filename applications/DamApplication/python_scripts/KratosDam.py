092549a47cb3dad53cd36242f72bc52ea6e625aa
Unknown <jirazabal@cimne.upc.edu>
2018-02-20 17:02:38 +0100

    Undoing some changes

--- applications/DamApplication/python_scripts/KratosDam.py
+++ applications/DamApplication/python_scripts/KratosDam.py
@@ -1,4 +1,9 @@
 import KratosMultiphysics
+import KratosMultiphysics.ExternalSolversApplication as KratosSolvers
+import KratosMultiphysics.ConvectionDiffusionApplication as KratosConvDiff
+import KratosMultiphysics.SolidMechanicsApplication as KratosSolid
+import KratosMultiphysics.PoromechanicsApplication as KratosPoro
+import KratosMultiphysics.DamApplication as KratosDam
 
 import dam_main as Main
 
