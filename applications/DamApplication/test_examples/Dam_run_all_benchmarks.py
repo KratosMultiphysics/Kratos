2bbd1eda61dacaa3937a3fe872071ef4674d2de2
Unknown <jirazabal@cimne.upc.edu>
2018-02-20 11:50:58 +0100

    Improving benchmark files

--- applications/DamApplication/test_examples/Dam_run_all_benchmarks.py
+++ applications/DamApplication/test_examples/Dam_run_all_benchmarks.py
@@ -5,8 +5,6 @@ import sys
 import platform
 import smtplib
 
-kratos_benchmarking_path = '../../../benchmarking'
-sys.path.append(kratos_benchmarking_path)
 path = '../test_examples'
 sys.path.append(path)
 
@@ -31,7 +29,7 @@ Benchmark_text = ["Running Dam Benchmark 2D 01... Mechanical-PlaneStrain with hy
                   "Running Dam Benchmark 3D 03... ThermoMechanical with hydrostatic pressure and imposed temperature\n",\
                   "Running Dam Benchmark 3D 04... ThermoMechanical with hydrostatic pressure and Bofang\n",\
                   "Running Dam Benchmark 3D 05... ThermoMechanical with heat flux\n",\
-                  "Running Dam Benchmark 3D 06... ThermoMechanical with imposed temperature\n"
+                  "Running Dam Benchmark 3D 06... ThermoMechanical with imposed temperature\n"]
 
 def Run():
 
@@ -42,90 +40,67 @@ def Run():
         g.write("\n========== DAM BENCHMARKING RESULTS ==========\n\n")
     Text = ""
 
-    #list_of_failed_tests = []
+    Benchmarks_list_2D = list(range(201,213))
+    Benchmarks_list_3D = list(range(301,307))
 
-    #Discontinuum Tests. From 1 to 17
-    D_DEM_Benchmarks_list = list(range(1,18))
-    #Continuum Tests
-    C_DEM_Benchmarks_list = list(range(20,26))
-    #Discontinuum Clusters Tests. From 30 to 33
-    Dcl_DEM_Benchmarks_list = list(range(30,34))
-
-    Total_DEM_Benchmarks_list = D_DEM_Benchmarks_list + C_DEM_Benchmarks_list + Dcl_DEM_Benchmarks_list
+    Total_Dam_Benchmarks_list = Benchmarks_list_2D + Benchmarks_list_3D
 
     failure = False
 
     with open("BenchTemp.info", "w") as f:
-        for benchmark in Total_DEM_Benchmarks_list:
-            print(Benchmark_text[benchmark - 1])
+        i = 0
+        for benchmark in Total_Dam_Benchmarks_list:
+            print(Benchmark_text[i])
+            i += 1
             try:
                 if platform.system()=="Windows":
                     os.system("setenv OMP_NUM_THREADS 1") # Is that the correct way to run on Windows?
-                    subprocess.check_call(["python", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.info"], stdout=f, stderr=f)
+                    subprocess.check_call(["python", path + "/Dam_benchmarks.py", str(benchmark), ">", "BenchTemp.info"], stdout=f, stderr=f)
                     os.system("setenv OMP_NUM_THREADS ") # Trying to set a 'default' value
 
                 else:
                     os.environ['OMP_NUM_THREADS']='1'
                     if sys.version_info >= (3, 0):
-                        subprocess.check_call(["python3", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.info"], stdout=f, stderr=f)
+                        subprocess.check_call(["python3", path + "/Dam_benchmarks.py", str(benchmark), ">", "BenchTemp.info"], stdout=f, stderr=f)
 
                     else:
-                        subprocess.check_call(["python", "-3", path + "/DEM_benchmarks.py", str(benchmark), ">", "BenchTemp.info"], stdout=f, stderr=f)
+                        subprocess.check_call(["python", "-3", path + "/Dam_benchmarks.py", str(benchmark), ">", "BenchTemp.info"], stdout=f, stderr=f)
                     os.system("export OMP_NUM_THREADS=") # Trying to set a 'default' value
             except:
                 #failure = True
                 #list_of_failed_tests += [benchmark]
                 print("A problem was found in DEM Benchmark " + str(benchmark) + "... Resuming...\n")
                 with open("errors.err", "a") as g:
-                    if benchmark == 10:
-                        g.write("\n===== THORNTON PAPER TESTS. FULL REGIME. LINEAR LAW =====\n\n")
-                    if benchmark == 11:
-                        g.write("\n===== THORNTON PAPER TESTS. FULL REGIME. HERTZIAN LAW ===\n\n")
-                    if benchmark == 12:
-                        g.write("\n===== WENSRICH PAPER TEST. ROLLING FRICTION =============\n\n")
-                    if benchmark == 13:
-                        g.write("\n===== DE/FE CONTACT BENCHMARKS ==========================\n\n")
-                    if benchmark == 20:
-                        g.write("\n===== BASIC CONTINUUM TESTS  ============================\n\n")
-                    if benchmark == 30:
-                        g.write("\n===== DISCONTINUUM CLUSTERS TESTS  ======================\n\n")
+                    if benchmark == 201:
+                        g.write("\n===================== 2D BENCHMARKS =====================\n\n")
+                    if benchmark == 301:
+                        g.write("\n===================== 3D BENCHMARKS =====================\n\n")
                     g.write("DEM Benchmark " + str(benchmark) + ": KO!........ Test " + str(benchmark) + " FAILED\n")
         print('\n')
-
 
     with open("errors.err", "a") as g:
         g.write("\n---------------------------------------------------------------------\n")
         g.write("\nList of Benchmarks:\n")
-        g.write("\nDISCONTINUUM TESTS:\n")
-        g.write("Benchmark 01. Elastic normal impact of two identical spheres\n")
-        g.write("Benchmark 02. Elastic normal impact of a sphere against a rigid plane\n")
-        g.write("Benchmark 03. Impact of a sphere against a rigid plane with different coefficients of restitution\n")
-        g.write("Benchmark 04. Oblique impact of a sphere with a rigid plane with constant velocity module and variable incident angles\n")
-        g.write("Benchmark 05. Oblique impact of a sphere with a rigid plane with constant normal velocity and different tangential velocities\n")
-        g.write("Benchmark 06. Impact of a sphere with a rigid plane with a constant normal velocity and variable angular velocities\n")
-        g.write("Benchmark 07. Impact of two identical spheres with a constant normal velocity and different angular velocities\n")
-        g.write("Benchmark 08. Impact of two differently sized spheres with a constant normal velocity and variable angular velocities\n")
-        g.write("Benchmark 09. Impact of two identical spheres with a constant normal velocity and different coefficients of restitution\n")
-        g.write("Benchmark 10. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
-        g.write("Benchmark 11. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
-        g.write("Benchmark 12. Sphere rotating over a plane surface with Rolling Friction\n")
-        g.write("Benchmark 13. Impact of a low stiffness sphere against a rigid plane divided in small triangular elements\n")
-        g.write("Benchmark 14. Impact of a low stiffness sphere against a rigid edge divided in small triangular elements\n")
-        g.write("Benchmark 15. Impact of a low stiffness sphere against a rigid vertex divided in small triangular elements\n")
-        g.write("Benchmark 16. Spheres contacting multiple entities (facets, edges and vertices)\n")
-        g.write("Benchmark 17. Sphere sliding on a plane (discretized with triangles and quadrilaterals) with friction\n")
-        g.write("\nCONTINUUM TESTS:\n")
-        g.write("Benchmark 20. Normal compression of two identical spheres\n")
-        g.write("Benchmark 21. Normal compression of two identical indented spheres\n")
-        g.write("Benchmark 22. Tensile test of two identical spheres\n")
-        g.write("Benchmark 23. Tensile test of two identical indented spheres\n")
-        g.write("Benchmark 24. Shear test of two identical spheres by applying rotation\n")
-        g.write("Benchmark 25. Shear test of two identical spheres by applying rotation and radius expansion\n")
-        g.write("\nDISCONTINUUM CLUSTERS TESTS:\n")
-        g.write("Benchmark 30. Cylinder cluster with imposed angular velocity in two axis (Velocity Verlet + Zhao scheme)\n")
-        g.write("Benchmark 31. Cylinder cluster with imposed angular velocity in two axis (Symplectic Euler + Runge-Kutta scheme)\n")
-        g.write("Benchmark 32. Fiber cluster bouncing without any damping (Velocity Verlet + Zhao scheme)\n")
-        g.write("Benchmark 33. Fiber cluster bouncing without any damping (Symplectic Euler + Runge-Kutta scheme)\n")
+        g.write("\nTWO-DIMENSIONAL TESTS:\n")
+        g.write("Dam Benchmark 2D 01: Mechanical-PlaneStrain with hydrostatic pressure\n")
+        g.write("Dam Benchmark 2D 02: Mechanical-PlaneStress with hydrostatic pressure\n")
+        g.write("Dam Benchmark 2D 03: ThermoMechanical-PlaneStrain with hydrostatic pressure and heat flux\n")
+        g.write("Dam Benchmark 2D 04: ThermoMechanical-PlaneStress with hydrostatic pressure and heat flux\n")
+        g.write("Dam Benchmark 2D 05: ThermoMechanical-PlaneStrain with hydrostatic pressure and imposed temperature\n")
+        g.write("Dam Benchmark 2D 06: ThermoMechanical-PlaneStress with hydrostatic pressure and imposed temperature\n")
+        g.write("Dam Benchmark 2D 07: ThermoMechanical-PlaneStrain with hydrostatic pressure and Bofang\n")
+        g.write("Dam Benchmark 2D 08: ThermoMechanical-PlaneStress with hydrostatic pressure and Bofang\n")
+        g.write("Dam Benchmark 2D 09: ThermoMechanical-PlaneStrain with heat flux\n")
+        g.write("Dam Benchmark 2D 10: ThermoMechanical-PlaneStress with heat flux\n")
+        g.write("Dam Benchmark 2D 11: ThermoMechanical-PlaneStrain with imposed temperature\n")
+        g.write("Dam Benchmark 2D 12: ThermoMechanical-PlaneStress with imposed temperature\n")
+        g.write("\nTHREE-DIMENSIONAL TESTS:\n")
+        g.write("Dam Benchmark 3D 01: Mechanical-PlaneStrain with hydrostatic pressure\n")
+        g.write("Dam Benchmark 3D 02: ThermoMechanical with hydrostatic pressure and heat flux\n")
+        g.write("Dam Benchmark 3D 03: ThermoMechanical with hydrostatic pressure and imposed temperature\n")
+        g.write("Dam Benchmark 3D 04: ThermoMechanical with hydrostatic pressure and Bofang\n")
+        g.write("Dam Benchmark 3D 05: ThermoMechanical with heat flux\n")
+        g.write("Dam Benchmark 3D 06: ThermoMechanical with imposed temperature\n")
 
     with open("errors.err") as g:
         file_contents = g.read()
@@ -136,22 +111,8 @@ def Run():
 
     Text += file_contents.rstrip("\n")
     Text += "\n\n\n"
-
-    # To send an email summary to the DEM Team
-    #list_of_failed_tests = ', '.join(str(e) for e in list_of_failed_tests)
-    #subject = "DEM Benchmarks Results. Problems found in tests number "
-    #subject += list_of_failed_tests
-    recipients = ["latorre@cimne.upc.edu"]
-    recipients += ["maceli@cimne.upc.edu", "gcasas@cimne.upc.edu", "farrufat@cimne.upc.edu", "jirazabal@cimne.upc.edu"]
-    subject = "Problems found in DEM Benchmarks"
-    message = "From: Kratos Benchmarking <no-reply-kratos-benchmarking@cimne.upc.es>\nSubject: " + subject + "\n" + Text
-
-    #if failure:
-    #if (__name__ != '__main__' and failure):
-    if (False):
-        smtplib.SMTP("smtps.cimne.upc.es").sendmail("Kratos Benchmarking <no-reply-kratos-benchmarking@cimne.upc.es>", recipients, message)
+    
     return Text
-
 
 if __name__ == '__main__':
     print(Run())
--- applications/DamApplication/test_examples/basic_benchmarks/Dam_benchmarks.py
+++ applications/DamApplication/test_examples/basic_benchmarks/Dam_benchmarks.py
@@ -1,25 +1,38 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Time monitoring
 import time as timer
+print(timer.ctime())
+initial_time = timer.perf_counter()
+
 import os
 import sys
 
-# Kratos
-from KratosMultiphysics import *
-from KratosMultiphysics.DEMApplication import *
+# Import kratos core and applications
+import KratosMultiphysics
+import KratosMultiphysics.ExternalSolversApplication as KratosSolvers
+import KratosMultiphysics.ConvectionDiffusionApplication as KratosConvDiff
+import KratosMultiphysics.SolidMechanicsApplication as KratosSolid
+import KratosMultiphysics.PoromechanicsApplication as KratosPoro
+import KratosMultiphysics.DamApplication as KratosDam
 
 import dam_main
-import plot_variables                # Related to benchmarks in Chung, Ooi
-import Dam_benchmarks_class as DBC   # Related to benchmarks in Chung, Ooi
+import Dam_benchmarks_class as DamBenchmarksClass   # Related to benchmarks in Chung, Ooi
 
 sys.path.insert(0,'')
 start = timer.time()
 benchmark_number = int(sys.argv[1])
-benchmark = getattr(DBC, 'Benchmark' + str(benchmark_number))()
+benchmark = getattr(DamBenchmarksClass, 'Benchmark' + str(benchmark_number))()
 
 list2D   = list(range(201,213))
 list3D   = list(range(301,307))
 
-class Solution(main_script.Solution):
+class Solution(dam_main.Solution):
+
+    def __init__(self):
+        super(Solution, self).__init__()
+        self.main_path = os.getcwd()
+        benchmark.set_initial_data()
 
     def LoadParametersFile(self):
         file_name = None
@@ -64,128 +77,18 @@ class Solution(main_script.Solution):
             sys.exit()
 
         with open(file_name, 'r') as parameters_file:
-            self.DEM_parameters = Parameters(parameters_file.read())
-
-    def __init__(self):
-        super(Solution, self).__init__()
-        self.nodeplotter = False
-        self.LoadParametersFile()
-        self.main_path = os.getcwd()
-
-    def GetProblemTypeFilename(self):
-        return benchmark
-
-    def model_part_reader(self, modelpart, nodeid=0, elemid=0, condid=0):
-        return ModelPartIO(modelpart)
-
-    def SetSolverStrategy(self):
-        # Strategy object
-        element_type = self.DEM_parameters["ElementType"].GetString()
-        if (element_type == "SphericPartDEMElement3D" or element_type == "CylinderPartDEMElement2D"):
-            import sphere_strategy as SolverStrategy
-        elif (element_type == "SphericContPartDEMElement3D" or element_type == "CylinderContPartDEMElement2D"):
-            import continuum_sphere_strategy as SolverStrategy
-        elif (element_type == "ThermalSphericContPartDEMElement3D"):
-            import thermal_continuum_sphere_strategy as SolverStrategy
-        elif (element_type == "ThermalSphericPartDEMElement3D"):
-            import thermal_sphere_strategy as SolverStrategy
-        elif (element_type == "SinteringSphericConPartDEMElement3D"):
-            import thermal_continuum_sphere_strategy as SolverStrategy
-        elif (element_type == "IceContPartDEMElement3D"):
-            import ice_continuum_sphere_strategy as SolverStrategy
-        else:
-            self.KRATOSprint('Error: Strategy unavailable. Select a different scheme-element')
-
-        return SolverStrategy
-
-    def SetSolver(self):
-        return self.solver_strategy.ExplicitStrategy(self.all_model_parts,
-                                                     self.creator_destructor,
-                                                     self.dem_fem_search,
-                                                     self.DEM_parameters,
-                                                     self.procedures)
-
-    def SetFinalTime(self):
-        self.final_time = final_time
-
-    def Setdt(self):
-        self.dt = dt
-
+            self.ProjectParameters = KratosMultiphysics.Parameters(parameters_file.read())
+       
     def Initialize(self):
-        self.DEM_parameters["problem_name"].SetString('benchmark' + str(benchmark_number))
-        #self.final_time = slt.final_time
-        #self.dt = slt.dt
-        #self.graph_print_interval = slt.graph_print_interval
         super(Solution, self).Initialize()
+        benchmark.set_initial_data()
 
-        print("Computing points in the curve...", 1 + self.number_of_points_in_the_graphic - self.iteration, "point(s) left to finish....",'\n')
-        list_of_nodes_ids = [1]
-        if self.nodeplotter:
-            os.chdir(self.main_path)
-            self.plotter = plot_variables.variable_plotter(self.spheres_model_part, list_of_nodes_ids, benchmark_number)
-            self.tang_plotter = plot_variables.tangential_force_plotter(self.spheres_model_part, list_of_nodes_ids, self.iteration)
+    def PrintOutput(self):
+        super(Solution, self).PrintOutput()
+        benchmark.generate_graph_points(self.main_model_part, self.time)
 
-    def ReadModelParts(self):
-        super(Solution, self).ReadModelParts()
-        benchmark.set_initial_data(self.spheres_model_part, self.rigid_face_model_part, self.iteration, self.number_of_points_in_the_graphic, coeff_of_restitution_iteration)
-
-    def GetMpFilename(self):
-        return 'benchmark' + str(benchmark_number) + "DEM"
-
-    def GetInletFilename(self):
-        return 'benchmarkDEM_Inlet'
-        #return 'benchmark' + str(benchmark_number) + "DEM_Inlet"
-
-    def GetFemFilename(self):
-        return 'benchmark' + str(benchmark_number) + "DEM_FEM_boundary"
-
-    def GetClusterFilename(self):
-        return 'benchmark' + str(benchmark_number) + "DEM_Clusters"
-
-    def GetProblemTypeFilename(self):
-        return 'benchmark' + str(benchmark_number)
-
-    def BeforeSolveOperations(self, time):
-        super(Solution, self).BeforeSolveOperations(time)
-        benchmark.ApplyNodalRotation(time, self.dt, self.spheres_model_part)
-
-    def BeforePrintingOperations(self, time):
-        super(Solution, self).BeforePrintingOperations(time)
-        self.Setdt()
-        benchmark.generate_graph_points(self.spheres_model_part, self.rigid_face_model_part, self.cluster_model_part, time, self.graph_print_interval, self.dt)
-
-    def Finalize(self):
-        benchmark.get_final_data(self.spheres_model_part, self.rigid_face_model_part, self.cluster_model_part)
-        super(Solution, self).Finalize()
-        if self.nodeplotter:
-            os.chdir(self.main_path)
-            self.plotter.close_files()
-            self.tang_plotter.close_files()
-
-    def FinalizeTimeStep(self, time):
-        super(Solution, self).FinalizeTimeStep(time)
-        if self.nodeplotter:
-            os.chdir(self.main_path)
-            self.plotter.plot_variables(time) #Related to the benchmark in Chung, Ooi
-            self.tang_plotter.plot_tangential_force(time)
-
-    def CleanUpOperations(self):
-        print("running CleanUpOperations")
-        #DBC.delete_archives() #.......Removing some unuseful files
-        super(Solution, self).CleanUpOperations()
-
-
-final_time, dt, graph_print_interval, number_of_points_in_the_graphic, number_of_coeffs_of_restitution = DBC.initialize_time_parameters(benchmark_number)
-for coeff_of_restitution_iteration in range(1, number_of_coeffs_of_restitution + 1):
-    for iteration in range(1, number_of_points_in_the_graphic + 1):
-        slt = Solution()
-        slt.iteration = iteration
-        slt.dt = dt
-        slt.final_time = final_time
-        slt.graph_print_interval = graph_print_interval
-        slt.number_of_points_in_the_graphic = number_of_points_in_the_graphic
-        slt.number_of_coeffs_of_restitution = number_of_coeffs_of_restitution
-        slt.Run()
-    end = timer.time()
-    benchmark.print_results(number_of_points_in_the_graphic, dt, elapsed_time = end - start)
-#DBC.delete_archives() #.......Removing some unuseful files
+solution = Solution()
+solution.Run()
+benchmark.print_results()
+benchmark.finalize_data()
+DamBenchmarksClass.delete_archives() #.......Removing some unuseful files
