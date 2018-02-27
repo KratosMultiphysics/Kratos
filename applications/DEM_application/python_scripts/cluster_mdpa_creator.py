318505c663ca99f44cf2dc4fd9121def6cabbcdf
Unknown <jirazabal@cimne.upc.edu>
2018-02-27 16:42:18 +0100

    Fixing problem when reading files with different separators
    Fixing problem when reading files with more than one space between data

--- applications/DEM_application/python_scripts/cluster_mdpa_creator.py
+++ applications/DEM_application/python_scripts/cluster_mdpa_creator.py
@@ -18,7 +18,7 @@ def WriteClusterMdpa():
         ClusterMdpa.write(Line)
         if 'CLUSTER_FILE_NAME' in Line:
             Line = Line.strip('\n')# Remove the line-ending characters
-            ClusterAvailableTypes.append(Line.split(' ')[1])
+            ClusterAvailableTypes.append(Line.split()[1])
 
     ClusterType = []
     NodeNumber  = []
@@ -32,25 +32,25 @@ def WriteClusterMdpa():
     qW          = []
 
     for Line in ClusterMeshInfo:
-        Line = Line.lstrip()# Remove the line-beginning characters
+        Line = Line.lstrip() # Remove the line-beginning characters
 
         if Line.startswith("Begin_Cluster"):
             Line = Line.strip('\n')# Remove the line-ending characters
             for i in range(len(ClusterAvailableTypes)):
-                if Line.split(' ')[1] + ".clu" == ClusterAvailableTypes[i]:
+                if Line.split()[1] + ".clu" == ClusterAvailableTypes[i]:
                     ClusterType.append(i+1)
 
         if Line[0].isdigit():
             Line = Line.strip('\n')# Remove the line-ending characters
-            NodeNumber.append(Line.split(' ')[0])
-            NodeX.append(Line.split(' ')[1])
-            NodeY.append(Line.split(' ')[2])
-            NodeZ.append(Line.split(' ')[3])
-            ChLength.append(Line.split(' ')[4])
-            qX.append(Line.split(' ')[5])
-            qY.append(Line.split(' ')[6])
-            qZ.append(Line.split(' ')[7])
-            qW.append(Line.split(' ')[8])
+            NodeNumber.append(Line.split()[0])
+            NodeX.append(Line.split()[1])
+            NodeY.append(Line.split()[2])
+            NodeZ.append(Line.split()[3])
+            ChLength.append(Line.split()[4])
+            qX.append(Line.split()[5])
+            qY.append(Line.split()[6])
+            qZ.append(Line.split()[7])
+            qW.append(Line.split()[8])
 
     ClusterMdpa.write('\n\nBegin Nodes\n')
 
