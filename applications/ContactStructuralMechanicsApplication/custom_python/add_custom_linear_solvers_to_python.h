f9a1796f3090c45d066e066f191908a4a3a6d88f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-03-04 22:14:28 +0100

    Adding MixedULMSolver

---
+++ applications/ContactStructuralMechanicsApplication/custom_python/add_custom_linear_solvers_to_python.h
@@ -0,0 +1,34 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:             BSD License
+//                                       license: StructuralMechanicsApplication/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+#if !defined(KRATOS_ADD_CUSTOM_LINEAR_SOLVERS_TO_PYTHON_H_INCLUDED )
+#define  KRATOS_ADD_CUSTOM_LINEAR_SOLVERS_TO_PYTHON_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+void  AddCustomLinearSolversToPython();
+
+}  // namespace Python.
+
+}  // namespace Kratos.
+
+#endif // KRATOS_ADD_CUSTOM_LINEAR_SOLVERS_TO_PYTHON_H_INCLUDED  defined 
