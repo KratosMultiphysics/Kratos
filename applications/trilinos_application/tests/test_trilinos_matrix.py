08c3c383730b0e65bc02b077650c6c7e1ad93606
Riccardo Rossi <rrossi@cimne.upc.edu>
2018-03-13 17:23:26 +0100

    adding test

--- applications/trilinos_application/tests/test_trilinos_matrix.py
+++ applications/trilinos_application/tests/test_trilinos_matrix.py
@@ -6,22 +6,17 @@ import KratosMultiphysics.mpi as KratosMPI
 import KratosMultiphysics.TrilinosApplication as KratosTrilinos
 
 
-class TestMPICommunicator(KratosUnittest.TestCase):
+class TestTrilinosMatrix(KratosUnittest.TestCase):
 
     def test_resize(self):
         comm = KratosMultiphysics.TrilinosApplication.CreateCommunicator()
         space = KratosMultiphysics.TrilinosApplication.TrilinosSparseSpace()
         
-        print(dir(space))
-        print(space)
         pb = space.CreateEmptyVectorPointer(comm)
-        print("000")
         space.ResizeVector(pb,2)
-        print("111")
-        rb = pb.GetReference()
-        print("222")
-        n = space.Size(rb)
-        self.assertEqual(n,0)
+
+        n = space.Size(pb.GetReference())
+        self.assertEqual(n,2)
         
 if __name__ == '__main__':
     KratosUnittest.main()
