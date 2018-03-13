9a1bbcaa3527ca5385314134dfb0ed4f6c38ebf5
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-03-13 13:54:52 +0100

    Correcting virtual small_displacement_bbar

--- applications/StructuralMechanicsApplication/custom_elements/small_displacement_bbar.h
+++ applications/StructuralMechanicsApplication/custom_elements/small_displacement_bbar.h
@@ -328,9 +328,9 @@ public:
 
         // A private default constructor necessary for serialization
 
-        virtual void save(Serializer& rSerializer) const override;
+        void save(Serializer& rSerializer) const override;
 
-        virtual void load(Serializer& rSerializer) override;
+        void load(Serializer& rSerializer) override;
 
         ///@name Private Inquiry
         ///@{
