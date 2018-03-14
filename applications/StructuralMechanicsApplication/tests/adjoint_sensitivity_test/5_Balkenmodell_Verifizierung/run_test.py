4e3e36d356bbe02bd89998a5db0457f2b50db715
MFusseder <martin.fusseder@tum.de>
2018-03-14 16:46:23 +0100

    modified test example for sensitivity analysis of beam structures

---
+++ applications/StructuralMechanicsApplication/tests/adjoint_sensitivity_test/5_Balkenmodell_Verifizierung/run_test.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+import os
+os.environ['OMP_NUM_THREADS'] = "1"
+#import kratos core and applications
+from KratosMultiphysics import *
+from KratosMultiphysics.StructuralMechanicsApplication import *
+import structural_mechanics_analysis
+
+# Create the primal solver
+with open("beam_test_parameters.json",'r') as parameter_file:
+    ProjectParametersPrimal = Parameters( parameter_file.read())
+primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(ProjectParametersPrimal)
+
+# Create the adjoint solver
+with open("beam_test_adjoint_parameters.json",'r') as parameter_file:
+    ProjectParametersAdjoint = Parameters( parameter_file.read())
+adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(ProjectParametersAdjoint)
+
+primal_analysis.Run()
+adjoint_analysis.Run()
