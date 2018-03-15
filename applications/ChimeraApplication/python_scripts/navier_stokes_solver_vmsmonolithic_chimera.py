14cdc86179e4cfc2a5a0c39f364c544576ab04fc
navaneethkn29@gmail.com <navaneethkn29@gmail.com>
2018-03-15 15:45:34 +0100

    Corrected chimera_solver.py according to updated master

--- applications/ChimeraApplication/python_scripts/navier_stokes_solver_vmsmonolithic_chimera.py
+++ applications/ChimeraApplication/python_scripts/navier_stokes_solver_vmsmonolithic_chimera.py
@@ -1,23 +1,29 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-# importing the Kratos Library
-import KratosMultiphysics
-import KratosMultiphysics.FluidDynamicsApplication as KratosCFD
-import KratosMultiphysics.ChimeraApplication as KratosChimera
-## Check that KratosMultiphysics was imported in the main script
-KratosMultiphysics.CheckForPreviousImport()
 
-## Import base class file
+# Importing the Kratos Library
+import KratosMultiphysics
+import KratosMultiphysics.ChimeraApplication as KratosChimera
+
+# Check that applications were imported in the main script
+KratosMultiphysics.CheckRegisteredApplications("FluidDynamicsApplication")
+
+# Import applications
+import KratosMultiphysics.FluidDynamicsApplication as KratosCFD
+
+# Import base class file
 import navier_stokes_base_solver
 
 def CreateSolver(main_model_part, custom_settings):
-    return NavierStokesSolver_VMSMonolithic_Chimera(main_model_part, custom_settings)
+    return NavierStokesSolverMonolithicChimera(main_model_part, custom_settings)
 
-class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierStokesBaseSolver):
+class NavierStokesSolverMonolithicChimera(navier_stokes_base_solver.NavierStokesBaseSolver):
 
     def __init__(self, main_model_part, custom_settings):
+        
         self.element_name = "VMS"
         self.condition_name = "MonolithicWallCondition"
         self.min_buffer_size = 2
+
         #TODO: shall obtain the compute_model_part from the MODEL once the object is implemented
         self.main_model_part = main_model_part
 
@@ -30,34 +36,19 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
                 "input_filename": "unknown_name"
             },
             "implementation"		  : "MPC",
-            
             "maximum_iterations": 10,
-            "dynamic_tau": 0.0,
+            "dynamic_tau": 0.01,
             "oss_switch": 0,
             "echo_level": 0,
             "consider_periodic_conditions": false,
             "compute_reactions": false,
-            "divergence_clearance_steps": 0,
             "reform_dofs_at_each_step": true,
             "relative_velocity_tolerance": 1e-3,
             "absolute_velocity_tolerance": 1e-5,
             "relative_pressure_tolerance": 1e-3,
             "absolute_pressure_tolerance": 1e-5,
-            "rotating_regions_used"        : true,
             "linear_solver_settings"        : {
-                    "solver_type" : "AMGCL",
-                    "smoother_type":"ilu0",
-                    "krylov_type": "gmres",
-                    "coarsening_type": "aggregation",
-                    "max_iteration": 200,
-                    "provide_coordinates": false,
-                    "gmres_krylov_space_dimension": 100,
-                    "verbosity" : 0,
-                    "tolerance": 1e-7,
-                    "scaling": false,
-                    "block_size": 1,
-                    "use_block_matrices_if_possible" : true,
-                    "coarse_enough" : 5000
+                "solver_type" : "AMGCL_NS_Solver"
             },
             "volume_model_part_name" : "volume_model_part",
             "skin_parts": [""],
@@ -73,8 +64,7 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
             "periodic": "periodic",
             "move_mesh_flag": false,
             "turbulence_model": "None",
-            "use_spalart_allmaras": false,
-            "reorder": false  
+            "reorder": false
         }""")
 
         ## Overwrite the default settings with user-provided parameters
@@ -85,31 +75,11 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
         import linear_solver_factory
         self.linear_solver = linear_solver_factory.ConstructSolver(self.settings["linear_solver_settings"])
 
-        ## Set the element replace settings
-        self.settings.AddEmptyValue("element_replace_settings")
-        if(self.main_model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE] == 3):
-            self.settings["element_replace_settings"] = KratosMultiphysics.Parameters("""
-                {
-                "element_name":"VMS3D4N",
-                "condition_name": "MonolithicWallCondition3D"
-                }
-                """)
-        elif(self.main_model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE] == 2):
-            self.settings["element_replace_settings"] = KratosMultiphysics.Parameters("""
-                {
-                "element_name":"VMS2D3N",
-                "condition_name": "MonolithicWallCondition2D"
-                }
-                """)
-        else:
-            raise Exception("domain size is not 2 or 3")
-
-        print("Construction of NavierStokesSolver_VMSMonolithic_Chimera finished")
+        KratosMultiphysics.Logger.PrintInfo("NavierStokesSolverMonolithicChimera", "Construction of NavierStokesSolverMonolithicChimera finished.")
 
 
     def AddVariables(self):
         ## Add base class variables
-        #super(NavierStokesSolver_VMSMonolithic_Chimera, self).AddVariables()
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VELOCITY)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.ACCELERATION)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.MESH_VELOCITY)
@@ -129,8 +99,17 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NORMAL)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.Y_WALL)
         self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.Q_VALUE)
+        if self.settings["consider_periodic_conditions"].GetBool() == True:
+            self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.PATCH_INDEX)
 
-        print("Monolithic fluid solver variables added correctly")
+        KratosMultiphysics.Logger.PrintInfo("NavierStokesSolverMonolithicChimera", "Fluid solver variables added correctly.")
+
+
+    def ImportModelPart(self):
+        super(NavierStokesSolverMonolithicChimera, self).ImportModelPart()
+        
+        ## Sets DENSITY, VISCOSITY and SOUND_VELOCITY
+        self._set_physical_properties()
 
 
     def Initialize(self):
@@ -139,13 +118,15 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
 
         # If needed, create the estimate time step utility
         if (self.settings["time_stepping"]["automatic_time_step"].GetBool()):
-            self.EstimateDeltaTimeUtility = self._GetAutomaticTimeSteppingUtility()
+            self.EstimateDeltaTimeUtility = self._get_automatic_time_stepping_utility()
 
         # Creating the solution strategy
         self.conv_criteria = KratosCFD.VelPrCriteria(self.settings["relative_velocity_tolerance"].GetDouble(),
                                                      self.settings["absolute_velocity_tolerance"].GetDouble(),
                                                      self.settings["relative_pressure_tolerance"].GetDouble(),
                                                      self.settings["absolute_pressure_tolerance"].GetDouble())
+
+        (self.conv_criteria).SetEchoLevel(self.settings["echo_level"].GetInt())
 
         if (self.settings["turbulence_model"].GetString() == "None"):
             if self.settings["consider_periodic_conditions"].GetBool() == True:
@@ -164,17 +145,14 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
             builder_and_solver = KratosCFD.ResidualBasedBlockBuilderAndSolverPeriodic(self.linear_solver,
                                                                                 KratosCFD.PATCH_INDEX)
         else:
-
             if (self.settings["implementation"].GetString() == "MPC"):
                 
-
                 builder_and_solver = KratosChimera.ResidualBasedBlockBuilderAndSolverWithMpcChimera(self.linear_solver)
 
             else:
                 builder_and_solver = KratosMultiphysics.ResidualBasedBlockBuilderAndSolver(self.linear_solver)
 
-
-            self.solver = KratosMultiphysics.ResidualBasedNewtonRaphsonStrategy(self.main_model_part,
+        self.solver = KratosMultiphysics.ResidualBasedNewtonRaphsonStrategy(self.main_model_part,
                                                                             self.time_scheme,
                                                                             self.linear_solver,
                                                                             self.conv_criteria,
@@ -190,75 +168,26 @@ class NavierStokesSolver_VMSMonolithic_Chimera(navier_stokes_base_solver.NavierS
         self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DYNAMIC_TAU, self.settings["dynamic_tau"].GetDouble())
         self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.OSS_SWITCH, self.settings["oss_switch"].GetInt())
 
-        print ("Monolithic solver initialization finished.")
-
-
-    def DivergenceClearance(self):
-
-        if self.settings["divergence_clearance_steps"].GetInt() > 0:
-            print("Calculating divergence-free initial condition")
-            ## Initialize with a Stokes solution step
-            try:
-                import KratosMultiphysics.ExternalSolversApplication as KratosExternalSolvers
-                smoother_type = KratosExternalSolvers.AMGCLSmoother.DAMPED_JACOBI
-                solver_type = KratosExternalSolvers.AMGCLIterativeSolverType.CG
-                gmres_size = 50
-                max_iter = 200
-                tol = 1e-7
-                verbosity = 0
-                stokes_linear_solver = KratosExternalSolvers.AMGCLSolver(smoother_type,
-                                                                         solver_type,
-                                                                         tol,
-                                                                         max_iter,
-                                                                         verbosity,
-                                                                         gmres_size)
-            except:
-                pPrecond = DiagonalPreconditioner()
-                stokes_linear_solver = BICGSTABSolver(1e-9, 5000, pPrecond)
-
-            stokes_process = KratosCFD.StokesInitializationProcess(self.main_model_part,
-                                                                   stokes_linear_solver,
-                                                                   self.computing_model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE],
-                                                                   KratosCFD.PATCH_INDEX)
-            ## Copy periodic conditions to Stokes problem
-            stokes_process.SetConditions(self.main_model_part.Conditions)
-            ## Execute Stokes process
-            stokes_process.Execute()
-            stokes_process = None
-
-            for node in self.main_model_part.Nodes:
-                node.SetSolutionStepValue(KratosMultiphysics.PRESSURE, 0, 0.0)
-                node.SetSolutionStepValue(KratosMultiphysics.ACCELERATION_X, 0, 0.0)
-                node.SetSolutionStepValue(KratosMultiphysics.ACCELERATION_Y, 0, 0.0)
-                node.SetSolutionStepValue(KratosMultiphysics.ACCELERATION_Z, 0, 0.0)
-##                vel = node.GetSolutionStepValue(VELOCITY)
-##                for i in range(0,2):
-##                    node.SetSolutionStepValue(VELOCITY,i,vel)
-
-            self.settings["divergence_clearance_steps"].SetInt(0)
-            print("Finished divergence clearance.")
-
-
-    def SolverInitialize(self):
-        self.DivergenceClearance()
         (self.solver).Initialize()
 
+        self.solver.Check()
 
-    def Solve(self):
-       
-        self.DivergenceClearance()
-       
-        (self.solver).Solve()
+        KratosMultiphysics.Logger.PrintInfo("NavierStokesSolverMonolithicChimera", "Solver initialization finished.")
 
 
-    def _ExecuteAfterReading(self):
-        super(NavierStokesSolver_VMSMonolithic_Chimera, self)._ExecuteAfterReading()
-
-        # Read the KINEMATIC VISCOSITY
+    def _set_physical_properties(self):
+        # Transfer density and (kinematic) viscostity to the nodes
         for el in self.main_model_part.Elements:
-            # rho = el.Properties.GetValue(KratosMultiphysics.DENSITY)
-            kin_viscosity = el.Properties.GetValue(KratosMultiphysics.VISCOSITY)
+            rho = el.Properties.GetValue(KratosMultiphysics.DENSITY)
+            print('rho', rho)
+            if rho <= 0.0:
+                raise Exception("DENSITY set to {0} in Properties {1}, positive number expected.".format(rho,el.Properties.Id))
+            dyn_viscosity = el.Properties.GetValue(KratosMultiphysics.DYNAMIC_VISCOSITY)
+            print('dyn_viscosity', dyn_viscosity)
+            if dyn_viscosity <= 0.0:
+                raise Exception("DYNAMIC_VISCOSITY set to {0} in Properties {1}, positive number expected.".format(dyn_viscosity,el.Properties.Id))
+            kin_viscosity = dyn_viscosity / rho
             break
 
-        # KratosMultiphysics.VariableUtils().SetScalarVar(KratosMultiphysics.DENSITY, rho, self.main_model_part.Nodes)
+        KratosMultiphysics.VariableUtils().SetScalarVar(KratosMultiphysics.DENSITY, rho, self.main_model_part.Nodes)
         KratosMultiphysics.VariableUtils().SetScalarVar(KratosMultiphysics.VISCOSITY, kin_viscosity, self.main_model_part.Nodes)
