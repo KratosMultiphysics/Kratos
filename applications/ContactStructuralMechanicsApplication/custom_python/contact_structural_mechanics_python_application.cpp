0657a6fae32c2cd7b567651c5a1a73983153ca39
Riccardo Rossi <rrossi@cimne.upc.edu>
2018-03-15 10:51:34 +0100

    fixing cotire compilation

--- applications/ContactStructuralMechanicsApplication/custom_python/contact_structural_mechanics_python_application.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_python/contact_structural_mechanics_python_application.cpp
@@ -63,27 +63,6 @@ PYBIND11_MODULE(KratosContactStructuralMechanicsApplication, m)
     // CONDITIONS
     // CONTACT
     /* Mortar contact */
-<<<<<<< HEAD
-    KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS( AUXILIAR_COORDINATES )     // Auxiliar coordinates used to map
-    KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS( DELTA_COORDINATES )        // Delta coordinates used to map
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( INTEGRATION_ORDER_CONTACT )                   // The integration order considered
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( DISTANCE_THRESHOLD )                          // The distance threshold considered
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( ACTIVE_CHECK_FACTOR )                         // The factor employed to search an active/inactive node
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( NORMAL_GAP )                                  // The normal gap employed in contact formulation
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( WEIGHTED_GAP )                                // The integrated gap employed in mortar formulation
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( WEIGHTED_SLIP )                               // The integrated slip employed in mortar formulation
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( WEIGHTED_SCALAR_RESIDUAL )                    // The integrated scalar residual
-    KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS( WEIGHTED_VECTOR_RESIDUAL ) // The integrated vector residual
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( ACTIVE_CHECK_FACTOR )                         // The factor employed to search an active/inactive node
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( ACTIVE_SET_CONVERGED )                        // To know if the active set has converged
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( DYNAMIC_FACTOR )                              // The factor considered for dynamic problems (in order to take intro account the gap evolution)
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( AUGMENTED_NORMAL_CONTACT_PRESSURE )           // The resultant augmented pressure in the normal direction
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( AUGMENTED_TANGENT_CONTACT_PRESSURE )          // The resultant augmented pressure in the tangent direction
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( TANGENT_FACTOR )                              // The proportion between the tangent and normal penalty
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( CONSIDER_NORMAL_VARIATION )                   // A value used to check if consider normal variation or not
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( ADAPT_PENALTY )                               // To set if the penalty is recalculated or not
-    KRATOS_REGISTER_IN_PYTHON_VARIABLE( MAX_GAP_FACTOR )                              // The factor between the nodal H and the max gap considered to recalculate the penalty
-=======
     KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS(m, AUXILIAR_COORDINATES )     // Auxiliar coordinates used to map
     KRATOS_REGISTER_IN_PYTHON_3D_VARIABLE_WITH_COMPONENTS(m, DELTA_COORDINATES )        // Delta coordinates used to map
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(m, INTEGRATION_ORDER_CONTACT )                   // The integration order considered
@@ -101,7 +80,6 @@ PYBIND11_MODULE(KratosContactStructuralMechanicsApplication, m)
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(m, CONSIDER_NORMAL_VARIATION )                   // A value used to check if consider normal variation or not
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(m, ADAPT_PENALTY )                               // To set if the penalty is recalculated or not
     KRATOS_REGISTER_IN_PYTHON_VARIABLE(m, MAX_GAP_FACTOR )                              // The factor between the nodal H and the max gap considered to recalculate the penalty
->>>>>>> e0a713cbed37795eb37acae285b589fd8cf2533f
 }
 
 
