f044f9b983236ec7a58786afbb1ab013a2054e93
baumgaertner <daniel.baumgaertner@tum.de>
2018-03-16 14:51:46 +0100

    added eigenfrequency to internal analyzer

--- applications/ShapeOptimizationApplication/python_scripts/analyzer_internal.py
+++ applications/ShapeOptimizationApplication/python_scripts/analyzer_internal.py
@@ -53,17 +53,6 @@ class KratosInternalAnalyzer( (__import__("analyzer_base")).AnalyzerBaseClass ):
 
             communicator.reportValue("strain_energy", self.listOfResponseFunctions["strain_energy"].GetValue())
 
-        # Calculation of value of mass
-        if communicator.isRequestingValueOf("mass"):
-
-            print("\n> Starting calculation of value of mass")
-            startTime = timer.time()
-            self.listOfResponseFunctions["mass"].CalculateValue()
-            constraintValue = self.listOfResponseFunctions["mass"].GetValue()
-            print("> Time needed for calculation of value of mass = ",round(timer.time() - startTime,2),"s")
-
-            communicator.reportValue("mass", constraintValue)
-
         # Calculation of gradients of strain energy
         if communicator.isRequestingGradientOf("strain_energy"):
 
@@ -75,6 +64,17 @@ class KratosInternalAnalyzer( (__import__("analyzer_base")).AnalyzerBaseClass ):
             gradientForCompleteModelPart = self.listOfResponseFunctions["strain_energy"].GetGradient()
             communicator.reportGradient("strain_energy", gradientForCompleteModelPart)
 
+        # Calculation of value of mass
+        if communicator.isRequestingValueOf("mass"):
+
+            print("\n> Starting calculation of value of mass")
+            startTime = timer.time()
+            self.listOfResponseFunctions["mass"].CalculateValue()
+            constraintValue = self.listOfResponseFunctions["mass"].GetValue()
+            print("> Time needed for calculation of value of mass = ",round(timer.time() - startTime,2),"s")
+
+            communicator.reportValue("mass", constraintValue)
+
         # Calculation of gradients of mass
         if communicator.isRequestingGradientOf("mass"):
 
@@ -85,6 +85,34 @@ class KratosInternalAnalyzer( (__import__("analyzer_base")).AnalyzerBaseClass ):
 
             gradientForCompleteModelPart = self.listOfResponseFunctions["mass"].GetGradient()
             communicator.reportGradient("mass", gradientForCompleteModelPart)
+
+        # Calculation of eigenfrequency
+        if communicator.isRequestingValueOf("eigenfrequency"):
+
+            print("\n> Starting StructuralMechanicsApplication to solve structure")
+            startTime = timer.time()
+            self.csm_analysis.InitializeTimeStep()
+            self.csm_analysis.SolveTimeStep()
+            self.csm_analysis.FinalizeTimeStep()
+            print("> Time needed for solving the structure = ",round(timer.time() - startTime,2),"s")
+
+            print("\n> Starting calculation of eigenfrequency")
+            startTime = timer.time()
+            self.listOfResponseFunctions["eigenfrequency"].CalculateValue()
+            print("> Time needed for calculation of eigenfrequency = ",round(timer.time() - startTime,2),"s")
+
+            communicator.reportValue("eigenfrequency", self.listOfResponseFunctions["eigenfrequency"].GetValue())
+
+        # Calculation of gradient of eigenfrequency
+        if communicator.isRequestingGradientOf("eigenfrequency"):
+
+            print("\n> Starting calculation of gradients of eigenfrequency")
+            startTime = timer.time()
+            self.listOfResponseFunctions["eigenfrequency"].CalculateGradient()
+            print("> Time needed for calculating gradients of eigenfrequency = ",round(timer.time() - startTime,2),"s")
+
+            gradientForCompleteModelPart = self.listOfResponseFunctions["eigenfrequency"].GetGradient()
+            communicator.reportGradient("eigenfrequency", gradientForCompleteModelPart)
 
     # --------------------------------------------------------------------------
     def FinalizeAfterOptimizationLoop( self ):
