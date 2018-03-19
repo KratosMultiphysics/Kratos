567b05f65b72b339668b8944ff5493f56acd5059
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-03-19 18:57:40 +0100

    Missing noalias

--- kratos/utilities/plane_approximation_utility.h
+++ kratos/utilities/plane_approximation_utility.h
@@ -145,7 +145,7 @@ private:
         const unsigned int n_points = rPointsCoords.size();
         KRATOS_ERROR_IF(n_points == 0) << "No base point can be computed. Points container is empty." << std::endl;
 
-        rBasePointCoords = ZeroVector(3);
+        noalias(rBasePointCoords) = ZeroVector(3);
         for (unsigned int j = 0; j < n_points; ++j){
             rBasePointCoords += rPointsCoords[j];
         }
