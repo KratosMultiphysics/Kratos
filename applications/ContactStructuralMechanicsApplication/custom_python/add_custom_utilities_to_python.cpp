8d47ee784b1d9060b089845312410af2d61a34fa
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-03-20 20:27:13 +0100

    Transition conflict

--- applications/ContactStructuralMechanicsApplication/custom_python/add_custom_utilities_to_python.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -12,14 +12,13 @@
 // System includes
 
 // External includes
+#include <boost/python.hpp>
 
 // Project includes
-#include "includes/define_python.h"
+#include "includes/define.h"
 #include "processes/process.h"
 #include "custom_python/add_custom_utilities_to_python.h"
-
-#include "spaces/ublas_space.h"
-#include "linear_solvers/linear_solver.h"
+#include "includes/ublas_interface.h"
 
 //Utilities
 #include "custom_utilities/tree_contact_search.h"
@@ -30,18 +29,15 @@ namespace Kratos
 {
 namespace Python
 {
-using namespace pybind11;
-
-void  AddCustomUtilitiesToPython(pybind11::module& m)
+void  AddCustomUtilitiesToPython()
 {
+    using namespace boost::python;
+
     // Tree contact search
-    class_<TreeContactSearch<2, 2>, TreeContactSearch<2, 2>::Pointer>(m, "TreeContactSearch2D2N")
-    .def(init<ModelPart&>())
+    class_<TreeContactSearch<2, 2>>("TreeContactSearch2D2N", init<ModelPart&>())
     .def(init<ModelPart&, Parameters>())
     .def("InitializeMortarConditions",&TreeContactSearch<2, 2>::InitializeMortarConditions)
-    .def("ClearScalarMortarConditions",&TreeContactSearch<2, 2>::ClearScalarMortarConditions)
-    .def("ClearComponentsMortarConditions",&TreeContactSearch<2, 2>::ClearComponentsMortarConditions)
-    .def("ClearALMFrictionlessMortarConditions",&TreeContactSearch<2, 2>::ClearALMFrictionlessMortarConditions)
+    .def("ClearMortarConditions",&TreeContactSearch<2, 2>::ClearMortarConditions)
     .def("CreatePointListMortar",&TreeContactSearch<2, 2>::CreatePointListMortar)
     .def("UpdatePointListMortar",&TreeContactSearch<2, 2>::UpdatePointListMortar)
     .def("UpdateMortarConditions",&TreeContactSearch<2, 2>::UpdateMortarConditions)
@@ -49,13 +45,10 @@ void  AddCustomUtilitiesToPython(pybind11::module& m)
     .def("CheckMortarConditions",&TreeContactSearch<2, 2>::CheckMortarConditions)
     .def("InvertSearch",&TreeContactSearch<2, 2>::InvertSearch)
     ;
-    class_<TreeContactSearch<3, 3>, TreeContactSearch<3, 3>::Pointer>(m, "TreeContactSearch3D3N")
-    .def(init<ModelPart&>())
+    class_<TreeContactSearch<3, 3>>("TreeContactSearch3D3N", init<ModelPart&>())
     .def(init<ModelPart&, Parameters>())
     .def("InitializeMortarConditions",&TreeContactSearch<3, 3>::InitializeMortarConditions)
-    .def("ClearScalarMortarConditions",&TreeContactSearch<3, 3>::ClearScalarMortarConditions)
-    .def("ClearComponentsMortarConditions",&TreeContactSearch<3, 3>::ClearComponentsMortarConditions)
-    .def("ClearALMFrictionlessMortarConditions",&TreeContactSearch<3, 3>::ClearALMFrictionlessMortarConditions)
+    .def("ClearMortarConditions",&TreeContactSearch<3, 3>::ClearMortarConditions)
     .def("CreatePointListMortar",&TreeContactSearch<3, 3>::CreatePointListMortar)
     .def("UpdatePointListMortar",&TreeContactSearch<3, 3>::UpdatePointListMortar)
     .def("UpdateMortarConditions",&TreeContactSearch<3, 3>::UpdateMortarConditions)
@@ -63,13 +56,10 @@ void  AddCustomUtilitiesToPython(pybind11::module& m)
     .def("CheckMortarConditions",&TreeContactSearch<3, 3>::CheckMortarConditions)
     .def("InvertSearch",&TreeContactSearch<3, 3>::InvertSearch)
     ;
-    class_<TreeContactSearch<3, 4>, TreeContactSearch<3, 4>::Pointer>(m, "TreeContactSearch3D4N")
-    .def(init<ModelPart&>())
+    class_<TreeContactSearch<3, 4>>("TreeContactSearch3D4N", init<ModelPart&>())
     .def(init<ModelPart&, Parameters>())
     .def("InitializeMortarConditions",&TreeContactSearch<3, 4>::InitializeMortarConditions)
-    .def("ClearScalarMortarConditions",&TreeContactSearch<3, 4>::ClearScalarMortarConditions)
-    .def("ClearComponentsMortarConditions",&TreeContactSearch<3, 4>::ClearComponentsMortarConditions)
-    .def("ClearALMFrictionlessMortarConditions",&TreeContactSearch<3, 4>::ClearALMFrictionlessMortarConditions)
+    .def("ClearMortarConditions",&TreeContactSearch<3, 4>::ClearMortarConditions)
     .def("CreatePointListMortar",&TreeContactSearch<3, 4>::CreatePointListMortar)
     .def("UpdatePointListMortar",&TreeContactSearch<3, 4>::UpdatePointListMortar)
     .def("UpdateMortarConditions",&TreeContactSearch<3, 4>::UpdateMortarConditions)
@@ -79,24 +69,25 @@ void  AddCustomUtilitiesToPython(pybind11::module& m)
     ;
 
     // Adding search related enums
-    enum_<SearchTreeType>(m, "SearchTreeType")
+    enum_<SearchTreeType>("SearchTreeType")
     .value("KdtreeInRadius", KdtreeInRadius)
     .value("KdtreeInBox", KdtreeInBox)
     .value("Kdop", Kdop)
     ;
 
-    enum_<CheckGap>(m, "CheckGap")
+    enum_<CheckGap>("CheckGap")
     .value("NoCheck", NoCheck)
     .value("DirectCheck", DirectCheck)
     .value("MappingCheck", MappingCheck)
     ;
 
     // Process Factory utility
-    class_<ProcessFactoryUtility, ProcessFactoryUtility::Pointer>(m, "ProcessFactoryUtility")
-    .def(init<  list&>())
-    .def(init< >())
+    class_<ProcessFactoryUtility>("ProcessFactoryUtility", init<>())
+    .def(init<boost::python::list&>())
+    .def(init<boost::python::object&>())
     .def("AddProcess",&ProcessFactoryUtility::AddProcess)
     .def("AddProcesses",&ProcessFactoryUtility::AddProcesses)
+    .def("ExecuteMethod",&ProcessFactoryUtility::ExecuteMethod)
     .def("ExecuteInitialize",&ProcessFactoryUtility::ExecuteInitialize)
     .def("ExecuteBeforeSolutionLoop",&ProcessFactoryUtility::ExecuteBeforeSolutionLoop)
     .def("ExecuteInitializeSolutionStep",&ProcessFactoryUtility::ExecuteInitializeSolutionStep)
@@ -104,12 +95,13 @@ void  AddCustomUtilitiesToPython(pybind11::module& m)
     .def("ExecuteBeforeOutputStep",&ProcessFactoryUtility::ExecuteBeforeOutputStep)
     .def("ExecuteAfterOutputStep",&ProcessFactoryUtility::ExecuteAfterOutputStep)
     .def("ExecuteFinalize",&ProcessFactoryUtility::ExecuteFinalize)
+    .def("IsOutputStep",&ProcessFactoryUtility::IsOutputStep)
+    .def("PrintOutput",&ProcessFactoryUtility::PrintOutput)
     .def("Clear",&ProcessFactoryUtility::Clear)
     ;
 
     // Sparse matrix multiplication utility
-    class_<SparseMatrixMultiplicationUtility, SparseMatrixMultiplicationUtility::Pointer>(m, "SparseMatrixMultiplicationUtility")
-    .def(init<>())
+    class_<SparseMatrixMultiplicationUtility>("SparseMatrixMultiplicationUtility", init<>())
     .def("MatrixMultiplicationSaad",&SparseMatrixMultiplicationUtility::MatrixMultiplicationSaad<CompressedMatrix, CompressedMatrix, CompressedMatrix>)
     .def("MatrixMultiplicationRMerge",&SparseMatrixMultiplicationUtility::MatrixMultiplicationRMerge<CompressedMatrix, CompressedMatrix, CompressedMatrix>)
     .def("MatrixAdd",&SparseMatrixMultiplicationUtility::MatrixAdd<CompressedMatrix, CompressedMatrix>)
