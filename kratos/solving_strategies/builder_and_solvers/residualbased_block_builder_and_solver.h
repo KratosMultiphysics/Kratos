65dc98f413d2725e371ce6a1feb06f941298e37b
adityaHome <shine.aditya@gmail.com>
2018-03-20 17:52:58 +0100

    Correcting the condition for accumulating the flag for all dofs

--- kratos/solving_strategies/builder_and_solvers/residualbased_block_builder_and_solver.h
+++ kratos/solving_strategies/builder_and_solvers/residualbased_block_builder_and_solver.h
@@ -707,7 +707,7 @@ public:
         for(int i = 0; i<num_dofs; i++)
         { 
             typename DofsArrayType::iterator dof_iterator = BaseType::mDofSet.begin() + i;
-            all_dofs_have_reactions = dof_iterator->HasReaction();
+            all_dofs_have_reactions = dof_iterator->HasReaction() && all_dofs_have_reactions;
         }
 
         KRATOS_ERROR_IF_NOT(all_dofs_have_reactions) << "All the DOFs do not have reaction variables set. Not possible to calculate reactions." <<std::endl;
