b66db796d5cfa4a1ac64d64a7f895a79fcf1de09
Carlos Roig <roigcarlo@gmail.com>
2018-03-20 10:29:58 +0100

    Rewriting Tester interface to enable enums

--- kratos/python/add_testing_to_python.cpp
+++ kratos/python/add_testing_to_python.cpp
@@ -32,47 +32,36 @@ void ListOfAllTestCases() {
 void  AddTestingToPython(pybind11::module& m) {
 	using namespace pybind11;
 
-  scope tester_scope = class_<Testing::Tester, Kratos::shared_ptr<Testing::Tester> >(m,"Tester")
+    class_<Testing::Tester, Kratos::shared_ptr<Testing::Tester> > TesterPyBind(m, "Tester");
 
-  // Properties
-  .def_static("SetVerbosity",&Testing::Tester::SetVerbosity)
-//   .staticmethod("SetVerbosity")
+    // Properties
+    TesterPyBind
+        .def_static("SetVerbosity",&Testing::Tester::SetVerbosity)
+        // Run methods
+        .def_static("RunAllTestCases", &Testing::Tester::RunAllTestCases)
+        .def_static("RunTestSuite", &Testing::Tester::RunTestSuite)
+        .def_static("RunTestCases", &Testing::Tester::RunTestCases)
 
-  // Run
-  .def_static("RunAllTestCases", &Testing::Tester::RunAllTestCases)
-//   .staticmethod("RunAllTestCases")
-  .def_static("RunTestSuite", &Testing::Tester::RunTestSuite)
-//   .staticmethod("RunTestSuite")
-  .def_static("RunTestCases", &Testing::Tester::RunTestCases)
-//   .staticmethod("RunTestCases")
+        // Profile tests
+        .def_static("ProfileAllTestCases", &Testing::Tester::ProfileAllTestCases)
+        .def_static("ProfileTestSuite", &Testing::Tester::ProfileTestSuite)
 
-  // Profile tests
-  .def_static("ProfileAllTestCases", &Testing::Tester::ProfileAllTestCases)
-//   .staticmethod("ProfileAllTestCases")
-  .def_static("ProfileTestSuite", &Testing::Tester::ProfileTestSuite)
-//   .staticmethod("ProfileTestSuite")
+        // Utils
+        .def_static("NumberOfFailedTestCases", &Testing::Tester::NumberOfFailedTestCases)
+        .def_static("ResetAllTestCasesResults", &Testing::Tester::ResetAllTestCasesResults)
 
-  // Utils
-  .def_static("NumberOfFailedTestCases", &Testing::Tester::NumberOfFailedTestCases)
-//   .staticmethod("NumberOfFailedTestCases")
-  .def_static("ResetAllTestCasesResults", &Testing::Tester::ResetAllTestCasesResults)
-//   .staticmethod("ResetAllTestCasesResults")
+        // Info
+        .def_static("ListOfAllTestCases", ListOfAllTestCases)
+    ;
 
-  // Info
-  .def_static("ListOfAllTestCases", ListOfAllTestCases)
-//   .staticmethod("ListOfAllTestCases")
-
-  ;
-
-  enum_<Testing::Tester::Verbosity>(m,"Verbosity")
-  // Enums
-  .value("QUITE", Testing::Tester::Verbosity::QUITE)
-  .value("PROGRESS", Testing::Tester::Verbosity::PROGRESS)
-  .value("TESTS_LIST", Testing::Tester::Verbosity::TESTS_LIST)
-  .value("FAILED_TESTS_OUTPUTS", Testing::Tester::Verbosity::FAILED_TESTS_OUTPUTS)
-  .value("TESTS_OUTPUTS", Testing::Tester::Verbosity::TESTS_OUTPUTS);
-
+    enum_<Testing::Tester::Verbosity>(TesterPyBind, "Verbosity")
+        .value("QUITE", Testing::Tester::Verbosity::QUITE)
+        .value("PROGRESS", Testing::Tester::Verbosity::PROGRESS)
+        .value("TESTS_LIST", Testing::Tester::Verbosity::TESTS_LIST)
+        .value("FAILED_TESTS_OUTPUTS", Testing::Tester::Verbosity::FAILED_TESTS_OUTPUTS)
+        .value("TESTS_OUTPUTS", Testing::Tester::Verbosity::TESTS_OUTPUTS)
+    ;
 }
-}  // namespace Python.
 
+}  // namespace Python.
 } // Namespace Kratos
