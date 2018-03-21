788e7ca107ac4d0904e0554febd3d67ab9393a72
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-03-21 20:09:06 +0100

    Toward Flag transfer (WIP)

--- kratos/processes/fast_transfer_between_model_parts_process.h
+++ kratos/processes/fast_transfer_between_model_parts_process.h
@@ -20,8 +20,8 @@
 
 // Project includes
 #include "includes/model_part.h"
-#include "includes/kratos_parameters.h"
 #include "processes/process.h"
+#include "utilities/openmp_utils.h"
 
 namespace Kratos
 {
@@ -67,6 +67,9 @@ public:
     ///@name Type Definitions
     ///@{
 
+    /// The type used for the node
+    typedef Node<3> NodeType;
+
     /// The type used to identify the size
     typedef std::size_t SizeType;
 
@@ -77,14 +80,22 @@ public:
     ///@name Life Cycle
     ///@{
     
+    /**
+     * @brief Default constructor. Without flag
+     * @param rDestinationModelPart The destination model part
+     * @param rOriginModelPart The origin model part
+     * @param EntityString The elements to transfer
+     */
     FastTransferBetweenModelPartsProcess(
         ModelPart& rDestinationModelPart,
         ModelPart& rOriginModelPart,
-        const std::string EntityString
+        const std::string EntityString = "All",
+        const std::string FlagName = ""
         ) : Process(),
             mrDestinationModelPart(rDestinationModelPart), 
             mrOriginModelPart(rOriginModelPart),
-            mEntity(ConvertEntity(EntityString))
+            mEntity(ConvertEntity(EntityString)),
+            mFlagName(FlagName)
     {
         KRATOS_TRY
                 
@@ -115,20 +126,96 @@ public:
     {
         KRATOS_TRY;
         
-        const SizeType num_nodes = mrOriginModelPart.Nodes().size();
+        // In case of not flag defined we transfer all the elements
+        if (!KratosComponents<Flags>::Has(mFlagName)) {
+            const SizeType num_nodes = mrOriginModelPart.Nodes().size();
 
-        if (num_nodes != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::NODES || mEntity == EntityTransfered::NODESANDELEMENTS))
-            mrDestinationModelPart.AddNodes(mrOriginModelPart.NodesBegin(),mrOriginModelPart.NodesEnd());
+            if (num_nodes != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::NODES || mEntity == EntityTransfered::NODESANDELEMENTS))
+                mrDestinationModelPart.AddNodes(mrOriginModelPart.NodesBegin(),mrOriginModelPart.NodesEnd());
 
-        const SizeType num_elements = mrOriginModelPart.Elements().size();
+            const SizeType num_elements = mrOriginModelPart.Elements().size();
 
-        if (num_elements != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::ELEMENTS || mEntity == EntityTransfered::NODESANDELEMENTS))
-            mrDestinationModelPart.AddElements(mrOriginModelPart.ElementsBegin(),mrOriginModelPart.ElementsEnd());
+            if (num_elements != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::ELEMENTS || mEntity == EntityTransfered::NODESANDELEMENTS))
+                mrDestinationModelPart.AddElements(mrOriginModelPart.ElementsBegin(),mrOriginModelPart.ElementsEnd());
 
-        const SizeType num_conditions = mrOriginModelPart.Conditions().size();
+            const SizeType num_conditions = mrOriginModelPart.Conditions().size();
 
-        if (num_conditions != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::CONDITIONS))
-             mrDestinationModelPart.AddConditions(mrOriginModelPart.ConditionsBegin(),mrOriginModelPart.ConditionsEnd());
+            if (num_conditions != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::CONDITIONS))
+                mrDestinationModelPart.AddConditions(mrOriginModelPart.ConditionsBegin(),mrOriginModelPart.ConditionsEnd());
+        } else {
+//             const Flags this_flag = KratosComponents<Flags>::Get(mFlagName);
+//
+//             std::vector<NodeType::Pointer> vector_nodes;
+//             std::vector<Element::Pointer> vector_elements;
+//             std::vector<Condition::Pointer> vector_conditions;
+//
+//             // Creating a buffer for parallel vector fill
+//             const int num_threads = OpenMPUtils::GetNumThreads();
+//             std::vector<std::vector<NodeType::Pointer>> nodes_buffer(num_threads);
+//             std::vector<std::vector<Element::Pointer>> elements_buffer(num_threads);
+//             std::vector<std::vector<Condition::Pointer>> conditions_buffer(num_threads);
+//
+//             // Auxiliar sizes
+//             const int num_nodes = static_cast<int>(mrOriginModelPart.Nodes().size());
+//             const int num_elements = static_cast<int>(mrOriginModelPart.Elements().size());
+//             const int num_conditions = static_cast<int>(mrOriginModelPart.Conditions().size());
+//
+//             #pragma omp parallel
+//             {
+//                 const int id = OpenMPUtils::ThisThread();
+//
+//                 if (num_nodes != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::NODES || mEntity == EntityTransfered::NODESANDELEMENTS)) {
+//                     #pragma omp for
+//                     for(int i = 0; i < num_nodes; ++i) {
+//                         auto it_node = mrOriginModelPart.NodesBegin() + i;
+//                         if (it_node->Is(this_flag)) {
+//                             (nodes_buffer[id]).push_back(*(it_node.base()));
+//                         }
+//                     }
+//                 }
+//
+//                 if (num_elements != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::ELEMENTS || mEntity == EntityTransfered::NODESANDELEMENTS)) {
+//                     #pragma omp for
+//                     for(int i = 0; i < num_elements; ++i) {
+//                         auto it_elem = mrOriginModelPart.ElementsBegin() + i;
+//                         if (it_elem->Is(this_flag)) {
+//                             (elements_buffer[id]).push_back(*(it_elem.base()));
+//                         }
+//                     }
+//                 }
+//
+//                 if (num_conditions != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::CONDITIONS)) {
+//                     #pragma omp for
+//                     for(int i = 0; i < num_nodes; ++i) {
+//                         auto it_cond = mrOriginModelPart.ConditionsBegin() + i;
+//                         if (it_cond->Is(this_flag)) {
+//                             (conditions_buffer[id]).push_back(*(it_cond.base()));
+//                         }
+//                     }
+//                 }
+//
+//                 // Combine buffers together
+//                 #pragma omp single
+//                 {
+//                     for( auto& node_buffer : nodes_buffer)
+//                         std::move(node_buffer.begin(),node_buffer.end(),back_inserter(vector_nodes));
+//                     for( auto& element_buffer : elements_buffer)
+//                         std::move(element_buffer.begin(),element_buffer.end(),back_inserter(vector_elements));
+//                     for( auto& condition_buffer : conditions_buffer)
+//                         std::move(condition_buffer.begin(),condition_buffer.end(),back_inserter(vector_conditions));
+//                 }
+//             }
+//
+//             // We transfer
+//             if (num_nodes != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::NODES || mEntity == EntityTransfered::NODESANDELEMENTS))
+//                 mrDestinationModelPart.AddNodes(vector_nodes.begin(),vector_nodes.end());
+//
+//             if (num_elements != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::ELEMENTS || mEntity == EntityTransfered::NODESANDELEMENTS))
+//                 mrDestinationModelPart.AddElements(vector_elements.begin(),vector_elements.end());
+//
+//             if (num_conditions != 0 && (mEntity == EntityTransfered::ALL || mEntity == EntityTransfered::CONDITIONS))
+//                 mrDestinationModelPart.AddConditions(vector_conditions.begin(),vector_conditions.end());
+        }
 
         KRATOS_CATCH("");
     }
@@ -210,6 +297,8 @@ private:
     ModelPart& mrOriginModelPart;      /// The origin model part
     
     const EntityTransfered mEntity;    /// The entity to transfer
+
+    const std::string mFlagName;       /// A flag in order to tranfer only components with that flag
     
     ///@}
     ///@name Private Operators
@@ -220,25 +309,26 @@ private:
     ///@{
     
     /**
-     * This converts the entity string to an enum
-     * @param str The string that you want to convert in the equivalent enum
+     * @brief This converts the entity string to an enum
+     * @param Str The string that you want to convert in the equivalent enum
      * @return Interpolation: The equivalent enum (this requires less memmory than a std::string)
      */
 
-    EntityTransfered ConvertEntity(const std::string& str)
+    EntityTransfered ConvertEntity(const std::string& Str)
     {
-        if(str == "Nodes")
+        if(Str == "Nodes" || Str == "NODES")
             return EntityTransfered::NODES;
-        else if(str == "Elements")
+        else if(Str == "Elements" || Str == "ELEMENTS")
             return EntityTransfered::ELEMENTS;
-        else if(str == "NodesAndElements")
+        else if(Str == "NodesAndElements" || Str == "NODESANDELEMENTS")
             return EntityTransfered::NODESANDELEMENTS;
-        else if(str == "Conditions")
+        else if(Str == "Conditions" || Str == "CONDITIONS")
             return EntityTransfered::CONDITIONS;
-        else if(str == "All")
+        else if(Str == "All" || Str == "ALL")
             return EntityTransfered::ALL;
         else
-            return EntityTransfered::ALL;
+            KRATOS_ERROR << "The entity declared " << Str << " is not on the following list\n" \
+                         << "- NODES\n" << "- ELEMENTS\n" << "- NODESANDELEMENTS\n" << "- CONDITIONS\n" << "- ALL\n" << std::endl;
     }
     
     ///@}
