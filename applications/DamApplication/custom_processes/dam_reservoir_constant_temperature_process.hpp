6f0d9c5c81d40f6008f2d88a03c898ac0692f8ab
Unknown <djvicente@cimne.upc.edu>
2018-03-21 10:31:12 +0100

    Fix error related to water temperature process
    A 'comma' in the Default parameters has been removed.

--- applications/DamApplication/custom_processes/dam_reservoir_constant_temperature_process.hpp
+++ applications/DamApplication/custom_processes/dam_reservoir_constant_temperature_process.hpp
@@ -55,7 +55,7 @@ class DamReservoirConstantTemperatureProcess : public Process
                 "Water_temp"                                       : 0.0,
                 "Water_temp_Table"                                 : 0,                
                 "Water_level"                                      : 0.0,
-                "Water_level_Table"                                : 0,
+                "Water_level_Table"                                : 0
             }  )");
 
         // Some values need to be mandatorily prescribed since no meaningful default value exist. For this reason try accessing to them
