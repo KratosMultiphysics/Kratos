dd01258e308e7c7e103a1fffa176c106939e21dd
MFusseder <martin.fusseder@tum.de>
2018-03-21 16:46:25 +0100

    renamed folder 'adjoint_beam_structure'

--- applications/StructuralMechanicsApplication/tests/test_adjoint_sensitity_analysis_beam_3d2n_structure.py
+++ applications/StructuralMechanicsApplication/tests/test_adjoint_sensitity_analysis_beam_3d2n_structure.py
@@ -17,7 +17,7 @@ class TestCase(KratosUnittest.TestCase):
             os.remove(file_path)
 
     def _solve_primal_problem(self):
-        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure/beam_test_parameters.json",'r') as parameter_file:
+        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure_3d2n/beam_test_parameters.json",'r') as parameter_file:
             ProjectParametersPrimal = Parameters( parameter_file.read())
         parameter_file.close()
         primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(ProjectParametersPrimal)
@@ -28,7 +28,7 @@ class TestCase(KratosUnittest.TestCase):
         # Solve primal problem (only in one test case necessary)
         self._solve_primal_problem()
         # Create the adjoint solver
-        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure/beam_test_local_stress_adjoint_parameters.json",'r') as parameter_file:
+        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure_3d2n/beam_test_local_stress_adjoint_parameters.json",'r') as parameter_file:
             ProjectParametersAdjoint = Parameters( parameter_file.read())
         adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(ProjectParametersAdjoint)
         adjoint_analysis.Run()
@@ -46,7 +46,7 @@ class TestCase(KratosUnittest.TestCase):
 
     def test_nodal_displacement_response(self):
         # Create the adjoint solver
-        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure/beam_test_nodal_disp_adjoint_parameters.json",'r') as parameter_file:
+        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure_3d2n/beam_test_nodal_disp_adjoint_parameters.json",'r') as parameter_file:
             ProjectParametersAdjoint = Parameters( parameter_file.read())
         adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(ProjectParametersAdjoint)
 
@@ -65,7 +65,7 @@ class TestCase(KratosUnittest.TestCase):
 
     def test_strain_energy_response(self):
         # Create the adjoint solver
-        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure/beam_test_strain_energy_adjoint_parameters.json",'r') as parameter_file:
+        with open("./adjoint_sensitivity_test_martin/adjoint_beam_structure_3d2n/beam_test_strain_energy_adjoint_parameters.json",'r') as parameter_file:
             ProjectParametersAdjoint = Parameters( parameter_file.read())
         adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(ProjectParametersAdjoint)
 
@@ -84,8 +84,8 @@ class TestCase(KratosUnittest.TestCase):
 
 
         # Delete *.h5 only after last test case because primal solution is used in each test case
-        self._remove_file("./adjoint_sensitivity_test_martin/adjoint_beam_structure/beam_test_0.h5")
-        self._remove_file("./adjoint_sensitivity_test_martin/adjoint_beam_structure/Beam_structure.time")
+        self._remove_file("./adjoint_sensitivity_test_martin/adjoint_beam_structure_3d2n/beam_test_0.h5")
+        self._remove_file("./adjoint_sensitivity_test_martin/adjoint_beam_structure_3d2n/Beam_structure.time")
 
     def tearDown(self):
         pass
