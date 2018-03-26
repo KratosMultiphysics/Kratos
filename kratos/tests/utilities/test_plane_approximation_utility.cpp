87425f071fb678378aca6f0ae20f3559f4965bfc
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-03-26 10:48:31 +0200

    Bug correction in 2D plus @pooyan-dadvand suggestions.

--- kratos/tests/utilities/test_plane_approximation_utility.cpp
+++ kratos/tests/utilities/test_plane_approximation_utility.cpp
@@ -35,7 +35,7 @@ namespace Testing {
 
         // Compute the plane approximation
         array_1d<double,3> base_point_coords, normal;
-        PlaneApproximationUtility::ComputePlaneApproximation(points_coordinates, base_point_coords, normal);
+        PlaneApproximationUtility<2>::ComputePlaneApproximation(points_coordinates, base_point_coords, normal);
 
         // Compute and check the obtained plane distance values
         const double d_1 = inner_prod(normal , point_1.Coordinates() - base_point_coords);
@@ -64,7 +64,7 @@ namespace Testing {
 
         // Compute the plane approximation
         array_1d<double,3> base_point_coords, normal;
-        PlaneApproximationUtility::ComputePlaneApproximation(points_coordinates, base_point_coords, normal);
+        PlaneApproximationUtility<2>::ComputePlaneApproximation(points_coordinates, base_point_coords, normal);
 
         // Compute and check the obtained plane distance values
         const double d_1 = inner_prod(normal , point_1.Coordinates() - base_point_coords);
@@ -72,10 +72,10 @@ namespace Testing {
         const double d_3 = inner_prod(normal , point_3.Coordinates() - base_point_coords);
         const double d_4 = inner_prod(normal , point_4.Coordinates() - base_point_coords);
 
-        KRATOS_CHECK_NEAR(d_1, -0.89886, 1e-6);
-        KRATOS_CHECK_NEAR(d_2, -0.110655, 1e-6);
-        KRATOS_CHECK_NEAR(d_3, 0.110655, 1e-6);
-        KRATOS_CHECK_NEAR(d_4, 0.89886, 1e-6);
+        KRATOS_CHECK_NEAR(d_1, 0.0674564, 1e-6);
+        KRATOS_CHECK_NEAR(d_2, -0.547956, 1e-6);
+        KRATOS_CHECK_NEAR(d_3, 0.547956, 1e-6);
+        KRATOS_CHECK_NEAR(d_4, -0.0674564, 1e-6);
     }
 
     KRATOS_TEST_CASE_IN_SUITE(PlaneApproximationUtilityCircleTriangleIntersection, KratosCoreFastSuite)
@@ -95,7 +95,7 @@ namespace Testing {
 
         // Compute the plane approximation
         array_1d<double,3> base_point_coords, normal;
-        PlaneApproximationUtility::ComputePlaneApproximation(points_coordinates, base_point_coords, normal);
+        PlaneApproximationUtility<2>::ComputePlaneApproximation(points_coordinates, base_point_coords, normal);
 
         // Compute and check the obtained plane distance values
         const double d_1 = inner_prod(normal , point_1.Coordinates() - base_point_coords);
@@ -103,10 +103,10 @@ namespace Testing {
         const double d_3 = inner_prod(normal , point_3.Coordinates() - base_point_coords);
         const double d_4 = inner_prod(normal , point_4.Coordinates() - base_point_coords);
 
-        KRATOS_CHECK_NEAR(d_1, 0.636396, 1e-6);
-        KRATOS_CHECK_NEAR(d_2, -0.636396, 1e-6);
-        KRATOS_CHECK_NEAR(d_3, 0.556776, 1e-6);
-        KRATOS_CHECK_NEAR(d_4, -0.556776, 1e-6);
+        KRATOS_CHECK_NEAR(d_1, -0.0353553, 1e-6);
+        KRATOS_CHECK_NEAR(d_2, -0.0353553, 1e-6);
+        KRATOS_CHECK_NEAR(d_3, 0.0353553, 1e-6);
+        KRATOS_CHECK_NEAR(d_4, 0.0353553, 1e-6);
     }
 
 }  // namespace Testing.
