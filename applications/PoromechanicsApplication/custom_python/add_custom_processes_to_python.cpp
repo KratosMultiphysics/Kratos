3204a88e896ffdfeda7ee3bba6f0b1fc15d67202
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2018-03-26 22:53:27 +0200

    Included model_part to avoid complation error

--- applications/PoromechanicsApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/PoromechanicsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -8,6 +8,7 @@
 // External includes
 
 // Project includes
+#include "includes/model_part.h"
 #include "processes/process.h"
 #include "custom_python/add_custom_processes_to_python.h"
 #include "includes/kratos_parameters.h"
