0e1aa3b19f4d9565778aae85712e010504c044e9
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-03-26 10:54:26 +0200

    Back to previous tolerance value.
    Back to 1e-6 tolerance value. Otherwise the test is failing in Travis.

--- kratos/tests/utilities/test_intersection_utilities.cpp
+++ kratos/tests/utilities/test_intersection_utilities.cpp
@@ -227,9 +227,9 @@ namespace Testing {
         // Compute and check the obtained intersection point coordinates
         const array_1d<double,3> int_pt_coords = int_pt.Coordinates();
         KRATOS_CHECK_EQUAL(int_id, 1);
-        KRATOS_CHECK_NEAR(int_pt_coords(0), 0.857143, 1e-10);
-        KRATOS_CHECK_NEAR(int_pt_coords(1), 0.0952381, 1e-10);
-        KRATOS_CHECK_NEAR(int_pt_coords(2), 0.0952381, 1e-10);
+        KRATOS_CHECK_NEAR(int_pt_coords(0), 0.857143, 1e-6);
+        KRATOS_CHECK_NEAR(int_pt_coords(1), 0.0952381, 1e-6);
+        KRATOS_CHECK_NEAR(int_pt_coords(2), 0.0952381, 1e-6);
     }
 
     KRATOS_TEST_CASE_IN_SUITE(IntersectionUtilitiesTriangleLineNoIntersection, KratosCoreFastSuite)
