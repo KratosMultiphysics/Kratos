a9e97ca4e4198c911e2c3a9505b449b5f5be6a34
Unknown <daniel.baumgaertner@tum.de>
2018-04-03 11:58:19 +0200

    removed unnecessary includes

--- applications/ShapeOptimizationApplication/custom_utilities/damping/damping_function.h
+++ applications/ShapeOptimizationApplication/custom_utilities/damping/damping_function.h
@@ -22,13 +22,6 @@
 #include <algorithm>
 
 // ------------------------------------------------------------------------------
-// External includes
-// ------------------------------------------------------------------------------
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/vector.hpp>
-#include <boost/numeric/ublas/io.hpp>
-
-// ------------------------------------------------------------------------------
 // Project includes
 // ------------------------------------------------------------------------------
 #include "includes/define.h"
