71471d36d4f646e4d4359c97f73a2c7073ca26a0
Unknown <mmaso@cimne.upc.edu>
2018-04-04 17:11:49 +0200

    adding analysis file

---
+++ applications/ShallowWaterApplication/python_scripts/shallow_water_analysis.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing Kratos
+import KratosMultiphysics as KM
+import KratosMultiphysics.ShallowWaterApplication as SW
+
+# Importing the solvers (if available)
+try:
+    import KratosMultiphysics.ExternalSolversApplication
+    KratosMultiphysics.Logger.PrintInfo("ExternalSolversApplication", "succesfully imported")
+except ImportError:
+    KratosMultiphysics.Logger.PrintInfo("ExternalSolversApplication", "not imported")
+
+class ShallowWaterAnalysis(object): # TODO in the future this could derive from a BaseClass in the Core
+    
