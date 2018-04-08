40d9a4c702885152932c7a22df3b53b7c37f9517
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-04-08 11:01:25 +0200

    Revert "Another recursive test with components"
    This reverts commit 9863efad91066902f78a67c3fb3a5392417035c8.

--- kratos/tests/containers/test_data_value_container.cpp
+++ kratos/tests/containers/test_data_value_container.cpp
@@ -46,7 +46,7 @@ KRATOS_TEST_CASE_IN_SUITE(DataValueContainer, KratosCoreFastSuite) {
 /**
  * This test checks recursively the proper functioning of the DataValueContainer
  */
-KRATOS_TEST_CASE_IN_SUITE(DataValueContainerRecursive, KratosCoreFastSuite) {
+KRATOS_TEST_CASE_IN_SUITE(DataValueContainerRecursive, KratosCoreFastSuiteTest) {
   DataValueContainer container;
   Vector original_distances(4);
   original_distances[0] = 0.00;
@@ -61,30 +61,10 @@ KRATOS_TEST_CASE_IN_SUITE(DataValueContainerRecursive, KratosCoreFastSuite) {
     const auto& nodal_h = container.GetValue(NODAL_H);
     const auto& distances = container.GetValue(ELEMENTAL_DISTANCES);
 
-    KRATOS_CHECK_EQUAL(nodal_h, 0.5);
     for (std::size_t i = 0; i < distances.size(); i++) {
+        KRATOS_CHECK_EQUAL(nodal_h, 0.5);
         KRATOS_CHECK_EQUAL(distances[i], original_distances[i]);
     }
-  }
-}
-
-/**
- * This test checks recursively the proper functioning of the DataValueContainer for components varioables
- */
-KRATOS_TEST_CASE_IN_SUITE(DataValueContainerRecursiveComponents, KratosCoreFastSuite) {
-  DataValueContainer container;
-  array_1d<double, 3> aux_zero_array_value(3, 0.0);
-
-  const std::size_t number_recursions = 10;
-  for (std::size_t i = 0; i < number_recursions; ++i) {
-    container.SetValue(DISPLACEMENT, aux_zero_array_value);
-    container.SetValue(DISPLACEMENT_Y, 1.0);
-    container.SetValue(DISPLACEMENT_Z, 2.0);
-    const auto& displacement = container.GetValue(DISPLACEMENT);
-
-    KRATOS_CHECK_EQUAL(displacement[0], 0.0);
-    KRATOS_CHECK_EQUAL(displacement[1], 1.0);
-    KRATOS_CHECK_EQUAL(displacement[2], 2.0);
   }
 }
 }
