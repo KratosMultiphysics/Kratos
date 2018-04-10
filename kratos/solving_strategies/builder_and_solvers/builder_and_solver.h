f3f86dd21de6de4b85997395c3fde96f1fb7a814
philbucher <philipp.bucher@tum.de>
2018-04-10 13:38:20 +0200

    Using Check Consistently

--- kratos/solving_strategies/builder_and_solvers/builder_and_solver.h
+++ kratos/solving_strategies/builder_and_solvers/builder_and_solver.h
@@ -2,13 +2,13 @@
 //    ' /   __| _` | __|  _ \   __|
 //    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics 
+//                   Multi-Physics
 //
-//  License:		 BSD License 
+//  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
 //  Main authors:    Riccardo Rossi
-//                    
+//
 //
 #if !defined(KRATOS_BUILDER_AND_SOLVER )
 #define  KRATOS_BUILDER_AND_SOLVER
@@ -139,16 +139,16 @@ public:
 
       std::vector<TSystemVectorType> *mpRHS_Element_Components;
       const std::vector< Variable< LocalSystemVectorType > > *mpRHS_Element_Variables;
-      
+
       //conditions
       std::vector<TSystemMatrixType> *mpLHS_Condition_Components;
       const std::vector< Variable< LocalSystemMatrixType > > *mpLHS_Condition_Variables;
 
       std::vector<TSystemVectorType> *mpRHS_Condition_Components;
       const std::vector< Variable< LocalSystemVectorType > > *mpRHS_Condition_Variables;
-      
+
     public:
-      
+
       void Initialize()
       {
 	mpLHS_Element_Components = NULL;
@@ -504,16 +504,12 @@ public:
      */
     virtual void Clear()
     {
-        this->mDofSet.clear(); // = DofsArrayType();
-
+        this->mDofSet.clear();
         TSparseSpace::Clear(this->mpReactionsVector);
-        // 			this->mReactionsVector = TSystemVectorType();
+        this->mpLinearSystemSolver->Clear();
 
-        if (this->GetEchoLevel() > 0)
-        {
-
-            std::cout << "BuilderAndSolver Clear Function called" << std::endl;
-        }
+        KRATOS_INFO_IF("BuilderAndSolver", this->GetEchoLevel() > 0)
+            << "Clear Function called" << std::endl;
     }
 
     /**
