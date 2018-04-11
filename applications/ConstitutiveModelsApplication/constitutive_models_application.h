a30cc847ac5de5cac5c9e09e5b1ef98e75041a78
roigcarlo <roigcarlo@gmail.com>
2018-04-11 11:11:21 +0200

    Exposing application main class

--- applications/ConstitutiveModelsApplication/constitutive_models_application.h
+++ applications/ConstitutiveModelsApplication/constitutive_models_application.h
@@ -128,7 +128,7 @@ namespace Kratos {
   /// Short class definition.
   /** Detail class definition.
    */
-  class KratosConstitutiveModelsApplication : public KratosApplication
+  class KRATOS_API(CONSTITUTIVE_MODELS_APPLICATION) KratosConstitutiveModelsApplication : public KratosApplication
   {
   public:
     ///@name Type Definitions
