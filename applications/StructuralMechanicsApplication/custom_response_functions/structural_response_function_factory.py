0cef48e629fc31b3d456c091a7c1a1bf5c2751bf
arming <armin.geiser@tum.de>
2018-04-11 13:26:14 +0200

    hints for available reponse types

--- applications/StructuralMechanicsApplication/custom_response_functions/structural_response_function_factory.py
+++ applications/StructuralMechanicsApplication/custom_response_functions/structural_response_function_factory.py
@@ -22,8 +22,10 @@ def CreateResponseFunction(response_id, response_settings, model_part):
         elif response_settings["weighting_method"].GetString() == "linear_scaling":
             response_function_utility = StructuralMechanicsApplication.EigenfrequencyResponseFunctionLinScal( model_part, response_settings )
         else:
-            raise NameError("The following weighting_method is not valid for eigenfrequency response: " + response_settings["weighting_method"].GetString())
+            raise NameError("The following weighting_method is not valid for eigenfrequency response: " + response_settings["weighting_method"].GetString() +
+                            ".\nAvailable weighting methods are: 'none', 'linear_scaling'. Default: 'none'")
         return structural_response.SimpleResponseFunctionWrapper(response_id, response_settings, response_function_utility, model_part)
 
     else:
-        raise NameError("The type of the following response function is not specified: " + response_id)
+        raise NameError("The type of the following response function is not specified: "+ response_id +
+                        ".\nAvailable types are: 'mass', 'strain_energy', 'eigenfrequency'." )
