aee1bce1f836555dda9d9cc603ad386fabca262b
dbaumgaertner <daniel.baumgaertner@tum.de>
2018-04-11 10:14:39 +0200

    added airfoil test example

--- applications/ShapeOptimizationApplication/test_examples/05_airfoil_optimization_with_SU2/run_optimization.py
+++ applications/ShapeOptimizationApplication/test_examples/05_airfoil_optimization_with_SU2/run_optimization.py
@@ -7,62 +7,52 @@ from KratosMultiphysics.ShapeOptimizationApplication import *
 
 # Additional imports
 from interface_su2 import InterfaceSU2
+from analyzer_base import AnalyzerBaseClass
 
-# Create and use SU2 translator
-interface_parameters = Parameters("""
-{
-    "su2_related":
-    {
-        "config_file"        : "inv_NACA0012.cfg",
-        "number_of_cores"    : 14,
-        "gradient_method"    : "DISCRETE_ADJOINT",
-        "mesh_file"          : "mesh_NACA0012_inv.su2",
-        "mesh_motion_file"   : "mesh_motion.dat",
-        "design_surface_tag" : "airfoil"
-    },
-    "kratos_related":
-    {
-        "mdpa_file"      : "mesh_NACA0012_inv.mdpa",
-        "write_elements" : false
-    }
-}""")
+# =======================================================================================================
+# Define external analyzer
+# =======================================================================================================
 
-interface_su2 = InterfaceSU2(interface_parameters)
+with open("parameters.json",'r') as parameter_file:
+    parameters = Parameters(parameter_file.read())
 
-print("> Start writing mdpa file...")
-interface_su2.WriteSU2MeshAsMDPA()
+interface_su2 = InterfaceSU2(parameters["su2_interface_settings"])
+# interface_su2.WriteSU2MeshAsMDPA()
 
-# read mdpa from su2
-print("> Start reading mdpa file...")
-mdpa_from_su2 = ModelPart("mesh_NACA0012_inv")
-mdpa_from_su2.ProcessInfo.SetValue(DOMAIN_SIZE, 2)
+# Definition of external analyzer
+class CustomSU2Analyzer(AnalyzerBaseClass):
 
-model_part_io = ModelPartIO(("mesh_NACA0012_inv"))
-model_part_io.ReadModelPart(mdpa_from_su2)
+    # --------------------------------------------------------------------------
+    def __init__( self ):
+        interface_su2.InitializeNewSU2Project()
 
-# Work with su2 interface
-print("\n> Initializing SU2 project...")
-interface_su2.InitializeNewSU2Project()
+    # --------------------------------------------------------------------------------------------------
+    def AnalyzeDesignAndReportToCommunicator(self, current_design, optimization_iteration, communicator):
 
-design_surface = mdpa_from_su2.GetSubModelPart("airfoil")
+        if optimization_iteration == 1:
+            interface_su2.WriteNodesAsSU2MeshMotionFile(current_design.GetNodes())
+        else:
+            previos_iteration = optimization_iteration-1
+            interface_su2.WriteNodesAsSU2MeshMotionFile(current_design.GetNodes(),"DESIGNS/DSN_"+str(previos_iteration).zfill(3))
 
-print("> Start writing SU2 file for mesh motion...")
-interface_su2.WriteNodesAsSU2MeshMotionFile(design_surface.GetNodes())
+        if communicator.isRequestingValueOf("drag"):
+            update_mesh = True
+            [value] = interface_su2.ComputeValues(["DRAG"], update_mesh, optimization_iteration)
+            communicator.reportValue("drag", value)
 
-for iteration in range(1,4):
+        if communicator.isRequestingGradientOf("drag"):
+            update_mesh = False
+            gradient = interface_su2.ComputeGradient(["DRAG"], update_mesh, optimization_iteration)
+            communicator.reportGradient("drag", gradient)
 
-    print("#############################################")
-    print("> Starting iteration ",iteration)
-    print("#############################################")
 
-    print("> Starting SU2 response calculations...")
-    drag,lift = interface_su2.ComputeValues(["DRAG","LIFT"],True)
+# =======================================================================================================
+# Perform optimization
+# =======================================================================================================
 
-    print("> Start writing SU2 file for mesh motion...")
-    for node in design_surface.Nodes:
-        node.Y0 = node.Y0+0.1*iteration*node.X0
-    interface_su2.WriteNodesAsSU2MeshMotionFile(design_surface.GetNodes(),"DESIGNS/DSN_00"+str(iteration))
+optimization_model_part = ModelPart(parameters["optimization_settings"]["design_variables"]["optimization_model_part_name"].GetString())
+optimization_model_part.ProcessInfo.SetValue(DOMAIN_SIZE, parameters["optimization_settings"]["design_variables"]["domain_size"].GetInt())
 
-    # print("> Starting SU2 gradient calculations...")
-    # ddrag = interface_su2.ComputeGradients(["DRAG"],False,True)
-    # ddlift = interface_su2.ComputeGradients(["LIFT"],False,True)
+import optimizer_factory
+optimizer = optimizer_factory.CreateOptimizer(parameters, optimization_model_part, CustomSU2Analyzer())
+optimizer.Optimize()
