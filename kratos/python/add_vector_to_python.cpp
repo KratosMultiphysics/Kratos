6dd728ea4f002eadcf768f854176dc38fab63d74
jcotela <jcotela@cimne.upc.edu>
2018-04-12 12:15:26 +0200

    Adding slicing and tests.

--- kratos/python/add_vector_to_python.cpp
+++ kratos/python/add_vector_to_python.cpp
@@ -33,7 +33,7 @@ namespace Python
 
     template< typename TVectorType > class_< TVectorType > CreateVectorInterface(pybind11::module& m, std::string Name )
         {
-            
+
         class_< TVectorType, std::shared_ptr<TVectorType> > binder(m,Name.c_str());
         binder.def(init<>());
 
@@ -41,16 +41,16 @@ namespace Python
         binder.def("Size", [](const TVectorType& self){return self.size();} );
         binder.def("Resize", [](TVectorType& self, const typename TVectorType::size_type  new_size){if(self.size() != new_size) self.resize(new_size, false);} );
         binder.def("__len__", [](const TVectorType& self){return self.size();} );
-        
+
         //operating on the object itself, +=, -=, *=, etc
         binder.def("__iadd__", [](TVectorType& self, const double scalar){for(unsigned int i=0; i<self.size(); ++i) self[i]+=scalar; return self;}, is_operator());
         binder.def("__isub__", [](TVectorType& self, const double scalar){for(unsigned int i=0; i<self.size(); ++i) self[i]-=scalar; return self;}, is_operator());
         binder.def("__imul__", [](TVectorType& self, const double scalar){for(unsigned int i=0; i<self.size(); ++i) self[i]*=scalar; return self;}, is_operator());
         binder.def("__itruediv__", [](TVectorType& self, const double scalar){for(unsigned int i=0; i<self.size(); ++i) self[i]/=scalar; return self;}, is_operator());
-        
+
         binder.def("__iadd__", [](TVectorType& self, const TVectorType& other_vec){noalias(self) += other_vec; return self;}, is_operator());
         binder.def("__isub__", [](TVectorType& self, const TVectorType& other_vec){noalias(self) -= other_vec; return self; }, is_operator());
-       
+
         //returning a different object
 //         binder.def("__add__", [](TVectorType vec1, const double scalar){for(unsigned int i=0; i<vec1.size(); ++i) vec1[i]+=scalar; return vec1;}, is_operator());
 //         binder.def("__sub__", [](TVectorType vec1, const double scalar){for(unsigned int i=0; i<vec1.size(); ++i) vec1[i]-=scalar; return vec1;}, is_operator());
@@ -62,14 +62,36 @@ namespace Python
          binder.def("__rdiv__", [](TVectorType vec1, const double scalar){for(unsigned int i=0; i<vec1.size(); ++i) vec1[i]/=scalar;}, is_operator());
         binder.def("__add__", [](const TVectorType& vec1, const TVectorType& vec2){Vector aux(vec1); aux += vec2; return aux;}, is_operator());
         binder.def("__sub__", [](const TVectorType& vec1, const TVectorType& vec2){Vector aux(vec1); aux -= vec2; return aux;}, is_operator());
-         
+
         //access operators
         binder.def("__setitem__", [](TVectorType& self, const unsigned int i, const typename TVectorType::value_type value){self[i] = value;} );
         binder.def("__getitem__", [](const TVectorType& self, const unsigned int i){return self[i];} );
-        
+
+        //access using slices
+        binder.def("__setitem__", [](TVectorType &self, pybind11::slice this_slice, const TVectorType &value) {
+            size_t start, stop, step, slicelength;
+            if (!this_slice.compute(self.size(), &start, &stop, &step, &slicelength))
+                throw pybind11::error_already_set();
+            if (slicelength != value.size())
+                throw std::runtime_error("Left and right hand size of slice assignment have different sizes!");
+            for (size_t i = 0; i < slicelength; ++i) {
+                self[start] = value[i]; start += step;
+            }
+        });
+        binder.def("__getitem__", [](const TVectorType &self, pybind11::slice this_slice) -> TVectorType* {
+            size_t start, stop, step, slicelength;
+            if (!this_slice.compute(self.size(), &start, &stop, &step, &slicelength))
+                throw pybind11::error_already_set();
+            TVectorType *sliced_self = new TVectorType(slicelength);
+            for (size_t i = 0; i < slicelength; ++i) {
+                (*sliced_self)[i] = self[start]; start += step;
+            }
+            return sliced_self;
+        });
+
         binder.def("__iter__", [](TVectorType& self){ return make_iterator(self.begin(), self.end(), return_value_policy::reference_internal); } , keep_alive<0,1>() ) ;
-        binder.def("__repr__", [](const TVectorType& self) -> const std::string { std::stringstream ss;  ss << self; const std::string out = ss.str();  return out; }); 
-        
+        binder.def("__repr__", [](const TVectorType& self) -> const std::string { std::stringstream ss;  ss << self; const std::string out = ss.str();  return out; });
+
         return binder;
         }
 
@@ -92,8 +114,8 @@ namespace Python
 
 
 
-        
-        
+
+
         auto array3_binder = CreateVectorInterface< Kratos::array_1d<double,3> >(m, "Array3");
         array3_binder.def(init( [](double value){
                                 array_1d<double,3> tmp;
@@ -104,7 +126,7 @@ namespace Python
         array3_binder.def(init( [](const Vector& input){
                                 if(input.size() != 3)
                                     KRATOS_ERROR << "expected size should be 3 when constructing an Array3. Provide Input  size is: " << input.size() << std::endl;
-                            
+
                                 array_1d<double,3> tmp(input);
                                 return tmp;
                                 })   );
@@ -112,7 +134,7 @@ namespace Python
         array3_binder.def(init( [](const list& input){
                                 if(input.size() != 3)
                                     KRATOS_ERROR << "expected size should be 3 when constructing an Array3. Provide Input  size is: " << input.size() << std::endl;
-                            
+
                                 array_1d<double,3> tmp;
                                 for(unsigned int i=0; i<3; ++i)
                                     tmp[i] = cast<double>(input[i]);
