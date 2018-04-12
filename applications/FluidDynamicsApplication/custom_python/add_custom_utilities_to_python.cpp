8b89c817e417f691875b667acaecd03703318057
reginadicuori <elisa.magliozzi@gmail.com>
2018-04-12 18:32:57 +0200

    Adding utility to python

--- applications/FluidDynamicsApplication/custom_python/add_custom_utilities_to_python.cpp
+++ applications/FluidDynamicsApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -31,6 +31,7 @@
 #include "custom_utilities/fractional_step_settings.h"
 #include "custom_utilities/integration_point_to_node_transformation_utility.h"
 #include "custom_utilities/periodic_condition_utilities.h"
+#include "custom_utilities/compressible_element_rotation_utility.h"
 
 #include "utilities/split_tetrahedra.h"
 
@@ -150,6 +151,13 @@ void  AddCustomUtilitiesToPython(pybind11::module& m)
         .def("CalculateBodyFittedDrag", &DragUtilities::CalculateBodyFittedDrag)
         .def("CalculateEmbeddedDrag", &DragUtilities::CalculateEmbeddedDrag)
         ;
+
+    class_<CompressibleElementRotationUtility<LocalSpaceType::MatrixType,LocalSpaceType::VectorType>,
+            CompressibleElementRotationUtility<LocalSpaceType::MatrixType,LocalSpaceType::VectorType>::Pointer,
+            bases< CoordinateTransformationUtils<LocalSpaceType::MatrixType,LocalSpaceType::VectorType,double> >>
+    (m,"CompressibleElementRotationUtility")
+    .def(init<const unsigned int,const Variable<double>&>())
+    ;
 
 }
 
