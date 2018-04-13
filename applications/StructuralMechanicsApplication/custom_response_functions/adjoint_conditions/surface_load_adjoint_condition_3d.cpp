ddfbdedefb2b664fcfbd97e0c80221ba9b20b896
MFusseder <martin.fusseder@tum.de>
2018-04-13 15:55:44 +0200

    some renamings acccording to style guide

--- applications/StructuralMechanicsApplication/custom_response_functions/adjoint_conditions/surface_load_adjoint_condition_3d.cpp
+++ applications/StructuralMechanicsApplication/custom_response_functions/adjoint_conditions/surface_load_adjoint_condition_3d.cpp
@@ -84,18 +84,18 @@ namespace Kratos
     {
         KRATOS_TRY
 
-        const unsigned int NumberOfNodes = GetGeometry().size();
+        const unsigned int number_of_nodes = GetGeometry().size();
         const unsigned int dim = GetGeometry().WorkingSpaceDimension();
-        if (rResult.size() != dim * NumberOfNodes)
+        if (rResult.size() != dim * number_of_nodes)
         {
-            rResult.resize(dim*NumberOfNodes,false);
+            rResult.resize(dim*number_of_nodes,false);
         }
 
         const unsigned int pos = this->GetGeometry()[0].GetDofPosition(ADJOINT_DISPLACEMENT_X);
 
         if(dim == 2)
         {
-            for (unsigned int i = 0; i < NumberOfNodes; ++i)
+            for (unsigned int i = 0; i < number_of_nodes; ++i)
             {
                 const unsigned int index = i * 2;
                 rResult[index    ] = GetGeometry()[i].GetDof(ADJOINT_DISPLACEMENT_X,pos    ).EquationId();
@@ -104,7 +104,7 @@ namespace Kratos
         }
         else
         {
-            for (unsigned int i = 0; i < NumberOfNodes; ++i)
+            for (unsigned int i = 0; i < number_of_nodes; ++i)
             {
                 const unsigned int index = i * 3;
                 rResult[index    ] = GetGeometry()[i].GetDof(ADJOINT_DISPLACEMENT_X,pos    ).EquationId();
@@ -121,14 +121,14 @@ namespace Kratos
     {
         KRATOS_TRY
 
-        const unsigned int NumberOfNodes = GetGeometry().size();
+        const unsigned int number_of_nodes = GetGeometry().size();
         const unsigned int dim =  GetGeometry().WorkingSpaceDimension();
         ElementalDofList.resize(0);
-        ElementalDofList.reserve(dim * NumberOfNodes);
+        ElementalDofList.reserve(dim * number_of_nodes);
 
         if(dim == 2)
         {
-            for (unsigned int i = 0; i < NumberOfNodes; ++i)
+            for (unsigned int i = 0; i < number_of_nodes; ++i)
             {
                 ElementalDofList.push_back( GetGeometry()[i].pGetDof(ADJOINT_DISPLACEMENT_X));
                 ElementalDofList.push_back( GetGeometry()[i].pGetDof(ADJOINT_DISPLACEMENT_Y));
@@ -136,7 +136,7 @@ namespace Kratos
         }
         else
         {
-            for (unsigned int i = 0; i < NumberOfNodes; ++i)
+            for (unsigned int i = 0; i < number_of_nodes; ++i)
             {
                 ElementalDofList.push_back( GetGeometry()[i].pGetDof(ADJOINT_DISPLACEMENT_X));
                 ElementalDofList.push_back( GetGeometry()[i].pGetDof(ADJOINT_DISPLACEMENT_Y));
@@ -152,16 +152,16 @@ namespace Kratos
 
     void SurfaceLoadAdjointCondition3D::GetValuesVector(Vector& rValues, int Step)
     {
-        const unsigned int NumberOfNodes = GetGeometry().size();
+        const unsigned int number_of_nodes = GetGeometry().size();
         const unsigned int dim = GetGeometry().WorkingSpaceDimension();
-        const unsigned int MatSize = NumberOfNodes * dim;
+        const unsigned int MatSize = number_of_nodes * dim;
 
         if (rValues.size() != MatSize)
         {
             rValues.resize(MatSize, false);
         }
 
-        for (unsigned int i = 0; i < NumberOfNodes; i++)
+        for (unsigned int i = 0; i < number_of_nodes; i++)
         {
             const array_1d<double, 3 > & Displacement = GetGeometry()[i].FastGetSolutionStepValue(ADJOINT_DISPLACEMENT, Step);
             unsigned int index = i * dim;
@@ -180,9 +180,9 @@ namespace Kratos
     {
         KRATOS_TRY
 
-        const unsigned int NumberOfNodes = GetGeometry().size();
-        const unsigned int Dimension = GetGeometry().WorkingSpaceDimension();
-        const unsigned int MatSize = NumberOfNodes * Dimension;
+        const unsigned int number_of_nodes = GetGeometry().size();
+        const unsigned int dimension = GetGeometry().WorkingSpaceDimension();
+        const unsigned int MatSize = number_of_nodes * dimension;
 
         if ( rLeftHandSideMatrix.size1() != MatSize )
             rLeftHandSideMatrix.resize( MatSize, MatSize, false );
@@ -333,7 +333,7 @@ namespace Kratos
         {
             KRATOS_ERROR <<  "DISPLACEMENT has Key zero! (check if the application is correctly registered" << std::endl;
         }
-         if ( DISPLACEMENT.Key() == 0 )
+        if ( DISPLACEMENT.Key() == 0 )
         {
             KRATOS_ERROR <<  "DISPLACEMENT has Key zero! (check if the application is correctly registered" << std::endl;
         }
