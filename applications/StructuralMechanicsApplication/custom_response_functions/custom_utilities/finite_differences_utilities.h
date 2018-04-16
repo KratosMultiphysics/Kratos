aaf4ff434a5aae9c09f24e1347d58c1f2baa33f0
MFusseder <martin.fusseder@tum.de>
2018-04-16 13:28:55 +0200

    Modification of the header

--- applications/StructuralMechanicsApplication/custom_response_functions/custom_utilities/finite_differences_utilities.h
+++ applications/StructuralMechanicsApplication/custom_response_functions/custom_utilities/finite_differences_utilities.h
@@ -1,13 +1,15 @@
-// ==============================================================================
-//  KratosStructuralMechanicsApplication
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
 //
-//  License:         BSD License
-//                   license: StructuralMechanicsApplication/license.txt
+//  License:		 BSD License
+//					 license: structural_mechanics_application/license.txt
 //
-//  Main authors:    Fusseder Martin, https://github.com/MFusseder
-//
-// ==============================================================================
-//TODO: check what is really necessary to include!!
+//  Main authors:    Martin Fusseder, https://github.com/MFusseder 
+//   
+
+
 #ifndef FINITE_DIFFERENCES_UTILITIES_H
 #define FINITE_DIFFERENCES_UTILITIES_H
 
