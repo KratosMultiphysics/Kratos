6ec555f0a528d24a1148edbf81380e86f4e4011e
philbucher <philipp.bucher@tum.de>
2018-04-20 09:17:48 +0200

    bugfixes in BaseAnalysis

--- kratos/python_scripts/base_analysis_stage.py
+++ kratos/python_scripts/base_analysis_stage.py
@@ -49,10 +49,10 @@ class AnalysisStage(object):
             process.ExecuteBeforeSolutionLoop()
 
         while self.time < self.end_time:
-            solver.AdvanceInTime(self.time)
+            self.time = self.solver.AdvanceInTime(self.time)
             self.InitializeSolutionStep()
-            solver.Predict()
-            solver.SolveSolutionStep()
+            self.solver.Predict()
+            self.solver.SolveSolutionStep()
             self.FinalizeSolutionStep()
             self.OutputSolutionStep()
 
@@ -63,7 +63,7 @@ class AnalysisStage(object):
         Usage: It is designed to be called ONCE, BEFORE the execution of the solution-loop
         This function has to be implemented in deriving classes!
         """
-        solver.ImportModelPart()
+        self.solver.ImportModelPart()
         self.ModifyInitialProperties()
         self.ModifyInitialGeometry()
 
@@ -71,7 +71,7 @@ class AnalysisStage(object):
         for process in self.list_of_processes:
             process.ExecuteInitialize()
 
-        solver.Initialize()
+        self.solver.Initialize()
 
 
     def Finalize(self):
@@ -87,13 +87,15 @@ class AnalysisStage(object):
         """
         self.ApplyBoundaryConditions() #here the processes are called
         self.ChangeMaterialProperties() #this is normally empty
-        solver.InitializeSolutionStep()
+        self.solver.InitializeSolutionStep()
 
 
     def FinalizeSolutionStep(self):
         """This function performs all the required operations that should be executed
         (for each step) AFTER solving the solution step.
         """
+        self.solver.FinalizeSolutionStep()
+
         for process in self.list_of_processes:
             process.ExecuteFinalizeSolutionStep()
 
