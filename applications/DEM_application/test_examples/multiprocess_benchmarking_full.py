f9799f133f87ff1e7e82a92fc00c4aaa2799bc90
Miguel Angel <maceli@cimne.upc.edu>
2018-04-24 19:40:11 +0200

    Deletes files even if Benchmarks stop prematurely

--- applications/DEM_application/test_examples/multiprocess_benchmarking_full.py
+++ applications/DEM_application/test_examples/multiprocess_benchmarking_full.py
@@ -96,88 +96,92 @@ def worker(queue):
             g.close()
 
 def main():
-    print("\nAdding processes to DEM parallel Benchmarking..............\n")
-    g = open("errors.err", "w")
-    g.write("The complete list of benchmarks are included at the end of this message as a quick reference.\n")
-    g.write("\n========== DEM BENCHMARKING RESULTS ==========\n")
-    g.write("\n=========== DEM DISCONTINUUM TESTS ===========\n")
-    g.write("\n==== TSUJI PAPER BENCHMARKS. SLIDING REGIME ==\n\n")
-    g.close()
-    Text = ""
-    failure = False
+    try:
+        print("\nAdding processes to DEM parallel Benchmarking..............\n")
+        g = open("errors.err", "w")
+        g.write("The complete list of benchmarks are included at the end of this message as a quick reference.\n")
+        g.write("\n========== DEM BENCHMARKING RESULTS ==========\n")
+        g.write("\n=========== DEM DISCONTINUUM TESTS ===========\n")
+        g.write("\n==== TSUJI PAPER BENCHMARKS. SLIDING REGIME ==\n\n")
+        g.close()
+        Text = ""
+        failure = False
 
-    q = queue.Queue()
+        q = queue.Queue()
 
-    #Discontinuum Tests. From 1 to 17
-    D_DEM_Benchmarks_list = list(range(1,18))
+        #Discontinuum Tests. From 1 to 17
+        D_DEM_Benchmarks_list = list(range(1,18))
 
-    #Continuum Tests
-    C_DEM_Benchmarks_list = list(range(20,26))
+        #Continuum Tests
+        C_DEM_Benchmarks_list = list(range(20,26))
 
-    #Discontinuum Clusters Tests. From 30 to 33
-    Dcl_DEM_Benchmarks_list = list(range(30,34))
+        #Discontinuum Clusters Tests. From 30 to 33
+        Dcl_DEM_Benchmarks_list = list(range(30,34))
 
-    Total_DEM_Benchmarks_list = D_DEM_Benchmarks_list + C_DEM_Benchmarks_list + Dcl_DEM_Benchmarks_list
+        Total_DEM_Benchmarks_list = D_DEM_Benchmarks_list + C_DEM_Benchmarks_list + Dcl_DEM_Benchmarks_list
 
-    for item in Total_DEM_Benchmarks_list:
-        #print(Benchmark_text[item - 1])
-        q.put_nowait(item)
+        for item in Total_DEM_Benchmarks_list:
+            #print(Benchmark_text[item - 1])
+            q.put_nowait(item)
 
-    threads = [Thread(target=worker, args=(q,)) for _ in range(int(initial_number_of_threads))]
-    for t in threads:
-        t.daemon = True # threads die if the program dies
-        t.start()
-    for _ in threads: q.put_nowait(None) # signal no more files
-    for t in threads: t.join() # wait for completion
+        threads = [Thread(target=worker, args=(q,)) for _ in range(int(initial_number_of_threads))]
+        for t in threads:
+            t.daemon = True # threads die if the program dies
+            t.start()
+        for _ in threads: q.put_nowait(None) # signal no more files
+        for t in threads: t.join() # wait for completion
 
-    print('\n')
-    g = open("errors.err", 'a')
-    g.write("\n---------------------------------------------------------------------\n")
-    g.write("\nList of Benchmarks:\n")
-    g.write("\nDISCONTINUUM TESTS:\n")
-    g.write("Benchmark 01. Elastic normal impact of two identical spheres\n")
-    g.write("Benchmark 02. Elastic normal impact of a sphere against a rigid plane\n")
-    g.write("Benchmark 03. Impact of a sphere against a rigid plane with different coefficients of restitution\n")
-    g.write("Benchmark 04. Oblique impact of a sphere with a rigid plane with constant velocity module and variable incident angles\n")
-    g.write("Benchmark 05. Oblique impact of a sphere with a rigid plane with constant normal velocity and different tangential velocities\n")
-    g.write("Benchmark 06. Impact of a sphere with a rigid plane with a constant normal velocity and variable angular velocities\n")
-    g.write("Benchmark 07. Impact of two identical spheres with a constant normal velocity and different angular velocities\n")
-    g.write("Benchmark 08. Impact of two differently sized spheres with a constant normal velocity and variable angular velocities\n")
-    g.write("Benchmark 09. Impact of two identical spheres with a constant normal velocity and different coefficients of restitution\n")
-    g.write("Benchmark 10. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
-    g.write("Benchmark 11. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
-    g.write("Benchmark 12. Sphere rotating over a plane surface with Rolling Friction\n")
-    g.write("Benchmark 13. Impact of a low stiffness sphere against a rigid plane divided in small triangular elements\n")
-    g.write("Benchmark 14. Impact of a low stiffness sphere against a rigid edge divided in small triangular elements\n")
-    g.write("Benchmark 15. Impact of a low stiffness sphere against a rigid vertex divided in small triangular elements\n")
-    g.write("Benchmark 16. Spheres contacting multiple entities (facets, edges and vertices)\n")
-    g.write("Benchmark 17. Sphere sliding on a plane (discretized with triangles and quadrilaterals) with friction\n")
-    g.write("\nCONTINUUM TESTS:\n")
-    g.write("Benchmark 20. Normal compression of two identical spheres\n")
-    g.write("Benchmark 21. Normal compression of two identical indented spheres\n")
-    g.write("Benchmark 22. Tensile test of two identical spheres\n")
-    g.write("Benchmark 23. Tensile test of two identical indented spheres\n")
-    g.write("Benchmark 24. Shear test of two identical spheres by applying rotation\n")
-    g.write("Benchmark 25. Shear test of two identical spheres by applying rotation and radius expansion\n")
-    g.write("\nDISCONTINUUM CLUSTERS TESTS:\n")
-    g.write("Benchmark 30. Cylinder cluster with imposed angular velocity in two axis (Velocity Verlet + Zhao scheme)\n")
-    g.write("Benchmark 31. Cylinder cluster with imposed angular velocity in two axis (Symplectic Euler + Runge-Kutta scheme)\n")
-    g.write("Benchmark 32. Fiber cluster bouncing without any damping (Velocity Verlet + Zhao scheme)\n")
-    g.write("Benchmark 33. Fiber cluster bouncing without any damping (Symplectic Euler + Runge-Kutta scheme)\n")
-    g.close()
+        print('\n')
+        g = open("errors.err", 'a')
+        g.write("\n---------------------------------------------------------------------\n")
+        g.write("\nList of Benchmarks:\n")
+        g.write("\nDISCONTINUUM TESTS:\n")
+        g.write("Benchmark 01. Elastic normal impact of two identical spheres\n")
+        g.write("Benchmark 02. Elastic normal impact of a sphere against a rigid plane\n")
+        g.write("Benchmark 03. Impact of a sphere against a rigid plane with different coefficients of restitution\n")
+        g.write("Benchmark 04. Oblique impact of a sphere with a rigid plane with constant velocity module and variable incident angles\n")
+        g.write("Benchmark 05. Oblique impact of a sphere with a rigid plane with constant normal velocity and different tangential velocities\n")
+        g.write("Benchmark 06. Impact of a sphere with a rigid plane with a constant normal velocity and variable angular velocities\n")
+        g.write("Benchmark 07. Impact of two identical spheres with a constant normal velocity and different angular velocities\n")
+        g.write("Benchmark 08. Impact of two differently sized spheres with a constant normal velocity and variable angular velocities\n")
+        g.write("Benchmark 09. Impact of two identical spheres with a constant normal velocity and different coefficients of restitution\n")
+        g.write("Benchmark 10. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
+        g.write("Benchmark 11. Oblique impact of a sphere with an elastic plane with constant normal velocity and different angular velocities\n")
+        g.write("Benchmark 12. Sphere rotating over a plane surface with Rolling Friction\n")
+        g.write("Benchmark 13. Impact of a low stiffness sphere against a rigid plane divided in small triangular elements\n")
+        g.write("Benchmark 14. Impact of a low stiffness sphere against a rigid edge divided in small triangular elements\n")
+        g.write("Benchmark 15. Impact of a low stiffness sphere against a rigid vertex divided in small triangular elements\n")
+        g.write("Benchmark 16. Spheres contacting multiple entities (facets, edges and vertices)\n")
+        g.write("Benchmark 17. Sphere sliding on a plane (discretized with triangles and quadrilaterals) with friction\n")
+        g.write("\nCONTINUUM TESTS:\n")
+        g.write("Benchmark 20. Normal compression of two identical spheres\n")
+        g.write("Benchmark 21. Normal compression of two identical indented spheres\n")
+        g.write("Benchmark 22. Tensile test of two identical spheres\n")
+        g.write("Benchmark 23. Tensile test of two identical indented spheres\n")
+        g.write("Benchmark 24. Shear test of two identical spheres by applying rotation\n")
+        g.write("Benchmark 25. Shear test of two identical spheres by applying rotation and radius expansion\n")
+        g.write("\nDISCONTINUUM CLUSTERS TESTS:\n")
+        g.write("Benchmark 30. Cylinder cluster with imposed angular velocity in two axis (Velocity Verlet + Zhao scheme)\n")
+        g.write("Benchmark 31. Cylinder cluster with imposed angular velocity in two axis (Symplectic Euler + Runge-Kutta scheme)\n")
+        g.write("Benchmark 32. Fiber cluster bouncing without any damping (Velocity Verlet + Zhao scheme)\n")
+        g.write("Benchmark 33. Fiber cluster bouncing without any damping (Symplectic Euler + Runge-Kutta scheme)\n")
+        g.close()
 
-    if 'FAILED' in open('errors.err').read():
-        failure = True
+        if 'FAILED' in open('errors.err').read():
+            failure = True
 
-    g = open("errors.err")
-    file_contents = g.read()
-    g.close()
-    os.remove("errors.err")
+        g = open("errors.err")
+        file_contents = g.read()
+        g.close()
+        os.remove("errors.err")
 
-    Text += file_contents.rstrip("\n")
-    Text += "\n\n\n"
-    delete_archives()
-    return Text
+        Text += file_contents.rstrip("\n")
+        Text += "\n\n\n"
+        delete_archives()
+        return Text
+
+    except:
+        delete_archives()
 
 
 def delete_archives():
@@ -189,6 +193,7 @@ def delete_archives():
     files_to_delete_list.extend(glob('*.txt'))
     files_to_delete_list.extend(glob('*.lst'))
     files_to_delete_list.extend(glob('*.info'))
+    files_to_delete_list.extend(glob('*.err'))
 
     for to_erase_file in files_to_delete_list:
         os.remove(to_erase_file)
