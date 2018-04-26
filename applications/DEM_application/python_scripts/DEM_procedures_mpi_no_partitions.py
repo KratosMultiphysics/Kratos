3d50726d8e63ff97dfd0a3dabbcdb0ba48347903
Miguel Angel <maceli@cimne.upc.edu>
2018-04-26 17:38:12 +0200

    Codacy suggestions

--- applications/DEM_application/python_scripts/DEM_procedures_mpi_no_partitions.py
+++ applications/DEM_application/python_scripts/DEM_procedures_mpi_no_partitions.py
@@ -7,8 +7,7 @@ from KratosMultiphysics.mpi import *
 import DEM_procedures
 import DEM_procedures_mpi
 
-class PostUtils(DEM_procedures.PostUtils):
-    pass
+PostUtils = DEM_procedures.PostUtils
 
 class Procedures(DEM_procedures_mpi.Procedures):
     def AddMpiVariables(self, model_part):
@@ -17,41 +16,20 @@ class Procedures(DEM_procedures_mpi.Procedures):
     def FindMaxNodeIdInModelPart(self, model_part):
         DEM_procedures.Procedures.FindMaxNodeIdInModelPart(model_part)
 
-class MultifileList(object):
-    def __init__(self,name,step):
+class MultifileList(DEM_procedures.MultifileList):
+    def __init__(self, name, step):
         if (mpi.rank == 0):
             DEM_procedures.MultifileList.__init__(name,step)
 
-class DEMFEMProcedures(DEM_procedures_mpi.DEMFEMProcedures):
-    pass
-
-class Report(DEM_procedures.Report):
-    pass
-    '''
-    def __init__(self):
-        super(Report,self).__init__()
-        '''
-
-class MaterialTest(DEM_procedures.MaterialTest):
-    pass
-    '''
-    def __init__(self):
-        super(MaterialTest,self).__init__()
-        '''
+DEMFEMProcedures = DEM_procedures_mpi.DEMFEMProcedures
+Report = DEM_procedures.Report
+MaterialTest = DEM_procedures.MaterialTest
 
 class DEMIo(DEM_procedures.DEMIo):
     def PrintResults(self, all_model_parts, creator_destructor, dem_fem_search, time, bounding_box_time_limits):
         if (mpi.rank == 0):
             super(DEMIo,self).PrintResults(all_model_parts, creator_destructor, dem_fem_search, time, bounding_box_time_limits)
 
-class ParallelUtils(DEM_procedures.ParallelUtils):
-    pass
-    '''
-    def __init__(self):
-        super(ParallelUtils,self).__init__()
-        '''
-class SetOfModelParts(DEM_procedures.SetOfModelParts):
-    pass
-
-class DEMEnergyCalculator(DEM_procedures.DEMEnergyCalculator):
-    pass
+ParallelUtils = DEM_procedures.ParallelUtils
+SetOfModelParts = DEM_procedures.SetOfModelParts
+DEMEnergyCalculator = DEM_procedures.DEMEnergyCalculator
