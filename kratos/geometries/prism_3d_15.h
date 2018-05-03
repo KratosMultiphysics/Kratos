f52d92253ad664b6eedb4905c26b3bd9c9782762
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-05-03 15:00:12 +0200

    Changing vector to DenseVector

--- kratos/geometries/prism_3d_15.h
+++ kratos/geometries/prism_3d_15.h
@@ -707,7 +707,7 @@ public:
         Matrix ShapeFunctionsGradientInIntegrationPoint =
             shape_functions_gradients( IntegrationPointIndex );
         //values of shape functions in integration points
-        vector<double> ShapeFunctionsValuesInIntegrationPoint = ZeroVector( 15 );
+        DenseVector<double> ShapeFunctionsValuesInIntegrationPoint = ZeroVector( 15 );
         /*vector<double>*/
         ShapeFunctionsValuesInIntegrationPoint =
             row( CalculateShapeFunctionsIntegrationPointsValues( ThisMethod ), IntegrationPointIndex );
