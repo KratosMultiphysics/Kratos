d449319f312d79fa9d8f89a5b842b4ced9ded24b
sunethwarna <suneth.warna@gmail.com>
2018-05-03 12:02:42 +0200

    changed to new header

--- applications/AdjointFluidApplication/custom_utilities/numerical_diffusion.h
+++ applications/AdjointFluidApplication/custom_utilities/numerical_diffusion.h
@@ -1,10 +1,15 @@
-//  KratosAdjointFluidApplication
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
 //  License:		 BSD License
-//					 license: AdjointFluidApplication/license.txt
+//					 license: KratosAdjointFluidApplication/license.txt
 //
-//  Main authors:    Suneth Warnakulasriya, https://github.com/sunethwarna
+//  Main author:    Suneth Warnakulasriya, https://github.com/sunethwarna
 //
+
 
 #if !defined(KRATOS_NUMERICAL_DIFFUSION)
 #define KRATOS_NUMERICAL_DIFFUSION
