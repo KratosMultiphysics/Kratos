000c5ac5b4df6e900bf6cdba0f85c529e5ea40d3
jcotela <jcotela@cimne.upc.edu>
2018-05-11 14:57:14 +0200

    Updating license header (with permission).

--- applications/FluidDynamicsApplication/custom_response_functions/drag_response_function.h
+++ applications/FluidDynamicsApplication/custom_response_functions/drag_response_function.h
@@ -1,7 +1,11 @@
-//  KratosAdjointFluidApplication
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
-//  License:		 BSD License
-//					 license: AdjointFluidApplication/license.txt
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Michael Andre, https://github.com/msandre
 //
