2fb45268c6b928727966f7de2008912125f6a59b
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-05-11 12:19:12 +0200

    Removing deprecated and unused buffer check

--- applications/FluidDynamicsApplication/python_scripts/navier_stokes_embedded_fm_ale_solver.py
+++ applications/FluidDynamicsApplication/python_scripts/navier_stokes_embedded_fm_ale_solver.py
@@ -137,14 +137,11 @@ class NavierStokesEmbeddedFMALEMonolithicSolver(navier_stokes_embedded_solver.Na
             # Compute the BDF coefficients
             (self.bdf_process).Execute()
 
-            #TODO: REMOVE THIS IF STATEMENT ONCE #1904 IS MERGED
-            step = self.main_model_part.ProcessInfo[KratosMultiphysics.STEP]
-            if(step + 1 >= self.GetMinimumBufferSize()):
-                # Perform the FM-ALE operations
-                if (self._is_fm_ale_step()):
-                    self._do_fm_ale_operations()
-                else:
-                    self.fm_ale_step += 1
+            # Perform the FM-ALE operations
+            if (self._is_fm_ale_step()):
+                self._do_fm_ale_operations()
+            else:
+                self.fm_ale_step += 1
 
             # Fluid solver step initialization
             (self.solver).InitializeSolutionStep()
