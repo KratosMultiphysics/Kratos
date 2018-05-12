642e699c096b7770c3ee31dafd48b3752ff029d7
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-05-12 23:20:02 +0200

    Moving header only
    I am weak

--- kratos/utilities/process_factory_utility.h
+++ kratos/utilities/process_factory_utility.h
@@ -79,14 +79,25 @@ public:
      * @brief Constructor using a list of processes
      * @param ProcessesList List of processes that will be used to build the vector of processes
      */
-    ProcessFactoryUtility(ListType& ProcessesList);
+    ProcessFactoryUtility(ListType& ProcessesList)
+    {
+        const std::size_t size_processes = len(ProcessesList);
+        mProcesses.resize(size_processes);
+        for (std::size_t i_process = 0; i_process < size_processes; ++i_process)
+            mProcesses[i_process] = pybind11::cast<ObjectType>(ProcessesList[i_process]);
+    }
 
     /**
      * @brief Constructor using just one process
      * @param rProcess The process that will be added  at the begining of the vector of processes
      * @note This constructor overrides the previous ones ("everything" is an object, so here I try first to work as it is a list)
      */
-    ProcessFactoryUtility(ObjectType& rProcess);
+    ProcessFactoryUtility(ObjectType& rProcess)
+    {
+        mProcesses.resize(1);
+        mProcesses[0] = rProcess;
+    }
+
 
     /// Destructor.
     virtual ~ProcessFactoryUtility()= default;
@@ -109,81 +120,123 @@ public:
      * @param rProcess The process that will be appended at the vector of processes
      */
 
-    void AddProcess(ObjectType& rProcess);
+    void AddProcess(ObjectType& rProcess)
+    {
+        mProcesses.push_back(rProcess);
+    }
 
     /**
      * @brief It add new processes to the existing process list
      * @param ProcessesList List of processes that will be appended the vector of processes
      */
 
-    void AddProcesses(ListType& ProcessesList);
+    void AddProcesses(ListType& ProcessesList)
+    {
+        const std::size_t size_processes = len(ProcessesList);
+        for (std::size_t i_process = 0; i_process < size_processes; ++i_process)
+            mProcesses.push_back(pybind11::cast<ObjectType>(ProcessesList[i_process]));
+    }
 
     /**
      * @brief It executes the method considered in the input
      * @param rNameMethod The method to be executed
      */
 
-    void ExecuteMethod(const std::string& rNameMethod);
+    void ExecuteMethod(const std::string& rNameMethod)
+    {
+        for (auto& process : mProcesses)
+            process.attr(rNameMethod.c_str())();
+    }
 
     /**
      * @brief It executes the ExecuteInitialize() from the list of processes
      */
 
-    void ExecuteInitialize();
+    void ExecuteInitialize()
+    {
+        ExecuteMethod("ExecuteInitialize");
+    }
 
     /**
      * @brief It executes the ExecuteBeforeSolutionLoop() from the list of processes
      */
 
-    void ExecuteBeforeSolutionLoop();
+    void ExecuteBeforeSolutionLoop()
+    {
+        ExecuteMethod("ExecuteBeforeSolutionLoop");
+    }
 
     /**
      * @brief It executes the ExecuteInitializeSolutionStep() from the list of processes
      */
 
-    void ExecuteInitializeSolutionStep();
+    void ExecuteInitializeSolutionStep()
+    {
+        ExecuteMethod("ExecuteInitializeSolutionStep");
+    }
 
     /**
      * @brief It executes the ExecuteFinalizeSolutionStep() from the list of processes
      */
 
-    void ExecuteFinalizeSolutionStep();
+    void ExecuteFinalizeSolutionStep()
+    {
+        ExecuteMethod("ExecuteFinalizeSolutionStep");
+    }
 
     /**
      * @brief It executes the ExecuteBeforeOutputStep() from the list of processes
      */
 
-    void ExecuteBeforeOutputStep();
+    void ExecuteBeforeOutputStep()
+    {
+        ExecuteMethod("ExecuteBeforeOutputStep");
+    }
 
     /**
      * @brief It executes the ExecuteAfterOutputStep() from the list of processes
      */
 
-    void ExecuteAfterOutputStep();
+    void ExecuteAfterOutputStep()
+    {
+        ExecuteMethod("ExecuteAfterOutputStep");
+    }
 
     /**
      * @brief It executes the ExecuteFinalize() from the list of processes
      */
 
-    void ExecuteFinalize();
+    void ExecuteFinalize()
+    {
+        ExecuteMethod("ExecuteFinalize");
+    }
 
     /**
      * @brief It executes the IsOutputStep() from the list of processes
      */
 
-    void IsOutputStep();
+    void IsOutputStep()
+    {
+        ExecuteMethod("IsOutputStep");
+    }
 
     /**
      * @brief It executes the PrintOutput() from the list of processes
      */
 
-    void PrintOutput();
+    void PrintOutput()
+    {
+        ExecuteMethod("PrintOutput");
+    }
 
     /**
      * @brief It executes the Clear() from the list of processes
      */
 
-    void Clear();
+    void Clear()
+    {
+        ExecuteMethod("Clear");
+    }
 
     ///@}
     ///@name Access
