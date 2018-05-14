835bcc906e0f12a7950ef018d7d45dc9c9da3e9c
Klaus Bernd Sautter <klaus.sautter@tum.de>
2018-05-14 15:59:46 +0200

    moved if check

--- applications/DEM_application/custom_strategies/strategies/explicit_solver_strategy.cpp
+++ applications/DEM_application/custom_strategies/strategies/explicit_solver_strategy.cpp
@@ -845,8 +845,7 @@ namespace Kratos {
             for (ConditionsArrayType::iterator it = it_begin; it != it_end; ++it) { //each iteration refers to a different triangle or quadrilateral
 
                 Condition::GeometryType& geom = it->GetGeometry();
-                if (geom.size()>2)
-                {
+                
                 //double Element_Area = geom.Area();
 
                 it->CalculateRightHandSide(rhs_cond, r_process_info);
@@ -854,7 +853,7 @@ namespace Kratos {
                 p_wall->CalculateElasticForces(rhs_cond_elas, r_process_info);
                 array_1d<double, 3> Normal_to_Element = ZeroVector(3);
 
-                p_wall->CalculateNormal(Normal_to_Element);
+                if (geom.size()>2) p_wall->CalculateNormal(Normal_to_Element);
 
                 const unsigned int& dim = geom.WorkingSpaceDimension();
 
@@ -881,7 +880,6 @@ namespace Kratos {
                     noalias(node_rhs_tang) += rhs_cond_comp - GeometryFunctions::DotProduct(rhs_cond_comp, Normal_to_Element) * Normal_to_Element;
 
                     geom[i].UnSetLock();
-                }
                 }
             }
         }
