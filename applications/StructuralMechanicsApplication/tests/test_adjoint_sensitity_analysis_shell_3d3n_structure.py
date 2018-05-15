06755e02eabbe8579b70d2d27cc3e293ef665dc2
MFusseder <martin.fusseder@tum.de>
2018-05-15 14:34:54 +0200

    modified tests

--- applications/StructuralMechanicsApplication/tests/test_adjoint_sensitity_analysis_shell_3d3n_structure.py
+++ applications/StructuralMechanicsApplication/tests/test_adjoint_sensitity_analysis_shell_3d3n_structure.py
@@ -1,6 +1,4 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-import os
-os.environ['OMP_NUM_THREADS'] = "1"
 #import kratos core and applications
 from KratosMultiphysics import *
 from KratosMultiphysics.StructuralMechanicsApplication import *
