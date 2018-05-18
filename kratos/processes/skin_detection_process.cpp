cbaa6b188e4ed5bf157b2ca8e3d121d870f56266
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-05-18 18:55:02 +0200

    Using map instead of set to keep the faces non inverted

--- kratos/processes/skin_detection_process.cpp
+++ kratos/processes/skin_detection_process.cpp
@@ -49,8 +49,8 @@ void SkinDetectionProcess<TDim>::Execute()
     const SizeType echo_level = mThisParameters["echo_level"].GetInt();
 
     /* NEIGHBOUR ELEMENTS */
-    // Create the inverse_face_set
-    HashSetVectorIntType inverse_face_set;
+    // Create the inverse_face_map
+    HashMapVectorIntType inverse_face_map;
 
     for(IndexType i = 0; i < number_of_elements; ++i) {
         auto it_elem = mrModelPart.Elements().begin() + i;
@@ -64,26 +64,29 @@ void SkinDetectionProcess<TDim>::Execute()
             /* FACES/EDGES */
             const SizeType number_nodes = TDim == 2 ? geom.Edges()[i_face].size() : geom.Faces()[i_face].size();
             VectorIndexType vector_ids(number_nodes);
+            VectorIndexType ordered_vector_ids(number_nodes);
 
             /* FACE/EDGE */
             if (TDim == 2) {
                 for (IndexType i_node = 0; i_node < number_nodes; ++i_node) {
                     vector_ids[i_node] = geom.Edges()[i_face][i_node].Id();
+                    ordered_vector_ids[i_node] = vector_ids[i_node];
                 }
             } else {
                 for (IndexType i_node = 0; i_node < number_nodes; ++i_node) {
                     vector_ids[i_node] = geom.Faces()[i_face][i_node].Id();
+                    ordered_vector_ids[i_node] = vector_ids[i_node];
                 }
             }
 
             /*** THE ARRAY OF IDS MUST BE ORDERED!!! ***/
             std::sort(vector_ids.begin(), vector_ids.end());
-            // Check if the elements already exist in the HashSetVectorIntType
-            HashSetVectorIntTypeIteratorType it_check = inverse_face_set.find(vector_ids);
+            // Check if the elements already exist in the HashMapVectorIntType
+            HashMapVectorIntTypeIteratorType it_check = inverse_face_map.find(vector_ids);
 
-            if(it_check == inverse_face_set.end() ) {
+            if(it_check == inverse_face_map.end() ) {
                 // If it doesn't exist it is added to the database
-                inverse_face_set.insert(vector_ids);
+                inverse_face_map.insert(std::pair<VectorIndexType, VectorIndexType>(vector_ids, ordered_vector_ids));
             }
         }
     }
@@ -122,7 +125,7 @@ void SkinDetectionProcess<TDim>::Execute()
 
             if(it_check != face_set.end() ) {
                 // If it exists we remove from the inverse map
-                inverse_face_set.erase(vector_ids);
+                inverse_face_map.erase(vector_ids);
             } else {
                 // If it doesn't exist it is added to the database
                 face_set.insert(vector_ids);
@@ -145,30 +148,32 @@ void SkinDetectionProcess<TDim>::Execute()
 
     // Create the auxiliar conditions
     Properties::Pointer p_prop_0 = mrModelPart.pGetProperties(0);
-    for (auto& set : inverse_face_set) {
+    for (auto& map : inverse_face_map) {
         condition_id += 1;
 
-        for (auto& index : set)
+        const VectorIndexType& nodes_face = map.second;
+
+        for (auto& index : nodes_face)
             nodes_in_the_skin.insert(index);
 
-        const std::string complete_name = pre_name + name_condition + std::to_string(TDim) + "D" + std::to_string(set.size()) + "N"; // If the condition doesn't follow this structure...sorry, we then need to modify this...
+        const std::string complete_name = pre_name + name_condition + std::to_string(TDim) + "D" + std::to_string(nodes_face.size()) + "N"; // If the condition doesn't follow this structure...sorry, we then need to modify this...
         if (TDim == 2) {
-            auto p_cond = mrModelPart.CreateNewCondition(complete_name, condition_id, set, p_prop_0);
+            auto p_cond = mrModelPart.CreateNewCondition(complete_name, condition_id, nodes_face, p_prop_0);
             p_auxiliar_model_part->AddCondition(p_cond);
             p_cond->Set(INTERFACE, true);
         } else {
-            auto p_cond = mrModelPart.CreateNewCondition(complete_name, condition_id, set, p_prop_0);
+            auto p_cond = mrModelPart.CreateNewCondition(complete_name, condition_id, nodes_face, p_prop_0);
             p_auxiliar_model_part->AddCondition(p_cond);
             p_cond->Set(INTERFACE, true);
         }
     }
 
     // Adding to the auxiliar model part
-    std::vector<IndexType> indexes_skin;
+    VectorIndexType indexes_skin;
     indexes_skin.insert(indexes_skin.end(), nodes_in_the_skin.begin(), nodes_in_the_skin.end());
     p_auxiliar_model_part->AddNodes(indexes_skin);
 
-    KRATOS_INFO_IF("SkinDetectionProcess", echo_level > 0) << inverse_face_set.size() << "have been created" << std::endl;
+    KRATOS_INFO_IF("SkinDetectionProcess", echo_level > 0) << inverse_face_map.size() << "have been created" << std::endl;
 
     // Now we set the falg on the nodes. The list of nodes of the auxiliar model part
     auto& nodes_array = p_auxiliar_model_part->Nodes();
