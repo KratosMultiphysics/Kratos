01809b76c6732e9fbceab020a0fc5f429dd4e18f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-05-20 12:20:12 +0200

    Removing remesh process from criteria

--- applications/MeshingApplication/custom_strategies/custom_convergencecriterias/error_mesh_criteria.h
+++ applications/MeshingApplication/custom_strategies/custom_convergencecriterias/error_mesh_criteria.h
@@ -28,9 +28,6 @@
 #include "processes/find_nodal_h_process.h"
 #include "custom_processes/metric_fast_init_process.h"
 #include "custom_processes/metrics_spr_error_process.h"
-#ifdef INCLUDE_MMG
-    #include "custom_processes/mmg_process.h"
-#endif
 
 namespace Kratos
 {
@@ -99,8 +96,6 @@ public:
     ///@name Enum's
     ///@{
 
-    enum class RemeshingUtilities {MMG = 0};
-
     ///@}
     ///@name Life Cycle
     ///@{
@@ -114,24 +109,7 @@ public:
         {
             "error_mesh_tolerance" : 5.0e-3,
             "error_mesh_constant"  : 5.0e-3,
-            "remeshing_utility"    : "MMG",
             "strategy"             : "Error",
-            "remeshing_parameters":
-            {
-                "filename"                             : "out",
-                "framework"                            : "Lagrangian",
-                "internal_variables_parameters"        :
-                {
-                    "allocation_size"                      : 1000,
-                    "bucket_size"                          : 4,
-                    "search_factor"                        : 2,
-                    "interpolation_type"                   : "LST",
-                    "internal_variable_interpolation_list" :[]
-                },
-                "save_external_files"              : false,
-                "max_number_of_searchs"            : 1000,
-                "echo_level"                       : 0
-            },
             "error_strategy_parameters":
             {
                 "minimal_size"                        : 0.01,
@@ -150,11 +128,7 @@ public:
         
         mErrorTolerance = mThisParameters["error_mesh_tolerance"].GetDouble();
         mConstantError = mThisParameters["error_mesh_constant"].GetDouble();
-        mRemeshingUtilities = ConvertRemeshUtil(mThisParameters["remeshing_utility"].GetString());
         
-#if !defined(INCLUDE_MMG)
-        KRATOS_ERROR_IF(mRemeshingUtilities == RemeshingUtilities::MMG) << "YOU CANNOT USE MMG LIBRARY. CHECK YOUR COMPILATION" << std::endl;
-#endif
     }
 
     ///Copy constructor 
@@ -211,8 +185,33 @@ public:
         const TSystemVectorType& b
         ) override
     {
-        const bool converged_error = ComputeRemesh(rModelPart);
-        
+        // The process info
+        ProcessInfo& process_info = rModelPart.GetProcessInfo();
+
+        // Computing metric
+        double estimated_error = 0;
+        if (process_info[DOMAIN_SIZE] == 2) {
+            SPRMetricProcess<2> ComputeMetric = SPRMetricProcess<2>(rModelPart, mThisParameters["error_strategy_parameters"]);
+            ComputeMetric.Execute();
+        } else {
+            SPRMetricProcess<3> ComputeMetric = SPRMetricProcess<3>(rModelPart, mThisParameters["error_strategy_parameters"]);
+            ComputeMetric.Execute();
+        }
+
+        // We get the estimated error
+        estimated_error = process_info[ERROR_ESTIMATE];
+
+        // We check if converged
+        const bool converged_error = (estimated_error > mErrorTolerance) ? false : true;
+
+        if (converged_error) {
+            KRATOS_INFO_IF("ErrorMeshCriteria", rModelPart.GetCommunicator().MyPID() == 0 && this->GetEchoLevel() > 0) << "NL ITERATION: " << process_info[NL_ITERATION_NUMBER] << "\tThe error due to the mesh size: " << estimated_error << " is under the tolerance prescribed: " << mErrorTolerance << ". No remeshing required" << std::endl;
+        } else {
+            KRATOS_INFO_IF("ErrorMeshCriteria", rModelPart.GetCommunicator().MyPID() == 0 && this->GetEchoLevel() > 0)
+            << "NL ITERATION: " << process_info[NL_ITERATION_NUMBER] << "\tThe error due to the mesh size: " << estimated_error << " is bigger than the tolerance prescribed: " << mErrorTolerance << ". Remeshing required" << std::endl
+            << "AVERAGE_NODAL_ERROR: " << process_info[AVERAGE_NODAL_ERROR] << std::endl;
+        }
+
         return converged_error;
     }
 
@@ -244,20 +243,6 @@ protected:
     ///@}
     ///@name Protected Operators
     ///@{
-
-    /**
-     * @brief This converts the remehing utility string to an enum
-     * @param str The string that you want to comvert in the equivalent enum
-     * @return RemeshingUtilities: The equivalent enum (this requires less memmory than a std::string)
-     */
-    RemeshingUtilities ConvertRemeshUtil(const std::string& str)
-    {
-        if(str == "MMG") {
-            return RemeshingUtilities::MMG;
-        } else {
-            return RemeshingUtilities::MMG;
-        }
-    }
     
     ///@}
     ///@name Protected Operations
@@ -286,8 +271,6 @@ private:
 
     Parameters mThisParameters;                /// The parameters
     
-    RemeshingUtilities mRemeshingUtilities;    /// The remeshing utilities to use
-    
     double mErrorTolerance;                    /// The error tolerance considered
     double mConstantError;                     /// The constant considered in the remeshing process
     
@@ -298,64 +281,6 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
-
-    /**
-     * @brief This process computes the remeshing depending of the error
-     * @param rModelPart The model part of interest
-     * @return True if converged, false otherwise
-     */
-    bool ComputeRemesh(ModelPart& rModelPart)
-    {
-        // The process info
-        ProcessInfo& process_info = rModelPart.GetProcessInfo();
-
-        // Computing metric
-        double estimated_error = 0;
-        if (process_info[DOMAIN_SIZE] == 2) {
-            SPRMetricProcess<2> ComputeMetric = SPRMetricProcess<2>(rModelPart, mThisParameters["error_strategy_parameters"]);
-            ComputeMetric.Execute();
-        } else {
-            SPRMetricProcess<3> ComputeMetric = SPRMetricProcess<3>(rModelPart, mThisParameters["error_strategy_parameters"]);
-            ComputeMetric.Execute();
-        }
-
-        // We get the estimated error
-        estimated_error = process_info[ERROR_ESTIMATE];
-
-        // We check if converged
-        const bool converged_error = (estimated_error > mErrorTolerance) ? false : true;
-
-        if (converged_error) {
-            KRATOS_INFO_IF("ErrorMeshCriteria", rModelPart.GetCommunicator().MyPID() == 0 && this->GetEchoLevel() > 0) << "NL ITERATION: " << process_info[NL_ITERATION_NUMBER] << "\tThe error due to the mesh size: " << estimated_error << " is under the tolerance prescribed: " << mErrorTolerance << ". No remeshing required" << std::endl;
-            process_info[ERROR_ESTIMATE] = -1.0;
-        } else {
-            KRATOS_INFO_IF("ErrorMeshCriteria", rModelPart.GetCommunicator().MyPID() == 0 && this->GetEchoLevel() > 0)
-            << "NL ITERATION: " << process_info[NL_ITERATION_NUMBER] << "\tThe error due to the mesh size: " << estimated_error << " is bigger than the tolerance prescribed: " << mErrorTolerance << ". Remeshing required" << std::endl
-            << "AVERAGE_NODAL_ERROR: " << process_info[AVERAGE_NODAL_ERROR] << std::endl;
-
-            // Remeshing
-            if (mRemeshingUtilities == RemeshingUtilities::MMG) {
-            #ifdef INCLUDE_MMG
-                if (process_info[DOMAIN_SIZE] == 2) {
-                    MmgProcess<2> MmgRemesh = MmgProcess<2>(rModelPart, mThisParameters["remeshing_parameters"]);
-                    MmgRemesh.Execute();
-                } else {
-                    MmgProcess<3> MmgRemesh = MmgProcess<3>(rModelPart, mThisParameters["remeshing_parameters"]);
-                    MmgRemesh.Execute();
-                }
-            #else
-                KRATOS_ERROR << "Please compile with MMG to use this utility" << std::endl;
-            #endif
-            } else {
-                KRATOS_ERROR << "Not an alternative utility" << std::endl;
-            }
-
-            // We set the model part as modified
-            rModelPart.Set(MODIFIED, true);
-        }
-
-        return converged_error;
-    }
 
     ///@}
     ///@name Private  Access
