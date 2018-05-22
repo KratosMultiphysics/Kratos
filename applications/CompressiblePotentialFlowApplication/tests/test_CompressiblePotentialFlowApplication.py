e281ae385b9ce497be728dee3df360ec513036cd
Inigo Lopez <inigolcanalejo@gmail.com>
2018-05-22 15:40:39 +0200

    spelling suit -> suite

--- applications/CompressiblePotentialFlowApplication/tests/test_CompressiblePotentialFlowApplication.py
+++ applications/CompressiblePotentialFlowApplication/tests/test_CompressiblePotentialFlowApplication.py
@@ -1,14 +1,13 @@
 # import Kratos
+import KratosMultiphysics
 from KratosMultiphysics import *
 from KratosMultiphysics.CompressiblePotentialFlowApplication import *
+import run_cpp_unit_tests
 
 # Import Kratos "wrapper" for unittests
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 
-# Import the tests or test_classes to create the suits
-
-from test_potential_element_2d import TestPotentialElement2D
-
+# Import the tests or test_classes to create the suites
 
 def AssambleTestSuites():
     ''' Populates the test suites to run.
@@ -25,18 +24,18 @@ def AssambleTestSuites():
 
     suites = KratosUnittest.KratosSuites
 
-    # Create a test suit with the selected tests (Small tests):
+    # Create a test suite with the selected tests (Small tests):
     # smallSuite will contain the following tests:
     # - testSmallExample
     smallSuite = suites['small']
-    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestPotentialElement2D]))
+    #smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestPotentialElement2D]))
 
-    # Create a test suit with the selected tests
+    # Create a test suite with the selected tests
     # nightSuite will contain the following tests:
     nightSuite = suites['nightly']
     nightSuite.addTests(smallSuite)
 
-    # Create a test suit that contains all the tests from every testCase
+    # Create a test suite that contains all the tests from every testCase
     # in the list:
     allSuite = suites['all']
     allSuite.addTests(nightSuite) # already contains the smallSuite
@@ -44,10 +43,10 @@ def AssambleTestSuites():
     return suites
 
 if __name__ == '__main__':
-    print("\nRunning cpp unit tests ...")
+    KratosMultiphysics.Logger.PrintInfo("Unittests", "\nRunning cpp unit tests ...")
     run_cpp_unit_tests.run()
-    print("Finished running cpp unit tests!")
+    KratosMultiphysics.Logger.PrintInfo("Unittests", "Finished running cpp unit tests!")
     
-    print("\nRunning python tests ...")
+    KratosMultiphysics.Logger.PrintInfo("Unittests", "\nRunning python tests ...")
     KratosUnittest.runTests(AssambleTestSuites())
-    print("Finished python tests!")
+    KratosMultiphysics.Logger.PrintInfo("Unittests", "Finished python tests!")
