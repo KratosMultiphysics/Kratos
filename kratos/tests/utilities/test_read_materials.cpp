3fe3b3ef141f003b9791fb79351649156a788c2d
Marcelo <marcelo.raschi@gmail.com>
2018-05-28 17:28:55 +0200

    Add test (WIP)

--- kratos/tests/utilities/test_read_materials.cpp
+++ kratos/tests/utilities/test_read_materials.cpp
@@ -15,70 +15,67 @@ KRATOS_TEST_CASE_IN_SUITE(ReadMaterialsUtility, KratosCoreFastSuite)
 {
     Parameters parameters(R"(
         {
-        "properties": [{
-            "model_part_name": "MATERIAL_HOMOGENEOUS",
-            "properties_id": 1,
-            "Material": {
-                "name": "homog",
-                "constitutive_law": {
-                    "name": "LinearElastic3DLaw"
-                },
-                "Variables": {
-                    "DENSITY": 7850.0,
-                    "YOUNG_MODULUS": 210000000000.0,
-                    "POISSON_RATIO": 0.3
-                    },
-                 "Tables": {
-                    "Table1": {
-        		    "input_variable": "TEMPERATURE",
-        		    "output_variable": "YOUNG_MODULUS",
-        		    "data": [ [0, 100], [1, 200], [2, 300] ]
-                    },
-        		        "Table2": {
-                        "input_variable": "TEMPERATURE",
-        	                "output_variable": "YOUNG_MODULUS",
-        	                "data": [ [3, 400], [4, 500], [5, 600] ]
-                        }
-        	            }
-                }
-            }, {
-            "model_part_name": "MATERIAL_MULTISCALE",
-            "properties_id": 2,
-            "Material": {
-                "name": "multiscale",
-                "constitutive_law": {
-                    "name": "LinearElastic3DLaw"
-                },
-                "Variables": {
-                    "DENSITY": 7.0,
-                    "YOUNG_MODULUS": 2100.0,
-                    "POISSON_RATIO": 0.3
-                    },
-                "Tables":  {}
-                }
-            }]
+            "materials_filename": "/home/mraschi/Projects/Kratos/kratos/tests/utilities/test_read_materials.json"
         })");
 
-    ModelPart model_part("dummy");
-    //Properties::Pointer p_elem_prop = model_part.pGetProperties(0);
-    //NodeType::Pointer p_node_1 = model_part.CreateNewNode(1, 0.0 , 0.0 , 0.0);
-    //NodeType::Pointer p_node_2 = model_part.CreateNewNode(2, 1.0 , 0.0 , 0.0);
-    //NodeType::Pointer p_node_3 = model_part.CreateNewNode(3, 1.0 , 1.0 , 0.0);
-    //std::vector<NodeType::Pointer> element_nodes_0 (3);
-    //element_nodes_0[0] = p_node_1;
-    //element_nodes_0[1] = p_node_2;
-    //element_nodes_0[2] = p_node_3;
-    //Triangle2D3 <NodeType> triangle_0( element_nodes_0 );
-    //Element::Pointer p_elem_0 = model_part.CreateNewElement("Element2D3N", 1, triangle_0, p_elem_prop);
-    //p_sub_modelpart_1->AddElement(p_elem_0);
+//        {
+//        "properties": [{
+//            "model_part_name": "MATERIAL_HOMOGENEOUS",
+//            "properties_id": 1,
+//            "Material": {
+//                "name": "homog",
+//                "constitutive_law": {
+//                    "name": "LinearElastic3DLaw"
+//                },
+//                "Variables": {
+//                    "DENSITY": 7850.0,
+//                    "YOUNG_MODULUS": 210000000000.0,
+//                    "POISSON_RATIO": 0.3
+//                    },
+//                 "Tables": {
+//                    "Table1": {
+//        		    "input_variable": "TEMPERATURE",
+//        		    "output_variable": "YOUNG_MODULUS",
+//        		    "data": [ [0, 100], [1, 200], [2, 300] ]
+//                    },
+//        		        "Table2": {
+//                        "input_variable": "TEMPERATURE",
+//        	                "output_variable": "YOUNG_MODULUS",
+//        	                "data": [ [3, 400], [4, 500], [5, 600] ]
+//                        }
+//        	            }
+//                }
+//            }, {
+//            "model_part_name": "MATERIAL_MULTISCALE",
+//            "properties_id": 2,
+//            "Material": {
+//                "name": "multiscale",
+//                "constitutive_law": {
+//                    "name": "LinearElastic3DLaw"
+//                },
+//                "Variables": {
+//                    "DENSITY": 7.0,
+//                    "YOUNG_MODULUS": 2100.0,
+//                    "POISSON_RATIO": 0.3
+//                    },
+//                "Tables":  {}
+//                }
+//            }]
+//        }
 
+    ModelPart model_part("dummy");
     ReadMaterialsUtility(model_part, parameters);
 
-    Properties::Pointer p_prop = model_part.pGetProperties(1, 0);
+    KRATOS_CHECK_EQUAL(model_part.NumberOfProperties(), 2);
 
-    //p_prop->
-    //auto variable = KratosComponents<Variable<double>>().Get(iter.name());
-    KRATOS_CHECK_EQUAL(p_prop["Variables"]["DENSITY"].GetDouble(), 1000);
+    Properties property = model_part.GetProperties(1, 0);
+	KRATOS_CHECK(property.HasVariables());
+    KRATOS_CHECK(property.Has(CONSTITUTIVE_LAW));
+    KRATOS_CHECK(property.HasTable(TEMPERATURE, YOUNG_MODULUS));
+    KRATOS_CHECK(property.HasTable(TEMPERATURE, VISCOSITY));
+
+    Properties p_prop = model_part.GetProperties(2, 0);
+
 }
 }
 } // namespace Kratos.
