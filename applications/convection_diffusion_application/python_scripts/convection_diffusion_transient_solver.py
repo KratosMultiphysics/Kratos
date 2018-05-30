4a9c8db71e97813776155f83e698f1e2aa569aa4
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-05-30 20:21:53 +0200

    Typo

--- applications/convection_diffusion_application/python_scripts/convection_diffusion_transient_solver.py
+++ applications/convection_diffusion_application/python_scripts/convection_diffusion_transient_solver.py
@@ -44,7 +44,7 @@ class ConvectionDiffusionTransientSolver(convection_diffusion_base_solver.Convec
 
     def _create_solution_scheme(self):
         #Variable defining the temporal scheme (0: Forward Euler, 1: Backward Euler, 0.5: Crank-Nicolson)
-        self.GetComputingModelPart().ProcessInfo[ConvectionDiffusionApplication.THETA] = self.settings["time_stepping"]["theta"].GetDouble()
-        self.GetComputingModelPart().ProcessInfo[KratosMultiphysics.DYNAMIC_TAU] = self.settings["dynamic_tau"].GetDouble()
+        self.GetComputingModelPart().ProcessInfo[ConvectionDiffusionApplication.THETA] = self.transient_settings["time_stepping"]["theta"].GetDouble()
+        self.GetComputingModelPart().ProcessInfo[KratosMultiphysics.DYNAMIC_TAU] = self.transient_settings["dynamic_tau"].GetDouble()
         mechanical_scheme = KratosMultiphysics.ResidualBasedIncrementalUpdateStaticScheme()
         return mechanical_scheme
