4db60b1fcc58de18808b1078ddb8d5a51cae7d00
Josep Maria <cpuigbo@cimne.upc.edu>
2018-06-01 23:47:07 +0200

    corrections in scripts

--- applications/SolidMechanicsApplication/python_scripts/schemes_factory.py
+++ applications/SolidMechanicsApplication/python_scripts/schemes_factory.py
@@ -201,6 +201,10 @@ class SolutionScheme:
                 if( len(variables) == 4 ):
                     integration_method = scalar_integration_method(variables[0],variables[1],variables[2],variables[3])
                 elif( len(variables) == 1 ):
+                    if(integration_method_name.find("Step") != -1):
+                        scalar_integration_method = getattr(KratosSolid, 'StaticStepScalarIntegration')
+                    else:
+                        scalar_integration_method = getattr(KratosSolid, 'StaticScalarIntegration')
                     integration_method = scalar_integration_method(variables[0])
                 else:
                     raise Exception('len(variables) = ' + str(len(variables)))
