c74aea5edae090f7989511c90c0e65a6757d6c05
jcotela <jcotela@cimne.upc.edu>
2018-06-01 11:57:58 +0200

    Updating python solvers wrapper for the fluid.

--- applications/FluidDynamicsApplication/python_scripts/python_solvers_wrapper_fluid.py
+++ applications/FluidDynamicsApplication/python_scripts/python_solvers_wrapper_fluid.py
@@ -2,10 +2,10 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 
 import KratosMultiphysics
 
-def CreateSolver(main_model_part, custom_settings):
+def CreateSolver(model, custom_settings):
 
-    if (type(main_model_part) != KratosMultiphysics.ModelPart):
-        raise Exception("input is expected to be provided as a Kratos ModelPart object")
+    if (type(model) != KratosMultiphysics.Model):
+        raise Exception("input is expected to be provided as a Kratos Model object")
 
     if (type(custom_settings) != KratosMultiphysics.Parameters):
         raise Exception("input is expected to be provided as a Kratos Parameters object")
@@ -46,13 +46,13 @@ def CreateSolver(main_model_part, custom_settings):
 
         elif (solver_type == "EmbeddedAusas"):
             solver_module_name = "trilinos_navier_stokes_embedded_ausas_solver"
-  
+
         else:
             raise Exception("the requested solver type is not in the python solvers wrapper")
     else:
         raise Exception("parallelism is neither OpenMP nor MPI")
 
     solver_module = __import__(solver_module_name)
-    solver = solver_module.CreateSolver(main_model_part, custom_settings["solver_settings"])
+    solver = solver_module.CreateSolver(model, custom_settings["solver_settings"])
 
     return solver
