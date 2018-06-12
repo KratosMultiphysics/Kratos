d7387868ebf23dc56a96ba39c97245c110b9b9e4
jcotela <jcotela@cimne.upc.edu>
2018-06-12 14:03:22 +0200

    Removing extra file closes.

--- applications/FluidDynamicsApplication/python_scripts/compute_drag_process.py
+++ applications/FluidDynamicsApplication/python_scripts/compute_drag_process.py
@@ -52,7 +52,6 @@ class ComputeDragProcess(python_process.PythonProcess):
                     file.write(settings["model_part_name"].GetString() + " drag \n")
                     file.write("\n")
                     file.write("Time   Fx   Fy   Fz \n")
-                    file.close()
 
 
     def ExecuteFinalizeSolutionStep(self):
@@ -72,4 +71,3 @@ class ComputeDragProcess(python_process.PythonProcess):
                 if (self.write_drag_output_file):
                     with open(self.drag_filename, 'a') as file:
                         file.write(str(current_time)+"   "+format(drag_force[0],self.format)+"   "+format(drag_force[1],self.format)+"   "+format(drag_force[2],self.format)+"\n")
-                        file.close()
