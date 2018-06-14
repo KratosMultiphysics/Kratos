5bc6ff92f1112c0914252547cbadb136a0401627
sunethwarna <suneth.warna@gmail.com>
2018-06-14 11:24:26 +0200

    bugfix: not reading old type of processes

--- applications/FluidDynamicsApplication/python_scripts/adjoint_fluid_analysis.py
+++ applications/FluidDynamicsApplication/python_scripts/adjoint_fluid_analysis.py
@@ -48,6 +48,52 @@ class AdjointFluidAnalysis(AnalysisStage):
     def _CreateSolver(self):
         import python_solvers_wrapper_adjoint_fluid
         return python_solvers_wrapper_adjoint_fluid.CreateSolver(self.model, self.project_parameters)            
+    
+    def _CreateProcesses(self, parameter_name, initialization_order):
+            """Create a list of Processes
+            This method is TEMPORARY to not break existing code
+            It will be removed in the future
+            """
+            list_of_processes = super(AdjointFluidAnalysis, self)._CreateProcesses(parameter_name, initialization_order)
+
+            # The list of processes will contain a list with each individual process already constructed (boundary conditions, initial conditions and gravity)
+            # Note 1: gravity is constructed first. Outlet process might need its information.
+            # Note 2: initial conditions are constructed before BCs. Otherwise, they may overwrite the BCs information.
+            if parameter_name == "processes":
+                processes_block_names = ["gravity", "initial_conditions_process_list", "boundary_conditions_process_list", "auxiliar_process_list"]
+                if len(list_of_processes) == 0: # Processes are given in the old format
+                    Kratos.Logger.PrintInfo(self.__class__.__name__, "Using the old way to create the processes, this will be removed!")
+                    from process_factory import KratosProcessFactory
+                    factory = KratosProcessFactory(self.model)
+                    for process_name in processes_block_names:
+                        if (self.project_parameters.Has(process_name) is True):
+                            list_of_processes += factory.ConstructListOfProcesses(self.project_parameters[process_name])
+                else: # Processes are given in the new format
+                    for process_name in processes_block_names:
+                        if (self.project_parameters.Has(process_name) is True):
+                            raise Exception("Mixing of process initialization is not alowed!")
+            elif parameter_name == "output_processes":
+                if self.project_parameters.Has("output_configuration"):
+                    #KratosMultiphysics.Logger.PrintInfo("FluidDynamicsAnalysis", "Using the old way to create the gid-output, this will be removed!")
+                    gid_output= self._SetUpGiDOutput()
+                    list_of_processes += [gid_output,]
+            else:
+                raise NameError("wrong parameter name")
+
+            return list_of_processes
+    
+    def _SetUpGiDOutput(self):
+        '''Initialize a GiD output instance'''
+        if self.parallel_type == "OpenMP":
+            from gid_output_process import GiDOutputProcess as OutputProcess
+        elif self.parallel_type == "MPI":
+            from gid_output_process_mpi import GiDOutputProcessMPI as OutputProcess
+
+        output = OutputProcess(self._GetSolver().GetComputingModelPart(),
+                                self.project_parameters["problem_data"]["problem_name"].GetString() ,
+                                self.project_parameters["output_configuration"])
+
+        return output            
 
     def RunSolutionLoop(self):
         """Note that the adjoint problem is solved in reverse time
