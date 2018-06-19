e6c91e64edf2785862703885ce912762654bce36
philbucher <philipp.bucher@tum.de>
2018-06-19 11:00:41 +0200

    Added some docu

--- kratos/utilities/point_locator.h
+++ kratos/utilities/point_locator.h
@@ -49,191 +49,235 @@ namespace Kratos
   ///@name Kratos Classes
   ///@{
 
-  /// Short class definition.
-  /** Detail class definition.
-  */
-  class KRATOS_API(KRATOS_CORE) PointLocator
+/**
+ * @class PointLocator
+ * @ingroup KratosCore
+ * @brief Utility class to find an entity of a mesh based on a location
+ * @details Based on the location of a point, the corresponding entity
+ * (node, element or condition) in a mesh is found and it's id is returned
+ * @author Philipp Bucher
+ */
+class KRATOS_API(KRATOS_CORE) PointLocator
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of PointLocator
+    KRATOS_CLASS_POINTER_DEFINITION(PointLocator);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    PointLocator(ModelPart& rModelPart) : mrModelPart(rModelPart) {}
+
+    /// Destructor.
+    virtual ~PointLocator() {}
+
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief This function finds a node based on a location
+     * @param rThePoint the location to search
+     * @param DistanceThreshold threshold for the distance
+     * @return Id of the found node. -1 if no node was found
+     */
+    int FindNode(const Point& rThePoint, const double DistanceThreshold=1e-12) const;
+
+    /**
+     * @brief This function finds an element based on a location
+     * @param rThePoint the location to search
+     * @param rShapeFunctionValues vector containing the shape-function values for the given point
+     * @return Id of the found element. -1 if no element was found
+     */
+    int FindElement(const Point& rThePoint, Vector& rShapeFunctionValues) const;
+
+    /**
+     * @brief This function finds a condition based on a location
+     * @param rThePoint the location to search
+     * @param rShapeFunctionValues vector containing the shape-function values for the given point
+     * @return Id of the found condition. -1 if no condition was found
+     */
+    int FindCondition(const Point& rThePoint, Vector& rShapeFunctionValues) const;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    virtual std::string Info() const
     {
-    public:
-      ///@name Type Definitions
-      ///@{
+        std::stringstream buffer;
+        buffer << "PointLocator" ;
+        return buffer.str();
+    }
 
-      /// Pointer definition of PointLocator
-      KRATOS_CLASS_POINTER_DEFINITION(PointLocator);
+    /// Print information about this object.
+    virtual void PrintInfo(std::ostream& rOStream) const {rOStream << "PointLocator";}
 
-      ///@}
-      ///@name Life Cycle
-      ///@{
-
-      /// Default constructor.
-      PointLocator(ModelPart& rModelPart) : mrModelPart(rModelPart) {}
-
-      /// Destructor.
-      virtual ~PointLocator() {}
+    /// Print object's data.
+    virtual void PrintData(std::ostream& rOStream) const {}
 
 
-      ///@}
-      ///@name Operators
-      ///@{
+    ///@}
+    ///@name Friends
+    ///@{
 
 
-      ///@}
-      ///@name Operations
-      ///@{
+    ///@}
 
-      int FindNode(const Point& rThePoint, const double DistanceThreshold=1e-12) const;
-
-      int FindElement(const Point& rThePoint, Vector& rShapeFunctionValues) const;
-
-      int FindCondition(const Point& rThePoint, Vector& rShapeFunctionValues) const;
-
-      ///@}
-      ///@name Access
-      ///@{
+protected:
+    ///@name Protected static Member Variables
+    ///@{
 
 
-      ///@}
-      ///@name Inquiry
-      ///@{
+    ///@}
+    ///@name Protected member Variables
+    ///@{
 
 
-      ///@}
-      ///@name Input and output
-      ///@{
-
-      /// Turn back information as a string.
-      virtual std::string Info() const
-      {
-	        std::stringstream buffer;
-          buffer << "PointLocator" ;
-          return buffer.str();
-      }
-
-      /// Print information about this object.
-      virtual void PrintInfo(std::ostream& rOStream) const {rOStream << "PointLocator";}
-
-      /// Print object's data.
-      virtual void PrintData(std::ostream& rOStream) const {}
+    ///@}
+    ///@name Protected Operators
+    ///@{
 
 
-      ///@}
-      ///@name Friends
-      ///@{
+    ///@}
+    ///@name Protected Operations
+    ///@{
 
 
-      ///@}
-
-    protected:
-      ///@name Protected static Member Variables
-      ///@{
+    ///@}
+    ///@name Protected  Access
+    ///@{
 
 
-      ///@}
-      ///@name Protected member Variables
-      ///@{
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
 
 
-      ///@}
-      ///@name Protected Operators
-      ///@{
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
 
 
-      ///@}
-      ///@name Protected Operations
-      ///@{
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
 
 
-      ///@}
-      ///@name Protected  Access
-      ///@{
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ModelPart& mrModelPart;
+
+    ///@}
+    ///@name Private Operators
+    ///@{
 
 
-      ///@}
-      ///@name Protected Inquiry
-      ///@{
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    /**
+     * @brief This function finds an object based on a location
+     * @param rObjects the objects to search
+     * @param rObjectType type of the object => "Element"/"Condition"
+     * @param rThePoint the location to search
+     * @param rObjectId Id of the found condition. -1 if no object was found
+     * @param rShapeFunctionValues vector containing the shape-function values for the given point
+     */
+    template<typename TObjectType>
+    void FindObject(const TObjectType& rObjects, const std::string& rObjectType,
+                    const Point& rThePoint, int& rObjectId, Vector& rShapeFunctionValues) const;
+
+    /**
+     * @brief This function performs some checks after the search
+     * @param rObjectType type of the object => "Node"/"Element"/"Condition"
+     * @param rThePoint the location to search
+     * @param LocalObjectsFound number of found results
+     */
+    void CheckResults(const std::string& rObjectType,
+                      const Point& rThePoint,
+                      int LocalObjectsFound) const;
+
+    /**
+     * @brief This function checks whether a node is close to a point based on a threshold
+     * @param rNode the node to check
+     * @param rThePoint the location to search
+     * @param DistanceThreshold threshold for the distance
+     * @return whether the rNode is close to rThePoint
+     */
+    bool NodeIsCloseEnough(const Node<3>& rNode,
+                           const Point& rThePoint,
+                           double DistanceThreshold) const;
+
+    ///@}
+    ///@name Private  Access
+    ///@{
 
 
-      ///@}
-      ///@name Protected LifeCycle
-      ///@{
+    ///@}
+    ///@name Private Inquiry
+    ///@{
 
 
-      ///@}
-
-    private:
-      ///@name Static Member Variables
-      ///@{
+    ///@}
+    ///@name Un accessible methods
+    ///@{
 
 
-      ///@}
-      ///@name Member Variables
-      ///@{
+    ///@}
 
-      ModelPart& mrModelPart;
+}; // Class PointLocator
 
-      ///@}
-      ///@name Private Operators
-      ///@{
+///@}
 
-
-      ///@}
-      ///@name Private Operations
-      ///@{
-
-      template<typename TObjectType>
-      void FindObject(const TObjectType& rObjects, const std::string& rObjectType,
-                      const Point& rThePoint, int& rObjectId, Vector& rShapeFunctionValues) const;
-
-      void CheckResults(const std::string& rObjectType,
-                        const Point& rThePoint,
-                        int GlobalObjectsFound) const;
-
-      bool NodeIsCloseEnough(const Node<3>& rNode,
-                             const Point& rThePoint,
-                             double DistanceThreshold) const;
-
-      ///@}
-      ///@name Private  Access
-      ///@{
+///@name Type Definitions
+///@{
 
 
-      ///@}
-      ///@name Private Inquiry
-      ///@{
+///@}
+///@name Input and output
+///@{
 
+/// output stream function
+inline std::ostream& operator << (std::ostream& rOStream,
+                const PointLocator& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
 
-      ///@}
-      ///@name Un accessible methods
-      ///@{
+    return rOStream;
+}
+///@}
 
-
-      ///@}
-
-    }; // Class PointLocator
-
-  ///@}
-
-  ///@name Type Definitions
-  ///@{
-
-
-  ///@}
-  ///@name Input and output
-  ///@{
-
-  /// output stream function
-  inline std::ostream& operator << (std::ostream& rOStream,
-				    const PointLocator& rThis)
-  {
-      rThis.PrintInfo(rOStream);
-      rOStream << std::endl;
-      rThis.PrintData(rOStream);
-
-      return rOStream;
-  }
-  ///@}
-
-  ///@} addtogroup block
+///@} addtogroup block
 
 }  // namespace Kratos.
 
