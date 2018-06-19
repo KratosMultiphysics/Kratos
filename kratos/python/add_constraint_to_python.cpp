36172b83cc02dc6eaa03ab32de286bc230d0a5ef
AdityaGhantasala <shine.aditya@gmail.com>
2018-06-19 17:35:08 +0200

    exposing setData and GetData function for accessing variables/flags

--- kratos/python/add_constraint_to_python.cpp
+++ kratos/python/add_constraint_to_python.cpp
@@ -10,36 +10,51 @@
 //  Main authors:    Aditya Ghantasala
 //
 
-
 // System includes
 
 // External includes
 
-
 // Project includes
 #include "includes/define_python.h"
 #include "add_constraint_to_python.h"
-#include "utilities/constraint.h" 
+#include "utilities/constraint.h"
 
 namespace Kratos
 {
 namespace Python
 {
-    using namespace pybind11;
-    
-void  AddConstraintToPython(pybind11::module& m)
-{
-    class_<MasterSlaveConstraint>(m, "MasterSlaveConstraint" )
-	.def(init<>())
-    .def("Clear", &MasterSlaveConstraint::Clear)
-    .def("AddMaster", &MasterSlaveConstraint::AddMaster)
-    .def("AddSlave", &MasterSlaveConstraint::AddSlave)
-    .def("GetNumberOfMasters", &MasterSlaveConstraint::GetNumberOfMasters)
-    ;
+using namespace pybind11;
 
+template <class TContainerType, class TVariableType>
+bool HasHelperFunction(TContainerType &el, const TVariableType &rVar)
+{
+    return el.Has(rVar);
 }
 
-}  // namespace Python.
+template <class TContainerType, class TVariableType>
+void SetValueHelperFunction(TContainerType &el, const TVariableType &rVar, const typename TVariableType::Type &Data)
+{
+    el.SetValue(rVar, Data);
+}
+
+template <class TContainerType, class TVariableType>
+typename TVariableType::Type GetValueHelperFunction(TContainerType &el, const TVariableType &rVar)
+{
+    return el.GetValue(rVar);
+}
+
+void AddConstraintToPython(pybind11::module &m)
+{
+    class_<MasterSlaveConstraint>(m, "MasterSlaveConstraint")
+        .def(init<>())
+        .def("__setitem__", SetValueHelperFunction< MasterSlaveConstraint, Variable< bool > >)
+        .def("__getitem__", GetValueHelperFunction< MasterSlaveConstraint, Variable< bool > >)
+        .def("Has", HasHelperFunction< MasterSlaveConstraint, Variable< bool > >)
+        .def("SetValue", SetValueHelperFunction< MasterSlaveConstraint, Variable< bool > >)
+        .def("GetValue", GetValueHelperFunction< MasterSlaveConstraint, Variable< bool > >)
+        ;
+}
+
+} // namespace Python.
 
 } // Namespace Kratos
-
