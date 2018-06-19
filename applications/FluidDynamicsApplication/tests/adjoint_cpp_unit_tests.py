08e7827b3fc92469fe555e11ee3ea31f53d01258
sunethwarna <suneth.warna@gmail.com>
2018-06-19 16:31:47 +0200

    moving adjoint cpp unit tests to flud dynamics

---
+++ applications/FluidDynamicsApplication/tests/adjoint_cpp_unit_tests.py
@@ -0,0 +1,19 @@
+from KratosMultiphysics import *
+
+class AdjointCPPUnitTests(KratosUnittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def testSensitivityCPP(self):
+        Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
+        Tester.RunTestSuite("KratosSensitivityTestSuite")
+    
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    test = AdjointCPPUnitTests()
+    test.setUp()
+    test.testSensitivityCPP()
+    test.tearDown()
