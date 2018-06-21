cacbcf9a0e0f39c59ac432a23191d010ef8f887b
Miguel Masó <mmaso@cimne.upc.edu>
2018-06-21 16:24:00 +0200

    this check was making model_part_io fail

--- kratos/sources/model_part.cpp
+++ kratos/sources/model_part.cpp
@@ -551,12 +551,12 @@ void ModelPart::AddProperties(ModelPart::PropertiesType::Pointer pNewProperties,
         mpParentModelPart->AddProperties(pNewProperties, ThisIndex);
     }
 
-    auto pprop_it = GetMesh(ThisIndex).Properties().find(pNewProperties->Id());
-    if( pprop_it != GetMesh(ThisIndex).Properties().end() )
+    auto existing_prop_it = GetMesh(ThisIndex).Properties().find(pNewProperties->Id());
+    if( existing_prop_it != GetMesh(ThisIndex).Properties().end() )
     {
-        if( &(*(pprop_it.base())) != &pNewProperties )
+        if( &(*existing_prop_it) != pNewProperties.get() )
         {
-            KRATOS_ERROR << "trying to add a property with existing Id within the model part : " << Name() << " Property Id is :" << ThisIndex;
+            KRATOS_ERROR << "trying to add a property with existing Id within the model part : " << Name() << ", property Id is :" << pNewProperties->Id();
         }
     }
     else
