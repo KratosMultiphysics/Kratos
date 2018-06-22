94f843510b41b68c1c0c4abed0b7e8c196eb9d9a
jcotela <jcotela@cimne.upc.edu>
2018-06-22 09:19:53 +0200

    Initialize OSS_SWITCH to prevent a potential race condition.

--- applications/FluidDynamicsApplication/custom_strategies/strategies/residualbased_simple_steady_scheme.h
+++ applications/FluidDynamicsApplication/custom_strategies/strategies/residualbased_simple_steady_scheme.h
@@ -123,6 +123,17 @@ public:
     KRATOS_CATCH("");
   }
 
+  int Check(ModelPart& rModelPart) override
+  {
+    // Ensure that OSS_SWITCH is initialized. This will prevent race conditions when we read it in a parallel region later.
+    ProcessInfo& r_process_info = rModelPart.GetProcessInfo();
+    if (!r_process_info.Has(OSS_SWITCH)) {
+      r_process_info.SetValue(OSS_SWITCH,0);
+    }
+
+    return BaseType::Check(rModelPart);
+  }
+
   void CalculateSystemContributions(
       Element::Pointer rCurrentElement,
       LocalSystemMatrixType& LHS_Contribution,
@@ -135,7 +146,7 @@ public:
     rCurrentElement->InitializeNonLinearIteration(CurrentProcessInfo);
     rCurrentElement->CalculateLocalSystem(LHS_Contribution, RHS_Contribution, CurrentProcessInfo);
     Matrix Mass;
-    int oss_switch = CurrentProcessInfo.Has(OSS_SWITCH) ? CurrentProcessInfo[OSS_SWITCH] : 0;
+    const int oss_switch = CurrentProcessInfo[OSS_SWITCH];
     CurrentProcessInfo.SetValue(OSS_SWITCH,1);
     rCurrentElement->CalculateMassMatrix(Mass, CurrentProcessInfo);
     CurrentProcessInfo.SetValue(OSS_SWITCH,oss_switch);
@@ -167,7 +178,7 @@ public:
     rCurrentCondition->InitializeNonLinearIteration(CurrentProcessInfo);
     rCurrentCondition->CalculateLocalSystem(LHS_Contribution, RHS_Contribution, CurrentProcessInfo);
     Matrix Mass;
-    int oss_switch = CurrentProcessInfo.Has(OSS_SWITCH) ? CurrentProcessInfo[OSS_SWITCH] : 0;
+    const int oss_switch = CurrentProcessInfo[OSS_SWITCH];
     CurrentProcessInfo.SetValue(OSS_SWITCH,1);
     rCurrentCondition->CalculateMassMatrix(Mass, CurrentProcessInfo);
     CurrentProcessInfo.SetValue(OSS_SWITCH,oss_switch);
