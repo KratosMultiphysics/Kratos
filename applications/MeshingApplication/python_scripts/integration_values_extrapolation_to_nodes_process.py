5bb0664a9f785d0e2b26d4521a40751cac3b5967
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-06-23 20:56:00 +0200

    Adding python script

---
+++ applications/MeshingApplication/python_scripts/integration_values_extrapolation_to_nodes_process.py
@@ -0,0 +1,53 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing the Kratos Library
+import KratosMultiphysics 
+ 
+def Factory(settings, Model): 
+    if(type(settings) != KratosMultiphysics.Parameters): 
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string") 
+    return IntegrationValuesExtrapolationToNodesProcess(Model, settings["Parameters"])
+ 
+## All the processes python should be derived from "Process" 
+class IntegrationValuesExtrapolationToNodesProcess(KratosMultiphysics.Process):
+    def __init__(self, Model, settings ): 
+        KratosMultiphysics.Process.__init__(self) 
+ 
+        default_settings = KratosMultiphysics.Parameters(""" 
+        {
+            "echo_level"                 : 1,
+            "list_of_variables"          : [],
+            "extrapolate_non_historical" : true
+        }
+        """
+        )
+
+        settings.ValidateAndAssignDefaults(default_settings)
+
+        self.model_part = Model[settings["model_part_name"].GetString()]
+
+        self.integration_values_extrapolation_to_nodes_process = KratosMultiphysics.IntegrationValuesExtrapolationToNodesProcess(self.model_part, settings)
+
+    def ExecuteInitialize(self):
+        pass
+
+    def ExecuteBeforeSolutionLoop(self):
+        pass
+
+    def ExecuteInitializeSolutionStep(self):
+        pass
+
+    def ExecuteFinalizeSolutionStep(self):
+        self.integration_values_extrapolation_to_nodes_process.Execute()
+
+    def ExecuteBeforeOutputStep(self):
+        pass
+
+    def ExecuteAfterOutputStep(self):
+        pass
+
+    def ExecuteFinalize(self):
+        pass
+
+    def Clear(self):
+        pass
