9297860406bcf210ff968477e86b8e70103ae999
sunethwarna <suneth.warna@gmail.com>
2018-06-25 10:54:40 +0200

    update response function with MPI

--- applications/ShapeOptimizationApplication/test_examples/05_External_Fluid_Adjoint_Transient_Analyzer/base_case/response_analysis.py
+++ applications/ShapeOptimizationApplication/test_examples/05_External_Fluid_Adjoint_Transient_Analyzer/base_case/response_analysis.py
@@ -10,10 +10,17 @@ from adjoint_fluid_analysis import AdjointFluidAnalysis
 def Run(optimization_model_part, response_data, custom_settings):
     primal_parameter_file_name = "ProjectParametersPrimal.json"
     adjoint_parameter_file_name = "ProjectParametersAdjoint.json"
+    is_MPI = False
 
     if custom_settings["parallel_type"].GetString() == "MPI":
-        primal_parameter_file_name = "ProjectParametersPrimalMPI.json"
-        adjoint_parameter_file_name = "ProjectParametersAdjointMPI.json"
+        try:
+            import KratosMultiphysics.mpi as KratosMPI
+            is_MPI = True
+            primal_parameter_file_name = "ProjectParametersPrimalMPI.json"
+            adjoint_parameter_file_name = "ProjectParametersAdjointMPI.json"
+        except ImportError:
+            print("Warning: Couldn't load kratos MPI modules. Continuing with OpenMP")
+
 
     parameters = Kratos.Parameters(r'''{}''')
 
@@ -23,26 +30,34 @@ def Run(optimization_model_part, response_data, custom_settings):
     with open(adjoint_parameter_file_name,'r') as adjoint_parameter_file:
         parameters.AddValue("adjoint_settings", Kratos.Parameters(adjoint_parameter_file.read()))
 
-    model = Kratos.Model()
-
     print("> Running primal simulation")
+    model = Kratos.Model()
     primal_simulation = FluidDynamicsAnalysis(model,parameters["primal_settings"])
     primal_simulation.Run()
+    if is_MPI:
+        KratosMPI.mpi.world.barrier()
 
     print("> Running adjoint simulation")
     adjoint_model = Kratos.Model()
     adjoint_simulation = AdjointFluidAnalysis(adjoint_model,parameters["adjoint_settings"])
     adjoint_simulation.Run()
 
-    problem_data = parameters["adjoint_settings"]["problem_data"]
-    end_time = problem_data["start_step"].GetDouble()
-    nsteps = problem_data["nsteps"].GetInt()
-    time_step = parameters["adjoint_settings"]["solver_settings"]["time_stepping"]["time_step"].GetDouble()
+    rank = 0
+    if is_MPI:
+        rank = adjoint_simulation._GetSolver().main_model_part.GetCommunicator().MyPID()
+        adjoint_simulation._GetSolver().main_model_part.GetCommunicator().AssembleCurrentData(KratosMultiphysics.SHAPE_SENSITIVITY)
+        KratosMPI.mpi.world.barrier()
 
-    start_time = end_time + time_step*float(nsteps)
+    if rank==0:
+        problem_data = parameters["adjoint_settings"]["problem_data"]
+        end_time = problem_data["start_step"].GetDouble()
+        nsteps = problem_data["nsteps"].GetInt()
+        time_step = parameters["adjoint_settings"]["solver_settings"]["time_stepping"]["time_step"].GetDouble()
 
-    response_data["response_value"] = __calculate_drag("NoSlip2D_Cylinder.drag", start_time, end_time)
-    response_data["response_gradients"] = __get_response_gradients(adjoint_simulation)
+        start_time = end_time + time_step*float(nsteps)
+
+        response_data["response_value"] = __calculate_drag("NoSlip2D_Cylinder.drag", start_time, end_time)
+        response_data["response_gradients"] = __get_response_gradients(adjoint_simulation)
 
 def __get_response_gradients(adjoint_simulation):
     gradient = {}
@@ -93,10 +108,18 @@ def __calculate_drag(filename, start_time, end_time):
 
 
 if __name__ == "__main__":
+    import sys
+
     response_values = {}
+
+    if len(sys.argv) > 1:
+        parallel_type = sys.argv[1]
+    else:
+        parallel_type = "OpenMP"
+
     parameters = Kratos.Parameters(r'''{
-        "parallel_type" : "OpenMP"
-    }''')
+        "parallel_type" : "%s"
+    }''' % parallel_type)
 
     Run(None, response_values, parameters)
     print(response_values)
