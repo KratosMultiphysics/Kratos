33fd528d2d4e7ade1c6869ae679be05a8b48eeb3
Carlos Roig <roigcarlo@gmail.com>
2018-06-29 09:53:30 +0200

    Restore changes to static bins

--- kratos/spatial_containers/bins_static.h
+++ kratos/spatial_containers/bins_static.h
@@ -104,10 +104,13 @@ public:
     Bins( IteratorType const& PointBegin, IteratorType const& PointEnd, SizeType BucketSize = 1 )
         : mPointBegin(PointBegin), mPointEnd(PointEnd)
     {
+        auto NumPoints = std::distance(mPointBegin, mPointEnd);
+        
         if(mPointBegin==mPointEnd)
             return;
+
         CalculateBoundingBox();
-        CalculateCellSize();
+        CalculateCellSize(NumPoints);
         AllocateCellsContainer();
         GenerateBins();
     }
@@ -126,6 +129,8 @@ public:
     Bins( IteratorType const& PointBegin, IteratorType const& PointEnd, PointType const& MinPoint, PointType const& MaxPoint, SizeType BucketSize = 1 )
         : mPointBegin(PointBegin), mPointEnd(PointEnd)
     {
+        auto NumPoints = std::distance(mPointBegin, mPointEnd);
+
         if(mPointBegin==mPointEnd)
             return;
 
@@ -135,7 +140,7 @@ public:
             mMaxPoint[i] = MaxPoint[i];
         }
 
-        CalculateCellSize();
+        CalculateCellSize(NumPoints);
         AllocateCellsContainer();
         GenerateBins();
     }
@@ -155,8 +160,9 @@ public:
     {
         if(mPointBegin==mPointEnd)
             return;
+
         CalculateBoundingBox();
-        CalculateCellSize(cellsize);
+        AssignCellSize(cellsize);
         AllocateCellsContainer();
         GenerateBins();
     }
@@ -251,48 +257,49 @@ private:
 
     //************************************************************************
 
-    void CalculateCellSize()
+    /** 
+     * @brief Calculates the cell size of the bins.
+     * 
+     * Calculates the cell size of the bins using an average aproximation of the objects in the bins.
+     * 
+     * @param ApproximatedSize Aproximate number of objects that will be stored in the bins
+     */
+    void CalculateCellSize(std::size_t ApproximatedSize) 
     {
-
-        CoordinateType delta[TDimension];
-        CoordinateType alpha[TDimension];
-        CoordinateType mult_delta = 1.00;
-        SizeType index = 0;
-        for(SizeType i = 0 ; i < TDimension ; i++)
-        {
-            delta[i] = mMaxPoint[i] - mMinPoint[i];
-            if ( delta[i] > delta[index] )
-                index = i;
-            delta[i] = (delta[i] == 0.00) ? 1.00 : delta[i];
+        std::size_t average_number_of_cells = static_cast<std::size_t>(std::pow(static_cast<double>(ApproximatedSize), 1.00 / Dimension));
+        
+        std::array<double, 3> lengths;
+        double average_length = 0.00;
+        
+        for (int i = 0; i < Dimension; i++) {
+            lengths[i] = mMaxPoint[i] - mMinPoint[i];
+            average_length += lengths[i];
         }
+        average_length *= 1.00 / 3.00;
 
-        for(SizeType i = 0 ; i < TDimension ; i++)
-        {
-            alpha[i] = delta[i] / delta[index];
-            mult_delta *= alpha[i];
-        }
-
-        mN[index] = static_cast<SizeType>( pow(static_cast<CoordinateType>(SearchUtils::PointerDistance(mPointBegin,mPointEnd)/mult_delta), 1.00/TDimension)+1 );
-
-        for(SizeType i = 0 ; i < TDimension ; i++)
-        {
-            if(i!=index)
-            {
-                mN[i] = static_cast<SizeType>(alpha[i] * mN[index]);
-                mN[i] = ( mN[i] == 0 ) ? 1 : mN[i];
+        if (average_length < std::numeric_limits<double>::epsilon()) {
+            for(int i = 0; i < Dimension; i++) {
+                mN[i] = 1;
             }
+            return;
         }
 
-        for(SizeType i = 0 ; i < TDimension ; i++)
-        {
-            mCellSize[i] = delta[i] / mN[i];
+        for (int i = 0; i < Dimension; i++) {
+             mN[i] = static_cast<std::size_t>(lengths[i] / average_length * (double)average_number_of_cells) + 1;
+            
+            if (mN[i] > 1) {
+                mCellSize[i] = lengths[i] / mN[i];
+            } else {
+                mCellSize[i] = average_length;
+            }
+
             mInvCellSize[i] = 1.00 / mCellSize[i];
         }
     }
 
     //************************************************************************
 
-    void CalculateCellSize( CoordinateType BoxSize )
+    void AssignCellSize( CoordinateType BoxSize )
     {
         for(SizeType i = 0 ; i < TDimension ; i++)
         {
@@ -891,15 +898,15 @@ public:
 private:
 
     // Point Access Iterators (vector reordered!!)
-    IteratorType     mPointBegin;
-    IteratorType     mPointEnd;
+    IteratorType    mPointBegin;
+    IteratorType    mPointEnd;
 
     // Bin Parameters (Sizes,BoundingBox,...)
-    PointType  mMinPoint;
-    PointType  mMaxPoint;
-    PointType  mCellSize;
-    PointType  mInvCellSize;
-    Tvector<SizeType,TDimension>  mN;
+    PointType       mMinPoint;
+    PointType       mMaxPoint;
+    CoordinateArray mCellSize;
+    CoordinateArray mInvCellSize;
+    SizeArray       mN;
 
     // Bins Access Vector ( vector<Iterator> )
     IteratorVector           mIndexCell;
