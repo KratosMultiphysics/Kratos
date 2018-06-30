042df20442e0d2d6320284e9641417bed4564314
philbucher <phibbi@gmx.net>
2018-06-30 20:18:59 +0200

    fixing test

--- kratos/tests/utilities/test_stl_io.cpp
+++ kratos/tests/utilities/test_stl_io.cpp
@@ -22,27 +22,23 @@ namespace Testing {
 
 KRATOS_TEST_CASE_IN_SUITE(StdVectorOutputStream, KratosCoreFastSuite)
 {
+    using Kratos::operator<<; // needed bcs it is inside the namespace "Testing"
+
     std::vector<int> int_vector {1,5,-63,581,6};
     std::vector<double> double_vector {4.335, 8.639, -888.47, 9874.0};
     std::vector<std::string> string_vector {"val_1", "custom", "again_test"};
 
-    std::cout << int_vector;
+    std::stringstream ss_int;
+    ss_int << int_vector;
+    KRATOS_CHECK_EQUAL(ss_int.str(), "[1, 5, -63, 581, 6]");
 
-    // std::stringstream ss;
-    // ss << out.rdbuf();
-    // std::string myString = ss.str();
+    std::stringstream ss_double;
+    ss_double << double_vector;
+    KRATOS_CHECK_EQUAL(ss_double.str(), "[4.335, 8.639, -888.47, 9874]");
 
-    // std::ostream ss_int;
-    // ss_int << int_vector;
-    // KRATOS_CHECK_EQUAL(ss_int.rdbuf().str(), "label");
-
-    // std::stringstream ss_double;
-    // ss_double << double_vector;
-    // KRATOS_CHECK_EQUAL(ss_double.str(), "label");
-
-    // std::stringstream ss_string;
-    // ss_string << string_vector;
-    // KRATOS_CHECK_EQUAL(ss_string.str(), "label");
+    std::stringstream ss_string;
+    ss_string << string_vector;
+    KRATOS_CHECK_EQUAL(ss_string.str(), "[val_1, custom, again_test]");
 }
 
 }   // namespace Testing
