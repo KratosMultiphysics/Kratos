34cc8be303fadbb9647b991d1866af551167b8d9
Riccardo Rossi <rrossi@cimne.upc.edu>
2018-07-02 19:06:51 +0200

    correction

--- kratos/tests/test_model.py
+++ kratos/tests/test_model.py
@@ -24,9 +24,11 @@ class TestModel(KratosUnittest.TestCase):
         if (sys.version_info < (3, 2)):
             self.assertRaisesRegex = self.assertRaisesRegexp
 
+        self.assertEqual(aaa, current_model["aaa"]) #search by flat name - should be eventually deprecated
+        
         #check that a meaningful error is thrown
-        with self.assertRaisesRegex(RuntimeError, "Error: The ModelPart named : \"aaa\" was not found as root-ModelPart. The total input string was \"aaa\""):
-            current_model["aaa"]
+        with self.assertRaisesRegex(RuntimeError, "Error: The ModelPart named : \"abc\" was not found either as root-ModelPart or as a flat name. The total input string was \"abc\""):
+            current_model["abc"]
 
         #check that a meaningful error is thrown
         with self.assertRaisesRegex(RuntimeError, "The ModelPart named : \"aaa\" was not found as SubModelPart of : \"Inlets\". The total input string was \"Main.Inlets.aaa\""):
