bff23132dc6d8bda7998491e375cd5ed126a5947
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-07-05 20:35:06 +0200

    Removing read_materials_process_with_create

--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
@@ -316,10 +316,7 @@ class MechanicalSolver(PythonSolver):
     def import_constitutive_laws(self):
         materials_filename = self.settings["material_import_settings"]["materials_filename"].GetString()
         if (materials_filename != ""):
-            import read_materials_process_with_create as read_materials_process
-            # Create a dictionary of model parts.
-            Model = KratosMultiphysics.Model()
-            Model.AddModelPart(self.main_model_part)
+            import read_materials_process
             # Add constitutive laws and material properties from json file to model parts.
             read_materials_process.ReadMaterialsProcess(self.model, self.settings["material_import_settings"])
             materials_imported = True
