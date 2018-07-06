7318177a6d83b138ed486633792f9f24e9d17934
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-07-06 09:35:44 +0200

    Moving viscous generalized Maxwell to cpp

---
+++ applications/StructuralMechanicsApplication/custom_constitutive/viscous_generalized_maxwell_3d.cpp
@@ -0,0 +1,146 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:         BSD License
+//                   license: structural_mechanics_application/license.txt
+//
+//  Main authors:    Alejandro Cornejo & Lucia Barbu 
+//  Collaborator:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "utilities/math_utils.h"
+#include "structural_mechanics_application_variables.h"
+#include "custom_constitutive/viscous_generalized_maxwell_3d.h"
+
+namespace Kratos
+{
+void ViscousGeneralizedMaxwell3D::CalculateMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues)
+{
+    this->CalculateMaterialResponseCauchy(rValues);
+}
+void ViscousGeneralizedMaxwell3D::CalculateMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues)
+{
+    this->CalculateMaterialResponseCauchy(rValues);
+}
+void ViscousGeneralizedMaxwell3D::CalculateMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues)
+{
+    this->CalculateMaterialResponseCauchy(rValues);
+}
+
+void ViscousGeneralizedMaxwell3D::CalculateMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues)
+{
+    // Integrate Stress Damage
+    const Properties& rMaterialProperties = rValues.GetMaterialProperties();
+    const int VoigtSize = this->GetStrainSize();
+    Vector& IntegratedStressVector = rValues.GetStressVector(); // To be updated
+    const Vector& StrainVector = rValues.GetStrainVector();
+    Matrix& TangentTensor = rValues.GetConstitutiveMatrix(); // todo modify after integration
+    const ProcessInfo& ProcessInfo = rValues.GetProcessInfo();
+    const double TimeStep = ProcessInfo[DELTA_TIME];
+
+    const double Kvisco    = rMaterialProperties[VISCOUS_PARAMETER]; //  C1/Cinf
+    const double DelayTime = rMaterialProperties[DELAY_TIME];
+
+    // Elastic Matrix
+    Matrix C;
+    this->CalculateElasticMatrix(C, rMaterialProperties);
+
+    const Vector& PreviousStrain  = this->GetPreviousStrainVector();
+    const Vector& PreviousStress  = this->GetPreviousStressVector();
+    const Vector& StrainIncrement = StrainVector - PreviousStrain;
+
+    const double coef = Kvisco * TimeStep / ((1.0 + Kvisco)*2.0*DelayTime);
+    const Vector& Aux = -(StrainVector - StrainIncrement)*std::exp(-TimeStep/DelayTime)*(1.0 + coef)
+                            + StrainVector*(1.0 - coef);
+
+    noalias(IntegratedStressVector) = PreviousStress*std::exp(-TimeStep/DelayTime) + prod(C, Aux);
+    noalias(TangentTensor) = C;
+
+    this->SetNonConvPreviousStressVector(IntegratedStressVector);
+    this->SetNonConvPreviousStrainVector(StrainVector);
+
+} // End CalculateMaterialResponseCauchy
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ViscousGeneralizedMaxwell3D::FinalizeSolutionStep(
+    const Properties& rMaterialProperties,
+    const GeometryType& rElementGeometry,
+    const Vector& rShapeFunctionsValues,
+    const ProcessInfo& rCurrentProcessInfo
+    )
+{
+    // Update the required vectors
+    this->SetPreviousStrainVector(this->GetNonConvPreviousStrainVector());
+    this->SetPreviousStressVector(this->GetNonConvPreviousStressVector());
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ViscousGeneralizedMaxwell3D::CalculateElasticMatrix(
+    Matrix &rElasticityTensor,
+    const Properties &rMaterialProperties
+    )
+{
+    const double E = rMaterialProperties[YOUNG_MODULUS];
+    const double poisson_ratio = rMaterialProperties[POISSON_RATIO];
+    const double lambda =
+        E * poisson_ratio / ((1. + poisson_ratio) * (1.0 - 2.0 * poisson_ratio));
+    const double mu = E / (2.0 + 2.0 * poisson_ratio);
+
+    if (rElasticityTensor.size1() != 6 || rElasticityTensor.size2() != 6)
+        rElasticityTensor.resize(6, 6, false);
+    rElasticityTensor.clear();
+
+    rElasticityTensor(0, 0) = lambda + 2.0 * mu;
+    rElasticityTensor(0, 1) = lambda;
+    rElasticityTensor(0, 2) = lambda;
+    rElasticityTensor(1, 0) = lambda;
+    rElasticityTensor(1, 1) = lambda + 2.0 * mu;
+    rElasticityTensor(1, 2) = lambda;
+    rElasticityTensor(2, 0) = lambda;
+    rElasticityTensor(2, 1) = lambda;
+    rElasticityTensor(2, 2) = lambda + 2.0 * mu;
+    rElasticityTensor(3, 3) = mu;
+    rElasticityTensor(4, 4) = mu;
+    rElasticityTensor(5, 5) = mu;
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ViscousGeneralizedMaxwell3D::FinalizeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ViscousGeneralizedMaxwell3D::FinalizeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ViscousGeneralizedMaxwell3D::FinalizeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ViscousGeneralizedMaxwell3D::FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+} // namespace kratos
--- applications/StructuralMechanicsApplication/custom_constitutive/viscous_generalized_maxwell_3d.h
+++ applications/StructuralMechanicsApplication/custom_constitutive/viscous_generalized_maxwell_3d.h
@@ -7,23 +7,18 @@
 //                   license: structural_mechanics_application/license.txt
 //
 //  Main authors:    Alejandro Cornejo & Lucia Barbu 
+//  Collaborator:    Vicente Mataix Ferrandiz
 //
 
 #if !defined (KRATOS_VISCOUS_GENERALIZED_MAXWELL_H_INCLUDED)
 #define  KRATOS_VISCOUS_GENERALIZED_MAXWELL_H_INCLUDED
 
 // System includes
-#include <string>
-#include <iostream>
+
+// External includes
 
 // Project includes
-#include "includes/define.h"
-#include "includes/serializer.h"
-#include "includes/properties.h"
-#include "utilities/math_utils.h"
-
 #include "includes/constitutive_law.h"
-#include "structural_mechanics_application_variables.h"
 
 namespace Kratos
 {
@@ -79,18 +74,17 @@ public:
     */
     ConstitutiveLaw::Pointer Clone() const override
     {
-        ViscousGeneralizedMaxwell3D::Pointer p_clone
-            (new ViscousGeneralizedMaxwell3D(*this));
-        return p_clone;
+        return Kratos::make_shared<ViscousGeneralizedMaxwell3D>(*this);
     }
 
     /**
     * Copy constructor.
     */
     ViscousGeneralizedMaxwell3D (const ViscousGeneralizedMaxwell3D& rOther)
-    : ConstitutiveLaw(rOther)
+        : ConstitutiveLaw(rOther)
     {
     }
+
     /**
     * Destructor.
     */
@@ -122,120 +116,68 @@ public:
         return 6;
     };
 
-    int GetVoigtSize() {return 6;}
-    int GetWorkingSpaceDimension() {return 3;}
+    /**
+     * Computes the material response in terms of 1st Piola-Kirchhoff stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override;
 
-    Vector GetPreviousStressVector() { return mPrevStressVector; }
-    void SetPreviousStressVector(Vector toStress) { mPrevStressVector = toStress; }
-    Vector GetNonConvPreviousStressVector() { return mNonConvPrevStressVector; }
-    void SetNonConvPreviousStressVector(Vector toStress) { mNonConvPrevStressVector = toStress; }
+    /**
+     * Computes the material response in terms of 2nd Piola-Kirchhoff stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override;
 
-    Vector GetPreviousStrainVector() { return mPrevStrainVector; }
-    void SetPreviousStrainVector(Vector toStrain) { mPrevStrainVector = toStrain; }
-    Vector GetNonConvPreviousStrainVector() { return mNonConvPrevStrainVector; }
-    void SetNonConvPreviousStrainVector(Vector toStrain) { mNonConvPrevStrainVector = toStrain; }
+    /**
+     * Computes the material response in terms of Kirchhoff stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override;
 
+    /**
+     * Computes the material response in terms of Cauchy stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override;
 
-    void CalculateMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override
-    {
-        this->CalculateMaterialResponseCauchy(rValues);
-    }
-    void CalculateMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override
-    {
-        this->CalculateMaterialResponseCauchy(rValues);
-    }
-    void CalculateMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override
-    {
-        this->CalculateMaterialResponseCauchy(rValues);
-    }
-
-    void CalculateMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override
-    {
-        // Integrate Stress Damage
-        const Properties& rMaterialProperties = rValues.GetMaterialProperties();
-        const int VoigtSize = this->GetVoigtSize();
-        Vector& IntegratedStressVector = rValues.GetStressVector(); // To be updated
-        const Vector& StrainVector = rValues.GetStrainVector();
-        Matrix& TangentTensor = rValues.GetConstitutiveMatrix(); // todo modify after integration
-        const ProcessInfo& ProcessInfo = rValues.GetProcessInfo();
-        const double TimeStep = ProcessInfo[DELTA_TIME];
-
-        const double Kvisco    = rMaterialProperties[VISCOUS_PARAMETER]; //  C1/Cinf
-        const double DelayTime = rMaterialProperties[DELAY_TIME];
-
-        // Elastic Matrix
-        Matrix C;
-        this->CalculateElasticMatrix(C, rMaterialProperties);
-
-        const Vector& PreviousStrain  = this->GetPreviousStrainVector();
-        const Vector& PreviousStress  = this->GetPreviousStressVector();
-        const Vector& StrainIncrement = StrainVector - PreviousStrain;
-
-        const double coef = Kvisco * TimeStep / ((1.0 + Kvisco)*2.0*DelayTime);
-        const Vector& Aux = -(StrainVector - StrainIncrement)*std::exp(-TimeStep/DelayTime)*(1.0 + coef)
-                             + StrainVector*(1.0 - coef);
-        
-        noalias(IntegratedStressVector) = PreviousStress*std::exp(-TimeStep/DelayTime) + prod(C, Aux);
-        noalias(TangentTensor) = C;
-
-        this->SetNonConvPreviousStressVector(IntegratedStressVector);
-        this->SetNonConvPreviousStrainVector(StrainVector);
-
-    } // End CalculateMaterialResponseCauchy
-
+    /**
+     * to be called at the end of each solution step
+     * (e.g. from Element::FinalizeSolutionStep)
+     * @param rMaterialProperties the Properties instance of the current element
+     * @param rElementGeometry the geometry of the current element
+     * @param rShapeFunctionsValues the shape functions values in the current integration point
+     * @param the current ProcessInfo instance
+     */
     void FinalizeSolutionStep(
         const Properties& rMaterialProperties,
         const GeometryType& rElementGeometry,
         const Vector& rShapeFunctionsValues,
         const ProcessInfo& rCurrentProcessInfo
-    ) override
-    {
-        // Update the required vectors
-        this->SetPreviousStrainVector(this->GetNonConvPreviousStrainVector());
-        this->SetPreviousStressVector(this->GetNonConvPreviousStressVector());
-    }
+        ) override;
 
-    void CalculateElasticMatrix(
-        Matrix &rElasticityTensor,
-        const Properties &rMaterialProperties
-    )
-    {
-        const double E = rMaterialProperties[YOUNG_MODULUS];
-        const double poisson_ratio = rMaterialProperties[POISSON_RATIO];
-        const double lambda =
-            E * poisson_ratio / ((1. + poisson_ratio) * (1.0 - 2.0 * poisson_ratio));
-        const double mu = E / (2.0 + 2.0 * poisson_ratio);
+    /**
+     * Finalize the material response in terms of 1st Piola-Kirchhoff stresses
+     * @see Parameters
+     */
+    void FinalizeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override;
 
-        if (rElasticityTensor.size1() != 6 || rElasticityTensor.size2() != 6)
-            rElasticityTensor.resize(6, 6, false);
-        rElasticityTensor.clear();
+    /**
+     * Finalize the material response in terms of 2nd Piola-Kirchhoff stresses
+     * @see Parameters
+     */
+    void FinalizeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override;
 
-        rElasticityTensor(0, 0) = lambda + 2.0 * mu;
-        rElasticityTensor(0, 1) = lambda;
-        rElasticityTensor(0, 2) = lambda;
-        rElasticityTensor(1, 0) = lambda;
-        rElasticityTensor(1, 1) = lambda + 2.0 * mu;
-        rElasticityTensor(1, 2) = lambda;
-        rElasticityTensor(2, 0) = lambda;
-        rElasticityTensor(2, 1) = lambda;
-        rElasticityTensor(2, 2) = lambda + 2.0 * mu;
-        rElasticityTensor(3, 3) = mu;
-        rElasticityTensor(4, 4) = mu;
-        rElasticityTensor(5, 5) = mu;
-    }
+    /**
+     * Finalize the material response in terms of Kirchhoff stresses
+     * @see Parameters
+     */
+    void FinalizeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override;
 
-    void FinalizeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues)
-    {
-    }
-    void FinalizeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues)
-    {
-    }
-    void FinalizeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues)
-    {
-    }
-    void FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues)
-    {
-    }
+    /**
+     * Finalize the material response in terms of Cauchy stresses
+     * @see Parameters
+     */
+    void FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override;
 
     ///@}
     ///@name Access
@@ -300,7 +242,6 @@ private:
     Vector mNonConvPrevStressVector = ZeroVector(6);
     Vector mNonConvPrevStrainVector = ZeroVector(6);
 
-
     ///@}
     ///@name Private Operators
     ///@{
@@ -308,6 +249,26 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
+
+    Vector GetPreviousStressVector() { return mPrevStressVector; }
+    void SetPreviousStressVector(Vector toStress) { mPrevStressVector = toStress; }
+    Vector GetNonConvPreviousStressVector() { return mNonConvPrevStressVector; }
+    void SetNonConvPreviousStressVector(Vector toStress) { mNonConvPrevStressVector = toStress; }
+
+    Vector GetPreviousStrainVector() { return mPrevStrainVector; }
+    void SetPreviousStrainVector(Vector toStrain) { mPrevStrainVector = toStrain; }
+    Vector GetNonConvPreviousStrainVector() { return mNonConvPrevStrainVector; }
+    void SetNonConvPreviousStrainVector(Vector toStrain) { mNonConvPrevStrainVector = toStrain; }
+
+    /**
+     * @brief This method computes the elastic tensor
+     * @param rElasticityTensor The elastic tensor
+     * @param rMaterialProperties The material properties
+     */
+    void CalculateElasticMatrix(
+        Matrix &rElasticityTensor,
+        const Properties &rMaterialProperties
+        );
 
     ///@}
     ///@name Private  Access
