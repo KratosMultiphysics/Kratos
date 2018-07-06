49a9d06960fb3e32a9737f4e225dec9653a18818
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-07-06 10:31:54 +0200

    Moving GenericSmallStrainIsotropicPlasticity3D to cpp

--- applications/StructuralMechanicsApplication/custom_constitutive/generic_small_strain_isotropic_plasticity_3d.h
+++ applications/StructuralMechanicsApplication/custom_constitutive/generic_small_strain_isotropic_plasticity_3d.h
@@ -7,24 +7,18 @@
 //                   license: structural_mechanics_application/license.txt
 //
 //  Main authors:    Alejandro Cornejo & Lucia Barbu
+//  Collaborator:    Vicente Mataix Ferrandiz
 //
 
 #if !defined (KRATOS_GENERIC_SMALL_STRAIN_ISOTROPIC_PLASTICITY_3D_H_INCLUDED)
 #define  KRATOS_GENERIC_SMALL_STRAIN_ISOTROPIC_PLASTICITY_3D_H_INCLUDED
 
 // System includes
-#include <string>
-#include <iostream>
+
+// External includes
+
 // Project includes
-#include "includes/define.h"
-#include "includes/serializer.h"
-#include "includes/properties.h"
-#include "utilities/math_utils.h"
 #include "includes/constitutive_law.h"
-#include "structural_mechanics_application_variables.h"
-#include "custom_utilities/tangent_operator_calculator_utility.h"
-
-
 
 namespace Kratos
 {
@@ -81,9 +75,7 @@ public:
     */
     ConstitutiveLaw::Pointer Clone() const override
     {
-        GenericSmallStrainIsotropicPlasticity3D<ConstLawIntegratorType>::Pointer p_clone
-            (new GenericSmallStrainIsotropicPlasticity3D<ConstLawIntegratorType>(*this));
-        return p_clone;
+        return Kratos::make_shared<GenericSmallStrainIsotropicPlasticity3D<ConstLawIntegratorType>>(*this);
     }
 
     /**
@@ -112,25 +104,6 @@ public:
     ///@name Operations
     ///@{
 
-    int GetVoigtSize(){return 6;}
-    int GetWorkingSpaceDimension() {return 3;}
-
-    double GetThreshold() {return mThreshold;}
-    double GetPlasticDissipation() {return mPlasticDissipation;}
-    Vector GetPlasticStrain(){return mPlasticStrain;}
-
-    double GetNonConvThreshold() {return mNonConvThreshold;}
-    double GetNonConvPlasticDissipation() {return mNonConvPlasticDissipation;}
-    Vector GetNonConvPlasticStrain(){return mNonConvPlasticStrain;}
-
-    void SetThreshold(const double& toThreshold) {mThreshold = toThreshold;}
-    void SetPlasticDissipation(const double& toCapap) {mPlasticDissipation = toCapap;}
-    void SetPlasticStrain(const Vector& Ep){mPlasticStrain = Ep;}
-
-    void SetNonConvThreshold(const double& toThreshold) {mNonConvThreshold = toThreshold;}
-    void SetNonConvPlasticDissipation(const double& toCapap) {mNonConvPlasticDissipation = toCapap;}
-    void SetNonConvPlasticStrain(const Vector& Ep){mNonConvPlasticStrain = Ep;}
-
     /**
      * @brief Dimension of the law:
      */
@@ -147,258 +120,91 @@ public:
         return 6;
     };
     
+    /**
+     * Computes the material response in terms of 1st Piola-Kirchhoff stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override;
 
-    void CalculateMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override
-    {
-        this->CalculateMaterialResponseCauchy(rValues);
-    }
-    void CalculateMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override
-    {
-        this->CalculateMaterialResponseCauchy(rValues);
-    }
-    void CalculateMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override
-    {
-        this->CalculateMaterialResponseCauchy(rValues);
-    }
+    /**
+     * Computes the material response in terms of 2nd Piola-Kirchhoff stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override;
 
-    void CalculateMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override
-    {
-        // Integrate Stress plasticity
-        const Properties& rMaterialProperties = rValues.GetMaterialProperties();
-        const int VoigtSize = this->GetVoigtSize();
-        Vector& IntegratedStressVector = rValues.GetStressVector();
-        Matrix& TangentTensor = rValues.GetConstitutiveMatrix(); // todo modify after integration
-        const double CharacteristicLength = rValues.GetElementGeometry().Length();
-        const Flags& ConstitutiveLawOptions = rValues.GetOptions();
+    /**
+     * Computes the material response in terms of Kirchhoff stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override;
 
-        // Elastic Matrix
-        Matrix C;
-        this->CalculateElasticMatrix(C, rMaterialProperties);
+    /**
+     * Computes the material response in terms of Cauchy stresses and constitutive tensor
+     * @see Parameters
+     */
+    void CalculateMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override;
 
-        double Threshold, PlasticDissipation;
-        Vector PlasticStrain = ZeroVector(this->GetVoigtSize());
-
-        Threshold = this->GetThreshold();
-        PlasticDissipation = this->GetPlasticDissipation();
-        PlasticStrain = this->GetPlasticStrain();
-
-        // S0 = C:(E-Ep)
-        Vector PredictiveStressVector = prod(C, rValues.GetStrainVector() - PlasticStrain);
-
-        // Initialize Plastic Parameters
-        double UniaxialStress = 0.0, PlasticDenominator = 0.0;
-        Vector Fflux = ZeroVector(VoigtSize), Gflux = ZeroVector(VoigtSize); // DF/DS & DG/DS
-        Vector PlasticStrainIncrement = ZeroVector(VoigtSize);
-
-        ConstLawIntegratorType::CalculatePlasticParameters(PredictiveStressVector, rValues.GetStrainVector(),
-            UniaxialStress, Threshold, PlasticDenominator, Fflux, Gflux, PlasticDissipation,
-            PlasticStrainIncrement, C, rMaterialProperties, CharacteristicLength);
-
-        const double F = UniaxialStress - Threshold; 
-
-        if (F <= std::abs(1.0e-8 * Threshold)) {   // Elastic case
-
-            noalias(IntegratedStressVector) = PredictiveStressVector;
-            this->SetNonConvPlasticDissipation(PlasticDissipation);
-            this->SetNonConvPlasticStrain(PlasticStrain);
-            this->SetNonConvThreshold(Threshold);
-
-            if (ConstitutiveLawOptions.Is(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR) == true) {
-                noalias(TangentTensor) = C;
-                this->SetValue(UNIAXIAL_STRESS, UniaxialStress, rValues.GetProcessInfo());
-            }
-
-        } else { // Plastic case
-
-            // while loop backward euler 
-            /* Inside "IntegrateStressVector" the PredictiveStressVector
-               is updated to verify the yield criterion */
-            ConstLawIntegratorType::IntegrateStressVector(PredictiveStressVector, rValues.GetStrainVector(), 
-                UniaxialStress, Threshold, PlasticDenominator, Fflux, Gflux, PlasticDissipation, PlasticStrainIncrement, 
-                C, PlasticStrain, rMaterialProperties, CharacteristicLength);
-
-            noalias(IntegratedStressVector) = PredictiveStressVector;
-            this->SetNonConvPlasticDissipation(PlasticDissipation);
-            this->SetNonConvPlasticStrain(PlasticStrain);
-            this->SetNonConvThreshold(Threshold);
-
-            if (ConstitutiveLawOptions.Is(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR) == true) {
-                this->CalculateTangentTensor(rValues); // this modifies the ConstitutiveMatrix
-                noalias(TangentTensor) = rValues.GetConstitutiveMatrix();
-                this->SetValue(UNIAXIAL_STRESS, UniaxialStress, rValues.GetProcessInfo());
-            }
-        }
-        
-    } // End CalculateMaterialResponseCauchy
-
-    void CalculateTangentTensor(ConstitutiveLaw::Parameters& rValues) 
-    {
-        // Calculates the Tangent Constitutive Tensor by perturbation
-        TangentOperatorCalculatorUtility::CalculateTangentTensor(rValues, this);
-    }
-
+    /**
+     * to be called at the end of each solution step
+     * (e.g. from Element::FinalizeSolutionStep)
+     * @param rMaterialProperties the Properties instance of the current element
+     * @param rElementGeometry the geometry of the current element
+     * @param rShapeFunctionsValues the shape functions values in the current integration point
+     * @param the current ProcessInfo instance
+     */
     void FinalizeSolutionStep(
         const Properties& rMaterialProperties,
         const GeometryType& rElementGeometry,
         const Vector& rShapeFunctionsValues,
         const ProcessInfo& rCurrentProcessInfo
-    ) override
-    {
-        this->SetPlasticDissipation(this->GetNonConvPlasticDissipation());
-        this->SetThreshold(this->GetNonConvThreshold());
-        this->SetPlasticStrain(this->GetNonConvPlasticStrain());
-    }
+        ) override;
 
-    void FinalizeMaterialResponse(
-        Parameters& rValues,
-        const StressMeasure& rStressMeasure)
-    {
-        // Integrate Stress plasticity
-        // const Properties& rMaterialProperties = rValues.GetMaterialProperties();
-        // const int VoigtSize = this->GetVoigtSize();
-        // Vector& IntegratedStressVector = rValues.GetStressVector();
-        // Matrix& TangentTensor = rValues.GetConstitutiveMatrix(); // todo modify after integration
-        // const double CharacteristicLength = rValues.GetElementGeometry().Length();
-        // const Flags& ConstitutiveLawOptions = rValues.GetOptions();
+//     /**
+//      * Finalize the material response,  called by the element in FinalizeSolutionStep.
+//      * @see Parameters
+//      * @see StressMeasures
+//      */
+//      void FinalizeMaterialResponse (
+//          Parameters& rValues,
+//          const StressMeasure& rStressMeasure
+//          ) override;
 
-        // // Elastic Matrix
-        // Matrix C;
-        // this->CalculateElasticMatrix(C, rMaterialProperties);
-
-        // double Threshold, PlasticDissipation;
-        // Vector PlasticStrain = ZeroVector(this->GetVoigtSize());
-
-        // Threshold = this->GetThreshold();
-        // PlasticDissipation = this->GetPlasticDissipation();
-        // PlasticStrain = this->GetPlasticStrain();
-
-        // // S0 = C:(E-Ep)
-        // Vector PredictiveStressVector = prod(C, rValues.GetStrainVector() - PlasticStrain);
-
-        // // Initialize Plastic Parameters
-        // double UniaxialStress = 0.0, PlasticDenominator = 0.0;
-        // Vector Fflux = ZeroVector(VoigtSize), Gflux = ZeroVector(VoigtSize); // DF/DS & DG/DS
-        // Vector PlasticStrainIncrement = ZeroVector(VoigtSize);
-
-        // ConstLawIntegratorType::CalculatePlasticParameters(PredictiveStressVector, rValues.GetStrainVector(),
-        //     UniaxialStress, Threshold, PlasticDenominator, Fflux, Gflux, PlasticDissipation,
-        //     PlasticStrainIncrement, C, rMaterialProperties, CharacteristicLength);
-
-        // const double F = UniaxialStress - Threshold; 
-
-        // if (F <= std::abs(1.0e-8 * Threshold)) {   // Elastic case
-
-        //     noalias(IntegratedStressVector) = PredictiveStressVector;
-
-        // } else { // Plastic case
-
-        //     // while loop backward euler 
-        //     /* Inside "IntegrateStressVector" the PredictiveStressVector
-        //        is updated to verify the yield criterion */
-        //     ConstLawIntegratorType::IntegrateStressVector(PredictiveStressVector, rValues.GetStrainVector(), 
-        //         UniaxialStress, Threshold, PlasticDenominator, Fflux, Gflux, PlasticDissipation, PlasticStrainIncrement, 
-        //         C, PlasticStrain, rMaterialProperties, CharacteristicLength);
-
-        //     noalias(IntegratedStressVector) = PredictiveStressVector;
-        // }
-
-        // // Save Internal variables
-        // this->SetPlasticDissipation(PlasticDissipation);
-        // this->SetThreshold(Threshold);
-        // this->SetPlasticStrain(PlasticStrain);
-
-    } // FinalizeMaterialResponse
-
-    void CalculateElasticMatrix(
-        Matrix &rElasticityTensor,
-        const Properties &rMaterialProperties
-    )
-    {
-        const double E = rMaterialProperties[YOUNG_MODULUS];
-        const double poisson_ratio = rMaterialProperties[POISSON_RATIO];
-        const double lambda =
-            E * poisson_ratio / ((1. + poisson_ratio) * (1.0 - 2.0 * poisson_ratio));
-        const double mu = E / (2.0 + 2.0 * poisson_ratio);
-
-        if (rElasticityTensor.size1() != 6 || rElasticityTensor.size2() != 6)
-            rElasticityTensor.resize(6, 6, false);
-        rElasticityTensor.clear();
-
-        rElasticityTensor(0, 0) = lambda + 2.0 * mu;
-        rElasticityTensor(0, 1) = lambda;
-        rElasticityTensor(0, 2) = lambda;
-        rElasticityTensor(1, 0) = lambda;
-        rElasticityTensor(1, 1) = lambda + 2.0 * mu;
-        rElasticityTensor(1, 2) = lambda;
-        rElasticityTensor(2, 0) = lambda;
-        rElasticityTensor(2, 1) = lambda;
-        rElasticityTensor(2, 2) = lambda + 2.0 * mu;
-        rElasticityTensor(3, 3) = mu;
-        rElasticityTensor(4, 4) = mu;
-        rElasticityTensor(5, 5) = mu;
-    }
-    
     /**
      * Finalize the material response in terms of 1st Piola-Kirchhoff stresses
      * @see Parameters
      */
-    void FinalizeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override
-    {
-    }
+    void FinalizeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues) override;
 
     /**
      * Finalize the material response in terms of 2nd Piola-Kirchhoff stresses
      * @see Parameters
      */
-    void FinalizeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override
-    {
-    }
+    void FinalizeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues) override;
 
     /**
      * Finalize the material response in terms of Kirchhoff stresses
      * @see Parameters
      */
-    void FinalizeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override
-    {
-    }
-
+    void FinalizeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues) override;
     /**
      * Finalize the material response in terms of Cauchy stresses
      * @see Parameters
      */
-    void FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override
-    {
-    }
+    void FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues) override;
 
     /**
      * @brief Returns whether this constitutive Law has specified variable (double)
      * @param rThisVariable the variable to be checked for
      * @return true if the variable is defined in the constitutive law
      */
-    bool Has(const Variable<double>& rThisVariable) override
-    {
-        if (rThisVariable == UNIAXIAL_STRESS) {
-            return true;
-        } else if (rThisVariable == PLASTIC_DISSIPATION) {
-            return true;
-        }
-
-        return false;
-    }
+    bool Has(const Variable<double>& rThisVariable) override;
 
     /**
      * @brief Returns whether this constitutive Law has specified variable (Vector)
      * @param rThisVariable the variable to be checked for
      * @return true if the variable is defined in the constitutive law
      */
-    bool Has(const Variable<Vector>& rThisVariable) override
-    {
-        if (rThisVariable == PLASTIC_STRAIN_VECTOR) {
-            return true;
-        }
-
-        return false;
-    }
+    bool Has(const Variable<Vector>& rThisVariable) override;
 
     /**
      * @brief Sets the value of a specified variable (double)
@@ -410,14 +216,7 @@ public:
         const Variable<double>& rThisVariable,
         const double& rValue,
         const ProcessInfo& rCurrentProcessInfo
-        ) override
-    {
-        if (rThisVariable == UNIAXIAL_STRESS) {
-            mUniaxialStress = rValue;
-        } else if (rThisVariable == PLASTIC_DISSIPATION) {
-            mPlasticDissipation = rValue;
-        }
-    }
+        ) override;
 
     /**
      * @brief Sets the value of a specified variable (Vector)
@@ -425,16 +224,11 @@ public:
      * @param rValue new value of the specified variable
      * @param rCurrentProcessInfo the process info
      */
-    virtual void SetValue(
+    void SetValue(
         const Variable<Vector>& rThisVariable,
         const Vector& rValue,
         const ProcessInfo& rCurrentProcessInfo
-        ) override
-    {
-        if(rThisVariable == PLASTIC_STRAIN_VECTOR) {
-            mPlasticStrain = rValue;
-        }
-    }
+        ) override;
 
     /**
      * @brief Returns the value of a specified variable (double)
@@ -445,16 +239,7 @@ public:
     double& GetValue(
         const Variable<double>& rThisVariable,
         double& rValue
-        ) override
-    {
-        if(rThisVariable == UNIAXIAL_STRESS) {
-            rValue = mUniaxialStress;
-        } else if (rThisVariable == PLASTIC_DISSIPATION) {
-            rValue = mPlasticDissipation;
-        }
-
-        return rValue;
-    }
+        ) override;
 
     /**
      * @brief Returns the value of a specified variable (Vector)
@@ -465,14 +250,7 @@ public:
     Vector& GetValue(
         const Variable<Vector>& rThisVariable,
         Vector& rValue
-        ) override
-    {
-        if (rThisVariable == PLASTIC_STRAIN_VECTOR) {
-            rValue = mPlasticStrain;
-        }
-
-        return rValue;
-    }
+        ) override;
 
     ///@}
     ///@name Access
@@ -537,10 +315,11 @@ private:
     // Non Converged values
     double mNonConvPlasticDissipation = 0.0;
     double mNonConvThreshold = 0.0;
-    Vector mNonConvPlasticStrain = ZeroVector(this->GetVoigtSize());
+    Vector mNonConvPlasticStrain = ZeroVector(this->GetStrainSize());
 
-    // auxiliar to print
+    // Auxiliar to print (NOTE: Alejandro do we need this now?)
     double mUniaxialStress = 0.0;
+
     ///@}
     ///@name Private Operators
     ///@{
@@ -548,6 +327,38 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
+
+    double GetThreshold() {return mThreshold;}
+    double GetPlasticDissipation() {return mPlasticDissipation;}
+    Vector GetPlasticStrain(){return mPlasticStrain;}
+
+    double GetNonConvThreshold() {return mNonConvThreshold;}
+    double GetNonConvPlasticDissipation() {return mNonConvPlasticDissipation;}
+    Vector GetNonConvPlasticStrain(){return mNonConvPlasticStrain;}
+
+    void SetThreshold(const double& toThreshold) {mThreshold = toThreshold;}
+    void SetPlasticDissipation(const double& toCapap) {mPlasticDissipation = toCapap;}
+    void SetPlasticStrain(const Vector& Ep){mPlasticStrain = Ep;}
+
+    void SetNonConvThreshold(const double& toThreshold) {mNonConvThreshold = toThreshold;}
+    void SetNonConvPlasticDissipation(const double& toCapap) {mNonConvPlasticDissipation = toCapap;}
+    void SetNonConvPlasticStrain(const Vector& Ep){mNonConvPlasticStrain = Ep;}
+
+    /**
+     * @brief This method computes the tangent tensor
+     * @param rValues The constitutive law parameters and flags
+     */
+    void CalculateTangentTensor(ConstitutiveLaw::Parameters& rValues);
+
+    /**
+     * @brief This method computes the elastic tensor
+     * @param rElasticityTensor The elastic tensor
+     * @param rMaterialProperties The material properties
+     */
+    void CalculateElasticMatrix(
+        Matrix &rElasticityTensor,
+        const Properties &rMaterialProperties
+        );
 
     ///@}
     ///@name Private  Access
