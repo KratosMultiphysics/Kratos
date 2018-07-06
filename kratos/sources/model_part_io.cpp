736b0983fccf3c69568fcdbe30f6dc74cee0e7b3
Carlos Roig <roigcarlo@gmail.com>
2018-07-06 09:54:55 +0200

    We have moved to std::shared_ptr so we can use the check

--- kratos/sources/model_part_io.cpp
+++ kratos/sources/model_part_io.cpp
@@ -70,12 +70,12 @@ namespace Kratos
       , mOptions(Options)
     {
         // nullptr test can be confusing with Kratos::shared_ptr. Commented until we move to std::shared_ptr
-        // if (Stream == nullptr)
-        //    KRATOS_THROW_ERROR(std::invalid_argument, "Error: ModelPartIO Stream is invalid ", "");
+        if (Stream == nullptr)
+           KRATOS_THROW_ERROR(std::invalid_argument, "Error: ModelPartIO Stream is invalid ", "");
 
         // Check if the pointer was .reset() or never initialized and if its a NULL pointer)
-        // if (Stream == NULL || Stream == Kratos::shared_ptr<std::iostream>(NULL))
-        //    KRATOS_THROW_ERROR(std::invalid_argument, "Error: ModelPartIO Stream is invalid ", "");
+        if (Stream == NULL || Stream == Kratos::shared_ptr<std::iostream>(NULL))
+           KRATOS_THROW_ERROR(std::invalid_argument, "Error: ModelPartIO Stream is invalid ", "");
 
         mpStream = Stream;
     }
