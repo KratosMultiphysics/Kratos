710a77fafc2a5bfdd019a0052d6c7cc1d2d32206
MFusseder <martin.fusseder@tum.de>
2018-07-09 15:32:00 +0200

    Improvements in strain energy response

--- applications/StructuralMechanicsApplication/custom_response_functions/response_utilities/adjoint_strain_energy_response_function.cpp
+++ applications/StructuralMechanicsApplication/custom_response_functions/response_utilities/adjoint_strain_energy_response_function.cpp
@@ -47,13 +47,13 @@ namespace Kratos
         ModelPart& r_model_part = this->GetModelPart();
 
         #pragma omp parallel for
-        for(int i=0; i< static_cast<int>(r_model_part.Elements().size()); i++)
+        for(int i=0; i< static_cast<int>(r_model_part.Elements().size()); ++i)
         {
             ModelPart::ElementsContainerType::iterator it = r_model_part.ElementsBegin() + i;
             it->Initialize();
         }
         #pragma omp parallel for
-        for(int i=0; i< static_cast<int>(r_model_part.Conditions().size()); i++)
+        for(int i=0; i< static_cast<int>(r_model_part.Conditions().size()); ++i)
         {
             ModelPart::ConditionsContainerType::iterator it = r_model_part.ConditionsBegin() + i;
             it->Initialize();
