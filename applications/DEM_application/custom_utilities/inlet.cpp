a39ed99f59ac311e24ef0120d02a09a12a982c02
ferran <farrufat@cimne.upc.edu>
2018-07-09 10:52:04 +0200

    change to double instead

--- applications/DEM_application/custom_utilities/inlet.cpp
+++ applications/DEM_application/custom_utilities/inlet.cpp
@@ -501,10 +501,10 @@ namespace Kratos {
                     const double maximum_time_until_release = estimated_mass_of_a_particle * mesh_size_elements / mass_flow;
                     double minimum_velocity = mean_radius * 3.0 / maximum_time_until_release; //The distance necessary to get out of the injector, over the time.
                     array_1d<double, 3> & proposed_velocity = mp[INLET_INITIAL_PARTICLES_VELOCITY];
-                    array_1d<double, 3> & limited_velocity = mp[INLET_MAX_PARTICLES_VELOCITY];
+                    double& limited_velocity = mp[INLET_MAX_PARTICLES_VELOCITY];
                     const double modulus_of_proposed_velocity = DEM_MODULUS_3(proposed_velocity);
                     const double factor = 2.0;
-                    if (minimum_velocity > DEM_MODULUS_3(limited_velocity)) {minimum_velocity = DEM_MODULUS_3(limited_velocity);}
+                    if (minimum_velocity > limited_velocity) {minimum_velocity = limited_velocity;}
                     DEM_MULTIPLY_BY_SCALAR_3(proposed_velocity, factor * minimum_velocity / modulus_of_proposed_velocity);
                 }
             }
