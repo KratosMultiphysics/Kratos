5950d8a3caeaf6609962fd24769dc4bb49a1a4ab
ferran <farrufat@cimne.upc.edu>
2018-07-10 11:26:05 +0200

    fix

--- applications/DEM_application/custom_utilities/inlet.cpp
+++ applications/DEM_application/custom_utilities/inlet.cpp
@@ -499,13 +499,14 @@ namespace Kratos {
                     const double density = mInletModelPart.GetProperties(mp[PROPERTIES_ID])[PARTICLE_DENSITY];
                     const double estimated_mass_of_a_particle = density * 4.0/3.0 * Globals::Pi * mean_radius * mean_radius * mean_radius;
                     const double maximum_time_until_release = estimated_mass_of_a_particle * mesh_size_elements / mass_flow;
-                    double minimum_velocity = mean_radius * 3.0 / maximum_time_until_release; //The distance necessary to get out of the injector, over the time.
+                    const double minimum_velocity = mean_radius * 3.0 / maximum_time_until_release; //The distance necessary to get out of the injector, over the time.
                     array_1d<double, 3> & proposed_velocity = mp[INLET_INITIAL_PARTICLES_VELOCITY];
                     double& limited_velocity = mp[INLET_MAX_PARTICLES_VELOCITY];
                     const double modulus_of_proposed_velocity = DEM_MODULUS_3(proposed_velocity);
                     const double factor = 2.0;
-                    if (minimum_velocity > limited_velocity) {minimum_velocity = limited_velocity;}
-                    DEM_MULTIPLY_BY_SCALAR_3(proposed_velocity, factor * minimum_velocity / modulus_of_proposed_velocity);
+                    double injection_speed = factor * minimum_velocity;
+                    if (injection_speed > limited_velocity) {injection_speed = limited_velocity;}
+                    DEM_MULTIPLY_BY_SCALAR_3(proposed_velocity, injection_speed / modulus_of_proposed_velocity);
                 }
             }
             else {
