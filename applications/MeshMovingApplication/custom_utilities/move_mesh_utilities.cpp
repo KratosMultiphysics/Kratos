9ce2778601223b39b852e5630b8efee614eec65f
Riccardo Rossi <rrossi@cimne.upc.edu>
2018-07-12 16:57:16 +0200

    fixing a dangling pointer issue

--- applications/MeshMovingApplication/custom_utilities/move_mesh_utilities.cpp
+++ applications/MeshMovingApplication/custom_utilities/move_mesh_utilities.cpp
@@ -124,12 +124,11 @@ void SetMeshToInitialConfiguration(
 
 //******************************************************************************
 //******************************************************************************
-ModelPart* GenerateMeshPart(ModelPart &rModelPart,
+std::unique_ptr<ModelPart> GenerateMeshPart(ModelPart &rModelPart,
                                     const std::string &rElementName) {
   KRATOS_TRY;
 
-  ModelPart* pmesh_model_part;
-  pmesh_model_part = Kratos::make_shared<ModelPart>("MeshPart", 1);
+  std::unique_ptr<ModelPart> pmesh_model_part = Kratos::make_unique<ModelPart>("MeshPart", 1);
 
   // initializing mesh nodes and variables
   pmesh_model_part->Nodes() = rModelPart.Nodes();
@@ -149,7 +148,7 @@ ModelPart* GenerateMeshPart(ModelPart &rModelPart,
     rmesh_elements.push_back(p_element);
   }
 
-  return pmesh_model_part;
+  return std::move(pmesh_model_part);
 
   KRATOS_CATCH("");
 }
