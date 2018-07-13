2575589dc013b934fd4f11ded17c23067a667020
Tobias Teschemacher <tobias.teschemacher@tum.de>
2018-07-13 16:37:31 +0200

    Cleanup in TrussDiscreteElement

--- applications/IGAStructuralMechanicsApplication/custom_elements/truss_discrete_element.cpp
+++ applications/IGAStructuralMechanicsApplication/custom_elements/truss_discrete_element.cpp
@@ -4,8 +4,8 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/IGAStructuralMechanicsApplication/license.txt
+//  License:         BSD License
+//                     Kratos default license: kratos/IGAStructuralMechanicsApplication/license.txt
 //
 //  Main authors:    Tobias Teschemacher
 //                   Riccardo Rossi
@@ -30,207 +30,97 @@
 
 namespace Kratos
 {
-	//************************************************************************************
-	//************************************************************************************
-	void TrussDiscreteElement::EquationIdVector(
-		EquationIdVectorType& rResult,
-		ProcessInfo& rCurrentProcessInfo
-	)
-	{
-		KRATOS_TRY;
-		unsigned int number_of_nodes = GetGeometry().size();
-		unsigned int dim = number_of_nodes * 3;
+    //************************************************************************************
+    //************************************************************************************
+    void TrussDiscreteElement::CalculateAll(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo,
+        const bool CalculateStiffnessMatrixFlag,
+        const bool CalculateResidualVectorFlag
+    )
+    {
+        KRATOS_TRY
+        // definition of problem size
+        const unsigned int number_of_control_points = GetGeometry().size();
+        unsigned int number_of_dofs = number_of_control_points * 3;
 
-		if (rResult.size() != dim)
-			rResult.resize(dim);
+        //set up properties for Constitutive Law
+        ConstitutiveLaw::Parameters Values(GetGeometry(), GetProperties(), rCurrentProcessInfo);
 
-		for (unsigned int i = 0; i < number_of_nodes; i++)
-		{
-			int index = i * 3;
-			rResult[index]     = GetGeometry()[i].GetDof(DISPLACEMENT_X).EquationId();
-			rResult[index + 1] = GetGeometry()[i].GetDof(DISPLACEMENT_Y).EquationId();
-			rResult[index + 2] = GetGeometry()[i].GetDof(DISPLACEMENT_Z).EquationId();
-		}
-		KRATOS_CATCH("")
-	};
+        Values.GetOptions().Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, false);
+        Values.GetOptions().Set(ConstitutiveLaw::COMPUTE_STRESS);
+        Values.GetOptions().Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR);
 
-	//************************************************************************************
-	//************************************************************************************
-	void TrussDiscreteElement::GetDofList(
-		DofsVectorType& rElementalDofList,
-		ProcessInfo& rCurrentProcessInfo
-	)
-	{
-		KRATOS_TRY;
-		//if (rElementalDofList.size() != msElementSize) {
-		//	rElementalDofList.resize(msElementSize);
-		//}
+        //resizing as needed the LHS
+        if (CalculateStiffnessMatrixFlag == true) //calculation of the matrix is required
+        {
+            if (rLeftHandSideMatrix.size1() != number_of_dofs)
+                rLeftHandSideMatrix.resize(number_of_dofs, number_of_dofs);
+            noalias(rLeftHandSideMatrix) = ZeroMatrix(number_of_dofs, number_of_dofs); //resetting LHS
+        }
+        //resizing as needed the RHS
+        if (CalculateResidualVectorFlag == true) //calculation of the matrix is required
+        {
+            if (rRightHandSideVector.size() != number_of_dofs)
+                rRightHandSideVector.resize(number_of_dofs);
+            rRightHandSideVector = ZeroVector(number_of_dofs); //resetting RHS
+        }
 
-		//for (int i = 0; i < msNumberOfNodes; ++i) {
-		//	int index = i * msLocalSize;
-		//	rElementalDofList[index] = this->GetGeometry()[i].pGetDof(DISPLACEMENT_X);
-		//	rElementalDofList[index + 1] = this->GetGeometry()[i].pGetDof(DISPLACEMENT_Y);
-		//	rElementalDofList[index + 2] = this->GetGeometry()[i].pGetDof(ROTATION_Z);
-		//}
+        //reading in of integration weight, shape function values and shape function derivatives
+        const double& integration_weight = this->GetValue(INTEGRATION_WEIGHT);
+        const Vector&   N     = this->GetValue(SHAPE_FUNCTION_VALUES);
+        const Matrix&  DN_De  = this->GetValue(SHAPE_FUNCTION_LOCAL_DERIVATIVES);
 
-		rElementalDofList.resize(0);
+        //Bending stabilization
+        const double E = GetProperties()[YOUNG_MODULUS];
+        const double area = GetProperties()[CROSS_AREA];
+        const double prestress = GetProperties()[PRESTRESS_CAUCHY];
 
-		for (unsigned int i = 0; i < GetGeometry().size(); i++)
-		{
-			rElementalDofList.push_back(GetGeometry()[i].pGetDof(DISPLACEMENT_X));
-			rElementalDofList.push_back(GetGeometry()[i].pGetDof(DISPLACEMENT_Y));
-			rElementalDofList.push_back(GetGeometry()[i].pGetDof(DISPLACEMENT_Z));
-		}
+        Vector base_vector = ZeroVector(3);
+        GetBaseVector(base_vector, DN_De);
 
-		KRATOS_CATCH("")
-	};
-	//***********************************************************************************
-	//***********************************************************************************
-	void TrussDiscreteElement::FinalizeSolutionStep(
-		ProcessInfo& rCurrentProcessInfo)
-	{
-		mConstitutiveLawVector[0]->FinalizeSolutionStep(GetProperties(),
-			GetGeometry(),
-			this->GetValue(SHAPE_FUNCTION_VALUES),
-			rCurrentProcessInfo);
-	}
-	//************************************************************************************
-	//************************************************************************************
-	void TrussDiscreteElement::InitializeMaterial()
-	{
-		KRATOS_TRY
+        double A0 = norm_2(mBaseVector0);
+        double a = norm_2(base_vector);
 
-			if (GetProperties()[CONSTITUTIVE_LAW] != NULL)
-			{
-				//get shape functions for evaluation of stresses inside the constitutive law
-				const Vector&  N = this->GetValue(SHAPE_FUNCTION_VALUES);
-				const double integration_weight = this->GetValue(INTEGRATION_WEIGHT);
+        //stresses
+        //Green Lagrange formulation (strain)
+        double E11_membrane = 0.5 * (pow(a, 2) - pow(A0,2)); 
 
-                mConstitutiveLawVector[0] = GetProperties()[CONSTITUTIVE_LAW]->Clone();
-				ProcessInfo emptyProcessInfo = ProcessInfo();
-                mConstitutiveLawVector[0]->InitializeMaterial(GetProperties(), GetGeometry(), N);
-			}
-			else
-			{
-				KRATOS_ERROR << "A constitutive law needs to be specified for the element with ID " << this->Id() << std::endl;
-			}
+        //normal force
+        double S11_membrane = prestress * area + E11_membrane * area * E / pow(A0,2);
 
-		KRATOS_CATCH("");
-	}
-	//************************************************************************************
-	//************************************************************************************
-	void TrussDiscreteElement::CalculateAll(
-		MatrixType& rLeftHandSideMatrix,
-		VectorType& rRightHandSideVector,
-		ProcessInfo& rCurrentProcessInfo,
-		const bool CalculateStiffnessMatrixFlag,
-		const bool CalculateResidualVectorFlag
-	)
-	{
-		KRATOS_TRY
-		// definition of problem size
-		const unsigned int number_of_control_points = GetGeometry().size();
-		unsigned int number_of_dofs = number_of_control_points * 3;
+        // 1st variation of the axial strain 
+        Vector epsilon_var_dof = ZeroVector(number_of_dofs);
+        Get1stVariationsAxialStrain(epsilon_var_dof, base_vector, 3, DN_De);
+        epsilon_var_dof = epsilon_var_dof / pow(A0, 2);
 
-		//set up properties for Constitutive Law
-		ConstitutiveLaw::Parameters Values(GetGeometry(), GetProperties(), rCurrentProcessInfo);
+        // 2nd variation of the axial strain 
+        Matrix epsilon_var_2_dof = ZeroMatrix(number_of_dofs, number_of_dofs);
+        Get2ndVariationsAxialStrain(epsilon_var_2_dof, 3, DN_De);
+        epsilon_var_2_dof = epsilon_var_2_dof / pow(A0, 2);
 
-		Values.GetOptions().Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, false);
-		Values.GetOptions().Set(ConstitutiveLaw::COMPUTE_STRESS);
-		Values.GetOptions().Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR);
+        // LEFT HAND SIDE MATRIX
+        if (CalculateStiffnessMatrixFlag == true)
+        {
+            for (int r = 0; r < number_of_dofs; r++) {
+                for (int s = 0; s < number_of_dofs; s++) {
+                    rLeftHandSideMatrix(r, s) = E * area * epsilon_var_dof[r] * epsilon_var_dof[s] + S11_membrane * epsilon_var_2_dof(r, s);
+                }
+            }
+        }
 
-		//resizing as needed the LHS
-		if (CalculateStiffnessMatrixFlag == true) //calculation of the matrix is required
-		{
-			if (rLeftHandSideMatrix.size1() != number_of_dofs)
-				rLeftHandSideMatrix.resize(number_of_dofs, number_of_dofs);
-			noalias(rLeftHandSideMatrix) = ZeroMatrix(number_of_dofs, number_of_dofs); //resetting LHS
-		}
-		//resizing as needed the RHS
-		if (CalculateResidualVectorFlag == true) //calculation of the matrix is required
-		{
-			if (rRightHandSideVector.size() != number_of_dofs)
-				rRightHandSideVector.resize(number_of_dofs);
-			rRightHandSideVector = ZeroVector(number_of_dofs); //resetting RHS
-		}
+        // RIGHT HAND SIDE VECTOR
+        if (CalculateResidualVectorFlag == true) //calculation of the matrix is required
+        {
+            rRightHandSideVector = -S11_membrane * epsilon_var_dof;
+        }
 
-		//reading in of integration weight, shape function values and shape function derivatives
-		const double& integration_weight = this->GetValue(INTEGRATION_WEIGHT);
-		const Vector&   N     = this->GetValue(SHAPE_FUNCTION_VALUES);
-		const Matrix&  DN_De  = this->GetValue(SHAPE_FUNCTION_LOCAL_DERIVATIVES);
+        rLeftHandSideMatrix = rLeftHandSideMatrix * integration_weight;
+        rRightHandSideVector = rRightHandSideVector * integration_weight;
 
-		//Bending stabilization
-		double E = GetProperties()[YOUNG_MODULUS];
-		double area = GetProperties()[AREA];
-		double prestress = GetProperties()[PRESTRESS_CAUCHY];
-
-		Vector base_vector = ZeroVector(3);
-		GetBaseVector(base_vector, DN_De);
-
-		double a0 = norm_2(mBaseVector0);
-		double a = norm_2(base_vector);
-
-		//stresses
-		double E11_membrane = 0.5 * (pow(a, 2) - pow(a0,2));   //Green Lagrange formulation (strain)
-		//cfloat E11_b = (c_b - c_B);   //Green Lagrange formulation (strain)
-		//cfloat E11_n = (c_n - c_N);   //Green Lagrange formulation (strain)
-
-		double S11_membrane = prestress * area + E11_membrane * area*E / pow(a0,2);//normal force
-
-		// 1st variation
-		// variation of the axial strain 
-		Vector epsilon_var_dof = ZeroVector(number_of_dofs);
-		Get1stVariationsAxialStrain(epsilon_var_dof, base_vector, 3, DN_De);
-		epsilon_var_dof = epsilon_var_dof / pow(a0, 2);
-
-		// 2nd variation
-		// variation of the axial strain 
-		Matrix epsilon_var_2_dof = ZeroMatrix(number_of_dofs, number_of_dofs);
-		Get2ndVariationsAxialStrain(epsilon_var_2_dof, 3, DN_De);
-		epsilon_var_2_dof = epsilon_var_2_dof / pow(a0, 2);
-
-		for (int r = 0; r<number_of_dofs; r++)
-			for (int s = 0; s<number_of_dofs; s++)
-			{
-				rLeftHandSideMatrix(r, s) = E * area * epsilon_var_dof[r] * epsilon_var_dof[s] + S11_membrane * epsilon_var_2_dof(r, s);
-			}
-
-		rRightHandSideVector = -S11_membrane * epsilon_var_dof;
-
-		rLeftHandSideMatrix = rLeftHandSideMatrix * integration_weight;
-		rRightHandSideVector = rRightHandSideVector * integration_weight;
-
-		KRATOS_CATCH("");
-	}
-
-
-	//************************************************************************************
-	//************************************************************************************
-	void TrussDiscreteElement::CalculateOnIntegrationPoints(
-		const Variable<double>& rVariable,
-		std::vector<double>& rOutput,
-		const ProcessInfo& rCurrentProcessInfo
-	)
-	{
-		if (rOutput.size() != 1)
-			rOutput.resize(1);
-
-		rOutput[0] = 0.0;// mConstitutiveLawVector[0]->GetValue(rVariable, rOutput[0]);
-	}
-
-	void TrussDiscreteElement::CalculateOnIntegrationPoints(
-		const Variable<Vector>& rVariable,
-		std::vector<Vector>& rValues,
-		const ProcessInfo& rCurrentProcessInfo
-	)
-	{
-		if (rValues.size() != 1)
-		{
-			rValues.resize(1);
-		}
-
-		rValues[0] = ZeroVector(3);
-	}
+        KRATOS_CATCH("");
+    }
 } // Namespace Kratos
 
 
--- applications/IGAStructuralMechanicsApplication/custom_elements/truss_discrete_element.h
+++ applications/IGAStructuralMechanicsApplication/custom_elements/truss_discrete_element.h
@@ -5,14 +5,11 @@
 // System includes
 #include "includes/define.h"
 #include "includes/element.h"
-//#include "includes/ublas_interface.h"
 #include "includes/variables.h"
 
 // External includes
-//#include "boost/smart_ptr.hpp"
 
 // Project includes
-#include "custom_elements/meshless_base_element.h"
 #include "custom_elements/curve_base_discrete_element.h"
 
 namespace Kratos
@@ -34,133 +31,79 @@ public:
     ///@name Life Cycle
     ///@{
     /// Default constructor.
-	// Constructor using an array of nodes
-	TrussDiscreteElement(IndexType NewId, GeometryType::Pointer pGeometry)
-		: CurveBaseDiscreteElement(NewId, pGeometry)
-	{};
-	// Constructor using an array of nodes with properties
-	TrussDiscreteElement(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties)
-		: CurveBaseDiscreteElement(NewId, pGeometry, pProperties)
-	{};
+    // Constructor using an array of nodes
+    TrussDiscreteElement(IndexType NewId, GeometryType::Pointer pGeometry)
+        : CurveBaseDiscreteElement(NewId, pGeometry)
+    {};
+    // Constructor using an array of nodes with properties
+    TrussDiscreteElement(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties)
+        : CurveBaseDiscreteElement(NewId, pGeometry, pProperties)
+    {};
 
-	// default constructor necessary for serialization
-	TrussDiscreteElement() : CurveBaseDiscreteElement() {};
+    // default constructor necessary for serialization
+    TrussDiscreteElement() : CurveBaseDiscreteElement() {};
 
-	/// Destructor.
-	virtual ~TrussDiscreteElement() override
-	{};
+    /// Destructor.
+    virtual ~TrussDiscreteElement() override
+    {};
 
-	Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const override
-	{
-		return Kratos::make_shared< TrussDiscreteElement >(NewId, GetGeometry().Create(ThisNodes), pProperties);
-	};
+    Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const override
+    {
+        return Kratos::make_shared< TrussDiscreteElement >(NewId, GetGeometry().Create(ThisNodes), pProperties);
+    };
 
     ///@}
     ///@name Operations
-	///@{
+    ///@{
 
-	/**
-	* This functions calculates both the RHS and the LHS
-	* @param rLeftHandSideMatrix: The LHS
-	* @param rRightHandSideVector: The RHS
-	* @param rCurrentProcessInfo: The current process info instance
-	* @param CalculateStiffnessMatrixFlag: The flag to set if compute the LHS
-	* @param CalculateResidualVectorFlag: The flag to set if compute the RHS
-	*/
-	void CalculateAll(
-		MatrixType& rLeftHandSideMatrix,
-		VectorType& rRightHandSideVector,
-		ProcessInfo& rCurrentProcessInfo,
-		const bool CalculateStiffnessMatrixFlag,
-		const bool CalculateResidualVectorFlag
-	) override;
+    /**
+    * This functions calculates both the RHS and the LHS
+    * @param rLeftHandSideMatrix: The LHS
+    * @param rRightHandSideVector: The RHS
+    * @param rCurrentProcessInfo: The current process info instance
+    * @param CalculateStiffnessMatrixFlag: The flag to set if compute the LHS
+    * @param CalculateResidualVectorFlag: The flag to set if compute the RHS
+    */
+    void CalculateAll(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo,
+        const bool CalculateStiffnessMatrixFlag,
+        const bool CalculateResidualVectorFlag
+    ) override;
 
-	/**
-	* Calculate a double Variable on the Element Constitutive Law
-	* @param rVariable: The variable we want to get
-	* @param rOutput: The values obtained int the integration points
-	* @param rCurrentProcessInfo: the current process info instance
-	*/
-	void CalculateOnIntegrationPoints(
-		const Variable<double>& rVariable,
-		std::vector<double>& rOutput,
-		const ProcessInfo& rCurrentProcessInfo
-	) override;
-
-	/**
-	* Calculate a Vector Variable on the Element Constitutive Law
-	* @param rVariable: The variable we want to get
-	* @param rOutput: The values obtained int the integration points
-	* @param rCurrentProcessInfo: the current process info instance
-	*/
-	void CalculateOnIntegrationPoints(
-		const Variable<Vector>& rVariable,
-		std::vector<Vector>& rValues,
-		const ProcessInfo& rCurrentProcessInfo
-	) override;
-
-	/**
-	* Sets on rResult the ID's of the element degrees of freedom
-	* @param rResult: The vector containing the equation id
-	* @param rCurrentProcessInfo: The current process info instance
-	*/
-	void EquationIdVector(
-		EquationIdVectorType& rResult,
-		ProcessInfo& rCurrentProcessInfo
-	) override;
-
-	/**
-	* Sets on rElementalDofList the degrees of freedom of the considered element geometry
-	* @param rElementalDofList: The vector containing the dof of the element
-	* @param rCurrentProcessInfo: The current process info instance
-	*/
-	void GetDofList(
-		DofsVectorType& rElementalDofList,
-		ProcessInfo& rCurrentProcessInfo
-	) override;
-
-	///@}
+    ///@}
 
 protected:
-	/**
-	* It initializes the material
-	*/
-	virtual void InitializeMaterial();
-	/**
-	* Called at the end of eahc solution step
-	* @param rCurrentProcessInfo: the current process info instance
-	*/
-	void FinalizeSolutionStep(ProcessInfo& rCurrentProcessInfo) override;
 
 private:
-	///@name Static Member Variables
-	///@{
-	///@}
-	///@name Operations
-	///@{
+    ///@name Static Member Variables
+    ///@{
+    ///@}
+    ///@name Operations
+    ///@{
 
+    ///@}
+    ///@name Serialization
+    ///@{
 
-	///@}
-	///@name Serialization
-	///@{
+    friend class Serializer;
 
-	friend class Serializer;
+    void save(Serializer& rSerializer) const override
+    {
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Element)
+    }
 
-	void save(Serializer& rSerializer) const override
-	{
-		KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Element)
-	}
+    void load(Serializer& rSerializer) override
+    {
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Element)
+    }
 
-	void load(Serializer& rSerializer) override
-	{
-		KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Element)
-	}
+    ///@}
 
-	///@}
-
-};	 // Class TrussDiscreteElement
+};  // Class TrussDiscreteElement
 ///@}
 
 }  // namespace Kratos.
 
-#endif // KRATOS_MESHLESS_TRUSS_DISCRETE_ELEMENT_H_INCLUDED  defined 
+#endif // KRATOS_TRUSS_DISCRETE_ELEMENT_H_INCLUDED  defined 
