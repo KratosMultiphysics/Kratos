e5ca5f0ca2d3b486e38fb6e8ede3d314b9429343
Tobias Teschemacher <tobias.teschemacher@tum.de>
2018-07-17 16:46:21 +0200

    Load Curve Discrete Condition

---
+++ applications/IGAStructuralMechanicsApplication/custom_conditions/load_curve_discrete_condition.cpp
@@ -0,0 +1,97 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/IGAStructuralMechanicsApplication/license.txt
+//
+//  Main authors:    Tobias Teschemacher
+//
+
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "custom_conditions/load_curve_discrete_condition.h"
+#include "utilities/math_utils.h"
+#include "includes/define.h"
+
+#include "iga_structural_mechanics_application_variables.h"
+#include "iga_structural_mechanics_application.h"
+
+
+namespace Kratos
+{
+    //************************************************************************************
+    //************************************************************************************
+    void LoadCurveDiscreteCondition::CalculateAll(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo,
+        const bool CalculateStiffnessMatrixFlag,
+        const bool CalculateResidualVectorFlag
+    )
+    {
+        const int number_of_control_points = GetGeometry().size();
+        const int mat_size = number_of_control_points * 3;
+
+        Vector fLoads = ZeroVector(mat_size);
+
+        if (rLeftHandSideMatrix.size1() != mat_size)
+            rLeftHandSideMatrix.resize(mat_size, mat_size, false);
+        noalias(rLeftHandSideMatrix) = ZeroMatrix(mat_size, mat_size); //resetting LHS
+
+        if (rRightHandSideVector.size() != mat_size)
+            rRightHandSideVector.resize(mat_size, false);
+        rRightHandSideVector = ZeroVector(mat_size); //resetting RHS
+
+        const Vector& N = this->GetValue(SHAPE_FUNCTION_VALUES);
+
+        // Edge loads
+        if (this->Has(LINE_LOAD))
+        {
+            array_1d<double, 3> line_load = this->GetValue(LINE_LOAD);
+
+            for (unsigned int i = 0; i < number_of_control_points; i++)
+            {
+                int index = 3 * i;
+                fLoads[index]     = - line_load[0] * N[i];
+                fLoads[index + 1] = - line_load[1] * N[i];
+                fLoads[index + 2] = - line_load[2] * N[i];
+            }
+        }
+
+        // Pressure loads
+        if (this->Has(PRESSURE))
+        {
+            const double integration_weight = this->GetValue(INTEGRATION_WEIGHT);
+            const Matrix& DN_De = this->GetValue(SHAPE_FUNCTION_LOCAL_DERIVATIVES);
+
+            Vector g3 = ZeroVector(3);
+            CalculateBaseVector(g3, DN_De);
+
+            const double dArea = norm_2(g3);
+            const double integration_weight_area = integration_weight * dArea;
+
+            double pressure = this->GetValue(PRESSURE);
+
+            array_1d<double, 3> direction = g3 / dArea;
+
+            for (int i = 0; i < number_of_control_points; i++)
+            {
+                int index = 3 * i;
+                fLoads[index]     = - direction[0] * pressure * integration_weight_area * N[i];
+                fLoads[index + 1] = - direction[1] * pressure * integration_weight_area * N[i];
+                fLoads[index + 2] = - direction[2] * pressure * integration_weight_area * N[i];
+            }
+        }
+
+        noalias(rRightHandSideVector) -= fLoads;
+    }
+} // Namespace Kratos
---
+++ applications/IGAStructuralMechanicsApplication/custom_conditions/load_curve_discrete_condition.h
@@ -0,0 +1,134 @@
+#if !defined(KRATOS_LOAD_CURVE_DISCRETE_CONDITION_H_INCLUDED )
+#define  KRATOS_LOAD_CURVE_DISCRETE_CONDITION_H_INCLUDED
+
+
+
+// System includes
+
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/serializer.h"
+#include "includes/condition.h"
+#include "includes/variables.h"
+
+#include "iga_structural_mechanics_application_variables.h"
+#include "custom_conditions/curve_base_discrete_condition.h"
+
+
+namespace Kratos
+{
+    class LoadCurveDiscreteCondition
+        : public CurveBaseDiscreteCondition
+    {
+    public:
+        ///@name Type Definitions
+        ///@{
+
+        /// Counted pointer of LoadCurveDiscreteCondition
+        KRATOS_CLASS_POINTER_DEFINITION(LoadCurveDiscreteCondition);
+
+        ///@}
+        ///@name Life Cycle
+        ///@{
+
+        /// Default constructor.
+        // Constructor using an array of nodes
+        LoadCurveDiscreteCondition(IndexType NewId, GeometryType::Pointer pGeometry)
+            : CurveBaseDiscreteCondition(NewId, pGeometry)
+        {};
+        // Constructor using an array of nodes with properties
+        LoadCurveDiscreteCondition(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties)
+            : CurveBaseDiscreteCondition(NewId, pGeometry, pProperties)
+        {};
+
+        LoadCurveDiscreteCondition() : CurveBaseDiscreteCondition()
+        {};
+
+        /// Destructor.
+        virtual ~LoadCurveDiscreteCondition() override
+        {};
+
+        Condition::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const override
+        {
+            return Kratos::make_shared< LoadCurveDiscreteCondition >(NewId, GetGeometry().Create(ThisNodes), pProperties);
+        };
+
+
+        ///@}
+        ///@name Operators
+        ///@{
+
+
+        ///@}
+        ///@name Operations
+        ///@{
+
+        /**
+        * This functions calculates both the RHS and the LHS
+        * @param rLeftHandSideMatrix: The LHS
+        * @param rRightHandSideVector: The RHS
+        * @param rCurrentProcessInfo: The current process info instance
+        * @param CalculateStiffnessMatrixFlag: The flag to set if compute the LHS
+        * @param CalculateResidualVectorFlag: The flag to set if compute the RHS
+        */
+        void CalculateAll(
+            MatrixType& rLeftHandSideMatrix,
+            VectorType& rRightHandSideVector,
+            ProcessInfo& rCurrentProcessInfo,
+            const bool CalculateStiffnessMatrixFlag,
+            const bool CalculateResidualVectorFlag
+        );
+
+        /// Turn back information as a string.
+        std::string Info() const override
+        {
+            std::stringstream buffer;
+            buffer << "\"LoadCurveDiscreteCondition\" #" << Id();
+            return buffer.str();
+        }
+
+        /// Print information about this object.
+        void PrintInfo(std::ostream& rOStream) const override
+        {
+            rOStream << "\"LoadCurveDiscreteCondition\" #" << Id();
+        }
+
+        /// Print object's data.
+        void PrintData(std::ostream& rOStream) const {
+            pGetGeometry()->PrintData(rOStream);
+        }
+
+        ///@}
+
+    protected:
+
+    private:
+        ///@name Static Member Variables
+        ///@{
+        
+        ///@}
+        ///@name Member Variables
+        ///@{
+
+        friend class Serializer;
+
+        virtual void save(Serializer& rSerializer) const override
+        {
+            KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, CurveBaseDiscreteCondition);
+        }
+
+        virtual void load(Serializer& rSerializer) override
+        {
+            KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, CurveBaseDiscreteCondition);
+        }
+        ///@}
+
+    }; // Class LoadCurveDiscreteCondition
+
+
+}  // namespace Kratos.
+
+#endif // KRATOS_LOAD_CURVE_DISCRETE_CONDITION_H_INCLUDED  defined 
