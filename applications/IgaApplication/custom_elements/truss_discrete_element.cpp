2b6d0a9a71e3e277a0684eb34eabd97983ab1229
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-07-24 17:06:17 +0200

    Do not store variations in matrix
    performance increasing ~20%

--- applications/IgaApplication/custom_elements/truss_discrete_element.cpp
+++ applications/IgaApplication/custom_elements/truss_discrete_element.cpp
@@ -106,37 +106,45 @@ void TrussDiscreteElement::CalculateAll(
     const double reference_a = norm_2(mBaseVector0);
     const double actual_a = norm_2(actual_base_vector);
 
+    const double actual_aa = actual_a * actual_a;
+    const double reference_aa = reference_a * reference_a;
+
     // green-lagrange strain
 
-    const double e11_membrane = 0.5 * (actual_a * actual_a - reference_a *
-        reference_a);
+    const double e11_membrane = 0.5 * (actual_aa - reference_aa);
 
     // normal force
 
     const double s11_membrane = prestress * A + e11_membrane * A * E /
-        (reference_a * reference_a);
+        reference_aa;
 
-    // 1st variation of the axial strain
+    for (size_t r = 0; r < number_of_dofs; r++) {
+        const size_t dof_type_r = r % DofsPerNode();
+        const size_t shape_index_r = r / DofsPerNode();
 
-    Vector epsilon_var_1_dof = ZeroVector(number_of_dofs);
-    Get1stVariationsAxialStrain(epsilon_var_1_dof, actual_base_vector, 3,
-        shape_derivatives);
-    epsilon_var_1_dof = epsilon_var_1_dof / (reference_a * reference_a);
+        const double epsilon_var_r = actual_base_vector[dof_type_r] *
+            shape_derivatives(shape_index_r, 0) / reference_aa;
 
-    // 2nd variation of the axial strain 
+        for (size_t s = 0; s < number_of_dofs; s++) {
+            const size_t dof_type_s = s % DofsPerNode();
+            const size_t shape_index_s = s / DofsPerNode();
 
-    Matrix epsilon_var_2 = ZeroMatrix(number_of_dofs, number_of_dofs);
-    Get2ndVariationsAxialStrain(epsilon_var_2, 3, shape_derivatives);
-    epsilon_var_2 = epsilon_var_2 / (reference_a * reference_a);
+            const double epsilon_var_s = actual_base_vector[dof_type_s] *
+                shape_derivatives(shape_index_s, 0) / reference_aa;
 
-    for (std::size_t r = 0; r < number_of_dofs; r++) {
-        for (std::size_t s = 0; s < number_of_dofs; s++) {
-            rLeftHandSideMatrix(r, s) = E * A * epsilon_var_1_dof[r] *
-                epsilon_var_1_dof[s] + s11_membrane * epsilon_var_2(r, s);
+            rLeftHandSideMatrix(r, s) = E * A * epsilon_var_r * epsilon_var_s;
+
+            if (dof_type_r == dof_type_s) {
+                const double epsilon_var_rs =
+                    shape_derivatives(shape_index_r, 0) *
+                    shape_derivatives(shape_index_s, 0) / reference_aa;
+
+                rLeftHandSideMatrix(r, s) += s11_membrane * epsilon_var_rs;
         }
     }
 
-    rRightHandSideVector = -s11_membrane * epsilon_var_1_dof;
+        rRightHandSideVector[r] = -s11_membrane * epsilon_var_r;
+    }
 
     rLeftHandSideMatrix *= integration_weight * reference_a;
     rRightHandSideVector *= integration_weight * reference_a;
