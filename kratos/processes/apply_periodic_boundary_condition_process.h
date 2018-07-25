eb4c276c1d0f1084835db97ba4fed56fbaca1fe7
AdityaGhantasala <shine.aditya@gmail.com>
2018-07-25 11:19:05 +0200

    bare empty class of periodic bcs

---
+++ kratos/processes/apply_periodic_boundary_condition_process.h
@@ -0,0 +1,41 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Aditya Ghantasala
+//
+//
+
+#ifndef APPLY_PERIODIC_CONDITION_PROCESS_H
+#define APPLY_PERIODIC_CONDITION_PROCESS_H
+
+// System includes
+#include <string>
+#include <iostream>
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "geometries/point.h"
+#include "processes/process.h"
+#include "utilities/math_utils.h"
+#include "includes/kratos_parameters.h"
+#include "utilities/binbased_fast_point_locator.h"
+#include "spatial_containers/spatial_containers.h"
+
+
+namespace Kratos
+{
+
+class ApplyPeriodicConditionProcess : public Process
+{
+    
+};
+
+}
