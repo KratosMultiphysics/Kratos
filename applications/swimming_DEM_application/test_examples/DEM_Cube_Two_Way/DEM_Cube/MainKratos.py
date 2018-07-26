d85da37ca74d0f461029fdec5afdef211bc3b3a1
Unknown <jgonzalez@cimne.upc.edu>
2018-07-26 12:18:21 +0200

    Added one example with one and two way coupling and eliminated another

---
+++ applications/swimming_DEM_application/test_examples/DEM_Cube_Two_Way/DEM_Cube/MainKratos.py
@@ -0,0 +1,10 @@
+import KratosMultiphysics
+import KratosMultiphysics.ExternalSolversApplication
+import KratosMultiphysics.PfemApplication
+import KratosMultiphysics.PfemFluidDynamicsApplication     
+import KratosMultiphysics.SolidMechanicsApplication
+
+import MainFluidPFEM
+
+MainFluidPFEM.Solution().Run()
+
