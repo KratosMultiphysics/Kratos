bfbf8234cd1a0d054001606016bfabf5b67e2587
msandre <michael.andre@tum.de>
2018-07-30 14:47:50 +0200

    Added pybind trampoline for DragResponseFunction.

--- applications/FluidDynamicsApplication/custom_python/add_custom_response_functions_to_python.cpp
+++ applications/FluidDynamicsApplication/custom_python/add_custom_response_functions_to_python.cpp
@@ -10,6 +10,27 @@
 
 namespace Kratos
 {
+
+template<unsigned TDim>
+class PyDragResponseFunction : public DragResponseFunction<TDim>
+{
+    public:
+        using DragResponseFunction<TDim>::DragResponseFunction;
+        void Initialize() override {
+            PYBIND11_OVERLOAD(void, DragResponseFunction<TDim>, Initialize, );
+        }
+        void InitializeSolutionStep() override {
+            PYBIND11_OVERLOAD(void, DragResponseFunction<TDim>, InitializeSolutionStep, );
+        }
+        void FinalizeSolutionStep() override {
+            PYBIND11_OVERLOAD(void, DragResponseFunction<TDim>, FinalizeSolutionStep, );
+        }
+        double CalculateValue() override {
+            PYBIND11_OVERLOAD_PURE(double, DragResponseFunction<TDim>, CalculateValue, );
+        }
+};
+
+
 namespace Python
 {
 using namespace pybind11;
@@ -23,12 +44,14 @@ void AddCustomResponseFunctionsToPython(pybind11::module& m)
 
     class_<
         DragResponseFunction<2>,
+        PyDragResponseFunction<2>,
         DragResponseFunction<2>::Pointer,
         AdjointResponseFunction>(m,"DragResponseFunction2D")
         .def(init<Parameters&, ModelPart&>());
 
     class_<
         DragResponseFunction<3>,
+        PyDragResponseFunction<3>,
         DragResponseFunction<3>::Pointer,
         AdjointResponseFunction>(m,"DragResponseFunction3D")
         .def(init<Parameters&, ModelPart&>());
