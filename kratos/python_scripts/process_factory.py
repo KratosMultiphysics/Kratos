8e2e626a292b843a6f745026ef623c06bd895aff
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-07-30 16:56:18 +0200

    Adding description+error

--- kratos/python_scripts/process_factory.py
+++ kratos/python_scripts/process_factory.py
@@ -9,12 +9,18 @@ class KratosProcessFactory(object):
         constructed_processes = []
         for i in range(0,process_list.size()):
             item = process_list[i]
+            # The kratos_module is the application where the script must be loaded. ex. KratosMultiphysics.StructuralMechanicsApplication
             if(item.Has("kratos_module")):
                 kratos_module = __import__(item["kratos_module"].GetString())
+            # The python_module is the actual scrpt that must be launch
             if(item.Has("python_module")):
                 python_module = __import__(item["python_module"].GetString())
                 p = python_module.Factory(item, self.Model)
                 constructed_processes.append( p )
+            else: # Otherwise an error is thrown
+                Logger.PrintWarning("Your list of processes: ", process_list)
+                raise NameError("python_module must be defined in your parameters. Check all your processes")
+                
                 
         return constructed_processes
     
