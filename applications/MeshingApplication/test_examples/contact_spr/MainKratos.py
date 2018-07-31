a05275afcb4e47266da0849e7e00d9c2296f7e8d
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-07-31 18:43:15 +0200

    Adding contact example

---
+++ applications/MeshingApplication/test_examples/contact_spr/MainKratos.py
@@ -0,0 +1,18 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing Kratos
+import KratosMultiphysics as KM
+import KratosMultiphysics.StructuralMechanicsApplication as SMA
+import KratosMultiphysics.ContactStructuralMechanicsApplication as CSMA
+
+import adaptative_contact_structural_mechanics_analysis as Analysis
+
+## Import define_output
+with open("ProjectParameters.json",'r') as parameter_file:
+    ProjectParameters = KM.Parameters(parameter_file.read())
+
+# Creating the test
+model = KM.Model()
+analysis = Analysis.AdaptativeContactStructuralMechanicsAnalysis(model, ProjectParameters)
+KM.Logger.GetDefaultOutput().SetSeverity(KM.Logger.Severity.INFO)
+analysis.Run()
