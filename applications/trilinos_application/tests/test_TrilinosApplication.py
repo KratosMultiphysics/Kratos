c10b45718f841fd8ca9f2f480b3f29667a99eac9
philbucher <philipp.bucher@tum.de>
2018-08-02 08:01:45 +0200

    adding test to trilinos tests

--- applications/trilinos_application/tests/test_TrilinosApplication.py
+++ applications/trilinos_application/tests/test_TrilinosApplication.py
@@ -16,6 +16,7 @@ import test_mpi_communicator
 import test_trilinos_matrix
 import test_trilinos_redistance
 import test_trilinos_levelset_convection
+import test_kratos_mpi_interface
 
 def AssembleTestSuites():
     ''' Populates the test suites to run.
@@ -39,13 +40,14 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_trilinos_matrix.TestTrilinosMatrix]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_trilinos_redistance.TestTrilinosRedistance]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_trilinos_levelset_convection.TestTrilinosLevelSetConvection]))
-    
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_kratos_mpi_interface.TestKratosMPIInterface]))
+
     # Create a test suite with the selected tests plus all small tests
     nightSuite = suites['nightly']
     nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_trilinos_linear_solvers.TestLinearSolvers]))
     nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_mpi_communicator.TestMPICommunicator]))
     nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_trilinos_matrix.TestTrilinosMatrix]))
-    
+
     # Create a test suite that contains all the tests:
     allSuite = suites['all']
     allSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_trilinos_linear_solvers.TestLinearSolvers,
