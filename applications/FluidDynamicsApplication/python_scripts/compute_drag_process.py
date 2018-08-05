045125085beb6ef13b079d28bc15fd6cb40e85ed
aditya_laptop <shine.aditya@gmail.com>
2018-08-05 19:03:31 +0200

    moving setting write buffer to file writer utility

--- applications/FluidDynamicsApplication/python_scripts/compute_drag_process.py
+++ applications/FluidDynamicsApplication/python_scripts/compute_drag_process.py
@@ -36,14 +36,12 @@ class ComputeDragProcess(KratosMultiphysics.Process):
                 "print_format"              : ".8f",
                 "write_drag_output_file"    : true,
                 "output_file_settings": {
-                    "write_buffer_size" : -1
+                    "write_buffer_size" : 1
                 }
             }
             """)
 
         self.params = params
-        # check the set write_buffer_size
-        self.initial_write_buffer_size = self.params["output_file_settings"]["write_buffer_size"].GetInteger()
         # Detect "End" as a tag and replace it by a large number
         if(self.params.Has("interval")):
             if(self.params["interval"][1].IsString()):
@@ -86,14 +84,6 @@ class ComputeDragProcess(KratosMultiphysics.Process):
                 else:
                     file_handler_params.AddEmptyValue("file_name")
                     file_handler_params["file_name"].SetString(output_file_name)
-
-                is_mpi_execution = (model_part.GetCommunicator().TotalProcesses() > 1)
-                if not is_mpi_execution and self.initial_write_buffer_size != -1:
-                    info_msg  = "File output buffer size set to 1 \n"
-                    info_msg += "for ComputeDragProcess output file "+ self.file_name
-                    KratosMultiphysics.Logger.PrintInfo("ComputeDragProcess", info_msg)
-                    file_handler_params["write_buffer_size"].SetInteger(1)
-
                 file_header = self._GetFileHeader()
                 self.output_file = TimeBasedAsciiFileWriterUtility(self.model_part,
                     file_handler_params, file_header).file
