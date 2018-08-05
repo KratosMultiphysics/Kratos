ff85167b5194cfc5ed878530ad52486bfcb80802
philbucher <phibbi@gmx.net>
2018-08-05 18:04:02 +0200

    Adding warnings in Fluid

--- applications/FluidDynamicsApplication/python_scripts/fluid_dynamics_analysis.py
+++ applications/FluidDynamicsApplication/python_scripts/fluid_dynamics_analysis.py
@@ -15,6 +15,15 @@ class FluidDynamicsAnalysis(AnalysisStage):
     def __init__(self,model,parameters):
         # Deprecation warnings
         solver_settings = parameters["solver_settings"]
+
+        if solver_settings.Has("domain_size") and parameters["problem_data"].Has("domain_size"):
+            warn_msg = '"domain_size" defined twice, using the one in "solver_settings"'
+            Kratos.Logger.PrintWarning("FluidDynamicsAnalysis", warn_msg)
+
+        if solver_settings.Has("model_part_name") and parameters["problem_data"].Has("model_part_name"):
+            warn_msg = '"model_part_name" defined twice, using the one in "solver_settings"'
+            Kratos.Logger.PrintWarning("FluidDynamicsAnalysis", warn_msg)
+
         if not solver_settings.Has("domain_size") and parameters["problem_data"].Has("domain_size"):
             Kratos.Logger.PrintInfo("FluidDynamicsAnalysis", "Using the old way to pass the domain_size, this will be removed!")
             solver_settings.AddEmptyValue("domain_size")
