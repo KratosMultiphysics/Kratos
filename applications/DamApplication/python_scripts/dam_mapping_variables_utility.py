d440e065b30967246c10f195039bdd9bda18921c
Unknown <djvicente@cimne.upc.edu>
2018-08-10 12:30:36 +0200

    Creation of different functions for each type of transfer result problem

--- applications/DamApplication/python_scripts/dam_mapping_variables_utility.py
+++ applications/DamApplication/python_scripts/dam_mapping_variables_utility.py
@@ -11,7 +11,7 @@ import shutil
 
 class MappingVariablesUtility:
 
-    def __init__(self,domain_size,main_model_part,post_model_part_mechanical,post_model_part_thermal,type_of_results):
+    def __init__(self,domain_size):
 
         # Construct the utility
         self.domain_size = domain_size
@@ -20,24 +20,37 @@ class MappingVariablesUtility:
         else:
             self.MappingVariablesUtility = KratosDam.MappingVariables3DUtilities()
 
-        main_model_part = self.GenereateNewModelPart(main_model_part,
-                                                     post_model_part_mechanical,
-                                                     post_model_part_thermal,
-                                                     type_of_results)
 
-    def GenereateNewModelPart(self,main_model_part,post_model_part_mechanical,post_model_part_thermal,type_of_results):
+    def GenerateNewModelPartMechanical(self,main_model_part,post_model_part_mechanical):
+        ### Mapping between old and new model parts ------------------------------------------------------------------
+        self.MappingVariablesUtility.MappingMechanicalModelParts(post_model_part_mechanical,main_model_part)
+
+        # delete auxiliary model_part
+        del post_model_part_mechanical
+
+        return main_model_part
+
+    def GenerateNewModelPartThermal(self,main_model_part,post_model_part_thermal):
 
         ### Mapping between old and new model parts ------------------------------------------------------------------
-        if type_of_results == "Mechanical":
-            self.MappingVariablesUtility.MappingMechanicalModelParts(post_model_part_mechanical,main_model_part)
-        if type_of_results == "Thermal":
-            self.MappingVariablesUtility.MappingThermalModelParts(post_model_part_thermal,main_model_part)
-        if type_of_results == "Thermo-Mechanical":
-            self.MappingVariablesUtility.MappingMechanicalModelParts(post_model_part_mechanical,main_model_part)
-            self.MappingVariablesUtility.MappingThermalModelParts(post_model_part_thermal,main_model_part)
+        self.MappingVariablesUtility.MappingThermalModelParts(post_model_part_thermal,main_model_part)
+
+        # delete auxiliary model_part
+        del post_model_part_thermal
+
+        return main_model_part
+
+    def GenerateNewModelPartThermoMechanical(self,main_model_part,post_model_part_mechanical,post_model_part_thermal):
+
+        ### Mapping between old and new model parts ------------------------------------------------------------------
+        self.MappingVariablesUtility.MappingMechanicalModelParts(post_model_part_mechanical,main_model_part)
+        self.MappingVariablesUtility.MappingThermalModelParts(post_model_part_thermal,main_model_part)
 
         # delete auxiliary model_part
         del post_model_part_mechanical
         del post_model_part_thermal
 
         return main_model_part
+
+
+
