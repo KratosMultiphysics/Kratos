3cef5ad271faa7d82c13c90fd0ded3bccc7c8325
Miguel Masó <mmaso@cimne.upc.edu>
2018-08-10 13:38:38 +0200

    updating mdpa with generic elements

--- applications/ShallowWaterApplication/custom_problemtype/ShallowWaterApplication.gid/Mdpa.tcl
+++ applications/ShallowWaterApplication/custom_problemtype/ShallowWaterApplication.gid/Mdpa.tcl
@@ -26,8 +26,8 @@ proc WriteMdpa { basename dir problemtypedir } {
     ## Nodes
     set Nodes [GiD_Info Mesh Nodes]
     puts $FileVar "Begin Nodes"
-    for {set i 0} {$i < [llength $Nodes]} {incr i 4} {
-        # puts $FileVar "  [lindex $Nodes $i]  [lindex $Nodes [expr { $i+1 }]] [lindex $Nodes [expr { $i+2 }]] [lindex $Nodes [expr { $i+3 }]]"
+    for {set i 0} {$i < [llength $Nodes]} {incr i 4}
+    {
         puts -nonewline $FileVar "  [lindex $Nodes $i]  "
         puts -nonewline $FileVar [format  "%.10f" [lindex $Nodes [expr { $i+1 }]]]
         puts -nonewline $FileVar " "
@@ -44,46 +44,14 @@ proc WriteMdpa { basename dir problemtypedir } {
     set FrameworkType [GiD_AccessValue get gendata Framework]
     # Body_Part
     set Groups [GiD_Info conditions Bottom_friction groups]
-    if {$FrameworkType eq "Pfem2"} {
-        if {$VariablesType eq "Primitive"} {
-            for {set i 0} {$i < [llength $Groups]} {incr i} {
-                # Elements Property
-                set BodyElemsProp [dict get $PropertyDict [lindex [lindex $Groups $i] 1]]
-                # PrimitiveVarElement2D3N
-                WriteElements FileVar [lindex $Groups $i] triangle PrimitiveVarElement2D3N $BodyElemsProp Triangle2D3Connectivities
-                # PrimitiveVarElement2D4N
-                WriteElements FileVar [lindex $Groups $i] quadrilateral PrimitiveVarElement2D4N $BodyElemsProp Quadrilateral2D4Connectivities
-            }
-        } else {
-            for {set i 0} {$i < [llength $Groups]} {incr i} {
-                # Elements Property
-                set BodyElemsProp [dict get $PropertyDict [lindex [lindex $Groups $i] 1]]
-                # ConservedVarElement2D3N
-                WriteElements FileVar [lindex $Groups $i] triangle ConservedVarElement2D3N $BodyElemsProp Triangle2D3Connectivities
-                # ConservedVarElement2D4N
-                WriteElements FileVar [lindex $Groups $i] quadrilateral ConservedVarElement2D4N $BodyElemsProp Quadrilateral2D4Connectivities
-            }
-        }
-    } else {
-        if {$VariablesType eq "Primitive"} {
-            for {set i 0} {$i < [llength $Groups]} {incr i} {
-                # Elements Property
-                set BodyElemsProp [dict get $PropertyDict [lindex [lindex $Groups $i] 1]]
-                # EulerPrimVarElement2D3N
-                WriteElements FileVar [lindex $Groups $i] triangle EulerPrimVarElement2D3N $BodyElemsProp Triangle2D3Connectivities
-                # EulerPrimVarElement2D4N
-                WriteElements FileVar [lindex $Groups $i] quadrilateral EulerPrimVarElement2D4N $BodyElemsProp Quadrilateral2D4Connectivities
-            }
-        } else {
-            for {set i 0} {$i < [llength $Groups]} {incr i} {
-                # Elements Property
-                set BodyElemsProp [dict get $PropertyDict [lindex [lindex $Groups $i] 1]]
-                # EulerConsVarElement2D3N
-                WriteElements FileVar [lindex $Groups $i] triangle EulerConsVarElement2D3N $BodyElemsProp Triangle2D3Connectivities
-                # EulerConsVarElement2D4N
-                WriteElements FileVar [lindex $Groups $i] quadrilateral EulerConsVarElement2D4N $BodyElemsProp Quadrilateral2D4Connectivities
-            }
-        }
+    for {set i 0} {$i < [llength $Groups]} {incr i}
+    {
+        # Elements Property
+        set BodyElemsProp [dict get $PropertyDict [lindex [lindex $Groups $i] 1]]
+        # Trinagles: Element2D3N
+        WriteElements FileVar [lindex $Groups $i] triangle Element2D3N $BodyElemsProp Triangle2D3Connectivities
+        # Quadrilaterals: Element2D4N
+        WriteElements FileVar [lindex $Groups $i] quadrilateral Element2D4N $BodyElemsProp Quadrilateral2D4Connectivities
     }
 
 
@@ -96,21 +64,21 @@ proc WriteMdpa { basename dir problemtypedir } {
     set Groups [GiD_Info conditions Slip_condition groups]
     if {$Dim eq 2} {
         # NothingCondition2D2N
-        WriteFaceConditions FileVar ConditionId ConditionDict $Groups NothingCondition2D2N $PropertyDict
+        WriteFaceConditions FileVar ConditionId ConditionDict $Groups Condition2D2N $PropertyDict
     }
 
     # Water_height
     set Groups [GiD_Info conditions Water_height groups]
     if {$Dim eq 2} {
         # NothingCondition2D2N
-        WriteFaceConditions FileVar ConditionId ConditionDict $Groups NothingCondition2D2N $PropertyDict
+        WriteFaceConditions FileVar ConditionId ConditionDict $Groups Condition2D2N $PropertyDict
     }
 
     # Imposed_flux
     set Groups [GiD_Info conditions Imposed_flux groups]
     if {$Dim eq 2} {
         # NothingCondition2D2N
-        WriteFaceConditions FileVar ConditionId ConditionDict $Groups NothingCondition2D2N $PropertyDict
+        WriteFaceConditions FileVar ConditionId ConditionDict $Groups Condition2D2N $PropertyDict
     }
 
 
@@ -130,6 +98,4 @@ proc WriteMdpa { basename dir problemtypedir } {
 
 
     close $FileVar
-
-    #~ return $TableDict
 }
