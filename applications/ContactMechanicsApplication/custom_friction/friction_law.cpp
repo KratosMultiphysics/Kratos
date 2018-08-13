261e6a081c339098ce328390389618bd52b4baaa
Josep Maria <cpuigbo@cimne.upc.edu>
2018-08-14 00:49:58 +0200

    clean friction law

--- applications/ContactMechanicsApplication/custom_friction/friction_law.cpp
+++ applications/ContactMechanicsApplication/custom_friction/friction_law.cpp
@@ -12,28 +12,12 @@
 namespace Kratos
 {
 
-   /**
-    * Constructor.
-    */
-   FrictionLaw::FrictionLaw()
+   void FrictionLaw::InitializeSolutionStep()
    {
-      mPlasticSlip = 0.0;
-      mPlasticSlipNew = 0.0;
-      mDeltaPlasticSlip = 0.0;
-   }
-
-   /**
-    * Clone function (has to be implemented by any derived class)
-    * @return a pointer to a new instance of this friction law
-    */
-   FrictionLaw::Pointer FrictionLaw::Clone() const
-   {
-      return Kratos::make_shared<FrictionLaw>(*this);
    }
 
    void FrictionLaw::FinalizeSolutionStep()
    {
-
       mDeltaPlasticSlip = mPlasticSlipNew - mPlasticSlip;
       mPlasticSlip = mPlasticSlipNew;
    }
@@ -197,4 +181,5 @@ namespace Kratos
    }
 
 
-} // namespace Kratos
+}
+// namespace Kratos
