e3ea3ff6dc48d304454e9f6ffa8fcfeba9875873
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-08-19 11:51:45 +0200

    New embedded discontinuous element tests addition and test values

--- applications/FluidDynamicsApplication/tests/cpp_tests/test_fluid_element_data.cpp
+++ applications/FluidDynamicsApplication/tests/cpp_tests/test_fluid_element_data.cpp
@@ -20,6 +20,7 @@
 
 #include "custom_utilities/fluid_element_data.h"
 #include "custom_constitutive/newtonian_2d_law.h"
+#include "fluid_dynamics_application_variables.h"
 
 namespace Kratos {
 namespace Testing {
@@ -267,6 +268,164 @@ KRATOS_TEST_CASE_IN_SUITE(FluidElementDataCheck, FluidDynamicsApplicationFastSui
     KRATOS_CHECK_EQUAL(process_info_data.DeltaTime, 0.0);
 }
 
+KRATOS_TEST_CASE_IN_SUITE(EmbeddedDiscontinuousElement2D3N, FluidDynamicsApplicationFastSuite)
+{
+    ModelPart model_part("Main");
+    model_part.SetBufferSize(3);
+
+    // Variables addition
+    model_part.AddNodalSolutionStepVariable(DENSITY); // TODO: To be removed once the element migration is finally finished
+    model_part.AddNodalSolutionStepVariable(DYNAMIC_VISCOSITY); // TODO: To be removed once the element migration is finally finished
+    model_part.AddNodalSolutionStepVariable(BODY_FORCE);
+    model_part.AddNodalSolutionStepVariable(DYNAMIC_TAU);
+    model_part.AddNodalSolutionStepVariable(SOUND_VELOCITY);
+    model_part.AddNodalSolutionStepVariable(PRESSURE);
+    model_part.AddNodalSolutionStepVariable(VELOCITY);
+    model_part.AddNodalSolutionStepVariable(MESH_VELOCITY);
+    model_part.AddNodalSolutionStepVariable(DISTANCE);
+    model_part.AddNodalSolutionStepVariable(ACCELERATION);
+
+    // For VMS comparison
+    model_part.AddNodalSolutionStepVariable(NODAL_AREA);
+    model_part.AddNodalSolutionStepVariable(ADVPROJ);
+    model_part.AddNodalSolutionStepVariable(DIVPROJ);
+
+
+    // Process info creation
+    double delta_time = 0.1;
+    model_part.GetProcessInfo().SetValue(DYNAMIC_TAU, 0.001);
+    model_part.GetProcessInfo().SetValue(SOUND_VELOCITY, 1.0e+3);
+    model_part.GetProcessInfo().SetValue(DELTA_TIME, delta_time);
+    model_part.GetProcessInfo().SetValue(SLIP_LENGTH, 1.0e+8);
+    model_part.GetProcessInfo().SetValue(PENALTY_COEFFICIENT, 10.0);
+    Vector bdf_coefs(3);
+    bdf_coefs[0] = 3.0/(2.0*delta_time);
+    bdf_coefs[1] = -2.0/delta_time;
+    bdf_coefs[2] = 0.5*delta_time;
+    model_part.GetProcessInfo().SetValue(BDF_COEFFICIENTS, bdf_coefs);
+
+    // Set the element properties
+    Properties::Pointer p_properties = model_part.pGetProperties(0);
+    p_properties->SetValue(DENSITY, 1000.0);
+    p_properties->SetValue(DYNAMIC_VISCOSITY, 1.0e-05);
+    ConstitutiveLaw::Pointer pConsLaw(new Newtonian2DLaw());
+    p_properties->SetValue(CONSTITUTIVE_LAW, pConsLaw);
+
+    // Geometry creation
+    model_part.CreateNewNode(1, 0.0, 0.0, 0.0);
+    model_part.CreateNewNode(2, 1.0, 0.0, 0.0);
+    model_part.CreateNewNode(3, 0.0, 1.0, 0.0);
+
+    for (ModelPart::NodeIterator it_node=model_part.NodesBegin(); it_node<model_part.NodesEnd(); ++it_node){
+        it_node->AddDof(VELOCITY_X,REACTION_X);
+        it_node->AddDof(VELOCITY_Y,REACTION_Y);
+        it_node->AddDof(VELOCITY_Z,REACTION_Z);
+        it_node->AddDof(PRESSURE,REACTION_WATER_PRESSURE);
+    }
+
+    std::vector<ModelPart::IndexType> element_nodes {1, 2, 3};
+    model_part.CreateNewElement("EmbeddedQSVMSDiscontinuous2D3N", 1, element_nodes, p_properties);
+    model_part.CreateNewElement("EmbeddedSymbolicNavierStokesDiscontinuous2D3N", 2, element_nodes, p_properties);
+
+    // Define the nodal values
+    Matrix reference_velocity(3,2);
+    reference_velocity(0,0) = 0.0; reference_velocity(0,1) = 0.1;
+    reference_velocity(1,0) = 0.1; reference_velocity(1,1) = 0.2;
+    reference_velocity(2,0) = 0.2; reference_velocity(2,1) = 0.3;
+
+    Element::Pointer p_element = model_part.pGetElement(1);
+
+    for(unsigned int i=0; i<3; i++){
+        p_element->GetGeometry()[i].FastGetSolutionStepValue(DENSITY) = p_properties->GetValue(DENSITY); // TODO: To be removed once the element migration is finally finished
+        p_element->GetGeometry()[i].FastGetSolutionStepValue(DYNAMIC_VISCOSITY) = p_properties->GetValue(DYNAMIC_VISCOSITY); // TODO: To be removed once the element migration is finally finished
+        p_element->GetGeometry()[i].FastGetSolutionStepValue(PRESSURE)    = 0.0;
+        p_element->GetGeometry()[i].FastGetSolutionStepValue(PRESSURE, 1) = 0.0;
+        p_element->GetGeometry()[i].FastGetSolutionStepValue(PRESSURE, 2) = 0.0;
+        for(unsigned int k=0; k<2; k++){
+            p_element->GetGeometry()[i].FastGetSolutionStepValue(VELOCITY)[k]    = reference_velocity(i,k);
+            p_element->GetGeometry()[i].FastGetSolutionStepValue(VELOCITY, 1)[k] = 0.9*reference_velocity(i,k);
+            p_element->GetGeometry()[i].FastGetSolutionStepValue(VELOCITY, 2)[k] = 0.75*reference_velocity(i,k);
+            p_element->GetGeometry()[i].FastGetSolutionStepValue(MESH_VELOCITY)[k]    = 0.0;
+            p_element->GetGeometry()[i].FastGetSolutionStepValue(MESH_VELOCITY, 1)[k] = 0.0;
+            p_element->GetGeometry()[i].FastGetSolutionStepValue(MESH_VELOCITY, 2)[k] = 0.0;
+        }
+    }
+
+    // RHS and LHS
+    Vector RHS = ZeroVector(9);
+    Matrix LHS = ZeroMatrix(9,9);
+
+    std::vector< std::vector<double> > output_uncut(6);
+    output_uncut[0] = {-21.81650306,-40.75920676,-0.6557581669,54.90454836,132.1891487,0.1308774929,90.0369547,179.8200581,0.374880674}; // EmbeddedQSVMSDiscontinuous
+    output_uncut[1] = {-0.6361617846,8.819948812,-0.6557582459,67.57989341,174.5435981,0.1308775154,110.444523,215.3506723,0.3748807306}; // EmbeddedSymbolicNavierStokesDiscontinuous
+    int counter = 0;
+
+    // Test Uncut element
+    p_element->GetGeometry()[0].FastGetSolutionStepValue(DISTANCE) = 1.0;
+    p_element->GetGeometry()[1].FastGetSolutionStepValue(DISTANCE) = 1.0;
+    p_element->GetGeometry()[2].FastGetSolutionStepValue(DISTANCE) = 1.0;
+
+    // Set the ELEMENTAL_DISTANCES vector in all elements (from the nodal distance)
+    for (unsigned int i=0; i<model_part.NumberOfElements(); ++i){
+        Element::Pointer p_element_i = model_part.pGetElement(i+1);
+        Vector elem_dist(3);
+        for (unsigned int i_node=0; i_node<3; ++i_node){
+            elem_dist[i_node] = p_element_i->GetGeometry()[i_node].FastGetSolutionStepValue(DISTANCE);
+        }
+        p_element_i->SetValue(ELEMENTAL_DISTANCES, elem_dist);
+    }
+
+    for (ModelPart::ElementIterator i = model_part.ElementsBegin(); i != model_part.ElementsEnd(); i++) {
+        i->Initialize(); // Initialize the element to initialize the constitutive law
+        i->Check(model_part.GetProcessInfo()); // Otherwise the constitutive law is not seen here
+        i->CalculateLocalSystem(LHS, RHS, model_part.GetProcessInfo());
+
+        // std::cout << i->Info() << std::setprecision(10) << std::endl;
+        // KRATOS_WATCH(RHS);
+
+        for (unsigned int j = 0; j < RHS.size(); j++) {
+            KRATOS_CHECK_NEAR(RHS[j], output_uncut[counter][j], 1e-6);
+        }
+
+        counter++;
+    }
+
+    std::vector< std::vector<double> > output_cut(6);
+    output_cut[0] = {3.77784112,-1592.753447,-0.4453264623,42.1943771,-1876.944984,0.3953264623,32.91666667,-7245.299638,-0.1}; // EmbeddedQSVMSDiscontinuous
+    output_cut[1] = {18.84222818,-1545.613012,-0.4453265312,49.82664608,-1836.628433,0.3953265312,32.91666667,-7245.299638,-0.1}; // EmbeddedSymbolicNavierStokesDiscontinuous
+    counter = 0;
+
+    // Test Uncut element
+    p_element->GetGeometry()[0].FastGetSolutionStepValue(DISTANCE) = -1.0;
+    p_element->GetGeometry()[1].FastGetSolutionStepValue(DISTANCE) = -1.0;
+    p_element->GetGeometry()[2].FastGetSolutionStepValue(DISTANCE) = 0.5;
+
+    // Set the ELEMENTAL_DISTANCES vector in all elements (from the nodal distance)
+    for (unsigned int i=0; i<model_part.NumberOfElements(); ++i){
+        Element::Pointer p_element_i = model_part.pGetElement(i+1);
+        Vector elem_dist(3);
+        for (unsigned int i_node=0; i_node<3; ++i_node){
+            elem_dist[i_node] = p_element_i->GetGeometry()[i_node].FastGetSolutionStepValue(DISTANCE);
+        }
+        p_element_i->SetValue(ELEMENTAL_DISTANCES, elem_dist);
+    }
+
+    for (ModelPart::ElementIterator i = model_part.ElementsBegin(); i != model_part.ElementsEnd(); i++) {
+        i->Initialize(); // Initialize the element to initialize the constitutive law
+        i->Check(model_part.GetProcessInfo()); // Otherwise the constitutive law is not seen here
+        i->CalculateLocalSystem(LHS, RHS, model_part.GetProcessInfo());
+
+        // std::cout << i->Info() << std::setprecision(10) << std::endl;
+        // KRATOS_WATCH(RHS);
+
+        for (unsigned int j = 0; j < RHS.size(); j++) {
+            KRATOS_CHECK_NEAR(RHS[j], output_cut[counter][j], 1e-6);
+        }
+
+        counter++;
+    }
+}
+
 KRATOS_TEST_CASE_IN_SUITE(EmbeddedElement2D3N, FluidDynamicsApplicationFastSuite)
 {
     ModelPart model_part("Main");
@@ -295,6 +454,8 @@ KRATOS_TEST_CASE_IN_SUITE(EmbeddedElement2D3N, FluidDynamicsApplicationFastSuite
     model_part.GetProcessInfo().SetValue(DYNAMIC_TAU, 0.001);
     model_part.GetProcessInfo().SetValue(SOUND_VELOCITY, 1.0e+3);
     model_part.GetProcessInfo().SetValue(DELTA_TIME, delta_time);
+    model_part.GetProcessInfo().SetValue(SLIP_LENGTH, 1.0e+8);
+    model_part.GetProcessInfo().SetValue(PENALTY_COEFFICIENT, 10.0);
     Vector bdf_coefs(3);
     bdf_coefs[0] = 3.0/(2.0*delta_time);
     bdf_coefs[1] = -2.0/delta_time;
@@ -327,7 +488,6 @@ KRATOS_TEST_CASE_IN_SUITE(EmbeddedElement2D3N, FluidDynamicsApplicationFastSuite
     model_part.CreateNewElement("SymbolicNavierStokes2D3N", 4, element_nodes, p_properties);
     model_part.CreateNewElement("TimeIntegratedQSVMS2D3N", 5, element_nodes, p_properties);
     model_part.CreateNewElement("EmbeddedQSVMS2D3N", 6, element_nodes, p_properties);
-    //TODO: ADD THE NEW AUSAS ELEMENTS
 
     // Define the nodal values
     Matrix reference_velocity(3,2);
@@ -387,7 +547,7 @@ KRATOS_TEST_CASE_IN_SUITE(EmbeddedElement2D3N, FluidDynamicsApplicationFastSuite
     }
 
     std::vector< std::vector<double> > output_cut(6);
-    output_cut[0] = {-0.008024691358,-0.01358024691,-0.05463909243,-0.008641975309,-0.01419753086,0.01767964152,28.23027063,46.48478011,0.02029278424}; // EmbeddedNavierStokes
+    output_cut[0] = {-0.008024691358,-0.01358024691,-0.05463909243,-0.008641975309,-0.01419753086,0.01767964152,-2867.408637,-4779.580067,0.02029278424}; // EmbeddedNavierStokes
     output_cut[1] = {-0.008024691358,-0.01358024691,-0.07247223359,-0.008641975309,-0.01419753086,0.02427807437,-5126.588376,-8545.245994,0.03152749255}; // EmbeddedFluidElement
     output_cut[2] = {7.04404006297,26.5610397941,-0.50338808232,60.9307110186,153.370250664,0.0853525646157,95.7560183031,186.729364853,0.268035517704}; // NavierStokes
     output_cut[3] = {-0.6361617846,8.819948812,-0.6557582459,67.57989341,174.5435981,0.1308775154,110.444523,215.3506723,0.3748807306}; // SymbolicNavierStokes2D3N
@@ -442,7 +602,7 @@ KRATOS_TEST_CASE_IN_SUITE(EmbeddedElement2D3N, FluidDynamicsApplicationFastSuite
 
     std::vector< std::vector<double> > output_embedded_velocity;
     output_embedded_velocity.resize(6);
-    output_embedded_velocity[0] = {0.0475308641975,0.0975308641975,-0.0546390924304,0.0469135802469,0.0969135802469,0.0176796415227,28.2302706291,46.484780109,0.020292784241}; // EmbeddedNavierStokes
+    output_embedded_velocity[0] = {0.0475308641975,0.0975308641975,-0.0546390924304,0.0469135802469,0.0969135802469,0.0176796415227,16436.8507492,33828.9387065,0.020292784241}; // EmbeddedNavierStokes
     output_embedded_velocity[1] = {0.0475308641975,0.0975308641975,-0.0724722335903,0.0469135802469,0.0969135802469,0.0242780743742,29260.904177,60229.7391127,0.0315274925494}; // EmbeddedFluidElement
     output_embedded_velocity[2] = {7.044040063,26.56103979,-0.5033880823,60.93071102,153.3702507,0.08535256462,95.7560183,186.7293649,0.2680355177}; // NavierStokes
     output_embedded_velocity[3] = {-0.636161784635,8.8199488124,-0.655758245947,67.5798934101,174.543598083,0.130877515367,110.444522985,215.350672279,0.37488073058}; // SymbolicNavierStokes2D3N
