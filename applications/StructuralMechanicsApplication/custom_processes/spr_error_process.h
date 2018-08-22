ecac30ade96e02ca5fd003166bc49d3f303f7b8c
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-08-22 15:19:07 +0200

    Replace {} by inline method

--- applications/StructuralMechanicsApplication/custom_processes/spr_error_process.h
+++ applications/StructuralMechanicsApplication/custom_processes/spr_error_process.h
@@ -244,6 +244,12 @@ private:
     ///@name Private Operations
     ///@{
 
+    /**
+     * @brief This method performs a neighnour search
+     * @param rModelPart The model part where to search
+     */
+    static inline void FindNodalNeighbours(ModelPart& rModelPart);
+
     ///@}
     ///@name Private  Access
     ///@{
