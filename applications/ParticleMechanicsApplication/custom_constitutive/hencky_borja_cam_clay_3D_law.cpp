05ecc6ce402869928c7a021c227e38711f79fe2d
Nanda <bodhinandach@gmail.com>
2018-08-30 14:27:03 +1000

    Adding hencky borja cam clay const new file

---
+++ applications/ParticleMechanicsApplication/custom_constitutive/hencky_borja_cam_clay_3D_law.cpp
@@ -0,0 +1,74 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		BSD License
+//					Kratos default license: kratos/license.txt
+//
+//  Main authors:    Bodhinanda Chandra
+//
+// System includes
+#include <iostream>
+
+// External includes
+#include<cmath>
+
+// Project includes
+#include "includes/properties.h"
+#include "custom_constitutive/hencky_borja_cam_clay_3D_law.hpp"
+#include "particle_mechanics_application.h"
+
+namespace Kratos
+{
+
+//******************************CONSTRUCTOR*******************************************
+//************************************************************************************
+
+HenckyBorjaCamClayPlastic3DLaw::HenckyBorjaCamClayPlastic3DLaw()
+    : HenckyElasticPlastic3DLaw()
+{
+    mpHardeningLaw   = HardeningLaw::Pointer( new HardeningLaw() );
+    mpYieldCriterion = YieldCriterion::Pointer( new MCYieldCriterion(mpHardeningLaw) );
+    mpMPMFlowRule    = MPMFlowRule::Pointer( new MCPlasticFlowRule(mpYieldCriterion) );
+}
+
+
+//******************************CONSTRUCTOR*******************************************
+//************************************************************************************
+
+HenckyBorjaCamClayPlastic3DLaw::HenckyBorjaCamClayPlastic3DLaw(FlowRulePointer pMPMFlowRule, YieldCriterionPointer pYieldCriterion, HardeningLawPointer pHardeningLaw)
+{
+    mpHardeningLaw    =  pHardeningLaw;
+    mpYieldCriterion  =  YieldCriterion::Pointer( new MCYieldCriterion(mpHardeningLaw) );
+    mpMPMFlowRule        =  pMPMFlowRule;
+}
+
+//******************************COPY CONSTRUCTOR**************************************
+//************************************************************************************
+
+HenckyBorjaCamClayPlastic3DLaw::HenckyBorjaCamClayPlastic3DLaw(const HenckyBorjaCamClayPlastic3DLaw& rOther)
+    : HenckyElasticPlastic3DLaw(rOther)
+{
+
+}
+
+//********************************CLONE***********************************************
+//************************************************************************************
+
+ConstitutiveLaw::Pointer HenckyBorjaCamClayPlastic3DLaw::Clone() const
+{
+    HenckyBorjaCamClayPlastic3DLaw::Pointer p_clone(new HenckyBorjaCamClayPlastic3DLaw(*this));
+    return p_clone;
+}
+
+//*******************************DESTRUCTOR*******************************************
+//************************************************************************************
+
+HenckyBorjaCamClayPlastic3DLaw::~HenckyBorjaCamClayPlastic3DLaw()
+{
+}
+
+
+} // Namespace Kratos
