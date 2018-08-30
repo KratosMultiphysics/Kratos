4d1e4e3ffbc1738ec98ec88bdb80d4cc1d2929fd
philbucher <philipp.bucher@tum.de>
2018-08-30 13:55:55 +0200

    removing GetValueOnIntegrationPoints from shell-header-files

--- applications/StructuralMechanicsApplication/custom_elements/shell_thick_element_3D3N.hpp
+++ applications/StructuralMechanicsApplication/custom_elements/shell_thick_element_3D3N.hpp
@@ -156,15 +156,6 @@ namespace Kratos
 
         void CalculateMassMatrix(MatrixType& rMassMatrix, ProcessInfo& rCurrentProcessInfo) override;
 
-        // Results calculation on integration points
-        void GetValueOnIntegrationPoints(const Variable<double>& rVariable, std::vector<double>& rValues, const ProcessInfo& rCurrentProcessInfo) override;
-
-        void GetValueOnIntegrationPoints(const Variable<Vector>& rVariable, std::vector<Vector>& rValues, const ProcessInfo& rCurrentProcessInfo) override;
-
-        void GetValueOnIntegrationPoints(const Variable<Matrix>& rVariable, std::vector<Matrix>& rValues, const ProcessInfo& rCurrentProcessInfo) override;
-
-        void GetValueOnIntegrationPoints(const Variable<array_1d<double, 3> >& rVariable, std::vector<array_1d<double, 3> >& rValues, const ProcessInfo& rCurrentProcessInfo) override;
-
         // More results calculation on integration points to interface with python
         void CalculateOnIntegrationPoints(const Variable<double>& rVariable,
             std::vector<double>& rValues, const ProcessInfo& rCurrentProcessInfo) override;
