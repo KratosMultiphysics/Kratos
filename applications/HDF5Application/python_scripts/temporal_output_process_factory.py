cb2195040b431c3ddc30a347673b94fb5f1981dd
sunethwarna <suneth.warna@gmail.com>
2018-08-30 15:52:16 +0200

    removing methods from results factory

--- applications/HDF5Application/python_scripts/temporal_output_process_factory.py
+++ applications/HDF5Application/python_scripts/temporal_output_process_factory.py
@@ -17,8 +17,8 @@ class FactoryHelper:
         hdf5_file_factory = self.FileFactory(settings["file_settings"])
         model_part_output = self.ModelPartOutput(settings["model_part_output_settings"])
         nodal_solution_step_output = self.NodalSolutionStepDataOutput(settings["nodal_solution_step_data_settings"])
-        element_data_value_output = self.ElementDataValueOutput(settings["element_data_value_settings"])
-        nodal_data_value_output = self.NodalDataValueOutput(settings["nodal_data_value_settings"])
+        element_data_value_output = hdf5_io.ElementDataValueOutput(settings["element_data_value_settings"])
+        nodal_data_value_output = hdf5_io.NodalDataValueOutput(settings["nodal_data_value_settings"])
         temporal_output_process = hdf5_io.TemporalOutputProcess(
             model_part, hdf5_file_factory, settings["output_time_settings"], [model_part_output, nodal_solution_step_output, element_data_value_output, nodal_data_value_output])
         return (temporal_output_process, model_part_output, [nodal_solution_step_output, element_data_value_output, nodal_data_value_output])
@@ -31,7 +31,6 @@ class SerialFactory:
 
     def ModelPartOutput(self, model_part_output_settings):
         return hdf5_io.ModelPartOutput(model_part_output_settings)
-
 
 class ParallelFactory:
 
@@ -47,13 +46,6 @@ class ResultsFactory:
     def NodalSolutionStepDataOutput(self, nodal_solution_step_data_settings):
         return hdf5_io.NodalSolutionStepDataOutput(nodal_solution_step_data_settings)
 
-    def ElementDataValueOutput(self, element_data_value_settings):
-        return hdf5_io.ElementDataValueOutput(element_data_value_settings)
-
-    def NodalDataValueOutput(self, nodal_data_value_settings):
-        return hdf5_io.NodalDataValueOutput(nodal_data_value_settings)
-
-
 class PrimalResultsFactory:
 
     def __init__(self, alpha_bossak):
@@ -61,13 +53,6 @@ class PrimalResultsFactory:
 
     def NodalSolutionStepDataOutput(self, nodal_solution_step_data_settings):
         return hdf5_io.PrimalBossakOutput(nodal_solution_step_data_settings, self.alpha_bossak)
-
-    def ElementDataValueOutput(self, element_data_value_settings):
-        return hdf5_io.ElementDataValueOutput(element_data_value_settings)
-
-    def NodalDataValueOutput(self, nodal_data_value_settings):
-        return hdf5_io.NodalDataValueOutput(nodal_data_value_settings)
-
 
 class TemporalOutputFactoryHelper(FactoryHelper, SerialFactory, ResultsFactory):
     pass
