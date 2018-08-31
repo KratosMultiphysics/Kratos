885190fa7b81098f39641a47166d6e0cb76a6e40
philbucher <philipp.bucher@tum.de>
2018-08-31 09:22:41 +0200

    commenting functions that wll be removed

--- applications/StructuralMechanicsApplication/custom_elements/shell_thick_element_3D4N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/shell_thick_element_3D4N.cpp
@@ -796,91 +796,91 @@ void ShellThickElement3D4N::CalculateOnIntegrationPoints(const Variable<double>&
 	}
 }
 
-void ShellThickElement3D4N::CalculateOnIntegrationPoints(const Variable<Vector>& rVariable,
-        std::vector<Vector>& rValues,
-        const ProcessInfo& rCurrentProcessInfo)
-{
-	if (rVariable == LOCAL_AXIS_1)
-	{
-		// LOCAL_AXIS_1 output DOES NOT include the effect of section
-		// orientation, which rotates the entrire element section in-plane
-		// and is used in element stiffness calculation.
+// void ShellThickElement3D4N::CalculateOnIntegrationPoints(const Variable<Vector>& rVariable,
+//         std::vector<Vector>& rValues,
+//         const ProcessInfo& rCurrentProcessInfo)
+// {
+// 	if (rVariable == LOCAL_AXIS_1)
+// 	{
+// 		// LOCAL_AXIS_1 output DOES NOT include the effect of section
+// 		// orientation, which rotates the entrire element section in-plane
+// 		// and is used in element stiffness calculation.
 
-        // Resize output
-		if (rValues.size() != 4) rValues.resize(4);
+//         // Resize output
+// 		if (rValues.size() != 4) rValues.resize(4);
 
-        for (int i = 0; i < 4; ++i) rValues[i] = ZeroVector(3);
+//         for (int i = 0; i < 4; ++i) rValues[i] = ZeroVector(3);
 
-		// Initialize common calculation variables
-		// Compute the local coordinate system.
-		ShellQ4_LocalCoordinateSystem localCoordinateSystem(
-			mpCoordinateTransformation->CreateReferenceCoordinateSystem());
+// 		// Initialize common calculation variables
+// 		// Compute the local coordinate system.
+// 		ShellQ4_LocalCoordinateSystem localCoordinateSystem(
+// 			mpCoordinateTransformation->CreateReferenceCoordinateSystem());
 
-		for (SizeType GP = 0; GP < 4; GP++)
-		{
-			rValues[GP] = localCoordinateSystem.Vx();
-		}
-	}
-	else if (rVariable == LOCAL_MATERIAL_ORIENTATION_VECTOR_1)
-	{
-		// LOCAL_MATERIAL_ORIENTATION_VECTOR_1 output DOES include the effect of
-		// section orientation, which rotates the entrire element section
-		// in-plane and is used in element stiffness calculation.
+// 		for (SizeType GP = 0; GP < 4; GP++)
+// 		{
+// 			rValues[GP] = localCoordinateSystem.Vx();
+// 		}
+// 	}
+// 	else if (rVariable == LOCAL_MATERIAL_ORIENTATION_VECTOR_1)
+// 	{
+// 		// LOCAL_MATERIAL_ORIENTATION_VECTOR_1 output DOES include the effect of
+// 		// section orientation, which rotates the entrire element section
+// 		// in-plane and is used in element stiffness calculation.
 
-		// Resize output
-		if (rValues.size() != 4) rValues.resize(4);
+// 		// Resize output
+// 		if (rValues.size() != 4) rValues.resize(4);
 
-        for (int i = 0; i < 4; ++i) rValues[i] = ZeroVector(3);
+//         for (int i = 0; i < 4; ++i) rValues[i] = ZeroVector(3);
 
-		// Initialize common calculation variables
-		// Compute the local coordinate system.
-		ShellQ4_LocalCoordinateSystem localCoordinateSystem(
-			mpCoordinateTransformation->CreateReferenceCoordinateSystem());
+// 		// Initialize common calculation variables
+// 		// Compute the local coordinate system.
+// 		ShellQ4_LocalCoordinateSystem localCoordinateSystem(
+// 			mpCoordinateTransformation->CreateReferenceCoordinateSystem());
 
-		// Get local axis 1 in flattened LCS space
-		Vector3 localAxis1 = localCoordinateSystem.P2() - localCoordinateSystem.P1();
+// 		// Get local axis 1 in flattened LCS space
+// 		Vector3 localAxis1 = localCoordinateSystem.P2() - localCoordinateSystem.P1();
 
-		// Perform rotation of local axis 1 to fiber1 in flattened LCS space
-		Matrix localToFiberRotation = Matrix(3, 3, 0.0);
-		double fiberSectionRotation = mSections[0]->GetOrientationAngle();
-		double c = std::cos(fiberSectionRotation);
-		double s = std::sin(fiberSectionRotation);
+// 		// Perform rotation of local axis 1 to fiber1 in flattened LCS space
+// 		Matrix localToFiberRotation = Matrix(3, 3, 0.0);
+// 		double fiberSectionRotation = mSections[0]->GetOrientationAngle();
+// 		double c = std::cos(fiberSectionRotation);
+// 		double s = std::sin(fiberSectionRotation);
 
-		localToFiberRotation(0, 0) = c;
-		localToFiberRotation(0, 1) = -s;
-		localToFiberRotation(1, 0) = s;
-		localToFiberRotation(1, 1) = c;
-		localToFiberRotation(2, 2) = 1.0;
+// 		localToFiberRotation(0, 0) = c;
+// 		localToFiberRotation(0, 1) = -s;
+// 		localToFiberRotation(1, 0) = s;
+// 		localToFiberRotation(1, 1) = c;
+// 		localToFiberRotation(2, 2) = 1.0;
 
-        Vector3 temp = prod(localToFiberRotation, localAxis1);
+//         Vector3 temp = prod(localToFiberRotation, localAxis1);
 
-        // Transform result back to global cartesian coords
-		// Includes warpage correction
-		/*
-		Matrix localToGlobalLarge;
-		localCoordinateSystem.ComputeLocalToGlobalTransformationMatrix(localToGlobalLarge);
-		Matrix localToGlobalSmall = Matrix(3, 3, 0.0);
-		for (SizeType i = 0; i < 3; i++)
-		{
-		for (SizeType j = 0; j < 3; j++)
-		{
-		localToGlobalSmall(i, j) = localToGlobalLarge(i, j);
-		}
-		}
-		*/
-		// Basic rotation without warpage correction
-		Matrix localToGlobalSmall = localCoordinateSystem.Orientation();
+//         // Transform result back to global cartesian coords
+// 		// Includes warpage correction
+// 		/*
+// 		Matrix localToGlobalLarge;
+// 		localCoordinateSystem.ComputeLocalToGlobalTransformationMatrix(localToGlobalLarge);
+// 		Matrix localToGlobalSmall = Matrix(3, 3, 0.0);
+// 		for (SizeType i = 0; i < 3; i++)
+// 		{
+// 		for (SizeType j = 0; j < 3; j++)
+// 		{
+// 		localToGlobalSmall(i, j) = localToGlobalLarge(i, j);
+// 		}
+// 		}
+// 		*/
+// 		// Basic rotation without warpage correction
+// 		Matrix localToGlobalSmall = localCoordinateSystem.Orientation();
 
-		Vector3 fiberAxis1 = prod(trans(localToGlobalSmall), temp);
-		fiberAxis1 /= std::sqrt(inner_prod(fiberAxis1, fiberAxis1));
+// 		Vector3 fiberAxis1 = prod(trans(localToGlobalSmall), temp);
+// 		fiberAxis1 /= std::sqrt(inner_prod(fiberAxis1, fiberAxis1));
 
-		//write results
-		for (SizeType dir = 0; dir < 1; dir++)
-		{
-			rValues[dir] = fiberAxis1;
-		}
-	}
-}
+// 		//write results
+// 		for (SizeType dir = 0; dir < 1; dir++)
+// 		{
+// 			rValues[dir] = fiberAxis1;
+// 		}
+// 	}
+// }
 
 void ShellThickElement3D4N::CalculateOnIntegrationPoints(const Variable<Matrix>& rVariable,
         std::vector<Matrix>& rValues,
@@ -913,7 +913,11 @@ void ShellThickElement3D4N::CalculateOnIntegrationPoints(const Variable<array_1d
         std::vector<array_1d<double,3> >& rValues,
         const ProcessInfo& rCurrentProcessInfo)
 {
-    if(TryCalculateOnIntegrationPoints_MaterialOrientation(rVariable, rValues, rCurrentProcessInfo)) return;
+    // if
+
+
+
+    // if(TryCalculateOnIntegrationPoints_MaterialOrientation(rVariable, rValues, rCurrentProcessInfo)) return;
 }
 
 void ShellThickElement3D4N::Calculate(const Variable<Matrix>& rVariable, Matrix & Output, const ProcessInfo & rCurrentProcessInfo)
@@ -1687,71 +1691,71 @@ void ShellThickElement3D4N::AddBodyForces(const array_1d<double,4> & dA, VectorT
     }
 }
 
-bool ShellThickElement3D4N::TryCalculateOnIntegrationPoints_MaterialOrientation(const Variable<array_1d<double,3> >& rVariable,
-        std::vector<array_1d<double,3> >& rValues,
-        const ProcessInfo& rCurrentProcessInfo)
-{
-    // Check the required output
+// bool ShellThickElement3D4N::TryCalculateOnIntegrationPoints_MaterialOrientation(const Variable<array_1d<double,3> >& rVariable,
+//         std::vector<array_1d<double,3> >& rValues,
+//         const ProcessInfo& rCurrentProcessInfo)
+// {
+//     // Check the required output
 
-    int ijob = 0;
-    if(rVariable == MATERIAL_ORIENTATION_DX)
-        ijob = 1;
-    else if(rVariable == MATERIAL_ORIENTATION_DY)
-        ijob = 2;
-    else if(rVariable == MATERIAL_ORIENTATION_DZ)
-        ijob = 3;
+//     int ijob = 0;
+//     if(rVariable == MATERIAL_ORIENTATION_DX)
+//         ijob = 1;
+//     else if(rVariable == MATERIAL_ORIENTATION_DY)
+//         ijob = 2;
+//     else if(rVariable == MATERIAL_ORIENTATION_DZ)
+//         ijob = 3;
 
-    // quick return
+//     // quick return
 
-    if(ijob == 0) return false;
+//     if(ijob == 0) return false;
 
-    // resize output
+//     // resize output
 
-    SizeType size = 4;
-    if(rValues.size() != size)
-        rValues.resize(size);
+//     SizeType size = 4;
+//     if(rValues.size() != size)
+//         rValues.resize(size);
 
-    // Get some references.
+//     // Get some references.
 
-    //GeometryType & geom = GetGeometry();
+//     //GeometryType & geom = GetGeometry();
 
-    // Compute the local coordinate system.
+//     // Compute the local coordinate system.
 
-    ShellQ4_LocalCoordinateSystem localCoordinateSystem(
-        mpCoordinateTransformation->CreateLocalCoordinateSystem() );
+//     ShellQ4_LocalCoordinateSystem localCoordinateSystem(
+//         mpCoordinateTransformation->CreateLocalCoordinateSystem() );
 
-    Vector3Type eZ = localCoordinateSystem.Vz();
+//     Vector3Type eZ = localCoordinateSystem.Vz();
 
-    // Gauss Loop
+//     // Gauss Loop
 
-    if(ijob == 1)
-    {
-        Vector3Type eX = localCoordinateSystem.Vx();
-        for(int i = 0; i < 4; i++)
-        {
-            QuaternionType q = QuaternionType::FromAxisAngle(eZ(0), eZ(1), eZ(2), mSections[i]->GetOrientationAngle());
-            q.RotateVector3(eX, rValues[i]);
-        }
-    }
-    else if(ijob == 2)
-    {
-        Vector3Type eY = localCoordinateSystem.Vy();
-        for(int i = 0; i < 4; i++)
-        {
-            QuaternionType q = QuaternionType::FromAxisAngle(eZ(0), eZ(1), eZ(2), mSections[i]->GetOrientationAngle());
-            q.RotateVector3(eY, rValues[i]);
-        }
-    }
-    else if(ijob == 3)
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            noalias( rValues[i] ) = eZ;
-        }
-    }
+//     if(ijob == 1)
+//     {
+//         Vector3Type eX = localCoordinateSystem.Vx();
+//         for(int i = 0; i < 4; i++)
+//         {
+//             QuaternionType q = QuaternionType::FromAxisAngle(eZ(0), eZ(1), eZ(2), mSections[i]->GetOrientationAngle());
+//             q.RotateVector3(eX, rValues[i]);
+//         }
+//     }
+//     else if(ijob == 2)
+//     {
+//         Vector3Type eY = localCoordinateSystem.Vy();
+//         for(int i = 0; i < 4; i++)
+//         {
+//             QuaternionType q = QuaternionType::FromAxisAngle(eZ(0), eZ(1), eZ(2), mSections[i]->GetOrientationAngle());
+//             q.RotateVector3(eY, rValues[i]);
+//         }
+//     }
+//     else if(ijob == 3)
+//     {
+//         for(int i = 0; i < 4; i++)
+//         {
+//             noalias( rValues[i] ) = eZ;
+//         }
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
 bool ShellThickElement3D4N::TryCalculateOnIntegrationPoints_GeneralizedStrainsOrStresses(const Variable<Matrix>& rVariable,
         std::vector<Matrix>& rValues,
