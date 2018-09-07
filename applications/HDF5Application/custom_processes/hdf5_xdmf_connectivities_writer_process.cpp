0fe6dc543d710ee241f844863aa8baac3561e95e
philbucher <philipp.bucher@tum.de>
2018-09-07 14:10:15 +0200

    fix for non-consecutive nodes

--- applications/HDF5Application/custom_processes/hdf5_xdmf_connectivities_writer_process.cpp
+++ applications/HDF5Application/custom_processes/hdf5_xdmf_connectivities_writer_process.cpp
@@ -30,7 +30,7 @@ XdmfConnectivitiesWriterProcess::XdmfConnectivitiesWriterProcess(const std::stri
     mpFile->ReadDataSet(node_ids_path, node_ids, 0, num_points);
 
     // Set the parametric coordinate ids.
-    mKratosToXdmfIdTable.resize(num_points);
+    mKratosToXdmfIdTable.resize(*(std::max_element(node_ids.begin(), node_ids.end())));
 #pragma omp parallel for
     for (int i = 0; i < num_points; ++i)
     {
