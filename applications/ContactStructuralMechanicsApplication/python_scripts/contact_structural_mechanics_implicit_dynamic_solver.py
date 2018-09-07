92097f02abd913092b00a5061c2c2180d437fe3a
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-09-07 09:57:23 +0200

    Reactivating parameter

--- applications/ContactStructuralMechanicsApplication/python_scripts/contact_structural_mechanics_implicit_dynamic_solver.py
+++ applications/ContactStructuralMechanicsApplication/python_scripts/contact_structural_mechanics_implicit_dynamic_solver.py
@@ -326,7 +326,7 @@ class ContactImplicitMechanicalSolver(structural_mechanics_implicit_dynamic_solv
         newton_parameters.AddValue("adaptative_strategy", self.contact_settings["adaptative_strategy"])
         newton_parameters.AddValue("split_factor", self.contact_settings["split_factor"])
         newton_parameters.AddValue("max_number_splits", self.contact_settings["max_number_splits"])
-        #newton_parameters.AddValue("inner_loop_iterations", self.contact_settings["inner_loop_iterations"])
+        newton_parameters.AddValue("inner_loop_iterations", self.contact_settings["inner_loop_iterations"])
         return CSMA.ResidualBasedNewtonRaphsonContactStrategy(computing_model_part,
                                                                 self.mechanical_scheme,
                                                                 self.linear_solver,
