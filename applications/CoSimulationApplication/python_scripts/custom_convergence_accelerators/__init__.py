696e31ce2ff47a3a58929c636d1d943dc844d295
aditya_laptop <shine.aditya@gmail.com>
2018-09-10 14:20:45 +0200

    adding init file to convergence accelerators folder

---
+++ applications/CoSimulationApplication/python_scripts/custom_convergence_accelerators/__init__.py
@@ -0,0 +1,30 @@
+# Adding the current directory to the path such that the modules can be imported with __import__ in the factory
+from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+
+available_convergence_accelerators = {
+    "constant_relaxation"  : "convergence_accelerator_constant",
+    "aitken"               : "convergence_accelerator_aitken",
+    "iqnils"               : "convergence_accelerator_iqnils",
+    "mvqn"                 : "convergence_accelerator_mvqn"
+    }
+
+def CreateConvergenceAccelerator(settings, solvers, level):
+    """This function creates and returns the convergence accelerator used for CoSimulation
+    New convergence accelerators have to be registered by adding them to "available_convergence_accelerators"
+    """
+    if (type(settings) != dict):
+        raise Exception("Input is expected to be provided as a python dictionary")
+
+    accelerator_type = settings["type"]
+
+    if accelerator_type in available_convergence_accelerators:
+        accelerator_module = __import__(available_convergence_accelerators[accelerator_type])
+        return accelerator_module.Create(settings, solvers, level)
+    else:
+        err_msg  = 'The requested convergence accelerator "' + accelerator_type + '" is not available!\n'
+        err_msg += 'The following convergence accelerators are available:\n'
+        for avail_accelerator in available_convergence_accelerators:
+            err_msg += "\t" + avail_accelerator + "\n"
+        raise NameError(err_msg)
