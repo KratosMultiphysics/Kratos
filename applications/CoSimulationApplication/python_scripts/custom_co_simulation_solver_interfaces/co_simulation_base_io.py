97a9b8ea48eff3056b1e509c7e35ac0a53c179f1
aditya_laptop <shine.aditya@gmail.com>
2018-09-11 10:41:04 +0200

    base Io with comments and final functions

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/co_simulation_base_io.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/co_simulation_base_io.py
@@ -1,31 +1,64 @@
 from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
 
-# Importing the Kratos Library
-import KratosMultiphysics
-import KratosMultiphysics.MappingApplication as KratosMapping
-
 class CoSimulationBaseIO(object):
-    def __init__(self, solvers, solver_name, level):
-        self.solvers = solvers
-        self.solver_name = solver_name
+    ## The constructor
+    #
+    #  @param self            The object pointer.
+    def __init__(self, solver):
         self.lvl = level
         self.echo_level = 0
 
-    def ImportData(self, data_settings, from_client):
-        pass
-    def ImportMesh(self, data_settings, from_client):
+    ## ImportData :  used to import data from other clients
+    #
+    #  @param self            The object pointer.
+    #  @param data_config     python dictionary : configuration of the data to be imported.
+    #                                             data will be imported into this dictionary
+    #  @param from_client     python object : The client from which data is to be imported.
+    def ImportData(self, data_config, from_client):
         pass
 
-    def ExportData(self, data_settings, to_client):
-        pass
-    def ExportMesh(self, data_settings, to_client):
+    ## ImportMesh :  used to import mesh from other clients
+    #
+    #  @param self            The object pointer.
+    #  @param mesh_conig      python dictionary : configuration of the mesh to be imported.
+    #                                             mesh will be imported into this dictionary.
+    #  @param from_client     python object : The client from which mesh is to be imported.
+    def ImportMesh(self, mesh_conig, from_client):
         pass
 
+    ## ExportData :  used to export data to other clients
+    #
+    #  @param self            The object pointer.
+    #  @param data_config     python dictionary : configuration of the mesh to be exported.
+    #                                             also contains the data to export.
+    #  @param to_client       python object : The client to which mesh is to be exported.
+    def ExportData(self, data_config, to_client):
+        pass
+
+    ## ExportMesh :  used to export mesh to other clients
+    #
+    #  @param self            The object pointer.
+    #  @param mesh_conig      python dictionary : configuration of the mesh to be exported.
+    #                                             also contains the mesh data to export.
+    #  @param to_client       python object : The client to which mesh is to be exported.
+    def ExportMesh(self, mesh_conig, to_client):
+        pass
+
+    ## Sets the echo level of for this IO object. Used for output of information during CoSimulation
+    #
+    #  @param self            The object pointer.
+    #  @param level           int : echo level
     def SetEchoLevel(self, level):
         self.echo_level = level
 
+    ## Prints the information about the current IO. The derived classes should implement it.
+    #
+    #  @param self            The object pointer.
     def PrintInfo(self):
-        print("IO does not yet implement PrintInfo!")
+        print("!!!WARNING!!! Calling PrintInfo from base IO class!")
 
+    ## Check : Checks the current IO against faulty settings. Derived classes may implement this function.
+    #
+    #  @param self            The object pointer.
     def Check(self):
-        print("IO does not yet implement Check!")
+        print("!!!WARNING!!! Calling Check from base IO class!")
