b3a5bd4f45ff79d1ea980cd10cf9309624d22ea9
aditya_laptop <shine.aditya@gmail.com>
2018-09-11 18:04:14 +0200

    removing factory from init file

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/__init__.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/__init__.py
@@ -8,65 +8,7 @@ sys.path.append(current_dir_name)
 for dirName, subdirList, fileList in os.walk(current_dir_name):
     sys.path.append(dirName)
 
-"""
-This is a map of the name of the available solver interfaces to be specified in
-JSON file and their python module (file) name. New solver interfaces should be registered
-here with an additional entry.
-eg : "name_in_JSON" : "python module(file) name"
-"""
-available_solver_interfaces = {
-    "kratos_structure"      : "kratos_structure_co_simulation_solver_wrapper",
-    "kratos_fluid"          : "kratos_fluid_co_simulation_solver_wrapper",
-    "dummy"                 : "dummy_co_simulation_solver_wrapper",
-    "su2"                   : "su2_co_simulation_solver_wrapper",
-    }
-
-def CreateSolverInterface(settings, solvers, level):
-    """
-    This function creates and returns the solver interface used for CoSimulation
-    New solver interface have to be registered by adding them to "available_convergence_accelerators"
-    """
-    if (type(settings) != dict):
-        raise Exception("Input is expected to be provided as a python dictionary")
-
-    solver_interface_type = settings["type"]
-
-    if solver_interface_type in available_solver_interfaces:
-        solver_interface_module = __import__(available_solver_interfaces[solver_interface_type])
-        return solver_interface_module.Create(settings, solvers, level)
-    else:
-        err_msg  = 'The requested solver interface "' + solver_interface_type + '" is not available!\n'
-        err_msg += 'Available solver interfaces are :\n'
-        for avail_accelerator in available_convergence_accelerators:
-            err_msg += "\t" + avail_accelerator + "\n"
-        raise NameError(err_msg)
 
 
 
 
-
-"""
-This is a map of the name of the available IO types to be specified in
-JSON file and their python module (file) name.
-New IOs should be registered here with an additional entry.
-eg : "name_in_JSON" : "python module(file) name"
-"""
-available_ios = {
-    "kratos" : "kratos_io",
-    "sdof"   : "su2_io",
-}
-
-def CreateIO(io_name, solvers, solver_name, level):
-    """
-    This function creates and returns the IO used for CoSimulation
-    New IOs have to be registered by adding them to "available_ios"
-    """
-    if io_type in available_ios:
-        io_module = __import__(available_ios[io_type])
-        return io_module.Create(solvers, solver_name, level)
-    else:
-        err_msg  = 'The requested IO "' + io_name + '" is not available!\n'
-        err_msg += 'The available IOs are :\n'
-        for avail_io in available_ios:
-            err_msg += "\t" + avail_io + "\n"
-        raise NameError(err_msg)
