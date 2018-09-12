0657c757db775ff8e288f797f0e3379126bf1a40
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-09-12 17:56:49 +0200

    Minor

--- applications/StructuralMechanicsApplication/custom_constitutive/hyper_elastic_isotropic_kirchhoff_plane_stress_2d.h
+++ applications/StructuralMechanicsApplication/custom_constitutive/hyper_elastic_isotropic_kirchhoff_plane_stress_2d.h
@@ -211,12 +211,12 @@ private:
 
     void save(Serializer& rSerializer) const override 
     {
-        KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, ConstitutiveLaw )
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, BaseType )
     }
 
     void load(Serializer& rSerializer) override 
     {
-        KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, ConstitutiveLaw)
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, BaseType)
     }
 
 
