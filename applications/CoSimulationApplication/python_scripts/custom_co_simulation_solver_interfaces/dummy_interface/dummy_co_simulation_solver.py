a24b3e54ede51a22cc803da6b9609d171fc9d39b
aditya_laptop <shine.aditya@gmail.com>
2018-09-13 12:44:49 +0200

    adding dummy cosim solver class and basib functions

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/dummy_interface/dummy_co_simulation_solver.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/dummy_interface/dummy_co_simulation_solver.py
@@ -1,4 +1,23 @@
+# co simulation imports
+from co_simulation_base_solver import CoSimulationBaseSolver
+import co_simulation_tools as tools
 
 
 def Create(settings):
-    pass
+    return DummyCoSimulationSolver(settings)
+
+
+class DummyCoSimulationSolver(CoSimulationBaseSolver):
+    def __init__(self, custom_settings):
+        super(DummyCoSimulationSolver, self).__init__(custom_settings)
+        self.name = custom_settings["name"]
+        pass
+
+    def PrintInfo(self):
+        print("This is an object of DummyCoSimulationSolver with name : ", self.name)
+
+    def AdvanceInTime(self, current_time):
+        return current_time + self.GetDeltaTime()
+
+    def GetDeltaTime(self):
+        return 0.1
