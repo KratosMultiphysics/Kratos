e874085e1e88b00a6df70979cd152250bf1ac1d0
aditya_laptop <shine.aditya@gmail.com>
2018-09-14 14:32:32 +0200

    adding initialize and get io name functions to dummy solver

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/dummy_interface/dummy_co_simulation_solver.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/dummy_interface/dummy_co_simulation_solver.py
@@ -11,13 +11,22 @@ class DummyCoSimulationSolver(CoSimulationBaseSolver):
     def __init__(self, custom_settings):
         super(DummyCoSimulationSolver, self).__init__(custom_settings)
         self.name = custom_settings["name"]
-        pass
+
+    def Initialize(self):
+        self.InitializeIO()
 
     def PrintInfo(self):
-        print("This is an object of DummyCoSimulationSolver with name : ", self.name)
+        print( self.data_list.keys() )
+        print(tools.bcolors.BLUE+"This is an object of DummyCoSimulationSolver with name : ", self.name + tools.bcolors.ENDC)
 
     def AdvanceInTime(self, current_time):
         return current_time + self.GetDeltaTime()
 
     def GetDeltaTime(self):
-        return 0.1
+        return 0.2
+
+    def Check(self):
+        print(tools.bcolors.GREEN+"Check from dummy co simulation solver : CHECKED !"+tools.bcolors.ENDC)
+
+    def _GetIOName(self):
+        return "dummy"
