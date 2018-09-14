4001f50d7125d70026734270c5010b4adb44de94
aditya_laptop <shine.aditya@gmail.com>
2018-09-14 17:14:31 +0200

    comments, add get solver details, redfine sync output data

--- applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_coupled_solver.py
+++ applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_coupled_solver.py
@@ -36,6 +36,8 @@ class CoSimulationBaseCoupledSolver(CoSimulationBaseSolver):
         # Get the participating solvers a map with their names and objects
         self.participating_solvers = self._GetSolvers(self.full_settings['solvers'])
 
+        self.solver_settings = self._GetSolverCoSimulationDetails(self.full_settings['coupled_solver_settings']["participants"])
+
         # With this setting the coupling can start later
         self.start_coupling_time = 0.0
         if "start_coupling_time" in self.settings:
@@ -136,14 +138,21 @@ class CoSimulationBaseCoupledSolver(CoSimulationBaseSolver):
             solver.PrintInfo()
 
     ## _SynchronizeInputData : Private Function to obtain new (if any) input data for the
-    #                          participating solvers.
+    #                          participating solvers. That will get the data necessary from
+    #                          python cosimm solver with name solver_name and export (if necessary)
+    #                          it to the remote solver of solver_name
+    #
+    #
+    #  So here the _SynchronizeXXXXXData functions exchange data between the python co simulation solvers.
+    #  How the solvers get their data from their remote solvers is handled in ImportData and ExportData
+    #  functions. Here Import and Export data functions of to_solver and from_solver are called.
     #
     #  @param self            The object pointer.
     #  @param solver_name     string: name of the solver for which data has to be synchronized
     def _SynchronizeInputData(self, solver_name):
         if self.coupling_started:
             solver = self.participating_solvers[solver_name]
-            input_data_list = self.cosim_solver_details[solver_name]["input_data_list"]
+            input_data_list = self.solver_settings[solver_name]["input_data_list"]
             for input_data in input_data_list:
                 from_solver = self.participating_solvers[input_data["from_solver"]]
                 data_name = input_data["data_name"]
@@ -152,22 +161,19 @@ class CoSimulationBaseCoupledSolver(CoSimulationBaseSolver):
                 if( from_solver_data_conf["dimension"] == current_sovler_data_conf["dimension"] ):
                     solver.ImportData(data_name, from_solver)
 
-    ## _SynchronizeOutputData : Private Function to export/output new (if any) output data in the
-    #                           participating solvers.
-    # TODO: Find a nice solution to the data_settings field
+    ## _SynchronizeOutputData : Private Function to synchronize the out put data between the solver
+    #                           interface and the remote solver. This assumes that the remote solver
+    #                           has output the data inthe format specified in the settings (of the out put data def in JSON)
     #
     #  @param self            The object pointer.
-    def _SynchronizeOutputData(self, solver, solver_name):
+    def _SynchronizeOutputData(self, solver_name):
         if self.coupling_started:
-            output_data_list = self.cosim_solver_details[solver_name]["output_data_list"]
+            solver = self.participating_solvers[solver_name]
+            output_data_list = self.solver_settings[solver_name]["output_data_list"]
             for output_data in output_data_list:
-                to_solver = self.solvers[output_data["to_solver"]]
                 data_name = output_data["data_name"]
-                data_definition = to_solver.GetDataDefinition(data_name)
-                data_settings = { "data_format" : data_definition["data_format"],
-                                  "data_name"   : data_name,
-                                  "io_settings" : output_data["io_settings"] }
-                solver.ExportData(data_settings, to_solver)
+                data_conf = solver.GetDataConfig(data_name)
+                solver.ExportData(data_conf)
 
     ## _GetSolvers : Private Function to make the paticipating solver objects
     #
@@ -183,3 +189,20 @@ class CoSimulationBaseCoupledSolver(CoSimulationBaseSolver):
             solvers_map[solver_name] = solver
 
         return solvers_map
+
+    ## _GetSolverCoSimulationDetails : Private Function to obtain a dict of setting with solver
+    #                                  name as key
+    #
+    #  @param self            The object pointer.
+    def _GetSolverCoSimulationDetails(self,co_simulation_solver_settings):
+        num_solvers = len(co_simulation_solver_settings)
+        solver_cosim_details = {}
+        for solver_settings in co_simulation_solver_settings:
+            solver_name = solver_settings["name"]
+            solver_cosim_details[solver_name] = solver_settings
+        # TODO check if the data is consitently defined! => maybe do at another place though...
+        # - input in one is output in another
+        # - one IO is defined for each data_name
+        # - if the same data is defined multiple times
+        # - check if data format has been specified
+        return solver_cosim_details
