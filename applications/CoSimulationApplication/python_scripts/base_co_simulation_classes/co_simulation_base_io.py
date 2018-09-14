4cbf7dd060c9ffbe80983eb325bd712797b95b01
aditya_laptop <shine.aditya@gmail.com>
2018-09-14 14:27:21 +0200

    name changes and comments to base io

--- applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_io.py
+++ applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_io.py
@@ -1,48 +1,53 @@
 from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+import co_simulation_tools as tools
 
+##
+#  IMPORTANT : This is a BASE CLASS
+#               Please do not change any thing in this class.
+#
+# This Class servers as a base class for all the Input-output methods to be implemented
 class CoSimulationBaseIO(object):
     ## The constructor
     #
     #  @param self            The object pointer.
-    def __init__(self, solver):
-        self.lvl = level
+    def __init__(self):
         self.echo_level = 0
 
-    ## ImportData :  used to import data from other clients
+    ## ImportData :  used to import data from other solvers
     #
     #  @param self            The object pointer.
     #  @param data_config     python dictionary : configuration of the data to be imported.
     #                                             data will be imported into this dictionary
-    #  @param from_client     python object : The client from which data is to be imported.
-    def ImportData(self, data_config, from_client):
-        pass
+    #  @param from_solver     python object : The solver from which data is to be imported.
+    def ImportData(self, data_config, from_solver):
+        raise NotImplementedError(tools.bcolors.FAIL + "From BaseIO : The method ImportData is not implemented in the IO class!" + tools.bcolors.ENDC)
 
-    ## ImportMesh :  used to import mesh from other clients
+    ## ImportMesh :  used to import mesh from other solvers
     #
     #  @param self            The object pointer.
     #  @param mesh_conig      python dictionary : configuration of the mesh to be imported.
     #                                             mesh will be imported into this dictionary.
-    #  @param from_client     python object : The client from which mesh is to be imported.
-    def ImportMesh(self, mesh_conig, from_client):
-        pass
+    #  @param from_solver     python object : The solver from which mesh is to be imported.
+    def ImportMesh(self, mesh_conig, from_solver):
+        raise NotImplementedError(tools.bcolors.FAIL + "From BaseIO : The method ImportMesh is not implemented in the IO class!" + tools.bcolors.ENDC)
 
-    ## ExportData :  used to export data to other clients
+    ## ExportData :  used to export data to other solvers
     #
     #  @param self            The object pointer.
     #  @param data_config     python dictionary : configuration of the mesh to be exported.
     #                                             also contains the data to export.
-    #  @param to_client       python object : The client to which mesh is to be exported.
-    def ExportData(self, data_config, to_client):
-        pass
+    #  @param to_solver       python object : The solver to which mesh is to be exported.
+    def ExportData(self, data_config, to_solver):
+        raise NotImplementedError(tools.bcolors.FAIL + "From BaseIO : The method ExportData is not implemented in the IO class!" + tools.bcolors.ENDC)
 
-    ## ExportMesh :  used to export mesh to other clients
+    ## ExportMesh :  used to export mesh to other solvers
     #
     #  @param self            The object pointer.
     #  @param mesh_conig      python dictionary : configuration of the mesh to be exported.
     #                                             also contains the mesh data to export.
-    #  @param to_client       python object : The client to which mesh is to be exported.
-    def ExportMesh(self, mesh_conig, to_client):
-        pass
+    #  @param to_solver       python object : The solver to which mesh is to be exported.
+    def ExportMesh(self, mesh_conig, to_solver):
+        raise NotImplementedError(tools.bcolors.FAIL + "From BaseIO : The method ExportMesh is not implemented in the IO class!" + tools.bcolors.ENDC)
 
     ## Sets the echo level of for this IO object. Used for output of information during CoSimulation
     #
@@ -55,10 +60,10 @@ class CoSimulationBaseIO(object):
     #
     #  @param self            The object pointer.
     def PrintInfo(self):
-        print("!!!WARNING!!! Calling PrintInfo from base IO class!")
+        print(tools.bcolors.WARNING + "!!!WARNING!!! Calling PrintInfo from base IO class!" + tools.bcolors.ENDC)
 
     ## Check : Checks the current IO against faulty settings. Derived classes may implement this function.
     #
     #  @param self            The object pointer.
     def Check(self):
-        print("!!!WARNING!!! Calling Check from base IO class!")
+        print(tools.bcolors.WARNING + "!!!WARNING!!! Calling Check from base IO class!" + tools.bcolors.ENDC)
