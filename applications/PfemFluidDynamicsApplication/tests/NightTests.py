3adaa2f6d89097e6709f7c3797b64eb05125e93b
Unknown <falessandro@cimne.upc.edu>
2018-09-14 18:24:25 +0200

    Test for bingham 2d case

--- applications/PfemFluidDynamicsApplication/tests/NightTests.py
+++ applications/PfemFluidDynamicsApplication/tests/NightTests.py
@@ -15,7 +15,9 @@ class Water_sloshing_Box_3D_Newtonian_Test(TF.TestFactory):
 class Water_sloshing_Box_3D_Non_Newtonian_Test(TF.TestFactory):
       file_name = "fluid_element_tests/Water_sloshing_Box/Non_Newtonian_fluid/Water_sloshing_Box"
       file_parameters = "fluid_element_tests/Water_sloshing_Box/Non_Newtonian_fluid/ProjectParameters.json"
-
+class Bingham_Dam_Break_2D_Test(TF.TestFactory):
+    file_name = "fluid_element_tests/Test_2D_Bingham/Test_2D_Bingham"
+    file_parameters = "fluid_element_tests/Test_2D_Bingham/ProjectParameters.json"
 
 
 
@@ -24,9 +26,10 @@ def SetTestSuite(suites):
 
     night_suite.addTests(
         KratosUnittest.TestLoader().loadTestsFromTestCases([
-            Dam_Break_2D_Newtonian_Test,
-            Water_sloshing_Box_3D_Non_Newtonian_Test,
-            Water_sloshing_Box_3D_Newtonian_Test
+            # Dam_Break_2D_Newtonian_Test,
+            # Water_sloshing_Box_3D_Non_Newtonian_Test,
+            # Water_sloshing_Box_3D_Newtonian_Test
+            Bingham_Dam_Break_2D_Test
         ])
     )
 
--- applications/PfemFluidDynamicsApplication/tests/SmallTests.py
+++ applications/PfemFluidDynamicsApplication/tests/SmallTests.py
@@ -18,15 +18,20 @@ class Water_Sloshing_2D_Test_Mu_Rheology(TF.TestFactory):
     file_name = "fluid_element_tests/Water_sloshing_2D/Mu_Rhelogy/Water_sloshing_2D"
     file_parameters = "fluid_element_tests/Water_sloshing_2D/Mu_Rheology/ProjectParameters.json"
 
+class Bingham_Dam_Break_2D_Test(TF.TestFactory):
+    file_name = "fluid_element_tests/Test_2D_Bingham/Test_2D_Bingham"
+    file_parameters = "fluid_element_tests/Test_2D_Bingham/ProjectParameters.json"
+
 
 def SetTestSuite(suites):
     small_suite = suites['small']
 
     small_suite.addTests(
         KratosUnittest.TestLoader().loadTestsFromTestCases([
-            Water_Sloshing_2D_Test_Newtonian,
-            Water_Sloshing_2D_Test_Non_Newtonian,
-            Water_Sloshing_2D_Test_Mu_Rheology
+           # Water_Sloshing_2D_Test_Newtonian,
+           # Water_Sloshing_2D_Test_Non_Newtonian,
+           # Water_Sloshing_2D_Test_Mu_Rheology,
+           Bingham_Dam_Break_2D_Test
         ])
     )
 
