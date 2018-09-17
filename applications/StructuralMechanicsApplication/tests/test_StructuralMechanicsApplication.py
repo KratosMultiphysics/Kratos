d839aec8f4115c5b4c33b76637075afcf1d7f560
arming <armin.geiser@tum.de>
2018-09-17 12:31:06 +0200

    add FD response test to nighlty test suite

--- applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
+++ applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
@@ -183,6 +183,7 @@ from structural_response_function_test_factory import TestAdjointDisplacementRes
 from structural_response_function_test_factory import TestAdjointStressResponseFunction as TTestAdjointStressResponseFunction
 from structural_response_function_test_factory import TestMassResponseFunction as TTestMassResponseFunction
 from structural_response_function_test_factory import TestStrainEnergyResponseFunction as TTestStrainEnergyResponseFunction
+from structural_response_function_test_factory import TestFDStrainEnergyResponseFunction as TTestFDStrainEnergyResponseFunction
 
 def AssembleTestSuites():
     ''' Populates the test suites to run.
@@ -328,6 +329,7 @@ def AssembleTestSuites():
 
     nightSuite.addTest(TTestMassResponseFunction('test_execution'))
     nightSuite.addTest(TTestStrainEnergyResponseFunction('test_execution'))
+    nightSuite.addTest(TTestFDStrainEnergyResponseFunction('test_execution'))
     nightSuite.addTest(TTestAdjointStrainEnergyResponseFunction('test_execution'))
     nightSuite.addTest(TTestAdjointDisplacementResponseFunction('test_execution'))
     nightSuite.addTest(TTestAdjointStressResponseFunction('test_execution'))
