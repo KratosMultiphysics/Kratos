68f24d35137906c5f8ec933d96ff23a6e555845d
AdityaGhantasala <shine.aditya@gmail.com>
2018-09-18 10:59:52 +0200

    error handling and using tools to check and validate

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/kratos_interfaces/kratos_io.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/kratos_interfaces/kratos_io.py
@@ -1,16 +1,23 @@
-import KratosMultiphysics
-# Check that applications were imported in the main script
-KratosMultiphysics.CheckRegisteredApplications("CoSimulationApplication")
-import KratosMultiphysics.CoSimulationApplication as CoSimApp
+try:
+    import KratosMultiphysics
+    # Check that applications were imported in the main script
+    KratosMultiphysics.CheckRegisteredApplications("CoSimulationApplication")
+    import KratosMultiphysics.CoSimulationApplication as CoSimApp
+except:
+except ModuleNotFoundError:
+        print(tools.bcolors.FAIL + 'KRATOS is not available ! Please ensure that Kratos is available for usage !'+ tools.bcolors.ENDC)
+        exit()
+
+from co_simulation_base_io import CoSimulationBaseIO
+import co_simulation_tools as tools
 
 # Other imports
 import os
 
-def CreateIo(custom_settings):
+def Create(custom_settings):
     return KratosIo(custom_settings)
 
-
-class KratosIo(CoSimApp.CoSimulationBaseIo):
+class KratosIo(CoSimulationBaseIO):
     def __init__(self, custom_settings):
 
         ##settings string in json format
@@ -21,10 +28,7 @@ class KratosIo(CoSimApp.CoSimulationBaseIo):
                 }
             }
         """)
-
-        self.settings = custom_settings
-        self.settings.ValidateAndAssignDefaults(default_settings)
+        self.settings = tools.ValidateAndAssignInputParameters(default_settings, custom_settings, False)
 
         super(KratosIo, self).__init__(custom_settings)
-
         ### Constructing the IO for this solver
