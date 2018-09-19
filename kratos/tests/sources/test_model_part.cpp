35bfb413dbc5c8d6f6c9150e3b49eb72c6134f8f
philbucher <philipp.bucher@tum.de>
2018-09-20 01:22:08 +0200

    minor

--- kratos/tests/sources/test_model_part.cpp
+++ kratos/tests/sources/test_model_part.cpp
@@ -16,30 +16,30 @@
 #include "includes/model_part.h"
 
 namespace Kratos {
-	namespace Testing {
+    namespace Testing {
 
-		KRATOS_TEST_CASE_IN_SUITE(ModelPartSubModelPartsIterator, KratosCoreFastSuite)
-		{
-			ModelPart model_part("Main");
+        KRATOS_TEST_CASE_IN_SUITE(ModelPartSubModelPartsIterator, KratosCoreFastSuite)
+        {
+            ModelPart model_part("Main");
 
-			model_part.CreateSubModelPart("Inlet1");
-			model_part.CreateSubModelPart("Inlet2");
-			model_part.CreateSubModelPart("Outlet");
-			model_part.CreateSubModelPart("AnotherOutlet");
+            model_part.CreateSubModelPart("Inlet1");
+            model_part.CreateSubModelPart("Inlet2");
+            model_part.CreateSubModelPart("Outlet");
+            model_part.CreateSubModelPart("AnotherOutlet");
 
-			std::size_t id = 1;
-			for(auto i_SubModelPart = model_part.SubModelPartsBegin() ; i_SubModelPart != model_part.SubModelPartsEnd() ; i_SubModelPart++){
-				i_SubModelPart->CreateNewNode(id++, 0.00,0.00,0.00);
-			}
+            std::size_t id = 1;
+            for(auto i_SubModelPart = model_part.SubModelPartsBegin() ; i_SubModelPart != model_part.SubModelPartsEnd() ; i_SubModelPart++){
+                i_SubModelPart->CreateNewNode(id++, 0.00,0.00,0.00);
+            }
 
-			KRATOS_CHECK_EQUAL(model_part.NumberOfNodes(), 4);
-			KRATOS_CHECK_EQUAL(model_part.GetSubModelPart("Inlet1").NumberOfNodes(), 1);
-			KRATOS_CHECK_EQUAL(model_part.GetSubModelPart("Outlet").NumberOfNodes(), 1);
-		}
+            KRATOS_CHECK_EQUAL(model_part.NumberOfNodes(), 4);
+            KRATOS_CHECK_EQUAL(model_part.GetSubModelPart("Inlet1").NumberOfNodes(), 1);
+            KRATOS_CHECK_EQUAL(model_part.GetSubModelPart("Outlet").NumberOfNodes(), 1);
+        }
 
-		KRATOS_TEST_CASE_IN_SUITE(ModelPartAddNodalSolutionStepVariable, KratosCoreFastSuite)
-		{
-			ModelPart model_part("Main");
+        KRATOS_TEST_CASE_IN_SUITE(ModelPartAddNodalSolutionStepVariable, KratosCoreFastSuite)
+        {
+            ModelPart model_part("Main");
 
             model_part.AddNodalSolutionStepVariable(VELOCITY);
 
@@ -49,16 +49,16 @@ namespace Kratos {
                 "Error: Attempting to add the variable \"PRESSURE\" to the model part with name \"Main\" which is not empty");
 
             model_part.AddNodalSolutionStepVariable(VELOCITY); // Adding the same Variable twice is fine bcs it wont do anything
-		}
+        }
 
-		KRATOS_TEST_CASE_IN_SUITE(ModelPartHasNodalSolutionStepVariable, KratosCoreFastSuite)
-		{
-			ModelPart model_part("Main");
+        KRATOS_TEST_CASE_IN_SUITE(ModelPartHasNodalSolutionStepVariable, KratosCoreFastSuite)
+        {
+            ModelPart model_part("Main");
 
             model_part.AddNodalSolutionStepVariable(VELOCITY);
 
-			KRATOS_CHECK(model_part.HasNodalSolutionStepVariable(VELOCITY));
-			KRATOS_CHECK_IS_FALSE(model_part.HasNodalSolutionStepVariable(PRESSURE));
-		}
-	}
+            KRATOS_CHECK(model_part.HasNodalSolutionStepVariable(VELOCITY));
+            KRATOS_CHECK_IS_FALSE(model_part.HasNodalSolutionStepVariable(PRESSURE));
+        }
+    }
 }  // namespace Kratos.
