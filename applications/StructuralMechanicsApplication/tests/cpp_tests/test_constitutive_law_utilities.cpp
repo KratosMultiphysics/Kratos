5e54c2a405ada7640f1639d72573e51bcad30303
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-09-20 11:32:56 +0200

    Adding test for Hencky and Biot

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_constitutive_law_utilities.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_constitutive_law_utilities.cpp
@@ -27,7 +27,7 @@ namespace Testing
     static constexpr double tolerance = 1.0e-6;
 
 /**
-* Check the correct calculation of the CL utilities
+* Check the correct calculation of the CL utilities principal stress
 */
 KRATOS_TEST_CASE_IN_SUITE(ConstitutiveLawUtilitiesPrincipalStresses, KratosStructuralMechanicsFastSuite)
 {
@@ -70,6 +70,45 @@ KRATOS_TEST_CASE_IN_SUITE(ConstitutiveLawUtilitiesPrincipalStresses, KratosStruc
     KRATOS_CHECK_LESS_EQUAL((1.16178e+07 - principal_stress_vector[0])/principal_stress_vector[0], tolerance);
     KRATOS_CHECK_LESS_EQUAL((-9.00151e+06 - principal_stress_vector[1])/principal_stress_vector[1], tolerance);
     KRATOS_CHECK_LESS_EQUAL((-2.53163e+07 - principal_stress_vector[2])/principal_stress_vector[2], tolerance);
+}
+
+/**
+* Check the correct calculation of the CL utilities Hencky and Biot strains
+*/
+KRATOS_TEST_CASE_IN_SUITE(ConstitutiveLawUtilitiesHenckyAndBiot, KratosStructuralMechanicsFastSuite)
+{
+    // Declaration of Cauchy tensor and strain vector
+    Matrix C = ZeroMatrix(3, 3);
+    Vector strain_vector(6);
+
+    // Fill Cauchy tensor
+    C(0,0) = 1.1;
+    C(1,1) = 1.0;
+    C(2,2) = 1.0;
+    C(1,0) = -1.0e-1;
+    C(0,1) = -1.0e-1;
+
+    // Compute Hencky
+    ConstitutiveLawUtilities<6>::CalculateHenckyStrain(C, strain_vector);
+
+//     // Debug
+//     KRATOS_WATCH(strain_vector)
+
+    KRATOS_CHECK_LESS_EQUAL((0.0454445 - strain_vector[0])/strain_vector[0], tolerance);
+    KRATOS_CHECK_LESS_EQUAL((-0.0023557 - strain_vector[1])/strain_vector[1], tolerance);
+    KRATOS_CHECK_LESS_EQUAL((0.0 - strain_vector[2])/strain_vector[2], tolerance);
+    KRATOS_CHECK_LESS_EQUAL((-0.0956005 - strain_vector[3])/strain_vector[3], tolerance);
+
+    // Compute Biot
+    ConstitutiveLawUtilities<6>::CalculateBiotStrain(C, strain_vector);
+
+//     // Debug
+//     KRATOS_WATCH(strain_vector)
+
+    KRATOS_CHECK_LESS_EQUAL((0.0476699 - strain_vector[0])/strain_vector[0], tolerance);
+    KRATOS_CHECK_LESS_EQUAL((-0.00119458 - strain_vector[1])/strain_vector[1], tolerance);
+    KRATOS_CHECK_LESS_EQUAL((0.0 - strain_vector[2])/strain_vector[2], tolerance);
+    KRATOS_CHECK_LESS_EQUAL((-0.097729 - strain_vector[3])/strain_vector[3], tolerance);
 }
 } // namespace Testing
 } // namespace Kratos
