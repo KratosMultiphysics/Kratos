f3ce58edfd3315fe2de6a43a99a4b471c7b36e0d
philbucher <philipp.bucher@tum.de>
2018-09-25 15:41:11 +0200

    minor

--- kratos/python/add_model_part_to_python.cpp
+++ kratos/python/add_model_part_to_python.cpp
@@ -882,7 +882,6 @@ void AddModelPartToPython(pybind11::module& m)
         .def("CreateNewMasterSlaveConstraint",CreateNewMasterSlaveConstraint3, return_value_policy::reference_internal)
         KRATOS_DEF_PYTHON_STR(ModelPart)
         ;
-        //KRATOS_DEF_PYTHON_STR(ModelPart, py_class);
 }
 
 } // namespace Python.
