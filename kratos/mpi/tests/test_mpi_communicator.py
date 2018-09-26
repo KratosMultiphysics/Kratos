f815f8a9ba3e4611a843afa4d55402a5e631050c
jcotela <jcotela@cimne.upc.edu>
2018-09-26 18:53:02 +0200

    First test.

---
+++ kratos/mpi/tests/test_mpi_communicator.py
@@ -0,0 +1,17 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.KratosMultiphysicsMPI as MPI #TODO: do not import the so directly (but I need a nice Python module first)
+
+import KratosMultiphysics.KratosUnittest as UnitTest
+
+class MPICommunicatorTest(UnitTest.TestCase):
+
+    def testMPICommunicatorSetup(self):
+        dir(MPI)
+        model_part = Kratos.ModelPart("Test_model_part")
+        MPI.ModelPartCommunicatorUtilities.SetMPICommunicator(model_part)
+
+        # I'd do more complex tests, but this one should work in serial too (JC)
+        self.assertEqual(model_part.GetCommunicator().__str__(), "MPICommunicator")
+
+if __name__ == "__main__":
+    UnitTest.main()
