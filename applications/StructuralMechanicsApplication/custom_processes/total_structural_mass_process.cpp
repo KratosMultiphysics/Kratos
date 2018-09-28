94ea42b8375d27d5dba3e8c860d750882d12c2e1
philbucher <philipp.bucher@tum.de>
2018-09-28 14:54:15 +0200

    computing nodal mass

--- applications/StructuralMechanicsApplication/custom_processes/total_structural_mass_process.cpp
+++ applications/StructuralMechanicsApplication/custom_processes/total_structural_mass_process.cpp
@@ -16,6 +16,7 @@
 // Project includes
 #include "custom_processes/total_structural_mass_process.h"
 #include "structural_mechanics_application_variables.h"
+#include "utilities/variable_utils.h"
 
 namespace Kratos
 {
@@ -34,15 +35,17 @@ void TotalStructuralMassProcess::Execute()
 {
     KRATOS_TRY
 
-    // We initialize the total mass
-    double total_mass  = 0.0;
+    // We initialize the element mass
+    double element_mass = 0.0;
 
     const std::size_t dimension = mrThisModelPart.GetProcessInfo()[DOMAIN_SIZE];
 
     // Now we iterate over the elements to calculate the total mass
     ElementsArrayType& elements_array = mrThisModelPart.GetCommunicator().LocalMesh().Elements();
 
-    #pragma omp parallel for reduction(+:total_mass)
+    VariableUtils().SetNonHistoricalVariable(NODAL_MASS, 0.0, mrThisModelPart.Nodes()); // local- AND ghost-nodes
+
+    #pragma omp parallel for private(element_mass)
     for(int i = 0; i < static_cast<int>(elements_array.size()); ++i){
         const auto it_elem = elements_array.begin() + i;
 
@@ -63,32 +66,34 @@ void TotalStructuralMassProcess::Execute()
 
         if (local_space_dimension == 0) { // POINT MASSES
             if (it_elem->Has(NODAL_MASS))
-                total_mass += it_elem->GetValue(NODAL_MASS);
+                element_mass = it_elem->GetValue(NODAL_MASS);
         } else if (local_space_dimension == 1) { // BEAM CASE
             const double density = GetFromProperty(this_properties,DENSITY);
             const double area = GetFromProperty(this_properties,CROSS_AREA);
-            total_mass += density * area * r_this_geometry.Length();
+            element_mass = density * area * r_this_geometry.Length();
         } else if (local_space_dimension == 2 && dimension == 3) { // SHELL-MEMBRANE
             const double area = r_this_geometry.Area();
             if (this_properties.Has(SHELL_ORTHOTROPIC_LAYERS)) { // composite material
                 const auto orthotropic_layers = this_properties[SHELL_ORTHOTROPIC_LAYERS];
                 for (std::size_t i=0; i<orthotropic_layers.size1(); ++i)
-                    total_mass += orthotropic_layers(i,0) * orthotropic_layers(i,2) * area; // thickness*density*area
+                    element_mass = orthotropic_layers(i,0) * orthotropic_layers(i,2) * area; // thickness*density*area
             } else {
                 const double thickness = GetFromProperty(this_properties,THICKNESS);
                 const double density = GetFromProperty(this_properties,DENSITY);
-                total_mass += density * thickness * area;
+                element_mass = density * thickness * area;
             }
         } else { // SOLID
             const double thickness = (dimension == 2) ? (this_properties.Has(THICKNESS)) ? this_properties[THICKNESS] : 1.0 : 1.0;
             const double volume = (dimension == 2) ? r_this_geometry.Area() : r_this_geometry.Volume();
             const double density = GetFromProperty(this_properties,DENSITY);
-            total_mass += density * thickness * volume;
+            element_mass = density * thickness * volume;
         }
 
-        // We restore the current configuration
-        for (std::size_t i_node = 0; i_node < number_of_nodes; ++i_node)
+        // We restore the current configuration and compute the nodal mass
+        for (std::size_t i_node = 0; i_node < number_of_nodes; ++i_node) {
             noalias(r_this_geometry[i_node].Coordinates()) = current_coordinates[i_node];
+            r_this_geometry[i_node].GetValue(NODAL_MASS) += element_mass / number_of_nodes;
+        }
     }
 
     std::stringstream info_stream;
