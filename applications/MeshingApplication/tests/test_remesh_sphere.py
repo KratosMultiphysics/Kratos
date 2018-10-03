1c7a2a060866b63832946a0e57941c52bdb39148
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-10-03 23:59:03 +0200

    Fix meshing tests

--- applications/MeshingApplication/tests/test_remesh_sphere.py
+++ applications/MeshingApplication/tests/test_remesh_sphere.py
@@ -14,7 +14,8 @@ class TestRemeshMMG(KratosUnittest.TestCase):
         KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
 
         # We create the model part
-        main_model_part = KratosMultiphysics.ModelPart("MainModelPart")
+        current_model = KratosMultiphysics.Model()
+        main_model_part = current_model.CreateModelPart("MainModelPart")
         main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, 3)
         main_model_part.ProcessInfo.SetValue(KratosMultiphysics.TIME, 0.0)
         main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DELTA_TIME, 1.0)
@@ -120,16 +121,13 @@ class TestRemeshMMG(KratosUnittest.TestCase):
                             """)
         check_parameters["reference_file_name"].SetString(file_path + "/" + check_parameters["reference_file_name"].GetString())
         check_parameters["output_file_name"].SetString(file_path + "/" + check_parameters["output_file_name"].GetString())
-        check_files = CompareTwoFilesCheckProcess(main_model_part, check_parameters)
+        check_files = CompareTwoFilesCheckProcess(check_parameters)
 
         check_files.ExecuteInitialize()
         check_files.ExecuteBeforeSolutionLoop()
         check_files.ExecuteInitializeSolutionStep()
         check_files.ExecuteFinalizeSolutionStep()
         check_files.ExecuteFinalize()
-
-        model = KratosMultiphysics.Model()
-        model.AddModelPart(main_model_part)
 
         import from_json_check_result_process
 
@@ -142,7 +140,7 @@ class TestRemeshMMG(KratosUnittest.TestCase):
         }
         """)
 
-        check = from_json_check_result_process.FromJsonCheckResultProcess(model, check_parameters)
+        check = from_json_check_result_process.FromJsonCheckResultProcess(current_model, check_parameters)
         check.ExecuteInitialize()
         check.ExecuteBeforeSolutionLoop()
         check.ExecuteFinalizeSolutionStep()
@@ -159,7 +157,7 @@ class TestRemeshMMG(KratosUnittest.TestCase):
         #}
         #""")
 
-        #out = json_output_process.JsonOutputProcess(model, out_parameters)
+        #out = json_output_process.JsonOutputProcess(current_model, out_parameters)
         #out.ExecuteInitialize()
         #out.ExecuteBeforeSolutionLoop()
         #out.ExecuteFinalizeSolutionStep()
