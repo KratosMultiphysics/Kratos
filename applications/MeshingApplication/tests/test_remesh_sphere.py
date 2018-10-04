e7f1456eb3faff8778497606619a3c4465790e33
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-10-05 01:59:11 +0200

    Adding remesh test

--- applications/MeshingApplication/tests/test_remesh_sphere.py
+++ applications/MeshingApplication/tests/test_remesh_sphere.py
@@ -164,5 +164,140 @@ class TestRemeshMMG(KratosUnittest.TestCase):
         #out.ExecuteBeforeSolutionLoop()
         #out.ExecuteFinalizeSolutionStep()
 
+    def test_remesh_sphere_skin(self):
+        KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
+
+        # We create the model part
+        main_model_part = KratosMultiphysics.ModelPart("MainModelPart")
+        main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, 3)
+        main_model_part.ProcessInfo.SetValue(KratosMultiphysics.TIME, 0.0)
+        main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DELTA_TIME, 1.0)
+        #main_model_part.ProcessInfo.SetValue(KratosMultiphysics.STEP, 1)
+
+        # We add the variables needed
+        main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE)
+        main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE_GRADIENT)
+        main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_H)
+        main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
+
+        for node in main_model_part.Nodes:
+            node.AddDof(KratosMultiphysics.DISTANCE)
+
+        # We import the model main_model_part
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        KratosMultiphysics.ModelPartIO(file_path + "/mmg_eulerian_test/coarse_sphere_skin_test").ReadModelPart(main_model_part)
+
+        # We calculate the gradient of the distance variable
+        find_nodal_h = KratosMultiphysics.FindNodalHProcess(main_model_part)
+        find_nodal_h.Execute()
+
+        # We set to zero the metric
+        metric_vector = KratosMultiphysics.Vector(6)
+        metric_vector[0] = 1.0
+        metric_vector[1] = 1.0
+        metric_vector[2] = 1.0
+        metric_vector[3] = 0.0
+        metric_vector[4] = 0.0
+        metric_vector[5] = 0.0
+
+        for node in main_model_part.Nodes:
+            node.SetValue(MeshingApplication.METRIC_TENSOR_3D, metric_vector)
+
+        mmg_parameters = KratosMultiphysics.Parameters("""
+        {
+            "filename"                         : "mmg_eulerian_test/coarse_sphere_skin_test",
+            "save_external_files"              : true,
+            "echo_level"                       : 0
+        }
+        """)
+
+        # We create the remeshing utility
+        mmg_parameters["filename"].SetString(file_path + "/" + mmg_parameters["filename"].GetString())
+        mmg_process = MeshingApplication.MmgProcess3DSurfaces(main_model_part, mmg_parameters)
+
+        # We remesh
+        mmg_process.Execute()
+
+        # Finally we export to GiD
+        from gid_output_process import GiDOutputProcess
+        gid_output = GiDOutputProcess(main_model_part,
+                                    "gid_output",
+                                    KratosMultiphysics.Parameters("""
+                                        {
+                                            "result_file_configuration" : {
+                                                "gidpost_flags": {
+                                                    "GiDPostMode": "GiD_PostBinary",
+                                                    "WriteDeformedMeshFlag": "WriteUndeformed",
+                                                    "WriteConditionsFlag": "WriteConditions",
+                                                    "MultiFileFlag": "SingleFile"
+                                                },
+                                                "nodal_results"       : ["DISTANCE"]
+                                            }
+                                        }
+                                        """)
+                                    )
+
+        gid_output.ExecuteInitialize()
+        gid_output.ExecuteBeforeSolutionLoop()
+        gid_output.ExecuteInitializeSolutionStep()
+        gid_output.PrintOutput()
+        gid_output.ExecuteFinalizeSolutionStep()
+        gid_output.ExecuteFinalize()
+
+        from compare_two_files_check_process import CompareTwoFilesCheckProcess
+        check_parameters = KratosMultiphysics.Parameters("""
+                            {
+                                "reference_file_name"   : "mmg_eulerian_test/coarse_sphere_skin_test_result.sol",
+                                "output_file_name"      : "mmg_eulerian_test/coarse_sphere_skin_test_step=0.sol",
+                                "dimension"             : 3,
+                                "comparison_type"       : "sol_file"
+                            }
+                            """)
+        check_parameters["reference_file_name"].SetString(file_path + "/" + check_parameters["reference_file_name"].GetString())
+        check_parameters["output_file_name"].SetString(file_path + "/" + check_parameters["output_file_name"].GetString())
+        check_files = CompareTwoFilesCheckProcess(main_model_part, check_parameters)
+
+        check_files.ExecuteInitialize()
+        check_files.ExecuteBeforeSolutionLoop()
+        check_files.ExecuteInitializeSolutionStep()
+        check_files.ExecuteFinalizeSolutionStep()
+        check_files.ExecuteFinalize()
+
+        #model = KratosMultiphysics.Model()
+        #model.AddModelPart(main_model_part)
+
+        #import from_json_check_result_process
+
+        #check_parameters = KratosMultiphysics.Parameters("""
+        #{
+            #"check_variables"      : ["DISTANCE"],
+            #"input_file_name"      : "mmg_eulerian_test/distante_extrapolation_skin.json",
+            #"model_part_name"      : "MainModelPart",
+            #"time_frequency"       : 0.0
+        #}
+        #""")
+
+        #check = from_json_check_result_process.FromJsonCheckResultProcess(model, check_parameters)
+        #check.ExecuteInitialize()
+        #check.ExecuteBeforeSolutionLoop()
+        #check.ExecuteFinalizeSolutionStep()
+
+        ## The following is used to create the solution database
+        #import json_output_process
+
+        #out_parameters = KratosMultiphysics.Parameters("""
+        #{
+            #"output_variables"     : ["DISTANCE"],
+            #"output_file_name"     : "mmg_eulerian_test/distante_extrapolation_skin.json",
+            #"model_part_name"      : "MainModelPart",
+            #"time_frequency"       : 0.0
+        #}
+        #""")
+
+        #out = json_output_process.JsonOutputProcess(model, out_parameters)
+        #out.ExecuteInitialize()
+        #out.ExecuteBeforeSolutionLoop()
+        #out.ExecuteFinalizeSolutionStep()
+
 if __name__ == '__main__':
     KratosUnittest.main()
