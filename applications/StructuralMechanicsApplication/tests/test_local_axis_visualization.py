f6681ffa21865dccd2a5e748aa9d1d4e7ad1822d
philbucher <philipp.bucher@tum.de>
2018-10-16 08:20:55 +0200

    finished first version of test

--- applications/StructuralMechanicsApplication/tests/test_local_axis_visualization.py
+++ applications/StructuralMechanicsApplication/tests/test_local_axis_visualization.py
@@ -4,7 +4,6 @@ import KratosMultiphysics
 import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.kratos_utilities as kratos_utils
-from postprocess_eigenvalues_process import PostProcessEigenvaluesProcess
 from compare_two_files_check_process import CompareTwoFilesCheckProcess
 
 import os, math
@@ -39,21 +38,22 @@ def CreateShellElements(mp,element_name):
         mp.CreateNewElement(element_name, 4, [4,1,5], mp.GetProperties()[1])
 
 
-def CreateGiDOutput(model_part):
+def WriteGiDOutput(model_part):
     from gid_output_process import GiDOutputProcess
     gid_output = GiDOutputProcess(model_part,
-        "gid_output_"+model_part.Name,
+        "local_axis_"+model_part.Name,
         KratosMultiphysics.Parameters("""
             {
                 "result_file_configuration" : {
                     "gidpost_flags": {
-                        "GiDPostMode": "GiD_PostAscii",
-                        "WriteDeformedMeshFlag": "WriteUndeformed",
-                        "WriteConditionsFlag": "WriteConditions",
-                        "MultiFileFlag": "SingleFile"
+                        "GiDPostMode"           : "GiD_PostAscii",
+                        "WriteDeformedMeshFlag" : "WriteUndeformed",
+                        "WriteConditionsFlag"   : "WriteConditions",
+                        "MultiFileFlag"         : "SingleFile"
                     },
                     "nodal_results"       : [],
-                    "gauss_point_results" : ["LOCAL_AXIS_1","LOCAL_AXIS_2","LOCAL_AXIS_3", "LOCAL_MATERIAL_AXIS_1", "LOCAL_MATERIAL_AXIS_2"]
+                    "gauss_point_results" : ["LOCAL_AXIS_1","LOCAL_AXIS_2","LOCAL_AXIS_3",
+                                             "LOCAL_MATERIAL_AXIS_1", "LOCAL_MATERIAL_AXIS_2"]
                 }
             }
             """)
@@ -68,28 +68,55 @@ def CreateGiDOutput(model_part):
 
 
 class TestLocalAxisVisualization(KratosUnittest.TestCase):
-    def tearDown(self):
-        kratos_utils.DeleteFileIfExisting("Structure_EigenResults_0.post.msh")
-        kratos_utils.DeleteFileIfExisting("Structure_EigenResults_0.post.res") # usually this is deleted by the check process but not if it fails
+    @classmethod
+    def tearDownClass(cls):
+    # def tearDown(self):
+        # delete all files leftover from the tests
+        kratos_utils.DeleteFileIfExisting("local_axis_ShellThickElementCorotational3D4N_0.post.msh")
+        kratos_utils.DeleteFileIfExisting("local_axis_ShellThickElementCorotational3D4N_0.post.res") # usually this is deleted by the check process but not if it fails
 
-    def test_ThickQuadShellLocalAxis(self):
+    def test_ThickQuadShellElement(self):
         element_name = "ShellThickElementCorotational3D4N"
-        test_model = KratosMultiphysics.Model()
         model_part = KratosMultiphysics.ModelPart(element_name)
-        test_model.AddModelPart(model_part)
 
         CreateShellNodes(model_part, element_name)
         CreateShellElements(model_part, element_name)
 
-
         for i, elem in enumerate(model_part.Elements):
-            elem.SetValue(StructuralMechanicsApplication.MATERIAL_ORIENTATION_ANGLE, (i+1)/18*math.pi) # every 10 degrees
+            angle = i*25*math.pi/180 # radians, every 25 degree
+            elem.SetValue(StructuralMechanicsApplication.MATERIAL_ORIENTATION_ANGLE, angle)
+
+        WriteGiDOutput(model_part)
+        reference_file_name = "local_axis_ShellThickElementCorotational3D4N_0.post.res.ref"
+        reference_file_name = os.path.join("local_axis_visualization_ref_result_files", reference_file_name)
+        output_file_name = "local_axis_ShellThickElementCorotational3D4N_0.post.res"
+        self.__CheckResults(reference_file_name, output_file_name)
 
 
-        CreateGiDOutput(model_part)
+    def test_3DBeamElement(self):
+        pass
+    def test_3DLinearBeamElement(self):
+        pass
 
+    def __CheckResults(self, ref_file_name, out_file_name):
+        # check the results
+        settings_check_process = KratosMultiphysics.Parameters("""
+        {
+            "reference_file_name"   : \"""" + ref_file_name + """\",
+            "output_file_name"      : \"""" + out_file_name + """\",
+            "comparison_type"       : "post_res_file"
+        }
+        """)
 
+        check_process = CompareTwoFilesCheckProcess(settings_check_process)
 
+        check_process.ExecuteInitialize()
+        check_process.ExecuteBeforeSolutionLoop()
+        check_process.ExecuteInitializeSolutionStep()
+        check_process.ExecuteFinalizeSolutionStep()
+        check_process.ExecuteBeforeOutputStep()
+        check_process.ExecuteAfterOutputStep()
+        check_process.ExecuteFinalize()
 
 
 
