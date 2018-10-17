c68d57fab0d8d66d5123489d37aaa937e2a4a016
jcotela <jcotela@cimne.upc.edu>
2018-10-17 10:32:21 +0200

    Fixing incomplete argument type.

--- kratos/python/add_matrix_to_python.cpp
+++ kratos/python/add_matrix_to_python.cpp
@@ -42,32 +42,32 @@ template< typename TMatrixType > class_< TMatrixType > CreateMatrixInterface(pyb
         binder.def("Size1", [](const TMatrixType& self){return self.size1();} );
         binder.def("Size2", [](const TMatrixType& self){return self.size2();} );
         binder.def("Resize", [](TMatrixType& self, const typename TMatrixType::size_type new_size1, const typename TMatrixType::size_type new_size2)
-                            {if(self.size1() != new_size1 || self.size2() != new_size2) 
+                            {if(self.size1() != new_size1 || self.size2() != new_size2)
                                 self.resize(new_size1, new_size2, false);} );
         binder.def("__setitem__", [](TMatrixType& self, const std::pair<int,int> index, const  typename TMatrixType::value_type value)
                                     {
-                                        const int index_i = index.first; 
-                                        const int index_j = index.second; 
+                                        const int index_i = index.first;
+                                        const int index_j = index.second;
                                         self(index_i,index_j) = value;
                                     } );
         binder.def("__getitem__", [](const TMatrixType& self, const std::pair<int,int> index)
                                     {
-                                        const int index_i = index.first; 
-                                        const int index_j = index.second; 
+                                        const int index_i = index.first;
+                                        const int index_j = index.second;
                                         return self(index_i, index_j);} );
-        
+
         //out of place versions
         binder.def("__add__", [](const TMatrixType& m1, const TMatrixType& m2){ return TMatrixType(m1+m2);}, is_operator());
         binder.def("__sub__", [](const TMatrixType& m1, const TMatrixType& m2){ return TMatrixType(m1-m2);}, is_operator());
         binder.def("__mul__", [](const TMatrixType& m1, const TMatrixType& m2){ return TMatrixType(prod(m1,m2));}, is_operator());
         binder.def("__mul__", [](const TMatrixType& m1, const typename TMatrixType::value_type& value){ return TMatrixType(m1*value);}, is_operator());
         binder.def("__truediv__", [](const TMatrixType& m1, const typename TMatrixType::value_type& value){ return TMatrixType(m1/value);}, is_operator());
-        
+
         //inplace versions
         binder.def("__imul__", [](TMatrixType& m1, const typename TMatrixType::value_type& value){ m1*=value; return m1;}, is_operator());
         binder.def("__itruediv__", [](TMatrixType& m1, const typename TMatrixType::value_type& value){ m1/=value; return m1;}, is_operator());
 
-        binder.def("__repr__", [](const TMatrixType& self)-> const std::string { std::stringstream ss;  ss << self; const std::string out = ss.str();  return out; });      
+        binder.def("__repr__", [](const TMatrixType& self)-> const std::string { std::stringstream ss;  ss << self; const std::string out = ss.str();  return out; });
         return binder;
         }
 
@@ -81,7 +81,7 @@ void  AddMatrixToPython(pybind11::module& m)
 #ifdef KRATOS_USE_AMATRIX   // This macro definition is for the migration period and to be removed afterward please do not use it
 	// This constructor is not supported by AMatrix
 	//matrix_binder.def(init<const DenseMatrix<double>::size_type, const DenseMatrix<double>::size_type, const DenseMatrix<double>::value_type >());
-	matrix_binder.def("fill", [](DenseMatrix<double>& self, const  typename double value) { self.fill(value); });
+	matrix_binder.def("fill", [](DenseMatrix<double>& self, const typename DenseMatrix<double>::value_type value) { self.fill(value); });
 	matrix_binder.def("fill_identity", [](DenseMatrix<double>& self) { self.fill_identity(); });
 #else
 	matrix_binder.def(init<const DenseMatrix<double>::size_type, const DenseMatrix<double>::size_type, const DenseMatrix<double>::value_type >());
@@ -91,7 +91,7 @@ void  AddMatrixToPython(pybind11::module& m)
     matrix_binder.def("__mul__", [](const DenseMatrix<double>& m1, const array_1d<double,3>& v){ if(m1.size2() != 3) KRATOS_ERROR << "matrix size2 is not 3!" << std::endl; return Vector(prod(m1,v));}, is_operator());
 
     ;
-    
+
     //here we add the sparse matrix
     auto compressed_matrix_binder = CreateMatrixInterface< CompressedMatrix >(m,"CompressedMatrix");
     compressed_matrix_binder.def(init<const CompressedMatrix::size_type, const CompressedMatrix::size_type>());
