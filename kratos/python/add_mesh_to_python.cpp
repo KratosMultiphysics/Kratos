96dea9c35569a2a1dd2d80376ca638bcda766dec
jcotela <jcotela@cimne.upc.edu>
2018-10-17 13:02:56 +0200

    Disallowing passing a vector to SetValue methods for array1d.

--- kratos/python/add_mesh_to_python.cpp
+++ kratos/python/add_mesh_to_python.cpp
@@ -1,8 +1,8 @@
-//    |  /           |             
+//    |  /           |
 //    ' /   __| _` | __|  _ \   __|
 //    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics 
+//                   Multi-Physics
 //
 //  License:         BSD License
 //                     Kratos default license: kratos/license.txt
@@ -58,7 +58,7 @@ typedef Geometry<Node<3> >::IntegrationPointsArrayType IntegrationPointsArrayTyp
 typedef Point::CoordinatesArrayType CoordinatesArrayType;
 
 array_1d<double,3> GetNormalFromCondition(
-    Condition& dummy, 
+    Condition& dummy,
     CoordinatesArrayType& LocalCoords
     )
 {
@@ -120,7 +120,7 @@ NodeType::Pointer GetNodeFromCondition( Condition& dummy, unsigned int index )
     return( dummy.GetGeometry().pGetPoint(index) );
 }
 
-void ConditionCalculateLocalSystemStandard( Condition& dummy, 
+void ConditionCalculateLocalSystemStandard( Condition& dummy,
                                                 Matrix& rLeftHandSideMatrix,
                                                 Vector& rRightHandSideVector,
                                                 ProcessInfo& rCurrentProcessInfo)
@@ -250,7 +250,7 @@ void SetValuesOnIntegrationPointsDouble( TObject& dummy, const Variable<double>&
 
     if(values.size() != integration_points.size())
         KRATOS_ERROR << "size of values is : " << values.size() << " while the integration points size is " << integration_points.size() << std::endl;
-    
+
     dummy.SetValueOnIntegrationPoints( rVariable, values, rCurrentProcessInfo );
 }
 
@@ -445,28 +445,24 @@ void  AddMeshToPython(pybind11::module& m)
     .def("__getitem__", GetValueHelperFunction< Element, Variable< array_1d<double, 3>  > >)
     .def("Has", HasHelperFunction< Element, Variable< array_1d<double, 3>  > >)
     .def("SetValue", SetValueHelperFunction< Element, Variable< array_1d<double, 3>  > >)
-    .def("SetValue", [](Element& self, const Variable< array_1d<double,3>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,3>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Element, Variable< array_1d<double, 3>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Element, Variable< array_1d<double, 4>  > >)
     .def("__getitem__", GetValueHelperFunction< Element, Variable< array_1d<double, 4>  > >)
     .def("Has", HasHelperFunction< Element, Variable< array_1d<double, 4>  > >)
     .def("SetValue", SetValueHelperFunction< Element, Variable< array_1d<double, 4>  > >)
-    .def("SetValue", [](Element& self, const Variable< array_1d<double,4>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,4>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Element, Variable< array_1d<double, 4>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Element, Variable< array_1d<double, 6>  > >)
     .def("__getitem__", GetValueHelperFunction< Element, Variable< array_1d<double, 6>  > >)
     .def("Has", HasHelperFunction< Element, Variable< array_1d<double, 6>  > >)
     .def("SetValue", SetValueHelperFunction< Element, Variable< array_1d<double, 6>  > >)
-    .def("SetValue", [](Element& self, const Variable< array_1d<double,6>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,6>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Element, Variable< array_1d<double, 6>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Element, Variable< array_1d<double, 9>  > >)
     .def("__getitem__", GetValueHelperFunction< Element, Variable< array_1d<double, 9>  > >)
     .def("Has", HasHelperFunction< Element, Variable< array_1d<double, 9>  > >)
     .def("SetValue", SetValueHelperFunction< Element, Variable< array_1d<double, 9>  > >)
-    .def("SetValue", [](Element& self, const Variable< array_1d<double,9>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,9>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Element, Variable< array_1d<double, 9>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Element, Variable< Vector > >)
@@ -571,28 +567,24 @@ void  AddMeshToPython(pybind11::module& m)
     .def("__getitem__", GetValueHelperFunction< Condition, Variable< array_1d<double, 3>  > >)
     .def("Has", HasHelperFunction< Condition, Variable< array_1d<double, 3>  > >)
     .def("SetValue", SetValueHelperFunction< Condition, Variable< array_1d<double, 3>  > >)
-    .def("SetValue", [](Condition& self, const Variable< array_1d<double,3>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,3>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Condition, Variable< array_1d<double, 3>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Condition, Variable< array_1d<double, 4>  > >)
     .def("__getitem__", GetValueHelperFunction< Condition, Variable< array_1d<double, 4>  > >)
     .def("Has", HasHelperFunction< Condition, Variable< array_1d<double, 4>  > >)
     .def("SetValue", SetValueHelperFunction< Condition, Variable< array_1d<double, 4>  > >)
-    .def("SetValue", [](Condition& self, const Variable< array_1d<double,4>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,4>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Condition, Variable< array_1d<double, 4>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Condition, Variable< array_1d<double, 6>  > >)
     .def("__getitem__", GetValueHelperFunction< Condition, Variable< array_1d<double, 6>  > >)
     .def("Has", HasHelperFunction< Condition, Variable< array_1d<double, 6>  > >)
     .def("SetValue", SetValueHelperFunction< Condition, Variable< array_1d<double, 6>  > >)
-    .def("SetValue", [](Condition& self, const Variable< array_1d<double,6>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,6>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Condition, Variable< array_1d<double, 6>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Condition, Variable< array_1d<double, 9>  > >)
     .def("__getitem__", GetValueHelperFunction< Condition, Variable< array_1d<double, 9>  > >)
     .def("Has", HasHelperFunction< Condition, Variable< array_1d<double, 9>  > >)
     .def("SetValue", SetValueHelperFunction< Condition, Variable< array_1d<double, 9>  > >)
-    .def("SetValue", [](Condition& self, const Variable< array_1d<double,9>>& rVar, const Vector& value){self.SetValue(rVar, array_1d<double,9>(value));  }  )  //to allow passing a Vector instead of an array_1d
     .def("GetValue", GetValueHelperFunction< Condition, Variable< array_1d<double, 9>  > >)
 
     .def("__setitem__", SetValueHelperFunction< Condition, Variable< Vector > >)
