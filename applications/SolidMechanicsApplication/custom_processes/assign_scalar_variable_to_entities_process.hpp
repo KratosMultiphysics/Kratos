c6e35f57b1d65928aa1b6b99abc06cd64d1bd0b0
Josep Maria <cpuigbo@cimne.upc.edu>
2018-10-18 23:34:03 +0200

    kratos API missing

--- applications/SolidMechanicsApplication/custom_processes/assign_scalar_variable_to_entities_process.hpp
+++ applications/SolidMechanicsApplication/custom_processes/assign_scalar_variable_to_entities_process.hpp
@@ -30,7 +30,7 @@ namespace Kratos
 /// The base class for assigning a value to scalar variables or array_1d components processes in Kratos.
 /** This function assigns a value to a variable belonging to all of the nodes in a given mesh
 */
-class AssignScalarVariableToEntitiesProcess : public Process
+class KRATOS_API(SOLID_MECHANICS_APPLICATION) AssignScalarVariableToEntitiesProcess : public Process
 {
 public:
     ///@name Type Definitions
