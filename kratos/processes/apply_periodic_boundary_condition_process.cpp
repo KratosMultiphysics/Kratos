5b70607ffb69f4fb9672cb9188652f1d66d07369
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2018-10-23 17:09:49 +0200

    Update kratos/processes/apply_periodic_boundary_condition_process.cpp
    Co-Authored-By: adityaghantasala <shine.aditya@gmail.com>

--- kratos/processes/apply_periodic_boundary_condition_process.cpp
+++ kratos/processes/apply_periodic_boundary_condition_process.cpp
@@ -196,7 +196,7 @@ namespace Kratos
     void ApplyPeriodicConditionProcess::ConstraintSlaveNodeWithConditionForVectorVariable(NodeType& rSalveNode, const GeometryType& rHostedGeometry, const VectorType& rWeights, const std::string& rVarName )
     {
         const VariableComponentType r_var_x = KratosComponents<VariableComponentType>::Get(rVarName + std::string("_X"));
-        const VariableComponentType r_var_y = KratosComponents<VariableComponentType>::Get(rVarName + std::string("_Y"));
+        const VariableComponentType& r_var_y = KratosComponents<VariableComponentType>::Get(rVarName + std::string("_Y"));
         const VariableComponentType& r_var_z = KratosComponents<VariableComponentType>::Get(rVarName + std::string("_Z"));
 
         IndexType master_index = 0;
