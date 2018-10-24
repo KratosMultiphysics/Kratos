43ba0b57097fa175e7c75d9da1da79720d7b7d8a
philbucher <philipp.bucher@tum.de>
2018-10-24 12:00:18 +0200

    checking insertion

--- applications/HDF5Application/custom_processes/hdf5_xdmf_connectivities_writer_process.cpp
+++ applications/HDF5Application/custom_processes/hdf5_xdmf_connectivities_writer_process.cpp
@@ -32,7 +32,9 @@ XdmfConnectivitiesWriterProcess::XdmfConnectivitiesWriterProcess(const std::stri
     mKratosToXdmfIdMap.reserve(num_points);
     // Set the parametric coordinate ids.
     for (int i = 0; i < num_points; ++i) {
-        mKratosToXdmfIdMap.insert(IdMapType::value_type(node_ids[i], i)); // TODO check if this entry is already present?
+        const bool inserted = mKratosToXdmfIdMap.insert(IdMapType::value_type(node_ids[i], i)).second;
+        KRATOS_WARNING_IF("Kratos-Xdmf-Map", !inserted) << "Node-Id #" << node_ids[i]
+            << " is duplicated and only inserted once!" << std::endl;
     }
 
     KRATOS_CATCH("");
