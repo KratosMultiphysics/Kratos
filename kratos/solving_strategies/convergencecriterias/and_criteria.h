0803e27c70c9a88b4c5d95551266653f64a3f1d7
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-10-27 19:42:12 +0200

    [Core] Adding Info() to convergence criteria

--- kratos/solving_strategies/convergencecriterias/and_criteria.h
+++ kratos/solving_strategies/convergencecriterias/and_criteria.h
@@ -323,6 +323,28 @@ public:
     ///@{
 
     ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        return "And_Criteria";
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
+
+    ///@}
     ///@name Friends
     ///@{
 
