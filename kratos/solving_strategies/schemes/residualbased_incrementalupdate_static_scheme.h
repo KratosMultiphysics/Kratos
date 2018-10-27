a017507d96de177830dade5bb751869c2e448402
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-10-27 18:47:41 +0200

    [Core] Adding Info() to schemes

--- kratos/solving_strategies/schemes/residualbased_incrementalupdate_static_scheme.h
+++ kratos/solving_strategies/schemes/residualbased_incrementalupdate_static_scheme.h
@@ -51,7 +51,8 @@ namespace Kratos
 template<class TSparseSpace,
          class TDenseSpace //= DenseSpace<double>
          >
-class ResidualBasedIncrementalUpdateStaticScheme : public Scheme<TSparseSpace,TDenseSpace>
+class ResidualBasedIncrementalUpdateStaticScheme
+    : public Scheme<TSparseSpace,TDenseSpace>
 {
 
 public:
@@ -362,6 +363,28 @@ public:
     ///@}
     ///@name Inquiry
     ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        return "ResidualBasedIncrementalUpdateStaticScheme";
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
 
     ///@}
     ///@name Friends
