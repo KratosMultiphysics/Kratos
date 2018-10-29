9570567fe32c4a1050e554cd5c4a3bc5f1eaa556
jcotela <jcotela@cimne.upc.edu>
2018-10-29 12:17:46 +0100

    Adding tests for temporal input and initialization processes.

---
+++ applications/HDF5Application/tests/test_hdf5_processes.py
@@ -0,0 +1,181 @@
+import os
+from KratosMultiphysics import *
+from KratosMultiphysics.HDF5Application import *
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+from single_mesh_temporal_output_process import Factory as OutputFactory
+from single_mesh_temporal_input_process import Factory as TimeInputFactory
+from single_mesh_initialization_process import Factory as InitializationFactory
+
+class TestHDF5Processes(KratosUnittest.TestCase):
+
+    def setUp(self):
+        self.model = Model()
+        self.buffer_size = 2
+
+    def tearDown(self):
+        pass
+
+    def testSingleMeshTemporalOutputInput(self):
+        """
+        Output ModelPart using SingleMeshTemporalOutputProcess
+        Use output file to set values in a new ModelPart with SingleMeshTemporalInputProcess
+        """
+        write_model_part = self._CreateNewModelPart("WriteModelPart")
+        self._InitializeModelPart(write_model_part)
+
+        read_model_part = self._CreateNewModelPart("ReadModelPart")
+
+        output_settings = Parameters(r"""{
+            "Parameters" : {
+                "model_part_name" : "WriteModelPart",
+                "nodal_solution_step_data_settings" : {
+                    "list_of_variables": ["VELOCITY", "DENSITY"]
+                },
+                "output_time_settings" : {
+                    "output_step_frequency": 1
+                }
+            }
+        }""")
+        output_process = OutputFactory(output_settings, self.model)
+
+        input_settings = Parameters(r"""{
+            "Parameters" : {
+                "model_part_name" : "ReadModelPart",
+                "nodal_solution_step_data_settings" : {
+                    "list_of_variables": ["VELOCITY", "DENSITY"]
+                },
+                "file_name": "WriteModelPart"
+            }
+        }""")
+        input_process = TimeInputFactory(input_settings, self.model)
+
+        output_process.ExecuteBeforeSolutionLoop()
+
+        for step in range(1,3):
+            # Run step on write model part and dump results
+            self._SimulateTimeStep(write_model_part, 0.1*step)
+            output_process.ExecuteFinalizeSolutionStep()
+
+            # Read results on read model part
+            read_model_part.CloneTimeStep(0.1*step)
+            input_process.ExecuteInitializeSolutionStep()
+
+            for read_node,write_node in zip(read_model_part.Nodes, write_model_part.Nodes):
+                self.assertEqual(read_node.GetSolutionStepValue(VELOCITY_X,0), write_node.GetSolutionStepValue(VELOCITY_X,0))
+                self.assertEqual(read_node.GetSolutionStepValue(VELOCITY_Y,0), write_node.GetSolutionStepValue(VELOCITY_Y,0))
+                self.assertEqual(read_node.GetSolutionStepValue(VELOCITY_Z,0), write_node.GetSolutionStepValue(VELOCITY_Z,0))
+                self.assertEqual(read_node.GetSolutionStepValue(DENSITY,0),    write_node.GetSolutionStepValue(DENSITY,0))
+                # reaction is not written, so it should not be updated
+                self.assertEqual(read_node.GetSolutionStepValue(REACTION_X,0), 0.0)
+                self.assertEqual(read_node.GetSolutionStepValue(REACTION_Y,0), 0.0)
+                self.assertEqual(read_node.GetSolutionStepValue(REACTION_Z,0), 0.0)
+
+        self._remove_h5_files("WriteModelPart")
+
+    def testSingleMeshTemporalOutputInitialization(self):
+        """
+        Output ModelPart using SingleMeshTemporalOutputProcess
+        Use output file to initialize (restart) a simulation with SingleMeshTemporalInputProcess
+        """
+        write_model_part = self._CreateNewModelPart("WriteModelPart")
+        self._InitializeModelPart(write_model_part)
+
+        read_model_part = self._CreateNewModelPart("ReadModelPart")
+
+        output_settings = Parameters(r"""{
+            "Parameters" : {
+                "model_part_name" : "WriteModelPart",
+                "nodal_solution_step_data_settings" : {
+                    "list_of_variables": ["VELOCITY", "DENSITY"]
+                },
+                "output_time_settings" : {
+                    "output_step_frequency": 1
+                }
+            }
+        }""")
+        output_process = OutputFactory(output_settings, self.model)
+
+        input_settings = Parameters(r"""{
+            "Parameters" : {
+                "model_part_name" : "ReadModelPart",
+                "nodal_solution_step_data_settings" : {
+                    "list_of_variables": ["VELOCITY", "DENSITY"]
+                },
+                "file_name": "WriteModelPart-0.2000"
+            }
+        }""")
+        input_process = InitializationFactory(input_settings, self.model)
+
+        output_process.ExecuteBeforeSolutionLoop()
+
+        for step in range(1,3):
+            # Run step on write model part and dump results
+            self._SimulateTimeStep(write_model_part, 0.1*step)
+            output_process.ExecuteFinalizeSolutionStep()
+
+        # Now initialize the read model part and compare the two model parts
+        input_process.ExecuteInitialize()
+
+        for read_node,write_node in zip(read_model_part.Nodes, write_model_part.Nodes):
+            self.assertEqual(read_node.GetSolutionStepValue(VELOCITY_X,0), write_node.GetSolutionStepValue(VELOCITY_X,0))
+            self.assertEqual(read_node.GetSolutionStepValue(VELOCITY_Y,0), write_node.GetSolutionStepValue(VELOCITY_Y,0))
+            self.assertEqual(read_node.GetSolutionStepValue(VELOCITY_Z,0), write_node.GetSolutionStepValue(VELOCITY_Z,0))
+            self.assertEqual(read_node.GetSolutionStepValue(DENSITY,0),    write_node.GetSolutionStepValue(DENSITY,0))
+            # reaction is not written, so it should not be updated
+            self.assertEqual(read_node.GetSolutionStepValue(REACTION_X,0), 0.0)
+            self.assertEqual(read_node.GetSolutionStepValue(REACTION_Y,0), 0.0)
+            self.assertEqual(read_node.GetSolutionStepValue(REACTION_Z,0), 0.0)
+
+        self._remove_h5_files("WriteModelPart")
+
+    def _CreateNewModelPart(self,label):
+        model_part = self.model.CreateModelPart(label,self.buffer_size)
+        model_part.AddNodalSolutionStepVariable(VELOCITY)
+        model_part.AddNodalSolutionStepVariable(REACTION)
+        model_part.AddNodalSolutionStepVariable(DENSITY)
+
+        model_part.CreateNewNode(1, 0.0, 0.0, 0.0)
+        model_part.CreateNewNode(2, 1.0, 0.0, 0.0)
+        model_part.CreateNewNode(3, 1.0, 1.0, 0.0)
+        model_part.CreateNewNode(4, 0.0, 0.0, 1.0)
+
+        properties = model_part.GetProperties(1,0)
+
+        model_part.CreateNewElement("Element3D4N", 1, [1, 2, 3, 4], properties)
+        model_part.CreateNewCondition("SurfaceCondition3D3N", 1, [2, 3, 4], properties)
+
+        return model_part
+
+    def _InitializeModelPart(self, model_part):
+
+        properties = model_part.GetProperties(1,0)
+        properties[VISCOSITY] = 0.01
+
+        for node in model_part.Nodes:
+            node.SetSolutionStepValue(VELOCITY_X, 0, 10. + node.X)
+            node.SetSolutionStepValue(VELOCITY_Y, 0, 10. + node.Y)
+            node.SetSolutionStepValue(VELOCITY_Z, 0, 10. + node.Z)
+            node.SetSolutionStepValue(REACTION_X, 0, 20. + node.X)
+            node.SetSolutionStepValue(REACTION_Y, 0, 20. + node.Y)
+            node.SetSolutionStepValue(REACTION_Z, 0, 20. + node.Z)
+            node.SetSolutionStepValue(DENSITY, 0, 5.*node.Id)
+
+    def _SimulateTimeStep(self, model_part, new_time):
+        model_part.CloneTimeStep(new_time)
+        for node in model_part.Nodes:
+            u = node.GetSolutionStepValue(VELOCITY,0)
+            f = node.GetSolutionStepValue(REACTION,0)
+            rho = node.GetSolutionStepValue(DENSITY,0)
+
+            node.SetSolutionStepValue(VELOCITY, 0, u*(1+new_time))
+            node.SetSolutionStepValue(REACTION, 0, f*(1+new_time))
+            node.SetSolutionStepValue(DENSITY, 0, rho*(1+new_time))
+
+    def _remove_h5_files(self, model_part_name):
+        for name in os.listdir():
+            if name.find(model_part_name) == 0:
+                kratos_utilities.DeleteFileIfExisting(name)
+
+if __name__ == "__main__":
+    KratosUnittest.main()
