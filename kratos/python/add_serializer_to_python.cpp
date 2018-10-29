a20d4bfbdb91e80dca58a2737f068a2f3f29b41a
riccardo <rrossi@cimne.upc.edu>
2018-10-29 17:44:13 +0100

    adding a StreamSerializer wrapper

--- kratos/python/add_serializer_to_python.cpp
+++ kratos/python/add_serializer_to_python.cpp
@@ -28,29 +28,34 @@ namespace Kratos
 
         //this object is needed because data may contain binary data so the treatment of strings 
     //in python makes impossible to use them (reasons related to locale)
-    class RawSerializedObject 
+    class StreamSerializer : public Serializer 
     {
         public:
-            KRATOS_CLASS_POINTER_DEFINITION(RawSerializedObject); 
+            KRATOS_CLASS_POINTER_DEFINITION(StreamSerializer); 
 
-            RawSerializedObject(const std::string& data)
-                : mdata(data)
+            StreamSerializer(TraceType const& rTrace=SERIALIZER_NO_TRACE)
+                : Serializer(rTrace)
             {}
 
-            virtual ~RawSerializedObject(){}
+            StreamSerializer(const std::string& data,TraceType const& rTrace=SERIALIZER_NO_TRACE)
+                : Serializer(rTrace)
+            {
+                *(this->pGetBuffer()) << data << std::endl;  
+            }
 
-            const std::string& GetData() const {
-                return mdata;
-                }
+            std::string GetStringRepresentation() {
+                return ((std::stringstream*)(this->pGetBuffer()))->str();
+             }
+
+            virtual ~StreamSerializer(){}
 
         private:
-            std::string mdata;
 
             /// Assignment operator.
-            RawSerializedObject& operator=(RawSerializedObject const& rOther) = delete;
+            StreamSerializer& operator=(StreamSerializer const& rOther) = delete;
 
             /// Copy constructor.
-            RawSerializedObject(RawSerializedObject const& rOther) = delete;
+            StreamSerializer(StreamSerializer const& rOther) = delete;
     };
     
 namespace Python
@@ -80,22 +85,6 @@ void SerializerPrint(Serializer& rSerializer)
 void  AddSerializerToPython(pybind11::module& m)
 {
 
-    py::class_<RawSerializedObject, RawSerializedObject::Pointer >(m,"RawSerializedObject")
-    .def("GetData",&RawSerializedObject::GetData)
-    .def("__getstate__", [](const RawSerializedObject &self) { //METHOD NEEDED FOR PICKLE
-        /* Return a tuple that fully encodes the state of the object */
-        return py::make_tuple(py::bytes(self.GetData()));
-    })
-    .def("__setstate__", [](RawSerializedObject &self, py::tuple t) {
-        if (t.size() != 1)
-            throw std::runtime_error("Invalid state!");
-
-        /* Invoke the in-place constructor. Note that this is needed even
-           when the object just has a trivial default constructor */
-        new (&self) RawSerializedObject(t[0].cast<std::string>());
-    })
-    ;
-
 
     py::class_<Serializer, Serializer::Pointer >(m,"Serializer")
     .def(py::init<>())
@@ -109,17 +98,26 @@ void  AddSerializerToPython(pybind11::module& m)
     .def("Load",SerializerLoad<Model>)
     .def("Save",SerializerSave<Model>)
     .def("Print", SerializerPrint)
-    .def("GetStringRepresentation", [](Serializer& self)
-        {
-            return Kratos::make_shared<RawSerializedObject>(((std::stringstream*)(self.pGetBuffer()))->str());
-        })
-    .def("FillBuffer", [](Serializer& self, RawSerializedObject& input_data)
-        {
-            (*self.pGetBuffer()) << input_data.GetData() << std::endl;  
-        } 
-    ) 
     ;
+    
+    py::class_<StreamSerializer, StreamSerializer::Pointer, Serializer >(m,"StreamSerializer")
+    .def(py::init<>())
+    .def(py::init<std::string const&>())
+    .def(py::init<Serializer::TraceType>())
+    .def(py::init<std::string const&, Serializer::TraceType>())
+    .def("__getstate__", [](StreamSerializer &self) { //METHOD NEEDED FOR PICKLE
+        /* Return a tuple that fully encodes the state of the object */
+        return py::make_tuple(py::bytes(self.GetStringRepresentation()));
+    })
+    .def("__setstate__", [](StreamSerializer &self, py::tuple t) { //METHOD NEEDED FOR PICKLE
+        if (t.size() != 1)
+            throw std::runtime_error("Invalid state!");
 
+        /* Invoke the in-place constructor. Note that this is needed even
+           when the object just has a trivial default constructor */
+        new (&self) StreamSerializer(t[0].cast<std::string>());
+    })
+    ;
 
     py::enum_<Serializer::TraceType>(m,"SerializerTraceType")
     .value("SERIALIZER_NO_TRACE", Serializer::SERIALIZER_NO_TRACE)
