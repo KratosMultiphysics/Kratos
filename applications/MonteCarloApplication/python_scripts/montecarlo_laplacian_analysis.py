617da35fad0b2fce986aaf8cc6664fc8f8769956
RTosi <riccardo.tosi@hotmail.it>
2018-10-31 17:11:51 +0100

    Adding MC library

--- applications/MonteCarloApplication/python_scripts/montecarlo_laplacian_analysis.py
+++ applications/MonteCarloApplication/python_scripts/montecarlo_laplacian_analysis.py
@@ -20,6 +20,9 @@ from pycompss.api.task import task
 from pycompss.api.api import compss_wait_on
 from pycompss.api.parameter import *
 
+# Import Monte Carlo library
+import mc as mc
+
 class MonteCarloAnalysis(AnalysisStage):
     """Main script for Monte Carlo simulations using the pure_diffusion solver"""
 
@@ -27,19 +30,19 @@ class MonteCarloAnalysis(AnalysisStage):
     def __init__(self,model,parameters,sample):
         self.sample = sample
         super(MonteCarloAnalysis,self).__init__(model,parameters)
-
+        self._GetSolver().main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
 
     def _CreateSolver(self):
-        import pure_diffusion_solver
-        solver = pure_diffusion_solver.CreateSolver(self.model,self.project_parameters["solver_settings"])
-        self.PureDiffusionSolver = solver
-        return self.PureDiffusionSolver
+        import convection_diffusion_stationary_solver
+        solver = convection_diffusion_stationary_solver.CreateSolver(self.model,self.project_parameters["solver_settings"])
+        self.LaplacianSolver = solver
+        return self.LaplacianSolver
 
     
     def _GetSimulationName(self):
         return "Monte Carlo Analysis"
 
-    
+
     def ApplyBoundaryConditions(self):
         super(MonteCarloAnalysis,self).ApplyBoundaryConditions()
         '''define the forcing function'''
@@ -101,6 +104,10 @@ class MonteCarloAnalysis(AnalysisStage):
 #     return sample
 
 
+'''
+function generating the random sample
+here the sample has a beta distribution with parameters alpha = 2.0 and beta = 6.0
+'''
 def GenerateBetaSample():
     alpha = 2.0
     beta = 6.0
@@ -109,6 +116,10 @@ def GenerateBetaSample():
     return sample
 
 
+'''
+function evaluating the QoI of the problem: int_{domain} TEMPERATURE(x,y) dx dy
+right now we are using the midpoint rule to evaluate the integral: improve!
+'''
 def EvaluateQuantityOfInterest(simulation):
     """here we evaluate the QoI of the problem: int_{domain} SOLUTION(x,y) dx dy
     we use the midpoint rule to evaluate the integral"""
@@ -120,29 +131,10 @@ def EvaluateQuantityOfInterest(simulation):
     return Q
 
 
-def update_onepass_M(sample, old_mean, old_M2, nsam):
-    '''update mean and second moment values
-    M_{2,n} = sum_{i=1}^{n} (x_i - mean(x)_n)^2
-    M_{2,n} = M_{2,n-1} + (x_n - mean(x)_{n-1}) * (x_n - mean(x)_{n})
-    s_n^2 = M_{2,n} / (n-1)'''
-    delta = np.subtract(sample, old_mean)
-    if nsam == 1:
-        new_mean = sample
-        new_M2 = np.zeros(np.size(sample))
-        new_M2 = np.asscalar(new_M2)
-        '''do so to have a list of scalars, and not a list of arrays of one element'''
-    else:
-        new_mean = old_mean + np.divide(delta,nsam)
-        new_M2 = old_M2 + delta*np.subtract(sample,new_mean)
-    new_sample_variance = compute_sample_variance_from_M2(new_M2,nsam)
-    return new_mean, new_M2, new_sample_variance
-
-
-def compute_sample_variance_from_M2(M2,nsam):
-    sample_variance = np.divide(M2,np.subtract(nsam,1))
-    return sample_variance
-
-
+'''
+The following commented execution_task function is analogous to the uncommented execution_task,
+the difference is what the function returns, and we need only the Quantity of Interest (QoI)
+'''
 #@task(model_part_file_name=FILE_IN, parameter_file_name=FILE_IN, returns=4)
 # def execution_task(model_part_file_name, parameter_file_name):
 #     with open(parameter_file_name,'r') as parameter_file:
@@ -163,13 +155,23 @@ def compute_sample_variance_from_M2(M2,nsam):
 #         node_information.append([node.GetSolutionStepValue(KratosMultiphysics.TEMPERATURE), node.GetSolutionStepValue(KratosMultiphysics.NODAL_AREA), node.X, node.Y])
 #     return 1, QoI, node_information, simulation.sample
 
+
+'''
+function executing the problem
+input:
+        model_part_file_name : path of the model part file (still to implement how to import in efficient way in a loop where I have different model part files and different ProjectParameters files, thus for now read model part name from the ProjectParameters.json file)
+        parameter_file_name  : path of the Project Parameters file
+        sample               : stochastic random variable
+output:
+        QoI                  : Quantity of Interest
+'''
 @task(model_part_file_name=FILE_IN, parameter_file_name=FILE_IN, returns=1)
 def execution_task(model_part_file_name, parameter_file_name):
     with open(parameter_file_name,'r') as parameter_file:
         parameters = KratosMultiphysics.Parameters(parameter_file.read())
     local_parameters = parameters # in case there are more parameters file, we rename them
     model = KratosMultiphysics.Model()
-    # local_parameters["solver_settings"]["model_import_settings"]["input_filename"].SetString(model_part_file_name[:-5])
+    local_parameters["solver_settings"]["model_import_settings"]["input_filename"].SetString(model_part_file_name[:-5])
     sample = GenerateBetaSample()
     simulation = MonteCarloAnalysis(model,local_parameters,sample)
     simulation.Run()
@@ -177,15 +179,28 @@ def execution_task(model_part_file_name, parameter_file_name):
     return QoI
 
 
+'''
+The following is the function implemented with Ramon to compute the mean
+Right now we are using the update_onepass_M function, computed in mc.py
+'''
 # @task(returns=2)
-def mean_task(*args):
-    sum_val = 0
-    amount_elems = 0
-    for i in range(int(len(args) / 2)):
-        sum_val = sum_val + (args[2 * i] * args[2 * i + 1])
-        amount_elems = amount_elems + args[2 * i]
-    return amount_elems, sum_val / amount_elems
-    
+# def mean_task(*args):
+#     sum_val = 0
+#     amount_elems = 0
+#     for i in range(int(len(args) / 2)):
+#         sum_val = sum_val + (args[2 * i] * args[2 * i + 1])
+#         amount_elems = amount_elems + args[2 * i]
+#     return amount_elems, sum_val / amount_elems
+
+
+'''
+function executing the problem for sample = 1.0
+input:
+        model_part_file_name  : path of the model part file
+        parameter_file_name   : path of the Project Parameters file
+output:
+        ExactExpectedValueQoI : Quantity of Interest for sample = 1.0
+'''
 @task(model_part_file_name=FILE_IN, parameter_file_name=FILE_IN,returns=1)
 def exact_execution_task(model_part_file_name, parameter_file_name):
     with open(parameter_file_name,'r') as parameter_file:
@@ -200,6 +215,16 @@ def exact_execution_task(model_part_file_name, parameter_file_name):
     ExactExpectedValueQoI = 0.25 * EvaluateQuantityOfInterest(simulation)
     return ExactExpectedValueQoI
 
+
+
+'''
+function computing the relative error between the Multilevel Monte Carlo expected value and the exact expected value
+input :
+        AveragedMeanQoI       : Multilevel Monte Carlo expected value
+        ExactExpectedValueQoI : exact expected value
+output :
+        relative_error        : relative error
+'''
 @task(returns=1)
 def compare_mean(AveragedMeanQoI,ExactExpectedValueQoI):
     relative_error = abs((AveragedMeanQoI - ExactExpectedValueQoI)/ExactExpectedValueQoI)
@@ -221,15 +246,13 @@ if __name__ == '__main__':
     if len(argv) == 2: # ProjectParameters is being passed from outside
         parameter_file_name = argv[1]
     else: # using default name
-        parameter_file_name = "/home/kratos105b/Kratos/applications/MonteCarloApplication/tests/Level0/ProjectParameters.json"
+        parameter_file_name = "/home/kratos105b/Kratos/applications/MonteCarloApplication/tests/Level1/ProjectParameters.json"
 
     with open(parameter_file_name,'r') as parameter_file:
         parameters = KratosMultiphysics.Parameters(parameter_file.read())
     local_parameters = parameters # in case there are more parameters file, we rename them
-    '''Read the number of cycles of the Monte Carlo algorithm from the .json file'''
-    number_samples = local_parameters["problem_data"]["number_samples"].GetInt()
     
-    number_samples = 5
+    number_samples = 10
     Qlist = []
     run_results = []
 
@@ -237,12 +260,6 @@ if __name__ == '__main__':
     ExactExpectedValueQoI = exact_execution_task(local_parameters["solver_settings"]["model_import_settings"]["input_filename"].GetString() + ".mdpa", parameter_file_name)
     
     for instance in range (0,number_samples):
-#        model = KratosMultiphysics.Model()      
-#        sample = GenerateStochasticContribute(local_parameters)
-#        simulation = MonteCarloAnalysis(model,local_parameters,sample)
-#        simulation.Run()
-#        QoI =  EvaluateQuantityOfInterest(simulation)
-#        Qlist.append(QoI)
         Qlist.append(execution_task(local_parameters["solver_settings"]["model_import_settings"]["input_filename"].GetString() + ".mdpa", parameter_file_name))
         
     
@@ -277,7 +294,7 @@ if __name__ == '__main__':
     MC_second_moment = 0.0
     for i in range (0,number_samples):
         nsam = i+1
-        MC_mean, MC_second_moment, MC_variance = update_onepass_M(Qlist[i], MC_mean, MC_second_moment, nsam)
+        MC_mean, MC_second_moment, MC_variance = mc.update_onepass_M(Qlist[i], MC_mean, MC_second_moment, nsam)
     '''Evaluation of the relative error between the computed mean value and the expected value of the QoI'''
     relative_error = compare_mean(MC_mean,ExactExpectedValueQoI)
     # print("Values QoI:",Qlist)
