a5a6e2ddd49e18d095ba21fd409dc4b1e5a6675f
riccardo <rrossi@cimne.upc.edu>
2018-11-02 17:57:14 +0100

    improving pickle interface to avoid a deprecation warning and deprecating Serializer constructors

--- kratos/python/add_serializer_to_python.cpp
+++ kratos/python/add_serializer_to_python.cpp
@@ -18,6 +18,8 @@
 // Project includes
 #include "includes/define_python.h"
 #include "includes/serializer.h"
+#include "includes/file_serializer.h"
+#include "includes/stream_serializer.h"
 #include "python/add_serializer_to_python.h"
 #include "includes/model_part.h"
 #include "includes/kratos_parameters.h"
@@ -28,43 +30,6 @@ namespace Kratos
     
 namespace Python
 {
-
-    //This is a Helper object to provide a simpler interface for serialization to a string instead of to a file
-    class StreamSerializer : public Serializer 
-    {
-        public:
-            KRATOS_CLASS_POINTER_DEFINITION(StreamSerializer); 
-
-            ///this constructor simply wraps the standard Serializer and defines output to basic_iostream
-            ///@param rTrace type of serialization to be employed
-            StreamSerializer(TraceType const& rTrace=SERIALIZER_NO_TRACE)
-                : Serializer(rTrace)
-            {}
-
-            //this constructor generates a standard Serializer AND fills the buffer with the data contained in "data"
-            ///@param data a string contained the data to be used in filling the buffer
-            ///@param rTrace type of serialization to be employed
-            StreamSerializer(const std::string& data,TraceType const& rTrace=SERIALIZER_NO_TRACE)
-                : Serializer(rTrace)
-            {
-                *(this->pGetBuffer()) << data << std::endl;  
-            }
-
-            //get a string representation of the serialized data
-            std::string GetStringRepresentation() {
-                return ((std::stringstream*)(this->pGetBuffer()))->str();
-            }
-
-            virtual ~StreamSerializer(){}
-
-        private:
-
-            /// Assignment operator.
-            StreamSerializer& operator=(StreamSerializer const& rOther) = delete;
-
-            /// Copy constructor.
-            StreamSerializer(StreamSerializer const& rOther) = delete;
-    };
     
 namespace py = pybind11;
 
@@ -91,12 +56,19 @@ void SerializerPrint(Serializer& rSerializer)
 void  AddSerializerToPython(pybind11::module& m)
 {
 
-
     py::class_<Serializer, Serializer::Pointer >(m,"Serializer")
-    .def(py::init<>())
-    .def(py::init<std::string const&>())
-    .def(py::init<Serializer::TraceType>())
-    .def(py::init<std::string const&, Serializer::TraceType>())
+    .def(py::init([](const std::string& FileName) {
+                    KRATOS_WARNING("DEPRECATION") << "Please use FileSerializer(FileName) instead of Serializer(FileName)" << std::endl;
+                    return std::make_shared<FileSerializer>(FileName);
+                }
+            )
+        )
+    .def(py::init([](const std::string& FileName, Serializer::TraceType& rTraceType) {
+                    KRATOS_WARNING("DEPRECATION") << "Please use FileSerializer(FileName,TraceType) instead of Serializer(FileName,TraceType)" << std::endl;
+                    return std::make_shared<FileSerializer>(FileName,rTraceType);
+                }
+            )
+        )
     .def("Load",SerializerLoad<ModelPart>)
     .def("Save",SerializerSave<ModelPart>)
     .def("Load",SerializerLoad<Parameters>)
@@ -105,26 +77,41 @@ void  AddSerializerToPython(pybind11::module& m)
     .def("Save",SerializerSave<Model>)
     .def("Print", SerializerPrint)
     ;
+
+    py::class_<FileSerializer, FileSerializer::Pointer, Serializer >(m,"FileSerializer")
+    .def(py::init<std::string const&>())
+    .def(py::init<std::string const&, Serializer::TraceType>())
+    ;
     
     py::class_<StreamSerializer, StreamSerializer::Pointer, Serializer >(m,"StreamSerializer")
     .def(py::init<>())
     .def(py::init<std::string const&>())
     .def(py::init<Serializer::TraceType>())
     .def(py::init<std::string const&, Serializer::TraceType>())
-    .def("__getstate__", [](StreamSerializer &self) { //METHOD NEEDED FOR PICKLE
-        /* Return a tuple that fully encodes the state of the object */
-        //note that we return a "bytes" object to avoid any "locale" conversion
-        return py::make_tuple(py::bytes(self.GetStringRepresentation()),self.GetTraceType());
-    })
-    .def("__setstate__", [](StreamSerializer &self, py::tuple t) { //METHOD NEEDED FOR PICKLE
-        if (t.size() != 2)
-            throw std::runtime_error("Invalid state!");
-
-        /* Invoke the in-place constructor. Note that this is needed even
-           when the object just has a trivial default constructor */
-        new (&self) StreamSerializer(t[0].cast<std::string>(), t[1].cast<Serializer::TraceType>());
-    })
+    .def(py::pickle(
+            [](StreamSerializer &self) { // __getstate__
+                return py::make_tuple(py::bytes(self.GetStringRepresentation()),self.GetTraceType());
+            },
+            [](py::tuple t) { // __setstate__, note: no `self` argument
+                return Kratos::make_shared<StreamSerializer>(t[0].cast<std::string>(), t[1].cast<Serializer::TraceType>());
+            }
+        )
+    )
     ;
+    // .def("__getstate__", [](StreamSerializer &self) { //METHOD NEEDED FOR PICKLE
+    //     /* Return a tuple that fully encodes the state of the object */
+    //     //note that we return a "bytes" object to avoid any "locale" conversion
+    //     return py::make_tuple(py::bytes(self.GetStringRepresentation()),self.GetTraceType());
+    // })
+    // .def("__setstate__", [](StreamSerializer &self, py::tuple t) { //METHOD NEEDED FOR PICKLE
+    //     if (t.size() != 2)
+    //         throw std::runtime_error("Invalid state!");
+
+    //     /* Invoke the in-place constructor. Note that this is needed even
+    //        when the object just has a trivial default constructor */
+    //     new (&self) StreamSerializer(t[0].cast<std::string>(), t[1].cast<Serializer::TraceType>());
+    // })
+    //;
 
     py::enum_<Serializer::TraceType>(m,"SerializerTraceType")
     .value("SERIALIZER_NO_TRACE", Serializer::SERIALIZER_NO_TRACE)
