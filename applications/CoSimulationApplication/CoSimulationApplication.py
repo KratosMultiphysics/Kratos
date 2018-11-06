2a707bffcc5be59f1d6968c72e5a35186a554418
AdityaGhantasala <shine.aditya@gmail.com>
2018-11-06 09:35:28 +0100

    changes to add the sub folders as sub modules

--- applications/CoSimulationApplication/CoSimulationApplication.py
+++ applications/CoSimulationApplication/CoSimulationApplication.py
@@ -8,7 +8,7 @@ application_name = "KratosCoSimulationApplication"
 application_folder = "CoSimulationApplication"
 
 # The following lines are common for all applications
-from . import application_importer
+from .. import application_importer
 import inspect
 caller = inspect.stack()[1] # Information about the file that imported this, to check for unexpected imports
-application_importer.ImportApplication(application,application_name,application_folder,caller)
+application_importer.ImportApplication(application,application_name,application_folder,caller, __path__)
--- applications/CoSimulationApplication/python_scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-# Adding the current directory to the path such that the modules can be imported with __import__ in the factory
-from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
-import sys, os
-current_dir_name = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir_name)
-
-# Append all the sub directories containing the solver interfaces to the path
-for dirName, subdirList, fileList in os.walk(current_dir_name):
-    sys.path.append(dirName)
