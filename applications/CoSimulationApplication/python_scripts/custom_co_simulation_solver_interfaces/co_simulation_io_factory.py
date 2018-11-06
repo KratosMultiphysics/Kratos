7e08d3e9166f4c26a09eda31710ef2a42f28d735
AdityaGhantasala <shine.aditya@gmail.com>
2018-11-06 16:31:24 +0100

    changes to use new str and sub modules

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/co_simulation_io_factory.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_solver_interfaces/co_simulation_io_factory.py
@@ -6,7 +6,7 @@ New IOs should be registered here with an additional entry.
 eg : "name_in_JSON" : "python module(file) name"
 """
 available_ios = {
-    "kratos" : "kratos_io",
+    "kratos" : "kratos_interfaces.kratos_io",
     "sdof"   : "su2_io",
     "dummy"  : "dummy_co_simulation_io"
 }
@@ -17,7 +17,8 @@ def CreateIO(io_type, settings):
     New IOs have to be registered by adding them to "available_ios"
     """
     if io_type in available_ios:
-        io_module = __import__(available_ios[io_type])
+        module_full_name = 'custom_co_simulation_solver_interfaces.'+available_ios[io_type]
+        io_module = __import__(module_full_name,fromlist=[available_ios[io_type]])
         return io_module.Create(settings)
     else:
         err_msg  = 'The requested IO "' + io_name + '" is not available!\n'
