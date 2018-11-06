5f14bef9b5582a125508d1bdc60e8d5682c1f32c
AdityaGhantasala <shine.aditya@gmail.com>
2018-11-06 09:36:45 +0100

    changes to coupled solver fact to use new data str

--- applications/CoSimulationApplication/python_scripts/custom_co_simulation_coupled_solvers/co_simulation_coupled_solver_factory.py
+++ applications/CoSimulationApplication/python_scripts/custom_co_simulation_coupled_solvers/co_simulation_coupled_solver_factory.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+
 """
 This is a map of the name of coupled solver to be specified in
 JSON file and their python module (file) name. New coupled solvers should
@@ -17,16 +18,15 @@ def CreateCoupledSolver(settings):
     This function creates and returns the coupled solver used for CoSimulation
     One can register the a new coupled solver by adding them to the above map "available_coupled_solvers"
     """
-    if (type(settings) != dict):
-        raise Exception("Input is expected to be provided as a python dictionary")
-
     coupled_solver_type = settings["coupled_solver_settings"]["solver_type"]
 
-    if coupled_solver_type in available_coupled_solvers:
-        coupled_solver_module = __import__(available_coupled_solvers[coupled_solver_type])
+    if coupled_solver_type.GetString() in available_coupled_solvers:
+        module_name = available_coupled_solvers[coupled_solver_type.GetString()]
+        module_full = 'custom_co_simulation_coupled_solvers.'+module_name
+        coupled_solver_module = __import__(module_full,fromlist=[module_name])
         return coupled_solver_module.Create(settings)
     else:
-        err_msg  = 'The requested coupled solver "' + coupled_solver_type + '" is not available!\n'
+        err_msg  = 'The requested coupled solver "' + coupled_solver_type.GetString() + '" is not available!\n'
         err_msg += 'Available coupled solvers are :\n'
         for available_coupled_solver in available_coupled_solvers:
             err_msg += "\t" + available_coupled_solver + "\n"
