079cdaf9fb7101b416384afe89c8704691ddfafa
Unknown <cgguillem@gmail.com>
2018-11-06 14:49:39 +0100

    Removing swimming_DEM_application again
    This is an attempt to remove the conflicts that arose when people added changes to the folder that I had just renamed (removed)

--- applications/swimming_DEM_application/python_scripts/colloids/run_colloids.py
+++
@@ -1,5 +0,0 @@
-import KratosSwimmingDEM as script
-
-import colloids_algorithm
-test = script.Solution(colloids_algorithm)
-test.Run()
--- applications/swimming_DEM_application/python_scripts/turbulent_cube/run_turbulent_cube.py
+++
@@ -1,6 +0,0 @@
-import KratosSwimmingDEM as script
-import os
-
-import pre_calculated_fluid_algorithm
-test = script.Solution(pre_calculated_fluid_algorithm)
-test.Run()
