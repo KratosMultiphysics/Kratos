fa4fec86ad1dd2b998478883ff49a5ce2b1e167c
Josep Maria <cpuigbo@cimne.upc.edu>
2018-11-08 15:24:17 +0100

    recover original step integration

--- applications/SolidMechanicsApplication/custom_solvers/time_integration_methods/newmark_step_method.hpp
+++ applications/SolidMechanicsApplication/custom_solvers/time_integration_methods/newmark_step_method.hpp
@@ -299,11 +299,11 @@ namespace Kratos
       KRATOS_TRY
 
       // predict step variable from previous and current values
-      // TValueType& CurrentStepVariable            = rNode.FastGetSolutionStepValue(*this->mpStepVariable,     0);
-      // const TValueType& CurrentVariable          = rNode.FastGetSolutionStepValue(*this->mpVariable,         0);
-      // const TValueType& PreviousVariable         = rNode.FastGetSolutionStepValue(*this->mpVariable,         1);
+      TValueType& CurrentStepVariable            = rNode.FastGetSolutionStepValue(*this->mpStepVariable,     0);
+      const TValueType& CurrentVariable          = rNode.FastGetSolutionStepValue(*this->mpVariable,         0);
+      const TValueType& PreviousVariable         = rNode.FastGetSolutionStepValue(*this->mpVariable,         1);
 
-      // CurrentStepVariable  = CurrentVariable-PreviousVariable;
+      CurrentStepVariable  = CurrentVariable-PreviousVariable;
 
       KRATOS_CATCH( "" )
     }
