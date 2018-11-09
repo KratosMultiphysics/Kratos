6944c06e4385cd4e38ba5d480e389a9b895f8b95
philbucher <philipp.bucher@tum.de>
2018-11-09 15:37:43 +0100

    solvers_wrapper update

--- applications/IgaApplication/python_scripts/python_solvers_wrapper_iga.py
+++ applications/IgaApplication/python_scripts/python_solvers_wrapper_iga.py
@@ -2,38 +2,42 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 
 import KratosMultiphysics
 
+def CreateSolverByParameters(model, solver_settings, parallelism):
 
-def CreateSolver(model, custom_settings):
-
-    if not (isinstance(model, KratosMultiphysics.Model)):
-        raise Exception("input is expected to be provided as a Kratos Model object")
-
-    if not (isinstance(custom_settings, KratosMultiphysics.Parameters)):
-        raise Exception("input is expected to be provided as a Kratos Parameters object")
-
-    parallelism = custom_settings["problem_data"]["parallel_type"].GetString()
-    solver_type = custom_settings["solver_settings"]["solver_type"].GetString()
+    solver_type = solver_settings["solver_type"].GetString()
 
     # Solvers for OpenMP parallelism
     if (parallelism == "OpenMP"):
-        if (solver_type == "static" or solver_type == "Static"):
+        if (solver_type == "static"):
             solver_module_name = "iga_static_solver"
-
         else:
             err_msg =  "The requested solver type \"" + solver_type + "\" is not in the python solvers wrapper\n"
             err_msg += "Available options are: \"static\""
             raise Exception(err_msg)
-
     else:
         err_msg =  "The requested parallel type \"" + parallelism + "\" is not available!\n"
         err_msg += "Available options are: \"OpenMP\""
         raise Exception(err_msg)
 
     # Remove settings that are not needed any more
-    custom_settings["solver_settings"].RemoveValue("solver_type")
-    custom_settings["solver_settings"].RemoveValue("time_integration_method") # does not throw even if the value is not existing
+    solver_settings.RemoveValue("solver_type")
+    solver_settings.RemoveValue("time_integration_method") # does not throw even if the value is not existing
 
     solver_module = __import__(solver_module_name)
-    solver = solver_module.CreateSolver(model, custom_settings["solver_settings"])
+    solver = solver_module.CreateSolver(model, solver_settings)
 
     return solver
+
+
+def CreateSolver(model, custom_settings):
+
+    if (type(model) != KratosMultiphysics.Model):
+        raise Exception("input is expected to be provided as a Kratos Model object")#
+
+    if (type(custom_settings) != KratosMultiphysics.Parameters):
+        raise Exception("input is expected to be provided as a Kratos Parameters object")
+
+    solver_settings = custom_settings["solver_settings"]
+    parallelism = custom_settings["problem_data"]["parallel_type"].GetString()
+
+    return CreateSolverByParameters(model, solver_settings, parallelism)
