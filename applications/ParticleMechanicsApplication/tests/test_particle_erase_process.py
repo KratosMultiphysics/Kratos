4e18cb4b923de05f698648016b28332cac0e27a2
Nanda <bodhinandach@gmail.com>
2018-11-13 11:06:03 +0100

    finalize test particle erase

--- applications/ParticleMechanicsApplication/tests/test_particle_erase_process.py
+++ applications/ParticleMechanicsApplication/tests/test_particle_erase_process.py
@@ -7,8 +7,10 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 class TestParticleEraseProcess(KratosUnittest.TestCase):
 
-    def _generate_particle_element_and_check(self, current_model, dimension, geometry_element, num_particle, expected_num_particle):
+    def _generate_particle_element_and_check(self, current_model):
         KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
+
+        dimension = 3
 
         # Initialize model part
         ## Material model part definition
@@ -25,70 +27,108 @@ class TestParticleEraseProcess(KratosUnittest.TestCase):
 
         # Create element and nodes
         sub_mp = initial_material_model_part.CreateSubModelPart("test")
-        self._create_nodes(sub_mp, dimension, geometry_element)
-        self._create_elements(sub_mp,dimension, geometry_element)
+        self._create_nodes(sub_mp)
+        self._create_elements(sub_mp)
+
+        # Create background element and nodes
+        background_sub_mp = grid_model_part.CreateSubModelPart("test")
+        self._create_nodes(background_sub_mp)
+        self._create_elements(background_sub_mp)
 
         # Initialize linear_solver
         linear_solver = KratosMultiphysics.SkylineLUFactorizationSolver()
 
         # Initialize element
-        if geometry_element == "Triangle":
-            if (dimension == 2):
-                new_element = KratosParticle.CreateUpdatedLagragian2D3N()
-            else:
-                new_element = KratosParticle.CreateUpdatedLagragian3D4N()
-        elif geometry_element == "Quadrilateral":
-            if (dimension == 2):
-                new_element = KratosParticle.CreateUpdatedLagragian2D4N()
-            else:
-                new_element = KratosParticle.CreateUpdatedLagragian3D8N()
+        new_element = KratosParticle.CreateUpdatedLagragian3D8N()
 
         # Initialize solver
-        if(dimension==2):
-            solver = KratosParticle.MPM2D(grid_model_part, initial_material_model_part, material_model_part, linear_solver, new_element, False, "static", geometry_element, num_particle, False, False)
-        else:
-            solver = KratosParticle.MPM3D(grid_model_part, initial_material_model_part, material_model_part, linear_solver, new_element, False, "static", geometry_element, num_particle, False, False)
+        self.solver = KratosParticle.MPM3D(grid_model_part, initial_material_model_part, material_model_part, linear_solver, new_element, False, "static", "Quadrilateral", 4, False, False)
 
-    def _create_nodes(self, initial_mp, dimension, geometry_element):
-        if geometry_element == "Triangle":
-            initial_mp.CreateNewNode(1, 0.0, 0.0, 0.0)
-            initial_mp.CreateNewNode(2, 1.0, 0.0, 0.0)
-            initial_mp.CreateNewNode(3, 0.0, 1.0, 0.0)
-            if (dimension == 3):
-                initial_mp.CreateNewNode(4, 0.0, 0.0, 1.0)
-        elif geometry_element == "Quadrilateral":
-            initial_mp.CreateNewNode(1, -0.5, -0.5, 0.0)
-            initial_mp.CreateNewNode(2,  0.5, -0.5, 0.0)
-            initial_mp.CreateNewNode(3,  0.5,  0.5, 0.0)
-            initial_mp.CreateNewNode(4, -0.5,  0.5, 0.0)
-            if (dimension == 3):
-                initial_mp.CreateNewNode(5, -0.5, -0.5, 1.0)
-                initial_mp.CreateNewNode(6,  0.5, -0.5, 1.0)
-                initial_mp.CreateNewNode(7,  0.5,  0.5, 1.0)
-                initial_mp.CreateNewNode(8, -0.5,  0.5, 1.0)
+    def _create_nodes(self, initial_mp):
+        initial_mp.CreateNewNode(1, -0.5, -0.5, 0.0)
+        initial_mp.CreateNewNode(2,  0.5, -0.5, 0.0)
+        initial_mp.CreateNewNode(3,  0.5,  0.5, 0.0)
+        initial_mp.CreateNewNode(4, -0.5,  0.5, 0.0)
+        initial_mp.CreateNewNode(5, -0.5, -0.5, 1.0)
+        initial_mp.CreateNewNode(6,  0.5, -0.5, 1.0)
+        initial_mp.CreateNewNode(7,  0.5,  0.5, 1.0)
+        initial_mp.CreateNewNode(8, -0.5,  0.5, 1.0)
 
-    def _create_elements(self, initial_mp, dimension, geometry_element):
-        if geometry_element == "Triangle":
-            if (dimension == 2):
-                initial_mp.CreateNewElement("UpdatedLagrangian2D3N", 1, [1,2,3], initial_mp.GetProperties()[1])
-            if (dimension == 3):
-                initial_mp.CreateNewElement("UpdatedLagrangian3D4N", 1, [1,2,3,4], initial_mp.GetProperties()[1])
-        elif geometry_element == "Quadrilateral":
-            if (dimension == 2):
-                initial_mp.CreateNewElement("UpdatedLagrangian2D4N", 1, [1,2,3,4], initial_mp.GetProperties()[1])
-            if (dimension == 3):
-                initial_mp.CreateNewElement("UpdatedLagrangian3D8N", 1, [1,2,3,4,5,6,7,8], initial_mp.GetProperties()[1])
-
+    def _create_elements(self, initial_mp):
+        initial_mp.CreateNewElement("UpdatedLagrangian3D8N", 1, [1,2,3,4,5,6,7,8], initial_mp.GetProperties()[1])
         KratosMultiphysics.VariableUtils().SetFlag(KratosMultiphysics.ACTIVE, True, initial_mp.Elements)
+
+    def _search_element(self, current_model):
+        # Default
+        max_num_results = 1000
+        specific_tolerance = 1.e-5
+
+        # Get model part
+        material_model_part = current_model.GetModelPart("dummy_name")
+        grid_model_part     = current_model.GetModelPart("Background_Grid")
+
+        # Search element
+        self.solver.SearchElement(grid_model_part, material_model_part, max_num_results, specific_tolerance)
+
 
     def test_ParticleEraseOutsideGivenDomain(self):
         current_model = KratosMultiphysics.Model()
-        self._generate_particle_element_and_check(current_model, dimension=2, geometry_element="Triangle", num_particle=1, expected_num_particle=1)
+        self._generate_particle_element_and_check(current_model)
+
+        # Get model part
+        material_model_part = current_model.GetModelPart("dummy_name")
+
+        # Move particle
+        for mpm in material_model_part.Elements:
+            new_coordinate = mpm.GetValue(KratosParticle.GAUSS_COORD) + [0.3, 0.23, 0.22]
+            mpm.SetValue(KratosParticle.GAUSS_COORD, new_coordinate)
+
+        # Check outside given domain
+        for mpm in material_model_part.Elements:
+            new_coordinate = mpm.GetValue(KratosParticle.GAUSS_COORD)
+            if(new_coordinate[0] < -0.5 or new_coordinate[0] > 0.5 or new_coordinate[1] < -0.5 or new_coordinate[1] > 0.5 or new_coordinate[2] < -0.5 or new_coordinate[2] > 0.5 ):
+                mpm.Set(KratosMultiphysics.TO_ERASE, True)
+
+        # Initiate process
+        process = KratosParticle.ParticleEraseProcess(material_model_part)
+
+        # Execute
+        process.Execute()
+
+        # Check total number of element
+        particle_counter = len(material_model_part.Elements)
+        self.assertEqual(particle_counter, 1)
+        expected_id = 9
+        for mpm in material_model_part.Elements:
+            self.assertEqual(mpm.Id, expected_id)
 
     def test_ParticleEraseBySearch(self):
         current_model = KratosMultiphysics.Model()
-        self._generate_particle_element_and_check(current_model, dimension=2, geometry_element="Triangle", num_particle=3, expected_num_particle=3)
+        self._generate_particle_element_and_check(current_model)
 
+        # Get model part
+        material_model_part = current_model.GetModelPart("dummy_name")
+
+        # Move particle
+        for mpm in material_model_part.Elements:
+            new_coordinate = mpm.GetValue(KratosParticle.GAUSS_COORD) + [0.3, 0.23, 0.22]
+            mpm.SetValue(KratosParticle.GAUSS_COORD, new_coordinate)
+
+        # Call Search
+        self._search_element(current_model)
+
+        # Initiate process
+        process = KratosParticle.ParticleEraseProcess(material_model_part)
+
+        # Execute
+        process.Execute()
+
+        # Check total number of element
+        particle_counter = len(material_model_part.Elements)
+        self.assertEqual(particle_counter, 1)
+        expected_id = 9
+        for mpm in material_model_part.Elements:
+            self.assertEqual(mpm.Id, expected_id)
 
 if __name__ == '__main__':
     KratosUnittest.main()
