75629bdb0ce0e00890755c58c3aecd60decf2507
philbucher <philipp.bucher@tum.de>
2018-11-18 20:52:48 +0100

    updating missalinged logo

--- applications/CoSimulationApplication/co_simulation_application.cpp
+++ applications/CoSimulationApplication/co_simulation_application.cpp
@@ -26,10 +26,10 @@ void KratosCoSimulationApplication::Register()
 {
     // calling base class register to register Kratos components
     KratosApplication::Register();
-	std::cout  << "KRATOS / ___|  ___   ___ (_) _ __   _   _| | __ _| |_(_)            " << std::endl;
-	std::cout  << "       | |    / _ \\/ __| | '_ ` _ \\| | | | |/ _` | __| |/ _ \\| '_ \\ " << std::endl;
-	std::cout  << "       | |__ | (_) |\\__ \\| | | | | | |_| | | (_| | |_| | (_) | | | |" << std::endl;
-	std::cout  << "       \\____| \\___/___/_/| | | | |\\__,_|_|__\\__,_|\\__|_|\\___/|_| |_| Application" << std::endl;
-    std::cout << "Initializing KratosCoSimulationApplication... " << std::endl;
+	KRATOS_INFO("") << "   KRATOS  / ___|___/ ___|(_)_ __ ___  _   _| | __ _| |_(_) ___  _ __                     " << std::endl
+	                << "          | |   / _ \\___ \\| | '_ ` _ \\| | | | |/ _` | __| |/ _ \\| '_ \\               " << std::endl
+	                << "          | |__| (_) |__) | | | | | | | |_| | | (_| | |_| | (_) | | | |                   " << std::endl
+	                << "           \\____\\___/____/|_|_| |_| |_|\\__,_|_|\\__,_|\\__|_|\\___/|_| |_| Application " << std::endl
+                    << "Initializing KratosCoSimulationApplication...										      " << std::endl;
 }
 } // namespace Kratos.
