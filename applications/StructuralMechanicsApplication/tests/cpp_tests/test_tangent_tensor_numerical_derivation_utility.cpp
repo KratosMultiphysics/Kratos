f034cc4c02c4b1c3744509173cf9028c5206e171
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-11-18 10:48:21 +0100

    Adding hyperelastic test

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_tangent_tensor_numerical_derivation_utility.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_tangent_tensor_numerical_derivation_utility.cpp
@@ -17,8 +17,7 @@
 // Project includes
 #include "includes/process_info.h"
 #include "testing/testing.h"
-
-/* Contitutive Law */
+#include "utilities/math_utils.h"
 #include "custom_utilities/tangent_operator_calculator_utility.h"
 
 namespace Kratos
@@ -27,9 +26,9 @@ namespace Testing
 {
 
 /**
-* Check the correct calculation of the uniaxial stress of the yield surfaces
-*/
-KRATOS_TEST_CASE_IN_SUITE(PertubationTensorTestUtility, KratosStructuralMechanicsFastSuite)
+ * @brief This test tests that the perturbation utility is valid for computing the elastic linear tensor
+ */
+KRATOS_TEST_CASE_IN_SUITE(LinearElasticCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite)
 {
     ConstitutiveLaw::Parameters cl_configuration_values;
     Properties material_properties;
@@ -54,10 +53,16 @@ KRATOS_TEST_CASE_IN_SUITE(PertubationTensorTestUtility, KratosStructuralMechanic
     strain_vector[2] = 8.0e-5;
     strain_vector[5] = 1.6941e-21;
 
-    Matrix F = IdentityMatrix(6);
+    Matrix deformation_gradient(3, 3);
+    deformation_gradient(0,0) = 1.0;
+    deformation_gradient(1,1) = 1.0;
+    deformation_gradient(2,2) = (1.0 - 8.0e-5);
+    deformation_gradient(2,0) = 0.5 * -1.6941e-21;
+    deformation_gradient(0,2) = 0.5 * -1.6941e-21;
 
     cl_configuration_values.SetMaterialProperties(material_properties);
-    cl_configuration_values.SetDeformationGradientF(F);
+    cl_configuration_values.SetDeformationGradientF(deformation_gradient);
+    cl_configuration_values.SetDeterminantF(MathUtils<double>::DetMat(deformation_gradient));
     cl_configuration_values.SetStrainVector(strain_vector);
     cl_configuration_values.SetStressVector(stress_vector);
     cl_configuration_values.SetOptions(constitutive_law_options);
@@ -70,6 +75,68 @@ KRATOS_TEST_CASE_IN_SUITE(PertubationTensorTestUtility, KratosStructuralMechanic
     p_constitutive_law->CalculateValue(cl_configuration_values,CONSTITUTIVE_MATRIX, C);
 
     TangentOperatorCalculatorUtility::CalculateTangentTensor(cl_configuration_values, p_constitutive_law.get());
+    Matrix& r_tangent_moduli = cl_configuration_values.GetConstitutiveMatrix();
+
+    const double tolerance = 1.0e-6;
+    for (std::size_t i = 0; i < 6; ++i) {
+        for (std::size_t j = 0; j < 6; ++j) {
+            if (std::abs(r_tangent_moduli(i, j)) > 0.0) {
+                KRATOS_CHECK_NEAR(C(i, j), r_tangent_moduli(i, j), tolerance);
+            }
+        }
+    }
+}
+
+/**
+ * @brief This test tests that the perturbation utility is valid for computing the hyperelastic tensor
+ */
+KRATOS_TEST_CASE_IN_SUITE(HyperElasticCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite)
+{
+    ConstitutiveLaw::Parameters cl_configuration_values;
+    Properties material_properties;
+    Vector stress_vector, strain_vector;
+    ProcessInfo CurrentProcessInfo;
+
+    Flags constitutive_law_options;
+    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, false);
+    constitutive_law_options.Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, false);
+    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_STRESS, true);
+
+    material_properties.SetValue(YOUNG_MODULUS, 210e9);
+    material_properties.SetValue(POISSON_RATIO, 0.22);
+
+    stress_vector = ZeroVector(6);
+    stress_vector[0] = 5.40984e+06;
+    stress_vector[1] = 5.40984e+06;
+    stress_vector[2] = 1.91803e+07;
+    stress_vector[5] = 1.45804e-10;
+
+    strain_vector = ZeroVector(6);
+    strain_vector[2] = 8.0e-5;
+    strain_vector[5] = 1.6941e-21;
+
+    Matrix deformation_gradient(3, 3);
+    deformation_gradient(0,0) = 1.0;
+    deformation_gradient(1,1) = 1.0;
+    deformation_gradient(2,2) = (1.0 - 8.0e-5);
+    deformation_gradient(2,0) = 0.5 * -1.6941e-21;
+    deformation_gradient(0,2) = 0.5 * -1.6941e-21;
+
+    cl_configuration_values.SetMaterialProperties(material_properties);
+    cl_configuration_values.SetDeformationGradientF(deformation_gradient);
+    cl_configuration_values.SetDeterminantF(MathUtils<double>::DetMat(deformation_gradient));
+    cl_configuration_values.SetStrainVector(strain_vector);
+    cl_configuration_values.SetStressVector(stress_vector);
+    cl_configuration_values.SetOptions(constitutive_law_options);
+    cl_configuration_values.SetProcessInfo(CurrentProcessInfo);
+
+    auto p_constitutive_law = KratosComponents<ConstitutiveLaw>().Get("HyperElastic3DLaw").Clone();
+
+    Matrix C = ZeroMatrix(6, 6);
+    cl_configuration_values.SetConstitutiveMatrix(C);
+    p_constitutive_law->CalculateValue(cl_configuration_values,CONSTITUTIVE_MATRIX_PK2, C);
+
+    TangentOperatorCalculatorUtility::CalculateTangentTensorFiniteDeformation(cl_configuration_values, p_constitutive_law.get());
     Matrix& r_tangent_moduli = cl_configuration_values.GetConstitutiveMatrix();
 
     const double tolerance = 1.0e-6;
