32ba806ee6c191baf36bd10c67e62717bef07811
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-11-19 13:35:56 +0100

    Clean up test

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_tangent_tensor_numerical_derivation_utility.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_tangent_tensor_numerical_derivation_utility.cpp
@@ -25,6 +25,56 @@ namespace Kratos
 namespace Testing
 {
 
+void SettingBasicCase(
+    ConstitutiveLaw::Parameters& rCLConfigurationValues,
+    Properties& rProperties,
+    Vector& rStressVector,
+    Vector& rStrainVector,
+    Matrix& rTangentModuli,
+    Matrix& rF,
+    const bool ElementProvidedStrain = true
+    )
+{
+    ProcessInfo current_process_info;
+
+    Flags constitutive_law_options;
+    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, false);
+    constitutive_law_options.Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, ElementProvidedStrain);
+    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_STRESS, true);
+
+    rProperties.SetValue(YOUNG_MODULUS, 210e9);
+    rProperties.SetValue(POISSON_RATIO, 0.22);
+
+    rStressVector = ZeroVector(6);
+    rStressVector[0] = 5.40984e+06;
+    rStressVector[1] = 5.40984e+06;
+    rStressVector[2] = 1.91803e+07;
+    rStressVector[5] = 1.45804e-10;
+
+    rStrainVector = ZeroVector(6);
+    rStrainVector[2] = 8.0e-5;
+    rStrainVector[5] = 1.6941e-21;
+
+    rF = ZeroMatrix(3, 3);
+    rF(0,0) = 1.0;
+    rF(1,1) = 1.0;
+    rF(2,2) = (1.0 - 8.0e-5);
+    rF(2,0) = 0.5 * -1.6941e-21;
+    rF(0,2) = 0.5 * -1.6941e-21;
+    double detF = MathUtils<double>::DetMat(rF);
+
+    rTangentModuli = ZeroMatrix(6, 6);
+
+    rCLConfigurationValues.SetMaterialProperties(rProperties);
+    rCLConfigurationValues.SetDeformationGradientF(rF);
+    rCLConfigurationValues.SetDeterminantF(detF);
+    rCLConfigurationValues.SetStrainVector(rStrainVector);
+    rCLConfigurationValues.SetStressVector(rStressVector);
+    rCLConfigurationValues.SetOptions(constitutive_law_options);
+    rCLConfigurationValues.SetProcessInfo(current_process_info);
+    rCLConfigurationValues.SetConstitutiveMatrix(rTangentModuli);
+}
+
 /**
  * @brief This test tests that the perturbation utility is valid for computing the elastic linear tensor
  */
@@ -33,44 +83,8 @@ KRATOS_TEST_CASE_IN_SUITE(LinearElasticCasePertubationTensorUtility, KratosStruc
     ConstitutiveLaw::Parameters cl_configuration_values;
     Properties material_properties;
     Vector stress_vector, strain_vector;
-    ProcessInfo CurrentProcessInfo;
-
-    Flags constitutive_law_options;
-    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, false);
-    constitutive_law_options.Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, true);
-    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_STRESS, true);
-
-    material_properties.SetValue(YOUNG_MODULUS, 210e9);
-    material_properties.SetValue(POISSON_RATIO, 0.22);
-
-    stress_vector = ZeroVector(6);
-    stress_vector[0] = 5.40984e+06;
-    stress_vector[1] = 5.40984e+06;
-    stress_vector[2] = 1.91803e+07;
-    stress_vector[5] = 1.45804e-10;
-
-    strain_vector = ZeroVector(6);
-    strain_vector[2] = 8.0e-5;
-    strain_vector[5] = 1.6941e-21;
-
-    Matrix deformation_gradient = ZeroMatrix(3, 3);
-    deformation_gradient(0,0) = 1.0;
-    deformation_gradient(1,1) = 1.0;
-    deformation_gradient(2,2) = (1.0 - 8.0e-5);
-    deformation_gradient(2,0) = 0.5 * -1.6941e-21;
-    deformation_gradient(0,2) = 0.5 * -1.6941e-21;
-    double detF = MathUtils<double>::DetMat(deformation_gradient);
-
-    Matrix tangent_moduli = ZeroMatrix(6, 6);
-
-    cl_configuration_values.SetMaterialProperties(material_properties);
-    cl_configuration_values.SetDeformationGradientF(deformation_gradient);
-    cl_configuration_values.SetDeterminantF(detF);
-    cl_configuration_values.SetStrainVector(strain_vector);
-    cl_configuration_values.SetStressVector(stress_vector);
-    cl_configuration_values.SetOptions(constitutive_law_options);
-    cl_configuration_values.SetProcessInfo(CurrentProcessInfo);
-    cl_configuration_values.SetConstitutiveMatrix(tangent_moduli);
+    Matrix tangent_moduli, deformation_gradient_F;
+    SettingBasicCase(cl_configuration_values, material_properties, stress_vector, strain_vector, tangent_moduli, deformation_gradient_F);
 
     auto p_constitutive_law = KratosComponents<ConstitutiveLaw>().Get("LinearElastic3DLaw").Clone();
 
@@ -99,44 +113,8 @@ KRATOS_TEST_CASE_IN_SUITE(HyperElasticCasePertubationTensorUtility, KratosStruct
     ConstitutiveLaw::Parameters cl_configuration_values;
     Properties material_properties;
     Vector stress_vector, strain_vector;
-    ProcessInfo CurrentProcessInfo;
-
-    Flags constitutive_law_options;
-    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, false);
-    constitutive_law_options.Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, false);
-    constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_STRESS, true);
-
-    material_properties.SetValue(YOUNG_MODULUS, 210e9);
-    material_properties.SetValue(POISSON_RATIO, 0.22);
-
-    stress_vector = ZeroVector(6);
-    stress_vector[0] = 5.40984e+06;
-    stress_vector[1] = 5.40984e+06;
-    stress_vector[2] = 1.91803e+07;
-    stress_vector[5] = 1.45804e-10;
-
-    strain_vector = ZeroVector(6);
-    strain_vector[2] = 8.0e-5;
-    strain_vector[5] = 1.6941e-21;
-
-    Matrix deformation_gradient = ZeroMatrix(3, 3);
-    deformation_gradient(0,0) = 1.0;
-    deformation_gradient(1,1) = 1.0;
-    deformation_gradient(2,2) = (1.0 - 8.0e-5);
-    deformation_gradient(2,0) = 0.5 * -1.6941e-21;
-    deformation_gradient(0,2) = 0.5 * -1.6941e-21;
-    const double detF = MathUtils<double>::DetMat(deformation_gradient);
-
-    Matrix tangent_moduli = ZeroMatrix(6, 6);
-
-    cl_configuration_values.SetMaterialProperties(material_properties);
-    cl_configuration_values.SetDeformationGradientF(deformation_gradient);
-    cl_configuration_values.SetDeterminantF(detF);
-    cl_configuration_values.SetStrainVector(strain_vector);
-    cl_configuration_values.SetStressVector(stress_vector);
-    cl_configuration_values.SetOptions(constitutive_law_options);
-    cl_configuration_values.SetProcessInfo(CurrentProcessInfo);
-    cl_configuration_values.SetConstitutiveMatrix(tangent_moduli);
+    Matrix tangent_moduli, deformation_gradient_F;
+    SettingBasicCase(cl_configuration_values, material_properties, stress_vector, strain_vector, tangent_moduli, deformation_gradient_F, false);
 
     auto p_constitutive_law = KratosComponents<ConstitutiveLaw>().Get("HyperElastic3DLaw").Clone();
 
