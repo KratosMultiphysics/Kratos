5062003c014898a2fa8d10f927543e2121e9d63f
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-11-19 20:14:10 +0100

    Corrections towards embedded FSI

--- kratos/utilities/embedded_skin_utility.cpp
+++ kratos/utilities/embedded_skin_utility.cpp
@@ -82,9 +82,9 @@ namespace Kratos
         VariableUtils().SetFlag<ModelPart::ConditionsContainerType>(TO_ERASE, true, mrSkinModelPart.Conditions());
 
         // Remove all the skin model part geometrical entities
-        // mrSkinModelPart.RemoveNodes(TO_ERASE);
-        // mrSkinModelPart.RemoveElements(TO_ERASE);
-        // mrSkinModelPart.RemoveConditions(TO_ERASE);
+        mrSkinModelPart.RemoveNodes(TO_ERASE);
+        mrSkinModelPart.RemoveElements(TO_ERASE);
+        mrSkinModelPart.RemoveConditions(TO_ERASE);
     }
 
     template<std::size_t TDim>
