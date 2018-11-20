5f03cf5576cfd778fdbec0c69d0d00ff0f7dac7c
Guillermo Casas <gcasas@cimne.upc.edu>
2018-11-20 12:56:21 +0100

    Typo

--- applications/SwimmingDEMApplication/python_scripts/derivative_recovery/lagrangian_recoverer.py
+++ applications/SwimmingDEMApplication/python_scripts/derivative_recovery/lagrangian_recoverer.py
@@ -7,6 +7,6 @@ from . import standard_recoverer
 
 class LagrangianMaterialAccelerationRecoverer(standard_recoverer.StandardMaterialAccelerationRecoverer):
     def __init__(self, pp, model_part):
-        standard_recoverer.MaterialAccelerationRecoverer.__init__(self, pp, model_part)
+        standard_recoverer.StandardMaterialAccelerationRecoverer.__init__(self, pp, model_part)
     def RecoverMaterialAcceleration(self):
         self.cplusplus_recovery_tool.RecoverLagrangianAcceleration(self.model_part)
