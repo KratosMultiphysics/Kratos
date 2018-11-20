4aa4cadedde153fa02c21c3b15c94525ddca58ee
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-11-20 18:28:02 +0100

    Adding plasticity test

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_tangent_tensor_numerical_derivation_utility.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_tangent_tensor_numerical_derivation_utility.cpp
@@ -16,6 +16,9 @@
 
 // Project includes
 #include "includes/process_info.h"
+#include "containers/model.h"
+#include "geometries/tetrahedra_3d_4.h"
+#include "geometries/hexahedra_3d_8.h"
 #include "testing/testing.h"
 #include "utilities/math_utils.h"
 #include "custom_utilities/tangent_operator_calculator_utility.h"
@@ -25,12 +28,15 @@ namespace Kratos
 namespace Testing
 {
 
+/// Node definition
+typedef Node<3> NodeType;
+
 /**
  * @brief This sets the basic case
  */
 void SettingBasicCase(
     ConstitutiveLaw::Parameters& rCLConfigurationValues,
-    Properties& rProperties,
+    Properties& rMaterialProperties,
     Vector& rStressVector,
     Vector& rStrainVector,
     Matrix& rTangentModuli,
@@ -46,8 +52,15 @@ void SettingBasicCase(
     constitutive_law_options.Set(ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN, ElementProvidedStrain);
     constitutive_law_options.Set(ConstitutiveLaw::COMPUTE_STRESS, true);
 
-    rProperties.SetValue(YOUNG_MODULUS, 210e9);
-    rProperties.SetValue(POISSON_RATIO, 0.22);
+    rMaterialProperties.SetValue(YOUNG_MODULUS, 210e9);
+    rMaterialProperties.SetValue(POISSON_RATIO, 0.22);
+    rMaterialProperties.SetValue(YIELD_STRESS_COMPRESSION, 3.0e6);
+    rMaterialProperties.SetValue(YIELD_STRESS_TENSION, 3.0e6);
+    rMaterialProperties.SetValue(FRICTION_ANGLE, 32.0);
+    rMaterialProperties.SetValue(DILATANCY_ANGLE, 16.0);
+    rMaterialProperties.SetValue(SOFTENING_TYPE, 1);
+    rMaterialProperties.SetValue(FRACTURE_ENERGY, 1000.0);
+    rMaterialProperties.SetValue(HARDENING_CURVE, 0);
 
     rStressVector = ZeroVector(6);
 
@@ -71,7 +84,7 @@ void SettingBasicCase(
 
     rTangentModuli = ZeroMatrix(6, 6);
 
-    rCLConfigurationValues.SetMaterialProperties(rProperties);
+    rCLConfigurationValues.SetMaterialProperties(rMaterialProperties);
     rCLConfigurationValues.SetDeformationGradientF(rF);
     rCLConfigurationValues.SetDeterminantF(rDetF);
     rCLConfigurationValues.SetStrainVector(rStrainVector);
@@ -82,7 +95,188 @@ void SettingBasicCase(
 }
 
 /**
- * This computes the convergence rate of the CL
+ * @brief This method creates a simple geometry (hexahedra)
+ */
+void Create3DGeometryHexahedra(
+    ModelPart& rThisModelPart,
+    const std::string ElementName = "SmallDisplacementElement3D8N"
+    )
+{
+    auto& process_info = rThisModelPart.GetProcessInfo();
+    process_info[STEP] = 1;
+    process_info[NL_ITERATION_NUMBER] = 1;
+
+    // Create nodes and elements
+    Properties::Pointer p_elem_prop = rThisModelPart.pGetProperties(1);
+
+    // First we create the nodes
+    NodeType::Pointer p_node_1 = rThisModelPart.CreateNewNode(1 , 0.0 , 1.0 , 1.0);
+    NodeType::Pointer p_node_2 = rThisModelPart.CreateNewNode(2 , 0.0 , 1.0 , 0.0);
+    NodeType::Pointer p_node_3 = rThisModelPart.CreateNewNode(3 , 0.0 , 0.0 , 1.0);
+    NodeType::Pointer p_node_4 = rThisModelPart.CreateNewNode(4 , 1.0 , 1.0 , 1.0);
+    NodeType::Pointer p_node_5 = rThisModelPart.CreateNewNode(5 , 0.0 , 0.0 , 0.0);
+    NodeType::Pointer p_node_6 = rThisModelPart.CreateNewNode(6 , 1.0 , 1.0 , 0.0);
+    NodeType::Pointer p_node_7 = rThisModelPart.CreateNewNode(7 , 1.0 , 0.0 , 1.0);
+    NodeType::Pointer p_node_8 = rThisModelPart.CreateNewNode(8 , 1.0 , 0.0 , 0.0);
+
+    // Now we create the "conditions"
+    std::vector<NodeType::Pointer> element_nodes (8);
+    element_nodes[0] = p_node_5;
+    element_nodes[1] = p_node_8;
+    element_nodes[2] = p_node_6;
+    element_nodes[3] = p_node_2;
+    element_nodes[4] = p_node_3;
+    element_nodes[5] = p_node_7;
+    element_nodes[6] = p_node_4;
+    element_nodes[7] = p_node_1;
+
+    Hexahedra3D8 <NodeType> hexahedra( PointerVector<NodeType>{element_nodes} );
+    Element::Pointer p_elem_0 = rThisModelPart.CreateNewElement(ElementName, 1, hexahedra, p_elem_prop);
+
+    // Initialize elements
+    for (auto& elem : rThisModelPart.Elements()) {
+        elem.Initialize();
+        elem.InitializeSolutionStep(process_info);
+        elem.InitializeNonLinearIteration(process_info);
+    }
+}
+
+/**
+ * @brief This method creates a simple geometry (tetrahedra)
+ */
+void Create3DGeometryTetrahedra(
+    ModelPart& rThisModelPart,
+    const std::string ElementName = "SmallDisplacementElement3D4N"
+    )
+{
+    auto& process_info = rThisModelPart.GetProcessInfo();
+    process_info[STEP] = 1;
+    process_info[NL_ITERATION_NUMBER] = 1;
+
+    // Create nodes and elements
+    Properties::Pointer p_elem_prop = rThisModelPart.pGetProperties(1);
+    // First we create the nodes
+    NodeType::Pointer p_node_1 = rThisModelPart.CreateNewNode(1 , 0.0 , 1.0 , 1.0);
+    NodeType::Pointer p_node_2 = rThisModelPart.CreateNewNode(2 , 0.0 , 1.0 , 0.0);
+    NodeType::Pointer p_node_3 = rThisModelPart.CreateNewNode(3 , 0.0 , 0.0 , 1.0);
+    NodeType::Pointer p_node_4 = rThisModelPart.CreateNewNode(4 , 1.0 , 1.0 , 1.0);
+    NodeType::Pointer p_node_5 = rThisModelPart.CreateNewNode(5 , 0.0 , 0.0 , 0.0);
+    NodeType::Pointer p_node_6 = rThisModelPart.CreateNewNode(6 , 1.0 , 1.0 , 0.0);
+    NodeType::Pointer p_node_7 = rThisModelPart.CreateNewNode(7 , 1.0 , 0.0 , 1.0);
+    NodeType::Pointer p_node_8 = rThisModelPart.CreateNewNode(8 , 1.0 , 0.0 , 0.0);
+    NodeType::Pointer p_node_9 = rThisModelPart.CreateNewNode(9 , 2.0 , 1.0 , 1.0);
+    NodeType::Pointer p_node_10 = rThisModelPart.CreateNewNode(10 , 2.0 , 1.0 , 0.0);
+    NodeType::Pointer p_node_11 = rThisModelPart.CreateNewNode(11 , 2.0 , 0.0 , 1.0);
+    NodeType::Pointer p_node_12 = rThisModelPart.CreateNewNode(12 , 2.0 , 0.0 , 0.0);
+
+    // Now we create the "conditions"
+    std::vector<NodeType::Pointer> element_nodes_0 (4);
+    element_nodes_0[0] = p_node_12;
+    element_nodes_0[1] = p_node_10;
+    element_nodes_0[2] = p_node_8;
+    element_nodes_0[3] = p_node_9;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_0( PointerVector<NodeType>{element_nodes_0} );
+    std::vector<NodeType::Pointer> element_nodes_1 (4);
+    element_nodes_1[0] = p_node_4;
+    element_nodes_1[1] = p_node_6;
+    element_nodes_1[2] = p_node_9;
+    element_nodes_1[3] = p_node_7;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_1( PointerVector<NodeType>{element_nodes_1} );
+    std::vector<NodeType::Pointer> element_nodes_2 (4);
+    element_nodes_2[0] = p_node_11;
+    element_nodes_2[1] = p_node_7;
+    element_nodes_2[2] = p_node_9;
+    element_nodes_2[3] = p_node_8;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_2( PointerVector<NodeType>{element_nodes_2} );
+    std::vector<NodeType::Pointer> element_nodes_3 (4);
+    element_nodes_3[0] = p_node_5;
+    element_nodes_3[1] = p_node_3;
+    element_nodes_3[2] = p_node_8;
+    element_nodes_3[3] = p_node_6;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_3( PointerVector<NodeType>{element_nodes_3} );
+    std::vector<NodeType::Pointer> element_nodes_4 (4);
+    element_nodes_4[0] = p_node_4;
+    element_nodes_4[1] = p_node_6;
+    element_nodes_4[2] = p_node_7;
+    element_nodes_4[3] = p_node_3;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_4( PointerVector<NodeType>{element_nodes_4} );
+    std::vector<NodeType::Pointer> element_nodes_5 (4);
+    element_nodes_5[0] = p_node_2;
+    element_nodes_5[1] = p_node_3;
+    element_nodes_5[2] = p_node_5;
+    element_nodes_5[3] = p_node_6;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_5( PointerVector<NodeType>{element_nodes_5} );
+    std::vector<NodeType::Pointer> element_nodes_6 (4);
+    element_nodes_6[0] = p_node_10;
+    element_nodes_6[1] = p_node_9;
+    element_nodes_6[2] = p_node_6;
+    element_nodes_6[3] = p_node_8;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_6( PointerVector<NodeType>{element_nodes_6} );
+    std::vector<NodeType::Pointer> element_nodes_7 (4);
+    element_nodes_7[0] = p_node_7;
+    element_nodes_7[1] = p_node_8;
+    element_nodes_7[2] = p_node_3;
+    element_nodes_7[3] = p_node_6;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_7( PointerVector<NodeType>{element_nodes_7} );
+    std::vector<NodeType::Pointer> element_nodes_8 (4);
+    element_nodes_8[0] = p_node_7;
+    element_nodes_8[1] = p_node_8;
+    element_nodes_8[2] = p_node_6;
+    element_nodes_8[3] = p_node_9;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_8( PointerVector<NodeType>{element_nodes_8} );
+    std::vector<NodeType::Pointer> element_nodes_9 (4);
+    element_nodes_9[0] = p_node_4;
+    element_nodes_9[1] = p_node_1;
+    element_nodes_9[2] = p_node_6;
+    element_nodes_9[3] = p_node_3;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_9( PointerVector<NodeType>{element_nodes_9} );
+    std::vector<NodeType::Pointer> element_nodes_10 (4);
+    element_nodes_10[0] = p_node_9;
+    element_nodes_10[1] = p_node_12;
+    element_nodes_10[2] = p_node_11;
+    element_nodes_10[3] = p_node_8;
+
+    Tetrahedra3D4 <NodeType> tetrahedra_10( PointerVector<NodeType>{element_nodes_10} );
+    std::vector<NodeType::Pointer> element_nodes_11 (4);
+    element_nodes_11[0] = p_node_3;
+    element_nodes_11[1] = p_node_2;
+    element_nodes_11[2] = p_node_1;
+    element_nodes_11[3] = p_node_6;
+    Tetrahedra3D4 <NodeType> tetrahedra_11( PointerVector<NodeType>{element_nodes_11} );
+
+    Element::Pointer p_elem_0 = rThisModelPart.CreateNewElement(ElementName, 1, tetrahedra_0, p_elem_prop);
+    Element::Pointer p_elem_1 = rThisModelPart.CreateNewElement(ElementName, 2, tetrahedra_1, p_elem_prop);
+    Element::Pointer p_elem_2 = rThisModelPart.CreateNewElement(ElementName, 3, tetrahedra_2, p_elem_prop);
+    Element::Pointer p_elem_3 = rThisModelPart.CreateNewElement(ElementName, 4, tetrahedra_3, p_elem_prop);
+    Element::Pointer p_elem_4 = rThisModelPart.CreateNewElement(ElementName, 5, tetrahedra_4, p_elem_prop);
+    Element::Pointer p_elem_5 = rThisModelPart.CreateNewElement(ElementName, 6, tetrahedra_5, p_elem_prop);
+    Element::Pointer p_elem_6 = rThisModelPart.CreateNewElement(ElementName, 7, tetrahedra_6, p_elem_prop);
+    Element::Pointer p_elem_7 = rThisModelPart.CreateNewElement(ElementName, 8, tetrahedra_7, p_elem_prop);
+    Element::Pointer p_elem_8 = rThisModelPart.CreateNewElement(ElementName, 9, tetrahedra_8, p_elem_prop);
+    Element::Pointer p_elem_9 = rThisModelPart.CreateNewElement(ElementName, 10, tetrahedra_9, p_elem_prop);
+    Element::Pointer p_elem_10 = rThisModelPart.CreateNewElement(ElementName, 11, tetrahedra_10, p_elem_prop);
+    Element::Pointer p_elem_11 = rThisModelPart.CreateNewElement(ElementName, 12, tetrahedra_11, p_elem_prop);
+
+    // Initialize elements
+    for (auto& elem : rThisModelPart.Elements()) {
+        elem.Initialize();
+        elem.InitializeSolutionStep(process_info);
+        elem.InitializeNonLinearIteration(process_info);
+    }
+}
+
+/**
+ * @brief This computes the convergence rate of the CL
  */
 void ComputingConvergenceRate(
     ConstitutiveLaw::Pointer pConstitutiveLaw,
@@ -149,14 +343,9 @@ void ComputingConvergenceRate(
             TangentOperatorCalculatorUtility::CalculateTangentTensor(rCLConfigurationValues, pConstitutiveLaw.get(), ConstitutiveLaw::StressMeasure::StressMeasure_PK2);
         }
 
-//         const Matrix C = rTangentModuli;
-//
 //         cl_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, true);
 //         pConstitutiveLaw->CalculateMaterialResponse(rCLConfigurationValues, ConstitutiveLaw::StressMeasure::StressMeasure_PK2);
 //         cl_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, false);
-//
-//         const Matrix aux_error = rTangentModuli - C;
-//         const double error = norm_frobenius(aux_error);
 
         noalias(computed_delta_stress) = prod(rTangentModuli, delta_strain_vector);
         const Vector aux_error_vector = computed_delta_stress - expected_delta_stress;
@@ -166,6 +355,11 @@ void ComputingConvergenceRate(
 
         if (Debug) {
             KRATOS_WATCH(error)
+            if (pConstitutiveLaw->Has(PLASTIC_DISSIPATION)) {
+                double plastic_dissipation;
+                pConstitutiveLaw->GetValue(PLASTIC_DISSIPATION, plastic_dissipation);
+                KRATOS_WARNING_IF("PLASTICITY", plastic_dissipation < 1.0e-12) << "This test is not in plastic range" << std::endl;
+            }
         }
     }
 
@@ -187,8 +381,11 @@ void ComputingConvergenceRate(
  */
 KRATOS_TEST_CASE_IN_SUITE(LinearElasticCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite)
 {
+    Model this_model;
+    ModelPart& r_model_part = this_model.CreateModelPart("Main");
+
     ConstitutiveLaw::Parameters cl_configuration_values;
-    Properties material_properties;
+    Properties material_properties = r_model_part.GetProperties(1);
     Vector stress_vector, strain_vector;
     Matrix tangent_moduli, deformation_gradient_F;
     double det_deformation_gradient_F;
@@ -219,8 +416,11 @@ KRATOS_TEST_CASE_IN_SUITE(LinearElasticCasePertubationTensorUtility, KratosStruc
  */
 KRATOS_TEST_CASE_IN_SUITE(HyperElasticCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite)
 {
+    Model this_model;
+    ModelPart& r_model_part = this_model.CreateModelPart("Main");
+
     ConstitutiveLaw::Parameters cl_configuration_values;
-    Properties material_properties;
+    Properties material_properties = r_model_part.GetProperties(1);
     Vector stress_vector, strain_vector;
     Matrix tangent_moduli, deformation_gradient_F;
     double det_deformation_gradient_F;
@@ -251,8 +451,11 @@ KRATOS_TEST_CASE_IN_SUITE(HyperElasticCasePertubationTensorUtility, KratosStruct
  */
 KRATOS_TEST_CASE_IN_SUITE(QuadraticLinearElasticCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite)
 {
+    Model this_model;
+    ModelPart& r_model_part = this_model.CreateModelPart("Main");
+
     ConstitutiveLaw::Parameters cl_configuration_values;
-    Properties material_properties;
+    Properties material_properties = r_model_part.GetProperties(1);
     Vector stress_vector, strain_vector;
     Matrix tangent_moduli, deformation_gradient_F;
     double det_deformation_gradient_F;
@@ -269,8 +472,11 @@ KRATOS_TEST_CASE_IN_SUITE(QuadraticLinearElasticCasePertubationTensorUtility, Kr
  */
 KRATOS_TEST_CASE_IN_SUITE(QuadraticHyperElasticCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite2)
 {
+    Model this_model;
+    ModelPart& r_model_part = this_model.CreateModelPart("Main");
+
     ConstitutiveLaw::Parameters cl_configuration_values;
-    Properties material_properties;
+    Properties material_properties = r_model_part.GetProperties(1);
     Vector stress_vector, strain_vector;
     Matrix tangent_moduli, deformation_gradient_F;
     double det_deformation_gradient_F;
@@ -279,7 +485,43 @@ KRATOS_TEST_CASE_IN_SUITE(QuadraticHyperElasticCasePertubationTensorUtility, Kra
     auto p_constitutive_law = KratosComponents<ConstitutiveLaw>().Get("HyperElastic3DLaw").Clone();
     p_constitutive_law->CalculateMaterialResponse(cl_configuration_values, ConstitutiveLaw::StressMeasure::StressMeasure_PK2);
 
-    ComputingConvergenceRate(p_constitutive_law, cl_configuration_values, stress_vector, strain_vector, tangent_moduli, deformation_gradient_F, det_deformation_gradient_F, true, true);
+//     ComputingConvergenceRate(p_constitutive_law, cl_configuration_values, stress_vector, strain_vector, tangent_moduli, deformation_gradient_F, det_deformation_gradient_F, true, true);
+}
+
+/**
+ * @brief This test tests that the perturbation utility is valid for computing the ehyper lastic tensor
+ */
+KRATOS_TEST_CASE_IN_SUITE(QuadraticSmallStrainIsotropicPlasticity3DVonMisesVonMisesCasePertubationTensorUtility, KratosStructuralMechanicsFastSuite2)
+{
+    Model this_model;
+    ModelPart& r_model_part = this_model.CreateModelPart("Main");
+
+    ConstitutiveLaw::Parameters cl_configuration_values;
+    Properties& r_material_properties = r_model_part.GetProperties(1);
+    Vector stress_vector, strain_vector;
+    Matrix tangent_moduli, deformation_gradient_F;
+    double det_deformation_gradient_F;
+    SettingBasicCase(cl_configuration_values, r_material_properties, stress_vector, strain_vector, tangent_moduli, deformation_gradient_F, det_deformation_gradient_F);
+
+    // Creating constitutive law
+    auto p_constitutive_law = KratosComponents<ConstitutiveLaw>().Get("SmallStrainIsotropicPlasticity3DVonMisesVonMises").Clone();
+    r_material_properties.SetValue(CONSTITUTIVE_LAW, p_constitutive_law);
+
+    // Creating geometry
+    Create3DGeometryHexahedra(r_model_part);
+
+    // Assigning geometry
+    auto& r_geom = r_model_part.Elements().begin()->GetGeometry();
+    cl_configuration_values.SetElementGeometry(r_geom);
+
+    // Initializing law
+//     p_constitutive_law->InitializeMaterialResponse(cl_configuration_values, ConstitutiveLaw::StressMeasure::StressMeasure_PK2);
+    Vector N = ZeroVector(8);
+    p_constitutive_law->InitializeMaterial(r_material_properties, r_geom, N);
+    p_constitutive_law->CalculateMaterialResponse(cl_configuration_values, ConstitutiveLaw::StressMeasure::StressMeasure_PK2);
+
+    // Computing convergence rate
+    ComputingConvergenceRate(p_constitutive_law, cl_configuration_values, stress_vector, strain_vector, tangent_moduli, deformation_gradient_F, det_deformation_gradient_F, false, true);
 }
 
 } // namespace Testing
