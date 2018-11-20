333070150c4f19428ec524972e7eee0a4d9224c4
Miguel Angel <maceli@cimne.upc.edu>
2018-11-20 10:01:44 +0100

    Gid_io makes a method virtual for possible overrides

--- kratos/includes/gid_io.h
+++ kratos/includes/gid_io.h
@@ -1155,7 +1155,7 @@ public:
      * or undeformed state
      * @param Mode either GiD_PostAscii (default) or GiD_PostBinary
      */
-    void WriteNodeMesh( MeshType& rThisMesh )
+    virtual void WriteNodeMesh( MeshType& rThisMesh )
     {
         KRATOS_TRY
 
