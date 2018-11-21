fixing nonlinear strain energy calculation

applications/StructuralMechanicsApplication/python_scripts/structural_response.py
@@ -421,19 +421,6 @@ class NonlinearAdjointStrainEnergy(ResponseFunctionBase):
         if not self.primal_analysis.time < self.primal_analysis.end_time:
             self.primal_analysis.end_time += 1
         
-        self.strain_energy = 0
-        # initializing the load and displacement vectors
-        self.elements_load_vectors = []
-        self.elements_displacement_vector_previous_step = []
-        self.element_displacement_vector = []
-        for element in self.primal_model_part.Elements:
-            self.elements_load_vectors.append(np.zeros(6))
-            self.elements_displacement_vector_previous_step.append(np.zeros(6))
-            self.element_displacement_vector.append(np.zeros(6))
-
-        self.LHS = KratosMultiphysics.Matrix(6,6)
-        self.RHS = KratosMultiphysics.Vector(6)
-        
         #self.primal_analysis.RunSolutionLoop()
         ## run the solution loop
         while self.primal_analysis.time < self.primal_analysis.end_time:
@@ -443,34 +430,76 @@ class NonlinearAdjointStrainEnergy(ResponseFunctionBase):
             self.primal_analysis._GetSolver().SolveSolutionStep()
             self.primal_analysis.FinalizeSolutionStep()
             self.primal_analysis.OutputSolutionStep()
-            
-            #self.ReadDisplacementData(self.primal_analysis.time)
-            #self.CalculateStrainEnergy()
-            self.strain_energy += self.CalculateStrainEnergyIncrement()
-            print(self.strain_energy)
+            self.CalculateResponseIncrement()
         
         Logger.PrintInfo("> Time needed for solving the primal analysis = ",round(timer.time() - startTime,2),"s")
 
         # TODO the response value calculation for stresses currently only works on the adjoint modelpart
         # this needs to be improved, also the response value should be calculated on the PRIMAL modelpart!!
-    #    self.adjoint_analysis.time = self.adjoint_analysis._GetSolver().AdvanceInTime(self.adjoint_analysis.time)
-    #    self.adjoint_analysis.InitializeSolutionStep()
+        
+        self.adjoint_analysis.time = self.adjoint_analysis._GetSolver().AdvanceInTime(self.adjoint_analysis.time)
+        self.adjoint_analysis.InitializeSolutionStep()
 
-    #AdjointNonlinearStrainEnergyResponseFunction
+    def CalculateResponseIncrement(self):
+        startTime = timer.time()
+        value = self._GetResponseFunctionUtility().CalculateResponseIncrement(self.primal_model_part)
+        Logger.PrintInfo("> Time needed for calculating the response value = ",round(timer.time() - startTime,2),"s")
 
-    def CalculateStrainEnergyIncrement(self):
+
+    def CalculateValue(self):
         startTime = timer.time()
         value = self._GetResponseFunctionUtility().CalculateValue(self.primal_model_part)
         Logger.PrintInfo("> Time needed for calculating the response value = ",round(timer.time() - startTime,2),"s")
 
-        return value
+        self.primal_model_part.ProcessInfo[StructuralMechanicsApplication.RESPONSE_VALUE] = value
+        
+        print("strain energy" , value)
 
-    # def CalculateValue(self):
+    
+    ## TODO implement update sensitivities for this class
+    # def CalculateGradient(self):
+    #     Logger.PrintInfo("\n> Starting adjoint analysis for response:", self.identifier)
     #     startTime = timer.time()
-    #     value = self._GetResponseFunctionUtility().CalculateValue(self.primal_model_part)
-    #     Logger.PrintInfo("> Time needed for calculating the response value = ",round(timer.time() - startTime,2),"s")
+    #     self.adjoint_analysis._GetSolver().Predict()
+    #     self.adjoint_analysis._GetSolver().SolveSolutionStep()
+    #     Logger.PrintInfo("> Time needed for solving the adjoint analysis = ",round(timer.time() - startTime,2),"s")
 
-    #     self.primal_model_part.ProcessInfo[StructuralMechanicsApplication.RESPONSE_VALUE] = value
+    # def GetValue(self):
+    #     return self.primal_model_part.ProcessInfo[StructuralMechanicsApplication.RESPONSE_VALUE]
+
+
+    # def GetShapeGradient(self):
+    #     gradient = {}
+    #  #   for node in self.adjoint_model_part.Nodes:
+    #  #       gradient[node.Id] = node.GetSolutionStepValue(SHAPE_SENSITIVITY)
+    #     return gradient
+
+
+    # def FinalizeSolutionStep(self):
+    #     self.adjoint_analysis.FinalizeSolutionStep()
+    #     self.adjoint_analysis.OutputSolutionStep()
+
+
+    def Finalize(self):
+        self.primal_analysis.Finalize()
+     #   self.adjoint_analysis.Finalize()
+
+
+    def _GetResponseFunctionUtility(self):
+        return self.adjoint_analysis._GetSolver().response_function
+
+
+    # def _SynchronizeAdjointFromPrimal(self):
+    #     if len(self.primal_model_part.Nodes) != len(self.adjoint_model_part.Nodes):
+    #         raise RuntimeError("_SynchronizeAdjointFromPrimal: Model parts have a different number of nodes!")
+
+    #     for primal_node, adjoint_node in zip(self.primal_model_part.Nodes, self.adjoint_model_part.Nodes):
+    #         adjoint_node.X0 = primal_node.X0
+    #         adjoint_node.Y0 = primal_node.Y0
+    #         adjoint_node.Z0 = primal_node.Z0
+    #         adjoint_node.X = primal_node.X
+    #         adjoint_node.Y = primal_node.Y
+    #         adjoint_node.Z = primal_node.Z
 
     # ## this function calculates the response only in the python interface
     # def CalculateStrainEnergy(self):
@@ -516,47 +545,3 @@ class NonlinearAdjointStrainEnergy(ResponseFunctionBase):
     #         element_displacement = np.concatenate([node_displacement[0], node_displacement[1]])
     #         self.element_displacement_vector[element.Id - 1] = element_displacement     
 
-
-    # def CalculateGradient(self):
-    #     Logger.PrintInfo("\n> Starting adjoint analysis for response:", self.identifier)
-    #     startTime = timer.time()
-    # #    self.adjoint_analysis._GetSolver().Predict()
-    # #    self.adjoint_analysis._GetSolver().SolveSolutionStep()
-    #     Logger.PrintInfo("> Time needed for solving the adjoint analysis = ",round(timer.time() - startTime,2),"s")
-
-    # def GetValue(self):
-    #     return self.primal_model_part.ProcessInfo[StructuralMechanicsApplication.RESPONSE_VALUE]
-
-
-    # def GetShapeGradient(self):
-    #     gradient = {}
-    #  #   for node in self.adjoint_model_part.Nodes:
-    #  #       gradient[node.Id] = node.GetSolutionStepValue(SHAPE_SENSITIVITY)
-    #     return gradient
-
-
-    # def FinalizeSolutionStep(self):
-    #     self.adjoint_analysis.FinalizeSolutionStep()
-    #     self.adjoint_analysis.OutputSolutionStep()
-
-
-    def Finalize(self):
-        self.primal_analysis.Finalize()
-     #   self.adjoint_analysis.Finalize()
-
-
-    def _GetResponseFunctionUtility(self):
-        return self.adjoint_analysis._GetSolver().response_function
-
-
-    # def _SynchronizeAdjointFromPrimal(self):
-    #     if len(self.primal_model_part.Nodes) != len(self.adjoint_model_part.Nodes):
-    #         raise RuntimeError("_SynchronizeAdjointFromPrimal: Model parts have a different number of nodes!")
-
-    #     for primal_node, adjoint_node in zip(self.primal_model_part.Nodes, self.adjoint_model_part.Nodes):
-    #         adjoint_node.X0 = primal_node.X0
-    #         adjoint_node.Y0 = primal_node.Y0
-    #         adjoint_node.Z0 = primal_node.Z0
-    #         adjoint_node.X = primal_node.X
-    #         adjoint_node.Y = primal_node.Y
-    #         adjoint_node.Z = primal_node.Z