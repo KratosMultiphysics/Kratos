a1d9dba79869ec8348c5b5851f5ec61039893948
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2018-11-21 17:44:19 +0100

    Adding lagrangian rotation process. Work in progress

---
+++ applications/PfemFluidDynamicsApplication/python_scripts/lagrangian_rotation_process.py
@@ -0,0 +1,37 @@
+import KratosMultiphysics
+import KratosMultiphysics.PoromechanicsApplication as KratosPoro
+
+def Factory(settings, Model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return LagrangianRotationProcess(Model, settings["Parameters"])
+
+## All the processes python should be derived from "Process"
+
+class LagrangianRotationProcess(KratosMultiphysics.Process):
+    def __init__(self, Model, settings ):
+        KratosMultiphysics.Process.__init__(self)
+
+        model_part = Model[settings["model_part_name"].GetString()]
+
+        params = KratosMultiphysics.Parameters("{}")
+        params.AddValue("model_part_name",settings["model_part_name"])
+        params.AddValue("dimension",settings["dimension"])
+        params.AddValue("stress_limit",settings["stress_limit"])
+
+        self.process = KratosPoro.PeriodicInterfaceProcess(model_part, params)
+
+        if(settings["dimension"].GetInt() == 2):
+            self.FindNodalNeigh = KratosMultiphysics.FindNodalNeighboursProcess(model_part,2,5)
+        else:
+            self.FindNodalNeigh = KratosMultiphysics.FindNodalNeighboursProcess(model_part,10,10)
+
+    def ExecuteInitialize(self):
+
+        self.FindNodalNeigh.Execute()
+
+        self.process.ExecuteInitialize()
+
+    def ExecuteFinalizeSolutionStep(self):
+
+        self.process.ExecuteFinalizeSolutionStep()
