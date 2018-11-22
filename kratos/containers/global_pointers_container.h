cf86006ee874d9056043d873f985dfb2acc08e9a
riccardo <rrossi@cimne.upc.edu>
2018-11-22 17:43:08 +0100

    adding a container for global pointers with a working serialization mechanism

---
+++ kratos/containers/global_pointers_container.h
@@ -0,0 +1,301 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics 
+//
+//  License:		 BSD License 
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Riccardo Rossi 
+//
+	           
+#if !defined(KRATOS_GLOBAL_POINTER_CONTAINER_H_INCLUDED )
+#define  KRATOS_GLOBAL_POINTER_CONTAINER_H_INCLUDED
+
+// System includes
+#include <vector>
+#include <iostream> 
+
+
+// External includes 
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/global_pointer.h"
+#include "includes/serializer.h"
+
+namespace Kratos
+{
+  ///@addtogroup ApplicationNameApplication
+  ///@{
+
+  ///@name Kratos Globals
+  ///@{ 
+  
+  ///@} 
+  ///@name Type Definitions
+  ///@{ 
+  
+  ///@} 
+  ///@name  Enum's
+  ///@{
+      
+  ///@}
+  ///@name  Functions 
+  ///@{
+      
+  ///@}
+  ///@name Kratos Classes
+  ///@{
+  
+  /// Short class definition.
+  /** Detail class definition.
+  */
+  template< class TDataType >
+  class GlobalPointersContainer : public std::vector< GlobalPointer<TDataType> >
+    {
+    public:
+      ///@name Type Definitions
+      ///@{
+      
+      /// Pointer definition of GlobalPointersContainer
+      KRATOS_CLASS_POINTER_DEFINITION(GlobalPointersContainer);
+  
+      ///@}
+      ///@name Life Cycle 
+      ///@{ 
+      
+      /// Default constructor.
+      GlobalPointersContainer(){}
+
+      /// Destructor.
+      virtual ~GlobalPointersContainer(){}
+
+      template < class TContainerType > 
+      void FillFromContainer( TContainerType& container)
+      {
+          this->reserve(container.size());
+          for(auto& item : container)
+          {
+              this->push_back(GlobalPointer<TDataType>(&item));
+          }
+      }
+      
+
+      ///@}
+      ///@name Operators 
+      ///@{
+      
+      
+      ///@}
+      ///@name Operations
+      ///@{
+      
+      
+      ///@}
+      ///@name Access
+      ///@{ 
+      
+      
+      ///@}
+      ///@name Inquiry
+      ///@{
+      
+      
+      ///@}      
+      ///@name Input and output
+      ///@{
+
+      /// Turn back information as a string.
+      virtual std::string Info() const
+      {
+	    std::stringstream buffer;
+        buffer << "GlobalPointersContainer" ;
+        return buffer.str();
+      }
+      
+      /// Print information about this object.
+      virtual void PrintInfo(std::ostream& rOStream) const {rOStream << "GlobalPointersContainer";}
+
+      /// Print object's data.
+      virtual void PrintData(std::ostream& rOStream) const {}
+      
+            
+      ///@}      
+      ///@name Friends
+      ///@{
+      
+            
+      ///@}
+      
+    protected:
+      ///@name Protected static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operators
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Protected  Access 
+      ///@{ 
+        
+        
+      ///@}      
+      ///@name Protected Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Protected LifeCycle 
+      ///@{ 
+      
+            
+      ///@}
+      
+    private:
+      ///@name Static Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Member Variables 
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private Operators
+      ///@{ 
+        friend class Serializer;
+
+        void save(Serializer& rSerializer) const
+        {
+            std::size_t pointer_size = sizeof(GlobalPointer<TDataType> );
+
+            std::string data;
+            data.resize( this->size() * pointer_size);
+            for(std::size_t i=0; i<this->size(); ++i)
+            {
+                (*this)[i].save(&data[0]+i*pointer_size);
+            }
+
+            rSerializer.save("Size", this->size());
+            rSerializer.save("Data", data);
+
+            // the following version works but it should be less optimized than the version above
+            // rSerializer.save("Size", this->size());
+            // for(const auto& item : *this)
+            //     rSerializer.save("Gp", item);
+        }
+
+        void load(Serializer& rSerializer)
+        {
+            std::size_t pointer_size = sizeof(GlobalPointer<TDataType> );
+
+            std::size_t size;
+            rSerializer.load("Size", size);
+            this->reserve(size);
+
+            std::string tmp;
+            rSerializer.load("Data", tmp);
+
+            for(std::size_t i = 0; i<size; ++i)
+            {
+                GlobalPointer<TDataType> p(nullptr);
+                p.load(&tmp[0]+i*pointer_size);
+                this->push_back(p);
+           }
+
+            // the following version works but it should be less optimized than the version above
+            // std::size_t size;
+            // rSerializer.load("Size", size);
+            // this->reserve(size);
+            // for(std::size_t i = 0; i<size; ++i)
+            // {
+            //     GlobalPointer<TDataType> p(nullptr);
+            //     rSerializer.load("Gp", p);
+            //     this->push_back(p);
+            // }
+        }
+        
+        
+      ///@} 
+      ///@name Private Operations
+      ///@{ 
+        
+        
+      ///@} 
+      ///@name Private  Access 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Private Inquiry 
+      ///@{ 
+        
+        
+      ///@}    
+      ///@name Un accessible methods 
+      ///@{ 
+      
+      /// Assignment operator.
+      GlobalPointersContainer& operator=(GlobalPointersContainer const& rOther){}
+
+      /// Copy constructor.
+      GlobalPointersContainer(GlobalPointersContainer const& rOther){}
+
+        
+      ///@}    
+        
+    }; // Class GlobalPointersContainer 
+
+  ///@} 
+  
+  ///@name Type Definitions       
+  ///@{ 
+  
+  
+  ///@} 
+  ///@name Input and output 
+  ///@{ 
+        
+ 
+  /// input stream function
+  template< class TDataType >
+  inline std::istream& operator >> (std::istream& rIStream, 
+				    GlobalPointersContainer<TDataType>& rThis){}
+
+  /// output stream function
+  template< class TDataType >
+  inline std::ostream& operator << (std::ostream& rOStream, 
+				    const GlobalPointersContainer<TDataType>& rThis)
+    {
+      rThis.PrintInfo(rOStream);
+      rOStream << std::endl;
+      rThis.PrintData(rOStream);
+
+      return rOStream;
+    }
+  ///@}
+
+  ///@} addtogroup block
+  
+}  // namespace Kratos.
+
+#endif // KRATOS_GLOBAL_POINTER_CONTAINER_H_INCLUDED  defined 
+
+
---
+++ kratos/tests/global_pointers/test_global_pointers_container.cpp
@@ -0,0 +1,51 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Riccardo Rossi
+
+#include "testing/testing.h"
+#include "includes/global_pointer.h"
+#include "containers/global_pointers_container.h"
+#include "containers/model.h"
+#include "includes/model_part.h" 
+#include "includes/stream_serializer.h" 
+
+
+namespace Kratos {
+namespace Testing {
+
+
+KRATOS_TEST_CASE_IN_SUITE(GlobalPointersContainerTest, KratosCoreFastSuite)
+{
+    Model current_model;
+    ModelPart& mp = current_model.CreateModelPart("test");
+
+    mp.CreateNewNode(1,1.0,2.0,3.0);
+    mp.CreateNewNode(2,1.0,2.0,3.0);
+    mp.CreateNewNode(3,1.0,2.0,3.0);
+
+    GlobalPointersContainer<Node<3>> global_pointers_container;
+    global_pointers_container.FillFromContainer(mp.Nodes());
+
+    StreamSerializer serializer;
+    serializer.save("global_pointers_container", global_pointers_container);
+
+    GlobalPointersContainer<Node<3>> new_global_pointers;
+    serializer.load("global_pointers_container",new_global_pointers);
+
+    for(std::size_t i=0; i<global_pointers_container.size(); ++i)
+    {
+        KRATOS_CHECK_EQUAL(&*new_global_pointers[i], &*global_pointers_container[i]);
+    }
+    
+
+}
+
+} // namespace Testing
+} // namespace Kratos
