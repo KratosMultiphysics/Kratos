1150c9ed993229dc14df7cb68981890fa82b5880
Miguel Angel Celigueta <maceli@cimne.upc.edu>
2018-11-23 17:27:11 +0100

    codacy suggestion

--- applications/PfemFluidDynamicsApplication/python_scripts/pfem_fluid_gid_output_process.py
+++ applications/PfemFluidDynamicsApplication/python_scripts/pfem_fluid_gid_output_process.py
@@ -1,5 +1,4 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-import os
 from KratosMultiphysics import *
 import KratosMultiphysics.PfemFluidDynamicsApplication as PfemFluid
 CheckForPreviousImport()
