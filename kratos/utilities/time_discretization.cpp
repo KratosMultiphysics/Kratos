3c743285d7da37328765c2ed0c969f6711701882
philbucher <philipp.bucher@tum.de>
2018-11-24 10:49:43 +0100

    adding empty files

---
+++ kratos/utilities/time_discretization.cpp
@@ -0,0 +1,28 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "time_discretization.h"
+
+namespace Kratos {
+namespace TimeDiscretization {
+
+
+
+} // namespace TimeDiscretization.
+}  // namespace Kratos.
+
+
---
+++ kratos/utilities/time_discretization.h
@@ -0,0 +1,31 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+#if !defined(KRATOS_TIME_DISCRETIZATION_H_INCLUDED )
+#define  KRATOS_TIME_DISCRETIZATION_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+
+namespace Kratos {
+namespace TimeDiscretization {
+
+
+
+} // namespace TimeDiscretization.
+}  // namespace Kratos.
+
+#endif // KRATOS_TIME_DISCRETIZATION_H_INCLUDED  defined
