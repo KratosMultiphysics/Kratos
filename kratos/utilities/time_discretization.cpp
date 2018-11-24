37652c5fc0d34a9b49c37f87bd2c42311c47234b
philbucher <philipp.bucher@tum.de>
2018-11-24 11:20:31 +0100

    adding BDF-classes

--- kratos/utilities/time_discretization.cpp
+++ kratos/utilities/time_discretization.cpp
@@ -20,6 +20,80 @@
 namespace Kratos {
 namespace TimeDiscretization {
 
+void BDF1::ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients)
+{
+
+    if (rCoefficients.size() != 2) rCoefficients.resize(2);
+
+    rCoefficients[0] =  1.0/DeltaTime;
+    rCoefficients[1] = -1.0/DeltaTime;
+}
+
+void BDF2::ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients)
+{
+    if (rCoefficients.size() != 3) rCoefficients.resize(3);
+
+    const double denom = 1.0/(2.0*DeltaTime);
+
+    rCoefficients[0] =  3.0 / denom;
+    rCoefficients[1] = -4.0 / denom;
+    rCoefficients[2] =  2.0 / denom;
+
+}
+
+void BDF3::ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients)
+{
+    if (rCoefficients.size() != 4) rCoefficients.resize(4);
+
+    const double denom = 1.0/(6.0*DeltaTime);
+
+    rCoefficients[0] =  11.0 / denom;
+    rCoefficients[1] = -18.0 / denom;
+    rCoefficients[2] =   9.0 / denom;
+    rCoefficients[3] =  -2.0 / denom;
+}
+
+void BDF4::ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients)
+{
+    if (rCoefficients.size() != 5) rCoefficients.resize(5);
+
+    const double denom = 1.0/(12.0*DeltaTime);
+
+    rCoefficients[0] =  25.0 / denom;
+    rCoefficients[1] = -48.0 / denom;
+    rCoefficients[2] =  36.0 / denom;
+    rCoefficients[3] = -16.0 / denom;
+    rCoefficients[4] =   3.0 / denom;
+}
+
+void BDF5::ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients)
+{
+    if (rCoefficients.size() != 6) rCoefficients.resize(6);
+
+    const double denom = 1.0/(60.0*DeltaTime);
+
+    rCoefficients[0] =  137.0 / denom;
+    rCoefficients[1] = -300.0 / denom;
+    rCoefficients[2] =  300.0 / denom;
+    rCoefficients[3] = -200.0 / denom;
+    rCoefficients[4] =   75.0 / denom;
+    rCoefficients[5] =  -12.0 / denom;
+}
+
+void BDF6::ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients)
+{
+    if (rCoefficients.size() != 7) rCoefficients.resize(7);
+
+    const double denom = 1.0/(60.0*DeltaTime);
+
+    rCoefficients[0] =  147.0 / denom;
+    rCoefficients[1] = -360.0 / denom;
+    rCoefficients[2] =  450.0 / denom;
+    rCoefficients[3] = -400.0 / denom;
+    rCoefficients[4] =  225.0 / denom;
+    rCoefficients[5] =  -72.0 / denom;
+    rCoefficients[6] =   10.0 / denom;
+}
 
 
 } // namespace TimeDiscretization.
--- kratos/utilities/time_discretization.h
+++ kratos/utilities/time_discretization.h
@@ -14,15 +14,61 @@
 #define  KRATOS_TIME_DISCRETIZATION_H_INCLUDED
 
 // System includes
+#include <vector>
 
 // External includes
 
 // Project includes
-#include "includes/define.h"
 
 namespace Kratos {
 namespace TimeDiscretization {
 
+class BDF1
+{
+public:
+    void ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients);
+};
+
+class BDF2
+{
+public:
+    void ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients);
+};
+
+class BDF3
+{
+public:
+    void ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients);
+};
+
+class BDF4
+{
+public:
+    void ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients);
+};
+
+class BDF5
+{
+public:
+    void ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients);
+};
+
+class BDF6
+{
+public:
+    void ComputeBDFCoefficients(const double DeltaTime, std::vector<double>& rCoefficients);
+};
+
+std::size_t GetMinimumBufferSize(const BDF1& rTimeDisc) { return 2;}
+std::size_t GetMinimumBufferSize(const BDF2& rTimeDisc) { return 3;}
+std::size_t GetMinimumBufferSize(const BDF3& rTimeDisc) { return 4;}
+std::size_t GetMinimumBufferSize(const BDF4& rTimeDisc) { return 5;}
+std::size_t GetMinimumBufferSize(const BDF5& rTimeDisc) { return 6;}
+std::size_t GetMinimumBufferSize(const BDF6& rTimeDisc) { return 7;}
+
+// std::size_t GetMinimumBufferSize(const Newmark& rTimeDisc)          { return 2;}
+// std::size_t GetMinimumBufferSize(const Bossak& rTimeDisc)           { return 2;}
+// std::size_t GetMinimumBufferSize(const GeneralizedAlpha& rTimeDisc) { return 2;}
 
 
 } // namespace TimeDiscretization.
