3d47b097e71f4b26bc7202393a898389a45f61b7
philbucher <philipp.bucher@tum.de>
2018-11-25 12:14:44 +0100

    revert adding flag, was not being considered anyway

--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
@@ -55,8 +55,7 @@ class MechanicalSolver(PythonSolver):
             "analysis_type": "non_linear",
             "model_import_settings": {
                 "input_type": "mdpa",
-                "input_filename": "unknown_name",
-                "skip_timer": true
+                "input_filename": "unknown_name"
             },
             "computing_model_part_name" : "computing_domain",
             "material_import_settings" :{
