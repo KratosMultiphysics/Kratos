b98730fc0aeb8c5c5e95330d0a037c3aa4ffefe5
philbucher <philipp.bucher@tum.de>
2018-11-26 08:27:37 +0100

    cleanup

--- kratos/tests/test_importing.py
+++ kratos/tests/test_importing.py
@@ -14,27 +14,26 @@ class TestImporting(KratosUnittest.TestCase):
     def test_importing(self):
         current_model = Model()
 
-        #import KratosMultiphysics.FluidDynamicsApplication
         model_part= current_model.CreateModelPart("Main")
         model_part.AddNodalSolutionStepVariable(VELOCITY)
 
         model_part.CreateNewNode(1,0.0,0.0,0.0)
         model_part.SetBufferSize(3)
         model_part.CloneTimeStep(1.0)
-        #model_part.Nodes[1].SetSolutionStepValue(VELOCITY_X,0,1.0) #here i set VELOCITY_X to 1.0
+        model_part.Nodes[1].SetSolutionStepValue(VELOCITY_X,0,1.0) #here i set VELOCITY_X to 1.0
 
         #import aux_external_import
         self._aux_func(model_part) #here i set VELOCITY_Y to 2.0
 
-        #self.assertTrue(model_part.Nodes[1].GetSolutionStepValue(VELOCITY_X) == 1.0)
+        self.assertTrue(model_part.Nodes[1].GetSolutionStepValue(VELOCITY_X) == 1.0)
         self.assertTrue(model_part.Nodes[1].GetSolutionStepValue(VELOCITY_Y) == 2.0)
 
     def _aux_func(self,model_part):
         try:
-            import KratosMultiphysics.FluidDynamicsApplication #upon importing the key of velocity is changed, so that the database does not work any longer
+            import KratosMultiphysics.FluidDynamicsApplication
         except:
             self.skipTest("KratosMultiphysics.FluidDynamicsApplication is not available")
-        model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VELOCITY) #the problem is that here the key of VELOCITY is changed...
+        model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VELOCITY)
         model_part.Nodes[1].SetSolutionStepValue(VELOCITY_Y,0,2.0)
 
     def test_has_application(self):
@@ -50,7 +49,7 @@ class TestImporting(KratosUnittest.TestCase):
         self.assertTrue(Kernel().IsImported("ExternalSolversApplication"))
 
     def test_variable_keys_reordering(self):
-        key_vel_before_import = VELOCITY.Key()
+        key_vel_before_import = VELOCITY.Key() # test a var from the Kerne
 
         try:
             import KratosMultiphysics.FluidDynamicsApplication as KratosCFD
@@ -59,7 +58,7 @@ class TestImporting(KratosUnittest.TestCase):
 
         self.assertEqual(key_vel_before_import, VELOCITY.Key())
 
-        key_ssp_before_import = KratosCFD.SUBSCALE_PRESSURE.Key()
+        key_ssp_before_import = KratosCFD.SUBSCALE_PRESSURE.Key() # test an Application-Variable
 
         try:
             import KratosMultiphysics.StructuralMechanicsApplication
