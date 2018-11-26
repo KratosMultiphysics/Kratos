a9839110b0cace7595955c7f2081db4ee3e04f7b
Guillermo Casas <gcasas@cimne.upc.edu>
2018-11-26 16:37:57 +0100

    Adding analysis_stage

---
+++ applications/DEMApplication/python_scripts/KratosDEMAnalysis.py
@@ -0,0 +1,35 @@
+import KratosMultiphysics
+from KratosMultiphysics import Logger
+Logger.GetDefaultOutput().SetSeverity(Logger.Severity.INFO)
+import KratosMultiphysics.DEMApplication
+from DEM_analysis_stage import DEMAnalysisStage
+
+import time
+""" model = KratosMultiphysics.Model()
+solution = Main.Solution(model)
+solution.Run() """
+
+class DEMAnalysisStageWithFlush(DEMAnalysisStage):
+
+    def __init__(self, model, project_parameters, flush_frequency=10.0):
+        super(DEMAnalysisStageWithFlush,self).__init__(model, project_parameters)
+        self.flush_frequency = flush_frequency
+        self.last_flush = time.time()
+
+    def FinalizeSolutionStep(self):
+        super(DEMAnalysisStageWithFlush,self).FinalizeSolutionStep()
+
+        if self.parallel_type == "OpenMP":
+            now = time.time()
+            if now - self.last_flush > self.flush_frequency:
+                sys.stdout.flush()
+                self.last_flush = now
+
+if __name__ == "__main__":
+
+    with open("ProjectParametersDEM.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = DEMAnalysisStageWithFlush(model,parameters)
+    simulation.Run()
