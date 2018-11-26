7b4599bea2c5cecd35459121d2f4abc18306ba0d
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-11-26 12:25:17 +0100

    Validation contains all

--- applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
+++ applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
@@ -421,7 +421,8 @@ def AssembleTestSuites():
 
     # Create a test suit that contains all the tests:
     allSuite = suites['all']
-    allSuite.addTests(nightSuite) # already contains the smallSuite
+    allSuite.addTests(nightSuite) # Already contains the smallSuite
+    validationSuite.addTests(allSuite) # Validation contains all
 
     return suites
 
