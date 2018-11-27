0bed2b89fc50163c6ceff929628d2c3e9647aeba
RTosi <riccardo.tosi@hotmail.it>
2018-11-27 16:28:53 +0100

    Minor changes to try to debug

--- applications/MultilevelMonteCarloApplication/python_scripts/montecarlo_analysis_adaptive_refinement.py
+++ applications/MultilevelMonteCarloApplication/python_scripts/montecarlo_analysis_adaptive_refinement.py
@@ -76,7 +76,6 @@ class MonteCarloAnalysis(AnalysisStage):
             super(MonteCarloAnalysis,self).__init__(current_model,current_parameters)
             self._GetSolver().main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
             self._GetSolver().main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_H)
-            self._GetSolver().main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
 
         else:
             print("serializing model and parameters")
@@ -168,14 +167,37 @@ output:
         ExactExpectedValueQoI : Quantity of Interest for sample = 1.0
 OBSERVATION: here we multiply by 0.25 because it is the mean value of beta(2,6)
 '''
-@ExaquteTask(returns=2)
+@ExaquteTask(returns=1)
 def exact_execution_task(model, parameters):
     sample = 1.0
     simulation = MonteCarloAnalysis(model,parameters,sample)
     simulation.Run()
     QoI =  EvaluateQuantityOfInterest(simulation)
     ExactExpectedValueQoI = 0.25 * EvaluateQuantityOfInterest(simulation)
-    return simulation,ExactExpectedValueQoI
+    return ExactExpectedValueQoI
+
+
+'''
+function executing the problem
+input:
+        model       : serialization of the model
+        parameters  : serialization of the Project Parameters
+output:
+        QoI                        : Quantity of Interest
+        serialized_model_to_refine : model serialized
+'''
+@ExaquteTask(returns=2)
+def execution_task_for_refinement(model, parameters):
+    sample = GenerateBetaSample()
+    simulation = MonteCarloAnalysis(model,parameters,sample)
+    simulation.Run()
+    QoI =  EvaluateQuantityOfInterest(simulation)
+    '''serialize model to export from the task
+    observe only pickleable objects can be passed in compss'''
+    serialized_model_to_refine = KratosMultiphysics.StreamSerializer()
+    serialized_model_to_refine.Save("ModelSerialization",simulation.model)
+    return QoI,serialized_model_to_refine
+
 
 
 '''
@@ -243,20 +265,22 @@ if __name__ == '__main__':
     print("\n############## Serialization completed ##############\n")
 
     '''evaluate the exact expected value of Q (sample = 1.0)'''
-    simulation_0,ExactExpectedValueQoI = exact_execution_task(serialized_model_0,serialized_parameters_0)
+    ExactExpectedValueQoI,serialized_model_to_refine = execution_task_for_refinement(serialized_model_0,serialized_parameters_0)
 
     '''compute the refinement of the mesh and build the correspondent serialized parameters and model'''
-    serialized_model_1,serialized_parameters_1 = refinement.compute_refinement(simulation_0,0.1,0.2,serialized_parameters_0)
+    serialized_model_1,serialized_parameters_1 = refinement.compute_refinement(serialized_model_to_refine,0.2,0.3,serialized_parameters_0,"MLMCLaplacianModelPart")
 
+    stop
+    
     '''evaluate the exact expected value of Q (sample = 1.0)'''
-    simulation_1,ExactExpectedValueQoI = exact_execution_task(serialized_model_1,serialized_parameters_1)
+    simulation_1 = exact_execution_task(serialized_model_1,serialized_parameters_1)
 
 
     number_samples = 10
-    Qlist = []
+    simulation_list = []
 
     for instance in range (0,number_samples):
-        Qlist.append(execution_task(serialized_model_1,serialized_parameters_1))
+        simulation_list.append(execution_task(serialized_model_1,serialized_parameters_1))
         
 
     '''Compute mean, second moment and sample variance'''
@@ -264,14 +288,14 @@ if __name__ == '__main__':
     MC_second_moment = 0.0
     for i in range (0,number_samples):
         nsam = i+1
-        MC_mean, MC_second_moment, MC_variance = mc.update_onepass_M_VAR(Qlist[i], MC_mean, MC_second_moment, nsam)
+        MC_mean, MC_second_moment, MC_variance = mc.update_onepass_M_VAR(simulation_list[i].QoI, MC_mean, MC_second_moment, nsam)
     '''Evaluation of the relative error between the computed mean value and the expected value of the QoI'''
-    relative_error = compare_mean(MC_mean,ExactExpectedValueQoI)
-    # print("Values QoI:",Qlist)
+    relative_error = compare_mean(MC_mean,simulation_1.QoI)
+    # print("Values QoI:",simulation_list.QoI)
     MC_mean = compss_wait_on(MC_mean)
-    ExactExpectedValueQoI = compss_wait_on(ExactExpectedValueQoI)
+    ExactExpectedValueQoI = compss_wait_on(simulation_1.QoI)
     relative_error = compss_wait_on(relative_error)
-    print("\nMC mean = ",MC_mean,"exact mean = ",ExactExpectedValueQoI)
+    print("\nMC mean = ",MC_mean,"exact mean = ",simulation_1.QoI)
     print("relative error: ",relative_error)
 
 
