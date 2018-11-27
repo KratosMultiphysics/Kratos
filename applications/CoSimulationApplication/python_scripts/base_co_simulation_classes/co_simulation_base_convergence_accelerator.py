0f6dbf2414bb8518ab26b9a192f875e0f431c63d
AdityaGhantasala <shine.aditya@gmail.com>
2018-11-27 16:38:36 +0100

    using cosim analysis from the app

--- applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_convergence_accelerator.py
+++ applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_convergence_accelerator.py
@@ -31,57 +31,57 @@ class CoSimulationBaseConvergenceAccelerator(object):
 
     ## InitializeSolutionStep : Called once in the beginning of the solution step
     #
-    #  @param self            The object pointer.
+
     def InitializeSolutionStep(self):
         pass
 
     ## FinalizeSolutionStep : Called once at the end of the solution step
     #
-    #  @param self            The object pointer.
+
     def FinalizeSolutionStep(self):
         pass
 
     ## FinalizeNonLinearIteration : Function initializes the non linear iteration (coupling iteration)
     #                               Called at the beginning of the nonlinear iteration (coupling iteration)
     #
-    #  @param self            The object pointer.
+
     def InitializeNonLinearIteration(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseConvergenceAccelerator : Calling InitializeNonLinearIteration function !" + tools.bcolors.ENDC)
 
     ## FinalizeNonLinearIteration : Function finalizes the non linear iteration (coupling iteration)
     #                               Called at the end of the nonlinear iteration (coupling iteration)
     #
-    #  @param self            The object pointer.
+
     def FinalizeNonLinearIteration(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseConvergenceAccelerator : Calling FinalizeNonLinearIteration function !" + tools.bcolors.ENDC)
 
     ## ComputeUpdate : Function to compute the update for the fields. Should be called during the nonlinear (coupling iteration).
     #
-    #  @param self            The object pointer.
+
     def ComputeUpdate(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseConvergenceAccelerator : Calling ComputeUpdate function !" + tools.bcolors.ENDC)
 
     ## PrintInfo : Function to print the information of the convergence accelerator
     #
-    #  @param self            The object pointer.
+
     def PrintInfo(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseConvergenceAccelerator : Calling PrintInfo function !" + tools.bcolors.ENDC)
 
     ## SetEchoLevel : Function to set the echo_level of the convergence accelerator
     #
-    #  @param self            The object pointer.
+
     #  @param level           int : echo level to be set
     def SetEchoLevel(self, level):
         self.echo_level = level
 
     ## Check : Function to Check the setup of the convergence accelerator
     #
-    #  @param self            The object pointer.
+
     def Check(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseConvergenceAccelerator : Calling Check function !" + tools.bcolors.ENDC)
 
     ## _Name : Function to get the name of the convergence accelerator
     #
-    #  @param self            The object pointer.
+
     def _Name(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseConvergenceAccelerator : Calling _Name function !" + tools.bcolors.ENDC)
---
+++ applications/CoSimulationApplication/test_examples/01_test_one/MainKratos.py
@@ -0,0 +1,19 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+import json
+import KratosMultiphysics
+from KratosMultiphysics import *
+import KratosMultiphysics.CoSimulationApplication as CoSimulationApplication
+from __init__ import *
+from co_simulation_analysis import CoSimulationAnalysis
+
+filename = 'test_json.json'
+
+#Read JSON data into the datastore variable
+if filename:
+    with open(filename, 'r') as f:
+        json_data = json.load(f)
+
+
+cs = CoSimulationAnalysis(json_data)
+cs.PrintInfo()
+cs.Run()
