91479e1e3a8374383c57b638ce1c3eb4488fac1c
AdityaGhantasala <shine.aditya@gmail.com>
2018-11-27 15:27:30 +0100

    adding base extrapolator

---
+++ applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_extrapolator.py
@@ -0,0 +1,86 @@
+from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+
+# Other imports
+import co_simulation_tools as tools
+
+
+##
+#  IMPORTANT : This is a BASE CLASS
+#               Please do not change any thing in this class.
+#
+# This Class servers as a base class for all the Convergence  extrapolator techniques
+# used in co simulation.
+class CoSimulationBaseExtrapolator(object):
+    def __init__(self, settings, solver):
+        self.solver = solver
+        self.settings = settings
+        self.echo_level = 0
+
+    ## Initialize : Initialize function for the  extrapolator class. Necessary
+    #               initialization of the variables and objects to be done here.
+    #  @param self                      The object pointer.
+    def Initialize(self):
+        pass
+
+    ## Finalize :  Initialize function for the  extrapolator class.
+    #               finalization of the variables and objects to be done here.
+    #  @param self                      The object pointer.
+    def Finalize(self):
+        pass
+
+    ## InitializeSolutionStep : Called once in the beginning of the solution step
+    #
+    #  @param self            The object pointer.
+    def InitializeSolutionStep(self):
+        pass
+
+    ## FinalizeSolutionStep : Called once at the end of the solution step
+    #
+    #  @param self            The object pointer.
+    def FinalizeSolutionStep(self):
+        pass
+
+    ## FinalizeNonLinearIteration : Function initializes the non linear iteration (coupling iteration)
+    #                               Called at the beginning of the nonlinear iteration (coupling iteration)
+    #
+    #  @param self            The object pointer.
+    def InitializeNonLinearIteration(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling InitializeNonLinearIteration function !" + tools.bcolors.ENDC)
+
+    ## FinalizeNonLinearIteration : Function finalizes the non linear iteration (coupling iteration)
+    #                               Called at the end of the nonlinear iteration (coupling iteration)
+    #
+    #  @param self            The object pointer.
+    def FinalizeNonLinearIteration(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling FinalizeNonLinearIteration function !" + tools.bcolors.ENDC)
+
+    ## ComputeUpdate : Function to compute the update for the fields. Should be called during the nonlinear (coupling iteration).
+    #
+    #  @param self            The object pointer.
+    def ComputeUpdate(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling ComputeUpdate function !" + tools.bcolors.ENDC)
+
+    ## PrintInfo : Function to print the information of the   extrapolator
+    #
+    #  @param self            The object pointer.
+    def PrintInfo(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling PrintInfo function !" + tools.bcolors.ENDC)
+
+    ## SetEchoLevel : Function to set the echo_level of the   extrapolator
+    #
+    #  @param self            The object pointer.
+    #  @param level           int : echo level to be set
+    def SetEchoLevel(self, level):
+        self.echo_level = level
+
+    ## Check : Function to Check the setup of the   extrapolator
+    #
+    #  @param self            The object pointer.
+    def Check(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling Check function !" + tools.bcolors.ENDC)
+
+    ## _Name : Function to get the name of the   extrapolator
+    #
+    #  @param self            The object pointer.
+    def _Name(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling _Name function !" + tools.bcolors.ENDC)
