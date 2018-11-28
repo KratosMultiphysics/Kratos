fe4303c43ef443590fb055ddebc3a9de06861ea5
Simon v. Wenczowski <simon.wenczowski@outlook.de>
2018-11-28 17:50:06 +0100

    Combining serial and MPI version of the process

--- applications/FluidDynamicsApplication/python_scripts/apply_mass_conservation_check_process.py
+++ applications/FluidDynamicsApplication/python_scripts/apply_mass_conservation_check_process.py
@@ -1,16 +1,21 @@
+from __future__ import absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import math
 import KratosMultiphysics
 import KratosMultiphysics.FluidDynamicsApplication as KratosFluid
 
 def Factory(settings, Model):
     if( not isinstance(settings, KratosMultiphysics.Parameters) ):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
-    return ApplyMassConservationCheckProcess(Model, settings["Parameters"])
+    return ApplyMassConservationCheckProcess( Model, settings["Parameters"] )
 
 class ApplyMassConservationCheckProcess(KratosMultiphysics.Process):
 
     def __init__(self, Model, settings):
 
         KratosMultiphysics.Process.__init__(self)
+
+        self._my_log_file = "Mass_Conservation.log"
 
         default_parameters = KratosMultiphysics.Parameters( """
         {
@@ -22,35 +27,56 @@ class ApplyMassConservationCheckProcess(KratosMultiphysics.Process):
 
         settings.ValidateAndAssignDefaults(default_parameters)
 
-        self.fluid_model_part = Model[settings["model_part_name"].GetString()]
-        self.write_to_log = settings["write_to_log_file"].GetBool()
-        self.mass_conservation_check_process = KratosFluid.MassConservationCheckProcess(self.fluid_model_part, settings)
+        self._fluid_model_part = Model[settings["model_part_name"].GetString()]
+        self._write_to_log = settings["write_to_log_file"].GetBool()
+        self._is_printing_rank = ( self._fluid_model_part.GetCommunicator().MyPID() == 0 )
+
+        self.mass_conservation_check_process = KratosFluid.MassConservationCheckProcess(self._fluid_model_part, settings)
 
         # writing first line in file
-        if ( self.write_to_log ):
-            with open("ApplyMassConservationCheckProcess.log", "w") as logFile:
+        if ( self._write_to_log and self._is_printing_rank ):
+            with open(self._my_log_file, "w") as logFile:
                 logFile.write( "positiveVolume" + "\t" + "negativeVolume" + "\n" )
+                logFile.close()
+
+        if self._is_printing_rank:
+            KratosMultiphysics.Logger.PrintInfo("ApplyMassConservationCheckProcess","Construction of Mass Conservation Check Process finished.")
+
 
     def Execute(self):
 
-        # retrieve information if the values were updated
-        updated = self.mass_conservation_check_process.GetUpdateStatus()
+        updated = int( self.mass_conservation_check_process.GetUpdateStatus() )
 
-        if ( updated ):
+        # force a pause until the last process has its status
+        self._fluid_model_part.GetCommunicator().Barrier()
+
+        # would take the value "0" if one is not updated
+        updated = self._fluid_model_part.GetCommunicator().MinAll( updated )
+
+        if ( updated > 0 ):
+
             posVol = self.mass_conservation_check_process.GetPositiveVolume()
             negVol = self.mass_conservation_check_process.GetNegativeVolume()
             initPosVol = self.mass_conservation_check_process.GetInitialPositiveVolume()
             initNegVol = self.mass_conservation_check_process.GetInitialNegativeVolume()
 
-            # managing the output to the console
-            KratosMultiphysics.Logger.PrintInfo("ApplyMassConservationCheckProcess", "Positive Volume = " + str(posVol) + "  ( initially " + str(initPosVol) + ")" )
-            KratosMultiphysics.Logger.PrintInfo("ApplyMassConservationCheckProcess", "Negative Volume = " + str(negVol) + "  ( initially " + str(initNegVol) + ")" )
-            KratosMultiphysics.Logger.Flush()
+            self._fluid_model_part.GetCommunicator().Barrier()
+            posVol = self._fluid_model_part.GetCommunicator().SumAll( posVol )
+            negVol = self._fluid_model_part.GetCommunicator().SumAll( negVol )
+            initPosVol = self._fluid_model_part.GetCommunicator().SumAll( initPosVol )
+            initNegVol = self._fluid_model_part.GetCommunicator().SumAll( initNegVol )
 
-            # adds additional lines to the log file
-            if ( self.write_to_log ):
-                with open("ApplyMassConservationCheckProcess.log", "a+") as logFile:
-                    logFile.write( str(posVol) + "\t" + str(negVol) + "\n" )
+            # find the printing rank
+            if ( self._fluid_model_part.GetCommunicator().MyPID() == 0 ):
+                # managing the output to the console
+                KratosMultiphysics.Logger.PrintInfo("ApplyMassConservationCheckProcess", "Positive Volume = " + str(posVol) + "  ( initially " + str(initPosVol) + ")" )
+                KratosMultiphysics.Logger.PrintInfo("ApplyMassConservationCheckProcess", "Negative Volume = " + str(negVol) + "  ( initially " + str(initNegVol) + ")" )
+                KratosMultiphysics.Logger.Flush()
+
+                # adds additional lines to the log file
+                if ( self._write_to_log ):
+                    with open(self._my_log_file, "a+") as logFile:
+                        logFile.write( str(posVol) + "\t" + str(negVol) + "\n" )
 
 
     def ExecuteInitialize(self):
