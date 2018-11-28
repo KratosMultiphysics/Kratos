74d1f5a79c20f3fdab73a644776405e893e75e9d
Simon v. Wenczowski <simon.wenczowski@outlook.de>
2018-11-28 14:24:15 +0100

    Adding the MPI variant of the TwoFluid solver to wrapper

--- applications/FluidDynamicsApplication/python_scripts/python_solvers_wrapper_fluid.py
+++ applications/FluidDynamicsApplication/python_scripts/python_solvers_wrapper_fluid.py
@@ -44,6 +44,9 @@ def CreateSolverByParameters(model, solver_settings, parallelism):
         elif (solver_type == "Embedded"):
             solver_module_name = "trilinos_navier_stokes_embedded_solver"
 
+        elif (solver_type == "TwoFluids"):
+            solver_module_name = "trilinos_navier_stokes_two_fluid_solver"
+
         else:
             raise Exception("the requested solver type is not in the python solvers wrapper. Solver type is : " + solver_type)
 
