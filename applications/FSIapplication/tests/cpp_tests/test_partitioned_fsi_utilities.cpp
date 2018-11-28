60d240f3c1f01c1f630bdd771213fddd2f12892c
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2018-11-28 15:48:36 +0100

    Compute consistent residual test addition

--- applications/FSIapplication/tests/cpp_tests/test_partitioned_fsi_utilities.cpp
+++ applications/FSIapplication/tests/cpp_tests/test_partitioned_fsi_utilities.cpp
@@ -28,6 +28,23 @@
 namespace Kratos {
 namespace Testing {
 
+    void GenerateTestSkinModelPart(ModelPart &rTestSkinModelPart)
+    {
+        rTestSkinModelPart.SetBufferSize(1);
+        Properties::Pointer p_properties_1(new Properties(1));
+        rTestSkinModelPart.AddProperties(p_properties_1);
+        rTestSkinModelPart.CreateNewNode(1, 0.0, 0.0, 0.0);
+        rTestSkinModelPart.CreateNewNode(2, 1.0, 0.0, 0.0);
+        rTestSkinModelPart.CreateNewNode(3, 1.0, 1.0, 0.0);
+        rTestSkinModelPart.CreateNewNode(4, 0.0, 1.0, 0.0);
+        std::vector<ModelPart::IndexType> cond_nodes_1 = {1,2};
+        std::vector<ModelPart::IndexType> cond_nodes_2 = {2,3};
+        std::vector<ModelPart::IndexType> cond_nodes_3 = {3,4};
+        rTestSkinModelPart.CreateNewCondition("Condition2D2N", 1, cond_nodes_1, p_properties_1);
+        rTestSkinModelPart.CreateNewCondition("Condition2D2N", 2, cond_nodes_2, p_properties_1);
+        rTestSkinModelPart.CreateNewCondition("Condition2D2N", 3, cond_nodes_3, p_properties_1);
+    }
+
     KRATOS_TEST_CASE_IN_SUITE(PartitionedFSIUtilitiesCopySkinToElements, FSIApplicationFastSuite)
     {
         // Set the partitioned FSI utilities
@@ -37,19 +54,7 @@ namespace Testing {
         // Set the model part that contains the condition based skin
         Model model;
         ModelPart &main_model_part = model.CreateModelPart("OriginModelPart");
-        main_model_part.SetBufferSize(1);
-        Properties::Pointer p_properties_1(new Properties(1));
-        main_model_part.AddProperties(p_properties_1);
-        main_model_part.CreateNewNode(1, 0.0, 0.0, 0.0);
-        main_model_part.CreateNewNode(2, 1.0, 0.0, 0.0);
-        main_model_part.CreateNewNode(3, 1.0, 1.0, 0.0);
-        main_model_part.CreateNewNode(4, 0.0, 1.0, 0.0);
-        std::vector<ModelPart::IndexType> cond_nodes_1 = {1,2};
-        std::vector<ModelPart::IndexType> cond_nodes_2 = {2,3};
-        std::vector<ModelPart::IndexType> cond_nodes_3 = {3,4};
-        main_model_part.CreateNewCondition("Condition2D2N", 1, cond_nodes_1, p_properties_1);
-        main_model_part.CreateNewCondition("Condition2D2N", 2, cond_nodes_2, p_properties_1);
-        main_model_part.CreateNewCondition("Condition2D2N", 3, cond_nodes_3, p_properties_1);
+        GenerateTestSkinModelPart(main_model_part);
 
         // Set the new skin model part
         ModelPart &element_based_skin = model.CreateModelPart("ElementBasedSkin");
@@ -59,6 +64,44 @@ namespace Testing {
         KRATOS_CHECK_EQUAL(element_based_skin.NumberOfNodes(), 4);
         KRATOS_CHECK_EQUAL(element_based_skin.NumberOfElements(), 3);
         KRATOS_CHECK_EQUAL(element_based_skin.NumberOfConditions(), 0);
+    }
+
+    KRATOS_TEST_CASE_IN_SUITE(PartitionedFSIUtilitiesComputeConsistentResidual, FSIApplicationFastSuite)
+    {
+        // Set the partitioned FSI utilities
+        typedef UblasSpace<double, Matrix, Vector > TSpace;
+        PartitionedFSIUtilities<TSpace, 2> partitioned_fsi_utilities;
+
+        // Set the model part in where the error is to be computed
+        Model model;
+        ModelPart &main_model_part = model.CreateModelPart("OriginModelPart");
+        main_model_part.AddNodalSolutionStepVariable(VELOCITY);
+        main_model_part.AddNodalSolutionStepVariable(VECTOR_PROJECTED);
+        main_model_part.AddNodalSolutionStepVariable(FSI_INTERFACE_RESIDUAL);
+        GenerateTestSkinModelPart(main_model_part);
+
+        // Set a fake field to compute the error
+        array_1d<double,3> unit_val;
+        unit_val[0] = 1.0;
+        unit_val[1] = 1.0;
+        unit_val[2] = 1.0;
+        for (auto &it_node : main_model_part.Nodes()) {
+            it_node.FastGetSolutionStepValue(VELOCITY) = unit_val;
+            it_node.FastGetSolutionStepValue(VECTOR_PROJECTED) = 2.0 * unit_val;
+        }
+        
+        // Compute the consistent residual vector
+        partitioned_fsi_utilities.ComputeConsistentResidual(
+            main_model_part,
+            VECTOR_PROJECTED,
+            VELOCITY,
+            FSI_INTERFACE_RESIDUAL);
+
+        KRATOS_WATCH(main_model_part.GetNode(1).FastGetSolutionStepValue(FSI_INTERFACE_RESIDUAL));
+        KRATOS_WATCH(main_model_part.GetNode(2).FastGetSolutionStepValue(FSI_INTERFACE_RESIDUAL));
+        KRATOS_WATCH(main_model_part.GetNode(3).FastGetSolutionStepValue(FSI_INTERFACE_RESIDUAL));
+        KRATOS_WATCH(main_model_part.GetNode(4).FastGetSolutionStepValue(FSI_INTERFACE_RESIDUAL));
+
     }
 
 } // namespace Testing
