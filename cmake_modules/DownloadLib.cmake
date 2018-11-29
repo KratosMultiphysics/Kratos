791cbd4a8ae866c0f8519c39dacdddbb51fb9faa
pooyan <pooyan@altair.com>
2018-11-29 15:35:35 +0100

    changing CMAKE_SOURCE_DIR to KRATOS_SOURCE_DIR

--- cmake_modules/DownloadLib.cmake
+++ cmake_modules/DownloadLib.cmake
@@ -5,10 +5,10 @@ set(__DOWNLIB_INCLUDED TRUE)
 
 function (DownloadLib _name _src)
     message("-- ${_name} not found, trying to download from ${_src}")
-    file(DOWNLOAD "${_src}" "${CMAKE_SOURCE_DIR}/external_libraries/${_name}.zip")
+    file(DOWNLOAD "${_src}" "${KRATOS_SOURCE_DIR}/external_libraries/${_name}.zip")
     execute_process(
         COMMAND ${CMAKE_COMMAND} 
-        -E tar xzf "${CMAKE_SOURCE_DIR}/external_libraries/${_name}.zip"
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/external_libraries")
-    message("-- Finish unpacking: ${CMAKE_SOURCE_DIR}/external_libraries/${_name} ")
+        -E tar xzf "${KRATOS_SOURCE_DIR}/external_libraries/${_name}.zip"
+        WORKING_DIRECTORY "${KRATOS_SOURCE_DIR}/external_libraries")
+    message("-- Finish unpacking: ${KRATOS_SOURCE_DIR}/external_libraries/${_name} ")
 endfunction(DownloadLib)
