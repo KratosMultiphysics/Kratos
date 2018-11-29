a5385b006763d72f0fbc9366ae3a1fd0cb1c5632
pooyan <pooyan@altair.com>
2018-11-29 11:39:32 +0100

    Moving KratosLoader to python interface folder

---
+++ kratos/python_interface/KratosLoader.py
@@ -0,0 +1,9 @@
+import os.path
+kratos_libs=os.path.abspath(os.path.join(os.path.dirname(__file__),'../libs'))
+kratos_applications=os.path.abspath(os.path.join(os.path.dirname(__file__),'../applications'))
+kratos_scripts=os.path.abspath(os.path.join(os.path.dirname(__file__),'../kratos/python_scripts'))
+kratos_tests=os.path.abspath(os.path.join(os.path.dirname(__file__),'../kratos/tests'))
+sys.path.append(kratos_libs)
+sys.path.append(kratos_applications)
+sys.path.append(kratos_scripts)
+sys.path.append(kratos_tests)
