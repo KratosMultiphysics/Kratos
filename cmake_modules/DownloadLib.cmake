e71f26922c1d4ed63a9d46d18834620dd763dfa8
pooyan <pooyan@altair.com>
2018-11-29 11:07:03 +0100

    Adding DownloadLib.cmake

---
+++ cmake_modules/DownloadLib.cmake
@@ -0,0 +1,15 @@
+if(__DOWNLIB_INCLUDED)
+    return()
+endif()
+set(__DOWNLIB_INCLUDED TRUE)
+
+function (DownloadLib _name _src)
+    message("-- ${_name} not found, trying to download from ${_src}")
+    # file(DOWNLOAD "${_src}" "${CMAKE_SOURCE_DIR}/external_libraries/${_name}.zip")
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} 
+        -E tar xzf "${CMAKE_SOURCE_DIR}/external_libraries/${_name}.zip"
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/external_libraries)"
+    message("-- Finish unpacking: ${CMAKE_SOURCE_DIR}/external_libraries/${_name} ")
+    )
+endfunction(DownloadLib)
