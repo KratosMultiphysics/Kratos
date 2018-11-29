69c4359955fbda88b0c60d12aff67784ecfa75df
AdityaGhantasala <shine.aditya@gmail.com>
2018-11-29 10:56:31 +0100

    name and output correction in predictor base

--- applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_predictor.py
+++ applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_predictor.py
@@ -10,7 +10,7 @@ import co_simulation_tools as tools
 #
 # This Class servers as a base class for all the Convergence  extrapolator techniques
 # used in co simulation.
-class CoSimulationBaseExtrapolator(object):
+class CoSimulationBasePredictor(object):
     def __init__(self, settings, solver):
         self.solver = solver
         self.settings = settings
@@ -45,26 +45,26 @@ class CoSimulationBaseExtrapolator(object):
     #
     #  @param self            The object pointer.
     def InitializeNonLinearIteration(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling InitializeNonLinearIteration function !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBasePredictor : Calling InitializeNonLinearIteration function !" + tools.bcolors.ENDC)
 
     ## FinalizeNonLinearIteration : Function finalizes the non linear iteration (coupling iteration)
     #                               Called at the end of the nonlinear iteration (coupling iteration)
     #
     #  @param self            The object pointer.
     def FinalizeNonLinearIteration(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling FinalizeNonLinearIteration function !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBasePredictor : Calling FinalizeNonLinearIteration function !" + tools.bcolors.ENDC)
 
     ## ComputeUpdate : Function to compute the update for the fields. Should be called during the nonlinear (coupling iteration).
     #
     #  @param self            The object pointer.
     def ComputeUpdate(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling ComputeUpdate function !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBasePredictor : Calling ComputeUpdate function !" + tools.bcolors.ENDC)
 
     ## PrintInfo : Function to print the information of the   extrapolator
     #
     #  @param self            The object pointer.
     def PrintInfo(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling PrintInfo function !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBasePredictor : Calling PrintInfo function !" + tools.bcolors.ENDC)
 
     ## SetEchoLevel : Function to set the echo_level of the   extrapolator
     #
@@ -77,10 +77,10 @@ class CoSimulationBaseExtrapolator(object):
     #
     #  @param self            The object pointer.
     def Check(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling Check function !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBasePredictor : Calling Check function !" + tools.bcolors.ENDC)
 
     ## _Name : Function to get the name of the   extrapolator
     #
     #  @param self            The object pointer.
     def _Name(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseExtrapolator : Calling _Name function !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBasePredictor : Calling _Name function !" + tools.bcolors.ENDC)
