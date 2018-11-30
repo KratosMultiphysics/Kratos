64733fef31a2f6a9bc9d406406014179cc29aa19
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-11-30 12:10:55 +0100

    Minor correction

--- applications/StructuralMechanicsApplication/custom_constitutive/elastic_isotropic_3d.cpp
+++ applications/StructuralMechanicsApplication/custom_constitutive/elastic_isotropic_3d.cpp
@@ -59,24 +59,21 @@ void  ElasticIsotropic3D::CalculateMaterialResponsePK2(ConstitutiveLaw::Paramete
 {
     KRATOS_TRY;
     // b.- Get Values to compute the constitutive law:
-    Flags & r_options=rValues.GetOptions();
+    Flags & r_constitutive_law_options = rValues.GetOptions();
 
     Vector& r_strain_vector = rValues.GetStrainVector();
 
     //NOTE: SINCE THE ELEMENT IS IN SMALL STRAINS WE CAN USE ANY STRAIN MEASURE. HERE EMPLOYING THE CAUCHY_GREEN
-    if( r_options.IsNot( ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN )) {
+    if( r_constitutive_law_options.IsNot( ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN )) {
         CalculateCauchyGreenStrain( rValues, r_strain_vector);
     }
 
-    if( r_options.Is( ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR ) ) {
-        Matrix& r_constitutive_matrix = rValues.GetConstitutiveMatrix();
-        CalculateElasticMatrix( r_constitutive_matrix, rValues);
-    }
-
-    if( r_options.Is( ConstitutiveLaw::COMPUTE_STRESS ) ) {
+    if( r_constitutive_law_options.Is( ConstitutiveLaw::COMPUTE_STRESS ) ||
+        r_constitutive_law_options.Is( ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR )) {
         Vector& r_stress_vector = rValues.GetStressVector();
-        if( r_options.Is( ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR ) ) {
+        if( r_constitutive_law_options.Is( ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR ) ) {
             Matrix& r_constitutive_matrix = rValues.GetConstitutiveMatrix();
+            CalculateElasticMatrix( r_constitutive_matrix, rValues);
             noalias(r_stress_vector) = prod( r_constitutive_matrix, r_strain_vector);
         } else {
             CalculatePK2Stress( r_strain_vector, r_stress_vector, rValues);
