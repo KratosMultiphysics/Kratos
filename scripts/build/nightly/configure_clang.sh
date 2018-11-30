4d9b54d69794ee30f3d368856f6c77d61487b1db
philbucher <philipp.bucher@tum.de>
2018-11-30 09:10:39 +0100

    adding ANurbs-root

--- scripts/build/nightly/configure_clang.sh
+++ scripts/build/nightly/configure_clang.sh
@@ -46,6 +46,7 @@ cmake .. \
 -DCONSTITUTIVE_MODELS_APPLICATION=ON                                                            \
 -DSTRUCTURAL_MECHANICS_APPLICATION=ON                                                           \
 -DIGA_APPLICATION=ON                                                                            \
+-DANURBS_ROOT="${HOME}/ANurbs"                                                                  \
 -DSWIMMING_DEM_APPLICATION=OFF                                                                  \
 -DTHERMO_MECHANICAL_APPLICATION=ON                                                              \
 -DCONTACT_STRUCTURAL_MECHANICS_APPLICATION=ON                                                   \
