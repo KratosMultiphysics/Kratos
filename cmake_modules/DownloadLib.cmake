4df254ed19f8bef1bbea4e0bdcc101589a0ef74d
pooyan <pooyan@altair.com>
2018-12-03 19:13:30 +0100

    Check if zip file downloaded before downloading it

--- cmake_modules/DownloadLib.cmake
+++ cmake_modules/DownloadLib.cmake
@@ -4,8 +4,14 @@ endif()
 set(__DOWNLIB_INCLUDED TRUE)
 
 function (DownloadLib _name _src)
-    message("-- ${_name} not found, trying to download from ${_src}")
+message("-- ${_name} not found, trying to download from ${_src}")
+if(EXISTS "${KRATOS_SOURCE_DIR}/external_libraries/${_name}.zip")
+    message("-- ${_name}.zip found. unpacking...")
+else()
+    message("-- ${_name}.zip not found, trying to download it from ${_src}")
     file(DOWNLOAD "${_src}" "${KRATOS_SOURCE_DIR}/external_libraries/${_name}.zip")
+    message("-- ${_name}.zip downloaded. unpacking...")
+endif()
     execute_process(
         COMMAND ${CMAKE_COMMAND} 
         -E tar xzf "${KRATOS_SOURCE_DIR}/external_libraries/${_name}.zip"
