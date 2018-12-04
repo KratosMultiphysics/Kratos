a8d3f94c3b9f2fa744bfc20554d24972c893fe7e
philbucher <philipp.bucher@tum.de>
2018-12-04 12:39:48 +0100

    Revert "make output of debug-modelpart depending on verbosity"
    This reverts commit 99047e78419706fd72697697607dfd154b003783.

--- applications/metis_application/custom_processes/metis_divide_heterogeneous_input_in_memory_process.h
+++ applications/metis_application/custom_processes/metis_divide_heterogeneous_input_in_memory_process.h
@@ -324,10 +324,11 @@ public:
             streams[mpi_rank]->write(mpi_recv_buffer[mpi_rank], msgRecvSize[mpi_rank]);
         }
 
-        if (mVerbosity > 1) {
-            std::ofstream debug_ofstream("MetisDivideHeterogeneousInputInMemoryProcess_debug_modelpart_"+std::to_string(mpi_rank)+".mdpa");
-            debug_ofstream << stringbufs[mpi_rank].str() << std::endl;
-        }
+#ifdef KRATOS_DEBUG
+        // Print the partitions in debug mode
+        std::ofstream debug_ofstream("debug_modelpart_"+std::to_string(mpi_rank)+".mdpa");
+        debug_ofstream << stringbufs[mpi_rank].str() << std::endl;
+#endif
 
         // TODO: Try to come up with a better way to change the buffer.
         mrSerialIO.SwapStreamSource(streams[mpi_rank]);
