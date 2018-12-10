6e9c8defee38ae81ac9470e1080db7fe10208d37
philbucher <philipp.bucher@tum.de>
2018-12-10 20:07:50 +0100

    moving functionto cpp

--- applications/MeshMovingApplication/custom_utilities/mesh_velocity_calculation.cpp
+++ applications/MeshMovingApplication/custom_utilities/mesh_velocity_calculation.cpp
@@ -21,6 +21,38 @@
 namespace Kratos {
 namespace MeshVelocityCalculation {
 
+namespace { // helpers-namespace
+void CalculateMeshVelocitiesGeneralizedAlpha(ModelPart& rModelPart,
+                                             const double Beta,
+                                             const double Gamma)
+{
+    const int num_local_nodes = rModelPart.GetCommunicator().LocalMesh().NumberOfNodes();
+    const auto nodes_begin = rModelPart.GetCommunicator().LocalMesh().NodesBegin();
+    const double delta_time = rModelPart.GetProcessInfo()[DELTA_TIME];
+    const double const_u = Gamma / (delta_time * Beta);
+    const double const_v = 1.0 - Gamma / Beta;
+    const double const_a = delta_time * (1.0 - Gamma / (2.0 * Beta));
+
+    #pragma omp parallel for
+    for (int i=0; i<num_local_nodes; i++) {
+        const auto it_node  = nodes_begin + i;
+        const auto& r_mesh_u0 = it_node->FastGetSolutionStepValue(MESH_DISPLACEMENT);
+        auto&       r_mesh_v0 = it_node->FastGetSolutionStepValue(MESH_VELOCITY);
+        auto&       r_mesh_a0 = it_node->FastGetSolutionStepValue(MESH_ACCELERATION);
+
+        const auto& r_mesh_u1 = it_node->FastGetSolutionStepValue(MESH_DISPLACEMENT, 1);
+        const auto& r_mesh_v1 = it_node->FastGetSolutionStepValue(MESH_VELOCITY,     1);
+        const auto& r_mesh_a1 = it_node->FastGetSolutionStepValue(MESH_ACCELERATION, 1);
+
+        r_mesh_v0 = const_u * (r_mesh_u0 - r_mesh_u1) + const_v * r_mesh_v1 + const_a * r_mesh_a1;
+        r_mesh_a0 = (1.0 / (delta_time * Gamma)) * (r_mesh_v0 - r_mesh_v1) - ((1 - Gamma) / Gamma) * r_mesh_a1;
+    }
+
+    rModelPart.GetCommunicator().SynchronizeVariable(MESH_VELOCITY);
+    rModelPart.GetCommunicator().SynchronizeVariable(MESH_ACCELERATION);
+}
+}// helpers-namespace
+
 void CalculateMeshVelocities(ModelPart& rModelPart,
                              const TimeDiscretization::BDF1& rBDF)
 {
@@ -65,6 +97,30 @@ void CalculateMeshVelocities(ModelPart& rModelPart,
     }
 
     rModelPart.GetCommunicator().SynchronizeVariable(MESH_VELOCITY);
+}
+
+void CalculateMeshVelocities(ModelPart& rModelPart,
+                             const TimeDiscretization::Newmark& rGenAlpha)
+{
+    CalculateMeshVelocitiesGeneralizedAlpha(rModelPart,
+                                            rGenAlpha.GetBeta(),
+                                            rGenAlpha.GetGamma());
+}
+
+void CalculateMeshVelocities(ModelPart& rModelPart,
+                             const TimeDiscretization::Bossak& rGenAlpha)
+{
+    CalculateMeshVelocitiesGeneralizedAlpha(rModelPart,
+                                            rGenAlpha.GetBeta(),
+                                            rGenAlpha.GetGamma());
+}
+
+void CalculateMeshVelocities(ModelPart& rModelPart,
+                             const TimeDiscretization::GeneralizedAlpha& rGenAlpha)
+{
+    CalculateMeshVelocitiesGeneralizedAlpha(rModelPart,
+                                            rGenAlpha.GetBeta(),
+                                            rGenAlpha.GetGamma());
 }
 
 } // namespace MeshVelocityCalculation.
--- applications/MeshMovingApplication/custom_utilities/mesh_velocity_calculation.h
+++ applications/MeshMovingApplication/custom_utilities/mesh_velocity_calculation.h
@@ -27,62 +27,20 @@
 namespace Kratos {
 namespace MeshVelocityCalculation {
 
-template<class TGenAlpha>
-void CalculateMeshVelocitiesGeneralizedAlpha(ModelPart& rModelPart,
-                                             const TGenAlpha& rGenAlphaHelper)
-{
-    const double beta = rGenAlphaHelper.GetBeta();
-    const double gamma = rGenAlphaHelper.GetGamma();
-    const int num_local_nodes = rModelPart.GetCommunicator().LocalMesh().NumberOfNodes();
-    const auto nodes_begin = rModelPart.GetCommunicator().LocalMesh().NodesBegin();
-    const double delta_time = rModelPart.GetProcessInfo()[DELTA_TIME];
-    const double const_u = gamma / (delta_time * beta);
-    const double const_v = 1.0 - gamma / beta;
-    const double const_a = delta_time * (1.0 - gamma / (2.0 * beta));
-
-    #pragma omp parallel for
-    for (int i=0; i<num_local_nodes; i++) {
-        const auto it_node  = nodes_begin + i;
-        const auto& r_mesh_u0 = it_node->FastGetSolutionStepValue(MESH_DISPLACEMENT);
-        auto&       r_mesh_v0 = it_node->FastGetSolutionStepValue(MESH_VELOCITY);
-        auto&       r_mesh_a0 = it_node->FastGetSolutionStepValue(MESH_ACCELERATION);
-
-        const auto& r_mesh_u1 = it_node->FastGetSolutionStepValue(MESH_DISPLACEMENT, 1);
-        const auto& r_mesh_v1 = it_node->FastGetSolutionStepValue(MESH_VELOCITY,     1);
-        const auto& r_mesh_a1 = it_node->FastGetSolutionStepValue(MESH_ACCELERATION, 1);
-
-        r_mesh_v0 = const_u * (r_mesh_u0 - r_mesh_u1) + const_v * r_mesh_v1 + const_a * r_mesh_a1;
-        r_mesh_a0 = (1.0 / (delta_time * gamma)) * (r_mesh_v0 - r_mesh_v1) - ((1 - gamma) / gamma) * r_mesh_a1;
-    }
-
-    rModelPart.GetCommunicator().SynchronizeVariable(MESH_VELOCITY);
-    rModelPart.GetCommunicator().SynchronizeVariable(MESH_ACCELERATION);
-}
-
 void CalculateMeshVelocities(ModelPart& rModelPart,
                              const TimeDiscretization::BDF1& rBDF);
 
 void CalculateMeshVelocities(ModelPart& rModelPart,
                              const TimeDiscretization::BDF2& rBDF);
 
-inline void CalculateMeshVelocities(ModelPart& rModelPart,
-                                    const TimeDiscretization::Newmark& rGenAlpha)
-{
-    CalculateMeshVelocitiesGeneralizedAlpha(rModelPart, rGenAlpha);
-}
+void CalculateMeshVelocities(ModelPart& rModelPart,
+                             const TimeDiscretization::Newmark& rGenAlpha);
 
-inline void CalculateMeshVelocities(ModelPart& rModelPart,
-                                    const TimeDiscretization::Bossak& rGenAlpha)
-{
-    CalculateMeshVelocitiesGeneralizedAlpha(rModelPart, rGenAlpha);
-}
+void CalculateMeshVelocities(ModelPart& rModelPart,
+                             const TimeDiscretization::Bossak& rGenAlpha);
 
-inline void CalculateMeshVelocities(ModelPart& rModelPart,
-                                    const TimeDiscretization::GeneralizedAlpha& rGenAlpha)
-{
-    CalculateMeshVelocitiesGeneralizedAlpha(rModelPart, rGenAlpha);
-}
-
+void CalculateMeshVelocities(ModelPart& rModelPart,
+                             const TimeDiscretization::GeneralizedAlpha& rGenAlpha);
 
 } // namespace MeshVelocityCalculation
 } // namespace Kratos.
