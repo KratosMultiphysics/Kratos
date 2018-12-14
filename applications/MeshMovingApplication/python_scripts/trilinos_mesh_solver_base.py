59f9400984a7316afb007a75d56d5ecf94cdc4a4
philbucher <philipp.bucher@tum.de>
2018-12-14 17:14:58 +0100

    updating names

--- applications/MeshMovingApplication/python_scripts/trilinos_mesh_solver_base.py
+++ applications/MeshMovingApplication/python_scripts/trilinos_mesh_solver_base.py
@@ -20,7 +20,7 @@ class TrilinosMeshSolverBase(mesh_solver_base.MeshSolverBase):
     def __init__(self, mesh_model_part, custom_settings):
         if not custom_settings.Has("mesh_motion_linear_solver_settings"): # Override defaults in the base class.
             linear_solver_settings = KratosMultiphysics.Parameters("""{
-                "solver_type" : "AmesosSolver",
+                "solver_type" : "amesos",
                 "amesos_solver_type" : "Amesos_Klu"
             }""")
             custom_settings.AddValue("mesh_motion_linear_solver_settings", linear_solver_settings)
