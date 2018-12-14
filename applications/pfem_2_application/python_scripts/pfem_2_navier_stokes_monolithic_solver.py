1ef9ca299e0fa8819c262533e7c1a80d4cd376bd
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2018-12-14 16:43:46 +0100

    adding variables and dofs

--- applications/pfem_2_application/python_scripts/pfem_2_navier_stokes_monolithic_solver.py
+++ applications/pfem_2_application/python_scripts/pfem_2_navier_stokes_monolithic_solver.py
@@ -22,56 +22,15 @@ class PFEM2NavierStokesMonolithicSolver(NavierStokesSolverMonolithic):
         super(PFEM2NavierStokesMonolithicSolver,self).__init__(model,custom_settings)
 
     def AddVariables(self):
+        # FluidDynamics Variables
         super(PFEM2NavierStokesMonolithicSolver,self).AddVariables()
 
-        # TODO
-
-        # FluidDynamics Variables
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VELOCITY)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.ACCELERATION)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.MESH_VELOCITY)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.PRESSURE)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.IS_STRUCTURE)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISPLACEMENT)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VISCOSITY)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DENSITY)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.BODY_FORCE)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_H)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.ADVPROJ)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DIVPROJ)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.REACTION)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.REACTION_WATER_PRESSURE)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.EXTERNAL_PRESSURE)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NORMAL)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.Y_WALL)
-        # self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.Q_VALUE)
-        # if self.settings["consider_periodic_conditions"].GetBool() == True:
-        #     self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.PATCH_INDEX)
-
         # PFEM2 Variables
-        # model_part.AddNodalSolutionStepVariable(PRESSURE);
-        # model_part.AddNodalSolutionStepVariable(DISTANCE);
-        # model_part.AddNodalSolutionStepVariable(VELOCITY);
-        # model_part.AddNodalSolutionStepVariable(ACCELERATION);
-        # model_part.AddNodalSolutionStepVariable(YP);
-        # model_part.AddNodalSolutionStepVariable(PRESS_PROJ);
-        # model_part.AddNodalSolutionStepVariable(RHS);
-        # model_part.AddNodalSolutionStepVariable(PROJECTED_VELOCITY);
-        # model_part.AddNodalSolutionStepVariable(NORMAL);
-        # model_part.AddNodalSolutionStepVariable(PREVIOUS_ITERATION_PRESSURE);
-        # model_part.AddNodalSolutionStepVariable(DELTA_VELOCITY)
-        # model_part.AddNodalSolutionStepVariable(PRESS_PROJ_NO_RO)
-        # model_part.AddNodalSolutionStepVariable(MEAN_SIZE)
-        # model_part.AddNodalSolutionStepVariable(NODAL_AREA)
-        # model_part.AddNodalSolutionStepVariable(NODAL_MASS)
-        # model_part.AddNodalSolutionStepVariable(BODY_FORCE)
-        # model_part.AddNodalSolutionStepVariable(MESH_VELOCITY)
-
-        # model_part.AddNodalSolutionStepVariable(VISCOSITY_AIR)
-        # model_part.AddNodalSolutionStepVariable(VISCOSITY_WATER)
-        # model_part.AddNodalSolutionStepVariable(DENSITY_AIR)
-        # model_part.AddNodalSolutionStepVariable(DENSITY_WATER)
+        model_part.AddNodalSolutionStepVariable(YP)
+        model_part.AddNodalSolutionStepVariable(PRESS_PROJ); # Tihs variable is required but unused by ModeParticleUtilityPFEM2
+        model_part.AddNodalSolutionStepVariable(PROJECTED_VELOCITY)
+        model_part.AddNodalSolutionStepVariable(DELTA_VELOCITY)
+        model_part.AddNodalSolutionStepVariable(MEAN_SIZE) # Tihs variable is required but unused by ModeParticleUtilityPFEM2
 
         if self._IsPrintingRank():
             KratosMultiphysics.Logger.PrintInfo("PFEM2NavierStokesMonolithicSolver", "Fluid solver variables added correctly.")
@@ -79,15 +38,7 @@ class PFEM2NavierStokesMonolithicSolver(NavierStokesSolverMonolithic):
     def AddDofs(self):
         super(PFEM2NavierStokesMonolithicSolver,self).AddDofs()
 
-        # TODO
-
-        # FluidDynamics Dofs
-        # KratosMultiphysics.VariableUtils().AddDof(KratosMultiphysics.VELOCITY_X, KratosMultiphysics.REACTION_X,self.main_model_part)
-        # KratosMultiphysics.VariableUtils().AddDof(KratosMultiphysics.VELOCITY_Y, KratosMultiphysics.REACTION_Y,self.main_model_part)
-        # KratosMultiphysics.VariableUtils().AddDof(KratosMultiphysics.VELOCITY_Z, KratosMultiphysics.REACTION_Z,self.main_model_part)
-        # KratosMultiphysics.VariableUtils().AddDof(KratosMultiphysics.PRESSURE, KratosMultiphysics.REACTION_WATER_PRESSURE,self.main_model_part)
-
-        # PFEM2 Dofs
+        # PFEM2 Dofs # mms: we don't need DISTANCE dof (it is the particles pseudo-levelset)
         # KratosMultiphysics.VariableUtils().AddDof(KratosMultiphysics.DISTANCE, self.main_model_part)
 
         if self._IsPrintingRank():
