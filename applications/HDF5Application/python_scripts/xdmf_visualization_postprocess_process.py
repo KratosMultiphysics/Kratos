3bff49a1eb6dd18f1a723f9cbbc8295c01c9bcf0
philbucher <philipp.bucher@tum.de>
2018-12-17 19:48:39 +0100

    add working xdmf_visualization_postprocess_process

--- applications/HDF5Application/python_scripts/xdmf_visualization_postprocess_process.py
+++ applications/HDF5Application/python_scripts/xdmf_visualization_postprocess_process.py
@@ -1,26 +1,35 @@
+import KratosMultiphysics as KM
 import temporal_output_process_factory
-from xdmf_io import XdmfOutput
-
-def GetFactoryHelper():
-    """Return the FactoryHelper to be used for the TemporalOutput"""
-    return temporal_output_process_factory.TemporalOutputFactoryHelper()
-
-def IsRankZero():
-    return True
+import file_utilities
+import temporal_output_process_factory
 
 def Factory(settings, Model):
-    ###### """Return a process for writing simulation results for a single mesh to HDF5."""
-    factory_helper = GetFactoryHelper()
-    list_aux_initial_outputs = []
-    if IsRankZero():
-        list_aux_initial_outputs.append(XdmfOutput(settings["Parameters"]["xdmf_settings"]))
-        settings["Parameters"].RemoveValue("xdmf_settings")
+    """Return a process for writing simulation results for a single mesh to HDF5.
+    It also creates the xdmf-files on the fly
+    """
+    if not isinstance(settings, KM.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
 
-    (temporal_output_process, model_part_output, list_of_results_output) = factory_helper.Execute(settings["Parameters"], Model)
-    list_of_results_output.extend(list_aux_initial_outputs)
+    params = settings["Parameters"]
+    model_part_name = params["model_part_name"].GetString() # name of modelpart must be specified!
+
+    if Model[model_part_name].GetCommunicator().TotalProcesses() > 1:
+        factory_helper = temporal_output_process_factory.PartitionedTemporalOutputFactoryHelper()
+    else:
+        factory_helper = temporal_output_process_factory.TemporalOutputFactoryHelper()
+
+    (temporal_output_process, model_part_output, list_of_results_output) = factory_helper.Execute(params, Model)
     for results_output in list_of_results_output:
         temporal_output_process.AddOutput(results_output)
-    # for aux_output in list_aux_initial_outputs:
-    #     temporal_output_process._initial_output.AddOutput(aux_output)
+
+    temporal_output_process._initial_output.AddOutput(file_utilities.DeleteOldH5Files())
+
+    # in case the h5py-module is not installed (e.g. on clusters) we don't want it to crash the simulation!
+    # => in such a case the xdmf can be created manually afterwards locally
+    try:
+        from xdmf_io import XdmfOutput
+        temporal_output_process.AddOutput(XdmfOutput()) # xdmf should be the last in the list
+    except ImportError:
+        KM.Logger.PrintWarning("XDMF-Writing is not available!")
 
     return temporal_output_process
