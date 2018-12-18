6f511d64a2e390654bcb6567073bf8618e74537e
RTosi <riccardo.tosi@hotmail.it>
2018-12-18 12:40:36 +0100

    Small grammar changes

--- applications/MultilevelMonteCarloApplication/python_scripts/montecarlo_analysis.py
+++ applications/MultilevelMonteCarloApplication/python_scripts/montecarlo_analysis.py
@@ -16,8 +16,8 @@ KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logg
 from analysis_stage import AnalysisStage
 
 # Import exaqute
-from exaqute.ExaquteTaskPyCOMPSs import *   # to exequte with pycompss
-# from exaqute.ExaquteTaskHyperLoom import *  # to exequte with the IT4 scheduler
+from exaqute.ExaquteTaskPyCOMPSs import *   # to execute with pycompss
+# from exaqute.ExaquteTaskHyperLoom import *  # to execute with the IT4 scheduler
 # from exaqute.ExaquteTaskLocal import *      # to execute with python3
 '''
 get_value_from_remote is the equivalent of compss_wait_on: a synchronization point
