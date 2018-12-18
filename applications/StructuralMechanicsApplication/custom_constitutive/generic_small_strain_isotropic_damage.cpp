0e8822a1cc7f4d440ec1c9c2a98d4dc47337984a
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2018-12-18 15:32:18 +0100

    updating the results of the tests for the damage CL

--- applications/StructuralMechanicsApplication/custom_constitutive/generic_small_strain_isotropic_damage.cpp
+++ applications/StructuralMechanicsApplication/custom_constitutive/generic_small_strain_isotropic_damage.cpp
@@ -191,6 +191,7 @@ void GenericSmallStrainIsotropicDamage<TConstLawIntegratorType>::FinalizeSolutio
     const ProcessInfo& rCurrentProcessInfo
     )
 {
+    // Deprecated
 }
 
 /***********************************************************************************/
@@ -236,12 +237,12 @@ void GenericSmallStrainIsotropicDamage<TConstLawIntegratorType>::FinalizeMateria
     Vector& r_strain_vector = rValues.GetStrainVector();
 
     //NOTE: SINCE THE ELEMENT IS IN SMALL STRAINS WE CAN USE ANY STRAIN MEASURE. HERE EMPLOYING THE CAUCHY_GREEN
-    if( r_constitutive_law_options.IsNot( ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN )) {
+    if (r_constitutive_law_options.IsNot( ConstitutiveLaw::USE_ELEMENT_PROVIDED_STRAIN)) {
         this->CalculateValue(rValues, STRAIN, r_strain_vector);
     }
 
     // Elastic Matrix
-    if( r_constitutive_law_options.Is( ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR ) ) {
+    if (r_constitutive_law_options.Is( ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR ) ) {
         Matrix& r_constitutive_matrix = rValues.GetConstitutiveMatrix();
         this->CalculateValue(rValues, CONSTITUTIVE_MATRIX, r_constitutive_matrix);
     }
