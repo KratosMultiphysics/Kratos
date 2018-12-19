e9bfac529e986302656b0def2130f2815b881453
philbucher <philipp.bucher@tum.de>
2018-12-19 14:44:27 +0100

    cleaning metis-app includes

--- applications/metis_application/custom_processes/metis_partitioning_process.h
+++ applications/metis_application/custom_processes/metis_partitioning_process.h
@@ -16,21 +16,12 @@
 #if !defined(KRATOS_METIS_PARTITIONING_PROCESS_INCLUDED )
 #define  KRATOS_METIS_PARTITIONING_PROCESS_INCLUDED
 
-
-
 // System includes
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <fstream>
 
 // External includes
 #include <parmetis.h>
 
-
 // Project includes
-#include "includes/define.h"
-#include "processes/process.h"
 #include "processes/graph_coloring_process.h"
 #include "includes/model_part.h"
 #include "mpi/includes/mpi_communicator.h"
--- applications/metis_application/metis_application.cpp
+++ applications/metis_application/metis_application.cpp
@@ -11,26 +11,9 @@
 // External includes
 
 // Project includes
-#include "includes/define.h"
-
 #include "metis_application.h"
-#include "includes/variables.h"
-//#include "geometries/triangle_2d.h"
 
 namespace Kratos {
-
-/*	KRATOS_CREATE_VARIABLE(double, NODAL_AREA)
-	KRATOS_CREATE_VARIABLE(double, NODAL_H)
-	KRATOS_CREATE_VARIABLE(double, IS_STRUCTURE)
-	KRATOS_CREATE_VARIABLE(double, IS_FLUID)
-	KRATOS_CREATE_VARIABLE(double, IS_BOUNDARY)
-	KRATOS_CREATE_VARIABLE(double, IS_FREE_SURFACE)
-	KRATOS_CREATE_VARIABLE(double, IS_FREE_SURFACE)
-	KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(NORMAL_TO_WALL)
-*/
-//	KRATOS_CREATE_VARIABLE(double, PRESSURE_OLD_IT)
-//	KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(FRACT_VEL)
-KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(VAUX)
 
 KratosMetisApplication::KratosMetisApplication()
     : KratosApplication("MetisApplication") {}
