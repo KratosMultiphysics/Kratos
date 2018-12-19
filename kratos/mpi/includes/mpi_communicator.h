9c1fb167a83e620dc55cdb3e8b6037169548e7a6
jcotela <jcotela@cimne.upc.edu>
2018-12-19 18:59:11 +0100

    Adding And Synchronization.

--- kratos/mpi/includes/mpi_communicator.h
+++ kratos/mpi/includes/mpi_communicator.h
@@ -855,6 +855,63 @@ public:
     {
         //TODO: the DataCommunicator should be a member of the MPICommunicator class (to allow for non-world communicators)
         MPIDataCommunicator world_comm(MPI_COMM_WORLD);
+
+        constexpr unsigned int flag_size = sizeof(Flags) / sizeof(double);
+
+        const NeighbourIndicesContainerType& r_neighbour_indices = NeighbourIndices();
+        const unsigned int number_of_communication_steps = r_neighbour_indices.size();
+
+        std::vector<double> send_buffer;
+        std::vector< std::vector<double>*> receive_buffers(number_of_communication_steps);
+
+        for (unsigned int step = 0; step < number_of_communication_steps; step++)
+        {
+            if (r_neighbour_indices[step] >= 0) // If this rank communicates during this step
+            {
+                NodesContainerType& r_local_nodes = LocalMesh(step).Nodes();
+                NodesContainerType& r_ghost_nodes = GhostMesh(step).Nodes();
+
+                unsigned int send_size = r_ghost_nodes.size() * flag_size;
+                unsigned int recv_size = r_local_nodes.size() * flag_size;
+
+                if ( (send_size == 0) && (recv_size == 0) )
+                    continue;
+
+                send_buffer.resize(send_size);
+                receive_buffers[step] = new std::vector<double>(recv_size);
+
+                double* isend = send_buffer.data();
+                for (ModelPart::NodeIterator inode = r_ghost_nodes.begin(); inode < r_ghost_nodes.end(); ++inode)
+                {
+                    *(Flags*)(isend) = Flags(*inode);
+                    isend += flag_size;
+                }
+
+                world_comm.SendRecv(
+                    send_buffer, r_neighbour_indices[step], step,
+                    *(receive_buffers[step]), r_neighbour_indices[step], step);
+            }
+        }
+
+        for (unsigned int step = 0; step < number_of_communication_steps; step++)
+        {
+            if (r_neighbour_indices[step] >= 0) // If this rank communicates during this step
+            {
+                double* irecv = receive_buffers[step]->data();
+                NodesContainerType& r_local_nodes = LocalMesh(step).Nodes();
+                for (ModelPart::NodeIterator inode = r_local_nodes.begin(); inode < r_local_nodes.end(); ++inode)
+                {
+                    Flags recv = *reinterpret_cast<Flags*>(irecv);
+                    (*inode) &= recv | ~TheFlags;
+                    irecv += flag_size;
+                }
+
+                delete receive_buffers[step];
+            }
+        }
+
+        SynchronizeNodalFlags(TheFlags);
+
         return true;
     }
 
