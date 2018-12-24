97c12cad260c8965f8a4074677a1aefc568249d9
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-12-24 19:47:30 +0100

    Correcting test

--- kratos/tests/test_tetrahedral_mesh_orientation_check.py
+++ kratos/tests/test_tetrahedral_mesh_orientation_check.py
@@ -20,19 +20,6 @@ class TestTetrahedralMeshOrientationCheck(KratosUnittest.TestCase):
         check_mesh = KratosMultiphysics.TetrahedralMeshOrientationCheck(model_part, True)
         check_mesh.Execute()
 
-    @KratosUnittest.expectedFailure
-    def test_ErrorTetrahedralMeshOrientationCheck(self):
-        KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
-        current_model = KratosMultiphysics.Model()
-
-        model_part = current_model.CreateModelPart("Main")
-        model_part_io = KratosMultiphysics.ModelPartIO(GetFilePath("coarse_sphere"))
-        model_part_io.ReadModelPart(model_part)
-
-        check_mesh = KratosMultiphysics.TetrahedralMeshOrientationCheck(model_part, True)
-        check_mesh.SwapAll()
-        check_mesh.Execute()
-
 if __name__ == '__main__':
     KratosUnittest.main()
 
