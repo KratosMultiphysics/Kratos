68a036c24b5fe6633575a4f912d0949d4a35071e
philbucher <philipp.bucher@tum.de>
2018-12-25 12:14:47 +0100

    making Check in Element const

--- kratos/includes/element.h
+++ kratos/includes/element.h
@@ -905,7 +905,7 @@ public:
      * this method is: MANDATORY
      */
 
-    virtual int Check(const ProcessInfo& rCurrentProcessInfo)
+    virtual int Check(const ProcessInfo& rCurrentProcessInfo) const
     {
         KRATOS_TRY
 
