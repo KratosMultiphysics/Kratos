76d6c09f940b32b239967cb47d9cfe8cee8afa86
philbucher <philipp.bucher@tum.de>
2018-12-27 00:41:55 +0100

    some improvements in python-process

--- kratos/python_scripts/vtk_output_process.py
+++ kratos/python_scripts/vtk_output_process.py
@@ -7,14 +7,13 @@ def Factory(settings, Model):
     return VtkOutputProcess(Model, settings["Parameters"])
 
 
-## All the processes python should be derived from "Process"
 class VtkOutputProcess(KratosMultiphysics.Process):
-    "IMPORTANT : when output_control_type is time, then paraview will not be able to group them "
     def __init__(self, Model, settings ):
         KratosMultiphysics.Process.__init__(self)
 
-        default_parameters = KratosMultiphysics.Parameters("""
-        {
+        # IMPORTANT: when "output_control_type" is "time",
+        # then paraview will not be able to group them
+        default_parameters = KratosMultiphysics.Parameters("""{
             "model_part_name"                    : "PLEASE_SPECIFY_MODEL_PART_NAME",
             "file_format"                        : "ascii",
             "output_precision"                   : 7,
@@ -26,21 +25,19 @@ class VtkOutputProcess(KratosMultiphysics.Process):
             "nodal_solution_step_data_variables" : [],
             "nodal_data_value_variables"         : [],
             "element_data_value_variables"       : [],
-            "condition_data_value_variables"       : []
-        }
-        """)
+            "condition_data_value_variables"     : []
+        }""")
 
-        self.model = Model
         model_part_name = settings["model_part_name"].GetString()
-        self.model_part = self.model[model_part_name]
+        self.model_part = Model[model_part_name]
 
         self.settings = settings
         self.settings.ValidateAndAssignDefaults(default_parameters)
-        folder_name = self.settings["folder_name"].GetString()
 
         if self.settings["save_output_files_in_folder"].GetBool() :
+            folder_name = self.settings["folder_name"].GetString()
             if(os.path.isdir(folder_name)):
-                if(not self.model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]):
+                if not self.model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
                     import shutil
                     shutil.rmtree(folder_name)
             os.mkdir(folder_name)
@@ -48,13 +45,9 @@ class VtkOutputProcess(KratosMultiphysics.Process):
         self.vtk_io = KratosMultiphysics.VtkOutput(self.model_part, self.settings)
 
         self.output_frequency = self.settings["output_frequency"].GetDouble()
-        #
         self.output_control = self.settings["output_control_type"].GetString()
         self.next_output = 0.0
         self.step_count = 0
-
-    def ExecuteInitializeSolutionStep(self):
-        self.step_count += 1
 
     def ExecuteInitialize(self):
         if self.output_control == "time":
@@ -62,29 +55,31 @@ class VtkOutputProcess(KratosMultiphysics.Process):
         else:
             self.next_output = self.model_part.ProcessInfo[KratosMultiphysics.STEP]
 
-    def PrintOutput(self):
-        if(self.IsOutputStep()):
-            self.vtk_io.PrintOutput()
+    def ExecuteInitializeSolutionStep(self):
+        self.step_count += 1
 
-            # Schedule next output
-            time = self.__GetPrettyTime(self.model_part.ProcessInfo[KratosMultiphysics.TIME])
-            if self.output_frequency > 0.0: # Note: if == 0, we'll just always print
-                if self.output_control == "time":
-                    while self.__GetPrettyTime(self.next_output) <= time:
-                        self.next_output += self.output_frequency
-                else:
-                    while self.next_output <= self.step_count:
-                        self.next_output += self.output_frequency
+    def PrintOutput(self):
+        self.vtk_io.PrintOutput()
+
+        # Schedule next output
+        time = GetPrettyTime(self.model_part.ProcessInfo[KratosMultiphysics.TIME])
+        if self.output_frequency > 0.0: # Note: if == 0, we'll just always print
+            if self.output_control == "time":
+                while GetPrettyTime(self.next_output) <= time:
+                    self.next_output += self.output_frequency
+            else:
+                while self.next_output <= self.step_count:
+                    self.next_output += self.output_frequency
 
     def IsOutputStep(self):
         if self.output_control == "time":
-            time = self.__GetPrettyTime(self.model_part.ProcessInfo[KratosMultiphysics.TIME])
-            return (time >= self.__GetPrettyTime(self.next_output))
+            time = GetPrettyTime(self.model_part.ProcessInfo[KratosMultiphysics.TIME])
+            return (time >= GetPrettyTime(self.next_output))
         else:
             return ( self.step_count >= self.next_output )
 
 
-    def __GetPrettyTime(self,time):
-        pretty_time = "{0:.12g}".format(time)
-        pretty_time = float(pretty_time)
-        return pretty_time
+def GetPrettyTime(time):
+    pretty_time = "{0:.12g}".format(time)
+    pretty_time = float(pretty_time)
+    return pretty_time
