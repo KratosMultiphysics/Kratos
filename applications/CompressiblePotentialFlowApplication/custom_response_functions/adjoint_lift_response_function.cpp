bbd427a8d78e7413814772c9f375f654d795da9b
marcnunezc <marcnc94@gmail.com>
2019-01-05 20:04:22 +0100

    Created lift response function file, adjoint potential scheme file and replace adjoint element and conditions process. Adapting from StructuralMechanics application

---
+++ applications/CompressiblePotentialFlowApplication/custom_response_functions/adjoint_lift_response_function.cpp
@@ -0,0 +1,118 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:		 BSD License
+//					 license: structural_mechanics_application/license.txt
+//
+//  Main authors:    Martin Fusseder, https://github.com/MFusseder
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "adjoint_lift_response_function.h"
+
+namespace Kratos
+{
+    AdjointLiftResponseFunction::AdjointLiftResponseFunction(ModelPart& rModelPart, Parameters ResponseSettings)
+     : AdjointStructuralResponseFunction(rModelPart, ResponseSettings)
+    {
+        // This response function currently only works in 2D!
+        ProcessInfo& r_current_process_info = rModelPart.GetProcessInfo();
+        const int domain_size = r_current_process_info[DOMAIN_SIZE];
+        KRATOS_ERROR_IF(domain_size != 2) << "Invalid DOMAIN_SIZE: " << domain_size << std::endl;
+    }
+
+    AdjointLiftResponseFunction::~AdjointLiftResponseFunction(){}
+
+    void AdjointLiftResponseFunction::CalculateGradient(const Element& rAdjointElement,
+                                   const Matrix& rResidualGradient,
+                                   Vector& rResponseGradient,
+                                   const ProcessInfo& rProcessInfo)
+    {
+        KRATOS_TRY;
+
+        if (rResponseGradient.size() != rResidualGradient.size1())
+            rResponseGradient.resize(rResidualGradient.size1(), false);
+
+        rResponseGradient.clear();
+
+
+        KRATOS_CATCH("");
+    }
+
+    void AdjointLiftResponseFunction::CalculatePartialSensitivity(Element& rAdjointElement,
+                                             const Variable<double>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo)
+    {
+        KRATOS_TRY
+
+        if (rSensitivityGradient.size() != 0)
+            rSensitivityGradient.resize(0, false);
+
+        KRATOS_CATCH("")
+    }
+
+    void AdjointLiftResponseFunction::CalculatePartialSensitivity(Condition& rAdjointCondition,
+                                             const Variable<double>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo)
+    {
+        KRATOS_TRY;
+
+        if (rSensitivityGradient.size() != 0)
+            rSensitivityGradient.resize(0, false);
+
+        KRATOS_CATCH("");
+    }
+
+    void AdjointLiftResponseFunction::CalculatePartialSensitivity(Element& rAdjointElement,
+                                             const Variable<array_1d<double, 3>>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo)
+    {
+        KRATOS_TRY
+
+        if (rSensitivityGradient.size() != 0)
+            rSensitivityGradient.resize(0, false);
+
+        KRATOS_CATCH("")
+    }
+
+    void AdjointLiftResponseFunction::CalculatePartialSensitivity(Condition& rAdjointCondition,
+                                             const Variable<array_1d<double, 3>>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo)
+    {
+        KRATOS_TRY;
+
+        if (rSensitivityGradient.size() != 0)
+            rSensitivityGradient.resize(0, false);
+
+        KRATOS_CATCH("");
+    }
+
+    double AdjointLiftResponseFunction::CalculateValue(ModelPart& rModelPart)
+    {
+        KRATOS_TRY;
+
+        const VariableComponentType& r_traced_dof =
+            KratosComponents<VariableComponentType>::Get(mTracedDofLabel);
+
+        return mpTracedNode->FastGetSolutionStepValue(r_traced_dof, 0);
+
+        KRATOS_CATCH("");
+    }
+
+} // namespace Kratos.
+
+
