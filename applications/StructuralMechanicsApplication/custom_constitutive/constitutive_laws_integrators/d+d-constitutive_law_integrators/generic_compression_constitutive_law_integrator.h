d78b6151adfa18255f405072c2e87a905bed50d3
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-01-07 15:07:17 +0100

    correcting minor mistakes

--- applications/StructuralMechanicsApplication/custom_constitutive/constitutive_laws_integrators/d+d-constitutive_law_integrators/generic_compression_constitutive_law_integrator.h
+++ applications/StructuralMechanicsApplication/custom_constitutive/constitutive_laws_integrators/d+d-constitutive_law_integrators/generic_compression_constitutive_law_integrator.h
@@ -128,7 +128,7 @@ class GenericCompressionConstitutiveLawIntegratorDplusDminusDamage
         )
     {
         const Properties& r_material_properties = rValues.GetMaterialProperties();
-        const int softening_type = r_material_properties.Has(SOFTENING_TYPE_COMPRESSION) ? r_material_properties[SOFTENING_TYPE_COMPRESSION] : r_material_properties[SOFTENING_TYPE]
+		const int softening_type = r_material_properties.Has(SOFTENING_TYPE_COMPRESSION) ? r_material_properties[SOFTENING_TYPE_COMPRESSION] : r_material_properties[SOFTENING_TYPE];
         double damage_parameter;
         CalculateDamageParameterCompression(rValues, damage_parameter, CharacteristicLength);
 
@@ -158,11 +158,11 @@ class GenericCompressionConstitutiveLawIntegratorDplusDminusDamage
         const double CharacteristicLength
         )
     {
-        const double fracture_energy_compression = r_material_properties[FRACTURE_ENERGY_COMPRESSION];
+        const double fracture_energy_compression = rValues.GetMaterialProperties()[FRACTURE_ENERGY_COMPRESSION];
         ConstitutiveLaw::Parameters modified_values = rValues;
-        auto& r_properties = modified_values.GetMaterialProperties();
-        material_props.SetValue(FRACTURE_ENERGY, fracture_energy_compression);
-        modified_values.SetMaterialProperties(material_props);
+        auto r_properties = modified_values.GetMaterialProperties();
+        r_properties.SetValue(FRACTURE_ENERGY, fracture_energy_compression);
+        modified_values.SetMaterialProperties(r_properties);
         TYieldSurfaceType::CalculateDamageParameter(modified_values, rDamageParameter, CharacteristicLength);
     }
 
