66061a1facc74969874765e8cdadd981bda97c34
DB <daniel.baumgaertner@tum.de>
2019-01-24 13:41:17 +0100

    added external adjoint response function

---
+++ applications/StructuralMechanicsApplication/custom_response_functions/response_utilities/adjoint_external_response_function.h
@@ -0,0 +1,213 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:		 BSD License
+//					 license: structural_mechanics_application/license.txt
+//
+//  Main authors:    Baumgaertner Daniel, https://github.com/dbaumgaertner
+//
+
+#ifndef ADJOINT_EXTERNAL_RESPONSE_FUNCTION_H
+#define ADJOINT_EXTERNAL_RESPONSE_FUNCTION_H
+
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "adjoint_structural_response_function.h"
+
+namespace Kratos
+{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/** \brief AdjointExternalResponseFunction
+*
+* This is a response function which traces a chosen displacement or rotation of a single
+* node as response. It is designed to be used in adjoint sensitivity analysis.
+*/
+class KRATOS_API(STRUCTURAL_MECHANICS_APPLICATION) AdjointExternalResponseFunction : public AdjointStructuralResponseFunction
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    typedef Element::DofsVectorType DofsVectorType;
+
+    ///@}
+    ///@name Pointer Definitions
+    /// Pointer definition of AdjointExternalResponseFunction
+    KRATOS_CLASS_POINTER_DEFINITION(AdjointExternalResponseFunction);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    AdjointExternalResponseFunction(ModelPart& rModelPart, Parameters ResponseSettings);
+
+    /// Destructor.
+    ~AdjointExternalResponseFunction() override;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    void InitializeSolutionStep() override;
+
+    void CalculateGradient(const Element& rAdjointElement,
+                                   const Matrix& rResidualGradient,
+                                   Vector& rResponseGradient,
+                                   const ProcessInfo& rProcessInfo) override;
+
+    void CalculatePartialSensitivity(Element& rAdjointElement,
+                                             const Variable<double>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo) override;
+
+    void CalculatePartialSensitivity(Condition& rAdjointCondition,
+                                             const Variable<double>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo) override;
+
+    void CalculatePartialSensitivity(Element& rAdjointElement,
+                                             const Variable<array_1d<double, 3>>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo) override;
+
+    void CalculatePartialSensitivity(Condition& rAdjointCondition,
+                                             const Variable<array_1d<double, 3>>& rVariable,
+                                             const Matrix& rSensitivityMatrix,
+                                             Vector& rSensitivityGradient,
+                                             const ProcessInfo& rProcessInfo) override;
+
+
+    double CalculateValue(ModelPart& rModelPart) override;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+    ///@}
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+    ///@}
+
+private:
+
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    std::map<unsigned int, bool> mIsNodeAlreadyConsideredInCalculateGradient;
+    std::map<unsigned int, bool> mIsNodeAlreadyConsideredInCalculatePartialSensitivity;
+    unsigned int mRequiredNumDofPerNode = 3;
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    ///@}
+
+}; // Class AdjointExternalResponseFunction
+
+///@}
+
+///@name Type Definitions
+///@{
+
+///@}
+///@name Input and output
+///@{
+
+///@}
+
+} // namespace Kratos.
+
+#endif // ADJOINT_EXTERNAL_RESPONSE_FUNCTION_H_INCLUDED
--- applications/StructuralMechanicsApplication/python_scripts/structural_response_function_factory.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_response_function_factory.py
@@ -25,6 +25,9 @@ def CreateResponseFunction(response_id, response_settings, model):
     elif response_type == "adjoint_local_stress":
         return structural_response.AdjointResponseFunction(response_id, response_settings, model)
 
+    elif response_type == "adjoint_external_function":
+        return structural_response.AdjointResponseFunction(response_id, response_settings, model)
+
     else:
         raise NameError("The type of the following response function is not specified: "+ response_id +
-                        ".\nAvailable types are: 'mass', 'strain_energy', 'eigenfrequency', 'adjoint_nodal_displacement', 'adjoint_linear_strain_energy', 'adjoint_local_stress'." )
+                        ".\nAvailable types are: 'mass', 'strain_energy', 'eigenfrequency', 'adjoint_nodal_displacement', 'adjoint_linear_strain_energy', 'adjoint_local_stress', 'adjoint_external_function'." )
