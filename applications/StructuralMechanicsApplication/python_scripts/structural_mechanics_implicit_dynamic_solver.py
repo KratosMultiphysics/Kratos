cd4b5679e0ff8233b40069cfcfb11a1e0c7391a7
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2019-01-25 17:23:58 +0100

    Correction (II)

--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_implicit_dynamic_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_implicit_dynamic_solver.py
@@ -10,7 +10,7 @@ import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsA
 import structural_mechanics_solver
 
 # Import common utilities
-import common_methods_solvers
+from KratosMultiphysics.StructuralMechanicsApplication import common_methods_solvers
 
 def CreateSolver(model, custom_settings):
     return ImplicitMechanicalSolver(model, custom_settings)
