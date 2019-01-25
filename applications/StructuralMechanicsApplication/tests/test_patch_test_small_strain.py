02c5206fa3bbdf5e61bdf0a2eb7912ea001aee81
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2019-01-25 15:37:52 +0100

    Updating result
    (Apparently the OMP is not right, I will fix that)

--- applications/StructuralMechanicsApplication/tests/test_patch_test_small_strain.py
+++ applications/StructuralMechanicsApplication/tests/test_patch_test_small_strain.py
@@ -459,7 +459,7 @@ class TestPatchTestSmallStrain(KratosUnittest.TestCase):
         # Testing explicit utilities
         empty_param = KratosMultiphysics.Parameters("""{}""")
         max_delta_time = StructuralMechanicsApplication.CalculateDeltaTime(mp, empty_param)
-        self.assertAlmostEqual(max_delta_time,7.082863323669629e-07)
+        self.assertAlmostEqual(max_delta_time,4.764516014904737e-07)
 
     def __post_process(self, main_model_part):
         from gid_output_process import GiDOutputProcess
