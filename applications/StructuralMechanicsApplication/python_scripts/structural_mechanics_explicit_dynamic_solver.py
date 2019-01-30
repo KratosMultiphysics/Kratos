0a5d5d0aba38e5f41d35847de052f28c37e383c9
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-01-30 16:21:54 +0100

    Moving computation to process

--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_explicit_dynamic_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_explicit_dynamic_solver.py
@@ -9,8 +9,6 @@ import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsA
 # Import base class file
 import structural_mechanics_solver
 
-# Import common utilities
-from KratosMultiphysics.StructuralMechanicsApplication import common_methods_solvers
 
 def CreateSolver(model, custom_settings):
     return ExplicitMechanicalSolver(model, custom_settings)
@@ -37,15 +35,7 @@ class ExplicitMechanicalSolver(structural_mechanics_solver.MechanicalSolver):
             "rayleigh_beta"              : 0.0
         }
         """)
-        damping_settings = common_methods_solvers.AuxiliarDampingSettings()
-        if custom_settings.Has("damping_settings"):
-            if custom_settings["damping_settings"].Has("determine_rayleigh_damping_settings"):
-                if custom_settings["damping_settings"]["determine_rayleigh_damping_settings"].Has("eigen_system_settings"):
-                    if custom_settings["damping_settings"]["determine_rayleigh_damping_settings"]["eigen_system_settings"].Has("solver_type"):
-                        solver_type = custom_settings["damping_settings"]["determine_rayleigh_damping_settings"]["eigen_system_settings"]["solver_type"].GetString()
-                        eigen_system_settings = common_methods_solvers.AuxiliarEigenSettings(solver_type)
-                        damping_settings["damping_settings"]["determine_rayleigh_damping_settings"]["eigen_system_settings"] = eigen_system_settings["eigen_system_settings"]
-        self.dynamic_settings.AddValue("damping_settings", damping_settings["damping_settings"])
+
         self.validate_and_transfer_matching_settings(custom_settings, self.dynamic_settings)
         # Validate the remaining settings in the base class.
 
@@ -93,14 +83,6 @@ class ExplicitMechanicalSolver(structural_mechanics_solver.MechanicalSolver):
             self.delta_time = StructuralMechanicsApplication.CalculateDeltaTime(self.GetComputingModelPart(), self.delta_time_settings)
         else:
             self.delta_time = self.settings["time_stepping"]["time_step"].GetDouble()
-            
-    def InitializeSolutionStep(self):
-        # Using the base InitializeSolutionStep
-        super(ExplicitMechanicalSolver, self).InitializeSolutionStep()
-
-        # We compute for the different parts (each body can have it own damping)
-        if self.dynamic_settings["damping_settings"]["determine_rayleigh_damping"].GetBool() and self.main_model_part.ProcessInfo[KratosMultiphysics.STEP] == 1:
-            common_methods_solvers.ComputeDampingCoefficients(self.model, self.settings, self.dynamic_settings["damping_settings"])
 
     #### Specific internal functions ####
     def _create_solution_scheme(self):
--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_implicit_dynamic_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_implicit_dynamic_solver.py
@@ -9,8 +9,6 @@ import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsA
 # Import base class file
 import structural_mechanics_solver
 
-# Import common utilities
-from KratosMultiphysics.StructuralMechanicsApplication import common_methods_solvers
 
 def CreateSolver(model, custom_settings):
     return ImplicitMechanicalSolver(model, custom_settings)
@@ -31,24 +29,13 @@ class ImplicitMechanicalSolver(structural_mechanics_solver.MechanicalSolver):
         # Set defaults and validate custom settings.
         self.dynamic_settings = KratosMultiphysics.Parameters("""
         {
-            "scheme_type"                : "bossak",
-            "damp_factor_m"              : -0.3,
-            "rayleigh_alpha"             : 0.0,
-            "rayleigh_beta"              : 0.0
+            "scheme_type"   : "bossak",
+            "damp_factor_m" :-0.3,
+            "rayleigh_alpha": 0.0,
+            "rayleigh_beta" : 0.0
         }
         """)
-
-        damping_settings = common_methods_solvers.AuxiliarDampingSettings()
-        if custom_settings.Has("damping_settings"):
-            if custom_settings["damping_settings"].Has("determine_rayleigh_damping_settings"):
-                if custom_settings["damping_settings"]["determine_rayleigh_damping_settings"].Has("eigen_system_settings"):
-                    if custom_settings["damping_settings"]["determine_rayleigh_damping_settings"]["eigen_system_settings"].Has("solver_type"):
-                        solver_type = custom_settings["damping_settings"]["determine_rayleigh_damping_settings"]["eigen_system_settings"]["solver_type"].GetString()
-                        eigen_system_settings = common_methods_solvers.AuxiliarEigenSettings(solver_type)
-                        damping_settings["damping_settings"]["determine_rayleigh_damping_settings"]["eigen_system_settings"] = eigen_system_settings["eigen_system_settings"]
-        self.dynamic_settings.AddValue("damping_settings", damping_settings["damping_settings"])
         self.validate_and_transfer_matching_settings(custom_settings, self.dynamic_settings)
-        # Validate the remaining settings in the base class.
         # Validate the remaining settings in the base class.
 
         # Construct the base solver.
@@ -75,10 +62,8 @@ class ImplicitMechanicalSolver(structural_mechanics_solver.MechanicalSolver):
         # Using the base InitializeSolutionStep
         super(ImplicitMechanicalSolver, self).InitializeSolutionStep()
 
-        # We compute for the different parts (each body can have it own damping)
-        if self.dynamic_settings["damping_settings"]["determine_rayleigh_damping"].GetBool() and self.main_model_part.ProcessInfo[KratosMultiphysics.STEP] == 1:
-            common_methods_solvers.ComputeDampingCoefficients(self.model, self.settings, self.dynamic_settings["damping_settings"])
-
+        # Some pre-processes may affect the system of equations, we rebuild the equation ids
+        if self.main_model_part.ProcessInfo[KratosMultiphysics.STEP] == 1:
             # Resetting the global equations ids
             self.get_builder_and_solver().SetUpSystem(self.GetComputingModelPart())
 
