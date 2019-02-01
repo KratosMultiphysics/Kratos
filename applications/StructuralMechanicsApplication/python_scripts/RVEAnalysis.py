151d6ac29e0ad2ef3d6bd2b93f0f72177aa22167
Riccardo Rossi <rrossi@cimne.upc.edu>
2019-02-01 21:12:13 +0100

    importing following new style

--- applications/StructuralMechanicsApplication/python_scripts/RVEAnalysis.py
+++ applications/StructuralMechanicsApplication/python_scripts/RVEAnalysis.py
@@ -3,7 +3,7 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 import KratosMultiphysics
 import KratosMultiphysics.StructuralMechanicsApplication
 import math
-from structural_mechanics_analysis import StructuralMechanicsAnalysis
+from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_analysis import StructuralMechanicsAnalysis
 
 """
 For user-scripting it is intended that a new class is derived
