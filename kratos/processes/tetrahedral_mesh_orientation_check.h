8988d68d1d236651f51443d88896da8029bafef1
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-02-02 18:02:04 +0100

    Setting NOT_ALLOW_REPEATED_CONDITIONS

--- kratos/processes/tetrahedral_mesh_orientation_check.h
+++ kratos/processes/tetrahedral_mesh_orientation_check.h
@@ -90,7 +90,7 @@ public:
     TetrahedralMeshOrientationCheck(
         ModelPart& rModelPart,
         bool ThrowErrors,
-        Flags options = NOT_COMPUTE_NODAL_NORMALS | NOT_COMPUTE_CONDITION_NORMALS | NOT_ASSIGN_NEIGHBOUR_ELEMENTS_TO_CONDITIONS
+        Flags options = NOT_COMPUTE_NODAL_NORMALS | NOT_COMPUTE_CONDITION_NORMALS | NOT_ASSIGN_NEIGHBOUR_ELEMENTS_TO_CONDITIONS | NOT_ALLOW_REPEATED_CONDITIONS
         ):  Process(),
             mrModelPart(rModelPart),
             mThrowErrors(ThrowErrors), //to be changed to a flag
