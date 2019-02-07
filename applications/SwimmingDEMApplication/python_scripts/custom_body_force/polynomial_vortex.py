0ae464d88fee51600b007e59e288291d7becab33
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2019-02-07 17:23:45 +0100

    adimensional vortex

--- applications/SwimmingDEMApplication/python_scripts/custom_body_force/polynomial_vortex.py
+++ applications/SwimmingDEMApplication/python_scripts/custom_body_force/polynomial_vortex.py
@@ -24,32 +24,33 @@ class PolynomialVortex(ManufacturedSolution):
 
         settings.ValidateAndAssignDefaults(default_settings)
 
+        self.a = 16*np.sqrt(3**3)
         self.U = settings["velocity"].GetDouble()
+        self.sq_au = np.sqrt(self.a * self.U)
         self.L = settings["length"].GetDouble()
         self.rho = settings["density"].GetDouble()
         self.nu = settings["viscosity"].GetDouble() / self.rho
         self.T = self.L / self.U
-        self.omega = settings["frequency"].GetDouble() * 2 * np.pi / self.T
-        self.damp = settings["damping"].GetDouble() * 2 * np.pi / self.T
-        self.k = np.pi / self.L
+        self.omega = settings["frequency"].GetDouble() * 2 * np.pi
+        self.damp = settings["damping"].GetDouble()
 
     def f(self, x):
-        return self.U * x**2 * (self.L - x)**2
+        return self.sq_au * x**2 * (self.L - x)**2
 
     def g(self, t):
         return np.cos(self.omega * t) * np.e**(-self.damp * t)
 
     def df(self, x):
-        return 2 * self.U * x * (self.L - x)**2 - 2 * self.U * x**2 * (self.L - x)
+        return 2 * self.sq_au * x * (self.L - x)**2 - 2 * self.sq_au * x**2 * (self.L - x)
 
     def dg(self, t):
         return -self.omega * np.sin(self.omega * t) * np.e**(-self.damp * t) -self.damp * np.cos(self.omega * t) * np.e**(-self.damp * t)
 
     def ddf(self, x):
-        return 2 * self.U * (self.L**2 - 6*self.L*x + 6*x**2)
+        return 2 * self.sq_au * (self.L**2 - 6*self.L*x + 6*x**2)
 
     def dddf(self, x):
-        return 12 * self.U * (2*x - self.L)
+        return 12 * self.sq_au * (2*x - self.L)
 
     def u1(self, x1, x2, t):
         return self.f(x1) * self.df(x2) * self.g(t)
