7e2daf3f8e7c41108423f845e4ba2b9a9ed20b6f
Simon v. Wenczowski <simon.wenczowski@outlook.de>
2019-02-08 18:20:53 +0100

    Outline class "Preprocessor" (to be filled)

---
+++ applications/GeodataProcessingApplication/python_scripts/Preprocessor.py
@@ -0,0 +1,20 @@
+
+
+# one class for Preprocessor (no derived classes intended) 
+class Preprocessor:
+
+
+
+# attributes (data members)
+
+point_list = []
+
+
+
+# read_functions (pattern: read_FILENAME)
+
+# operation_functions (pattern: OPERATIONNAME)
+
+# write_functions (pattern: write_FILENAME)
+
+# handling_functions ( clear(), statistics() )
