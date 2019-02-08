662e2250efa61493ebef301f86da5d1f548f936f
Philipp Bucher <philipp.bucher@tum.de>
2019-02-08 17:48:56 +0100

    codacy suggestion

--- kratos/python_scripts/process_factory.py
+++ kratos/python_scripts/process_factory.py
@@ -19,8 +19,8 @@ class KratosProcessFactory(object):
             if item.Has("kratos_module"): # for Kratos-processes
                 '''Location of the process in Kratos; e.g.:
                 - KratosMultiphysics
-                - KratosMultiphysics.FluidDynamicsApplication
-                '''
+                - KratosMultiphysics.FluidDynamicsApplication'''
+                
                 kratos_module_name = item["kratos_module"].GetString()
                 if not kratos_module_name.startswith("KratosMultiphysics"):
                     kratos_module_name = "KratosMultiphysics." + kratos_module_name
