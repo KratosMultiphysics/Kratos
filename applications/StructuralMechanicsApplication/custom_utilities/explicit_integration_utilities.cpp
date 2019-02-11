ea904d6c974074878d08d3a2029e3dbadd975b1f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-02-11 18:28:25 +0100

    [StructuralMechanicsApplication] Using shorter side of element instead of radius for critical time step
    @frastellini suggested this change. This way the security factor can be more realistic (0.55 instead of 0.4)

--- applications/StructuralMechanicsApplication/custom_utilities/explicit_integration_utilities.cpp
+++ applications/StructuralMechanicsApplication/custom_utilities/explicit_integration_utilities.cpp
@@ -34,7 +34,7 @@ double CalculateDeltaTime(
     {
         "time_step_prediction_level" : 2.0,
         "max_delta_time"             : 1.0e-3,
-        "safety_factor"              : 0.4,
+        "safety_factor"              : 0.55,
         "mass_factor"                : 1.0,
         "desired_delta_time"         : -1.0,
         "max_number_of_iterations"   : 10
@@ -121,7 +121,7 @@ double InnerCalculateDeltaTime(
 
         /* Get geometric and material properties */
         const Properties& r_properties = it_elem->GetProperties();
-        const auto& r_geometry = it_elem->GetGeometry();
+        auto& r_geometry = it_elem->GetGeometry();
 
         // Initialize
         check_has_all_variables = true;
@@ -162,17 +162,11 @@ double InnerCalculateDeltaTime(
         }
 
         if (check_has_all_variables) {
-            // Computing length as the element radius
+            // Computing length as the smallest side of the geometry
 //             const double length = it_elem->GetGeometry().Length();
-            double length = 0.0;
-            const Point& r_center = r_geometry.Center();
-
-            array_1d<double, 3> aux_vector;
-            for(std::size_t i_node = 0; i_node < r_geometry.PointsNumber(); ++i_node)  {
-                noalias(aux_vector) = r_center.Coordinates() - r_geometry[i_node].Coordinates();
-                const double aux_value = norm_2(aux_vector);
-                if(aux_value > length)
-                    length = aux_value;
+            double length = std::numeric_limits<double>::max();
+            for (IndexType i_edge = 0; i_edge < r_geometry.EdgesNumber(); ++i_edge) {
+                length = std::min(r_geometry.Edges()[i_edge].Length(), length);
             }
 
             // Compute courant criterion
