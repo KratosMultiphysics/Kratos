61c4b71e52c3c1de857b2caf7178199dd9fd0e2c
Nicola <nicola.germano@unich.it>
2019-02-11 18:43:38 +0100

    added stl_import

--- applications/GeodataProcessingApplication/python_scripts/geo_importer.py
+++ applications/GeodataProcessingApplication/python_scripts/geo_importer.py
@@ -12,11 +12,31 @@ class GeoImporter( GeoProcessor ):
 
     ### --- function imports the nodes from a *.stl file --- ###
 
-    def stl_import( self, stl_file_name_input ):
+    def stl_import(self, stl_file_name_input):
 
         self._initialize_model_part()
 
-        print( "I will keep my hands off this one - you are the expert, Nicola." )
+        with open (stl_file_name_input) as read_file:
+
+            node_dict = {}        # dictionary with all vertices. key = (x, y, z); value = node_id
+            node_id = 1
+
+            for row in read_file.readlines():
+                row = row.split()
+                
+                if (row[0] == "vertex" and all(self._isfloat(n) for n in row[1:])):
+                    # row[0] = "vertex"; row[1] = x coordinate; row[2] = y coordinate; row[3] = z coordinate
+                    X_coord, Y_coord, Z_coord = [float(coord) for coord in row[1:]]
+
+                    if (X_coord, Y_coord, Z_coord) not in node_dict:
+                        node_dict[X_coord, Y_coord, Z_coord] = node_id
+                        
+                        node_id += 1
+
+        node_dict = self._delete_node_on_base(node_dict)
+
+        for coord, node_id in node_dict.items():
+            self.ModelPart.CreateNewNode(node_id, coord[0], coord[1], coord[2])
 
         self.HasModelPart = True
 
@@ -32,7 +52,7 @@ class GeoImporter( GeoProcessor ):
             for row in read_file.readlines():
                 row = row.split()
                 if ( all( self._isfloat(n) for n in row) ):
-				    # row[0] = x coordinate; row[1] = y coordinate; row[2] = z coordinate
+                    # row[0] = x coordinate; row[1] = y coordinate; row[2] = z coordinate
                     X_coord, Y_coord, Z_coord = [float(coord) for coord in row[0:3]]
                     n = self.ModelPart.CreateNewNode(node_id, X_coord, Y_coord, Z_coord)
                     node_id += 1
@@ -77,3 +97,13 @@ class GeoImporter( GeoProcessor ):
         self.ModelPart.Nodes.clear()
         self.ModelPart.Elements.clear()
         self.ModelPart.Conditions.clear()
+
+
+    def _delete_node_on_base(self, node_dict):
+        # this function delete all nodes with Z == 0
+
+        for coords, _ in list(node_dict.items()):
+            if (coords[2] == 0.0):    # if (Z == 0.0) the node is excluded
+                del (node_dict[coords])
+
+        return node_dict
