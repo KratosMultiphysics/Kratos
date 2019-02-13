047da4cf13fdb48a100975478954c5a32f55f8a1
Nicola <nicola.germano@unich.it>
2019-02-13 15:08:30 +0100

    updated name of functions in according with 'Python style conventions'. The Cut function is more general now.

--- applications/GeodataProcessingApplication/python_scripts/geo_preprocessor.py
+++ applications/GeodataProcessingApplication/python_scripts/geo_preprocessor.py
@@ -26,7 +26,7 @@ class GeoPreprocessor( GeoProcessor ):
     # for xyz files
     #########################################################################
 
-    def read_xyz(self, xyz_in_file):
+    def ReadXYZ(self, xyz_in_file):
 
         filename_in, extension_in = os.path.splitext(xyz_in_file)
 
@@ -38,7 +38,7 @@ class GeoPreprocessor( GeoProcessor ):
         with open(xyz_in_file) as read_file:
             for line in read_file.readlines():
                 items = line.split()
-                if (all(self._isfloat(n) for n in items)):
+                if (all(self._IsFloat(n) for n in items)):
                     x = float(items[0])
                     y = float(items[1])
                     z = float(items[2])
@@ -46,7 +46,7 @@ class GeoPreprocessor( GeoProcessor ):
                     self.point_list.append([x,y,z])
 
 
-    def write_xyz(self, type_file="standard", xyz_file_name_output="", list_to_write=[]):
+    def WriteXYZ(self, type_file="standard", xyz_file_name_output="", list_to_write=[]):
         # function to write a xyz file from a list of points
 
         filename_out, extension_out = os.path.splitext(xyz_file_name_output)
@@ -87,8 +87,8 @@ class GeoPreprocessor( GeoProcessor ):
     ########################################################################
     # for stl files
     #########################################################################
-    
-    def read_stl(self, stl_file_name_input):
+
+    def ReadSTL(self, stl_file_name_input):
 
         filename_in, extension_in = os.path.splitext(stl_file_name_input)
 
@@ -100,14 +100,14 @@ class GeoPreprocessor( GeoProcessor ):
         with open (stl_file_name_input) as read_file:
             for row in read_file.readlines():
                 row = row.split()
-                if (row[0] == "vertex" and all(self._isfloat(n) for n in row[1:])):
+                if (row[0] == "vertex" and all(self._IsFloat(n) for n in row[1:])):
                     # row[0] = "vertex"; row[1] = x coordinate; row[2] = y coordinate; row[3] = z coordinate
                     X_coord, Y_coord, Z_coord = [float(coord) for coord in row[1:]]
 
                     self.point_list.append([X_coord, Y_coord, Z_coord])
    
 
-    def write_stl(self, stl_file_name_output="", list_to_write=[]):
+    def WriteSTL(self, stl_file_name_output="", list_to_write=[]):
         # function to write a stl file from a list of points
 
         if (stl_file_name_output == ""):
@@ -131,7 +131,7 @@ class GeoPreprocessor( GeoProcessor ):
             p2 = self.point_list[pointer+1]
             p3 = self.point_list[pointer+2]
 
-            nx, ny, nz = self._normal_triangle(p1, p2, p3)
+            nx, ny, nz = self._NormalTriangle(p1, p2, p3)
 
             pointer += 3
 
@@ -153,7 +153,22 @@ class GeoPreprocessor( GeoProcessor ):
     # general functions
     #########################################################################
 
-    def cut(self, bounding_box=(-999999999.0, -999999999.0, 999999999.0, 999999999.0)):
+    def CentroidRect2D(self):
+        # function to compute the centroid in a rectangle in 2D
+
+        x_coords = []
+        y_coords = []
+        for coord in self.point_list:
+            x_coords.append(coord[0])
+            y_coords.append(coord[1])
+        
+        x_centr = max(x_coords)/2
+        y_centr = max(y_coords)/2
+
+        return (x_centr, y_centr)
+
+
+    def Cut(self, shift_geometry=False, bounding_box=(-999999999.0, -999999999.0, 999999999.0, 999999999.0)):
 
         x_min = bounding_box[0]
         y_min = bounding_box[1]
@@ -165,23 +180,32 @@ class GeoPreprocessor( GeoProcessor ):
         print( "x_max = " + str(x_max) )
         print( "y_max = " + str(y_max) )
         # num_lines = sum(1 for line in open( xyz_in_file ))
-        num_nodes = 0
 
         # the coordinates are already modified and (x=0,y=0) is located at x_min, y_min
+        del_list = []
         for num in range(len(self.point_list)):
             coord = self.point_list[num]
-            if ((x_min <= coord[0] <= x_max) and (y_min <= coord[1] <= y_max)):
+            if not ((x_min <= coord[0] <= x_max) and (y_min <= coord[1] <= y_max)):
+                # if coord it isn't in the bounding_box
+                del_list.append(coord)
+
+        for coord in del_list:
+            self.point_list.remove(coord)
+
+        if not self.point_list:
+            # if self.point_list is empty
+            KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given bounding box!")
+
+        if shift_geometry:
+            # shitf the origin in (x_min, y_min)
+            for num in range(len(self.point_list)):
+                coord = self.point_list[num]
                 coord[0] -= x_min
                 coord[1] -= y_min
                 self.point_list[num] = coord
 
-                num_nodes += 1
 
-        if num_nodes == 0:
-            KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given bounding box!")
-
-
-    def shift(self, x_shift, y_shift, z_shift=0):
+    def Shift(self, x_shift, y_shift, z_shift=0):
 
         print("Shift defined as")
         print("x_shift = ", x_shift)
@@ -196,7 +220,7 @@ class GeoPreprocessor( GeoProcessor ):
             self.point_list[num] = coord
     
 
-    def valley(self, max_height):
+    def ExtractValley(self, max_height):
 
         print("Maximal height defined as")
         print("max_height = ", max_height)
@@ -216,7 +240,7 @@ class GeoPreprocessor( GeoProcessor ):
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
 
 
-    def mountain(self, min_height):
+    def ExtractMountain(self, min_height):
 
         print("Minimal height defined as")
         print("min_height = ", min_height)
@@ -236,7 +260,7 @@ class GeoPreprocessor( GeoProcessor ):
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
 
 
-    def river(self, radius, start_x_y):
+    def ExtractRiver(self, radius, start_x_y):
 
         current_center = start_x_y                  # starting point for the search
         previous_center = (0.0, 0.0)
@@ -244,32 +268,9 @@ class GeoPreprocessor( GeoProcessor ):
         print("Starting identification of the river bed")
         print(" - starting point at ", current_center)
 
-        num_points = 0                              # used for checks
-        dict_all_points = {}                        # key = id, value = ( x, y, z )
         dict_active_points = {}                     # key = id, value = z
         list_visited_points = []                    # all points that have ever been active (index only)
                                                     # always extend( list_active_points )
-
-        # # reading all points into a single list out of tuples (x,y,z)
-        # # the coordinates are already modified and (x=0,y=0) is located at x_min, y_min
-        # with open( xyz_in_file ) as f:
-        #     lines = f.readlines()
-        #     for line in lines:
-        #         items = line.split()
-        #         if ( all( self._isfloat(n) for n in items) ):
-        #             x = float(items[0])
-        #             y = float(items[1])
-        #             z = float(items[2])
-        #             dict_all_points[num_points] = [x,y,z]
-        #             num_points += 1
-
-        for pnt_id in range(len(self.point_list)):
-            coord = self.point_list[pnt_id]
-            dict_all_points[num_points] = [coord[0], coord[1], coord[2]]
-            num_points += 1
-        
-        if num_points == 0:
-            KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
 
         num = 5                 # number of points for the search of minimal terrain height
         dist = radius / 4.0     # distance to shift the search circle
@@ -284,72 +285,55 @@ class GeoPreprocessor( GeoProcessor ):
             print( " --- Generation " + str(generation) + " --- ")
 
             # points inside the current search circle are set acitve
-            dict_active_points = self._find_points_within_radius( dict_all_points, current_center, radius )
+            dict_active_points = self._FindPointsWithinRadius( current_center, radius )
 
             # Check if still new unvisited points are found
-            if not self._iteration_goes_on( dict_active_points, list_visited_points ):
+            if not self._IterationGoesOn( dict_active_points, list_visited_points ):
                 print( " - No more unvisited points found - Assembly of the output in progress")
                 break
 
             # keeping track of the already visite points
             # these points mark the relevant terrain parts
-            list_visited_points = self._add_ids_of_active_points( list_visited_points, dict_active_points )
+            list_visited_points = self._AddIdsOfActivePoints( list_visited_points, dict_active_points )
             print( " - " + str( len(dict_active_points) ) + " active points" )
 
             # find the active points with the minimal terrain height
-            list_lowest_points = self._find_lowest_points( dict_active_points, num )
+            list_lowest_points = self._FindLowestPoints( dict_active_points, num )
 
             # moving the circle forward
             previous_center = current_center
-            current_center = self._compute_new_center( dict_all_points, list_lowest_points, dist, previous_center )
+            current_center = self._ComputeNewCenter( list_lowest_points, dist, previous_center )
             print( " - new center at " + str( current_center ) )
 
             # proceeding to the new generation
             generation += 1
             current_length += dist
 
-        # # extracting the "relevant part" marked by the visited points
-        # point_list = self._create_list_of_visited_points( list_visited_points, dict_all_points )
+        # extracting the "relevant part" marked by the visited points
+        self._CheckListOfVisitedPoints( list_visited_points )
 
-        ### CHECK THIS PART ###
-        # HERE WE FILLED (AGAIN) self.point_list
-        self.point_list = self._create_list_of_visited_points( list_visited_points, dict_all_points )
-
-
-    def centroid_rect2D(self):
-        # function to compute the centroid in a rectangle in 2D
-
-        x_coords = []
-        y_coords = []
-        for coord in self.point_list:
-            x_coords.append(coord[0])
-            y_coords.append(coord[1])
-        
-        x_centr = max(x_coords)/2
-        y_centr = max(y_coords)/2
-
-        return (x_centr, y_centr)
+        if not self.point_list:
+            # if self.point_list is empty
+            KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
 
 
     ########################################################################
     # auxiliary functions
     #########################################################################
 
-    def _create_list_of_visited_points( self, list_visited_points, dict_all_points ):
+    def _CheckListOfVisitedPoints( self, list_visited_points ):
 
-        point_list = []
+        del_list = []
+        for num in range(len(self.point_list)):
+            if id not in list_visited_points:
+                coord = self.point_list[num]
+                del_list.append(coord)
 
-        for p_id, coords in dict_all_points.items():
-            if ( p_id in list_visited_points ):
-                x = coords[0]
-                y = coords[1]
-                z = coords[2]
-                point_list.append([x,y,z])
-
-        return point_list
+        for coord in del_list:
+            self.point_list.remove(coord)
 
 
-    def _iteration_goes_on( self, dict_active_points, list_visited_points ):
+    def _IterationGoesOn( self, dict_active_points, list_visited_points ):
 
         goes_on = False
         counter = 0
@@ -365,14 +349,14 @@ class GeoPreprocessor( GeoProcessor ):
         return goes_on
 
 
-    def _compute_new_center( self, dict_all_points, list_lowest_points, dist, previous_center ):
+    def _ComputeNewCenter( self, list_lowest_points, dist, previous_center ):
 
         x_list = []
         y_list = []
         current_center = (0.0, 0.0)
 
         for p_id in list_lowest_points:
-            coords = dict_all_points[p_id]
+            coords = self.point_list[p_id]
             x = coords[0]
             y = coords[1]
             x_list.append( x )
@@ -388,11 +372,12 @@ class GeoPreprocessor( GeoProcessor ):
         return current_center
 
 
-    def _find_points_within_radius( self, dict_all_points, current_center, radius ):
+    def _FindPointsWithinRadius( self, current_center, radius ):
 
         dict_active_points = {}
 
-        for id, coords in dict_all_points.items():
+        for id in range(len(self.point_list)):
+            coords = self.point_list[id]
             x = coords[0]
             y = coords[1]
             dist = math.sqrt( ( x - current_center[0] )**2 + ( y - current_center[1] )**2 )
@@ -400,11 +385,11 @@ class GeoPreprocessor( GeoProcessor ):
             if dist < radius:
                 z = coords[2]
                 dict_active_points[ id ] = z
-
+        
         return dict_active_points
 
 
-    def _add_ids_of_active_points( self, list_visited_points, dict_active_points ):
+    def _AddIdsOfActivePoints( self, list_visited_points, dict_active_points ):
 
         for id, _ in dict_active_points.items():
             if ( not ( id in list_visited_points ) ):
@@ -413,7 +398,7 @@ class GeoPreprocessor( GeoProcessor ):
         return list_visited_points
 
 
-    def _find_lowest_points( self, dict_active_points, num ):
+    def _FindLowestPoints( self, dict_active_points, num ):
 
         sorted_list_active_points = sorted( dict_active_points.items(), key=lambda kv: kv[1] )
 
@@ -427,12 +412,12 @@ class GeoPreprocessor( GeoProcessor ):
         return ids_of_lowest_points
 
 
-    def _dist( self, x_start, y_start, x, y ):
+    def _Dist( self, x_start, y_start, x, y ):
         squares = (x - x_start)**2 + (y - y_start)**2
         return math.sqrt( squares )
 
 
-    def _isfloat( self, value):
+    def _IsFloat( self, value):
         try:
             float( value )
             return True
@@ -440,7 +425,7 @@ class GeoPreprocessor( GeoProcessor ):
             return False
 
 
-    def _normal_triangle(self, P1, P2, P3):
+    def _NormalTriangle(self, P1, P2, P3):
         # function to calc the normal of the triangle
         
         nx = (P2[1]-P1[1])*(P3[2]-P1[2]) - (P3[1]-P1[1])*(P2[2]-P1[2])
@@ -459,7 +444,7 @@ class GeoPreprocessor( GeoProcessor ):
         return (nx, ny, nz)
 
 
-    def _remove_duplicate(self, point_list):
+    def _RemoveDuplicate(self, point_list):
         no_duplicate = []        # point_list without duplicates
         for point in point_list:
             if (point not in no_duplicate):
