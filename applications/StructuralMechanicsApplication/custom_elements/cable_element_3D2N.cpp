26c866f6da1a3215e35dc7e47e846255d2b138e8
Klaus Bernd Sautter <klaus.sautter@tum.de>
2019-02-15 16:22:04 +0100

    explicit only rhs & slid,ring with lhs again

--- applications/StructuralMechanicsApplication/custom_elements/cable_element_3D2N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/cable_element_3D2N.cpp
@@ -134,6 +134,42 @@ void CableElement3D2N::UpdateInternalForces(
   KRATOS_CATCH("");
 }
 
+
+void CableElement3D2N::CalculateOnIntegrationPoints(
+    const Variable<Vector> &rVariable, std::vector<Vector> &rOutput,
+    const ProcessInfo &rCurrentProcessInfo) {
+  KRATOS_TRY
+  const GeometryType::IntegrationPointsArrayType &integration_points =
+      GetGeometry().IntegrationPoints();
+  if (rOutput.size() != integration_points.size()) {
+    rOutput.resize(integration_points.size());
+  }
+  if (rVariable == GREEN_LAGRANGE_STRAIN_VECTOR) {
+    Vector strain = ZeroVector(msDimension);
+    strain[0] = this->CalculateGreenLagrangeStrain();
+    strain[1] = 0.00;
+    strain[2] = 0.00;
+    rOutput[0] = strain;
+  }
+  if ((rVariable == PK2_STRESS_VECTOR) && !this->mIsCompressed) {
+
+    array_1d<double, 3 > truss_stresses;
+    array_1d<double, msDimension> temp_internal_stresses = ZeroVector(msDimension);
+    ProcessInfo temp_process_information;
+
+    ConstitutiveLaw::Parameters Values(this->GetGeometry(),this->GetProperties(),temp_process_information);
+    Vector temp_strain = ZeroVector(1);
+    temp_strain[0] = this->CalculateGreenLagrangeStrain();
+    Values.SetStrainVector(temp_strain);
+    this->mpConstitutiveLaw->CalculateValue(Values,FORCE,temp_internal_stresses);
+
+    rOutput[0] = temp_internal_stresses;
+  }
+
+
+  KRATOS_CATCH("")
+}
+
 void CableElement3D2N::save(Serializer &rSerializer) const {
   KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, TrussElement3D2N);
   rSerializer.save("mIscompressed", this->mIsCompressed);
