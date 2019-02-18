f041238098e113a349835d6584748656f7d98a98
kevin braun <cd>
2019-02-18 15:19:20 +0100

    18.02.19: derivative builder: change of the structure

--- applications/StructuralMechanicsApplication/state_derivative/response_functions/derivative_builder.h
+++ applications/StructuralMechanicsApplication/state_derivative/response_functions/derivative_builder.h
@@ -24,6 +24,7 @@
 #include "includes/process_info.h"
 #include "custom_response_functions/response_utilities/stress_response_definitions.h"
 #include "includes/define.h"
+#include "state_derivative/output_utilities/output_utility.h"
 
 
 namespace Kratos
@@ -45,19 +46,41 @@ public:
 
     typedef Element::DofsVectorType DofsVectorType;
 
-           
+    typedef VariableComponent<VectorComponentAdaptor<array_1d<double, 3>>> VariableComponentType;
+
+    
+    // Calculate the derivative of the response variable and assemble the results in a vector        
     template <typename TDataType>
-    static void ComputeDerivative(Element& rDirectElement, 
+    static void ComputeDerivative(const std::string& DerivativeFlag,
+                                Element& rDirectElement, 
                                 Variable<TDataType> const& rResponseVariable,
-                                const Variable<Matrix >& rDerivativeVariable,
                                 std::vector<std::vector<TDataType>>& rOutput, 
                                 const ProcessInfo rCurrentProcessInfo)
-    {
+    {   
+        if (rResponseVariable == MOMENT)
+        {
+            std::array<std::string, 3> moments = { std::string("MX"), std::string("MY"), std::string("MZ") };
+            DeriveStressVariable(DerivativeFlag, rDirectElement, moments , rResponseVariable, rOutput, rCurrentProcessInfo);
+        }        
+    }
+    
+    
+    
+private:
+
+    template <typename TDataType>
+    static void DeriveStressVariable(const std::string& DerivativeFlag,
+                                Element& rDirectElement,                                
+                                const std::array<std::string, 3>& rTracedStresses,
+                                Variable<TDataType> const& rResponseVariable,
+                                std::vector<std::vector<TDataType>>& rOutput, 
+                                const ProcessInfo rCurrentProcessInfo)
+    {        
         // Define working variables
         Matrix DerivativeMatrix;
         DerivativeMatrix.clear();
-        std::vector<TDataType> dummy_vector; 
-        
+        std::vector<TDataType> dummy_vector;
+
         // To get the number of Dofs
         DofsVectorType dofs_of_element;    
         ProcessInfo process_info = rCurrentProcessInfo;    
@@ -66,69 +89,39 @@ public:
         // To get the number of integration points
         rDirectElement.CalculateOnIntegrationPoints(rResponseVariable, dummy_vector, rCurrentProcessInfo);
         
-        // Define seizes
-        const SizeType num_gp = dummy_vector.size();
-        SizeType num_derivatives;
-        if(rDerivativeVariable == STRESS_DISP_DERIV_ON_GP)
-            num_derivatives = dofs_of_element.size();
-        else if (rDerivativeVariable == STRESS_DESIGN_DERIVATIVE_ON_GP)
-            num_derivatives = 1;
-        else
-        {
-            num_derivatives = 0;
-            KRATOS_ERROR << "Response Variable can only get derived by the initial state results or by a design variable!" << std::endl;
-        }
-           
         // Size rOutput
-        rOutput.resize(num_derivatives);
+        if(DerivativeFlag == "DISPLACEMENT_DERIVATIVE")
+            rOutput.resize( dofs_of_element.size() );
+        else if (DerivativeFlag == "DESIGN_VARIABLE_DERIVATIVE")
+            rOutput.resize(1);
+        else
+            KRATOS_ERROR << "Response Variable can only get derived by the initial state results or by a design variable!" << std::endl;
+                
         for (IndexType i = 0; i < rOutput.size(); ++i)
-            rOutput[i].resize(num_gp);
-
-        // Calculate the derivative of the response variable and assemble the results in a vector 
-        if (rResponseVariable == MOMENT)
-        {
-            std::array<std::string, 3> moments = { std::string("MX"), std::string("MY"), std::string("MZ") };
-            DeriveStressVariable(moments, rDirectElement, rResponseVariable, rDerivativeVariable, rOutput, rCurrentProcessInfo);
-        }
-    }
-    
-    
-    
-private:
-
-    template <typename TDataType>
-    static void DeriveStressVariable(std::array<std::string, 3>& rTracedStresses,
-                                Element& rDirectElement, 
-                                Variable<TDataType> const& rResponseVariable,
-                                const Variable<Matrix >& rDerivativeVariable,
-                                std::vector<std::vector<TDataType>>& rOutput, 
-                                const ProcessInfo rCurrentProcessInfo)
-    {
-        // Define working variables
-        Matrix DerivativeMatrix;
-        DerivativeMatrix.clear();
-
-        // Define sizes
+            rOutput[i].resize( dummy_vector.size() );
+        
+        // Define sizes        
         const SizeType num_derivatives = rOutput.size();
         const SizeType num_gp = rOutput[0].size();
-
+        
         for (IndexType dir_it = 0; dir_it < 3; ++dir_it)
         {   
             TracedStressType traced_stress = StressResponseDefinitions::ConvertStringToTracedStressType(rTracedStresses[dir_it]);  
             rDirectElement.SetValue(TRACED_STRESS_TYPE, static_cast<int>(traced_stress));
-
-            if(rDerivativeVariable == STRESS_DISP_DERIV_ON_GP)
+            
+            if(DerivativeFlag == "DISPLACEMENT_DERIVATIVE")
                 rDirectElement.Calculate(STRESS_DISP_DERIV_ON_GP, DerivativeMatrix, rCurrentProcessInfo);
-            else if (rDerivativeVariable == STRESS_DESIGN_DERIVATIVE_ON_GP)
+            else if (DerivativeFlag == "DESIGN_VARIABLE_DERIVATIVE")
                 rDirectElement.Calculate(STRESS_DESIGN_DERIVATIVE_ON_GP, DerivativeMatrix, rCurrentProcessInfo);
 
             for (IndexType deriv_it = 0; deriv_it < num_derivatives; ++deriv_it)
                 for(IndexType gp_it = 0; gp_it < num_gp; ++gp_it)
-                    rOutput[deriv_it][gp_it][dir_it] = DerivativeMatrix(deriv_it, gp_it);
-                
+                    rOutput[deriv_it][gp_it][dir_it] = DerivativeMatrix(deriv_it, gp_it);  
+               
             DerivativeMatrix.clear();
-        }        
+        }    
     }
+
     
     
 }; // class DerivativeBuilder
