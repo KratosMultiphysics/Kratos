ebf6215fa01c90cfca176066fa7fcd652b4ab075
riccardotosi <rtosi@cimne.upc.edu>
2019-02-23 18:32:42 +0100

    Use # instead of '

--- applications/MultilevelMonteCarloApplication/test_examples/poisson_square_2d/simulation_definition.py
+++ applications/MultilevelMonteCarloApplication/test_examples/poisson_square_2d/simulation_definition.py
@@ -14,15 +14,20 @@ from  KratosMultiphysics.analysis_stage import AnalysisStage
 KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
 
 
-'''
-This Analysis Stage implementation solves the elliptic PDE in (0,1)^2 with zero Dirichlet boundary conditions
--lapl(u) = xi*f,    f= -432*x*(x-1)*y*(y-1)
-                    f= -432*(x**2+y**2-x-y)
-where xi is a Beta(2,6) random variable, and computes statistic of the QoI
-Q = int_(0,1)^2 u(x,y)dxdy
-'''
+"""
+This Analysis Stage solves the elliptic PDE in domain = (0,1)^2 with zero Dirichlet boundary conditions
+-lapl(u) = \varepsilon*f    u \in domain
+u = 0                       u \in \partial(domain)
+where
+\varepsilon \sim Beta(2,6)
+and f may be
+f= -432*x*(x-1)*y*(y-1)
+or
+f= -432*(x**2+y**2-x-y)
+the QoI is
+QoI = \int_(domain) u(x,y)dxdy
+"""
 class SimulationScenario(AnalysisStage):
-    '''Main analysis stage for Monte Carlo simulations'''
     def __init__(self,input_model,input_parameters,sample):
         self.sample = sample
         super(SimulationScenario,self).__init__(input_model,input_parameters)
@@ -32,7 +37,10 @@ class SimulationScenario(AnalysisStage):
         import convection_diffusion_stationary_solver
         return convection_diffusion_stationary_solver.CreateSolver(self.model,self.project_parameters["solver_settings"])
 
-    '''Introduce here the stochasticity in the right hand side defining the forcing function and apply the stochastic contribute'''
+    """
+    function introducing the stochasticity in the right hand side
+    input:  self: an instance of the class
+    """
     def ModifyInitialProperties(self):
         for node in self.model.GetModelPart("MLMCLaplacianModelPart").Nodes:
             coord_x = node.X
@@ -41,10 +49,11 @@ class SimulationScenario(AnalysisStage):
             forcing = -432.0 * (coord_x**2 + coord_y**2 - coord_x - coord_y) # this forcing presents the below commented analytical solution
             node.SetSolutionStepValue(KratosMultiphysics.HEAT_FLUX,forcing*self.sample)
 
-    '''
+    """
     function evaluating the QoI of the problem: int_{domain} TEMPERATURE(x,y) dx dy
     midpoint rule used to compute the integral
-    '''
+    input:  self: an instance of the class
+    """
     def EvaluateQuantityOfInterest(self):
         KratosMultiphysics.CalculateNodalAreaProcess(self._GetSolver().main_model_part,2).Execute()
         Q = 0.0
