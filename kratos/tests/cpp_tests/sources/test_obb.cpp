1b91eb2b553d97782083d3ffb03a21702eb41920
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-02-23 20:18:05 +0100

    [skip ci] Better test

--- kratos/tests/cpp_tests/sources/test_obb.cpp
+++ kratos/tests/cpp_tests/sources/test_obb.cpp
@@ -105,7 +105,49 @@ namespace Kratos {
 //         gid_io.InitializeResults(0, r_main_model_part.GetMesh());
 //         gid_io.FinalizeResults();
 
-        const bool has_intersection = first_obb.HasIntersection(second_obb);
+        bool has_intersection = first_obb.HasIntersection(second_obb);
+
+        KRATOS_CHECK_EQUAL(has_intersection_reference, has_intersection);
+
+        // Moving up tyhe second OBBHasIntersection2D
+        second_center[1] = 0.5;
+        second_obb.SetCenter(second_center);
+        second_quad = second_obb.GetEquiavelentGeometry();
+
+//         // Debug
+//         Model current_model;
+//         ModelPart& r_main_model_part = current_model.CreateModelPart("Main");
+//         ModelPart& r_model_part1 = r_main_model_part.CreateSubModelPart("1");
+//         ModelPart& r_model_part2 = r_main_model_part.CreateSubModelPart("2");
+//
+//         for (int i = 0; i < 4; ++i) {
+//             r_model_part1.CreateNewNode(i + 1, first_quad[i].X(), first_quad[i].Y(), first_quad[i].Z());
+//             r_model_part2.CreateNewNode(i + 5, second_quad[i].X(), second_quad[i].Y(), second_quad[i].Z());
+//         }
+//
+//         Properties::Pointer p_properties_0 = Kratos::make_shared<Properties>(0);
+//         Properties::Pointer p_properties_1 = Kratos::make_shared<Properties>(1);
+//
+//         r_model_part1.CreateNewElement("Element3D4N", 1, {1, 2, 3, 4}, p_properties_0);
+//         r_model_part2.CreateNewElement("Element3D4N", 2, {5, 6, 7, 8}, p_properties_1);
+//
+//         GidIO<> gid_io("test", GiD_PostBinary, SingleFile, WriteDeformed, WriteConditions);
+//         gid_io.InitializeMesh(0.0);
+//         gid_io.WriteMesh(r_main_model_part.GetMesh());
+//         gid_io.FinalizeMesh();
+//         gid_io.InitializeResults(0, r_main_model_part.GetMesh());
+//         gid_io.FinalizeResults();
+
+        has_intersection_reference = false;
+
+        for (auto& r_point : first_quad) {
+            if (second_quad.IsInside(r_point.Coordinates(), local_coords)) {
+                has_intersection_reference = true;
+                break;
+            }
+        }
+
+        has_intersection = first_obb.HasIntersection(second_obb);
 
         KRATOS_CHECK_EQUAL(has_intersection_reference, has_intersection);
     }
