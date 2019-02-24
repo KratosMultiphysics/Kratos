0a74f0f6ecbd992e9f5ecf69cad25e3db6f87a25
marcnunezc <mnunez@cimne.upc.edu>
2019-02-24 21:42:02 +0100

    Editing cmake list and custom python

--- applications/CompressiblePotentialFlowApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_python/add_custom_processes_to_python.cpp
@@ -19,9 +19,12 @@
 #include "includes/define.h"
 #include "custom_python/add_custom_processes_to_python.h"
 #include "custom_processes/kutta_condition_process.h"
+#include "custom_processes/metrics_potential_hessian_process.h"
 
 namespace Kratos {
 namespace Python {
+
+typedef VariableComponent< VectorComponentAdaptor<array_1d<double, 3> > > ComponentType;
 
 void  AddCustomProcessesToPython(pybind11::module& m)
 {
@@ -31,7 +34,16 @@ void  AddCustomProcessesToPython(pybind11::module& m)
         (m, "KuttaConditionProcess")
         .def(py::init<ModelPart&>())
         ;
-}
+    // HESSIAN PROCESS
+        py::class_<ComputePotentialHessianSolMetricProcess, ComputePotentialHessianSolMetricProcess::Pointer, Process>
+        (m, "ComputePotentialHessianSolMetricProcess")
+        .def(py::init<ModelPart&, Variable<double>&>())
+        .def(py::init<ModelPart&, Variable<double>&, Parameters>())
+        .def(py::init<ModelPart&, ComponentType&>())
+        .def(py::init<ModelPart&, ComponentType&, Parameters>())
+        ;
+
+        m.attr("ComputeHessianSolMetricProcess2D") = m.attr("ComputeHessianSolMetricProcess");
 
 }  // namespace Python.
 
