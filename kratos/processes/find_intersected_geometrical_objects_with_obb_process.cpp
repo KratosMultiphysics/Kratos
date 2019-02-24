160e2d88351c93c5f7942ab81a894655d72f2bb9
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-02-24 21:12:20 +0100

    Computing OBB in 3D

--- kratos/processes/find_intersected_geometrical_objects_with_obb_process.cpp
+++ kratos/processes/find_intersected_geometrical_objects_with_obb_process.cpp
@@ -205,61 +205,95 @@ bool FindIntersectedGeometricalObjectsWithOBBProcess<TEntity>::HasIntersection3D
     GeometryType& rSecondGeometry
     )
 {
-//     // The local dimensions
-//     const std::size_t local_dimension_1 = rFirstGeometry.LocalSpaceDimension();
-//     const std::size_t local_dimension_2 = rSecondGeometry.LocalSpaceDimension();
-//
-//     // The faces
-//     PointerVector<GeometryType> r_faces_1 = rFirstGeometry.Faces();
-//     const std::size_t number_of_faces_1 = (local_dimension_1 < 2) ? 1 : r_faces_1.size();
-//
-//     PointerVector<GeometryType> r_faces_2 = rSecondGeometry.Faces();
-//     const std::size_t number_of_faces_2 = (local_dimension_2 < 2) ? 1 : r_faces_2.size();
-//
-//     // First geometry
-//     for (std::size_t i_1 = 0; i_1 < number_of_faces_1; ++i_1) {
-//         auto& r_face_1 = *(r_faces_1.begin() + i_1);
-//
-//         // Check the intersection of each face of the object bounding box against the intersecting object bounding box
-//         NodeType first_geometry_low_node, first_geometry_high_node;
-//         r_face_1.BoundingBox(first_geometry_low_node, first_geometry_high_node);
-//
-//         // Creating OBB
-//         array_1d<double, 3> first_direction_vector = first_geometry_high_node - first_geometry_low_node;
-//         const double norm_first_direction_vector = norm_2(first_direction_vector);
-//         first_direction_vector /= norm_first_direction_vector;
-//         const array_1d<double, 3> first_center_point = 0.5 * (first_geometry_low_node.Coordinates() + first_geometry_high_node.Coordinates());
-//         array_1d<double, 3> first_half_distances;
-//         first_half_distances[0] = 0.5 * norm_first_direction_vector + mBoundingBoxFactor;
-//         first_half_distances[1] = mBoundingBoxFactor;
-//         first_half_distances[2] = mBoundingBoxFactor;
-//         OBB<3> first_obb(first_center_point, first_direction_vector, first_half_distances);
-//
-//         // Second geometry
-//         for (std::size_t i_2 = 0; i_2 < number_of_faces_2; ++i_2) {
-//             auto& r_face_2 = *(r_faces_2.begin() + i_2);
-//
-//             // Check the intersection of each face of the object bounding box against the intersecting object bounding box
-//             NodeType second_geometry_low_node, second_geometry_high_node;
-//             r_face_2.BoundingBox(second_geometry_low_node, second_geometry_high_node);
-//
-//             // Creating OBB
-//             array_1d<double, 3> second_direction_vector = second_geometry_high_node - second_geometry_low_node;
-//             const double norm_second_direction_vector = norm_2(second_direction_vector);
-//             second_direction_vector /= norm_second_direction_vector;
-//             const array_1d<double, 3> second_center_point = 0.5 * (second_geometry_low_node.Coordinates() + second_geometry_high_node.Coordinates());
-//             array_1d<double, 3> second_half_distances;
-//             second_half_distances[0] = 0.5 * norm_second_direction_vector + mBoundingBoxFactor;
-//             second_half_distances[1] = mBoundingBoxFactor;
-//             second_half_distances[2] = mBoundingBoxFactor;
-//             OBB<3> second_obb(second_center_point, second_direction_vector, second_half_distances);
-//
-//             // Computing intersection OBB
-//             if (first_obb.HasIntersection(second_obb)){
-//                 return true;
-//             }
-//         }
-//     }
+    // The local dimensions
+    const std::size_t local_dimension_1 = rFirstGeometry.LocalSpaceDimension();
+    const std::size_t local_dimension_2 = rSecondGeometry.LocalSpaceDimension();
+
+    // The faces
+    PointerVector<GeometryType> r_faces_1 = rFirstGeometry.Faces();
+    const std::size_t number_of_faces_1 = (local_dimension_1 < 2) ? 1 : r_faces_1.size();
+
+    PointerVector<GeometryType> r_faces_2 = rSecondGeometry.Faces();
+    const std::size_t number_of_faces_2 = (local_dimension_2 < 2) ? 1 : r_faces_2.size();
+
+    // First geometry
+    for (std::size_t i_1 = 0; i_1 < number_of_faces_1; ++i_1) {
+        auto& r_face_1 = *(r_faces_1.begin() + i_1);
+
+        // Check the intersection of each face of the object bounding box against the intersecting object bounding box
+        NodeType first_geometry_low_node, first_geometry_high_node;
+        r_face_1.BoundingBox(first_geometry_low_node, first_geometry_high_node);
+
+        // Creating OBB
+        array_1d<array_1d<double, 3>, 3> first_direction_vector;
+        noalias(first_direction_vector[0]) = first_geometry_high_node - first_geometry_low_node;
+        const double norm_first_direction_vector = norm_2(first_direction_vector[0]);
+        first_direction_vector[0] /= norm_first_direction_vector;
+        MathUtils<double>::OrthonormalBasis(first_direction_vector[0], first_direction_vector[1], first_direction_vector[2]);
+        const array_1d<double, 3> first_center_point = 0.5 * (first_geometry_low_node.Coordinates() + first_geometry_high_node.Coordinates());
+        array_1d<double, 3> first_half_distances;
+        first_half_distances[0] = 0.5 * norm_first_direction_vector + mBoundingBoxFactor;
+        double distance_1 = 0;
+        double distance_2 = 0;
+        for (auto& r_node : r_face_1) {
+            if (r_node.Id() != first_geometry_low_node.Id() && r_node.Id() != first_geometry_high_node.Id()) {
+                const array_1d<double, 3> vector_points = r_node.Coordinates() - first_center_point;
+                distance_1 = std::max(distance_1, inner_prod(vector_points, first_direction_vector[1]));
+                distance_2 = std::max(distance_2, inner_prod(vector_points, first_direction_vector[2]));
+            }
+        }
+        first_half_distances[1] = distance_1 + mBoundingBoxFactor;
+        first_half_distances[2] = distance_2 + mBoundingBoxFactor;
+        OBB<3> first_obb(first_center_point, first_direction_vector, first_half_distances);
+
+        // We create new elements for debugging
+        if (mDebugOBB) {
+            auto p_prop = this->GetModelPart1().pGetProperties(1001);
+            CreateDebugOBB3D(this->GetModelPart1(), p_prop, first_obb);
+        }
+
+        // Second geometry
+        for (std::size_t i_2 = 0; i_2 < number_of_faces_2; ++i_2) {
+            auto& r_face_2 = *(r_faces_2.begin() + i_2);
+
+            // Check the intersection of each face of the object bounding box against the intersecting object bounding box
+            NodeType second_geometry_low_node, second_geometry_high_node;
+            r_face_2.BoundingBox(second_geometry_low_node, second_geometry_high_node);
+
+            // Creating OBB
+            array_1d<array_1d<double, 3>, 3> second_direction_vector;
+            noalias(second_direction_vector[0]) = second_geometry_high_node - second_geometry_low_node;
+            const double norm_second_direction_vector = norm_2(second_direction_vector[0]);
+            second_direction_vector[0] /= norm_second_direction_vector;
+            MathUtils<double>::OrthonormalBasis(second_direction_vector[0], second_direction_vector[1], second_direction_vector[2]);
+            const array_1d<double, 3> second_center_point = 0.5 * (second_geometry_low_node.Coordinates() + second_geometry_high_node.Coordinates());
+            array_1d<double, 3> second_half_distances;
+            second_half_distances[0] = 0.5 * norm_second_direction_vector + mBoundingBoxFactor;
+            distance_1 = 0;
+            distance_2 = 0;
+            for (auto& r_node : r_face_2) {
+                if (r_node.Id() != second_geometry_low_node.Id() && r_node.Id() != second_geometry_high_node.Id()) {
+                    const array_1d<double, 3> vector_points = r_node.Coordinates() - second_center_point;
+                    distance_1 = std::max(distance_1, inner_prod(vector_points, second_direction_vector[1]));
+                    distance_2 = std::max(distance_2, inner_prod(vector_points, second_direction_vector[2]));
+                }
+            }
+            second_half_distances[1] = distance_1 + mBoundingBoxFactor;
+            second_half_distances[2] = distance_2 + mBoundingBoxFactor;
+            OBB<3> second_obb(second_center_point, second_direction_vector, second_half_distances);
+
+            // We create new elements for debugging
+            if (mDebugOBB) {
+                auto p_prop = this->GetModelPart2().pGetProperties(1002);
+                CreateDebugOBB3D(this->GetModelPart2(), p_prop, second_obb);
+            }
+
+            // Computing intersection OBB
+            if (first_obb.HasIntersection(second_obb)){
+                return true;
+            }
+        }
+    }
 
     return false;
 }
