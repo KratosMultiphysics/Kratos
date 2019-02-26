537d32d55c58f030112e6b6b09431c557857f5de
marcnunezc <marcnc94@gmail.com>
2019-02-26 16:34:33 +0100

    Adding custom nodal gradient process

--- applications/CompressiblePotentialFlowApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_python/add_custom_processes_to_python.cpp
@@ -20,6 +20,7 @@
 #include "custom_python/add_custom_processes_to_python.h"
 #include "custom_processes/kutta_condition_process.h"
 #include "custom_processes/metrics_potential_hessian_process.h"
+#include "custom_processes/compute_custom_nodal_gradient_process.h"
 
 namespace Kratos {
 namespace Python {
@@ -37,13 +38,21 @@ void  AddCustomProcessesToPython(pybind11::module& m)
     // HESSIAN PROCESS
         py::class_<ComputePotentialHessianSolMetricProcess, ComputePotentialHessianSolMetricProcess::Pointer, Process>
         (m, "ComputePotentialHessianSolMetricProcess")
-        .def(py::init<ModelPart&, Variable<double>&>())
-        .def(py::init<ModelPart&, Variable<double>&, Parameters>())
-        .def(py::init<ModelPart&, ComponentType&>())
-        .def(py::init<ModelPart&, ComponentType&, Parameters>())
+        .def(py::init<ModelPart&, Parameters>())
+        ;
+        m.attr("ComputePotentialHessianSolMetricProcess2D") = m.attr("ComputePotentialHessianSolMetricProcess");
+
+        /* Historical */
+        py::class_<ComputeCustomNodalGradient< ComputeCustomNodalGradientSettings::SaveAsHistoricalVariable>, ComputeCustomNodalGradient<ComputeCustomNodalGradientSettings::SaveAsHistoricalVariable>::Pointer, Process>(m,"ComputeCustomNodalGradientProcess")
+        .def(py::init<ModelPart&, Variable<array_1d<double,3> >& , Variable<double>& >())
         ;
 
-        m.attr("ComputeHessianSolMetricProcess2D") = m.attr("ComputeHessianSolMetricProcess");
+        /* Non-Historical */
+        py::class_<ComputeCustomNodalGradient<ComputeCustomNodalGradientSettings::SaveAsNonHistoricalVariable>, ComputeCustomNodalGradient<ComputeCustomNodalGradientSettings::SaveAsNonHistoricalVariable>::Pointer, Process>(m,"ComputeNonHistoricalCustomNodalGradientProcess")
+        .def(py::init<ModelPart&, Variable<array_1d<double,3> >& , Variable<double>& >())
+        ;
+
+}
 
 }  // namespace Python.
 
