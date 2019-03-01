282748a9940e3a5a4d9733b0fa90806882d1bbb5
riccardotosi <rtosi@cimne.upd.edu>
2019-03-01 17:36:53 +0100

    Add temporarily to run compressible before generalizing

---
+++ applications/MultilevelMonteCarloApplication/python_scripts/compressible_generator_utilities.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# TODO: move in an external file and create a generator class, e.g. MonteCarlo.generator.Generate()
+
+'''
+function generatng the random variable
+'''
+def GenerateSample():
+    # alpha = 2.0
+    # beta = 6.0
+    # number_samples = 1
+    # sample = np.random.beta(alpha,beta,number_samples)
+    # return sample
+    sample = []
+    mean_Mach = 0.3
+    std_deviation_Mach = 0.01
+    number_samples = 1
+    sample.append(np.random.normal(mean_Mach,std_deviation_Mach,number_samples))
+    mean_angle_attack = 0.0 # [rad] = 0 [degrees] airfoil already has 5 degrees
+    std_deviation_angle_attack = np.deg2rad(0.1)
+    sample.append(np.random.normal(mean_angle_attack,std_deviation_angle_attack,number_samples))
+    if sample[0] >= 1.0 or sample[0] <= 0.0 :
+        raise Exception ("stochastic Mach number computed > 1 or < 0")
+    return sample
