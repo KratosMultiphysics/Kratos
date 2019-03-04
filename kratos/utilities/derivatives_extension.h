d9b617fd273c08be323b6264d3170b32cc5dcb6b
sunethwarna <suneth.warna@gmail.com>
2019-03-04 23:29:56 +0100

    adding error messages to detect unimplemnted but used methods

--- kratos/utilities/derivatives_extension.h
+++ kratos/utilities/derivatives_extension.h
@@ -48,6 +48,11 @@ public:
                                           std::size_t Step,
                                           ProcessInfo& rCurrentProcessInfo)
     {
+        KRATOS_THROW_ERROR(
+            std::runtime_error,
+            "Calling DerivativesExtension base class GetZeroDerivativesVector "
+            "method. Please implement it in derrived class.",
+            "");
     }
 
     virtual void GetFirstDerivativesVector(std::size_t NodeId,
@@ -55,6 +60,11 @@ public:
                                            std::size_t Step,
                                            ProcessInfo& rCurrentProcessInfo)
     {
+        KRATOS_THROW_ERROR(
+            std::runtime_error,
+            "Calling DerivativesExtension base class GetFirstDerivativesVector "
+            "method. Please implement it in derrived class.",
+            "");
     }
 
     virtual void GetSecondDerivativesVector(std::size_t NodeId,
@@ -62,39 +72,74 @@ public:
                                             std::size_t Step,
                                             ProcessInfo& rCurrentProcessInfo)
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetSecondDerivativesVector method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual void GetZeroDerivativesDofsVector(std::size_t NodeId,
                                               std::vector<Dof<double>::Pointer>& rVector,
                                               ProcessInfo& rCurrentProcessInfo)
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetZeroDerivativesDofsVector method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual void GetFirstDerivativesDofsVector(std::size_t NodeId,
                                                std::vector<Dof<double>::Pointer>& rVector,
                                                ProcessInfo& rCurrentProcessInfo)
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetFirstDerivativesDofsVector method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual void GetSecondDerivativesDofsVector(std::size_t NodeId,
                                                 std::vector<Dof<double>::Pointer>& rVector,
                                                 ProcessInfo& rCurrentProcessInfo)
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetSecondDerivativesDofsVector method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual void GetZeroDerivativesVariables(std::vector<VariableData const*>& rVariables,
-                                                   ProcessInfo& rCurrentProcessInfo)
+                                             ProcessInfo& rCurrentProcessInfo) const
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetZeroDerivativesVariables method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual void GetFirstDerivativesVariables(std::vector<VariableData const*>& rVariables,
-                                                    ProcessInfo& rCurrentProcessInfo)
+                                              ProcessInfo& rCurrentProcessInfo) const
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetFirstDerivativesVariables method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual void GetSecondDerivativesVariables(std::vector<VariableData const*>& rVariables,
-                                                     ProcessInfo& rCurrentProcessInfo)
+                                               ProcessInfo& rCurrentProcessInfo) const
     {
+        KRATOS_THROW_ERROR(std::runtime_error,
+                           "Calling DerivativesExtension base class "
+                           "GetSecondDerivativesVariables method. Please "
+                           "implement it in derrived class.",
+                           "");
     }
 
     virtual std::ostream& Print(std::ostream& os) const
