5a75c834c5ae96d5dcc05464eed771be0ecf846f
DB <daniel.baumgaertner@tum.de>
2019-03-05 16:41:18 +0100

    introduced skipping check to unit tests

--- applications/ShapeOptimizationApplication/tests/shape_optimization_test_factory.py
+++ applications/ShapeOptimizationApplication/tests/shape_optimization_test_factory.py
@@ -7,6 +7,12 @@ import KratosMultiphysics.kratos_utilities as kratos_utilities
 # Other imports
 import os
 
+# Check if external Apps are available
+has_eigen_app =  kratos_utilities.CheckIfApplicationsAvailable("EigenSolversApplication")
+has_csm_app = kratos_utilities.CheckIfApplicationsAvailable("StructuralMechanicsApplication")
+has_mesh_moving_app = kratos_utilities.CheckIfApplicationsAvailable("MeshMovingApplication")
+
+# Define controlled execution scope
 class controlledExecutionScope:
     def __init__(self, scope):
         self.currentPath = os.getcwd()
@@ -44,30 +50,41 @@ class opt_process_vertex_morphing_test(ShapeOptimizationTestFactory):
     execution_directory = "opt_process_vertex_morphing_test"
     execution_file = "run_test"
 
+class mapper_test(ShapeOptimizationTestFactory):
+    execution_directory = "mapper_test"
+    execution_file = "run_test"
+
+@KratosUnittest.skipUnless(has_csm_app,"Missing required application: StructuralMechanicsApplication")
 class opt_process_shell_test(ShapeOptimizationTestFactory):
     execution_directory = "opt_process_shell_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app and has_mesh_moving_app,"Missing (one or all) required applications: StructuralMechanicsApplication, MeshMovingApplication")
 class opt_process_solid_test(ShapeOptimizationTestFactory):
     execution_directory = "opt_process_solid_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app and has_eigen_app,"Missing (one or all) required applications: StructuralMechanicsApplication, EigenSolversApplication")
 class opt_process_eigenfrequency_test(ShapeOptimizationTestFactory):
     execution_directory = "opt_process_eigenfrequency_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app and has_eigen_app,"Missing (one or all) required applications: StructuralMechanicsApplication, EigenSolversApplication")
 class opt_process_weighted_eigenfrequency_test(ShapeOptimizationTestFactory):
     execution_directory = "opt_process_weighted_eigenfrequency_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app,"Missing required application: StructuralMechanicsApplication")
 class algorithm_steepest_descent_test(ShapeOptimizationTestFactory):
     execution_directory = "algorithm_steepest_descent_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app,"Missing required application: StructuralMechanicsApplication")
 class algorithm_penalized_projection_test(ShapeOptimizationTestFactory):
     execution_directory = "algorithm_penalized_projection_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app,"Missing required application: StructuralMechanicsApplication")
 class algorithm_trust_region_test(ShapeOptimizationTestFactory):
     execution_directory = "algorithm_trust_region_test"
     execution_file = "run_test"
@@ -76,14 +93,12 @@ class trust_region_projector_test(ShapeOptimizationTestFactory):
     execution_directory = "trust_region_projector_test"
     execution_file = "run_test"
 
+@KratosUnittest.skipUnless(has_csm_app,"Missing required application: StructuralMechanicsApplication")
 class algorithm_bead_optimization_test(ShapeOptimizationTestFactory):
     execution_directory = "algorithm_bead_optimization_test"
     execution_file = "run_test"
 
-class mapper_test(ShapeOptimizationTestFactory):
-    execution_directory = "mapper_test"
-    execution_file = "run_test"
-
+@KratosUnittest.skipUnless(has_csm_app,"Missing required application: StructuralMechanicsApplication")
 class opt_process_step_adaption_test(ShapeOptimizationTestFactory):
     execution_directory = "opt_process_step_adaption_test"
     execution_file = "run_test"
--- applications/ShapeOptimizationApplication/tests/test_ShapeOptimizationApplication.py
+++ applications/ShapeOptimizationApplication/tests/test_ShapeOptimizationApplication.py
@@ -10,25 +10,6 @@ import KratosMultiphysics
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.kratos_utilities as kratos_utilities
 
-# Check if external Apps are available
-if  kratos_utilities.CheckIfApplicationsAvailable("EigenSolversApplication"):
-    is_eigen_app_missing = False
-else:
-    KratosMultiphysics.Logger.PrintInfo("Unittests", "WARNING: EigenSolversApplication is not available, skipping related tests!")
-    is_eigen_app_missing = True
-
-if  kratos_utilities.CheckIfApplicationsAvailable("StructuralMechanicsApplication"):
-    is_csm_app_missing = False
-else:
-    KratosMultiphysics.Logger.PrintInfo("Unittests", "WARNING: StructuralMechanicsApplication is not available, skipping related tests!")
-    is_csm_app_missing = True
-
-if  kratos_utilities.CheckIfApplicationsAvailable("MeshMovingApplication"):
-    is_mesh_moving_app_missing = False
-else:
-    KratosMultiphysics.Logger.PrintInfo("Unittests", "WARNING: MeshMovingApplication is not available, skipping related tests!")
-    is_mesh_moving_app_missing = True
-
 # ==============================================================================
 # Import the tests or test_classes to create the suits
 # ==============================================================================
@@ -72,25 +53,16 @@ def AssembleTestSuites():
     smallSuite = suites['small']
     smallSuite.addTest(mapper_test('test_execution'))
     smallSuite.addTest(opt_process_vertex_morphing_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(opt_process_shell_test('test_execution'))
-    if not is_mesh_moving_app_missing:
-        smallSuite.addTest(opt_process_solid_test('test_execution'))
-    if not is_eigen_app_missing and not is_csm_app_missing:
-        smallSuite.addTest(opt_process_eigenfrequency_test('test_execution'))
-        smallSuite.addTest(opt_process_weighted_eigenfrequency_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(algorithm_steepest_descent_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(algorithm_penalized_projection_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(algorithm_trust_region_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(trust_region_projector_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(algorithm_bead_optimization_test('test_execution'))
-    if not is_csm_app_missing:
-        smallSuite.addTest(opt_process_step_adaption_test('test_execution'))
+    smallSuite.addTest(opt_process_shell_test('test_execution'))
+    smallSuite.addTest(opt_process_solid_test('test_execution'))
+    smallSuite.addTest(opt_process_eigenfrequency_test('test_execution'))
+    smallSuite.addTest(opt_process_weighted_eigenfrequency_test('test_execution'))
+    smallSuite.addTest(algorithm_steepest_descent_test('test_execution'))
+    smallSuite.addTest(algorithm_penalized_projection_test('test_execution'))
+    smallSuite.addTest(algorithm_trust_region_test('test_execution'))
+    smallSuite.addTest(trust_region_projector_test('test_execution'))
+    smallSuite.addTest(algorithm_bead_optimization_test('test_execution'))
+    smallSuite.addTest(opt_process_step_adaption_test('test_execution'))
 
     # Adding nightly tests (tests that take < 10min)
     nightSuite = suites['nightly']
