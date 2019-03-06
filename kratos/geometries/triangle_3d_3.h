dc7d6b41b95d51a159709243121dbb0b92e08e21
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-03-06 20:54:49 +0100

    Replacing tolerance with a Warning

--- kratos/geometries/triangle_3d_3.h
+++ kratos/geometries/triangle_3d_3.h
@@ -18,6 +18,7 @@
 #define  KRATOS_TRIANGLE_3D_3_H_INCLUDED
 
 // System includes
+#include <iomanip>
 
 // External includes
 
@@ -909,7 +910,53 @@ public:
         CoordinatesArrayType point_projected = rPoint;
         const array_1d<double,3> vector_points = rPoint - r_center.Coordinates();
         const double distance = inner_prod(vector_points, normal);
-        if (std::abs(distance) > std::numeric_limits<double>::epsilon()) { // Not in the plane, projecting
+
+        // We check if we are on the plane
+        if (std::abs(distance) > std::numeric_limits<double>::epsilon()) {
+            if (std::abs(distance) > 1.0e-6 * Length()) {
+
+                std::stringstream point_string_out;
+
+                point_string_out
+                << " :\tX:\t"
+                << std::setiosflags(std::ios::scientific)
+                << std::setprecision(3)
+                << std::uppercase
+                << rPoint[0] << "\tY:\t"
+                << std::setiosflags(std::ios::scientific)
+                << std::setprecision(3)
+                << std::uppercase
+                << rPoint[1] << "\tZ:\t"
+                << std::setiosflags(std::ios::scientific)
+                << std::setprecision(3)
+                << std::uppercase
+                << rPoint[2] << std::endl;
+
+                const std::string point_string_to_print = point_string_out.str();
+
+                std::stringstream triangle_string_out;
+                for (IndexType i = 0; i < 3; ++i) {
+                    triangle_string_out
+                    << "Point " << i <<" :\tX:\t"
+                    << std::setiosflags(std::ios::scientific)
+                    << std::setprecision(3)
+                    << std::uppercase
+                    << BaseType::GetPoint( i ).X() << "\tY:\t"
+                    << std::setiosflags(std::ios::scientific)
+                    << std::setprecision(3)
+                    << std::uppercase
+                    << BaseType::GetPoint( i ).Y() << "\tZ:\t"
+                    << std::setiosflags(std::ios::scientific)
+                    << std::setprecision(3)
+                    << std::uppercase
+                    << BaseType::GetPoint( i ).Z() << std::endl;
+                }
+
+                const std::string triangle_string_to_print = triangle_string_out.str();
+
+                KRATOS_WARNING("Triangle3D3") << "The point of coordinates" << point_string_to_print << "is not projected on the 3D triangle:\n" << triangle_string_to_print << "it is in a distance: " << std::abs(distance) << std::endl;
+            }
+            // Not in the plane, but allowing certain distance, projecting
             noalias(point_projected) = rPoint - normal * distance;
         }
 
@@ -948,7 +995,6 @@ public:
 
         rResult(0) = xi;
         rResult(1) = eta;
-        rResult(2) = 0.0;
 
         return rResult;
     }
