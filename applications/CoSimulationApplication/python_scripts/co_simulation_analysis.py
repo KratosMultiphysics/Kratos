478501604c2f03aaf6c8e2af09cbad97d83c0527
joris <Joris.Degroote@UGent.be>
2019-03-08 14:15:06 +0100

    Improved consistency by replacing cosim by cs.

--- applications/CoSimulationApplication/python_scripts/co_simulation_analysis.py
+++ applications/CoSimulationApplication/python_scripts/co_simulation_analysis.py
@@ -1,22 +1,19 @@
 from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
 
-#@Aditya we cannot import this here, this will not work with only python
-import KratosMultiphysics.CoSimulationApplication as CoSimulationApplication
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
-
 import sys
 
 class CoSimulationAnalysis(object):
     """
     The base class for the CoSimulation-AnalysisStage
     """
-    def __init__(self, cosim_settings):
-        self.cosim_settings = cosim_settings
+    def __init__(self, cs_settings):
+        self.cs_settings = cs_settings
         self.flush_stdout = False
 
         self.echo_level = 0
-        if "echo_level" in self.cosim_settings["problem_data"]:
-            self.echo_level = self.cosim_settings["problem_data"]["echo_level"].GetInt()
+        if "echo_level" in self.cs_settings["problem_data"]:
+            self.echo_level = self.cs_settings["problem_data"]["echo_level"].GetInt()
 
     def Run(self):
         self.Initialize()
@@ -44,8 +41,8 @@ class CoSimulationAnalysis(object):
             self._GetSolver().PrintInfo()
 
         ## Stepping and time settings
-        self.end_time = self.cosim_settings["problem_data"]["end_time"].GetDouble()
-        self.time = self.cosim_settings["problem_data"]["start_time"].GetDouble()
+        self.end_time = self.cs_settings["problem_data"]["end_time"].GetDouble()
+        self.time = self.cs_settings["problem_data"]["start_time"].GetDouble()
         self.step = 0
 
         sys.stdout.flush()
@@ -74,15 +71,15 @@ class CoSimulationAnalysis(object):
         return self._solver
 
     def _CreateSolver(self):
-        if("coupled_solver_settings" in self.cosim_settings.keys()):
+        if("coupled_solver_settings" in self.cs_settings.keys()):
             import KratosMultiphysics.CoSimulationApplication.custom_co_simulation_coupled_solvers.co_simulation_coupled_solver_factory as coupled_solver_factory
-            self._solver = coupled_solver_factory.CreateCoupledSolver(self.cosim_settings)
+            self._solver = coupled_solver_factory.CreateCoupledSolver(self.cs_settings)
             return self._solver
-        elif ("solvers" in self.cosim_settings.keys()):
-            num_solvers = len(self.cosim_settings["solvers"])
+        elif ("solvers" in self.cs_settings.keys()):
+            num_solvers = len(self.cs_settings["solvers"])
             if(num_solvers > 1 or num_solvers == 0):
                 Exception("More than one or no solvers defined without coupled solver !")
             else:
                 import co_simulation_solver_factory as solver_factory
-                self._solver = solver_factory.CreateSolverInterface(self.cosim_settings["solvers"][0])
+                self._solver = solver_factory.CreateSolverInterface(self.cs_settings["solvers"][0])
                 return self._solver
