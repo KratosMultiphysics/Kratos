7908563b1c2650a6fe295e6541b0e854a52ab7c7
joris <Joris.Degroote@UGent.be>
2019-03-08 16:23:57 +0100

    Removed list with solvers which is automatically available as list of files.

--- applications/CoSimulationApplication/python_scripts/custom_coupled_solvers/co_simulation_coupled_solver_factory.py
+++ applications/CoSimulationApplication/python_scripts/custom_coupled_solvers/co_simulation_coupled_solver_factory.py
@@ -1,32 +1,12 @@
 from __future__ import print_function, absolute_import, division  # makes backward compatible with python 2.6 and 2.7
 
-"""
-This is a map of the name of coupled solver to be specified in
-JSON file and their python module (file) name. New coupled solvers should
-be registered by an additional entry here.
-eg : "name_in_JSON" : "python module(file) name"
-"""
-available_coupled_solvers = {
-    "gauss_seidel_strong_coupling"  : "gauss_seidel_strong_coupled_solver",
-    "gauss_seidel_loose_coupling"   : "gauss_seidel_loose_coupled_solver",
-}
-
 
 def CreateCoupledSolver(settings):
     """
-    This function creates and returns the coupled solver used for CoSimulation
-    One can register the a new coupled solver by adding them to the above map "available_coupled_solvers"
+    This function creates and returns the coupled solver.
     """
     coupled_solver_type = settings["coupled_solver_settings"]["solver_type"]
-
-    if coupled_solver_type.GetString() in available_coupled_solvers:
-        module_name = available_coupled_solvers[coupled_solver_type.GetString()]
-        module_full = 'KratosMultiphysics.CoSimulationApplication.custom_coupled_solvers.'+module_name
-        coupled_solver_module = __import__(module_full,fromlist=[module_name])
-        return coupled_solver_module.Create(settings)
-    else:
-        err_msg  = 'The requested coupled solver "' + coupled_solver_type.GetString() + '" is not available!\n'
-        err_msg += 'Available coupled solvers are :\n'
-        for available_coupled_solver in available_coupled_solvers:
-            err_msg += "\t" + available_coupled_solver + "\n"
-        raise NameError(err_msg)
+    module_name = coupled_solver_type.GetString()
+    module_full = 'KratosMultiphysics.CoSimulationApplication.custom_coupled_solvers.'+module_name+'_coupled_solver'
+    coupled_solver_module = __import__(module_full, fromlist=[module_name])
+    return coupled_solver_module.Create(settings)
