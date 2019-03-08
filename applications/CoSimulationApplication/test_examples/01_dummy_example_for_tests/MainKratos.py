b364252dfc7a830943f5392abeb6369e65915b7d
joris <Joris.Degroote@UGent.be>
2019-03-08 11:21:20 +0100

    Removed duplicate code.

--- applications/CoSimulationApplication/test_examples/01_dummy_example_for_tests/MainKratos.py
+++ applications/CoSimulationApplication/test_examples/01_dummy_example_for_tests/MainKratos.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+from __future__ import print_function, absolute_import, division  # makes backward compatible with python 2.6 and 2.7
 
 import KratosMultiphysics.CoSimulationApplication
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
