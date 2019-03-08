4e2e83a1c4cf4cd1993590c6ed10b484201523fd
joris <Joris.Degroote@UGent.be>
2019-03-08 11:21:20 +0100

    Revert "Removed duplicate code."
    This reverts commit 2f3cc31d8cc0e78d668d3f699c79ee9ff87ac216.

--- applications/CoSimulationApplication/python_scripts/co_simulation_analysis.py
+++ applications/CoSimulationApplication/python_scripts/co_simulation_analysis.py
@@ -84,3 +84,25 @@ class CoSimulationAnalysis(object):
                 import co_simulation_solver_factory as solver_factory
                 self._solver = solver_factory.CreateSolverInterface(self.cs_settings["solvers"][0])
                 return self._solver
+
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) != 2:
+        err_msg  = 'Wrong number of input arguments!\n'
+        err_msg += 'Use this script in the following way:\n'
+        err_msg += '    "python co_simulation_analysis.py <cosim-parameter-file>.json"\n'
+        raise Exception(err_msg)
+
+    parameter_file_name = argv[1]
+    global cs_data_structure
+    cs_data_structure = cs_tools.ImportDataStructure(parameter_file_name)
+
+    # Now we import actual parameters from the cs_data_structure
+    with open(parameter_file_name,'r') as parameter_file:
+        parameters = cs_data_structure.Parameters(parameter_file.read())
+
+    simulation = CoSimulationAnalysis(parameters)
+    simulation.Run()
