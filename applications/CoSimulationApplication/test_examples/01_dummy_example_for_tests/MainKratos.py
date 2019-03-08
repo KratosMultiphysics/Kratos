66fe40064290e00c74367f45e5ab3dd758397fee
joris <Joris.Degroote@UGent.be>
2019-03-08 14:09:59 +0100

    Improved comments in MainKratos.

--- applications/CoSimulationApplication/test_examples/01_dummy_example_for_tests/MainKratos.py
+++ applications/CoSimulationApplication/test_examples/01_dummy_example_for_tests/MainKratos.py
@@ -6,19 +6,20 @@ from KratosMultiphysics.CoSimulationApplication.co_simulation_analysis import Co
 if __name__ == '__main__':
     from sys import argv
 
+    # Check number of command line arguments
     if len(argv) != 2:
         err_msg = 'Wrong number of input arguments!\n'
         err_msg += 'Use this script in the following way:\n'
         err_msg += '    "python MainKratos.py <cosim-parameter-file>.json"\n'
         raise Exception(err_msg)
 
+    # Import data structure
     parameter_file_name = argv[1]
     cs_data_structure = ImportDataStructure(parameter_file_name)
 
-    # Now we import actual parameters from the cs_data_structure
+    # Import parameters using the data structure
     with open(parameter_file_name, 'r') as parameter_file:
         parameters = cs_data_structure.Parameters(parameter_file.read())
 
     simulation = CoSimulationAnalysis(parameters)
     simulation.Run()
-
