a7fc0ae8ed9ee0b8c1edfe5a388f50e977a19f72
jcotela <jcotela@cimne.upc.edu>
2019-03-14 18:53:53 +0100

    Adding test for historical data assembly (WIP).

--- kratos/mpi/tests/sources/test_mpi_communicator.cpp
+++ kratos/mpi/tests/sources/test_mpi_communicator.cpp
@@ -29,11 +29,11 @@ namespace Internals {
 
 void ModelPartForMPICommunicatorTests(ModelPart& rModelPart, const DataCommunicator& rComm)
 {
+    /* NOTE: the modelpart should at least have PARTITION_INDEX in the nodal solution step data */
     constexpr double pi = 3.141592653589793238462643383279502884197169399375105820974944592308;
     constexpr double total_angle = pi/2.0;
     constexpr double side_length = 1.0;
 
-    rModelPart.AddNodalSolutionStepVariable(PARTITION_INDEX);
     Properties::Pointer p_properties = rModelPart.CreateNewProperties(0);
 
     const int rank = rComm.Rank();
@@ -51,15 +51,16 @@ void ModelPartForMPICommunicatorTests(ModelPart& rModelPart, const DataCommunica
     const double x2 = side_length * std::cos(angle_end);
     const double y2 = side_length * std::sin(angle_end);
 
-    const unsigned int index = rank + 2;
-    auto p_node_1 = rModelPart.CreateNewNode(index  , x1, y1, 0.0);
-    auto p_node_2 = rModelPart.CreateNewNode(index+1, x2, y2, 0.0);
+    const unsigned int local_index = rank + 2;
+    const unsigned int ghost_index = (size == 1) || (rank != size-1) ? rank + 3 : 2;
+    auto p_node_1 = rModelPart.CreateNewNode(local_index, x1, y1, 0.0);
+    auto p_node_2 = rModelPart.CreateNewNode(ghost_index, x2, y2, 0.0);
 
     p_node_1->FastGetSolutionStepValue(PARTITION_INDEX) = 1.0*rank;
-    const int remote_index = (rank == size-1) ? rank : rank + 1;
-    p_node_2->FastGetSolutionStepValue(PARTITION_INDEX) = 1.0*remote_index;
+    const int remote_rank = (rank != size-1) ? rank + 1 : 0;
+    p_node_2->FastGetSolutionStepValue(PARTITION_INDEX) = 1.0*remote_rank;
 
-    std::vector<ModelPart::IndexType> element_nodes{1, index, index+1};
+    std::vector<ModelPart::IndexType> element_nodes{1, local_index, ghost_index};
     rModelPart.CreateNewElement("Element2D3N", rank+1, element_nodes, p_properties);
 
     ParallelFillCommunicator(rModelPart).Execute();
@@ -102,6 +103,8 @@ KRATOS_TEST_CASE_IN_SUITE(MPICommunicatorSynchronizeOr, KratosMPICoreFastSuite)
 {
     Model model;
     ModelPart& r_model_part = model.CreateModelPart("TestModelPart");
+    r_model_part.AddNodalSolutionStepVariable(PARTITION_INDEX);
+
     MPIDataCommunicator world_comm(MPI_COMM_WORLD);
     Internals::ModelPartForMPICommunicatorTests(r_model_part, world_comm);
 
@@ -139,6 +142,8 @@ KRATOS_TEST_CASE_IN_SUITE(MPICommunicatorSynchronizeAnd, KratosMPICoreFastSuite)
 {
     Model model;
     ModelPart& r_model_part = model.CreateModelPart("TestModelPart");
+    r_model_part.AddNodalSolutionStepVariable(PARTITION_INDEX);
+
     MPIDataCommunicator world_comm(MPI_COMM_WORLD);
     Internals::ModelPartForMPICommunicatorTests(r_model_part, world_comm);
 
@@ -171,6 +176,73 @@ KRATOS_TEST_CASE_IN_SUITE(MPICommunicatorSynchronizeAnd, KratosMPICoreFastSuite)
         KRATOS_CHECK_EQUAL(r_center.Is(OUTLET), rank_is_even);
     }
     KRATOS_CHECK_EQUAL(r_center.Is(PERIODIC), rank_is_even); // This one should be left untouched
+}
+
+KRATOS_TEST_CASE_IN_SUITE(MPICommunicatorHistoricalAssembly, KratosMPICoreFastSuite)
+{
+    Model model;
+    ModelPart& r_model_part = model.CreateModelPart("TestModelPart");
+    r_model_part.AddNodalSolutionStepVariable(PARTITION_INDEX);
+    r_model_part.AddNodalSolutionStepVariable(DOMAIN_SIZE);           // Variable<int>
+    r_model_part.AddNodalSolutionStepVariable(TEMPERATURE);           // Variable<double>
+    r_model_part.AddNodalSolutionStepVariable(VELOCITY);              // Variable< array_1d<double,3> >
+    r_model_part.AddNodalSolutionStepVariable(CAUCHY_STRESS_VECTOR);  // Variable<Vector>
+    r_model_part.AddNodalSolutionStepVariable(DEFORMATION_GRADIENT);  // Variable<Matrix>
+
+    MPIDataCommunicator comm_world(MPI_COMM_WORLD);
+    Internals::ModelPartForMPICommunicatorTests(r_model_part, comm_world);
+    int rank = comm_world.Rank();
+    int size = comm_world.Size();
+
+    for (auto i_node = r_model_part.NodesBegin(); i_node != r_model_part.NodesEnd(); ++i_node)
+    {
+        i_node->FastGetSolutionStepValue(DOMAIN_SIZE, 0) = 1;
+        i_node->FastGetSolutionStepValue(TEMPERATURE, 0) = 2.0;
+        i_node->FastGetSolutionStepValue(VELOCITY_X, 0) = 1.0;
+        i_node->FastGetSolutionStepValue(VELOCITY_Y, 0) = 2.0;
+        Vector& r_cauchy_stress = i_node->FastGetSolutionStepValue(CAUCHY_STRESS_VECTOR, 0);
+        r_cauchy_stress.resize(2, false);
+        r_cauchy_stress = ZeroVector(2);
+        r_cauchy_stress[1] = 1.0;
+        Matrix& r_deformation_gradient = i_node->FastGetSolutionStepValue(DEFORMATION_GRADIENT, 0);
+        r_deformation_gradient.resize(2,2,false);
+        r_deformation_gradient = IdentityMatrix(2);
+    }
+
+    Communicator& r_comm = r_model_part.GetCommunicator();
+
+    // center is local to rank 0 and ghost in all other ranks
+    Node<3>& r_center = r_model_part.Nodes()[1];
+    // local and ghost nodes are each known in two ranks
+    const unsigned int local_id = rank + 2;
+    unsigned int ghost_id = (size == 1) || (rank != size-1) ? rank + 3 : 2;
+    Node<3>& r_local = r_model_part.Nodes()[local_id];
+    Node<3>& r_ghost = r_model_part.Nodes()[ghost_id];
+
+    r_comm.AssembleCurrentData(DOMAIN_SIZE);
+    int expected_int = (size > 1) ? 2 : 1;
+    KRATOS_CHECK_EQUAL(r_center.FastGetSolutionStepValue(DOMAIN_SIZE,0), size);
+    KRATOS_CHECK_EQUAL( r_local.FastGetSolutionStepValue(DOMAIN_SIZE,0), expected_int);
+    KRATOS_CHECK_EQUAL( r_ghost.FastGetSolutionStepValue(DOMAIN_SIZE,0), expected_int);
+
+    r_comm.AssembleCurrentData(TEMPERATURE);
+    int expected_double = 2.0*expected_int;
+    KRATOS_CHECK_EQUAL(r_center.FastGetSolutionStepValue(TEMPERATURE,0), 2.0*size);
+    KRATOS_CHECK_EQUAL( r_local.FastGetSolutionStepValue(TEMPERATURE,0), expected_double);
+    KRATOS_CHECK_EQUAL( r_ghost.FastGetSolutionStepValue(TEMPERATURE,0), expected_double);
+
+    r_comm.AssembleCurrentData(VELOCITY);
+    KRATOS_CHECK_EQUAL(r_center.FastGetSolutionStepValue(VELOCITY_X,0), 1.0*size);
+    KRATOS_CHECK_EQUAL( r_local.FastGetSolutionStepValue(VELOCITY_X,0), 1.0*expected_int);
+    KRATOS_CHECK_EQUAL( r_ghost.FastGetSolutionStepValue(VELOCITY_X,0), 1.0*expected_int);
+
+    KRATOS_CHECK_EQUAL(r_center.FastGetSolutionStepValue(VELOCITY_Y,0), 2.0*size);
+    KRATOS_CHECK_EQUAL( r_local.FastGetSolutionStepValue(VELOCITY_Y,0), 2.0*expected_int);
+    KRATOS_CHECK_EQUAL( r_ghost.FastGetSolutionStepValue(VELOCITY_Y,0), 2.0*expected_int);
+
+    KRATOS_CHECK_EQUAL(r_center.FastGetSolutionStepValue(VELOCITY_Z,0), 0.0);
+    KRATOS_CHECK_EQUAL( r_local.FastGetSolutionStepValue(VELOCITY_Z,0), 0.0);
+    KRATOS_CHECK_EQUAL( r_ghost.FastGetSolutionStepValue(VELOCITY_Z,0), 0.0);
 }
 
 }
