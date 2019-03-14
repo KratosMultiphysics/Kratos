707cd07dacf8a2e70177bf60205e8fee54e8dd2d
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-03-14 19:45:35 +0100

    Flag  setting fix

--- applications/ContactStructuralMechanicsApplication/custom_processes/find_intersected_geometrical_objects_with_obb_for_search_process.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_processes/find_intersected_geometrical_objects_with_obb_for_search_process.cpp
@@ -46,16 +46,21 @@ void FindIntersectedGeometricalObjectsWithOBBForSearchProcess::MarkIfIntersected
     )
 {
     if (rCondition1.Is(SLAVE)) {
+        // We clear previously assign flag
+        rCondition1.Reset(SELECTED);
+
         // The difference with the normal one is that we assign the flag to all "paired" conditions
         for (auto p_leaf : rLeaves) {
+            auto& r_leaf = *(p_leaf->pGetObjects());
             // We clear previously assign flags
-            rCondition1.Reset(SELECTED);
-            for (auto p_condition_2 : *(p_leaf->pGetObjects())) {
-                p_condition_2->Reset(SELECTED);
+            for (auto p_condition_2 : r_leaf) {
+                if (p_condition_2->IsNot(VISITED)) {
+                    p_condition_2->Reset(SELECTED);
+                }
             }
 
             // We iterate again and check intersection
-            for (auto p_condition_2 : *(p_leaf->pGetObjects())) {
+            for (auto p_condition_2 : r_leaf) {
                 if (p_condition_2->IsNot(VISITED)) {
                     if (HasIntersection(rCondition1.GetGeometry(), p_condition_2->GetGeometry())) {
                         rCondition1.Set(SELECTED);
@@ -67,7 +72,8 @@ void FindIntersectedGeometricalObjectsWithOBBForSearchProcess::MarkIfIntersected
         }
         // Reset VISITED flag
         for (auto p_leaf : rLeaves) {
-            for (auto p_condition_2 : *(p_leaf->pGetObjects())) {
+            auto& r_leaf = *(p_leaf->pGetObjects());
+            for (auto p_condition_2 : r_leaf) {
                 p_condition_2->Reset(VISITED);
             }
         }
