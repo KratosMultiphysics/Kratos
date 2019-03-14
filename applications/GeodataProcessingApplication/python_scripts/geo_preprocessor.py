91011fb0c30974491abaf3a1bd6a8a2c3da35abc
Simon v. Wenczowski <simon.wenczowski@outlook.de>
2019-03-14 02:53:54 +0100

    Adding function OBJtoMDPA

--- applications/GeodataProcessingApplication/python_scripts/geo_preprocessor.py
+++ applications/GeodataProcessingApplication/python_scripts/geo_preprocessor.py
@@ -53,16 +53,18 @@ class GeoPreprocessor( GeoProcessor ):
         if not extension_out:
             # extension_out is empty
             extension_out = ".xyz"
-        
+
         if (list_to_write):
             # list_to_write is full
             self.point_list = list_to_write
 
         if (type_file == "standard"):
             out_name = filename_out + "_standard" + extension_out
+            print("Writing to file " + out_name)
 
         elif (type_file == "qgis"):
             out_name = filename_out + "_qgis" + extension_out
+            print("Writing to file " + out_name)
 
             # sorting the list to enable output of the selected points in QGIS (Formatted ASCII-XYZ)
             # here with a lambda function
@@ -70,7 +72,7 @@ class GeoPreprocessor( GeoProcessor ):
             #           x-value as secondary sorting parameter
             num_nodes = len(self.point_list)
             self.point_list.sort(key=lambda x: float( x[1] * (num_nodes+1) + x[0]))
-        
+
         else:
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: type_data not valid. Please insert \"standard\" or \"qgis\".")
 
@@ -105,7 +107,7 @@ class GeoPreprocessor( GeoProcessor ):
                     X_coord, Y_coord, Z_coord = [float(coord) for coord in row[1:]]
 
                     self.point_list.append([X_coord, Y_coord, Z_coord])
-   
+
 
     def WriteSTL(self, stl_file_name_output="", list_to_write=[]):
         # function to write a stl file from a list of points
@@ -115,11 +117,11 @@ class GeoPreprocessor( GeoProcessor ):
         else:
             filename_out, extension_out = os.path.splitext(stl_file_name_output)
             out_name = filename_out + "_new" + extension_out
-        
+
         if (list_to_write):
             # list_to_write is full
             self.point_list = list_to_write
-        
+
         if ((len(self.point_list)%3) != 0):
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "There are some free points.")
 
@@ -142,9 +144,9 @@ class GeoPreprocessor( GeoProcessor ):
             string_out += "\t\t\tvertex {} {} {}\n".format(p3[0], p3[1], p3[2])
             string_out += "\t\tendloop\n"
             string_out += "\tendfacet\n"
-        
+
         string_out += "endsolid model\n"    # end of file stl
-        
+
         with open(out_name, "w") as fout:
             fout.write(string_out)
 
@@ -161,7 +163,7 @@ class GeoPreprocessor( GeoProcessor ):
         for coord in self.point_list:
             x_coords.append(coord[0])
             y_coords.append(coord[1])
-        
+
         x_centr = max(x_coords)/2
         y_centr = max(y_coords)/2
 
@@ -174,7 +176,7 @@ class GeoPreprocessor( GeoProcessor ):
         y_min = bounding_box[1]
         x_max = bounding_box[2]
         y_max = bounding_box[3]
-        print( "Bounding box defined as " )
+        print( "Bounding box for Cutting defined as " )
         print( "x_min = " + str(x_min) )
         print( "y_min = " + str(y_min) )
         print( "x_max = " + str(x_max) )
@@ -182,15 +184,15 @@ class GeoPreprocessor( GeoProcessor ):
         # num_lines = sum(1 for line in open( xyz_in_file ))
 
         # the coordinates are already modified and (x=0,y=0) is located at x_min, y_min
-        del_list = []
+        # del_list = []
         for num in range(len(self.point_list)):
             coord = self.point_list[num]
             if not ((x_min <= coord[0] <= x_max) and (y_min <= coord[1] <= y_max)):
                 # if coord it isn't in the bounding_box
-                del_list.append(coord)
+                self.point_list.remove(coord)
 
-        for coord in del_list:
-            self.point_list.remove(coord)
+        # for coord in del_list:
+        #     self.point_list.remove(coord)
 
         if not self.point_list:
             # if self.point_list is empty
@@ -218,7 +220,7 @@ class GeoPreprocessor( GeoProcessor ):
             coord[1] += y_shift
             coord[2] += z_shift
             self.point_list[num] = coord
-    
+
 
     def ExtractValley(self, max_height):
 
@@ -231,10 +233,10 @@ class GeoPreprocessor( GeoProcessor ):
             coord = self.point_list[num]
             if (coord[2] > max_height):
                 del_list.append(coord)
-        
+
         for coord in del_list:
             self.point_list.remove(coord)
-        
+
         if not self.point_list:
             # if self.point_list is empty
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
@@ -251,10 +253,10 @@ class GeoPreprocessor( GeoProcessor ):
             coord = self.point_list[num]
             if (coord[2] < min_height):
                 del_list.append(coord)
-        
+
         for coord in del_list:
             self.point_list.remove(coord)
-        
+
         if not self.point_list:
             # if self.point_list is empty
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
@@ -315,6 +317,126 @@ class GeoPreprocessor( GeoProcessor ):
         if not self.point_list:
             # if self.point_list is empty
             KratosMultiphysics.Logger.PrintWarning("GeoPreprocessor", "WARNING: No points inside the given valley!")
+
+
+    def ObjToMdpa(self, obj_file_name_input, mdpa_file_name_output, SingleMdpaFile = False):
+        # a sub_model_part is created for each Building
+
+        current_model = KratosMultiphysics.Model()
+        model_part = current_model.CreateModelPart("ModelPart")
+
+        # read buildings model to extract verices and elements information
+        with open (obj_file_name_input) as read_file:
+            vertex_coord = []		# here the coordinates of the node will be saved
+            ID_vertex = 1
+
+            # ( 1st iteration : All nodes from the file )
+            for row in read_file.readlines():
+                row = row.split()
+
+                if row:
+
+                    if (row[0] == "v"):			# vertex: "v 0.0 0.0 0.0"
+                        for coord in row[1:]:
+                            vertex_coord.append(float(coord))
+                            print( vertex_coord )
+
+                        # we create new nodes in SubModelPart
+                        # when we importing OBJ file, there is a change in coordinate system: x = x; y = -z; z = y
+                        model_part.CreateNewNode(ID_vertex, vertex_coord[0], vertex_coord[1], vertex_coord[2])
+                        vertex_coord = []
+                        ID_vertex += 1
+
+
+        with open (obj_file_name_input) as read_file:
+            num_building = 1		# progressive number to count the number of buildings
+            vertices_to_element = [0,0,0]
+            ID_elem = 1
+
+            # ( 2nd iteration : Reading the faces and assigning them to objects )
+            for row in read_file.readlines():
+                row = row.split()
+
+                if row:
+
+                    if (row[0] == "o"):			# when there is 'o' we create a new sub_model_part because there is a new object
+                        name_sub_model_building = "Object_{}".format(num_building)
+                        current_sub_model_building = model_part.CreateSubModelPart(name_sub_model_building)
+                        num_building += 1
+
+                    elif (row[0] == "f"):		# face: "f 1 2 3"
+                        vertices_to_element[0] = int(row[1])
+                        vertices_to_element[1] = int(row[2])
+                        vertices_to_element[2] = int(row[3])
+
+                        # we add nodes into SubModelPart
+                        for node in vertices_to_element:
+                            if (node not in current_sub_model_building.Nodes):
+                                current_sub_model_building.AddNodes([node])
+
+                        # we create new elements in SubModelPart
+                        current_sub_model_building.CreateNewElement("Element3D3N", ID_elem, [vertices_to_element[0], vertices_to_element[1], vertices_to_element[2]], model_part.GetProperties()[2])
+                        ID_elem += 1
+
+        if SingleMdpaFile:
+
+            model_part_io = KratosMultiphysics.ModelPartIO(mdpa_file_name_output, KratosMultiphysics.IO.WRITE)
+            model_part_io.WriteModelPart( model_part )
+
+        else:
+
+            sub_model_part_counter = 1
+            for sub_model_part in model_part.SubModelParts:
+
+                writing_model_part = current_model.CreateModelPart("WritingModelPart")
+
+                prop = model_part.Properties[0]
+                for node in sub_model_part.Nodes:
+                    n = writing_model_part.CreateNewNode( node.Id, node.X, node.Y, node.Z )
+                for elem in sub_model_part.Elements:
+                    nodes = elem.GetNodes()
+                    e = writing_model_part.CreateNewElement("Element3D3N", elem.Id,  [nodes[0].Id, nodes[1].Id, nodes[2].Id], prop)
+
+                # writing to mdpa file
+                file_name = "{}_{}".format( mdpa_file_name_output, sub_model_part_counter )
+                model_part_io = KratosMultiphysics.ModelPartIO(file_name, KratosMultiphysics.IO.WRITE)
+                model_part_io.WriteModelPart( writing_model_part )
+
+                current_model.DeleteModelPart("WritingModelPart")
+                sub_model_part_counter += 1
+
+
+    def ShiftObjCoordinates(self, obj_file_name_input, obj_file_name_output, x_shift, y_shift, z_shift ):
+
+        # read buildings model to extract vertices and elements information
+        with open (obj_file_name_input) as read_file:
+            with open (obj_file_name_output, 'w') as write_file:
+
+                content = "# OBJ file after coordinate shift\n"
+
+                for row in read_file.readlines():
+
+                    print( row )
+
+                    row_split = row.split()
+                    if row_split:
+                        if (row_split[0] == "v"):			# vertex: "v 0.0 0.0 0.0"
+                            x = float( row_split[1] ) + x_shift
+                            y = float( row_split[2] ) + y_shift
+                            z = float( row_split[3] ) + z_shift
+                            row = "v " + str(x) + " " + str(y) + " "  + str(z) + "\n"
+
+                    content += row
+
+                print( content )
+                write_file.write( content )
+
+
+
+
+
+
+
 
 
     ########################################################################
@@ -385,7 +507,7 @@ class GeoPreprocessor( GeoProcessor ):
             if dist < radius:
                 z = coords[2]
                 dict_active_points[ id ] = z
-        
+
         return dict_active_points
 
 
@@ -427,7 +549,7 @@ class GeoPreprocessor( GeoProcessor ):
 
     def _NormalTriangle(self, P1, P2, P3):
         # function to calc the normal of the triangle
-        
+
         nx = (P2[1]-P1[1])*(P3[2]-P1[2]) - (P3[1]-P1[1])*(P2[2]-P1[2])
         ny = (P2[2]-P1[2])*(P3[0]-P1[0]) - (P2[0]-P1[0])*(P3[2]-P1[2])
         nz = (P2[0]-P1[0])*(P3[1]-P1[1]) - (P3[0]-P1[0])*(P2[1]-P1[1])
