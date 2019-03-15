c43941f63aebea3e6a64509f031f048bf3646d21
Nicola <nicola.germano@unich.it>
2019-03-15 14:53:20 +0100

    adding function ObjImport and improvement of StlImpor

--- applications/GeodataProcessingApplication/python_scripts/geo_importer.py
+++ applications/GeodataProcessingApplication/python_scripts/geo_importer.py
@@ -10,33 +10,105 @@ class GeoImporter( GeoProcessor ):
         KratosMultiphysics.Logger.PrintWarning("GeoImporter: SetGeoModelPart", "This function cannot be used. The model is created be reading a file.")
 
 
+    ''' FUNCTION UNDER CONSTRUCTION '''
+    def ObjImport(self, obj_file_name_input, name_model_part="ModelPart"):
+        # a sub_model_part is created for each Building
+
+        self._InitializeModelPart(name_model_part)
+
+        # read buildings model to extract verices and elements information
+        with open (obj_file_name_input) as read_file:
+            vertex_coord = []		# here the coordinates of the node will be saved
+            num_building = 1		# progressive number to count the number of buildings
+            vertices_to_element = [0,0,0]
+
+            ID_vertex = 1
+            ID_elem = 1
+
+            '''current_sub_model_building = self.ModelPart ### CHECK THIS ###'''
+
+            for row in read_file.readlines():
+                row = row.split()
+                if row:							# check if "row" is empty. To avoid an error about "out of range"
+                    if (row[0] == "v"):			# vertex: "v 0.0 0.0 0.0"
+                        for coord in row[1:]:
+                            vertex_coord.append(float(coord))
+                        
+                        # we create new nodes in SubModelPart
+                        # when we importing OBJ file, there is a change in coordinate system: x = x; y = -z; z = y
+                        self.ModelPart.CreateNewNode(ID_vertex, vertex_coord[0], -vertex_coord[2], vertex_coord[1])
+                        vertex_coord = []
+                        ID_vertex += 1
+                    
+                    elif (row[0] == "f"):		# face: "f 1 2 3"
+                        vertices_to_element[0] = int(row[1])
+                        vertices_to_element[1] = int(row[2])
+                        vertices_to_element[2] = int(row[3])
+                        
+                        # we add nodes into SubModelPart
+                        # current_sub_model_building.AddNodes(vertices_to_element[0], vertices_to_element[1], vertices_to_element[2])
+                        for node in vertices_to_element:
+                            if (node not in current_sub_model_building.Nodes):
+                                current_sub_model_building.AddNodes([node])
+                    
+                        # we create new elements in SubModelPart
+                        current_sub_model_building.CreateNewElement("Element2D3N", ID_elem, [vertices_to_element[0], vertices_to_element[1], vertices_to_element[2]], self.ModelPart.GetProperties()[2])
+                        ID_elem += 1
+                    
+                    elif (row[0] == "o") and ("Building" in row[1]):			# when there is 'o Building' we create a new sub_model_part because there is a new Building
+                        name_sub_model_building = "Building_{}".format(num_building)
+                        current_sub_model_building = self.ModelPart.CreateSubModelPart(name_sub_model_building)
+                        num_building += 1
+
+        self.HasModelPart = True
+
+
     ### --- function imports the nodes from a *.stl file --- ###
 
-    def StlImport(self, stl_file_name_input):
+    def StlImport(self, stl_file_name_input, name_model_part="ModelPart"):
 
-        self._InitializeModelPart()
+        self._InitializeModelPart(name_model_part)
 
         with open (stl_file_name_input) as read_file:
 
             node_dict = {}        # dictionary with all vertices. key = (x, y, z); value = node_id
             node_id = 1
 
+            elem_dict = {}
+            elem_id = 1
+
             for row in read_file.readlines():
                 row = row.split()
+
+                if (row[0] == "outer"):
+                    elem_dict[elem_id] = []
                 
-                if (row[0] == "vertex" and all(self._IsFloat(n) for n in row[1:])):
+                elif (row[0] == "endloop"):
+                    elem_id += 1
+                
+                elif (row[0] == "vertex" and all(self._IsFloat(n) for n in row[1:])):
                     # row[0] = "vertex"; row[1] = x coordinate; row[2] = y coordinate; row[3] = z coordinate
                     X_coord, Y_coord, Z_coord = [float(coord) for coord in row[1:]]
 
                     if (X_coord, Y_coord, Z_coord) not in node_dict:
                         node_dict[X_coord, Y_coord, Z_coord] = node_id
-                        
+                        elem_dict[elem_id].append(node_id)
                         node_id += 1
 
-        node_dict = self._DeleteNodeOnBase(node_dict)
+                    else:
+                        # this vertex is already in dictionary
+                        for coord, id in node_dict.items():
+                            if (coord == (X_coord, Y_coord, Z_coord)):
+                                elem_dict[elem_id].append(id)           # if the "coord" are already in the "node_dict", we append just his Id in "elem_dict" and we leave the loop
+                                break
+
+        # node_dict = self._DeleteNodeOnBase(node_dict)
 
         for coord, node_id in node_dict.items():
             self.ModelPart.CreateNewNode(node_id, coord[0], coord[1], coord[2])
+        
+        for id_elem, ids_node in elem_dict.items():
+            self.ModelPart.CreateNewElement("Element2D3N", id_elem, [ids_node[0], ids_node[1], ids_node[2]], self.ModelPart.GetProperties()[1])
 
         self.HasModelPart = True
 
@@ -73,11 +145,11 @@ class GeoImporter( GeoProcessor ):
             return False
 
 
-    def _InitializeModelPart( self ):
+    def _InitializeModelPart( self, name_model_part="ModelPart" ):
         # this function adds all variables that will be needed during the pipeline (please add)
 
         current_model = KratosMultiphysics.Model()
-        self.ModelPart = current_model.CreateModelPart("ModelPart")
+        self.ModelPart = current_model.CreateModelPart(name_model_part)
         self.ModelPart.SetBufferSize(1)
 
         self.ModelPart.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, 3)
