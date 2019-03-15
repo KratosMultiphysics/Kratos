56700e8799d857f7a5f7a53786b980a0523e5141
Nicola <nicola.germano@unich.it>
2019-03-15 12:24:59 +0100

    adding function MeshCircleWithTerrainPoints

--- applications/GeodataProcessingApplication/python_scripts/geo_mesher.py
+++ applications/GeodataProcessingApplication/python_scripts/geo_mesher.py
@@ -243,8 +243,6 @@ class GeoMesher( GeoProcessor ):
         variational_distance_process.Execute()
 
 
-
-
     def RefineMeshNearGround( self, single_parameter ):
 
         find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess( self.ModelPart )
@@ -286,12 +284,241 @@ class GeoMesher( GeoProcessor ):
 
 
 
-    def MeshCircleWithTerrainPoints( self ):
+    def MeshCircleWithTerrainPoints( self, h_value=0.0, extract_center=False ):
 
-        print("Not interfereing here, Nicola - you are the expert!")
+        ### reading points from model part
+        X = []; Y = []; Z = []      # lists are generated to use predefined operations
+        ids_all = []                # id node with Z!=0
+        coord_2D = []               # list with XY coordinates of vertices [X, Y]
+        for node in self.ModelPart.Nodes:
+            X.append(node.X)
+            Y.append(node.Y)
+            Z.append(node.Z)
+            ids_all.append(node.Id)
+            coord_2D.append([node.X, node.Y])
+        
+        # calculating minimum and maximum value of X, Y and Z coordinates of the entire initial domain
+        x_min = min(X);    x_max = max(X)
+        y_min = min(Y);    y_max = max(Y)
+        z_min = min(Z);    z_max = max(Z)
 
+        # the centre of the circle
+        x_center = (x_min + x_max)/2
+        y_center = (y_min + y_max)/2
 
+        # radius calculation, considering the smaller side of the rectangle
+        r_boundary = min((x_max-x_min), (y_max-y_min))/2    # radius of the domain
+        r_ground = r_boundary * 2.0 / 3.0                   # 2/3 of the radius of the domain
+        r_buildings = r_boundary / 3.0                      # 1/3 of the radius of the domain
 
+        # buffering zone
+        delta = r_boundary/1000         # evaluate this value!!!
+
+        """ SMOOTHING PROCEDURE """
+        del_id = []             # list where there are the ids to be deleted because are outside of r_boundary
+        Z = []
+        # for index in idx_to_smooth:
+        for node in self.ModelPart.Nodes:
+            x_curr = node.X             # current X coordinate
+            y_curr = node.Y             # current Y coordinate
+
+            dist = math.sqrt(((x_center-x_curr)**2)+((y_center-y_curr)**2))     # calculate the distance between current node and center of circle
+
+            if (dist > r_boundary-delta):
+                del_id.append(node.Id)
+                continue
+            else:
+                # we fill the Z list to compute the minimum with the only nodes inside the domain
+                Z.append(node.Z)        # this list is useful to compute the min(Z) after
+
+        # the nodes from SubModelPart("bottom_terrain") that are outside the r_boundary-delta are deleted
+        for node in self.ModelPart.Nodes:
+            node.Set(KratosMultiphysics.TO_ERASE,False)
+        
+        for id in del_id:
+            self.ModelPart.GetNode(id).Set(KratosMultiphysics.TO_ERASE,True)
+        
+        self.ModelPart.RemoveNodesFromAllLevels(KratosMultiphysics.TO_ERASE)
+
+        z_min = min(Z)
+        for node in self.ModelPart.Nodes:
+            x_curr = node.X
+            y_curr = node.Y
+            dist = math.sqrt(((x_center-x_curr)**2)+((y_center-y_curr)**2))
+            if dist > r_ground:
+                Z_beta = (-(dist-r_ground) / (r_boundary-r_ground))+1       # we calculate beta
+                self.ModelPart.GetNode(node.Id).Z = (node.Z - z_min) * Z_beta + z_min
+
+        # a list with 360 degree (from 0 to 360)
+        # theta = self._custom_range(0.0, 2*math.pi, math.pi/90)
+        theta = self._custom_range(0.0, 2*math.pi, math.pi/30)  ###########################################################################################################################
+
+        # lists with nodes on circle boundary
+        x_circle = []; y_circle = []
+        for th in theta:
+            x_circle.append(r_boundary * math.cos(th) + x_center)   # we move the node along X to consider that the circle have not the centre in (0.0, 0.0)
+            y_circle.append(r_boundary * math.sin(th) + y_center)   # we move the node along Y to consider that the circle have not the centre in (0.0, 0.0)
+
+        """ TRIANGLE """
+        coord_2D = []       # vertically list with X and Y coordinates (TO AVOID NUMPY VSTACK)
+        all_points = []     # list with all coordinates of nodes
+        for node in self.ModelPart.Nodes:
+            coord_2D.append([node.X, node.Y])     # we add coordinates X and Y of terrain nodes
+            all_points.append((node.X, node.Y, node.Z))
+        
+        # we add circumference nodes into coord_2D list
+        for i in range(len(x_circle)):
+            coord_2D.append([x_circle[i], y_circle[i]])
+
+        ### preparing nodes to be handed over to triangle
+        A = dict(vertices = coord_2D)
+        triangle_dict = triangle.triangulate(A)     # triangle_dict {vertices: [...], triangles: [...], vertex_markers: [...]}
+
+        inner_id_terrain = len(all_points)		# id up to this moment
+        list_id_circle_bottom = []				# list with only ids of the bottom circle
+
+        for i in range(len(x_circle)):
+            all_points.append((x_circle[i], y_circle[i], z_min))
+            list_id_circle_bottom.append(i + inner_id_terrain)
+
+        all_facets = []		# list with all node ids of faces
+        all_markers = []	# list of integers [1 = bottom, 2 = topper and 3 = lateral]
+
+        # we fill the list for "bottom"
+        for f in triangle_dict["triangles"]:
+            all_facets.append([f[0], f[1], f[2]])
+            all_markers.append(1)
+
+        number_node_terrain = len(all_points)		# number of points up to now
+        volume_height = z_max + h_value             # CHECK THIS VALUE
+
+        # we fill all_points list for "top"
+        for node in self.ModelPart.Nodes:
+            all_points.append((node.X, node.Y, volume_height))
+
+        inner_id_top = len(all_points)              # id up to this moment
+        list_id_circle_top = []                     # list with only ids of the top circle
+
+        for i in range(len(x_circle)):
+            all_points.append((x_circle[i], y_circle[i], volume_height))
+            list_id_circle_top.append(i + inner_id_top)
+
+        # we fill all_facets list with "top" faces
+        for f in triangle_dict["triangles"]:
+            all_facets.append([f[0]+number_node_terrain, f[1]+number_node_terrain, f[2]+number_node_terrain])		# list of lists. "...+number_node_terrain" is UGLY! improve it
+            all_markers.append(2)
+
+        # we fill all_facets list with "lateral" faces
+        for i in range(len(list_id_circle_bottom)):
+            if (i == len(list_id_circle_bottom)-1):
+                # here we have the last value
+                all_facets.append([list_id_circle_bottom[i], list_id_circle_bottom[0], list_id_circle_top[0], list_id_circle_top[i]])
+                all_markers.append(3)
+                break
+            all_facets.append([list_id_circle_bottom[i], list_id_circle_bottom[i+1], list_id_circle_top[i+1], list_id_circle_top[i]])
+            all_markers.append(3)
+
+        """ TETGEN (VIA MESHPY) """
+        ### using Meshpy for the creation of an initial mesh https://mathema.tician.de/software/meshpy/
+        mesh_info = MeshInfo()
+        mesh_info.set_points(all_points)
+        mesh_info.set_facets(all_facets, markers=all_markers)
+
+        # build the mesh
+        mesh = build(mesh_info)
+        self.ModelPart.Nodes.clear()
+        self.ModelPart.Elements.clear()
+        self.ModelPart.Conditions.clear()
+
+        lateral_model_part = self.ModelPart.CreateSubModelPart("LateralModelPart")
+        bottom_model_part = self.ModelPart.CreateSubModelPart("BottomModelPart")
+        top_model_part = self.ModelPart.CreateSubModelPart("TopModelPart")
+        properties = self.ModelPart.Properties[0]
+
+        bottom_cond = []; bottom_points = []
+        top_cond = []; top_points = []
+        lateral_cond = []; lateral_points = []
+        
+        # AWARE: Shift by 1 in index!!!
+        ### Nodes
+        for i in range(len(mesh.points)):
+            # node id is shifted up by 1
+            coords = mesh.points[i]
+            self.ModelPart.CreateNewNode((i+1), coords[0], coords[1], coords[2])
+
+        ### Conditions and Nodes in SubModelParts
+        for j in range(len(mesh.faces)):
+            points = mesh.faces[j]
+            marker = mesh.face_markers[j]
+            if (marker == 1):
+                self.ModelPart.CreateNewCondition("Condition3D", (j+1), [points[0]+1, points[1]+1, points[2]+1], properties)
+                bottom_cond.append(j+1)
+            elif (marker == 2):
+                self.ModelPart.CreateNewCondition("Condition3D", (j+1), [points[0]+1, points[1]+1, points[2]+1], properties)
+                top_cond.append(j+1)
+            elif ( marker == 3 ):
+                self.ModelPart.CreateNewCondition("Condition3D", (j+1), [points[0]+1, points[1]+1, points[2]+1], properties)
+                lateral_cond.append(j+1)
+        
+        bottom_model_part.AddConditions(bottom_cond)
+        top_model_part.AddConditions(top_cond)
+        lateral_model_part.AddConditions(lateral_cond)
+
+        for cond in bottom_model_part.Conditions:
+            for node in cond.GetNodes():
+                bottom_points.append(node.Id)
+        for cond in top_model_part.Conditions:
+            for node in cond.GetNodes():
+                top_points.append(node.Id)
+        for cond in lateral_model_part.Conditions:
+            for node in cond.GetNodes():
+                lateral_points.append(node.Id)
+        
+        bottom_model_part.AddNodes(bottom_points)
+        top_model_part.AddNodes(top_points)
+        lateral_model_part.AddNodes(lateral_points)
+
+        ### Elements
+        for k in range(len(mesh.elements)):
+            points = mesh.elements[k]
+            self.ModelPart.CreateNewElement("Element3D4N", (k+1), [points[0]+1, points[1]+1, points[2]+1, points[3]+1], properties)
+        
+
+        """ OPTIONAL VALUES """
+        if extract_center:
+            # we extract the center of the geometry
+
+            elem_inside = {}        # dictionary with the information about the element that are in r_buildings
+            elem_id = 1
+
+            elem_list = []  ####################################################################################################################################
+
+            for node in triangle_dict["triangles"]:
+                for i in range(3):
+                    x_curr = self.ModelPart.GetNode(node[i]+1).X
+                    y_curr = self.ModelPart.GetNode(node[i]+1).Y
+                    dist = math.sqrt(((x_center-x_curr)**2) + ((y_center-y_curr)**2))
+
+                    if dist > r_buildings:
+                        break   # if at least one node is external to r_buildings, we reject the entire element
+                
+                else:
+                    # we get here if all nodes of the element are inside the r_buildings
+                    node1 = self.ModelPart.GetNode(node[0]+1)
+                    node2 = self.ModelPart.GetNode(node[1]+1)
+                    node3 = self.ModelPart.GetNode(node[2]+1)
+
+                    elem_inside[elem_id] = [[node1.Id, node1.X, node1.Y, node1.Z],
+                                            [node2.Id, node2.X, node2.Y, node2.Z],
+                                            [node3.Id, node3.X, node3.Y, node3.Z]]
+                    
+                    elem_list.append([node1.X, node1.Y, node1.Z]) ####################################################################################################
+                    elem_list.append([node2.X, node2.Y, node2.Z]) ####################################################################################################
+                    elem_list.append([node3.X, node3.Y, node3.Z]) ####################################################################################################
+                    
+                    elem_id += 1
+            
+            return elem_list    # JUST FOR THE CHECK
 
 
 ############################ --- Auxiliary functions --- ###########################################
@@ -325,3 +552,11 @@ class GeoMesher( GeoProcessor ):
                 KratosMultiphysics.VariationalDistanceCalculationProcess3D.CALCULATE_EXACT_DISTANCES_TO_PLANE,
         		aux_name )
         return variational_distance_process
+
+
+    def _custom_range(self, start, stop, step=1.0):
+        # function to replace the numpy range function
+        list_floats = [float(start)]
+        while (list_floats[-1]+step) < stop:
+            list_floats.append(list_floats[-1]+step)
+        return list_floats
