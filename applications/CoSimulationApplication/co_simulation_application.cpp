cedd00252673f31b3dd543091027280f79cc651f
philbucher <philipp.bucher@tum.de>
2019-03-19 21:27:43 +0100

    adding the logo :)

--- applications/CoSimulationApplication/co_simulation_application.cpp
+++ applications/CoSimulationApplication/co_simulation_application.cpp
@@ -28,6 +28,10 @@ void KratosCoSimulationApplication::Register()
 {
     // calling base class register to register Kratos components
     KratosApplication::Register();
-    KRATOS_INFO("") << "Initializing KratosCoSimulationApplication..." << std::endl;
+	KRATOS_INFO("") << "    KRATOS  / ___|___/ ___|(_)_ __ ___  _   _| | __ _| |_(_) ___  _ __\n"
+	                << "           | |   / _ \\___ \\| | '_ ` _ \\| | | | |/ _` | __| |/ _ \\| '_ \\\n"
+	                << "           | |__| (_) |__) | | | | | | | |_| | | (_| | |_| | (_) | | | |\n"
+	                << "            \\____\\___/____/|_|_| |_| |_|\\__,_|_|\\__,_|\\__|_|\\___/|_| |_|\n"
+                    << "Initializing KratosCoSimulationApplication..." << std::endl;
 }
 }  // namespace Kratos.
