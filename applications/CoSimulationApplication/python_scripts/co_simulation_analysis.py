f00bd889d10e56f29683d9da28b0f9820be6ef74
joris <Joris.Degroote@UGent.be>
2019-03-25 15:12:02 +0100

    Consistency between co_simulation_analysis and MainKratos

--- applications/CoSimulationApplication/python_scripts/co_simulation_analysis.py
+++ applications/CoSimulationApplication/python_scripts/co_simulation_analysis.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, absolute_import, division  # makes backward compatible with python 2.6 and 2.7
 
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
+from KratosMultiphysics.CoSimulationApplication.co_simulation_tools import ImportDataStructure
 
 
 class CoSimulationAnalysis(object):
@@ -86,22 +87,22 @@ class CoSimulationAnalysis(object):
                 return self._solver
 
 
-
 if __name__ == '__main__':
     from sys import argv
 
+    # Check number of command line arguments
     if len(argv) != 2:
-        err_msg  = 'Wrong number of input arguments!\n'
+        err_msg = 'Wrong number of input arguments!\n'
         err_msg += 'Use this script in the following way:\n'
         err_msg += '    "python co_simulation_analysis.py <cosim-parameter-file>.json"\n'
         raise Exception(err_msg)
 
+    # Import data structure
     parameter_file_name = argv[1]
-    global cs_data_structure
-    cs_data_structure = cs_tools.ImportDataStructure(parameter_file_name)
+    cs_data_structure = ImportDataStructure(parameter_file_name)
 
-    # Now we import actual parameters from the cs_data_structure
-    with open(parameter_file_name,'r') as parameter_file:
+    # Import parameters using the data structure
+    with open(parameter_file_name, 'r') as parameter_file:
         parameters = cs_data_structure.Parameters(parameter_file.read())
 
     simulation = CoSimulationAnalysis(parameters)
