234c6a46a293795e660100a7f4caac035f3d1450
AdityaGhantasala <aditya.ghantasala@tum.de>
2019-03-25 18:46:04 +0100

    function name changes in base filter and using data

--- applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_filter.py
+++ applications/CoSimulationApplication/python_scripts/base_co_simulation_classes/co_simulation_base_filter.py
@@ -12,7 +12,7 @@ import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as tools
 # used in co simulation.
 class CoSimulationBaseFilter(object):
     def __init__(self, settings, data):
-        self.solver = data
+        self.data = data
         self.settings = settings
         self.echo_level = 0
 
@@ -32,7 +32,7 @@ class CoSimulationBaseFilter(object):
     #               Calculation of the update and applying update to the data field will happen here.
     #  @param self                      The object pointer.
     def Apply(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling Apply function from base accelerator !" + tools.bcolors.ENDC)
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling Apply function from base filter !" + tools.bcolors.ENDC)
 
     ## InitializeSolutionStep : Called once in the beginning of the solution step
     #
@@ -46,23 +46,19 @@ class CoSimulationBaseFilter(object):
     def FinalizeSolutionStep(self):
         pass
 
-    ## FinalizeNonLinearIteration : Function initializes the non linear iteration (coupling iteration)
-    #                               Called at the beginning of the nonlinear iteration (coupling iteration)
+    ## InitializeCouplingIteration : Called once in the beginning of the coupled iteration
     #
-
-    def InitializeNonLinearIteration(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling InitializeNonLinearIteration function !" + tools.bcolors.ENDC)
-
-    ## FinalizeNonLinearIteration : Function finalizes the non linear iteration (coupling iteration)
-    #                               Called at the end of the nonlinear iteration (coupling iteration)
+    #  @param self            The object pointer.
+    def InitializeCouplingIteration(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling InitializeCouplingIteration function !" + tools.bcolors.ENDC)
+    ## FinalizeCouplingIteration : Called once at the end of the coupled iteration
     #
-
-    def FinalizeNonLinearIteration(self):
-        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling FinalizeNonLinearIteration function !" + tools.bcolors.ENDC)
+    #  @param self            The object pointer.
+    def FinalizeCouplingIteration(self):
+        raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling FinalizeCouplingIteration function !" + tools.bcolors.ENDC)
 
     ## ComputeUpdate : Function to compute the update for the fields. Should be called during the nonlinear (coupling iteration).
     #
-
     def ComputeUpdate(self):
         raise NotImplementedError(tools.bcolors.FAIL + "CoSimulationBaseFilter : Calling ComputeUpdate function !" + tools.bcolors.ENDC)
 
