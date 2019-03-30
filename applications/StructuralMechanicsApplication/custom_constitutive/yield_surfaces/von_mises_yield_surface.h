3be9918037c5f91d7297f31c3119ca3f90e7c9cd
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-03-30 18:46:59 +0100

    missing removal

--- applications/StructuralMechanicsApplication/custom_constitutive/yield_surfaces/von_mises_yield_surface.h
+++ applications/StructuralMechanicsApplication/custom_constitutive/yield_surfaces/von_mises_yield_surface.h
@@ -209,9 +209,7 @@ public:
         )
     {
         array_1d<double, VoigtSize> second_vector;
-
         ConstitutiveLawUtilities<VoigtSize>::CalculateSecondVector(rDeviator, J2, second_vector);
-        ConstitutiveLawUtilities<VoigtSize>::CalculateThirdVector(rDeviator, J2, third_vector);
         const double c2 = std::sqrt(3.0);
 
         noalias(rFFlux) = c2 * second_vector;
