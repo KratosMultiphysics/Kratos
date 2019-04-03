eb0e4f74ade2e43ea9060dae9c0f54cc2bc51755
Philipp Bucher <philipp.bucher@tum.de>
2019-04-03 10:50:58 +0200

    Update applications/CompressiblePotentialFlowApplication/custom_elements/incompressible_adjoint_potential_flow_element.h
    Co-Authored-By: marcnunezc <mnunez@cimne.upc.edu>

--- applications/CompressiblePotentialFlowApplication/custom_elements/incompressible_adjoint_potential_flow_element.h
+++ applications/CompressiblePotentialFlowApplication/custom_elements/incompressible_adjoint_potential_flow_element.h
@@ -17,7 +17,6 @@
 
 // Project includes
 #include "includes/element.h"
-#include "includes/kratos_flags.h"
 #include "compressible_potential_flow_application_variables.h"
 #include "incompressible_potential_flow_element.h"
 
