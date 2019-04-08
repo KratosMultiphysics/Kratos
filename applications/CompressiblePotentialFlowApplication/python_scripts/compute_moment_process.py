fixed integer conversion error which caused wrong results for moment

applications/CompressiblePotentialFlowApplication/python_scripts/compute_moment_process.py
@@ -27,7 +27,7 @@ class ComputeMomentProcess(KratosMultiphysics.Process):
         self.velocity_infinity[1] = settings["velocity_infinity"][1].GetDouble()
         self.velocity_infinity[2] = settings["velocity_infinity"][2].GetDouble()
         self.reference_area = settings["reference_area"].GetDouble()
-        self.reference_point = np.array([0,0,0])
+        self.reference_point = np.array([0.0,0.0,0.0])
         self.create_output_file = settings["create_output_file"].GetBool()
         self.reference_point[0] = settings["reference_point"][0].GetDouble()
         self.reference_point[1] = settings["reference_point"][1].GetDouble()
@@ -39,7 +39,7 @@ class ComputeMomentProcess(KratosMultiphysics.Process):
         m = [0.0,0.0,0.0]
 
         for cond in self.body_model_part.Conditions:
-            n =  np.array(cond.GetValue(KratosMultiphysics.NORMAL)) #Inigo: normal direction? (ass. inward of domain)
+            n =  np.array(cond.GetValue(KratosMultiphysics.NORMAL)) #Inigo: normal direction? (ass. outward of domain)
             cp = cond.GetValue(KratosMultiphysics.PRESSURE)
             mid_point = cond.GetGeometry().Center()
             mid_point =  np.array([mid_point.X, mid_point.Y, mid_point.Z])