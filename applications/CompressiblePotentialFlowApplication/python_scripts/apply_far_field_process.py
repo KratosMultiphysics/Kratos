660dc5cc1d038b1a205ba203705c2499415648c4
Inigo Lopez <inigolcanalejo@gmail.com>
2019-04-08 21:12:07 +0200

    adding inlet and initialitation

--- applications/CompressiblePotentialFlowApplication/python_scripts/apply_far_field_process.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/apply_far_field_process.py
@@ -1,6 +1,5 @@
 import KratosMultiphysics
 import KratosMultiphysics.CompressiblePotentialFlowApplication as CPFApp
-import math
 import numpy as np
 
 def Factory(settings, Model):
@@ -22,21 +21,23 @@ class ApplyFarFieldProcess(KratosMultiphysics.Process):
                 "density_infinity"  : 1.0,
                 "speed_of_sound": 340,
                 "gamma": 1.4,
-                "pressure_infinity": 101325
+                "pressure_infinity": 101325,
+                "initialize_flow_field": true
             }  """)
 
         settings.ValidateAndAssignDefaults(default_parameters)
 
-        self.model_part = Model[settings["model_part_name"].GetString()]
-        self.fluid_model_part = self.model_part.GetRootModelPart()
+        self.far_field_model_part = Model[settings["model_part_name"].GetString()]
+        self.fluid_model_part = self.far_field_model_part.GetRootModelPart()
 
-        self.inlet_phi = settings["inlet_phi"].GetDouble()
+        self.inlet_phi_0 = settings["inlet_phi"].GetDouble()
         self.density_inf = settings["density_infinity"].GetDouble()
         self.mach_inf = settings["mach_infinity"].GetDouble()
         self.gamma = settings["gamma"].GetDouble()
         self.aoa = settings["angle_of_attack"].GetDouble()
         self.pressure_inf = settings["pressure_infinity"].GetDouble()
         self.a_inf = settings["speed_of_sound"].GetDouble()
+        self.initialize = settings["initialize_flow_field"].GetBool()
 
         # Computing free stream velocity
         self.u_inf = self.mach_inf * self.a_inf
@@ -46,7 +47,7 @@ class ApplyFarFieldProcess(KratosMultiphysics.Process):
         self.velocity_inf[2] = 0.0
 
         # For the model part
-        self.model_part.ProcessInfo.SetValue(CPFApp.VELOCITY_INFINITY, self.velocity_inf)
+        self.far_field_model_part.ProcessInfo.SetValue(CPFApp.VELOCITY_INFINITY, self.velocity_inf)
 
         # For the conditions
         self.fluid_model_part.GetProperties()[0].SetValue(CPFApp.DENSITY_INFINITY, self.density_inf)
@@ -61,46 +62,60 @@ class ApplyFarFieldProcess(KratosMultiphysics.Process):
         self.fluid_model_part.GetProperties()[1].SetValue(CPFApp.PRESSURE_INFINITY, self.pressure_inf)
 
     def Execute(self):
-        #KratosMultiphysics.VariableUtils().SetVectorVar(CPFApp.VELOCITY_INFINITY, self.velocity_inf, self.model_part.Conditions)
-        for cond in self.model_part.Conditions:
+        #KratosMultiphysics.VariableUtils().SetVectorVar(CPFApp.VELOCITY_INFINITY, self.velocity_inf, self.far_field_model_part.Conditions)
+        for cond in self.far_field_model_part.Conditions:
             cond.SetValue(CPFApp.VELOCITY_INFINITY, self.velocity_inf)
 
-        #select the first node
-        for node in self.model_part.Nodes:
-            node1 = node
+        # Select the first node in the mesh as reference
+        for node in self.far_field_model_part.Nodes:
+            x0 = node.X
+            y0 = node.Y
+            z0 = node.Z
             break
-        
-        #find the node with the minimal x
-        x0 = node1.X
-        y0 = node1.X
-        z0 = node1.X
 
+        # Find smallest distance_to_reference
         pos = 1e30
-        for node in self.model_part.Nodes:
+        for node in self.far_field_model_part.Nodes:
+            # Computing distance to reference
             dx = node.X - x0
             dy = node.Y - y0
             dz = node.Z - z0
-            
-            tmp = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2]
-            
-            if(tmp < pos):
-                pos = tmp
 
-        for node in self.model_part.Nodes:
-            dx = node.X - x0
-            dy = node.Y - y0
-            dz = node.Z - z0
-            
-            tmp = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2]
-            
-            if(tmp < pos+1e-9):
-                node.Fix(CPFApp.VELOCITY_POTENTIAL)
-                node.SetSolutionStepValue(CPFApp.VELOCITY_POTENTIAL,0,self.inlet_phi)
-                if self.model_part.HasNodalSolutionStepVariable(CPFApp.ADJOINT_VELOCITY_POTENTIAL):
-                    node.Fix(CPFApp.ADJOINT_VELOCITY_POTENTIAL)
-                    node.SetSolutionStepValue(CPFApp.ADJOINT_VELOCITY_POTENTIAL,0,0.0)
-        
+            distance_to_reference = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2]
+
+            if(distance_to_reference < pos):
+                pos = distance_to_reference
+                self.reference_inlet_node = node
+
+        # Fix nodes in the inlet
+        for cond in self.far_field_model_part.Conditions:
+            normal = cond.GetValue(KratosMultiphysics.NORMAL)
+            projection = np.dot(normal,self.velocity_inf)
+
+            if( projection < 0):
+                for node in cond.GetNodes():
+                    # Computing distance to reference
+                    dx = node.X - self.reference_inlet_node.X
+                    dy = node.Y - self.reference_inlet_node.Y
+                    dz = node.Z - self.reference_inlet_node.Z
+
+                    inlet_phi = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2] + self.inlet_phi_0
+                    node.Fix(CPFApp.VELOCITY_POTENTIAL)
+                    node.SetSolutionStepValue(CPFApp.VELOCITY_POTENTIAL,0,inlet_phi)
+                    if self.far_field_model_part.HasNodalSolutionStepVariable(CPFApp.ADJOINT_VELOCITY_POTENTIAL):
+                        node.Fix(CPFApp.ADJOINT_VELOCITY_POTENTIAL)
+                        node.SetSolutionStepValue(CPFApp.ADJOINT_VELOCITY_POTENTIAL,0,0.0)
+
+        if(self.initialize):
+            for node in self.fluid_model_part.Nodes:
+                # Computing distance to reference
+                dx = node.X - self.reference_inlet_node.X
+                dy = node.Y - self.reference_inlet_node.Y
+                dz = node.Z - self.reference_inlet_node.Z
+
+                initial_phi = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2] + self.inlet_phi_0
+                node.SetSolutionStepValue(CPFApp.VELOCITY_POTENTIAL,0,initial_phi)
+                node.SetSolutionStepValue(CPFApp.AUXILIARY_VELOCITY_POTENTIAL,0,initial_phi)
+
     def ExecuteInitializeSolutionStep(self):
         self.Execute()
-        
-        
