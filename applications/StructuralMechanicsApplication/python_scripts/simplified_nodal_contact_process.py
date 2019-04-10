bc94e088e38b994f62bd236127b4123185e7b8df
philbucher <philipp.bucher@tum.de>
2019-04-10 11:26:21 +0200

    correct name

--- applications/StructuralMechanicsApplication/python_scripts/simplified_nodal_contact_process.py
+++ applications/StructuralMechanicsApplication/python_scripts/simplified_nodal_contact_process.py
@@ -4,7 +4,7 @@ from __future__ import print_function, absolute_import, division  # makes Kratos
 import KratosMultiphysics
 
 def Factory(settings, Model):
-    if not isinstance(settings, KM.Parameters):
+    if not isinstance(settings, KratosMultiphysics.Parameters):
         raise Exception("Expected input shall be a Parameters object, encapsulating a json string")
     return SimplifiedNodalContactProcess(Model, settings["Parameters"])
 
