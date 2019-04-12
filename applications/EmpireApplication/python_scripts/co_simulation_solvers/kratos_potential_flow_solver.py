b9d58d93d3bd952a3bf605be9d955ac5e72a51c1
BasselSaridar <bassel.saridar@tum.de>
2019-04-12 16:01:18 +0200

    adding fsi potential solver

---
+++ applications/EmpireApplication/python_scripts/co_simulation_solvers/kratos_potential_flow_solver.py
@@ -0,0 +1,42 @@
+
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing the Kratos Library
+import KratosMultiphysics
+import KratosMultiphysics.CompressiblePotentialFlowApplication
+try:
+    import KratosMultiphysics.MeshMovingApplication
+    KratosMultiphysics.Logger.PrintInfo("MeshMovingApplication", "succesfully imported")
+except ImportError:
+    KratosMultiphysics.Logger.PrintInfo("MeshMovingApplication", "not imported")
+
+# Importing the base class
+from kratos_base_field_solver import KratosBaseFieldSolver
+
+# Other imports
+from potential_flow_analysis import PotentialFlowAnalysis
+from KratosMultiphysics.CompressiblePotentialFlowApplication.forces_elements_to_nodes_process import ForcesElementsToNodesProcess
+from KratosMultiphysics.CompressiblePotentialFlowApplication.define_wake_process_2d import DefineWakeProcess2D
+
+def CreateSolver(cosim_solver_settings, level):
+    return KratosPotentialFlowSolver(cosim_solver_settings, level)
+
+class KratosPotentialFlowSolver(KratosBaseFieldSolver):
+    def _CreateAnalysisStage(self):
+        return PotentialFlowAnalysis(self.model, self.project_parameters)
+        self.wake_process = DefineWakeProcess2D(self.model,self.project_parameters["wake_parameters"])
+        self.conversion_process = ForcesElementsToNodesProcess(self.model, self.project_parameters["conversion_process"])
+
+    def SolveSolutionStep(self):
+        self.wake_process.SelectWakeElements()
+        super(KratosPotentialFlowSolver, self).SolveSolutionStep()
+
+        self.conversion_process.Execute()
+        self.wake_process.DeselectWakeElements()
+
+    def _GetParallelType(self):
+        return self.project_parameters["problem_data"]["parallel_type"].GetString()
+
+    def _Name(self):
+        return self.__class__.__name__
+
