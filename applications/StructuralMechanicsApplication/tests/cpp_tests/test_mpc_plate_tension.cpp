31b5debeb4808262370ed9575b2059030b8b639f
yaman <yamanato@gmail.com>
2019-04-15 17:28:01 +0200

    add header

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_mpc_plate_tension.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_mpc_plate_tension.cpp
@@ -1,3 +1,14 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:             BSD License
+//                                       license: StructuralMechanicsApplication/license.txt
+//
+//  Main authors:    Yaman Zendaki , Hoang-Giang Bui
+//
+
 #include <iostream>
 #include "includes/gid_io.h"
 #include "testing/testing.h"
