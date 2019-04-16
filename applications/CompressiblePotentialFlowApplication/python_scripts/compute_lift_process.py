bea10b4948f1fa79611e75dc3e9b353a08cb1284
Inigo Lopez <inigolcanalejo@gmail.com>
2019-04-16 17:07:28 +0200

    formatting

--- applications/CompressiblePotentialFlowApplication/python_scripts/compute_lift_process.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/compute_lift_process.py
@@ -17,7 +17,7 @@ def Factory(settings, Model):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return ComputeLiftProcess(Model, settings["Parameters"])
 
-##all the processes python processes should be derived from "python_process"
+# All the processes python processes should be derived from "python_process"
 class ComputeLiftProcess(KratosMultiphysics.Process):
     def __init__(self, Model, settings ):
         KratosMultiphysics.Process.__init__(self)
