6cc063cc8b57f1f393be8575d7e8dc7df4ef8faa
Lukas Hoeffner <lukas.hoeffner@tum.de>
2019-04-16 14:51:40 +0200

    started own coupling solver for steady cases

---
+++ applications/EmpireApplication/python_scripts/co_simulation_solvers/simple_steady_coupling_solver.py
@@ -0,0 +1,37 @@
+# Importing the base class
+from co_simulation_solvers.co_simulation_base_coupling_solver import CoSimulationBaseCouplingSolver
+
+# Other imports
+from co_simulation_convergence_criteria.co_simulation_convergence_criteria_factory import CreateConvergenceCriteria
+from co_simulation_tools import couplingsolverprint, red, green, cyan, bold
+
+
+def CreateSolver(cosim_solver_settings, level):
+    return SimpleSteadyCouplingSolver(cosim_solver_settings, level)
+
+class SimpleSteadyCouplingSolver(CoSimulationBaseCouplingSolver):
+    def __init__(self, cosim_solver_settings, level):
+        if not len(cosim_solver_settings["solvers"]) == 2:
+            raise Exception("Exactly two solvers have to be specified for the " + self.__class__.__name__ + "!")
+        super(SimpleSteadyCouplingSolver, self).__init__(cosim_solver_settings, level)
+
+        self.convergence_criteria = CreateConvergenceCriteria(
+            self.cosim_solver_settings["convergence_criteria_settings"],
+            self.solvers, self.lvl)
+        self.convergence_criteria.SetEchoLevel(self.echo_level)
+
+        self.num_coupling_iterations = self.cosim_solver_settings["num_coupling_iterations"]
+
+
+
+    def SolveSolutionStep(self):
+        for k in range(self.num_coupling_iterations):
+            if self.echo_level > 0:
+                couplingsolverprint(self.lvl, self._Name(),
+                                    cyan("Coupling iteration:"), bold(str(k+1)+" / " + str(self.num_coupling_iterations)))
+
+            for solver_name in self.solver_names:
+                solver = self.solvers[solver_name]
+                self._SynchronizeInputData(solver, solver_name)
+                solver.SolveSolutionStep()
+                self._SynchronizeOutputData(solver, solver_name)
