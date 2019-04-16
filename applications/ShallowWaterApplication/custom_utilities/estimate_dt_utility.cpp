626bfa8ae8e346f9c80a8d73af4271f83c27d6ae
Miguel <mmaso@cimne.upc.edu>
2019-04-16 11:44:05 +0200

    suggested changes

--- applications/ShallowWaterApplication/custom_utilities/estimate_dt_utility.cpp
+++ applications/ShallowWaterApplication/custom_utilities/estimate_dt_utility.cpp
@@ -19,8 +19,6 @@
 
 // Project includes
 #include "estimate_dt_utility.h"
-#include "utilities/openmp_utils.h"
-#include "processes/find_nodal_h_process.h"
 #include "shallow_water_application_variables.h"
 
 
@@ -29,7 +27,7 @@ namespace Kratos
 
 EstimateDtShallow::EstimateDtShallow(
     ModelPart& rThisModelPart,
-    Parameters& rThisParameters
+    Parameters ThisParameters
 ) : mrModelPart(rThisModelPart)
 {
 
@@ -38,81 +36,49 @@ EstimateDtShallow::EstimateDtShallow(
         "time_step"             : 1.0,
         "courant_number"        : 1.0,
         "consider_froude"       : true,
-        "compute_nodal_h"       : false,
         "minimum_delta_time"    : 1e-4,
         "maximum_delta_time"    : 1e+6
     })");
 
-    rThisParameters.ValidateAndAssignDefaults(default_parameters);
+    ThisParameters.ValidateAndAssignDefaults(default_parameters);
 
-    mEstimateDt = rThisParameters["automatic_time_step"].GetBool();
-    mConstantDt = rThisParameters["time_step"].GetDouble();
-    mCourant = rThisParameters["courant_number"].GetDouble();
-    mConsiderFroude = rThisParameters["consider_froude"].GetBool();
-    mComputeNodalH = rThisParameters["compute_nodal_h"].GetBool();
-    mMinDt = rThisParameters["minimum_delta_time"].GetDouble();
-    mMaxDt = rThisParameters["maximum_delta_time"].GetDouble();
-    mEpsilon = std::numeric_limits<double>::epsilon();
+    mEstimateDt = ThisParameters["automatic_time_step"].GetBool();
+    mConstantDt = ThisParameters["time_step"].GetDouble();
+    mCourant = ThisParameters["courant_number"].GetDouble();
+    mConsiderFroude = ThisParameters["consider_froude"].GetBool();
+    mMinDt = ThisParameters["minimum_delta_time"].GetDouble();
+    mMaxDt = ThisParameters["maximum_delta_time"].GetDouble();
 
-    if (mComputeNodalH)
-    {
-        FindNodalHProcess<true>(mrModelPart)();
+}
+
+
+double EstimateDtShallow::EstimateDt() const
+{
+    if (mEstimateDt) {
+        return EstimateTimeStep();
+    } else {
+        return mConstantDt;
     }
 }
 
 
-double EstimateDtShallow::EstimateDt()
+double EstimateDtShallow::EstimateTimeStep() const
 {
-    double time_step = mConstantDt;
-    if (mEstimateDt)
+    const double gravity = mrModelPart.GetProcessInfo().GetValue(GRAVITY_Z);
+    const auto nodes_begin = mrModelPart.NodesBegin();
+    double min_characteristic_time = std::numeric_limits<double>::max();
+
+    #pragma omp parallel for shared(min_characteristic_time)
+    for (int i = 0; i < static_cast<int>(mrModelPart.NumberOfNodes()); ++i)
     {
-        time_step = EstimateTimeStep();
-    }
-    return time_step;
-}
-
-
-double EstimateDtShallow::EstimateTimeStep()
-{
-    mGravity = mrModelPart.GetProcessInfo().GetValue(GRAVITY_Z);
-
-    unsigned int num_threads = OpenMPUtils::GetNumThreads();
-    OpenMPUtils::PartitionVector NodesPartition;
-    OpenMPUtils::DivideInPartitions(mrModelPart.NumberOfNodes(),num_threads, NodesPartition);
-
-    std::vector<double> min_characteristic_time(num_threads, 0.0);
-
-    #pragma omp parallel shared(min_characteristic_time)
-    {
-        int k = OpenMPUtils::ThisThread();
-        ModelPart::NodeIterator NodeBegin = mrModelPart.NodesBegin() + NodesPartition[k];
-        ModelPart::NodeIterator NodeEnd = mrModelPart.NodesBegin() + NodesPartition[k+1];
-
-        double min_thread_time = std::numeric_limits<double>::infinity();
-
-        for(ModelPart::NodeIterator itNode = NodeBegin; itNode != NodeEnd; ++itNode)
+        const double local_thread_min_characteristic_time = NodalCharacteristicTime(*(nodes_begin+i), gravity);
+        #pragma omp critical
         {
-            double characteristic_time = NodalCharacteristicTime(*itNode);
-            if (characteristic_time < min_thread_time)
-            {
-                min_thread_time = characteristic_time;
-            }
-        }
-
-        min_characteristic_time[k] = min_thread_time;
-    }
-
-    // Reduce to maximum the thread results
-    // Note that MSVC14 does not support max reductions, which are part of OpenMP 3.1
-    double current_time = min_characteristic_time[0];
-    for (unsigned int k = 1; k < num_threads; k++)
-    {
-        if (min_characteristic_time[k] < current_time)
-        {
-            current_time = min_characteristic_time[k];
+            min_characteristic_time = std::min(min_characteristic_time, local_thread_min_characteristic_time);
         }
     }
-    current_time *= mCourant;
+
+    double current_time = min_characteristic_time * mCourant;
 
     if (current_time < mMinDt) {current_time = mMinDt;}
     else if (current_time > mMaxDt) {current_time = mMaxDt;}
@@ -121,15 +87,16 @@ double EstimateDtShallow::EstimateTimeStep()
 }
 
 
-double EstimateDtShallow::NodalCharacteristicTime(Node<3>& rNode)
+double EstimateDtShallow::NodalCharacteristicTime(const Node<3>& rNode, double gravity) const
 {
     double velocity = norm_2(rNode.FastGetSolutionStepValue(VELOCITY));
     double wave_vel = 0.0;
+    const double epsilon = std::numeric_limits<double>::epsilon();
     if (mConsiderFroude)
     {
-        wave_vel = std::sqrt(mGravity * rNode.FastGetSolutionStepValue(HEIGHT));
+        wave_vel = std::sqrt(gravity * rNode.FastGetSolutionStepValue(HEIGHT));
     }
-    return rNode.FastGetSolutionStepValue(NODAL_H) / (velocity + wave_vel + mEpsilon);
+    return rNode.FastGetSolutionStepValue(NODAL_H) / (velocity + wave_vel + epsilon);
 }
 
 }  // namespace Kratos.
