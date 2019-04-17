e2f1b8cce95916d36a3c73e73dc39c63318b990d
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-04-17 19:21:18 +0200

    Update test

--- applications/ContactStructuralMechanicsApplication/tests/cpp_tests/test_weighted_gap.cpp
+++ applications/ContactStructuralMechanicsApplication/tests/cpp_tests/test_weighted_gap.cpp
@@ -91,12 +91,12 @@ namespace Kratos
                 p_node_2->Set(ACTIVE, true);
             }
 
-            std::size_t id_cond = 0;
+            std::size_t r_cond = 0;
             std::vector<Condition::Pointer> slave_conds;
             for (std::size_t i = 0; i < NumberOfDivisions; i++) {
-                id_cond++;
+                r_cond++;
                 const std::size_t ref_id = (2 * i)+1;
-                Condition::Pointer pcond = rModelPart.CreateNewCondition("Condition3D4N", id_cond, {{ref_id, ref_id + 1, ref_id + 3, ref_id + 2}}, p_cond_prop);
+                Condition::Pointer pcond = rModelPart.CreateNewCondition("Condition3D4N", r_cond, {{ref_id, ref_id + 1, ref_id + 3, ref_id + 2}}, p_cond_prop);
                 slave_model_part.AddCondition(pcond);
                 pcond->Set(SLAVE, true);
                 pcond->Set(MASTER, false);
@@ -129,10 +129,10 @@ namespace Kratos
             IndexSet this_set;
             std::vector<Condition::Pointer> master_conds;
             for (std::size_t i = 0; i < count - 1; i++) {
-                id_cond++;
-                this_set.AddId(id_cond);
+                r_cond++;
+                this_set.AddId(r_cond);
                 const std::size_t ref_id = (2 * (i + NumberOfDivisions + 1)+1);
-                Condition::Pointer pcond = rModelPart.CreateNewCondition("Condition3D4N", id_cond, {{ref_id +2, ref_id + 3, ref_id + 1, ref_id}}, p_cond_prop);
+                Condition::Pointer pcond = rModelPart.CreateNewCondition("Condition3D4N", r_cond, {{ref_id +2, ref_id + 3, ref_id + 1, ref_id}}, p_cond_prop);
                 master_model_part.AddCondition(pcond);
                 pcond->Set(SLAVE, false);
                 pcond->Set(MASTER, true);
@@ -150,11 +150,11 @@ namespace Kratos
             }
 
             // We set the auxiliar Coordinates
-            for(auto& inode : rModelPart.Nodes()) {
-                if (inode.Is(MASTER))
-                    inode.SetValue(AUXILIAR_COORDINATES, inode.Coordinates());
+            for(auto& r_node : rModelPart.Nodes()) {
+                if (r_node.Is(MASTER))
+                    r_node.SetValue(AUXILIAR_COORDINATES, r_node.Coordinates());
                 else
-                    inode.SetValue(AUXILIAR_COORDINATES, ZeroVector(3));
+                    r_node.SetValue(AUXILIAR_COORDINATES, ZeroVector(3));
             }
 
             // We set the mapper parameters
@@ -166,29 +166,29 @@ namespace Kratos
             mapper.Execute();
 
             // We compute now the normal gap and set the nodes under certain threshold as active
-            for(auto& inode : rModelPart.Nodes()) {
-                if (inode.Is(SLAVE)) {
+            for(auto& r_node : rModelPart.Nodes()) {
+                if (r_node.Is(SLAVE)) {
                     // We compute the gap
-                    const array_1d<double, 3>& normal = inode.FastGetSolutionStepValue(NORMAL);
-                    const array_1d<double, 3>& auxiliar_coordinates = inode.GetValue(AUXILIAR_COORDINATES);
-                    const array_1d<double, 3>& components_gap = ( inode.Coordinates() - auxiliar_coordinates);
+                    const array_1d<double, 3>& normal = r_node.FastGetSolutionStepValue(NORMAL);
+                    const array_1d<double, 3>& auxiliar_coordinates = r_node.GetValue(AUXILIAR_COORDINATES);
+                    const array_1d<double, 3>& components_gap = ( r_node.Coordinates() - auxiliar_coordinates);
                     const double gap = inner_prod(components_gap, - normal);
-                    inode.SetValue(NORMAL_GAP, gap);
+                    r_node.SetValue(NORMAL_GAP, gap);
                 } else
-                    inode.SetValue(NORMAL_GAP, 0.0);
+                    r_node.SetValue(NORMAL_GAP, 0.0);
             }
 
             // We set the database
             auto& process_info = rModelPart.GetProcessInfo();
             ModelPart& computing_rcontact_model_part = rModelPart.GetSubModelPart("ComputingContact");
-            for (auto& slave_cond : slave_conds) {
-                for (auto& master_cond : master_conds) {
-                    id_cond++;
-                    Condition::Pointer p_auxiliar_condition = computing_rcontact_model_part.CreateNewCondition("ALMFrictionalMortarContactCondition3D4N", id_cond, slave_cond->GetGeometry(), p_cond_prop);
+            for (auto& r_slave_cond : slave_conds) {
+                for (auto& r_master_cond : master_conds) {
+                    r_cond++;
+                    Condition::Pointer p_auxiliar_condition = computing_rcontact_model_part.CreateNewCondition("ALMFrictionalMortarContactCondition3D4N", r_cond, r_slave_cond->GetGeometry(), p_cond_prop);
                     // We set the geometrical values
-                    p_auxiliar_condition->SetValue(PAIRED_GEOMETRY, master_cond->pGetGeometry());
-                    p_auxiliar_condition->SetValue(NORMAL, slave_cond->GetValue(NORMAL));
-                    p_auxiliar_condition->SetValue(PAIRED_NORMAL, master_cond->GetValue(NORMAL));
+                    p_auxiliar_condition->SetValue(PAIRED_GEOMETRY, r_master_cond->pGetGeometry());
+                    p_auxiliar_condition->SetValue(NORMAL, r_slave_cond->GetValue(NORMAL));
+                    p_auxiliar_condition->SetValue(PAIRED_NORMAL, r_master_cond->GetValue(NORMAL));
                     // We activate the condition and initialize it
                     p_auxiliar_condition->Set(ACTIVE, true);
                     p_auxiliar_condition->Initialize();
@@ -198,34 +198,38 @@ namespace Kratos
 
             // We move mesh in order to test the slip
             if (MoveMesh) {
-                for (auto& inode : rModelPart.Nodes()) {
-                    if (inode.Is(MASTER)) {
-                        inode.FastGetSolutionStepValue(DISPLACEMENT_X) = 0.1;
-                        inode.Coordinates() += inode.FastGetSolutionStepValue(DISPLACEMENT);
+                for (auto& r_node : rModelPart.Nodes()) {
+                    if (r_node.Is(MASTER)) {
+                        r_node.FastGetSolutionStepValue(DISPLACEMENT_X) = 0.1;
+                        r_node.Coordinates() += r_node.FastGetSolutionStepValue(DISPLACEMENT);
                     }
+                }
+                // Update the previous operator
+                for (auto& r_cond : rModelPart.Conditions()) {
+                    r_cond.FinalizeSolutionStep(process_info);
                 }
 
                 // We set the auxiliar Coordinates
-                for(auto& inode : rModelPart.Nodes()) {
-                    if (inode.Is(MASTER))
-                        inode.SetValue(AUXILIAR_COORDINATES, inode.Coordinates());
+                for(auto& r_node : rModelPart.Nodes()) {
+                    if (r_node.Is(MASTER))
+                        r_node.SetValue(AUXILIAR_COORDINATES, r_node.Coordinates());
                     else
-                        inode.SetValue(AUXILIAR_COORDINATES, ZeroVector(3));
+                        r_node.SetValue(AUXILIAR_COORDINATES, ZeroVector(3));
                 }
 
                 mapper.Execute();
 
                 // We compute now the normal gap and set the nodes under certain threshold as active
-                for(auto& inode : rModelPart.Nodes()) {
-                    if (inode.Is(SLAVE)) {
+                for(auto& r_node : rModelPart.Nodes()) {
+                    if (r_node.Is(SLAVE)) {
                         // We compute the gap
-                        const array_1d<double, 3>& normal = inode.FastGetSolutionStepValue(NORMAL);
-                        const array_1d<double, 3>& auxiliar_coordinates = inode.GetValue(AUXILIAR_COORDINATES);
-                        const array_1d<double, 3>& components_gap = ( inode.Coordinates() - auxiliar_coordinates);
+                        const array_1d<double, 3>& normal = r_node.FastGetSolutionStepValue(NORMAL);
+                        const array_1d<double, 3>& auxiliar_coordinates = r_node.GetValue(AUXILIAR_COORDINATES);
+                        const array_1d<double, 3>& components_gap = ( r_node.Coordinates() - auxiliar_coordinates);
                         const double gap = inner_prod(components_gap, - normal);
-                        inode.SetValue(NORMAL_GAP, gap);
+                        r_node.SetValue(NORMAL_GAP, gap);
                     } else
-                        inode.SetValue(NORMAL_GAP, 0.0);
+                        r_node.SetValue(NORMAL_GAP, 0.0);
                 }
             }
         }
@@ -234,7 +238,6 @@ namespace Kratos
         * Checks the correct work of the weighted gap computation
         * Test 1
         */
-
         KRATOS_TEST_CASE_IN_SUITE(WeightedGap1, KratosContactStructuralMechanicsFastSuite)
         {
             Model this_model;
@@ -265,18 +268,18 @@ namespace Kratos
 
             // We compute the explicit contribution
             VariableUtils().SetScalarVar<Variable<double>>(WEIGHTED_GAP, 0.0, r_model_part.Nodes());
-            for (auto& id_cond : r_model_part.GetSubModelPart("ComputingContact").Conditions())
-                id_cond.AddExplicitContribution(process_info);
+            for (auto& r_cond : r_model_part.GetSubModelPart("ComputingContact").Conditions())
+                r_cond.AddExplicitContribution(process_info);
 
             // DEBUG
 //             GiDIOGapDebug(r_model_part);
 
             const double tolerance = 1.0e-4;
-            for (auto& inode : r_model_part.Nodes()) {
-                if (inode.Is(SLAVE)) {
-                    if (std::abs(inode.FastGetSolutionStepValue(WEIGHTED_GAP)) > 0.0) {
-                        const double normal_gap = inode.GetValue(NORMAL_GAP);
-                        const double weighted_gap_corrected = inode.FastGetSolutionStepValue(WEIGHTED_GAP)/inode.GetValue(NODAL_AREA);
+            for (auto& r_node : r_model_part.Nodes()) {
+                if (r_node.Is(SLAVE)) {
+                    if (std::abs(r_node.FastGetSolutionStepValue(WEIGHTED_GAP)) > 0.0) {
+                        const double normal_gap = r_node.GetValue(NORMAL_GAP);
+                        const double weighted_gap_corrected = r_node.FastGetSolutionStepValue(WEIGHTED_GAP)/r_node.GetValue(NODAL_AREA);
                         KRATOS_CHECK_LESS_EQUAL(std::abs(weighted_gap_corrected - normal_gap)/std::abs(normal_gap), tolerance);
                     }
                 }
@@ -287,7 +290,6 @@ namespace Kratos
         * Checks the correct work of the weighted gap computation
         * Test 2
         */
-
         KRATOS_TEST_CASE_IN_SUITE(WeightedGap2, KratosContactStructuralMechanicsFastSuite)
         {
             Model this_model;
@@ -317,24 +319,24 @@ namespace Kratos
             CreatePlaneCilynderProblem(r_model_part, number_of_divisions, lenght, radius, angle, slope);
 
             // We compute the explicit contribution
-            const array_1d<double, 3> zero_vector(3, 0.0);
+            const array_1d<double, 3> zero_vector = ZeroVector(3);;
             VariableUtils().SetScalarVar<Variable<double>>(WEIGHTED_GAP, 0.0, r_model_part.Nodes());
             VariableUtils().SetVectorVar(WEIGHTED_SLIP, zero_vector, r_model_part.Nodes());
-            for (auto& id_cond : r_model_part.GetSubModelPart("ComputingContact").Conditions())
-                id_cond.AddExplicitContribution(process_info);
+            for (auto& r_cond : r_model_part.GetSubModelPart("ComputingContact").Conditions())
+                r_cond.AddExplicitContribution(process_info);
 
             // DEBUG
 //             GiDIOGapDebug(r_model_part);
 
             const double tolerance = 1.0e-4;
-            for (auto& inode : r_model_part.Nodes()) {
-                if (inode.Is(SLAVE)) {
-                    if (std::abs(inode.FastGetSolutionStepValue(WEIGHTED_GAP)) > 0.0) {
-                        const double normal_gap = inode.GetValue(NORMAL_GAP);
-                        const double weighted_gap_corrected = inode.FastGetSolutionStepValue(WEIGHTED_GAP)/inode.GetValue(NODAL_AREA);
+            for (auto& r_node : r_model_part.Nodes()) {
+                if (r_node.Is(SLAVE)) {
+                    if (std::abs(r_node.FastGetSolutionStepValue(WEIGHTED_GAP)) > 0.0) {
+                        const double normal_gap = r_node.GetValue(NORMAL_GAP);
+                        const double weighted_gap_corrected = r_node.FastGetSolutionStepValue(WEIGHTED_GAP)/r_node.GetValue(NODAL_AREA);
                         KRATOS_CHECK_LESS_EQUAL(std::abs(weighted_gap_corrected - normal_gap)/std::abs(normal_gap), tolerance);
                     }
-                    KRATOS_CHECK_LESS_EQUAL(norm_2(inode.FastGetSolutionStepValue(WEIGHTED_SLIP)), tolerance);
+                    KRATOS_CHECK_LESS_EQUAL(norm_2(r_node.FastGetSolutionStepValue(WEIGHTED_SLIP)), tolerance);
                 }
             }
         }
@@ -343,7 +345,6 @@ namespace Kratos
         * Checks the correct work of the weighted gap computation
         * Test 3
         */
-
         KRATOS_TEST_CASE_IN_SUITE(WeightedGap3, KratosContactStructuralMechanicsFastSuite)
         {
             Model this_model;
@@ -373,25 +374,26 @@ namespace Kratos
             CreatePlaneCilynderProblem(r_model_part, number_of_divisions, lenght, radius, angle, slope, true);
 
             // We compute the explicit contribution
-            const array_1d<double, 3> zero_vector(3, 0.0);
+            const array_1d<double, 3> zero_vector = ZeroVector(3);;
             VariableUtils().SetScalarVar<Variable<double>>(WEIGHTED_GAP, 0.0, r_model_part.Nodes());
             VariableUtils().SetVectorVar(WEIGHTED_SLIP, zero_vector, r_model_part.Nodes());
-            for (auto& id_cond : r_model_part.GetSubModelPart("ComputingContact").Conditions())
-                id_cond.AddExplicitContribution(process_info);
+            for (auto& r_cond : r_model_part.GetSubModelPart("ComputingContact").Conditions())
+                r_cond.AddExplicitContribution(process_info);
 
 //             // DEBUG
 //             GiDIOGapDebug(r_model_part);
 
             const double tolerance = 1.0e-4;
-            array_1d<double, 3> slip(3, 0.0);
+            array_1d<double, 3> slip = ZeroVector(3);
             slip[0] = 0.1;
-            for (auto& inode : r_model_part.Nodes()) {
-                if (inode.Is(SLAVE)) {
-                    const double normal_gap = inode.GetValue(NORMAL_GAP);
-                    const double weighted_gap_corrected = inode.FastGetSolutionStepValue(WEIGHTED_GAP)/inode.GetValue(NODAL_AREA);
+            for (auto& r_node : r_model_part.Nodes()) {
+                if (r_node.Is(SLAVE)) {
+                    const double normal_gap = r_node.GetValue(NORMAL_GAP);
+                    const double weighted_gap_corrected = r_node.FastGetSolutionStepValue(WEIGHTED_GAP)/r_node.GetValue(NODAL_AREA);
                     if (std::abs(weighted_gap_corrected - normal_gap)/std::abs(normal_gap) < tolerance) {
-                        if (norm_2(inode.FastGetSolutionStepValue(WEIGHTED_SLIP)) > 0.0) {
-                            const array_1d<double, 3> weighted_slip_corrected = inode.FastGetSolutionStepValue(WEIGHTED_SLIP)/inode.GetValue(NODAL_AREA);
+                        if (norm_2(r_node.FastGetSolutionStepValue(WEIGHTED_SLIP)) > 0.0) {
+                            const array_1d<double, 3> weighted_slip_corrected = r_node.FastGetSolutionStepValue(WEIGHTED_SLIP)/r_node.GetValue(NODAL_AREA);
+                            KRATOS_WATCH(weighted_slip_corrected)
                             KRATOS_CHECK_LESS_EQUAL(norm_2(weighted_slip_corrected - slip)/norm_2(slip), tolerance);
                         }
                     }
