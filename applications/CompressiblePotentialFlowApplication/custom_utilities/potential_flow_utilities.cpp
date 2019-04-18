3274b8ca46640e4e56a30c926732606320f91a42
Inigo Lopez <inigolcanalejo@gmail.com>
2019-04-18 13:37:27 +0200

    including proper header

--- applications/CompressiblePotentialFlowApplication/custom_utilities/potential_flow_utilities.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_utilities/potential_flow_utilities.cpp
@@ -1,3 +1,14 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Inigo Lopez
+
 #include "custom_utilities/potential_flow_utilities.h"
 
 namespace Kratos
