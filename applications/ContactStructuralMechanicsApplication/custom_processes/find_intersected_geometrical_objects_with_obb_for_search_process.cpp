a802fb8bf95c48ec11b4d197364713fcbb8b31a8
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-04-21 22:47:07 +0200

    Moving Parameters to base class

--- applications/ContactStructuralMechanicsApplication/custom_processes/find_intersected_geometrical_objects_with_obb_for_search_process.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_processes/find_intersected_geometrical_objects_with_obb_for_search_process.cpp
@@ -39,17 +39,17 @@ FindIntersectedGeometricalObjectsWithOBBForSearchProcess::FindIntersectedGeometr
 {
     const Parameters default_parameters = GetDefaultParameters();
     ThisParameters.RecursivelyValidateAndAssignDefaults(default_parameters);
-    BaseProcessType::mThisParameters = ThisParameters;
+    BaseType::mThisParameters = ThisParameters;
 
     // Checking that the names of the model parts are not empty (this is supposed to be already declared)
-    const std::string& r_first_model_part_name = BaseProcessType::mThisParameters["first_model_part_name"].GetString();
-    const std::string& r_second_model_part_name = BaseProcessType::mThisParameters["second_model_part_name"].GetString();
+    const std::string& r_first_model_part_name = BaseType::mThisParameters["first_model_part_name"].GetString();
+    const std::string& r_second_model_part_name = BaseType::mThisParameters["second_model_part_name"].GetString();
 
     KRATOS_ERROR_IF(r_first_model_part_name == "") << "first_model_part_name must be defined on parameters" << std::endl;
     KRATOS_ERROR_IF(r_second_model_part_name == "") << "second_model_part_name must be defined on parameters" << std::endl;
 
     // Setting the bounding box factor
-    mBoundingBoxFactor = BaseProcessType::mThisParameters["bounding_box_factor"].GetDouble();
+    mBoundingBoxFactor = BaseType::mThisParameters["bounding_box_factor"].GetDouble();
 
     // In case we consider the bounding box we set the BOUNDARY flag
     if (mBoundingBoxFactor > 0.0)
@@ -58,10 +58,10 @@ FindIntersectedGeometricalObjectsWithOBBForSearchProcess::FindIntersectedGeometr
         this->Set(BOUNDARY, false);
 
     // If we debug OBB
-    mDebugOBB = BaseProcessType::mThisParameters["debug_obb"].GetBool();
+    mDebugOBB = BaseType::mThisParameters["debug_obb"].GetBool();
 
     // The intersection type
-    mIntersectionType = BaseType::ConvertInter(BaseProcessType::mThisParameters["OBB_intersection_type"].GetString());
+    mIntersectionType = BaseType::ConvertInter(BaseType::mThisParameters["OBB_intersection_type"].GetString());
 
     // We create new properties for debugging
     if (BaseType::mDebugOBB) {
