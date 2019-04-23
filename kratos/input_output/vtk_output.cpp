f7fbf09f8849a0407e809c030369f844486845a9
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-04-23 07:43:37 +0200

    [Core] Adding quadratic geometries VTKOutput
    Fixes 4709. See https://vtk.org/wp-content/uploads/2015/04/file-formats.pdf page 10

--- kratos/input_output/vtk_output.cpp
+++ kratos/input_output/vtk_output.cpp
@@ -339,7 +339,15 @@ void VtkOutput::WriteCellType(const TContainerType& rContainer, std::ofstream& r
         { GeometryData::KratosGeometryType::Kratos_Quadrilateral3D4, 9 },
         { GeometryData::KratosGeometryType::Kratos_Tetrahedra3D4,    10 },
         { GeometryData::KratosGeometryType::Kratos_Hexahedra3D8,     12 },
-        { GeometryData::KratosGeometryType::Kratos_Prism3D6,         13 }
+        { GeometryData::KratosGeometryType::Kratos_Prism3D6,         13 },
+        { GeometryData::KratosGeometryType::Kratos_Line2D3,          21 },
+        { GeometryData::KratosGeometryType::Kratos_Line3D3,          21 },
+        { GeometryData::KratosGeometryType::Kratos_Triangle2D6,      22 },
+        { GeometryData::KratosGeometryType::Kratos_Triangle3D6,      22 },
+        { GeometryData::KratosGeometryType::Kratos_Quadrilateral2D8, 23 },
+        { GeometryData::KratosGeometryType::Kratos_Quadrilateral3D8, 23 },
+        { GeometryData::KratosGeometryType::Kratos_Tetrahedra3D10,   24 }
+//         { GeometryData::KratosGeometryType::Kratos_Hexahedra3D20,    25 } // NOTE: Quadratic hexahedra (20) requires a conversor, order does not coincide with VTK
     };
     // Write entity types
     for (const auto& r_entity : rContainer) {
