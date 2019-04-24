8acf639e4d8fb7eba64bef81de52cc4fe1849ab3
philbucher <philipp.bucher@tum.de>
2019-04-24 12:52:33 +0200

    updating coupling-interface-data

--- applications/CoSimulationApplication/python_scripts/coupling_interface_data.py
+++ applications/CoSimulationApplication/python_scripts/coupling_interface_data.py
@@ -22,7 +22,7 @@ class CouplingInterfaceData(object):
 
         self.name = custom_config["name"].GetString()
         self.variable = None
-        # self.filters = []
+        self.filters = []
         self.solver = solver
         self.dimension = custom_config["dimension"].GetInt()
         self.location_on_mesh = custom_config["location_on_mesh"].GetString()
@@ -31,34 +31,34 @@ class CouplingInterfaceData(object):
         self.origin_data = None
         self.mapper_settings = None
 
-    # def ApplyFilters(self):
-    #     for filter in self.filters:
-    #         filter.Apply()
+    def ApplyFilters(self):
+        for filter in self.filters:
+            filter.Apply()
 
     def GetPythonList(self):
-        data = []
         data_mesh = self.solver.model[self.mesh_name]
+        data = [0]*len(data_mesh.Nodes)*self.dimension
         data_variable = cs_data_structure.KratosGlobals.GetVariable(self.name)
+        node_index = 0
         for node in data_mesh.Nodes:
             data_value = node.GetSolutionStepValue(data_variable,0) #TODO what if non-historical?
-            for value in data_value:
-                data.append(value)
+            for i in range(self.dimension):
+                data[node_index*self.dimension + i] = data_value[i]
+            node_index+=1
         return data
 
     def GetNumpyArray(self):
-        return np.array(self.GetPythonList())
+        return np.asarray(self.GetPythonList(), dtype=np.float64)
 
     def ApplyUpdateToData(self, update):
         data_mesh = self.solver.model[self.mesh_name]
         data_variable = cs_data_structure.KratosGlobals.GetVariable(self.name)
-        index = 0
+        node_index = 0
         for node in data_mesh.Nodes: # #TODO: local nodes to also work in MPI?
-            updated_value = []
-            value = node.GetSolutionStepValue(data_variable,0)
+            updated_value = [0]*self.dimension
             # TODO: aditya the data might also be non-historical => GetValue
-            for value_i in value:
-                updated_value.append(update[index])
-                index = index + 1
+            for i in range(self.dimension):
+                updated_value[i] = update[node_index*self.dimension + i]
+
             node.SetSolutionStepValue(data_variable, 0, updated_value)
-
-
+            node_index += 1
