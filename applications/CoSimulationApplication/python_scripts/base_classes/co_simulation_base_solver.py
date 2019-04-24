a3a3d972de71755b0436042ecf2036442b3c5a87
philbucher <philipp.bucher@tum.de>
2019-04-24 12:47:46 +0200

    adding settings to base-solver

--- applications/CoSimulationApplication/python_scripts/base_classes/co_simulation_base_solver.py
+++ applications/CoSimulationApplication/python_scripts/base_classes/co_simulation_base_solver.py
@@ -3,6 +3,11 @@ from __future__ import print_function, absolute_import, division
 # Other imports
 import KratosMultiphysics.CoSimulationApplication.solver_interfaces.co_simulation_io_factory as io_factory
 
+import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as tools
+# Other imports
+cs_data_structure = tools.cs_data_structure
+from KratosMultiphysics.CoSimulationApplication.coupling_interface_data import CouplingInterfaceData
+
 def CreateSolver(model, cosim_solver_settings):
     return CoSimulationBaseSolver(model, cosim_solver_settings)
 
@@ -13,13 +18,21 @@ class CoSimulationBaseSolver(object):
         """Constructor of the Base-Solver
         Deriving classes should call it in their constructors
         """
+        default_settings = cs_data_structure.Parameters("""{
+            "solver_type" : "",
+            "io_settings" : {},
+            "settings"    : {},
+            "data"        : [],
+            "echo_level"  : 0
+        }""")
+
         self.model = model
         self.cosim_solver_settings = cosim_solver_settings
+        self.cosim_solver_settings.AddMissingParameters(default_settings)
         self.name = solver_name
-        self.echo_level = 0
-        if "echo_level" in self.cosim_solver_settings:
-            self.echo_level = self.cosim_solver_settings["echo_level"]
+        self.echo_level = self.cosim_solver_settings["echo_level"].GetInt()
         self.io_is_initialized = False
+        # self.data_map = self.__CreateInterfaceDataMap()
 
     def Initialize(self):
         pass
@@ -101,3 +114,15 @@ class CoSimulationBaseSolver(object):
     def _GetIOName(self):
         raise Exception('"_GetIOName" function must be implemented in derived class!')
 
+    ## __CreateInterfaceDataMap : Private Function to obtain the map of data objects
+    #
+    #  @param self            The object pointer.
+    def __CreateInterfaceDataMap(self):
+        data_map = dict()
+        num_data = self.cosim_solver_settings["data"].size()
+        for data_conf in self.cosim_solver_settings["data"]:
+            data_name = data_conf["name"].GetString()
+            data_obj = CouplingInterfaceData(data_conf, self)
+            data_map[data_name] = data_obj
+
+        return data_map
