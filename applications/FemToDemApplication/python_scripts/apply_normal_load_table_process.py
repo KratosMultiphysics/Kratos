12805181ae9b97792e7c854850fcea7907a99f28
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-04-26 14:24:57 +0200

    more removing

--- applications/FemToDemApplication/python_scripts/apply_normal_load_table_process.py
+++ applications/FemToDemApplication/python_scripts/apply_normal_load_table_process.py
@@ -19,22 +19,13 @@ class ApplyNormalLoadTableProcess(KratosMultiphysics.Process):
             normal_params = KratosMultiphysics.Parameters("{}")
             normal_params.AddValue("model_part_name",settings["model_part_name"])
             normal_params.AddValue("variable_name",settings["variable_name"])
-            if settings["hydrostatic"].GetBool() == False:
-                normal_params.AddValue("value",settings["value"][0])
-                if settings["table"][0].GetInt() == 0:
-                    self.components_process_list.append(KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, normal_params))
-                else:
-                    normal_params.AddValue("table",settings["table"][0])
-                    self.components_process_list.append(KratosPoro.ApplyDoubleTableProcess(model_part, normal_params))
+
+            normal_params.AddValue("value",settings["value"][0])
+            if settings["table"][0].GetInt() == 0:
+                self.components_process_list.append(KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, normal_params))
             else:
-                normal_params.AddValue("gravity_direction",settings["gravity_direction"])
-                normal_params.AddValue("reference_coordinate",settings["reference_coordinate"])
-                normal_params.AddValue("specific_weight",settings["specific_weight"])
-                if settings["table"][0].GetInt() == 0:
-                    self.components_process_list.append(KratosPoro.ApplyConstantHydrostaticPressureProcess(model_part, normal_params))
-                else:
-                    normal_params.AddValue("table",settings["table"][0])
-                    self.components_process_list.append(KratosPoro.ApplyHydrostaticPressureTableProcess(model_part, normal_params))
+                normal_params.AddValue("table",settings["table"][0])
+                self.components_process_list.append(KratosFemDem.ApplyDoubleTableProcess(model_part, normal_params))
 
         if settings["active"][1].GetBool() == True:
             tangential_params = KratosMultiphysics.Parameters("{}")
@@ -45,7 +36,7 @@ class ApplyNormalLoadTableProcess(KratosMultiphysics.Process):
                 self.components_process_list.append(KratosMultiphysics.ApplyConstantScalarValueProcess(model_part, tangential_params))
             else:
                 tangential_params.AddValue("table",settings["table"][1])
-                self.components_process_list.append(KratosPoro.ApplyDoubleTableProcess(model_part, tangential_params))
+                self.components_process_list.append(KratosFemDem.ApplyDoubleTableProcess(model_part, tangential_params))
                 
     def ExecuteInitialize(self):
         for component in self.components_process_list:
