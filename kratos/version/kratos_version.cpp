294304ba233e096dcd8f3cf97b44809837d4b2a6
Carlos Roig <roigcarlo@gmail.com>
2019-04-29 10:06:22 +0200

    Split version and patch

--- kratos/version/kratos_version.cpp
+++ kratos/version/kratos_version.cpp
@@ -2,19 +2,6 @@
 
 namespace Kratos {
 
-// Kratos Minor, major and patch
-#ifndef KRATOS_MAJOR_VERSION
-#define KRATOS_MAJOR_VERSION 7
-#endif
-
-#ifndef KRATOS_MINOR_VERSION
-#define KRATOS_MINOR_VERSION 0
-#endif
-
-#ifndef KRATOS_PATCH_VERSION
-#define KRATOS_PATCH_VERSION 0
-#endif
-
 // GiT revision at configure
 #ifndef KRATOS_SHA1_NUMBER
 #define KRATOS_SHA1_NUMBER "0"
@@ -33,18 +20,6 @@ KRATOS_TO_STRING(KRATOS_MINOR_VERSION) "." \
 KRATOS_TO_STRING(KRATOS_PATCH_VERSION) "-" \
 KRATOS_SHA1_NUMBER "-" \
 KRATOS_BUILD_TYPE
-
-int GetMajorVersion() {
-    return KRATOS_MAJOR_VERSION;
-}
-
-int GetMinorVersion() {
-    return KRATOS_MINOR_VERSION;
-}
-
-int GetPatchVersion() {
-    return KRATOS_PATCH_VERSION;
-}
 
 const char * GetCommitVersion() {
     return KRATOS_SHA1_NUMBER;
--- kratos/version/kratos_version.h
+++ kratos/version/kratos_version.h
@@ -2,9 +2,31 @@
 
 namespace Kratos {
 
-int GetMajorVersion();
-int GetMinorVersion();
-int GetPatchVersion();
+// Kratos Minor, major and patch
+#ifndef KRATOS_MAJOR_VERSION
+#define KRATOS_MAJOR_VERSION 7
+#endif
+
+#ifndef KRATOS_MINOR_VERSION
+#define KRATOS_MINOR_VERSION 0
+#endif
+
+#ifndef KRATOS_PATCH_VERSION
+#define KRATOS_PATCH_VERSION 0
+#endif
+
+constexpr int GetMajorVersion() {
+    return KRATOS_MAJOR_VERSION;
+}
+
+constexpr int GetMinorVersion() {
+    return KRATOS_MINOR_VERSION;
+}
+
+constexpr int GetPatchVersion() {
+    return KRATOS_PATCH_VERSION;
+}
+
 const char * GetCommit();
 const char * GetBuildType();
 const char * GetVersionString();
