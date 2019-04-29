121353824000ec4c41568f9eb69bd00b3d396849
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2019-04-29 12:36:59 +0200

    Documentation

--- kratos/processes/calculate_embedded_nodal_variable_from_skin_process.h
+++ kratos/processes/calculate_embedded_nodal_variable_from_skin_process.h
@@ -637,7 +637,12 @@ protected:
 
     /**
      * @brief Construct a new Calculate Embedded Nodal Variable From Skin Process object
-     * Constructor with background and skin model parts as well as json settings.
+     * Constructor with background and skin model parts as well as json settings. This
+     * constructor is intentionally protected to avoid exposing it to the user since it
+     * is intended to serve as an auxiliar constructor to bridge from the model and
+     * parameters one, which checks the provided settings with the defaults, to the "old
+     * fashioned" one. This allows keeping the member variables as const as well as to
+     * have a unique implementation of the constructor required checks and operations.
      * @param rBaseModelPart Background mesh model part reference
      * @param rSkinModelPart Embedded skin model part reference
      * @param rSettings Settings json string
