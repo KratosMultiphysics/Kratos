680a2b933d94987d4d2c6ab3109b23797803e172
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-04-29 19:31:26 +0200

    Revert "Refactoring for contact"
    This reverts commit ff014578088db0c675378d318db27e4875832034.

--- applications/ContactStructuralMechanicsApplication/custom_processes/find_intersected_geometrical_objects_with_obb_for_search_process.h
+++ applications/ContactStructuralMechanicsApplication/custom_processes/find_intersected_geometrical_objects_with_obb_for_search_process.h
@@ -17,8 +17,7 @@
 // External includes
 
 // Project includes
-#include "processes/find_intersected_geometrical_objects_process.h"
-#include "includes/oriented_bounding_box.h"
+#include "processes/find_intersected_geometrical_objects_with_obb_process.h"
 
 namespace Kratos
 {
@@ -49,7 +48,7 @@ namespace Kratos
  * @author Vicente Mataix Ferrandiz
 */
 class KRATOS_API(CONTACT_STRUCTURAL_MECHANICS_APPLICATION) FindIntersectedGeometricalObjectsWithOBBForSearchProcess
-    : public Process
+    : public FindIntersectedGeometricalObjectsWithOBBProcess<Condition>
 {
 public:
     ///@name Type Definitions
@@ -57,13 +56,6 @@ public:
 
     /// Pointer definition of FindIntersectedGeometricalObjectsWithOBBForSearchProcess
     KRATOS_CLASS_POINTER_DEFINITION(FindIntersectedGeometricalObjectsWithOBBForSearchProcess);
-
-    /// Octree definitions
-    using ConfigurationType = Internals::ImplementationDistanceSpatialContainersConfigure<Condition>;
-    using CellType = OctreeBinaryCell<ConfigurationType>;
-    using OctreeType = OctreeBinary<CellType>;
-    using CellNodeDataType = typename ConfigurationType::cell_node_data_type;
-    typedef std::vector<typename OctreeType::cell_type*> OtreeCellVectorType;
 
     /// Definition of the index type
     typedef std::size_t IndexType;
@@ -74,11 +66,23 @@ public:
     /// Definition of the point type
     typedef Point PointType;
 
+    /// Definition of the base type
+    typedef FindIntersectedGeometricalObjectsProcess<Condition> BaseProcessType;
+
+    /// Definition of the base type
+    typedef FindIntersectedGeometricalObjectsWithOBBProcess<Condition> BaseType;
+
+    /// Octree type definition
+    typedef typename BaseType::OctreeType OctreeType;
+
     /// Definition of the node type
     using NodeType = Node<3>;
 
     /// Definition of the geometry type
     using GeometryType = Geometry<NodeType>;
+
+    /// Definition of the entity container type
+    typedef PointerVectorSet<Condition, IndexedObject> EntityContainerType;
 
     ///@}
     ///@name Life Cycle
@@ -92,12 +96,12 @@ public:
 
     /**
      * @brief Constructor to be used.
-     * @param rModelPartIntersected First model part (the one to compute the intersection)
-     * @param rModelPartIntersecting Second model part (the "skin" model part)
+     * @param rPart1 First model part (the one to compute the intersection)
+     * @param rPart2 Second model part (the "skin" model part)
      */
     FindIntersectedGeometricalObjectsWithOBBForSearchProcess(
-        ModelPart& rModelPartIntersected,
-        ModelPart& rModelPartIntersecting,
+        ModelPart& rPart1,
+        ModelPart& rPart2,
         const double BoundingBoxFactor = -1.0,
         const bool DebugOBB = false,
         OBBHasIntersectionType IntersectionType = OBBHasIntersectionType::SeparatingAxisTheorem
@@ -122,78 +126,9 @@ public:
     ///@name Member Variables
     ///@{
 
-    std::vector<PointerVector<GeometricalObject>> mIntersectedObjects; /// The list of intersected objects
-
     ///@}
     ///@name Operations
     ///@{
-
-    /**
-     * @brief This function is designed for being called at the beginning of the computations right after reading the model and the groups
-     * @todo This should be moved to ExecuteInitialize (base class of Process)
-     */
-    void Initialize();
-
-    /**
-     * @brief This method finds the intersected objects with the skin
-     * @param rResults The vector containing the intersected objects with the skin
-     */
-    void FindIntersectedSkinObjects(std::vector<PointerVector<GeometricalObject>>& rResults);
-
-    /**
-     * @brief This method finds different intersections
-     */
-    void FindIntersections();
-
-    /**
-     * @brief This method returns the intersections
-     * @return The vector containing the intersections found
-     */
-    std::vector<PointerVector<GeometricalObject>>& GetIntersections();
-
-    /**
-     * @brief Returns the first model part
-     * @return The first model part
-     */
-    ModelPart& GetModelPart1();
-
-    /**
-     * @brief Returns the second model part
-     * @return The second model part
-     */
-    ModelPart& GetModelPart2();
-
-    /**
-     * @brief This method returns the Octree conatined in the class
-     * @return The octree contained in this process
-     */
-    OctreeBinary<OctreeBinaryCell<ConfigurationType>>* GetOctreePointer();
-
-    /**
-     * @brief This clears the database
-     * @warning This conflicts with flags Clear
-     */
-    void Clear();
-
-    /**
-     * @brief Execute method is used to execute the Process algorithms.
-     */
-    void Execute() override;
-
-    /**
-     * @brief this function is designed for being called at the beginning of the computations right after reading the model and the groups
-     */
-    void ExecuteInitialize() override;
-
-    /**
-     * @brief This method indetifies near entities and marks if intersected (Condition)
-     * @param pCondition The pointer to the condition of interest
-     * @param rLeaves The Octree cells vectors
-     */
-    void IdentifyNearEntitiesAndCheckEntityForIntersection(
-        Condition::Pointer pCondition,
-        OtreeCellVectorType& rLeaves
-        );
 
     ///@}
     ///@name Input and output
@@ -235,7 +170,7 @@ protected:
     /**
      * @brief This method sets the Octree bounding box
      */
-    void SetOctreeBoundingBox();
+    void SetOctreeBoundingBox() override;
 
     /**
      * @brief This method marks if intersected
@@ -245,96 +180,7 @@ protected:
     void MarkIfIntersected(
         Condition& rCondition1,
         OtreeCellVectorType& rLeaves
-        );
-
-     /**
-     * @brief This method check if there is an intersection between two geometries
-     * @param rFirstGeometry The first geometry
-     * @param rSecondGeometry The second geometry
-     */
-    bool HasIntersection(
-        GeometryType& rFirstGeometry,
-        GeometryType& rSecondGeometry
-        );
-
-    /**
-     * @brief This method check if there is an intersection between two geometries in 2D
-     * @param rFirstGeometry The first geometry
-     * @param rSecondGeometry The second geometry
-     */
-    bool HasIntersection2D(
-        GeometryType& rFirstGeometry,
-        GeometryType& rSecondGeometry
-        );
-
-    /**
-     * @brief This method check if there is an intersection between two geometries in 3D
-     * @param rFirstGeometry The first geometry
-     * @param rSecondGeometry The second geometry
-     */
-    bool HasIntersection3D(
-        GeometryType& rFirstGeometry,
-        GeometryType& rSecondGeometry
-        );
-
-    /**
-     * @brief This method check if there is an intersection between two geometries in 2D (with OBB)
-     * @param rFirstGeometry The first geometry
-     * @param rSecondGeometry The second geometry
-     */
-    bool HasIntersection2DWithOBB(
-        GeometryType& rFirstGeometry,
-        GeometryType& rSecondGeometry
-        );
-
-    /**
-     * @brief This method check if there is an intersection between two geometries in 3D (with OBB)
-     * @param rFirstGeometry The first geometry
-     * @param rSecondGeometry The second geometry
-     */
-    bool HasIntersection3DWithOBB(
-        GeometryType& rFirstGeometry,
-        GeometryType& rSecondGeometry
-        );
-
-    /**
-     * @brief This creates auxiliar elements with the provided OBB (2D)
-     * @param rModelPart The model part where to add the elements
-     * @param pProperties Pointer to the considered properties
-     * @param rOrientedBoundingBox The bounding box to be postprocessed
-     */
-    void CreateDebugOBB2D(
-        ModelPart& rModelPart,
-        Properties::Pointer pProperties,
-        OrientedBoundingBox<2>& rOrientedBoundingBox
-        );
-
-    /**
-     * @brief This creates auxiliar elements with the provided OBB (3D)
-     * @param rModelPart The model part where to add the elements
-     * @param pProperties Pointer to the considered properties
-     * @param rOrientedBoundingBox The bounding box to be postprocessed
-     */
-    void CreateDebugOBB3D(
-        ModelPart& rModelPart,
-        Properties::Pointer pProperties,
-        OrientedBoundingBox<3>& rOrientedBoundingBox
-        );
-
-    /**
-     * @brief This converts the interpolation string to an enum
-     * @param Str The string that you want to comvert in the equivalent enum
-     * @return OBBHasIntersectionType: The equivalent enum (this requires less memmory than a std::string)
-     */
-    OBBHasIntersectionType ConvertInter(const std::string& Str)
-    {
-        if(Str == "Direct" || Str == "direct")
-            return OBBHasIntersectionType::Direct;
-        else if(Str == "SeparatingAxisTheorem" || Str == "separating_axis_theorem")
-            return OBBHasIntersectionType::SeparatingAxisTheorem;
-        else
-            return OBBHasIntersectionType::SeparatingAxisTheorem;
-    }
+        ) override;
 
     ///@}
     ///@name Protected  Access
@@ -358,55 +204,12 @@ private:
     ///@name Member Variables
     ///@{
 
-    ModelPart& mrModelPartIntersected;  /// Model part intersected
-    ModelPart& mrModelPartIntersecting; /// Model part intersecting
-    OctreeType mOctree;                 /// The octree structucture that performs the search
-
-    double mBoundingBoxFactor = -1.0;         /// The factor to be consider when computing the bounding box (if negative not considered)
-    bool mDebugOBB = false;                   /// If we debug the boxes
-    OBBHasIntersectionType mIntersectionType; /// Intersection type
-    Parameters mThisParameters;               /// The configuration parameters
-
     double mLowerBBCoefficient  = 0.0;
     double mHigherBBCoefficient = 1.0;
 
     ///@}
     ///@name Private Operations
     ///@{
-
-    /**
-     * @brief Returns the current working space dimension
-     * @return The current working space dimension
-     */
-    std::size_t WorkingSpaceDimension();
-
-    /**
-     * @brief This method generates a new Octree class
-     */
-    void GenerateOctree();
-
-    /**
-     * @brief This method finds intected skin objects
-     * @param rCondition The entity of interest
-     * @param rLeaves The Octree cells vectors
-     * @param rResults The expected results
-     */
-    void FindIntersectedSkinObjects(
-        Condition& rCondition,
-        OtreeCellVectorType& rLeaves,
-        PointerVector<GeometricalObject>& rResults
-        )
-    {
-        for (auto p_leaf : rLeaves) {
-            for (auto p_intersecting_entity : *(p_leaf->pGetObjects())) {
-                if (HasIntersection(rCondition.GetGeometry(), p_intersecting_entity->GetGeometry())) {
-                    rCondition.Set(SELECTED);
-                    if(std::find(rResults.ptr_begin(), rResults.ptr_end(), p_intersecting_entity) == rResults.ptr_end())
-                        rResults.push_back(p_intersecting_entity);
-                }
-            }
-        }
-    }
 
     /**
      * @brief This method provides the defaults parameters to avoid conflicts between the different constructors
