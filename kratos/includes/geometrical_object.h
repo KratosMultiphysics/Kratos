080600775dce0322094fa9cfd6c4662cc45f63fb
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-04-30 12:01:33 +0200

    @jcotela commentaries + doxygen

--- kratos/includes/geometrical_object.h
+++ kratos/includes/geometrical_object.h
@@ -25,7 +25,6 @@
 #include "containers/flags.h"
 #include "geometries/geometry.h"
 
-
 namespace Kratos
 {
 
@@ -48,10 +47,15 @@ namespace Kratos
 ///@name Kratos Classes
 ///@{
 
-/// Short class definition.
-/** Detail class definition.
+/**
+ * @class GeometricalObject
+ * @ingroup KratosCore
+ * @brief This defines the geometrical object, base definition of the element and condition entities
+ * @details Derives from IndexedObject, so it has an ID, and from Flags
+ * @author Pooyan Dadvand
 */
-class GeometricalObject : public IndexedObject, public Flags
+class GeometricalObject
+    : public IndexedObject, public Flags
 {
 public:
     ///@name Type Definitions
@@ -60,14 +64,16 @@ public:
     /// Pointer definition of GeometricalObject
     KRATOS_CLASS_POINTER_DEFINITION(GeometricalObject);
 
-    typedef IndexedObject BaseType;
+    /// Definition of the node type
+    typedef Node <3> NodeType;
 
-    typedef Node < 3 > NodeType;
-
+    /// The geometry type definition
     typedef Geometry<NodeType> GeometryType;
 
+    /// Defines the index type
     typedef std::size_t IndexType;
 
+    /// Defines the result type
     typedef std::size_t result_type;
 
     ///@}
@@ -76,14 +82,14 @@ public:
 
     /// Default constructor.
     explicit GeometricalObject(IndexType NewId = 0)
-        : BaseType(NewId),
+        : IndexedObject(NewId),
           Flags(),
           mpGeometry()
     {}
 
     /// Default constructor.
     GeometricalObject(IndexType NewId, GeometryType::Pointer pGeometry)
-        : BaseType(NewId),
+        : IndexedObject(NewId),
           Flags(),
           mpGeometry(pGeometry)
     {}
@@ -93,7 +99,7 @@ public:
 
     /// Copy constructor.
     GeometricalObject(GeometricalObject const& rOther)
-        : BaseType(rOther.Id()),
+        : IndexedObject(rOther.Id()),
           Flags(rOther),
           mpGeometry(rOther.mpGeometry)
     {}
@@ -106,7 +112,7 @@ public:
     /// Assignment operator.
     GeometricalObject& operator=(GeometricalObject const& rOther)
     {
-        BaseType::operator=(rOther);
+        IndexedObject::operator=(rOther);
         Flags::operator =(rOther);
         return *this;
     }
@@ -115,45 +121,68 @@ public:
     ///@name Operations
     ///@{
 
-
     ///@}
     ///@name Access
     ///@{
 
+    /**
+     * @brief Returns the pointer to the geometry
+     * @return The pointer of the geometry
+     */
     GeometryType::Pointer pGetGeometry()
     {
         return mpGeometry;
     }
 
+    /**
+     * @brief Returns the pointer to the geometry (const version)
+     * @return The pointer of the geometry
+     */
     const GeometryType::Pointer pGetGeometry() const
     {
         return mpGeometry;
     }
 
+    /**
+     * @brief Returns the reference of the geometry
+     * @return The reference of the geometry
+     */
     GeometryType& GetGeometry()
     {
         return *mpGeometry;
     }
 
+    /**
+     * @brief Returns the reference of the geometry (const version)
+     * @return The reference of the geometry
+     */
     GeometryType const& GetGeometry() const
     {
         return *mpGeometry;
     }
 
-    ///@}
-    ///@name Flags
-    ///@{
-
+    /**
+     * @brief Returns the flags of the object
+     * @return The  flags of the object
+     */
     Flags& GetFlags()
     {
         return *this;
     }
 
+    /**
+     * @brief Returns the flags of the object (const version)
+     * @return The  flags of the object
+     */
     Flags const& GetFlags() const
     {
         return *this;
     }
 
+    /**
+     * @brief Sets the flags of the object
+     * @param rThisFlags The flags to be set
+     */
     void SetFlags(Flags const& rThisFlags)
     {
         Flags::operator=(rThisFlags);
@@ -163,26 +192,50 @@ public:
     ///@name Inquiry
     ///@{
 
+    /**
+     * @brief Checks if two GeometricalObject have the same type
+     * @return True if the objects are the same type, false otherwise
+     */
     inline static bool HasSameType(const GeometricalObject& rLHS, const GeometricalObject& rRHS) {
         return (typeid(rLHS) == typeid(rRHS));
     }
 
+    /**
+     * @brief Checks if two GeometricalObject have the same type (pointer version)
+     * @return True if the objects are the same type, false otherwise
+     */
     inline static bool HasSameType(const GeometricalObject * rLHS, const GeometricalObject* rRHS) {
         return GeometricalObject::HasSameType(*rLHS, *rRHS);
     }
 
+    /**
+     * @brief Checks if two GeometricalObject have the same geometry type
+     * @return True if the geometries are the same type, false otherwise
+     */
     inline static bool HasSameGeometryType(const GeometricalObject& rLHS, const GeometricalObject& rRHS) {
         return (rLHS.GetGeometry().GetGeometryType() == rRHS.GetGeometry().GetGeometryType());
     }
 
+    /**
+     * @brief Checks if two GeometricalObject have the same geometry type (pointer version)
+     * @return True if the geometries are the same type, false otherwise
+     */
     inline static bool HasSameGeometryType(const GeometricalObject* rLHS, const GeometricalObject* rRHS) {
         return GeometricalObject::HasSameGeometryType(*rLHS, *rRHS);
     }
 
+    /**
+     * @brief Checks if two GeometricalObject are the same
+     * @return True if the object is the same, false otherwise
+     */
     inline static bool IsSame(const GeometricalObject& rLHS, const GeometricalObject& rRHS) {
         return GeometricalObject::HasSameType(rLHS, rRHS) && GeometricalObject::HasSameGeometryType(rLHS, rRHS);
     }
 
+    /**
+     * @brief Checks if two GeometricalObject are the same (pointer version)
+     * @return True if the object is the same, false otherwise
+     */
     inline static bool IsSame(const GeometricalObject* rLHS, const GeometricalObject* rRHS) {
         return GeometricalObject::HasSameType(*rLHS, *rRHS) && GeometricalObject::HasSameGeometryType(*rLHS, *rRHS);
     }
@@ -195,7 +248,7 @@ public:
     std::string Info() const override
     {
         std::stringstream buffer;
-        buffer << "geometrical object # "
+        buffer << "Geometrical object # "
                << Id();
         return buffer.str();
     }
@@ -210,7 +263,6 @@ public:
     void PrintData(std::ostream& rOStream) const override
     {
     }
-
 
     ///@}
     ///@name Friends
@@ -265,10 +317,7 @@ private:
     ///@name Member Variables
     ///@{
 
-    /**
-     * pointer to the condition geometry
-     */
-    GeometryType::Pointer mpGeometry;
+    GeometryType::Pointer mpGeometry; /// Pointer to the entity geometry
 
     ///@}
     ///@name Private Operators
