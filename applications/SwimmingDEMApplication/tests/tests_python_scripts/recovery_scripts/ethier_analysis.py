bae6f8e357863c9ee0c85feb3b5bcab6c36f0f8f
gcasas <gcasas@cimne.upc.edu>
2019-05-09 11:39:01 +0200

    Intermediate stage of this file
    I am transitioning to spliting the corresponging test in 2

--- applications/SwimmingDEMApplication/tests/tests_python_scripts/recovery_scripts/ethier_analysis.py
+++ applications/SwimmingDEMApplication/tests/tests_python_scripts/recovery_scripts/ethier_analysis.py
@@ -28,14 +28,18 @@ class EthierBenchmarkAnalysis(SwimmingDEMAnalysis):
     def PerformZeroStepInitializations(self):
         self.mat_deriv_errors = []
         self.laplacian_errors = []
+        self.pressure_gradient_errors = []
         self.current_mat_deriv_errors = np.zeros(2)
         self.current_laplacian_errors = np.zeros(2)
+        self.current_pressure_gradient_errors = np.zeros(2)
 
         for node in self.fluid_model_part.Nodes:
             vel= Vector(3)
             coor = Vector([node.X, node.Y, node.Z])
             self.flow_field.Evaluate(0.0, coor, vel, 0)
+            pressure = self.pressure_field.Evaluate(0.0, coor, 0)
             node.SetSolutionStepValue(Kratos.VELOCITY, vel)
+            node.SetSolutionStepValue(Kratos.PRESSURE, pressure)
 
     def FinalizeSolutionStep(self):
         super(EthierBenchmarkAnalysis, self).FinalizeSolutionStep()
@@ -44,8 +48,12 @@ class EthierBenchmarkAnalysis(SwimmingDEMAnalysis):
     def GetFieldUtility(self):
         a = math.pi / 4
         d = math.pi / 2
-
-        self.pressure_field = SDEM.RealField()
+        c = 1.0
+        bx, by, bz = 1.0, 2.0, 5.0
+        self.fx = SDEM.LinearFunction(c, bx)
+        self.fy = SDEM.LinearFunction(0.0, by)
+        self.fz = SDEM.LinearFunction(0.0, bz)
+        self.pressure_field = SDEM.LinearRealField(1.0, 1.0, 1.0, self.fx, self.fy, self.fz)
         self.flow_field = SDEM.EthierVelocityField(a, d)
         space_time_set = SDEM.SpaceTimeSet()
         self.field_utility = SDEM.FluidFieldUtility(space_time_set,
@@ -63,14 +71,20 @@ class EthierBenchmarkAnalysis(SwimmingDEMAnalysis):
         L2_norm_mat_deriv_error = 0.
         L2_norm_laplacian = 0.
         L2_norm_laplacian_error = 0.
+        L2_norm_pressure_gradient = 0.
+        L2_norm_pressure_gradient_error = 0.
         max_mat_deriv_error = 0.
         max_laplacian_error = 0.
+        max_pressure_gradient_error = 0.
         total_volume = 0.
 
         calc_mat_deriv = np.zeros(3)
         calc_laplacian = np.zeros(3)
-        mat_deriv= Vector(3)
-        laplacian= Vector(3)
+        calc_pressure_gradient = np.zeros(3)
+
+        mat_deriv = Vector(3)
+        laplacian = Vector(3)
+        pressure_gradient = Vector(3)
 
         for node in self.fluid_model_part.Nodes:
             nodal_volume = node.GetSolutionStepValue(Kratos.NODAL_AREA)
@@ -79,44 +93,68 @@ class EthierBenchmarkAnalysis(SwimmingDEMAnalysis):
 
             self.flow_field.CalculateMaterialAcceleration(0., coor, mat_deriv, 0)
             self.flow_field.CalculateLaplacian(0., coor, laplacian, 0)
+            self.pressure_field.CalculateGradient(self.time, coor, pressure_gradient, 0)
+            #Say(pressure_gradient)
             calc_mat_deriv = node.GetSolutionStepValue(Kratos.MATERIAL_ACCELERATION)
             calc_laplacian = node.GetSolutionStepValue(Kratos.VELOCITY_LAPLACIAN)
+            calc_pressure_gradient = node.GetSolutionStepValue(Kratos.PRESSURE_GRADIENT)
 
             module_mat_deriv_squared = sum(x**2 for x in mat_deriv)
             module_laplacian_squared = sum(x**2 for x in laplacian)
+            module_pressure_gradient_squared = sum(x**2 for x in pressure_gradient)
+
             L2_norm_mat_deriv += module_mat_deriv_squared * nodal_volume
             L2_norm_laplacian += module_laplacian_squared * nodal_volume
+            L2_norm_pressure_gradient += module_pressure_gradient_squared * nodal_volume
+
             diff_mat_deriv = calc_mat_deriv - mat_deriv
             diff_laplacian = calc_laplacian - laplacian
+            diff_pressure_gradient = calc_pressure_gradient - pressure_gradient
+            Say(calc_pressure_gradient[0], pressure_gradient[0])
             node.SetSolutionStepValue(SDEM.VECTORIAL_ERROR, Vector(list(diff_mat_deriv)))
             node.SetSolutionStepValue(SDEM.VECTORIAL_ERROR_1, Vector(list(diff_laplacian)))
             module_mat_deriv_error_squared = sum(x**2 for x in diff_mat_deriv)
             module_laplacian_error_squared = sum(x**2 for x in diff_laplacian)
+            module_pressure_gradient_error_squared = sum(x**2 for x in diff_pressure_gradient)
             L2_norm_mat_deriv_error += module_mat_deriv_error_squared * nodal_volume
             L2_norm_laplacian_error += module_laplacian_error_squared * nodal_volume
+            L2_norm_pressure_gradient_error += module_pressure_gradient_error_squared * nodal_volume
             max_mat_deriv_error = max(max_mat_deriv_error, module_mat_deriv_error_squared)
             max_laplacian_error = max(max_laplacian_error, module_laplacian_error_squared)
+            max_pressure_gradient_error = max(max_pressure_gradient_error, module_pressure_gradient_error_squared)
+
 
         L2_norm_mat_deriv **= 0.5
         L2_norm_mat_deriv /= total_volume ** 0.5
         L2_norm_mat_deriv_error **= 0.5
         L2_norm_mat_deriv_error /= total_volume ** 0.5
+
         L2_norm_laplacian **= 0.5
         L2_norm_laplacian /= total_volume ** 0.5
         L2_norm_laplacian_error **= 0.5
         L2_norm_laplacian_error /= total_volume ** 0.5
+
+        L2_norm_pressure_gradient **= 0.5
+        L2_norm_pressure_gradient /= total_volume ** 0.5
+        L2_norm_pressure_gradient_error **= 0.5
+        L2_norm_pressure_gradient_error /= total_volume ** 0.5
+
         max_mat_deriv_error **= 0.5
         max_laplacian_error **= 0.5
+        max_pressure_gradient_error **= 0.5
 
-        if L2_norm_mat_deriv > 0 and L2_norm_laplacian > 0:
+        if L2_norm_mat_deriv > 0 and L2_norm_laplacian > 0 and L2_norm_pressure_gradient > 0:
             SDP.MultiplyNodalVariableByFactor(self.fluid_model_part, SDEM.VECTORIAL_ERROR, 1.0 / L2_norm_mat_deriv)
             SDP.MultiplyNodalVariableByFactor(self.fluid_model_part, SDEM.VECTORIAL_ERROR_1, 1.0 / L2_norm_laplacian)
             self.current_mat_deriv_errors[0] = L2_norm_mat_deriv_error / L2_norm_mat_deriv
             self.current_mat_deriv_errors[1] = max_mat_deriv_error / L2_norm_mat_deriv
             self.current_laplacian_errors[0] = L2_norm_laplacian_error / L2_norm_laplacian
             self.current_laplacian_errors[1] = max_laplacian_error / L2_norm_laplacian
+            self.current_pressure_gradient_errors[0] = L2_norm_pressure_gradient_error / L2_norm_pressure_gradient
+            self.current_pressure_gradient_errors[1] = max_pressure_gradient_error / L2_norm_pressure_gradient
             self.mat_deriv_errors.append(self.current_mat_deriv_errors)
             self.laplacian_errors.append(self.current_laplacian_errors)
+            self.pressure_gradient_errors.append(self.current_pressure_gradient_errors)
 
             text_width = 40
             Say('\n' + '-.' * text_width)
@@ -124,4 +162,6 @@ class EthierBenchmarkAnalysis(SwimmingDEMAnalysis):
             Say('max error for the material derivative'.ljust(text_width), self.current_mat_deriv_errors[1])
             Say('L2 error for the laplacian'.ljust(text_width), self.current_laplacian_errors[0])
             Say('max error for the laplacian'.ljust(text_width), self.current_laplacian_errors[1])
+            Say('L2 error for the pressure_gradient'.ljust(text_width), self.current_pressure_gradient_errors[0])
+            Say('max error for the pressure_gradient'.ljust(text_width), self.current_pressure_gradient_errors[1])
             Say('-.' * text_width + '\n')
