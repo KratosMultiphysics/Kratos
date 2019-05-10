31d08f3ebab87c99b0971c7e0bf6ff59151b4ef7
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-05-10 18:43:05 +0200

    Reducing code duplication

--- kratos/processes/apply_component_table_process.cpp
+++ kratos/processes/apply_component_table_process.cpp
@@ -4,15 +4,20 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:         BSD License
+//                     Kratos default license: kratos/license.txt
 //
 //  Main authors:    Ignasi de Pouplana
 //                   Alejandro Cornejo
 //
 
-#include "processes/apply_component_table_process.h"
+// System includes
 
+// External includes
+
+// Project includes
+#include "utilities/variable_utils.h"
+#include "processes/apply_component_table_process.h"
 
 namespace Kratos
 {
@@ -21,24 +26,24 @@ namespace Kratos
 ///@{
 template<class TVariableType>
 ApplyComponentTableProcess<TVariableType>::ApplyComponentTableProcess(
-    ModelPart& rModelPart, 
-    Parameters rParameters) 
-    : Process(Flags()), 
+    ModelPart& rModelPart,
+    Parameters rParameters)
+    : Process(Flags()),
       mrModelPart(rModelPart)
 {
     KRATOS_TRY
 
-    //only include validation with c++11 since raw_literals do not exist in c++03
+    // Only include validation with c++11 since raw_literals do not exist in c++03
     Parameters default_parameters(R"(
-        {
-            "model_part_name":"PLEASE_CHOOSE_MODEL_PART_NAME",
-            "variable_name": "PLEASE_PRESCRIBE_VARIABLE_NAME",
-            "time_variable_name" : "TIME",
-            "is_fixed": false,
-            "value" : 1.0,
-            "table" : 1
-        })");
-    
+    {
+        "model_part_name":"PLEASE_CHOOSE_MODEL_PART_NAME",
+        "variable_name": "PLEASE_PRESCRIBE_VARIABLE_NAME",
+        "time_variable_name" : "TIME",
+        "is_fixed": false,
+        "value" : 1.0,
+        "table" : 1
+    })");
+
     // Some values need to be mandatorily prescribed since no meaningful default value exist. For this reason try accessing to them
     // So that an error is thrown if they don't exist
     rParameters["table"];
@@ -53,38 +58,35 @@ ApplyComponentTableProcess<TVariableType>::ApplyComponentTableProcess(
     mTimeVariableName = rParameters["time_variable_name"].GetString();
     mIsFixed = rParameters["is_fixed"].GetBool();
     mInitialValue = rParameters["value"].GetDouble();
-    
-    unsigned int TableId = rParameters["table"].GetInt();
-    mpTable = rModelPart.pGetTable(TableId);
 
-	KRATOS_CATCH("")
+    const unsigned int table_id = rParameters["table"].GetInt();
+    mpTable = rModelPart.pGetTable(table_id);
+
+    KRATOS_CATCH("")
 }
 
 /***********************************************************************************/
 /***********************************************************************************/
+
+template<class TVariableType>
+void ApplyComponentTableProcess<TVariableType>::Execute()
+{
+    ExecuteInitialize();
+    ExecuteInitializeSolutionStep();
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
 template<>
 void ApplyComponentTableProcess<ComponentVariableType>::ExecuteInitialize()
 {
     KRATOS_TRY
 
-	ComponentVariableType variable_component = KratosComponents<ComponentVariableType>::Get(mVariableName);
-    
-    const int nnodes = static_cast<int>(mrModelPart.Nodes().size());
+    const auto& r_variable_component = KratosComponents<ComponentVariableType>::Get(mVariableName);
 
-    if (nnodes != 0) {
-        ModelPart::NodesContainerType::iterator it_begin = mrModelPart.NodesBegin();
+    ImplementationExecuteInitialize(r_variable_component);
 
-        #pragma omp parallel for
-        for (int i = 0; i<nnodes; i++) {
-            ModelPart::NodesContainerType::iterator it = it_begin + i;
-
-            if (mIsFixed) {
-                it->Fix(variable_component);
-            }
-
-            it->FastGetSolutionStepValue(variable_component) = mInitialValue;
-        }
-    }
     KRATOS_CATCH("")
 }
 
@@ -94,22 +96,11 @@ template<>
 void ApplyComponentTableProcess<DoubleVariableType>::ExecuteInitialize()
 {
     KRATOS_TRY
-    
-	DoubleVariableType variable = KratosComponents<DoubleVariableType>::Get(mVariableName);
-    const int nnodes = static_cast<int>(mrModelPart.Nodes().size());
 
-    if (nnodes != 0) {
-        ModelPart::NodesContainerType::iterator it_begin = mrModelPart.NodesBegin();
+    const auto& r_variable_double = KratosComponents<DoubleVariableType>::Get(mVariableName);
 
-        #pragma omp parallel for
-        for (int i = 0; i < nnodes; i++) {
-            ModelPart::NodesContainerType::iterator it = it_begin + i;
-            if (mIsFixed) {
-                it->Fix(variable);
-            }
-            it->FastGetSolutionStepValue(variable) = mInitialValue;
-        }
-    }
+    ImplementationExecuteInitialize(r_variable_double);
+
     KRATOS_CATCH("");
 }
 
@@ -119,19 +110,10 @@ template<>
 void ApplyComponentTableProcess<ComponentVariableType>::ExecuteInitializeSolutionStep()
 {
     KRATOS_TRY
-    
-    const ComponentVariableType& variable_component = KratosComponents<ComponentVariableType>::Get(mVariableName);
-	DoubleVariableType time_var_component = KratosComponents<DoubleVariableType>::Get(mTimeVariableName);
-    
-    const double time = mrModelPart.GetProcessInfo()[time_var_component];
-    const double value = mpTable->GetValue(time);
-    
-    const int nnodes = static_cast<int>(mrModelPart.Nodes().size());
 
-    if (nnodes != 0) {
-        auto& r_nodes_array = mrModelPart.Nodes();
-        VariableUtils().SetScalarVar<ComponentVariableType>(variable_component, value, r_nodes_array);
-    }
+    const auto& r_variable_component = KratosComponents<ComponentVariableType>::Get(mVariableName);
+
+    ImplementationExecuteInitializeSolutionStep(r_variable_component);
     KRATOS_CATCH("")
 }
 
@@ -142,20 +124,61 @@ void ApplyComponentTableProcess<DoubleVariableType>::ExecuteInitializeSolutionSt
 {
     KRATOS_TRY
 
-	DoubleVariableType variable   = KratosComponents<DoubleVariableType>::Get(mVariableName);
-	DoubleVariableType time_var_component = KratosComponents<DoubleVariableType>::Get(mTimeVariableName);
-    
-    const double time = mrModelPart.GetProcessInfo()[time_var_component];
-    const double value = mpTable->GetValue(time);
-    
-    const int nnodes = static_cast<int>(mrModelPart.Nodes().size());
+    const auto& r_variable_double = KratosComponents<DoubleVariableType>::Get(mVariableName);
 
-    if (nnodes != 0) {
-        auto& r_nodes_array = mrModelPart.Nodes();
-        VariableUtils().SetScalarVar<DoubleVariableType>(variable, value, r_nodes_array);
+    ImplementationExecuteInitializeSolutionStep(r_variable_double);
+
+    KRATOS_CATCH("")
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template<class TVariableType>
+void ApplyComponentTableProcess<TVariableType>::ImplementationExecuteInitialize(const TVariableType& rVariable)
+{
+    KRATOS_TRY
+
+    const int number_of_nodes = static_cast<int>(mrModelPart.Nodes().size());
+
+    if (number_of_nodes != 0) {
+        const auto it_node_begin = mrModelPart.NodesBegin();
+
+        #pragma omp parallel for
+        for (int i = 0; i<number_of_nodes; i++) {
+            auto it_node = it_node_begin + i;
+
+            if (mIsFixed) {
+                it_node->Fix(rVariable);
+            }
+
+            it_node->FastGetSolutionStepValue(rVariable) = mInitialValue;
+        }
     }
     KRATOS_CATCH("")
-}  
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template<class TVariableType>
+void ApplyComponentTableProcess<TVariableType>::ImplementationExecuteInitializeSolutionStep(const TVariableType& rVariable)
+{
+    KRATOS_TRY
+
+    const auto& r_time_variable = KratosComponents<DoubleVariableType>::Get(mTimeVariableName);
+    const double time = mrModelPart.GetProcessInfo()[r_time_variable];
+    const double value = mpTable->GetValue(time);
+
+    const int number_of_nodes = static_cast<int>(mrModelPart.Nodes().size());
+
+    if (number_of_nodes != 0) {
+        auto& r_nodes_array = mrModelPart.Nodes();
+        VariableUtils().SetScalarVar<TVariableType>(rVariable, value, r_nodes_array);
+    }
+
+    KRATOS_CATCH("")
+}
 
 /***********************************************************************************/
 /***********************************************************************************/
--- kratos/processes/apply_component_table_process.h
+++ kratos/processes/apply_component_table_process.h
@@ -20,27 +20,46 @@
 
 // Project includes
 #include "includes/table.h"
-#include "includes/kratos_flags.h"
 #include "includes/kratos_parameters.h"
 #include "processes/process.h"
 #include "includes/model_part.h"
-#include "utilities/variable_utils.h"
 
 namespace Kratos
 {
 
-///@name Kratos Classes
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
 ///@{
 
 // Variables types
 typedef VariableComponent<VectorComponentAdaptor<array_1d<double,3>>>  ComponentVariableType;
 typedef Variable<double>                                                  DoubleVariableType;
 
-/// This class applies a value from a table to a BC o load
-/** This function applies a table value to a component
-*/
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @ingroup KratosCore
+ * @class ApplyComponentTableProcess
+ * @brief This class applies a value from a table to a BC o load
+ * @details This function applies a table value to a component
+ * @author Ignasi de Pouplana
+ * @author Alejandro Cornejo
+ */
 template<class TVariableType>
-class KRATOS_API(KRATOS_CORE) ApplyComponentTableProcess 
+class KRATOS_API(KRATOS_CORE) ApplyComponentTableProcess
     : public Process
 {
 
@@ -50,27 +69,65 @@ public:
 
     /// Pointer definition of ApplyConstantScalarValueProcess
     KRATOS_CLASS_POINTER_DEFINITION(ApplyComponentTableProcess);
-    
+
     /// Defining a table with double argument and result type as table type.
     typedef Table<double,double> TableType;
-    
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
     /**
      * @brief Default constructor.
      * @param rModelPart The model part where the search of neighbours is performed
      * @param ThisParameters The parameters of configuration
      */
-	ApplyComponentTableProcess(ModelPart& rModelPart, Parameters rParameters);
-   
+    ApplyComponentTableProcess(ModelPart& rModelPart, Parameters rParameters);
+
     /// Destructor
     virtual ~ApplyComponentTableProcess() override {}
-    
-    /// This function is designed for being called at the beginning of the computations
-    /// right after reading the model and the groups
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    void operator()()
+    {
+        Execute();
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief This method executes the algorithm that looks for neighbour nodes and elements in a  mesh of prismatic elements
+     */
+    void Execute() override;
+
+    /**
+     * @brief This function is designed for being called at the beginning of the computations right after reading the model and the groups
+     */
     void ExecuteInitialize() override;
 
-
-    /// this function will be executed at every time step BEFORE performing the solve phase
+    /**
+     * @brief This function will be executed at every time step BEFORE performing the solve phase
+     */
     void ExecuteInitializeSolutionStep() override;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
 
     /// Turn back information as a string.
     std::string Info() const override
@@ -88,26 +145,61 @@ public:
     void PrintData(std::ostream& rOStream) const override
     {
     }
-
-
 protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    ModelPart& mrModelPart;             /// The main model part
+    std::string mVariableName;          /// The variable to be fixed with the value of the table
+    std::string mTimeVariableName;      /// The variable that represents "time"
+    bool mIsFixed;                      /// Boolean var to know wether the DoF is fixed
+    double mInitialValue;               /// Initial value of the variable
+    TableType::Pointer mpTable;         /// Table containing all the information
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    /**
+     * @brief Auxiliar implementation of ExecuteInitialize to reduce code duplication
+     */
+    void ImplementationExecuteInitialize(const TVariableType& rVariable);
+
+    /**
+     * @brief Auxiliar implementation of ExecuteInitializeSolutionStep to reduce code duplication
+     */
+    void ImplementationExecuteInitializeSolutionStep(const TVariableType& rVariable);
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
 
     /// Copy constructor.
     ApplyComponentTableProcess(ApplyComponentTableProcess const& rOther);
 
-    /// Member Variables
-    ModelPart& mrModelPart;  // The main model part
-    std::string mVariableName;  // The variable to be fixed with the value of the table
-    std::string mTimeVariableName;  // The variable that represents "time"
-    bool mIsFixed;  // Boolean var to know wether the DoF is fixed
-    double mInitialValue;  // Initial value of the variable
-    TableType::Pointer mpTable;  // Table containing all the information
-    
+    ///@}
+
 private:
 
     /// Assignment operator.
     ApplyComponentTableProcess& operator=(ApplyComponentTableProcess const& rOther);
-    
+
 }; // Class ApplyComponentTableProcess
 
 /// input stream function
