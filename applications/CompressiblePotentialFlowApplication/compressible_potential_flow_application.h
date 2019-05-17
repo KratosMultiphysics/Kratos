f3542b6067ae24b67adcb840074cb7a60d340eb1
marcnunezc <marcnc94@gmail.com>
2019-05-17 11:35:08 +0200

    fixing format

--- applications/CompressiblePotentialFlowApplication/compressible_potential_flow_application.h
+++ applications/CompressiblePotentialFlowApplication/compressible_potential_flow_application.h
@@ -102,7 +102,7 @@ private:
     const IncompressiblePotentialFlowElement<2,3> mIncompressiblePotentialFlowElement2D3N;
     const CompressiblePotentialFlowElement<2,3> mCompressiblePotentialFlowElement2D3N;
     const AdjointPotentialFlowElement<IncompressiblePotentialFlowElement<2,3>> mAdjointIncompressiblePotentialFlowElement2D3N;
-	const AdjointPotentialFlowElement<CompressiblePotentialFlowElement<2,3>> mAdjointCompressiblePotentialFlowElement2D3N;
+    const AdjointPotentialFlowElement<CompressiblePotentialFlowElement<2,3>> mAdjointCompressiblePotentialFlowElement2D3N;
     const EmbeddedIncompressiblePotentialFlowElement<2,3> mEmbeddedIncompressiblePotentialFlowElement2D3N;
 
     const PotentialWallCondition<2,2> mPotentialWallCondition2D2N;
