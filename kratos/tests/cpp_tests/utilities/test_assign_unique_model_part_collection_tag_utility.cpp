b71fb97ba854780522a2b38f33f83ae7850df872
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-05-19 13:13:49 +0200

    Reducing code on test

--- kratos/tests/cpp_tests/utilities/test_assign_unique_model_part_collection_tag_utility.cpp
+++ kratos/tests/cpp_tests/utilities/test_assign_unique_model_part_collection_tag_utility.cpp
@@ -15,7 +15,6 @@
 
 // Project includes
 #include "containers/model.h"
-#include "geometries/triangle_2d_3.h"
 #include "testing/testing.h"
 #include "includes/kratos_flags.h"
 
@@ -63,35 +62,10 @@ namespace Kratos
             first_sub_modelpart_3.AddNode(p_node_5);
             first_sub_modelpart_4.AddNode(p_node_6);
 
-            // Now we create the "conditions"
-            std::vector<NodeType::Pointer> element_nodes_0 (3);
-            element_nodes_0[0] = p_node_1;
-            element_nodes_0[1] = p_node_2;
-            element_nodes_0[2] = p_node_3;
-            Triangle2D3 <NodeType> triangle_0( PointerVector<NodeType>{element_nodes_0} );
-
-            std::vector<NodeType::Pointer> element_nodes_1 (3);
-            element_nodes_1[0] = p_node_1;
-            element_nodes_1[1] = p_node_3;
-            element_nodes_1[2] = p_node_4;
-            Triangle2D3 <NodeType> triangle_1( PointerVector<NodeType>{element_nodes_1} );
-
-            std::vector<NodeType::Pointer> element_nodes_2 (3);
-            element_nodes_2[0] = p_node_2;
-            element_nodes_2[1] = p_node_5;
-            element_nodes_2[2] = p_node_3;
-            Triangle2D3 <NodeType> triangle_2( PointerVector<NodeType>{element_nodes_2} );
-
-            std::vector<NodeType::Pointer> element_nodes_3 (3);
-            element_nodes_3[0] = p_node_5;
-            element_nodes_3[1] = p_node_6;
-            element_nodes_3[2] = p_node_3;
-            Triangle2D3 <NodeType> triangle_3( PointerVector<NodeType>{element_nodes_3} );
-
-            Element::Pointer p_elem_0 = first_model_part.CreateNewElement("Element2D3N", 1, triangle_0, p_elem_prop);
-            Element::Pointer p_elem_1 = first_model_part.CreateNewElement("Element2D3N", 2, triangle_1, p_elem_prop);
-            Element::Pointer p_elem_2 = first_model_part.CreateNewElement("Element2D3N", 3, triangle_2, p_elem_prop);
-            Element::Pointer p_elem_3 = first_model_part.CreateNewElement("Element2D3N", 4, triangle_3, p_elem_prop);
+            Element::Pointer p_elem_0 = first_model_part.CreateNewElement("Element2D3N", 1, {{1,2,3}}, p_elem_prop);
+            Element::Pointer p_elem_1 = first_model_part.CreateNewElement("Element2D3N", 2, {{1,3,4}}, p_elem_prop);
+            Element::Pointer p_elem_2 = first_model_part.CreateNewElement("Element2D3N", 3, {{2,5,3}}, p_elem_prop);
+            Element::Pointer p_elem_3 = first_model_part.CreateNewElement("Element2D3N", 4, {{5,6,3}}, p_elem_prop);
 
             // Adding nodes to random submodelparts
             first_sub_modelpart_1.AddElement(p_elem_0);
@@ -200,35 +174,10 @@ namespace Kratos
             first_sub_modelpart_3.AddNode(p_node_5);
             first_sub_modelpart_4.AddNode(p_node_6);
 
-            // Now we create the "conditions"
-            std::vector<NodeType::Pointer> element_nodes_0 (3);
-            element_nodes_0[0] = p_node_1;
-            element_nodes_0[1] = p_node_2;
-            element_nodes_0[2] = p_node_3;
-            Triangle2D3 <NodeType> triangle_0( PointerVector<NodeType>{element_nodes_0} );
-
-            std::vector<NodeType::Pointer> element_nodes_1 (3);
-            element_nodes_1[0] = p_node_1;
-            element_nodes_1[1] = p_node_3;
-            element_nodes_1[2] = p_node_4;
-            Triangle2D3 <NodeType> triangle_1( PointerVector<NodeType>{element_nodes_1} );
-
-            std::vector<NodeType::Pointer> element_nodes_2 (3);
-            element_nodes_2[0] = p_node_2;
-            element_nodes_2[1] = p_node_5;
-            element_nodes_2[2] = p_node_3;
-            Triangle2D3 <NodeType> triangle_2( PointerVector<NodeType>{element_nodes_2} );
-
-            std::vector<NodeType::Pointer> element_nodes_3 (3);
-            element_nodes_3[0] = p_node_5;
-            element_nodes_3[1] = p_node_6;
-            element_nodes_3[2] = p_node_3;
-            Triangle2D3 <NodeType> triangle_3( PointerVector<NodeType>{element_nodes_3} );
-
-            Element::Pointer p_elem_0 = first_model_part.CreateNewElement("Element2D3N", 1, triangle_0, p_elem_prop);
-            Element::Pointer p_elem_1 = first_model_part.CreateNewElement("Element2D3N", 2, triangle_1, p_elem_prop);
-            Element::Pointer p_elem_2 = first_model_part.CreateNewElement("Element2D3N", 3, triangle_2, p_elem_prop);
-            Element::Pointer p_elem_3 = first_model_part.CreateNewElement("Element2D3N", 4, triangle_3, p_elem_prop);
+            Element::Pointer p_elem_0 = first_model_part.CreateNewElement("Element2D3N", 1, {{1,2,3}}, p_elem_prop);
+            Element::Pointer p_elem_1 = first_model_part.CreateNewElement("Element2D3N", 2, {{1,3,4}}, p_elem_prop);
+            Element::Pointer p_elem_2 = first_model_part.CreateNewElement("Element2D3N", 3, {{2,5,3}}, p_elem_prop);
+            Element::Pointer p_elem_3 = first_model_part.CreateNewElement("Element2D3N", 4, {{5,6,3}}, p_elem_prop);
 
             // Adding nodes to random submodelparts
             first_sub_modelpart_1.AddElement(p_elem_0);
