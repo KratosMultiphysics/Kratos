bbbb2d6c544c8c90635b4703db40eaa13e7d8ab4
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-05-21 00:15:55 +0300

    Missing COTIRE macros

--- kratos/utilities/os_utilities.cpp
+++ kratos/utilities/os_utilities.cpp
@@ -19,6 +19,12 @@
 #ifdef KRATOS_COMPILED_IN_WINDOWS
 #include <direct.h>
 #include "Shlwapi.h"
+#ifndef S_ISDIR
+#define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
+#endif
+#ifndef S_ISREG
+#define S_ISREG(mode)  (((mode) & S_IFMT) == S_IFREG)
+#endif
 #define GetCurrentDir _getcwd
 #else
 #include <unistd.h>
