653060f5f8a3e311ea94236b03f44e4ecf4e73a9
marcnunezc <marcnc94@gmail.com>
2019-05-20 14:46:19 +0200

    Using move-model-aprt process and defining protected methods

--- applications/CompressiblePotentialFlowApplication/python_scripts/initialize_geometry.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/initialize_geometry.py
@@ -9,15 +9,7 @@ def Factory(settings, Model):
     if( not isinstance(settings,KratosMultiphysics.Parameters) ):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return InitializeGeometryProcess(Model, settings["Parameters"])
-def MoveModelPart(origin, skin_model_part):
-    for node in skin_model_part.Nodes:
-        node.X=origin[0]+node.X
-        node.Y=origin[1]+node.Y
-def RotateModelPart(origin, angle, model_part):
-    ox,oy=origin
-    for node in model_part.Nodes:
-        node.X = ox+math.cos(angle)*(node.X - ox)-math.sin(angle)*(node.Y - oy)
-        node.Y = oy+math.sin(angle)*(node.X - ox)+math.cos(angle)*(node.Y - oy)
+
 ## All the processes python should be derived from "Process"
 class InitializeGeometryProcess(KratosMultiphysics.Process):
     def __init__(self, Model, settings ):
@@ -27,10 +19,13 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
             {
                 "model_part_name": "insert_model_part",
                 "skin_model_part_name": "insert_skin_model_part",
-                "geometry_parameter": 0.0,
                 "maximum_iterations": 1,
                 "remeshing_flag": false,
-                "initial_point": [0,0],
+                "moving_parameters":    {
+                    "origin"                        : [0.0,0.0,0.0],
+                    "rotation_angle"                : 0.0,
+                    "sizing_multiplier"             : 1.0
+                },
                 "metric_parameters":  {
                     "minimal_size"                         : 5e-3,
                     "maximal_size"                         : 1.0,
@@ -58,8 +53,6 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
         settings.ValidateAndAssignDefaults(default_parameters)
         self.model=Model
         self.main_model_part = Model.GetModelPart(settings["model_part_name"].GetString()).GetRootModelPart()
-        self.geometry_parameter = settings["geometry_parameter"].GetDouble()
-        self.initial_point = settings["initial_point"].GetVector()
         self.skin_model_part_name=settings["skin_model_part_name"].GetString()
 
         '''Remeshing loop parameters'''
@@ -67,27 +60,28 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
         self.step = 0
         self.max_iter = settings["maximum_iterations"].GetInt()
 
+        self.moving_parameters = settings["moving_parameters"]
         self.metric_parameters = settings["metric_parameters"]
         self.distance_modification_parameters = settings["distance_modification_parameters"]
 
         KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(MeshingApplication.METRIC_TENSOR_2D, self.main_model_part.Nodes)
 
-    def Execute(self):
+    def ExecuteInitialize(self):
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','Executing Initialize Geometry')
-        self.InitializeSkinModelPart()
-        self.CalculateDistance()
+        self._InitializeSkinModelPart()
+        self._CalculateDistance()
 
         ini_time=time.time()
         while self.step < self.max_iter and self.do_remeshing:
             self.step += 1
             KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','##### Executing refinement #', self.step, ' #####')
-            self.ExtendDistance()
-            self.RefineMesh()
-            self.CalculateDistance()
-            self.UpdateParameters()
-        self.ModifyFinalDistance()
-        self.ExtendDistance()
-        self.CopyAndDeleteDefaultDistance()
+            self._ExtendDistance()
+            self._RefineMesh()
+            self._CalculateDistance()
+            self._UpdateParameters()
+        self._ModifyFinalDistance()
+        self._ExtendDistance()
+        self._CopyAndDeleteDefaultDistance()
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','Elapsed time: ',time.time()-ini_time)
 
         ''' ############################################################################################# '''
@@ -99,34 +93,27 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
             self.main_model_part, avg_elem_num, avg_node_num).Execute()
         ''' ############################################################################################## '''
 
-    def ExecuteInitialize(self):
-        self.Execute()
-
-    def InitializeSkinModelPart(self):
-        ''' This function loads and moves the skin_model_part in the main_model_part to the desired initial point (self.initial_point).
-            It also rotates the skin model part around the initial point according to the (self.geometry_parameter)'''
+    def _InitializeSkinModelPart(self):
+        ''' This function loads and moves the skin_model_part in the main_model_part to the desired initial point (origin).
+            It also rotates the skin model part around the origin point according to the rotation_angle'''
         self.skin_model_part=self.model.CreateModelPart("skin")
-        KratosMultiphysics.ModelPartIO(self.skin_model_part_name).ReadModelPart(self.skin_model_part)
         ini_time=time.time()
-        if self.skin_model_part_name=='naca0012':
-            MoveModelPart(self.initial_point, self.skin_model_part)
-            self.origin=[0.25+self.initial_point[0],0+self.initial_point[1]]
-            angle=math.radians(-self.geometry_parameter)
-            RotateModelPart(self.origin,angle,self.skin_model_part)
-        elif self.skin_model_part_name=='ellipse':
-            MoveModelPart(self.initial_point, self.skin_model_part)
-            self.origin=[self.initial_point[0],self.initial_point[1]]
-            angle=math.radians(-self.geometry_parameter)
-            RotateModelPart(self.origin,angle,self.skin_model_part)
+        # Reading skin model part
+        KratosMultiphysics.ModelPartIO(self.skin_model_part_name).ReadModelPart(self.skin_model_part)
+        # Moving and rotating the skin model part
+        angle=math.radians(-self.moving_parameters["rotation_angle"].GetDouble())
+        self.moving_parameters["rotation_angle"].SetDouble(angle)
+        CompressiblePotentialFlow.MoveModelPartProcess(self.skin_model_part, self.moving_parameters).Execute()
+
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','InitializeSkin time: ',time.time()-ini_time)
 
-    def CalculateDistance(self):
+    def _CalculateDistance(self):
         ''' This function calculate the distance to skin for every node in the main_model_part.'''
         ini_time=time.time()
         KratosMultiphysics.CalculateDistanceToSkinProcess2D(self.main_model_part, self.skin_model_part).Execute()
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','CalculateDistance time: ',time.time()-ini_time)
 
-    def ExtendDistance(self):
+    def _ExtendDistance(self):
         ''' This function extends the distance field to all the nodes of the main_model_part in order to
             remesh the background mesh.'''
         ini_time=time.time()
@@ -159,7 +146,7 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','Variational distance process time: ',time.time()-ini_time)
 
 
-    def RefineMesh(self):
+    def _RefineMesh(self):
         ''' This function remeshes the main_model_part according to the distance, using the MMG process from the MeshingApplication.
             In order to perform the refinement, it is needed to calculate the distance gradient, the initial nodal_h and the level_set metric.
         '''
@@ -186,14 +173,13 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
         mmg_process.Execute()
 
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','Remesh time: ',time.time()-ini_time)
-        self.PrintOutput('remeshed_output'+str(self.step))
 
-    def UpdateParameters(self):
+    def _UpdateParameters(self):
         ''' This process updates remeshing parameters in case more than one iteration is performed'''
         previous_size=self.metric_parameters["minimal_size"].GetDouble()
         self.metric_parameters["minimal_size"].SetDouble(previous_size*0.5)
 
-    def ModifyFinalDistance(self):
+    def _ModifyFinalDistance(self):
         ''' This function modifies the distance field to avoid ill defined cuts.
         '''
         ini_time = time.time()
@@ -202,7 +188,7 @@ class InitializeGeometryProcess(KratosMultiphysics.Process):
         KratosMultiphysics.FluidDynamicsApplication.DistanceModificationProcess(self.main_model_part,self.distance_modification_parameters).Execute()
         KratosMultiphysics.Logger.PrintInfo('InitializeGeometry','Modify distance time: ',time.time()-ini_time)
 
-    def CopyAndDeleteDefaultDistance(self):
+    def _CopyAndDeleteDefaultDistance(self):
         ''' This function copies the distance field to an auxiliary distance variable and sets
         to zero the default one.
         '''
