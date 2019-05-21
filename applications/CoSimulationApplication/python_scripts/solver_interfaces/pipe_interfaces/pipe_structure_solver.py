6278a3d465c02bec23bdd2347bbe10c49edd4edc
joris <Joris.Degroote@UGent.be>
2019-05-21 14:45:56 +0200

    Moved the checking of initialization and finalization to component.

--- applications/CoSimulationApplication/python_scripts/solver_interfaces/pipe_interfaces/pipe_structure_solver.py
+++ applications/CoSimulationApplication/python_scripts/solver_interfaces/pipe_interfaces/pipe_structure_solver.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math as m
-import os
 
 from KratosMultiphysics.CoSimulationApplication.co_simulation_component import CoSimulationComponent
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
@@ -13,6 +12,8 @@ def Create(parameters):
 
 class SolverInterfacePipeStructure(CoSimulationComponent):
     def __init__(self, parameters):
+        super().__init__()
+
         self.parameters = parameters
         self.settings = parameters["settings"]
 
@@ -38,50 +39,13 @@ class SolverInterfacePipeStructure(CoSimulationComponent):
         self.a0 = m.pi * self.d ** 2 / 4.0  # Reference area of cross section
         self.c02 = self.cmk2 - self.p0 / 2.0  # Wave speed squared with reference pressure
 
-        self.initialized = False
-        self.initializedstep = False
-
-    def getinputgrid(self):
-        return self.z
-
-    def setinputgrid(self, z):
-        if np.linalg.norm(self.z - z) / np.linalg.norm(self.z) > np.finfo(float).eps:
-            Exception("Mapper not implemented")
-
-    def getoutputgrid(self):
-        return self.z
-
-    def setoutputgrid(self, z):
-        if np.linalg.norm(self.z - z) / np.linalg.norm(self.z) > np.finfo(float).eps:
-            Exception("Mapper not implemented")
-
-    def getinputdata(self):
-        return self.p
-
-    def gettimestep(self):
-        return self.dt
-
-    def settimestep(self, dt):
-        if self.initializedstep:
-            Exception("Step ongoing")
-        else:
-            self.dt = dt
-
     def Initialize(self):
-        if self.initialized:
-            Exception("Already initialized")
-        else:
-            self.initialized = True
+        super().Initialize()
 
     def InitializeSolutionStep(self):
-        if self.initialized:
-            if self.initializedstep:
-                Exception("Step ongoing")
-            else:
-                self.n += 1
-                self.initializedstep = True
-        else:
-            Exception("Not initialized")
+        super().InitializeSolutionStep()
+
+        self.n += 1
 
     def calculate(self, p):
         # Independent rings model
@@ -95,16 +59,7 @@ class SolverInterfacePipeStructure(CoSimulationComponent):
         return np.array(self.a)
 
     def FinalizeSolutionStep(self):
-        if self.initialized:
-            if self.initializedstep:
-                self.initializedstep = False
-            else:
-                Exception("No step ongoing")
-        else:
-            Exception("Not initialized")
+        super().FinalizeSolutionStep()
 
     def Finalize(self):
-        if self.initialized:
-            self.initialized = False
-        else:
-            Exception("Not initialized")
+        super().Finalize()
