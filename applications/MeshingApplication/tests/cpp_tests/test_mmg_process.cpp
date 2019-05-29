eb6a01eb6b080f246bedc03b9b82aa6e36313c11
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-05-29 16:51:01 +0200

    Update test

--- applications/MeshingApplication/tests/cpp_tests/test_mmg_process.cpp
+++ applications/MeshingApplication/tests/cpp_tests/test_mmg_process.cpp
@@ -90,7 +90,7 @@ namespace Kratos
             }
 
             // Compute remesh
-            Parameters params = Parameters(R"({ "echo_level" : 0 })" );
+            Parameters params = Parameters(R"({ "echo_level" : 0, "preserve_flags" : true })" );
             MmgProcess<MMGLibrary::MMG2D> mmg_process(r_model_part, params);
             mmg_process.Execute();
 
@@ -172,7 +172,7 @@ namespace Kratos
             }
 
             // Compute remesh
-            Parameters params = Parameters(R"({ "echo_level" : 0 })" );
+            Parameters params = Parameters(R"({ "echo_level" : 0, "preserve_flags" : true })" );
             MmgProcess<MMGLibrary::MMG3D> mmg_process(r_model_part, params);
             mmg_process.Execute();
 
