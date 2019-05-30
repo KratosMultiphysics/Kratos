2f4d8d677e7a2d84490f763f0287330866ea1849
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2019-05-30 20:15:49 +0200

    Update position from VariableUtils()

--- applications/FSIapplication/python_scripts/fsi_coupling_interface.py
+++ applications/FSIapplication/python_scripts/fsi_coupling_interface.py
@@ -95,11 +95,7 @@ class FSICouplingInterface():
         return self.GetInterfaceModelPart().ProcessInfo[KratosMultiphysics.FSI_INTERFACE_RESIDUAL_NORM]
 
     def UpdatePosition(self):
-        for node in self.GetInterfaceModelPart().Nodes:
-            aux_disp = node.GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT)
-            node.X = node.X0 + aux_disp[0]
-            node.Y = node.Y0 + aux_disp[1]
-            node.Z = node.Z0 + aux_disp[2]
+        KratosMultiphysics.VariableUtils().UpdateCurrentPosition(self.GetInterfaceModelPart().Nodes)
 
     def GetValuesFromFatherModelPart(self, variable):
         buffer_step = 0
