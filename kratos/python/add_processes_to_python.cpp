7b600c65ea72c62e0818a54c4b4a4bfceb3d4be6
pooyan <pooyan@altair.com>
2019-06-04 15:27:17 +0200

    changing the VoxelMeshColoringProcess constructor

--- kratos/python/add_processes_to_python.cpp
+++ kratos/python/add_processes_to_python.cpp
@@ -579,7 +579,7 @@ void  AddProcessesToPython(pybind11::module& m)
 
 
     py::class_<VoxelMeshColoringProcess, VoxelMeshColoringProcess::Pointer, Process>(m,"VoxelMeshColoringProcess")
-            .def(py::init<Point const&, Point const&, ModelPart&, ModelPart&, Parameters&>()) 
+            .def(py::init<Point const&, Point const&, array_1d<std::size_t,3> const&, ModelPart&, ModelPart&, Parameters&>()) 
     ;
 
     py::class_<VoxelMeshGeneratorProcess, VoxelMeshGeneratorProcess::Pointer, Process>(m,"VoxelMeshGeneratorProcess")
