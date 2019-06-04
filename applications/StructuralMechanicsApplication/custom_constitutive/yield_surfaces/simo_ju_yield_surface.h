1f864145b641c18361e37e96cf3010d0dea83484
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-06-04 12:05:09 +0200

    a

--- applications/StructuralMechanicsApplication/custom_constitutive/yield_surfaces/simo_ju_yield_surface.h
+++ applications/StructuralMechanicsApplication/custom_constitutive/yield_surfaces/simo_ju_yield_surface.h
@@ -183,10 +183,10 @@ class SimoJuYieldSurface
         const double n = yield_compression / yield_tension;
 
         if (r_material_properties[SOFTENING_TYPE] == static_cast<int>(SofteningType::Exponential)) {
-            rAParameter = 1.0 / (fracture_energy * n * n / (CharacteristicLength * std::pow(yield_compressionompression, 2)) - 0.5);
+            rAParameter = 1.0 / (fracture_energy * n * n / (CharacteristicLength * std::pow(yield_compression, 2)) - 0.5);
             KRATOS_ERROR_IF(rAParameter < 0.0) << "Fracture energy is too low, increase FRACTURE_ENERGY..." << std::endl;
         } else { // linear
-            rAParameter = -std::pow(yield_compressionompression, 2) / (2.0 * fracture_energy * n * n / CharacteristicLength);
+            rAParameter = -std::pow(yield_compression, 2) / (2.0 * fracture_energy * n * n / CharacteristicLength);
         }
     }
 
