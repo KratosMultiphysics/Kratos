e024fc075e34c8ddae35b2e431c1b0797f10b1c0
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-06-04 10:05:23 +0200

    removing unised includes

--- applications/FemToDemApplication/custom_python/add_custom_utilities_to_python.cpp
+++ applications/FemToDemApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -16,14 +16,8 @@
 // External includes
 #include <pybind11/pybind11.h>
 
-
 // Project includes
-#include "includes/define.h"
-#include "processes/process.h"
 #include "custom_python/add_custom_utilities_to_python.h"
-
-#include "spaces/ublas_space.h"
-#include "linear_solvers/linear_solver.h"
 
 namespace Kratos
 {
@@ -32,11 +26,6 @@ namespace Python
 void  AddCustomUtilitiesToPython(pybind11::module& m)
 {
 	using namespace pybind11;
-
-	typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
-	typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
-	typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
-
 }
 
 }  // namespace Python.
