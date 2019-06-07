9b48a7b1be2bff99639e9c7ffe35364da4e4528f
marcnunezc <marcnc94@gmail.com>
2019-06-07 16:18:16 +0200

    adding more functions

--- applications/CompressiblePotentialFlowApplication/python_scripts/define_embedded_wake_process_2d.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/define_embedded_wake_process_2d.py
@@ -45,17 +45,23 @@ class DefineEmbeddedWakeProcess(DefineWakeProcess2D):
         '''
         This function deactivates all the elements near the trailing edge node to consistenly attach the wake.
         '''
-        deactivated_model_part=self.fluid_model_part.CreateSubModelPart("deactivated")
+        self.deactivated_model_part=self.fluid_model_part.CreateSubModelPart("deactivated")
         deactivated_element_id_list=[]
-        max_x_node=-1e10
         for element in self.boundary_sub_model_part.Elements:
             for elnode in element.GetNodes():
                 if elnode.Id == self.max_inactive_node.Id:
                     self.__DeactivateActive(element)
                     deactivated_element_id_list.append(element.Id)
-        deactivated_model_part.AddElements(deactivated_element_id_list)
+        self.deactivated_model_part.AddElements(deactivated_element_id_list)
+        self.__FindTrailingEdgeNodeFromInactiveBall()
+
+    def __FindTrailingEdgeNodeFromInactiveBall(self):
+        '''
+        This function find the furthest inactive node that will be defined as the trailing edge
+        '''
+        max_x_node=-1e10
         max_x_center=-1e10
-        for element in deactivated_model_part.Elements:
+        for element in self.deactivated_model_part.Elements:
             n_center = 0
             center_X=element.GetGeometry().Center().X
             if center_X>max_x_center:
@@ -67,12 +73,12 @@ class DefineEmbeddedWakeProcess(DefineWakeProcess2D):
                 max_x_node=elnode.X
                 max_node=elnode
 
-        self.trailing_edge_node = max_node
-
         for element in self.boundary_sub_model_part.Elements:
             center_X=element.GetGeometry().Center().X
             if center_X>self.max_inactive_node.X:
                 self.__DeactivateBoundary(element)
+
+        self.trailing_edge_node = max_node
 
     def __DeactivateBoundary(self,elem):
         elem.Set(KratosMultiphysics.TO_SPLIT,False)
