41a266a930d1919f9345203f1ffd4dcd723be2eb
philbucher <philipp.bucher@tum.de>
2019-06-07 16:59:44 +0200

    cleaning vtk_output_process.py

--- kratos/python_scripts/vtk_output_process.py
+++ kratos/python_scripts/vtk_output_process.py
@@ -11,37 +11,15 @@ class VtkOutputProcess(KratosMultiphysics.Process):
     def __init__(self, model, settings ):
         KratosMultiphysics.Process.__init__(self)
 
-        # IMPORTANT: when "output_control_type" is "time",
-        # then paraview will not be able to group them
-        default_parameters = KratosMultiphysics.Parameters("""{
-            "model_part_name"                    : "PLEASE_SPECIFY_MODEL_PART_NAME",
-            "file_format"                        : "ascii",
-            "output_precision"                   : 7,
-            "output_control_type"                : "step",
-            "output_frequency"                   : 1.0,
-            "output_sub_model_parts"             : false,
-            "folder_name"                        : "VTK_Output",
-            "custom_name_prefix"                 : "",
-            "save_output_files_in_folder"        : true,
-            "nodal_solution_step_data_variables" : [],
-            "nodal_data_value_variables"         : [],
-            "nodal_flags"                        : [],
-            "element_data_value_variables"       : [],
-            "element_flags"                      : [],
-            "condition_data_value_variables"     : [],
-            "condition_flags"                    : [],
-            "gauss_point_variables"              : []
-        }""")
-
         model_part_name = settings["model_part_name"].GetString()
         self.model_part = model[model_part_name]
 
-        self.settings = settings
-        self.settings.ValidateAndAssignDefaults(default_parameters)
+        # default settings can be found in "vtk_output.cpp"
+        self.vtk_io = KratosMultiphysics.VtkOutput(self.model_part, settings) # this also validates the settings
 
-        if self.settings["save_output_files_in_folder"].GetBool():
+        if settings["save_output_files_in_folder"].GetBool():
             if self.model_part.GetCommunicator().MyPID() == 0:
-                folder_name = self.settings["folder_name"].GetString()
+                folder_name = settings["folder_name"].GetString()
                 if not self.model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
                     import KratosMultiphysics.kratos_utilities as kratos_utils
                     kratos_utils.DeleteDirectoryIfExisting(folder_name)
@@ -49,10 +27,8 @@ class VtkOutputProcess(KratosMultiphysics.Process):
                     os.mkdir(folder_name)
             self.model_part.GetCommunicator().GetDataCommunicator().Barrier()
 
-        self.vtk_io = KratosMultiphysics.VtkOutput(self.model_part, self.settings)
-
-        self.output_frequency = self.settings["output_frequency"].GetDouble()
-        self.output_control = self.settings["output_control_type"].GetString()
+        self.output_frequency = settings["output_frequency"].GetDouble()
+        self.output_control = settings["output_control_type"].GetString()
         self.next_output = 0.0
 
         self.__ScheduleNextOutput() # required here esp for restart
