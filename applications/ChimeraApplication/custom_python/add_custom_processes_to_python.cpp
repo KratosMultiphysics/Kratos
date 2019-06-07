43845afda27f486b91b0360ec9848b1f211d9e51
AdityaGhantasala <aditya.ghantasala@tum.de>
2019-06-07 17:56:45 +0200

    deleting unused exposes and modification for new classes

--- applications/ChimeraApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/ChimeraApplication/custom_python/add_custom_processes_to_python.cpp
@@ -15,10 +15,8 @@
 // External includes
 
 // Project includes
-#include "includes/model_part.h"
 #include "processes/process.h"
 #include "custom_python/add_custom_processes_to_python.h"
-
 
 //#include "custom_processes/spalart_allmaras_turbulence_model_for_chimera.h"
 #include "custom_processes/custom_hole_cutting_process.h"
@@ -33,61 +31,37 @@ namespace Kratos
 
 namespace Python
 {
-using namespace pybind11;
 
-void AddCustomProcessesToPython(pybind11::module& m)
+void AddCustomProcessesToPython(pybind11::module &m)
 {
 
-	class_<ApplyChimeraProcessMonolithic<2>, ApplyChimeraProcessMonolithic<2>::Pointer, Process>(m, "ApplyChimeraProcessMonolithic2d")
-		.def(init< ModelPart&, Parameters >())
-		.def("ApplyMpcConstraint", &ApplyChimeraProcessMonolithic<2>::ApplyMpcConstraint)
-		.def("FormulateChimera2D", &ApplyChimeraProcessMonolithic<2>::FormulateChimera)
-		.def("SetOverlapDistance",&ApplyChimeraProcessMonolithic<2>::SetOverlapDistance)
-		;
+    namespace py = pybind11;
+    py::class_<ApplyChimeraProcessMonolithic<2>, ApplyChimeraProcessMonolithic<2>::Pointer, Process>(m, "ApplyChimeraProcessMonolithic2d")
+        .def(py::init<ModelPart &, Parameters>());
 
-	class_<ApplyChimeraProcessMonolithic<3>, ApplyChimeraProcessMonolithic<3>::Pointer, Process>(m, "ApplyChimeraProcessMonolithic3d")
-		.def(init< ModelPart&, Parameters >())
-		.def("ApplyMpcConstraint", &ApplyChimeraProcessMonolithic<3>::ApplyMpcConstraint)
-		.def("FormulateChimera2D", &ApplyChimeraProcessMonolithic<3>::FormulateChimera)
-		.def("SetOverlapDistance",&ApplyChimeraProcessMonolithic<3>::SetOverlapDistance)
-		;
+    py::class_<ApplyChimeraProcessMonolithic<3>, ApplyChimeraProcessMonolithic<3>::Pointer, Process>(m, "ApplyChimeraProcessMonolithic3d")
+        .def(py::init<ModelPart &, Parameters>());
 
-	class_<ApplyChimeraProcessFractionalStep<2>, ApplyChimeraProcessFractionalStep<2>::Pointer, Process>(m, "ApplyChimeraProcessFractionalStep2d")
-		.def(init< ModelPart&, Parameters >())
-		.def("FormulateChimera2D", &ApplyChimeraProcessFractionalStep<2>::FormulateChimera)
-		.def("SetOverlapDistance",&ApplyChimeraProcessFractionalStep<2>::SetOverlapDistance)
-		.def("CalculateNodalAreaAndNodalMass",&ApplyChimeraProcessFractionalStep<2>::CalculateNodalAreaAndNodalMass)
-		;
+    py::class_<ApplyChimeraProcessFractionalStep<2>, ApplyChimeraProcessFractionalStep<2>::Pointer, Process>(m, "ApplyChimeraProcessFractionalStep2d")
+        .def(py::init<ModelPart &, Parameters>());
 
-	class_<ApplyChimeraProcessFractionalStep<3>, ApplyChimeraProcessFractionalStep<3>::Pointer, Process>(m, "ApplyChimeraProcessFractionalStep3d")
-		.def(init< ModelPart&, Parameters >())
-		.def("FormulateChimera2D", &ApplyChimeraProcessFractionalStep<3>::FormulateChimera)
-		.def("SetOverlapDistance",&ApplyChimeraProcessFractionalStep<3>::SetOverlapDistance)
-		.def("CalculateNodalAreaAndNodalMass",&ApplyChimeraProcessFractionalStep<3>::CalculateNodalAreaAndNodalMass)
-		;
+    py::class_<ApplyChimeraProcessFractionalStep<3>, ApplyChimeraProcessFractionalStep<3>::Pointer, Process>(m, "ApplyChimeraProcessFractionalStep3d")
+        .def(py::init<ModelPart &, Parameters>());
 
+    py::class_<RotateRegionProcess, RotateRegionProcess::Pointer, Process>(m, "RotateRegionProcess")
+        .def(py::init<ModelPart &, Parameters>())
+        .def("SetCentreOfRotation", &RotateRegionProcess::SetCentreOfRotation)
+        .def("ChangeAngularVelocity", &RotateRegionProcess::ChangeAngularVelocity);
 
-    class_< RotateRegionProcess,RotateRegionProcess::Pointer, Process >(m, "RotateRegionProcess")
-		.def(init< ModelPart&, Parameters >())
-		.def("SetCentreOfRotation", &RotateRegionProcess::SetCentreOfRotation)
-		.def("ChangeAngularVelocity", &RotateRegionProcess::ChangeAngularVelocity)
-		;
+    py::class_<CustomCalculateSignedDistanceProcess<2>>(m, "SignedDistanceProcess2d")
+        .def(py::init<>())
+        .def("CalculateSignedDistance", &CustomCalculateSignedDistanceProcess<2>::CalculateSignedDistance);
 
-	class_<CustomCalculateSignedDistanceProcess<2>>(m, "SignedDistanceProcess2d")
-		.def(init<>())
-		.def("CalculateSignedDistance", &CustomCalculateSignedDistanceProcess<2>::CalculateSignedDistance)
-		;
-
-	class_<CustomCalculateSignedDistanceProcess<3>>(m, "SignedDistanceProcess3d")
-		.def(init<>())
-		.def("CalculateSignedDistance", &CustomCalculateSignedDistanceProcess<3>::CalculateSignedDistance)
-		;
-
+    py::class_<CustomCalculateSignedDistanceProcess<3>>(m, "SignedDistanceProcess3d")
+        .def(py::init<>())
+        .def("CalculateSignedDistance", &CustomCalculateSignedDistanceProcess<3>::CalculateSignedDistance);
 }
-
-
 
 } // namespace Python.
 
 } // Namespace Kratos
-
