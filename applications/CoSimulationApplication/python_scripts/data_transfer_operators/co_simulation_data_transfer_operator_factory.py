84443527569f2a62cc3736e5b661118931d91d67
philbucher <philipp.bucher@tum.de>
2019-06-11 15:52:30 +0200

    typo

--- applications/CoSimulationApplication/python_scripts/data_transfer_operators/co_simulation_data_transfer_operator_factory.py
+++ applications/CoSimulationApplication/python_scripts/data_transfer_operators/co_simulation_data_transfer_operator_factory.py
@@ -4,7 +4,7 @@ def CreateDataTransferOperator(data_transfer_operator_settings):
     """
     This function creates and returns the data transfer operator.
     """
-    data_transfer_operator_type = settings["type"].GetString()
+    data_transfer_operator_type = data_transfer_operator_settings["type"].GetString()
     module_full  = 'KratosMultiphysics.CoSimulationApplication.data_transfer_operators.'+data_transfer_operator_type
     module_full += '_data_transfer_operator'
     data_tranfer_operator_module = __import__(module_full, fromlist=[data_transfer_operator_type])
