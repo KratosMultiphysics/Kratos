e4a78ced32a6f86593d115bd61d0d829a8e8f37d
philbucher <philipp.bucher@tum.de>
2019-06-11 13:51:32 +0200

    deriving ConvAcc from CouplingOperation

--- applications/CoSimulationApplication/python_scripts/base_classes/co_simulation_base_convergence_accelerator.py
+++ applications/CoSimulationApplication/python_scripts/base_classes/co_simulation_base_convergence_accelerator.py
@@ -1,40 +1,23 @@
 from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
 
+from . import co_simulation_base_coupling_operation
 # Other imports
-import numpy as np
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
 
-class CoSimulationBaseConvergenceAccelerator(object):
+class CoSimulationBaseConvergenceAccelerator(co_simulation_base_coupling_operation.CoSimulationBaseCouplingOperation):
     def __init__(self, settings, solver):
-        self.settings = settings
-        self.settings.RecursivelyValidateAndAssignDefaults(self._GetDefaultSettings())
+        super(CoSimulationBaseConvergenceAccelerator, self).__init__(settings)
 
         self.solver = solver
         self.interface_data = self.solver.GetInterfaceData(self.settings["data_name"].GetString())
 
-        self.echo_level = self.settings["echo_level"].GetInt()
 
-    def Initialize(self):
-        pass
-
-    def Finalize(self):
-        pass
-
-    def InitializeSolutionStep(self):
-        pass
-
-    def FinalizeSolutionStep(self):
-        pass
-
-    def InitializeNonLinearIteration(self):
+    def InitializeCouplingIteration(self):
         # Saving the previous data for the computation of the residual
         # and the computation of the solution update
         self.input_data = self.interface_data.GetNumpyArray()
 
-    def FinalizeNonLinearIteration(self):
-        pass
-
-    def ComputeUpdate(self):
+    def Execute(self):
         current_data = self.interface_data.GetNumpyArray()
         residual = current_data - self.input_data
         updated_data = self.input_data + self._ComputeUpdate(residual, self.input_data)
@@ -46,12 +29,6 @@ class CoSimulationBaseConvergenceAccelerator(object):
         '''
         cs_tools.classprint("Convergence Accelerator", cs_tools.bold(self._Name()))
 
-    def SetEchoLevel(self, level):
-        self.echo_level = level
-
-    def _Name(self):
-        return self.__class__.__name__
-
     def Check(self):
         print("ConvAcc does not yet implement Check")
 
@@ -60,9 +37,12 @@ class CoSimulationBaseConvergenceAccelerator(object):
 
     @classmethod
     def _GetDefaultSettings(cls):
-        return cs_tools.cs_data_structure.Parameters("""{
+        this_defaults = cs_tools.cs_data_structure.Parameters("""{
             "type"       : "UNSPECIFIED",
             "solver"     : "UNSPECIFIED",
-            "data_name"  : "UNSPECIFIED",
-            "echo_level" : 0
+            "data_name"  : "UNSPECIFIED"
         }""")
+
+        this_defaults.AddMissingParameters(super(CoSimulationBaseConvergenceAccelerator, cls)._GetDefaultSettings())
+
+        return this_defaults
