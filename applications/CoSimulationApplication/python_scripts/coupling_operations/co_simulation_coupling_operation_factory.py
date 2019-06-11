9c74afb4f1e3b0865e30a0002e27125719d48847
philbucher <philipp.bucher@tum.de>
2019-06-11 13:42:32 +0200

    adding initial version for couplingiteration

---
+++ applications/CoSimulationApplication/python_scripts/coupling_operations/co_simulation_coupling_operation_factory.py
@@ -0,0 +1,11 @@
+from __future__ import print_function, absolute_import, division  # makes backward compatible with python 2.6 and 2.7
+
+def CreateCouplingOperation(coupling_operation_settings, solvers):
+    """
+    This function creates and returns the coupling operation.
+    """
+    coupling_operation_type = settings["type"].GetString()
+    module_full  = 'KratosMultiphysics.CoSimulationApplication.coupling_operations.'+coupling_operation_type
+    module_full += '_coupling_operation'
+    coupling_operation_module = __import__(module_full, fromlist=[coupling_operation_type])
+    return coupling_operation_module.Create(coupling_operation_settings, solvers)
