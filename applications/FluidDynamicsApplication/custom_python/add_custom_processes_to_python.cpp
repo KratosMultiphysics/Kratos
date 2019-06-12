c98dc7b162f6a54b85add29109a55388ee47afe9
riccardotosi <rtosi@cimne.upd.edu>
2019-06-12 09:46:38 +0200

    Meging from master

--- applications/FluidDynamicsApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/FluidDynamicsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -38,7 +38,6 @@
 #include "custom_processes/mass_conservation_check_process.h"
 #include "custom_processes/move_rotor_process.h"
 #include "custom_processes/two_fluids_inlet_process.h"
-#include "custom_processes/weighted_divergence_calculation_process.h"
 #include "spaces/ublas_space.h"
 
 #include "linear_solvers/linear_solver.h"
@@ -141,12 +140,6 @@ void AddCustomProcessesToPython(pybind11::module& m)
     (m,"TwoFluidsInletProcess")
     .def(py::init< ModelPart&, Parameters&, Process::Pointer >())
     .def("SmoothDistanceField", &TwoFluidsInletProcess::SmoothDistanceField)
-    ;
-
-    py::class_<WeightedDivergenceCalculationProcess, WeightedDivergenceCalculationProcess::Pointer, Process >
-        (m, "WeightedDivergenceCalculationProcess")
-        .def(py::init<ModelPart&>())
-        .def(py::init<ModelPart&, Parameters>())
     ;
 }
 
