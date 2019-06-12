536c0ce651418dc78506e72052c63c0576c7d24e
riccardotosi <rtosi@cimne.upd.edu>
2019-06-12 09:05:18 +0200

    Add author

--- applications/ExaquteSandboxApplication/exaqute_sandbox_application.cpp
+++ applications/ExaquteSandboxApplication/exaqute_sandbox_application.cpp
@@ -5,9 +5,9 @@
 //                   Multi-Physics
 //
 //  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//               Kratos default license: kratos/license.txt
 //
-//  Main authors:    @{KRATOS_APP_AUTHOR}
+//  Main authors:    Riccardo Tosi
 //
 
 
