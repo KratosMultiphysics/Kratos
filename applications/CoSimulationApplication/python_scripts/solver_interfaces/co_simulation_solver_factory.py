0e6cd33ca8f7a0e0931abe04a92b70037334e970
philbucher <philipp.bucher@tum.de>
2019-06-12 14:15:49 +0200

    further improved solvers import

--- applications/CoSimulationApplication/python_scripts/solver_interfaces/co_simulation_solver_factory.py
+++ applications/CoSimulationApplication/python_scripts/solver_interfaces/co_simulation_solver_factory.py
@@ -5,7 +5,7 @@ def CreateSolverInterface(model, settings, solver_name):
     This function creates and returns the coupled solver.
     """
     coupled_solver_type = settings["type"].GetString()
-    module_full  = 'KratosMultiphysics.CoSimulationApplication.solver_interfaces.'+coupled_solver_type
+    module_full  = 'KratosMultiphysics.CoSimulationApplication.'+coupled_solver_type
     module_full += '_solver'
     coupled_solver_module = __import__(module_full, fromlist=[coupled_solver_type])
     return coupled_solver_module.CreateSolver(model, settings, solver_name)
