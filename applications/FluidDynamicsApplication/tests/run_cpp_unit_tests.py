cf9019adddbaeb678ecfeee0e60ee03c9b583708
Marc Núñez <mnunez@cimne.upc.edu>
2019-06-13 10:59:57 +0200

    adding sensitivty tests again

--- applications/FluidDynamicsApplication/tests/run_cpp_unit_tests.py
+++ applications/FluidDynamicsApplication/tests/run_cpp_unit_tests.py
@@ -4,6 +4,7 @@ from KratosMultiphysics.FluidDynamicsApplication import *
 def run():
     Tester.SetVerbosity(Tester.Verbosity.PROGRESS) # TESTS_OUTPUTS
     Tester.RunTestSuite("FluidDynamicsApplicationFastSuite")
+    Tester.RunTestSuite("KratosSensitivityTestSuite")
 
 if __name__ == '__main__':
     run()
