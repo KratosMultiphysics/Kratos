c22e41c0327292f0f054da44df8f3a15619ce700
AdityaGhantasala <aditya.ghantasala@tum.de>
2019-06-13 18:21:20 +0200

    changung the position of local falags

--- applications/ChimeraApplication/chimera_application_variables.cpp
+++ applications/ChimeraApplication/chimera_application_variables.cpp
@@ -3,7 +3,7 @@
 namespace Kratos
 {
 // Flag for distinguishing b/w velocity and pressure constraints. Used in fractional-step approach
-KRATOS_CREATE_FLAG(FS_CHIMERA_VEL_CONSTRAINT, 100);
-KRATOS_CREATE_FLAG(FS_CHIMERA_PRE_CONSTRAINT, 100);
+KRATOS_CREATE_FLAG(FS_CHIMERA_VEL_CONSTRAINT, 10);
+KRATOS_CREATE_FLAG(FS_CHIMERA_PRE_CONSTRAINT, 10);
 
 }
