8e9195abedb4ddebc85bdfc11bf21c755255e4a0
philbucher <philipp.bucher@tum.de>
2019-06-17 20:41:31 +0200

    adding docu to dummy-io

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/dummy_io.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/dummy_io.py
@@ -1,9 +1,16 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_io import CoSimulationIO
 
 def Create(model, custom_settings):
     return DummyIO(model, custom_settings)
 
 class DummyIO(CoSimulationIO):
+    """This class is used if a Solver directly uses Kratos as a data-structure
+    e.g. Kratos itself or simple-solvers written in Python
+    """
+
     def ImportCouplingInterfaceData(self, data_object, from_solver=None):
         pass
 
