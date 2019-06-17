0ce6720659b76336a358d68b608af0397dfb2688
philbucher <philipp.bucher@tum.de>
2019-06-17 11:52:28 +0200

    improve naming conv-crit

--- applications/CoSimulationApplication/python_scripts/convergence_criteria/relative_norm_previous_residual.py
+++ applications/CoSimulationApplication/python_scripts/convergence_criteria/relative_norm_previous_residual.py
@@ -6,45 +6,28 @@ from numpy import linalg as la
 from KratosMultiphysics.CoSimulationApplication.co_simulation_tools import classprint, bold, green, red
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
 
-def CreateConvergenceCriteria(settings, solver_wrapper):
+# Importing the base class
+from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_convergence_criteria import CoSimulationConvergenceCriteria
+
+def Create(settings, solver_wrapper):
     cs_tools.SettingsTypeCheck(settings)
-    return CoSimulationConvergenceCriteria(settings, solver_wrapper)
+    return RelativeNormPreviousResidual(settings, solver_wrapper)
 
-class CoSimulationConvergenceCriteria(object):
+class RelativeNormPreviousResidual(CoSimulationConvergenceCriteria):
     def __init__(self, settings, solver_wrapper):
-        self.settings = settings
-        self.settings.RecursivelyValidateAndAssignDefaults(self._GetDefaultSettings())
+        super(RelativeNormPreviousResidual, self).__init__( settings, solver_wrapper)
 
-        self.interface_data = solver_wrapper.GetInterfaceData(self.settings["data_name"].GetString())
-
-        self.echo_level = self.settings["echo_level"].GetInt()
         self.abs_tolerance = self.settings["abs_tolerance"].GetDouble()
         self.rel_tolerance = self.settings["rel_tolerance"].GetDouble()
 
-
-    def Initialize(self):
-        pass
-
-    def Finalize(self):
-        pass
-
-    def InitializeSolutionStep(self):
-        pass
-
-    def FinalizeSolutionStep(self):
-        pass
-
     def InitializeCouplingIteration(self):
         # Saving the previous data (at beginning of iteration) for the computation of the residual
-        self.old_data = self.interface_data.GetNumpyArray()
-
-    def FinalizeCouplingIteration(self):
-        pass
+        self.prev_data = self.interface_data.GetNumpyArray()
 
     def IsConverged(self):
         new_data = self.interface_data.GetNumpyArray()
 
-        residual = new_data - self.old_data
+        residual = new_data - self.prev_data
         res_norm = la.norm(residual)
         norm_new_data = la.norm(new_data)
         if norm_new_data < 1e-15:
@@ -68,25 +51,12 @@ class CoSimulationConvergenceCriteria(object):
 
         return is_converged
 
-    def PrintInfo(self):
-        classprint("Convergence Criteria", bold(self._Name()))
-
-    def Check(self):
-        print("ConvCrit does not implement Check yet!")
-
-    def SetEchoLevel(self, level):
-        self.echo_level = level
-
-    def _Name(self):
-        return self.__class__.__name__
-
     @classmethod
     def _GetDefaultSettings(cls):
-        return cs_tools.cs_data_structure.Parameters("""{
-            "type"       : "UNSPECIFIED",
-            "solver"     : "UNSPECIFIED",
-            "data_name"  : "UNSPECIFIED",
+        this_defaults = cs_tools.cs_data_structure.Parameters("""{
             "abs_tolerance" : 1e-5,
-            "rel_tolerance" : 1e-5,
-            "echo_level" : 0
+            "rel_tolerance" : 1e-5
         }""")
+        this_defaults.AddMissingParameters(super(RelativeNormPreviousResidual, cls)._GetDefaultSettings())
+        return this_defaults
+
