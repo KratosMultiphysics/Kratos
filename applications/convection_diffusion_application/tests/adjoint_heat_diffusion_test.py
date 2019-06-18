a27dc2915f6f894125e588f3b94f24b13c18aaaf
jcotela <jcotela@cimne.upc.edu>
2019-06-18 11:18:35 +0200

    Updating tests script (WIP).

--- applications/convection_diffusion_application/tests/adjoint_heat_diffusion_test.py
+++ applications/convection_diffusion_application/tests/adjoint_heat_diffusion_test.py
@@ -1,22 +1,39 @@
-import KratosMultipyhsics as kratos
-import KratosMultipyhsics.ConvectionDiffusionApplication as convdiff
+import KratosMultiphysics as kratos
+import KratosMultiphysics.ConvectionDiffusionApplication as convdiff
 
-import KratosMultipyhsics.Unittest as unittest
+import KratosMultiphysics.KratosUnittest as unittest
 
 from convection_diffusion_analysis import ConvectionDiffusionAnalysis
 
 class AdjointHeatDiffusionTest(unittest.TestCase):
 
+    def setUp(self):
+        self.input_file = "adjoint_test"
+        #self.reference_file = "reference10_qasgs"
+        self.work_folder = "adjoint_diffusion_test"
+        self.primal_parameter_file_name = "ProjectParameters.json"
+        self.adjoint_parameter_file_name = "ProjectParameters.json"
+
+        self.check_tolerance = 1e-6
+        self.print_output = False
+        self.print_reference_values = False
+
     def testAdjointHeatDiffusion(self):
-        model = km.Model()
-        settings = km.Parameters(r'''{}''')
+        model = kratos.Model()
+        settings = kratos.Parameters(r'''{}''')
 
-        with open(primal_parameter_file_name,'r') as primal_parameter_file:
-            settings.AddValue("primal_settings", km.Parameters(primal_parameter_file.read()))
+        with unittest.WorkFolderScope(self.work_folder, __file__):
+            with open(self.primal_parameter_file_name,'r') as primal_parameter_file:
+                settings.AddValue("primal_settings", kratos.Parameters(primal_parameter_file.read()))
 
-        analysis = ConvectionDiffusionAnalysis(model,settings)
-        analysis.Run()
+            primal_analysis = ConvectionDiffusionAnalysis(model,settings["primal_settings"])
+            primal_analysis.Run()
 
+            with open(self.adjoint_parameter_file_name,'r') as adjoint_parameter_file:
+                settings.AddValue("adjoint_settings", kratos.Parameters(adjoint_parameter_file.read()))
+
+            adjoint_analysis = ConvectionDiffusionAnalysis(model,settings["adjoint_settings"])
+            adjoint_analysis.Run()
 
 if __name__ == '__main__':
     unittest.main()
