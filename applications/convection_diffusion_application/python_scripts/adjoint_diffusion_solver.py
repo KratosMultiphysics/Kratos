3734e29a1f1eb2bc69dd772db6a07fa06b11acd7
jcotela <jcotela@cimne.upc.edu>
2019-06-18 17:48:40 +0200

    Incremental updates.

--- applications/convection_diffusion_application/python_scripts/adjoint_diffusion_solver.py
+++ applications/convection_diffusion_application/python_scripts/adjoint_diffusion_solver.py
@@ -9,14 +9,16 @@ def CreateSolver(model, settings):
 class AdjointDiffusionSolver(PythonSolver):
 
     def __init__(self,model,settings):
-        super(self,AdjointDiffusionSolver).__init__(model, settings)
+        super(AdjointDiffusionSolver,self).__init__(model, settings)
+
+        self.ValidateSettings()
 
         domain_size = self.settings["domain_size"].GetInt()
         if domain_size not in [2,3]:
             raise Exception("Unsupported domain_size: ", domain_size)
 
-        model_part_name = self.settings["model_part_name"]
-        if model_part_name = "":
+        model_part_name = self.settings["model_part_name"].GetString()
+        if model_part_name == "":
             raise Exception("Empty model_part_name provided")
 
         if self.model.HasModelPart(model_part_name):
@@ -27,11 +29,27 @@ class AdjointDiffusionSolver(PythonSolver):
         self.model_part.ProcessInfo[kratos.DOMAIN_SIZE] = domain_size
 
     def AddVariables(self):
+        self.model_part.AddNodalSolutionStepVariable(kratos.TEMPERATURE)
         self.model_part.AddNodalSolutionStepVariable(convdiff.ADJOINT_HEAT_TRANSFER)
 
     def AddDofs(self):
         for node in self.model_part.Nodes:
             node.AddDof(convdiff.ADJOINT_HEAT_TRANSFER)
+
+    def GetComputingModelPart(self):
+        return self.model_part
+
+    def ImportModelPart(self):
+        # we can use the default implementation in the base class
+        self._ImportModelPart(self.model_part,self.settings["model_import_settings"])
+
+    def PrepareModelPart(self):
+        if not self.model_part.ProcessInfo[kratos.IS_RESTARTED]:
+
+            throw_errors = False
+            kratos.TetrahedralMeshOrientationCheck(self.model_part, throw_errors).Execute()
+
+            kratos.ReplaceElementsAndConditionsProcess(self.model_part,self.settings["element_replace_settings"]).Execute()
 
     def Initialize(self):
 
@@ -42,31 +60,31 @@ class AdjointDiffusionSolver(PythonSolver):
         else:
             raise Exception("invalid response_type: " + self.settings["response_function_settings"]["response_type"].GetString())
 
-        self.sensitivity_builder = KratosMultiphysics.SensitivityBuilder(self.settings["sensitivity_settings"], self.model_part, self.response_function)
+        self.sensitivity_builder = kratos.SensitivityBuilder(self.settings["sensitivity_settings"], self.model_part, self.response_function)
 
 
-        import kratos.python_linear_solver_factory as linear_solver_factory
+        import KratosMultiphysics.python_linear_solver_factory as linear_solver_factory
         self.linear_solver = linear_solver_factory.ConstructSolver(self.settings["linear_solver_settings"])
 
-        self.time_scheme = KratosMultiphysics.ResidualBasedAdjointSteadyScheme(self.response_function)
+        self.time_scheme = kratos.ResidualBasedAdjointStaticScheme(self.response_function)
 
-        builder_and_solver = KratosMultiphysics.ResidualBasedBlockBuilderAndSolver(self.linear_solver)
+        builder_and_solver = kratos.ResidualBasedBlockBuilderAndSolver(self.linear_solver)
 
-        self.solver = KratosMultiphysics.ResidualBasedLinearStrategy(self.model_part,
-                                                                     self.time_scheme,
-                                                                     self.linear_solver,
-                                                                     builder_and_solver,
-                                                                     False,
-                                                                     False,
-                                                                     False,
-                                                                     False)
+        self.solver = kratos.ResidualBasedLinearStrategy(self.model_part,
+                                                         self.time_scheme,
+                                                         self.linear_solver,
+                                                         builder_and_solver,
+                                                         False,
+                                                         False,
+                                                         False,
+                                                         False)
 
         self.solver.SetEchoLevel(self.settings["echo_level"].GetInt())
 
         self.solver.Initialize()
         self.response_function.Initialize()
         self.sensitivity_builder.Initialize()
-        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Solver initialization finished.")
+        kratos.Logger.PrintInfo(self.__class__.__name__, "Solver initialization finished.")
 
     def InitializeSolutionStep(self):
         self.solver.InitializeSolutionStep()
@@ -104,6 +122,10 @@ class AdjointDiffusionSolver(PythonSolver):
             },
             "linear_solver_settings" : {
                 "solver_type" : "amgcl"
+            },
+            "element_replace_settings" : {
+                "element_name" : "AdjointHeatDiffusionElement2D3N",
+                "condition_name" : ""
             },
             "volume_model_part_name" : "volume_model_part",
             "domain_model_parts"                 : [],
