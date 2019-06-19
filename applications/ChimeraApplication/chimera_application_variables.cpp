6fa63d096f6f47e1a667463d24cae9ca9353e85d
AdityaGhantasala <aditya.ghantasala@tum.de>
2019-06-19 19:26:07 +0200

    modifying the position of flags

--- applications/ChimeraApplication/chimera_application_variables.cpp
+++ applications/ChimeraApplication/chimera_application_variables.cpp
@@ -4,6 +4,6 @@ namespace Kratos
 {
 // Flag for distinguishing b/w velocity and pressure constraints. Used in fractional-step approach
 KRATOS_CREATE_FLAG(FS_CHIMERA_VEL_CONSTRAINT, 10);
-KRATOS_CREATE_FLAG(FS_CHIMERA_PRE_CONSTRAINT, 10);
+KRATOS_CREATE_FLAG(FS_CHIMERA_PRE_CONSTRAINT, 11);
 
 }
