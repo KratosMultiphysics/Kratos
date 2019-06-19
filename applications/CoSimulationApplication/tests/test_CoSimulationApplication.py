56960c80c73ca80e5b61f8995d14efab195f4799
philbucher <philipp.bucher@tum.de>
2019-06-19 10:10:49 +0200

    cleaning test_CoSimulationApplication

--- applications/CoSimulationApplication/tests/test_CoSimulationApplication.py
+++ applications/CoSimulationApplication/tests/test_CoSimulationApplication.py
@@ -1,22 +1,9 @@
-# import Kratos
-import KratosMultiphysics
-import KratosMultiphysics.CoSimulationApplication
-
 # Import Kratos "wrapper" for unittests
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 
-from co_simulation_solver_test_factory import TestKratosSolver
-from co_simulation_solver_test_factory import TestSDoFSolver
-from co_simulation_solver_test_factory import TestMDoFSolver
-from co_simulation_solver_test_factory import TestEmpireSolver
 from co_simulation_test_factory import TestSmallCoSimulationCases
 from co_simulation_test_factory import TestCoSimulationCases
 
-import os
-if "EMPIRE_API_LIBSO_ON_MACHINE" in os.environ:
-    empire_available = True
-else:
-    empire_available = False # the EMPIRE environment is not set
 
 def AssembleTestSuites():
     ''' Populates the test suites to run.
@@ -33,10 +20,6 @@ def AssembleTestSuites():
     suites = KratosUnittest.KratosSuites
 
     smallSuite = suites['small'] # These tests are executed by the continuous integration tool
-    # smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestKratosSolver]))
-    # smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestSDoFSolver]))
-    # smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestMDoFSolver]))
-    # smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestEmpireSolver]))
 
     nightSuite = suites['nightly'] # These tests are executed in the nightly build
     nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestSmallCoSimulationCases]))
@@ -47,17 +30,6 @@ def AssembleTestSuites():
     # For very long tests that should not be in nighly and you can use to validate
     validationSuite = suites['validation']
     validationSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestCoSimulationCases]))
-
-    # If EMPIRE is available then also add the tests involving EMPIRE
-    if empire_available:
-        pass
-
-
-
-
-
-
-
 
     # Create a test suit that contains all the tests:
     allSuite = suites['all']
