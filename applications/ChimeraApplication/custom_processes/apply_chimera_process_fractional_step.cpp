09823129463ce8c733c9030a83b1608f1a4d9e3a
AdityaGhantasala <aditya.ghantasala@tum.de>
2019-06-21 17:15:07 +0200

    adaption to diff sub modelparts and reformulation in base

--- applications/ChimeraApplication/custom_processes/apply_chimera_process_fractional_step.cpp
+++ applications/ChimeraApplication/custom_processes/apply_chimera_process_fractional_step.cpp
@@ -22,6 +22,128 @@
 namespace Kratos
 {
 
+template <int TDim>
+void ApplyChimeraProcessFractionalStep<TDim>::ApplyContinuityWithMpcs(ModelPart &rBoundaryModelPart, PointLocatorPointerType &pBinLocator)
+{
+    //loop over nodes and find the triangle in which it falls, then do interpolation
+    MasterSlaveContainerVectorType velocity_ms_container_vector;
+    MasterSlaveContainerVectorType pressure_ms_container_vector;
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+            velocity_ms_container_vector.resize(omp_get_num_threads());
+            for (auto &container : velocity_ms_container_vector)
+                container.reserve(1000);
+
+            pressure_ms_container_vector.resize(omp_get_num_threads());
+            for (auto &container : pressure_ms_container_vector)
+                container.reserve(1000);
+        }
+    }
+    std::vector<int> constraints_id_vector;
+
+    int num_constraints_required = (TDim + 1) * (rBoundaryModelPart.Nodes().size());
+    BaseType::CreateConstraintIds(constraints_id_vector, num_constraints_required);
+
+    const int max_results = 10000;
+    const unsigned int n_boundary_nodes = rBoundaryModelPart.Nodes().size();
+    std::size_t counter = 0;
+    std::size_t removed_counter = 0;
+    std::size_t not_found_counter = 0;
+
+    for (unsigned int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn)
+    {
+        ModelPart::NodesContainerType::iterator i_boundary_node = rBoundaryModelPart.NodesBegin() + i_bn;
+        Node<3>::Pointer p_boundary_node = *(i_boundary_node.base());
+
+        BaseType::mNodeIdToConstraintIdsMap[p_boundary_node->Id()].reserve(150);
+    }
+
+#pragma omp parallel for shared(constraints_id_vector, velocity_ms_container_vector, pressure_ms_container_vector, pBinLocator) reduction(+:not_found_counter) reduction(+:removed_counter) reduction(+ \
+                                                                                                                                           : counter)
+    for (unsigned int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn)
+    {
+
+        Vector shape_fun_weights;
+        typename PointLocatorType::ResultContainerType results(max_results);
+        auto &velocity_ms_container = velocity_ms_container_vector[omp_get_thread_num()]; //TODO: change this to out of loop.
+        auto &pressure_ms_container = pressure_ms_container_vector[omp_get_thread_num()]; //TODO: change this to out of loop.
+
+        ModelPart::NodesContainerType::iterator i_boundary_node = rBoundaryModelPart.NodesBegin() + i_bn;
+        Node<3>::Pointer p_boundary_node = *(i_boundary_node.base());
+        ConstraintIdsVectorType constrainIds_for_the_node;
+        unsigned int start_constraint_id = i_bn * (TDim + 1) * (TDim + 1);
+        bool node_coupled = false;
+        if ((p_boundary_node)->IsDefined(VISITED))
+            node_coupled = (p_boundary_node)->Is(VISITED);
+
+        typename PointLocatorType::ResultIteratorType result_begin = results.begin();
+        Element::Pointer p_element;
+        bool is_found = false;
+        is_found = pBinLocator->FindPointOnMesh(p_boundary_node->Coordinates(), shape_fun_weights, p_element, result_begin, max_results);
+
+        if (node_coupled && is_found)
+        {
+            constrainIds_for_the_node = BaseType::mNodeIdToConstraintIdsMap[p_boundary_node->Id()];
+            for (auto const &constraint_id : constrainIds_for_the_node)
+            {
+                BaseType::mrMainModelPart.RemoveMasterSlaveConstraintFromAllLevels(constraint_id);
+                removed_counter++;
+            }
+            p_boundary_node->Set(VISITED, false);
+        }
+
+        if (is_found == true)
+        {
+            Geometry<Node<3>> &r_geom = p_element->GetGeometry();
+            int init_index = 0;
+            BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_X, start_constraint_id+init_index, constraints_id_vector, velocity_ms_container);
+            init_index+=3;
+            BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_Y, start_constraint_id+init_index, constraints_id_vector, velocity_ms_container);
+            init_index+=3;
+            if(TDim == 3){
+                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_Z, start_constraint_id+init_index, constraints_id_vector, velocity_ms_container);
+                init_index+=3;
+            }
+            BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, PRESSURE, start_constraint_id+init_index, constraints_id_vector, pressure_ms_container);
+            init_index+=3;
+            counter += 1;
+        }else{
+            not_found_counter+=1;
+        }
+        p_boundary_node->Set(VISITED, true);
+    } // end of loop over boundary nodes
+
+    for (auto &container : velocity_ms_container_vector){
+        for(auto& constraint : container){
+            // TODO: Set the FS_CHIMERA_VEL_CONSTRAINT variable to true
+            constraint.Set(FS_CHIMERA_PRE_CONSTRAINT, false);
+            constraint.Set(FS_CHIMERA_VEL_CONSTRAINT, true);
+            constraint.Set(ACTIVE);
+        }
+        auto& vel_modelpart = BaseType::mrMainModelPart.GetSubModelPart("fs_velocity_model_part");
+        vel_modelpart.AddMasterSlaveConstraints(container.begin(), container.end());
+    }
+
+    for (auto &container : pressure_ms_container_vector){
+        for(auto& constraint : container){
+            // TODO: Set the FS_CHIMERA_PRE_CONSTRAINT variable to true
+            constraint.Set(FS_CHIMERA_PRE_CONSTRAINT, true);
+            constraint.Set(FS_CHIMERA_VEL_CONSTRAINT, false);
+            constraint.Set(ACTIVE);
+        }
+        auto& pre_modelpart = BaseType::mrMainModelPart.GetSubModelPart("fs_pressure_model_part");
+        pre_modelpart.AddMasterSlaveConstraints(container.begin(), container.end());
+    }
+
+    KRATOS_INFO("Number of boundary nodes in : ") << rBoundaryModelPart.Name() << " is coupled " << rBoundaryModelPart.NumberOfNodes() << std::endl;
+    KRATOS_INFO("Number of Boundary nodes found : ") << counter<<". Number of constraints : "<<counter*9<< std::endl;
+    KRATOS_INFO("Number of Boundary nodes not found  : ") << not_found_counter << std::endl;
+}
+
+
+
 template class ApplyChimeraProcessFractionalStep<2>;
 template class ApplyChimeraProcessFractionalStep<3>;
 
--- applications/ChimeraApplication/custom_processes/apply_chimera_process_fractional_step.h
+++ applications/ChimeraApplication/custom_processes/apply_chimera_process_fractional_step.h
@@ -56,6 +56,10 @@ public:
     KRATOS_CLASS_POINTER_DEFINITION(ApplyChimeraProcessFractionalStep);
     typedef ApplyChimeraProcessMonolithic<TDim> BaseType;
     typedef typename BaseType::MasterSlaveConstraintContainerType MasterSlaveConstraintContainerType;
+    typedef typename BaseType::PointLocatorType PointLocatorType;
+    typedef typename BaseType::PointLocatorPointerType PointLocatorPointerType;
+    typedef typename BaseType::MasterSlaveContainerVectorType MasterSlaveContainerVectorType;
+    typedef typename BaseType::ConstraintIdsVectorType ConstraintIdsVectorType;
 
     ///@}
     ///@name Life Cycle
@@ -99,57 +103,13 @@ protected:
     ///@}
     ///@name Protected Operations
     ///@{
+
     /**
-     * @brief Applies the master-slave constraint to enforce the continuity between a given geometry/element and a boundary node
-     * @param rGeometry The geometry of the element
-     * @param rBoundaryNode The boundary node for which the connections are to be made.
-     * @param rShapeFuncWeights The shape function weights for the node in the rGeometry.
-     * @param StartIndex The start Index of the constraints which are to be added.
-     * @param rConstraintIdVector The vector of the constraints Ids which is accessed with StartIndex.
-     * @param rMsContainer The Constraint container to which the contraints are added.
+     * @brief Applies the continuity between the boundary modelpart and the background.
+     * @param rBoundaryModelPart The boundary modelpart for which the continuity is to be enforced.
+     * @param pBinLocator The bin based locator formulated on the background. This is used to locate nodes on rBoundaryModelPart.
      */
-    void ApplyContinuityWithElement(Geometry<Node<3>> &rGeometry,
-                                    Node<3> &rBoundaryNode,
-                                    Vector &rShapeFuncWeights,
-                                    unsigned int StartId,
-                                    std::vector<int> &ConstraintIdVector,
-                                    typename BaseType::MasterSlaveConstraintContainerType &rMsContainer) override
-    {
-        const auto &r_clone_constraint = (LinearMasterSlaveConstraint)KratosComponents<MasterSlaveConstraint>::Get("LinearMasterSlaveConstraint");
-        // Initialise the boundary nodes dofs to 0 at ever time steps
-        rBoundaryNode.FastGetSolutionStepValue(VELOCITY_X, 0) = 0.0;
-        rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Y, 0) = 0.0;
-        if (TDim == 3)
-            rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Z, 0) = 0.0;
-        rBoundaryNode.FastGetSolutionStepValue(PRESSURE, 0) = 0.0;
-        for (std::size_t i = 0; i < rGeometry.size(); i++)
-        {
-            //Interpolation of velocity
-            rBoundaryNode.FastGetSolutionStepValue(VELOCITY_X, 0) += rGeometry[i].GetDof(VELOCITY_X).GetSolutionStepValue(0) * rShapeFuncWeights[i];
-            rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Y, 0) += rGeometry[i].GetDof(VELOCITY_Y).GetSolutionStepValue(0) * rShapeFuncWeights[i];
-            //Interpolation of pressure
-            rBoundaryNode.FastGetSolutionStepValue(PRESSURE, 0) += rGeometry[i].GetDof(PRESSURE).GetSolutionStepValue(0) * rShapeFuncWeights[i];
-
-            //Define master slave relation for velocity X and Y
-            AddMasterSlaveRelationVelocity(rMsContainer, r_clone_constraint, ConstraintIdVector[StartId++], rGeometry[i], VELOCITY_X, rBoundaryNode, VELOCITY_X, rShapeFuncWeights[i]);
-            AddMasterSlaveRelationVelocity(rMsContainer, r_clone_constraint, ConstraintIdVector[StartId++], rGeometry[i], VELOCITY_Y, rBoundaryNode, VELOCITY_Y, rShapeFuncWeights[i]);
-            if (TDim == 3)
-            {
-                //Interpolation of velocity Z
-                rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Z, 0) += rGeometry[i].GetDof(VELOCITY_Z).GetSolutionStepValue(0) * rShapeFuncWeights[i];
-                AddMasterSlaveRelationVelocity(rMsContainer, r_clone_constraint, ConstraintIdVector[StartId++], rGeometry[i], VELOCITY_Z, rBoundaryNode, VELOCITY_Z, rShapeFuncWeights[i]);
-            }
-            //Defining master slave relation for pressure
-            AddMasterSlaveRelationPressure(rMsContainer, r_clone_constraint, ConstraintIdVector[StartId++], rGeometry[i], PRESSURE, rBoundaryNode, PRESSURE, rShapeFuncWeights[i]);
-        } // end of loop over host element nodes
-
-        // Setting the buffer 1 same buffer 0
-        rBoundaryNode.FastGetSolutionStepValue(VELOCITY_X, 1) = rBoundaryNode.FastGetSolutionStepValue(VELOCITY_X, 0);
-        rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Y, 1) = rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Y, 0);
-        if (TDim == 3)
-            rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Z, 1) = rBoundaryNode.FastGetSolutionStepValue(VELOCITY_Z, 0);
-        rBoundaryNode.FastGetSolutionStepValue(PRESSURE, 1) = rBoundaryNode.FastGetSolutionStepValue(PRESSURE, 0);
-    }
+    void ApplyContinuityWithMpcs(ModelPart &rBoundaryModelPart, PointLocatorPointerType &pBinLocator) override;
 
     ///@}
     ///@name Protected  Access
@@ -180,78 +140,6 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
-
-    /**
-     * @brief Applies the master-slave constraint between the given master and slave nodes with corresponding variable.
-     *          In addition it sets the FS_CHIMERA_VEL_CONSTRAINT flag on the constraint which is used in the FS-Strategy for chimera
-     * @param rMasterSlaveContainer The container to which the constraint to be added (useful to so OpenMP loop)
-     * @param rCloneConstraint The prototype of constraint which is to be added.
-     * @param ConstraintId The ID of the constraint to be added.
-     * @param rMasterNode The Master node of the constraint.
-     * @param rMasterVariable The variable for the master node.
-     * @param rSlaveNode The Slave node of the constraint.
-     * @param rSlaveVariable The variable for the slave node.
-     * @param Weight The weight of the Master node.
-     * @param Constant The constant of the master slave relation.
-     */
-    template <typename TVariableType>
-    inline void AddMasterSlaveRelationVelocity(typename BaseType::MasterSlaveConstraintContainerType &rMasterSlaveContainer,
-                                       const LinearMasterSlaveConstraint &rCloneConstraint,
-                                       unsigned int ConstraintId,
-                                       Node<3> &rMasterNode,
-                                       TVariableType &rMasterVariable,
-                                       Node<3> &rSlaveNode,
-                                       TVariableType &rSlaveVariable,
-                                       const double Weight,
-                                       const double Constant = 0.0)
-    {
-        rSlaveNode.Set(SLAVE);
-        ModelPart::MasterSlaveConstraintType::Pointer p_new_constraint = rCloneConstraint.Create(ConstraintId, rMasterNode, rMasterVariable, rSlaveNode, rSlaveVariable, Weight, Constant);
-        p_new_constraint->Set(TO_ERASE);
-        BaseType::mNodeIdToConstraintIdsMap[rSlaveNode.Id()].push_back(ConstraintId);
-        rMasterSlaveContainer.insert(rMasterSlaveContainer.begin(), p_new_constraint);
-
-        // TODO: Set the FS_CHIMERA_VEL_CONSTRAINT variable to true
-        p_new_constraint->Set(FS_CHIMERA_PRE_CONSTRAINT, false);
-        p_new_constraint->Set(FS_CHIMERA_VEL_CONSTRAINT, true);
-        p_new_constraint->Set(ACTIVE);
-    }
-
-    /**
-     * @brief Applies the master-slave constraint between the given master and slave nodes with corresponding variable.
-     *          In addition it sets the FS_CHIMERA_PRE_CONSTRAINT flag on the constraint which is used in the FS-Strategy for chimera
-     * @param rMasterSlaveContainer The container to which the constraint to be added (useful to so OpenMP loop)
-     * @param rCloneConstraint The prototype of constraint which is to be added.
-     * @param ConstraintId The ID of the constraint to be added.
-     * @param rMasterNode The Master node of the constraint.
-     * @param rMasterVariable The variable for the master node.
-     * @param rSlaveNode The Slave node of the constraint.
-     * @param rSlaveVariable The variable for the slave node.
-     * @param Weight The weight of the Master node.
-     * @param Constant The constant of the master slave relation.
-     */
-    template <typename TVariableType>
-    inline void AddMasterSlaveRelationPressure(typename BaseType::MasterSlaveConstraintContainerType &rMasterSlaveContainer,
-                                       const LinearMasterSlaveConstraint &rCloneConstraint,
-                                       unsigned int ConstraintId,
-                                       Node<3> &rMasterNode,
-                                       TVariableType &rMasterVariable,
-                                       Node<3> &rSlaveNode,
-                                       TVariableType &rSlaveVariable,
-                                       const double Weight,
-                                       const double Constant = 0.0)
-    {
-        rSlaveNode.Set(SLAVE);
-        ModelPart::MasterSlaveConstraintType::Pointer p_new_constraint = rCloneConstraint.Create(ConstraintId, rMasterNode, rMasterVariable, rSlaveNode, rSlaveVariable, Weight, Constant);
-        p_new_constraint->Set(TO_ERASE);
-        BaseType::mNodeIdToConstraintIdsMap[rSlaveNode.Id()].push_back(ConstraintId);
-        rMasterSlaveContainer.insert(rMasterSlaveContainer.begin(), p_new_constraint);
-
-        // TODO: Set the FS_CHIMERA_PRE_CONSTRAINT variable to true
-        p_new_constraint->Set(FS_CHIMERA_PRE_CONSTRAINT, true);
-        p_new_constraint->Set(FS_CHIMERA_VEL_CONSTRAINT, false);
-        p_new_constraint->Set(ACTIVE);
-    }
 
     ///@}
     ///@name Private  Access
