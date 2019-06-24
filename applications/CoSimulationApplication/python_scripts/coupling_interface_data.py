b2024bbfd92ec22933431c1674296a2ff106d52d
philbucher <philipp.bucher@tum.de>
2019-06-24 16:23:46 +0200

    CouplingData: Delete old methods

--- applications/CoSimulationApplication/python_scripts/coupling_interface_data.py
+++ applications/CoSimulationApplication/python_scripts/coupling_interface_data.py
@@ -210,39 +210,6 @@ class CouplingInterfaceData(object):
                 raise Exception("accessing data from previous steps is only possible with historical nodal data!")
 
 
-
-
-
-
-
-
-
-    def GetPythonList(self, solution_step_index=0):
-        model_part = self.GetModelPart()
-        data = [0]*len(model_part.Nodes)*self.dimension
-        node_index = 0
-        for node in model_part.Nodes:
-            data_value = node.GetSolutionStepValue(self.variable,solution_step_index) #TODO what if non-historical?
-            for i in range(self.dimension):
-                data[node_index*self.dimension + i] = data_value[i]
-            node_index+=1
-        return data
-
-    def GetNumpyArray(self, solution_step_index=0):
-        return np.asarray(self.GetPythonList(solution_step_index), dtype=np.float64)
-
-    def ApplyUpdateToData(self, update):
-        model_part = self.GetModelPart()
-        node_index = 0
-        updated_value = [0]*3 # TODO this is a hack, find better solution! => check var_type
-        for node in model_part.Nodes: # #TODO: local nodes to also work in MPI?
-            # TODO: aditya the data might also be non-historical => GetValue
-            for i in range(self.dimension):
-                updated_value[i] = update[node_index*self.dimension + i]
-
-            node.SetSolutionStepValue(self.variable, 0, updated_value)
-            node_index += 1
-
 def GetValue(entity, variable):
     return entity.GetValue(variable)
 
