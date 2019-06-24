de228b119a35e935772b7f7f6cc8a21fbd3db8c8
Inigo Lopez <inigolcanalejo@gmail.com>
2019-06-24 11:49:11 +0200

    creating and adding cpp test

--- applications/CompressiblePotentialFlowApplication/tests/cpp_tests/test_processes.cpp
+++ applications/CompressiblePotentialFlowApplication/tests/cpp_tests/test_processes.cpp
@@ -15,6 +15,7 @@
 #include "containers/model.h"
 #include "testing/testing.h"
 #include "custom_processes/move_model_part_process.h"
+#include "custom_processes/define_2d_wake_process.h"
 
 namespace Kratos {
   namespace Testing {
@@ -54,6 +55,46 @@ namespace Kratos {
           KRATOS_CHECK_NEAR(model_part.GetNode(i_node+1).Coordinates()[i_dim], reference(i_node,i_dim), 1e-6);
         }
       }
+    }
+
+    KRATOS_TEST_CASE_IN_SUITE(Define2DWakeProcessProcess, CompressiblePotentialApplicationFastSuite)
+    {
+      // Create model_part
+      Model this_model;
+      ModelPart& model_part = this_model.CreateModelPart("Main", 3);
+      model_part.AddNodalSolutionStepVariable(DISTANCE);
+
+      // Set model_part properties
+      BoundedVector<double, 3> free_stream_velocity = ZeroVector(3);
+      free_stream_velocity(0) = 10.0;
+      model_part.GetProcessInfo()[FREE_STREAM_VELOCITY] = free_stream_velocity;
+
+      // Create nodes
+      model_part.CreateNewNode(1, 2.0, 0.0, 0.0);
+      model_part.CreateNewNode(2, 2.0, 2.0, 0.0);
+      ModelPart::NodeType::Pointer pNode = model_part.CreateNewNode(3, 0.0, 1.0, 0.0);
+
+      // Create element
+      model_part.CreateNewProperties(0);
+      Properties::Pointer pElemProp = model_part.pGetProperties(0);
+      std::vector<ModelPart::IndexType> elemNodes{ 1, 2, 3 };
+      ModelPart::ElementType::Pointer pElement = model_part.CreateNewElement("IncompressiblePotentialFlowElement2D3N", 1, elemNodes, pElemProp);
+
+      // Create body sub_model_part
+      ModelPart& body_model_part = model_part.CreateSubModelPart("body_model_part");
+      body_model_part.AddNode(pNode);
+
+      // Set Tolerance
+      const double tolerance = 1e-9;
+
+      // Construct the Define2DWakeProcess
+      Define2DWakeProcess Define2DWakeProcess(body_model_part, tolerance);
+
+      // Execute the Define2DWakeProcess
+      Define2DWakeProcess.ExecuteInitialize();
+
+      const int wake = pElement->GetValue(WAKE);
+      KRATOS_CHECK_NEAR(wake, 1, 1e-6);
     }
   } // namespace Testing
 }  // namespace Kratos.
