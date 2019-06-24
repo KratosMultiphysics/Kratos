b726ea477aee0a019006696064e65e1f5ea7b590
philbucher <philipp.bucher@tum.de>
2019-06-24 18:29:53 +0200

    adding comment

--- applications/CoSimulationApplication/custom_data_structure/configure_py_co_sim.sh
+++ applications/CoSimulationApplication/custom_data_structure/configure_py_co_sim.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # this file is supposed to be called from "cmake_build"
+# it configures and installs the python-only version of the CoSimulationApplication
 
 rm -f CMakeCache.txt
 rm -f *.cmake
 rm -rf CMakeFiles\
 
-cmake ../applications/CoSimulationApplication                                                \
+cmake ../applications/CoSimulationApplication  \
 -DCO_SIMULATION_APPLICATION_PYTHON=ON
 
 make install
