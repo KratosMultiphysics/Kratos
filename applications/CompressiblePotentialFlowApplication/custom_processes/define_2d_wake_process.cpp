6ebfdc35002654f30d0bf348770d6449ee3eccae
Inigo Lopez <inigolcanalejo@gmail.com>
2019-06-24 14:04:01 +0200

    making nodal_distances_to_wake const

--- applications/CompressiblePotentialFlowApplication/custom_processes/define_2d_wake_process.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_processes/define_2d_wake_process.cpp
@@ -293,7 +293,7 @@ const bool Define2DWakeProcess::CheckIfTrailingEdgeElementIsCutByWake(const Elem
     unsigned int number_of_nodes_with_negative_distance = 0;
     // REMINDER: In 3D the elemental_distances may not be match with the nodal
     // distances if CalculateDistanceToSkinProcess is used.
-    auto nodal_distances_to_wake = rElement->GetValue(ELEMENTAL_DISTANCES);
+    const auto nodal_distances_to_wake = rElement->GetValue(ELEMENTAL_DISTANCES);
 
     // Count how many element nodes are above and below the wake
     for (unsigned int i = 0; i < nodal_distances_to_wake.size(); i++) {
