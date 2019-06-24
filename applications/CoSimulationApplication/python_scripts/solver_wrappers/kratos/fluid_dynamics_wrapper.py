c3dbb7dc47d527c99112be3a46c4905d069b13c7
philbucher <philipp.bucher@tum.de>
2019-06-24 16:32:34 +0200

    adding check for apps-availability

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/fluid_dynamics_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/fluid_dynamics_wrapper.py
@@ -1,9 +1,14 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
+# Importing the Kratos Library
+from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
+
 # Importing the base class
 from . import kratos_base_wrapper
 
 # Importing FluidDynamics
+if not CheckIfApplicationsAvailable("FluidDynamicsApplication"):
+    raise ImportError("The FluidDynamicsApplication is not available!")
 from KratosMultiphysics.FluidDynamicsApplication.fluid_dynamics_analysis import FluidDynamicsAnalysis
 
 def Create(settings, solver_name):
--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/structural_mechanics_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/structural_mechanics_wrapper.py
@@ -1,9 +1,14 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
+# Importing the Kratos Library
+from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
+
 # Importing the base class
 from . import kratos_base_wrapper
 
 # Importing StructuralMechanics
+if not CheckIfApplicationsAvailable("StructuralMechanicsApplication"):
+    raise ImportError("The StructuralMechanicsApplication is not available!")
 from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_analysis import StructuralMechanicsAnalysis
 
 def Create(settings, solver_name):
