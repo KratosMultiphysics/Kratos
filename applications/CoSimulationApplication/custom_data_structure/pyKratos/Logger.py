7f8aea3c23b842c1765bdf9b6a86eaae69787820
philbucher <philipp.bucher@tum.de>
2019-06-26 14:30:34 +0200

    minor formatting

--- applications/CoSimulationApplication/custom_data_structure/pyKratos/Logger.py
+++ applications/CoSimulationApplication/custom_data_structure/pyKratos/Logger.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
 
 class Logger(object):
+
     @staticmethod
     def PrintInfo(label, *args):
         print(label, " ".join(map(str,args)))
