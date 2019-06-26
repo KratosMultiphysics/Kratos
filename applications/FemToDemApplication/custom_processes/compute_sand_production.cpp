4cde7b5c04ceda28f5866602aebbd7f8252ca4d6
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-06-26 20:53:06 +0200

    ADDING PROCESS

---
+++ applications/FemToDemApplication/custom_processes/compute_sand_production.cpp
@@ -0,0 +1,38 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics FemDem Application
+//
+//  License:		 BSD License
+//					 Kratos default license:
+//kratos/license.txt
+//
+//  Main authors:    Alejandro Cornejo Velazquez
+//
+
+#include "custom_processes/compute_sand_production.h"
+#include "includes/define.h"
+#include "includes/kratos_flags.h"
+
+namespace Kratos {
+
+
+ComputeSandProduction::ComputeSandProduction(
+    ModelPart& rModelPart)
+    : mrModelPart(rModelPart) 
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void ComputeSandProduction::Execute() 
+{
+
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+}  // namespace Kratos
