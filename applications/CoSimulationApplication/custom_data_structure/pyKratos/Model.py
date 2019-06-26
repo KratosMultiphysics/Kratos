b111ac37e0261996d062f620b26bfa164e4834a1
philbucher <philipp.bucher@tum.de>
2019-06-26 14:30:25 +0200

    updating pyKratos Model

--- applications/CoSimulationApplication/custom_data_structure/pyKratos/Model.py
+++ applications/CoSimulationApplication/custom_data_structure/pyKratos/Model.py
@@ -5,13 +5,14 @@ from .ModelPart import ModelPart
 
 class Model(dict):
     def GetModelPart(self, name):
+        # TODO this should get the ModelParts through splitting the names, same as in Kratos
         return self.__dict__[name]
 
-    def CreateModelPart(self, name):
+    def CreateModelPart(self, name="default", buffer_size=1):
         if(name in self.__dict__.keys() ):
             RuntimeError("The modelpart with name ", name, " already exist !")
 
-        self.__dict__[name] = ModelPart(name)
+        self.__dict__[name] = ModelPart(name, buffer_size, True)
         return self.__dict__[name]
 
     def HasModelPart(self, name):
