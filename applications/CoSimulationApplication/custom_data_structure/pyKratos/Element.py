b6d346963da99d362b74492b551bfc323dee1101
philbucher <philipp.bucher@tum.de>
2019-06-26 11:48:17 +0200

    major Cleanup pyKratos Element

--- applications/CoSimulationApplication/custom_data_structure/pyKratos/Element.py
+++ applications/CoSimulationApplication/custom_data_structure/pyKratos/Element.py
@@ -1,58 +1,41 @@
-from __future__ import print_function, absolute_import, division
-class Element:
+from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
 
-    def __init__(self, Id, nodes_vector):
-        self.variables = []
-        self.Id = Id
-        self.nodes = nodes_vector
+class Element(object):
 
-    def __getitem__(self, key):
-        return self.nodes[key]
+    def __init__(self, elem_id, nodes):
+        self.Id = elem_id
+        self._nodes = nodes
+        self._variables = {}
 
-    def GetNumberOfNodes(self):
-        return len(self.nodes)
-
-    def AddVariable(self, variable_name):
-        for i in range(0, len(self.variables)):
-            self.variables[i][variable_name] = 0
-
-    def SetValue(self):
-        pass # For non historical values
-
-    def GetValue(self):
-        pass
-
-    def Has(self):
-        pass
-
-    def HasSolutionStepValue(self):
-        pass
-
-    def GetSolutionStepValue(self, variable_name, step):
-        return self.variables[step][variable_name]
-
-    def SetSolutionStepValue(self, variable_name, step, value):
-        if variable_name in list(self.variables[step].keys()):
-            self.variables[step][variable_name] = value
-        else:
-            raise Exception(
-                "trying to set an non-existing variable with name ",
-                variable_name,
-                " on node ",
-                self.Id)
-
-    def GetArea(self):
-        pass
+    def GetNode(self, node_index):
+        return self._nodes[node_index]
 
     def GetNodes(self):
-        pass
+        return self._nodes
 
-    def GetNormal(self):
-        pass
+    def SetValue(self, variable, value):
+        # overwrite existing value or add new one
+        self._variables[variable] = value
+
+    def GetValue(self, variable):
+        if not variable in self._variables:
+            # allocate this variable if it does not yet exist
+            # this matches the Kratos behavior
+            if isinstance(variable, list):
+                zero_val = [0.0, 0.0, 0.0]
+            else:
+                zero_val = 0.0
+
+            self._variables[variable] = zero_val
+
+        return self._variables[variable]
+
+    def Has(self, variable):
+        return variable in self._variables
 
     def Initialize(self):
         pass
 
     def __str__(self):
-        return  "Element #{0} with {1}".format(self.Id, self.variables)
+        return  "Element #{} with {}".format(self.Id, self._variables)
 
