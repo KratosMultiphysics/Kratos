227b96daced110fadcbb499963ac9e6d502e228e
philbucher <philipp.bucher@tum.de>
2019-07-01 18:50:53 +0200

    adding comments

--- applications/CoSimulationApplication/python_scripts/function_callback_utility.py
+++ applications/CoSimulationApplication/python_scripts/function_callback_utility.py
@@ -1,7 +1,11 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# executing a function specified as a string, similar to "kratos/utilities/python_function_callback_utility.h"
+
 from math import *
 
 # list of safe methods that can be used
-safe_list = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos',
+safe_methods_list = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos',
              'cosh', 'degrees', 'e', 'exp', 'fabs', 'floor',
              'fmod', 'frexp', 'hypot', 'ldexp', 'log', 'log10',
              'modf', 'pi', 'pow', 'radians', 'sin', 'sinh', 'sqrt',
@@ -9,7 +13,7 @@ safe_list = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos',
 
 
 safe_dict = {}
-for k in safe_list:
+for k in safe_methods_list:
     safe_dict[k] = locals()[k]
 
 def GenericCallFunction(func_string, scope_vars):
