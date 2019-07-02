603302083b5487360607869fd7bfe6e1818e6416
Ferran Arrufat <farrufat@cimne.upc.edu>
2019-07-02 16:03:38 +0200

    Update explicit_solver_continuum.cpp

--- applications/DEMApplication/custom_strategies/strategies/explicit_solver_continuum.cpp
+++ applications/DEMApplication/custom_strategies/strategies/explicit_solver_continuum.cpp
@@ -149,7 +149,6 @@ namespace Kratos {
         VariablesList r_modelpart_nodal_variables_list = r_model_part.GetNodalSolutionStepVariablesList();
         if (r_modelpart_nodal_variables_list.Has(PARTITION_INDEX)) has_mpi = true;
 
-        //InitializeSolutionStep();
         SearchDEMOperations(r_model_part, has_mpi);
         SearchFEMOperations(r_model_part, has_mpi);
         ForceOperations(r_model_part);
