79eec26a5dd6b4e17fd48ec9506c7797e7999d2f
philbucher <philipp.bucher@tum.de>
2019-07-03 14:00:09 +0200

    adding namespace

--- applications/CoSimulationApplication/custom_python/add_custom_io_to_python.cpp
+++ applications/CoSimulationApplication/custom_python/add_custom_io_to_python.cpp
@@ -29,22 +29,22 @@ void  AddCustomIOToPython(pybind11::module& m)
 
     auto mEMPIREAPI = m.def_submodule("EMPIRE_API");
 
-    mEMPIREAPI.def("EMPIRE_API_Connect", EMPIRE_API_Connect);
-    mEMPIREAPI.def("EMPIRE_API_Disconnect", EMPIRE_API_Disconnect);
+    mEMPIREAPI.def("EMPIRE_API_Connect", CoSimEMPIRE_API::EMPIRE_API_Connect);
+    mEMPIREAPI.def("EMPIRE_API_Disconnect", CoSimEMPIRE_API::EMPIRE_API_Disconnect);
 
-    mEMPIREAPI.def("EMPIRE_API_getUserDefinedText", EMPIRE_API_getUserDefinedText);
+    mEMPIREAPI.def("EMPIRE_API_getUserDefinedText", CoSimEMPIRE_API::EMPIRE_API_getUserDefinedText);
 
-    mEMPIREAPI.def("EMPIRE_API_sendMesh", EMPIRE_API_sendMesh);
-    // mEMPIREAPI.def("EMPIRE_API_recvMesh", EMPIRE_API_recvMesh); // TODO check how to handle double**
+    mEMPIREAPI.def("EMPIRE_API_sendMesh", CoSimEMPIRE_API::EMPIRE_API_sendMesh);
+    // mEMPIREAPI.def("EMPIRE_API_recvMesh", CoSimEMPIRE_API::EMPIRE_API_recvMesh); // TODO check how to handle double**
 
-    mEMPIREAPI.def("EMPIRE_API_sendDataField", EMPIRE_API_sendDataField);
-    mEMPIREAPI.def("EMPIRE_API_recvDataField", EMPIRE_API_recvDataField);
+    mEMPIREAPI.def("EMPIRE_API_sendDataField", CoSimEMPIRE_API::EMPIRE_API_sendDataField);
+    mEMPIREAPI.def("EMPIRE_API_recvDataField", CoSimEMPIRE_API::EMPIRE_API_recvDataField);
 
-    mEMPIREAPI.def("EMPIRE_API_sendSignal_double", EMPIRE_API_sendSignal_double);
-    mEMPIREAPI.def("EMPIRE_API_recvSignal_double", EMPIRE_API_recvSignal_double);
+    mEMPIREAPI.def("EMPIRE_API_sendSignal_double", CoSimEMPIRE_API::EMPIRE_API_sendSignal_double);
+    mEMPIREAPI.def("EMPIRE_API_recvSignal_double", CoSimEMPIRE_API::EMPIRE_API_recvSignal_double);
 
-    mEMPIREAPI.def("EMPIRE_API_recvConvergenceSignal", EMPIRE_API_recvConvergenceSignal);
-    mEMPIREAPI.def("EMPIRE_API_sendConvergenceSignal", EMPIRE_API_sendConvergenceSignal);
+    mEMPIREAPI.def("EMPIRE_API_recvConvergenceSignal", CoSimEMPIRE_API::EMPIRE_API_recvConvergenceSignal);
+    mEMPIREAPI.def("EMPIRE_API_sendConvergenceSignal", CoSimEMPIRE_API::EMPIRE_API_sendConvergenceSignal);
 }
 
 }  // namespace Python.
