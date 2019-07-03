5a5936441d1057ce33cb59d6f4cc8d2b97af3829
Carlos Roig <roigcarlo@gmail.com>
2019-07-03 10:23:43 +0200

    Remove comments

--- cmake_modules/install_function.cmake
+++ cmake_modules/install_function.cmake
@@ -1,10 +1,9 @@
-#this functin installs a symbolic link
+# This functin installs a symbolic link
 macro(install_symlink filepath sympath)
     install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${filepath} ${sympath})")
-    ##install(CODE "message(\"-- Created symlink: ${sympath} -> ${filepath}\")")
 endmacro(install_symlink)
 
-#this function install a symlinc to an entire directory
+# This function install a symlinc to an entire directory
 macro(kratos_python_install_directory INSTALL_PYTHON_USING_LINKS origin_dir destination_dir)
     install(DIRECTORY DESTINATION ${CMAKE_INSTALL_PREFIX}/${destination_dir})
     file(GLOB SRC_FILES ${origin_dir}/*.py)
