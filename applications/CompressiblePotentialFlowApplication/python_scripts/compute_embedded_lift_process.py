327af53277d7dc72bc13f07479dced5c58cf096f
marcnunezc <marcnc94@gmail.com>
2019-07-03 07:22:44 +0200

    adding reference area

--- applications/CompressiblePotentialFlowApplication/python_scripts/compute_embedded_lift_process.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/compute_embedded_lift_process.py
@@ -14,17 +14,18 @@ class ComputeEmbeddedLiftProcess(KratosMultiphysics.Process):
             {
 
                 "model_part_name" : "please specify the main model part",
-                "reference_area": 1
+                "reference_area": 1.0
             }  """)
 
         settings.ValidateAndAssignDefaults(default_parameters)
         self.main_model_part=Model.GetModelPart(settings["model_part_name"].GetString()).GetRootModelPart()
         self.result_force=KratosMultiphysics.Vector(3)
+        self.reference_area =  settings["reference_area"].GetDouble()
 
     def ExecuteFinalizeSolutionStep(self):
         CPFApp.ComputeEmbeddedLiftProcess(self.main_model_part,self.result_force).Execute()
-        self.lift_coefficient = self.result_force[1]
-        self.drag_coefficient = self.result_force[0]
+        self.lift_coefficient = self.result_force[1]/self.reference_area
+        self.drag_coefficient = self.result_force[0]/self.reference_area
 
         KratosMultiphysics.Logger.PrintInfo('ComputeEmbeddedLiftProcess',' Cl = ', self.lift_coefficient)
         KratosMultiphysics.Logger.PrintInfo('ComputeEmbeddedLiftProcess',' Cd = ', self.drag_coefficient)
