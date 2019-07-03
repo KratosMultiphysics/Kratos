c735f8afaf2b2e75ee72818c36c94769fd74a83b
philbucher <philipp.bucher@tum.de>
2019-07-03 12:06:13 +0200

    test for EMPIRE_API_sendConvergenceSignal

--- applications/CoSimulationApplication/tests/test_cosim_EMPIRE_API.py
+++ applications/CoSimulationApplication/tests/test_cosim_EMPIRE_API.py
@@ -5,12 +5,32 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 import KratosMultiphysics.CoSimulationApplication as KratosCoSim
 
+import os
+
 class TestCoSim_EMPIRE_API(KratosUnittest.TestCase):
 
     def test_unused_fcts(self):
         KratosCoSim.EMPIRE_API.EMPIRE_API_Connect("dummy.xml")
         KratosCoSim.EMPIRE_API.EMPIRE_API_Disconnect()
         KratosCoSim.EMPIRE_API.EMPIRE_API_getUserDefinedText("dummy_element")
+
+    def test_EMPIRE_API_sendConvergenceSignal(self):
+        with self.assertRaisesRegex(RuntimeError, "Input can only be 0 non-convergence or 1 convergence, called with: 2"):
+            KratosCoSim.EMPIRE_API.EMPIRE_API_sendConvergenceSignal(2)
+
+        signal = 0 # means convergence
+        conv_signal_file_name = "EMPIRE_convergence_signal.dat" # this is hardcoded in C++
+
+        KratosCoSim.EMPIRE_API.EMPIRE_API_sendConvergenceSignal(signal)
+
+        self.assertTrue(os.path.isfile(conv_signal_file_name))
+
+        with open(conv_signal_file_name, 'r') as conv_signal_file:
+            content = conv_signal_file.read()
+            self.assertEqual(content, str(signal))
+
+        os.remove(conv_signal_file_name)
+
 
 if __name__ == '__main__':
     KratosUnittest.main()
