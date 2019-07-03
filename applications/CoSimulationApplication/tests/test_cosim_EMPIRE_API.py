2c775c28172208d570863194660329205c7aac07
philbucher <philipp.bucher@tum.de>
2019-07-03 11:02:53 +0200

    adding test-template for api

---
+++ applications/CoSimulationApplication/tests/test_cosim_EMPIRE_API.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+
+import KratosMultiphysics as KM
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+import KratosMultiphysics.CoSimulationApplication as KratosCoSim
+
+class TestCoSim_EMPIRE_API(KratosUnittest.TestCase):
+
+    # def setUp(self):
+
+    def test_dummy(self):
+        pass
+
+if __name__ == '__main__':
+    KratosUnittest.main()
