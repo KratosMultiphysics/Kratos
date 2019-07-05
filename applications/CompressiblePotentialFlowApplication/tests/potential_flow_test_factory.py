eb657149ea32186ebd82912852e3d2f0d87ad5fd
marcnunezc <marcnc94@gmail.com>
2019-07-05 15:33:56 +0200

    updating test settings

--- applications/CompressiblePotentialFlowApplication/tests/potential_flow_test_factory.py
+++ applications/CompressiblePotentialFlowApplication/tests/potential_flow_test_factory.py
@@ -141,7 +141,7 @@ class PotentialFlowTests(UnitTest.TestCase):
                                     "skin_output"         : false,
                                     "plane_output"        : [],
                                     "nodal_results"       : ["VELOCITY_POTENTIAL","AUXILIARY_VELOCITY_POTENTIAL"],
-                                    "nodal_nonhistorical_results": ["TRAILING_EDGE","DISTANCE"],
+                                    "nodal_nonhistorical_results": ["TRAILING_EDGE","wAKE_DISTANCE"],
                                     "elemental_conditional_flags_results": ["STRUCTURE"],
                                     "gauss_point_results" : ["PRESSURE_COEFFICIENT","VELOCITY","VELOCITY_LOWER","PRESSURE_LOWER","WAKE","WAKE_ELEMENTAL_DISTANCES","KUTTA"]
                                 },
