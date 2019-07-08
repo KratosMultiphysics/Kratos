8052294ddbdcc2e5bf8ecc637bbad314f0dd03e1
Carlos Roig <roigcarlo@users.noreply.github.com>
2019-07-08 10:56:07 +0200

    Update applications/FluidDynamicsApplication/python_scripts/stokes_solver.py
    Co-Authored-By: Philipp Bucher <philipp.bucher@tum.de>

--- applications/FluidDynamicsApplication/python_scripts/stokes_solver.py
+++ applications/FluidDynamicsApplication/python_scripts/stokes_solver.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 # importing the Kratos Library
 import KratosMultiphysics as kratoscore
-# import KratosMultiphysics.IncompressibleFluidApplication as incompressibleapp
 import KratosMultiphysics.FluidDynamicsApplication as cfd
 import KratosMultiphysics.python_linear_solver_factory as linear_solver_factory
 
