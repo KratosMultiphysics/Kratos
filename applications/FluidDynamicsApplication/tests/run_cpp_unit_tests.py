832dbd0fc8b3a0b663a5a8a3f824f1f5c6b37d89
marcnunezc <marcnc94@gmail.com>
2019-07-09 11:28:08 +0200

    removing file

--- applications/FluidDynamicsApplication/tests/run_cpp_unit_tests.py
+++
@@ -1,9 +0,0 @@
-from KratosMultiphysics import *
-from KratosMultiphysics.FluidDynamicsApplication import *
-
-def run():
-    Tester.SetVerbosity(Tester.Verbosity.PROGRESS) # TESTS_OUTPUTS
-    Tester.RunTestSuite("FluidDynamicsApplicationFastSuite")
-
-if __name__ == '__main__':
-    run()
