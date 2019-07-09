038d46b613f30cb0d9775a298af7ad657df4752f
Nanda <bodhinandach@gmail.com>
2019-07-09 10:24:31 +0200

    update particle wrapper

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/particle_mechanics_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/particle_mechanics_wrapper.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
 # Importing the Kratos Library
+import KratosMultiphysics as KM
 from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
 
 # Importing the base class
@@ -9,7 +10,11 @@ from . import kratos_base_wrapper
 # Importing ParticleMechanics
 if not CheckIfApplicationsAvailable("ParticleMechanicsApplication"):
     raise ImportError("The ParticleMechanicsApplication is not available!")
-from KratosMultiphysics.ParticleMechanicsApplication.structural_mechanics_analysis import ParticleMechanicsAnalysis
+import KratosMultiphysics.ParticleMechanicsApplication as KPM
+from KratosMultiphysics.ParticleMechanicsApplication.particle_mechanics_analysis import ParticleMechanicsAnalysis
+
+# Other imports
+import math
 
 def Create(settings, solver_name):
     return ParticleMechanicsWrapper(settings, solver_name)
@@ -19,3 +24,38 @@ class ParticleMechanicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
 
     def _CreateAnalysisStage(self):
         return ParticleMechanicsAnalysis(self.model, self.project_parameters)
+
+    def SolveSolutionStep(self):
+        coupling_model_part = self.model.GetModelPart("MPM_Coupling_Interface")
+        model_part_name = self.project_parameters["coupling_settings"]["interface_model_part_name"].GetString()
+        model_part = self.model.GetModelPart(model_part_name)
+
+        ## Transfer information from coupling_mp to mp
+        for coupling_node in coupling_model_part.Nodes:
+            coupling_id  = coupling_node.Id
+
+            ## IMPOSED DISPLACEMENT
+            total_displacement = coupling_node.GetSolutionStepValue(KM.DISPLACEMENT,0)
+            old_displacement = model_part.GetCondition(coupling_id).GetValue(KPM.MPC_DISPLACEMENT)
+            incremental_displacement = total_displacement - old_displacement
+            model_part.GetCondition(coupling_id).SetValue(KPM.MPC_IMPOSED_DISPLACEMENT,incremental_displacement)
+
+            ## ADD VELOCITY
+            current_velocity = coupling_node.GetSolutionStepValue(KM.VELOCITY,0)
+            model_part.GetCondition(coupling_id).SetValue(KPM.MPC_VELOCITY, current_velocity)
+
+            ## ADD NORMAL
+            normal = coupling_node.GetSolutionStepValue(KM.NORMAL,0)
+            # Check and see whether the normal is not zero
+            norm_normal = math.sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2])
+            if norm_normal > 1.e-10:
+                model_part.GetCondition(coupling_id).SetValue(KPM.MPC_NORMAL, normal)
+
+        super(ParticleMechanicsWrapper, self).SolveSolutionStep()
+
+        ### Get contact force from mp to coupling_mp
+        for mpc in model_part.Conditions:
+            if (mpc.Is(KM.INTERFACE)):
+                coupling_id   = mpc.Id
+                contact_force = mpc.GetValue(KPM.MPC_CONTACT_FORCE)
+                coupling_model_part.GetNode(coupling_id).SetSolutionStepValue(KM.CONTACT_FORCE,0,contact_force)
