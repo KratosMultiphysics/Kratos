73f797994b9f803302e4636d427ed385a569d48d
marcnunezc <mnunez@cimne.upc.edu>
2019-07-10 21:28:53 +0200

    avoiding first private definition and defining const variables

--- applications/CompressiblePotentialFlowApplication/custom_processes/apply_far_field_process.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_processes/apply_far_field_process.cpp
@@ -34,15 +34,13 @@ void ApplyFarFieldProcess::FindFarthestUpstreamBoundaryNode()
     std::size_t num_threads = omp_get_max_threads();
     std::vector<double> min_projections(num_threads, std::numeric_limits<double>::epsilon());
     std::vector<std::size_t> nodes_id_list(num_threads, 0);
-
-    //Find minimum node in the direction of the free stream velocity in openmp.
-    double distance_projection=0.0;
-    #pragma omp parallel for firstprivate (distance_projection)
+    // Find minimum node in the direction of the free stream velocity in openmp.
+    #pragma omp parallel for
     for (int i = 0; i < static_cast<int>(mrModelPart.Nodes().size()); i++) {
         auto it_node = mrModelPart.NodesBegin() + i;
         std::size_t thread_id = omp_get_thread_num();
         const auto& r_coordinates = it_node->Coordinates();
-        distance_projection = inner_prod(r_coordinates, mFreeStreamVelocity);
+        const double distance_projection = inner_prod(r_coordinates, mFreeStreamVelocity);
         if (distance_projection < min_projections[thread_id]){
             min_projections[thread_id] = distance_projection;
             nodes_id_list[thread_id] = it_node->Id();
@@ -63,26 +61,23 @@ void ApplyFarFieldProcess::FindFarthestUpstreamBoundaryNode()
 
 void ApplyFarFieldProcess::AssignFarFieldBoundaryConditions()
 {
-    double velocity_projection = 0.0;
-    array_1d<double,3> normal;
-    array_1d<double,3> aux_coordinates;
-
-    #pragma omp parallel for firstprivate(velocity_projection, normal, aux_coordinates)
+    #pragma omp parallel for
     for (int i = 0; i < static_cast<int>(mrModelPart.Conditions().size()); i++) {
         auto it_cond = mrModelPart.ConditionsBegin() + i;
         auto& r_geometry = it_cond->GetGeometry();
-        // Computing normal
-        r_geometry.PointLocalCoordinates(aux_coordinates, r_geometry.Center());
-        normal = it_cond->GetGeometry().Normal(aux_coordinates);
 
-        velocity_projection = inner_prod(normal, mFreeStreamVelocity);
+        // Computing normal
+        array_1d<double,3> aux_coordinates;
+        r_geometry.PointLocalCoordinates(aux_coordinates, r_geometry.Center());
+        const array_1d<double,3> normal = it_cond->GetGeometry().Normal(aux_coordinates);
+
+        const double velocity_projection = inner_prod(normal, mFreeStreamVelocity);
         if (velocity_projection < 0.0) {
             AssignDirichletFarFieldBoundaryCondition(r_geometry);
         }
         else {
             AssignNeumannFarFieldBoundaryCondition(*it_cond);
         }
-
     }
 }
 
@@ -113,15 +108,13 @@ void ApplyFarFieldProcess::AssignNeumannFarFieldBoundaryCondition(Condition& rCo
 void ApplyFarFieldProcess::InitializeFlowField()
 {
     ModelPart& root_model_part = mrModelPart.GetRootModelPart();
-    double inlet_potential = 0.0;
-    array_1d<double,3> relative_coordinates;
 
-    #pragma omp parallel for firstprivate(inlet_potential,relative_coordinates)
+    #pragma omp parallel for
     for (int i = 0; i < static_cast<int>(root_model_part.Nodes().size()); i++) {
         auto it_node = root_model_part.NodesBegin() + i;
 
-        relative_coordinates = it_node->Coordinates() - mpReferenceNode->Coordinates();
-        inlet_potential = inner_prod(relative_coordinates, mFreeStreamVelocity);
+        const array_1d<double,3> relative_coordinates = it_node->Coordinates() - mpReferenceNode->Coordinates();
+        const double inlet_potential = inner_prod(relative_coordinates, mFreeStreamVelocity);
 
         it_node->FastGetSolutionStepValue(VELOCITY_POTENTIAL) = inlet_potential + mReferencePotential;
         it_node->FastGetSolutionStepValue(AUXILIARY_VELOCITY_POTENTIAL) = inlet_potential + mReferencePotential;
