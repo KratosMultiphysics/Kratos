eec280b21af33c71e966a62f8da455881b558f32
AdityaGhantasala <aditya.ghantasala@tum.de>
2019-07-10 09:28:07 +0200

    removing debug output

--- applications/ChimeraApplication/tests/chimera_analysis_base_test.py
+++ applications/ChimeraApplication/tests/chimera_analysis_base_test.py
@@ -18,7 +18,7 @@ class ChimeraAnalysisBaseTest(UnitTest.TestCase):
 
     def setUp(self):
         # Set to true to get post-process files for the test
-        self.print_output = True
+        self.print_output = False
 
     def _run_test(self,settings_file_name):
         model = KratosMultiphysics.Model()
@@ -33,51 +33,7 @@ class ChimeraAnalysisBaseTest(UnitTest.TestCase):
                     "process_name"  : "VtkOutputProcess",
                     "help"          : "This process writes postprocessing files for Paraview",
                     "Parameters"    : {
-                        "model_part_name"                    : "GENERIC_background",
-                        "output_control_type"                : "step",
-                        "output_frequency"                   : 1,
-                        "file_format"                        : "ascii",
-                        "output_precision"                   : 3,
-                        "output_sub_model_parts"             : false,
-                       "write_deformed_configuration"        : true,
-                        "folder_name"                        : "test_vtk_output",
-                        "save_output_files_in_folder"        : true,
-                        "nodal_solution_step_data_variables" : ["VELOCITY","PRESSURE","DISTANCE","MESH_VELOCITY"],
-                        "nodal_data_value_variables"         : [],
-                        "element_flags"                      : ["ACTIVE"],
-                        "nodal_flags"                        : ["VISITED"],
-                        "element_data_value_variables"       : [],
-                        "condition_data_value_variables"     : []
-                    }
-                },{
-                    "python_module" : "vtk_output_process",
-                    "kratos_module" : "KratosMultiphysics",
-                    "process_name"  : "VtkOutputProcess",
-                    "help"          : "This process writes postprocessing files for Paraview",
-                    "Parameters"    : {
-                        "model_part_name"                    : "GENERIC_patch_1_1",
-                        "output_control_type"                : "step",
-                        "output_frequency"                   : 1,
-                        "file_format"                        : "ascii",
-                        "output_precision"                   : 3,
-                        "output_sub_model_parts"             : false,
-                       "write_deformed_configuration"        : true,
-                        "folder_name"                        : "test_vtk_output",
-                        "save_output_files_in_folder"        : true,
-                        "nodal_solution_step_data_variables" : ["VELOCITY","PRESSURE","DISTANCE","MESH_VELOCITY"],
-                        "nodal_data_value_variables"         : [],
-                        "element_flags"                      : ["ACTIVE"],
-                        "nodal_flags"                        : ["VISITED"],
-                        "element_data_value_variables"       : [],
-                        "condition_data_value_variables"     : []
-                    }
-                },{
-                    "python_module" : "vtk_output_process",
-                    "kratos_module" : "KratosMultiphysics",
-                    "process_name"  : "VtkOutputProcess",
-                    "help"          : "This process writes postprocessing files for Paraview",
-                    "Parameters"    : {
-                        "model_part_name"                    : "GENERIC_patch_2_1",
+                        "model_part_name"                    : "FluidModelPart",
                         "output_control_type"                : "step",
                         "output_frequency"                   : 1,
                         "file_format"                        : "ascii",
