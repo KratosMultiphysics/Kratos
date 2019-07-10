3b5cd2439a98fa1543191654472e287c1e9c7d39
marcnunezc <marcnc94@gmail.com>
2019-07-10 14:37:09 +0200

    declaring first private variables

--- applications/CompressiblePotentialFlowApplication/custom_processes/apply_far_field_process.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_processes/apply_far_field_process.cpp
@@ -28,12 +28,14 @@ void ApplyFarFieldProcess::Execute()
     std::vector<double> min_projections(num_threads, std::numeric_limits<double>::epsilon());
     std::vector<std::size_t> nodes_id_list(num_threads, 0);
 
-    #pragma omp parallel for
+    double distance_projection=0.0;
+
+    #pragma omp parallel for firstprivate (distance_projection)
     for (int i = 0; i < static_cast<int>(mrModelPart.Nodes().size()); i++) {
         auto it_node = mrModelPart.NodesBegin() + i;
         std::size_t thread_id = omp_get_thread_num();
         const auto& r_coordinates = it_node->Coordinates();
-        double distance_projection = inner_prod(r_coordinates,mFreeStreamVelocity);
+        distance_projection = inner_prod(r_coordinates,mFreeStreamVelocity);
         if (distance_projection < min_projections[thread_id]){
             min_projections[thread_id] = distance_projection;
             nodes_id_list[thread_id] = it_node->Id();
@@ -49,19 +51,24 @@ void ApplyFarFieldProcess::Execute()
 
     mpReferenceNode = mrModelPart.pGetNode(nodes_id_list[minimum_node_thread_id]);
 
-    #pragma omp parallel for
+    double velocity_projection = 0.0;
+    double inlet_potential = 0.0;
+    array_1d<double,3> normal;
+    array_1d<double,3> aux_coordinates;
+    array_1d<double,3> relative_coordinates;
+
+    #pragma omp parallel for firstprivate(velocity_projection, normal, aux_coordinates, inlet_potential, relative_coordinates)
     for (int i = 0; i < static_cast<int>(mrModelPart.Conditions().size()); i++) {
         auto it_cond = mrModelPart.ConditionsBegin() + i;
         auto& r_geometry = it_cond->GetGeometry();
-        array_1d<double,3> aux_coordinates;
         r_geometry.PointLocalCoordinates(aux_coordinates, r_geometry.Center());
-        array_1d<double,3> normal = it_cond->GetGeometry().Normal(aux_coordinates);
+        normal = it_cond->GetGeometry().Normal(aux_coordinates);
 
-        double velocity_projection = inner_prod(normal, mFreeStreamVelocity);
+        velocity_projection = inner_prod(normal, mFreeStreamVelocity);
         if (velocity_projection < 0.0) {
             for (std::size_t i_node = 0; i_node < r_geometry.size(); i_node++){
-                array_1d<double,3> relative_coordinates = r_geometry[i_node].Coordinates() - mpReferenceNode->Coordinates();
-                double inlet_potential = inner_prod(relative_coordinates, mFreeStreamVelocity);
+                relative_coordinates = r_geometry[i_node].Coordinates() - mpReferenceNode->Coordinates();
+                inlet_potential = inner_prod(relative_coordinates, mFreeStreamVelocity);
                 r_geometry[i_node].Fix(VELOCITY_POTENTIAL);
                 r_geometry[i_node].FastGetSolutionStepValue(VELOCITY_POTENTIAL) = inlet_potential + mReferencePotential;
                 if (r_geometry[i_node].SolutionStepsDataHas(ADJOINT_VELOCITY_POTENTIAL)){
@@ -80,12 +87,15 @@ void ApplyFarFieldProcess::Execute()
 void ApplyFarFieldProcess::InitializeFlowField()
 {
     ModelPart& root_model_part = mrModelPart.GetRootModelPart();
-    #pragma omp parallel for
+    double inlet_potential = 0.0;
+    array_1d<double,3> relative_coordinates;
+
+    #pragma omp parallel for firstprivate(inlet_potential,relative_coordinates)
     for (int i = 0; i < static_cast<int>(root_model_part.Nodes().size()); i++) {
         auto it_node = root_model_part.NodesBegin() + i;
 
-        array_1d<double,3> relative_coordinates = it_node->Coordinates() - mpReferenceNode->Coordinates();
-        double inlet_potential = inner_prod(relative_coordinates, mFreeStreamVelocity);
+        relative_coordinates = it_node->Coordinates() - mpReferenceNode->Coordinates();
+        inlet_potential = inner_prod(relative_coordinates, mFreeStreamVelocity);
 
         it_node->FastGetSolutionStepValue(VELOCITY_POTENTIAL) = inlet_potential + mReferencePotential;
         it_node->FastGetSolutionStepValue(AUXILIARY_VELOCITY_POTENTIAL) = inlet_potential + mReferencePotential;
