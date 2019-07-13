9a07028f011d3e00524a02404120ceae2e612e39
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-07-13 20:19:18 +0200

    Adding TimerProcess

---
+++ kratos/python_scripts/timer_process.py
@@ -0,0 +1,64 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+# Importing the Kratos Library
+import KratosMultiphysics
+
+def Factory(settings, Model):
+    if not isinstance(settings, KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return TimerProcess(Model, settings["Parameters"])
+
+# All the processes python processes should be derived from "Process"
+class TimerProcess(KratosMultiphysics.Process):
+    """This process helps to measure the time consumed on the simulations
+
+    Only the member variables listed below should be accessed directly.
+
+    Public member variables:
+    Model -- the container of the different model parts.
+    settings -- Kratos parameters containing solver settings.
+    """
+
+    def __init__(self, Model, settings ):
+        """ The default constructor of the class
+
+        Keyword arguments:
+        self -- It signifies an instance of a class.
+        Model -- the container of the different model parts.
+        settings -- Kratos parameters containing solver settings.
+        """
+
+        KratosMultiphysics.Process.__init__(self)
+
+        #The value can be a double or a string (function)
+        default_settings = KratosMultiphysics.Parameters("""
+        {
+            "help"            : "This process helps to measure the time consumed on the simulations",
+            "output_filename" : ""
+            "interval_name"   : "Analysis"
+        }
+        """
+        )
+
+        #assign this here since it will change the "interval" prior to validation
+        self.interval = KratosMultiphysics.IntervalUtility(settings)
+
+        settings.ValidateAndAssignDefaults(default_settings)
+
+        self.interval_name = settings["interval_name"].GetString()
+        self.output_filename = settings["output_filename"].GetString()
+
+        # Defining timer
+        self.timer = KratosMultiphysics.Timer()
+        if self.output_filename != "":
+            self.SetOuputFile.Start(self.output_filename)
+        self.timer.Start(self.interval_name)
+
+    def ExecuteFinalize(self):
+        """ This function is designed for being called at the end of the computations
+
+        Keyword arguments:
+        self -- It signifies an instance of a class.
+        """
+        self.timer.Stop(self.interval_name)
+        if self.output_filename != "":
+            self.SetOuputFile.CloseOuputFile()
