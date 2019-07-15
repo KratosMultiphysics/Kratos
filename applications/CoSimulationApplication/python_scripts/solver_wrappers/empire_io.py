8d1b5505ea817cd2a745f1196319355177190cd3
philbucher <philipp.bucher@tum.de>
2019-07-15 23:39:55 +0200

    first version or EmpireIO

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/empire_io.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/empire_io.py
@@ -1,30 +1,80 @@
-from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from __future__ import print_function, absolute_import, division  # makes these scripts backward compatible with python 2.6 and 2.7
+
+# Importing the Kratos Library
+import KratosMultiphysics as KM
 
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_io import CoSimulationIO
 
-def Create(model, custom_settings):
-    return EmpireIO(model, custom_settings)
+# CoSimulation imports
+import KratosMultiphysics.CoSimulationApplication as KratosCoSim
+import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
+import KratosMultiphysics.CoSimulationApplication.colors as colors
+
+def Create(settings, model):
+    return EmpireIO(settings, model)
 
 class EmpireIO(CoSimulationIO):
-    """This class is used if a Solver directly uses Kratos as a data-structure
-    e.g. Kratos itself or simple-solvers written in Python
+    """IO for the legacy EMPIRE_API
     """
+    def __init__(self, settings, model):
+        super(EmpireIO, self).__init__(settings, model)
+        KratosCoSim.EMPIRE_API.EMPIRE_API_Connect(self.settings["api_configuration_file_name"].GetString())
 
-    def ImportCouplingInterfaceData(self, data_object, from_solver=None):
-        pass
+    def ImportCouplingInterface(self, interface_config):
+        model_part_name = interface_config["model_part_name"]
+        comm_name = interface_config["comm_name"]
 
-    def ImportCouplingInterface(self, mesh_config, from_solver=None):
-        pass
+        if not self.model.HasModelPart(model_part_name):
+            main_model_part_name, *sub_model_part_names = model_part_name.split(".")
+            RecursiveCreateModelParts(self.model[main_model_part_name], ".".join(sub_model_part_names))
 
-    def ExportCouplingInterfaceData(self, data_object, to_solver=None):
-        pass
+        model_part = self.model[model_part_name]
+        KratosCoSim.EMPIRE_API.EMPIRE_API_recvMesh(model_part, comm_name)
 
-    def ExportCouplingInterface(self, mesh_config, to_solver=None):
-        pass
+    def ExportCouplingInterface(self, interface_config):
+        model_part_name = interface_config["model_part_name"]
+        comm_name = interface_config["comm_name"]
+        KratosCoSim.EMPIRE_API.EMPIRE_API_sendMesh(self.model[model_part_name], comm_name)
+
+    def ImportCouplingInterfaceData(self, data_config):
+        data_type = data_config["type"]
+        if data_type == "coupling_interface_data":
+            interface_data = data_config["interface_data"]
+            KratosCoSim.EMPIRE_API.EMPIRE_API_recvDataField(interface_data.GetModelPart(), interface_data.name, interface_data.variable)
+        else:
+            raise NotImplementedError('Importing interface data of type "{}" is not implemented for this IO: "{}"'.format(data_type, self._ClassName()))
+
+    def ExportCouplingInterfaceData(self, data_config):
+        data_type = data_config["type"]
+        if data_type == "coupling_interface_data":
+            interface_data = data_config["interface_data"]
+            KratosCoSim.EMPIRE_API.EMPIRE_API_sendDataField(interface_data.GetModelPart(), interface_data.name, interface_data.variable)
+        else:
+            raise NotImplementedError('Exporting interface data of type "{}" is not implemented for this IO: "{}"'.format(data_type, self._ClassName()))
+
 
     def PrintInfo(self):
-        print("This is the dummy-IO")
+        print("This is the EMPIRE-IO")
 
     def Check(self):
         pass
+
+    @classmethod
+    def _GetDefaultSettings(cls):
+        this_defaults = KM.Parameters("""{
+            "api_configuration_file_name" : "UNSPECIFIED",
+            "model_parts_send" : {},
+            "model_parts_recv" : {}
+        }""")
+        this_defaults.AddMissingParameters(super(EmpireIO, cls)._GetDefaultSettings())
+
+        return this_defaults
+
+def RecursiveCreateModelParts(model_part, model_part_name):
+    model_part_name, *sub_model_part_names = model_part_name.split(".")
+    if not model_part.HasSubModelPart(model_part_name):
+        print("creating", model_part_name, "as smp of", model_part.Name)
+        model_part = model_part.CreateSubModelPart(model_part_name)
+    if len(sub_model_part_names) > 0:
+        RecursiveCreateModelParts(model_part, ".".join(sub_model_part_names))
