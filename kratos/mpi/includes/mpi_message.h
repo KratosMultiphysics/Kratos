88fffea64a93ae2fd5740d5ba178fbeef2920843
jcotela <jcotela@cimne.upc.edu>
2019-07-15 10:38:09 +0200

    Avoiding passing term [0] to compute size.

--- kratos/mpi/includes/mpi_message.h
+++ kratos/mpi/includes/mpi_message.h
@@ -33,6 +33,7 @@ template<> struct MPIDataType<int>
     {
         return MPI_INT;
     }
+    static constexpr int LengthPerObject = 1;
 };
 
 template<> struct MPIDataType<unsigned int>
@@ -41,6 +42,7 @@ template<> struct MPIDataType<unsigned int>
     {
         return MPI_UNSIGNED;
     }
+    static constexpr int LengthPerObject = 1;
 };
 
 template<> struct MPIDataType<long unsigned int>
@@ -49,6 +51,7 @@ template<> struct MPIDataType<long unsigned int>
     {
         return MPI_UNSIGNED_LONG;
     }
+    static constexpr int LengthPerObject = 1;
 };
 
 template<> struct MPIDataType<double>
@@ -57,6 +60,7 @@ template<> struct MPIDataType<double>
     {
         return MPI_DOUBLE;
     }
+    static constexpr int LengthPerObject = 1;
 };
 
 template<> struct MPIDataType<std::string>
@@ -65,6 +69,7 @@ template<> struct MPIDataType<std::string>
     {
         return MPI_CHAR;
     }
+    static constexpr int LengthPerObject = 1;
 };
 
 template<> struct MPIDataType<Flags::BlockType>
@@ -73,6 +78,7 @@ template<> struct MPIDataType<Flags::BlockType>
     {
         return MPI_INT64_T;
     }
+    static constexpr int LengthPerObject = 1;
 };
 
 template<class TDataType> class ValueMessage
@@ -109,7 +115,7 @@ public:
 
     static inline int Size(const std::vector<TDataType>& rValue)
     {
-        return rValue.size() * ValueMessage<TDataType>::Size(rValue[0]);
+        return rValue.size() * MPIDataType<TDataType>::LengthPerObject;
     }
 };
 
@@ -136,7 +142,7 @@ public:
 
     static inline int Size(const array_1d<TDataType,Dimension>& rValues)
     {
-        return Dimension * ValueMessage<TDataType>::Size(rValues[0]);
+        return Dimension * MPIDataType<TDataType>::LengthPerObject;
     }
 };
 
