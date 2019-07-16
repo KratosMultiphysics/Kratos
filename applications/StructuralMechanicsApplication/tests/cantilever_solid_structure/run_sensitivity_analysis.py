21499d98c392024efa5884ea67fde90d9ec2a440
arming <armin.geiser@tum.de>
2019-07-16 11:58:33 +0200

    add modification of youngs modulus - atm via hardcoded input

--- applications/StructuralMechanicsApplication/tests/cantilever_solid_structure/run_sensitivity_analysis.py
+++ applications/StructuralMechanicsApplication/tests/cantilever_solid_structure/run_sensitivity_analysis.py
@@ -7,6 +7,7 @@ import KratosMultiphysics
 from KratosMultiphysics.StructuralMechanicsApplication import *
 import structural_response_function_factory
 import structural_mechanics_analysis
+import structural_response
 import KratosMultiphysics.kratos_utilities as kratos_utilities
 
 if kratos_utilities.CheckIfApplicationsAvailable("EigenSolversApplication"):
@@ -30,36 +31,89 @@ def _get_test_working_dir():
     this_file_dir = os.path.dirname(os.path.realpath(__file__))
     return os.path.join(this_file_dir)
 
+def ReadYoungsModulusDict(model_part):
+    youngs_modulus_dict = {}
+
+    test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 29, 30, 31, 32, 39, 40, 41, 42, 49, 50, 51, 52, 59, 60, 61, 62, 69, 70, 71, 72, 79, 80, 81, 82, 89, 90, 91, 92, 99, 100, 101, 102, 109, 110, 111, 112, 119, 120, 121, 122, 129, 130, 131, 132, 139, 140, 141, 142, 149, 150, 151, 152, 159, 160, 161, 162, 169, 170, 171, 172, 179, 180, 181, 182, 189, 190, 191, 192, 199, 200]
+
+    for element in model_part.Elements:
+
+        if element.Id in test_list:
+            youngs_modulus = 10000.0
+        else:
+            youngs_modulus = 0.001
+
+        # youngs_modulus = 4600 # constant youngs modulus for all elements
+
+        youngs_modulus_dict[element.Id] = youngs_modulus
+    return youngs_modulus_dict
+
+def ModifyYoungsModulus(model_part, youngs_modulus_dict):
+
+    original_properties = model_part.GetProperties()
+
+    for element in model_part.Elements:
+        youngs_modulus = youngs_modulus_dict[element.Id]
+
+        old_element_property = element.Properties
+
+        original_property = original_properties[old_element_property.Id]
+
+        new_property = KratosMultiphysics.Properties(old_element_property.Id)
+
+        # TODO use a clone method and only overwrite the youngs modulus
+        new_property.SetValue(KratosMultiphysics.DENSITY, original_property.GetValue(KratosMultiphysics.DENSITY) )
+        new_property.SetValue(KratosMultiphysics.POISSON_RATIO, original_property.GetValue(KratosMultiphysics.POISSON_RATIO) )
+        new_property.SetValue(KratosMultiphysics.CONSTITUTIVE_LAW, original_property.GetValue(KratosMultiphysics.CONSTITUTIVE_LAW) )
+        new_property.SetValue(KratosMultiphysics.YOUNG_MODULUS, youngs_modulus)
+
+        element.Properties = new_property
+
+        # for output
+        element.SetValue(KratosMultiphysics.YOUNG_MODULUS, youngs_modulus)
+
+class SiemensTopologyResponseFunction(structural_response.AdjointResponseFunction):
+
+    def Initialize(self):
+        super().Initialize()
+
+        youngs_modulus_dict = ReadYoungsModulusDict(self.primal_model_part)
+
+        ModifyYoungsModulus(self.primal_model_part, youngs_modulus_dict)
+        ModifyYoungsModulus(self.adjoint_model_part, youngs_modulus_dict)
+
+        print(self.primal_model_part.GetElement(1).Properties)
+
 
 with controlledExecutionScope(_get_test_working_dir()):
 
     # -----------------------------------------------
-    ## computation using python structural response
+    # # computation using python structural response
     # -----------------------------------------------
-    #with open("adjoint_strain_energy_response_parameters.json",'r') as parameter_file:
-    #    parameters = KratosMultiphysics.Parameters( parameter_file.read())
-    #
-    #model = KratosMultiphysics.Model()
-    #response_function = structural_response_function_factory.CreateResponseFunction("strain_energy", parameters["kratos_response_settings"], model)
-    #response_function.RunCalculation(True)
+    with open("adjoint_strain_energy_response_parameters.json",'r') as parameter_file:
+       parameters = KratosMultiphysics.Parameters( parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    response_function = SiemensTopologyResponseFunction("adjoint_linear_strain_energy", parameters["kratos_response_settings"], model)
+    response_function.RunCalculation(True)
 
     # -----------------------------------------------
     ## computation structural mechanics analysis
     # -----------------------------------------------
 
-    # primal analysis
-    with open("ProjectParameters.json",'r') as parameter_file:
-        ProjectParametersPrimal = KratosMultiphysics.Parameters( parameter_file.read())
-    model_primal = KratosMultiphysics.Model()
-    primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersPrimal)
-    primal_analysis.Run()
+    # # primal analysis
+    # with open("ProjectParameters.json",'r') as parameter_file:
+    #     ProjectParametersPrimal = KratosMultiphysics.Parameters( parameter_file.read())
+    # model_primal = KratosMultiphysics.Model()
+    # primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersPrimal)
+    # primal_analysis.Run()
 
-    # adjoint analysis
-    with open("AdjointParameters.json",'r') as parameter_file:
-        ProjectParametersAdjoint = KratosMultiphysics.Parameters( parameter_file.read())
-    model_adjoint = KratosMultiphysics.Model()
-    adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_adjoint, ProjectParametersAdjoint)
-    adjoint_analysis.Run()
+    # # adjoint analysis
+    # with open("AdjointParameters.json",'r') as parameter_file:
+    #     ProjectParametersAdjoint = KratosMultiphysics.Parameters( parameter_file.read())
+    # model_adjoint = KratosMultiphysics.Model()
+    # adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_adjoint, ProjectParametersAdjoint)
+    # adjoint_analysis.Run()
 
 
     # -----------------------------------------------
