a0e0a8fa97307df27d471fd1b2300b7c4933fe1b
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-07-16 14:19:52 +0200

    Missing headers

--- applications/CSharpWrapperApplication/external_interface_bindings.cpp
+++ applications/CSharpWrapperApplication/external_interface_bindings.cpp
@@ -1,7 +1,30 @@
-#define EXPORT __declspec(dllexport)
+//    _____  _____ _                  __          __                                                 _ _           _   _
+//   / ____|/ ____| |                 \ \        / /                               /\               | (_)         | | (_)
+//  | |    | (___ | |__   __ _ _ __ _ _\ \  /\  / / __ __ _ _ __  _ __   ___ _ __ /  \   _ __  _ __ | |_  ___ __ _| |_ _  ___  _ __
+//  | |     \___ \| '_ \ / _` | '__| '_ \ \/  \/ / '__/ _` | '_ \| '_ \ / _ \ '__/ /\ \ | '_ \| '_ \| | |/ __/ _` | __| |/ _ \| '_  |
+//  | |____ ____) | | | | (_| | |  | |_) \  /\  /| | | (_| | |_) | |_) |  __/ | / ____ \| |_) | |_) | | | (_| (_| | |_| | (_) | | | |
+//   \_____|_____/|_| |_|\__,_|_|  | .__/ \/  \/ |_|  \__,_| .__/| .__/ \___|_|/_/    \_\ .__/| .__/|_|_|\___\__,_|\__|_|\___/|_| |_|
+//                                 | |                     | |   | |                    | |   | |
+//                                 |_|                     |_|   |_|                    |_|   |_|
+//
+//
+//  License: BSD License
+//   license: CSharpWrapperApplication/license.txt
+//
+//  Main authors:    Hubert Balcerzak
+//                   Riccardo Rossi
+//                   Vicente Mataix Ferrandiz
+//
+
+// System includes
 #include <stdlib.h>
-#include "external_interface.h"
 #include <iostream>
+#define EXPORT __declspec(dllexport)
+
+// External includes
+
+// Project includes
+#include "external_interface.h"
 
 using namespace std;
 using namespace CSharpKratosWrapper;
--- applications/CSharpWrapperApplication/mesh_converter.cpp
+++ applications/CSharpWrapperApplication/mesh_converter.cpp
@@ -1,9 +1,28 @@
+//    _____  _____ _                  __          __                                                 _ _           _   _
+//   / ____|/ ____| |                 \ \        / /                               /\               | (_)         | | (_)
+//  | |    | (___ | |__   __ _ _ __ _ _\ \  /\  / / __ __ _ _ __  _ __   ___ _ __ /  \   _ __  _ __ | |_  ___ __ _| |_ _  ___  _ __
+//  | |     \___ \| '_ \ / _` | '__| '_ \ \/  \/ / '__/ _` | '_ \| '_ \ / _ \ '__/ /\ \ | '_ \| '_ \| | |/ __/ _` | __| |/ _ \| '_  |
+//  | |____ ____) | | | | (_| | |  | |_) \  /\  /| | | (_| | |_) | |_) |  __/ | / ____ \| |_) | |_) | | | (_| (_| | |_| | (_) | | | |
+//   \_____|_____/|_| |_|\__,_|_|  | .__/ \/  \/ |_|  \__,_| .__/| .__/ \___|_|/_/    \_\ .__/| .__/|_|_|\___\__,_|\__|_|\___/|_| |_|
+//                                 | |                     | |   | |                    | |   | |
+//                                 |_|                     |_|   |_|                    |_|   |_|
+//
+//
+//  License: BSD License
+//   license: CSharpWrapperApplication/license.txt
+//
+//  Main authors:    Hubert Balcerzak
+//                   Riccardo Rossi
+//
 
-
-
+// System includes
 #include <vector>
 #include <algorithm>
 #include <iostream>
+
+// External includes
+
+// Project includes
 #include "mesh_converter.h"
 #include "containers/array_1d.h"
 #include "vector3.h"
