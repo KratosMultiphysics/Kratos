f37002e52416991b4e12f71eedff1d5e00fcd1b5
marcnunezc <marcnc94@gmail.com>
2019-07-17 10:05:41 +0200

    [skip ci] adding functions and comments

--- applications/CompressiblePotentialFlowApplication/python_scripts/define_embedded_wake_process_2d.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/define_embedded_wake_process_2d.py
@@ -33,10 +33,23 @@ class DefineEmbeddedWakeProcess(KratosMultiphysics.Process):
     def ExecuteInitializeSolutionStep(self):
         ini_time = time.time()
 
+        self._DefineWakeModelPart()
+        self._MoveAndRotateWake()
+        # Executing define wake process
+        CPFApp.DefineEmbeddedWakeProcess(self.main_model_part, self.wake_model_part).Execute()
+
+        KratosMultiphysics.Logger.PrintInfo('EmbeddedWake','Wake computation time: ',time.time()-ini_time)
+
+    def _DefineWakeModelPart(self):
+        ''' This function generates the modelpart of the wake. TODO: make end of the domain user-definable.
+        '''
         self.wake_model_part.CreateNewNode(1, 0.0, 0.0, 0.0)
         self.wake_model_part.CreateNewNode(2, 200.0, 0.0, 0.0)
         self.wake_model_part.CreateNewElement("Element2D2N", 1, [1,2], KratosMultiphysics.Properties(0))
 
+    def _MoveAndRotateWake(self):
+        ''' This function moves and rotates the wake with the same parameters as the geometry.
+        '''
         self.moving_parameters = KratosMultiphysics.Parameters()
         self.moving_parameters.AddEmptyValue("origin")
         self.moving_parameters["origin"].SetVector(self.main_model_part.ProcessInfo.GetValue(CPFApp.WAKE_ORIGIN))
@@ -44,8 +57,3 @@ class DefineEmbeddedWakeProcess(KratosMultiphysics.Process):
         angle=math.radians(-self.main_model_part.ProcessInfo.GetValue(CPFApp.ROTATION_ANGLE))
         self.moving_parameters["rotation_angle"].SetDouble(angle)
         CPFApp.MoveModelPartProcess(self.wake_model_part, self.moving_parameters).Execute()
-
-        CPFApp.DefineEmbeddedWakeProcess(self.main_model_part, self.wake_model_part).Execute()
-
-        KratosMultiphysics.Logger.PrintInfo('EmbeddedWake','Wake computation time: ',time.time()-ini_time)
-
