36dd9f82efd95dfe99a49643a1f847457d01fde4
jcotela <jcotela@cimne.upc.edu>
2019-07-24 18:44:21 +0200

    WIP defining fluid trilinos extension module.

---
+++ applications/FluidDynamicsApplication/trilinos_extension/custom_python/fluid_dynamics_trilinos_extension.cpp
@@ -0,0 +1,27 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Jordi Cotela
+//
+
+#ifdef KRATOS_PYTHON
+#include <pybind11/pybind11.h>
+
+namespace Kratos {
+namespace Python {
+
+PYBIND11_MODULE(KratosFluidDynamicsTrilinosExtension,m)
+{
+
+}
+
+}
+}
+
+#endif
