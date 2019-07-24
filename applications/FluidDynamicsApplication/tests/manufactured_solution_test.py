b566cf223b5104dba89ef3a891ecaffce55ad3da
jcotela <jcotela@cimne.upc.edu>
2019-07-24 10:58:36 +0200

    Removing duplicate imports (merge mistakes).

--- applications/FluidDynamicsApplication/tests/manufactured_solution_test.py
+++ applications/FluidDynamicsApplication/tests/manufactured_solution_test.py
@@ -10,9 +10,6 @@ import KratosMultiphysics.kratos_utilities as KratosUtilities
 import KratosMultiphysics.FluidDynamicsApplication as KratosFluid
 from KratosMultiphysics.FluidDynamicsApplication import python_solvers_wrapper_fluid
 
-import KratosMultiphysics.FluidDynamicsApplication as KratosFluid
-import KratosMultiphysics.FluidDynamicsApplication.python_solvers_wrapper_fluid as python_solvers_wrapper_fluid
-
 have_external_solvers = KratosUtilities.CheckIfApplicationsAvailable("ExternalSolversApplication")
 
 @KratosUnittest.skipUnless(have_external_solvers, "Missing required application: ExternalSolversApplication")
--- applications/FluidDynamicsApplication/tests/navier_stokes_wall_condition_test.py
+++ applications/FluidDynamicsApplication/tests/navier_stokes_wall_condition_test.py
@@ -5,7 +5,6 @@ import KratosMultiphysics.kratos_utilities as KratosUtilities
 from KratosMultiphysics.FluidDynamicsApplication import python_solvers_wrapper_fluid
 
 import KratosMultiphysics.FluidDynamicsApplication as KratosFluid
-import KratosMultiphysics.FluidDynamicsApplication.python_solvers_wrapper_fluid as python_solvers_wrapper_fluid
 
 have_external_solvers = KratosUtilities.CheckIfApplicationsAvailable("ExternalSolversApplication")
 
