4a9982f2a307135ea09027d9e331b68eec60363f
Nanda <bodhinandach@gmail.com>
2019-07-24 13:07:59 +0200

    rotate displacement

--- applications/ParticleMechanicsApplication/custom_utilities/mpm_boundary_rotation_utility.h
+++ applications/ParticleMechanicsApplication/custom_utilities/mpm_boundary_rotation_utility.h
@@ -326,15 +326,8 @@ public:
 		return is_penalty;
 	}
 
-	/// Same functionalities as RotateVelocities, just to have a clear function naming
-	virtual	void RotateDisplacements(ModelPart& rModelPart) const
-	{
-		this->RotateVelocities(rModelPart);
-	}
-
 	/// Transform nodal displacement to the rotated coordinates (aligned with each node's normal)
-	/// The name is kept to be Rotate Velocities, since it is currently a derived class of coordinate_transformation_utilities in the core
-	void RotateVelocities(ModelPart& rModelPart) const override
+	virtual	void RotateDisplacements(ModelPart& rModelPart) const
 	{
 		TLocalVectorType displacement(this->GetDomainSize());
 		TLocalVectorType Tmp(this->GetDomainSize());
@@ -371,15 +364,8 @@ public:
 		}
 	}
 
-	/// Same functionalities as RecoverVelocities, just to have a clear function naming
-	virtual void RecoverDisplacements(ModelPart& rModelPart) const
-	{
-		this->RecoverVelocities(rModelPart);
-	}
-
 	/// Transform nodal displacement from the rotated system to the original configuration
-	/// The name is kept to be Recover Velocities, since it is currently a derived class of coordinate_transformation_utilities in the core
-	void RecoverVelocities(ModelPart& rModelPart) const override
+	virtual void RecoverDisplacements(ModelPart& rModelPart) const
 	{
 		TLocalVectorType displacement(this->GetDomainSize());
 		TLocalVectorType Tmp(this->GetDomainSize());
