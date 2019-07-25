5ef850035e5cf26e2c4b8b1ec5563255ee4b9df5
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-07-25 14:10:51 +0200

    Adding test for HexahedraMeshToTetrahedraMesh

--- applications/MeshingApplication/tests/cpp_tests/test_meshing_utilities.cpp
+++ applications/MeshingApplication/tests/cpp_tests/test_meshing_utilities.cpp
@@ -19,12 +19,25 @@
 #include "geometries/tetrahedra_3d_4.h"
 #include "containers/model.h"
 #include "custom_utilities/meshing_utilities.h"
+// #include "includes/gid_io.h"
 
 namespace Kratos {
     namespace Testing {
 
         typedef Node<3> NodeType;
         typedef Geometry<NodeType> GeometryType;
+
+//         void GiDIODebugMeshingUtilities(ModelPart& ThisModelPart)
+//         {
+//             GidIO<> gid_io("TEST_MeshingUtilities", GiD_PostBinary, SingleFile, WriteUndeformed,  WriteConditions);
+//             const int nl_iter = ThisModelPart.GetProcessInfo()[NL_ITERATION_NUMBER];
+//             const double label = static_cast<double>(nl_iter);
+//
+//             gid_io.InitializeMesh(label);
+//             gid_io.WriteMesh(ThisModelPart.GetMesh());
+//             gid_io.FinalizeMesh();
+//             gid_io.InitializeResults(label, ThisModelPart.GetMesh());
+//         }
 
         void CreateDummy2DModelPart(ModelPart& rModelPart)
         {
@@ -79,6 +92,25 @@ namespace Kratos {
             rModelPart.CreateNewElement("Element3D4N", 12, {{3,2,1,6}}, p_elem_prop);
         }
 
+        void CreateDummyHexa3DModelPart(ModelPart& rModelPart)
+        {
+            Properties::Pointer p_elem_prop = rModelPart.CreateNewProperties(0);
+
+            // First we create the nodes
+            rModelPart.CreateNewNode(1 , 0.0 , 1.0 , 1.0);
+            rModelPart.CreateNewNode(2 , 0.0 , 1.0 , 0.0);
+            rModelPart.CreateNewNode(3 , 0.0 , 0.0 , 1.0);
+            rModelPart.CreateNewNode(4 , 1.0 , 1.0 , 1.0);
+            rModelPart.CreateNewNode(5 , 0.0 , 0.0 , 0.0);
+            rModelPart.CreateNewNode(6 , 1.0 , 1.0 , 0.0);
+            rModelPart.CreateNewNode(7 , 1.0 , 0.0 , 1.0);
+            rModelPart.CreateNewNode(8 , 1.0 , 0.0 , 0.0);
+
+            // Now we create the elements and conditions
+            rModelPart.CreateNewElement("Element3D8N", 1, {{8,6,2,5,7,4,1,3}}, p_elem_prop);
+            rModelPart.CreateNewCondition("SurfaceCondition3D4N", 1, {{7,4,1,3}}, p_elem_prop);
+        }
+
         /**
         * Checks the correct work of the BlockThresholdSizeElements
         * Test triangle
@@ -131,6 +163,40 @@ namespace Kratos {
                 KRATOS_CHECK(r_element.Is(BLOCKED));
 //                 KRATOS_WATCH(r_element.GetValue(ELEMENT_H))
             }
+        }
+
+        /**
+        * Checks the correct work of the HexahedraMeshToTetrahedraMesh
+        */
+        KRATOS_TEST_CASE_IN_SUITE(HexahedraMeshToTetrahedraMesh, KratosMeshingApplicationFastSuite)
+        {
+            Model this_model;
+            ModelPart& r_model_part = this_model.CreateModelPart("Main", 2);
+            ProcessInfo& r_current_process_info = r_model_part.GetProcessInfo();
+            r_current_process_info[DOMAIN_SIZE] = 3;
+
+            CreateDummyHexa3DModelPart(r_model_part);
+
+            const double volume_reference = r_model_part.GetElement(1).GetGeometry().Volume();
+            const double area_reference = r_model_part.GetCondition(1).GetGeometry().Area();
+
+//             GiDIODebugMeshingUtilities(r_model_part);
+
+            MeshingUtilities::HexahedraMeshToTetrahedraMesh(r_model_part);
+
+//             GiDIODebugMeshingUtilities(r_model_part);
+
+            double volume = 0;
+            double area = 0;
+            for (auto& r_element: r_model_part.Elements()) {
+                volume += r_element.GetGeometry().Volume();
+            }
+            for (auto& r_condition: r_model_part.Conditions()) {
+                area += r_condition.GetGeometry().Area();
+            }
+
+            KRATOS_CHECK_NEAR(volume, volume_reference, 1.0e-12);
+            KRATOS_CHECK_NEAR(area, area_reference, 1.0e-12);
         }
 
     } // namespace Testing
