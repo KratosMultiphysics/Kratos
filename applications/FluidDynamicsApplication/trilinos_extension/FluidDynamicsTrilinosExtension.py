19b218f9e943e47d3b9176657172867aafc7aac1
jcotela <jcotela@cimne.upc.edu>
2019-07-26 09:15:37 +0200

    Adding basic python module.

---
+++ applications/FluidDynamicsApplication/trilinos_extension/FluidDynamicsTrilinosExtension.py
@@ -0,0 +1,2 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from KratosFluidDynamicsTrilinosExtension import *
