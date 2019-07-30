c0e5d31b274553a1ba1b58ad3784e76556b5ba38
kevin braun <cd>
2019-07-30 13:33:25 +0200

    adjoint_point_moment_condition: in Create(): changed make_shared to make_intrusive

--- applications/StructuralMechanicsApplication/custom_response_functions/adjoint_conditions/adjoint_semi_analytic_point_moment_condition.h
+++ applications/StructuralMechanicsApplication/custom_response_functions/adjoint_conditions/adjoint_semi_analytic_point_moment_condition.h
@@ -98,7 +98,7 @@ public:
                               NodesArrayType const& ThisNodes,
                               typename PropertiesType::Pointer pProperties) const override
     {
-        return Kratos::make_shared<AdjointSemiAnalyticPointMomentCondition<TPrimalCondition>>(
+        return Kratos::make_intrusive<AdjointSemiAnalyticPointMomentCondition<TPrimalCondition>>(
             NewId, this->GetGeometry().Create(ThisNodes), pProperties);
     }
 
@@ -106,7 +106,7 @@ public:
                               typename GeometryType::Pointer pGeometry,
                               typename PropertiesType::Pointer pProperties) const override
     {
-        return Kratos::make_shared<AdjointSemiAnalyticPointMomentCondition<TPrimalCondition>>(
+        return Kratos::make_intrusive<AdjointSemiAnalyticPointMomentCondition<TPrimalCondition>>(
             NewId, pGeometry, pProperties);
     }
 
