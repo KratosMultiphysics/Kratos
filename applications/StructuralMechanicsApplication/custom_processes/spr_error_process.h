21ba04d675ff7dad2fc94bbe080a428cdc759299
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-07-31 19:18:09 +0200

    Minor

--- applications/StructuralMechanicsApplication/custom_processes/spr_error_process.h
+++ applications/StructuralMechanicsApplication/custom_processes/spr_error_process.h
@@ -171,9 +171,9 @@ protected:
     ///@name Protected member Variables
     ///@{
 
-    ModelPart& mThisModelPart;                                 /// The model part to compute
+    ModelPart& mThisModelPart;                                  /// The model part to compute
     Variable<Vector>* mpStressVariable = &CAUCHY_STRESS_VECTOR; /// The stress variable considered
-    SizeType mEchoLevel;                                       /// The echo level
+    SizeType mEchoLevel;                                        /// The echo level
 
     ///@}
     ///@name Protected Operators
