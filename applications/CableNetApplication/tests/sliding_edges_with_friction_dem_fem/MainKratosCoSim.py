6cc57c20220074ae47c8ec6e51c1cca13d9019e8
KlausBSautter <klaus.sautter@tum.de>
2019-08-01 13:23:06 +0200

    added 1st test

---
+++ applications/CableNetApplication/tests/sliding_edges_with_friction_dem_fem/MainKratosCoSim.py
@@ -0,0 +1,19 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import KratosMultiphysics as KM
+from KratosMultiphysics.CoSimulationApplication.co_simulation_analysis import CoSimulationAnalysis
+
+import KratosMultiphysics.CableNetApplication
+
+"""
+For user-scripting it is intended that a new class is derived
+from CoSimulationAnalysis to do modifications
+Check also "kratos/python_scripts/analysis-stage.py" for available methods that can be overridden
+"""
+
+parameter_file_name = "cosim_dem_fem_cable_net_parameters.json"
+with open(parameter_file_name,'r') as parameter_file:
+    parameters = KM.Parameters(parameter_file.read())
+
+simulation = CoSimulationAnalysis(parameters)
+simulation.Run()
