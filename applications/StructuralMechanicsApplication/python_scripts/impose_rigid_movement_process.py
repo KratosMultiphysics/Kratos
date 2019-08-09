38ab630a8a42d97be3ff420e53fd61712c6232d8
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-08-09 16:02:04 +0200

    Minor clean up

--- applications/StructuralMechanicsApplication/python_scripts/impose_rigid_movement_process.py
+++ applications/StructuralMechanicsApplication/python_scripts/impose_rigid_movement_process.py
@@ -1,8 +1,11 @@
-import KratosMultiphysics as KratosMultiphysics
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing the Kratos Library
+import KratosMultiphysics
 import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
 
 def Factory(settings, Model):
-    if(type(settings) != KratosMultiphysics.Parameters):
+    if not isinstance(settings, KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return ImposeRigidMovementProcess(Model, settings["Parameters"])
 
