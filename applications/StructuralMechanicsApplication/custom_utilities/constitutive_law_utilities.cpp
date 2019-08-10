d9c4b0a026ee9d31f80521447957fb32bf4f780d
philbucher <philipp.bucher@tum.de>
2019-08-10 23:25:14 +0200

    another test

--- applications/StructuralMechanicsApplication/custom_utilities/constitutive_law_utilities.cpp
+++ applications/StructuralMechanicsApplication/custom_utilities/constitutive_law_utilities.cpp
@@ -47,6 +47,16 @@ void ConstitutiveLawUtilities<3>::CalculateI1Invariant(
         rI1 += rStressVector[i];
 }
 
+
+template<SizeType TVoigtSize>
+void ConstitutiveLawUtilities<TVoigtSize>::CalculateI1Invariant(
+    const BoundedVectorType& rStressVector,
+    double& rI1
+    )
+{
+
+}
+
 /***********************************************************************************/
 /***********************************************************************************/
 
