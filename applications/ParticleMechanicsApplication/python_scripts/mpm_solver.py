7c0423a19e26fce634d3b43d695b90b9511b1365
Veronika Singer <veronika.singer@tum.de>
2019-08-13 15:29:57 +0200

    convergence  displacement criteria

--- applications/ParticleMechanicsApplication/python_scripts/mpm_solver.py
+++ applications/ParticleMechanicsApplication/python_scripts/mpm_solver.py
@@ -199,7 +199,7 @@ class MPMSolver(PythonSolver):
     def _GenerateMaterialPoint(self):
         pressure_dofs          = self.settings["pressure_dofs"].GetBool()
         axis_symmetric_flag    = self.settings["axis_symmetric_flag"].GetBool()
-        
+
         # Assigning extra information to the main model part
         self.material_point_model_part.SetNodes(self.grid_model_part.GetNodes())
         self.material_point_model_part.ProcessInfo = self.grid_model_part.ProcessInfo
@@ -347,10 +347,15 @@ class MPMSolver(PythonSolver):
             R_AT = convergence_criterion_parameters["residual_absolute_tolerance"].GetDouble()
             convergence_criterion = KratosMultiphysics.ResidualCriteria(R_RT, R_AT)
             convergence_criterion.SetEchoLevel(convergence_criterion_parameters["echo_level"].GetInt())
+        elif (convergence_criterion_parameters["convergence_criterion"].GetString() == "displacement_criterion"):
+                D_RT = convergence_criterion_parameters["displacement_relative_tolerance"].GetDouble()
+                D_AT = convergence_criterion_parameters["displacement_absolute_tolerance"].GetDouble()
+                convergence_criterion = KratosMultiphysics.ResidualCriteria(D_RT, D_AT)
+                convergence_criterion.SetEchoLevel(convergence_criterion_parameters["echo_level"].GetInt())
         else:
             err_msg  = "The requested convergence criteria \"" + convergence_criterion_parameters["convergence_criterion"].GetString()
             err_msg += "\" is not supported for ParticleMechanicsApplication!\n"
-            err_msg += "Available options are: \"residual_criterion\""
+            err_msg += "Available options are: \"residual_criterion\" or \"displacement_criterion\""
             raise Exception(err_msg)
 
         return convergence_criterion
