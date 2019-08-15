40dc5a66ab90709d23dc6c6faec881eb84738b2c
Tobias Teschemacher <tobias.teschemacher@tum.de>
2019-08-15 15:03:45 +0200

    BrepFaceCurve

---
+++ kratos/geometries/brep_face_curve.h
@@ -0,0 +1,362 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Tobias Teschemacher
+//  contributors:    Pooyan Dadvand
+//                   Philipp Bucher
+//
+
+#if !defined(KRATOS_BREP_FACE_CURVE_3D_H_INCLUDED )
+#define  KRATOS_BREP_FACE_CURVE_3D_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "geometries/geometry.h"
+#include "geometries/nurbs_shape_function_utilities/nurbs_interval.h"
+
+
+namespace Kratos
+{
+///@name Kratos Classes
+///@{
+
+/**
+ * @class BrepFaceCurve
+ * @ingroup KratosCore
+ * @brief The BrepFaceCurve acts as topology for faces having a pointer
+ *        to a surface geometry and a list of BrepEdge s. Those edges
+ *        define the trimming of the respective surface.
+ */
+template<class TPointType, class TPointEmbeddedType = TPointType>
+class BrepFaceCurve
+    : public Geometry<TPointType>
+{
+public:
+    ///@}
+    ///@name Type Definitions
+    ///@{
+
+    /** Pointer definition of BrepFaceCurve */
+    KRATOS_CLASS_POINTER_DEFINITION( BrepFaceCurve );
+
+    typedef TPointType PointType;
+
+    typedef Geometry<TPointType> BaseType;
+    typedef Geometry<TPointType> GeometryType;
+
+    typedef GeometryData::IntegrationMethod IntegrationMethod;
+
+    typedef NurbsSurfaceGeometry<3, TPointType> NurbsSurfaceType;
+    typedef NurbsCurveGeometry<2, TPointEmbeddedType> NurbsCurveType;
+
+    typedef typename BaseType::GeometriesArrayType GeometriesArrayType;
+
+    typedef typename BaseType::IndexType IndexType;
+    typedef typename BaseType::SizeType SizeType;
+
+    typedef typename BaseType::PointsArrayType PointsArrayType;
+    typedef typename BaseType::CoordinatesArrayType CoordinatesArrayType;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    BrepFaceCurve( 
+        typename NurbsSurfaceType::Pointer pSurface,
+        typename NurbsCurveType::Pointer pCurve)
+        : BaseType(PointsArrayType(), &msGeometryData)
+        , mpNurbsSurface(pSurface)
+        , mpNurbsCurve(pCurve)
+        , mCurveInterval(pCurve->DomainInterval())
+    {
+    }
+
+    BrepFaceCurve(
+        typename NurbsSurfaceType::Pointer pSurface,
+        typename NurbsCurveType::Pointer pCurve,
+        Interval CurveInterval)
+        : BaseType(PointsArrayType(), &msGeometryData)
+        , mpNurbsSurface(pSurface)
+        , mpNurbsCurve(pCurve)
+        , mCurveInterval(CurveInterval)
+    {
+    }
+
+    explicit BrepFaceCurve(const PointsArrayType& ThisPoints)
+        : BaseType(ThisPoints, &msGeometryData)
+    {
+    }
+
+    /**
+     * Copy constructor.
+     * Construct this geometry as a copy of given geometry.
+     *
+     * @note This copy constructor does not copy the points and new
+     * geometry shares points with given source geometry. It is
+     * obvious that any change to this new geometry's point affect
+     * source geometry's points too.
+     */
+    BrepFaceCurve( BrepFaceCurve const& rOther )
+        : BaseType( rOther )
+    {
+    }
+
+    /**
+     * Copy constructor from a geometry with other point type.
+     * Construct this geometry as a copy of given geometry which
+     * has different type of points. The given goemetry's
+     * TOtherPointType* must be implicity convertible to this
+     * geometry PointType.
+     *
+     * @note This copy constructor does not copy the points and new
+     * geometry shares points with given source geometry. It is
+     * obvious that any change to this new geometry's point affect
+     * source geometry's points too.
+     */
+    template<class TOtherPointType, class TOtherPointEmbeddedType> explicit BrepFaceCurve(
+        BrepFaceCurve<TOtherPointType, TOtherPointEmbeddedType> const& rOther )
+        : BaseType( rOther )
+    {
+    }
+
+    /// Destructor
+    ~BrepFaceCurve() override = default;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /**
+     * Assignment operator.
+     *
+     * @note This operator don't copy the points and this
+     * geometry shares points with given source geometry. It's
+     * obvious that any change to this geometry's point affect
+     * source geometry's points too.
+     *
+     * @see Clone
+     * @see ClonePoints
+     */
+    BrepFaceCurve& operator=( const BrepFaceCurve& rOther )
+    {
+        BaseType::operator=( rOther );
+        return *this;
+    }
+
+    /**
+     * Assignment operator for geometries with different point type.
+     *
+     * @note This operator don't copy the points and this
+     * geometry shares points with given source geometry. It's
+     * obvious that any change to this geometry's point affect
+     * source geometry's points too.
+     *
+     * @see Clone
+     * @see ClonePoints
+     */
+    template<class TOtherPointType, class TOtherPointEmbeddedType>
+    BrepFaceCurve& operator=( BrepFaceCurve<TOtherPointType, TOtherPointEmbeddedType> const & rOther )
+    {
+        BaseType::operator=( rOther );
+        return *this;
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
+    {
+        return typename BaseType::Pointer( new BrepFaceCurve( ThisPoints ) );
+    }
+
+    ///@}
+    ///@name Geometrical Operations
+    ///@{
+
+    /*
+    * @brief This method maps from local space to working space and computes the
+    *        number of derivatives at the local space parameter in the dimension of the object.
+    * @param LocalCoordinates The local coordinates in dimension space
+    * @param Derivative Number of computed derivatives
+    * @return std::vector<array_1d<double, 3>> with the coordinates in working space
+    * @see PointLocalCoordinates
+    */
+    //std::vector<CoordinatesArrayType> GlobalDerivatives(
+    //    const CoordinatesArrayType& rCoordinates,
+    //    const SizeType DerivativeOrder) const
+    //{
+    //    CoordinatesArrayType result(3, 0.0);
+
+    //    mpNurbsCurve->GlobalCoordinates(result, rCoordinates);
+
+    //    return mpNurbsSurface->GlobalDerivatives(rCoordinates, DerivativeOrder);
+    //}
+
+    /*
+    * @brief This method maps from dimension space to working space.
+    * @param rResult array_1d<double, 3> with the coordinates in working space
+    * @param LocalCoordinates The local coordinates in dimension space
+    * @return array_1d<double, 3> with the coordinates in working space
+    * @see PointLocalCoordinates
+    */
+    CoordinatesArrayType& GlobalCoordinates(
+        CoordinatesArrayType& rResult,
+        const CoordinatesArrayType& rLocalCoordinates
+    ) const override
+     {
+        CoordinatesArrayType surface_coordinates(3, 0.0);
+        mpNurbsCurve->GlobalCoordinates(surface_coordinates, rLocalCoordinates);
+        mpNurbsSurface->GlobalCoordinates(rResult, surface_coordinates);
+
+        return rResult;
+    }
+
+    ///@name Shape Function
+    ///@{
+
+    Vector& ShapeFunctionsValues(
+        Vector &rResult,
+        const CoordinatesArrayType& rCoordinates) const override
+    {
+        CoordinatesArrayType surface_coordinates(3, 0.0);
+        mpNurbsCurve->GlobalCoordinates(surface_coordinates, rCoordinates);
+        mpNurbsSurface->ShapeFunctionsValues(rResult, surface_coordinates);
+
+        return rResult;
+    }
+
+    Matrix& ShapeFunctionsLocalGradients(
+        Matrix& rResult,
+        const CoordinatesArrayType& rCoordinates) const override
+    {
+        CoordinatesArrayType surface_coordinates(3, 0.0);
+        mpNurbsCurve->GlobalCoordinates(surface_coordinates, rCoordinates);
+        mpNurbsSurface->ShapeFunctionsLocalGradients(rResult, surface_coordinates);
+
+        return rResult;
+    }
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+
+    ///@}
+    ///@name Information
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        return "Brep face curve";
+    }
+
+    /// Print information about this object.
+    void PrintInfo( std::ostream& rOStream ) const override
+    {
+        rOStream << "Brep face curve";
+    }
+
+    /// Print object's data.
+    void PrintData( std::ostream& rOStream ) const override
+    {
+        BaseType::PrintData( rOStream );
+        std::cout << std::endl;
+        rOStream << "    Brep face curve : " << std::endl;
+    }
+
+    ///@}
+
+protected:
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+    static const GeometryData msGeometryData;
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    typename NurbsSurfaceType::Pointer mpNurbsSurface;
+    typename NurbsCurveType::Pointer mpNurbsCurve;
+
+    Interval mCurveInterval;
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    void save( Serializer& rSerializer ) const override
+    {
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS( rSerializer, BaseType );
+        rSerializer.save("NurbsSurface", mpNurbsSurface);
+        rSerializer.save("NurbsCurve", mpNurbsCurve);
+    }
+
+    void load( Serializer& rSerializer ) override
+    {
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, BaseType );
+        rSerializer.load("NurbsSurface", mpNurbsSurface);
+        rSerializer.load("NurbsCurve", mpNurbsCurve);
+    }
+
+    BrepFaceCurve()
+        : BaseType( PointsArrayType(), &msGeometryData )
+    {}
+
+    ///@}
+    ///@name Private Friends
+    ///@{
+
+    template<class TOtherPointType, class TOtherPointEmbeddedType> friend class BrepFaceCurve;
+
+    ///@}
+}; // Class BrepFaceCurve
+
+///@name Input and output
+///@{
+
+/// input stream functions
+template<class TPointType, class TPointEmbeddedType> inline std::istream& operator >> (
+    std::istream& rIStream,
+    BrepFaceCurve<TPointType, TPointEmbeddedType>& rThis );
+
+/// output stream functions
+template<class TPointType, class TPointEmbeddedType> inline std::ostream& operator << (
+    std::ostream& rOStream,
+    const BrepFaceCurve<TPointType, TPointEmbeddedType>& rThis )
+{
+    rThis.PrintInfo( rOStream );
+    rOStream << std::endl;
+    rThis.PrintData( rOStream );
+    return rOStream;
+}
+
+///@}
+///@name Static Type Declarations
+///@{
+
+template<class TPointType, class TPointEmbeddedType> const
+GeometryData BrepFaceCurve<TPointType, TPointEmbeddedType>::msGeometryData(
+    1, 3, 2,
+    GeometryData::GI_GAUSS_1,
+    {}, {}, {});
+
+///@}
+}// namespace Kratos.
+
+#endif // KRATOS_BREP_FACE_CURVE_3D_H_INCLUDED  defined
---
+++ kratos/tests/cpp_tests/geometries/test_brep_face_curve.cpp
@@ -0,0 +1,109 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Tobias Teschemacher
+//
+
+// System includes
+#include <limits>
+
+// External includes
+
+// Project includes
+#include "testing/testing.h"
+#include "geometries/nurbs_curve_geometry.h"
+#include "geometries/nurbs_surface_geometry.h"
+#include "geometries/brep_face_curve.h"
+
+#include "tests/cpp_tests/geometries/test_geometry.h"
+
+namespace Kratos {
+namespace Testing {
+
+    typedef Node<3> NodeType;
+
+    NurbsSurfaceGeometry<3, NodeType>::Pointer GenerateReferenceNodeSurfacePointer() {
+        Geometry<NodeType>::PointsArrayType points;
+
+        points.push_back(NodeType::Pointer(new NodeType(1, 0, 5, 0)));
+        points.push_back(NodeType::Pointer(new NodeType(2, 5, 5, 0)));
+        points.push_back(NodeType::Pointer(new NodeType(3, 10, 5, -4)));
+        points.push_back(NodeType::Pointer(new NodeType(4, 0, 0, 0)));
+        points.push_back(NodeType::Pointer(new NodeType(5, 5, 0, 0)));
+        points.push_back(NodeType::Pointer(new NodeType(6, 10, 0, -4)));
+
+        Vector knot_u = ZeroVector(4);
+        knot_u[0] = 0.0;
+        knot_u[1] = 0.0;
+        knot_u[2] = 10.0;
+        knot_u[3] = 10.0;
+        Vector knot_v = ZeroVector(2); 
+        knot_v[0] = 0.0;
+        knot_v[1] = 5.0;
+
+        int p = 2;
+        int q = 1;
+
+        return Kratos::make_shared < NurbsSurfaceGeometry<3, NodeType >>(points, p, q, knot_u, knot_v);
+    }
+
+    NurbsCurveGeometry<2, Point>::Pointer GenerateReferenceCurve2dPointer()
+    {
+        NurbsCurveGeometry<2, Point>::PointsArrayType points;
+
+        points.push_back(Point::Pointer(new Point(0, 0)));
+        points.push_back(Point::Pointer(new Point(3.3333333333333335, 1.6666666666666667)));
+        points.push_back(Point::Pointer(new Point(6.6666666666666661, 3.333333333333333)));
+        points.push_back(Point::Pointer(new Point(10, 5)));
+
+        Vector knot_vector = ZeroVector(6);
+        knot_vector[0] = 0.0;
+        knot_vector[1] = 0.0;
+        knot_vector[2] = 0.0;
+        knot_vector[3] = 11.180339887498949;
+        knot_vector[4] = 11.180339887498949;
+        knot_vector[5] = 11.180339887498949;
+
+        int p = 3;
+
+        return Kratos::make_shared<NurbsCurveGeometry<2, Point>>(points, p, knot_vector);
+    }
+
+    ///// Tests
+    KRATOS_TEST_CASE_IN_SUITE(NurbsBrepFaceCurve, KratosCoreGeometriesFastSuite) {
+        auto p_surface = GenerateReferenceNodeSurfacePointer();
+        auto p_curve = GenerateReferenceCurve2dPointer();
+
+        auto p_brep_face_curve = BrepFaceCurve<NodeType, Point>(
+            p_surface, p_curve);
+
+        // Check general information, input to ouput
+        KRATOS_CHECK_EQUAL(p_brep_face_curve.Dimension(), 1);
+        KRATOS_CHECK_EQUAL(p_brep_face_curve.WorkingSpaceDimension(), 3);
+        KRATOS_CHECK_EQUAL(p_brep_face_curve.LocalSpaceDimension(), 2);
+
+        array_1d<double, 3> coords(3, 0.0);
+        coords[0] = 1.0;
+        Vector N;
+
+        p_brep_face_curve.ShapeFunctionsValues(N, coords);
+        KRATOS_WATCH(N)
+        Matrix DN_De;
+        p_brep_face_curve.ShapeFunctionsLocalGradients(DN_De, coords);
+        KRATOS_WATCH(DN_De)
+
+        array_1d<double, 3> result(3, 0.0);
+        p_brep_face_curve.GlobalCoordinates(result, coords);
+        KRATOS_WATCH(result)
+
+        //auto results = p_brep_face_curve.GlobalDerivatives(coords, 3);
+        //KRATOS_WATCH(results[0])
+    }
+} // namespace Testing.
+} // namespace Kratos.
