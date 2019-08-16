b6e758e0b7a4e56bd9346823641de1a2c7cfe2e1
KlausBSautter <klaus.sautter@tum.de>
2019-08-16 14:55:41 +0200

    fixed element

--- applications/StructuralMechanicsApplication/custom_elements/weak_coupling_slide.hpp
+++ applications/StructuralMechanicsApplication/custom_elements/weak_coupling_slide.hpp
@@ -130,6 +130,12 @@ namespace Kratos
             Variable<array_1d<double, 3>>& rDestinationVariable,
             const ProcessInfo& rCurrentProcessInfo) override;
 
+        void AddExplicitContribution(
+            const VectorType& rRHSVector,
+            const Variable<VectorType>& rRHSVariable,
+            Variable<double >& rDestinationVariable,
+            const ProcessInfo& rCurrentProcessInfo) override;
+
 
         void GetValuesVector(
             Vector& rValues,
