50e0e046651403cd753a995d0f92e91f90507a87
Tobias Teschemacher <tobias.teschemacher@tum.de>
2019-08-22 14:33:29 +0200

    small fixes in test_quadrature_point.cpp

--- kratos/tests/cpp_tests/geometries/test_quadrature_point.cpp
+++ kratos/tests/cpp_tests/geometries/test_quadrature_point.cpp
@@ -26,6 +26,9 @@ namespace Kratos {
     namespace Testing {
         typedef Node<3> NodeType;
 
+        typedef std::size_t SizeType;
+        typedef std::size_t IndexType;
+
         /** Generates a point type sample triangle2D3.
          * Generates a point type right triangle with origin in the origin and leg size 1.
          * @return  Pointer to a triangle2D3
@@ -41,49 +44,46 @@ namespace Kratos {
         Geometry<Node<3>>::Pointer GenerateQuadraturePoint() {
             auto triangle = GeneratePointsTriangle2D3();
 
-            auto integration_method = triangle->GetDefaultIntegrationMethod();
+            //auto integration_method = triangle->GetDefaultIntegrationMethod();
             auto integration_points = triangle->IntegrationPoints();
 
             auto r_N = triangle->ShapeFunctionsValues();
 
-            for (int i = 0; i < integration_points.size(); ++i)
+            Matrix N_i = ZeroMatrix(1, triangle->size());
+            for (IndexType j = 0; j < triangle->size(); ++j)
             {
-                Matrix N_i = ZeroMatrix(1, triangle->size());
-                for (std::size_t j = 0; j < triangle->size(); ++j)
-                {
-                    N_i(0, j) = r_N(i, j);
-                }
-                Matrix DN_De = triangle->ShapeFunctionLocalGradient(i);
-
-                QuadraturePoint<2, 2, Node<3>>::IntegrationPointsArrayType ips(1);
-                ips[0] = integration_points[i];
-
-                QuadraturePoint<2, 2, Node<3>>::IntegrationPointsContainerType ips_container =
-                { { ips } };
-
-                QuadraturePoint<2, 2, Node<3>>::ShapeFunctionsValuesContainerType N_container =
-                { { N_i } };
-
-                auto DN_De_array = DenseVector<Matrix>(1);
-                DN_De_array[0] = DN_De;
-
-                QuadraturePoint<2, 2, Node<3>>::ShapeFunctionsLocalGradientsContainerType DN_De_container =
-                { { DN_De_array } };
-
-                GeometryShapeFunctionContainer<GeometryData::IntegrationMethod> data_container(
-                    GeometryData::GI_GAUSS_1,
-                    integration_points[i],
-                    N_i,
-                    DN_De);
-
-                Geometry<Node<3>>::Pointer p_this_quadrature_point(
-                    Kratos::make_shared<QuadraturePoint<2, 2, Node<3>>>(
-                        triangle->Points(),
-                        data_container,
-                        triangle.get()));
-
-                return p_this_quadrature_point;
+                N_i(0, j) = r_N(0, j);
             }
+            Matrix DN_De = triangle->ShapeFunctionLocalGradient(0);
+
+            QuadraturePoint<2, 2, Node<3>>::IntegrationPointsArrayType ips(1);
+            ips[0] = integration_points[0];
+
+            QuadraturePoint<2, 2, Node<3>>::IntegrationPointsContainerType ips_container =
+            { { ips } };
+
+            QuadraturePoint<2, 2, Node<3>>::ShapeFunctionsValuesContainerType N_container =
+            { { N_i } };
+
+            auto DN_De_array = DenseVector<Matrix>(1);
+            DN_De_array[0] = DN_De;
+
+            QuadraturePoint<2, 2, Node<3>>::ShapeFunctionsLocalGradientsContainerType DN_De_container =
+            { { DN_De_array } };
+
+            GeometryShapeFunctionContainer<GeometryData::IntegrationMethod> data_container(
+                GeometryData::GI_GAUSS_1,
+                integration_points[0],
+                N_i,
+                DN_De);
+
+            Geometry<Node<3>>::Pointer p_this_quadrature_point(
+                Kratos::make_shared<QuadraturePoint<2, 2, Node<3>>>(
+                    triangle->Points(),
+                    data_container,
+                    triangle.get()));
+
+            return p_this_quadrature_point;
         }
 
         KRATOS_TEST_CASE_IN_SUITE(QuadraturePoint2d, KratosCoreFastSuite)
