092501331daac75ca6c1a9d4d88e116ebbc7ee80
Andreas Apostolatos <andreas.apostolatos@tum.de>
2019-08-23 15:27:41 +0200

    Adding test for nurbs curve shape functions

--- kratos/geometries/nurbs_shape_function_utilities/nurbs_curve_shape_functions.h
+++ kratos/geometries/nurbs_shape_function_utilities/nurbs_curve_shape_functions.h
@@ -80,7 +80,8 @@ public:
         // Not possible to compute shape functions where derivative order
         // is higher than the polynomial degree. Those are returned as zero
         // within the access function ShapeFunctionValue
-        mDerivativeOrder = std::min(DerivativeOrder, PolynomialDegree);
+        // mDerivativeOrder = std::min(DerivativeOrder, PolynomialDegree);
+        mDerivativeOrder = DerivativeOrder;
 
         mValues.resize((mDerivativeOrder + 1) * (PolynomialDegree + 1));
         mLeft.resize(PolynomialDegree);
