a13c34c113ae52daac7285808e4d501ea88e2728
Tobias Teschemacher <tobias.teschemacher@tum.de>
2019-08-26 13:20:37 +0200

    typedef for size/index type

--- kratos/geometries/quadrature_point.h
+++ kratos/geometries/quadrature_point.h
@@ -50,6 +50,9 @@ public:
     typedef Geometry<TPointType> BaseType;
     typedef Geometry<TPointType> GeometryType;
 
+    typedef GeometryType::IndexType IndexType;
+    typedef GeometryType::SizeType SizeType;
+
     typedef typename GeometryType::PointsArrayType PointsArrayType;
     typedef typename GeometryType::CoordinatesArrayType CoordinatesArrayType;
 
