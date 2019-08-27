c3f7fc22b1e35b19bb821a52c470c8f4b5363605
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-08-27 16:21:17 +0200

    adding

--- applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
+++ applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
@@ -6,3 +6,34 @@ import KratosMultiphysics.FemToDemApplication as KratosFemDem
 import CouplingFemDem
 import math
 import KratosMultiphysics.MeshingApplication as MeshingApplication
+
+def Wait():
+    input("Press Something")
+
+class MainCoupledFemDem_Solution:
+
+#============================================================================================================================
+    def __init__(self, Model):
+
+        # Initialize solutions
+        self.FEM_Solution = FEM.FEM_for_coupling_Solution(Model)
+        self.DEM_Solution = DEM.DEM_for_coupling_Solution(Model)
+
+        # Initialize Remeshing files
+        self.DoRemeshing = self.FEM_Solution.ProjectParameters["AMR_data"]["activate_AMR"].GetBool()
+        if self.DoRemeshing:
+            self.mmg_parameter_file = open("MMGParameters.json",'r')
+            self.mmg_parameters = KratosMultiphysics.Parameters(self.mmg_parameter_file.read())
+            self.RemeshingProcessMMG = MMG.MmgProcess(Model, self.mmg_parameters)
+
+        self.InitializePlotsFiles()
+        self.echo_level = 0
+
+#============================================================================================================================
+    def Run(self):
+
+        self.Initialize()
+        self.RunMainTemporalLoop()
+        self.Finalize()
+
+#============================================================================================================================
