e6266e735f6e01c47226bc50809f0d62ad23811d
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-08-27 16:16:59 +0200

    unique main creation

---
+++ applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import FEMDEMParticleCreatorDestructor as PCD
+import KratosMultiphysics
+import KratosMultiphysics.FemToDemApplication as KratosFemDem
+import CouplingFemDem
+import math
+import KratosMultiphysics.MeshingApplication as MeshingApplication
