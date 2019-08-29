9f7c192af6409b03ac8499b2c6f060f9bd857aa1
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2019-08-29 17:15:44 +0200

    initial commit

---
+++ applications/ShallowWaterApplication/custom_elements/rv_swe.cpp
@@ -0,0 +1,448 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "includes/checks.h"
+#include "includes/cfd_variables.h"
+#include "utilities/math_utils.h"
+#include "utilities/geometry_utilities.h"
+#include "shallow_water_application_variables.h"
+#include "rv_swe.h"
+
+namespace Kratos
+{
+
+template< size_t TNumNodes, Framework TFramework >
+int RV_SWE<TNumNodes, TFramework>::Check( const ProcessInfo& rCurrentProcessInfo )
+{
+    KRATOS_TRY
+
+    // Base class checks for positive Jacobian and Id > 0
+    int ierr = Element::Check(rCurrentProcessInfo);
+    if(ierr != 0) return ierr;
+
+    // Check that all required variables have been registered
+    KRATOS_CHECK_VARIABLE_KEY(VELOCITY)
+    KRATOS_CHECK_VARIABLE_KEY(HEIGHT)
+    KRATOS_CHECK_VARIABLE_KEY(PROJECTED_SCALAR1)
+    KRATOS_CHECK_VARIABLE_KEY(PROJECTED_VECTOR1)
+    KRATOS_CHECK_VARIABLE_KEY(BATHYMETRY)
+    KRATOS_CHECK_VARIABLE_KEY(RAIN)
+    KRATOS_CHECK_VARIABLE_KEY(MANNING)
+    KRATOS_CHECK_VARIABLE_KEY(GRAVITY)
+    KRATOS_CHECK_VARIABLE_KEY(DELTA_TIME)
+    KRATOS_CHECK_VARIABLE_KEY(DYNAMIC_TAU)
+    KRATOS_CHECK_VARIABLE_KEY(WATER_HEIGHT_UNIT_CONVERTER)
+
+    // Check that the element's nodes contain all required SolutionStepData and Degrees of freedom
+    for ( size_t i = 0; i < TNumNodes; i++ )
+    {
+        Node<3>& node = this->GetGeometry()[i];
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(VELOCITY, node)
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(HEIGHT, node)
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(PROJECTED_VECTOR1, node)
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(PROJECTED_SCALAR1, node)
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(BATHYMETRY, node)
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(RAIN, node)
+
+        KRATOS_CHECK_DOF_IN_NODE(VELOCITY_X, node)
+        KRATOS_CHECK_DOF_IN_NODE(VELOCITY_Y, node)
+        KRATOS_CHECK_DOF_IN_NODE(HEIGHT, node)
+    }
+
+    return ierr;
+
+    KRATOS_CATCH("")
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& rCurrentProcessInfo)
+{
+    KRATOS_TRY
+
+    size_t element_size = TNumNodes*3;
+    if(rResult.size() != element_size)
+        rResult.resize(element_size,false);                         // False says not to preserve existing storage!!
+
+    GeometryType& rGeom = GetGeometry();
+    int counter=0;
+    for (size_t i = 0; i < TNumNodes; i++)
+    {
+        rResult[counter++] = rGeom[i].GetDof(VELOCITY_X).EquationId();
+        rResult[counter++] = rGeom[i].GetDof(VELOCITY_Y).EquationId();
+        rResult[counter++] = rGeom[i].GetDof(HEIGHT).EquationId();
+    }
+
+    KRATOS_CATCH("")
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::GetDofList(DofsVectorType& rElementalDofList,ProcessInfo& rCurrentProcessInfo)
+{
+    KRATOS_TRY
+
+    const size_t element_size = TNumNodes*3;
+    if(rElementalDofList.size() != element_size)
+        rElementalDofList.resize(element_size);
+
+    GeometryType& rGeom = GetGeometry();
+    int counter=0;
+    for (size_t i = 0; i < TNumNodes; i++)
+    {
+        rElementalDofList[counter++] = rGeom[i].pGetDof(VELOCITY_X);
+        rElementalDofList[counter++] = rGeom[i].pGetDof(VELOCITY_Y);
+        rElementalDofList[counter++] = rGeom[i].pGetDof(HEIGHT);
+    }
+
+    KRATOS_CATCH("")
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::CalculateLocalSystem(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo)
+{
+    // Resize of the Left and Right Hand side
+    unsigned int element_size = TNumNodes*3;
+    if(rLeftHandSideMatrix.size1() != element_size)
+        rLeftHandSideMatrix.resize(element_size,element_size,false); // False says not to preserve existing storage!!
+
+    if(rRightHandSideVector.size() != element_size)
+        rRightHandSideVector.resize(element_size,false);             // False says not to preserve existing storage!!
+
+    const GeometryType& Geom = this->GetGeometry();
+
+    ElementVariables variables;
+    this->InitializeElementVariables(variables, rCurrentProcessInfo);
+
+    BoundedMatrix<double,TNumNodes, TNumNodes> NContainer;  // In this case, number of Gauss points and number of nodes coincides
+    NContainer = Geom.ShapeFunctionsValues( GeometryData::GI_GAUSS_2 );
+
+    BoundedMatrix<double,TNumNodes, 2> DN_DX;  // Shape function gradients are constant since we are using linear functions
+    array_1d<double,TNumNodes> N;
+    double Area;
+    this-> CalculateGeometry(DN_DX,Area);
+
+    for (size_t i_gauss = 0; i_gauss < TNumNodes; ++i_gauss)
+    {
+        noalias(N) = row(NContainer, i_gauss);
+
+        BuildMassMatrices(N, variables);
+        BuildGradientMatrices(N, DN_DX, variables);
+        BuildDiffusivityMatrices(DN_DX, variables);
+
+        AddInertiaTerms(rLeftHandSideMatrix, rRightHandSideVector, variables);
+        AddConvectiveTerms(rLeftHandSideMatrix, rRightHandSideVector, variables);
+        AddWaveTerms(rLeftHandSideMatrix, rRightHandSideVector, variables);
+        AddFrictionTerms(rLeftHandSideMatrix, rRightHandSideVector, variables);
+        AddStabilizationTerms(rLeftHandSideMatrix, rRightHandSideVector, variables);
+        AddSourceTerms(rLeftHandSideMatrix, rRightHandSideVector, variables);
+    }
+
+    rRightHandSideVector *= Area * variables.lumping_factor;
+    rLeftHandSideMatrix  *= Area * variables.lumping_factor;
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo)
+{
+    KRATOS_THROW_ERROR(std::logic_error,  "method not implemented" , "");
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::GetValueOnIntegrationPoints(const Variable<double>& rVariable, std::vector<double>& rValues, const ProcessInfo& rCurrentProcessInfo)
+{
+    if (rVariable == VEL_ART_VISC || rVariable == PR_ART_VISC || rVariable == RESIDUAL_NORM || rVariable == MIU)
+    {
+        for (size_t PointNumber = 0; PointNumber < 1; PointNumber++)
+            rValues[PointNumber] = double(this->GetValue(rVariable));
+    }
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::InitializeElementVariables(ElementVariables& rVariables, const ProcessInfo& rCurrentProcessInfo)
+{
+    const double delta_t = rCurrentProcessInfo[DELTA_TIME];
+    rVariables.dt_inv = 1.0 / delta_t;
+    rVariables.lumping_factor = 1.0 / static_cast<double>(TNumNodes);
+    rVariables.dyn_tau = rCurrentProcessInfo[DYNAMIC_TAU];
+    rVariables.gravity = rCurrentProcessInfo[GRAVITY_Z];
+    rVariables.manning2 = 0.0;//std::pow( GetProperties()[MANNING], 2);
+    rVariables.height_units = rCurrentProcessInfo[WATER_HEIGHT_UNIT_CONVERTER];
+
+    GeometryType& rGeom = GetGeometry();
+    for (size_t i = 0; i < TNumNodes; i++)
+    {
+        rVariables.manning2 += rGeom[i].FastGetSolutionStepValue(EQUIVALENT_MANNING);
+    }
+    rVariables.manning2 *= rVariables.lumping_factor;
+    rVariables.manning2 = std::pow(rVariables.manning2, 2);
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::CalculateGeometry(BoundedMatrix<double, TNumNodes, 2>& rDN_DX, double& rArea)
+{
+    const GeometryType& rGeom = this->GetGeometry();
+
+    // We select GI_GAUSS_1 due to we are computing at the barycenter.
+    const GeometryType::IntegrationPointsArrayType& integration_points = rGeom.IntegrationPoints(GeometryData::GI_GAUSS_1);
+    const size_t NumGPoints = integration_points.size();
+    rArea = rGeom.Area();
+    GeometryType::ShapeFunctionsGradientsType DN_DXContainer( NumGPoints );
+    rGeom.ShapeFunctionsIntegrationPointsGradients(DN_DXContainer, GeometryData::GI_GAUSS_1);
+
+    noalias( rDN_DX ) = DN_DXContainer[0];
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::GetNodalValues(ElementVariables& rVariables)
+{
+    GeometryType& rGeom = GetGeometry();
+    size_t counter = 0;
+    for (size_t i = 0; i < TNumNodes; i++)
+    {
+        rVariables.depth[counter] = 0;
+        rVariables.rain[counter]  = 0;
+        rVariables.unknown[counter]  = rGeom[i].FastGetSolutionStepValue(VELOCITY_X);
+        rVariables.proj_unk[counter]  = rGeom[i].FastGetSolutionStepValue(PROJECTED_VECTOR1_X);
+        counter++;
+
+        rVariables.depth[counter] = 0;
+        rVariables.rain[counter]  = 0;
+        rVariables.unknown[counter]  = rGeom[i].FastGetSolutionStepValue(VELOCITY_Y);
+        rVariables.proj_unk[counter]  = rGeom[i].FastGetSolutionStepValue(PROJECTED_VECTOR1_Y);
+        counter++;
+
+        rVariables.depth[counter] = rGeom[i].FastGetSolutionStepValue(BATHYMETRY) / rVariables.height_units;
+        rVariables.rain[counter]  = rGeom[i].FastGetSolutionStepValue(RAIN);
+        rVariables.unknown[counter]  = rGeom[i].FastGetSolutionStepValue(HEIGHT);
+        rVariables.proj_unk[counter]  = rGeom[i].FastGetSolutionStepValue(PROJECTED_SCALAR1);
+        counter++;
+    }
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::GetElementValues(const BoundedMatrix<double,TNumNodes, 2>& rDN_DX, ElementVariables& rVariables)
+{
+    // Initialize outputs
+    rVariables.height = 0;
+    rVariables.velocity = ZeroVector(2);
+    rVariables.height_grad = ZeroVector(2);
+
+    // integrate over the element
+    for (size_t i = 0; i < TNumNodes; i++)
+    {
+        rVariables.velocity[0] += rVariables.unknown[  + 3*i];
+        rVariables.velocity[1] += rVariables.unknown[1 + 3*i];
+        rVariables.height += rVariables.unknown[2 + 3*i];
+        rVariables.height_grad[0] += rDN_DX(i,0) * rVariables.unknown[2 + 3*i];
+        rVariables.height_grad[1] += rDN_DX(i,1) * rVariables.unknown[2 + 3*i];
+    }
+
+    rVariables.velocity *= rVariables.lumping_factor;
+    rVariables.height *= rVariables.lumping_factor * rVariables.height_units;
+    rVariables.height_grad *= rVariables.height_units;
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::ComputeStabilizationParameters(const ElementVariables& rVariables,
+                                                                    const double& rElemSize,
+                                                                    double& rTauU,
+                                                                    double& rTauH,
+                                                                    double& rKdc)
+{
+    // Initialize outputs
+    rTauU = 0;
+    rTauH = 0;
+    rKdc  = 0;
+
+    // Get element values
+    double height_grad_norm = norm_2(rVariables.height_grad);
+
+    // Compute stabilization parameters
+    bool stabilization = true;
+    double CTau = rVariables.dyn_tau;  // 0.005 ~ 0.002  // 0.002; //
+    double abs_height = std::abs(rVariables.height);
+    if (stabilization && abs_height > 1e-6)
+    {
+        rTauU = CTau/rElemSize*std::sqrt(rVariables.gravity/abs_height);
+        rTauH = CTau/rElemSize*std::sqrt(abs_height/rVariables.gravity);
+    }
+
+    // Compute discontinuity capturing parameters
+    bool discontinuity_capturing = true;
+    double gradient_threshold = 1e-6;
+    //~ double residual;
+    if (discontinuity_capturing && height_grad_norm > gradient_threshold)
+    {
+        rKdc = 0.5*0.1*rElemSize*height_grad_norm;  // Residual formulation
+    }
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::BuildMassMatrices(
+    array_1d<double,TNumNodes>& rN,
+    ElementVariables& rVariables)
+{
+    // Auxiliary definitions
+    BoundedMatrix<double,2,TNumNodes*3> N_vel    = ZeroMatrix(2,TNumNodes*3);  // Shape functions matrix (for velocity unknown)
+    BoundedMatrix<double,1,TNumNodes*3> N_height = ZeroMatrix(1,TNumNodes*3);  // Shape functions vector (for height unknown)
+
+    // Build the shape and derivatives functions at the Gauss point
+    for(size_t node = 0; node < TNumNodes; ++node)
+    {
+        // Height shape funtions
+        N_height(0, 2+node*3) = rN[node];
+        // Velocity shape functions
+        N_vel(0,   node*3) = rN[node];
+        N_vel(1, 1+node*3) = rN[node];
+    }
+    noalias(rVariables.MassMatrixVector) += prod(trans(N_vel),N_vel);       // q * h
+    noalias(rVariables.MassMatrixScalar) += prod(trans(N_height),N_height); // w * u
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::BuildGradientMatrices(
+    array_1d<double,TNumNodes>& rN,
+    BoundedMatrix<double, TNumNodes, 2>& rDN_DX,
+    ElementVariables& rVariables)
+{
+    // Auxiliary definitions
+    BoundedMatrix<double,2,TNumNodes*3> N_vel        = ZeroMatrix(2,TNumNodes*3);  // Shape functions matrix (for velocity unknown)
+    BoundedMatrix<double,1,TNumNodes*3> N_height     = ZeroMatrix(1,TNumNodes*3);  // Shape functions vector (for height unknown)
+    BoundedMatrix<double,1,TNumNodes*3> DN_DX_vel    = ZeroMatrix(1,TNumNodes*3);  // Shape functions gradients vector (for velocity unknown)
+    BoundedMatrix<double,2,TNumNodes*3> DN_DX_height = ZeroMatrix(2,TNumNodes*3);  // Shape functions gradients matrix (for height unknown)
+
+    // Build the shape and derivatives functions at the Gauss point
+    for(size_t node = 0; node < TNumNodes; ++node)
+    {
+        // Height gradient
+        DN_DX_height(0, 2+node*3) = rDN_DX(node,0);
+        DN_DX_height(1, 2+node*3) = rDN_DX(node,1);
+        // Velocity divergence
+        DN_DX_vel(0,   node*3) = rDN_DX(node,0);
+        DN_DX_vel(0, 1+node*3) = rDN_DX(node,1);
+        // Height shape funtions
+        N_height(0, 2+node*3) = rN[node];
+        // Velocity shape functions
+        N_vel(0,   node*3) = rN[node];
+        N_vel(1, 1+node*3) = rN[node];
+    }
+    noalias(rVariables.VectorDiv)  += prod(trans(N_height),DN_DX_vel); // q * div_u
+    noalias(rVariables.ScalarGrad) += prod(trans(N_vel),DN_DX_height); // w * grad_h
+}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::BuildDiffusivityMatrices(
+    BoundedMatrix<double, TNumNodes, 2>& rDN_DX,
+    ElementVariables& rVariables)
+{
+    // Some auxilary definitions
+    BoundedMatrix<double,1,TNumNodes*3> DN_DX_vel    = ZeroMatrix(1,TNumNodes*3);  // Shape functions gradients vector (for velocity unknown)
+    BoundedMatrix<double,2,TNumNodes*3> DN_DX_height = ZeroMatrix(2,TNumNodes*3);  // Shape functions gradients matrix (for height unknown)
+
+    // Build the shape and derivatives functions at the Gauss point
+    for(size_t node = 0; node < TNumNodes; ++node)
+    {
+        // Height gradient
+        DN_DX_height(0, 2+node*3) = rDN_DX(node,0);
+        DN_DX_height(1, 2+node*3) = rDN_DX(node,1);
+        // Velocity divergence
+        DN_DX_vel(0,   node*3) = rDN_DX(node,0);
+        DN_DX_vel(0, 1+node*3) = rDN_DX(node,1);
+    }
+    noalias(rVariables.VectorDiff) += prod(trans(DN_DX_vel),DN_DX_vel);       // div_w * div_u
+    noalias(rVariables.ScalarDiff) += prod(trans(DN_DX_height),DN_DX_height); // grad_q * grad_h
+}
+
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::AddInertiaTerms(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ElementVariables& rVariables)
+{}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::AddConvectiveTerms(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ElementVariables& rVariables)
+{}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::AddWaveTerms(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ElementVariables& rVariables)
+{}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::AddFrictionTerms(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ElementVariables& rVariables)
+{}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::AddStabilizationTerms(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ElementVariables& rVariables)
+{}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::AddSourceTerms(
+    MatrixType& rLeftHandSideMatrix,
+    VectorType& rRightHandSideVector,
+    ElementVariables& rVariables)
+{}
+
+
+template< size_t TNumNodes, Framework TFramework >
+void RV_SWE<TNumNodes, TFramework>::CalculateLumpedMassMatrix(BoundedMatrix<double, TNumNodes*3, TNumNodes*3>& rMassMatrix)
+{
+    const size_t element_size = 3*TNumNodes;
+    rMassMatrix  = IdentityMatrix(element_size, element_size);
+    rMassMatrix /= static_cast<double>(TNumNodes);
+}
+
+
+template class RV_SWE<3, EULERIAN_FRAMEWORK>;
+template class RV_SWE<4, EULERIAN_FRAMEWORK>;
+template class RV_SWE<3, PFEM2_FRAMEWORK>;
+template class RV_SWE<4, PFEM2_FRAMEWORK>;
+
+} // namespace Kratos
---
+++ applications/ShallowWaterApplication/custom_elements/rv_swe.h
@@ -0,0 +1,353 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+#ifndef KRATOS_RV_SWE_H_INCLUDED
+#define KRATOS_RV_SWE_H_INCLUDED
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "includes/define.h"
+#include "includes/element.h"
+#include "includes/serializer.h"
+
+namespace Kratos
+{
+///@addtogroup ShallowWaterApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/// Implementation of a linear element for shallow water problems
+template< size_t TNumNodes, Framework TFramework >
+class RV_SWE : public Element
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Counted pointer of RV_SWE
+    KRATOS_CLASS_POINTER_DEFINITION( RV_SWE );
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    RV_SWE() : Element(){}
+
+    /// Constructor using a Geometry instance
+    RV_SWE(IndexType NewId, GeometryType::Pointer pGeometry) : Element(NewId, pGeometry){}
+
+    /// Constructor using geometry and properties
+    RV_SWE(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties) : Element(NewId, pGeometry, pProperties){}
+
+    /// Destructor.
+    virtual ~ RV_SWE(){}
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    Element::Pointer Create(IndexType NewId, NodesArrayType const& ThisNodes, PropertiesType::Pointer pProperties) const override
+    {
+        return Kratos::make_intrusive< RV_SWE <TNumNodes, TFramework> >(NewId, this->GetGeometry().Create(ThisNodes), pProperties);
+    }
+
+    Element::Pointer Create(IndexType NewId, GeometryType::Pointer pGeom, PropertiesType::Pointer pProperties) const override
+    {
+        return Kratos::make_intrusive< RV_SWE <TNumNodes, TFramework> >(NewId, pGeom, pProperties);
+    }
+
+    /**
+     * It clones the selected element variables, creating a new one
+     * @param NewId the ID of the new element
+     * @param rThisNodes the nodes of the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Clone(IndexType NewId, NodesArrayType const& ThisNodes) const override
+    {
+        Element::Pointer p_new_elem = Create(NewId, this->GetGeometry().Create(ThisNodes), this->pGetProperties());
+        p_new_elem->SetData(this->GetData());
+        p_new_elem->Set(Flags(*this));
+        return p_new_elem;
+    }
+
+
+    /// Check that all required data containers are properly initialized and registered in Kratos
+    /**
+     * @return 0 if no errors are detected.
+     */
+    int Check(const ProcessInfo& rCurrentProcessInfo) override;
+
+    /// Fill given vector with the linear system row index for the element's degrees of freedom
+    /**
+     * @param rResult
+     * @param rCurrentProcessInfo
+     */
+    void EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& rCurrentProcessInfo) override;
+
+    /// Fill given array with containing the element's degrees of freedom
+    /**
+     * @param rElementalDofList
+     * @param rCurrentProcessInfo
+     */
+    void GetDofList(DofsVectorType& rElementalDofList,ProcessInfo& rCurrentProcessInfo) override;
+
+    /// Evaluate the elemental contribution to the problem for turbulent viscosity.
+    /**
+     * @param rLeftHandSideMatrix Elemental left hand side matrix
+     * @param rRightHandSideVector Elemental right hand side vector
+     * @param rCurrentProcessInfo Reference to the ProcessInfo from the ModelPart containg the element
+     */
+    void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix, VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo) override;
+
+    void CalculateRightHandSide(VectorType& rRightHandSideVector, ProcessInfo& rCurrentProcessInfo) override;
+
+    void GetValueOnIntegrationPoints(const Variable<double>& rVariable, std::vector<double>& rValues, const ProcessInfo& rCurrentProcessInfo) override;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    struct ElementVariables
+    {
+        static constexpr size_t LocalSize = TNumNodes*3;
+
+        double dt_inv;
+        double lumping_factor;
+        double dyn_tau;
+        double gravity;
+        double manning2;
+        double height_units;
+
+        double height;
+        array_1d<double,2> velocity;
+        array_1d<double,2> height_grad;
+        BoundedMatrix<double,2,2> velocity_grad;
+        double velocity_div;
+
+        array_1d<double, TNumNodes*3> depth;
+        array_1d<double, TNumNodes*3> rain;
+        array_1d<double, TNumNodes*3> unknown;
+        array_1d<double, TNumNodes*3> prev_unk;
+        array_1d<double, TNumNodes*3> proj_unk;
+
+        BoundedMatrix<double,TNumNodes*3,TNumNodes*3> MassMatrixScalar;
+        BoundedMatrix<double,TNumNodes*3,TNumNodes*3> MassMatrixVector;
+        BoundedMatrix<double,TNumNodes*3,TNumNodes*3> ScalarGrad;
+        BoundedMatrix<double,TNumNodes*3,TNumNodes*3> VectorDiv;
+        BoundedMatrix<double,TNumNodes*3,TNumNodes*3> ScalarDiff;
+        BoundedMatrix<double,TNumNodes*3,TNumNodes*3> VectorDiff;
+    };
+
+    virtual void InitializeElementVariables(ElementVariables& rVariables, const ProcessInfo& rCurrentProcessInfo);
+
+    virtual void CalculateGeometry(BoundedMatrix<double, TNumNodes, 2>& rDN_DX, double& rArea);
+
+    virtual void GetNodalValues(ElementVariables& rVariables);
+
+    virtual void GetElementValues(const BoundedMatrix<double,TNumNodes, 2>& rDN_DX, ElementVariables& rVariables);
+
+    virtual void ComputeStabilizationParameters(
+        const ElementVariables& rVariables,
+        const double& rElemSize,
+        double& rTauU,
+        double& rTauH,
+        double& rKdc);
+
+    virtual void BuildMassMatrices(
+        array_1d<double,TNumNodes>& rN,
+        ElementVariables& rVariables);
+
+    virtual void BuildGradientMatrices(
+        array_1d<double,TNumNodes>& rN,
+        BoundedMatrix<double, TNumNodes, 2>& rDN_DX,
+        ElementVariables& rVariables);
+
+    virtual void BuildDiffusivityMatrices(
+        BoundedMatrix<double, TNumNodes, 2>& rDN_DX,
+        ElementVariables& rVariables);
+
+    virtual void AddInertiaTerms(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ElementVariables& rVariables);
+    
+    virtual void AddConvectiveTerms(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ElementVariables& rVariables);
+    
+    virtual void AddWaveTerms(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ElementVariables& rVariables);
+    
+    virtual void AddFrictionTerms(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ElementVariables& rVariables);
+    
+    virtual void AddStabilizationTerms(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ElementVariables& rVariables);
+    
+    virtual void AddSourceTerms(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ElementVariables& rVariables);
+
+    virtual void CalculateLumpedMassMatrix(BoundedMatrix<double, TNumNodes*3, TNumNodes*3>& rMassMatrix);
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+
+    ///@}
+
+}; // Class RV_SWE
+
+///@}
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+///@}
+
+///@} addtogroup block
+
+}  // namespace Kratos.
+
+#endif // KRATOS_RV_SWE_H_INCLUDED  defined
