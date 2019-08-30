69e86eb802169e4703a357cb76878251b14065d4
sunethwarna <suneth.warna@gmail.com>
2019-08-30 09:08:02 +0200

    adding get nodal array

--- applications/RANSModellingApplication/custom_utilities/rans_variable_utils.h
+++ applications/RANSModellingApplication/custom_utilities/rans_variable_utils.h
@@ -290,6 +290,20 @@ public:
                 (rNodes.begin() + i_node)->FastGetSolutionStepValue(rVariable);
     }
 
+    void GetNodalArray(Vector& rNodalValues, const Element& rElement, const Variable<double>& rVariable)
+    {
+        const Geometry<ModelPart::NodeType>& r_geometry = rElement.GetGeometry();
+        std::size_t number_of_nodes = r_geometry.PointsNumber();
+
+        if (rNodalValues.size() != number_of_nodes)
+            rNodalValues.resize(number_of_nodes);
+
+        for (std::size_t i_node = 0; i_node < number_of_nodes; ++i_node)
+        {
+            rNodalValues[i_node] = r_geometry[i_node].FastGetSolutionStepValue(rVariable);
+        }
+    }
+
     void SetNodalVariables(const Vector& rValues,
                            ModelPart::NodesContainerType& rNodes,
                            const Variable<double>& rVariable)
