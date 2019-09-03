7264b62e0e7481d504a1b18a71d5771550553dc1
jcotela <jcotela@cimne.upc.edu>
2019-09-03 10:29:58 +0200

    Test for union and intersection of communicators.

--- kratos/mpi/tests/sources/test_data_communicator_manipulation.cpp
+++ kratos/mpi/tests/sources/test_data_communicator_manipulation.cpp
@@ -11,6 +11,8 @@
 //
 //
 
+#include "mpi.h"
+
 #include "includes/data_communicator.h"
 #include "mpi/includes/mpi_data_communicator.h"
 
@@ -22,7 +24,7 @@ namespace Testing {
 
 // MPI Communicator splitting /////////////////////////////////////////////////
 
-KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(MPIDataCommunicatorSplit, KratosMPICoreFastSuite)
+KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(CreateMPIDataCommunicatorSplit, KratosMPICoreFastSuite)
 {
     const DataCommunicator& r_comm = DataCommunicator::GetDefault();
     const int global_rank = r_comm.Rank();
@@ -43,6 +45,114 @@ KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(MPIDataCommunicatorSplit, KratosMPICoreFas
         // Here keys in the global_rank >= i side of the split range between 1 and (global_size-i)
         KRATOS_CHECK_EQUAL(r_split_comm.Size(), expected_size);
         KRATOS_CHECK_EQUAL(r_split_comm.Rank(), expected_rank);
+    }
+}
+
+KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(CreateMPIDataCommunicatorFromRanks, KratosMPICoreFastSuite)
+{
+    const DataCommunicator& r_comm = DataCommunicator::GetDefault();
+    const int global_size = r_comm.Size();
+
+    if (global_size > 1) // This test assumes at least two processes
+    {
+        // Create a communicator using all ranks except the first
+        std::vector<int> rank_list(global_size-1);
+        for (int i = 0; i < global_size-1; i++)
+        {
+            rank_list[i] = i+1;
+        }
+
+        // Note: communicator creation is a collective on all ranks of the "parent" communicator,
+        // even if they do not participate in the new one
+        const std::string new_comm_name("NewCommunicator");
+        const DataCommunicator& r_new_comm = MPIDataCommunicator::CreateDataCommunicatorFromRanks(
+            r_comm, rank_list, new_comm_name);
+
+        const int global_rank = r_comm.Rank();
+        if (global_rank == 0)
+        {
+            // The communicator is equal to MPI_COMM_NULL for ranks that do not participate in in
+            // Note: MPI_Rank and MPI_Size are not valid operations on ranks that do not participate in communication.
+            KRATOS_CHECK_EQUAL(MPIDataCommunicator::GetMPICommunicator(r_new_comm), MPI_COMM_NULL);
+        }
+        else
+        {
+            KRATOS_CHECK_EQUAL(r_new_comm.Rank(), global_rank-1);
+            KRATOS_CHECK_EQUAL(r_new_comm.Size(), global_size-1);
+        }
+    }
+}
+
+KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(CreateMPIDataCommunicatorUnion, KratosMPICoreFastSuite)
+{
+    const DataCommunicator& r_comm = DataCommunicator::GetDefault();
+    const int global_size = r_comm.Size();
+
+    if (global_size > 2) // This test assumes at least three processes
+    {
+        // Create a communicator using all ranks except the first and another using all ranks except the last
+        std::vector<int> all_except_first(global_size-1);
+        std::vector<int> all_except_last(global_size-1);
+        for (int i = 0; i < global_size-1; i++)
+        {
+            all_except_first[i] = i+1;
+            all_except_last[i] = i;
+        }
+
+        const DataCommunicator& r_all_except_first_comm = MPIDataCommunicator::CreateDataCommunicatorFromRanks(
+            r_comm, all_except_first, "AllExceptFirst");
+
+        const DataCommunicator& r_all_except_last_comm = MPIDataCommunicator::CreateDataCommunicatorFromRanks(
+            r_comm, all_except_last, "AllExceptLast");
+
+        const DataCommunicator& r_union_comm = MPIDataCommunicator::CreateUnionDataCommunicator(
+            r_all_except_first_comm, r_all_except_last_comm, r_comm, "UnionCommunicator");
+
+        // our union MPI comm involves all ranks in the parent communicator, so it should be defined everywhere
+        KRATOS_CHECK_NOT_EQUAL(MPIDataCommunicator::GetMPICommunicator(r_union_comm), MPI_COMM_NULL);
+
+        KRATOS_CHECK_EQUAL(r_union_comm.Rank(), r_comm.Rank());
+        KRATOS_CHECK_EQUAL(r_union_comm.Size(), global_size);
+    }
+}
+
+KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(CreateMPIDataCommunicatorIntersection, KratosMPICoreFastSuite)
+{
+    const DataCommunicator& r_comm = DataCommunicator::GetDefault();
+    const int global_size = r_comm.Size();
+
+    if (global_size > 2) // This test assumes at least three processes
+    {
+        // Create a communicator using all ranks except the first and another using all ranks except the last
+        std::vector<int> all_except_first(global_size-1);
+        std::vector<int> all_except_last(global_size-1);
+        for (int i = 0; i < global_size-1; i++)
+        {
+            all_except_first[i] = i+1;
+            all_except_last[i] = i;
+        }
+
+        const int global_rank = r_comm.Rank();
+
+        const DataCommunicator& r_all_except_first_comm = MPIDataCommunicator::CreateDataCommunicatorFromRanks(
+            r_comm, all_except_first, "_AllExceptFirst");
+
+        const DataCommunicator& r_all_except_last_comm = MPIDataCommunicator::CreateDataCommunicatorFromRanks(
+            r_comm, all_except_last, "_AllExceptLast");
+
+        const DataCommunicator& r_intersection_comm = MPIDataCommunicator::CreateIntersectionDataCommunicator(
+            r_all_except_first_comm, r_all_except_last_comm, r_comm, "IntersectionCommunicator");
+
+        if ( (global_rank == 0) || (global_rank == global_size-1 ) )
+        {
+            // The first and last ranks do not participate in the intersection communicator
+            KRATOS_CHECK_EQUAL(MPIDataCommunicator::GetMPICommunicator(r_intersection_comm), MPI_COMM_NULL);
+        }
+        else
+        {
+            KRATOS_CHECK_EQUAL(r_intersection_comm.Rank(), r_comm.Rank() - 1);
+            KRATOS_CHECK_EQUAL(r_intersection_comm.Size(), r_comm.Size() - 2);
+        }
     }
 }
 
