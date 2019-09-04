204ecb8170e5a5c3dfc575ed0c38019d9e512eba
Tobias Teschemacher <tobias.teschemacher@tum.de>
2019-09-04 17:21:01 +0200

    Adding penalty_coupling_condition

---
+++ applications/IgaApplication/custom_conditions/penalty_coupling_condition.h
@@ -0,0 +1,183 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Tobias Tescheamacher
+//                   Michael Breitenberger
+//                   Riccardo Rossi
+//
+
+#if !defined(KRATOS_PENALTY_COUPLING_CONDITION_H_INCLUDED )
+#define  KRATOS_PENALTY_COUPLING_CONDITION_H_INCLUDED
+
+// System includes
+#include "includes/define.h"
+#include "includes/condition.h"
+
+// External includes
+
+// Project includes
+#include "iga_application_variables.h"
+#include "custom_utilities/iga_flags.h"
+
+
+namespace Kratos
+{
+
+/// Penalty factor based coupling condition.
+/** This condition can be used to apply continuity between different
+*   discretizations with the penalty approach.
+*
+*   The aproach is described in https://doi.org/10.1186/s40323-018-0109-4
+*   Eq 15 ff
+*
+*   The condition needs a PENALTY as parameter in the Properties.
+*   The Geometry needs to be of type CouplingMasterSlave and must have
+*   at least one slave geometry.
+*   The continuities can be enabled or disabled with the
+*   FIX_DISPLACEMENT_{dir} flags.
+*/
+class PenaltyCouplingCondition
+    : public Condition
+{
+public:
+
+    /// Counted pointer of PenaltyCouplingCondition
+    KRATOS_CLASS_POINTER_DEFINITION(PenaltyCouplingCondition);
+
+    /// Default constructor.
+    PenaltyCouplingCondition(IndexType NewId, GeometryType::Pointer pGeometry)
+        : Condition(NewId, pGeometry)
+    {};
+
+    PenaltyCouplingCondition(IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties)
+        : Condition(NewId, pGeometry, pProperties)
+    {};
+
+    PenaltyCouplingCondition()
+        : Condition()
+    {};
+
+    /**
+    * @brief Creates a new element
+    * @param NewId The Id of the new created element
+    * @param pGeom The pointer to the geometry of the element
+    * @param pProperties The pointer to property
+    * @return The pointer to the created element
+    */
+    Condition::Pointer Create(
+        IndexType NewId,
+        GeometryType::Pointer pGeom,
+        PropertiesType::Pointer pProperties
+    ) const override
+    {
+        return Kratos::make_intrusive<PenaltyCouplingCondition>(
+            NewId, pGeom, pProperties);
+    };
+
+    Condition::Pointer Create(
+        IndexType NewId,
+        NodesArrayType const& ThisNodes,
+        PropertiesType::Pointer pProperties
+    ) const override
+    {
+        return Kratos::make_intrusive< PenaltyCouplingCondition >(
+            NewId, GetGeometry().Create(ThisNodes), pProperties);
+    };
+
+    /// Destructor.
+    virtual ~PenaltyCouplingCondition() override
+    {};
+
+
+    void CalculateRightHandSide(
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo)
+    {
+        MatrixType left_hand_side_matrix = Matrix(0, 0);
+
+        CalculateAll(left_hand_side_matrix, rRightHandSideVector,
+            rCurrentProcessInfo, false, true);
+    }
+
+    void CalculateLeftHandSide(
+        MatrixType& rLeftHandSideMatrix,
+        ProcessInfo& rCurrentProcessInfo)
+    {
+        VectorType right_hand_side_vector = Vector(0);
+
+        CalculateAll(rLeftHandSideMatrix, right_hand_side_vector,
+            rCurrentProcessInfo, true, false);
+    }
+
+    void CalculateLocalSystem(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo)
+    {
+        CalculateAll(rLeftHandSideMatrix, rRightHandSideVector,
+            rCurrentProcessInfo, true, true);
+    }
+
+    /**
+    * @brief Sets on rResult the ID's of the element degrees of freedom
+    * @param rResult The vector containing the equation id
+    * @param rCurrentProcessInfo The current process info instance
+    */
+    void EquationIdVector(
+        EquationIdVectorType& rResult,
+        ProcessInfo& rCurrentProcessInfo
+    ) override;
+
+    /**
+    * @brief Sets on rElementalDofList the degrees of freedom of the considered element geometry
+    * @param rElementalDofList The vector containing the dof of the element
+    * @param rCurrentProcessInfo The current process info instance
+    */
+    void GetDofList(
+        DofsVectorType& rElementalDofList,
+        ProcessInfo& rCurrentProcessInfo
+    ) override;
+
+    /**
+    * This functions calculates both the RHS and the LHS
+    * @param rLeftHandSideMatrix: The LHS
+    * @param rRightHandSideVector: The RHS
+    * @param rCurrentProcessInfo: The current process info instance
+    * @param CalculateStiffnessMatrixFlag: The flag to set if compute the LHS
+    * @param CalculateResidualVectorFlag: The flag to set if compute the RHS
+    */
+    void CalculateAll(
+        MatrixType& rLeftHandSideMatrix,
+        VectorType& rRightHandSideVector,
+        ProcessInfo& rCurrentProcessInfo,
+        const bool CalculateStiffnessMatrixFlag,
+        const bool CalculateResidualVectorFlag
+    );
+
+private:
+
+    friend class Serializer;
+
+    virtual void save(Serializer& rSerializer) const override
+    {
+        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, Condition);
+    }
+
+    virtual void load(Serializer& rSerializer) override
+    {
+        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, Condition);
+    }
+
+}; // Class PenaltyCouplingCondition
+
+}  // namespace Kratos.
+
+#endif // KRATOS_PENALTY_COUPLING_CONDITION_H_INCLUDED  defined 
+
+
