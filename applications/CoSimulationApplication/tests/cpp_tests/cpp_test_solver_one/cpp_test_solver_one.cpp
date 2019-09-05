de10a3ba619bdcf12562e9bba9248403581636ed
adityaghantasala <aditya.ghantasala@gmail.com>
2019-09-05 17:37:21 +0200

    adding ping and pong solvers

---
+++ applications/CoSimulationApplication/tests/cpp_tests/cpp_test_solver_one/cpp_test_solver_one.cpp
@@ -0,0 +1,36 @@
+#include "custom_io/co_sim_EMPIRE_API.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    EMPIRE_API_Connect("ping.xml");
+
+    // Setting mesh in solver 1
+    std::cout<<"Solver 1 : "<<"------------ Defining the mesh"<<std::endl;
+    int numNodes = 4;
+    int numElems = 1;
+    double nodes[] = {
+                        0,0,0,
+                        1,0,0,
+                        1,1,0,
+                        0,1,0
+                        };
+    int nodeIDs[] = {1,2,3,4};
+    int elems[] = {1,2,3,4};
+    int numNodesPerElem[] = {4};
+    std::cout<<"Solver 1 : "<< "------------ Setting the mesh"<<std::endl;
+    EMPIRE_API_sendMesh("mesh1_cpp", numNodes, numElems, nodes, nodeIDs, numNodesPerElem, elems);
+
+    double toReceive = -1;
+    std::cout<<"Solver 1 : "<< "Receiving ... \n";
+    EMPIRE_API_recvSignal_double("signal2", 1, &toReceive);
+    std::cout<<"Solver 1 : "<<"Received: "<<toReceive<<std::endl;
+
+    double toSend = 111;
+    std::cout<<"Solver 1 : "<<"Sending ... \n";
+    EMPIRE_API_sendSignal_double("signal1", 1, &toSend);
+    std::cout<<"Solver 1 : "<<"Sent: "<< toSend<<std::endl;
+
+    return (0);
+}
+
---
+++ applications/CoSimulationApplication/tests/cpp_tests/cpp_test_solver_two/cpp_test_solver_two.cpp
@@ -0,0 +1,37 @@
+#include "custom_io/co_sim_EMPIRE_API.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    EMPIRE_API_Connect("ping.xml");
+
+
+    // Setting mesh in solver 2
+    std::cout<<"Solver 2 : "<< "------------ Defining the mesh"<<std::endl;
+    int numNodes = 4;
+    int numElems = 1;
+    double nodes[] = {
+                        0,0,0,
+                        1,0,0,
+                        1,1,0,
+                        0,1,0
+                        };
+    int nodeIDs[] = {1,2,3,4};
+    int elems[] = {1,2,3,4};
+    int numNodesPerElem[] = {4};
+    std::cout<<"Solver 2 : "<< "------------ Setting the mesh"<<std::endl;
+    EMPIRE_API_sendMesh("mesh2_cpp", numNodes, numElems, nodes, nodeIDs, numNodesPerElem, elems);    
+
+    double toSend = 222;
+    std::cout<<"Solver 2 : "<<"Sending ... \n";
+    EMPIRE_API_sendSignal_double("signal2", 1, &toSend);
+    std::cout<<"Solver 2 : "<<"Sent: "<<toSend<<std::endl;
+
+    std::cout<<"Solver 2 : "<<"Receiving ... \n";
+    double toReceive = -1;
+    EMPIRE_API_recvSignal_double("signal1", 1, &toReceive);
+    std::cout<<"Solver 2 : "<<"Received: "<<toReceive<<std::endl;
+
+
+    return (0);
+}
