0600cb2a2250cb59cc45a3588705f2ee45e53c7f
adityaghantasala <aditya.ghantasala@gmail.com>
2019-09-07 14:25:15 +0200

    creating fake node and adding default settings

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/cpp_test/cpp_test_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/cpp_test/cpp_test_wrapper.py
@@ -22,24 +22,35 @@ class TestCppWrapper(CoSimulationSolverWrapper):
         super(TestCppWrapper, self).__init__(settings, solver_name)
 
         settings_defaults = KM.Parameters("""{
-            "executable_name" : ""
+            "main_model_part_name" : "",
+            "domain_size" : 2,                    
+            "executable_name"  : ""
         }""")
 
         self.settings["solver_wrapper_settings"].ValidateAndAssignDefaults(settings_defaults)
+        model_part_name = self.settings["solver_wrapper_settings"]["main_model_part_name"].GetString()
+        cs_tools.CreateMainModelPartsFromCouplingData(self.data_dict.values(), self.model, self.name)
+        cs_tools.AllocateHistoricalVariablesFromCouplingData(self.data_dict.values(), self.model, self.name)        
+        self.mp = self.model[model_part_name]
+        self.mp.CreateNewNode(1,0,0,0)
 
     def Initialize(self):
         super(TestCppWrapper, self).Initialize()
-        self.__RunExecutable()
 
     def Finalize(self):
         super(TestCppWrapper, self).Finalize()
         with self.rv.stdout, open(self.name +'.log', 'w') as file:
             for line in self.rv.stdout:
+                #print("Finalze :: ",self.name)
                 #print (line)
                 file.write(line.decode("utf-8"))
 
     def AdvanceInTime(self, current_time):
         return 1.0
+
+    def SolveSolutionStep(self):
+        super(TestCppWrapper, self).SolveSolutionStep()
+        self.__RunExecutable()      
 
     def PrintInfo(self):
         cs_tools.cs_print_info(self._ClassName(), "printing info...")
@@ -51,4 +62,5 @@ class TestCppWrapper(CoSimulationSolverWrapper):
     def __RunExecutable(self):
         command_txt = self.settings["solver_wrapper_settings"]["executable_name"].GetString()
         print("Running : ", command_txt)
-        self.rv = subprocess.Popen(command_txt, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True, start_new_session=True)
+        self.rv = subprocess.Popen(command_txt, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True, start_new_session=True)     
+        #subprocess.Popen(command_txt)     
