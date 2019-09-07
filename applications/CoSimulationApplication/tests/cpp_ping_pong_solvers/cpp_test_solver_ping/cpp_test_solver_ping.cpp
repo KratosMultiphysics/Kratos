9fddf9c869d00d9b96e8fb1179ea083af803d0d9
adityaghantasala <aditya.ghantasala@gmail.com>
2019-09-07 15:08:49 +0200

    formatting and more name chanes

--- applications/CoSimulationApplication/tests/cpp_ping_pong_solvers/cpp_test_solver_ping/cpp_test_solver_ping.cpp
+++ applications/CoSimulationApplication/tests/cpp_ping_pong_solvers/cpp_test_solver_ping/cpp_test_solver_ping.cpp
@@ -1,11 +1,12 @@
-#include "custom_io/co_sim_EMPIRE_API.h"
+//NOTE: This can use any implementation of IO which communicates with IO of CoSimulationApplication's PingPongIO.py
+//      Here the header-only version of co_sim_EMPIRE_API.h is used as it is already available.
+#include "custom_io/co_sim_EMPIRE_API.h" 
 #include <assert.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
     EMPIRE_API_Connect("ping.xml");
 
-    // Setting mesh in solver 1
     std::cout<<"Solver 1 : "<<"------------ Defining the mesh"<<std::endl;
     int numNodes = 4;
     int numElems = 1;
@@ -19,17 +20,17 @@ int main(int argc, char **argv) {
     int elems[] = {1,2,3,4};
     int numNodesPerElem[] = {4};
     std::cout<<"Solver 1 : "<< "------------ Setting the mesh"<<std::endl;
-    EMPIRE_API_sendMesh("mesh1_cpp", numNodes, numElems, nodes, nodeIDs, numNodesPerElem, elems);
+    EMPIRE_API_sendMesh("mesh_ping", numNodes, numElems, nodes, nodeIDs, numNodesPerElem, elems);
 
     double toSend = 111;
-    std::cout<<"Solver 1 : "<<"Sending ... \n";
+    std::cout<<"Solver PING : "<<"Sending ... \n";
     EMPIRE_API_sendDataField("ping_send_data", 1, &toSend);
-    std::cout<<"Solver 1 : "<<"Sent: "<< toSend<<std::endl;
+    std::cout<<"Solver PING : "<<"Sent: "<< toSend<<std::endl;
 
     double toReceive = -1;
-    std::cout<<"Solver 1 : "<< "Receiving ... \n";
+    std::cout<<"Solver PING : "<< "Receiving ... \n";
     EMPIRE_API_recvDataField("ping_recv_data", 1, &toReceive);
-    std::cout<<"Solver 1 : "<<"Received: "<<toReceive<<std::endl;
+    std::cout<<"Solver PING : "<<"Received: "<<toReceive<<std::endl;
 
     return (0);
 }
--- applications/CoSimulationApplication/tests/cpp_ping_pong_solvers/cpp_test_solver_pong/cpp_test_solver_pong.cpp
+++ applications/CoSimulationApplication/tests/cpp_ping_pong_solvers/cpp_test_solver_pong/cpp_test_solver_pong.cpp
@@ -1,3 +1,5 @@
+//NOTE: This can use any implementation of IO which communicates with IO of CoSimulationApplication's PingPongIO.py
+//      Here the header-only version of co_sim_EMPIRE_API.h is used as it is already available.
 #include "custom_io/co_sim_EMPIRE_API.h"
 #include <assert.h>
 #include <stdio.h>
@@ -5,8 +7,6 @@
 int main(int argc, char **argv) {
     EMPIRE_API_Connect("pong.xml");
 
-
-    // Setting mesh in solver 2
     std::cout<<"Solver 2 : "<< "------------ Defining the mesh"<<std::endl;
     int numNodes = 4;
     int numElems = 1;
@@ -19,18 +19,18 @@ int main(int argc, char **argv) {
     int nodeIDs[] = {1,2,3,4};
     int elems[] = {1,2,3,4};
     int numNodesPerElem[] = {4};
-    std::cout<<"Solver 2 : "<< "------------ Setting the mesh"<<std::endl;
-    EMPIRE_API_sendMesh("mesh2_cpp", numNodes, numElems, nodes, nodeIDs, numNodesPerElem, elems);    
+    std::cout<<"Solver PONG : "<< "------------ Setting the mesh"<<std::endl;
+    EMPIRE_API_sendMesh("mesh_pong", numNodes, numElems, nodes, nodeIDs, numNodesPerElem, elems);    
 
-    std::cout<<"Solver 2 : "<<"Receiving ... \n";
+    std::cout<<"Solver PONG : "<<"Receiving ... \n";
     double toReceive = -1;
     EMPIRE_API_recvDataField("pong_recv_data", 1, &toReceive);
-    std::cout<<"Solver 2 : "<<"Received: "<<toReceive<<std::endl;
+    std::cout<<"Solver PONG : "<<"Received: "<<toReceive<<std::endl;
 
     double toSend = 222;
-    std::cout<<"Solver 2 : "<<"Sending ... \n";
+    std::cout<<"Solver PONG : "<<"Sending ... \n";
     EMPIRE_API_sendDataField("pong_send_data", 1, &toSend);
-    std::cout<<"Solver 2 : "<<"Sent: "<<toSend<<std::endl;
+    std::cout<<"Solver PONG : "<<"Sent: "<<toSend<<std::endl;
 
     return (0);
 }
