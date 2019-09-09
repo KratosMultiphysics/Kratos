266beae59756868270e3b35d0558828210caa986
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-09-09 08:10:26 +0200

    Fix compilation

--- kratos/python/add_utilities_to_python.cpp
+++ kratos/python/add_utilities_to_python.cpp
@@ -291,6 +291,23 @@ void PrintTimingInformation(Timer& rTimer)
     rTimer.PrintTimingInformation();
 }
 
+template<class TContainerType>
+void InvertNormal1(
+    TContainerType& rContainer
+    )
+{
+    MortarUtilities::InvertNormal<TContainerType>(rContainer);
+}
+
+template<class TContainerType>
+void InvertNormal2(
+    TContainerType& rContainer,
+    const Flags Flag
+    )
+{
+    MortarUtilities::InvertNormal<TContainerType>(rContainer, Flag);
+}
+
 void AddUtilitiesToPython(pybind11::module &m)
 {
     namespace py = pybind11;
@@ -873,8 +890,10 @@ void AddUtilitiesToPython(pybind11::module &m)
 
     // Mortar utilities
     m.def("ComputeNodesMeanNormalModelPart",&MortarUtilities::ComputeNodesMeanNormalModelPart);
-    m.def("InvertNormal",&MortarUtilities::InvertNormal<PointerVectorSet<Element, IndexedObject>>);
-    m.def("InvertNormal",&MortarUtilities::InvertNormal<PointerVectorSet<Condition, IndexedObject>>);
+    m.def("InvertNormal",InvertNormal1<PointerVectorSet<Element, IndexedObject>>);
+    m.def("InvertNormal",InvertNormal1<PointerVectorSet<Condition, IndexedObject>>);
+    m.def("InvertNormal",InvertNormal2<PointerVectorSet<Element, IndexedObject>>);
+    m.def("InvertNormal",InvertNormal2<PointerVectorSet<Condition, IndexedObject>>);
 
     // Read materials utility
     py::class_<ReadMaterialsUtility, typename ReadMaterialsUtility::Pointer>(m, "ReadMaterialsUtility")
