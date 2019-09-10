8b818c8b54636eca010fe51d45144466d9658f2a
philbucher <philipp.bucher@tum.de>
2019-09-10 22:59:26 +0200

    adding co_sim_data_containers

---
+++ applications/CoSimulationApplication/custom_io/co_sim_data_containers.h
@@ -0,0 +1,43 @@
+// KRATOS  / ___|___/ ___|(_)_ __ ___  _   _| | __ _| |_(_) ___  _ ___
+//        | |   / _ \___ \| | '_ ` _ \| | | | |/ _` | __| |/ _ \| '_  |
+//        | |__| (_) |__) | | | | | | | |_| | | (_| | |_| | (_) | | | |
+//         \____\___/____/|_|_| |_| |_|\__,_|_|\__,_|\__|_|\___/|_| |_|
+//
+//  License:		 BSD License
+//                   license: CoSimulationApplication/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+#ifndef KRATOS_CO_SIM_DATA_CONTAINERS_H_INCLUDED
+#define KRATOS_CO_SIM_DATA_CONTAINERS_H_INCLUDED
+
+// System includes
+#include <vector>
+#include <string>
+
+namespace CoSim {
+
+namespace DataContainers {
+    struct Mesh
+    {
+        std::vector<double> node_coords;
+        std::vector<int> node_ids;
+        std::vector<int> connectivities;
+        std::vector<int> element_ids;
+        std::vector<int> element_types;
+
+        const std::string type="Mesh"; // maybe a macro can do this job => #rDataContainer
+    };
+
+    struct ConvergenceSignal
+    {
+        bool is_converged;
+
+        const std::string type="ConvergenceSignal";
+    };
+}
+
+} // namespace CoSim
+
+#endif /* KRATOS_CO_SIM_DATA_CONTAINERS_H_INCLUDED */
