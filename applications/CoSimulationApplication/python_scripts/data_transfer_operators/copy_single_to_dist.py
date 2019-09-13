5cf09aca6ead9339c0950e1a8bc83f74aa375d72
philbucher <philipp.bucher@tum.de>
2019-09-13 11:58:00 +0200

    some refactor

--- applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_dist.py
+++ applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_dist.py
@@ -12,19 +12,18 @@ class CopySingleToDist(CoSimulationDataTransferOperator):
     def TransferData(self, from_solver_data, to_solver_data, transfer_options):
         self._CheckAvailabilityTransferOptions(transfer_options)
 
-        size = to_solver_data.Size()
-        transfer_options_list = transfer_options.GetStringArray()
+        to_solver_values = to_solver_data.GetData()
         data_value = from_solver_data.GetData()
 
-        data_array = np.zeros(size)
-        for i in range(0, size, 1):
-            data_array[i] = data_value[0]
+        if not data_value.size() == 0:
+            raise Exception('Expected one value, got: {}'.format(data_value.size()))
 
-        if "swap_sign" in transfer_options_list:
-            data_value *= (-1)
-            to_solver_data.SetData(data_array)
-        else:
-            to_solver_data.SetData(data_array)
+        to_solver_values.fill(data_value[0])
+
+        if "swap_sign" in transfer_options.GetStringArray():
+            to_solver_values *= (-1)
+
+        to_solver_data.SetData(to_solver_values)
 
     @classmethod
     def _GetListAvailableTransferOptions(cls):
--- applications/CoSimulationApplication/python_scripts/data_transfer_operators/sum_dist_to_single.py
+++ applications/CoSimulationApplication/python_scripts/data_transfer_operators/sum_dist_to_single.py
@@ -8,23 +8,23 @@ import numpy as np
 def Create(settings):
     return SumAndCopyDistToSingle(settings)
 
-class SumAndCopyDistToSingle(CoSimulationDataTransferOperator):
+class SumDistToSingle(CoSimulationDataTransferOperator):
 
     def TransferData(self, from_solver_data, to_solver_data, transfer_options):
         self._CheckAvailabilityTransferOptions(transfer_options)
 
-        transfer_options_list = transfer_options.GetStringArray()
         data_array = np.array([])
         data_array = from_solver_data.GetData()
 
         value = sum(data_array)
+        if from_solver_data.IsDistributed():
+            value = from_solver_data.GetModelPart.GetCommunicator().GetDataCommunicator().SumAll(value)
         summed_data_array = np.array([value])
 
-        if "swap_sign" in transfer_options_list:
+        if "swap_sign" in transfer_options.GetStringArray():
             summed_data_array *= (-1)
-            to_solver_data.SetData(summed_data_array)
-        else:
-            to_solver_data.SetData(summed_data_array)
+
+        to_solver_data.SetData(summed_data_array)
 
     @classmethod
     def _GetListAvailableTransferOptions(cls):
