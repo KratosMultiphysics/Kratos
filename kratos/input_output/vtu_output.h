180d27c31e916335a847ec6adb5c631371787a02
philbucher <philipp.bucher@tum.de>
2019-09-13 17:32:32 +0200

    Remove most code from VTK

--- kratos/input_output/vtu_output.h
+++ kratos/input_output/vtu_output.h
@@ -14,16 +14,9 @@
 #if !defined( KRATOS_VTU_OUTPUT_H_INCLUDED )
 #define KRATOS_VTU_OUTPUT_H_INCLUDED
 
-// System includes
-#include <unordered_map>
-
-// External includes
-#include "inc/vtu11.hpp"
-
 // Project includes
 #include "includes/kratos_parameters.h"
 #include "includes/io.h"
-#include "processes/integration_values_extrapolation_to_nodes_process.h"
 
 namespace Kratos
 {
@@ -67,7 +60,7 @@ public:
     static Parameters GetDefaultParameters();
 
     /**
-     * @brief Prints mrModelPart in VTK format together with the results
+     * @brief Prints mrModelPart in VTU format together with the results
      */
     void PrintOutput();
 
@@ -94,8 +87,8 @@ public:
     }
 
     enum class FileFormat {
-        VTK_ASCII,
-        VTK_BINARY
+        VTU_ASCII,
+        VTU_BINARY
     };
 
 protected:
@@ -106,301 +99,12 @@ protected:
     VtuOutput::FileFormat mFileFormat;             /// The file format considered
 
     Parameters mOutputSettings;                    /// The configuration parameters
-    unsigned int mDefaultPrecision;                /// The default precision
-    std::unordered_map<int, int> mKratosIdToVtkId; /// The map storing the relationship between the Kratos ID and VTK ID
-    bool mShouldSwap = false;                      /// If it should swap
-
-    // pointer to object of the extrapolation from gauss point to nodes process
-    IntegrationValuesExtrapolationToNodesProcess::UniquePointer mpGaussToNodesProcess;
-
 
     ///@}
     ///@name Operations
     ///@{
 
-    /**
-     * @brief Interpolates the gauss point results on to the node using IntegrationValuesExtrapolationToNodesProcess
-     */
-    void PrepareGaussPointResults();
-
-
-
-    /**
-     * @brief Print the given rModelPart as VTK file together with the requested results
-     * @param rModelPart modelpart which is beging output
-     * @param IsSubModelPart whether the modelpart is to be treated as a submodelpart
-     * this is only relevant for the file-name
-     */
-    void WriteModelPartToFile(const ModelPart& rModelPart, const bool IsSubModelPart);
-
-    /**
-     * @brief Get the output file name based on the provided settings and the MPI rank
-     * @param rModelPart modelpart which is beging output
-     */
-    std::string GetOutputFileName(const ModelPart& rModelPart, const bool IsSubModelPart);
-
-    /**
-     * @brief Initialize function for the class
-     * @param rModelPart modelpart which is beging output
-     */
-    void Initialize(const ModelPart& rModelPart);
-
-    /**
-     * @brief Create a map with kratos nodeId as key and VTK nodeId as value. This require for VTK that the node numbers are in sequence.
-     * @param rModelPart modelpart which is beging output
-     */
-    void CreateMapFromKratosIdToVTKId(const ModelPart& rModelPart);
-
-    /**
-     * @brief Write the VTK header for the output of given rModelPart.
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteHeaderToFile(const ModelPart& rModelPart, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the mesh from rModelPart. Nodes, Elements or/and Conditions.
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteMeshToFile(const ModelPart& rModelPart, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the nodes in the rModelPart.
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteNodesToFile(const ModelPart& rModelPart, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the elements and conditions in rModelPart.
-     *        IMPORTANT : Need to write them together because of the CELLS block in VTK format
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteConditionsAndElementsToFile(const ModelPart& rModelPart, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Calculate the total number of cells which are in the provided rModelPart. = num_elements + num_conditions
-     *          It is necessary to be known prior to output
-     * @tparam TContainerType type of container.
-     * @param rContainer the container which is beging output
-     */
-    template<typename TContainerType>
-    unsigned int DetermineVtkCellListSize(const TContainerType& rContainer) const;
-
-    /**
-     * @brief Write the element/condition WriteConnectivity provided the container they are in
-     * @tparam TEntity Element/Condition
-     * @param rContainer The container containing elements/conditions
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template <typename TContainerType>
-    void WriteConnectivity(const TContainerType& rContainer, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the element/condition cell types provided the container they are in
-     * @tparam TEntity Element/Condition
-     * @param rContainer The container containing elements/conditions
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template <typename TContainerType>
-    void WriteCellType(const TContainerType& rContainer, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the results on the nodes.
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteNodalResultsToFile(const ModelPart& rModelPart, std::ofstream& rFileStream);
-
-    /**
-     * @brief Write the results/flags on the elements of rModelPart.
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteElementResultsToFile(const ModelPart& rModelPart, std::ofstream& rFileStream);
-
-    /**
-     * @brief Write the results/flags on the conditions of rModelPart.
-     * @param rModelPart modelpart which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteConditionResultsToFile(const ModelPart& rModelPart, std::ofstream& rFileStream);
-
-    /**
-     * @brief Write the results of rNodes. Synchronization is necessary because both local
-     * and ghost-node-values are printed in MPI and can overlap!
-     * @param rVariableName name of the result to be written.
-     * @param rNodes the nodes which is beging output
-     * @param IsHistoricalValue whether the values are historical or not
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    void WriteNodalContainerResults(
-        const std::string& rVariableName,
-        const ModelPart::NodesContainerType& rNodes,
-        const bool IsHistoricalValue,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the variable results of rContainer (Elements or Conditions).
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @param rVariableName name of the result to be written.
-     * @param rContainer the container which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType>
-    void WriteGeometricalContainerResults(const std::string& rVariableName,
-                                          const TContainerType& rContainer,
-                                          std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Writes scalar results of rNodes. Wraps the necessary synchronization-calls
-     * @param rNodes the nodes which is beging output
-     * @param rVariable Variable of the result to be written.
-     * @param IsHistoricalValue whether the values are historical or not
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<class TVarType>
-    void WriteNodalScalarValues(
-        const ModelPart::NodesContainerType& rNodes,
-        const TVarType& rVariable,
-        const bool IsHistoricalValue,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Writes vector results of rNodes. Wraps the necessary synchronization-calls
-     * @param rNodes the nodes which is beging output
-     * @param rVariable Variable of the result to be written.
-     * @param IsHistoricalValue whether the values are historical or not
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<class TVarType>
-    void WriteNodalVectorValues(
-        const ModelPart::NodesContainerType& rNodes,
-        const TVarType& rVariable,
-        const bool IsHistoricalValue,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the scalar-historical variable results of rContainer.
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @tparam TVarType The type of Variable of the entity on which the results are to be written
-     * @param rContainer the container which is beging output
-     * @param rVariable Variable of the result to be written.
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType, class TVarType>
-    void WriteScalarSolutionStepVariable(
-        const TContainerType& rContainer,
-        const TVarType& rVariable,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the vector-historical variable results of rContainer.
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @tparam TVarType The type of Variable of the entity on which the results are to be written
-     * @param rContainer the container which is beging output
-     * @param rVariable Variable of the result to be written.
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType, class TVarType>
-    void WriteVectorSolutionStepVariable(
-        const TContainerType& rContainer,
-        const TVarType& rVariable,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the flag results of rContainer.
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @param rContainer the container which is beging output
-     * @param Flag The flag to be considered to be written
-     * @param rFlagName The name of the flag that will appear on the post file
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType>
-    void WriteFlagContainerVariable(
-        const TContainerType& rContainer,
-        const Flags Flag,
-        const std::string& rFlagName,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the scalar-nonhistorical variable results of rContainer.
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @tparam TVarType The type of Variable of the entity on which the results are to be written
-     * @param rContainer the container which is beging output
-     * @param rVariable Variable of the result to be written.
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType, class TVarType>
-    void WriteScalarContainerVariable(
-        const TContainerType& rContainer,
-        const TVarType& rVariable,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the vector-nonhistorical variable results of rContainer.
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @tparam TVarType The type of Variable of the entity on which the results are to be written
-     * @param rContainer the container which is beging output
-     * @param rVariable Variable of the result to be written.
-     * @param VtkDataType type of vtk data
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType, class TVarType>
-    void WriteVectorContainerVariable(
-        const TContainerType& rContainer,
-        const TVarType& rVariable,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the scalar value to the file provided, takes care of binary and ascii formats
-     * @tparam TData The type of data to be written to the file stream rFileStream
-     * @param rData data to be written
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template <typename TData>
-    void WriteScalarDataToFile(const TData& rData, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Write the vector values to the file provided, takes care of binary and ascii formats
-     * @tparam TData The type of data to be written to the file stream rFileStream
-     * @param rData data to be written
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template <typename TData>
-    void WriteVectorDataToFile(const TData& rData, std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Only used in the binary format output.
-     * This function forces the big endian format for the input binary stream
-     * @param pBytes bytes on which the big endian format is to be applied
-     */
-    void ForceBigEndian(unsigned char* pBytes) const;
-
-    ///@}
-
-private:
-    ///@name Operations
-    ///@{
-
-    /**
-     * @brief Prints the Properties Id as an integer variable in each element/condition
-     * @tparam TContainerType The type of container of the entity on which the results are to be written
-     * @param rContainer the container which is beging output
-     * @param rFileStream the file stream to which data is to be written.
-     */
-    template<typename TContainerType>
-    void WritePropertiesIdsToFile(
-        const TContainerType& rContainer,
-        std::ofstream& rFileStream) const;
-
-    /**
-     * @brief Print the given rModelPart as VTK file together with the requested results (Only for model parts without nodes)
-     * @param rModelPart modelpart which is beging output
-     */
-    void WriteModelPartWithoutNodesToFile(ModelPart& rModelPart);
+    std::string GetOutputFileName(const ModelPart& rModelPart) const;
 
     ///@}
 };
