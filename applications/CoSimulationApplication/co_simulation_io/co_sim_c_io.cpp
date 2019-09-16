6d8b7c4f4e07e599da137765e67d1029d3e914a1
Philipp <philipp.bucher@tum.de>
2019-09-16 12:12:23 +0200

    minor in C-Interface

--- applications/CoSimulationApplication/co_simulation_io/co_sim_c_io.cpp
+++ applications/CoSimulationApplication/co_simulation_io/co_sim_c_io.cpp
@@ -1,4 +1,13 @@
+// KRATOS  / ___|___/ ___|(_)_ __ ___  _   _| | __ _| |_(_) ___  _ ___
+//        | |   / _ \___ \| | '_ ` _ \| | | | |/ _` | __| |/ _ \| '_  |
+//        | |__| (_) |__) | | | | | | | |_| | | (_| | |_| | (_) | | | |
+//         \____\___/____/|_|_| |_| |_|\__,_|_|\__,_|\__|_|\___/|_| |_|
+//
+//  License:		 BSD License
+//                   license: CoSimulationApplication/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
 
-
-
+// Project includes
 #include "co_sim_io.h"
