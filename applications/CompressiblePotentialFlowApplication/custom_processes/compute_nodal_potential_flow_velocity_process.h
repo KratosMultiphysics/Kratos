d038daf0d2b442bf67e025c97787893677cb8c50
marcnunezc <marcnc94@gmail.com>
2019-09-19 14:25:18 +0200

    header format

--- applications/CompressiblePotentialFlowApplication/custom_processes/compute_nodal_potential_flow_velocity_process.h
+++ applications/CompressiblePotentialFlowApplication/custom_processes/compute_nodal_potential_flow_velocity_process.h
@@ -1,11 +1,11 @@
 //    |  /           |
 //    ' /   __| _` | __|  _ \   __|
-//    . \  |   (   | |   (   |\__ \.
+//    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Marc Nunez, based on R.Rossi and V.Mataix work
 //
