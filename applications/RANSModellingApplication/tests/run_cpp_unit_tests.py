4329ac37d039591d6d58a296415e6686e565e24f
sunethwarna <suneth.warna@gmail.com>
2019-09-23 16:54:48 +0200

    adding failed tests output only

--- applications/RANSModellingApplication/tests/run_cpp_unit_tests.py
+++ applications/RANSModellingApplication/tests/run_cpp_unit_tests.py
@@ -2,7 +2,7 @@ from KratosMultiphysics import *
 from KratosMultiphysics.RANSModellingApplication import *
 
 def run():
-    Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
+    Tester.SetVerbosity(Tester.Verbosity.FAILED_TESTS_OUTPUTS)
     Tester.RunTestSuite("KratosRANSTestSuite")
 
 if __name__ == '__main__':
