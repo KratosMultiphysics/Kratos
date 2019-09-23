6e32ab1e9196dad5a1f3f40397acf9ab409faa74
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-09-23 16:51:02 +0200

    CREATING variable

--- applications/FemToDemApplication/custom_processes/regenerate_pfem_pressure_conditions_process.cpp
+++ applications/FemToDemApplication/custom_processes/regenerate_pfem_pressure_conditions_process.cpp
@@ -28,31 +28,20 @@ RegeneratePfemPressureConditionsProcess<TDim>::RegeneratePfemPressureConditionsP
 template <SizeType TDim>
 void RegeneratePfemPressureConditionsProcess<TDim>::Execute()
 {
-    int max_id;
-    this->GetMaximumConditionIdOnSubmodelPart(max_id);
-    const auto it_cond_begin = mrModelPart.ConditionsBegin();
-    ModelPart::PropertiesType::Pointer pProperties = it_cond_begin->pGetProperties();
+    // We search the neighbours for the generation of line loads
+    auto find_neigh = FindElementalNeighboursProcess(mrModelPart, TDim, 5);
+    find_neigh.Execute();
+    auto& r_process_info = mrModelPart.GetProcessInfo();
 
-    std::vector<IndexType> condition_nodes_id(3);
-    auto &elem = mrModelPart.GetElement(194);
-    condition_nodes_id[0] = elem.GetGeometry()[0].Id();
-    condition_nodes_id[1] = elem.GetGeometry()[1].Id();
-    condition_nodes_id[2] = elem.GetGeometry()[2].Id();
-    max_id++;
-    max_id = 1000000;
-    mrModelPart.CreateSubModelPart("dummy_pressure");
-    auto& r_sub_model = mrModelPart.GetSubModelPart("dummy_pressure");
-
-    r_sub_model.AddNodes(condition_nodes_id);
-    // We create the Line Load Condition
-    const auto p_pressure_condition = r_sub_model.CreateNewCondition(
-                                        "SurfaceLoadCondition3D3N",
-                                        max_id,
-                                        condition_nodes_id,
-                                        pProperties, 0);
-    p_pressure_condition->SetValue(POSITIVE_FACE_PRESSURE, -1000);
-    // Adding the conditions to the computing model part
-    mrModelPart.GetSubModelPart("computing_domain").AddCondition(p_pressure_condition);
+    // Remove previous line loads-> Only the 1st iteration
+    if (r_process_info[PFEM_PRESSURE_ITERATION] == 1) {
+        // We fill the properties vectors to be reassigned afterwards
+        this->SavePreviousProperties();
+        this->RemovePreviousLineLoads();
+        this->ResetFlagOnElements();
+    }
+    // Generate the new ones
+    this->CreateNewConditions();
 }
 
 /***********************************************************************************/
@@ -66,6 +55,40 @@ void RegeneratePfemPressureConditionsProcess<TDim>::GetMaximumConditionIdOnSubmo
     for (auto it_cond = mrModelPart.ConditionsBegin(); it_cond != mrModelPart.ConditionsEnd(); it_cond++) {
         rMaximumConditionId = (((*it_cond)).Id() > rMaximumConditionId) ? ((*it_cond)).Id() : rMaximumConditionId;
     }
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+template <>
+void ExtendPressureConditionProcess<3>::CreatePressureLoads(
+    const int Id1,
+    const int Id2,
+    const int Id3,
+	ModelPart::ElementsContainerType::ptr_iterator itElem,
+	ModelPart& rSubModelPart,
+    ModelPart::PropertiesType::Pointer pProperties,
+    int& rMaximumConditionId
+    )
+{
+    auto& r_geom = (*itElem)->GetGeometry();
+    std::vector<IndexType> condition_nodes_id(3);
+    condition_nodes_id[0] = r_geom[Id1].Id();
+    condition_nodes_id[1] = r_geom[Id2].Id();
+    condition_nodes_id[2] = r_geom[Id3].Id();
+    rMaximumConditionId++;
+
+    // Adding the nodes to the SubModelPart
+	rSubModelPart.AddNodes(condition_nodes_id);
+
+    // We create the Line Load Condition
+    const auto p_pressure_condition = rSubModelPart.CreateNewCondition(
+                                        "SurfaceLoadCondition3D3N",
+                                        rMaximumConditionId,
+                                        condition_nodes_id,
+                                        pProperties, 0);
+
+    // Adding the conditions to the computing model part
+    mrModelPart.GetSubModelPart("computing_domain").AddCondition(p_pressure_condition); 
 }
 
 /***********************************************************************************/
