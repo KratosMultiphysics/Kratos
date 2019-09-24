ec74fd091546fae0132a0d805351cde58e1de23e
adityaghantasala <aditya.ghantasala@gmail.com>
2019-09-24 14:03:22 +0200

    marking unformulated

--- applications/ChimeraApplication/custom_processes/apply_chimera_process.h
+++ applications/ChimeraApplication/custom_processes/apply_chimera_process.h
@@ -176,8 +176,8 @@ public:
         if(mReformulateEveryStep)
         {
             mrMainModelPart.RemoveMasterSlaveConstraintsFromAllLevels(TO_ERASE);
-            mIsFormulated = false;
         }
+        mIsFormulated = false;
     }
 
     virtual std::string Info() const override
