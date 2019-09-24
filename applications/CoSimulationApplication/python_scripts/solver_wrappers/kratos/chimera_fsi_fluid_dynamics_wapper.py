1953ce021de6104e72c4c4e447ee08a5e2771431
adityaghantasala <aditya.ghantasala@gmail.com>
2019-09-24 14:04:42 +0200

    changes to co sim wrapper to fix errors

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/chimera_fsi_fluid_dynamics_wapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/chimera_fsi_fluid_dynamics_wapper.py
@@ -3,21 +3,14 @@ from __future__ import print_function, absolute_import, division  # makes Kratos
 # Importing the Kratos Library
 import KratosMultiphysics
 import KratosMultiphysics.FluidDynamicsApplication
-import KratosMultiphysics.ChimeraApplication
-import KratosMultiphysics.ChimeraApplication as kchim
-try:
-    import KratosMultiphysics.MeshMovingApplication
-    KratosMultiphysics.Logger.PrintInfo("MeshMovingApplication", "succesfully imported")
-except ImportError:
-    KratosMultiphysics.Logger.PrintInfo("MeshMovingApplication", "not imported")
-
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.solver_wrappers.kratos import kratos_base_wrapper
 
 # Other imports
-import KratosMultiphysics.ChimeraApplication.fluid_chimera_analysis import FluidChimeraAnalysis
+from KratosMultiphysics.FluidDynamicsApplication.fluid_dynamics_analysis import FluidDynamicsAnalysis
+from KratosMultiphysics.ChimeraApplication.fluid_chimera_analysis import FluidChimeraAnalysis
 
-def CreateSolver(cosim_solver_settings, level):
+def Create(cosim_solver_settings, level):
     return ChimeraFsiFluidDynamicsWrapper(cosim_solver_settings, level)
 
 class ChimeraFsiFluidDynamicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
@@ -25,9 +18,9 @@ class ChimeraFsiFluidDynamicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
         return FluidChimeraAnalysis(self.model, self.project_parameters)
 
     def SolveSolutionStep(self):
-        self._analysis_stage.chimera_process.InitializeSolutionStep()
-        self._analysis_stage._GetSolver().SolveSolutionStep()
-        super(KratosBaseWrapper, self).SolveSolutionStep()
+        self._analysis_stage.FinalizeSolutionStep()
+        self._analysis_stage.InitializeSolutionStep()
+        super(ChimeraFsiFluidDynamicsWrapper, self).SolveSolutionStep()
 
     def _GetParallelType(self):
         return self.project_parameters["problem_data"]["parallel_type"].GetString()
