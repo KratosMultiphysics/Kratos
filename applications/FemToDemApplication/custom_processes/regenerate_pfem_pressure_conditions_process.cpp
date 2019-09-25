e691041c3ecdc1042f736edd1d25238fee1e4d1b
AcornejoLaptop <alejandro.cornejo23@gmail.com>
2019-09-25 17:58:07 +0200

    avoid comilation error in linux

--- applications/FemToDemApplication/custom_processes/regenerate_pfem_pressure_conditions_process.cpp
+++ applications/FemToDemApplication/custom_processes/regenerate_pfem_pressure_conditions_process.cpp
@@ -25,39 +25,6 @@ RegeneratePfemPressureConditionsProcess<TDim>::RegeneratePfemPressureConditionsP
 
 /***********************************************************************************/
 /***********************************************************************************/
-template <SizeType TDim>
-void RegeneratePfemPressureConditionsProcess<TDim>::Execute()
-{
-    // We search the neighbours for the generation of line loads
-    auto find_neigh = FindElementalNeighboursProcess(mrModelPart, TDim, 5);
-    find_neigh.Execute();
-    auto& r_process_info = mrModelPart.GetProcessInfo();
-
-    if (!mrModelPart.HasSubModelPart("PFEMPressureConditions")) {
-		mrModelPart.CreateSubModelPart("PFEMPressureConditions");
-    }
-    // Remove previous line loads-> Only the 1st iteration
-    this->RemovePreviousPressureLoads();
-
-    // Generate the new ones
-    this->CreateNewConditions();
-}
-
-/***********************************************************************************/
-/***********************************************************************************/
-template <SizeType TDim>
-void RegeneratePfemPressureConditionsProcess<TDim>::GetMaximumConditionIdOnSubmodelPart(
-    int& rMaximumConditionId
-)
-{
-    rMaximumConditionId = 0;
-    for (auto it_cond = mrModelPart.ConditionsBegin(); it_cond != mrModelPart.ConditionsEnd(); it_cond++) {
-        rMaximumConditionId = (((*it_cond)).Id() > rMaximumConditionId) ? ((*it_cond)).Id() : rMaximumConditionId;
-    }
-}
-
-/***********************************************************************************/
-/***********************************************************************************/
 template <>
 void RegeneratePfemPressureConditionsProcess<3>::CreatePressureLoads(
     const int Id1,
@@ -88,6 +55,95 @@ void RegeneratePfemPressureConditionsProcess<3>::CreatePressureLoads(
 
     // Adding the conditions to the computing model part
     mrModelPart.GetSubModelPart("computing_domain").AddCondition(p_pressure_condition); 
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+template<>
+void RegeneratePfemPressureConditionsProcess<3>::GeneratePressureLoads3WetNodes(
+    const int NonWetLocalIdNode,
+    int& rMaximumConditionId,
+    ElementIterator itElem
+    )
+{
+    auto& r_sub_model_part = mrModelPart.GetSubModelPart("PFEMPressureConditions");
+    auto it_cond = mrModelPart.ConditionsBegin();
+    ModelPart::PropertiesType::Pointer p_properties = it_cond->pGetProperties();
+    auto& r_geom = (itElem)->GetGeometry();
+
+    const IndexType id_1 = (NonWetLocalIdNode == 0) ? 0 : (NonWetLocalIdNode == 1) ? 1 : (NonWetLocalIdNode == 2) ? 2 : 3;
+    const IndexType id_2 = (NonWetLocalIdNode == 0) ? 3 : (NonWetLocalIdNode == 1) ? 0 : (NonWetLocalIdNode == 2) ? 3 : 0;
+    const IndexType id_3 = (NonWetLocalIdNode == 0) ? 2 : (NonWetLocalIdNode == 1) ? 2 : (NonWetLocalIdNode == 2) ? 1 : 1;
+    const IndexType id_4 = (NonWetLocalIdNode == 0) ? 1 : (NonWetLocalIdNode == 1) ? 3 : (NonWetLocalIdNode == 2) ? 0 : 2;
+
+    // We only create pressure loads when the surface is skin
+    auto& r_elem_neigb = (itElem)->GetValue(NEIGHBOUR_ELEMENTS);
+    if (r_elem_neigb[NonWetLocalIdNode].Id() == (itElem)->Id()) {
+        this->CreatePressureLoads(id_2, id_3, id_4, itElem, r_sub_model_part, p_properties, rMaximumConditionId);  
+    }
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+template<>
+void RegeneratePfemPressureConditionsProcess<3>::GeneratePressureLoads4WetNodes(
+    int& rMaximumConditionId,
+    ElementIterator itElem
+    )
+{
+    auto& r_sub_model_part = mrModelPart.GetSubModelPart("PFEMPressureConditions");
+    auto it_cond = mrModelPart.ConditionsBegin();
+    ModelPart::PropertiesType::Pointer p_properties = it_cond->pGetProperties();
+    const int id = (itElem)->Id();
+
+    // We only create pressure loads when the surface is skin
+    auto& r_elem_neigb = (itElem)->GetValue(NEIGHBOUR_ELEMENTS);
+
+    // Loop over the faces
+    for (int i = 0; i < r_elem_neigb.size(); i++) {
+        if (r_elem_neigb[i].Id() == id) { // it is skin face
+            // The associated node to this skin face is excluded
+            const int excluded_local_id_node = i;
+
+            const IndexType id_1 = (excluded_local_id_node == 0) ? 3 : (excluded_local_id_node == 1) ? 0 : (excluded_local_id_node == 2) ? 3 : 0;
+            const IndexType id_2 = (excluded_local_id_node == 0) ? 2 : (excluded_local_id_node == 1) ? 2 : (excluded_local_id_node == 2) ? 1 : 1;
+            const IndexType id_3 = (excluded_local_id_node == 0) ? 1 : (excluded_local_id_node == 1) ? 3 : (excluded_local_id_node == 2) ? 0 : 2;
+            this->CreatePressureLoads(id_1, id_2, id_3, itElem, r_sub_model_part, p_properties, rMaximumConditionId);
+        }
+    }
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+template <SizeType TDim>
+void RegeneratePfemPressureConditionsProcess<TDim>::Execute()
+{
+    // We search the neighbours for the generation of line loads
+    auto find_neigh = FindElementalNeighboursProcess(mrModelPart, TDim, 5);
+    find_neigh.Execute();
+    auto& r_process_info = mrModelPart.GetProcessInfo();
+
+    if (!mrModelPart.HasSubModelPart("PFEMPressureConditions")) {
+		mrModelPart.CreateSubModelPart("PFEMPressureConditions");
+    }
+    // Remove previous line loads-> Only the 1st iteration
+    this->RemovePreviousPressureLoads();
+
+    // Generate the new ones
+    this->CreateNewConditions();
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+template <SizeType TDim>
+void RegeneratePfemPressureConditionsProcess<TDim>::GetMaximumConditionIdOnSubmodelPart(
+    int& rMaximumConditionId
+)
+{
+    rMaximumConditionId = 0;
+    for (auto it_cond = mrModelPart.ConditionsBegin(); it_cond != mrModelPart.ConditionsEnd(); it_cond++) {
+        rMaximumConditionId = (((*it_cond)).Id() > rMaximumConditionId) ? ((*it_cond)).Id() : rMaximumConditionId;
+    }
 }
 
 /***********************************************************************************/
@@ -154,63 +210,8 @@ void RegeneratePfemPressureConditionsProcess<3>::CreateNewConditions()
 
 /***********************************************************************************/
 /***********************************************************************************/
-template<>
-void RegeneratePfemPressureConditionsProcess<3>::GeneratePressureLoads3WetNodes(
-    const int NonWetLocalIdNode,
-    int& rMaximumConditionId,
-    ElementIterator itElem
-    )
-{
-    auto& r_sub_model_part = mrModelPart.GetSubModelPart("PFEMPressureConditions");
-    auto it_cond = mrModelPart.ConditionsBegin();
-    ModelPart::PropertiesType::Pointer p_properties = it_cond->pGetProperties();
-    auto& r_geom = (itElem)->GetGeometry();
-
-    const IndexType id_1 = (NonWetLocalIdNode == 0) ? 0 : (NonWetLocalIdNode == 1) ? 1 : (NonWetLocalIdNode == 2) ? 2 : 3;
-    const IndexType id_2 = (NonWetLocalIdNode == 0) ? 3 : (NonWetLocalIdNode == 1) ? 0 : (NonWetLocalIdNode == 2) ? 3 : 0;
-    const IndexType id_3 = (NonWetLocalIdNode == 0) ? 2 : (NonWetLocalIdNode == 1) ? 2 : (NonWetLocalIdNode == 2) ? 1 : 1;
-    const IndexType id_4 = (NonWetLocalIdNode == 0) ? 1 : (NonWetLocalIdNode == 1) ? 3 : (NonWetLocalIdNode == 2) ? 0 : 2;
-
-    // We only create pressure loads when the surface is skin
-    auto& r_elem_neigb = (itElem)->GetValue(NEIGHBOUR_ELEMENTS);
-    if (r_elem_neigb[NonWetLocalIdNode].Id() == (itElem)->Id()) {
-        this->CreatePressureLoads(id_2, id_3, id_4, itElem, r_sub_model_part, p_properties, rMaximumConditionId);  
-    }
-}
-
-/***********************************************************************************/
-/***********************************************************************************/
-template<>
-void RegeneratePfemPressureConditionsProcess<3>::GeneratePressureLoads4WetNodes(
-    int& rMaximumConditionId,
-    ElementIterator itElem
-    )
-{
-    auto& r_sub_model_part = mrModelPart.GetSubModelPart("PFEMPressureConditions");
-    auto it_cond = mrModelPart.ConditionsBegin();
-    ModelPart::PropertiesType::Pointer p_properties = it_cond->pGetProperties();
-    const int id = (itElem)->Id();
-
-    // We only create pressure loads when the surface is skin
-    auto& r_elem_neigb = (itElem)->GetValue(NEIGHBOUR_ELEMENTS);
-
-    // Loop over the faces
-    for (int i = 0; i < r_elem_neigb.size(); i++) {
-        if (r_elem_neigb[i].Id() == id) { // it is skin face
-            // The associated node to this skin face is excluded
-            const int excluded_local_id_node = i;
-
-            const IndexType id_1 = (excluded_local_id_node == 0) ? 3 : (excluded_local_id_node == 1) ? 0 : (excluded_local_id_node == 2) ? 3 : 0;
-            const IndexType id_2 = (excluded_local_id_node == 0) ? 2 : (excluded_local_id_node == 1) ? 2 : (excluded_local_id_node == 2) ? 1 : 1;
-            const IndexType id_3 = (excluded_local_id_node == 0) ? 1 : (excluded_local_id_node == 1) ? 3 : (excluded_local_id_node == 2) ? 0 : 2;
-            this->CreatePressureLoads(id_1, id_2, id_3, itElem, r_sub_model_part, p_properties, rMaximumConditionId);
-        }
-    }
-}
-
-/***********************************************************************************/
-/***********************************************************************************/
 
 template class RegeneratePfemPressureConditionsProcess<3>;
+template class RegeneratePfemPressureConditionsProcess<2>;
 
 }  // namespace Kratos
