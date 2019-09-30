7c64b25794f81854f37c360d6f81bcaa4ff7e0c6
Hubert Balcerzak <hubertbalc@gmail.com>
2019-10-01 00:30:37 +0200

    linux scripts

---
+++ scripts/wheels/linux/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import os
+
+#
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+class BinaryDistribution(setuptools.Distribution):
+    def has_ext_modules(foo):
+        return True
+
+
+class EmptyListWithLength(list):
+    def __len__(self):
+        return 1
+
+setuptools.setup(
+    name="KratosMultiphysicsWheelTest",
+    version="0.1",
+    author="Kratos Multiphisics",
+    description="KRATOS Multiphysics (\"Kratos\") is a framework for building parallel, multi-disciplinary simulation software, aiming at modularity, extensibility, and high performance. Kratos is written in C++, and counts with an extensive Python interface.",
+    url="https://github.com/KratosMultiphysics/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    package_data={
+        'KratosMultiphysics': list(map(lambda x: ".libs/" + x, os.listdir("KratosMultiphysics/.libs")))
+            },
+    python_requires='>=3.5',
+    ext_modules=EmptyListWithLength(),
+    distclass=BinaryDistribution
+)
+
