ed92fc5dd21895437e6fce1feb13c6cebc2124b4
Riccardo Rossi <rrossi@cimne.upc.edu>
2019-10-05 18:08:05 +0200

    this PR makes global pointers to occupy exactly 1 ptr if KRATOS_USING_MPI is NOT defined

--- kratos/includes/global_pointer.h
+++ kratos/includes/global_pointer.h
@@ -28,7 +28,9 @@ private:
 
   /// Pointer to the data
   TDataType * mDataPointer;
+#ifdef KRATOS_USING_MPI
   int mRank;
+#endif
 
 public:
 
@@ -40,7 +42,9 @@ public:
 	*/
 	GlobalPointer() {
     mDataPointer = nullptr;
+#ifdef KRATOS_USING_MPI
     this->mRank = 0;
+#endif
   };
 
   /** Constructor by Data
@@ -55,8 +59,11 @@ public:
    */
   GlobalPointer(TDataType * DataPointer, int Rank = 0)
     : mDataPointer(DataPointer)
-    , mRank(Rank) {
-  }
+#ifdef KRATOS_USING_MPI
+    , mRank(Rank)
+#endif
+     {
+     }
 
   /** Constructor by Kratos::shared_ptr
    * Constructor by Kratos::shared_ptr
@@ -64,7 +71,10 @@ public:
    */
   GlobalPointer(Kratos::shared_ptr<TDataType> DataPointer, int Rank = 0)
     : mDataPointer(DataPointer.get())
-    , mRank(Rank) {
+#ifdef KRATOS_USING_MPI
+    , mRank(Rank) 
+#endif
+  {
   }
 
   /** Constructor by Kratos::shared_ptr
@@ -73,7 +83,10 @@ public:
    */
   GlobalPointer(Kratos::intrusive_ptr<TDataType>& DataPointer, int Rank = 0)
     : mDataPointer(DataPointer.get())
-    , mRank(Rank) {
+#ifdef KRATOS_USING_MPI
+    , mRank(Rank) 
+#endif
+  {
   }
 
   /** Constructor by Kratos::weak_ptr
@@ -82,7 +95,10 @@ public:
    */
   GlobalPointer(Kratos::weak_ptr<TDataType> DataPointer, int Rank = 0)
     : mDataPointer(DataPointer.lock().get())
-    , mRank(Rank) {
+#ifdef KRATOS_USING_MPI
+    , mRank(Rank) 
+#endif
+  {
   }
 
   /** Constructor by std::unique_ptr
@@ -97,7 +113,10 @@ public:
    */
   GlobalPointer(const GlobalPointer & rOther)
     : mDataPointer(rOther.mDataPointer)
-    , mRank(rOther.mRank) {
+#ifdef KRATOS_USING_MPI
+    , mRank(rOther.mRank)
+#endif
+  {
   }
 
   /** Move constructor
@@ -106,7 +125,10 @@ public:
    */
   GlobalPointer(const GlobalPointer && rOther)
     : mDataPointer(std::move(rOther.mDataPointer))
-    , mRank(std::move(rOther.mRank)) {
+#ifdef KRATOS_USING_MPI
+    , mRank(std::move(rOther.mRank))
+#endif
+  {
   }
 
   /** Assignment Operator
@@ -114,7 +136,9 @@ public:
    */
   GlobalPointer & operator=(const GlobalPointer & rOther) {
     mDataPointer = rOther.mDataPointer;
+#ifdef KRATOS_USING_MPI
     mRank = rOther.mRank;
+#endif
 
     return *this;
   }
@@ -179,7 +203,11 @@ public:
    * @return rank of the global pointer data or 0
    */
   int GetRank() const {
+#ifdef KRATOS_USING_MPI
     return this->mRank;
+#else
+    return 0;
+#endif
   }
 
   private:
@@ -198,9 +226,9 @@ public:
       {
         rSerializer.save("D", mDataPointer);
       }
-
+#ifdef KRATOS_USING_MPI
       rSerializer.save("R", mRank);
-
+#endif
   }
 
   void load(Serializer& rSerializer)
@@ -215,9 +243,9 @@ public:
       {
         rSerializer.load("D", mDataPointer);
       }
-
+#ifdef KRATOS_USING_MPI
       rSerializer.load("R", mRank);
-
+#endif
   }
 
 };
@@ -233,7 +261,9 @@ struct GlobalPointerHasher
     {
         std::size_t seed = 0;
         HashCombine(seed, &(*pGp) );
+#ifdef KRATOS_USING_MPI
         HashCombine(seed, pGp.GetRank());
+#endif
         return seed;
     }
 };
@@ -252,7 +282,11 @@ struct GlobalPointerComparor
      */
     bool operator()(const GlobalPointer<TDataType>& pGp1, const GlobalPointer<TDataType>& pGp2) const
     {
+#ifdef KRATOS_USING_MPI
         return ( &(*pGp1) == &(*pGp2)  &&  pGp1.GetRank() == pGp2.GetRank()  );
+#else
+        return ( &(*pGp1) == &(*pGp2) );
+#endif
     }
 };
 
