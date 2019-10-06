bf55e1b2077b506489611ea61276cbddcf9452b4
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-10-06 02:19:24 +0200

    Minor

--- kratos/tests/cpp_tests/containers/test_properties.cpp
+++ kratos/tests/cpp_tests/containers/test_properties.cpp
@@ -34,16 +34,17 @@ KRATOS_TEST_CASE_IN_SUITE(SubpropertiesInterface, KratosCoreFastSuite) {
     Properties::Pointer p12 = r_model_part.CreateNewProperties(12);
     Properties::Pointer p111 = r_model_part.CreateNewProperties(111);
 
-    //adding existing subproperties
+    // Adding existing subproperties
     p1->AddSubProperties(p11);
     p1->AddSubProperties(p12);
     p11->AddSubProperties(p111);
 
-    //add a new one
-    //note that i am creating a new property "1", belonging to 1.12 IT IS DIFFERENT FROM p1
-    r_model_part.GetSubProperties("1.12").AddSubProperties(Kratos::make_shared<Properties>(1)); 
-    
+    KRATOS_CHECK(p1->HasSubProperties(11));
+    KRATOS_CHECK(p11->HasSubProperties(111));
 
+    // Add a new one
+    // Note that i am creating a new property "1", belonging to 1.12 IT IS DIFFERENT FROM p1
+    r_model_part.GetSubProperties("1.12").AddSubProperties(Kratos::make_shared<Properties>(1)); 
 
     p1->SetValue(YOUNG_MODULUS,1.0);
     p11->SetValue(YOUNG_MODULUS,11.0);
@@ -62,12 +63,13 @@ KRATOS_TEST_CASE_IN_SUITE(SubpropertiesInterface, KratosCoreFastSuite) {
 
     KRATOS_CHECK_EQUAL(p1->GetValue(YOUNG_MODULUS), 1.0);
 
-    unsigned int found = 0;
+    std::size_t found = 0;
     for(const auto& prop_it : p1->GetSubProperties())
         if(prop_it.Id() == 11 || prop_it.Id() == 12)
             found++;
         else
             KRATOS_ERROR << "the property with Id " << prop_it.Id() << " should not exist in theh first layer";
+
     KRATOS_CHECK_EQUAL(found,2);
         
 }
