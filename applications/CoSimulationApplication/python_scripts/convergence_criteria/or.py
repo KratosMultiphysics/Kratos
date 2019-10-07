2f82aaab8aadc27610e793ff69310f83faac6532
joris <Joris.Degroote@UGent.be>
2019-10-07 13:38:00 +0200

    Moved duplicate code in and/or to combined.

--- applications/CoSimulationApplication/python_scripts/convergence_criteria/or.py
+++ applications/CoSimulationApplication/python_scripts/convergence_criteria/or.py
@@ -1,4 +1,4 @@
-from KratosMultiphysics.CoSimulationApplication.co_simulation_component import CoSimulationComponent
+from KratosMultiphysics.CoSimulationApplication.convergence_criteria.combined import ConvergenceCriterionCombined
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
 cs_data_structure = cs_tools.cs_data_structure
 
@@ -7,67 +7,7 @@ def Create(parameters):
     return ConvergenceCriterionOr(parameters)
 
 
-class ConvergenceCriterionOr(CoSimulationComponent):
-    def __init__(self, parameters):
-        super().__init__()
-
-        settings = parameters["settings"]
-        self.convergence_criteria = []
-        index = 0
-        while True:
-            key = "convergence_criterion" + str(index)
-            if key in settings.keys():
-                self.convergence_criteria.append(cs_tools.CreateInstance(settings[key]))
-                index += 1
-            else:
-                break
-
-    def Initialize(self):
-        super().Initialize()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.Initialize()
-
-    def Finalize(self):
-        super().Finalize()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.Finalize()
-
-    def InitializeSolutionStep(self):
-        super().InitializeSolutionStep()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.InitializeSolutionStep()
-
-    def FinalizeSolutionStep(self):
-        super().FinalizeSolutionStep()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.FinalizeSolutionStep()
-
-    def OutputSolutionStep(self):
-        super().OutputSolutionStep()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.OutputSolutionStep()
-
-    def Check(self):
-        super().Check()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.Check()
-
-    def PrintInfo(self):
-        super().PrintInfo()
-
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.PrintInfo()
-
-    def Update(self, r):
-        for convergence_criterion in self.convergence_criteria:
-            convergence_criterion.Update(r)
-
+class ConvergenceCriterionOr(ConvergenceCriterionCombined):
     def IsSatisfied(self):
         is_satisfied = False
         for convergence_criterion in self.convergence_criteria:
