8d45f419975949b4a9603c805d7e220492bd8e20
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2019-10-09 12:11:09 +0200

    Adding color compatibility for Windows

--- kratos/utilities/color_utilities.h
+++ kratos/utilities/color_utilities.h
@@ -11,50 +11,43 @@
 //                    
 //
 
-#ifndef _COLORS_
+#if !defined(_COLORS_)
 #define _COLORS_
 
-/* FOREGROUND */
-#if !defined(_WIN32)
-    #define RST  "\x1B[0m"
-    #define KRED  "\x1B[31m"
-    #define KGRN  "\x1B[32m"
-    #define KYEL  "\x1B[33m"
-    #define KBLU  "\x1B[34m"
-    #define KMAG  "\x1B[35m"
-    #define KCYN  "\x1B[36m"
-    #define KWHT  "\x1B[37m"
-
-    #define FRED(x) KRED x RST
-    #define FGRN(x) KGRN x RST
-    #define FYEL(x) KYEL x RST
-    #define FBLU(x) KBLU x RST
-    #define FMAG(x) KMAG x RST
-    #define FCYN(x) KCYN x RST
-    #define FWHT(x) KWHT x RST
-
-    #define BOLDFONT(x) "\x1B[1m" x RST
-    #define UNDL(x) "\x1B[4m" x RST
-#else
-    #define RST  ""
-    #define KRED ""
-    #define KGRN ""
-    #define KYEL ""
-    #define KBLU ""
-    #define KMAG ""
-    #define KCYN ""
-    #define KWHT ""
-
-    #define FRED(x) KRED x RST
-    #define FGRN(x) KGRN x RST
-    #define FYEL(x) KYEL x RST
-    #define FBLU(x) KBLU x RST
-    #define FMAG(x) KMAG x RST
-    #define FCYN(x) KCYN x RST
-    #define FWHT(x) KWHT x RST
-
-    #define BOLDFONT(x) "" x RST
-    #define UNDL(x) "" x RST
+// System includes
+#ifdef _WIN32
+#include <Windows.h>
 #endif
+// External includes
+
+// Project includes
+
+/* FOREGROUND */
+#ifdef _WIN32
+    #define PREPARE_STL_OUTPUT SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+#else
+    #define PREPARE_STL_OUTPUT void();
+#endif
+    
+#define RST   "\x1B[0m"
+#define BOLD  "\x1B[1m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLDFONT(x) BOLD x RST
+#define UNDL(x) "\x1B[4m" x RST
 
 #endif  /* _COLORS_ */
