3bfb2622677612cf66e980dc10ad4e1fee738951
philbucher <philipp.bucher@tum.de>
2019-10-10 17:06:01 +0200

    adding api-tools

---
+++ applications/CoSimulationApplication/co_simulation_api/co_sim_api_tools.h
@@ -0,0 +1,57 @@
+// KRATOS  / ___|___/ ___|(_)_ __ ___  _   _| | __ _| |_(_) ___  _ ___
+//        | |   / _ \___ \| | '_ ` _ \| | | | |/ _` | __| |/ _ \| '_  |
+//        | |__| (_) |__) | | | | | | | |_| | | (_| | |_| | (_) | | | |
+//         \____\___/____/|_|_| |_| |_|\__,_|_|\__,_|\__|_|\___/|_| |_|
+//
+//  License:		 BSD License
+//                   license: CoSimulationApplication/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+#ifndef KRATOS_CO_SIM_API_TOOLS_H_INCLUDED
+#define KRATOS_CO_SIM_API_TOOLS_H_INCLUDED
+
+// System includes
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+namespace CoSim {
+namespace Tools {
+
+typedef std::unordered_map<std::string, std::string> SettingsType;
+
+void AddMissingSettings(const SettingsType& rDefaultSettings, SettingsType& rSettings)
+{
+    for (const auto& r_setting : rDefaultSettings) {
+        if (rSettings.count(r_setting.first) == 0) {
+            rSettings[r_setting.first] = r_setting.second;
+        }
+    }
+}
+
+SettingsType ReadSettingsFile(const std::string& rSettingsFileName)
+{
+    std::ifstream settings_file(rSettingsFileName);
+
+    if (!settings_file.good()) {
+        std::cout << "Input file \"" << rSettingsFileName << "\" could not be read, using default configuration" << std::endl;
+        return SettingsType();
+    }
+
+    SettingsType settings;
+    std::string current_line;
+    while (std::getline(settings_file, current_line)) {
+        // TODO implement this
+    }
+    return settings;
+}
+
+} // namespace Tools
+} // namespace CoSim
+
+#endif /* KRATOS_CO_SIM_API_TOOLS_H_INCLUDED */
