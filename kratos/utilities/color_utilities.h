78907512e591b3f682b588671d94498cec99e1fb
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2019-10-11 14:48:31 +0200

    Update

--- kratos/utilities/color_utilities.h
+++ kratos/utilities/color_utilities.h
@@ -19,23 +19,25 @@
 // External includes
 
 // Project includes
-
-#ifdef KRATOS_COMPILED_IN_WINDOWS
-    #define PREPARE_STL_OUTPUT SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-#else
-    #define PREPARE_STL_OUTPUT void();
-#endif
+#include "includes/define.h"
 
 /* FOREGROUND */    
-#define RST   "\x1B[0m"
-#define BOLD  "\x1B[1m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#ifdef KRATOS_COMPILED_IN_WINDOWS
+    #define BASE_STRING_ANSI   "\033"
+#else
+    #define BASE_STRING_ANSI   "\x1B"
+#endif
+
+#define RST   BASE_STRING_ANSI "[0m"
+#define BOLD_FONT  BASE_STRING_ANSI "[1m"
+#define KRED  BASE_STRING_ANSI "[31m"
+#define KGRN  BASE_STRING_ANSI "[32m"
+#define KYEL  BASE_STRING_ANSI "[33m"
+#define KBLU  BASE_STRING_ANSI "[34m"
+#define KMAG  BASE_STRING_ANSI "[35m"
+#define KCYN  BASE_STRING_ANSI "[36m"
+#define KWHT  BASE_STRING_ANSI "[37m"
+#define UNDERLINE_FONT  BASE_STRING_ANSI "[4m"
 
 #define FRED(x) KRED x RST
 #define FGRN(x) KGRN x RST
@@ -45,7 +47,7 @@
 #define FCYN(x) KCYN x RST
 #define FWHT(x) KWHT x RST
 
-#define BOLDFONT(x) BOLD x RST
-#define UNDL(x) "\x1B[4m" x RST
+#define BOLDFONT(x) BOLD_FONT x RST
+#define UNDL(x) UNDERLINE_FONT x RST
 
 #endif  /* COLOR_UTILITIES_H_INCLUDED */
