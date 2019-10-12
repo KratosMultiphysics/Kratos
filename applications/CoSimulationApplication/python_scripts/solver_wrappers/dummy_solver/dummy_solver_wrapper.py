c525ffb116abaac2996bcf27605a706dc9bc0daa
Philipp <philipp.bucher@tum.de>
2019-10-12 19:47:00 +0200

    advances in dummy solver wrapper

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/dummy_solver/dummy_solver_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/dummy_solver/dummy_solver_wrapper.py
@@ -15,7 +15,7 @@ def Create(settings, solver_name):
     return DummySolverWrapper(settings, solver_name)
 
 class DummySolverWrapper(CoSimulationSolverWrapper):
-    """This class serves as wrapper for the cpp ping and pong solvers
+    """This class serves as wrapper for the cpp/fortran dummy solver
     """
     def __init__(self, settings, solver_name):
         super(DummySolverWrapper, self).__init__(settings, solver_name)
@@ -25,12 +25,21 @@ class DummySolverWrapper(CoSimulationSolverWrapper):
         start_external_solver = wrapper_settings["start_external_solver"].GetBool()
         if start_external_solver:
             command_txt = wrapper_settings["external_solver_start_command"].GetString()
-            print("Running : ", command_txt)
-            self.rv = subprocess.Popen(command_txt, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True, start_new_session=True) # TODO check this, esp it should also block the caller if it hangs (basically propagate all errors etc)
+            command_args = wrapper_settings["external_solver_arguments"].GetStringArray()
+            cs_tools.cs_print_info(self._ClassName(), 'Running external solver with command: {} | arguments: {}'.format(command_txt, command_args))
+
+            full_command = [command_txt]
+            full_command.extend(command_args)
+            self.external_solver_process = subprocess.Popen(full_command, stderr=subprocess.PIPE, start_new_session=True)
 
         self.controlling_external_solver = wrapper_settings["controlling_external_solver"].GetBool()
 
+        model_part_name = self.settings["solver_wrapper_settings"]["main_model_part_name"].GetString()
+        cs_tools.CreateMainModelPartsFromCouplingData(self.data_dict.values(), self.model, self.name)
+        cs_tools.AllocateHistoricalVariablesFromCouplingData(self.data_dict.values(), self.model, self.name)
+
     def AdvanceInTime(self, current_time):
+        self.__CheckExternalSolverProcess()
         if self.controlling_external_solver:
             self.__SendControlSignal("AdvanceInTime")
             # TODO this requires more!
@@ -81,7 +90,7 @@ class DummySolverWrapper(CoSimulationSolverWrapper):
         cs_tools.cs_print_info(self._ClassName(), "printing info...")
 
     def _GetIOType(self):
-        return "dummy_solver_io"
+        return "dummy_solver.dummy_solver_io"
 
     def __SendControlSignal(self, signal):
         data_config = {
@@ -89,3 +98,9 @@ class DummySolverWrapper(CoSimulationSolverWrapper):
             "signal" : signal
         }
         self.ExportData(data_config)
+
+    def __CheckExternalSolverProcess(self):
+        if hasattr(self, 'external_solver_process'):
+            _, process_stderr = self.external_solver_process.communicate()
+            if process_stderr:
+                raise Exception("{} terminated with the following error:\n{}".format(self._ClassName(), process_stderr.decode('ascii')))
