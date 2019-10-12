57f11806d9ea1dcf4fc7cb6d0c1e2a65d66a56ef
adityagantasala <shine.aditya@gmail.com>
2019-10-12 23:21:06 +0200

    [skip ci] [skip cd] skiping for first step

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/chimera_fsi_fluid_dynamics_wapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/chimera_fsi_fluid_dynamics_wapper.py
@@ -15,15 +15,24 @@ def Create(cosim_solver_settings, level):
 
 class ChimeraFsiFluidDynamicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
     def _CreateAnalysisStage(self):
+        self.coupling_iteration = 0
         return FluidChimeraAnalysis(self.model, self.project_parameters)
 
     def SolveSolutionStep(self):
-        self._analysis_stage.FinalizeSolutionStep()
-        self._analysis_stage.InitializeSolutionStep()
+        if self.coupling_iteration != 0
+            self._analysis_stage.FinalizeSolutionStep()
+            self._analysis_stage.InitializeSolutionStep()
         super(ChimeraFsiFluidDynamicsWrapper, self).SolveSolutionStep()
+        self.coupling_iteration += 1
 
     def _GetParallelType(self):
         return self.project_parameters["problem_data"]["parallel_type"].GetString()
+
+    def InitializeSolutionStep(self):
+        self.coupling_iteration = 0
+
+    def FinalizeSolutionStep(self):
+        self.coupling_iteration = 0
 
     def _Name(self):
         return self.__class__.__name__
