5b20ec396a3316c7f6592a39ec0d44e6b904b11c
Toon Demeester <toon.demeester@ugent.be>
2019-10-15 16:21:07 +0200

    Start on tube2D test

---
+++ applications/CoSimulationApplication/tests/solver_wrappers/fluent/2019R1/test_tube2d/test_solver_wrapper.py
@@ -0,0 +1,17 @@
+import KratosMultiphysics as KM
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+
+class TestSolverWrapperFluent2019R1Tube2D(KratosUnittest.TestCase):
+    def test_solver_wrapper(self):
+
+        print('test for Fluent with Tube2D')
+
+        self.assertTrue(False)
+        self.assertAlmostEqual(1, 1)
+
+
+
+
+if __name__ == '__main__':
+    KratosUnittest.main()
