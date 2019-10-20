55bdfb45b4c70c214089a04f287b866f6dfecf98
ga29wun <aditya.ghantasala@tum.de>
2019-10-20 13:38:10 +0200

    calling base class functions

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/chimera_fsi_fluid_dynamics_wapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/chimera_fsi_fluid_dynamics_wapper.py
@@ -19,9 +19,9 @@ class ChimeraFsiFluidDynamicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
         return FluidChimeraAnalysis(self.model, self.project_parameters)
 
     def SolveSolutionStep(self):
-        if self.coupling_iteration != 0
-            self._analysis_stage.FinalizeSolutionStep()
-            self._analysis_stage.InitializeSolutionStep()
+        if self.coupling_iteration != 0:
+          self._analysis_stage.FinalizeSolutionStep()
+          self._analysis_stage.InitializeSolutionStep()
         super(ChimeraFsiFluidDynamicsWrapper, self).SolveSolutionStep()
         self.coupling_iteration += 1
 
@@ -29,9 +29,11 @@ class ChimeraFsiFluidDynamicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
         return self.project_parameters["problem_data"]["parallel_type"].GetString()
 
     def InitializeSolutionStep(self):
+        super(ChimeraFsiFluidDynamicsWrapper, self).InitializeSolutionStep()
         self.coupling_iteration = 0
 
     def FinalizeSolutionStep(self):
+        super(ChimeraFsiFluidDynamicsWrapper, self).FinalizeSolutionStep()
         self.coupling_iteration = 0
 
     def _Name(self):
