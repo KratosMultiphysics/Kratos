0b6c2dcbaee1d7f00529a975a322eb52b60b3b3e
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2019-10-22 12:51:25 +0200

    Updated tests expected results

--- applications/DEMApplication/tests/test_glued_particles.py
+++ applications/DEMApplication/tests/test_glued_particles.py
@@ -46,13 +46,13 @@ class GluedParticlesTestSolution(DEM_analysis_stage.DEMAnalysisStage):
                     self.CheckValue("Angular Velocity at time "+ str(time), angular_velocity[0], 2.0, tolerance)
 
                 if time > 0.499999 and time < 0.5000001:
-                    self.CheckValue("X Coordinate at time 0.5", node.X, -1.0,     tolerance)
-                    self.CheckValue("Y Coordinate at time 0.5", node.Y,  0.6638445208099379, tolerance)
-                    self.CheckValue("Z Coordinate at time 0.5", node.Z,  0.21679366644461678, tolerance)
+                    self.CheckValue("X Coordinate at time 0.5", node.X, -1.0, tolerance)
+                    self.CheckValue("Y Coordinate at time 0.5", node.Y, 0.6634116060768411, tolerance)
+                    self.CheckValue("Z Coordinate at time 0.5", node.Z, 0.21679366644461678, tolerance)
 
                 if time > 0.999999 and time < 1.0000001:
                     self.CheckValue("X Coordinate at time 1.0", node.X, -1.0, tolerance)
-                    self.CheckValue("Y Coordinate at time 1.0", node.Y,  0.6359488, tolerance)
+                    self.CheckValue("Y Coordinate at time 1.0", node.Y, 0.6359488, tolerance)
                     self.CheckValue("Z Coordinate at time 1.0", node.Z, -0.1657309642449, tolerance)
 
     @classmethod
