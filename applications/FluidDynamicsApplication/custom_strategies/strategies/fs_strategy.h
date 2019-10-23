32a330b20794ffe51fe26b21af37dc75c3eff198
adityaghantasala <aditya.ghantasala@gmail.com>
2019-10-23 10:37:56 +0200

    using info if

--- applications/FluidDynamicsApplication/custom_strategies/strategies/fs_strategy.h
+++ applications/FluidDynamicsApplication/custom_strategies/strategies/fs_strategy.h
@@ -642,13 +642,11 @@ protected:
 
         double Ratio = NormDv / NormV;
 
-        if ( BaseType::GetEchoLevel() > 0 ){
-            KRATOS_INFO("Fractional Step Strategy : ") << "CONVERGENCE CHECK:" << std::endl;
-            KRATOS_INFO("Fractional Step Strategy : ") <<  std::scientific << std::setprecision(8)
-                                                       << "FRAC VEL.: ratio = "
-                                                       << Ratio <<"; exp.ratio = " << mVelocityTolerance
-                                                       << " abs = " << NormDv << std::endl;
-        }
+        KRATOS_INFO_IF("Fractional Step Strategy : ", BaseType::GetEchoLevel() > 0) << "CONVERGENCE CHECK:" << std::endl;
+        KRATOS_INFO_IF("Fractional Step Strategy : ", BaseType::GetEchoLevel() > 0) <<  std::scientific << std::setprecision(8)
+                                                                                    << "FRAC VEL.: ratio = "
+                                                                                    << Ratio <<"; exp.ratio = " << mVelocityTolerance
+                                                                                    << " abs = " << NormDv << std::endl;
 
         if (Ratio < mVelocityTolerance)
         {
