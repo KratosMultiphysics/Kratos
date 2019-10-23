ed296d69003d9c8e1f5090ac06a03604a0f8fcc6
adityaghantasala <aditya.ghantasala@gmail.com>
2019-10-23 11:10:43 +0200

    some pointer checks + new method for setting strategy with b and s

--- applications/FluidDynamicsApplication/custom_utilities/solver_settings.h
+++ applications/FluidDynamicsApplication/custom_utilities/solver_settings.h
@@ -61,6 +61,7 @@ public:
     typedef SolvingStrategy<TSparseSpace,TDenseSpace,TLinearSolver> StrategyType;
     typedef typename StrategyType::Pointer StrategyPointerType;
     typedef typename Process::Pointer ProcessPointerType;
+    typedef BuilderAndSolver<TSparseSpace,TDenseSpace,TLinearSolver> TBuilderAndSolverType;
 
     enum StrategyLabel { Velocity, Pressure, /*EddyViscosity,*/ NumLabels };
 
@@ -118,12 +119,18 @@ public:
     virtual void SetStrategy(StrategyLabel const& rStrategyLabel,
                              typename TLinearSolver::Pointer pLinearSolver,
                              const double Tolerance,
-                             const unsigned int MaxIter) = 0;
+                             const unsigned int MaxIter) {}
+
+    virtual void SetStrategy(StrategyLabel const& rStrategyLabel,
+                             typename TLinearSolver::Pointer pLinearSolver,
+                             const double Tolerance,
+                             const std::size_t MaxIter,
+                             typename TBuilderAndSolverType::Pointer pNewBuilderAndSolver){}
 
     virtual void SetTurbulenceModel(TurbulenceModelLabel const& rTurbulenceModel,
                                     typename TLinearSolver::Pointer pLinearSolver,
                                     const double Tolerance,
-                                    const unsigned int MaxIter) = 0;
+                                    const unsigned int MaxIter) {}
 
     virtual void SetTurbulenceModel(ProcessPointerType pTurbulenceModel)
     {
@@ -169,11 +176,19 @@ public:
 
         if ( itStrategy != mStrategies.end() )
         {
-            pThisStrategy.swap(itStrategy->second);
-            return true;
+            if(itStrategy->second != nullptr)
+            {
+                pThisStrategy.swap(itStrategy->second);
+                return true;
+            } else {
+                KRATOS_INFO("SolverSettingsFSStrategy")<<"Strategy for :: "<<rStrategyLabel<<" not found."<<std::endl;
+                return false;
+            }
         }
-        else
+        else {
+            KRATOS_INFO("SolverSettingsFSStrategy")<<"Strategy for :: "<<rStrategyLabel<<" not found."<<std::endl;
             return false;
+        }
     }
 
     virtual bool FindTolerance(StrategyLabel const& rStrategyLabel,
