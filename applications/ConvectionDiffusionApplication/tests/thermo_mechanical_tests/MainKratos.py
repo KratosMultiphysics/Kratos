db2ccd5591c73af42aff46ea4575a77575a856f3
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2019-10-23 16:44:39 +0200

    adding test

---
+++ applications/ConvectionDiffusionApplication/tests/thermo_mechanical_tests/MainKratos.py
@@ -0,0 +1,33 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import KratosMultiphysics
+from KratosMultiphysics.ConvectionDiffusionApplication.convection_diffusion_analysis import ConvectionDiffusionAnalysis
+
+import sys
+import time
+
+class ConvectionDiffusionAnalysisWithFlush(ConvectionDiffusionAnalysis):
+
+    def __init__(self,model,project_parameters,flush_frequency=10.0):
+        super(ConvectionDiffusionAnalysisWithFlush,self).__init__(model,project_parameters)
+
+        self.flush_frequency = flush_frequency
+        self.last_flush = time.time()
+
+    def FinalizeSolutionStep(self):
+        super(ConvectionDiffusionAnalysisWithFlush,self).FinalizeSolutionStep()
+
+        if self.parallel_type == "OpenMP":
+            now = time.time()
+            if now - self.last_flush > self.flush_frequency:
+                sys.stdout.flush()
+                self.last_flush = now
+
+if __name__ == "__main__":
+
+    with open("ProjectParameters.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = ConvectionDiffusionAnalysisWithFlush(model,parameters)
+    simulation.Run()
