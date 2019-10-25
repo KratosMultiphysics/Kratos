7cedeac7e01aa19969e656aa10f2ab1540f52dd9
Carlos Roig <roigcarlo@gmail.com>
2019-10-25 12:10:38 +0200

    Increasing small time FOR TESTING ONLY

--- kratos/python_scripts/run_tests.py
+++ kratos/python_scripts/run_tests.py
@@ -262,7 +262,7 @@ def main():
     # Set timeout of the different levels
     signalTime = None
     if level == 'small':
-        signalTime = int(60)
+        signalTime = int(120)
     elif level == 'nightly':
         signalTime = int(900)
 
