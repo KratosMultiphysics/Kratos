a7e749d0202fc00abc0cf4c77c58c1f3bee84c61
adityaghantasala <aditya.ghantasala@gmail.com>
2019-10-30 17:18:08 +0100

    header only fs settings

--- applications/ChimeraApplication/custom_utilities/fractional_step_settings_for_chimera.h
+++ applications/ChimeraApplication/custom_utilities/fractional_step_settings_for_chimera.h
@@ -105,10 +105,12 @@ public:
                    const std::size_t ThisTimeOrder,
                    const bool UseSlip,
                    const bool MoveMeshFlag,
-                   const bool ReformDofSet);
+                   const bool ReformDofSet):
+        BaseType(rModelPart,ThisDomainSize,ThisTimeOrder,UseSlip,MoveMeshFlag,ReformDofSet)
+    {}
 
     /// Destructor.
-    ~FractionalStepSettingsForChimera() override;
+    ~FractionalStepSettingsForChimera() override{}
 
     ///@}
     ///@name Operators
@@ -126,11 +128,87 @@ public:
     void SetStrategy(StrategyLabel const& rStrategyLabel,
                              typename TLinearSolver::Pointer pLinearSolver,
                              const double Tolerance,
-                             const unsigned int MaxIter) override ;
+                             const unsigned int MaxIter) override 
+    {
+        KRATOS_TRY;
+
+        // pointer types for solution strategy construcion
+        typedef typename Scheme< TSparseSpace, TDenseSpace >::Pointer SchemePointerType;
+        typedef ResidualBasedBlockBuilderAndSolverWithConstraintsForChimera<TSparseSpace, TDenseSpace, TLinearSolver > ResidualBasedBlockBuilderAndSolverWithConstraintsForChimeraType;
+
+        // Default, fixed flags
+        bool CalculateReactions = false;
+        bool CalculateNormDxFlag = true;
+
+        ModelPart& rModelPart = BaseType::GetModelPart();
+
+        bool UseSlip = BaseType::UseSlipConditions();
+        // Modification of the DofSet is managed by the fractional step strategy, not the auxiliary velocity and pressure strategies.
+        bool ReformDofSet = false; //BaseType::GetReformDofSet();
+        std::size_t EchoLevel = BaseType::GetEchoLevel();
+        std::size_t StrategyEchoLevel = (EchoLevel > 0) ? (EchoLevel-1) : 0;
+
+        if ( rStrategyLabel == BaseType::Velocity )
+        {
+            ModelPart &r_fs_velocity_model_part = rModelPart.CreateSubModelPart("fs_velocity_model_part");
+            FastTransferBetweenModelPartsProcess(r_fs_velocity_model_part, rModelPart).Execute();
+            // Velocity Builder and Solver
+            //ResidualBasedBlockBuilderAndSolverPointerType pBuildAndSolver = new ResidualBasedBlockBuilderAndSolverWithConstraintsForChimera<TSparseSpace, TDenseSpace, TLinearSolver >(pLinearSolver);
+            ResidualBasedBlockBuilderAndSolverPointerType pBuildAndSolver =  Kratos::make_shared<ResidualBasedBlockBuilderAndSolverWithConstraintsForChimeraType>(pLinearSolver);
+
+            SchemePointerType pScheme;
+            //initializing fractional velocity solution step
+            if (UseSlip)
+            {
+                std::size_t DomainSize = BaseType::GetDomainSize();
+                SchemePointerType Temp = SchemePointerType(new ResidualBasedIncrementalUpdateStaticSchemeSlip< TSparseSpace, TDenseSpace > (DomainSize,DomainSize));
+                pScheme.swap(Temp);
+            }
+            else
+            {
+                SchemePointerType Temp = SchemePointerType(new ResidualBasedIncrementalUpdateStaticScheme< TSparseSpace, TDenseSpace > ());
+                pScheme.swap(Temp);
+            }
+
+            // Strategy
+            BaseType::mStrategies[rStrategyLabel] = StrategyPointerType(new ResidualBasedLinearStrategy<TSparseSpace, TDenseSpace, TLinearSolver >
+                                                                        (r_fs_velocity_model_part, pScheme, pLinearSolver, pBuildAndSolver, CalculateReactions, ReformDofSet, CalculateNormDxFlag));
+        }
+        else if ( rStrategyLabel == BaseType::Pressure )
+        {
+            ModelPart &r_fs_pressure_model_part = rModelPart.CreateSubModelPart("fs_pressure_model_part");
+            FastTransferBetweenModelPartsProcess(r_fs_pressure_model_part, rModelPart).Execute();
+            // Pressure Builder and Solver
+            ResidualBasedBlockBuilderAndSolverPointerType pBuildAndSolver =  Kratos::make_shared<ResidualBasedBlockBuilderAndSolverWithConstraintsForChimeraType>(pLinearSolver);
+            SchemePointerType pScheme = SchemePointerType(new ResidualBasedIncrementalUpdateStaticScheme< TSparseSpace, TDenseSpace > ());
+
+            // Strategy
+            BaseType::mStrategies[rStrategyLabel] = StrategyPointerType(new ResidualBasedLinearStrategy<TSparseSpace, TDenseSpace, TLinearSolver >
+                                                                        (r_fs_pressure_model_part, pScheme, pLinearSolver, pBuildAndSolver, CalculateReactions, ReformDofSet, CalculateNormDxFlag));
+        }
+        else
+        {
+            KRATOS_THROW_ERROR(std::runtime_error,"Error in FractionalStepSettingsForChimera: Unknown strategy label.","");
+        }
+
+        BaseType::mTolerances[rStrategyLabel] = Tolerance;
+
+        BaseType::mMaxIter[rStrategyLabel] = MaxIter;
+
+        BaseType::mStrategies[rStrategyLabel]->SetEchoLevel(StrategyEchoLevel);
+
+        KRATOS_CATCH("");
+    }
 
 
     bool FindStrategy(StrategyLabel const& rStrategyLabel,
-                              StrategyPointerType& pThisStrategy) override;
+                              StrategyPointerType& pThisStrategy) override
+    {
+        pThisStrategy = BaseType::mStrategies[rStrategyLabel];
+        if(pThisStrategy != nullptr)
+            return true;
+        return false;
+    }
 
     ///@}
     ///@name Inquiry
@@ -141,13 +219,18 @@ public:
     ///@{
 
     /// Turn back information as a string.
-    std::string Info() const override;
+    std::string Info() const override
+    {
+        std::stringstream buffer;
+        buffer << "FractionalStepSettingsForChimera" ;
+        return buffer.str();
+    }
 
     /// Print information about this object.
-    void PrintInfo(std::ostream& rOStream) const override;
+    void PrintInfo(std::ostream& rOStream) const override {rOStream << "FractionalStepSettingsForChimera";}
 
     /// Print object's data.
-    void PrintData(std::ostream& rOStream) const override;
+    void PrintData(std::ostream& rOStream) const override {}
 
 
     ///@}
@@ -229,13 +312,13 @@ private:
     ///@{
 
     /// Default constructor.
-    FractionalStepSettingsForChimera() = delete;
+    FractionalStepSettingsForChimera(){}
 
     /// Assignment operator.
-    FractionalStepSettingsForChimera& operator=(FractionalStepSettingsForChimera const& rOther) = delete;
+    FractionalStepSettingsForChimera& operator=(FractionalStepSettingsForChimera const& rOther){}
 
     /// Copy constructor.
-    FractionalStepSettingsForChimera(FractionalStepSettingsForChimera const& rOther) = delete;
+    FractionalStepSettingsForChimera(FractionalStepSettingsForChimera const& rOther){}
 
 
     ///@}
