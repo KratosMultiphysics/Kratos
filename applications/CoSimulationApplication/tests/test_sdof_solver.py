14a072206b8d5d73d536d84e5ba21ef897ac394a
Philipp <philipp.bucher@tum.de>
2019-10-30 16:12:34 +0100

    refactored sdof solver tests

--- applications/CoSimulationApplication/tests/test_sdof_solver.py
+++ applications/CoSimulationApplication/tests/test_sdof_solver.py
@@ -30,9 +30,9 @@ class TestSdofSolver(KratosUnittest.TestCase):
         self.time = 0.0
 
     def tearDown(self):
-        os.remove("./result.dat")
+        os.remove("result.dat")
 
-    def _compare_results(self, reference, result):
+    def __CompareResults(self, reference, result):
         ref = np.loadtxt(reference, skiprows=1)
         res = np.loadtxt(result, skiprows=1)
         self.assertEqual(len(ref), len(res))
@@ -40,6 +40,19 @@ class TestSdofSolver(KratosUnittest.TestCase):
             self.assertEqual(len(line_ref), len(line_res))
             for entry_ref, entry_res in zip(line_ref, line_res):
                 self.assertAlmostEqual(entry_ref, entry_res)
+
+    def __ExecuteTest(self, settings, ref_file_name):
+        settings.update(self.system_settings)
+        system = SDoFSolver(settings)
+        system.Initialize()
+
+        while(self.time <= self.end_time):
+            self.time =  system.AdvanceInTime(self.time)
+            system.SolveSolutionStep()
+            system.OutputSolutionStep()
+
+        self.__CompareResults(os.path.join("reference_files", ref_file_name), "result.dat")
+
 
     def test_initial_displacement(self):
         settings = {
@@ -49,17 +62,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
             "acceleration"  : 0.0
             }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_initial_displacement.dat", "./result.dat")
-
+        self.__ExecuteTest(settings, "ref_sdof_initial_displacement.dat")
 
     def test_initial_velocity(self):
         settings = {
@@ -69,16 +72,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
             "acceleration"  : 0.0
             }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_initial_velocity.dat", "./result.dat")
+        self.__ExecuteTest(settings, "ref_sdof_initial_velocity.dat")
 
     def test_impulse(self):
         settings = {
@@ -86,17 +80,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
                     "load_impulse" : 1000.0
                 }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_impulse.dat", "./result.dat")
-
+        self.__ExecuteTest(settings, "ref_sdof_impulse.dat")
 
     def test_force_excitation(self):
         settings = {
@@ -110,16 +94,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
                     "amplitude_force": 1000.0
                 }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_force_excitation.dat", "./result.dat")
+        self.__ExecuteTest(settings, "ref_sdof_force_excitation.dat")
 
     def test_root_point_excitation(self):
         settings = {
@@ -133,16 +108,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
                     "amplitude_force": 0.0
                 }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_root_point_displacement.dat", "./result.dat")
+        self.__ExecuteTest(settings, "ref_sdof_root_point_displacement.dat")
 
     def test_root_point_excitation_force_excitation(self):
         settings = {
@@ -156,16 +122,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
                     "amplitude_force": 1000.0
                 }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_root_point_displacement_external_force.dat", "./result.dat")
+        self.__ExecuteTest(settings, "ref_sdof_root_point_displacement_external_force.dat")
 
     def test_root_point_excitation_force_excitation_impulse(self):
         settings = {
@@ -179,16 +136,7 @@ class TestSdofSolver(KratosUnittest.TestCase):
                     "amplitude_force": 1000.0
                 }
         }
-        settings.update(self.system_settings)
-        system = SDoFSolver(settings)
-        system.Initialize()
-
-        while(self.time <= self.end_time):
-            self.time =  system.AdvanceInTime(self.time)
-            system.SolveSolutionStep()
-            system.OutputSolutionStep()
-
-        self._compare_results("./reference_files/ref_sdof_root_point_displacement_impulse.dat", "./result.dat")
+        self.__ExecuteTest(settings, "ref_sdof_root_point_displacement_impulse.dat")
 
 if __name__ == '__main__':
     KratosUnittest.main()
