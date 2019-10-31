ee351070ec298f8620a774881e9756fd35752f78
Philipp <philipp.bucher@tum.de>
2019-10-31 19:43:59 +0100

    missing var

--- applications/CoSimulationApplication/custom_data_structure/pyKratos/variables_co_simulation.py
+++ applications/CoSimulationApplication/custom_data_structure/pyKratos/variables_co_simulation.py
@@ -8,3 +8,4 @@ def RegisterVariables(module):
     CreateDoubleVariable(module, "SCALAR_ROOT_POINT_DISPLACEMENT")
     CreateDoubleVariable(module, "SCALAR_REACTION")
     CreateDoubleVariable(module, "SCALAR_FORCE")
+    CreateDoubleVariable(module, "SCALAR_VOLUME_ACCELERATION")
