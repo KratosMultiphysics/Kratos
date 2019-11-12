24c9ccde8395cdb469fd723929c0dfaec21973ad
roigcarlo <roigcarlo@gmail.com>
2019-11-12 17:02:54 +0100

    Restore file

--- scripts/build/appveyor/configure_appveyor_VS2017.bat
+++ scripts/build/appveyor/configure_appveyor_VS2017.bat
@@ -2,7 +2,7 @@ rem For any question please contact with us in:
 rem  - https://github.com/KratosMultiphysics/Kratos
 
 rem Set compiler
-@echo on
+@echo off
 set CC=cl.exe
 set CXX=cl.exe
 
@@ -16,6 +16,8 @@ set KRATOS_APPLICATIONS=
 CALL :add_app %KRATOS_APP_DIR%\StructuralMechanicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\ContactStructuralMechanicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\FluidDynamicsApplication;
+CALL :add_app %KRATOS_APP_DIR%\MeshingApplication;
+CALL :add_app %KRATOS_APP_DIR%\MeshMovingApplication;
 CALL :add_app %KRATOS_APP_DIR%\DEMApplication;
 CALL :add_app %KRATOS_APP_DIR%\SwimmingDEMApplication;
 CALL :add_app %KRATOS_APP_DIR%\CSharpWrapperApplication;
@@ -26,10 +28,8 @@ CALL :add_app %KRATOS_APP_DIR%\ContactMechanicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\PfemApplication;
 CALL :add_app %KRATOS_APP_DIR%\PfemFluidDynamicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\PfemSolidMechanicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\MeshingApplication;
-CALL :add_app %KRATOS_APP_DIR%\MeshMovingApplication;
 
-echo %KRATOS_APPLICATIONS%
+echo 
 
 rem Clean
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\cmake_install.cmake"
@@ -37,6 +37,7 @@ del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeCache.txt"
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeFiles"
 
 rem Configure
+@echo on
  cmake                                              ^
  -G"Visual Studio 15 2017 Win64"                    ^
  -H"%KRATOS_SOURCE%"                                ^
