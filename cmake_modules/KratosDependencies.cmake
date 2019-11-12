83d0a98c7b5466b2e7b91c08bb13240b6feca8d2
roigcarlo <roigcarlo@gmail.com>
2019-11-12 16:29:29 +0100

    Adding warning

--- cmake_modules/KratosDependencies.cmake
+++ cmake_modules/KratosDependencies.cmake
@@ -2,7 +2,7 @@
 macro(kratos_add_dependency application_path)
 get_filename_component(application_name ${application_path} NAME )
     IF(NOT TARGET Kratos${application_name})
-        message("Adding dependency ${application_name}")
+        message("[Warning] Adding dependency ${application_name}")
         add_subdirectory(${application_path} ${CMAKE_CURRENT_BINARY_DIR}/applications/${application_name} )
     endif(NOT TARGET Kratos${application_name})
 endmacro(kratos_add_dependency)
