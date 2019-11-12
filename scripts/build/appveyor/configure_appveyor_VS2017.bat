d5d86c53c44d50cd7585a827a45b290e3490b22e
roigcarlo <roigcarlo@gmail.com>
2019-11-12 16:42:55 +0100

    Adding debug

--- scripts/build/appveyor/configure_appveyor_VS2017.bat
+++ scripts/build/appveyor/configure_appveyor_VS2017.bat
@@ -29,23 +29,26 @@ CALL :add_app %KRATOS_APP_DIR%\PfemApplication;
 CALL :add_app %KRATOS_APP_DIR%\PfemFluidDynamicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\PfemSolidMechanicsApplication;
 
+@echo on
+echo %KRATOS_APPLICATIONS%
+
 rem Clean
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\cmake_install.cmake"
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeCache.txt"
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeFiles"
 
 rem Configure
-@echo on
  cmake                                              ^
  -G"Visual Studio 15 2017 Win64"                    ^
  -H"%KRATOS_SOURCE%"                                ^
  -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"             ^
  -DBOOST_ROOT="C:\Libraries\boost_1_65_1"           ^
  -DPYTHON_EXECUTABLE="C:\Python36-x64\python.exe"   ^
- -DINCLUDE_FEAST=OFF
+ -DINCLUDE_FEAST=OFF                                ^
+ -DUSE_COTIRE=ON
 
 rem Build
-cmake --build "%KRATOS_BUILD%/%KRATOS_BUILD_TYPE%" --target install
+cmake --build "%KRATOS_BUILD%/%KRATOS_BUILD_TYPE%" --target all_unity
 goto:eof
 
 :add_app
