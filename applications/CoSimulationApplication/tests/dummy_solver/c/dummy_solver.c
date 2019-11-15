8eba558a8402edbe39d87fb3371a2915aee47308
Philipp <philipp.bucher@tum.de>
2019-11-15 16:14:35 +0100

    init version of dummy c solver

---
+++ applications/CoSimulationApplication/tests/dummy_solver/c/dummy_solver.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void abc() {
+    printf("Hello, abc!");
+}
+
+ int puts(const char *s)
+ {
+    printf("Called the puts function!");
+ }
+
+
+
+int main()
+{
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
+    return 0;
+}
