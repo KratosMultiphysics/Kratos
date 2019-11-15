fcf157c9eacf7f9cae7e59eb61a6c2f395ac83af
MFusseder <martin.fusseder@tum.de>
2019-11-15 11:20:44 +0100

    enhance declaration of dof lists

--- applications/StructuralMechanicsApplication/custom_response_functions/adjoint_elements/adjoint_finite_difference_base_element.h
+++ applications/StructuralMechanicsApplication/custom_response_functions/adjoint_elements/adjoint_finite_difference_base_element.h
@@ -472,31 +472,13 @@ protected:
 
         // Build vector of variables containing the DOF-variables of the primal problem
         std::vector<VariableComponent<VectorComponentAdaptor<array_1d<double, 3>>>*> primal_solution_variable_list;
-        primal_solution_variable_list.reserve(num_dofs_per_node);
-        primal_solution_variable_list.push_back(&DISPLACEMENT_X);
-        primal_solution_variable_list.push_back(&DISPLACEMENT_Y);
-        primal_solution_variable_list.push_back(&DISPLACEMENT_Z);
-
-        if(mHasRotationDofs)
-        {
-            primal_solution_variable_list.push_back(&ROTATION_X);
-            primal_solution_variable_list.push_back(&ROTATION_Y);
-            primal_solution_variable_list.push_back(&ROTATION_Z);
-        }
+        (mHasRotationDofs) ? primal_solution_variable_list = {&DISPLACEMENT_X, &DISPLACEMENT_Y, &DISPLACEMENT_Z, &ROTATION_X, &ROTATION_Y, &ROTATION_Z} :
+                             primal_solution_variable_list = {&DISPLACEMENT_X, &DISPLACEMENT_Y, &DISPLACEMENT_Z};
 
         // Build vector of variables containing the DOF-variables of the adjoint problem
         std::vector<VariableComponent<VectorComponentAdaptor<array_1d<double, 3>>>*> adjoint_solution_variable_list;
-        adjoint_solution_variable_list.reserve(num_dofs_per_node);
-        adjoint_solution_variable_list.push_back(&ADJOINT_DISPLACEMENT_X);
-        adjoint_solution_variable_list.push_back(&ADJOINT_DISPLACEMENT_Y);
-        adjoint_solution_variable_list.push_back(&ADJOINT_DISPLACEMENT_Z);
-
-        if(mHasRotationDofs)
-        {
-            adjoint_solution_variable_list.push_back(&ADJOINT_ROTATION_X);
-            adjoint_solution_variable_list.push_back(&ADJOINT_ROTATION_Y);
-            adjoint_solution_variable_list.push_back(&ADJOINT_ROTATION_Z);
-        }
+        (mHasRotationDofs) ? adjoint_solution_variable_list = {&ADJOINT_DISPLACEMENT_X, &ADJOINT_DISPLACEMENT_Y, &ADJOINT_DISPLACEMENT_Z, &ADJOINT_ROTATION_X, &ADJOINT_ROTATION_Y, &ADJOINT_ROTATION_Z} :
+                             adjoint_solution_variable_list = {&ADJOINT_DISPLACEMENT_X, &ADJOINT_DISPLACEMENT_Y, &ADJOINT_DISPLACEMENT_Z};
 
        for (IndexType i = 0; i < num_nodes; ++i)
         {
