423968fa9a643c01af872a90fb8cb4f434b8d99c
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-11-18 10:58:20 +0100

    Missing noalias

--- applications/FemToDemApplication/custom_processes/fix_free_velocity_on_nodes_process.cpp
+++ applications/FemToDemApplication/custom_processes/fix_free_velocity_on_nodes_process.cpp
@@ -75,7 +75,7 @@ void FixFreeVelocityOnNodesProcess::Execute()
                 auto &r_prev_displ    = it_node->FastGetSolutionStepValue(DISPLACEMENT, 1);
                 const auto current_displ_backup = it_node->FastGetSolutionStepValue(DISPLACEMENT_BACKUP, 0);
                 const auto prev_displ_backup    = it_node->FastGetSolutionStepValue(DISPLACEMENT_BACKUP, 1);
-                r_current_displ = current_displ_backup;
+                noalias(r_current_displ) = current_displ_backup;
                 r_prev_displ = prev_displ_backup;
             }
         }
