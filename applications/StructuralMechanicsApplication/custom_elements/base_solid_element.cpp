a1c5c88c3deef040099ebf81af7cbc2060f1f18d
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-11-20 00:30:13 +0100

    Revert "Adding element utilities"
    This reverts commit 4abb03b84e90c07c8b3396a867b94f44a8ad077c.

--- applications/StructuralMechanicsApplication/custom_elements/base_solid_element.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/base_solid_element.cpp
@@ -15,13 +15,11 @@
 // External includes
 
 // Project includes
+#include "includes/checks.h"
+#include "custom_elements/base_solid_element.h"
 #include "utilities/math_utils.h"
 #include "utilities/geometry_utilities.h"
-
-// Application includes
-#include "custom_elements/base_solid_element.h"
 #include "structural_mechanics_application_variables.h"
-#include "custom_utilities/element_utilities.h"
 #include "custom_utilities/structural_mechanics_element_utilities.h"
 
 namespace Kratos
@@ -1413,17 +1411,48 @@ int  BaseSolidElement::Check( const ProcessInfo& rCurrentProcessInfo )
 {
     KRATOS_TRY;
 
-    int check = Element::Check(rCurrentProcessInfo);
+    Element::Check(rCurrentProcessInfo);
 
-    // Basic check
-    check = ElementUtilities::BaseElementCheck(this, rCurrentProcessInfo);
+    const SizeType number_of_nodes = this->GetGeometry().size();
+    const SizeType dimension = this->GetGeometry().WorkingSpaceDimension();
+
+    // Verify that the variables are correctly initialized
+    KRATOS_CHECK_VARIABLE_KEY(DISPLACEMENT)
+    KRATOS_CHECK_VARIABLE_KEY(VELOCITY)
+    KRATOS_CHECK_VARIABLE_KEY(ACCELERATION)
+    KRATOS_CHECK_VARIABLE_KEY(DENSITY)
+    KRATOS_CHECK_VARIABLE_KEY(VOLUME_ACCELERATION)
+    KRATOS_CHECK_VARIABLE_KEY(THICKNESS)
+
+    // Check that the element's nodes contain all required SolutionStepData and Degrees of freedom
+    for ( IndexType i = 0; i < number_of_nodes; i++ ) {
+        const NodeType &rnode = this->GetGeometry()[i];
+        KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(DISPLACEMENT,rnode)
+//         KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(VELOCITY,rnode)
+//         KRATOS_CHECK_VARIABLE_IN_NODAL_DATA(ACCELERATION,rnode)
+
+        KRATOS_CHECK_DOF_IN_NODE(DISPLACEMENT_X, rnode)
+        KRATOS_CHECK_DOF_IN_NODE(DISPLACEMENT_Y, rnode)
+        KRATOS_CHECK_DOF_IN_NODE(DISPLACEMENT_Z, rnode)
+    }
+
+    // Verify that the constitutive law exists
+    KRATOS_ERROR_IF_NOT(this->GetProperties().Has( CONSTITUTIVE_LAW )) << "Constitutive law not provided for property " << this->GetProperties().Id() << std::endl;
+
+    // Verify that the constitutive law has the correct dimension
+    const SizeType strain_size = this->GetProperties().GetValue( CONSTITUTIVE_LAW )->GetStrainSize();
+    if ( dimension == 2 ) {
+        KRATOS_ERROR_IF( strain_size < 3 || strain_size > 4) << "Wrong constitutive law used. This is a 2D element! expected strain size is 3 or 4 (el id = ) " << this->Id() << std::endl;
+    } else {
+        KRATOS_ERROR_IF_NOT(strain_size == 6) << "Wrong constitutive law used. This is a 3D element! expected strain size is 6 (el id = ) "<<  this->Id() << std::endl;
+    }
 
     // Check constitutive law
     if ( mConstitutiveLawVector.size() > 0 ) {
         return mConstitutiveLawVector[0]->Check( GetProperties(), GetGeometry(), rCurrentProcessInfo );
     }
 
-    return check;
+    return 0;
 
     KRATOS_CATCH( "" );
 }
