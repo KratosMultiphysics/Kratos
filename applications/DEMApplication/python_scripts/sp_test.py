1425a4612cca21171ebb20fe44d8fa11ec6dabba
Unknown <farrufat@cimne.upc.edu>
2019-11-21 10:51:35 +0100

    update

--- applications/DEMApplication/python_scripts/sp_test.py
+++ applications/DEMApplication/python_scripts/sp_test.py
@@ -16,72 +16,92 @@ class SPTest(DEM_material_test_script.MaterialTest):
       super(SPTest,self).__init__(DEM_parameters, procedures, solver, graphs_path, post_path, spheres_model_part, rigid_face_model_part)
 
   def Initialize(self):
-      super(SPTest,self).Initialize()
-      self.PrepareTestSP()
+    self.PrepareTests()
+    self.DefineBoundaries()
 
-  def PrepareTestSP(self):
+
+  def PrepareTests(self):
+
+    ##Fixing vertical
     if self.test_type == "SandP":
+        for node in self.TOP:
+            node.SetSolutionStepValue(VELOCITY_Z, 0.0)
+            node.Fix(VELOCITY_Z)
 
-        ####### Correction Coefs  TODO 0.25* for cylinder section EXXON
-        self.alpha_top = 3.141592*self.diameter*self.diameter*0.25/(self.xtop_area + 0.70710678*self.xtopcorner_area)
-        self.alpha_bot = 3.141592*self.diameter*self.diameter*0.25/(self.xbot_area + 0.70710678*self.xbotcorner_area)
-        self.alpha_lat = 3.141592*self.diameter*self.height/(self.xlat_area + 0.70710678*self.xtopcorner_area + 0.70710678*self.xbotcorner_area)
+    if self.test_type == "SandP":
+        absolute_path_to_file = os.path.join(self.graphs_path, self.problem_name + "_graph.grf")
+        self.graph_export   = open(absolute_path_to_file, 'w')
+
+        self.Procedures.KratosPrintInfo('Initial Height of the Model: ' + str(self.height)+'\n')
+
+        (self.xlat_area) = self.CircularSkinDetermination()
+
+
+  def CircularSkinDetermination(self):
+
+        # Cylinder dimensions
+        d = self.diameter
+        eps = 2.0
+
+        perimeter = 3.141592 * d
+        xlat_area = 0.0
+
+        for element in self.spheres_model_part.Elements:
+
+            element.GetNode(0).SetSolutionStepValue(SKIN_SPHERE, 0)
+
+            node = element.GetNode(0)
+            r = node.GetSolutionStepValue(RADIUS)
+            x = node.X
+            y = node.Y
+
+            cross_section = 2.0 * r
+
+            if ((x * x + y * y) >= ((d / 2 - eps * r) * (d / 2 - eps * r))):
+
+                element.GetNode(0).SetSolutionStepValue(SKIN_SPHERE, 1)
+                self.LAT.append(node)
+                xlat_area = xlat_area + cross_section
+
+        if(len(self.LAT)==0):
+            self.Procedures.KratosPrintWarning("ERROR! in Circular Skin Determination - NO LATERAL PARTICLES" + "\n")
+
+        return (xlat_area)
+
 
   def MeasureForcesAndPressure(self):
     super(SPTest,self).MeasureForcesAndPressure()
-
-    average_value = 0.0
-    # for node in self.top_mesh_nodes:
-
-    #   force_node_y = node.GetSolutionStepValue(ELASTIC_FORCES)[1]
-
-    #   total_force_top += force_node_y
-
-    # self.total_stress_top = total_force_top/(self.MeasuringSurface)
-
+    average_zstress_value = 0.0
 
     if self.test_type == "SandP":
-
-      # self.Pressure = min(self.total_stress, self.ConfinementPressure * 1e6)
-      # self.aux = AuxiliaryUtilities() from material test init
-      average_value = self.aux.ComputeAverageZStressFor2D(self.spheres_model_part)
-      print (average_value)
-
-      # aixo estara immposat desde stage
-      # self.spheres_model_part.ProcessInfo.SetValue(IMPOSED_Z_STRAIN_VALUE, -1e-6 * self.time)
-
-      # self.customapply(z_tensor_value, self.XLAT, self.XBOT, self.XTOP, self.XBOTCORNER, self.XTOPCORNER,self.alpha_top,self.alpha_bot,self.alpha_lat)
+      average_zstress_value = self.aux.ComputeAverageZStressFor2D(self.spheres_model_part)
+      print (average_zstress_value)
+      self.ApplyLateralStress(average_zstress_value, self.LAT)
 
 
 
+  def ApplyLateralStress(self, average_zstress_value, LAT):
 
-
-
-  def customapply(self, Pressure, XLAT, XBOT, XTOP, XBOTCORNER, XTOPCORNER, alpha_top, alpha_bot, alpha_lat):
-
-      for node in XLAT:
+      for node in LAT:
 
           r = node.GetSolutionStepValue(RADIUS)
           x = node.X
           y = node.Y
-          z = node.Z
 
           values = Array3()
           vect = Array3()
 
-          cross_section = 3.141592 * r * r
+          cross_section = 2.0 * r
 
           # vector normal al centre:
-          vect_moduli = math.sqrt(x * x + z * z)
+          vect_moduli = math.sqrt(x * x + y * y)
 
           if(vect_moduli > 0.0):
               vect[0] = -x / vect_moduli
-              vect[1] = 0
-              vect[2] = -z / vect_moduli
+              vect[1] = -y / vect_moduli
 
-          values[0] = cross_section * alpha_lat * Pressure * vect[0]
-          values[1] = 0.0
-          values[2] = cross_section * alpha_lat * Pressure * vect[2]
+          values[0] = cross_section * average_zstress_value * vect[0]
+          values[1] = cross_section * average_zstress_value * vect[1]
 
           node.SetSolutionStepValue(EXTERNAL_APPLIED_FORCE, values)
 
