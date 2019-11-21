d8ba0827e050d30dc3432e8404b15dad4001cd4f
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2019-11-21 15:05:50 +0100

    Refactor ComputeEquivalentF

--- applications/StructuralMechanicsApplication/custom_elements/small_displacement.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/small_displacement.cpp
@@ -295,24 +295,7 @@ void SmallDisplacement::ComputeEquivalentF(
     const Vector& rStrainTensor
     ) const
 {
-    const SizeType dim = GetGeometry().WorkingSpaceDimension();
-
-    if(dim == 2) {
-        rF(0,0) = 1.0+rStrainTensor(0);
-        rF(0,1) = 0.5*rStrainTensor(2);
-        rF(1,0) = 0.5*rStrainTensor(2);
-        rF(1,1) = 1.0+rStrainTensor(1);
-    } else {
-        rF(0,0) = 1.0+rStrainTensor(0);
-        rF(0,1) = 0.5*rStrainTensor(3);
-        rF(0,2) = 0.5*rStrainTensor(5);
-        rF(1,0) = 0.5*rStrainTensor(3);
-        rF(1,1) = 1.0+rStrainTensor(1);
-        rF(1,2) = 0.5*rStrainTensor(4);
-        rF(2,0) = 0.5*rStrainTensor(5);
-        rF(2,1) = 0.5*rStrainTensor(4);
-        rF(2,2) = 1.0+rStrainTensor(2);
-    }
+    ElementUtilities::ComputeEquivalentF(this, rF, rStrainTensor);
 }
 
 /***********************************************************************************/
