45e8e8d371baf59455a9ee2a4955a506580a2d0a
Philipp <philipp.bucher@tum.de>
2019-11-21 17:47:37 +0100

    starting to fill C-dummy solver

--- applications/CoSimulationApplication/custom_helpers/dummy_solver/c/dummy_solver.c
+++ applications/CoSimulationApplication/custom_helpers/dummy_solver/c/dummy_solver.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "co_sim_c_io.h"
 
 void abc() {
@@ -8,6 +9,7 @@ void abc() {
  int puts(const char *s)
  {
     printf("Called the puts function!");
+    return 0;
  }
 
 
@@ -15,6 +17,18 @@ void abc() {
 int main()
 {
     CoSimIO_Connect("aaa", "ccc");
+
+
+    int incoming_data_size = 1;
+
+    double**
+    values_raw = (double**)malloc(sizeof(double*)*1);
+    values_raw[0]=(double*)malloc(sizeof(double)*incoming_data_size);
+
+    CoSimIO_ImportData("aaa", "bbb", &incoming_data_size, values_raw);
+
+    free(values_raw[1]);
+    free(values_raw);
 
     // printf() displays the string inside quotation
     printf("Hello, World!");
