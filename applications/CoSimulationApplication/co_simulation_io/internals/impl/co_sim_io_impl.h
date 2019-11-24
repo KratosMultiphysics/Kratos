3ceeb4e2c4a9a9ca4cca580ece2515f013b2a702
Unknown <philipp.bucher@tum.de>
2019-11-24 09:37:37 +0100

    minor renaming for consistency

--- applications/CoSimulationApplication/co_simulation_io/internals/impl/co_sim_io_impl.h
+++ applications/CoSimulationApplication/co_simulation_io/internals/impl/co_sim_io_impl.h
@@ -33,17 +33,17 @@ namespace CoSimIO {
 
 namespace Internals {
 // TODO make sure this is unique even across compilation units (test somehow)
-static std::unordered_map<std::string, std::unique_ptr<CoSimConnection>> s_co_sim_ios;
+static std::unordered_map<std::string, std::unique_ptr<CoSimConnection>> s_co_sim_connections;
 
 static bool HasIO(const std::string& rConnectionName)
 {
-    return s_co_sim_ios.find(rConnectionName) != s_co_sim_ios.end();
+    return s_co_sim_connections.find(rConnectionName) != s_co_sim_connections.end();
 }
 
-static CoSimConnection& GetIO(const std::string& rConnectionName)
+static CoSimConnection& GetConnection(const std::string& rConnectionName)
 {
     KRATOS_CO_SIM_ERROR_IF_NOT(HasIO(rConnectionName)) << "Trying to use connection \"" << rConnectionName << "\" which does not exist!" << std::endl;
-    return *s_co_sim_ios.at(rConnectionName);
+    return *s_co_sim_connections.at(rConnectionName);
 }
 
 } // namespace Internals
@@ -53,8 +53,8 @@ inline void Connect(const std::string& rConnectionName, const std::string& pSett
     using namespace Internals;
     KRATOS_CO_SIM_ERROR_IF(HasIO(rConnectionName)) << "A connection for \"" << rConnectionName << "\" already exists!" << std::endl;
 
-    s_co_sim_ios[std::string(rConnectionName)] = std::unique_ptr<CoSimConnection>(new CoSimConnection(rConnectionName, pSettingsFileName));
-    GetIO(rConnectionName).Connect();
+    s_co_sim_connections[std::string(rConnectionName)] = std::unique_ptr<CoSimConnection>(new CoSimConnection(rConnectionName, pSettingsFileName));
+    GetConnection(rConnectionName).Connect();
 }
 
 inline void Disconnect(const std::string& rConnectionName)
@@ -62,8 +62,8 @@ inline void Disconnect(const std::string& rConnectionName)
     using namespace Internals;
     KRATOS_CO_SIM_ERROR_IF_NOT(HasIO(rConnectionName)) << "Trying to disconnect connection \"" << rConnectionName << "\" which does not exist!" << std::endl;
 
-    GetIO(rConnectionName).Disconnect();
-    s_co_sim_ios.erase(std::string(rConnectionName));
+    GetConnection(rConnectionName).Disconnect();
+    s_co_sim_connections.erase(std::string(rConnectionName));
 }
 
 // Version for C++, there this input is a std::vector, which we have to wrap before passing it on
@@ -76,7 +76,7 @@ inline void ImportData(
 {
     using namespace CoSimIO::Internals;
     std::unique_ptr<DataContainer<double>> p_container(new DataContainerStdVector<double>(pData));
-    GetIO(rConnectionName).ImportData(rIdentifier, rSize, *p_container);
+    GetConnection(rConnectionName).ImportData(rIdentifier, rSize, *p_container);
 }
 
 // Version for C and fortran, there we already get a container
@@ -87,7 +87,7 @@ inline void ImportData(
     int& rSize,
     CoSimIO::Internals::DataContainer<double>& pData)
 {
-    Internals::GetIO(rConnectionName).ImportData(rIdentifier, rSize, pData);
+    Internals::GetConnection(rConnectionName).ImportData(rIdentifier, rSize, pData);
 }
 
 // Version for C++, there this input is a std::vector, which we have to wrap before passing it on
@@ -100,7 +100,7 @@ inline void ExportData(
 {
     using namespace CoSimIO::Internals;
     std::unique_ptr<DataContainer<double>> p_container(new DataContainerStdVector<double>(pData));
-    GetIO(rConnectionName).ExportData(rIdentifier, Size, *p_container);
+    GetConnection(rConnectionName).ExportData(rIdentifier, Size, *p_container);
 }
 
 // Version for C and fortran, there we already get a container
@@ -111,7 +111,7 @@ inline void ExportData(
     const int Size,
     CoSimIO::Internals::DataContainer<double>& pData)
 {
-    Internals::GetIO(rConnectionName).ExportData(rIdentifier, Size, pData);
+    Internals::GetConnection(rConnectionName).ExportData(rIdentifier, Size, pData);
 }
 
 template<>
@@ -128,7 +128,7 @@ inline void ImportMesh(
     std::unique_ptr<DataContainer<double>> p_container_coords(new DataContainerStdVector<double>(pNodalCoordinates));
     std::unique_ptr<DataContainer<int>> p_container_conn(new DataContainerStdVector<int>(pElementConnectivities));
     std::unique_ptr<DataContainer<int>> p_container_types(new DataContainerStdVector<int>(pElementTypes));
-    Internals::GetIO(rConnectionName).ImportMesh(rIdentifier, rNumberOfNodes, rNumberOfElements, *p_container_coords, *p_container_conn, *p_container_types);
+    Internals::GetConnection(rConnectionName).ImportMesh(rIdentifier, rNumberOfNodes, rNumberOfElements, *p_container_coords, *p_container_conn, *p_container_types);
 }
 
 template<>
@@ -141,7 +141,7 @@ inline void ImportMesh(
     CoSimIO::Internals::DataContainer<int>& pElementConnectivities,
     CoSimIO::Internals::DataContainer<int>& pElementTypes)
 {
-    Internals::GetIO(rConnectionName).ImportMesh(rIdentifier, rNumberOfNodes, rNumberOfElements, pNodalCoordinates, pElementConnectivities, pElementTypes);
+    Internals::GetConnection(rConnectionName).ImportMesh(rIdentifier, rNumberOfNodes, rNumberOfElements, pNodalCoordinates, pElementConnectivities, pElementTypes);
 }
 
 template<>
@@ -158,7 +158,7 @@ inline void ExportMesh(
     std::unique_ptr<DataContainer<double>> p_container_coords(new DataContainerStdVector<double>(pNodalCoordinates));
     std::unique_ptr<DataContainer<int>> p_container_conn(new DataContainerStdVector<int>(pElementConnectivities));
     std::unique_ptr<DataContainer<int>> p_container_types(new DataContainerStdVector<int>(pElementTypes));
-    Internals::GetIO(rConnectionName).ExportMesh(rIdentifier, NumberOfNodes, NumberOfElements, *p_container_coords, *p_container_conn, *p_container_types);
+    Internals::GetConnection(rConnectionName).ExportMesh(rIdentifier, NumberOfNodes, NumberOfElements, *p_container_coords, *p_container_conn, *p_container_types);
 }
 
 template<>
@@ -171,17 +171,17 @@ inline void ExportMesh(
     CoSimIO::Internals::DataContainer<int>& pElementConnectivities,
     CoSimIO::Internals::DataContainer<int>& pElementTypes)
 {
-    Internals::GetIO(rConnectionName).ExportMesh(rIdentifier, NumberOfNodes, NumberOfElements, pNodalCoordinates, pElementConnectivities, pElementTypes);
+    Internals::GetConnection(rConnectionName).ExportMesh(rIdentifier, NumberOfNodes, NumberOfElements, pNodalCoordinates, pElementConnectivities, pElementTypes);
 }
 
 inline void IsConverged(const std::string& rConnectionName, int* pConvergenceSignal)
 {
-    Internals::GetIO(rConnectionName).IsConverged(pConvergenceSignal);
+    Internals::GetConnection(rConnectionName).IsConverged(pConvergenceSignal);
 }
 
 inline void Run(const std::string& rConnectionName)
 {
-    Internals::GetIO(rConnectionName).Run();
+    Internals::GetConnection(rConnectionName).Run();
 }
 
 template<typename TFunctionType>
@@ -193,7 +193,7 @@ inline void Register(
     // TODO If I use structs (base-class etc, constructed right here) then I can save all of them in one map and do the corresponding actions with them, since I know thich interface-fct to call!
     // => this means that I have to specialize this fct, but the CoSimConnection class will be much cleaner
     // (maybe internally they could be registered as void* to void* but probably not (a good idea))
-    Internals::GetIO(rConnectionName).Register(rFunctionName, rFunction);
+    Internals::GetConnection(rConnectionName).Register(rFunctionName, rFunction);
 }
 
 } // namespace CoSimIO
