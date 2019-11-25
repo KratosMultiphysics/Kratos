6a0581a62e82ba18d861705e9d6c76617726645b
Carlos Roig <roigcarlo@gmail.com>
2019-11-25 10:40:03 +0100

    Reducing the list of default apps

--- configure.bat
+++ configure.bat
@@ -22,21 +22,9 @@ set PYTHON_EXECUTABLE=C:\Users\Kratos64\AppData\Local\Programs\Python\Python35\p
 
 rem Set applications to compile
 set KRATOS_APPLICATIONS=
+CALL :add_app %KRATOS_APP_DIR%\ExternalSolversApplication;
 CALL :add_app %KRATOS_APP_DIR%\StructuralMechanicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\ContactStructuralMechanicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\FluidDynamicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\MeshingApplication;
-CALL :add_app %KRATOS_APP_DIR%\MeshMovingApplication;
-CALL :add_app %KRATOS_APP_DIR%\DEMApplication;
-CALL :add_app %KRATOS_APP_DIR%\SwimmingDEMApplication;
-CALL :add_app %KRATOS_APP_DIR%\CSharpWrapperApplication;
-CALL :add_app %KRATOS_APP_DIR%\SolidMechanicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\ConstitutiveModelsApplication;
-CALL :add_app %KRATOS_APP_DIR%\DelaunayMeshingApplication;
-CALL :add_app %KRATOS_APP_DIR%\ContactMechanicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\PfemApplication;
-CALL :add_app %KRATOS_APP_DIR%\PfemFluidDynamicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\PfemSolidMechanicsApplication;
 
 rem Clean
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\cmake_install.cmake"
