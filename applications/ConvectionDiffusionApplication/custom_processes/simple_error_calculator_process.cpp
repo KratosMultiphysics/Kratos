951cb14dfb9f568b3dec9643848cdfd4e12ccaa2
Saransh <saransh.saxena@tum.de>
2019-11-25 14:21:29 +0100

    Updated Code

--- applications/ConvectionDiffusionApplication/custom_processes/simple_error_calculator_process.cpp
+++ applications/ConvectionDiffusionApplication/custom_processes/simple_error_calculator_process.cpp
@@ -17,3 +17,36 @@
 // Project includes
 #include "custom_processes/simple_error_calculator_process.h"
 #include "utilities/variable_utils.h"
+
+namespace Kratos
+{
+template<SizeType TDim>
+SimpleErrorCalculatorProcess<TDim>::SimpleErrorCalculatorProcess(ModelPart& rThisModelPart, Parameters ThisParameters):mrThisModelPart(rThisModelPart)
+{
+    //WIP
+    Parameters default_parameters = Parameters(R"(
+    {
+        "minimal_size"                        : 0.01,
+        "maximal_size"                        : 10.0,
+        "refinement_strategy"                 : "Simple_Error_Calculator",
+        "reference_variable_name"             : "ERROR_RATIO",
+        "mean_distribution_strategy":
+        {
+            "target_refinement_coefficient"       : 0.9,
+            "refinement_bound"                    : 2.0,
+            "reference_norm_name"                 : "VELOCITY_H1_SEMINORM"
+        },
+        "maximum_strategy":
+        {
+            "target_refinement_coefficient"       : 0.1,
+            "refinement_coefficient"              : 2.0
+        },
+        "global_tolerance_strategy":
+        {
+            "global_tolerance"                 : 0.1
+        },
+        "echo_level"                          : 0
+    })"
+    );
+
+    ThisParameters.RecursivelyValidateAndAssignDefaults(default_parameters);
