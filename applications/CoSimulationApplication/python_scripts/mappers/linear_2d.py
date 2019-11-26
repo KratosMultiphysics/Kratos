d176b7a90f30ef6d186c25c4b8d2fb679ef162f6
Toon Demeester <toon.demeester@ugent.be>
2019-11-26 17:43:53 +0100

    Add 2D linear mapper and test

--- applications/CoSimulationApplication/python_scripts/mappers/linear_2d.py
+++ applications/CoSimulationApplication/python_scripts/mappers/linear_2d.py
@@ -14,8 +14,9 @@ class MapperLinear1D(object):
     def __init__(self, parameters):
         """
         This mapper uses the two closest points; this does
-        not mean that the point to be interpolated lies in
-        between them, e.g. when there is grid stretching.
+        not mean that the projection of the point to be
+        interpolated lies in between them, e.g. when
+        there is grid stretching.
         This can lead to discontinuities if grid spacing
         changes quickly.
 
@@ -27,40 +28,60 @@ class MapperLinear1D(object):
         self.settings = parameters['settings']
 
     def Initialize(self, model_part_from, model_part_to):
-        coord = self.settings['direction'].GetString().upper()
-        if coord not in ['X', 'Y', 'Z']:
-            raise ValueError(f'{coord} is not a valid direction.')
+        coord1 = self.settings['direction_1'].GetString().upper()
+        coord2 = self.settings['direction_2'].GetString().upper()
+        for coord in [coord1, coord2]:
+            if coord not in ['X', 'Y', 'Z']:
+                raise ValueError(f'{coord} is not a valid direction.')
 
         self.n_from = model_part_from.NumberOfNodes()
-        coords_from = np.zeros((self.n_from, 1))
+        coords_from = np.zeros((self.n_from, 2))
         for i, node in enumerate(model_part_from.Nodes):
-            coords_from[i] = getattr(node, coord)
+            coords_from[i, 0] = getattr(node, coord1)
+            coords_from[i, 1] = getattr(node, coord2)
 
         self.n_to = model_part_to.NumberOfNodes()
-        coords_to = np.zeros((self.n_to, 1))
+        coords_to = np.zeros((self.n_to, 2))
         for i, node in enumerate(model_part_to.Nodes):
-            coords_to[i] = getattr(node, coord)
+            coords_to[i, 0] = getattr(node, coord1)
+            coords_to[i, 1] = getattr(node, coord2)
 
         tree = cKDTree(coords_from)  # time-intensive part
         _, self.nearest = tree.query(coords_to, k=2, n_jobs=-1)
 
         # linear interpolation/extrapolation from nearest points
+        def normalize(v):
+            n = v / np.linalg.norm(v)
+            return n
+
+        # loop over all points
         self.coeffs = np.zeros((self.n_to, 2))
         for i in range(self.n_to):
-            x = coords_to[i]
-            x_1 = coords_from[self.nearest[i, 0]]
-            x_2 = coords_from[self.nearest[i, 1]]
-            if x_1 == x_2:
-                print(x, x_1, x_2)
+            p_0 = coords_to[i, :]  # point to be projected
+            p_1 = coords_from[self.nearest[i, 0], :]  # nearest point 1
+            p_2 = coords_from[self.nearest[i, 1], :]  # nearest point 2
+            v_01 = p_1 - p_0
+            v_12 = p_2 - p_1  # vector from 1 to 2
+            v_t = normalize(v_12)  # tangential vector
+            v_para = v_t * np.dot(v_t, v_01)
+            v_perp = v_01 - v_para
+            p = p_0 + v_perp  # projected point
 
+            x, x_1, x_2 = p[0], p_1[0], p_2[0]
             c_1 = (x - x_2) / (x_1 - x_2)
             c_2 = (x - x_1) / (x_2 - x_1)
             self.coeffs[i, :] = [c_1, c_2]
+
+            error = np.abs(1 - (c_1 + c_2))
+            if error > 1e-12:
+                raise ValueError(f'sum of coefficients is not 1 (error = {error:.1e})')
 
     def Finalize(self):
         pass
 
     def __call__(self, args_from, args_to):
+        # exactly the same as linear_1d
+
         model_part_from, var_from = args_from
         model_part_to, var_to = args_to
 
--- applications/CoSimulationApplication/tests/mappers/test_linear.py
+++ applications/CoSimulationApplication/tests/mappers/test_linear.py
@@ -61,6 +61,69 @@ class TestMapperLinear(KratosUnittest.TestCase):
             plt.show()
             plt.close()
 
+    def test_mapper_linear_2d(self):
+
+        parameter_file_name = 'test_linear_2d.json'   #***
+
+        cs_data_structure = ImportDataStructure(parameter_file_name)
+        with open(parameter_file_name, 'r') as parameter_file:
+            parameters = cs_data_structure.Parameters(parameter_file.read())
+
+        # values on straight line, irregular grid spacing
+        var_from = vars(KM)["TEMPERATURE"]
+        model_from = cs_data_structure.Model()
+        model_part_from = model_from.CreateModelPart('wall_from')
+        model_part_from.AddNodalSolutionStepVariable(var_from)
+
+        n_from = 30
+        x_from = np.sqrt(np.linspace(.1, .9, n_from))
+        y_from = 3.3 * x_from
+        v_from = -1.7 * x_from + 2.5 * y_from
+        for i in range(n_from):
+            node = model_part_from.CreateNewNode(i, x_from[i], y_from[i], 0.)
+            node.SetSolutionStepValue(var_from, 0, v_from[i])
+
+        var_to = vars(KM)["PRESSURE"]
+        model_to = cs_data_structure.Model()
+        model_part_to = model_to.CreateModelPart('wall_to')
+        model_part_to.AddNodalSolutionStepVariable(var_to)
+
+        n_to = 25
+        x_to = np.sqrt(np.linspace(0, 1, n_to))
+        y_to = 3.3 * x_to
+        for i in range(n_to):
+            model_part_to.CreateNewNode(i, x_to[i], y_to[i], 0.)
+
+        mapper = cs_tools.CreateInstance(parameters['mapper'])
+        mapper.Initialize(model_part_from, model_part_to)
+        mapper((model_part_from, var_from), (model_part_to, var_to))
+
+        v_to = np.zeros(n_to)
+        for i, node in enumerate(model_part_to.Nodes):
+            v_to[i] = node.GetSolutionStepValue(var_to)
+
+        for i in range(n_to):
+            self.assertAlmostEqual(-1.7 * x_to[i] + 2.5 * y_to[i], v_to[i], delta=1e-8)
+
+        if False:
+            # visualization of results
+            _, ax = plt.subplots(ncols=2)
+
+            ax[0].plot(x_from, v_from, 'b', label='from', linewidth=2.5)
+            ax[0].scatter(x_from, v_from, s=20, color='b')
+            ax[0].plot(x_to, v_to, 'r', label='to', linewidth=1.5)
+            ax[0].scatter(x_to, v_to, s=15, color='r')
+
+            ax[1].plot(y_from, v_from, 'b', label='from', linewidth=2.5)
+            ax[1].scatter(y_from, v_from, s=20, color='b')
+            ax[1].plot(y_to, v_to, 'r', label='to', linewidth=1.5)
+            ax[1].scatter(y_to, v_to, s=15, color='r')
+
+            for a in ax:
+                a.legend()
+            plt.show()
+            plt.close()
+
 
 if __name__ == '__main__':
     KratosUnittest.main()
