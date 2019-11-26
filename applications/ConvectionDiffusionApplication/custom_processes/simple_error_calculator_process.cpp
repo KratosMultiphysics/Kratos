0e730694c5ab207496025dca1609c0471d2196cd
Saransh <saaransh55@outlook.com>
2019-11-27 00:30:32 +0100

    Working on Error Process, Retrieved Temperature

--- applications/ConvectionDiffusionApplication/custom_processes/simple_error_calculator_process.cpp
+++ applications/ConvectionDiffusionApplication/custom_processes/simple_error_calculator_process.cpp
@@ -30,23 +30,77 @@ SimpleErrorCalculatorProcess<TDim>::SimpleErrorCalculatorProcess(ModelPart& rThi
         "maximal_size"                        : 10.0,
         "refinement_strategy"                 : "Simple_Error_Calculator",
         "reference_variable_name"             : "ERROR_RATIO",
-        "mean_distribution_strategy":
-        {
-            "target_refinement_coefficient"       : 0.9,
-            "refinement_bound"                    : 2.0,
-            "reference_norm_name"                 : "VELOCITY_H1_SEMINORM"
-        },
-        "maximum_strategy":
-        {
-            "target_refinement_coefficient"       : 0.1,
-            "refinement_coefficient"              : 2.0
-        },
-        "global_tolerance_strategy":
-        {
-            "global_tolerance"                 : 0.1
-        },
         "echo_level"                          : 0
     })"
     );
 
     ThisParameters.RecursivelyValidateAndAssignDefaults(default_parameters);
+    mMinSize = ThisParameters["minimal_size"].GetDouble();
+    mMaxSize = ThisParameters["maximal_size"].GetDouble();
+    mEchoLevel = ThisParameters["echo_level"].GetInt();
+    mReferenceVariable = ThisParameters["reference_variable_name"].GetString();
+}
+
+template<SizeType TDim>
+void SimpleErrorCalculatorProcess<TDim>::Execute()
+{
+    // Initialize the metric
+    // a) Check for Metric Scalar in Meshing Application
+    KRATOS_ERROR_IF_NOT(KratosComponents<Variable<double>>::Has("METRIC_SCALAR")) << "Import Meshing Application" <<std::endl;
+    const double& scalar_variable = KratosComponents<Variable<double>>::Get("METRIC_SCALAR");
+
+    // b) Retrive Nodes and Elements from the Model Part
+    NodesArrayType& nodes_array = mrThisModelPart.Nodes();
+    KRATOS_DEBUG_ERROR_IF(nodes_array.size() == 0) <<  "ERROR:: Empty list of nodes" << std::endl;
+    ElementsArrayType& elements_array = mrThisModelPart.Elements();
+    KRATOS_DEBUG_ERROR_IF(elements_array.size() == 0) <<  "ERROR:: Empty list of elements" << std::endl;
+
+    // c) Initialize Metric Scalar for all nodes to 0
+    const auto it_node_begin = nodes_array.begin();
+    if (it_node_begin->Has(scalar_variable) == false) {
+        // Iterate over the nodes
+        #pragma omp parallel for
+        for(int i = 0; i < static_cast<int>(nodes_array.size()); ++i)
+            (it_node_begin + i)->SetValue(scalar_variable, 0.0);
+    }
+
+    // d) Call the Error Estimation Calculator
+    ErrorEstimatorImplementation();
+
+    // e) Call the MetricScalar Function 
+    CalculateMetricScalar();
+}
+
+template<SizeType TDim>
+void SimpleErrorCalculatorProcess<TDim>::ErrorEstimatorImplementation()
+{
+    // a) Obtain Nodes and Elements from Model Part
+    NodesArrayType& nodes_array = mrThisModelPart.Nodes();
+    ElementsArrayType& elements_array = mrThisModelPart.Elements();
+
+    // b) Loop over Elements and calculate RHS
+    const int number_nodes = static_cast<int>(nodes_array.size());
+    const int number_elements = static_cast<int>(elements_array.size());
+    KRATOS_DEBUG_ERROR_IF(number_nodes == 0) <<  "ERROR:: Empty list of nodes" << std::endl;
+
+    array_1d<double, number_elements> elem_sigma; // Container for element sigma star value
+    const auto it_elem_begin = elements_array.begin();
+    auto n_nodes = it_elem_begin->GetGeometry().size();
+    BoundedMatrix<double, n_nodes, TDim> DN_DX; // Container for shape function gradient 
+    // Loop over the elements
+    for (int i_elem = 0; i_elem < number_elements; ++i_elem) {
+        auto it_elem = it_elem_begin + i_elem;
+        auto r_geometry = it_elem->GetGeometry();
+        auto n_nodes = r_geomentry.size();
+        array_1d<double, n_nodes> nodal_temp; 
+        for (unsigned int i_node = 0; i_node < n_nodes; ++i_node) {
+            nodal_temp[i_node] = r_geometry[i].FastGetSolutionStepValue(TEMPERATURE)
+        }
+        //const GeometryType::IntegrationPointsArrayType& integration_points = r_geometry.IntegrationPoints( GeometryData::GI_GAUSS_1 );
+        auto integration_points = r_geometry.IntegrationPoints( GeometryData::GI_GAUSS_1 );
+        const unsigned int NumGPoints = integration_points.size();
+        GeometryType::ShapeFunctionsGradientsType DN_DXContainer(NumGPoints);
+        r_geometry.ShapeFunctionsIntegrationPointsGradients(DN_DXContainer, GeometryData::GI_GAUSS_1);
+        noalias(DN_DX) = DN_DXContainer[0];
+
+}
