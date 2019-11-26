b244a6fe886bf3f9ba711e47cad0159a0dfb4f3f
Philipp <philipp.bucher@tum.de>
2019-11-26 19:55:44 +0100

    corrections in KratosCoSimIO

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos_co_sim_io.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos_co_sim_io.py
@@ -1,10 +1,14 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
+# Importing the Kratos Library
+import KratosMultiphysics as KM
+
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_io import CoSimulationIO
 
 # CoSimulation imports
-import KratosMultiphysics.CoSimulationApplication.CoSimIO as CoSimIO
+import KratosMultiphysics.CoSimulationApplication as KratosCoSim
+import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
 
 # Other imports
 import os
@@ -18,27 +22,29 @@ class KratosCoSimIO(CoSimulationIO):
     def __init__(self, settings, model, solver_name):
         super(KratosCoSimIO, self).__init__(settings, model, solver_name)
 
-        CoSimIO.Connect(self.solver_name, ParametersToStringDict(self.settings))
+        print(cs_tools.ParametersToStringDict(self.settings))
+
+        KratosCoSim.CoSimIO.Connect(self.solver_name, cs_tools.ParametersToStringDict(self.settings))
 
     def Finalize(self):
-        CoSimIO.Disconnect(self.solver_name)
+        KratosCoSim.CoSimIO.Disconnect(self.solver_name)
 
     def ImportCouplingInterface(self, interface_config):
         model_part_name = interface_config["model_part_name"]
-        CoSimIO.ImportMesh(self.solver_name, model_part_name, self.model[model_part_name]) # TODO this can also be geometry at some point
+        KratosCoSim.CoSimIO.ImportMesh(self.solver_name, model_part_name, self.model[model_part_name]) # TODO this can also be geometry at some point
 
-    def ImportCouplingInterface(self, interface_config):
+    def ExportCouplingInterface(self, interface_config):
         model_part_name = interface_config["model_part_name"]
-        CoSimIO.ExportMesh(self.solver_name, model_part_name, self.model[model_part_name]) # TODO this can also be geometry at some point
+        KratosCoSim.CoSimIO.ExportMesh(self.solver_name, model_part_name, self.model[model_part_name]) # TODO this can also be geometry at some point
 
     def ImportData(self, data_config):
         data_type = data_config["type"]
         if data_type == "coupling_interface_data":
             interface_data = data_config["interface_data"]
-            CoSimIO.ImportData(self.solver_name, interface_data.name, interface_data.GetModelPart(), interface_data.variable, GetDataLocation(interface_data.location))
+            KratosCoSim.CoSimIO.ImportData(self.solver_name, interface_data.name, interface_data.GetModelPart(), interface_data.variable, GetDataLocation(interface_data.location))
 
         elif data_type == "time":
-            time_list = KratosCoSim.CoSimIO.ImportData("time_to_co_sim", time_list)
+            time_list = KratosCoSim.KratosCoSim.CoSimIO.ImportData("time_to_co_sim", time_list)
             if len(time_list) != 1:
                 raise Exception("Wrong size received!")
             data_config["time"] = time_list[0]
@@ -49,22 +55,22 @@ class KratosCoSimIO(CoSimulationIO):
         data_type = data_config["type"]
         if data_type == "coupling_interface_data":
             interface_data = data_config["interface_data"]
-            CoSimIO.ExportData(self.solver_name, interface_data.name, interface_data.GetModelPart(), interface_data.variable, GetDataLocation(interface_data.location))
+            KratosCoSim.CoSimIO.ExportData(self.solver_name, interface_data.name, interface_data.GetModelPart(), interface_data.variable, GetDataLocation(interface_data.location))
 
         elif data_type == "control_signal":
             control_signal_key = data_config["signal"]
-            CoSimIO.SendControlSignal(self.solver_name, data_config["identifier"], control_signal_key)
+            KratosCoSim.CoSimIO.SendControlSignal(self.solver_name, data_config["identifier"], control_signal_key)
 
         elif data_type == "time":
             current_time = data_config["time"]
-            CoSimIO.ExportData(self.solver_name, "time_from_co_sim", [current_time])
+            KratosCoSim.CoSimIO.ExportData(self.solver_name, "time_from_co_sim", [current_time])
 
         elif data_type == "convergence_signal":
             if data_config["is_converged"]:
-                control_signal_key = CoSimIO.ControlSignal.ConvergenceAchieved
+                control_signal_key = KratosCoSim.CoSimIO.ControlSignal.ConvergenceAchieved
             else:
-                control_signal_key = CoSimIO.ControlSignal.Dummy
-            CoSimIO.SendControlSignal(self.solver_name, "", control_signal_key)
+                control_signal_key = KratosCoSim.CoSimIO.ControlSignal.Dummy
+            KratosCoSim.CoSimIO.SendControlSignal(self.solver_name, "", control_signal_key)
         else:
             raise NotImplementedError('Exporting interface data of type "{}" is not implemented for this IO: "{}"'.format(data_type, self._ClassName()))
 
@@ -84,32 +90,12 @@ class KratosCoSimIO(CoSimulationIO):
         this_defaults.AddMissingParameters(super(KratosCoSimIO, cls)._GetDefaultSettings())
         return this_defaults
 
-
-def ParametersToStringDict(param):
-    string_dict = {}
-
-    for k,v in param.items():
-        if v.IsInt():
-            v = v.GetInt()
-        elif v.IsBool():
-            v = int(v.GetBool())
-        elif v.IsDouble():
-            v = v.GetDouble()
-        elif v.IsString():
-            v = v.GetString()
-        else:
-            raise Exception("Only int, double, bool and string are allowed!")
-
-        string_dict[k] = str(v)
-
-    return string_dict
-
 def GetDataLocation(location_str):
     location_map = {
-        "node_historical"     : CoSimIO.DataLocation.NodeHistorical,
-        "node_non_historical" : CoSimIO.DataLocation.NodeNonHistorical,
-        "element"             : CoSimIO.DataLocation.Element,
-        "condition"           : CoSimIO.DataLocation.Condition,
-        "model_part"          : CoSimIO.DataLocation.ModelPart
+        "node_historical"     : KratosCoSim.CoSimIO.DataLocation.NodeHistorical,
+        "node_non_historical" : KratosCoSim.CoSimIO.DataLocation.NodeNonHistorical,
+        "element"             : KratosCoSim.CoSimIO.DataLocation.Element,
+        "condition"           : KratosCoSim.CoSimIO.DataLocation.Condition,
+        "model_part"          : KratosCoSim.CoSimIO.DataLocation.ModelPart
     }
     return location_map[location_str]
