15322dfbc1d0024c62b1c43fcaddcc1a98df86dc
Philipp <philipp.bucher@tum.de>
2019-11-27 01:22:02 +0100

    disabling helpers since require fortran compiler

--- scripts/build/appveyor/configure_appveyor_VS2017.bat
+++ scripts/build/appveyor/configure_appveyor_VS2017.bat
@@ -10,6 +10,7 @@ cmake .. -G "Visual Studio 15 2017 Win64"                                       
 -DSTRUCTURAL_MECHANICS_APPLICATION=ON                                           ^
 -DCONTACT_STRUCTURAL_MECHANICS_APPLICATION=ON                                   ^
 -DCO_SIMULATION_APPLICATION=ON                                                  ^
+-DCO_SIM_HELPERS=OFF                                                            ^
 -DSWIMMING_DEM_APPLICATION=ON                                                   ^
 -DMESH_MOVING_APPLICATION=ON                                                    ^
 -DMESHING_APPLICATION=ON                                                        ^
