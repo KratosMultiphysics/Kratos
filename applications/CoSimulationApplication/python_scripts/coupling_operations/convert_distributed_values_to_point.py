5af404f1ab9065c1358ac330c4decff74497d9d1
Philipp <philipp.bucher@tum.de>
2019-11-27 13:22:16 +0100

    initial version of redistributen operations

--- applications/CoSimulationApplication/python_scripts/coupling_operations/convert_distributed_values_to_point.py
+++ applications/CoSimulationApplication/python_scripts/coupling_operations/convert_distributed_values_to_point.py
@@ -6,7 +6,7 @@ import KratosMultiphysics as KM
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_coupling_operation import CoSimulationCouplingOperation
 
-class DistributePointValuesOperation(CoSimulationCouplingOperation):
+class ConvertDistributedValuesToPoint(CoSimulationCouplingOperation):
 
     def __init__(self, settings, solver_wrappers):
         super(ComputeNormalsOperation, self).__init__(settings)
@@ -14,21 +14,16 @@ class DistributePointValuesOperation(CoSimulationCouplingOperation):
         data_name = self.settings["data_name"].GetString()
         self.interface_data = solver_wrappers[solver_name].GetInterfaceData(data_name)
 
-        self.redistribution_iterations = self.settings["redistribution_tolerance"].GetDouble()
-        self.redistribution_tolerance  = self.settings["redistribution_iterations"].GetInt()
-
         if self.interface_data.is_scalar_variable:
             self.intermediate_variable = KM.KratosGlobals.GetVariable(self.settings["intermediate_variable_scalar"].GetString())
         else:
             self.intermediate_variable = KM.KratosGlobals.GetVariable(self.settings["intermediate_variable_scalar"].GetString())
 
     def Execute(self):
-        KM.VariableRedistributionUtility.DistributePointValues(
+        KM.VariableRedistributionUtility.ConvertDistributedValuesToPoint(
             self.interface_data.GetModelPart(),
-            self.intermediate_variable,
             self.interface_data.variable,
-            self.redistribution_tolerance,
-            self.redistribution_iterations)
+            self.intermediate_variable)
 
     def Check(self):
         # currently the redistribution-utility only works with conditions
@@ -39,24 +34,14 @@ class DistributePointValuesOperation(CoSimulationCouplingOperation):
             raise Exception('No conditions found in ModelPart "{}" of interface data "{}" of solver "{}"!'.format(var.Name(), self.interface_data.model_part_name, self.interface_data.name, self.interface_data.solver_name))
 
         # currently the redistribution-utility only supports historical values on the nodes
-        vars_to_check = [self.intermediate_variable]
-
-        if self.interface_data.is_scalar_variable:
-            vars_to_check.append(KM.MAPPER_SCALAR_PROJECTION_RHS)
-        else:
-            vars_to_check.append(KM.MAPPER_VECTOR_PROJECTION_RHS)
-
-        for var in vars_to_check:
-            if not self.interface_data.GetModelPart().HasNodalSolutionStepVariable(var):
-                raise Exception('Variable "{}" is missing as SolutionStepVariable in ModelPart "{}" of interface data "{}" of solver "{}"!'.format(var.Name(), self.interface_data.model_part_name, self.interface_data.name, self.interface_data.solver_name))
+        if not self.interface_data.GetModelPart().HasNodalSolutionStepVariable(self.intermediate_variable):
+            raise Exception('Variable "{}" is missing as SolutionStepVariable in ModelPart "{}" of interface data "{}" of solver "{}"!'.format(var.Name(), self.interface_data.model_part_name, self.interface_data.name, self.interface_data.solver_name))
 
     @classmethod
     def _GetDefaultSettings(cls):
         this_defaults = KM.Parameters("""{
             "solver"                       : "UNSPECIFIED",
             "data_name"                    : "UNSPECIFIED",
-            "redistribution_tolerance"     : 1e-7,
-            "redistribution_iterations"    : 100,
             "intermediate_variable_scalar" : "NODAL_PAUX",
             "intermediate_variable_vector" : "NODAL_VAUX"
         }""")
