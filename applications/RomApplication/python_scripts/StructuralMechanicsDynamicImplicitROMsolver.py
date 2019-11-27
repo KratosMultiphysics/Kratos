8a13483f04edbbc00be0910c1c23f61be113c82d
Rbravo555 <rbravo555@gmail.com>
2019-11-27 10:56:51 +0100

    Deriving HROM python solver for structural dynamic and static

--- applications/RomApplication/python_scripts/StructuralMechanicsDynamicImplicitROMsolver.py
+++ applications/RomApplication/python_scripts/StructuralMechanicsDynamicImplicitROMsolver.py
@@ -23,18 +23,7 @@ class ROMSolver(ImplicitMechanicalSolver):
     """
 
     def __init__(self, main_model_part, custom_settings):
-        # Set defaults and validate custom settings.
-        #self.stationary_settings = KratosMultiphysics.Parameters(r"""{}""")
-
-        # Construct the base solver and validate the remaining settings in the base class
         super(ROMSolver, self).__init__(main_model_part, custom_settings)
-
-        # # Overwrite the base solver minimum buffer size
-        # if (self.settings["element_replace_settings"]["element_name"].GetString() == "EulerianConvDiff"):
-        #     self.min_buffer_size = 2
-        # else:
-        #     self.min_buffer_size = 1
-
         KratosMultiphysics.Logger.PrintInfo("::[ROMSolver]:: ", "Construction finished")
 
     #### Private functions ####
@@ -43,8 +32,8 @@ class ROMSolver(ImplicitMechanicalSolver):
         default_settings = KratosMultiphysics.Parameters("""
         {
             "rom_settings": {
-            "nodal_unknowns": [ "DISPLACEMENT", "DISPLACEMENT_Y"],
-            "number_of_rom_dofs": 3
+            "nodal_unknowns": [ "DISPLACEMENT", "3"],
+            "number_of_rom_dofs": 10
             }
         }
         """)
@@ -54,15 +43,6 @@ class ROMSolver(ImplicitMechanicalSolver):
     def AddVariables(self):
         super(ROMSolver, self).AddVariables() #Adding nodal area variable
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
-
-
-    # def _create_solution_scheme(self):
-    #     #Variable defining the temporal scheme (0: Forward Euler, 1: Backward Euler, 0.5: Crank-Nicolson)
-    #     #self.GetComputingModelPart().ProcessInfo[ConvectionDiffusionApplication.THETA] = 1.0
-    #     #self.GetComputingModelPart().ProcessInfo[KratosMultiphysics.DYNAMIC_TAU] = 0.0
-    #     structural_mechanics_scheme = KratosMultiphysics.ResidualBasedIncrementalUpdateStaticScheme()
-    #     return structural_mechanics_scheme
-
 
     def _create_builder_and_solver(self):
         linear_solver = self.get_linear_solver()
--- applications/RomApplication/python_scripts/StructuralMechanicsROMsolver.py
+++ applications/RomApplication/python_scripts/StructuralMechanicsROMsolver.py
@@ -7,14 +7,13 @@ import KratosMultiphysics
 import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
 
 # Import base class file
-from KratosMultiphysics.ConvectionDiffusionApplication import convection_diffusion_base_solver
-from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_solver import MechanicalSolver
+from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_static_solver import StaticMechanicalSolver
 import KratosMultiphysics.RomApplication as romapp
 
 def CreateSolver(main_model_part, custom_settings):
     return ROMSolver(main_model_part, custom_settings)
 
-class ROMSolver(MechanicalSolver):
+class ROMSolver(StaticMechanicalSolver):
     """The stationary class for ROM structural mechanics solvers.
 
     Public member variables:
@@ -24,18 +23,7 @@ class ROMSolver(MechanicalSolver):
     """
 
     def __init__(self, main_model_part, custom_settings):
-        # Set defaults and validate custom settings.
-        #self.stationary_settings = KratosMultiphysics.Parameters(r"""{}""")
-
-        # Construct the base solver and validate the remaining settings in the base class
         super(ROMSolver, self).__init__(main_model_part, custom_settings)
-
-        # # Overwrite the base solver minimum buffer size
-        # if (self.settings["element_replace_settings"]["element_name"].GetString() == "EulerianConvDiff"):
-        #     self.min_buffer_size = 2
-        # else:
-        #     self.min_buffer_size = 1
-
         KratosMultiphysics.Logger.PrintInfo("::[ROMSolver]:: ", "Construction finished")
 
     #### Private functions ####
@@ -55,15 +43,6 @@ class ROMSolver(MechanicalSolver):
     def AddVariables(self):
         super(ROMSolver, self).AddVariables() #Adding nodal area variable
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
-
-
-    def _create_solution_scheme(self):
-        #Variable defining the temporal scheme (0: Forward Euler, 1: Backward Euler, 0.5: Crank-Nicolson)
-        #self.GetComputingModelPart().ProcessInfo[ConvectionDiffusionApplication.THETA] = 1.0
-        #self.GetComputingModelPart().ProcessInfo[KratosMultiphysics.DYNAMIC_TAU] = 0.0
-        structural_mechanics_scheme = KratosMultiphysics.ResidualBasedIncrementalUpdateStaticScheme()
-        return structural_mechanics_scheme
-
 
     def _create_builder_and_solver(self):
         linear_solver = self.get_linear_solver()
