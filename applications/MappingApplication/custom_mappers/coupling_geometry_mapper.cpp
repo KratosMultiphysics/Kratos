c815bcca265966ec48816734507654aa68286a1b
philbucher <philipp.bucher@tum.de>
2019-11-29 00:58:07 +0100

    major updates to Mapper to make it compile/run

--- applications/MappingApplication/custom_mappers/coupling_geometry_mapper.cpp
+++ applications/MappingApplication/custom_mappers/coupling_geometry_mapper.cpp
@@ -7,11 +7,9 @@
 //  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
-//  Main authors:    Philipp Bucher, Jordi Cotela
+//  Main authors:    Philipp Bucher
+//                   Tobias Teschemachen
 //
-// See Master-Thesis P.Bucher
-// "Development and Implementation of a Parallel
-//  Framework for Non-Matching Grid Mapping"
 
 // System includes
 
@@ -20,9 +18,14 @@
 // Project includes
 #include "coupling_geometry_mapper.h"
 #include "mapping_application_variables.h"
+#include "custom_utilities/mapper_typedefs.h"
+#include "custom_utilities/mapping_matrix_utilities.h"
+#include "custom_utilities/mapper_utilities.h"
+#include "utilities/variable_utils.h"
 
 namespace Kratos
 {
+
 void CouplingGeometryLocalSystem::CalculateAll(MatrixType& rLocalMappingMatrix,
                     EquationIdVectorType& rOriginIds,
                     EquationIdVectorType& rDestinationIds,
@@ -31,10 +34,10 @@ void CouplingGeometryLocalSystem::CalculateAll(MatrixType& rLocalMappingMatrix,
     const auto& r_geometry_origin = mpGeom->GetGeometryPart(0);
     const auto& r_geometry_destination = mpGeom->GetGeometryPart(1);
 
-    const int number_of_nodes_origin = r_geometry_origin.size();
-    const int number_of_nodes_destination = r_geometry_destination.size();
+    const std::size_t number_of_nodes_origin = r_geometry_origin.size();
+    const std::size_t number_of_nodes_destination = r_geometry_destination.size();
 
-    const int mat_size = (number_of_nodes_origin + number_of_nodes_destination);
+    const std::size_t mat_size = (number_of_nodes_origin + number_of_nodes_destination);
 
     rPairingStatus = MapperLocalSystem::PairingStatus::InterfaceInfoFound;
 
@@ -42,22 +45,20 @@ void CouplingGeometryLocalSystem::CalculateAll(MatrixType& rLocalMappingMatrix,
         rLocalMappingMatrix.resize(mat_size, mat_size, false);
     }
     if (rOriginIds.size()      != number_of_nodes_origin) rOriginIds.resize(number_of_nodes_origin);
-    if (rDestinationIds.size() != r_geometry_destination) rDestinationIds.resize(r_geometry_destination);
+    if (rDestinationIds.size() != number_of_nodes_destination) rDestinationIds.resize(number_of_nodes_destination);
 
-    auto sf_values_origin = r_geometry_origin.ShapeFunctionValues();
+    auto sf_values_origin = r_geometry_origin.ShapeFunctionsValues();
     auto integration_point_values_origin = r_geometry_origin.IntegrationPoints();
-    auto determinant_of_jacobian_values_origin = r_geometry_origin.DeterminatOfJacobian();
+    Vector det_jacobian;
+    auto determinant_of_jacobian_values_origin = r_geometry_origin.DeterminantOfJacobian(det_jacobian);
 
-    auto sf_values_destination = r_geometry_destination.ShapeFunctionValues();
+    auto sf_values_destination = r_geometry_destination.ShapeFunctionsValues();
     //auto integration_point_values_destination = r_geometry_destination.IntegrationPoints();
-    //auto determinant_of_jacobian_values_destination = r_geometry_destination.DeterminatOfJacobian();
+    //auto determinant_of_jacobian_values_destination = r_geometry_destination.DeterminantOfJacobian();
 
-    for (IndexType integration_point_itr=0; integration_point_itr<sf_values_origin.size1(); ++integration_point_itr)
-    {
-        for (IndexType i=0; i<sf_values_origin.size2(); ++i)
-        {
-            for (IndexType j=0; j<sf_values_destination.size2(); ++j)
-            {
+    for (IndexType integration_point_itr=0; integration_point_itr<sf_values_origin.size1(); ++integration_point_itr){
+        for (IndexType i=0; i<sf_values_origin.size2(); ++i) {
+            for (IndexType j=0; j<sf_values_destination.size2(); ++j) {
                 rLocalMappingMatrix(i,j) = sf_values_origin( integration_point_itr, i )
                  * sf_values_destination( integration_point_itr, j )
                  * integration_point_values_origin[integration_point_itr].Weight()
@@ -66,31 +67,108 @@ void CouplingGeometryLocalSystem::CalculateAll(MatrixType& rLocalMappingMatrix,
         }
     }
 
-    for (IndexType i=0; i<sf_values_origin.size2(); ++i)
-    {
-        rOriginIds[i] = r_geometry_origin[i]->GetValue(INTERFACE_EQUATION_ID);
+    for (IndexType i=0; i<sf_values_origin.size2(); ++i) {
+        rOriginIds[i] = r_geometry_origin[i].GetValue(INTERFACE_EQUATION_ID);
     }
-    for (IndexType i=0; i<sf_values_destination.size2(); ++i)
-    {
-        rDestinationIds[0] = sf_values_destination[i]->GetValue(INTERFACE_EQUATION_ID);
+    for (IndexType i=0; i<sf_values_destination.size2(); ++i) {
+        rDestinationIds[0] = r_geometry_origin[i].GetValue(INTERFACE_EQUATION_ID);
     }
 }
 
 std::string CouplingGeometryLocalSystem::PairingInfo(const int EchoLevel) const
 {
-    KRATOS_DEBUG_ERROR_IF_NOT(mpNode) << "Members are not intitialized!" << std::endl;
+    // KRATOS_DEBUG_ERROR_IF_NOT(mpNode) << "Members are not intitialized!" << std::endl;
 
-    std::stringstream buffer;
-    buffer << "CouplingGeometryLocalSystem based on " << mpNode->Info();
-    if (EchoLevel > 1) { // TODO leave here?
-        buffer << " at Coodinates " << Coordinates()[0] << " | " << Coordinates()[1] << " | " << Coordinates()[2];
-        if (mPairingStatus == MapperLocalSystem::PairingStatus::Approximation) {
-            mpNode->SetValue(PAIRING_STATUS, 0);
-        } else {
-            mpNode->SetValue(PAIRING_STATUS, -1);
-        }
-    }
-    return buffer.str();
+    // std::stringstream buffer;
+    // buffer << "CouplingGeometryLocalSystem based on " << mpNode->Info();
+    // if (EchoLevel > 1) { // TODO leave here?
+    //     buffer << " at Coodinates " << Coordinates()[0] << " | " << Coordinates()[1] << " | " << Coordinates()[2];
+    //     if (mPairingStatus == MapperLocalSystem::PairingStatus::Approximation) {
+    //         mpNode->SetValue(PAIRING_STATUS, 0);
+    //     } else {
+    //         mpNode->SetValue(PAIRING_STATUS, -1);
+    //     }
+    // }
+    // return buffer.str();
+    return "";
 }
+
+
+
+template<class TSparseSpace, class TDenseSpace>
+void CouplingGeomteryMapper<TSparseSpace, TDenseSpace>::InitializeInterface(Kratos::Flags MappingOptions)
+{
+    // @tteschemachen here kann man theoretisch auch das Origin-MP nehmen
+    // kommt drauf an, wo die Coupling-Geometries sind
+    CreateMapperLocalSystems(mrModelPartDestination.GetCommunicator(),
+                             mMapperLocalSystems);
+
+    BuildMappingMatrix(MappingOptions);
+}
+
+/* Performs operations that are needed for Initialization and when the interface is updated (All cases)
+I.e. Operations that can be performed several times in the livetime of the mapper
+*/
+template<class TSparseSpace, class TDenseSpace>
+void CouplingGeomteryMapper<TSparseSpace, TDenseSpace>::BuildMappingMatrix(Kratos::Flags MappingOptions)
+{
+    AssignInterfaceEquationIds(); // Has to be done ever time in case of overlapping interfaces!
+
+    const int echo_level = mMapperSettings["echo_level"].GetInt();
+
+    MappingMatrixUtilities::BuildMappingMatrix<TSparseSpace, TDenseSpace>(
+        mpMappingMatrix,
+        mpInterfaceVectorContainerOrigin->pGetVector(),
+        mpInterfaceVectorContainerDestination->pGetVector(),
+        mpInterfaceVectorContainerOrigin->GetModelPart(),
+        mpInterfaceVectorContainerDestination->GetModelPart(),
+        mMapperLocalSystems,
+        echo_level
+    );
+
+}
+
+template<class TSparseSpace, class TDenseSpace>
+void CouplingGeomteryMapper<TSparseSpace, TDenseSpace>::MapInternal(
+    const Variable<double>& rOriginVariable,
+    const Variable<double>& rDestinationVariable,
+    Kratos::Flags MappingOptions)
+{
+    mpInterfaceVectorContainerOrigin->UpdateSystemVectorFromModelPart(rOriginVariable, MappingOptions);
+
+    TSparseSpace::Mult(
+        *mpMappingMatrix,
+        mpInterfaceVectorContainerOrigin->GetVector(),
+        mpInterfaceVectorContainerDestination->GetVector()); // rQd = rMdo * rQo
+
+    mpInterfaceVectorContainerDestination->UpdateModelPartFromSystemVector(rDestinationVariable, MappingOptions);
+}
+
+template<class TSparseSpace, class TDenseSpace>
+void CouplingGeomteryMapper<TSparseSpace, TDenseSpace>::MapInternal(
+    const Variable<array_1d<double, 3>>& rOriginVariable,
+    const Variable<array_1d<double, 3>>& rDestinationVariable,
+    Kratos::Flags MappingOptions)
+{
+    const std::vector<std::string> var_comps{"_X", "_Y", "_Z"};
+
+    for (const auto& var_ext : var_comps) {
+        const auto& var_origin = KratosComponents<ComponentVariableType>::Get(rOriginVariable.Name() + var_ext);
+        const auto& var_destination = KratosComponents<ComponentVariableType>::Get(rDestinationVariable.Name() + var_ext);
+
+        mpInterfaceVectorContainerOrigin->UpdateSystemVectorFromModelPart(var_origin, MappingOptions);
+
+        TSparseSpace::Mult(
+            *mpMappingMatrix,
+            mpInterfaceVectorContainerOrigin->GetVector(),
+            mpInterfaceVectorContainerDestination->GetVector()); // rQd = rMdo * rQo
+
+        mpInterfaceVectorContainerDestination->UpdateModelPartFromSystemVector(var_destination, MappingOptions);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Class template instantiation
+template class CouplingGeomteryMapper< MapperDefinitions::SparseSpaceType, MapperDefinitions::DenseSpaceType >;
 
 }  // namespace Kratos.
--- applications/MappingApplication/custom_mappers/coupling_geometry_mapper.h
+++ applications/MappingApplication/custom_mappers/coupling_geometry_mapper.h
@@ -7,11 +7,9 @@
 //  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
-//  Main authors:    Philipp Bucher, Jordi Cotela
+//  Main authors:    Philipp Bucher
+//                   Tobias Teschemachen
 //
-// See Master-Thesis P.Bucher
-// "Development and Implementation of a Parallel
-//  Framework for Non-Matching Grid Mapping"
 
 #if !defined(KRATOS_COUPLING_GEOMETRY_MAPPER_H_INCLUDED)
 #define  KRATOS_COUPLING_GEOMETRY_MAPPER_H_INCLUDED
@@ -22,6 +20,9 @@
 
 // Project includes
 #include "mapper.h"
+#include "custom_utilities/interface_vector_container.h"
+#include "custom_utilities/mapper_flags.h"
+#include "custom_utilities/mapper_local_system.h"
 
 
 namespace Kratos
@@ -29,11 +30,11 @@ namespace Kratos
 ///@name Kratos Classes
 ///@{
 
-class CouplingGeomteryLocalSystem : public MapperLocalSystem
+class CouplingGeometryLocalSystem : public MapperLocalSystem
 {
 public:
 
-    explicit CouplingGeomteryLocalSystem(GeometryPointerType pGeom) : mpGeom(pGeom) {}
+    explicit CouplingGeometryLocalSystem(GeometryPointerType pGeom) : mpGeom(pGeom) {}
 
     void CalculateAll(MatrixType& rLocalMappingMatrix,
                       EquationIdVectorType& rOriginIds,
@@ -43,7 +44,8 @@ public:
     CoordinatesArrayType& Coordinates() const override
     {
         KRATOS_DEBUG_ERROR_IF_NOT(mpGeom) << "Members are not intitialized!" << std::endl;
-        // return mpGeom->Center();
+        // return mpGeom->Center(); // check why not compiling...
+        KRATOS_ERROR << "not implemented, needs checking" << std::endl;
     }
 
     /// Turn back information as a string.
@@ -54,13 +56,6 @@ private:
 
 };
 
-/// Nearest Neighbor Mapper
-/** This class implements the Nearest Neighbor Mapping technique.
-* Each node on the destination side gets assigned is's closest neighbor on the other side of the interface.
-* In the mapping phase every node gets assigned the value of it's neighbor
-* For information abt the available echo_levels and the JSON default-parameters
-* look into the class description of the MapperCommunicator
-*/
 template<class TSparseSpace, class TDenseSpace>
 class CouplingGeomteryMapper : public Mapper<TSparseSpace, TDenseSpace>
 {
@@ -75,10 +70,20 @@ public:
     KRATOS_CLASS_POINTER_DEFINITION(CouplingGeomteryMapper);
 
     typedef Mapper<TSparseSpace, TDenseSpace> BaseType;
-    typedef typename BaseType::MapperUniquePointerType MapperUniquePointerType;
-    // typedef typename BaseType::MapperInterfaceInfoUniquePointerType MapperInterfaceInfoUniquePointerType;
 
+    typedef Kratos::unique_ptr<MapperLocalSystem> MapperLocalSystemPointer;
+    typedef std::vector<MapperLocalSystemPointer> MapperLocalSystemPointerVector;
+
+    typedef InterfaceVectorContainer<TSparseSpace, TDenseSpace> InterfaceVectorContainerType;
+    typedef Kratos::unique_ptr<InterfaceVectorContainerType> InterfaceVectorContainerPointerType;
+
+    typedef std::size_t IndexType;
+
+    typedef typename BaseType::MapperUniquePointerType MapperUniquePointerType;
     typedef typename BaseType::TMappingMatrixType TMappingMatrixType;
+    typedef Kratos::unique_ptr<TMappingMatrixType> TMappingMatrixUniquePointerType;
+
+    typedef VariableComponent< VectorComponentAdaptor<array_1d<double, 3> > > ComponentVariableType;
 
     ///@}
     ///@name Life Cycle
@@ -86,14 +91,21 @@ public:
 
     // Default constructor, needed for registration
     CouplingGeomteryMapper(ModelPart& rModelPartOrigin,
-                          ModelPart& rModelPartDestination){}
+                         ModelPart& rModelPartDestination)
+                         : mrModelPartOrigin(rModelPartOrigin),
+                           mrModelPartDestination(rModelPartDestination) {}
 
     CouplingGeomteryMapper(ModelPart& rModelPartOrigin,
-                          ModelPart& rModelPartDestination,
-                          Parameters JsonParameters)
+                         ModelPart& rModelPartDestination,
+                         Parameters JsonParameters)
+                         : mrModelPartOrigin(rModelPartOrigin),
+                           mrModelPartDestination(rModelPartDestination),
+                           mMapperSettings(JsonParameters)
     {
-        // this->ValidateInput();
-        // this->Initialize();
+        mpInterfaceVectorContainerOrigin = Kratos::make_unique<InterfaceVectorContainerType>(rModelPartOrigin);
+        mpInterfaceVectorContainerDestination = Kratos::make_unique<InterfaceVectorContainerType>(rModelPartDestination);
+
+        this->InitializeInterface();
     }
 
     /// Destructor.
@@ -103,71 +115,53 @@ public:
     ///@name Operations
     ///@{
 
-    virtual void UpdateInterface(
+    void UpdateInterface(
         Kratos::Flags MappingOptions,
-        double SearchRadius) override {}
+        double SearchRadius) override
+    {
+        KRATOS_ERROR << "Not implemented!" << std::endl;
+    }
 
-    /**
-    * @brief Mapping from Origin to Destination, Scalar Variable
-    * Data is exchanged on the Interface, from the Origin-Modelpart
-    * to the Destination-ModelPart (the modelparts were specified in the
-    * construction Phase of the Mapper)
-    * @param rOriginVariable Variable on the Origin-ModelPart
-    * @param rDestinationVariable Variable on the Destination-ModelPart
-    * @param MappingOptions flags used to specify options for the mapping
-    * @see InverseMap
-    */
-    virtual void Map(
+    void Map(
         const Variable<double>& rOriginVariable,
         const Variable<double>& rDestinationVariable,
-        Kratos::Flags MappingOptions) override {}
+        Kratos::Flags MappingOptions) override
+    {
+        MapInternal(rOriginVariable, rDestinationVariable, MappingOptions);
+    }
 
-    /**
-    * @brief Mapping from Origin to Destination, Vector Variable
-    * Same as Map, but maps an array3-variable
-    * @see Map
-    */
-    virtual void Map(
+    void Map(
         const Variable< array_1d<double, 3> >& rOriginVariable,
         const Variable< array_1d<double, 3> >& rDestinationVariable,
-        Kratos::Flags MappingOptions) override {}
+        Kratos::Flags MappingOptions) override
+    {
+        MapInternal(rOriginVariable, rDestinationVariable, MappingOptions);
+    }
 
-    /**
-    * @brief Mapping from Destination to Origin, Scalar Variable
-    * Data is exchanged on the Interface, from the Destination-Modelpart
-    * to the Origin-ModelPart (the modelparts were specified in the
-    * construction Phase of the Mapper)
-    * It does the opposite of Map
-    * @param rOriginVariable Variable on the Origin-ModelPart
-    * @param rDestinationVariable Variable on the Destination-ModelPart
-    * @param MappingOptions flags used to specify options for the mapping
-    * @see Map
-    */
-    virtual void InverseMap(
+    void InverseMap(
         const Variable<double>& rOriginVariable,
         const Variable<double>& rDestinationVariable,
-        Kratos::Flags MappingOptions) override {}
+        Kratos::Flags MappingOptions) override
+    {
+        KRATOS_ERROR << "Not implemented!" << std::endl;
+    }
 
-    /**
-    * @brief Mapping from Destination to Origin, Vector Variable
-    * Same as InveseMap, but maps an array3-variable
-    * @see InverseMap
-    */
-    virtual void InverseMap(
+    void InverseMap(
         const Variable< array_1d<double, 3> >& rOriginVariable,
         const Variable< array_1d<double, 3> >& rDestinationVariable,
-        Kratos::Flags MappingOptions)override
-        {}
+        Kratos::Flags MappingOptions) override
+    {
+        KRATOS_ERROR << "Not implemented!" << std::endl;
+    }
 
     ///@}
     ///@name Access
     ///@{
 
-    /**
-     * @brief This method returns the mapping-matrix
-     * @return The mapping-matrix
-     */
-    virtual TMappingMatrixType* pGetMappingMatrix() override {return nullptr;}
+    TMappingMatrixType* pGetMappingMatrix() override
+    {
+        KRATOS_ERROR << "Not implemented!" << std::endl;
+    }
 
     MapperUniquePointerType Clone(ModelPart& rModelPartOrigin,
                                   ModelPart& rModelPartDestination,
@@ -210,26 +204,53 @@ private:
     ///@name Private Operations
     ///@{
 
+    ModelPart& mrModelPartOrigin;
+    ModelPart& mrModelPartDestination;
+
+    Parameters mMapperSettings;
+
+    MapperUniquePointerType mpInverseMapper = nullptr;
+
+    TMappingMatrixUniquePointerType mpMappingMatrix;
+
+    MapperLocalSystemPointerVector mMapperLocalSystems;
+
+    InterfaceVectorContainerPointerType mpInterfaceVectorContainerOrigin;
+    InterfaceVectorContainerPointerType mpInterfaceVectorContainerDestination;
+
+
+    void InitializeInterface(Kratos::Flags MappingOptions = Kratos::Flags());
+
+    void BuildMappingMatrix(Kratos::Flags MappingOptions = Kratos::Flags());
+
+    void AssignInterfaceEquationIds()
+    {
+        MapperUtilities::AssignInterfaceEquationIds(mrModelPartOrigin.GetCommunicator());
+        MapperUtilities::AssignInterfaceEquationIds(mrModelPartDestination.GetCommunicator());
+    }
+
+    void MapInternal(const Variable<double>& rOriginVariable,
+                     const Variable<double>& rDestinationVariable,
+                     Kratos::Flags MappingOptions);
+
+    void MapInternal(const Variable<array_1d<double, 3>>& rOriginVariable,
+                     const Variable<array_1d<double, 3>>& rDestinationVariable,
+                     Kratos::Flags MappingOptions);
+
     void CreateMapperLocalSystems(
         const Communicator& rModelPartCommunicator,
         std::vector<Kratos::unique_ptr<MapperLocalSystem>>& rLocalSystems)
     {
-        // MapperUtilities::CreateMapperLocalSystemsFromNodes<CouplingGeomteryLocalSystem>(
-        //     rModelPartCommunicator,
-        //     rLocalSystems);
+        MapperUtilities::CreateMapperLocalSystemsFromGeometries<CouplingGeometryLocalSystem>(
+            rModelPartCommunicator,
+            rLocalSystems);
     }
-
-    // MapperInterfaceInfoUniquePointerType GetMapperInterfaceInfo() const override
-    // {
-    //     return Kratos::make_unique<CouplingGeomteryInterfaceInfo>();
-    // }
 
     Parameters GetMapperDefaultSettings() const
     {
+        // @tobiasteschemachen
         return Parameters( R"({
-            "search_radius"            : -1.0,
-            "search_iterations"        : 3,
-            "echo_level"               : 0
+            "echo_level" : 0
         })");
     }
 
@@ -240,4 +261,4 @@ private:
 ///@} addtogroup block
 }  // namespace Kratos.
 
-#endif // KRATOS_COUPLING_GEOMETRY_MAPPER_H_INCLUDED  defined
+#endif // KRATOS_COUPLING_GEOMETRY_MAPPER_H_INCLUDED defined
