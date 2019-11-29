8068fd48a3f4b42bf73fb10f4d0f927ed16abf88
Miguel Angel <maceli@cimne.upc.edu>
2019-11-29 15:24:16 +0100

    Codacy suggestion

--- applications/DEMApplication/tests/test_kinematic_constraints.py
+++ applications/DEMApplication/tests/test_kinematic_constraints.py
@@ -2,7 +2,6 @@ import os
 import KratosMultiphysics as Kratos
 from KratosMultiphysics import Logger
 Logger.GetDefaultOutput().SetSeverity(Logger.Severity.WARNING)
-from KratosMultiphysics.DEMApplication import *
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.DEMApplication.DEM_analysis_stage
 
