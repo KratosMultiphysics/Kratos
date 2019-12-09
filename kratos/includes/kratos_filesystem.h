7eb4c893b165e6a0bf745c915c89ca6b49b19012
philbucher <philipp.bucher@tum.de>
2019-12-09 20:04:57 +0100

    adding description

--- kratos/includes/kratos_filesystem.h
+++ kratos/includes/kratos_filesystem.h
@@ -18,6 +18,13 @@
     #include <filesystem>
     #define KRATOS_FILESYSTEM_AVAILABLE
 #else
+    //---------------------------------------------------------------------------------------
+    // fs_fwd.hpp - The forwarding header for the header/implementation seperated usage of
+    //              ghc::filesystem.
+    // This file can be include at any place, where ghc::filesystem api is needed while
+    // not bleeding implementation details (e.g. system includes) into the global namespace,
+    // as long as one cpp includes fs_impl.hpp to deliver the matching implementations.
+    //---------------------------------------------------------------------------------------
     #include "ghc/fs_fwd.hpp"
 #endif
 
--- kratos/sources/kratos_filesystem.cpp
+++ kratos/sources/kratos_filesystem.cpp
@@ -14,6 +14,14 @@
 #ifndef KRATOS_FILESYSTEM_AVAILABLE
 // only required if we use ghc/filesystem
 // has to be included before kratos_filesystem!
+
+//---------------------------------------------------------------------------------------
+// fs_impl.hpp - The implementation header for the header/implementation seperated usage of
+//               ghc::filesystem.
+// This file can be used to hide the implementation of ghc::filesystem into a single cpp.
+// The cpp has to include this before including fs_fwd.hpp directly or via a different
+// header to work.
+//---------------------------------------------------------------------------------------
 #include "ghc/fs_impl.hpp"
 #endif
 
