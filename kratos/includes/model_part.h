7d2f92ff0be8042cd46eb9cf89a3cb6d1fad4055
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-12-10 17:25:32 +0100

    Only const version

--- kratos/includes/model_part.h
+++ kratos/includes/model_part.h
@@ -1517,20 +1517,6 @@ public:
     ///@}
     ///@name Operations
     ///@{
-
-    /**
-     * @brief This method returns the full name of the model part (including the parents model parts)
-     * @details This is evaluated in a recursive way
-     * @return The full name of the model part
-     */
-    std::string FullName()
-    {
-        std::string full_name = this->Name();
-        if (this->IsSubModelPart()) {
-            full_name = this->GetParentModelPart()->FullName() + "." + full_name;
-        }
-        return full_name;
-    }
     
     /**
      * @brief This method returns the full name of the model part (including the parents model parts) (constant version)
