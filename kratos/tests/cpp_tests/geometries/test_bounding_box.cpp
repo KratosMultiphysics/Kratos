5687c46177c0dc061b19d0767d35cd95211e550c
pooyan <pooyan@altair.com>
2019-12-17 16:30:32 +0100

    Adding test for set and extend methods

--- kratos/tests/cpp_tests/geometries/test_bounding_box.cpp
+++ kratos/tests/cpp_tests/geometries/test_bounding_box.cpp
@@ -83,6 +83,54 @@ namespace Testing {
         KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[2], 0.30, tolerance);
     }
 
+    /** Checks BoundingBox set method
+    */
+    KRATOS_TEST_CASE_IN_SUITE(BoundingBoxSet, KratosCoreGeometriesFastSuite) {
+        constexpr double tolerance = 1e-12;
+
+        std::vector<Point> points;
+        points.push_back(Point{0.0, 0.4, -0.3});
+        points.push_back(Point{0.9, -0.3, 0.1});
+        points.push_back(Point{-0.2, 0.4, 0.3});
+        points.push_back(Point{0.0, 0.8, -0.5});
+
+
+        BoundingBox<Point> bounding_box;
+
+        bounding_box.Set(points.begin(), points.end());
+
+        KRATOS_CHECK_NEAR(bounding_box.GetMinPoint()[0],-0.20, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMinPoint()[1],-0.30, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMinPoint()[2],-0.50, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[0], 0.90, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[1], 0.80, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[2], 0.30, tolerance);
+    }
+
+    /** Checks BoundingBox set method
+    */
+    KRATOS_TEST_CASE_IN_SUITE(BoundingSet, KratosCoreGeometriesFastSuite) {
+        constexpr double tolerance = 1e-12;
+
+        std::vector<Point> points;
+        points.push_back(Point{0.0, 0.4, -0.3});
+        points.push_back(Point{0.9, -0.3, 0.1});
+
+        BoundingBox<Point> bounding_box(points.begin(), points.end());
+
+        points[0] = Point{-0.2, 0.4, 0.3};
+        points[1] = Point{0.0, 0.8, -0.5};
+
+        bounding_box.Extend(points.begin(), points.end());
+
+        KRATOS_CHECK_NEAR(bounding_box.GetMinPoint()[0],-0.20, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMinPoint()[1],-0.30, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMinPoint()[2],-0.50, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[0], 0.90, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[1], 0.80, tolerance);
+        KRATOS_CHECK_NEAR(bounding_box.GetMaxPoint()[2], 0.30, tolerance);
+    }
+
  
 } // namespace Testing.
 } // namespace Kratos.
