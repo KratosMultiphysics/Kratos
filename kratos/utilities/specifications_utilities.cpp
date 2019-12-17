2c86c20775d7ac4cabfc8ea6ad636b9a7a2e998e
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2019-12-17 19:56:04 +0100

    Adding SpecificationsUtilities (WIP)

---
+++ kratos/utilities/specifications_utilities.cpp
@@ -0,0 +1,51 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "utilities/specifications_utilities.h"
+#include "utilities/variable_utils.h"
+
+namespace Kratos
+{
+namespace SpecificationsUtilities
+{
+void AddMissingVariables(
+    ModelPart& rModelPart,
+    const Parameters ThisParameters
+    )
+{
+    KRATOS_TRY
+
+
+    KRATOS_CATCH("")
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void AddMissingDofs(
+    ModelPart& rModelPart,
+    const Parameters ThisParameters
+    )
+{
+    KRATOS_TRY
+
+
+    KRATOS_CATCH("")
+}
+
+} // namespace SpecificationsUtilities
+} // namespace Kratos
---
+++ kratos/utilities/specifications_utilities.h
@@ -0,0 +1,74 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+#if !defined(KRATOS_SPECIFICATIONS_UTILITIES)
+#define KRATOS_SPECIFICATIONS_UTILITIES
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/model_part.h"
+#include "includes/kratos_parameters.h"
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+/**
+ * @namespace SpecificationsUtilities
+ * @ingroup KratosCore
+ * @brief This namespace includes several utilities necessaries for evaluate specifications
+ * @author Vicente Mataix Ferrandiz
+ */
+namespace SpecificationsUtilities
+{
+    /**
+     * @brief This method adds to the model part the missing variables
+     * @param rModelPart Reference to the ModelPart containing the problem
+     * @param ThisParameters The specifications parameters
+     */
+    void KRATOS_API(KRATOS_CORE) AddMissingVariables(
+        ModelPart& rModelPart,
+        const Parameters ThisParameters
+        );
+
+    /**
+     * @brief This method adds to the model part the missing dofs
+     * @param rModelPart Reference to the ModelPart containing the problem
+     * @param ThisParameters The specifications parameters
+     */
+    void KRATOS_API(KRATOS_CORE) AddMissingDofs(
+        ModelPart& rModelPart,
+        const Parameters ThisParameters
+        );
+
+}; // namespace SpecificationsUtilities
+}  // namespace Kratos
+#endif /* KRATOS_SPECIFICATIONS_UTILITIES defined */
