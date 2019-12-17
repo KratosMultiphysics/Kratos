36964a009cc7e0cba704c34f89f2a6a1794e9908
arming <armin.geiser@tum.de>
2019-12-17 10:26:29 +0100

    add test for in plane opt

--- applications/ShapeOptimizationApplication/tests/test_ShapeOptimizationApplication.py
+++ applications/ShapeOptimizationApplication/tests/test_ShapeOptimizationApplication.py
@@ -31,6 +31,7 @@ from shape_optimization_test_factory import opt_process_stress_test
 from shape_optimization_test_factory import sensitivity_verification_semi_analytic_process_test
 from shape_optimization_test_factory import sensitivity_verification_in_design_space_process_test
 from shape_optimization_test_factory import sensitivity_verification_in_geometry_space_process_test
+from shape_optimization_test_factory import in_plane_opt_test
 from wrl_io_test.test_wrl_io import WrlIOTest
 
 # Niglty tests
@@ -70,6 +71,7 @@ def AssembleTestSuites():
     smallSuite.addTest(opt_process_step_adaption_test('test_execution'))
     smallSuite.addTest(opt_process_multiobjective_test('test_execution'))
     smallSuite.addTest(opt_process_stress_test('test_execution'))
+    smallSuite.addTest(in_plane_opt_test('test_execution'))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([WrlIOTest]))
 
     # Adding nightly tests (tests that take < 10min)
