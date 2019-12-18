c97d0e1f9f8f5cf7b8faca619ad21338aa0240ba
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2019-12-18 17:12:12 +0100

    Framework method

--- kratos/utilities/specifications_utilities.cpp
+++ kratos/utilities/specifications_utilities.cpp
@@ -374,7 +374,88 @@ std::string DetermineFramework(ModelPart& rModelPart)
 {
     KRATOS_TRY
     
-    std::string framework = "";
+    std::string framework = "NONE";
+    
+    // Define specifications
+    Parameters specifications;
+    
+    // We are going to procede like the following, we are going to iterate over all the elements and compare with the components, we will save the type and we will compare until we get that the type of element has changed
+    const auto& r_elements_array = rModelPart.Elements();
+    if (r_elements_array.size() > 0) {
+        std::string element_name;
+        const auto it_elem_begin = r_elements_array.begin();
+        
+        specifications = it_elem_begin->GetSpecifications();
+        CompareElementsAndConditionsUtility::GetRegisteredName(*it_elem_begin, element_name);
+        if (specifications.Has("framework")) {
+            const std::string& r_framework = specifications["framework"].GetString();
+            if (framework == "NONE") {
+                framework = r_framework;
+            } else {
+                CompareElementsAndConditionsUtility::GetRegisteredName(*it_elem_begin, element_name);
+                KRATOS_WARNING("SpecificationsUtilities") << "Framework:" << r_framework << " is used by the element: " << element_name << ". But also the framework: " << framework << " is used" << std::endl; 
+            }
+        }
+            
+        // Now we iterate over all the elements
+        for(std::size_t i = 1; i < r_elements_array.size(); i++) {
+            const auto it_elem_previous = it_elem_begin + i - 1;
+            const auto it_elem_current = it_elem_begin + i;
+            
+            if(!GeometricalObject::IsSame(*it_elem_previous, *it_elem_current)) {
+                specifications = it_elem_current->GetSpecifications();
+                CompareElementsAndConditionsUtility::GetRegisteredName(*it_elem_begin, element_name);
+                if (specifications.Has("flags_used")) {
+                    const std::string& r_framework = specifications["framework"].GetString();
+                    if (framework == "NONE") {
+                        framework = r_framework;
+                    } else {
+                        CompareElementsAndConditionsUtility::GetRegisteredName(*it_elem_current, element_name);
+                        KRATOS_WARNING("SpecificationsUtilities") << "Framework:" << r_framework << " is used by the element: " << element_name << ". But also the framework: " << framework << " is used" << std::endl; 
+                    }
+                }
+            }
+        }
+    }
+    
+    // We are going to procede like the following, we are going to iterate over all the conditions and compare with the components, we will save the type and we will compare until we get that the type of condent has changed
+    const auto& r_conditions_array = rModelPart.Conditions();
+    if (r_conditions_array.size() > 0) {
+        std::string condition_name;
+        const auto it_cond_begin = r_conditions_array.begin();
+        
+        specifications = it_cond_begin->GetSpecifications();
+        CompareElementsAndConditionsUtility::GetRegisteredName(*it_cond_begin, condition_name);
+        if (specifications.Has("framework")) {
+            const std::string& r_framework = specifications["framework"].GetString();
+            if (framework == "NONE") {
+                framework = r_framework;
+            } else {
+                CompareElementsAndConditionsUtility::GetRegisteredName(*it_cond_begin, condition_name);
+                KRATOS_WARNING("SpecificationsUtilities") << "Framework:" << r_framework << " is used by the condition: " << condition_name << ". But also the framework: " << framework << " is used" << std::endl; 
+            }
+        }
+            
+        // Now we iterate over all the conditions
+        for(std::size_t i = 1; i < r_conditions_array.size(); i++) {
+            const auto it_cond_previous = it_cond_begin + i - 1;
+            const auto it_cond_current = it_cond_begin + i;
+            
+            if(!GeometricalObject::IsSame(*it_cond_previous, *it_cond_current)) {
+                specifications = it_cond_current->GetSpecifications();
+                CompareElementsAndConditionsUtility::GetRegisteredName(*it_cond_begin, condition_name);
+                if (specifications.Has("flags_used")) {
+                    const std::string& r_framework = specifications["framework"].GetString();
+                    if (framework == "NONE") {
+                        framework = r_framework;
+                    } else {
+                        CompareElementsAndConditionsUtility::GetRegisteredName(*it_cond_current, condition_name);
+                        KRATOS_WARNING("SpecificationsUtilities") << "Framework:" << r_framework << " is used by the condition: " << condition_name << ". But also the framework: " << framework << " is used" << std::endl; 
+                    }
+                }
+            }
+        }
+    }
     
     return framework;
     
