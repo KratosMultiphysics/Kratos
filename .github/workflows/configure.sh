1a3aea1f2bea44b51c43c6552ee951a13be08c2e
Philipp Bucher <philipp.bucher@tum.de>
2019-12-23 00:07:16 +0100

    removing duplicate

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -46,7 +46,6 @@ add_app ${KRATOS_APP_DIR}/FSIApplication;
 add_app ${KRATOS_APP_DIR}/MeshingApplication;
 add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
 add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication;
-add_app ${KRATOS_APP_DIR}/MeshingApplication;
 
 # Clean
 clear
