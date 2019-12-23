26522245d14abf80f4b4bc2ccd756952a3e2ee2f
Philipp Bucher <philipp.bucher@tum.de>
2019-12-23 12:07:09 +0100

    adding swimmingdem

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -41,6 +41,7 @@ add_app ${KRATOS_APP_DIR}/FSIApplication;
 add_app ${KRATOS_APP_DIR}/MeshingApplication;
 add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
 add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication;
+add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication;
 
 # Clean
 clear
