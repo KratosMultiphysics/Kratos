2bcd6aad8c417e56c865e9858f729d56ee8fec08
Tobias Teschemacher <tobias.teschemacher@tum.de>
2019-12-28 16:15:25 +1000

    Add Test for GeometryModel in ModelPart

---
+++ kratos/tests/cpp_tests/containers/test_model_part_geometry_container.cpp
@@ -0,0 +1,97 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Tobias Teschemacher
+//                   Pooyan Dadvand
+//
+
+// System includes
+#include <limits>
+
+// External includes
+
+// Project includes
+#include "testing/testing.h"
+#include "geometries/line_3d_2.h"
+
+#include "containers/model.h"
+
+namespace Kratos {
+namespace Testing {
+
+    Line3D2<Point>::Pointer GenerateLineModelPartGeometryContainer() {
+        return Kratos::make_shared<Line3D2<Point>>(
+            Kratos::make_shared<Point>(0.0, 0.0, 0.0),
+            Kratos::make_shared<Point>(1.0, 1.0, 1.0)
+            );
+    }
+
+    ///// Test Geometry Container
+    KRATOS_TEST_CASE_IN_SUITE(TestModelPartGeometryContainer, KratosCoreGeometryContainerFastSuite) {
+        Model model;
+
+        auto& model_part = model.CreateModelPart("Main");
+
+        auto& model_part_lines = model_part.CreateSubModelPart("Lines");
+
+        auto& model_part_no_lines = model_part.CreateSubModelPart("NoLines");
+
+        auto p_line_1 = GenerateLineGeometry();
+        p_line_1->SetId(1);
+
+        model_part_lines.AddGeometry(p_line_1);
+
+        auto p_line_2 = GenerateLineGeometry();
+        p_line_2->SetId(1);
+
+        // check correct error if multiple geometries with sam id are added
+        KRATOS_CHECK_EXCEPTION_IS_THROWN(
+            model_part_lines.AddGeometry(p_line_2),
+            "Geometry with Id: 1 exists already.");
+
+        p_line_2->SetId(2);
+        model_part_lines.AddGeometry(p_line_2);
+
+        // check correct number of geometries
+        KRATOS_CHECK_EQUAL(model_part_lines.NumberOfGeometries(), 2);
+        KRATOS_CHECK_EQUAL(model_part.NumberOfGeometries(), 2);
+        KRATOS_CHECK_EQUAL(model_part_no_lines.NumberOfGeometries(), 0);
+
+        // check adding with string
+        auto p_line_3 = GenerateLineGeometry();
+        p_line_3->SetId("GeometryLine1");
+        model_part_lines.AddGeometry(p_line_3);
+
+        KRATOS_CHECK_EQUAL(model_part_lines.NumberOfGeometries(), 3);
+
+        // check if correct element is returned
+        KRATOS_CHECK_EQUAL(model_part_lines.GetGeometry(1).Id(), 1);
+        KRATOS_CHECK_EQUAL(model_part_lines.pGetGeometry(1)->Id(), 1);
+
+        // check if correct element is returned from root
+        KRATOS_CHECK_EQUAL(model_part.GetGeometry(1).Id(), 1);
+        KRATOS_CHECK_EQUAL(model_part.pGetGeometry(1)->Id(), 1);
+
+        // check remove functions
+        geometry_container.RemoveGeometry("GeometryLine1");
+        geometry_container.RemoveGeometryFromAllLevels(1);
+        KRATOS_CHECK_EQUAL(model_part_lines.NumberOfGeometries(), 1);
+        KRATOS_CHECK_EQUAL(model_part.NumberOfGeometries(), 2);
+        KRATOS_CHECK_EQUAL(model_part.NumberOfGeometries(), 2);
+
+        // check if correct geometries are removed
+        KRATOS_CHECK_IS_FALSE(model_part_lines.HasGeometry("GeometryLine1"));
+        KRATOS_CHECK(model_part.HasGeometry("GeometryLine1"));
+
+        // check if correct geometries are removed
+        KRATOS_CHECK_IS_FALSE(model_part_lines.HasGeometry(1));
+        KRATOS_CHECK_IS_FALSE(model_part.HasGeometry(1));
+    }
+} // namespace Testing.
+} // namespace Kratos.
