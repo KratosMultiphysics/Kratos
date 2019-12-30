381f87a73031f92d534571f546107575c35317bd
riccardotosi <rtosi@cimne.upc.edu>
2019-12-30 10:18:56 +0100

    Remove

--- applications/MultilevelMonteCarloApplication/custom_external_libraries/exaqute/ExecutionCharacteristics.py
+++
@@ -1,8 +0,0 @@
-class ExecutionCharacteristics(object):
-    def __init__(self, generationNodes, generationCpus_per_node, runNodes,
-                 runCpus_per_node, amountExecutions):
-        self.generationNodes = generationNodes
-        self.generationCpus_per_node = generationCpus_per_node
-        self.runNodes = runNodes
-        self.runCpus_per_node = runCpus_per_node
-        self.amountExecutions = amountExecutions
