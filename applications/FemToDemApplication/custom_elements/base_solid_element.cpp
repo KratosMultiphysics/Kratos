d167562ca2e23e69c25ccd6e1cdb4d647768dd0d
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-06 15:01:54 +0100

    Update

--- applications/FemToDemApplication/custom_elements/base_solid_element.cpp
+++ applications/FemToDemApplication/custom_elements/base_solid_element.cpp
@@ -1932,7 +1932,7 @@ const Parameters BaseSolidElement::GetSpecifications() const
         "required_variables"         : ["DISPLACEMENT"],
         "required_dofs"              : ["DISPLACEMENT_X","DISPLACEMENT_Y","DISPLACEMENT_Z"],
         "flags_used"                 : [],
-        "compatible_geometries"      : ["Line2D2", "Triangle2D3", "Quadrilateral2D4", "Line3D2", "Triangle3D3","Tetrahedra3D4", "Prism3D6", "Hexahedra3D8", "Tetrahedra3D10"],
+        "compatible_geometries"      : ["Triangle2D3", "Triangle2D6", "Quadrilateral2D4", "Quadrilateral2D8", "Quadrilateral2D9","Tetrahedra3D4", "Prism3D6", "Prism3D15", "Hexahedra3D8", "Hexahedra3D20", "Hexahedra3D27", "Tetrahedra3D10"],
         "is_implicit"                : true,
         "element_integrates_in_time" : true,
         "compatible_constitutive_laws": {
