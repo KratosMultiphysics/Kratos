e91686d6f652be799e8d2914d0db9088e0a8eadf
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2020-01-08 11:07:24 +0100

    adding new vars to femdem

--- applications/FemToDemApplication/python_scripts/MainFEM_for_coupling.py
+++ applications/FemToDemApplication/python_scripts/MainFEM_for_coupling.py
@@ -3,6 +3,7 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 import KratosMultiphysics
 import KratosMultiphysics.FemToDemApplication.MainFemDem as MainFemDem
 import KratosMultiphysics.FemToDemApplication as KratosFemDem
+import KratosMultiphysics.DEMApplication as DEM
 
 # Python script created to modify the existing one due to the coupling of the DEM app in 2D
 
@@ -28,6 +29,8 @@ class FEM_for_coupling_Solution(MainFemDem.FEM_Solution):
         self.main_model_part.AddNodalSolutionStepVariable(KratosFemDem.NODAL_DAMAGE)
         self.main_model_part.AddNodalSolutionStepVariable(KratosFemDem.EQUIVALENT_STRESS_VM)
         self.main_model_part.AddNodalSolutionStepVariable(KratosFemDem.DISPLACEMENT_INCREMENT)
+        self.main_model_part.AddNodalSolutionStepVariable(DEM.DEM_PRESSURE)
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.TOTAL_FORCES)
 
 
         # Read model_part (note: the buffer_size is set here) (restart is read here)
