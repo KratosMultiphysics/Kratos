9a22cbd542e891a0fab45f348f4f3decf9b1ee26
Philipp <philipp.bucher@tum.de>
2020-01-08 19:00:58 +0100

    adding test for ConvergenceCriteriaWrapper

--- applications/CoSimulationApplication/tests/test_convergence_criteria.py
+++ applications/CoSimulationApplication/tests/test_convergence_criteria.py
@@ -7,6 +7,91 @@ from KratosMultiphysics.CoSimulationApplication.coupling_interface_data import C
 from testing_utilities import DummySolverWrapper
 
 from KratosMultiphysics.CoSimulationApplication.factories.convergence_criterion_factory import CreateConvergenceCriterion
+from KratosMultiphysics.CoSimulationApplication.convergence_criteria.convergence_criteria_wrapper import ConvergenceCriteriaWrapper
+
+from unittest.mock import Mock
+import numpy as np
+from random import uniform
+
+if KM.IsDistributedRun():
+    import KratosMultiphysics.mpi as KratosMPI
+
+class TestConvergenceCriteriaWrapper(KratosUnittest.TestCase):
+
+    def setUp(self):
+        self.model = KM.Model()
+        self.model_part = self.model.CreateModelPart("default")
+        self.model_part.AddNodalSolutionStepVariable(KM.PRESSURE)
+        self.model_part.AddNodalSolutionStepVariable(KM.PARTITION_INDEX)
+        self.dimension = 3
+        self.model_part.ProcessInfo[KM.DOMAIN_SIZE] = self.dimension
+
+        num_proc = KM.DataCommunicator.GetDefault().Size()
+        self.my_pid = KM.DataCommunicator.GetDefault().Rank()
+        self.num_nodes = self.my_pid % 5 + 3 # num_nodes in range (3 ... 7)
+        if self.my_pid == 4:
+            self.num_nodes = 0 # in order to emulate one partition not having local nodes
+
+        for i in range(self.num_nodes):
+            node = self.model_part.CreateNewNode(i, 0.1*i, 0.0, 0.0) # this creates the same coords in different ranks, which does not matter for this test
+
+            node.SetSolutionStepValue(KM.PARTITION_INDEX, self.my_pid)
+            node.SetSolutionStepValue(KM.PRESSURE, uniform(-10, 50))
+
+        if KM.IsDistributedRun():
+            KratosMPI.ParallelFillCommunicator(self.model_part).Execute()
+
+        data_settings = KM.Parameters("""{
+            "model_part_name" : "default",
+            "variable_name"   : "PRESSURE"
+        }""")
+        self.interface_data = CouplingInterfaceData(data_settings, self.model)
+        self.interface_data.Initialize()
+
+        self.dummy_solver_wrapper = DummySolverWrapper({"data_4_testing" : self.interface_data})
+
+    def test_wrapper(self):
+        conv_acc_settings = KM.Parameters("""{
+            "type"      : "relative_norm_previous_residual",
+            "data_name" : "data_4_testing"
+        }""")
+        conv_crit_wrapper = ConvergenceCriteriaWrapper(conv_acc_settings, self.dummy_solver_wrapper)
+
+        exp_inp = self.interface_data.GetData()
+        # num_nodes_global = KM.DataCommunicator.GetDefault().SumAll(self.num_nodes)
+        # update_solution_return_value = [uniform(-10, 50) for _ in range(self.num_nodes)]
+
+        # global_update_solution_return_value = np.array(np.concatenate(KM.DataCommunicator.GetDefault().GathervDoubles(update_solution_return_value, 0)))
+
+        conv_crit_mock = Mock()
+
+        is_converged = True
+        attrs = {
+            'IsConverged.return_value' : is_converged
+        }
+        conv_crit_mock.configure_mock(**attrs)
+
+        conv_crit_wrapper.conv_crit = conv_crit_mock
+
+        conv_crit_wrapper.InitializeSolutionStep()
+
+        conv_crit_wrapper.InitializeNonLinearIteration()
+
+        # setting new solution for computing the residual
+        rand_data = [uniform(-10, 50) for _ in range(self.num_nodes)]
+        self.interface_data.SetData(rand_data)
+        exp_res = rand_data - exp_inp
+
+        self.assertEqual(conv_crit_wrapper.IsConverged(), is_converged)
+
+        self.assertEqual(conv_crit_mock.IsConverged.call_count, int(self.my_pid == 0)) # only one rank calls "IsConverged"
+
+        global_exp_res = np.array(np.concatenate(KM.DataCommunicator.GetDefault().GathervDoubles(exp_res, 0)))
+        global_rand_data_inp = np.array(np.concatenate(KM.DataCommunicator.GetDefault().GathervDoubles(rand_data, 0)))
+        if self.my_pid == 0:
+            # numpy arrays cannot be compared using the mock-functions, hence using the numpy functions
+            np.testing.assert_array_equal(global_exp_res, conv_crit_mock.IsConverged.call_args[0][0])
+            np.testing.assert_array_equal(global_rand_data_inp, conv_crit_mock.IsConverged.call_args[0][1])
 
 
 class TestConvergenceCriteria(KratosUnittest.TestCase):
