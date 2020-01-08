97a5a5a7c016985d64833377b162dafd553423a6
ssaxena93-tum <saransh.saxena@tum.de>
2020-01-08 13:41:13 +0100

    Updated Code

--- cmake_debug/CMakeFiles/FortranCInterface/symbols/my_moduleSmy_sub.c
+++
@@ -1,4 +0,0 @@
-const char* my_module$my_sub(void)
-{
-  return "INFO:symbol[my_module$my_sub]";
-}
--- cmake_debug/kratos/CMakeFiles/KratosVersion.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/KratosVersion.dir/sources/kratos_version.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/KratosVersion.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
