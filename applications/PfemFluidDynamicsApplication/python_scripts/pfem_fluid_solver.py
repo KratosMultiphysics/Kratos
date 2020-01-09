ec772a2e778cd99f1bfc4de2b94daffd255efa3d
Massimiliano Zecchetto <mzecchetto@cimne.upc.edu>
2020-01-09 12:45:54 +0100

    PrepareModelPart added to pfem_fluid_solver.py

--- applications/PfemFluidDynamicsApplication/python_scripts/pfem_fluid_solver.py
+++ applications/PfemFluidDynamicsApplication/python_scripts/pfem_fluid_solver.py
@@ -217,15 +217,16 @@ class PfemFluidSolver(PythonSolver):
             node.AddDof(KratosMultiphysics.DISPLACEMENT_Z)
         print("::[Pfem Fluid Solver]:: DOF's ADDED")
 
-
     def ImportModelPart(self):
-
-        print("::[Pfem Fluid Solver]:: Model reading starts.")
-
-        self.computing_model_part_name = "fluid_computing_domain"
 
         # we can use the default implementation in the base class
         self._ImportModelPart(self.main_model_part,self.settings["model_import_settings"])
+
+    def PrepareModelPart(self):
+
+        print("::[Pfem Fluid Solver]:: Model preparing started.")
+
+        self.computing_model_part_name = "fluid_computing_domain"
 
         # Auxiliary Kratos parameters object to be called by the CheckAndPepareModelProcess
         params = KratosMultiphysics.Parameters("{}")
@@ -257,7 +258,7 @@ class PfemFluidSolver(PythonSolver):
 
         self.main_model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED] = False
 
-        print ("::[Pfem Fluid Solver]:: Model reading finished.")
+        print ("::[Pfem Fluid Solver]:: Model preparing finished.")
 
 
     def CheckAndPrepareModelProcess(self, params):
