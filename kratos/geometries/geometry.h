a7ce6250af5fb92b060bbd2ece8a817323d4e830
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-01-12 14:15:21 +1000

    Change location of ~

--- kratos/geometries/geometry.h
+++ kratos/geometries/geometry.h
@@ -3471,7 +3471,7 @@ private:
     /// Sets first bit in Id to 0 -> no name/ string
     static inline void SetIdNotGeneratedFromString(IndexType& Id)
     {
-        Id &= ~(IndexType(1) << (sizeof(IndexType) - 1));
+        Id &= (IndexType(1) << ~(sizeof(IndexType) - 1));
     }
 
     /// Checks second bit in Id. 0 -> defined id; 1 -> self assigned
@@ -3489,7 +3489,7 @@ private:
     /// Sets second bit in Id to 0 -> not self assigned
     static inline void SetIdNotSelfAssigned(IndexType& Id)
     {
-        Id &= ~(IndexType(1) << (sizeof(IndexType) - 2));
+        Id &= (IndexType(1) << ~(sizeof(IndexType) - 2));
     }
 
     ///@}
