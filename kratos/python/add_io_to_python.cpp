0b6a9d11d23c72bf7ee8e0a9c3c93bc8b00e3115
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-01-16 14:14:13 +1000

    Adding use of EchoLevel to python

--- kratos/python/add_io_to_python.cpp
+++ kratos/python/add_io_to_python.cpp
@@ -328,7 +328,9 @@ void  AddIOToPython(pybind11::module& m)
 
     py::class_<CadJsonInput<>, CadJsonInput<>::Pointer>(m, "CadJsonInput")
         .def(py::init<const std::string &>())
+        .def(py::init<const std::string&, std::size_t>())
         .def(py::init<Parameters>())
+        .def(py::init<Parameters, std::size_t>())
         .def("ReadModelPart", &CadJsonInput<>::ReadModelPart)
         .def("ReadGeometryModelPart", &CadJsonInput<>::ReadGeometryModelPart)
         ;
