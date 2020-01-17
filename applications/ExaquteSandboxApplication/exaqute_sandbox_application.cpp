eeeb2da4cabbadbcbc1ff1d02c97baa049ec31cd
riccardotosi <rtosi@cimne.upc.edu>
2020-01-17 10:48:55 +0100

    Add python process and rename variable

--- applications/ExaquteSandboxApplication/exaqute_sandbox_application.cpp
+++ applications/ExaquteSandboxApplication/exaqute_sandbox_application.cpp
@@ -34,7 +34,7 @@ void KratosExaquteSandboxApplication::Register()
      KratosApplication::Register();
      KRATOS_INFO("") << "Initializing KratosExaquteSandboxApplication..." << std::endl;
 
-  KRATOS_REGISTER_VARIABLE( AVERAGE_DIVERGENCE )
+  KRATOS_REGISTER_VARIABLE( AVERAGED_DIVERGENCE )
   KRATOS_REGISTER_VARIABLE( DIVERGENCE )
   KRATOS_REGISTER_VARIABLE( VELOCITY_H1_SEMINORM )
 
