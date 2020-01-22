f6990a4cd2b5983c308d4bac9decdd6f5f6dd5f5
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-01-22 18:07:26 +1000

    Add add_integration_to_python

---
+++ kratos/python/add_integration_to_python.cpp
@@ -0,0 +1,35 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                     Kratos default license: kratos/license.txt
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define_python.h"
+
+
+namespace Kratos
+{
+
+namespace Python
+{
+
+void  AddIOToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+}
+
+}  // namespace Python.
+
+} // Namespace Kratos
+
+
