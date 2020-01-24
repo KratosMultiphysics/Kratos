5c5133778b7b73d6493f1442d3010ca659d7508f
aditya_laptop <shine.aditya@gmail.com>
2020-01-24 17:48:13 +0100

    correcting builder and solver

--- applications/ChimeraApplication/trilinos_extension/custom_utilities/helper_classes_for_constraint_builder.h
+++ applications/ChimeraApplication/trilinos_extension/custom_utilities/helper_classes_for_constraint_builder.h
@@ -637,22 +637,22 @@ private:
         //     master_i++;
         // }
 
-        // For K(s,m) and K(m,s). This is to be done at the end only
-        for (auto& master_index : mLocalIndices.processed_master_indices) {
-            for (auto& slave_index : mLocalIndices.slave_index_vector) {
-                rLHSContribution(slave_index, master_index) = 0.0;
-                rLHSContribution(master_index, slave_index) = 0.0;
-            }
-        }
+        // // For K(s,m) and K(m,s). This is to be done at the end only
+        // for (auto& master_index : mLocalIndices.processed_master_indices) {
+        //     for (auto& slave_index : mLocalIndices.slave_index_vector) {
+        //         rLHSContribution(slave_index, master_index) = 0.0;
+        //         rLHSContribution(master_index, slave_index) = 0.0;
+        //     }
+        // }
 
 
-        // For K(u,s) and K(s,u). This is to be done at the end only
-        for (auto& slave_index : mLocalIndices.slave_index_vector) {
-            for (auto& internal_index : mLocalIndices.internal_index_vector) {
-                rLHSContribution(slave_index, internal_index) = 0.0;
-                rLHSContribution(internal_index, slave_index) = 0.0;
-            }
-        }
+        // // For K(u,s) and K(s,u). This is to be done at the end only
+        // for (auto& slave_index : mLocalIndices.slave_index_vector) {
+        //     for (auto& internal_index : mLocalIndices.internal_index_vector) {
+        //         rLHSContribution(slave_index, internal_index) = 0.0;
+        //         rLHSContribution(internal_index, slave_index) = 0.0;
+        //     }
+        // }
 
         // For K(s,s)
         for (auto& slave_index1 : mLocalIndices.slave_index_vector) {
