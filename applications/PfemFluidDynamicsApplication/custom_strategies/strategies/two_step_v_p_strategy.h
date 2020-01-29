539837d145e399ff48201a6ffcea1dd17cfeeb69
Franci <falessandro@cimne.upc.edu>
2020-01-29 12:43:31 +0100

    Small improvement in the strategy

--- applications/PfemFluidDynamicsApplication/custom_strategies/strategies/two_step_v_p_strategy.h
+++ applications/PfemFluidDynamicsApplication/custom_strategies/strategies/two_step_v_p_strategy.h
@@ -1297,7 +1297,9 @@ protected:
     isItNan = std::isnan(DvErrorNorm);
     bool isItInf = false;
     isItInf = std::isinf(DvErrorNorm);
-    if ((DvErrorNorm > minTolerance || (DvErrorNorm < 0 && DvErrorNorm > 0) || (DvErrorNorm != DvErrorNorm) || isItNan == true || isItInf == true) && DvErrorNorm != 0 && DvErrorNorm != 1)
+    if ((DvErrorNorm > minTolerance || (DvErrorNorm < 0 && DvErrorNorm > 0) || (DvErrorNorm != DvErrorNorm) || isItNan == true || isItInf == true) &&
+        DvErrorNorm != 0 &&
+        (DvErrorNorm != 1 || currentTime > timeInterval))
     {
       rCurrentProcessInfo.SetValue(BAD_VELOCITY_CONVERGENCE, true);
       std::cout << "NOT GOOD CONVERGENCE!!! I'll reduce the next time interval" << DvErrorNorm << std::endl;
@@ -1331,6 +1333,8 @@ protected:
   {
     ModelPart &rModelPart = BaseType::GetModelPart();
     ProcessInfo &rCurrentProcessInfo = rModelPart.GetProcessInfo();
+    double currentTime = rCurrentProcessInfo[TIME];
+    double timeInterval = rCurrentProcessInfo[DELTA_TIME];
     double minTolerance = 0.99999;
     bool fixedTimeStep = false;
 
@@ -1338,7 +1342,9 @@ protected:
     isItNan = std::isnan(DvErrorNorm);
     bool isItInf = false;
     isItInf = std::isinf(DvErrorNorm);
-    if ((DvErrorNorm > minTolerance || (DvErrorNorm < 0 && DvErrorNorm > 0) || (DvErrorNorm != DvErrorNorm) || isItNan == true || isItInf == true) && DvErrorNorm != 0 && DvErrorNorm != 1)
+    if ((DvErrorNorm > minTolerance || (DvErrorNorm < 0 && DvErrorNorm > 0) || (DvErrorNorm != DvErrorNorm) || isItNan == true || isItInf == true) &&
+        DvErrorNorm != 0 &&
+        (DvErrorNorm != 1 || currentTime > timeInterval))
     {
       rCurrentProcessInfo.SetValue(BAD_VELOCITY_CONVERGENCE, true);
       std::cout << "           BAD CONVERGENCE DETECTED DURING THE ITERATIVE LOOP!!! error: " << DvErrorNorm << " higher than 0.9999" << std::endl;
@@ -1381,7 +1387,9 @@ protected:
     isItNan = std::isnan(DvErrorNorm);
     bool isItInf = false;
     isItInf = std::isinf(DvErrorNorm);
-    if ((DvErrorNorm > minTolerance || (DvErrorNorm < 0 && DvErrorNorm > 0) || (DvErrorNorm != DvErrorNorm) || isItNan == true || isItInf == true) && DvErrorNorm != 0 && DvErrorNorm != 1)
+    if ((DvErrorNorm > minTolerance || (DvErrorNorm < 0 && DvErrorNorm > 0) || (DvErrorNorm != DvErrorNorm) || isItNan == true || isItInf == true) &&
+        DvErrorNorm != 0 &&
+        (DvErrorNorm != 1 || currentTime > timeInterval))
     {
       fixedTimeStep = true;
       rCurrentProcessInfo.SetValue(BAD_PRESSURE_CONVERGENCE, true);
