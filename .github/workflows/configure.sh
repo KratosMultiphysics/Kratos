cc6df0944752a16a793e69fe52663a6a92278baf
arming <armin.geiser@tum.de>
2020-01-30 18:24:37 +0100

    revert debug changes (finally)

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -23,25 +23,25 @@ export PYTHON_EXECUTABLE="/usr/bin/python3.6"
 export KRATOS_INSTALL_PYTHON_USING_LINKS=ON
 
 # Set applications to compile
-#add_app ${KRATOS_APP_DIR}/ExternalSolversApplication;
+add_app ${KRATOS_APP_DIR}/ExternalSolversApplication;
 add_app ${KRATOS_APP_DIR}/EigenSolversApplication;
-# add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication;
-# add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication;
-# add_app ${KRATOS_APP_DIR}/MeshMovingApplication;
-# add_app ${KRATOS_APP_DIR}/DEMApplication;
-# add_app ${KRATOS_APP_DIR}/CSharpWrapperApplication;
-# add_app ${KRATOS_APP_DIR}/MetisApplication;
-# add_app ${KRATOS_APP_DIR}/TrilinosApplication;
-# add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication;
-# add_app ${KRATOS_APP_DIR}/CoSimulationApplication;
-# add_app ${KRATOS_APP_DIR}/CableNetApplication;
-# add_app ${KRATOS_APP_DIR}/RANSApplication;
-# add_app ${KRATOS_APP_DIR}/MappingApplication;
-# add_app ${KRATOS_APP_DIR}/FSIApplication;
-# add_app ${KRATOS_APP_DIR}/MeshingApplication;
-# add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
-# add_app ${KRATOS_APP_DIR}/HDF5Application;
-# add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication;
+add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication;
+add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication;
+add_app ${KRATOS_APP_DIR}/MeshMovingApplication;
+add_app ${KRATOS_APP_DIR}/DEMApplication;
+add_app ${KRATOS_APP_DIR}/CSharpWrapperApplication;
+add_app ${KRATOS_APP_DIR}/MetisApplication;
+add_app ${KRATOS_APP_DIR}/TrilinosApplication;
+add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication;
+add_app ${KRATOS_APP_DIR}/CoSimulationApplication;
+add_app ${KRATOS_APP_DIR}/CableNetApplication;
+add_app ${KRATOS_APP_DIR}/RANSApplication;
+add_app ${KRATOS_APP_DIR}/MappingApplication;
+add_app ${KRATOS_APP_DIR}/FSIApplication;
+add_app ${KRATOS_APP_DIR}/MeshingApplication;
+add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
+add_app ${KRATOS_APP_DIR}/HDF5Application;
+add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication;
 
 # Clean
 clear
@@ -60,7 +60,6 @@ cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" \
 -Werror=misleading-indentation -Werror=return-type \
 -Werror=sign-compare -Werror=unused-but-set-variable \
 -Werror=unused-local-typedefs -Werror=reorder -Werror=maybe-uninitialized -Wno-deprecated-declarations" \
--DEIGEN_ROOT="/usr/include/eigen3" \
 -DTRILINOS_INCLUDE_DIR="/usr/include/trilinos" \
 -DTRILINOS_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" \
 -DTRILINOS_LIBRARY_PREFIX="trilinos_" \
