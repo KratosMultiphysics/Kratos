9015a379643f8d527ff62e3e7fe9a966e45be2d5
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-31 16:24:19 +0100

    Adding create with Id for Quadrilateral3D9

--- kratos/geometries/quadrilateral_3d_9.h
+++ kratos/geometries/quadrilateral_3d_9.h
@@ -232,6 +232,14 @@ public:
         KRATOS_ERROR_IF( this->PointsNumber() != 9 ) << "Invalid points number. Expected 9, given " << this->PointsNumber() << std::endl;
     }
 
+    explicit Quadrilateral3D9( 
+        IndexType Id,
+        const PointsArrayType& ThisPoints 
+        ) : BaseType( Id, ThisPoints, &msGeometryData )
+    {
+        KRATOS_ERROR_IF( this->PointsNumber() != 9 ) << "Invalid points number. Expected 9, given " << this->PointsNumber() << std::endl;
+    }
+    
     /**
      * Copy constructor.
      * Constructs this geometry as a copy of given geometry.
@@ -323,9 +331,29 @@ public:
     /**
      * Operations
      */
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
         return typename BaseType::Pointer( new Quadrilateral3D9( ThisPoints ) );
+    }
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param NewId the ID of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
+    typename BaseType::Pointer Create(
+        IndexType NewId,  
+        PointsArrayType const& ThisPoints
+        ) const override
+    {
+        return typename BaseType::Pointer( new Quadrilateral3D9( NewId, ThisPoints ) );
     }
 
     //     Geometry< Point<3> >::Pointer Clone() const override
