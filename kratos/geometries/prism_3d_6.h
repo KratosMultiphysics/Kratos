c877372611460a03ff3058dcc81e3fe667f545e8
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-31 16:22:45 +0100

    Adding create with Id for Prism3D6

--- kratos/geometries/prism_3d_6.h
+++ kratos/geometries/prism_3d_6.h
@@ -222,7 +222,15 @@ public:
     {
         KRATOS_ERROR_IF( this->PointsNumber() != 6 ) << "Invalid points number. Expected 6, given " << this->PointsNumber() << std::endl;
     }
-
+    
+    explicit Prism3D6( 
+        IndexType Id,
+        const PointsArrayType& ThisPoints 
+        ) : BaseType( Id, ThisPoints, &msGeometryData )
+    {
+        KRATOS_ERROR_IF( this->PointsNumber() != 6 ) << "Invalid points number. Expected 6, given " << this->PointsNumber() << std::endl;
+    }
+    
     /**
      * Copy constructor.
      * Construct this geometry as a copy of given geometry.
@@ -311,12 +319,30 @@ public:
     /**
      * Operations
      */
-
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
         return typename BaseType::Pointer( new Prism3D6( ThisPoints ) );
     }
 
+    /**
+     * @brief It creates a new geometry pointer
+     * @param NewId the ID of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
+    typename BaseType::Pointer Create(
+        IndexType NewId,  
+        PointsArrayType const& ThisPoints
+        ) const override
+    {
+        return typename BaseType::Pointer( new Prism3D6( NewId, ThisPoints ) );
+    }
 
     //     Geometry< Point<3> >::Pointer Clone() const override
     // {
