0a6ffae6b2eeb8f3d0dbcdb23d60a6726fc4176c
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-31 16:21:57 +0100

    Adding create with Id for Line3D2

--- kratos/geometries/line_3d_2.h
+++ kratos/geometries/line_3d_2.h
@@ -186,6 +186,14 @@ public:
             KRATOS_ERROR << "Invalid points number. Expected 2, given " << BaseType::PointsNumber() << std::endl;
     }
 
+    explicit Line3D2( 
+        IndexType Id,
+        const PointsArrayType& ThisPoints 
+        ) : BaseType( Id, ThisPoints, &msGeometryData )
+    {
+        KRATOS_ERROR_IF( this->PointsNumber() != 2 ) << "Invalid points number. Expected 2, given " << this->PointsNumber() << std::endl;
+    }
+
     /** Copy constructor.
     Construct this geometry as a copy of given geometry.
 
@@ -272,9 +280,28 @@ public:
     ///@name Operations
     ///@{
 
+    /**
+     * @brief It creates a new geometry pointer
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
         return typename BaseType::Pointer( new Line3D2( ThisPoints ) );
+    }
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param NewId the ID of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
+    typename BaseType::Pointer Create(
+        IndexType NewId,  
+        PointsArrayType const& ThisPoints
+        ) const override
+    {
+        return typename BaseType::Pointer( new Line3D2( NewId, ThisPoints ) );
     }
 
     // Geometry< Point<3> >::Pointer Clone() const override
