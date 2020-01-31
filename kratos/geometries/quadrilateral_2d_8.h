f668a9a1ea3794844b901072f4ca9ae99327b401
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-31 16:23:21 +0100

    Adding create with Id for Quadrilateral2D8

--- kratos/geometries/quadrilateral_2d_8.h
+++ kratos/geometries/quadrilateral_2d_8.h
@@ -232,6 +232,14 @@ public:
             KRATOS_ERROR << "Invalid points number. Expected 8, given " << this->PointsNumber() << std::endl;
     }
 
+    explicit Quadrilateral2D8( 
+        IndexType Id,
+        const PointsArrayType& ThisPoints 
+        ) : BaseType( Id, ThisPoints, &msGeometryData )
+    {
+        KRATOS_ERROR_IF( this->PointsNumber() != 8 ) << "Invalid points number. Expected 8, given " << this->PointsNumber() << std::endl;
+    }
+    
     /**
      * Copy constructor.
      * Constructs this geometry as a copy of given geometry.
@@ -322,11 +330,30 @@ public:
     /**
      * Operations
      */
+    /**
+     * @brief It creates a new geometry pointer
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
         return typename BaseType::Pointer( new Quadrilateral2D8( ThisPoints ) );
     }
-
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param NewId the ID of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
+    typename BaseType::Pointer Create(
+        IndexType NewId,  
+        PointsArrayType const& ThisPoints
+        ) const override
+    {
+        return typename BaseType::Pointer( new Quadrilateral2D8( NewId, ThisPoints ) );
+    }
+    
     // Geometry< Point<3> >::Pointer Clone() const override
     // {
     //     Geometry< Point<3> >::PointsArrayType NewPoints;
