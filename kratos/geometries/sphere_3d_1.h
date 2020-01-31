13b66b68b159519cb06392a255fb4e09f8eafcb2
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-31 16:24:29 +0100

    Adding create with Id for Sphere3D

--- kratos/geometries/sphere_3d_1.h
+++ kratos/geometries/sphere_3d_1.h
@@ -169,6 +169,14 @@ public:
         if ( BaseType::PointsNumber() != 1 )
             KRATOS_ERROR << "Invalid points number. Expected 1, given " << BaseType::PointsNumber() << std::endl;
     }
+    
+    explicit Sphere3D1( 
+        IndexType Id,
+        const PointsArrayType& ThisPoints 
+        ) : BaseType( Id, ThisPoints, &msGeometryData )
+    {
+        KRATOS_ERROR_IF( this->PointsNumber() != 1 ) << "Invalid points number. Expected 1, given " << this->PointsNumber() << std::endl;
+    }
 
     /** Copy constructor.
     Construct this geometry as a copy of given geometry.
@@ -256,9 +264,28 @@ public:
     ///@name Operations
     ///@{
 
+    /**
+     * @brief It creates a new geometry pointer
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
         return typename BaseType::Pointer( new Sphere3D1( ThisPoints ) );
+    }
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param NewId the ID of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
+    typename BaseType::Pointer Create(
+        IndexType NewId,  
+        PointsArrayType const& ThisPoints
+        ) const override
+    {
+        return typename BaseType::Pointer( new Sphere3D1( NewId, ThisPoints ) );
     }
 
     // Geometry< Point<3> >::Pointer Clone() const override
