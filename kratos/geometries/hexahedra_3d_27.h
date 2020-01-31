6f89b112a514dde7c91c204531875e4f9224f18c
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-01-31 16:21:08 +0100

    Adding create with Id for Hexahedra3D27

--- kratos/geometries/hexahedra_3d_27.h
+++ kratos/geometries/hexahedra_3d_27.h
@@ -283,6 +283,14 @@ public:
         if ( this->PointsNumber() != 27 )
             KRATOS_ERROR << "Invalid points number. Expected 27, given " << this->PointsNumber() << std::endl;
     }
+    
+    explicit Hexahedra3D27( 
+        IndexType Id,
+        const PointsArrayType& ThisPoints 
+        ) : BaseType( Id, ThisPoints, &msGeometryData )
+    {
+        KRATOS_ERROR_IF( this->PointsNumber() != 27 ) << "Invalid points number. Expected 27, given " << this->PointsNumber() << std::endl;
+    }
 
     /**
      * Copy constructor.
@@ -372,10 +380,29 @@ public:
     /**
      * Operations
      */
-
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
         return typename BaseType::Pointer( new Hexahedra3D27( ThisPoints ) );
+    }
+    
+    /**
+     * @brief It creates a new geometry pointer
+     * @param NewId the ID of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return a Pointer to the new geometry
+     */
+    typename BaseType::Pointer Create(
+        IndexType NewId,  
+        PointsArrayType const& ThisPoints
+        ) const override
+    {
+        return typename BaseType::Pointer( new Hexahedra3D27( NewId, ThisPoints ) );
     }
 
     //     Geometry< Point<3> >::Pointer Clone() const override
