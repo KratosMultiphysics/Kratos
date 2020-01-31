b971b9ccd48170a00d37d8039b4c34ecd69bad4b
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-01-31 14:16:55 +1000

    Add test for nurbs surface 3d

--- kratos/tests/test_geometries.py
+++ kratos/tests/test_geometries.py
@@ -67,6 +67,42 @@ class TestGeometry(KratosUnittest.TestCase):
         tester = GeometryTesterUtility()
         self.assertTrue( tester.TestHexahedraInterface3D8N(model_part) )
 
+    def create_geometry(model_part):
+        current_model = Model()
+        model_part= current_model.CreateModelPart("Main")
+
+        model_part.CreateNewNode(1, 0.0, 0.0, 0)
+        model_part.CreateNewNode(2, 2.5, 0.0, 0)
+        model_part.CreateNewNode(3, 5.0, 0.0, 0)
+
+        model_part.CreateNewNode(4, 0.0, 0.5, 0)
+        model_part.CreateNewNode(5, 2.5, 0.5, 0)
+        model_part.CreateNewNode(6, 5.0, 0.5, 0)
+
+        model_part.CreateNewNode(7, 0.0, 1.0, 0)
+        model_part.CreateNewNode(8, 2.5, 1.0, 0)
+        model_part.CreateNewNode(9, 5.0, 1.0, 0)
+
+        nodes = model_part.NodesArray
+
+        knots_u = Vector(4)
+        knots_u[0] = 0.0
+        knots_u[1] = 0.0
+        knots_u[2] = 5.0
+        knots_u[3] = 5.0
+
+        knots_v = Vector(4)
+        knots_v[0] = 0.0
+        knots_v[1] = 0.0
+        knots_v[2] = 1.0
+        knots_v[3] = 1.0
+
+        surface = NurbsSurfaceGeometry3D(
+            nodes,
+            2,
+            2,
+            knots_u,
+            knots_v)
 
 if __name__ == '__main__':
     KratosUnittest.main()
