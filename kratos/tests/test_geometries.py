9666672626473daaadb8d158027c96cadc46f04f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-02-02 09:27:00 +0100

    Proper import

--- kratos/tests/test_geometries.py
+++ kratos/tests/test_geometries.py
@@ -1,74 +1,74 @@
 from __future__ import print_function, absolute_import, division
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
-from KratosMultiphysics import *
+import KratosMultiphysics as KM
 
 class TestGeometry(KratosUnittest.TestCase):
     def setUp(self):
         pass
 
     def test_tetrahedra_3D4N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestTetrahedra3D4N(model_part) )
 
     def test_tetrahedra_2D3N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestTriangle2D3N(model_part) )
 
     def test_tetrahedra_2D6N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestTriangle2D6N(model_part) )
 
     def test_tetrahedra_3D10N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestTetrahedra3D10N(model_part) )
 
     def test_tetrahedra_3D8N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestHexahedra3D8N(model_part) )
 
     def test_tetrahedra_3D27N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestHexahedra3D27N(model_part) )
         
     def test_tetrahedra_3D20N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestHexahedra3D20N(model_part) )
 
     def test_quadrilateral_interface_2D4N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestQuadrilateralInterface2D4N(model_part) )
 
     def test_prism_interface_2D4N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestPrismInterface3D6N(model_part) )
 
     def test_hexahedra_interface_2D4N(self):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
-        tester = GeometryTesterUtility()
+        tester = KM.GeometryTesterUtility()
         self.assertTrue( tester.TestHexahedraInterface3D8N(model_part) )
 
     def test_nurbs_surface_3d(model_part):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
 
         model_part.CreateNewNode(1, 0.0, 0.0, 0)
@@ -85,19 +85,19 @@ class TestGeometry(KratosUnittest.TestCase):
 
         nodes = model_part.NodesArray
 
-        knots_u = Vector(4)
+        knots_u = KM.Vector(4)
         knots_u[0] = 0.0
         knots_u[1] = 0.0
         knots_u[2] = 5.0
         knots_u[3] = 5.0
 
-        knots_v = Vector(4)
+        knots_v = KM.Vector(4)
         knots_v[0] = 0.0
         knots_v[1] = 0.0
         knots_v[2] = 1.0
         knots_v[3] = 1.0
 
-        surface = NurbsSurfaceGeometry3D(
+        surface = KM.NurbsSurfaceGeometry3D(
             nodes, 2, 2, knots_u, knots_v)
 
         # check nodes
@@ -113,7 +113,7 @@ class TestGeometry(KratosUnittest.TestCase):
         self.assertFalse(surface.IsRational())
 
     def test_nurbs_curve_3d(model_part):
-        current_model = Model()
+        current_model = KM.Model()
         model_part= current_model.CreateModelPart("Main")
 
         model_part.CreateNewNode(1, 0, 0, 0)
@@ -123,7 +123,7 @@ class TestGeometry(KratosUnittest.TestCase):
 
         nodes = model_part.NodesArray
 
-        knots = Vector(6)
+        knots = KM.Vector(6)
         knots[0] = 0.0
         knots[1] = 0.0
         knots[2] = 0.0
@@ -131,7 +131,7 @@ class TestGeometry(KratosUnittest.TestCase):
         knots[4] = 11.180339887498949
         knots[5] = 11.180339887498949
 
-        curve = NurbsCurveGeometry3D(
+        curve = KM.NurbsCurveGeometry3D(
             nodes, 3, knots)
 
         # check nodes
