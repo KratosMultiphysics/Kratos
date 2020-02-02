b8584cef4f96309cbddd156dba0e14772801a03b
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-02-02 16:06:26 +1000

    Adding Create with GeometryName to Quadrilateral2D4

--- kratos/geometries/quadrilateral_2d_4.h
+++ kratos/geometries/quadrilateral_2d_4.h
@@ -231,6 +231,7 @@ public:
         this->Points().push_back( pFourthPoint );
     }
 
+    /// Constructor with Geometry Id
     explicit Quadrilateral2D4( const PointsArrayType& ThisPoints )
         : BaseType( ThisPoints, &msGeometryData )
     {
@@ -238,12 +239,21 @@ public:
             KRATOS_ERROR << "Invalid points number. Expected 4, given " << this->PointsNumber() << std::endl;
     }
 
+    /// Constructor with Geometry Name
     explicit Quadrilateral2D4( 
-        IndexType Id,
-        const PointsArrayType& ThisPoints 
-        ) : BaseType( Id, ThisPoints, &msGeometryData )
+        IndexType GeometryId,
+        const PointsArrayType& ThisPoints
+    ) : BaseType(GeometryId, ThisPoints, &msGeometryData)
     {
         KRATOS_ERROR_IF( this->PointsNumber() != 4 ) << "Invalid points number. Expected 4, given " << this->PointsNumber() << std::endl;
+    }
+
+    explicit Quadrilateral2D4(
+        IndexType Id,
+        const PointsArrayType& ThisPoints
+    ) : BaseType(Id, ThisPoints, &msGeometryData)
+    {
+        KRATOS_ERROR_IF(this->PointsNumber() != 4) << "Invalid points number. Expected 4, given " << this->PointsNumber() << std::endl;
     }
 
     /**
@@ -334,11 +344,11 @@ public:
     ///@}
     ///@name Operations
     ///@{
-    
+
     /**
-     * @brief It creates a new geometry pointer
+     * @brief Creates a new geometry pointer
      * @param ThisPoints the nodes of the new geometry
-     * @return a Pointer to the new geometry
+     * @return Pointer to the new geometry
      */
     typename BaseType::Pointer Create( PointsArrayType const& ThisPoints ) const override
     {
@@ -346,19 +356,33 @@ public:
     }
 
     /**
-     * @brief It creates a new geometry pointer
-     * @param NewId the ID of the new geometry
+     * @brief Creates a new geometry pointer
+     * @param NewGeometryId the ID of the new geometry
      * @param ThisPoints the nodes of the new geometry
-     * @return a Pointer to the new geometry
+     * @return Pointer to the new geometry
      */
     typename BaseType::Pointer Create(
-        IndexType NewId,  
+        IndexType NewGeometryId,
         PointsArrayType const& ThisPoints
-        ) const override
+    ) const override
     {
-        return typename BaseType::Pointer( new Quadrilateral2D4( NewId, ThisPoints ) );
+        return typename BaseType::Pointer( new Quadrilateral2D4( NewGeometryId, ThisPoints ) );
     }
-    
+
+    /**
+     * @brief Creates a new geometry pointer
+     * @param NewGeometryName the name of the new geometry
+     * @param ThisPoints the nodes of the new geometry
+     * @return Pointer to the new geometry
+     */
+    virtual Pointer Create(
+        IndexType NewGeometryName,
+        PointsArrayType const& ThisPoints
+    ) const
+    {
+        return typename BaseType::Pointer( new Quadrilateral2D4( NewGeometryName, ThisPoints ) );
+    }
+
     // Geometry< Point<3> >::Pointer Clone() const override
     // {
     //     Geometry< Point<3> >::PointsArrayType NewPoints;
