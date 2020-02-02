86bdbcb10dc0c4bb52497b6d5b256a0750f313b2
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-02-02 13:49:18 +1000

    Add test_nurbs_curve_3d

--- kratos/tests/test_geometries.py
+++ kratos/tests/test_geometries.py
@@ -67,7 +67,7 @@ class TestGeometry(KratosUnittest.TestCase):
         tester = GeometryTesterUtility()
         self.assertTrue( tester.TestHexahedraInterface3D8N(model_part) )
 
-    def test_nurbs_surface(model_part):
+    def test_nurbs_surface_3d(model_part):
         current_model = Model()
         model_part= current_model.CreateModelPart("Main")
 
@@ -98,25 +98,58 @@ class TestGeometry(KratosUnittest.TestCase):
         knots_v[3] = 1.0
 
         surface = NurbsSurfaceGeometry3D(
-            nodes,
-            2,
-            2,
-            knots_u,
-            knots_v)
+            nodes, 2, 2, knots_u, knots_v)
 
         # check nodes
         self.assertAlmostEqual(surface[0].X, 0.0)
         self.assertAlmostEqual(surface[1].X, 2.5)
 
         # check knot spans
-        self.assertEqual(surface.KnotsU()[3], 5.0)
+        self.assertAlmostEqual(surface.KnotsU()[3], 5.0)
         self.assertEqual(surface.NumberOfKnotsU(), 4)
         self.assertEqual(surface.NumberOfControlPointsU(), 3)
-
 
         # check rational
         self.assertTrue(!surface.IsRational())
 
+    def test_nurbs_curve_3d(model_part):
+        current_model = Model()
+        model_part= current_model.CreateModelPart("Main")
+
+        model_part.CreateNewNode(1, 0, 0, 0)
+        model_part.CreateNewNode(2, 3.3333333333333335, 1.6666666666666667, 0)
+        model_part.CreateNewNode(3, 6.6666666666666661, 3.333333333333333, 0)
+        model_part.CreateNewNode(4, 10, 5, 0)
+
+        nodes = model_part.NodesArray
+
+        knots = Vector(4)
+        knots[0] = 0.0
+        knots[1] = 0.0
+        knots[2] = 0.0
+        knots[3] = 11.180339887498949
+        knots[4] = 11.180339887498949
+        knots[5] = 11.180339887498949
+
+        curve = NurbsCurveGeometry3D(
+            nodes, 3, knots)
+
+        # check nodes
+        self.assertAlmostEqual(surface[0].X, 0.0)
+        self.assertAlmostEqual(surface[1].X, 2.5)
+
+        # check knot spans
+        self.assertAlmostEqual(surface.Knots()[4], 11.180339887498949)
+        self.assertEqual(surface.NumberOfKnots(), 6)
+        self.assertEqual(surface.NumberOfControlPoints(), 4)
+
+        # check rational
+        self.assertTrue(!curve.IsRational())
+
+        # check general information
+        self.assertEqual(curve.Dimension(), 1);
+        self.assertEqual(curve.WorkingSpaceDimension(), 3);
+        self.assertEqual(curve.LocalSpaceDimension(), 1);
 
 if __name__ == '__main__':
     KratosUnittest.main()
