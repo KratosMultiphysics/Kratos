84516026b9376f434d2ebf0077be8f0e946b8e05
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-02-05 16:36:07 +0100

    Exposing parameters constructor

--- kratos/python/add_strategies_to_python.cpp
+++ kratos/python/add_strategies_to_python.cpp
@@ -460,9 +460,7 @@ namespace Kratos
             typedef ResidualBasedBlockBuilderAndSolver< SparseSpaceType, LocalSpaceType, LinearSolverType > ResidualBasedBlockBuilderAndSolverType;
             py::class_< ResidualBasedBlockBuilderAndSolverType, ResidualBasedBlockBuilderAndSolverType::Pointer,BuilderAndSolverType>(m,"ResidualBasedBlockBuilderAndSolver")
             .def(py::init< LinearSolverType::Pointer > ())
-            .def(py::init< LinearSolverType::Pointer, const bool > ())
-            .def(py::init< LinearSolverType::Pointer, const bool, const bool > ())
-            .def(py::init< LinearSolverType::Pointer, const bool, const bool, const bool > ())
+            .def(py::init< LinearSolverType::Pointer, Parameters > ())
             ;
 
             //********************************************************************
