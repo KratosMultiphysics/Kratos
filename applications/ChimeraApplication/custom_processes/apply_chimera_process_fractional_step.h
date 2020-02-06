6328d232439d8b5e3cbac724e027e3d72446e201
aditya_laptop <shine.aditya@gmail.com>
2020-02-06 10:56:20 +0100

    formatting + using base functions

--- applications/ChimeraApplication/custom_processes/apply_chimera_process_fractional_step.h
+++ applications/ChimeraApplication/custom_processes/apply_chimera_process_fractional_step.h
@@ -27,8 +27,7 @@
 // Application includes
 #include "apply_chimera_process.h"
 
-namespace Kratos
-{
+namespace Kratos {
 
 ///@name Kratos Globals
 ///@{
@@ -49,8 +48,8 @@ namespace Kratos
 ///@name Kratos Classes
 ///@{
 template <int TDim>
-class KRATOS_API(CHIMERA_APPLICATION) ApplyChimeraProcessFractionalStep : public ApplyChimera<TDim>
-{
+class KRATOS_API(CHIMERA_APPLICATION) ApplyChimeraProcessFractionalStep
+    : public ApplyChimera<TDim> {
 public:
     ///@name Type Definitions
     ///@{
@@ -69,7 +68,7 @@ public:
     ///@}
     ///@name Life Cycle
     ///@{
-    ApplyChimeraProcessFractionalStep(ModelPart &rMainModelPart, Parameters iParameters)
+    ApplyChimeraProcessFractionalStep(ModelPart& rMainModelPart, Parameters iParameters)
         : BaseType(rMainModelPart, iParameters)
     {
     }
@@ -81,12 +80,13 @@ public:
 
     virtual void ExecuteFinalizeSolutionStep() override
     {
-        if(BaseType::mReformulateEveryStep)
-        {
-            auto &vel_modelpart = BaseType::mrMainModelPart.GetSubModelPart("fs_velocity_model_part");
+        if (BaseType::mReformulateEveryStep) {
+            auto& vel_modelpart = BaseType::mrMainModelPart.GetSubModelPart(
+                "fs_velocity_model_part");
             vel_modelpart.MasterSlaveConstraints().clear();
 
-            auto &pre_modelpart = BaseType::mrMainModelPart.GetSubModelPart("fs_pressure_model_part");
+            auto& pre_modelpart = BaseType::mrMainModelPart.GetSubModelPart(
+                "fs_pressure_model_part");
             pre_modelpart.MasterSlaveConstraints().clear();
         }
 
@@ -107,13 +107,13 @@ public:
     }
 
     /// Print information about this object.
-    void PrintInfo(std::ostream &rOStream) const override
+    void PrintInfo(std::ostream& rOStream) const override
     {
         rOStream << "ApplyChimeraProcessFractionalStep" << std::endl;
     }
 
     /// Print object's data.
-    void PrintData(std::ostream &rOStream) const override
+    void PrintData(std::ostream& rOStream) const override
     {
         KRATOS_INFO("ApplyChimeraProcessFractionalStep") << std::endl;
     }
@@ -141,113 +141,60 @@ protected:
     ///@{
 
     /**
-     * @brief Applies the continuity between the boundary modelpart and the background.
-     * @param rBoundaryModelPart The boundary modelpart for which the continuity is to be enforced.
-     * @param rBinLocator The bin based locator formulated on the background. This is used to locate nodes on rBoundaryModelPart.
+     * @brief Applies the continuity between the boundary modelpart and the
+     * background.
+     * @param rBoundaryModelPart The boundary modelpart for which the continuity
+     * is to be enforced.
+     * @param rBinLocator The bin based locator formulated on the background.
+     * This is used to locate nodes on rBoundaryModelPart.
      */
-    void ApplyContinuityWithMpcs(ModelPart &rBoundaryModelPart, PointLocatorType &rBinLocator) override
+    void ApplyContinuityWithMpcs(ModelPart& rBoundaryModelPart, PointLocatorType& rBinLocator) override
     {
-        //loop over nodes and find the triangle in which it falls, then do interpolation
+        // loop over nodes and find the triangle in which it falls, then do
+        // interpolation
         MasterSlaveContainerVectorType velocity_ms_container_vector;
         MasterSlaveContainerVectorType pressure_ms_container_vector;
-        BaseType::ReserveMemoryForConstraintContainers(rBoundaryModelPart, velocity_ms_container_vector);
-        BaseType::ReserveMemoryForConstraintContainers(rBoundaryModelPart, pressure_ms_container_vector);
-        std::vector<int> constraints_id_vector;
+        BaseType::ReserveMemoryForConstraintContainers(
+            rBoundaryModelPart, velocity_ms_container_vector);
+        BaseType::ReserveMemoryForConstraintContainers(
+            rBoundaryModelPart, pressure_ms_container_vector);
 
-        int num_constraints_required = (TDim + 1) * (rBoundaryModelPart.Nodes().size());
-        BaseType::CreateConstraintIds(constraints_id_vector, num_constraints_required);
-
-        const int max_results = 10000;
-        const int n_boundary_nodes = static_cast<int> (rBoundaryModelPart.Nodes().size());
-        IndexType counter = 0;
-        IndexType removed_counter = 0;
-        IndexType not_found_counter = 0;
-
-        for (int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn)
-        {
-            ModelPart::NodesContainerType::iterator i_boundary_node = rBoundaryModelPart.NodesBegin() + i_bn;
+        const int n_boundary_nodes = static_cast<int>(rBoundaryModelPart.Nodes().size());
+        for (int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn) {
+            ModelPart::NodesContainerType::iterator i_boundary_node =
+                rBoundaryModelPart.NodesBegin() + i_bn;
             Node<3>::Pointer p_boundary_node = *(i_boundary_node.base());
 
             BaseType::mNodeIdToConstraintIdsMap[p_boundary_node->Id()].reserve(150);
         }
 
-        BuiltinTimer loop_over_b_nodes;
-
-#pragma omp parallel for shared(constraints_id_vector, velocity_ms_container_vector, pressure_ms_container_vector, rBinLocator) reduction(+                                                             \
-                                                                                                                                          : not_found_counter) reduction(+                              \
-                                                                                                                                                                         : removed_counter) reduction(+ \
-                                                                                                                                                                                                      : counter)
-        for (int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn)
-        {
-
-            Vector shape_fun_weights;
-            typename PointLocatorType::ResultContainerType results(max_results);
-            auto &velocity_ms_container = velocity_ms_container_vector[omp_get_thread_num()]; //TODO: change this to out of loop.
-            auto &pressure_ms_container = pressure_ms_container_vector[omp_get_thread_num()]; //TODO: change this to out of loop.
-
-            ModelPart::NodesContainerType::iterator i_boundary_node = rBoundaryModelPart.NodesBegin() + i_bn;
-            Node<3>::Pointer p_boundary_node = *(i_boundary_node.base());
-            IndexType start_constraint_id = i_bn * (TDim + 1) * (TDim + 1);
-            bool node_coupled = false;
-            if ((p_boundary_node)->IsDefined(VISITED))
-                node_coupled = (p_boundary_node)->Is(VISITED);
-
-            typename PointLocatorType::ResultIteratorType result_begin = results.begin();
-            Element::Pointer p_element;
-            bool is_found = false;
-            is_found = rBinLocator.FindPointOnMesh(p_boundary_node->Coordinates(), shape_fun_weights, p_element, result_begin, max_results);
-
-            if (node_coupled && is_found)
-            {
-                removed_counter+=BaseType::RemoveExistingConstraintsForNode(*p_boundary_node);
-                p_boundary_node->Set(VISITED, false);
-            }
-
-            if (is_found == true)
-            {
-                Geometry<Node<3>> &r_geom = p_element->GetGeometry();
-                int init_index = 0;
-                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_X, start_constraint_id + init_index, constraints_id_vector, velocity_ms_container);
-                init_index += (TDim+1);
-                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_Y, start_constraint_id + init_index, constraints_id_vector, velocity_ms_container);
-                init_index += (TDim+1);
-                if (TDim == 3)
-                {
-                    BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_Z, start_constraint_id + init_index, constraints_id_vector, velocity_ms_container);
-                    init_index += (TDim+1);
-                }
-                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, PRESSURE, start_constraint_id + init_index, constraints_id_vector, pressure_ms_container);
-                init_index += (TDim+1);
-                counter += 1;
-            }
-            else
-            {
-                not_found_counter += 1;
-            }
-            p_boundary_node->Set(VISITED, true);
-        } // end of loop over boundary nodes
-
-        KRATOS_INFO_IF("Loop over boundary nodes took : ", BaseType::mEchoLevel > 1)<< loop_over_b_nodes.ElapsedSeconds()<< " seconds"<< std::endl;
+        BaseType::FormulateConstraints(rBoundaryModelPart, rBinLocator, velocity_ms_container_vector,
+                                       pressure_ms_container_vector);
 
         BuiltinTimer mpc_add_time;
-        auto &vel_modelpart = BaseType::mrMainModelPart.GetSubModelPart("fs_velocity_model_part");
+        auto& vel_modelpart =
+            BaseType::mrMainModelPart.GetSubModelPart("fs_velocity_model_part");
         BaseType::AddConstraintsToModelpart(vel_modelpart, velocity_ms_container_vector);
-        VariableUtils().SetFlag(FS_CHIMERA_PRESSURE_CONSTRAINT, false, vel_modelpart.MasterSlaveConstraints());
-        VariableUtils().SetFlag(FS_CHIMERA_VELOCITY_CONSTRAINT, true, vel_modelpart.MasterSlaveConstraints());
+        VariableUtils().SetFlag(FS_CHIMERA_PRESSURE_CONSTRAINT, false,
+                                vel_modelpart.MasterSlaveConstraints());
+        VariableUtils().SetFlag(FS_CHIMERA_VELOCITY_CONSTRAINT, true,
+                                vel_modelpart.MasterSlaveConstraints());
         VariableUtils().SetFlag(ACTIVE, true, vel_modelpart.MasterSlaveConstraints());
 
-        auto &pre_modelpart = BaseType::mrMainModelPart.GetSubModelPart("fs_pressure_model_part");
+        auto& pre_modelpart =
+            BaseType::mrMainModelPart.GetSubModelPart("fs_pressure_model_part");
         BaseType::AddConstraintsToModelpart(pre_modelpart, pressure_ms_container_vector);
-        VariableUtils().SetFlag(FS_CHIMERA_PRESSURE_CONSTRAINT, true, vel_modelpart.MasterSlaveConstraints());
-        VariableUtils().SetFlag(FS_CHIMERA_VELOCITY_CONSTRAINT, false, vel_modelpart.MasterSlaveConstraints());
+        VariableUtils().SetFlag(FS_CHIMERA_PRESSURE_CONSTRAINT, true,
+                                vel_modelpart.MasterSlaveConstraints());
+        VariableUtils().SetFlag(FS_CHIMERA_VELOCITY_CONSTRAINT, false,
+                                vel_modelpart.MasterSlaveConstraints());
         VariableUtils().SetFlag(ACTIVE, true, pre_modelpart.MasterSlaveConstraints());
 
-        KRATOS_INFO_IF("Adding of MPCs from containers to modelpart took : ", BaseType::mEchoLevel > 1)<< mpc_add_time.ElapsedSeconds()<< " seconds"<< std::endl;
-
-        KRATOS_INFO_IF("Number of boundary nodes in : ", BaseType::mEchoLevel > 1) << rBoundaryModelPart.Name() << " is coupled " << rBoundaryModelPart.NumberOfNodes() << std::endl;
-        KRATOS_INFO_IF("Number of Boundary nodes found : ", BaseType::mEchoLevel > 1) << counter << ". Number of constraints : " << counter * 9 << std::endl;
-        KRATOS_INFO_IF("Number of Boundary nodes not found  : ", BaseType::mEchoLevel > 1) << not_found_counter << std::endl;
-        KRATOS_INFO_IF("Number of constraints removed  : ", BaseType::mEchoLevel > 1) << removed_counter << std::endl;
+        KRATOS_INFO_IF("Adding of MPCs from containers to modelpart took : ", BaseType::mEchoLevel > 1)
+            << mpc_add_time.ElapsedSeconds() << " seconds" << std::endl;
+        KRATOS_INFO_IF("Number of boundary nodes in : ", BaseType::mEchoLevel > 1)
+            << rBoundaryModelPart.Name() << " : "
+            << rBoundaryModelPart.NumberOfNodes() << std::endl;
     }
 
     ///@}
@@ -293,7 +240,7 @@ private:
     ///@{
 
     /// Assignment operator.
-    ApplyChimeraProcessFractionalStep &operator=(ApplyChimeraProcessFractionalStep const &rOther);
+    ApplyChimeraProcessFractionalStep& operator=(ApplyChimeraProcessFractionalStep const& rOther);
 
     ///@}
 
--- applications/ChimeraApplication/custom_processes/apply_chimera_process_monolithic.h
+++ applications/ChimeraApplication/custom_processes/apply_chimera_process_monolithic.h
@@ -20,18 +20,17 @@
 #define KRATOS_APPLY_CHIMERA_MONOLITHIC_H_INCLUDED
 
 // System includes
+#include "omp.h"
 #include <algorithm>
 #include <numeric>
 #include <unordered_map>
-#include "omp.h"
 
 // External includes
 
 // Project includes
 #include "apply_chimera_process.h"
 
-namespace Kratos
-{
+namespace Kratos {
 
 ///@name Kratos Globals
 ///@{
@@ -52,8 +51,8 @@ namespace Kratos
 ///@name Kratos Classes
 ///@{
 template <int TDim>
-class KRATOS_API(CHIMERA_APPLICATION) ApplyChimeraProcessMonolithic : public ApplyChimera<TDim>
-{
+class KRATOS_API(CHIMERA_APPLICATION) ApplyChimeraProcessMonolithic
+    : public ApplyChimera<TDim> {
 public:
     ///@name Type Definitions
     ///@{
@@ -74,15 +73,19 @@ public:
 
     /**
      * @brief Constructor
-     * @param rMainModelPart The reference to the modelpart which will be used for computations later on.
+     * @param rMainModelPart The reference to the modelpart which will be used
+     * for computations later on.
      * @param iParameters The settings parameters.
      */
-    explicit ApplyChimeraProcessMonolithic(ModelPart &rMainModelPart, Parameters iParameters) : BaseType(rMainModelPart, iParameters)
+    explicit ApplyChimeraProcessMonolithic(ModelPart& rMainModelPart, Parameters iParameters)
+        : BaseType(rMainModelPart, iParameters)
     {
     }
 
     /// Destructor.
-    virtual ~ApplyChimeraProcessMonolithic() {}
+    virtual ~ApplyChimeraProcessMonolithic()
+    {
+    }
 
     ///@}
     ///@name Operators
@@ -98,13 +101,13 @@ public:
     }
 
     /// Print information about this object.
-    void PrintInfo(std::ostream &rOStream) const override
+    void PrintInfo(std::ostream& rOStream) const override
     {
         rOStream << "ApplyChimeraProcessMonolithic" << std::endl;
     }
 
     /// Print object's data.
-    void PrintData(std::ostream &rOStream) const override
+    void PrintData(std::ostream& rOStream) const override
     {
         KRATOS_INFO("ApplyChimeraProcessMonolithic") << std::endl;
     }
@@ -132,100 +135,42 @@ protected:
     ///@{
 
     /**
-     * @brief Applies the continuity between the boundary modelpart and the background.
-     * @param rBoundaryModelPart The boundary modelpart for which the continuity is to be enforced.
-     * @param rBinLocator The bin based locator formulated on the background. This is used to locate nodes of rBoundaryModelPart on background.
+     * @brief Applies the continuity between the boundary modelpart and the
+     * background.
+     * @param rBoundaryModelPart The boundary modelpart for which the continuity
+     * is to be enforced.
+     * @param rBinLocator The bin based locator formulated on the background.
+     * This is used to locate nodes of rBoundaryModelPart on background.
      */
-    void ApplyContinuityWithMpcs(ModelPart &rBoundaryModelPart, PointLocatorType &rBinLocator) override
+    void ApplyContinuityWithMpcs(ModelPart& rBoundaryModelPart, PointLocatorType& rBinLocator) override
     {
-        //loop over nodes and find the triangle in which it falls, then do interpolation
+        // loop over nodes and find the triangle in which it falls, then do
+        // interpolation
         MasterSlaveContainerVectorType master_slave_container_vector;
-        BaseType::ReserveMemoryForConstraintContainers(rBoundaryModelPart, master_slave_container_vector);
+        BaseType::ReserveMemoryForConstraintContainers(
+            rBoundaryModelPart, master_slave_container_vector);
         std::vector<int> constraints_id_vector;
         int num_constraints_required = (TDim + 1) * (rBoundaryModelPart.Nodes().size());
         BaseType::CreateConstraintIds(constraints_id_vector, num_constraints_required);
 
-        const int max_results = 10000;
-        const int n_boundary_nodes = static_cast<int> (rBoundaryModelPart.Nodes().size());
-        IndexType counter = 0;
-        IndexType removed_counter = 0;
-        IndexType not_found_counter = 0;
-
-        for (int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn)
-        {
-            ModelPart::NodesContainerType::iterator i_boundary_node = rBoundaryModelPart.NodesBegin() + i_bn;
+        const int n_boundary_nodes = static_cast<int>(rBoundaryModelPart.Nodes().size());
+        for (int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn) {
+            ModelPart::NodesContainerType::iterator i_boundary_node =
+                rBoundaryModelPart.NodesBegin() + i_bn;
             BaseType::mNodeIdToConstraintIdsMap[i_boundary_node->Id()].reserve(150);
         }
 
-#pragma omp parallel for shared(constraints_id_vector, master_slave_container_vector, rBinLocator) reduction(+                                                             \
-                                                                                                             : not_found_counter) reduction(+                              \
-                                                                                                                                            : removed_counter) reduction(+ \
-                                                                                                                                                                         : counter)
-        for (int i_bn = 0; i_bn < n_boundary_nodes; ++i_bn)
-        {
-            Vector shape_fun_weights;
-            typename PointLocatorType::ResultContainerType results(max_results);
-            auto &ms_container = master_slave_container_vector[omp_get_thread_num()];
-
-            ModelPart::NodesContainerType::iterator i_boundary_node = rBoundaryModelPart.NodesBegin() + i_bn;
-            Node<3>::Pointer p_boundary_node = *(i_boundary_node.base());
-            unsigned int start_constraint_id = i_bn * (TDim + 1) * (TDim + 1);
-            bool node_coupled = false;
-            if ((p_boundary_node)->IsDefined(VISITED))
-                node_coupled = (p_boundary_node)->Is(VISITED);
-
-            typename PointLocatorType::ResultIteratorType result_begin = results.begin();
-            Element::Pointer p_element;
-            bool is_found = false;
-            is_found = rBinLocator.FindPointOnMesh(p_boundary_node->Coordinates(), shape_fun_weights, p_element, result_begin, max_results);
-
-            if (node_coupled && is_found)
-            {
-                auto &constrainIds_for_the_node = BaseType::mNodeIdToConstraintIdsMap[p_boundary_node->Id()];
-                for (auto const &constraint_id : constrainIds_for_the_node)
-                {
-#pragma omp critical
-                    {
-                        BaseType::mrMainModelPart.RemoveMasterSlaveConstraintFromAllLevels(constraint_id);
-                        removed_counter++;
-                    }
-                }
-                constrainIds_for_the_node.clear();
-                p_boundary_node->Set(VISITED, false);
-            }
-
-            if (is_found)
-            {
-                Geometry<Node<3>> &r_geom = p_element->GetGeometry();
-                int init_index = 0;
-                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_X, start_constraint_id + init_index, constraints_id_vector, ms_container);
-                init_index += (TDim+1);
-                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_Y, start_constraint_id + init_index, constraints_id_vector, ms_container);
-                init_index += (TDim+1);
-                if (TDim == 3)
-                {
-                    BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, VELOCITY_Z, start_constraint_id + init_index, constraints_id_vector, ms_container);
-                    init_index += (TDim+1);
-                }
-                BaseType::ApplyContinuityWithElement(r_geom, *p_boundary_node, shape_fun_weights, PRESSURE, start_constraint_id + init_index, constraints_id_vector, ms_container);
-                init_index += (TDim+1);
-                counter += 1;
-            }
-            else
-            {
-                not_found_counter += 1;
-            }
-            p_boundary_node->Set(VISITED, true);
-        } // end of loop over boundary nodes
-
-        BaseType::AddConstraintsToModelpart(BaseType::mrMainModelPart, master_slave_container_vector);
-
-        KRATOS_INFO_IF("Number of boundary nodes in : ", BaseType::mEchoLevel > 1) << rBoundaryModelPart.Name() << " is coupled " << rBoundaryModelPart.NumberOfNodes() << std::endl;
-        KRATOS_INFO_IF("Number of Boundary nodes found : ", BaseType::mEchoLevel > 1) << counter << ". Number of constraints : " << counter * 9 << std::endl;
-        KRATOS_INFO_IF("Number of Boundary nodes not found  : ", BaseType::mEchoLevel > 1) << not_found_counter << std::endl;
-        KRATOS_INFO_IF("Number of constraints removed  : ", BaseType::mEchoLevel > 1) << removed_counter << std::endl;
+        BaseType::FormulateConstraints(rBoundaryModelPart, rBinLocator,
+                                       master_slave_container_vector,
+                                       master_slave_container_vector);
+        BuiltinTimer mpc_add_time;
+        BaseType::AddConstraintsToModelpart(BaseType::mrMainModelPart,
+                                            master_slave_container_vector);
+        KRATOS_INFO_IF(
+            "Adding of MPCs from containers to modelpart took         : ",
+            BaseType::mEchoLevel > 1)
+            << mpc_add_time.ElapsedSeconds() << " seconds" << std::endl;
     }
-
     ///@}
     ///@name Protected  Access
     ///@{
@@ -269,7 +214,7 @@ private:
     ///@{
 
     /// Assignment operator.
-    ApplyChimeraProcessMonolithic &operator=(ApplyChimeraProcessMonolithic const &rOther);
+    ApplyChimeraProcessMonolithic& operator=(ApplyChimeraProcessMonolithic const& rOther);
 
     ///@}
 }; // Class ApplyChimeraProcessMonolithic
