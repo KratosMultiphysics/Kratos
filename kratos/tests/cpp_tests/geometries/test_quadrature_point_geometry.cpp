2fa9bcf737395e7f302e127c40f1e6cef55722e6
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-02-06 13:40:49 +1000

    Add typedefs to clean readeability

--- kratos/tests/cpp_tests/geometries/test_quadrature_point_geometry.cpp
+++ kratos/tests/cpp_tests/geometries/test_quadrature_point_geometry.cpp
@@ -27,16 +27,21 @@
 
 namespace Kratos {
     namespace Testing {
-        typedef Node<3> NodeType;
-
         typedef std::size_t SizeType;
         typedef std::size_t IndexType;
+
+        typedef Node<3> NodeType;
+        typedef Geometry<Node<3>> GeometryType;
+        typedef typename GeometryType::Pointer GeometryPointerType;
+
+        typedef QuadraturePointGeometry<NodeType, 2, 2> QPGeometryType;
+        typedef typename QPGeometryType::Pointer QPGeometryPointerType;
 
         /** Generates a point type sample triangle2D3.
          * Generates a point type right triangle with origin in the origin and leg size 1.
          * @return  Pointer to a triangle2D3
          */
-        Geometry<Node<3>>::Pointer GeneratePointsTriangle2D3() {
+        GeometryPointerType GeneratePointsTriangle2D3() {
             return Kratos::make_shared<Triangle2D3<NodeType>>(
                 new NodeType(1, 0.0, 0.0, 0.0),
                 new NodeType(2, 1.0, 0.0, 0.0),
@@ -44,7 +49,7 @@ namespace Kratos {
                 );
         }
 
-        Geometry<Node<3>>::Pointer GenerateQuadraturePointGeometry() {
+        GeometryPointerType GenerateQuadraturePointGeometry() {
             auto triangle = GeneratePointsTriangle2D3();
 
             //auto integration_method = triangle->GetDefaultIntegrationMethod();
@@ -53,8 +58,7 @@ namespace Kratos {
             auto r_N = triangle->ShapeFunctionsValues();
 
             Matrix N_i = ZeroMatrix(1, triangle->size());
-            for (IndexType j = 0; j < triangle->size(); ++j)
-            {
+            for (IndexType j = 0; j < triangle->size(); ++j) {
                 N_i(0, j) = r_N(0, j);
             }
             Matrix DN_De = triangle->ShapeFunctionLocalGradient(0);
@@ -65,7 +69,7 @@ namespace Kratos {
                 N_i,
                 DN_De);
 
-            Geometry<Node<3>>::Pointer p_this_quadrature_point(
+            GeometryPointerType p_this_quadrature_point(
                 Kratos::make_shared<QuadraturePointGeometry<Node<3>, 2, 2>>(
                     triangle->Points(),
                     data_container,
@@ -74,7 +78,7 @@ namespace Kratos {
             return p_this_quadrature_point;
         }
 
-        QuadraturePointGeometry<Node<3>, 2, 2>::Pointer GenerateQuadraturePointGeometry2() {
+        QPGeometryPointerType GenerateQuadraturePointGeometry2() {
             auto triangle = GeneratePointsTriangle2D3();
 
             auto integration_points = triangle->IntegrationPoints();
@@ -82,17 +86,17 @@ namespace Kratos {
             auto r_N = triangle->ShapeFunctionsValues();
 
             Matrix N_i = ZeroMatrix(1, triangle->size());
-            for (IndexType j = 0; j < triangle->size(); ++j)
-            {
+            for (IndexType j = 0; j < triangle->size(); ++j) {
                 N_i(0, j) = r_N(0, j);
             }
             const Matrix& DN_De = triangle->ShapeFunctionLocalGradient(0);
 
             return Kratos::make_shared<QuadraturePointGeometry<Node<3>, 2, 2>>(
-                    triangle->Points(),
-                    integration_points[0],
-                    N_i, DN_De,
-                    triangle.get());
+                triangle->Points(),
+                integration_points[0],
+                N_i,
+                DN_De,
+                triangle.get());
         }
 
         KRATOS_TEST_CASE_IN_SUITE(QuadraturePointUtility, KratosCoreFastSuite)
@@ -127,9 +131,9 @@ namespace Kratos {
 
         KRATOS_TEST_CASE_IN_SUITE(QuadraturePointGeometry2dCopyConstructor, KratosCoreFastSuite)
         {
-            auto p_this_quadrature_point = GenerateQuadraturePointGeometry2();
+            QPGeometryPointerType p_this_quadrature_point = GenerateQuadraturePointGeometry2();
 
-            QuadraturePointGeometry<Node<3>, 2, 2> geom(*p_this_quadrature_point);
+            QPGeometryType geom(*p_this_quadrature_point);
 
             KRATOS_CHECK_EQUAL(geom.size(), 3);
             KRATOS_CHECK_EQUAL(geom.WorkingSpaceDimension(), 2);
@@ -147,9 +151,9 @@ namespace Kratos {
 
         KRATOS_TEST_CASE_IN_SUITE(QuadraturePointGeometry2dAssignmentOperator, KratosCoreFastSuite)
         {
-            auto p_this_quadrature_point = GenerateQuadraturePointGeometry2();
+            QPGeometryPointerType p_this_quadrature_point = GenerateQuadraturePointGeometry2();
 
-            QuadraturePointGeometry<Node<3>, 2, 2> geom = *p_this_quadrature_point;
+            QPGeometryType geom = *p_this_quadrature_point;
 
             KRATOS_CHECK_EQUAL(geom.size(), 3);
             KRATOS_CHECK_EQUAL(geom.WorkingSpaceDimension(), 2);
