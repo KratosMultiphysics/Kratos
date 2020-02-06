589b49b03dda951c16bde0688ac40382c6df615a
aditya_laptop <shine.aditya@gmail.com>
2020-02-06 17:07:24 +0100

    adding flag var to modelpart

--- applications/ChimeraApplication/python_scripts/navier_stokes_solver_fractionalstep_chimera.py
+++ applications/ChimeraApplication/python_scripts/navier_stokes_solver_fractionalstep_chimera.py
@@ -23,6 +23,7 @@ class NavierStokesSolverFractionalStepForChimera(NavierStokesSolverFractionalSte
 
     def AddVariables(self):
         super(NavierStokesSolverFractionalStepForChimera,self).AddVariables()
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.FLAG_VARIABLE)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE)
         self.main_model_part.AddNodalSolutionStepVariable(KratosChimera.CHIMERA_DISTANCE)
         self.main_model_part.AddNodalSolutionStepVariable(KratosChimera.ROTATIONAL_ANGLE)
