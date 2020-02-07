c2ca02f0f9cd83d4818f491586ccb7f92e672b15
aditya_laptop <shine.aditya@gmail.com>
2020-02-07 11:45:03 +0100

    revert changes in mp.h

--- kratos/includes/model_part.h
+++ kratos/includes/model_part.h
@@ -369,7 +369,6 @@ public:
                 else
                     aux.push_back( *(it.base()) );
             }
-            // (aux.end()-1)->SetSolutionStepVariablesList(mpVariablesList);
         }
 
         //now add to the root model part
