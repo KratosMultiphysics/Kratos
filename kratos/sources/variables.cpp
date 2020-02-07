6bb2f71b8b943c109ceda4c8d9693a689e0a537a
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2020-02-07 18:02:45 +0100

    IMPOSED_Z_STRAIN_VALUE moved to core

--- kratos/sources/variables.cpp
+++ kratos/sources/variables.cpp
@@ -441,6 +441,10 @@ KRATOS_CREATE_VARIABLE(double, INTEGRATION_WEIGHT)
 KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(INTEGRATION_COORDINATES)
 KRATOS_CREATE_VARIABLE(TableStreamUtility::Pointer, TABLE_UTILITY )
 
+// for DEM-FEM 2D
+KRATOS_CREATE_VARIABLE(double, IMPOSED_Z_STRAIN_VALUE)
+KRATOS_CREATE_VARIABLE(bool, IMPOSED_Z_STRAIN_OPTION)
+
 //------------------------------------------------------------------------------//
 //------------------------------------------------------------------------------//
 //------------------------------------------------------------------------------//
@@ -882,5 +886,9 @@ void KratosApplication::RegisterVariables() {
     KRATOS_REGISTER_3D_VARIABLE_WITH_COMPONENTS(INTEGRATION_COORDINATES)
 
     KRATOS_REGISTER_VARIABLE(TABLE_UTILITY)
+
+    // for DEM-FEM 2D
+    KRATOS_REGISTER_VARIABLE(IMPOSED_Z_STRAIN_VALUE)
+    KRATOS_REGISTER_VARIABLE(IMPOSED_Z_STRAIN_OPTION)
 }
 }  // namespace Kratos.
