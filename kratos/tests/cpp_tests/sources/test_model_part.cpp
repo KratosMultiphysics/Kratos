3aba2c28a6f8f8fcb3242fe366407f8977a37178
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-02-12 14:47:38 +0100

    Clean up

--- kratos/tests/cpp_tests/sources/test_model_part.cpp
+++ kratos/tests/cpp_tests/sources/test_model_part.cpp
@@ -28,60 +28,24 @@ namespace Kratos {
         Properties::Pointer p_elem_prop = rModelPart.CreateNewProperties(0);
 
         // First we create the nodes
-        NodeType::Pointer p_node_1 = rModelPart.CreateNewNode(1, 0.0 , 0.0 , 0.0);
-        NodeType::Pointer p_node_2 = rModelPart.CreateNewNode(2, 1.0 , 0.0 , 0.0);
-        NodeType::Pointer p_node_3 = rModelPart.CreateNewNode(3, 1.0 , 1.0 , 0.0);
-        NodeType::Pointer p_node_4 = rModelPart.CreateNewNode(4, 0.0 , 1.0 , 0.0);
-        NodeType::Pointer p_node_5 = rModelPart.CreateNewNode(5, 2.0 , 0.0 , 0.0);
-        NodeType::Pointer p_node_6 = rModelPart.CreateNewNode(6, 2.0 , 1.0 , 0.0);
+        rModelPart.CreateNewNode(1, 0.0 , 0.0 , 0.0);
+        rModelPart.CreateNewNode(2, 1.0 , 0.0 , 0.0);
+        rModelPart.CreateNewNode(3, 1.0 , 1.0 , 0.0);
+        rModelPart.CreateNewNode(4, 0.0 , 1.0 , 0.0);
+        rModelPart.CreateNewNode(5, 2.0 , 0.0 , 0.0);
+        rModelPart.CreateNewNode(6, 2.0 , 1.0 , 0.0);
 
         // Now we create the "conditions"
-        std::vector<NodeType::Pointer> condition_nodes_0 (2);
-        condition_nodes_0[0] = p_node_1;
-        condition_nodes_0[1] = p_node_2;
-
-        std::vector<NodeType::Pointer> condition_nodes_1 (2);
-        condition_nodes_1[0] = p_node_1;
-        condition_nodes_1[1] = p_node_4;
-
-        std::vector<NodeType::Pointer> condition_nodes_2 (2);
-        condition_nodes_2[0] = p_node_2;
-        condition_nodes_2[1] = p_node_5;
-
-        std::vector<NodeType::Pointer> condition_nodes_3 (2);
-        condition_nodes_3[0] = p_node_5;
-        condition_nodes_3[1] = p_node_6;
-
-        Condition::Pointer p_cond_0 = rModelPart.CreateNewCondition("Condition2D2N", 1, PointerVector<NodeType>{condition_nodes_0}, p_elem_prop);
-        Condition::Pointer p_cond_1 = rModelPart.CreateNewCondition("Condition2D2N", 2, PointerVector<NodeType>{condition_nodes_1}, p_elem_prop);
-        Condition::Pointer p_cond_2 = rModelPart.CreateNewCondition("Condition2D2N", 3, PointerVector<NodeType>{condition_nodes_2}, p_elem_prop);
-        Condition::Pointer p_cond_3 = rModelPart.CreateNewCondition("Condition2D2N", 4, PointerVector<NodeType>{condition_nodes_3}, p_elem_prop);
+        rModelPart.CreateNewCondition("Condition2D2N", 1, {{1, 2}}, p_elem_prop);
+        rModelPart.CreateNewCondition("Condition2D2N", 2, {{1, 4}}, p_elem_prop);
+        rModelPart.CreateNewCondition("Condition2D2N", 3, {{2, 5}}, p_elem_prop);
+        rModelPart.CreateNewCondition("Condition2D2N", 4, {{5, 6}}, p_elem_prop);
 
         // Now we create the "elements"
-        std::vector<NodeType::Pointer> element_nodes_0 (3);
-        element_nodes_0[0] = p_node_1;
-        element_nodes_0[1] = p_node_2;
-        element_nodes_0[2] = p_node_3;
-
-        std::vector<NodeType::Pointer> element_nodes_1 (3);
-        element_nodes_1[0] = p_node_1;
-        element_nodes_1[1] = p_node_3;
-        element_nodes_1[2] = p_node_4;
-
-        std::vector<NodeType::Pointer> element_nodes_2 (3);
-        element_nodes_2[0] = p_node_2;
-        element_nodes_2[1] = p_node_5;
-        element_nodes_2[2] = p_node_3;
-
-        std::vector<NodeType::Pointer> element_nodes_3 (3);
-        element_nodes_3[0] = p_node_5;
-        element_nodes_3[1] = p_node_6;
-        element_nodes_3[2] = p_node_3;
-
-        Element::Pointer p_elem_0 = rModelPart.CreateNewElement("Element2D3N", 1, PointerVector<NodeType>{element_nodes_0}, p_elem_prop);
-        Element::Pointer p_elem_1 = rModelPart.CreateNewElement("Element2D3N", 2, PointerVector<NodeType>{element_nodes_1}, p_elem_prop);
-        Element::Pointer p_elem_2 = rModelPart.CreateNewElement("Element2D3N", 3, PointerVector<NodeType>{element_nodes_2}, p_elem_prop);
-        Element::Pointer p_elem_3 = rModelPart.CreateNewElement("Element2D3N", 4, PointerVector<NodeType>{element_nodes_3}, p_elem_prop);
+        rModelPart.CreateNewElement("Element2D3N", 1, {{1,2,3}}, p_elem_prop);
+        rModelPart.CreateNewElement("Element2D3N", 2, {{1,3,4}}, p_elem_prop);
+        rModelPart.CreateNewElement("Element2D3N", 3, {{2,5,3}}, p_elem_prop);
+        rModelPart.CreateNewElement("Element2D3N", 4, {{5,6,3}}, p_elem_prop);
     }
 
     KRATOS_TEST_CASE_IN_SUITE(ModelPartSubModelPartsIterator, KratosCoreFastSuite)
