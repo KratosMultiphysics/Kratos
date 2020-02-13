4a6ae47d93d4bc368790c3810299abd047cfd715
sunethwarna <suneth.warna@gmail.com>
2020-02-13 14:14:16 +0100

    adding mpi tests

--- applications/StatisticsApplication/tests/test_StatisticsApplication.py
+++ applications/StatisticsApplication/tests/test_StatisticsApplication.py
@@ -1,6 +1,9 @@
 # import Kratos
 import KratosMultiphysics
+import KratosMultiphysics.kratos_utilities as kratos_utilities
 import KratosMultiphysics.StatisticsApplication
+
+import os, subprocess
 
 # Import Kratos "wrapper" for unittests
 import KratosMultiphysics.KratosUnittest as KratosUnittest
@@ -59,6 +62,17 @@ if __name__ == '__main__':
     KratosMultiphysics.Logger.PrintInfo("Unittests", "\nRunning cpp unit tests ...")
     run_cpp_unit_tests.run()
     KratosMultiphysics.Logger.PrintInfo("Unittests", "Finished running cpp unit tests!")
+
+    if kratos_utilities.IsMPIAvailable() and kratos_utilities.CheckIfApplicationsAvailable("MetisApplication", "TrilinosApplication"):
+        KratosMultiphysics.Logger.PrintInfo("Unittests", "\nRunning mpi python tests ...")
+        p = subprocess.Popen(
+            ["mpiexec", "-np", "2", "python3", "test_StatisticsApplication_mpi.py", "--using-mpi"],
+            stdout=subprocess.PIPE,
+            cwd=os.path.dirname(os.path.abspath(__file__)))
+        p.wait()
+        KratosMultiphysics.Logger.PrintInfo("Unittests", "Finished mpi python tests!")
+    else:
+        KratosMultiphysics.Logger.PrintInfo("Unittests", "\nSkipping mpi python tests due to missing dependencies")
 
     KratosMultiphysics.Logger.PrintInfo("Unittests", "\nRunning python tests ...")
     KratosUnittest.runTests(AssembleTestSuites())
