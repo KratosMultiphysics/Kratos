13d70ba24884156f288da20e21bb8a2aee3a5bd6
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-02-13 18:04:05 +1000

    Expose GeometriesVector to python

--- kratos/python/containers_interface.h
+++ kratos/python/containers_interface.h
@@ -98,6 +98,27 @@ class PointerVectorPythonInterface
 };
 
 template< class TContainerType >
+class PointerVectorPythonInterface2
+{
+public:
+
+    PointerVectorPythonInterface2() {};
+    virtual ~PointerVectorPythonInterface2() {};
+
+    void CreateInterface(pybind11::module& m, std::string ContainerName)
+    {
+        py::class_<TContainerType, typename TContainerType::Pointer  >(m, ContainerName.c_str())
+            .def(py::init<>())
+            .def("__len__", [](TContainerType& self) {return self.size(); })
+            .def("__setitem__", [](TContainerType& self, unsigned int i, typename TContainerType::value_type& value) {self[i] = value; })
+            .def("__getitem__", [](TContainerType& self, unsigned int i) {return self(i); })
+            .def("__iter__", [](TContainerType& self) {return py::make_iterator(self.begin(), self.end()); }, py::keep_alive<0, 1>())
+            //.def("append", [](TContainerType& self, typename TContainerType::value_type& value) {self.push_back(&value); })
+            ;
+    }
+};
+
+template< class TContainerType >
 class PointerVectorSetPythonInterface
 {
 public:
