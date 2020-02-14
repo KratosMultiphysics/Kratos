dafd35c5909a937a409b9460e49b61c08358c0cf
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-02-14 14:55:57 +1000

    Add additional test with distorted knot vectors

--- applications/IgaApplication/tests/shell_3p_element_tests.py
+++ applications/IgaApplication/tests/shell_3p_element_tests.py
@@ -159,10 +159,71 @@ class Shell3pElementTests(KratosUnittest.TestCase):
             return surface
 
         surface = Shell3pElementTests.solve_cantilever(create_geometry)
-
+        
         for node in surface:
             self.assertAlmostEqual(node.GetValue(KM.DISPLACEMENT_X), 0)
             self.assertAlmostEqual(node.GetValue(KM.DISPLACEMENT_Y), 0)
+
+        self.assertAlmostEqual(surface[0].Z, 0.0)
+        self.assertAlmostEqual(surface[1].Z, 0.0)
+        self.assertAlmostEqual(surface[2].Z, 0.0)
+
+        self.assertAlmostEqual(surface[3].Z, 0.0)
+        self.assertAlmostEqual(surface[4].Z, 0.0)
+        self.assertAlmostEqual(surface[5].Z, 0.0)
+
+        self.assertAlmostEqual(surface[6].Z, -0.141013552223511)
+        self.assertAlmostEqual(surface[7].Z, -0.140899868451905)
+        self.assertAlmostEqual(surface[8].Z, -0.141013552223511)
+
+    # test same geometry but with different knot  vector parameters.
+    def testCantileverOneQuadraticSpanWithParameterDistortion(self):
+        def create_geometry(model_part):
+            node1 = model_part.CreateNewNode(1, 0.0, 0.0, 0)
+            node2 = model_part.CreateNewNode(2, 2.5, 0.0, 0)
+            node3 = model_part.CreateNewNode(3, 5.0, 0.0, 0)
+
+            node4 = model_part.CreateNewNode(4, 0.0, 0.5, 0)
+            node5 = model_part.CreateNewNode(5, 2.5, 0.5, 0)
+            node6 = model_part.CreateNewNode(6, 5.0, 0.5, 0)
+
+            node7 = model_part.CreateNewNode(7, 0.0, 1.0, 0)
+            node8 = model_part.CreateNewNode(8, 2.5, 1.0, 0)
+            node9 = model_part.CreateNewNode(9, 5.0, 1.0, 0)
+
+            nodes = KM.NodesVector()
+            nodes.append(node1)
+            nodes.append(node2)
+            nodes.append(node3)
+            nodes.append(node4)
+            nodes.append(node5)
+            nodes.append(node6)
+            nodes.append(node7)
+            nodes.append(node8)
+            nodes.append(node9)
+
+            knots_u = KM.Vector(4)
+            knots_u[0] = 0.0
+            knots_u[1] = 0.0
+            knots_u[2] = 1.0
+            knots_u[3] = 1.0
+
+            knots_v = KM.Vector(4)
+            knots_v[0] = 0.0
+            knots_v[1] = 0.0
+            knots_v[2] = 2.0
+            knots_v[3] = 2.0
+
+            surface = KM.NurbsSurfaceGeometry3D(
+                nodes, 2, 2, knots_u, knots_v)
+
+            return surface
+
+        surface = ShellKLDiscreteElementTests.solve_cantilever(create_geometry)
+
+        for node in surface:
+            self.assertAlmostEqual(node.GetValue(DISPLACEMENT_X), 0)
+            self.assertAlmostEqual(node.GetValue(DISPLACEMENT_Y), 0)
 
         self.assertAlmostEqual(surface[0].Z, 0.0)
         self.assertAlmostEqual(surface[1].Z, 0.0)
