3e975938fb0b3e824dbafa2be7dd5b038c86cdcb
marcnunezc <mnunez@cimne.upc.edu>
2020-02-17 16:50:04 +0100

    Remove reference definition in data copy

--- applications/CompressiblePotentialFlowApplication/custom_elements/adjoint_base_potential_flow_element.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_elements/adjoint_base_potential_flow_element.cpp
@@ -35,7 +35,7 @@ namespace Kratos
     template <class TPrimalElement>
     void AdjointBasePotentialFlowElement<TPrimalElement>::InitializeSolutionStep(ProcessInfo& rCurrentProcessInfo)
     {
-        auto& adjoint_geometry_data = this->GetGeometry().GetData();
+        auto adjoint_geometry_data = this->GetGeometry().GetData();
         mpPrimalElement->GetGeometry().SetData(adjoint_geometry_data);
         mpPrimalElement->Set(Flags(*this));
         mpPrimalElement->InitializeSolutionStep(rCurrentProcessInfo);
