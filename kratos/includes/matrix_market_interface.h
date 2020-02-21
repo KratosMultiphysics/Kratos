2b05285a16282b77f2029ea7036258d3f9e11029
qaumann <qaumann@users.noreply.github.com>
2020-02-21 15:30:34 +0100

    add support for complex input

--- kratos/includes/matrix_market_interface.h
+++ kratos/includes/matrix_market_interface.h
@@ -45,10 +45,37 @@ extern "C"
 namespace Kratos
 {
 
+// Type checks
+inline bool IsCorrectType(MM_typecode& mm_code, const double& value)
+{
+    return mm_is_real(mm_code);
+}
+
+inline bool IsCorrectType(MM_typecode& mm_code, const std::complex<double>& value)
+{
+    return mm_is_complex(mm_code);
+}
+
 // Matrix I/O routines
+inline bool ReadMatrixMarketMatrixEntry(FILE *f, int& I, int& J, double& V)
+{
+
+    return fscanf(f, "%d %d %lg", &I, &J, &V) == 3;
+}
+
+inline bool ReadMatrixMarketMatrixEntry(FILE *f, int& I, int& J, std::complex<double>& V)
+{
+    double real;
+    double imag;
+    const int i = fscanf(f, "%d %d %lg %lg", &I, &J, &real, &imag);
+    V = std::complex<double>(real, imag);
+    return i == 4;
+}
 
 template <typename CompressedMatrixType> inline bool ReadMatrixMarketMatrix(const char *FileName, CompressedMatrixType &M)
 {
+    typedef typename CompressedMatrixType::value_type ValueType;
+
     // Open MM file for reading
     FILE *f = fopen(FileName, "r");
 
@@ -76,9 +103,9 @@ template <typename CompressedMatrixType> inline bool ReadMatrixMarketMatrix(cons
     }
 
     // Check for supported types of MM file
-    if (!((mm_is_real(mm_code) || mm_is_integer(mm_code) || mm_is_pattern(mm_code)) && mm_is_coordinate(mm_code) && mm_is_sparse(mm_code)))
+    if (!(mm_is_coordinate(mm_code) && mm_is_sparse(mm_code)))
     {
-        printf("ReadMatrixMarketMatrix(): invalid MatrixMarket type, \"%s\".\n",  mm_typecode_to_str(mm_code));
+        printf("ReadMatrixMarketMatrix(): unspported MatrixMarket type, \"%s\".\n",  mm_typecode_to_str(mm_code));
         fclose(f);
         return false;
     }
@@ -96,7 +123,15 @@ template <typename CompressedMatrixType> inline bool ReadMatrixMarketMatrix(cons
     // Allocate temporary arrays
     int *I = new int[nnz];
     int *J = new int[nnz];
-    double *V = new double[nnz];
+    ValueType *V = new ValueType[nnz];
+
+    // Check if matrix type matches MM file
+    if (!IsCorrectType(mm_code, V[0]))
+    {
+        printf("ReadMatrixMarketMatrix(): MatrixMarket type, \"%s\" does not match provided matrix type.\n",  mm_typecode_to_str(mm_code));
+        fclose(f);
+        return false;
+    }
 
     // Read MM file
 
@@ -126,7 +161,7 @@ template <typename CompressedMatrixType> inline bool ReadMatrixMarketMatrix(cons
     else
         for (int i = 0; i < nnz; i++)
         {
-            if (fscanf(f, "%d %d %lg", &I[i], &J[i], &V[i]) != 3)
+            if (! ReadMatrixMarketMatrixEntry(f, I[i], J[i], V[i]))
             {
                 printf("ReadMatrixMarketMatrix(): invalid data.\n");
                 fclose(f);
@@ -187,7 +222,7 @@ template <typename CompressedMatrixType> inline bool ReadMatrixMarketMatrix(cons
     int *filled = new int[size1];
     int *indices = new int[size1];
     int *columns = new int[nnz2];
-    double *values = new double[nnz2];
+    ValueType *values = new ValueType[nnz2];
 
     indices[0] = 0;
     for (int i = 1; i < size1; i++)
@@ -394,8 +429,24 @@ template <typename CompressedMatrixType> inline bool WriteMatrixMarketMatrix(con
 
 // Vector I/O routines
 
+inline bool ReadMatrixMarketVectorEntry(FILE *f, double& entry)
+{
+    return fscanf(f, "%lg", &entry) == 1;
+}
+
+inline bool ReadMatrixMarketVectorEntry(FILE *f, std::complex<double>& entry)
+{
+    double real;
+    double imag;
+    const int i = fscanf(f, "%lg %lg", &real, &imag);
+    entry = std::complex<double>(real, imag);
+    return i == 2;
+}
+
 template <typename VectorType> inline bool ReadMatrixMarketVector(const char *FileName, VectorType &V)
 {
+    typedef typename VectorType::value_type ValueType;
+    
     // Open MM file for reading
     FILE *f = fopen(FileName, "r");
 
@@ -423,9 +474,9 @@ template <typename VectorType> inline bool ReadMatrixMarketVector(const char *Fi
     }
 
     // Check for supported types of MM file
-    if (!((mm_is_real(mm_code) || mm_is_integer(mm_code)) && mm_is_array(mm_code)))
+    if (!((!mm_is_pattern(mm_code)) && mm_is_array(mm_code)))
     {
-        printf("ReadMatrixMarketVector(): invalid MatrixMarket type, \"%s\".\n",  mm_typecode_to_str(mm_code));
+        printf("ReadMatrixMarketVector(): unsupported MatrixMarket type, \"%s\".\n",  mm_typecode_to_str(mm_code));
         fclose(f);
         return false;
     }
@@ -449,13 +500,21 @@ template <typename VectorType> inline bool ReadMatrixMarketVector(const char *Fi
     }
 
     VectorType *v = new VectorType(size1);
-    double T;
+    ValueType T;
+
+    // Check if vector type matches MM file
+    if (!IsCorrectType(mm_code, T))
+    {
+        printf("ReadMatrixMarketVector(): MatrixMarket type, \"%s\" does not match provided vector type.\n",  mm_typecode_to_str(mm_code));
+        fclose(f);
+        return false;
+    }
 
     // Read MM file
 
     for (int i = 0; i < size1; i++)
     {
-        if (fscanf(f, "%lg", &T) != 1)
+        if (! ReadMatrixMarketVectorEntry(f, T))
         {
             printf("ReadMatrixMarketVector(): invalid data.\n");
             fclose(f);
