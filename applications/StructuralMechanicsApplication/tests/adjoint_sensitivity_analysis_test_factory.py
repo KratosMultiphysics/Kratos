13c7b2736b09599cf82549109daca0d7e1b495c4
MFusseder <martin.fusseder@tum.de>
2020-02-25 18:20:57 +0100

    move solver selection to external function

--- applications/StructuralMechanicsApplication/tests/adjoint_sensitivity_analysis_test_factory.py
+++ applications/StructuralMechanicsApplication/tests/adjoint_sensitivity_analysis_test_factory.py
@@ -8,6 +8,7 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 from KratosMultiphysics.StructuralMechanicsApplication import structural_mechanics_analysis
 import KratosMultiphysics.kratos_utilities as kratos_utilities
 from KratosMultiphysics import IsDistributedRun
+from structural_mechanics_test_factory import SelectAndVerifyLinearSolver
 
 has_hdf5_application = kratos_utilities.CheckIfApplicationsAvailable("HDF5Application")
 
@@ -17,58 +18,35 @@ class AdjointSensitivityAnalysisTestFactory(KratosUnittest.TestCase):
         with KratosUnittest.WorkFolderScope(".", __file__):
             # Reading the ProjectParameters
             with open(self.primal_file_name,'r') as parameter_file:
-                self.primal_parameters = KratosMultiphysics.Parameters(parameter_file.read())
+                primal_parameters = KratosMultiphysics.Parameters(parameter_file.read())
             with open(self.adjoint_file_name,'r') as parameter_file:
                 self.adjoint_parameters = KratosMultiphysics.Parameters(parameter_file.read())
-            self.problem_name = self.primal_parameters["problem_data"]["problem_name"].GetString()
-            self.model_part_name = self.primal_parameters["solver_settings"]["model_part_name"].GetString()
+            self.problem_name = primal_parameters["problem_data"]["problem_name"].GetString()
+            self.model_part_name = primal_parameters["solver_settings"]["model_part_name"].GetString()
 
             # To avoid many prints
-            if (self.primal_parameters["problem_data"]["echo_level"].GetInt() == 0 or self.adjoint_parameters["problem_data"]["echo_level"].GetInt() == 0):
+            if (primal_parameters["problem_data"]["echo_level"].GetInt() == 0 or self.adjoint_parameters["problem_data"]["echo_level"].GetInt() == 0):
                 KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
 
-            self.verify_linear_solver(self.primal_parameters)
-            self.verify_linear_solver(self.adjoint_parameters)
+            SelectAndVerifyLinearSolver(primal_parameters, self.skipTest)
+            SelectAndVerifyLinearSolver(self.adjoint_parameters, self.skipTest)
 
             # solve primal problem
             model_primal = KratosMultiphysics.Model()
-            primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, self.primal_parameters)
+            primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, primal_parameters)
             primal_analysis.Run()
             # create adjoint analysis
             model_adjoint = KratosMultiphysics.Model()
             self.adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_adjoint, self.adjoint_parameters)
             self.adjoint_analysis.Initialize()
 
-    def verify_linear_solver(self, parameter_file):
-        # The mechanical solver selects automatically the fastest linear-solver available
-        # this might not be appropriate for a test, therefore in case nothing is specified,
-        # the previous default linear-solver is set
-        if not parameter_file["solver_settings"].Has("linear_solver_settings"):
-            # check if running in MPI because there we use a different default linear solver
-            if IsDistributedRun():
-                default_lin_solver_settings = KratosMultiphysics.Parameters("""{
-                    "solver_type" : "amesos",
-                    "amesos_solver_type" : "Amesos_Klu"
-                }""")
-            else:
-                default_lin_solver_settings = KratosMultiphysics.Parameters("""{
-                    "solver_type": "EigenSolversApplication.sparse_lu"
-                }""")
-            parameter_file["solver_settings"].AddValue("linear_solver_settings", default_lin_solver_settings)
-
-        solver_type = parameter_file["solver_settings"]["linear_solver_settings"]["solver_type"].GetString()
-        solver_type_splitted = solver_type.split(".")
-        if len(solver_type_splitted) == 2:
-            # this means that we use a solver from an application
-            # hence we have to check if it exists, otherwise skip the test
-            app_name = solver_type_splitted[0]
-            solver_name = solver_type_splitted[1]
-            if not kratos_utilities.CheckIfApplicationsAvailable(app_name):
-                self.skipTest('Application "{}" is needed for the specified solver "{}" but is not available'.format(app_name, solver_name))
-
-    def solve_adjoint_problem(self):
+    def test_execution(self):
         with KratosUnittest.WorkFolderScope(".", __file__):
             self.adjoint_analysis.RunSolutionLoop()
+            self.perform_additional_checks()
+
+    def perform_additional_checks(self):
+        pass
 
     def tearDown(self):
         # Within this location context:
@@ -85,9 +63,7 @@ class TestAdjointSensitivityAnalysisBeamStructureLocalStress(AdjointSensitivityA
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_local_stress_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         reference_values = [-87.62277093392399, 9.497391494932984, 38.125186783868, 0.6250049974719261, 0.15624887499699122]
         sensitivities_to_check = []
         element_list = [1,2,3,4,5,6,10]
@@ -111,9 +87,7 @@ class TestAdjointSensitivityAnalysisBeamStructureNodalDisplacement(AdjointSensit
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_nodal_disp_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         reference_values = [-0.45410279537614157, -0.37821875982596204, -0.006200296058668847, 0.0004340210813670321]
         sensitivities_to_check = []
         element_list = [1,6,10]
@@ -133,9 +107,7 @@ class TestAdjointSensitivityAnalysisBeamStructureStrainEnergy(AdjointSensitivity
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_strain_energy_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         reference_values = [-9.082055907522943, -7.5643751965193164, -0.12400592117339182, 0.017360843254681547]
         sensitivities_to_check = []
         element_list = [1,6,10]
@@ -155,9 +127,7 @@ class TestAdjointSensitivityAnalysisBeamStructureNodalReaction(AdjointSensitivit
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_beam_structure_3d2n/beam_test_nodal_reaction_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         adjoint_model_part = self.adjoint_analysis.model.GetModelPart(self.model_part_name)
         reference_values = [-0.31249774999397384, -1.249]
         sensitivities_to_check = []
@@ -173,9 +143,7 @@ class TestAdjointSensitivityAnalysisShellStructureLocalStress(AdjointSensitivity
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_shell_structure_3d3n/linear_shell_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_shell_structure_3d3n/linear_shell_test_local_stress_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         reference_values = [1.7135092490964121, -6.860092387341681, 0.14749301178647778, -0.0823339298948347]
         sensitivities_to_check = []
         element_list = [1,2,8]
@@ -195,9 +163,7 @@ class TestAdjointSensitivityAnalysisShellStructureNodalDisplacement(AdjointSensi
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_shell_structure_3d3n/linear_shell_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_shell_structure_3d3n/linear_shell_test_nodal_disp_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         reference_values = [-0.09916013365433643, -0.23348175177098657, -0.04942512089147077, 0.012125502238309537]
         sensitivities_to_check = []
         element_list = [1,2,8]
@@ -217,9 +183,7 @@ class TestAdjointSensitivityAnalysisShellStructureStrainEnergy(AdjointSensitivit
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_shell_structure_3d3n/linear_shell_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_shell_structure_3d3n/linear_shell_test_strain_energy_adjoint_parameters.json"
 
-    def test_execution(self):
-        self.solve_adjoint_problem()
-
+    def perform_additional_checks(self):
         reference_values = [-0.4958006682716821, -1.1674087588549331, -0.2471256044520311, 0.12125502238309535]
         sensitivities_to_check = []
         element_list = [1,2,8]
@@ -238,23 +202,32 @@ class TestAdjointSensitivityAnalysisShellStructureStrainEnergy(AdjointSensitivit
 class TestAdjointSensitivityAnalysisSpringDamperElement(AdjointSensitivityAnalysisTestFactory):
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_spring_damper_element_3d2n/ProjectParameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_spring_damper_element_3d2n/AdjointParameters.json"
-    def test_execution(self):
-        self.solve_adjoint_problem()
 
 
 @KratosUnittest.skipUnless(has_hdf5_application,"Missing required application: HDF5Application")
 class TestAdjointSensitivityAnalysisLinearTrussStructure(AdjointSensitivityAnalysisTestFactory):
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_truss_stucture_3d2n/linear_truss_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_truss_stucture_3d2n/linear_truss_test_local_stress_adjoint_parameters.json"
-    def test_execution(self):
-        self.solve_adjoint_problem()
+
 
 @KratosUnittest.skipUnless(has_hdf5_application,"Missing required application: HDF5Application")
 class TestAdjointSensitivityAnalysisNonLinearTrussStructure(AdjointSensitivityAnalysisTestFactory):
     primal_file_name = "adjoint_sensitivity_analysis_tests/adjoint_truss_stucture_3d2n/nonlinear_truss_test_parameters.json"
     adjoint_file_name = "adjoint_sensitivity_analysis_tests/adjoint_truss_stucture_3d2n/nonlinear_truss_test_local_stress_adjoint_parameters.json"
-    def test_execution(self):
-        self.solve_adjoint_problem()
 
 if __name__ == '__main__':
-    KratosUnittest.main()
+    suites = KratosUnittest.KratosSuites
+    smallSuite = suites['small'] # These tests are executed by the continuous integration tool
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisBeamStructureLocalStress]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisBeamStructureNodalDisplacement]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisBeamStructureStrainEnergy]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisBeamStructureNodalReaction]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisShellStructureLocalStress]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisShellStructureNodalDisplacement]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisShellStructureStrainEnergy]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisSpringDamperElement]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisLinearTrussStructure]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestAdjointSensitivityAnalysisNonLinearTrussStructure]))
+    allSuite = suites['all']
+    allSuite.addTests(smallSuite)
+    KratosUnittest.runTests(suites)
