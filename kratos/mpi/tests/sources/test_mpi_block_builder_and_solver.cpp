5dddbcbda03dfc7f973aaadee104531c3b7f07b4
philbucher <philipp.bucher@tum.de>
2020-02-28 01:59:44 +0100

    adding test for MPIBlockBuilderAndSolver

---
+++ kratos/mpi/tests/sources/test_mpi_block_builder_and_solver.cpp
@@ -0,0 +1,22 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+//
+
+#include "mpi/solving_strategies/builder_and_solvers/mpi_block_builder_and_solver.h"
+
+#include "testing/testing.h"
+
+namespace Kratos {
+namespace Testing {
+
+}
+}
