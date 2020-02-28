16fbe344985562e2be835f00fa14dbd678e6445f
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2020-02-28 17:29:09 +0100

    RK4 substeps clean up

--- kratos/solving_strategies/strategies/explicit_solving_strategy_runge_kutta_4.h
+++ kratos/solving_strategies/strategies/explicit_solving_strategy_runge_kutta_4.h
@@ -180,7 +180,7 @@ protected:
         const auto& r_lumped_mass_vector = p_explicit_bs->GetLumpedMassMatrixVector();
 
         // Set the auxiliary RK vectors
-        LocalSystemVectorType u_n(dof_size);
+        LocalSystemVectorType u_n(dof_size); // TODO: THIS IS INEFICCIENT. CREATE A UNORDERED_SET WITH THE IDOF AND VALUE AS ENTRIES. THIS HAS TO BE OPTIONAL
         LocalSystemVectorType rk_k1(dof_size);
         LocalSystemVectorType rk_k2(dof_size);
         LocalSystemVectorType rk_k3(dof_size);
@@ -189,8 +189,6 @@ protected:
         // Perform the RK 4 update
         const double dt = BaseType::GetDeltaTime();
         KRATOS_ERROR_IF(dt < 1.0e-12) << "ProcessInfo DELTA_TIME is close to zero." << std::endl;
-        auto& r_model_part = BaseType::GetModelPart();
-        auto& r_process_info = r_model_part.GetProcessInfo();
 
         // Set the previous step solution in the current buffer position
         // Note that we only do this for the DOFs that are not fixed
@@ -206,79 +204,11 @@ protected:
             }
         }
 
-        // 1st RK step
-        r_process_info.SetValue(RUNGE_KUTTA_STEP, 1);
-        p_explicit_bs->BuildRHS(r_model_part);
-#pragma omp parallel for firstprivate(dof_size)
-        for (int i_dof = 0; i_dof < dof_size; ++i_dof) {
-            auto it_dof = r_dof_set.begin() + i_dof;
-            // Save current value in the corresponding vector
-            const double& r_res = it_dof->GetSolutionStepReactionValue();
-            rk_k1(i_dof) = r_res;
-            // Do the DOF update
-            double& r_u = it_dof->GetSolutionStepValue(0);
-            const double& r_u_old = it_dof->GetSolutionStepValue(1);
-            if (!it_dof->IsFixed()) {
-                const double mass = r_lumped_mass_vector(i_dof);
-                r_u = r_u_old + mA21 * (dt / mass) * r_res;
-            } else {
-                const double delta_u = u_n(i_dof) - r_u_old;
-                r_u = r_u_old + mA21 * delta_u;
-            }
-        }
-
-        // 2nd RK STEP
-        r_process_info.SetValue(RUNGE_KUTTA_STEP, 2);
-        p_explicit_bs->BuildRHS(r_model_part);
-#pragma omp parallel for firstprivate(dof_size)
-        for (int i_dof = 0; i_dof < dof_size; ++i_dof) {
-            auto it_dof = r_dof_set.begin() + i_dof;
-            // Save current value in the corresponding vector
-            const double& r_res = it_dof->GetSolutionStepReactionValue();
-            rk_k2(i_dof) = r_res;
-            // Do the DOF update
-            double& r_u = it_dof->GetSolutionStepValue(0);
-            const double& r_u_old = it_dof->GetSolutionStepValue(1);
-            if (!it_dof->IsFixed()) {
-                const double mass = r_lumped_mass_vector(i_dof);
-                r_u = r_u_old + mA32 * (dt / mass) * r_res;
-            } else {
-                const double delta_u = u_n(i_dof) - r_u_old;
-                r_u = r_u_old + mA32 * delta_u;
-            }
-        }
-
-        // 3rd RK STEP
-        r_process_info.SetValue(RUNGE_KUTTA_STEP, 3);
-        p_explicit_bs->BuildRHS(r_model_part);
-#pragma omp parallel for firstprivate(dof_size)
-        for (int i_dof = 0; i_dof < dof_size; ++i_dof) {
-            auto it_dof = r_dof_set.begin() + i_dof;
-            // Save current value in the corresponding vector
-            const double& r_res = it_dof->GetSolutionStepReactionValue();
-            rk_k3(i_dof) = r_res;
-            // Do the DOF update
-            double& r_u = it_dof->GetSolutionStepValue(0);
-            const double& r_u_old = it_dof->GetSolutionStepValue(1);
-            if (!it_dof->IsFixed()) {
-                const double mass = r_lumped_mass_vector(i_dof);
-                r_u = r_u_old + mA43 * (dt / mass) * r_res;
-            } else {
-                const double delta_u = u_n(i_dof) - r_u_old;
-                r_u = r_u_old + mA43 * delta_u;
-            }
-        }
-
-        // 4rd RK STEP
-        r_process_info.SetValue(RUNGE_KUTTA_STEP, 4);
-        p_explicit_bs->BuildRHS(r_model_part);
-#pragma omp parallel for firstprivate(dof_size)
-        for (int i_dof = 0; i_dof < dof_size; ++i_dof) {
-            const auto it_dof = r_dof_set.begin() + i_dof;
-            // Save current value in the corresponding vector
-            const double& r_res = it_dof->GetSolutionStepReactionValue();
-            rk_k4(i_dof) = r_res;
-        }
+        // Calculate the RK4 intermediate sub steps
+        PerformRungeKuttaIntermediateSubStep(1, mA21, u_n, rk_k1);
+        PerformRungeKuttaIntermediateSubStep(2, mA32, u_n, rk_k2);
+        PerformRungeKuttaIntermediateSubStep(3, mA43, u_n, rk_k3);
+        PerformRungeKuttaLastSubStep(rk_k4);
 
         // Do the final solution update
 #pragma omp parallel for firstprivate(dof_size)
@@ -349,6 +279,94 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
+
+    /**
+     * @brief Performs an intermediate RK4 step
+     * This functions performs all the operations required in an intermediate RK4 sub step
+     * @param SubStepIndex The sub step index
+     * @param SubStepCoefficient The sub step coefficient (these are saved as member variables)
+     * @param rFixedDofsValues The vector containing the step n+1 values of the fixed DOFs
+     * @param rIntermediateStepResidualVector The vector to store the intermediate sub step residual
+     */
+    void PerformRungeKuttaIntermediateSubStep(
+        const IndexType SubStepIndex,
+        const double SubStepCoefficient,
+        const LocalSystemVectorType& rFixedDofsValues,
+        LocalSystemVectorType& rIntermediateStepResidualVector)
+    {
+        // Get the required data from the explicit builder and solver
+        const auto p_explicit_bs = BaseType::pGetExplicitBuilderAndSolver();
+        auto& r_dof_set = p_explicit_bs->GetDofSet();
+        const unsigned int dof_size = p_explicit_bs->GetEquationSystemSize();
+        const auto& r_lumped_mass_vector = p_explicit_bs->GetLumpedMassMatrixVector();
+
+        // Get model part and information
+        const double dt = BaseType::GetDeltaTime();
+        KRATOS_ERROR_IF(dt < 1.0e-12) << "ProcessInfo DELTA_TIME is close to zero." << std::endl;
+        auto& r_model_part = BaseType::GetModelPart();
+        auto& r_process_info = r_model_part.GetProcessInfo();
+
+        // Set the RUNGE_KUTTA_STEP value. This has to be done prior to the InitializeRungeKuttaStep()
+        r_process_info.GetValue(RUNGE_KUTTA_STEP) = SubStepIndex;
+
+        // Perform the intermidate sub step update
+        InitializeRungeKuttaSubStep();
+        p_explicit_bs->BuildRHS(r_model_part);
+
+#pragma omp parallel for firstprivate(dof_size)
+        for (int i_dof = 0; i_dof < dof_size; ++i_dof) {
+            auto it_dof = r_dof_set.begin() + i_dof;
+            // Save current value in the corresponding vector
+            const double& r_res = it_dof->GetSolutionStepReactionValue();
+            rIntermediateStepResidualVector(i_dof) = r_res;
+            // Do the DOF update
+            double& r_u = it_dof->GetSolutionStepValue(0);
+            const double& r_u_old = it_dof->GetSolutionStepValue(1);
+            if (!it_dof->IsFixed()) {
+                const double mass = r_lumped_mass_vector(i_dof);
+                r_u = r_u_old + SubStepCoefficient * (dt / mass) * r_res;
+            } else {
+                const double delta_u = rFixedDofsValues(i_dof) - r_u_old;
+                r_u = r_u_old + SubStepCoefficient * delta_u;
+            }
+        }
+
+        FinalizeRungeKuttaSubStep();
+    }
+
+    /**
+     * @brief Performs the last RK4 step
+     * This functions performs all the operations required in the last RK4 sub step
+     * @param rLastStepResidualVector The vector to store the last sub step residual
+     */
+    void PerformRungeKuttaLastSubStep(LocalSystemVectorType& rLastStepResidualVector)
+    {
+        // Get the required data from the explicit builder and solver
+        const auto p_explicit_bs = BaseType::pGetExplicitBuilderAndSolver();
+        auto& r_dof_set = p_explicit_bs->GetDofSet();
+        const unsigned int dof_size = p_explicit_bs->GetEquationSystemSize();
+
+        // Get model part
+        auto& r_model_part = BaseType::GetModelPart();
+        auto& r_process_info = r_model_part.GetProcessInfo();
+
+        // Set the RUNGE_KUTTA_STEP value. This has to be done prior to the InitializeRungeKuttaStep()
+        r_process_info.GetValue(RUNGE_KUTTA_STEP) = 4;
+
+        // Perform the last sub step residual calculation
+        InitializeRungeKuttaSubStep();
+        p_explicit_bs->BuildRHS(r_model_part);
+
+#pragma omp parallel for firstprivate(dof_size)
+        for (int i_dof = 0; i_dof < dof_size; ++i_dof) {
+            const auto it_dof = r_dof_set.begin() + i_dof;
+            // Save current value in the corresponding vector
+            const double& r_res = it_dof->GetSolutionStepReactionValue();
+            rLastStepResidualVector(i_dof) = r_res;
+        }
+
+        FinalizeRungeKuttaSubStep();
+    }
 
 
     ///@}
