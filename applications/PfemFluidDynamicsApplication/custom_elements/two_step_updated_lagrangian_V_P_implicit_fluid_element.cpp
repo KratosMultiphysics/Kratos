e037aa0cf3052067366cb6949bc80b1fe2b4e2ca
Massimiliano Zecchetto <mzecchetto@cimne.upc.edu>
2020-03-01 17:29:28 +0100

    cleanup - behaviour not changed (III)

--- applications/PfemFluidDynamicsApplication/custom_elements/two_step_updated_lagrangian_V_P_implicit_fluid_element.cpp
+++ applications/PfemFluidDynamicsApplication/custom_elements/two_step_updated_lagrangian_V_P_implicit_fluid_element.cpp
@@ -83,17 +83,6 @@ void TwoStepUpdatedLagrangianVPImplicitFluidElement<TDim>::ComputeMaterialParame
   else if (staticFrictionCoefficient != 0)
   {
     DeviatoricCoeff = this->ComputePapanastasiouMuIrheologyViscosity(rElementalVariables);
-    // if(regularizationCoefficient!=0 && inertialNumberThreshold==0){
-    // 	// DeviatoricCoeff=this->ComputeBercovierMuIrheologyViscosity(rElementalVariables);
-    // 	DeviatoricCoeff=this->ComputePapanastasiouMuIrheologyViscosity(rElementalVariables);
-    // }
-    // else if(regularizationCoefficient==0 && inertialNumberThreshold!=0){
-    // 	DeviatoricCoeff=this->ComputeBarkerMuIrheologyViscosity(rElementalVariables);
-    // }else if(regularizationCoefficient!=0 && inertialNumberThreshold!=0){
-    // 	DeviatoricCoeff=this->ComputeBarkerBercovierMuIrheologyViscosity(rElementalVariables);
-    // }else{
-    // 	DeviatoricCoeff=this->ComputeJopMuIrheologyViscosity(rElementalVariables);
-    // }
   }
   else
   {
@@ -101,23 +90,10 @@ void TwoStepUpdatedLagrangianVPImplicitFluidElement<TDim>::ComputeMaterialParame
     DeviatoricCoeff = this->GetProperties()[DYNAMIC_VISCOSITY];
   }
 
-  // this->ComputeMaterialParametersGranularGas(rElementalVariables,VolumetricCoeff,DeviatoricCoeff);
-  // std::cout<<"Density "<<Density<<std::endl;
-  // std::cout<<"FluidBulkModulus "<<FluidBulkModulus<<std::endl;
-  // std::cout<<"staticFrictionCoefficient "<<staticFrictionCoefficient<<std::endl;
-  // std::cout<<"DeviatoricCoeff "<<DeviatoricCoeff<<std::endl;
-
   this->mMaterialDeviatoricCoefficient = DeviatoricCoeff;
   this->mMaterialVolumetricCoefficient = VolumetricCoeff;
   this->mMaterialDensity = Density;
 
-  // const SizeType NumNodes = this->GetGeometry().PointsNumber();
-  // for (SizeType i = 0; i < NumNodes; ++i)
-  //   {
-  // 	this->GetGeometry()[i].FastGetSolutionStepValue(ADAPTIVE_EXPONENT)=VolumetricCoeff;
-  // 	this->GetGeometry()[i].FastGetSolutionStepValue(ALPHA_PARAMETER)=DeviatoricCoeff;
-  // 	this->GetGeometry()[i].FastGetSolutionStepValue(FLOW_INDEX)=rElementalVariables.EquivalentStrainRate;
-  //   }
 }
 
 template <unsigned int TDim>
