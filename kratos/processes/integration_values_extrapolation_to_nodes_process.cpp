05c5421918dee27525efcf200e40c815389f8988
sunethwarna <suneth.warna@gmail.com>
2020-03-03 14:48:46 +0100

    initial commit

--- kratos/processes/integration_values_extrapolation_to_nodes_process.cpp
+++ kratos/processes/integration_values_extrapolation_to_nodes_process.cpp
@@ -217,6 +217,47 @@ void IntegrationValuesExtrapolationToNodesProcess::ExecuteFinalizeSolutionStep()
             }
         }
     }
+
+    // Assemble nodal data
+    if (mExtrapolateNonHistorical)
+    {
+        for (const auto p_var : mDoubleVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleCurrentData(*p_var);
+        }
+        for (const auto p_var : mArrayVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleCurrentData(*p_var);
+        }
+        for (const auto p_var : mVectorVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleCurrentData(*p_var);
+        }
+        for (const auto p_var : mMatrixVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleCurrentData(*p_var);
+        }
+    }
+    else
+    {
+        for (const auto p_var : mDoubleVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleNonHistoricalData(*p_var);
+        }
+        for (const auto p_var : mArrayVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleNonHistoricalData(*p_var);
+        }
+        for (const auto p_var : mVectorVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleNonHistoricalData(*p_var);
+        }
+        for (const auto p_var : mMatrixVariable)
+        {
+            mrModelPart.GetCommunicator().AssembleNonHistoricalData(*p_var);
+        }
+    }
+
 }
 
 /***********************************************************************************/
@@ -301,6 +342,8 @@ void IntegrationValuesExtrapolationToNodesProcess::InitializeMaps()
             }
         }
     }
+
+    mrModelPart.GetCommunicator().AssembleNonHistoricalData(*mpAverageVariable);
 
     // The process info
     const ProcessInfo& r_process_info = mrModelPart.GetProcessInfo();
