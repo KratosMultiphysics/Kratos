92d6758419c7de13659bf7b9e52857cb93eb585b
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2020-03-04 12:56:18 +0100

    removing add nodal variable in the DEM

--- applications/FemToDemApplication/python_scripts/MainDEM_for_coupling.py
+++ applications/FemToDemApplication/python_scripts/MainDEM_for_coupling.py
@@ -2,6 +2,22 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 
 import KratosMultiphysics.DEMApplication
 import KratosMultiphysics.DEMApplication.main_script as MainDEM
+import KratosMultiphysics.FemToDemApplication as FEMDEM
+
+if IsDistributedRun():
+    if "DO_NOT_PARTITION_DOMAIN" in os.environ:
+        Logger.PrintInfo("DEM", "Running under MPI........")
+        from KratosMultiphysics.mpi import *
+        import KratosMultiphysics.DEMApplication.DEM_procedures_mpi_no_partitions as DEM_procedures
+    else:
+        Logger.PrintInfo("DEM", "Running under OpenMP........")
+        from KratosMultiphysics.MetisApplication import *
+        from KratosMultiphysics.MPISearchApplication import *
+        from KratosMultiphysics.mpi import *
+        import KratosMultiphysics.DEMApplication.DEM_procedures_mpi as DEM_procedures
+else:
+    Logger.PrintInfo("DEM", "Running under OpenMP........")
+    import KratosMultiphysics.FemToDemApplication.DEM_procedures_for_coupling as DEM_procedures
 
 class DEM_for_coupling_Solution(MainDEM.Solution):
 
