2a1c288d479e9edd4fb09d1468f8dc347408ecf0
ga69cal <altug.emiroglu@tum.de>
2020-03-05 14:54:13 +0100

    fixing search for active dofs

--- applications/StructuralMechanicsApplication/custom_strategies/custom_strategies/eigensolver_strategy.hpp
+++ applications/StructuralMechanicsApplication/custom_strategies/custom_strategies/eigensolver_strategy.hpp
@@ -700,7 +700,7 @@ private:
                 for (std::size_t j = 0; j < NumNodeDofs; j++)
                 {
                     const auto itDof = std::begin(NodeDofs) + j;
-                    bool is_active = !(*(r_dof_set.find(**itDof)) == *(r_dof_set.end()));
+                    bool is_active = !(r_dof_set.find(**itDof) == r_dof_set.end());
                     if ((*itDof)->IsFree() && is_active) {
                        rNodeEigenvectors(i,j) = rEigenvectors(i,(*itDof)->EquationId());
                     }
