2054223851dfc2374b76688724c464fe42f0f53a
arming <armin.geiser@tum.de>
2020-03-06 16:56:57 +0100

    small simplifacation progress

--- applications/ShapeOptimizationApplication/python_scripts/mapping/progress_bar.py
+++ applications/ShapeOptimizationApplication/python_scripts/mapping/progress_bar.py
@@ -1,5 +1,5 @@
 # Print iterations progress https://stackoverflow.com/a/34325723
-def printProgressBar (iteration, total, prefix = 'Progress', suffix = 'Complete', decimals = 1, length = 50, fill = '█', printEnd = "\r"):
+def printProgressBar (iteration, total, prefix = 'Progress', suffix = 'Complete', decimals = 1, length = 50, fill = '█', ):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -10,12 +10,11 @@ def printProgressBar (iteration, total, prefix = 'Progress', suffix = 'Complete'
         decimals    - Optional  : positive number of decimals in percent complete (Int)
         length      - Optional  : character length of bar (Int)
         fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = "")
     # Print New Line on Complete
     if iteration == total:
         print()
