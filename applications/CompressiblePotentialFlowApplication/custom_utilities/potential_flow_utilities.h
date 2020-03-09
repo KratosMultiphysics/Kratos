c05ae5fca84c50d29245ebe4d73fdbb86355fb60
Inigo Lopez <inigolcanalejo@gmail.com>
2020-03-09 18:38:20 +0100

    add ComputeDrhoDphiUpSupersonicAccelerating

--- applications/CompressiblePotentialFlowApplication/custom_utilities/potential_flow_utilities.h
+++ applications/CompressiblePotentialFlowApplication/custom_utilities/potential_flow_utilities.h
@@ -126,6 +126,9 @@ template <int Dim, int NumNodes>
 BoundedVector<double, NumNodes> ComputeDrhoDphiSupersonicAccelerating(const Element& rElement, const Element& rUpstreamElement, const ProcessInfo& rCurrentProcessInfo);
 
 template <int Dim, int NumNodes>
+BoundedVector<double, NumNodes> ComputeDrhoDphiUpSupersonicAccelerating(const Element& rElement, const Element& rUpstreamElement, const ProcessInfo& rCurrentProcessInfo);
+
+template <int Dim, int NumNodes>
 bool CheckIfElementIsCutByDistance(const BoundedVector<double, NumNodes>& rNodalDistances);
 
 bool CheckIfElementIsTrailingEdge(const Element& rElement);
