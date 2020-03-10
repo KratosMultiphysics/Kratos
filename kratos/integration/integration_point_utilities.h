d23ca4cf6de1c2612b69f4e6b3adbc3ab3291ef8
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-03-10 15:59:58 +1000

    Add IntegrationPointUtilities

---
+++ kratos/integration/integration_point_utilities.h
@@ -0,0 +1,65 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+
+
+#if !defined(KRATOS_INTEGRATION_POINT_UTILITIES_INCLUDED )
+#define  KRATOS_INTEGRATION_POINT_UTILITIES_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "integration/integration_point.h"
+#include "geometries/geometry.h"
+
+namespace Kratos
+{
+class KRATOS_API(KRATOS_CORE) IntegrationPointUtilities
+{
+
+public:
+    ///@name private static members
+    ///@{
+
+    static const std::vector<std::vector<std::vector<double>>> s_gauss_legendre;
+
+    ///@}
+public:
+
+    ///@name Type Definitions
+    ///@{
+
+    typedef std::size_t SizeType;
+    typedef std::size_t IndexType;
+
+    typedef IntegrationPoint<3> IntegrationPointType;
+    typedef std::vector<IntegrationPointType> IntegrationPointsArrayType;
+
+    ///@}
+    ///@name Define Integration Points
+    ///@{
+
+    static void IntegrationPoints1D(
+        IntegrationPointsArrayType& rIntegrationPoints,
+        SizeType PointsInU,
+        double U0, double U1);
+
+    static void IntegrationPoints2D(
+        IntegrationPointsArrayType& rIntegrationPoints,
+        SizeType PointsInU, SizeType PointsInV,
+        double U0, double U1, double V0, double V1);
+
+    ///@}
+};
+
+}
+#endif // KRATOS_INTEGRATION_POINT_UTILITIES_INCLUDED  defined
