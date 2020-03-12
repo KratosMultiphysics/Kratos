6f2308313252c19a0966d676b0f4db16fa1cc587
Toon Demeester <toon.demeester@ugent.be>
2020-03-12 17:13:28 +0100

    Finish first version of axisymmetric_3d_to_2d mapper

--- applications/CoSimulationApplication/python_scripts/mappers/axisymmetric_3d_to_2d.py
+++ applications/CoSimulationApplication/python_scripts/mappers/axisymmetric_3d_to_2d.py
@@ -7,11 +7,11 @@ cs_data_structure = cs_tools.cs_data_structure
 
 
 def Create(parameters):
-    return MapperAxisymmetric2DTo3D(parameters)
+    return MapperAxisymmetric3DTo2D(parameters)
 
 
-# Class MapperAxisymmetric2DTo3D: map 2D axisymmetric to 3D.
-class MapperAxisymmetric2DTo3D(CoSimulationComponent):
+# Class MapperAxisymmetric3DTo2D: map 3D to 2D axisymmetric.
+class MapperAxisymmetric3DTo2D(CoSimulationComponent):
     def __init__(self, parameters):
         """
         TODO
@@ -38,42 +38,42 @@ class MapperAxisymmetric2DTo3D(CoSimulationComponent):
     def Initialize(self, model_part_in, forward):
         super().Initialize()
 
-        if forward:
-            self.n_from = model_part_in.NumberOfNodes()
+        if not forward:
+            self.n_to = model_part_in.NumberOfNodes()
 
-            n_t = self.settings['n_tangential'].GetInt()  # int parameter
-            if n_t < 6:  # hardcode minimum value?
+            self.n_t = self.settings['n_tangential'].GetInt()  # int parameter
+            if self.n_t < 6:  # hardcode minimum value?
                 raise ValueError('minimum value for n_tangential is 6')
-            self.n_to = n_t * self.n_from
+            self.n_from = self.n_t * self.n_to
 
             model = cs_data_structure.Model()
             model_part_out = model.CreateModelPart('no_name')
             model_part_out._ModelPart__hist_variables = model_part_in._ModelPart__hist_variables
 
-            self.nearest = np.zeros((self.n_to, 1)).astype(int)
-            self.theta = np.zeros((self.n_to, 1))
+            self.nearest = np.zeros((self.n_to, self.n_t)).astype(int)
+            self.coeffs = np.ones((self.n_to, self.n_t)) / self.n_t
+            self.theta = np.zeros((self.n_to, self.n_t))
 
             i_from = 0
             i_to = 0
             for node in model_part_in.Nodes:
                 coords = np.array([node.X0, node.Y0, node.Z0])
                 r = coords[self.dir_r]
-                for i in range(n_t):
-                    self.nearest[i_to, 0] = i_from
-                    self.theta[i_to] = i * 2 * np.pi / n_t
+                for i in range(self.n_t):
+                    self.nearest[i_to, i] = i_from
+                    self.theta[i_to, i] = i * 2 * np.pi / self.n_t
 
-                    coords[self.dir_r] = r * np.cos(self.theta[i_to])
-                    coords[self.dir_3d] = r * np.sin(self.theta[i_to])
+                    coords[self.dir_r] = r * np.cos(self.theta[i_to, i])
+                    coords[self.dir_3d] = r * np.sin(self.theta[i_to, i])
 
-                    model_part_out.CreateNewNode(i_to, *tuple(coords))
-                    i_to += 1
-                i_from += 1
+                    model_part_out.CreateNewNode(i_from, *tuple(coords))
+                    i_from += 1
+                i_to += 1
 
             return model_part_out
 
         else:
-            raise NotImplementedError('Backward Initialization not implemented for MapperAxisymmetric2DTo3D.')
-
+            raise NotImplementedError('Forward Initialization not implemented for MapperAxisymmetric3DTo2D.')
 
     def __call__(self, args_from, args_to):
         model_part_from, var_from = args_from
@@ -90,7 +90,8 @@ class MapperAxisymmetric2DTo3D(CoSimulationComponent):
                 hist_var_from[i_from] = node_from.GetSolutionStepValue(var_from)
 
             for i_to, node_to in enumerate(model_part_to.Nodes):
-                node_to.SetSolutionStepValue(var_to, 0, hist_var_from[self.nearest[i_to, 0]])
+                hist_var_to = np.dot(self.coeffs[i_to], hist_var_from[self.nearest[i_to]])
+                node_to.SetSolutionStepValue(var_to, 0, hist_var_to)
 
         # vector interpolation
         elif var_from.Type() == 'Array':
@@ -98,13 +99,37 @@ class MapperAxisymmetric2DTo3D(CoSimulationComponent):
             for i_from, node_from in enumerate(model_part_from.Nodes):
                 hist_var_from[i_from] = node_from.GetSolutionStepValue(var_from)
 
+            # *** old method: works
+            # for i_to, node_to in enumerate(model_part_to.Nodes):
+            #     v_a = 0.
+            #     v_r = 0.
+            #     for j in range(self.n_t):
+            #         tmp = hist_var_from[self.nearest[i_to, j]]
+            #         v_a += tmp[self.dir_a]
+            #         theta = self.theta[i_to, j]
+            #         v_r += tmp[self.dir_r] * np.cos(-theta) + tmp[self.dir_3d] * np.cos(np.pi / 2 - theta)
+            #
+            #     v_a /= self.n_t
+            #     v_r /= self.n_t
+            #     # v_r = 0.
+            #
+            #     hist_var_to = [0., 0., 0.]
+            #     hist_var_to[self.dir_a] = v_a
+            #     hist_var_to[self.dir_r] = v_r
+            #     hist_var_to[self.dir_3d] = 0.
+            #
+            #     # print(hist_var_to)
+            #
+            #     node_to.SetSolutionStepValue(var_to, 0, hist_var_to)
+
+            # *** new method: works too!
             for i_to, node_to in enumerate(model_part_to.Nodes):
-                hist_var_to = hist_var_from[self.nearest[i_to, 0]].copy()
-
-                tmp = hist_var_to[self.dir_r]
-                hist_var_to[self.dir_r] = tmp * np.cos(self.theta[i_to])
-                hist_var_to[self.dir_3d] = tmp * np.sin(self.theta[i_to])
-
+                hist_var_to = [0., 0., 0.]
+                tmp = hist_var_from[self.nearest[i_to]]
+                hist_var_to[self.dir_a] = np.dot(self.coeffs[i_to], tmp[:, self.dir_a])
+                hist_var_to[self.dir_r] = np.dot(self.coeffs[i_to],
+                                                 tmp[:, self.dir_r] * np.cos(-self.theta[i_to]) +
+                                                 tmp[:, self.dir_3d] * np.cos(np.pi / 2 - self.theta[i_to]))
                 node_to.SetSolutionStepValue(var_to, 0, hist_var_to)
 
         # other types of Variables
--- applications/CoSimulationApplication/tests/mappers/test_axisymmetric_3d_to_2d.py
+++ applications/CoSimulationApplication/tests/mappers/test_axisymmetric_3d_to_2d.py
@@ -10,9 +10,9 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 
-class TestMapperAxisymmetric2DTo3D(KratosUnittest.TestCase):
-    def test_mapper_axisymmetric_2d_to_3d(self):
-        parameter_file_name = os.path.join(os.path.dirname(__file__), 'test_axisymmetric_2d_to_3d.json')
+class TestMapperAxisymmetric3DTo2D(KratosUnittest.TestCase):
+    def test_mapper_axisymmetric_3d_to_2d(self):
+        parameter_file_name = os.path.join(os.path.dirname(__file__), 'test_axisymmetric_3d_to_2d.json')
         cs_data_structure = ImportDataStructure(parameter_file_name)
         with open(parameter_file_name, 'r') as parameter_file:
             parameters = cs_data_structure.Parameters(parameter_file.read())
@@ -22,39 +22,54 @@ class TestMapperAxisymmetric2DTo3D(KratosUnittest.TestCase):
             var_s = vars(KM)["TEMPERATURE"]
             var_v = vars(KM)["VELOCITY"]
             model = cs_data_structure.Model()
-            model_part_from = model.CreateModelPart('wall_from')
-            model_part_from.AddNodalSolutionStepVariable(var_s)
-            model_part_from.AddNodalSolutionStepVariable(var_v)
+            model_part_to = model.CreateModelPart('wall_from')
+            model_part_to.AddNodalSolutionStepVariable(var_s)
+            model_part_to.AddNodalSolutionStepVariable(var_v)
 
-            n_from = 10
-            x_from = np.linspace(0, 2 * np.pi, n_from)
-            y_from = 1. + 0.2 * np.sin(x_from)
-            z_from = np.zeros(n_from)
-            v_s_from = x_from * y_from / 10
-            v_v_from = np.zeros((n_from, 3))
-            v_v_from[:, 0] = v_s_from * 0.1
-            v_v_from[:, 0] = v_s_from * 0.5
-            for i in range(n_from):
-                node = model_part_from.CreateNewNode(i, x_from[i], y_from[i], z_from[i])
-                node.SetSolutionStepValue(var_s, 0, v_s_from[i])
-                node.SetSolutionStepValue(var_v, 0, tuple(v_v_from[i, :]))
-
-            # model_part_from given (forward initialization)
-            mapper = cs_tools.CreateInstance(parameters['mapper'])
-            model_part_to = mapper.Initialize(model_part_from, forward=True)
-            mapper((model_part_from, var_s), (model_part_to, var_s))
-
-            n_to = model_part_to.NumberOfNodes()
-            x_to = np.zeros(n_to)
-            y_to = np.zeros(n_to)
+            n_to = 10
+            x_to = np.linspace(0, 1, n_to)
+            y_to = 1. + 0.2 * np.sin(x_to / 2 / np.pi)
             z_to = np.zeros(n_to)
+            for i in range(n_to):
+                node = model_part_to.CreateNewNode(i, x_to[i], y_to[i], z_to[i])
+
+            # model_part_to given (backward initialization)
+            mapper = cs_tools.CreateInstance(parameters['mapper'])
+            model_part_from = mapper.Initialize(model_part_to, forward=False)
+
+
+
+            n_from = model_part_from.NumberOfNodes()
+            print(n_from, n_to)
+            x_from = np.zeros(n_from)
+            y_from = np.zeros(n_from)
+            z_from = np.zeros(n_from)
+            v_s_from = np.zeros(n_from)
+            v_v_from = np.zeros((n_from, 3))
+            for i, node in enumerate(model_part_from.Nodes):
+                x_from[i], y_from[i], z_from[i] = node.X0, node.Y0, node.Z0
+                v_s_from[i] = x_from[i]
+                node.SetSolutionStepValue(var_s, 0, v_s_from[i])
+
+                theta = np.arctan2(z_from[i], y_from[i])
+
+                v_v_from[i] = np.array([x_from[i] * .5, x_from[i] * np.cos(theta), x_from[i] * np.sin(theta)])
+                node.SetSolutionStepValue(var_v, 0, tuple(v_v_from[i]))
+
+            print(v_v_from)
+
+            mapper((model_part_from, var_s), (model_part_to, var_s))
+            mapper((model_part_from, var_v), (model_part_to, var_v))
+
+
             v_s_to = np.zeros(n_to)
             v_v_to = np.zeros((n_to, 3))
-
             for i, node in enumerate(model_part_to.Nodes):
                 x_to[i], y_to[i], z_to[i] = node.X0, node.Y0, node.Z0
                 v_s_to[i] = node.GetSolutionStepValue(var_s)
                 v_v_to[i, :] = np.array(node.GetSolutionStepValue(var_v))
+            print(v_v_to)
+
 
             # plot results
             c_from = cm.jet((v_s_from - v_s_from.min()) / (v_s_from.max() - v_s_from.min()))
@@ -63,17 +78,21 @@ class TestMapperAxisymmetric2DTo3D(KratosUnittest.TestCase):
             fig = plt.figure()
 
             ax_s = fig.add_subplot(121, projection='3d')
-            ax_s.scatter(x_from, y_from, z_from, s=50, c=c_from, depthshade=True, marker='s')
-            ax_s.scatter(x_to, y_to, z_to, s=20, c=c_to, depthshade=True)
+            ax_s.scatter(x_from, y_from, z_from, s=20, c=c_from, depthshade=True)
+            ax_s.scatter(x_to, y_to, z_to, s=50, c=c_to, depthshade=True, marker='s')
 
             ax_v = fig.add_subplot(122, projection='3d')
-            ax_v.scatter(x_from, y_from, z_from, s=50, c=c_from, depthshade=True, marker='s')
-            ax_v.scatter(x_to, y_to, z_to, s=20, c=c_to, depthshade=True)
+            # ax_v.quiver(x_from, y_from, z_from, v_v_from[:, 0], v_v_from[:, 1], v_v_from[:, 2],
+            #             pivot='tail', arrow_length_ratio=0.1, normalize=False, length=0.2)
+            # ax_v.quiver(x_to, y_to, z_to, v_v_to[:, 0], v_v_to[:, 1], v_v_to[:, 2],
+            #             pivot='tail', arrow_length_ratio=0.2, normalize=False, length=0.05, colors='r', linewidth=3)
 
-            ax_v.quiver(x_from, y_from, z_from, v_v_from[:, 0], v_v_from[:, 1], v_v_from[:, 2],
+            print(x_from.shape, v_v_from.shape)
+
+            ax_v.quiver(x_from, y_from, z_from, *[v.flatten() for v in np.hsplit(v_v_from, 3)],
                         pivot='tail', arrow_length_ratio=0.1, normalize=False, length=0.2)
-            ax_v.quiver(x_to, y_to, z_to, v_v_to[:, 0], v_v_to[:, 1], v_v_to[:, 2],
-                        pivot='tail', arrow_length_ratio=0.1, normalize=False, length=0.2)
+            ax_v.quiver(x_to, y_to, z_to, *[v.flatten() for v in np.hsplit(v_v_to, 3)],
+                        pivot='tail', arrow_length_ratio=0.3, normalize=False, length=0.2, colors='r', linewidth=2)
 
             for ax in [ax_s, ax_v]:
                 ax.set_xlabel('x')
