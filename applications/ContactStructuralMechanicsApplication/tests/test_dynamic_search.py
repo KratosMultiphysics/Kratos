40bf212e35416cf91939636c1d25e79a21e543ab
Riccardo Rossi <rrossi@cimne.upc.edu>
2020-03-16 00:04:53 +0100

    minor modification to import the StructuralMechanicsApplication, needed to allow running the tests as standalone

--- applications/ContactStructuralMechanicsApplication/tests/test_dynamic_search.py
+++ applications/ContactStructuralMechanicsApplication/tests/test_dynamic_search.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, absolute_import, division  # makes KM backward compatible with python 2.6 and 2.7
 
 import KratosMultiphysics as KM
+import KratosMultiphysics.StructuralMechanicsApplication
 import KratosMultiphysics.ContactStructuralMechanicsApplication as CSMA
 
 # Some imports
