5e0e7e549c0793b39be17b28fb623d6a7598407a
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-03-16 17:39:47 +1000

    change to assignment operator

--- applications/ParticleMechanicsApplication/tests/cpp_tests/test_energy_calculation_utility.cpp
+++ applications/ParticleMechanicsApplication/tests/cpp_tests/test_energy_calculation_utility.cpp
@@ -75,9 +75,9 @@ namespace Testing
         mp_strain[3] = 0.4;
         mp_strain[4] = 0.5;
         mp_strain[5] = 0.6;
-        pElement->SetValueOnIntegrationPoints(MP_VOLUME, std::vector<double>{ 2.5 }, rModelPart.GetProcessInfo());
-        pElement->SetValueOnIntegrationPoints(MP_CAUCHY_STRESS_VECTOR, std::vector<Vector>{mp_cauchy_stress}, rModelPart.GetProcessInfo());
-        pElement->SetValueOnIntegrationPoints(MP_ALMANSI_STRAIN_VECTOR, std::vector<Vector>{ mp_strain }, rModelPart.GetProcessInfo());
+        pElement->SetValueOnIntegrationPoints(MP_VOLUME, std::vector<double>({ 2.5 }), rModelPart.GetProcessInfo());
+        pElement->SetValueOnIntegrationPoints(MP_CAUCHY_STRESS_VECTOR, std::vector<Vector>({mp_cauchy_stress}), rModelPart.GetProcessInfo());
+        pElement->SetValueOnIntegrationPoints(MP_ALMANSI_STRAIN_VECTOR, std::vector<Vector>({ mp_strain }), rModelPart.GetProcessInfo());
     }
 
     /**
