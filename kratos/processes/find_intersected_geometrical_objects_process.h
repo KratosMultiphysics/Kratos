0327daa255ffe9a23f964d3f592aa1515bc47fc0
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-03-16 13:58:32 +0100

    Adding const find_intersected_geometrical_objects_process

--- kratos/processes/find_intersected_geometrical_objects_process.h
+++ kratos/processes/find_intersected_geometrical_objects_process.h
@@ -471,7 +471,7 @@ public:
     /**
      * @brief This method provides the defaults parameters to avoid conflicts between the different constructors
      */
-    Parameters GetDefaultParameters() override;
+    const Parameters GetDefaultParameters() const override;
 
     ///@}
     ///@name Input and output
