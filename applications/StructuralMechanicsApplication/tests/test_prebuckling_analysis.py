d76ca225c1dcc4957ccf16b3bb5c00b368d6b2c0
Manuel Messmer <manuel.messmer@tum.de>
2020-03-16 11:14:02 +0100

    Tolerance changed again.

--- applications/StructuralMechanicsApplication/tests/test_prebuckling_analysis.py
+++ applications/StructuralMechanicsApplication/tests/test_prebuckling_analysis.py
@@ -90,7 +90,7 @@ class BaseTestPrebucklingAnalysis(KratosUnittest.TestCase):
         eigensolver_settings = KratosMultiphysics.Parameters("""
         {
             "max_iteration"         : 10000,
-            "tolerance"             : 1e-20,
+            "tolerance"             : 1e-10,
             "number_of_eigenvalues" : 2,
             "echo_level"            : 0,
             "normalize_eigenvectors": false
