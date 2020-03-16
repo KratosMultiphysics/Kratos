823601a22f60001a4de7cf89c096c7bf787c13a2
Raul <rbravo555@gmail.com>
2020-03-16 16:08:43 +0100

    finding out types

--- applications/RomApplication/custom_strategies/rom_builder_and_solver.h
+++ applications/RomApplication/custom_strategies/rom_builder_and_solver.h
@@ -410,7 +410,9 @@ public:
         // else{
         //     // Only selected conditions are considered for the calculation on an H-ROM simualtion.
             const auto cond_begin = mSelectedConditions.begin();
+            std::sting WantToKnowYourType = cond_begin;
             const auto nconditions = mSelectedConditions.size();        
+            std::sting WantToKnowYourTypeToo = nconditions;            
         //}
 
         //contributions to the system
