91b38161c24eca66663da2f86cdbb202d1621516
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-03-16 13:59:29 +0100

    Adding const ComputeHessianSolMetricProcess

--- applications/MeshingApplication/custom_processes/metrics_hessian_process.h
+++ applications/MeshingApplication/custom_processes/metrics_hessian_process.h
@@ -192,7 +192,7 @@ public:
     /**
      * @brief This method provides the defaults parameters to avoid conflicts between the different constructors
      */
-    Parameters GetDefaultParameters() override;
+    const Parameters GetDefaultParameters() const override;
 
     ///@}
     ///@name Access
