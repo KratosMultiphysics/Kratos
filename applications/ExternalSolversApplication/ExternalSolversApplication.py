d6f9505d0b6959ca292d45d563db14373778a606
Philipp Bucher <philipp.bucher@tum.de>
2020-03-17 12:21:01 +0100

    adding descriptions and deprecation warnings

--- applications/ExternalSolversApplication/ExternalSolversApplication.py
+++ applications/ExternalSolversApplication/ExternalSolversApplication.py
@@ -1,8 +1,11 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import KratosMultiphysics as KM
+from KratosMultiphysics.kratos_utilities import IssueDeprecationWarning
 from KratosExternalSolversApplication import *
 application = KratosExternalSolversApplication()
 application_name = "KratosExternalSolversApplication"
 application_folder = "ExternalSolversApplication"
 
 KM._ImportApplicationAsModule(application, application_name, application_folder, __path__)
+
+IssueDeprecationWarning("", "The ExternalSolversApplication is deprecated and will replaced by the EigenSolversApplication in the future")
