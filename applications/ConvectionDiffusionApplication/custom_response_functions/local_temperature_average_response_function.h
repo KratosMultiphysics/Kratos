9989e7ed09ecd60bb710b075536d6780fef55073
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-03-17 16:02:29 +0100

    using variable_utils sum method

--- applications/ConvectionDiffusionApplication/custom_response_functions/local_temperature_average_response_function.h
+++ applications/ConvectionDiffusionApplication/custom_response_functions/local_temperature_average_response_function.h
@@ -160,24 +160,14 @@ public:
     double CalculateValue(ModelPart& rModelPart) override
     {
         KRATOS_TRY;
-	
-        const ModelPart& r_target_model_part = GetTargetModelPart(rModelPart, mTargetModelPartName);	
+        const ModelPart& r_target_model_part =
+            GetTargetModelPart(rModelPart, mTargetModelPartName);
+
+        const double domain_aggregated_temperature =
+            VariableUtils().SumHistoricalNodeScalarVariable(TEMPERATURE, r_target_model_part);
 
         const Communicator& r_communicator = r_target_model_part.GetCommunicator();
         const int number_of_nodes = r_communicator.LocalMesh().NumberOfNodes();
-        
-	double domain_aggregated_temperature = 0.0;
-
-#pragma omp parallel for reduction(+ : domain_aggregated_temperature)
-        for (int i = 0; i < number_of_nodes; ++i)
-        {
-            const ModelPart::NodeType& r_node =
-                *(r_communicator.LocalMesh().NodesBegin() + i);
-            domain_aggregated_temperature += r_node.FastGetSolutionStepValue(TEMPERATURE);
-        }
-
-        domain_aggregated_temperature =
-            r_communicator.GetDataCommunicator().SumAll(domain_aggregated_temperature);
         const int total_nodes = r_communicator.GetDataCommunicator().SumAll(number_of_nodes);
 
         return domain_aggregated_temperature / static_cast<double>(total_nodes);
