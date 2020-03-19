7bd1a60017535c422d79ca2792208ae1a1cc4b55
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-03-19 19:39:43 +0100

    Preparing solver

--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
@@ -114,7 +114,19 @@ class MechanicalSolver(PythonSolver):
             "line_search": false,
             "compute_reactions": true,
             "block_builder" : true,
+            "scheme_settings" : {
+                "name"                               : "static",
+                "rayleigh_beta_variable"             : "RAYLEIGH_BETA"
+            },
+            "convergence_criteria_settings" : {
+                 "name"                              : "residual_criteria",
+                 "residual_absolute_tolerance"       : 1.0e-9,
+                 "residual_relative_tolerance"       : 1.0e-4,
+                 "displacement_absolute_tolerance"   : 1.0e-9,
+                 "displacement_relative_tolerance"   : 1.0e-4
+            },
             "builder_and_solver_settings" : {
+                "name"                               : "block_builder_and_solver",
                 "diagonal_values_for_dirichlet_dofs" : "use_max_diagonal",
                 "silent_warnings"                    : false
             },
