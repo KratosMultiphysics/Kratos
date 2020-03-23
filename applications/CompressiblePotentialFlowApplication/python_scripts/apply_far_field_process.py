ba1f5aee9da82d5c23bb886134b983b0764631ff
Inigo Lopez <inigolcanalejo@gmail.com>
2020-03-23 15:25:11 +0100

    0.01 step

--- applications/CompressiblePotentialFlowApplication/python_scripts/apply_far_field_process.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/apply_far_field_process.py
@@ -67,6 +67,7 @@ class ApplyFarFieldProcess(KratosMultiphysics.Process):
 
     def ExecuteInitializeSolutionStep(self):
         self.step = self.fluid_model_part.ProcessInfo[KratosMultiphysics.STEP]
+        self.free_stream_mach += 0.01
         #self.free_stream_mach = self.step / 1.0 * self.free_stream_mach_final
         #self.free_stream_mach = self.free_stream_mach_final + self.step / 100.0
         # if self.step > 6:
