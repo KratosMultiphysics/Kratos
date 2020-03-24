422f6c3c7afe6b2ce9197258c14a39c7fbe60652
Inigo Lopez <inigolcanalejo@gmail.com>
2020-03-24 12:42:12 +0100

    debugging

--- kratos/solving_strategies/convergencecriterias/residual_criteria.h
+++ kratos/solving_strategies/convergencecriterias/residual_criteria.h
@@ -176,7 +176,7 @@ public:
             const TDataType float_size_residual = static_cast<TDataType>(size_residual);
             const TDataType absolute_norm = (mCurrentResidualNorm/float_size_residual);
 
-            KRATOS_INFO_IF("RESIDUAL CRITERION", this->GetEchoLevel() > 1 && rModelPart.GetCommunicator().MyPID() == 0) << " :: [ Initial residual norm = " << mInitialResidualNorm << "; Current residual norm =  " << mCurrentResidualNorm << "]" << std::endl;
+            KRATOS_INFO_IF("RESIDUAL CRITERION", this->GetEchoLevel() > 0 && rModelPart.GetCommunicator().MyPID() == 0) << " :: [ Initial residual norm = " << mInitialResidualNorm << "; Current residual norm =  " << mCurrentResidualNorm << "]" << std::endl;
             KRATOS_INFO_IF("RESIDUAL CRITERION", this->GetEchoLevel() > 0 && rModelPart.GetCommunicator().MyPID() == 0) << " :: [ Obtained ratio = " << ratio << "; Expected ratio = " << mRatioTolerance << "; Absolute norm = " << absolute_norm << "; Expected norm =  " << mAlwaysConvergedNorm << "]" << std::endl;
 
             rModelPart.GetProcessInfo()[CONVERGENCE_RATIO] = ratio;
@@ -361,6 +361,8 @@ protected:
 
         rDofNum = dof_num;
         rResidualSolutionNorm = std::sqrt(residual_solution_norm);
+        KRATOS_WATCH(rDofNum)
+        KRATOS_WATCH(rResidualSolutionNorm)
     }
 
     ///@}
