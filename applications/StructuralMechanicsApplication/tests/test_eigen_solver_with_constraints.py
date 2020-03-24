1b2b8af4eb59098ccf58fa44fb54370f98eac2b6
philbucher <philipp.bucher@tum.de>
2020-03-24 20:15:26 +0100

    modifying tests to use block and elim B&S

--- applications/StructuralMechanicsApplication/tests/test_eigen_solver_with_constraints.py
+++ applications/StructuralMechanicsApplication/tests/test_eigen_solver_with_constraints.py
@@ -47,7 +47,13 @@ class TestEigenSolverWithConstraints(KratosUnittest.TestCase):
     # muting the output
     KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
 
-    def test_eigen_with_constraints(self):
+    def test_eigen_with_constraints_block_builder(self):
+        self.execute_test_eigen_with_constraints(use_block_builder=True)
+
+    def test_eigen_with_constraints_elimination_builder(self):
+        self.execute_test_eigen_with_constraints(use_block_builder=False)
+
+    def execute_test_eigen_with_constraints(self, use_block_builder):
         analysis_parameters = KratosMultiphysics.Parameters("""{
             "problem_data"    : {
                 "parallel_type" : "OpenMP",
@@ -66,9 +72,14 @@ class TestEigenSolverWithConstraints(KratosUnittest.TestCase):
                     "time_step" : 1.1
                 },
                 "use_computing_model_part" : false,
-                "rotation_dofs"            : true
+                "rotation_dofs"            : true,
+                "block_builder"            : true
             }
         }""")
+
+        analysis_parameters["solver_settings"]["block_builder"].SetBool(use_block_builder)
+        analysis_parameters_with_constraints = analysis_parameters.Clone()
+        analysis_parameters_with_constraints["solver_settings"]["block_builder"].SetBool(True) # Currently the EliminationB&S does not reliably work with constraints
 
         model = KratosMultiphysics.Model()
         analysis = StructuralMechanicsAnalysis(model, analysis_parameters.Clone())
@@ -77,7 +88,7 @@ class TestEigenSolverWithConstraints(KratosUnittest.TestCase):
         analysis.Run()
 
         model_with_constraints = KratosMultiphysics.Model()
-        analysis_with_constraints = StructuralMechanicsAnalysisWithConstraints(model_with_constraints, analysis_parameters.Clone())
+        analysis_with_constraints = StructuralMechanicsAnalysisWithConstraints(model_with_constraints, analysis_parameters_with_constraints)
         model_part_with_constraints = model_with_constraints["Structure"]
         SetupSystem(model_part_with_constraints, use_constraints=True)
         analysis_with_constraints.Run()
