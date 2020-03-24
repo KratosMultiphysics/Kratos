c87b733ca6d01940beb43918f897e03247f06069
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-03-24 20:11:26 +1000

    AddCustomUtilitiesToPython

---
+++ applications/MappingApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -0,0 +1,34 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:         BSD License
+//                   license: structural_mechanics_application/license.txt
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "custom_python/add_custom_utilities_to_python.h"
+
+//Utilities
+#include "custom_utilities/geometrical_utilities/intersection_utilities.h"
+
+namespace Kratos {
+namespace Python {
+
+void  AddCustomUtilitiesToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+    // IntersectionUtilities
+    m.def("FindIntersection1DGeometries2D", &IntersectionUtilities::FindIntersection1DGeometries2D);
+
+}
+
+}  // namespace Python.
+} // Namespace Kratos
+
