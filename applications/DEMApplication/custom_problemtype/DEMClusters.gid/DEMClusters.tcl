a6014269dde494f90a6343dfa22d7024d9e307a6
ferran <farrufat@cimne.upc.edu>
2020-03-27 11:46:31 +0100

    update
    stopped working. had to begin again

---
+++ applications/DEMApplication/custom_problemtype/DEMClusters.gid/DEMClusters.tcl
@@ -0,0 +1,54 @@
+## GiD events --------------------------------------------------------------------------------------------------------------------------------------------------
+
+proc InitGIDProject { dir } {
+
+    # Create buttons for calculate mass, center of mass and inertia later on.
+    # if { [GidUtils::IsTkDisabled] eq 0} {
+    #     GiDMenu::Create "Sphere Cluster Creation" PRE
+    #     GiDMenu::InsertOption "Sphere Cluster Creation" [list "Inertia"] 0 PRE "GidOpenConditions \"Inertia\"" "" ""
+    #     GiDMenu::UpdateMenus
+    # }
+
+    # Load the application scripts
+    set scripts_dir [file join $dir .. .. ]
+    # set tcl_filename [file join $scripts_dir scripts initptype.tcl]
+    # if {[catch {source $tcl_filename} msg]} {
+    #     WarnWinText $msg
+    #     return 1
+    # }
+}
+
+#-------------------------------------------------------------------------------
+
+proc BeforeMeshGeneration {elementsize} {
+    W "execute BeforeMeshGeneration"
+
+}
+
+proc AfterMeshGeneration {fail} {
+    W "execute AfterMeshGeneration"
+    CalculateInertia
+}
+
+#-------------------------------------------------------------------------------
+
+proc BeforeRunCalculation { batfilename basename dir problemtypedir gidexe args } {
+
+    # Write file
+    source [file join $problemtypedir file.tcl]
+}
+
+#-------------------------------------------------------------------------------
+
+proc CalculateInertia  { } { 
+    W "execute CalculateInertia"
+    foreach volume_id [GiD_Geometry list volume 1:end] {
+        set volume_info [GiD_Info list_entities volume $volume_id]
+    }
+    
+    set triangles [GiD_Mesh list -element_type {triangle} element]
+    W $triangles
+
+    set tetra [GiD_Mesh list -element_type {tetrahedra} element]
+    W $tetra
+}
