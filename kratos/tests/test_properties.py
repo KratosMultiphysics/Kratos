327011d165554a33afa4038b9c22b41c08f2540e
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-03-27 17:18:21 +0100

    Test

--- kratos/tests/test_properties.py
+++ kratos/tests/test_properties.py
@@ -21,6 +21,23 @@ class TestProperties(KratosUnittest.TestCase):
         self.assertEqual(properties.GetValue(KM.YOUNG_MODULUS), 1.0)
         self.assertEqual(cloned_properties.GetValue(KM.YOUNG_MODULUS), 10.0)
 
+    def test_data_properties(self):
+        current_model = KM.Model()
+
+        model_part= current_model.CreateModelPart("Main")
+
+        properties = model_part.CreateNewProperties(1)
+
+        data = properties.Data
+        data.SetValue(KM.YOUNG_MODULUS, 1.0)
+        self.assertEqual(properties.GetValue(KM.YOUNG_MODULUS), 1.0)
+
+        data_copy = KM.DataValueContainer(properties.Data)
+        self.assertEqual(data_copy.GetValue(KM.YOUNG_MODULUS), 1.0)
+
+        data.SetValue(KM.DENSITY, 12.0)
+        self.assertEqual(properties.GetValue(KM.DENSITY), 12.0)
+        self.assertEqual(data_copy.Has(KM.DENSITY), False)
 
 if __name__ == '__main__':
     KratosUnittest.main()
