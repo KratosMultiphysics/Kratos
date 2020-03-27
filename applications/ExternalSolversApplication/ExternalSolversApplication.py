d7066d446d723da10bdb525cc6769a9b12ec961a
Philipp Bucher <philipp.bucher@tum.de>
2020-03-27 16:24:19 +0100

    missing

--- applications/ExternalSolversApplication/ExternalSolversApplication.py
+++ applications/ExternalSolversApplication/ExternalSolversApplication.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import KratosMultiphysics as KM
-from KratosMultiphysics.kratos_utilities import IssueDeprecationWarning
 from KratosExternalSolversApplication import *
 application = KratosExternalSolversApplication()
 application_name = "KratosExternalSolversApplication"
