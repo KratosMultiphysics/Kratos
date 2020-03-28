a9c00f7a8de98814dabe276c834bbdd1a192d6be
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-03-28 20:43:14 +0100

    [EigenSolversApplication] Update import script
    That

--- applications/EigenSolversApplication/EigenSolversApplication.py
+++ applications/EigenSolversApplication/EigenSolversApplication.py
@@ -9,10 +9,13 @@
 #   Author: Thomas Oberbichler
 #
 
+# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from __future__ import print_function, absolute_import, division
+
+# Application dependent names and paths
 import KratosMultiphysics as KM
 from KratosEigenSolversApplication import *
 application = KratosEigenSolversApplication()
-application_name = "KratosEigenSolversApplication"
-application_folder = "EigenSolversApplication"
+application_name = "EigenSolversApplication"
 
-KM._ImportApplicationAsModule(application, application_name, application_folder, __path__)
+KM._ImportApplication(application, application_name)
