bb763d22c69e4945288072319be8b61b372c6229
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-03-30 14:18:56 +1000

    Remove KRATOS_CHECK_VARIABLE_KEY

--- applications/IgaApplication/custom_elements/shell_3p_element.cpp
+++ applications/IgaApplication/custom_elements/shell_3p_element.cpp
@@ -709,8 +709,6 @@ namespace Kratos
 
     int Shell3pElement::Check(const ProcessInfo& rCurrentProcessInfo)
     {
-        KRATOS_CHECK_VARIABLE_KEY(DISPLACEMENT)
-
         // Verify that the constitutive law exists
         if (this->GetProperties().Has(CONSTITUTIVE_LAW) == false)
         {
