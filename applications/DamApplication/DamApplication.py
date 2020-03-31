43af58d5a81bf408d6062b8cda91e285075c9e76
Philipp Bucher <philipp.bucher@tum.de>
2020-03-31 10:55:43 +0200

    missed another one

--- applications/DamApplication/DamApplication.py
+++ applications/DamApplication/DamApplication.py
@@ -2,9 +2,9 @@
 from __future__ import print_function, absolute_import, division
 
 # Application dependent names and paths
-import KratosMultiphysics as KM
+from KratosMultiphysics import _ImportApplication
 from KratosDamApplication import *
 application = KratosDamApplication()
 application_name = "KratosDamApplication"
 
-KM._ImportApplication(application,application_name)
+_ImportApplication(application,application_name)
