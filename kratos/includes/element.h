36388487134d53f606f3b60237793255b82829d8
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-03-31 21:22:40 +0200

    Defining output and documentation

--- kratos/includes/element.h
+++ kratos/includes/element.h
@@ -1360,6 +1360,12 @@ public:
         "framework"                  : "eulerian",                           // NOTE: Options are eulerian, lagrangian, ALE
         "symmetric_lhs"              : true,                                 // NOTE: Options are true/false
         "positive_definite_lhs"      : false,                                // NOTE: Options are true/false
+        "output"                     : {                                     // NOTE: Values compatible as output
+                "gauss_point"            : ["INTEGRATION_WEIGTH"],
+                "nodal_historical"       : ["DISPLACEMENT"],
+                "nodal_non_historical"   : [],
+                "entity"                 : []
+        },
         "required_variables"         : ["DISPLACEMENT"],                     // NOTE: Fill with the required variables
         "required_dofs"              : ["DISPLACEMENT_X", "DISPLACEMENT_Y"], // NOTE: Fill with the required dofs
         "flags_used"                 : ["BOUNDARY", "ACTIVE"],               // NOTE: Fill with the flags used
@@ -1370,7 +1376,8 @@ public:
             "type"         : ["PlaneStress","PlaneStrain"],                  // NOTE: List of CL compatible types. Options are "PlaneStress", "PlaneStrain", "3D"
             "dimension"   : ["2D", "2D"],                                    // NOTE: List of dimensions. Options are "2D", "3D", "2DAxysimm"
             "strain_size" : [3,3]                                            // NOTE: List of strain sizes
-            }
+            },
+        "documentation"              : "This is an element"                  // NOTE: The documentation of the entity
         }
      * @return specifications The required specifications/requirements
      */
@@ -1381,6 +1388,12 @@ public:
             "framework"                  : "lagrangian",
             "symmetric_lhs"              : false,
             "positive_definite_lhs"      : false,
+            "output"                     : {
+                "gauss_point"            : [],
+                "nodal_historical"       : [],
+                "nodal_non_historical"   : [],
+                "entity"                 : []
+            },
             "required_variables"         : [],
             "required_dofs"              : [],
             "flags_used"                 : [],
@@ -1391,7 +1404,10 @@ public:
                 "type"        : [],
                 "dimension"   : [],
                 "strain_size" : []
-            }})");
+            },
+            "documentation"   : "This is the base element"
+
+        })");
         return specifications;
     }
 
