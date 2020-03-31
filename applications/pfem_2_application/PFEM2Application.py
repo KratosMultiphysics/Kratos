4f813fe5b1c97295839c9e39e20737a4dc1130fe
Philipp Bucher <philipp.bucher@tum.de>
2020-03-31 10:53:24 +0200

    missed one

--- applications/pfem_2_application/PFEM2Application.py
+++ applications/pfem_2_application/PFEM2Application.py
@@ -2,9 +2,9 @@
 from __future__ import print_function, absolute_import, division
 
 # Application dependent names and paths
-import KratosMultiphysics as KM
+from KratosMultiphysics import _ImportApplication
 from KratosPFEM2Application import *
 application = KratosPFEM2Application()
 application_name = "KratosPFEM2Application"
 
-KM._ImportApplication(application,application_name)
+_ImportApplication(application,application_name)
