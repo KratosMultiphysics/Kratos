cf58c523da4bf7de377a8b0bf124e0ed56c1ed4c
pooyan <pooyan@altair.com>
2020-04-01 14:29:01 +0200

    Fixing static object

--- kratos/containers/vector_component_adaptor.h
+++ kratos/containers/vector_component_adaptor.h
@@ -112,7 +112,9 @@ public:
 
     static VectorComponentAdaptor const& StaticObject()
     {
-        return msStaticObject;
+
+        static VectorComponentAdaptor const static_object = VectorComponentAdaptor<TVectorType>(VectorComponentAdaptor<TVectorType>::SourceVariableType::StaticObject(), 0);
+        return static_object;
     }
 
     ///@}
@@ -204,8 +206,6 @@ private:
     ///@name Static Member Variables
     ///@{
 
-    static VectorComponentAdaptor const msStaticObject;
-
     ///@}
     ///@name Member Variables
     ///@{
@@ -250,9 +250,6 @@ private:
 }; // Class VectorComponentAdaptor
 
 ///@}
-
-template<class TVectorType>
-const VectorComponentAdaptor<TVectorType> VectorComponentAdaptor<TVectorType>::msStaticObject = VectorComponentAdaptor<TVectorType>(VectorComponentAdaptor<TVectorType>::SourceVariableType::StaticObject(), 0);
 
 ///@name Type Definitions
 ///@{
