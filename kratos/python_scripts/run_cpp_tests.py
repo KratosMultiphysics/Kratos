ed8ec2e5a693350e2124aeda1dc53f666d0acc4e
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-04-03 19:49:50 +0200

    Update run_cpp_tests.py

--- kratos/python_scripts/run_cpp_tests.py
+++ kratos/python_scripts/run_cpp_tests.py
@@ -1,17 +1,3 @@
 import KratosMultiphysics as KM
-
-try:
-    import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
-except ImportError as e:
-    KM.Logger.PrintWarning("StructuralMechanicsApplication is not available")
-try:
-    import KratosMultiphysics.FluidDynamicsApplication as FluidDynamicsApplication
-except ImportError as e:
-    KM.Logger.PrintWarning("FluidDynamicsApplication is not available")
-try:
-    import KratosMultiphysics.ConvectionDiffusionApplication as ConvectionDiffusionApplication
-except ImportError as e:
-    KM.Logger.PrintWarning("ConvectionDiffusionApplication is not available")
-
 KM.Tester.SetVerbosity(KM.Tester.Verbosity.TESTS_OUTPUTS)
 KM.Tester.RunAllTestCases()
