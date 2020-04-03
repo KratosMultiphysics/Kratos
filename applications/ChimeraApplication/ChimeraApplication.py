3bc7d65e53a50cdc1e91df7e881b68edfd2e3755
adityagantasala <shine.aditya@gmail.com>
2020-04-03 11:30:48 +0200

    test import fd app

--- applications/ChimeraApplication/ChimeraApplication.py
+++ applications/ChimeraApplication/ChimeraApplication.py
@@ -3,6 +3,7 @@ from __future__ import print_function, absolute_import, division
 
 # Application dependent names and paths
 from KratosMultiphysics import _ImportApplication
+from KratosFluidDynamicsApplication import *
 from KratosChimeraApplication import *
 application = KratosChimeraApplication()
 application_name = "KratosChimeraApplication"
