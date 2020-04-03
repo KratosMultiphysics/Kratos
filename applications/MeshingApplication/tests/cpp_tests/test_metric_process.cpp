df349008b79191ba23922b82b2f9ea467f2388ff
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-04-03 13:06:44 +0200

    Update SKIP macro (I)

--- applications/MeshingApplication/tests/cpp_tests/test_metric_process.cpp
+++ applications/MeshingApplication/tests/cpp_tests/test_metric_process.cpp
@@ -290,8 +290,7 @@ namespace Kratos
             process_info.SetValue(NL_ITERATION_NUMBER, 1);
 
             // In case the StructuralMechanicsApplciation is not compiled we skip the test
-            if (!KratosComponents<ConstitutiveLaw>::Has("LinearElasticPlaneStrain2DLaw"))
-                return void();
+            KRATOS_SKIP_TEST_IF_NOT(KratosComponents<ConstitutiveLaw>::Has("LinearElasticPlaneStrain2DLaw")) << "This test needs the StructuralMechanicsApplication" << std::endl;
 
             CppTestsUtilities::Create2DGeometry(r_model_part, "SmallDisplacementElement2D3N", false);
 
@@ -353,8 +352,7 @@ namespace Kratos
             process_info.SetValue(NL_ITERATION_NUMBER, 1);
 
             // In case the StructuralMechanicsApplciation is not compiled we skip the test
-            if (!KratosComponents<ConstitutiveLaw>::Has("LinearElastic3DLaw"))
-                return void();
+            KRATOS_SKIP_TEST_IF_NOT(KratosComponents<ConstitutiveLaw>::Has("LinearElastic3DLaw")) << "This test needs the StructuralMechanicsApplication" << std::endl;
 
             CppTestsUtilities::Create3DGeometry(r_model_part, "SmallDisplacementElement3D4N", false);
 
