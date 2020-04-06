26b441b138c833dd50e7ec9cea14d6ac4f714ab2
riccardotosi <rtosi@cimne.upc.edu>
2020-04-06 16:23:37 +0200

    Update parameters

--- applications/MultilevelMonteCarloApplication/external_libraries/pycompss/exaqute/ExaquteParameter.py
+++ applications/MultilevelMonteCarloApplication/external_libraries/pycompss/exaqute/ExaquteParameter.py
@@ -4,3 +4,13 @@ INOUT = None
 FILE_IN = None
 FILE_OUT = None
 FILE_INOUT = None
+
+COLLECTION_IN = None
+COLLECTION_INOUT = None
+COLLECTION_OUT = None
+
+Type = 'type'  # parameter type
+Direction = 'direction'  # parameter type
+StdIOStream = 'stream'  # parameter stream
+Prefix = 'prefix'  # parameter prefix
+Depth = 'depth'  # collection recursive depth
