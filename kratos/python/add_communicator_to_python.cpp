a5a6f15321afe247048d912ee9196960a41def5d
philbucher <philipp.bucher@tum.de>
2020-04-07 17:42:11 +0200

    finished empty file

--- kratos/python/add_communicator_to_python.cpp
+++ kratos/python/add_communicator_to_python.cpp
@@ -1,1 +1,31 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main author:     Philipp Bucher
+//
 
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define_python.h"
+#include "add_communicator_to_python.h"
+#include "includes/communicator.h"
+
+namespace Kratos {
+namespace Python {
+
+void AddCommunicatorToPython(pybind11::module &m)
+{
+    namespace py = pybind11;
+}
+
+} // namespace Python.
+} // Namespace Kratos
