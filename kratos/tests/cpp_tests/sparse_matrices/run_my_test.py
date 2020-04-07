91e35b0fb66128a8da3e6020b433e14c9d65389d
Riccardo Rossi <rrossi@cimne.upc.edu>
2020-04-07 17:13:21 +0200

    script to run tests

---
+++ kratos/tests/cpp_tests/sparse_matrices/run_my_test.py
@@ -0,0 +1,9 @@
+from KratosMultiphysics import *
+Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
+Tester.RunTestCases("TestGraphConstruction")
+Tester.RunTestCases("TestGraphContiguousRowConstruction")
+#Tester.RunTestCases("TestDistributedGraphConstruction")
+Tester.RunTestCases("TestCsrMatrixAssemble")
+Tester.RunTestCases("TestPerformanceBenchmarkSparseGraph")
+Tester.RunTestCases("TestPerformanceBenchmarkSparseContiguousRowGraph")
+#Tester.RunTestSuite("KratosCoreFastSuite")
