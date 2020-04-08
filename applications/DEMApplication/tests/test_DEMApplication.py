111d0ce882a17e60f5a6987c7b7dd1e0ef8e8c52
Miguel Angel <maceli@cimne.upc.edu>
2020-04-08 23:09:17 +0200

    leaving out the restart tests (tests taking too long)

--- applications/DEMApplication/tests/test_DEMApplication.py
+++ applications/DEMApplication/tests/test_DEMApplication.py
@@ -57,8 +57,8 @@ def AssembleTestSuites():
 
     smallSuite.addTest(test_DEM_3D_continuum.TestDEM3DContinuum("test_DEM3D_continuum"))
 
-    smallSuite.addTest(test_restart.TestRestartOneBall("test_execution"))
-    smallSuite.addTest(test_restart.TestRestartTwoBalls("test_execution"))
+    #smallSuite.addTest(test_restart.TestRestartOneBall("test_execution"))
+    #smallSuite.addTest(test_restart.TestRestartTwoBalls("test_execution"))
 
     # Create a test suit with the selected tests plus all small tests
     nightSuite = suites['nightly']
