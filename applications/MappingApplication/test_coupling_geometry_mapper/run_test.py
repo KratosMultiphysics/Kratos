8f588e3e0f0ee37c11ba50ff11e76897ff0b1102
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-04-11 14:12:15 +1000

    Update run_test.py

--- applications/MappingApplication/test_coupling_geometry_mapper/run_test.py
+++ applications/MappingApplication/test_coupling_geometry_mapper/run_test.py
@@ -16,18 +16,20 @@ model_part_origin = current_model.CreateModelPart("origin")
 model_part_destination = current_model.CreateModelPart("destination")
 
 model_part_coupling = current_model.CreateModelPart("coupling")
+model_part_coupling_quadrature_points = current_model.CreateModelPart("coupling_quadrature_points")
 
 ReadModelPart(model_part_origin, "../coupled_cantilever/domainA.gid/domainA")
 ReadModelPart(model_part_destination, "../coupled_cantilever/domainB.gid/domainB")
 
 KratosMapping.IntersectionUtilities.FindIntersection1DGeometries2D(model_part_origin, model_part_destination, model_part_coupling)
+KratosMapping.IntersectionUtilities.CreateQuadraturePointsCoupling1DGeometries2D(model_part_coupling, model_part_coupling_quadrature_points, 1e-6)
 
 mapper_params = KM.Parameters("""{
     "mapper_type": "coupling_geometry",
     "echo_level" : 0
 }""")
 
-mapper = KratosMapping.MapperFactory.CreateMapper(model_part_origin, model_part_destination, mapper_params)
+mapper = KratosMapping.MapperFactory.CreateMapper(model_part_coupling_quadrature_points, model_part_destination, mapper_params)
 
 for node in model_part_origin.Nodes:
     node.SetSolutionStepValue(KM.DISPLACEMENT_X, 1.0, model_part_origin.ProcessInfo)
