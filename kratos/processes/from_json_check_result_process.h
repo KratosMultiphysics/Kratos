c05e55d5732b707bfa521c737211ea30b806304e
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-04-13 08:30:07 +0200

    Using GetValue instead

--- kratos/processes/from_json_check_result_process.h
+++ kratos/processes/from_json_check_result_process.h
@@ -142,7 +142,7 @@ public:
         const SizeType GPIndex = 0
         ) const
     {
-        return GetResultaData(GPIndex)[ComponentIndex]->GetNearestValue(Time);
+        return GetResultaData(GPIndex)[ComponentIndex]->GetValue(Time);
     }
 
     /**
