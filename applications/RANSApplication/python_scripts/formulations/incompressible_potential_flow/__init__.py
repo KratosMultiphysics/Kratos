61b0b5d47e24f9fc31a8bc07ed79f1d8e7fe7785
sunethwarna <suneth.warna@gmail.com>
2020-04-13 08:39:29 +0200

    add incompressible potential flow formulation

---
+++ applications/RANSApplication/python_scripts/formulations/incompressible_potential_flow/__init__.py
@@ -0,0 +1,55 @@
+from __future__ import print_function, absolute_import, division
+
+# import kratos
+import KratosMultiphysics as Kratos
+
+# import RANS
+import KratosMultiphysics.RANSApplication as KratosRANS
+
+# import formulation interface
+from KratosMultiphysics.RANSApplication.formulations.formulation import Formulation
+
+# import formulations
+from .incompressible_potential_flow_velocity_formulation import IncompressiblePotentialFlowVelocityFormulation
+from .incompressible_potential_flow_pressure_formulation import IncompressiblePotentialFlowPressureFormulation
+
+
+class IncompressiblePotentialFlowFormulation(Formulation):
+    def __init__(self, model_part, settings):
+        super(IncompressiblePotentialFlowFormulation, self).__init__(model_part, settings)
+
+        default_settings = Kratos.Parameters(r'''
+        {
+            "formulation_name": "incompressible_potential_flow",
+            "velocity_potential_flow_solver_settings":{},
+            "pressure_potential_flow_solver_settings":{}
+        }''')
+        self.settings.ValidateAndAssignDefaults(default_settings)
+
+        self.AddFormulation(IncompressiblePotentialFlowVelocityFormulation(model_part, settings["velocity_potential_flow_solver_settings"]))
+        self.AddFormulation(IncompressiblePotentialFlowPressureFormulation(model_part, settings["pressure_potential_flow_solver_settings"]))
+
+    def AddVariables(self):
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(Kratos.VELOCITY)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(Kratos.PRESSURE)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(Kratos.EXTERNAL_PRESSURE)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(Kratos.BODY_FORCE)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(Kratos.DENSITY)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(Kratos.NORMAL)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(KratosRANS.VELOCITY_POTENTIAL)
+        self.GetBaseModelPart().AddNodalSolutionStepVariable(KratosRANS.PRESSURE_POTENTIAL)
+
+        Kratos.Logger.PrintInfo(self.GetName(), "Added solution step variables.")
+
+    def AddDofs(self):
+        Kratos.VariableUtils().AddDof(Kratos.VELOCITY_X, self.GetBaseModelPart())
+        Kratos.VariableUtils().AddDof(Kratos.VELOCITY_Y, self.GetBaseModelPart())
+        Kratos.VariableUtils().AddDof(Kratos.VELOCITY_Z, self.GetBaseModelPart())
+        Kratos.VariableUtils().AddDof(Kratos.PRESSURE, self.GetBaseModelPart())
+        Kratos.VariableUtils().AddDof(KratosRANS.VELOCITY_POTENTIAL, self.GetBaseModelPart())
+        Kratos.VariableUtils().AddDof(KratosRANS.PRESSURE_POTENTIAL, self.GetBaseModelPart())
+
+        Kratos.Logger.PrintInfo(self.GetName(), "Added solution step dofs.")
+
+    def GetMinimumBufferSize(self):
+        return 1
---
+++ applications/RANSApplication/python_scripts/formulations/incompressible_potential_flow/incompressible_potential_flow_velocity_formulation.py
@@ -0,0 +1,114 @@
+from __future__ import print_function, absolute_import, division
+
+# import kratos
+import KratosMultiphysics as Kratos
+
+# import required applications
+import KratosMultiphysics.RANSApplication as KratosRANS
+
+# impot processes
+from KratosMultiphysics import IntegrationValuesExtrapolationToNodesProcess as extrapolation_process
+
+# import formulation interface
+from KratosMultiphysics.RANSApplication.formulations.formulation import Formulation
+
+# import utilities
+from KratosMultiphysics.RANSApplication import RansVariableUtilities
+from KratosMultiphysics.RANSApplication.formulations.utilities import CreateLinearSolver
+from KratosMultiphysics.RANSApplication.formulations.utilities import CreateFormulationModelPart
+from KratosMultiphysics.RANSApplication.formulations.utilities import CalculateNormalsOnConditions
+from KratosMultiphysics.RANSApplication.formulations.utilities import CreateResidualBasedBlockBuilderAndSolver
+from KratosMultiphysics.RANSApplication.formulations.utilities import CreateResidualCriteria
+from KratosMultiphysics.RANSApplication.formulations.utilities import CreateResidualBasedNewtonRaphsonStrategy
+from KratosMultiphysics.RANSApplication.formulations.utilities import CreateIncremantalUpdateScheme
+
+class IncompressiblePotentialFlowVelocityFormulation(Formulation):
+    def __init__(self, model_part, settings):
+        super(IncompressiblePotentialFlowVelocityFormulation, self).__init__(model_part, settings)
+
+        defaults = Kratos.Parameters(r"""{
+            "linear_solver_settings": {
+                "solver_type": "amgcl"
+            },
+            "echo_level": 0
+        }""")
+
+        self.settings.ValidateAndAssignDefaults(defaults)
+
+    def PrepareModelPart(self):
+        self.velocity_model_part = CreateFormulationModelPart(self,
+                                                                "RansIncompressiblePotentialFlowVelocity",
+                                                                "RansIncompressiblePotentialFlowVelocity")
+
+        Kratos.Logger.PrintInfo(self.GetName(), "Created formulation model part.")
+
+    def Initialize(self):
+        CalculateNormalsOnConditions(self.GetBaseModelPart())
+
+        solver_settings = self.settings
+        linear_solver = CreateLinearSolver(solver_settings["linear_solver_settings"])
+        builder_and_solver = CreateResidualBasedBlockBuilderAndSolver(linear_solver, self.IsPeriodic(), self.GetCommunicator())
+        convergence_criteria = CreateResidualCriteria(1e-12, 1e-12)
+        self.velocity_strategy = CreateResidualBasedNewtonRaphsonStrategy(
+                    self.velocity_model_part, CreateIncremantalUpdateScheme(), linear_solver,
+                    convergence_criteria, builder_and_solver, 2, False, False, False)
+
+        builder_and_solver.SetEchoLevel(
+            solver_settings["echo_level"].GetInt() - 3)
+        self.velocity_strategy.SetEchoLevel(
+            solver_settings["echo_level"].GetInt() - 2)
+        convergence_criteria.SetEchoLevel(
+            solver_settings["echo_level"].GetInt() - 1)
+
+        self.velocity_strategy.Initialize()
+        Kratos.Logger.PrintInfo(self.GetName(), "Initialized formulation")
+
+    def InitializeSolutionStep(self):
+        if (not hasattr(self, "is_initialized")):
+            self.is_initialized = True
+
+            RansVariableUtilities.FixFlaggedDofs(self.velocity_model_part,
+                                                 KratosRANS.VELOCITY_POTENTIAL,
+                                                 Kratos.OUTLET)
+
+            self.velocity_strategy.InitializeSolutionStep()
+
+    def IsConverged(self):
+        if (hasattr(self, "is_solved")):
+            return self.is_solved
+        return False
+
+    def SolveCouplingStep(self):
+        self.is_solved = True
+        self.velocity_strategy.Predict()
+        self.velocity_strategy.SolveSolutionStep()
+
+    def ExecuteAfterCouplingSolveStep(self):
+        RansVariableUtilities.CopyFlaggedVariableToNonHistorical(
+            self.velocity_model_part, Kratos.VELOCITY, Kratos.INLET)
+
+        # extrapolate gauss point velocities to nodal velocities
+        extrapolation_settings = Kratos.Parameters('''
+        {
+            "echo_level"                 : 0,
+            "area_average"               : true,
+            "average_variable"           : "NODAL_AREA",
+            "list_of_variables"          : ["VELOCITY"],
+            "extrapolate_non_historical" : false
+        }''')
+        extrapolation_process(self.velocity_model_part,
+                                extrapolation_settings).Execute()
+
+        # take back the original inlet velocities
+        RansVariableUtilities.CopyFlaggedVariableFromNonHistorical(
+            self.velocity_model_part, Kratos.VELOCITY, Kratos.INLET)
+
+    def FinializeSolutionStep(self):
+        self.velocity_strategy.FinializeSolutionStep()
+
+    def Check(self):
+        self.velocity_strategy.Check()
+
+    def Clear(self):
+        self.velocity_strategy.Clear()
+
