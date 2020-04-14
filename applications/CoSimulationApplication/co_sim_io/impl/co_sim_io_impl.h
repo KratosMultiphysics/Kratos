dfd8f9d85b0dacc46187e99ab70f921baf67cf40
philbucher <philipp.bucher@tum.de>
2020-04-15 01:13:44 +0200

    leftover string conversion

--- applications/CoSimulationApplication/co_sim_io/impl/co_sim_io_impl.h
+++ applications/CoSimulationApplication/co_sim_io/impl/co_sim_io_impl.h
@@ -47,7 +47,7 @@ inline void Connect(const std::string& rConnectionName, CoSimIO::SettingsType Se
     using namespace Internals;
     KRATOS_CO_SIM_ERROR_IF(HasIO(rConnectionName)) << "A connection for \"" << rConnectionName << "\" already exists!" << std::endl;
 
-    s_co_sim_connections[std::string(rConnectionName)] = std::unique_ptr<CoSimConnection>(new CoSimConnection(rConnectionName, Settings));
+    s_co_sim_connections[rConnectionName] = std::unique_ptr<CoSimConnection>(new CoSimConnection(rConnectionName, Settings));
     GetConnection(rConnectionName).Connect();
 }
 
@@ -62,7 +62,7 @@ inline void Disconnect(const std::string& rConnectionName)
     KRATOS_CO_SIM_ERROR_IF_NOT(HasIO(rConnectionName)) << "Trying to disconnect connection \"" << rConnectionName << "\" which does not exist!" << std::endl;
 
     GetConnection(rConnectionName).Disconnect();
-    s_co_sim_connections.erase(std::string(rConnectionName));
+    s_co_sim_connections.erase(rConnectionName);
 }
 
 // Version for C++, there this input is a std::vector, which we have to wrap before passing it on
