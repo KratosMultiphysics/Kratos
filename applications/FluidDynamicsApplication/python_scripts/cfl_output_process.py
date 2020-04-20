a70f69cbdb092cf04fff67353db376fa42b88366
ma6yu <sakuma.m.aa@gmail.com>
2020-04-20 17:52:12 +0200

    no change

--- applications/FluidDynamicsApplication/python_scripts/cfl_output_process.py
+++ applications/FluidDynamicsApplication/python_scripts/cfl_output_process.py
@@ -52,7 +52,7 @@ class CFLOutputProcess(KratosMultiphysics.Process):
         # getting the ModelPart from the Model
         self.model_part_name = params["model_part_name"].GetString()
         if self.model_part_name == "":
-            raise Exception('No "model_part_name" was specified! aaa')
+            raise Exception('No "model_part_name" was specified!')
         else:
             self.model_part = model[self.model_part_name]
 
