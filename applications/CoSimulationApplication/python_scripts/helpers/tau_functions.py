e436cfbec9edbb3e9fde5e2b5dbed38182a41cc9
juliePiquee <piquee@hirusako.aer.mw.tum.de>
2020-04-20 16:03:50 +0200

    test commit

--- applications/CoSimulationApplication/python_scripts/helpers/tau_functions.py
+++ applications/CoSimulationApplication/python_scripts/helpers/tau_functions.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import itertools
-def findInterfaceFileName(list_of_interface_file_paths,working_path,this_step_out):
+
+def findInterfaceFileName(list_of_interface_file_paths,working_path,this_step_out): # add a word = "/Outputs/airfoilSol.MEMBRANE_i=" or "/Mesh/airfoil_Structured_scaliert.grid.def."
     this_step_out +=1
     for file in list_of_interface_file_paths:
-        if file.startswith('%s'%working_path + '/Outputs/airfoilSol.MEMBRANE_i=' + '%s'%this_step_out):
+        if file.startswith('%s'%working_path + '/Outputs/airfoilSol.MEMBRANE_i=' + '%s'%this_step_out): ####### i would like to make it general #######
             print file
             return file
 
@@ -15,3 +16,6 @@ def findInterfaceFileNumberOfLines(fname):
             if 'E+' in line or 'E-' in line:
                 it = it+1
     return it
+
+def PrintBlockHeader(header):
+ 	tau_python.tau_msg("\n" + 50 * "*" + "\n" + "* %s\n" %header + 50*"*" + "\n")
