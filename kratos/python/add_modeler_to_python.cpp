142d4589f2b3faf10cb87f3133ee800f5ca57500
tteschemacher <tteschemacher@users.noreply.github.com>
2020-04-27 13:19:55 +1000

    Fix typo from Modeler to Model

--- kratos/python/add_modeler_to_python.cpp
+++ kratos/python/add_modeler_to_python.cpp
@@ -61,7 +61,7 @@ void  AddModelerToPython(pybind11::module& m)
 {
     py::class_<Modeler, Modeler::Pointer>(m,"Modeler")
     .def(py::init<>())
-    .def(py::init<Modeler&, Parameters>())
+    .def(py::init<Model&, Parameters>())
     // Modeler Stages Initialize
     .def("ImportGeometryModel", &Modeler::ImportGeometryModel)
     .def("PrepareGeometryModel", &Modeler::PrepareGeometryModel)
