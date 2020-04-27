10a14817b28c057b90720d5cc6306dd3d32d4319
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-04-27 23:13:57 +0200

    Missing

--- kratos/solving_strategies/schemes/scheme.h
+++ kratos/solving_strategies/schemes/scheme.h
@@ -60,6 +60,9 @@ public:
     /// Pointer definition of Scheme
     KRATOS_CLASS_POINTER_DEFINITION(Scheme);
 
+    /// The definition of the current class
+    typedef Scheme< TSparseSpace, TDenseSpace > ClassType;
+
     /// Data type definition
     typedef typename TSparseSpace::DataType TDataType;
     /// Matrix type definition
@@ -200,6 +203,19 @@ public:
     ///@}
     ///@name Operators
     ///@{
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief Create method
+     * @param ThisParameters The configuration parameters
+     */
+    virtual typename ClassType::Pointer Create(Parameters ThisParameters) const
+    {
+        return Kratos::make_shared<ClassType>(ThisParameters);
+    }
 
     /**
      * @brief Clone method
@@ -1070,10 +1086,6 @@ public:
     {
         return "scheme";
     }
-
-    ///@}
-    ///@name Operations
-    ///@{
 
     ///@}
     ///@name Access
