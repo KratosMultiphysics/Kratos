d2b5e671210dcc6c28dc565a8d2f1c103a49d1b2
roigcarlo <roigcarlo@gmail.com>
2020-04-30 16:04:11 +0000

    Changing everything

--- applications/MultilevelMonteCarloApplication/MultilevelMonteCarloApplication.py
+++ applications/MultilevelMonteCarloApplication/MultilevelMonteCarloApplication.py
@@ -1,6 +1,8 @@
 # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 from __future__ import print_function, absolute_import, division
 
+import sys
+
 # Application dependent names and paths
 from KratosMultiphysics import _ImportApplication
 from KratosMultilevelMonteCarloApplication import *
@@ -8,3 +10,6 @@ application = KratosMultilevelMonteCarloApplication()
 application_name = "KratosMultilevelMonteCarloApplication"
 
 _ImportApplication(application, application_name)
+
+# xmc and PyCOMPSs location
+sys.path.append("external_libraries")
