11b410a02620f0ca6ec6f247493a6827022ec15a
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-05-01 11:06:06 +0200

    Clean up (IV)

--- kratos/factories/standard_strategy_factory.cpp
+++ kratos/factories/standard_strategy_factory.cpp
@@ -16,6 +16,7 @@
 
 // Project includes
 #include "includes/define.h"
+#include "factories/base_factory.h"
 #include "factories/standard_strategy_factory.h"
 #include "spaces/ublas_space.h"
 
@@ -36,7 +37,6 @@ namespace Kratos
         typedef TUblasDenseSpace<double> LocalSpaceType;
         typedef LinearSolver<SpaceType, LocalSpaceType> LinearSolverType;
 
-        typedef SolvingStrategy<SpaceType,  LocalSpaceType, LinearSolverType> SolvingStrategyType;
         typedef ResidualBasedLinearStrategy<SpaceType,  LocalSpaceType, LinearSolverType> ResidualBasedLinearStrategyType;
         typedef ResidualBasedNewtonRaphsonStrategy<SpaceType,  LocalSpaceType, LinearSolverType> ResidualBasedNewtonRaphsonStrategyType;
         typedef AdaptiveResidualBasedNewtonRaphsonStrategy<SpaceType,  LocalSpaceType, LinearSolverType> AdaptiveResidualBasedNewtonRaphsonStrategyType;
@@ -44,17 +44,17 @@ namespace Kratos
         typedef ExplicitStrategy<SpaceType,  LocalSpaceType, LinearSolverType> ExplicitStrategyType;
 
         //NOTE: here we must create persisting objects for the strategies
-        static auto ResidualBasedLinearStrategyFactory = StandardStrategyFactory< SolvingStrategyType, ResidualBasedLinearStrategyType>();
-        static auto ResidualBasedNewtonRaphsonStrategyFactory = StandardStrategyFactory< SolvingStrategyType, ResidualBasedNewtonRaphsonStrategyType>();
-        static auto AdaptiveResidualBasedNewtonRaphsonStrategyFactory = StandardStrategyFactory< SolvingStrategyType, AdaptiveResidualBasedNewtonRaphsonStrategyType>();
-        static auto LineSearchStrategyFactory = StandardStrategyFactory< SolvingStrategyType, LineSearchStrategyType>();
-        static auto ExplicitStrategyFactory = StandardStrategyFactory< SolvingStrategyType, ExplicitStrategyType>();
+        static ResidualBasedLinearStrategyType msResidualBasedLinearStrategy;
+        static ResidualBasedNewtonRaphsonStrategyType msResidualBasedNewtonRaphsonStrategy;
+        static AdaptiveResidualBasedNewtonRaphsonStrategyType msAdaptiveResidualBasedNewtonRaphsonStrategy;
+        static LineSearchStrategyType msLineSearchStrategy;
+        static ExplicitStrategyType msExplicitStrategy;
 
         // Registration of strategies
-        KRATOS_REGISTER_STRATEGY(ResidualBasedLinearStrategyType::Name(), ResidualBasedLinearStrategyFactory);
-        KRATOS_REGISTER_STRATEGY(ResidualBasedNewtonRaphsonStrategyType::Name(), ResidualBasedNewtonRaphsonStrategyFactory);
-        KRATOS_REGISTER_STRATEGY(AdaptiveResidualBasedNewtonRaphsonStrategyType::Name(), AdaptiveResidualBasedNewtonRaphsonStrategyFactory);
-        KRATOS_REGISTER_STRATEGY(LineSearchStrategyType::Name(), LineSearchStrategyFactory);
-        KRATOS_REGISTER_STRATEGY(ExplicitStrategyType::Name(), ExplicitStrategyFactory);
+        KRATOS_REGISTER_STRATEGY(ResidualBasedLinearStrategyType::Name(), msResidualBasedLinearStrategy);
+        KRATOS_REGISTER_STRATEGY(ResidualBasedNewtonRaphsonStrategyType::Name(), msResidualBasedNewtonRaphsonStrategy);
+        KRATOS_REGISTER_STRATEGY(AdaptiveResidualBasedNewtonRaphsonStrategyType::Name(), msAdaptiveResidualBasedNewtonRaphsonStrategy);
+        KRATOS_REGISTER_STRATEGY(LineSearchStrategyType::Name(), msLineSearchStrategy);
+        KRATOS_REGISTER_STRATEGY(ExplicitStrategyType::Name(), msExplicitStrategy);
     };
 } // Namespace Kratos
--- kratos/factories/standard_strategy_factory.h
+++ kratos/factories/standard_strategy_factory.h
@@ -20,75 +20,11 @@
 
 // Project includes
 #include "includes/define.h"
-#include "factories/base_factory.h"
 
 namespace Kratos
 {
 ///@name Kratos Classes
 ///@{
-
-/**
- * @class StandardStrategyFactory
- * @ingroup KratosCore
- * @brief Here we add the functions needed for the registration of strategys
- * @details Defines the standard strategy factory
- * @author Vicente Mataix Ferrandiz
- * @tparam TStrategyType The strategy type
- * @tparam TCustomStrategyType The strategy type  (derived class)
- */
-template <typename TStrategyType, typename TCustomStrategyType>
-class StandardStrategyFactory
-    : public BaseFactory<TStrategyType>
-{
-public:
-    ///@name Type Definitions
-    ///@{
-
-    /// The definition of the strategy
-    typedef TStrategyType StrategyType;
-
-    ///@}
-    ///@name Operations
-    ///@{
-
-    /**
-     * @brief This method is an auxiliar method to create a new strategy
-     * @return The pointer to the strategy of interest
-     */
-    typename StrategyType::Pointer CreateClass(ModelPart& rModelPart, Kratos::Parameters Settings) const override
-    {
-        return Kratos::make_shared<TCustomStrategyType>(rModelPart, Settings);
-    }
-
-    ///@}
-    ///@name Input and output
-    ///@{
-
-    /// Turn back information as a string.
-    std::string Info() const override
-    {
-        return "StandardStrategyFactory";
-    }
-};
-
-///@}
-///@name Input and output
-///@{
-
-/// output stream function
-template <typename TStrategyType, typename TCustomStrategyType>
-inline std::ostream& operator << (std::ostream& rOStream,
-                                  const StandardStrategyFactory<TStrategyType, TCustomStrategyType>& rThis)
-{
-    rThis.PrintInfo(rOStream);
-    rOStream << std::endl;
-    rThis.PrintData(rOStream);
-
-    return rOStream;
-}
-
-///@}
-///@name Input and output
 
 void RegisterStrategies();
 
