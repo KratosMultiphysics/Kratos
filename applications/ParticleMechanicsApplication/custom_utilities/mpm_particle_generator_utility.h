2dcf384cef1d42722cca74f56560905d90271afd
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-05-06 09:32:24 +1000

    increase serarch tolerances to default

--- applications/ParticleMechanicsApplication/custom_utilities/mpm_particle_generator_utility.h
+++ applications/ParticleMechanicsApplication/custom_utilities/mpm_particle_generator_utility.h
@@ -255,7 +255,7 @@ namespace MPMParticleGeneratorUtility
                         Vector N;
 
                         // FindPointOnMesh find the background element in which a given point falls and the relative shape functions
-                        bool is_found = SearchStructure.FindPointOnMesh(xg[0], N, pelem, result_begin, 10, 1e-3);
+                        bool is_found = SearchStructure.FindPointOnMesh(xg[0], N, pelem, result_begin);
                         if (!is_found) KRATOS_WARNING("MPM particle generator utility") << "::search failed." << std::endl;
 
                         pelem->Set(ACTIVE);
