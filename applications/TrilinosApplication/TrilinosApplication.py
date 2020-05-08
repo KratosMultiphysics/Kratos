55508abb296c21ce80053da0bb09dbbd1381e0bd
Philipp Bucher <philipp.bucher@tum.de>
2020-05-08 10:55:51 +0200

    use try-except

--- applications/TrilinosApplication/TrilinosApplication.py
+++ applications/TrilinosApplication/TrilinosApplication.py
@@ -1,7 +1,13 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 from KratosMultiphysics import _ImportApplication
 import KratosMultiphysics.mpi # importing the MPI-Core, since the TrilinosApp directly links to it
-from KratosTrilinosApplication import *
+try:
+    from KratosTrilinosApplication import *
+except: # TODO how to catch all errors
+    print("The import of the TrilinosApplication failed with the following error:")
+    # print(the_error)
+    print("Please visit the following website for known issues:")
+    print("https://github.com/KratosMultiphysics/Kratos/tree/master/applications/TrilinosApplication#known-issues")
 application = KratosTrilinosApplication()
 application_name = "KratosTrilinosApplication"
 
