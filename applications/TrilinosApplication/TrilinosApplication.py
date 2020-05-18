9846b87e469ae820ec4e6ad33ff7f28a0d8fa3d2
Philipp Bucher <philipp.bucher@tum.de>
2020-05-18 10:04:56 +0200

    using logger to only print on one rank

--- applications/TrilinosApplication/TrilinosApplication.py
+++ applications/TrilinosApplication/TrilinosApplication.py
@@ -1,13 +1,13 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-from KratosMultiphysics import _ImportApplication
+from KratosMultiphysics import Logger, _ImportApplication
 import KratosMultiphysics.mpi # importing the MPI-Core, since the TrilinosApp directly links to it
 try:
     from KratosTrilinosApplication import *
 except BaseException as error:
-    print("The import of the TrilinosApplication failed with the following error:")
-    print(error)
-    print("Please visit the following website for known issues:")
-    print("https://github.com/KratosMultiphysics/Kratos/tree/master/applications/TrilinosApplication#known-issues")
+    Logger.PrintInfo("TrilinosApplication", "The import of the TrilinosApplication failed with the following error:")
+    Logger.PrintInfo("TrilinosApplication", error)
+    Logger.PrintInfo("TrilinosApplication", "Please visit the following website for known issues:")
+    Logger.PrintInfo("TrilinosApplication", "https://github.com/KratosMultiphysics/Kratos/tree/master/applications/TrilinosApplication#known-issues")
 application = KratosTrilinosApplication()
 application_name = "KratosTrilinosApplication"
 
