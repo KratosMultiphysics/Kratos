312cb0bd17e4b2337f271e8f1c05bf9d4049c5e8
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2020-05-21 11:54:04 +0200

    adding methods

--- applications/FemToDemApplication/python_scripts/fem_dem_coupled_gid_output.py
+++ applications/FemToDemApplication/python_scripts/fem_dem_coupled_gid_output.py
@@ -11,7 +11,10 @@ from KratosMultiphysics import gid_output # this is deprecated
 
 class FemDemCoupledGiDOutput(gid_output.GiDOutput):
 
-    # ------------------------------------------------------------------------------------------- #
+    """The constructor of the FemDemCoupledGiDOutput-Object.
+    This object prints the results from the PFEM, FEMDEM and DEM 
+    apps in the same post file 
+    """
     def __init__(self,
                 file_name,
                 vol_output,
@@ -40,19 +43,149 @@ class FemDemCoupledGiDOutput(gid_output.GiDOutput):
         self.outerlistfilename = os.path.join("..", self.listfilename)
 
         self.solid_model_part                   = solid_model_part
+        self.fluid_model_part                   = fluid_model_part
+
         self.balls_model_part                   = balls_model_part
         self.clusters_model_part                = clusters_model_part
         self.rigid_faces_model_part             = rigid_faces_model_part
         self.mixed_model_part                   = mixed_model_part
+
         self.mixed_solid_fluid_model_part       = mixed_solid_fluid_model_part
         self.mixed_solid_balls_model_part       = mixed_solid_balls_model_part
         self.mixed_solid_balls_fluid_model_part = mixed_solid_balls_fluid_model_part
 
         self.solid_nodal_results                   = []
+        self.fluid_nodal_results                   = []
         self.dem_nodal_results                     = []
         self.clusters_nodal_results                = []
         self.rigid_faces_nodal_results             = []
+
         self.mixed_solid_fluid_nodal_results       = []
         self.mixed_solid_balls_nodal_results       = []
         self.mixed_solid_balls_fluid_nodal_results = []
-        self.structures_gauss_points_results       = []
+
+        self.solid_gauss_points_results            = []
+
+
+    """ initialize_dem_fem_results
+    This method initializes the post coupled post process
+    """
+    def initialize_dem_fem_results(self,
+                                   solid_nodal_results,
+                                   fluid_nodal_results,
+                                   dem_nodal_results,
+                                   clusters_nodal_results,
+                                   rigid_faces_nodal_results,
+                                   mixed_solid_fluid_nodal_results,
+                                   mixed_solid_balls_nodal_results,
+                                   mixed_solid_balls_fluid_nodal_results,
+                                   solid_gauss_points_results):
+
+        self.solid_nodal_results       = solid_nodal_results
+        self.fluid_nodal_results       = fluid_nodal_results
+        self.dem_nodal_results         = dem_nodal_results
+        self.clusters_nodal_results    = clusters_nodal_results
+        self.rigid_faces_nodal_results = rigid_faces_nodal_results
+
+        self.mixed_solid_fluid_nodal_results       = mixed_solid_fluid_nodal_results
+        self.mixed_solid_balls_nodal_results       = mixed_solid_balls_nodal_results
+        self.mixed_solid_balls_fluid_nodal_results = mixed_solid_balls_fluid_nodal_results
+
+        self.solid_gauss_points_results = solid_gauss_points_results
+
+        if self.multi_file == MultiFileFlag.SingleFile:
+            print("Singlefile option is not available for the DEM-Structures Coupling application!")
+            mesh_name = 0.0
+            self.io.InitializeMesh(mesh_name)
+            self.io.WriteSphereMesh(self.balls_model_part.GetMesh())
+            self.io.WriteMesh(self.clusters_model_part.GetMesh())
+            self.io.WriteMesh(self.rigid_faces_model_part.GetMesh())
+            self.io.WriteMesh(self.mixed_solid_fluid_model_part.GetMesh())
+            self.io.WriteMesh(self.mixed_solid_balls_model_part.GetMesh())
+            self.io.WriteMesh(self.mixed_solid_balls_fluid_model_part.GetMesh())
+            self.io.FinalizeMesh()
+            self.io.InitializeResults(mesh_name, self.mixed_solid_fluid_model_part.GetMesh())
+            self.io.InitializeResults(mesh_name, self.mixed_solid_balls_model_part.GetMesh())
+            self.io.InitializeResults(mesh_name, self.mixed_solid_balls_fluid_model_part.GetMesh())
+
+        # Initialize list file
+        with open(self.listfilename, "w") as listfile:
+            if self.multi_file == MultiFileFlag.MultipleFiles:
+                listfile.write("Multiple\n")
+            elif self.multi_file == MultiFileFlag.SingleFile:
+                listfile.write("Single\n")
+        if self.multi_file == MultiFileFlag.SingleFile:
+            if self.post_mode == GiDPostMode.GiD_PostBinary:
+                self.write_step_to_list()
+            else:
+                self.write_step_to_list(0)
+
+
+    """ write_step_to_list
+    """
+    def write_step_to_list(self, step_label):
+    
+        if self.post_mode   == GiDPostMode.GiD_PostBinary:
+            ext = ".post.bin"
+        elif self.post_mode == GiDPostMode.GiD_PostAscii:
+            ext = ".post.res"
+        elif self.post_mode == GiDPostMode.GiD_PostAsciiZipped:
+            ext = ".post.res"
+
+        with open(self.listfilename, "a") as listfile:
+            listfile.write("Multiple\n")
+            listfile.write(self.filename+"_"+"%.12g"%step_label+ext+"\n")
+
+
+    """ write_step_to_outer_list
+    """
+    def write_step_to_outer_list(self, step_label):
+
+        if self.post_mode   == GiDPostMode.GiD_PostBinary:
+            ext = ".post.bin"
+        elif self.post_mode == GiDPostMode.GiD_PostAscii:
+            ext = ".post.res"
+        elif self.post_mode == GiDPostMode.GiD_PostAsciiZipped:
+            ext = ".post.res"
+
+        with open(self.outerlistfilename, "a") as listfile:
+            listfile.write("Multiple\n")
+            folder_name = self.filename + "_Post_Files"
+            full_string_to_write = os.path.join(folder_name, self.filename + "_" + "%.12g"%step_label + ext)
+            listfile.write(full_string_to_write + "\n")
+
+
+    """ Writeresults
+    """
+    def Writeresults(self, time):
+
+        Logger.PrintInfo("","")
+        Logger.PrintInfo("","*****************  PRINTING RESULTS FOR GID  *************************")
+        Logger.Flush()
+
+        if self.GiDMultiFileFlag == "Multiples":
+            self.mixed_solid_fluid_model_part.Elements.clear()
+            self.mixed_solid_fluid_model_part.Nodes.clear()
+
+            self.mixed_solid_balls_model_part.Elements.clear()
+            self.mixed_solid_balls_model_part.Nodes.clear()
+
+            self.mixed_solid_balls_fluid_model_part.Elements.clear()
+            self.mixed_solid_balls_fluid_model_part.Nodes.clear()
+
+            # Now we fill the mixed MDPA in order to print
+
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_fluid_model_part, self.solid_model_part)
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_fluid_model_part, self.fluid_model_part)
+
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_model_part, self.balls_model_part)
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_model_part, self.rigid_faces_model_part)
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_model_part, self.solid_model_part)
+
+
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_nodal_results, self.balls_model_part)
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_nodal_results, self.rigid_faces_model_part)
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_nodal_results, self.solid_model_part)
+            DEMApplication.PostUtilities().AddModelPartToModelPart(self.mixed_solid_balls_fluid_nodal_results, self.fluid_model_part)
+
+        self.write_dem_fem_results(time)
