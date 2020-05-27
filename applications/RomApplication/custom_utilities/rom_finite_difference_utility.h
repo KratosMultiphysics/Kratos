ab946eeb4ad4ffd772811a2df635ddec481444fe
ga69cal <altug.emiroglu@tum.de>
2020-05-27 14:55:56 +0200

    fixed compile warnings

--- applications/RomApplication/custom_utilities/rom_finite_difference_utility.h
+++ applications/RomApplication/custom_utilities/rom_finite_difference_utility.h
@@ -20,6 +20,7 @@
 // Project includes
 #include "includes/define.h"
 #include "includes/element.h"
+#include "utilities/openmp_utils.h"
 
 namespace Kratos
 {
@@ -41,7 +42,39 @@ public:
                                                 Dof<double>& rDof,
                                                 const double& rPertubationSize,
                                                 Matrix& rOutput,
-                                                ProcessInfo& rCurrentProcessInfo);
+                                                ProcessInfo& rCurrentProcessInfo){
+
+
+    KRATOS_TRY;
+
+        KRATOS_WARNING_IF("RomFiniteDifferenceUtility::CalculateLeftHandSideDerivative", OpenMPUtils::IsInParallel() != 0)
+            << "The call of this non omp-parallelized function within a parallel section should be avoided for efficiency reasons!" << std::endl;
+        
+        #pragma omp critical
+        {
+            // define working variables
+            Matrix LHS;
+            Matrix LHS_perturbed;
+            Vector dummy;
+
+            // compute LHS before perturbation
+            rElement.CalculateLocalSystem(LHS, dummy, rCurrentProcessInfo);
+            
+            // perturb the design variable
+            rDof.GetSolutionStepValue() += rPertubationSize;
+            
+            // compute LHS after perturbation
+            rElement.CalculateLocalSystem(LHS_perturbed, dummy, rCurrentProcessInfo);
+            
+            //compute derivative of RHS w.r.t. design variable with finite differences
+            noalias(rOutput) = (LHS_perturbed - LHS) / rPertubationSize;
+            
+            // unperturb the design variable
+            rDof.GetSolutionStepValue() -= rPertubationSize;
+
+        }
+        KRATOS_CATCH("");
+    }
 
 
 }; // class RomFiniteDifferenceUtility.
