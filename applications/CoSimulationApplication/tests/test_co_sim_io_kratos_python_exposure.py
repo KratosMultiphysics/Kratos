28d94f5830fa7987b3357c6a51b1f8ae50f49ef3
philbucher <philipp.bucher@tum.de>
2020-05-28 00:13:54 +0200

    advances in Test

--- applications/CoSimulationApplication/tests/test_co_sim_io_kratos_python_exposure.py
+++ applications/CoSimulationApplication/tests/test_co_sim_io_kratos_python_exposure.py
@@ -7,6 +7,7 @@ from KratosMultiphysics.CoSimulationApplication import CoSimIO
 from KratosMultiphysics import kratos_utilities as kratos_utils
 
 import os
+import threading
 
 def GetFilePath(fileName):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), fileName)
@@ -14,6 +15,18 @@ def GetFilePath(fileName):
 class TestCoSimIOKratosPythonExposure(KratosUnittest.TestCase):
 
     def test_Connect_Disconnect(self):
+        def Connect_Disconnect():
+            connection_settings = CoSimIO.Info()
+            connection_settings.SetString("connection_name", "abcc")
+            CoSimIO.Connect(connection_settings)
+
+            disconnect_settings = CoSimIO.Info()
+            disconnect_settings.SetString("connection_name", "abcc")
+
+            CoSimIO.Disconnect(disconnect_settings)
+
+        t = RunInThread(Connect_Disconnect, [])
+
         connection_settings = CoSimIO.Info()
         connection_settings.SetString("connection_name", "abcc")
         CoSimIO.Connect(connection_settings)
@@ -22,6 +35,46 @@ class TestCoSimIOKratosPythonExposure(KratosUnittest.TestCase):
         disconnect_settings.SetString("connection_name", "abcc")
 
         CoSimIO.Disconnect(disconnect_settings)
+
+        t.join()
+
+    def test_Export_Import_Data(self):
+        def Import_Export_Data():
+            connection_settings = CoSimIO.Info()
+            connection_settings.SetString("connection_name", "abcc")
+            CoSimIO.Connect(connection_settings)
+
+
+            disconnect_settings = CoSimIO.Info()
+            disconnect_settings.SetString("connection_name", "abcc")
+
+            CoSimIO.Disconnect(disconnect_settings)
+
+        t = RunInThread(Connect_Disconnect, [])
+
+        connection_settings = CoSimIO.Info()
+        connection_settings.SetString("connection_name", "abcc")
+        CoSimIO.Connect(connection_settings)
+
+
+        disconnect_settings = CoSimIO.Info()
+        disconnect_settings.SetString("connection_name", "abcc")
+
+        CoSimIO.Disconnect(disconnect_settings)
+
+        t.join()
+
+
+def RunInThread(target_fct, arguments):
+    t = threading.Thread(
+        target=target_fct,
+        args=arguments
+    )
+
+    t.start()
+    return t
+
+
 
 if __name__ == '__main__':
     KratosUnittest.main()
