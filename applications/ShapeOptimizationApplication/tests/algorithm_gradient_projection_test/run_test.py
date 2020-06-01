b4a7b61bc7dee329c1b6b9dd4910b1b57fb2ae54
Armin Geiser <armin.geiser@tum.de>
2020-06-01 19:11:35 +0200

    smaller test

--- applications/ShapeOptimizationApplication/tests/algorithm_gradient_projection_test/run_test.py
+++ applications/ShapeOptimizationApplication/tests/algorithm_gradient_projection_test/run_test.py
@@ -2,77 +2,85 @@
 import KratosMultiphysics as KM
 
 # Additional imports
-from KratosMultiphysics.ShapeOptimizationApplication import optimizer_factory
 from KratosMultiphysics.ShapeOptimizationApplication.analyzer_base import AnalyzerBaseClass
+from KratosMultiphysics.ShapeOptimizationApplication import optimizer_factory
 from KratosMultiphysics.KratosUnittest import TestCase
 import KratosMultiphysics.kratos_utilities as kratos_utilities
 import csv, os
 
-# Read parameters
-with open("parameters.json",'r') as parameter_file:
-    parameters = KM.Parameters(parameter_file.read())
-
-model = KM.Model()
-
 # =======================================================================================================
 # Define external analyzer
 # =======================================================================================================
+class Target():
 
-# The external analyzer provides a response to constrain the distance of a specific node to a given target
+    target_z = 2.0
+
+    @classmethod
+    def Value(cls, model_part):
+        f = 0
+        for node in model_part.Nodes:
+            f += (cls.target_z - node.Z) ** 2
+        return f
+
+    @classmethod
+    def Gradient(cls, model_part):
+        gradient = {}
+        for node in model_part.Nodes:
+            sz = - 2 * (cls.target_z - node.Z)
+            gradient[node.Id] = [0.0, 0.0, sz]
+        return gradient
+
+class CoordZ():
+
+    @staticmethod
+    def Value(model_part, node_id):
+        return model_part.Nodes[node_id].Z
+
+    @staticmethod
+    def Gradient(model_part, node_id):
+        gradient = {}
+        for node in model_part.Nodes:
+            if node.Id == node_id:
+                gradient[node.Id] = [0.0, 0.0, 1.0]
+            else:
+                gradient[node.Id] = [0.0, 0.0, 0.0]
+        return gradient
+
+
 class CustomAnalyzer(AnalyzerBaseClass):
+
     # --------------------------------------------------------------------------------------------------
     def AnalyzeDesignAndReportToCommunicator(self, current_design, optimization_iteration, communicator):
 
-        # Constraint 1
-        constraint_node_id = 893
-        if communicator.isRequestingValueOf("y_value_893"):
-            value = current_design.Nodes[constraint_node_id].Y
-            communicator.reportValue("y_value_893", value)
+        if communicator.isRequestingValueOf("target"):
+            communicator.reportValue("target", Target.Value(current_design))
+        if communicator.isRequestingGradientOf("target"):
+            communicator.reportGradient("target", Target.Gradient(current_design))
 
-        if communicator.isRequestingGradientOf("y_value_893"):
-            gradient = {}
-            for node in current_design.Nodes:
-                if node.Id == constraint_node_id:
-                    gradient[node.Id] = [0.0,1.0,0.0]
-                else:
-                    gradient[node.Id] = [0.0,0.0,0.0]
-            communicator.reportGradient("y_value_893", gradient)
+        if communicator.isRequestingValueOf("z_15"):
+            communicator.reportValue("z_15", CoordZ.Value(current_design, 15))
+        if communicator.isRequestingGradientOf("z_15"):
+            communicator.reportGradient("z_15", CoordZ.Gradient(current_design, 15))
 
-        # Constraint 2
-        constrained_node_id =1861
-        target_x = 11.0
-        target_y = 0.0
-        target_z = 10.0
-        if communicator.isRequestingValueOf("distance_1861"):
-            constrained_node = current_design.GetNodes()[constrained_node_id]
+        if communicator.isRequestingValueOf("z_40"):
+            communicator.reportValue("z_40", CoordZ.Value(current_design, 40))
+        if communicator.isRequestingGradientOf("z_40"):
+            communicator.reportGradient("z_40", CoordZ.Gradient(current_design, 40))
 
-            distance = [0,0,0]
-            distance[0] = constrained_node.X0 - target_x
-            distance[1] = constrained_node.Y0 - target_y
-            distance[2] = constrained_node.Z0 - target_z
-            value = distance[0]**2 + distance[1]**2 + distance[2]**2
-
-            communicator.reportValue("distance_1861", value)
-
-        if communicator.isRequestingGradientOf("distance_1861"):
-            gradient = {}
-            for node in current_design.Nodes:
-                if node.Id == constrained_node_id:
-                    grad = [0,0,0]
-                    grad[0] = 2*(constrained_node.X0 - target_x)
-                    grad[1] = 2*(constrained_node.Y0 - target_y)
-                    grad[2] = 2*(constrained_node.Z0 - target_z)
-                    gradient[node.Id] = [grad[0],grad[1],grad[2]]
-                else:
-                    gradient[node.Id] = [0.0,0.0,0.0]
-
-            communicator.reportGradient("distance_1861", gradient)
+        if communicator.isRequestingValueOf("z_65"):
+            communicator.reportValue("z_65", CoordZ.Value(current_design, 65))
+        if communicator.isRequestingGradientOf("z_65"):
+            communicator.reportGradient("z_65", CoordZ.Gradient(current_design, 65))
 
 # =======================================================================================================
 # Perform optimization
 # =======================================================================================================
 
-# Create optimizer and perform optimization
+with open("parameters.json",'r') as parameter_file:
+    parameters = KM.Parameters(parameter_file.read())
+
+model = KM.Model()
+
 optimizer = optimizer_factory.CreateOptimizer(parameters["optimization_settings"], model, CustomAnalyzer())
 optimizer.Optimize()
 
@@ -84,32 +92,27 @@ optimization_log_filename = parameters["optimization_settings"]["output"]["optim
 optimization_model_part_name = parameters["optimization_settings"]["model_settings"]["model_part_name"].GetString()
 
 # Testing
-original_directory = os.getcwd()
-os.chdir(output_directory)
-
-with open(optimization_log_filename, 'r') as csvfile:
+with open(os.path.join(output_directory, optimization_log_filename), 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
-
-    all_lines = []
     last_line = None
     for line in reader:
         if not line:
             continue
         else:
-            all_lines.append(line)
             last_line = line
 
-    resulting_optimization_iterations = int(last_line[0].strip())
-    resulting_improvement = float(last_line[2].strip())
-    resulting_c1 = float(last_line[4].strip())
-    resulting_c2 = float(last_line[6].strip())
+    n_iter = int(last_line[0].strip())
+    f = float(last_line[1].strip())
+    df = float(last_line[2].strip())
+    c1 = float(last_line[4].strip())
+    c2 = float(last_line[6].strip())
+    c3 = float(last_line[8].strip())
 
-    TestCase().assertEqual(resulting_optimization_iterations, 4)
-    TestCase().assertAlmostEqual(resulting_improvement, -2.37411E+01, 4)
-    TestCase().assertAlmostEqual(resulting_c1, 1.07574E+01, 4)
-    TestCase().assertAlmostEqual(resulting_c2, 4.27570E-01, 4)
-
-
-os.chdir(original_directory)
+    # Check against specifications
+    TestCase().assertEqual(n_iter, 10)
+    TestCase().assertAlmostEqual(f, 6.61655E+01, 4)
+    TestCase().assertAlmostEqual(c1, -9.98583E-01, 4)
+    TestCase().assertAlmostEqual(c2, 2.03941E+00, 4)
+    TestCase().assertAlmostEqual(c3, 1.00106E+00, 4)
 
 # =======================================================================================================
