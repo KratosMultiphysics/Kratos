1dfddf785e95c94bf1d737e05f6bd6f268e4d006
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-06-01 13:57:21 +1000

    Add possibility to use cpp processes in process_factory

--- kratos/python_scripts/process_factory.py
+++ kratos/python_scripts/process_factory.py
@@ -22,7 +22,10 @@ class KratosProcessFactory(object):
             # python-script that contains the process
             python_module_name = item["python_module"].GetString()
 
-            if item.Has("kratos_module"): # for Kratos-processes
+            if KM.ProcessFactory.Has(python_module_name):
+                constructed_processes.append( KM.ProcessFactory.Create(python_module_name) )
+
+            elif item.Has("kratos_module"): # for Kratos-processes
                 """Location of the process in Kratos; e.g.:
                 - KratosMultiphysics
                 - KratosMultiphysics.FluidDynamicsApplication
