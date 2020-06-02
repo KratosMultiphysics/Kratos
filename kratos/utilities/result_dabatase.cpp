54a1bcfc1e3f49897f33f1f4969faa7bf36cf37b
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-02 16:00:05 +0200

    Clean up

--- kratos/utilities/result_dabatase.cpp
+++ kratos/utilities/result_dabatase.cpp
@@ -150,7 +150,19 @@ void ResultDatabase::Initialize(
     KRATOS_ERROR_IF_NOT(rVariablesIndexes.size() == rValuesSizes.size()) << "Inconsistent sizes in the values sizes and the variable indexes" << std::endl;
 
     // Auxiliar lambda to generate vectors of tables
-    auto table_generator =[](const SizeType NumberOfEntites, const SizeType NumberOfComponents, const SizeType NumberOfGP){std::vector<Table<double, double>*> aux_1(NumberOfComponents, nullptr); EntityDatabase aux_2(NumberOfGP, aux_1); VariableDatabase data(NumberOfEntites, aux_2); for (IndexType k = 0; k < NumberOfEntites; ++k){for (IndexType j = 0; j < NumberOfGP; ++j){ for (IndexType i = 0; i < NumberOfComponents; ++i){ data[k][j][i] = new Table<double, double>();}}}; return data;};
+    auto table_generator =[](const SizeType NumberOfEntites, const SizeType NumberOfComponents, const SizeType NumberOfGP){
+        std::vector<Table<double, double>*> aux_1(NumberOfComponents, nullptr);
+        EntityDatabase aux_2(NumberOfGP, aux_1);
+        VariableDatabase data(NumberOfEntites, aux_2);
+        for (IndexType k = 0; k < NumberOfEntites; ++k){
+            for (IndexType j = 0; j < NumberOfGP; ++j){
+                for (IndexType i = 0; i < NumberOfComponents; ++i){
+                    data[k][j][i] = new Table<double, double>();
+                }
+            }
+        };
+        return data;
+    };
 
     // Fill the inner map of tables
     for (IndexType i = 0; i < rVariablesIndexes.size(); ++i) {
