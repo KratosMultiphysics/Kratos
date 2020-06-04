93127a08f3912419dff9ad34a358078a78797c0e
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-04 14:04:05 +0200

    Activating test (III)

--- kratos/tests/cpp_tests/external_libraries/csv-parser/test_csv_buffer.cpp
+++ kratos/tests/cpp_tests/external_libraries/csv-parser/test_csv_buffer.cpp
@@ -26,84 +26,97 @@ namespace Testing {
 
 using namespace csv::internals;
 
-size_t split_at(BufferPtr buffer, ColumnPositions pos, int n) {
+std::size_t split_at(BufferPtr buffer, ColumnPositions pos, int n) {
     return buffer->split_buffer[pos.start + n];
 }
 
 std::string get_string(
     const std::string& buffer,
-    const std::pair<size_t, size_t>& row_str) {
+    const std::pair<std::size_t, std::size_t>& row_str) {
     return std::string(
         buffer.c_str() + row_str.first, // Beginning
         row_str.second // Count
     );
 }
 
-// KRATOS_TEST_CASE_IN_SUITE(GiantStringBufferTest, KratosExternalLibrariesFastSuite) {
-//     BufferPtr buffer = BufferPtr(new RawRowBuffer());
-//
-//     buffer->buffer.append("1234");
-//     std::string first_row = get_string(
-//         buffer->buffer,
-//         buffer->get_row().row_str
-//     );
-//
-//     buffer->buffer.append("5678");
-//     std::string second_row = get_string(
-//         buffer->buffer,
-//         buffer->get_row().row_str
-//     );
-//
-//     buffer = buffer->reset();
-//     buffer->buffer.append("abcd");
-//     std::string third_row = get_string(
-//         buffer->buffer,
-//         buffer->get_row().row_str
-//     );
-//
-//     KRATOS_CHECK(first_row == "1234");
-//     KRATOS_CHECK(second_row == "5678");
-//     KRATOS_CHECK(third_row == "abcd");
-// }
-//
-// KRATOS_TEST_CASE_IN_SUITE(GiantSplitBufferTest, KratosExternalLibrariesFastSuite) {
-//     BufferPtr buffer = BufferPtr(new RawRowBuffer());
-//     auto * splits = &(buffer->split_buffer);
-//
-//     splits->push_back(1);
-//     splits->push_back(2);
-//     splits->push_back(3);
-//
-//     auto pos = buffer->get_row().col_pos;
-//     KRATOS_CHECK(split_at(buffer, pos, 0) == 1);
-//     KRATOS_CHECK(split_at(buffer, pos, 1) == 2);
-//     KRATOS_CHECK(split_at(buffer, pos, 2) == 3);
-//     KRATOS_CHECK(pos.n_cols == 4);
-//
-//     SECTION("Two Splits Test") {
-//         splits->push_back(4);
-//         splits->push_back(5);
-//
-//         pos = buffer->get_row().col_pos;
-//         KRATOS_CHECK(split_at(buffer, pos, 0) == 4);
-//         KRATOS_CHECK(split_at(buffer, pos, 1) == 5);
-//         KRATOS_CHECK(pos.n_cols == 3);
-//     }
-//
-//     SECTION("Reset In Middle Test") {
-//         splits->push_back(1);
-//         buffer = buffer->reset();
-//         splits = &(buffer->split_buffer);
-//         splits->push_back(2);
-//         splits->push_back(3);
-//
-//         pos = buffer->get_row().col_pos;
-//         KRATOS_CHECK(split_at(buffer, pos, 0) == 1);
-//         KRATOS_CHECK(split_at(buffer, pos, 1) == 2);
-//         KRATOS_CHECK(split_at(buffer, pos, 2) == 3);
-//         KRATOS_CHECK(pos.n_cols == 4);
-//     }
-// }
+KRATOS_TEST_CASE_IN_SUITE(GiantStringBufferTest, KratosExternalLibrariesFastSuite)
+{
+    BufferPtr buffer = BufferPtr(new RawRowBuffer());
+
+    buffer->buffer.append("1234");
+    std::string first_row = get_string(
+        buffer->buffer,
+        buffer->get_row().row_str
+    );
+
+    buffer->buffer.append("5678");
+    std::string second_row = get_string(
+        buffer->buffer,
+        buffer->get_row().row_str
+    );
+
+    buffer = buffer->reset();
+    buffer->buffer.append("abcd");
+    std::string third_row = get_string(
+        buffer->buffer,
+        buffer->get_row().row_str
+    );
+
+    KRATOS_CHECK_STRING_EQUAL(first_row, "1234");
+    KRATOS_CHECK_STRING_EQUAL(second_row, "5678");
+    KRATOS_CHECK_STRING_EQUAL(third_row, "abcd");
+}
+
+KRATOS_TEST_CASE_IN_SUITE(GiantSplitBufferTest, KratosExternalLibrariesFastSuite)
+{
+    BufferPtr buffer_1 = BufferPtr(new RawRowBuffer());
+    auto* splits_1 = &(buffer_1->split_buffer);
+
+    splits_1->push_back(1);
+    splits_1->push_back(2);
+    splits_1->push_back(3);
+
+    auto pos = buffer_1->get_row().col_pos;
+    KRATOS_CHECK_EQUAL(split_at(buffer_1, pos, 0), 1);
+    KRATOS_CHECK_EQUAL(split_at(buffer_1, pos, 1), 2);
+    KRATOS_CHECK_EQUAL(split_at(buffer_1, pos, 2), 3);
+    KRATOS_CHECK_EQUAL(pos.n_cols, 4);
+
+    // Two Splits Test
+    {
+        splits_1->push_back(4);
+        splits_1->push_back(5);
+
+        pos = buffer_1->get_row().col_pos;
+        KRATOS_CHECK_EQUAL(split_at(buffer_1, pos, 0), 4);
+        KRATOS_CHECK_EQUAL(split_at(buffer_1, pos, 1), 5);
+        KRATOS_CHECK_EQUAL(pos.n_cols, 3);
+    }
+
+    BufferPtr buffer_2 = BufferPtr(new RawRowBuffer());
+    auto* splits_2 = &(buffer_2->split_buffer);
+
+    splits_2->push_back(1);
+    splits_2->push_back(2);
+    splits_2->push_back(3);
+
+    pos = buffer_2->get_row().col_pos;
+
+    // Reset In Middle Test
+    {
+        splits_2->push_back(1);
+        buffer_2 = buffer_2->reset();
+        splits_2 = &(buffer_2->split_buffer);
+        splits_2->push_back(2);
+        splits_2->push_back(3);
+
+        pos = buffer_2->get_row().col_pos;
+        KRATOS_CHECK_EQUAL(split_at(buffer_2, pos, 0), 1);
+        KRATOS_CHECK_EQUAL(split_at(buffer_2, pos, 1), 2);
+        KRATOS_CHECK_EQUAL(split_at(buffer_2, pos, 2), 3);
+        KRATOS_CHECK_EQUAL(pos.n_cols, 4);
+    }
+}
 
 } // namespace Testing.
 } // namespace Kratos.
