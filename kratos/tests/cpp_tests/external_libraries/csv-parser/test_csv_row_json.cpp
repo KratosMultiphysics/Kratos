1ce92209e610f014480cb036a88829d49874f010
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-04 15:43:54 +0200

    Activating test (VII)

--- kratos/tests/cpp_tests/external_libraries/csv-parser/test_csv_row_json.cpp
+++ kratos/tests/cpp_tests/external_libraries/csv-parser/test_csv_row_json.cpp
@@ -42,103 +42,101 @@ CSVRow make_csv_row(std::vector<std::string> data, std::vector<std::string> col_
     return CSVRow(concat, splits, std::make_shared<internals::ColNames>(col_names));
 }
 
-// KRATOS_TEST_CASE_IN_SUITE(json_escape_string()Test, KratosExternalLibrariesFastSuite) {
-//     using internals::json_escape_string;
-//
-//     // Assert that special characters are escaped properly
-//     KRATOS_CHECK(json_escape_string("Quote\"Quote") == "Quote\\\"Quote");
-//     KRATOS_CHECK(json_escape_string("RSolidus\\RSolidus")
-//         == "RSolidus\\\\RSolidus");
-//     KRATOS_CHECK(json_escape_string("Backspace\bBackspace")
-//         == "Backspace\\bBackspace");
-//     KRATOS_CHECK(json_escape_string("Formfeed\fFormfeed")
-//         == "Formfeed\\fFormfeed");
-//     KRATOS_CHECK(json_escape_string("Newline\nNewline")
-//         == "Newline\\nNewline");
-//     KRATOS_CHECK(json_escape_string("CarriageReturn\rCarriageReturn")
-//         == "CarriageReturn\\rCarriageReturn");
-//     KRATOS_CHECK(json_escape_string("Tab\tTab")
-//         == "Tab\\tTab");
-//
-//     // Assert that control characters are escaped properly
-//     KRATOS_CHECK(json_escape_string("Null\0Null")
-//         == "Null\u0000Null");
-// }
-//
-// KRATOS_TEST_CASE_IN_SUITE(CSVRowto_json()Test, KratosExternalLibrariesFastSuite) {
-//     CSVRow row = make_csv_row(
-//         { "Col 1", "Col 2" },   // Fields
-//         { "A", "B" }            // Column names
-//     );
-//
-//     KRATOS_CHECK(row.to_json() == "{\"A\":\"Col 1\",\"B\":\"Col 2\"}");
-// }
-//
-// KRATOS_TEST_CASE_IN_SUITE(CSVRowto_json()TestwithNumbers, KratosExternalLibrariesFastSuite) {
-//     CSVRow row = make_csv_row(
-//         { "1234.3", "234" },    // Fields
-//         { "A", "B"}             // Column names
-//     );
-//
-//     KRATOS_CHECK(row.to_json() == "{\"A\":1234.3,\"B\":234}");
-// }
-//
-// KRATOS_TEST_CASE_IN_SUITE(CSVRowto_json()Test-Mixed, KratosExternalLibrariesFastSuite) {
-//     CSVRow row = make_csv_row(
-//         { "1234.3", "234", "ABCD", "AB1", "1337" },     // Fields
-//         { "A", "B", "C", "D", "E" }                     // Column names
-//     );
-//
-//     // Full Row
-//     {
-//         KRATOS_CHECK(row.to_json() == "{\"A\":1234.3,\"B\":234,\"C\":\"ABCD\",\"D\":\"AB1\",\"E\":1337}");
-//     }
-//
-//     // Subset
-//     {
-//         KRATOS_CHECK(row.to_json({ "B", "C" }) == "{\"B\":234,\"C\":\"ABCD\"}");
-//         KRATOS_CHECK(row.to_json({ "B", "A" }) == "{\"B\":234,\"A\":1234.3}");
-//     }
-// }
-//
-// KRATOS_TEST_CASE_IN_SUITE(CSVRowto_json_array()Test()-Mixed, KratosExternalLibrariesFastSuite) {
-//     CSVRow row = make_csv_row(
-//         { "1234.3", "234", "ABCD", "AB1", "1337" },     // Fields
-//         { "A", "B", "C", "D", "E" }                     // Column names
-//     );
-//
-//     // Full Row 
-//     {
-//         KRATOS_CHECK(row.to_json_array() == "[1234.3,234,\"ABCD\",\"AB1\",1337]");
-//     }
-//
-//     // Subset
-//     {
-//         KRATOS_CHECK(row.to_json_array({ "B", "C" }) == "[234,\"ABCD\"]");
-//         KRATOS_CHECK(row.to_json_array({ "B", "A" }) == "[234,1234.3]");
-//     }
-// }
-//
-// // Reported in: https://github.com/vincentlaucsb/csv-parser/issues/68
-// KRATOS_TEST_CASE_IN_SUITE(CSVRowto_json()withWrongColumns, KratosExternalLibrariesFastSuite) {
-//     std::string csv_string(R"(A,B,C,
-// 123,345,678,)");
-//
-//     auto format = csv::CSVFormat();
-//     format.column_names({ "A", "B" });
-//
-//     csv::CSVReader reader(format);
-//     reader.feed(csv_string);
-//     reader.end_feed();
-//
-//     CSVRow first_row;
-//     reader.read_row(first_row);
-//
-//     // Since the column names provided were wrong, there won't be any data.
-//     // to_json() method should then produce an empty object instead of segfaulting.
-//     KRATOS_CHECK(first_row.to_json() == "{}");
-//     KRATOS_CHECK(first_row.to_json_array() == "[]");
-// }
+KRATOS_TEST_CASE_IN_SUITE(json_escape_stringTest, KratosExternalLibrariesFastSuite)
+{
+    using internals::json_escape_string;
+
+    // Assert that special characters are escaped properly
+    KRATOS_CHECK_EQUAL(json_escape_string("Quote\"Quote"), "Quote\\\"Quote");
+    KRATOS_CHECK_EQUAL(json_escape_string("RSolidus\\RSolidus"), "RSolidus\\\\RSolidus");
+    KRATOS_CHECK_EQUAL(json_escape_string("Backspace\bBackspace"), "Backspace\\bBackspace");
+    KRATOS_CHECK_EQUAL(json_escape_string("Formfeed\fFormfeed"), "Formfeed\\fFormfeed");
+    KRATOS_CHECK_EQUAL(json_escape_string("Newline\nNewline"), "Newline\\nNewline");
+    KRATOS_CHECK_EQUAL(json_escape_string("CarriageReturn\rCarriageReturn"), "CarriageReturn\\rCarriageReturn");
+    KRATOS_CHECK_EQUAL(json_escape_string("Tab\tTab"), "Tab\\tTab");
+
+    // Assert that control characters are escaped properly
+    KRATOS_CHECK_EQUAL(json_escape_string("Null\0Null"), "Null\u0000Null");
+}
+
+KRATOS_TEST_CASE_IN_SUITE(CSVRowto_jsonTest, KratosExternalLibrariesFastSuite)
+{
+    CSVRow row = make_csv_row(
+        { "Col 1", "Col 2" },   // Fields
+        { "A", "B" }            // Column names
+    );
+
+    KRATOS_CHECK_EQUAL(row.to_json(), "{\"A\":\"Col 1\",\"B\":\"Col 2\"}");
+}
+
+KRATOS_TEST_CASE_IN_SUITE(CSVRowto_jsonwithNumbers, KratosExternalLibrariesFastSuite)
+{
+    CSVRow row = make_csv_row(
+        { "1234.3", "234" },    // Fields
+        { "A", "B"}             // Column names
+    );
+
+    KRATOS_CHECK_EQUAL(row.to_json(), "{\"A\":1234.3,\"B\":234}");
+}
+
+KRATOS_TEST_CASE_IN_SUITE(CSVRowto_jsonMixed, KratosExternalLibrariesFastSuite)
+{
+    CSVRow row = make_csv_row(
+        { "1234.3", "234", "ABCD", "AB1", "1337" },     // Fields
+        { "A", "B", "C", "D", "E" }                     // Column names
+    );
+
+    // Full Row
+    {
+        KRATOS_CHECK_EQUAL(row.to_json(), "{\"A\":1234.3,\"B\":234,\"C\":\"ABCD\",\"D\":\"AB1\",\"E\":1337}");
+    }
+
+    // Subset
+    {
+        KRATOS_CHECK_EQUAL(row.to_json({ "B", "C" }), "{\"B\":234,\"C\":\"ABCD\"}");
+        KRATOS_CHECK_EQUAL(row.to_json({ "B", "A" }), "{\"B\":234,\"A\":1234.3}");
+    }
+}
+
+KRATOS_TEST_CASE_IN_SUITE(CSVRowto_json_arrayMixed, KratosExternalLibrariesFastSuite)
+{
+    CSVRow row = make_csv_row(
+        { "1234.3", "234", "ABCD", "AB1", "1337" },     // Fields
+        { "A", "B", "C", "D", "E" }                     // Column names
+    );
+
+    // Full Row
+    {
+        KRATOS_CHECK_EQUAL(row.to_json_array(), "[1234.3,234,\"ABCD\",\"AB1\",1337]");
+    }
+
+    // Subset
+    {
+        KRATOS_CHECK_EQUAL(row.to_json_array({ "B", "C" }), "[234,\"ABCD\"]");
+        KRATOS_CHECK_EQUAL(row.to_json_array({ "B", "A" }), "[234,1234.3]");
+    }
+}
+
+// Reported in: https://github.com/vincentlaucsb/csv-parser/issues/68
+KRATOS_TEST_CASE_IN_SUITE(CSVRowto_jsonwithWrongColumns, KratosExternalLibrariesFastSuite)
+{
+    std::string csv_string(R"(A,B,C,123,345,678,)");
+
+    auto format = csv::CSVFormat();
+    format.column_names({ "A", "B" });
+
+    csv::CSVReader reader(format);
+    reader.feed(csv_string);
+    reader.end_feed();
+
+    CSVRow first_row;
+    reader.read_row(first_row);
+
+    // Since the column names provided were wrong, there won't be any data.
+    // to_json() method should then produce an empty object instead of segfaulting.
+    KRATOS_CHECK_EQUAL(first_row.to_json(), "{}");
+    KRATOS_CHECK_EQUAL(first_row.to_json_array(), "[]");
+}
 
 } // namespace Testing.
 } // namespace Kratos.
