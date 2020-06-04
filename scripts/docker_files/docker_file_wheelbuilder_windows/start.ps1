417dc07a57f6b14af254678b26d1540ffbd12954
Hubert Balcerzak <hubertbalc@gmail.com>
2020-06-04 04:18:34 +0200

    python versions parameter on windows

--- scripts/docker_files/docker_file_wheelbuilder_windows/start.ps1
+++ scripts/docker_files/docker_file_wheelbuilder_windows/start.ps1
@@ -1,15 +1,16 @@
-param([string]$branch="master",
-      [string]$cotire="OFF")
+param([string]$branch = "master",
+    [string]$cotire = "OFF",
+    [string[]]$pythons = @("38", "37", "36", "35"))
 
-echo starting build for branch $branch
+Write-Host "starting build for branch $( $branch )"
 
 #Load development env
 cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
 
 Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
-  if ($_ -match "^(.*?)=(.*)$") {
-    Set-Content "env:\$($matches[1])" $matches[2]
-  }
+    if ($_ -match "^(.*?)=(.*)$") {
+        Set-Content "env:\$( $matches[1] )" $matches[2]
+    }
 }
 
 mkdir c:\kratos
@@ -17,4 +18,4 @@ cd c:\kratos
 git clone --depth 1 --single-branch --branch $branch https://github.com/KratosMultiphysics/Kratos.git
 
 cp c:\kratos\Kratos\scripts\wheels\windows\build.ps1 c:\scripts\build.ps1
-& "c:\scripts\build.ps1" -cotire $cotire
+& "c:\scripts\build.ps1" -cotire $cotire -pythons $pythons
--- scripts/wheels/windows/build.ps1
+++ scripts/wheels/windows/build.ps1
@@ -1,6 +1,6 @@
-param([System.String]$cotire="OFF")
+param([string]$cotire = "OFF",
+    [string[]] $pythons)
 
-$pythons = "38","37","36","35"
 $env:kratos_version = "7.0.3"
 
 $kratosRoot = "c:\kratos\kratos"
@@ -9,80 +9,81 @@ $wheelRoot = "c:\wheel"
 $wheelOutDir = "c:\out"
 
 function exec_build($python, $pythonPath) {
-    cmd.exe /c "call configure.bat $($pythonPath) $($kratosRoot) OFF"
-    cmake --build "$($kratosRoot)/build/Release" --target install -- /property:configuration=Release /p:Platform=x64
+    cmd.exe /c "call configure.bat $( $pythonPath ) $( $kratosRoot ) OFF"
+    cmake --build "$( $kratosRoot )/build/Release" --target install -- /property:configuration = Release /p:Platform = x64
 }
 
 function exec_build_cotire($python, $pythonPath) {
-    cmd.exe /c "call configure.bat $($pythonPath) $($kratosRoot) ON"
-    cmake --build "$($kratosRoot)/build/Release" --target all_unity -- /property:configuration=Release /p:Platform=x64
-    cmake --build "$($kratosRoot)/build/Release" --target install -- /property:configuration=Release /p:Platform=x64
+    cmd.exe /c "call configure.bat $( $pythonPath ) $( $kratosRoot ) ON"
+    cmake --build "$( $kratosRoot )/build/Release" --target all_unity -- /property:configuration = Release /p:Platform = x64
+    cmake --build "$( $kratosRoot )/build/Release" --target install -- /property:configuration = Release /p:Platform = x64
 }
 
-function build ($python, $pythonPath) {
+function build($python, $pythonPath) {
     cd $kratosRoot
-    cp "$($kratosRoot)\scripts\wheels\windows\configure.bat" .\configure.bat
+    cp "$( $kratosRoot )\scripts\wheels\windows\configure.bat" .\configure.bat
 
-    if($cotire -eq "ON"){
+    if ($cotire -eq "ON") {
         exec_build_cotire $python $pythonPath
-    }else {
+    }
+    else {
         exec_build $python $pythonPath
     }
 }
 
 function  setup_wheel_dir {
     cd $kratosRoot
-    mkdir c:\wheel
+    mkdir c\wheel
     cp scripts\wheels\setup.py c:\wheel\setup.py
     mkdir c:\wheel\KratosMultiphysics
     mkdir c:\wheel\KratosMultiphysics\.libs
 }
 
-function create_core_wheel ($pythonPath) {
+function create_core_wheel($pythonPath) {
     setup_wheel_dir
     cd $kratosRoot
-    cp bin\release\KratosMultiphysics\* "$($wheelRoot)\KratosMultiphysics"
-    cp scripts\wheels\windows\KratosMultiphysics.json "$($wheelRoot)\wheel.json"
+    cp bin\release\KratosMultiphysics\* "$( $wheelRoot )\KratosMultiphysics"
+    cp scripts\wheels\windows\KratosMultiphysics.json "$( $wheelRoot )\wheel.json"
 
 
-    cp scripts\wheels\__init__.py "$($wheelRoot)\KratosMultiphysics\__init__.py"
+    cp scripts\wheels\__init__.py "$( $wheelRoot )\KratosMultiphysics\__init__.py"
     cd $wheelRoot
     & $pythonPath setup.py bdist_wheel
-    cp "$($wheelRoot)\dist\*" $wheelOutDir
+    cp "$( $wheelRoot )\dist\*" $wheelOutDir
     cd c:\
     rm -r $wheelRoot
 }
 
-function create_application_wheel ($pythonPath, $app) {
+function create_application_wheel($pythonPath, $app) {
     setup_wheel_dir
-    cp "$($kratosRoot)\scripts\wheels\windows\applications\$($app)" c:\wheel\wheel.json
+    cp "$( $kratosRoot )\scripts\wheels\windows\applications\$( $app )" c\wheel\wheel.json
     cd $wheelRoot
     & $pythonPath setup.py bdist_wheel #pythonpath
-    cp "$($wheelRoot)\dist\*" $wheelOutDir
+    cp "$( $wheelRoot )\dist\*" $wheelOutDir
     cd c:\
     rm -r $wheelRoot
 }
 
-foreach ($python in $pythons){
-    Write-Host "Begining build for python $($python)"
+foreach ($python in $pythons) {
+    Write-Host "Begining build for python $( $python )"
     $env:python = $python
 
     cd $kratosRoot
     git clean -ffxd
-    $pythonPath = "$($env:pythonRoot)\$($python)\python.exe"
+    $pythonPath = "$( $env:pythonRoot )\$( $python )\python.exe"
 
     build $python $pythonPath
 
     Write-Host "Finished build"
-    Write-Host "Begining wheel construction for python $($python)"
+    Write-Host "Begining wheel construction for python $( $python )"
 
     create_core_wheel $pythonPath
 
-    $applications = Get-ChildItem "$($kratosRoot)\scripts\wheels\windows\applications"
+    $applications = Get-ChildItem "$( $kratosRoot )\scripts\wheels\windows\applications"
 
-    foreach($app in $applications) {
+    foreach ($app in $applications) {
         create_application_wheel $pythonPath $app
     }
 
-    Write-Host "Finished wheel construction for python $($python)"
+    Write-Host "Finished wheel construction for python $( $python )"
 }
