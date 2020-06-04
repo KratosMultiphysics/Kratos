e1dbf4dacda8f28a8238cc6bc0c59f133a5059b7
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-06-04 14:17:04 +1000

    make functions inline

--- applications/ParticleMechanicsApplication/custom_constitutive/johnson_cook_thermal_plastic_3D_law.hpp
+++ applications/ParticleMechanicsApplication/custom_constitutive/johnson_cook_thermal_plastic_3D_law.hpp
@@ -196,11 +196,11 @@ private:
         return trace;
     }
 
-    double GetSqrt32() { return 1.2247448713915900000; } //sqrt(3.0/2.0)
+    inline double GetSqrt32() { return 1.2247448713915900000; } //sqrt(3.0/2.0)
 
-    double GetSqrt23() { return 0.8164965809277260000; } //sqrt(2.0/3.0)
+    inline double GetSqrt23() { return 0.8164965809277260000; } //sqrt(2.0/3.0)
 
-    double GetSqrt6() { return 2.4494897427831800000; } //sqrt(6.0)
+    inline double GetSqrt6() { return 2.4494897427831800000; } //sqrt(6.0)
 
     friend class Serializer;
 
