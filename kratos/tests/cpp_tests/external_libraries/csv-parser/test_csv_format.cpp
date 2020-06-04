4129c65b43a88b33108f45f26c630926f25b04d0
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-04 14:46:53 +0200

    Activate test (V)

--- kratos/tests/cpp_tests/external_libraries/csv-parser/test_csv_format.cpp
+++ kratos/tests/cpp_tests/external_libraries/csv-parser/test_csv_format.cpp
@@ -30,63 +30,75 @@ static std::string err_preamble = "There should be no overlap between "
     "the quote character, the set of possible "
     "delimiters and the set of whitespace characters.";
 
-// // Assert that an error is thrown if whitespace, delimiter, and quote
-// KRATOS_TEST_CASE_IN_SUITE(CSVFormat-OverlappingCharacters, KratosExternalLibrariesFastSuite) {
-//     CSVFormat format;
-//     bool err_caught = false;
-//
-//     // Tab
-//     {
-//         try {
-//             format.delimiter('\t').quote('"').trim({ '\t' });
-//         }
-//         catch (std::runtime_error& err) {
-//             err_caught = true;
-//             KRATOS_CHECK(err.what() == std::string(err_preamble + " Offending characters: '\t'."));
-//         }
-//
-//         KRATOS_CHECK(err_caught);
-//     }
-//
-//     // Tab with multiple other characters 
-//     {
-//         try {
-//             format.delimiter({ ',', '\t' }).quote('"').trim({ ' ', '\t' });
-//         }
-//         catch (std::runtime_error& err) {
-//             err_caught = true;
-//             KRATOS_CHECK(err.what() == std::string(err_preamble + " Offending characters: '\t'."));
-//         }
-//
-//         KRATOS_CHECK(err_caught);
-//     }
-//
-//     // Repeated quote
-//     {
-//         try {
-//             format.delimiter({ ',', '"' }).quote('"').trim({ ' ', '\t' });
-//         }
-//         catch (std::runtime_error& err) {
-//             err_caught = true;
-//             KRATOS_CHECK(err.what() == std::string(err_preamble + " Offending characters: '\"'."));
-//         }
-//
-//         KRATOS_CHECK(err_caught);
-//     }
-//
-//     // Multiple offenders
-//     {
-//         try {
-//             format.delimiter({ ',', '\t', ' ' }).quote('"').trim({ ' ', '\t' });
-//         }
-//         catch (std::runtime_error& err) {
-//             err_caught = true;
-//             KRATOS_CHECK(err.what() == std::string(err_preamble + " Offending characters: '\t', ' '."));
-//         }
-//
-//         KRATOS_CHECK(err_caught);
-//     }
-// }
+/**
+ * Assert that an error is thrown if whitespace, delimiter, and quote
+ */
+KRATOS_TEST_CASE_IN_SUITE(CSVFormatOverlappingCharacters, KratosExternalLibrariesFastSuite)
+{
+    CSVFormat format_1;
+    bool err_caught = false;
+
+    // Tab
+    {
+        try {
+            format_1.delimiter('\t').quote('"').trim({ '\t' });
+        }
+        catch (std::runtime_error& err) {
+            err_caught = true;
+            KRATOS_CHECK_EQUAL(err.what(), std::string(err_preamble + " Offending characters: '\t'."));
+        }
+
+        KRATOS_CHECK(err_caught);
+    }
+
+    CSVFormat format_2;
+    err_caught = false;
+
+    // Tab with multiple other characters
+    {
+        try {
+            format_2.delimiter({ ',', '\t' }).quote('"').trim({ ' ', '\t' });
+        }
+        catch (std::runtime_error& err) {
+            err_caught = true;
+            KRATOS_CHECK_EQUAL(err.what(), std::string(err_preamble + " Offending characters: '\t'."));
+        }
+
+        KRATOS_CHECK(err_caught);
+    }
+
+    CSVFormat format_3;
+    err_caught = false;
+
+    // Repeated quote
+    {
+        try {
+            format_3.delimiter({ ',', '"' }).quote('"').trim({ ' ', '\t' });
+        }
+        catch (std::runtime_error& err) {
+            err_caught = true;
+            KRATOS_CHECK_EQUAL(err.what(), std::string(err_preamble + " Offending characters: '\"'."));
+        }
+
+        KRATOS_CHECK(err_caught);
+    }
+
+    CSVFormat format_4;
+    err_caught = false;
+
+    // Multiple offenders
+    {
+        try {
+            format_4.delimiter({ ',', '\t', ' ' }).quote('"').trim({ ' ', '\t' });
+        }
+        catch (std::runtime_error& err) {
+            err_caught = true;
+            KRATOS_CHECK_EQUAL(err.what(), std::string(err_preamble + " Offending characters: '\t', ' '."));
+        }
+
+        KRATOS_CHECK(err_caught);
+    }
+}
 
 } // namespace Testing.
 } // namespace Kratos.
