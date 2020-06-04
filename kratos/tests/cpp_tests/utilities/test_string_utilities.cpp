aeb87f74f1d80f89aaf48a782913d9bffb74b28f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-04 19:49:28 +0200

    Adding test

--- kratos/tests/cpp_tests/utilities/test_string_utilities.cpp
+++ kratos/tests/cpp_tests/utilities/test_string_utilities.cpp
@@ -25,5 +25,19 @@ KRATOS_TEST_CASE_IN_SUITE(ConvertCammelCaseToSnakeCase, KratosCoreFastSuite)
     KRATOS_CHECK_STRING_EQUAL(snake_case, "test_in_cammel_case");
 }
 
+KRATOS_TEST_CASE_IN_SUITE(ErasePartialString, KratosCoreFastSuite)
+{
+    const std::string text_with_stuff = "textwithstuff";
+    const std::string text = StringUtilities::ErasePartialString(text_with_stuff, "withstuff");
+    KRATOS_CHECK_STRING_EQUAL(text, "text");
+}
+
+KRATOS_TEST_CASE_IN_SUITE(RemoveWhiteSpaces, KratosCoreFastSuite)
+{
+    const std::string text_with_spaces = "textwithspaces                ";
+    const std::string text = StringUtilities::RemoveWhiteSpaces(text_with_spaces);
+    KRATOS_CHECK_STRING_EQUAL(text, "textwithspaces");
+}
+
 }   // namespace Testing
 }  // namespace Kratos.
