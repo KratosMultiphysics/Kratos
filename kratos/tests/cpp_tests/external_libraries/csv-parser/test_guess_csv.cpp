51102f67ad07329105d450e174b8adeec2f221d8
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-04 17:48:25 +0200

    Clear (I)

--- kratos/tests/cpp_tests/external_libraries/csv-parser/test_guess_csv.cpp
+++ kratos/tests/cpp_tests/external_libraries/csv-parser/test_guess_csv.cpp
@@ -21,6 +21,7 @@
 // Project includes
 #include "testing/testing.h"
 #include "includes/kratos_filesystem.h"
+#include "utilities/string_utilities.h"
 #include "csv-parser/include/csv.hpp"
 
 namespace Kratos {
@@ -31,28 +32,9 @@ using namespace csv;
 using std::vector;
 using std::string;
 
-/**
- * Erase First Occurrence of given  substring from main string.
- */
-std::string ErasePartialString(const std::string& rMainStrint, const std::string& rToErase)
-{
-    // Value to return
-    std::string sub_string = rMainStrint;
-
-    // Search for the substring in string
-    size_t pos = sub_string.find(rToErase);
-
-    if (pos != std::string::npos) {
-        // If found then erase it from string
-        sub_string.erase(pos, rToErase.length());
-    }
-
-    return sub_string;
-}
-
 KRATOS_TEST_CASE_IN_SUITE(guess_delimPipe, KratosExternalLibrariesFastSuite)
 {
-    const std::string working_dir = ErasePartialString(__FILE__, "test_guess_csv.cpp");
+    const std::string working_dir = StringUtilities::ErasePartialString(__FILE__, "test_guess_csv.cpp");
     CSVGuessResult format = guess_format(Kratos::FilesystemExtensions::JoinPaths({working_dir,"data/real_data/2009PowerStatus.txt"}));
     KRATOS_CHECK_EQUAL(format.delim, '|');
     KRATOS_CHECK_EQUAL(format.header_row, 0);
@@ -60,7 +42,7 @@ KRATOS_TEST_CASE_IN_SUITE(guess_delimPipe, KratosExternalLibrariesFastSuite)
 
 KRATOS_TEST_CASE_IN_SUITE(guess_delimSemiColon, KratosExternalLibrariesFastSuite)
 {
-    const std::string working_dir = ErasePartialString(__FILE__, "test_guess_csv.cpp");
+    const std::string working_dir = StringUtilities::ErasePartialString(__FILE__, "test_guess_csv.cpp");
     CSVGuessResult format = guess_format(Kratos::FilesystemExtensions::JoinPaths({working_dir,"data/real_data/YEAR07_CBSA_NAC3.txt"}));
     KRATOS_CHECK_EQUAL(format.delim, ';');
     KRATOS_CHECK_EQUAL(format.header_row, 0);
@@ -68,7 +50,7 @@ KRATOS_TEST_CASE_IN_SUITE(guess_delimSemiColon, KratosExternalLibrariesFastSuite
 
 KRATOS_TEST_CASE_IN_SUITE(guess_delimCSVwithComments, KratosExternalLibrariesFastSuite)
 {
-    const std::string working_dir = ErasePartialString(__FILE__, "test_guess_csv.cpp");
+    const std::string working_dir = StringUtilities::ErasePartialString(__FILE__, "test_guess_csv.cpp");
     CSVGuessResult format = guess_format(Kratos::FilesystemExtensions::JoinPaths({working_dir,"data/fake_data/ints_comments.csv"}));
     KRATOS_CHECK_EQUAL(format.delim, ',');
     KRATOS_CHECK_EQUAL(format.header_row, 5);
