d8d9dccbfe8d34135a3698dcf080ff5885bd2af4
Inigo Lopez <inigolcanalejo@gmail.com>
2020-06-08 18:15:57 +0200

    cleaning

--- applications/CoSimulationApplication/tests/test_tau_functions/test_tau_functions.py
+++ applications/CoSimulationApplication/tests/test_tau_functions/test_tau_functions.py
@@ -7,92 +7,17 @@ import os, subprocess
 
 class TestTauFunctions(KratosUnittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    # def test_RemoveFilesFromPreviousSimulations(self):
-    #     # Define test file
-    #     test_file = 'test_RemoveFilesFromPreviousSimulations.py'
-
-    #     # Run function
-    #     self.execute_test(test_file)
-
-    # def test_FindInterfaceFilename(self):
-    #     # Define test file
-    #     test_file = 'test_FindInterfaceFilename.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_SaveCoordinatesList(self):
-    #     # Define test file
-    #     test_file = 'test_SaveCoordinatesList.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_SavePressure(self):
-    #     # Define test file
-    #     test_file = 'test_SavePressure.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_GetCellNodeIds(self):
-    #     # Define test file
-    #     test_file = 'test_GetCellNodeIds.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_CalculateCellForce(self):
-    #     # Define test file
-    #     test_file = 'test_CalculateCellForce.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_FindPrimaryGridFilename(self):
-    #     # Define test file
-    #     test_file = 'test_FindPrimaryGridFilename.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_FindOutputFilename(self):
-    #     # Define test file
-    #     test_file = 'test_FindOutputFilename.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_ReadHeader(self):
-    #     # Define test file
-    #     test_file = 'test_ReadHeader.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
-    # def test_ReadNodalData(self):
-    #     # Define test file
-    #     test_file = 'test_ReadNodalData.py'
-
-    #     # Run test
-    #     self.execute_test(test_file)
-
     def test_tau_functions(self):
         # Define test file
         test_file = 'test_tau_functions_impl.py'
 
-        # Run test
-        self.execute_test(test_file)
-
-    def execute_test(self, test_file):
+        # Define command for subprocess
         full_command = ['python']
         full_command.extend([test_file])
 
         working_directory = os.path.dirname(os.path.abspath(__file__))
 
+        # Run test
         p = subprocess.Popen(full_command, cwd=working_directory)
         p.communicate()
 
--- applications/CoSimulationApplication/tests/test_tau_functions/test_tau_functions_impl.py
+++ applications/CoSimulationApplication/tests/test_tau_functions/test_tau_functions_impl.py
@@ -11,6 +11,25 @@ import tau_functions as TauFunctions
 
 class TestTauFunctionsImpl(unittest.TestCase):
 
+    def test_RemoveFilesFromPreviousSimulations(self):
+        self.createOutputsDirectory()
+        os.mkdir('Mesh')
+
+        # Create dummy empty output files
+        for i in range(3):
+            output_file_name = 'Outputs/test_file_' + str(i)
+            open(output_file_name,'w').close()
+
+            mesh_file_name = 'Mesh/airfoil_Structured_scaliert.grid.def' + str(i)
+            open(mesh_file_name,'w').close()
+
+        TauFunctions.RemoveFilesFromPreviousSimulations()
+
+        # Check if files have been successfully removed
+        os.rmdir('Outputs')
+        os.rmdir('Mesh')
+
+
     def test_ReadInterfaceFile(self):
         self.setReference()
         self.createDummyInterfaceFile()
@@ -28,7 +47,157 @@ class TestTauFunctionsImpl(unittest.TestCase):
         # Remove interface file
         os.remove(self.interface_filename)
 
+
+    def test_FindInterfaceFilename(self):
+        self.createOutputsDirectory()
+        self.createInterfaceFile()
+
+        # Retrive the file
+        file_name = TauFunctions.FindInterfaceFilename(self.path, self.step)
+
+        # Check
+        self.assertMultiLineEqual(file_name, self.reference_interface_file_name)
+
+        # Remove dummy file and directory
+        TauFunctions.RemoveFilesFromPreviousSimulations()
+        os.rmdir('Outputs')
+
+
+    def test_SaveCoordinatesList(self):
+        self.setReference()
+
+        # Save coordinates list
+        X, Y, Z = TauFunctions.SaveCoordinatesList(self.reference_nodal_data, self.reference_position_info, self.reference_mesh_info[0])
+
+        # Check
+        self.assertListEqual(X, self.reference_X)
+        self.assertListEqual(Y, self.reference_Y)
+        self.assertListEqual(Z, self.reference_Z)
+
+
+    def test_SavePressure(self):
+        self.setReference()
+
+        # Save pressure
+        nodal_pressure = TauFunctions.SavePressure(self.reference_nodal_data, self.reference_position_info, self.reference_mesh_info[0], 20.0)
+
+        # Define reference nodal_pressure
+        reference_nodal_pressure = np.array([336.0, 816.0, 1056.0, 1728.0, 1440.0, 1944.0])
+
+        # Check
+        np.testing.assert_almost_equal(nodal_pressure, reference_nodal_pressure, decimal=16)
+
+
+    def test_GetCellNodeIds(self):
+        # Define elem_connectivities
+        elem_connectivities = np.array([24, 35, 87, 94], dtype=int)
+        cell = 0
+
+        # Compute the normal vector
+        node_ids = TauFunctions.GetCellNodeIds(elem_connectivities, cell)
+
+        # Check
+        np.testing.assert_almost_equal(node_ids, elem_connectivities - 1, decimal=16)
+
+
+    def test_CalculateCellForce(self):
+        X, Y, Z, node_ids = self.create_dummy_cell()
+
+        # Define nodal pressure
+        nodal_pressures = [1.4, 3.6, 5.7, 7.9]
+
+        # Compute the normal vector
+        cell_force = TauFunctions.CalculateCellForce(node_ids, nodal_pressures, X, Y, Z)
+
+        # Define reference normal
+        reference_cell_force = np.array([-4.65, 0.0, 4.65])
+
+        # Check
+        np.testing.assert_almost_equal(cell_force, reference_cell_force, decimal=16)
+
+
+    def test_FindPrimaryGridFilename(self):
+        # Create dummy file
+        os.mkdir('Mesh')
+        path = os.getcwd() + '/'
+        pattern = 'airfoil_Structured_scaliert.grid'
+        reference_file_name = path + 'Mesh/' + pattern
+        open(reference_file_name, 'w').close()
+
+        # Retrive the file
+        step = 200
+        start_step = step
+        file_name = TauFunctions.FindPrimaryGridFilename(path, step, start_step)
+
+        # Check
+        self.assertMultiLineEqual(file_name, reference_file_name)
+
+        # Remove dummy file and directory
+        os.remove(file_name)
+        os.rmdir('Mesh')
+
+
+    def test_FindOutputFilename(self):
+        self.createOutputsDirectory()
+        self.createInterfaceFile()
+
+        # Retrive the file
+        file_name = TauFunctions.FindOutputFilename(self.path, self.step)
+
+        # Check
+        self.assertMultiLineEqual(file_name, self.reference_file_name)
+
+        # Remove dummy file and directory
+        TauFunctions.RemoveFilesFromPreviousSimulations()
+        os.rmdir('Outputs')
+
+
+    def test_ReadHeader(self):
+        # Set reference
+        self.reference_position_info = ['VARIABLES', '=', '"x"', '"y"', '"z"', '"density"', '"cp"']
+        self.reference_mesh_info = [6, 2]
+
+        # Create dummy interface file
+        self.interface_filename = 'dummy_header_file.dat'
+        with open(self.interface_filename, 'w') as interface_file:
+            self.WriteHeader(interface_file)
+
+        # Read interface file
+        with open(self.interface_filename, 'r') as interface_file:
+            line = interface_file.readline()
+            position_info, mesh_info, line = TauFunctions.ReadHeader(interface_file, line)
+
+        # Check
+        self.assertListEqual(position_info, self.reference_position_info)
+        self.assertListEqual(mesh_info, self.reference_mesh_info)
+
+        # Remove interface file
+        os.remove(self.interface_filename)
+
+
+    def test_ReadNodalData(self):
+        # Set reference
+        self.setReference()
+
+        # Create dummy interface file
+        self.interface_filename = 'dummy_header_file.dat'
+        with open(self.interface_filename, 'w') as interface_file:
+            self.WriteNodalData(interface_file)
+
+        # Read interface file
+        with open(self.interface_filename, 'r') as interface_file:
+            line = interface_file.readline()
+            nodal_data, line = TauFunctions.ReadNodalData(interface_file, line)
+
+        # Check
+        self.assertListEqual(nodal_data, self.reference_nodal_data)
+
+        # Remove interface file
+        os.remove(self.interface_filename)
+
+
     def test_ReadElementConnectivities(self):
+        # Set reference
         number_of_elements = 2
         self.reference_elem_connectivities = np.array([1, 2, 5, 6, 2, 3, 4, 5], dtype=int)
 
@@ -49,20 +218,110 @@ class TestTauFunctionsImpl(unittest.TestCase):
         # Remove interface file
         os.remove(self.interface_filename)
 
+
+    def test_CalculateCellPressure(self):
+        # Define nodal pressure and node ids
+        nodal_pressures = [1.4, 3.6, 5.7, 7.9]
+        node_ids = np.array([0, 1, 2, 3], dtype=int)
+
+        # Compute the normal vector
+        cell_pressure = TauFunctions.CalculateCellPressure(nodal_pressures, node_ids)
+
+        # Define reference normal
+        reference_pressure = 4.65
+
+        # Check
+        np.testing.assert_almost_equal(cell_pressure, reference_pressure, decimal=16)
+
+
+    def test_CalculateCellArea(self):
+        X, Y, Z, node_ids = self.create_dummy_cell()
+
+        # Compute the normal vector
+        cell_area = TauFunctions.CalculateCellArea(X, Y, Z, node_ids)
+
+        # Define reference normal
+        reference_area = 1.4142135623730951
+
+        # Check
+        np.testing.assert_almost_equal(cell_area, reference_area, decimal=16)
+
+
+    def test_CalculateCellNormal(self):
+        X, Y, Z, node_ids = self.create_dummy_cell()
+
+        # Compute the normal vector
+        cell_normal = TauFunctions.CalculateCellNormal(X, Y, Z, node_ids)
+
+        # Define reference normal
+        reference_normal = np.array([-0.7071067811865475, 0.0, 0.7071067811865475])
+
+        # Check
+        np.testing.assert_almost_equal(cell_normal, reference_normal, decimal=16)
+
+
+    def test_FindInitialMeshFilename(self):
+        # Create dummy file
+        path = os.getcwd() + '/'
+        pattern = 'airfoil_Structured_scaliert.grid'
+        reference_file_name = path + pattern
+        open(reference_file_name, 'w').close()
+
+        # Retrive the file
+        file_name = TauFunctions.FindInitialMeshFilename(path, pattern)
+
+        # Check
+        self.assertMultiLineEqual(file_name, reference_file_name)
+
+        # Remove dummy file
+        os.remove(file_name)
+
+
+    def test_FindFilename(self):
+        self.createOutputsDirectory()
+        self.createInterfaceFile()
+
+        # Retrive the file
+        file_name = TauFunctions.FindFilename(self.path + 'Outputs/', self.pattern, self.step+1)
+
+        # Check
+        self.assertMultiLineEqual(file_name, self.reference_file_name)
+
+        # Remove dummy file and directory
+        TauFunctions.RemoveFilesFromPreviousSimulations()
+        os.rmdir('Outputs')
+
+
+    def test_CalculateDistanceVector(self):
+        # Populate two nodes with random coordinates
+        X = [1.6, 19.1]
+        Y = [-0.3, 3.6]
+        Z = [4.36, -6.78]
+
+        # Compute the distance vector
+        distance = TauFunctions.CalculateDistanceVector(X, Y, Z, 0, 1)
+
+        # Define reference distance
+        reference_distance = np.array([17.5, 3.9, -11.14])
+
+        # Check
+        np.testing.assert_almost_equal(reference_distance, distance, decimal=16)
+
+
     def setReference(self):
-        self.reference_position_info = ['VARIABLES', '=', '"x"', '"y"', '"z"', '"density"', '"pressure"']
+        self.reference_position_info = ['VARIABLES', '=', '"x"', '"y"', '"z"', '"density"', '"cp"']
         self.reference_mesh_info = [6, 2]
-        reference_X = [0.0, 1.0, 2.0, 2.0, 1.0, 0.0]
-        reference_Y = [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]
-        reference_Z = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.reference_X = [0.0, 1.0, 2.0, 2.0, 1.0, 0.0]
+        self.reference_Y = [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]
+        self.reference_Z = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         reference_density = [1.2, 1.2, 1.2, 1.2, 1.2, 1.2]
-        reference_pressure = [3.4, 3.4, 3.4, 3.4, 3.4, 3.4]
+        reference_cp = [1.4, 3.4, 4.4, 7.2, 6.0, 8.1]
         self.reference_nodal_data = []
-        self.reference_nodal_data.extend(reference_X)
-        self.reference_nodal_data.extend(reference_Y)
-        self.reference_nodal_data.extend(reference_Z)
+        self.reference_nodal_data.extend(self.reference_X)
+        self.reference_nodal_data.extend(self.reference_Y)
+        self.reference_nodal_data.extend(self.reference_Z)
         self.reference_nodal_data.extend(reference_density)
-        self.reference_nodal_data.extend(reference_pressure)
+        self.reference_nodal_data.extend(reference_cp)
         self.reference_elem_connectivities = np.array([1, 2, 5, 6, 2, 3, 4, 5], dtype=int)
 
 
@@ -73,12 +332,11 @@ class TestTauFunctionsImpl(unittest.TestCase):
             self.WriteNodalData(interface_file)
             self.WriteElementConnectivities(interface_file)
 
-
     def WriteHeader(self, interface_file):
         title = 'TITLE     = '
         title += '"Grid: /path/to/Mesh/airfoil_Structured_scaliert.grid, '
         title += 'Pointdata: path/to/Outputs/airfoilSol.pval.unsteady_i=201_t=1.00500e+00"'
-        variables = 'VARIABLES = "x" "y" "z" "density" "pressure"'
+        variables = 'VARIABLES = "x" "y" "z" "density" "cp"'
         zone = 'ZONE T="MEMBRANE"'
         mesh_info = 'N=6, E=2, F=FEBLOCK ET=Quadrilateral'
         interface_file_lines = [title, variables, zone, mesh_info]
@@ -104,43 +362,30 @@ class TestTauFunctionsImpl(unittest.TestCase):
             else:
                 interface_file.write(' ')
 
-    def test_CalculateCellPressure(self):
-        # Define nodal pressure and node ids
-        nodal_pressures = [1.4, 3.6, 5.7, 7.9]
-        node_ids = np.array([0, 1, 2, 3], dtype=int)
 
-        # Compute the normal vector
-        cell_pressure = TauFunctions.CalculateCellPressure(nodal_pressures, node_ids)
+    def createOutputsDirectory(self):
+        # Remove files from previous tests if they exist
+        TauFunctions.RemoveFilesFromPreviousSimulations()
+        path = os.getcwd() + '/'
+        if os.path.exists(path + "Outputs"):
+            os.rmdir(path + "Outputs")
 
-        # Define reference normal
-        reference_pressure = 4.65
+        # Create outputs directory
+        os.mkdir('Outputs')
 
-        # Check
-        np.testing.assert_almost_equal(cell_pressure, reference_pressure, decimal=16)
 
-    def test_CalculateCellArea(self):
-        X, Y, Z, node_ids = self.create_dummy_cell()
+    def createInterfaceFile(self):
+        # Define file names
+        self.path = os.getcwd() + '/'
+        self.pattern = 'airfoilSol.pval.unsteady_i='
+        self.step = 304
+        self.reference_file_name = self.path + 'Outputs/' + self.pattern + str(self.step + 1)
+        self.reference_interface_file_name = self.reference_file_name.replace('pval', 'surface.pval') + '.dat'
 
-        # Compute the normal vector
-        cell_area = TauFunctions.CalculateCellArea(X, Y, Z, node_ids)
+        # Create dummy files
+        open(self.reference_file_name, 'w').close()
+        open(self.reference_interface_file_name, 'w').close()
 
-        # Define reference normal
-        reference_area = 1.4142135623730951
-
-        # Check
-        np.testing.assert_almost_equal(cell_area, reference_area, decimal=16)
-
-    def test_CalculateCellNormal(self):
-        X, Y, Z, node_ids = self.create_dummy_cell()
-
-        # Compute the normal vector
-        cell_normal = TauFunctions.CalculateCellNormal(X, Y, Z, node_ids)
-
-        # Define reference normal
-        reference_normal = np.array([-0.7071067811865475, 0.0, 0.7071067811865475])
-
-        # Check
-        np.testing.assert_almost_equal(cell_normal, reference_normal, decimal=16)
 
     def create_dummy_cell(self):
         # Define cell geometry
@@ -151,54 +396,6 @@ class TestTauFunctionsImpl(unittest.TestCase):
         node_ids = np.array([0, 1, 2, 3], dtype=int)
 
         return X, Y, Z, node_ids
-
-    def test_FindInitialMeshFilename(self):
-        # Create dummy file
-        path = os.getcwd() + '/'
-        pattern = 'airfoil_Structured_scaliert.grid'
-        reference_file_name = path + pattern
-        open(reference_file_name, 'w').close()
-
-        # Retrive the file
-        file_name = TauFunctions.FindInitialMeshFilename(path, pattern)
-
-        # Check
-        self.assertMultiLineEqual(file_name, reference_file_name)
-
-        # Remove dummy file
-        os.remove(file_name)
-
-    def test_FindFilename(self):
-        # Create dummy file
-        path = os.getcwd() + '/'
-        pattern = 'airfoilSol.pval.unsteady_i='
-        step = 304
-        reference_file_name = path + pattern + str(step)
-        open(reference_file_name, 'w').close()
-
-        # Retrive the file
-        file_name = TauFunctions.FindFilename(path, pattern, step)
-
-        # Check
-        self.assertMultiLineEqual(file_name, reference_file_name)
-
-        # Remove dummy file
-        os.remove(file_name)
-
-    def test_CalculateDistanceVector(self):
-        # Populate two nodes with random coordinates
-        X = [1.6, 19.1]
-        Y = [-0.3, 3.6]
-        Z = [4.36, -6.78]
-
-        # Compute the distance vector
-        distance = TauFunctions.CalculateDistanceVector(X, Y, Z, 0, 1)
-
-        # Define reference distance
-        reference_distance = np.array([17.5, 3.9, -11.14])
-
-        # Check
-        np.testing.assert_almost_equal(reference_distance, distance, decimal=16)
 
 
 
