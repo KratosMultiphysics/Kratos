49acf0b358accd4908e299b6f655fffff31df0a1
riccardo <rrossi@cimne.upc.edu>
2020-06-12 08:18:41 +0200

    adding comments

--- kratos/utilities/atomic_utilities.h
+++ kratos/utilities/atomic_utilities.h
@@ -27,32 +27,60 @@
 namespace Kratos
 {
 ///@addtogroup KratosCore
+/**
+ * collection of utilities for atomic updates of simple types. (essentially mimics the omp atomic)
+ */
+
+/** @param target variable being atomically updated by doing target += value
+ * @param value value being added
+ */ 
 template<class TDataType>
 inline void AtomicAdd(TDataType& target, const TDataType& value ) {
     #pragma omp atomic
     target += value;
 }
 
+/** @param target vector variable being atomically updated by doing target += value
+ * @param value vector value being added
+ * Note that the update is not really atomic, but rather is done component by component
+ */ 
 template<class TVectorType1, class TVectorType2>
 inline void AtomicAdd(TVectorType1& target, const TVectorType2& value ) {
+
+    KRATOS_DEBUG_ERROR_IF(target.size() != value.size()) << "vector size mismatch in vector AtomicAdd- Sizes are: " 
+        << target.size() << " for target and " << value.size() << " for value " <<std::endl;
     for(unsigned int i=0: i<target.size(); ++i){
        AtomicAdd(target[i], value[i];)
     }
 }
 
+/** @param target vector variable being atomically updated by doing target -= value
+ * @param value vector value being subtracted
+ * Note that the update is not really atomic, but rather is done component by component
+ */ 
 template<class TDataType>
-inline void AtomicAdd(TDataType& target, const TDataType& value ) {
+inline void AtomicSub(TDataType& target, const TDataType& value ) {
     #pragma omp atomic
-    target += value;
+    target -= value;
 }
 
+/** @param target vector variable being atomically updated by doing target -= value
+ * @param value vector value being subtracted
+ * Note that the update is not really atomic, but rather is done component by component
+ */ 
 template<class TVectorType1, class TVectorType2>
 inline void AtomicSub(TVectorType1& target, const TVectorType2& value ) {
+    KRATOS_DEBUG_ERROR_IF(target.size() != value.size()) << "vector size mismatch in vector AtomicSub- Sizes are: " 
+        << target.size() << " for target and " << value.size() << " for value " <<std::endl;
     for(unsigned int i=0: i<target.size(); ++i){
        AtomicSub(target[i], value[i];)
     }
 }
 
+/** @param target variable being atomically updated by doing target = value
+ * @param value valuev to which the target is set
+ * KLUDGE: might not be supported by all compilers even though the openmp standard does support it
+ */ 
 template<class TDataType>
 inline void AtomicAssign(TDataType& target, const TDataType& value) {
     #pragma omp atomic write
