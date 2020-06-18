e060ec15f05e34a79e904adcf30bb39852040cd2
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-06-19 09:43:50 +1000

    clean up test to only use modeler

--- applications/MappingApplication/test_coupling_geometry_mapper/run_test.py
+++ applications/MappingApplication/test_coupling_geometry_mapper/run_test.py
@@ -33,103 +33,86 @@ destinationInterface.AddNodes([6])
 destinationInterface.AddNodes([3])
 destinationInterface.AddNodes([1])
 
-is_use_modeler = False #for debugging, update the one in the parameters below too
 mapper_params = KM.Parameters("""{
     "mapper_type": "coupling_geometry",
     "echo_level" : 0,
     "dual_mortar": true,
     "consistency_scaling" : false,
-    "is_use_modeler" : false,
     "modeler_name" : "MappingGeometriesModeler",
     "modeler_parameters":{
         "origin_model_part_name" : "origin",
-        "destination_model_part_name" : "destination"
+        "destination_model_part_name" : "destination",
+        "is_interface_sub_model_parts_specified" : true,
+        "origin_interface_sub_model_part_name" : "interface",
+        "destination_interface_sub_model_part_name" : "interface"
     }
 }""")
 
-
+# ======== Left in here because this is a temporary technique to construct coupling geoms =======
 # Create dummy line conditions on submodel parts to couple together
 originProps = originInterface.GetProperties()[1]
 nodeOffset = 327
 for conditionIndex in range(1,6):
     cNode = nodeOffset + conditionIndex
     originInterface.CreateNewCondition("LineCondition2D2N", conditionIndex+100, [cNode,cNode+1], originProps)
-
 destProps = destinationInterface.GetProperties()[1]
 destinationInterface.CreateNewCondition("LineCondition2D2N", 101, [1,3], destProps)
 destinationInterface.CreateNewCondition("LineCondition2D2N", 102, [3,6], destProps)
 destinationInterface.CreateNewCondition("LineCondition2D2N", 103, [6,10], destProps)
+# ===============================================================================================
 
 
-if is_use_modeler == False:
-    # TODO, add some logic to call the correct intersection functions. that would depend on the dimension of the coupling interfaces
-    model_part_origin.CreateSubModelPart("coupling")
-    model_part_coupling = model_part_origin.GetSubModelPart("coupling")
-    KratosMapping.FindIntersection1DGeometries2D(originInterface, destinationInterface, model_part_coupling, 1e-6)
-    KratosMapping.CreateQuadraturePointsCoupling1DGeometries2D(model_part_coupling, 1e-6)
-    # TODO, the above problem need args changed to just origin_model_part and destination_model_part
 
 mapper = KratosMapping.MapperFactory.CreateMapper(model_part_origin, model_part_destination, mapper_params)
-
-# Plan ---------------------------------------
-#mapper = KratosMapping.MapperFactory.CreateMapper(current_model, mapper_params)
-#   (this creates a mapper modeller)
-#   modeller.CreateModelPart   (this creates model_part_coupling)
-#       DetermineIntersectingGeometries()
-#       CreateCouplingGeometries()
-#mapper.CreateMappingMatrix()
-#
+#mapper = KratosMapping.MapperFactory.CreateMapper(current_model, mapper_params) # TODO @phil could you add this to the mapper_factory please
 
 
+
+
+
+
+# Small test. The energy error should be zero
 prescribed_displacement = 2.5
 prescribed_force = 3.0
-print("\nMapping uniform displacement ",prescribed_displacement," from origin to destination")
+#print("\nMapping uniform displacement ",prescribed_displacement," from origin to destination")
 for node in originInterface.Nodes:
     node.SetSolutionStepValue(KM.DISPLACEMENT_X, prescribed_displacement)
 
-
 mapper.Map(KM.DISPLACEMENT, KM.DISPLACEMENT)
-print("destination interface nodes displacement values")
-for node in destinationInterface.Nodes:
-    print("\t",node.Id," displacement = ",node.GetSolutionStepValue(KM.DISPLACEMENT_X))
-    #self.assertAlmostEqual(node.GetSolutionStepValue(KM.DISPLACEMENT_X),prescribed_displacement)
+#print("destination interface nodes displacement values")
+#for node in destinationInterface.Nodes:
+#    print("\t",node.Id," displacement = ",node.GetSolutionStepValue(KM.DISPLACEMENT_X))
+#    self.assertAlmostEqual(node.GetSolutionStepValue(KM.DISPLACEMENT_X),prescribed_displacement)
 
-
-# clear values
-#for node in model_part_origin.Nodes:
-#    node.SetSolutionStepValue(KM.DISPLACEMENT_X, 0.0)
-
-print("\n\nInverse mapping from destination to origin")
+#print("\n\nInverse mapping from destination to origin")
 for node in destinationInterface.Nodes:
     node.SetSolutionStepValue(KM.FORCE_X, prescribed_force)
     if node.Id == 1 or node.Id == 10:
         node.SetSolutionStepValue(KM.FORCE_X, prescribed_force/2.0)
-    print("\t",node.Id," displacement = ",node.GetSolutionStepValue(KM.FORCE_X))
-    print("\t",node.Id," Y position = ",node.Y)
+    #print("\t",node.Id," displacement = ",node.GetSolutionStepValue(KM.FORCE_X))
+    #print("\t",node.Id," Y position = ",node.Y)
 
 mapper.InverseMap(KM.FORCE, KM.FORCE, KratosMapping.Mapper.USE_TRANSPOSE)
-print("destination interface nodes displacement values")
+#print("destination interface nodes displacement values")
 
+#for node in originInterface.Nodes:
+#    print("\t",node.Id," forces = ",node.GetSolutionStepValue(KM.FORCE_X))
+#    print("\t",node.Id," forces = ",node.Y)
 
-for node in originInterface.Nodes:
-    print("\t",node.Id," forces = ",node.GetSolutionStepValue(KM.FORCE_X))
-    print("\t",node.Id," forces = ",node.Y)
-
-
-print("\n\nCHECKING ENERGY BALANCE")
+#print("\n\nCHECKING ENERGY BALANCE")
 energy_origin = 0.0
-print("Origin part")
+#print("Origin part")
 for node in originInterface.Nodes:
-    print("\t",node.Id,"\n\t\tforces = ",node.GetSolutionStepValue(KM.FORCE_X),"\n\t\tdisp = ",node.GetSolutionStepValue(KM.DISPLACEMENT_X))
+    #print("\t",node.Id,"\n\t\tforces = ",node.GetSolutionStepValue(KM.FORCE_X),"\n\t\tdisp = ",node.GetSolutionStepValue(KM.DISPLACEMENT_X))
     energy_origin += node.GetSolutionStepValue(KM.FORCE_X) * node.GetSolutionStepValue(KM.DISPLACEMENT_X)
 
 energy_dest = 0.0
-print("Destination part")
+#print("Destination part")
 for node in destinationInterface.Nodes:
-    print("\t",node.Id,"\n\t\tforces = ",node.GetSolutionStepValue(KM.FORCE_X),"\n\t\tdisp = ",node.GetSolutionStepValue(KM.DISPLACEMENT_X))
+    #print("\t",node.Id,"\n\t\tforces = ",node.GetSolutionStepValue(KM.FORCE_X),"\n\t\tdisp = ",node.GetSolutionStepValue(KM.DISPLACEMENT_X))
     energy_dest += node.GetSolutionStepValue(KM.FORCE_X) * node.GetSolutionStepValue(KM.DISPLACEMENT_X)
 
-print(energy_origin)
-print(energy_dest)
-print("ebergy balance = ",energy_origin-energy_dest)
+print("Energy origin = ",energy_origin)
+print("Energy destination = ",energy_dest)
+print("Energy error = ",energy_origin-energy_dest)
 #self.assertAlmostEqual(energy_origin, energy_dest)
