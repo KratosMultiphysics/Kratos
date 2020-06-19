2339b9013f77f7abbc210a486c935aaf8c57ca2c
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-06-19 12:40:30 +0200

    [StatApp] Getting ready statistics application for CI integration (#7094)
    * add statistics application
    
    * add stat app to windows ci
    
    * adding to nightly build
    
    * removing extra braces
    
    * change typename to class
    
    * adding default destructor
    
    * casting to double
    
    * removing atomic update directive
    
    * changing to auto
    
    * changing to unsigned int
    
    * moving to nightly tests
    
    * adding kratos_api
    
    * removing mpi tests from default tests
    
    * temporarily remove other apps from windows CI
    
    * temporariliy remove other apps from CI nightly
    
    * temporarily remove other apps from linux ci
    
    * adding additional kratos_api calls
    
    * reactivating linux CI apps
    
    * windows build with CI try - 1
    
    * windows build with CI try - 2
    
    * windows build with CI try - 3
    
    * windows build with CI try - 4
    
    * windows build with CI try - 5 - moving template instantiaion to first
    
    * windows build with CI try - 6
    
    * windows build with CI try - 7
    
    * Missing api
    
    * Typos + moving instantiation below impl
    
    * Not needed instantiations
    
    * reactivating windows CI apps
    
    * reactivating windows nighly build apps
    
    * minor
    
    * moving tests
    
    * remove from CI
    
    * remove trailing space
    
    * remove trailing spaces
    
    Co-authored-by: Carlos <roigcarlo@gmail.com>

--- applications/StatisticsApplication/custom_methods/temporal_max_method.h
+++ applications/StatisticsApplication/custom_methods/temporal_max_method.h
@@ -38,22 +38,23 @@ namespace Kratos
 
 namespace TemporalMethods
 {
-template <typename TContainerType, typename TContainerItemType, template <typename T> typename TDataRetrievalFunctor, template <typename T> typename TDataStorageFunctor>
+template <class TContainerType, class TContainerItemType, template <class T> class TDataRetrievalFunctor, template <class T> class TDataStorageFunctor>
 class TemporalMaxMethod
 {
 public:
-    template <typename TDataType>
+    template <class TDataType>
     class NormMethod : public TemporalMethod
     {
     public:
         KRATOS_CLASS_POINTER_DEFINITION(NormMethod);
 
-        NormMethod(ModelPart& rModelPart,
-                   const std::string& rNormType,
-                   const Variable<TDataType>& rInputVariable,
-                   const int EchoLevel,
-                   const Variable<double>& rOutputVariable,
-                   const Variable<double>& rMaxTimeValueVariable)
+        NormMethod(
+            ModelPart& rModelPart,
+            const std::string& rNormType,
+            const Variable<TDataType>& rInputVariable,
+            const int EchoLevel,
+            const Variable<double>& rOutputVariable,
+            const Variable<double>& rMaxTimeValueVariable)
             : TemporalMethod(rModelPart, EchoLevel),
               mNormType(rNormType),
               mrInputVariable(rInputVariable),
@@ -118,8 +119,8 @@ public:
 
             auto& initializer_method =
                 TemporalMethodUtilities::InitializeVariables<TContainerType, TContainerItemType, TDataStorageFunctor>;
-            initializer_method(r_container, mrOutputVariable,
-                               std::numeric_limits<double>::lowest());
+            initializer_method(
+                r_container, mrOutputVariable, std::numeric_limits<double>::lowest());
             initializer_method(r_container, mrMaxTimeValueVariable, 0.0);
 
             KRATOS_INFO_IF("TemporalNormMaxMethod", this->GetEchoLevel() > 0)
