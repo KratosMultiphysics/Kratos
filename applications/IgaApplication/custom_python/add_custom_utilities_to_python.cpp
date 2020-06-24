045cb640f43d840c2cf18b3721ed572f2e32c68e
rickyaristio <ricky.aristio@gmail.com>
2020-06-24 14:53:18 +0200

    add iga_flags attributes

--- applications/IgaApplication/custom_python/add_custom_utilities_to_python.cpp
+++ applications/IgaApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -19,6 +19,7 @@
 
 #include "iga_application_variables.h"
 
+#include "custom_utilities/iga_flags.h"
 
 namespace Kratos {
 namespace Python {
@@ -27,6 +28,17 @@ void AddCustomUtilitiesToPython(
     pybind11::module& m)
 {
 
+    using namespace pybind11::literals;
+    using namespace pybind11;
+    
+    class_< IgaFlags > iga_flags = class_< IgaFlags >(m, "IgaFlags").def(init<>());
+
+    iga_flags.attr("FIX_DISPLACEMENT_X") = IgaFlags::FIX_DISPLACEMENT_X;
+    iga_flags.attr("FIX_DISPLACEMENT_Y") = IgaFlags::FIX_DISPLACEMENT_Y;
+    iga_flags.attr("FIX_DISPLACEMENT_Z") = IgaFlags::FIX_DISPLACEMENT_Z;
+    iga_flags.attr("FIX_ROTATION_X") = IgaFlags::FIX_ROTATION_X;
+    iga_flags.attr("FIX_ROTATION_Y") = IgaFlags::FIX_ROTATION_Y;
+    iga_flags.attr("FIX_ROTATION_Z") = IgaFlags::FIX_ROTATION_Z;
 }
 
 } // namespace Python
