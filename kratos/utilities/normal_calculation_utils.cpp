843392104a52ee1d2de9c4a692cc96a26b25abd4
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-06-25 12:59:23 +0300

    Fix compilation

--- kratos/utilities/normal_calculation_utils.cpp
+++ kratos/utilities/normal_calculation_utils.cpp
@@ -418,4 +418,14 @@ void NormalCalculationUtils::CalculateNormal3D(
     itCond->SetValue(NORMAL, rAn);
 }
 
+/***********************************************************************************/
+/***********************************************************************************/
+
+template KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::InitializeNormals<Condition>(ModelPart& rModelPart);
+template KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::InitializeNormals<Element>(ModelPart& rModelPart);
+template KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::CalculateNormals<Condition>(ModelPart& rModelPart, const bool EnforceGenericAlgorithm);
+template KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::CalculateNormals<Element>(ModelPart& rModelPart, const bool EnforceGenericAlgorithm);
+template KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::CalculateUnitNormals<Condition>(ModelPart& rModelPart, const bool EnforceGenericAlgorithm);
+template KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::CalculateUnitNormals<Element>(ModelPart& rModelPart, const bool EnforceGenericAlgorithm);
+
 } // namespace Kratos
