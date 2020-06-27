bbe94f6caf57420fd5744601bd33634f73c379d3
sunethwarna <suneth.warna@gmail.com>
2020-06-27 06:44:50 +0200

    adding distribute method to variable_utils

--- kratos/utilities/variable_utils.h
+++ kratos/utilities/variable_utils.h
@@ -505,6 +505,30 @@ public:
     }
 
     /**
+     * @brief Distributes variable values in TContainerType container to nodes
+     *
+     * This method distributes variables values stored in TContainerType data value container in rModelPart
+     * to nodes. Constant weighting is used for each node based on rWeightVariable value. The result
+     * is stored in nodal non-historical data value container under the same rVariable. If IsInverseWeightProvided
+     * is true, then the weights provided by rWeightVariable is inverted to get nodal weight. Otherwise, the value
+     * given by rWeightVariable is used as weight.
+     *
+     *
+     * @tparam TDataType               Data type
+     * @tparam TContainerType          ContainerType of model part
+     * @param rModelPart               Model part
+     * @param rVariable                Variable to be distributed
+     * @param rWeightVariable          Variable which holds weight to distribute entity values to nodes
+     * @param IsInverseWeightProvided  Whether the weight is provided as inverse or not.
+     */
+    template <class TDataType, class TContainerType>
+    void DistributeVariable(
+        ModelPart& rModelPart,
+        const Variable<TDataType>& rVariable,
+        const Variable<double>& rWeightVariable,
+        const bool IsInverseWeightProvided = false);
+
+    /**
      * @brief Sets a flag according to a given status over a given container
      * @param rFlag flag to be set
      * @param rFlagValue flag value to be set
@@ -1243,6 +1267,9 @@ private:
         return true;
         KRATOS_CATCH("")
     }
+
+    template <class TContainerType>
+    TContainerType& GetContainer(ModelPart& rModelPart);
 
     ///@}
     ///@name Private  Acces
