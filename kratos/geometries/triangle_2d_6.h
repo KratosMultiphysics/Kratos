479b3c5d81eff6dc6fdaa17452c45dcb8d42140d
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2020-06-29 00:13:40 +0200

    Clean up duplicated in triangle

--- kratos/geometries/triangle_2d_6.h
+++ kratos/geometries/triangle_2d_6.h
@@ -377,20 +377,6 @@ public:
     {
         return typename BaseType::Pointer( new Triangle2D6( NewGeometryId, rThisPoints ) );
     }
-
-    /**
-     * @brief Creates a new geometry pointer
-     * @param rNewGeometryName the name of the new geometry
-     * @param rThisPoints the nodes of the new geometry
-     * @return Pointer to the new geometry
-     */
-    typename BaseType::Pointer Create(
-        const std::string& rNewGeometryName,
-        PointsArrayType const& rThisPoints
-        ) const override
-    {
-        return typename BaseType::Pointer( new Triangle2D6( rNewGeometryName, rThisPoints ) );
-    }
     
     /**
      * @brief Creates a new geometry pointer
@@ -418,22 +404,6 @@ public:
         ) const override
     {
         auto p_geometry = typename BaseType::Pointer( new Triangle2D6( NewGeometryId, pGeometry->Points() ) );
-        p_geometry->SetData(pGeometry->GetData());
-        return p_geometry;
-    }
-
-    /**
-     * @brief Creates a new geometry pointer
-     * @param rNewGeometryName the name of the new geometry
-     * @param pGeometry Pointer to an existing geometry
-     * @return Pointer to the new geometry
-     */
-    typename BaseType::Pointer Create(
-        const std::string& rNewGeometryName,
-        typename BaseType::Pointer pGeometry
-        ) const override
-    {
-        auto p_geometry = typename BaseType::Pointer( new Triangle2D6( rNewGeometryName, pGeometry->Points() ) );
         p_geometry->SetData(pGeometry->GetData());
         return p_geometry;
     }
