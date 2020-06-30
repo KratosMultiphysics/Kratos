0e8aae2d424f549e430a6ab02309d3f98ba4001e
rickyaristio <ricky.aristio@gmail.com>
2020-06-30 14:14:11 +0200

    apply the strong support for the tests

--- applications/IgaApplication/tests/single_patch_four_point_sail_linear_static_test/single_patch_four_point_sail_linear_static_main.py
+++ applications/IgaApplication/tests/single_patch_four_point_sail_linear_static_test/single_patch_four_point_sail_linear_static_main.py
@@ -9,27 +9,6 @@ class ModifiedStructuralMechanicsAnalysis(StructuralMechanicsAnalysis):
     def Initialize(self):
         super(ModifiedStructuralMechanicsAnalysis, self).Initialize()
 
-        for node in self._GetSolver().main_model_part.Nodes:
-            if node.X == -10 and node.Y == -10 and node.Z == 10:
-                node.Fix(KratosMultiphysics.DISPLACEMENT_X)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Y)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Z)
-
-            if node.X == -10 and node.Y == 10 and node.Z == 0:
-                node.Fix(KratosMultiphysics.DISPLACEMENT_X)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Y)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Z)
-
-            if node.X == 10 and node.Y == -10 and node.Z == 0:
-                node.Fix(KratosMultiphysics.DISPLACEMENT_X)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Y)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Z)
-
-            if node.X == 10 and node.Y == 10 and node.Z == 10:
-                node.Fix(KratosMultiphysics.DISPLACEMENT_X)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Y)
-                node.Fix(KratosMultiphysics.DISPLACEMENT_Z)
-
 if __name__ == "__main__":
     with open("single_patch_four_point_sail_linear_static_project_parameters.json",'r') as parameter_file:
         parameters = KratosMultiphysics.Parameters(parameter_file.read())
