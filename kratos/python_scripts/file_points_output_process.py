373ecb1ee531124b83d7c9494f33aa3edcf16122
franzi <franzi_8.5@web.de>
2020-07-01 15:07:24 +0200

    specifying file_path now needed containing file name and ending, renamed c to p_count

--- kratos/python_scripts/file_points_output_process.py
+++ kratos/python_scripts/file_points_output_process.py
@@ -16,7 +16,7 @@ class FilePointsOutputProcess(KratosMultiphysics.Process):
     """This process writes output for several points given in a file to an output file
     Internally it holds an object of type "MultiplePointsOutputProcess"
     Usage:
-        - file path and name of an .csv-file for the import of sampling points need to be specified
+        - file path of an .csv-file (!) for the import of sampling points need to be specified
         - the first line of the .csv-file should state the column names: "x,y,z"
         - the following rows should each be one point with its x,y and z value in the respective column of the .csv-file, separated by a comma 
         - the number of sampling points needs to be specified
@@ -28,9 +28,7 @@ class FilePointsOutputProcess(KratosMultiphysics.Process):
             "help"              : "This process writes output for several points given in a file to a file. Internally it holds an object of type MultiplePointsOutputProcess",
             "model_part_name"   : "",
             "entity_type"       : "element",
-            "file_path"         : "/",
-            "file_name"         : "points",
-            "file_type"         : ".csv",
+            "file_path"         : "",
             "sampling_points"   : 3,
             "output_variables"  : [],
             "historical_value"  : true,
@@ -45,10 +43,6 @@ class FilePointsOutputProcess(KratosMultiphysics.Process):
         file_path = params["file_path"].GetString()
         if len(file_path) == 0:
             raise Exception('A file path has has to be provided!')
-        file_name = params["file_name"].GetString()
-        if len(file_name) == 0:
-            raise Exception('A file name has has to be provided!')
-        file_type = ".csv"
 
         # get and check number of sampling points
         number_of_sampling_points = params["sampling_points"].GetInt()
@@ -60,36 +54,27 @@ class FilePointsOutputProcess(KratosMultiphysics.Process):
         if not entity_type == "element" or entity_type == "condition":
             raise Exception('"entity_type" can be either "element" or "condition"!')
 
-        # get complete file name
-        if file_path.endswith("/"):
-            seperator=""
-        else:
-            seperator="/"
-        if file_name.endswith(file_type):
-            file_type = ""
-        file_csv = file_path + seperator + file_name + file_type
-
         # initialize position matrix for sampling points
         positions = KratosMultiphysics.Matrix(number_of_sampling_points, 3)
 
         # check and read file
-        if os.path.exists(file_csv) and os.access(file_csv, os.R_OK):
+        if os.path.exists(file_path) and os.access(file_path, os.R_OK):
 
             # open file
             try:
-                with open(file_csv, newline=None) as f:
-                    reader = csv.DictReader(f, delimiter=',')
-                    for c, row in enumerate(reader):
+                with open(file_path, newline=None) as file:
+                    reader = csv.DictReader(file, delimiter=',')
+                    for p_count, row in enumerate(reader):
 
                         # check if number of sampling points is already reached
-                        if c == number_of_sampling_points:
-                            KratosMultiphysics.Logger.PrintWarning("Number of points in file higher than number of samples!")
+                        if p_count == number_of_sampling_points:
+                            KratosMultiphysics.Logger.PrintWarning("FilePointsOutputProcess: Number of points in file higher than number of samples!")
                             break
 
                         # add position of sampling point
-                        positions[c,0] = float(row['x'])
-                        positions[c,1] = float(row['y'])
-                        positions[c,2] = float(row['z'])
+                        positions[p_count,0] = float(row['x'])
+                        positions[p_count,1] = float(row['y'])
+                        positions[p_count,2] = float(row['z'])
 
             except:
                 raise Exception('The .csv-file can not be read (correctly)!')
@@ -97,9 +82,9 @@ class FilePointsOutputProcess(KratosMultiphysics.Process):
             raise Exception('The file does not exist or can not be accessed!')
 
         # check if enough points were found
-        c += 1
-        if c < number_of_sampling_points:
-            KratosMultiphysics.Logger.PrintWarning("Found " + str(c) + " points in file. Number of samples given was " + str(number_of_sampling_points) + "!")
+        p_count += 1
+        if p_count < number_of_sampling_points:
+            KratosMultiphysics.Logger.PrintWarning("FilePointsOutputProcess: Found " + str(p_count) + " points in file. Number of samples given was " + str(number_of_sampling_points) + "!")
 
         params.RemoveValue("file_path")
         params.RemoveValue("file_name")
