01a866f1e3055b729ad66b1bce6407443afd6cfa
Jonas <jonas.hillenbrand@tum.de>
2020-07-03 12:02:41 +0200

    adjoint extensions added to adjoint_finite_difference_nodal_concentrated_element

--- applications/StructuralMechanicsApplication/custom_response_functions/adjoint_elements/adjoint_finite_difference_nodal_concentrated_element.h
+++ applications/StructuralMechanicsApplication/custom_response_functions/adjoint_elements/adjoint_finite_difference_nodal_concentrated_element.h
@@ -27,6 +27,32 @@ namespace Kratos
 template <typename TPrimalElement>
 class AdjointFiniteDifferenceNodalConcentratedElement: public AdjointFiniteDifferencingBaseElement<TPrimalElement>
 {
+    class ThisExtensions : public AdjointExtensions
+    {
+        Element* mpElement;
+
+    public:
+        explicit ThisExtensions(Element* pElement);
+
+        void GetFirstDerivativesVector(std::size_t NodeId,
+                                       std::vector<IndirectScalar<double>>& rVector,
+                                       std::size_t Step) override;
+
+        void GetSecondDerivativesVector(std::size_t NodeId,
+                                        std::vector<IndirectScalar<double>>& rVector,
+                                        std::size_t Step) override;
+
+        void GetAuxiliaryVector(std::size_t NodeId,
+                                std::vector<IndirectScalar<double>>& rVector,
+                                std::size_t Step) override;
+
+        void GetFirstDerivativesVariables(std::vector<VariableData const*>& rVariables) const override;
+
+        void GetSecondDerivativesVariables(std::vector<VariableData const*>& rVariables) const override;
+
+        void GetAuxiliaryVariables(std::vector<VariableData const*>& rVariables) const override;
+    };
+    
 public:
 
     // redefine the typedefs because of templated base class
