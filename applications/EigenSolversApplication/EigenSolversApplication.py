61be3e2db1c7ffeb77f68fe4f2abd408e1a4f18f
philbucher <phibbi@gmx.net>
2020-07-07 14:23:10 +0200

    fixes

--- applications/EigenSolversApplication/EigenSolversApplication.py
+++ applications/EigenSolversApplication/EigenSolversApplication.py
@@ -2,4 +2,4 @@ from KratosMultiphysics.kratos_utilities import IssueDeprecationWarning
 
 IssueDeprecationWarning('EigenSolversApplication', 'please the "LinearSolversApplication" instead')
 
-from LinearSolversApplication import *
+from KratosMultiphysics.LinearSolversApplication import *
--- applications/LinearSolversApplication/EigenSolversApplication.py
+++ applications/LinearSolversApplication/EigenSolversApplication.py
@@ -9,13 +9,10 @@
 #   Author: Thomas Oberbichler
 #
 
-# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-from __future__ import print_function, absolute_import, division
-
 # Application dependent names and paths
 from KratosMultiphysics import _ImportApplication
 from KratosEigenSolversApplication import *
 application = KratosEigenSolversApplication()
-application_name = "EigenSolversApplication"
+application_name = "LinearSolversApplication"
 
 _ImportApplication(application, application_name)
