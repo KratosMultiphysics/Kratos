13eb0e40d7460a386f5378b89ad5e2979b897522
philbucher <phibbi@gmx.net>
2020-07-07 15:37:35 +0200

    fully backward compatible

--- applications/EigenSolversApplication/EigenSolversApplication.py
+++ applications/EigenSolversApplication/EigenSolversApplication.py
@@ -3,3 +3,4 @@ from KratosMultiphysics.kratos_utilities import IssueDeprecationWarning
 IssueDeprecationWarning('EigenSolversApplication', 'please the "LinearSolversApplication" instead')
 
 from KratosMultiphysics.LinearSolversApplication import *
+from KratosMultiphysics.LinearSolversApplication import dense_linear_solver_factory
