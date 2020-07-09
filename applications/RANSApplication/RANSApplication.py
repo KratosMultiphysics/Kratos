243403e90f0ee888d5359bbce15d03cc6c053d28
sunethwarna <suneth.warna@gmail.com>
2020-07-09 12:14:08 +0200

    remove python 2 leftovers

--- applications/RANSApplication/RANSApplication.py
+++ applications/RANSApplication/RANSApplication.py
@@ -1,11 +1,16 @@
-# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-from __future__ import print_function, absolute_import, division
-
 # Application dependent names and paths
 from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
 from KratosMultiphysics import IsDistributedRun
 if (IsDistributedRun() and CheckIfApplicationsAvailable("TrilinosApplication")):
     from KratosMultiphysics.TrilinosApplication import *
+elif (IsDistributedRun()):
+    raise Exception("Distributed run requires TrilinosApplication")
+
+if (CheckIfApplicationsAvailable("FluidDynamicsApplication")):
+    from KratosMultiphysics.FluidDynamicsApplication import *
+else:
+    raise Exception("RANSApplication requires FluidDynamicsApplication.")
+
 from KratosRANSApplication import *
 
 from KratosMultiphysics import _ImportApplication
