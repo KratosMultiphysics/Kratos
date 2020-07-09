de5a4f5d5d590c595b60fc93969011957e67e59d
Philipp Bucher <philipp.bucher@tum.de>
2020-07-09 12:55:27 +0200

    satisfy codacy

--- kratos/python_scripts/testing/utilities.py
+++ kratos/python_scripts/testing/utilities.py
@@ -2,7 +2,7 @@
 from shutil import which
 
 def GetPython3Command():
-    """Return the name of the python command, can be used with subprocess"""
+    """Return the name of the python command, can be used with subprocess."""
     if which("python3"):
         return "python3"
     elif which("python"):
