a55887770f9dadeaf3df496312444b97a7906b06
sunethwarna <suneth.warna@gmail.com>
2020-07-10 13:38:30 +0200

    moving to debug_helpers

---
+++ kratos/tests/cpp_tests/utilities/test_debug_helpers.cpp
@@ -0,0 +1,35 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Suneth Warnakulasuriya
+//
+//
+
+// Project includes
+#include "includes/debug_helpers.h"
+#include "includes/ublas_interface.h"
+#include "testing/testing.h"
+
+namespace Kratos
+{
+namespace Testing
+{
+KRATOS_TEST_CASE_IN_SUITE(DebugHelpersKRATOS_WATCH_VECTOR_WITH_PRECISION, KratosCoreFastSuite)
+{
+    Vector temp(4, 10.0);
+    KRATOS_WATCH_VECTOR_WITH_PRECISION(temp, 12);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(DebugHelpersKRATOS_WATCH_MATRIX_WITH_PRECISION, KratosCoreFastSuite)
+{
+    Matrix temp(4, 4, 10.0);
+    KRATOS_WATCH_MATRIX_WITH_PRECISION(temp, 12);
+}
+} // namespace Testing
+} // namespace Kratos
