5270a60df850fd0bed3f1134aabb7118a9bdabde
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-07-10 11:34:40 +0200

    Github merge error, duplicated code

--- kratos/solving_strategies/strategies/explicit_strategy.h
+++ kratos/solving_strategies/strategies/explicit_strategy.h
@@ -329,15 +329,6 @@ static std::string Name()
     return "explicit_strategy";
 }
 
-/**
- * @brief Returns the name of the class as used in the settings (snake_case format)
- * @return The name of the class
- */
-static std::string Name()
-{
-    return "explicit_strategy";
-}
-
 /// Turn back information as a string.
 std::string Info() const override
 {
