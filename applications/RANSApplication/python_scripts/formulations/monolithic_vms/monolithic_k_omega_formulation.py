e463656ec880f4aab11dee8e762b8a26cb99bad9
sunethwarna <suneth.warna@gmail.com>
2020-07-11 17:47:27 +0200

    adding k_omega formulation

---
+++ applications/RANSApplication/python_scripts/formulations/monolithic_vms/monolithic_k_omega_formulation.py
@@ -0,0 +1,42 @@
+from __future__ import print_function, absolute_import, division
+
+# import kratos
+import KratosMultiphysics as Kratos
+
+# import formulation interface
+from KratosMultiphysics.RANSApplication.formulations.formulation import Formulation
+
+# import formulations
+from ..incompressible_potential_flow import IncompressiblePotentialFlowFormulation
+from ..k_omega import KOmegaFormulation
+from .monolithic_velocity_pressure_formulation import MonolithicVelocityPressureFormulation
+
+class MonolithicKOmegaFormulation(Formulation):
+    def __init__(self, model_part, settings):
+        super(MonolithicKOmegaFormulation, self).__init__(model_part, settings)
+
+        default_settings = Kratos.Parameters(r'''
+        {
+            "formulation_name": "monolithic_k_omega",
+            "incompressible_potential_flow_initialization_settings": {},
+            "monolithic_flow_solver_settings": {},
+            "k_omega_solver_settings": {},
+            "max_iterations": 1
+        }''')
+        self.settings.ValidateAndAssignDefaults(default_settings)
+
+        if (not self.settings["incompressible_potential_flow_initialization_settings"].IsEquivalentTo(
+                Kratos.Parameters("{}"))):
+            self.incompressible_potential_flow_formulation = IncompressiblePotentialFlowFormulation(model_part, settings["incompressible_potential_flow_initialization_settings"])
+            self.AddFormulation(self.incompressible_potential_flow_formulation)
+
+        self.monolithic_formulation = MonolithicVelocityPressureFormulation(model_part, settings["monolithic_flow_solver_settings"])
+        self.AddFormulation(self.monolithic_formulation)
+
+        self.k_omega_formulation = KOmegaFormulation(model_part, settings["k_omega_solver_settings"])
+        self.AddFormulation(self.k_omega_formulation)
+
+        self.SetMaxCouplingIterations(self.settings["max_iterations"].GetInt())
+
+    def SetConstants(self, settings):
+        self.k_omega_formulation.SetConstants(settings)
