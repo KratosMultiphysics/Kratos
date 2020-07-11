fe6800a57890949cb8543e77e50711a35871d67b
sunethwarna <suneth.warna@gmail.com>
2020-07-11 08:24:46 +0200

    add openmp strategies

--- applications/RANSApplication/custom_python/add_custom_strategies_to_python.cpp
+++ applications/RANSApplication/custom_python/add_custom_strategies_to_python.cpp
@@ -16,16 +16,27 @@
 #include <pybind11/pybind11.h>
 
 // Project includes
-#include "custom_python/add_custom_strategies_to_python.h"
 #include "includes/define_python.h"
 #include "spaces/ublas_space.h"
 
 // strategies
+#include "solving_strategies/strategies/solving_strategy.h"
+#include "custom_strategies/rans_fractional_step_strategy.h"
+#include "custom_utilities/solver_settings.h"
+
+// schemes
 #include "custom_strategies/generic_residual_based_bossak_velocity_scalar_scheme.h"
 #include "custom_strategies/generic_residualbased_simple_steady_scalar_scheme.h"
+#include "custom_strategies/algebraic_flux_corrected_scalar_steady_scheme.h"
 
 // convergence criterians
 #include "custom_strategies/generic_convergence_criteria.h"
+
+//linear solvers
+#include "linear_solvers/linear_solver.h"
+
+// Include base h
+#include "custom_python/add_custom_strategies_to_python.h"
 
 namespace Kratos
 {
@@ -37,25 +48,36 @@ void AddCustomStrategiesToPython(pybind11::module& m)
 
     using LocalSpaceType = UblasSpace<double, Matrix, Vector>;
     using SparseSpaceType = UblasSpace<double, CompressedMatrix, Vector>;
+    using LinearSolverType = LinearSolver<SparseSpaceType, LocalSpaceType>;
     using BaseSchemeType = Scheme<SparseSpaceType, LocalSpaceType>;
+    using BaseConvergenceCriteriaType = ConvergenceCriteria<SparseSpaceType, LocalSpaceType>;
+    using BaseSolvingStrategyType = SolvingStrategy<SparseSpaceType, LocalSpaceType, LinearSolverType>;
 
     // Convergence criteria
-    py::class_<GenericConvergenceCriteria<SparseSpaceType, LocalSpaceType>,
-               typename GenericConvergenceCriteria<SparseSpaceType, LocalSpaceType>::Pointer,
-               ConvergenceCriteria<SparseSpaceType, LocalSpaceType>>(
-        m, "GenericScalarConvergenceCriteria")
+    using GenericConvergenceCriteriaType = GenericConvergenceCriteria<SparseSpaceType, LocalSpaceType>;
+    py::class_<GenericConvergenceCriteriaType, typename GenericConvergenceCriteriaType::Pointer, BaseConvergenceCriteriaType>(m, "GenericScalarConvergenceCriteria")
         .def(py::init<double, double>());
 
-    py::class_<GenericResidualBasedBossakVelocityScalarScheme<SparseSpaceType, LocalSpaceType>,
-               typename GenericResidualBasedBossakVelocityScalarScheme<SparseSpaceType, LocalSpaceType>::Pointer, BaseSchemeType>(
-        m, "GenericResidualBasedBossakVelocityDynamicScalarScheme")
-        .def(py::init<const double, const double, const Variable<double>&,
-                      const Variable<double>&, const Variable<double>&>());
+    // add schemes
+    using GenericResidualBasedBossakVelocityScalarSchemeType = GenericResidualBasedBossakVelocityScalarScheme<SparseSpaceType, LocalSpaceType>;
+    py::class_<GenericResidualBasedBossakVelocityScalarSchemeType, typename GenericResidualBasedBossakVelocityScalarSchemeType::Pointer, BaseSchemeType>(m, "GenericResidualBasedBossakVelocityDynamicScalarScheme")
+        .def(py::init<const double, const double, const Variable<double>&, const Variable<double>&, const Variable<double>&>());
 
-    py::class_<GenericResidualBasedSimpleSteadyScalarScheme<SparseSpaceType, LocalSpaceType>,
-               typename GenericResidualBasedSimpleSteadyScalarScheme<SparseSpaceType, LocalSpaceType>::Pointer, BaseSchemeType>(
-        m, "GenericResidualBasedSimpleSteadyScalarScheme")
+    using GenericResidualBasedSimpleSteadyScalarSchemeType = GenericResidualBasedSimpleSteadyScalarScheme<SparseSpaceType, LocalSpaceType>;
+    py::class_<GenericResidualBasedSimpleSteadyScalarSchemeType, typename GenericResidualBasedSimpleSteadyScalarSchemeType::Pointer, BaseSchemeType>(m, "GenericResidualBasedSimpleSteadyScalarScheme")
         .def(py::init<const double>());
+
+    using AlgebraicFluxCorrectedScalarSteadySchemeType = AlgebraicFluxCorrectedScalarSteadyScheme<SparseSpaceType, LocalSpaceType>;
+    py::class_<AlgebraicFluxCorrectedScalarSteadySchemeType, typename AlgebraicFluxCorrectedScalarSteadySchemeType::Pointer, BaseSchemeType>(
+        m, "AlgebraicFluxCorrectedScalarSteadyScheme")
+        .def(py::init<const double, const Flags&>())
+        .def(py::init<const double, const Flags&, const Variable<int>&>());
+
+    // strategies
+    using RansFractionalStepStrategyType = RansFractionalStepStrategy<SparseSpaceType, LocalSpaceType, LinearSolverType>;
+    py::class_<RansFractionalStepStrategyType, typename RansFractionalStepStrategyType::Pointer, BaseSolvingStrategyType>(m, "RansFractionalStepStrategy")
+        .def(py::init<ModelPart&, SolverSettings<SparseSpaceType, LocalSpaceType, LinearSolverType>&, bool, bool>())
+        .def(py::init<ModelPart&, SolverSettings<SparseSpaceType, LocalSpaceType, LinearSolverType>&, bool, bool, const Kratos::Variable<int>&>());
 
 }
 
---
+++ applications/RANSApplication/custom_strategies/rans_fractional_step_strategy.h
@@ -0,0 +1,306 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Jordi Cotela
+//
+//  Extended by :    Suneth Warnakulasuriya
+
+#if !defined(KRATOS_RANS_FRACTIONAL_STEP_STRATEGY)
+#define KRATOS_RANS_FRACTIONAL_STEP_STRATEGY
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "custom_strategies/strategies/fractional_step_strategy.h"
+
+// Application includes
+
+namespace Kratos
+{
+///@addtogroup FluidDynamicsApplication
+///@{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @brief Fractional-step strategy for incompressible Navier-Stokes formulation
+ * This strategy implements a splitting scheme for the incompressible
+ * Navier-Stokes equations. It is intended to be used in combination with the
+ * FractionalStep element in the FluidDynamicsApplicatoin. The fractional step
+ * index, which is stored in the ProcessInfo, takes the values 1 : Momentum step
+ * (calculate fractional step velocity) 2-3 : Unused (reserved for componentwise
+ * calculation of frac step velocity) 4 : Pressure step 5 : Computation of
+ * projections 6 : End of step velocity
+ * @tparam TSparseSpace Sparse space template type
+ * @tparam TDenseSpace Dense space template type
+ * @tparam TLinearSolver Linear solver template type
+ */
+template <class TSparseSpace, class TDenseSpace, class TLinearSolver>
+class RansFractionalStepStrategy
+    : public FractionalStepStrategy<TSparseSpace, TDenseSpace, TLinearSolver>
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Counted pointer of RansFractionalStepStrategy
+    KRATOS_CLASS_POINTER_DEFINITION(RansFractionalStepStrategy);
+
+    typedef FractionalStepStrategy<TSparseSpace, TDenseSpace, TLinearSolver> BaseType;
+
+    typedef SolverSettings<TSparseSpace, TDenseSpace, TLinearSolver> SolverSettingsType;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    RansFractionalStepStrategy(ModelPart& rModelPart,
+                               SolverSettingsType& rSolverConfig,
+                               bool PredictorCorrector,
+                               bool CalculateReactionsFlag)
+        : BaseType(rModelPart, rSolverConfig, PredictorCorrector, CalculateReactionsFlag)
+    {
+        KRATOS_INFO(this->Info()) << "Created fractional step strategy." << std::endl;
+    }
+
+    RansFractionalStepStrategy(ModelPart& rModelPart,
+                               SolverSettingsType& rSolverConfig,
+                               bool PredictorCorrector,
+                               bool CalculateReactionsFlag,
+                               const Kratos::Variable<int>& PeriodicVar)
+        : BaseType(rModelPart, rSolverConfig, PredictorCorrector, CalculateReactionsFlag, PeriodicVar)
+    {
+        KRATOS_INFO(this->Info()) << "Created periodic fractional step strategy." << std::endl;
+    }
+
+    /// Destructor.
+    ~RansFractionalStepStrategy() override = default;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        std::stringstream buffer;
+        buffer << "RansFractionalStepStrategy";
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override
+    {
+    }
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+    ///@}
+protected:
+    ///@name Protected Life Cycle
+    ///@{
+
+    ///@}
+    ///@name Protected static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    std::tuple<bool, double> SolveStep() override
+    {
+        ModelPart& rModelPart = BaseType::GetModelPart();
+        const int n_nodes = rModelPart.NumberOfNodes();
+
+        ProcessInfo& r_process_info = rModelPart.GetProcessInfo();
+
+        // 1. Fractional step momentum iteration
+        r_process_info.SetValue(FRACTIONAL_STEP, 1);
+
+        bool Converged = false;
+        for (unsigned int it = 0; it < this->mMaxVelocityIter; ++it)
+        {
+            KRATOS_INFO_IF("RansFractionalStepStrategy", BaseType::GetEchoLevel() > 1)
+                << "Momentum iteration " << it << std::endl;
+
+            // build momentum system and solve for fractional step velocity increment
+            r_process_info.SetValue(FRACTIONAL_STEP, 1);
+            double NormDv = this->mpMomentumStrategy->Solve();
+
+            // Check convergence
+            Converged = this->CheckFractionalStepConvergence(NormDv);
+
+            if (Converged)
+            {
+                KRATOS_INFO_IF("RansFractionalStepStrategy", BaseType::GetEchoLevel() > 0)
+                    << "Fractional velocity converged in " << it + 1
+                    << " iterations." << std::endl;
+                break;
+            }
+        }
+
+        KRATOS_INFO_IF("RansFractionalStepStrategy", !Converged && BaseType::GetEchoLevel() > 0)
+            << "Fractional velocity iterations did not converge." << std::endl;
+
+        // Compute projections (for stabilization)
+        r_process_info.SetValue(FRACTIONAL_STEP, 4);
+        this->ComputeSplitOssProjections(rModelPart);
+
+        // 2. Pressure solution (store pressure variation in PRESSURE_OLD_IT)
+        r_process_info.SetValue(FRACTIONAL_STEP, 5);
+
+        const double eta = (r_process_info.Has(PRESSURE_COEFFICIENT))
+                               ? r_process_info[PRESSURE_COEFFICIENT]
+                               : 1.0;
+
+#pragma omp parallel for
+        for (int i_node = 0; i_node < n_nodes; ++i_node)
+        {
+            auto it_node = rModelPart.NodesBegin() + i_node;
+            const double old_press = it_node->FastGetSolutionStepValue(PRESSURE);
+            it_node->FastGetSolutionStepValue(PRESSURE_OLD_IT) = -eta * old_press;
+        }
+
+        KRATOS_INFO_IF("RansFractionalStepStrategy", BaseType::GetEchoLevel() > 0)
+            << "Calculating Pressure." << std::endl;
+        double NormDp = this->mpPressureStrategy->Solve();
+
+#pragma omp parallel for
+        for (int i_node = 0; i_node < n_nodes; ++i_node)
+        {
+            auto it_node = rModelPart.NodesBegin() + i_node;
+            it_node->FastGetSolutionStepValue(PRESSURE_OLD_IT) +=
+                it_node->FastGetSolutionStepValue(PRESSURE);
+        }
+
+        // 3. Compute end-of-step velocity
+        KRATOS_INFO_IF("RansFractionalStepStrategy", BaseType::GetEchoLevel() > 0)
+            << "Updating Velocity." << std::endl;
+        r_process_info.SetValue(FRACTIONAL_STEP, 6);
+
+        this->CalculateEndOfStepVelocity();
+
+        // Set the output tuple as the fractional velocity convergence and pressure norm
+        return std::make_tuple(Converged, NormDp);
+    }
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+    ///@}
+private:
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    /// Assignment operator.
+    RansFractionalStepStrategy& operator=(RansFractionalStepStrategy const& rOther)
+    {
+    }
+
+    /// Copy constructor.
+    RansFractionalStepStrategy(RansFractionalStepStrategy const& rOther)
+    {
+    }
+
+    ///@}
+
+}; /// Class FStepStrategy
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+
+///@} // addtogroup
+
+} // namespace Kratos.
+
+#endif // KRATOS_RANS_FRACTIONAL_STEP_STRATEGY
