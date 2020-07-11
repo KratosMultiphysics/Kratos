72656ddea5194038284abdcedc603db8300ef973
sunethwarna <suneth.warna@gmail.com>
2020-07-11 18:28:12 +0200

    add k_omega_sst formulation tests

--- applications/RANSApplication/tests/test_RANSApplication.py
+++ applications/RANSApplication/tests/test_RANSApplication.py
@@ -33,6 +33,11 @@ from monolithic_k_omega_formulation_tests import MonolithicKOmegaTest
 from monolithic_k_omega_formulation_tests import MonolithicKOmegaPeriodicTest
 from fractional_step_k_omega_formulation_tests import FractionalStepKOmegaTest
 
+### k-omega-sst test_classes
+from monolithic_k_omega_sst_formulation_tests import MonolithicKOmegaSSTTest
+from monolithic_k_omega_sst_formulation_tests import MonolithicKOmegaSSTPeriodicTest
+from fractional_step_k_omega_sst_formulation_tests import FractionalStepKOmegaSSTTest
+
 def AssembleTestSuites():
     ''' Populates the test suites to run.
 
@@ -59,6 +64,13 @@ def AssembleTestSuites():
     # adding k-omega high re periodic tests to small suite
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([MonolithicKOmegaPeriodicTest]))
 
+    # adding k-omega-sst high re periodic tests to small suite
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([MonolithicKOmegaSSTPeriodicTest]))
+
+    # adding representative transient tests to small suite
+    smallSuite.addTest(FractionalStepKOmegaSSTTest("testRfcVelocityTransient"))
+    smallSuite.addTest(MonolithicKOmegaSSTTest("testRfcVelocityTransient"))
+
     # Create a test suite with the selected tests plus all small tests
     nightSuite = suites['nightly']
     nightSuite.addTests(smallSuite)
@@ -81,6 +93,11 @@ def AssembleTestSuites():
     nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([MonolithicKOmegaTest]))
     # adding fractional step k-omega high re tests
     nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([FractionalStepKOmegaTest]))
+
+    # adding monolithic k-omega high re tests
+    nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([MonolithicKOmegaSSTTest]))
+    # adding fractional step k-omega high re tests
+    nightSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([FractionalStepKOmegaSSTTest]))
 
     # For very long tests that should not be in nighly and you can use to validate
     # validationSuite = suites['validation']
