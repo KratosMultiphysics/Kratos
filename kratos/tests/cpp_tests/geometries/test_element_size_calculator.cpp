59661cf291482ef4e0d9478f22c633ec5f2dc5c7
mohitt <mohit.tyagi@altair.com>
2020-07-14 13:30:26 +0530

    split the test

--- kratos/tests/cpp_tests/geometries/test_element_size_calculator.cpp
+++ kratos/tests/cpp_tests/geometries/test_element_size_calculator.cpp
@@ -25,9 +25,8 @@ namespace Kratos
 {
     namespace Testing
     {
-        KRATOS_TEST_CASE_IN_SUITE(Prism3D6ElementSize, KratosCoreGeometriesFastSuite)
+        KRATOS_TEST_CASE_IN_SUITE(Prism3D6ElementSizeCase1, KratosCoreGeometriesFastSuite)
         {
-            // Case 1
             Geometry<NodeType>::PointsArrayType nodes;
             nodes.push_back(NodeType::Pointer(new NodeType(1, 0.0, 0.0, 0.0)));
             nodes.push_back(NodeType::Pointer(new NodeType(2, 1.0, 0.0, 0.0)));
@@ -36,14 +35,15 @@ namespace Kratos
             nodes.push_back(NodeType::Pointer(new NodeType(5, 1.0, 0.0, 1.0)));
             nodes.push_back(NodeType::Pointer(new NodeType(6, 0.0, 1.0, 1.0)));
             const auto prism1 = *GeometryType::Pointer(new Prism3D6<NodeType>(nodes));
-            double minimum_size = ElementSizeCalculator<3, 6>::MinimumElementSize(prism1);
-            double average_size = ElementSizeCalculator<3, 6>::AverageElementSize(prism1);
+            const double minimum_size = ElementSizeCalculator<3, 6>::MinimumElementSize(prism1);
+            const double average_size = ElementSizeCalculator<3, 6>::AverageElementSize(prism1);
             KRATOS_CHECK_NEAR(minimum_size, std::sqrt(0.5), TOLERANCE);
             KRATOS_CHECK_NEAR(average_size, std::pow(0.5, 1.0 / 3.0), TOLERANCE);
+        }
 
-
-            // Case 2
-            nodes.clear();
+        KRATOS_TEST_CASE_IN_SUITE(Prism3D6ElementSizeCase2, KratosCoreGeometriesFastSuite)
+        {
+            Geometry<NodeType>::PointsArrayType nodes;
             nodes.push_back(NodeType::Pointer(new NodeType(1, 0.0, 0.0, 0.0)));
             nodes.push_back(NodeType::Pointer(new NodeType(2, 1.0, 0.0, 0.0)));
             nodes.push_back(NodeType::Pointer(new NodeType(3, 0.0, 1.0, 0.0)));
@@ -51,8 +51,8 @@ namespace Kratos
             nodes.push_back(NodeType::Pointer(new NodeType(5, 1.0, 0.0, 0.5)));
             nodes.push_back(NodeType::Pointer(new NodeType(6, 0.0, 1.0, 0.5)));
             const auto prism2 = *GeometryType::Pointer(new Prism3D6<NodeType>(nodes));
-            minimum_size = ElementSizeCalculator<3, 6>::MinimumElementSize(prism2);
-            average_size = ElementSizeCalculator<3, 6>::AverageElementSize(prism2);
+            const double minimum_size = ElementSizeCalculator<3, 6>::MinimumElementSize(prism2);
+            const double average_size = ElementSizeCalculator<3, 6>::AverageElementSize(prism2);
             KRATOS_CHECK_NEAR(minimum_size, 0.5, TOLERANCE);
             KRATOS_CHECK_NEAR(average_size, std::pow(0.25, 1.0 / 3.0), TOLERANCE); //
         }
