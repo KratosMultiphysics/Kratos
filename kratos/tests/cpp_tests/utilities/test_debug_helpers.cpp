25545d678c99e7212b17b21b8aeeb39cca0c6dba
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-07-15 16:40:13 +0200

    adding explanation comments

--- kratos/tests/cpp_tests/utilities/test_debug_helpers.cpp
+++ kratos/tests/cpp_tests/utilities/test_debug_helpers.cpp
@@ -22,12 +22,16 @@ namespace Testing
 {
 KRATOS_TEST_CASE_IN_SUITE(DebugHelpersKRATOS_WATCH_VECTOR_WITH_PRECISION, KratosCoreFastSuite)
 {
+    // This test is added merely to check debug_helpers.h is compilable since this header is not supposed to be
+    // included in any of the production codes.
     Vector temp(1, 10.0);
     KRATOS_WATCH_VECTOR_WITH_PRECISION(temp, 12);
 }
 
 KRATOS_TEST_CASE_IN_SUITE(DebugHelpersKRATOS_WATCH_MATRIX_WITH_PRECISION, KratosCoreFastSuite)
 {
+    // This test is added merely to check debug_helpers.h is compilable since this header is not supposed to be
+    // included in any of the production codes.    
     Matrix temp(1, 1, 10.0);
     KRATOS_WATCH_MATRIX_WITH_PRECISION(temp, 12);
 }
