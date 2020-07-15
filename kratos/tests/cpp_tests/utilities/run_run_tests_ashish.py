d526f5a294d0f46a18f144c193d3a17cc902749c
philbucher <philipp.bucher@tum.de>
2020-07-15 14:40:55 +0200

    add script to run test

---
+++ kratos/tests/cpp_tests/utilities/run_run_tests_ashish.py
@@ -0,0 +1,5 @@
+from KratosMultiphysics import *
+
+Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS) # Set the verbosity level
+
+Tester.RunTestCases("*AuxiliarModelPartUtilities*") #Test a specific case
