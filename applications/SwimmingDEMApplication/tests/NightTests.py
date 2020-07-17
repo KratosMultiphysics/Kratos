fcd7fe84c8b07871c155e5e4723cc58d7a9adabf
Guillermo Casas <gcasas@cimne.upc.edu>
2020-07-17 10:02:33 +0200

    Changing strategy to fix import errors

--- applications/SwimmingDEMApplication/tests/NightTests.py
+++ applications/SwimmingDEMApplication/tests/NightTests.py
@@ -5,75 +5,68 @@ import KratosMultiphysics
 import KratosMultiphysics.DEMApplication
 import KratosMultiphysics.SwimmingDEMApplication
 
-# Import TestFactory
-import InterpolationTestFactory as InterpolationTF
-import TestFactory as TF
-import FluidDEMTestFactory as FDEMTF
-import SPFEMTestFactory as SPFEMTF
-
 # Import KratosUnittest
 import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+# Importing test factories if possible
+try:
+     import CandelierTestFactory as CandelierTF
+     candelier_imports_available = True
+except ImportError:
+     candelier_imports_available = False
+try:
+     import InterpolationTestFactory as InterpolationTF
+     interpolation_imports_available = True
+except ImportError:
+     interpolation_imports_available = False
+
 import importlib
 
-class candelier_no_history_test(TF.TestFactory):
-     file_name = "candelier_tests/candelier"
-     file_parameters = "candelier_tests/ProjectParametersNoHistory.json"
-     required_non_standard_modules = ['numpy', 'scipy']
+if candelier_imports_available:
+     class candelier_no_history_test(CandelierTF.TestFactory):
+          file_name = "candelier_tests/candelier"
+          file_parameters = "candelier_tests/ProjectParametersNoHistory.json"
 
-class candelier_no_history_with_lift_test(TF.TestFactory):
-     file_name = "candelier_tests/candelier"
-     file_parameters = "candelier_tests/ProjectParametersNoHistoryWithLift.json"
-     required_non_standard_modules = ['numpy', 'scipy']
+     class candelier_no_history_with_lift_test(CandelierTF.TestFactory):
+          file_name = "candelier_tests/candelier"
+          file_parameters = "candelier_tests/ProjectParametersNoHistoryWithLift.json"
 
-class candelier_no_history_non_inertial_test(TF.TestFactory):
-     file_name = "candelier_tests/candelier"
-     file_parameters = "candelier_tests/ProjectParametersNoHistoryNonInertial.json"
-     required_non_standard_modules = ['numpy', 'scipy']
+     class candelier_no_history_non_inertial_test(CandelierTF.TestFactory):
+          file_name = "candelier_tests/candelier"
+          file_parameters = "candelier_tests/ProjectParametersNoHistoryNonInertial.json"
 
-class candelier_with_history_test(TF.TestFactory):
-     file_name = "candelier_tests/candelier"
-     file_parameters = "candelier_tests/ProjectParametersWithHistory.json"
-     required_non_standard_modules = ['numpy', 'scipy']
+     class candelier_with_history_test(CandelierTF.TestFactory):
+          file_name = "candelier_tests/candelier"
+          file_parameters = "candelier_tests/ProjectParametersWithHistory.json"
 
-class candelier_with_history_hinsberg_test(TF.TestFactory):
-     file_name = "candelier_tests/candelier"
-     file_parameters = "candelier_tests/ProjectParametersWithHistoryHinsberg.json"
-     required_non_standard_modules = ['numpy', 'scipy']
+     class candelier_with_history_hinsberg_test(CandelierTF.TestFactory):
+          file_name = "candelier_tests/candelier"
+          file_parameters = "candelier_tests/ProjectParametersWithHistoryHinsberg.json"
 
-# This test is ready to run but the implementation is not complete
-# # (it is non-trivial), so the result is not correct
-# class candelier_with_history_non_inertial_test(TF.TestFactory):
-#      file_name = "candelier_tests/candelier"
-#      file_parameters = "candelier_tests/ProjectParametersWithHistoryNonInertial.json"
-#      required_non_standard_modules = ['numpy', 'scipy']
+     # This test is ready to run but the implementation is not complete
+     # # (it is non-trivial), so the result is not correct
+     # class candelier_with_history_non_inertial_test(CandelierTF.TestFactory):
+     #      file_name = "candelier_tests/candelier"
+     #      file_parameters = "candelier_tests/ProjectParametersWithHistoryNonInertial.json"
 
-class interpolation_test_linear(InterpolationTF.TestFactory):
-     file_name = "interpolation_tests/cube"
-     file_parameters = "interpolation_tests/ProjectParametersCubeLinear.json"
+     candelier_test_list = [test_class for test_class in CandelierTF.TestFactory.__subclasses__()]
 
-class interpolation_test_nonlinear_time_no_substepping(InterpolationTF.TestFactory):
-     file_name = "interpolation_tests/cube"
-     file_parameters = "interpolation_tests/ProjectParametersCubeNonlinearTimeNoSubstepping.json"
+if interpolation_imports_available:
+     class interpolation_test_linear(InterpolationTF.TestFactory):
+          file_name = "interpolation_tests/cube"
+          file_parameters = "interpolation_tests/ProjectParametersCubeLinear.json"
 
+     class interpolation_test_nonlinear_time_no_substepping(InterpolationTF.TestFactory):
+          file_name = "interpolation_tests/cube"
+          file_parameters = "interpolation_tests/ProjectParametersCubeNonlinearTimeNoSubstepping.json"
+
+     interpolation_test_list = [test_class for test_class in InterpolationTF.TestFactory.__subclasses__()]
 
 # List of all classes above
-list_of_classes = [test_class for test_class in
-                    TF.TestFactory.__subclasses__()
-                  + InterpolationTF.TestFactory.__subclasses__()]
-
-def CheckImportsArePossible(test):
-     if hasattr(test, 'required_non_standard_modules'):
-          for module in test.required_non_standard_modules:
-               try:
-                    importlib.import_module(module)
-               except:
-                    return False
-     return True
+available_tests = candelier_test_list + interpolation_test_list
 
 def SetTestSuite(suites):
     night_suite = suites['nightly']
-
-    available_tests = [test for test in list_of_classes if CheckImportsArePossible(test)]
 
     night_suite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases(available_tests))
 
