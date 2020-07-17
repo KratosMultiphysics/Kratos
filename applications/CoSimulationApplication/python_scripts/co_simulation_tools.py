3e0c98e4aa9aec23ff4e30622e12fc5b950cf12e
Philipp Bucher <philipp.bucher@tum.de>
2020-07-17 14:51:01 +0200

    fix
    @inigolcanalejo this should fix it properly :)

--- applications/CoSimulationApplication/python_scripts/co_simulation_tools.py
+++ applications/CoSimulationApplication/python_scripts/co_simulation_tools.py
@@ -65,8 +65,10 @@ def CreateModelPartsFromCouplingData(data_list, model, solver_name):
         splitted_name = data.model_part_name.split(".")
         main_model_part_name = splitted_name[0]
         sub_model_part_names = splitted_name[1:]
-        if not model.HasModelPart(main_model_part_name):
+        if model.HasModelPart(main_model_part_name):
+            main_model_part = model.GetModelPart(main_model_part_name)
+        else:
             main_model_part = model.CreateModelPart(main_model_part_name)
             cs_print_info("CoSimTools", 'Created ModelPart "{}" for solver "{}"'.format(main_model_part_name, solver_name))
-            if len(sub_model_part_names) > 0:
-                RecursiveCreateModelParts(main_model_part, ".".join(sub_model_part_names))
+        if len(sub_model_part_names) > 0:
+            RecursiveCreateModelParts(main_model_part, ".".join(sub_model_part_names))
