d02e1d8ed84d727ac220d1d11b8ead0ef7264342
philbucher <philipp.bucher@tum.de>
2020-07-23 18:39:41 +0200

    add test

--- kratos/tests/cpp_tests/sources/test_filesystem.cpp
+++ kratos/tests/cpp_tests/sources/test_filesystem.cpp
@@ -50,6 +50,15 @@ KRATOS_TEST_CASE_IN_SUITE(FileSystemJoinPaths, KratosCoreFastSuite)
     KRATOS_CHECK_STRING_EQUAL(Kratos::FilesystemExtensions::JoinPaths(paths_2), "");
 }
 
+KRATOS_TEST_CASE_IN_SUITE(FileSystemJoinInvalidPaths, KratosCoreFastSuite)
+{
+    std::vector<std::string> paths_1 {"eee", "", "gt"};
+
+    KRATOS_CHECK_EXCEPTION_IS_THROWN(
+        Kratos::FilesystemExtensions::JoinPaths(paths_1),
+        "Path in position 1 is empty!");
+}
+
 KRATOS_TEST_CASE_IN_SUITE(FileSystemIsRegularFile, KratosCoreFastSuite)
 {
     const std::string file_name("dummy_file.txt");
