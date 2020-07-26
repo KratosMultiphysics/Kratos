9c06881c3f120e0a9b1b35eed9404c37518fa491
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@inria.fr>
2020-07-27 01:23:57 +0200

    Missing python test

--- kratos/tests/test_factories.py
+++ kratos/tests/test_factories.py
@@ -35,6 +35,19 @@ class TestFactories(KratosUnittest.TestCase):
         """)
         self._auxiliary_test_function_BuilderAndSolver(settings, "ResidualBasedBlockBuilderAndSolver")
 
+    def _auxiliary_test_function_ExplicitBuilder(self, settings, name):
+        builder_and_solver = KM.ExplicitBuilderFactory().Create(settings)
+        self.assertTrue(KM.ExplicitBuilderFactory().Has(settings["name"].GetString()))
+        self.assertEqual(builder_and_solver.Info(), name)
+
+    def test_ExplicitBuilder(self):
+        settings = KM.Parameters("""
+        {
+            "name" : "explicit_builder"
+        }
+        """)
+        self._auxiliary_test_function_ExplicitBuilder(settings, "ExplicitBuilder")
+
     def _auxiliary_test_function_ConvergenceCriteria(self, settings, name):
         conv_crit = KM.ConvergenceCriteriaFactory().Create(settings)
         self.assertTrue(KM.ConvergenceCriteriaFactory().Has(settings["name"].GetString()))
@@ -239,6 +252,24 @@ class TestFactories(KratosUnittest.TestCase):
         }
         """)
         self._auxiliary_test_function_Strategy(settings, "ExplicitStrategy")
+
+    def _auxiliary_test_function_ExplicitStrategy(self, settings, name):
+        this_model = KM.Model()
+        model_part = this_model.CreateModelPart("Main")
+        strategy = KM.ExplicitStrategyFactory().Create(model_part, settings)
+        self.assertTrue(KM.ExplicitStrategyFactory().Has(settings["name"].GetString()))
+        self.assertEqual(strategy.Info(), name)
+
+    def test_ExplicitSolvingStrategyRungeKutta4(self):
+        settings = KM.Parameters("""
+        {
+            "name" : "explicit_solving_strategy_runge_kutta_4",
+            "explicit_builder_settings" : {
+                "name": "explicit_builder"
+            }
+        }
+        """)
+        self._auxiliary_test_function_ExplicitStrategy(settings, "ExplicitSolvingStrategyRungeKutta4")
 
 if __name__ == '__main__':
     KratosUnittest.main()
