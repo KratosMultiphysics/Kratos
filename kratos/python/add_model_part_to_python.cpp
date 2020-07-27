6bdd5e7751c45d6db54d3242bad6a4c4fb60197e
tteschemacher <tteschemacher@users.noreply.github.com>
2020-07-27 17:15:11 +1000

    Revert using lambda for CreateNewElement/Condition with index ids

--- kratos/python/add_model_part_to_python.cpp
+++ kratos/python/add_model_part_to_python.cpp
@@ -874,18 +874,14 @@ void AddModelPartToPython(pybind11::module& m)
         .def("GetNodalSolutionStepTotalDataSize", &ModelPart::GetNodalSolutionStepTotalDataSize)
         .def("OverwriteSolutionStepData", &ModelPart::OverwriteSolutionStepData)
         .def("CreateNewNode", ModelPartCreateNewNode)
-        .def("CreateNewElement", [](ModelPart& rModelPart, const std::string ElementName, ModelPart::IndexType Id,
-            std::vector<ModelPart::IndexType> ElementNodeIds, ModelPart::PropertiesType::Pointer pProperties)
-            {return rModelPart.CreateNewElement(ElementName, Id, ElementNodeIds, pProperties); })
+        .def("CreateNewElement", ModelPartCreateNewElement)
         .def("CreateNewElement", [](ModelPart& rModelPart, const std::string ElementName, ModelPart::IndexType Id,
             ModelPart::GeometryType::Pointer pGeometry, ModelPart::PropertiesType::Pointer pProperties)
             {return rModelPart.CreateNewElement(ElementName, Id, pGeometry, pProperties);})
-        .def("CreateNewCondition", [](ModelPart& rModelPart, const std::string ConditionName, ModelPart::IndexType Id,
-            std::vector<ModelPart::IndexType> ConditionNodeIds, ModelPart::PropertiesType::Pointer pProperties)
-            {return rModelPart.CreateNewCondition(ConditionName, Id, ConditionNodeIds, pProperties); })
+        .def("CreateNewCondition", ModelPartCreateNewCondition)
         .def("CreateNewCondition", [](ModelPart& rModelPart, const std::string ConditionName, ModelPart::IndexType Id, 
-                                      ModelPart::GeometryType::Pointer pGeometry, ModelPart::PropertiesType::Pointer pProperties)
-                                      {return rModelPart.CreateNewCondition(ConditionName, Id, pGeometry, pProperties);})
+            ModelPart::GeometryType::Pointer pGeometry, ModelPart::PropertiesType::Pointer pProperties)
+            {return rModelPart.CreateNewCondition(ConditionName, Id, pGeometry, pProperties);})
         .def("GetCommunicator", ModelPartGetCommunicator, py::return_value_policy::reference_internal)
         .def("Check", &ModelPart::Check)
         .def("IsSubModelPart", &ModelPart::IsSubModelPart)
