f7c8f61c3f4dd6d039cca9290eff471c44ecdc0b
sunethwarna <suneth.warna@gmail.com>
2020-07-28 19:48:44 +0200

    adding doxygen

--- applications/RANSApplication/python_scripts/apply_wall_function_process.py
+++ applications/RANSApplication/python_scripts/apply_wall_function_process.py
@@ -3,16 +3,36 @@ import KratosMultiphysics.RANSApplication as KratosRANS
 
 
 def Factory(settings, Model):
-    if (type(settings) != KratosMultiphysics.Parameters):
+    if (not isinstance(Model, KratosMultiphysics.Model)):
+        raise Exception(
+            "expected input shall be a Model object, encapsulating a json string"
+        )
+    if (not isinstance(settings, KratosMultiphysics.Parameters)):
         raise Exception(
             "expected input shall be a Parameters object, encapsulating a json string"
         )
     return ApplyWallFunctionProcess(Model, settings["Parameters"])
 
 
-## All the processes python should be derived from "Process"
 class ApplyWallFunctionProcess(KratosMultiphysics.Process):
     def __init__(self, Model, settings):
+        """This process sets wall function parameters
+
+        This process is used to activate wall function in wall conditions by setting
+        RANS_IS_WALL_FUNCTION_ACTIVE to 1 in conditions. Additionally it also sets STRUCTURE and SLIP flag
+        on the same nodes as well as conditions in same model part.
+
+        Currently, all the wall conditions should be in one model part/sub model part. Having wall conditions
+        in more than one model part/sub model part is not allowed.
+
+        Args:
+            Model (Kratos.Model): [description]
+            settings (Kratos.Parameters): [description]
+
+        Raises:
+            Exception: [description]
+        """
+
         KratosMultiphysics.Process.__init__(self)
 
         default_parameters = KratosMultiphysics.Parameters("""
