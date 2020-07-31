ad810d7cfac91f57e93b855edbc033e5ed58d4c2
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2020-07-31 14:54:45 +0200

    Syntax error

--- applications/TrilinosApplication/TrilinosApplication.py
+++ applications/TrilinosApplication/TrilinosApplication.py
@@ -1,7 +1,7 @@
 from KratosMultiphysics import _ImportApplication
 from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
 import KratosMultiphysics.mpi # importing the MPI-Core, since the TrilinosApp directly links to it
-if CheckIfApplicationsAvailable("MetisApplication")
+if CheckIfApplicationsAvailable("MetisApplication"):
     import KratosMultiphysics.MetisApplication # importing to avoid problems with loading the metis library
 from KratosTrilinosApplication import *
 application = KratosTrilinosApplication()
