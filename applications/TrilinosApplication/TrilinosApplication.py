4d57dec042594a340acb652e27012626514be36f
philbucher <philipp.bucher@tum.de>
2020-07-31 12:19:57 +0200

    remove deprecated

--- applications/TrilinosApplication/TrilinosApplication.py
+++ applications/TrilinosApplication/TrilinosApplication.py
@@ -1,4 +1,3 @@
-from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 from KratosMultiphysics import _ImportApplication
 from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
 import KratosMultiphysics.mpi # importing the MPI-Core, since the TrilinosApp directly links to it
