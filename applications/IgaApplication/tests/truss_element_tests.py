50ad9cb8cac955a183d749c0236b03abda38926b
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-07-31 16:55:29 +1000

    Update python test

--- applications/IgaApplication/tests/truss_element_tests.py
+++ applications/IgaApplication/tests/truss_element_tests.py
@@ -1,6 +1,6 @@
-from KratosMultiphysics import *
-from KratosMultiphysics.IgaApplication import *
-from KratosMultiphysics.StructuralMechanicsApplication import *
+import KratosMultiphysics as KM
+import KratosMultiphysics.IgaApplication as IGA
+import KratosMultiphysics.StructuralMechanicsApplication as SMA
 import KratosMultiphysics.python_linear_solver_factory as linear_solver_factory
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
@@ -13,30 +13,29 @@ def GetFilePath(fileName):
 class TrussElementTests(KratosUnittest.TestCase):
 
     def solve(create_geometry):
-        model = Model()
+        model = KM.Model()
         model_part = model.CreateModelPart('Model')
 
-        model_part.AddNodalSolutionStepVariable(DISPLACEMENT)
-        model_part.AddNodalSolutionStepVariable(REACTION)
-        model_part.AddNodalSolutionStepVariable(POINT_LOAD)
+        model_part.AddNodalSolutionStepVariable(KM.DISPLACEMENT)
+        model_part.AddNodalSolutionStepVariable(KM.REACTION)
+        model_part.AddNodalSolutionStepVariable(SMA.POINT_LOAD)
 
         # create property for truss elements
-
         truss_properties = model_part.GetProperties()[1]
-        truss_properties.SetValue(CROSS_AREA      , 0.01  )
-        truss_properties.SetValue(PRESTRESS_CAUCHY, 0     )
-        truss_properties.SetValue(YOUNG_MODULUS   , 210000)
-        truss_properties.SetValue(POISSON_RATIO   , 0     )
-        truss_properties.SetValue(DENSITY         , 7856  )
+        truss_properties.SetValue(IGA.CROSS_AREA      , 0.01  )
+        truss_properties.SetValue(IGA.PRESTRESS_CAUCHY, 0     )
+        truss_properties.SetValue(KM.YOUNG_MODULUS   , 210000)
+        truss_properties.SetValue(KM.POISSON_RATIO   , 0     )
+        truss_properties.SetValue(KM.DENSITY         , 7856  )
         truss_properties.SetValue(KM.CONSTITUTIVE_LAW,
-            TrussConstitutiveLaw())
+            SMA.TrussConstitutiveLaw())
 
         # create a node based geometry
         node_1, node_2, curve = create_geometry(model_part)
 
         # create quadrature_point_geometries
         quadrature_point_geometries = KM.GeometriesVector()
-        curve.CreateQuadraturePointGeometries(quadrature_point_geometries, 1)
+        curve.CreateQuadraturePointGeometries(quadrature_point_geometries, 2)
 
         id = 1
         for i in range(0, len(quadrature_point_geometries)):
@@ -44,45 +43,43 @@ class TrussElementTests(KratosUnittest.TestCase):
             id += 1
 
         # add dofs
-        VariableUtils().AddDof(DISPLACEMENT_X, REACTION_X, model_part)
-        VariableUtils().AddDof(DISPLACEMENT_Y, REACTION_Y, model_part)
-        VariableUtils().AddDof(DISPLACEMENT_Z, REACTION_Z, model_part)
+        KM.VariableUtils().AddDof(KM.DISPLACEMENT_X, KM.REACTION_X, model_part)
+        KM.VariableUtils().AddDof(KM.DISPLACEMENT_Y, KM.REACTION_Y, model_part)
+        KM.VariableUtils().AddDof(KM.DISPLACEMENT_Z, KM.REACTION_Z, model_part)
 
         # apply dirichlet conditions
-        node_1.Fix(DISPLACEMENT_X)
-        node_1.Fix(DISPLACEMENT_Y)
-        node_1.Fix(DISPLACEMENT_Z)
+        node_1.Fix(KM.DISPLACEMENT_X)
+        node_1.Fix(KM.DISPLACEMENT_Y)
+        node_1.Fix(KM.DISPLACEMENT_Z)
 
-        node_2.Fix(DISPLACEMENT_Y)
-        node_2.Fix(DISPLACEMENT_Z)
+        node_2.Fix(KM.DISPLACEMENT_Y)
+        node_2.Fix(KM.DISPLACEMENT_Z)
 
         # apply neumann conditions
         prop = model_part.GetProperties()[2]
-
-        model_part.CreateNewCondition('PointLoadCondition3D1N', 2, [node_2.Id], prop)
-
-        node_2.SetSolutionStepValue(POINT_LOAD_X, 1000)
+        condition = model_part.CreateNewCondition('PointLoadCondition3D1N', 2, [node_2.Id], prop)
+        node_2.SetSolutionStepValue(SMA.POINT_LOAD_X, 1000)
 
         # setup solver
         model_part.SetBufferSize(1)
 
-        time_scheme = ResidualBasedIncrementalUpdateStaticScheme()
+        time_scheme = KM.ResidualBasedIncrementalUpdateStaticScheme()
 
-        linear_solver = linear_solver_factory.ConstructSolver(Parameters(
+        linear_solver = linear_solver_factory.ConstructSolver(KM.Parameters(
             r'{"solver_type": "skyline_lu_factorization"}'))
 
         relative_tolerance = 1e-7
         absolute_tolerance = 1e-7
 
-        conv_criteria = ResidualCriteria(relative_tolerance, absolute_tolerance)
+        conv_criteria = KM.ResidualCriteria(relative_tolerance, absolute_tolerance)
         conv_criteria.SetEchoLevel(0)
 
         maximum_iterations = 100
-        compute_reactions = False
+        compute_reactions = True
         reform_dofs_at_each_iteration = True
         move_mesh_flag = True
 
-        solver = ResidualBasedNewtonRaphsonStrategy(
+        solver = KM.ResidualBasedNewtonRaphsonStrategy(
             model_part,
             time_scheme,
             linear_solver,
@@ -93,10 +90,8 @@ class TrussElementTests(KratosUnittest.TestCase):
             move_mesh_flag
         )
 
-        solver.SetEchoLevel(0)
-
+        solver.SetEchoLevel(5)
         model_part.CloneTimeStep(1)
-
         solver.Solve()
 
         return node_1, node_2, curve
@@ -107,18 +102,18 @@ class TrussElementTests(KratosUnittest.TestCase):
             node_2 = model_part.CreateNewNode(2, 2.0, 0.0, 0.0)
 
             nodes = KM.NodesVector()
-            nodes.append(node1)
-            nodes.append(node2)
+            nodes.append(node_1)
+            nodes.append(node_2)
 
             knots_u = KM.Vector(2)
             knots_u[0] = 0.0
             knots_u[1] = 2.0
 
-            curve = NurbsCurveGeometry3D(nodes, 1, knots_u)
+            curve = KM.NurbsCurveGeometry3D(nodes, 1, knots_u)
 
             return node_1, node_2, curve
 
-        node_1, node_2, _ = IgaTrussElementTests.solve(create_geometry)
+        node_1, node_2, _ = TrussElementTests.solve(create_geometry)
 
         self.assertAlmostEqual(node_1.X, 0.0               )
         self.assertAlmostEqual(node_1.Y, 0.0               )
@@ -134,18 +129,18 @@ class TrussElementTests(KratosUnittest.TestCase):
             node_2 = model_part.CreateNewNode(2, 2.0, 0.0, 0.0)
 
             nodes = KM.NodesVector()
-            nodes.append(node1)
-            nodes.append(node2)
+            nodes.append(node_1)
+            nodes.append(node_2)
 
             knots_u = KM.Vector(2)
             knots_u[0] = 0.0
             knots_u[1] = 5.0
 
-            curve = NurbsCurveGeometry3D(nodes, 1, knots_u)
+            curve = KM.NurbsCurveGeometry3D(nodes, 1, knots_u)
 
             return node_1, node_2, curve
 
-        node_1, node_2, _ = IgaTrussElementTests.solve(create_geometry)
+        node_1, node_2, _ = TrussElementTests.solve(create_geometry)
 
         self.assertAlmostEqual(node_1.X, 0.0               )
         self.assertAlmostEqual(node_1.Y, 0.0               )
@@ -162,20 +157,20 @@ class TrussElementTests(KratosUnittest.TestCase):
             node_3 = model_part.CreateNewNode(3, 2.0, 0.0, 0.0)
 
             nodes = KM.NodesVector()
-            nodes.append(node1)
-            nodes.append(node2)
-            nodes.append(node3)
+            nodes.append(node_1)
+            nodes.append(node_2)
+            nodes.append(node_3)
 
             knots_u = KM.Vector(3)
             knots_u[0] = 0.0
             knots_u[1] = 1.0
             knots_u[2] = 2.0
 
-            curve = NurbsCurveGeometry3D(nodes, 1, knots_u)
+            curve = KM.NurbsCurveGeometry3D(nodes, 1, knots_u)
 
             return node_1, node_3, curve
 
-        node_1, node_2, _ = IgaTrussElementTests.solve(create_geometry)
+        node_1, node_2, _ = TrussElementTests.solve(create_geometry)
 
         self.assertAlmostEqual(node_1.X, 0.0)
         self.assertAlmostEqual(node_1.Y, 0.0)
@@ -192,20 +187,20 @@ class TrussElementTests(KratosUnittest.TestCase):
             node_3 = model_part.CreateNewNode(3, 2.0, 0.0, 0.0)
 
             nodes = KM.NodesVector()
-            nodes.append(node1)
-            nodes.append(node2)
-            nodes.append(node3)
+            nodes.append(node_1)
+            nodes.append(node_2)
+            nodes.append(node_3)
 
             knots_u = KM.Vector(3)
             knots_u[0] = 0.0
             knots_u[1] = 3.0
             knots_u[2] = 4.0
 
-            curve = NurbsCurveGeometry3D(nodes, 1, knots_u)
+            curve = KM.NurbsCurveGeometry3D(nodes, 1, knots_u)
 
             return node_1, node_3, curve
 
-        node_1, node_2, _ = IgaTrussElementTests.solve(create_geometry)
+        node_1, node_2, _ = TrussElementTests.solve(create_geometry)
 
         self.assertAlmostEqual(node_1.X, 0.0)
         self.assertAlmostEqual(node_1.Y, 0.0)
@@ -222,9 +217,9 @@ class TrussElementTests(KratosUnittest.TestCase):
             node_3 = model_part.CreateNewNode(3, 2.0, 0.0, 0.0)
 
             nodes = KM.NodesVector()
-            nodes.append(node1)
-            nodes.append(node2)
-            nodes.append(node3)
+            nodes.append(node_1)
+            nodes.append(node_2)
+            nodes.append(node_3)
 
             knots_u = KM.Vector(4)
             knots_u[0] = 0.0
@@ -232,11 +227,11 @@ class TrussElementTests(KratosUnittest.TestCase):
             knots_u[2] = 2.0
             knots_u[3] = 2.0
 
-            curve = NurbsCurveGeometry3D(nodes, 2, knots_u)
+            curve = KM.NurbsCurveGeometry3D(nodes, 2, knots_u)
 
             return node_1, node_3, curve
 
-        node_1, node_2, _ = IgaTrussElementTests.solve(create_geometry)
+        node_1, node_2, _ = TrussElementTests.solve(create_geometry)
 
         self.assertAlmostEqual(node_1.X, 0.0)
         self.assertAlmostEqual(node_1.Y, 0.0)
