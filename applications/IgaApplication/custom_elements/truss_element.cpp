3a22f3670c85ea612c3306df77e71c2c6234b60b
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-07-31 17:50:10 +1000

    Enhance element for multiple integration points

--- applications/IgaApplication/custom_elements/truss_element.cpp
+++ applications/IgaApplication/custom_elements/truss_element.cpp
@@ -115,78 +115,81 @@ void TrussElement::CalculateAll(
 
     const auto& r_geometry = GetGeometry();
     const IndexType num_dofs = r_geometry.size() * 3;
-    // get integration data
-    auto& r_integration_points = r_geometry.IntegrationPoints();
-    const double& integration_weight = r_integration_points[0].Weight();
-    const Matrix& r_DN_De = r_geometry.ShapeFunctionDerivatives(1, 0);
 
     // get properties
     const auto& properties = GetProperties();
-
-    const double E = properties[YOUNG_MODULUS];
     const double A = properties[CROSS_AREA];
     const double prestress = (GetProperties().Has(PRESTRESS_CAUCHY))
         ? properties[PRESTRESS_CAUCHY]
         : 0.0;
+    Vector E_vector = ComputeTangentModulus(rCurrentProcessInfo);
 
-    // compute base vectors
-    const array_1d<double, 3> actual_base_vector = GetActualBaseVector(0);
+    // get integration data
+    auto& r_integration_points = r_geometry.IntegrationPoints();
+    for (IndexType point_number = 0; point_number < mConstitutiveLawVector.size(); ++point_number) {
+        const double EA = E_vector[point_number] * A;
 
-    const double reference_a = norm_2(mReferenceBaseVector);
-    const double actual_a = norm_2(actual_base_vector);
+        const double& integration_weight = r_integration_points[point_number].Weight();
+        const Matrix& r_DN_De = r_geometry.ShapeFunctionDerivatives(1, point_number);
 
-    const double actual_aa = actual_a * actual_a;
-    const double reference_aa = reference_a * reference_a;
+        // compute base vectors
+        const array_1d<double, 3> actual_base_vector = GetActualBaseVector(0);
 
-    // green-lagrange strain
-    const double e11_membrane = 0.5 * (actual_aa - reference_aa);
+        const double reference_a = norm_2(mReferenceBaseVector);
+        const double actual_a = norm_2(actual_base_vector);
 
-    // normal force
-    const double s11_membrane = prestress * A + e11_membrane * A * E /
-        reference_aa;
+        const double actual_aa = actual_a * actual_a;
+        const double reference_aa = reference_a * reference_a;
 
-    for (IndexType r = 0; r < num_dofs; r++) {
-        const IndexType dof_type_r = r % 3;
-        const IndexType shape_index_r = r / 3;
+        // green-lagrange strain
+        const double e11_membrane = 0.5 * (actual_aa - reference_aa);
 
-        const double epsilon_var_r = actual_base_vector[dof_type_r] *
-            r_DN_De(0, shape_index_r) / reference_aa;
+        // normal force
+        const double s11_membrane = prestress * A + e11_membrane * EA /
+            reference_aa;
 
-        if (ComputeLeftHandSide) {
-            for (IndexType s = 0; s < num_dofs; s++) {
-                const IndexType dof_type_s = s % 3;
-                const IndexType shape_index_s = s / 3;
+        for (IndexType r = 0; r < num_dofs; r++) {
+            const IndexType dof_type_r = r % 3;
+            const IndexType shape_index_r = r / 3;
 
-                const double epsilon_var_s =
-                    actual_base_vector[dof_type_s] *
-                    r_DN_De(0, shape_index_s) / reference_aa;
+            const double epsilon_var_r = actual_base_vector[dof_type_r] *
+                r_DN_De(0, shape_index_r) / reference_aa;
 
-                rLeftHandSideMatrix(r, s) = E * A * epsilon_var_r *
-                    epsilon_var_s;
+            if (ComputeLeftHandSide) {
+                for (IndexType s = 0; s < num_dofs; s++) {
+                    const IndexType dof_type_s = s % 3;
+                    const IndexType shape_index_s = s / 3;
 
-                if (dof_type_r == dof_type_s) {
-                    const double epsilon_var_rs =
-                        r_DN_De(0, shape_index_r) *
+                    const double epsilon_var_s =
+                        actual_base_vector[dof_type_s] *
                         r_DN_De(0, shape_index_s) / reference_aa;
 
-                    rLeftHandSideMatrix(r, s) += s11_membrane * epsilon_var_rs;
+                    rLeftHandSideMatrix(r, s) = EA * epsilon_var_r *
+                        epsilon_var_s;
+
+                    if (dof_type_r == dof_type_s) {
+                        const double epsilon_var_rs =
+                            r_DN_De(0, shape_index_r) *
+                            r_DN_De(0, shape_index_s) / reference_aa;
+
+                        rLeftHandSideMatrix(r, s) += s11_membrane * epsilon_var_rs;
+                    }
                 }
+            }
+
+            if (ComputeRightHandSide) {
+                rRightHandSideVector[r] = -s11_membrane * epsilon_var_r;
             }
         }
 
+        if (ComputeLeftHandSide) {
+            rLeftHandSideMatrix *= reference_a * integration_weight;
+        }
+
         if (ComputeRightHandSide) {
-            rRightHandSideVector[r] = -s11_membrane * epsilon_var_r;
+            rRightHandSideVector *= reference_a * integration_weight;
         }
     }
-
-    if (ComputeLeftHandSide) {
-        rLeftHandSideMatrix *= reference_a * integration_weight;
-    }
-
-    if (ComputeRightHandSide) {
-        rRightHandSideVector *= reference_a * integration_weight;
-    }
-
     KRATOS_CATCH("")
 }
 
@@ -205,13 +208,13 @@ Vector TrussElement::ComputeTangentModulus(
     Vector green_lagrange_strains = ZeroVector(num_integration_points);
     CalculateGreenLagrangeStrain(green_lagrange_strains);
     for (IndexType i = 0; i < num_integration_points; ++i) {
-        //Vector strain_vector = ZeroVector(mpConstitutiveLaw->GetStrainSize());
-        //strain_vector[0] = green_lagrange_strains[i];
+        Vector strain_vector = ZeroVector(mConstitutiveLawVector[i]->GetStrainSize());
+        strain_vector[0] = green_lagrange_strains[i];
 
-        //ConstitutiveLaw::Parameters Values(GetGeometry(), GetProperties(), rCurrentProcessInfo);
-        //Values.SetStrainVector(strain_vector);
+        ConstitutiveLaw::Parameters Values(GetGeometry(), GetProperties(), rCurrentProcessInfo);
+        Values.SetStrainVector(strain_vector);
 
-        //mpConstitutiveLaw->CalculateValue(Values, TANGENT_MODULUS, tangent_moduli[i]);
+        mConstitutiveLawVector[i]->CalculateValue(Values, TANGENT_MODULUS, tangent_moduli[i]);
     }
     return tangent_moduli;
 }
