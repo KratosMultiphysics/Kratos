7f25810ac0c3eff8777502553b043cfaad125736
roigcarlo <roigcarlo@gmail.com>
2020-08-03 16:01:31 +0000

    GetVaraibles now returns the name of the component and the variable type in dicc format

--- kratos/mpi/python_scripts/debug_utilities.py
+++ kratos/mpi/python_scripts/debug_utilities.py
@@ -1,46 +1,64 @@
 import KratosMultiphysics
 import KratosMultiphysics.mpi
-import KratosMultiphysics.mpi.debug_utilities as debug_utilities
 
-def GetVariable(self, VarName):
+def GetVariableAndType(varName, splitByCommponents):
     """ This method returns the variable with the given name
 
     Keyword arguments:
-    self -- It signifies an instance of a class.
-    VarName -- The name of the variable to return
+    varName -- The name of the variable to return
 
     """
 
-    if KratosGlobals.Kernel.HasDoubleVariable(VarName):
-        return KratosGlobals.Kernel.GetDoubleVariable(VarName)
-    elif KratosGlobals.Kernel.HasArrayVariable(VarName):
-        return KratosGlobals.Kernel.GetArrayVariable(VarName)
-    elif KratosGlobals.Kernel.HasArray4Variable(VarName):
-        return KratosGlobals.Kernel.GetArray4Variable(VarName)
-    elif KratosGlobals.Kernel.HasArray6Variable(VarName):
-        return KratosGlobals.Kernel.GetArray6Variable(VarName)
-    elif KratosGlobals.Kernel.HasArray9Variable(VarName):
-        return KratosGlobals.Kernel.GetArray9Variable(VarName)
-    elif KratosGlobals.Kernel.HasBoolVariable(VarName):
-        return KratosGlobals.Kernel.GetBoolVariable(VarName)
-    elif KratosGlobals.Kernel.HasIntVariable(VarName):
-        return KratosGlobals.Kernel.GetIntVariable(VarName)
-    elif KratosGlobals.Kernel.HasUnsignedIntVariable(VarName):
-        return KratosGlobals.Kernel.GetUnsignedIntVariable(VarName)
-    elif KratosGlobals.Kernel.HasVectorVariable(VarName):
-        return KratosGlobals.Kernel.GetVectorVariable(VarName)
-    elif KratosGlobals.Kernel.HasMatrixVariable(VarName):
-        return KratosGlobals.Kernel.GetMatrixVariable(VarName)
-    elif KratosGlobals.Kernel.HasStringVariable(VarName):
-        return KratosGlobals.Kernel.GetStringVariable(VarName)
-    elif KratosGlobals.Kernel.HasFlagsVariable(VarName):
-        return KratosGlobals.Kernel.GetFlagsVariable(VarName)
-    elif KratosGlobals.Kernel.HasVariableData(VarName):
-        raise ValueError("\nKernel.GetVariable() ERROR: Variable {0} is defined but is of unsupported type\n".format(VarName))
-    else:
-        raise ValueError("\nKernel.GetVariable() ERROR: Variable {0} is unknown. Maybe you need to import the application where it is defined?\n".format(VarName))
+    KratosGlobals = KratosMultiphysics.KratosGlobals
 
-def GetHistoricalVariableList(model_part, skip_full_check=True):
+    if KratosGlobals.Kernel.HasDoubleVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetDoubleVariable(varName)}
+    elif KratosGlobals.Kernel.HasArrayVariable(varName):
+        if splitByCommponents:
+            # Todo: Check if it is a symetric 2D tensor
+            for component in ["X","Y","Z","XX","YY","XY"]:
+                if KratosGlobals.Kernel.HasDoubleVariable(varName+"_"+component):
+                    yield {"name": varName+"_"+component, "value": KratosGlobals.Kernel.GetDoubleVariable(varName+"_"+component)}
+        else:
+            yield {"name": varName, "value": KratosGlobals.Kernel.GetArrayVariable(varName)}
+    elif KratosGlobals.Kernel.HasArray4Variable(varName):
+        if splitByCommponents:
+            for component in ["XX","XY","YX","YY"]:
+                yield {"name": varName+"_"+component, "value": KratosGlobals.Kernel.GetDoubleVariable(varName+"_"+component)}
+        else:
+            yield {"name": varName, "value": KratosGlobals.Kernel.GetArray4Variable(varName)}
+    elif KratosGlobals.Kernel.HasArray6Variable(varName):
+        if splitByCommponents:
+            for component in ["XX","YY","ZZ","XY","YZ","XZ"]:
+                yield {"name": varName+"_"+component, "value": KratosGlobals.Kernel.GetDoubleVariable(varName+"_"+component)}
+        else:
+            yield {"name": varName, "value": KratosGlobals.Kernel.GetArray6Variable(varName)}
+    elif KratosGlobals.Kernel.HasArray9Variable(varName):
+        if splitByCommponents:
+            for component in ["XX","XY","XZ","YX","YY","YZ","ZX","ZY","ZZ"]:
+                yield {"name": varName+"_"+component, "value": KratosGlobals.Kernel.GetDoubleVariable(varName+"_"+component)}
+        else:
+            yield {"name": varName, "value": KratosGlobals.Kernel.GetArray9Variable(varName)}
+    elif KratosGlobals.Kernel.HasBoolVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetBoolVariable(varName)}
+    elif KratosGlobals.Kernel.HasIntVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetIntVariable(varName)}
+    elif KratosGlobals.Kernel.HasUnsignedIntVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetUnsignedIntVariable(varName)}
+    elif KratosGlobals.Kernel.HasVectorVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetVectorVariable(varName)}
+    elif KratosGlobals.Kernel.HasMatrixVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetMatrixVariable(varName)}
+    elif KratosGlobals.Kernel.HasStringVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetStringVariable(varName)}
+    elif KratosGlobals.Kernel.HasFlagsVariable(varName):
+        yield {"name": varName, "value": KratosGlobals.Kernel.GetFlagsVariable(varName)}
+    elif KratosGlobals.Kernel.HasVariableData(varName):
+        raise ValueError("\nKernel.GetVariable() ERROR: Variable {0} is defined but is of unsupported type\n".format(varName))
+    else:
+        raise ValueError("\nKernel.GetVariable() ERROR: Variable {0} is unknown. Maybe you need to import the application where it is defined?\n".format(varName))
+
+def GetHistoricalVariableList(model_part, container, full_search=False):
     """ Get all variables in the historical database ( GetSolutionStepValues )
 
     Get all variables in the historical database ( GetSolutionStepValues ).
@@ -52,16 +70,13 @@ def GetHistoricalVariableList(model_part, skip_full_check=True):
 
     """
 
-    variable_names = KratosGlobals.Kernel.GetallVariablesNames()
+    variable = GetAsCommponents(GetVariableAndType(model_part.GetHistoricalVariables(container, full_search)))
 
-    # if skip_full_check:
-    # else:
-    #     for node in model_part.Nodes:
-    #         for name in variable_names:
-    #             yield GetVariable(name)
+    return []
 
 
 def GetNonHistoricalVariableList(model_part):
+    pass
 
 def CheckAll(model_part):
     CheckAllHistoricalVariables(model_part)
@@ -73,10 +88,16 @@ def CheckAllHistoricalVariables(model_part):
         debug_utilities.CheckHistoricalVariable(model_part, variable)
 
 def CheckAllNonHistoricalVariables(model_part):
-    for variable in GetNonHistoricalVariableList(model_part):
-        debug_utilities.CheckNonHistoricalVariable(model_part, variable)
+    for variable in GetHistoricalVariableList(model_part, model_part.Nodes):
+        debug_utilities.CheckHistoricalVariable(model_part, model_part.Nodes, variable)
 
-def CheckAllVariablesStatistics():
+    for variable in GetHistoricalVariableList(model_part, model_part.Elements):
+        debug_utilities.CheckHistoricalVariable(model_part, variable)
+
+    for variable in GetHistoricalVariableList(model_part, model_part.Conditions):
+        debug_utilities.CheckHistoricalVariable(model_part, variable)
+
+def CheckAllVariablesStatistics(model_part):
     for variable in GetNonHistoricalVariableList(model_part):
         CheckVariableStatistics(model_part, variable)
 
@@ -102,4 +123,4 @@ def CheckVariableStatistics(model_part, input_variable):
             fixed_count += int(node.IsFixed(input_variable))
             mean_var += node.GetSolutionStepValue(input_variable)
 
-        print("Mean", input_variable.Name(), mean_var, model_part.NumberOfNodes(), mean_var/model_part.NumberOfNodes(), fixed_count)% 
+        print("Mean", input_variable.Name(), mean_var, model_part.NumberOfNodes(), mean_var/model_part.NumberOfNodes(), fixed_count) 
