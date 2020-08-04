6c8bb081f53c2a20fe9f5597ab04dc3380ca2f15
adityaghantasala <aditya.ghantasala@gmail.com>
2020-08-04 11:51:21 +0200

    use create transpose of epetra ext

--- applications/TrilinosApplication/custom_strategies/builder_and_solvers/trilinos_block_builder_and_solver_with_constraints.h
+++ applications/TrilinosApplication/custom_strategies/builder_and_solvers/trilinos_block_builder_and_solver_with_constraints.h
@@ -36,6 +36,7 @@
 #include "Epetra_SerialDenseVector.h"
 #include "Epetra_Vector.h"
 #include "EpetraExt_MatrixMatrix.h"
+#include "Epetra_RowMatrixTransposer.h"
 
 namespace Kratos {
 
@@ -400,7 +401,6 @@ public:
         BaseType::ResizeAndInitializeVectors(pScheme, rpA, rpDx, rpb, rModelPart);
 
         ConstructMasterSlaveConstraintsStructure(rModelPart);
-        // ConstructMasterSlaveConstraintsStructureTranspose(rModelPart);
         KRATOS_CATCH("")
     }
 
@@ -429,7 +429,7 @@ protected:
     ///@{
 
     TSystemMatrixPointerType mpT;  /// The transformation matrix.
-    TSystemMatrixPointerType mpTt; /// Transpose of T
+    Epetra_CrsMatrix* mpTt; /// Transpose of T
     TSystemVectorPointerType mpConstantVector; /// This is vector containing the rigid movement of the constraint
     std::vector<IndexType> mSlaveIds;  /// The equation ids of the slaves
     std::vector<IndexType> mMasterIds; /// The equation ids of the master
@@ -459,6 +459,11 @@ protected:
             // We make T and L matrices and C vector
             auto start_build_ms_time = std::chrono::steady_clock::now();
             BuildMasterSlaveConstraints(rModelPart);
+            // START : Create transpose of T
+                auto transposer = Epetra_RowMatrixTransposer(mpT.get());
+                transposer.CreateTranspose(false, mpTt);
+
+            // END : Create transpose of T
             auto end_build_ms_time = std::chrono::steady_clock::now();
             KRATOS_INFO_IF("TrilinosBuilderAndSolverWithConstraints",BaseType::GetEchoLevel() > 0)
                     <<"Build Master-Slave constraints time : "<< std::chrono::duration_cast<std::chrono::milliseconds>(end_build_ms_time - start_build_ms_time).count()/1000.0 <<" s"<<std::endl;
@@ -487,7 +492,7 @@ protected:
             auto start_mod_a_time = std::chrono::steady_clock::now();
             { // To delete aux_mat
                 TSystemMatrixType aux_mat(Copy, mpT->RowMap(), 0);
-                err = EpetraExt::MatrixMatrix::Multiply(*mpT, false, rA, false, aux_mat, false);
+                err = EpetraExt::MatrixMatrix::Multiply(*mpTt, false, rA, false, aux_mat, false);
                 KRATOS_ERROR_IF(err != 0)<<"EpetraExt MatrixMatrix multiplication(T'*A) not successful !"<<std::endl;
                 aux_mat.FillComplete();
                 { // To delete mod_a
@@ -523,7 +528,6 @@ protected:
         KRATOS_TRY
 
         TSparseSpace::SetToZero(*mpT);
-        // TSparseSpace::SetToZero(*mpTt);
         // TSparseSpace::SetToZero(mConstantVector);
 
         // The current process info
@@ -556,7 +560,6 @@ protected:
                 slave_eq_ids_set.insert(slave_equation_ids.begin(), slave_equation_ids.end());
                 // Assemble transformation matrix
                 AssembleTMatrixContribution(*mpT, transformation_matrix, slave_equation_ids, master_equation_ids);
-                // AssembleTMatrixContribution(*mpTt, transformation_matrix, master_equation_ids, slave_equation_ids);
                 // Assemble the constant vector
                 TSparseSpace::AssembleRHS(*mpConstantVector, constant_vector, slave_equation_ids);
             } else { // Taking into account inactive constraints
@@ -573,15 +576,12 @@ protected:
             if(my_rank == dof.GetSolutionStepValue(PARTITION_INDEX))
                 if(slave_eq_ids_set.count(eq_id)==0){ // Its a master
                     mpT->ReplaceGlobalValues(1, &eq_id, 1, &eq_id, &value);
-                    // mpTt->ReplaceGlobalValues(1, &eq_id, 1, &eq_id, &value);
                 }
         }
 
         mpT->GlobalAssemble();
-        // mpTt->GlobalAssemble();
 
         TSparseSpace::WriteMatrixMarketMatrix("TMatrix.mm", *mpT, false);
-        // TSparseSpace::WriteMatrixMarketMatrix("TtMatrix.mm", *mpTt, false);
 
         KRATOS_CATCH("")
     }
@@ -693,102 +693,6 @@ protected:
 
         }
     }
-
-    virtual void ConstructMasterSlaveConstraintsStructureTranspose(ModelPart& rModelPart)
-    {
-        const int global_num_constraints = GetGlobalNumberOfConstraints(rModelPart);
-        if (global_num_constraints > 0) {
-            TSparseSpace::Clear(mpTt);
-            IndexType number_of_local_dofs = BaseType::mLastMyId - BaseType::mFirstMyId;
-            std::vector<int> local_eq_ids;
-            local_eq_ids.reserve(number_of_local_dofs);
-            // generate map - use the "temp" array here
-            for (IndexType i = 0; i != number_of_local_dofs; i++)
-                local_eq_ids.push_back( BaseType::mFirstMyId + i );
-
-            std::sort(local_eq_ids.begin(), local_eq_ids.end());
-
-            const ProcessInfo& r_current_process_info = rModelPart.GetProcessInfo();
-
-            // Constraint initial iterator
-            const auto it_const_begin = rModelPart.MasterSlaveConstraints().begin();
-            std::map<IndexType, std::set<IndexType>> indices;
-
-            Element::EquationIdVectorType slave_ids;
-            Element::EquationIdVectorType master_ids;
-
-            for (int i_const = 0; i_const < static_cast<int>(rModelPart.MasterSlaveConstraints().size()); ++i_const) {
-                auto it_const = it_const_begin + i_const;
-
-                // Detect if the constraint is active or not. If the user did not make any choice the constraint
-                // It is active by default
-                bool constraint_is_active = it_const->IsDefined(ACTIVE) ? it_const->Is(ACTIVE) : true;
-                if(constraint_is_active) {
-                    it_const->EquationIdVector(slave_ids, master_ids, r_current_process_info);
-
-                    // Master DoFs
-                    for (auto &id_i : master_ids) {
-                        indices[id_i].insert(slave_ids.begin(), slave_ids.end());
-                    }
-                }
-            }
-            for(const auto& local_eq_id:local_eq_ids)
-                indices[local_eq_id].insert(local_eq_id);
-
-            // Count the row sizes
-            std::size_t nnz = 10; // This is a guess. This means each slave has some number of masters
-            const Epetra_Map row_map(-1, local_eq_ids.size(), local_eq_ids.data(), 0, BaseType::mrComm);
-            std::set<IndexType> column_eq_ids_set;
-            for(auto& slave_masters_pair : indices){
-                for (auto it = slave_masters_pair.second.begin(); it != slave_masters_pair.second.end(); ++it) {
-                    column_eq_ids_set.insert(*it);
-                }
-            }
-
-            std::vector<int> col_eq_ids_vector(column_eq_ids_set.begin(), column_eq_ids_set.end());
-            column_eq_ids_set.clear();
-            const Epetra_Map col_map(-1, col_eq_ids_vector.size(), col_eq_ids_vector.data(), 0, BaseType::mrComm);
-            Epetra_FECrsGraph t_graph(Copy, row_map, col_map, nnz);
-            col_eq_ids_vector.clear();
-            // Actually inserting indices into the graph
-            int master_eq_id = 0;
-            for(auto& slave_masters_pair : indices){
-                master_eq_id = slave_masters_pair.first;
-                std::vector<int> slave_eq_ids(slave_masters_pair.second.begin(), slave_masters_pair.second.end());
-
-                int ierr = t_graph.InsertGlobalIndices(1, &master_eq_id, slave_eq_ids.size(), slave_eq_ids.data());
-                KRATOS_ERROR_IF(ierr != 0)
-                    << ": Epetra failure in Graph.InsertGlobalIndices. Error code: " << ierr
-                    << std::endl;
-
-                slave_masters_pair.second.clear(); //deallocating the memory
-            }
-
-
-            // The diagonal values everywhere except at the slaves
-            for(const auto& eq_id : local_eq_ids)
-            {
-                if(indices.count(eq_id) == 0)
-                {
-                    int ierr = t_graph.InsertGlobalIndices(1, &eq_id, 1, &eq_id);
-                    KRATOS_ERROR_IF(ierr != 0)
-                        << ": Epetra failure in Graph.InsertGlobalIndices. Error code: " << ierr
-                        << std::endl;
-                }
-            }
-
-            int ierr = t_graph.GlobalAssemble();
-            KRATOS_ERROR_IF(ierr != 0)
-                << ": Epetra failure in Graph.GlobalAssemble. Error code: " << ierr
-                << std::endl;
-
-            // generate a new matrix pointer according to this graph
-            TSystemMatrixPointerType p_new_t =
-                TSystemMatrixPointerType(new TSystemMatrixType(Copy, t_graph));
-            mpTt.swap(p_new_t);
-        }
-    }
-
 
     ///@}
     ///@name Protected  Access
