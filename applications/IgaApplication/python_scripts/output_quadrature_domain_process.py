4a97de8939f79ccf0c9f3626b4ec1ee63e77dca0
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-08-10 17:14:25 +1000

    add python script to process output_quadrature_domain_process.py

---
+++ applications/IgaApplication/python_scripts/output_quadrature_domain_process.py
@@ -0,0 +1,19 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+# Importing the Kratos Library
+import KratosMultiphysics
+import KratosMultiphysics.IgaApplication as KratosIga
+
+
+def Factory(settings, model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("Expected input shall be a Parameters object, encapsulating a json string")
+    return IgaOutputIntegrationDomainProcess(model, settings["Parameters"])
+
+class IgaOutputIntegrationDomainProcess(KratosMultiphysics.Process):
+
+    def __init__(self, model, params):
+        KratosMultiphysics.Process.__init__(self)
+        self.process = KratosMultiphysics.OutputQuadratureDomainProcess(model, params)
+
+    def ExecuteBeforeSolutionLoop(self):
+        self.process.ExecuteBeforeSolutionLoop()
