376ba791f3002f21edb55872957b8dd199c5c7aa
Tobias Teschemacher <tobias.teschemacher@tum.de>
2020-08-10 17:13:31 +1000

    add output_quadrature_domain_process

---
+++ applications/IgaApplication/custom_processes/output_quadrature_domain_process.cpp
@@ -0,0 +1,73 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+
+// System includes
+#include <iostream>
+#include <fstream>
+
+// External includes
+
+// Project includes
+#include "containers/model.h"
+#include "processes/output_quadrature_domain_process.h"
+
+namespace Kratos
+{
+
+OutputQuadratureDomainProcess::OutputQuadratureDomainProcess(
+    Model& rModel,
+    Parameters ThisParameters
+    ) : mrModel(rModel)
+      , mThisParameters(ThisParameters)
+{
+    mThisParameters.ValidateAndAssignDefaults(this->GetDefaultParameters());
+}
+
+void OutputQuadratureDomainProcess::ExecuteBeforeSolutionLoop()
+{
+    bool OutputGeometryElements = mThisParameters["output_geometry_elements"].GetBool();
+    bool OutputGeometryConditions = mThisParameters["output_geometry_conditions"].GetBool();
+    bool OutputBrepElements = mThisParameters["output_brep_elements"].GetBool();
+    bool OutputBrepConditions = mThisParameters["output_brep_conditions"].GetBool();
+
+    std::string model_part_name = mThisParameters["model_part_name"].GetString();
+    ModelPart& r_model_part = mrModel.GetModelPart(model_part_name);
+    std::string output_file_name = mThisParameters["output_file_name"].GetString();
+
+    std::ofstream file;
+    file.open(output_file_name);
+
+    file << "{ \"geometry_integration_points\":["
+
+    for (auto element : r_model_part.Elements()) {
+        auto integration_point = element.GetGeometry().IntegtrationPoints()[0];
+        file << "[" << element.Id << "," << element.GetGeometryParent(0).Id() << ",[" << integration_point[0] << "," << integration_point[0] << "]],"
+    }
+    /// cut off last ","
+    file.pop_back();
+    file << "}";
+    file.close();
+}
+
+const Parameters OutputQuadratureDomainProcess::GetDefaultParameters() const
+{
+    const Parameters default_parameters = Parameters(R"(
+    {
+        "output_file_name"           : "",
+        "model_part_name"            : "",
+        "output_geometry_elements"   : true,
+        "output_geometry_conditions" : false,
+        "output_brep_elements"       : false,
+        "output_brep_conditions"     : false
+    })" );
+    return default_parameters;
+}
+
+} // namespace Kratos
---
+++ applications/IgaApplication/custom_processes/output_quadrature_domain_process.h
@@ -0,0 +1,141 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+
+#if !defined(KRATOS_OUTPUT_QUADRATURE_DOMAIN_PROCESS_H_INCLUDED )
+#define  KRATOS_OUTPUT_QUADRATURE_DOMAIN_PROCESS_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "processes/process.h"
+
+namespace Kratos
+{
+
+///@name Kratos Classes
+///@{
+
+/**
+ * @class OutputQuadratureDomainProcess
+ * @ingroup KratosCore
+ * @brief This class is used in order to check results using a json file containing the solution a given model part with a certain frequency
+ * @details This stores the dababase in a class denominated ResultDatabase which considers Table to store the information, therefore being able to interpolate results
+ * @author Vicente Mataix Ferrandiz
+*/
+class KRATOS_API(KRATOS_CORE) OutputQuadratureDomainProcess
+    : public Process
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of OutputQuadratureDomainProcess
+    KRATOS_CLASS_POINTER_DEFINITION(OutputQuadratureDomainProcess);
+
+    typedef std::size_t IndexType;
+    typedef std::size_t SizeType;
+
+    /// The node type definiton
+    typedef Node<3> NodeType;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Constructor
+    OutputQuadratureDomainProcess(
+        Model& rModel,
+        Parameters ThisParameters = Parameters(R"({})")
+        );
+
+    /// Destructor.
+    virtual ~OutputQuadratureDomainProcess() {}
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    void operator()()
+    {
+        Execute();
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /// Called only once befor the solution loop
+    void ExecuteBeforeSolutionLoop() override;
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        return "OutputQuadratureDomainProcess";
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override
+    {
+        rOStream << "OutputQuadratureDomainProcess";
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override
+    {
+    }
+
+private:
+    ///@name Member Variables
+    ///@{
+
+    /// Model part and different settings
+    Model& mrModel;         /// The main model part
+    Parameters mThisParameters;     /// The parameters (can be used for general pourposes)
+
+    ///@}
+
+}; // Class OutputQuadratureDomainProcess
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+/// input stream function
+inline std::istream& operator >> (std::istream& rIStream,
+                                  OutputQuadratureDomainProcess& rThis);
+
+/// output stream function
+inline std::ostream& operator << (std::ostream& rOStream,
+                                  const OutputQuadratureDomainProcess& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+///@}
+
+
+}  // namespace Kratos.
+
+#endif // KRATOS_OUTPUT_QUADRATURE_DOMAIN_PROCESS_H_INCLUDED  defined
