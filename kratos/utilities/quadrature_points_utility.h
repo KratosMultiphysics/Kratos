6b9fef0cbc36785e89e6a52e0e172684d1323d7f
tteschemacher <tteschemacher@users.noreply.github.com>
2020-08-11 13:12:16 +1000

    Add Curve3D quadrature points without parent

--- kratos/utilities/quadrature_points_utility.h
+++ kratos/utilities/quadrature_points_utility.h
@@ -196,6 +196,11 @@ namespace Kratos
                 QuadraturePointGeometry<TPointType, 2, 1>>(
                     rPoints,
                     rShapeFunctionContainer);
+            else if (WorkingSpaceDimension == 3 && LocalSpaceDimension == 1)
+                return Kratos::make_shared<
+                QuadraturePointGeometry<TPointType, 3, 1>>(
+                    rPoints,
+                    rShapeFunctionContainer);
             else if (WorkingSpaceDimension == 2 && LocalSpaceDimension == 2)
                 return Kratos::make_shared<
                 QuadraturePointGeometry<TPointType, 2>>(
