24bac9288a03e903c168ac40f8b03323f1bc106c
tteschemacher <tteschemacher@users.noreply.github.com>
2020-08-11 13:09:50 +1000

    Add creation of 3D curve quadrature points

--- kratos/utilities/quadrature_points_utility.h
+++ kratos/utilities/quadrature_points_utility.h
@@ -148,6 +148,12 @@ namespace Kratos
                         rPoints,
                         rShapeFunctionContainer,
                         pGeometryParent);
+            else if (WorkingSpaceDimension == 3 && LocalSpaceDimension == 1)
+                return Kratos::make_shared<
+                    QuadraturePointGeometry<TPointType, 3, 1>>(
+                        rPoints,
+                        rShapeFunctionContainer,
+                        pGeometryParent);
             else if (WorkingSpaceDimension == 2 && LocalSpaceDimension == 2)
                 return Kratos::make_shared<
                     QuadraturePointGeometry<TPointType, 2>>(
