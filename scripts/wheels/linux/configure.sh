cff07a22283339aedd7bacdedbba90a5e6fde4b9
Armin Geiser <armin.geiser@tum.de>
2020-08-16 13:23:37 +0200

    add ShapeOpt to wheels

--- scripts/wheels/linux/configure.sh
+++ scripts/wheels/linux/configure.sh
@@ -24,17 +24,18 @@ export PYTHON_EXECUTABLE=$1
 # Set applications to compile
 export KRATOS_APPLICATIONS=
 add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
-add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
-add_app ${KRATOS_APP_DIR}/DEMApplication
-add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication
-add_app ${KRATOS_APP_DIR}/ParticleMechanicsApplication;
+#add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+#add_app ${KRATOS_APP_DIR}/DEMApplication
+#add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication
+#add_app ${KRATOS_APP_DIR}/ParticleMechanicsApplication;
 add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication;
 #add_app ${KRATOS_APP_DIR}/DamApplication;
-add_app ${KRATOS_APP_DIR}/PoromechanicsApplication;
-add_app ${KRATOS_APP_DIR}/FSIApplication;
-add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication;
+#add_app ${KRATOS_APP_DIR}/PoromechanicsApplication;
+#add_app ${KRATOS_APP_DIR}/FSIApplication;
+#add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication;
 add_app ${KRATOS_APP_DIR}/ExternalSolversApplication;
 add_app ${KRATOS_APP_DIR}/EigenSolversApplication;
+add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication;
 
 # Clean
 rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/cmake_install.cmake"
