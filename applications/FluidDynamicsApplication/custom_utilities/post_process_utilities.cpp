5520e65371d8d8ec9fb569ec80a1e7b5d87d4f35
adityaghantasala <aditya.ghantasala@gmail.com>
2020-08-19 11:20:38 +0200

    formatting

--- applications/FluidDynamicsApplication/custom_utilities/post_process_utilities.cpp
+++ applications/FluidDynamicsApplication/custom_utilities/post_process_utilities.cpp
@@ -4,10 +4,11 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:        BSD License
+//                  Kratos default license: kratos/license.txt
 //
-//  Main authors:    Miguel Angel Celigueta
+//  Main authors:   Miguel Angel Celigueta
+//
 
 #include "post_process_utilities.h"
 
--- applications/FluidDynamicsApplication/custom_utilities/post_process_utilities.h
+++ applications/FluidDynamicsApplication/custom_utilities/post_process_utilities.h
@@ -4,10 +4,11 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:      BSD License
+//                Kratos default license: kratos/license.txt
 //
-//  Main authors:    Miguel Angel Celigueta
+//  Main authors: Miguel Angel Celigueta
+//
 
 #ifndef POST_PROCESS_UTILITIES_H
 #define POST_PROCESS_UTILITIES_H
