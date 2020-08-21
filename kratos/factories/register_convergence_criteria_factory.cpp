f3edde00556e01757e5c19ff4797cad09f826fb7
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@inria.fr>
2020-08-21 12:52:11 +0200

    Register MixedGenericCriteria

--- kratos/factories/register_convergence_criteria_factory.cpp
+++ kratos/factories/register_convergence_criteria_factory.cpp
@@ -25,6 +25,7 @@
 #include "solving_strategies/convergencecriterias/residual_criteria.h"
 #include "solving_strategies/convergencecriterias/and_criteria.h"
 #include "solving_strategies/convergencecriterias/or_criteria.h"
+#include "solving_strategies/convergencecriterias/mixed_generic_criteria.h"
 
 namespace Kratos
 {
@@ -37,18 +38,21 @@ void RegisterConvergenceCriterias()
     typedef ResidualCriteria<SpaceType,  LocalSpaceType> ResidualCriteriaType;
     typedef And_Criteria<SpaceType,  LocalSpaceType> And_CriteriaType;
     typedef Or_Criteria<SpaceType,  LocalSpaceType> Or_CriteriaType;
+    typedef MixedGenericCriteria<SpaceType,  LocalSpaceType> MixedGenericCriteriaType;
 
     //NOTE: here we must create persisting objects for the linear solvers
     static DisplacementCriteriaType msDisplacementCriteria;
     static ResidualCriteriaType msResidualCriteria;
     static And_CriteriaType msAnd_Criteria;
     static Or_CriteriaType msOr_Criteria;
+    static MixedGenericCriteriaType mMixedGenericCriteria;
 
     // Registration of convergence solvers
     KRATOS_REGISTER_CONVERGENCE_CRITERIA(DisplacementCriteriaType::Name(), msDisplacementCriteria);
     KRATOS_REGISTER_CONVERGENCE_CRITERIA(ResidualCriteriaType::Name(), msResidualCriteria);
     KRATOS_REGISTER_CONVERGENCE_CRITERIA(And_CriteriaType::Name(), msAnd_Criteria );
     KRATOS_REGISTER_CONVERGENCE_CRITERIA(Or_CriteriaType::Name(), msOr_Criteria );
+    KRATOS_REGISTER_CONVERGENCE_CRITERIA(MixedGenericCriteriaType::Name(), mMixedGenericCriteria );
 };
 } // Namespace Kratos
 
