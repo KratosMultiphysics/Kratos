1cd2a0581c5d0cf5c2e2a33688c2f96f5155013a
roigcarlo <roigcarlo@gmail.com>
2020-08-21 17:23:16 +0000

    add buit in test for line_prof

--- kratos/tests/test_decorator_injector.py
+++ kratos/tests/test_decorator_injector.py
@@ -2,6 +2,10 @@ from __future__ import print_function, absolute_import, division
 
 import sys
 import types
+import builtins
+
+import line_profiler
+
 import KratosMultiphysics
 import KratosMultiphysics.decorator_injector as DecroatorInjector
 import KratosMultiphysics.KratosUnittest as KratosUnittest
@@ -11,12 +15,21 @@ def GetFilePath(fileName):
 
 def dummy_decorator_repeat(func):
     def wrapper(*args, **kwargs):
-        print("Wrapped Call")
         func(*args, **kwargs)
         func(*args, **kwargs)
     return wrapper
 
 class TestNodalDivergence(KratosUnittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.prof = line_profiler.LineProfiler()
+        if 'profile' not in builtins.__dict__ or builtins.__dict__['profile'] is None:
+            builtins.__dict__['profile'] = cls.prof
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.prof.print_stats()
 
     def setUp(self):
         def add_once(input):
@@ -26,7 +39,8 @@ class TestNodalDivergence(KratosUnittest.TestCase):
         def add_once_decorated(input):
             input[0] += 1
 
-        virtual_module = types.ModuleType('VirtualModule', 'Module created to provide a context for tests')
+        # Generate a virtual module. This should be equivalent to "import VirtualModule"
+        virtual_module = types.ModuleType('VirtualModule', 'Module created to provide a context for the decorator injector')
         virtual_module.__dict__.update({
             "add_once":add_once,
             "add_once_decorated":add_once_decorated
@@ -52,12 +66,26 @@ class TestNodalDivergence(KratosUnittest.TestCase):
         self.virtual_module.add_once(a)
         self.assertEqual(a[0], 2)
 
-    def test_virtual_module_decorated__injected(self):
+    def test_virtual_module_decorated_injected(self):
         DecroatorInjector.InjectIntoModule(self.virtual_module, dummy_decorator_repeat, lambda *x: True)
 
         a = [0]
         self.virtual_module.add_once_decorated(a)
         self.assertEqual(a[0], 4)
+
+    def test_virtual_module_profiler_injected(self):
+        DecroatorInjector.InjectIntoModule(self.virtual_module, builtins.__dict__['profile'], lambda *x: True)
+
+        a = [0]
+        self.virtual_module.add_once(a)
+        self.assertEqual(a[0], 1)
+
+    def test_virtual_module_profiler_decorated_injected(self):
+        DecroatorInjector.InjectIntoModule(self.virtual_module, builtins.__dict__['profile'], lambda *x: True)
+
+        a = [0]
+        self.virtual_module.add_once_decorated(a)
+        self.assertEqual(a[0], 2)
 
     def tearDown(self):
         pass
