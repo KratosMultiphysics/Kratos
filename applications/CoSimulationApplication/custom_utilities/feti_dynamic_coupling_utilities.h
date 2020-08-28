f4e32735ec31ae37d4c002c860206fe87711f607
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-08-28 14:16:22 +1000

    fix CI error

--- applications/CoSimulationApplication/custom_utilities/feti_dynamic_coupling_utilities.h
+++ applications/CoSimulationApplication/custom_utilities/feti_dynamic_coupling_utilities.h
@@ -45,10 +45,7 @@ namespace Kratos
         typedef Matrix DenseMappingMatrixType;
         typedef Kratos::shared_ptr<DenseMappingMatrixType> DenseMappingMatrixUniquePointerType;
 
-        typedef UblasSpace<double, CompressedMatrix, boost::numeric::ublas::vector<double>> SparseSpaceType;
         typedef typename SparseSpaceType::MatrixType MappingMatrixType;
-
-
 
         FetiDynamicCouplingUtilities(ModelPart & rInterfaceOrigin,
             ModelPart & rInterFaceDestination,
@@ -72,8 +69,6 @@ namespace Kratos
         void SetMappingMatrix(DenseMappingMatrixUniquePointerType pMappingMatrix)
         {
             mpMappingMatrix = pMappingMatrix;
-
-            KRATOS_WATCH(*(mpMappingMatrix.get()))
         };
 
         void EquilibrateDomains();
@@ -125,30 +120,7 @@ namespace Kratos
             const Variable< array_1d<double, 3> >& rVariable,
             const Vector& rCorrection);
 
-        void PrintInterfaceVelocity(const bool IsOrigin)
-        {
-            const SizeType dim_origin = mpOriginDomain->ElementsBegin()->GetGeometry().WorkingSpaceDimension();
-            const SizeType origin_interface_dofs = dim_origin * mrOriginInterfaceModelPart.NumberOfNodes();
-            Vector interface_velocities(origin_interface_dofs);
-
-            ModelPart& r_interface = (IsOrigin) ? mrOriginInterfaceModelPart : mrDestinationInterfaceModelPart;
-
-            auto interface_nodes = r_interface.NodesArray();
-            for (size_t i = 0; i < interface_nodes.size(); i++)
-            {
-                IndexType interface_id = interface_nodes[i]->GetValue(INTERFACE_EQUATION_ID);
-                array_1d<double, 3>& vel = interface_nodes[i]->FastGetSolutionStepValue(VELOCITY);
-
-                for (size_t dof = 0; dof < dim_origin; dof++)
-                {
-                    interface_velocities[interface_id * dim_origin + dof] = vel[dof];
-                }
-            }
-
-            std::cout << "\n\nInterface velocities, is origin = " << IsOrigin
-                << "\n" << interface_velocities << "\n\n";
-        }
-
+        // Printing method for debugging
         void PrintInterfaceKinematics(const Variable< array_1d<double, 3> >& rVariable, const bool IsOrigin)
         {
             const SizeType dim_origin = mpOriginDomain->ElementsBegin()->GetGeometry().WorkingSpaceDimension();
