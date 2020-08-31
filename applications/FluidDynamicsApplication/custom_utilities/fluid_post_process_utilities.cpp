45442d9fe21b29423395b4eb082a5577b6a23b87
adityaghantasala <aditya.ghantasala@gmail.com>
2020-08-31 16:26:32 +0200

    reverting use of integration points

--- applications/FluidDynamicsApplication/custom_utilities/fluid_post_process_utilities.cpp
+++ applications/FluidDynamicsApplication/custom_utilities/fluid_post_process_utilities.cpp
@@ -24,37 +24,27 @@ namespace Kratos {
         auto& r_data_comm = rModelPart.GetCommunicator().GetDataCommunicator();
 
         #pragma omp parallel for reduction(+:flow)
-        for(int i_c=0; i_c<num_conditions;++i_c)
-        {
+        for(int i_c=0; i_c<num_conditions;++i_c) {
+
             auto it_cond = conditions_begin + i_c;
             const auto& r_geom = it_cond->GetGeometry();
-            const IndexType num_nodes = r_geom.size();
 
-            const IntegrationMethodType integration_method = r_geom.GetDefaultIntegrationMethod();
-            const auto& integration_points = r_geom.IntegrationPoints(integration_method);
-            const Matrix& rNcontainer = r_geom.ShapeFunctionsValues(integration_method);
-            const int num_integration_points = (int)integration_points.size();
+            const double area = r_geom.Area();
+            GeometryType::CoordinatesArrayType point_local;
+            r_geom.PointLocalCoordinates(point_local, r_geom.Center()) ;
+            array_1d<double,3> unit_normal = r_geom.Normal(point_local);
+
+            if( area < std::numeric_limits<double>::epsilon())
+                unit_normal *= 0.0;
+            else
+                unit_normal *= 1/area;
 
             double average_velocity = 0.0;
-            const double area = r_geom.Area();
 
-            for (int point_number = 0; point_number < num_integration_points; ++point_number)
-            {
-                const auto point_local = integration_points[point_number];
-                array_1d<double,3> unit_normal = r_geom.Normal(point_local);
-                if( area < std::numeric_limits<double>::epsilon())
-                    unit_normal *= 0.0;
-                else
-                    unit_normal *= 1/area;
-
-                // Calculating the velocity on the gauss point
-                Vector gauss_velocity = ZeroVector(3);
-                for ( IndexType ii = 0; ii < num_nodes; ii++ ) {
-                    gauss_velocity += rNcontainer( point_number, ii ) * r_geom[ii].FastGetSolutionStepValue( VELOCITY );
-                }
-                average_velocity += MathUtils<double>::Dot(gauss_velocity, unit_normal);
+            for (int j=0; j<(int)r_geom.size(); j++) {
+                average_velocity += MathUtils<double>::Dot(r_geom[j].FastGetSolutionStepValue(VELOCITY), unit_normal);
             }
-            average_velocity /= num_integration_points;
+            average_velocity *= (1.0 / (double)r_geom.size());
 
             const double condition_flow = area * average_velocity;
 
