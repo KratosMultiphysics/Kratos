889a21c6208ca63e6d1be76194219c5db85c6745
philbucher <philipp.bucher@tum.de>
2020-09-01 16:39:48 +0200

    adding empty application

---
+++ applications/ConstitutiveLawsApplication/constitutive_laws_application_variables.h
@@ -0,0 +1,28 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    @{KRATOS_APP_AUTHOR}
+//
+
+#if !defined(KRATOS_CONSTITUTIVE_LAWS_APPLICATION_VARIABLES_H_INCLUDED )
+#define  KRATOS_CONSTITUTIVE_LAWS_APPLICATION_VARIABLES_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+
+namespace Kratos
+{
+
+}
+
+#endif // KRATOS_CONSTITUTIVE_LAWS_APPLICATION_VARIABLES_H_INCLUDED
