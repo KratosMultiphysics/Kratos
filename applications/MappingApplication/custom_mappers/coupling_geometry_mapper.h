720f590809723d0ed60d476e33022af3e4d4632e
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-09-03 10:26:26 +1000

    add sparsespacetype

--- applications/MappingApplication/custom_mappers/coupling_geometry_mapper.h
+++ applications/MappingApplication/custom_mappers/coupling_geometry_mapper.h
@@ -103,6 +103,7 @@ public:
     typedef Matrix DenseMappingMatrixType;
     typedef Kratos::unique_ptr<DenseMappingMatrixType> DenseMappingMatrixUniquePointerType;
 
+    typedef UblasSpace<double, CompressedMatrix, boost::numeric::ublas::vector<double>> SparseSpaceType;
     typedef typename UblasSpace<double, Matrix, Vector> LocalSpaceType;
     typedef typename LinearSolver<SparseSpaceType, LocalSpaceType> LinearSolverType;
     typedef Kratos::shared_ptr<LinearSolverType> LinearSolverSharedPointerType;
