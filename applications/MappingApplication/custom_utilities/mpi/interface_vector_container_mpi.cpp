4af04b5e81a9dada95cb5e31e339f757dcc2f40e
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-09-03 13:27:48 +1000

    Update interface_vector_container_mpi.cpp

--- applications/MappingApplication/custom_utilities/mpi/interface_vector_container_mpi.cpp
+++ applications/MappingApplication/custom_utilities/mpi/interface_vector_container_mpi.cpp
@@ -24,10 +24,10 @@
 
 namespace Kratos
 {
-typedef typename MapperDefinitions::MPISparseSpaceType SparseSpaceType;
+typedef typename MapperDefinitions::MPISparseSpaceType MPISparseSpaceType;
 typedef typename MapperDefinitions::DenseSpaceType DenseSpaceType;
 
-typedef InterfaceVectorContainer<SparseSpaceType, DenseSpaceType> VectorContainerType;
+typedef InterfaceVectorContainer<MPISparseSpaceType, DenseSpaceType> VectorContainerType;
 
 /***********************************************************************************/
 /* PUBLIC Methods */
@@ -48,6 +48,6 @@ void VectorContainerType::UpdateModelPartFromSystemVector(const Variable<double>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Class template instantiation
-template class InterfaceVectorContainer< SparseSpaceType, DenseSpaceType >;
+template class InterfaceVectorContainer< MPISparseSpaceType, DenseSpaceType >;
 
 }  // namespace Kratos.
