b172b1767b46b27e99fe43dbcaadd5cd4c94fc3a
philbucher <philipp.bucher@tum.de>
2020-09-04 17:00:39 +0200

    first changes

--- applications/CoSimulationApplication/python_scripts/utilities/model_part_utilities.py
+++ applications/CoSimulationApplication/python_scripts/utilities/model_part_utilities.py
@@ -1,11 +1,13 @@
 # Importing the Kratos Library
 import KratosMultiphysics as KM
 
-def AllocateHistoricalVariablesFromCouplingData(data_list, model, solver_name):
+def AllocateHistoricalVariablesFromCouplingDataSettings(data_settings_list, model, solver_name):
     '''This function retrieves the historical variables that are needed for the ModelParts from the
-    specified CouplingInterfaceDatas and allocates them on the ModelParts
+    specified CouplingInterfaceData-settings and allocates them on the ModelParts
     Note that it can only be called after the (Main-)ModelParts are created
     '''
+    data_settings_list = data_settings_list.Clone() # clone to not mess with the following validation
+
     for data in data_list:
         hist_var_dict = data.GetHistoricalVariableDict()
         for full_model_part_name, variable in hist_var_dict.items():
@@ -17,9 +19,11 @@ def AllocateHistoricalVariablesFromCouplingData(data_list, model, solver_name):
                 cs_print_info("CoSimTools", 'Allocating historical variable "{}" in ModelPart "{}" for solver "{}"'.format(variable.Name(), main_model_part_name, solver_name))
                 main_model_part.AddNodalSolutionStepVariable(variable)
 
-def CreateMainModelPartsFromCouplingData(data_list, model, solver_name):
-    '''This function creates the Main-ModelParts that are used in the specified CouplingInterfaceDatas
+def CreateMainModelPartsFromCouplingDataSettings(data_settings_list, model, solver_name):
+    '''This function creates the Main-ModelParts that are used in the specified CouplingInterfaceData-settings
     '''
+    data_settings_list = data_settings_list.Clone() # clone to not mess with the following validation
+
     for data in data_list:
         main_model_part_name = data.model_part_name.split(".")[0]
         if not model.HasModelPart(main_model_part_name):
@@ -36,9 +40,11 @@ def RecursiveCreateModelParts(model_part, model_part_name):
     if len(sub_model_part_names) > 0:
         RecursiveCreateModelParts(model_part, ".".join(sub_model_part_names))
 
-def CreateModelPartsFromCouplingData(data_list, model, solver_name):
-    '''This function creates the ModelParts-hierarchie that are used in the specified CouplingInterfaceDatas
+def CreateModelPartsFromCouplingDataSettings(data_settings_list, model, solver_name):
+    '''This function creates the ModelParts-hierarchie that are used in the specified CouplingInterfaceData-settings
     '''
+    data_settings_list = data_settings_list.Clone() # clone to not mess with the following validation
+
     for data in data_list:
         splitted_name = data.model_part_name.split(".")
         main_model_part_name = splitted_name[0]
