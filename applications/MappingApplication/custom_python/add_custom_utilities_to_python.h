214df3284720ed4c3cda5d3dc140e4683da36cec
peterjwilson <p.j.wilson@uqconnect.edu.au>
2020-09-11 09:59:16 +1000

    revert files to master

--- applications/MappingApplication/custom_python/add_custom_utilities_to_python.h
+++ applications/MappingApplication/custom_python/add_custom_utilities_to_python.h
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-// KRATOS  ___|  |                   |                   |
-//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
-//             | |   |    |   | (    |   |   | |   (   | |
-//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
-//
-//  License:		 BSD License
-//					 license: structural_mechanics_application/license.txt
-//
-//  Main authors:    Riccardo Rossi
-//
-
-
-#if !defined(KRATOS_ADD_UTILITIES_TO_PYTHON_H_INCLUDED )
-#define  KRATOS_ADD_UTILITIES_TO_PYTHON_H_INCLUDED
-
-
-
-// System includes
-#include <pybind11/pybind11.h>
-
-// External includes
-
-
-// Project includes
-#include "includes/define_python.h"
-
-
-namespace Kratos
-{
-
-namespace Python
-{
-
-void  AddCustomUtilitiesToPython(pybind11::module& m);
-
-}  // namespace Python.
-
-}  // namespace Kratos.
-
-#endif // KRATOS_ADD_UTILITIES_TO_PYTHON_H_INCLUDED  defined
-=======
 
 //    |  /           |
 //    ' /   __| _` | __|  _ \   __|
@@ -76,4 +34,3 @@ void AddCustomUtilitiesToPython(pybind11::module& m);
 }  // namespace Kratos.
 
 #endif // KRATOS_MAPPING_ADD_UTILITIES_TO_PYTHON_H_INCLUDED  defined
->>>>>>> master
