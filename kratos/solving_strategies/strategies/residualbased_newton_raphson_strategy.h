5be3da72225167e5f7e419ee3d8c98afedbd3a63
Philipp Bucher <philipp.bucher@tum.de>
2020-09-14 14:47:19 +0200

    using full name

--- kratos/solving_strategies/strategies/residualbased_newton_raphson_strategy.h
+++ kratos/solving_strategies/strategies/residualbased_newton_raphson_strategy.h
@@ -807,24 +807,24 @@ class ResidualBasedNewtonRaphsonStrategy
                 //setting up the list of the DOFs to be solved
                 BuiltinTimer setup_dofs_time;
                 p_builder_and_solver->SetUpDofSet(p_scheme, r_model_part);
-                KRATOS_INFO_IF("NewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "Setup Dofs Time: "
+                KRATOS_INFO_IF("ResidualBasedNewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "Setup Dofs Time: "
                     << setup_dofs_time.ElapsedSeconds() << std::endl;
 
                 //shaping correctly the system
                 BuiltinTimer setup_system_time;
                 p_builder_and_solver->SetUpSystem(r_model_part);
-                KRATOS_INFO_IF("NewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "Setup System Time: "
+                KRATOS_INFO_IF("ResidualBasedNewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "Setup System Time: "
                     << setup_system_time.ElapsedSeconds() << std::endl;
 
                 //setting up the Vectors involved to the correct size
                 BuiltinTimer system_matrix_resize_time;
                 p_builder_and_solver->ResizeAndInitializeVectors(p_scheme, mpA, mpDx, mpb,
                                                                  r_model_part);
-                KRATOS_INFO_IF("NewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "System Matrix Resize Time: "
+                KRATOS_INFO_IF("ResidualBasedNewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "System Matrix Resize Time: "
                     << system_matrix_resize_time.ElapsedSeconds() << std::endl;
             }
 
-            KRATOS_INFO_IF("NewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "System Construction Time: "
+            KRATOS_INFO_IF("ResidualBasedNewtonRaphsonStrategy", BaseType::GetEchoLevel() > 0) << "System Construction Time: "
                 << system_construction_time.ElapsedSeconds() << std::endl;
 
             TSystemMatrixType& rA  = *mpA;
@@ -1032,7 +1032,7 @@ class ResidualBasedNewtonRaphsonStrategy
         if (iteration_number >= mMaxIterationNumber) {
             MaxIterationsExceeded();
         } else {
-            KRATOS_INFO_IF("NewtonRaphsonStrategy", this->GetEchoLevel() > 0)
+            KRATOS_INFO_IF("ResidualBasedNewtonRaphsonStrategy", this->GetEchoLevel() > 0)
                 << "Convergence achieved after " << iteration_number << " / "
                 << mMaxIterationNumber << " iterations" << std::endl;
         }
@@ -1353,7 +1353,7 @@ class ResidualBasedNewtonRaphsonStrategy
 
     virtual void MaxIterationsExceeded()
     {
-        KRATOS_INFO_IF("NewtonRaphsonStrategy", this->GetEchoLevel() > 0)
+        KRATOS_INFO_IF("ResidualBasedNewtonRaphsonStrategy", this->GetEchoLevel() > 0)
             << "ATTENTION: max iterations ( " << mMaxIterationNumber
             << " ) exceeded!" << std::endl;
     }
