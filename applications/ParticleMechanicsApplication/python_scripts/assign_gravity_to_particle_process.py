ce767cb26d0ae584181f0c1d3602264dfcf9005b
V. Singer <veronika.singer@tum.de>
2020-09-18 12:11:27 +0200

    updating gravity (to check)

--- applications/ParticleMechanicsApplication/python_scripts/assign_gravity_to_particle_process.py
+++ applications/ParticleMechanicsApplication/python_scripts/assign_gravity_to_particle_process.py
@@ -42,12 +42,52 @@ class AssignGravityToParticleProcess(KratosMultiphysics.Process):
 
         # Default settings
         self.model_part = Model[settings["model_part_name"].GetString()]
-        self.modulus = settings["modulus"].GetDouble()
+        
         self.gravity_direction = settings["direction"].GetVector()
-        self.gravity_acceleration = self.modulus * self.gravity_direction
+        #self.gravity_acceleration = self.modulus * self.gravity_direction
+        self.value_is_numeric = False
+        if settings["modulus"].IsNumber():
+            self.value_is_numeric = True
+            self.modulus = settings["modulus"].GetDouble()
+            self.gravity_acceleration = self.gravity_direction * self.modulus
+        else:
+            self.function_string = settings["modulus"].GetString()
+            self.aux_function = KratosMultiphysics.PythonGenericFunctionUtility(self.function_string, settings["local_axes"])
+
 
     def ExecuteBeforeSolutionLoop(self):
         # Assign gravity to MP after solver.Initialize() - only apply once at the beginning!
         for element in self.model_part.Elements:
-            element.SetValuesOnIntegrationPoints(KratosParticle.MP_VOLUME_ACCELERATION,[self.gravity_acceleration],self.model_part.ProcessInfo)
-            element.SetValuesOnIntegrationPoints(KratosParticle.MP_ACCELERATION,[self.gravity_acceleration],self.model_part.ProcessInfo)
+            if self.value_is_numeric:
+                self.gravity_acceleration = self.gravity_direction * self.modulus
+                element.SetValuesOnIntegrationPoints(KratosParticle.MP_VOLUME_ACCELERATION,[self.gravity_acceleration],self.model_part.ProcessInfo)
+                element.SetValuesOnIntegrationPoints(KratosParticle.MP_ACCELERATION,[self.gravity_acceleration],self.model_part.ProcessInfo)
+
+    def ExecuteInitializeSolutionStep(self):
+        """ This method is executed in order to initialize the current step
+
+        Keyword arguments:
+        self -- It signifies an instance of a class.
+        """
+        
+        for element in self.model_part.Elements:
+            step = self.model_part.ProcessInfo[KratosMultiphysics.STEP]
+            current_time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
+            time_step = current_time / step
+            
+           
+            if not self.value_is_numeric:
+                if self.aux_function.DependsOnSpace() == False: #depends on time only
+                    delta_gravity_acceleration = self.aux_function.CallFunction(0.0,0.0,0.0,time_step,0.0,0.0,0.0) * self.gravity_direction
+
+                volume_gravity_acceleration = element.CalculateOnIntegrationPoints(KratosParticle.MP_VOLUME_ACCELERATION,self.model_part.ProcessInfo)[0]
+                gravity_acceleration = element.CalculateOnIntegrationPoints(KratosParticle.MP_ACCELERATION,self.model_part.ProcessInfo)[0]
+
+                volume_gravity_acceleration += delta_gravity_acceleration
+                gravity_acceleration += delta_gravity_acceleration
+                        
+                
+                element.SetValuesOnIntegrationPoints(KratosParticle.MP_VOLUME_ACCELERATION,[volume_gravity_acceleration],self.model_part.ProcessInfo)
+                element.SetValuesOnIntegrationPoints(KratosParticle.MP_ACCELERATION,[gravity_acceleration],self.model_part.ProcessInfo)
+                
+            
