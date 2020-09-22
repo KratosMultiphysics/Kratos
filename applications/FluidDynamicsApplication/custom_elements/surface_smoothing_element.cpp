97f497030abfeeea61d204de541980318c6ab33d
Mohammad Hashemi <mohammadrhashemi@gmail.com>
2020-09-22 17:32:20 +0200

    fix

--- applications/FluidDynamicsApplication/custom_elements/surface_smoothing_element.cpp
+++ applications/FluidDynamicsApplication/custom_elements/surface_smoothing_element.cpp
@@ -235,7 +235,7 @@ void SurfaceSmoothingElement::CalculateLocalSystem(
     //GeometryType::Pointer p_geom = this->pGetGeometry();
     //const double he = ElementSizeCalculator<3,4>::AverageElementSize(*p_geom);
     const double he = ElementSizeCalculator<3,4>::AverageElementSize(GetGeometry()); //this->GetValue(ELEMENT_H);
-    const double epsilon = 5.0e3*dt*he*he;
+    const double epsilon = 2.0e3*dt*he*he;
     //KRATOS_INFO("smoothing coefficient:") << epsilon << std::endl;
 
     BoundedMatrix<double,num_nodes,num_dim> DN_DX;  // Gradients matrix 
