91de9c9b5e629cf4d39ccf1ef16bd247d9519fda
Joaquin Gonzalez <jgonzalez@cimne.upc.edu>
2020-09-28 16:37:35 +0200

    including fluid_element.cpp and qs_vms.cpp in qsvms_dem_coupled.h

--- applications/SwimmingDEMApplication/custom_elements/qsvms_dem_coupled.h
+++ applications/SwimmingDEMApplication/custom_elements/qsvms_dem_coupled.h
@@ -26,9 +26,11 @@
 #include "utilities/geometry_utilities.h"
 
 #include "includes/cfd_variables.h"
-#include "../../applications/FluidDynamicsApplication/custom_elements/qs_vms.h"
-#include "../../applications/FluidDynamicsApplication/custom_elements/fluid_element.h"
-#include "../../applications/FluidDynamicsApplication/fluid_dynamics_application_variables.h"
+#include "../applications/FluidDynamicsApplication/custom_elements/fluid_element.h"
+#include "../applications/FluidDynamicsApplication/custom_elements/qs_vms.h"
+#include "../applications/FluidDynamicsApplication/custom_elements/fluid_element.cpp"
+#include "../applications/FluidDynamicsApplication/custom_elements/qs_vms.cpp"
+#include "../applications/FluidDynamicsApplication/fluid_dynamics_application_variables.h"
 
 namespace Kratos
 {
