3150401f6b2bbd66feab95a3790f723cfadd3ee0
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2020-10-05 09:51:09 +0200

    fsi test in 2D

---
+++ applications/FemToDemApplication/tests/fsi_tests/wall_2d/materials.py
@@ -0,0 +1,18 @@
+
+from __future__ import print_function, absolute_import, division
+from KratosMultiphysics import *
+from KratosMultiphysics.SolidMechanicsApplication import *
+from KratosMultiphysics.FemToDemApplication import *
+
+def AssignMaterial(Properties):
+
+    prop_id = 1
+    prop = Properties[prop_id]
+    mat = HyperElasticIsotropicNeoHookeanPlaneStrain2D()
+    prop.SetValue(CONSTITUTIVE_LAW, mat.Clone())
+
+    prop_id = 2
+    prop = Properties[prop_id]
+    mat = HyperElasticIsotropicNeoHookeanPlaneStrain2D()
+    prop.SetValue(CONSTITUTIVE_LAW, mat.Clone())
+
