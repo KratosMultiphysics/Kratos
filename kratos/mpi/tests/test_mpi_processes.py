dd7d3c9b49dc973d311ba3ad8f5b85e8a24d855a
marcnunezc <marcnc94@gmail.com>
2020-10-06 09:04:57 +0200

    Extending test to cover all hist-non-hist combinations

--- kratos/mpi/tests/test_mpi_processes.py
+++ kratos/mpi/tests/test_mpi_processes.py
@@ -30,18 +30,33 @@ class TestMPIProcesses(KratosUnittest.TestCase):
         for node in main_model_part.Nodes:
             distance = node.X**2+node.Y**2+node.Z**2 - 1
             node.SetSolutionStepValue(KratosMultiphysics.DISTANCE,distance)
-
-        local_gradient = KratosMultiphysics.ComputeNodalGradientProcess(main_model_part,
-        KratosMultiphysics.DISTANCE,
-        KratosMultiphysics.DISTANCE_GRADIENT,
-        KratosMultiphysics.NODAL_AREA)
-        local_gradient.Execute()
+            node.SetValue(KratosMultiphysics.DISTANCE,distance)
 
         ## Read reference
         file_name = "auxiliar_files_for_python_unittest/reference_files/test_compute_nodal_gradient_process_results.json"
         reference_file_name = GetFilePath(file_name)
         with open(reference_file_name, 'r') as f:
             reference_values = json.load(f)
+
+        gradient_process_hist_hist = KratosMultiphysics.ComputeNodalGradientProcess(main_model_part,
+        KratosMultiphysics.DISTANCE,
+        KratosMultiphysics.DISTANCE_GRADIENT,
+        KratosMultiphysics.NODAL_AREA)
+        gradient_process_hist_hist.Execute()
+
+        for node in main_model_part.Nodes:
+                distance_gradient = node.GetSolutionStepValue(KratosMultiphysics.DISTANCE_GRADIENT)
+                ref_gradient = reference_values[str(node.Id)]
+                for gradient_i, gradient_i_ref in zip(distance_gradient, ref_gradient):
+                    self.assertAlmostEqual(gradient_i, gradient_i_ref)
+
+        non_historical_origin_variable = True
+        gradient_process_non_hist_hist = KratosMultiphysics.ComputeNodalGradientProcess(main_model_part,
+        KratosMultiphysics.DISTANCE,
+        KratosMultiphysics.DISTANCE_GRADIENT,
+        KratosMultiphysics.NODAL_AREA,
+        non_historical_origin_variable)
+        gradient_process_non_hist_hist.Execute()
 
         for node in main_model_part.Nodes:
             distance_gradient = node.GetSolutionStepValue(KratosMultiphysics.DISTANCE_GRADIENT)
@@ -57,6 +72,7 @@ class TestMPIProcesses(KratosUnittest.TestCase):
 
         ## Add variables to the model part
         main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.PARTITION_INDEX)
+        main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE)
 
         ## Serial partition of the original .mdpa file
         input_filename = GetFilePath("auxiliar_files_for_python_unittest/mdpa_files/test_mpi_serializer")
@@ -67,15 +83,22 @@ class TestMPIProcesses(KratosUnittest.TestCase):
 
         for node in main_model_part.Nodes:
             distance = node.X**2+node.Y**2+node.Z**2 - 1
+            node.SetSolutionStepValue(KratosMultiphysics.DISTANCE,distance)
             node.SetValue(KratosMultiphysics.DISTANCE,distance)
 
-        non_historical_gradient_variable = True
-        local_gradient = KratosMultiphysics.ComputeNonHistoricalNodalGradientProcess(main_model_part,
+        gradient_process_hist_non_hist = KratosMultiphysics.ComputeNonHistoricalNodalGradientProcess(main_model_part,
+        KratosMultiphysics.DISTANCE,
+        KratosMultiphysics.DISTANCE_GRADIENT,
+        KratosMultiphysics.NODAL_AREA)
+        gradient_process_hist_non_hist.Execute()
+
+        non_historical_origin_variable = True
+        gradient_process_non_hist_non_hist = KratosMultiphysics.ComputeNonHistoricalNodalGradientProcess(main_model_part,
         KratosMultiphysics.DISTANCE,
         KratosMultiphysics.DISTANCE_GRADIENT,
         KratosMultiphysics.NODAL_AREA,
-        non_historical_gradient_variable)
-        local_gradient.Execute()
+        non_historical_origin_variable)
+        gradient_process_non_hist_non_hist.Execute()
 
         ## Read reference
         file_name = "auxiliar_files_for_python_unittest/reference_files/test_compute_nodal_gradient_process_results.json"
