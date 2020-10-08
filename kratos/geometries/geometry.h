19d7fbf39edb466b79e1c77c4b52ddac385eb3f6
kodakkal <kodakkal.anoop@gmail.com>
2020-10-08 12:20:52 +0200

    mmg

--- kratos/geometries/geometry.h
+++ kratos/geometries/geometry.h
@@ -1372,7 +1372,10 @@ public:
         array_1d<double, 3> normal = Normal(rPointLocalCoordinates);
         const double norm_normal = norm_2(normal);
         if (norm_normal > std::numeric_limits<double>::epsilon()) normal /= norm_normal;
-        else KRATOS_ERROR << "ERROR: The normal norm is zero or almost zero. Norm. normal: " << norm_normal << std::endl;
+        else {
+            std::cout << rPointLocalCoordinates << std::endl;
+            KRATOS_ERROR << "ERROR: The normal norm is zero or almost zero. Norm. normal: " << norm_normal << std::endl;
+        }
         return normal;
     }
 
