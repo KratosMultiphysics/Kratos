868e6cdff93fc01e74e39e4e3dd50905663282b5
Vahid Galavi <vahid.galavi@deltares.nl>
2020-10-08 11:55:22 +0200

    Modifications suggested by reviewer

--- applications/GeoMechanicsApplication/GeoMechanicsApplication.py
+++ applications/GeoMechanicsApplication/GeoMechanicsApplication.py
@@ -1,16 +1,4 @@
-# # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-# from __future__ import print_function, absolute_import, division
-
-# # Application dependent names and paths
-# import KratosMultiphysics as KM
-# from KratosGeoMechanicsApplication import *
-# application = KratosGeoMechanicsApplication()
-# application_name = "KratosGeoMechanicsApplication"
-# application_folder = "GeoMechanicsApplication"
-
-# KM._ImportApplication(application, application_name, application_folder, __path__)
-
-
+# Application dependent names and paths
 from KratosMultiphysics import _ImportApplication
 from KratosGeoMechanicsApplication import *
 application = KratosGeoMechanicsApplication()
--- applications/GeoMechanicsApplication/MainKratos_multiple_stages_template.py
+++ applications/GeoMechanicsApplication/MainKratos_multiple_stages_template.py
@@ -1,10 +1,5 @@
-from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import os
 import KratosMultiphysics
-import KratosMultiphysics.ExternalSolversApplication
-# import KratosMultiphysics.TrilinosApplication as TrilinosApplication
-import KratosMultiphysics.StructuralMechanicsApplication
-import KratosMultiphysics.GeoMechanicsApplication
 
 from KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis import GeoMechanicsAnalysis
 
