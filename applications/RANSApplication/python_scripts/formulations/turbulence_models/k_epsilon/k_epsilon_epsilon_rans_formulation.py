65f480efd382febcbb44e00ebd15310ab10af4e8
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-10-09 14:31:16 +0200

    minor

--- applications/RANSApplication/python_scripts/formulations/turbulence_models/k_epsilon/k_epsilon_epsilon_rans_formulation.py
+++ applications/RANSApplication/python_scripts/formulations/turbulence_models/k_epsilon/k_epsilon_epsilon_rans_formulation.py
@@ -5,8 +5,8 @@ import KratosMultiphysics.RANSApplication as KratosRANS
 from KratosMultiphysics.RANSApplication.formulations.turbulence_models.scalar_turbulence_model_rans_formulation import ScalarTurbulenceModelRansFormulation
 
 # import utilities
-from KratosMultiphysics import VariableUtils
 from KratosMultiphysics.RANSApplication.formulations.utilities import CalculateNormalsOnConditions
+from KratosMultiphysics.RANSApplication.formulations.utilities import InitializeYPlusVariablesInConditions
 
 class KEpsilonEpsilonRansFormulation(ScalarTurbulenceModelRansFormulation):
     def GetSolvingVariable(self):
@@ -19,11 +19,7 @@ class KEpsilonEpsilonRansFormulation(ScalarTurbulenceModelRansFormulation):
         return "RansKEpsilonEpsilon"
 
     def Initialize(self):
-        VariableUtils().SetNonHistoricalVariableToZero(
-            KratosRANS.RANS_Y_PLUS, self.GetModelPart().Conditions)
-        VariableUtils().SetNonHistoricalVariableToZero(
-            KratosRANS.FRICTION_VELOCITY, self.GetModelPart().Conditions)
-
+        InitializeYPlusVariablesInConditions(self.GetModelPart())
         CalculateNormalsOnConditions(self.GetModelPart())
 
         super().Initialize()
