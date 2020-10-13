6cf76a8266fa8cd870c4ee2d412f419ac7edbe3d
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2020-10-13 16:51:03 +0200

    import structural

--- applications/FemToDemApplication/FemToDemApplication.py
+++ applications/FemToDemApplication/FemToDemApplication.py
@@ -4,7 +4,7 @@ from __future__ import print_function, absolute_import, division
 from KratosMultiphysics.MeshingApplication import *
 from KratosMultiphysics.PfemFluidDynamicsApplication import *
 from KratosMultiphysics.DEMApplication import *
-# from KratosMultiphysics.StructuralMechanicsApplication import *
+from KratosMultiphysics.StructuralMechanicsApplication import *
 
 # Application dependent names and paths
 from KratosMultiphysics import _ImportApplication
