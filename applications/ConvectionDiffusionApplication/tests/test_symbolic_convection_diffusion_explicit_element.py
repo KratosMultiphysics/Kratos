2b4d773a5e4b3e22c25009bc7b60461ccbab9841
riccardotosi <43809717+riccardotosi@users.noreply.github.com>
2020-10-13 12:06:48 +0200

    Update applications/ConvectionDiffusionApplication/tests/test_symbolic_convection_diffusion_explicit_element.py
    Co-authored-by: Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>

--- applications/ConvectionDiffusionApplication/tests/test_symbolic_convection_diffusion_explicit_element.py
+++ applications/ConvectionDiffusionApplication/tests/test_symbolic_convection_diffusion_explicit_element.py
@@ -47,7 +47,7 @@ class TestSymbolicEulerianConvectionDiffusionElement(KratosUnittest.TestCase):
             y = node.Y
             u_analytical = x - sin(bar_simulation.time)
             u_numerical = node.GetSolutionStepValue(KratosMultiphysics.TEMPERATURE)
-            error = error + (((u_analytical - u_numerical)**2)*node.GetSolutionStepValue(KratosMultiphysics.NODAL_AREA))
+            error += (((u_analytical - u_numerical)**2)*node.GetSolutionStepValue(KratosMultiphysics.NODAL_AREA))
         error = sqrt(error)
         self.assertAlmostEqual(error,0.0017678016487242358,delta=1e-12)
 
