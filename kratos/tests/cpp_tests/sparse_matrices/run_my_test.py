756398b7ea1962dd1ebf037a4db7b3269f30e515
Riccardo Rossi <rrossi@cimne.upc.edu>
2020-10-15 16:43:48 +0200

    all tests

--- kratos/tests/cpp_tests/sparse_matrices/run_my_test.py
+++ kratos/tests/cpp_tests/sparse_matrices/run_my_test.py
@@ -4,8 +4,12 @@ Tester.RunTestCases("TestGraphConstruction")
 Tester.RunTestCases("TestGraphContiguousRowConstruction")
 #Tester.RunTestCases("TestDistributedGraphConstruction")
 Tester.RunTestCases("TestCsrMatrixAssemble")
+Tester.RunTestCases("TestSystemVectorAssembly")
 
 Tester.RunTestCases("TestCSRConstruction")
+
+Tester.RunTestCases("TestSpMV")
+
 
 #Tester.RunTestCases("TestPerformanceBenchmarkSparseGraph")
 #Tester.RunTestCases("TestPerformanceBenchmarkSparseContiguousRowGraph")
