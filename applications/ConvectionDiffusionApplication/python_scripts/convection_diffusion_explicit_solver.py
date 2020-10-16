afb2a2a350beaecc96081af1e5ce020e3bbd1af1
riccardotosi <rtosi@cimne.upc.edu>
2020-10-16 11:09:28 +0200

    Black formatting style

--- applications/ConvectionDiffusionApplication/python_scripts/convection_diffusion_explicit_solver.py
+++ applications/ConvectionDiffusionApplication/python_scripts/convection_diffusion_explicit_solver.py
@@ -5,12 +5,18 @@ import KratosMultiphysics
 import KratosMultiphysics.ConvectionDiffusionApplication as ConvectionDiffusionApplication
 
 # Import base class file
-from KratosMultiphysics.ConvectionDiffusionApplication import convection_diffusion_base_solver
+from KratosMultiphysics.ConvectionDiffusionApplication import (
+    convection_diffusion_base_solver,
+)
+
 
 def CreateSolver(model, custom_settings):
     return ConvectionDiffusionExplicitSolver(model, custom_settings)
 
-class ConvectionDiffusionExplicitSolver(convection_diffusion_base_solver.ConvectionDiffusionBaseSolver):
+
+class ConvectionDiffusionExplicitSolver(
+    convection_diffusion_base_solver.ConvectionDiffusionBaseSolver
+):
     """
     The explicit class for convection-diffusion solvers.
     See convection_diffusion_base_solver.py for more information.
@@ -21,36 +27,60 @@ class ConvectionDiffusionExplicitSolver(convection_diffusion_base_solver.Convect
         super(ConvectionDiffusionExplicitSolver, self).__init__(model, custom_settings)
 
         # Overwrite the base solver minimum buffer size
-        self.min_buffer_size = 1
+        self.min_buffer_size = 2
 
-        element_name = self.settings["element_replace_settings"]["element_name"].GetString()
-        if (self.settings["use_orthogonal_subscales"].GetBool() is True):
-            if element_name in ("QSConvectionDiffusionExplicit","DConvectionDiffusionExplicit"):
-                self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.OSS_SWITCH, 1)
+        element_name = self.settings["element_replace_settings"][
+            "element_name"
+        ].GetString()
+        if self.settings["use_orthogonal_subscales"].GetBool() is True:
+            if element_name in (
+                "QSConvectionDiffusionExplicit",
+                "DConvectionDiffusionExplicit",
+            ):
+                self.main_model_part.ProcessInfo.SetValue(
+                    KratosMultiphysics.OSS_SWITCH, 1
+                )
             else:
-                err_msg =  self.__class__.__name__
-                err_msg += "The selected element", element_name, "does not support OSS projection. Select QSConvectionDiffusionExplicit or DConvectionDiffusionExplicit instead."
+                err_msg = self.__class__.__name__
+                err_msg += (
+                    "The selected element",
+                    element_name,
+                    "does not support OSS projection. Select QSConvectionDiffusionExplicit or DConvectionDiffusionExplicit instead.",
+                )
                 raise Exception(err_msg)
         else:
-            if element_name in ("QSConvectionDiffusionExplicit","DConvectionDiffusionExplicit"):
-                self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.OSS_SWITCH, 0)
+            if element_name in (
+                "QSConvectionDiffusionExplicit",
+                "DConvectionDiffusionExplicit",
+            ):
+                self.main_model_part.ProcessInfo.SetValue(
+                    KratosMultiphysics.OSS_SWITCH, 0
+                )
             else:
-                err_msg =  self.__class__.__name__
-                err_msg += "The selected element", element_name, "does not support OSS projection. Select QSConvectionDiffusionExplicit or DConvectionDiffusionExplicit instead."
+                err_msg = self.__class__.__name__
+                err_msg += (
+                    "The selected element",
+                    element_name,
+                    "does not support OSS projection. Select QSConvectionDiffusionExplicit or DConvectionDiffusionExplicit instead.",
+                )
                 raise Exception(err_msg)
 
-        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Construction finished")
+        KratosMultiphysics.Logger.PrintInfo(
+            self.__class__.__name__, "Construction finished"
+        )
 
     @classmethod
     def GetDefaultParameters(cls):
-        default_settings = KratosMultiphysics.Parameters("""
+        default_settings = KratosMultiphysics.Parameters(
+            """
         {
             "use_orthogonal_subscales" : false,
             "explicit_parameters" : {
                 "dynamic_tau": 1.0
             }
         }
-        """)
+        """
+        )
 
         default_settings.AddMissingParameters(super().GetDefaultParameters())
         return default_settings
@@ -66,16 +96,20 @@ class ConvectionDiffusionExplicitSolver(convection_diffusion_base_solver.Convect
         return convection_diffusion_solution_strategy
 
     def _create_solution_scheme(self):
-        self.GetComputingModelPart().ProcessInfo[KratosMultiphysics.DYNAMIC_TAU] = self.settings["explicit_parameters"]["dynamic_tau"].GetDouble()
+        self.GetComputingModelPart().ProcessInfo[
+            KratosMultiphysics.DYNAMIC_TAU
+        ] = self.settings["explicit_parameters"]["dynamic_tau"].GetDouble()
 
     def _create_runge_kutta_4_strategy(self):
         computing_model_part = self.GetComputingModelPart()
-        convection_diffusion_scheme = self.get_solution_scheme() # to call _create_solution_scheme method of the solver class
+        convection_diffusion_scheme = (
+            self.get_solution_scheme()
+        )  # to call _create_solution_scheme method of the solver class
         explicit_builder_and_solver = self.get_builder_and_solver()
         rebuild_level = 0
         return ConvectionDiffusionApplication.ExplicitSolvingStrategyRungeKutta4ConvectionDiffusion(
             computing_model_part,
             explicit_builder_and_solver,
             self.settings["move_mesh_flag"].GetBool(),
-            rebuild_level
-            )
+            rebuild_level,
+        )
