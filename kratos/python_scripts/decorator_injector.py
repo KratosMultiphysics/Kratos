b2290a9f1dcc7ebdee6eda738fced0648f976f0b
roigcarlo <roigcarlo@gmail.com>
2020-10-20 09:22:42 +0000

    Remove debug

--- kratos/python_scripts/decorator_injector.py
+++ kratos/python_scripts/decorator_injector.py
@@ -7,7 +7,6 @@ def InjectIntoContainer(container, function, expression):
     for symbol_name in container.__dict__:
         symbol = getattr(container, symbol_name)
 
-        print(symbol_name, symbol, callable(symbol), inspect.isclass(symbol))
         if callable(symbol) and not inspect.isclass(symbol):
             if expression(symbol_name):
                 if hasattr(symbol, "__wrapped__"):
