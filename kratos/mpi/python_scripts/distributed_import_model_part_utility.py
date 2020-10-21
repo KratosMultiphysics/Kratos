977140f5cd305e05354837c0801ce828a0f86819
philbucher <philipp.bucher@tum.de>
2020-10-21 22:53:55 +0200

    removed python3 leftovers

--- kratos/mpi/python_scripts/distributed_import_model_part_utility.py
+++ kratos/mpi/python_scripts/distributed_import_model_part_utility.py
@@ -1,12 +1,11 @@
-from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-
 # Importing the Kratos Library
 import KratosMultiphysics
 import KratosMultiphysics.mpi as KratosMPI
 
+# Other imports
 import os
 
-class DistributedImportModelPartUtility(object):
+class DistributedImportModelPartUtility:
 
     def __init__(self, main_model_part, settings):
         self.main_model_part = main_model_part
