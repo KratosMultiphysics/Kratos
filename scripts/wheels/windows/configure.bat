7c341e25b361589f313dd7d15c9a3fb202196d15
roigcarlo <roigcarlo@gmail.com>
2020-10-22 10:17:03 +0200

    directly using cmake from choco

--- scripts/wheels/windows/configure.bat
+++ scripts/wheels/windows/configure.bat
@@ -3,7 +3,6 @@
 set CC=cl.exe
 set CXX=cl.exe
 
-
 set KRATOS_SOURCE=%2
 set KRATOS_BUILD=%KRATOS_SOURCE%/build
 set KRATOS_APP_DIR=applications
@@ -32,7 +31,7 @@ del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\cmake_install.cmake"
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeCache.txt"
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeFiles"
 
-%cmake% -G"Visual Studio 16 2019" -H"%KRATOS_SOURCE%" -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"        ^
+cmake -G"Visual Studio 16 2019" -H"%KRATOS_SOURCE%" -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"        ^
 -DINSTALL_EMBEDDED_PYTHON=OFF                                                                       ^
 -DLAPACK_LIBRARIES=%LAPACK%                                                                         ^
 -DBLAS_LIBRARIES=%BLAS%                                                                             ^
