c455566a8cdddccb9294036cbf23a67ea848aa1c
Miguel <mmaso@cimne.upc.edu>
2020-10-26 23:35:05 +0100

    new test

--- applications/ShallowWaterApplication/tests/shallow_water_test_factory.py
+++ applications/ShallowWaterApplication/tests/shallow_water_test_factory.py
@@ -4,13 +4,10 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 from KratosMultiphysics.ShallowWaterApplication.shallow_water_analysis import ShallowWaterAnalysis
 
 class ShallowWaterTestFactory(KratosUnittest.TestCase):
-
     def test_execution(self):
         with KratosUnittest.WorkFolderScope(self.execution_directory, __file__):
-
             with open(self.execution_file + "_parameters.json",'r') as parameter_file:
                 ProjectParameters = KratosMultiphysics.Parameters(parameter_file.read())
-
             model = KratosMultiphysics.Model()
             test = ShallowWaterAnalysis(model, ProjectParameters)
             test.Run()
@@ -22,6 +19,10 @@ class TestLagrangianShallowWaterElement(ShallowWaterTestFactory):
 class TestShallowWaterElement(ShallowWaterTestFactory):
     execution_directory = "element_tests"
     execution_file = "swe"
+
+class TestShallowWater2D3NElement(ShallowWaterTestFactory):
+    execution_directory = "element_tests"
+    execution_file = "shallow_water_2d_3n"
 
 class TestSetTopographyProcess(ShallowWaterTestFactory):
     execution_directory = "processes_tests"
