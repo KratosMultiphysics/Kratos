f84275f2c16b8844a9a222b0db97d73fcaa0f439
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-10-26 20:54:30 +0100

    Making const element

--- kratos/utilities/sensitivity_builder.cpp
+++ kratos/utilities/sensitivity_builder.cpp
@@ -119,7 +119,8 @@ struct LocalSensitivityBuilder
     {
         KRATOS_TRY;
         rElement.CalculateSensitivityMatrix(rVariable, SensitivityMatrix, rProcessInfo);
-        dynamic_cast<const TElement&>(rElement).GetValuesVector(AdjointVector);
+        const auto& const_element = rElement;
+        const_element.GetValuesVector(AdjointVector);
         KRATOS_ERROR_IF(AdjointVector.size() != SensitivityMatrix.size2())
             << "AdjointVector.size(): " << AdjointVector.size()
             << " incompatible with SensitivityMatrix.size1(): "
