2469a8238f883d71b03ef27eda4e3435118037ef
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2020-10-28 22:55:51 +0100

    Update kratos/utilities/normal_calculation_utils.cpp

--- kratos/utilities/normal_calculation_utils.cpp
+++ kratos/utilities/normal_calculation_utils.cpp
@@ -122,7 +122,7 @@ KRATOS_API(KRATOS_CORE) void NormalCalculationUtils::CalculateNormals<Condition>
     use_simplex = rModelPart.GetCommunicator().GetDataCommunicator().MaxAll(use_simplex);
     if (use_simplex) {
         CalculateOnSimplex(rModelPart, dimension);
-    else
+    } else {
     {
         CalculateNormalsUsingGenericAlgorithm<ModelPart::ConditionsContainerType>(rModelPart, ConsiderUnitNormal);
     }
