55a01873912e5dbac59f58916d6a0aec0d1e1d82
Manuel <manuel.messmer@tum.de>
2020-10-29 16:35:39 +0100

    Add pseudo test.

---
+++ kratos/tests/cpp_tests/geometries/test_nurbs_volume_geometry.cpp
@@ -0,0 +1,33 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Manuel Messmer
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "testing/testing.h"
+#include "geometries/nurbs_volume_geometry.h"
+#include "geometries/nurbs_shape_function_utilities/nurbs_volume_shape_function.h"
+
+
+
+
+namespace Kratos {
+namespace Testing {
+
+    //typedef Node<3> NodeType;
+
+    KRATOS_TEST_CASE_IN_SUITE(NurbsVolumeGeometry, KratosCoreNurbsGeometriesFastSuite) {}
+
+} // End namespace Testsing
+} // End namespace Kratos
