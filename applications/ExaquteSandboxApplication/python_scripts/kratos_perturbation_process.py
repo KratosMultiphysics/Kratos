d880b88e0bf27067e8c3924ede10b213082aa426
riccardotosi <rtosi@cimne.upc.edu>
2020-10-29 11:54:16 +0100

    Aplpy Codacy

--- applications/ExaquteSandboxApplication/python_scripts/kratos_perturbation_process.py
+++ applications/ExaquteSandboxApplication/python_scripts/kratos_perturbation_process.py
@@ -8,9 +8,7 @@ __all__ = ['Factory', 'ImposePerturbedInitialConditionProcess']
 from collections import namedtuple
 from collections import Mapping
 
-from math import floor
-from math import ceil
-from math import log
+from math import floor, ceil
 
 import numpy as np
 
@@ -258,24 +256,26 @@ class ImposePerturbedInitialConditionAnalysisStage(ConvectionDiffusionAnalysis):
             node.SetSolutionStepValue(KratosMultiphysics.VELOCITY,velocity_field_loaded_and_correlated_noise + penalty_coeff*gradient_solution)
 
 
-"""
-@brief Process generating divergence-free correlated noise, imposing no-penetrability condition on bluff bodies,
-preserving boundary conditions on boundaries and (if required) adding the contribution of a manually-loaded velocity field.
-The generated initial field reads
-    u_0 = u_T + c*P(u_{cn}) + c*\nabla potential ,
-where u_T is the velocity field we load, c a penalty coefficient, and P(u_{cn}) and \nabla potential are discussed next.
-@details The process first projects the correlated noise u_{cn} into the model part. The projected correlated noise is denoted as P(u_{cn}).
-Then, the associated poisson problem for the unknown TEMPERATURE is solved:
-    - \nabla \cdot \nabla TEMPERATURE = \nabla \cdot (c*P(u_{cn}) + u_{T})  on \Omega
-    c*P(u_{cn}) \cdot NORMAL = - \nabla TEMPERATURE \cdot NORMAL  on \partial \Omega_{bluff body}
-    TEMPERATURE = 0  on \Omega_{outlet}
-Then, the tangential velocity is set to zero on the boundaries \partial \Omega_{bluff body} and \partial \Omega_{external domain} by doing:
-    P(u_{cn}) + \nabla TEMPERATURE = 0 on \Gamma
-The correlated noise satisfying the no-penetrability and solenoidal conditions and respecting the boundary conditions is: VELOCITY = u_T + c*P(u_{cn}) + c*\nabla TEMPERATURE.
-@param model    : Kratos Model
-       settings : Kratos Parameters
-"""
 class ImposePerturbedInitialConditionProcess(KratosMultiphysics.Process):
+    """
+    Process generating divergence-free correlated noise, imposing no-penetrability condition on bluff bodies,
+    preserving boundary conditions on boundaries and (if required) adding the contribution of a manually-loaded velocity field.
+    The generated initial field reads
+        u_0 = u_T + c*P(u_{cn}) + c*\nabla potential ,
+    where u_T is the velocity field we load, c a penalty coefficient, and P(u_{cn}) and \nabla potential are discussed next.
+    @details The process first projects the correlated noise u_{cn} into the model part. The projected correlated noise is denoted as P(u_{cn}).
+    Then, the associated poisson problem for the unknown TEMPERATURE is solved:
+        - \nabla \cdot \nabla TEMPERATURE = \nabla \cdot (c*P(u_{cn}) + u_{T})  on \Omega
+        c*P(u_{cn}) \cdot NORMAL = - \nabla TEMPERATURE \cdot NORMAL  on \partial \Omega_{bluff body}
+        TEMPERATURE = 0  on \Omega_{outlet}
+    Then, the tangential velocity is set to zero on the boundaries \partial \Omega_{bluff body} and \partial \Omega_{external domain} by doing:
+        P(u_{cn}) + \nabla TEMPERATURE = 0 on \Gamma
+    The correlated noise satisfying the no-penetrability and solenoidal conditions and respecting the boundary conditions is: VELOCITY = u_T + c*P(u_{cn}) + c*\nabla TEMPERATURE.
+
+    Parameters:
+    model    : Kratos Model
+    settings : Kratos Parameters
+    """
 
     @property
     def model_part_nodes(self):
