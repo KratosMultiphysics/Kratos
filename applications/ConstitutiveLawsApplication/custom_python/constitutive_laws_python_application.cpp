2fc53359692cc4ce749569f7b1d7447589ef6cda
philbucher <philipp.bucher@tum.de>
2020-10-29 11:01:58 +0100

    necessary changes

--- applications/ConstitutiveLawsApplication/custom_python/constitutive_laws_python_application.cpp
+++ applications/ConstitutiveLawsApplication/custom_python/constitutive_laws_python_application.cpp
@@ -4,12 +4,11 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Alejandro Cornejo Velazquez
 //                   Riccardo Rossi
-//
 //
 
 
