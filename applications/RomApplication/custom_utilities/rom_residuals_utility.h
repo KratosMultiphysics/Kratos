95a382d20093624a158a8b493d3b31cd92d3a2bb
philbucher <philipp.bucher@tum.de>
2020-11-08 21:18:09 +0100

    removals in ROMApp

--- applications/RomApplication/custom_utilities/rom_residuals_utility.h
+++ applications/RomApplication/custom_utilities/rom_residuals_utility.h
@@ -132,9 +132,6 @@ namespace Kratos
                             PhiElemental.resize(dofs.size(), mRomDofs,false);
                         GetPhiElemental(PhiElemental, dofs, geom);
                         noalias(row(MatrixResiduals, k)) = prod(trans(PhiElemental), RHS_Contribution); // The size of the residual will vary only when using more ROM modes, one row per condition
-
-                        // clean local elemental me overridemory
-                        mpScheme->CleanMemory(*it_el);
                     }
 
                 }
@@ -158,9 +155,6 @@ namespace Kratos
                             PhiElemental.resize(dofs.size(), mRomDofs,false);
                         GetPhiElemental(PhiElemental, dofs, geom);
                         noalias(row(MatrixResiduals, k+nelements)) = prod(trans(PhiElemental), RHS_Contribution); // The size of the residual will vary only when using more ROM modes, one row per condition
-
-                        // clean local elemental memory
-                        mpScheme->CleanMemory(*it);
                     }
                 }
             }
