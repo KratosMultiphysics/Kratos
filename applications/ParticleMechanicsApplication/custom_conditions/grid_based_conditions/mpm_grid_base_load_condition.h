6af97a5734d2c2ef8e95d8e1532d24336a7e6411
philbucher <philipp.bucher@tum.de>
2020-11-08 21:54:30 +0100

    missing const

--- applications/ParticleMechanicsApplication/custom_conditions/grid_based_conditions/mpm_grid_base_load_condition.h
+++ applications/ParticleMechanicsApplication/custom_conditions/grid_based_conditions/mpm_grid_base_load_condition.h
@@ -192,7 +192,7 @@ public:
      */
     void AddExplicitContribution(const VectorType& rRHS,
         const Variable<VectorType>& rRHSVariable,
-        Variable<array_1d<double,3> >& rDestinationVariable,
+        const Variable<array_1d<double,3> >& rDestinationVariable,
         const ProcessInfo& rCurrentProcessInfo) override;
 
     /**
