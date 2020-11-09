600eafb953e31d9513f9778443b32ecf0a3576b5
qaumann <qaumann@users.noreply.github.com>
2020-11-09 18:18:23 +0100

    small fix

--- applications/MORApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/MORApplication/custom_python/add_custom_processes_to_python.cpp
@@ -8,21 +8,18 @@
 //					 Kratos default license: kratos/license.txt
 
 // System includes
+#include "includes/define_python.h"
 #include "custom_python/add_custom_processes_to_python.h"
 
 // External includes
 
 // Project includes
-// #include "includes/define.h"
-// #include "includes/define_python.h"
-#include "spaces/ublas_space.h"
-#include "linear_solvers/linear_solver.h"
-// #include "strategies/residualbased_linear_strategy.h"
-
 #include "processes/process.h"
 #include "custom_processes/monolithic_mapping_process.hpp"
 #include "custom_processes/pml_direction_process.hpp"
 
+#include "spaces/ublas_space.h"
+#include "linear_solvers/linear_solver.h"
 namespace Kratos
 {
 
@@ -37,23 +34,15 @@ void  AddCustomProcessesToPython(pybind11::module& m)
     typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
     typedef UblasSpace<double, Matrix, Vector> DenseSpaceType;
     typedef LinearSolver<SparseSpaceType, DenseSpaceType > LinearSolverType;
-    // typedef UblasSpace<double, CompressedMatrix, boost::numeric::ublas::vector<double>> SparseSpaceType;
-    // typedef UblasSpace<double, Matrix, Vector> DenseSpaceType;
-    // typedef TUblasSparseSpace<double> SparseSpaceType;
-    // typedef TUblasDenseSpace<double> DenseSpaceType;
+
     typedef SparseSpaceType::MatrixType SparseMatrixType;
-
-    // typedef LinearSolver<SparseSpaceType, DenseSpaceType> LinearSolverType;
-
-    // typedef SolvingStrategy<SparseSpaceType, DenseSpaceType, LinearSolverType> SolvingStrategyType;
 
     py::class_<MonolithicMappingProcess<SparseMatrixType>, typename MonolithicMappingProcess<SparseMatrixType>::Pointer, Process>(m, "MonolithicMappingProcess")
         .def(py::init<ModelPart&, ModelPart&, SparseMatrixType&>())
         ;
 
-    py::class_<PMLDirectionProcess<SparseSpaceType, DenseSpaceType, LinearSolverType>, typename PMLDirectionProcess<SparseMatrixType, DenseSpaceType, LinearSolverType>::Pointer, Process>(m, "PMLDirectionProcess")
+    py::class_<PMLDirectionProcess<SparseSpaceType, DenseSpaceType, LinearSolverType>, typename PMLDirectionProcess<SparseSpaceType, DenseSpaceType, LinearSolverType>::Pointer, Process>(m, "PMLDirectionProcess")
         .def(py::init<ModelPart&, ModelPart&, ModelPart&, LinearSolverType::Pointer>())
-        // .def(PMLDirectionProcess::Execute)
         ;
 
 }
