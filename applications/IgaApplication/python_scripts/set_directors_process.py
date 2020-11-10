972b983e06b9087189225fa0be6162c26d2532c9
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2020-11-10 15:08:08 +0100

    fixed python director update process

--- applications/IgaApplication/python_scripts/set_directors_process.py
+++ applications/IgaApplication/python_scripts/set_directors_process.py
@@ -39,38 +39,53 @@ class SetDirectorsProcess(KratosMultiphysics.Process):
         self.model_part = Model[settings["model_part_name"].GetString()]
 
     def ExecuteInitialize(self):
-     director = KratosMultiphysics.Vector(3) 
-     director[0] = 0
-     director[1] = 0
-     director[2] = 1
+        director = KratosMultiphysics.Vector(3) 
+        director[0] = 0
+        director[1] = 0
+        director[2] = 1
 
-     dirTang = KratosMultiphysics.Matrix(3,2) 
-     dirTang[0,0] = 1
-     dirTang[1,0] = 0
-     dirTang[2,0] = 0
+        dirTang = KratosMultiphysics.Matrix(3,2) 
+        dirTang[0,0] = 1
+        dirTang[1,0] = 0
+        dirTang[2,0] = 0
 
-     dirTang[0,1] = 1
-     dirTang[1,1] = 0
-     dirTang[2,1] = 0
-     for node in self.model_part.Nodes:
-         node.SetValue(IGA.DIRECTOR, director)
-         node.SetValue(IGA.DIRECTORTANGENTSPACE, dirTang)
-
-#       for node in self.model_part.Nodes:
-#          
-#          node.SetValue(DIRECTOR, director);
-#
-#
-#        for element in self.model_part.Elements:
-#            A3 = element.GetGeometry().Normal(point_number); //this makes only sense if the geometry is undeformed
-#            A3 = A3 / sqrt((A3 * A3));
-#            //Vector m_Nvec = trans(row(m_N, point_number));
-#            rRightHandSideMatrix = rRightHandSideMatrix + outer_prod(trans(row(m_N, point_number)),  trans(A3)     ) ;
-
-#            rLeftHandSideMatrix = rLeftHandSideMatrix + outer_prod(row(m_N,point_number), row(m_N, point_number));
+        dirTang[0,1] = 1
+        dirTang[1,1] = 0
+        dirTang[2,1] = 0
+        for node in self.model_part.Nodes:
+            node.SetValue(IGA.DIRECTOR, director)
+            node.SetValue(IGA.DIRECTORTANGENTSPACE, dirTang)
 
 
-    def ExecuteInitializeSolutionStep(self):
+    def TangentSpaceFromStereographicProjection(director):
+        BLA = KratosMultiphysics.Matrix(3,2) 
+        y = KratosMultiphysics.Vector(2) 
+        st =  np.sign(tloc[2])
+        s  = 1/(1+st*(tloc[2]))
+        y[0] = tloc[0] * s;
+        y[1] = tloc[1] * s;
+        ys1 = y[0]*y[0];
+        ys2 = y[1]*y[1];
+        s2 = 2*(1+ys1+ys2);
+
+        BLA[0,0] = s2-4*ys1
+        BLA[1,0] = -4*y(0)*y(1)
+        BLA[2,0] = -st*4*y(0)
+
+        BLA[0,1] = -4*y(0)*y(1)
+        BLA[1,1] = s2-4*ys2
+        BLA[2,1] = -st*4*y(1)
+
+        normcol0 = 1/( math.sqrt(BLA[0,0] *BLA[0,0] + BLA[1,0] *  BLA[1,0] + BLA[2,0] * BLA[2,0]))
+        normcol1 = 1/( math.sqrt(BLA[0,1] *BLA[0,1] + BLA[1,1] *  BLA[1,1] + BLA[2,1] * BLA[2,1]))
+
+        for i in range(3):
+            BLA[i,0] =  BLA[i,0] /normcol0
+            BLA[i,1] =  BLA[i,1] /normcol1
+
+        return dirTang
+
+    def ExecuteFinalizeSolutionStep(self):
         for node in self.model_part.Nodes:
             director = node.GetValue(IGA.DIRECTOR)
             inc2d = node.GetSolutionStepValue(IGA.DIRECTORINC)
@@ -84,4 +99,4 @@ class SetDirectorsProcess(KratosMultiphysics.Process):
             director = director / math.sqrt(director[0] * director[0] + director[1] * director[1] + director[2] * director[2])
 
             node.SetValue(IGA.DIRECTOR, director)
-            # node.SetValue(DIRECTORTANGENTSPACE, director)
+            node.SetValue(IGA.DIRECTORTANGENTSPACE, TangentSpaceFromStereographicProjection(director))
