cae4bc394873be4eefbfc2d993171b5387b421db
mohitt <mohit.tyagi@altair.com>
2020-11-11 17:24:20 +0530

    variable name correction

--- applications/FluidDynamicsApplication/python_scripts/apply_slip_process.py
+++ applications/FluidDynamicsApplication/python_scripts/apply_slip_process.py
@@ -51,11 +51,11 @@ class ApplySlipProcess(KratosMultiphysics.Process):
     def ExecuteInitialize(self):
         # Compute the normal on the nodes of interest -
         # Note that the model part employed here is supposed to only have slip "conditions"
-        consider_unit_normal = True
-        KratosMultiphysics.NormalCalculationUtils().CalculateNormals(self.model_part, consider_unit_normal)
+        enforce_generic_algorithm = True
+        KratosMultiphysics.NormalCalculationUtils().CalculateNormals(self.model_part, enforce_generic_algorithm)
 
     def ExecuteInitializeSolutionStep(self):
         # Recompute the normals if needed
         if self.avoid_recomputing_normals == False:
-            consider_unit_normal = True
-            KratosMultiphysics.NormalCalculationUtils().CalculateNormals(self.model_part, consider_unit_normal)
+            enforce_generic_algorithm = True
+            KratosMultiphysics.NormalCalculationUtils().CalculateNormals(self.model_part, enforce_generic_algorithm)
