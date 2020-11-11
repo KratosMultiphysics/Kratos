0228abe394a7d22a9cb8261dc68d1f0e74a8c93c
Miguel <mmaso@cimne.upc.edu>
2020-11-11 19:28:18 +0100

    initial commit

---
+++ applications/ShallowWaterApplication/custom_elements/monotonic_element_2d_3n.cpp
@@ -0,0 +1,52 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "monotonic_element_2d_3n.h"
+
+namespace Kratos
+{
+
+void MonotonicElement2D3N::CalculateMassMatrix(
+    MatrixType& rMassMatrix,
+    const ProcessInfo& rCurrentProcessInfo)
+{
+
+}
+
+void MonotonicElement2D3N::AddGradientTerms(
+    MatrixType& rLHS,
+    VectorType& rRHS,
+    const ElementData& rData,
+    const array_1d<double,3>& rN,
+    const BoundedMatrix<double,3,2>& rDN_DX)
+{
+
+}
+
+void MonotonicElement2D3N::AddSourceTerms(
+    MatrixType& rLHS,
+    VectorType& rRHS,
+    const ElementData& rData,
+    const array_1d<double,3>& rN,
+    const BoundedMatrix<double,3,2>& rDN_DX)
+{
+
+}
+
+} // namespace kratos
