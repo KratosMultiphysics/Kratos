6b8652a2db66dd8438a5ecde161aa5c00094d08b
marcnunezc <marcnc94@gmail.com>
2020-11-13 16:39:05 +0100

    Correct license

--- applications/MeshingApplication/custom_processes/pmmg_process.h
+++ applications/MeshingApplication/custom_processes/pmmg_process.h
@@ -36,12 +36,12 @@
 /* =============================================================================
 **  Copyright (c) Bx INP/Inria/UBordeaux, 2017- .
 **
-**  mmg is free software: you can redistribute it and/or modify it
+**  parmmg is free software: you can redistribute it and/or modify it
 **  under the terms of the GNU Lesser General Public License as published
 **  by the Free Software Foundation, either version 3 of the License, or
 **  (at your option) any later version.
 **
-**  mmg is distributed in the hope that it will be useful, but WITHOUT
+**  parmmg is distributed in the hope that it will be useful, but WITHOUT
 **  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 **  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
 **  License for more details.
