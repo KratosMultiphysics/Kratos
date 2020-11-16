9b17383bd85742602f49c72261bb89f35add6abd
rickyaristio <ricky.aristio@gmail.com>
2020-11-16 08:16:46 +0100

    Penalty condition: calculate the initial determinant of jacobian

--- applications/IgaApplication/custom_conditions/penalty_coupling_condition.cpp
+++ applications/IgaApplication/custom_conditions/penalty_coupling_condition.cpp
@@ -57,6 +57,11 @@ namespace Kratos
 
         // Integration
         const GeometryType::IntegrationPointsArrayType& integration_points = r_geometry_master.IntegrationPoints();
+
+        // initial determinant of jacobian 
+        Vector determinat_jacobian_vector_initial(integration_points.size());
+        DeterminantOfJacobianInitial(r_geometry_master, determinat_jacobian_vector_initial);
+
         for (IndexType point_number = 0; point_number < integration_points.size(); point_number++)
         {
             Matrix N_master = r_geometry_master.ShapeFunctionsValues();
@@ -88,7 +93,7 @@ namespace Kratos
 
             // Differential area
             const double integration_weight = integration_points[point_number].Weight();
-            const double determinat_jacobian = r_geometry_master.DeterminantOfJacobian(point_number);
+            const double determinat_jacobian = determinat_jacobian_vector_initial[point_number];
 
             // Assembly
             if (CalculateStiffnessMatrixFlag) {
@@ -121,6 +126,45 @@ namespace Kratos
         }
 
         KRATOS_CATCH("")
+    }
+
+    void PenaltyCouplingCondition::DeterminantOfJacobianInitial(
+        const GeometryType& rGeometry,
+        Vector& rDeterminantOfJacobian)
+    {
+        const IndexType nb_integration_points = rGeometry.IntegrationPointsNumber();
+        if (rDeterminantOfJacobian.size() != nb_integration_points) {
+            rDeterminantOfJacobian.resize(nb_integration_points, false);
+        }
+
+        const SizeType working_space_dimension = rGeometry.WorkingSpaceDimension();
+        const SizeType local_space_dimension = rGeometry.LocalSpaceDimension();
+        const SizeType nb_nodes = rGeometry.PointsNumber();
+
+        Matrix J = ZeroMatrix(working_space_dimension, local_space_dimension);
+        for (IndexType pnt = 0; pnt < nb_integration_points; pnt++)
+        {
+            const Matrix& r_DN_De = rGeometry.ShapeFunctionsLocalGradients()[pnt];
+            J.clear();
+            for (IndexType i = 0; i < nb_nodes; ++i) {
+                const array_1d<double, 3>& r_coordinates = rGeometry[i].GetInitialPosition();
+                for (IndexType k = 0; k < working_space_dimension; ++k) {
+                    const double value = r_coordinates[k];
+                    for (IndexType m = 0; m < local_space_dimension; ++m) {
+                        J(k, m) += value * r_DN_De(i, m);
+                    }
+                }
+            }
+
+            //Compute the tangent and  the normal to the boundary vector
+            array_1d<double, 3> local_tangent;
+            GetGeometry().GetGeometryPart(0).Calculate(LOCAL_TANGENT, local_tangent);
+
+            array_1d<double, 3> a_1 = column(J, 0);
+            array_1d<double, 3> a_2 = column(J, 1);
+
+            rDeterminantOfJacobian[pnt] = norm_2(a_1 * local_tangent[0] + a_2 * local_tangent[1]);
+        }
     }
 
     void PenaltyCouplingCondition::CalculateDampingMatrix(
