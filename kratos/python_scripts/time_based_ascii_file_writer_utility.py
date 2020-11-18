547f4020da47a661363c445fd154dbc09c0ef877
philbucher <philipp.bucher@tum.de>
2020-11-18 11:22:06 +0100

    python 2 leftovers

--- kratos/python_scripts/time_based_ascii_file_writer_utility.py
+++ kratos/python_scripts/time_based_ascii_file_writer_utility.py
@@ -1,5 +1,3 @@
-from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-
 # Importing the Kratos Library
 import KratosMultiphysics
 
@@ -7,7 +5,7 @@ import KratosMultiphysics
 import os
 from pathlib import Path
 
-class TimeBasedAsciiFileWriterUtility(object):
+class TimeBasedAsciiFileWriterUtility:
     """This utility handles a file to which results are to be written.
     It internally handles restarts and the closing/opening of files.
 
