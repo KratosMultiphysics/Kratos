3b845a7df472928c1b7cf60c8c162c78ebdde64a
marcnunezc <marcnc94@gmail.com>
2020-11-20 14:46:38 +0100

    Flushing color file

--- applications/MeshingApplication/custom_utilities/mmg/mmg_utilities.cpp
+++ applications/MeshingApplication/custom_utilities/mmg/mmg_utilities.cpp
@@ -2451,7 +2451,7 @@ void MmgUtilities<TMMGLibrary>::OutputReferenceEntitities(
     std::filebuf elem_buffer;
     elem_buffer.open(rOutputName + ".elem.ref.json",std::ios::out);
     std::ostream elem_os(&elem_buffer);
-    elem_os << r_elem_json_text;
+    elem_os << r_elem_json_text << std::flush;
     elem_buffer.close();
 
     /* CONDITIONS */
@@ -2469,7 +2469,7 @@ void MmgUtilities<TMMGLibrary>::OutputReferenceEntitities(
     std::filebuf cond_buffer;
     cond_buffer.open(rOutputName + ".cond.ref.json",std::ios::out);
     std::ostream cond_os(&cond_buffer);
-    cond_os << r_cond_json_text;
+    cond_os << r_cond_json_text << std::flush;
     cond_buffer.close();
 
     KRATOS_CATCH("");
