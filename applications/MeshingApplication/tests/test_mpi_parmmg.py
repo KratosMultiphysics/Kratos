9c1f4c5d8ce56bce6a8627db3d82570bc423c294
marcnunezc <marcnc94@gmail.com>
2020-11-23 19:25:31 +0100

    Remove unused import

--- applications/MeshingApplication/tests/test_mpi_parmmg.py
+++ applications/MeshingApplication/tests/test_mpi_parmmg.py
@@ -7,7 +7,6 @@ import KratosMultiphysics.MeshingApplication
 import KratosMultiphysics.kratos_utilities as kratos_utilities
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 from KratosMultiphysics.testing.utilities import ReadModelPart
-from KratosMultiphysics.testing.utilities import ReadSerialModelPart
 
 def GetFilePath(fileName):
     return os.path.dirname(os.path.realpath(__file__)) + "/" + fileName
