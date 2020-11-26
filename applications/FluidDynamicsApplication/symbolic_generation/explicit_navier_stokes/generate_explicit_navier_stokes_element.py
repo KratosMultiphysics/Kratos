e9d46f6e92d81487bdbe7b5280892ca7a7003ea4
riccardotosi <rtosi@cimne.upc.edu>
2020-11-26 11:19:58 +0100

    First commit to prepare Python symbolic generator towards fractional step

--- applications/FluidDynamicsApplication/symbolic_generation/explicit_navier_stokes/generate_explicit_navier_stokes_element.py
+++ applications/FluidDynamicsApplication/symbolic_generation/explicit_navier_stokes/generate_explicit_navier_stokes_element.py
@@ -108,6 +108,8 @@ for dim in dim_vector:
     # Unknown fields definition
     v = DefineMatrix('v',n_nodes,dim)            # Current step velocity (v(i,j) refers to velocity of node i component j)
     vn = DefineMatrix('vn',n_nodes,dim)          # Previous step velocity
+    fracv = DefineMatrix('fracv',n_nodes,dim)    # Current step fractional velocity (fracv(i,j) refers to fractional velocity of node i component j)
+    fracvn = DefineMatrix('fracvn',n_nodes,dim)  # Previous step fractional velocity
     p = DefineVector('p',n_nodes)                # Pressure
     pn = DefineVector('pn',n_nodes)              # Previous step pressure
 
@@ -121,12 +123,15 @@ for dim in dim_vector:
     rho = Symbol('rho', positive = True)         # Density
     nu  = Symbol('nu', positive = True)          # Kinematic viscosity (mu/rho)
     mu  = Symbol('mu', positive = True)          # Dynamic viscosity
+    gamma = Symbol('gamma')                      # Splitting parameter
     h = Symbol('h', positive = True)
     dyn_tau = Symbol('dyn_tau', positive = True)
     stab_c1 = Symbol('stab_c1', positive = True)
     stab_c2 = Symbol('stab_c2', positive = True)
 
-    res_tot = Matrix(zeros(1,1))
+    res_momentum_tot = Matrix(zeros(1,1))
+    res_mass_tot = Matrix(zeros(1,1))
+    res_endofstep_tot = Matrix(zeros(1,1))
 
     for i_gauss in range(n_gauss):
         print("\tGauss point: " + str(i_gauss))
@@ -139,7 +144,9 @@ for dim in dim_vector:
         # Data interpolation to the Gauss points
         f_gauss = f.transpose()*N
         v_gauss = v.transpose()*N
+        fracv_gauss = fracv.transpose()*N
         p_gauss = p.transpose()*N
+        pn_gauss = pn.transpose()*N
         w_gauss = w.transpose()*N
         q_gauss = q.transpose()*N
 
@@ -147,29 +154,33 @@ for dim in dim_vector:
         grad_w = DfjDxi(DN,w)
         grad_q = DfjDxi(DN,q)
         grad_p = DfjDxi(DN,p)
+        grad_p_minus_gammapn = DfjDxi(DN,p-gamma*pn)
         grad_v = DfjDxi(DN,v)
+        grad_fracv = DfjDxi(DN,fracv)
         div_v = ones(1,1)*sum([DN[i]*v[i] for i in range (0,len(DN))])
+        div_fracv = ones(1,1)*sum([DN[i]*fracv[i] for i in range (0,len(DN))])
         div_w = ones(1,1)*sum([DN[i]*w[i] for i in range (0,len(DN))])
         # TODO: check following is correct
         grad_sym_v = grad_sym_voigtform(DN,v) # Symmetric gradient of v in Voigt notation
+        grad_sym_fracv = grad_sym_voigtform(DN,fracv) # Symmetric gradient of fractional v in Voigt notation
         grad_sym_w = grad_sym_voigtform(DN,w) # Symmetric gradient of w in Voigt notation
         # Recall that the grad(w):grad(v) contraction equals grad_sym(w)*grad_sym(v) in Voigt notation since they are symmetric tensors.
 
         # Convective velocity definition
         if convective_term:
             if (linearisation == "Picard"):
-                vconv = DefineMatrix('vconv',n_nodes,dim) # Convective velocity defined a symbol
+                fracvconv = DefineMatrix('fracvconv',n_nodes,dim) # Convective fractional velocity defined a symbol
             elif (linearisation == "FullNR"):
                 vmesh = DefineMatrix('vmesh',n_nodes,dim) # Mesh velocity
-                vconv = v - vmesh                         # Convective velocity defined as a velocity dependent variable
+                fracvconv = v - vmesh                         # Convective velocity defined as a velocity dependent variable
             else:
                 raise Exception("Wrong linearisation \'" + linearisation + "\' selected. Available options are \'Picard\' and \'FullNR\'.")
-            vconv_gauss = vconv.transpose()*N
+            fracvconv_gauss = fracvconv.transpose()*N
         if convective_term:
-            div_vconv = div(DN,vconv)
-            div_vconv = ones(1,1)*sum([DN[i]*vconv[i] for i in range (0,len(DN))])
+            div_fracvconv = div(DN,fracvconv)
+            div_fracvconv = ones(1,1)*sum([DN[i]*fracvconv[i] for i in range (0,len(DN))])
         if convective_term:
-            convective_term_gauss = (vconv_gauss.transpose()*grad_v)
+            convective_term_gauss = (fracvconv_gauss.transpose()*grad_fracv)
 
         # Compute the stabilization parameters
         if convective_term:
@@ -183,48 +194,82 @@ for dim in dim_vector:
             tau1 = 1.0/((rho*dyn_tau)/dt + (stab_c1*mu)/(h*h)) # Stabilization parameter 1
             tau2 = (h*h) / (stab_c1 * tau1)                    # Stabilization parameter 2
 
-        # Compute functionals
+        # Update functionals
 
-        # Compute galerkin functional
-        # Navier-Stokes functional
-        res_galerkin = w_gauss.transpose()*f_gauss - nu*grad_sym_w.transpose()*grad_sym_v + div_w*p_gauss - q_gauss*div_v
+        # Update momentum functional
+        res_momentum_galerkin = w_gauss.transpose()*f_gauss - nu*grad_sym_w.transpose()*grad_sym_fracv + gamma*div_w*pn_gauss
         if convective_term:
-            res_galerkin -= rho*w_gauss.transpose()*convective_term_gauss.transpose()
+            res_momentum_galerkin -= rho*w_gauss.transpose()*convective_term_gauss.transpose()
+
+        # Update mass functional
+        res_mass_galerkin = q_gauss*div_fracv + dt/rho*(grad_p_minus_gammapn.transpose()*grad_q)
+
+        # Update end-of-step functional
+        res_endofstep_galerkin = -rho/dt*(v_gauss-fracv_gauss).transpose()*w_gauss + (p-gamma*pn)*div_w
 
         # Add the stabilization terms to the original residual terms
-        if (stabilization):
-            res = res_galerkin + res_stabilization
+        if stabilization:
+            res_momentum = res_momentum_galerkin + res_stabilization
+            res_mass = res_mass_galerkin + res_stabilization
+            res_endofstep = res_endofstep_galerkin + res_stabilization
         else:
-            res = res_galerkin
+            res_momentum = res_momentum_galerkin
+            res_mass = res_mass_galerkin
+            res_endofstep = res_endofstep_galerkin
 
         # Accumulate in the total residual
-        res_tot += res
+        res_momentum_tot += res_momentum
+        res_mass_tot += res_mass
+        res_endofstep_tot += res_endofstep
 
-    # Define DOFs and test function vectors
-    dofs = Matrix( zeros(n_nodes*(dim+1), 1) )
-    testfunc = Matrix( zeros(n_nodes*(dim+1), 1) )
+    # Define DOFs and test function vectors for each equation
+    dofs_momentum = Matrix( zeros(n_nodes*(dim), 1) )
+    testfunc_momentum = Matrix( zeros(n_nodes*(dim), 1) )
+    dofs_mass = Matrix( zeros(n_nodes*(1), 1) )
+    testfunc_mass = Matrix( zeros(n_nodes*(1), 1) )
+    dofs_endofstep = Matrix( zeros(n_nodes*(dim), 1) )
+    testfunc_endofstep = Matrix( zeros(n_nodes*(dim), 1) )
 
     for i in range(0,n_nodes):
 
-        # Velocity DOFs and test functions
+        # Velocity and fractional velocity DOFs and test functions
         for k in range(0,dim):
-            dofs[i*(dim+1)+k] = v[i,k]
-            testfunc[i*(dim+1)+k] = w[i,k]
+            dofs_momentum[i*(dim)+k] = fracv[i,k]
+            testfunc_momentum[i*(dim)+k] = w[i,k]
+            dofs_endofstep[i*(dim)+k] = v[i,k]
+            testfunc_endofstep[i*(dim)+k] = w[i,k]
 
         # Pressure DOFs and test functions
-        dofs[i*(dim+1)+dim] = p[i,0]
-        testfunc[i*(dim+1)+dim] = q[i,0]
+        dofs_mass[i*(1)] = p[i,0]
+        testfunc_mass[i*(1)] = q[i,0]
 
-    # Compute LHS and RHS
-    print("Computing " + str(dim) + "D RHS Gauss point contribution\n")
-    rhs = Compute_RHS(res_tot.copy(), testfunc, do_simplifications)
-    rhs_out = OutputVector_CollectingFactors(rhs, "rRightHandSideBoundedVector", mode)
+    # Compute LHSs and RHSs
+    rhs_momentum = Compute_RHS(res_momentum_tot.copy(), testfunc_momentum, do_simplifications)
+    rhs_momentum_out = OutputVector_CollectingFactors(rhs_momentum, "rhs_momentum", mode)
+
+    rhs_mass = Compute_RHS(res_mass_tot.copy(), testfunc_mass, do_simplifications)
+    rhs_mass_out = OutputVector_CollectingFactors(rhs_mass, "rhs_mass", mode)
+    lhs_mass = Compute_LHS(rhs_mass, testfunc_mass, dofs_mass, do_simplifications)
+    lhs_mass_out = OutputMatrix_CollectingFactors(lhs_mass, "lhs_mass", mode)
+
+    rhs_endofstep = Compute_RHS(res_endofstep_tot.copy(), testfunc_endofstep, do_simplifications)
+    rhs_endofstep_out = OutputVector_CollectingFactors(rhs_endofstep, "rhs_endofstep", mode)
+    lhs_endofstep = Compute_LHS(rhs_endofstep, testfunc_endofstep, dofs_endofstep, do_simplifications)
+    lhs_endofstep_out = OutputMatrix_CollectingFactors(lhs_endofstep, "lhs_endofstep", mode)
 
     # Replace the computed RHS and LHS in the template outstring
     if(dim == 2):
-        outstring = outstring.replace("//substitute_rhs_2D", rhs_out)
+        outstring = outstring.replace("//substitute_rhs_momentum_2D", rhs_momentum_out)
+        outstring = outstring.replace("//substitute_rhs_mass_2D", rhs_mass_out)
+        outstring = outstring.replace("//substitute_lhs_mass_2D", lhs_mass_out)
+        outstring = outstring.replace("//substitute_rhs_endofstep_2D", rhs_endofstep_out)
+        outstring = outstring.replace("//substitute_lhs_endofstep_2D", lhs_endofstep_out)
     elif(dim == 3):
-        outstring = outstring.replace("//substitute_rhs_3D", rhs_out)
+        outstring = outstring.replace("//substitute_rhs_momentum_3D", rhs_momentum_out)
+        outstring = outstring.replace("//substitute_rhs_mass_3D", rhs_mass_out)
+        outstring = outstring.replace("//substitute_lhs_mass_3D", lhs_mass_out)
+        outstring = outstring.replace("//substitute_rhs_endofstep_3D", rhs_endofstep_out)
+        outstring = outstring.replace("//substitute_lhs_endofstep_3D", lhs_endofstep_out)
 
     # Substitute the shape function gradients container accesses
     for i_node in range(n_nodes):
