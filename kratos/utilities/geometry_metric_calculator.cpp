e9a99557ff9e1c31bbdc2102aba247c629b5ae1d
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2020-12-03 09:48:04 +0100

    Documentation

--- kratos/utilities/geometry_metric_calculator.cpp
+++ kratos/utilities/geometry_metric_calculator.cpp
@@ -37,8 +37,8 @@ void KRATOS_API(KRATOS_CORE) GeometryMetricCalculator::CalculateMetricTensorData
     const array_1d<double, 3>& r_p_3 = rGeometry[2].Coordinates();
 
     // Solve the metric problem trans(e)*M*e = 1
-    // This means, find the coefficients of the matrix M such that all the edges have unit length
-    Vector sol; //FIXME: WE SHOULD MAKE THE MATH UTILS COMPATIBLE WITH THIS
+    // This means, find the coefficients of the matrix M such that all the geometry edges have unit length
+    Vector sol;
     array_1d<double,3> aux_vect;
     BoundedMatrix<double,3,3> aux_mat;
     aux_mat(0,0) = std::pow(r_p_1[0]-r_p_2[0], 2); aux_mat(0,1) = 2.0*(r_p_1[0]-r_p_2[0])*(r_p_1[1]-r_p_2[1]); aux_mat(0,2) = std::pow(r_p_1[1]-r_p_2[1], 2);
@@ -53,11 +53,11 @@ void KRATOS_API(KRATOS_CORE) GeometryMetricCalculator::CalculateMetricTensorData
     rMetricTensor(0,0) = sol[0]; rMetricTensor(0,1) = sol[1];
     rMetricTensor(1,0) = sol[1]; rMetricTensor(1,1) = sol[2];
 
-    // Calculate the eigenvalues of the metric tensor to obtain the ellipsis of inertia axes lengths
+    // Calculate the eigenvalues of the metric tensor to obtain the ellipsis of inertia axes
     BoundedMatrix<double,2,2> eigenvects, eigenvals;
     MathUtils<double>::GaussSeidelEigenSystem(rMetricTensor, eigenvects, eigenvals);
 
-    // Calculate the reference element size as the average of the ellipsis of intertia axes lengths
+    // Calculate the reference element size as the average of the ellipsis of inertia axes lengths
     const double h_1 = std::sqrt(1.0 / eigenvals(0,0));
     const double h_2 = std::sqrt(1.0 / eigenvals(1,1));
     rReferenceElementSize = 0.5 * (h_1 + h_2);
@@ -78,46 +78,46 @@ void KRATOS_API(KRATOS_CORE) GeometryMetricCalculator::CalculateMetricTensorData
     double& rMetricInfimum,
     double& rMetricSupremum)
 {
-        // Solve the metric problem trans(e)*M*e = 1
-        // This means, find the coefficients of the matrix M such that all the edges have unit length
-        Vector sol;
-        array_1d<double, 6> aux_vect;
-        BoundedMatrix<double, 6, 6> aux_mat;
-        unsigned int row = 0;
-        for (unsigned int i = 0; i < 3; ++i) {
-            const auto& i_coord = rGeometry[i].Coordinates();
-            for (unsigned int j = i + 1; j < 4; ++j) {
-                const auto& j_coord = rGeometry[j].Coordinates();
-                aux_mat(row, 0) = std::pow(i_coord[0]-j_coord[0], 2);
-                aux_mat(row, 1) = 2.0*(i_coord[0]-j_coord[0])*(i_coord[1]-j_coord[1]);
-                aux_mat(row, 2) = 2.0*(i_coord[0]-j_coord[0])*(i_coord[2]-j_coord[2]);
-                aux_mat(row, 3) = std::pow(i_coord[1]-j_coord[1], 2);
-                aux_mat(row, 4) = 2.0*(i_coord[1]-j_coord[1])*(i_coord[2]-j_coord[2]);
-                aux_mat(row, 5) = std::pow(i_coord[2]-j_coord[2], 2);
-                aux_vect(row) = 1.0;
-                row++;
-            }
+    // Solve the metric problem trans(e)*M*e = 1
+    // This means, find the coefficients of the matrix M such that all the geometry edges have unit length
+    Vector sol;
+    array_1d<double, 6> aux_vect;
+    BoundedMatrix<double, 6, 6> aux_mat;
+    unsigned int row = 0;
+    for (unsigned int i = 0; i < 3; ++i) {
+        const auto& i_coord = rGeometry[i].Coordinates();
+        for (unsigned int j = i + 1; j < 4; ++j) {
+            const auto& j_coord = rGeometry[j].Coordinates();
+            aux_mat(row, 0) = std::pow(i_coord[0]-j_coord[0], 2);
+            aux_mat(row, 1) = 2.0*(i_coord[0]-j_coord[0])*(i_coord[1]-j_coord[1]);
+            aux_mat(row, 2) = 2.0*(i_coord[0]-j_coord[0])*(i_coord[2]-j_coord[2]);
+            aux_mat(row, 3) = std::pow(i_coord[1]-j_coord[1], 2);
+            aux_mat(row, 4) = 2.0*(i_coord[1]-j_coord[1])*(i_coord[2]-j_coord[2]);
+            aux_mat(row, 5) = std::pow(i_coord[2]-j_coord[2], 2);
+            aux_vect(row) = 1.0;
+            row++;
         }
-        MathUtils<double>::Solve(aux_mat, sol, aux_vect);
+    }
+    MathUtils<double>::Solve(aux_mat, sol, aux_vect);
 
-        // Set the metric tensor
-        rMetricTensor(0,0) = sol[0]; rMetricTensor(0,1) = sol[1]; rMetricTensor(0,2) = sol[2];
-        rMetricTensor(1,0) = sol[1]; rMetricTensor(1,1) = sol[3]; rMetricTensor(1,2) = sol[4];
-        rMetricTensor(2,0) = sol[2]; rMetricTensor(2,1) = sol[4]; rMetricTensor(2,2) = sol[5];
+    // Set the metric tensor
+    rMetricTensor(0,0) = sol[0]; rMetricTensor(0,1) = sol[1]; rMetricTensor(0,2) = sol[2];
+    rMetricTensor(1,0) = sol[1]; rMetricTensor(1,1) = sol[3]; rMetricTensor(1,2) = sol[4];
+    rMetricTensor(2,0) = sol[2]; rMetricTensor(2,1) = sol[4]; rMetricTensor(2,2) = sol[5];
 
-        // Calculate the eigenvalues of the metric tensor to obtain the ellipsis of inertia axes lengths
-        BoundedMatrix<double,3,3> eigenvects, eigenvals;
-        MathUtils<double>::GaussSeidelEigenSystem(rMetricTensor, eigenvects, eigenvals);
+    // Calculate the eigenvalues of the metric tensor to obtain the ellipsis of inertia axes
+    BoundedMatrix<double,3,3> eigenvects, eigenvals;
+    MathUtils<double>::GaussSeidelEigenSystem(rMetricTensor, eigenvects, eigenvals);
 
-        // Calculate the reference element size as the average of the ellipsis of intertia axes lengths
-        const double h_1 = std::sqrt(1.0 / eigenvals(0,0));
-        const double h_2 = std::sqrt(1.0 / eigenvals(1,1));
-        const double h_3 = std::sqrt(1.0 / eigenvals(2,2));
-        rReferenceElementSize = (h_1 + h_2 + h_3) / 3.0;
+    // Calculate the reference element size as the average of the ellipsis of inertia axes lengths
+    const double h_1 = std::sqrt(1.0 / eigenvals(0,0));
+    const double h_2 = std::sqrt(1.0 / eigenvals(1,1));
+    const double h_3 = std::sqrt(1.0 / eigenvals(2,2));
+    rReferenceElementSize = (h_1 + h_2 + h_3) / 3.0;
 
-        // Get te infimum and supremum of the metric
-        rMetricInfimum = std::min(eigenvals(0,0), std::min(eigenvals(1,1), eigenvals(2,2)));
-        rMetricSupremum = std::max(eigenvals(0,0), std::max(eigenvals(1,1), eigenvals(2,2)));
+    // Get te infimum and supremum of the metric
+    rMetricInfimum = std::min(eigenvals(0,0), std::min(eigenvals(1,1), eigenvals(2,2)));
+    rMetricSupremum = std::max(eigenvals(0,0), std::max(eigenvals(1,1), eigenvals(2,2)));
 }
 
 } // namespace Kratos.
--- kratos/utilities/geometry_metric_calculator.h
+++ kratos/utilities/geometry_metric_calculator.h
@@ -5,9 +5,9 @@
 //                   Multi-Physics
 //
 //  License:         BSD License
-//                     Kratos default license: kratos/license.txt
+//                   Kratos default license: kratos/license.txt
 //
-//  Main authors:    Riccardo Rossi
+//  Main authors:    Ruben Zorrilla
 //
 //
 
@@ -82,6 +82,22 @@ public:
     ///@name Operations
     ///@{
 
+    /**
+     * @brief Calculate the metric tensor of a given geometry
+     * This function calculates the metric tensor and its data for a given geometry.
+     * The metric tensor M is computed by solving the problem trans(e)*M*e = 1, that means find
+     * the coefficients of the matrix M such that all the geometry edges (e) have unit lenght.
+     * The eigenvalues of the metric tensor are also computed in order to obtain the lenghts
+     * of the Steiner inertia ellipsis semiaxes. This allows computing the reference element
+     * size (as the average of the semiaxes lengths) and the infimum and supremum norms of M.
+     * @tparam TDim Geometry working dimension
+     * @tparam TNumNodes Geometry number of nodes
+     * @param rGeometry Reference to the geometry of interest
+     * @param rMetricTensor Reference to the metric tensor
+     * @param rReferenceElementSize Reference to the reference element size
+     * @param rMetricInfimum Reference to the metric infimum
+     * @param rMetricSupremum Reference to the metric supremum
+     */
     template<unsigned int TDim, unsigned int TNumNodes>
     static void KRATOS_API(KRATOS_CORE) CalculateMetricTensorData(
         const GeometryType& rGeometry,
@@ -89,13 +105,6 @@ public:
         double& rReferenceElementSize,
         double& rMetricInfimum,
         double& rMetricSupremum);
-
-    ///@}
-
-private:
-    ///@name Private Operations
-    ///@{
-
 
     ///@}
 }; // Class GeometryMetricCalculator
