cec9d5a1ec0eada930ec84e182f4699b779f037f
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-12-04 13:45:45 +0100

    add adjoint_monolithic_solver

--- applications/FluidDynamicsApplication/python_scripts/adjoint_monolithic_solver.py
+++ applications/FluidDynamicsApplication/python_scripts/adjoint_monolithic_solver.py
@@ -7,17 +7,79 @@ import KratosMultiphysics.FluidDynamicsApplication as KratosCFD
 # Import base class file
 from KratosMultiphysics.FluidDynamicsApplication.adjoint_fluid_solver import AdjointFluidSolver
 
+class StabilizedAdjointFormulation(object):
+    """Helper class to define stabilization-dependent parameters."""
+    def __init__(self,settings):
+        self.element_name = None
+        self.element_integrates_in_time = False
+        self.element_has_nodal_properties = False
+        self.process_data = {}
+
+        if settings.Has("element_type"):
+            formulation = settings["element_type"].GetString()
+            if formulation == "vms":
+                self._SetUpClassicAdjointVMS(settings)
+            elif formulation == "qsvms":
+                self._SetUpAdjointQSVMS(settings)
+        else:
+            print(settings)
+            raise RuntimeError("Argument \'element_type\' not found in stabilization settings.")
+
+    def SetProcessInfo(self,model_part):
+        for variable,value in self.process_data.items():
+            model_part.ProcessInfo[variable] = value
+
+    def _SetUpClassicAdjointVMS(self,settings):
+        default_settings = KratosMultiphysics.Parameters(r"""{
+            "element_type": "vms",
+            "use_orthogonal_subscales": false,
+            "dynamic_tau" : 0.0
+        }""")
+
+        # if non-newtonian, there are some extra options
+        if settings.Has("non_newtonian_fluid_parameters"):
+            raise Exception("Adjoints are not yet supported for HerschelBulkleyVMS")
+
+        self.element_name = 'VMSAdjointElement'
+
+        settings.ValidateAndAssignDefaults(default_settings)
+
+        # set the nodal material properties flag
+        self.element_has_nodal_properties = True
+
+        self.process_data[KratosMultiphysics.DYNAMIC_TAU] = settings["dynamic_tau"].GetDouble()
+        use_oss = settings["use_orthogonal_subscales"].GetBool()
+        self.process_data[KratosMultiphysics.OSS_SWITCH] = int(use_oss)
+
+    def _SetUpAdjointQSVMS(self,settings):
+        default_settings = KratosMultiphysics.Parameters(r"""{
+            "element_type": "qsvms",
+            "use_orthogonal_subscales": false,
+            "dynamic_tau": 0.0,
+            "element_manages_time_integration": false
+        }""")
+        settings.ValidateAndAssignDefaults(default_settings)
+
+        if settings["element_manages_time_integration"].GetBool() == False:
+            self.element_name = "QSVMSAdjoint"
+            self.element_integrates_in_time = False
+        else:
+            raise Exception("Only bossak time integrations is supported with adjoints")
+
+        self.process_data[KratosMultiphysics.DYNAMIC_TAU] = settings["dynamic_tau"].GetDouble()
+        use_oss = settings["use_orthogonal_subscales"].GetBool()
+        self.process_data[KratosMultiphysics.OSS_SWITCH] = int(use_oss)
+
 def CreateSolver(main_model_part, custom_settings):
-    return AdjointVMSMonolithicSolver(main_model_part, custom_settings)
+    return AdjointMonolithicSolver(main_model_part, custom_settings)
 
-class AdjointVMSMonolithicSolver(AdjointFluidSolver):
-
+class AdjointMonolithicSolver(AdjointFluidSolver):
     @classmethod
     def GetDefaultParameters(cls):
         # default settings string in json format
         default_settings = KratosMultiphysics.Parameters("""
         {
-            "solver_type" : "adjoint_vmsmonolithic_solver",
+            "solver_type" : "adjoint_monolithic_solver",
             "model_part_name": "",
             "domain_size": -1,
             "scheme_settings" : {
@@ -48,38 +110,37 @@ class AdjointVMSMonolithicSolver(AdjointFluidSolver):
                 "time_step"           : -0.1
             },
             "consider_periodic_conditions": false,
-            "assign_neighbour_elements_to_conditions": true
+            "assign_neighbour_elements_to_conditions": true,
+            "formulation": {
+                "element_type": "vms"
+            }
         }""")
 
-        default_settings.AddMissingParameters(super(AdjointVMSMonolithicSolver, cls).GetDefaultParameters())
+        default_settings.AddMissingParameters(super(AdjointMonolithicSolver, cls).GetDefaultParameters())
         return default_settings
 
     def __init__(self, model, custom_settings):
         self._validate_settings_in_baseclass=True # To be removed eventually
-        super(AdjointVMSMonolithicSolver,self).__init__(model,custom_settings)
-        self.element_has_nodal_properties = True
+        super(AdjointMonolithicSolver,self).__init__(model,custom_settings)
 
-        self.element_name = "VMSAdjointElement"
-        if self.settings["domain_size"].GetInt() == 2:
-            self.condition_name = "LineCondition"
-        elif self.settings["domain_size"].GetInt() == 3:
-            self.condition_name = "SurfaceCondition"
+        self._SetFormulation()
 
-        self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.OSS_SWITCH, self.settings["oss_switch"].GetInt())
-        self.main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DYNAMIC_TAU, self.settings["dynamic_tau"].GetDouble())
-
-        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Construction of AdjointVMSMonolithicSolver finished.")
+        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Construction of AdjointMonolithicSolver finished.")
 
     def AddVariables(self):
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VELOCITY)
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.MESH_VELOCITY)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.ACCELERATION)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.PRESSURE)
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.RELAXED_ACCELERATION)
         self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.ADJOINT_FLUID_VECTOR_1)
         self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.ADJOINT_FLUID_VECTOR_2)
         self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.ADJOINT_FLUID_VECTOR_3)
         self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.AUX_ADJOINT_FLUID_VECTOR_1)
         self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.ADJOINT_FLUID_SCALAR_1)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VISCOSITY)
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.ADVPROJ)
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DIVPROJ)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DENSITY)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.BODY_FORCE)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.SHAPE_SENSITIVITY)
@@ -91,6 +152,10 @@ class AdjointVMSMonolithicSolver(AdjointFluidSolver):
         KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Adjoint fluid solver variables added correctly.")
 
     def Initialize(self):
+        # If the solver requires an instance of the stabilized formulation class, set the process info variables
+        if hasattr(self, 'formulation'):
+            self.formulation.SetProcessInfo(self.GetComputingModelPart())
+
         # Construct and set the solution strategy
         solution_strategy = self._GetSolutionStrategy()
         solution_strategy.SetEchoLevel(self.settings["echo_level"].GetInt())
@@ -131,3 +196,13 @@ class AdjointVMSMonolithicSolver(AdjointFluidSolver):
             reform_dof_set_at_each_step,
             calculate_norm_dx_flag,
             move_mesh_flag)
+
+    def _SetFormulation(self):
+        self.formulation = StabilizedAdjointFormulation(self.settings["formulation"])
+        self.element_name = self.formulation.element_name
+        self.element_has_nodal_properties = self.formulation.element_has_nodal_properties
+
+        if self.settings["domain_size"].GetInt() == 2:
+            self.condition_name = "LineCondition"
+        elif self.settings["domain_size"].GetInt() == 3:
+            self.condition_name = "SurfaceCondition"
