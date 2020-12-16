439175406ad6ebce8bdf4e4cdf949036fa5b2937
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2020-12-16 16:03:12 +0100

    Include leftover

--- applications/FluidDynamicsApplication/fluid_dynamics_application.h
+++ applications/FluidDynamicsApplication/fluid_dynamics_application.h
@@ -82,7 +82,6 @@
 #include "custom_utilities/fic_data.h"
 #include "custom_utilities/time_integrated_fic_data.h"
 #include "custom_utilities/symbolic_stokes_data.h"
-#include "custom_utilities/symbolic_navier_stokes_data.h"
 #include "custom_utilities/two_fluid_navier_stokes_data.h"
 #include "custom_utilities/weakly_compressible_navier_stokes_data.h"
 
