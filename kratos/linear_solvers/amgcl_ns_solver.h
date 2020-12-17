f8118f0dc8fbd83af253fe5e04f781ff162b76a9
Riccardo Rossi <rrossi@cimne.upc.edu>
2020-12-17 16:40:06 +0100

    improving to use recent version of amgcl and to accept inputs directly as accepted by amgcl

--- kratos/linear_solvers/amgcl_ns_solver.h
+++ kratos/linear_solvers/amgcl_ns_solver.h
@@ -7,7 +7,7 @@
 //  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
-//  Main authors:    Riccardo Rossi
+//  Main authors:    Denis Demidov
 //
 //
 
@@ -33,10 +33,12 @@
 #include <boost/range/iterator_range.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
+#include <amgcl/preconditioner/runtime.hpp>
 #include <amgcl/adapter/crs_tuple.hpp>
 #include <amgcl/adapter/ublas.hpp>
 #include <amgcl/adapter/zero_copy.hpp>
 #include <amgcl/adapter/block_matrix.hpp>
+#include <amgcl/make_block_solver.hpp>
 #include <amgcl/backend/builtin.hpp>
 #include <amgcl/value_type/static_matrix.hpp>
 #include <amgcl/make_solver.hpp>
@@ -74,113 +76,62 @@ public:
 
         Parameters default_parameters( R"(
                                        {
-                                       "solver_type" : "AMGCL_NS_Solver",
-                                       "krylov_type" : "fgmres",
-                                       "velocity_block_preconditioner" :
-                                        {
-                                            "krylov_type" : "lgmres",
-                                            "tolerance" : 1e-3,
-                                            "preconditioner_type" : "ilu0",
-                                            "max_iteration": 5
-                                        },
-                                        "pressure_block_preconditioner" :
-                                        {
-                                            "krylov_type" : "lgmres",
-                                            "tolerance" : 1e-2,
-                                            "preconditioner_type" : "spai0",
-                                            "max_iteration": 20
-                                        },
-                                       "tolerance" : 1e-9,
-                                       "gmres_krylov_space_dimension": 50,
-                                       "coarsening_type": "aggregation",
-                                       "max_iteration": 50,
+                                       "solver_type" : "amgcl_ns",
                                        "verbosity" : 1,
                                        "scaling": false,
-                                       "coarse_enough" : 5000
+                                       "schur_variable" : "PRESSURE",
+                                       "inner_settings" : {
+                                            "solver": {
+                                                "type": "lgmres",
+                                                "M": 50,
+                                                "maxiter": 1000,
+                                                "tol": 1e-8,
+                                                "verbose": true
+                                            },
+                                            "precond": {
+                                                "pmask_size": -1,
+                                                "adjust_p": 0, 
+                                                "type": 2,
+                                                "usolver": {
+                                                    "solver": {
+                                                        "type": "preonly"
+                                                    },
+                                                    "precond": {
+                                                        "relax": {
+                                                            "type": "ilup"
+                                                        },
+                                                        "coarsening": {
+                                                            "type": "aggregation",
+                                                            "aggr": {
+                                                                "eps_strong": 0
+                                                            }
+                                                        }
+                                                    }
+                                                },
+                                                "psolver": {
+                                                    "solver": {
+                                                        "type": "preonly"
+                                                    }
+                                                }
+                                            }
+                                        }
                                    }  )" );
 
 
         //now validate agains defaults -- this also ensures no type mismatch
         rParameters.ValidateAndAssignDefaults(default_parameters);
 
-        std::stringstream msg;
+        //kratos specific settings
+        mTol = rParameters["inner_settings"]["solver"]["tol"].GetDouble();
+        mVerbosity=rParameters["verbosity"].GetInt();
+        mndof = 1; 
 
-        //validate if values are admissible
-        std::set<std::string> available_preconditioners = {"spai0","ilu0","damped_jacobi","gauss_seidel","chebyshev"};
+        //pass settings to amgcl
+        std::stringstream inner_settings;
+        inner_settings << rParameters["inner_settings"].PrettyPrintJsonString() << std::endl;
+        //KRATOS_WATCH(inner_settings)
+        boost::property_tree::read_json(inner_settings, mprm);
 
-        //check velocity block settings
-        if(available_preconditioners.find(rParameters["velocity_block_preconditioner"]["preconditioner_type"].GetString()) == available_preconditioners.end())
-        {
-            msg << "currently prescribed velocity_block_preconditioner preconditioner_type : " << rParameters["velocity_block_preconditioner"]["smoother_type"].GetString() << std::endl;
-            msg << "admissible values are : spai0,ilu0,damped_jacobi,gauss_seidel,chebyshev"<< std::endl;
-            KRATOS_THROW_ERROR(std::invalid_argument," smoother_type is invalid: ",msg.str());
-        }
-
-
-//         //check outer (Pressure Schur) settings
-//         if(available_solvers.find(rParameters["krylov_type"].GetString()) == available_solvers.end())
-//         {
-//             msg << "currently prescribed krylov_type : " << rParameters["krylov_type"].GetString() << std::endl;
-//             msg << "admissible values are : gmres,bicgstab,cg,bicgstabl,bicgstab_with_gmres_fallback"<< std::endl;
-//             KRATOS_THROW_ERROR(std::invalid_argument," krylov_type is invalid: available possibilities are : ",msg.str());
-//         }
-//         if(available_coarsening.find(rParameters["coarsening_type"].GetString()) == available_coarsening.end())
-//         {
-//             msg << "currently prescribed krylov_type : " << rParameters["coarsening_type"].GetString() << std::endl;
-//             msg << "admissible values are : ruge_stuben,aggregation,smoothed_aggregation,smoothed_aggr_emin" << std::endl;
-//             KRATOS_THROW_ERROR(std::invalid_argument," coarsening_type is invalid: available possibilities are : ",msg.str());
-//         }
-
-
-//HERE THE setting of AMGCL (not kratos)
-//     "precond" : {
-//         "usolver" : {
-//             "precond" : {
-//                 "type" : "ilu0"
-//             },
-//             "solver" : {
-//                 "tol" : 1e-2
-//             }
-//         },
-//         "psolver" : {
-//             "solver" : {
-//                 "type" : "lgmres",
-//                 "maxiter" : 20,
-//                 "tol" : 1e-1
-//             }
-//         }
-//     },
-//     "solver" : {
-//         "type" : "fgmres"
-//     }
-
-
-        mcoarse_enough = rParameters["coarse_enough"].GetInt();
-
-        mTol = rParameters["tolerance"].GetDouble();
-        mmax_it = rParameters["max_iteration"].GetInt();
-        mverbosity=rParameters["verbosity"].GetInt();
-        mprm.put("solver.type", rParameters["krylov_type"].GetString());
-
-        mndof = 1; //this will be computed automatically later on
-
-        if(rParameters["krylov_type"].GetString() == "gmres" || rParameters["krylov_type"].GetString() == "lgmres" || rParameters["krylov_type"].GetString() == "fgmres")
-            mprm.put("solver.M",  rParameters["gmres_krylov_space_dimension"].GetInt());
-
-        //setting velocity solver options
-        mprm.put("precond.usolver.solver.type", rParameters["velocity_block_preconditioner"]["krylov_type"].GetString());
-        mprm.put("precond.usolver.solver.tol", rParameters["velocity_block_preconditioner"]["tolerance"].GetDouble());
-        mprm.put("precond.usolver.solver.maxiter", rParameters["velocity_block_preconditioner"]["max_iteration"].GetInt());
-        mprm.put("precond.usolver.precond.type", rParameters["velocity_block_preconditioner"]["preconditioner_type"].GetString());
-
-        //setting pressure solver options
-        mprm.put("precond.psolver.solver.type", rParameters["pressure_block_preconditioner"]["krylov_type"].GetString());
-        mprm.put("precond.psolver.solver.tol", rParameters["pressure_block_preconditioner"]["tolerance"].GetDouble());
-        mprm.put("precond.psolver.solver.maxiter", rParameters["pressure_block_preconditioner"]["max_iteration"].GetInt());
-        mprm.put("precond.psolver.precond.relax.type", rParameters["pressure_block_preconditioner"]["preconditioner_type"].GetString());
-        mprm.put("precond.psolver.precond.coarsening.aggr.eps_strong", 0.0);
-        mprm.put("precond.psolver.precond.coarsening.aggr.block_size", 1);
-        mprm.put("precond.psolver.precond.coarse_enough",mcoarse_enough);
     }
 
     /**
@@ -198,32 +149,38 @@ public:
      */
     bool Solve(SparseMatrixType& rA, VectorType& rX, VectorType& rB) override
     {
-        mprm.put("solver.tol", mTol);
-        mprm.put("solver.maxiter", mmax_it);
-
+        
         mprm.put("precond.pmask", static_cast<void*>(&mp[0]));
         mprm.put("precond.pmask_size", mp.size());
 
-        typedef amgcl::backend::builtin<double> Backend;
+        typedef amgcl::backend::builtin<double> SolverBackend;
+        typedef amgcl::backend::builtin<amgcl::static_matrix<float,3,3>> uBackend;
+        typedef amgcl::backend::builtin<float> pBackend;
 
-        typedef amgcl::make_solver<
-            amgcl::relaxation::as_preconditioner<Backend, amgcl::runtime::relaxation::wrapper>,
-            amgcl::runtime::solver::wrapper<Backend>
+        typedef amgcl::make_block_solver<
+            amgcl::runtime::preconditioner<uBackend>,
+            amgcl::runtime::solver::wrapper<uBackend>
             > USolver;
 
         typedef amgcl::make_solver<
-            amgcl::amg<
-                Backend,
-                amgcl::runtime::coarsening::wrapper,
-                amgcl::runtime::relaxation::wrapper
-                >,
-            amgcl::runtime::solver::wrapper<Backend>
+            amgcl::runtime::preconditioner<pBackend>,
+            amgcl::runtime::solver::wrapper<pBackend>
             > PSolver;
 
-        if(mverbosity > 1)
+        // typedef amgcl::make_solver<
+        //     amgcl::runtime::preconditioner<Backend>,
+        //     amgcl::runtime::solver::wrapper<Backend>
+        //     > USolver;
+
+        // typedef amgcl::make_solver<
+        //     amgcl::runtime::preconditioner<Backend>,
+        //     amgcl::runtime::solver::wrapper<Backend>
+        //     > PSolver;
+
+        if(mVerbosity > 1)
             write_json(std::cout, mprm);
 
-        if(mverbosity == 4)
+        if(mVerbosity == 4)
         {
             //output to matrix market
             std::stringstream matrix_market_name;
@@ -240,16 +197,16 @@ public:
 
         amgcl::make_solver<
             amgcl::preconditioner::schur_pressure_correction<USolver, PSolver>,
-            amgcl::runtime::solver::wrapper<Backend>
-            > solve(amgcl::adapter::zero_copy(rA.size1(), rA.index1_data().begin(), rA.index2_data().begin(), rA.value_data().begin()), mprm);
+            amgcl::runtime::solver::wrapper<SolverBackend>
+            > solve(*amgcl::adapter::zero_copy(rA.size1(), rA.index1_data().begin(), rA.index2_data().begin(), rA.value_data().begin()), mprm);
 
         size_t iters;
         double resid;
         std::tie(iters, resid) = solve(rB, rX);
 
-        KRATOS_WARNING_IF("AMGCL NS Linear Solver", mTol < resid)<<"Non converged linear solution. ["<< resid << " > "<< mTol << "]" << std::endl;
+        KRATOS_WARNING_IF("AMGCL NS Linear Solver", mTol < resid)<<"Non converged linear solution. "<< resid  << std::endl;
 
-        if(mverbosity > 1)
+        if(mVerbosity > 1)
         {
             std::cout << "Iterations: " << iters << std::endl
                       << "Error: " << resid << std::endl
@@ -313,64 +270,112 @@ public:
         SparseMatrixType& rA,
         VectorType& rX,
         VectorType& rB,
-        typename ModelPart::DofsArrayType& rdof_set,
-        ModelPart& r_model_part
+        typename ModelPart::DofsArrayType& rDofSet,
+        ModelPart& rModelPart
     ) override
     {
-//         int old_ndof = -1;
-//         unsigned int old_node_id = rdof_set.begin()->Id();
-//         int ndof=0;
+        //*****************************
+        //compute block size
+        int old_ndof = -1;
+        int ndof=0;
+        if (!rModelPart.IsDistributed())
+        {
+            unsigned int old_node_id = rDofSet.size() ? rDofSet.begin()->Id() : 0;
+            for (auto it = rDofSet.begin(); it!=rDofSet.end(); it++) {
+                if(it->EquationId() < TSparseSpaceType::Size1(rA) ) {
+                    IndexType id = it->Id();
+                    if(id != old_node_id) {
+                        old_node_id = id;
+                        if(old_ndof == -1) old_ndof = ndof;
+                        else if(old_ndof != ndof) { //if it is different than the block size is 1
+                            old_ndof = -1;
+                            break;
+                        }
 
+                        ndof=1;
+                    } else {
+                        ndof++;
+                    }
+                }
+            }
+
+            if(old_ndof == -1)
+                mndof = 1;
+            else
+                mndof = ndof;
+
+        }
+        else //distribute
+        {
+            const std::size_t system_size = TSparseSpaceType::Size1(rA);
+            int current_rank = rModelPart.GetCommunicator().GetDataCommunicator().Rank();
+            unsigned int old_node_id = rDofSet.size() ? rDofSet.begin()->Id() : 0;
+            for (auto it = rDofSet.begin(); it!=rDofSet.end(); it++) {
+                if(it->EquationId() < system_size  && it->GetSolutionStepValue(PARTITION_INDEX) == current_rank) {
+                    IndexType id = it->Id();
+                    if(id != old_node_id) {
+                        old_node_id = id;
+                        if(old_ndof == -1) old_ndof = ndof;
+                        else if(old_ndof != ndof) { //if it is different than the block size is 1
+                            old_ndof = -1;
+                            break;
+                        }
+
+                        ndof=1;
+                    } else {
+                        ndof++;
+                    }
+                }
+            }
+
+            if(old_ndof != -1)
+                mndof = ndof;
+
+            int max_block_size = rModelPart.GetCommunicator().GetDataCommunicator().MaxAll(mndof);
+
+            if( old_ndof == -1) {
+                mndof = max_block_size;
+            }
+
+            KRATOS_ERROR_IF(mndof != max_block_size) << "Block size is not consistent. Local: " << mndof  << " Max: " << max_block_size << std::endl;
+        }
+
+        KRATOS_INFO_IF("AMGCL NS Solver", mVerbosity > 1) << "mndof: " << mndof << std::endl;
+
+        // if(mProvideCoordinates) {
+        //     mCoordinates.resize(TSparseSpaceType::Size1(rA)/mndof);
+        //     unsigned int i=0;
+        //     for (auto it_dof = rDofSet.begin(); it_dof!=rDofSet.end(); it_dof+=mndof) {
+        //         if(it_dof->EquationId() < TSparseSpaceType::Size1(rA) ) {
+        //             auto it_node = rModelPart.Nodes().find(it_dof->Id());
+        //             mCoordinates[ i ] = it_node->Coordinates();
+        //             i++;
+        //         }
+        //     }
+        // }
+
+
+        //*****************************
+        //compute pressure mask
         if(mp.size() != rA.size1()) mp.resize( rA.size1() );
-
-        for (ModelPart::DofsArrayType::iterator it = rdof_set.begin(); it!=rdof_set.end(); it++)
+        for (ModelPart::DofsArrayType::iterator it = rDofSet.begin(); it!=rDofSet.end(); it++)
         {
             const unsigned int eq_id = it->EquationId();
             if( eq_id < rA.size1() )
             {
-//                 unsigned int id = it->Id();
-//                 if(id != old_node_id)
-//                 {
-//                     old_node_id = id;
-//                     if(old_ndof == -1) old_ndof = ndof;
-//                     else if(old_ndof != ndof) //if it is different than the block size is 1
-//                     {
-//                         old_ndof = -1;
-//                         break;
-//                     }
-//
-//                     ndof=1;
-//                 }
-//                 else
-//                 {
-//                     ndof++;
-//                 }
-
                 mp[eq_id]  = (it->GetVariable().Key() == PRESSURE);
             }
         }
 
-        mndof = 1;
-//         if(old_ndof == -1)
-//             mndof = 1;
-//         else
-//             mndof = ndof;
-
-//         if(mverbosity > 0)
-//         {
-//                 KRATOS_WATCH(mndof);
-//         }
 
     }
 
 private:
 
     double mTol;
-    unsigned int mmax_it;
-    int mverbosity;
+    int mVerbosity;
     int mndof;
     std::vector< char > mp;
-    unsigned int mcoarse_enough;
 
     boost::property_tree::ptree mprm;
 
