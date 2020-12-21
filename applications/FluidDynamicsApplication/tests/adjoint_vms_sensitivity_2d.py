9fdc733b8c832a63bc387e3502880d0f3b26fee8
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2020-12-21 16:47:33 +0100

    add norm_square test

--- applications/FluidDynamicsApplication/tests/adjoint_vms_sensitivity_2d.py
+++ applications/FluidDynamicsApplication/tests/adjoint_vms_sensitivity_2d.py
@@ -88,6 +88,44 @@ class AdjointVMSSensitivity2D(KratosUnittest.TestCase):
             self._writeNodalCoordinates(node_id,coord,model_part_file_name)
         return sensitivity
 
+    def _computeFiniteDifferenceNormSquareSensitivity(self,node_ids,step_size,model_part_file_name,drag_direction,drag_file_name):
+        sensitivity = []
+
+        def calculate_objective_value():
+            parameters = KratosMultiphysics.Parameters("""{
+                "norm_model_part_name": "NoSlip2D_Cylinder",
+                "velocity_norm_factor": 1e-4,
+                "pressure_norm_factor": 1e-4
+            }""")
+            response_function = KratosMultiphysics.FluidDynamicsApplication.VelocityPressureNormSquareResponseFunction(
+                parameters,
+                self.test._GetSolver().main_model_part)
+            response_function.Initialize()
+            return response_function.CalculateValue(self.test._GetSolver().main_model_part)
+
+        # unperturbed drag
+        self.solve(model_part_file_name)
+        drag0 = calculate_objective_value()
+        for node_id in node_ids:
+            node_sensitivity = []
+            coord = self._readNodalCoordinates(node_id,model_part_file_name)
+            # X + h
+            perturbed_coord = [coord[0] + step_size, coord[1], coord[2]]
+            self._writeNodalCoordinates(node_id,perturbed_coord,model_part_file_name)
+            self.solve(model_part_file_name)
+            drag = calculate_objective_value()
+            node_sensitivity.append((drag - drag0) / step_size)
+            # Y + h
+            perturbed_coord = [coord[0], coord[1] + step_size, coord[2]]
+            self._writeNodalCoordinates(node_id,perturbed_coord,model_part_file_name)
+            self.solve(model_part_file_name)
+            drag = calculate_objective_value()
+            node_sensitivity.append((drag - drag0) / step_size)
+            sensitivity.append(node_sensitivity)
+            # return mdpa file to unperturbed state
+            self._writeNodalCoordinates(node_id,coord,model_part_file_name)
+        return sensitivity
+
     def _readParameters(self, parameter_file_name):
         with open(parameter_file_name + '_parameters.json', 'r') as parameter_file:
             project_parameters = Parameters(parameter_file.read())
@@ -95,16 +133,16 @@ class AdjointVMSSensitivity2D(KratosUnittest.TestCase):
         return project_parameters
 
     def _createFluidTest(self, parameter_file_name):
-        test = FluidDynamicsAnalysis(Model(), self._readParameters(parameter_file_name))
-        return test
+        self.test = FluidDynamicsAnalysis(Model(), self._readParameters(parameter_file_name))
+        return self.test
 
     def _createAdjointTest(self, parameter_file_name):
         test = AdjointFluidAnalysis(Model(), self._readParameters(parameter_file_name))
         return test
 
     def solve(self, parameter_file_name):
-        test = self._createFluidTest(parameter_file_name)
-        test.Run()
+        self.test = self._createFluidTest(parameter_file_name)
+        self.test.Run()
 
     def testOneElement(self):
         with ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
@@ -172,6 +210,28 @@ class AdjointVMSSensitivity2D(KratosUnittest.TestCase):
             kratos_utils.DeleteFileIfExisting("./steady_cylinder_test.post.bin")
             kratos_utils.DeleteFileIfExisting("./tests.post.lst")
 
+    def testSteadyNormCylinder(self):
+        with ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            # solve fluid
+            self.solve('AdjointVMSSensitivity2DTest/steady_cylinder_test')
+            # solve adjoint
+            test = self._createAdjointTest('AdjointVMSSensitivity2DTest/steady_cylinder_test_norm_adjoint')
+            test.Run()
+            Sensitivity = [[]]
+            Sensitivity[0].append(test._GetSolver().main_model_part.GetNode(1968).GetSolutionStepValue(SHAPE_SENSITIVITY_X))
+            Sensitivity[0].append(test._GetSolver().main_model_part.GetNode(1968).GetSolutionStepValue(SHAPE_SENSITIVITY_Y))
+
+            # calculate sensitivity by finite difference
+            step_size = 1e-9
+            FDSensitivity = self._computeFiniteDifferenceNormSquareSensitivity([1968],step_size,'./AdjointVMSSensitivity2DTest/steady_cylinder_test',[1.0,0.0,0.0],'MainModelPart.NoSlip2D_Cylinder_drag.dat')
+            self.assertAlmostEqual(Sensitivity[0][0], FDSensitivity[0][0], 4)
+            self.assertAlmostEqual(Sensitivity[0][1], FDSensitivity[0][1], 2)
+            self._removeH5Files("MainModelPart")
+            kratos_utils.DeleteFileIfExisting("./AdjointVMSSensitivity2DTest/steady_cylinder_test.time")
+            kratos_utils.DeleteFileIfExisting("./NoSlip2D_Cylinder_drag.dat")
+            kratos_utils.DeleteFileIfExisting("./steady_cylinder_test.post.bin")
+            kratos_utils.DeleteFileIfExisting("./tests.post.lst")
+
     def tearDown(self):
         pass
 
@@ -186,19 +246,19 @@ def _getTimeAveragedDrag(direction,drag_file_name):
     return total_drag
 
 def _readDrag(filename):
-        with open(filename, "r") as file_input:
-            lines = file_input.readlines()
-        time_steps = []
-        reaction = []
-        for line in lines:
-            line = line.strip()
-            if len(line) == 0 or line[0] == '#':
-                continue
-            time_step_data = [float(v) for v in line.split()]
-            time, fx, fy, fz = time_step_data
-            time_steps.append(time)
-            reaction.append([fx, fy, fz])
-        return time_steps, reaction
+    with open(filename, "r") as file_input:
+        lines = file_input.readlines()
+    time_steps = []
+    reaction = []
+    for line in lines:
+        line = line.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
+        time_step_data = [float(v) for v in line.split()]
+        time, fx, fy, fz = time_step_data
+        time_steps.append(time)
+        reaction.append([fx, fy, fz])
+    return time_steps, reaction
 
 if __name__ == '__main__':
     KratosUnittest.main()
