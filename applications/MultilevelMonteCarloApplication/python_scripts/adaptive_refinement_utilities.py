07fb504c1456f6b5193f5530f5aff5f482783c4c
riccardotosi <rtosi@cimne.upc.edu>
2020-12-22 12:37:47 +0100

    Clean-up of the file

--- applications/MultilevelMonteCarloApplication/python_scripts/adaptive_refinement_utilities.py
+++ applications/MultilevelMonteCarloApplication/python_scripts/adaptive_refinement_utilities.py
@@ -3,15 +3,15 @@ import numpy as np
 
 # Import Kratos
 import KratosMultiphysics
-import KratosMultiphysics.MeshingApplication
-try:
-    import KratosMultiphysics.FluidDynamicsApplication
-    have_fluid_dynamics_application = True
-except:
-    have_fluid_dynamics_application = False
-
 from KratosMultiphysics.MultilevelMonteCarloApplication.tools import ParametersWrapper
-
+from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable, IsMPIAvailable()
+if KratosMultiphysics.IsDistributedRun():
+    if not IsMPIAvailable():
+        raise Exception("[MultilevelMonteCarloApplication]: KratosMultiphysics MPI core is not available, but it is required since runnning with MPI.")
+if CheckIfApplicationsAvailable("MeshingApplication"):
+    import KratosMultiphysics.MeshingApplication
+if CheckIfApplicationsAvailable("ExaquteSandboxApplication"):
+    import KratosMultiphysics.ExaquteSandboxApplication
 
 class AdaptiveRefinement(object):
     """
@@ -56,86 +56,62 @@ class AdaptiveRefinement(object):
         problem_type = self.problem_type
         current_level = self.current_level
 
+        # check MeshingApplication is imported,
+        # otherwise raise an error
+        if not CheckIfApplicationsAvailable("MeshingApplication"):
+            raise Exception("[MultilevelMonteCarloApplication]: MeshingApplication cannot be imported, but it is necessary to perform adaptive refinement.")
+
         if (self.metric is "hessian"):
             # initialize interpolation error
             original_interp_error = metric_param["hessian_strategy_parameters"]["interpolation_error"].GetDouble()
+            # set interpolation error for current level
+            if current_level > 0:
+                coefficient_interp_error =  metric_param["hessian_strategy_parameters"]["coefficient_interpolation_error"].GetDouble()
+                metric_param["hessian_strategy_parameters"].RemoveValue("coefficient_interpolation_error")
+                interp_error = original_interp_error*(coefficient_interp_error)**(-current_level)
+                # interp_error = original_interp_error/(coefficient_interp_error*current_level)
+                metric_param["hessian_strategy_parameters"]["interpolation_error"].SetDouble(interp_error)
+            # Setting metric tensor to 0
+            domain_size = self.wrapper.GetDomainSize()
+            model_part_name = parameters_coarse["solver_settings"]["model_part_name"].GetString()
+            if domain_size == 2:
+                KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(KratosMultiphysics.MeshingApplication.METRIC_TENSOR_2D,model_coarse.GetModelPart(model_part_name).Nodes)
+            elif domain_size == 3:
+                KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(KratosMultiphysics.MeshingApplication.METRIC_TENSOR_3D,model_coarse.GetModelPart(model_part_name).Nodes)
+            else:
+                err_msg = "Domain size is {}. Supported values are 2 and 3.\n".format(domain_size)
+            # calculate NODAL_H
+            find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(model_coarse.GetModelPart(model_part_name))
+            find_nodal_h.Execute()
 
             # problem dependent section
             # we build the metric depending on the problem
-            if (problem_type == "potential_flow"):
-                model_part_name = parameters_coarse["solver_settings"]["model_part_name"].GetString()
-                # set NODAL_AREA and NODAL_H as non historical variables
-                KratosMultiphysics.VariableUtils().SetNonHistoricalVariable(KratosMultiphysics.NODAL_AREA, 0.0, model_coarse.GetModelPart(model_part_name).Nodes)
-                KratosMultiphysics.VariableUtils().SetNonHistoricalVariable(KratosMultiphysics.NODAL_H, 0.0, model_coarse.GetModelPart(model_part_name).Nodes)
-                # Setting Metric Tensor to 0
-                KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(KratosMultiphysics.MeshingApplication.METRIC_TENSOR_2D,model_coarse.GetModelPart(model_part_name).Nodes)
-                # calculate NODAL_H
-                find_nodal_h = KratosMultiphysics.FindNodalHProcess(model_coarse.GetModelPart(model_part_name))
-                find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(model_coarse.GetModelPart(model_part_name))
-                find_nodal_h.Execute()
-                custom_gradient = KratosMultiphysics.CompressiblePotentialFlowApplication.ComputeCustomNodalGradientProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.VELOCITY, KratosMultiphysics.NODAL_AREA)
-                custom_gradient.Execute()
-
-                if metric_param.Has("local_gradient_variable"):
-                    metric_param.RemoveValue("local_gradient_variable")
-                if current_level > 0:
-                    coefficient_interp_error =  metric_param["hessian_strategy_parameters"]["coefficient_interpolation_error"].GetDouble()
-                    metric_param["hessian_strategy_parameters"].RemoveValue("coefficient_interpolation_error")
-                    # interp_error = original_interp_error*(coefficient_interp_error)**(-current_level)
-                    interp_error = original_interp_error/(coefficient_interp_error*current_level)
-                    metric_param["hessian_strategy_parameters"]["interpolation_error"].SetDouble(interp_error)
-
-                local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.VELOCITY_X,metric_param)
-                local_gradient.Execute()
-                local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.VELOCITY_Y,metric_param)
-                local_gradient.Execute()
-
-            elif (problem_type == "monolithic"):
-                if have_fluid_dynamics_application:
-                    if metric_param.Has("local_gradient_variable"):
-                        metric_param.RemoveValue("local_gradient_variable")
-                    if current_level > 0:
-                        coefficient_interp_error =  metric_param["hessian_strategy_parameters"]["coefficient_interpolation_error"].GetDouble()
-                        metric_param["hessian_strategy_parameters"].RemoveValue("coefficient_interpolation_error")
-                        # interp_error = original_interp_error*(coefficient_interp_error)**(-current_level)
-                        interp_error = original_interp_error/(coefficient_interp_error*current_level)
-                        metric_param["hessian_strategy_parameters"]["interpolation_error"].SetDouble(interp_error)
-                    model_part_name = parameters_coarse["solver_settings"]["model_part_name"].GetString()
-
-                    # Setting Metric Tensor to 0
-                    KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(KratosMultiphysics.MeshingApplication.METRIC_TENSOR_2D,model_coarse.GetModelPart(model_part_name).Nodes)
-
-                    # calculate NODAL_H
-                    find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(model_coarse.GetModelPart(model_part_name))
-                    find_nodal_h.Execute()
-                    local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.FluidDynamicsApplication.AVERAGE_VELOCITY_X,metric_param)
-                    local_gradient.Execute()
-                    local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.FluidDynamicsApplication.AVERAGE_VELOCITY_Y,metric_param)
-                    local_gradient.Execute()
-                else:
-                    raise Exception("[MultilevelMonteCarloApplication]: Tried to perform adaptive refinement with problem type \"monolithic\", but FluidDynamicsApplication import failed. This application is required to for problem type \"monolithic\".")
-
-            elif (problem_type == "stationary"):
-                if current_level > 0:
-                    coefficient_interp_error =  metric_param["hessian_strategy_parameters"]["coefficient_interpolation_error"].GetDouble()
-                    metric_param["hessian_strategy_parameters"].RemoveValue("coefficient_interpolation_error")
-                    interp_error = original_interp_error*(coefficient_interp_error)**(-current_level)
-                    metric_param["hessian_strategy_parameters"]["interpolation_error"].SetDouble(interp_error)
-                model_part_name = parameters_coarse["solver_settings"]["model_part_name"].GetString()
-                # Setting Metric Tensor to 0
-                KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(KratosMultiphysics.MeshingApplication.METRIC_TENSOR_2D,model_coarse.GetModelPart(model_part_name).Nodes)
-                # calculate NODAL_H
-                find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(model_coarse.GetModelPart(model_part_name))
-                find_nodal_h.Execute()
+            if (problem_type == "stationary"):
                 local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.TEMPERATURE,metric_param)
                 local_gradient.Execute()
 
+            elif (problem_type == "monolithic"):
+                if CheckIfApplicationsAvailable("ExaquteSandboxApplication"):
+                    local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.ExaquteSandboxApplication.AVERAGED_VELOCITY_X,metric_param)
+                    local_gradient.Execute()
+                    local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.ExaquteSandboxApplication.AVERAGED_VELOCITY_Y,metric_param)
+                    local_gradient.Execute()
+                    local_gradient = KratosMultiphysics.MeshingApplication.ComputeHessianSolMetricProcess(model_coarse.GetModelPart(model_part_name),KratosMultiphysics.ExaquteSandboxApplication.AVERAGED_VELOCITY_Z,metric_param)
+                    local_gradient.Execute()
+                else:
+                    raise Exception("[MultilevelMonteCarloApplication]: ExaquteSandboxApplication cannot be imported, but it is necessary to perform adaptive refinement for problems type monolithic.")
+
             # create the remeshing process and execute it
-            domain_size = self.wrapper.GetDomainSize()
             if domain_size == 2:
-                MmgProcess = KratosMultiphysics.MeshingApplication.MmgProcess2D(model_coarse.GetModelPart(model_part_name),remesh_param)
+                if(hasattr(MeshingApplication,"MmgProcess2D")):
+                    MmgProcess = KratosMultiphysics.MeshingApplication.MmgProcess2D(model_coarse.GetModelPart(model_part_name),remesh_param)
+                else:
+                    raise Exception("[MultilevelMonteCarloApplication]: MmgProcess2D atrribute not found within MeshingApplcation. It is required to perform remeshing.")
             elif domain_size == 3:
-                MmgProcess = KratosMultiphysics.MeshingApplication.MmgProcess3D(model_coarse.GetModelPart(model_part_name),remesh_param)
+                if(hasattr(MeshingApplication,"MmgProcess3D")):
+                    MmgProcess = KratosMultiphysics.MeshingApplication.MmgProcess3D(model_coarse.GetModelPart(model_part_name),remesh_param)
+                else:
+                    raise Exception("[MultilevelMonteCarloApplication]: MmgProcess3D atrribute not found within MeshingApplcation. It is required to perform remeshing.")
             else:
                 err_msg = "Domain size is {}. Supported values are 2 and 3.\n".format(domain_size)
             raise Exception(err_msg)
