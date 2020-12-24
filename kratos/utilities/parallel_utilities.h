b63b33334fc0283591372796481967b1dc68c8ee
philbucher <philipp.bucher@tum.de>
2020-12-25 00:33:31 +0100

    removing leftovers

--- kratos/utilities/parallel_utilities.h
+++ kratos/utilities/parallel_utilities.h
@@ -80,7 +80,6 @@ template<
 class BlockPartition
 {
 public:
-Helpers
     /** @param it_begin - iterator pointing at the beginning of the container
      *  @param it_end - iterator pointing to the end of the container
      *  @param Nchunks - number of threads to be used in the loop (must be lower than TMaxThreads)
