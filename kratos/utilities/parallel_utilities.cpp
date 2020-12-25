bbc710027b61f419e935bf435b93ca00f26be4ba
philbucher <philipp.bucher@tum.de>
2020-12-25 12:59:27 +0100

    improvements

--- kratos/utilities/parallel_utilities.cpp
+++ kratos/utilities/parallel_utilities.cpp
@@ -13,12 +13,9 @@
 //
 
 // System includes
-#include <cstdlib>
+#include <cstdlib> // for std::getenv
 
 // External includes
-#ifdef KRATOS_SMP_OPENMP
-#include <omp.h>
-#endif
 
 // Project includes
 #include "input_output/logger.h"
@@ -35,11 +32,18 @@ int ParallelUtilities::GetNumThreads()
 
 void ParallelUtilities::SetNumThreads(const int NumThreads)
 {
+#ifdef KRATOS_SMP_NONE
+    // do nothing if is shared memory parallelization is disabled
+    return;
+#endif
+
     const int num_procs = GetNumProcs();
-    KRATOS_WARNING_IF("ParallelUtilities", NumThreads > num_procs) << "Maximum number of threads (" << num_procs << ") is exceeded!" << std::endl;
+    KRATOS_WARNING_IF("ParallelUtilities", NumThreads > num_procs) << "The number of requested threads (" << NumThreads << ") exceeds the number of available threads (" << num_procs << ")!" << std::endl;
     msNumThreads = NumThreads;
 
 #if defined(KRATOS_SMP_OPENMP)
+    // external libraries included in Kratos still use OpenMP (such as AMGCL)
+    // this makes sure that they use the same number of threads as Kratos itself.
     omp_set_num_threads(NumThreads);
 #endif
 }
@@ -51,7 +55,11 @@ int ParallelUtilities::GetNumProcs()
 
 #elif defined(KRATOS_SMP_CXX11)
     // NOTE: std::thread::hardware_concurrency() can return 0 in some systems!
-    return std::max(1, std::thread::hardware_concurrency());
+    unsigned num_procs = std::thread::hardware_concurrency();
+
+    KRATOS_WARNING_IF("ParallelUtilities", num_procs == 0) << "The number of processors cannot be determined correctly on this machine. Please check your setup carefully!" << std::endl;
+
+    return std::max(1, num_procs);
 
 #else
     return 1;
@@ -60,40 +68,48 @@ int ParallelUtilities::GetNumProcs()
 
 int ParallelUtilities::InitializeNumberOfThreads()
 {
-#ifdef KRATOS_SMP_OPENMP
-    return omp_get_max_threads();
+#ifdef KRATOS_SMP_NONE
+    return 1;
 
-#elif defined(KRATOS_SMP_CXX11)
+#else
     const char* env_kratos = std::getenv("KRATOS_NUM_THREADS");
     const char* env_omp    = std::getenv("OMP_NUM_THREADS");
 
-    int num_threads = 1;
+    int num_threads;
 
     if (env_kratos) {
         // "KRATOS_NUM_THREADS" is in the environment
         // giving highest priority to this variable
         num_threads = std::atoi( env_kratos );
-        KRATOS_DETAIL("Kernel") << "Using \"KRATOS_NUM_THREADS\" for \"GetNumThreads\": " << num_threads << std::endl;
+        KRATOS_DETAIL("Kernel") << "Using \"KRATOS_NUM_THREADS\" for getting the number of threads: " << num_threads << std::endl;
     } else if (env_omp) {
         // "KRATOS_NUM_THREADS" is not in the environment,
         // checking if "OMP_NUM_THREADS" is
         num_threads = std::atoi( env_omp );
-        KRATOS_DETAIL("Kernel") << "Using \"OMP_NUM_THREADS\" for \"GetNumThreads\": " << num_threads << std::endl;
+        KRATOS_DETAIL("Kernel") << "Using \"OMP_NUM_THREADS\" for getting the number of threads: " << num_threads << std::endl;
     } else {
         // if neither "KRATOS_NUM_THREADS" not "OMP_NUM_THREADS"
         // is in the environment, then check the C++ thread function
         // NOTE: this can return 0 in some systems!
         num_threads = std::thread::hardware_concurrency();
-        KRATOS_DETAIL("Kernel") << "Using \"std::thread::hardware_concurrency\" for \"GetNumThreads\": " << num_threads << std::endl;
+        KRATOS_DETAIL("Kernel") << "Using \"std::thread::hardware_concurrency\" for getting the number of threads: " << num_threads << std::endl;
     }
 
-    return std::max(1, num_threads);
+    KRATOS_WARNING_IF("ParallelUtilities", num_threads < 1) << "The number of threads could not be determined correctly, which means that Kratos runs in serial / without shared memory parallelism.\nPlease set \"KRATOS_NUM_THREADS\" as environment variable with a value > 0" << std::endl;
 
-#else
-    return 1;
+    num_threads = std::max(1, num_threads);
+
+#if defined(KRATOS_SMP_OPENMP)
+    // external libraries included in Kratos still use OpenMP (such as AMGCL)
+    // this makes sure that they use the same number of threads as Kratos itself.
+    omp_set_num_threads(num_threads);
+#endif
+
+    return num_threads;
 #endif
 }
 
+// initialize the number of threads to use
 int ParallelUtilities::msNumThreads = ParallelUtilities::InitializeNumberOfThreads();
 
 }  // namespace Kratos.
