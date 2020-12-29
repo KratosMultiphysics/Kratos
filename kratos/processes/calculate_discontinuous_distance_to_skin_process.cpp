ebc6a8d7b97b8c25a4d1743ee5922a58d7d614af
Carlos <roigcarlo@gmail.com>
2020-12-29 14:33:06 +0100

    missing template<>

--- kratos/processes/calculate_discontinuous_distance_to_skin_process.cpp
+++ kratos/processes/calculate_discontinuous_distance_to_skin_process.cpp
@@ -544,7 +544,7 @@ namespace Kratos
 	template class KRATOS_API(KRATOS_CORE) Kratos::CalculateDiscontinuousDistanceToSkinProcess<3>;
 
 	// Local flag creation without using the KRATOS_CREATE_LOCAL_FLAG macro since this has a template parameter.
-	const Kratos::Flags CalculateDiscontinuousDistanceToSkinProcess<2>::CALCULATE_ELEMENTAL_EDGE_DISTANCES(Kratos::Flags::Create(0));
-	const Kratos::Flags CalculateDiscontinuousDistanceToSkinProcess<3>::CALCULATE_ELEMENTAL_EDGE_DISTANCES(Kratos::Flags::Create(0));
+	template<> const Kratos::Flags CalculateDiscontinuousDistanceToSkinProcess<2>::CALCULATE_ELEMENTAL_EDGE_DISTANCES(Kratos::Flags::Create(0));
+	template<> const Kratos::Flags CalculateDiscontinuousDistanceToSkinProcess<3>::CALCULATE_ELEMENTAL_EDGE_DISTANCES(Kratos::Flags::Create(0));
 
 }  // namespace Kratos.
