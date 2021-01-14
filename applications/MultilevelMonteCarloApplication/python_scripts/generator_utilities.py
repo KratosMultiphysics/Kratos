ff57540fa5de8a3dc86fa501e9494761c1a1108e
riccardotosi <rtosi@cimne.upc.edu>
2021-01-14 10:03:49 +0100

    Merge with current master

--- applications/MultilevelMonteCarloApplication/python_scripts/generator_utilities.py
+++ applications/MultilevelMonteCarloApplication/python_scripts/generator_utilities.py
@@ -1,14 +1,11 @@
-# Import Python libraries
 import numpy as np
 
-def GenerateSample(problem_name):
-    """
-    Function handling the generation of random variables for the
-    MultilevelMonteCarloApplication algorithms and implementations.
+# TODO: move in an external file and create a generator class, e.g. MonteCarlo.generator.Generate()
 
-    Input:
-    - problem_name: string containing the problem definition.
-    """
+'''
+function generatng the random variable
+'''
+def GenerateSample(problem_name):
 
     # Poisson equation
     if (problem_name == "poisson_square_2d"):
@@ -30,7 +27,7 @@ def GenerateSample(problem_name):
         if sample[0] >= 1.0 or sample[0] <= 0.0 :
             raise Exception ("stochastic Mach number computed > 1 or < 0")
 
-    # Problem zero problem
+    # # Problem zero problem
     if (problem_name == "ProblemZero"):
         sample = []
         uref = 10 # wind speed of 10 m/s
