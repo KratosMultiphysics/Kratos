689a544669a1078fa912d84ce7dbefcb165c9d3a
Tobias Teschemacher <tobias.teschemacher@tum.de>
2021-01-17 16:28:13 +1000

    add CalculateEstimatedKnotLengthness

--- kratos/geometries/nurbs_surface_geometry.h
+++ kratos/geometries/nurbs_surface_geometry.h
@@ -229,6 +229,22 @@ public:
     }
 
     ///@}
+    ///@name Dynamic access to internals
+    ///@{
+
+    /// Calculate with array_1d<double, 3>
+    void Calculate(
+        const Variable<array_1d<double, 3>>& rVariable,
+        array_1d<double, 3>& rOutput) const override
+    {
+        if (rVariable == CHARACTERISTIC_LENGTH)
+        {
+            const CoordinatesArrayType local_coordinates = rOutput;
+            CalculateEstimatedKnotLengthness(rOutput, local_coordinates);
+        }
+    }
+
+    ///@}
     ///@name Get and Set functions
     ///@{
 
@@ -420,6 +436,37 @@ public:
         }
 
         return result;
+    }
+
+    void CalculateEstimatedKnotLengthness(
+        CoordinatesArrayType& rKnotLengthness,
+        const CoordinatesArrayType& rLocalCoordinates)
+    {
+        const IndexType SpanU = NurbsUtilities::GetLowerSpan(PolynomialDegreeU(), KnotsU(), rLocalCoordinates[0]);
+        const IndexType SpanV = NurbsUtilities::GetLowerSpan(PolynomialDegreeV(), KnotsV(), rLocalCoordinates[1]);
+
+        CoordinatesArrayType p1, p2, p3, p4;
+        CoordinatesArrayType gp1, gp2, gp3, gp4;
+        p1[0] = mKnotsU[SpanU];
+        p1[1] = mKnotsV[SpanV];
+
+        p2[0] = mKnotsU[SpanU + 1];
+        p2[1] = mKnotsV[SpanV];
+
+        p3[0] = mKnotsU[SpanU + 1];
+        p3[1] = mKnotsV[SpanV + 1];
+
+        p4[0] = mKnotsU[SpanU];
+        p4[1] = mKnotsV[SpanV + 1];
+
+        GlobalCoordinates(gp1, p1);
+        GlobalCoordinates(gp2, p2);
+        GlobalCoordinates(gp3, p3);
+        GlobalCoordinates(gp4, p4);
+
+        rKnotLengthness[0] = (norm_2(p1 - p2) + norm_2(p3 - p4)) / 2;
+        rKnotLengthness[1] = (norm_2(p1 - p3) + norm_2(p2 - p4)) / 2;
+        rKnotLengthness[2] = 0;
     }
 
     ///@}
