3ef25491b69edf2def6bdb0d622ea451694203e0
riccardotosi <rtosi@cimne.upc.edu>
2021-01-18 10:54:36 +0100

    Set IS_RESTARTED to false, since we are actually restarting after remeshing.

--- applications/MultilevelMonteCarloApplication/python_scripts/adaptive_refinement_utilities.py
+++ applications/MultilevelMonteCarloApplication/python_scripts/adaptive_refinement_utilities.py
@@ -132,8 +132,11 @@ class AdaptiveRefinement(object):
                 mmg_process.Execute()
 
             # reset variables if needed
-            model_coarse.GetModelPart(model_part_name).ProcessInfo.SetValue(KratosMultiphysics.TIME , 0.0)
-            model_coarse.GetModelPart(model_part_name).ProcessInfo.SetValue(KratosMultiphysics.STEP , 0)
+            model_coarse.GetModelPart(model_part_name).ProcessInfo.SetValue(KratosMultiphysics.TIME, 0.0)
+            model_coarse.GetModelPart(model_part_name).ProcessInfo.SetValue(KratosMultiphysics.STEP, 0)
+            model_coarse.GetModelPart(model_part_name).ProcessInfo.SetValue(KratosMultiphysics.IS_RESTARTED, False)
+            if (problem_type in ["monolithic", "FractionalStep"]):
+                model_coarse.GetModelPart(model_part_name).RemoveSubModelPart("fluid_computational_model_part")
 
             # the refinement process empties the coarse model part object and fill it with the refined model part
             # the solution on the refined grid is obtained from the interpolation of the coarse solution
