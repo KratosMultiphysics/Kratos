ba23027617ef13c0371e8745cc7426efba962227
Miguel <mmaso@cimne.upc.edu>
2021-01-20 17:31:20 +0100

    removing unused elem

--- applications/ShallowWaterApplication/shallow_water_application.cpp
+++ applications/ShallowWaterApplication/shallow_water_application.cpp
@@ -39,8 +39,6 @@ namespace Kratos
 
         mShallowWater2D3N(0, Element::GeometryType::Pointer(new Triangle2D3<Node<3>>(Element::GeometryType::PointsArrayType(3)))),
 
-        mMonotonicElement2D3N(0, Element::GeometryType::Pointer(new Triangle2D3<Node<3>>(Element::GeometryType::PointsArrayType(3)))),
-
         mNothingCondition2D2N( 0, Element::GeometryType::Pointer( new Line2D2< Node<3> >( Element::GeometryType::PointsArrayType (2) ) ) )
     {}
 
@@ -120,7 +118,6 @@ namespace Kratos
         KRATOS_REGISTER_ELEMENT("LagrangianSWE2D4N", mLagrangianSWE2D4N)
 
         KRATOS_REGISTER_ELEMENT("ShallowWater2D3N", mShallowWater2D3N)
-        KRATOS_REGISTER_ELEMENT("MonotonicElement2D3N", mMonotonicElement2D3N)
 
         KRATOS_REGISTER_CONDITION("NothingCondition2D2N", mNothingCondition2D2N)
     }
