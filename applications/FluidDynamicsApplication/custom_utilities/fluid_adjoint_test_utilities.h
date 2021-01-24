81d1b90081e31afc5f837ce842a430138d03716d
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-01-24 18:36:50 +0100

    add fluid_adjoint_test_utilties

---
+++ applications/FluidDynamicsApplication/custom_utilities/fluid_adjoint_test_utilities.h
@@ -0,0 +1,152 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Suneth Warnakulasuriya
+//
+
+#if !defined(KRATOS_FLUID_ADJOINT_TEST_UTILITIES_H_INCLUDED)
+#define KRATOS_FLUID_ADJOINT_TEST_UTILITIES_H_INCLUDED
+
+// System includes
+#include <functional>
+
+// External includes
+
+// Project includes
+#include "containers/model.h"
+#include "includes/model_part.h"
+#include "processes/process.h"
+
+// Application includes
+
+namespace Kratos
+{
+///@name Classes
+///@{
+
+class FluidAdjointTestUtilities
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    using IndexType = std::size_t;
+
+    using NodeType = ModelPart::NodeType;
+
+    ///@}
+    ///@name Classes
+    ///@{
+
+    template <class TDataType>
+    class DataTypeUtilities
+    {
+    public:
+        ///@name Static Operations
+        ///@{
+
+        static TDataType ComputeRelaxedVariableRate(
+            const double BossakAlpha,
+            const Variable<TDataType>& rVariable,
+            const NodeType& rNode);
+
+        static std::function<double&(NodeType&, const IndexType)> GetPerturbationMethod(
+            const Variable<TDataType>& rPerturbationVariable);
+
+        static IndexType GetVariableDimension(
+            const Variable<TDataType>& rVariable,
+            const ProcessInfo& rProcessInfo);
+
+        static void AssignRandomValues(
+            TDataType& rValue,
+            const std::string& rSeed,
+            const IndexType DomainSize,
+            const double MinValue = 0.0,
+            const double MaxValue = 1.0);
+
+        ///@}
+    };
+
+    template<class TContainerType, class TDataType>
+    class ContainerDataTypeUtilities
+    {
+    public:
+        ///@name Static Operations
+        ///@{
+
+        static void RunAdjointElementTest(
+            ModelPart& rPrimalModelPart,
+            ModelPart& rAdjointModelPart,
+            const std::function<void(ModelPart&)>& rUpdateModelPart,
+            const Variable<TDataType>& rVariable,
+            const std::function<void(Matrix&, typename TContainerType::data_type&, const ProcessInfo&)>& rCalculateElementResidualDerivatives,
+            const IndexType EquationOffset,
+            const IndexType DerivativeOffset,
+            const double Delta,
+            const double Tolerance);
+
+        static void RandomFillNodalHistoricalVariable(
+            ModelPart& rModelPart,
+            const Variable<TDataType>& rVariable,
+            const double MinValue = 0.0,
+            const double MaxValue = 1.0,
+            const int Step = 0);
+
+        static void RandomFillContainerVariable(
+            ModelPart& rModelPart,
+            const Variable<TDataType>& rVariable,
+            const double MinValue = 0.0,
+            const double MaxValue = 1.0);
+
+        ///@}
+    };
+
+    ///@}
+    ///@name Static operations
+    ///@{
+
+    template <class TClassType>
+    static void CalculateResidual(
+        Vector& residual,
+        TClassType& rClassTypeObject,
+        const ProcessInfo& rProcessInfo);
+
+    template <class TContainerType>
+    static TContainerType& GetContainer(ModelPart& rModelPart);
+
+    static ModelPart& CreateTestModelPart(
+        Model& rModel,
+        const std::string& rElementName,
+        const std::string& rConditionName,
+        const std::function<ModelPart::PropertiesType::Pointer(ModelPart&)>& rGetElementProperties,
+        const std::function<ModelPart::PropertiesType::Pointer(ModelPart&)>& rGetConditionProperties,
+        const std::function<void(ModelPart&)>& rAddNodalSolutionStepVariablesFuncion,
+        const std::function<void(NodeType&)>& rAddDofsFunction,
+        const int BufferSize = 2);
+
+    template<class TDataType>
+    static void RandomFillNodalHistoricalVariable(
+        ModelPart& rModelPart,
+        const Variable<TDataType>& rVariable,
+        const double MinValue = 0.0,
+        const double MaxValue = 1.0,
+        const int Step = 0)
+    {
+        ContainerDataTypeUtilities<ModelPart::ElementsContainerType, TDataType>::RandomFillNodalHistoricalVariable(
+            rModelPart, rVariable, MinValue, MaxValue, Step);
+    }
+
+    ///@}
+};
+
+///@}
+
+} // namespace Kratos
+
+#endif // KRATOS_FLUID_ADJOINT_TEST_UTILITIES_H_INCLUDED
