03236bedabbbae9a48dce13026b55a451bdf15ac
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-01-25 10:47:03 +0100

    IDE tabs

--- kratos/includes/initial_state.h
+++ kratos/includes/initial_state.h
@@ -1,13 +1,14 @@
 //    |  /           |
 //    ' /   __| _` | __|  _ \   __|
-//    . \  |   (   | |   (   |\__ \.
+//    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics
+//                   Multi-Physics 
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:         BSD License 
+//                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Alejandro Cornejo
+//                    
 //
 
 #if !defined(KRATOS_INITIAL_STATE_H_INCLUDED)
