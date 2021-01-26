8e73e0209d47fe726585139f4d895529aaeb64c9
philbucher <philipp.bucher@tum.de>
2021-01-26 17:25:45 +0100

    adding AtomicMult

--- kratos/utilities/atomic_utilities.h
+++ kratos/utilities/atomic_utilities.h
@@ -78,6 +78,16 @@ inline void AtomicSub(TVectorType1& target, const TVectorType2& value ) {
     }
 }
 
+/** @param target vector variable being atomically updated by doing target *= value
+ * @param value vector value being multiplied
+ * Note that the update is not really atomic, but rather is done component by component
+ */
+template<class TDataType>
+inline void AtomicMult(TDataType& target, const TDataType& value) {
+    #pragma omp atomic
+    target *= value;
+}
+
 }  // namespace Kratos.
 
 #endif // KRATOS_ATOMIC_UTILITIES_H_INCLUDED  defined
