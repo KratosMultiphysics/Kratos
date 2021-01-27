691adffbaa112d1077c38af77bf586b30507d073
philbucher <philipp.bucher@tum.de>
2021-01-27 10:05:07 +0100

    fixed test

--- kratos/tests/test_point.py
+++ kratos/tests/test_point.py
@@ -8,18 +8,18 @@ class TestPoint(KratosUnittest.TestCase):
         arr = KM.Array3(coords)
 
         point = KM.Point(arr)
-        self.assertAlmostEqual(point.X, coords[1])
-        self.assertAlmostEqual(point.Y, coords[2])
-        self.assertAlmostEqual(point.Z, coords[3])
+        self.assertAlmostEqual(point.X, coords[0])
+        self.assertAlmostEqual(point.Y, coords[1])
+        self.assertAlmostEqual(point.Z, coords[2])
 
     def test_point_constructor_with_kratos_vector(self):
         coords = [1.0, -2.5, 3.3]
         vec = KM.Vector(coords)
 
         point = KM.Point(vec)
-        self.assertAlmostEqual(point.X, coords[1])
-        self.assertAlmostEqual(point.Y, coords[2])
-        self.assertAlmostEqual(point.Z, coords[3])
+        self.assertAlmostEqual(point.X, coords[0])
+        self.assertAlmostEqual(point.Y, coords[1])
+        self.assertAlmostEqual(point.Z, coords[2])
 
 
 if __name__ == '__main__':
