53298de8ca197234202a18fac7a10ad5545b2a18
Philipp Bucher <philipp.bucher@tum.de>
2021-01-28 12:28:05 +0100

    Update applications/ExternalSolversApplication/ExternalSolversApplication.py

--- applications/ExternalSolversApplication/ExternalSolversApplication.py
+++ applications/ExternalSolversApplication/ExternalSolversApplication.py
@@ -11,4 +11,4 @@ _ImportApplication(application, application_name)
 
 print("    !!! DEPRECATION WARNING !!!")
 print("    The ExternalSolversApplication is deprecated in favor of the LinearSolversApplication")
-print("    It is no longer updated/maintained and will be removed in the future")
+print("    It is no longer updated/maintained and will be removed in the future, tentatively in July 2021")
