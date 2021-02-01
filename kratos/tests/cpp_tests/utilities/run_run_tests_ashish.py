281eb9e29aa82c89cbff2516c41d9c488cb96533
Philipp Bucher <philipp.bucher@tum.de>
2021-02-01 15:49:31 +0100

    Delete run_run_tests_ashish.py

--- kratos/tests/cpp_tests/utilities/run_run_tests_ashish.py
+++
@@ -1,6 +0,0 @@
-from KratosMultiphysics import *
-
-Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS) # Set the verbosity level
-
-Tester.RunTestCases("*AuxiliarModelPartUtilities*") #Test a specific case
-
