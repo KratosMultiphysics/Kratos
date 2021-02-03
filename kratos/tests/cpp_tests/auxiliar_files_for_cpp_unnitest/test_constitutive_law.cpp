433257dbbe5c06587ecd07e13d6d73b422d4aa27
Alejandro Cornejo <alejandro.cornejo23@gmail.com>
2021-02-03 10:21:31 +0100

    ading dummy CL

---
+++ kratos/tests/cpp_tests/auxiliar_files_for_cpp_unnitest/test_constitutive_law.cpp
@@ -0,0 +1,227 @@
+// KRATOS  ___|  |                   |                   |
+//       \___ \  __|  __| |   |  __| __| |   |  __| _` | |
+//             | |   |    |   | (    |   |   | |   (   | |
+//       _____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_| MECHANICS
+//
+//  License:         BSD License
+//                   license: structural_mechanics_application/license.txt
+//
+//  Main authors:    Alejandro Cornejo
+//
+// System includes
+#include <iostream>
+
+// External includes
+
+// Project includes
+#include "tests/cpp_tests/auxiliar_files_for_cpp_unnitest/test_constitutive_law.h"
+#include "includes/checks.h"
+
+
+namespace Kratos
+{
+/******************************CONSTRUCTOR******************************************/
+/***********************************************************************************/
+
+TestConstitutiveLaw::TestConstitutiveLaw()
+    : ConstitutiveLaw()
+{
+}
+
+/******************************COPY CONSTRUCTOR*************************************/
+/***********************************************************************************/
+
+TestConstitutiveLaw::TestConstitutiveLaw(const TestConstitutiveLaw& rOther)
+    : ConstitutiveLaw(rOther)
+{
+}
+
+/********************************CLONE**********************************************/
+/***********************************************************************************/
+
+ConstitutiveLaw::Pointer TestConstitutiveLaw::Clone() const
+{
+    return Kratos::make_shared<TestConstitutiveLaw>(*this);
+}
+
+/*******************************DESTRUCTOR******************************************/
+/***********************************************************************************/
+
+TestConstitutiveLaw::~TestConstitutiveLaw()
+{
+};
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void  TestConstitutiveLaw::CalculateMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CalculateMaterialResponsePK1 (ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CalculateMaterialResponseKirchhoff (ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CalculateMaterialResponseCauchy (ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::InitializeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::InitializeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::InitializeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::InitializeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::FinalizeMaterialResponsePK1(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::FinalizeMaterialResponsePK2(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::FinalizeMaterialResponseKirchhoff(ConstitutiveLaw::Parameters& rValues)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+double& TestConstitutiveLaw::CalculateValue(
+    ConstitutiveLaw::Parameters& rParameterValues,
+    const Variable<double>& rThisVariable,
+    double& rValue
+    )
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+Vector& TestConstitutiveLaw::CalculateValue(
+    ConstitutiveLaw::Parameters& rParameterValues,
+    const Variable<Vector>& rThisVariable,
+    Vector& rValue
+    )
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+Matrix& TestConstitutiveLaw::CalculateValue(
+    ConstitutiveLaw::Parameters& rParameterValues,
+    const Variable<Matrix>& rThisVariable,
+    Matrix& rValue
+    )
+{
+}
+
+//*************************CONSTITUTIVE LAW GENERAL FEATURES *************************
+/***********************************************************************************/
+
+void TestConstitutiveLaw::GetLawFeatures(Features& rFeatures)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+int TestConstitutiveLaw::Check(
+    const Properties& rMaterialProperties,
+    const GeometryType& rElementGeometry,
+    const ProcessInfo& rCurrentProcessInfo
+    )
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CheckClearElasticMatrix(Matrix& rConstitutiveMatrix)
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CalculateElasticMatrix(
+    Matrix& rConstitutiveMatrix,
+    ConstitutiveLaw::Parameters& rValues
+    )
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CalculatePK2Stress(
+    const Vector& rStrainVector,
+    Vector& rStressVector,
+    ConstitutiveLaw::Parameters& rValues
+    )
+{
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void TestConstitutiveLaw::CalculateCauchyGreenStrain(
+    ConstitutiveLaw::Parameters& rValues,
+    Vector& rStrainVector
+    )
+{
+}
+
+} // Namespace Kratos
