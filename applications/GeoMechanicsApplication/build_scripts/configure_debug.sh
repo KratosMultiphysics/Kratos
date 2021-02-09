ecd9d50c4817a0b457fd8ae1a739fca985f00f61
Vahid Galavi <vahid.galavi@gmail.com>
2021-02-09 15:22:13 +0100

    adding build scripts for Linux

---
+++ applications/GeoMechanicsApplication/build_scripts/configure_debug.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Please do not modify this script
+
+# You can use your interpreter of choice (bash, sh, zsh, ...)
+
+# For any question please contact with us in:
+#   - https://github.com/KratosMultiphysics/Kratos
+
+# Optional parameters:
+# You can find a list will all the compiation options in INSTALL.md or here:
+#   - https://github.com/KratosMultiphysics/Kratos/wiki/Compilation-options
+
+# Function to add apps
+add_app () {
+    export KRATOS_APPLICATIONS="${KRATOS_APPLICATIONS}$1;"
+}
+
+# Set compiler
+export CC=gcc
+export CXX=g++
+
+# Set variables
+export KRATOS_SOURCE="${KRATOS_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}"
+export KRATOS_BUILD="${KRATOS_SOURCE}/build"
+export KRATOS_APP_DIR="${KRATOS_SOURCE}/applications"
+# export KRATOS_INSTALL_PYTHON_USING_LINKS=ON
+
+# Set basic configuration
+export KRATOS_BUILD_TYPE=${KRATOS_BUILD_TYPE:-"Debug"}
+export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
+export BOOST_ROOT=${BOOST_ROOT:-"/home/vahid/Dropbox/Libraries/C++/boost/boost_1_72_0"}
+
+# Set applications to compile
+export KRATOS_APPLICATIONS=
+add_app ${KRATOS_APP_DIR}/LinearSolversApplication
+add_app ${KRATOS_APP_DIR}/ExternalSolversApplication
+add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
+add_app ${KRATOS_APP_DIR}/GeoMechanicsApplication
+
+# Clean
+clear
+rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/cmake_install.cmake"
+rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeCache.txt"
+rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeFiles"
+
+# Configure
+cmake ..                                                                                            \
+-H"${KRATOS_SOURCE}"                                                                                \
+-B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}"                                                            \
+-DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -msse3 -std=c++11 -fopenmp -Wl,--no-as-needed -ldl"           \
+-DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -msse3 -Wl,--no-as-needed -ldl"                                   \
+-DUSE_MPI=OFF                                                                                       \
+-DUSE_COTIRE=OFF 						       		                    \
+-DUSE_EIGEN_MKL=OFF
+
+
+# Buid
+cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install -- -j$(nproc)
+
+#Compilation Performance
+# cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target all_unity -- -j4 && \
+# cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install/fast -- -j4
