c227cad354449bfda78546f1da5b0dd2a055d545
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2021-02-10 21:45:01 +0100

    Minor

--- kratos/solving_strategies/schemes/residual_based_bossak_displacement_scheme.hpp
+++ kratos/solving_strategies/schemes/residual_based_bossak_displacement_scheme.hpp
@@ -216,7 +216,7 @@ public:
         mpDofUpdater->UpdateDofs(rDofSet, rDx);
 
         // Updating time derivatives (nodally for efficiency)
-        block_for_each(rModelPart.Nodes(), array_1d<double,3>(), [&](Nodes<3>& rNode, array_1d<double,3>& rDeltaDisplacementTLS){
+        block_for_each(rModelPart.Nodes(), array_1d<double,3>(), [&](Node<3>& rNode, array_1d<double,3>& rDeltaDisplacementTLS){
             noalias(rDeltaDisplacementTLS) = rNode.FastGetSolutionStepValue(DISPLACEMENT) - rNode.FastGetSolutionStepValue(DISPLACEMENT, 1);
 
             array_1d<double, 3>& r_current_velocity = rNode.FastGetSolutionStepValue(VELOCITY);
