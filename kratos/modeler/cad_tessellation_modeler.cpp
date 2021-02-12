355499a1e9428cb882798844978043756b3bccaf
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2021-02-12 18:17:16 +0100

    Make it compile

--- kratos/modeler/cad_tessellation_modeler.cpp
+++ kratos/modeler/cad_tessellation_modeler.cpp
@@ -29,19 +29,17 @@ namespace Kratos
 
         const auto& r_geometries = cad_model_part.Geometries();
 
-        for (auto it = r_geometries.begin(); it != r_geometries.end(); ++it)
-        {
-            if (it->GetGeometryType() == GeometryData::Kratos_Brep_Curve)
-            {
-
-                // BrepCurveOnSurface *p_aux = &it;
-                // p_aux->pGetCurveOnSurface();
-                // auto tessellation = NurbsCurveTessellation<2, PointerVector<Point>>::ComputeTessellation(
-                //     p_aux->pGetCurveOnSurface(),
-                //     p_aux->PolynomialDegree(0),
-                //     p_aux->DomainInterval(),
-                //     p_aux->KnotSpanIntervals(),
-                //     1e-2);
+        for (auto it = r_geometries.begin(); it != r_geometries.end(); ++it) {
+            if (it->GetGeometryType() == GeometryData::Kratos_Brep_Curve) {
+                const auto& r_aux_geometry = *it;
+                const auto r_brep_geom = dynamic_cast<const BrepCurveOnSurface<PointerVector<Node<3>>, PointerVector<Point>>&>(r_aux_geometry);
+                const auto p_curve_on_surface = r_brep_geom.pGetCurveOnSurface();
+                auto tessellation = NurbsCurveTessellation<2, PointerVector<Node<3>>>::ComputeTessellation(
+                    *p_curve_on_surface,
+                    r_brep_geom.PolynomialDegree(0),
+                    r_brep_geom.DomainInterval(),
+                    r_brep_geom.KnotSpanIntervals(),
+                    1e-2);
             }
         }
     }
