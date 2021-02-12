8f7dd09b9acc7e068d3df65d1dd203f71db1ad77
marandra <marcelo.raschi@gmail.com>
2021-02-12 16:30:37 +0100

    Convert to functions deformation gradient components

--- kratos/python_scripts/set_initial_state_process.py
+++ kratos/python_scripts/set_initial_state_process.py
@@ -72,7 +72,7 @@ class SetInitialStateProcess(KratosMultiphysics.Process):
             "mesh_id"         : 0,
             "model_part_name" : "please_specify_model_part_name",
             "dimension"       : 3,
-            "imposed_strain"  : [0,0,0,0,0,0],
+            "imposed_strain"  : ["0*t","0*t","0*t",0,0,0,0],
             "imposed_stress"  : [0,0,0,0,0,0],
             "imposed_deformation_gradient"  : [[1,0,0],[0,1,0],[0,0,1]],
             "interval"        : [0.0, 1e30]
@@ -87,14 +87,26 @@ class SetInitialStateProcess(KratosMultiphysics.Process):
         self.model_part = Model[settings["model_part_name"].GetString()]
         self.dimension = settings["dimension"].GetInt()
 
+        # init strain
         self.strain_functions = components_to_functions(settings["imposed_strain"])
-        self.stress_functions = components_to_functions(settings["imposed_stress"])
-        # to be implemented
-        self.imposed_deformation_gradient = settings[
-            "imposed_deformation_gradient"
-        ].GetMatrix()
+        nr_comps = len(self.strain_functions)
+        self.imposed_strain = KratosMultiphysics.Vector(nr_comps)
 
-        self.nr_comps = len(settings["imposed_strain"].GetVector())
+        # init stress
+        self.stress_functions = components_to_functions(settings["imposed_stress"])
+        nr_comps = len(self.stress_functions)
+        self.imposed_stress = KratosMultiphysics.Vector(nr_comps)
+
+        # init deformation gradient
+        aux_matrix = settings["imposed_deformation_gradient"]
+        self.deformation_functions = []
+        for row in aux_matrix:
+            aux_vector = components_to_functions(row)
+            self.deformation_functions.append(aux_vector)
+        nrows = aux_matrix.size()
+        ncols = aux_matrix[0].size()
+        self.imposed_deformation_gradient = KratosMultiphysics.Matrix(nrows, ncols)
+
 
     def ExecuteInitializeSolutionStep(self):
         """This method is executed in order to initialize the current step
@@ -103,20 +115,35 @@ class SetInitialStateProcess(KratosMultiphysics.Process):
         self -- It signifies an instance of a class.
         """
         current_time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
-        nr_comps = self.nr_comps
 
         if self.interval.IsInInterval(current_time):
-            self.imposed_strain = [0.0] * nr_comps
+
+            # strain
+            nr_comps = len(self.strain_functions)
             for i in range(nr_comps):
                 self.imposed_strain[i] = self.strain_functions[i].CallFunction(
                     0, 0, 0, current_time, 0, 0, 0
                 )
-            self.imposed_stress = [0.0] * nr_comps
+
+            # stress
+            nr_comps = len(self.stress_functions)
             for i in range(nr_comps):
                 self.imposed_stress[i] = self.stress_functions[i].CallFunction(
                     0, 0, 0, current_time, 0, 0, 0
                 )
+
+            # deformation gradient
+            nr_rows = len(self.deformation_functions)
+            nr_cols = len(self.deformation_functions[0])
+            for r in range(nr_rows):
+                for c in range(nr_cols):
+                    self.imposed_deformation_gradient[r, c] =  \
+                        self.deformation_functions[r][c].CallFunction(
+                            0, 0, 0, current_time, 0, 0, 0
+                        )
+
             self.SetInitialState()
+
 
     def SetInitialState(self):
         """This method creates the c++ process and sets each entity
