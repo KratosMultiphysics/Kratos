ab5ea2b80b7462c7b873c094f32d0dbd2d9c8057
Miguel <mmaso@cimne.upc.edu>
2021-02-13 17:41:05 +0100

    restore verbosity

--- applications/ShallowWaterApplication/tests/run_cpp_unit_tests.py
+++ applications/ShallowWaterApplication/tests/run_cpp_unit_tests.py
@@ -2,7 +2,7 @@ import KratosMultiphysics
 import KratosMultiphysics.ShallowWaterApplication
 
 def run():
-    KratosMultiphysics.Tester.SetVerbosity(KratosMultiphysics.Tester.Verbosity.TESTS_OUTPUTS)
+    KratosMultiphysics.Tester.SetVerbosity(KratosMultiphysics.Tester.Verbosity.TESTS_LIST)
     KratosMultiphysics.Tester.RunTestSuite("ShallowWaterApplicationFastSuite")
 
 if __name__ == '__main__':
