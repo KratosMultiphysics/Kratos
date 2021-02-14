81499e052223e991750411c1d0321a007cd9a5e4
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-02-14 16:08:04 +0100

    update unit test execution python scripts

--- applications/RANSApplication/tests/monolithic_velocity_pressure_formulation_tests.py
+++ applications/RANSApplication/tests/monolithic_velocity_pressure_formulation_tests.py
@@ -9,8 +9,11 @@ class MonolithicVelocityPressureFormulationTest(flow_solver_test_case.FlowSolver
         super(MonolithicVelocityPressureFormulationTest, cls).setUpCase(
             "BackwardFacingStepTest",
             "backward_facing_step_mon_up_parameters.json",
+            "backward_facing_step_material_properties.json",
             False)
+
         cls.transient_scheme_type = "bossak"
+        cls.parameters["<CONSTITUTIVE_LAW>"] = "Newtonian2DLaw"
 
 
 if __name__ == '__main__':
