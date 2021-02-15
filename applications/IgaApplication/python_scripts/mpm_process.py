3f2ac1a681912d1337735f6a647c7c6573f44d18
tteschemacher <tteschemacher@users.noreply.github.com>
2021-02-15 18:04:42 +1000

    clear up python process
    Co-authored-by: Philipp Bucher <philipp.bucher@tum.de>

--- applications/IgaApplication/python_scripts/mpm_process.py
+++ applications/IgaApplication/python_scripts/mpm_process.py
@@ -6,16 +6,4 @@ import KratosMultiphysics.IgaApplication as IGA
 def Factory(settings, model):
     if not isinstance(settings, KratosMultiphysics.Parameters):
         raise Exception("Expected input shall be a Parameters object, encapsulating a json string")
-    return MpmProcess(model, settings["Parameters"])
-
-class MpmProcess(KratosMultiphysics.Process):
-
-    def __init__(self, model, params):
-        KratosMultiphysics.Process.__init__(self)
-        self.process = IGA.MpmProcess(model, params)
-
-    def ExecuteInitializeSolutionStep(self):
-        self.process.ExecuteInitializeSolutionStep()
-
-    def ExecuteFinalizeSolutionStep(self):
-        self.process.ExecuteFinalizeSolutionStep()
+    return IGA.MpmProcess(model, settings["Parameters"])
