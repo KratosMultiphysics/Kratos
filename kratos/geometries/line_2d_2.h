c2dd9129eb134dd73c077ed7a0e951156fee445b
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-02-16 11:42:28 +0100

    Simplify IsInside

--- kratos/geometries/line_2d_2.h
+++ kratos/geometries/line_2d_2.h
@@ -910,9 +910,8 @@ public:
         ) const override
     {
         // We compute the distance, if it is not in the pane we
-        const Point point_to_project(rPoint);
-        Point point_projected;
-        const double distance = GeometricalProjectionUtilities::FastProjectOnLine2D(*this, point_to_project, point_projected);
+        CoordinatesArrayType point_projected;
+        const double distance = GeometricalProjectionUtilities::FastProjectOnLine2D(*this, rPoint, point_projected);
 
         // We check if we are on the plane
         if (std::abs(distance) > std::numeric_limits<double>::epsilon()) {
