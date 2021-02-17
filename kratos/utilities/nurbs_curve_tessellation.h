96180b851eaebbde378f36a5e4779516ea78642c
Dagmawi Bekel <dagmawi.bekel@tum.de>
2021-02-17 17:26:40 +0100

    remove whitespace and fix typo

--- kratos/utilities/nurbs_curve_tessellation.h
+++ kratos/utilities/nurbs_curve_tessellation.h
@@ -10,7 +10,7 @@
 //  Main authors:    Andreas Apostolatos
 //					 Tobias Teschemacher
 //					 Thomas Oberbichler
-//					
+//
 //
 //  Ported from the ANurbs library (https://github.com/oberbichler/ANurbs)
 //
@@ -48,7 +48,7 @@ private:
     ///@{
 
     static double DistanceToLine(
-        const typename GeometryType::CoordinatesArrayType& rPoint, 
+        const typename GeometryType::CoordinatesArrayType& rPoint,
         const typename GeometryType::CoordinatesArrayType& rLineA,
         const typename GeometryType::CoordinatesArrayType& rLineB
         )
@@ -69,7 +69,7 @@ public:
     {
     }
 
-    /** 
+    /**
     * @brief This method tessellates a curve and stores the tessellation in the class
     * @param rGeometry Reference to the geometry
     * @param PolynomialDegree The polynomial degree of the curve
@@ -79,7 +79,7 @@ public:
     * @see ComputeTessellation
     */
     void Tessellate(
-        const GeometryType& rGeometry, 
+        const GeometryType& rGeometry,
         const int PolynomialDegree,
         const NurbsInterval DomainInterval,
         const std::vector<NurbsInterval>& rKnotSpanIntervals,
@@ -93,13 +93,13 @@ public:
             Tolerance);
     }
 
-    /** 
+    /**
     * @brief This method returns the tessellation of a curve
     * @param pGeometry Pointer to the geometry
     * @param PolynomialDegree The polynomial degree of the curve
     * @param DomainInterval The curve interval which is to be tessellated
     * @param KnotSpanIntervals The knot span intervals laying in the DomainInterval
-    * @param Tolerance Tolerance for the choral error
+    * @param Tolerance Tolerance for the chordal error
     * @return std::vector<std::pair<double, Vector>> tessellation
     * @see ANurbs library (https://github.com/oberbichler/ANurbs)
     */
@@ -235,7 +235,7 @@ public:
         return points;
     }
 
-    /** 
+    /**
     * @brief This method returns the already computed tessellation of a curve
     * @return return std::vector<std::pair<double, Vector>> tessellation
     */
