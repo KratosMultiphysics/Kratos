feac5b714ae9834fabbd485e9e3d414a2bf3ef0c
Philipp Bucher <philipp.bucher@tum.de>
2021-02-17 20:34:41 +0100

    Slightly less strict tolerance

--- applications/StructuralMechanicsApplication/tests/test_eigen_solver_with_constraints.py
+++ applications/StructuralMechanicsApplication/tests/test_eigen_solver_with_constraints.py
@@ -107,7 +107,7 @@ class TestEigenSolverWithConstraints(KratosUnittest.TestCase):
             eig_vec_mat = node[StructuralMechanicsApplication.EIGENVECTOR_MATRIX]
             eig_vec_mat_contr = node_const[StructuralMechanicsApplication.EIGENVECTOR_MATRIX]
 
-            self.__CompareMatrix(eig_vec_mat, eig_vec_mat_contr, 10) # Note: this might me too strict depending on the eigenvalue solver (works fine with eigen_eigensystem in compination with the eigen sparse-lu)
+            self.__CompareMatrix(eig_vec_mat, eig_vec_mat_contr, 9) # Note: this might me too strict depending on the eigenvalue solver (works fine with eigen_eigensystem in compination with the eigen sparse-lu)
 
     def __CompareEigenSolutionMasterSlave(self, model_part_with_constraints):
         
