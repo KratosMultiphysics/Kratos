c4dafa820fd695ba5f02ae774d9884eb6a9cf9b4
philbucher <philipp.bucher@tum.de>
2021-02-19 10:16:49 +0100

    adapt tests

--- kratos/tests/cpp_tests/containers/test_array_1d.cpp
+++ kratos/tests/cpp_tests/containers/test_array_1d.cpp
@@ -23,23 +23,13 @@
 namespace Kratos {
 namespace Testing {
 
-KRATOS_TEST_CASE_IN_SUITE(Array1DInitialization, KratosCoreFastSuite) {
-    array_1d<double, 3> arr;
-    KRATOS_CHECK_VECTOR_EQUAL(arr, ZeroVector(3));
-}
-
-KRATOS_TEST_CASE_IN_SUITE(Array1DInitializationSize, KratosCoreFastSuite) {
-    array_1d<double, 3> arr(2);
-    KRATOS_CHECK_VECTOR_EQUAL(arr, ZeroVector(3)); // still initializes all entries
-}
-
 KRATOS_TEST_CASE_IN_SUITE(Array1DInitializationValue, KratosCoreFastSuite) {
     array_1d<double, 3> arr(2, 2.2);
     Vector ref(3);
     ref[0] = 2.2;
     ref[1] = 2.2;
-    ref[2] = 0.0;
-    KRATOS_CHECK_VECTOR_EQUAL(arr, ref);
+    KRATOS_CHECK_DOUBLE_EQUAL(arr[0], ref[0]);
+    KRATOS_CHECK_DOUBLE_EQUAL(arr[1], ref[1]);
 }
 
 KRATOS_TEST_CASE_IN_SUITE(Array1DInitializerList, KratosCoreFastSuite) {
@@ -48,17 +38,6 @@ KRATOS_TEST_CASE_IN_SUITE(Array1DInitializerList, KratosCoreFastSuite) {
     ref[0] = 1.1;
     ref[1] = -2.3;
     ref[2] = 3.4;
-    KRATOS_CHECK_VECTOR_EQUAL(arr, ref);
-}
-
-KRATOS_TEST_CASE_IN_SUITE(Array1DInitializerListShort, KratosCoreFastSuite) {
-    array_1d<double, 5> arr {1.1, -2.3, 3.4}; // shorter list, other values should be default initialized
-    Vector ref(5);
-    ref[0] = 1.1;
-    ref[1] = -2.3;
-    ref[2] = 3.4;
-    ref[3] = 0.0;
-    ref[4] = 0.0;
     KRATOS_CHECK_VECTOR_EQUAL(arr, ref);
 }
 
