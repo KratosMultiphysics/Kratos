95a2824f186f48e147f0609627ed523204d99e45
philbucher <philipp.bucher@tum.de>
2021-02-22 18:07:17 +0100

    initial add of new version

--- applications/CoSimulationApplication/custom_external_libraries/co_sim_io/impl/define.hpp
+++ applications/CoSimulationApplication/custom_external_libraries/co_sim_io/impl/define.hpp
@@ -10,16 +10,22 @@
 //  Main authors:    Philipp Bucher (https://github.com/philbucher)
 //
 
-#ifndef CO_SIM_IO_DEFINE_H_INCLUDED
-#define CO_SIM_IO_DEFINE_H_INCLUDED
+#ifndef CO_SIM_IO_DEFINE_INCLUDED
+#define CO_SIM_IO_DEFINE_INCLUDED
 
 // System includes
-#include <string>
-#include <unordered_map>
+#include <cstddef> // std::ptrdiff_t
+#include <memory> // std::unique_ptr
+#include <vector>
+#include <array>
 
 namespace CoSimIO {
 
-typedef std::unordered_map<std::string, std::string> SettingsType;
+// signed integer type, 32 bit in 32 bit systems, but 64bit in 64 bit systems => like std::size_t but signed
+using IdType = std::ptrdiff_t;
+
+using CoordinatesType = std::array<double,3>;
+using ConnectivitiesType = std::vector<IdType>;
 
 enum class ControlSignal
 {
@@ -34,8 +40,6 @@ enum class ControlSignal
     FinalizeSolutionStep,
     OutputSolutionStep,
 
-    ImportGeometry,
-    ExportGeometry,
     ImportMesh,
     ExportMesh,
     ImportData,
@@ -51,6 +55,39 @@ enum ConnectionStatus
     DisconnectionError
 };
 
+enum class ElementType
+{
+    Hexahedra3D20,
+    Hexahedra3D27,
+    Hexahedra3D8,
+    Prism3D15,
+    Prism3D6,
+    Quadrilateral2D4,
+    Quadrilateral2D8,
+    Quadrilateral2D9,
+    Quadrilateral3D4,
+    Quadrilateral3D8,
+    Quadrilateral3D9,
+    Tetrahedra3D10,
+    Tetrahedra3D4,
+    Triangle2D3,
+    Triangle2D6,
+    Triangle3D3,
+    Triangle3D6,
+    Line2D2,
+    Line2D3,
+    Line3D2,
+    Line3D3,
+    Point2D,
+    Point3D
+};
+
+// Note: std::make_unique is C++14, this can be updated once we upgrade from C++11
+template<typename C, typename...Args>
+std::unique_ptr<C> make_unique(Args &&...args) {
+    return std::unique_ptr<C>(new C(std::forward<Args>(args)...));
+}
+
 } //namespace CoSimIO
 
-#endif // CO_SIM_IO_DEFINE_H_INCLUDED
+#endif // CO_SIM_IO_DEFINE_INCLUDED
