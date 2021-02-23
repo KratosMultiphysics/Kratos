41b7865e5e15a24bddc65e828dc6bccfc6a6f249
philbucher <philipp.bucher@tum.de>
2021-02-23 16:57:45 +0100

    updated KratosCoSimIO

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos_co_sim_io.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos_co_sim_io.py
@@ -15,18 +15,24 @@ class KratosCoSimIO(CoSimulationIO):
     """Wrapper for the CoSimIO to be used with Kratos
     """
     def __init__(self, settings, model, solver_name):
-        super(KratosCoSimIO, self).__init__(settings, model, solver_name)
+        super().__init__(settings, model, solver_name)
+
+        connect_to = self.settings["connect_to"].GetString()
+        if connect_to == "":
+            raise Exception('"connect_to" must be specified!')
 
         connection_settings = CoSimIO.InfoFromParameters(self.settings)
-        connection_settings.SetString("connection_name", solver_name)
+        connection_settings.SetString("my_name", solver_name)
 
         info = CoSimIO.Connect(connection_settings)
         if info.GetInt("connection_status") != CoSimIO.ConnectionStatus.Connected:
             raise Exception("Connecting failed!")
 
+        self.connection_name = info.GetString("connection_name")
+
     def Finalize(self):
         disconnect_settings = CoSimIO.Info()
-        disconnect_settings.SetString("connection_name", self.solver_name)
+        disconnect_settings.SetString("connection_name", self.connection_name)
 
         info = CoSimIO.Disconnect(disconnect_settings)
         if info.GetInt("connection_status") != CoSimIO.ConnectionStatus.Disconnected:
@@ -36,7 +42,7 @@ class KratosCoSimIO(CoSimulationIO):
         model_part_name = interface_config["model_part_name"]
 
         info = CoSimIO.Info()
-        info.SetString("connection_name", self.solver_name)
+        info.SetString("connection_name", self.connection_name)
         info.SetString("identifier", model_part_name)
 
         CoSimIO.ImportMesh(info, self.model[model_part_name]) # TODO this can also be geometry at some point
@@ -45,7 +51,7 @@ class KratosCoSimIO(CoSimulationIO):
         model_part_name = interface_config["model_part_name"]
 
         info = CoSimIO.Info()
-        info.SetString("connection_name", self.solver_name)
+        info.SetString("connection_name", self.connection_name)
         info.SetString("identifier", model_part_name)
 
         CoSimIO.ExportMesh(info, self.model[model_part_name]) # TODO this can also be geometry at some point
@@ -55,16 +61,10 @@ class KratosCoSimIO(CoSimulationIO):
         if data_type == "coupling_interface_data":
             interface_data = data_config["interface_data"]
             info = CoSimIO.Info()
-            info.SetString("connection_name", self.solver_name)
+            info.SetString("connection_name", self.connection_name)
             info.SetString("identifier", interface_data.name)
 
             CoSimIO.ImportData(info, interface_data.GetModelPart(), interface_data.variable, GetDataLocation(interface_data.location))
-
-        elif data_type == "time":
-            time_list = CoSimIO.ImportData(self.solver_name, "time_to_co_sim")
-            if len(time_list) != 1:
-                raise Exception("Wrong size received!")
-            data_config["time"] = time_list[0]
         else:
             raise NotImplementedError('Exporting interface data of type "{}" is not implemented for this IO: "{}"'.format(data_type, self._ClassName()))
 
@@ -73,27 +73,10 @@ class KratosCoSimIO(CoSimulationIO):
         if data_type == "coupling_interface_data":
             interface_data = data_config["interface_data"]
             info = CoSimIO.Info()
-            info.SetString("connection_name", self.solver_name)
+            info.SetString("connection_name", self.connection_name)
             info.SetString("identifier", interface_data.name)
 
             CoSimIO.ExportData(info, interface_data.GetModelPart(), interface_data.variable, GetDataLocation(interface_data.location))
-
-        elif data_type == "control_signal":
-            control_signal_key = data_config["signal"]
-            CoSimIO.SendControlSignal(self.solver_name, data_config["identifier"], control_signal_key)
-
-        elif data_type == "time":
-            current_time = data_config["time"]
-            CoSimIO.ExportData(self.solver_name, "time_from_co_sim", [current_time])
-
-        elif data_type == "convergence_signal":
-            if data_config["is_converged"]:
-                control_signal_key = CoSimIO.ControlSignal.ConvergenceAchieved
-            else:
-                control_signal_key = CoSimIO.ControlSignal.Dummy
-            info = CoSimIO.Info()
-            info.SetString("connection_name", self.solver_name)
-            CoSimIO.SendControlSignal(info, control_signal_key)
         else:
             raise NotImplementedError('Exporting interface data of type "{}" is not implemented for this IO: "{}"'.format(data_type, self._ClassName()))
 
@@ -106,11 +89,11 @@ class KratosCoSimIO(CoSimulationIO):
     @classmethod
     def _GetDefaultParameters(cls):
         this_defaults = KM.Parameters("""{
-            "is_connection_master" : true,
+            "connect_to"           : "",
             "communication_format" : "file",
             "print_timing"         : false
         }""")
-        this_defaults.AddMissingParameters(super(KratosCoSimIO, cls)._GetDefaultParameters())
+        this_defaults.AddMissingParameters(super()._GetDefaultParameters())
         return this_defaults
 
 def GetDataLocation(location_str):
