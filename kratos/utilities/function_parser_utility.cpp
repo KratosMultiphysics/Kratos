18a129a07e5369525bcc25f2ca05d7c6265e71b6
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-02-25 16:29:10 +0100

    Adding utility

---
+++ kratos/utilities/function_parser_utility.cpp
@@ -0,0 +1,183 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Riccardo Rossi
+//  Collaborator:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+// External includes
+#include "tinyexpr/tinyexpr/tinyexpr.h"
+
+// Project includes
+#include "utilities/function_parser_utility.h"
+#include "utilities/string_utilities.h"
+
+namespace Kratos
+{
+
+GenericFunctionUtility::GenericFunctionUtility(
+    const std::string& rFunctionBody,
+    Parameters LocalSystem
+    ) : mOriginalFunctionBody(rFunctionBody),
+        mFunctionBody(rFunctionBody)
+{
+    // Correcting function from python style to tinyexpr
+    std::unordered_map<std::string,std::string> aux_replace_letters({{"**","^"},{"X","a"},{"Y","b"},{"Z","c"}});
+    for (auto& r_pair : aux_replace_letters) {
+        mFunctionBody = StringUtilities::ReplaceAllSubstrings(mFunctionBody, r_pair.first, r_pair.second);
+    }
+
+    // Defining namespace
+    mNameSpace.insert(std::pair<std::string, double>("x", 0.0));
+    mNameSpace.insert(std::pair<std::string, double>("y", 0.0));
+    mNameSpace.insert(std::pair<std::string, double>("z", 0.0));
+    mNameSpace.insert(std::pair<std::string, double>("t", 0.0));
+    mNameSpace.insert(std::pair<std::string, double>("X", 0.0));
+    mNameSpace.insert(std::pair<std::string, double>("Y", 0.0));
+    mNameSpace.insert(std::pair<std::string, double>("Z", 0.0));
+
+    // Initialize parser classes
+    InitializeParser();
+
+    // Here get the local system if it is provided
+    if(LocalSystem.Has("origin")) {
+        mUseLocalSystem = true;
+
+        for(IndexType i = 0; i<3; ++i) {
+            mCenterCoordinates[i] = LocalSystem["origin"][i].GetDouble();
+            for(IndexType j = 0; j<3; ++j) {
+                mRotationMatrix(i, j) = LocalSystem["axes"][i][j].GetDouble();
+            }
+        }
+    } else {
+        mUseLocalSystem = false;
+    }
+
+    // Check if it depends on space
+    if (mOriginalFunctionBody.find(std::string("x")) == std::string::npos &&
+        mOriginalFunctionBody.find(std::string("y")) == std::string::npos &&
+        mOriginalFunctionBody.find(std::string("z")) == std::string::npos &&
+        mOriginalFunctionBody.find(std::string("X")) == std::string::npos &&
+        mOriginalFunctionBody.find(std::string("Y")) == std::string::npos &&
+        mOriginalFunctionBody.find(std::string("Z")) == std::string::npos) {
+        mDependsOnSpace = false;
+    }
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+GenericFunctionUtility::GenericFunctionUtility(GenericFunctionUtility const& rOther)
+    : mNameSpace(rOther.mNameSpace),
+      mOriginalFunctionBody(rOther.mOriginalFunctionBody),
+      mFunctionBody(rOther.mFunctionBody),
+      mDependsOnSpace(rOther.mDependsOnSpace),
+      mUseLocalSystem(rOther.mUseLocalSystem),
+      mRotationMatrix(rOther.mRotationMatrix),
+      mCenterCoordinates(rOther.mCenterCoordinates)
+{
+    // Initialize parser classes
+    InitializeParser();
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+GenericFunctionUtility::~GenericFunctionUtility()
+{
+    te_free(mpTinyExpr);
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+std::string GenericFunctionUtility::FunctionBody()
+{
+    return mOriginalFunctionBody;
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+double GenericFunctionUtility::RotateAndCallFunction(
+    const double x,
+    const double y,
+    const double z,
+    const double t,
+    const double X,
+    const double Y,
+    const double Z
+    )
+{
+    array_1d<double,3> xglobal;
+    xglobal[0] = x;
+    xglobal[1] = y;
+    xglobal[2] = z;
+    array_1d<double,3> xlocal = prod(mRotationMatrix, (xglobal - mCenterCoordinates) );
+    array_1d<double,3> xglobal_initial;
+    xglobal_initial[0] = X;
+    xglobal_initial[1] = Y;
+    xglobal_initial[2] = Z;
+    array_1d<double,3> xlocal_initial = prod(mRotationMatrix, (xglobal_initial - mCenterCoordinates) );
+    return CallFunction(xlocal[0],xlocal[1],xlocal[2], t,xlocal_initial[0],xlocal_initial[1],xlocal_initial[2]);
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+double GenericFunctionUtility::CallFunction(
+    const double x,
+    const double y,
+    const double z,
+    const double t,
+    const double X,
+    const double Y,
+    const double Z
+    )
+{
+    mNameSpace["x"] = x;
+    mNameSpace["y"] = y;
+    mNameSpace["z"] = z;
+    mNameSpace["X"] = X;
+    mNameSpace["Y"] = Y;
+    mNameSpace["Z"] = Z;
+    mNameSpace["t"] = t;
+
+    return te_eval(mpTinyExpr);
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void GenericFunctionUtility::InitializeParser()
+{
+    // Initialize
+    if (mpTinyExpr == nullptr) {
+        int err;
+
+        /* Defining table */
+        double& x = mNameSpace["x"];
+        double& y = mNameSpace["y"];
+        double& z = mNameSpace["z"];
+        double& t = mNameSpace["t"];
+        double& X = mNameSpace["X"];
+        double& Y = mNameSpace["Y"];
+        double& Z = mNameSpace["Z"];
+
+        /* Store variable names and pointers. */
+        const te_variable vars[] = {{"x", &x}, {"y", &y}, {"z", &z}, {"t", &t}, {"a", &X}, {"b", &Y}, {"c", &Z}};
+
+        /* Compile the expression with variables. */
+        mpTinyExpr = te_compile(mFunctionBody.c_str(), vars, 7, &err);
+    }
+}
+
+} /// namespace Kratos
---
+++ kratos/utilities/function_parser_utility.h
@@ -0,0 +1,269 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ \.
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Riccardo Rossi
+//  Collaborator:    Vicente Mataix Ferrandiz
+//
+
+#if !defined(KRATOS_FUNCTION_PARSER_UTILITY_H_INCLUDED)
+#define  KRATOS_FUNCTION_PARSER_UTILITY_H_INCLUDED
+
+// System includes
+#include <unordered_map>
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/kratos_parameters.h"
+
+// Forward declaration to reduce compilation time
+struct te_expr;
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @class GenericFunctionUtility
+ * @ingroup KratosCore
+ * @brief This function allows to call a function method of type f(x, y, z, t) implemented in python style.
+ * @details The functions can be constructed by providing a python-defined method of the type
+ *
+ *  class aux_object_cpp_callback:
+ *    def __init__(self, function_string ):
+ *        #TODO: check python version
+ *        self.compiled_function = compile(function_string, '', 'eval', optimize=2)
+ *
+ *    def f(self,x,y,z,t,X,Y,Z):
+ *        return  eval(self.compiled_function)
+ *
+ * The object is then insantiated as
+ * aux_function = GenericFunctionUtility(aux_object_cpp_callback(self.function_string))
+ *
+ * Optionally one can specify a rotation matrix and an origin so that the function can be defined in a rotated system of coordinates
+ * @author Riccardo Rossi
+ * @author Vicente Mataix Ferrandiz
+ */
+class KRATOS_API(KRATOS_CORE) GenericFunctionUtility
+{
+public:
+    ///@name Type definitions
+    ///@{
+
+    /// The index type definition
+    typedef std::size_t IndexType;
+
+    /// Counted pointer of GenericFunctionUtility
+    KRATOS_CLASS_POINTER_DEFINITION(GenericFunctionUtility);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /**
+     * @brief Default constructor
+     * @param rFunctionBody The string defining the function
+     * @param LocalSystem The parameters defining the local system
+     */
+    GenericFunctionUtility(
+        const std::string& rFunctionBody,
+        Parameters LocalSystem = Parameters{}
+        );
+
+    ///Copy constructor
+    GenericFunctionUtility(GenericFunctionUtility const& rOther);
+
+    /// Destructor.
+    ~GenericFunctionUtility();
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief This method clones the current function instance
+     * @return A clone of the current class
+     */
+    Pointer Clone()
+    {
+        return Kratos::make_shared<GenericFunctionUtility>(*this);
+    }
+
+    /**
+     * @brief This method returns if it depends on space
+     * @return True if it uses the local system, false otherwise
+     */
+    bool UseLocalSystem()
+    {
+        return mUseLocalSystem;
+    }
+
+    /**
+     * @brief This method returns if it depends on space
+     * @return True if it depends on space, false otherwise
+     */
+    bool DependsOnSpace()
+    {
+        return mDependsOnSpace;
+    }
+
+    /**
+     * @brief This method returns the function body
+     * @return The function body
+     */
+    std::string FunctionBody();
+
+    /**
+     * @brief This method rotates and calls the evaluation function
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param z The z coordinate
+     * @param t The time variable
+     * @param X The initial x coordinate
+     * @param Y The initial y coordinate
+     * @param Z The initial z coordinate
+     */
+    double RotateAndCallFunction(
+        const double x,
+        const double y,
+        const double z,
+        const double t,
+        const double X = 0.0,
+        const double Y = 0.0,
+        const double Z = 0.0
+        );
+
+    /**
+     * @brief This calls the evaluation function
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param z The z coordinate
+     * @param t The time variable
+     * @param X The initial x coordinate
+     * @param Y The initial y coordinate
+     * @param Z The initial z coordinate
+     */
+    double CallFunction(
+        const double x,
+        const double y,
+        const double z,
+        const double t,
+        const double X = 0.0,
+        const double Y = 0.0,
+        const double Z = 0.0
+        );
+
+    ///@}
+
+private:
+
+    ///@name Member Variables
+    ///@{
+
+    std::unordered_map<std::string, double>  mNameSpace;   /// The variables considered on the function
+    te_expr* mpTinyExpr = nullptr;                         /// The function parser
+    std::string mOriginalFunctionBody;                     /// The function body (not modified)
+    std::string mFunctionBody;                             /// The function body
+
+    bool mDependsOnSpace = true;                 /// If it depends on space
+    bool mUseLocalSystem = false;                /// If we use a local system
+    BoundedMatrix<double, 3, 3> mRotationMatrix; /// The rotation matrix
+    array_1d<double, 3> mCenterCoordinates;      /// The center of coordinates
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    /**
+     * @brief This method initializes the parser classes
+     */
+    void InitializeParser();
+
+    ///@}
+}; /// GenericFunctionUtility
+
+/**
+ * @class FunctionParser
+ * @ingroup KratosCore
+ * @brief This class parses a string as a std::function
+ * @author Vicente Mataix Ferrandiz
+ */
+class FunctionParser
+{
+public:
+    /**
+     * @brief This method parses a string into a std::function
+     * @details This is the version with 3 arguments (x,y,z)
+     * @param rFunctionBody The string defining the function
+     */
+    static std::function<double(const double, const double, const double)> GenerateFunctionSpace(const std::string& rFunctionBody)
+    {
+        std::function<double(const double, const double, const double)> function = [rFunctionBody](const double x, const double y, const double z) -> double
+        {
+            GenericFunctionUtility parser(rFunctionBody);
+            return parser.CallFunction(x,y,z,0.0);
+        };
+
+        return function;
+    }
+
+    /**
+     * @brief This method parses a string into a std::function
+     * @details This is the version with 4 arguments (x,y,z,t)
+     * @param rFunctionBody The string defining the function
+     */
+    static std::function<double(const double, const double, const double, const double)> GenerateFunction(const std::string& rFunctionBody)
+    {
+        std::function<double(const double, const double, const double, const double)> function = [rFunctionBody](const double x, const double y, const double z, const double t) -> double
+        {
+            GenericFunctionUtility parser(rFunctionBody);
+            return parser.CallFunction(x,y,z,t);
+        };
+
+        return function;
+    }
+
+    /**
+     * @brief This method parses a string into a std::function
+     * @details This is the version with 7 arguments (x,y,z,t)
+     * @param rFunctionBody The string defining the function
+     */
+    static std::function<double(const double, const double, const double, const double, const double, const double, const double)> GenerateFunctionInitialCoordinates(const std::string& rFunctionBody)
+    {
+        std::function<double(const double, const double, const double, const double, const double, const double, const double)> function = [rFunctionBody](const double x, const double y, const double z, const double t, const double X, const double Y, const double Z) -> double
+        {
+            GenericFunctionUtility parser(rFunctionBody);
+            return parser.CallFunction(x,y,z,t,X,Y,Z);
+        };
+
+        return function;
+    }
+}; /// FunctionParser
+
+} /// namespace Kratos
+
+#endif // KRATOS_FUNCTION_PARSER_UTILITY_H_INCLUDED
