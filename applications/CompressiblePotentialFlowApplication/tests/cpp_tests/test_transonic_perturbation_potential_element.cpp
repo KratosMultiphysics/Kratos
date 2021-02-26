e7c8a95f1690190f9bb2a1dbea298665d116dd90
Iñigo López Canalejo <inigo.lopez@tum.de>
2021-02-26 12:35:03 +0100

    Update applications/CompressiblePotentialFlowApplication/tests/cpp_tests/test_transonic_perturbation_potential_element.cpp
    Co-authored-by: Philipp Bucher <philipp.bucher@tum.de>

--- applications/CompressiblePotentialFlowApplication/tests/cpp_tests/test_transonic_perturbation_potential_element.cpp
+++ applications/CompressiblePotentialFlowApplication/tests/cpp_tests/test_transonic_perturbation_potential_element.cpp
@@ -565,7 +565,7 @@ BoundedVector<double,3> AssignDistancesToPerturbationTransonicElement()
     return distances;
 }
 
-void AssignPotentialsToWakeTransonicPerturbationElement(Element::Pointer pElement, const array_1d<double, 3>& rDistances, const std::array<double, 6> rPotential)
+void AssignPotentialsToWakeTransonicPerturbationElement(Element::Pointer pElement, const array_1d<double, 3>& rDistances, const std::array<double, 6>& rPotential)
 {
     for (unsigned int i = 0; i < 3; i++){
         if (rDistances(i) > 0.0)
