ac744296a307bead26d17492e12b9774d624a402
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-03-01 17:43:21 +0100

    add average location deviation response

--- applications/RANSApplication/python_scripts/response_functions/rans_response_function_factory.py
+++ applications/RANSApplication/python_scripts/response_functions/rans_response_function_factory.py
@@ -1,11 +1,16 @@
 # importing the Kratos Library
 from KratosMultiphysics.RANSApplication.response_functions.lift_to_drag_response_function import LiftToDragResponseFunction
+from KratosMultiphysics.RANSApplication.response_functions.average_location_deviation_response_function import AverageLocationDeviationResponseFunction
 
 def CreateResponseFunction(response_id, response_settings, model):
     response_type = response_settings["response_type"].GetString()
 
     if response_type == "lift_to_drag_response":
         return LiftToDragResponseFunction(response_id, response_settings, model)
+    elif response_type == "average_location_deviation_response":
+        return AverageLocationDeviationResponseFunction(response_id, response_settings, model)
     else:
         raise NameError("The type of the following response function is not specified: "+ response_id +
-                        ".\nAvailable types are: 'lift_to_drag_response'." )
+                        ".\nAvailable types are: " +
+                        "\n\t 'lift_to_drag_response'" +
+                        "\n\t 'average_location_deviation_response'." )
--- applications/ShapeOptimizationApplication/python_scripts/analyzer_internal.py
+++ applications/ShapeOptimizationApplication/python_scripts/analyzer_internal.py
@@ -121,7 +121,7 @@ class KratosInternalAnalyzer( AnalyzerBaseClass ):
         sho_response_functions = ["plane_based_packaging", "mesh_based_packaging", "surface_normal_shape_change"]
         csm_response_functions = ["strain_energy", "mass", "eigenfrequency", "adjoint_local_stress", "adjoint_max_stress"]
         convdiff_response_functions = ["point_temperature"]
-        rans_respone_functions = ["lift_to_drag_response"]
+        rans_respone_functions = ["lift_to_drag_response", "average_location_deviation_response"]
 
         for (response_id, response_settings) in specified_responses:
             if response_id in response_functions.keys():
