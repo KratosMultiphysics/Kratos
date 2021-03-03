47e5a612560fb47c8754419d464ad502cdbe5c3d
Inigo Lopez <inigolcanalejo@gmail.com>
2021-03-03 09:12:44 +0100

    print parent element from condition

--- applications/CompressiblePotentialFlowApplication/custom_conditions/potential_wall_condition.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_conditions/potential_wall_condition.cpp
@@ -204,6 +204,8 @@ void PotentialWallCondition<TDim, TNumNodes>::FinalizeNonLinearIteration(const P
     std::vector<double> local_speed_of_sound;
     pElem->CalculateOnIntegrationPoints(SOUND_VELOCITY, local_speed_of_sound, rCurrentProcessInfo);
     this->SetValue(SOUND_VELOCITY, local_speed_of_sound[0]);
+
+    this->SetValue(PRINTED_STEP, pElem->Id());
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
