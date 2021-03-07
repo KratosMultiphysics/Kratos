eb0e38fa4c7c54891885d982a85ef1497a96ad5f
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-03-07 12:29:49 +0100

    python process

--- applications/ShallowWaterApplication/python_scripts/apply_absorbing_boundary_process.py
+++ applications/ShallowWaterApplication/python_scripts/apply_absorbing_boundary_process.py
@@ -24,29 +24,50 @@ class ApplyAbsorbingBoundaryProcess(KM.Process):
 
         KM.Process.__init__(self)
 
-        settings.ValidateAndAssignDefaults(self.GetDefaultParameters())
+        self.settings = settings
+        self.settings.ValidateAndAssignDefaults(self.GetDefaultParameters())
 
         self.processes = []
-        self.model_part = model[settings["computing_model_part_name"].GetString()]
-        boundaries_names = settings["absorbing_boundaries_list"].GetStringArray()
+        self.model_part = model[self.settings["computing_model_part_name"].GetString()]
+        boundaries_names = self.settings["absorbing_boundaries_list"].GetStringArray()
+        distance_calculator_settings = KM.Parameters()
+        distance_calculator_settings.AddValue("r_squared_threshold", self.settings["r_squared_threshold"])
         for name in boundaries_names:
             boundary_part = self.model_part.GetSubModelPart(name)
-            self.processes.append(SW.CalculateDistanceToBoundaryProcess(self.model_part, boundary_part, settings))
+            self.processes.append(SW.CalculateDistanceToBoundaryProcess(self.model_part, boundary_part, distance_calculator_settings))
 
         for process in self.processes:
             process.Check()
 
+    def Check(self):
+        if not self.settings["boundary_velocity"].GetVector().Size() == 3:
+            raise Exception("The boundary velocity must be specified with a three dimensional array")
+
     def ExecuteInitialize(self):
         KM.VariableUtils().SetVariable(KM.DISTANCE, 1e+38, self.model_part.Nodes)
+
+        boundaries_names = self.settings["absorbing_boundaries_list"].GetStringArray()
+        for name in boundaries_names:
+            boundary_part = self.model_part.GetSubModelPart(name)
+            KM.VariableUtils().ApplyFixity(KM.MOMENTUM_X, True, boundary_part.Nodes)
+            KM.VariableUtils().ApplyFixity(KM.MOMENTUM_Y, True, boundary_part.Nodes)
 
     def ExecuteBeforeSolutionLoop(self):
         for process in self.processes:
             process.ExecuteBeforeSolutionLoop()
+
+    def ExecuteInitializeSolutionStep(self):
+        self.model_part.ProcessInfo.SetValue(SW.ABSORBING_DISTANCE, self.settings["absorbing_distance"].GetDouble())
+        self.model_part.ProcessInfo.SetValue(SW.DISSIPATION, self.settings["dissipation_factor"].GetDouble())
+        self.model_part.ProcessInfo.SetValue(SW.BOUNDARY_VELOCITY, self.settings["boundary_velocity"].GetVector())
 
     @staticmethod
     def GetDefaultParameters():
         return KM.Parameters("""{
                 "computing_model_part_name" : "PLEASE_CHOOSE_MODEL_PART_NAME",
                 "absorbing_boundaries_list" : [],
-                "r_squared_treshold"        : 0.99
+                "r_squared_threshold"       : 0.99,
+                "absorbing_distance"        : 0.0,
+                "dissipation_factor"        : 0.0,
+                "boundary_velocity"         : [0.0, 0.0, 0.0]
             }""")
