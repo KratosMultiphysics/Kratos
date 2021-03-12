1fab60491bd96faf8958d2dc715acbc6090653fe
riccardotosi <rtosi@cimne.upc.edu>
2021-03-12 09:07:08 +0100

    Add init file

---
+++ applications/MultilevelMonteCarloApplication/external_libraries/PyCOMPSs/exaqute/__init__.py
@@ -0,0 +1,20 @@
+
+import os
+
+from .common import *  # noqa
+
+exacute_backend = os.environ.get("EXAQUTE_BACKEND")
+print("EXAQUTE_BACKEND=", exacute_backend)
+
+if exacute_backend:
+    exacute_backend = exacute_backend.lower()
+
+if not exacute_backend:
+    print("ExaQUte backend: Local")
+    from .local import *  # noqa
+elif exacute_backend == "hyperloom":
+    from .hyperloom import *  # noqa
+elif exacute_backend == "pycompss":
+    from .pycompss import *
+else:
+    raise Exception("Unknown exaqute backend: {}".format(exacute_backend))
