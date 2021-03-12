e22ddf74f1a1ea66712d841a12a9d60134bc3573
Philipp Bucher <philipp.bucher@tum.de>
2021-03-12 11:48:02 +0100

    Update .github/workflows/configure.cmd
    Co-authored-by: Marc Núñez <marcnc94@gmail.com>

--- .github/workflows/configure.cmd
+++ .github/workflows/configure.cmd
@@ -36,7 +36,7 @@ cmake                                                ^
   -G"Visual Studio 16 2019"                          ^
   -H"%KRATOS_SOURCE%"                                ^
   -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"             ^
-  -DBOOST_ROOT="%TEMP\boost%"                        ^
+  -DBOOST_ROOT="$env:TEMP\boost"                        ^
   -DINSTALL_RUNKRATOS=OFF                            ^
   -DCMAKE_CXX_FLAGS="/Od /we4661 /we4804 /WX"        ^
   -DFORCE_LOCAL_ZLIB_COMPILATION=ON                  ^
