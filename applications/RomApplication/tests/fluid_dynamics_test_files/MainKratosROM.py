6a904295b2ce35bb33f04dc72ee855b7c403362d
Raul Bravo <rbravo555@gmail.com>
2021-03-19 17:05:19 +0100

    Adding test for CFD ROM (re-using geometry from FluidDynamicsApp's CouetteFlowTest test)

---
+++ applications/RomApplication/tests/fluid_dynamics_test_files/MainKratosROM.py
@@ -0,0 +1,40 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import KratosMultiphysics
+from KratosMultiphysics.RomApplication.fluid_dynamics_analysis_rom import FluidDynamicsAnalysisROM
+
+import sys
+import time
+import numpy as np
+import pdb
+
+class TestFluidDynamicsROM(FluidDynamicsAnalysisROM):
+
+    def __init__(self,model,project_parameters):
+        super().__init__(model,project_parameters)
+        self.time_step_solution_container = []
+
+    def FinalizeSolutionStep(self):
+        super(TestFluidDynamicsROM,self).FinalizeSolutionStep()
+        ArrayOfResults = []
+        for node in self._GetSolver().GetComputingModelPart().Nodes:
+            ArrayOfResults.append(node.GetSolutionStepValue(KratosMultiphysics.VELOCITY_X, 0))
+            ArrayOfResults.append(node.GetSolutionStepValue(KratosMultiphysics.VELOCITY_Y, 0))
+            ArrayOfResults.append(node.GetSolutionStepValue(KratosMultiphysics.PRESSURE, 0))
+        self.time_step_solution_container.append(ArrayOfResults)
+
+    def EvaluateQuantityOfInterest(self):
+        SnapshotMatrix = np.zeros((len(self.time_step_solution_container[0]), len(self.time_step_solution_container)))
+        for i in range(len(self.time_step_solution_container)):
+            Snapshot_i= np.array(self.time_step_solution_container[i])
+            SnapshotMatrix[:,i] = Snapshot_i.transpose()
+        return SnapshotMatrix
+
+
+if __name__ == "__main__":
+
+    with open("ProjectParameters.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+    model = KratosMultiphysics.Model()
+    simulation = TestFluidDynamicsROM(model,parameters)
+    simulation.Run()
