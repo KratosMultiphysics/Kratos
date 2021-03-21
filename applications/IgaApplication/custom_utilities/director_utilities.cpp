4662df1fa1b530c51aaf25a028b6fec2508e7d6d
Tobias Teschemacher <tobias.teschemacher@tum.de>
2021-03-21 17:28:55 +1000

    update director utilities

--- applications/IgaApplication/custom_utilities/director_utilities.cpp
+++ applications/IgaApplication/custom_utilities/director_utilities.cpp
@@ -9,6 +9,7 @@
 //
 
 // System includes
+#include <map>
 
 // External includes
 
@@ -34,70 +35,46 @@ namespace Kratos
         Vector brep_ids = mParameters["brep_ids"].GetVector();
         for (IndexType i = 0; i < brep_ids.size(); ++i) {
             auto& r_geometry = mrModelPart.GetRootModelPart().GetGeometry((IndexType)brep_ids[i]);
-
             size_t number_of_control_points = r_geometry.size();
 
             std::vector<IndexType> eqID(number_of_control_points);
-            for (SizeType inodes = 0; inodes < number_of_control_points; ++inodes)
+            for (IndexType inodes = 0; inodes < number_of_control_points; ++inodes)
                 eqID[inodes] = r_geometry[inodes].GetId();
 
-            SparseMatrixType NTN(number_of_control_points, number_of_control_points, number_of_control_points*4); //inital guess how much non-zero are there
-            // Can't we solve each patch independently? -> much more efficient
+            SparseMatrixType NTN(number_of_control_points, number_of_control_points, number_of_control_points*number_of_control_points); //inital guess how much non-zero are there
             Matrix directorAtIntgrationPoints{ ZeroMatrix(number_of_control_points, 3) };
-
             SparseSpaceType::SetToZero(NTN);
-            Matrix NTNele;
-            Matrix RhsEle;
-
-            RhsEle = zero_matrix<double>(number_of_control_points,3);
-            NTNele = zero_matrix<double>(number_of_control_points);
 
             PointerVector<Geometry<Node<3>>> quad_points;
             r_geometry.CreateQuadraturePointGeometries(quad_points, 3);
-            for (SizeType iP = 0; iP < quad_points.size(); ++iP)
+            for (IndexType iP = 0; iP < quad_points.size(); ++iP)
             {
                 const Matrix& r_N = quad_points[iP].ShapeFunctionsValues();
-                const array_1d<double, 3> A3 = quad_points[iP].UnitNormal(iP);
+                const array_1d<double, 3> A3 = quad_points[iP].UnitNormal(0);
 
-                NTNele += outer_prod(row(r_N,0), row(r_N, 0));
+                NTN += outer_prod(row(r_N,0), row(r_N, 0));
 
-                for(SizeType inodes=0; inodes< number_of_control_points;++inodes)
-                   row( RhsEle,inodes) += r_N(0, inodes)*trans(A3);
+                for (IndexType inodes = 0; inodes < quad_points.size(); ++inodes) {
+                    auto it = find(eqID.begin(), eqID.end(), quad_points[iP][inodes].Id());
+                    IndexType node_index = it - eqID.begin();
+
+                    row(directorAtIntgrationPoints, node_index) += r_N(0, inodes) * trans(A3);
+                }
             }
-
-            for (SizeType inodes = 0; inodes < number_of_control_points; ++inodes)
-                row(directorAtIntgrationPoints, eqID[inodes]) += row(RhsEle, inodes);
-
-            SparseSpaceType::AssembleLHS(NTN, NTNele, eqID);
-
-            std::cout << "asdasdads" << std::endl;
-        
             Parameters solver_parameters(mParameters["linear_solver_settings"]);
             if (!solver_parameters.Has("solver_type")) solver_parameters.AddString("solver_type", "skyline_lu_factorization");
 
-            Matrix nodalDirectors(number_of_control_points, 3);
+            Matrix nodalDirectors = ZeroMatrix(number_of_control_points, 3);
             auto solver = LinearSolverFactory<SparseSpaceType, LocalSpaceType>().Create(solver_parameters);
-            std::cout << "asdasdads" << std::endl;
-           // DenseVectorType nodalDirectorsVec(number_of_control_points);
-           // DenseVectorType directorAtIntgrationPointsVec(number_of_control_points);
+
             solver->Solve(NTN, nodalDirectors, directorAtIntgrationPoints);
-           // for (SizeType i = 0; i<3; ++i)
-           // { 
-            //   solver->Solve(NTN, nodalDirectorsVec, directorAtIntgrationPointsVec);
-           // }
-            std::cout << "asdasdads" << brep_ids.size()<< std::endl;
 
-            std::cout << nodalDirectors << number_of_control_points<< std::endl;
-
-            for (SizeType i = 0; i < number_of_control_points; ++i)
+            for (IndexType i = 0; i < number_of_control_points; ++i)
             {
-                std::cout << row(nodalDirectors, i) << std::endl;
                 r_geometry[i].SetValue(DIRECTOR, row(nodalDirectors, i));
                 r_geometry[i].SetValue(DIRECTORLENGTH, norm_2(row(nodalDirectors, i)));
-                KRATOS_WATCH(Shell5pElement::TangentSpaceFromStereographicProjection(row(nodalDirectors, i)))
                 r_geometry[i].SetValue(DIRECTORTANGENTSPACE, Shell5pElement::TangentSpaceFromStereographicProjection(row(nodalDirectors, i)));
             }
-            KRATOS_WATCH(nodalDirectors)
         }
     }
 }  // namespace Kratos.
