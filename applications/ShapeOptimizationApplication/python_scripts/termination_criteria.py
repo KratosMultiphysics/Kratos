90f87723c54d85d31f97d152285d2cbbe46104a5
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-03-22 06:33:38 +0100

    adding moving min criteria

--- applications/ShapeOptimizationApplication/python_scripts/termination_criteria.py
+++ applications/ShapeOptimizationApplication/python_scripts/termination_criteria.py
@@ -5,7 +5,7 @@ import KratosMultiphysics as Kratos
 def CreateTerminationCriteria(parameters):
     termination_criteria_type = parameters["termination_criteria"].GetString()
 
-    available_criterians = ["relative_value", "moving_max"]
+    available_criterians = ["relative_value", "moving_max", "moving_min"]
     if (termination_criteria_type not in available_criterians):
         msg = "Unsupported termination criteria requested. [ \"termination_criteria\" = " + termination_criteria_type
         msg += "\nSupported types: \n\t"
@@ -16,6 +16,8 @@ def CreateTerminationCriteria(parameters):
         return RelativeToleranceTerminationCriteria(parameters)
     elif (termination_criteria_type == "moving_max"):
         return MovingAverageTerminationCriteria(parameters)
+    elif (termination_criteria_type == "moving_min"):
+        return MovingMinTerminationCriteria(parameters)
 
 class TerminationCriteria:
     def __init__(self, parameters):
@@ -83,5 +85,43 @@ class MovingAverageTerminationCriteria(TerminationCriteria):
         return is_converged
 
 
+class MovingMinTerminationCriteria(TerminationCriteria):
+    def __init__(self, parameters):
+        super().__init__(parameters)
 
+        defaults = Kratos.Parameters("""{
+            "termination_criteria"  : "moving_min",
+            "moving_min_window"     : 10,
+            "list_of_identifiers"   : []
+        }""")
+
+        self.parameters.ValidateAndAssignDefaults(defaults)
+        self.moving_min_window = self.parameters["moving_min_window"].GetInt()
+        self.list_of_identifiers = self.parameters["list_of_identifiers"].GetStringArray()
+        self.identifier_min_values = {}
+
+    def IsConverged(self, data_logger, optimization_iteration):
+        responses_and_values = data_logger.GetValues("response_value")
+
+        is_converged = optimization_iteration > self.moving_min_window
+
+        if (is_converged):
+            Kratos.Logger.Print("")
+            for identifier, response_values in responses_and_values.items():
+                if (identifier in self.list_of_identifiers):
+                    moving_min = response_values[optimization_iteration]
+                    for i in range(optimization_iteration - 1, optimization_iteration - self.moving_min_window, -1):
+                        moving_min = min(moving_min, response_values[i])
+
+                    if (identifier in self.identifier_min_values.keys()):
+                        all_time_min = self.identifier_min_values[identifier]
+                        is_converged = is_converged and (all_time_min < moving_min)
+                        self.identifier_min_values[identifier] = min(all_time_min, moving_min)
+                    else:
+                        is_converged = False
+                        self.identifier_min_values[identifier] = moving_min
+
+                    Kratos.Logger.PrintInfo("MovingAverageTerminationCriteria", "identifier = {:s}, Current min = {:1.4e}, moving min = {:1.4e}".format(identifier, self.identifier_min_values[identifier], moving_min))
+
+        return is_converged
 
