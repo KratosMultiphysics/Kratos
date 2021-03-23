07cd6c1cd48b7b76ab002aa2b4503de3cd7acb45
jgonzalezusua <39963156+jgonzalezusua@users.noreply.github.com>
2021-03-23 11:52:58 +0100

    Declare seed and generator member variable

--- applications/DEMApplication/custom_utilities/inlet.h
+++ applications/DEMApplication/custom_utilities/inlet.h
@@ -46,7 +46,7 @@ namespace Kratos {
         KRATOS_CLASS_POINTER_DEFINITION(DEM_Inlet);
 
         /// Constructor:
-        DEM_Inlet(ModelPart& inlet_modelpart);
+        DEM_Inlet(ModelPart& inlet_modelpart, int& seed, bool& use_external_seed);
 
         /// Destructor.
         virtual ~DEM_Inlet(){}
@@ -96,7 +96,9 @@ namespace Kratos {
         std::vector<int> mNumberOfParticlesInjected;
         std::map<int, std::string> mOriginInletSubmodelPartIndexes;
         double mTotalMassInjected;
+        int mSeed;
         std::vector<double> mMassInjected;
+        std::mt19937 mGenerator;
         // The following two ratios mark the limit indentation (normalized by the radius) for releasing a particle
         // and for allowing a new one to be injected. admissible_indentation_ratio_for_release should be smaller
         // (more strict), since we want to make sure that the particle is taken far enough to avoid interferences
