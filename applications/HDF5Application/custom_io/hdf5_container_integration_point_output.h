8a12c4322d8342879443538cc29955cc6140f172
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-03-24 06:41:57 +0100

    add core integration point output h

---
+++ applications/HDF5Application/custom_io/hdf5_container_integration_point_output.h
@@ -0,0 +1,102 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: HDF5Application/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya, https://github.com/sunethwarna
+//
+
+#if !defined(KRATOS_HDF5_CONTAINER_INTEGRATION_POINT_OUTPUT_H_INCLUDED)
+#define KRATOS_HDF5_CONTAINER_INTEGRATION_POINT_OUTPUT_H_INCLUDED
+
+// System includes
+#include <string>
+#include <vector>
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/process_info.h"
+
+// Application includes
+#include "custom_io/hdf5_file.h"
+#include "hdf5_application_define.h"
+
+namespace Kratos
+{
+class Parameters;
+
+namespace HDF5
+{
+///@addtogroup HDF5Application
+///@{
+
+///@name Kratos Classes
+///@{
+
+/// A class for IO of element data in HDF5.
+template <typename TContainerType, typename... TComponents>
+class ContainerIntegrationPointOutput
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    using TContainerItemType = typename TContainerType::value_type;
+
+    /// Pointer definition
+    KRATOS_CLASS_POINTER_DEFINITION(ContainerIntegrationPointOutput);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Constructor.
+    ContainerIntegrationPointOutput(Parameters Settings, File::Pointer pFile, const std::string& rPath);
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    ///@}
+
+protected:
+    ///@name Protected Operations
+    ///@{
+
+    void WriteContainerIntegrationPointsValues(
+        TContainerType& rContainerItems,
+        const ProcessInfo& rProcessInfo);
+
+    ///@}
+
+private:
+    ///@name Member Variables
+    ///@{
+    File::Pointer mpFile;
+    std::string mVariablePath;
+    std::vector<std::string> mVariableNames;
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    template <typename... TArgs>
+    void WriteRegisteredIntegrationPointValues(
+        const std::string& rComponentName,
+        TArgs&... args);
+
+    ///@}
+
+}; // class ContainerIntegrationPointOutput.
+
+///@} // Kratos Classes
+///@} addtogroup
+} // namespace HDF5.
+} // namespace Kratos.
+
+#endif // KRATOS_HDF5_CONTAINER_INTEGRATION_POINT_OUTPUT_H_INCLUDED defined
