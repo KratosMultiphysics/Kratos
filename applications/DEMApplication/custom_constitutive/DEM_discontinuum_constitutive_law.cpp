dd5c87638c572172a454e20237f4bed70ba668c3
Joaquín Irazabal <jirazabal@cimne.upc.edu>
2021-03-25 08:34:59 +0100

    Minor

--- applications/DEMApplication/custom_constitutive/DEM_discontinuum_constitutive_law.cpp
+++ applications/DEMApplication/custom_constitutive/DEM_discontinuum_constitutive_law.cpp
@@ -55,7 +55,7 @@ namespace Kratos {
             KRATOS_WARNING("DEM")<<std::endl;
             KRATOS_WARNING("DEM")<<"WARNING: Variable FRICTION_DECAY should be present in the properties when using DEMDiscontinuumConstitutiveLaw. 500.0 value assigned by default."<<std::endl;
             KRATOS_WARNING("DEM")<<std::endl;
-            pProp->GetValue(FRICTION_DECAY) = 0.0;
+            pProp->GetValue(FRICTION_DECAY) = 500.0;
         }
         if(!pProp->Has(YOUNG_MODULUS)) {
             KRATOS_WARNING("DEM")<<std::endl;
