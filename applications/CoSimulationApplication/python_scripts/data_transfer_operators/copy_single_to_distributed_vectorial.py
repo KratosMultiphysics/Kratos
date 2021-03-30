f53460f27dec0f5675696e3185218d34f5eaf6fe
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-03-30 17:11:11 +0200

    Complete CopySingleToDistributedVectorial

--- applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_distributed_vectorial.py
+++ applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_distributed_vectorial.py
@@ -6,31 +6,37 @@ def Create(settings):
 
 class CopySingleToDistributedVectorial(CoSimulationDataTransferOperator):
     """DataTransferOperator to take one single value and set it to all values on another interface.
-    Used e.g. for FSI with SDof, where the SDof has one value and the fluid interface has many
+    It is a version of CopySingleToDistributed which does the same but for vectorial variables.
+    Used e.g. for FSI with RigidBody, where the RigidBody has one value and the fluid interface has many.
     """
     def _ExecuteTransferData(self, from_solver_data, to_solver_data, transfer_options):
         to_solver_values = to_solver_data.GetData()
         data_value = from_solver_data.GetData()
 
-        if not data_value.size == 1:
-            raise Exception('Interface data "{}" of solver "{}" requires to be of size 1, got: {}'.format(from_solver_data.name, from_solver_data.solver_name, data_value.size))
-
-        to_solver_values.fill(data_value[0])
+        for to_index in range(len(to_solver_values)):
+            from_index = to_index % from_solver_data.dimension
+            to_solver_values[to_index] = data_value[from_index]
 
         # the order is IMPORTANT here!
         if "swap_sign" in transfer_options.GetStringArray():
             to_solver_values *= (-1)
         if "distribute_values" in transfer_options.GetStringArray():
             data_comm = from_solver_data.model_part.GetCommunicator().GetDataCommunicator()
-            to_solver_values /= data_comm.SumAll(to_solver_data.Size())
+            to_solver_values /= data_comm.SumAll(to_solver_data.Size()) * to_solver_data.dimension
         if "add_values" in transfer_options.GetStringArray():
             to_solver_values += to_solver_data.GetData()
 
         to_solver_data.SetData(to_solver_values)
 
     def _Check(self, from_solver_data, to_solver_data):
-        if not to_solver_data.is_scalar_variable:
-            raise Exception('Variable of interface data "{}" of solver "{}" has to be a scalar!'.format(to_solver_data.name, to_solver_data.solver_name))
+        if to_solver_data.dimension != from_solver_data.dimension:
+            msg = 'Interface data "' + str(to_solver_data.name) + '" of solver "' + str(to_solver_data.solver_name) + '" requires to be '
+            msg += 'the same size as interface data "' + str(from_solver_data.name) + '" of solver "' + str(from_solver_data.solver_name)
+            msg += '", got ' + str(to_solver_data.dimension) + ' and ' + str(from_solver_data.dimension) + ' respectively.'
+            raise Exception(msg)
+        if from_solver_data.Size() != from_solver_data.dimension:
+            msg = 'Variable of interface data "{}" of solver "{}" has to be as large as its dimension.'
+            raise Exception(msg.format(from_solver_data.name, from_solver_data.solver_name))
 
     @classmethod
     def _GetListAvailableTransferOptions(cls):
