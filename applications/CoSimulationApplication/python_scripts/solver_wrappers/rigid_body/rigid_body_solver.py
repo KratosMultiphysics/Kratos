f4507bc2b2df0d5ee6f51070c7db4073376bbf5c
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-03-30 17:12:01 +0200

    Convert solution process to handle 3 displacements

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/rigid_body/rigid_body_solver.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/rigid_body/rigid_body_solver.py
@@ -32,6 +32,7 @@ class RigidBodySolver(object):
         self.active_dofs = self._CheckActiveDofs(parameters)
 
         # Fill with defaults and check that mandatory fields are given
+        self._CheckMandatoryInputParameters(parameters)
         dof_params, sol_params = self._ValidateAndAssignRigidBodySolverDefaults(parameters)
         
         # Safe all the filled data in their respective class variables
@@ -76,7 +77,6 @@ class RigidBodySolver(object):
         self.initial_acceleration = {}
 
         for dof in self.active_dofs:
-            print(dof_params)
             self.mass[dof] = dof_params[dof]['system_parameters']['mass'].GetDouble()
             self.stiffness[dof] = dof_params[dof]['system_parameters']['stiffness'].GetDouble()
             self.damping[dof] = dof_params[dof]['system_parameters']['damping'].GetDouble()
@@ -167,8 +167,8 @@ class RigidBodySolver(object):
         data_comm = KratosMultiphysics.DataCommunicator.GetDefault()
         if data_comm.Rank()==0:
             if self.write_output_file:
-                reaction = self.CalculateReaction()
                 for dof in self.active_dofs:
+                    reaction = self.CalculateReaction(dof)
                     with open(self.output_file_name[dof], "a") as results_rigid_body:
                         #outputs results
                         #x and dx contain: [displacement, velocity, acceleration]
@@ -182,94 +182,111 @@ class RigidBodySolver(object):
                                                 str(self.dx[dof][0] - self.dx_f[dof][0]) + " " +
                                                 str(self.dx[dof][1] - self.dx_f[dof][1]) + " " +
                                                 str(self.dx[dof][2] - self.dx_f[dof][2]) + " " +
-                                                str(reaction[dof]) + "\n")
+                                                str(reaction) + "\n")
 
 
     def AdvanceInTime(self, current_time):
-        # similar to the Kratos CloneTimeStep function
-        # advances values along the buffer axis (so rolling columns) using numpy's roll
-        self.x = np.roll(self.x,1,axis=1)
-        self.x_f = np.roll(self.x_f,1,axis=1)
-        # overwriting at the buffer_idx=0 the newest values
-        buffer_idx = 0
-        self.x[:,buffer_idx] = self.dx
-        self.x_f[:,buffer_idx] = self.dx_f
+        for dof in self.active_dofs:
+            # similar to the Kratos CloneTimeStep function
+            # advances values along the buffer axis (so rolling columns) using numpy's roll
+            self.x[dof] = np.roll(self.x[dof],1,axis=1)
+            self.x_f[dof] = np.roll(self.x_f[dof],1,axis=1)
+            # overwriting at the buffer_idx=0 the newest values
+            buffer_idx = 0
+            self.x[dof][:,buffer_idx] = self.dx[dof]
+            self.x_f[dof][:,buffer_idx] = self.dx_f[dof]
 
         self.time = current_time + self.delta_t
         return self.time
 
-    def CalculateEquivalentForceFromRootPointExcitation(self, d_f):
-        #d_f = self.root_point_displacement
-        v_f = self.x_f[1,0] + self.delta_t * (self.gamma * d_f + (1-self.gamma) * self.x_f[2,0])
-        a_f = 1/(self.delta_t**2 * self.beta) * (d_f - self.x_f[0,1])\
-            - 1/(self.delta_t * self.beta) * self.x_f[1,0]\
-            + (1-1/(2*self.beta)) * self.x_f[2,0]
-        self.dx_f = np.array([d_f, v_f, a_f])
-        b_f = np.array([0.0, 0.0, d_f * self.stiffness + v_f * self.damping])
+    def CalculateEquivalentForceFromRootPointExcitation(self, d_f_excitation):
+        b_f = {}
+        for dof in self.active_dofs:
+            d_f = d_f_excitation[dof] + self.root_point_displacement[dof]
+            v_f = self.x_f[dof][1,0] + self.delta_t * (self.gamma * d_f + (1-self.gamma) * self.x_f[dof][2,0])
+            a_f = 1/(self.delta_t**2 * self.beta) * (d_f - self.x_f[dof][0,1])\
+                - 1/(self.delta_t * self.beta) * self.x_f[dof][1,0]\
+                + (1-1/(2*self.beta)) * self.x_f[dof][2,0]
+            self.dx_f[dof] = np.array([d_f, v_f, a_f])
+            b_f[dof] = np.array([0.0, 0.0, d_f * self.stiffness[dof] + v_f * self.damping[dof]])
         return b_f
 
     def ApplyRootPointExcitation(self):
-        scope_vars = {'t' : self.time, 'omega': self.omega_root_point_displacement, 'A': self.amplitude_root_point_displacement}
-        return GenericCallFunction(self.excitation_function_root_point_displacement, scope_vars, check=False)
+        excitation = {}
+        for dof in self.active_dofs:
+            scope_vars = {'t' : self.time, 'omega': self.omega_root_point_displacement[dof], 'A': self.amplitude_root_point_displacement[dof]}
+            excitation[dof] = GenericCallFunction(self.excitation_function_root_point_displacement[dof], scope_vars, check=False)
+        return excitation
 
     def ApplyForceExcitation(self):
-        scope_vars = {'t' : self.time, 'omega': self.omega_force, 'A': self.amplitude_force}
-        return GenericCallFunction(self.excitation_function_force, scope_vars, check=False)
+        excitation = {}
+        for dof in self.active_dofs:
+            scope_vars = {'t' : self.time, 'omega': self.omega_force[dof], 'A': self.amplitude_force[dof]}
+            excitation[dof] = GenericCallFunction(self.excitation_function_force[dof], scope_vars, check=False)
+        return excitation
 
     def SolveSolutionStep(self):
-        b = self.RHS_matrix @ self.x[:,0]
         #external load
         excitation_load = self.ApplyForceExcitation()
-        self.load_vector[-1] += excitation_load
-        # print("external load= ", self.load_vector[-1])
-        b += self.load_vector
         #root point displacement
         d_f_excitation = self.ApplyRootPointExcitation()
-        d_f = d_f_excitation + self.root_point_displacement
-        b_f = self.CalculateEquivalentForceFromRootPointExcitation(d_f)
-        b += b_f
-        self.dx = np.linalg.solve(self.LHS, b)
-
-    def CalculateReaction(self, buffer_idx=0):
-        reaction = {}
+        #root point force
+        b_f = self.CalculateEquivalentForceFromRootPointExcitation(d_f_excitation)
         for dof in self.active_dofs:
-            reaction[dof] = self.damping[dof] * (self.dx[dof][1] - self.dx_f[dof][1]) \
-                        + self.stiffness[dof] * (self.dx[dof][0] - self.dx_f[dof][0])
+            RHS = self.RHS_matrix[dof] @ self.x[dof][:,0]
+            self.load_vector[dof][-1] += excitation_load[dof] # Why? Shouldn't it be just a "="?
+            # print("external load= ", self.load_vector[-1])
+            RHS += self.load_vector[dof]
+            RHS += b_f[dof]
+            self.dx[dof] = np.linalg.solve(self.LHS[dof], RHS)
+
+    def CalculateReaction(self, dof, buffer_idx=0):
+        reaction = self.damping[dof] * (self.dx[dof][1] - self.dx_f[dof][1]) \
+                + self.stiffness[dof] * (self.dx[dof][0] - self.dx_f[dof][0])
         return reaction
 
-    def CalculateSelfWeight(self):
-        self_weight = self.mass * self.modulus_self_weight
+    def CalculateSelfWeight(self, dof):
+        self_weight = self.mass[dof] * self.modulus_self_weight[dof]
         return self_weight
 
     def GetSolutionStepValue(self, identifier, buffer_idx=0):
-        if identifier == "DISPLACEMENT":
-            return self.x[:,buffer_idx][0]
-        elif identifier == "VELOCITY":
-            return self.x[:,buffer_idx][1]
-        elif identifier == "ACCELERATION":
-            return self.x[:,buffer_idx][2]
-        elif identifier == "REACTION":
-            return self.CalculateReaction()
-        elif identifier == "VOLUME_ACCELERATION":
-            return self.CalculateSelfWeight()
-        else:
-            raise Exception("Identifier is unknown!")
+        # Shouldn't it be df?
+        output = []
+        for dof in self.available_dofs:
+            if dof in self.active_dofs:
+                if identifier == "DISPLACEMENT":
+                    output.append(self.x[dof][:,buffer_idx][0])
+                elif identifier == "VELOCITY":
+                    output.append(self.x[dof][:,buffer_idx][1])
+                elif identifier == "ACCELERATION":
+                    output.append(self.x[dof][:,buffer_idx][2])
+                elif identifier == "REACTION":
+                    output.append(self.CalculateReaction(dof))
+                elif identifier == "VOLUME_ACCELERATION":
+                    output.append(self.CalculateSelfWeight(dof))
+                else:
+                    raise Exception("Identifier is unknown!")
+            else:
+                output.append(0.0)
+        return output
 
-    def SetSolutionStepValue(self, identifier, value, buffer_idx=0):
-        if identifier == "DISPLACEMENT":
-            self.x[:,buffer_idx][0] = value
-        elif identifier == "VELOCITY":
-            self.x[:,buffer_idx][1] = value
-        elif identifier == "ACCELERATION":
-            self.x[:,buffer_idx][2] = value
-        elif identifier == "LOAD":
-            self.load_vector[-1] = 0.0
-            self.load_vector[-1] = value
-        elif identifier == "ROOT_POINT_DISPLACEMENT":
-            self.root_point_displacement = 0.0
-            self.root_point_displacement = value
-        else:
-            raise Exception("Identifier is unknown!")
+    def SetSolutionStepValue(self, identifier, values, buffer_idx=0):
+        for val_id, value in enumerate(values):
+            if self.available_dofs[val_id] in self.active_dofs:
+                if identifier == "DISPLACEMENT":
+                    self.x[self.available_dofs[val_id]][:,buffer_idx][0] = value
+                elif identifier == "VELOCITY":
+                    self.x[self.available_dofs[val_id]][:,buffer_idx][1] = value
+                elif identifier == "ACCELERATION":
+                    self.x[self.available_dofs[val_id]][:,buffer_idx][2] = value
+                elif identifier == "LOAD":
+                    self.load_vector[self.available_dofs[val_id]][-1] = 0.0
+                    self.load_vector[self.available_dofs[val_id]][-1] = value
+                elif identifier == "ROOT_POINT_DISPLACEMENT":
+                    self.root_point_displacement[self.available_dofs[val_id]] = 0.0
+                    self.root_point_displacement[self.available_dofs[val_id]] = value
+                else:
+                    raise Exception("Identifier is unknown!")
 
     def _CheckActiveDofs(self, parameters):
 
@@ -295,8 +312,6 @@ class RigidBodySolver(object):
         return active_dofs
 
     def _ValidateAndAssignRigidBodySolverDefaults(self, parameters):
-        
-        self._CheckMandatoryInputParameters(parameters)
 
         default_single_dof_parameters = KratosMultiphysics.Parameters('''{
             "dof": "displacement_x",
