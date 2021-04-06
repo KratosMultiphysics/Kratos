16dec206e0a73a518f340305b58bf1407cbbcbc5
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-04-06 22:45:13 +0200

    Debug

--- kratos/containers/pointer_hash_map_set.h
+++ kratos/containers/pointer_hash_map_set.h
@@ -362,6 +362,8 @@ public:
         // Sort list
         std::sort(index_list.begin(), index_list.end());
 
+        for(auto& r_map : mData) KRATOS_WATCH(r_map.first);
+
         // Empty data
         this->empty();
 
@@ -369,6 +371,8 @@ public:
         for (auto& r_key : index_list) {
             this->insert(copy_data[r_key]);
         }
+
+        for(auto& r_map : mData) KRATOS_WATCH(r_map.first);
     }
 
 
