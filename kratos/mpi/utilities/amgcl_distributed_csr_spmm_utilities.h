7e4dc979cdfea3937191f490d0dc6b6a3ef410f7
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2021-04-06 14:41:25 +0200

    Using format (IV)

--- kratos/mpi/utilities/amgcl_distributed_csr_spmm_utilities.h
+++ kratos/mpi/utilities/amgcl_distributed_csr_spmm_utilities.h
@@ -15,11 +15,15 @@
 #if !defined(KRATOS_CSR_SPMM_UTILITIES_H_INCLUDED)
 #define  KRATOS_CSR_SPMM_UTILITIES_H_INCLUDED
 
-#include "containers/csr_matrix.h"
-#include "mpi/utilities/amgcl_distributed_csr_conversion_utilities.h"
+// System includes
 
+// External includes
 #include <amgcl/backend/builtin.hpp>
 #include <amgcl/adapter/zero_copy.hpp>
+
+// Project includes
+#include "containers/csr_matrix.h"
+#include "mpi/utilities/amgcl_distributed_csr_conversion_utilities.h"
 
 namespace Kratos
 {
