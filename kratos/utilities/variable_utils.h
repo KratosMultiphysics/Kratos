257757a49ee3cbb5d523af775cc9a6c5082d6e70
Mate Kelemen <ge49pil@mytum.de>
2021-04-07 21:41:19 +0200

    move Array3Reduction to reduction utils

--- kratos/utilities/variable_utils.h
+++ kratos/utilities/variable_utils.h
@@ -1177,7 +1177,7 @@ public:
 
         const auto &r_communicator = rModelPart.GetCommunicator();
 
-        using ReductionType = typename std::conditional< std::is_scalar<TDataType>::value , SumReduction<double> , Array3Reduction >::type;
+        using ReductionType = typename std::conditional< std::is_scalar<TDataType>::value , SumReduction<double> , SumReduction<array_1d<double,3>> >::type;
 
         TDataType sum_value = block_for_each<ReductionType>(r_communicator.LocalMesh().Nodes(),[&](Node<3>& rNode){
             return rNode.GetSolutionStepValue(rVariable, BuffStep);
@@ -1387,33 +1387,6 @@ private:
     ///@}
     ///@name Member Variables
     ///@{
-
-    // TODO use SumReduction once it supports array3 (- Philipp)
-    class Array3Reduction
-    {
-    public:
-        typedef array_1d<double,3> value_type;
-        typedef array_1d<double,3> return_type;
-
-        return_type mValue = ZeroVector(3);
-
-        /// access to reduced value
-        return_type GetValue() const
-        {
-            return mValue;
-        }
-
-        void LocalReduce(const value_type& value)
-        {
-            mValue += value;
-        }
-
-        void ThreadSafeReduce(const Array3Reduction& rOther)
-        {
-            AtomicAdd(mValue, rOther.mValue);
-        }
-    };
-
 
     ///@}
     ///@name Private Operators
