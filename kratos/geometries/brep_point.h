52097a7081b9e14e5068e53023116030081ae68b
Tobias Teschemacher <tobias.teschemacher@tum.de>
2021-04-07 15:07:01 +1000

    change order of space checks.

--- kratos/geometries/brep_point.h
+++ kratos/geometries/brep_point.h
@@ -70,13 +70,13 @@ public:
         , mLocalCoordinates(LocalCoordinates)
         , mpBackgroundGeometry(pBackgroundGeometry)
     {
-        KRATOS_ERROR_IF(pBackgroundGeometry->LocalSpaceDimension() != this->LocalSpaceDimension())
-            << "Local space dimension of background geometry (" << pBackgroundGeometry->LocalSpaceDimension()
-            << ") does not coincide with LocalSpaceDimension of this geometry (" << this->LocalSpaceDimension() << ")."
-            << std::endl;
         KRATOS_ERROR_IF(pBackgroundGeometry->WorkingSpaceDimension() != this->WorkingSpaceDimension())
             << "Working space dimension of background geometry (" << pBackgroundGeometry->WorkingSpaceDimension()
             << ") does not coincide with WorkingSpaceDimension of this geometry (" << this->WorkingSpaceDimension() << ")."
+            << std::endl;
+        KRATOS_ERROR_IF(pBackgroundGeometry->LocalSpaceDimension() != this->LocalSpaceDimension())
+            << "Local space dimension of background geometry (" << pBackgroundGeometry->LocalSpaceDimension()
+            << ") does not coincide with LocalSpaceDimension of this geometry (" << this->LocalSpaceDimension() << ")."
             << std::endl;
     }
 
