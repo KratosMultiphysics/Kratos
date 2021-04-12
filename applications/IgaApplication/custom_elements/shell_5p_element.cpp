e8c2acd53827aa2be0260c6e1508d2835c9796c4
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2021-04-12 14:33:13 +0200

    modified CalculateKinematics

--- applications/IgaApplication/custom_elements/shell_5p_element.cpp
+++ applications/IgaApplication/custom_elements/shell_5p_element.cpp
@@ -179,15 +179,26 @@ namespace Kratos
         rVar.P = IdentityMatrix(3) - tdyadt;
         rVar.P *= invL_t;
 
-        rVar.Q1 = normwquadinv * (inner_prod(t, dtd1) * (3.0 * tdyadt - IdentityMatrix(3)) - outer_prod(dtd1, t) - outer_prod(t, dtd1));
-        rVar.Q2 = normwquadinv * (inner_prod(t, dtd2) * (3.0 * tdyadt - IdentityMatrix(3)) - outer_prod(dtd2, t) - outer_prod(t, dtd2));
-        rVar.S1 = normwquadinv * (rKin.transShear[0] * (3.0 * tdyadt - IdentityMatrix(3)) - outer_prod(a1, t) - outer_prod(t, a1));
-        rVar.S2 = normwquadinv * (rKin.transShear[1] * (3.0 * tdyadt - IdentityMatrix(3)) - outer_prod(a2, t) - outer_prod(t, a2));
+        const double txdtd1 = inner_prod(t, dtd1);
+        const double txdtd2 = inner_prod(t, dtd2);
+        const double a1xdtd1 = inner_prod(a1, dtd1);
+        const double a2xdtd2 = inner_prod(a2, dtd2);
+        const double a1xdtd2 = inner_prod(a1, dtd2);
+        const double a2xdtd1 = inner_prod(a2, dtd1);
+        const Matrix3d a1dyadt = outer_prod(a1, t);
+        const Matrix3d a2dyadt = outer_prod(a2, t);
+        const Matrix3d dtd1dyadt = outer_prod(dtd1, t);
+        const Matrix3d dtd2dyadt = outer_prod(dtd2, t);
 
-        rVar.Chi11 = normwcubinv * (3.0 * inner_prod(t, dtd1) * (outer_prod(a1, t) + 0.5 * rKin.transShear[0] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * inner_prod(a1, dtd1) * tdyadt + rKin.transShear[0] * outer_prod(dtd1, t)) - outer_prod(a1, dtd1) - inner_prod(a1, dtd1) * 0.5 * IdentityMatrix(3));
-        rVar.Chi21 = normwcubinv * (3.0 * inner_prod(t, dtd1) * (outer_prod(a2, t) + 0.5 * rKin.transShear[1] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * inner_prod(a2, dtd1) * tdyadt + rKin.transShear[1] * outer_prod(dtd1, t)) - outer_prod(a2, dtd1) - inner_prod(a2, dtd1) * 0.5 * IdentityMatrix(3));
-        rVar.Chi12 = normwcubinv * (3.0 * inner_prod(t, dtd2) * (outer_prod(a1, t) + 0.5 * rKin.transShear[0] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * inner_prod(a1, dtd2) * tdyadt + rKin.transShear[0] * outer_prod(dtd2, t)) - outer_prod(a1, dtd2) - inner_prod(a1, dtd2) * 0.5 * IdentityMatrix(3));
-        rVar.Chi22 = normwcubinv * (3.0 * inner_prod(t, dtd2) * (outer_prod(a2, t) + 0.5 * rKin.transShear[1] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * inner_prod(a2, dtd2) * tdyadt + rKin.transShear[1] * outer_prod(dtd2, t)) - outer_prod(a2, dtd2) - inner_prod(a2, dtd2) * 0.5 * IdentityMatrix(3));
+        rVar.Q1 = normwquadinv * (txdtd1 * (3.0 * tdyadt - IdentityMatrix(3)) - dtd1dyadt - trans(dtd1dyadt));
+        rVar.Q2 = normwquadinv * (txdtd2 * (3.0 * tdyadt - IdentityMatrix(3)) - dtd2dyadt - trans(dtd2dyadt));
+        rVar.S1 = normwquadinv * (rKin.transShear[0] * (3.0 * tdyadt - IdentityMatrix(3)) - a1dyadt - trans(a1dyadt));
+        rVar.S2 = normwquadinv * (rKin.transShear[1] * (3.0 * tdyadt - IdentityMatrix(3)) - a2dyadt - trans(a2dyadt));
+
+        rVar.Chi11 = normwcubinv * (3.0 * txdtd1 * (a1dyadt + 0.5 * rKin.transShear[0] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * a1xdtd1 * tdyadt + rKin.transShear[0] * dtd1dyadt) - outer_prod(a1, dtd1) - a1xdtd1 * 0.5 * IdentityMatrix(3));
+        rVar.Chi21 = normwcubinv * (3.0 * txdtd1 * (a2dyadt + 0.5 * rKin.transShear[1] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * a2xdtd1 * tdyadt + rKin.transShear[1] * dtd1dyadt) - outer_prod(a2, dtd1) - a2xdtd1 * 0.5 * IdentityMatrix(3));
+        rVar.Chi12 = normwcubinv * (3.0 * txdtd2 * (a1dyadt + 0.5 * rKin.transShear[0] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * a1xdtd2 * tdyadt + rKin.transShear[0] * dtd2dyadt) - outer_prod(a1, dtd2) - a1xdtd2 * 0.5 * IdentityMatrix(3));
+        rVar.Chi22 = normwcubinv * (3.0 * txdtd2 * (a2dyadt + 0.5 * rKin.transShear[1] * (IdentityMatrix(3) - 5.0 * tdyadt)) + 3.0 * (0.5 * a2xdtd2 * tdyadt + rKin.transShear[1] * dtd2dyadt) - outer_prod(a2, dtd2) - a2xdtd2 * 0.5 * IdentityMatrix(3));
 
         rVar.Chi11 = trans(rVar.Chi11) + rVar.Chi11;
         rVar.Chi21 = trans(rVar.Chi21) + rVar.Chi21;
