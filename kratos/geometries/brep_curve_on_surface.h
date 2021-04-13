c6d95879ef4d5e031ff607a6f7fbd36c87df6360
tteschemacher <tteschemacher@users.noreply.github.com>
2021-04-13 17:08:00 +1000

    ::max to the end

--- kratos/geometries/brep_curve_on_surface.h
+++ kratos/geometries/brep_curve_on_surface.h
@@ -74,8 +74,8 @@ public:
     typedef typename BaseType::CoordinatesArrayType CoordinatesArrayType;
     typedef typename BaseType::IntegrationPointsArrayType IntegrationPointsArrayType;
 
-    static constexpr IndexType SURFACE_INDEX = std::numeric_limits::max<IndexType>();
-    static constexpr IndexType CURVE_ON_SURFACE_INDEX = std::numeric_limits::max<IndexType>() - 2;
+    static constexpr IndexType SURFACE_INDEX = std::numeric_limits<IndexType>::max();
+    static constexpr IndexType CURVE_ON_SURFACE_INDEX = std::numeric_limits<IndexType>()::max - 2;
 
     ///@}
     ///@name Life Cycle
