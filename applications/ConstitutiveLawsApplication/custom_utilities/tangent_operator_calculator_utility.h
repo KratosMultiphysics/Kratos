69fea08a6ff625eab435ec9f1a8b8553e240fdec
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-04-14 12:01:26 +0200

    whitespaces

--- applications/ConstitutiveLawsApplication/custom_utilities/tangent_operator_calculator_utility.h
+++ applications/ConstitutiveLawsApplication/custom_utilities/tangent_operator_calculator_utility.h
@@ -146,7 +146,7 @@ public:
         Matrix& r_tangent_tensor = rValues.GetConstitutiveMatrix();
         r_tangent_tensor.clear();
         Matrix auxiliar_tensor = ZeroMatrix(num_components,num_components);
-      
+
         // Calculate the perturbation
         double pertubation = PerturbationThreshold;
         for (IndexType i_component = 0; i_component < num_components; ++i_component) {
@@ -154,7 +154,7 @@ public:
             CalculatePerturbation(unperturbed_strain_vector_gp, i_component, component_perturbation);
             pertubation = std::max(component_perturbation, pertubation);
         }
-      
+
         // We check that the perturbation has a threshold value of PerturbationThreshold
         if (ConsiderPertubationThreshold && pertubation < PerturbationThreshold) pertubation = PerturbationThreshold;
 
@@ -259,7 +259,7 @@ public:
         }
         // We check that the perturbation has a threshold value of PerturbationThreshold
         if (ConsiderPertubationThreshold && pertubation < PerturbationThreshold) pertubation = PerturbationThreshold;
-      
+
         // Loop over components of the strain
         Vector& r_perturbed_strain = rValues.GetStrainVector();
         Vector& r_perturbed_integrated_stress = rValues.GetStressVector();
