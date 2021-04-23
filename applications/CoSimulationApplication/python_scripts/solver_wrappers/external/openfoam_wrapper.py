54f25c7811bfce76cb43692b8b2c097e4adac898
ashishdarekar <ashish.darekar@tum.de>
2021-04-23 13:46:03 +0530

    Implementation of Initialize() function

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/external/openfoam_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/external/openfoam_wrapper.py
@@ -1,5 +1,7 @@
 # Importing the Kratos Library
+from sys import argv
 import KratosMultiphysics as KM
+from KratosMultiphysics.CoSimulationApplication import CoSimIO
 
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_solver_wrapper import CoSimulationSolverWrapper
@@ -11,8 +13,7 @@ def Create(settings, model, solver_name):
     return OpenFOAMWrapper(settings, model, solver_name)
 
 class OpenFOAMWrapper(CoSimulationSolverWrapper):
-    """This class is a generic wrapper for connecting external solvers
-    The import of meshes is done once in the beginning
+    """This class serves as wrapper for the CFD solver OpenFOAM
     """
     def __init__(self, settings, model, solver_name):
         super().__init__(settings, model, solver_name)
@@ -23,17 +24,66 @@ class OpenFOAMWrapper(CoSimulationSolverWrapper):
             "import_data"      : [ ]
         }""")
 
-        self.settings["solver_wrapper_settings"].ValidateAndAssignDefaults(settings_defaults)
-
+        #self.settings["solver_wrapper_settings"].ValidateAndAssignDefaults(settings_defaults)
         model_part_utilities.CreateModelPartsFromCouplingDataSettings(self.settings["data"], self.model, self.name)
         model_part_utilities.AllocateHistoricalVariablesFromCouplingDataSettings(self.settings["data"], self.model, self.name)
 
     def Initialize(self):
         super().Initialize()
 
+        # Check which IO is used
+        self.type_io_used =  self.project_parameters["openfoam_kratos_wrapper"]["io_settings"]["type"]
+
+        # Connect to OpenFOAM adapter
+        connection_settings = CoSimIO.InfoFromParameters(self.project_parameters["openfoam_kratos_wrapper"]["io_settings"])
+        info = CoSimIO.Connect(connection_settings)
+        self.connection_name = info.GetString("connection_name")
+        if info.GetInt("connection_status") != CoSimIO.ConnectionStatus.Connected:
+            raise Exception("Connecting failed!")
+
         for model_part_name in self.settings["solver_wrapper_settings"]["import_meshes"].GetStringArray():
             interface_config = { "model_part_name" : model_part_name }
             self.ImportCouplingInterface(interface_config)
+
+        self.communication_settings = self.co_sim_settings["communication_settings"]
+
+        # Importing meshes from OpenFOAM to CoSimulation
+        for model_part_name in self.communication_settings["export_meshes"].GetStringArray():
+            info = CoSimIO.Info()
+            info.SetString("connection_name", self.connection_name)
+            info.SetString("identifier", model_part_name.replace(".", "-"))
+
+            CoSimIO.ImportData(info, self.model[model_part_name])
+
+    def __InnerLoop(self):
+        # Import fields
+        for field_settings in self.communication_settings["import_fields"]:
+            identifier = field_settings["identifier"].GetString()
+            model_part_name = field_settings["model_part_name"].GetString()
+            model_part = self.model[model_part_name]
+            variable_name = field_settings["variable_name"].GetString()
+            variable = KM.KratosGlobals.GetVariable(variable_name)
+
+            info = CoSimIO.Info()
+            info.SetString("connection_name", self.connection_name)
+            info.SetString("identifier", identifier)
+            CoSimIO.ImportData(info, model_part, variable, CoSimIO.DataLocation.NodeHistorical)
+
+        self._GetSolver().SolveSolutionStep()
+
+        # Export fields
+        for field_settings in self.communication_settings["export_fields"]:
+            identifier = field_settings["identifier"].GetString()
+            model_part_name = field_settings["model_part_name"].GetString()
+            model_part = self.model[model_part_name]
+            variable_name = field_settings["variable_name"].GetString()
+            variable = KM.KratosGlobals.GetVariable(variable_name)
+
+            info = CoSimIO.Info()
+            info.SetString("connection_name", self.connection_name)
+            info.SetString("identifier", identifier)
+            CoSimIO.ExportData(info, model_part, variable, CoSimIO.DataLocation.NodeHistorical)
+
 
     def AdvanceInTime(self, current_time):
         return 0.0 # TODO find a better solution here... maybe get time from solver through IO
@@ -57,3 +107,13 @@ class OpenFOAMWrapper(CoSimulationSolverWrapper):
 
     def _GetIOType(self):
         return self.settings["io_settings"]["type"].GetString()
+
+    def Finalize(self):
+        super().Finalize()
+
+        # DisConnect from OpenFOAM adapter
+        disconnect_settings = CoSimIO.Info()
+        disconnect_settings.SetString("connection_name", self.connection_name)
+        info = CoSimIO.Disconnect(disconnect_settings)
+        if info.GetInt("connection_status") != CoSimIO.ConnectionStatus.Disconnected:
+            raise Exception("Disconnecting failed!")
