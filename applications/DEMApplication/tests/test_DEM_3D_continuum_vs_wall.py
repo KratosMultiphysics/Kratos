0fe5a68e846bdf426987cc17352b43b49ff43931
Miguel Angel Celigueta <maceli@cimne.upc.edu>
2021-04-30 21:20:06 +0200

    test finished

--- applications/DEMApplication/tests/test_DEM_3D_continuum_vs_wall.py
+++ applications/DEMApplication/tests/test_DEM_3D_continuum_vs_wall.py
@@ -15,55 +15,73 @@ this_working_dir_backup = os.getcwd()
 def GetFilePath(fileName):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), fileName)
 
-class DEM3D_ContinuumTestVsWallSolution(KratosMultiphysics.DEMApplication.DEM_analysis_stage.DEMAnalysisStage):
+class DEM3D_ContinuumTestVsWallSolution(KratosMultiphysics.DEMApplication.DEM_analysis_stage.DEMAnalysisStage, KratosUnittest.TestCase):
 
-    def __init__(self, model, project_parameters):
-        super().__init__(model, project_parameters)
-        self.step = 0
+    def InitializeSolutionStep(self):
+        super().InitializeSolutionStep()
+        self.SetParticlePosition()
 
     def FinalizeSolutionStep(self):
         super().FinalizeSolutionStep()
-        self.SetParticlePosition()
+        self.CheckForces()
 
     def SetParticlePosition(self):
-        for node in self.spheres_model_part.Nodes:
-            if self.step == 0:
-                node.X = 0.75
-                node.Y = 0.7
-                node.Z = 0.6
-            elif self.step == 1:
-                node.X = 0.75
-                node.Y = 0.7
-                node.Z = 0.8
-            elif self.step == 2:
-                node.X = 0.4
-                node.Y = 0.7
-                node.Z = 0.8
-            elif self.step == 3:
-                node.X = 0.3
-                node.Y = 0.7
-                node.Z = 0.8
-            elif self.step == 4:
-                node.X = 0.3
-                node.Y = 0.7
-                node.Z = 0.6
-            elif self.step == 5:
-                node.X = 0.3
-                node.Y = 0.7
-                node.Z = 0.3
-            elif self.step == 6:
-                node.X = 0.5
-                node.Y = 0.7
-                node.Z = 0.3
-            elif self.step == 7:
-                node.X = 0.75
-                node.Y = 0.7
-                node.Z = 0.6
+        node = self.spheres_model_part.GetNode(1)
+        node.Fix(KratosMultiphysics.VELOCITY_X)
+        node.Fix(KratosMultiphysics.VELOCITY_Y)
+        node.Fix(KratosMultiphysics.VELOCITY_Z)
+        node.SetSolutionStepValue(KratosMultiphysics.VELOCITY_X, 0.0)
+        node.SetSolutionStepValue(KratosMultiphysics.VELOCITY_Y, 0.0)
+        node.SetSolutionStepValue(KratosMultiphysics.VELOCITY_Z, 0.0)
+        step = self.spheres_model_part.ProcessInfo.GetValue(KratosMultiphysics.TIME_STEPS)
 
-            node.SetSolutionStepValue(KratosMultiphysics.VELOCITY_X, 0.0)
-            node.SetSolutionStepValue(KratosMultiphysics.VELOCITY_Y, 0.0)
-            node.SetSolutionStepValue(KratosMultiphysics.VELOCITY_Z, 0.0)
-        self.step = self.step + 1
+        if step == 2:
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Z, 0.2)
+        elif step == 3:
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_X, -0.35)
+        elif step == 4:
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_X, -0.55)
+        elif step == 5:
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Z, -0.3)
+        elif step == 6:
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_X, -0.25)
+        elif step == 7:
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_X, 0.0)
+            node.SetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Z, 0.0)
+
+
+        node.X = node.X0 + node.GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_X)
+        node.Y = node.Y0 + node.GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Y)
+        node.Z = node.Z0 + node.GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Z)
+
+    def CheckForces(self):
+        node = self.spheres_model_part.GetNode(1)
+        step = self.spheres_model_part.ProcessInfo.GetValue(KratosMultiphysics.TIME_STEPS)
+
+        if step == 0:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 0.0)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), 0.0)
+        elif step == 1:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 0.0)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), 0.0)
+        elif step == 2:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), -3065.5072065106638)
+        elif step == 3:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 0.0)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), -3065.5072065106638)
+        elif step == 4:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 3065.5072065106638)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), -3065.5072065106638)
+        elif step == 5:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 3065.5072065106638)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), 0.0)
+        elif step == 6:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 0.0)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), 0.0)
+        elif step == 7:
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_X), 0.0)
+            self.assertAlmostEqual(node.GetSolutionStepValue(DEM.CONTACT_FORCES_Z), 0.0)
+
 
     @classmethod
     def GetMainPath(self):
