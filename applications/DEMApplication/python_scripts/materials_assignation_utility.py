0dfbfe29ace36f47b2b85ff99d0f37fc261c57d3
Miguel Angel Celigueta <maceli@cimne.upc.edu>
2021-05-02 01:38:01 +0200

    several bugfixes, now it's working

--- applications/DEMApplication/python_scripts/materials_assignation_utility.py
+++ applications/DEMApplication/python_scripts/materials_assignation_utility.py
@@ -21,9 +21,8 @@ class MaterialsAssignationUtility:
                 self.spheres_model_part.AddProperties(Kratos.Properties(material_id))
 
             properties_of_model_part_with_this_id = self.spheres_model_part.GetProperties()[material_id]
-            properties = material["variables"]
+            properties = material["Variables"]
             properties_of_model_part_with_this_id[Kratos.PARTICLE_MATERIAL] = material_id
-            #self.SetInputMaterialPropertiesIntoModelPartProperties(properties, properties_of_model_part_with_this_id)
             self.read_materials_utility.AssignMaterialToProperty(material, properties_of_model_part_with_this_id)
 
             for material_relation in list_of_material_relations:
@@ -37,72 +36,17 @@ class MaterialsAssignationUtility:
                     subprops = Kratos.Properties(index_of_the_other_material)
 
                 if subprops:
-                    contact_properties = material_relation["variables"]
-                    self.SetInputMaterialRelationPropertiesIntoModelPartSubProperties(contact_properties, subprops)
+                    contact_properties = material_relation["Variables"]
+                    self.read_materials_utility.AssignMaterialToProperty(material_relation, subprops)
+
+                    discontinuum_constitutive_law_instance = globals().get(subprops[DEM_DISCONTINUUM_CONSTITUTIVE_LAW_NAME])()
+                    discontinuum_constitutive_law_instance.SetConstitutiveLawInProperties(subprops, True)
+
+                    if subprops.Has(DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME):
+                        continuum_constitutive_law_instance = globals().get(subprops[DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME])()
+                        continuum_constitutive_law_instance.SetConstitutiveLawInPropertiesWithParameters(subprops, material_relation, True)
+
                     properties_of_model_part_with_this_id.AddSubProperties(subprops)
-
-
-    def SetInputMaterialPropertiesIntoModelPartProperties(self, input_properties, mp_properties):
-
-        if input_properties.Has("PARTICLE_DENSITY"):
-            mp_properties[PARTICLE_DENSITY] = input_properties["PARTICLE_DENSITY"].GetDouble()
-
-        mp_properties[Kratos.YOUNG_MODULUS] = input_properties["YOUNG_MODULUS"].GetDouble()
-
-        mp_properties[Kratos.POISSON_RATIO] = input_properties["POISSON_RATIO"].GetDouble()
-
-        if input_properties.Has("COMPUTE_WEAR"):
-            mp_properties[COMPUTE_WEAR] = input_properties["COMPUTE_WEAR"].GetBool()
-        else:
-            mp_properties[COMPUTE_WEAR] = False
-
-        if input_properties.Has("BREAKABLE_CLUSTER"):
-            mp_properties[BREAKABLE_CLUSTER] = input_properties["BREAKABLE_CLUSTER"].GetBool()
-
-        if input_properties.Has("CLUSTER_FILE_NAME"):
-            mp_properties[CLUSTER_FILE_NAME] = input_properties["CLUSTER_FILE_NAME"].GetString()
-
-        if input_properties.Has("DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME"):
-            mp_properties[DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME] = input_properties["DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME"].GetString()
-
-    def SetInputMaterialRelationPropertiesIntoModelPartSubProperties(self, contact_properties, subprops):
-        subprops[COEFFICIENT_OF_RESTITUTION] = contact_properties["COEFFICIENT_OF_RESTITUTION"].GetDouble()
-        subprops[STATIC_FRICTION] = contact_properties["STATIC_FRICTION"].GetDouble()
-        subprops[DYNAMIC_FRICTION] = contact_properties["DYNAMIC_FRICTION"].GetDouble()
-        subprops[FRICTION_DECAY] = contact_properties["FRICTION_DECAY"].GetDouble()
-        subprops[ROLLING_FRICTION] = contact_properties["ROLLING_FRICTION"].GetDouble()
-        subprops[ROLLING_FRICTION_WITH_WALLS] = contact_properties["ROLLING_FRICTION_WITH_WALLS"].GetDouble()
-        if contact_properties.Has("SEVERITY_OF_WEAR"):
-            subprops[SEVERITY_OF_WEAR] = contact_properties["SEVERITY_OF_WEAR"].GetDouble()
-        if contact_properties.Has("IMPACT_WEAR_SEVERITY"):
-            subprops[IMPACT_WEAR_SEVERITY] = contact_properties["IMPACT_WEAR_SEVERITY"].GetDouble()
-        if contact_properties.Has("BRINELL_HARDNESS"):
-            subprops[BRINELL_HARDNESS] = contact_properties["BRINELL_HARDNESS"].GetDouble()
-        if contact_properties.Has("CONICAL_DAMAGE_CONTACT_RADIUS"):
-            subprops[CONICAL_DAMAGE_CONTACT_RADIUS] = contact_properties["CONICAL_DAMAGE_CONTACT_RADIUS"].GetDouble()
-        if contact_properties.Has("CONICAL_DAMAGE_MAX_STRESS"):
-            subprops[CONICAL_DAMAGE_MAX_STRESS] = contact_properties["CONICAL_DAMAGE_MAX_STRESS"].GetDouble()
-        if contact_properties.Has("CONICAL_DAMAGE_ALPHA"):
-            subprops[CONICAL_DAMAGE_ALPHA] = contact_properties["CONICAL_DAMAGE_ALPHA"].GetDouble()
-        if contact_properties.Has("CONICAL_DAMAGE_GAMMA"):
-            subprops[CONICAL_DAMAGE_GAMMA] = contact_properties["CONICAL_DAMAGE_GAMMA"].GetDouble()
-        if contact_properties.Has("LEVEL_OF_FOULING"):
-            subprops[LEVEL_OF_FOULING] = contact_properties["LEVEL_OF_FOULING"].GetDouble()
-        if contact_properties.Has("PARTICLE_COHESION"):
-            subprops[PARTICLE_COHESION] = contact_properties["PARTICLE_COHESION"].GetDouble()
-        if contact_properties.Has("INITIAL_COHESION"):
-            subprops[INITIAL_COHESION] = contact_properties["INITIAL_COHESION"].GetDouble()
-        if contact_properties.Has("AMOUNT_OF_COHESION_FROM_STRESS"):
-            subprops[AMOUNT_OF_COHESION_FROM_STRESS] = contact_properties["AMOUNT_OF_COHESION_FROM_STRESS"].GetDouble()
-
-        subprops[DEM_DISCONTINUUM_CONSTITUTIVE_LAW_NAME] = contact_properties["DEM_DISCONTINUUM_CONSTITUTIVE_LAW_NAME"].GetString()
-        discontinuum_constitutive_law_instance = globals().get(subprops[DEM_DISCONTINUUM_CONSTITUTIVE_LAW_NAME])()
-        discontinuum_constitutive_law_instance.SetConstitutiveLawInProperties(subprops, True)
-
-        if contact_properties.Has("continuum_contact_law_parameters"):
-            subprops[DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME] = contact_properties["continuum_contact_law_parameters"]["DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME"].GetString()
-            continuum_constitutive_law_instance = globals().get(subprops[DEM_CONTINUUM_CONSTITUTIVE_LAW_NAME])()
-            continuum_constitutive_law_instance.SetConstitutiveLawInPropertiesWithParameters(subprops, contact_properties["continuum_contact_law_parameters"], True)
 
 
     def AssignPropertiesToEntities(self):
