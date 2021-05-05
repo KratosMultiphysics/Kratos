0343a7f8d4438cc0cefd40b106b940de15b74206
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-05-05 17:00:30 +0200

    IO DisplacementLagrangeMultiplierResidualFrictionalContactCriteria

--- applications/ContactStructuralMechanicsApplication/custom_strategies/custom_convergencecriterias/displacement_lagrangemultiplier_residual_frictional_contact_criteria.h
+++ applications/ContactStructuralMechanicsApplication/custom_strategies/custom_convergencecriterias/displacement_lagrangemultiplier_residual_frictional_contact_criteria.h
@@ -635,16 +635,34 @@ public:
     }
 
     ///@}
-    ///@name Operations
-    ///@{
-
-    ///@}
     ///@name Acces
     ///@{
 
     ///@}
     ///@name Inquiry
     ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        return "DisplacementLagrangeMultiplierResidualFrictionalContactCriteria";
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override
+    {
+        rOStream << Info();
+    }
 
     ///@}
     ///@name Friends
