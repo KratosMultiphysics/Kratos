51e0dbbcc17bda22adf3de20dae2243c8d08ae49
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-05-05 16:55:24 +0200

    Clean up DisplacementLagrangeMultiplierMixedContactCriteria

--- applications/ContactStructuralMechanicsApplication/custom_strategies/custom_convergencecriterias/displacement_lagrangemultiplier_residual_contact_criteria.h
+++ applications/ContactStructuralMechanicsApplication/custom_strategies/custom_convergencecriterias/displacement_lagrangemultiplier_residual_contact_criteria.h
@@ -74,28 +74,53 @@ public:
     KRATOS_DEFINE_LOCAL_FLAG( ROTATION_DOF_IS_CONSIDERED );
     KRATOS_DEFINE_LOCAL_FLAG( INITIAL_RESIDUAL_IS_SET );
 
-    /// The base class definition (and it subclasses)
-    typedef ConvergenceCriteria< TSparseSpace, TDenseSpace > BaseType;
-    typedef typename BaseType::TDataType                    TDataType;
-    typedef typename BaseType::DofsArrayType            DofsArrayType;
-    typedef typename BaseType::TSystemMatrixType    TSystemMatrixType;
-    typedef typename BaseType::TSystemVectorType    TSystemVectorType;
+    /// The base class definition
+    typedef ConvergenceCriteria< TSparseSpace, TDenseSpace >                                    BaseType;
+
+    /// The definition of the current class
+    typedef DisplacementLagrangeMultiplierResidualContactCriteria< TSparseSpace, TDenseSpace > ClassType;
+
+    /// The dofs array type
+    typedef typename BaseType::DofsArrayType                                               DofsArrayType;
+
+    /// The sparse matrix type
+    typedef typename BaseType::TSystemMatrixType                                       TSystemMatrixType;
+
+    /// The dense vector type
+    typedef typename BaseType::TSystemVectorType                                       TSystemVectorType;
 
     /// The sparse space used
-    typedef TSparseSpace                              SparseSpaceType;
+    typedef TSparseSpace                                                                 SparseSpaceType;
 
-    /// The r_table stream definition TODO: Replace by logger
-    typedef TableStreamUtility::Pointer       TablePrinterPointerType;
+    /// The table stream definition TODO: Replace by logger
+    typedef TableStreamUtility::Pointer                                          TablePrinterPointerType;
 
     /// The index type definition
-    typedef std::size_t                                     IndexType;
-
-    /// The key type definition
-    typedef std::size_t                                       KeyType;
+    typedef std::size_t                                                                        IndexType;
 
     ///@}
     ///@name Life Cycle
     ///@{
+
+    /**
+     * @brief Default constructor.
+     */
+    explicit DisplacementLagrangeMultiplierResidualContactCriteria()
+        : BaseType()
+    {
+    }
+
+    /**
+     * @brief Default constructor. (with parameters)
+     * @param ThisParameters The configuration parameters
+     */
+    explicit DisplacementLagrangeMultiplierResidualContactCriteria(Kratos::Parameters ThisParameters)
+        : BaseType()
+    {
+        // Validate and assign defaults
+        ThisParameters = this->ValidateAndAssignParameters(ThisParameters, this->GetDefaultParameters());
+        this->AssignSettings(ThisParameters);
+    }
 
     /**
      * @brief Default constructor (parameters)
@@ -139,18 +164,6 @@ public:
         // The normal contact residual
         mLMRatioTolerance = LMRatioTolerance;
         mLMAbsTolerance = LMAbsTolerance;
-    }
-
-    /**
-     * @brief Default constructor (parameters)
-     * @param ThisParameters The configuration parameters
-     */
-    explicit DisplacementLagrangeMultiplierResidualContactCriteria( Parameters ThisParameters = Parameters(R"({})"))
-        : BaseType()
-    {
-        // Validate and assign defaults
-        ThisParameters = this->ValidateAndAssignParameters(ThisParameters, this->GetDefaultParameters());
-        this->AssignSettings(ThisParameters);
     }
 
     //* Copy constructor.
