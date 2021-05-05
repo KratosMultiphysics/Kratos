6dd4b8f9ac1fa0a866a0033c4023373575b906f1
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-05-05 17:00:38 +0200

    Refactor DisplacementLagrangeMultiplierResidualFrictionalContactCriteria

--- applications/ContactStructuralMechanicsApplication/custom_strategies/custom_convergencecriterias/displacement_lagrangemultiplier_residual_frictional_contact_criteria.h
+++ applications/ContactStructuralMechanicsApplication/custom_strategies/custom_convergencecriterias/displacement_lagrangemultiplier_residual_frictional_contact_criteria.h
@@ -80,24 +80,29 @@ public:
     KRATOS_DEFINE_LOCAL_FLAG( INITIAL_STICK_RESIDUAL_IS_SET );
     KRATOS_DEFINE_LOCAL_FLAG( INITIAL_SLIP_RESIDUAL_IS_SET );
 
-    /// The base class definition (and it subclasses)
-    typedef ConvergenceCriteria< TSparseSpace, TDenseSpace > BaseType;
-    typedef typename BaseType::TDataType                    TDataType;
-    typedef typename BaseType::DofsArrayType            DofsArrayType;
-    typedef typename BaseType::TSystemMatrixType    TSystemMatrixType;
-    typedef typename BaseType::TSystemVectorType    TSystemVectorType;
+    /// The base class definition
+    typedef ConvergenceCriteria< TSparseSpace, TDenseSpace >                                              BaseType;
+
+    /// The definition of the current class
+    typedef DisplacementLagrangeMultiplierResidualFrictionalContactCriteria< TSparseSpace, TDenseSpace > ClassType;
+
+    /// The dofs array type
+    typedef typename BaseType::DofsArrayType                                                         DofsArrayType;
+
+    /// The sparse matrix type
+    typedef typename BaseType::TSystemMatrixType                                                 TSystemMatrixType;
+
+    /// The dense vector type
+    typedef typename BaseType::TSystemVectorType                                                 TSystemVectorType;
 
     /// The sparse space used
-    typedef TSparseSpace                              SparseSpaceType;
+    typedef TSparseSpace                                                                           SparseSpaceType;
 
-    /// The r_table stream definition TODO: Replace by logger
-    typedef TableStreamUtility::Pointer       TablePrinterPointerType;
+    /// The table stream definition TODO: Replace by logger
+    typedef TableStreamUtility::Pointer                                                    TablePrinterPointerType;
 
     /// The index type definition
-    typedef std::size_t                                     IndexType;
-
-    /// The key type definition
-    typedef std::size_t                                       KeyType;
+    typedef std::size_t                                                                                  IndexType;
 
     /// Zero tolerance definition
     static constexpr double ZeroTolerance = std::numeric_limits<double>::epsilon();
@@ -105,6 +110,26 @@ public:
     ///@}
     ///@name Life Cycle
     ///@{
+
+   /**
+     * @brief Default constructor.
+     */
+    explicit DisplacementLagrangeMultiplierResidualFrictionalContactCriteria()
+        : BaseType()
+    {
+    }
+
+    /**
+     * @brief Default constructor. (with parameters)
+     * @param ThisParameters The configuration parameters
+     */
+    explicit DisplacementLagrangeMultiplierResidualFrictionalContactCriteria(Kratos::Parameters ThisParameters)
+        : BaseType()
+    {
+        // Validate and assign defaults
+        ThisParameters = this->ValidateAndAssignParameters(ThisParameters, this->GetDefaultParameters());
+        this->AssignSettings(ThisParameters);
+    }
 
     /**
      * @brief Default constructor
@@ -169,18 +194,6 @@ public:
         mNormalTangentRatio = NormalTangentRatio;
     }
 
-    /**
-     * @brief Default constructor (parameters)
-     * @param ThisParameters The configuration parameters
-     */
-    explicit DisplacementLagrangeMultiplierResidualFrictionalContactCriteria( Parameters ThisParameters = Parameters(R"({})"))
-        : BaseType()
-    {
-        // Validate and assign defaults
-        ThisParameters = this->ValidateAndAssignParameters(ThisParameters, this->GetDefaultParameters());
-        this->AssignSettings(ThisParameters);
-    }
-
     // Copy constructor.
     DisplacementLagrangeMultiplierResidualFrictionalContactCriteria( DisplacementLagrangeMultiplierResidualFrictionalContactCriteria const& rOther )
       :BaseType(rOther)
@@ -215,6 +228,19 @@ public:
     ///@}
     ///@name Operators
     ///@{
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief Create method
+     * @param ThisParameters The configuration parameters
+     */
+    typename BaseType::Pointer Create(Parameters ThisParameters) const override
+    {
+        return Kratos::make_shared<ClassType>(ThisParameters);
+    }
 
     /**
      * @brief Compute relative and absolute error.
