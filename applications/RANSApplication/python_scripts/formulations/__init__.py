df7c20a69dfd8f9ea5b3ca4c25a2103800686fca
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-05-06 13:44:52 +0200

    added turbulence models

--- applications/RANSApplication/python_scripts/formulations/__init__.py
+++ applications/RANSApplication/python_scripts/formulations/__init__.py
@@ -9,20 +9,24 @@ from KratosMultiphysics.RANSApplication.formulations.fractional_step.fractional_
 
 # turbulence model formulations
 ### k-epsilon formulations
+from KratosMultiphysics.RANSApplication.formulations.turbulence_models.k_epsilon_rans_formulation import KEpsilonRansFormulation
 from KratosMultiphysics.RANSApplication.formulations.monolithic_vms.monolithic_k_epsilon_rans_formulation import MonolithicKEpsilonRansFormulation
 from KratosMultiphysics.RANSApplication.formulations.fractional_step.fractional_step_k_epsilon_rans_formulation import FractionalStepKEpsilonRansFormulation
 
 ### k-omega formulations
+from KratosMultiphysics.RANSApplication.formulations.turbulence_models.k_omega_rans_formulation import KOmegaRansFormulation
 from KratosMultiphysics.RANSApplication.formulations.monolithic_vms.monolithic_k_omega_rans_formulation import MonolithicKOmegaRansFormulation
 from KratosMultiphysics.RANSApplication.formulations.fractional_step.fractional_step_k_omega_rans_formulation import FractionalStepKOmegaRansFormulation
 
 ### k-omega-sst formulations
+from KratosMultiphysics.RANSApplication.formulations.turbulence_models.k_omega_sst_rans_formulation import KOmegaSSTRansFormulation
 from KratosMultiphysics.RANSApplication.formulations.monolithic_vms.monolithic_k_omega_sst_rans_formulation import MonolithicKOmegaSSTRansFormulation
 from KratosMultiphysics.RANSApplication.formulations.fractional_step.fractional_step_k_omega_sst_rans_formulation import FractionalStepKOmegaSSTRansFormulation
 
 def Factory(model_part, settings):
     formulation_name = settings["formulation_name"].GetString()
     formulations_list = [
+        # adding flow solvers including turbulence solvers
         ["incompressible_potential_flow", IncompressiblePotentialFlowRansFormulation],
         ["monolithic", MonolithicVelocityPressureRansFormulation],
         ["fractional_step", FractionalStepVelocityPressureRansFormulation],
@@ -31,7 +35,12 @@ def Factory(model_part, settings):
         ["monolithic_k_omega", MonolithicKOmegaRansFormulation],
         ["fractional_step_k_omega", FractionalStepKOmegaRansFormulation],
         ["monolithic_k_omega_sst", MonolithicKOmegaSSTRansFormulation],
-        ["fractional_step_k_omega_sst", FractionalStepKOmegaSSTRansFormulation]
+        ["fractional_step_k_omega_sst", FractionalStepKOmegaSSTRansFormulation],
+
+        # adding only the turbulence models, flow should be given as a solved quantity for following formulations
+        ["k_epsilon", KEpsilonRansFormulation],
+        ["k_omega", KOmegaRansFormulation],
+        ["k_omega_sst", KOmegaSSTRansFormulation]
     ]
 
     formulation_names_list = [
