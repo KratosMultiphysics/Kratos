3eaa4bbbeae997f2538c421628b8d83524d70eed
marcnunezc <marcnc94@gmail.com>
2021-05-07 12:38:21 +0200

    Move geometrical responses to separate file

--- applications/CompressiblePotentialFlowApplication/python_scripts/potential_flow_response_function_factory.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/potential_flow_response_function_factory.py
@@ -1,5 +1,6 @@
 
 from KratosMultiphysics.CompressiblePotentialFlowApplication import potential_flow_response
+from KratosMultiphysics.CompressiblePotentialFlowApplication import airfoil_2d_responses
 
 def CreateResponseFunction(response_id, response_settings, model):
     response_type = response_settings["response_type"].GetString()
@@ -7,11 +8,11 @@ def CreateResponseFunction(response_id, response_settings, model):
     if response_type == "adjoint_lift_potential_jump":
         return potential_flow_response.AdjointResponseFunction(response_id, response_settings, model)
     elif response_type == "angle_of_attack":
-        return potential_flow_response.AngleOfAttackResponseFunction(response_id, response_settings, model)
+        return airfoil_2d_responses.AngleOfAttackResponseFunction(response_id, response_settings, model)
     elif response_type == "chord_length":
-        return potential_flow_response.ChordLengthResponseFunction(response_id, response_settings, model)
+        return airfoil_2d_responses.ChordLengthResponseFunction(response_id, response_settings, model)
     elif response_type == "perimeter":
-        return potential_flow_response.PerimeterResponseFunction(response_id, response_settings, model)
+        return airfoil_2d_responses.PerimeterResponseFunction(response_id, response_settings, model)
     else:
         raise NameError("The type of the following response function is not specified: "+ response_id +
                         ".\nAvailable types are: 'adjoint_lift_potential_jump', 'angle_of_attack', 'chord_length'," +
