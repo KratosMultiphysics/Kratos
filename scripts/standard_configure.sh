c97c5e0e61e314fcc1f1c80cceabe852155cbe29
SADPR <Aresdeparga1>
2021-05-10 14:42:50 +0200

    My configure file

--- scripts/standard_configure.sh
+++ scripts/standard_configure.sh
@@ -23,26 +23,41 @@ export CXX=g++
 export KRATOS_SOURCE="${KRATOS_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}"
 export KRATOS_BUILD="${KRATOS_SOURCE}/build"
 export KRATOS_APP_DIR="${KRATOS_SOURCE}/applications"
-# export KRATOS_INSTALL_PYTHON_USING_LINKS=ON
+export KRATOS_INSTALL_PYTHON_USING_LINKS=ON
 
 # Set basic configuration
 export KRATOS_BUILD_TYPE=${KRATOS_BUILD_TYPE:-"Release"}
-export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
+export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"python"}
 
 # Set applications to compile
 export KRATOS_APPLICATIONS=
-add_app ${KRATOS_APP_DIR}/LinearSolversApplication
+#add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+#add_app ${KRATOS_APP_DIR}/HDF5Application
+#add_app ${KRATOS_APP_DIR}/MetisApplication
+#add_app ${KRATOS_APP_DIR}/TrilinosApplication
+# add_app ${KRATOS_APP_DIR}/RANSApplication
 add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
-add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+add_app ${KRATOS_APP_DIR}/LinearSolversApplication
+#add_app ${KRATOS_APP_DIR}/CoSimulationApplication
+add_app ${KRATOS_APP_DIR}/RomApplication
+# add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication
+# add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication
 
 # Clean
 clear
-rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/cmake_install.cmake"
-rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeCache.txt"
-rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeFiles"
+rm -rf "${KRATOS_BUILD}/${CC}/${KRATOS_BUILD_TYPE}/cmake_install.cmake"
+rm -rf "${KRATOS_BUILD}/${CC}/${KRATOS_BUILD_TYPE}/CMakeCache.txt"
+rm -rf "${KRATOS_BUILD}/${CC}/${KRATOS_BUILD_TYPE}/CMakeFiles"
 
 # Configure
-cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" -DUSE_MPI=OFF -DUSE_EIGEN_MKL=OFF
+cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${CC}/${KRATOS_BUILD_TYPE}" \
+-DUSE_MPI=ON \
+-DCMAKE_INSTALL_PREFIX="${KRATOS_SOURCE}/bin/${CC}/${KRATOS_BUILD_TYPE}" \
+-DCMAKE_CXX_FLAGS="-Wno-deprecated -Wno-deprecated-declarations" \
+# -DUSE_COTIRE=ON \
 
 # Buid
-cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install -- -j$(nproc)
+cmake --build "${KRATOS_BUILD}/${CC}/${KRATOS_BUILD_TYPE}" --target install -j10
+
+# cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target all_unity -- -j8 && \
+# cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install/fast -- -j8
