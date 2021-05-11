6a49ae3e2946ea432fbf43ef17959d2eede2d58f
Marcelo Raschi <marcelo.raschi@gmail.com>
2021-05-11 20:54:32 +0200

    Fix resize vector STRAIN

--- applications/StructuralMechanicsApplication/custom_constitutive/elastic_isotropic_3d.cpp
+++ applications/StructuralMechanicsApplication/custom_constitutive/elastic_isotropic_3d.cpp
@@ -224,6 +224,9 @@ Vector& ElasticIsotropic3D::CalculateValue(
         r_flags.Set(ConstitutiveLaw::COMPUTE_STRESS, false);
 
         ElasticIsotropic3D::CalculateMaterialResponsePK2(rParameterValues);
+        if (rValue.size() != GetStrainSize()) {
+            rValue.resize(GetStrainSize());
+        }
         noalias(rValue) = rParameterValues.GetStrainVector();
 
         // Previous flags restored
