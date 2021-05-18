42ec254a21625fefb9637cf6ce8d6c8d81f6881b
Alejandro Cornejo <alejandro.cornejo23@gmail.com>
2021-05-18 15:46:00 +0200

    .

--- applications/StructuralMechanicsApplication/custom_elements/truss_element_linear_3D2N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/truss_element_linear_3D2N.cpp
@@ -233,7 +233,7 @@ void TrussElementLinear3D2N::UpdateInternalForces(BoundedVector<double,msLocalSi
     ConstitutiveLaw::VoigtSizeVectorType temp_stress;
     temp_strain.resize(1, false); temp_stress.resize(1, false);
     noalias(temp_strain) = ZeroVector(1); noalias(temp_stress) = ZeroVector(1);
-    
+
     temp_strain[0] = CalculateLinearStrain();
     Values.SetStrainVector(temp_strain);
     Values.SetStressVector(temp_stress);
@@ -258,8 +258,8 @@ void TrussElementLinear3D2N::FinalizeSolutionStep(const ProcessInfo& rCurrentPro
 {
     KRATOS_TRY;
     ConstitutiveLaw::Parameters Values(GetGeometry(),GetProperties(),rCurrentProcessInfo);
-    Vector temp_strain = ZeroVector(1);
-    Vector temp_stress = ZeroVector(1);
+    ConstitutiveLaw::VoigtSizeVectorType temp_strain; temp_strain.resize(1, false); noalias(temp_strain) = ZeroVector(1);
+    ConstitutiveLaw::VoigtSizeVectorType temp_stress; temp_stress.resize(1, false); noalias(temp_stress) = ZeroVector(1);
     temp_strain[0] = CalculateLinearStrain();
     Values.SetStrainVector(temp_strain);
     Values.SetStressVector(temp_stress);
