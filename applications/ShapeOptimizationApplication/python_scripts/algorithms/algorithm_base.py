f7a8833a58f4792bfce56768ffda061f88de516b
adityaghantasala <aditya.ghantasala@gmail.com>
2021-05-26 11:32:55 +0200

    add init to base algo

--- applications/ShapeOptimizationApplication/python_scripts/algorithms/algorithm_base.py
+++ applications/ShapeOptimizationApplication/python_scripts/algorithms/algorithm_base.py
@@ -11,9 +11,14 @@
 
 # Making KratosMultiphysics backward compatible with python 2.6 and 2.7
 from __future__ import print_function, absolute_import, division
+from KratosMultiphysics.ShapeOptimizationApplication import mapper_factory
 
 # ==============================================================================
 class OptimizationAlgorithm:
+    # --------------------------------------------------------------------------
+    def __init__(self, settings, communicator, model_part_controller):
+        pass
+
     # --------------------------------------------------------------------------
     def CheckApplicability( self ):
         raise RuntimeError("Algorithm base class is called. Please check your implementation of the function >> CheckApplicability << .")
@@ -30,4 +35,7 @@ class OptimizationAlgorithm:
     def FinalizeOptimizationLoop( self ):
         raise RuntimeError("Algorithm base class is called. Please check your implementation of the function >> FinalizeOptimizationLoop << .")
 
+    # --------------------------------------------------------------------------
+    def _CreateMappers(self, mappers_settings):
+        pass
 # ==============================================================================
