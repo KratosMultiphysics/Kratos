ec46752492cee2bb8ccf53aab7b1a950d4e15c28
adityaghantasala <aditya.ghantasala@gmail.com>
2021-05-26 13:09:47 +0200

    add entries and Create maappers to base algo

--- applications/ShapeOptimizationApplication/python_scripts/algorithms/algorithm_base.py
+++ applications/ShapeOptimizationApplication/python_scripts/algorithms/algorithm_base.py
@@ -16,8 +16,16 @@ from KratosMultiphysics.ShapeOptimizationApplication import mapper_factory
 # ==============================================================================
 class OptimizationAlgorithm:
     # --------------------------------------------------------------------------
-    def __init__(self, settings, communicator, model_part_controller):
-        pass
+    def __init__(self, settings, analyzer, communicator, model_part_controller):
+        self.model_part_controller = model_part_controller
+        self.optimization_settings = settings
+        self.communicator = communicator
+        self.analyzer = analyzer
+        self.algorithm_settings = settings["optimization_algorithm"]
+        self.design_surfaces = self.model_part_controller.GetDesignSurfaces()
+        self.mappers = {}
+
+        self.optimization_model_part = self.model_part_controller.GetOptimizationModelPart()
 
     # --------------------------------------------------------------------------
     def CheckApplicability( self ):
@@ -37,5 +45,14 @@ class OptimizationAlgorithm:
 
     # --------------------------------------------------------------------------
     def _CreateMappers(self, mappers_settings):
-        pass
+        for settings in mappers_settings:
+            design_surface_name = settings["design_surface_name"].GetString()
+            design_surface = self.design_surfaces[design_surface_name]
+            settings.RemoveValue("design_surface_name")
+
+            mapper = mapper_factory.CreateMapper(design_surface, design_surface, settings)
+            mapper.Initialize()
+            self.mappers[design_surface_name] = mapper
+
+
 # ==============================================================================
