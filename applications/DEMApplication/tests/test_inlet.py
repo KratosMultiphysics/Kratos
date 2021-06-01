ce099957a3abf90ebf9ce10a4e05b6a89496e66a
Guillermo Casas <gcasas@cimne.upc.edu>
2021-06-01 13:06:21 +0200

    Now running until convergence

--- applications/DEMApplication/tests/test_inlet.py
+++ applications/DEMApplication/tests/test_inlet.py
@@ -1,24 +1,20 @@
 import os
 import numpy as np
-import KratosMultiphysics
-from KratosMultiphysics import Logger
+import KratosMultiphysics as Kratos
+from Kratos import Logger
 Logger.GetDefaultOutput().SetSeverity(Logger.Severity.DETAIL)
 import KratosMultiphysics.DEMApplication as DEM
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.DEMApplication.DEM_analysis_stage
 
-import auxiliary_functions_for_tests
+import random_variable_tests_files.random_variable as rv
 
 this_working_dir_backup = os.getcwd()
 
 def GetFilePath(fileName):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), fileName)
 
-def Gaussian(x, mu=0, sigma=1):
-	return 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (x - mu)**2 / (2 * sigma**2))
-
-
-class TestDEMInlet(KratosMultiphysics.DEMApplication.DEM_analysis_stage.DEMAnalysisStage, KratosUnittest.TestCase):
+class TestDEMInlet(Kratos.DEMApplication.DEM_analysis_stage.DEMAnalysisStage, KratosUnittest.TestCase):
     debug_mode = True
 
     @staticmethod
@@ -28,8 +24,18 @@ class TestDEMInlet(KratosMultiphysics.DEMApplication.DEM_analysis_stage.DEMAnaly
 
     @staticmethod
     def Error(values, values_ref, interval_widths):
-        return np.sqrt(sum((values_ref - values)**2 * interval_widths) / sum(interval_widths))
+        return sum(abs((values_ref - values)) * interval_widths)
 
+    def __init__(self, model, DEM_parameters):
+        self.tolerance = 0.05
+        self.error = self.tolerance + 1
+        self.n_bins = 25 # for the histogram
+        self.mu = 0.001 / 2
+        self.sigma = 0.00025
+        self.centers = []
+        self.empirical_pdf = []
+        self.pdf_expected = []
+        super().__init__(model, DEM_parameters)
 
     def setUp(self):
         if TestDEM3DContact.debug_mode:
@@ -37,14 +43,18 @@ class TestDEMInlet(KratosMultiphysics.DEMApplication.DEM_analysis_stage.DEMAnaly
         else:
             Logger.GetDefaultOutput().SetSeverity(Logger.Severity.WARNING)
 
-    def InitializeSolutionStep(self):
-        self.n_bins = 25 # for the histogram
-        self.tolerance = 0.005
-        self.n_max_experiments = int(1e6) # should be enough with the self.tolerance=0.005
-        self.mu = 0.0001 / 2
-        self.sigma = 0.000025
+    def Initialize(self):
+        self.samples = dict()
+        return super().Initialize()
 
+    def InitializeSolutionStep(self):
         return super().InitializeSolutionStep()
+
+    def HasConverged(self):
+        return self.error < self.tolerance
+
+    def KeepAdvancingSolutionLoop(self):
+        return super().KeepAdvancingSolutionLoop() and not self.HasConverged()
 
     @classmethod
     def GetMainPath(self):
@@ -54,33 +64,41 @@ class TestDEMInlet(KratosMultiphysics.DEMApplication.DEM_analysis_stage.DEMAnaly
         return os.path.join(self.main_path, self.DEM_parameters["problem_name"].GetString())
 
     def FinalizeSolutionStep(self):
+        particle_nodes = [node for node in self.spheres_model_part.Nodes if node.IsNot(Kratos.BLOCKED)]
+
+        for node in particle_nodes:
+            self.samples[node.Id] = node.GetSolutionStepValue(Kratos.RADIUS)
+
+        if particle_nodes:
+            self.empirical_pdf, interval_boundaries = np.histogram(list(self.samples.values()), self.n_bins, density=True)
+            self.centers = 0.5 * (interval_boundaries[1:] + interval_boundaries[:-1])
+            interval_widths = interval_boundaries[1:] - interval_boundaries[:-1]
+            range_x = np.linspace(interval_boundaries[0], interval_boundaries[-1], num=self.n_bins)
+            parameters = Kratos.Parameters("""{}""")
+            breakpoints = Kratos.Vector(list(range_x))
+            pdf_values = Kratos.Vector(list(rv.GaussianPDF(np.array(range_x), mu=self.mu, sigma=self.sigma)))
+            parameters.AddVector("breakpoints", breakpoints)
+            parameters.AddVector("values", pdf_values)
+            expected_rv = rv.PiecewiseLinearRV(parameters)
+            self.pdf_expected = np.array([expected_rv.InterpolateHeight(x) for x in self.centers])
+
+            self.error = TestDEMInlet.Error(self.empirical_pdf, self.pdf_expected, interval_widths)
+            TestDEMInlet.Say('Probability error:', self.error)
         super().FinalizeSolutionStep()
-        pass
 
     def Finalize(self):
-        particle_nodes = [node for node in self.spheres_model_part.Nodes if node.IsNot(KratosMultiphysics.BLOCKED)]
-        self.sample = np.zeros(len(particle_nodes))
-
-        for i, node in enumerate(particle_nodes):
-            self.sample[i] = node.GetSolutionStepValue(KratosMultiphysics.RADIUS)
-        TestDEMInlet.Say(self.sample)
+        self.samples = np.array(list(self.samples.values()))
+        TestDEMInlet.Say('samples:', self.samples)
 
         self.procedures.RemoveFoldersWithResults(str(self.main_path), str(self.problem_name), '')
-        empirical_pdf, interval_boundaries = np.histogram(self.sample, self.n_bins, density=True)
-        centers = 0.5 * (interval_boundaries[1:] + interval_boundaries[:-1])
-        interval_widths = interval_boundaries[1:] - interval_boundaries[:-1]
-        range_x = np.linspace(interval_boundaries[0], interval_boundaries[-1], num=self.n_bins)
-        pdf_expected = np.array([Gaussian(x, self.mu, self.sigma) for x in centers])
 
-        error = TestDEMInlet.Error(empirical_pdf, pdf_expected, interval_widths)
-
-        TestDEMInlet.Say('Error = ' + '{:.4f}'.format(error), '( self.tolerance = ' + '{:.4f}'.format(self.tolerance), ')')
+        TestDEMInlet.Say('Error = ' + '{:.4f}'.format(self.error), '( self.tolerance = ' + '{:.4f}'.format(self.tolerance), ')')
 
         if TestDEMInlet.debug_mode:
             import matplotlib.pyplot as plt
-            plt.plot(centers, empirical_pdf)
-            plt.hist(self.sample, bins = self.n_bins, density=True, label='obtained (' + str(i) + ' samples)')
-            plt.plot(centers, pdf_expected, label='desired')
+            plt.plot(self.centers, self.empirical_pdf)
+            plt.hist(self.samples, bins = self.n_bins, density=True, label='obtained (' + str(len(self.samples)) + ' samples)')
+            plt.plot(self.centers, self.pdf_expected, label='desired')
             plt.legend()
             plt.savefig('piecewise_pdf.pdf')
         super().Finalize()
@@ -106,11 +124,11 @@ class TestDEM3DContact(KratosUnittest.TestCase):
     def test_piecewise_linear_inlet(self):
         path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "piecewise_linear_inlet_tests_files")
         parameters_file_name = os.path.join(path, "ProjectParametersDEM.json")
-        model = KratosMultiphysics.Model()
+        model = Kratos.Model()
 
         # Test parallel computation.
         with open(parameters_file_name,'r') as parameter_file:
-            project_parameters = KratosMultiphysics.Parameters(parameter_file.read())
+            project_parameters = Kratos.Parameters(parameter_file.read())
         TestDEMInlet(model, project_parameters).Run()
 
 
