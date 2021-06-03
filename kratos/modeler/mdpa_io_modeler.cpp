4c1c10ad29d02aef91899ea5b67e0d67fcb1dcaa
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-06-03 14:28:54 +0200

    initial commit

---
+++ kratos/modeler/mdpa_io_modeler.cpp
@@ -0,0 +1,52 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License 
+//                   Kratos default license: kratos/license.txt
+//
+
+
+// Project includes
+#include "mdpa_io_modeler.h"
+#include "includes/model_part_io.h"
+
+
+namespace Kratos
+{
+    MdpaIoModeler::MdpaIoModeler(Model& rModel, Parameters ModelerParameters)
+        : Modeler(rModel, ModelerParameters)
+        , mpModel(&rModel)
+    {
+        mParameters.ValidateAndAssignDefaults(GetDefaultParameters());
+    }
+
+    const Parameters MdpaIoModeler::GetDefaultParameters() const
+    {
+        const Parameters default_parameters = Parameters(R"({
+            "input_file_name"                            : "",
+            "model_part_name"                            : "model_part",
+            "skip_timer"                                 : true,
+            "ignore_variables_not_in_solution_step_data" : false
+        })");
+        return default_parameters;
+    }
+
+    void MdpaIoModeler::SetupGeometryModel()
+    {
+        auto input_file_name = mParameters["input_file_name"].GetString();
+        auto model_part_name = mParameters["model_part_name"].GetString();
+        ModelPart& model_part =
+            mpModel->HasModelPart(model_part_name) ?
+            mpModel->GetModelPart(model_part_name) :
+            mpModel->CreateModelPart(model_part_name);
+        Flags options = IO::READ;
+        if (mParameters["skip_timer"].GetBool())
+            options = IO::SKIP_TIMER | options;
+        if (mParameters["ignore_variables_not_in_solution_step_data"].GetBool())
+            options = IO::IGNORE_VARIABLES_ERROR | options;
+        ModelPartIO(input_file_name, options).ReadModelPart(model_part);
+    }
+}
---
+++ kratos/modeler/mdpa_io_modeler.h
@@ -0,0 +1,151 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//
+
+
+#ifndef KRATOS_MDPA_IO_MODELER_H_INCLUDED
+#define KRATOS_MDPA_IO_MODELER_H_INCLUDED
+
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "modeler.h"
+
+namespace Kratos
+{
+
+///@name Kratos Classes
+///@{
+
+/**
+ * @class MdpaIoModeler
+ * @ingroup KratosCore
+ * @brief Mdpa file reading
+ * @details Import a model part from an mdpa file
+ * @author Miguel Maso Sotomayor
+ */
+class KRATOS_API(KRATOS_CORE) MdpaIoModeler : public Modeler
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of Modeler
+    KRATOS_CLASS_POINTER_DEFINITION(MdpaIoModeler);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /**
+     * @brief Default constructor
+     */
+    MdpaIoModeler() : Modeler()
+    {
+    }
+
+    /**
+     * @brief Constructor with Model and Parameters
+     */
+    MdpaIoModeler(Model& rModel, Parameters ModelerParameters = Parameters());
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~MdpaIoModeler() = default;
+
+    /**
+     * @brief Creates the Modeler Pointer
+     */
+    Modeler::Pointer Create(Model& rModel, const Parameters ModelParameters) const override
+    {
+        return Kratos::make_shared<MdpaIoModeler>(rModel, ModelParameters);
+    }
+
+    ///@}
+    ///@name Stages
+    ///@{
+
+    void SetupGeometryModel() override;
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        return "MdpaIoModeler";
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream & rOStream) const override
+    {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream & rOStream) const override
+    {
+    }
+
+    ///@}
+
+private:
+    ///@name Member variables
+    ///@{
+
+    Model* mpModel = nullptr;
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    const Parameters GetDefaultParameters() const;
+
+    ///@}
+    ///@name Serializer
+    ///@{
+
+    friend class Serializer;
+
+    ///@}
+
+}; // Class MdpaIoModeler
+
+///@}
+///@name Input and output
+///@{
+
+/// input stream function
+inline std::istream& operator >> (
+    std::istream& rIStream,
+    MdpaIoModeler& rThis);
+
+/// output stream function
+inline std::ostream& operator << (
+    std::ostream& rOStream,
+    const MdpaIoModeler& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+
+///@}
+
+}  // namespace Kratos.
+
+#endif // KRATOS_MDPA_IO_MODELER_H_INCLUDED  defined
