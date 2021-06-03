567734c4c2feea039f069fc585475bf50d4caef9
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-06-03 16:39:50 +0200

    detabulate

--- kratos/modeler/mdpa_io_modeler.cpp
+++ kratos/modeler/mdpa_io_modeler.cpp
@@ -16,39 +16,41 @@
 
 namespace Kratos
 {
-    MdpaIoModeler::MdpaIoModeler(Model& rModel, Parameters ModelerParameters)
-        : Modeler(rModel, ModelerParameters)
-        , mpModel(&rModel)
-    {
-        mParameters.ValidateAndAssignDefaults(GetDefaultParameters());
-    }
 
-    const Parameters MdpaIoModeler::GetDefaultParameters() const
-    {
-        const Parameters default_parameters = Parameters(R"({
-            "echo_level"                                 : 0,
-            "input_file_name"                            : "",
-            "model_part_name"                            : "model_part",
-            "skip_timer"                                 : true,
-            "ignore_variables_not_in_solution_step_data" : false
-        })");
-        return default_parameters;
-    }
+MdpaIoModeler::MdpaIoModeler(Model& rModel, Parameters ModelerParameters)
+    : Modeler(rModel, ModelerParameters)
+    , mpModel(&rModel)
+{
+    mParameters.ValidateAndAssignDefaults(GetDefaultParameters());
+}
 
-    void MdpaIoModeler::SetupGeometryModel()
-    {
-        const auto input_file_name = mParameters["input_file_name"].GetString();
-        const auto model_part_name = mParameters["model_part_name"].GetString();
-        ModelPart& model_part =
-            mpModel->HasModelPart(model_part_name) ?
-            mpModel->GetModelPart(model_part_name) :
-            mpModel->CreateModelPart(model_part_name);
-        Flags options = IO::READ;
-        if (mParameters["skip_timer"].GetBool())
-            options = IO::SKIP_TIMER | options;
-        if (mParameters["ignore_variables_not_in_solution_step_data"].GetBool())
-            options = IO::IGNORE_VARIABLES_ERROR | options;
-        KRATOS_INFO_IF("::[MdpaIoModeler]::", mParameters["echo_level"].GetInt() > 0) << "Importing mdpa from: " << input_file_name << std::endl;
-        ModelPartIO(input_file_name, options).ReadModelPart(model_part);
-    }
+const Parameters MdpaIoModeler::GetDefaultParameters() const
+{
+    const Parameters default_parameters = Parameters(R"({
+        "echo_level"                                 : 0,
+        "input_file_name"                            : "",
+        "model_part_name"                            : "model_part",
+        "skip_timer"                                 : true,
+        "ignore_variables_not_in_solution_step_data" : false
+    })");
+    return default_parameters;
+}
+
+void MdpaIoModeler::SetupGeometryModel()
+{
+    const auto input_file_name = mParameters["input_file_name"].GetString();
+    const auto model_part_name = mParameters["model_part_name"].GetString();
+    ModelPart& model_part =
+        mpModel->HasModelPart(model_part_name) ?
+        mpModel->GetModelPart(model_part_name) :
+        mpModel->CreateModelPart(model_part_name);
+    Flags options = IO::READ;
+    if (mParameters["skip_timer"].GetBool())
+        options = IO::SKIP_TIMER | options;
+    if (mParameters["ignore_variables_not_in_solution_step_data"].GetBool())
+        options = IO::IGNORE_VARIABLES_ERROR | options;
+    KRATOS_INFO_IF("::[MdpaIoModeler]::", mParameters["echo_level"].GetInt() > 0) << "Importing mdpa from: " << input_file_name << std::endl;
+    ModelPartIO(input_file_name, options).ReadModelPart(model_part);
+}
+
 }
