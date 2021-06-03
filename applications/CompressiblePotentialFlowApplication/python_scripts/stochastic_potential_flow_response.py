f68ba8733778cde4b258def9d7a00ae8735e29bf
marcnunezc <marcnc94@gmail.com>
2021-06-03 10:11:40 +0200

    Remove unused vars and funcs and primal analysis

--- applications/CompressiblePotentialFlowApplication/python_scripts/stochastic_potential_flow_response.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/stochastic_potential_flow_response.py
@@ -12,7 +12,6 @@ from exaqute import get_value_from_remote
 import json
 
 def _GetModelPart(model, solver_settings):
-    #TODO can be removed once model is fully available
     model_part_name = solver_settings["model_part_name"].GetString()
     if not model.HasModelPart(model_part_name):
         model_part = model.CreateModelPart(model_part_name, 2)
@@ -48,26 +47,21 @@ class AdjointResponseFunction(ResponseFunctionInterface):
             primal_parameters.AddString("design_surface_sub_model_part_name", self.design_surface_sub_model_part_name)
         open(self.response_settings["primal_settings"].GetString(), 'w').write(primal_parameters.PrettyPrintJsonString())
 
-        self.primal_model_part = _GetModelPart(model, primal_parameters["solver_settings"])
-
-        self.primal_analysis = potential_flow_analysis.PotentialFlowAnalysis(model, primal_parameters)
+        # Store model
+        self.current_model_part = _GetModelPart(model, primal_parameters["solver_settings"])
 
         # Create the adjoint solver
         adjoint_parameters = _CheckParameters(self._GetAdjointParameters())
         adjoint_model = KratosMultiphysics.Model()
         self.adjoint_model_part = _GetModelPart(adjoint_model, adjoint_parameters["solver_settings"])
-
         self.adjoint_analysis = potential_flow_analysis.PotentialFlowAnalysis(adjoint_model, adjoint_parameters)
 
-        self.primal_state_variables = [KCPFApp.VELOCITY_POTENTIAL, KCPFApp.AUXILIARY_VELOCITY_POTENTIAL]
-
     def Initialize(self):
-        self.primal_analysis.Initialize()
         self.adjoint_analysis.Initialize()
 
     def InitializeSolutionStep(self):
-        self.primal_model_part.RemoveSubModelPart("fluid_computational_model_part")
-        KratosMultiphysics.ModelPartIO(self.auxiliary_mdpa_path, KratosMultiphysics.IO.WRITE | KratosMultiphysics.IO.MESH_ONLY).WriteModelPart( self.primal_model_part)
+        self.current_model_part.RemoveSubModelPart("fluid_computational_model_part")
+        KratosMultiphysics.ModelPartIO(self.auxiliary_mdpa_path, KratosMultiphysics.IO.WRITE | KratosMultiphysics.IO.MESH_ONLY).WriteModelPart( self.current_model_part)
 
         self._RunXMC()
 
@@ -245,6 +239,7 @@ class SimulationScenario(potential_flow_analysis.PotentialFlowAnalysis):
         self.response_function.InitializeSolutionStep()
         # synchronize the modelparts
         self.adjoint_analysis.RunSolutionLoop()
+        self.adjoint_analysis.Finalize()
 
     """
     function introducing the stochasticity in the right hand side
@@ -279,32 +274,6 @@ class SimulationScenario(potential_flow_analysis.PotentialFlowAnalysis):
         Logger.PrintInfo("StochasticAdjointResponse", "Total number of QoI:",len(qoi_list))
         Logger.PrintInfo("StochasticAdjointResponse", "Total number of shape:",len(qoi_list[1]))
         Logger.PrintInfo("StochasticAdjointResponse", "Total number of exampleshape:",qoi_list[1][0])
-        return qoi_list
-
-    """
-    function mapping the pressure field on reference model
-    input:  self: an instance of the class
-    """
-    def MappingAndEvaluateQuantityOfInterest(self):
-        Logger.PrintInfo("StochasticAdjointResponse",  "Start Mapping")
-        # map from current model part of interest to reference model part
-        mapping_parameters = KratosMultiphysics.Parameters("""{
-            "mapper_type": "nearest_element",
-            "echo_level" : 0
-            }""")
-        mapping_parameters.AddString("interface_sub_model_part_origin", self.design_surface_sub_model_part_name)
-        mapping_parameters.AddString("interface_submodel_part_destination", self.design_surface_sub_model_part_name)
-        mapper = KratosMultiphysics.MappingApplication.MapperFactory.CreateMapper(self._GetSolver().main_model_part,self.mapping_reference_model.GetModelPart("model"),mapping_parameters)
-        mapper.Map(KratosMultiphysics.PRESSURE_COEFFICIENT, \
-            KratosMultiphysics.PRESSURE_COEFFICIENT,        \
-            KratosMultiphysics.MappingApplication.Mapper.FROM_NON_HISTORICAL |     \
-            KratosMultiphysics.MappingApplication.Mapper.TO_NON_HISTORICAL)
-        mapper.Map(KratosMultiphysics.SHAPE_SENSITIVITY, KratosMultiphysics.SHAPE_SENSITIVITY)
-        Logger.PrintInfo("StochasticAdjointResponse", "End Mapping")
-        # evaluate qoi
-        Logger.PrintInfo("StochasticAdjointResponse", "Start evaluating QoI")
-        qoi_list = self.EvaluateQuantityOfInterest()
-        Logger.PrintInfo("StochasticAdjointResponse", "End evaluating QoI")
         return qoi_list
 
     def _SynchronizeAdjointFromPrimal(self):
