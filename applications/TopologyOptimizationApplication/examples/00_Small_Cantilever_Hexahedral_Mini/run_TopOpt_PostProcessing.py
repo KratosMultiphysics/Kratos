9a653841d02b7b8e9d105c0379400b8b5044eb71
Hofer <philipp96.hofer@tum.de>
2021-06-16 07:58:57 +0200

    change post processing

--- applications/TopologyOptimizationApplication/examples/00_Small_Cantilever_Hexahedral_Mini/run_TopOpt_PostProcessing.py
+++ applications/TopologyOptimizationApplication/examples/00_Small_Cantilever_Hexahedral_Mini/run_TopOpt_PostProcessing.py
@@ -5,13 +5,14 @@ print(timer.ctime())
 
 #import kratos core and applications
 #TODO remove import *!
-from KratosMultiphysics import *
+
+import KratosMultiphysics as km
 from KratosMultiphysics.StructuralMechanicsApplication import *
 from KratosMultiphysics.TopologyOptimizationApplication import *
 from KratosMultiphysics.LinearSolversApplication import *
 
 # For GID output
-from KratosMultiphysics.gid_output import GiDOutput
+from KratosMultiphysics.gid_output_process import GiDOutputProcess
 
 # GiD output settings
 nodal_results=[]
@@ -24,11 +25,11 @@ GiDWriteParticlesFlag = False
 GiDMultiFileFlag = "Single"
        
 # Read optimized model part from restart file
-current_model = Model()
+current_model = km.Model()
 optimized_model_part = current_model.CreateModelPart("optimized_model_part")
-optimized_model_part.AddNodalSolutionStepVariable(NORMAL)
-restart_file_name = "/home/philipp/opt/kratosDev/applications/TopologyOptimizationApplication/examples/01_Small_Cantilever_Hexahedra_Klein/Small_Cantilever_Restart_File_15"
-model_part_io = ModelPartIO(restart_file_name)
+optimized_model_part.AddNodalSolutionStepVariable(km.NORMAL)
+restart_file_name = "Small_Cantilever_Restart_File_28"
+model_part_io = km.ModelPartIO(restart_file_name)
 model_part_io.ReadModelPart(optimized_model_part)
 
 # Extract volume
@@ -37,32 +38,71 @@ extracted_volume_model_part = current_model.CreateModelPart("extracted_volume_mo
 TopologyExtractorUtilities().ExtractVolumeMesh(optimized_model_part, threshold, extracted_volume_model_part)
 
 # Write extracted volume
-gid_io = GiDOutput("extracted_volume_model_part", VolumeOutput, GiDPostMode, GiDMultiFileFlag, GiDWriteMeshFlag, GiDWriteConditionsFlag)
-gid_io.initialize_results(extracted_volume_model_part)
-gid_io.write_results(1, extracted_volume_model_part, nodal_results, gauss_points_results)
-gid_io.finalize_results()
+gid_io = GiDOutputProcess(extracted_volume_model_part, "extracted_volume_model_part", 
+                                    km.Parameters("""
+                                        {
+                                            "result_file_configuration" : {
+                                                "gidpost_flags": {
+                                                    "GiDPostMode": "GiD_PostBinary",
+                                                    "WriteDeformedMeshFlag": "WriteUndeformed",
+                                                    "WriteConditionsFlag": "WriteConditions",
+                                                    "MultiFileFlag": "SingleFile"
+                                                },
+                                                "nodal_results"       : ["X_PHYS"]
+                                            }
+                                        }
+                                        """)
+                                    )
+#gid_io.initialize_results(extracted_volume_model_part)
+#gid_io.write_results(1, extracted_volume_model_part, nodal_results, gauss_points_results)
+#gid_io.finalize_results()
+gid_io.ExecuteInitialize()
+#gid_io.ExecuteBeforeSolutionLoop()
+#gid_io.ExecuteInitializeSolutionStep()
+gid_io.PrintOutput()
+#gid_io.ExecuteFinalizeSolutionStep()
+gid_io.ExecuteFinalize()
 
 # Extract surface
 extracted_surface_model_part = current_model.CreateModelPart("extracted_surface_model_part")
-#TopologyExtractorUtilities().ExtractSurfaceMesh(extracted_volume_model_part, extracted_surface_model_part)
+TopologyExtractorUtilities().ExtractSurfaceMesh(extracted_volume_model_part, extracted_surface_model_part)
 
 # Write extracted surface
-gid_io_2 = GiDOutput("extracted_surface_model_part", VolumeOutput, GiDPostMode, GiDMultiFileFlag, GiDWriteMeshFlag, GiDWriteConditionsFlag)
-gid_io_2.initialize_results(extracted_surface_model_part)
-gid_io_2.write_results(1, extracted_surface_model_part, nodal_results, gauss_points_results)
-gid_io_2.finalize_results()
+#gid_io_2 = GiDOutputProcess(extracted_volume_model_part,"extracted_surface_model_part",
+ #                                   km.Parameters("""
+  #                                      {
+   #                                         "result_file_configuration" : {
+    #                                            "gidpost_flags": {
+     #                                               "GiDPostMode": "GiD_PostBinary",
+      #                                              "WriteDeformedMeshFlag": "WriteUndeformed",
+       #                                             "WriteConditionsFlag": "WriteConditions",
+        #                                            "MultiFileFlag": "SingleFile"
+         #                                       },
+          #                                      "nodal_results"       : ["X_PHYS"]
+           #                                 }
+            #                            }
+             #                           """)
+              #s                      )
+#gid_io_2.initialize_results(extracted_surface_model_part)
+#gid_io_2.write_results(1, extracted_surface_model_part, nodal_results, gauss_points_results)
+#gid_io_2.finalize_results()
+
+#gid_io.ExecuteInitialize()
+#gid_io.ExecuteBeforeSolutionLoop()
+#gid_io.ExecuteInitializeSolutionStep()
+#gid_io.PrintOutput()
+#gid_io.ExecuteFinalizeSolutionStep()
+#gid_io.ExecuteFinalize()
+
 
 # Smooth extracted surface
 smoothing_relaxation_factor = 1
 smoothing_iterations = 5
 #smoothed_surface_model_part = current_model.ModelPart("extracted_surface_model_part")
-TopologySmoothingUtilities().SmoothMesh(extracted_surface_model_part, smoothing_relaxation_factor, smoothing_iterations)
+#TopologySmoothingUtilities().SmoothMesh(extracted_surface_model_part, smoothing_relaxation_factor, smoothing_iterations)
 
 # Write smoothed mesh
-gid_io_3 = GiDOutput("smoothed_surface_model_part", VolumeOutput, GiDPostMode, GiDMultiFileFlag, GiDWriteMeshFlag, GiDWriteConditionsFlag)
-gid_io_3.initialize_results(extracted_surface_model_part)
-gid_io_3.write_results(1, extracted_surface_model_part, nodal_results, gauss_points_results)
-gid_io_3.finalize_results()
-
-# Write stl of extracted surface
-IOUtilities().WriteSurfaceAsSTLFile("smoothed_design_21.stl",extracted_surface_model_part)
+#gid_io_3 = GiDOutputProcess("smoothed_surface_model_part", VolumeOutput, GiDPostMode, GiDMultiFileFlag, GiDWriteMeshFlag, GiDWriteConditionsFlag)
+#gid_io_3.initialize_results(extracted_surface_model_part)
+#gid_io_3.write_results(1, extracted_surface_model_part, nodal_results, gauss_points_results)
+#gid_io_3.finalize_results()
