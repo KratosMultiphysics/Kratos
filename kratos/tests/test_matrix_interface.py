d24915a132e181037548c85c1eb580106d64aa52
ashishdarekar <ashish.darekar@tum.de>
2021-06-21 12:50:14 +0530

    Addition of test for creating Kratos.Matrix from py-list_of_list

--- kratos/tests/test_matrix_interface.py
+++ kratos/tests/test_matrix_interface.py
@@ -120,5 +120,35 @@ class TestMatrixInterface(KratosUnittest.TestCase):
             for j in range(A.Size2()):
                 self.assertEqual(A[i,j], B[j,i])
 
+    def test_list_of_list_construction(self):
+        A = KM.Matrix([[2, 2, 2, 2]])                       #List 1
+        self.assertEqual(4, len(A))
+        self.assertEqual(1, A.Size1())
+        self.assertEqual(4, A.Size2())
+        expected = KM.Matrix(1,4)
+        expected.fill(2)
+
+        for i in range(expected.Size1()):
+            for j in range(expected.Size2()):
+                self.assertEqual(A[i,j], 2)
+
+        l2 = [[1.2, 2, 3.6], [-4], []]       #List 2
+        B = KM.Matrix(l2)
+        self.assertEqual(9, len(B))
+        self.assertEqual(3, B.Size1())
+        self.assertEqual(3, B.Size2())
+        expected = KM.Matrix(3,3)
+        expected[0,0] = 1.2
+        expected[0,1] = 2
+        expected[0,2] = 3.6
+        expected[1,0] = -4
+        expected[1,1] = expected[1,2]  = 0
+        expected[2,0] = expected[2,1] = expected[2,2] = 0
+
+        for i in range(expected.Size1()):
+            for j in range(expected.Size2()):
+                self.assertEqual(B[i,j], expected[i,j])
+
+
 if __name__ == '__main__':
     KratosUnittest.main()
