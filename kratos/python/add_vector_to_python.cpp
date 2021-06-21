eab919fda978b1b15b114855dd7713194d91296d
Philipp Bucher <philipp.bucher@tum.de>
2021-06-21 09:36:18 +0200

    using implicit conversion

--- kratos/python/add_vector_to_python.cpp
+++ kratos/python/add_vector_to_python.cpp
@@ -216,9 +216,8 @@ namespace Python
                                     tmp[i] = py::cast<double>(input[i]);
                                 return tmp;
                                 }));
-        vector_binder.def(py::init([](const VectorSlice& input){
-                                Vector tmp(input);
-                                return tmp;
+        vector_binder.def(py::init([](const VectorSlice& input) -> Vector {
+                                return input;
                                 }));
         py::implicitly_convertible<py::list, Vector>();
         py::implicitly_convertible<array_1d<double,3>, Vector>();
@@ -259,4 +258,3 @@ namespace Python
 }  // namespace Python.
 
 } // Namespace Kratos
-
