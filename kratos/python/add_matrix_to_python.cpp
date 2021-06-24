35a2d3f5c09b8d74a2fc19dc685cc3b6be174480
ashishdarekar <ashish.darekar@tum.de>
2021-06-24 09:46:13 +0530

    Finding size of a list using pybind11::len() function

--- kratos/python/add_matrix_to_python.cpp
+++ kratos/python/add_matrix_to_python.cpp
@@ -92,13 +92,13 @@ namespace Python
 
         matrix_binder.def(py::init([](const py::list& input){
           std::size_t num_rows = input.size();
-          if( num_rows == 0 || ( (num_rows == 1) && ((py::cast<py::list>(input[0])).size() == 0) ) )
+          if( num_rows == 0 || ( (num_rows == 1) && (pybind11::len(input[0]) == 0) ) )
             return DenseMatrix<double>(0,0);
           else{
-            std::size_t num_cols = (py::cast<py::list>(input[0])).size();
+            std::size_t num_cols = pybind11::len(input[0]);
             DenseMatrix<double>matrix = DenseMatrix<double>(num_rows, num_cols);
             for(std::size_t i = 0; i < num_rows; i++){
-              KRATOS_ERROR_IF( (py::cast<py::list>(input[i])).size() != num_cols ) << "Wrong size of a row\n";
+              KRATOS_ERROR_IF( (pybind11::len(input[i])) != num_cols ) << "Wrong size of a row\n";
               for(std::size_t j = 0; j < num_cols; j++){
                   matrix(i,j) = py::cast<double>((py::cast<py::list>(input[i]))[j]);
               }
