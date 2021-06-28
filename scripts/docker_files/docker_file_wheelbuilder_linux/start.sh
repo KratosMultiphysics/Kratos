10be382f0756966dff9f8ee3c3c7da7630953b95
unknown <roigcarlo@gmail.com>
2021-06-28 11:36:14 +0200

    Restoring old options

--- scripts/docker_files/docker_file_wheelbuilder_linux/start.sh
+++ scripts/docker_files/docker_file_wheelbuilder_linux/start.sh
@@ -1,10 +1,27 @@
-#!/bin/bash
+ #!/bin/bash
+ 
+if [ -z "$1" ];
+then
+    echo staring build for branch master
+    BRANCH=master
+else
+    echo staring build for branch $1
+    BRANCH=$1
+fi
 
-BRANCH=ci/updating-wheel-builder
+## Currently does nothing, as the build script calls to -j$(nproc) 
+if [ -z "$2" ];
+ then
+    echo using 4 cpus
+    CPUS=4
+else
+    echo using $2 cpus
+    CPUS=$2
+fi
 
 cd /workspace/kratos
 git clone --depth 1 --single-branch -b $BRANCH https://github.com/KratosMultiphysics/Kratos.git
 
 cd /workspace/kratos/Kratos/scripts/wheels/linux/
 chmod +x build.sh
-./build.sh
+./build.sh $CPUS
