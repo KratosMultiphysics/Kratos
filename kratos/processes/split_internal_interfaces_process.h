9a35598b618191d2e412eba8caf2e34652af96d0
Miguel Angel Celigueta <maceli@cimne.upc.edu>
2021-07-09 01:04:57 +0200

    windows compilation and style

--- kratos/processes/split_internal_interfaces_process.h
+++ kratos/processes/split_internal_interfaces_process.h
@@ -52,9 +52,8 @@ namespace Kratos
  * @author Riccardo Rossi
  * @author Miguel Angel Celigueta
  */
-class KRATOS_API(KRATOS_CORE) SplitInternalInterfaceProcess
-    : public Process
-{
+class /*KRATOS_API(KRATOS_CORE)*/  SplitInternalInterfaceProcess : public Process {
+
 public:
     ///@name Type Definitions
     ///@{
@@ -80,30 +79,19 @@ public:
      * @param rModelPart The model part to be computed
      * @param DomainSize The size of the space, if the value is not provided will compute from the model part
      */
-    SplitInternalInterfaceProcess(Model& rModel,
-                                  Parameters rParameters
-                                 )
-        : Process(Flags()), mrModelPart(rModel.GetModelPart(rParameters["model_part_name"].GetString()))
-    {
+    SplitInternalInterfaceProcess(Model& rModel, Parameters rParameters):Process(Flags()), mrModelPart(rModel.GetModelPart(rParameters["model_part_name"].GetString())) {
         KRATOS_TRY
-
-
         const Parameters default_parameters = GetDefaultParameters();
         rParameters.ValidateAndAssignDefaults(default_parameters);
-
         mConditionName = rParameters["condition_name"].GetString();
-KRATOS_WATCH("constructed ----------------------------------------------------")
         KRATOS_CATCH("");
     }
 
     /// Destructor.
-    virtual ~SplitInternalInterfaceProcess() override
-    {
-    }
+    virtual ~SplitInternalInterfaceProcess() override { }
 
 
-    const Parameters GetDefaultParameters() const override
-    {
+    const Parameters GetDefaultParameters() const override {
         const Parameters default_parameters( R"(
         {
             "model_part_name" :"MODEL_PART_NAME",
@@ -121,39 +109,25 @@ KRATOS_WATCH("constructed ----------------------------------------------------")
     ///@name Operations
     ///@{
 
-    void ExecuteInitialize() override
-    {
+    void ExecuteInitialize() override {
         std::set< std::size_t> property_ids;
 
-
-
-
-        for(auto& rElem : mrModelPart.Elements())
-        {
+        for(auto& rElem : mrModelPart.Elements()) {
             property_ids.insert( rElem.GetProperties().Id() );
         }
 
         std::size_t max_id = 0;
         std::size_t min_id = 1e6;
-        for(auto id : property_ids)
-        {
+        for(auto id : property_ids) {
             max_id = std::max(id,max_id);
             min_id = std::min(id,min_id);
         }
 
-        for(std::size_t id=min_id; id<max_id; id++)
-        {
+        for(std::size_t id=min_id; id<max_id; id++) {
             std::cout << "processing property Id "  << id << std::endl;
             SplitBoundary(id, mrModelPart);
         }
-
-
     }
-
-
-
-
-
 
     ///@}
     ///@name Access
@@ -169,20 +143,17 @@ KRATOS_WATCH("constructed ----------------------------------------------------")
     ///@{
 
     /// Turn back information as a string.
-    std::string Info() const override
-    {
+    std::string Info() const override {
         return "SplitInternalInterfaceProcess";
     }
 
     /// Print information about this object.
-    void PrintInfo(std::ostream& rOStream) const override
-    {
+    void PrintInfo(std::ostream& rOStream) const override {
         rOStream << "SplitInternalInterfaceProcess";
     }
 
     /// Print object's data.
-    void PrintData(std::ostream& rOStream) const override
-    {
+    void PrintData(std::ostream& rOStream) const override {
     }
 
 
@@ -201,8 +172,7 @@ protected:
     ///@}
     ///@name Protected member Variables
     ///@{
-    void SplitBoundary(const std::size_t PropertyIdBeingProcessed, ModelPart& rModelPart)
-    {
+    void SplitBoundary(const std::size_t PropertyIdBeingProcessed, ModelPart& rModelPart) {
         std::size_t domain_size = rModelPart.ElementsBegin()->GetGeometry().WorkingSpaceDimension(); //TODO: this may not be a very good solution.
         FindElementalNeighboursProcess(rModelPart, domain_size).Execute();
 
@@ -210,20 +180,14 @@ protected:
         std::vector< Geometry<Node<3> > > interface_faces;
         std::vector<
         std::pair<
-        Geometry<Node<3>>::Pointer,
-                 Geometry<Node<3>>::Pointer
-                 >
-                 > neighbouring_elements;
+        Geometry<Node<3>>::Pointer, Geometry<Node<3>>::Pointer > > neighbouring_elements;
 
-        for(auto& rElem : mrModelPart.Elements())
-        {
+        for(auto& rElem : mrModelPart.Elements()) {
             const auto& neighb = rElem.GetValue(NEIGHBOUR_ELEMENTS);
 
-            for(unsigned int i=0; i<rElem.GetGeometry().size(); ++i)
-            {
+            for(unsigned int i=0; i<rElem.GetGeometry().size(); ++i) {
 
-                if(rElem.GetProperties().Id() == PropertyIdBeingProcessed && neighb[i].GetProperties().Id() > PropertyIdBeingProcessed)
-                {
+                if(rElem.GetProperties().Id() == PropertyIdBeingProcessed && neighb[i].GetProperties().Id() > PropertyIdBeingProcessed) {
                     auto boundary_entities = rElem.GetGeometry().GenerateBoundariesEntities();
                     interface_faces.push_back(boundary_entities[i]);
                     neighbouring_elements.push_back( std::make_pair(rElem.pGetGeometry()  ,neighb[i].pGetGeometry()  ) );
@@ -233,24 +197,22 @@ protected:
 
         //construct list of nodes on the interface
         std::set< std::size_t > ids_on_interface;
-        for(auto& geom : interface_faces)
-        {
+        for(auto& geom : interface_faces) {
             for(auto& rNode : geom)
                 ids_on_interface.insert(rNode.Id());
         }
         //create duplicated nodes list
         std::size_t max_node_id = 0;
-        if(mrModelPart.GetRootModelPart().Nodes().size() != 0)
+        if(mrModelPart.GetRootModelPart().Nodes().size() != 0) {
             max_node_id = (mrModelPart.GetRootModelPart().Nodes().end()-1)->Id() + 1;
+        }
 
         std::map<std::size_t, Node<3>::Pointer> new_nodes_map;
-        for(auto& id : ids_on_interface)
-        {
+        for(auto& id : ids_on_interface) {
             auto& rOrigNode = rModelPart.Nodes()[id];
             auto pNode = mrModelPart.CreateNewNode(max_node_id++, rOrigNode );
             auto& origin_dofs = rOrigNode.GetDofs();
-            for (auto it_dof = origin_dofs.begin(); it_dof != origin_dofs.end(); it_dof++)
-            {
+            for (auto it_dof = origin_dofs.begin(); it_dof != origin_dofs.end(); it_dof++) {
                 pNode->pAddDof(**it_dof);
             }
 
@@ -259,16 +221,15 @@ protected:
 
         //now change the nodes to make the split and generate the new conditions
         std::size_t max_cond_id = 0;
-        if(mrModelPart.GetRootModelPart().Conditions().size() != 0)
+        if(mrModelPart.GetRootModelPart().Conditions().size() != 0) {
             max_cond_id = (mrModelPart.GetRootModelPart().Conditions().end()-1)->Id() + 1;
+        }
 
         Properties::Pointer pInterfaceProp = mrModelPart.pGetProperties(1); //TODO: understand if the property 1 is what we want
-        for(std::size_t i=0; i<interface_faces.size(); ++i)
-        {
+        for(std::size_t i=0; i<interface_faces.size(); ++i) {
             //do the split
             auto& pgeom = neighbouring_elements[i].second;
-            for(std::size_t k=0; k<pgeom->size(); ++k)
-            {
+            for(std::size_t k=0; k<pgeom->size(); ++k) {
                 auto it = new_nodes_map.find((*pgeom)[k].Id());
                 if( it != new_nodes_map.end() )
                     (*pgeom)(i) = it->second;
@@ -282,12 +243,6 @@ protected:
 
             rModelPart.CreateNewCondition(mConditionName, max_cond_id++, prism_ids, pInterfaceProp ); //TODO: understand if the property 1 is what we want
         }
-
-
-for(auto& cond : mrModelPart.Elements())
-    std::cout << cond << std::endl;
-
-
     }
 
     ///@}
@@ -355,7 +310,7 @@ private:
     ///@{
 
     /// Assignment operator.
-    SplitInternalInterfaceProcess& operator=(SplitInternalInterfaceProcess const& rOther);
+    //SplitInternalInterfaceProcess& operator=(SplitInternalInterfaceProcess const& rOther);
 
     /// Copy constructor.
     //SplitInternalInterfaceProcess(SplitInternalInterfaceProcess const& rOther);
@@ -377,16 +332,12 @@ private:
 
 
 /// input stream function
-inline std::istream& operator >> (std::istream& rIStream,
-                                  SplitInternalInterfaceProcess& rThis)
-{
+inline std::istream& operator >> (std::istream& rIStream, SplitInternalInterfaceProcess& rThis) {
     return rIStream;
 }
 
 /// output stream function
-inline std::ostream& operator << (std::ostream& rOStream,
-                                  const SplitInternalInterfaceProcess& rThis)
-{
+inline std::ostream& operator << (std::ostream& rOStream, const SplitInternalInterfaceProcess& rThis) {
     rThis.PrintInfo(rOStream);
     rOStream << std::endl;
     rThis.PrintData(rOStream);
@@ -398,6 +349,6 @@ inline std::ostream& operator << (std::ostream& rOStream,
 
 }  // namespace Kratos.
 
-#endif // KRATOS_SPLIT_INTERNAL_INTERFACE_PROCESS_H_INCLUDED  defined 
+#endif // KRATOS_SPLIT_INTERNAL_INTERFACE_PROCESS_H_INCLUDED  defined
 
 
