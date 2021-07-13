32250d42018c0c7130954e22d7a1b8eb51b89b19
unknown <roigcarlo@gmail.com>
2021-07-13 16:16:30 +0200

    Temporaly disabling hdf5 app

--- scripts/wheels/linux/configure.sh
+++ scripts/wheels/linux/configure.sh
@@ -48,7 +48,7 @@ add_app ${KRATOS_APP_DIR}/CableNetApplication;
 add_app ${KRATOS_APP_DIR}/RANSApplication;
 add_app ${KRATOS_APP_DIR}/MappingApplication;
 add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
-add_app ${KRATOS_APP_DIR}/HDF5Application;
+# add_app ${KRATOS_APP_DIR}/HDF5Application;
 add_app ${KRATOS_APP_DIR}/IgaApplication;
 add_app ${KRATOS_APP_DIR}/ChimeraApplication;
 add_app ${KRATOS_APP_DIR}/MultilevelMonteCarloApplication;
