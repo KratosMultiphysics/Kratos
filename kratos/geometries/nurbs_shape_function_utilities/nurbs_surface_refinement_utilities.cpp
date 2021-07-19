e9ee5cf609b47a4009654d1da6df79790556e917
tteschemacher <tobias.teschemacher@tum.de>
2021-07-19 21:02:49 +0100

    add missing alpha *

--- kratos/geometries/nurbs_shape_function_utilities/nurbs_surface_refinement_utilities.cpp
+++ kratos/geometries/nurbs_shape_function_utilities/nurbs_surface_refinement_utilities.cpp
@@ -160,7 +160,7 @@ namespace Kratos {
                         IndexType cp_index_refined_after = NurbsUtilities::GetVectorIndexFromMatrixIndices(
                             nb_cp_u_refined, nb_cp_v, index - 1, m);
 
-                        const array_1d<double, 3> cp_coordinates = rPointsRefined[cp_index_refined_after] + (1.0 - alpha) * rPointsRefined[cp_index_refined_before];
+                        const array_1d<double, 3> cp_coordinates = alpha * rPointsRefined[cp_index_refined_after] + (1.0 - alpha) * rPointsRefined[cp_index_refined_before];
 
                         rPointsRefined(cp_index_refined_after) = Kratos::make_intrusive<NodeType>(0, cp_coordinates);
                         rWeightsRefined[cp_index_refined_after] = rWeightsRefined[cp_index_refined_after] * alpha + rWeightsRefined[cp_index_refined_before] * (1 - alpha);
@@ -315,7 +315,7 @@ namespace Kratos {
                         IndexType cp_index_refined_after = NurbsUtilities::GetVectorIndexFromMatrixIndices(
                             nb_cp_u, nb_cp_v_refined, m, index - 1);
 
-                        array_1d<double, 3> cp_coordinates = rPointsRefined[cp_index_refined_after] + (1.0 - alpha) * rPointsRefined[cp_index_refined_before];
+                        array_1d<double, 3> cp_coordinates = alpha * rPointsRefined[cp_index_refined_after] + (1.0 - alpha) * rPointsRefined[cp_index_refined_before];
 
                         rPointsRefined(cp_index_refined_after) = Kratos::make_intrusive<NodeType>(0, cp_coordinates);
                         rWeightsRefined[cp_index_refined_after] = rWeightsRefined[cp_index_refined_after] * alpha + rWeightsRefined[cp_index_refined_before] * (1 - alpha);
