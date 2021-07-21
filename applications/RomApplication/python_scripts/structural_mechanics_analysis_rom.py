91c9b47490957bf875fe102b68d6d8789a8dc0fc
Raul Bravo <rbravo555@gmail.com>
2021-07-21 19:03:15 +0200

    Substituting hard-coded scheme by the one used by the FOM

--- applications/RomApplication/python_scripts/structural_mechanics_analysis_rom.py
+++ applications/RomApplication/python_scripts/structural_mechanics_analysis_rom.py
@@ -55,7 +55,7 @@ class StructuralMechanicsAnalysisROM(StructuralMechanicsAnalysis):
                 counter+=1
         if self.hyper_reduction_element_selector != None:
             if self.hyper_reduction_element_selector.Name == "EmpiricalCubature":
-                self.ResidualUtilityObject = romapp.RomResidualsUtility(self._GetSolver().GetComputingModelPart(), self.project_parameters["solver_settings"]["rom_settings"], KratosMultiphysics.ResidualBasedIncrementalUpdateStaticScheme())
+                self.ResidualUtilityObject = romapp.RomResidualsUtility(self._GetSolver().GetComputingModelPart(), self.project_parameters["solver_settings"]["rom_settings"], self._GetSolver().get_solution_scheme())
 
     def FinalizeSolutionStep(self):
         super().FinalizeSolutionStep()
