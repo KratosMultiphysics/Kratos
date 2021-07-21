092d4ac8e5c205a6044cb998beae74b1217d369f
ge69vud <manuel.messmer@tum.de>
2021-07-21 11:10:06 +0200

    Rename test.

--- kratos/tests/cpp_tests/geometries/test_surface_in_nurbs_volume.cpp
+++ kratos/tests/cpp_tests/geometries/test_surface_in_nurbs_volume.cpp
@@ -21,6 +21,7 @@
 #include "geometries/surface_in_nurbs_volume_geometry.h"
 #include "geometries/quadrature_point_surface_in_volume_geometry.h"
 #include "geometries/triangle_3d_3.h"
+#include "geometries/quadrilateral_3d_4.h"
 #include "tests/cpp_tests/geometries/test_geometry.h"
 
 namespace Kratos {
@@ -79,27 +80,30 @@ namespace Testing {
     {
         auto cube = GenerateCubeNurbsVolume();
 
-        IntegrationPoint<3> integration_point;
-        integration_point[0] = 0.5;
-        integration_point[1] = 0.5;
-        integration_point[2] = 0.5;
-        integration_point.Weight() = 0.8;
+        // IntegrationPoint<3> integration_point;
+        // integration_point[0] = 0.5;
+        // integration_point[1] = 0.5;
+        // integration_point[2] = 0.5;
+        // integration_point.Weight() = 0.8;
 
-        std::vector<IntegrationPoint<3>> integration_points(1);
-        integration_points[0] = integration_point;
 
         PointerVector<Geometry<NodeType>> quad_geometries(1);
-
-        auto p_node_1 = Kratos::make_intrusive<NodeType>(0, 0.0, 0.0, 0.0);
+        auto p_node_1 = Kratos::make_intrusive<NodeType>(0, 0.0, 1.0, 0.0);
         auto p_node_2 = Kratos::make_intrusive<NodeType>(1, 1.0, 1.0, 0.0);
-        auto p_node_3 = Kratos::make_intrusive<NodeType>(2, 0.0, 1.0, 0.0);
-        NodeType node1(0, 0.2, 0.2, 0.2);
-        auto triangle = Kratos::make_shared<Triangle3D3<NodeType>>( p_node_1, p_node_2, p_node_3);
-        // Create quadrature point geometries in the background.
-        SurfaceInNurbsVolumeGeometry<3, PointerVector<NodeType>> surface_in_volume(cube, triangle);
-        //cube.CreateQuadraturePointGeometries(quad_geometries, 2, integration_points);
+        auto p_node_3 = Kratos::make_intrusive<NodeType>(2, 1.0, 1.0, 0.0);
+        auto p_node_4 = Kratos::make_intrusive<NodeType>(3, 0.0, 1.0, 1.0);
 
-        // KRATOS_CHECK_EQUAL(surface_in_volume.HasGeometryPart( GeometryType::BACKGROUND_GEOMETRY_INDEX ), 1);
+        auto triangle = Kratos::make_shared<Triangle3D3<NodeType>>( p_node_1, p_node_3, p_node_4);
+        //auto quad = Kratos::make_shared<Quadrilateral3D4<NodeType>>( p_node_1, p_node_2, p_node_3, p_node_4);
+        // Create quadrature point geometries in the background.
+
+        SurfaceInNurbsVolumeGeometry<3, PointerVector<NodeType>> surface_in_volume(cube, triangle);
+
+        std::vector<IntegrationPoint<3>> dummy_integration_points(1);
+
+        surface_in_volume.CreateQuadraturePointGeometries(quad_geometries, 2, dummy_integration_points);
+
+        KRATOS_CHECK_EQUAL(surface_in_volume.HasGeometryPart( GeometryType::BACKGROUND_GEOMETRY_INDEX ), 1);
         KRATOS_CHECK_NEAR(surface_in_volume.Area(), 0.5, 1e-10);
         std::vector<double> center_ref = {1.0/3.0, 2.0/3.0, 0.0};
         KRATOS_CHECK_VECTOR_NEAR(surface_in_volume.Center(), center_ref, 1e-10);
@@ -108,8 +112,15 @@ namespace Testing {
         test_point[1] = 1.0/3.0;
         test_point[2] = 0.0;
         // Check what should determinatn be??
-        KRATOS_WATCH( surface_in_volume.DeterminantOfJacobian(test_point) );
+        KRATOS_CHECK_EQUAL( surface_in_volume.LocalSpaceDimension(), 3 );
+        KRATOS_CHECK_EQUAL( quad_geometries[0].LocalSpaceDimension(), 3 );
 
+        KRATOS_WATCH( surface_in_volume.LocalSpaceDimension() );
+        KRATOS_WATCH( quad_geometries[0].LocalSpaceDimension() );
+        KRATOS_WATCH( quad_geometries[0].IntegrationPoints() );
+        KRATOS_WATCH( surface_in_volume.DeterminantOfJacobian(test_point) );
+        KRATOS_WATCH( quad_geometries[0].DeterminantOfJacobian(0, quad_geometries[0].GetDefaultIntegrationMethod() ) );
+        KRATOS_WATCH( quad_geometries[0].Normal(0,quad_geometries[0].GetDefaultIntegrationMethod()) );
 
         // Check everthing after double DeterminantOfJacobian(
 
