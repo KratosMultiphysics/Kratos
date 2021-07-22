399327464a7283ff86191fbd1a98e8ca52e9915c
Manuel <manuel.messmer@tum.de>
2021-07-22 12:15:56 +0200

    Update test.

--- kratos/tests/cpp_tests/geometries/test_surface_in_nurbs_volume.cpp
+++ kratos/tests/cpp_tests/geometries/test_surface_in_nurbs_volume.cpp
@@ -21,6 +21,7 @@
 #include "geometries/surface_in_nurbs_volume_geometry.h"
 #include "geometries/quadrature_point_surface_in_volume_geometry.h"
 #include "geometries/triangle_3d_3.h"
+#include "geometries/triangle_3d_6.h"
 #include "geometries/quadrilateral_3d_4.h"
 #include "tests/cpp_tests/geometries/test_geometry.h"
 
@@ -31,7 +32,7 @@ namespace Testing {
     typedef Geometry<NodeType> GeometryType;
     typedef typename GeometryType::CoordinatesArrayType CoordinatesArrayType;
 
-    Kratos::shared_ptr<NurbsVolumeGeometry<PointerVector<NodeType>>> GenerateIrregularCubeNurbsVolume() {
+    Kratos::shared_ptr<NurbsVolumeGeometry<PointerVector<NodeType>>> GenerateCubeNurbsVolume() {
         PointerVector<NodeType> points(18);
 
         std::vector<double> z_direction = {0.0, 2.0};
@@ -133,7 +134,7 @@ namespace Testing {
 
     KRATOS_TEST_CASE_IN_SUITE(SurfaceInVolumeGeometryTriangleInCubeTest, KratosCoreNurbsGeometriesFastSuite)
     {
-        auto p_cube = GenerateIrregularCubeNurbsVolume();
+        auto p_cube = GenerateCubeNurbsVolume();
 
         PointerVector<Geometry<NodeType>> quad_geometries(1);
         auto p_node_1 = Kratos::make_intrusive<NodeType>(0, 0.0, 0.0, 0.0);
@@ -184,9 +185,8 @@ namespace Testing {
         KRATOS_CHECK_VECTOR_NEAR(quad_geometries[0].Normal( 0, integration_method), normal_ref, 1e-10 );
 
         // Check integration
-        //double global_area_triangle = local_area_triangle * p_nurbs_volume->DeterminantOfJacobian(test_point);
-        double global_area_triangle = 0.5;
-        KRATOS_CHECK_NEAR( surface_in_volume.DeterminantOfJacobian(test_point), 2.0*global_area_triangle, 1e-10);
+        double global_area_triangle_ref = 0.5;
+        KRATOS_CHECK_NEAR( surface_in_volume.DeterminantOfJacobian(test_point), 2.0*global_area_triangle_ref, 1e-10);
         auto integration_points = quad_geometries[0].IntegrationPoints();
         KRATOS_CHECK_EQUAL(integration_points.size(), 1);
         double weight = integration_points[0].Weight();
@@ -195,9 +195,9 @@ namespace Testing {
         KRATOS_CHECK_NEAR( quad_geometries[0].DeterminantOfJacobian(0, integration_method )*weight, 0.5, 1e-10);
     }
 
-KRATOS_TEST_CASE_IN_SUITE(SurfaceInVolumeGeometrySquareInCubeTest, KratosCoreNurbsGeometriesFastSuite)
+    KRATOS_TEST_CASE_IN_SUITE(SurfaceInVolumeGeometrySquareInCubeTest, KratosCoreNurbsGeometriesFastSuite)
     {
-        auto p_cube = GenerateIrregularCubeNurbsVolume();
+        auto p_cube = GenerateCubeNurbsVolume();
 
         PointerVector<Geometry<NodeType>> quad_geometries(1);
         auto p_node_1 = Kratos::make_intrusive<NodeType>(0, 0.0, 0.0, 0.0);
@@ -205,13 +205,14 @@ KRATOS_TEST_CASE_IN_SUITE(SurfaceInVolumeGeometrySquareInCubeTest, KratosCoreNur
         auto p_node_3 = Kratos::make_intrusive<NodeType>(2, 0.5, 0.5, 1.0);
         auto p_node_4 = Kratos::make_intrusive<NodeType>(3, 0.0, 0.0, 1.0);
 
-        auto p_quad = Kratos::make_shared<Quadrilateral3D4<NodeType>>( p_node_1, p_node_2, p_node_3, p_node_4);
-
+        auto p_quad = Kratos::make_shared<Quadrilateral3D4<NodeType>>( p_node_1, p_node_2, p_node_3, p_node_4 );
 
         SurfaceInNurbsVolumeGeometry<3, PointerVector<NodeType>> surface_in_volume(p_cube, p_quad);
 
-        std::vector<IntegrationPoint<3>> dummy_integration_points(1);
-        surface_in_volume.CreateQuadraturePointGeometries(quad_geometries, 2, dummy_integration_points);
+        std::vector<IntegrationPoint<3>> integration_points_created;
+        surface_in_volume.CreateIntegrationPoints(integration_points_created);
+
+        surface_in_volume.CreateQuadraturePointGeometries(quad_geometries, 2, integration_points_created);
 
         double local_area_triangle =  surface_in_volume.Area();
         KRATOS_CHECK_NEAR( local_area_triangle, sqrt(0.5*0.5 + 0.5*0.5), 1e-10);
@@ -244,22 +245,101 @@ KRATOS_TEST_CASE_IN_SUITE(SurfaceInVolumeGeometrySquareInCubeTest, KratosCoreNur
         KRATOS_CHECK_VECTOR_NEAR(quad_geometries[0].Normal( 0, integration_method), normal_ref, 1e-10 );
 
         // Check integration
-        auto integration_points = quad_geometries[0].IntegrationPoints();
-        double global_area_triangle;
+        KRATOS_CHECK_EQUAL( quad_geometries.size(), 4);
+        double global_area_triangle = 0.0;
         for( SizeType i = 0; i < quad_geometries.size(); ++i ){
-            global_area_triangle += quad_geometries[i].DeterminantOfJacobian(i, integration_method ) *
+            global_area_triangle += quad_geometries[i].DeterminantOfJacobian(0, integration_method ) *
                 quad_geometries[i].IntegrationPoints()[0].Weight();
         }
 
-        double global_area_triangle_ref = local_area_triangle*surface_in_volume.DeterminantOfJacobian(test_point);
-        KRATOS_CHECK_NEAR( global_area_triangle_ref, std::sqrt(2)*2, 1e-10);
-
-        KRATOS_CHECK_EQUAL(integration_points.size(), 4);
-        double weight = integration_points[0].Weight();
-        KRATOS_CHECK_VECTOR_NEAR(integration_points[0].Coordinates(), local_center_ref, 1e-10);
-        KRATOS_CHECK_NEAR(weight, 0.5, 1e-10);
-        KRATOS_CHECK_NEAR( quad_geometries[0].DeterminantOfJacobian(0, integration_method )*weight, global_area_triangle, 1e-10);
+        KRATOS_CHECK_NEAR( global_area_triangle, std::sqrt(2.0)*2.0, 1e-10);
     }
 
+    KRATOS_TEST_CASE_IN_SUITE(SurfaceInVolumeGeometryTriangleInCubeTest_2, KratosCoreNurbsGeometriesFastSuite)
+    {
+        auto p_cube = GenerateCubeNurbsVolume();
+
+        PointerVector<NodeType> points_global_space;
+        points_global_space.push_back( Kratos::make_intrusive<NodeType>(0, 0.0, 0.0, 0.0) );
+        points_global_space.push_back( Kratos::make_intrusive<NodeType>(1, 1.0, 0.0, 0.0) );
+        points_global_space.push_back( Kratos::make_intrusive<NodeType>(2, 2.0, 0.0, 1.0) );
+        points_global_space.push_back( Kratos::make_intrusive<NodeType>(3, 1.0, 0.5, 1.5) );
+        points_global_space.push_back( Kratos::make_intrusive<NodeType>(4, 3.0, 1.0, 2.0) );
+        points_global_space.push_back( Kratos::make_intrusive<NodeType>(5, 1.5, 0.5, 1.0) );
+
+        auto p_triangle_global_space = Kratos::make_shared<Triangle3D6<NodeType>>(points_global_space);
+
+        // KRATOS_WATCH( p_triangle->Area() );
+        // CoordinatesArrayType test_point;
+        // test_point[0] = 1.0/3.0;
+        // test_point[1] = 1.0/3.0;
+        // test_point[2] = 0.0;
+        // KRATOS_WATCH( p_triangle->Normal(test_point) );
+
+        PointerVector<NodeType> points_local_space;
+        for( IndexType i = 0; i < points_global_space.size(); ++i){
+            double x_coord_local = (points_global_space[i].Coordinates()[0] + 0.5) / 3.5;
+            double y_coord_local = (points_global_space[i].Coordinates()[1] + 1.0) / 2.0;
+            double z_coord_local = (points_global_space[i].Coordinates()[2] + 0.0) / 2.0;
+            points_local_space.push_back(Kratos::make_intrusive<NodeType>(i, x_coord_local, y_coord_local, z_coord_local));
+        }
+        auto p_triangle_local_space = Kratos::make_shared<Triangle3D6<NodeType>>(points_local_space);
+        // std::vector<Kratos_intrusive_ptr<NodeType> nodes_global = {p_node_global_1, p_node_global_2 , p_node_global_3
+        //      p_node_global_1, p_node_global_1, p_node_global_1,p_node_global_1}
+        // std::vector<double> z_direction = {0.0, 0.5, 1.1, 2.0};
+        // std::vector<double> y_direction = {-1.0, 1.0};
+        // std::vector<double> x_direction = {-0.5, 0.5, 1.0, 3.0};
+
+
+        // Transform nodes into parameter space of nurbs volume
+
+
+        // SurfaceInNurbsVolumeGeometry<3, PointerVector<NodeType>> surface_in_volume(p_cube, p_quad);
+
+        // std::vector<IntegrationPoint<3>> integration_points_created;
+        // surface_in_volume.CreateIntegrationPoints(integration_points_created);
+
+        // surface_in_volume.CreateQuadraturePointGeometries(quad_geometries, 2, integration_points_created);
+
+        // double local_area_triangle =  surface_in_volume.Area();
+        // KRATOS_CHECK_NEAR( local_area_triangle, sqrt(0.5*0.5 + 0.5*0.5), 1e-10);
+
+        // std::vector<double> local_center_ref = {0.25, 0.25, 0.5};
+        // KRATOS_CHECK_VECTOR_NEAR(surface_in_volume.Center(), local_center_ref, 1e-10);
+        // CoordinatesArrayType test_point;
+        // test_point[0] = 0.0;
+        // test_point[1] = 0.0;
+        // test_point[2] = 0.0;
+
+        // // Check Get functions
+        // KRATOS_CHECK_EQUAL(surface_in_volume.HasGeometryPart( GeometryType::BACKGROUND_GEOMETRY_INDEX ), 1);
+        // auto p_nurbs_volume = surface_in_volume.pGetGeometryPart( GeometryType::BACKGROUND_GEOMETRY_INDEX );
+        // auto p_surface = surface_in_volume.pGetSurface();
+
+        // // Check Dimesions
+        // KRATOS_CHECK_EQUAL( p_nurbs_volume->LocalSpaceDimension(), 3);
+        // KRATOS_CHECK_EQUAL( p_surface->LocalSpaceDimension(), 2);
+        // KRATOS_CHECK_EQUAL( surface_in_volume.LocalSpaceDimension(), 3 );
+        // KRATOS_CHECK_EQUAL( quad_geometries[0].LocalSpaceDimension(), 3 );
+
+        // // Check geometrical information
+        // CoordinatesArrayType global_coord;
+        // global_coord = surface_in_volume.GlobalCoordinates(global_coord, test_point);
+        // std::vector<double> global_center_ref = {0.5, 0.5, 1.0};
+        // KRATOS_CHECK_VECTOR_NEAR( global_coord, global_center_ref, 1e-10);
+        // std::vector<double> normal_ref = {0.5, -0.5, 0};
+        // auto integration_method = quad_geometries[0].GetDefaultIntegrationMethod();
+        // KRATOS_CHECK_VECTOR_NEAR(quad_geometries[0].Normal( 0, integration_method), normal_ref, 1e-10 );
+
+        // // Check integration
+        // KRATOS_CHECK_EQUAL( quad_geometries.size(), 4);
+        // double global_area_triangle = 0.0;
+        // for( SizeType i = 0; i < quad_geometries.size(); ++i ){
+        //     global_area_triangle += quad_geometries[i].DeterminantOfJacobian(0, integration_method ) *
+        //         quad_geometries[i].IntegrationPoints()[0].Weight();
+        // }
+
+        // KRATOS_CHECK_NEAR( global_area_triangle, std::sqrt(2.0)*2.0, 1e-10);
+    }
 } // End namespace Testsing
 } // End namespace Kratos
