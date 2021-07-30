7ca1ce2bcac318d1f703a1c1cd529fb53876d9cf
Philipp <philipp.bucher@tum.de>
2021-07-30 10:31:22 +0200

    cleanup

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/external/remote_controlled_solver_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/external/remote_controlled_solver_wrapper.py
@@ -8,45 +8,18 @@ from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_solve
 
 # Other imports
 from KratosMultiphysics.CoSimulationApplication.utilities import model_part_utilities
-import subprocess, os
 
 def Create(settings, model, solver_name):
     return RemoteControlledSolverWrapper(settings, model, solver_name)
 
 class RemoteControlledSolverWrapper(CoSimulationSolverWrapper):
-    """Interface for the CFD-Solver TAU
+    """This class is a generic wrapper for connecting external solvers that are being remote controlled
     """
     def __init__(self, settings, model, solver_name):
         super().__init__(settings, model, solver_name)
 
-        wrapper_settings = self.settings["solver_wrapper_settings"]
-
-
         model_part_utilities.CreateModelPartsFromCouplingDataSettings(self.settings["data"], self.model, self.name)
         model_part_utilities.AllocateHistoricalVariablesFromCouplingDataSettings(self.settings["data"], self.model, self.name)
-
-        # start_external_solver = wrapper_settings["start_external_solver"].GetBool()
-        # if start_external_solver:
-        #     command_txt = wrapper_settings["external_solver_start_command"].GetString()
-        #     path_to_tau = wrapper_settings["path_to_tau"].GetString()
-        #     # QUESTION: shall we use this:
-        #     parent_path = os.path.join(os.path.dirname(__file__), '../..')
-        #     # OR this:
-        #     # parent_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
-        #     path_to_tau_solver = parent_path + '/helpers/tau_solver.py'
-        #     path_to_tau_solver = parent_path + '/helpers/TauSolver.py'
-        #     tau_input_file = wrapper_settings["tau_input_file"].GetString()
-        #     tau_log_file = wrapper_settings["tau_log_file"].GetString()
-        #     # command_args = wrapper_settings["external_solver_arguments"].GetStringArray()
-        #     command_args = [path_to_tau, path_to_tau_solver, tau_input_file, tau_log_file]
-        #     cs_tools.cs_print_info(self._ClassName(), 'Running external solver with command: "{}" | arguments: "{}"'.format(command_txt, command_args))
-
-        #     full_command = [command_txt]
-        #     full_command.extend(command_args)
-        #     # self.external_solver_process = subprocess.Popen(full_command, stderr=subprocess.PIPE, start_new_session=True) # TODO check what to use here
-        #     # self.external_solver_process = subprocess.Popen(full_command)
-
-        # self.controlling_external_solver = wrapper_settings["controlling_external_solver"].GetBool()
 
     def Initialize(self):
         super().Initialize()
@@ -56,13 +29,11 @@ class RemoteControlledSolverWrapper(CoSimulationSolverWrapper):
             self.ImportCouplingInterface(interface_config)
 
     def AdvanceInTime(self, current_time):
-        # self.__CheckExternalSolverProcess() # TODO check why this is blocking
-
         settings = KM.Parameters("""{}""")
         settings.AddEmptyValue("current_time").SetDouble(current_time)
         self.__SendControlSignal("AdvanceInTime", settings)
 
-        # TODO here one could import back the new time from the solver
+        # here one could import back the new time from the solver
         return 0.0
 
     def Predict(self):
@@ -140,9 +111,3 @@ class RemoteControlledSolverWrapper(CoSimulationSolverWrapper):
             "settings"       : settings
         }
         self.ExportData(data_config)
-
-    def __CheckExternalSolverProcess(self):
-        if hasattr(self, 'external_solver_process') and self.external_solver_process.poll() is None:
-            _, process_stderr = self.external_solver_process.communicate()
-            if process_stderr:
-                raise Exception("{} terminated with the following error:\n{}".format(self._ClassName(), process_stderr.decode('ascii')))
