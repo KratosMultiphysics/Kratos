9fd77061b81ad1eedebbd10b834efa8580138069
RahulKN <rahul.melkote@gmail.com>
2021-08-04 13:59:46 +0200

    change convergence bug

--- applications/RANSApplication/python_scripts/formulations/turbulence_models/two_equation_turbulence_model_rans_formulation.py
+++ applications/RANSApplication/python_scripts/formulations/turbulence_models/two_equation_turbulence_model_rans_formulation.py
@@ -86,20 +86,11 @@ class TwoEquationTurbulenceModelRansFormulation(RansFormulation):
             self.ExecuteAfterCouplingSolveStep()
 
             Kratos.Logger.PrintInfo(self.__class__.__name__, "Solved coupling itr. {:d}/{:d}.".format(iteration + 1, max_iterations))
-            self.is_converged = self.nu_t_convergence_utility.CheckConvergence()
-            if (self.is_converged):
+            is_converged = self.nu_t_convergence_utility.CheckConvergence()
+            if (is_converged):
                 return True
 
-        return False    # changed from True to False
-
-    # Checking for nuT convergence
-    def IsConverged(self):
-        is_converged = super().IsConverged()
-
-        if hasattr(self, "is_converged"):
-            return is_converged and self.is_converged
-        else:
-            return is_converged
+        return False
 
     def ExecuteAfterCouplingSolveStep(self):
         super().ExecuteAfterCouplingSolveStep()
