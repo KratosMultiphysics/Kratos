c3351c4617fa776006f298b03031e521f6b36a40
tteschemacher <tteschemacher@users.noreply.github.com>
2021-08-09 14:12:36 +0200

    remove model_tolerance

--- kratos/geometries/coupling_geometry.h
+++ kratos/geometries/coupling_geometry.h
@@ -338,8 +338,6 @@ public:
         IntegrationPointsArrayType& rIntegrationPoints,
         IntegrationInfo& rIntegrationInfo) const override
     {
-        const double model_tolerance = 1e-3;
-
         if (this->Dimension() == 1) {
             std::vector<double> master_span_intersections_in_master_local_space;
             std::vector<double> slave_span_intersections_in_master_local_space;
