de1f31434fabe21452fa0ac80bb54e8575d2a1e3
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-08-10 12:18:49 +0200

    Refactor AALMAdaptPenaltyValueProcess

--- applications/ContactStructuralMechanicsApplication/custom_processes/aalm_adapt_penalty_value_process.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_processes/aalm_adapt_penalty_value_process.cpp
@@ -16,6 +16,7 @@
 // External includes
 
 // Project includes
+#include "utilities/parallel_utilities.h"
 #include "contact_structural_mechanics_application_variables.h"
 #include "custom_processes/aalm_adapt_penalty_value_process.h"
 
@@ -33,23 +34,19 @@ void AALMAdaptPenaltyValueProcess::Execute()
     
     // We iterate over the node
     NodesArrayType& r_nodes_array = mrThisModelPart.Nodes();
-    const auto it_node_begin = r_nodes_array.begin();
 
-    #pragma omp parallel for 
-    for(int i = 0; i < static_cast<int>(r_nodes_array.size()); ++i) {
-        auto it_node = it_node_begin + i;
-        
+    block_for_each(r_nodes_array, [&](NodeType& rNode) {
         // Initial value
-        const double penalty_parameter = initialize ? initial_penalty_parameter : it_node->GetValue(INITIAL_PENALTY);
+        const double penalty_parameter = initialize ? initial_penalty_parameter : rNode.GetValue(INITIAL_PENALTY);
         
         // Weighted values
-        const double nodal_area = it_node->Has(NODAL_AREA) ? it_node->GetValue(NODAL_AREA) : 0.0;
+        const double nodal_area = rNode.Has(NODAL_AREA) ? rNode.GetValue(NODAL_AREA) : 0.0;
         if (nodal_area > std::numeric_limits<double>::epsilon()) {
-            const double current_gap = it_node->FastGetSolutionStepValue(WEIGHTED_GAP)/nodal_area;
-            const double previous_gap = it_node->FastGetSolutionStepValue(WEIGHTED_GAP, 1)/nodal_area;
+            const double current_gap = rNode.FastGetSolutionStepValue(WEIGHTED_GAP)/nodal_area;
+            const double previous_gap = rNode.FastGetSolutionStepValue(WEIGHTED_GAP, 1)/nodal_area;
             
             // Nodal H x gap factor
-            const double max_gap = max_gap_factor * it_node->FastGetSolutionStepValue(NODAL_H); // NOTE: This value must be studied
+            const double max_gap = max_gap_factor * rNode.FastGetSolutionStepValue(NODAL_H); // NOTE: This value must be studied
             
             // DEBUG
             KRATOS_TRACE("Current gap") << "current_gap: " << current_gap << "\tprevious_gap: " << previous_gap << "\tmax_gap: " << max_gap << std::endl;
@@ -59,24 +56,24 @@ void AALMAdaptPenaltyValueProcess::Execute()
             
             if ((current_gap * previous_gap) < 0.0) {
                 if (std::abs(previous_gap) > max_gap) // NOTE: The abs is deduced from the paper (the algorithm is without abs)
-                    it_node->SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (current_gap) * (std::abs(current_gap) + max_gap)/(current_gap - previous_gap)));
+                    rNode.SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (current_gap) * (std::abs(current_gap) + max_gap)/(current_gap - previous_gap)));
                 else
-                    it_node->SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (10.0 * current_gap)));
+                    rNode.SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (10.0 * current_gap)));
             } else if (std::abs(current_gap) > max_gap) { // NOTE: The abs is deduced from the paper (the algorithm is without abs)
                 if (std::abs(current_gap - previous_gap) > std::max(current_gap/10.0, std::max(previous_gap/1.0, 5 * max_gap)))
-                    it_node->SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter);
+                    rNode.SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter);
                 else if ((std::abs(current_gap) <= std::abs(previous_gap) * 1.01 || std::abs(current_gap) >= std::abs(previous_gap) * 0.99) && (std::abs(current_gap) < 10.0 *  max_gap))
-                    it_node->SetValue(INITIAL_PENALTY, penalty_parameter * std::pow((std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0), 2.0));
+                    rNode.SetValue(INITIAL_PENALTY, penalty_parameter * std::pow((std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0), 2.0));
                 else if (std::abs(current_gap) > std::abs(previous_gap) * 1.01)
-                    it_node->SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter * (previous_gap/current_gap));
+                    rNode.SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter * (previous_gap/current_gap));
                 else
-                    it_node->SetValue(INITIAL_PENALTY, penalty_parameter * (std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0));
+                    rNode.SetValue(INITIAL_PENALTY, penalty_parameter * (std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0));
             } else
-                it_node->SetValue(INITIAL_PENALTY, penalty_parameter);
+                rNode.SetValue(INITIAL_PENALTY, penalty_parameter);
         } else {
-            it_node->SetValue(INITIAL_PENALTY, penalty_parameter);
+            rNode.SetValue(INITIAL_PENALTY, penalty_parameter);
         }
-    }
+    });
 
     KRATOS_CATCH("");
 } 
