3edea79423fd1280550efb5ef84476d3ffe10227
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-08-16 10:07:08 +0200

    using variable_utils for convective velocity

--- applications/ConvectionDiffusionApplication/python_scripts/coupled_structural_thermal_solver.py
+++ applications/ConvectionDiffusionApplication/python_scripts/coupled_structural_thermal_solver.py
@@ -168,5 +168,4 @@ class CoupledThermoMechanicalSolver(PythonSolver):
 
     def RemoveConvectiveVelocity(self):
         if self.is_dynamic:
-            for node in self.thermal_solver.GetComputingModelPart().Nodes:
-                node.SetSolutionStepValue(KratosMultiphysics.MESH_VELOCITY, node.GetSolutionStepValue(KratosMultiphysics.VELOCITY))
+            KratosMultiphysics.variable_utils().CopyModelPartNodalVar(KratosMultiphysics.VELOCITY, KratosMultiphysics.MESH_VELOCITY, self.thermal_solver.GetComputingModelPart(), self.thermal_solver.GetComputingModelPart())
