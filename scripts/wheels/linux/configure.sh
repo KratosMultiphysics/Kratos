db1578aad63b50c097d177c0b4ad890d1fccead7
Carlos Roig <roigcarlo@gmail.com>
2021-08-16 13:59:50 +0200

    Disabling for testing

--- scripts/wheels/linux/configure.sh
+++ scripts/wheels/linux/configure.sh
@@ -23,37 +23,37 @@ export PYTHON_EXECUTABLE=$1
 export KRATOS_APPLICATIONS=
 add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
 add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
-add_app ${KRATOS_APP_DIR}/DEMApplication
-add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication
-add_app ${KRATOS_APP_DIR}/ParticleMechanicsApplication;
-add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication;
-add_app ${KRATOS_APP_DIR}/DamApplication;
-add_app ${KRATOS_APP_DIR}/PoromechanicsApplication;
-add_app ${KRATOS_APP_DIR}/FSIApplication;
-add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication;
-add_app ${KRATOS_APP_DIR}/ExternalSolversApplication;
-add_app ${KRATOS_APP_DIR}/EigenSolversApplication;
-add_app ${KRATOS_APP_DIR}/LinearSolversApplication;
-add_app ${KRATOS_APP_DIR}/ConstitutiveLawsApplication;
-add_app ${KRATOS_APP_DIR}/FemToDemApplication;
-add_app ${KRATOS_APP_DIR}/PfemFluidDynamicsApplication;
-add_app ${KRATOS_APP_DIR}/DelaunayMeshingApplication;
-add_app ${KRATOS_APP_DIR}/MeshingApplication;
-add_app ${KRATOS_APP_DIR}/DemStructuresCouplingApplication;
-add_app ${KRATOS_APP_DIR}/MeshMovingApplication;
-add_app ${KRATOS_APP_DIR}/CSharpWrapperApplication;
-add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication;
-add_app ${KRATOS_APP_DIR}/CoSimulationApplication;
-add_app ${KRATOS_APP_DIR}/CableNetApplication;
-add_app ${KRATOS_APP_DIR}/RANSApplication;
-add_app ${KRATOS_APP_DIR}/MappingApplication;
-add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
-# add_app ${KRATOS_APP_DIR}/HDF5Application;
-add_app ${KRATOS_APP_DIR}/IgaApplication;
-add_app ${KRATOS_APP_DIR}/ChimeraApplication;
-add_app ${KRATOS_APP_DIR}/MultilevelMonteCarloApplication;
-add_app ${KRATOS_APP_DIR}/StatisticsApplication;
-add_app ${KRATOS_APP_DIR}/RomApplication;
+# add_app ${KRATOS_APP_DIR}/DEMApplication
+# add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication
+# add_app ${KRATOS_APP_DIR}/ParticleMechanicsApplication;
+# add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication;
+# add_app ${KRATOS_APP_DIR}/DamApplication;
+# add_app ${KRATOS_APP_DIR}/PoromechanicsApplication;
+# add_app ${KRATOS_APP_DIR}/FSIApplication;
+# add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication;
+# add_app ${KRATOS_APP_DIR}/ExternalSolversApplication;
+# add_app ${KRATOS_APP_DIR}/EigenSolversApplication;
+# add_app ${KRATOS_APP_DIR}/LinearSolversApplication;
+# add_app ${KRATOS_APP_DIR}/ConstitutiveLawsApplication;
+# add_app ${KRATOS_APP_DIR}/FemToDemApplication;
+# add_app ${KRATOS_APP_DIR}/PfemFluidDynamicsApplication;
+# add_app ${KRATOS_APP_DIR}/DelaunayMeshingApplication;
+# add_app ${KRATOS_APP_DIR}/MeshingApplication;
+# add_app ${KRATOS_APP_DIR}/DemStructuresCouplingApplication;
+# add_app ${KRATOS_APP_DIR}/MeshMovingApplication;
+# add_app ${KRATOS_APP_DIR}/CSharpWrapperApplication;
+# add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication;
+# add_app ${KRATOS_APP_DIR}/CoSimulationApplication;
+# add_app ${KRATOS_APP_DIR}/CableNetApplication;
+# add_app ${KRATOS_APP_DIR}/RANSApplication;
+# add_app ${KRATOS_APP_DIR}/MappingApplication;
+# add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication;
+# # add_app ${KRATOS_APP_DIR}/HDF5Application;
+# add_app ${KRATOS_APP_DIR}/IgaApplication;
+# add_app ${KRATOS_APP_DIR}/ChimeraApplication;
+# add_app ${KRATOS_APP_DIR}/MultilevelMonteCarloApplication;
+# add_app ${KRATOS_APP_DIR}/StatisticsApplication;
+# add_app ${KRATOS_APP_DIR}/RomApplication;
 
 # Clean
 rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/cmake_install.cmake"
