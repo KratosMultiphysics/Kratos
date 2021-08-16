5de9edf467aba218e4db805b299b8fcb3036a015
Carlos Roig <roigcarlo@gmail.com>
2021-08-16 09:53:16 +0200

    updating build script

--- scripts/wheels/build_release.ps1
+++ scripts/wheels/build_release.ps1
@@ -1,6 +1,7 @@
 RELEASE_BRANCH=Release-9.0
 
 HOST_SWAP=C:/data_swap_host
+GUEST_SWAP=/data_swap_guest
 
-docker run --name release_build -v ${HOST_SWAP}:/data_swap_guest -d kratos/wheels ${RELEASE_BRANCH}
+docker run --name release_build -v ${HOST_SWAP}:${GUEST_SWAP} -d kratos/wheels ${RELEASE_BRANCH}
 docker rm release_build
