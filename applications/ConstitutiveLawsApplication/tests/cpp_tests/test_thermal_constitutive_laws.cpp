867739f5941e7c1aa14ab115f16dd9f42adc8e45
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-08-18 14:37:53 +0200

    adding 3d case thermal with tables interpolation

--- applications/ConstitutiveLawsApplication/tests/cpp_tests/test_thermal_constitutive_laws.cpp
+++ applications/ConstitutiveLawsApplication/tests/cpp_tests/test_thermal_constitutive_laws.cpp
@@ -278,5 +278,103 @@ namespace Testing
         KRATOS_CHECK_RELATIVE_NEAR(output_von_mises[0],reference_von_mises_pk2, 1.0e-4);
     }
 
+    /**
+    * Checks the ThermalLinearIsotropic3DWithTable
+    */
+    KRATOS_TEST_CASE_IN_SUITE(SmallDisplacementElement2D3NThermalLinearIsotropic3DWithTable, KratosConstitutiveLawsFastSuite)
+    {
+        Model current_model;
+        auto &r_model_part = current_model.CreateModelPart("ModelPart",1);
+        r_model_part.GetProcessInfo().SetValue(DOMAIN_SIZE, 3);
+
+        const auto& r_process_info = r_model_part.GetProcessInfo();
+
+        r_model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
+        r_model_part.AddNodalSolutionStepVariable(VOLUME_ACCELERATION);
+        r_model_part.AddNodalSolutionStepVariable(TEMPERATURE);
+
+        // Set the element properties
+        auto p_elem_prop = r_model_part.CreateNewProperties(0);
+        p_elem_prop->SetValue(YOUNG_MODULUS, 2.0e+06);
+        p_elem_prop->SetValue(POISSON_RATIO, 0.3);
+        p_elem_prop->SetValue(THERMAL_EXPANSION_COEFFICIENT, 7.2e-6);
+        p_elem_prop->SetValue(REFERENCE_TEMPERATURE, 0.0);
+
+        Table<double> temp_E_table;
+        temp_E_table.insert(-1.0, 2.0e6);
+        temp_E_table.insert(3500.0, 2.1e4);
+        temp_E_table.insert(1.0e6, 2.1e4);
+        p_elem_prop->SetTable(TEMPERATURE, YOUNG_MODULUS, temp_E_table);
+
+        const auto &r_clone_cl = KratosComponents<ConstitutiveLaw>::Get("ThermalElasticIsotropic3D");
+        p_elem_prop->SetValue(CONSTITUTIVE_LAW, r_clone_cl.Clone());
+
+        // Create the test element
+        auto p_node_1 = r_model_part.CreateNewNode(1, 0.1 , 0.1 , 0.0);
+        auto p_node_2 = r_model_part.CreateNewNode(2, 0.5 , 0.2 , 0.0);
+        auto p_node_3 = r_model_part.CreateNewNode(3, 0.9 , 0.4 , 0.0);
+        auto p_node_4 = r_model_part.CreateNewNode(4, 0.3 , 0.3 , 0.5);
+
+        for (auto& r_node : r_model_part.Nodes()){
+            r_node.AddDof(DISPLACEMENT_X);
+            r_node.AddDof(DISPLACEMENT_Y);
+            r_node.AddDof(DISPLACEMENT_Z);
+        }
+
+        std::vector<ModelPart::IndexType> element_nodes {1,2,3,4};
+        auto p_element = r_model_part.CreateNewElement("SmallDisplacementElement3D4N", 1, element_nodes, p_elem_prop);
+        p_element->Initialize(r_model_part.GetProcessInfo());
+
+        // Set a displacement and temperature
+        array_1d<double, 3> aux_disp = ZeroVector(3);
+        noalias(p_node_1->FastGetSolutionStepValue(DISPLACEMENT)) = aux_disp;
+        noalias(p_node_2->FastGetSolutionStepValue(DISPLACEMENT)) = aux_disp;
+        noalias(p_node_3->FastGetSolutionStepValue(DISPLACEMENT)) = aux_disp;
+        aux_disp(0) = 0.001;
+        noalias(p_node_4->FastGetSolutionStepValue(DISPLACEMENT)) = aux_disp;
+        p_node_1->FastGetSolutionStepValue(TEMPERATURE) = 2400.2;
+        p_node_2->FastGetSolutionStepValue(TEMPERATURE) = 2400.2;
+        p_node_3->FastGetSolutionStepValue(TEMPERATURE) = 2400.2;
+        p_node_4->FastGetSolutionStepValue(TEMPERATURE) = 2400.2;
+
+        Matrix lhs;
+        Vector rhs;
+        const auto& const_procinfo_ref = r_model_part.GetProcessInfo();
+        p_element->InitializeSolutionStep(const_procinfo_ref);
+        p_element->InitializeNonLinearIteration(const_procinfo_ref);
+        p_element->CalculateLocalSystem(lhs,rhs,const_procinfo_ref);
+        p_element->FinalizeNonLinearIteration(const_procinfo_ref);
+        p_element->FinalizeSolutionStep(const_procinfo_ref);
+
+        std::vector<Vector> output_strains(1);
+        p_element->CalculateOnIntegrationPoints(GREEN_LAGRANGE_STRAIN_VECTOR,output_strains, r_model_part.GetProcessInfo());
+
+        std::vector<Vector> output_stress(1);
+        p_element->CalculateOnIntegrationPoints(PK2_STRESS_VECTOR,output_stress, r_model_part.GetProcessInfo());
+
+        std::vector<double> output_von_mises(1);
+        p_element->CalculateOnIntegrationPoints(VON_MISES_STRESS,output_von_mises, r_model_part.GetProcessInfo());
+
+        Vector reference_stress(6);
+        reference_stress(0) = -27766.1;
+        reference_stress(1) = -27766.1;
+        reference_stress(2) = -27766.1;
+        reference_stress(3) = 0.0;
+        reference_stress(4) = 0.0;
+        reference_stress(5) = 494.37;
+        Vector reference_strain(6);
+        reference_strain(0) = -0.0172814;
+        reference_strain(1) = -0.0172814;
+        reference_strain(2) = -0.0172814;
+        reference_strain(3) = 0.0;
+        reference_strain(4) = 0.0;
+        reference_strain(5) = 0.002;
+        const double reference_von_mises_pk2 =  856.274;
+
+        KRATOS_CHECK_VECTOR_RELATIVE_NEAR(output_strains[0], reference_strain, 1e-4);
+        KRATOS_CHECK_VECTOR_RELATIVE_NEAR(output_stress[0], reference_stress, 1e-4);
+        KRATOS_CHECK_RELATIVE_NEAR(output_von_mises[0],reference_von_mises_pk2, 1.0e-4);
+    }
+
 } // namespace Testing
 } // namespace Kratos.
