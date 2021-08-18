6c27f9aa9b2ad7d8bab215e4dc0658ad7d5fbd75
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2021-08-18 15:38:57 +0200

    Minor clean up

--- applications/FluidDynamicsApplication/python_scripts/check_and_prepare_model_process_fluid.py
+++ applications/FluidDynamicsApplication/python_scripts/check_and_prepare_model_process_fluid.py
@@ -25,14 +25,6 @@ class CheckAndPrepareModelProcess(KratosMultiphysics.Process):
 
         self.assign_neighbour_elements = Parameters["assign_neighbour_elements_to_conditions"].GetBool()
 
-
-        #self.volume_model_part_name = Parameters["volume_model_part_name"].GetString()
-        #self.list_of_inlets = Parameters["list_of_inlets"]
-        #self.list_of_slip = Parameters["list_of_inlets"]
-        #self.list_of_inlets = Parameters["list_of_inlets"]
-
-
-
     def Execute(self):
         if self.main_model_part.Name == self.volume_model_part_name:
             self.volume_model_part = self.main_model_part
@@ -43,8 +35,8 @@ class CheckAndPrepareModelProcess(KratosMultiphysics.Process):
         for i in range(self.skin_name_list.size()):
             skin_parts.append(self.main_model_part.GetSubModelPart(self.skin_name_list[i].GetString()))
 
-        #construct a model part which contains both the skin and the volume
-        #temporarily we call it "fluid_computational_model_part"
+        # Construct a model part which contains both the skin and the volume
+        # Temporarily we call it "fluid_computational_model_part"
         self.main_model_part.CreateSubModelPart("fluid_computational_model_part")
         fluid_computational_model_part= self.main_model_part.GetSubModelPart("fluid_computational_model_part")
         fluid_computational_model_part.ProcessInfo = self.main_model_part.ProcessInfo
@@ -54,7 +46,7 @@ class CheckAndPrepareModelProcess(KratosMultiphysics.Process):
         for elem in self.volume_model_part.Elements:
             fluid_computational_model_part.AddElement(elem,0)
 
-        #do some gymnastics to have this done fast. - create an ordered list to be added
+        # Do some gymnastics to have this done fast. - create an ordered list to be added
         list_of_ids = set()
         for part in skin_parts:
             for cond in part.Conditions:
@@ -62,7 +54,7 @@ class CheckAndPrepareModelProcess(KratosMultiphysics.Process):
 
         fluid_computational_model_part.AddConditions(list(list_of_ids))
 
-        #verify the orientation of the skin
+        # Verify the orientation of the skin
         tmoc = KratosMultiphysics.TetrahedralMeshOrientationCheck
         throw_errors = False
         flags = (tmoc.COMPUTE_NODAL_NORMALS).AsFalse() | (tmoc.COMPUTE_CONDITION_NORMALS).AsFalse()
