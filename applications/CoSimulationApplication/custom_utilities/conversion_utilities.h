7acad8b014a1d87b7054954f3792e0e07bee7aa1
Philipp Bucher <philipp.bucher@tum.de>
2021-08-20 11:53:44 +0200

    cleanup III

--- applications/CoSimulationApplication/custom_utilities/conversion_utilities.h
+++ applications/CoSimulationApplication/custom_utilities/conversion_utilities.h
@@ -59,8 +59,6 @@ public:
     ///@name Operations
     ///@{
 
-    //static void ConvertElementalDataToNodalData(ModelPart& rModelPart);
-    //static void ConvertElementalDataToNodalData(ModelPart& rModelPart, Variable<array_1d<double,3> >& rVariable);
     static void ConvertElementalDataToNodalData(
         ModelPart& rModelPart,
         const Variable<array_1d<double,3> >& rElementalVariable,
