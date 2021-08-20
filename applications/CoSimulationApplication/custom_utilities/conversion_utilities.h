9b365c539624085e4b83ed736e2414486ce32b24
Philipp Bucher <philipp.bucher@tum.de>
2021-08-20 11:52:40 +0200

    cleanup I

--- applications/CoSimulationApplication/custom_utilities/conversion_utilities.h
+++ applications/CoSimulationApplication/custom_utilities/conversion_utilities.h
@@ -15,8 +15,6 @@
 #define  KRATOS_COSIM_CONVERSION_UTILITIES_H_INCLUDED
 
 // System includes
-#include <string>
-#include <iostream>
 
 // External includes
 
@@ -26,25 +24,9 @@
 
 namespace Kratos
 {
-///@addtogroup ApplicationNameApplication
+///@addtogroup CoSimulationApplication
 ///@{
-
-///@name Kratos Globals
-///@{
-
-///@}
-///@name Type Definitions
-///@{
-
-///@}
-///@name  Enum's
-///@{
-
-///@}
-///@name  Functions
-///@{
-
-///@}
+    
 ///@name Kratos Classes
 ///@{
 
@@ -66,11 +48,12 @@ public:
 
     /// Default constructor.
     ConversionUtilities() = delete;
+    
+    /// Assignment operator.
+    ConversionUtilities& operator=(ConversionUtilities const& rOther) = delete;
 
-    ///@}
-    ///@name Operators
-    ///@{
-
+    /// Copy constructor.
+    ConversionUtilities(ConversionUtilities const& rOther) = delete;
 
     ///@}
     ///@name Operations
@@ -84,130 +67,13 @@ public:
         const Variable<array_1d<double,3> >& rNodalVariable );
 
     ///@}
-    ///@name Access
-    ///@{
-
-
-    ///@}
-    ///@name Inquiry
-    ///@{
-
-
-    ///@}
-    ///@name Input and output
-    ///@{
-
-    /// Turn back information as a string.
-    std::string Info() const;
-
-    /// Print information about this object.
-    void PrintInfo(std::ostream& rOStream) const;
-
-    /// Print object's data.
-    void PrintData(std::ostream& rOStream) const;
-
-
-    ///@}
-    ///@name Friends
-    ///@{
-
-
-    ///@}
-
-protected:
-    ///@name Protected static Member Variables
-    ///@{
-
-
-    ///@}
-    ///@name Protected member Variables
-    ///@{
-
-
-    ///@}
-    ///@name Protected Operators
-    ///@{
-
-
-    ///@}
-    ///@name Protected Operations
-    ///@{
-
-
-    ///@}
-    ///@name Protected  Access
-    ///@{
-
-
-    ///@}
-    ///@name Protected Inquiry
-    ///@{
-
-
-    ///@}
-    ///@name Protected LifeCycle
-    ///@{
-
-
-    ///@}
-
-private:
-    ///@name Static Member Variables
-    ///@{
-
-
-    ///@}
-    ///@name Member Variables
-    ///@{
-
-
-    ///@}
-    ///@name Private Operators
-    ///@{
-
-
-    ///@}
-    ///@name Private Operations
-    ///@{
-
-
-    ///@}
-    ///@name Private  Access
-    ///@{
-
-
-    ///@}
-    ///@name Private Inquiry
-    ///@{
-
-
-    ///@}
-    ///@name Un accessible methods
-    ///@{
-
-    /// Assignment operator.
-    ConversionUtilities& operator=(ConversionUtilities const& rOther);
-
-    /// Copy constructor.
-    ConversionUtilities(ConversionUtilities const& rOther);
-
-
-    ///@}
 
 }; // Class ConversionUtilities
 
 ///@}
 
-///@name Type Definitions
-///@{
-
-
-///@}
-///@name Input and output
-///@{
-
 ///@} addtogroup block
 
 }  // namespace Kratos.
 
-#endif // KRATOS_COSIM_CONVERSION_UTILITIES_H_INCLUDED  defined
+#endif // KRATOS_COSIM_CONVERSION_UTILITIES_H_INCLUDED defined
