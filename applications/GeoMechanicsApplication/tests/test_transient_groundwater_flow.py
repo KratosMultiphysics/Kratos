d390986a0aec60d3c9b95a7f4abc7ee74a6e5eba
Vahid <39699625+Vahid-Galavi@users.noreply.github.com>
2021-08-23 13:41:11 +0200

    - DPIPING-13: add a new test for transient flow 2D6N

--- applications/GeoMechanicsApplication/tests/test_transient_groundwater_flow.py
+++ applications/GeoMechanicsApplication/tests/test_transient_groundwater_flow.py
@@ -31,6 +31,16 @@ class KratosGeoMechanicsTransientGroundWaterFlowTests(KratosUnittest.TestCase):
 
         self.assertAlmostEqual(3.7224838666844984, p_height_1_72_m)
 
+    def test_Transient_Case_A1_2D6N(self):
+        test_name = 'test_Transient_Case_A1_2D6N'
+        file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
+        stages = test_helper.run_stages(file_path, 2)
+
+        water_pressure = test_helper.get_water_pressure(stages[-1])
+
+        p_height_1_72_m = water_pressure[87]
+
+        self.assertAlmostEqual(2.6423560731957005, p_height_1_72_m)
 
 
 if __name__ == '__main__':
