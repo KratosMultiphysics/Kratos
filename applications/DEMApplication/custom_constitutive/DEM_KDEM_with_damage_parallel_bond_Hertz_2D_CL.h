0aafc9c4f46e100468535fa84002f6e1f3254d9f
Salva Latorre <latorre@cimne.upc.edu>
2021-08-27 21:19:32 +0200

    Critical fixes

--- applications/DEMApplication/custom_constitutive/DEM_KDEM_with_damage_parallel_bond_Hertz_2D_CL.h
+++ applications/DEMApplication/custom_constitutive/DEM_KDEM_with_damage_parallel_bond_Hertz_2D_CL.h
@@ -27,6 +27,12 @@ namespace Kratos {
 
         void CalculateContactArea(double radius, double other_radius, double& calculation_area) override;
 
+        void CalculateElasticConstants(double& kn_el, double& kt_el, double initial_dist, double equiv_young,
+                                       double equiv_poisson, double calculation_area, SphericContinuumParticle* element1,
+                                       SphericContinuumParticle* element2, double indentation) override;
+        
+        void ComputeNormalUnbondedForce(double indentation) override;
+
     private:
 
         friend class Serializer;
