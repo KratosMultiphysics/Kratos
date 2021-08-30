28bccc731326a0a74f18ff37a376c32d842c69bc
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-08-30 17:46:33 +0200

    utility optimized to minimum number of axes

--- applications/StructuralMechanicsApplication/custom_utilities/set_local_axes_utility.cpp
+++ applications/StructuralMechanicsApplication/custom_utilities/set_local_axes_utility.cpp
@@ -30,37 +30,38 @@ void SetLocalAxesUtility::SetLocalAxisCartesianSystem(
     Parameters default_parameters = Parameters(R"(
     {
         "local_axes_coordinate_system"  : "cartesian",
-        "cartesian_local_axis"          : [[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]]
+        "cartesian_local_axis"          : [[1.0,0.0,0.0],[0.0,1.0,0.0]]
     })");
     ThisParameters.RecursivelyValidateAndAssignDefaults(default_parameters);
 
-    const Matrix cartesian_local_axes_matrix = ThisParameters["cartesian_local_axis"].GetMatrix();
     array_1d<double, 3> local_axis_1;
-    array_1d<double, 3> local_axis_2;
-    array_1d<double, 3> local_axis_3;
 
-    local_axis_1(0) = cartesian_local_axes_matrix(0, 0);
-    local_axis_1(1) = cartesian_local_axes_matrix(0, 1);
-    local_axis_1(2) = cartesian_local_axes_matrix(0, 2);
+    if (rModelPart.GetProcessInfo()[DOMAIN_SIZE] == 3) {
+        array_1d<double, 3> local_axis_2;
+        const Matrix& cartesian_local_axes_matrix = ThisParameters["cartesian_local_axis"].GetMatrix();
+        local_axis_1[0] = cartesian_local_axes_matrix(0, 0);
+        local_axis_1[1] = cartesian_local_axes_matrix(0, 1);
+        local_axis_1[2] = cartesian_local_axes_matrix(0, 2);
+        local_axis_2[0] = cartesian_local_axes_matrix(1, 0);
+        local_axis_2[1] = cartesian_local_axes_matrix(1, 1);
+        local_axis_2[2] = cartesian_local_axes_matrix(1, 2);
+        CheckAndNormalizeVector(local_axis_1);
+        CheckAndNormalizeVector(local_axis_2);
 
-    local_axis_2(0) = cartesian_local_axes_matrix(1, 0);
-    local_axis_2(1) = cartesian_local_axes_matrix(1, 1);
-    local_axis_2(2) = cartesian_local_axes_matrix(1, 2);
-
-    local_axis_3(0) = cartesian_local_axes_matrix(2, 0);
-    local_axis_3(1) = cartesian_local_axes_matrix(2, 1);
-    local_axis_3(2) = cartesian_local_axes_matrix(2, 2);
-
-    CheckAndNormalizeVector(local_axis_1);
-    CheckAndNormalizeVector(local_axis_2);
-    CheckAndNormalizeVector(local_axis_3);
-
-    block_for_each(rModelPart.Elements(), [&](Element& rElement) {
-        rElement.SetValue(LOCAL_AXIS_1, local_axis_1);
-        rElement.SetValue(LOCAL_AXIS_2, local_axis_2);
-        rElement.SetValue(LOCAL_AXIS_3, local_axis_3);
-    });
-
+        block_for_each(rModelPart.Elements(), [&](Element& rElement) {
+            rElement.SetValue(LOCAL_AXIS_1, local_axis_1);
+            rElement.SetValue(LOCAL_AXIS_2, local_axis_2);
+        });
+    } else if (rModelPart.GetProcessInfo()[DOMAIN_SIZE] == 2) {
+        const Vector& cartesian_local_axes_matrix = ThisParameters["cartesian_local_axis"].GetVector();
+        local_axis_1[0] = cartesian_local_axes_matrix[0];
+        local_axis_1[1] = cartesian_local_axes_matrix[1];
+        local_axis_1[2] = cartesian_local_axes_matrix[2];
+        CheckAndNormalizeVector(local_axis_1);
+        block_for_each(rModelPart.Elements(), [&](Element& rElement) {
+            rElement.SetValue(LOCAL_AXIS_1, local_axis_1);
+        });
+    }
     KRATOS_CATCH("")
 }
 
