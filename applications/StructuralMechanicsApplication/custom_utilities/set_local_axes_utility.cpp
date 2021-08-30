b3a19f57ef7885fbc5e0a6281b5a514bb6f25036
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-08-30 17:04:34 +0200

    avoid local axis 3

--- applications/StructuralMechanicsApplication/custom_utilities/set_local_axes_utility.cpp
+++ applications/StructuralMechanicsApplication/custom_utilities/set_local_axes_utility.cpp
@@ -87,7 +87,6 @@ void SetLocalAxesUtility::SetLocalAxisCylindricalSystem(
     block_for_each(rModelPart.Elements(), [&](Element &rElement) {
         array_1d<double, 3> local_axis_1;
         array_1d<double, 3> local_axis_2;
-        array_1d<double, 3> local_axis_3;
         const array_1d<double, 3> coords = rElement.GetGeometry().Center();
         const double c = -r_generatrix_axis(0) * coords(0) - r_generatrix_axis(1) * coords(1) - r_generatrix_axis(2) * coords(2);
         const double lambda = -(r_generatrix_axis(0) * r_generatrix_point(0) + r_generatrix_axis(1) * r_generatrix_point(1) + r_generatrix_axis(2) * r_generatrix_point(2) + c) / (std::pow(r_generatrix_axis(0), 2) + std::pow(r_generatrix_axis(1), 2) + std::pow(r_generatrix_axis(2), 2));
@@ -97,15 +96,12 @@ void SetLocalAxesUtility::SetLocalAxisCylindricalSystem(
 
         noalias(local_axis_1) = coords - intersection;
         noalias(local_axis_2) = r_generatrix_axis;
-        noalias(local_axis_3) = MathUtils<double>::CrossProduct(local_axis_1, local_axis_2);
 
         CheckAndNormalizeVector(local_axis_1);
         CheckAndNormalizeVector(local_axis_2);
-        CheckAndNormalizeVector(local_axis_3);
 
         rElement.SetValue(LOCAL_AXIS_1, local_axis_1);
         rElement.SetValue(LOCAL_AXIS_2, local_axis_2);
-        rElement.SetValue(LOCAL_AXIS_3, local_axis_3);
     });
 
     KRATOS_CATCH("")
@@ -135,7 +131,6 @@ void SetLocalAxesUtility::SetLocalAxisSphericalSystem(
     block_for_each(rModelPart.Elements(), [&](Element &rElement) {
         array_1d<double, 3> local_axis_1;
         array_1d<double, 3> local_axis_2;
-        array_1d<double, 3> local_axis_3;
 
         const array_1d<double, 3> coords = rElement.GetGeometry().Center();
         noalias(local_axis_1) = coords - spherical_central_point;
@@ -145,8 +140,6 @@ void SetLocalAxesUtility::SetLocalAxisSphericalSystem(
             std::abs(local_axis_1(1) * spherical_reference_axis(2) - local_axis_1(2) * spherical_reference_axis(1)) <= tolerance) {
             noalias(local_axis_2) = spherical_reference_axis;
             CheckAndNormalizeVector(local_axis_2);
-            noalias(local_axis_3) = MathUtils<double>::CrossProduct(local_axis_1, local_axis_2);
-            CheckAndNormalizeVector(local_axis_3);
         }
         // Let's compute the second local axis
         const double x = 1.0;
@@ -157,12 +150,8 @@ void SetLocalAxesUtility::SetLocalAxisSphericalSystem(
         local_axis_2(2) = z;
         CheckAndNormalizeVector(local_axis_2);
 
-        noalias(local_axis_3) = MathUtils<double>::CrossProduct(local_axis_1, local_axis_2);
-        CheckAndNormalizeVector(local_axis_3);
-
         rElement.SetValue(LOCAL_AXIS_1, local_axis_1);
         rElement.SetValue(LOCAL_AXIS_2, local_axis_2);
-        rElement.SetValue(LOCAL_AXIS_3, local_axis_3);
     });
 
     KRATOS_CATCH("")
