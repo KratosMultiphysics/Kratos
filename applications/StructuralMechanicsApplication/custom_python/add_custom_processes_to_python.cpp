efa0e8b134341b3302a7ad3470ace6f659d810c5
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-08-31 14:18:41 +0200

    add spherical prodcess

--- applications/StructuralMechanicsApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/StructuralMechanicsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -32,6 +32,7 @@
 #include "custom_processes/distribute_load_on_surface_process.h"
 #include "custom_processes/set_cartesian_local_axes_process.h"
 #include "custom_processes/set_cylindrical_local_axes_process.h"
+#include "custom_processes/set_spherical_local_axes_process.h"
 
 namespace Kratos {
 namespace Python {
@@ -103,6 +104,9 @@ void  AddCustomProcessesToPython(pybind11::module& m)
         .def(py::init<ModelPart&, Parameters>());
 
     py::class_<SetCylindricalLocalAxesProcess, SetCylindricalLocalAxesProcess::Pointer, Process>(m,"SetCylindricalLocalAxesProcess")
+        .def(py::init<ModelPart&, Parameters>());
+
+    py::class_<SetSphericalLocalAxesProcess, SetSphericalLocalAxesProcess::Pointer, Process>(m,"SetSphericalLocalAxesProcess")
         .def(py::init<ModelPart&, Parameters>());
 }
 
