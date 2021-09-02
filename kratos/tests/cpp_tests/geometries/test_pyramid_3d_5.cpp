85dbed61d5ec2a8db14e747a05bb23a770790ae9
Philipp <philipp.bucher@tum.de>
2021-09-02 14:13:07 +0200

    WIP adding Pyramid3D5 tests

---
+++ kratos/tests/cpp_tests/geometries/test_pyramid_3d_5.cpp
@@ -0,0 +1,239 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "testing/testing.h"
+#include "geometries/pyramid_3d_5.h"
+#include "tests/cpp_tests/geometries/test_geometry.h"
+
+
+namespace Kratos {
+namespace Testing {
+
+typedef Node<3>                       PointType;
+typedef Node<3>::Pointer              PointPtrType;
+typedef Geometry<PointType>           BaseGeometryType;
+typedef BaseGeometryType::Pointer     BaseGeometryPtrType;
+typedef Pyramid3D5<PointType>         PyramidGeometryType;
+typedef PyramidGeometryType::Pointer  PyramidGeometryPtrType;
+
+/** Generates a sample Pyramid3D5.
+ * Generates a pyramid defined by three random points in the space.
+ * @return  Pointer to a Pyramid3D5
+ */
+BaseGeometryPtrType GeneratePyramid3D5(
+    PointPtrType PointA = GeneratePoint<PointType>(),
+    PointPtrType PointB = GeneratePoint<PointType>(),
+    PointPtrType PointC = GeneratePoint<PointType>(),
+    PointPtrType PointD = GeneratePoint<PointType>(),
+    PointPtrType PointE = GeneratePoint<PointType>()) {
+    return BaseGeometryPtrType(new PyramidGeometryType(PointA, PointB, PointC, PointD, PointE));
+}
+
+/** Generates a sample Pyramid3D5.
+ * Generates a trirectangular pyramid on the origin with positive volume and side 1.
+ * @return  Pointer to a Pyramid3D5
+ */
+BaseGeometryPtrType GenerateRegularPyramid3D5() {
+    return BaseGeometryPtrType(new PyramidGeometryType(
+        GeneratePoint<PointType>(0.0, 0.0, 0.0),
+        GeneratePoint<PointType>(1.0, 0.0, 0.0),
+        GeneratePoint<PointType>(0.0, 1.0, 0.0),
+        GeneratePoint<PointType>(0.0, 0.0, 1.0),
+        GeneratePoint<PointType>(0.0, 1.0, 1.0)
+    ));
+}
+
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5EdgesNumber, KratosCoreGeometriesFastSuite) {
+    auto geomRegular = GenerateRegularPyramid3D5();
+
+    KRATOS_CHECK_EQUAL(geomRegular->EdgesNumber(), 8);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5FacesNumber, KratosCoreGeometriesFastSuite) {
+    auto geomRegular = GenerateRegularPyramid3D5();
+
+    KRATOS_CHECK_EQUAL(geomRegular->FacesNumber(), 5);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5Volume, KratosCoreGeometriesFastSuite) {
+    auto geomRegular = GenerateRegularPyramid3D5();
+
+    KRATOS_CHECK_NEAR(geomRegular->Volume(),  0.5, TOLERANCE);
+}
+
+/** Checks the inside test for a given point respect to the pyramid
+* Checks the inside test for a given point respect to the pyramid
+* It performs 4 tests:
+* A Point inside the pyramid: Expected result TRUE
+* A Point outside the pyramid: Expected result FALSE
+* A Point over a vertex of the pyramid: Expected result TRUE
+* A Point over an edge of the pyramid: Expected result TRUE
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5IsInside, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    Point PointInside(0.1666, 0.1666, 0.1666);
+    Point PointOutside(0.66, 0.66, 0.66);
+    Point PointInVertex(0.0, 0.0, 0.0);
+    Point PointInEdge(0.33, 0.33, 0.33);
+
+    Point LocalCoords;
+
+    KRATOS_CHECK(geom->IsInside(PointInside, LocalCoords, EPSILON));
+    KRATOS_CHECK_IS_FALSE(geom->IsInside(PointOutside, LocalCoords, EPSILON));
+    KRATOS_CHECK(geom->IsInside(PointInVertex, LocalCoords, EPSILON));
+    KRATOS_CHECK(geom->IsInside(PointInEdge, LocalCoords, EPSILON));
+}
+
+/** Checks the point local coordinates for a given point respect to the
+* pyramid. The centre of the pyramid is selected due to its known
+* solution.
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5PointLocalCoordinates, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    // Compute the global coordinates of the centre
+    auto points = geom->Points();
+    auto centre = Point{points[0] + points[1] + points[2] + points[3] + points[4] + points[5]};
+    centre /= 6.0; // TODO use Center()
+
+    // Compute the centre local coordinates
+    array_1d<double, 3> centre_local_coords;
+    geom->PointLocalCoordinates(centre_local_coords, centre);
+
+    KRATOS_CHECK_NEAR(centre_local_coords(0), 1.0/3.0, TOLERANCE);
+    KRATOS_CHECK_NEAR(centre_local_coords(1), 1.0/3.0, TOLERANCE);
+    KRATOS_CHECK_NEAR(centre_local_coords(2), 1.0/2.0, TOLERANCE);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5ShapeFunctionsValues, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+    array_1d<double, 3> coord(3);
+    coord[0] = 1.0 / 2.0;
+    coord[1] = 1.0 / 4.0;
+    coord[2] = 1.0 / 16.0;
+    KRATOS_CHECK_NEAR(geom->ShapeFunctionValue(0, coord), 0.234375, TOLERANCE);
+    KRATOS_CHECK_NEAR(geom->ShapeFunctionValue(1, coord), 0.46875, TOLERANCE);
+    KRATOS_CHECK_NEAR(geom->ShapeFunctionValue(2, coord), 0.234375, TOLERANCE);
+    KRATOS_CHECK_NEAR(geom->ShapeFunctionValue(3, coord), 0.015625, TOLERANCE);
+    KRATOS_CHECK_NEAR(geom->ShapeFunctionValue(4, coord), 0.03125, TOLERANCE);
+    KRATOS_CHECK_NEAR(geom->ShapeFunctionValue(5, coord), 0.015625, TOLERANCE);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5ShapeFunctionsLocalGradients, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+    Matrix gradient;
+
+    // Compute the global coordinates of the centre
+    auto points = geom->Points();
+    Point centre = Point{points[0] + points[1] + points[2] + points[3] + points[4] + points[5]};
+    centre /= 6.0; // TODO use Center()
+
+    // Compute the centre local coordinates
+    array_1d<double, 3> centre_local_coords;
+    geom->PointLocalCoordinates(centre_local_coords, centre);
+
+    gradient = geom->ShapeFunctionsLocalGradients(gradient, centre_local_coords);
+
+    KRATOS_CHECK_NEAR(gradient(0,0), -0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(0,1), -0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(0,2), -1.0/3.0, TOLERANCE);
+
+    KRATOS_CHECK_NEAR(gradient(1,0), 0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(1,1), 0.0, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(1,2), -1.0/3.0, TOLERANCE);
+
+    KRATOS_CHECK_NEAR(gradient(2,0), 0.0, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(2,1), 0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(2,2), -1.0/3.0, TOLERANCE);
+
+    KRATOS_CHECK_NEAR(gradient(3,0), -0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(3,1), -0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(3,2), 1.0/3.0, TOLERANCE);
+
+    KRATOS_CHECK_NEAR(gradient(4,0), 0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(4,1), 0.0, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(4,2), 1.0/3.0, TOLERANCE);
+
+    KRATOS_CHECK_NEAR(gradient(5,0), 0.0, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(5,1), 0.5, TOLERANCE);
+    KRATOS_CHECK_NEAR(gradient(5,2), 1.0/3.0, TOLERANCE);
+}
+
+/** Tests the area using 'GI_GAUSS_1' integration method.
+* Tests the area using 'GI_GAUSS_1' integration method.
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5GaussPoint1, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    const double expected_vol = 0.5;
+
+    KRATOS_CHECK_NEAR(CalculateAreaByIntegration(*geom, GeometryData::GI_GAUSS_1), expected_vol, TOLERANCE);
+    VerifyStrainExactness(*geom, GeometryData::GI_GAUSS_1);
+}
+
+/** Tests the area using 'GI_GAUSS_2' integration method.
+* Tests the area using 'GI_GAUSS_2' integration method.
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5GaussPoint2, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    const double expected_vol = 0.5;
+
+    KRATOS_CHECK_NEAR(CalculateAreaByIntegration(*geom, GeometryData::GI_GAUSS_2), expected_vol, TOLERANCE);
+    VerifyStrainExactness(*geom, GeometryData::GI_GAUSS_2);
+}
+
+/** Tests the area using 'GI_GAUSS_3' integration method.
+* Tests the area using 'GI_GAUSS_3' integration method.
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5GaussPoint3, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    const double expected_vol = 0.5;
+
+    KRATOS_CHECK_NEAR(CalculateAreaByIntegration(*geom, GeometryData::GI_GAUSS_3), expected_vol, TOLERANCE);
+    VerifyStrainExactness(*geom, GeometryData::GI_GAUSS_3);
+}
+
+/** Tests the area using 'GI_GAUSS_4' integration method.
+* Tests the area using 'GI_GAUSS_4' integration method.
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5GaussPoint4, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    const double expected_vol = 0.5;
+
+    KRATOS_CHECK_NEAR(CalculateAreaByIntegration(*geom, GeometryData::GI_GAUSS_4), expected_vol, TOLERANCE);
+    VerifyStrainExactness(*geom, GeometryData::GI_GAUSS_4);
+}
+
+/** Tests the area using 'GI_GAUSS_5' integration method.
+* Tests the area using 'GI_GAUSS_5' integration method.
+*/
+KRATOS_TEST_CASE_IN_SUITE(Pyramid3D5GaussPoint5, KratosCoreGeometriesFastSuite) {
+    auto geom = GenerateRegularPyramid3D5();
+
+    const double expected_vol = 0.5;
+
+    KRATOS_CHECK_NEAR(CalculateAreaByIntegration(*geom, GeometryData::GI_GAUSS_5), expected_vol, TOLERANCE);
+    VerifyStrainExactness(*geom, GeometryData::GI_GAUSS_5);
+}
+
+} // namespace Testing
+} // namespace Kratos.
