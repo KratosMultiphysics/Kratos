74284ece71b3f7074cde2a0596d0d25578cc6ed2
marcnunezc <marcnc94@gmail.com>
2021-09-09 17:30:28 +0200

    Wake defintion using upper/lower surfaces

--- applications/CompressiblePotentialFlowApplication/custom_processes/define_embedded_wake_process_3d.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_processes/define_embedded_wake_process_3d.cpp
@@ -15,6 +15,7 @@
 #include "processes/calculate_discontinuous_distance_to_skin_process.h"
 #include "compressible_potential_flow_application_variables.h"
 #include "custom_utilities/potential_flow_utilities.h"
+#include "modified_shape_functions/tetrahedra_3d_4_modified_shape_functions.h"
 
 namespace Kratos
 {
@@ -36,7 +37,178 @@ void DefineEmbeddedWakeProcess3D::Execute()
     ExecuteInitialize();
     ComputeDistanceToWake();
     MarkWakeElements();
+
+    // #pragma omp parallel for
+    for (int i = 0; i < static_cast<int>(mrModelPart.Elements().size()); i++) {
+        auto it_elem = mrModelPart.ElementsBegin() + i;
+        BoundedVector<double, 4> geometry_distances;
+        auto& r_geometry = it_elem->GetGeometry();
+        for(unsigned int i_node = 0; i_node< r_geometry.size(); i_node++){
+            geometry_distances[i_node] = r_geometry[i_node].GetSolutionStepValue(GEOMETRY_DISTANCE);
+        }
+        const bool is_embedded = PotentialFlowUtilities::CheckIfElementIsCutByDistance<3,4>(geometry_distances);
+
+        auto angle_in_deg = -1*mrModelPart.GetProcessInfo()[ROTATION_ANGLE];
+        auto wake_origin = mrModelPart.GetProcessInfo()[WAKE_ORIGIN];
+        BoundedVector<double, 3> wake_direction;
+        // wake_direction[0] = cos(angle_in_deg*Globals::Pi/180);
+        // wake_direction[1] = sin(angle_in_deg*Globals::Pi/180);
+        // wake_direction[2] = 0.0;
+        wake_direction[0] = sin(angle_in_deg*Globals::Pi/180);
+        wake_direction[1] = 0.0;
+        wake_direction[2] = -cos(angle_in_deg*Globals::Pi/180);
+        if (is_embedded && it_elem->Is(ACTIVE)) {
+
+            BoundedVector<double, 3> unit_normal = it_elem->GetValue(VELOCITY_LOWER);
+            // it_elem->SetValue(VELOCITY_LOWER, unit_normal);
+            double projection = inner_prod(unit_normal, wake_direction);
+
+            it_elem->SetValue(TEMPERATURE, unit_normal[0]*unit_normal[2]); //multiply projection to x-axis and z-axis
+
+            double side_limit = 0.001;
+            if (projection > side_limit) {
+                // upper_surface_elements_list.push_back(it_elem->Id());
+                for (std::size_t i_node = 0; i_node<it_elem->GetGeometry().size();i_node++) {
+                    r_geometry[i_node].SetValue(UPPER_SURFACE, true);
+                }
+            }
+            if (projection < -side_limit) {
+                // lower_surface_elements_list.push_back(it_elem->Id());
+                for (std::size_t i_node = 0; i_node<it_elem->GetGeometry().size();i_node++) {
+                    r_geometry[i_node].SetValue(LOWER_SURFACE, true);
+                }
+            }
+        }
+    }
+
+    for (auto& r_elem : mrModelPart.Elements()) {
+        bool is_upper = false;
+        bool is_lower = false;
+        bool is_wake = false;
+        auto& r_geometry = r_elem.GetGeometry();
+        for (auto& r_node : r_geometry) {
+            if (r_node.GetValue(UPPER_SURFACE) == 1) {
+                is_upper = true;
+            }
+            if (r_node.GetValue(LOWER_SURFACE) == 1) {
+                is_lower = true;
+            }
+            if (r_node.GetValue(WAKE) == 1) {
+                is_wake = true;
+            }
+        }
+
+        bool is_struct = r_elem.Is(STRUCTURE);
+        if (is_struct){
+            if ((is_upper && is_lower) or is_wake) {
+                for (auto& r_node : r_geometry) {
+                    r_node.SetValue(KUTTA, true);
+                }
+            } else {
+                r_elem.Set(STRUCTURE, false);
+                // r_elem.Set(MARKER, true);
+                r_elem.SetValue(WAKE, false);
+                if (is_lower) {
+                    r_elem.SetValue(WAKE, false);
+                    r_elem.Set(STRUCTURE, false);
+                    r_elem.SetValue(KUTTA, true);
+                    for (auto& r_node : r_geometry) {
+                        if (r_node.GetValue(WAKE_DISTANCE) > 0.0) {
+                            r_node.SetValue(TRAILING_EDGE, true);
+                        }
+                    }
+                }
+            }
+        } else {
+            // bool is_x = r_elem.GetGeometry().Center().X() > 0.0;
+            if (is_upper && is_lower) {
+                bool is_neighbour = false;
+                for (auto& r_node : r_geometry) {
+                    for (auto& r_elem_neigh : r_node.GetValue(NEIGHBOUR_ELEMENTS)) {
+                        if (r_elem_neigh.Is(STRUCTURE)) {
+                            is_neighbour = true;
+                            break;
+                        }
+                    }
+                }
+                if (is_neighbour) {
+                    r_elem.Set(MARKER, true);
+                    // r_elem.SetValue(WAKE, false);
+                    // r_elem.Set(STRUCTURE, false);
+                    // r_elem.SetValue(KUTTA, true);
+                    // for (auto& r_node : r_geometry) {
+                    //     if (r_node.GetValue(WAKE_DISTANCE) > 0.0) {
+                    //         r_node.SetValue(TRAILING_EDGE, true);
+                    //     }
+                    // }
+                }
+
+            }
+        }
+    }
+
     ComputeTrailingEdgeNode();
+
+    // for (int i = 0; i < static_cast<int>(mrModelPart.Elements().size()); i++) {
+    //     ModelPart::ElementIterator it_elem = mrModelPart.ElementsBegin() + i;
+    //     auto& r_geometry = it_elem->GetGeometry();
+
+
+    //     BoundedVector<double,4> geometry_distances;
+    //     for(unsigned int i_node = 0; i_node<r_geometry.size(); i_node++){
+    //         geometry_distances[i_node] = r_geometry[i_node].FastGetSolutionStepValue(GEOMETRY_DISTANCE);
+    //     }
+    //     const bool is_embedded = PotentialFlowUtilities::CheckIfElementIsCutByDistance<3,4>(geometry_distances);
+    //     bool is_touching_marker = false;
+    //     if (is_embedded){
+    //         for (auto& r_node : r_geometry) {
+    //             if (r_node.GetValue(KUTTA) == 1) {
+    //                 // std::cout << "Found embedded elem " << it_elem->Id() << " touching marker" << std::endl;
+    //                 is_touching_marker = true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     if (is_touching_marker) {
+    //         for (auto& r_node : r_geometry) {
+    //             r_node.SetValue(KUTTA, true);
+    //         }
+    //     }
+    // }
+
+    std::ofstream outfile_wake;
+    outfile_wake.open("wake_elements_id.txt");
+    std::ofstream outfile_marker;
+    outfile_wake.open("marker_elements_id.txt");
+    std::ofstream outfile_structure;
+    outfile_structure.open("structure_elements_id.txt");
+    std::ofstream outfile_kutta;
+    outfile_kutta.open("kutta_elements_id.txt");
+    for (auto& r_element : mrModelPart.Elements()){
+        if(r_element.Is(MARKER)){
+            outfile_marker << r_element.Id();
+            outfile_marker << " ";
+        }
+        if(!r_element.GetValue(WAKE)){
+            if(r_element.GetValue(KUTTA)){
+                outfile_kutta << r_element.Id();
+                outfile_kutta << " ";
+            }
+        }
+        else{
+            outfile_wake << r_element.Id();
+            outfile_wake << " ";
+            if(r_element.Is(STRUCTURE)){
+                outfile_structure << r_element.Id();
+                outfile_structure << " ";
+            }
+        }
+    }
+    outfile_kutta.close();
+    outfile_marker.close();
+    outfile_structure.close();
+    outfile_wake.close();
 
     KRATOS_CATCH("");
 }
@@ -51,6 +223,7 @@ void DefineEmbeddedWakeProcess3D::ExecuteInitialize() {
     block_for_each(mrModelPart.Nodes(), [&](ModelPart::NodeType& rNode) {
         rNode.SetValue(WAKE_DISTANCE, 0.0);
         rNode.SetValue(WAKE, false);
+        rNode.SetValue(TRAILING_EDGE, false);
         rNode.SetValue(KUTTA, false);
     });
 
@@ -78,13 +251,23 @@ void DefineEmbeddedWakeProcess3D::MarkWakeElements(){
         rElem.SetValue(WAKE_ELEMENTAL_DISTANCES, nodal_distances_to_wake);
 
         // Selecting the cut (wake) elements
-        const bool is_wake_element = PotentialFlowUtilities::CheckIfElementIsCutByDistance<2,3>(nodal_distances_to_wake);
+        const bool is_wake_element = PotentialFlowUtilities::CheckIfElementIsCutByDistance<3,4>(nodal_distances_to_wake);
 
         BoundedVector<double, 4> geometry_distances;
         for(unsigned int i_node = 0; i_node< r_geometry.size(); i_node++){
             geometry_distances[i_node] = r_geometry[i_node].GetSolutionStepValue(GEOMETRY_DISTANCE);
         }
         const bool is_embedded = PotentialFlowUtilities::CheckIfElementIsCutByDistance<3,4>(geometry_distances);
+
+        if (is_embedded){
+            Tetrahedra3D4ModifiedShapeFunctions tetrehedra_shape_func(rElem.pGetGeometry(), Vector(geometry_distances));
+            // Computing Normal
+            std::vector<Vector> cut_normal;
+            tetrehedra_shape_func.ComputePositiveSideInterfaceAreaNormals(cut_normal,GeometryData::GI_GAUSS_1);
+            double norm_normal = sqrt(inner_prod(cut_normal[0],cut_normal[0]));
+            array_1d<double,3> unit_normal = cut_normal[0]/norm_normal;
+            rElem.SetValue(VELOCITY_LOWER,unit_normal);
+        }
 
         // Mark wake element and save their nodal distances to the wake
         if (is_wake_element && rElem.Is(ACTIVE)) {
@@ -93,7 +276,7 @@ void DefineEmbeddedWakeProcess3D::MarkWakeElements(){
                 rElem.Set(STRUCTURE, true);
                 for (unsigned int i = 0; i < r_geometry.size(); i++) {
                     r_geometry[i].SetLock();
-                    r_geometry[i].SetValue(KUTTA, true);
+                    // r_geometry[i].SetValue(KUTTA, true);
                     r_geometry[i].SetValue(WAKE_DISTANCE, nodal_distances_to_wake(i));
                     r_geometry[i].UnSetLock();
                 }
@@ -137,17 +320,22 @@ void DefineEmbeddedWakeProcess3D::ComputeTrailingEdgeNode(){
 
     std::vector<std::size_t> trailing_edge_node_list;
 
+    // for (auto& r_node : mrModelPart.Nodes()) {
+    //     bool is_positive = r_node.GetValue(WAKE_DISTANCE) > 0.0;
+    //     bool is_wake = r_node.GetValue(WAKE);
+    //     bool is_kutta = r_node.GetValue(KUTTA);
+    //     if (is_positive && is_wake && is_kutta) {
+    //         r_node.SetValue(TRAILING_EDGE, true);
+    //         // return &r_node;
+    //         trailing_edge_node_list.push_back(r_node.Id());
+    //     }
+    // }
     for (auto& r_node : mrModelPart.Nodes()) {
-        bool is_positive = r_node.GetValue(WAKE_DISTANCE) > 0.0;
-        bool is_wake = r_node.GetValue(WAKE);
-        bool is_kutta = r_node.GetValue(KUTTA);
-        if (is_positive && is_wake && is_kutta) {
-            r_node.SetValue(TRAILING_EDGE, true);
-            // return &r_node;
+        bool is_te = r_node.GetValue(TRAILING_EDGE);
+        if (is_te) {
             trailing_edge_node_list.push_back(r_node.Id());
         }
     }
-
     if (mrModelPart.HasSubModelPart("trailing_edge_sub_model_part")){
         mrModelPart.RemoveSubModelPart("trailing_edge_sub_model_part");
     }
--- applications/CompressiblePotentialFlowApplication/python_scripts/define_embedded_wake_process_3d.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/define_embedded_wake_process_3d.py
@@ -1,7 +1,9 @@
 import KratosMultiphysics
 import KratosMultiphysics.CompressiblePotentialFlowApplication as CPFApp
 import time
+import KratosMultiphysics.MeshingApplication as MeshingApplication
 import math
+from KratosMultiphysics.gid_output_process import GiDOutputProcess
 
 
 def Factory(settings, Model):
@@ -19,14 +21,23 @@ class DefineEmbeddedWakeProcess(KratosMultiphysics.Process):
         # Check default settings
         default_settings = KratosMultiphysics.Parameters(r'''{
             "model_part_name": "",
-            "epsilon": 1e-9
+            "epsilon": 1e-9,
+            "refinement_iterations" : 0,
+            "target_h_wake" : 1.0
         }''')
         settings.ValidateAndAssignDefaults(default_settings)
+
+        self.model = Model
 
         self.main_model_part = Model[settings["model_part_name"].GetString()].GetRootModelPart()
         self.wake_model_part = Model.CreateModelPart("wake")
         # self.plane_model_part = Model.CreateModelPart("plane")
         # self.output_model_part = Model.CreateModelPart("output")
+        self.wake_normal = KratosMultiphysics.Vector(3, 0.0)
+        self.wake_normal[2] = 1.0
+        self.main_model_part.ProcessInfo.SetValue(CPFApp.WAKE_NORMAL,self.wake_normal)
+        self.refinement_iterations = settings["refinement_iterations"].GetInt()
+        self.target_h_wake = settings["target_h_wake"].GetDouble()
 
         self.epsilon = settings["epsilon"].GetDouble()
 
@@ -38,26 +49,162 @@ class DefineEmbeddedWakeProcess(KratosMultiphysics.Process):
         # self._MoveAndRotateWake()
         # Executing define wake process
         # KratosMultiphysics.CalculateDiscontinuousDistanceToSkinProcess3D(self.main_model_part, self.wake_model_part).Execute()
+        # Find nodal neigbours util call
+        KratosMultiphysics.FindGlobalNodalElementalNeighboursProcess(self.main_model_part).Execute()
+
         CPFApp.DefineEmbeddedWakeProcess3D(self.main_model_part, self.wake_model_part).Execute()
 
+        # self.skin_model_part = self.model.GetModelPart("skin")
+
+        for i in range(self.refinement_iterations):
+            find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(self.main_model_part)
+            find_nodal_h.Execute()
+
+
+            for node in self.main_model_part.Nodes:
+                node.Set(KratosMultiphysics.BLOCKED)
+                this_h = node.GetValue(KratosMultiphysics.NODAL_H)
+                tensor = KratosMultiphysics.Vector(6, 0.0)
+                tensor[0] = 1e-4
+                tensor[1] = 1e-4
+                tensor[2] = 1e-4
+                node.SetValue(MeshingApplication.METRIC_TENSOR_3D, tensor)
+
+            for elem in self.main_model_part.Elements:
+                if elem.GetValue(CPFApp.WAKE):
+                    for node in elem.GetNodes():
+                        node.Set(KratosMultiphysics.BLOCKED, False)
+                        tensor = KratosMultiphysics.Vector(6, 0.0)
+                        tensor[0] = 1/self.target_h_wake/self.target_h_wake
+                        tensor[1] = 1/self.target_h_wake/self.target_h_wake
+                        tensor[2] = 1/self.target_h_wake/self.target_h_wake
+                        old_tensor = node.GetValue(MeshingApplication.METRIC_TENSOR_3D)
+                        if old_tensor.norm_2() < tensor.norm_2():
+                            node.SetValue(MeshingApplication.METRIC_TENSOR_3D, tensor)
+
+            mmg_parameters = KratosMultiphysics.Parameters("""
+            {
+                "discretization_type"              : "STANDARD",
+                "save_external_files"              : false,
+                "initialize_entities"              : false,
+                "preserve_flags"                   : false,
+                "interpolate_nodal_values"                   : false,
+                "echo_level"                       : 0
+            }
+            """)
+
+            mmg_process =MeshingApplication.MmgProcess3D(self.main_model_part, mmg_parameters)
+            mmg_process.Execute()
+
+            self._CalculateDistance()
+            self._ModifyFinalDistance()
+
+            KratosMultiphysics.FindGlobalNodalElementalNeighboursProcess(self.main_model_part).Execute()
+
+            CPFApp.DefineEmbeddedWakeProcess3D(self.main_model_part, self.wake_model_part).Execute()
+
+            # self.target_h_wake = self.target_h_wake*0.5
+
+
         KratosMultiphysics.Logger.PrintInfo('EmbeddedWake','Wake computation time: ',time.time()-ini_time)
+        model_part_to_write=self.main_model_part
+        KratosMultiphysics.ModelPartIO('./Meshes/after_wake_remeshed', KratosMultiphysics.IO.WRITE | KratosMultiphysics.IO.MESH_ONLY).WriteModelPart(model_part_to_write)
+
+        gid_output = GiDOutputProcess(self.main_model_part,
+                                    "after_wake_process",
+                                    KratosMultiphysics.Parameters("""
+                                        {
+                                            "result_file_configuration" : {
+                                                "gidpost_flags": {
+                                                    "GiDPostMode": "GiD_PostAscii",
+                                                    "WriteDeformedMeshFlag": "WriteUndeformed",
+                                                    "WriteConditionsFlag": "WriteConditions",
+                                                    "MultiFileFlag": "SingleFile"
+                                                },
+                                            "nodal_results"       : ["VELOCITY_POTENTIAL","AUXILIARY_VELOCITY_POTENTIAL","GEOMETRY_DISTANCE"],
+                                            "nodal_nonhistorical_results": ["TRAILING_EDGE","WAKE_DISTANCE","LOWER_SURFACE","UPPER_SURFACE","TEMPERATURE","KUTTA","WAKE"],
+                                            "gauss_point_results" : ["PRESSURE_COEFFICIENT","VELOCITY","WAKE","KUTTA","DENSITY","VELOCITY_LOWER", "LOWER_WAKE"],
+                                            "nodal_flags_results": ["MARKER"],
+                                            "elemental_conditional_flags_results": ["TO_SPLIT","STRUCTURE","BOUNDARY","SOLID","MARKER"]
+                                            }
+                                        }
+                                        """)
+                                    )
+
+        gid_output.ExecuteInitialize()
+        gid_output.ExecuteBeforeSolutionLoop()
+        gid_output.ExecuteInitializeSolutionStep()
+        gid_output.PrintOutput()
+        gid_output.ExecuteFinalizeSolutionStep()
+        gid_output.ExecuteFinalize()
+
+
+        # for elem in self.main_model_part.Elements:
+        #     for node in elem.GetNodes():
+        #         if node.Id == 120883:
+        #             print("ID WAKE", elem.Id, elem.GetValue(CPFApp.WAKE))
+
+        # print("ELEM IDS")
+        # for elem in self.main_model_part.Elements:
+        #     for node in elem.GetNodes():
+        #         if node.Id == 120883:
+        #             print(elem.Id, end=" ")
+        # print("FINISHED IDS")
+
+
+    def _CalculateDistance(self):
+        ''' This function calculate the distance to skin for every node in the main_model_part.'''
+        ini_time=time.time()
+        KratosMultiphysics.CalculateDistanceToSkinProcess3D(self.main_model_part, self.skin_model_part, 1e-16).Execute()
+        KratosMultiphysics.Logger.PrintInfo('WakeProcess3D','CalculateDistance time: ',time.time()-ini_time)
+
+    def _ModifyFinalDistance(self):
+        ''' This function modifies the distance field to avoid ill defined cuts.
+        '''
+        distance_modification_parameters = KratosMultiphysics.Parameters("""{
+                "distance_threshold"                          : 0.001,
+                "check_at_each_time_step"                     : true,
+                "avoid_almost_empty_elements"                 : true,
+                "deactivate_full_negative_elements"           : true,
+                "full_negative_elements_fixed_variables_list" : ["VELOCITY_POTENTIAL","AUXILIARY_VELOCITY_POTENTIAL"]
+            }
+        """)
+
+        ini_time = time.time()
+        KratosMultiphysics.FluidDynamicsApplication.DistanceModificationProcess(self.main_model_part,distance_modification_parameters).Execute()
+        KratosMultiphysics.Logger.PrintInfo('LevelSetRemeshing','Modify distance time: ',time.time()-ini_time)
+        KratosMultiphysics.VariableUtils().CopyScalarVar(KratosMultiphysics.DISTANCE,CPFApp.GEOMETRY_DISTANCE, self.main_model_part.Nodes)
+        KratosMultiphysics.VariableUtils().SetHistoricalVariableToZero(KratosMultiphysics.DISTANCE, self.main_model_part.Nodes)
 
     def _DefineWakeModelPart(self):
         ''' This function generates the modelpart of the wake. TODO: make end of the domain user-definable.
         '''
-        self.wake_model_part.CreateNewNode(1, 0.0, 0.0, 0.0)
+        self.wake_model_part.CreateNewNode(1, 0.0, -0.01, 0.0)
         self.wake_model_part.CreateNewNode(2, 0.0, 2.0, 0.0)
 
-        self.wake_model_part.CreateNewNode(3, 200.0*math.cos(math.radians(-5.0)), 0.5, 200.0*math.sin(math.radians(-5.0)))
-        self.wake_model_part.CreateNewNode(4, 200.0*math.cos(math.radians(-5.0)), 0.0, 200.0*math.sin(math.radians(-5.0)))
+        # self.wake_model_part.CreateNewNode(3, 200.0*math.cos(math.radians(-5.0)), 2.0, 200.0*math.sin(math.radians(-5.0)))
+        # self.wake_model_part.CreateNewNode(4, 200.0*math.cos(math.radians(-5.0)), 0.0, 200.0*math.sin(math.radians(-5.0)))
+        # self.wake_model_part.CreateNewElement("Element3D3N", 1, [1,3,2], KratosMultiphysics.Properties(0))
+        # self.wake_model_part.CreateNewElement("Element3D3N", 2, [1,4,3], KratosMultiphysics.Properties(0))
 
-        # self.wake_model_part.CreateNewNode(1, 0.4980874, -0.1, -0.04357787)
-        # self.wake_model_part.CreateNewNode(2, 0.4980874, 0.6, -0.04357787)
-        # self.wake_model_part.CreateNewNode(3, 200.0, 0.6, -0.04357787)
-        # self.wake_model_part.CreateNewNode(4, 200.0, -0.1, -0.04357787)
+        self.wake_model_part.CreateNewNode(3, 0.498097, 2.0, -0.0435779)
+        self.wake_model_part.CreateNewNode(4, 0.498097, -0.01, -0.0435779)
+        self.wake_model_part.CreateNewNode(5, 200.0, 2.0, 0.0)
+        self.wake_model_part.CreateNewNode(6, 200.0, 0.0, 0.0)
 
         self.wake_model_part.CreateNewElement("Element3D3N", 1, [1,3,2], KratosMultiphysics.Properties(0))
         self.wake_model_part.CreateNewElement("Element3D3N", 2, [1,4,3], KratosMultiphysics.Properties(0))
+        self.wake_model_part.CreateNewElement("Element3D3N", 3, [4,5,3], KratosMultiphysics.Properties(0))
+        self.wake_model_part.CreateNewElement("Element3D3N", 4, [4,6,5], KratosMultiphysics.Properties(0))
+
+
+        # self.wake_model_part.CreateNewNode(1, 0.498097, 2.0, -0.0435779)
+        # self.wake_model_part.CreateNewNode(2, 0.498097, -0.01, -0.0435779)
+        # self.wake_model_part.CreateNewNode(3, 200.0, 2.0, 0.0)
+        # self.wake_model_part.CreateNewNode(4, 200.0, 0.0, 0.0)
+
+        # self.wake_model_part.CreateNewElement("Element3D3N", 1, [1,3,2], KratosMultiphysics.Properties(0))
+        # self.wake_model_part.CreateNewElement("Element3D3N", 2, [1,4,3], KratosMultiphysics.Properties(0))
 
     def _MoveAndRotateWake(self):
         ''' This function moves and rotates the wake with the same parameters as the geometry.
