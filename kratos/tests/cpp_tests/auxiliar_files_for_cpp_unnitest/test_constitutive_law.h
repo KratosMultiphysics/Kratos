307285e63857d7f84dd765084e2220f3bb3cc717
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2021-09-10 12:14:46 +0200

    Fix test constitutive law

--- kratos/tests/cpp_tests/auxiliar_files_for_cpp_unnitest/test_constitutive_law.h
+++ kratos/tests/cpp_tests/auxiliar_files_for_cpp_unnitest/test_constitutive_law.h
@@ -306,7 +306,7 @@ public:
         const Properties& rMaterialProperties,
         const GeometryType& rElementGeometry,
         const ProcessInfo& rCurrentProcessInfo
-        ) override;
+        ) const override;
 
 protected:
 
