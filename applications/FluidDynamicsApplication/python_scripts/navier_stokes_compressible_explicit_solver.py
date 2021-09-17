17ad9a3f48146cf807a3193e92e59a72a98ddfd8
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-09-17 11:51:33 +0200

    Enforcing code style

--- applications/FluidDynamicsApplication/python_scripts/navier_stokes_compressible_explicit_solver.py
+++ applications/FluidDynamicsApplication/python_scripts/navier_stokes_compressible_explicit_solver.py
@@ -136,14 +136,13 @@ class NavierStokesCompressibleExplicitSolver(FluidSolver):
         self.settings["time_stepping"]["consider_compressibility_in_CFL"] == True
         ```
         """
-
         if self.settings["time_stepping"].Has("consider_compressibility_in_CFL"):
             KratosMultiphysics.Logger.PrintWarning("", "User-specifed consider_compressibility_in_CFL will be overriden with TRUE")
         else:
             self.settings["time_stepping"].AddEmptyValue("consider_compressibility_in_CFL")
 
         self.settings["time_stepping"]["consider_compressibility_in_CFL"].SetBool(True)
-        
+
         estimate_dt_utility = KratosFluid.EstimateDtUtility(
                 self.GetComputingModelPart(),
                 self.settings["time_stepping"])
