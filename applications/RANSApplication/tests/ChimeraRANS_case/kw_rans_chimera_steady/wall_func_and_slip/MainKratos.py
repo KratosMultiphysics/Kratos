014df995f83c0e20223029c491eb0c00a35feecd
RahulKN <rahul.melkote@gmail.com>
2021-09-27 14:14:54 +0200

    testcase

---
+++ applications/RANSApplication/tests/ChimeraRANS_case/kw_rans_chimera_steady/wall_func_and_slip/MainKratos.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+import KratosMultiphysics
+from KratosMultiphysics.FluidDynamicsApplication.fluid_dynamics_analysis import FluidDynamicsAnalysis
+import KratosMultiphysics.ChimeraApplication as KratosChimera
+import KratosMultiphysics.ChimeraApplication.python_solvers_wrapper_fluid_chimera
+from KratosMultiphysics.ChimeraApplication.fluid_chimera_analysis import FluidChimeraAnalysis
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) > 2:
+        err_msg =  'Too many input arguments!\n'
+        err_msg += 'Use this script in the following way:\n'
+        err_msg += '- With default parameter file (assumed to be called "ProjectParameters.json"):\n'
+        err_msg += '    "python fluid_chimera_analysis.py"\n'
+        err_msg += '- With custom parameter file:\n'
+        err_msg += '    "python fluid_chimera_analysis.py <my-parameter-file>.json"\n'
+        raise Exception(err_msg)
+
+    if len(argv) == 2: # ProjectParameters is being passed from outside
+        parameter_file_name = argv[1]
+    else: # using default name
+        parameter_file_name = "ProjectParameters_ChimeraRANS.json"
+
+    with open(parameter_file_name,'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = FluidChimeraAnalysis(model,parameters)
+    simulation.Run()
