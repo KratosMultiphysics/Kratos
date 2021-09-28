0781c59666deb2966141de3f5f9e66d78e3c4801
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-09-28 12:41:30 +0200

    Codacy is complaining

--- applications/FluidDynamicsApplication/tests/test_apply_mach_dependent_boundary_conditions.py
+++ applications/FluidDynamicsApplication/tests/test_apply_mach_dependent_boundary_conditions.py
@@ -377,7 +377,7 @@ class ApplyMachDependentBoundaryConditionsTest(UnitTest.TestCase):
         self.assertIn("VELOCITY", str(context.exception))
 
     def testErrorTooManyValues(self):
-        "This error is particularly important because it prevents possible segmentation faults."
+        """This error is particularly important because it prevents possible segmentation faults."""
         settings = KratosMultiphysics.Parameters("""
         {
             "Parameters" : {
@@ -401,9 +401,9 @@ class ApplyMachDependentBoundaryConditionsTest(UnitTest.TestCase):
         self.assertIn("Allowed vector variables are at most 3-dimensional", str(context.exception))
         self.assertIn("VELOCITY", str(context.exception))
 
-    
+
     def testSameVariable(self):
-        "Fixing all axes in subsonic, but only VELOCITY_Z in supersonic."
+        """Fixing all axes in subsonic, but only VELOCITY_Z in supersonic."""
         settings = KratosMultiphysics.Parameters("""
         {
             "Parameters" : {
