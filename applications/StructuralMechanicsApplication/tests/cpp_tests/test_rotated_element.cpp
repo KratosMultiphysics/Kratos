d63e9aebc212f6ebd8d823f871ae410dce20bca4
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-09-30 17:37:56 +0200

    adding for cylindrical rotation

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_rotated_element.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_rotated_element.cpp
@@ -117,7 +117,103 @@ namespace Testing
         KRATOS_CHECK_VECTOR_EQUAL(r_computed_local_axis_2, local_axis_2);
     }
 
+    KRATOS_TEST_CASE_IN_SUITE(RotatedElementCylindrical2D3N, KratosStructuralMechanicsFastSuite)
+    {
+        Model current_model;
+        auto &r_model_part = current_model.CreateModelPart("ModelPart",1);
+        r_model_part.GetProcessInfo().SetValue(DOMAIN_SIZE, 2);
 
+        r_model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
+        r_model_part.AddNodalSolutionStepVariable(VOLUME_ACCELERATION);
+
+        // Set the element properties
+        auto p_elem_prop = r_model_part.CreateNewProperties(0);
+        p_elem_prop->SetValue(YOUNG_MODULUS, 2.0e+06);
+        p_elem_prop->SetValue(POISSON_RATIO, 0.3);
+        p_elem_prop->SetValue(THICKNESS, 0.01);
+        const auto &r_clone_cl = KratosComponents<ConstitutiveLaw>::Get("LinearElasticPlaneStress2DLaw");
+        p_elem_prop->SetValue(CONSTITUTIVE_LAW, r_clone_cl.Clone());
+
+        // Create the test element
+        auto p_node_1 = r_model_part.CreateNewNode(1, 0.0 , 0.0 , 0.0);
+        auto p_node_2 = r_model_part.CreateNewNode(2, 1.0 , 0.0 , 0.0);
+        auto p_node_3 = r_model_part.CreateNewNode(3, 0.0 , 1.0 , 0.0);
+
+        for (auto& r_node : r_model_part.Nodes()) {
+            r_node.AddDof(DISPLACEMENT_X);
+            r_node.AddDof(DISPLACEMENT_Y);
+            r_node.AddDof(DISPLACEMENT_Z);
+        }
+
+        std::vector<ModelPart::IndexType> element_nodes {1,2,3};
+        auto p_element = r_model_part.CreateNewElement("SmallDisplacementElement2D3N", 1, element_nodes, p_elem_prop);
+
+        // Now we create the rotation process
+        Parameters parameters = Parameters(R"(
+        {
+            "cylindrical_generatrix_axis"   : [0.0,0.0,1.0],
+            "cylindrical_generatrix_point"  : [0.0,0.0,0.0]
+        })");
+        auto &r_orientation_process = SetCylindricalLocalAxesProcess(r_model_part, parameters);
+        r_orientation_process.ExecuteInitialize();
+
+        array_1d<double, 3> local_axis_1 = ZeroVector(3);
+        local_axis_1[0] = 0.707107;
+        local_axis_1[1] = 0.707107;
+        const array_1d<double, 3>& r_computed_local_axis_1 = p_element->GetValue(LOCAL_AXIS_1);
+        KRATOS_CHECK_VECTOR_NEAR(r_computed_local_axis_1, local_axis_1, 1.0e-4);
+    }
+
+    KRATOS_TEST_CASE_IN_SUITE(RotatedElementCylindrical3D4N, KratosStructuralMechanicsFastSuite)
+    {
+        Model current_model;
+        auto &r_model_part = current_model.CreateModelPart("ModelPart",1);
+        r_model_part.GetProcessInfo().SetValue(DOMAIN_SIZE, 3);
+
+        r_model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
+        r_model_part.AddNodalSolutionStepVariable(VOLUME_ACCELERATION);
+
+        // Set the element properties
+        auto p_elem_prop = r_model_part.CreateNewProperties(0);
+        p_elem_prop->SetValue(YOUNG_MODULUS, 2.0e+06);
+        p_elem_prop->SetValue(POISSON_RATIO, 0.3);
+        const auto &r_clone_cl = KratosComponents<ConstitutiveLaw>::Get("LinearElastic3DLaw");
+        p_elem_prop->SetValue(CONSTITUTIVE_LAW, r_clone_cl.Clone());
+
+        // Create the test element
+        auto p_node_1 = r_model_part.CreateNewNode(1, 0.0 , 0.0 , 0.0);
+        auto p_node_2 = r_model_part.CreateNewNode(2, 1.0 , 0.0 , 0.0);
+        auto p_node_3 = r_model_part.CreateNewNode(3, 0.0 , 1.0 , 0.0);
+        auto p_node_4 = r_model_part.CreateNewNode(4, 0.0 , 0.0 , 1.0);
+
+        for (auto& r_node : r_model_part.Nodes()) {
+            r_node.AddDof(DISPLACEMENT_X);
+            r_node.AddDof(DISPLACEMENT_Y);
+            r_node.AddDof(DISPLACEMENT_Z);
+        }
+
+        std::vector<ModelPart::IndexType> element_nodes {1,2,3,4};
+        auto p_element = r_model_part.CreateNewElement("SmallDisplacementElement3D4N", 1, element_nodes, p_elem_prop);
+
+        // Now we create the rotation process
+        Parameters parameters = Parameters(R"(
+        {
+            "cylindrical_generatrix_axis"   : [0.0,0.0,1.0],
+            "cylindrical_generatrix_point"  : [0.0,0.0,0.0]
+        })");
+        auto &r_orientation_process = SetCylindricalLocalAxesProcess(r_model_part, parameters);
+        r_orientation_process.ExecuteInitialize();
+
+        array_1d<double, 3> local_axis_1 = ZeroVector(3);
+        array_1d<double, 3> local_axis_2 = ZeroVector(3);
+        local_axis_1[0] = 0.707107;
+        local_axis_1[1] = 0.707107;
+        local_axis_2[2] = 1.0;
+        const array_1d<double, 3>& r_computed_local_axis_1 = p_element->GetValue(LOCAL_AXIS_1);
+        const array_1d<double, 3>& r_computed_local_axis_2 = p_element->GetValue(LOCAL_AXIS_2);
+        KRATOS_CHECK_VECTOR_NEAR(r_computed_local_axis_1, local_axis_1, 1.0E-4);
+        KRATOS_CHECK_VECTOR_NEAR(r_computed_local_axis_2, local_axis_2, 1.0E-4);
+    }
 
 }
 }
