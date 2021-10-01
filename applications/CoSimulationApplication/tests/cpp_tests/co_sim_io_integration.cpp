09513fe3357544fdf4dbf06ff33baabf83f7e439
Philipp <philipp.bucher@tum.de>
2021-10-01 11:41:40 +0200

    started with tests

--- applications/CoSimulationApplication/tests/cpp_tests/co_sim_io_integration.cpp
+++ applications/CoSimulationApplication/tests/cpp_tests/co_sim_io_integration.cpp
@@ -24,19 +24,49 @@
 namespace Kratos {
 namespace Testing {
 
-KRATOS_TEST_CASE_IN_SUITE(CoSimIOModelPartToKratosModelPart, KratosCosimulationFastSuite)
+KRATOS_TEST_CASE_IN_SUITE(CoSimIOModelPartToKratosModelPart_NodesOnly, KratosCosimulationFastSuite)
 {
     Model model;
     auto& kratos_model_part = model.CreateModelPart("kratos_mp");
 
     CoSimIO::ModelPart co_sim_io_model_part("co_sim_io_mp");
 
+    constexpr std::size_t num_nodes = 5;
+
+    for (std::size_t i=0; i<num_nodes; ++i) {
+        co_sim_io_model_part.CreateNewNode(i+1, i*1.5, i+3.5, i-8.6);
+    }
+
+    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfNodes(), num_nodes);
+    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfLocalNodes(), num_nodes);
+    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfGhostNodes(), 0);
+    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfElements(), 0);
+
     CoSimIOConversionUtilities::CoSimIOModelPartToKratosModelPart(co_sim_io_model_part, kratos_model_part);
 
+    KRATOS_CHECK_EQUAL(kratos_model_part.NumberOfNodes(), num_nodes);
+    KRATOS_CHECK_EQUAL(kratos_model_part.NumberOfElements(), 0);
 
+    const auto nodes_begin = kratos_model_part.NodesBegin();
+
+    for (std::size_t i=0; i<num_nodes; ++i) {
+        const std::size_t exp_id = i+1;
+
+        const auto& r_node = *(nodes_begin+i);
+
+        const auto& r_co_sim_io_node = co_sim_io_model_part.GetNode(exp_id);
+
+        KRATOS_CHECK_EQUAL(r_node.Id(), exp_id);
+        KRATOS_CHECK_DOUBLE_EQUAL(r_node.X(), r_co_sim_io_node.X());
+        KRATOS_CHECK_DOUBLE_EQUAL(r_node.X0(), r_co_sim_io_node.X());
+        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Y(), r_co_sim_io_node.Y());
+        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Y0(), r_co_sim_io_node.Y());
+        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Z(), r_co_sim_io_node.Z());
+        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Z0(), r_co_sim_io_node.Z());
+    }
 }
 
-KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart, KratosCosimulationFastSuite)
+KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart_NodesOnly, KratosCosimulationFastSuite)
 {
     Model model;
     auto& kratos_model_part = model.CreateModelPart("kratos_mp");
@@ -46,6 +76,17 @@ KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart, KratosCosimulationF
     CoSimIOConversionUtilities::KratosModelPartToCoSimIOModelPart(kratos_model_part, co_sim_io_model_part);
 
 }
+
+KRATOS_TEST_CASE_IN_SUITE(CoSimIOModelPartToKratosModelPart, KratosCosimulationFastSuite)
+{
+
+}
+
+KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart_NodesOnly, KratosCosimulationFastSuite)
+{
+
+}
+
 
 
 } // namespace Testing
