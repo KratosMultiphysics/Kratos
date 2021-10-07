8e088c9e756bc5a74c259703330e273f6f228f75
EleniSmyrniou <eleni.smyrniou@deltares.nl>
2021-10-07 15:29:21 +0200

    PIPING - 9 - test updated that generates documentation files

--- applications/GeoMechanicsApplication/tests/test_steady_state_groundwater_flow.py
+++ applications/GeoMechanicsApplication/tests/test_steady_state_groundwater_flow.py
@@ -56,26 +56,33 @@ class KratosGeoMechanicsSteadyStateGroundWaterFlowTests(KratosUnittest.TestCase)
         self.assert_outflow_discharge(simulation, 2)
 
     def test_pressure_in_confined_aquifer(self):
+        # run kratos model
         test_name = 'test_pressure_in_confined_aquifer'
-        analytical_solution_Q = 1e-6
-        analytical_solution_water_pressure = -127.12110773605288
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
-
-        self.assert_total_discharge(simulation, analytical_solution_Q)
+        # run test for outflow discharge
+        analytical_solution_outflow_discharge = 2.54028
+        outflow_discharge = self.calculate_outflow_discharge(simulation)
+        error_outflow_discharge = abs(outflow_discharge - analytical_solution_outflow_discharge) / \
+                                  (analytical_solution_outflow_discharge + 1e-60)
+        self.assertTrue(error_outflow_discharge < 0.03)
+        analytical_solution_water_pressure = -127.53
         # get water pressure
         water_pressure = test_helper.get_water_pressure(simulation)
         pore_pressure_middle = water_pressure[343]
-        self.assertAlmostEqual(analytical_solution_water_pressure, pore_pressure_middle)
-        output_file_for_latex = open(r"test_pressure_in_confined_aquifer.csv","w")
-        output_file_for_latex.write("water pressure" +
-                                    str(analytical_solution_water_pressure) +
-                                    str(pore_pressure_middle) +
-                                    str(abs((analytical_solution_water_pressure-pore_pressure_middle)/analytical_solution_water_pressure)*100))
-        output_file_for_latex.write("Q" +
-                                    str(analytical_solution_Q) +
-                                    str(abs(self.calculate_total_discharge(simulation))) +
-                                    str(abs((analytical_solution_Q-abs(self.calculate_total_discharge(simulation)))/analytical_solution_Q)*100))
+        error_water_pressure = abs(pore_pressure_middle - analytical_solution_water_pressure) / \
+                                  (abs(analytical_solution_water_pressure) + 1e-60)
+        self.assertTrue(error_outflow_discharge < 0.03)
+        output_file_for_latex = open(file_path + "\\test_pressure_in_confined_aquifer.tex","w")
+        output_file_for_latex.write(' & '.join(['water pressure',
+                                                str(round(analytical_solution_water_pressure,2)),
+                                                str(round(pore_pressure_middle,2)),
+                                                str(round(error_water_pressure*100,2))]) + ' \\\\ \hline \n')
+        output_file_for_latex.write(' & '.join(['Q',
+                                                str(round(analytical_solution_outflow_discharge,2)),
+                                                str(round(outflow_discharge,2)),
+                                                str(round(error_outflow_discharge*100,2))]) +
+                                                ' \\\\ \hline \n')
         output_file_for_latex.close()
 
     def calculate_outflow_discharge(self, simulation):
