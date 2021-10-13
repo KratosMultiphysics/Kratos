095839a510e298775d0547397c07a2184429362c
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2021-10-13 10:44:17 +0200

    Revert "Revert changes suggested by @rubenzorrilla"
    This reverts commit 8202aa70f16ed74df8016f37bafb2ee4372e093c.

--- kratos/modeler/serial_model_part_combinator_modeler.h
+++ kratos/modeler/serial_model_part_combinator_modeler.h
@@ -42,7 +42,22 @@ namespace Kratos
 ///@}
 ///@name Kratos Classes
 ///@{
-    
+
+namespace SingleImportModelPart
+{
+    /**
+     * @brief This method imports one single ModelPart
+     * @param rModelPart The model part where to read the input
+     * @param ModelPartImportParameters The model part import parameters
+     * @param InputType The input type
+     */
+    void KRATOS_API(KRATOS_CORE) Import(
+        ModelPart& rModelPart, 
+        Parameters ModelPartImportParameters,
+        const std::string& InputType
+        );
+}
+
 /**
  * @class SerialModelPartCombinatorModeler
  * @ingroup KratosCore
@@ -180,18 +195,6 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
-
-    /**
-     * @brief This method imports one single ModelPart
-     * @param rModelPart The model part where to read the input
-     * @param ModelPartImportParameters The model part import parameters
-     * @param InputType The input type
-     */
-    void SingleImportModelPart(
-        ModelPart& rModelPart, 
-        Parameters ModelPartImportParameters,
-        const std::string& InputType
-        );
 
     ///@}
     ///@name Private  Access
