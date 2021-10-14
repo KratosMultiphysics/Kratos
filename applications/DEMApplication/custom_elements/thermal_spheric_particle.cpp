dfe9eafd237c901a99a29ea24104dcaf918d1353
rlrangel <rrangel@cimne.upc.edu>
2021-10-14 16:42:53 +0200

    Bug fix

--- applications/DEMApplication/custom_elements/thermal_spheric_particle.cpp
+++ applications/DEMApplication/custom_elements/thermal_spheric_particle.cpp
@@ -577,7 +577,7 @@ namespace Kratos
     }
 
     // Compute heat flux
-    return h * GetNeighborTemperature() - GetParticleTemperature();
+    return h * (GetNeighborTemperature() - GetParticleTemperature());
 
     KRATOS_CATCH("")
   }
@@ -659,7 +659,7 @@ namespace Kratos
     }
 
     // Compute heat flux
-    return h * GetNeighborTemperature() - GetParticleTemperature();
+    return h * (GetNeighborTemperature() - GetParticleTemperature());
 
     KRATOS_CATCH("")
   }
