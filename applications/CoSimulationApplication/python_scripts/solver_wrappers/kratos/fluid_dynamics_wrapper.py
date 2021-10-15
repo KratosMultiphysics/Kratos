3978a3676c1d23ef405d105b13977472efff08e6
Philipp <philipp.bucher@tum.de>
2021-10-15 20:48:54 +0200

    started to adapt fluid_dyn_wrapper

--- applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/fluid_dynamics_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/solver_wrappers/kratos/fluid_dynamics_wrapper.py
@@ -1,4 +1,5 @@
 # Importing the Kratos Library
+import KratosMultiphysics as KM
 from KratosMultiphysics.kratos_utilities import CheckIfApplicationsAvailable
 
 # Importing the base class
@@ -9,11 +10,33 @@ if not CheckIfApplicationsAvailable("FluidDynamicsApplication"):
     raise ImportError("The FluidDynamicsApplication is not available!")
 from KratosMultiphysics.FluidDynamicsApplication.fluid_dynamics_analysis import FluidDynamicsAnalysis
 
+# Other imports
+from KratosMultiphysics.CoSimulationApplication.utilities import data_communicator_utilities
+
+
 def Create(settings, model, solver_name):
     return FluidDynamicsWrapper(settings, model, solver_name)
+
 
 class FluidDynamicsWrapper(kratos_base_wrapper.KratosBaseWrapper):
     """This class is the interface to the FluidDynamicsApplication of Kratos"""
 
     def _CreateAnalysisStage(self):
         return FluidDynamicsAnalysis(self.model, self.project_parameters)
+
+    def _GetDataCommunicator(self):
+        if not KM.IsDistributedRun():
+            return KM.ParallelEnvironment.GetDefaultDataCommunicator()
+
+        # now we know that Kratos runs in MPI
+        parallel_type = self.project_parameters["problem_data"]["parallel_type"].GetString()
+
+        # first check if the solver uses MPI
+        if parallel_type != "MPI":
+            return data_communicator_utilities.GetRankZeroDataCommunicator()
+
+        # now we know that the solver uses MPI, only question left is whether to use all ranks or a subset
+        if self.settings["solver_wrapper_settings"].Has("data_communicator_creation"):
+            return data_communicator_utilities.CreateDataCommunicatorWithNProcesses(self.settings["solver_wrapper_settings"]["data_communicator_creation"])
+        else:
+            return KM.ParallelEnvironment.GetDefaultDataCommunicator()
