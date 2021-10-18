0bc4a2bb789081f41c49e1bdfafeaad66831a95c
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-10-18 13:11:40 +0200

    add ray locator utility

---
+++ applications/ShallowWaterApplication/custom_utilities/find_intersected_objects_utility.cpp
@@ -0,0 +1,156 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "includes/model_part.h"
+#include "utilities/parallel_utilities.h"
+#include "find_intersected_objects_utility.h"
+
+namespace Kratos
+{
+
+/// Local Flags
+KRATOS_CREATE_LOCAL_FLAG(FindIntersectedObjectsUtility, INTERSECT_ELEMENTS,   0);
+KRATOS_CREATE_LOCAL_FLAG(FindIntersectedObjectsUtility, INTERSECT_CONDITIONS, 1);
+
+FindIntersectedObjectsUtility::FindIntersectedObjectsUtility(
+    ModelPart& rThisModelPart,
+    Parameters ThisParameters
+) : mrModelPart(rThisModelPart)
+{
+    Parameters default_parameters(R"({
+        "intersect_elements"   : true,
+        "intersect_conditions" : false
+    })");
+
+    // Set the flags
+    const bool intersect_elements = ThisParameters["intersect_elements"].GetBool();
+    const bool intersect_conditions = ThisParameters["intersect_conditions"].GetBool();
+
+    mOptions.Set(INTERSECT_ELEMENTS, intersect_elements);
+    mOptions.Set(INTERSECT_CONDITIONS, intersect_conditions);
+
+    if (mrModelPart.NumberOfNodes() > 0) {
+        GenerateOctree();
+    }
+}
+
+void FindIntersectedObjectsUtility::UpdateSearchStructure()
+{
+    if (mrModelPart.NumberOfNodes() > 0) {
+        GenerateOctree();
+    }
+}
+
+void FindIntersectedObjectsUtility::FindIntersectedObjects(
+    const GeometryType& rGeometry,
+    PointerVector<GeometricalObject>& rResults)
+{
+    OctreeCellVectorType leaves;
+    leaves.clear();
+    auto p_geometrical_object = Kratos::make_intrusive<GeometricalObject>(0, Kratos::make_shared<GeometryType>(rGeometry));
+    mpOctree->GetIntersectedLeaves(p_geometrical_object, leaves);
+    FindIntersectedObjects(rGeometry, leaves, rResults);
+}
+
+void FindIntersectedObjectsUtility::FindIntersectedObjects(
+    const GeometryType& rIntersectionGeometry,
+    OctreeCellVectorType& rLeaves,
+    PointerVector<GeometricalObject>& rResults)
+{
+    for (auto p_leaf : rLeaves) {
+        for (auto p_volume_object : *(p_leaf->pGetObjects())) {
+            if (p_volume_object->GetGeometry().HasIntersection(rIntersectionGeometry)) {
+                rResults.push_back(p_volume_object);
+            }
+        }
+    }
+}
+
+void FindIntersectedObjectsUtility::GenerateOctree()
+{
+    SetOctreeBoundingBox();
+
+    // Adding the volume model part to the octree
+    for (auto it_node = mrModelPart.NodesBegin(); it_node != mrModelPart.NodesEnd(); it_node++) {
+#ifdef KRATOS_USE_AMATRIX   // This macro definition is for the migration period and to be removed afterward please do not use it
+        mpOctree->Insert(it_node->Coordinates().data());
+#else
+        mpOctree->Insert(it_node->Coordinates().data().data());
+#endif // ifdef KRATOS_USE_AMATRIX
+    }
+
+    // Add elements
+    if (mOptions.Is(INTERSECT_ELEMENTS)) {
+        auto& r_elements_array = mrModelPart.Elements();
+        const auto it_elements_begin = r_elements_array.begin();
+        const auto number_of_elements = r_elements_array.size();
+
+        // Iterate over the elements
+        for (int i = 0; i < static_cast<int>(number_of_elements); i++) {
+            auto it_elements = it_elements_begin + i;
+            mpOctree->Insert(*(it_elements).base());
+        }
+    }
+
+    // Add conditions
+    if (mOptions.Is(INTERSECT_CONDITIONS)) {
+        auto& r_conditions_array = mrModelPart.Conditions();
+        const auto it_conditions_begin = r_conditions_array.begin();
+        const auto number_of_conditions = r_conditions_array.size();
+
+        // Iterate over the conditions
+        for (int i = 0; i < static_cast<int>(number_of_conditions); i++) {
+            auto it_conditions = it_conditions_begin + i;
+            mpOctree->Insert(*(it_conditions).base());
+        }
+    }
+}
+
+void FindIntersectedObjectsUtility::SetOctreeBoundingBox()
+{
+    PointType low(mrModelPart.NodesBegin()->Coordinates());
+    PointType high(mrModelPart.NodesBegin()->Coordinates());
+
+    // Loop over all nodes in the volume model part
+    for (auto it_node = mrModelPart.NodesBegin(); it_node != mrModelPart.NodesEnd(); it_node++) {
+        const array_1d<double,3>& r_coordinates = it_node->Coordinates();
+        for (IndexType i = 0; i < 3; i++) {
+            low[i] = r_coordinates[i] < low[i] ? r_coordinates[i] : low[i];
+            high[i] = r_coordinates[i] > high[i] ? r_coordinates[i] : high[i];
+        }
+    }
+
+    // Slightly increase the bounding box size to avoid problems with geometries in the borders
+    // Note that std::numeric_limits<double>::double() is added for the 2D cases. Otherwise, the
+    // third component will be 0, breaking the octree behaviour.
+    for(IndexType i = 0 ; i < 3; i++) {
+        low[i] -= std::abs(high[i] - low[i])*1e-3 + std::numeric_limits<double>::epsilon();
+        high[i] += std::abs(high[i] - low[i])*1e-3 + std::numeric_limits<double>::epsilon();
+    }
+
+    // TODO: Octree needs refactoring to work with BoundingBox. Pooyan.
+#ifdef KRATOS_USE_AMATRIX   // This macro definition is for the migration period and to be removed afterward please do not use it
+    mpOctree->SetBoundingBox(low.data(), high.data());
+#else
+    mpOctree->SetBoundingBox(low.data().data(), high.data().data());
+#endif // ifdef KRATOS_USE_AMATRIX
+}
+
+}  // namespace Kratos.
---
+++ applications/ShallowWaterApplication/custom_utilities/find_intersected_objects_utility.h
@@ -0,0 +1,229 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+
+#ifndef KRATOS_FIND_INTERSECTED_ELEMENTS_UTILITY_H_INCLUDED
+#define KRATOS_FIND_INTERSECTED_ELEMENTS_UTILITY_H_INCLUDED
+
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "includes/kratos_parameters.h"
+#include "spatial_containers/octree_binary.h"
+#include "processes/find_intersected_geometrical_objects_process.h"
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @brief Forward declaration of ModelPart
+ */
+class ModelPart;
+
+/** 
+ * @brief Find the objects in a volume model part that are intersected ty the given geometry
+ * @author Miguel Maso Sotomayor
+ * @ingroup ShallowWaterApplication
+*/
+class KRATOS_API(SHALLOW_WATER_APPLICATION) FindIntersectedObjectsUtility
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of FindIntersectedObjectsUtility
+    KRATOS_CLASS_POINTER_DEFINITION(FindIntersectedObjectsUtility);
+
+    /// Local Flags
+    KRATOS_DEFINE_LOCAL_FLAG(INTERSECT_ELEMENTS);
+    KRATOS_DEFINE_LOCAL_FLAG(INTERSECT_CONDITIONS);
+
+    /// Definition of the point type
+    using PointType = Point;
+
+    /// Definition of the node type
+    using NodeType = Node<3>;
+
+    /// Definition of the geometry type
+    using GeometryType = Geometry<NodeType>;
+
+    /// Octree definitions
+    using ConfigurationType = Internals::DistanceSpatialContainersConfigure;
+    using CellType = OctreeBinaryCell<ConfigurationType>;
+    using OctreeType = OctreeBinary<CellType>;
+    using OctreePointerType = unique_ptr<OctreeType>;
+    using CellNodeDataType = typename ConfigurationType::cell_node_data_type;
+    using OctreeCellVectorType = std::vector<typename OctreeType::cell_type*>;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Constructor.
+    FindIntersectedObjectsUtility(ModelPart& rThisModelPart, Parameters ThisParameters = Parameters());
+
+    /// Destructor.
+    ~FindIntersectedObjectsUtility(){}
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    void UpdateSearchStructure();
+
+    void FindIntersectedObjects(const GeometryType& rGeometry, PointerVector<GeometricalObject>& rResults);
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const {
+        return "FindIntersectedObjectsUtility";
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const {}
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    Flags mOptions;
+    ModelPart& mrModelPart;
+    OctreePointerType mpOctree;
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    void GenerateOctree();
+
+    void SetOctreeBoundingBox();
+
+    void FindIntersectedObjects(
+        const GeometryType& rIntersectionGeometry,
+        OctreeCellVectorType& rLeaves,
+        PointerVector<GeometricalObject>& rResults);
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    /// Assignment operator.
+    FindIntersectedObjectsUtility& operator=(FindIntersectedObjectsUtility const& rOther);
+
+    /// Copy constructor.
+    FindIntersectedObjectsUtility(FindIntersectedObjectsUtility const& rOther);
+
+    ///@}
+
+}; // Class FindIntersectedObjectsUtility
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+/// input stream function
+inline std::istream& operator >> (std::istream& rIStream, FindIntersectedObjectsUtility& rThis);
+
+/// output stream function
+inline std::ostream& operator << (std::ostream& rOStream, const FindIntersectedObjectsUtility& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+///@}
+
+}  // namespace Kratos.
+
+#endif // KRATOS_FIND_INTERSECTED_ELEMENTS_UTILITY_H_INCLUDED  defined
