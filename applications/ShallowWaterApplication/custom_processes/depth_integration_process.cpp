5638eff4f2a16c4c1c8732e52a0776e6ed40b618
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-10-18 13:11:52 +0200

    add c++ process

---
+++ applications/ShallowWaterApplication/custom_processes/depth_integration_process.cpp
@@ -0,0 +1,123 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "includes/model_part.h"
+#include "geometries/line_3d_2.h"
+#include "depth_integration_process.h"
+#include "utilities/parallel_utilities.h"
+#include "shallow_water_application_variables.h"
+#include "custom_utilities/find_intersected_objects_utility.h"
+
+namespace Kratos
+{
+
+DepthIntegrationProcess::DepthIntegrationProcess(
+    Model& rModel,
+    Parameters ThisParameters
+    ) : Process(),
+        mrVolumeModelPart(rModel.GetModelPart(ThisParameters["volume_model_part_name"].GetString())),
+        mrInterfaceModelPart(rModel.GetModelPart(ThisParameters["interface_model_part_name"].GetString()))
+{
+    ThisParameters.ValidateAndAssignDefaults(this->GetDefaultParameters());
+    mDirection = ThisParameters["direction_of_integration"].GetVector();
+    mDirection /= norm_2(mDirection);
+}
+
+void DepthIntegrationProcess::Execute()
+{
+    double bottom, top;
+    GetVolumePartBounds(bottom, top);
+    FindIntersectedObjectsUtility intersections(mrVolumeModelPart);
+    for (auto& node : mrInterfaceModelPart.Nodes()) {
+        auto integration_line = CreateIntegrationLine(node, bottom, top);
+        PointerVector<GeometricalObject> intersected_objects;
+        intersections.FindIntersectedObjects(*integration_line, intersected_objects);
+        Integrate(intersected_objects, node);
+    }
+}
+
+void DepthIntegrationProcess::Integrate(PointerVector<GeometricalObject>& rObjects, NodeType& rNode)
+{
+    array_1d<double,3> velocity = ZeroVector(3);
+    double min_elevation = 1e6;
+    double max_elevation = -1e6;
+    int num_nodes = 0;
+    for (auto& object : rObjects) {
+        array_1d<double,3> obj_velocity = ZeroVector(3);
+        double obj_min_elevation = 1e6;
+        double obj_max_elevation = -1e6;
+        int obj_num_nodes = object.GetGeometry().size();
+        for (auto& node : object.GetGeometry()) {
+            velocity += node.FastGetSolutionStepValue(VELOCITY);
+            obj_min_elevation = std::min(obj_min_elevation, inner_prod(mDirection, node));
+            obj_max_elevation = std::max(obj_max_elevation, inner_prod(mDirection, node));
+        }
+        velocity += obj_velocity;
+        min_elevation = std::min(min_elevation, obj_min_elevation);
+        max_elevation = std::min(max_elevation, obj_max_elevation);
+        num_nodes += obj_num_nodes;
+    }
+    velocity /= num_nodes;
+    double height = max_elevation - min_elevation;
+    rNode.FastGetSolutionStepValue(VELOCITY) = velocity;
+    rNode.FastGetSolutionStepValue(HEIGHT) = height;
+}
+
+void DepthIntegrationProcess::GetVolumePartBounds(double& rMin, double& rMax)
+{
+    using MultipleReduction = CombinedReduction<MinReduction<double>,MaxReduction<double>>; 
+
+    std::tie(rMin, rMax) = block_for_each<MultipleReduction>(mrInterfaceModelPart.Nodes(), [&](NodeType& node){
+        const double distance = inner_prod(mDirection, node);
+        return std::make_tuple(distance, distance);
+    });
+}
+
+Geometry<Node<3>>::Pointer DepthIntegrationProcess::CreateIntegrationLine(
+    const NodeType& rNode,
+    const double Bottom,
+    const double Top)
+{
+    array_1d<double,3> origin = rNode - mDirection * inner_prod(rNode, mDirection);
+    array_1d<double,3> start = origin + Top * mDirection;
+    array_1d<double,3> end = origin + Bottom * mDirection;
+    PointerVector<Node<3>> nodes;
+    nodes.push_back(NodeType::Pointer(new NodeType(0, start)));
+    nodes.push_back(NodeType::Pointer(new NodeType(0, end)));
+    return Kratos::make_shared<Line3D2<NodeType>>(nodes);
+}
+
+int DepthIntegrationProcess::Check()
+{
+    KRATOS_ERROR_IF(mDirection.size() != 3) << "DepthIntegrationProcess: The direction of integration must have three coordinates." << std::endl;
+    return 0;
+}
+
+const Parameters DepthIntegrationProcess::GetDefaultParameters() const
+{
+    auto default_parameters = Parameters(R"(
+    {
+        "volume_model_part_name"    : "",
+        "interface_model_part_name" : "",
+        "direction_of_integration"  : [0.0, 0.0, 1.0]
+    })");
+    return default_parameters;
+}
+
+}  // namespace Kratos.
---
+++ applications/ShallowWaterApplication/custom_processes/depth_integration_process.h
@@ -0,0 +1,224 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Miguel Maso Sotomayor
+//
+
+#ifndef KRATOS_DEPTH_INTEGRATION_PROCESS_H_INCLUDED
+#define KRATOS_DEPTH_INTEGRATION_PROCESS_H_INCLUDED
+
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "containers/model.h"
+#include "processes/process.h"
+#include "includes/kratos_parameters.h"
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @brief Forward declaration of ModelPart
+ */
+class ModelPart;
+
+/**
+ * @class DepthIntegrationProcess
+ * @ingroup ShallowWaterApplication
+ * @brief Calculate the minimum distance from all the nodes to a boundary condition in 2D
+ * @details The boundary conditions are assumed to be contained in a line
+ * @author Miguel Maso Sotomayor
+ */
+class KRATOS_API(SHALLOW_WATER_APPLICATION) DepthIntegrationProcess : public Process
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of DepthIntegrationProcess
+    KRATOS_CLASS_POINTER_DEFINITION(DepthIntegrationProcess);
+
+    /// Definition of the node type
+    using NodeType = Node<3>;
+
+    /// Definition of the geometry type
+    using GeometryType = Geometry<NodeType>;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /**
+     * @brief Default constructor.
+     * @details Removed
+     */
+    DepthIntegrationProcess() = delete;
+
+    /**
+     * Constructor with Model and Parameters
+     */
+    DepthIntegrationProcess(Model& rModel, Parameters ThisParameters = Parameters());
+
+    /// Destructor.
+    ~DepthIntegrationProcess() override = default;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    void Execute() override;
+
+    int Check() override;
+
+    const Parameters GetDefaultParameters() const override;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override {
+        std::stringstream buffer;
+        buffer << "DepthIntegrationProcess";
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override {
+        rOStream << Info();
+    }
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override {}
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+    ModelPart& mrVolumeModelPart;
+    ModelPart& mrInterfaceModelPart;
+    array_1d<double,3> mDirection;
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    void Integrate(PointerVector<GeometricalObject>& rObjects, NodeType& rNode);
+
+    void GetVolumePartBounds(double& rMin, double& rMax);
+
+    GeometryType::Pointer CreateIntegrationLine(const NodeType& rNode, const double Bottom, const double Top);
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    /// Assignment operator.
+    DepthIntegrationProcess& operator=(DepthIntegrationProcess const& rOther) = delete;
+
+    /// Copy constructor.
+    DepthIntegrationProcess(DepthIntegrationProcess const& rOther) = delete;
+
+    ///@}
+
+}; // Class DepthIntegrationProcess
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+/// input stream function
+inline std::istream& operator >> (std::istream& rIStream, DepthIntegrationProcess& rThis);
+
+/// output stream function
+inline std::ostream& operator << (std::ostream& rOStream, const DepthIntegrationProcess& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+
+///@}
+
+}  // namespace Kratos.
+
+#endif // KRATOS_DEPTH_INTEGRATION_PROCESS_H_INCLUDED defined
