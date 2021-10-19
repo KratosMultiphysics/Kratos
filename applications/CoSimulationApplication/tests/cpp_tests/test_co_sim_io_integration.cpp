3b0e23e90d51c61a5dd0454197db46c9b2a9ee74
Philipp <philipp.bucher@tum.de>
2021-10-19 10:17:50 +0200

    enhance testing

--- applications/CoSimulationApplication/tests/cpp_tests/test_co_sim_io_integration.cpp
+++ applications/CoSimulationApplication/tests/cpp_tests/test_co_sim_io_integration.cpp
@@ -18,6 +18,7 @@
 #include "containers/model.h"
 #include "testing/testing.h"
 #include "custom_utilities/co_sim_io_conversion_utilities.h"
+#include "co_simulation_testing_utilities.h"
 
 // Utilities
 
@@ -44,27 +45,7 @@ KRATOS_TEST_CASE_IN_SUITE(CoSimIOModelPartToKratosModelPart_NodesOnly, KratosCos
 
     CoSimIOConversionUtilities::CoSimIOModelPartToKratosModelPart(co_sim_io_model_part, kratos_model_part);
 
-    KRATOS_CHECK_EQUAL(kratos_model_part.NumberOfNodes(), num_nodes);
-    KRATOS_CHECK_EQUAL(kratos_model_part.NumberOfElements(), 0);
-    KRATOS_CHECK_EQUAL(kratos_model_part.NumberOfProperties(), 0);
-
-    const auto nodes_begin = kratos_model_part.NodesBegin();
-
-    for (std::size_t i=0; i<num_nodes; ++i) {
-        const std::size_t exp_id = i+1;
-
-        const auto& r_node = *(nodes_begin+i);
-
-        const auto& r_co_sim_io_node = co_sim_io_model_part.GetNode(exp_id);
-
-        KRATOS_CHECK_EQUAL(r_node.Id(), exp_id);
-        KRATOS_CHECK_DOUBLE_EQUAL(r_node.X(), r_co_sim_io_node.X());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_node.X0(), r_co_sim_io_node.X());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Y(), r_co_sim_io_node.Y());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Y0(), r_co_sim_io_node.Y());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Z(), r_co_sim_io_node.Z());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_node.Z0(), r_co_sim_io_node.Z());
-    }
+    CheckModelPartsAreEqual(kratos_model_part, co_sim_io_model_part);
 }
 
 KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart_NodesOnly, KratosCosimulationFastSuite)
@@ -85,33 +66,40 @@ KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart_NodesOnly, KratosCos
 
     CoSimIOConversionUtilities::KratosModelPartToCoSimIOModelPart(kratos_model_part, co_sim_io_model_part);
 
-    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfNodes(), num_nodes);
-    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfLocalNodes(), num_nodes);
-    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfGhostNodes(), 0);
-    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfElements(), 0);
-
-    const auto nodes_begin = co_sim_io_model_part.NodesBegin();
-
-    for (std::size_t i=0; i<num_nodes; ++i) {
-        const int exp_id = i+1;
-
-        const auto& r_co_sim_io_node = **(nodes_begin+i);
-
-        const auto& r_kratos_node = kratos_model_part.GetNode(exp_id);
-
-        KRATOS_CHECK_EQUAL(r_co_sim_io_node.Id(), exp_id);
-        KRATOS_CHECK_DOUBLE_EQUAL(r_kratos_node.X(), r_co_sim_io_node.X());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_kratos_node.X0(), r_co_sim_io_node.X());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_kratos_node.Y(), r_co_sim_io_node.Y());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_kratos_node.Y0(), r_co_sim_io_node.Y());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_kratos_node.Z(), r_co_sim_io_node.Z());
-        KRATOS_CHECK_DOUBLE_EQUAL(r_kratos_node.Z0(), r_co_sim_io_node.Z());
-    }
+    CheckModelPartsAreEqual(kratos_model_part, co_sim_io_model_part);
 }
 
 KRATOS_TEST_CASE_IN_SUITE(CoSimIOModelPartToKratosModelPart, KratosCosimulationFastSuite)
 {
+    Model model;
+    auto& kratos_model_part = model.CreateModelPart("kratos_mp");
 
+    CoSimIO::ModelPart co_sim_io_model_part("co_sim_io_mp");
+
+    const int node_ids[] = {2, 159, 61}; // deliberately not ordered!
+    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
+    co_sim_io_model_part.CreateNewNode(node_ids[0], node_coords[0], node_coords[1], node_coords[2]);
+    co_sim_io_model_part.CreateNewNode(node_ids[1], node_coords[1], node_coords[2], node_coords[0]);
+    co_sim_io_model_part.CreateNewNode(node_ids[2], node_coords[2], node_coords[0], node_coords[1]);
+
+    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfNodes(), 3);
+
+    const int elem_ids[] = {21, 19, 1}; // deliberately not ordered!
+    const CoSimIO::ElementType elem_types[] = {
+        CoSimIO::ElementType::Point2D,
+        CoSimIO::ElementType::Point2D,
+        CoSimIO::ElementType::Line2D2
+    };
+
+    co_sim_io_model_part.CreateNewElement(elem_ids[0], elem_types[0], {node_ids[0]});
+    co_sim_io_model_part.CreateNewElement(elem_ids[1], elem_types[1], {node_ids[1]});
+    co_sim_io_model_part.CreateNewElement(elem_ids[2], elem_types[2], {node_ids[1], node_ids[2]});
+
+    KRATOS_CHECK_EQUAL(co_sim_io_model_part.NumberOfElements(), 3);
+
+    CoSimIOConversionUtilities::CoSimIOModelPartToKratosModelPart(co_sim_io_model_part, kratos_model_part);
+
+    CheckModelPartsAreEqual(kratos_model_part, co_sim_io_model_part);
 }
 
 KRATOS_TEST_CASE_IN_SUITE(KratosModelPartToCoSimIOModelPart, KratosCosimulationFastSuite)
