8810d173d6de95617f78d42c2bd0a0b469a60b55
Armin Geiser <armin.geiser@tum.de>
2021-10-21 10:36:08 +0200

    Update configure.bat

--- scripts/wheels/windows/configure.bat
+++ scripts/wheels/windows/configure.bat
@@ -13,38 +13,38 @@ set PYTHON_EXECUTABLE=%1
 
 set KRATOS_APPLICATIONS=
 CALL :add_app %KRATOS_APP_DIR%\StructuralMechanicsApplication
-CALL :add_app %KRATOS_APP_DIR%\FluidDynamicsApplication
-CALL :add_app %KRATOS_APP_DIR%\DEMApplication
-CALL :add_app %KRATOS_APP_DIR%\ContactStructuralMechanicsApplication
-CALL :add_app %KRATOS_APP_DIR%\ParticleMechanicsApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\FluidDynamicsApplication
+@REM CALL :add_app %KRATOS_APP_DIR%\DEMApplication
+@REM CALL :add_app %KRATOS_APP_DIR%\ContactStructuralMechanicsApplication
+@REM CALL :add_app %KRATOS_APP_DIR%\ParticleMechanicsApplication;
 CALL :add_app %KRATOS_APP_DIR%\ConvectionDiffusionApplication;
-CALL :add_app %KRATOS_APP_DIR%\DamApplication;
-CALL :add_app %KRATOS_APP_DIR%\PoromechanicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\FSIApplication;
-CALL :add_app %KRATOS_APP_DIR%\SwimmingDEMApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\DamApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\PoromechanicsApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\FSIApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\SwimmingDEMApplication;
 @REM CALL :add_app %KRATOS_APP_DIR%\ExternalSolversApplication;
 CALL :add_app %KRATOS_APP_DIR%\EigenSolversApplication;
 CALL :add_app %KRATOS_APP_DIR%\LinearSolversApplication;
-CALL :add_app %KRATOS_APP_DIR%\ConstitutiveLawsApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\ConstitutiveLawsApplication;
 @REM CALL :add_app %KRATOS_APP_DIR%\FemToDemApplication;
 @REM CALL :add_app %KRATOS_APP_DIR%\PfemFluidDynamicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\DelaunayMeshingApplication;
-CALL :add_app %KRATOS_APP_DIR%\MeshingApplication;
-CALL :add_app %KRATOS_APP_DIR%\DemStructuresCouplingApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\DelaunayMeshingApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\MeshingApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\DemStructuresCouplingApplication;
 CALL :add_app %KRATOS_APP_DIR%\MeshMovingApplication;
-CALL :add_app %KRATOS_APP_DIR%\CSharpWrapperApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\CSharpWrapperApplication;
 CALL :add_app %KRATOS_APP_DIR%\ShapeOptimizationApplication;
-CALL :add_app %KRATOS_APP_DIR%\CoSimulationApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\CoSimulationApplication;
 @REM CALL :add_app %KRATOS_APP_DIR%\CableNetApplication;
-CALL :add_app %KRATOS_APP_DIR%\RANSApplication;
-CALL :add_app %KRATOS_APP_DIR%\MappingApplication;
-CALL :add_app %KRATOS_APP_DIR%\CompressiblePotentialFlowApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\RANSApplication;
+REM CALL :add_app %KRATOS_APP_DIR%\MappingApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\CompressiblePotentialFlowApplication;
 @REM CALL :add_app %KRATOS_APP_DIR%\HDF5Application;
-CALL :add_app %KRATOS_APP_DIR%\IgaApplication;
-CALL :add_app %KRATOS_APP_DIR%\ChimeraApplication;
-CALL :add_app %KRATOS_APP_DIR%\MultilevelMonteCarloApplication;
-CALL :add_app %KRATOS_APP_DIR%\StatisticsApplication;
-CALL :add_app %KRATOS_APP_DIR%\RomApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\IgaApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\ChimeraApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\MultilevelMonteCarloApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\StatisticsApplication;
+@REM CALL :add_app %KRATOS_APP_DIR%\RomApplication;
 
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\cmake_install.cmake"
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\CMakeCache.txt"
