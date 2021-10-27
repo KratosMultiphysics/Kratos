59c292f4b1d04d4ec5d3c990e731783333bda253
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2021-10-27 15:05:17 +0200

    minor

--- applications/MetisApplication/MetisApplication.py
+++ applications/MetisApplication/MetisApplication.py
@@ -1,14 +1,14 @@
-print("1")
+print("1", flush=True)
 from KratosMultiphysics import _ImportApplication
-print("2")
+print("2", flush=True)
 import KratosMultiphysics.mpi # importing the MPI-Core, since the MetisApp directly links to it
-print("3")
+print("3", flush=True)
 from KratosMetisApplication import *
-print("4")
+print("4", flush=True)
 application = KratosMetisApplication()
-print("5")
+print("5", flush=True)
 application_name = "KratosMetisApplication"
-print("6")
+print("6", flush=True)
 
 _ImportApplication(application, application_name)
-print("7")
+print("7", flush=True)
