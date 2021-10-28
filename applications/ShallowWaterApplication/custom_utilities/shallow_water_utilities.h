c5f3c54c99c40e82df9a4fa2958fe13b52cd1e68
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-10-28 11:41:07 +0200

    add new variables tool

--- applications/ShallowWaterApplication/custom_utilities/shallow_water_utilities.h
+++ applications/ShallowWaterApplication/custom_utilities/shallow_water_utilities.h
@@ -65,6 +65,8 @@ public:
 
     typedef Geometry<NodeType> GeometryType;
 
+    typedef ModelPart::NodesContainerType NodesContainerType;
+
     ///@}
     ///@name Pointer definition
     ///@{
@@ -139,6 +141,29 @@ public:
      * @brief Move the z-coordinate of the mesh according to a variable
      */
     void SetMeshZCoordinate(ModelPart& rModelPart, const Variable<double>& rVariable);
+
+    /**
+     * @brief Swap the Y and Z components of a vector variable
+     */
+    void SwapYZComponents(const Variable<array_1d<double,3>>& rVariable, NodesContainerType& rNodes)
+    {
+        block_for_each(rNodes, [&](NodeType& rNode){
+            array_1d<double,3>& r_value = rNode.FastGetSolutionStepValue(rVariable);
+            std::swap(r_value[1], r_value[2]);
+        });
+    }
+
+    /**
+     * @brief Swap the Y and Z components of a vector variable
+     */
+    template<class TContainerType>
+    void SwapYZComponentsNonHistorical(const Variable<array_1d<double,3>>& rVariable, TContainerType& rContainer)
+    {
+        block_for_each(rContainer, [&](typename TContainerType::value_type& rEntity){
+            array_1d<double,3>& r_value = rEntity.GetValue(rVariable);
+            std::swap(r_value[1], r_value[2]);
+        });
+    }
 
     /**
      * @brief Compute the L-2 norm for the given double variable
