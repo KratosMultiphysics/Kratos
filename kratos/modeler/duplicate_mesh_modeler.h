1c2d8f96d2ff9a1ce640fe48e6e0a9d007148806
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-10-30 20:01:12 +0200

    modeler changes

--- kratos/modeler/duplicate_mesh_modeler.h
+++ kratos/modeler/duplicate_mesh_modeler.h
@@ -102,7 +102,7 @@ public:
     ///@name Operations
     ///@{
 
-    void GenerateMesh(ModelPart& rThisModelPart, Element const& rReferenceElement, Condition const& rReferenceCondition)
+    void GenerateMesh(ModelPart& rThisModelPart, Element const& rReferenceElement, Condition const& rReferenceCondition) override
     {
         KRATOS_TRY;
 
@@ -209,21 +209,21 @@ public:
 
     /// Turn back information as a string.
 
-    virtual std::string Info() const
+    virtual std::string Info() const override
     {
         return "DuplicateMeshModeler";
     }
 
     /// Print information about this object.
 
-    virtual void PrintInfo(std::ostream& rOStream) const
+    virtual void PrintInfo(std::ostream& rOStream) const override
     {
         rOStream << Info();
     }
 
     /// Print object's data.
 
-    virtual void PrintData(std::ostream& rOStream) const
+    virtual void PrintData(std::ostream& rOStream) const override
     {
     }
 
@@ -307,11 +307,11 @@ private:
     ///@name Un accessible methods
     ///@{
 
-    /// Assignment operator.
-    DuplicateMeshModeler & operator=(DuplicateMeshModeler const& rOther);
+    // /// Assignment operator.
+    // DuplicateMeshModeler & operator=(DuplicateMeshModeler const& rOther);
 
-    /// Copy constructor.
-    DuplicateMeshModeler(DuplicateMeshModeler const& rOther);
+    // /// Copy constructor.
+    // DuplicateMeshModeler(DuplicateMeshModeler const& rOther);
 
 
     ///@}
