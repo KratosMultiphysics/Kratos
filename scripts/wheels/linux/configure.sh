730c9bfdd467bd5100750af70a0ced59867491a5
unknown <roigcarlo@gmail.com>
2021-11-08 15:03:19 +0100

    Enabling triangle

--- scripts/wheels/linux/configure.sh
+++ scripts/wheels/linux/configure.sh
@@ -62,6 +62,7 @@ rm -rf "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}/CMakeFiles"
 
 ${CMAKE} -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" \
 -DCMAKE_INSTALL_PREFIX=$2                                              \
+-DUSE_TRIANGLE_NONFREE_TPL=ON                                          \
 -DUSE_MPI=OFF                                                          \
 -DCMAKE_C_COMPILER=/opt/rh/devtoolset-8/root/usr/bin/gcc               \
 -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-8/root/usr/bin/g++             \
