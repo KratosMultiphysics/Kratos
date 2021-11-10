5ab783fc2634f8814cfbfa07c46674cd1d4677ab
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2021-11-10 12:30:14 +0100

    Register CAD modeler test

--- kratos/tests/test_KratosCore.py
+++ kratos/tests/test_KratosCore.py
@@ -63,6 +63,7 @@ import test_mls_shape_functions_utility
 import test_model_part_combination_utilities
 import test_force_and_torque_utils
 import test_kernel
+import test_cad_tessellation_modeler
 
 def AssembleTestSuites():
     ''' Populates the test suites to run.
@@ -145,6 +146,7 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_model_part_combination_utilities.TestModelPartCombinationUtilities]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_force_and_torque_utils.TestForceAndTorqueUtils]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_kernel.TestKernel]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_cad_tessellation_modeler.TestCadTessellationModeler]))
 
     # Create a test suite with the selected tests plus all small tests
     nightSuite = suites['nightly']
