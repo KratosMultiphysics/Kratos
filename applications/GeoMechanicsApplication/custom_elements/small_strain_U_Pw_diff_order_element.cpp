2e7e5981c7c2ea7767f8c5a4c9fc8713948ea383
Carlos Lubbers <carlos.lubbers@deltares.nl>
2021-11-15 17:56:41 +0100

    Try to improve logging of inverted elements

--- applications/GeoMechanicsApplication/custom_elements/small_strain_U_Pw_diff_order_element.cpp
+++ applications/GeoMechanicsApplication/custom_elements/small_strain_U_Pw_diff_order_element.cpp
@@ -2511,8 +2511,15 @@ void SmallStrainUPwDiffOrderElement::
                                                   GPoint);
 
     if(detJ < 0.0) {
-        std::cerr << "Error: Element " + std::to_string(this->Id()) + "inverted. DetJ: " + std::to_string(detJ);
-        throw Kratos::Exception("Error: Element " + std::to_string(this->Id()) + "inverted. DetJ: " + std::to_string(detJ), KRATOS_CODE_LOCATION);
+        std::cerr << "Error: Element " + std::to_string(this->Id()) + " is inverted. DetJ: " + detJ + std::endl 
+        + "This usually indicates the deformations are too large for the mesh size.";
+
+        KRATOS_ERROR_IF(detJ < 0.0)
+            << "ERROR:: ELEMENT ID: "
+            << this->Id()
+            << " INVERTED. DETJ: "
+            << detJ
+            << std::endl;
     }
 
     // Deformation gradient
