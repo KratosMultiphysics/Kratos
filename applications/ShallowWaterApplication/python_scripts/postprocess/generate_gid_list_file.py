8b773199d1c984e341e285961039acd73f41f5db
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-11-17 11:27:14 +0100

    simplify args

--- applications/ShallowWaterApplication/python_scripts/postprocess/generate_gid_list_file.py
+++ applications/ShallowWaterApplication/python_scripts/postprocess/generate_gid_list_file.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 
-def GenerateGiDListFile(first_dir = "", other_dirs = [], file_name = ""):
-    first_dir = _CheckDirectory(first_dir)
-    files = _GetPostFilesList(first_dir)
-    for dir in other_dirs:
+def GenerateGiDListFile(directories = [], file_name = ""):
+    files = []
+    for dir in directories:
         dir = _CheckDirectory(dir)
         files += _GetPostFilesList(dir)
     files.sort()
@@ -46,8 +45,7 @@ def _WriteListFile(file_list, file_name):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Generate a GiD .post.lst file to merge the results of several simulations.")
-    parser.add_argument("first_location",  nargs="?", type=str, help="Where the post files are located. By default it is the current directory")
-    parser.add_argument("other_locations", nargs="*", type=str, help="Add more post files. By default it is empty")
+    parser.add_argument("directory", nargs="*", type=str, default="[./]", help="Where the post files are located. By default the first location is the current directory")
     parser.add_argument("-n", "--name", type=str, help="Name of the output file. By default it is the current folder name")
     args = parser.parse_args()
-    GenerateGiDListFile(args.first_location, args.other_locations, args.name)
+    GenerateGiDListFile(args.directory, args.name)
