8dc7c621c60f535e9dadd6c8a41fac4fba82e3a8
unknown <roigcarlo@gmail.com>
2021-11-17 11:10:16 +0100

    Added correct path

--- scripts/wheels/linux/configure.sh
+++ scripts/wheels/linux/configure.sh
@@ -72,5 +72,5 @@ ${CMAKE} -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" \
 -DLAPACK_LIBRARIES="/usr/lib64/liblapack.so.3"                         \
 -DBLAS_LIBRARIES="/usr/lib64/libblas.so.3"                             \
 -DINCLUDE_MMG=ON                                                       \
--DMMG_ROOT="/external_libraries/mmg/mmg_5_4_1"                         \
+-DMMG_ROOT="/workspace/external_libraries/mmg/mmg_5_4_1"               \
 -DINSTALL_RUNKRATOS=OFF
