0992e465d0fd12e31aff23a95704bd89167e2dec
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-11-19 08:44:23 +0100

    undo suggestion

--- kratos/utilities/intersection_utilities.h
+++ kratos/utilities/intersection_utilities.h
@@ -181,12 +181,11 @@ public:
         const array_1d<double,3>& rPoint0,
         const array_1d<double,3>& rPoint1)
     {
-
         // Check the intersection of each edge against the intersecting object
         array_1d<double,3> int_point;
-        return (ComputeLineLineIntersection(rVert0, rVert1, rPoint0, rPoint1, int_point));
-        return (ComputeLineLineIntersection(rVert1, rVert2, rPoint0, rPoint1, int_point));
-        return (ComputeLineLineIntersection(rVert2, rVert0, rPoint0, rPoint1, int_point));
+        if (ComputeLineLineIntersection(rVert0, rVert1, rPoint0, rPoint1, int_point)) return true;
+        if (ComputeLineLineIntersection(rVert1, rVert2, rPoint0, rPoint1, int_point)) return true;
+        if (ComputeLineLineIntersection(rVert2, rVert0, rPoint0, rPoint1, int_point)) return true;
 
         // Let check second geometry is inside the first one.
         if (PointInTriangle(rVert0, rVert1, rVert2, rPoint0)) return true;
