3fbc28325b56678456c8511913307ff13abf0b62
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-11-22 14:26:53 +0100

    Added header to source file

--- applications/FluidDynamicsApplication/custom_processes/shock_capturing_entropy_viscosity_process.cpp
+++ applications/FluidDynamicsApplication/custom_processes/shock_capturing_entropy_viscosity_process.cpp
@@ -1,3 +1,15 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main author:     Eduard Gómez
+//
+
 #include <cmath>
 #include <tuple>
 
