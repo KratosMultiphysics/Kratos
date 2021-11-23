eaaeff3b21d7429ee7238da9fa6864a38d927cba
Philipp <philipp.bucher@tum.de>
2021-11-23 09:51:27 +0100

    clean temp vars

--- applications/PfemFluidDynamicsApplication/custom_utilities/move_mesh_utility.cpp
+++ applications/PfemFluidDynamicsApplication/custom_utilities/move_mesh_utility.cpp
@@ -27,25 +27,17 @@ void MoveMeshUtility::ResetPfemKinematicValues(ModelPart& rFluidModelPart)
 {
     block_for_each(rFluidModelPart.Nodes(), [&](NodeType& rNode){
         if (rNode.IsNot(RIGID) && rNode.IsNot(SOLID)) { // We update only the fluid part
-            array_1d<double, 3>& r_current_displ = rNode.FastGetSolutionStepValue(DISPLACEMENT, 0);
-            array_1d<double, 3>& r_current_vel   = rNode.FastGetSolutionStepValue(VELOCITY, 0);
-            array_1d<double, 3>& r_current_acc   = rNode.FastGetSolutionStepValue(ACCELERATION, 0);
+            array_1d<double, 3>& r_current_disp = rNode.FastGetSolutionStepValue(DISPLACEMENT, 0);
+            array_1d<double, 3>& r_current_vel  = rNode.FastGetSolutionStepValue(VELOCITY, 0);
+            array_1d<double, 3>& r_current_acc  = rNode.FastGetSolutionStepValue(ACCELERATION, 0);
 
-            array_1d<double, 3>& r_old_displ     = rNode.FastGetSolutionStepValue(DISPLACEMENT, 1);
-            array_1d<double, 3>& r_old_vel       = rNode.FastGetSolutionStepValue(VELOCITY, 1);
-            array_1d<double, 3>& r_old_acc       = rNode.FastGetSolutionStepValue(ACCELERATION, 1);
+            const array_1d<double, 3> old_disp = rNode.FastGetSolutionStepValue(DISPLACEMENT, 1);
 
-            array_1d<double, 3> copy_old_displ   = r_old_displ;
-            array_1d<double, 3> copy_old_vel     = r_old_vel;
-            array_1d<double, 3> copy_old_acc     = r_old_acc;
+            noalias(rNode.Coordinates()) = rNode.GetInitialPosition() + old_disp;
 
-            array_1d<double, 3>& r_coordinates = rNode.Coordinates();
-            const array_1d<double, 3>& r_initial_coordinates = rNode.GetInitialPosition();
-            noalias(r_coordinates) = r_initial_coordinates + copy_old_displ;
-
-            noalias(r_current_displ) = copy_old_displ;
-            noalias(r_current_vel)   = copy_old_vel;
-            noalias(r_current_acc)   = copy_old_acc;
+            noalias(r_current_disp) = old_disp;
+            noalias(r_current_vel)  = rNode.FastGetSolutionStepValue(VELOCITY, 1);
+            noalias(r_current_acc)  = rNode.FastGetSolutionStepValue(ACCELERATION, 1);
         }
     });
 
