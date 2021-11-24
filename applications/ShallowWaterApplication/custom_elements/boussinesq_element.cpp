9ae55d81758efb63e1c2e743a8e0a6f5841ab58c
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-11-24 12:22:56 +0100

    linearization definition

--- applications/ShallowWaterApplication/custom_elements/boussinesq_element.cpp
+++ applications/ShallowWaterApplication/custom_elements/boussinesq_element.cpp
@@ -18,7 +18,7 @@
 
 // Project includes
 #include "includes/checks.h"
-#include "conservative_element.h"
+#include "boussinesq_element.h"
 #include "utilities/geometry_utilities.h"
 #include "custom_utilities/phase_function.h"
 #include "shallow_water_application_variables.h"
@@ -53,52 +53,55 @@ typename BoussinesqElement<TNumNodes>::LocalVectorType BoussinesqElement<TNumNod
 template<std::size_t TNumNodes>
 void BoussinesqElement<TNumNodes>::CalculateGaussPointData(ElementData& rData, const array_1d<double,TNumNodes>& rN)
 {
-    const double h = inner_prod(rData.nodal_h, rN);
-    const double c2 = rData.gravity * h;
+    const double eta = inner_prod(rData.nodal_h, rN);
+    const double H = -inner_prod(rData.nodal_z, rN);
+    const double g = rData.gravity;
+    const double e = rData.amplitude / H;  // the non linearity ratio
+    const double l = H / rData.wavelength; // the dispersive ratio
     const array_1d<double,3> v = WaveElementType::VectorProduct(rData.nodal_v, rN);
 
-    rData.height = h;
+    rData.height = H + e * eta;
     rData.velocity = v;
 
     /**
-     * A_1 = {{e * u_1   0   -u_1^2 + gh},
-     *        {  u_2    u_1   -u_1 * u_2},
-     *        {   1      0        0     }}
+     * A_1 = {{ e*u_1      0     g  },
+     *        {   0      e*u_1   0  },
+     *        {H + e*eta   0   e*u_1}}
      */
-    rData.A1(0,0) = v[0];
+    rData.A1(0,0) = e*v[0];
     rData.A1(0,1) = 0;
-    rData.A1(0,2) = -v[0]*v[0] + c2;
-    rData.A1(1,0) = v[1];
-    rData.A1(1,1) = v[0];
-    rData.A1(1,2) = -v[0]*v[1];
-    rData.A1(2,0) = 1;
+    rData.A1(0,2) = g;
+    rData.A1(1,0) = 0;
+    rData.A1(1,1) = e*v[0];
+    rData.A1(1,2) = 0;
+    rData.A1(2,0) = H + e*eta;
     rData.A1(2,1) = 0;
-    rData.A1(2,2) = 0;
+    rData.A1(2,2) = e*v[0];
 
     /*
-     * A_2 = {{u_2    u_1      -u_1 * u_2},
-     *        { 0   2 * u_2   -u_2^2 + gh},
-     *        { 0      1            0    }}
+     * A_2 = {{ e*u_2      0      0  },
+     *        {   0      e*u_2    g  },
+     *        {   0   H + e*eta e*u_2}}
      */
-    rData.A2(0,0) = v[1];
-    rData.A2(0,1) = v[0];
-    rData.A2(0,2) = -v[0]*v[1];
+    rData.A2(0,0) = e*v[1];
+    rData.A2(0,1) = 0;
+    rData.A2(0,2) = 0;
     rData.A2(1,0) = 0;
-    rData.A2(1,1) = 2*v[1];
-    rData.A2(1,2) = -v[1]*v[1] + c2;
+    rData.A2(1,1) = e*v[1];
+    rData.A2(1,2) = g;
     rData.A2(2,0) = 0;
-    rData.A2(2,1) = 1;
-    rData.A2(2,2) = 0;
+    rData.A2(2,1) = H + e*eta;
+    rData.A2(2,2) = e*v[1];
 
     /// b_1
-    rData.b1[0] = c2;
+    rData.b1[0] = 0;
     rData.b1[1] = 0;
-    rData.b1[2] = 0;
+    rData.b1[2] = v[0];
 
     /// b_2
     rData.b2[0] = 0;
-    rData.b2[1] = c2;
-    rData.b2[2] = 0;
+    rData.b2[1] = 0;
+    rData.b2[2] = v[1];
 }
 
 template<std::size_t TNumNodes>
