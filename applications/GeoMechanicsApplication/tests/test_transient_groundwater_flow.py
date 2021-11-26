5be1b74572a140be96c7a76746787644e45d4667
EleniSmyrniou <eleni.smyrniou@deltares.nl>
2021-11-26 09:31:15 +0100

    DPIPING-13 - added test of transient flow

--- applications/GeoMechanicsApplication/tests/test_transient_groundwater_flow.py
+++ applications/GeoMechanicsApplication/tests/test_transient_groundwater_flow.py
@@ -3,11 +3,12 @@ import os
 
 sys.path.append(os.path.join('..', '..', '..'))
 sys.path.append(os.path.join('..', 'python_scripts'))
+sys.path.append(os.path.join('D:\\kratos'))
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import test_helper
 
-class KratosGeoMechanicsTransientGroundWaterFlowTests(KratosUnittest.TestCase):
+class TestKratosGeoMechanicsTransientGroundWaterFlowTests(KratosUnittest.TestCase):
     """
     This class contains benchmark tests which are checked with the analytical solution
     """
@@ -20,16 +21,27 @@ class KratosGeoMechanicsTransientGroundWaterFlowTests(KratosUnittest.TestCase):
         # Code here will be placed AFTER every test in this TestCase.
         pass
 
+    def test_Transient_Case_B1_2D3N(self):
+        test_name = 'test_Transient_Case_B1_2D3N'
+        file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
+        stages = test_helper.run_stages(file_path, 2)
+
+        water_pressure_stage_2 = test_helper.get_water_pressure(stages[-1])
+
+        p_height_1_72_m = water_pressure_stage_2[32]
+
+        self.assertAlmostEqual(1.9762325593327588, p_height_1_72_m)
+
     def test_Transient_Case_A1_2D3N(self):
         test_name = 'test_Transient_Case_A1_2D3N'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         stages = test_helper.run_stages(file_path, 2)
 
-        water_pressure = test_helper.get_water_pressure(stages[-1])
+        water_pressure_stage_2 = test_helper.get_water_pressure(stages[-1])
 
-        p_height_1_72_m = water_pressure[32]
+        p_height_1_72_m = water_pressure_stage_2[32]
 
-        self.assertAlmostEqual(3.7224838666844984, p_height_1_72_m)
+        self.assertAlmostEqual(0.024467586026697394, p_height_1_72_m)
 
     def test_Transient_Case_A1_2D6N(self):
         test_name = 'test_Transient_Case_A1_2D6N'
