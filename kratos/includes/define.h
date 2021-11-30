07f6fb62ec1d9be1d1bd0c447a50de3350b7f1cc
Philipp Bucher <philipp.bucher@tum.de>
2021-11-30 16:31:53 +0100

    KRATOS_WATCH to stderr

--- kratos/includes/define.h
+++ kratos/includes/define.h
@@ -806,7 +806,7 @@ namespace Kratos
 /* template<class TFunction> ApplyToLinearSolver(String Name){ */
 
 //Print Trace if defined
-#define KRATOS_WATCH(variable) std::cout << #variable << " : " << variable << std::endl;
+#define KRATOS_WATCH(variable) std::cerr << #variable << " : " << variable << std::endl;
 
 }  /* namespace Kratos.*/
 
