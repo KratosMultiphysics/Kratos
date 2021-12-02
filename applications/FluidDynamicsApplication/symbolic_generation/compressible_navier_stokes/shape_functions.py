8b11540d9008774543c1dfde5bd03088cd35620f
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-12-02 14:45:06 +0100

    Finished shape functions

--- applications/FluidDynamicsApplication/symbolic_generation/compressible_navier_stokes/shape_functions.py
+++ applications/FluidDynamicsApplication/symbolic_generation/compressible_navier_stokes/shape_functions.py
@@ -1,13 +1,19 @@
 import sympy
 
 
+def DefineShapeFunctionsMatrix(dim, n_nodes, n_gauss):
+    quadrature_data = {
+        (2, 3): TriangleShapeFunctions,
+        (2, 4): QuadrilateralShapeFunctions,
+        (3, 4): TetrahedronShapeFunctions
+    }
+    return quadrature_data[(dim, n_nodes)](n_gauss)
+
+
 def TriangleShapeFunctions(n_gauss):
-    mat_N = sympy.Matrix(n_gauss, 3, lambda *_: 0)
+    mat_N = sympy.zeros(n_gauss, 3)
     if n_gauss == 1:
-        mat_N[0, 0] = 1.0 / 3.0
-        mat_N[0, 1] = 1.0 / 3.0
-        mat_N[0, 2] = 1.0 / 3.0
-        return mat_N
+        return sympy.Matrix(n_gauss, 3, lambda *_: 1/3)
     if n_gauss == 3:
         mat_N[0, 0] = 2.0 / 3.0
         mat_N[0, 1] = 1.0 / 6.0
@@ -24,40 +30,27 @@ def TriangleShapeFunctions(n_gauss):
 
 
 def QuadrilateralShapeFunctions(n_gauss):
+    N = [lambda xi, eta: 0.25 * (1 - xi) * (1 - eta),
+         lambda xi, eta: 0.25 * (1 + xi) * (1 - eta),
+         lambda xi, eta: 0.25 * (1 - xi) * (1 + eta),
+         lambda xi, eta: 0.25 * (1 + xi) * (1 + eta)]
+
     if n_gauss == 1:
-        return sympy.Matrix(n_gauss, 4, lambda *_: 0.25)
+        return sympy.Matrix(n_gauss, 4, lambda _, j: N[j](0, 0))
     if n_gauss == 4:
-        mat_N = sympy.Matrix(n_gauss, 4, lambda *_: 0)
-        mat_N[0, 0] = 0 # TODO
-        mat_N[0, 1] = 0 # TODO
-        mat_N[0, 2] = 0 # TODO
-        mat_N[0, 3] = 0 # TODO
-        mat_N[1, 0] = 0 # TODO
-        mat_N[1, 1] = 0 # TODO
-        mat_N[1, 2] = 0 # TODO
-        mat_N[1, 3] = 0 # TODO
-        mat_N[2, 0] = 0 # TODO
-        mat_N[2, 1] = 0 # TODO
-        mat_N[2, 2] = 0 # TODO
-        mat_N[2, 3] = 0 # TODO
-        mat_N[3, 0] = 0 # TODO
-        mat_N[3, 1] = 0 # TODO
-        mat_N[3, 2] = 0 # TODO
-        mat_N[3, 3] = 0 # TODO
-        return mat_N
+        xi = [-3**-0.5,  3**0.5, -3**-0.5, 3**0.5]
+        eta = [-3**-0.5, -3**0.5,  3**-0.5, 3**0.5]
+        return sympy.Matrix(n_gauss, 4, lambda i, j: N[j](xi[i], eta[i]))
+
     msg = "Invalid quadrature: tetrahedron with %d integration points".format(n_gauss)
-    raise NotImplementedError(msg)    
+    raise NotImplementedError(msg)
 
 
 def TetrahedronShapeFunctions(n_gauss):
-    mat_N = sympy.Matrix(n_gauss,  4, lambda *_: 0)
     if n_gauss == 1:
-        mat_N[0, 0] = 1.0 / 4.0
-        mat_N[0, 1] = 1.0 / 4.0
-        mat_N[0, 2] = 1.0 / 4.0
-        mat_N[0, 3] = 1.0 / 4.0
-        return mat_N
+        return sympy.Matrix(n_gauss, 4, lambda *_: 0.25)
     if n_gauss == 4:
+        mat_N = sympy.zeros(n_gauss,  4)
         mat_N[0, 0] = 0.58541020
         mat_N[0, 1] = 0.13819660
         mat_N[0, 2] = 0.13819660
@@ -77,12 +70,3 @@ def TetrahedronShapeFunctions(n_gauss):
         return mat_N
     msg = "Invalid quadrature: tetrahedron with %d integration points".format(n_gauss)
     raise NotImplementedError(msg)
-
-
-def DefineShapeFunctionsMatrix(dim, n_nodes, n_gauss):
-    quadrature_data = {
-        (2, 3): TriangleShapeFunctions,
-        (2, 4): QuadrilateralShapeFunctions,
-        (3, 4): TetrahedronShapeFunctions
-    }
-    return quadrature_data[(dim, n_nodes)](n_gauss)
