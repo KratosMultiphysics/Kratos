30f85e73581b14434f03d78daa730d19916d9c76
johannes <johannes@johannes-stud03>
2021-12-06 09:00:18 +0100

    copy single to distributed back to old version

--- applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_distributed.py
+++ applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_distributed.py
@@ -9,32 +9,30 @@ class CopySingleToDistributed(CoSimulationDataTransferOperator):
     Used e.g. for FSI with SDof, where the SDof has one value and the fluid interface has many
     """
     def _ExecuteTransferData(self, from_solver_data, to_solver_data, transfer_options):
-        if not to_solver_data.IsDefinedOnThisRank():
-            return
-
-        if from_solver_data.IsDefinedOnThisRank():
-            data_value = from_solver_data.GetData()
-
-            if from_solver_data.IsDistributed():
-                raise Exception("The source if the data cannot be distributed!")
-
-            if not data_value.size == 1:
-            # assert from_solver_data.Size() == 1
-                raise Exception('Interface data "{}" of solver "{}" requires to be of size 1, got: {}'.format(from_solver_data.name, from_solver_data.solver_name, data_value.size))
-            data_value = data_value[0]
-        else:
-            data_value = 0.0
-
-        data_comm = to_solver_data.model_part.GetCommunicator().GetDataCommunicator()
-        data_value = data_comm.Broadcast(data_value, 0)
-
         to_solver_values = to_solver_data.GetData()
-        to_solver_values.fill(data_value)
+        data_value = from_solver_data.GetData()
+
+        data_comm = from_solver_data.model_part.GetCommunicator().GetDataCommunicator()
+
+        if (not data_comm.SumAll(data_value.size) == 1):
+            raise Exception('Interface data "{}" of solver "{}" requires to be of size 1, got: {}'.format(from_solver_data.name, from_solver_data.solver_name, data_value.size))
+
+        # Get Value from solver, if not existent set 0
+        if data_value.size == 0:
+            value = float(0)
+        else:
+            value = data_value[0]        
+
+        # sum Up Value from solver from all ranks -> so not zero when not definied in this rank
+        value = from_solver_data.model_part.GetCommunicator().GetDataCommunicator().SumAll(value)
+
+        to_solver_values.fill(value)
 
         # the order is IMPORTANT here!
         if "swap_sign" in transfer_options.GetStringArray():
             to_solver_values *= (-1)
         if "distribute_values" in transfer_options.GetStringArray():
+            data_comm = from_solver_data.model_part.GetCommunicator().GetDataCommunicator()
             to_solver_values /= data_comm.SumAll(to_solver_data.Size())
         if "add_values" in transfer_options.GetStringArray():
             to_solver_values += to_solver_data.GetData()
@@ -42,8 +40,6 @@ class CopySingleToDistributed(CoSimulationDataTransferOperator):
         to_solver_data.SetData(to_solver_values)
 
     def _Check(self, from_solver_data, to_solver_data):
-        if not to_solver_data.IsDefinedOnThisRank():
-            return
         if not to_solver_data.is_scalar_variable:
             raise Exception('Variable of interface data "{}" of solver "{}" has to be a scalar!'.format(to_solver_data.name, to_solver_data.solver_name))
 
