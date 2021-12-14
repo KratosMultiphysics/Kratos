f24bbab18fab74fd9056c9a0317856b4ecea8405
matekelemen <kelemen.mate.12@gmail.com>
2021-12-14 14:52:49 +0100

    use CompareTwoFilesCheckProcess

--- applications/FreeSurfaceApplication/tests/generalTests.py
+++ applications/FreeSurfaceApplication/tests/generalTests.py
@@ -3,11 +3,9 @@ import KratosMultiphysics.FreeSurfaceApplication as KratosFreeSurface
 from KratosMultiphysics.FreeSurfaceApplication.free_surface_analysis import FreeSurfaceAnalysis
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
+from KratosMultiphysics.compare_two_files_check_process import CompareTwoFilesCheckProcess
 import pathlib
-
-
-def GetFilePath(fileName):
-    return os.path.dirname(__file__) + "/" + fileName
+import shutil
 
 
 class KratosFreeSurfaceGeneralTests(KratosUnittest.TestCase):
@@ -21,10 +19,10 @@ class KratosFreeSurfaceGeneralTests(KratosUnittest.TestCase):
             self.checkResults(self.reference_file)
 
     def test_Reservoir2D(self):
-        self.__RunFreeSurfaceAnalysis("Reservoir2D", "reference_reservoir_2D")
+        self.__RunFreeSurfaceAnalysis("Reservoir2D", "reference_reservoir_2D.csv")
 
     def test_Reservoir3D(self):
-        self.__RunFreeSurfaceAnalysis("Reservoir3D", "reference_reservoir_3D")
+        self.__RunFreeSurfaceAnalysis("Reservoir3D", "reference_reservoir_3D.csv")
 
     def __RunFreeSurfaceAnalysis(self, test_name: str, reference_name: str):
         with KratosUnittest.WorkFolderScope(test_name, __file__):
@@ -46,27 +44,30 @@ class KratosFreeSurfaceGeneralTests(KratosUnittest.TestCase):
         self.print_reference_values = False
 
     def checkResults(self, reference_file_name: str):
+        """Write results to a temporary file and compare its contents to the reference file."""
+
+        output_file_name = "free_surface_general_tests_tmp.csv"
+        with open(output_file_name, 'w') as output_file:
+            output_file.write("#ID, DISTANCE\n")
+            for node in self.main_model_part.Nodes:
+                dist = node.GetSolutionStepValue(KratosMultiphysics.DISTANCE,0)
+                output_file.write("{0}, {1}\n".format(node.Id, dist))
+
+        # Overwrite reference values if requested
         if self.print_reference_values:
-            with open(reference_file_name + '.csv','w') as ref_file:
-                ref_file.write("#ID, DISTANCE\n")
-                for node in self.main_model_part.Nodes:
-                    dist = node.GetSolutionStepValue(KratosMultiphysics.DISTANCE,0)
-                    ref_file.write("{0}, {1}\n".format(node.Id, dist))
-        else:
-            with open(reference_file_name + '.csv','r') as reference_file:
-                reference_file.readline() # skip header
-                line = reference_file.readline()
+            shutil.copy(output_file_name, reference_file_name)
 
-                for node in self.main_model_part.Nodes:
-                    values = [ float(i) for i in line.rstrip('\n ').split(',') ]
-                    reference_dist = values[1]
+        # Compare to reference file, and delete temporary output
+        comparison_parameters = KratosMultiphysics.Parameters("""{
+            "comparison_type"       : "csv_file",
+            "remove_output_file"    : true
+        }""")
+        comparison_parameters.AddString("reference_file_name", reference_file_name)
+        comparison_parameters.AddString("output_file_name", output_file_name)
+        comparison_parameters.AddDouble("tolerance", self.check_tolerance)
+        CompareTwoFilesCheckProcess(comparison_parameters).Execute()
 
-                    distance = node.GetSolutionStepValue(KratosMultiphysics.DISTANCE)
-                    self.assertAlmostEqual(reference_dist, distance, delta = self.check_tolerance)
 
-                    line = reference_file.readline()
-                if line != '': # If we did not reach the end of the reference file
-                    self.fail("The number of nodes in the mdpa is smaller than the number of nodes in the output file")
 
 
 if __name__ == "__main__":
