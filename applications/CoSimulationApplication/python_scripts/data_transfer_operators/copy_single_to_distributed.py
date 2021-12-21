4b7f9ff0108487141e9720e859df5171b2dfcebb
philbucher <philipp.bucher@tum.de>
2021-12-21 15:03:34 +0100

    renamed

--- applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_distributed.py
+++ applications/CoSimulationApplication/python_scripts/data_transfer_operators/copy_single_to_distributed.py
@@ -1,61 +1,6 @@
-# Importing the base class
-from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_data_transfer_operator import CoSimulationDataTransferOperator
+from KratosMultiphysics.kratos_utilities import IssueDeprecationWarning
+from . import copy_single_to_multiple
 
 def Create(settings):
-    return CopySingleToDistributed(settings)
-
-class CopySingleToDistributed(CoSimulationDataTransferOperator):
-    """DataTransferOperator to take one single value and set it to all values on another interface.
-    Used e.g. for FSI with SDof, where the SDof has one value and the fluid interface has many
-    """
-    def _ExecuteTransferData(self, from_solver_data, to_solver_data, transfer_options):
-        if not to_solver_data.IsDefinedOnThisRank():
-            return
-
-        data_value = 0.0
-        rank_for_broadcast = 0
-        if from_solver_data.IsDefinedOnThisRank():
-            data_value = from_solver_data.GetData()
-            if data_value.size == 1: # this is the rank that actually contains the value
-                data_value = data_value[0]
-                rank_for_broadcast = from_solver_data.GetModelPart().GetCommunicator().GetPID()
-
-        data_comm = to_solver_data.model_part.GetCommunicator().GetDataCommunicator()
-        data_value = data_comm.Broadcast(data_value, rank_for_broadcast)
-
-        to_solver_values = to_solver_data.GetData()
-        to_solver_values.fill(data_value)
-
-        # the order is IMPORTANT here!
-        if "swap_sign" in transfer_options.GetStringArray():
-            to_solver_values *= (-1)
-        if "distribute_values" in transfer_options.GetStringArray():
-            to_solver_values /= data_comm.SumAll(to_solver_data.Size())
-        if "add_values" in transfer_options.GetStringArray():
-            to_solver_values += to_solver_data.GetData()
-
-        to_solver_data.SetData(to_solver_values)
-
-    def _Check(self, from_solver_data, to_solver_data):
-        # check the from_solver_data
-        if from_solver_data.IsDefinedOnThisRank():
-            if not from_solver_data.is_scalar_variable:
-                raise Exception('Variable of interface data "{}" of solver "{}" has to be a scalar!'.format(from_solver_data.name, from_solver_data.solver_name))
-
-        # make sure there is only one value
-        if from_solver_data.IsDefinedOnThisRank():
-            data_size = from_solver_data.Size()
-            if from_solver_data.IsDistributed():
-                data_size = from_solver_data.GetModelPart().GetCommunicator().GetDataCommunicator().SumAll(data_size)
-
-            if not data_size == 1:
-                raise Exception('Interface data "{}" of solver "{}" requires to be of size 1, got: {}'.format(from_solver_data.name, from_solver_data.solver_name, data_size))
-
-        # check the to_solver_data
-        if to_solver_data.IsDefinedOnThisRank():
-            if not to_solver_data.is_scalar_variable:
-                raise Exception('Variable of interface data "{}" of solver "{}" has to be a scalar!'.format(to_solver_data.name, to_solver_data.solver_name))
-
-    @classmethod
-    def _GetListAvailableTransferOptions(cls):
-        return ["swap_sign", "distribute_values", "add_values"]
+    IssueDeprecationWarning('CopySingleToDistributed', 'please use "copy_single_to_multiple" instead')
+    return copy_single_to_multiple.CopySingleToMultiple(settings)
