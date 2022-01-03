c0eed04f9923a8cbb31848a28e9e9fb88ffdf94d
Armin Geiser <armin.geiser@tum.de>
2022-01-03 09:22:31 +0100

    fix apps and dependencies

--- scripts/wheels/linux/configure.sh
+++ scripts/wheels/linux/configure.sh
@@ -26,12 +26,12 @@ add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
 # add_app ${KRATOS_APP_DIR}/DEMApplication
 add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication
 # add_app ${KRATOS_APP_DIR}/ParticleMechanicsApplication;
-# add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication;
+add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication;
 # add_app ${KRATOS_APP_DIR}/DamApplication;
 # add_app ${KRATOS_APP_DIR}/PoromechanicsApplication;
 # add_app ${KRATOS_APP_DIR}/FSIApplication;
 # add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication;
-add_app ${KRATOS_APP_DIR}/EigenSolversApplication;
+# add_app ${KRATOS_APP_DIR}/EigenSolversApplication;
 add_app ${KRATOS_APP_DIR}/LinearSolversApplication;
 # add_app ${KRATOS_APP_DIR}/ConstitutiveLawsApplication;
 # add_app ${KRATOS_APP_DIR}/FemToDemApplication;
