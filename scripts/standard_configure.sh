4d5ece4b404d3958bb2b59fedc33b59eab8e7e8e
PhiHo-eng <74133157+PhiHo-eng@users.noreply.github.com>
2022-01-03 17:48:03 +0100

    Update scripts/standard_configure.sh
    Co-authored-by: Philipp Bucher <philipp.bucher@tum.de>

--- scripts/standard_configure.sh
+++ scripts/standard_configure.sh
@@ -34,7 +34,6 @@ export KRATOS_APPLICATIONS=
 add_app ${KRATOS_APP_DIR}/LinearSolversApplication
 add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
 add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
-add_app ${KRATOS_APP_DIR}/TopologyOptimizationApplication
 
 # Clean
 clear
