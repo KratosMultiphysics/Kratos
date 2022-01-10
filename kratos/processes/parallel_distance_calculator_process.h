088674319570e9f2f8f2e9a17b6729e5c1b6a286
DanielDiez <ddiez@europe.altair.com>
2022-01-10 10:47:18 +0100

    override

--- kratos/processes/parallel_distance_calculator_process.h
+++ kratos/processes/parallel_distance_calculator_process.h
@@ -125,7 +125,7 @@ public:
     ///@{
 
     /// Turn back information as a string.
-    virtual std::string Info() const
+    std::string Info() const override
     {
         std::stringstream buffer;
         buffer << "ParallelDistanceCalculatorProcess" << TDim << "D";
@@ -133,13 +133,13 @@ public:
     };
 
     /// Print information about this object.
-    virtual void PrintInfo(std::ostream& rOStream) const
+    void PrintInfo(std::ostream& rOStream) const override
     {
         rOStream << "ParallelDistanceCalculatorProcess" << TDim << "D";
     };
 
     /// Print object's data.
-    virtual void PrintData(std::ostream& rOStream) const {};
+    void PrintData(std::ostream& rOStream) const override {};
 
     ///@}
     ///@name Friends
