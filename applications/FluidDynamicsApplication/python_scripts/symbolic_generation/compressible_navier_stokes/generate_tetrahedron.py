938faaf3f26c002568f60922d37ae5820b2537bc
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2022-01-13 17:57:05 +0100

    Aesthetics

--- applications/FluidDynamicsApplication/python_scripts/symbolic_generation/compressible_navier_stokes/generate_tetrahedron.py
+++ applications/FluidDynamicsApplication/python_scripts/symbolic_generation/compressible_navier_stokes/generate_tetrahedron.py
@@ -2,9 +2,8 @@ import pathlib
 import os
 
 import KratosMultiphysics
-from KratosMultiphysics.FluidDynamicsApplication.symbolic_generation.compressible_navier_stokes. \
-    compressible_navier_stokes_symbolic_generator import CompressibleNavierStokesSymbolicGenerator
-
+from KratosMultiphysics.FluidDynamicsApplication.symbolic_generation.compressible_navier_stokes \
+    .compressible_navier_stokes_symbolic_generator import CompressibleNavierStokesSymbolicGenerator
 
 parameters = KratosMultiphysics.Parameters("""
 {
