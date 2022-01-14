64f41d1c32bfa6ed2cce3f5fae2ca6a8a77ce8a1
Riccardo Rossi <rrossi@cimne.upc.edu>
2022-01-14 10:01:31 +0100

    adding python buffer behaviour for SystemVector

--- kratos/python/add_sparse_matrices_to_python.cpp
+++ kratos/python/add_sparse_matrices_to_python.cpp
@@ -41,14 +41,18 @@ void AddSparseMatricesToPython(pybind11::module& m)
     //************************************************************************************************************
     //************************************************************************************************************
     py::class_<SparseGraph<IndexType>, SparseGraph<IndexType>::Pointer >(m, "SparseGraph")
+    .def(py::init<>())
     .def(py::init<IndexType>())
     .def("GetComm", &SparseGraph<IndexType>::GetComm)
     .def("pGetComm", &SparseGraph<IndexType>::pGetComm)
     .def("Size", &SparseGraph<IndexType>::Size)
     .def("IsEmpty", &SparseGraph<IndexType>::IsEmpty)
-    .def("AddEnty", &SparseGraph<IndexType>::AddEntry)
+    .def("AddEntry", &SparseGraph<IndexType>::AddEntry)
     .def("Finalize", &SparseGraph<IndexType>::Finalize)
     .def("GetGraph", &SparseGraph<IndexType>::GetGraph)
+    .def("AddEntries", [](SparseGraph<IndexType>& self, std::vector<IndexType>& indices){
+        self.AddEntries(indices);
+    })
     .def("ExportSingleVectorRepresentation", &SparseGraph<IndexType>::ExportSingleVectorRepresentation)
     .def("__str__", PrintObject<SparseGraph<IndexType>>);
 
@@ -60,7 +64,10 @@ void AddSparseMatricesToPython(pybind11::module& m)
     .def("GetComm", &SparseContiguousRowGraph<IndexType>::GetComm)
     .def("pGetComm", &SparseContiguousRowGraph<IndexType>::pGetComm)
     .def("Size", &SparseContiguousRowGraph<IndexType>::Size)
-    .def("AddEnty", &SparseContiguousRowGraph<IndexType>::AddEntry)
+    .def("AddEntry", &SparseContiguousRowGraph<IndexType>::AddEntry)
+    .def("AddEntries", [](SparseContiguousRowGraph<IndexType>& self, std::vector<IndexType>& indices){
+        self.AddEntries(indices);
+    })
     .def("Finalize", &SparseContiguousRowGraph<IndexType>::Finalize)
     .def("GetGraph", &SparseContiguousRowGraph<IndexType>::GetGraph)
     .def("ExportSingleVectorRepresentation", &SparseContiguousRowGraph<IndexType>::ExportSingleVectorRepresentation)
@@ -76,6 +83,8 @@ void AddSparseMatricesToPython(pybind11::module& m)
     .def("GetComm", &CsrMatrix<double,IndexType>::GetComm)
     .def("pGetComm", &CsrMatrix<double,IndexType>::pGetComm)
     .def("SetValue", &CsrMatrix<double,IndexType>::SetValue)
+    .def("Size1", &CsrMatrix<double,IndexType>::size1)
+    .def("Size2", &CsrMatrix<double,IndexType>::size2)
     .def("size1", &CsrMatrix<double,IndexType>::size1)
     .def("size2", &CsrMatrix<double,IndexType>::size2)
     .def("nnz", &CsrMatrix<double,IndexType>::nnz)
@@ -122,12 +131,14 @@ void AddSparseMatricesToPython(pybind11::module& m)
 
     //************************************************************************************************************
     //************************************************************************************************************
-    py::class_<SystemVector<double,IndexType>, SystemVector<double,IndexType>::Pointer >(m, "SystemVector")
+    py::class_<SystemVector<double,IndexType>, SystemVector<double,IndexType>::Pointer >(m, "SystemVector", py::buffer_protocol())
+    .def(py::init<IndexType>())
     .def(py::init<IndexType, DataCommunicator&>())
     .def(py::init<SparseGraph<IndexType>&>())
     .def(py::init<SparseContiguousRowGraph<IndexType>&>())
     .def("GetComm", &SystemVector<double,IndexType>::GetComm)
     .def("pGetComm", &SystemVector<double,IndexType>::pGetComm)
+    .def("Size", &SystemVector<double,IndexType>::size)
     .def("size", &SystemVector<double,IndexType>::size)
     .def("Clear", &SystemVector<double,IndexType>::Clear)
     .def("SetValue", &SystemVector<double,IndexType>::SetValue)
@@ -135,10 +146,23 @@ void AddSparseMatricesToPython(pybind11::module& m)
     .def("BeginAssemble", &SystemVector<double,IndexType>::BeginAssemble)
     .def("FinalizeAssemble", &SystemVector<double,IndexType>::FinalizeAssemble)
     .def("BeginAssemble", &SystemVector<double,IndexType>::BeginAssemble)
-    .def("Assemble", [](SystemVector<double,IndexType>& rV, 
+    .def("data", [](SystemVector<double,IndexType>& self){
+       return self.data();
+    })
+    .def_buffer([](SystemVector<double,IndexType>& self) -> py::buffer_info {
+        return py::buffer_info(
+            &(self.data())[0],                               /* Pointer to buffer */
+            sizeof(double),                          /* Size of one scalar */
+            py::format_descriptor<double>::format(), /* Python struct-style format descriptor */
+            1,                                      /* Number of dimensions */
+            { self.size() },                 /* Buffer dimensions */
+            { sizeof(double)  }             /* Strides (in bytes) for each index */
+        );
+    })
+    .def("Assemble", [](SystemVector<double,IndexType>& self, 
                         Vector& values, 
                         std::vector<IndexType>& indices){
-       rV.Assemble(values,indices);
+       self.Assemble(values,indices);
     })
 
     //inplace
@@ -156,6 +180,7 @@ void AddSparseMatricesToPython(pybind11::module& m)
     .def("__getitem__", [](const SystemVector<double,IndexType>& self, const unsigned int i){return self[i];} )
     .def("__str__", PrintObject<SystemVector<double,IndexType>>)
     ;
+
 
 }
 
