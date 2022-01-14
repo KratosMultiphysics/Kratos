4968c3647a1dd9e428a332c3b0f77654c37678a6
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2022-01-14 17:56:52 +0100

    Changed test to checksum

--- applications/FluidDynamicsApplication/tests/compressible_navier_stokes_symbolic_generator_test.py
+++ applications/FluidDynamicsApplication/tests/compressible_navier_stokes_symbolic_generator_test.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 import KratosMultiphysics
 import KratosMultiphysics.KratosUnittest as KratosUnittest
@@ -30,7 +31,8 @@ class CompressibleNavierStokesSymbolicGeneratorTest(KratosUnittest.TestCase):
         except FileNotFoundError:
             pass
 
-    def _RunTest(self, geometry_name):
+    def _RunAndHashResult(self, geometry_name):
+        """Runs the symbolic generator and computes the sha512 hash of the output file"""
         parameters = self._GetParameters(geometry_name)
 
         with KratosUnittest.WorkFolderScope(".", __file__):
@@ -41,23 +43,28 @@ class CompressibleNavierStokesSymbolicGeneratorTest(KratosUnittest.TestCase):
             generator.Write()
 
             with open(parameters["Parameters"]["output_filename"].GetString(), "r") as f:
-                result = f.readlines()
-
-            with open(parameters["reference"].GetString(), "r") as f:
-                reference = f.readlines()
+                result = hashlib.sha512(f.read().encode("utf-8"))
 
             self.RemoveTestOutput(parameters)
 
-        self.assertEqual(result, reference)
+        return result.hexdigest()
+
 
     def testGeneratorTetra(self):
-        self._RunTest("tetrahedron")
+        reference_hash = "63c2a914c7127f4272bb653739aef97507ddf396f21a21bf4a31198640878fa93b8a3f7028f12cdef1ca8deda8b1bf9fa8331211b026ece3680d0dc40cc733df"
+        obtained_hash  = self._RunAndHashResult("tetrahedron")
+        self.assertEqual(reference_hash, obtained_hash)
 
     def testGeneratorTriangle(self):
-        self._RunTest("triangle")
+        reference_hash = "8fd1983a5cd95f1c5c1504f3a5096672d1e8e714c0bde1c12f42d8ca2f04f58e6d01530d66a1cdb0832b09404663f295941614c5ee1f47aa28b8ee1c97f7d96f"
+        obtained_hash  = self._RunAndHashResult("triangle")
+        self.assertEqual(reference_hash, obtained_hash)
 
     def testGeneratorQuad(self):
-        self._RunTest("quadrilateral")
+        reference_hash = "f23fe07628e37ea74ab73d296c24b47dadf7b9dd1c38cf62f792de8843419114ea2b504ffce1e404b8b9c612b1003bafcafa5a0f8de15d00ce67d91a43021312"
+        obtained_hash  = self._RunAndHashResult("quadrilateral")
+        self.assertEqual(reference_hash, obtained_hash)
+
 
 
 if __name__ == '__main__':
