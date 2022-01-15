f5a7f766c5fdaba31befe8c4a0e9b98b411efc38
rlrangel <rrangel@cimne.upc.edu>
2022-01-16 00:10:35 +0100

    advancing...

--- applications/ThermalDEMApplication/python_scripts/KratosThermalDEMAnalysis.py
+++ applications/ThermalDEMApplication/python_scripts/KratosThermalDEMAnalysis.py
@@ -4,11 +4,11 @@ from KratosMultiphysics.ThermalDEMApplication import *
 from KratosMultiphysics.ThermalDEMApplication.thermal_dem_analysis import ThermalDEMAnalysis
 
 class ThermalDEMAnalysisWithFlush(ThermalDEMAnalysis):
-    def __init__(self,model,parameters)
+    def __init__(self,model,parameters):
 	    super(ThermalDEMAnalysisWithFlush,self).__init__(model,parameters)
 
 if __name__ == "__main__":
     model = Model()
-    with open('ProjectParameters.json','r') as parameter_file:
+    with open('ProjectParametersDEM.json','r') as parameter_file:
         parameters = Parameters(parameter_file.read())
     ThermalDEMAnalysisWithFlush(model,parameters).Run()
--- applications/ThermalDEMApplication/python_scripts/thermal_dem_analysis.py
+++ applications/ThermalDEMApplication/python_scripts/thermal_dem_analysis.py
@@ -1,15 +1,21 @@
-import os
-import sys
-import math
-import time as timer
-
+from   importlib import import_module
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.DEMApplication as DEM
-from   KratosMultiphysics.DEMApplication.DEM_analysis_stage import DEM_analysis_stage
+from   KratosMultiphysics.DEMApplication.DEM_analysis_stage import DEMAnalysisStage
 
-BaseAnalysis = DEM_analysis_stage.DEMAnalysisStage
-
-class ThermalDEMAnalysis(BaseAnalysis):
+class ThermalDEMAnalysis(DEMAnalysisStage):
 
     def __init__(self, model, parameters):
-        pass
+        super().__init__(model, parameters)
+
+    def _CreateSolver(self):
+        def SetSolverStrategy():
+            strategy_file_name = self.DEM_parameters["solver_settings"]["strategy"].GetString()
+            imported_module = import_module("KratosMultiphysics.ThermalDEMApplication" + "." + strategy_file_name)
+            return imported_module
+
+        return SetSolverStrategy().ExplicitStrategy(self.all_model_parts,
+                                                    self.creator_destructor,
+                                                    self.dem_fem_search,
+                                                    self.DEM_parameters,
+                                                    self.procedures)
