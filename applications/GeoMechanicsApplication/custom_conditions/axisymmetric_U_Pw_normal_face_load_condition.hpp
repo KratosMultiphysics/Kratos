dfb0bc84fb0cba7100c6d33613138dae1e7c780e
Vahid Galavi <vahid.galavi@gmail.com>
2022-01-16 15:56:44 +0100

    add axisymmetric Neumann BC and refactoring. Bug in finding neighbours of 3D elements

--- applications/GeoMechanicsApplication/custom_conditions/axisymmetric_U_Pw_normal_face_load_condition.hpp
+++ applications/GeoMechanicsApplication/custom_conditions/axisymmetric_U_Pw_normal_face_load_condition.hpp
@@ -8,19 +8,18 @@
 //  License:         geo_mechanics_application/license.txt
 //
 //
-//  Main authors:    Ignasi de Pouplana,
-//                   Vahid Galavi
+//  Main authors:    Vahid Galavi
 //
 
 
-#if !defined(KRATOS_GEO_U_PW_NORMAL_FACE_LOAD_CONDITION_H_INCLUDED )
-#define  KRATOS_GEO_U_PW_NORMAL_FACE_LOAD_CONDITION_H_INCLUDED
+#if !defined(KRATOS_GEO_AXISYMMETRIC_U_PW_NORMAL_FACE_LOAD_CONDITION_H_INCLUDED )
+#define  KRATOS_GEO_AXISYMMETRIC_U_PW_NORMAL_FACE_LOAD_CONDITION_H_INCLUDED
 
 // Project includes
 #include "includes/serializer.h"
 
 // Application includes
-#include "custom_conditions/U_Pw_condition.hpp"
+#include "custom_conditions/U_Pw_normal_face_load_condition.hpp"
 #include "custom_utilities/condition_utilities.hpp"
 #include "geo_mechanics_application_variables.h"
 
@@ -28,12 +27,13 @@ namespace Kratos
 {
 
 template< unsigned int TDim, unsigned int TNumNodes >
-class KRATOS_API(GEO_MECHANICS_APPLICATION) UPwNormalFaceLoadCondition : public UPwCondition<TDim,TNumNodes>
+class KRATOS_API(GEO_MECHANICS_APPLICATION)
+    AxisymmetricUPwNormalFaceLoadCondition : public UPwNormalFaceLoadCondition<TDim,TNumNodes>
 {
 
 public:
 
-    KRATOS_CLASS_INTRUSIVE_POINTER_DEFINITION( UPwNormalFaceLoadCondition );
+    KRATOS_CLASS_INTRUSIVE_POINTER_DEFINITION( AxisymmetricUPwNormalFaceLoadCondition );
 
     typedef std::size_t IndexType;
 	typedef Properties PropertiesType;
@@ -42,21 +42,21 @@ public:
     typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
     typedef Vector VectorType;
     typedef Matrix MatrixType;
-    using UPwCondition<TDim,TNumNodes>::mThisIntegrationMethod;
+    using UPwNormalFaceLoadCondition<TDim,TNumNodes>::mThisIntegrationMethod;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     // Default constructor
-    UPwNormalFaceLoadCondition() : UPwCondition<TDim,TNumNodes>() {}
+    AxisymmetricUPwNormalFaceLoadCondition() : UPwNormalFaceLoadCondition<TDim,TNumNodes>() {}
 
     // Constructor 1
-    UPwNormalFaceLoadCondition( IndexType NewId, GeometryType::Pointer pGeometry ) : UPwCondition<TDim,TNumNodes>(NewId, pGeometry) {}
+    AxisymmetricUPwNormalFaceLoadCondition( IndexType NewId, GeometryType::Pointer pGeometry ) : UPwNormalFaceLoadCondition<TDim,TNumNodes>(NewId, pGeometry) {}
 
     // Constructor 2
-    UPwNormalFaceLoadCondition( IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties ) : UPwCondition<TDim,TNumNodes>(NewId, pGeometry, pProperties) {}
+    AxisymmetricUPwNormalFaceLoadCondition( IndexType NewId, GeometryType::Pointer pGeometry, PropertiesType::Pointer pProperties ) : UPwNormalFaceLoadCondition<TDim,TNumNodes>(NewId, pGeometry, pProperties) {}
 
     // Destructor
-    ~UPwNormalFaceLoadCondition() override {}
+    ~AxisymmetricUPwNormalFaceLoadCondition() override {}
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -66,29 +66,12 @@ public:
 
 protected:
 
-    struct NormalFaceLoadVariables
-    {
-        array_1d<double,TNumNodes> NormalStressVector;
-        array_1d<double,TNumNodes> TangentialStressVector;
-    };
-
     // Member Variables
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    void CalculateRHS(VectorType& rRightHandSideVector,
-                      const ProcessInfo& CurrentProcessInfo) override;
-
-    void InitializeConditionVariables(NormalFaceLoadVariables& rVariables, const GeometryType& Geom);
-
-    void CalculateTractionVector(array_1d<double,TDim>& rTractionVector,
-                                 const Matrix& Jacobian,
-                                 const Matrix& NContainer,
-                                 const NormalFaceLoadVariables& Variables,
-                                 const unsigned int& GPoint);
-
-    virtual double CalculateIntegrationCoefficient(const IndexType PointNumber,
-                                                   const GeometryType::IntegrationPointsArrayType& IntegrationPoints) const;
+    double CalculateIntegrationCoefficient( const IndexType PointNumber,
+                                            const GeometryType::IntegrationPointsArrayType& IntegrationPoints ) const override;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -112,8 +95,8 @@ private:
         KRATOS_SERIALIZE_LOAD_BASE_CLASS( rSerializer, Condition )
     }
 
-}; // class UPwNormalFaceLoadCondition.
+}; // class AxisymmetricUPwNormalFaceLoadCondition.
 
 } // namespace Kratos.
 
-#endif // KRATOS_GEO_U_PW_NORMAL_FACE_LOAD_CONDITION_H_INCLUDED defined
+#endif // KRATOS_GEO_AXISYMMETRIC_U_PW_NORMAL_FACE_LOAD_CONDITION_H_INCLUDED defined
