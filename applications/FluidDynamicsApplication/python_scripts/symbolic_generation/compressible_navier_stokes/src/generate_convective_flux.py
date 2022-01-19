6a493fa92edbf81a37f22255813363c2d9998d9b
Eduard Gómez Escandell <egomez@cimne.upc.edu>
2022-01-19 09:54:08 +0100

    [Fluid] Refactoring of compressible Navier-Stokes symbolic element generation (#9426)
    * Cleaned up directory
    
    * Started
    
    * Finished shape functions
    
    * fixup
    
    * Started symbolic generator
    
    * Started template
    
    * Implemented residual projections
    
    * Removed auto-formater includes
    
    * Implemented RHS
    
    * Bugfix
    
    * Modified tri template to exclude non-specialized methods
    
    * Added quad element to CMakeLists.txt
    
    * Added missing header
    
    * Improvements
    
    * Dealt with element size
    
    * Registered element
    
    * Removed unnecessary casts
    
    * Fixed shape function gradients
    
    * Minor changes
    
    * Fixed compilation errors
    
    * Added specifications
    
    * Fixed compilation error in template
    
    * Started symbolic refactoring
    
    * Improvements
    
    * Fixed some errors
    
    * Moved symbolic to python_scripts
    
    * Created test
    
    * Improvements
    
    * Fixed some prints
    
    * Created reference with old generator
    
    * Fixed test
    
    * Stylistic changes
    
    * Bugfixes
    
    * Removed newer Python features
    
    * Removed deprecated files
    
    * Removed deleted files from CMAKE
    
    * Removed deleted file dependencies
    
    * Removed references to deleted files
    
    * Reorganized symbolic NS dir
    
    * Fix
    
    * Symbolic now accepts quad
    
    * Added quad to test
    
    * Added warning to generated files
    
    * Split templates by geometry
    
    * Clarified SFINAE in element header
    
    * Generated new elements
    
    * Fixes
    
    * Bugfix
    
    * Changed pragmas to Kratos AtomicUtils
    
    * Updated test references
    
    * Improved test filename
    
    * Cleaned up tetrahedral mesh orientation check
    
    * Aesthetics
    
    * Removed deprecated quad generator
    
    * Trying to make pylint happier
    
    * Making pylint happier 2
    
    * Update applications/FluidDynamicsApplication/custom_elements/compressible_navier_stokes_explicit.h
    
    Co-authored-by: Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
    
    * Update applications/FluidDynamicsApplication/custom_elements/compressible_navier_stokes_explicit.h
    
    Co-authored-by: Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
    
    * Renamed elements
    
    * Renamed method
    
    * Added warning when orientation check is skipped
    
    * Removed outdated documentation
    
    * Renamed lambda_ to lamb
    
    * Added sympy to CI docker
    
    * typo in filename
    
    * Fixup
    
    * Un-moved unrelated symbolic generators
    
    * Renamed defines
    
    * Added extra constrain to quad shape functions
    
    * Quickfix
    
    * Error Fix
    
    * Changed test to checksum
    
    * Added sympy to another docker
    
    * Renamed lambda_ to lamb
    
    * Undone changes to docker files
    
    * Down-graded to Python 3.5
    
    * Inline static functions to fix unity issues
    
    * Update compressible_navier_stokes_explicit.h
    
    * Fixed syntax
    
    Co-authored-by: Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>

---
+++ applications/FluidDynamicsApplication/python_scripts/symbolic_generation/compressible_navier_stokes/src/generate_convective_flux.py
@@ -0,0 +1,55 @@
+import sympy
+from KratosMultiphysics.FluidDynamicsApplication.symbolic_generation.compressible_navier_stokes \
+    .src.defines import CompressibleNavierStokesDefines as defs
+
+
+def ComputeEulerJacobianMatrix(dofs, params):
+    """This function calculates the Euler Jacobian matrix for convection"""
+
+    # Auxiliary variables
+    dim = params.dim
+    gamma = params.gamma
+    rho = dofs[0]
+    mom = []
+    vel = []
+    mom_prod = 0.0
+    for i in range(dim):
+        mom.append(dofs[i + 1])
+        vel.append(dofs[i + 1] / rho)
+        mom_prod += dofs[i + 1]**2
+    e_tot = dofs[dim + 1]
+    p = (gamma - 1) * (e_tot - 0.5 * mom_prod / rho)
+
+    # Define and fill the convective flux matrix
+    E = defs.Matrix('E', dim + 2, dim, real=True)
+    for j in range(dim):
+        E[0, j] = mom[j]
+        for d in range(dim):
+            E[1 + d, j] = mom[j]*vel[d]
+            if j == d:
+                E[1 + d, j] += p
+        E[dim + 1, j] = vel[j] * (e_tot + p)
+
+    # Define and derive the Euler Jacobian matrix
+    A = []
+    for j in range(dim):
+        A_j = defs.Matrix('A_j', dim + 2, dim + 2, real=True)
+        for m in range(dim + 2):
+            for n in range(dim + 2):
+                A_j[m, n] = sympy.diff(E[m, j], dofs[n])
+        A.append(A_j)
+
+    return A
+
+
+def printA(A, params):
+    dim = params.dim
+    tmp = []
+    print("The convective matrix is:\n")
+    for j in range(0, dim):
+        tmp = A[j]
+        for i in range(0, dim + 2):
+            for k in range(0, dim + 2):
+                print("A[{},{},{}]=[]\n".format(j, i, k, tmp[i, k]))
+
+    return 0
