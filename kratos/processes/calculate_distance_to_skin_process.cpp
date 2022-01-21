3422549b710104672d900bc1a6dc8dc28841a072
marcnunezc <marcnc94@gmail.com>
2022-01-21 11:08:11 +0100

    correct indent

--- kratos/processes/calculate_distance_to_skin_process.cpp
+++ kratos/processes/calculate_distance_to_skin_process.cpp
@@ -55,20 +55,20 @@ namespace Kratos
 		mRayCastingRelativeTolerance = mParameters["ray_casting_relative_tolerance"].GetDouble();
 	}
 
-    template<std::size_t TDim>
-    const Parameters CalculateDistanceToSkinProcess<TDim>::GetDefaultParameters() const
-    {
-        Parameters default_parameters = Parameters(R"(
-        {
-            "ray_casting_relative_tolerance"                   : 1.0e-8
+	template<std::size_t TDim>
+	const Parameters CalculateDistanceToSkinProcess<TDim>::GetDefaultParameters() const
+	{
+		Parameters default_parameters = Parameters(R"(
+		{
+            "ray_casting_relative_tolerance"        : 1.0e-8
         })" );
 
-		// Getting base class default parameters
-		const Parameters base_default_parameters = CalculateDiscontinuousDistanceToSkinProcess<TDim>::GetDefaultParameters();
-		default_parameters.RecursivelyAddMissingParameters(base_default_parameters);
+        // Getting base class default parameters
+        const Parameters base_default_parameters = CalculateDiscontinuousDistanceToSkinProcess<TDim>::GetDefaultParameters();
+        default_parameters.RecursivelyAddMissingParameters(base_default_parameters);
 
-		return default_parameters;
-    }
+        return default_parameters;
+	}
 
 	template<std::size_t TDim>
 	CalculateDistanceToSkinProcess<TDim>::~CalculateDistanceToSkinProcess()
