5469144983a1fc8e790d8f67d679a2de3cf6bfd0
Alejandro <alejandro.cornejo23@gmail.com>
2022-01-25 13:45:04 +0100

    better

--- kratos/utilities/math_utils.h
+++ kratos/utilities/math_utils.h
@@ -1613,18 +1613,22 @@ public:
         // Manual multiplication
         if (ClearMatrix)
             rA.clear();
+
         double Dkl, DklBlj;
         IndexType k,l,j,i, D_size;
         D_size = rD.size1();
 
-        for (k = 0; k < D_size; ++k) 
-            for (l = 0; l < D_size; ++l) 
+        for (k = 0; k < rD.size1(); ++k) {
+            for (l = 0; l < rD.size2(); ++l) {
                 Dkl = rD(k, l);
-                for (j = 0; j < B_size2; ++j) 
+                for (j = 0; j < rB.size2(); ++j) {
                     DklBlj = Dkl * rB(l, j);
-                    for (i = 0; i < B_size2; ++i)
+                    for (i = 0; i < rB.size2(); ++i) {
                         rA(i, j) += rB(k, i) * DklBlj;
-    }
+                    }
+                }
+            }
+        }
 
     /**
      * @brief Calculates the product operation BDB'
