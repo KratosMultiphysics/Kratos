f80a7005b4030836897fdb75bad642a19e8620ea
Miguel Maso <mmaso@cimne.upc.edu>
2022-01-29 18:10:17 +0100

    add shock capturing to process info

--- applications/ShallowWaterApplication/python_scripts/boussinesq_solver.py
+++ applications/ShallowWaterApplication/python_scripts/boussinesq_solver.py
@@ -47,8 +47,9 @@ class BoussinesqSolver(ShallowWaterBaseSolver):
 
     def _SetProcessInfo(self):
         super()._SetProcessInfo()
-        self.main_model_part.ProcessInfo.SetValue(KM.STABILIZATION_FACTOR, self.settings["stabilization_factor"].GetDouble())
         self.main_model_part.ProcessInfo.SetValue(SW.RELATIVE_DRY_HEIGHT, self.settings["relative_dry_height"].GetDouble())
+        self.main_model_part.ProcessInfo.SetValue(KM.STABILIZATION_FACTOR, self.settings["stabilization_factor"].GetDouble())
+        self.main_model_part.ProcessInfo.SetValue(SW.SHOCK_STABILIZATION_FACTOR, self.settings["shock_stabilization_factor"].GetDouble())
 
     def _CreateScheme(self):
         return SW.ResidualBasedAdamsMoultonScheme()
