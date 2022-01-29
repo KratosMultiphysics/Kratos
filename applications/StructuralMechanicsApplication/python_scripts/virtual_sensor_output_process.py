fbf69d93e1c9b3d79e8ac7a9cdd69f1c5d28ad88
Altug Emiroglu <altug.emiroglu@gmail.com>
2022-01-29 01:33:52 +0100

    working version of feedback to CSV

--- applications/StructuralMechanicsApplication/python_scripts/virtual_sensor_output_process.py
+++ applications/StructuralMechanicsApplication/python_scripts/virtual_sensor_output_process.py
@@ -1,5 +1,7 @@
 import KratosMultiphysics
 import KratosMultiphysics.kratos_utilities as kratos_utils
+#import KratosMultiphysics.IntegrationValuesExtrapolationToNodesProcess as extrapolate_to_nodes_process
+import KratosMultiphysics.integration_values_extrapolation_to_nodes_process as extrapolate_to_nodes_process
 
 import os
 import pandas as pd
@@ -10,7 +12,6 @@ def Factory(settings, model):
     if(type(settings) != KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
     return VirtualSensorOutputProcess(model, settings["Parameters"])
-
 class VirtualSensorOutputProcess(KratosMultiphysics.OutputProcess):
     def __init__(self, model, settings):
         super().__init__()
@@ -29,10 +30,9 @@ class VirtualSensorOutputProcess(KratosMultiphysics.OutputProcess):
                 "nodal_solution_step_data_variables": [
                     "DISPLACEMENT"
                 ],
-                "nodal_data_value_variables": [],
-                "element_data_value_variables": [],
-                "condition_data_value_variables": [],
-                "gauss_point_variables_extrapolated_to_nodes": []
+                "gauss_point_variables_extrapolated_to_nodes": [
+                    "CAUCHY_STRESS_TENSOR"
+                ]
             }
             """)
         self.settings = settings.Clone()
@@ -50,43 +50,84 @@ class VirtualSensorOutputProcess(KratosMultiphysics.OutputProcess):
         self.next_output = 0.0
         self.__ScheduleNextOutput() # required here esp for restart
 
-        self.map_variable_names_to_variables = {
-            "DISPLACEMENT" : KratosMultiphysics.DISPLACEMENT,
-            "ACCELERATION" : KratosMultiphysics.ACCELERATION,
-            "ROTATION"      : KratosMultiphysics.ROTATION
+        self.map_variable_names_to_kratos_variables = {
+            "DISPLACEMENT"          : KratosMultiphysics.DISPLACEMENT,
+            "ACCELERATION"          : KratosMultiphysics.ACCELERATION,
+            "ROTATION"              : KratosMultiphysics.ROTATION,
+            "CAUCHY_STRESS_TENSOR"  : KratosMultiphysics.CAUCHY_STRESS_TENSOR
         }
         
     def PrintOutput(self):
 
+        num_dimensions = 3
+
+        coordinates_param = self.settings["coordinates"]
+        coordinates = []
+        for coordinate in coordinates_param:
+            coordinates.append(coordinate.GetDouble())
+
+        distances = []
+        for node in self.model_part.Nodes:
+            node_distance = npla.norm([node.X - coordinates[0], node.Y - coordinates[1], node.Z - coordinates[2]])
+            distances.append(node_distance)
+        sum_distances = np.sum(distances)
+        nodal_weights = np.array(distances)/sum_distances
+
         for nodal_solution_step_data_variable in self.settings["nodal_solution_step_data_variables"]:
             variable_name = nodal_solution_step_data_variable.GetString()
-            kratos_variable = self.map_variable_names_to_variables[variable_name]
-            
-            coordinates_param = self.settings["coordinates"]
-            coordinates = []
-            for coordinate in coordinates_param:
-                coordinates.append(coordinate.GetDouble())
+            kratos_variable = self.map_variable_names_to_kratos_variables[variable_name]
 
-            distances = []
             nodal_values = []
             for node in self.model_part.Nodes:
-                node_distance = npla.norm([node.X - coordinates[0], node.Y - coordinates[1], node.Z - coordinates[2]])
-                distances.append(node_distance)
                 nodal_values.append(node.GetSolutionStepValue(kratos_variable))
-            sum_distances = np.sum(distances)
-            nodal_weights = np.array(distances)/sum_distances
 
-            node_ctr = 0
-            sensor_value = np.zeros(3)
-            for node in self.model_part.Nodes:
-                sensor_value += nodal_weights[node_ctr] * nodal_values[node_ctr]
-                node_ctr += 1
+            sensor_value = np.zeros(num_dimensions)
+            for i_node, node in enumerate(self.model_part.Nodes):
+                sensor_value += nodal_weights[i_node] * nodal_values[i_node]
             
             output_path = self.settings["output_path"].GetString()
             sub_model_part_name = self.settings["model_part_name"].GetString().split(".")[-1]
             output_file_path = os.path.join(output_path,sub_model_part_name+"_"+variable_name[0:3]+"_"+str(self.model_part.ProcessInfo[KratosMultiphysics.STEP]))+".npy"
             with open(output_file_path, "wb") as sensor_step_value_file:
                 np.save(sensor_step_value_file, sensor_value)
+
+        extrapolation_process_settings = KratosMultiphysics.Parameters("""
+            {
+                "model_part_name"            : "",
+                "echo_level"                 : 4,
+                "average_variable"           : "NODAL_AREA",
+                "area_average"               : true,
+                "list_of_variables"          : [],
+                "extrapolate_non_historical" : true
+            }
+            """
+        )
+
+        extrapolation_process_settings["list_of_variables"].SetStringArray(self.settings["gauss_point_variables_extrapolated_to_nodes"].GetStringArray())
+        extrapolation_process_settings["model_part_name"].SetString(self.settings["model_part_name"].GetString())
+        local_extrapolation_process = KratosMultiphysics.IntegrationValuesExtrapolationToNodesProcess(self.model_part, extrapolation_process_settings)
+        local_extrapolation_process.ExecuteBeforeSolutionLoop()
+        local_extrapolation_process.ExecuteFinalizeSolutionStep()
+        
+        for gauss_point_variable in self.settings["gauss_point_variables_extrapolated_to_nodes"]:
+            variable_name = gauss_point_variable.GetString()
+            kratos_variable = self.map_variable_names_to_kratos_variables[variable_name]
+
+            nodal_values = []
+            for node in self.model_part.Nodes:
+                nodal_values.append(node.GetValue(kratos_variable))
+
+            sensor_value = np.zeros((num_dimensions,num_dimensions))
+            for i_node, node in enumerate(self.model_part.Nodes):
+                sensor_value += nodal_weights[i_node] * nodal_values[i_node]
+
+            output_path = self.settings["output_path"].GetString()
+            sub_model_part_name = self.settings["model_part_name"].GetString().split(".")[-1]
+            output_file_path = os.path.join(output_path,sub_model_part_name+"_"+variable_name[0:3]+"_"+str(self.model_part.ProcessInfo[KratosMultiphysics.STEP]))+".npy"
+            with open(output_file_path, "wb") as sensor_step_value_file:
+                np.save(sensor_step_value_file, sensor_value)
+
+        local_extrapolation_process.ExecuteFinalize()
 
         self.__ScheduleNextOutput()
 
@@ -110,11 +151,12 @@ class VirtualSensorOutputProcess(KratosMultiphysics.OutputProcess):
 
         csv_df = pd.read_csv(csv_file_path, delimiter='\t')
 
+        output_path = self.settings["output_path"].GetString()
+        sub_model_part_name = self.settings["model_part_name"].GetString().split(".")[-1]
+
         for nodal_solution_step_data_variable in self.settings["nodal_solution_step_data_variables"]:
             variable_name = nodal_solution_step_data_variable.GetString()
 
-            output_path = self.settings["output_path"].GetString()
-            sub_model_part_name = self.settings["model_part_name"].GetString().split(".")[-1]
             output_file_path_prefix = os.path.join(output_path,sub_model_part_name+"_"+variable_name[0:3]+"_")
             #str(self.model_part.ProcessInfo[KratosMultiphysics.STEP]))+".npy"
             result_matrix = np.zeros((number_of_steps,num_dimensions))
@@ -128,13 +170,24 @@ class VirtualSensorOutputProcess(KratosMultiphysics.OutputProcess):
                 csv_label = variable_name[0:3]+"_"+csv_label_template+"_S_0"+dimension_labels[i_dim]+"0_0"
                 csv_df[csv_label] = list(result_matrix[:,i_dim]) + num_missing_values*[None]
         
+        for gauss_point_variable in self.settings["gauss_point_variables_extrapolated_to_nodes"]:
+            variable_name = gauss_point_variable.GetString()
+
+            output_file_path_prefix = os.path.join(output_path,sub_model_part_name+"_"+variable_name[0:3]+"_")
+            result_matrix = np.zeros((number_of_steps,num_dimensions))
+            for i_step in range(0,number_of_steps):
+                output_file_path = output_file_path_prefix + str(i_step+1) + ".npy"
+                with open(output_file_path, "rb") as sensor_step_value_file:
+                    result_matrix[i_step,:] = np.diag(np.load(sensor_step_value_file))
+
+            num_missing_values = len(csv_df)-np.shape(result_matrix)[0]
+            for i_dim in range(num_dimensions):
+                csv_label = variable_name[0:3]+"_"+csv_label_template+"_S_0"+dimension_labels[i_dim]+"0_0"
+                csv_df[csv_label] = list(result_matrix[:,i_dim]) + num_missing_values*[None]
+
         new_csv_file_path = csv_file_path.split(".csv")[0]+"_sim.csv"
 
         csv_df.to_csv(new_csv_file_path, sep='\t', index=False)
-
-        #raise NotImplementedError("This function is not implemented yet!")
-        
-        #TODO: implement this function to put everything back into the CSV file
 
     def __ScheduleNextOutput(self):
         if self.settings["output_interval"].GetDouble() > 0.0: # Note: if == 0, we'll just always print
