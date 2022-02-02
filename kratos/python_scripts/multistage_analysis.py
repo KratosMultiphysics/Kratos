b10814eb5508ea894eef9392149b1ad764d08f98
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2022-02-02 15:42:01 +0100

    Towards modelers

--- kratos/python_scripts/multistage_analysis.py
+++ kratos/python_scripts/multistage_analysis.py
@@ -3,52 +3,98 @@ import time
 import importlib
 
 import KratosMultiphysics
+from KratosMultiphysics.modeler_factory import KratosModelerFactory
+from KratosMultiphysics.operation_factory import KratosOperationFactory
 
 class MultistageAnalysis(object):
 
     def __init__(self, model, project_parameters) -> None:
         self.model = model
-        self.current_stage_index = None
+        self.current_stage_name = None
         self.settings = project_parameters
-        self.stages_list = self.__CreateStagesList()
+        self.stages_map = self.__CreateStagesMap()
+
+    def Check(self):
+        for stage_name in self.settings["execution_list"].GetStringArray():
+            self.CheckStage(stage_name)
+
+    def CheckStage(self, stage_name):
+        if self.settings["stages"][stage_name].Has("stage_postprocess"):
+            if self.settings["stages"][stage_name]["stage_postprocess"].Has("modelers"):
+                err_msg = f"Found 'modelers' field in 'stage_postprocess' of stage {stage_name}."
+                err_msg += f" Place the 'modelers' section in the next stage 'stage_postprocess'."
+                raise Exception(err_msg)
 
     def Run(self):
-        for i_stage in range(self.GetNumberOfStages()):
-            self.current_stage_index = i_stage
-            self.InitializeCurrentStage()
-            self.RunCurrentStage()
-            self.FinalizeCurrentStage()
+        # First check all the stages input
+        self.Check()
 
-    def InitializeCurrentStage(self):
-        pass
+        # Run the stages list
+        for stage_name in self.settings["execution_list"].GetStringArray():
+            self.current_stage_name = stage_name
+            self.RunCurrentStagePreprocess()
+            self.RunCurrentStage()
+            self.RunCurrentStagePostprocess()
+
+    def RunCurrentStagePreprocess(self):
+        if self.settings["stages"][self.GetCurrentStageName()].Has("stage_preprocess"):
+            if self.settings["stages"][self.GetCurrentStageName()]["stage_preprocess"].Has("modelers"):
+                for modeler in self.__GetModelers():
+                    #TODO: Should we do it in this way or to call the SetupGeometryModel for all the modelers, then the PrepareGeometryModel for all and so on
+                    #TODO: We (Charlie & Ruben) think that it must be done as in here. However this is against what we have currently implemented in the AnalysisStage
+                    modeler.SetupGeometryModel()
+                    modeler.PrepareGeometryModel()
+                    modeler.SetupModelPart()
+
+            if self.settings["stages"][self.GetCurrentStageName()]["stage_preprocess"].Has("operations"):
+                for operation in self.__GetOperations("stage_preprocess"):
+                    operation.Execute()
 
     def RunCurrentStage(self):
-        self.stages_list[self.current_stage_index].Run()
+        self.GetCurrentStage().Run()
 
-    def FinalizeCurrentStage(self):
-        pass
+    def RunCurrentStagePostprocess(self):
+        if self.settings["stages"][self.GetCurrentStageName()].Has("stage_postprocess"):
+            if self.settings["stages"][self.GetCurrentStageName()]["stage_postprocess"].Has("operations"):
+                for operation in self.__GetOperations("stage_postprocess"):
+                    operation.Execute()
 
     def GetNumberOfStages(self):
-        return len(self.stages_list)
+        return len(self.stages_map)
 
     def GetCurrentStage(self):
-        return self.stages_list[self.current_stage_index]
+        return self.stages_map[self.current_stage_name]
 
-    def GetCurrentStageIndex(self):
-        return self.current_stage_index
+    def GetCurrentStageName(self):
+        return self.current_stage_name
 
-    def __CreateStagesList(self):
-        stages_list = []
-        for stage_settings in self.settings["stages"]:
-            analysis_stage_module_name = stage_settings["analysis_stage"].GetString()
+    def __GetModelers(self):
+        #TODO: Add error thrown for wrong execution_point argument
+        execution_point_settings = self.settings["stages"][self.GetCurrentStageName()]["stage_preprocess"]
+
+        #TODO: To be changed to KratosOperationFactory
+        factory = KratosModelerFactory()
+        return factory.ConstructListOfModelers(self.model, execution_point_settings["modelers"])
+
+    def __GetOperations(self, execution_point):
+        #TODO: Add error thrown for wrong execution_point argument
+        execution_point_settings = self.settings["stages"][self.GetCurrentStageName()][execution_point]
+
+        factory = KratosOperationFactory(self.model)
+        return factory.ConstructListOfOperations(execution_point_settings["operations"])
+
+    def __CreateStagesMap(self):
+        stages_map = {}
+        for stage_name in self.settings["execution_list"].GetStringArray():
+            analysis_stage_module_name = self.settings["stages"][stage_name]["analysis_stage"].GetString()
             analysis_stage_class_name = analysis_stage_module_name.split('.')[-1]
             analysis_stage_class_name = ''.join(x.title() for x in analysis_stage_class_name.split('_'))
 
             analysis_stage_module = importlib.import_module(analysis_stage_module_name)
             analysis_stage_class = getattr(analysis_stage_module, analysis_stage_class_name)
 
-            stages_list.append(self.__CreateAnalysisStageWithFlushInstance(analysis_stage_class, self.model,  KratosMultiphysics.Parameters(stage_settings)))
-        return stages_list
+            stages_map[stage_name] = self.__CreateAnalysisStageWithFlushInstance(analysis_stage_class, self.model,  KratosMultiphysics.Parameters(self.settings["stages"][stage_name]))
+        return stages_map
 
     #TODO: Think about this
     def __CreateAnalysisStageWithFlushInstance(self, cls, global_model, parameters):
