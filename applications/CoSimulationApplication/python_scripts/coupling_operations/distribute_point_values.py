cfd18625d82c20706c6286875e2b92f8e7ce257a
Philipp <philipp.bucher@tum.de>
2022-02-09 11:30:37 +0100

    updates

--- applications/CoSimulationApplication/python_scripts/coupling_operations/distribute_point_values.py
+++ applications/CoSimulationApplication/python_scripts/coupling_operations/distribute_point_values.py
@@ -12,26 +12,37 @@ class DistributePointValuesOperation(CoSimulationCouplingOperation):
         super().__init__(settings, process_info, data_communicator)
 
         solver_name = self.settings["solver"].GetString()
-        data_name = self.settings["data_name"].GetString()
-        self.interface_data = solver_wrappers[solver_name].GetInterfaceData(data_name)
+        self.data_point = solver_wrappers[solver_name].GetInterfaceData(self.settings["data_point_values"].GetString())
+        self.data_dist  = solver_wrappers[solver_name].GetInterfaceData(self.settings["data_distributed_values"].GetString())
 
-        self.redistribution_iterations = self.settings["redistribution_tolerance"].GetInt()
-        self.redistribution_tolerance  = self.settings["redistribution_iterations"].GetDouble()
+        self.redistribution_iterations = self.settings["redistribution_iterations"].GetInt()
+        self.redistribution_tolerance  = self.settings["redistribution_tolerance"].GetDouble()
 
-        if self.interface_data.is_scalar_variable:
-            self.intermediate_variable = KM.KratosGlobals.GetVariable(self.settings["intermediate_variable_scalar"].GetString())
+        entities_to_use = self.settings["entities"].GetString()
+        if entities_to_use == "conditions":
+            self.entities = self.data_point.GetModelPart().Conditions
+        elif entities_to_use == "elements":
+            self.entities = self.data_point.GetModelPart().Elements
         else:
-            self.intermediate_variable = KM.KratosGlobals.GetVariable(self.settings["intermediate_variable_scalar"].GetString())
+            raise Exception('"entities" can only be "conditions" or "elements"!')
 
     def Execute(self):
+        # TODO adapt call if non-hist!
         KM.VariableRedistributionUtility.DistributePointValues(
-            self.interface_data.GetModelPart(),
-            self.interface_data.variable,
-            self.intermediate_variable,
+            self.data_point.GetModelPart(),
+            self.entities,
+            self.data_point.variable,
+            self.data_dist.variable,
             self.redistribution_tolerance,
             self.redistribution_iterations)
 
     def Check(self):
+        return
+        # check input MPs are the same!
+        # Check if have entities
+        # Check if is compatible type => nodal or nodal-non-hist
+        # Check vars have same type => either both are double or both are array3
+
         # currently the redistribution-utility only works with conditions
         num_local_conds = self.interface_data.GetModelPart().GetCommunicator().LocalMesh().NumberOfConditions()
         data_comm = self.interface_data.GetModelPart().GetCommunicator().GetDataCommunicator()
@@ -39,27 +50,15 @@ class DistributePointValuesOperation(CoSimulationCouplingOperation):
         if num_local_conds < 1:
             raise Exception('No conditions found in ModelPart "{}" of interface data "{}" of solver "{}"!'.format(var.Name(), self.interface_data.model_part_name, self.interface_data.name, self.interface_data.solver_name))
 
-        # currently the redistribution-utility only supports historical values on the nodes
-        vars_to_check = [self.intermediate_variable]
-
-        if self.interface_data.is_scalar_variable:
-            vars_to_check.append(KM.MAPPER_SCALAR_PROJECTION_RHS)
-        else:
-            vars_to_check.append(KM.MAPPER_VECTOR_PROJECTION_RHS)
-
-        for var in vars_to_check:
-            if not self.interface_data.GetModelPart().HasNodalSolutionStepVariable(var):
-                raise Exception('Variable "{}" is missing as SolutionStepVariable in ModelPart "{}" of interface data "{}" of solver "{}"!'.format(var.Name(), self.interface_data.model_part_name, self.interface_data.name, self.interface_data.solver_name))
-
     @classmethod
     def _GetDefaultParameters(cls):
         this_defaults = KM.Parameters("""{
-            "solver"                       : "UNSPECIFIED",
-            "data_name"                    : "UNSPECIFIED",
-            "redistribution_tolerance"     : 1e-7,
-            "redistribution_iterations"    : 100,
-            "intermediate_variable_scalar" : "NODAL_PAUX",
-            "intermediate_variable_vector" : "NODAL_VAUX"
+            "solver"                    : "UNSPECIFIED",
+            "data_point_values"         : "UNSPECIFIED",
+            "data_distributed_values"   : "UNSPECIFIED",
+            "entities"                  : "conditions",
+            "redistribution_tolerance"  : 1e-7,
+            "redistribution_iterations" : 100
         }""")
         this_defaults.AddMissingParameters(super()._GetDefaultParameters())
         return this_defaults
