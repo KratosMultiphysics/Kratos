38849d4a1e47005a770954681cdc6112f9bde7c7
tteschemacher <tteschemacher@users.noreply.github.com>
2022-02-09 11:07:09 +0100

    Update applications/IgaApplication/python_scripts/output_eigen_values_process.py
    Co-authored-by: Philipp Bucher <philipp.bucher@tum.de>

--- applications/IgaApplication/python_scripts/output_eigen_values_process.py
+++ applications/IgaApplication/python_scripts/output_eigen_values_process.py
@@ -5,11 +5,4 @@ import KratosMultiphysics.IgaApplication as IGA
 def Factory(settings, model):
     if not (isinstance(settings, KratosMultiphysics.Parameters)):
         raise Exception("Expected input shall be a Parameters object, encapsulating a json string")
-    return IgaEigenValuesOutputProcess(model, settings["Parameters"])
-
-class IgaEigenValuesOutputProcess(KratosMultiphysics.Process):
-
-    def __init__(self, model, params):
-        KratosMultiphysics.Process.__init__(self)
-        print(params)
-        self.process = IGA.OutputEigenValuesProcess(model, params)
+    return IGA.OutputEigenValuesProcess(model, params)
