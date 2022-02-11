143e373fd76c4db1e1e96ee8218bdc147769d601
migelmaso <mmaso@cimne.upc.edu>
2022-02-11 12:19:46 +0100

    missing gravity

--- applications/ShallowWaterApplication/tests/cpp_tests/test_depth_integration_process.cpp
+++ applications/ShallowWaterApplication/tests/cpp_tests/test_depth_integration_process.cpp
@@ -52,6 +52,7 @@ void FillModelParts2D(ModelPart& rVolumeModelPart, ModelPart& rInterfaceModelPar
     root_model_part.AddNodalSolutionStepVariable(MOMENTUM);
     root_model_part.AddNodalSolutionStepVariable(HEIGHT);
     root_model_part.GetProcessInfo().SetValue(DOMAIN_SIZE, 2);
+    root_model_part.GetProcessInfo().SetValue(GRAVITY, array_1d<double,3>({0.0, -9.81, 0.0}));
     StructuredMeshGeneratorProcess(geometry, rVolumeModelPart, mesher_parameters).Execute();
 
     auto id = root_model_part.NumberOfNodes();
@@ -84,6 +85,7 @@ void FillModelParts3D(ModelPart& rVolumeModelPart, ModelPart& rInterfaceModelPar
     root_model_part.AddNodalSolutionStepVariable(MOMENTUM);
     root_model_part.AddNodalSolutionStepVariable(HEIGHT);
     root_model_part.GetProcessInfo().SetValue(DOMAIN_SIZE, 3);
+    root_model_part.GetProcessInfo().SetValue(GRAVITY, array_1d<double,3>({0.0, 0.0, -9.81}));
     StructuredMeshGeneratorProcess(geometry, rVolumeModelPart, mesher_parameters).Execute();
 
     auto id = root_model_part.NumberOfNodes();
@@ -118,7 +120,6 @@ KRATOS_TEST_CASE_IN_SUITE(DepthIntegrationProcess2D, ShallowWaterApplicationFast
     {
         "volume_model_part_name"    : "model_part.volume",
         "interface_model_part_name" : "model_part.interface",
-        "direction_of_integration"  : [0.0, 1.0, 0.0],
         "store_historical_database" : false
     })");
     DepthIntegrationProcess<2>(model, process_parameters).Execute();
@@ -145,7 +146,6 @@ KRATOS_TEST_CASE_IN_SUITE(DepthIntegrationProcess3D, ShallowWaterApplicationFast
     {
         "volume_model_part_name"    : "model_part.volume",
         "interface_model_part_name" : "model_part.interface",
-        "direction_of_integration"  : [0.0, 0.0, 1.0],
         "store_historical_database" : false
     })");
     DepthIntegrationProcess<3>(model, process_parameters).Execute();
