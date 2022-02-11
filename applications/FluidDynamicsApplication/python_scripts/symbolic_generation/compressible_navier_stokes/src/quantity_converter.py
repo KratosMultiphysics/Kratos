90ab2a609e35c5a1024287190897aa73330861eb
Eduard Gómez <egomez@cimne.upc.edu>
2022-02-11 16:30:39 +0100

    Created quantity converter
    Removed redundant methods from PrimitiveVariables

---
+++ applications/FluidDynamicsApplication/python_scripts/symbolic_generation/compressible_navier_stokes/src/quantity_converter.py
@@ -0,0 +1,210 @@
+import sympy
+
+import KratosMultiphysics.sympy_fe_utilities as KratosSympy
+
+class QuantityConverter:
+    """
+    Converts between conservative and primitive variables.
+
+    When represented as vectors, their meanings are:
+    - `V`: Primitive magnitudes: `[p, vel, T]'`
+    - `U`: Conservative magnitudes: `[rho, mom, e_tot]'`
+
+    When represented as classes, they are named `primitives` and `conservatives`
+    """
+    def __init__(self):
+        raise RuntimeError("This class is purely static.")
+
+    @classmethod
+    def Primitives(cls, U, params):
+        vel = cls.velocity(U)
+        T = cls.temperature(U, params, vel=vel)
+        p = cls.pressure(U, params, T=T)
+
+        vel.simplify()
+        T.simplify()
+        p.simplify()
+
+        return (p, vel, T)
+
+    @classmethod
+    def PrimitivesGradients(cls, U, grad_U, params):
+        T = cls.temperature(U, params)
+
+        grad_V = cls.velocity_gradient(U, grad_U)
+        grad_T = cls.temperature_gradient(U, grad_U, params, T=T)
+        grad_P = cls.pressure_gradient(U, grad_U, params, T=T, grad_T=grad_T)
+
+        grad_V.simplify()
+        grad_T.simplify()
+        grad_P.simplify()
+
+        return (grad_P.T, grad_V.T, grad_T.T)
+
+    @classmethod
+    def density(cls, primitives, params):
+        "Returns the density as a function of primitive variables"
+        return primitives.P / (cls.gas_constant_R(params) * primitives.T)
+
+    @classmethod
+    def momentum(cls, primitives, params=None, density=None):
+        "Returns the momentum as a function of primitive variables"
+        if density is None and params is None:
+            raise RuntimeError("Either params or density must be specified")
+
+        if density is not None:
+            density = cls.density(primitives, params)
+
+        return primitives / density
+
+    @classmethod
+    def total_energy(cls, primitives, params, rho=None):
+        """Returns the density as a function of primitive variables"""
+        if rho is not None:
+            rho = cls.density(primitives, params)
+
+        v_2 = sum([vi*vi for vi in primitives.V])
+        T = primitives.T
+
+        return rho * (0.5 * v_2 + params.cv*T)
+
+    @classmethod
+    def velocity(cls, U):
+        return U[1:-1] / U[0]
+
+    @classmethod
+    def temperature(cls, U, params, vel=None):
+        rho = U[0]
+        if vel is None:
+            vel = cls.velocity(U)
+        e_tot = U[-1]
+
+        e_kinetic = 0.5 * rho * sum([v**2 for v in vel])
+        return (e_tot - e_kinetic) / (rho * params.c_v)
+
+    @classmethod
+    def pressure(cls, U, params, T=None):
+        rho = U[0]
+        R = cls.gas_constant_R(params)
+        if T is None:
+            T = cls.temperature(U, params)
+        return rho * R * T
+
+    @classmethod
+    def velocity_gradient(cls, U, DU):
+        """
+        Velocity gradient. Gradients defined as:
+
+        grad_f := df_j/dx_i
+
+        """
+        rho = U[0]
+        grad_rho = DU[0]
+        mom = U[1:-1]
+        grad_mom = DU[1:-1]
+        return (grad_mom*rho - mom*grad_rho) / rho**2
+
+
+    @classmethod
+    def temperature_gradient(cls, U, DU, params, vel=None, T=None, grad_vel=None):
+        """
+        Temperature gradient.  Gradients defined as:
+
+        grad_f := df_j/dx_i
+
+        Explanation
+        ---
+        Temperature is defined as:
+        ```
+            T = (e_t - e_k) / (rho * c_v)
+            [with e_k = 1/2 * rho * V²]
+        ```
+        where `e_k` is the kinetic energy.
+
+        The gradient is:
+        ```
+            grad(T) = grad(e_t - e_k) / (rho * c_v)   -   (e_t - e_k)/(rho²*c_v) * grad(rho)
+        ```
+        Rearranging yields:
+        ```
+            grad(T) = (grad(e_t) - grad(e_k)) / (rho * c_v)   -   T/rho * grad(rho)
+            [with grad(e_k) = rho*transp(V)*grad(V) + 1/2 * V² * grad(rho)]
+        ```
+
+        """
+        if vel is None:
+            vel = cls.velocity(U)
+        V2 = vel.transpose() * vel
+
+        if grad_vel is None:
+            grad_vel = cls.velocity_gradient(U, DU)
+
+        if T is None:
+            T = cls.temperature(U, params, vel)
+
+        rho = U[0]
+        grad_rho = DU[0]
+        grad_e_tot = DU[-1]
+
+        grad_e_k = rho*vel.transpose()*grad_vel + 0.5 * V2 * grad_rho
+        return (grad_e_tot - grad_e_k)/(rho*params.c_v) - T/rho * grad_rho
+
+    @classmethod
+    def pressure_gradient(cls, U, DU, params, T=None, grad_T=None):
+        """
+        Pressure gradient.  Gradients defined as:
+
+        grad_f := df_j/dx_i
+
+        """
+        if T is None:
+            T = cls.temperature(U, params)
+
+        if grad_T is None:
+            grad_T = cls.temperature_gradient(U, DU, params, T=T)
+
+        rho = U[0]
+        grad_rho = DU[0]
+
+        R = cls.gas_constant_R(params)
+
+        return (grad_rho*T + rho*grad_T) * R
+
+    @classmethod
+    def density_gradient(cls, primitives, params):
+        R =  cls.gas_constant_R(params)
+        return (primitives.T*primitives.grad_P - primitives.grad_T*primitives.P) / (R * primitives.T**2)
+
+    @classmethod
+    def gas_constant_R(cls, params):
+        return (params.gamma - 1) * params.c_v
+
+    @classmethod
+    def dVdU(cls, primitives, U):
+        """
+        Returns the derivative of the primitive vector V with respect to the conservative vector U
+        """
+        blocksize = primitives.ndims+2
+        V = primitives.AsVector()
+        D = sympy.zeros(blocksize, blocksize)
+        for i in range(blocksize):
+            for j in range(blocksize):
+                D[i,j] = sympy.diff(V[i], U[j])
+        return D
+
+    @classmethod
+    def SubstitutePrimitivesWithConservatives(cls, expr, primitives, U, grad_U, params):
+        """
+        Substitutes the primitive variables in the expression `expr` with their definitions as functions of constervative variables `U`
+        """
+        (P, V, T) = QuantityConverter.Primitives(U, params)
+
+        KratosSympy.SubstituteScalarValue(expr, primitives.P, P)
+        KratosSympy.SubstituteMatrixValue(expr, primitives.V, V)
+        KratosSympy.SubstituteScalarValue(expr, primitives.T, T)
+
+        (grad_P, grad_V, grad_T) = QuantityConverter.PrimitivesGradients(U, grad_U, params)
+
+        KratosSympy.SubstituteScalarValue(expr, primitives.grad_P, grad_P)
+        KratosSympy.SubstituteMatrixValue(expr, primitives.grad_V, grad_V)
+        KratosSympy.SubstituteScalarValue(expr, primitives.grad_T, grad_T)
--- applications/FluidDynamicsApplication/python_scripts/symbolic_generation/compressible_navier_stokes/src/symbolic_parameters.py
+++ applications/FluidDynamicsApplication/python_scripts/symbolic_generation/compressible_navier_stokes/src/symbolic_parameters.py
@@ -2,6 +2,8 @@ import sympy
 import KratosMultiphysics.sympy_fe_utilities as KratosSympy
 from KratosMultiphysics.FluidDynamicsApplication.symbolic_generation.compressible_navier_stokes.src.defines \
     import CompressibleNavierStokesDefines as defs
+from quantity_converter import QuantityConverter
+
 
 class FormulationParameters:
     "Dictionary of the constant parameters used in the Variational Formulation"
@@ -33,6 +35,7 @@ class ShockCapturingNodalParameters:
         self.beta  = defs.Vector('data.beta_sc_nodes', geometry.nnodes, positive=True)  # Nodal artificial bulk viscosity
         self.lamb  = defs.Vector('data.lamb_sc_nodes', geometry.nnodes, positive=True)  # Nodal artificial bulk viscosity
 
+
 class PrimitiveMagnitudes:
     """
     Primitive variables and their gradients.
@@ -57,181 +60,16 @@ class PrimitiveMagnitudes:
     ```
     """
 
-    GAUSSIAN = 1
-    NODAL = 2
+    def __init__(self, geometry):
+        self.P = sympy.Symbol('pressure', real=True)
+        self.V = defs.Vector('velocity', geometry.ndims, real=True)
+        self.T = sympy.Symbol('temperature', real=True)
+        self.grad_P = defs.Vector('grad_pressure', geometry.ndims, real=True)
+        self.grad_V = defs.Matrix('grad_velocity', geometry.ndims, geometry.ndims, real=True)
+        self.grad_T = defs.Vector('grad_temperature', geometry.ndims, real=True)
 
-    def __init__(self, geometry, params, Ug, grad_Ug, mode):
-        if mode == "nodal":
-            self.mode = self.NODAL
-            self.P = sympy.Symbol('pressure', real=True)
-            self.V = defs.Vector('velocity', geometry.ndims, real=True)
-            self.T = sympy.Symbol('temperature', real=True)
-            self.grad_P = defs.Matrix('grad_pressure', 1, geometry.ndims, real=True)
-            self.grad_V = defs.Matrix('grad_velocity', geometry.ndims, geometry.ndims, real=True)
-            self.grad_T = defs.Matrix('grad_temperature', 1, geometry.ndims, real=True)
-        elif mode == "gaussian":
-            self.mode = self.GAUSSIAN
-            (self.P, self.V, self.T) = self._PrimitivesFromConservatives(Ug, params)
-            (self.grad_P, self.grad_V, self.grad_T) = self._PrimitiveGradientsFromConservatives(Ug, grad_Ug, params)
-        else:
-            raise ValueError("Unrecognized magnitude interpolation mode.")
         self.nnodes = geometry.nnodes
         self.ndims = geometry.ndims
 
-    def InterpolateAndSubstitute(self, expr, U_nodes, N_gauss, DN_DX_gauss, params):
-        """
-        If necessary, the primitive variable symbols are substituted by their
-        expressions at the gauss points in terms of the conservative variables.
-
-        Arguments
-        ---------
-        expr:        The expression to perform the substitution in.
-        U_nodes:     The values of the conservative variables at the nodes.
-        N_gauss:     The values of the shape functions at this gauss point.
-        DN_DX_gauss: The values of the shape functions' gradients at this gauss point.
-        params:      The physical parameters of the formulation.
-        """
-        if self.mode == self.GAUSSIAN:
-            # Interpolation done during __init__
-            return
-
-        V_nodes = defs.ZeroMatrix(self.nnodes, self.ndims)
-        T_nodes = defs.ZeroVector(self.nnodes)
-        P_nodes = defs.ZeroVector(self.nnodes)
-
-        for n in range(self.nnodes):
-            (P, V, T) = self._PrimitivesFromConservatives(U_nodes[n, :], params)
-            P_nodes[n] = P
-            V_nodes[n, :] = V.transpose()
-            T_nodes[n] = T
-
-        P_g = P_nodes.transpose() * N_gauss
-        V_g = V_nodes.transpose() * N_gauss
-        T_g = T_nodes.transpose() * N_gauss
-
-        # Asserts to avoid unintelligible errors from the depths of sympy
-        assert(sympy.shape(V_g) == (self.ndims,1))
-        assert(sympy.shape(T_g) == (1,1))
-        assert(sympy.shape(P_g) == (1,1))
-
-        P_g = P_g[0, 0]
-        T_g = T_g[0, 0]
-
-        # Gradients
-        grad_P_g = KratosSympy.DfiDxj(DN_DX_gauss, P_nodes)
-        grad_V_g = KratosSympy.DfiDxj(DN_DX_gauss, V_nodes)
-        grad_T_g = KratosSympy.DfiDxj(DN_DX_gauss, T_nodes)
-
-        # Substitution
-        KratosSympy.SubstituteScalarValue(expr, self.P, P_g)
-        KratosSympy.SubstituteMatrixValue(expr, self.V, V_g)
-        KratosSympy.SubstituteScalarValue(expr, self.T, T_g)
-        KratosSympy.SubstituteMatrixValue(expr, self.grad_P, grad_P_g)
-        KratosSympy.SubstituteMatrixValue(expr, self.grad_V, grad_V_g)
-        KratosSympy.SubstituteMatrixValue(expr, self.grad_T, grad_T_g)
-
-    @classmethod
-    def _PrimitivesFromConservatives(cls, U, params):
-        rho = U[0]
-        mom = sympy.Matrix(U[1:-1])
-        e_tot = U[-1]
-
-        V = cls._velocity(rho, mom)
-        T = cls._temperature(rho, V, e_tot, params)
-        P = cls._pressure(rho, T, params)
-
-        V.simplify()
-        T.simplify()
-        P.simplify()
-
-        return (P, V, T)
-
-    @classmethod
-    def _PrimitiveGradientsFromConservatives(cls, U, grad_U, params):
-        rho = U[0]
-        mom = sympy.Matrix(U[1:-1])
-        e_tot = U[-1]
-
-        V = cls._velocity(rho, mom)
-        T = cls._temperature(rho, V, e_tot, params)
-
-        grad_rho = sympy.Matrix(grad_U[0, :])                   # d rho / dx_i
-        grad_mom = sympy.Matrix(grad_U[1:-1, :])                # d V_j / dx_i
-        grad_e_tot = sympy.Matrix(grad_U[-1, :])                # d e_tot / dx_i
-
-        grad_V = cls._velocity_gradient(rho, mom, grad_rho, grad_mom)
-        grad_T = cls._temperature_gradient(rho, e_tot, V, T, grad_rho, grad_e_tot, grad_V, params)
-        grad_P = cls._pressure_gradient(rho, grad_rho, T, grad_T, params)
-
-        grad_V.simplify()
-        grad_T.simplify()
-        grad_P.simplify()
-
-        return (grad_P.T, grad_V.T, grad_T.T)
-
-    @classmethod
-    def _velocity(cls, rho, mom):
-        return mom/rho
-
-    @classmethod
-    def _temperature(cls, rho, vel, e_tot, params):
-        e_kinetic = 0.5 * rho * sum([v**2 for v in vel])
-        return (e_tot - e_kinetic) / (rho * params.c_v)
-
-    @classmethod
-    def _pressure(cls, rho, T, params):
-        R = (params.gamma - 1) * params.c_v
-        return rho * R * T
-
-    @classmethod
-    def _velocity_gradient(cls, rho, mom, grad_rho, grad_mom):
-        """
-        Velocity gradient. Gradients defined as:
-
-        grad_f := df_j/dx_i
-
-        """
-        return (grad_mom*rho - mom*grad_rho) / rho**2
-
-
-    @classmethod
-    def _temperature_gradient(cls, rho, e_tot, vel, T, grad_rho, grad_e_tot, grad_vel, params):
-        """
-        Temperature gradient.  Gradients defined as:
-
-        grad_f := df_j/dx_i
-
-        Explanation
-        ---
-        Temperature is defined as:
-        ```
-            T = (e_t - e_k) / (rho * c_v)
-            [with e_k = 1/2 * rho * V²]
-        ```
-        where `e_k` is the kinetic energy.
-
-        The gradient is:
-        ```
-            grad(T) = grad(e_t - e_k) / (rho * c_v)   -   (e_t - e_k)/(rho²*c_v) * grad(rho)
-        ```
-        Rearranging yields:
-        ```
-            grad(T) = (grad(e_t) - grad(e_k)) / (rho * c_v)   -   T/rho * grad(rho)
-            [with grad(e_k) = rho*transp(V)*grad(V) + 1/2 * V² * grad(rho)]
-        ```
-
-        """
-        V2 = vel.transpose() * vel
-        grad_e_k = rho*vel.transpose()*grad_vel + 0.5 * V2 * grad_rho
-        return (grad_e_tot - grad_e_k)/(rho*params.c_v) - T/rho * grad_rho
-
-    @classmethod
-    def _pressure_gradient(cls, rho, grad_rho, T, grad_T, params):
-        """
-        Pressure gradient.  Gradients defined as:
-
-        grad_f := df_j/dx_i
-
-        """
-        R = (params.gamma - 1) * params.c_v
-        return R * (grad_rho*T + rho*grad_T)
+    def AsVector(self):
+        return sympy.Matrix([self.P, *self.V, self.T]).T
