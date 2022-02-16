b0499134ff3c5cd366f9b288bb82de913cb95781
Eduard Gómez <egomez@cimne.upc.edu>
2022-02-16 12:22:33 +0100

    Mimicked Fluid App structure

--- applications/CompressiblePotentialFlowApplication/python_scripts/potential_flow_analysis.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/potential_flow_analysis.py
@@ -1,8 +1,8 @@
 from KratosMultiphysics.analysis_stage import AnalysisStage
-from KratosMultiphysics.CompressiblePotentialFlowApplication.python_solvers_wrapper_compressible_potential import CreateSolverByParameters
+from KratosMultiphysics.CompressiblePotentialFlowApplication import python_solvers_wrapper_compressible_potential
 
 class PotentialFlowAnalysis(AnalysisStage):
     '''Main script for potential flow simulations.'''
 
     def _CreateSolver(self):
-        return CreateSolverByParameters(self.model, self.project_parameters["solver_settings"], self.parallel_type)
+        return python_solvers_wrapper_compressible_potential.CreateSolver(self.model, self.project_parameters)
