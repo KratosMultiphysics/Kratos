e06c893b9d3a9343b5d2d67b56a671cca841d957
Eduard Gómez <egomez@cimne.upc.edu>
2022-02-16 10:06:20 +0100

    Bugfix

--- applications/CompressiblePotentialFlowApplication/python_scripts/python_solvers_wrapper_compressible_potential.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/python_solvers_wrapper_compressible_potential.py
@@ -1,16 +1,15 @@
-from KratosMultiphysics.CompressiblePotentialFlowApplication import potential_flow_solver
-from KratosMultiphysics.CompressiblePotentialFlowApplication import ale_potential_flow_solver
-from KratosMultiphysics.CompressiblePotentialFlowApplication import potential_flow_adjoint_solver
-
 def CreateSolverByParameters(model, solver_settings, parallel_type):
 
     solver_type = solver_settings["solver_type"].GetString()
 
     if solver_type == "potential_flow":
+        from KratosMultiphysics.CompressiblePotentialFlowApplication import potential_flow_solver
         return potential_flow_solver.CreateSolver(model, solver_settings)
     elif solver_type == "ale_potential_flow":
+        from KratosMultiphysics.CompressiblePotentialFlowApplication import ale_potential_flow_solver
         return ale_potential_flow_solver.CreateSolver(model, solver_settings, parallel_type)
     elif solver_type == "adjoint_potential_flow":
+        from KratosMultiphysics.CompressiblePotentialFlowApplication import potential_flow_adjoint_solver
         return potential_flow_adjoint_solver.CreateSolver(model, solver_settings)
     else:
         raise ValueError("Solver type '"+str(solver_type)+"' not added. Please specify an available solver")
