f5b85adccf50f353a365773aece7e6deef2530d1
Eduard Gómez <egomez@cimne.upc.edu>
2022-02-22 17:41:53 +0100

    Removed extraneous print

--- applications/FluidDynamicsApplication/tests/compressible_navier_stokes_symbolic_generator_formulation_test.py
+++ applications/FluidDynamicsApplication/tests/compressible_navier_stokes_symbolic_generator_formulation_test.py
@@ -364,8 +364,6 @@ class CompressibleNavierStokesSymbolicGeneratorUnitTest(KratosUnitTest.TestCase)
         dummy_geneator = self._DummyGenerator(geometry)
         dummy_geneator.ComputeOSSProjectionsAtGaussPoint(acc, bdf, dUdt, f, forcing_terms, H, 0, mg, params, primitives, projections, res, rg, U, Ug, Un, Unn)
 
-        print(projections["energy"])
-
         N = geometry.N_gauss(0)
         DN_DX = geometry.DN()
 
