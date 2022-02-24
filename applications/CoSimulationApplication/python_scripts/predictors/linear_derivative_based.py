aa80a477057115ef3bb4542d58ec49274bf8c9c4
ashishdarekar <ashish.darekar@tum.de>
2022-02-24 14:27:21 +0100

    remove local changes made from PR#8950

--- applications/CoSimulationApplication/python_scripts/predictors/linear_derivative_based.py
+++ applications/CoSimulationApplication/python_scripts/predictors/linear_derivative_based.py
@@ -2,7 +2,7 @@
 import KratosMultiphysics as KM
 
 # Importing the base class
-from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_predictor import CoSimulationPredictor, HistoricalDataAccessor
+from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_predictor import CoSimulationPredictor
 
 # Other imports
 import KratosMultiphysics.CoSimulationApplication.co_simulation_tools as cs_tools
@@ -22,7 +22,6 @@ class LinearDerivativeBasedPredictor(CoSimulationPredictor):
         data  = self.interface_data.GetData(1)
         derivative_data  = self.interface_derivative_data.GetData(1)
 
-        # TODO this is a hack and currently is only supported by Kratos solvers!
         delta_time = self.interface_data.GetModelPart().ProcessInfo[KM.DELTA_TIME]
         if abs(delta_time) < 1E-15:
             raise Exception("delta-time is almost zero!")
