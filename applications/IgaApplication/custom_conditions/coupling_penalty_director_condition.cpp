eb28d172e220eccb110de6aee8aa782b68939d9b
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2022-03-03 09:28:05 +0100

    implemented penalty coupling for directors between patches

--- applications/IgaApplication/custom_conditions/coupling_penalty_director_condition.cpp
+++ applications/IgaApplication/custom_conditions/coupling_penalty_director_condition.cpp
@@ -14,9 +14,17 @@
 
 // Project includes
 #include "custom_conditions/coupling_penalty_director_condition.h"
+#include "custom_elements/shell_5p_element.h"
+#include "utilities/math_utils.h"
 
 namespace Kratos
 {
+
+    BoundedVector<double, 3> EuclideanDistanceVector(const BoundedVector<double, 3>& a, const BoundedVector<double, 3>& b)
+    {
+        return a - b;
+    }
+
     void CouplingPenaltyDirectorCondition::CalculateAll(
         MatrixType& rLeftHandSideMatrix,
         VectorType& rRightHandSideVector,
@@ -65,51 +73,137 @@ namespace Kratos
             r_geometry_master.DeterminantOfJacobian(determinant_jacobian_vector);
         }
 
-        for (IndexType point_number = 0; point_number < integration_points.size(); point_number++)
+        for (IndexType IntegrationPointIndex = 0; IntegrationPointIndex < integration_points.size(); IntegrationPointIndex++)
         {
-            Matrix N_master = r_geometry_master.ShapeFunctionsValues();
-            Matrix N_slave = r_geometry_slave.ShapeFunctionsValues();
+            Vector N_master = row(r_geometry_master.ShapeFunctionsValues(),IntegrationPointIndex);
+            Vector N_slave = row(r_geometry_slave.ShapeFunctionsValues(), IntegrationPointIndex);
 
-            //FOR DISPLACEMENTS
-            Matrix H = ZeroMatrix(2, mat_size);
-            for (IndexType i = 0; i < number_of_nodes_master; ++i)
-            {
-                IndexType index = 2 * i;
-                H(0, index) = N_master(point_number, i);
-                H(1, index + 1) = N_master(point_number, i);
-            }
+            BoundedVector<double, 3> t_master=ZeroVector(3);
+            for (IndexType i = 0; i < N_master.size(); ++i)
+                t_master += N_master[i] * r_geometry_master[i].GetValue(DIRECTOR);
 
-            for (IndexType i = 0; i < number_of_nodes_slave; ++i)
-            {
-                IndexType index = 2 * (i + number_of_nodes_master);
-                H(0, index)     = -N_slave(point_number, i);
-                H(1, index + 1) = -N_slave(point_number, i);
-            }
+            BoundedVector<double, 3> t_slave = ZeroVector(3);
+            for (IndexType i = 0; i < N_slave.size(); ++i)
+                t_slave += N_slave[i] * r_geometry_slave[i].GetValue(DIRECTOR);
+
+            const BoundedMatrix<double, 3, 3> Pd_master = (IdentityMatrix(3) - outer_prod(t_master, t_master)) / norm_2(t_master);
+            const BoundedMatrix<double, 3, 3> Pd_slave = (IdentityMatrix(3) - outer_prod(t_slave, t_slave)) / norm_2(t_slave);
+            const auto distanceVector = EuclideanDistanceVector(t_master, t_slave);
+            const BoundedMatrix<double, 3, 3> S_master =  (inner_prod(distanceVector, t_master) * (3.0 * outer_prod(t_master, t_master) - IdentityMatrix(3)) - outer_prod(distanceVector,t_master) - trans(outer_prod(distanceVector, t_master)))/ norm_2_square(t_master);
+            const BoundedMatrix<double, 3, 3> S_slave =  (inner_prod(distanceVector, t_slave) * (3.0 * outer_prod(t_slave, t_slave) - IdentityMatrix(3)) - outer_prod(distanceVector, t_slave) - trans(outer_prod(distanceVector, t_slave)))/ norm_2_square(t_slave);
+
+
 
             // Differential area
-            const double penalty_integration = penalty * integration_points[point_number].Weight() * determinant_jacobian_vector[point_number];
+            const double penalty_integration = penalty * integration_points[IntegrationPointIndex].Weight() * determinant_jacobian_vector[IntegrationPointIndex];
 
             // Assembly
             if (CalculateStiffnessMatrixFlag) {
-                noalias(rLeftHandSideMatrix) += prod(trans(H), H) * penalty_integration;
+                Matrix coupleStiffness = ZeroMatrix(mat_size, mat_size);
+                for (IndexType i = 0; i < number_of_nodes_master; ++i)
+                {
+                    IndexType indexI = 2 * i;
+
+                    for (IndexType j = 0; j < number_of_nodes_master; ++j)
+                    {
+                        IndexType indexJ = 2 * j;
+                    const BoundedMatrix<double, 3, 2> BLA_masterI = r_geometry_master[i].GetValue(DIRECTORTANGENTSPACE);
+                    const BoundedMatrix<double, 3, 2> BLA_masterJ = r_geometry_master[j].GetValue(DIRECTORTANGENTSPACE);
+                    const BoundedMatrix<double, 3, 2> tmp3 = prod(Pd_master + S_master, BLA_masterJ); //useless temp due to nonworking ublas prod(prod())
+                    const BoundedMatrix<double, 2, 2> tmp = N_master(j) * N_master(i) *  (prod(trans(BLA_masterI), tmp3) );
+
+                    coupleStiffness(indexI,     indexJ   ) = tmp(0,0);
+                    coupleStiffness(indexI + 1, indexJ   ) = tmp(1,0);
+                    coupleStiffness(indexI,     indexJ +1) = tmp(0,1);
+                    coupleStiffness(indexI + 1, indexJ +1) = tmp(1,1);
+                    }
+                }
+
+                for (IndexType i = 0; i < number_of_nodes_slave; ++i)
+                {
+                    IndexType indexI = 2 * (i + number_of_nodes_master);
+
+                    for (IndexType j = 0; j < number_of_nodes_slave; ++j)
+                    {
+                        IndexType indexJ = 2 * (j + number_of_nodes_master);
+                        const BoundedMatrix<double, 3, 2> BLA_slaveI = r_geometry_slave[i].GetValue(DIRECTORTANGENTSPACE);
+                        const BoundedMatrix<double, 3, 2> BLA_slaveJ = r_geometry_slave[j].GetValue(DIRECTORTANGENTSPACE);
+                        const BoundedMatrix<double, 3, 2> tmp3 = prod(Pd_slave + S_slave, BLA_slaveJ); //useless temp due to nonworking ublas prod(prod())
+                        const BoundedMatrix<double, 2, 2> tmp = N_slave(j) * N_slave(i) * (prod(trans(BLA_slaveI), tmp3));
+
+                        coupleStiffness(indexI, indexJ) = tmp(0, 0);
+                        coupleStiffness(indexI + 1, indexJ) = tmp(1, 0);
+                        coupleStiffness(indexI, indexJ + 1) = tmp(0, 1);
+                        coupleStiffness(indexI + 1, indexJ + 1) = tmp(1, 1);
+                    }
+                }
+
+
+                for (IndexType i = 0; i < number_of_nodes_slave; ++i)
+                {
+                    IndexType indexI = 2 * (i + number_of_nodes_master);
+
+                    for (IndexType j = 0; j < number_of_nodes_master; ++j)
+                    {
+                        IndexType indexJ = 2 * j;
+                        const BoundedMatrix<double, 3, 2> BLA_slaveI = r_geometry_slave[i].GetValue(DIRECTORTANGENTSPACE);
+                        const BoundedMatrix<double, 3, 2> BLA_masterJ = r_geometry_master[j].GetValue(DIRECTORTANGENTSPACE);
+                        const BoundedMatrix<double, 3, 3> tmp2 = prod(Pd_slave, Pd_master); //useless temp due to nonworking ublas prod(prod())
+                        const BoundedMatrix<double, 3, 2> tmp3 = prod(tmp2, BLA_masterJ); //useless temp due to nonworking ublas prod(prod())
+                        const BoundedMatrix<double, 2, 2> tmp = N_master(j) * N_slave(i) * prod(trans(BLA_slaveI), tmp3);
+
+                        coupleStiffness(indexI, indexJ) = tmp(0, 0);
+                        coupleStiffness(indexI + 1, indexJ) = tmp(1, 0);
+                        coupleStiffness(indexI, indexJ + 1) = tmp(0, 1);
+                        coupleStiffness(indexI + 1, indexJ + 1) = tmp(1, 1);
+                    }
+                }
+
+                // this can be skipped later by transposing the thing before and inserting in the correct index for debuggin the follwing lines are currently used
+                for (IndexType i = 0; i < number_of_nodes_master; ++i)
+                {
+                    IndexType indexI = 2 * i;
+
+                    for (IndexType j = 0; j < number_of_nodes_slave; ++j)
+                    {
+                        IndexType indexJ = 2 * (j + number_of_nodes_master);
+                        const BoundedMatrix<double, 3, 2> BLA_masterI = r_geometry_master[i].GetValue(DIRECTORTANGENTSPACE);
+                        const BoundedMatrix<double, 3, 2> BLA_slaveJ = r_geometry_slave[j].GetValue(DIRECTORTANGENTSPACE);
+                        const BoundedMatrix<double, 3, 3> tmp2 = prod(Pd_master, Pd_slave); //useless temp due to nonworking ublas prod(prod())
+                        const BoundedMatrix<double, 3, 2> tmp3 = prod(tmp2, BLA_slaveJ); //useless temp due to nonworking ublas prod(prod())
+                        const BoundedMatrix<double, 2, 2> tmp = N_slave(j) * N_master(i) * (prod(trans(BLA_masterI), tmp3));
+
+                        coupleStiffness(indexI, indexJ) = tmp(0, 0);
+                        coupleStiffness(indexI + 1, indexJ) = tmp(1, 0);
+                        coupleStiffness(indexI, indexJ + 1) = tmp(0, 1);
+                        coupleStiffness(indexI + 1, indexJ + 1) = tmp(1, 1);
+                    }
+                }
+                noalias(rLeftHandSideMatrix) += coupleStiffness * penalty_integration;
             }
             if (CalculateResidualVectorFlag) {
-
-                Vector u(mat_size);
+                Vector coupleForce = ZeroVector(mat_size);
                 for (IndexType i = 0; i < number_of_nodes_master; ++i)
                 {
                     IndexType index = 2 * i;
-                    u[index] = r_geometry_master[i].FastGetSolutionStepValue(DIRECTORINC_X);
-                    u[index + 1] = r_geometry_master[i].FastGetSolutionStepValue(DIRECTORINC_Y);
-                }
-                for (IndexType i = 0; i < number_of_nodes_slave; ++i)
-                {
-                    IndexType index = 2 * i;
-                    u[index]     = r_geometry_slave[i].FastGetSolutionStepValue(DIRECTORINC_X);
-                    u[index + 1] = r_geometry_slave[i].FastGetSolutionStepValue(DIRECTORINC_Y);
+                    const BoundedMatrix<double, 3, 2> BLA_master = r_geometry_master[i].GetValue(DIRECTORTANGENTSPACE);
+                    const BoundedMatrix<double, 3, 2> tmp = prod(Pd_master, BLA_master);
+                    const BoundedVector<double, 2> tmpVec = N_master(i) * prod(distanceVector, tmp);
+                    coupleForce(index) = tmpVec(0);
+                    coupleForce(index + 1) = tmpVec(1);
                 }
 
-                noalias(rRightHandSideVector) -= prod(prod(trans(H), H), u) * penalty_integration;
+                for (IndexType i = 0; i < number_of_nodes_slave; ++i)
+                {
+                    IndexType index = 2 * (i + number_of_nodes_master);
+                    const BoundedMatrix<double, 3, 2> BLA_slave = r_geometry_slave[i].GetValue(DIRECTORTANGENTSPACE);
+                    const BoundedMatrix<double, 3, 2> tmp = prod(Pd_slave, BLA_slave);
+                    const BoundedVector<double, 2> tmpVec = -N_slave(i) * prod(distanceVector, tmp);
+                    coupleForce(index) = tmpVec(0);
+                    coupleForce(index + 1) = tmpVec(1);
+                }
+
+                noalias(rRightHandSideVector) += coupleForce * penalty_integration;
             }
         }
 
