fc9bb947c4ecdd1886f26e97f08276c1c14efe64
noordam <aron.noordam@deltares.nl>
2022-03-17 11:43:58 +0100

    KRATOSGEO-5 pipe elements are now added in a pointer list

--- applications/GeoMechanicsApplication/custom_strategies/strategies/geo_mechanics_newton_raphson_erosion_process_strategy.hpp
+++ applications/GeoMechanicsApplication/custom_strategies/strategies/geo_mechanics_newton_raphson_erosion_process_strategy.hpp
@@ -28,10 +28,10 @@
 namespace Kratos
 {
 
-bool isOpen(Element element) {
-    if (element.IsDefined(ACTIVE))
+bool isOpen(Element* element) {
+    if (element->IsDefined(ACTIVE))
     {
-        return element.Is(ACTIVE);
+        return element->Is(ACTIVE);
     }
     else
         return true;
@@ -105,9 +105,9 @@ public:
         bool Equilibrium = false;
 
         double amax = 0.02; //todo calculate this value
-        double da = 0.0001; // todo calculate this value
+        double da = 0.00001; // todo calculate this value
 
-    	auto PipeElements = GetPipingElements();
+        std::vector<Element*> PipeElements = GetPipingElements();
 
         unsigned int n_el = PipeElements.size();
         //PipeElements.size()
@@ -119,7 +119,8 @@ public:
             prev_pipe_heights.push_back(0);
         }
         // Open tip element of pipe (activate next pipe element)
-        PipeElements.at(0).Set(ACTIVE, true);
+        Element* test_element = PipeElements.at(0);
+        test_element->Set(ACTIVE, true);
 
 
     	// Implement Piping Loop (non-lin picard iteration)
@@ -138,8 +139,8 @@ public:
             // Check if elements are in equilibrium
     		for (auto OpenPipeElement : OpenPipeElements)
             {
-                SteadyStatePwPipingElement& pElement = static_cast<SteadyStatePwPipingElement>(OpenPipeElement);
-                if (!pElement.InEquilibrium(OpenPipeElement.GetProperties(), OpenPipeElement.GetGeometry()))
+                SteadyStatePwPipingElement* pElement = static_cast<SteadyStatePwPipingElement*>(OpenPipeElement);
+                if (!pElement->InEquilibrium(OpenPipeElement->GetProperties(), OpenPipeElement->GetGeometry()))
                 {
                     Equilibrium = false;
                     break;
@@ -153,30 +154,30 @@ public:
                 // Update Piping Elements 
             	for (auto OpenPipeElement : OpenPipeElements)
                 {
-                    auto& pElement = static_cast<SteadyStatePwPipingElement>(OpenPipeElement);
+                    auto pElement = static_cast<SteadyStatePwPipingElement*>(OpenPipeElement);
 
                     // Update here
-                    auto& Geom = OpenPipeElement.GetGeometry();
-                    auto& prop = OpenPipeElement.GetProperties();
+                    auto& Geom = OpenPipeElement->GetGeometry();
+                    auto& prop = OpenPipeElement->GetProperties();
 
                     // todo set this property in the input
                     prop.SetValue(PIPE_MODEL_FACTOR, 1);
 
                     // calculate eq height
-                    double eq_height = pElement.CalculateEquilibriumPipeHeight(prop, Geom, OpenPipeElement.GetValue(PIPE_ELEMENT_LENGTH));
-                    double current_height = OpenPipeElement.GetValue(PIPE_HEIGHT);
+                    double eq_height = pElement->CalculateEquilibriumPipeHeight(prop, Geom, OpenPipeElement->GetValue(PIPE_ELEMENT_LENGTH));
+                    double current_height = OpenPipeElement->GetValue(PIPE_HEIGHT);
 
                     if (current_height > eq_height)
                     {
-                        OpenPipeElement.SetValue(PIPE_EROSION, true);
+                        OpenPipeElement->SetValue(PIPE_EROSION, true);
                     }
 
                     // todo check max pipe height
 
                     // check this if statement, I dont understand the check for pipe erosion
-                    if (((!OpenPipeElement.GetValue(PIPE_EROSION) ||(current_height > eq_height)) && current_height < amax))
+                    if (((!OpenPipeElement->GetValue(PIPE_EROSION) ||(current_height > eq_height)) && current_height < amax))
                     {
-                        OpenPipeElement.SetValue(PIPE_HEIGHT, OpenPipeElement.GetValue(PIPE_HEIGHT)+da);
+                        OpenPipeElement->SetValue(PIPE_HEIGHT, OpenPipeElement->GetValue(PIPE_HEIGHT)+da);
                         Equilibrium = false;
                     }
 
@@ -189,7 +190,7 @@ public:
                     //}
 
 
-                    double test = OpenPipeElement.GetValue(PIPE_HEIGHT);
+                    double test = OpenPipeElement->GetValue(PIPE_HEIGHT);
                     /*prop[MINIMUM_JOINT_WIDTH] = prop[MINIMUM_JOINT_WIDTH + 0.2;*/
                     double a = 1 + 1;
                        
@@ -201,7 +202,7 @@ public:
             {
                 // open new piping element
                 openPipeElements += 1;
-            	PipeElements.at(openPipeElements).Set(ACTIVE, true);
+            	PipeElements.at(openPipeElements)->Set(ACTIVE, true);
             }
     		
         }
@@ -243,16 +244,20 @@ private:
 	
     //-----------------------------Get Piping Elements--------------------------------------
 
-    std::vector<Kratos::Element> GetPipingElements() {
+    std::vector<Element*> GetPipingElements() {
         ModelPart& CurrentModelPart = this->GetModelPart();
-        std::vector<Element> PipeElements;
+        std::vector<Element*> PipeElements;
         double PipeElementStartX;
-        for (const Element element: CurrentModelPart.Elements())
+        ModelPart::ElementsContainerType::iterator element_it = CurrentModelPart.ElementsBegin();
+
+        for (Element& element: CurrentModelPart.Elements())
         {
         	if (element.GetProperties().Has(PIPE_START_ELEMENT))
             {
-                PipeElements.push_back(element);
-
+               // ModelPart::ElementsContainerType::iterator element_it = rModelPart.ElementsBegin()
+                element.Set(ACTIVE, false);
+                PipeElements.push_back(&element);
+                
                 auto startElement = element.GetProperties()[PIPE_START_ELEMENT];
 
                 // Debug Statement
@@ -268,19 +273,19 @@ private:
         }
 
         // Get Maximum X Value in Pipe
-        auto rightPipe = std::max_element(PipeElements.begin(), PipeElements.end(), [](const Element& a, const Element& b)
+        auto rightPipe = std::max_element(PipeElements.begin(), PipeElements.end(), [](const Element* a, const Element* b)
             {
-                return a.GetGeometry().GetPoint(0)[0] < b.GetGeometry().GetPoint(0)[0];
+                return a->GetGeometry().GetPoint(0)[0] < b->GetGeometry().GetPoint(0)[0];
             });
 
         // Get Minimum X Value in Pipe
-        auto leftPipe = std::min_element(PipeElements.begin(), PipeElements.end(), [](const Element& a, const Element& b)
+        auto leftPipe = std::min_element(PipeElements.begin(), PipeElements.end(), [](const Element* a, const Element* b)
             {
-                return a.GetGeometry().GetPoint(0)[0] < b.GetGeometry().GetPoint(0)[0];
+                return a->GetGeometry().GetPoint(0)[0] < b->GetGeometry().GetPoint(0)[0];
             });
 
-        double minX = leftPipe[0].GetGeometry().GetPoint(0)[0];
-        double maxX = rightPipe[0].GetGeometry().GetPoint(0)[0];
+        double minX = leftPipe[0]->GetGeometry().GetPoint(0)[0];
+        double maxX = rightPipe[0]->GetGeometry().GetPoint(0)[0];
 
         KRATOS_INFO("PipingLoop") << minX << " " << maxX << " " << PipeElementStartX << std::endl;
 
@@ -292,22 +297,22 @@ private:
         if (minX == PipeElementStartX)
         {
             // Pipe Left -> Right
-            sort(PipeElements.begin(), PipeElements.end(), [](const Element& lhs, const Element& rhs) {
-                return lhs.GetGeometry().GetPoint(0)[0] < rhs.GetGeometry().GetPoint(0)[0];
+            sort(PipeElements.begin(), PipeElements.end(), [](const Element* lhs, const Element* rhs) {
+                return lhs->GetGeometry().GetPoint(0)[0] < rhs->GetGeometry().GetPoint(0)[0];
                 });
         }
         else
         {
             // Pipe Right -> Left
-            sort(PipeElements.begin(), PipeElements.end(), [](const Element& lhs, const Element& rhs) {
-                return lhs.GetGeometry().GetPoint(0)[0] > rhs.GetGeometry().GetPoint(0)[0];
+            sort(PipeElements.begin(), PipeElements.end(), [](const Element* lhs, const Element* rhs) {
+                return lhs->GetGeometry().GetPoint(0)[0] > rhs->GetGeometry().GetPoint(0)[0];
                 });
         }
 
         KRATOS_INFO("PipingLoop") << "Number of Pipe Elements: " << PipeElements.size() << std::endl;
-        for (const Element pipeElement: PipeElements)
+        for (const Element* pipeElement: PipeElements)
         {
-            KRATOS_INFO("PipingLoop") << "PipeElementIDs (in order): " << pipeElement.Id() << std::endl;
+            KRATOS_INFO("PipingLoop") << "PipeElementIDs (in order): " << pipeElement->Id() << std::endl;
         }
 
     	return PipeElements;
