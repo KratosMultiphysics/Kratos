2e4250886e50e74e742aea206f7c55e1b7fbe200
Eduard Gómez <egomez@cimne.upc.edu>
2022-03-17 12:40:29 +0100

    Changed to using intervals

--- applications/FluidDynamicsApplication/python_scripts/decelerating_wall_process.py
+++ applications/FluidDynamicsApplication/python_scripts/decelerating_wall_process.py
@@ -10,90 +10,143 @@ def Factory(settings, model):
 class DeceleratingWallProcess(KratosMultiphysics.Process):
     """
     This process helps with convergence by slowly transforming an outlet into a
-    wall. During the start of the simulation, the normal component of the
-    momentum of the selected wall will have exponential decay. After its speed
-    is 1000 times smaller than originally, it becomes a wall.
+    wall. This process has four phases:
+    - Awaiting: Nothing is done before the interval starts
+    - Ramp-up: The time after interval start. Duration is rampup_period. The wall lets
+    a proportion of the fluid through.
+    - Steady state: Solid free-slip wall. Lasts until the end of the interval.
+    - Finished: Nothing is done after the interval end
 
-    The effect on the normal component of the momentum `f`, if only manipulated by
+    RAMP-UP
+    -------
+    During the start of the simulation, the normal component of the
+    momentum or velocity of the selected wall will have exponential decay. After its speed
+    is A times smaller than originally, it extits ramp-up stage.
+
+    This parameter A is hardcoded as `decay_constant`.
+
+    The effect on the normal component of the momentum/velocity `f`, if only manipulated by
     this process is:
     ```
-           { f0 * 1000^(-t/period)  if t < period
+           { f0 * A^(-t/period)  if t < period (RAMP-UP)
     f(t) = {
-           { 0                      if t >= period
+           { 0                      if t >= period (STEADY STATE)
     ```
-    If the momentum is being affected by any other variable, then the long-term
-    effect is less predictable but each step the momentum is diminished by a
-    factor of 1000^(-dt/period).
+    If the momentum/velocity is being affected by any other variable, then the long-term
+    effect is less predictable but each step the momentum/velocity is diminished by a
+    factor of A^(-dt/period).
+
+    STEADY STATE
+    ------------
+    The node is given the SLIP flag, which signals the stategy that it is in charge
+    of applying the boundary condition.
 
     Key parameters
-    - Period: For a variable with otherwise constant momentum, it's the time it
-    takes for the momentum to be 0.001 of the original. After this period, the
-    model part is treated like a solid wall.
+    --------------
+    - rampup_period: Duraation of the ramp-up stage.
+    - interval:  The time in this process runs (RAMP_UP + STEADY).
+    - recompute_normals: Enable computing the normals every step. Otherwise it's done only once.
+    - variable: The variable to control, usually MOMENTUM of VELOCITY
     """
+
+    # Stages
+    AWAITING = 0
+    RAMP_UP = 1
+    STEADY = 2
+    FINISHED = 3
+
+    # Decay constant
+    decay_constant = 1000
 
     def __init__(self, model, settings):
         KratosMultiphysics.Process.__init__(self)
 
-        if not settings.Has("period"):
-            raise KeyError("Missing period parameter")
+        self.interval = KratosMultiphysics.IntervalUtility(settings)
 
         settings.ValidateAndAssignDefaults(self.GetDefaultParameters())
-
         modelpart_name = settings["model_part_name"].GetString()
+
         self.model_part = model[modelpart_name]
-
-        self.period = settings["period"].GetDouble()
+        self.rampup_period = settings["rampup_period"].GetDouble()
         self.recompute_normals = settings["recompute_normals"].GetBool()
-
-        if self.period <= 0:
-            raise ValueError("Parameter period must be strictly positive.")
-
-        self.period_ended = False
+        self._stage = self.AWAITING
 
     def GetDefaultParameters(self):
         return KratosMultiphysics.Parameters(
             """
             {
-                "model_part_name" : "SPECIFY_MODEL_PART_NAME",
-                "period"        : 0,
-                "recompute_normals" : false
+                "model_part_name"   : "SPECIFY_MODEL_PART_NAME",
+                "interval"   : [0, "End"],
+                "rampup_period"   : 0,
+                "recompute_normals" : false,
+                "variable" : "MOMENTUM"
             }
             """
         )
 
+    def Check(self):
+        if self.rampup_period < 0:
+            raise ValueError("Parameter rampup_period must be strictly positive.")
+
+        full_duration = self.interval.GetIntervalEnd() - self.interval.GetIntervalBegin()
+        if self.rampup_period > full_duration:
+            msg = "Parameter rampup_period must greater than the interval ({0} < {1}).".format(self.rampup_period, full_duration)
+            raise ValueError(msg)
+
     def ExecuteInitialize(self):
         KratosMultiphysics.NormalCalculationUtils().CalculateNormals(self.model_part, True)
+        KratosMultiphysics.VariableUtils().SetFlag(KratosMultiphysics.SLIP, False, self.model_part.Nodes)
 
-    def ExecuteFinalizeSolutionStep(self):
-        if self.period_ended:
-            return
+    def ExecuteInitializeSolutionStep(self):
+        self._UpdateStage()
 
-        time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
-
-        if time > self.period:
-            KratosMultiphysics.VariableUtils().SetFlag(
-                KratosMultiphysics.SLIP, True, self.model_part.Nodes
-            )
-            self.period_ended = True
-            return
-
-        if self.recompute_normals:
+        if self.recompute_normals and self.stage in [self.RAMP_UP, self.STEADY]:
             KratosMultiphysics.NormalCalculationUtils().CalculateNormals(self.model_part, True)
 
+    def ExecuteFinalizeSolutionStep(self):
+        if self._stage != self.RAMP_UP:
+            return
+
         dt = self.model_part.ProcessInfo[KratosMultiphysics.DELTA_TIME]
-        decay = (1000)**(-dt/self.period)
+
+        decay = (self.decay_constant)**(-dt/self.rampup_period)
+        div_by_zero_tolerance = 1e-8
 
         for node in self.model_part.Nodes:
-            mom = node.GetSolutionStepValue(KratosMultiphysics.MOMENTUM)
-
             unit_normal = node.GetSolutionStepValue(KratosMultiphysics.NORMAL)
-            unit_normal /= unit_normal.norm_2()
+            norm2 = unit_normal.norm_2()
+            if norm2 < div_by_zero_tolerance:
+                continue
+            unit_normal /= norm2
 
+            mom = node.GetSolutionStepValue(KratosMultiphysics.MOMENTUM)
             mom_n = sum([m*n for m, n in zip(mom, unit_normal)])
             new_mom_n = mom_n * decay
 
             delta_mom_n = new_mom_n - mom_n
-
             new_mom = mom + delta_mom_n * unit_normal
 
             node.SetSolutionStepValue(KratosMultiphysics.MOMENTUM, new_mom)
+
+    def _GetStage(self):
+        time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
+        if time < self.interval.GetIntervalBegin():
+            return self.AWAITING
+        elif time < self.interval.GetIntervalBegin() + self.rampup_period:
+            return self.RAMP_UP
+        elif time < self.interval.GetIntervalEnd():
+            return self.STEADY
+        return self.FINISHED
+
+    def _UpdateStage(self):
+        new_stage = self._GetStage()
+
+        if new_stage == self._stage:
+            return
+
+        if new_stage == self.STEADY:
+            KratosMultiphysics.VariableUtils().SetFlag(KratosMultiphysics.SLIP, True, self.model_part.Nodes)
+        elif new_stage == self.FINISHED:
+            KratosMultiphysics.VariableUtils().SetFlag(KratosMultiphysics.SLIP, False, self.model_part.Nodes)
+
+        self._stage = new_stage
