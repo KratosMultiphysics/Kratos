feaa5697df654db5b3a81e48d11b917c84744cec
Miguel Angel <maceli@cimne.upc.edu>
2022-03-24 11:41:23 +0100

    windows compilation requirements

--- applications/DEMApplication/custom_utilities/create_and_destroy.h
+++ applications/DEMApplication/custom_utilities/create_and_destroy.h
@@ -328,6 +328,19 @@ private:
 
 }; // Class ParticleCreatorDestructor
 
+extern template void ParticleCreatorDestructor::DestroyParticles<SphericParticle>(ModelPart&);
+extern template void ParticleCreatorDestructor::DestroyParticles<Cluster3D>(ModelPart&);
+extern template bool ParticleCreatorDestructor::CheckParticlePreservationCriteria<SphericParticle>(const Element::Pointer, const double);
+extern template bool ParticleCreatorDestructor::CheckParticlePreservationCriteria<Cluster3D>(const Element::Pointer, const double);
+extern template void ParticleCreatorDestructor::DestroyParticles<SphericParticle>(ModelPart::MeshType&, const double);
+extern template void ParticleCreatorDestructor::DestroyParticles<Cluster3D>(ModelPart::MeshType&, const double);
+extern template void ParticleCreatorDestructor::MarkDistantParticlesForErasing<SphericParticle>(ModelPart&);
+extern template void ParticleCreatorDestructor::MarkDistantParticlesForErasing<Cluster3D>(ModelPart&);
+extern template void ParticleCreatorDestructor::MarkParticlesForErasingGivenBoundingBox<SphericParticle>(ModelPart&, array_1d<double, 3 >, array_1d<double, 3 >);
+extern template void ParticleCreatorDestructor::MarkParticlesForErasingGivenBoundingBox<Cluster3D>(ModelPart&, array_1d<double, 3 >, array_1d<double, 3 >);
+extern template void ParticleCreatorDestructor::DestroyParticlesOutsideBoundingBox<SphericParticle>(ModelPart&);
+extern template void ParticleCreatorDestructor::DestroyParticlesOutsideBoundingBox<Cluster3D>(ModelPart&);
+
 } // namespace Kratos
 
 #endif // CREATE_AND_DESTROY_H defined
