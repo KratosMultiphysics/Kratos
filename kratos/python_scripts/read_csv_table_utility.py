5dda25e597f292a9b0abd5dc60187047194f12d9
migelmaso <mmaso@cimne.upc.edu>
2022-04-29 18:39:23 +0200

    rename time value to first second

--- kratos/python_scripts/read_csv_table_utility.py
+++ kratos/python_scripts/read_csv_table_utility.py
@@ -5,28 +5,28 @@ class ReadCsvTableUtility:
     r"""This class is used to retrieve a table from the specified parameters.
 
     The following parameters can be specified:
-    |-------------------|---------------------------------------------------|
-    | "name"            | The type of input (csv_table)                     |
-    |-------------------|---------------------------------------------------|
-    | "filename"        | The file name                                     |
-    |-------------------|---------------------------------------------------|
-    | "delimiter"       | ","  comma                                        |
-    |                   | ";"  semicolon                                    |
-    |                   | "\t" tab                                          |
-    |                   | " "  spaces                                       |
-    |                   |      etc                                          |
-    |-------------------|---------------------------------------------------|
-    | "skiprows"        | The number of rows to skip before reading data    |
-    |-------------------|---------------------------------------------------|
-    | "time_column_id"  | The index of the time in the table (zero-based)   |
-    |-------------------|---------------------------------------------------|
-    | "value_column_id" | The index of the value in the table (zero-based)  |
-    |-------------------|---------------------------------------------------|
-    | "table_id"        | If >-1 the input table will be stored in the      |
-    |                   | model part                                        |
-    |-------------------|---------------------------------------------------|
-    | "na_replace"      | The value to apply when N/A is read               |
-    |-------------------|---------------------------------------------------|
+    |--------------------|------------------------------------------------------|
+    | "name"             | The type of input (csv_table)                        |
+    |--------------------|------------------------------------------------------|
+    | "filename"         | The file name                                        |
+    |--------------------|------------------------------------------------------|
+    | "delimiter"        | ","  comma                                           |
+    |                    | ";"  semicolon                                       |
+    |                    | "\t" tab                                             |
+    |                    | " "  spaces                                          |
+    |                    |      etc                                             |
+    |---------------- ---|------------------------------------------------------|
+    | "skiprows"         | The number of rows to skip before reading data       |
+    |--------------- ----|------------------------------------------------------|
+    | "first_column_id"  | The index of the first column to read (zero-based)   |
+    |----------------- --|------------------------------------------------------|
+    | "second_column_id" | The index of the second column to read (zero-based)  |
+    |--------------------|------------------------------------------------------|
+    | "table_id"         | If >-1 the input table will be stored in the         |
+    |                    | model part                                           |
+    |--------------------|------------------------------------------------------|
+    | "na_replace"       | The value to apply when N/A is read                  |
+    |--------------------|------------------------------------------------------|
     """
 
     def __init__(self, settings):
@@ -37,22 +37,22 @@ class ReadCsvTableUtility:
         settings -- Kratos parameters containing solver settings.
         """
         default_settings =  KM.Parameters("""{
-            "name"            : "csv_table",
-            "filename"        : "",
-            "delimiter"       : ",",
-            "skiprows"        : 0,
-            "time_column_id"  : 0,
-            "value_column_id" : 1,
-            "table_id"        : -1,
-            "na_replace"      : 0.0
+            "name"             : "csv_table",
+            "filename"         : "",
+            "delimiter"        : ",",
+            "skiprows"         : 0,
+            "first_column_id"  : 0,
+            "second_column_id" : 1,
+            "table_id"         : -1,
+            "na_replace"       : 0.0
         }""")
         settings.ValidateAndAssignDefaults(default_settings)
 
         self.filename = settings["filename"].GetString()
         self.delimiter = settings["delimiter"].GetString()
         self.skiprows = settings["skiprows"].GetInt()
-        self.time_column_id = settings["time_column_id"].GetInt()
-        self.value_column_id = settings["value_column_id"].GetInt()
+        self.first_column_id = settings["first_column_id"].GetInt()
+        self.second_column_id = settings["second_column_id"].GetInt()
         self.table_id = settings["table_id"].GetInt()
         self.na_replace = settings["na_replace"].GetDouble()
 
@@ -64,7 +64,7 @@ class ReadCsvTableUtility:
         model_part -- ModelPart where to store or apply the table.
         """
         table = KM.PiecewiseLinearTable()
-        minimum_columns = max(self.time_column_id, self.value_column_id) + 1
+        minimum_columns = max(self.first_column_id, self.second_column_id) + 1
         with open(self.filename, 'r') as table_file:
             data = csv.reader(table_file, delimiter=self.delimiter, skipinitialspace=True)
             for _ in range(self.skiprows):
@@ -76,7 +76,7 @@ class ReadCsvTableUtility:
                         msg += "There is not enough data, a {}-column row is found.\n".format(len(row))
                         msg += "In order to get the time at column {} and the value at column {}, the table must have at least {} columns.".format(self.time_column_id, self.value_column_id, minimum_columns)
                         raise Exception(msg)
-                    table.AddRow(self._Float(row[self.time_column_id]), self._Float(row[self.value_column_id]))
+                    table.AddRow(self._Float(row[self.first_column_id]), self._Float(row[self.second_column_id]))
         if self.table_id > -1:
             if model_part:
                 model_part.AddTable(self.table_id, table)
