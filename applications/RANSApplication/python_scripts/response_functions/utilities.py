938f652c22d333e40e7f45b26bfa0c0164fe282a
sunethwarna <suneth.warna@gmail.com>
2022-05-05 12:13:45 +0200

    minor

--- applications/RANSApplication/python_scripts/response_functions/utilities.py
+++ applications/RANSApplication/python_scripts/response_functions/utilities.py
@@ -144,6 +144,8 @@ def _GetResponseFunctionOutputProcess(kratos_parameters, model_part_name, respon
                 if is_valid_respones_function:
                     return process_settings
 
+    Kratos.Logger.PrintWarning("", "No output response function found maching following settings.\n{:s}".format(response_function_parameters))
+
     return None
 
 def ReadResponseValuesFile(file_name):
