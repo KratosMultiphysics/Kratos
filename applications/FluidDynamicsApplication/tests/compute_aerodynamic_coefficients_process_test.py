67886e10056fe548b67564279216f2e0e79c308f
Eduard Gómez <egomez@cimne.upc.edu>
2022-05-06 10:46:32 +0200

    Created unit tests

--- applications/FluidDynamicsApplication/tests/compute_aerodynamic_coefficients_process_test.py
+++ applications/FluidDynamicsApplication/tests/compute_aerodynamic_coefficients_process_test.py
@@ -1,22 +1,20 @@
 # Import kratos core and applications
 import KratosMultiphysics
+from KratosMultiphysics import FluidDynamicsApplication
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 from KratosMultiphysics.FluidDynamicsApplication import compute_aerodynamic_coefficients_process
-
-from KratosMultiphysics import DENSITY, PRESSURE, PRESSURE_COEFFICIENT
 
 
 class ComputeAerodynamicCoefficientsProcessTest(KratosUnittest.TestCase):
     @classmethod
-    def _CreateModel(self, *variables):
+    def _CreateModel(self):
         model = KratosMultiphysics.Model()
         mpart = model.CreateModelPart("main")
 
-        for var in variables:
-            mpart.AddNodalSolutionStepVariable(var)
+        mpart.AddNodalSolutionStepVariable(KratosMultiphysics.PRESSURE)
 
-        mpart.CreateNewNode(1, 0.0, 0.0, 0.0)
-        mpart.CreateNewNode(2, 1.0, 0.0, 0.0)
+        mpart.CreateNewNode(1, 0.0, 1.0, 0.0)
+        mpart.CreateNewNode(2, 2.0, 0.0, 0.0)
         mpart.CreateNewCondition("LineCondition2D2N", 1, [1, 2], mpart.GetProperties()[0])
 
         return model
@@ -33,40 +31,65 @@ class ComputeAerodynamicCoefficientsProcessTest(KratosUnittest.TestCase):
         }
         """)
 
-    def testNonsenseDensity(self):
-        model = self._CreateModel(DENSITY, PRESSURE)
+    def testPressureCoefficient(self):
+        model = self._CreateModel()
         mpart = model["main"]
+        mpart.GetNode(1).SetSolutionStepValue(KratosMultiphysics.PRESSURE, 1e6)
+        mpart.GetNode(2).SetSolutionStepValue(KratosMultiphysics.PRESSURE, 1e4)
+
         params = self._GetBlankParameters()
-        params["Parameters"].AddEmptyValue("density_database").SetString("nonsensical_string_hello!")
-
-        with self.assertRaises(RuntimeError) as context:
-            _ = compute_aerodynamic_coefficients_process.Factory(params, model)
-
-        self.assertIn("Error: Invalid database.", str(context.exception))
-        self.assertIn("- historical", str(context.exception))
-        self.assertIn("- non-historical", str(context.exception))
-        self.assertIn("- properties", str(context.exception))
-
-    def testHistorical(self):
-        model = self._CreateModel(DENSITY, PRESSURE)
-        mpart = model["main"]
-        params = self._GetBlankParameters()
-        params["Parameters"].AddEmptyValue("density_database").SetString("historical")
+        params["Parameters"].AddEmptyValue("freestream_pressure").SetDouble(1e5)
+        params["Parameters"].AddEmptyValue("freestream_velocity").SetDouble(100)
+        params["Parameters"].AddEmptyValue("freestream_density").SetDouble(1.2)
+        params["Parameters"].AddEmptyValue("compute_force_coefficient").SetBool(False)
 
         process = compute_aerodynamic_coefficients_process.Factory(params, model)
         process.ExecuteInitialize()
 
         for node in mpart.Nodes:
-            self.assertTrue(node.Has(PRESSURE_COEFFICIENT))
-            self.assertAlmostEqual(node.GetValue(PRESSURE_COEFFICIENT), 0.0)
+            self.assertTrue(node.Has(KratosMultiphysics.PRESSURE_COEFFICIENT))
+            self.assertAlmostEqual(node.GetValue(KratosMultiphysics.PRESSURE_COEFFICIENT), 0.0)
 
-        # WIP: This test is not yet complete.
+        process.ExecuteFinalizeSolutionStep()
+
+        self.assertAlmostEqual(mpart.GetNode(1).GetValue(KratosMultiphysics.PRESSURE_COEFFICIENT), 150.0)
+        self.assertAlmostEqual(mpart.GetNode(2).GetValue(KratosMultiphysics.PRESSURE_COEFFICIENT), -15.0)
+
+    def testForceCoefficients(self):
+        model = self._CreateModel()
+        mpart = model["main"]
+        mpart.GetNode(1).SetSolutionStepValue(KratosMultiphysics.PRESSURE, 1.5e5)
+        mpart.GetNode(2).SetSolutionStepValue(KratosMultiphysics.PRESSURE, 1.6e5)
+
+        params = self._GetBlankParameters()
+        p_inf = 1e5
+        rho_inf = 2.4
+        v_inf = 100
+        params["Parameters"].AddEmptyValue("freestream_pressure").SetDouble(p_inf)
+        params["Parameters"].AddEmptyValue("freestream_velocity").SetDouble(v_inf)
+        params["Parameters"].AddEmptyValue("freestream_density").SetDouble(rho_inf)
+        params["Parameters"].AddEmptyValue("compute_force_coefficient").SetBool(True)
 
 
+        process = compute_aerodynamic_coefficients_process.Factory(params, model)
+        process.ExecuteInitialize()
+        process.ExecuteFinalizeSolutionStep()
+
+        mean_pressure = 1.55e5
+        normal_x = 2.0
+        normal_y = 1.0
+
+        lift = normal_y * (mean_pressure - p_inf)
+        drag = normal_x * (mean_pressure - p_inf)
+        dynamic_pressure = 0.5 * rho_inf * v_inf**2
+        cord = 1
+
+        cl = lift / (dynamic_pressure * cord)
+        cd = drag / (dynamic_pressure * cord)
+
+        self.assertAlmostEqual(mpart.GetProperties()[0].GetValue(KratosMultiphysics.DRAG_COEFFICIENT), cd)
+        self.assertAlmostEqual(mpart.GetProperties()[0].GetValue(FluidDynamicsApplication.LIFT_COEFFICIENT), cl)
 
 
 if __name__ == '__main__':
     KratosUnittest.main()
-
-
-
