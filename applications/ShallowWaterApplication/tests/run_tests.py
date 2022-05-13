53c04441d7494b3104487ab46dd6e7eea1dee233
migelmaso <mmaso@cimne.upc.edu>
2022-05-13 11:55:01 +0200

    new

--- applications/ShallowWaterApplication/tests/run_tests.py
+++ applications/ShallowWaterApplication/tests/run_tests.py
@@ -3,23 +3,18 @@ import KratosMultiphysics
 import KratosMultiphysics.ShallowWaterApplication
 import test_ShallowWaterApplication as python_tests
 
-parser = argparse.ArgumentParser(description='run the ShallowWaterApplication tests. By default, both cpp and python tests are executed.')
-options = parser.add_mutually_exclusive_group()
-options.add_argument('-c', '--cpp', dest='py', action='store_false', help='run the cpp tests.')
-options.add_argument('-p', '--py', dest='cpp', action='store_false', help='run the python tests.')
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--level', default='all', choices=['all', 'cpp', 'nightly', 'small', 'validation'])
 args = parser.parse_args()
 
-print(args) # para ver que pasa
+try:
+    KratosMultiphysics.Tester.SetVerbosity(KratosMultiphysics.Tester.Verbosity.FAILED_TESTS_OUTPUTS)
+    KratosMultiphysics.Tester.RunTestSuite("ShallowWaterApplicationFastSuite")
+    cpp = 'OK'
+except:
+    cpp = 'Failed'
 
-if args.cpp:
-    try:
-        KratosMultiphysics.Tester.SetVerbosity(KratosMultiphysics.Tester.Verbosity.FAILED_TESTS_OUTPUTS)
-        KratosMultiphysics.Tester.RunTestSuite("ShallowWaterApplicationFastSuite")
-        cpp = 'OK'
-    except:
-        cpp = 'Failed'
-
-if args.py:
+if not args.level == 'cpp':
     try:
         python_tests.run()
         py = 'OK'
@@ -28,7 +23,6 @@ if args.py:
 
 print()
 print('ShallowWaterApplication tests:')
-if args.cpp:
-    print('cpp tests ..........', cpp)
-if args.py:
+print('cpp tests ..........', cpp)
+if not args.level == 'cpp':
     print('python tests .......', py)
