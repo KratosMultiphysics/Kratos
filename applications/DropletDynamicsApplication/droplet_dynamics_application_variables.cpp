186006d83e7633778204d6497a86789a54c246f4
mrhashemi <56388210+mrhashemi@users.noreply.github.com>
2022-06-12 12:17:22 +0200

    [DropletDynamics] New Application (#9936)
    * initialization
    
    * cleaning
    
    * first instance of solver
    
    * minimum requirements
    
    * droplet dynamics analysis
    
    * Codacy
    
    * Codacy
    
    * passing self: solver init
    
    * minor changes
    
    * dropping Python2 support
    
    * droplet dynamics analysis
    
    * fixing

---
+++ applications/DropletDynamicsApplication/droplet_dynamics_application_variables.cpp
@@ -0,0 +1,18 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Mohammad R. Hashemi
+//
+
+#include "droplet_dynamics_application_variables.h"
+
+namespace Kratos
+{
+
+}
