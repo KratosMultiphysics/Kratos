58ec8728f9ddc54fa2823f72c0a2e0eecd0df373
Ariadna Cortes Danes <acd200111@gmail.com>
2022-07-07 13:06:33 +0200

    initial commit

---
+++ kratos/utilities/volume_inside_voxel.cpp
@@ -0,0 +1,27 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Ariadna Cortés
+//
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "volume_inside_voxel.h"
+#include "includes/define.h"
+
+namespace Kratos
+{ 
+
+
+
+}
---
+++ kratos/utilities/volume_inside_voxel.h
@@ -0,0 +1,116 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Ariadna Cortés
+//
+//
+
+#if !defined(KRATOS_VOLUME_INSIDE_VOXEL)
+#define  KRATOS_VOLUME_INSIDE_VOXEL
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/geometrical_object.h"
+#include "includes/node.h"
+#include "geometries/geometry.h"
+#include "intersection_utilities.h"
+
+namespace Kratos
+{ 
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @class VolumeInsideVoxel
+ * @ingroup KratosCore
+ * @brief Utilities to compute the real volume inside a voxel
+ * @details This class provides static methods to compute (using different approximations) the portion of a 
+ * voxel that is actually filled with volume.
+ * @author Ariadna Cortés
+ */
+class VolumeInsideVoxel
+{
+public:
+
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of VoxelInsideVolume
+    KRATOS_CLASS_POINTER_DEFINITION( VolumeInsideVoxel );
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /**
+     * @brief Default constructor
+     */
+    VolumeInsideVoxel(){}
+
+    /// Destructor
+    virtual ~VolumeInsideVoxel(){}
+
+    ///@}
+    ///@name Operations
+    ///@{
+        /*
+        with doxygen shit 
+        */
+private:
+
+    ///@name Private static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+}; /* Class VoxelInsideVolume */
+
+///@name Type Definitions
+///@{
+
+///@}
+///@name Input and output
+///@{
+
+}  /* namespace Kratos.*/
+
+#endif /* KRATOS_VOXEL_INSIDE_VOLUME  defined */
