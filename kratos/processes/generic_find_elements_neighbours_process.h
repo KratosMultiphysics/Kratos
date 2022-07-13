ef3551c7e4eed30daf687ca646dd3c3fc237450f
Daniel Diez <ddiez@europe.altair.com>
2022-07-13 14:35:57 +0200

    fix find neighbors in mpi (#10026)
    Co-authored-by: Daniel Diez <ddiez@altair.com>

--- kratos/processes/generic_find_elements_neighbours_process.h
+++ kratos/processes/generic_find_elements_neighbours_process.h
@@ -155,7 +155,8 @@ private:
 
 
     GlobalPointer<Element> CheckForNeighbourElems (const Geometry<Node<3> >& rBoundaryGeom,
-                                                   Element & rElement);
+                                                   Element & rElement,
+                                                   const int Rank);
 
 
     ///@}
