ed409020ece9b93739b8e41639594a4e749d809f
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2022-08-03 11:29:44 +0200

    [Core] Adding architecture to the Kratos version

--- kratos/sources/kratos_version.cpp
+++ kratos/sources/kratos_version.cpp
@@ -35,6 +35,19 @@ namespace Kratos {
 #define KRATOS_BUILD_TYPE "Release"
 #endif
 
+// Architecture type
+#if defined(__x86_64__) || defined(_M_X64) || defined(__amd64)
+#define KRATOS_ARCH_TYPE "x86_64"
+#elif defined(__i386__) || defined(_M_IX86) || defined(_X86_) || defined(__i386)
+#define KRATOS_ARCH_TYPE "x86"
+#elif defined(__arm__)
+#define KRATOS_ARCH_TYPE "ARM32"
+#elif defined(__aarch64__)
+#define KRATOS_ARCH_TYPE "ARM64"
+#else
+#define KRATOS_ARCH_TYPE "Unknown architecture"
+#endif
+
 // Full version
 #define KRATOS_TO_STRING_(X) #X
 #define KRATOS_TO_STRING(X) KRATOS_TO_STRING_(X)
@@ -43,7 +56,8 @@ KRATOS_TO_STRING(KRATOS_MAJOR_VERSION) "." \
 KRATOS_TO_STRING(KRATOS_MINOR_VERSION) "." \
 KRATOS_TO_STRING(KRATOS_PATCH_VERSION) "-" \
 KRATOS_SHA1_NUMBER "-" \
-KRATOS_BUILD_TYPE
+KRATOS_BUILD_TYPE  "-" \
+KRATOS_ARCH_TYPE
 
 std::string GetPatchVersion() {
     return KRATOS_PATCH_VERSION;
