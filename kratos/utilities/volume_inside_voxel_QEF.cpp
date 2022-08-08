b2eba17d7c97067b74ab285e3811c495035939c6
Ariadna Cortes Danes <acd200111@gmail.com>
2022-08-08 16:57:44 +0200

    separe .cpp volume_inside_voxel_QEF

---
+++ kratos/utilities/volume_inside_voxel_QEF.cpp
@@ -0,0 +1,174 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Ariadna Cortés
+//
+
+//Project includes
+#include "volume_inside_voxel_qef.h"
+
+namespace Kratos { 
+
+    double VolumeInsideVoxelQEF::SimpleNodesQEFApproximation(
+        const GeometryType& rVoxel,  
+        const GeometryArrayType& rTriangles     
+    ) {
+        double volume = 0;
+        GeometryArrayType edges = rVoxel.GenerateEdges();
+        PointsArrayType Nodes = rVoxel.Points();
+
+        array_1d<double,3> qef = QEF::QEFPoint(rVoxel,rTriangles); 
+        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
+        
+        DenseMatrix<unsigned int> NodesInFaces(4,6);
+        rVoxel.NodesInFaces(NodesInFaces);
+
+        for(int i = 0; i < NodesInFaces.size2(); i++) {
+            double Portion = GetPortion(Nodes,NodesInFaces,i);
+            double dist = NormalizedDistanceToQEF(Nodes, NodesInFaces, qef, i);
+            
+            double PartialVolume = Portion*abs(dist)/3.0;   //Volume of a piramid
+            volume += PartialVolume;
+        }
+        //if (volume == 0) return EdgesPortionApproximation(rVoxel,rTriangles);
+        
+        return volume;
+    }
+
+    /***********************************************************************************
+     **********************************************************************************/
+
+    double VolumeInsideVoxelQEF::FacesPortionQEFApproximation(
+        const GeometryType& rVoxel,  
+        const GeometryArrayType& rTriangles     
+    ) {
+        double volume = 0;
+        GeometryArrayType Faces = rVoxel.GenerateFaces();
+
+        array_1d<double,3> qef = QEF::QEFPoint(rVoxel,rTriangles);
+        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
+
+        for(int i = 0; i < Faces.size(); i++) {
+            double Portion = EdgesPortionApproximation(Faces[i],rTriangles);
+            double dist = NormalizedDistanceToQEF(Faces[i], qef, i);
+            
+            double PartialVolume = Portion*abs(dist)/3.0;   //Volume of a piramid
+            volume += PartialVolume;
+        }
+        
+        return volume;
+    }
+
+    /***********************************************************************************
+     **********************************************************************************/
+
+    double VolumeInsideVoxelQEF::GeometricalQEFApproximation(
+        const GeometryType& rVoxel,  
+        const GeometryArrayType& rTriangles     
+    ) {
+        double Volume = 0;
+        GeometryArrayType Faces = rVoxel.GenerateFaces();
+
+        array_1d<double,3> QEF = QEF::QEFPoint(rVoxel,rTriangles); 
+        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
+
+        for(int i = 0; i < Faces.size(); i++) {
+            double Portion = NodesGeometrical2D(Faces[i],rTriangles);
+            double Dist = NormalizedDistanceToQEF(Faces[i], QEF, i);
+            
+            double PartialVolume = Portion*abs(Dist)/3.0;   //Volume of a piramid
+            Volume += PartialVolume;
+            //KRATOS_WATCH(PartialVolume);    
+        }
+        
+        if (Volume > 1) return NodesApproximation(rVoxel); //if our approximation fails, use a simpler one with nearly no additional cost
+        KRATOS_ERROR_IF(Volume < 0) << "Volume of a mesh element less than 0" << std::endl;
+        return Volume;
+    }
+
+    /***********************************************************************************
+     **********************************************************************************/
+
+    double VolumeInsideVoxelQEF::GeometricalCasesQEFApproximation(
+        const GeometryType& rVoxel,  
+        const GeometryArrayType& rTriangles     
+    ) {
+        double Volume = 0;
+        GeometryArrayType Faces = rVoxel.GenerateFaces();
+
+        array_1d<double,3> QEF = QEF::QEFPoint(rVoxel,rTriangles); 
+        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
+
+        for(int i = 0; i < Faces.size(); i++) {
+            double Portion = NodesGeometricalCases2D(Faces[i],rTriangles);
+            double Dist = NormalizedDistanceToQEF(Faces[i], QEF, i);
+            
+            double PartialVolume = Portion*abs(Dist)/3.0;   //Volume of a piramid
+            Volume += PartialVolume;
+            //KRATOS_WATCH(PartialVolume);    
+        }
+        
+        if (Volume > 1) return NodesApproximation(rVoxel); //if our approximation fails, use a simpler one with nearly no additional cost
+        KRATOS_ERROR_IF(Volume < 0) << "Volume of a mesh element less than 0" << std::endl;
+        return Volume;
+    }
+
+    /***********************************************************************************
+     **********************************************************************************/
+
+     double VolumeInsideVoxelQEF::GetPortion(PointsArrayType& Nodes,const DenseMatrix<unsigned int>& NodesInFaces, int& face) 
+    {
+        double Portion = 0;
+        for(int i = 0; i < NodesInFaces.size1(); i++) {
+            if( Nodes[NodesInFaces(i,face)].GetSolutionStepValue(DISTANCE) > 0) Portion += 1.0/4;
+        }
+        return Portion;
+    }
+
+    /***********************************************************************************
+     **********************************************************************************/
+
+    double VolumeInsideVoxelQEF::NormalizedDistanceToQEF(
+        PointsArrayType& Nodes,
+        const DenseMatrix<unsigned int>& NodesInFaces, 
+        const array_1d<double,3>& Point, int& face) 
+    {
+        array_1d<double, 3> edge1 = Nodes[NodesInFaces(1,face)] - Nodes[NodesInFaces(0,face)];
+        array_1d<double, 3> edge2 = Nodes[NodesInFaces(2,face)] - Nodes[NodesInFaces(0,face)];
+        array_1d<double, 3> mNormal;
+        MathUtils<double>::UnitCrossProduct(mNormal, edge1, edge2);
+
+        const double mConstant =  inner_prod(mNormal, Nodes[NodesInFaces(0,face)]);
+        double Side = Distance(Nodes[NodesInFaces(1,face)],Nodes[NodesInFaces(0,face)]);
+        double Distance = inner_prod(mNormal,Point) - mConstant;
+
+        return Distance/Side;
+    }
+
+    /***********************************************************************************
+     **********************************************************************************/
+    
+    double VolumeInsideVoxelQEF::NormalizedDistanceToQEF(
+        GeometryType& rFace, 
+        const array_1d<double,3>& Point, int& face) 
+    {
+        PointsArrayType Nodes = rFace.Points();
+        array_1d<double, 3> edge1 = Nodes[1]- Nodes[0];
+        array_1d<double, 3> edge2 = Nodes[2] - Nodes[0];
+        array_1d<double, 3> mNormal;
+        MathUtils<double>::UnitCrossProduct(mNormal, edge1, edge2);
+
+        const double mConstant =  inner_prod(mNormal, Nodes[0]);
+        double Side = Distance(Nodes[1],Nodes[0]);
+        double Distance = inner_prod(mNormal,Point) - mConstant;
+
+        return Distance/Side;
+    }
+    
+} //Namespace Kratos
--- kratos/utilities/volume_inside_voxel_QEF.h
+++ kratos/utilities/volume_inside_voxel_QEF.h
@@ -99,31 +99,7 @@ public:
      * parameters, it will only work with intersecting TRIANGLES, since the utility used to compute
      * the intersection does not allow templating.
      */ 
-    static double SimpleNodesQEFApproximation(
-        const GeometryType& rVoxel,  
-        const GeometryArrayType& rTriangles     
-    ) {
-        double volume = 0;
-        GeometryArrayType edges = rVoxel.GenerateEdges();
-        PointsArrayType Nodes = rVoxel.Points();
-
-        array_1d<double,3> qef = QEF::QEFPoint(rVoxel,rTriangles); 
-        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
-        
-        DenseMatrix<unsigned int> NodesInFaces(4,6);
-        rVoxel.NodesInFaces(NodesInFaces);
-
-        for(int i = 0; i < NodesInFaces.size2(); i++) {
-            double Portion = GetPortion(Nodes,NodesInFaces,i);
-            double dist = NormalizedDistanceToQEF(Nodes, NodesInFaces, qef, i);
-            
-            double PartialVolume = Portion*abs(dist)/3.0;   //Volume of a piramid
-            volume += PartialVolume;
-        }
-        //if (volume == 0) return EdgesPortionApproximation(rVoxel,rTriangles);
-        
-        return volume;
-    }
+    static double SimpleNodesQEFApproximation(const GeometryType& rVoxel, const GeometryArrayType& rTriangles);
 
     /**
      * @brief Aproximates the actual volume inside the voxel 
@@ -133,74 +109,12 @@ public:
      * @note This approximation finds the portion of each edge that is part of the volume (using
      * intersection point with triangles of the mesh).
      */ 
-    static double FacesPortionQEFApproximation(
-        const GeometryType& rVoxel,  
-        const GeometryArrayType& rTriangles     
-    ) {
-        double volume = 0;
-        GeometryArrayType Faces = rVoxel.GenerateFaces();
+    static double FacesPortionQEFApproximation(const GeometryType& rVoxel, const GeometryArrayType& rTriangles);
 
-        array_1d<double,3> qef = QEF::QEFPoint(rVoxel,rTriangles);
-        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
+    //PLEASE DONT USE THIS METHOD. Use GeometricalCasesQEFApproximation instead
+    static double GeometricalQEFApproximation(const GeometryType& rVoxel,const GeometryArrayType& rTriangles);
 
-        for(int i = 0; i < Faces.size(); i++) {
-            double Portion = EdgesPortionApproximation(Faces[i],rTriangles);
-            double dist = NormalizedDistanceToQEF(Faces[i], qef, i);
-            
-            double PartialVolume = Portion*abs(dist)/3.0;   //Volume of a piramid
-            volume += PartialVolume;
-        }
-        
-        return volume;
-    }
-
-    static double GeometricalQEFApproximation(
-        const GeometryType& rVoxel,  
-        const GeometryArrayType& rTriangles     
-    ) {
-        double Volume = 0;
-        GeometryArrayType Faces = rVoxel.GenerateFaces();
-
-        array_1d<double,3> QEF = QEF::QEFPoint(rVoxel,rTriangles); 
-        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
-
-        for(int i = 0; i < Faces.size(); i++) {
-            double Portion = NodesGeometrical2D(Faces[i],rTriangles);
-            double Dist = NormalizedDistanceToQEF(Faces[i], QEF, i);
-            
-            double PartialVolume = Portion*abs(Dist)/3.0;   //Volume of a piramid
-            Volume += PartialVolume;
-            //KRATOS_WATCH(PartialVolume);    
-        }
-        
-        if (Volume > 1) return NodesApproximation(rVoxel); //if our approximation fails, use a simpler one with nearly no additional cost
-        KRATOS_ERROR_IF(Volume < 0) << "Volume of a mesh element less than 0" << std::endl;
-        return Volume;
-    }
-
-    static double GeometricalCasesQEFApproximation(
-        const GeometryType& rVoxel,  
-        const GeometryArrayType& rTriangles     
-    ) {
-        double Volume = 0;
-        GeometryArrayType Faces = rVoxel.GenerateFaces();
-
-        array_1d<double,3> QEF = QEF::QEFPoint(rVoxel,rTriangles); 
-        //this is unefficient since we will repeat the same calculations to find the intersections afterwards 
-
-        for(int i = 0; i < Faces.size(); i++) {
-            double Portion = NodesGeometricalCases2D(Faces[i],rTriangles);
-            double Dist = NormalizedDistanceToQEF(Faces[i], QEF, i);
-            
-            double PartialVolume = Portion*abs(Dist)/3.0;   //Volume of a piramid
-            Volume += PartialVolume;
-            //KRATOS_WATCH(PartialVolume);    
-        }
-        
-        if (Volume > 1) return NodesApproximation(rVoxel); //if our approximation fails, use a simpler one with nearly no additional cost
-        KRATOS_ERROR_IF(Volume < 0) << "Volume of a mesh element less than 0" << std::endl;
-        return Volume;
-    }
+    static double GeometricalCasesQEFApproximation(const GeometryType& rVoxel, const GeometryArrayType& rTriangles);
 
 private:
 
@@ -226,14 +140,7 @@ private:
      * @param NodesInFaces matrix containing the index of the nodes of the geometry that belong to each faces
      * @return 
      * */
-    static double GetPortion(PointsArrayType& Nodes,const DenseMatrix<unsigned int>& NodesInFaces, int& face) 
-    {
-        double Portion = 0;
-        for(int i = 0; i < NodesInFaces.size1(); i++) {
-            if( Nodes[NodesInFaces(i,face)].GetSolutionStepValue(DISTANCE) > 0) Portion += 1.0/4;
-        }
-        return Portion;
-    }
+    static double GetPortion(PointsArrayType& Nodes,const DenseMatrix<unsigned int>& NodesInFaces, int& face);
 
     /**
      * @brief Calculates the distance from a face (given by its nodes) and the given point, normalized 
@@ -245,22 +152,7 @@ private:
     static double NormalizedDistanceToQEF(
         PointsArrayType& Nodes,
         const DenseMatrix<unsigned int>& NodesInFaces, 
-        const array_1d<double,3>& Point, int& face) 
-    {
-        array_1d<double, 3> edge1 = Nodes[NodesInFaces(1,face)] - Nodes[NodesInFaces(0,face)];
-        array_1d<double, 3> edge2 = Nodes[NodesInFaces(2,face)] - Nodes[NodesInFaces(0,face)];
-        array_1d<double, 3> mNormal;
-        MathUtils<double>::UnitCrossProduct(mNormal, edge1, edge2);
-
-        const double mConstant =  inner_prod(mNormal, Nodes[NodesInFaces(0,face)]);
-        double Side = Distance(Nodes[NodesInFaces(1,face)],Nodes[NodesInFaces(0,face)]);
-        double Distance = inner_prod(mNormal,Point) - mConstant;
-
-        //std:: cout << "Plane Vector 1: " << edge1 <<std::endl << "Plain Vector 2: " << edge2 << std::endl;
-        //std::cout << "Normal vector: " << mNormal <<std::endl;
-
-        return Distance/Side;
-    }
+        const array_1d<double,3>& Point, int& face);
 
     /**
      * @brief Calculates the distance from a face (given by its nodes) and the given point, normalized 
@@ -271,20 +163,7 @@ private:
      * */
     static double NormalizedDistanceToQEF(
         GeometryType& rFace, 
-        const array_1d<double,3>& Point, int& face) 
-    {
-        PointsArrayType Nodes = rFace.Points();
-        array_1d<double, 3> edge1 = Nodes[1]- Nodes[0];
-        array_1d<double, 3> edge2 = Nodes[2] - Nodes[0];
-        array_1d<double, 3> mNormal;
-        MathUtils<double>::UnitCrossProduct(mNormal, edge1, edge2);
-
-        const double mConstant =  inner_prod(mNormal, Nodes[0]);
-        double Side = Distance(Nodes[1],Nodes[0]);
-        double Distance = inner_prod(mNormal,Point) - mConstant;
-
-        return Distance/Side;
-    }
+        const array_1d<double,3>& Point, int& face);
 
 }; /* Class VoxelInsideVolumeQEF */
 
