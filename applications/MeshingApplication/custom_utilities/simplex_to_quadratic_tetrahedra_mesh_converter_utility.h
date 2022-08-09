5188f7f394d731b434986bf91e6ef03be9bb48c7
Ariadna Cortes Danes <acd200111@gmail.com>
2022-08-09 17:10:14 +0200

    requested changes

--- applications/MeshingApplication/custom_utilities/simplex_to_quadratic_tetrahedra_mesh_converter_utility.h
+++ applications/MeshingApplication/custom_utilities/simplex_to_quadratic_tetrahedra_mesh_converter_utility.h
@@ -101,29 +101,29 @@ private:
     * Creates a new tetrahedra3D10
     * @return the reference to the new tetrahedra
     */
-    Tetrahedra3D10<Node<3>> GenerateTetrahedra(ModelPart& ThisModelPart, std::vector<int>& nodeIds) {
-        unsigned int i0 = nodeIds[0];
-        unsigned int i1 = nodeIds[1];
-        unsigned int i2 = nodeIds[2];
-        unsigned int i3 = nodeIds[3];
-        unsigned int i4 = nodeIds[4];
-        unsigned int i6 = nodeIds[5];
-        unsigned int i7 = nodeIds[6];
-        unsigned int i5 = nodeIds[7];
-        unsigned int i8 = nodeIds[8];
-        unsigned int i9 = nodeIds[9];
+    Tetrahedra3D10<Node<3>> GenerateTetrahedra(const ModelPart& rThisModelPart, const std::vector<int>& rNodeIds) {
+        unsigned int i0 = rNodeIds[0];
+        unsigned int i1 = rNodeIds[1];
+        unsigned int i2 = rNodeIds[2];
+        unsigned int i3 = rNodeIds[3];
+        unsigned int i4 = rNodeIds[4];
+        unsigned int i6 = rNodeIds[5];
+        unsigned int i7 = rNodeIds[6];
+        unsigned int i5 = rNodeIds[7];
+        unsigned int i8 = rNodeIds[8];
+        unsigned int i9 = rNodeIds[9];
 
         Tetrahedra3D10<Node < 3 > > Geom(
-            ThisModelPart.Nodes()(i0),
-            ThisModelPart.Nodes()(i1),
-            ThisModelPart.Nodes()(i2),
-            ThisModelPart.Nodes()(i3),
-            ThisModelPart.Nodes()(i4),
-            ThisModelPart.Nodes()(i5),
-            ThisModelPart.Nodes()(i6),
-            ThisModelPart.Nodes()(i7),
-            ThisModelPart.Nodes()(i8),
-            ThisModelPart.Nodes()(i9)
+            rThisModelPart.Nodes()(i0),
+            rThisModelPart.Nodes()(i1),
+            rThisModelPart.Nodes()(i2),
+            rThisModelPart.Nodes()(i3),
+            rThisModelPart.Nodes()(i4),
+            rThisModelPart.Nodes()(i5),
+            rThisModelPart.Nodes()(i6),
+            rThisModelPart.Nodes()(i7),
+            rThisModelPart.Nodes()(i8),
+            rThisModelPart.Nodes()(i9)
         );
         return Geom;
     }
@@ -132,21 +132,21 @@ private:
     * Creates a new triangle3D6
     * @return the reference to the new triangle
     */
-    Triangle3D6<Node<3>> GenerateTriangle3D6(ModelPart& ThisModelPart, array_1d<int, 6>& nodeIds) {
-        unsigned int i0   = nodeIds[0];
-        unsigned int i1   = nodeIds[1];
-        unsigned int i2   = nodeIds[2];
-        unsigned int i3   = nodeIds[3];
-        unsigned int i4   = nodeIds[4];
-        unsigned int i5   = nodeIds[5];
+    Triangle3D6<Node<3>> GenerateTriangle3D6(ModelPart& rThisModelPart, array_1d<int, 6>& rNodeIds) {
+        unsigned int i0   = rNodeIds[0];
+        unsigned int i1   = rNodeIds[1];
+        unsigned int i2   = rNodeIds[2];
+        unsigned int i3   = rNodeIds[3];
+        unsigned int i4   = rNodeIds[4];
+        unsigned int i5   = rNodeIds[5];
 
         Triangle3D6<Node<3> > Geom(
-                ThisModelPart.Nodes()(i0),
-                ThisModelPart.Nodes()(i1),
-                ThisModelPart.Nodes()(i2),
-                ThisModelPart.Nodes()(i3),
-                ThisModelPart.Nodes()(i4),
-                ThisModelPart.Nodes()(i5)
+                rThisModelPart.Nodes()(i0),
+                rThisModelPart.Nodes()(i1),
+                rThisModelPart.Nodes()(i2),
+                rThisModelPart.Nodes()(i3),
+                rThisModelPart.Nodes()(i4),
+                rThisModelPart.Nodes()(i5)
                 );
         return Geom;
     }
@@ -156,77 +156,77 @@ private:
     * @param Coord: The compressed matrix containing at (i,j) the id of the node created between nodes i,j
     * @param New_Elements: The new elements created
     * @param interpolate_internal_variables: A boolean that defines if it is necessary to interpolate the internal variables
-    * @return ThisModelPart: The model part of the model (it is the input too)
+    * @return rThisModelPart: The model part of the model (it is the input too)
     */
 
     void EraseOldElementAndCreateNewElement(
-            ModelPart& ThisModelPart,
+            ModelPart& rThisModelPart,
             const compressed_matrix<int>& Coord,
             PointerVector< Element >& NewElements,
             bool interpolate_internal_variables
     ) override
     {
-	ElementsArrayType& rElements = ThisModelPart.Elements();
-	ElementsArrayType::iterator it_begin = rElements.ptr_begin();
-	ElementsArrayType::iterator it_end = rElements.ptr_end();
+        auto& rElements = rThisModelPart.Elements();
+        auto it_begin = rElements.ptr_begin();
+        auto it_end = rElements.ptr_end();
 
-	const ProcessInfo& rCurrentProcessInfo = ThisModelPart.GetProcessInfo();
-	int edge_ids[6];
-	std::vector<int> node_ids;
+        const auto& rCurrentProcessInfo = rThisModelPart.GetProcessInfo();
+        int edge_ids[6];
+        std::vector<int> node_ids;
 
-    const Element& rElem = KratosComponents<Element>::Get("Element3D10N");
-	for (ElementsArrayType::iterator it = it_begin; it != it_end; ++it)
-	{
-        GlobalPointersVector< Element >& rChildElements = it->GetValue(NEIGHBOUR_ELEMENTS);
-        rChildElements.resize(0);
-
-        CalculateEdges(it->GetGeometry(), Coord, edge_ids, node_ids);
-
-        // Generate the new Tetrahedra3D10 element
-        Tetrahedra3D10<Node<3>> geom = GenerateTetrahedra(ThisModelPart, node_ids);
-        Element::Pointer p_element;
-        p_element = rElem.Create(it->Id(), geom, it->pGetProperties());
-        p_element->Initialize(rCurrentProcessInfo);
-        p_element->InitializeSolutionStep(rCurrentProcessInfo);
-        p_element->FinalizeSolutionStep(rCurrentProcessInfo);
-
-        // Setting the internal variables in the "child" elem (the element replacing the old one)
-        if (interpolate_internal_variables == true)
+        const Element& rElem = KratosComponents<Element>::Get("Element3D10N");
+        for (auto it = it_begin; it != it_end; ++it)
         {
-            //This method only copies the current information to the new element
-            InterpolateInteralVariables(0, *it.base(), p_element, rCurrentProcessInfo);
+            GlobalPointersVector< Element >& rChildElements = it->GetValue(NEIGHBOUR_ELEMENTS);
+            rChildElements.resize(0);
+
+            CalculateEdges(it->GetGeometry(), Coord, edge_ids, node_ids);
+
+            // Generate the new Tetrahedra3D10 element
+            Tetrahedra3D10<Node<3>> geom = GenerateTetrahedra(rThisModelPart, node_ids);
+            Element::Pointer p_element;
+            p_element = rElem.Create(it->Id(), geom, it->pGetProperties());
+            p_element->Initialize(rCurrentProcessInfo);
+            p_element->InitializeSolutionStep(rCurrentProcessInfo);
+            p_element->FinalizeSolutionStep(rCurrentProcessInfo);
+
+            // Setting the internal variables in the "child" elem (the element replacing the old one)
+            if (interpolate_internal_variables == true)
+            {
+                //This method only copies the current information to the new element
+                InterpolateInteralVariables(0, *it.base(), p_element, rCurrentProcessInfo);
+            }
+
+            // Transfer elemental variables to new element
+            p_element->Data() = it->Data();
+            p_element->GetValue(SPLIT_ELEMENT) = false;
+            NewElements.push_back(p_element);
+
+            rChildElements.push_back( Element::WeakPointer(p_element) );
         }
 
-        // Transfer elemental variables to new element
-        p_element->Data() = it->Data();
-        p_element->GetValue(SPLIT_ELEMENT) = false;
-        NewElements.push_back(p_element);
-
-        rChildElements.push_back( Element::WeakPointer(p_element) );
+        // Now replace the elements in SubModelParts
+        if ( NewElements.size() > 0 ) {
+            ReplaceElementsInSubModelPart(rThisModelPart);
+        }
     }
-
-    // Now replace the elements in SubModelParts
-    if ( NewElements.size() > 0 ) {
-        ReplaceElementsInSubModelPart(ThisModelPart);
-    }
-}
 
     /**
     * It replaces the old Tetrahedra3D4 elements in its corresponding submodelpart by its new Tetrahedra3D10
-    * @param ThisModelPart: The modelpart or submodelpart to replace the elements
+    * @param rThisModelPart: The modelpart or submodelpart to replace the elements
     */
-    void ReplaceElementsInSubModelPart(ModelPart& ThisModelPart) {
-        for(Element::Pointer& p_element : ThisModelPart.ElementsArray()){
-                if( p_element->GetValue(SPLIT_ELEMENT) )
-                {
-                    GlobalPointersVector< Element >& children = p_element->GetValue(NEIGHBOUR_ELEMENTS);
-                    p_element = children[0].shared_from_this();
-                } 
+    void ReplaceElementsInSubModelPart(ModelPart& rThisModelPart) {
+        for(auto& p_element : rThisModelPart.ElementsArray()){
+            if( p_element->GetValue(SPLIT_ELEMENT) )
+            {
+                GlobalPointersVector< Element >& children = p_element->GetValue(NEIGHBOUR_ELEMENTS);
+                p_element = children[0].shared_from_this();
+            } 
         }
 
         //Recursively for all subModelParts
-        for (ModelPart::SubModelPartIterator iSubModelPart = ThisModelPart.SubModelPartsBegin();
-                iSubModelPart != ThisModelPart.SubModelPartsEnd(); iSubModelPart++)
+        for (ModelPart::SubModelPartIterator iSubModelPart = rThisModelPart.SubModelPartsBegin();
+                iSubModelPart != rThisModelPart.SubModelPartsEnd(); iSubModelPart++)
         {
             ReplaceElementsInSubModelPart(*iSubModelPart);
         }
@@ -235,30 +235,30 @@ private:
     /**
     * Remove the old Trangle3D3 conditions and replace them by the new Trangle3D6 ones
     * @param Coord: The coordinates of the nodes of the geometry
-    * @return ThisModelPart: The model part of the model (it is the input too)
+    * @return rThisModelPart: The model part of the model (it is the input too)
     */
 
     void EraseOldConditionsAndCreateNew(
-            ModelPart& ThisModelPart,
+            ModelPart& rThisModelPart,
             const compressed_matrix<int>& Coord
             ) override
     {
         KRATOS_TRY;
 
         PointerVector< Condition > NewConditions;
-        ConditionsArrayType& rConditions = ThisModelPart.Conditions();
+        auto& rConditions = rThisModelPart.Conditions();
 
         if(rConditions.size() > 0)
         {
-            ConditionsArrayType::iterator it_begin = rConditions.ptr_begin();
-            ConditionsArrayType::iterator it_end = rConditions.ptr_end();
+            auto it_begin = rConditions.ptr_begin();
+            auto it_end = rConditions.ptr_end();
             int  edge_ids[3];
             array_1d<int, 6> node_ids;
 
-            const ProcessInfo& rCurrentProcessInfo = ThisModelPart.GetProcessInfo();
+            const auto& rCurrentProcessInfo = rThisModelPart.GetProcessInfo();
             const Condition& rCond = KratosComponents<Condition>::Get("SurfaceCondition3D6N");
 
-            for (ConditionsArrayType::iterator it = it_begin; it != it_end; ++it)
+            for (auto it = it_begin; it != it_end; ++it)
             {
                 CalculateEdgesFaces(it->GetGeometry(), Coord, edge_ids, node_ids);
 
@@ -266,7 +266,7 @@ private:
                 rChildConditions.resize(0);
 
                 //Generate the new condition
-                Triangle3D6<Node<3> > geom = GenerateTriangle3D6 (ThisModelPart, node_ids);
+                Triangle3D6<Node<3> > geom = GenerateTriangle3D6 (rThisModelPart, node_ids);
                 Condition::Pointer pcond;
                 pcond = rCond.Create(it->Id(), geom, it->pGetProperties());
                 pcond ->Initialize(rCurrentProcessInfo);
@@ -283,7 +283,7 @@ private:
 
             // Replace the conditions in SubModelParts
             if (NewConditions.size() > 0) {
-                ReplaceConditionsInSubModelPart(ThisModelPart);
+                ReplaceConditionsInSubModelPart(rThisModelPart);
             }
         }
         KRATOS_CATCH("");
@@ -292,18 +292,18 @@ private:
 
     /**
     * It replaces the old Triangle3D3 elements in its corresponding submodelpart by its new Triangle3D6
-    * @param ThisModelPart: The modelpart or submodelpart to replace the elements
+    * @param rThisModelPart: The modelpart or submodelpart to replace the elements
     */
-    void ReplaceConditionsInSubModelPart(ModelPart& ThisModelPart) {
-        for(Condition::Pointer& pCond : ThisModelPart.ConditionsArray()){
-                if( pCond->GetValue(SPLIT_ELEMENT) )
-                {
-                    GlobalPointersVector< Condition >& children = pCond->GetValue(NEIGHBOUR_CONDITIONS);
-                    pCond = children[0].shared_from_this();
-                } 
+    void ReplaceConditionsInSubModelPart(ModelPart& rThisModelPart) {
+        for(auto& pCond : rThisModelPart.ConditionsArray()){
+            if( pCond->GetValue(SPLIT_ELEMENT) )
+            {
+                GlobalPointersVector< Condition >& children = pCond->GetValue(NEIGHBOUR_CONDITIONS);
+                pCond = children[0].shared_from_this();
+            } 
         }
-        for (ModelPart::SubModelPartIterator iSubModelPart = ThisModelPart.SubModelPartsBegin();
-                iSubModelPart != ThisModelPart.SubModelPartsEnd(); iSubModelPart++)
+        for (ModelPart::SubModelPartIterator iSubModelPart = rThisModelPart.SubModelPartsBegin();
+                iSubModelPart != rThisModelPart.SubModelPartsEnd(); iSubModelPart++)
         {
             ReplaceConditionsInSubModelPart(*iSubModelPart);
         }
