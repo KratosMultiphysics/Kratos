5ac880d68c016b806b40b1c95022cab518f5377f
matekelemen <kelemen.mate.12@gmail.com>
2022-08-11 11:40:02 +0200

    add tests for MPI string broadcasting

--- kratos/mpi/tests/test_mpi_data_communicator_python.py
+++ kratos/mpi/tests/test_mpi_data_communicator_python.py
@@ -161,13 +161,17 @@ class TestMPIDataCommunicatorPython(UnitTest.TestCase):
         source_rank = 0
         broadcast_int = self.world.Broadcast(self.rank, source_rank)
         broadcast_double = self.world.Broadcast(2.0*self.rank, source_rank)
+        broadcast_string = self.world.Broadcast(str(self.rank), source_rank)
         broadcast_int_list = self.world.BroadcastInts([self.rank,-self.rank], source_rank)
         broadcast_double_list = self.world.BroadcastDoubles([2.0*self.rank,-2.0*self.rank], source_rank)
+        broadcast_string_list = self.world.BroadcastStrings([f"{self.rank}_{i}" for i in range(3)], source_rank)
 
         self.assertEqual(broadcast_int, source_rank)
         self.assertEqual(broadcast_double, 2.0*source_rank)
+        self.assertEqual(broadcast_string, str(source_rank))
         self.assertEqual(broadcast_int_list, [source_rank, -source_rank])
         self.assertEqual(broadcast_double_list, [2.0*source_rank, -2.0*source_rank])
+        self.assertEqual(broadcast_string_list, [f"{source_rank}_{i}" for i in range(3)])
 
     def testScatterOperations(self):
         source_rank = self.size-1
