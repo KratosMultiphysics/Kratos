b2b527d338c1b98685fb127289eccff98190b1e1
Jonathan Nuttall <j.d.nuttall@gmail.com>
2022-08-18 10:34:29 +0200

    Revert "Provided alternative to python parameterized only using pytest calls"
    This reverts commit 7f62042d7f145c31cdee8b969b38fbe27eea8972.

--- applications/GeoMechanicsApplication/tests/test_sellmeijers_rule.py
+++ applications/GeoMechanicsApplication/tests/test_sellmeijers_rule.py
@@ -2,20 +2,13 @@ import sys
 import os
 import json
 import math
-#from parameterized import parameterized
+from parameterized import parameterized
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 sys.path.append(os.path.join('..', 'python_scripts'))
 import test_helper
 
-def pytest_generate_tests(metafunc):
-    # called once per each test function
-    funcarglist = metafunc.cls.params[metafunc.function.__name__]
-    argnames = sorted(funcarglist[0])
-    metafunc.parametrize(
-        argnames, [[funcargs[name] for name in argnames] for funcargs in funcarglist]
-    )
 
 class LatexWriterFile:
     """ Class that writes results for latex documentation """
@@ -117,44 +110,43 @@ class TestSellmeijersRule(KratosUnittest.TestCase):
             critical_head_found, length = self.linear_search(file_path, heads)
         return critical_head_found, length
 
-    params = {test_sellmeijers_rule_height:
-        [dict(name='7.1',  d70=1.00E-04, kappa=1.157E-12, Hc=3.43,  Hn=3.7,  length_n=6,    test_name_gid='test_compare_sellmeijer/HeightAquiferD10L30.gid'),
-         dict(name='7.2',  d70=1.00E-04, kappa=1.157E-12, Hc=6.37,  Hn=7.4,  length_n=12,   test_name_gid='test_compare_sellmeijer/HeightAquiferD10L60.gid'),
-         dict(name='7.3',  d70=1.00E-04, kappa=1.157E-12, Hc=9.18,  Hn=11.2, length_n=13.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD10L90.gid'),
-         dict(name='7.4',  d70=1.00E-04, kappa=1.157E-12, Hc=3,     Hn=3.3,  length_n=9,    test_name_gid='test_compare_sellmeijer/HeightAquiferD20L30.gid'),
-         dict(name='7.5',  d70=1.00E-04, kappa=1.157E-12, Hc=5.44,  Hn=6.1,  length_n=15,   test_name_gid='test_compare_sellmeijer/HeightAquiferD20L60.gid'),
-         dict(name='7.6',  d70=1.00E-04, kappa=1.157E-12, Hc=7.81,  Hn=9.1,  length_n=21,   test_name_gid='test_compare_sellmeijer/HeightAquiferD20L90.gid'),
-         dict(name='7.7',  d70=1.00E-04, kappa=1.157E-12, Hc=2.6,   Hn=3.1,  length_n=10.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD30L30.gid'),
-         dict(name='7.8',  d70=1.00E-04, kappa=1.157E-12, Hc=5.02,  Hn=5.7,  length_n=22.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD30L60.gid'),
-         dict(name='7.9',  d70=1.00E-04, kappa=1.157E-12, Hc=7.13,  Hn=8.1,  length_n=24,   test_name_gid='test_compare_sellmeijer/HeightAquiferD30L90.gid'),
-         dict(name='7.10', d70=3.00E-04, kappa=1.157E-12, Hc=10.29, Hn=11.3, length_n=7.5,  test_name_gid='test_compare_sellmeijer/HeightAquiferD10L30.gid'),
-         dict(name='7.11', d70=3.00E-04, kappa=1.157E-12, Hc=19.1,  Hn=21.9, length_n=10.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD10L60.gid'),
-         dict(name='7.12', d70=3.00E-04, kappa=1.157E-12, Hc=27.54, Hn=32.8, length_n=15,   test_name_gid='test_compare_sellmeijer/HeightAquiferD10L90.gid'),
-         dict(name='7.13', d70=3.00E-04, kappa=1.157E-12, Hc=9.01,  Hn=9.9,  length_n=10.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD20L30.gid'),
-         dict(name='7.14', d70=3.00E-04, kappa=1.157E-12, Hc=16.33, Hn=18.2, length_n=16.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD20L60.gid'),
-         dict(name='7.15', d70=3.00E-04, kappa=1.157E-12, Hc=23.42, Hn=27,   length_n=21,   test_name_gid='test_compare_sellmeijer/HeightAquiferD20L90.gid'),
-         dict(name='7.16', d70=3.00E-04, kappa=1.157E-12, Hc=7.8,   Hn=9.4,  length_n=12,   test_name_gid='test_compare_sellmeijer/HeightAquiferD30L30.gid'),
-         dict(name='7.17', d70=3.00E-04, kappa=1.157E-12, Hc=15.05, Hn=16.8, length_n=22.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD30L60.gid'),
-         dict(name='7.18', d70=3.00E-04, kappa=1.157E-12, Hc=21.4,  Hn=24.1, length_n=28.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD30L90.gid'),
-         dict(name='7.19', d70=1.00E-04, kappa=1.157E-10, Hc=0.74,  Hn=0.8,  length_n=6,    test_name_gid='test_compare_sellmeijer/HeightAquiferD10L30.gid'),
-         dict(name='7.20', d70=1.00E-04, kappa=1.157E-10, Hc=1.37,  Hn=1.6,  length_n=10.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD10L60.gid'),
-         dict(name='7.21', d70=1.00E-04, kappa=1.157E-10, Hc=1.98,  Hn=2.4,  length_n=12,   test_name_gid='test_compare_sellmeijer/HeightAquiferD10L90.gid'),
-         dict(name='7.22', d70=1.00E-04, kappa=1.157E-10, Hc=0.65,  Hn=0.7,  length_n=9,    test_name_gid='test_compare_sellmeijer/HeightAquiferD20L30.gid'),
-         dict(name='7.23', d70=1.00E-04, kappa=1.157E-10, Hc=1.17,  Hn=1.3,  length_n=13.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD20L60.gid'),
-         dict(name='7.24', d70=1.00E-04, kappa=1.157E-10, Hc=1.68,  Hn=1.9,  length_n=15,   test_name_gid='test_compare_sellmeijer/HeightAquiferD20L90.gid'),
-         dict(name='7.25', d70=1.00E-04, kappa=1.157E-10, Hc=0.56,  Hn=0.6,  length_n=6,    test_name_gid='test_compare_sellmeijer/HeightAquiferD30L30.gid'),
-         dict(name='7.26', d70=1.00E-04, kappa=1.157E-10, Hc=1.08,  Hn=1.2,  length_n=16.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD30L60.gid'),
-         dict(name='7.27', d70=1.00E-04, kappa=1.157E-10, Hc=1.54,  Hn=1.7,  length_n=18,   test_name_gid='test_compare_sellmeijer/HeightAquiferD30L90.gid'),
-         dict(name='7.28', d70=3.00E-04, kappa=1.157E-10, Hc=2.22,  Hn=2.4,  length_n=6,    test_name_gid='test_compare_sellmeijer/HeightAquiferD10L30.gid'),
-         dict(name='7.29', d70=3.00E-04, kappa=1.157E-10, Hc=4.12,  Hn=4.8,  length_n=10.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD10L60.gid'),
-         dict(name='7.30', d70=3.00E-04, kappa=1.157E-10, Hc=5.93,  Hn=7.2,  length_n=12,   test_name_gid='test_compare_sellmeijer/HeightAquiferD10L90.gid'),
-         dict(name='7.31', d70=3.00E-04, kappa=1.157E-10, Hc=1.94,  Hn=2.1,  length_n=9,    test_name_gid='test_compare_sellmeijer/HeightAquiferD20L30.gid'),
-         dict(name='7.32', d70=3.00E-04, kappa=1.157E-10, Hc=3.52,  Hn=4,    length_n=13.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD20L60.gid'),
-         dict(name='7.33', d70=3.00E-04, kappa=1.157E-10, Hc=5.05,  Hn=5.9,  length_n=21,   test_name_gid='test_compare_sellmeijer/HeightAquiferD20L90.gid'),
-         dict(name='7.34', d70=3.00E-04, kappa=1.157E-10, Hc=1.68,  Hn=2,    length_n=9,    test_name_gid='test_compare_sellmeijer/HeightAquiferD30L30.gid'),
-         dict(name='7.35', d70=3.00E-04, kappa=1.157E-10, Hc=3.24,  Hn=3.7,  length_n=22.5, test_name_gid='test_compare_sellmeijer/HeightAquiferD30L60.gid'),
-         dict(name='7.36', d70=3.00E-04, kappa=1.157E-10, Hc=4.61,  Hn=5.3,  length_n=27,   test_name_gid='test_compare_sellmeijer/HeightAquiferD30L90.gid')])}
-         
+    @parameterized.expand(
+        [('7.1', 1.00E-04, 1.157E-12, 3.43, 3.7, 6, 'test_compare_sellmeijer/HeightAquiferD10L30.gid'),
+         ('7.2', 1.00E-04, 1.157E-12, 6.37, 7.4, 12, 'test_compare_sellmeijer/HeightAquiferD10L60.gid'),
+         ('7.3', 1.00E-04, 1.157E-12, 9.18, 11.2, 13.5, 'test_compare_sellmeijer/HeightAquiferD10L90.gid'),
+         ('7.4', 1.00E-04, 1.157E-12, 3, 3.3, 9, 'test_compare_sellmeijer/HeightAquiferD20L30.gid'),
+         ('7.5', 1.00E-04, 1.157E-12, 5.44, 6.1, 15, 'test_compare_sellmeijer/HeightAquiferD20L60.gid'),
+         ('7.6', 1.00E-04, 1.157E-12, 7.81, 9.1, 21, 'test_compare_sellmeijer/HeightAquiferD20L90.gid'),
+         ('7.7', 1.00E-04, 1.157E-12, 2.6, 3.1, 10.5, 'test_compare_sellmeijer/HeightAquiferD30L30.gid'),
+         ('7.8', 1.00E-04, 1.157E-12, 5.02, 5.7, 22.5, 'test_compare_sellmeijer/HeightAquiferD30L60.gid'),
+         ('7.9', 1.00E-04, 1.157E-12, 7.13, 8.1, 24, 'test_compare_sellmeijer/HeightAquiferD30L90.gid'),
+         ('7.10', 3.00E-04, 1.157E-12, 10.29, 11.3, 7.5, 'test_compare_sellmeijer/HeightAquiferD10L30.gid'),
+         ('7.11', 3.00E-04, 1.157E-12, 19.1, 21.9, 10.5, 'test_compare_sellmeijer/HeightAquiferD10L60.gid'),
+         ('7.12', 3.00E-04, 1.157E-12, 27.54, 32.8, 15, 'test_compare_sellmeijer/HeightAquiferD10L90.gid'),
+         ('7.13', 3.00E-04, 1.157E-12, 9.01, 9.9, 10.5, 'test_compare_sellmeijer/HeightAquiferD20L30.gid'),
+         ('7.14', 3.00E-04, 1.157E-12, 16.33, 18.2, 16.5, 'test_compare_sellmeijer/HeightAquiferD20L60.gid'),
+         ('7.15', 3.00E-04, 1.157E-12, 23.42, 27, 21, 'test_compare_sellmeijer/HeightAquiferD20L90.gid'),
+         ('7.16', 3.00E-04, 1.157E-12, 7.8, 9.4, 12, 'test_compare_sellmeijer/HeightAquiferD30L30.gid'),
+         ('7.17', 3.00E-04, 1.157E-12, 15.05, 16.8, 22.5, 'test_compare_sellmeijer/HeightAquiferD30L60.gid'),
+         ('7.18', 3.00E-04, 1.157E-12, 21.4, 24.1, 28.5, 'test_compare_sellmeijer/HeightAquiferD30L90.gid'),
+         ('7.19', 1.00E-04, 1.157E-10, 0.74, 0.8, 6, 'test_compare_sellmeijer/HeightAquiferD10L30.gid'),
+         ('7.20', 1.00E-04, 1.157E-10, 1.37, 1.6, 10.5, 'test_compare_sellmeijer/HeightAquiferD10L60.gid'),
+         ('7.21', 1.00E-04, 1.157E-10, 1.98, 2.4, 12, 'test_compare_sellmeijer/HeightAquiferD10L90.gid'),
+         ('7.22', 1.00E-04, 1.157E-10, 0.65, 0.7, 9, 'test_compare_sellmeijer/HeightAquiferD20L30.gid'),
+         ('7.23', 1.00E-04, 1.157E-10, 1.17, 1.3, 13.5, 'test_compare_sellmeijer/HeightAquiferD20L60.gid'),
+         ('7.24', 1.00E-04, 1.157E-10, 1.68, 1.9, 15, 'test_compare_sellmeijer/HeightAquiferD20L90.gid'),
+         ('7.25', 1.00E-04, 1.157E-10, 0.56, 0.6, 6, 'test_compare_sellmeijer/HeightAquiferD30L30.gid'),
+         ('7.26', 1.00E-04, 1.157E-10, 1.08, 1.2, 16.5, 'test_compare_sellmeijer/HeightAquiferD30L60.gid'),
+         ('7.27', 1.00E-04, 1.157E-10, 1.54, 1.7, 18, 'test_compare_sellmeijer/HeightAquiferD30L90.gid'),
+         ('7.28', 3.00E-04, 1.157E-10, 2.22, 2.4, 6, 'test_compare_sellmeijer/HeightAquiferD10L30.gid'),
+         ('7.29', 3.00E-04, 1.157E-10, 4.12, 4.8, 10.5, 'test_compare_sellmeijer/HeightAquiferD10L60.gid'),
+         ('7.30', 3.00E-04, 1.157E-10, 5.93, 7.2, 12, 'test_compare_sellmeijer/HeightAquiferD10L90.gid'),
+         ('7.31', 3.00E-04, 1.157E-10, 1.94, 2.1, 9, 'test_compare_sellmeijer/HeightAquiferD20L30.gid'),
+         ('7.32', 3.00E-04, 1.157E-10, 3.52, 4, 13.5, 'test_compare_sellmeijer/HeightAquiferD20L60.gid'),
+         ('7.33', 3.00E-04, 1.157E-10, 5.05, 5.9, 21, 'test_compare_sellmeijer/HeightAquiferD20L90.gid'),
+         ('7.34', 3.00E-04, 1.157E-10, 1.68, 2, 9, 'test_compare_sellmeijer/HeightAquiferD30L30.gid'),
+         ('7.35', 3.00E-04, 1.157E-10, 3.24, 3.7, 22.5, 'test_compare_sellmeijer/HeightAquiferD30L60.gid'),
+         ('7.36', 3.00E-04, 1.157E-10, 4.61, 5.3, 27, 'test_compare_sellmeijer/HeightAquiferD30L90.gid')])
     def test_sellmeijers_rule_height(self, name, d70, kappa, Hc, Hn, length_n, test_name_gid):
         file_path = test_helper.get_file_path(os.path.join('./', test_name_gid))
         os.chdir(file_path)
