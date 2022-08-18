1dcb611034c7550eb66df070bf72c5757a1aa2b4
Vicente Mataix Ferándiz <vmataix@altair.com>
2022-08-18 08:49:19 +0000

    Missing template

--- kratos/includes/key_hash.h
+++ kratos/includes/key_hash.h
@@ -222,7 +222,9 @@ namespace Kratos
 
     /**
      * @brief This is a hasher for indexed objects
+     * @tparam TIndexedObject Type of indexed object
      */
+    template<class TIndexedObject>
     struct IndexedObjectHasher
     {
         /**
@@ -230,7 +232,7 @@ namespace Kratos
          * @param rIndexedObject The indexed object to be hashed
          * @return The corresponding hash
          */
-        HashType operator()(const IndexedObject& rIndexedObject) const
+        HashType operator()(const TIndexedObject& rIndexedObject) const
         {
             return rIndexedObject.Id();
         }
@@ -238,7 +240,9 @@ namespace Kratos
 
     /**
      * @brief This is a key comparer between two indexed objects
+     * @tparam TIndexedObject Type of indexed object
      */
+    template<class TIndexedObject>
     struct IndexedObjectComparator
     {
         /**
@@ -247,8 +251,8 @@ namespace Kratos
          * @param rSecond The second class to be compared
          */
         bool operator()(
-            const IndexedObject& rFirst,
-            const IndexedObject& rSecond
+            const TIndexedObject& rFirst,
+            const TIndexedObject& rSecond
             ) const
         {
             return rFirst.Id() == rSecond.Id();
@@ -257,7 +261,7 @@ namespace Kratos
 
     /**
      * @brief This is a hasher for indexed objects (pointer)
-     * @param TpIndexedObject Pointer type to indexed object
+     * @tparam TpIndexedObject Pointer type to indexed object
      * @note Must be tenmplated to take into account the shared, intrussive,etc... pointers
      */
     template<class TpIndexedObject>
@@ -276,7 +280,7 @@ namespace Kratos
 
     /**
      * @brief This is a key comparer between two indexed objects (pointer)
-     * @param TpIndexedObject Pointer type to indexed object
+     * @tparam TpIndexedObject Pointer type to indexed object
      * @note Must be tenmplated to take into account the shared, intrussive,etc... pointers
      */
     template<class TpIndexedObject>
