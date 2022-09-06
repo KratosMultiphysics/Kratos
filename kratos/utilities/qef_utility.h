d534d3d15d66ecdeda8dbb8a09b68b375f64e04a
Ariadna Cortes Danes <acd200111@gmail.com>
2022-09-06 17:59:24 +0200

    add qef for boundingBox

--- kratos/utilities/qef_utility.h
+++ kratos/utilities/qef_utility.h
@@ -24,6 +24,7 @@
 #include "includes/geometrical_object.h"
 #include "includes/node.h"
 #include "geometries/geometry.h"
+#include "geometries/bounding_box.h"
 #include "geometries/hexahedra_3d_8.h"
 #include "geometries/triangle_3d_3.h"
 #include "intersection_utilities.h"
@@ -108,6 +109,18 @@ public:
     );
 
     /**
+     * @brief Finds the QuadraticErrorFunction point of a voxel 
+     * @param rVoxel references to the voxel whose x-point will be calculated
+     * @param rTriangles references to the triangles which intersect the voxel at some edge.
+     * @return The QuadraticErrorFunction point (x,y,z) 
+     */
+
+    static array_1d<double,3> QuadraticErrorFunctionPoint (
+        const BoundingBox<Point>& rBox,  
+        const std::vector<GeometricalObject*>& rTriangles     
+    );
+
+    /**
      * @brief Calculates the normal vector to the surface of a 3D triangle 
      * @param rTriangle reference to the triangle
      * @return Normal vector (x,y,z)
@@ -130,6 +143,10 @@ private:
     ///@}
     ///@name Private Operations
     ///@{
+
+    static Point FirstEnd(int i, const BoundingBox<Point>& rBox);
+    static Point SecondEnd(int i, const BoundingBox<Point>& rBox);
+
 
 }; /* Class VoxelInsideVolumeUtility */
 
