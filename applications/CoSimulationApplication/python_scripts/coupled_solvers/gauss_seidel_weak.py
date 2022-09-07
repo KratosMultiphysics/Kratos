94312e4cfc3252c1610d4f0f625340f7bcea9279
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-09-07 17:20:29 +0200

    Adding custom base class to coupling solvers

--- applications/CoSimulationApplication/python_scripts/coupled_solvers/gauss_seidel_weak.py
+++ applications/CoSimulationApplication/python_scripts/coupled_solvers/gauss_seidel_weak.py
@@ -1,20 +1,23 @@
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_coupled_solver import CoSimulationCoupledSolver
 
-def Create(settings, models, solver_name):
-    return GaussSeidelWeakCoupledSolver(settings, models, solver_name)
+def Create(settings, models, solver_name, base_class = CoSimulationCoupledSolver):
+    return GetGaussSeidelWeakCoupledSolver(base_class)(settings, models, solver_name)
 
-class GaussSeidelWeakCoupledSolver(CoSimulationCoupledSolver):
-    def SolveSolutionStep(self):
-        for coupling_op in self.coupling_operations_dict.values():
-            coupling_op.InitializeCouplingIteration()
+def GetGaussSeidelWeakCoupledSolver(base):
+    class TemplatedGaussSeidelWeakCoupledSolver(GetGaussSeidelWeakCoupledSolver):
+        def SolveSolutionStep(self):
+            for coupling_op in self.coupling_operations_dict.values():
+                coupling_op.InitializeCouplingIteration()
 
-        for solver_name, solver in self.solver_wrappers.items():
-            self._SynchronizeInputData(solver_name)
-            solver.SolveSolutionStep()
-            self._SynchronizeOutputData(solver_name)
+            for solver_name, solver in self.solver_wrappers.items():
+                self._SynchronizeInputData(solver_name)
+                solver.SolveSolutionStep()
+                self._SynchronizeOutputData(solver_name)
 
-        for coupling_op in self.coupling_operations_dict.values():
-            coupling_op.FinalizeCouplingIteration()
+            for coupling_op in self.coupling_operations_dict.values():
+                coupling_op.FinalizeCouplingIteration()
 
-        return True
+            return True
+            
+    return TemplatedGaussSeidelWeakCoupledSolver
--- applications/CoSimulationApplication/python_scripts/coupled_solvers/jacobi_weak.py
+++ applications/CoSimulationApplication/python_scripts/coupled_solvers/jacobi_weak.py
@@ -1,24 +1,27 @@
 # Importing the base class
 from KratosMultiphysics.CoSimulationApplication.base_classes.co_simulation_coupled_solver import CoSimulationCoupledSolver
 
-def Create(settings, models, solver_name):
-    return JacobiWeakCoupledSolver(settings, models, solver_name)
+def Create(settings, models, solver_name, base_class = CoSimulationCoupledSolver): 
+    return GetJacobiWeakCoupledSolver(base_class)(settings, models, solver_name)
 
-class JacobiWeakCoupledSolver(CoSimulationCoupledSolver):
-    def SolveSolutionStep(self):
-        for coupling_op in self.coupling_operations_dict.values():
-            coupling_op.InitializeCouplingIteration()
+def GetJacobiWeakCoupledSolver(base):
+    class TemplatedJacobiWeakCoupledSolver(base):
+        def SolveSolutionStep(self):
+            for coupling_op in self.coupling_operations_dict.values():
+                coupling_op.InitializeCouplingIteration()
 
-        for solver_name, solver in self.solver_wrappers.items():
-            self._SynchronizeInputData(solver_name)
+            for solver_name, solver in self.solver_wrappers.items():
+                self._SynchronizeInputData(solver_name)
 
-        for solver_name, solver in self.solver_wrappers.items():
-            solver.SolveSolutionStep()
+            for solver_name, solver in self.solver_wrappers.items():
+                solver.SolveSolutionStep()
 
-        for solver_name, solver in self.solver_wrappers.items():
-            self._SynchronizeOutputData(solver_name)
+            for solver_name, solver in self.solver_wrappers.items():
+                self._SynchronizeOutputData(solver_name)
 
-        for coupling_op in self.coupling_operations_dict.values():
-            coupling_op.FinalizeCouplingIteration()
+            for coupling_op in self.coupling_operations_dict.values():
+                coupling_op.FinalizeCouplingIteration()
 
-        return True
+            return True
+
+    return TemplatedJacobiWeakCoupledSolver
