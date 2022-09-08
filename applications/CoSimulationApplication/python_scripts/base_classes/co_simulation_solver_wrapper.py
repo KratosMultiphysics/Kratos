86bb3fbfca612d6cf178a5d264de5dcba0f41d39
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-09-08 12:14:37 +0200

    Change type

--- applications/CoSimulationApplication/python_scripts/base_classes/co_simulation_solver_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/base_classes/co_simulation_solver_wrapper.py
@@ -159,7 +159,7 @@ class CoSimulationSolverWrapper:
     def _GetDefaultParameters(cls):
         return KM.Parameters("""{
             "type"                    : "",
-            "base_type"               : "",
+            "base_type"               : {},
             "solver_wrapper_settings" : {},
             "io_settings"             : {},
             "data"                    : {},
