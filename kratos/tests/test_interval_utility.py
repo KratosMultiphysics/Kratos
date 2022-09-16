5b52e6a0ad21aad396f08ee3fcc7da6f5a0ed382
migelmaso <mmaso@cimne.upc.edu>
2022-09-16 15:31:37 +0200

    add another test

--- kratos/tests/test_interval_utility.py
+++ kratos/tests/test_interval_utility.py
@@ -7,8 +7,18 @@ class TestInitialTime(KratosUnittest.TestCase):
         settings = KratosMultiphysics.Parameters()
 
         # Default interval [0, 1e30]
+        small = 1e-12
         interval = KratosMultiphysics.IntervalUtility(settings)
         self.assertTrue(interval.IsInInterval(0.0))
+        self.assertFalse(interval.IsInInterval(-small))
+
+        # Custom interval [25, 1e30]
+        small = 1e-12
+        settings["interval"][0].SetDouble(25.0)
+        settings["interval"][1].SetDouble(1000.0)
+        interval = KratosMultiphysics.IntervalUtility(settings)
+        self.assertTrue(interval.IsInInterval(25.0))
+        self.assertFalse(interval.IsInInterval(25.0 - small))
 
         # Initial interval [0, 0]
         small = 1e-12
@@ -20,7 +30,7 @@ class TestInitialTime(KratosUnittest.TestCase):
         self.assertFalse(interval.IsInInterval(-small))
 
         # Initial interval [1000, 1000]
-        small = 1e-12
+        small = 1e-10       # far from 0, bigger threshold
         settings["interval"][0].SetDouble(1000.0)
         settings["interval"][1].SetDouble(1000.0)
         interval = KratosMultiphysics.IntervalUtility(settings)
@@ -29,7 +39,7 @@ class TestInitialTime(KratosUnittest.TestCase):
         self.assertFalse(interval.IsInInterval(1000.0 + small))
 
         # Custom interval [0, 1000]
-        small = 1e-10       # bigger interval, smaller absolute tolerance
+        small = 1e-10       # bigger interval, higher absolute tolerance
         settings["interval"][0].SetDouble(0.0)
         settings["interval"][1].SetDouble(1000.0)
         interval = KratosMultiphysics.IntervalUtility(settings)
