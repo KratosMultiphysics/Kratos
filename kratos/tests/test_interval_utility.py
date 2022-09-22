14cb9853bfb0bafeb340f10e164a3630e612b6f3
matekelemen <kelemen.mate.12@gmail.com>
2022-09-22 14:44:54 +0200

    add tests from #10243

--- kratos/tests/test_interval_utility.py
+++ kratos/tests/test_interval_utility.py
@@ -320,6 +320,51 @@ class TestDiscreteIntervalUtility(KratosUnittest.TestCase):
             except Exception as exception:
                 pass # expected
 
+    def testIntervalUtilityDefault(self):
+        settings = KratosMultiphysics.Parameters()
+
+        # Default interval [-inf, inf]
+        small = 1e-12
+        interval = KratosMultiphysics.IntervalUtility(settings)
+        self.assertTrue(interval.IsInInterval(0.0))
+
+        # Custom interval [25, 1e30]
+        small = 1e-12
+        settings["interval"][0].SetDouble(25.0)
+        settings["interval"][1].SetDouble(1000.0)
+        interval = KratosMultiphysics.IntervalUtility(settings)
+        self.assertTrue(interval.IsInInterval(25.0))
+        self.assertFalse(interval.IsInInterval(25.0 - small))
+
+        # Initial interval [0, 0]
+        small = 1e-12
+        settings["interval"][0].SetDouble(0.0)
+        settings["interval"][1].SetDouble(0.0)
+        interval = KratosMultiphysics.IntervalUtility(settings)
+        self.assertTrue(interval.IsInInterval(0.0))
+        self.assertFalse(interval.IsInInterval(small))
+        self.assertFalse(interval.IsInInterval(-small))
+
+        # Initial interval [1000, 1000]
+        small = 1e-10       # far from 0, bigger threshold
+        settings["interval"][0].SetDouble(1000.0)
+        settings["interval"][1].SetDouble(1000.0)
+        interval = KratosMultiphysics.IntervalUtility(settings)
+        self.assertTrue(interval.IsInInterval(1000.0))
+        self.assertFalse(interval.IsInInterval(1000.0 - small))
+        self.assertFalse(interval.IsInInterval(1000.0 + small))
+
+        # Custom interval [0, 1000]
+        small = 1e-10       # bigger interval, higher absolute tolerance
+        settings["interval"][0].SetDouble(0.0)
+        settings["interval"][1].SetDouble(1000.0)
+        interval = KratosMultiphysics.IntervalUtility(settings)
+        self.assertTrue(interval.IsInInterval(0.0))
+        self.assertTrue(interval.IsInInterval(1.0))
+        self.assertTrue(interval.IsInInterval(1000.0))
+        self.assertFalse(interval.IsInInterval(-small))
+        self.assertFalse(interval.IsInInterval(1000.0 + small))
+
 
 
 if __name__ == "__main__":
