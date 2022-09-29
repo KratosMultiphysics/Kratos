4df0fc44e309f013c66e065ce13f8b910b2e1f2c
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2022-09-29 14:29:34 +0200

    Reverting undesired changes

--- kratos/python_scripts/operations/foo_operation.py
+++
@@ -1,9 +0,0 @@
-import KratosMultiphysics
-from KratosMultiphysics import Operation
-
-class FooOperation(KratosMultiphysics.Operation):
-    def Execute(self):
-        print("Patata")
-
-def Factory(settings, model):
-    return FooOperation().Create(model,settings)
