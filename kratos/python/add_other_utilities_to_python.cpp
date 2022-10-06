3122390f2964d0d4f8f5fd8026fe57e4b52f8b8c
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2022-10-06 10:37:23 +0200

    Meshless surrogate interface utility

--- kratos/python/add_other_utilities_to_python.cpp
+++ kratos/python/add_other_utilities_to_python.cpp
@@ -65,6 +65,7 @@
 #include "utilities/rve_periodicity_utility.h"
 #include "utilities/communication_coloring_utilities.h"
 #include "utilities/model_part_graph_utilities.h"
+#include "utilities/shifted_boundary_meshless_interface_utility.h"
 
 namespace Kratos {
 namespace Python {
@@ -734,6 +735,11 @@ void AddOtherUtilitiesToPython(pybind11::module &m)
 
     auto fs_extensions = m.def_submodule("FilesystemExtensions");
     fs_extensions.def("MPISafeCreateDirectories", &FilesystemExtensions::MPISafeCreateDirectories );
+
+    py::class_<ShiftedBoundaryMeshlessInterfaceUtility, ShiftedBoundaryMeshlessInterfaceUtility::Pointer>(m,"ShiftedBoundaryMeshlessInterfaceUtility")
+        .def(py::init<Model&, Parameters>())
+        .def("CalculateExtensionOperator", &ShiftedBoundaryMeshlessInterfaceUtility::CalculateExtensionOperator)
+    ;
 }
 
 } // namespace Python.
