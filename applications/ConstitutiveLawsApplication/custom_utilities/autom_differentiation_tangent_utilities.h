0a14d79c02dbacc6bda8ac3e5779cb76c5787ecb
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2022-10-07 09:25:59 +0200

    defining class

---
+++ applications/ConstitutiveLawsApplication/custom_utilities/autom_differentiation_tangent_utilities.h
@@ -0,0 +1,112 @@
+// KRATOS ___                _   _ _         _   _             __                       _
+//       / __\___  _ __  ___| |_(_) |_ _   _| |_(_)_   _____  / /  __ ___      _____   /_\  _ __  _ __
+//      / /  / _ \| '_ \/ __| __| | __| | | | __| \ \ / / _ \/ /  / _` \ \ /\ / / __| //_\\| '_ \| '_  |
+//     / /__| (_) | | | \__ \ |_| | |_| |_| | |_| |\ V /  __/ /__| (_| |\ V  V /\__ \/  _  \ |_) | |_) |
+//     \____/\___/|_| |_|___/\__|_|\__|\__,_|\__|_| \_/ \___\____/\__,_| \_/\_/ |___/\_/ \_/ .__/| .__/
+//                                                                                         |_|   |_|
+//
+//  License:         BSD License
+//                   license: structural_mechanics_application/license.txt
+//
+//  Main authors:    Alejandro Cornejo
+//
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+    /// The size type definition
+    typedef std::size_t SizeType;
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+/**
+ * @class AdvancedConstitutiveLawUtilities
+ * @ingroup StructuralMechanicsApplication
+ * @brief This class includes several utilities necessaries for the computation of the constitutive law
+ * @details The methods are static, so it can be called without constructing the class
+ * @tparam TVoigtSize The number of components on the Voigt notation
+ * @author Alejandro Cornejo
+ * @author Vicente Mataix Ferrandiz
+ */
+template <SizeType TVoigtSize = 6, class TYieldSurfaceType, SizeType TSofteningType>
+class KRATOS_API(CONSTITUTIVE_LAWS_APPLICATION) AutomaticDifferentiationTangentUtilities
+{
+  public:
+    ///@name Type definitions
+    ///@{
+
+    /// The index type definition
+    typedef std::size_t IndexType;
+
+    /// We define the dimension
+    static constexpr SizeType Dimension = TVoigtSize == 6 ? 3 : 2;
+
+    /// We define the Voigt size
+    static constexpr SizeType VoigtSize = TVoigtSize;
+
+    /// The matrix type definition
+    typedef Matrix MatrixType;
+
+    /// the vector type definition
+    typedef Vector VectorType;
+
+    /// The definition of the bounded vector type
+    typedef array_1d<double, VoigtSize> BoundedVectorType;
+
+    /// The definition of the bounded matrix type
+    typedef BoundedMatrix<double, Dimension, Dimension> BoundedMatrixType;
+
+    /// The definition of the bounded matrix type
+    typedef BoundedMatrix<double, VoigtSize, VoigtSize> BoundedMatrixVoigtType;
+
+    /// Node type definition
+    typedef Node<3> NodeType;
+
+    /// Geometry definitions
+    typedef Geometry<NodeType> GeometryType;
+
+    /// The zero tolerance
+    static constexpr double tolerance = std::numeric_limits<double>::epsilon();
+
+
+    ///@}
+    ///@name  Enum's
+    ///@{
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+
+private:
+
+};
+}
