5199345fa8cfe36b7bc016ac4a4e6bb45bbc4f04
Carlos Lubbers <carlos.lubbers@deltares.nl>
2022-10-17 10:24:39 +0200

    Test remove shape function

--- applications/GeoMechanicsApplication/custom_elements/small_strain_U_Pw_diff_order_element.cpp
+++ applications/GeoMechanicsApplication/custom_elements/small_strain_U_Pw_diff_order_element.cpp
@@ -1189,7 +1189,7 @@ void SmallStrainUPwDiffOrderElement::
         for ( unsigned int GPoint = 0; GPoint < NumGPoints; GPoint++ ) {
             double HydraulicHead = 0.0;
             for (unsigned int node = 0; node < NumUNodes; ++node)
-                HydraulicHead += NContainer(GPoint, node) * NodalHydraulicHead[node];
+                HydraulicHead += NodalHydraulicHead[node];
 
             rOutput[GPoint] = HydraulicHead;
         }
