70b6d6fb959045bc79c34937eeceb4c2ca5d433c
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-10-26 12:00:33 +0200

    Revert "Moving API"
    This reverts commit 7062962146227a0c48fdcd3ebe3742ad9f448193.

--- kratos/utilities/strategies_utilities.h
+++ kratos/utilities/strategies_utilities.h
@@ -49,7 +49,7 @@ enum class SCALING_DIAGONAL {NO_SCALING = 0, CONSIDER_NORM_DIAGONAL = 1, CONSIDE
  * @brief This namespace includes several utilities necessaries for the strategies
  * @author Vicente Mataix Ferrandiz
  */
-class KRATOS_API(KRATOS_CORE) StrategiesUtilities
+class StrategiesUtilities
 {
 public:
     ///@name Operations
@@ -65,7 +65,7 @@ public:
      * @return The scale norm
      */
     template<class TSparseSpace>
-    static double CheckAndCorrectZeroDiagonalValues(
+    static double KRATOS_API(KRATOS_CORE) CheckAndCorrectZeroDiagonalValues(
         ModelPart& rModelPart,
         typename TSparseSpace::MatrixType& rA,
         typename TSparseSpace::VectorType& rb,
@@ -110,7 +110,7 @@ public:
      * @return The diagonal norm
      */
     template<class TSparseSpace>
-    static double GetDiagonalNorm(typename TSparseSpace::MatrixType& rA);
+    static double KRATOS_API(KRATOS_CORE) GetDiagonalNorm(typename TSparseSpace::MatrixType& rA);
 
     /**
      * @brief This method returns the diagonal max value
@@ -129,7 +129,7 @@ public:
      * @return The diagonal  max value
      */
     template<class TSparseSpace>
-    static double GetMaxDiagonal(typename TSparseSpace::MatrixType& rA);
+    static double KRATOS_API(KRATOS_CORE) GetMaxDiagonal(typename TSparseSpace::MatrixType& rA);
 
     /**
      * @brief This method returns the diagonal min value
@@ -137,7 +137,7 @@ public:
      * @return The diagonal min value
      */
     template<class TSparseSpace>
-    static double GetMinDiagonal(typename TSparseSpace::MatrixType& rA);
+    static double KRATOS_API(KRATOS_CORE) GetMinDiagonal(typename TSparseSpace::MatrixType& rA);
 
     ///@}
 
