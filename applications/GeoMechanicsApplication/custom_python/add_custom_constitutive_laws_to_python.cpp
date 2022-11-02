00893e7d451415547a5ea28c0120575c128cf2d2
Carlos Roig <roigcarlo@gmail.com>
2022-11-02 10:20:41 +0100

    Naming convenvtions and namepsaces

--- applications/GeoMechanicsApplication/custom_python/add_custom_constitutive_laws_to_python.cpp
+++ applications/GeoMechanicsApplication/custom_python/add_custom_constitutive_laws_to_python.cpp
@@ -24,25 +24,22 @@
 #include "custom_constitutive/bilinear_cohesive_3D_law.hpp"
 #include "custom_constitutive/bilinear_cohesive_2D_law.hpp"
 
-namespace Kratos
-{
-
-namespace Python
-{
-
-using namespace pybind11;
+namespace Kratos {
+namespace Python {
 
 void  AddCustomConstitutiveLawsToPython(pybind11::module& m)
 {
+    namespace py = pybind11;
+    
     // Module local to avoid conflicts with other apps
-    class_< BilinearCohesive3DLaw, BilinearCohesive3DLaw::Pointer, ConstitutiveLaw >
+    py::class_< BilinearCohesive3DLaw, BilinearCohesive3DLaw::Pointer, ConstitutiveLaw >
     (m, "BilinearCohesive3DLaw", py::module_local())
-    .def( init<>() );
-    class_< BilinearCohesive2DLaw, BilinearCohesive2DLaw::Pointer, ConstitutiveLaw >
+    .def(py::init<>() );
+    
+    py::class_< BilinearCohesive2DLaw, BilinearCohesive2DLaw::Pointer, ConstitutiveLaw >
     (m, "BilinearCohesive2DLaw", py::module_local())
-    .def( init<>() ) ;
-
+    .def(py::init<>() ) ;
 }
 
-}  // namespace Python.
-}  // namespace Kratos.
+} // namespace Python.
+} // namespace Kratos.
