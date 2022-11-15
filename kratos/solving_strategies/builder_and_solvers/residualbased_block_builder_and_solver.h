964147e810c17395249ab7a7c0cf27344663b6db
rlrangel <rrangel@cimne.upc.edu>
2022-11-15 22:30:56 +0100

    test

--- kratos/solving_strategies/builder_and_solvers/residualbased_block_builder_and_solver.h
+++ kratos/solving_strategies/builder_and_solvers/residualbased_block_builder_and_solver.h
@@ -277,9 +277,9 @@ public:
         if (step == 1 || step % 2 == 0) {
           double h = 0.0;
           for (int k = 0; k < nconditions; k++) {
-            ModelPart::ConditionsContainerType::iterator it = cond_begin + k;
-            ThermalFace& cond = dynamic_cast<ThermalFace&> (*it);
-            h += cond.mConvectionCoefficient;
+            //ModelPart::ConditionsContainerType::iterator it = cond_begin + k;
+            //ThermalFace& cond = dynamic_cast<ThermalFace&> (*it);
+            //h += cond.mConvectionCoefficient;
           }
           h = h / nconditions;
 
