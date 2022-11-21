b6bd72d3612d1a7f849117cfb36c03995eb305e3
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-11-21 12:31:20 +0100

    Fix test

--- kratos/tests/cpp_tests/geometries/test_triangle_3d_6.cpp
+++ kratos/tests/cpp_tests/geometries/test_triangle_3d_6.cpp
@@ -61,7 +61,7 @@ namespace {
 
     KRATOS_TEST_CASE_IN_SUITE(Triangle3D6Volume, KratosCoreGeometriesFastSuite) {
         auto geom = GenerateReferenceTriangle3D6();
-        KRATOS_CHECK_EXCEPTION_IS_THROWN(geom->Volume(), "Calling base class 'Volume' method instead of derived class one.");
+        KRATOS_CHECK_NEAR(geom->Volume(), 0.0, ZERO_TOLERANCE);
     }
 
     KRATOS_TEST_CASE_IN_SUITE(Triangle3D6MinEdgeLength, KratosCoreGeometriesFastSuite) {
