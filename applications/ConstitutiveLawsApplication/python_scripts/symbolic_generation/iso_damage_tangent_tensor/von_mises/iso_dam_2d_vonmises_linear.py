5227ec3420d4cb08633fe9771963cb5691ae097e
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2022-11-22 11:46:26 +0100

    VM 2D lin

--- applications/ConstitutiveLawsApplication/python_scripts/symbolic_generation/iso_damage_tangent_tensor/von_mises/iso_dam_2d_vonmises_linear.py
+++ applications/ConstitutiveLawsApplication/python_scripts/symbolic_generation/iso_damage_tangent_tensor/von_mises/iso_dam_2d_vonmises_linear.py
@@ -1,6 +1,6 @@
 from KratosMultiphysics.sympy_fe_utilities import *
 from sympy import *
-
+import KratosMultiphysics.ConstitutiveLawsApplication.symbolic_generation.iso_damage_tangent_tensor.auxiliary_methods_automatic_differentiation as AuxLib
 
 mode = "c"
 
@@ -8,7 +8,6 @@ mode = "c"
 Strain0 = Symbol("r_strain[0]")
 Strain1 = Symbol("r_strain[1]")
 Strain2 = Symbol("r_strain[2]")
-
 
 # Tangent constitutive tensor
 Ct = DefineMatrix('r_Ct', 3, 3)
@@ -26,41 +25,30 @@ Gf = Symbol("Gf")
 characteristic_length = Symbol("characteristic_length")
 
 
-c0 = Young / ((1.00 + nu)*(1 - 2 * nu));
-c1 = (1.00 - nu)*c0;
-c2 = c0 * nu;
-c3 = (0.5 - nu)*c0;
-
-Seff[0] = c1 * Strain0 + c2 *Strain1;
-Seff[1] = c2 * Strain0 + c1 * Strain1;
-Seff[2] = c3 * Strain2;
+Seff[0], Seff[1], Seff[2] = AuxLib.ComputePredictorStressVector2D(Young, nu, Strain0, Strain1, Strain2)
 
 
-pmean = (Seff[0] + Seff[1]) / 3.0
+pmean = AuxLib.ComputePmean2D(Seff)
 Deviator[0] = Seff[0] - pmean
 Deviator[1] = Seff[1] - pmean
 Deviator[2] = Seff[2]
 
-
 # ONLY FOR VON MISES!!
-J2 =  0.5 * (Deviator[0]**2.0 + Deviator[1]**2.0 + pmean**2.0) + Deviator[2]**2.0;
+J2 =  AuxLib.ComputeJ2Invariant2D(Deviator, pmean)
 VonMisesStress = sqrt(3.0*J2)
 
-
 # Assuming Von Mises stress!
-A = -threshold**2 / (2.0 * Young * Gf / characteristic_length);
-
+A = AuxLib.ComputeAParameterLinear(threshold, Gf, characteristic_length, Young)
 
 # only for linear softening!
-damage = (1.0 - threshold / VonMisesStress) / (1.0 + A);
+damage = AuxLib.ComputeDamageLinear(VonMisesStress, threshold, A)
 
 # # Integrated stress
 Stress[0] = (1.0 - damage)*Seff[0]
 Stress[1] = (1.0 - damage)*Seff[1]
 Stress[2] = (1.0 - damage)*Seff[2]
 
-
-# # Ct = dS/dE
+# Ct = dS/dE
 Ct[0,0] = ((Stress[0]).diff(Strain0))
 Ct[0,1] = ((Stress[0]).diff(Strain1))
 Ct[0,2] = ((Stress[0]).diff(Strain2))
