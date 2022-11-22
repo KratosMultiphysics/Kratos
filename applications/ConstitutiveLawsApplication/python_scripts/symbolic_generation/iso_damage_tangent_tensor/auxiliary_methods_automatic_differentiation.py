28ae7c2dbdf33fab2ea9610cc42a48d11513d51f
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2022-11-22 15:29:49 +0100

    VM 2d lin done

--- applications/ConstitutiveLawsApplication/python_scripts/symbolic_generation/iso_damage_tangent_tensor/auxiliary_methods_automatic_differentiation.py
+++ applications/ConstitutiveLawsApplication/python_scripts/symbolic_generation/iso_damage_tangent_tensor/auxiliary_methods_automatic_differentiation.py
@@ -47,9 +47,69 @@ def ComputeJ2Invariant2D(Deviator, pmean):
 def ComputePmean2D(StressVector):
     return (StressVector[0] + StressVector[1]) / 3.0
 
+def ComputeDamage(UniaxialStress, Threshold, Gf, L, Young, Softening):
+    if (Softening ==  "Linear"):
+        A = ComputeAParameterLinear(Threshold, Gf, L, Young)
+        return ComputeDamageLinear(UniaxialStress, Threshold, A)
+    else: # Exponential
+        A = ComputeAParameterExponential(Threshold, Gf, L, Young)
+        return ComputeDamageExponential(UniaxialStress, Threshold, A)
+
 def ComputeDamageLinear(UniaxialStress, Threshold, A):
     return (1.0 - Threshold / UniaxialStress) / (1.0 + A)
+
+def ComputeDamageExponential(UniaxialStress, Threshold, A):
+    return 1.0 - (Threshold / UniaxialStress) * exp(A * (1.0 - UniaxialStress / Threshold))
 
 def ComputeAParameterLinear(Threshold, Gf, L, Young):
     return -Threshold**2 / (2.0 * Young * Gf / L)
 
+def ComputeAParameterExponential(Threshold, Gf, L, Young):
+    return 1.0 / (Gf * Young / (L * Threshold**2) - 0.5)
+
+
+
+
+"""
+Here we define the problem in terms of dimension, yield surface and linear/exponential softening
+"""
+
+# INPUT DATA
+dimension = 2 # 3
+yield_surface = "VonMises" # DruckerPrager, ModifiedMohrCoulomb, Rankine
+softening = "Linear" # Exponential
+
+# Common variables
+mode = "c"
+
+if (dimension == 2):
+    Strain0, Strain1, Strain2, Seff, Stress, Deviator, Ct, Young, nu, threshold, Gf, characteristic_length = SetUp2DProblem()
+    Seff[0], Seff[1], Seff[2] = ComputePredictorStressVector2D(Young, nu, Strain0, Strain1, Strain2)
+
+    pmean = ComputePmean2D(Seff)
+    Deviator[0] = Seff[0] - pmean
+    Deviator[1] = Seff[1] - pmean
+    Deviator[2] = Seff[2]
+    J2 = 0.0; uniaxial_stress = 0.0; A = 0.0; damage = 0.0;
+    if (yield_surface == "VonMises"):
+        J2 =  ComputeJ2Invariant2D(Deviator, pmean)
+        VonMisesStress = sqrt(3.0*J2)
+        damage = ComputeDamage(VonMisesStress, threshold, Gf, characteristic_length, Young, softening)
+
+    Stress[0] = (1.0 - damage)*Seff[0]
+    Stress[1] = (1.0 - damage)*Seff[1]
+    Stress[2] = (1.0 - damage)*Seff[2]
+
+    # Ct = dS/dE
+    Ct[0,0] = ((Stress[0]).diff(Strain0))
+    Ct[0,1] = ((Stress[0]).diff(Strain1))
+    Ct[0,2] = ((Stress[0]).diff(Strain2))
+    Ct[1,0] = ((Stress[1]).diff(Strain0))
+    Ct[1,1] = ((Stress[1]).diff(Strain1))
+    Ct[1,2] = ((Stress[1]).diff(Strain2))
+    Ct[2,0] = ((Stress[2]).diff(Strain0))
+    Ct[2,1] = ((Stress[2]).diff(Strain1))
+    Ct[2,2] = ((Stress[2]).diff(Strain2))
+
+    out = OutputMatrix_CollectingFactors(Ct, "r_Ct", mode)
+    print(out)
