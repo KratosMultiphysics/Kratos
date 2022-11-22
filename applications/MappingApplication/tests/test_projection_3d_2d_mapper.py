ef6df711634e3a1ebbafe77398237b0b97c214c8
Vicente Mataix Ferándiz <vmataix@altair.com>
2022-11-22 15:24:40 +0000

    WIP

--- applications/MappingApplication/tests/test_projection_3d_2d_mapper.py
+++ applications/MappingApplication/tests/test_projection_3d_2d_mapper.py
@@ -9,6 +9,7 @@ if default_data_comm.IsDistributed():
 from KratosMultiphysics.testing import utilities as testing_utils
 import mapper_test_case
 import os
+from math import sin, cos
 from sys import version_info as py_version_info
 
 def GetFilePath(file_name):
@@ -23,6 +24,8 @@ class Projection3D2DMapper(mapper_test_case.MapperTestCase):
         mdpa_1 = "3D_blocks_mesh1"
         mdpa_2 = "3D_blocks_mesh2"
         super().setUpModelParts(mdpa_1, mdpa_2)
+
+        cls.mapper_type = mapper_parameters["mapper_type"].GetString()
 
         if default_data_comm.IsDistributed():
             map_creator = MappingMPIExtension.MPIMapperFactory.CreateMapper
@@ -44,8 +47,59 @@ class Projection3D2DMapper(mapper_test_case.MapperTestCase):
         }""")
         cls.setUpMapper(mapper_params)
 
-    def test_Projection3D2DMapper(self):
-        pass
+    def test_Projection3D2DMapper_Map_non_constant_scalar(self):
+        SetHistoricalNonUniformSolutionScalar(self.model_part_origin.Nodes, KM.PRESSURE)
+        self.mapper.Map(KM.PRESSURE, KM.TEMPERATURE)
+        # mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_destination, KM.TEMPERATURE, GetFilePath(self._GetFileName("map_scalar")))
+
+    # def test_Projection3D2DMapper_InverseMap_non_constant_scalar(self):
+    #     SetHistoricalNonUniformSolutionScalar(self.model_part_destination.Nodes, KM.TEMPERATURE)
+    #     self.mapper.InverseMap(KM.PRESSURE, KM.TEMPERATURE)
+    #     mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_origin, KM.PRESSURE, GetFilePath(self._GetFileName("inverse_map_scalar")))
+
+    # def test_Projection3D2DMapper_Map_non_constant_vector(self):
+    #     SetHistoricalNonUniformSolutionVector(self.model_part_origin.Nodes, KM.FORCE)
+    #     self.mapper.Map(KM.FORCE, KM.VELOCITY)
+    #     mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_destination, KM.VELOCITY, GetFilePath(self._GetFileName("map_vector")))
+
+    # def test_Projection3D2DMapper_InverseMap_non_constant_vector(self):
+    #     SetHistoricalNonUniformSolutionVector(self.model_part_destination.Nodes, KM.VELOCITY)
+    #     self.mapper.InverseMap(KM.FORCE, KM.VELOCITY)
+    #     mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_origin, KM.FORCE, GetFilePath(self._GetFileName("inverse_map_vector")))
+
+    def _CheckHistoricalUniformValuesScalar(self, nodes, variable, exp_value):
+        for node in nodes:
+            self.assertAlmostEqual(node.GetSolutionStepValue(variable), exp_value)
+
+    def _CheckHistoricalUniformValuesVector(self, nodes, variable, exp_value):
+        for node in nodes:
+            self.assertVectorAlmostEqual(node.GetSolutionStepValue(variable), exp_value)
+
+    def _CheckUniformValuesScalar(self, entities, variable, exp_value):
+        for entity in entities:
+            self.assertAlmostEqual(entity.GetValue(variable), exp_value)
+
+    def _CheckUniformValuesVector(self, entities, variable, exp_value):
+        for entity in entities:
+            self.assertVectorAlmostEqual(entity.GetValue(variable), exp_value)
+
+    def _GetFileName(self, file_appendix):
+        return os.path.join("result_files", self.mapper_type, self.__class__.__name__ + "_" + file_appendix)
+
+def SetHistoricalNonUniformSolutionScalar(nodes, variable):
+    for node in nodes:
+        val = 12*sin(node.X0) + node.Y0*15
+        node.SetSolutionStepValue(variable, val)
+
+def SetHistoricalNonUniformSolutionVector(nodes, variable):
+    for node in nodes:
+        val_1 = 12*sin(node.X0) + node.Y0*15
+        val_2 = 33*cos(node.X0) + node.Y0*5
+        val_3 = 12*sin(node.Y0) + 22*node.X0
+        node.SetSolutionStepValue(variable, KM.Vector([val_1, val_2, val_3]))
+
+def GetNodes(model_part):
+    return model_part.Nodes
 
 if __name__ == '__main__':
     KM.Logger.GetDefaultOutput().SetSeverity(KM.Logger.Severity.WARNING)
