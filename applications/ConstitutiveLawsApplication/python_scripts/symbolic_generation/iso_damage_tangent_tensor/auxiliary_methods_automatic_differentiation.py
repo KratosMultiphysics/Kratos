2dee6c79468a48ec9020d5616009c1abc6d52565
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2022-11-23 10:21:41 +0100

    final version

--- applications/ConstitutiveLawsApplication/python_scripts/symbolic_generation/iso_damage_tangent_tensor/auxiliary_methods_automatic_differentiation.py
+++ applications/ConstitutiveLawsApplication/python_scripts/symbolic_generation/iso_damage_tangent_tensor/auxiliary_methods_automatic_differentiation.py
@@ -55,22 +55,45 @@ def SetUp3DProblem():
 
 
 def ComputePredictorStressVector2D(Young, nu, Strain0, Strain1, Strain2):
-    c0 = Young / ((1.00 + nu)*(1 - 2 * nu));
-    c1 = (1.00 - nu)*c0;
-    c2 = c0 * nu;
-    c3 = (0.5 - nu)*c0;
+    c0 = Young / ((1.0 + nu)*(1.0 - 2.0 * nu))
+    c1 = (1.0 - nu)*c0
+    c2 = c0 * nu
+    c3 = (0.5 - nu)*c0
 
-    S0 = c1 * Strain0 + c2 *Strain1;
-    S1 = c2 * Strain0 + c1 * Strain1;
-    S2 = c3 * Strain2;
+    S0 = c1 * Strain0 + c2 *Strain1
+    S1 = c2 * Strain0 + c1 * Strain1
+    S2 = c3 * Strain2
 
     return S0, S1, S2
+def ComputePredictorStressVector3D(Young, nu, Strain0, Strain1, Strain2, Strain3, Strain4, Strain5):
+    c1 = Young / ((1.0 + nu) * (1.0 - 2.0 * nu))
+    c2 = c1 * (1.0 - nu)
+    c3 = c1 * nu
+    c4 = c1 * 0.5 * (1.0 - 2.0 * nu)
+
+    S0 = c2 * Strain0 + c3 * Strain1 + c3 * Strain2
+    S1 = c3 * Strain0 + c2 * Strain1 + c3 * Strain2
+    S2 = c3 * Strain0 + c3 * Strain1 + c2 * Strain2
+    S3 = c4 * Strain3
+    S4 = c4 * Strain4
+    S5 = c4 * Strain5
+
+    return S0, S1, S2, S3, S4, S5
 
 def ComputeJ2Invariant2D(Deviator, pmean):
     return 0.5 * (Deviator[0]**2.0 + Deviator[1]**2.0 + pmean**2.0) + Deviator[2]**2.0
+def ComputeJ2Invariant3D(Deviator, pmean):
+    return 0.5*(Deviator[0]**2+Deviator[1]**2+Deviator[2]**2) + (Deviator[3]**2+Deviator[4]**2+Deviator[5]**2)
+
+def ComputeJ3_2D(Deviator):
+    return Deviator[0] * Deviator[1] - (Deviator[2]**2)
+def ComputeJ3_3D(Deviator):
+    return Deviator[0] * (Deviator[1] * Deviator[2] - Deviator[4] * Deviator[4]) + Deviator[3] * (-Deviator[3] * Deviator[2] + Deviator[5] * Deviator[4]) + Deviator[5] * (Deviator[3] * Deviator[4] - Deviator[5] * Deviator[1])
 
 def ComputePmean2D(StressVector):
     return (StressVector[0] + StressVector[1]) / 3.0
+def ComputePmean3D(StressVector):
+    return (StressVector[0] + StressVector[1] + StressVector[2]) / 3.0
 
 def ComputeDamage(UniaxialStress, Threshold, Gf, L, Young, Softening):
     if (Softening ==  "Linear"):
@@ -92,10 +115,6 @@ def ComputeAParameterLinear(Threshold, Gf, L, Young):
 def ComputeAParameterExponential(Threshold, Gf, L, Young):
     return 1.0 / (Gf * Young / (L * Threshold**2) - 0.5)
 
-def ComputeJ3_2D(Deviator):
-    return Deviator[0] * Deviator[1] - (Deviator[2]**2)
-
-
 
 
 """
@@ -103,13 +122,20 @@ Here we define the problem in terms of dimension, yield surface and linear/expon
 """
 
 # INPUT DATA
-dimension = 3 # 3
+"""
+---------------------------------------------------------------------------------------------------
+"""
+dimension = 3 # 2, 3
 yield_surface = "VonMises" # DruckerPrager, ModifiedMohrCoulomb, Rankine, VonMises
 softening = "Exponential" # Exponential, Linear
+"""
+---------------------------------------------------------------------------------------------------
+"""
 
 # Common variables
 mode = "c"
 
+J2 = 0.0; uniaxial_stress = 0.0; A = 0.0; damage = 0.0;
 if (dimension == 2):
     Strain0, Strain1, Strain2, Seff, Stress, Deviator, Ct, Young, nu, threshold, Gf, characteristic_length = SetUp2DProblem()
 
@@ -121,7 +147,6 @@ if (dimension == 2):
     Deviator[1] = Seff[1] - pmean
     Deviator[2] = Seff[2]
 
-    J2 = 0.0; uniaxial_stress = 0.0; A = 0.0; damage = 0.0;
     if (yield_surface == "VonMises"):
         J2 =  ComputeJ2Invariant2D(Deviator, pmean)
         VonMisesStress = sqrt(3.0*J2)
@@ -166,6 +191,8 @@ if (dimension == 2):
         else:
             A = -threshold_compression**2 / (2.0 * Young * Gf *R*R / characteristic_length)
             damage = (1.0 - threshold_compression / ModifiedMohrCoulombStress) / (1.0 + A)
+    else:
+        raise Exception("yield_surface not correctly defined, options available: VonMises, DruckerPrager, Rankine and ModifiedMohrCoulomb")
 
     Stress[0] = (1.0 - damage)*Seff[0]
     Stress[1] = (1.0 - damage)*Seff[1]
@@ -185,5 +212,140 @@ if (dimension == 2):
     out = OutputMatrix_CollectingFactors(Ct, "r_Ct", mode)
     print(out)
 
-# else: # 3D
+else: # 3D
+    Strain0, Strain1, Strain2, Strain3, Strain4, Strain5, Seff, Stress, Deviator, Ct, Young, nu, threshold, Gf, characteristic_length = SetUp3DProblem()
+    Seff[0], Seff[1], Seff[2], Seff[3], Seff[4], Seff[5] = ComputePredictorStressVector3D(Young, nu, Strain0, Strain1, Strain2)
+    pmean = ComputePmean3D(Seff)
 
+    Deviator[0] = Seff[0] - pmean
+    Deviator[1] = Seff[1] - pmean
+    Deviator[2] = Seff[2] - pmean
+    Deviator[3] = Seff[3]
+    Deviator[4] = Seff[4]
+    Deviator[5] = Seff[5]
+
+    if (yield_surface == "VonMises"):
+        J2 =  ComputeJ2Invariant3D(Deviator, pmean)
+        VonMisesStress = sqrt(3.0*J2)
+        damage = ComputeDamage(VonMisesStress, threshold, Gf, characteristic_length, Young, softening)
+    elif (yield_surface == "Rankine"):
+        norm_stress_vector = DefineVector('norm_stress_vector', 6)
+        PrincipalStresses = DefineVector('PrincipalStresses', 3)
+        norm_frobenius = sqrt(Seff[0]**2 + Seff[1]**2 + Seff[2]**2+Seff[3]**2 + Seff[4]**2 + Seff[5]**2)
+        norm_stress_vector[0] = Seff[0] / norm_frobenius
+        norm_stress_vector[1] = Seff[1] / norm_frobenius
+        norm_stress_vector[2] = Seff[2] / norm_frobenius
+        norm_stress_vector[3] = Seff[3] / norm_frobenius
+        norm_stress_vector[4] = Seff[4] / norm_frobenius
+        norm_stress_vector[5] = Seff[5] / norm_frobenius
+
+        I1 = norm_stress_vector[0] + norm_stress_vector[1] + norm_stress_vector[2]
+        I2 = (norm_stress_vector[0] + norm_stress_vector[2]) * norm_stress_vector[1] + norm_stress_vector[0] * norm_stress_vector[2] -norm_stress_vector[3] * norm_stress_vector[3] - norm_stress_vector[4] * norm_stress_vector[4] - norm_stress_vector[5] * norm_stress_vector[5];
+        I3 = (norm_stress_vector[1] * norm_stress_vector[2] - norm_stress_vector[4] * norm_stress_vector[4]) * norm_stress_vector[0] - norm_stress_vector[1] * norm_stress_vector[5] * norm_stress_vector[5] - norm_stress_vector[2] * norm_stress_vector[3] * norm_stress_vector[3] + 2.0 * norm_stress_vector[3] * norm_stress_vector[4] * norm_stress_vector[5]
+        II1 = I1**2
+        R = (2.0 * II1 * I1 - 9.0 * I2 * I1 + 27.0 * I3) / 54.0;
+        Q = (3.0 * I2 - II1) / 9.0;
+        cos_phi = R / (sqrt(-Q**3));
+        phi = acos(cos_phi);
+        phi_3 = phi / 3.0;
+        aux1 = 2.0 * sqrt(-Q);
+        aux2 = I1 / 3.0;
+        deg_120 = 2.0 / 3.0 * math.pi;
+        for i in range(3):
+            PrincipalStresses[i] = norm_frobenius * (aux2 + aux1 * cos(phi_3 + deg_120 * i));
+
+        rankine_yield = PrincipalStresses[0]
+        damage = ComputeDamage(rankine_yield, threshold, Gf, characteristic_length, Young, softening)
+    elif (yield_surface == "DruckerPrager"):
+        sin_phi = Symbol("sin_phi")
+        J2 =  ComputeJ2Invariant3D(Deviator, pmean)
+        root_3 = math.sqrt(3.0)
+        CFL = -root_3 * (3.0 - sin_phi) / (3.0 * sin_phi - 3.0)
+        TEN0 = 6.0 * pmean * sin_phi / (root_3 * (3.0 - sin_phi)) + sqrt(J2)
+        DruckerPragerStress = CFL*TEN0
+        damage_threshold = abs(threshold * (3.0 + sin_phi) / (3.0 * sin_phi - 3.0))
+        if (softening == "Exponential"):
+            A = 1.0 / (Gf * Young / (characteristic_length * threshold**2) - 0.5)
+            damage_threshold = abs(threshold * (3.0 + sin_phi) / (3.0 * sin_phi - 3.0))
+            damage = 1.0 - (damage_threshold / DruckerPragerStress) * exp(A * (1.0 - DruckerPragerStress / damage_threshold))
+        else:
+            A = -threshold**2 / (2.0 * Young * Gf / characteristic_length)
+            damage = (1.0 - damage_threshold / DruckerPragerStress) / (1.0 + A)
+    elif (yield_surface == "ModifiedMohrCoulomb"):
+        phi = Symbol("phi")
+        threshold_tension = Symbol("threshold_tension") # f_t
+        threshold_compression = Symbol("threshold_compression") # f_t
+        J2 =  ComputeJ2Invariant3D(Deviator, pmean)
+        R = abs(threshold_compression / threshold_tension)
+        Rmohr = (tan((math.pi / 4.0) + phi / 2.0))**2
+        alpha_r = R / Rmohr
+        sin_phi = sin(phi);
+        J3 = ComputeJ3_3D(Deviator)
+        K1 = 0.5 * (1.0 + alpha_r) - 0.5 * (1.0 - alpha_r) * sin_phi;
+        K2 = 0.5 * (1.0 + alpha_r) - 0.5 * (1.0 - alpha_r) / sin_phi;
+        K3 = 0.5 * (1.0 + alpha_r) * sin_phi - 0.5 * (1.0 - alpha_r);
+        sint3 = (-3.0 * sqrt(3.0) * J3) / (2.0 * J2 * sqrt(J2))
+        LodeAngle = asin(sint3) / 3.0;
+        ModifiedMohrCoulombStress = (2.0 * tan(math.pi * 0.25 + phi * 0.5) / cos(phi)) * ((3.0*pmean * K3 / 3.0) +sqrt(J2) * (K1 * cos(LodeAngle) - K2 * sin(LodeAngle) * sin_phi / sqrt(3.0)))
+        if (softening == "Exponential"):
+            A = 1.0 / (Gf * Young * R * R / (characteristic_length * threshold_compression**2) - 0.5)
+            damage = 1.0 - (threshold_compression / ModifiedMohrCoulombStress) * exp(A * (1.0 - ModifiedMohrCoulombStress / threshold_compression))
+        else:
+            A = -threshold_compression**2 / (2.0 * Young * Gf *R*R / characteristic_length)
+            damage = (1.0 - threshold_compression / ModifiedMohrCoulombStress) / (1.0 + A)
+    else:
+        raise Exception("yield_surface not correctly defined, options available: VonMises, DruckerPrager, Rankine and ModifiedMohrCoulomb")
+
+    Stress[0] = (1.0 - damage)*Seff[0]
+    Stress[1] = (1.0 - damage)*Seff[1]
+    Stress[2] = (1.0 - damage)*Seff[2]
+    Stress[3] = (1.0 - damage)*Seff[3]
+    Stress[4] = (1.0 - damage)*Seff[4]
+    Stress[5] = (1.0 - damage)*Seff[5]
+
+    # # Ct = dS/dE
+    Ct[0,0] = ((Stress[0]).diff(Strain0))
+    Ct[0,1] = ((Stress[0]).diff(Strain1))
+    Ct[0,2] = ((Stress[0]).diff(Strain2))
+    Ct[0,3] = ((Stress[0]).diff(Strain3))
+    Ct[0,4] = ((Stress[0]).diff(Strain4))
+    Ct[0,5] = ((Stress[0]).diff(Strain5))
+
+    Ct[1,0] = ((Stress[1]).diff(Strain0))
+    Ct[1,1] = ((Stress[1]).diff(Strain1))
+    Ct[1,2] = ((Stress[1]).diff(Strain2))
+    Ct[1,3] = ((Stress[1]).diff(Strain3))
+    Ct[1,4] = ((Stress[1]).diff(Strain4))
+    Ct[1,5] = ((Stress[1]).diff(Strain5))
+
+    Ct[2,0] = ((Stress[2]).diff(Strain0))
+    Ct[2,1] = ((Stress[2]).diff(Strain1))
+    Ct[2,2] = ((Stress[2]).diff(Strain2))
+    Ct[2,3] = ((Stress[2]).diff(Strain3))
+    Ct[2,4] = ((Stress[2]).diff(Strain4))
+    Ct[2,5] = ((Stress[2]).diff(Strain5))
+
+    Ct[3,0] = ((Stress[3]).diff(Strain0))
+    Ct[3,1] = ((Stress[3]).diff(Strain1))
+    Ct[3,2] = ((Stress[3]).diff(Strain2))
+    Ct[3,3] = ((Stress[3]).diff(Strain3))
+    Ct[3,4] = ((Stress[3]).diff(Strain4))
+    Ct[3,5] = ((Stress[3]).diff(Strain5))
+
+    Ct[4,0] = ((Stress[4]).diff(Strain0))
+    Ct[4,1] = ((Stress[4]).diff(Strain1))
+    Ct[4,2] = ((Stress[4]).diff(Strain2))
+    Ct[4,3] = ((Stress[4]).diff(Strain3))
+    Ct[4,4] = ((Stress[4]).diff(Strain4))
+    Ct[4,5] = ((Stress[4]).diff(Strain5))
+
+    Ct[5,0] = ((Stress[5]).diff(Strain0))
+    Ct[5,1] = ((Stress[5]).diff(Strain1))
+    Ct[5,2] = ((Stress[5]).diff(Strain2))
+    Ct[5,3] = ((Stress[5]).diff(Strain3))
+    Ct[5,4] = ((Stress[5]).diff(Strain4))
+    Ct[5,5] = ((Stress[5]).diff(Strain5))
+    # ...
+
+    out = OutputMatrix_CollectingFactors(Ct, "r_Ct", mode)
+    print(out)
