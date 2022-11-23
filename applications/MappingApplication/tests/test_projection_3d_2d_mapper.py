f5d997e57bda428d3254e3e23bf5bf9172cee578
Vicente Mataix Ferándiz <vmataix@altair.com>
2022-11-23 07:11:30 +0000

    Fix test

--- applications/MappingApplication/tests/test_projection_3d_2d_mapper.py
+++ applications/MappingApplication/tests/test_projection_3d_2d_mapper.py
@@ -13,7 +13,7 @@ from math import sin, cos
 def GetFilePath(file_name):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), file_name)
 
-class Projection3D2DMapperBase(mapper_test_case.MapperTestCase):
+class Projection3D2DMapper(mapper_test_case.MapperTestCase):
     '''This class contains the tests for 3D-2D projections
     '''
 
@@ -23,33 +23,45 @@ class Projection3D2DMapperBase(mapper_test_case.MapperTestCase):
         mdpa_2 = "3D_blocks_mesh2"
         super().setUpModelParts(mdpa_1, mdpa_2)
 
-        cls.mapper_type = mapper_parameters["mapper_type"].GetString()
+        cls.mapper_type = mapper_parameters["mapper_type"].GetString() + "_" + mapper_parameters["base_mapper"].GetString()
 
         if data_comm.IsDistributed():
             cls.mapper = MappingMPIExtension.MPIMapperFactory.CreateMapper(cls.model_part_origin, cls.model_part_destination, mapper_parameters)
         else:
             cls.mapper = KM.MapperFactory.CreateMapper(cls.model_part_origin, cls.model_part_destination, mapper_parameters)
 
+    @classmethod
+    def setUpClass(cls):
+        mapper_params = KM.Parameters("""{
+            "mapper_type" : "projection_3D_2D",
+            "base_mapper" : "nearest_neighbor",
+            "echo_level"  : 0
+        }""")
+        cls.setUpMapper(mapper_params)
+
     def test_Projection3D2DMapper_Map_non_constant_scalar(self):
         SetHistoricalNonUniformSolutionScalar(self.model_part_origin.Nodes, KM.PRESSURE)
         self.mapper.Map(KM.PRESSURE, KM.TEMPERATURE)
-        self.CheckHistoricalNonUniformValues(self.model_part_destination, KM.TEMPERATURE, GetFilePath(self._GetFileName("map_scalar")), True)
-        self.VtkOutputNodesHistorical(self.model_part_destination, KM.TEMPERATURE)
+        mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_destination, KM.TEMPERATURE, GetFilePath(self._GetFileName("map_scalar")))
+        #mapper_test_case.VtkOutputNodesHistorical(self.model_part_destination, KM.TEMPERATURE)
 
     # def test_Projection3D2DMapper_InverseMap_non_constant_scalar(self):
     #     SetHistoricalNonUniformSolutionScalar(self.model_part_destination.Nodes, KM.TEMPERATURE)
     #     self.mapper.InverseMap(KM.PRESSURE, KM.TEMPERATURE)
-    #     self.CheckHistoricalNonUniformValues(self.model_part_origin, KM.PRESSURE, GetFilePath(self._GetFileName("inverse_map_scalar")))
+    #     mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_origin, KM.PRESSURE, GetFilePath(self._GetFileName("inverse_map_scalar")), True)
+    #     #mapper_test_case.VtkOutputNodesHistorical(self.model_part_origin, KM.PRESSURE)
 
-    # def test_Projection3D2DMapper_Map_non_constant_vector(self):
-    #     SetHistoricalNonUniformSolutionVector(self.model_part_origin.Nodes, KM.FORCE)
-    #     self.mapper.Map(KM.FORCE, KM.VELOCITY)
-    #     self.CheckHistoricalNonUniformValues(self.model_part_destination, KM.VELOCITY, GetFilePath(self._GetFileName("map_vector")))
+    def test_Projection3D2DMapper_Map_non_constant_vector(self):
+        SetHistoricalNonUniformSolutionVector(self.model_part_origin.Nodes, KM.FORCE)
+        self.mapper.Map(KM.FORCE, KM.VELOCITY)
+        mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_destination, KM.VELOCITY, GetFilePath(self._GetFileName("map_vector")))
+        #mapper_test_case.VtkOutputNodesHistorical(self.model_part_destination, KM.VELOCITY)
 
     # def test_Projection3D2DMapper_InverseMap_non_constant_vector(self):
     #     SetHistoricalNonUniformSolutionVector(self.model_part_destination.Nodes, KM.VELOCITY)
     #     self.mapper.InverseMap(KM.FORCE, KM.VELOCITY)
-    #     self.CheckHistoricalNonUniformValues(self.model_part_origin, KM.FORCE, GetFilePath(self._GetFileName("inverse_map_vector")))
+    #     mapper_test_case.CheckHistoricalNonUniformValues(self.model_part_origin, KM.FORCE, GetFilePath(self._GetFileName("inverse_map_vector")), True)
+    #     #mapper_test_case.VtkOutputNodesHistorical(self.model_part_origin, KM.FORCE)
 
     def _GetFileName(self, file_appendix):
         return os.path.join("result_files", self.mapper_type, self.__class__.__name__ + "_" + file_appendix)
@@ -65,16 +77,6 @@ def SetHistoricalNonUniformSolutionVector(nodes, variable):
         val_2 = 33*cos(node.X0) + node.Y0*5
         val_3 = 12*sin(node.Y0) + 22*node.X0
         node.SetSolutionStepValue(variable, KM.Vector([val_1, val_2, val_3]))
-
-class Projection3D2DMapper(Projection3D2DMapperBase):
-    @classmethod
-    def setUpClass(cls):
-        mapper_params = KM.Parameters("""{
-            "mapper_type" : "projection_3D_2D",
-            "base_mapper" : "nearest_neighbor",
-            "echo_level"  : 0
-        }""")
-        cls.setUpMapper(mapper_params)
 
 if __name__ == '__main__':
     KM.Logger.GetDefaultOutput().SetSeverity(KM.Logger.Severity.WARNING)
