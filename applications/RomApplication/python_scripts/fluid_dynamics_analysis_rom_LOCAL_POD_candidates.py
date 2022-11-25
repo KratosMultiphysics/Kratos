68bc4e1e8561d8a2a126eeba5a30665e2a90bd3d
Raul Bravo <rbravo555@gmail.com>
2022-11-25 14:53:29 +0100

    Draft modification to cope with ALE

--- applications/RomApplication/python_scripts/fluid_dynamics_analysis_rom_LOCAL_POD_candidates.py
+++ applications/RomApplication/python_scripts/fluid_dynamics_analysis_rom_LOCAL_POD_candidates.py
@@ -60,104 +60,116 @@ class FluidDynamicsAnalysisROM(FluidDynamicsAnalysis):
                         for i in range(rom_dofs[k]):
                             aux[j,i] = nodal_modes[Counter][str(k)][j][i]
                     POD_BASES[k].SetNodalBasis(node.Id, aux)
-            distance_to_clusters = romapp.DistanceToClusters(number_of_bases)
-            w = data["w"]
-            z0 = data["z0"]
-            for i in range(number_of_bases):
-                for j in range(number_of_bases):
-                    distance_to_clusters.SetZEntry(z0[f'{i}'][f'{j}'],i,j)
-                    for k in range(number_of_bases):
-                        entries = len(w[f'{i}'][f'{j}'][f'{k}'])
-                        temp_vector = KratosMultiphysics.Vector(entries)
-                        for entry in range(entries):
-                            temp_vector[entry] = w[f'{i}'][f'{j}'][f'{k}'][entry]
-                        distance_to_clusters.SetWEntry(temp_vector,i,j,k)
+            # distance_to_clusters = romapp.DistanceToClusters(number_of_bases)
+            # w = data["w"]
+            # z0 = data["z0"]
+            # for i in range(number_of_bases):
+            #     for j in range(number_of_bases):
+            #         distance_to_clusters.SetZEntry(z0[f'{i}'][f'{j}'],i,j)
+            #         for k in range(number_of_bases):
+            #             entries = len(w[f'{i}'][f'{j}'][f'{k}'])
+            #             temp_vector = KratosMultiphysics.Vector(entries)
+            #             for entry in range(entries):
+            #                 temp_vector[entry] = w[f'{i}'][f'{j}'][f'{k}'][entry]
+            #             distance_to_clusters.SetWEntry(temp_vector,i,j,k)
         Bases = romapp.RomBases()
         for i in range(number_of_bases):
             Bases.AddBasis(i, POD_BASES[i])
         self.Number_Of_Clusters = number_of_bases
         self._GetSolver().fluid_solver._GetBuilderAndSolver().SetUpBases(Bases)
-        self._GetSolver().fluid_solver._GetBuilderAndSolver().SetUpDistances(distance_to_clusters)
+        #self._GetSolver().fluid_solver._GetBuilderAndSolver().SetUpDistances(distance_to_clusters)
 
         self.step_index = 0
+
+        if self.hyper_reduction_element_selector != None:
+            if self.hyper_reduction_element_selector.Name == "EmpiricalCubature":
+                for i in range(self.Number_Of_Clusters):
+                    self.time_step_residual_matrix_container.append([])
+                self.ResidualUtilityObject = romapp.RomResidualsUtility(self._GetSolver().GetComputingModelPart(), self.project_parameters["solver_settings"]["rom_settings"], self._GetSolver().fluid_solver._GetScheme(), Bases)
+
+
 
 
     def InitializeSolutionStep(self):
         super().InitializeSolutionStep()
-        self._GetSolver().fluid_solver._GetBuilderAndSolver().UpdateCurrentCluster()
+        #self._GetSolver().fluid_solver._GetBuilderAndSolver().UpdateCurrentCluster()
 
 
     def FinalizeSolutionStep(self):
-        self._GetSolver().fluid_solver._GetBuilderAndSolver().UpdateZMatrix()
+        #self._GetSolver().fluid_solver._GetBuilderAndSolver().UpdateZMatrix()
         if self.hyper_reduction_element_selector != None:
-            if self.hyper_reduction_element_selector.Name == "EmpiricalCubature":
-                print('\n\n\n\nGenerating matrix of residuals')
-                ResMat = self.ResidualUtilityObject.GetResiduals(self._GetSolver().fluid_solver._GetBuilderAndSolver().GetCurrentCluster())
-                #print(np.shape(ResMat))
-                NP_ResMat = np.array(ResMat, copy=False)
-                self.time_step_residual_matrix_container[self._GetSolver().fluid_solver._GetBuilderAndSolver().GetCurrentCluster()].append(NP_ResMat)
-                #self.time_step_residual_matrix_container.append(NP_ResMat)
+            if self.time>0.2: #TODO why starting from 0 causes a segmentation fault? clearly, not everything is correctly set at the beggining
+                if self.hyper_reduction_element_selector.Name == "EmpiricalCubature":
+                    print('\n\n\n\nGenerating matrix of residuals')
+                    ResMat = self.ResidualUtilityObject.GetResiduals(self._GetSolver().fluid_solver._GetBuilderAndSolver().GetCurrentCluster())
+                    #print(np.shape(ResMat))
+                    NP_ResMat = np.array(ResMat, copy=False)
+                    self.time_step_residual_matrix_container[self._GetSolver().fluid_solver._GetBuilderAndSolver().GetCurrentCluster()].append(NP_ResMat)
+                    #self.time_step_residual_matrix_container.append(NP_ResMat)
         super().FinalizeSolutionStep()
 
-    # def Finalize(self):
-    #     super().Finalize()
-    #     #for the case of multiple scenarios, one should store and concatenate the projected residuals projected onto each basis. The
-    #     # method is then:
-    #     # 1 during the simulation store the projected residuals onto the basis used
-    #     # 2 check whether a file in format .npy with the residials projected already exists
-    #     #  if it does exist, append the new residuals to the correspoding basis
-    #     #  if it does not exist, create a new file with the residuals projected onto the basis
-    #     if self.hyper_reduction_element_selector != None:
-    #         if self.hyper_reduction_element_selector.Name == "EmpiricalCubature":
-    #             z_i = []
-    #             w_i = []
-    #             z = None
-    #             number_bases =len(self.time_step_residual_matrix_container)
-    #             for i in range(number_bases):
-    #                 #TODO implement here the construction of the single set of elements and multiple bases
-    #                 basis_i = self._ObtainBasis(self.time_step_residual_matrix_container[i])
-    #                 self.hyper_reduction_element_selector.SetUp(basis_i.T, z)
-    #                 self.hyper_reduction_element_selector.Initialize()
-    #                 self.hyper_reduction_element_selector.Calculate()
-    #                 w_i.append(np.squeeze(self.hyper_reduction_element_selector.w))
-    #                 z_i.append(np.squeeze(self.hyper_reduction_element_selector.z))
-    #                 if z is None:
-    #                     z = z_i[i]
-    #                 else:
-    #                     z = np.union1d(z,z_i[i])
-    #                 print(z)
-
-    #             WeightsMatrix = np.zeros(( np.size(z) ,number_bases))
-
-    #             for i in range(number_bases):
-    #                 for j in range(len(z_i[i])):
-    #                     index = np.where( z ==   z_i[i][j] )
-    #                     if np.array([0]) == index[0]:
-    #                         WeightsMatrix[index[0] , i] = w_i[i][j]
-    #                     elif not index[0]:
-    #                         pass
-    #                     else:
-    #                         WeightsMatrix[index[0] , i] = w_i[i][j]
+    def Finalize(self):
+        super().Finalize()
+        #for the case of multiple scenarios, one should store and concatenate the projected residuals projected onto each basis. The
+        # method is then:
+        # 1 during the simulation store the projected residuals onto the basis used
+        # 2 check whether a file in format .npy with the residials projected already exists
+        #  if it does exist, append the new residuals to the correspoding basis
+        #  if it does not exist, create a new file with the residuals projected onto the basis
 
 
-    #             np.save('WeightsMatrix.npy',WeightsMatrix )
-    #             np.save('Elementsvector.npy', z)
+        if self.hyper_reduction_element_selector != None:
+            if self.hyper_reduction_element_selector.Name == "EmpiricalCubature":
+                z_i = []
+                w_i = []
+                z = None
+                number_bases =len(self.time_step_residual_matrix_container)
+                for i in range(number_bases):
+                    #TODO implement here the construction of the single set of elements and multiple bases
+                    basis_i = self._ObtainBasis(self.time_step_residual_matrix_container[i])
+                    np.save(f'HROM_Local/Sr_{i}.npy', basis_i)
+                #     self.hyper_reduction_element_selector.SetUp(basis_i.T, z)
+                #     self.hyper_reduction_element_selector.Initialize()
+                #     self.hyper_reduction_element_selector.Calculate()
+                #     w_i.append(np.squeeze(self.hyper_reduction_element_selector.w))
+                #     z_i.append(np.squeeze(self.hyper_reduction_element_selector.z))
+                #     if z is None:
+                #         z = z_i[i]
+                #     else:
+                #         z = np.union1d(z,z_i[i])
+                #     print(z)
 
-    #             OriginalNumberOfElements = self._GetSolver().fluid_solver.GetComputingModelPart().NumberOfElements()
-    #             ModelPartName = self._GetSolver().fluid_solver.settings["model_import_settings"]["input_filename"].GetString()
-    #             self.hyper_reduction_element_selector.WriteSelectedElements(OriginalNumberOfElements, ModelPartName,z)
+                # WeightsMatrix = np.zeros(( np.size(z) ,number_bases))
+
+                # for i in range(number_bases):
+                #     for j in range(len(z_i[i])):
+                #         index = np.where( z ==   z_i[i][j] )
+                #         if np.array([0]) == index[0]:
+                #             WeightsMatrix[index[0] , i] = w_i[i][j]
+                #         elif not index[0]:
+                #             pass
+                #         else:
+                #             WeightsMatrix[index[0] , i] = w_i[i][j]
+
+
+                # np.save('WeightsMatrix.npy',WeightsMatrix )
+                # np.save('Elementsvector.npy', z)
+
+                # OriginalNumberOfElements = self._GetSolver().fluid_solver.GetComputingModelPart().NumberOfElements()
+                # ModelPartName = self._GetSolver().fluid_solver.settings["model_import_settings"]["input_filename"].GetString()
+                # self.hyper_reduction_element_selector.WriteSelectedElements(OriginalNumberOfElements, ModelPartName,z)
 
 
 
-    # def _ObtainBasis(self, a_list):
-    #     ### Building the Snapshot matrix ####
-    #     for i in range (len(a_list)):
-    #         if i == 0:
-    #             SnapshotMatrix = a_list[i]
-    #         else:
-    #             SnapshotMatrix = np.c_[SnapshotMatrix,a_list[i]]
-    #     U,_,_,_ = RandomizedSingularValueDecomposition().Calculate(SnapshotMatrix, 1e-6)
-    #     return U
+    def _ObtainBasis(self, a_list):
+        ### Building the Snapshot matrix ####
+        for i in range (len(a_list)):
+            if i == 0:
+                SnapshotMatrix = a_list[i]
+            else:
+                SnapshotMatrix = np.c_[SnapshotMatrix,a_list[i]]
+        #U,_,_,_ = RandomizedSingularValueDecomposition().Calculate(SnapshotMatrix, 1e-6)
+        return SnapshotMatrix
 
 
 
