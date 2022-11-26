2d817ec85af5d682ef87701fe4e43b49eddd501e
Philipp <philipp.bucher@tum.de>
2022-11-26 12:08:39 +0100

    cleanup

--- applications/MetisApplication/custom_processes/metis_divide_heterogeneous_input_in_memory_process.cpp
+++ applications/MetisApplication/custom_processes/metis_divide_heterogeneous_input_in_memory_process.cpp
@@ -20,7 +20,6 @@
 
 
 // Project includes
-#include "mpi/includes/mpi_data_communicator.h"
 #include "metis_divide_heterogeneous_input_in_memory_process.h"
 
 
@@ -30,19 +29,10 @@ void MetisDivideHeterogeneousInputInMemoryProcess::Execute()
 {
     KRATOS_TRY
 
-    MPI_Comm this_comm = MPIDataCommunicator::GetMPICommunicator(mrDataComm);
+    const int mpi_rank = mrDataComm.Rank();
+    const int mpi_size = mrDataComm.Size();
 
-    int mpi_rank = mrDataComm.Rank();
-    int mpi_size = mrDataComm.Size();
-
-    // const char ** mpi_send_buffer = new const char * [mpi_size];
-    // std::string * str = new std::string[mpi_size];
-
-
-
-    using BufferType = std::vector<char>;
-
-    std::vector<BufferType> send_buffer;
+    std::vector<std::vector<char>> send_buffer;
 
     // Main process calculates the partitions and writes the result into temporal streams
     if(mpi_rank == 0) {
@@ -80,78 +70,19 @@ void MetisDivideHeterogeneousInputInMemoryProcess::Execute()
         }
     }
 
-    KRATOS_WATCH("1111");
+    auto recv_buffer = mrDataComm.Scatterv(send_buffer, 0);
 
-    BufferType recv_buffer = mrDataComm.Scatterv(send_buffer, 0);
-    KRATOS_WATCH("222");
 
-    // // Calculate the message and prepare the buffers
-    // std::vector<int> send_sizes(mpi_size);
-    // if(mpi_rank == 0) {
-    //     for(auto i = 0; i < mpi_size; i++) {
-    //         str[i] = stringbufs[i].str();
-    //         send_sizes[i] = str[i].size();
-    //         mpi_send_buffer[i] = str[i].c_str();
-    //     }
-    // }
-
-    // // Send the message size to all processes
-    // std::vector<int> vec_recv_size(1);
-    // mrDataComm.Scatter(send_sizes, vec_recv_size, 0);
-    // const int recv_size = vec_recv_size[0];
-
-    // // Calculate the number of events:
-    // auto NumberOfCommunicationEvents = 1 + mpi_size * !mpi_rank;
-    // auto NumberOfCommunicationEventsIndex = 0;
-
-    // // Prepare the communication events
-    // MPI_Request * reqs = new MPI_Request[NumberOfCommunicationEvents];
-    // MPI_Status * stats = new MPI_Status[NumberOfCommunicationEvents];
-
-    // // Set up all receive and send events
-    // if( mpi_rank == 0) {
-    //     for(auto i = 0; i < mpi_size; i++) {
-    //         char* aux_char = const_cast<char*>(mpi_send_buffer[i]);
-    //         MPI_Isend(aux_char,send_sizes[i],MPI_CHAR,i,0,this_comm,&reqs[NumberOfCommunicationEventsIndex++]);
-    //     }
-    // }
-
-    // // Receive the buffers
-    // std::vector<char> recv_buffer(recv_size);
-    // MPI_Irecv(recv_buffer.data(), recv_size,MPI_CHAR,0,0,this_comm,&reqs[NumberOfCommunicationEventsIndex++]);
-
-    // // Wait untill all communications finish
-    // if( MPI_Waitall(NumberOfCommunicationEvents, reqs, stats) != MPI_SUCCESS ) {
-    //     KRATOS_ERROR << "Error in metis_partition_mem" << std::endl;
-    // }
-
-    // mrDataComm.Barrier();
-
-    // if(mpi_rank != 0) {
-        // streams[mpi_rank]->write(recv_buffer.data(), recv_buffer.size());
-    // }
-    KRATOS_WATCH("333");
+    auto p_local_stream(Kratos::make_shared<std::iostream>(new std::stringbuf()));
+    p_local_stream->write(recv_buffer.data(), recv_buffer.size());
 
     // if (mVerbosity > 1) {
     //     std::ofstream debug_ofstream("MetisDivideHeterogeneousInputInMemoryProcess_debug_modelpart_"+std::to_string(mpi_rank)+".mdpa");
     //     debug_ofstream << stringbufs[mpi_rank].str() << std::endl;
     // }
-    KRATOS_WATCH("444");
-
-    auto p_local_stream(Kratos::make_shared<std::iostream>(new std::stringbuf()));
-    p_local_stream->write(recv_buffer.data(), recv_buffer.size());
 
     // TODO: Try to come up with a better way to change the buffer.
     mrSerialIO.SwapStreamSource(p_local_stream);
-    KRATOS_WATCH("555");
-
-    // Free buffers
-    // delete [] reqs;
-    // delete [] stats;
-
-    // delete [] mpi_send_buffer;
-    // delete [] str;
-    KRATOS_WATCH("666");
 
     KRATOS_CATCH("")
 }
