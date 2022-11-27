af3f067f01f7ef9da03a17976cd52c7ab57c269b
Philipp <philipp.bucher@tum.de>
2022-11-27 11:56:24 +0100

    add empty WriteModelpart

--- applications/MedApplication/custom_io/med_model_part_io.cpp
+++ applications/MedApplication/custom_io/med_model_part_io.cpp
@@ -24,12 +24,32 @@ namespace Kratos {
 MedModelPartIO::MedModelPartIO(const std::filesystem::path& rFileName)
     : mFileName(rFileName)
 {
+    KRATOS_TRY
 
+
+    KRATOS_CATCH("")
 }
 
 void MedModelPartIO::ReadModelPart(ModelPart& rThisModelPart)
 {
+    KRATOS_TRY
+
     KRATOS_ERROR << "Calling base class method (ReadModelPart). Please check the definition of derived class" << std::endl;
+
+    KRATOS_CATCH("")
+}
+
+void MedModelPartIO::WriteModelPart(ModelPart& rThisModelPart)
+{
+    KRATOS_TRY
+
+    // writing should not modify the original object
+    // hence using a const reference in the following
+    const auto& r_model_part = rThisModelPart;
+
+    KRATOS_ERROR << "Calling base class method (WriteModelPart). Please check the definition of derived class" << std::endl;
+
+    KRATOS_CATCH("")
 }
 
 } // namespace Kratos.
