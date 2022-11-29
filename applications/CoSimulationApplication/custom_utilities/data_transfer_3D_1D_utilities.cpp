b1f9556012dea9af8fad3c6170f5a63f8f09b245
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-11-29 14:13:50 +0100

    More WIP

--- applications/CoSimulationApplication/custom_utilities/data_transfer_3D_1D_utilities.cpp
+++ applications/CoSimulationApplication/custom_utilities/data_transfer_3D_1D_utilities.cpp
@@ -31,115 +31,125 @@ void DataTransfer3D1DUtilities::From3Dto1DDataTransfer(
     Parameters ThisParameters
     )
 {
-    // // Validate deafult parameters
-    // ThisParameters.ValidateAndAssignDefaults(GetDefaultParameters());
+    // Validate deafult parameters
+    ThisParameters.ValidateAndAssignDefaults(GetDefaultParameters());
 
-    // // Getting variables
-    // std::vector<const Variable<double>*> origin_list_variables, destination_list_variables;
-    // GetVariablesList(ThisParameters, origin_list_variables, destination_list_variables);
+    // Getting variables
+    std::vector<const Variable<double>*> origin_list_variables, destination_list_variables;
+    GetVariablesList(ThisParameters, origin_list_variables, destination_list_variables);
     
-    // // Max length of the elements considered
-    // auto& r_elements_array = rModelPart1D.Elements();
-    // const auto it_elem_begin = r_elements_array.begin();
-    // KRATOS_ERROR_IF(r_elements_array.size() == 0) << "Empty 1D model part" << std::endl;
-    // double max_length = 0.0;
-    // max_length = block_for_each<MaxReduction<double>>(r_elements_array, [&](Element& rElement) {
-    //     return rElement.GetGeometry().Length();
-    // });
+    // Max length of the elements considered
+    auto& r_elements_array = rModelPart1D.Elements();
+    const auto it_elem_begin = r_elements_array.begin();
+    KRATOS_ERROR_IF(r_elements_array.size() == 0) << "Empty 1D model part" << std::endl;
+    double max_length = 0.0;
+    max_length = block_for_each<MaxReduction<double>>(r_elements_array, [&](Element& rElement) {
+        return rElement.GetGeometry().Length();
+    });
 
-    // /// Type definitions for the tree
-    // using PointType = PointElement;
-    // using PointTypePointer = PointType::Pointer;
-    // using PointVector = std::vector<PointTypePointer>;
-    // using PointIterator = PointVector::iterator;
-    // using DistanceVector = std::vector<double>;
-    // using DistanceIterator = DistanceVector::iterator;
+    /// Type definitions for the tree
+    using PointType = PointElement;
+    using PointTypePointer = PointType::Pointer;
+    using PointVector = std::vector<PointTypePointer>;
+    using PointIterator = PointVector::iterator;
+    using DistanceVector = std::vector<double>;
+    using DistanceIterator = DistanceVector::iterator;
 
-    // /// KDtree definitions
-    // using BucketType = Bucket< 3ul, PointType, PointVector, PointTypePointer, PointIterator, DistanceIterator >;
-    // using KDTree = Tree< KDTreePartition<BucketType> >;
+    /// KDtree definitions
+    using BucketType = Bucket< 3ul, PointType, PointVector, PointTypePointer, PointIterator, DistanceIterator >;
+    using KDTree = Tree< KDTreePartition<BucketType> >;
 
-    // // Some auxiliary values
-    // const IndexType allocation_size = ThisParameters["search_parameters"]["allocation_size"].GetInt();                 // Allocation size for the vectors and max number of potential results
-    // const double search_factor = ThisParameters["search_parameters"]["search_factor"].GetDouble();                     // The search factor to be considered
-    // const double search_increment_factor = ThisParameters["search_parameters"]["search_increment_factor"].GetDouble(); // The search increment factor to be considered
-    // IndexType bucket_size = ThisParameters["search_parameters"]["bucket_size"].GetInt();                               // Bucket size for kd-tree
+    // Some auxiliary values
+    const IndexType allocation_size = ThisParameters["search_parameters"]["allocation_size"].GetInt();                 // Allocation size for the vectors and max number of potential results
+    const double search_factor = ThisParameters["search_parameters"]["search_factor"].GetDouble();                     // The search factor to be considered
+    const double search_increment_factor = ThisParameters["search_parameters"]["search_increment_factor"].GetDouble(); // The search increment factor to be considered
+    IndexType bucket_size = ThisParameters["search_parameters"]["bucket_size"].GetInt();                               // Bucket size for kd-tree
 
-    // PointVector points_vector;
-    // points_vector.reserve(r_elements_array.size());
-    // for (std::size_t i = 0; i < r_elements_array.size(); ++i) {
-    //     auto it_elem = it_elem_begin + i;
-    //     points_vector.push_back(PointTypePointer(new PointType(*(it_elem.base()))));
-    // }
-    // KDTree tree_points(points_vector.begin(), points_vector.end(), bucket_size);
+    PointVector points_vector;
+    points_vector.reserve(r_elements_array.size());
+    for (std::size_t i = 0; i < r_elements_array.size(); ++i) {
+        auto it_elem = it_elem_begin + i;
+        points_vector.push_back(PointTypePointer(new PointType(*(it_elem.base()))));
+    }
+    KDTree tree_points(points_vector.begin(), points_vector.end(), bucket_size);
 
-    // // Auxiliary values
-    // array_1d<double,3> intersection_point1, intersection_point2;
-    // array_1d<double,4> values_origin, values_destination;
-    // BoundedMatrix<double, 4, 4> N_values, inverted_N_values;
-    // std::array<GeometryType::Pointer, 4> lines;
-    // std::array<Vector, 4> N_line;
-    // double aux_det;
+    // Auxiliary values
+    struct AuxValues {
+        array_1d<double, 3> aux_coordinates;
+        array_1d<double,3> intersection_point1, intersection_point2;
+        array_1d<double,2> values_origin;
+        array_1d<double,4> values_origin_interpolated, values_destination;
+        BoundedMatrix<double, 4, 4> N_values, inverted_N_values;
+        std::array<GeometryType::Pointer, 4> lines;
+        std::array<Vector, 4> N_line;
+        double aux_det;
+    };
 
-    // // Initialize the NODAL_VOLUME
-    // VariableUtils().SetNonHistoricalVariableToZero(NODAL_VOLUME, rModelPart3D.Nodes());
+    // Initialize the NODAL_VOLUME
+    VariableUtils().SetNonHistoricalVariableToZero(NODAL_VOLUME, rModelPart3D.Nodes());
 
-    // // Swap sign
-    // const double swap_sign = ThisParameters["swap_sign"].GetBool();
-    // const double constant = swap_sign ? -1.0 : 1.0;
+    // Swap sign
+    const double swap_sign = ThisParameters["swap_sign"].GetBool();
+    const double constant = swap_sign ? -1.0 : 1.0;
 
-    // // Iterate over the elements
-    // block_for_each(rModelPart3D.Elements(), [&](Element& rElement) {
-    //     double search_radius = search_factor * max_length;
+    // Iterate over the elements
+    block_for_each(rModelPart3D.Elements(), AuxValues(), [&](Element& rElement, AuxValues av) {
+        double search_radius = search_factor * max_length;
 
-    //     // Initialize values
-    //     PointVector points_found(allocation_size);
-    //     IndexType number_points_found = 0;
-    //     const auto& r_geometry_tetra = rElement.GetGeometry();
-    //     const Point center = r_geometry_tetra.Center();
-    //     while (number_points_found == 0) {
-    //         search_radius *= search_increment_factor;
-    //         const PointElement point(center.X(), center.Y(), center.Z());
-    //         number_points_found = tree_points.SearchInRadius(point, search_radius, points_found.begin(), allocation_size);
-    //     }
+        // Initialize values
+        PointVector points_found(allocation_size);
+        IndexType number_points_found = 0;
+        auto& r_geometry_tetra = rElement.GetGeometry();
+        const Point center = r_geometry_tetra.Center();
+        while (number_points_found == 0) {
+            search_radius *= search_increment_factor;
+            const PointElement point(center.X(), center.Y(), center.Z());
+            number_points_found = tree_points.SearchInRadius(point, search_radius, points_found.begin(), allocation_size);
+        }
 
-    //     unsigned int counter = 0;
-    //     for (IndexType i = 0; i < number_points_found; ++i) {
-    //         auto p_point = points_found[i];
-    //         auto p_geometry = p_point->pGetElement()->pGetGeometry();
-    //         auto& r_geometry = *p_geometry;
-    //         const int intersection = IntersectionUtilities::ComputeTetrahedraLineIntersection(r_geometry_tetra, r_geometry[0].Coordinates(), r_geometry[1].Coordinates(), intersection_point1, intersection_point2);
-    //         if (intersection == 1) { // Two intersection points
-    //             // Check position of the nodes in the line
-    //             for (unsigned int i = 0; i < 4; ++i) {
-    //                 lines[i] = p_geometry;
-    //             }
-    //             // TODO
-    //             counter += 4;
-    //         } else if (intersection == 2) { // One intersection point
-    //             // Check position of the node in the line
-    //             // TODO
-    //             counter += 1;
-    //         }
-    //         if (counter == 4) {
-    //             break;
-    //         }
-    //     }
+        unsigned int counter = 0;
+        for (IndexType i = 0; i < number_points_found; ++i) {
+            auto p_point = points_found[i];
+            auto p_geometry = p_point->pGetElement()->pGetGeometry();
+            auto& r_geometry = *p_geometry;
+            const int intersection = IntersectionUtilities::ComputeTetrahedraLineIntersection(r_geometry_tetra, r_geometry[0].Coordinates(), r_geometry[1].Coordinates(), av.intersection_point1, av.intersection_point2);
+            if (intersection == 1) { // Two intersection points
+                // Check position of the nodes in the line
+                for (unsigned int i = 0; i < 4; ++i) {
+                    av.lines[i] = p_geometry;
+                    
+                    //r_geometry.ShapeFunctionsValues( N, av.aux_coordinates );
+                }
+                // TODO
+                counter += 4;
+            } else if (intersection == 2) { // One intersection point
+                // Check position of the node in the line
+                // TODO
+                counter += 1;
+            }
+            if (counter == 4) {
+                break;
+            }
+        }
 
-    //     // Calculate values
-    //     MathUtils<double>::InvertMatrix( N_values, inverted_N_values, aux_det);
-    //     for (std::size_t i_var = 0; i_var < origin_list_variables.size(); ++i_var) {
+        // Calculate values
+        MathUtils<double>::InvertMatrix( av.N_values, av.inverted_N_values, av.aux_det);
+        for (std::size_t i_var = 0; i_var < origin_list_variables.size(); ++i_var) {
+            for (unsigned int i_line = 0; i_line < 4; ++i_line) {
+                auto& r_line = *av.lines[i_line];
+                av.values_origin[0] = r_line[0].FastGetSolutionStepValue(*origin_list_variables[i_var]);
+                av.values_origin[1] = r_line[1].FastGetSolutionStepValue(*origin_list_variables[i_var]);
+                av.values_origin_interpolated[i_line] = MathUtils<double>::Dot(av.N_line[i_line], av.values_origin);
+            }
+            noalias(av.values_destination) = prod(av.inverted_N_values, av.values_origin_interpolated);
+            for (unsigned int i_node = 0; i_node < 4; ++i_node) {
+                r_geometry_tetra[i_node].FastGetSolutionStepValue(*destination_list_variables[i_var]) = constant * av.values_destination[i_node];
+            }
+        }
+    });
 
-    //         noalias(values_destination) = prod(inverted_N_values, values_origin);
-    //         for (unsigned int i_node = 0; i_node < 4; ++i_node) {
-    //             r_geometry_tetra[i_node].FastGetSolutionStepValue(*destination_list_variables[i_var]) = constant * values_destination[i];
-    //         }
-    //     }
-
-    // });
-
-    // // Initialize the NODAL_VOLUME
-    // VariableUtils().EraseNonHistoricalVariable(NODAL_VOLUME, rModelPart3D.Nodes());
+    // Initialize the NODAL_VOLUME
+    VariableUtils().EraseNonHistoricalVariable(NODAL_VOLUME, rModelPart3D.Nodes());
 }
 
 /***********************************************************************************/
