b1131b39856429168bf6ca6be5b4678bb39d0713
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-11-30 09:52:06 +0100

    Name consistency

--- applications/CoSimulationApplication/custom_utilities/data_transfer_3D_1D_utilities.cpp
+++ applications/CoSimulationApplication/custom_utilities/data_transfer_3D_1D_utilities.cpp
@@ -40,6 +40,98 @@ void DataTransfer3D1DUtilities::From3Dto1DDataTransfer(
     GetVariablesList(ThisParameters, origin_list_variables, destination_list_variables);
     
     // Max length of the elements considered
+    auto& r_elements_array = rModelPart3D.Elements();
+    const auto it_elem_begin = r_elements_array.begin();
+    KRATOS_ERROR_IF(r_elements_array.size() == 0) << "Empty 3D model part" << std::endl;
+    double max_length = 0.0;
+    max_length = block_for_each<MaxReduction<double>>(r_elements_array, [&](Element& rElement) {
+        return rElement.GetGeometry().Length();
+    });
+
+    /// Type definitions for the tree
+    using PointType = PointElement;
+    using PointTypePointer = PointType::Pointer;
+    using PointVector = std::vector<PointTypePointer>;
+    using PointIterator = PointVector::iterator;
+    using DistanceVector = std::vector<double>;
+    using DistanceIterator = DistanceVector::iterator;
+
+    /// KDtree definitions
+    using BucketType = Bucket< 3ul, PointType, PointVector, PointTypePointer, PointIterator, DistanceIterator >;
+    using KDTree = Tree< KDTreePartition<BucketType> >;
+
+    // Some auxiliary values
+    const IndexType allocation_size = ThisParameters["search_parameters"]["allocation_size"].GetInt();                 // Allocation size for the vectors and max number of potential results
+    const double search_factor = ThisParameters["search_parameters"]["search_factor"].GetDouble();                     // The search factor to be considered
+    const double search_increment_factor = ThisParameters["search_parameters"]["search_increment_factor"].GetDouble(); // The search increment factor to be considered
+    IndexType bucket_size = ThisParameters["search_parameters"]["bucket_size"].GetInt();                               // Bucket size for kd-tree
+
+    PointVector points_vector;
+    points_vector.reserve(r_elements_array.size());
+    for (std::size_t i = 0; i < r_elements_array.size(); ++i) {
+        auto it_elem = it_elem_begin + i;
+        points_vector.push_back(PointTypePointer(new PointType(*(it_elem.base()))));
+    }
+    KDTree tree_points(points_vector.begin(), points_vector.end(), bucket_size);
+
+    // Swap sign
+    const double swap_sign = ThisParameters["swap_sign"].GetBool();
+    const double constant = swap_sign ? -1.0 : 1.0;
+
+    // Iterate over the nodes
+    block_for_each(rModelPart1D.Nodes(), [&](NodeType& rNode) {
+        double search_radius = search_factor * max_length;
+
+        // Initialize values
+        PointVector points_found(allocation_size);
+        IndexType number_points_found = 0;
+        while (number_points_found == 0) {
+            search_radius *= search_increment_factor;
+            const PointElement point(rNode.X(), rNode.Y(), rNode.Z());
+            number_points_found = tree_points.SearchInRadius(point, search_radius, points_found.begin(), allocation_size);
+        }
+
+        array_1d<double, 3> aux_coordinates;
+        Vector N, values_origin;
+        for (IndexType i = 0; i < number_points_found; ++i) {
+            auto p_point = points_found[i];
+            auto& r_geometry = p_point->pGetElement()->GetGeometry();
+            if (r_geometry.IsInside(rNode.Coordinates(), aux_coordinates)) {
+                r_geometry.ShapeFunctionsValues( N, aux_coordinates );
+                for (std::size_t i_var = 0; i_var < origin_list_variables.size(); ++i_var) {
+                    double& r_destination_value = rNode.FastGetSolutionStepValue(*destination_list_variables[i_var]);
+                    if (values_origin.size() != r_geometry.size()) {
+                        values_origin.resize(r_geometry.size(), false);
+                    }
+                    for (std::size_t i_node = 0; i_node < r_geometry.PointsNumber(); ++i_node) {
+                        auto& r_origin_node = r_geometry[i_node];
+                        values_origin[i_node]= r_origin_node.FastGetSolutionStepValue(*origin_list_variables[i_var]);
+                    }
+                    const double origin_value = inner_prod(N, values_origin);
+                    r_destination_value = constant * origin_value;
+                }
+            }
+        }
+    });
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+void DataTransfer3D1DUtilities::From1Dto3DDataTransfer(
+    ModelPart& rModelPart3D,
+    ModelPart& rModelPart1D,
+    Parameters ThisParameters
+    )
+{
+    // Validate deafult parameters
+    ThisParameters.ValidateAndAssignDefaults(GetDefaultParameters());
+
+    // Getting variables
+    std::vector<const Variable<double>*> origin_list_variables, destination_list_variables;
+    GetVariablesList(ThisParameters, origin_list_variables, destination_list_variables);
+    
+    // Max length of the elements considered
     auto& r_elements_array = rModelPart1D.Elements();
     const auto it_elem_begin = r_elements_array.begin();
     KRATOS_ERROR_IF(r_elements_array.size() == 0) << "Empty 1D model part" << std::endl;
@@ -243,98 +335,6 @@ void DataTransfer3D1DUtilities::From3Dto1DDataTransfer(
 
     // Remove the NODAL_VOLUME
     VariableUtils().EraseNonHistoricalVariable(NODAL_VOLUME, rModelPart3D.Nodes());
-}
-
-/***********************************************************************************/
-/***********************************************************************************/
-
-void DataTransfer3D1DUtilities::From1Dto3DDataTransfer(
-    ModelPart& rModelPart3D,
-    ModelPart& rModelPart1D,
-    Parameters ThisParameters
-    )
-{
-    // Validate deafult parameters
-    ThisParameters.ValidateAndAssignDefaults(GetDefaultParameters());
-
-    // Getting variables
-    std::vector<const Variable<double>*> origin_list_variables, destination_list_variables;
-    GetVariablesList(ThisParameters, origin_list_variables, destination_list_variables);
-    
-    // Max length of the elements considered
-    auto& r_elements_array = rModelPart3D.Elements();
-    const auto it_elem_begin = r_elements_array.begin();
-    KRATOS_ERROR_IF(r_elements_array.size() == 0) << "Empty 3D model part" << std::endl;
-    double max_length = 0.0;
-    max_length = block_for_each<MaxReduction<double>>(r_elements_array, [&](Element& rElement) {
-        return rElement.GetGeometry().Length();
-    });
-
-    /// Type definitions for the tree
-    using PointType = PointElement;
-    using PointTypePointer = PointType::Pointer;
-    using PointVector = std::vector<PointTypePointer>;
-    using PointIterator = PointVector::iterator;
-    using DistanceVector = std::vector<double>;
-    using DistanceIterator = DistanceVector::iterator;
-
-    /// KDtree definitions
-    using BucketType = Bucket< 3ul, PointType, PointVector, PointTypePointer, PointIterator, DistanceIterator >;
-    using KDTree = Tree< KDTreePartition<BucketType> >;
-
-    // Some auxiliary values
-    const IndexType allocation_size = ThisParameters["search_parameters"]["allocation_size"].GetInt();                 // Allocation size for the vectors and max number of potential results
-    const double search_factor = ThisParameters["search_parameters"]["search_factor"].GetDouble();                     // The search factor to be considered
-    const double search_increment_factor = ThisParameters["search_parameters"]["search_increment_factor"].GetDouble(); // The search increment factor to be considered
-    IndexType bucket_size = ThisParameters["search_parameters"]["bucket_size"].GetInt();                               // Bucket size for kd-tree
-
-    PointVector points_vector;
-    points_vector.reserve(r_elements_array.size());
-    for (std::size_t i = 0; i < r_elements_array.size(); ++i) {
-        auto it_elem = it_elem_begin + i;
-        points_vector.push_back(PointTypePointer(new PointType(*(it_elem.base()))));
-    }
-    KDTree tree_points(points_vector.begin(), points_vector.end(), bucket_size);
-
-    // Swap sign
-    const double swap_sign = ThisParameters["swap_sign"].GetBool();
-    const double constant = swap_sign ? -1.0 : 1.0;
-
-    // Iterate over the nodes
-    block_for_each(rModelPart1D.Nodes(), [&](NodeType& rNode) {
-        double search_radius = search_factor * max_length;
-
-        // Initialize values
-        PointVector points_found(allocation_size);
-        IndexType number_points_found = 0;
-        while (number_points_found == 0) {
-            search_radius *= search_increment_factor;
-            const PointElement point(rNode.X(), rNode.Y(), rNode.Z());
-            number_points_found = tree_points.SearchInRadius(point, search_radius, points_found.begin(), allocation_size);
-        }
-
-        array_1d<double, 3> aux_coordinates;
-        Vector N, values_origin;
-        for (IndexType i = 0; i < number_points_found; ++i) {
-            auto p_point = points_found[i];
-            auto& r_geometry = p_point->pGetElement()->GetGeometry();
-            if (r_geometry.IsInside(rNode.Coordinates(), aux_coordinates)) {
-                r_geometry.ShapeFunctionsValues( N, aux_coordinates );
-                for (std::size_t i_var = 0; i_var < origin_list_variables.size(); ++i_var) {
-                    double& r_destination_value = rNode.FastGetSolutionStepValue(*destination_list_variables[i_var]);
-                    if (values_origin.size() != r_geometry.size()) {
-                        values_origin.resize(r_geometry.size(), false);
-                    }
-                    for (std::size_t i_node = 0; i_node < r_geometry.PointsNumber(); ++i_node) {
-                        auto& r_origin_node = r_geometry[i_node];
-                        values_origin[i_node]= r_origin_node.FastGetSolutionStepValue(*origin_list_variables[i_var]);
-                    }
-                    const double origin_value = inner_prod(N, values_origin);
-                    r_destination_value = constant * origin_value;
-                }
-            }
-        }
-    });
 }
 
 /***********************************************************************************/
