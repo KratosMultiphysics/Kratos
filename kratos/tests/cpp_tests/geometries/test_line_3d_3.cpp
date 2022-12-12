3cb23fc3c3c6a2354532674fa4d5797849a3e503
Vicente Mataix Ferándiz <vmataix@altair.com>
2022-12-12 10:22:28 +0000

    Comment unused

--- kratos/tests/cpp_tests/geometries/test_line_3d_3.cpp
+++ kratos/tests/cpp_tests/geometries/test_line_3d_3.cpp
@@ -38,16 +38,16 @@ namespace Kratos::Testing {
             );
         }
 
-        /** Generates a point type sample Line3D3N
-        * @return  Pointer to a Line3D3N
-        */
-        Line3D3<Point>::Pointer GeneratePointsUnitYDirectionLine3D3() {
-            return Kratos::make_shared<Line3D3<Point>>(
-            Kratos::make_shared<Point>(0.0, 0.0, 0.0),
-            Kratos::make_shared<Point>(0.0, 1.0, 0.0),
-            Kratos::make_shared<Point>(0.0, 0.5, 0.0)
-            );
-        }
+        // /** Generates a point type sample Line3D3N
+        // * @return  Pointer to a Line3D3N
+        // */
+        // Line3D3<Point>::Pointer GeneratePointsUnitYDirectionLine3D3() {
+        //     return Kratos::make_shared<Line3D3<Point>>(
+        //     Kratos::make_shared<Point>(0.0, 0.0, 0.0),
+        //     Kratos::make_shared<Point>(0.0, 1.0, 0.0),
+        //     Kratos::make_shared<Point>(0.0, 0.5, 0.0)
+        //     );
+        // }
 
         /** Generates a point type sample Line3D3N
         * @return  Pointer to a Line3D3N
@@ -71,12 +71,12 @@ namespace Kratos::Testing {
             );
         }
 
-        /** Generates a point type sample Line3D3N.
-        * @return  Pointer to a Line3D3N
-        */
-        Line3D3<Point>::Pointer GenerateLine3D3WithPoints(Point::Pointer pPointOne, Point::Pointer pPointTwo, Point::Pointer pPointThree ) {
-            return Kratos::make_shared<Line3D3<Point>>(pPointOne, pPointTwo, pPointThree);
-        }
+        // /** Generates a point type sample Line3D3N.
+        // * @return  Pointer to a Line3D3N
+        // */
+        // Line3D3<Point>::Pointer GenerateLine3D3WithPoints(Point::Pointer pPointOne, Point::Pointer pPointTwo, Point::Pointer pPointThree ) {
+        //     return Kratos::make_shared<Line3D3<Point>>(pPointOne, pPointTwo, pPointThree);
+        // }
     }
 
     /** Checks if the number of edges is correct.
