229fff32a3f2a31158a22ec442ccb87bb92f0190
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-12-14 12:59:06 +0100

    Remove deprecated
    Co-authored-by: Philipp Bucher <philipp.bucher@tum.de>

--- applications/MappingApplication/custom_mappers/projection_3D_2D_mapper.h
+++ applications/MappingApplication/custom_mappers/projection_3D_2D_mapper.h
@@ -449,11 +449,6 @@ public:
     ///@name Inquiry
     ///@{
 
-    int AreMeshesConforming() const override
-    {
-        KRATOS_WARNING_ONCE("Mapper") << "Developer-warning: \"AreMeshesConforming\" is deprecated and will be removed in the future" << std::endl;
-        return BaseType::mMeshesAreConforming;
-    }
 
     ///@}
     ///@name Input and output
