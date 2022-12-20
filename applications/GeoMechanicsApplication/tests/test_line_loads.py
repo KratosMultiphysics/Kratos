d3e625620080af2629eb734aabd153702bb22cad
Vahid <39699625+Vahid-Galavi@users.noreply.github.com>
2022-12-20 15:54:00 +0100

    Update test_line_loads.py
    commented lines are removed

--- applications/GeoMechanicsApplication/tests/test_line_loads.py
+++ applications/GeoMechanicsApplication/tests/test_line_loads.py
@@ -1,8 +1,4 @@
-# import sys
 import os
-
-# sys.path.append(os.path.join('..', '..', '..'))
-# sys.path.append(os.path.join('..', 'python_scripts'))
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import test_helper
