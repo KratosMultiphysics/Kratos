5a8c8cd9bb59f9285191ddfd6898c1c61a4ac731
Vicente Mataix Ferrandiz <vmataix@altair.com>
2022-12-22 17:11:37 +0100

    Extend integration utilities

--- kratos/utilities/integration_utilities.h
+++ kratos/utilities/integration_utilities.h
@@ -8,6 +8,7 @@
 //                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Riccardo Rossi
+//                   Vicente Mataix Ferrandiz
 //
 
 #pragma once
@@ -21,11 +22,24 @@
 
 namespace Kratos
 {
+
+/**
+ * @class IntegrationUtilities
+ * @ingroup KratosCore
+ * @brief Utilities to integrate in different cases
+ * @author Vicente Mataix Ferrandiz
+ */
 class IntegrationUtilities
 {
 public:
+    /** 
+     * @brief This method returns the integration order for the exact mass matrix evaluation
+     * @param rGeometry The geometry considered
+     * @tparam TPointType The Point type
+     * @return The integration order
+     */
     template<class TPointType>
-    static GeometryData::IntegrationMethod GetIntegrationMethodForExactMassMatrixEvaluation(Geometry<TPointType> const& rGeometry)
+    static GeometryData::IntegrationMethod GetIntegrationMethodForExactMassMatrixEvaluation(const Geometry<TPointType>& rGeometry)
     {
         GeometryData::IntegrationMethod integration_method = rGeometry.GetDefaultIntegrationMethod();
         if (integration_method == GeometryData::IntegrationMethod::GI_GAUSS_1)
@@ -38,6 +52,72 @@ public:
             integration_method = GeometryData::IntegrationMethod::GI_GAUSS_5;
         return integration_method;
     }
+
+    /** 
+     * @brief This method calculates and returns the domain size of the geometry from any geometry in a generic manner
+     * @param rGeometry The geometry considered
+     * @param rIntegrationPoints The integration points considered
+     * @tparam TPointType The Point type
+     * @return double value contains volume.
+     */
+    template<class TPointType>
+    static inline double ComputeDomainSize(
+        const Geometry<TPointType>& rGeometry,
+        const typename Geometry<TPointType>::IntegrationPointsArrayType& rIntegrationPoints
+        )
+    {
+        const auto number_gp = rIntegrationPoints.size();
+        Vector temp(number_gp);
+        temp = rGeometry.DeterminantOfJacobian(temp);
+        double domain_size = 0.0;
+        for (unsigned int i = 0; i < number_gp; ++i) {
+            domain_size += temp[i] * rIntegrationPoints[i].Weight();
+        }
+        return domain_size;
+    }
+
+    /** 
+     * @brief This method calculates and returns the volume of the geometry from a 3D geometry
+     * @param rGeometry The geometry considered
+     * @tparam TPointType The Point type
+     * @return double value contains volume.
+     */
+    template<class TPointType>
+    static inline double ComputeArea2DGeometry(const Geometry<TPointType>& rGeometry)
+    {
+        const auto integration_method = rGeometry.GetDefaultIntegrationMethod();
+        const auto& r_integration_points = rGeometry.IntegrationPoints( integration_method );
+        double volume = 0.0;
+        Matrix J(2, 2);
+        for ( unsigned int i = 0; i < r_integration_points.size(); i++ ) {
+            rGeometry.Jacobian( J, i, integration_method);
+            volume += (J(0,0)*J(1,1)-J(0,1)*J(1,0)) * r_integration_points[i].Weight();
+        }
+
+        return volume;
+    }
+
+    /** 
+     * @brief This method calculates and returns the volume of the geometry from a 3D geometry
+     * @param rGeometry The geometry considered
+     * @tparam TPointType The Point type
+     * @return double value contains volume.
+     */
+    template<class TPointType>
+    static inline double ComputeVolume3DGeometry(const Geometry<TPointType>& rGeometry)
+    {
+        const auto integration_method = rGeometry.GetDefaultIntegrationMethod();
+        const auto& r_integration_points = rGeometry.IntegrationPoints( integration_method );
+        double volume = 0.0;
+        Matrix J(3, 3);
+        for ( unsigned int i = 0; i < r_integration_points.size(); i++ ) {
+            rGeometry.Jacobian( J, i, integration_method);
+            volume += J(0,0)*(J(1,1)*J(2,2) - J(1,2)*J(2,1)) - J(0,1)*(J(1,0)*J(2,2) - J(1,2)*J(2,0)) + J(0,2)*(J(1,0)*J(2,1) - J(1,1)*J(2,0)) * r_integration_points[i].Weight();
+        }
+
+        return volume;
+    }
+
 };
 
 }  // namespace Kratos.
