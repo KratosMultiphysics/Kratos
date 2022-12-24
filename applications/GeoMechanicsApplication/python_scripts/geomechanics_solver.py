9cc7bfb6934145ba1f1e584231f7c1409117db30
Mohamed Nabi <mohamed.nabi@deltares.nl>
2022-12-24 21:05:02 +0100

    Task 3: Defind solver
    NB. The solver is added as py file(s)

--- applications/GeoMechanicsApplication/python_scripts/geomechanics_solver.py
+++ applications/GeoMechanicsApplication/python_scripts/geomechanics_solver.py
@@ -70,6 +70,9 @@ class GeoMechanicalSolver(PythonSolver):
         ## Fluid Variables
         self._add_water_variables()
 
+        # Add temperature variables
+        self._add_temperature_variables()
+        
         ## smoothing variables
         self._add_smoothing_variables()
 
@@ -204,6 +207,13 @@ class GeoMechanicalSolver(PythonSolver):
         # Add variables for the water conditions
         self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.HYDRAULIC_DISCHARGE)
 
+    def _add_temperature_variables(self):
+        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.TEMPERATURE)
+        # Add dynamic variables
+        self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.DT_TEMPERATURE)
+        # Add variables for the heat conditions
+        self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.NORMAL_HEAT_FLUX)
+        
     def _add_smoothing_variables(self):
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
         self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.NODAL_CAUCHY_STRESS_TENSOR)
