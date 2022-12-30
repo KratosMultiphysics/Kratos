ec9c10f22ffb5ee196520a4aae141dc53f0ab7b2
Vicente Mataix Ferrándiz <vmataix@altair.com>
2022-12-30 10:22:58 +0100

    Codacy suggestion

--- kratos/tests/test_model.py
+++ kratos/tests/test_model.py
@@ -69,7 +69,7 @@ class TestModel(KratosUnittest.TestCase):
 
     def test_model_datavaluecontainer(self):
         current_model = KratosMultiphysics.Model()
-        current_model.SetValue(KratosMultiphysics.DENSITY, 1.2)        
+        current_model.SetValue(KratosMultiphysics.DENSITY, 1.2)
         self.assertTrue(current_model.Has(KratosMultiphysics.DENSITY))
         self.assertFalse(current_model.Has(KratosMultiphysics.TEMPERATURE))
         self.assertEqual(current_model.GetValue(KratosMultiphysics.DENSITY), 1.2)
