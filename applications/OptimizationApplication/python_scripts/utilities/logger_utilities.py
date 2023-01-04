0f4afe445a0db81d73fb1bd55a799026be63abf6
sunethwarna <suneth.warna@gmail.com>
2023-01-04 02:20:25 +0100

    update logger utilities

--- applications/OptimizationApplication/python_scripts/utilities/logger_utilities.py
+++ applications/OptimizationApplication/python_scripts/utilities/logger_utilities.py
@@ -8,6 +8,7 @@
 #
 # ==============================================================================
 
+import time as timer
 import KratosMultiphysics as Kratos
 
 def AddFileLoggerOutput(logger_file_name):
@@ -33,3 +34,17 @@ class FileLogger:
 
     def __exit__(self, exit_type, exit_value, exit_traceback):
         RemoveFileLoggerOutput(self.__default_severity, self.__logger_file)
+
+class TimeLogger:
+    """@brief A context responsible for outputing execution times."""
+    def __init__(self, topic: str, entry_msg: str, exit_msg: str):
+        self.topic = topic
+        self.entry_msg = entry_msg
+        self.exit_msg = exit_msg
+
+    def __enter__(self):
+        Kratos.Logger.PrintInfo(self.topic, self.entry_msg)
+        self.start_time = timer.time()
+
+    def __exit__(self, exit_type, exit_value, exit_traceback):
+        Kratos.Logger.PrintInfo(self.topic, "{:s} - [ Elapsed time: {:d} s.]".format(self.exit_msg, round(timer.time() - self.start_time)))
