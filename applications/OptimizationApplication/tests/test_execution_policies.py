3892e5eb7c2345838fc0d86e5cf1fdf6b0b378ce
sunethwarna <suneth.warna@gmail.com>
2023-01-04 02:23:21 +0100

    add tests to execution policies

--- applications/OptimizationApplication/tests/test_execution_policies.py
+++ applications/OptimizationApplication/tests/test_execution_policies.py
@@ -4,6 +4,7 @@ import KratosMultiphysics as Kratos
 # Import KratosUnittest
 import KratosMultiphysics.KratosUnittest as kratos_unittest
 
+from KratosMultiphysics.OptimizationApplication.utilities.execution_policies.execution_policy_wrapper import ExecutionPolicyWrapper
 from KratosMultiphysics.OptimizationApplication.utilities.execution_policies.independent_analysis_execution_policy import IndependentAnalysisExecutionPolicy
 from KratosMultiphysics.OptimizationApplication.utilities.execution_policies.stepping_analysis_execution_policy import SteppingAnalysisExecutionPolicy
 
@@ -14,44 +15,20 @@ class TestExecutionPolicies(kratos_unittest.TestCase):
 
     def test_IndependentAnalysisExecutionPolicy(self):
         parameters = Kratos.Parameters("""{
-            "log_in_file": false,
-            "settings"       : {
-                "analysis_name": "KratosMultiphysics.multistage_analysis.MultistageAnalysis",
+            "name"                     : "test",
+            "execution_policy_type"    : "KratosMultiphysics.OptimizationApplication.utilities.execution_policies.independent_analysis_execution_policy.IndependentAnalysisExecutionPolicy",
+            "execution_policy_settings": {
+                "analysis_type"      : "KratosMultiphysics.multistage_analysis.MultistageAnalysis",
                 "analysis_parameters": {
-                    "stages": []
+                    "stages": [],
+                    "execution_list":[]
                 }
             }
         }""")
-        execution_policy = IndependentAnalysisExecutionPolicy(self.model, parameters)
+        execution_policy = ExecutionPolicyWrapper(self.model, parameters)
         execution_policy.Initialize({})
-        execution_policy.Run({})
-
-    # @kratos_unittest.skipIfApplicationsNotAvailable("StructuralMechanicsApplication")
-    # def test_SteppingAnalysisExecutionPolicy(self):
-    #     parameters = Kratos.Parameters("""{
-    #         "log_in_file": false,
-    #         "settings"       : {
-    #             "analysis_name": "KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_analysis.StructuralMechanicsAnalysis",
-    #             "analysis_parameters": {
-    #                 "problem_data": {
-    #                     "echo_level": 0,
-    #                     "parallel_type": "OpenMP"
-    #                 },
-    #                 "solver_settings": {
-    #                     "model_part_name": "Structure",
-    #                     "solver_type": "Dynamic",
-    #                     "domain_size": 3,
-    #                     "time_stepping":{
-
-    #                     }
-    #                 }
-    #             }
-    #         }
-    #     }""")
-    #     execution_policy = SteppingAnalysisExecutionPolicy(self.model, parameters)
-    #     execution_policy.Initialize({})
-    #     execution_policy.Run({})
+        execution_policy.Execute({})
 
 if __name__ == "__main__":
-    Kratos.Logger.GetDefaultOutput().SetSeverity(Kratos.Logger.Severity.WARNING)
+    Kratos.Tester.SetVerbosity(Kratos.Tester.Verbosity.PROGRESS)  # TESTS_OUTPUTS
     kratos_unittest.main()
