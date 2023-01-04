a42f379349d01b13833514b365e3036c758f8997
sunethwarna <suneth.warna@gmail.com>
2023-01-04 15:14:08 +0100

    unique names

--- applications/OptimizationApplication/python_scripts/analyses_controller.py
+++ applications/OptimizationApplication/python_scripts/analyses_controller.py
@@ -28,7 +28,10 @@ class AnalysesController:
         self.execution_policy_wrappers = {}
         for execution_policy_settings in self.execution_policies_settings:
             execution_policy_wrapper = ExecutionPolicyWrapper(self.model, execution_policy_settings)
-            self.execution_policy_wrappers[execution_policy_wrapper.GetExecutionPolicyName()] = execution_policy_wrapper
+            if not execution_policy_wrapper.GetExecutionPolicyName() in self.execution_policy_wrappers.keys():
+                self.execution_policy_wrappers[execution_policy_wrapper.GetExecutionPolicyName()] = execution_policy_wrapper
+            else:
+                raise RuntimeError(f"Found already existing exeuction policy with the name \"{execution_policy_wrapper.GetExecutionPolicyName()}\". Please provide unique names.")
 
     # --------------------------------------------------------------------------
     def GetExecutionPolicyWrapper(self, execution_policy_name: str):
