d14e26a94cca3a70b1cd6af3aa2698d5eb73fef3
sunethwarna <suneth.warna@gmail.com>
2023-01-04 14:21:54 +0100

    add error handling

--- applications/OptimizationApplication/python_scripts/execution_policies/execution_policy_wrapper.py
+++ applications/OptimizationApplication/python_scripts/execution_policies/execution_policy_wrapper.py
@@ -9,13 +9,14 @@
 # ==============================================================================
 
 from importlib import import_module
+from pathlib import Path
 
 import KratosMultiphysics as Kratos
 from KratosMultiphysics.model_parameters_factory import KratosModelParametersFactory
 from KratosMultiphysics.OptimizationApplication.execution_policies.execution_policy import ExecutionPolicy
 from KratosMultiphysics.OptimizationApplication.utilities.logger_utilities import FileLogger
 
-def RetrieveObject(model: Kratos.Model, parameters: Kratos.Parameters):
+def RetrieveObject(model: Kratos.Model, parameters: Kratos.Parameters, object_type = object):
     default_settings = Kratos.Parameters("""{
         "module"  : "",
         "type"    : "",
@@ -26,13 +27,44 @@ def RetrieveObject(model: Kratos.Model, parameters: Kratos.Parameters):
     class_name = parameters["type"].GetString()
     python_file_name = ''.join(['_' + c.lower() if c.isupper() else c for c in class_name]).lstrip('_')
 
-    module_name = parameters["module"].GetString()
+    module_prefix = parameters["module"].GetString()
+    module_name = module_prefix
     if module_name != "":
         module_name += "."
     module_name += python_file_name
 
-    module = import_module(module_name)
-    return getattr(module, class_name)(model, parameters["settings"])
+    try:
+        module = import_module(module_name)
+        retrieved_object = getattr(module, class_name)(model, parameters["settings"])
+        if not isinstance(retrieved_object, object_type):
+            raise TypeError(f"The retrieved object is of type \"{retrieved_object.__class__.__name__}\" which is not of the required type \"{object_type.__name__}\".")
+        else:
+            return retrieved_object
+    except AttributeError:
+        raise AttributeError(f"The python file \"{str(Path(module.__file__))}\" does not have a class named \"{class_name}\".")
+    except (ImportError, ModuleNotFoundError):
+        if module_prefix != "":
+            try:
+                module = import_module(module_prefix)
+            except (ImportError, ModuleNotFoundError):
+                raise RuntimeError(f"The module \"{module_prefix}\" cannot be imported.")
+            if hasattr(module, "__path__"):
+                list_of_available_options = []
+                for item in Path(module.__path__[0]).iterdir():
+                    if item.is_file() and item.name.endswith(".py"):
+                        try:
+                            current_module = import_module(module_prefix + "." + item.name[:-3])
+                            current_class_name = "".join([c.title() for c in item.name[:-3].split("_")])
+                            if hasattr(current_module, current_class_name):
+                                if issubclass(getattr(current_module, current_class_name), object_type):
+                                    list_of_available_options.append(current_class_name)
+                        except:
+                            pass
+                raise RuntimeError(f"Given type with name \"{class_name}\" is not found. Followings are available options:\n\t" + "\n\t".join(list_of_available_options))
+            else:
+                raise RuntimeError(f"Invalid module found under \"{module_prefix}\".")
+        else:
+            raise RuntimeError("Invalid \"module\" and/or \"type\" fields.")
 
 class ExecutionPolicyWrapper:
     def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters):
@@ -68,9 +100,7 @@ class ExecutionPolicyWrapper:
 
         # create execution policy
         parameters["execution_policy_settings"].ValidateAndAssignDefaults(default_parameters["execution_policy_settings"])
-        self.__execution_policy = RetrieveObject(model, parameters["execution_policy_settings"])
-        if not isinstance(self.__execution_policy, ExecutionPolicy):
-            raise RuntimeError(f"{self.__execution_policy.__class__.__name__} is not derrived from ExecutionPolicy.")
+        self.__execution_policy = RetrieveObject(model, parameters["execution_policy_settings"], ExecutionPolicy)
 
     def Initialize(self, optimization_info: dict):
         self.__execution_policy.Initialize(optimization_info)
