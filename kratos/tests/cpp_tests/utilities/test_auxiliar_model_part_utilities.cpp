7c52d10d4e552431b7ba6b7e06457f6ac4357145
Vicente Mataix Ferándiz <vmataix@altair.com>
2023-01-10 11:46:54 +0000

    Fix test

--- kratos/tests/cpp_tests/utilities/test_auxiliar_model_part_utilities.cpp
+++ kratos/tests/cpp_tests/utilities/test_auxiliar_model_part_utilities.cpp
@@ -254,12 +254,13 @@ KRATOS_TEST_CASE_IN_SUITE(AuxiliarModelPartUtilities_RemoveNodesFromSubModeParts
 
     KRATOS_CHECK_EQUAL(r_sub.NumberOfNodes(), 4);
     KRATOS_CHECK_EQUAL(r_sub.NumberOfElements(), 1);
-    KRATOS_CHECK_EQUAL(r_sub.NumberOfGeometries(), 1);
+    KRATOS_CHECK_EQUAL(r_sub.NumberOfGeometries(), 0);
 
     utilities.RemoveOrphanNodesFromSubModelParts();
 
     KRATOS_CHECK_EQUAL(r_sub.NumberOfNodes(), 3);
     KRATOS_CHECK_EQUAL(r_sub.NumberOfElements(), 1);
+    KRATOS_CHECK_EQUAL(r_sub.NumberOfGeometries(), 0);
 
     // Replace element by geometry
     r_sub.AddNode(r_model_part.pGetNode(4));
@@ -269,10 +270,11 @@ KRATOS_TEST_CASE_IN_SUITE(AuxiliarModelPartUtilities_RemoveNodesFromSubModeParts
     KRATOS_CHECK_EQUAL(r_sub.NumberOfNodes(), 4);
     KRATOS_CHECK_EQUAL(r_sub.NumberOfElements(), 0);
     KRATOS_CHECK_EQUAL(r_sub.NumberOfGeometries(), 1); 
-    
+
     utilities.RemoveOrphanNodesFromSubModelParts();
 
     KRATOS_CHECK_EQUAL(r_sub.NumberOfNodes(), 3);
+    KRATOS_CHECK_EQUAL(r_sub.NumberOfElements(), 0);
     KRATOS_CHECK_EQUAL(r_sub.NumberOfGeometries(), 1);
 }
 
