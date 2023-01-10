02da3ee0a4d6723b37cd3e526288a22d4c6c099b
Daniel Diez <ddiez@altair.com>
2023-01-10 13:53:23 +0100

    using model parameters factory

--- kratos/python_scripts/output_process_with_controller.py
+++ kratos/python_scripts/output_process_with_controller.py
@@ -1,6 +1,8 @@
 # Importing the Kratos Library
 import KratosMultiphysics
 
+from KratosMultiphysics.model_parameters_factory import KratosModelParametersFactory
+
 
 def Factory(settings, Model):
     if not isinstance(settings, KratosMultiphysics.Parameters):
@@ -19,13 +21,9 @@ class OutputProcessWithController(KratosMultiphysics.OutputProcess):
         self.params = params
         self.model = model
         self.params.ValidateAndAssignDefaults(default_settings)
-        controller_name = self.params["controller_settings"]["name"].GetString()
-        controller_prototype = KratosMultiphysics.Registry[f"{controller_name}.Prototype"]
-        output_process_name = self.params["output_settings"]["name"].GetString()
-        output_settings_prototype = KratosMultiphysics.Registry[f"{output_process_name}.Prototype"]
-
-        self.controller = controller_prototype.Create(self.model, self.params["controller_settings"]["parameters"])
-        self.print_process = output_settings_prototype.Create(self.model, self.params["output_settings"]["parameters"])
+        factory = KratosModelParametersFactory(self.model)
+        self.controller = factory.ConstructItem(self.params["controller_settings"])
+        self.print_process = factory.ConstructItem(self.params["output_settings"])
 
     @classmethod
     def GetDefaultParameters(self):
