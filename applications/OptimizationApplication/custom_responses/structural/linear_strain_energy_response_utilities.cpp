bbbd0f704d13585566a13c1af71e5baa337830b5
sunethwarna <suneth.warna@gmail.com>
2023-01-13 10:07:41 +0100

    parallelize shape sensitivity calculation

--- applications/OptimizationApplication/custom_responses/structural/linear_strain_energy_response_utilities.cpp
+++ applications/OptimizationApplication/custom_responses/structural/linear_strain_energy_response_utilities.cpp
@@ -14,12 +14,15 @@
 
 // System includes
 #include <tuple>
+#include <sstream>
 
 // Project includes
 #include "includes/model_part.h"
 #include "utilities/parallel_utilities.h"
 #include "utilities/reduction_utilities.h"
 #include "utilities/variable_utils.h"
+#include "utilities/atomic_utilities.h"
+#include "utilities/openmp_utils.h"
 
 // Application includes
 
@@ -65,60 +68,111 @@ void LinearStrainEnergyResponseUtilities::CalculateStrainEnergyShapeSensitivity(
 
     VariableUtils().SetNonHistoricalVariableToZero(rOutputSensitivityVariable, rModelPart.Nodes());
 
-    using tls_type = std::tuple<Vector, Vector, Vector>;
+    using tls_type = std::tuple<Vector, Vector, Vector, Element::Pointer>;
 
     const auto& r_process_info = rModelPart.GetProcessInfo();
+
+    const int max_id = block_for_each<MaxReduction<int>>(rModelPart.GetRootModelPart().Nodes(), [](const auto& rNode) {
+        return rNode.Id();
+    });
+
+    std::vector<std::string> model_part_names;
 
     block_for_each(rModelPart.Elements(), tls_type(), [&](auto& rElement, tls_type& rTLS) {
         if (!rElement.IsDefined(ACTIVE) || (rElement.Is(ACTIVE))) {
             Vector& r_u = std::get<0>(rTLS);
             Vector& r_ref_rhs = std::get<1>(rTLS);
             Vector& r_perturbed_rhs = std::get<2>(rTLS);
+            Element::Pointer& p_element = std::get<3>(rTLS);
+
+            auto& r_geometry = rElement.GetGeometry();
+            const auto domain_size = r_geometry.WorkingSpaceDimension();
 
             rElement.GetValuesVector(r_u);
 
             // calculate the reference value
             rElement.CalculateRightHandSide(r_ref_rhs, r_process_info);
 
-            auto& r_geometry = rElement.GetGeometry();
-            const auto domain_size = r_geometry.WorkingSpaceDimension();
+            if (!p_element) {
+
+                std::stringstream name;
+                const int thread_id = OpenMPUtils::ThisThread();
+                name << rModelPart.Name() << "_temp_" << thread_id;
+                GeometryType::PointsArrayType nodes;
+                #pragma omp critical
+                {
+                    model_part_names.push_back(name.str());
+                    auto& tls_model_part = rModelPart.CreateSubModelPart(name.str());
+
+                    for (IndexType i = 0; i < r_geometry.size(); ++i) {
+                        nodes.push_back(tls_model_part.CreateNewNode((thread_id + 1) * max_id + i + 1, r_geometry[i]));
+                    }
+                }
+
+                p_element = rElement.Create(1, nodes, rElement.pGetProperties());
+                p_element->Initialize(r_process_info);
+                p_element->InitializeSolutionStep(r_process_info);
+            }
+
+            *p_element = static_cast<const Element&>(rElement);
+
+            // TODO: For some reason, assignment operator of the element
+            //       assigns current position to initial position of the lhs element
+            //       hence these are corrected manually. But need to be checked
+            //       in the element/node/geometry assignment operators.
+            p_element->GetGeometry().SetData(r_geometry.GetData());
+            p_element->SetFlags(rElement.GetFlags());
+            for (IndexType i = 0; i < r_geometry.size(); ++i) {
+                auto& r_node = p_element->GetGeometry()[i];
+                const auto& r_orig_node = r_geometry[i];
+                r_node = r_orig_node;
+            }
 
             // now calculate perturbed
-            for (auto& r_node : r_geometry) {
-                r_node.SetLock();
+            for (IndexType i = 0; i < r_geometry.size(); ++i) {
+                auto& r_orig_node_sensitivity = r_geometry[i].GetValue(rOutputSensitivityVariable);
 
-                auto& r_output_value = r_node.GetValue(rOutputSensitivityVariable);
+                auto& r_node = p_element->GetGeometry()[i];
                 auto& r_coordinates = r_node.Coordinates();
                 auto& r_initial_coordintes = r_node.GetInitialPosition();
 
                 r_initial_coordintes[0] += Delta;
                 r_coordinates[0] += Delta;
-                rElement.CalculateRightHandSide(r_perturbed_rhs, r_process_info);
+                p_element->CalculateRightHandSide(r_perturbed_rhs, r_process_info);
                 r_initial_coordintes[0] -= Delta;
                 r_coordinates[0] -= Delta;
-                r_output_value[0] += 0.5 * inner_prod(r_u, r_perturbed_rhs - r_ref_rhs) / Delta;
+                AtomicAdd<double>(r_orig_node_sensitivity[0], 0.5 * inner_prod(r_u, r_perturbed_rhs - r_ref_rhs) / Delta);
 
                 r_initial_coordintes[1] += Delta;
                 r_coordinates[1] += Delta;
-                rElement.CalculateRightHandSide(r_perturbed_rhs, r_process_info);
+                p_element->CalculateRightHandSide(r_perturbed_rhs, r_process_info);
                 r_initial_coordintes[1] -= Delta;
                 r_coordinates[1] -= Delta;
-                r_output_value[1] += 0.5 * inner_prod(r_u, r_perturbed_rhs - r_ref_rhs) / Delta;
+                AtomicAdd<double>(r_orig_node_sensitivity[1], 0.5 * inner_prod(r_u, r_perturbed_rhs - r_ref_rhs) / Delta);
 
                 if (domain_size == 3) {
                     r_initial_coordintes[2] += Delta;
                     r_coordinates[2] += Delta;
-                    rElement.CalculateRightHandSide(r_perturbed_rhs, r_process_info);
+                    p_element->CalculateRightHandSide(r_perturbed_rhs, r_process_info);
                     r_initial_coordintes[2] -= Delta;
                     r_coordinates[2] -= Delta;
-                    r_output_value[2] += 0.5 * inner_prod(r_u, r_perturbed_rhs - r_ref_rhs) / Delta;
+                    AtomicAdd<double>(r_orig_node_sensitivity[2], 0.5 * inner_prod(r_u, r_perturbed_rhs - r_ref_rhs) / Delta);
                 }
-
-                r_node.UnSetLock();
             }
         }
     });
 
+    // now clear the temp model part
+    VariableUtils().SetFlag(TO_ERASE, false, rModelPart.Nodes());
+    for (const auto& r_model_part_name : model_part_names) {
+        auto& tls_model_part = rModelPart.GetSubModelPart(r_model_part_name);
+        for (auto& r_node : tls_model_part.Nodes()) {
+            r_node.Set(TO_ERASE, true);
+        }
+    }
+    rModelPart.RemoveNodesFromAllLevels(TO_ERASE);
+
+    // Assemble nodal result
     rModelPart.GetCommunicator().AssembleNonHistoricalData(rOutputSensitivityVariable);
 
     KRATOS_CATCH("");
