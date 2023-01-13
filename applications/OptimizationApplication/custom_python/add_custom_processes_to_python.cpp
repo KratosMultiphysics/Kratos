fbf018d086651889c7656b3f78957c0261268585
sunethwarna <suneth.warna@gmail.com>
2023-01-13 06:56:58 +0100

    add custom process to python

---
+++ applications/OptimizationApplication/custom_python/add_custom_processes_to_python.cpp
@@ -0,0 +1,38 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 license: HDF5Application/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+// System includes
+
+// // External includes
+#include <pybind11/stl.h>
+
+// Project includes
+
+// Application includes
+#include "custom_processes/entity_specific_properties_process.h"
+
+// Include base h
+#include "add_custom_processes_to_python.h"
+
+namespace Kratos {
+namespace Python {
+
+void AddCustomProcessesToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+    py::class_<EntitySpecificPropertiesProcess, EntitySpecificPropertiesProcess::Pointer, Process>(m, "EntitySpecificPropertiesProcess")
+        .def(py::init<Model&, Parameters&>());
+}
+
+} // namespace Python.
+} // Namespace Kratos
---
+++ applications/OptimizationApplication/custom_python/add_custom_processes_to_python.h
@@ -0,0 +1,29 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 license: HDF5Application/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+#include <pybind11/pybind11.h>
+
+// Project includes
+#include "includes/define.h"
+
+namespace Kratos {
+namespace Python {
+
+void AddCustomProcessesToPython(pybind11::module& m);
+
+} // namespace Python.
+} // namespace Kratos.
