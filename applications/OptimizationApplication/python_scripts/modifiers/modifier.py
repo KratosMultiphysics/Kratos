6debc471916ea567e838cd24d123c4a907664884
sunethwarna <suneth.warna@gmail.com>
2023-01-14 11:56:19 +0100

    add modifier interface

---
+++ applications/OptimizationApplication/python_scripts/modifiers/modifier.py
@@ -0,0 +1,32 @@
+from abc import ABC
+from abc import abstractmethod
+
+import KratosMultiphysics as Kratos
+from KratosMultiphysics.OptimizationApplication.optimization_info import OptimizationInfo
+from KratosMultiphysics.OptimizationApplication.utilities.helper_utils import ContainerEnum
+
+class Modifier(ABC):
+    def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, optimization_info: OptimizationInfo):
+        self.model = model
+        self.parameters = parameters
+        self.optimization_info = optimization_info
+
+    def Initialize(self):
+        pass
+
+    def InitializeSolutionStep(self):
+        pass
+
+    def FinalizeSolutionStep(self):
+        pass
+
+    def Finalize(self):
+        pass
+
+    @abstractmethod
+    def ModifySensitivities(self, sensitivities: Kratos.Vector, model_part: Kratos.ModelPart, container_type: ContainerEnum):
+        pass
+
+    @abstractmethod
+    def ModifyControlUpdates(self, update_change: Kratos.Vector, model_part: Kratos.ModelPart, container_type: ContainerEnum):
+        pass
