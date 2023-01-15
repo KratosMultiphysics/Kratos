54c72377f20b91966e0fbb4d27ec527955380ef1
sunethwarna <suneth.warna@gmail.com>
2023-01-15 18:43:24 +0100

    remove routine steps from optimization_info

--- applications/OptimizationApplication/python_scripts/optimization_info.py
+++ applications/OptimizationApplication/python_scripts/optimization_info.py
@@ -11,6 +11,7 @@ class OptimizationInfo:
         # initializing the buffer to one
         self.__iteration_data = [{}]
         self.__buffer_index = 0
+        self["step"] = 0
 
     def AddRoutine(self, routine: Routine):
         if not isinstance(routine, Routine):
@@ -19,30 +20,38 @@ class OptimizationInfo:
         class_hierrachy = getmro(routine.__class__)
         routine_index = class_hierrachy.index(Routine)
 
-        base_class = class_hierrachy[routine_index - 1]
-        if base_class not in self.__objects.keys():
-            self.__objects[base_class] = {}
+        base_class_name = class_hierrachy[routine_index - 1].__name__
+        if not self.HasRoutineType(base_class_name):
+            self.__objects[base_class_name] = []
 
-        routines_dict: dict = self.__objects[base_class]
-        if routine.GetName() in routines_dict.keys():
-            raise RuntimeError(f"Adding a routine with the name \"{routine.GetName()}\" while having another routine with the same name. Routine names for each type of object should be unique. [ Type of the routine: \"{base_class.__name__}\" ]")
+        if self.HasRoutine(base_class_name, routine.GetName()):
+            raise RuntimeError(f"Adding a routine with the name \"{routine.GetName()}\" while having another routine with the same name. Routine names for each type of object should be unique. [ Type of the routine: \"{base_class_name}\" ]")
 
-        routines_dict[routine.GetName()] = routine
+        self.__objects[base_class_name].append(routine)
 
-    def GetRoutine(self, routine_class_type, routine_name: str) -> 'routine_class_type':
-        if routine_class_type not in self.__objects.keys():
-            raise RuntimeError(f"No objects of type \"{routine_class_type.__name__}\" [ requested routine_name = \"{routine_name}\" ]. Following are the available type options: \n\t" + "\n\t".join(v.__name__ for v in self.__objects.keys()))
+    def HasRoutine(self, routine_class_type_name: str, routine_name: str) -> bool:
+        if not self.HasRoutineType(routine_class_type_name):
+            raise RuntimeError(f"No objects of type \"{routine_class_type_name}\" [ requested routine_name = \"{routine_name}\" ]. Following are the available type options: \n\t" + "\n\t".join(self.__objects.keys()))
 
-        if routine_name not in self.__objects[routine_class_type].keys():
-            raise RuntimeError(f"No routine with \"{routine_name}\" is available in the routines list for objects with type \"{routine_class_type.__name__}\". Followings are available options:\n\t" + "\n\t".join(v.GetName() for v in self.__objects[routine_class_type].values()))
+        return routine_name in [v.GetName() for v in self.__objects[routine_class_type_name]]
 
-        return self.__objects[routine_class_type][routine_name]
+    def GetRoutine(self, routine_class_type_name: str, routine_name: str) -> 'routine_class_type_name':
+        if not self.HasRoutineType(routine_class_type_name):
+            raise RuntimeError(f"No objects of type \"{routine_class_type_name}\" [ requested routine_name = \"{routine_name}\" ]. Following are the available type options: \n\t" + "\n\t".join(self.__objects.keys()))
 
-    def GetRoutines(self, routine_class_type):
-        if routine_class_type not in self.__objects.keys():
-            raise RuntimeError(f"No objects of type \"{routine_class_type.__name__}\". Following are the available type options: \n\t" + "\n\t".join(v.__name__ for v in self.__objects.keys()))
+        if not self.HasRoutine(routine_class_type_name, routine_name):
+            raise RuntimeError(f"No routine with \"{routine_name}\" is available in the routines list for objects with type \"{routine_class_type_name.__name__}\". Followings are available options:\n\t" + "\n\t".join(v.GetName() for v in self.__objects[routine_class_type_name].values()))
 
-        return self.__objects[routine_class_type].values()
+        return self.__objects[routine_class_type_name][[v.GetName() for v in self.__objects[routine_class_type_name]].index(routine_name)]
+
+    def HasRoutineType(self, routine_class_type_name: str) -> bool:
+        return routine_class_type_name in self.__objects.keys()
+
+    def GetRoutines(self, routine_class_type_name: str) -> list[Routine]:
+        if not self.HasRoutineType(routine_class_type_name):
+            raise RuntimeError(f"No objects of type \"{routine_class_type_name}\". Following are the available type options: \n\t" + "\n\t".join(self.__objects.keys()))
+
+        return self.__objects[routine_class_type_name]
 
     def SetBufferSize(self, buffer_size: int):
         if len(self.__iteration_data) != buffer_size:
@@ -56,22 +65,12 @@ class OptimizationInfo:
                     Kratos.Logger.PrintWarning(self.__class__.__name__, f"Changing buffer size with data will lose all the data in optimization info buffer. [ current_buffer_size = {len(self.__iteration_data)}, new_buffer_size = {buffer_size} ].")
             self.__iteration_data = [{} for i in range(buffer_size)]
             self.__buffer_index = 0
+            self["step"] = 0
             Kratos.Logger.PrintInfo(self.__class__.__name__, f"Optimization info buffer is set to {buffer_size}.")
 
-    def Initialize(self):
-        self["step"] = 1
-        self.__ExecuteRoutineMethod("Initialize")
-
-    def InitializeSolutionStep(self):
-        self.__ExecuteRoutineMethod("InitializeSolutionStep")
-
-    def FinalizeSolutionStep(self):
-        self.__ExecuteRoutineMethod("FinalizeSolutionStep")
+    def AdvanceSolutionStep(self):
         self.__buffer_index = (self.__buffer_index + 1) % len(self.__iteration_data)
         self["step"] = self["step", 1] + 1
-
-    def Finalize(self):
-        self.__ExecuteRoutineMethod("Finalize")
 
     def __getitem__(self, key):
         if isinstance(key, tuple):
@@ -108,8 +107,3 @@ class OptimizationInfo:
             raise RuntimeError(f"solution_step_index should be positive. [ solution_Step_index = {solution_step_index} ].")
         current_step_index = (self.__buffer_index - solution_step_index) % len(self.__iteration_data)
         return self.__iteration_data[current_step_index]
-
-    def __ExecuteRoutineMethod(self, method_name: str):
-        for routines_dict in self.__objects.values():
-            for routine in routines_dict.values():
-                getattr(routine, method_name)()
