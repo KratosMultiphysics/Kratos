f470cc56fd5ff23bd0ac66287533a27b8cac86ee
sunethwarna <suneth.warna@gmail.com>
2023-01-15 19:19:40 +0100

    add optimization_analysis.py

--- applications/OptimizationApplication/python_scripts/optimization_analysis.py
+++ applications/OptimizationApplication/python_scripts/optimization_analysis.py
@@ -5,6 +5,7 @@ from KratosMultiphysics.OptimizationApplication.utilities.helper_utils import Re
 
 from KratosMultiphysics.OptimizationApplication.mesh_controllers.mesh_controller import MeshController
 from KratosMultiphysics.OptimizationApplication.execution_policies.execution_policy_wrapper import ExecutionPolicyWrapper
+from KratosMultiphysics.OptimizationApplication.controls.control_wrapper import ControlWrapper
 from KratosMultiphysics.OptimizationApplication.responses.response_function_wrapper import CreateResponseFunctionWrapper
 
 class OptimizationAnalysis(AnalysisStage):
@@ -28,57 +29,57 @@ class OptimizationAnalysis(AnalysisStage):
         self._CreateAnalyses()
 
     def Initialize(self):
+        # first we initialize mesh controlelrs
+        for mesh_controller in self.__list_of_meshers:
+            mesh_controller.Initialize()
+
+        # we execute secondly the execution policy wrappers because they involve
+        # solving problems using analsis (or not).
+        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "Initialize")
+
+        # then we initialize the optimization solver
         super().Initialize()
 
+        # now we create other types
         self._CreateResponses()
         self._CreateControls()
 
-        # now set the buffers of the model parts
-        root_model_part_buffer_sizes = {}
-        for execution_policy_wrapper in self.optimization_info.GetExecutionPolicyWrappers():
-            execution_policy_wrapper: ExecutionPolicyWrapper = execution_policy_wrapper
-            analysis = execution_policy_wrapper.GetExecutionPolicy().GetAnalysis()
-            if analysis is not None:
-                analsis_solver = analysis._GetSolver()
-                analysis_model_part: Kratos.ModelPart = analsis_solver.GetComputingModelPart()
-                root_model_part_name = analysis_model_part.GetRootModelPart().FullName()
-
-                if root_model_part_name not in root_model_part_buffer_sizes.keys():
-                    root_model_part_buffer_sizes[root_model_part_name] = -1
-
-                root_model_part_buffer_sizes[root_model_part_name] = max(root_model_part_buffer_sizes[root_model_part_name], analsis_solver.GetMinimumBufferSize())
-
-        for mesh_controller in self.__list_of_meshers:
-            mesh_model_part_name = mesh_controller.GetModelPart().FullName()
-            if mesh_model_part_name in root_model_part_buffer_sizes.keys():
-                mesh_controller.GetModelPart().SetBufferSize(root_model_part_buffer_sizes[mesh_model_part_name])
-            mesh_controller.Initialize()
-
-        self.optimization_info.Initialize()
+        # now we initialize rest
+        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "Initialize")
+        self.__ExecuteRoutinesMethod("ControlsWrapper", "Initialize")
 
     def InitializeSolutionStep(self):
-        super().InitializeSolutionStep()
-
-        self.optimization_info.InitializeSolutionStep()
+        self.optimization_info.AdvanceSolutionStep()
 
         for mesh_controller in self.__list_of_meshers:
             mesh_controller.InitializeSolutionStep()
 
-    def FinalizeSolutionStep(self):
-        super().FinalizeSolutionStep()
+        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "InitializeSolutionStep")
 
+        super().InitializeSolutionStep()
+
+        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "InitializeSolutionStep")
+        self.__ExecuteRoutinesMethod("ControlsWrapper", "InitializeSolutionStep")
+
+    def FinalizeSolutionStep(self):
         for mesh_controller in self.__list_of_meshers:
             mesh_controller.FinalizeSolutionStep()
 
-        self.optimization_info.FinalizeSolutionStep()
+        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "FinalizeSolutionStep")
+        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "FinalizeSolutionStep")
+        self.__ExecuteRoutinesMethod("ControlsWrapper", "FinalizeSolutionStep")
+        super().FinalizeSolutionStep()
 
     def Finalize(self):
-        super().Finalize()
-
         for mesh_controller in self.__list_of_meshers:
             mesh_controller.Finalize()
 
-        self.optimization_info.Finalize()
+        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "Finalize")
+
+        super().Finalize()
+
+        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "Finalize")
+        self.__ExecuteRoutinesMethod("ControlsWrapper", "Finalize")
 
     def _CreateMeshes(self):
         for mesher_settings in self.project_parameters["meshes"]:
@@ -93,8 +94,16 @@ class OptimizationAnalysis(AnalysisStage):
             self.optimization_info.AddRoutine(CreateResponseFunctionWrapper(self.model, response_settings, self.optimization_info))
 
     def _CreateControls(self):
-        pass
+        for control_settings in self.project_parameters["controls"]:
+            self.optimization_info.AddRoutine(ControlWrapper(self.model, control_settings, self.optimization_info))
 
     def _CreateSolver(self):
         pass
         # return python_solvers_wrapper_fluid.CreateSolver(self.model, self.project_parameters)
+
+    def __ExecuteRoutinesMethod(self, routine_class_type_name: str, execution_method: str):
+        if self.optimization_info.HasRoutineType(routine_class_type_name):
+            for routine in self.optimization_info.GetRoutines(routine_class_type_name):
+                getattr(routine, execution_method)()
+        else:
+            Kratos.Logger.PrintWarning(self.__class__.__name__, f"No routine type \"{routine_class_type_name}\" is found in optimization info. Hence, skipping running \"{execution_method}\" methods on those types.")
