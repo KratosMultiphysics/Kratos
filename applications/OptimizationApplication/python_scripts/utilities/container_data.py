45f95fae796c6b4fa83072049291323f35647d93
sunethwarna <suneth.warna@gmail.com>
2023-01-18 19:30:00 +0100

    add contaienr data

---
+++ applications/OptimizationApplication/python_scripts/utilities/container_data.py
@@ -0,0 +1,97 @@
+from enum import Enum
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.OptimizationApplication as KratosOA
+
+class ContainerEnum(Enum):
+    NODES = 1
+    ELEMENTS = 2
+    CONDITIONS = 3
+    ELEMENT_PROPERTIES = 4
+    CONDITION_PROPERTIES = 5
+
+class ContainerData:
+    def __init__(self, model_part: Kratos.ModelPart, container_type: ContainerEnum):
+        self.__model_part = model_part
+        self.__container_type = container_type
+        self.__data = Kratos.Vector()
+
+    def GetModelPart(self):
+        return self.__model_part
+
+    def GetContainerTpe(self):
+        return self.__container_type
+
+    def GetData(self) -> Kratos.Vector:
+        return self.__data
+
+    def Clone(self):
+        other = ContainerData(self.__model_part, self.__container_type)
+        other.__data = Kratos.Vector(self.__data)
+        return other
+
+    def IsSameContainer(self, other):
+        return isinstance(other, ContainerData) and self.__model_part == other.__model_part and self.__container_type == other.__container_type
+
+    def ReadDataFromContainer(self, variable: any):
+        domain_size = self.__model_part.ProcessInfo[Kratos.DOMAIN_SIZE]
+        if self.__container_type == ContainerEnum.NODES:
+            KratosOA.OptimizationUtils.GetContainerVariableToVector(self.__model_part.Nodes, variable, domain_size, self.__data)
+        elif self.__container_type == ContainerEnum.CONDITIONS:
+            KratosOA.OptimizationUtils.GetContainerVariableToVector(self.__model_part.Conditions, variable, domain_size, self.__data)
+        elif self.__container_type == ContainerEnum.ELEMENTS:
+            KratosOA.OptimizationUtils.GetContainerVariableToVector(self.__model_part.Elements, variable, domain_size, self.__data)
+        elif self.__container_type == ContainerEnum.CONDITION_PROPERTIES:
+            KratosOA.OptimizationUtils.GetContainerPropertiesVariableToVector(self.__model_part.Conditions, variable, self.__data)
+        elif self.__container_type == ContainerEnum.ELEMENT_PROPERTIES:
+            KratosOA.OptimizationUtils.GetContainerPropertiesVariableToVector(self.__model_part.Elements, variable, self.__data)
+
+    def AssignDataToContainer(self, variable: any):
+        domain_size = self.__model_part.ProcessInfo[Kratos.DOMAIN_SIZE]
+        if self.__container_type == ContainerEnum.NODES:
+            KratosOA.OptimizationUtils.AssignVectorToContainer(self.__model_part.Nodes, variable, domain_size, self.__data)
+        elif self.__container_type == ContainerEnum.CONDITIONS:
+            KratosOA.OptimizationUtils.AssignVectorToContainer(self.__model_part.Conditions, variable, domain_size, self.__data)
+        elif self.__container_type == ContainerEnum.ELEMENTS:
+            KratosOA.OptimizationUtils.AssignVectorToContainer(self.__model_part.Elements, variable, domain_size, self.__data)
+        elif self.__container_type == ContainerEnum.CONDITION_PROPERTIES:
+            KratosOA.OptimizationUtils.AssignVectorToContainerProperties(self.__model_part.Conditions, variable, self.__data)
+        elif self.__container_type == ContainerEnum.ELEMENT_PROPERTIES:
+            KratosOA.OptimizationUtils.AssignVectorToContainerProperties(self.__model_part.Elements, variable, self.__data)
+
+    def __add__(self, other):
+        if not isinstance(other, ContainerData):
+            raise NotImplementedError(f"Adding an object of ContainerData to non object of ContainerData is not supprted. [ Type of right hand value = {type(other)} ].")
+
+        if not self.IsSameContainer(other):
+            raise RuntimeError(f"Trying to add {other} to {self} which are not of the same container.")
+
+        result = ContainerData(self.__model_part, self.__container_type)
+        KratosOA.OptimizationUtils.AddVectors(result.__data, self.__data, other.__data)
+        return result
+
+    def __sub__(self, other):
+        if not isinstance(other, ContainerData):
+            raise NotImplementedError(f"Substracting an object of ContainerData to non object of ContainerData is not supprted. [ Type of right hand value = {type(other)} ].")
+
+        if not self.IsSameContainer(other):
+            raise RuntimeError(f"Trying to substract {other} from {self} which are not of the same container.")
+
+        result = ContainerData(self.__model_part, self.__container_type)
+        KratosOA.OptimizationUtils.SubstractVectors(result.__data, self.__data, other.__data)
+        return result
+
+    def __mul__(self, other):
+        if not isinstance(other, (float, int)):
+            raise NotImplementedError(f"Multiplying an object of ContainerData with non float or int is not supprted. [ Type of right hand value = {type(other)} ].")
+
+        result = ContainerData(self.__model_part, self.__container_type)
+        KratosOA.OptimizationUtils.MultiplyVector(result.__data, self.__data, float(other))
+        return result
+
+    def __truediv__(self, other):
+        if not isinstance(other, (float, int)):
+            raise NotImplementedError(f"Dividing an object of ContainerData with non float or int is not supprted. [ Type of right hand value = {type(other)} ].")
+
+        result = ContainerData(self.__model_part, self.__container_type)
+        KratosOA.OptimizationUtils.DivideVector(result.__data, self.__data, float(other))
+        return result
---
+++ applications/OptimizationApplication/tests/test_container_data.py
@@ -0,0 +1,90 @@
+
+import KratosMultiphysics as Kratos
+
+# Import KratosUnittest
+import KratosMultiphysics.KratosUnittest as kratos_unittest
+
+from KratosMultiphysics.OptimizationApplication.utilities.container_data import ContainerData
+from KratosMultiphysics.OptimizationApplication.utilities.container_data import ContainerEnum
+
+class TestExecutionPolicies(kratos_unittest.TestCase):
+    def setUp(self):
+        self.model = Kratos.Model()
+        self.model_part = self.model.CreateModelPart("test")
+        self.model_part.AddNodalSolutionStepVariable(Kratos.DENSITY)
+        self.model_part.AddNodalSolutionStepVariable(Kratos.ACCELERATION)
+
+        number_of_nodes = 10
+        for id in range(1, number_of_nodes + 1):
+            node = self.model_part.CreateNewNode(id, id, id+1, id+2)
+            node.SetValue(Kratos.PRESSURE, id+3)
+            node.SetValue(Kratos.VELOCITY, Kratos.Array3([id+3, id+4, id+5]))
+
+        number_of_conditions = 11
+        for id in range(1, number_of_conditions + 1):
+            properties = self.model_part.CreateNewProperties(id)
+            properties.SetValue(Kratos.PRESSURE, id+400)
+            properties.SetValue(Kratos.VELOCITY, Kratos.Array3([id+500, id+600, id+700]))
+            condition = self.model_part.CreateNewCondition("LineCondition2D2N", id + 1, [(id % number_of_nodes) + 1, ((id + 1) % number_of_nodes) + 1 ], properties)
+            condition.SetValue(Kratos.PRESSURE, id+4)
+            condition.SetValue(Kratos.VELOCITY, Kratos.Array3([id+5, id+6, id+7]))
+
+        number_of_elements = 12
+        for id in range(1, number_of_elements + 1):
+            properties = self.model_part.CreateNewProperties(id + number_of_conditions)
+            properties.SetValue(Kratos.PRESSURE, id+500)
+            properties.SetValue(Kratos.VELOCITY, Kratos.Array3([id+600, id+700, id+800]))
+            element = self.model_part.CreateNewElement("Element2D3N", id + 2, [(id % number_of_nodes) + 1, ((id + 1) % number_of_nodes) + 1, ((id + 2) % number_of_nodes) + 1 ], properties)
+            element.SetValue(Kratos.PRESSURE, id+5)
+            element.SetValue(Kratos.VELOCITY, Kratos.Array3([id+6, id+7, id+8]))
+
+
+    def test_ContainerDataAdd(self):
+        a = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+        b = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+
+        a.ReadDataFromContainer(Kratos.PRESSURE)
+        b.ReadDataFromContainer(Kratos.PRESSURE)
+        c = a + b
+
+        c.AssignDataToContainer(Kratos.DENSITY)
+        for element in self.model_part.Elements:
+            self.assertEqual(element.Properties[Kratos.DENSITY], element.Properties[Kratos.PRESSURE] * 2)
+
+    def test_ContainerDataSubstract(self):
+        a = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+        b = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+
+        a.ReadDataFromContainer(Kratos.PRESSURE)
+        b.ReadDataFromContainer(Kratos.PRESSURE)
+        c = a * 4 - b
+
+        c.AssignDataToContainer(Kratos.DENSITY)
+        for element in self.model_part.Elements:
+            self.assertEqual(element.Properties[Kratos.DENSITY], element.Properties[Kratos.PRESSURE] * 3)
+
+    def test_ContainerDataMultiply(self):
+        a = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+        b = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+
+        a.ReadDataFromContainer(Kratos.PRESSURE)
+        c = a * 4
+
+        c.AssignDataToContainer(Kratos.DENSITY)
+        for element in self.model_part.Elements:
+            self.assertEqual(element.Properties[Kratos.DENSITY], element.Properties[Kratos.PRESSURE] * 4)
+
+    def test_ContainerDataDivide(self):
+        a = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+        b = ContainerData(self.model_part, ContainerEnum.ELEMENT_PROPERTIES)
+
+        a.ReadDataFromContainer(Kratos.PRESSURE)
+        c = a / self
+
+        c.AssignDataToContainer(Kratos.DENSITY)
+        for element in self.model_part.Elements:
+            self.assertEqual(element.Properties[Kratos.DENSITY], element.Properties[Kratos.PRESSURE] / 2)
+
+if __name__ == "__main__":
+    Kratos.Tester.SetVerbosity(Kratos.Tester.Verbosity.PROGRESS)  # TESTS_OUTPUTS
+    kratos_unittest.main()
