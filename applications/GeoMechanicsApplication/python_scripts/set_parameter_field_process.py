54a0e91c8885102a6f06c3f71f05042934f5f3a7
noordam <aron.noordam@deltares.nl>
2023-01-20 12:05:35 +0100

    initial commit set parameter field process python script

---
+++ applications/GeoMechanicsApplication/python_scripts/set_parameter_field_process.py
@@ -0,0 +1,8 @@
+
+import KratosMultiphysics
+import KratosMultiphysics.GeoMechanicsApplication as KratosGeo
+
+def Factory(settings, Model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return KratosGeo.SetParameterFieldProcess(Model, settings["Parameters"])
