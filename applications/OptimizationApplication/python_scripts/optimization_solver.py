22acc3f46b785a87c884952dd5da34e517bcc4b7
sunethwarna <suneth.warna@gmail.com>
2023-01-24 12:32:39 +0100

    increase modularity with design patterns in opt_solver

--- applications/OptimizationApplication/python_scripts/optimization_solver.py
+++ applications/OptimizationApplication/python_scripts/optimization_solver.py
@@ -1,15 +1,16 @@
 import KratosMultiphysics as Kratos
-import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.python_solver import PythonSolver
 
 from KratosMultiphysics.OptimizationApplication.optimization_info import OptimizationInfo
 from KratosMultiphysics.OptimizationApplication.utilities.helper_utils import Factory
+from KratosMultiphysics.OptimizationApplication.utilities.helper_utils import CallOnAll
+from KratosMultiphysics.OptimizationApplication.utilities.response_function_implementor import ObjectiveResponseFunctionImplementor
+from KratosMultiphysics.OptimizationApplication.utilities.response_function_implementor import ConstraintResponseFunctionImplementor
 
-from KratosMultiphysics.OptimizationApplication.mesh_controllers.mesh_controller import MeshController
-from KratosMultiphysics.OptimizationApplication.execution_policies.execution_policy_wrapper import ExecutionPolicyWrapper
-from KratosMultiphysics.OptimizationApplication.controls.control_wrapper import ControlWrapper
-from KratosMultiphysics.OptimizationApplication.responses.response_function_wrapper import ResponseFunctionWrapper
-from KratosMultiphysics.OptimizationApplication.algorithms.algorithm_wrapper import AlgorithmWrapper
+from KratosMultiphysics.OptimizationApplication.responses.response_function import ResponseFunction
+from KratosMultiphysics.OptimizationApplication.algorithms.algorithm import Algorithm
+from KratosMultiphysics.OptimizationApplication.model_part_controllers.model_part_controller import ModelPartController
+from KratosMultiphysics.OptimizationApplication.utilities.control_transformation_technique import ControlTransformationTechnique
 from KratosMultiphysics.OptimizationApplication.execution_policies.execution_policy_wrapper import ExecutionPolicyWrapper
 
 class OptimizationSolver(PythonSolver):
@@ -17,7 +18,7 @@ class OptimizationSolver(PythonSolver):
     def GetDefaultParameters(cls):
         return Kratos.Parameters("""{
             "model_part_name": "optimization_model_part",
-            "meshes"         : [],
+            "model_parts"    : [],
             "analyses"       : [],
             "responses"      : [],
             "controls"       : [],
@@ -30,18 +31,13 @@ class OptimizationSolver(PythonSolver):
 
         # creates the optimization info data holder
         self.optimization_info = OptimizationInfo()
-        self.__list_of_meshers = []
         self.__is_converged = False
+        self.__list_of_algorithms: 'list[Algorithm]' = []
+        self.__list_of_algorithm_properties: 'list[(list[str], Kratos.Parameters, Kratos.Parameters)]' = []
 
-        self._CreateMeshes()
+        self._CreateModelPartControllers()
         self._CreateAnalyses()
-
-        self.__list_of_algorithms = []
-        for algorithm_settings in settings["algorithms"]:
-            self.__list_of_algorithms.append(AlgorithmWrapper(model, algorithm_settings, self.optimization_info))
-
-        if len(self.__list_of_algorithms) == 0:
-            raise RuntimeError("No optimization solvers provided.")
+        self._CreateAlgorithms()
 
         # set the optimization info buffer size
         self.optimization_info.SetBufferSize(self.GetMinimumBufferSize())
@@ -50,63 +46,72 @@ class OptimizationSolver(PythonSolver):
         return self.optimization_info
 
     def GetMinimumBufferSize(self):
-        buffer_size = -1e+30
+        buffer_size = -1000
         for algorithm in self.__list_of_algorithms:
-            if algorithm.GetMinimumBufferSize() > buffer_size:
-                buffer_size = algorithm.GetMinimumBufferSize()
+            buffer_size = max(buffer_size, algorithm.GetMinimumBufferSize())
         return buffer_size
 
     def AddVariables(self):
-        self.__ExecuteMethod(self.__list_of_algorithms, "AddVariables")
+        CallOnAll(self.__list_of_algorithms, Algorithm.AddVariables)
 
     def AddDofs(self):
-        self.__ExecuteMethod(self.__list_of_algorithms, "AddDofs")
+        CallOnAll(self.__list_of_algorithms, Algorithm.AddDofs)
 
     def ImportModelPart(self):
-        self.__ExecuteMethod(self.__list_of_meshers, "ImportModelPart")
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ModelPartController), ModelPartController.ImportModelPart)
 
         # now we create other types because, responses and
         # controls can be used on submodel parts which are
         # only available after importing the whole model part
         self._CreateResponses()
-        self._CreateControls()
+        self._CreateControlTechniques()
+
+        # now we can assign responses and control techniques to algorithms
+        self._AssignAlgorithmProperties()
+
+    def Check(self):
+        CallOnAll(self.__list_of_algorithms, Algorithm.Check)
 
     def Initialize(self):
         # set the current step to 0
         self.optimization_info["step"] = 0
 
-        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "Initialize")
-        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "Initialize")
-        self.__ExecuteRoutinesMethod("ControlWrapper", "Initialize")
-        self.__ExecuteMethod(self.__list_of_algorithms, "Initialize")
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ModelPartController), ModelPartController.Initialize)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ExecutionPolicyWrapper), ExecutionPolicyWrapper.Initialize)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ResponseFunction), ResponseFunction.Initialize)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ControlTransformationTechnique), ControlTransformationTechnique.Initialize)
+        CallOnAll(self.__list_of_algorithms, Algorithm.Initialize)
 
     def InitializeSolutionStep(self):
-        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "InitializeSolutionStep")
-        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "InitializeSolutionStep")
-        self.__ExecuteRoutinesMethod("ControlWrapper", "InitializeSolutionStep")
-        self.__ExecuteMethod(self.__list_of_algorithms, "InitializeSolutionStep")
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ModelPartController), ModelPartController.InitializeSolutionStep)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ExecutionPolicyWrapper), ExecutionPolicyWrapper.InitializeSolutionStep)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ResponseFunction), ResponseFunction.InitializeSolutionStep)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ControlTransformationTechnique), ControlTransformationTechnique.InitializeSolutionStep)
+        CallOnAll(self.__list_of_algorithms, Algorithm.InitializeSolutionStep)
 
     def SolveSolutionStep(self):
-        self.__ExecuteMethod(self.__list_of_algorithms, "SolveSolutionStep")
+        CallOnAll(self.__list_of_algorithms, Algorithm.SolveSolutionStep)
         self.__is_converged = True
         for algorithm in self.__list_of_algorithms:
             self.__is_converged = self.__is_converged and algorithm.IsConverged()
         return self.__is_converged
 
     def IsConverged(self):
-        self.__is_converged
+        return self.__is_converged
 
     def FinalizeSolutionStep(self):
-        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "FinalizeSolutionStep")
-        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "FinalizeSolutionStep")
-        self.__ExecuteRoutinesMethod("ControlWrapper", "FinalizeSolutionStep")
-        self.__ExecuteMethod(self.__list_of_algorithms, "FinalizeSolutionStep")
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ModelPartController), ModelPartController.FinalizeSolutionStep)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ExecutionPolicyWrapper), ExecutionPolicyWrapper.FinalizeSolutionStep)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ResponseFunction), ResponseFunction.FinalizeSolutionStep)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ControlTransformationTechnique), ControlTransformationTechnique.FinalizeSolutionStep)
+        CallOnAll(self.__list_of_algorithms, Algorithm.FinalizeSolutionStep)
 
     def Finalize(self):
-        self.__ExecuteRoutinesMethod("ExecutionPolicyWrapper", "Finalize")
-        self.__ExecuteRoutinesMethod("ResponseFunctionWrapper", "Finalize")
-        self.__ExecuteRoutinesMethod("ControlWrapper", "Finalize")
-        self.__ExecuteMethod(self.__list_of_algorithms, "Finalize")
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ModelPartController), ModelPartController.Finalize)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ExecutionPolicyWrapper), ExecutionPolicyWrapper.Finalize)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ResponseFunction), ResponseFunction.Finalize)
+        CallOnAll(self.optimization_info.GetOptimizationRoutines(ControlTransformationTechnique), ControlTransformationTechnique.Finalize)
+        CallOnAll(self.__list_of_algorithms, Algorithm.Finalize)
 
     def GetComputingModelPart(self):
         model_part_name = self.settings["model_part_name"].GetString()
@@ -118,38 +123,73 @@ class OptimizationSolver(PythonSolver):
     def AdvanceInTime(self, _):
         self.optimization_info.AdvanceSolutionStep()
         self.optimization_info["step"] = self.optimization_info.GetSolutionStepData(1)["step"] + 1
+
+        # now apply the controls
+        if self.optimization_info["step"] > 1:
+            CallOnAll(self.optimization_info.GetOptimizationRoutines(ControlTransformationTechnique), ControlTransformationTechnique.ApplyControlUpdate)
+
+        self.GetComputingModelPart().ProcessInfo[Kratos.STEP] = self.optimization_info["step"]
+
         return self.optimization_info["step"]
 
-    def __ExecuteMethod(self, items, method_name: str):
-        for itr in items:
-            getattr(itr, method_name)()
-
-    def __ExecuteRoutinesMethod(self, routine_class_type_name: str, execution_method: str):
-        if self.optimization_info.HasOptimizationRoutineType(routine_class_type_name):
-            for routine in self.optimization_info.GetOptimizationRoutines(routine_class_type_name):
-                getattr(routine, execution_method)()
-        else:
-            Kratos.Logger.PrintWarning(self.__class__.__name__, f"No routine type \"{routine_class_type_name}\" is found in optimization info. Hence, skipping running \"{execution_method}\" methods on those types.")
-
-    def _CreateMeshes(self):
-        mesher_settings: Kratos.Parameters
-        for mesher_settings in self.settings["meshes"]:
-            default_mesher_settinsg = Kratos.Parameters("""{
-                "module"  : "KratosMultiphysics.OptimizationApplication.mesh_controllers",
-                "type"    : "MdpaImportOnlyMeshController",
+    def _CreateModelPartControllers(self):
+        for model_part_controller_settings in self.settings["model_parts"]:
+            default_model_part_controller_settings = Kratos.Parameters("""{
+                "name"    : "",
+                "module"  : "KratosMultiphysics.OptimizationApplication.model_part_controllers",
+                "type"    : "MdpaModelPartController",
                 "settings": {}
             }""")
-            mesher_settings.ValidateAndAssignDefaults(default_mesher_settinsg)
-            self.__list_of_meshers.append(Factory(mesher_settings["module"].GetString(), mesher_settings["type"].GetString(), self.model, mesher_settings["settings"], self.optimization_info, MeshController))
+            model_part_controller_settings.ValidateAndAssignDefaults(default_model_part_controller_settings)
+            routine: ModelPartController = Factory(model_part_controller_settings["module"].GetString(), model_part_controller_settings["type"].GetString(), self.model, model_part_controller_settings["settings"], self.optimization_info, ModelPartController)
+            self.optimization_info.AddOptimizationRoutine(ModelPartController, model_part_controller_settings["name"].GetString(), routine)
 
     def _CreateAnalyses(self):
         for analyses_settings in self.settings["analyses"]:
-            self.optimization_info.AddOptimizationRoutine(ExecutionPolicyWrapper(self.model, analyses_settings))
+            routine = ExecutionPolicyWrapper(self.model, analyses_settings)
+            self.optimization_info.AddOptimizationRoutine(ExecutionPolicyWrapper, routine.GetName(), routine)
 
     def _CreateResponses(self):
+        default_settings = Kratos.Parameters("""{
+            "name"     : "",
+            "module"   : "KratosMultiphysics.OptimizationApplication.responses",
+            "type"     : "",
+            "settings" : {}
+        }""")
         for response_settings in self.settings["responses"]:
-            self.optimization_info.AddOptimizationRoutine(ResponseFunctionWrapper(self.model, response_settings, self.optimization_info))
+            response_settings.ValidateAndAssignDefaults(default_settings)
+            routine: ResponseFunction = Factory(response_settings["module"].GetString(), response_settings["type"].GetString(), self.model, response_settings["settings"], self.optimization_info, ResponseFunction)
+            self.optimization_info.AddOptimizationRoutine(ResponseFunction, response_settings["name"].GetString(), routine)
 
-    def _CreateControls(self):
+    def _CreateControlTechniques(self):
         for control_settings in self.settings["controls"]:
-            self.optimization_info.AddOptimizationRoutine(ControlWrapper(self.model, control_settings, self.optimization_info))
+            routine = ControlTransformationTechnique(self.model, control_settings, self.optimization_info)
+            self.optimization_info.AddOptimizationRoutine(ControlTransformationTechnique, routine.GetName(), routine)
+
+    def _CreateAlgorithms(self):
+        default_settings = Kratos.Parameters("""{
+            "name"         : "",
+            "module"       : "KratosMultiphysics.OptimizationApplication.algorithms",
+            "type"         : "PLEASE_PROVIDE_AN_ALGORITHM_CLASS_NAME",
+            "control_names": [],
+            "objectives"   : [],
+            "constraints"  : [],
+            "settings"     : {}
+        }""")
+        for algorithm_settings in self.settings["algorithms"]:
+            algorithm_settings.ValidateAndAssignDefaults(default_settings)
+            algorithm: Algorithm = Factory(algorithm_settings["module"].GetString(), algorithm_settings["type"].GetString(), self.model, algorithm_settings["settings"], self.optimization_info, Algorithm)
+            algorithm.SetName(algorithm_settings["name"].GetString())
+            self.__list_of_algorithms.append(algorithm)
+            self.__list_of_algorithm_properties.append(
+                (
+                    algorithm_settings["control_names"].GetStringArray(),
+                    algorithm_settings["objectives"],
+                    algorithm_settings["constraints"]
+                ))
+
+    def _AssignAlgorithmProperties(self):
+        for algorithm, (control_names, objectives_settings, contraints_settings) in zip(self.__list_of_algorithms, self.__list_of_algorithm_properties):
+            algorithm.SetControllers([self.optimization_info.GetOptimizationRoutine(ControlTransformationTechnique, control_name) for control_name in control_names])
+            algorithm.SetObjectives([ObjectiveResponseFunctionImplementor(objective_settings, self.optimization_info) for objective_settings in objectives_settings])
+            algorithm.SetConstraints([ConstraintResponseFunctionImplementor(constraint_settings, self.optimization_info) for constraint_settings in contraints_settings])
