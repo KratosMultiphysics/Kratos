7fb6efe91e7fe10fc43ba973ed4d2251198fdd32
Daniel Diez <ddiez@europe.altair.com>
2023-01-25 15:49:12 +0100

    using root model part to do replacementes in conditions (#10679)
    Co-authored-by: Daniel Diez <ddiez@altair.com>

--- applications/MeshingApplication/custom_utilities/linear_to_quadratic_tetrahedra_mesh_converter_utility.cpp
+++ applications/MeshingApplication/custom_utilities/linear_to_quadratic_tetrahedra_mesh_converter_utility.cpp
@@ -197,7 +197,7 @@ namespace Kratos {
 
             // Replace the conditions in SubModelParts
             if (NewConditions.size() > 0) {
-                ReplaceConditionsInSubModelPart(rThisModelPart);
+                ReplaceConditionsInSubModelPart(rThisModelPart.GetRootModelPart());
             }
         }
         KRATOS_CATCH("");
