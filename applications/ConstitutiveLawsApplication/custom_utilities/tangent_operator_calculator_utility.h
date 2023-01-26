64b87443a56ff888a2af5165ab75963c70c76f52
Alejandro <alejandro.cornejo23@gmail.com>
2023-01-26 09:17:29 +0400

    symmetrize option small strain pert

--- applications/ConstitutiveLawsApplication/custom_utilities/tangent_operator_calculator_utility.h
+++ applications/ConstitutiveLawsApplication/custom_utilities/tangent_operator_calculator_utility.h
@@ -141,6 +141,8 @@ public:
         // Converged values to be storaged
         const Vector unperturbed_strain_vector_gp = Vector(rValues.GetStrainVector());
         const Vector unperturbed_stress_vector_gp = Vector(rValues.GetStressVector());
+        const auto &r_properties = Values.GetMaterialProperties();
+        const bool symmetrize_operator = (r_properties.Has(SYMMETRIZE_TANGENT_OPERATOR)) ? r_properties[SYMMETRIZE_TANGENT_OPERATOR] : false;
 
         // The number of components
         const SizeType num_components = unperturbed_stress_vector_gp.size();
@@ -152,8 +154,8 @@ public:
 
         // Calculate the perturbation
         double pertubation = PerturbationThreshold;
-        if (rValues.GetMaterialProperties().Has(PERTURBATION_SIZE)) {
-            pertubation = rValues.GetMaterialProperties()[PERTURBATION_SIZE];
+        if (rr_properties.Has(PERTURBATION_SIZE)) {
+            pertubation = r_properties[PERTURBATION_SIZE];
             if (pertubation == -1.0)
                 pertubation = std::sqrt(tolerance);
         } else {
@@ -256,7 +258,10 @@ public:
                 noalias(r_perturbed_integrated_stress) = unperturbed_stress_vector_gp;
             }
         }
-        noalias(r_tangent_tensor) = auxiliary_tensor;
+        if (symmetrize_operator)
+            noalias(r_tangent_tensor) = 0.5*(auxiliary_tensor + trans(auxiliary_tensor));
+        else
+            noalias(r_tangent_tensor) = auxiliary_tensor;
     }
 
     /**
