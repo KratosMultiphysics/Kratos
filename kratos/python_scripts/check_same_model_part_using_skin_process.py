9a80ece150be18d38b1b1028345d1d68e16e5d9f
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-02-01 13:18:14 +0100

    Clean settings

--- kratos/python_scripts/check_same_model_part_using_skin_process.py
+++ kratos/python_scripts/check_same_model_part_using_skin_process.py
@@ -26,23 +26,8 @@ class CheckSameModelPartUsingSkinDistanceProcess(KratosMultiphysics.Process):
         """
         KratosMultiphysics.Process.__init__(self)
 
-        #The value can be a double or a string (function)
-        default_settings = KratosMultiphysics.Parameters("""
-        {
-            "help"                                : "This process checks that the model part is the same using the skin distance",
-            "skin_model_part_1_name"              : "PLEASE_SPECIFY_SKIN_MODEL_PART_2_NAME",
-            "skin_model_part_2_name"              : "PLEASE_SPECIFY_SKIN_MODEL_PART_2_NAME",
-            "tolerance"                           : 1.0e-3,
-            "bounding_box_scale_factor"           : 1.5,
-            "number_of_divisions_background_mesh" : 30,
-            "discontinuous_distance_settings": {
-            }
-            }
-        """
-        )
         # Assigning values
         self.model = model
-        settings.ValidateAndAssignDefaults(default_settings)
 
         # Create the process
         self.process = KratosMultiphysics.CheckSameModelPartUsingSkinDistanceProcess3D(self.model, settings)
