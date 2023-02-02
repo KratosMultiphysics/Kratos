5736205a39b3d9779dfa36ca21bee7710ee18eb2
Carlos Roig <roigcarlo@gmail.com>
2023-02-02 12:53:24 +0100

    Adding kernel fixture to testing.h

--- kratos/testing/testing.h
+++ kratos/testing/testing.h
@@ -14,13 +14,27 @@
 #if !defined(KRATOS_TESTING_H_INCLUDED )
 #define  KRATOS_TESTING_H_INCLUDED
 
-#include "testing/test_suite.h" // This includes the test_case.h which includes tester.h
-#include "includes/checks.h"  // It is almost always necessary. includes the exception
+#include "includes/kernel.h"
+#include "includes/expects.h"  // It is almost always necessary. includes the exception
 #include "includes/data_communicator.h"
+
+#include "testing/test_suite.h" // This includes the test_case.h which includes tester.h
 #include "testing/test_skipped_exception.h" // Macros and exception class used to skip tests.
 
 namespace Kratos {
 namespace Testing {
+
+class KernelTest : public ::testing::Test 
+{
+    // We chose to initialize the mKernel in the constructor and not in the
+    // fixture SetUp because this way we can perfectly control the life
+    // cycle of the Kernel (as oposite of using a unique_ptr, for example) 
+	protected:
+        KernelTest() : mKernel() {}
+        ~KernelTest() {}
+
+	    Kratos::Kernel mKernel;
+};
 
 DataCommunicator& GetDefaultDataCommunicator();
 
