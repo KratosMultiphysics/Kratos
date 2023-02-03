f545b8de03b623c99b236b0f6b21ec8cc5bdee61
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-02-03 16:32:39 +0100

    Making everything more generic

--- kratos/processes/replace_elements_and_condition_process.cpp
+++ kratos/processes/replace_elements_and_condition_process.cpp
@@ -24,15 +24,16 @@
 namespace Kratos {
 namespace {
 
-/// Replace entities in a given container if the entity id is present in a list of ids.
-    /*  @param rReferenceEntity New type of entity that will replace old one
-     *  @param rEntityContainer Container of elements susceptible to be replaces
-     *  @param rSetOfIds Set of entities ids we want to replace
-     */
-template <class TEntity, class TEntityContainer>
+/**
+ * @brief Replace entities in a given container if the entity id is present in a list of ids.
+ * @param rReferenceEntity New type of entity that will replace old one
+ * @param rEntityContainer Container of elements susceptible to be replaces
+ * @param rSetOfIds Set of entities ids we want to replace
+ */
+template <class TEntity>
 void ReplaceEntities(
     const TEntity& rReferenceEntity,
-    TEntityContainer& rEntityContainer,
+    PointerVectorSet<TEntity, IndexedObject, std::less<typename IndexedObject::result_type>, std::equal_to<typename IndexedObject::result_type>, typename TEntity::Pointer, std::vector< typename TEntity::Pointer>>& rEntityContainer,
     std::unordered_set<std::size_t>& rSetOfIds
     )
 {
@@ -52,12 +53,55 @@ void ReplaceEntities(
     });
 }
 
-/// Replace elements in a given submodelpart using the elements from the root model part
-/// if the element id is present in a given set of ids
-    /*  @param rModelPart Model part whose elements we want to replace
-     *  @param rRootModelPart Root model part with the replaced elements
-     *  @param rSetOfElementsIds Set of elements ids we want to replace
-     */
+/**
+ * @brief Replace entities in a given container if the entity id is present in a list of ids.
+ * @param rReferenceEntity New type of entity that will replace old one
+ * @param rEntityContainer Container of elements susceptible to be replaces
+ * @param rSetOfIds Set of entities ids we want to replace
+ */
+template <class TEntity>
+void ReplaceEntities(
+    const Parameters ListReferenceEntity,
+    const std::unordered_map<GeometryData::KratosGeometryType, std::string>& rGeometryTypesToStrings,
+    PointerVectorSet<TEntity, IndexedObject, std::less<typename IndexedObject::result_type>, std::equal_to<typename IndexedObject::result_type>, typename TEntity::Pointer, std::vector< typename TEntity::Pointer>>& rEntityContainer,
+    std::unordered_set<std::size_t>& rSetOfIds
+    )
+{
+    TEntity reference_entity = TEntity();
+    GeometryData::KratosGeometryType current_geometry_type = GeometryData::KratosGeometryType::Kratos_generic_type;
+    GeometryData::KratosGeometryType reference_geometry_type = GeometryData::KratosGeometryType::Kratos_generic_type;
+    IndexPartition<std::size_t>(rEntityContainer.size()).for_each([&](std::size_t Index){
+        auto it_entity = rEntityContainer.begin() + Index;
+        if (rSetOfIds.find(it_entity->Id()) != rSetOfIds.end()) {
+            auto p_geometry = it_entity->pGetGeometry();
+            const auto& r_geometry_type = p_geometry->GetGeometryType();
+            // Checking if geometry type is the same
+            if (r_geometry_type != current_geometry_type) {
+                auto it_find = rGeometryTypesToStrings.find(r_geometry_type);
+                KRATOS_ERROR_IF(it_find == rGeometryTypesToStrings.end()) << "Trying to replace an element with a different geometry type. No compatible geometry type: " << static_cast<int>(r_geometry_type) << std::endl;
+                const std::string& r_type = it_find->second;
+                KRATOS_ERROR_IF_NOT(ListReferenceEntity.Has(r_type)) << "Trying to replace an element with a different geometry type. No reference entity found for geometry type: " << r_type << std::endl;
+                reference_entity = KratosComponents<TEntity>::Get(ListReferenceEntity[r_type].GetString()); // Yes, we coppied it
+                current_geometry_type = r_geometry_type;
+                reference_geometry_type = reference_entity.GetGeometry().GetGeometryType();
+            }
+            KRATOS_DEBUG_ERROR_IF_NOT(r_geometry_type == reference_geometry_type) << "Trying to replace an element with a different geometry type. Reference entity " << reference_entity.GetGeometry().Info() << " vs  " << p_geometry->Info() << "\n Entity info: " << reference_entity.Info() << std::endl;
+            auto p_new_entity = reference_entity.Create(it_entity->Id(), p_geometry, it_entity->pGetProperties());
+            // Deep copy data and flags
+            p_new_entity->Set(Flags(*it_entity));
+
+            (*it_entity.base()) = p_new_entity;
+        }
+    });
+}
+
+
+/**
+ * @brief Replace elements in a given submodelpart using the elements from the root model part if the element id is present in a given set of ids
+ * @param rModelPart Model part whose elements we want to replace
+ * @param rRootModelPart Root model part with the replaced elements
+ * @param rSetOfElementsIds Set of elements ids we want to replace
+ */
 void UpdateElementsInSubModelPart(
     ModelPart& rModelPart,
     ModelPart& rRootModelPart,
@@ -80,12 +124,12 @@ void UpdateElementsInSubModelPart(
     }
 }
 
-/// Replace conditions in a given submodelpart using the conditions from the root model part
-/// if the condition id is present in a given set of ids
-    /*  @param rModelPart Model part whose conditions we want to replace
-     *  @param rRootModelPart Root model part with the replaced conditions
-     *  @param rSetOfConditions Set of conditions ids we want to replace
-     */
+/**
+ * @brief Replace conditions in a given submodelpart using the conditions from the root model part if the condition id is present in a given set of ids
+ * @param rModelPart Model part whose conditions we want to replace
+ * @param rRootModelPart Root model part with the replaced conditions
+ * @param rSetOfConditions Set of conditions ids we want to replace
+ */
 void UpdateConditionsInSubModelPart(
     ModelPart& rModelPart,
     ModelPart& rRootModelPart,
@@ -113,29 +157,98 @@ void UpdateConditionsInSubModelPart(
 /***********************************************************************************/
 /***********************************************************************************/
 
+void ReplaceElementsAndConditionsProcess::InitializeMemberVariables()
+{
+    // We can provide a list of replacements instead of a single one
+
+    /* Elements */
+    if (mSettings["element_name"].IsSubParameter()) {
+        for (auto& r_sub_parameter : mSettings["element_name"]) {
+            const std::string& r_element_name = r_sub_parameter.GetString();
+            KRATOS_ERROR_IF(r_element_name != "" && !KratosComponents<Element>::Has(r_element_name)) << "Element name not found in KratosComponents< Element > -- name is " << r_element_name << std::endl;
+        }
+        mOnlyOneElementOrCondition[0] = false;
+    } else if (mSettings["element_name"].IsString()) {
+        const std::string& r_element_name = mSettings["element_name"].GetString();
+        // Some values need to be mandatorily prescribed since no meaningful default value exist. For this reason try accessing to them so that an error is thrown if they don't exist
+        KRATOS_ERROR_IF(r_element_name != "" && !KratosComponents<Element>::Has(r_element_name)) << "Element name not found in KratosComponents< Element > -- name is " << r_element_name << std::endl;
+        mOnlyOneElementOrCondition[0] = true;
+    } else {
+        KRATOS_ERROR << "The element_name should be either a string or a list of strings" << std::endl;
+    }
+
+    /* Conditions */
+    if (mSettings["condition_name"].IsSubParameter()) {
+        for (auto& r_sub_parameter : mSettings["condition_name"]) {
+            const std::string& r_condition_name = r_sub_parameter.GetString();
+            KRATOS_ERROR_IF(r_condition_name != "" && !KratosComponents<Condition>::Has(r_condition_name)) << "Condition name not found in KratosComponents< Condition > -- name is " << r_condition_name << std::endl;
+        }
+        mOnlyOneElementOrCondition[1] = false;
+    } else if (mSettings["condition_name"].IsString()) {
+        const std::string& r_condition_name = mSettings["condition_name"].GetString();
+        // Some values need to be mandatorily prescribed since no meaningful default value exist. For this reason try accessing to them so that an error is thrown if they don't exist
+        KRATOS_ERROR_IF(r_condition_name != "" && !KratosComponents<Element>::Has(r_condition_name)) << "Element name not found in KratosComponents< Element > -- name is " << r_condition_name << std::endl;
+        mOnlyOneElementOrCondition[1] = true;
+    } else {
+        KRATOS_ERROR << "The condition_name should be either a string or a list of strings" << std::endl;
+    }
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
 void ReplaceElementsAndConditionsProcess::Execute()
 {
-    const std::string& r_element_name = mSettings["element_name"].GetString();
-    const std::string& r_condition_name = mSettings["condition_name"].GetString();
     ModelPart& r_root_model_part = mrModelPart.GetRootModelPart();
-
-    if (r_element_name != "") {
+    /* Elements */
+    bool replace_elements = false;
+    if (mOnlyOneElementOrCondition[0]) {
+        if (mSettings["element_name"].GetString() != "") replace_elements = true;
+    } else {
+        std::size_t counter = 0;
+        for (auto& r_sub_parameter : mSettings["element_name"]) {
+            if (r_sub_parameter.GetString() != "") counter += 1;
+        }
+        if (counter > 0) replace_elements = true;
+    }
+    if (replace_elements) {
         std::unordered_set<std::size_t> set_element_ids (mrModelPart.NumberOfElements());
         for(auto& r_elem : mrModelPart.Elements()) {
             set_element_ids.insert(r_elem.Id());
         }
-        ReplaceEntities(KratosComponents<Element>::Get(r_element_name), r_root_model_part.Elements(), set_element_ids);
+        if (mOnlyOneElementOrCondition[0]) {
+            const std::string& r_element_name = mSettings["element_name"].GetString();
+            ReplaceEntities(KratosComponents<Element>::Get(r_element_name), r_root_model_part.Elements(), set_element_ids);
+        } else {
+            ReplaceEntities<Element>(mSettings["element_name"], mGeometryTypesToStrings, r_root_model_part.Elements(), set_element_ids);
+        }
         for (auto& r_sub_model_part : r_root_model_part.SubModelParts()) {
             UpdateElementsInSubModelPart(r_sub_model_part, r_root_model_part, set_element_ids);
         }
     }
 
-    if (r_condition_name != "") {
+    /* Conditions */
+    bool replace_conditions = false;
+    if (mOnlyOneElementOrCondition[1]) {
+        if (mSettings["condition_name"].GetString() != "") replace_conditions = true;
+    } else {
+        std::size_t counter = 0;
+        for (auto& r_sub_parameter : mSettings["condition_name"]) {
+            if (r_sub_parameter.GetString() != "") counter += 1;
+        }
+        if (counter > 0) replace_conditions = true;
+    }
+    if (replace_conditions) {
         std::unordered_set<std::size_t> set_conditions_ids (mrModelPart.NumberOfConditions());
         for(auto& r_cond : mrModelPart.Conditions()) {
             set_conditions_ids.insert(r_cond.Id());
         }
-        ReplaceEntities(KratosComponents<Condition>::Get(r_condition_name), r_root_model_part.Conditions(), set_conditions_ids);
+        if (mOnlyOneElementOrCondition[1]) {
+            const std::string& r_condition_name = mSettings["condition_name"].GetString();
+            ReplaceEntities(KratosComponents<Condition>::Get(r_condition_name), r_root_model_part.Conditions(), set_conditions_ids);
+        } else {
+            ReplaceEntities<Condition>(mSettings["condition_name"], mGeometryTypesToStrings, r_root_model_part.Conditions(), set_conditions_ids);
+        }
         for (auto& r_sub_model_part : r_root_model_part.SubModelParts()) {
             UpdateConditionsInSubModelPart(r_sub_model_part, r_root_model_part, set_conditions_ids);
         }
--- kratos/processes/replace_elements_and_condition_process.h
+++ kratos/processes/replace_elements_and_condition_process.h
@@ -19,9 +19,10 @@
 // External includes
 
 // Project includes
-#include "includes/model_part.h"
+#include "containers/model.h"
 #include "includes/kratos_parameters.h"
 #include "processes/process.h"
+#include "geometries/geometry_data.h"
 
 namespace Kratos
 {
@@ -52,6 +53,26 @@ public:
 
     /**
      * @brief Default constructor
+     * @param rModel The model containing the model part where to assign the conditions and elements
+     * @param Settings The parameters containing the names of the conditions and elements
+     */
+    ReplaceElementsAndConditionsProcess(
+        Model& rModel,
+        Parameters Settings
+        ) : Process(Flags()) ,
+            mrModelPart(rModel.GetModelPart(Settings["model_part_name"].GetString())),
+            mSettings( Settings)
+    {
+        KRATOS_TRY
+
+        // Initialize member variables
+        InitializeMemberVariables();
+
+        KRATOS_CATCH("")
+    }
+
+    /**
+     * @brief Default constructor
      * @param rModelPart The model part where to assign the conditions and elements
      * @param Settings The parameters containing the names of the conditions and elements
      */
@@ -64,16 +85,8 @@ public:
     {
         KRATOS_TRY
 
-        const std::string element_name = Settings["element_name"].GetString();
-        const std::string condition_name = Settings["condition_name"].GetString();
-
-        // Some values need to be mandatorily prescribed since no meaningful default value exist. For this reason try accessing to them
-        // so that an error is thrown if they don't exist
-        KRATOS_ERROR_IF(element_name != "" && !KratosComponents<Element>::Has(element_name)) << "Element name not found in KratosComponents< Element > -- name is " << element_name << std::endl;
-        KRATOS_ERROR_IF(condition_name != "" && !KratosComponents<Condition>::Has(condition_name)) << "Condition name not found in KratosComponents< Condition > -- name is " << condition_name << std::endl;
-
-        // Now validate agains defaults -- this also ensures no type mismatch
-        Settings.ValidateAndAssignDefaults(GetDefaultParameters());
+        // Initialize member variables
+        InitializeMemberVariables();
 
         KRATOS_CATCH("")
     }
@@ -91,17 +104,28 @@ public:
     /// Assignment operator.
     ReplaceElementsAndConditionsProcess& operator=(ReplaceElementsAndConditionsProcess const& rOther) = delete;
 
-    /// This operator is provided to call the process as a function and simply calls the Execute method.
-    void operator()()
-    {
-        Execute();
-    }
-
     ///@}
     ///@name Operations
     ///@{
 
-    /// Execute method is used to execute the ReplaceElementsAndConditionsProcess algorithms.
+    /**
+     * @brief This method creates an pointer of the process
+     * @details We consider as input a Model and a set of Parameters for the sake of generality
+     * @warning Must be overrided in each process implementation
+     * @param rModel The model to be consider
+     * @param ThisParameters The configuration parameters
+     */
+    Process::Pointer Create(
+        Model& rModel,
+        Parameters ThisParameters
+        ) override
+    {
+        return Kratos::make_shared<ReplaceElementsAndConditionsProcess>(rModel, ThisParameters);
+    }
+
+    /**
+     * @brief Execute method is used to execute the ReplaceElementsAndConditionsProcess algorithms.
+     */
     void Execute() override;
 
     /**
@@ -110,8 +134,9 @@ public:
     const Parameters GetDefaultParameters() const override
     {
         const Parameters default_parameters( R"({
-            "element_name"   : "PLEASE_CHOOSE_MODEL_PART_NAME",
-            "condition_name" : "PLEASE_PRESCRIBE_VARIABLE_NAME"
+            "model_part_name" : "PLEASE_CHOOSE_MODEL_PART_NAME",
+            "element_name"    : "PLEASE_CHOOSE_ELEMENT_NAME",
+            "condition_name"  : "PLEASE_CHOOSE_CONDITION_NAME"
         } )" );
         return default_parameters;
     }
@@ -142,8 +167,57 @@ protected:
     ///@name Member Variables
     ///@{
 
-    ModelPart& mrModelPart; /// The main model part where the elements and conditions will be replaced
-    Parameters mSettings;   /// The settings of the problem (names of the conditions and elements)
+    ModelPart& mrModelPart;                         /// The main model part where the elements and conditions will be replaced
+    Parameters mSettings;                           /// The settings of the problem (names of the conditions and elements)
+    std::array<bool, 2> mOnlyOneElementOrCondition; /// This array stores if only one element or condition type is going to be replaced
+
+    /// Equivalent types definition
+    const std::unordered_map<GeometryData::KratosGeometryType, std::string> mGeometryTypesToStrings = {
+        {GeometryData::KratosGeometryType::Kratos_Point2D, "Point2D"},
+        {GeometryData::KratosGeometryType::Kratos_Point3D, "Point3D"},
+        {GeometryData::KratosGeometryType::Kratos_Line2D2, "Line2D2"},
+        {GeometryData::KratosGeometryType::Kratos_Line2D3, "Line2D3"},
+        {GeometryData::KratosGeometryType::Kratos_Line2D4, "Line2D4"},
+        {GeometryData::KratosGeometryType::Kratos_Line2D5, "Line2D5"},
+        {GeometryData::KratosGeometryType::Kratos_Line3D2, "Line3D2"},
+        {GeometryData::KratosGeometryType::Kratos_Line3D3, "Line3D3"},
+        {GeometryData::KratosGeometryType::Kratos_Triangle2D3, "Triangle2D3"},
+        {GeometryData::KratosGeometryType::Kratos_Triangle2D6, "Triangle2D6"},
+        {GeometryData::KratosGeometryType::Kratos_Triangle2D10, "Triangle2D10"},
+        {GeometryData::KratosGeometryType::Kratos_Triangle2D15, "Triangle2D15"},
+        {GeometryData::KratosGeometryType::Kratos_Triangle3D3, "Triangle3D3"},
+        {GeometryData::KratosGeometryType::Kratos_Triangle3D6, "Triangle3D6"},
+        {GeometryData::KratosGeometryType::Kratos_Quadrilateral2D4, "Quadrilateral2D4"},
+        {GeometryData::KratosGeometryType::Kratos_Quadrilateral2D8, "Quadrilateral2D8"},
+        {GeometryData::KratosGeometryType::Kratos_Quadrilateral2D9, "Quadrilateral2D9"},
+        {GeometryData::KratosGeometryType::Kratos_Quadrilateral3D4, "Quadrilateral3D4"},
+        {GeometryData::KratosGeometryType::Kratos_Quadrilateral3D8, "Quadrilateral3D8"},
+        {GeometryData::KratosGeometryType::Kratos_Quadrilateral3D9, "Quadrilateral3D9"},
+        {GeometryData::KratosGeometryType::Kratos_Tetrahedra3D4, "Tetrahedra3D4"},
+        {GeometryData::KratosGeometryType::Kratos_Tetrahedra3D10, "Tetrahedra3D10"},
+        {GeometryData::KratosGeometryType::Kratos_Hexahedra3D8, "Hexahedra3D8"},
+        {GeometryData::KratosGeometryType::Kratos_Hexahedra3D20, "Hexahedra3D20"},
+        {GeometryData::KratosGeometryType::Kratos_Hexahedra3D27, "Hexahedra3D27"},
+        {GeometryData::KratosGeometryType::Kratos_Prism3D6, "Prism3D6"},
+        {GeometryData::KratosGeometryType::Kratos_Prism3D15, "Prism3D15"},
+        {GeometryData::KratosGeometryType::Kratos_Pyramid3D5, "Pyramid3D5"},
+        {GeometryData::KratosGeometryType::Kratos_Pyramid3D13, "Pyramid3D13"}
+    };
+    
+    ///@}
+
+private:
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    /**
+     * @brief This method initializes the member variables
+     */
+    void InitializeMemberVariables();
 
     ///@}
 
