7e1f48e4973b0ca9bc41cdcf710482210d4711dc
Fabian Meister <f.meister@tu-bs.de>
2023-02-03 14:08:42 +0100

    Added displacement response value calculation

--- applications/OptimizationApplication/python_scripts/responses/displacement_response_function.py
+++ applications/OptimizationApplication/python_scripts/responses/displacement_response_function.py
@@ -1,57 +1,51 @@
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.OptimizationApplication.optimization_info import OptimizationInfo
+from KratosMultiphysics.OptimizationApplication.execution_policies.execution_policy_wrapper import ExecutionPolicyWrapper
 from KratosMultiphysics.OptimizationApplication.responses.response_function import ResponseFunction
-from KratosMultiphysics.OptimizationApplication.utilities.container_data import ContainerData
 
 
 class DisplacementResponseFunction(ResponseFunction):
-    def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, _: OptimizationInfo):
+    def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, optimization_info: OptimizationInfo):
         default_settings = Kratos.Parameters("""{
-            "evaluated_model_part_name": "PLEASE_PROVIDE_A_MODEL_PART_NAME"
+            "evaluated_model_part_name": "PLEASE_PROVIDE_A_MODEL_PART_NAME",
+            "primal_analysis_name"     : "",
+            "perturbation_size"        : 1e-8
         }""")
         parameters.ValidateAndAssignDefaults(default_settings)
         self.model_part = model[parameters["evaluated_model_part_name"].GetString()]
+        self.primal_analysis_execution_policy_wrapper: ExecutionPolicyWrapper = optimization_info.GetOptimizationRoutine(ExecutionPolicyWrapper, parameters["primal_analysis_name"].GetString())
+        self.perturbation_size = parameters["perturbation_size"].GetDouble()
 
     def Check(self):
-        data_communicator = self.model_part.GetCommunicator().GetDataCommunicator()
-        if not KratosOA.OptimizationUtils.IsVariableExistsInAllContainerProperties(self.model_part.Elements, Kratos.YOUNG_MODULUS, data_communicator):
-            raise RuntimeError(f"Some elements' properties in {self.model_part.FullName()} does not have YOUNG_MODULUS variable.")
-
-        if not KratosOA.OptimizationUtils.AreAllEntitiesOfSameGeometryType(self.model_part.Elements, data_communicator):
-            raise RuntimeError(
-                f"{self.model_part.FullName()} has elements with different geometry types. Please break down this response to SumResponseFunction where each sub response function only has elements with one geometry type.")
+        pass
 
     def CalculateValue(self) -> float:
-
         # execute the primal analysis
         self.primal_analysis_execution_policy_wrapper.Execute()
 
-        print(self.model_part)
-        # computes the strain energy
-        return KratosOA.LinearStrainEnergyResponseUtilities.CalculateStrainEnergy(self.model_part)
+        nodes = self.model_part.GetNodes()
+        response_value = 0
+        for i, node in enumerate(nodes):
+            response_value += abs(node.GetSolutionStepValue(Kratos.DISPLACEMENT_X))
+            response_value += abs(node.GetSolutionStepValue(Kratos.DISPLACEMENT_Y))
+            response_value += abs(node.GetSolutionStepValue(Kratos.DISPLACEMENT_Z))
 
-        # print(f"{self}")
+        print(response_value)
+        return response_value
 
-        # return KratosOA.MassResponseUtilities.CalculateMass(self.model_part)
-
-    def CalculateSensitivity(self, sensitivity_variable, sensitivity_container: ContainerData):
-        if sensitivity_variable == Kratos.SHAPE_SENSITIVITY and sensitivity_container.GetContainerTpe() == ContainerData.ContainerEnum.NODES:
-            KratosOA.MassResponseUtilities.CalculateMassShapeSensitivity(sensitivity_container.GetModelPart(), sensitivity_variable)
-        elif sensitivity_variable == KratosOA.DENSITY_SENSITIVITY and sensitivity_container.GetContainerTpe() == ContainerData.ContainerEnum.ELEMENT_PROPERTIES:
-            KratosOA.MassResponseUtilities.CalculateMassDensitySensitivity(sensitivity_container.GetModelPart(), sensitivity_variable)
-        elif sensitivity_variable == KratosOA.THICKNESS_SENSITIVITY and sensitivity_container.GetContainerTpe() == ContainerData.ContainerEnum.ELEMENT_PROPERTIES:
-            KratosOA.MassResponseUtilities.CalculateMassThicknessSensitivity(sensitivity_container.GetModelPart(), sensitivity_variable)
-        elif sensitivity_variable == KratosOA.CROSS_AREA_SENSITIVITY and sensitivity_container.GetContainerTpe() == ContainerData.ContainerEnum.ELEMENT_PROPERTIES:
-            KratosOA.MassResponseUtilities.CalculateMassCrossAreaSensitivity(sensitivity_container.GetModelPart(), sensitivity_variable)
+    def CalculateSensitivity(self, sensitivity_variable: any, sensitivity_model_part: Kratos.ModelPart):
+        # computes strain energy derivatives
+        if sensitivity_variable == Kratos.SHAPE_SENSITIVITY:
+            KratosOA.LinearStrainEnergyResponseUtilities.CalculateStrainEnergyShapeSensitivity(sensitivity_model_part, self.perturbation_size, sensitivity_variable)
+        elif sensitivity_variable in [KratosOA.DENSITY_SENSITIVITY, KratosOA.YOUNG_MODULUS_SENSITIVITY, KratosOA.POISSON_RATIO_SENSITIVITY]:
+            primal_variable = Kratos.KratosGlobals.GetVariable(sensitivity_variable.Name()[:-12])
+            KratosOA.LinearStrainEnergyResponseUtilities.CalculateStrainEnergyElementPropertiesSensitivity(sensitivity_model_part, self.perturbation_size, primal_variable, sensitivity_variable)
         else:
-            msg = f"Unsupported sensitivity w.r.t. {sensitivity_variable.Name()} requested for {str(sensitivity_container)}."
+            msg = f"Unsupported sensitivity w.r.t. {sensitivity_variable.Name()} requested for {sensitivity_model_part.FullName()}."
             msg += "Followings are supported options:"
-            msg += "\n\tSHAPE_SENSITIVITY for NODES container"
-            msg += "\n\tDENSITY_SENSITIVITY for ELEMENT_PROPERTIES container"
-            msg += "\n\tTHICKNESS_SENSITIVITY for ELEMENT_PROPERTIES container"
-            msg += "\n\tCROSS_AREA_SENSITIVITY for ELEMENT_PROPERTIES container"
+            msg += "\n\tSHAPE_SENSITIVITY"
+            msg += "\n\tDENSITY_SENSITIVITY"
+            msg += "\n\tYOUNG_MODULUS_SENSITIVITY"
+            msg += "\n\tPOISSON_RATIO_SENSITIVITY"
             raise RuntimeError(msg)
-
-        # read the computed sensitivities to the vector
-        sensitivity_container.ReadDataFromContainerVariable(sensitivity_variable)
