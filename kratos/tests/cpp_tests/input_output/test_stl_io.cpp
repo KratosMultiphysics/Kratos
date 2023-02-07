9178386cba9b3315d33444a1c7c2a96b46b97d30
Qiukai Lu <qiukailu@gmail.com>
2023-02-07 10:54:07 -0600

    placeholder for cpp test

--- kratos/tests/cpp_tests/input_output/test_stl_io.cpp
+++ kratos/tests/cpp_tests/input_output/test_stl_io.cpp
@@ -24,17 +24,17 @@ namespace Kratos::Testing {
 
 KRATOS_TEST_CASE_IN_SUITE(ReadTriangleFromSTL, KratosCoreFastSuite)
 {
-std::stringstream* p_input = new std::stringstream(R"input(
-solid 1 triangle
-    facet normal  1.000000 0.000000 0.000000 
-        outer loop 
-           vertex 0.1 -2.56114e-08 0.1
-           vertex 0.1 -0.499156 -0.0352136
-           vertex 0.1 -0.473406 -0.0446259
-        endloop 
-    endfacet 
-endsolid 1 triangle
-)input");  
+    std::stringstream* p_input = new std::stringstream(R"input(
+    solid 1 triangle
+        facet normal  1.000000 0.000000 0.000000 
+            outer loop 
+            vertex 0.1 -2.56114e-08 0.1
+            vertex 0.1 -0.499156 -0.0352136
+            vertex 0.1 -0.473406 -0.0446259
+            endloop 
+        endfacet 
+    endsolid 1 triangle
+    )input");  
 
     Model current_model;
     ModelPart& r_model_part = current_model.CreateModelPart("Main");
@@ -51,31 +51,31 @@ endsolid 1 triangle
 
 KRATOS_TEST_CASE_IN_SUITE(ReadMultipleTrianglesFromSTL, KratosCoreFastSuite)
 {
-std::stringstream* p_input = new std::stringstream(R"input(
-solid 3 triangles
-    facet normal  1.000000 0.000000 0.000000 
-        outer loop 
-           vertex 0.1 -2.56114e-08 0.1
-           vertex 0.1 -0.499156 -0.0352136
-           vertex 0.1 -0.473406 -0.0446259
-        endloop 
-    endfacet 
-    facet normal  1.000000 -0.000000 0.000000 
-        outer loop 
-           vertex 0.1 -0.473406 -0.0446259
-           vertex 0.1 -0.447464 -0.0534931
-           vertex 0.1 -2.56114e-08 0.1
-        endloop 
-    endfacet 
-    facet normal  1.000000 0.000000 0.000000 
-        outer loop 
-           vertex 0.1 -0.6 0.1
-           vertex 0.1 -0.524702 -0.0252604
-           vertex 0.1 -0.499156 -0.0352136
-        endloop 
-    endfacet 
-endsolid 3 triangles
-)input");  
+    std::stringstream* p_input = new std::stringstream(R"input(
+    solid 3 triangles
+        facet normal  1.000000 0.000000 0.000000 
+            outer loop 
+            vertex 0.1 -2.56114e-08 0.1
+            vertex 0.1 -0.499156 -0.0352136
+            vertex 0.1 -0.473406 -0.0446259
+            endloop 
+        endfacet 
+        facet normal  1.000000 -0.000000 0.000000 
+            outer loop 
+            vertex 0.1 -0.473406 -0.0446259
+            vertex 0.1 -0.447464 -0.0534931
+            vertex 0.1 -2.56114e-08 0.1
+            endloop 
+        endfacet 
+        facet normal  1.000000 0.000000 0.000000 
+            outer loop 
+            vertex 0.1 -0.6 0.1
+            vertex 0.1 -0.524702 -0.0252604
+            vertex 0.1 -0.499156 -0.0352136
+            endloop 
+        endfacet 
+    endsolid 3 triangles
+    )input");  
 
     Model current_model;
     ModelPart& r_model_part = current_model.CreateModelPart("Main");
@@ -88,6 +88,28 @@ endsolid 3 triangles
     KRATOS_CHECK(r_model_part.HasSubModelPart("3 triangles"));
     KRATOS_CHECK_EQUAL(r_model_part.GetSubModelPart("3 triangles").NumberOfNodes(), 9);
     KRATOS_CHECK_EQUAL(r_model_part.GetSubModelPart("3 triangles").NumberOfElements(), 3);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(WriteTriangleToSTL, KratosCoreFastSuite)
+{
+    Model current_model;
+    ModelPart& r_model_part = current_model.CreateModelPart("Main");
+
+    // create a few nodes
+    r_model_part.CreateNewNode(1, 0.1, -2.56114e-08, 0.1);
+    r_model_part.CreateNewNode(2, 0.1, -0.499156, -0.0352136);
+    r_model_part.CreateNewNode(3, 0.1, -0.473406, -0.0446259);
+
+    // create a triangle element
+    Properties::Pointer p_properties(new Properties(0));
+    r_model_part.CreateNewElement("Element3D3N", 101, {1, 2, 3}, p_properties);
+
+    // 
+    StlIO stl_io ("test_stl_write.stl");
+    stl_io.WriteModelPart(r_model_part);
+
+    // intentional failure
+    KRATOS_CHECK_EQUAL(true, false);
 }
 
 }  // namespace Kratos::Testing.
