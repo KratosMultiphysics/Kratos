ab2ccddc5c9dbefbcd70d5ddd1c249679447d73b
jrubio <jrubio@altair.com>
2023-02-10 12:45:24 +0100

    minor fix

--- kratos/input_output/stl_io.cpp
+++ kratos/input_output/stl_io.cpp
@@ -45,7 +45,7 @@ StlIO::StlIO(std::iostream* pInputStream)
 
 void StlIO::ReadModelPart(ModelPart & rThisModelPart)
 {
-    if(!rThisModelPart.HasProperties(0))
+    if(!rThisModelPart.RecursivelyHasProperties(0))
         rThisModelPart.CreateNewProperties(0);
         
     while(!mpInputStream->eof())
