71b0667a185f8d6c04d5575c0239a33f5b964f27
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-02-15 10:26:01 +0100

    Extend test

--- kratos/tests/test_kratos_globals.py
+++ kratos/tests/test_kratos_globals.py
@@ -44,6 +44,56 @@ class TestKratosGlobals(KratosUnittest.TestCase):
         law_name = "ConstitutiveLaw"
         KratosMultiphysics.KratosGlobals.GetConstitutiveLaw(law_name)
 
+    def test_has_modeler(self):
+        modeler_name = "Modeler"
+        self.assertTrue(KratosMultiphysics.KratosGlobals.HasModeler(modeler_name))
+        modeler_name = "Modeler2"
+        self.assertFalse(KratosMultiphysics.KratosGlobals.HasModeler(modeler_name))
+
+    def test_get_modeler(self):
+        modeler_name = "Modeler"
+        KratosMultiphysics.KratosGlobals.GetModeler(modeler_name)
+
+    def test_has_geometry(self):
+        geometry_name = "Line2D2"
+        self.assertTrue(KratosMultiphysics.KratosGlobals.HasGeometry(geometry_name))
+        geometry_name = "Lime2D2"
+        self.assertFalse(KratosMultiphysics.KratosGlobals.HasGeometry(geometry_name))
+
+    def test_get_geometry(self):
+        geometry_name = "Line2D2"
+        KratosMultiphysics.KratosGlobals.GetGeometry(geometry_name)
+
+    def test_has_condition(self):
+        condition_name = "PointCondition3D1N"
+        self.assertTrue(KratosMultiphysics.KratosGlobals.HasCondition(condition_name))
+        condition_name = "PointKondition3D1N"
+        self.assertFalse(KratosMultiphysics.KratosGlobals.HasCondition(condition_name))
+
+    def test_get_condition(self):
+        condition_name = "PointCondition3D1N"
+        KratosMultiphysics.KratosGlobals.GetCondition(condition_name)
+
+    def test_has_element(self):
+        element_name = "Element3D1N"
+        self.assertTrue(KratosMultiphysics.KratosGlobals.HasElement(element_name))
+        element_name = "Element3D1Ñ"
+        self.assertFalse(KratosMultiphysics.KratosGlobals.HasElement(element_name))
+
+    def test_get_element(self):
+        element_name = "Element3D1N"
+        KratosMultiphysics.KratosGlobals.GetElement(element_name)
+
+    def test_has_master_slave_constraint(self):
+        constraint_name = "MasterSlaveConstraint"
+        self.assertTrue(KratosMultiphysics.KratosGlobals.HasMasterSlaveConstraint(constraint_name))
+        constraint_name = "MasterSlaveKonsstraint"
+        self.assertFalse(KratosMultiphysics.KratosGlobals.HasMasterSlaveConstraint(constraint_name))
+
+    def test_get_master_slave_constraint(self):
+        constraint_name = "MasterSlaveConstraint"
+        KratosMultiphysics.KratosGlobals.GetMasterSlaveConstraint(constraint_name)
+
 if __name__ == '__main__':
     KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
     KratosUnittest.main()
