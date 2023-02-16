734df471a4d68f008e44094417c545d5ba4cb33d
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-02-16 11:15:18 +0100

    Revert "Suggested refactor"
    This reverts commit 2b3fd09a65d3d89ac6c5d19e272492ba70b4249f.

--- kratos/tests/cpp_tests/processes/test_calculate_distance_to_path_process.cpp
+++ kratos/tests/cpp_tests/processes/test_calculate_distance_to_path_process.cpp
@@ -28,10 +28,6 @@ namespace Kratos::Testing
 
     KRATOS_TEST_CASE_IN_SUITE(FastMinimalDistanceOnLineWithRadius, KratosCoreFastSuite)
 	{
-    	Model current_model;
-        Parameters empty_parameters(R"({})");
-        auto process = CalculateDistanceToPathProcess<true>(current_model, empty_parameters);
-
         double distance;
         double radius = 0.0;
         auto line = Kratos::make_shared<Line3D2<Node<3>>>(
@@ -40,27 +36,27 @@ namespace Kratos::Testing
         );
         Point point1(0.0,0.0,0.1);
         distance = GeometricalProjectionUtilities::FastMinimalDistanceOnLine(*line, point1);
-        auto distance_computed_type = process.FastMinimalDistanceOnLineWithRadius(distance, *line, point1, radius);
+        auto distance_computed_type = CalculateDistanceToPathProcess<true>::FastMinimalDistanceOnLineWithRadius(distance, *line, point1, radius);
         KRATOS_CHECK_NEAR(distance, 0.1, TOLERANCE_DISTANCE_PATH);
         KRATOS_CHECK_EQUAL(distance_computed_type, CalculateDistanceToPathProcess<true>::DistanceComputed::NO_RADIUS);
 
         radius = 0.01;
         Point point2(0.0,0.0,0.1);
         distance = GeometricalProjectionUtilities::FastMinimalDistanceOnLine(*line, point2);
-        distance_computed_type = process.FastMinimalDistanceOnLineWithRadius(distance, *line, point2, radius);
+        distance_computed_type = CalculateDistanceToPathProcess<true>::FastMinimalDistanceOnLineWithRadius(distance, *line, point2, radius);
         KRATOS_CHECK_NEAR(distance, 0.09, TOLERANCE_DISTANCE_PATH);
         KRATOS_CHECK_EQUAL(distance_computed_type, CalculateDistanceToPathProcess<true>::DistanceComputed::RADIUS_PROJECTED);
 
         Point point3(-0.1,0.0,0.1);
         distance = GeometricalProjectionUtilities::FastMinimalDistanceOnLine(*line, point3);
-        distance_computed_type = process.FastMinimalDistanceOnLineWithRadius(distance, *line, point3, radius);
+        distance_computed_type = CalculateDistanceToPathProcess<true>::FastMinimalDistanceOnLineWithRadius(distance, *line, point3, radius);
         KRATOS_CHECK_NEAR(distance, std::sqrt(std::pow(0.09, 2) * 2), TOLERANCE_DISTANCE_PATH);
         KRATOS_CHECK_EQUAL(distance_computed_type, CalculateDistanceToPathProcess<true>::DistanceComputed::RADIUS_NOT_PROJECTED_OUTSIDE);
 
         radius = 0.1;
         Point point4(-0.1,0.0,0.09);
         distance = GeometricalProjectionUtilities::FastMinimalDistanceOnLine(*line, point4);
-        distance_computed_type = process.FastMinimalDistanceOnLineWithRadius(distance, *line, point4, radius);
+        distance_computed_type = CalculateDistanceToPathProcess<true>::FastMinimalDistanceOnLineWithRadius(distance, *line, point4, radius);
         KRATOS_CHECK_NEAR(distance, -(std::sqrt(std::pow(0.01, 2) + std::pow(0.1, 2))), TOLERANCE_DISTANCE_PATH);
         KRATOS_CHECK_EQUAL(distance_computed_type, CalculateDistanceToPathProcess<true>::DistanceComputed::RADIUS_NOT_PROJECTED_INSIDE);
     }
