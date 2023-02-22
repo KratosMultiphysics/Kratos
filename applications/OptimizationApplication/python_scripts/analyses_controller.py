d8b35dff7abe72ca38b9a37f22f744b50d8f878e
sunethwarna <suneth.warna@gmail.com>
2023-02-22 12:29:39 +0100

    rename wrappers to decorators

--- applications/OptimizationApplication/python_scripts/analyses_controller.py
+++ applications/OptimizationApplication/python_scripts/analyses_controller.py
@@ -26,20 +26,20 @@ class AnalysesController:
         self.model_parts_controller = model_parts_controller
         self.model = model
 
-        self.execution_policy_wrappers = {}
+        self.execution_policy_decorators = {}
         for execution_policy_settings in self.execution_policies_settings:
             execution_policy_wrapper = ExecutionPolicyDecorator(self.model, execution_policy_settings, OptimizationInfo())
-            if not execution_policy_wrapper.GetExecutionPolicyName() in self.execution_policy_wrappers.keys():
-                self.execution_policy_wrappers[execution_policy_wrapper.GetExecutionPolicyName()] = execution_policy_wrapper
+            if not execution_policy_wrapper.GetExecutionPolicyName() in self.execution_policy_decorators.keys():
+                self.execution_policy_decorators[execution_policy_wrapper.GetExecutionPolicyName()] = execution_policy_wrapper
             else:
                 raise RuntimeError(f"Found already existing execution policy with the name \"{execution_policy_wrapper.GetExecutionPolicyName()}\". Please provide unique names.")
 
     # --------------------------------------------------------------------------
     def GetExecutionPolicyDecorator(self, execution_policy_name: str) -> ExecutionPolicyDecorator:
-        if not execution_policy_name in self.execution_policy_wrappers.keys():
+        if not execution_policy_name in self.execution_policy_decorators.keys():
             raise RuntimeError("AnalysesController: Try to get an execution policy {} which does not exist.".format(execution_policy_name))
         else:
-            return self.execution_policy_wrappers[execution_policy_name]
+            return self.execution_policy_decorators[execution_policy_name]
 
     # --------------------------------------------------------------------------
     def GetAnalysis(self, execution_policy_name: str):
@@ -47,7 +47,7 @@ class AnalysesController:
 
     # --------------------------------------------------------------------------
     def Initialize(self):
-        for execution_policy_wrapper in self.execution_policy_wrappers.values():
+        for execution_policy_wrapper in self.execution_policy_decorators.values():
             execution_policy_name = execution_policy_wrapper.GetExecutionPolicyName()
             with TimeLogger(self.__class__.__name__, f"Initializing {execution_policy_name}...", f"Finished initializing {execution_policy_name}"):
                 execution_policy_wrapper.ExecuteInitialize()
