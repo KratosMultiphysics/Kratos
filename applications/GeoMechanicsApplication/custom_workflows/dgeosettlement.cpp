67ca866315795313ad2d5aff2b7095997a0fc12d
mcgicjn2 <jonathan.nuttall@deltares.nl>
2023-02-27 15:06:08 +0100

    Commented out standard kratos execution call templated
    Commented out the expected main call in the workflow, as no parameters are delivered to it yet

--- applications/GeoMechanicsApplication/custom_workflows/dgeosettlement.cpp
+++ applications/GeoMechanicsApplication/custom_workflows/dgeosettlement.cpp
@@ -14,7 +14,6 @@
 
 #include "dgeosettlement.h"
 
-
 namespace Kratos
 {
     KratosGeoSettlement::KratosGeoSettlement()
@@ -22,13 +21,13 @@ namespace Kratos
     {
     }
 
-    int execute_application(std::string workingDirectory, std::string parameterName,
+    int KratosGeoSettlement::execute_application(std::string workingDirectory, std::string parameterName,
         std::function<void(char*)> logCallback,
         std::function<void(double)> reportProgress,
         std::function<void(char*)> reportTextualProgress,
         std::function<bool()> shouldCancel)
     {
-    	execute_kratos_calculation(model_part, processes, p_solving_strategy);
+        // execute_kratos_calculation(model_part, processes, p_solving_strategy);
         return 0;
     }
 
