0816e1b16c882028cdeaf04ac6aab5d1207d5cf1
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2023-02-28 09:43:47 +0100

    Update applications/OptimizationApplication/python_scripts/responses/response_function.py
    Co-authored-by: Fabian Meister <39196745+FMeister@users.noreply.github.com>

--- applications/OptimizationApplication/python_scripts/responses/response_function.py
+++ applications/OptimizationApplication/python_scripts/responses/response_function.py
@@ -8,7 +8,7 @@ class ResponseFunction(Kratos.Process):
         raise NotImplementedError("Calling base class ResponseFunction::Check method. Please implement in the derived class.")
 
     def CalculateValue(self) -> float:
-        raise NotImplementedError("Calling base class ResponseFunction::CalculateValue method. Please implement in the derrived class.")
+        raise NotImplementedError("Calling base class ResponseFunction::CalculateValue method. Please implement in the derived class.")
 
     def CalculateSensitivity(self, sensitivity_variable: any, sensitivity_model_part: Kratos.ModelPart):
         raise NotImplementedError("Calling base class ResponseFunction::CalculateSensitivity method. Please implement in the derrived class.")
