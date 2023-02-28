8b331e09eec1d5b19ab78475a41da87cf98d9141
Alejandro <alejandro.cornejo23@gmail.com>
2023-02-28 10:15:59 +0400

    cleaning

--- applications/ConstitutiveLawsApplication/python_scripts/add_texture_normal_distribution_process.py
+++ applications/ConstitutiveLawsApplication/python_scripts/add_texture_normal_distribution_process.py
@@ -1,5 +1,4 @@
 import KratosMultiphysics as KM
-import KratosMultiphysics.ConstitutiveLawsApplication as CLApp
 import numpy as np
 
 def Factory(settings, Model):
@@ -11,8 +10,7 @@ def Factory(settings, Model):
 
 class AddTextureNormalDistributionProcess(KM.Process):
 
-    """This process sets the pre-stressing imposed strain according to a load factor (can depend on time) inside a defined Interval.
-    This process should be called together with set_up_pre_stressed_oriented_composite_materials.py in order to work properly (it sets the initial value of imposed strain)
+    """This process perturbates the coordinates of the nodes of the designated submodelpart according to a normal distribution with [mu,sigma] by using numpy capabilities (https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html)
 
 
     Public member variables:
@@ -36,22 +34,21 @@ class AddTextureNormalDistributionProcess(KM.Process):
         {
             "help"                       : "This process perturbates the coordinates of the nodes according to a normal distribution",
             "model_part_name"            : "please_specify_model_part_name",
-            "normal_mu"                  : "0.0",
+            "normal_mu"                  : 0.0,
             "normal_sigma"               : 0.1,
-            "print_modified_coordinates" : true
+            "print_modified_coordinates" : true,
+            "echo_level"                 : 1
         }
         """
         )
         settings.ValidateAndAssignDefaults(default_settings)
 
-        if not settings.Has("load_factor"):
-            raise RuntimeError("Please specify the value to set the vector to. Example:\n" + '{\n\t"load_factor" : 0.1*t*x\n}\n')
-
         self.model_part = Model[settings["model_part_name"].GetString()]
-        # self.interval = KM.IntervalUtility(settings)
-        # self.load_factor_function = self.CreateFunction(settings["load_factor"])
+        self.mu    = settings["normal_mu"].GetDouble()    # average
+        self.sigma = settings["normal_sigma"].GetDouble() # standard deviation
+        self.print_coords = settings["print_modified_coordinates"].GetBool()
         self.echo = settings["echo_level"].GetInt()
-        # self.old_load_factor = 1.0e30
+
 
     def ExecuteInitializeSolutionStep(self):
         """This method is executed in order to initialize the current step
@@ -59,20 +56,6 @@ class AddTextureNormalDistributionProcess(KM.Process):
         Keyword arguments:
         self -- It signifies an instance of a class.
         """
-        # current_time = self.model_part.ProcessInfo[KM.TIME]
-        # step = self.model_part.ProcessInfo[KM.STEP]
 
-        # if self.interval.IsInInterval(current_time):
-        #     current_load_factor = self.load_factor_function.CallFunction(0,0,0,current_time,0,0,0)
-        #     if self.echo > 0:
-        #         KM.Logger.PrintInfo("ApplyPreStressingImposedStrainProcess", "Applying a load factor of " + "{0:.4e}".format(current_load_factor).rjust(11) + ".")
-        #     for element in self.model_part.Elements:
-        #         if element.Has(CLApp.SERIAL_PARALLEL_IMPOSED_STRAIN): # With previous imposed strain
-        #             current_strain = element.GetValue(CLApp.SERIAL_PARALLEL_IMPOSED_STRAIN)
-        #             if step == 1:
-        #                 element.SetValue(CLApp.SERIAL_PARALLEL_IMPOSED_STRAIN, current_strain * current_load_factor)
-        #             else:
-        #                 element.SetValue(CLApp.SERIAL_PARALLEL_IMPOSED_STRAIN, current_strain * current_load_factor / self.old_load_factor)
-        #     self.old_load_factor = current_load_factor
 
 
