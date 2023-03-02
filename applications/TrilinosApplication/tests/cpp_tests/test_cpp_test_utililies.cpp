1e1c953a2c5fb4288aa50b40a14a9c808d869c00
Vicente Mataix Ferándiz <vmataix@altair.com>
2023-03-02 16:09:39 +0000

    More test

--- applications/TrilinosApplication/tests/cpp_tests/test_cpp_test_utililies.cpp
+++ applications/TrilinosApplication/tests/cpp_tests/test_cpp_test_utililies.cpp
@@ -47,4 +47,31 @@ KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(TrilinosCPPTestUtilitiesGenerateSparseMatr
     TrilinosCPPTestUtilities::CheckSparseMatrix(A, row_indexes, column_indexes, values);
 }
 
+KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(TrilinosCPPTestUtilitiesGenerateSparseMatrixRealCase, KratosTrilinosApplicationMPITestSuite)
+{
+    // The data communicator
+    const auto& r_comm = Testing::GetDefaultDataCommunicator();
+
+    // Size of the system
+    const int size = 6;
+
+    // Generate A matrix
+    std::vector<int> row_indexes = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5};
+    std::vector<int> column_indexes = {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5};
+    std::vector<double> values = {2069000000.0, 0.0, -2069000000.0, 0.0, 0.0, 0.0, 0.0, 0.0, -2069000000.0, 0.0, 4138000000.0, 0.0, -2069000000.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -2069000000.0, 0.0, 2069000000.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    auto A = TrilinosCPPTestUtilities::GenerateSparseMatrix(r_comm, size, row_indexes, column_indexes, values);
+
+    // Check
+    TrilinosCPPTestUtilities::CheckSparseMatrix(A, row_indexes, column_indexes, values);
+
+    // Generate T matrix
+    row_indexes = {0,1,2,3,4,4,5};
+    column_indexes = {0,1,2,3,2,4,5};
+    values = {1,1,1,1,1,0,1};
+    auto T = TrilinosCPPTestUtilities::GenerateSparseMatrix(r_comm, size, row_indexes, column_indexes, values);
+
+    // Check
+    TrilinosCPPTestUtilities::CheckSparseMatrix(T, row_indexes, column_indexes, values);
+}
+
 } // namespace Kratos::Testing
