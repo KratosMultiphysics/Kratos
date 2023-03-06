5b5a8905889ccd8eace5eb5ae47d68a75cc5040e
sunethwarna <suneth.warna@gmail.com>
2023-03-06 08:34:25 +0100

    update mass response function

--- applications/OptimizationApplication/python_scripts/responses/mass_response_function.py
+++ applications/OptimizationApplication/python_scripts/responses/mass_response_function.py
@@ -5,19 +5,28 @@ from KratosMultiphysics.OptimizationApplication.responses.response_function impo
 class MassResponseFunction(ResponseFunction):
     def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, _):
         default_settings = Kratos.Parameters("""{
-            "evaluated_model_part_name": "PLEASE_PROVIDE_A_MODEL_PART_NAME"
+            "evaluated_model_part_names": [
+                "PLEASE_PROVIDE_A_MODEL_PART_NAME"
+            ]
         }""")
         parameters.ValidateAndAssignDefaults(default_settings)
-        self.model_part = model[parameters["evaluated_model_part_name"].GetString()]
 
-    def GetModelPart(self) -> Kratos.ModelPart:
-        return self.model_part
+        self.model_parts: 'list[Kratos.ModelPart]' = []
+        for model_part_name in parameters["evaluated_model_part_names"].GetStringArray():
+            self.model_parts.append(model[model_part_name])
+
+        if len(self.model_parts) == 0:
+            raise RuntimeError(f"No model parts were provided for MassResponseFunction.")
 
     def Check(self) -> None:
-        KratosOA.ResponseUtils.MassResponseUtils.Check(self.model_part)
+        for model_part in self.model_parts:
+            KratosOA.ResponseUtils.MassResponseUtils.Check(model_part)
 
     def CalculateValue(self) -> float:
-        return KratosOA.ResponseUtils.MassResponseUtils.CalculateValue(self.model_part)
+        value = 0.0
+        for model_part in self.model_parts:
+            value += KratosOA.ResponseUtils.MassResponseUtils.CalculateValue(model_part)
+        return value
 
     def CalculateSensitivity(self, sensitivity_variable: any, sensitivity_model_part: Kratos.ModelPart) -> None:
         KratosOA.ResponseUtils.MassResponseUtils.CalculateSensitivity(sensitivity_model_part, sensitivity_variable)
