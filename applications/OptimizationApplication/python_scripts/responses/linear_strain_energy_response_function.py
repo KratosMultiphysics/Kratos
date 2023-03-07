3614bdcec9430871dca64f6b9ac76289dc6b9c9a
sunethwarna <suneth.warna@gmail.com>
2023-03-07 15:20:27 +0100

    update response function with thickness sens.

--- applications/OptimizationApplication/python_scripts/responses/linear_strain_energy_response_function.py
+++ applications/OptimizationApplication/python_scripts/responses/linear_strain_energy_response_function.py
@@ -1,7 +1,5 @@
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
-from KratosMultiphysics.OptimizationApplication.utilities.optimization_info import OptimizationInfo
-from KratosMultiphysics.OptimizationApplication.execution_policies.execution_policy_decorator import ExecutionPolicyDecorator
 from KratosMultiphysics.OptimizationApplication.responses.response_function import ResponseFunction
 from KratosMultiphysics.OptimizationApplication.responses.response_function import SupportedSensitivityFieldVariableTypes
 
@@ -9,19 +7,17 @@ from KratosMultiphysics.OptimizationApplication.responses.response_function impo
 class LinearStrainEnergyResponseFunction(ResponseFunction):
     @classmethod
     def GetSensitivityFieldVariables(cls) -> 'list[SupportedSensitivityFieldVariableTypes]':
-        return [Kratos.SHAPE_SENSITIVITY, KratosOA.YOUNG_MODULUS_SENSITIVITY, KratosOA.POISSON_RATIO_SENSITIVITY]
+        return [Kratos.SHAPE_SENSITIVITY, KratosOA.YOUNG_MODULUS_SENSITIVITY, KratosOA.THICKNESS_SENSITIVITY, KratosOA.POISSON_RATIO_SENSITIVITY]
 
-    def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, optimization_info: OptimizationInfo):
+    def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, _):
         default_settings = Kratos.Parameters("""{
             "evaluated_model_part_names": [
                 "PLEASE_PROVIDE_A_MODEL_PART_NAME"
             ],
-            "primal_analysis_name"      : "",
             "perturbation_size"         : 1e-8
         }""")
         parameters.ValidateAndAssignDefaults(default_settings)
 
-        self.primal_analysis_execution_policy_wrapper: ExecutionPolicyDecorator = optimization_info.GetOptimizationProcess(ExecutionPolicyDecorator, parameters["primal_analysis_name"].GetString())
         self.perturbation_size = parameters["perturbation_size"].GetDouble()
 
         self.model_parts: 'list[Kratos.ModelPart]' = []
@@ -35,10 +31,6 @@ class LinearStrainEnergyResponseFunction(ResponseFunction):
         pass
 
     def CalculateValue(self) -> float:
-        # execute the primal analysis
-        self.primal_analysis_execution_policy_wrapper.Execute()
-
-        # computes the strain energy
         return KratosOA.ResponseUtils.LinearStrainEnergyResponseUtils.CalculateValue(self.model_parts)
 
     def CalculateSensitivity(self, sensitivity_model_part_variable_info: 'dict[Kratos.ModelPart, list[SupportedSensitivityFieldVariableTypes]]') -> None:
