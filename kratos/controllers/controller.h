212b814e1c9b540fa363d092ddc6c720123d8f73
Daniel Diez <ddiez@europe.altair.com>
2023-03-09 09:22:21 +0100

    Update kratos/controllers/controller.h
    Co-authored-by: Suneth Warnakulasuriya <suneth.warna@gmail.com>

--- kratos/controllers/controller.h
+++ kratos/controllers/controller.h
@@ -75,7 +75,7 @@ public:
     ///@{
 
     /**
-     * @brief This method creates an pointer of the controlle
+     * @brief This method creates an pointer of the controller
      * @details We consider as input, a Model and a set of Parameters for the sake of generality
      * @warning Must be overrided in controller implementation
      * @param rModel The model to be consider
