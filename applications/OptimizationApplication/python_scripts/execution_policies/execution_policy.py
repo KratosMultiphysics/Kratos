3c64537bafd7a1a475acc6f17f7890b570b25c09
sunethwarna <suneth.warna@gmail.com>
2023-03-09 16:12:36 +0100

    update exec policy to return Analysis model part

--- applications/OptimizationApplication/python_scripts/execution_policies/execution_policy.py
+++ applications/OptimizationApplication/python_scripts/execution_policies/execution_policy.py
@@ -4,5 +4,8 @@ class ExecutionPolicy(Kratos.Process):
     def __init__(self):
         super().__init__()
 
+    def GetAnalysisModelPart(self):
+        raise NotImplementedError("Calling base class ExecutionPolicy::GetAnalysisModelPart method. This should be implemented in the derived class.")
+
     def Execute(self):
         raise NotImplementedError("Calling base class ExecutionPolicy::Execute method. This should be implemented in the derived class.")
