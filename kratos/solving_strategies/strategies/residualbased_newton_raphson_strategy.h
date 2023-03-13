bc16d8b553cfd585cc273971b983f53399abdbef
Alejandro <alejandro.cornejo23@gmail.com>
2023-03-13 17:46:24 +0400

    using mStiffnessMatrixIsBuilt now

--- kratos/solving_strategies/strategies/residualbased_newton_raphson_strategy.h
+++ kratos/solving_strategies/strategies/residualbased_newton_raphson_strategy.h
@@ -921,8 +921,9 @@ class ResidualBasedNewtonRaphsonStrategy
 
 
         // Function to perform the building and the solving phase.
-        if ((GetUseInitialStiffnessFlag() && r_model_part.GetProcessInfo()[STEP] == 1)) {
+        if ((GetUseInitialStiffnessFlag() && BaseType::mStiffnessMatrixIsBuilt == false)) {
             p_builder_and_solver->BuildAndSolve(p_scheme, r_model_part, rA, rDx, rb);
+            SetStiffnessMatrixIsBuilt(true);
         } else if (GetUseInitialStiffnessFlag()) {
             TSparseSpace::SetToZero(rDx);  // Dx = 0.00;
             TSparseSpace::SetToZero(rb);
