72f2ea387375a8ca7b1b6414100b942ebeba5d70
sunethwarna <suneth.warna@gmail.com>
2023-03-13 14:33:44 +0100

    add mpi tests

---
+++ applications/OptimizationApplication/tests/test_OptimizationApplication_mpi.py
@@ -0,0 +1,46 @@
+# Importing the Kratos Library
+import KratosMultiphysics as KM
+
+if not KM.IsDistributedRun():
+    raise Exception("This test script can only be executed in MPI!")
+
+# Import Kratos "wrapper" for unittests
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+
+# Import the tests or test_classes to create the suits
+from test_model_part_utils import TestModelPartUtils
+
+def AssembleTestSuites():
+    ''' Populates the test suites to run.
+
+    Populates the test suites to run. At least, it should pupulate the suites:
+    "small", "nighlty" and "all"
+
+    Return
+    ------
+
+    suites: A dictionary of suites
+        The set of suites with its test_cases added.
+    '''
+    suites = KratosUnittest.KratosSuites
+
+    ### Small MPI tests ########################################################
+    smallMPISuite = suites['mpi_small']
+
+    # adding custom process tests
+    smallMPISuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestModelPartUtils]))
+
+    ### Nightly MPI tests ######################################################
+    nightlyMPISuite = suites['mpi_nightly']
+    nightlyMPISuite.addTests(smallMPISuite)
+
+    ### Full MPI set ###########################################################
+    allMPISuite = suites['mpi_all']
+    allMPISuite.addTests(nightlyMPISuite) # already contains the smallMPISuite
+
+    return suites
+
+
+if __name__ == '__main__':
+    KratosUnittest.runTests(AssembleTestSuites())
