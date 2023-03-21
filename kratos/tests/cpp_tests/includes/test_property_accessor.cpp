540b126a4ea26807daf60260527865c91e4cfc2c
Alejandro <alejandro.cornejo23@gmail.com>
2023-03-21 09:46:00 +0400

    dummy

---
+++ kratos/tests/cpp_tests/includes/test_property_accessor.cpp
@@ -0,0 +1,36 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:   BSD License
+//      Kratos default license: kratos/license.txt
+//
+//  Main authors:    Alejandro Cornejo
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "testing/testing.h"
+// #include "includes/kratos_flags.h"
+#include "includes/properties.h"
+
+namespace Kratos 
+{
+    namespace Testing 
+    {
+        /**
+        * Checks the correct work of the Has methods
+        */
+        KRATOS_TEST_CASE_IN_SUITE(PropertyAccessorSimpleProperties, KratosCoreFastSuite)
+        {
+
+        }
+
+
+    } // namespace Testing
+}  // namespace Kratos.
