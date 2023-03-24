d6212e7ae1dff71ee031fb6bf243f172a515b0d9
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-03-24 09:53:12 +0100

    Missing

--- kratos/sources/profiler.cpp
+++ kratos/sources/profiler.cpp
@@ -1,3 +1,15 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Máté Kelemen
+//
+
 // --- Core Includes ---
 #include "includes/define.h"
 #include "includes/kratos_parameters.h"
