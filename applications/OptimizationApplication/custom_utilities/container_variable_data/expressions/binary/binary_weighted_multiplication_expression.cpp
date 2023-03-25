f0a872c1593aaeacf93b35b759179b073b5fe32c
sunethwarna <suneth.warna@gmail.com>
2023-03-26 00:51:09 +0100

    add binary_weighted_expression

---
+++ applications/OptimizationApplication/custom_utilities/container_variable_data/expressions/binary/binary_weighted_multiplication_expression.cpp
@@ -0,0 +1,102 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: OptimizationApplication/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+// System includes
+#include <sstream>
+#include <vector>
+
+// Project includes
+
+// Application includes
+#include "custom_utilities/container_variable_data/expressions/literal/literal_flat_expression.h"
+
+// Include base h
+#include "binary_weighted_multiplication_expression.h"
+
+namespace Kratos {
+
+namespace BinaryExpressionHelperUtilities {
+using IndexType = std::size_t;
+
+std::string GetShape(const std::vector<IndexType>& rShape)
+{
+    std::stringstream msg;
+    msg << "[";
+    for (const auto v : rShape) {
+        msg << v << ",";
+    }
+    if (rShape.size() != 0) {
+        msg << "\b";
+    }
+
+    msg << "]";
+    return msg.str();
+}
+
+} // namespace BinaryExpressionHelperUtilities
+
+BinaryWeightedMultiplicationExpression::BinaryWeightedMultiplicationExpression(
+    Expression::Pointer pLeft,
+    Expression::Pointer pRight)
+    : mpLeft(pLeft),
+      mpRight(pRight)
+{
+    const auto& r_left_shape = mpLeft->GetShape();
+    const auto& r_right_shape = mpRight->GetShape();
+
+    KRATOS_ERROR_IF_NOT(mpRight->GetLocalSize() == 1 &&
+                        typeid(*mpRight) == typeid(LiteralFlatExpression(0, {})))
+        << "Binary weighted multiplication should have right hand with a "
+           "vector having shape of [1]. ["
+        << "lhs shape = " << BinaryExpressionHelperUtilities::GetShape(r_left_shape) << ", "
+        << "rhs shape = " << BinaryExpressionHelperUtilities::GetShape(r_right_shape) << " ].\n"
+        << "Expression:\n"
+        << "lhs = " << *mpLeft << "\n"
+        << "rhs = " << *mpRight << "\n";
+
+    KRATOS_ERROR_IF_NOT(mpLeft.get())
+        << "Binary operation is provided with uninitialized left hand side "
+           "expression.\n";
+
+    KRATOS_ERROR_IF_NOT(mpRight.get())
+        << "Binary operation is provided with uninitialized right hand side "
+           "expression.\n";
+}
+
+Expression::Pointer BinaryWeightedMultiplicationExpression::Create(
+    Expression::Pointer pLeft,
+    Expression::Pointer pRight)
+{
+    return Kratos::make_intrusive<BinaryWeightedMultiplicationExpression>(pLeft, pRight);
+}
+
+double BinaryWeightedMultiplicationExpression::Evaluate(
+    const IndexType EntityIndex,
+    const IndexType ComponentIndex) const
+{
+    return mpLeft->Evaluate(EntityIndex, ComponentIndex) *
+           mpRight->Evaluate(EntityIndex, 0);
+}
+
+const std::vector<std::size_t> BinaryWeightedMultiplicationExpression::GetShape() const
+{
+    return this->mpLeft->GetShape();
+}
+
+std::string BinaryWeightedMultiplicationExpression::Info() const
+{
+    std::stringstream msg;
+    msg << "(" << this->mpLeft->Info() << ":" << this->mpRight->Info() << ")";
+    return msg.str();
+}
+
+} // namespace Kratos
---
+++ applications/OptimizationApplication/custom_utilities/container_variable_data/expressions/binary/binary_weighted_multiplication_expression.h
@@ -0,0 +1,74 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: OptimizationApplication/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+#pragma once
+
+// System includes
+#include <vector>
+#include <string>
+
+// Project includes
+
+// Application includes
+#include "custom_utilities/container_variable_data/expressions/expression.h"
+
+namespace Kratos {
+
+///@name Kratos Classes
+///@{
+
+/**
+ * @brief Expression to compute weighted multiplication.
+ *
+ */
+class BinaryWeightedMultiplicationExpression : public Expression {
+public:
+    ///@name Life cycle
+    ///@{
+
+    BinaryWeightedMultiplicationExpression(
+        Expression::Pointer pLeft,
+        Expression::Pointer pRight);
+
+    BinaryWeightedMultiplicationExpression(const BinaryWeightedMultiplicationExpression& rOther) = delete;
+
+    ~BinaryWeightedMultiplicationExpression() override = default;
+
+    ///@}
+    ///@name Public operations
+    ///@{
+
+    static Expression::Pointer Create(
+        Expression::Pointer pLeft,
+        Expression::Pointer pRight);
+
+    double Evaluate(
+        const IndexType EntityIndex,
+        const IndexType ComponentIndex) const override;
+
+    const std::vector<IndexType> GetShape() const override;
+
+    std::string Info() const override;
+
+    ///@}
+protected:
+    ///@name Private member variables
+    ///@{
+
+    const Expression::Pointer mpLeft;
+
+    const Expression::Pointer mpRight;
+
+    ///@}
+};
+
+} // namespace Kratos
