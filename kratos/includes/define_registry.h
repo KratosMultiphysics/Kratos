03b24196ea5f5afa8897787ad79976110a36064e
Carlos Roig <roigcarlo@gmail.com>
2023-03-27 12:35:11 +0200

    Remove variadrics from proto register

--- kratos/includes/define_registry.h
+++ kratos/includes/define_registry.h
@@ -40,14 +40,14 @@
  * the dispatcher function directly inside the std::move would break current code as the compiler will deduce the type
  * of the lambda function to something that is not an std::function.
  */
-#define KRATOS_REGISTRY_ADD_PROTOTYPE(NAME, X, ...)                                       \
+#define KRATOS_REGISTRY_ADD_PROTOTYPE(NAME, X)                                            \
     static inline bool KRATOS_REGISTRY_NAME(_is_registered_, __LINE__) = []() -> bool {   \
         using TFunctionType = std::function<std::shared_ptr<X>()>;                        \
         std::string key_name = NAME + std::string(".") + std::string(#X);                 \
         if (!Registry::HasItem(key_name))                                                 \
         {                                                                                 \
             auto &r_item = Registry::AddItem<RegistryItem>(key_name);                     \
-            TFunctionType dispatcher = [](){return std::make_shared<X>(__VA_ARGS__);};    \
+            TFunctionType dispatcher = [](){return std::make_shared<X>();};               \
             r_item.AddItem<TFunctionType>("Prototype", std::move(dispatcher));            \
         }                                                                                 \
         return Registry::HasItem(key_name);                                               \
