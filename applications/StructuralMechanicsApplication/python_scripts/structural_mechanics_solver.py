db6651a8d95453c39825796ac2974b166a68fc33
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-03-27 17:43:17 +0200

    Fix

--- applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
+++ applications/StructuralMechanicsApplication/python_scripts/structural_mechanics_solver.py
@@ -337,8 +337,7 @@ class MechanicalSolver(PythonSolver):
         if not hasattr(self, '_builder_and_solver'):
             self._builder_and_solver = self._CreateBuilderAndSolver()
         elif not self.use_block_builder: # Block builder and solver are unified with MPC and without. In the case of the elimination this could be a problem
-            if self.settings["multi_point_constraints_used"].GetBool() is False and
-                self.GetComputingModelPart().NumberOfMasterSlaveConstraints() > 0 and not self.mpc_block_builder_initialized:
+            if self.settings["multi_point_constraints_used"].GetBool() is False and self.GetComputingModelPart().NumberOfMasterSlaveConstraints() > 0 and not self.mpc_block_builder_initialized:
                 self.settings["multi_point_constraints_used"].SetBool(True)
                 self._builder_and_solver = self._CreateBuilderAndSolver()
                 self.mpc_block_builder_initialized = True
@@ -348,8 +347,7 @@ class MechanicalSolver(PythonSolver):
         if not hasattr(self, '_mechanical_solution_strategy'):
             self._mechanical_solution_strategy = self._CreateSolutionStrategy()
         elif not self.use_block_builder: # Block builder and solver are unified with MPC and without. In the case of the elimination this could be a problem
-            if self.settings["multi_point_constraints_used"].GetBool() is False and
-                self.GetComputingModelPart().NumberOfMasterSlaveConstraints() > 0 and not self.mpc_block_builder_initialized:
+            if self.settings["multi_point_constraints_used"].GetBool() is False and self.GetComputingModelPart().NumberOfMasterSlaveConstraints() > 0 and not self.mpc_block_builder_initialized:
                 self._mechanical_solution_strategy = self._CreateSolutionStrategy()
         return self._mechanical_solution_strategy
 
