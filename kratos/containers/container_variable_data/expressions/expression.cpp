f031f84fcad0ba62ed1a30f5a67bc2c5d97491be
sunethwarna <suneth.warna@gmail.com>
2023-03-27 15:53:27 +0200

    adding expression

---
+++ kratos/containers/container_variable_data/expressions/expression.cpp
@@ -0,0 +1,44 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: OptimizationApplication/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+// System includes
+#include <functional>
+#include <numeric>
+
+// Project includes
+
+// Include base h
+#include "expression.h"
+
+namespace Kratos {
+
+std::size_t Expression::GetLocalSize() const
+{
+    const auto& r_shape = this->GetShape();
+    return std::transform_reduce(r_shape.begin(), r_shape.end(), 1UL, std::multiplies{},
+                                 [](const auto v) { return v; });
+}
+
+void intrusive_ptr_add_ref(const Expression* x)
+{
+    x->mReferenceCounter.fetch_add(1, std::memory_order_relaxed);
+}
+
+void intrusive_ptr_release(const Expression* x)
+{
+    if (x->mReferenceCounter.fetch_sub(1, std::memory_order_release) == 1) {
+        std::atomic_thread_fence(std::memory_order_acquire);
+        delete x;
+    }
+}
+
+} // namespace Kratos
