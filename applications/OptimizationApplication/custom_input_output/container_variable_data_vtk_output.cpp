f8ecd0c9284fdee2368b745f921671a3bc755039
Fabian <f.meister@tu-bs.de>
2023-03-30 10:21:52 +0200

    Init class

---
+++ applications/OptimizationApplication/custom_input_output/container_variable_data_vtk_output.cpp
@@ -0,0 +1,20 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: OptimizationApplication/license.txt
+//
+//  Main author:     Fabian Meister
+//
+
+#include "input_output/vtk_output.h"
+
+// Include base h
+#include "container_variable_data_vtk_output.h"
+
+namespace Kratos
+{
+}
---
+++ applications/OptimizationApplication/custom_input_output/container_variable_data_vtk_output.h
@@ -0,0 +1,37 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: OptimizationApplication/license.txt
+//
+//  Main author:     Fabian Meister
+//
+
+#include "input_output/vtk_output.h"
+
+#include "custom_utilities/container_variable_data/container_variable_data.h"
+
+namespace Kratos
+{
+        /** \brief VtkOutput
+         * A simple class that has functionality to write vtk outputs for container variable data - extention of the kratos core vtk output
+         * @see : https://www.vtk.org/wp-content/uploads/2015/04/file-formats.pdf
+         */
+
+        /**
+         * @brief Print the given rContainerVariableData as a VTK file
+         * @param rContainerVariableData ContainerVariableData which is outputted
+         * @param rDataName name of the data in the vtk file
+         */
+        class ContainerVariableDataVtkOutput : public VtkOutput
+        {
+                template <class TContainerType>
+                void WriteContainerDataToFile(const ContainerVariableData<TContainerType> &rContainerVariableData, const std::string &rDataName, std::ofstream &rFileStream)
+                {
+                }
+        }
+
+}
