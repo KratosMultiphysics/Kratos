c3c1d3dbe9456493b5ff63ed3b8cac54c80dfbe1
noordam <aron.noordam@deltares.nl>
2023-04-05 14:17:57 +0200

    added test for the local system for 3d2n spring damper element

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_spring_damper_element.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_spring_damper_element.cpp
@@ -41,23 +41,26 @@ namespace Testing
         }
     }
 
-
+    /**
+     * \brief Sets up a 2D spring damper element
+     * \param  rModelPart Current model part
+     * \return SpringDamper2D2N element
+     */
     intrusive_ptr<Element> SetUpElement2D(ModelPart& rModelPart)
     {
-        Model current_model;
-        auto& r_model_part = current_model.CreateModelPart("ModelPart", 1);
-        r_model_part.GetProcessInfo().SetValue(DOMAIN_SIZE, 3);
 
-        r_model_part.AddNodalSolutionStepVariable(DISPLACEMENT);
-        r_model_part.AddNodalSolutionStepVariable(ROTATION);
+        rModelPart.GetProcessInfo().SetValue(DOMAIN_SIZE, 2);
+
+        rModelPart.AddNodalSolutionStepVariable(DISPLACEMENT);
+        rModelPart.AddNodalSolutionStepVariable(ROTATION);
 
 
         // Set the element properties
-        const auto p_elem_prop = r_model_part.CreateNewProperties(0);
+        const auto p_elem_prop = rModelPart.CreateNewProperties(0);
 
         // Create the test element
-        auto p_node_1 = r_model_part.CreateNewNode(1, 0.0, 0.0, 0.0);
-        auto p_node_2 = r_model_part.CreateNewNode(2, 0.1, 0.0, 0.0);
+        auto p_node_1 = rModelPart.CreateNewNode(1, 0.0, 0.0, 0.0);
+        auto p_node_2 = rModelPart.CreateNewNode(2, 0.1, 0.0, 0.0);
 
         p_node_1->FastGetSolutionStepValue(DISPLACEMENT) = array_1d<double, 3>{ 5.0, 6.0, 0 };
         p_node_1->FastGetSolutionStepValue(ROTATION) = array_1d<double, 3>{ 0, 0, 7.0 };
@@ -65,16 +68,57 @@ namespace Testing
         p_node_2->FastGetSolutionStepValue(DISPLACEMENT) = array_1d<double, 3>{ 0.0, 0.0, 0 };
         p_node_2->FastGetSolutionStepValue(ROTATION) = array_1d<double, 3>{ 0, 0, 0.0 };
 
-        AddDofsElement2D(r_model_part);
+        AddDofsElement2D(rModelPart);
 
         const std::vector<ModelPart::IndexType> element_nodes{ 1,2 };
-        const auto p_element = r_model_part.CreateNewElement("SpringDamperElement2D2N", 1, element_nodes, p_elem_prop);
+        const auto p_element = rModelPart.CreateNewElement("SpringDamperElement2D2N", 1, element_nodes, p_elem_prop);
 
         p_element->SetValue(NODAL_DISPLACEMENT_STIFFNESS, array_1d<double, 3>{ 2.0, 3.0, 0 });
         p_element->SetValue(NODAL_ROTATIONAL_STIFFNESS, array_1d<double, 3>{ 0.0, 0.0, 4.0 });
 
         p_element->SetValue(NODAL_DAMPING_RATIO, array_1d<double, 3>{ 8.0, 9.0, 0 });
         p_element->SetValue(NODAL_ROTATIONAL_DAMPING_RATIO, array_1d<double, 3>{ 0.0, 0.0, 10.0 });
+
+        return p_element;
+    }
+
+    /**
+     * \brief Sets up a 3D spring damper element
+     * \param  rModelPart Current model part 
+     * \return SpringDamper3D2N element
+     */
+    intrusive_ptr<Element> SetUpElement3D(ModelPart& rModelPart)
+    {
+
+        rModelPart.GetProcessInfo().SetValue(DOMAIN_SIZE, 3);
+
+        rModelPart.AddNodalSolutionStepVariable(DISPLACEMENT);
+        rModelPart.AddNodalSolutionStepVariable(ROTATION);
+
+
+        // Set the element properties
+        const auto p_elem_prop = rModelPart.CreateNewProperties(0);
+
+        // Create the test element
+        auto p_node_1 = rModelPart.CreateNewNode(1, 0.0, 0.0, 0.0);
+        auto p_node_2 = rModelPart.CreateNewNode(2, 0.1, 0.0, 0.0);
+
+        p_node_1->FastGetSolutionStepValue(DISPLACEMENT) = array_1d<double, 3>{ 5.0, 6.0, 7.0 };
+        p_node_1->FastGetSolutionStepValue(ROTATION) = array_1d<double, 3>{ 8.0, 9.0, 10.0 };
+
+        p_node_2->FastGetSolutionStepValue(DISPLACEMENT) = array_1d<double, 3>{ 0.0, 0.0, 0 };
+        p_node_2->FastGetSolutionStepValue(ROTATION) = array_1d<double, 3>{ 0, 0, 0.0 };
+
+        AddDofsElement3D(rModelPart);
+
+        const std::vector<ModelPart::IndexType> element_nodes{ 1,2 };
+        const auto p_element = rModelPart.CreateNewElement("SpringDamperElement3D2N", 1, element_nodes, p_elem_prop);
+
+        p_element->SetValue(NODAL_DISPLACEMENT_STIFFNESS, array_1d<double, 3>{ 2.0, 3.0, 4.0 });
+        p_element->SetValue(NODAL_ROTATIONAL_STIFFNESS, array_1d<double, 3>{ 11.0, 12.0, 13.0 });
+
+        p_element->SetValue(NODAL_DAMPING_RATIO, array_1d<double, 3>{ 14.0, 15.0, 16.0 });
+        p_element->SetValue(NODAL_ROTATIONAL_DAMPING_RATIO, array_1d<double, 3>{ 17.0, 18.0, 19.0 });
 
         return p_element;
     }
@@ -182,5 +226,83 @@ namespace Testing
         KRATOS_CHECK_MATRIX_NEAR(expected_damping_matrix, calculated_damping_matrix, 1e-10);
 
     }
+
+
+    // Tests the lhs matrix and the rhs vector of the SpringDamperElement3D2N
+    KRATOS_TEST_CASE_IN_SUITE(SpringDamperLocalSystem3D2N, KratosStructuralMechanicsFastSuite)
+    {
+        // create model part
+        Model current_model;
+        auto& r_model_part = current_model.CreateModelPart("ModelPart", 1);
+
+        // set up 2d element
+        auto p_element = SetUpElement3D(r_model_part);
+
+        const auto& r_process_info = r_model_part.GetProcessInfo();
+
+        // initialize element
+        p_element->Initialize(r_process_info);
+
+        // check element
+        p_element->Check(r_process_info);
+
+
+        // Run test
+        const unsigned int number_of_nodes = p_element->GetGeometry().size();
+        const unsigned int number_of_dofs = number_of_nodes * 6;
+        Matrix calculated_lhs = ZeroMatrix(number_of_dofs, number_of_dofs);
+        Vector calculated_rhs = ZeroVector(number_of_dofs);
+        p_element->CalculateLocalSystem(calculated_lhs, calculated_rhs, r_process_info);
+
+        // Set expected damping matrix
+        Matrix expected_lhs = ZeroMatrix(number_of_dofs);
+        expected_lhs(0, 0) = 2.0;
+        expected_lhs(0, 6) = -2.0;
+        expected_lhs(1, 1) = 3.0;
+        expected_lhs(1, 7) = -3.0;
+        expected_lhs(2, 2) = 4.0;
+        expected_lhs(2, 8) = -4.0;
+        expected_lhs(3, 3) = 11.0;
+        expected_lhs(3, 9) = -11.0;
+        expected_lhs(4, 4) = 12.0;
+        expected_lhs(4, 10) = -12.0;
+        expected_lhs(5, 5) = 13.0;
+        expected_lhs(5, 11) = -13.0;
+
+        expected_lhs(6, 6) = 2.0;
+        expected_lhs(6, 0) = -2.0;
+        expected_lhs(7, 7) = 3.0;
+        expected_lhs(7, 1) = -3.0;
+        expected_lhs(8, 8) = 4.0;
+        expected_lhs(8, 2) = -4.0;
+        expected_lhs(9, 9) = 11.0;
+        expected_lhs(9, 3) = -11.0;
+        expected_lhs(10, 10) = 12.0;
+        expected_lhs(10, 4) = -12.0;
+        expected_lhs(11, 11) = 13.0;
+        expected_lhs(11, 5) = -13.0;
+
+        // Assert Lhs
+        KRATOS_CHECK_MATRIX_NEAR(expected_lhs, calculated_lhs, 1e-10);
+
+        // Set expected rhs
+        Vector expected_rhs = ZeroVector(number_of_dofs);
+        expected_rhs(0) = -2.0 * 5.0;
+        expected_rhs(1) = -3.0 * 6.0;
+        expected_rhs(2) = -4.0 * 7.0;
+        expected_rhs(3) = -11.0 * 8.0;
+        expected_rhs(4) = -12.0 * 9.0;
+        expected_rhs(5) = -13.0 * 10.0;
+        expected_rhs(6) = 2.0 * 5.0;
+        expected_rhs(7) = 3.0 * 6.0;
+        expected_rhs(8) = 4.0 * 7.0;
+        expected_rhs(9) = 11.0 * 8.0;
+        expected_rhs(10) = 12.0 * 9.0;
+        expected_rhs(11) = 13.0 * 10.0;
+
+        // Assert rhs
+        KRATOS_CHECK_VECTOR_NEAR(expected_rhs, calculated_rhs, 1e-10);
+    }
+
 }
 }
