46639435914b70ab6b2825be54bc85a91b21a912
sunethwarna <suneth.warna@gmail.com>
2023-04-08 08:56:44 +0200

    update optimization_info

--- applications/OptimizationApplication/python_scripts/utilities/optimization_info.py
+++ applications/OptimizationApplication/python_scripts/utilities/optimization_info.py
@@ -1,10 +1,22 @@
+from typing import Any
+
 class OptimizationInfo:
     """Optimization info data container with buffered data ability
 
-    Instances of this can hold (str, any) data pairs in hierachychal data
+    Instances of this can hold (str, Any) data pairs in hierachychal data
     structure where each sub item can have their own buffer sizes. Hence,
     Different sub structures can have their own buffers and can be advanced
     seperately or all together.
+
+    Overwriting of the existing data is not allowed because, this is used
+    as a central place to store data from various objects, hence allowing
+    overwriting will make difficult to track the changes.
+
+    Buffer size setting can only be done at the construction time
+    of the instance to not to allow re sizing because then it is not
+    clear how to handle the connected steps.
+
+    ClearStep can be used to clear all the data in the specified step_index.
 
     The buffered data is stored in a cyclic buffer.
     """
@@ -17,11 +29,11 @@ class OptimizationInfo:
             buffer_size (int, optional): Cyclic buffer size. Defaults to 1.
         """
         self.__buffer_index = 0
-        self.__buffered_data: 'list[dict[str, any]]' = []
+        self.__buffered_data: 'list[dict[str, Any]]' = []
         self.__sub_items: 'dict[str, OptimizationInfo]' = {}
 
         # sets the buffer
-        self.SetBufferSize(buffer_size)
+        self.__SetBufferSize(buffer_size)
 
 
     def AdvanceStep(self, recursive = True) -> None:
@@ -58,27 +70,6 @@ class OptimizationInfo:
             # now clear the sub items
             for sub_item in self.__sub_items.values():
                 sub_item.ClearStep(step_index, recursive)
-
-    def SetBufferSize(self, buffer_size: int, recursive = False) -> None:
-        """Sets the buffer size of the optimization info
-
-        This sets buffer size of the cyclic buffer in this instance (and sub items)
-        to specified value.
-
-        This clears the whole buffer of this instance (and sub items).
-
-        Args:
-            buffer_size (int): Size of the cyclic buffer
-            recursive (bool, optional): Set sub item cyclic buffers. Defaults to False.
-        """
-        self.__buffer_index = 0
-        self.__buffered_data.clear()
-        for _ in range(buffer_size):
-            self.__buffered_data.append({})
-
-        if recursive:
-            for sub_item in self.__sub_items.values():
-                sub_item.SetBufferSize(buffer_size, recursive)
 
     def GetBufferSize(self) -> int:
         """Returns the buffer size of current isntance of optimization info
@@ -122,7 +113,7 @@ class OptimizationInfo:
                 # not found the subkey. Hence the given key is not available.
                 return False
 
-    def GetValue(self, key: str, step_index: int = 0) -> any:
+    def GetValue(self, key: str, step_index: int = 0) -> Any:
         """Get the value given by the key at the specified step_index.
 
         This method retrieves the value given by the key at the specified step_index.
@@ -139,14 +130,14 @@ class OptimizationInfo:
             RuntimeError: If parent keys are not found.
 
         Returns:
-            any: Value stored at the key for the specified step_index.
+            Any: Value stored at the key for the specified step_index.
         """
         pos = key.find("/")
 
         if pos == -1:
             # this is a leaf key, then look for the value
             if not self.HasValue(key, step_index):
-                raise RuntimeError(f"The key \"{key}\" not found in the optimization info. OptimizationInfo:\n{self}")
+                raise RuntimeError(f"The key \"{key}\" not found in the optimization info [ step_index = {step_index} ]. OptimizationInfo:\n{self}")
 
             # first in the value container
             if step_index < self.GetBufferSize() and step_index >= 0:
@@ -163,7 +154,7 @@ class OptimizationInfo:
             else:
                 raise RuntimeError(f"The key \"{current_key}\" not found in the optimization info which is a parent key for \"{key}\". OptimizationInfo:\n{self}")
 
-    def SetValue(self, key: str, value: any, step_index: int = 0, overwrite = False) -> None:
+    def SetValue(self, key: str, value: Any, step_index: int = 0) -> None:
         """Sets a value for specified key at specified step_index.
 
         This method sets the value at the key at the specified step_index.
@@ -171,46 +162,42 @@ class OptimizationInfo:
         It can include "/" seperators to set a value which is in sub items. In this case,
         it will be set by recursive calls.
 
-        Overwrite allows to overwrite existing keys with new values.
-
         Args:
             key (str): Relative path to the value.
-            value (any): value to be set
+            value (Any): value to be set
             step_index (int, optional): Step index the value should be set. Defaults to 0.
-            overwrite (bool, optional): Allows overwriting of existing values. Defaults to False.
 
         Raises:
-            RuntimeError: If overwrite is False, and a value is found for the same key.
+            RuntimeError: If a value is overwritten.
         """
         pos = key.find("/")
         if pos == -1:
-            if not overwrite and self.HasValue(key, step_index):
-                raise RuntimeError(f"Trying to overwrite \"{key}\" with value {value} [ Existing value = {self.GetValue(key)}]. OptimizationInfo:\n{self}")
-
             # this is a leaf, then we check the type of the value and add appropriately
             if isinstance(value, dict):
                 # if the given value is a dict, then convert the structure
                 # to OptimizationInfo while keeping the sub_item structure.
                 sub_item = OptimizationInfo(self.GetBufferSize())
-                self.SetValue(key, sub_item)
+                self.__AddSubItem(key, sub_item)
+
+                # now iterate through all the sub_keys and values of the dictionary and
+                # add them to sub_item
                 for sub_key, sub_value in value.items():
-                    sub_item.SetValue(f"{sub_key}", sub_value, step_index, overwrite)
+                    sub_item.SetValue(f"{sub_key}", sub_value, step_index)
             elif isinstance(value, OptimizationInfo):
                 # if the given value is of type OptimizationInfo, then put it to sub_items.
-                self.__sub_items[key] = value
+                self.__AddSubItem(key, value)
             else:
                 # if not any of the above, it is a normal value. Then put it to buffer.
-                buffer_data = self.__buffered_data[self.__GetBufferIndex(step_index)]
-                buffer_data[key] = value
+                self.__AddBufferedValue(key, value, step_index)
         else:
             # if it is not a leaf key. then look for the sub_item, and recursively
             # call the Set method.
             current_key = key[:pos]
             if not current_key in self.__sub_items.keys():
-                # no sub key found. then create it.
-                self.__sub_items[current_key] = OptimizationInfo(self.GetBufferSize())
+                # no existing key found then create it.
+                self.__AddSubItem(current_key, OptimizationInfo(self.GetBufferSize()))
 
-            self.__sub_items[current_key].SetValue(key[pos+1:], value, step_index, overwrite)
+            self.__sub_items[current_key].SetValue(key[pos+1:], value, step_index)
 
     def RemoveValue(self, key: str, step_index: int = 0) -> None:
         """Remove value at the key in the specified step_index
@@ -280,6 +267,42 @@ class OptimizationInfo:
         info = f"{tabbing}{info}"
         return info
 
+    def __AddBufferedValue(self, key: str, value: Any, step_index: int) -> None:
+        # first check whether this key exists in the specified step_index
+        buffer_data = self.__buffered_data[self.__GetBufferIndex(step_index)]
+        if key in buffer_data.keys():
+            raise RuntimeError(f"Trying to add a buffer value with key = \"{key}\" when already value exists for the key [ Existing value = {buffer_data[key]}]. OptimizationInfo:\n{self}")
+
+        # now check whether a sub item exists
+        if key in self.__sub_items.keys():
+            raise RuntimeError(f"Trying to add a buffer value with key = \"{key}\" when a subitem exists with the same key. OptimizationInfo:\n{self}")
+
+        # now add the buffer value
+        buffer_data[key] = value
+
+    def __AddSubItem(self, key: str, value: Any) -> None:
+        # first check if any of the buffered data has the same key.
+        # this is because, sub_items are valid for all step_indices.
+        if any([key in buffer_data.keys() for buffer_data in self.__buffered_data]):
+            raise RuntimeError(f"Trying to add a new a sub item with key = \"{key}\" when a value with the same key exists in buffered data. OptimizationInfo:\n{self}")
+
+        # now check if the item exists in the sub_items
+        if key in self.__sub_items.keys():
+            raise RuntimeError(f"Trying to add a new sub_item with key = \"{key}\" when already a sub item exists. OptimizationInfo:\n{self}")
+
+        # now add the sub_item
+        self.__sub_items[key] = value
+
+    def __SetBufferSize(self, buffer_size: int, recursive = False) -> None:
+        self.__buffer_index = 0
+        self.__buffered_data.clear()
+        for _ in range(buffer_size):
+            self.__buffered_data.append({})
+
+        if recursive:
+            for sub_item in self.__sub_items.values():
+                sub_item.__SetBufferSize(buffer_size, recursive)
+
     def __GetBufferIndex(self, step_index: int) -> int:
         if step_index >= self.GetBufferSize() or step_index < 0:
             raise RuntimeError(f"Invalid step index. Allowed step indices are in [0, {self.GetBufferSize()}) [ StepIndex = {step_index} ]. OptimizationInfo:\n{self}")
@@ -308,7 +331,7 @@ class OptimizationInfo:
         info += f"\n{tabbing}" + "}"
         return info
 
-    def __getitem__(self, key: str) -> any:
+    def __getitem__(self, key: str) -> Any:
         if isinstance(key, str):
             return self.GetValue(key)
         elif isinstance(key, tuple) and len(key) == 2:
@@ -316,7 +339,7 @@ class OptimizationInfo:
         else:
             raise RuntimeError(f"The key should be either a string (representing the key path) or (string, int) tuple (representing key path and step index) [ key = {key}].")
 
-    def __setitem__(self, key: str, value: any) -> None:
+    def __setitem__(self, key: str, value: Any) -> None:
         if isinstance(key, str):
             return self.SetValue(key, value)
         elif isinstance(key, tuple) and len(key) == 2:
--- applications/OptimizationApplication/tests/test_optimization_info.py
+++ applications/OptimizationApplication/tests/test_optimization_info.py
@@ -6,93 +6,127 @@ import KratosMultiphysics.KratosUnittest as kratos_unittest
 from KratosMultiphysics.OptimizationApplication.utilities.optimization_info import OptimizationInfo
 
 class TestOptimizationInfo(kratos_unittest.TestCase):
-    class TestRoutine(Kratos.Process):
-        def __init__(self):
-            super().__init__()
-
-    def test_AdvanceSolutionStepGet(self):
-        optimization_info = OptimizationInfo()
-        optimization_info.SetBufferSize(3)
+    def test_AdvanceStep(self):
+        optimization_info = OptimizationInfo(3)
 
         optimization_info["step"] = 1
         self.assertEqual(optimization_info["step"], 1)
 
-        optimization_info.AdvanceSolutionStep()
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
         optimization_info["step"] = 2
         self.assertEqual(optimization_info["step"], 2)
-        self.assertEqual(optimization_info.GetSolutionStepData(1)["step"], 1)
+        self.assertEqual(optimization_info["step", 1], 1)
 
-        optimization_info.AdvanceSolutionStep()
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
         optimization_info["step"] = 3
         self.assertEqual(optimization_info["step"], 3)
-        self.assertEqual(optimization_info.GetSolutionStepData(1)["step"], 2)
-        self.assertEqual(optimization_info.GetSolutionStepData(2)["step"], 1)
+        self.assertEqual(optimization_info["step", 1], 2)
+        self.assertEqual(optimization_info["step", 2], 1)
 
-        optimization_info.AdvanceSolutionStep()
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
         optimization_info["step"] = 4
         self.assertEqual(optimization_info["step"], 4)
-        self.assertEqual(optimization_info.GetSolutionStepData(1)["step"], 3)
-        self.assertEqual(optimization_info.GetSolutionStepData(2)["step"], 2)
+        self.assertEqual(optimization_info["step", 1], 3)
+        self.assertEqual(optimization_info["step", 2], 2)
         with self.assertRaises(RuntimeError):
-            optimization_info.GetSolutionStepData(3)["step"]
+            optimization_info["step", 3]
 
-    def test_AdvanceSolutionStepSet(self):
-        optimization_info = OptimizationInfo()
-        optimization_info.SetBufferSize(3)
+    def test_AdvanceStepSet(self):
+        optimization_info = OptimizationInfo(3)
+        optimization_info["test_1/test_sub_1"] = OptimizationInfo(2)
 
-        optimization_info["step"] = 1
-        self.assertEqual(optimization_info["step"], 1)
+        self.assertEqual(optimization_info["test_1"].GetBufferSize(), 3)
+        self.assertEqual(optimization_info["test_1/test_sub_1"].GetBufferSize(), 2)
 
-        optimization_info.AdvanceSolutionStep()
-        optimization_info["step"] = 2
-        optimization_info.GetSolutionStepData(1)["step"] = 10
-        self.assertEqual(optimization_info["step"], 2)
-        self.assertEqual(optimization_info.GetSolutionStepData(1)["step"], 10)
+        optimization_info["test_1/int"] = 1
+        optimization_info["test_1/test_sub_1/int"] = 2
 
-        optimization_info.AdvanceSolutionStep()
-        optimization_info["step"] = 3
-        optimization_info.GetSolutionStepData(2)["step"] = 15
-        self.assertEqual(optimization_info["step"], 3)
-        self.assertEqual(optimization_info.GetSolutionStepData(1)["step"], 2)
-        self.assertEqual(optimization_info.GetSolutionStepData(2)["step"], 15)
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
+        optimization_info["test_1/int"] = 3
+        optimization_info["test_1/test_sub_1/int"] = 4
 
-        optimization_info.AdvanceSolutionStep()
-        optimization_info["step"] = 4
-        self.assertEqual(optimization_info["step"], 4)
-        self.assertEqual(optimization_info.GetSolutionStepData(1)["step"], 3)
-        self.assertEqual(optimization_info.GetSolutionStepData(2)["step"], 2)
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
+        optimization_info["test_1/int"] = 5
+        optimization_info["test_1/test_sub_1/int"] = 6
+
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
+        optimization_info["test_1/int"] = 7
+        optimization_info["test_1/test_sub_1/int"] = 8
+
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
+        optimization_info["test_1/int"] = 9
+        optimization_info["test_1/test_sub_1/int"] = 10
+
+        optimization_info.AdvanceStep()
+        optimization_info.ClearStep()
+        optimization_info["test_1/int"] = 11
+        optimization_info["test_1/test_sub_1/int"] = 12
+
+        self.assertEqual(optimization_info["test_1/int", 0], 11)
+        self.assertEqual(optimization_info["test_1/int", 1], 9)
+        self.assertEqual(optimization_info["test_1/int", 2], 7)
+        with self.assertRaises(RuntimeError):
+            optimization_info["test_1/int", 3]
+
+        self.assertEqual(optimization_info["test_1/test_sub_1/int", 0], 12)
+        self.assertEqual(optimization_info["test_1/test_sub_1/int", 1], 10)
+        with self.assertRaises(RuntimeError):
+            optimization_info["test_1/test_sub_1/int", 3]
+
+    def test_HasValue(self):
+        optimization_info = OptimizationInfo(2)
+        optimization_info["data/sub_1"] = 1
+        optimization_info["data/sub_2", 1] = 2
+        optimization_info.SetValue("data/sub_3/sub_sub1", 3, 1)
+
+        self.assertTrue(optimization_info.HasValue("data/sub_2", 1))
+        self.assertTrue(optimization_info["data"].HasValue("sub_3/sub_sub1", 1))
+        self.assertTrue(optimization_info.HasValue("data/sub_2", 1))
+        self.assertTrue(optimization_info.HasValue("data/sub_3", 0))
+        self.assertTrue(optimization_info.HasValue("data/sub_3", 1))
+        self.assertFalse(optimization_info.HasValue("data/sub_2"))
+
+    def test_RemoveValue(self):
+        optimization_info = OptimizationInfo(3)
+        optimization_info["data/sub_1"] = 1
+        optimization_info["data/sub_1", 1] = 2
+        optimization_info["data/sub_1", 2] = 3
+
+        self.assertTrue(optimization_info.HasValue("data/sub_1", 0))
+        self.assertTrue(optimization_info.HasValue("data/sub_1", 1))
+        self.assertTrue(optimization_info.HasValue("data/sub_1", 2))
+
+        del optimization_info["data/sub_1", 0]
+        self.assertFalse(optimization_info.HasValue("data/sub_1", 0))
+        self.assertTrue(optimization_info.HasValue("data/sub_1", 1))
+        self.assertTrue(optimization_info.HasValue("data/sub_1", 2))
+
+        del optimization_info["data/sub_1", 1]
+        self.assertFalse(optimization_info.HasValue("data/sub_1", 0))
+        self.assertFalse(optimization_info.HasValue("data/sub_1", 1))
+        self.assertTrue(optimization_info.HasValue("data/sub_1", 2))
+
+        del optimization_info["data/sub_1", 2]
+        self.assertFalse(optimization_info.HasValue("data/sub_1", 0))
+        self.assertFalse(optimization_info.HasValue("data/sub_1", 1))
+        self.assertFalse(optimization_info.HasValue("data/sub_1", 2))
+        self.assertTrue(optimization_info.HasValue("data"))
+
+    def test_SetValue(self):
+        optimization_info = OptimizationInfo(3)
+        optimization_info["data/sub_1", 1] = 1
+        with self.assertRaises(RuntimeError):
+            optimization_info["data/sub_1/int"]
 
         with self.assertRaises(RuntimeError):
-            optimization_info.GetSolutionStepData(3)["step"]
-
-    def test_OptimizationProcess(self):
-        optimization_info = OptimizationInfo()
-        optimization_info.SetBufferSize(1)
-        temp = TestOptimizationInfo.TestRoutine()
-        optimization_info.AddOptimizationProcess(TestOptimizationInfo.TestRoutine, "temp", temp)
-
-        self.assertTrue(optimization_info.HasOptimizationProcess(TestOptimizationInfo.TestRoutine, "temp"))
-        self.assertTrue(optimization_info.HasOptimizationProcessType(TestOptimizationInfo.TestRoutine))
-        self.assertEqual(temp, optimization_info.GetOptimizationProcess(TestOptimizationInfo.TestRoutine, "temp"))
-        self.assertEqual([temp], optimization_info.GetOptimizationProcesses(TestOptimizationInfo.TestRoutine))
-
-    def test_HasSetGetValue(self):
-        optimization_info = OptimizationInfo()
-        optimization_info.SetBufferSize(3)
-
-        self.assertFalse(optimization_info.HasValue("test/hello/there/3"))
-        optimization_info.SetValue("test/hello/there/3", 20)
-        self.assertTrue(optimization_info.HasValue("test"))
-        self.assertTrue(optimization_info.HasValue("test/hello"))
-        self.assertTrue(optimization_info.HasValue("test/hello/there"))
-        self.assertTrue(optimization_info.HasValue("test/hello/there/3"))
-        self.assertEqual(optimization_info.GetValue("test/hello/there/3"), 20)
-
-        optimization_info.SetValue("test/hello/there/3", 40, overwrite=True)
-        self.assertEqual(optimization_info.GetValue("test/hello/there/3"), 40)
-
-        optimization_info.SetValue("test/hello/there/8", 60, 1)
-        self.assertEqual(optimization_info.GetValue("test/hello/there/8", 1), 60)
+            optimization_info["data"] = 2
 
 if __name__ == "__main__":
     Kratos.Tester.SetVerbosity(Kratos.Tester.Verbosity.PROGRESS)  # TESTS_OUTPUTS
