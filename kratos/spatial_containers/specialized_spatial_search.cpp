8d5119dd0869bfe4f79e014b2ad724be9dc5f7cb
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-04-10 17:20:51 +0200

    Refactor

--- kratos/spatial_containers/specialized_spatial_search.cpp
+++ kratos/spatial_containers/specialized_spatial_search.cpp
@@ -15,7 +15,6 @@
 // External includes
 
 // Project includes
-#include "utilities/parallel_utilities.h"
 #include "spatial_containers/specialized_spatial_search.h"
 #include "spatial_containers/spatial_containers.h"
 
@@ -64,34 +63,16 @@ void SpecializedSpatialSearch<TSearchBackend>::SearchElementsInRadiusExclusive(
 {
     // Defining the point type for the search
     using PointType = PointObject<Element>;
-    using PointTypePointer = PointType::Pointer;
-    using PointVector = std::vector<PointType::Pointer>;
-    using PointIterator = std::vector<PointType::Pointer>::iterator;
-    using DistanceVector = std::vector<double>;
+    using PointTypePointer = PointObject<Element>::Pointer;
+    using PointVector = std::vector<PointTypePointer>;
+    using PointIterator = PointVector::iterator;
     using DistanceIterator = std::vector<double>::iterator;
 
     // Retrieving parameters
-    const int allocation_size = mParameters["allocation_size"].GetInt();
     const int bucket_size = mParameters["bucket_size"].GetInt();
 
     // Defining the PointVector
-    PointVector points;
-    const std::size_t structure_size = rStructureElements.size();
-    points.reserve(structure_size);
-    const auto it_begin = rStructureElements.begin();
-    for (std::size_t i = 0; i < structure_size; ++i) {
-        auto it_elem = it_begin + i;
-        points.push_back(PointTypePointer(new PointType(*(it_elem.base()))));
-    }
-
-    // Resizing the results
-    const std::size_t input_size = rInputElements.size();
-    if (rResults.size() != input_size) {
-        rResults.resize(input_size);
-    }
-    if (rResultsDistance.size() != input_size) {
-        rResultsDistance.resize(input_size);
-    }
+    PointVector points = PrepareSearch(rStructureElements, rInputElements, rResults, rResultsDistance);
 
     // Defining the search structure
     if constexpr (TSearchBackend == SpatialContainer::KDTree) {
@@ -103,24 +84,7 @@ void SpecializedSpatialSearch<TSearchBackend>::SearchElementsInRadiusExclusive(
         KDTree kd_tree(points.begin(), points.end(), bucket_size);
 
         // Performing search
-        IndexPartition<std::size_t>(input_size).for_each([&](std::size_t i) {
-            auto it_elem = rInputElements.begin() + i;
-            PointType aux_point(*(it_elem.base()));
-            PointVector results;
-            DistanceVector results_distances;
-            const std::size_t number_of_results = kd_tree.SearchInRadius(aux_point, rRadius[i], results.begin(), results_distances.begin(), allocation_size);
-            if (number_of_results > 0) {
-                auto& r_results = rResults[i];
-                auto& r_results_distance = rResultsDistance[i];
-                r_results.reserve(number_of_results);
-                r_results_distance.reserve(number_of_results);
-                for (std::size_t j = 0; j < number_of_results; ++j) {
-                    PointType::Pointer p_point = results[j];
-                    r_results.push_back(p_point->pGetObject());
-                    r_results_distance.push_back(results_distances[j]);
-                }
-            }
-        });
+        ParallelSearch(rInputElements, rRadius, kd_tree, rResults, rResultsDistance);
     } else if constexpr (TSearchBackend == SpatialContainer::Octree) {
         /// Octree definitions
         using BucketType = Bucket< 3ul, PointType, PointVector, PointTypePointer, PointIterator, DistanceIterator>;
@@ -169,9 +133,8 @@ void SpecializedSpatialSearch<TSearchBackend>::SearchNodesInRadiusExclusive(
     // Defining the point type for the search
     using PointType = PointObject<Node<3>>;
     using PointTypePointer = PointType::Pointer;
-    using PointVector = std::vector<PointType::Pointer>;
-    using PointIterator = std::vector<PointType::Pointer>::iterator;
-    using DistanceVector = std::vector<double>;
+    using PointVector = std::vector<PointTypePointer>;
+    using PointIterator = PointVector::iterator;
     using DistanceIterator = std::vector<double>::iterator;
 
     // Retrieving parameters
@@ -230,9 +193,8 @@ void SpecializedSpatialSearch<TSearchBackend>::SearchConditionsInRadiusExclusive
     // Defining the point type for the search
     using PointType = PointObject<Condition>;
     using PointTypePointer = PointType::Pointer;
-    using PointVector = std::vector<PointType::Pointer>;
-    using PointIterator = std::vector<PointType::Pointer>::iterator;
-    using DistanceVector = std::vector<double>;
+    using PointVector = std::vector<PointTypePointer>;
+    using PointIterator = PointVector::iterator;
     using DistanceIterator = std::vector<double>::iterator;
 
     // Retrieving parameters
