66707739d79975ff0ddf9ae6e4183cf107536b15
aronnoordam <51492202+aronnoordam@users.noreply.github.com>
2023-04-12 09:22:46 +0200

    Update applications/StructuralMechanicsApplication/custom_elements/spring_damper_element.cpp
    Co-authored-by: Philipp Bucher <philipp.bucher@tum.de>

--- applications/StructuralMechanicsApplication/custom_elements/spring_damper_element.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/spring_damper_element.cpp
@@ -423,10 +423,7 @@ void SpringDamperElement<TDim>::ConstCalculateLeftHandSide(MatrixType& rLeftHand
     array_1d<double, 3> nodal_stiffness = ZeroVector(3);
     array_1d<double, 3> nodal_rot_stiffness = ZeroVector(3);
 
-    if (this->Has(NODAL_DISPLACEMENT_STIFFNESS))
-    {
-        nodal_stiffness = this->GetValue(NODAL_DISPLACEMENT_STIFFNESS);
-    }
+    const array_1d<double, 3>& nodal_stiffness = this->GetValue(NODAL_DISPLACEMENT_STIFFNESS);
     if (this->Has(NODAL_ROTATIONAL_STIFFNESS))
     {
         nodal_rot_stiffness = this->GetValue(NODAL_ROTATIONAL_STIFFNESS);
