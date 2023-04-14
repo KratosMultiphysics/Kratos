befbc18e967cad11e5ef718a479d00fed683c852
sunethwarna <suneth.warna@gmail.com>
2023-04-14 10:45:50 +0200

    update shape control test

--- applications/OptimizationApplication/tests/test_shape_control.py
+++ applications/OptimizationApplication/tests/test_shape_control.py
@@ -6,80 +6,55 @@ import KratosMultiphysics.StructuralMechanicsApplication
 # Import KratosUnittest
 import KratosMultiphysics.KratosUnittest as kratos_unittest
 from KratosMultiphysics.kratos_utilities import DeleteFileIfExisting
-from KratosMultiphysics.OptimizationApplication.optimization_info import OptimizationInfo
-from KratosMultiphysics.OptimizationApplication.utilities.execution_policy_decorator import ExecutionPolicyDecorator
-from KratosMultiphysics.OptimizationApplication.controls.shape_control import ShapeControl
+from KratosMultiphysics.OptimizationApplication.controls.shape.shape_control_new import ShapeControlNew
 
 class TestShapeControl(kratos_unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = Kratos.Model()
-        cls.optimization_info = OptimizationInfo()
-        cls.optimization_info.SetBufferSize(1)
-        cls.optimization_info["step"] = 0
         cls.model_part = cls.model.CreateModelPart("Structure")
         cls.model_part.ProcessInfo[Kratos.DOMAIN_SIZE] = 3
         cls.model_part.AddNodalSolutionStepVariable(Kratos.MESH_DISPLACEMENT)
-        Kratos.ModelPartIO("linear_element_test/Structure", Kratos.ModelPartIO.READ | Kratos.ModelPartIO.MESH_ONLY).ReadModelPart(cls.model_part)
+        Kratos.ModelPartIO("linear_strain_energy_test/Structure", Kratos.ModelPartIO.READ | Kratos.ModelPartIO.MESH_ONLY).ReadModelPart(cls.model_part)
 
-        material_settings = Kratos.Parameters("""{"Parameters": {"materials_filename": "linear_element_test/StructuralMaterials.json"}} """)
+        material_settings = Kratos.Parameters("""{"Parameters": {"materials_filename": "linear_strain_energy_test/StructuralMaterials.json"}} """)
         Kratos.ReadMaterialsUtility(material_settings, cls.model)
 
-        execution_policy_decorator_Executeparameters = Kratos.Parameters("""{
-            "execution_policy_name"    : "test",
-            "execution_policy_type"    : "IndependentAnalysisExecutionPolicy",
-            "execution_policy_settings": {
-                "analysis_module"  : "KratosMultiphysics",
-                "analysis_type"    : "MultistageAnalysis",
-                "analysis_settings": {
-                    "stages": [],
-                    "execution_list":[]
-                }
+        parameters = Kratos.Parameters("""{
+            "model_part_names"                 : ["Structure.structure"],
+            "mesh_motion_execution_policy_type": "IndependentAnalysisExecutionPolicy",
+            "mesh_motion_module"               : "KratosMultiphysics",
+            "mesh_motion_analysis_type"        : "MultistageAnalysis",
+            "mesh_motion_settings"             : {
+                "stages"        : [],
+                "execution_list":[]
             }
         }""")
 
-        cls.execution_policy_decorator = ExecutionPolicyDecorator(cls.model, execution_policy_decorator_Executeparameters)
-        cls.optimization_info.AddOptimizationProcess(ExecutionPolicyDecorator, cls.execution_policy_decorator.GetExecutionPolicyName(), cls.execution_policy_decorator)
-
-        parameters = Kratos.Parameters("""{
-            "model_part_names"         : ["Structure.structure"],
-            "mesh_moving_analysis_name": "test"
-        }""")
-
-        cls.shape_control = ShapeControl(cls.model, parameters, cls.optimization_info)
+        cls.shape_control = ShapeControlNew(cls.model, parameters, None)
 
     @classmethod
     def tearDownClass(cls):
-        with kratos_unittest.WorkFolderScope("linear_element_test", __file__):
+        with kratos_unittest.WorkFolderScope("linear_strain_energy_test", __file__):
             DeleteFileIfExisting("Structure.time")
 
     def test_ShapeControl(self):
-        self.execution_policy_decorator.ExecuteInitialize()
-        self.shape_control.ExecuteInitialize()
+        self.shape_control.Initialize()
 
         # run for 3 iterations
         for i in range(1, 4, 1):
-            self.optimization_info.AdvanceSolutionStep()
-            self.optimization_info["step"] = i
-            self.execution_policy_decorator.ExecuteInitializeSolutionStep()
-            self.shape_control.ExecuteInitializeSolutionStep()
+            if (i > 1):
+                test = Kratos.ContainerExpression.HistoricalExpression(self.model_part.GetSubModelPart("structure"))
+                test.Read(Kratos.MESH_DISPLACEMENT)
+                temp = Kratos.ContainerExpression.HistoricalExpression(self.model_part.GetSubModelPart("structure"))
+                temp.CopyFrom(data)
+                self.assertEqual(KratosOA.ContainerExpressionUtils.NormInf(test - temp), 0.0)
 
-            if (self.optimization_info["step"] > 1):
-                test = KratosOA.HistoricalContainerVariableDataHolder(self.model_part.GetSubModelPart("structure"))
-                test.ReadDataFromContainerVariable(Kratos.MESH_DISPLACEMENT)
-                temp = KratosOA.HistoricalContainerVariableDataHolder(self.model_part.GetSubModelPart("structure"))
-                temp.CopyDataFrom(data)
-                self.assertEqual(KratosOA.ContainerVariableDataHolderUtils.NormInf(test - temp), 0.0)
+            data = Kratos.ContainerExpression.HistoricalExpression(self.model_part.GetSubModelPart("structure"))
+            data.SetData(Kratos.Array3([i, i, i]))
+            self.shape_control.Update(KratosOA.ContainerExpression.CollectiveExpressions([data]))
 
-            data = KratosOA.HistoricalContainerVariableDataHolder(self.model_part.GetSubModelPart("structure"))
-            data.SetDataForContainerVariable(Kratos.MESH_DISPLACEMENT, Kratos.Array3([i, i, i]))
-            self.shape_control.UpdateControl(KratosOA.CollectiveVariableDataHolder([data]))
-
-            self.execution_policy_decorator.ExecuteFinalizeSolutionStep()
-            self.shape_control.ExecuteFinalizeSolutionStep()
-
-        self.execution_policy_decorator.ExecuteFinalize()
-        self.shape_control.ExecuteFinalize()
+        self.shape_control.Finalize()
 
 if __name__ == "__main__":
     Kratos.Tester.SetVerbosity(Kratos.Tester.Verbosity.PROGRESS)  # TESTS_OUTPUTS
