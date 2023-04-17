fe0767d8eb639ff4e2e78676f46e9591903816bb
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-04-17 11:25:30 +0200

    Defining as static so other functions can acess it

--- kratos/input_output/vtk_output.cpp
+++ kratos/input_output/vtk_output.cpp
@@ -59,6 +59,32 @@ Parameters VtkOutput::GetDefaultParameters()
     return default_parameters;
 }
 
+// IMPORTANT: The map geo_type_vtk_cell_type_map is to be extended to support new geometries
+// NOTE: See https://vtk.org/wp-content/uploads/2015/04/file-formats.pdf and https://vtk.org/wp-content/uploads/2021/08/VTKUsersGuide.pdf
+static const std::map<GeometryData::KratosGeometryType, int> geo_type_vtk_cell_type_map = {
+    { GeometryData::KratosGeometryType::Kratos_Point2D,          1 },
+    { GeometryData::KratosGeometryType::Kratos_Point3D,          1 },
+    { GeometryData::KratosGeometryType::Kratos_Line2D2,          3 },
+    { GeometryData::KratosGeometryType::Kratos_Line3D2,          3 },
+    { GeometryData::KratosGeometryType::Kratos_Triangle2D3,      5 },
+    { GeometryData::KratosGeometryType::Kratos_Triangle3D3,      5 },
+    { GeometryData::KratosGeometryType::Kratos_Quadrilateral2D4, 9 },
+    { GeometryData::KratosGeometryType::Kratos_Quadrilateral3D4, 9 },
+    { GeometryData::KratosGeometryType::Kratos_Tetrahedra3D4,    10 },
+    { GeometryData::KratosGeometryType::Kratos_Hexahedra3D8,     12 },
+    { GeometryData::KratosGeometryType::Kratos_Prism3D6,         13 },
+    { GeometryData::KratosGeometryType::Kratos_Pyramid3D5,       14 },
+    { GeometryData::KratosGeometryType::Kratos_Line2D3,          21 },
+    { GeometryData::KratosGeometryType::Kratos_Line3D3,          21 },
+    { GeometryData::KratosGeometryType::Kratos_Triangle2D6,      22 },
+    { GeometryData::KratosGeometryType::Kratos_Triangle3D6,      22 },
+    { GeometryData::KratosGeometryType::Kratos_Quadrilateral2D8, 23 },
+    { GeometryData::KratosGeometryType::Kratos_Quadrilateral3D8, 23 },
+    { GeometryData::KratosGeometryType::Kratos_Tetrahedra3D10,   24 },
+    // { GeometryData::KratosGeometryType::Kratos_Hexahedra3D20,    25 }, // NOTE: Quadratic hexahedra (20) requires a conversor, order does not coincide with VTK
+    // { GeometryData::KratosGeometryType::Kratos_Prism3D15,        26 }, // NOTE: Quadratic prism (15) requires a conversor, order does not coincide with VTK
+    { GeometryData::KratosGeometryType::Kratos_Pyramid3D13,      27 }
+};
 
 VtkOutput::VtkOutput(
     ModelPart& rModelPart,
@@ -387,32 +413,6 @@ void VtkOutput::WriteConnectivity(const TContainerType& rContainer, std::ofstrea
 template <typename TContainerType>
 void VtkOutput::WriteCellType(const TContainerType& rContainer, std::ofstream& rFileStream) const
 {
-    // IMPORTANT: The map geo_type_vtk_cell_type_map is to be extended to support new geometries
-    // NOTE: See https://vtk.org/wp-content/uploads/2015/04/file-formats.pdf
-    const std::map<GeometryData::KratosGeometryType, int> geo_type_vtk_cell_type_map = {
-        { GeometryData::KratosGeometryType::Kratos_Point2D,          1 },
-        { GeometryData::KratosGeometryType::Kratos_Point3D,          1 },
-        { GeometryData::KratosGeometryType::Kratos_Line2D2,          3 },
-        { GeometryData::KratosGeometryType::Kratos_Line3D2,          3 },
-        { GeometryData::KratosGeometryType::Kratos_Triangle2D3,      5 },
-        { GeometryData::KratosGeometryType::Kratos_Triangle3D3,      5 },
-        { GeometryData::KratosGeometryType::Kratos_Quadrilateral2D4, 9 },
-        { GeometryData::KratosGeometryType::Kratos_Quadrilateral3D4, 9 },
-        { GeometryData::KratosGeometryType::Kratos_Tetrahedra3D4,    10 },
-        { GeometryData::KratosGeometryType::Kratos_Hexahedra3D8,     12 },
-        { GeometryData::KratosGeometryType::Kratos_Prism3D6,         13 },
-        { GeometryData::KratosGeometryType::Kratos_Pyramid3D5,       14 },
-        { GeometryData::KratosGeometryType::Kratos_Line2D3,          21 },
-        { GeometryData::KratosGeometryType::Kratos_Line3D3,          21 },
-        { GeometryData::KratosGeometryType::Kratos_Triangle2D6,      22 },
-        { GeometryData::KratosGeometryType::Kratos_Triangle3D6,      22 },
-        { GeometryData::KratosGeometryType::Kratos_Quadrilateral2D8, 23 },
-        { GeometryData::KratosGeometryType::Kratos_Quadrilateral3D8, 23 },
-        { GeometryData::KratosGeometryType::Kratos_Tetrahedra3D10,   24 },
-        // { GeometryData::KratosGeometryType::Kratos_Hexahedra3D20,    25 }, // NOTE: Quadratic hexahedra (20) requires a conversor, order does not coincide with VTK
-        // { GeometryData::KratosGeometryType::Kratos_Prism3D15,        26 }, // NOTE: Quadratic prism (15) requires a conversor, order does not coincide with VTK
-        { GeometryData::KratosGeometryType::Kratos_Pyramid3D13,      27 }
-    };
     // Write entity types
     for (const auto& r_entity : rContainer) {
         int cell_type = -1;
