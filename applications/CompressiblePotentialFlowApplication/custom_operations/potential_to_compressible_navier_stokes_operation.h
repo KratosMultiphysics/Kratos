c1ade4a00dc7b2092399cf48185092e15ab56722
marco1410 <marcozuniga1410@gmail.com>
2023-04-25 10:05:21 +0200

    Add potential_to_compressible_navier_stokes_operation

---
+++ applications/CompressiblePotentialFlowApplication/custom_operations/potential_to_compressible_navier_stokes_operation.h
@@ -0,0 +1,121 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Marco Antonio Zuñiga Perez
+//                   
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "operations/operation.h"
+
+namespace Kratos
+{
+  ///@addtogroup CompressiblePotentialFlowApplication
+  ///@{
+
+///@name Kratos Classes
+///@{
+
+/**
+ * @author Marco Antonio Zuñiga Perez
+ * @class PotentialToCompressibleNavierStokesOperation
+ * @ingroup CompressiblePotentialFlowApplication
+ * @brief This operation pass the nodal velocities from Potential model part as initial condition of a compressible 
+ * Navier Stokes model part in conservative variables form.
+*/
+class KRATOS_API(KRATOS_COMPRESSIBLE_POTENTIAL_FLOW_APPLICATION) PotentialToCompressibleNavierStokesOperation : public Operation
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of Operation
+    KRATOS_CLASS_POINTER_DEFINITION(PotentialToCompressibleNavierStokesOperation);
+
+    /// Registry current operation
+    KRATOS_REGISTRY_ADD_PROTOTYPE("Operations.KratosMultiphysics.CompressiblePotentialFlowApplication", PotentialToCompressibleNavierStokesOperation)
+    KRATOS_REGISTRY_ADD_PROTOTYPE("Operations.All", PotentialToCompressibleNavierStokesOperation)
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    PotentialToCompressibleNavierStokesOperation() : Operation() {}
+
+    PotentialToCompressibleNavierStokesOperation(
+        Model& rModel,
+        Parameters OperationParameters);
+
+    /// Destructor.
+    ~PotentialToCompressibleNavierStokesOperation() override = default;
+    /// Copy constructor.
+    //TODO: Check. It is required by the registry
+    PotentialToCompressibleNavierStokesOperation(PotentialToCompressibleNavierStokesOperation const& rOther)  = delete;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /// Assignment operator.
+    PotentialToCompressibleNavierStokesOperation& operator=(PotentialToCompressibleNavierStokesOperation const& rOther) = delete;
+
+    /// This operator is provided to call the process as a function and simply calls the Execute method.
+    void operator()()
+    {
+        Execute();
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief This method creates an pointer of the operation
+     * @details We consider as input a Mmodel and a set of Parameters for the sake of generality
+     * @param rModel The model to be consider
+     * @param ThisParameters The configuration parameters
+     */
+    Operation::Pointer Create(
+        Model& rModel,
+        Parameters ThisParameters) const override;
+
+    /**
+     * @brief This method provides the defaults parameters to avoid conflicts between the different constructors
+     */
+    const Parameters GetDefaultParameters() const override;
+
+    /**
+     * @brief Execute method is used to execute the Operation algorithms.
+     */
+    void Execute() override;
+
+    ///@}
+private:
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    std::vector<const Variable<array_1d<double, 3>>*>    mArrayVariablesList;
+    std::vector<const Variable<double>*>                 mDoubleVariablesList;
+    Model* mpModel = nullptr;
+    Parameters mParameters;
+
+    ///@}
+}; // Class PotentialToCompressibleNavierStokesOperation
+
+    ///@} addtogroup block
+
+}  // namespace Kratos.
