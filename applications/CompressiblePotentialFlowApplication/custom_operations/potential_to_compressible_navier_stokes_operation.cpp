1f5466b5eb77264fc5cea6e162b0868745b998c1
marco1410 <marcozuniga1410@gmail.com>
2023-04-27 10:03:37 +0200

    little changes

--- applications/CompressiblePotentialFlowApplication/custom_operations/potential_to_compressible_navier_stokes_operation.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_operations/potential_to_compressible_navier_stokes_operation.cpp
@@ -16,6 +16,7 @@
 // External includes
 
 // Project includes
+
 #include "utilities/parallel_utilities.h"
 #include "potential_to_compressible_navier_stokes_operation.h"
 #include "fluid_dynamics_application_variables.h"
@@ -64,8 +65,8 @@ void PotentialToCompressibleNavierStokesOperation::Execute()
     auto& r_origin_model_part = mpModel->GetModelPart(origin_model_part_name);
     auto& r_destination_model_part = mpModel->GetModelPart(destination_model_part_name);
 
-    const int n_orig_nodes = mOriginModelPart.NumberOfNodes();
-    const int n_dest_nodes = mDestinationModelPart.NumberOfNodes();
+    const int n_orig_nodes = r_origin_model_part.NumberOfNodes();
+    const int n_dest_nodes = r_destination_model_part.NumberOfNodes();
 
     // Check number of nodes
     KRATOS_ERROR_IF_NOT(n_orig_nodes == n_dest_nodes)
@@ -74,43 +75,43 @@ void PotentialToCompressibleNavierStokesOperation::Execute()
         << "\n\t- Number of destination nodes: " << n_dest_nodes << std::endl;
 
     // Getting the required variables
-    const double gamma = mOriginModelPart.GetProcessInfo().GetValue(HEAT_CAPACITY_RATIO);
-    const double sound_velocity = mOriginModelPart.GetProcessInfo().GetValue(SOUND_VELOCITY);
-    const double free_stream_rho = mOriginModelPart.GetProcessInfo().GetValue(FREE_STREAM_DENSITY);
-    const double free_stream_mach = mOriginModelPart.GetProcessInfo().GetValue(FREE_STREAM_MACH);
-    const double specific_heat = (std::pow(sound_velocity,2) / (gamma * reference_temperature)) / (gamma - 1.0);
+    const double heat_capacity_ratio = r_origin_model_part.GetProcessInfo().GetValue(HEAT_CAPACITY_RATIO);
+    const double sound_velocity = r_origin_model_part.GetProcessInfo().GetValue(SOUND_VELOCITY);
+    const double free_stream_density = r_origin_model_part.GetProcessInfo().GetValue(FREE_STREAM_DENSITY);
+    const double free_stream_mach = r_origin_model_part.GetProcessInfo().GetValue(FREE_STREAM_MACH);
+    const double specific_heat = (std::pow(sound_velocity,2) / (heat_capacity_ratio * reference_temperature)) / (heat_capacity_ratio - 1.0);
     
     // Calculate de nodal velocities
     if (compute_nodal_velocities){
         // Construct the ComputeNodalPotentialFlowVelocityProcess
         const std::vector<std::string> variable_array = {"VELOCITY"};
-        ComputeNodalValueProcess ComputeNodalValueProcess(mOriginModelPart, variable_array);
+        ComputeNodalValueProcess ComputeNodalValueProcess(r_origin_model_part, variable_array);
 
         // Execute the ComputeNodalPotentialFlowVelocityProcess
         ComputeNodalValueProcess.Execute();}
 
     // Transfer the potential flow values as initial condition for the compressible problem
     IndexPartition<std::size_t>(n_orig_nodes).for_each([&](std::size_t index)
-                                                       {
-        auto it_dest_node = mDestinationModelPart.NodesBegin() + index;
-        const auto it_orig_node = mOriginModelPart.NodesBegin() + index;
+    {
+        auto it_dest_node = r_destination_model_part.NodesBegin() + index;
+        const auto it_orig_node = r_origin_model_part.NodesBegin() + index;
 
         // Getting potential flow velocities
         const auto &r_velocity = it_orig_node->GetValue(VELOCITY);
         
         // Calculate the conservative variables
-        const double velocity_norm_2 = velocity[0] * velocity[0] + velocity[1] * velocity[1];
+        const double velocity_norm_2 = r_velocity[0] * r_velocity[0] + r_velocity[1] * r_velocity[1] + r_velocity[2] * r_velocity[2];
         const double velocity_norm = std::sqrt(velocity_norm_2);
         const double mach = velocity_norm / sound_velocity;
-        const double num = 1.0 + 0.5 * (gamma - 1.0) * std::pow(free_stream_mach,2);
-        const double den = 1.0 + 0.5 * (gamma - 1.0) * std::pow(mach,2);
-        const double density = free_stream_rho * std::pow((num / den),(1.0 / (gamma - 1.0)));
+        const double num = 1.0 + 0.5 * (heat_capacity_ratio - 1.0) * std::pow(free_stream_mach,2);
+        const double den = 1.0 + 0.5 * (heat_capacity_ratio - 1.0) * std::pow(mach,2);
+        const double density = free_stream_density * std::pow((num / den),(1.0 / (heat_capacity_ratio - 1.0)));
         const double energy = specific_heat * reference_temperature + 0.5 * velocity_norm_2;
         
         // Setting the Navier Stokes initial condition
         it_dest_node->FastGetSolutionStepValue(DENSITY) = density; 
-        it_dest_node->FastGetSolutionStepValue(MOMENTUM) = density*velocity;
-        it_dest_node->FastGetSolutionStepValue(TOTAL_ENERGY) = density*energy;
+        it_dest_node->FastGetSolutionStepValue(MOMENTUM) = density * r_velocity;
+        it_dest_node->FastGetSolutionStepValue(TOTAL_ENERGY) = density * energy;
     });
 
     KRATOS_CATCH("")
