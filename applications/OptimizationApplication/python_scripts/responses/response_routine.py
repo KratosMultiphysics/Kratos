0f5ffb8f36e28d65b43c6cbac33422bee98b83f5
sunethwarna <suneth.warna@gmail.com>
2023-04-28 09:25:52 +0200

    minor

--- applications/OptimizationApplication/python_scripts/responses/response_routine.py
+++ applications/OptimizationApplication/python_scripts/responses/response_routine.py
@@ -1,7 +1,6 @@
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.OptimizationApplication.utilities.optimization_info import OptimizationInfo
-from KratosMultiphysics.OptimizationApplication.responses.response_function import ResponseFunction
 from KratosMultiphysics.OptimizationApplication.controls.master_control import MasterControl
 
 class ResponseRoutine:
@@ -23,17 +22,53 @@ class ResponseRoutine:
 
         response_data = self.__optimization_info.GetReponseData(self.__response_name)
 
+        # now set the response value in response data container
         if len(modified_model_parts) > 0:
-            # if there are any modified model parts, then this changes state of is_executed in
-            # the execution policies which has some intersection with the modified
-            #  model parts to false.
+            # if there are any modified model parts, then this changes state of is_executed to false
+            # in the execution policies which has some intersection with the modified model parts.
             ChangeExecutionPolicyStates(modified_model_parts, False, self.__optimization_info)
-            return self.__response.CalculateValue()
-        else:
-            # nothing has changed in the model parts. Hence
-            if response_data.HasValue("value"):
-                return response_data.GetValue("value")
+            response_data.SetValue("value", self.__response.CalculateValue())
+
+        if not response_data.HasValue("value"):
+            # this can happen, if there is no change in model part and we need the response value
+            # for a new design iteration, but the response is already converged.
+            # then we try to get the response value from the previous iteration.
+            if response_data.HasValue("value", 1):
+                response_data.SetValue("value", response_data.GetValue("value", 1))
             else:
+                # this block is reached usually in the initial iteration where no model parts are updated
+                # as well as no value is found for this response in current or previous iteration.
+                # then we need to calculate the response value.
+                response_data.SetValue("value", self.__response.CalculateValue())
 
+        # return response value from the container
+        return response_data.GetValue("value")
 
+    def CalculateGradient(self, control_space_updates: KratosOA.ContainerExpression.CollectiveExpressions) -> KratosOA.ContainerExpression.CollectiveExpressions:
+        #
+        # update using the master control and get updated states.
+        update_states = self.__master_control.Update(control_space_updates)
 
+        # now get modified model parts
+        modified_model_parts: 'list[Kratos.ModelPart]' = []
+        for control, is_updated in update_states.items():
+            if is_updated:
+                modified_model_parts.append(control.GetEmptyControlField().GetModelPart())
+
+        # create the required physical control fields to compute gradients
+        required_physical_gradients = self.__master_control.GetPhysicalVariableCollectiveExpressionsMap()
+
+        # now check which are the dependent physical space variables for the response, if not then remove
+        # that variable
+        for required_physical_variable in required_physical_gradients.keys():
+            if required_physical_variable not in self.__response.GetDependentPhysicalVariable():
+                del required_physical_gradients[required_physical_variable]
+
+        response_data = self.__optimization_info.GetReponseData(self.__response_name)
+
+        # now set the response value in response data container
+        if len(modified_model_parts) > 0:
+            # if there are any modified model parts, then this changes state of is_executed to false
+            # in the execution policies which has some intersection with the modified model parts.
+            ChangeExecutionPolicyStates(modified_model_parts, False, self.__optimization_info)
+            response_data.SetValue("value", self.__response.CalculateValue())
