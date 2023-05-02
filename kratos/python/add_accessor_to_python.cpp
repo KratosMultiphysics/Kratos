cb42a6ada5eff9bff3eb1e65495f7c50ac00d148
Alejandro <alejandro.cornejo23@gmail.com>
2023-05-02 09:12:39 +0400

    registering all methods Accessor to python

--- kratos/python/add_accessor_to_python.cpp
+++ kratos/python/add_accessor_to_python.cpp
@@ -34,7 +34,42 @@ void AddAccessorToPython(pybind11::module& m)
             { 
                 return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
             })
-
+        .def("GetValue", [&](Accessor &self, Variable<int> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<bool> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<Vector> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<Matrix> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<std::string> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<array_1d<double, 3>> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<array_1d<double, 4>> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<array_1d<double, 6>> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+        .def("GetValue", [&](Accessor &self, Variable<array_1d<double, 9>> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
         ;
 }
 
