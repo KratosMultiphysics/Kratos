c5f789a812237c510f7130b2ca60a13d3ab22754
Alejandro <alejandro.cornejo23@gmail.com>
2023-05-02 08:44:41 +0400

    adding add_accessor_to_python files

---
+++ kratos/python/add_accessor_to_python.cpp
@@ -0,0 +1,34 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Alejandro Cornejo
+//
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define_python.h"
+#include "sources/accessor.h"
+
+
+namespace Kratos::Python
+{
+
+void AddAccessorToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+
+}
+
+}  // namespace Kratos::Python.
+
