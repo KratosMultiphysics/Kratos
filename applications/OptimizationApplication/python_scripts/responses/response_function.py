2e0d1072223694f0b2e81292eb01c03f0d57e58c
sunethwarna <suneth.warna@gmail.com>
2023-05-02 16:06:27 +0200

    minor

--- applications/OptimizationApplication/python_scripts/responses/response_function.py
+++ applications/OptimizationApplication/python_scripts/responses/response_function.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Any
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import SupportedSensitivityFieldVariableTypes
