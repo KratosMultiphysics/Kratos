99973455d0364720f3761a5ba3984d6d498c8bd2
Alejandro <alejandro.cornejo23@gmail.com>
2023-05-02 09:10:24 +0400

    exporting accessor double GetValue

--- kratos/python/add_accessor_to_python.cpp
+++ kratos/python/add_accessor_to_python.cpp
@@ -18,6 +18,7 @@
 // Project includes
 #include "includes/define_python.h"
 #include "python/add_accessor_to_python.h"
+#include "includes/properties.h"
 #include "includes/accessor.h"
 
 namespace Kratos::Python
@@ -27,7 +28,14 @@ void AddAccessorToPython(pybind11::module& m)
 {
     namespace py = pybind11;
 
-    py::class_<Accessor, Accessor::UniquePointer>(m, "Accessor");
+    py::class_<Accessor, Accessor::UniquePointer>(m, "Accessor")
+        .def(py::init<>())
+        .def("GetValue", [&](Accessor &self, Variable<double> &rVariable, Properties &rProperties, Accessor::GeometryType &rGeometry, Vector &rShapeFunctionVector, ProcessInfo &rProcessInfo)
+            { 
+                return self.GetValue(rVariable, rProperties, rGeometry, rShapeFunctionVector, rProcessInfo); 
+            })
+
+        ;
 }
 
 }  // namespace Kratos::Python.
