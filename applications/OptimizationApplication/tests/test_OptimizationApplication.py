5634eb675cd2768b7e8732a97da30989606313e9
sunethwarna <suneth.warna@gmail.com>
2023-05-03 17:39:12 +0200

    test import change

--- applications/OptimizationApplication/tests/test_OptimizationApplication.py
+++ applications/OptimizationApplication/tests/test_OptimizationApplication.py
@@ -26,7 +26,7 @@ import test_container_expression
 import test_collective_expressions
 import test_buffered_dict
 import test_standardized_responses
-import test_material_properties_control
+import control.material.test_material_properties_control
 
 # Nightly tests
 
@@ -73,7 +73,7 @@ def AssembleTestSuites():
 
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_model_part_controllers.TestMdpaModelPartController]))
 
-    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_material_properties_control.TestMaterialPropertiesControl]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([control.material.test_material_properties_control.TestMaterialPropertiesControl]))
 
     # Adding nightly tests (tests that take < 10min)
     nightSuite = suites['nightly']
