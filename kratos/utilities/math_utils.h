315230476f413f2c881c609f8b2b2b29671e75ab
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-05-15 12:53:12 +0200

    Move Solve method to cpp

--- kratos/utilities/math_utils.h
+++ kratos/utilities/math_utils.h
@@ -313,16 +313,7 @@ public:
         MatrixType A,
         VectorType& rX,
         const VectorType& rB
-        )
-    {
-        const SizeType size1 = A.size1();
-        rX = rB;
-        typedef permutation_matrix<SizeType> pmatrix;
-        pmatrix pm(size1);
-        int singular = lu_factorize(A,pm);
-        KRATOS_DEBUG_ERROR_IF(singular == 1) << "Matrix is singular: " << A << std::endl;
-        lu_substitute(A, pm, rX);
-    }
+        );
 
     /**
      * @brief It inverts matrices of order 2, 3 and 4
