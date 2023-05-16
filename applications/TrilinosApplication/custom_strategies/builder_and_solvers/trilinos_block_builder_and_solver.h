aec3b92f5ba04eeb76cdbaf8d4d4f20bdf375ab4
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-05-16 11:12:21 +0200

    Minor

--- applications/TrilinosApplication/custom_strategies/builder_and_solvers/trilinos_block_builder_and_solver.h
+++ applications/TrilinosApplication/custom_strategies/builder_and_solvers/trilinos_block_builder_and_solver.h
@@ -992,8 +992,8 @@ protected:
     double mScaleFactor = 1.0;         /// The manually set scale factor
 
     /* Flags */
-    SCALING_DIAGONAL mScalingDiagonal = SCALING_DIAGONAL::CONSIDER_MAX_DIAGONAL;; /// We identify the scaling considered for the dirichlet dofs
-    Flags mOptions;                    /// Some flags used internally
+    SCALING_DIAGONAL mScalingDiagonal = SCALING_DIAGONAL::CONSIDER_MAX_DIAGONAL; /// We identify the scaling considered for the dirichlet dofs
+    Flags mOptions;                                                              /// Some flags used internally
 
     ///@}
     ///@name Protected Operators
