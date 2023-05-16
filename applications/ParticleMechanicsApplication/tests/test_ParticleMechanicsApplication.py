ebd66c32f6198213212e6ce6bf215b8ba82f3319
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2023-05-16 10:07:49 +0000

    Add tests for `ParticleVtkOutput` process

--- applications/ParticleMechanicsApplication/tests/test_ParticleMechanicsApplication.py
+++ applications/ParticleMechanicsApplication/tests/test_ParticleMechanicsApplication.py
@@ -60,6 +60,7 @@ from test_generate_mpm_particle_condition   import TestGenerateMPMParticleCondit
 from test_particle_erase_process            import TestParticleEraseProcess           as TTestParticleEraseProcess
 from test_search_mpm_particle               import TestSearchMPMParticle              as TTestSearchMPMParticle
 from test_search_mpm_particle_condition     import TestSearchMPMParticleCondition     as TTestSearchMPMParticleCondition
+from test_particle_vtk_output_process       import TestParticleVtkOutputProcess       as TTestParticleVtkOutputProcess
 from test_static_loading_conditions_point   import TestStaticLoadingConditionsPoint   as TTestStaticLoadingConditionsPoint
 from test_static_loading_conditions_line    import TestStaticLoadingConditionsLine    as TTestStaticLoadingConditionsLine
 from test_static_loading_conditions_surface import TestStaticLoadingConditionsSurface as TTestStaticLoadingConditionsSurface
@@ -92,6 +93,7 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TTestParticleEraseProcess]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TTestSearchMPMParticle]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TTestSearchMPMParticleCondition]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TTestParticleVtkOutputProcess]))
 
     # TODO: Look further into these three tests as they are still failing for AMatrix
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TTestStaticLoadingConditionsPoint]))    # FIXME:
