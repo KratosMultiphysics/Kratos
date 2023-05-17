b83dcdb38ddba8c509ccbefd36a88f74aad9d34a
Vicente Mataix Ferándiz <vmataix@altair.com>
2023-05-17 13:02:32 +0000

    Running all MPI tests, not just Core ones

--- kratos/python_scripts/testing/run_cpp_mpi_tests.py
+++ kratos/python_scripts/testing/run_cpp_mpi_tests.py
@@ -3,10 +3,9 @@ import sys
 from KratosMultiphysics import mpi, Tester
 
 if __name__ == '__main__':
-    #Tester.SetVerbosity(Tester.Verbosity.TESTS_LIST)
-    Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
-
     if len(sys.argv) < 2:
-        Tester.RunTestSuite("KratosMPICoreFastSuite")
-    else:
+        Tester.SetVerbosity(Tester.Verbosity.TESTS_LIST)
+        Tester.RunAllDistributedTestCases()
+    else :
+        Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
         Tester.RunTestCases(sys.argv[1])
