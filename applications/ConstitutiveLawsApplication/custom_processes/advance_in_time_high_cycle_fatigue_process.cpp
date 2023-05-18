5287694ada3775774ab696b4c156b232e383afbd
Luis Antônio Gonçalves Junior <lagoncalves@cimne.upc.edu>
2023-05-18 12:26:10 +0200

    AITS debug adjust

--- applications/ConstitutiveLawsApplication/custom_processes/advance_in_time_high_cycle_fatigue_process.cpp
+++ applications/ConstitutiveLawsApplication/custom_processes/advance_in_time_high_cycle_fatigue_process.cpp
@@ -221,15 +221,6 @@ void AdvanceInTimeHighCycleFatigueProcess::StableConditionForAdvancingStrategy(b
         }
     }
 
-    // if (time > 6.15986000000e+05 && (acumulated_max_stress_rel_error >= 1e-3 || acumulated_rev_factor_rel_error >= 1e-3)){
-        
-    //     KRATOS_WATCH(local_number_of_cycles)
-    //     KRATOS_WATCH(DamageIndicator)
-    //     KRATOS_WATCH(fatigue_in_course)
-    //     KRATOS_WATCH(acumulated_max_stress_rel_error)
-    //     KRATOS_WATCH(acumulated_rev_factor_rel_error)
-    // }
-
     if ((acumulated_max_stress_rel_error < 1e-4 && acumulated_rev_factor_rel_error < 1e-4 && fatigue_in_course) || (DamageIndicator && acumulated_max_stress_rel_error < 3e-3 && acumulated_rev_factor_rel_error < 3e-3 && fatigue_in_course)) {
         rAdvancingStrategy = true;
     }
