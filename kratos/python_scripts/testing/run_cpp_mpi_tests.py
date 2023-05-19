74dd1d12d462fc1258d3927410a81916751ff736
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-05-19 15:15:19 +0200

    Correctly parse --using-mpi if given

--- kratos/python_scripts/testing/run_cpp_mpi_tests.py
+++ kratos/python_scripts/testing/run_cpp_mpi_tests.py
@@ -1,11 +1,27 @@
-import sys
+import argparse
 
 from KratosMultiphysics import mpi, Tester
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--using-mpi',
+        action='store_true',
+        help="Force MPI mode if auto-detection fails")
+    parser.add_argument(
+        'match_string',
+        metavar='MATCH-STRING',
+        nargs='?',
+        help="run cases with names matching MATCH-STRING")
+
+    args = parser.parse_args()
+
+    if args.match_string:
+        Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
+        Tester.RunTestCases(args.match_string)
+    else:
         Tester.SetVerbosity(Tester.Verbosity.TESTS_LIST)
         Tester.RunAllDistributedTestCases()
-    else :
-        Tester.SetVerbosity(Tester.Verbosity.TESTS_OUTPUTS)
-        Tester.RunTestCases(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
