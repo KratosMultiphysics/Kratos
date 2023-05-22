3e159424d82e7a662202aee4a88ad991f608a50b
sunethwarna <suneth.warna@gmail.com>
2023-05-22 13:06:01 +0200

    update vtu python

--- kratos/python_scripts/vtu_output_process.py
+++ kratos/python_scripts/vtu_output_process.py
@@ -15,23 +15,22 @@ class VtuOutputProcess(Kratos.OutputProcess):
     def GetDefaultParameters(self) -> Kratos.Parameters:
         return Kratos.Parameters("""
         {
-            "model_part_name"                             : "PLEASE_SPECIFY_MODEL_PART_NAME",
-            "file_format"                                 : "binary",
-            "output_precision"                            : 7,
-            "output_control_type"                         : "step",
-            "output_interval"                             : 1.0,
-            "output_sub_model_parts"                      : false,
-            "output_path"                                 : "VTU_Output",
-            "custom_name_prefix"                          : "",
-            "save_output_files_in_folder"                 : true,
-            "write_deformed_configuration"                : false,
-            "nodal_solution_step_data_variables"          : [],
-            "nodal_data_value_variables"                  : [],
-            "nodal_flags"                                 : [],
-            "element_data_value_variables"                : [],
-            "element_flags"                               : [],
-            "condition_data_value_variables"              : [],
-            "condition_flags"                             : []
+            "model_part_name"                   : "PLEASE_SPECIFY_MODEL_PART_NAME",
+            "file_format"                       : "binary",
+            "output_precision"                  : 7,
+            "output_control_type"               : "step",
+            "output_interval"                   : 1.0,
+            "output_sub_model_parts"            : false,
+            "output_path"                       : "VTU_Output",
+            "save_output_files_in_folder"       : true,
+            "write_deformed_configuration"      : false,
+            "nodal_solution_step_data_variables": [],
+            "nodal_data_value_variables"        : [],
+            "nodal_flags"                       : [],
+            "element_data_value_variables"      : [],
+            "element_flags"                     : [],
+            "condition_data_value_variables"    : [],
+            "condition_flags"                   : []
 
         }""")
 
@@ -44,7 +43,14 @@ class VtuOutputProcess(Kratos.OutputProcess):
         self.write_deformed_configuration = parameters["write_deformed_configuration"].GetBool()
         self.output_precision = parameters["output_precision"].GetInt()
         self.output_sub_model_parts = parameters["output_sub_model_parts"].GetBool()
-        self.custom_name_prefix = parameters["custom_name_prefix"].GetString()
+
+        self.nodal_solution_step_data_variables = parameters["nodal_solution_step_data_variables"].GetStringArray()
+        self.nodal_data_value_variables = parameters["nodal_data_value_variables"].GetStringArray()
+        self.nodal_flags = parameters["nodal_flags"].GetStringArray()
+        self.element_data_value_variables = parameters["element_data_value_variables"].GetStringArray()
+        self.element_flags = parameters["element_flags"].GetStringArray()
+        self.condition_data_value_variables = parameters["condition_data_value_variables"].GetStringArray()
+        self.condition_flags = parameters["condition_flags"].GetStringArray()
 
         file_format = parameters["file_format"].GetString()
         if file_format == "ascii":
@@ -55,17 +61,14 @@ class VtuOutputProcess(Kratos.OutputProcess):
             raise RuntimeError(f"Unsupported file format requested [ requested format = {file_format} ]. Supported file formats:\n\tascii\n\tbinary")
 
         if parameters["save_output_files_in_folder"].GetBool():
-            output_path = parameters["output_path"].GetString()
+            self.output_path = Path(parameters["output_path"].GetString())
             if not self.model_part.ProcessInfo[Kratos.IS_RESTARTED]:
-                kratos_utils.DeleteDirectoryIfExisting(output_path)
+                kratos_utils.DeleteDirectoryIfExisting(str(self.output_path))
             self.model_part.GetCommunicator().GetDataCommunicator().Barrier()
             # now create the output path
-            p_output_path = Path(output_path)
-            p_output_path.mkdir(parents=True, exist_ok=True)
-            self.p_output_file_name_prefix = p_output_path / self.custom_name_prefix
+            self.output_path.mkdir(parents=True, exist_ok=True)
         else:
-            self.p_output_file_name_prefix = Path(self.custom_name_prefix)
-
+            self.output_path = Path(".")
 
         self.output_interval = parameters["output_interval"].GetDouble()
         self.output_control = parameters["output_control_type"].GetString()
@@ -79,24 +82,26 @@ class VtuOutputProcess(Kratos.OutputProcess):
         # check and create all the vtu outputs
         self.vtu_output_ios.append(Kratos.VtuOutput(self.model_part, not self.write_deformed_configuration, self.writer_format, self.output_precision))
 
-
         if self.output_sub_model_parts:
             for sub_model_part in self.model_part.SubModelParts():
                 self.vtu_output_ios.append(Kratos.VtuOutput(sub_model_part, not self.write_deformed_configuration, self.writer_format, self.output_precision))
 
-    def PrintOutput(self):
+        for vtu_output_io in self.vtu_output_ios:
+            self.__AddData(vtu_output_io)
+
+    def PrintOutput(self) -> None:
         for vtu_output in self.vtu_output_ios:
-            vtu_output.PrintOutput(str(self.p_output_file_name_prefix) + vtu_output.GetModelPart().FullName())
+            vtu_output.PrintOutput(str(self.output_path / vtu_output.GetModelPart().FullName()))
 
         self.__ScheduleNextOutput()
 
-    def IsOutputStep(self):
+    def IsOutputStep(self) -> bool:
         if self.output_control == "time":
             return self.__GetTime() >= self.next_output
         else:
             return self.model_part.ProcessInfo[Kratos.STEP] >= self.next_output
 
-    def __ScheduleNextOutput(self):
+    def __ScheduleNextOutput(self) -> None:
         if self.output_interval > 0.0: # Note: if == 0, we'll just always print
             if self.output_control == "time":
                 while self.next_output <= self.__GetTime():
@@ -105,7 +110,25 @@ class VtuOutputProcess(Kratos.OutputProcess):
                 while self.next_output <= self.model_part.ProcessInfo[Kratos.STEP]:
                     self.next_output += self.output_interval
 
-    def __GetTime(self):
+    def __GetTime(self) -> float:
         # remove rounding errors that mess with the comparison
         # e.g. 1.99999999999999999 => 2.0
         return float("{0:.12g}".format(self.model_part.ProcessInfo[Kratos.TIME]))
+
+    def __AddData(self, vtu_output_io: Kratos.VtuOutput) -> None:
+        for variable in self.nodal_solution_step_data_variables:
+            vtu_output_io.AddHistoricalVariable(Kratos.KratosGlobals.GetVariable(variable))
+        for variable in self.nodal_data_value_variables:
+            vtu_output_io.AddNonHistoricalVariable(Kratos.KratosGlobals.GetVariable(variable), Kratos.VtuOutput.NODES)
+        for variable in self.condition_data_value_variables:
+            vtu_output_io.AddNonHistoricalVariable(Kratos.KratosGlobals.GetVariable(variable), Kratos.VtuOutput.CONDITIONS)
+        for variable in self.element_data_value_variables:
+            vtu_output_io.AddNonHistoricalVariable(Kratos.KratosGlobals.GetVariable(variable), Kratos.VtuOutput.ELEMENTS)
+
+        for flag in self.nodal_flags:
+            vtu_output_io.AddFlagVariable(flag, Kratos.KratosGlobals.GetFlag(flag), Kratos.VtuOutput.NODES)
+        for flag in self.condition_flags:
+            vtu_output_io.AddFlagVariable(flag, Kratos.KratosGlobals.GetFlag(flag), Kratos.VtuOutput.CONDITIONS)
+        for flag in self.element_flags:
+            vtu_output_io.AddFlagVariable(flag, Kratos.KratosGlobals.GetFlag(flag), Kratos.VtuOutput.ELEMENTS)
+
