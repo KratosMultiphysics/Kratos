1177c2df016e7fe7e5ac51bcca237e0b2952d261
sunethwarna <suneth.warna@gmail.com>
2023-05-22 19:11:40 +0200

    add comments

--- kratos/input_output/vtu_output.h
+++ kratos/input_output/vtu_output.h
@@ -29,6 +29,14 @@
 
 
 namespace Kratos {
+/**
+ * @brief Class to output Kratos Flags, Variables and ContainerExpressions
+ *        to vtu. Supports both shared and distributed memory architectures.
+ *
+ * This class does not create or destroy any folder structures, hence the output
+ * file name prefix should be have a valid parent directory.
+ *
+ */
 class KRATOS_API(KRATOS_CORE) VtuOutput : public IO
 {
 public:
@@ -81,6 +89,16 @@ public:
     ///@name Life cycle
     ///@{
 
+    /**
+     * @brief Construct a new Vtu Output IO
+     *
+     * Constructs a new VtuOuput IO instance with the given parameters.
+     *
+     * @param rModelPart                Model part to be used.
+     * @param IsInitialConfiguration    If true, the initial configuration is written.
+     * @param OutputFormat              Output format. Either ASCII or BINARY supported.
+     * @param Precision                 Precision of the double output.
+     */
     VtuOutput(
         ModelPart& rModelPart,
         const bool IsInitialConfiguration = true,
@@ -91,19 +109,52 @@ public:
     ///@name Public operations
     ///@{
 
+    /**
+     * @brief Adds historical variables to the output.
+     *
+     * @tparam TDataType
+     * @param rVariable
+     */
     template<class TDataType>
     void AddHistoricalVariable(const Variable<TDataType>& rVariable);
 
+    /**
+     * @brief Adds non historical variables to the output.
+     *
+     * @tparam TDataType
+     * @param rVariable         Variable to be added.
+     * @param rEntityFlags      Considered container for the variable. Either NODES, CONDITIONS or ELEMENTS
+     */
     template<class TDataType>
     void AddNonHistoricalVariable(
         const Variable<TDataType>& rVariable,
         const Flags& rEntityFlags);
 
+    /**
+     * @brief Adds flag output.
+     *
+     * @param rFlagName         Flag name.
+     * @param rFlagVariable     Variable to be added.
+     * @param rEntityFlags      Considered container for the variable. Either NODES, CONDITIONS or ELEMENTS
+     */
     void AddFlagVariable(
         const std::string& rFlagName,
         const Flags& rFlagVariable,
         const Flags& rEntityFlags);
 
+    /**
+     * @brief Adds container expressions to the vtu output.
+     *
+     * This adds container expressions to the output. Proper care should be taken when updating ContainerExpressions because
+     * In python, when a container expression is assigned with a new container expression, it does not call the assignment operator.
+     * Hence, the new expression takes place. Therefore, when container expressions required to be outputted, then it is best
+     * to always clear the existing container expressions and add the new ones. Otherwise, the vtu output may be writing not the
+     * latest container expression.
+     *
+     * @tparam TContainerType
+     * @param rExpressionName           Name for the container expression.
+     * @param pContainerExpression      Container expression.
+     */
     template <class TContainerType>
     void AddContainerExpression(
         const std::string& rExpressionName,
@@ -125,6 +176,16 @@ public:
 
     const ModelPart& GetModelPart() const;
 
+    /**
+     * @brief Writes the Vtu file.
+     *
+     * This writes the final vtu file. If this is a transient output, then @ref rOutputFilenamePrefix
+     * should have indication of the step, otherwise this will overwrite the same file.
+     *
+     * In the MPI case, this will create one .vtu file per rank, and a .pvtu file to combine them.
+     *
+     * @param rOutputFilenamePrefix         Output file name prefix.
+     */
     void PrintOutput(const std::string& rOutputFilenamePrefix);
 
     ///@}
