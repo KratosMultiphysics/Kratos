d31ff1d62c14c995a22cabfc19a9bc56e8e0a758
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-05-26 18:30:24 +0200

    More advances

--- kratos/spatial_containers/spatial_search_result_container.cpp
+++ kratos/spatial_containers/spatial_search_result_container.cpp
@@ -11,6 +11,7 @@
 //
 
 // System includes
+#include <functional>
 
 // External includes
 
@@ -24,8 +25,7 @@ namespace Kratos
 {
 
 template <class TObjectType>
-SpatialSearchResultContainer<TObjectType>::SpatialSearchResultContainer(const DataCommunicator& rDataCommunicator)
-    : mrDataCommunicator(rDataCommunicator)
+SpatialSearchResultContainer<TObjectType>::SpatialSearchResultContainer()
 {
     // TODO: Add something if required
 }
@@ -41,7 +41,7 @@ void SpatialSearchResultContainer<TObjectType>::AddResult(SpatialSearchResult<TO
     mLocalPointers.push_back(Kratos::intrusive_ptr<TObjectType>(p_local_result));
 
     // Add distances
-    const std::size_t id = p_local_result->Id();
+    const IndexType id = p_local_result->Id();
     mLocalDistances.insert({id, rResult.GetDistance()});
 }
 
@@ -65,13 +65,13 @@ void SpatialSearchResultContainer<TObjectType>::Clear()
 /***********************************************************************************/
 
 template <class TObjectType>
-void SpatialSearchResultContainer<TObjectType>::SynchronizeAll()
+void SpatialSearchResultContainer<TObjectType>::SynchronizeAll(const DataCommunicator& rDataCommunicator)
 {
     // Synchronize local pointers to global pointers
-    mGlobalPointers = GlobalPointerUtilities::GlobalRetrieveGlobalPointers(mLocalPointers, mrDataCommunicator);
+    mGlobalPointers = GlobalPointerUtilities::GlobalRetrieveGlobalPointers(mLocalPointers, rDataCommunicator);
 
     // Generate the communicator
-    mpGlobalPointerCommunicator = Kratos::make_shared<GlobalPointerCommunicator<TObjectType>>(mrDataCommunicator, mGlobalPointers.ptr_begin(), mGlobalPointers.ptr_end());
+    mpGlobalPointerCommunicator = Kratos::make_shared<GlobalPointerCommunicator<TObjectType>>(rDataCommunicator, mGlobalPointers.ptr_begin(), mGlobalPointers.ptr_end());
 }
 
 /***********************************************************************************/
@@ -100,6 +100,9 @@ void SpatialSearchResultContainer<TObjectType>::PrintInfo(std::ostream& rOStream
 template <class TObjectType>
 void SpatialSearchResultContainer<TObjectType>::PrintData(std::ostream& rOStream) const
 {
+    rOStream << "SpatialSearchResultContainer data summary: " << "\n";
+    rOStream << "\tNumber of local pointers: " << mLocalPointers.size() << "\n";
+    rOStream << "\tNumber of global pointers: " << mGlobalPointers.size() << "\n";
 }
 
 /***********************************************************************************/
@@ -110,6 +113,7 @@ void SpatialSearchResultContainer<TObjectType>::save(Serializer& rSerializer) co
 { 
     rSerializer.save("LocalPointers", mLocalPointers);
     rSerializer.save("GlobalPointers", mGlobalPointers);
+    rSerializer.save("LocalDistances", mLocalDistances);
     //rSerializer.save("GlobalPointerCommunicator", mpGlobalPointerCommunicator); // Not necessary, is created and filled during use
 }
 
@@ -121,6 +125,7 @@ void SpatialSearchResultContainer<TObjectType>::load(Serializer& rSerializer)
 {
     rSerializer.load("LocalPointers", mLocalPointers);
     rSerializer.load("GlobalPointers", mGlobalPointers);
+    rSerializer.load("LocalDistances", mLocalDistances);
     //rSerializer.load("GlobalPointerCommunicator", mpGlobalPointerCommunicator); // Not necessary, is created and filled during use
 }
 
@@ -128,5 +133,108 @@ void SpatialSearchResultContainer<TObjectType>::load(Serializer& rSerializer)
 /***********************************************************************************/
 
 template class SpatialSearchResultContainer<GeometricalObject>;
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+SpatialSearchResultContainer<TObjectType>& SpatialSearchResultContainerMap<TObjectType>::InitializeResult(const array_1d<double, 3>& rCoordinates)
+{
+    const HashType hash = Hash(rCoordinates);
+    // If doesn't exists, create it
+    if (!HasResult(rCoordinates)) {
+        mPointResults.insert({hash, SpatialSearchResultContainer<TObjectType>()});
+    }
+    return mPointResults[hash];
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+bool SpatialSearchResultContainerMap<TObjectType>::HasResult(const array_1d<double, 3>& rCoordinates) const
+{
+    const HashType hash = Hash(rCoordinates);
+    if (mPointResults.find(hash) != mPointResults.end()) {
+        return true;
+    }
+    return false;       
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+void SpatialSearchResultContainerMap<TObjectType>::Clear()
+{
+    mPointResults.clear();
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+typename SpatialSearchResultContainerMap<TObjectType>::HashType SpatialSearchResultContainerMap<TObjectType>::Hash(const array_1d<double, 3>& rCoordinates) const
+{
+    std::hash<array_1d<double, 3>> hasher;
+    return hasher(rCoordinates);
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+std::string SpatialSearchResultContainerMap<TObjectType>::Info() const
+{
+    std::stringstream buffer;
+    buffer << "SpatialSearchResultContainerMap" ;
+    return buffer.str();
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+void SpatialSearchResultContainerMap<TObjectType>::PrintInfo(std::ostream& rOStream) const 
+{
+    rOStream << "SpatialSearchResultContainerMap";
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+void SpatialSearchResultContainerMap<TObjectType>::PrintData(std::ostream& rOStream) const
+{
+    // Print results
+    rOStream << "SpatialSearchResultContainerMap data summary: " << "\n";
+    for (auto it = mPointResults.begin(); it != mPointResults.end(); ++it) {
+        rOStream << "Hash " << it->first << ":\n";
+        it->second.PrintData(rOStream);
+    }
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+void SpatialSearchResultContainerMap<TObjectType>::save(Serializer& rSerializer) const
+{ 
+    rSerializer.save("PointResults", mPointResults);
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template <class TObjectType>
+void SpatialSearchResultContainerMap<TObjectType>::load(Serializer& rSerializer)
+{
+    rSerializer.load("PointResults", mPointResults);
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template class SpatialSearchResultContainerMap<GeometricalObject>;
 
 }  // namespace Kratos
--- kratos/spatial_containers/spatial_search_result_container.h
+++ kratos/spatial_containers/spatial_search_result_container.h
@@ -22,6 +22,7 @@
 #include "spatial_containers/spatial_search_result.h"
 #include "containers/pointer_vector_set.h"
 #include "includes/indexed_object.h"
+#include "containers/array_1d.h"
 
 namespace Kratos
 {
@@ -51,8 +52,8 @@ public:
     /// Pointer definition of SpatialSearchResultContainer
     KRATOS_CLASS_POINTER_DEFINITION(SpatialSearchResultContainer);
 
-    /// Global pointer definition of TObjectType
-    using TPointerType = GlobalPointer<TObjectType>;
+    /// The index type
+    using IndexType = std::size_t;
 
     ///@}
     ///@name Life Cycle
@@ -60,9 +61,8 @@ public:
 
     /**
      * @brief Constructor
-     * @param rDataCommunicator The data communicator
      */
-    SpatialSearchResultContainer(const DataCommunicator& rDataCommunicator);
+    SpatialSearchResultContainer();
 
     /// Destructor.
     virtual ~SpatialSearchResultContainer() = default;
@@ -90,8 +90,9 @@ public:
     /**
      * @brief Synchronize the container between partitions
      * @details This method synchronizes the container between partitions
+     * @param rDataCommunicator The data communicator
      */
-    void SynchronizeAll();
+    void SynchronizeAll(const DataCommunicator& rDataCommunicator);
 
     /**
      * @brief Applies a user-provided function to the global pointers and return a proxy to the results.
@@ -106,9 +107,7 @@ public:
     > Apply(TFunctorType&& UserFunctor)
     {
         // Check if the communicator has been created
-        if (mpGlobalPointerCommunicator == nullptr) {
-            SynchronizeAll();
-        }
+        KRATOS_ERROR_IF(mpGlobalPointerCommunicator == nullptr) << "The communicator has not been created." << std::endl;
 
         // Apply the user-provided function
         return mpGlobalPointerCommunicator->Apply(std::forward<TFunctorType>(UserFunctor));
@@ -132,7 +131,6 @@ private:
     ///@name Member Variables
     ///@{
     
-    const DataCommunicator& mrDataCommunicator;                                                     /// The data communicator
     PointerVectorSet<TObjectType,
                      IndexedObject,
                      std::less<typename IndexedObject::result_type>,
@@ -141,12 +139,168 @@ private:
                      std::vector< typename TObjectType::Pointer >
                      > mLocalPointers;                                                              /// Local pointers of the container
     GlobalPointersVector<TObjectType> mGlobalPointers;                                              /// Global pointers of the container
-    std::unordered_map<std::size_t, double> mLocalDistances;                                        /// The local distances 
+    std::unordered_map<IndexType, double> mLocalDistances;                                          /// The local distances 
     typename GlobalPointerCommunicator<TObjectType>::Pointer mpGlobalPointerCommunicator = nullptr; /// Global pointer to the communicator 
 
     ///@}
     ///@name Private Operations
     ///@{
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    /**
+     * @brief Save the object in a serializer
+     * @param rSerializer The serializer
+     */
+    void save(Serializer& rSerializer) const;
+
+    /**
+     * @brief Load the object in a serializer
+     * @param rSerializer The serializer
+     */
+    void load(Serializer& rSerializer);
+
+    ///@}
+}; // Class SpatialSearchResultContainer
+
+/**
+ * @class SpatialSearchResultContainerMap
+ * @brief Spatial search result container map
+ * @details This class is used to store the results of a spatial search, in a map to be identify results for a given coordinates
+ * @tparam TObjectType The type of the object
+ * @ingroup KratosCore
+ * @author Vicente Mataix Ferrandiz
+ */
+template <class TObjectType>
+class KRATOS_API(KRATOS_CORE) SpatialSearchResultContainerMap
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of SpatialSearchResultContainerMap
+    KRATOS_CLASS_POINTER_DEFINITION(SpatialSearchResultContainerMap);
+
+    /// The hash type
+    using HashType = std::size_t;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Constructor.
+    SpatialSearchResultContainerMap() = default;
+
+    /// Destructor.
+    virtual ~SpatialSearchResultContainerMap() = default;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /**
+     * @brief Operator []
+     * @param rCoordinates The coordinates
+     * @return The result container
+     */
+    SpatialSearchResultContainer<TObjectType>& operator[](const array_1d<double, 3>& rCoordinates)
+    {
+        return mPointResults[Hash(rCoordinates)];
+    }
+
+    /**
+     * @brief Operator []
+     * @param rCoordinates The coordinates
+     * @return The result container
+     */
+    const SpatialSearchResultContainer<TObjectType>& operator[](const array_1d<double, 3>& rCoordinates) const
+    {
+        const HashType hash = Hash(rCoordinates);
+        const auto it = mPointResults.find(hash);
+        KRATOS_ERROR_IF(it == mPointResults.end()) << "The result container does not exist." << std::endl;
+        return it->second;
+    }
+
+    /**
+     * @brief Operator ()
+     * @param rCoordinates The coordinates
+     * @return The result container
+     */
+    SpatialSearchResultContainer<TObjectType>& operator()(const array_1d<double, 3>& rCoordinates)
+    {
+        return mPointResults[Hash(rCoordinates)];
+    }
+
+    /**
+     * @brief Operator ()
+     * @param rCoordinates The coordinates
+     * @return The result container
+     */
+    const SpatialSearchResultContainer<TObjectType>& operator()(const array_1d<double, 3>& rCoordinates) const
+    {
+        const HashType hash = Hash(rCoordinates);
+        const auto it = mPointResults.find(hash);
+        KRATOS_ERROR_IF(it == mPointResults.end()) << "The result container does not exist." << std::endl;
+        return it->second;
+    }
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief Initialize the container
+     * @param rCoordinates The coordinates
+     */
+    SpatialSearchResultContainer<TObjectType>& InitializeResult(const array_1d<double, 3>& rCoordinates);
+
+    /**
+     * @brief Check if coordinates are initialized
+     * @param rCoordinates The coordinates
+     * @return True if coordinates are initialized, false otherwise
+    */
+    bool HasResult(const array_1d<double, 3>& rCoordinates) const;
+
+    /**
+     * @brief Clear the containers
+     * @details This method clears the containers
+     */
+    void Clear();
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    virtual std::string Info() const;
+
+    /// Print information about this object.
+    virtual void PrintInfo(std::ostream& rOStream) const;
+
+    /// Print object's data.
+    virtual void PrintData(std::ostream& rOStream) const;
+
+    ///@}
+private:
+    ///@name Member Variables
+    ///@{
+    
+    std::unordered_map<HashType, SpatialSearchResultContainer<TObjectType>> mPointResults;  /// The results of each point
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    /**
+     * @brief Hash the coordinates
+     * @param rCoordinates The coordinates
+     * @return The hash
+     */
+    HashType Hash(const array_1d<double, 3>& rCoordinates) const;
 
     ///@}
     ///@name Serialization
