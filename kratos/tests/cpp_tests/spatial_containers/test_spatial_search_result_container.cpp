d268863cfc57fde788b423ceab3c6d6b470d5949
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-05-29 16:34:02 +0200

    WIP test

--- kratos/tests/cpp_tests/spatial_containers/test_spatial_search_result_container.cpp
+++ kratos/tests/cpp_tests/spatial_containers/test_spatial_search_result_container.cpp
@@ -18,10 +18,34 @@
 // Project includes
 #include "testing/testing.h"
 #include "spatial_containers/spatial_search_result.h"
+#include "spatial_containers/spatial_search_result_container.h"
 
 namespace Kratos::Testing 
 {
 
+KRATOS_TEST_CASE_IN_SUITE(SpatialSearchResultContainerAddResult, KratosCoreFastSuite)
+{
+    // Create a test object
+    SpatialSearchResultContainer<GeometricalObject> container;
 
+    // Create a test result
+    GeometricalObject object = GeometricalObject();
+    object.SetId(1);
+    SpatialSearchResult<GeometricalObject> result(&object);
+    result.SetDistance(0.5);
+
+    // Add the result to the container
+    container.AddResult(result);
+
+    // Check that the result was added correctly
+    auto& r_local_pointers = container.GetLocalPointers();
+    KRATOS_CHECK_EQUAL(r_local_pointers.size(), 1);
+
+    auto& r_distances = container.GetLocalDistances();
+    KRATOS_CHECK_EQUAL(r_distances.size(), 1);
+    KRATOS_CHECK_EQUAL(r_distances[1], 0.5);
+
+    // TODO: Memory error with destructor
+}
 
 }  // namespace Kratos::Testing
