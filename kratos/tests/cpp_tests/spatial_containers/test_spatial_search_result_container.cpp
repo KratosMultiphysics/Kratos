1e0ae8a07e18144c685c1380733d43a7910c8bab
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-05-29 14:38:52 +0200

    WIP test

---
+++ kratos/tests/cpp_tests/spatial_containers/test_spatial_search_result_container.cpp
@@ -0,0 +1,27 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "testing/testing.h"
+#include "spatial_containers/spatial_search_result.h"
+
+namespace Kratos::Testing 
+{
+
+
+
+}  // namespace Kratos::Testing
