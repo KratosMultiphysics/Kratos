b146b1e83ba4adead78c5db723b9d1147c57aa0b
Luis Antônio Gonçalves Junior <lagoncalves@cimne.upc.edu>
2023-06-01 10:45:58 +0200

    HCF tolerance adjust

--- applications/StructuralMechanicsApplication/custom_processes/set_automated_initial_variable_process.cpp
+++ applications/StructuralMechanicsApplication/custom_processes/set_automated_initial_variable_process.cpp
@@ -68,8 +68,12 @@ void SetAutomatedInitialVariableProcess::ExecuteInitialize()
 
         double centroid_relative_distance = MathUtils<double>::Norm3(radial_position_vector) - hole_radius_offset;
 
+        // if (centroid_relative_distance < tolerance){
+        //     centroid_relative_distance = 0.0;
+        // }
+
         if (centroid_relative_distance < tolerance){
-            centroid_relative_distance = 0.0;
+            centroid_relative_distance = 2.4878275E-06;
         }
 
         // if (centroid_relative_distance < 0.0){
