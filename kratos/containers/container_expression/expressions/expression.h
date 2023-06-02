0b86db39eeae05e18e5aec95a42d381de710dbd6
matekelemen <kelemen.mate.12@gmail.com>
2023-06-02 11:36:36 +0200

    declare view operators for Expression

--- kratos/containers/container_expression/expressions/expression.h
+++ kratos/containers/container_expression/expressions/expression.h
@@ -8,6 +8,7 @@
 //                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Suneth Warnakulasuriya
+//                   Máté Kelemen
 //
 
 #pragma once
@@ -184,6 +185,115 @@ public:
     IndexType GetItemComponentCount() const;
 
     ///@}
+    /// @name View Operators
+    /// @{
+
+    /** @brief Construct an expression containing a subset of the components of all items.
+     *
+     *  @details Slicing is done on each entitiy's data array, and not on the flattened
+     *           expression. For example:
+     *
+     *           Assume an @ref Expression of shape [5] and 2 entities with
+     *           the following data in the flattened representation:
+     *           @code
+     *           data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+     *                   <---- 1 ----> <----- 2 ----->
+     *           @endcode
+     *           Data for entity 1 is represented with <--1-->.
+     *
+     *           Let @code Offset = 1 @code and @code Stride = 3 @endcode. The resulting sliced expression
+     *           then represents the following data:
+     *
+     *           output_data = [2, 3, 4, 7, 8, 9]
+     *           output container shape = [3] = equal to Stride.
+     *
+     *           Slicing will always create a one dimensional array even if the input expression is multidimensional.
+     *           @see Reshape to reshape the one dimensional array to the desired shape if required.
+     *
+     *           This creates a lazy expression, hence it has a constant cost complexity irrespective of the data size.
+     *
+     *  @param Offset Index of the first component to begin slicing at.
+     *  @param Stride Number of components from the offset in the sliced item.
+     */
+    Expression::Pointer Slice(std::size_t Offset, std::size_t Stride) const;
+
+    /** @brief Construct an expression with identical data but interpreted with a new item shape.
+     *
+     *  @details Reshaping is done on each entitiy's data array, and not on the flattened
+     *           expression. For example:
+     *
+     *           Assume an @ref Expression of shape [2, 3] and 2 entities with
+     *           following data in the flattened representation:
+     *           @code
+     *           data = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+     *                   <-------- 1 --------->  <----------- 2 ----------->
+     *           @endcode
+     *           The underlying data of the reshaped expression is interpreted as follows:
+     *           @code
+     *           output_data = [[[1, 2], [3, 4], [5, 6]], [[7, 8], [9, 10], [11, 12]]]
+     *           output container shape = [3, 2]
+     *           @endcode
+     *           This creates a lazy expression, hence it has a constant cost complexity irrespective of the data size.
+     *
+     *  @param rExpression Expression to reshape.
+     *  @param NewShapeBegin Iterator pointing to the first component of the new shape.
+     *  @param NewShapeEnd Iterator past the last component of the new shape.
+     */
+    template <class TIterator>
+    Expression::Pointer Reshape(TIterator NewShapeBegin,
+                                TIterator NewShapeEnd) const;
+
+    /** @brief Construct an expression with identical data but interpreted with a new item shape.
+     *
+     *  @details Reshaping is done on each entitiy's data array, and not on the flattened
+     *           expression. For example:
+     *
+     *           Assume an @ref Expression of shape [2, 3] and 2 entities with
+     *           following data in the flattened representation:
+     *           @code
+     *           data = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+     *                   <-------- 1 --------->  <----------- 2 ----------->
+     *           @endcode
+     *           The underlying data of the reshaped expression is interpreted as follows:
+     *           @code
+     *           output_data = [[[1, 2], [3, 4], [5, 6]], [[7, 8], [9, 10], [11, 12]]]
+     *           output container shape = [3, 2]
+     *           @endcode
+     *           This creates a lazy expression, hence it has a constant cost complexity irrespective of the data size.
+     *
+     *  @param rExpression Expression to reshape.
+     *  @param rNewShape New shape to used to reshape the existing expression.
+     */
+    Expression::Pointer Reshape(const std::vector<std::size_t>& rNewShape) const;
+
+    /** @brief Returns current expression reshaped to specified shape.
+     *
+     *  @details Reshaping is done on each entitiy's data array, and not on the flattened
+     *           expression. For example:
+     *
+     *           Assume a SpecializedContainerExpression with an expression of shape [2, 3] and 2 entities with
+     *           following data values in the flattened representation.
+     *
+     *           data = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+     *                   <-------- 1 --------->  <----------- 2 ----------->
+     *
+     *           If the rShape = [3, 2] then the returned @ref SpecializedContainerExpression will have a lazy
+     *           expression which will return the following output data when Evaluate is called.
+     *
+     *           output_data = [[[1, 2], [3, 4], [5, 6]], [[7, 8], [9, 10], [11, 12]]]
+     *           output container shape = [3, 2]
+     *
+     *           This creates a lazy expression, hence it has a constant cost complexity irrespective of the data size.
+     *
+     *  @param rExpression Expression to reshape.
+     *  @param NewShapeBegin Iterator to the first component of the array defining the new shape.
+     *  @param NewShapeEnd Iterator past the last component of the new shape array.
+     */
+    Expression::Pointer Comb(const Expression::Pointer& rpOther) const;
+
+    Expression::Pointer Comb(const std::vector<Expression::Pointer>& rOthers) const;
+
+    /// @}
     ///@name Input and output
     ///@{
 
