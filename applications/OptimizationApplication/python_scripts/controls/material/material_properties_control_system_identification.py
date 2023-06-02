86292050880836fdd8007c11664a6a295e9e4b34
Fabian <f.meister@tu-bs.de>
2023-06-02 15:45:50 +0200

    Added control test

--- applications/OptimizationApplication/python_scripts/controls/material/material_properties_control_system_identification.py
+++ applications/OptimizationApplication/python_scripts/controls/material/material_properties_control_system_identification.py
@@ -1,11 +1,12 @@
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.OptimizationApplication.controls.control import Control
-from KratosMultiphysics.OptimizationApplication.controls.material_properties_control import MaterialPropertiesControl
+from KratosMultiphysics.OptimizationApplication.controls.material.material_properties_control import MaterialPropertiesControl
 from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import ContainerExpressionTypes
 from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import SupportedSensitivityFieldVariableTypes
 from KratosMultiphysics.OptimizationApplication.utilities.helper_utilities import IsSameContainerExpression
 from KratosMultiphysics.OptimizationApplication.utilities.model_part_utilities import ModelPartUtilities
+
 
 def Factory(model: Kratos.Model, parameters: Kratos.Parameters, _) -> Control:
     if not parameters.Has("name"):
@@ -14,6 +15,7 @@ def Factory(model: Kratos.Model, parameters: Kratos.Parameters, _) -> Control:
         raise RuntimeError(f"MaterialPropertiesControlSystemIdentification instantiation requires a \"settings\" in parameters [ parameters = {parameters}].")
 
     return MaterialPropertiesControlSystemIdentification(parameters["name"].GetString(), model, parameters["settings"])
+
 
 class MaterialPropertiesControlSystemIdentification(MaterialPropertiesControl):
     """Material properties control
@@ -24,32 +26,48 @@ class MaterialPropertiesControlSystemIdentification(MaterialPropertiesControl):
     TODO: Extend with filtering techniques when they are implemented.
 
     """
+
     def __init__(self, name: str, model: Kratos.Model, parameters: Kratos.Parameters):
-        super().__init__(name,model,parameters)
+        super().__init__(name, model, parameters)
 
         default_settings = Kratos.Parameters("""{
             "model_part_names"     : [""],
-            "control_variable_name": "YOUNG_MODULUS"
+            "control_variable_name": "YOUNG_MODULUS",
+            "mapping_options":{
+                "use_constant_multiplication_factor": true,
+                "constant_multiplication_factor" : 30.0
+            }
         }""")
         parameters.ValidateAndAssignDefaults(default_settings)
+        self.mapping_options = parameters["mapping_options"]
 
     def MapGradient(self, physical_gradient_variable_container_expression_map: dict[SupportedSensitivityFieldVariableTypes, ContainerExpressionTypes]) -> ContainerExpressionTypes:
         keys = physical_gradient_variable_container_expression_map.keys()
         if len(keys) != 1:
             raise RuntimeError(f"Provided more than required gradient fields for control \"{self.GetName()}\". Following are the variables:\n\t" + "\n\t".join([k.Name() for k in keys]))
         if self.controlled_physical_variable not in keys:
-            raise RuntimeError(f"The required gradient for control \"{self.GetName()}\" w.r.t. {self.controlled_physical_variable.Name()} not found. Followings are the variables:\n\t" + "\n\t".join([k.Name() for k in keys]))
+            raise RuntimeError(
+                f"The required gradient for control \"{self.GetName()}\" w.r.t. {self.controlled_physical_variable.Name()} not found. Followings are the variables:\n\t" + "\n\t".join([k.Name() for k in keys]))
 
         physical_gradient = physical_gradient_variable_container_expression_map[self.controlled_physical_variable]
         if not IsSameContainerExpression(physical_gradient, self.GetEmptyField()):
-            raise RuntimeError(f"Gradients for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.model_part.FullName()}, given model part name: {physical_gradient.GetModelPart().FullName()} ]")
+            raise RuntimeError(
+                f"Gradients for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.model_part.FullName()}, given model part name: {physical_gradient.GetModelPart().FullName()} ]")
 
         # TODO: Implement filtering mechanisms here
-        return physical_gradient_variable_container_expression_map[self.controlled_physical_variable].Clone()
+        cloned_container = physical_gradient_variable_container_expression_map[self.controlled_physical_variable].Clone()
+
+        if self.mapping_options["use_constant_multiplication_factor"].GetBool():
+
+            for element in cloned_container.GetContainer():
+                element.Properties[list(keys)[0]] *= self.mapping_options["constant_multiplication_factor"].GetDouble()
+
+        return cloned_container
 
     def Update(self, control_field: ContainerExpressionTypes) -> bool:
         if not IsSameContainerExpression(control_field, self.GetEmptyField()):
-            raise RuntimeError(f"Updates for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.model_part.FullName()}, given model part name: {control_field.GetModelPart().FullName()} ]")
+            raise RuntimeError(
+                f"Updates for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.model_part.FullName()}, given model part name: {control_field.GetModelPart().FullName()} ]")
 
         # TODO: Implement inverse filtering mechanisms here
         # since no filtering is implemented yet, we are checking the unfiltered updates with the filtered updates. This needs to be changed once the
