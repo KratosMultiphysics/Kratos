27ec43a20b1af49df80208b4a3e812a06fcce78e
Philipp <philipp.bucher@tum.de>
2023-06-12 23:03:00 +0200

    WIP expose global vars

---
+++ kratos/python/add_global_variables_to_python.cpp
@@ -0,0 +1,30 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "python/add_global_variables_to_python.h"
+#include "includes/define_python.h"
+#include "includes/global_variables.h"
+
+namespace Kratos::Python {
+
+void AddGlobalVariablesToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+}
+
+} // namespace Kratos::Python
