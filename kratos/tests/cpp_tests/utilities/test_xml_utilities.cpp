7a1ce12bc8fda4a97c414e88e9e7135f2a3bad45
Daniel Diez <ddiez@europe.altair.com>
2023-06-14 09:17:48 +0200

    fix test in windows (#11268)
    Co-authored-by: Daniel Diez <ddiez@altair.com>

--- kratos/tests/cpp_tests/utilities/test_xml_utilities.cpp
+++ kratos/tests/cpp_tests/utilities/test_xml_utilities.cpp
@@ -183,12 +183,12 @@ KRATOS_TEST_CASE_IN_SUITE(XmlOStreamWriterWriteDataElementAsciiDouble, KratosCor
     element.AddAttribute("attribute1", "value1");
 
     std::stringstream ss;
-    XmlOStreamAsciiWriter writer(ss, 0);
+    XmlOStreamAsciiWriter writer(ss, 1);
     writer.WriteElement(element, 1);
 
     KRATOS_CHECK_EQUAL(ss.str(),
             "   <DataArray type=\"Float64\" Name=\"data_1\" NumberOfComponents=\"3\" attribute1=\"value1\" format=\"ascii\">\n"
-            "     0e+00  1e+00  2e+00  3e+00  4e+00  5e+00  0e+00  1e+00  2e+00  3e+00  4e+00  5e+00  6e+00  7e+00  8e+00\n"
+            "     0.0e+00  1.0e+00  2.0e+00  3.0e+00  4.0e+00  5.0e+00  0.0e+00  1.0e+00  2.0e+00  3.0e+00  4.0e+00  5.0e+00  6.0e+00  7.0e+00  8.0e+00\n"
             "   </DataArray>\n");
 }
 
@@ -211,12 +211,12 @@ KRATOS_TEST_CASE_IN_SUITE(XmlOStreamWriterWriteDataElementAsciiMixed, KratosCore
     element.AddAttribute("attribute1", "value1");
 
     std::stringstream ss;
-    XmlOStreamAsciiWriter writer(ss, 0);
+    XmlOStreamAsciiWriter writer(ss, 1);
     writer.WriteElement(element, 1);
 
     KRATOS_CHECK_EQUAL(ss.str(),
             "   <DataArray type=\"Float64\" Name=\"data_1\" NumberOfComponents=\"3\" attribute1=\"value1\" format=\"ascii\">\n"
-            "     0e+00  1e+00  2e+00  3e+00  4e+00  5e+00  0e+00  1e+00  2e+00  3e+00  4e+00  5e+00  6e+00  7e+00  8e+00\n"
+            "     0.0e+00  1.0e+00  2.0e+00  3.0e+00  4.0e+00  5.0e+00  0.0e+00  1.0e+00  2.0e+00  3.0e+00  4.0e+00  5.0e+00  6.0e+00  7.0e+00  8.0e+00\n"
             "   </DataArray>\n");
 }
 
