3b71c9ebb25741c9fca833b1774ae7a08e27d2c0
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-06-15 14:53:42 +0200

    WIP

---
+++ kratos/mpi/spatial_containers/specialized_spatial_search_mpi.cpp
@@ -0,0 +1,17 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+// External includes
+
+// Project includes
---
+++ kratos/mpi/spatial_containers/specialized_spatial_search_mpi.h
@@ -0,0 +1,19 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
