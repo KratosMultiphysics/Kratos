c3018011f37989b991596ed3133ee3314df8d798
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2023-06-15 07:36:45 +0200

    add empty container test

--- kratos/tests/test_container_expression.py
+++ kratos/tests/test_container_expression.py
@@ -580,6 +580,15 @@ class TestContainerExpression(ABC):
         a = self._GetContainerExpression()
         self.assertEqual(self._GetContainer(), a.GetContainer())
 
+    def test_EmptyContainer(self):
+        model_part = self.model.CreateModelPart("empty_model_part")
+        a = type(self._GetContainerExpression())(model_part)
+        self._Read(a, Kratos.VELOCITY)
+
+        a *= 3
+        self._Evaluate(a, Kratos.ACCELERATION)
+        self.assertEqual(a.Evaluate().shape, (0, ))
+
     @abstractmethod
     def _GetContainerExpression(self) -> Union[Kratos.Expression.NodalExpression, Kratos.Expression.ElementExpression, Kratos.Expression.ConditionExpression]:
         pass
