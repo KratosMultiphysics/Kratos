27a5adc9104ee60c7a4ffd9815725cbf2275d587
sunethwarna <suneth.warna@gmail.com>
2023-06-16 14:30:07 +0200

    update model part utilities

--- applications/OptimizationApplication/python_scripts/utilities/model_part_utilities.py
+++ applications/OptimizationApplication/python_scripts/utilities/model_part_utilities.py
@@ -6,12 +6,12 @@ class ModelPartUtilities:
     @staticmethod
     def __GenerateUniqueModelPartName(prefix: str, list_of_names: 'list[str]', add_neighbours: bool) -> str:
         sorted_names = sorted(list_of_names)
-        post_fix = "_IN" if add_neighbours else "_EN"
-        return (f"{prefix}_" + "_".join(sorted_names) + post_fix).replace(".", "#")
+        post_fix = "IN" if add_neighbours else "EN"
+        return (f"{prefix}_{post_fix}_" + "_".join(sorted_names)).replace(".", "-")
 
     @staticmethod
     def UnionModelParts(main_model_part: Kratos.ModelPart, union_model_parts: 'list[Kratos.ModelPart]', add_neighbours: bool) -> 'tuple[bool, Kratos.ModelPart]':
-        unique_name = ModelPartUtilities.__GenerateUniqueModelPartName("Union", [model_part.FullName() for model_part in union_model_parts], add_neighbours)
+        unique_name = ModelPartUtilities.__GenerateUniqueModelPartName("UNION", [model_part.FullName() for model_part in union_model_parts], add_neighbours)
         if main_model_part.HasSubModelPart(unique_name):
             return False, main_model_part.GetSubModelPart(unique_name)
         else:
@@ -21,7 +21,7 @@ class ModelPartUtilities:
 
     @staticmethod
     def IntersectModelParts(main_model_part: Kratos.ModelPart, intersecting_model_parts: 'list[Kratos.ModelPart]', add_neighbours: bool) -> 'tuple[bool, Kratos.ModelPart]':
-        unique_name = ModelPartUtilities.__GenerateUniqueModelPartName("Intersect", [model_part.FullName() for model_part in intersecting_model_parts], add_neighbours)
+        unique_name = ModelPartUtilities.__GenerateUniqueModelPartName("INTERSECT", [model_part.FullName() for model_part in intersecting_model_parts], add_neighbours)
         if main_model_part.HasSubModelPart(unique_name):
             return False, main_model_part.GetSubModelPart(unique_name)
         else:
@@ -31,7 +31,7 @@ class ModelPartUtilities:
 
     @staticmethod
     def SubstractModelParts(main_model_part: Kratos.ModelPart, substracting_model_parts: 'list[Kratos.ModelPart]', add_neighbours: bool) -> 'tuple[bool, Kratos.ModelPart]':
-        unique_name = ModelPartUtilities.__GenerateUniqueModelPartName("Substract", [model_part.FullName() for model_part in substracting_model_parts], add_neighbours)
+        unique_name = ModelPartUtilities.__GenerateUniqueModelPartName("SUBSTRACT", [model_part.FullName() for model_part in substracting_model_parts], add_neighbours)
         if main_model_part.HasSubModelPart(unique_name):
             return False, main_model_part.GetSubModelPart(unique_name)
         else:
