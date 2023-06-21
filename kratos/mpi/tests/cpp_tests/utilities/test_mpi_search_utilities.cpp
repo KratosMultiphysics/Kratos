681067f07d9a2643007ddf5268f054dc51998880
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-06-21 16:26:23 +0200

    Rename method

--- kratos/mpi/tests/cpp_tests/utilities/test_mpi_search_utilities.cpp
+++ kratos/mpi/tests/cpp_tests/utilities/test_mpi_search_utilities.cpp
@@ -84,19 +84,19 @@ KRATOS_DISTRIBUTED_TEST_CASE_IN_SUITE(MPISynchronousPointSynchronizationAllPoint
     double value = static_cast<double>(rank);
     double value2 = 2 * value;
     std::vector<Point> points({Point(value, value, value), Point(value2, value2, value2)});
-    std::vector<double> local_distances = {value, value2};
+    std::vector<double> local_radius = {value, value2};
 
     // Call the function
     std::vector<double> all_points_coordinates;
-    auto distances = MPISearchUtilities::MPISynchronousPointSynchronizationWithDistances(points.begin(), points.end(), all_points_coordinates, local_distances, r_data_comm);
+    auto radius = MPISearchUtilities::MPISynchronousPointSynchronizationWithRadius(points.begin(), points.end(), all_points_coordinates, local_radius, r_data_comm);
 
     // Check the results
     KRATOS_CHECK_EQUAL(static_cast<int>(all_points_coordinates.size()), 2 * 3 * world_size);
     for (int i_rank = 0; i_rank < world_size; ++i_rank) {
         value = static_cast<double>(i_rank);
         value2 = 2 * value;
-        KRATOS_CHECK_DOUBLE_EQUAL(distances[i_rank * 2    ], value );
-        KRATOS_CHECK_DOUBLE_EQUAL(distances[i_rank * 2 + 1], value2);
+        KRATOS_CHECK_DOUBLE_EQUAL(radius[i_rank * 2    ], value );
+        KRATOS_CHECK_DOUBLE_EQUAL(radius[i_rank * 2 + 1], value2);
         for (int j = 0; j < 3; ++j) {
             KRATOS_CHECK_DOUBLE_EQUAL(all_points_coordinates[i_rank * 6 + j    ], value );
             KRATOS_CHECK_DOUBLE_EQUAL(all_points_coordinates[i_rank * 6 + j + 3], value2);
