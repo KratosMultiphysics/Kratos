04228705708b0f4a18e8c61c447f88ee0aa756a6
sunethwarna <suneth.warna@gmail.com>
2023-07-07 07:50:37 +0200

    update generic reduction utils

--- applications/StatisticsApplication/custom_utilities/generic_reduction_utilities.h
+++ applications/StatisticsApplication/custom_utilities/generic_reduction_utilities.h
@@ -13,11 +13,10 @@
 #pragma once
 
 // System includes
-#include <tuple>
+#include <cmath>
 
 // Project includes
 #include "includes/data_communicator.h"
-#include "utilities/atomic_utilities.h"
 #include "utilities/parallel_utilities.h"
 
 // Application incldues
@@ -26,376 +25,87 @@
 namespace Kratos
 {
 
-template<class TDataType>
-class GenericSumReduction
+///@name Classes
+///@{
+
+class GenericReductionUtilities
 {
-public:
-    using return_type = TDataType;
+private:
+    ///@name Private class definitions
+    ///@{
 
-    static constexpr double mInitializationValue = 0.0;
-
-    return_type mValue;
-
-    /// Constructor
-    GenericSumReduction()
+    template<class TOperationType>
+    class GenericReducer
     {
-        DataTypeTraits<TDataType>::Initialize(mValue, mInitializationValue);
-    }
+    public:
+        using return_type = TOperationType;
 
-    /// access to reduced value
-    return_type GetValue() const
-    {
-        return mValue;
-    }
+        return_type mValue;
 
-    /// NON-THREADSAFE (fast) value of reduction, to be used within a single thread
-    void LocalReduce(const return_type& value){
-        if (DataTypeTraits<TDataType>::Resize(mValue, value)) {
-            DataTypeTraits<TDataType>::Initialize(mValue, mInitializationValue);
+        /// access to reduced value
+        return_type GetValue() const
+        {
+            return mValue;
         }
-        mValue += value;
-    }
 
-    /// THREADSAFE (needs some sort of lock guard) reduction, to be used to sync threads
-    void ThreadSafeReduce(const GenericSumReduction<TDataType>& rOther)
-    {
+        /// NON-THREADSAFE (fast) value of reduction, to be used within a single thread
+        void LocalReduce(const return_type& rValue){
+            mValue.Execute(rValue);
+        }
+
+        /// THREADSAFE (needs some sort of lock guard) reduction, to be used to sync threads
+        void ThreadSafeReduce(const GenericReducer<TOperationType>& rOther)
         {
             KRATOS_CRITICAL_SECTION
-            if (DataTypeTraits<TDataType>::Resize(mValue, rOther.mValue)) {
-                DataTypeTraits<TDataType>::Initialize(mValue, mInitializationValue);
-            }
+            mValue.Execute(rOther.mValue);
         }
+    };
 
-        if constexpr(std::is_same_v<return_type, Vector>) {
-            AtomicAddVector(mValue, rOther.mValue);
-        } else if constexpr(std::is_same_v<return_type, Matrix>) {
-            AtomicAddMatrix(mValue, rOther.mValue);
-        } else {
-            AtomicAdd(mValue, rOther.mValue);
-        }
+    ///@}
 
-    }
-
-    static void Synchronize(
-        return_type& rValue,
-        const DataCommunicator& rDataCommunicator)
-    {
-        if (DataTypeTraits<TDataType>::SynchronizeSize(rValue, rDataCommunicator)) {
-            DataTypeTraits<TDataType>::Initialize(rValue, mInitializationValue);
-        }
-
-        typename DataTypeTraits<TDataType>::VectorType local_values, global_values;
-        DataTypeTraits<TDataType>::FillToVector(local_values, rValue);
-        rDataCommunicator.SumAll(local_values, global_values);
-        DataTypeTraits<TDataType>::FillFromVector(rValue, global_values);
-    }
-};
-
-template<class TDataType>
-class GenericMinReduction
-{
 public:
-    using data_type = TDataType;
+    ///@name Static operations
+    ///@{
 
-    using indices_type = std::vector<int>;
-
-    using return_type = std::tuple<TDataType, indices_type>;
-
-    static constexpr double mInitializationValue = std::numeric_limits<double>::max();
-
-    return_type mValue;
-
-    /// Constructor
-    GenericMinReduction()
+    template<class TDataContainerType, class TNormType, template <class T1> class TOperationType, bool IdRequired, int TPower = 1>
+    static TOperationType<typename TNormType::ResultantValueType<typename TDataContainerType::DataType>> GenericReduction(
+        const DataCommunicator& rDataCommunicator,
+        const TDataContainerType& rDataContainer,
+        const TNormType& rNorm)
     {
-        auto& min_values = std::get<0>(mValue);
-        auto& min_indices = std::get<1>(mValue);
+        KRATOS_TRY
 
-        DataTypeTraits<TDataType>::Initialize(min_values, mInitializationValue);
-        DataTypeTraits<indices_type>::Resize(min_indices, indices_type(DataTypeTraits<TDataType>::Size(min_values)));
-        DataTypeTraits<indices_type>::Initialize(min_indices, std::numeric_limits<unsigned int>::max());
+        using entity_data_type = typename TDataContainerType::DataType;
+
+        using norm_resultant_type = typename TNormType::ResultantValueType<typename TDataContainerType::DataType>;
+
+        using operation_type = TOperationType<norm_resultant_type>;
+
+        using reducer_type = GenericReducer<operation_type>;
+
+        auto local_value = IndexPartition<IndexType>(rDataContainer.Size()).for_each<reducer_type>(entity_data_type{}, [&rDataContainer, &rNorm](const IndexType Index, entity_data_type& rTLS) {
+            rDataContainer.GetValue(rTLS, Index);
+            auto norm_value = rNorm.Evaluate(rTLS);
+            for (IndexType i = 0; i < DataTypeTraits<norm_resultant_type>::Size(norm_value); ++i) {
+                DataTypeTraits<norm_resultant_type>::GetComponent(norm_value, i) = std::pow(DataTypeTraits<norm_resultant_type>::GetComponent(norm_value, i), TPower);
+            }
+
+            if constexpr(!IdRequired) {
+                return operation_type(norm_value);
+            } else {
+                return operation_type(norm_value, rDataContainer.GetId(Index));
+            }
+
+        });
+
+        local_value.Synchronize(rDataCommunicator);
+
+        return local_value;
+
+        KRATOS_CATCH("");
     }
 
-    /// access to reduced value
-    return_type GetValue() const
-    {
-        return mValue;
-    }
-
-    /// NON-THREADSAFE (fast) value of reduction, to be used within a single thread
-    void LocalReduce(const return_type& value){
-        auto& min_values = std::get<0>(mValue);
-        auto& min_indices = std::get<1>(mValue);
-
-        const auto& values = std::get<0>(value);
-        const auto& indices = std::get<1>(value);
-
-        if (DataTypeTraits<TDataType>::Resize(min_values, values)) {
-            // if resized, then initialize values
-            DataTypeTraits<TDataType>::Initialize(min_values, mInitializationValue);
-            // now initialize the indices vector size
-            DataTypeTraits<indices_type>::Resize(min_indices, indices_type(DataTypeTraits<TDataType>::Size(min_values)));
-            DataTypeTraits<indices_type>::Initialize(min_indices, std::numeric_limits<int>::max());
-        }
-
-        if constexpr(std::is_arithmetic_v<data_type>) {
-            if (min_values > values) {
-                min_values = values;
-                min_indices = indices;
-            }
-        } else if constexpr(std::is_same_v<data_type, Matrix>) {
-            for (IndexType i = 0; i < values.size1() * values.size2(); ++i) {
-                if (min_values.data()[i] > values.data()[i]) {
-                    min_values.data()[i] = value.data()[i];
-                    min_indices[i] = indices[i];
-                }
-            }
-        } else {
-            for (IndexType i = 0; i < value.size(); ++i) {
-                if (min_values[i] > values[i]) {
-                    min_values[i] = value[i];
-                    min_indices[i] = indices[i];
-                }
-            }
-        }
-    }
-
-    /// THREADSAFE (needs some sort of lock guard) reduction, to be used to sync threads
-    void ThreadSafeReduce(const GenericMinReduction<TDataType>& rOther)
-    {
-        KRATOS_CRITICAL_SECTION
-
-        auto& min_values = std::get<0>(mValue);
-        auto& min_indices = std::get<1>(mValue);
-
-        const auto& values = std::get<0>(rOther.mValue);
-        const auto& indices = std::get<1>(rOther.mValue);
-
-        if (DataTypeTraits<TDataType>::Resize(min_values, values)) {
-            // if resized, then initialize values
-            DataTypeTraits<TDataType>::Initialize(min_values, mInitializationValue);
-            // now initialize the indices vector size
-            DataTypeTraits<indices_type>::Resize(min_indices, indices_type(DataTypeTraits<TDataType>::Size(min_values)));
-            DataTypeTraits<indices_type>::Initialize(min_indices, std::numeric_limits<int>::max());
-        }
-
-        if constexpr(std::is_arithmetic_v<data_type>) {
-            if (min_values > values) {
-                min_values = values;
-                min_indices = indices;
-            }
-        } else if constexpr(std::is_same_v<data_type, Matrix>) {
-            for (IndexType i = 0; i < values.size1() * values.size2(); ++i) {
-                if (min_values.data()[i] > values.data()[i]) {
-                    min_values.data()[i] = values.data()[i];
-                    min_indices[i] = indices[i];
-                }
-            }
-        } else {
-            for (IndexType i = 0; i < values.size(); ++i) {
-                if (min_values[i] > values[i]) {
-                    min_values[i] = values[i];
-                    min_indices[i] = indices[i];
-                }
-            }
-        }
-    }
-
-    static void Synchronize(
-        return_type& rValue,
-        const DataCommunicator& rDataCommunicator)
-    {
-        auto& min_values = std::get<0>(rValue);
-        auto& min_indices = std::get<1>(rValue);
-
-        if (DataTypeTraits<TDataType>::SynchronizeSize(min_values, rDataCommunicator)) {
-            DataTypeTraits<TDataType>::Initialize(min_values, mInitializationValue);
-            DataTypeTraits<indices_type>::Resize(min_indices, indices_type(DataTypeTraits<TDataType>::Size(min_values)));
-            DataTypeTraits<indices_type>::Initialize(min_indices, std::numeric_limits<int>::max());
-        }
-
-        const int world_size = rDataCommunicator.Size();
-
-        // first get the values from all ranks
-        typename DataTypeTraits<TDataType>::VectorType local_values;
-        DataTypeTraits<TDataType>::FillToVector(local_values, min_values);
-        std::vector<typename DataTypeTraits<TDataType>::VectorType> global_values(world_size);
-        global_values = rDataCommunicator.AllGatherv(local_values);
-
-        // now get indices corresponding to above values from all ranks
-        std::vector<indices_type> global_indices(world_size);
-        global_indices = rDataCommunicator.AllGatherv(min_indices);
-
-        // now get the min
-        TDataType current_values;
-        DataTypeTraits<TDataType>::Resize(current_values, min_values);
-        for (int rank = 0; rank < world_size; ++rank) {
-            const auto& rank_values = global_values[rank];
-            const auto& rank_indices = global_indices[rank];
-            DataTypeTraits<TDataType>::FillFromVector(current_values, rank_values);
-
-            for (IndexType i = 0; i < DataTypeTraits<TDataType>::Size(rank_values); ++i) {
-                if (DataTypeTraits<TDataType>::GetComponent(current_values, i) < DataTypeTraits<TDataType>::GetComponent(min_values, i)) {
-                    DataTypeTraits<TDataType>::GetComponent(min_values, i) = DataTypeTraits<TDataType>::GetComponent(current_values, i);
-                    DataTypeTraits<TDataType>::GetComponent(min_indices, i) = rank_indices[i];
-                }
-            }
-        }
-    }
-};
-
-template<class TDataType>
-class GenericMaxReduction
-{
-public:
-    using data_type = TDataType;
-
-    using indices_type = std::vector<int>;
-
-    using return_type = std::tuple<TDataType, indices_type>;
-
-    static constexpr double mInitializationValue = std::numeric_limits<double>::lowest();
-
-    // initialize values and indices containers to min
-    return_type mValue;
-
-    /// Constructor
-    GenericMaxReduction()
-    {
-        auto& max_values = std::get<0>(mValue);
-        auto& max_indices = std::get<1>(mValue);
-
-        DataTypeTraits<TDataType>::Initialize(max_values, mInitializationValue);
-        DataTypeTraits<indices_type>::Resize(max_indices, indices_type(DataTypeTraits<TDataType>::Size(max_values)));
-        DataTypeTraits<indices_type>::Initialize(max_indices, std::numeric_limits<unsigned int>::max());
-    }
-
-    /// access to reduced value
-    return_type GetValue() const
-    {
-        return mValue;
-    }
-
-    /// NON-THREADSAFE (fast) value of reduction, to be used within a single thread
-    void LocalReduce(const return_type& value){
-        auto& max_values = std::get<0>(mValue);
-        auto& max_indices = std::get<1>(mValue);
-
-        const auto& values = std::get<0>(value);
-        const auto& indices = std::get<1>(value);
-
-        if (DataTypeTraits<TDataType>::Resize(max_values, values)) {
-            // if resized, then initialize values
-            DataTypeTraits<TDataType>::Initialize(max_values, mInitializationValue);
-            // now initialize the indices vector size
-            DataTypeTraits<indices_type>::Resize(max_indices, indices_type(DataTypeTraits<TDataType>::Size(max_values)));
-            DataTypeTraits<indices_type>::Initialize(max_indices, std::numeric_limits<unsigned int>::max());
-        }
-
-        if constexpr(std::is_arithmetic_v<data_type>) {
-            if (max_values < values) {
-                max_values = values;
-                max_indices = indices;
-            }
-        } else if constexpr(std::is_same_v<data_type, Matrix>) {
-            for (IndexType i = 0; i < values.size1() * values.size2(); ++i) {
-                if (max_values.data()[i] < values.data()[i]) {
-                    max_values.data()[i] = values.data()[i];
-                    max_indices[i] = indices[i];
-                }
-            }
-        } else {
-            for (IndexType i = 0; i < values.size(); ++i) {
-                if (max_values[i] < values[i]) {
-                    max_values[i] = values[i];
-                    max_indices[i] = indices[i];
-                }
-            }
-        }
-    }
-
-    /// THREADSAFE (needs some sort of lock guard) reduction, to be used to sync threads
-    void ThreadSafeReduce(const GenericMinReduction<TDataType>& rOther)
-    {
-        KRATOS_CRITICAL_SECTION
-
-        auto& max_values = std::get<0>(mValue);
-        auto& max_indices = std::get<1>(mValue);
-
-        const auto& values = std::get<0>(rOther.mValue);
-        const auto& indices = std::get<1>(rOther.mValue);
-
-        if (DataTypeTraits<TDataType>::Resize(max_values, values)) {
-            // if resized, then initialize values
-            DataTypeTraits<TDataType>::Initialize(max_values, mInitializationValue);
-            // now initialize the indices vector size
-            DataTypeTraits<indices_type>::Resize(max_indices, indices_type(DataTypeTraits<TDataType>::Size(max_values)));
-            DataTypeTraits<indices_type>::Initialize(max_indices, std::numeric_limits<unsigned int>::max());
-        }
-
-        if constexpr(std::is_arithmetic_v<data_type>) {
-            if (max_values < values) {
-                max_values = values;
-                max_indices = indices;
-            }
-        } else if constexpr(std::is_same_v<data_type, Matrix>) {
-            for (IndexType i = 0; i < values.size1() * values.size2(); ++i) {
-                if (max_values.data()[i] < values.data()[i]) {
-                    max_values.data()[i] = values.data()[i];
-                    max_indices[i] = indices[i];
-                }
-            }
-        } else {
-            for (IndexType i = 0; i < values.size(); ++i) {
-                if (max_values[i] < values[i]) {
-                    max_values[i] = values[i];
-                    max_indices[i] = indices[i];
-                }
-            }
-        }
-    }
-
-    static void Synchronize(
-        return_type& rValue,
-        const DataCommunicator& rDataCommunicator)
-    {
-        auto& max_values = std::get<0>(rValue);
-        auto& max_indices = std::get<1>(rValue);
-
-        if (DataTypeTraits<TDataType>::SynchronizeSize(max_values, rDataCommunicator)) {
-            DataTypeTraits<TDataType>::Initialize(max_values, mInitializationValue);
-            DataTypeTraits<indices_type>::Resize(max_indices, indices_type(DataTypeTraits<TDataType>::Size(max_values)));
-            DataTypeTraits<indices_type>::Initialize(max_indices, std::numeric_limits<unsigned int>::max());
-        }
-
-        const int world_size = rDataCommunicator.Size();
-
-        // first get the values from all ranks
-        typename DataTypeTraits<TDataType>::VectorType local_values;
-        DataTypeTraits<TDataType>::FillToVector(local_values, max_values);
-        std::vector<typename DataTypeTraits<TDataType>::VectorType> global_values(world_size);
-        global_values = rDataCommunicator.AllGatherv(local_values);
-
-        // now get indices corresponding to above values from all ranks
-        std::vector<indices_type> global_indices(world_size);
-        global_indices = rDataCommunicator.AllGatherv(max_indices);
-
-        // now get the min
-        TDataType current_values;
-        DataTypeTraits<TDataType>::Resize(current_values, max_values);
-        for (int rank = 0; rank < world_size; ++rank) {
-            const auto& rank_values = global_values[rank];
-            const auto& rank_indices = global_indices[rank];
-            DataTypeTraits<TDataType>::FillFromVector(current_values, rank_values);
-
-            for (IndexType i = 0; i < DataTypeTraits<TDataType>::Size(rank_values); ++i) {
-                if (DataTypeTraits<TDataType>::GetComponent(current_values, i) > DataTypeTraits<TDataType>::GetComponent(max_values, i)) {
-                    DataTypeTraits<TDataType>::GetComponent(max_values, i) = DataTypeTraits<TDataType>::GetComponent(current_values, i);
-                    DataTypeTraits<TDataType>::GetComponent(max_indices, i) = rank_indices[i];
-                }
-            }
-        }
-    }
+    ///@}
 };
 
 } // namespace Kratos
