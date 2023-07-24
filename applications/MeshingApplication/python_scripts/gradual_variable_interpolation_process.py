90de0c2ed8e1098d0e40a22fb67cecf1421fbfa4
SADPR <aresdepargas@gmail.com>
2023-07-24 16:11:17 +0200

    Working process, ready to test.

--- applications/MeshingApplication/python_scripts/gradual_variable_interpolation_process.py
+++ applications/MeshingApplication/python_scripts/gradual_variable_interpolation_process.py
@@ -1,28 +1,50 @@
 import KratosMultiphysics
-import numpy as np
 import KratosMultiphysics.MeshingApplication as KratosMA
 
 def Factory(settings, model):
     if not isinstance(settings, KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
-    return InterpolateVelocityProcess(model, settings["Parameters"])
+    return GradualVariableInterpolationProcess(model, settings["Parameters"])
 
-## All the processes python should be derived from "Process"
-class InterpolateVelocityProcess(KratosMultiphysics.Process):
+class GradualVariableInterpolationProcess(KratosMultiphysics.Process):
+    """This class defines a process for gradually interpolating nodal values from one 
+    model part to another, called the origin and destination model parts respectively. 
+    The rate of interpolation can be controlled through an increment parameter 'alpha_increment' 
+    or by specifying the total number of steps for the full interpolation 'steps_for_full_interpolation'. 
+    The list of variables to be interpolated is also user-definable."""
     def __init__(self, model, settings):
         KratosMultiphysics.Process.__init__(self)
 
         default_settings = KratosMultiphysics.Parameters("""{
             "origin_model_part_file_name" : "NameOfMDPAfile",
             "destination_model_part_name" : "ModelPartName",
-            "interpolation_variables_list": []
+            "interpolation_variables_list": [],
+            "alpha_increment": 0.0,
+            "steps_for_full_interpolation": 0.0
         }""")
 
         settings.ValidateAndAssignDefaults(default_settings)
 
         self.model = model
         self.settings = settings
-        self.alpha = 1e-3
+        self.alpha_increment = self.settings["alpha_increment"].GetDouble()
+        self.steps_for_full_interpolation = self.settings["steps_for_full_interpolation"].GetInt()
+
+        if not self.alpha_increment and not self.steps_for_full_interpolation:
+            KratosMultiphysics.Logger.PrintWarning('GradualVariableInterpolationProcess', 'Neither alpha_increment nor steps_for_full_interpolation was specified; defaulting to alpha_increment = 1.0')
+            self.alpha_increment = 1.0
+        elif self.alpha_increment and self.steps_for_full_interpolation:
+            KratosMultiphysics.Logger.PrintWarning('GradualVariableInterpolationProcess', 'Both alpha_increment and steps_for_full_interpolation were specified; using alpha_increment and ignoring steps_for_full_interpolation')
+        elif self.alpha_increment:
+            if self.alpha_increment < 0 or self.alpha_increment > 1:
+                raise Exception("'alpha_increment' must be in the interval (0, 1].")
+        elif self.steps_for_full_interpolation:
+            if self.steps_for_full_interpolation <= 0:
+                raise Exception("'steps_for_full_interpolation' must be a positive integer.")
+            else:
+                self.alpha_increment = 1.0 / self.steps_for_full_interpolation
+
+        self.alpha = 0.0
         self.expected_alpha = 1.0
         self.interpolation_variables_list = [KratosMultiphysics.KratosGlobals.GetVariable(i) for i in self.settings["interpolation_variables_list"].GetStringArray()]
 
@@ -46,7 +68,7 @@ class InterpolateVelocityProcess(KratosMultiphysics.Process):
         #Set velocity field to origin model part
         for node in self.origin_model_part.Nodes:
             for variable in self.interpolation_variables_list:
-                node.SetValue(variable, self.alpha * node.GetSolutionStepValue(variable))
+                node.SetValue(variable, self.alpha_increment * node.GetSolutionStepValue(variable))
 
         #Interpolate velocity to destination model part 
         interpolation = KratosMA.NodalValuesInterpolationProcess2D(self.origin_model_part, self.destination_model_part)
@@ -54,10 +76,13 @@ class InterpolateVelocityProcess(KratosMultiphysics.Process):
         for node in self.destination_model_part.Nodes:
             for variable in self.interpolation_variables_list:
                 node.SetSolutionStepValue(variable, node.GetValue(variable))
+                node.Fix(variable)
 
     def ExecuteInitializeSolutionStep(self):
         self.old_alpha = self.alpha
-        self.alpha += 1e-3
+        if self.old_alpha == 0.0:
+            self.old_alpha = self.alpha_increment
+        self.alpha += self.alpha_increment
         if self.alpha < self.expected_alpha:
             for node in self.destination_model_part.Nodes:
                 for variable in self.interpolation_variables_list:
