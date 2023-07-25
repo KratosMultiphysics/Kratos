0c6e1eee7c9620d460fbc9747008e75026710ac7
Carlos Roig <roigcarlo@gmail.com>
2023-07-25 11:13:45 +0200

    Removing references to runkratos

--- .github/workflows/configure.cmd
+++ .github/workflows/configure.cmd
@@ -44,7 +44,6 @@ cmake                                                 ^
   -H"%KRATOS_SOURCE%"                                 ^
   -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"              ^
   -DBOOST_ROOT="%TEMP%\boost"                         ^
-  -DINSTALL_RUNKRATOS=OFF                             ^
   -DCMAKE_CXX_FLAGS="/Od /we4661 /we4804 /WX /wd4996" ^
   -DFORCE_LOCAL_ZLIB_COMPILATION=ON                   ^
   -DCMAKE_UNITY_BUILD=ON                                    || goto :error
