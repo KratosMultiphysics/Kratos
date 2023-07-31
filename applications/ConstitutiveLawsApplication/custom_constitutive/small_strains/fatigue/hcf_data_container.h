f05a0005c459586d94cea2a61e465c86d5fad2c8
Alireza Taherzadeh Fard <a.t.fard@gmail.com>
2023-07-31 11:04:58 +0200

    Methods and variables added to the hcf container

--- applications/ConstitutiveLawsApplication/custom_constitutive/small_strains/fatigue/hcf_data_container.h
+++ applications/ConstitutiveLawsApplication/custom_constitutive/small_strains/fatigue/hcf_data_container.h
@@ -20,6 +20,7 @@
 
 // Project includes
 #include "custom_constitutive/auxiliary_files/cl_integrators/high_cycle_fatigue_law_integrator.h"
+#include "custom_utilities/advanced_constitutive_law_utilities.h"
 
 namespace Kratos
 {
@@ -39,6 +40,106 @@ namespace Kratos
 class KRATOS_API(KRATOS_CORE) HCFDataContainer
 {
 
+public:
+	HCFDataContainer()
+    {};
+
+    // Defining fatigue methods
+
+    void CalculateSminAndSmax(const double CurrentStress,
+                            double& rMaximumStress,
+                            double& rMinimumStress,
+                            const Vector& PreviousStresses,
+                            bool& rMaxIndicator,
+                            bool& rMinIndicator)
+    {
+        HighCycleFatigueLawIntegrator<6>::CalculateMaximumAndMinimumStresses(CurrentStress,
+                                                                            rMaximumStress,
+                                                                            rMinimumStress,
+                                                                            PreviousStresses,
+                                                                            rMaxIndicator,
+                                                                            rMinIndicator);
+    }
+
+    double CalculateTensionOrCompressionIdentifier(const Vector& rStressVector)
+    {
+        HighCycleFatigueLawIntegrator<6>::CalculateTensionCompressionFactor(rStressVector);
+    }
+
+    double CalculateReversionFactor(const double MaxStress, const double MinStress)
+    {
+        HighCycleFatigueLawIntegrator<6>::CalculateReversionFactor(MaxStress, MinStress);
+    }
+
+    void CalculateFatigueParameters(const double MaxStress,
+                                    double ReversionFactor,
+                                    const Properties& rMaterialParameters,
+                                    double& rB0,
+                                    double& rSth,
+                                    double& rAlphat,
+                                    double& rN_f)
+    {
+        HighCycleFatigueLawIntegrator<6>::CalculateFatigueParameters(MaxStress,
+                                                                    ReversionFactor,
+                                                                    rMaterialParameters,
+                                                                    rB0,
+                                                                    rSth,
+                                                                    rAlphat,
+                                                                    rN_f);
+    }
+
+    void CalculateFatigueReductionFactorAndWohlerStress(const Properties& rMaterialParameters,
+                                                        const double MaxStress,
+                                                        unsigned int LocalNumberOfCycles,
+                                                        unsigned int GlobalNumberOfCycles,
+                                                        const double B0,
+                                                        const double Sth,
+                                                        const double Alphat,
+                                                        double& rFatigueReductionFactor,
+                                                        double& rWohlerStress)
+    {
+        HighCycleFatigueLawIntegrator<6>::CalculateFatigueReductionFactorAndWohlerStress(rMaterialParameters,
+                                                                                        MaxStress,
+                                                                                        LocalNumberOfCycles,
+                                                                                        GlobalNumberOfCycles,
+                                                                                        B0,
+                                                                                        Sth,
+                                                                                        Alphat,
+                                                                                        rFatigueReductionFactor,
+                                                                                        rWohlerStress);
+    }
+
+    // Defining fatigue variables
+
+    ///@name Member Variables
+    ///@{
+    double mMaxStress;
+    double mMinStress;
+    bool mMaxDetected;
+    bool mMinDetected;
+    double mFatigueReductionFactor;
+    double mReversionFactorRelativeError;
+    double mMaxStressRelativeError;
+    unsigned int mNumberOfCyclesGlobal;
+    unsigned int mNumberOfCyclesLocal;
+    double mFatigueReductionParameter;
+    double mPreviousMaxStress;
+    double mPreviousMinStress;
+    double mWohlerStress;
+    double mThresholdStress;
+    double mCyclesToFailure;
+    ///@}
+
+private:
+
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
 
 }; // class
 ///@}
