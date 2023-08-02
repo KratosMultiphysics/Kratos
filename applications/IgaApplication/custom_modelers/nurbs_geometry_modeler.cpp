33058bfacf284683a1c7316681eb140f0f6743a1
ge69vud <manuel.messmer@tum.de>
2023-08-02 10:17:28 +0200

    Update 2D modeler.

--- applications/IgaApplication/custom_modelers/nurbs_geometry_modeler.cpp
+++ applications/IgaApplication/custom_modelers/nurbs_geometry_modeler.cpp
@@ -84,7 +84,7 @@ namespace Kratos
             SizeType num_knot_span_u =  mParameters["number_of_knot_spans"].GetArrayItem(0).GetInt();
             SizeType num_knot_span_v =  mParameters["number_of_knot_spans"].GetArrayItem(1).GetInt();
 
-            CreateAndAddRegularGrid2D(model_part, point_a_xyz, point_b_xyz, p_u, p_v, num_knot_span_u, num_knot_span_v);
+            CreateAndAddRegularGrid2D(model_part, point_a_xyz, point_b_xyz, point_a_uvw, point_b_uvw, p_u, p_v, num_knot_span_u, num_knot_span_v);
         }
         else if( local_space_dimension == 3) {
             SizeType p_u =  mParameters["polynomial_order"].GetArrayItem(0).GetInt();
@@ -105,18 +105,18 @@ namespace Kratos
     ///@}
     ///@name Private Operations
     ///@{
-    void NurbsGeometryModeler::CreateAndAddRegularGrid2D( ModelPart& r_model_part, const Point& A, const Point& B,
-        SizeType OrderU, SizeType OrderV,SizeType NumKnotSpansU, SizeType NumKnotSpansV)
+    void NurbsGeometryModeler::CreateAndAddRegularGrid2D( ModelPart& r_model_part, const Point& A_xyz, const Point& B_xyz,
+        const Point& A_uvw, const Point& B_uvw, SizeType OrderU, SizeType OrderV,SizeType NumKnotSpansU, SizeType NumKnotSpansV)
     {
-        KRATOS_ERROR_IF( B.X() <= A.X() || B.Y() <= A.Y() ) << "NurbsGeometryModeler: "
-            << "The two Points A and B must meet the following requirement: (B-A) > (0,0,0). However, (B-A)=" << B-A << std::endl;
+        KRATOS_ERROR_IF( B_xyz.X() <= A_xyz.X() || B_xyz.Y() <= A_xyz.Y() ) << "NurbsGeometryModeler: "
+            << "The two Points A and B must meet the following requirement: (B-A) > (0,0,0). However, (B-A)=" << B_xyz-A_xyz << std::endl;
 
         PointerVector<NodeType> points;
-        double delta_x = std::abs(B.X()-A.X())/OrderU;
-        double delta_y = std::abs(B.Y()-A.Y())/OrderV;
+        double delta_x = std::abs(B_xyz.X()-A_xyz.X())/OrderU;
+        double delta_y = std::abs(B_xyz.Y()-A_xyz.Y())/OrderV;
 
-        double x = A.X();
-        double y = A.Y();
+        double x = A_xyz.X();
+        double y = A_xyz.Y();
 
         for( IndexType j=0; j < OrderV+1; ++j){
             for( IndexType i = 0; i < OrderU+1; ++i){
@@ -124,7 +124,7 @@ namespace Kratos
                 points.push_back(Kratos::make_intrusive<NodeType>(0, x, y, 0.0 ));
                 x += delta_x;
             }
-            x = A.X();
+            x = A_xyz.X();
             y += delta_y;
         }
 
@@ -133,18 +133,18 @@ namespace Kratos
         Vector knot_vector_u(number_of_knots_u);
         for( IndexType i=0; i < number_of_knots_u; ++i){
             if( i < OrderU ){
-                knot_vector_u[i] = 0.0;
+                knot_vector_u[i] = A_uvw[0];
             } else {
-                knot_vector_u[i] = 1.0;
+                knot_vector_u[i] = B_uvw[0];
             }
         }
         SizeType number_of_knots_v = 2*OrderV;
         Vector knot_vector_v(number_of_knots_v);
         for( IndexType i=0; i < number_of_knots_v; ++i){
             if( i < OrderV ){
-                knot_vector_v[i] = 0.0;
+                knot_vector_v[i] = A_uvw[1];
             } else {
-                knot_vector_v[i] = 1.0;
+                knot_vector_v[i] = B_uvw[1];
             }
         }
 
@@ -153,16 +153,16 @@ namespace Kratos
             points, OrderU, OrderV, knot_vector_u, knot_vector_v);
 
         // Set up knots for knot refinement according to the given number of elements in each direction.
-        double delta_knot_u = 1.0 / NumKnotSpansU;
-        double knot_u = 0.0;
+        double delta_knot_u = (B_uvw[0]-A_uvw[0]) / NumKnotSpansU;
+        double knot_u = A_uvw[0];
         std::vector<double> insert_knots_u(NumKnotSpansU-1);
         for( IndexType i = 0; i < NumKnotSpansU-1; i++){
             knot_u += delta_knot_u;
             insert_knots_u[i] = knot_u;
         }
 
-        double delta_knot_v = 1.0 / NumKnotSpansV;
-        double knot_v = 0.0;
+        double delta_knot_v = (B_uvw[1]-A_uvw[1]) / NumKnotSpansV;
+        double knot_v = A_uvw[1];
         std::vector<double> insert_knots_v(NumKnotSpansV-1);
         for( IndexType i = 0; i < NumKnotSpansV-1; i++){
             knot_v += delta_knot_v;
