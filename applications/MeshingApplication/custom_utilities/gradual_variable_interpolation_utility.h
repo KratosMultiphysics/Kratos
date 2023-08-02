710067e41101dd681fecbf8418efd2107f00e773
SADPR <aresdepargas@gmail.com>
2023-08-02 11:38:03 +0200

    Splitting into header and source file.

--- applications/MeshingApplication/custom_utilities/gradual_variable_interpolation_utility.h
+++ applications/MeshingApplication/custom_utilities/gradual_variable_interpolation_utility.h
@@ -12,13 +12,6 @@
 //
 //  Main authors:    Sebastian Ares de Parga Regalado
 
-// System includes
-#include <string>
-#include <iostream>
-#include <algorithm>
-
-// External includes
-
 // Project includes
 #include "processes/process.h"
 #include "includes/model_part.h"
@@ -27,106 +20,46 @@
 #include "includes/parallel_environment.h"
 #include "utilities/parallel_utilities.h"
 
-
 namespace Kratos {
+
 class GradualVariableInterpolationUtility {
 public:
     KRATOS_CLASS_POINTER_DEFINITION(GradualVariableInterpolationUtility);
 
+    /**
+     * @brief Initializes interpolation and applies constraints to the given variables
+     * @param rOriginModelPart The original model part
+     * @param rDestinationModelPart The destination model part
+     * @param rInterpolationVariablesList List of variables to be interpolated
+     * @param AlphaRampUpIncrement Alpha increment for the ramp-up
+     * @param DomainSize Size of the domain (2D or 3D)
+     * @param ConstrainVariables Flag to apply constraints to the variables
+     */
     static void InitializeInterpolationAndConstraints(
         ModelPart& rOriginModelPart,
         ModelPart& rDestinationModelPart,
         std::vector<std::string>& rInterpolationVariablesList,
         double AlphaRampUpIncrement,
         int DomainSize,
-        bool ConstrainVariables) 
-        {
-            // Set variables field to origin model part
-            for(auto& r_variable_name : rInterpolationVariablesList){
-                auto& r_variable = KratosComponents<Variable<double>>::Get(r_variable_name);
-                for(auto& rNode: rOriginModelPart.Nodes()) {
-                    const double value = rNode.FastGetSolutionStepValue(r_variable);
-                    rNode.SetValue(r_variable, AlphaRampUpIncrement * value);
-                }
-            }
+        bool ConstrainVariables);
 
-            // Interpolate variables to destination model part
-            if (DomainSize == 2) {
-                NodalValuesInterpolationProcess<2> interpolation(rOriginModelPart, rDestinationModelPart);
-                interpolation.Execute();
-            } else if (DomainSize == 3) {
-                NodalValuesInterpolationProcess<3> interpolation(rOriginModelPart, rDestinationModelPart);
-                interpolation.Execute();
-            } else {
-                KRATOS_ERROR << "Invalid DomainSize: " << DomainSize << ". DomainSize should be 2 or 3." << std::endl;
-            }
-
-            for(auto& r_variable_name : rInterpolationVariablesList){
-                auto& r_variable = KratosComponents<Variable<double>>::Get(r_variable_name);
-                for(auto& rNode: rDestinationModelPart.Nodes()) {
-                    rNode.AddDof(r_variable);
-                }
-            }
-
-            // Using the parallel utilities
-            if(ConstrainVariables)
-            {
-                // Apply constraints to variables
-                for(auto& r_variable_name : rInterpolationVariablesList){
-                    auto& r_variable = KratosComponents<Variable<double>>::Get(r_variable_name);
-                    block_for_each(rDestinationModelPart.Nodes(), [&](Node& rNode){
-                        const double value = rNode.GetValue(r_variable);
-                        rNode.FastGetSolutionStepValue(r_variable) = value;
-                        rNode.Fix(r_variable);
-                    });
-                }
-            }
-            else
-            {
-                // Simply set the variables
-                for(auto& r_variable_name : rInterpolationVariablesList){
-                    auto& r_variable = KratosComponents<Variable<double>>::Get(r_variable_name);
-                    block_for_each(rDestinationModelPart.Nodes(), [&](Node& rNode){
-                        const double value = rNode.GetValue(r_variable);
-                        rNode.FastGetSolutionStepValue(r_variable) = value;
-                    });
-                }
-            }
-        }
-    
+    /**
+     * @brief Updates the solution step variables
+     * @param rDestinationModelPart The destination model part
+     * @param rInterpolationVariablesList List of variables to be interpolated
+     * @param rAlpha Alpha value for updating the variables
+     * @param rOldAlpha Previous alpha value
+     * @param ConstrainVariables Flag to apply constraints to the variables
+     */
     static void UpdateSolutionStepVariables(
         ModelPart& rDestinationModelPart,
         std::vector<std::string>& rInterpolationVariablesList,
         double& rAlpha,
         double& rOldAlpha,
-        bool ConstrainVariables) 
-        {
-            if(ConstrainVariables)
-            {
-                for(auto& r_variable_name : rInterpolationVariablesList){
-                    auto& r_variable = KratosComponents<Variable<double>>::Get(r_variable_name);
-                    block_for_each(rDestinationModelPart.Nodes(), [&](Node& rNode){
-                        const double value = rNode.FastGetSolutionStepValue(r_variable);
-                        rNode.FastGetSolutionStepValue(r_variable) = rAlpha * value / rOldAlpha;
-                        rNode.Fix(r_variable);
-                    });
-                }
-            }
-            else
-            {
-                for(auto& r_variable_name : rInterpolationVariablesList){
-                    auto& r_variable = KratosComponents<Variable<double>>::Get(r_variable_name);
-                    block_for_each(rDestinationModelPart.Nodes(), [&](Node& rNode){
-                        const double value = rNode.FastGetSolutionStepValue(r_variable);
-                        rNode.FastGetSolutionStepValue(r_variable) = rAlpha * value / rOldAlpha;
-                    });
-                }
-            }
-        }
+        bool ConstrainVariables);
 };
 
 }  // namespace Kratos.
 
 #endif /* GRADUAL_VARIABLE_INTERPOLATION_UTILITY_H defined */
-
 
