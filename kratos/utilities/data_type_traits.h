336050e420d73d105d5246fc15450a3296878385
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2023-08-12 23:12:26 +0200

    Update kratos/utilities/data_type_traits.h
    Co-authored-by: Máté Kelemen <44344022+matekelemen@users.noreply.github.com>

--- kratos/utilities/data_type_traits.h
+++ kratos/utilities/data_type_traits.h
@@ -1399,7 +1399,7 @@ public:
         if constexpr(IsContiguous) {
             return rValue.data();
         } else {
-            static_assert(!std::is_same_v<TDataType, TDataType>, "This should be only called if the rValue is contiguous only.");
+            static_assert(!std::is_same_v<TDataType, TDataType>, "GetContiguousData should only be called if rValue is contiguous.");
         }
     }
 
