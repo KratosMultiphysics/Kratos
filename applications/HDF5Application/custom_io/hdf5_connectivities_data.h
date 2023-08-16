f4518899fa6fcc43d930854dbd4b1ebff4fe5731
sunethwarna <suneth.warna@gmail.com>
2023-08-16 18:00:27 +0200

    update connectivities data

--- applications/HDF5Application/custom_io/hdf5_connectivities_data.h
+++ applications/HDF5Application/custom_io/hdf5_connectivities_data.h
@@ -4,14 +4,14 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 license: HDF5Application/license.txt
+//  License:        BSD License
+//                  license: HDF5Application/license.txt
 //
 //  Main author:    Michael Andre, https://github.com/msandre
+//                  Suneth Warnakulasuriya
 //
 
-#if !defined(KRATOS_HDF5_CONNECTIVITIES_DATA_H_INCLUDED)
-#define KRATOS_HDF5_CONNECTIVITIES_DATA_H_INCLUDED
+#pragma once
 
 // System includes
 #include <string>
@@ -43,85 +43,56 @@ namespace Internals
 /**
  * Acts as the intermediary between the HDF5 file and the Kratos elements and conditions.
  */
+template<class TContainerType>
 class ConnectivitiesData
 {
 public:
     ///@name Type Definitions
     ///@{
+
+    using IndexType = unsigned int;
+
+    using EntityType = typename TContainerType::value_type;
+
     /// Pointer definition
     KRATOS_CLASS_POINTER_DEFINITION(ConnectivitiesData);
 
     ///@}
     ///@name Life Cycle
     ///@{
+
+    ConnectivitiesData(
+        const std::string& rPrefix,
+        File::Pointer pFile);
+
     ///@}
     ///@name Operations
     ///@{
-    inline std::string const& Name() const
-    {
-        return mName;
-    }
-
-    inline unsigned size() const
-    {
-        return mIds.size();
-    }
 
     /// Read data from a file.
     /**
      * Ensures valid element or condition data is read from the given path on
      * return. Previously stored data is replaced.
      */
-    void ReadData(File& rFile, const std::string& rPath, unsigned StartIndex, unsigned BlockSize);
+    void Read(
+        const std::string& rEntityName,
+        NodesContainerType& rNodes,
+        PropertiesContainerType& rProperties,
+        TContainerType& rEntities);
 
     /// Write data to a file.
-    void WriteData(File& rFile, const std::string& rPath, WriteInfo& rInfo);
+    void Write(const TContainerType& rEntities);
 
-    // Create and append new elements to the container.
-    void CreateEntities(NodesContainerType& rNodes,
-                        PropertiesContainerType& rProperties,
-                        ElementsContainerType& rElements) const;
-
-    // Create and append new conditions to the container.
-    void CreateEntities(NodesContainerType& rNodes,
-                        PropertiesContainerType& rProperties,
-                        ConditionsContainerType& rConditions) const;
-
-    // Fill data from elements of a single element type.
-    /**
-     * Expects a uniform, non-empty container of a single element type.
-     */
-    void SetData(ElementsContainerType const& rElements);
-
-    // Fill data from elements of a single element type.
-    /**
-     * Expects a registered element name and a uniform container of the
-     * corresponding element type. The container may be empty.
-     */
-    void SetData(const std::string& rName, ElementsContainerType const& rElements);
-
-    // Fill data from conditions of a single condition type.
-    /**
-     * Expects a uniform, non-empty container of a single condition type.
-     */
-    void SetData(ConditionsContainerType const& rConditions);
-
-    // Fill data from conditions of a single condition type.
-    /**
-     * Expects a registered condition name and a uniform container of the
-     * corresponding condition type. The container may be empty.
-     */
-    void SetData(const std::string& rName, ConditionsContainerType const& rConditions);
-
-    void Clear();
     ///@}
+
 private:
     ///@name Member Variables
     ///@{
-    std::string mName;
-    Vector<int> mIds;
-    Vector<int> mPropertiesIds;
-    Matrix<int> mConnectivities;
+
+    File::Pointer mpFile;
+
+    std::string mPrefix;
+
     ///@}
 }; // class ConnectivitiesData
 
@@ -130,5 +101,3 @@ private:
 } // namespace Internals.
 } // namespace HDF5.
 } // namespace Kratos.
-
-#endif // KRATOS_HDF5_CONNECTIVITIES_DATA_H_INCLUDED defined
