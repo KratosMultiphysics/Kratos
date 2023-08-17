32e3486b1574d5a0c1283be74776f265dcf09250
AlejandroCornejo <alejandro@DELL-Alejandro.localdomain>
2023-08-17 12:00:40 +0400

    deprecated messages addressed

--- applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
+++ applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
@@ -74,7 +74,7 @@ class MainCoupledFemDem_Solution:
                                                                            self.DEMProperties,
                                                                            self.DEMParameters)
         if self.domain_size == 3:
-            self.nodal_neighbour_finder = KratosMultiphysics.FindNodalNeighboursProcess(self.FEM_Solution.main_model_part)
+            self.nodal_neighbour_finder = KratosMultiphysics.FindGlobalNodalNeighboursProcess(self.FEM_Solution.main_model_part)
 
         if self.FEM_Solution.ProjectParameters.Has("transfer_dem_contact_forces") == False:
             self.TransferDEMContactForcesToFEM = True
