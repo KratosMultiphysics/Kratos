6ca3ed166133e45183e32041b75e0097290b673d
Chengshun Shang <chengshun.shang1996@gmail.com>
2023-08-17 10:03:35 +0200

    Added .vtk output for contacts

--- applications/DEMApplication/python_scripts/dem_vtk_output.py
+++ applications/DEMApplication/python_scripts/dem_vtk_output.py
@@ -66,117 +66,12 @@ class VtkOutput():
         self.PostTangentialImpactVelocity = GetBoolParameterIfItExists(self.DEM_parameters, "PostTangentialImpactVelocity")
         self.PostControlModule = GetBoolParameterIfItExists(self.DEM_parameters, "PostControlModule")
         self.VelTrapGraphExportFreq = self.DEM_parameters["VelTrapGraphExportFreq"].GetDouble()
-        
         self.PostDeltaDisplacement = GetBoolParameterIfItExists(self.DEM_parameters, "PostDeltaDisplacement")
         self.PostCharacteristicLength = GetBoolParameterIfItExists(self.DEM_parameters, "PostCharacteristicLength")
         self.PostBoundingBox = GetBoolParameterIfItExists(self.DEM_parameters, "PostBoundingBox")
         '''
-
-        # for particles
-        self.particles_X = np.empty(0)
-        self.particles_Y = np.empty(0)
-        self.particles_Z = np.empty(0)
-        self.particles_material = np.empty(0)
-        
-        if self.PostRadius:
-            self.particles_R = np.empty(0)
-
-        if self.PostVelocity:
-            self.velocities_X = np.empty(0)
-            self.velocities_Y = np.empty(0)
-            self.velocities_Z = np.empty(0)
-        
-        if self.PostDisplacement:
-            self.displacement_X = np.empty(0)
-            self.displacement_Y = np.empty(0)
-            self.displacement_Z = np.empty(0)
-
-        if self.PostTotalForces:
-            self.total_force_X = np.empty(0)
-            self.total_force_Y = np.empty(0)
-            self.total_force_Z = np.empty(0)
-
-        if self.PostNonDimensionalVolumeWear:
-            self.non_dimensional_volume_wear = np.empty(0)
-        
-        if self.PostAppliedForces:
-            self.applied_force_X = np.empty(0)
-            self.applied_force_Y = np.empty(0)
-            self.applied_force_Z = np.empty(0)
-
-        if self.PostDampForces:
-            self.damp_force_X = np.empty(0)
-            self.damp_force_Y = np.empty(0)
-            self.damp_force_Z = np.empty(0)
-
-        if self.PostGroupId:
-            self.group_id = np.empty(0)
-
-        if self.PostExportId:
-            self.export_id = np.empty(0)
-
-        if self.PostSkinSphere:
-            self.skin_sphere = np.empty(0)
-        
-        if self.PostAngularVelocity:
-            self.angular_velocity_X = np.empty(0)
-            self.angular_velocity_Y = np.empty(0)
-            self.angular_velocity_Z = np.empty(0)
-        
-        if self.PostParticleMoment:
-            self.particle_moment_X = np.empty(0)
-            self.particle_moment_Y = np.empty(0)
-            self.particle_moment_Z = np.empty(0)
-        
-        if self.PostEulerAngles:
-            self.euler_angles_X = np.empty(0)
-            self.euler_angles_Y = np.empty(0)
-            self.euler_angles_Z = np.empty(0)
-        
-        if self.PostRollingResistanceMoment:
-            self.rolling_resistance_moment_X = np.empty(0)
-            self.rolling_resistance_moment_Y = np.empty(0)
-            self.rolling_resistance_moment_Z = np.empty(0)
-        
-        if self.PostNeighbourSize:
-            self.neighbour_size = np.empty(0)
-        
-        if self.PostDamageRatio:
-            self.damage_ratio = np.empty(0)
-
         self.spheres_model_part = weakref.proxy(spheres_model_part)
-
-        #for contacts
-        if self.PostLocalContactForce:
-            self.local_contact_force_X = np.empty(0)
-            self.local_contact_force_Y = np.empty(0)
-            self.local_contact_force_Z = np.empty(0)
-
-        if self.PostFailureCriterionState:
-            self.failure_criterion_state = np.empty(0)
-
-        if self.PostContactFailureId:
-            self.contact_failure_id = np.empty(0)
-
-        if self.PostContactTau:
-            self.contact_tau = np.empty(0)
-
-        if self.PostContactSigma:
-            self.contact_sigma = np.empty(0)
-
-        if self.PostMeanContactArea:
-            self.mean_contact_area = np.empty(0)
-        
         self.contact_model_part = weakref.proxy(contact_model_part)
-        
-        #for geometery
-        self.walls_X = np.empty(0)
-        self.walls_Y = np.empty(0)
-        self.walls_Z = np.empty(0)
-        self.walls_connectivity = np.empty(0)
-        self.walls_offsets = np.empty(0)
-        self.walls_cell_types = np.empty(0)
-
         self.rigid_face_model_part = weakref.proxy(rigid_face_model_part)
 
         self.vtk_post_path_directory = os.path.join(main_path, problem_name + "_Post_VTK_Files")
