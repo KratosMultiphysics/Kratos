cfc96181a1f3b57426f319f8e456a0fa13358386
sunethwarna <suneth.warna@gmail.com>
2023-08-29 22:55:47 +0200

    extend tests

--- applications/HDF5Application/tests/test_HDF5Application.py
+++ applications/HDF5Application/tests/test_HDF5Application.py
@@ -21,7 +21,7 @@ from test_hdf5_xdmf import TestCreateXdmfTemporalGridFromMultifile
 from test_vertex import TestVertex
 from test_point_set_output_process import TestPointSetOutputProcess as TestHDF5PointSetOutputProcess
 from test_line_output_process import TestLineOutputProcess as TestHDF5LineOutputProcess
-
+from test_expression_io import TestExpressionIO
 
 def AssembleTestSuites():
     suites = KratosUnittest.KratosSuites
@@ -41,6 +41,7 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestFindMatchingFiles]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestCreateXdmfTemporalGridFromMultifile]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestVertex, TestHDF5PointSetOutputProcess, TestHDF5LineOutputProcess]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([TestExpressionIO]))
     nightSuite = suites['nightly']
     nightSuite.addTests(smallSuite)
     allSuite = suites['all']
