0ba2de49cd9768e71c79e4eac65637261c8c481d
sunethwarna <suneth.warna@gmail.com>
2023-08-30 23:09:03 +0200

    add variables

--- applications/HDF5Application/hdf5_application_variables.cpp
+++ applications/HDF5Application/hdf5_application_variables.cpp
@@ -1,6 +1,28 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: HDF5Application/license.txt
+//
+//  Main authors:    Jordi Cotela
+//                   Suneth Warnakulasuriya
+//
+
+// System includes
+
+// Project includes
+
+// Application includes
+
+// Include base h
 #include "hdf5_application_variables.h"
 
 namespace Kratos
 {
 
-}
+    KRATOS_CREATE_VARIABLE(HDF5::MeshLocationContainer::Pointer, HDF5_MESH_LOCATION_CONTAINER)
+
+}  // namespace Kratos.
--- applications/HDF5Application/hdf5_application_variables.h
+++ applications/HDF5Application/hdf5_application_variables.h
@@ -4,14 +4,14 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 license: HDF5Application/license.txt
+//  License:         BSD License
+//                   license: HDF5Application/license.txt
 //
 //  Main authors:    Jordi Cotela
+//                   Suneth Warnakulasuriya
 //
 
-#if !defined(KRATOS_HDF5_APPLICATION_VARIABLES_H_INCLUDED )
-#define  KRATOS_HDF5_APPLICATION_VARIABLES_H_INCLUDED
+#pragma once
 
 // System includes
 
@@ -19,12 +19,14 @@
 
 // Project includes
 #include "includes/define.h"
-#include "includes/variables.h"
-#include "includes/kratos_application.h"
+#include "containers/variable.h"
+
+// Application includes
+#include "custom_utilities/mesh_location_container.h"
 
 namespace Kratos
 {
 
-}
+    KRATOS_DEFINE_APPLICATION_VARIABLE(HDF5_APPLICATION, HDF5::MeshLocationContainer::Pointer, HDF5_MESH_LOCATION_CONTAINER)
 
-#endif	/* KRATOS_HDF5_APPLICATION_VARIABLES_H_INCLUDED */
+}  // namespace Kratos.
