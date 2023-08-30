e371e284f60000e4543628ea782f7374e5d74d25
Carlos Roig <roigcarlo@gmail.com>
2023-08-30 17:53:55 +0200

    Restoring original checks for expect before moving to expect + message from gtest

--- kratos/includes/expect.h
+++ kratos/includes/expect.h
@@ -172,13 +172,13 @@ try {                                                                           
 }
 
 #define KRATOS_EXPECT_VARIABLE_IN_NODAL_DATA(TheVariable, TheNode)                          \
-    KRATOS_EXPECT_TRUE(TheNode.SolutionStepsDataHas(TheVariable));                         \
-        KRATOS_ERROR << "Missing " << TheVariable.Name() << " variable in solution step data for node " \
+    KRATOS_ERROR_IF_NOT(TheNode.SolutionStepsDataHas(TheVariable))                         \
+        << "Missing " << TheVariable.Name() << " variable in solution step data for node " \
         << TheNode.Id() << "." << std::endl;
 
 #define KRATOS_EXPECT_DOF_IN_NODE(TheVariable, TheNode)            \
-    KRATOS_EXPECT_TRUE(TheNode.HasDofFor(TheVariable));           \
-        KRATOS_ERROR << "Missing Degree of Freedom for " << TheVariable.Name() \
+    KRATOS_ERROR_IF_NOT(TheNode.HasDofFor(TheVariable))           \
+        << "Missing Degree of Freedom for " << TheVariable.Name() \
         << " in node " << TheNode.Id() << "." << std::endl;
 
 #ifdef KRATOS_DEBUG
