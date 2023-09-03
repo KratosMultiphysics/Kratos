0bbf48e6ea3005e424f0be16180a23a2496255ae
sunethwarna <suneth.warna@gmail.com>
2023-09-03 19:36:33 +0200

    add expression xdmf tests

--- applications/HDF5Application/tests/test_dataset_generator.py
+++ applications/HDF5Application/tests/test_dataset_generator.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import KratosMultiphysics as Kratos
+import KratosMultiphysics.HDF5Application as KratosHDF5
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.kratos_utilities as kratos_utils
 from KratosMultiphysics.compare_two_files_check_process import CompareTwoFilesCheckProcess
@@ -10,6 +11,7 @@ from KratosMultiphysics.HDF5Application.core.dataset_generator import SingleFile
 from KratosMultiphysics.HDF5Application.core.dataset_generator import MultiFileDatasetsGenerator
 from KratosMultiphysics.HDF5Application.xdmf_utils import WriteDataSetsToXdmf
 
+from KratosMultiphysics.HDF5Application.core.processes import HDF5OutputProcess
 from KratosMultiphysics.HDF5Application.single_mesh_temporal_output_process import SingleMeshTemporalOutputProcess
 from KratosMultiphysics.HDF5Application.multiple_mesh_temporal_output_process import MultipleMeshTemporalOutputProcess
 
@@ -28,6 +30,10 @@ class TestDatasetGenerator(KratosUnittest.TestCase):
         cls.model_part.CreateNewCondition("LineCondition2D2N", 1, [1, 2], properties)
 
         for entity in cls.model_part.Nodes:
+            entity.SetValue(Kratos.PRESSURE, entity.Id)
+            entity.SetValue(Kratos.VELOCITY, [entity.Id, entity.Id + 1, entity.Id + 2])
+
+        for entity in cls.model_part.Conditions:
             entity.SetValue(Kratos.PRESSURE, entity.Id)
             entity.SetValue(Kratos.VELOCITY, [entity.Id, entity.Id + 1, entity.Id + 2])
 
@@ -298,6 +304,72 @@ class TestDatasetGenerator(KratosUnittest.TestCase):
             CompareTwoFilesCheckProcess(Kratos.Parameters("""
             {
                 "reference_file_name"   : "single_mesh4.orig.xdmf",
+                "output_file_name"      : "single_mesh5.xdmf",
+                "remove_output_file"    : true,
+                "comparison_type"       : "deterministic"
+            }""")).Execute()
+
+    def testSingleMeshMultiFileSameDatasetsGenerator(self):
+        number_of_time_steps = 5
+
+        for t in range(1, number_of_time_steps + 1, 1):
+            self.addCleanup(kratos_utils.DeleteFileIfExisting, str((Path(__file__).parent / f"auxiliar_files/xdmf/test-{t:0.7f}.h5").absolute()))
+
+        nodal_cexp = Kratos.Expression.NodalExpression(self.model_part)
+        cond_cexp = Kratos.Expression.ConditionExpression(self.model_part)
+        elem_cexp = Kratos.Expression.ElementExpression(self.model_part)
+
+        Kratos.Expression.VariableExpressionIO.Read(nodal_cexp, Kratos.VELOCITY, False)
+        Kratos.Expression.VariableExpressionIO.Read(cond_cexp, Kratos.VELOCITY)
+        Kratos.Expression.VariableExpressionIO.Read(elem_cexp, Kratos.VELOCITY)
+
+        with KratosUnittest.WorkFolderScope("auxiliar_files/xdmf", __file__):
+            process = HDF5OutputProcess(self.model_part)
+            process_id = process.GetProcessId()
+
+            parameters = Kratos.Parameters("""{
+                "file_name"        : "<model_part_name>-<time>.h5",
+                "file_access_mode" : "exclusive",
+                "custom_attributes": {
+                    "__hdf5_process_id": []
+                }
+            }""")
+            parameters["custom_attributes"]["__hdf5_process_id"].Append(process_id[0])
+            parameters["custom_attributes"]["__hdf5_process_id"].Append(process_id[1])
+            model_part_written = False
+            for t in range(1, number_of_time_steps + 1, 1):
+                parameters["file_name"].SetString(f"test-{t:0.7f}.h5")
+                h5_file = KratosHDF5.HDF5File(self.model_part.GetCommunicator().GetDataCommunicator(), parameters)
+                if not model_part_written:
+                    model_param = Kratos.Parameters("""{
+                        "prefix": "/ModelData",
+                        "custom_attributes": {
+                            "__hdf5_process_id": []
+                        }
+                    }""")
+                    model_param["custom_attributes"]["__hdf5_process_id"].Append(process_id[0])
+                    model_param["custom_attributes"]["__hdf5_process_id"].Append(process_id[1])
+                    KratosHDF5.HDF5ModelPartIO(model_param, h5_file).WriteModelPart(self.model_part)
+                    model_part_written = True
+
+                nodal_cexp += nodal_cexp
+                cond_cexp += cond_cexp
+                elem_cexp += elem_cexp
+
+                attribs = Kratos.Parameters("""{
+                    "__hdf5_process_id": []
+                }""")
+                attribs["__hdf5_process_id"].Append(process_id[0])
+                attribs["__hdf5_process_id"].Append(process_id[1])
+                KratosHDF5.ExpressionIO(Kratos.Parameters("""{"prefix":"/Results/"}"""), h5_file).Write("nodal_values", nodal_cexp, attribs)
+                KratosHDF5.ExpressionIO(Kratos.Parameters("""{"prefix":"/Results/"}"""), h5_file).Write("cond_values", cond_cexp, attribs)
+                KratosHDF5.ExpressionIO(Kratos.Parameters("""{"prefix":"/Results/"}"""), h5_file).Write("elem_values", elem_cexp, attribs)
+
+            generator = SingleMeshMultiFileSameDatasetsGenerator("test-<time>.h5")
+            WriteDataSetsToXdmf(generator, "single_mesh5.xdmf")
+            CompareTwoFilesCheckProcess(Kratos.Parameters("""
+            {
+                "reference_file_name"   : "single_mesh5.orig.xdmf",
                 "output_file_name"      : "single_mesh5.xdmf",
                 "remove_output_file"    : true,
                 "comparison_type"       : "deterministic"
