bc7963b7c2eb7bfe685cebdccc9f20af2f2b6628
Rishith Ellath Meethal <emsanurth@gmail.com>
2023-09-04 22:08:05 +0000

    making test functions correct

--- applications/NeuralNetworkApplication/python_scripts/input_dataclasses.py
+++ applications/NeuralNetworkApplication/python_scripts/input_dataclasses.py
@@ -2,6 +2,7 @@ import numpy as np
 from dataclasses import dataclass, field
 from typing import List
 
+
 @dataclass
 class NeuralNetworkData:
     data: np.ndarray = None
@@ -14,13 +15,13 @@ class NeuralNetworkData:
 
     def ExportAsArray(self):
         try:
-            '''for converting pytorch predictions to numpy'''
+            """for converting pytorch predictions to numpy"""
             self.data = self.data.detach().numpy()
             return self.data
         except:
             return self.data
 
-    def ExportElementAsArray(self, id):
+    def ExportElementAsArray(self, element_id):
         return self.ExportAsArray()
 
     def ExportDataOnly(self):
@@ -41,6 +42,7 @@ class NeuralNetworkData:
     def GetDataShape(self):
         return self.data.shape
 
+
 @dataclass
 class DataWithLookback(NeuralNetworkData):
     lookback_index: int = 1
@@ -59,25 +61,36 @@ class DataWithLookback(NeuralNetworkData):
                     new_array = np.squeeze(self.lookback_data)
                 else:
                     if self.data.size == 1:
-                        new_array = np.concatenate((np.reshape(np.squeeze(self.data), (1,)), np.squeeze(self.lookback_data)))
+                        new_array = np.concatenate(
+                            (
+                                np.reshape(np.squeeze(self.data), (1,)),
+                                np.squeeze(self.lookback_data),
+                            )
+                        )
                     else:
                         try:
                             new_array = np.hstack((self.data, self.lookback_data))
                         except ValueError:
-                            raise Exception("Dimension mismatch between the lookback and the data. Check if the dimensions are compatible.")
+                            raise Exception(
+                                "Dimension mismatch between the lookback and the data. Check if the dimensions are compatible."
+                            )
                 try:
-                    new_array = np.reshape(new_array, (1, new_array.shape[0], new_array.shape[1]))
+                    new_array = np.reshape(
+                        new_array, (1, new_array.shape[0], new_array.shape[1])
+                    )
                 except IndexError:
                     # if self.lookback_index == 1:
                     #     new_array = np.reshape(new_array, (1, 1, new_array.shape[0]))
                     # else:
-                        new_array = np.reshape(new_array, (1, new_array.shape[0], 1))
+                    new_array = np.reshape(new_array, (1, new_array.shape[0], 1))
                 if self.reorder_partitions > 1:
                     new_array = self.Reorder(new_array, self.reorder_partitions)
                 return new_array
             else:
                 try:
-                    new_array = np.reshape(self.data, (1, self.data.shape[0], self.data.shape[1]))
+                    new_array = np.reshape(
+                        self.data, (1, self.data.shape[0], self.data.shape[1])
+                    )
                 except IndexError:
                     new_array = np.reshape(self.data, (1, self.data.shape[0], 1))
                 if self.reorder_partitions > 1:
@@ -86,7 +99,9 @@ class DataWithLookback(NeuralNetworkData):
         else:
             new_array = self.lookback_data
             try:
-                new_array = np.reshape(new_array, (1, new_array.shape[0], new_array.shape[1]))
+                new_array = np.reshape(
+                    new_array, (1, new_array.shape[0], new_array.shape[1])
+                )
             except IndexError:
                 new_array = np.reshape(new_array, (1, new_array.shape[0], 1))
             if self.reorder_partitions > 1:
@@ -97,7 +112,7 @@ class DataWithLookback(NeuralNetworkData):
         partitions = np.dsplit(array, number_of_partitions)
         new_array = array
         for i in range(len(partitions)):
-            new_array[...,i::number_of_partitions] = partitions[i]
+            new_array[..., i::number_of_partitions] = partitions[i]
 
         return new_array
 
@@ -107,13 +122,12 @@ class DataWithLookback(NeuralNetworkData):
     def UpdateLookbackLast(self, new_data):
         new_array = np.zeros_like(self.lookback_data)
         if self.lookback_index > 1:
-
             # if len(new_data.shape)>1:
             #     new_array[:-new_data.shape[0]] = self.lookback_data[new_data.shape[0]:]
             #     new_array[-new_data.shape[0]:] = new_data
             # else:
-                new_array[:-1] = self.lookback_data[1:]
-                new_array[-1] = new_data
+            new_array[:-1] = self.lookback_data[1:]
+            new_array[-1] = new_data
         else:
             new_array = new_data
         self.lookback_data = new_array
@@ -130,7 +144,7 @@ class DataWithLookback(NeuralNetworkData):
     def UpdateLookbackAll(self, new_array):
         self.lookback_data = new_array
         try:
-            if len(new_array.shape)>1:
+            if len(new_array.shape) > 1:
                 self.lookback_index = new_array.shape[0]
         except AttributeError:
             pass
@@ -141,7 +155,9 @@ class DataWithLookback(NeuralNetworkData):
             self.UpdateLookbackLast(new_data)
         else:
             if self.lookback_index > 1:
-                self.lookback_data = np.stack(list(np.zeros_like(new_data) for _ in range(self.lookback_index)))
+                self.lookback_data = np.stack(
+                    list(np.zeros_like(new_data) for _ in range(self.lookback_index))
+                )
                 self.lookback_data[-1] = new_data
             else:
                 self.lookback_data = new_data
@@ -149,8 +165,14 @@ class DataWithLookback(NeuralNetworkData):
             self.lookback_state = True
 
     def FlattenTo2DLookback(self):
-        if len(self.lookback_data.shape)>2:
-            self.lookback_data = np.reshape(self.lookback_data, (self.lookback_index, int(self.lookback_data.size/self.lookback_index)))
+        if len(self.lookback_data.shape) > 2:
+            self.lookback_data = np.reshape(
+                self.lookback_data,
+                (
+                    self.lookback_index,
+                    int(self.lookback_data.size / self.lookback_index),
+                ),
+            )
         else:
             pass
 
@@ -159,7 +181,9 @@ class DataWithLookback(NeuralNetworkData):
             self.UpdateRecordLast(new_data)
         else:
             if self.lookback_index > 1:
-                self.data = np.stack(list(np.zeros_like(new_data) for _ in range(self.lookback_index)))
+                self.data = np.stack(
+                    list(np.zeros_like(new_data) for _ in range(self.lookback_index))
+                )
                 self.data[-1] = new_data
             else:
                 self.data = new_data
@@ -171,7 +195,7 @@ class DataWithLookback(NeuralNetworkData):
     def ExtendFromNeuralNetworkData(self, data_structure):
         self.data = data_structure.data
 
-    def SetOnlyLookback(self, switch_to = True):
+    def SetOnlyLookback(self, switch_to=True):
         self.only_lookback = switch_to
 
     def GetLookbackIndex(self):
@@ -179,46 +203,53 @@ class DataWithLookback(NeuralNetworkData):
 
     def GetLookbackShape(self):
         if self.lookback_index == 1:
-                return tuple((1, len(self.lookback_data)))
+            return tuple((1, len(self.lookback_data)))
         else:
             try:
                 return self.lookback_data.shape
             except AttributeError:
-
-                    return tuple((self.lookback_index,0))
+                return tuple((self.lookback_index, 0))
 
     def ExportElementAsArray(self, id):
-
         array = self.ExportAsArray()
 
         try:
-            if self.only_lookback and (self.timesteps_as_features or self.features_as_timesteps):
+            if self.only_lookback and (
+                self.timesteps_as_features or self.features_as_timesteps
+            ):
                 feature_length = self.GetLookbackShape()[1] * self.lookback_index
             elif self.only_lookback:
                 feature_length = self.GetLookbackShape()[1]
             elif self.timesteps_as_features or self.features_as_timesteps:
-                feature_length = self.GetDataShape()[1]+self.GetLookbackShape()[1] * self.lookback_index
+                feature_length = (
+                    self.GetDataShape()[1]
+                    + self.GetLookbackShape()[1] * self.lookback_index
+                )
             else:
-                feature_length = self.GetDataShape()[1]+self.GetLookbackShape()[1]
+                feature_length = self.GetDataShape()[1] + self.GetLookbackShape()[1]
         except IndexError:
             if self.timesteps_as_features or self.features_as_timesteps:
-                feature_length = self.GetDataShape()[0]+self.GetLookbackShape()[1] * self.lookback_index
+                feature_length = (
+                    self.GetDataShape()[0]
+                    + self.GetLookbackShape()[1] * self.lookback_index
+                )
             else:
-                feature_length = self.GetDataShape()[0]+self.GetLookbackShape()[1]
+                feature_length = self.GetDataShape()[0] + self.GetLookbackShape()[1]
 
         if self.timesteps_as_features:
-            array = np.reshape(array, (1,1, feature_length))
+            array = np.reshape(array, (1, 1, feature_length))
         elif self.features_as_timesteps:
-            array = np.reshape(array, (1, feature_length,1))
+            array = np.reshape(array, (1, feature_length, 1))
         else:
-            array = np.reshape(array, (1,self.lookback_index, feature_length))
+            array = np.reshape(array, (1, self.lookback_index, feature_length))
         return array
 
-    def SetTimestepsAsFeatures(self, switch_to = True):
+    def SetTimestepsAsFeatures(self, switch_to=True):
         self.timesteps_as_features = switch_to
 
-    def SetFeaturesAsTimesteps(self, switch_to = True):
+    def SetFeaturesAsTimesteps(self, switch_to=True):
         self.features_as_timesteps = switch_to
+
 
 @dataclass
 class ListNeuralNetworkData:
@@ -227,7 +258,9 @@ class ListNeuralNetworkData:
     def AddToList(self, new_array):
         if isinstance(new_array, NeuralNetworkData):
             self.data_array.append(new_array)
-        elif isinstance(new_array, np.ndarray) or isinstance(new_array, (list, float, np.floating)):
+        elif isinstance(new_array, np.ndarray) or isinstance(
+            new_array, (list, float, np.floating)
+        ):
             self.data_array.append(NeuralNetworkData(new_array))
         else:
             self.data_array.append(NeuralNetworkData(np.ndarray(new_array)))
@@ -236,19 +269,26 @@ class ListNeuralNetworkData:
         array = []
         for i in range(len(self.data_array)):
             try:
-                if len(array)>0 or not isinstance(array, list):
+                if len(array) > 0 or not isinstance(array, list):
                     new_timestep = self.data_array[i].ExportAsArray()
                     array = np.vstack([array, new_timestep])
                 else:
                     array = self.data_array[i].ExportAsArray()
             except TypeError:
-                array = np.reshape(array,array.shape + tuple((1,)))
+                array = np.reshape(array, array.shape + tuple((1,)))
                 array = np.vstack([array, self.data_array[i].ExportAsArray()])
 
         # THIS CAN BE MORE EFFICIENT, RESHAPE TAKES A LOT OF TIME
         if not self.data_array[0].GetDataShape() == ():
             try:
-                array = np.reshape(array, (len(self.data_array),self.data_array[0].GetDataShape()[0], self.data_array[0].GetDataShape()[1]))
+                array = np.reshape(
+                    array,
+                    (
+                        len(self.data_array),
+                        self.data_array[0].GetDataShape()[0],
+                        self.data_array[0].GetDataShape()[1],
+                    ),
+                )
             except IndexError:
                 pass
         return array
@@ -276,7 +316,7 @@ class ListNeuralNetworkData:
     def __getitem__(self, key):
         return self.data_array[key]
 
-    def KeepPart(self,key):
+    def KeepPart(self, key):
         self.data_array = self.data_array[key]
 
     def GetDataShape(self):
@@ -298,19 +338,21 @@ class ListDataWithLookback(ListNeuralNetworkData):
         elif isinstance(new_array, np.ndarray) or isinstance(new_array, list):
             self.data_array.append(DataWithLookback(new_array, self.lookback_index))
         else:
-            self.data_array.append(DataWithLookback(np.ndarray(new_array), self.lookback_index))
+            self.data_array.append(
+                DataWithLookback(np.ndarray(new_array), self.lookback_index)
+            )
 
     def ExportAsArray(self):
         array = []
         for i in range(len(self.data_array)):
             try:
-                if len(array)>0 or not isinstance(array, list):
+                if len(array) > 0 or not isinstance(array, list):
                     new_timestep = self.data_array[i].ExportAsArray()
                     array = np.vstack([array, new_timestep])
                 else:
                     array = self.data_array[i].ExportAsArray()
             except TypeError:
-                array = np.reshape(array,array.shape + tuple((1,)))
+                array = np.reshape(array, array.shape + tuple((1,)))
                 array = np.vstack([array, self.data_array[i].ExportAsArray()])
         out_array = self.__Reshape(array)
         return out_array
@@ -319,20 +361,19 @@ class ListDataWithLookback(ListNeuralNetworkData):
         array = []
         for i in range(len(self.data_array)):
             try:
-                if len(array)>0 or not isinstance(array, list):
+                if len(array) > 0 or not isinstance(array, list):
                     new_timestep = self.data_array[i].ExportDataOnly()
                     array = np.vstack([array, new_timestep])
                 else:
                     array = self.data_array[i].ExportDataOnly()
             except TypeError:
-                array = np.reshape(array,array.shape + tuple((1,)))
+                array = np.reshape(array, array.shape + tuple((1,)))
                 array = np.vstack([array, self.data_array[i].ExportDataOnly()])
 
         out_array = self.__Reshape(array, data_only=True)
         return out_array
 
     def ExportElementAsArray(self, element_id):
-
         array = self.data_array[element_id].ExportElementAsArray(element_id)
 
         # try:
@@ -358,13 +399,12 @@ class ListDataWithLookback(ListNeuralNetworkData):
         #     array = np.reshape(array, (1,self.lookback_index, feature_length))
         return array
 
-
     def ExportLookbackAsArray(self):
         array = []
         for i in range(len(self.data_array)):
             self.data_array[i].SetOnlyLookback()
             try:
-                if len(array)>0 or not isinstance(array, list):
+                if len(array) > 0 or not isinstance(array, list):
                     array = np.vstack([array, self.data_array[i].ExportAsArray()])
                 else:
                     array = self.data_array[i].ExportAsArray()
@@ -377,7 +417,7 @@ class ListDataWithLookback(ListNeuralNetworkData):
             self.data_array[i].UpdateLookbackLast(array[i])
 
     def UpdateLookbackAll(self, array):
-       for i in range(array.shape[0]):
+        for i in range(array.shape[0]):
             self.data_array[i].UpdateLookbackAll(array[i])
 
     def CheckLookbackAndUpdate(self, array):
@@ -401,16 +441,16 @@ class ListDataWithLookback(ListNeuralNetworkData):
         for i in range(len(self.data_array)):
             self.data_array[i].FlattenTo2DLookback()
 
-    def SetOnlyLookback(self, switch_to = True):
+    def SetOnlyLookback(self, switch_to=True):
         for i in range(len(self.data_array)):
             self.data_array[i].SetOnlyLookback(switch_to)
 
-    def SetTimestepsAsFeatures(self, switch_to = True):
+    def SetTimestepsAsFeatures(self, switch_to=True):
         self.timesteps_as_features = switch_to
         for entry in self.data_array:
             entry.SetTimestepsAsFeatures(switch_to)
 
-    def SetFeaturesAsTimesteps(self, switch_to = True):
+    def SetFeaturesAsTimesteps(self, switch_to=True):
         self.features_as_timesteps = switch_to
         for entry in self.data_array:
             entry.SetFeaturesAsTimesteps(switch_to)
@@ -430,22 +470,27 @@ class ListDataWithLookback(ListNeuralNetworkData):
         self.data_array[-1].UpdateData(new_data)
         self.data_array[-1].CheckLookbackAndUpdate(new_lookback)
 
-    def __Reshape(self, array, data_only = False):
-
-         # THIS CAN BE MORE EFFICIENT, RESHAPE TAKES A LOT OF TIME
+    def __Reshape(self, array, data_only=False):
+        # THIS CAN BE MORE EFFICIENT, RESHAPE TAKES A LOT OF TIME
 
         # Calculate the length of the lookback (flattened or not)
         if self.timesteps_as_features or self.features_as_timesteps:
-            lookback_length = self.data_array[0].GetLookbackShape()[1] * self.lookback_index
+            lookback_length = (
+                self.data_array[0].GetLookbackShape()[1] * self.lookback_index
+            )
             # Calculate the lenght of the data (with record of the lookback or not)
             try:
                 if self.record_data:
-                    data_length = self.data_array[0].GetDataShape()[1] * self.lookback_index
+                    data_length = (
+                        self.data_array[0].GetDataShape()[1] * self.lookback_index
+                    )
                 else:
                     data_length = self.data_array[0].GetDataShape()[1]
             except IndexError:
                 if self.record_data:
-                    data_length = self.data_array[0].GetDataShape()[0] * self.lookback_index
+                    data_length = (
+                        self.data_array[0].GetDataShape()[0] * self.lookback_index
+                    )
                 else:
                     data_length = self.data_array[0].GetDataShape()[0]
         else:
@@ -468,11 +513,13 @@ class ListDataWithLookback(ListNeuralNetworkData):
         # Reshape for output
         if not self.data_array[0].GetDataShape() == ():
             if self.timesteps_as_features:
-                out_array = np.reshape(array, (len(self.data_array),1, feature_length))
+                out_array = np.reshape(array, (len(self.data_array), 1, feature_length))
             elif self.features_as_timesteps:
-                out_array = np.reshape(array, (len(self.data_array), feature_length,1))
+                out_array = np.reshape(array, (len(self.data_array), feature_length, 1))
             elif data_only and not self.record_data:
                 out_array = np.reshape(array, (len(self.data_array), feature_length))
             else:
-                out_array = np.reshape(array, (len(self.data_array),self.lookback_index, feature_length))
+                out_array = np.reshape(
+                    array, (len(self.data_array), self.lookback_index, feature_length)
+                )
         return out_array
--- applications/NeuralNetworkApplication/tests/test_data_generation.py
+++ applications/NeuralNetworkApplication/tests/test_data_generation.py
@@ -2,11 +2,14 @@ import KratosMultiphysics
 from KratosMultiphysics import IsDistributedRun
 import os
 
-from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_analysis import StructuralMechanicsAnalysis
+from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_analysis import (
+    StructuralMechanicsAnalysis,
+)
 import KratosMultiphysics.NeuralNetworkApplication as NeuralNetworkApplication
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.kratos_utilities as kratos_utils
 import KratosMultiphysics.NeuralNetworkApplication.data_loading_utilities as DataLoadingUtils
+
 
 def SelectAndVerifyLinearSolver(settings, skiptest):
     # The mechanical solver selects automatically the fastest linear-solver available
@@ -15,18 +18,26 @@ def SelectAndVerifyLinearSolver(settings, skiptest):
     if not settings["solver_settings"].Has("linear_solver_settings"):
         # check if running in MPI because there we use a different default linear solver
         if IsDistributedRun():
-            default_lin_solver_settings = KratosMultiphysics.Parameters("""{
+            default_lin_solver_settings = KratosMultiphysics.Parameters(
+                """{
                 "solver_type" : "amesos",
                 "amesos_solver_type" : "Amesos_Klu"
-            }""")
+            }"""
+            )
 
         else:
-            default_lin_solver_settings = KratosMultiphysics.Parameters("""{
+            default_lin_solver_settings = KratosMultiphysics.Parameters(
+                """{
                 "solver_type": "LinearSolversApplication.sparse_lu"
-            }""")
-        settings["solver_settings"].AddValue("linear_solver_settings", default_lin_solver_settings)
+            }"""
+            )
+        settings["solver_settings"].AddValue(
+            "linear_solver_settings", default_lin_solver_settings
+        )
 
-    solver_type = settings["solver_settings"]["linear_solver_settings"]["solver_type"].GetString()
+    solver_type = settings["solver_settings"]["linear_solver_settings"][
+        "solver_type"
+    ].GetString()
     solver_type_splitted = solver_type.split(".")
     if len(solver_type_splitted) == 2:
         # this means that we use a solver from an application
@@ -34,25 +45,31 @@ def SelectAndVerifyLinearSolver(settings, skiptest):
         app_name = solver_type_splitted[0]
         solver_name = solver_type_splitted[1]
         if not kratos_utils.CheckIfApplicationsAvailable(app_name):
-            skiptest('Application "{}" is needed for the specified solver "{}" but is not available'.format(app_name, solver_name))
+            skiptest(
+                'Application "{}" is needed for the specified solver "{}" but is not available'.format(
+                    app_name, solver_name
+                )
+            )
+
 
 class TestDataGenerationFactory(KratosUnittest.TestCase):
-
     def _check_data_dimensions(self):
-
-        input_data = DataLoadingUtils.ImportDataFromFile(self.output_file_in, 'InputData')
-        self.assertEqual(input_data[0].size, self.dimension_in)
+        input_data = DataLoadingUtils.ImportDataFromFile(
+            self.output_file_in, "InputData"
+        )
+        self.assertEqual(input_data[0].data.size, self.dimension_in)
         # os.remove(self.output_file_in)
-        output_data = DataLoadingUtils.ImportDataFromFile(self.output_file_out, 'OutputData')
-        self.assertEqual(output_data[0].size, self.dimension_out)
+        output_data = DataLoadingUtils.ImportDataFromFile(
+            self.output_file_out, "OutputData"
+        )
+        self.assertEqual(output_data[0].data.size, self.dimension_out)
         # os.remove(self.output_file_out)
 
     def setUp(self):
         # Within this location context:
         with KratosUnittest.WorkFolderScope(".", __file__):
-
             # Reading the ProjectParameters
-            with open(self.file_name + "_parameters.json",'r') as parameter_file:
+            with open(self.file_name + "_parameters.json", "r") as parameter_file:
                 ProjectParameters = KratosMultiphysics.Parameters(parameter_file.read())
 
             SelectAndVerifyLinearSolver(ProjectParameters, self.skipTest)
@@ -61,9 +78,13 @@ class TestDataGenerationFactory(KratosUnittest.TestCase):
 
             # To avoid many prints
             if ProjectParameters["problem_data"]["echo_level"].GetInt() == 0:
-                KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.WARNING)
+                KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(
+                    KratosMultiphysics.Logger.Severity.WARNING
+                )
             else:
-                KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(KratosMultiphysics.Logger.Severity.INFO)
+                KratosMultiphysics.Logger.GetDefaultOutput().SetSeverity(
+                    KratosMultiphysics.Logger.Severity.INFO
+                )
 
             # Creating the test
             model = KratosMultiphysics.Model()
@@ -80,14 +101,13 @@ class TestDataGenerationFactory(KratosUnittest.TestCase):
         # Within this location context:
         with KratosUnittest.WorkFolderScope(".", __file__):
             self.test.RunSolutionLoop()
-            # self._check_data_dimensions()
-
+            self._check_data_dimensions()
 
     def tearDown(self):
         # Within this location context:
         with KratosUnittest.WorkFolderScope(".", __file__):
             self.test.Finalize()
-            self._check_data_dimensions()
+            # self._check_data_dimensions()
 
 
 class TestDataGeneration1I1OVariablesText(TestDataGenerationFactory):
@@ -97,12 +117,14 @@ class TestDataGeneration1I1OVariablesText(TestDataGenerationFactory):
     dimension_in = 1
     dimension_out = 1
 
+
 class TestDataGeneration1I2OVariablesText(TestDataGenerationFactory):
     file_name = "beam_cantilever_nonlinear/1I2OVariablesText"
     output_file_in = "data/training_in_raw.dat"
     output_file_out = "data/training_out_raw.dat"
     dimension_in = 1
     dimension_out = 2
+
 
 class TestDataGeneration1IVectorOVariablesText(TestDataGenerationFactory):
     file_name = "beam_cantilever_nonlinear/1IVectorOVariablesText"
@@ -111,12 +133,14 @@ class TestDataGeneration1IVectorOVariablesText(TestDataGenerationFactory):
     dimension_in = 1
     dimension_out = 3
 
+
 class TestDataGeneration1I1OVariablesHDF5(TestDataGenerationFactory):
     file_name = "beam_cantilever_nonlinear/1I1OVariablesHDF5"
     output_file_in = "data/training_in_raw_1o.h5"
     output_file_out = "data/training_out_raw_1o.h5"
     dimension_in = 1
     dimension_out = 1
+
 
 class TestDataGeneration1I2OVariablesHDF5(TestDataGenerationFactory):
     file_name = "beam_cantilever_nonlinear/1I2OVariablesHDF5"
@@ -125,6 +149,7 @@ class TestDataGeneration1I2OVariablesHDF5(TestDataGenerationFactory):
     dimension_in = 1
     dimension_out = 2
 
+
 class TestDataGeneration1IVectorOVariablesHDF5(TestDataGenerationFactory):
     file_name = "beam_cantilever_nonlinear/1IVectorOVariablesHDF5"
     output_file_in = "data/training_in_raw_vector.h5"
@@ -132,5 +157,6 @@ class TestDataGeneration1IVectorOVariablesHDF5(TestDataGenerationFactory):
     dimension_in = 1
     dimension_out = 3
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     KratosUnittest.main()
