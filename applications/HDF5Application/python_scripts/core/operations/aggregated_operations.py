595cf094f6578f3457b4ebbc60f55dcf1887c7d6
sunethwarna <suneth.warna@gmail.com>
2023-09-04 09:01:50 +0200

    move custom args to controlledopt

--- applications/HDF5Application/python_scripts/core/operations/aggregated_operations.py
+++ applications/HDF5Application/python_scripts/core/operations/aggregated_operations.py
@@ -9,10 +9,12 @@ class ControlledOperation:
     def __init__(self,
                  operation_type: 'typing.Type[Kratos.Operation]',
                  parameters: Kratos.Parameters,
-                 controller: Kratos.Controller) -> None:
+                 controller: Kratos.Controller,
+                 *args) -> None:
         self.__parameters = parameters
         self.__operation_type = operation_type
         self.__controller = controller
+        self.__other_args = args
 
         if self.__parameters is not None:
             self.__parameters.AddMissingParameters(self.__operation_type.GetDefaultParameters())
@@ -25,7 +27,7 @@ class ControlledOperation:
 
     def Execute(self, model_part: Kratos.ModelPart, hdf5_file: KratosHDF5.HDF5File) -> None:
         if self.Evaluate():
-            self.__operation_type(model_part, self.__parameters, hdf5_file).Execute()
+            self.__operation_type(model_part, self.__parameters, hdf5_file, *self.__other_args).Execute()
 
     def Update(self) -> None:
         self.__controller.Update()
@@ -33,9 +35,9 @@ class ControlledOperation:
 class AggregatedControlledOperations:
     def __init__(self,
                  model_part: Kratos.ModelPart,
-                 parameters: Kratos.Parameters) -> None:
+                 hdf5_file_settings: Kratos.Parameters) -> None:
         self.__model_part = model_part
-        self.__hdf5_file_parameters = parameters
+        self.__hdf5_file_parameters = hdf5_file_settings
         self.__list_of_controlled_operations: 'list[ControlledOperation]' = []
 
     def Check(self):
