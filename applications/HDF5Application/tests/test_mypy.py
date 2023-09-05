0b41a91fafa22efdec283e6895f6a33b51fc5b0d
sunethwarna <suneth.warna@gmail.com>
2023-09-05 18:43:26 +0200

    minor

--- applications/HDF5Application/tests/test_mypy.py
+++ applications/HDF5Application/tests/test_mypy.py
@@ -4,6 +4,11 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 class TestTypeHinting(KratosUnittest.TestCase):
     def test_TypeHinting(self):
+        try:
+            import mypy
+        except:
+            self.skipTest("Requires h5py.")
+
         install_path = Kratos.KratosPaths.kratos_install_path
         self.assertEqual(subprocess.run(["mypy", f"{install_path}/KratosMultiphysics/HDF5Application"]).returncode, 0)
 
