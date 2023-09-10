0663adbc5815f9d384db8834357ead6ac19236d3
SADPR <aresdepargas@gmail.com>
2023-09-10 12:04:57 +0200

    Updated dynamic thermal test

--- applications/RomApplication/tests/test_thermal_lspg_rom.py
+++ applications/RomApplication/tests/test_thermal_lspg_rom.py
@@ -13,7 +13,7 @@ if kratos_utilities.CheckIfApplicationsAvailable("ConvectionDiffusionApplication
 class TestThermalLSPGRom(KratosUnittest.TestCase):
 
     def setUp(self):
-        self.relative_tolerance = 1.0e-8
+        self.relative_tolerance = 1.0e-12
 
     def testConvDiffStationaryLSPGRom2D(self):
         self.work_folder = "thermal_static_test_files/LSPGROM/"
@@ -36,56 +36,55 @@ class TestThermalLSPGRom(KratosUnittest.TestCase):
             nodal_area = rom_testing_utilities.GetNodalAreaVector(self.simulation._GetSolver().GetComputingModelPart())
 
             l2 = np.sqrt((sum(nodal_area*((1 - obtained_output/expected_output )**2)))/(sum(nodal_area)))*100
-            print(l2)
-            # self.assertLess(l2, self.relative_tolerance)
+            self.assertLess(l2, self.relative_tolerance)
 
-    # def testConvDiffDynamicRom2D(self):
-    #     self.work_folder = "thermal_dynamic_test_files/LSPGROM/"
-    #     parameters_filename = "../ProjectParameters.json"
-    #     expected_output_filename = "ExpectedOutputLSPGROM.npy"
+    def testConvDiffDynamicRom2D(self):
+        self.work_folder = "thermal_dynamic_test_files/LSPGROM/"
+        parameters_filename = "../ProjectParameters.json"
+        expected_output_filename = "ExpectedOutputLSPGROM.npy"
 
-    #     time_snapshots = [500,1200,2500,3000,3600]
+        time_snapshots = [500,1200,2500,3000,3600]
 
-    #     with KratosUnittest.WorkFolderScope(self.work_folder, __file__):
-    #         # Set up and run simulation
-    #         with open(parameters_filename,'r') as parameter_file:
-    #             parameters = KratosMultiphysics.Parameters(parameter_file.read())
-    #         model = KratosMultiphysics.Model()
-    #         self.simulation = rom_testing_utilities.SetUpSimulationInstance(model, parameters)
+        with KratosUnittest.WorkFolderScope(self.work_folder, __file__):
+            # Set up and run simulation
+            with open(parameters_filename,'r') as parameter_file:
+                parameters = KratosMultiphysics.Parameters(parameter_file.read())
+            model = KratosMultiphysics.Model()
+            self.simulation = rom_testing_utilities.SetUpSimulationInstance(model, parameters)
 
-    #         # Patch the RomAnalysis class to save the selected time steps results
-    #         def Initialize(cls):
-    #             super(type(self.simulation), cls).Initialize()
-    #             cls.selected_time_step_solution_container = []
+            # Patch the RomAnalysis class to save the selected time steps results
+            def Initialize(cls):
+                super(type(self.simulation), cls).Initialize()
+                cls.selected_time_step_solution_container = []
 
-    #         def FinalizeSolutionStep(cls):
-    #             super(type(self.simulation), cls).FinalizeSolutionStep()
+            def FinalizeSolutionStep(cls):
+                super(type(self.simulation), cls).FinalizeSolutionStep()
 
-    #             array_of_temperatures = []
-    #             if int(cls._GetSolver().GetComputingModelPart().ProcessInfo[KratosMultiphysics.TIME]) in time_snapshots:
-    #                 for node in cls._solver.GetComputingModelPart().Nodes:
-    #                     array_of_temperatures.append(node.GetSolutionStepValue(KratosMultiphysics.TEMPERATURE))
-    #                 cls.selected_time_step_solution_container.append(array_of_temperatures)
+                array_of_temperatures = []
+                if int(cls._GetSolver().GetComputingModelPart().ProcessInfo[KratosMultiphysics.TIME]) in time_snapshots:
+                    for node in cls._solver.GetComputingModelPart().Nodes:
+                        array_of_temperatures.append(node.GetSolutionStepValue(KratosMultiphysics.TEMPERATURE))
+                    cls.selected_time_step_solution_container.append(array_of_temperatures)
 
-    #         self.simulation.Initialize  = types.MethodType(Initialize, self.simulation)
-    #         self.simulation.FinalizeSolutionStep  = types.MethodType(FinalizeSolutionStep, self.simulation)
+            self.simulation.Initialize  = types.MethodType(Initialize, self.simulation)
+            self.simulation.FinalizeSolutionStep  = types.MethodType(FinalizeSolutionStep, self.simulation)
 
-    #         # Run test case
-    #         self.simulation.Run()
+            # Run test case
+            self.simulation.Run()
 
-    #         # Check results
-    #         expected_output = np.load(expected_output_filename)
-    #         n_nodes = len(self.simulation.selected_time_step_solution_container[0])
-    #         n_snapshots = len(self.simulation.selected_time_step_solution_container)
-    #         obtained_snapshot_matrix = np.zeros((n_nodes, n_snapshots))
-    #         for i in range(n_snapshots):
-    #             snapshot_i= np.array(self.simulation.selected_time_step_solution_container[i])
-    #             obtained_snapshot_matrix[:,i] = snapshot_i.transpose()
-    #         nodal_area = rom_testing_utilities.GetNodalAreaVector(self.simulation._GetSolver().GetComputingModelPart())
-
-    #         for i in range (n_snapshots):
-    #             l2 = np.sqrt((sum(nodal_area*((1 - obtained_snapshot_matrix[:,i]/expected_output[:,i] )**2)))/(sum(nodal_area)))*100
-    #             self.assertLess(l2, self.relative_tolerance)
+            # Check results
+            expected_output = np.load(expected_output_filename)
+            n_nodes = len(self.simulation.selected_time_step_solution_container[0])
+            n_snapshots = len(self.simulation.selected_time_step_solution_container)
+            obtained_snapshot_matrix = np.zeros((n_nodes, n_snapshots))
+            for i in range(n_snapshots):
+                snapshot_i= np.array(self.simulation.selected_time_step_solution_container[i])
+                obtained_snapshot_matrix[:,i] = snapshot_i.transpose()
+            nodal_area = rom_testing_utilities.GetNodalAreaVector(self.simulation._GetSolver().GetComputingModelPart())
+            
+            for i in range (n_snapshots):
+                l2 = np.sqrt((sum(nodal_area*((1 - obtained_snapshot_matrix[:,i]/expected_output[:,i] )**2)))/(sum(nodal_area)))*100
+                self.assertLess(l2, self.relative_tolerance)
 
     def tearDown(self):
         with KratosUnittest.WorkFolderScope(self.work_folder, __file__):
