ac648d8786a91998c572bc8cabc76b005ab9c53d
SADPR <aresdepargas@gmail.com>
2023-09-10 11:58:32 +0200

    Removing KratosWatch.

--- applications/RomApplication/custom_strategies/lspg_rom_builder_and_solver.h
+++ applications/RomApplication/custom_strategies/lspg_rom_builder_and_solver.h
@@ -394,9 +394,6 @@ protected:
         using EigenDynamicVector = Eigen::Matrix<double, Eigen::Dynamic, 1>;
         Eigen::Map<EigenDynamicVector> dxrom_eigen(dxrom.data().begin(), dxrom.size());
         dxrom_eigen = rEigenRomA.colPivHouseholderQr().solve(rEigenRomB);
-        KRATOS_WATCH(rEigenRomA)
-        KRATOS_WATCH(rEigenRomB)
-        KRATOS_WATCH(dxrom_eigen)
         KRATOS_INFO_IF("LeastSquaresPetrovGalerkinROMBuilderAndSolver", (BaseType::GetEchoLevel() > 0)) << "Solve reduced system time: " << solving_timer.ElapsedSeconds() << std::endl;
 
         // Save the ROM solution increment in the root modelpart database
@@ -406,7 +403,6 @@ protected:
         // project reduced solution back to full order model
         const auto backward_projection_timer = BuiltinTimer();
         BaseType::ProjectToFineBasis(dxrom, rModelPart, rDx);
-        KRATOS_WATCH(rDx)
         KRATOS_INFO_IF("LeastSquaresPetrovGalerkinROMBuilderAndSolver", (BaseType::GetEchoLevel() > 0)) << "Project to fine basis time: " << backward_projection_timer.ElapsedSeconds() << std::endl;
 
         KRATOS_CATCH("")
