5a053cea85f34087f3d732ec1a4c4d0232af58b9
Carlos Roig <roigcarlo@gmail.com>
2023-09-12 11:35:49 +0200

    Moving test utility files outside cpp test folder

--- kratos/tests/test_utilities/test_laplacian_element.cpp
+++ kratos/tests/test_utilities/test_laplacian_element.cpp
@@ -20,7 +20,7 @@
 #include "includes/variables.h"
 #include "utilities/math_utils.h"
 #include "utilities/geometry_utilities.h"
-#include "tests/cpp_tests/auxiliar_files_for_cpp_unnitest/test_laplacian_element.h"
+#include "tests/test_utilities/test_laplacian_element.h"
 
 namespace Kratos::Testing
 {
