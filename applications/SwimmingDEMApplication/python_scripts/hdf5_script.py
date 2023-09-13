9fc24615293c45d71e524e863e7b7153f32ccaa2
jgonzalezusua <jgonzalez@cimne.upc.edu>
2023-09-13 14:54:43 +0200

    including new post processing

--- applications/SwimmingDEMApplication/python_scripts/hdf5_script.py
+++ applications/SwimmingDEMApplication/python_scripts/hdf5_script.py
@@ -5,6 +5,7 @@ import numpy as np
 import h5py
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.DEMApplication as DEM
+import KratosMultiphysics.SwimmingDEMApplication as SDEM
 import KratosMultiphysics.FluidDynamicsApplication as Fluid
 
 class ErrorProjectionPostProcessTool(object):
@@ -158,3 +159,163 @@ class ParticleDragForcePostProcessTool(object):
         data_array = np.rec.fromarrays(data, dtype = column_name)
 
         self.sub_group.create_dataset(name = self.group_name, data = data_array)
+
+class ForceBalance(object):
+    def __init__(self):
+        """The default constructor of the class.
+
+        Keyword arguments:
+        self -- It signifies an instance of a class.
+        """
+        self.parameters = Kratos.Parameters( """
+        {
+            "file_name": "sp_data",
+            "target_porosity" : 0.3,
+            "probe_height" : 0.2032
+        }  """ )
+
+
+        self.problem_path = os.getcwd()
+        self.dtype = np.float64
+        self.hydrodynamic_force_x = []
+        self.hydrodynamic_force_y = []
+        self.hydrodynamic_force_z = []
+        self.hydrodynamic_reaction_x = []
+        self.hydrodynamic_reaction_y = []
+        self.hydrodynamic_reaction_z = []
+        self.hydrodynamic_reaction_x = []
+        self.hydrodynamic_reaction_y = []
+        self.hydrodynamic_reaction_z = []
+        self.projected_hydrodynamic_reaction_x = []
+        self.projected_hydrodynamic_reaction_y = []
+        self.projected_hydrodynamic_reaction_z = []
+        self.error = []
+        self.time = []
+        self.velocity = []
+
+    def WriteData(self, spheres_model_part, fluid_model_part, time):
+
+        self.delta_time = round(fluid_model_part.ProcessInfo.GetValue(Kratos.DELTA_TIME),3)
+
+        element_size = [Element.GetGeometry().Length() for Element in fluid_model_part.Elements]
+        self.element_size = np.max(element_size)
+        self.group_name = str(self.element_size)
+
+        self.file_path = os.path.join(str(self.problem_path),self.parameters["file_name"].GetString() + "_" + str(self.delta_time) + ".hdf5")
+
+        hydrodynamic_force_x = hydrodynamic_force_y = hydrodynamic_force_z = hydrodynamic_reaction_x = hydrodynamic_reaction_y = hydrodynamic_reaction_z = 0.0
+        projected_hydrodynamic_reaction_x = projected_hydrodynamic_reaction_y = projected_hydrodynamic_reaction_z = velocity = 0.0
+
+        for node in spheres_model_part.Nodes:
+            hydrodynamic_force_x += node.GetSolutionStepValue(Kratos.HYDRODYNAMIC_FORCE_X)
+            hydrodynamic_force_y += node.GetSolutionStepValue(Kratos.HYDRODYNAMIC_FORCE_Y)
+            hydrodynamic_force_z += node.GetSolutionStepValue(Kratos.HYDRODYNAMIC_FORCE_Z)
+            projected_hydrodynamic_reaction_x += node.GetSolutionStepValue(SDEM.HYDRODYNAMIC_REACTION_PROJECTED_X)
+            projected_hydrodynamic_reaction_y += node.GetSolutionStepValue(SDEM.HYDRODYNAMIC_REACTION_PROJECTED_Y)
+            projected_hydrodynamic_reaction_z += node.GetSolutionStepValue(SDEM.HYDRODYNAMIC_REACTION_PROJECTED_Z)
+            velocity += node.GetSolutionStepValue(Kratos.VELOCITY_Z)
+
+        for node in fluid_model_part.Nodes:
+            hydrodynamic_reaction_x += node.GetSolutionStepValue(Kratos.HYDRODYNAMIC_REACTION_X)
+            hydrodynamic_reaction_y += node.GetSolutionStepValue(Kratos.HYDRODYNAMIC_REACTION_Y)
+            hydrodynamic_reaction_z += node.GetSolutionStepValue(Kratos.HYDRODYNAMIC_REACTION_Z)
+
+        error = np.abs(np.sqrt(hydrodynamic_force_x**2 + hydrodynamic_force_y**2 + hydrodynamic_force_z**2)-np.sqrt(hydrodynamic_reaction_x**2 + hydrodynamic_reaction_y**2 + hydrodynamic_reaction_z**2))/np.sqrt(hydrodynamic_force_x**2 + hydrodynamic_force_y**2 + hydrodynamic_force_z**2)
+
+        self.time.append(time)
+        self.hydrodynamic_force_x.append(hydrodynamic_force_x)
+        self.hydrodynamic_force_y.append(hydrodynamic_force_y)
+        self.hydrodynamic_force_z.append(hydrodynamic_force_z)
+        self.hydrodynamic_reaction_x.append(hydrodynamic_reaction_x)
+        self.hydrodynamic_reaction_y.append(hydrodynamic_reaction_y)
+        self.hydrodynamic_reaction_z.append(hydrodynamic_reaction_z)
+        self.projected_hydrodynamic_reaction_x.append(projected_hydrodynamic_reaction_x)
+        self.projected_hydrodynamic_reaction_y.append(projected_hydrodynamic_reaction_y)
+        self.projected_hydrodynamic_reaction_z.append(projected_hydrodynamic_reaction_z)
+        self.error.append(error)
+        self.velocity.append(velocity)
+
+        with h5py.File(self.file_path, 'a') as f:
+                self.WriteDataToFile(file_or_group = f,
+                            names = ['HYDRODYNAMIC_FORCE_X','HYDRODYNAMIC_FORCE_Y','HYDRODYNAMIC_FORCE_Z','HYDRODYNAMIC_REACTION_X','HYDRODYNAMIC_REACTION_Y', 'HYDRODYNAMIC_REACTION_Z', 'HYDRODYNAMIC_REACTION_PROJECTED_X', 'HYDRODYNAMIC_REACTION_PROJECTED_Y', 'HYDRODYNAMIC_REACTION_PROJECTED_Z', 'ERROR', 'VELOCITY', 'TIME'],
+                            data = [self.hydrodynamic_force_x ,self.hydrodynamic_force_y, self.hydrodynamic_force_z, self.hydrodynamic_reaction_x, self.hydrodynamic_reaction_y, self.hydrodynamic_reaction_z, self.projected_hydrodynamic_reaction_x, self.projected_hydrodynamic_reaction_y, self.projected_hydrodynamic_reaction_z, self.error, self.velocity, self.time])
+
+    def WriteDataToFile(self, file_or_group, names, data):
+        if self.group_name in file_or_group:
+            file_or_group['/'].__delitem__(self.group_name)
+            self.sub_group = file_or_group.create_group(self.group_name)
+        else:
+            self.sub_group = file_or_group.create_group(self.group_name)
+
+        self.sub_group.attrs['element_size'] = str(self.element_size)
+        self.sub_group.attrs['delta_time'] = str(self.delta_time)
+
+        for name, datum in zip(names, data):
+            if name in file_or_group:
+                file_or_group.__delitem__(name)
+        for name, datum in zip(names, data):
+            self.sub_group.create_dataset(name = name, data = datum)
+
+class Pressure(object):
+    def __init__(self,test_number):
+        """The default constructor of the class.
+
+        Keyword arguments:
+        self -- It signifies an instance of a class.
+        """
+        self.parameters = Kratos.Parameters( """
+        {
+            "file_name": "sp_data.hdf5",
+            "target_porosity" : 0.3,
+            "probe_height" : 0.2032
+        }  """ )
+
+
+        self.problem_path = os.getcwd()
+        self.dtype = np.float64
+        self.pressure = []
+        self.velocity_x = []
+        self.velocity_y = []
+        self.time = []
+        self.file_path = os.path.join(str(self.problem_path),self.parameters["file_name"].GetString())
+        self.group_name = str(test_number)
+
+    def WriteData(self, fluid_model_part, model_type, alpha_min, reynolds_number):
+
+        self.model_type = model_type
+        self.alpha_min = alpha_min
+        self.reynolds_number = reynolds_number
+        time = fluid_model_part.ProcessInfo[Kratos.TIME]
+
+        for node in fluid_model_part.Nodes:
+            if node.X == 6.15 and node.Y == 4.0:
+                pressure = node.GetSolutionStepValue(Kratos.PRESSURE)
+                vel_x = node.GetSolutionStepValue(Kratos.VELOCITY_X)
+                vel_y = node.GetSolutionStepValue(Kratos.VELOCITY_Y)
+                break
+
+        self.pressure.append(pressure)
+        self.velocity_x.append(vel_x)
+        self.velocity_y.append(vel_y)
+        self.time.append(time)
+
+        with h5py.File(self.file_path, 'a') as f:
+                self.WriteDataToFile(file_or_group = f,
+                            names = ['PRESSURE', 'VELOCITY_X', 'VELOCITY_Y', 'TIME'],
+                            data = [self.pressure, self.velocity_x, self.velocity_y, self.time])
+    def WriteDataToFile(self, file_or_group, names, data):
+        if self.group_name in file_or_group:
+            file_or_group['/'].__delitem__(self.group_name)
+            self.sub_group = file_or_group.create_group(self.group_name)
+        else:
+            self.sub_group = file_or_group.create_group(self.group_name)
+
+        self.sub_group.attrs['model_type'] = str(self.model_type)
+        self.sub_group.attrs['alpha_min'] = str(self.alpha_min)
+        self.sub_group.attrs['Reynolds_number'] = str(self.reynolds_number)
+
+        for name, datum in zip(names, data):
+            if name in file_or_group:
+                file_or_group.__delitem__(name)
+        for name, datum in zip(names, data):
+            self.sub_group.create_dataset(name = name, data = datum)
