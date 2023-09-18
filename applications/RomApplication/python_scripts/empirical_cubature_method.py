49c7fcfa00afd9395ca7bc3d1c96068de7b51a33
Raul Bravo <rbravo555@gmail.com>
2023-09-18 10:43:11 +0200

    Placing docstrings in the correct place

--- applications/RomApplication/python_scripts/empirical_cubature_method.py
+++ applications/RomApplication/python_scripts/empirical_cubature_method.py
@@ -14,29 +14,34 @@ class EmpiricalCubatureMethod():
     Reference: Hernandez 2020. "A multiscale method for periodic structures using domain decomposition and ECM-hyperreduction"
     """
 
-
-    """
-    Constructor setting up the parameters for the Element Selection Strategy
-        ECM_tolerance: approximation tolerance for the element selection algorithm
-        Filter_tolerance: parameter limiting the number of candidate points (elements) to those above this tolerance
-        Plotting: whether to plot the error evolution of the element selection algorithm
-    """
-    def __init__(self, ECM_tolerance = 0, Filter_tolerance = 1e-16, Plotting = False):
+    def __init__(
+        self,
+        ECM_tolerance = 0,
+        Filter_tolerance = 1e-16,
+        Plotting = False):
+        """
+        Constructor setting up the parameters for the Element Selection Strategy
+            ECM_tolerance: approximation tolerance for the element selection algorithm
+            Filter_tolerance: parameter limiting the number of candidate points (elements) to those above this tolerance
+            Plotting: whether to plot the error evolution of the element selection algorithm
+        """
         self.ECM_tolerance = ECM_tolerance
         self.Filter_tolerance = Filter_tolerance
         self.Name = "EmpiricalCubature"
         self.Plotting = Plotting
 
-
-
-    """
-    Method for setting up the element selection
-    input:  - ResidualsBasis: numpy array containing a basis to the residuals projected
-            - constrain_sum_of_weights: enable the user to constrain weights to be the sum of the number of entities.
-            - constrain_conditions: enable the user to enforce weights to consider conditions (for specific boundary conditions).
-    """
-    def SetUp(self, ResidualsBasis, constrain_sum_of_weights=True, constrain_conditions = False, number_of_conditions = 0):
-
+    def SetUp(
+        self,
+        ResidualsBasis,
+        constrain_sum_of_weights=True,
+        constrain_conditions = False,
+        number_of_conditions = 0):
+        """
+        Method for setting up the element selection
+        input:  - ResidualsBasis: numpy array containing a basis to the residuals projected
+                - constrain_sum_of_weights: enable the user to constrain weights to be the sum of the number of entities.
+                - constrain_conditions: enable the user to enforce weights to consider conditions (for specific boundary conditions).
+        """
         self.W = np.ones(np.shape(ResidualsBasis)[0])
         self.G = ResidualsBasis.T
         self.add_constrain_count = None
@@ -67,11 +72,10 @@ class EmpiricalCubatureMethod():
             self.add_constrain_count = -2
         self.b = self.G @ self.W
 
-
-    """
-    Method performing calculations required before launching the Calculate method
-    """
     def Initialize(self):
+        """
+        Method performing calculations required before launching the Calculate method
+        """
         self.Gnorm = np.sqrt(sum(np.multiply(self.G, self.G), 0))
         M = np.shape(self.G)[1]
         normB = np.linalg.norm(self.b)
@@ -88,17 +92,14 @@ class EmpiricalCubatureMethod():
         self.nerror = np.linalg.norm(self.r)/normB
         self.nerrorACTUAL = self.nerror
 
-
     def Run(self):
         self.Initialize()
         self.Calculate()
 
-
-    """
-    Method launching the element selection algorithm to find a set of elements: self.z, and wiegths: self.w
-    """
     def Calculate(self):
-
+        """
+        Method launching the element selection algorithm to find a set of elements: self.z, and wiegths: self.w
+        """
         k = 1 # number of iterations
         while self.nerrorACTUAL > self.ECM_tolerance and self.mPOS < self.m and len(self.y) != 0:
 
@@ -163,12 +164,10 @@ class EmpiricalCubatureMethod():
             plt.ylabel('Error %')
             plt.show()
 
-
-
-    """
-    Method for the quick update of weights (self.w), whenever a negative weight is found
-    """
     def _UpdateWeightsInverse(self, A,Aast,a,xold):
+        """
+        Method for the quick update of weights (self.w), whenever a negative weight is found
+        """
         c = np.dot(A.T, a)
         d = np.dot(Aast, c).reshape(-1, 1)
         s = np.dot(a.T, a) - np.dot(c.T, d)
@@ -182,24 +181,20 @@ class EmpiricalCubatureMethod():
         x = np.vstack([(xold - d * v), v])
         return Bast, x
 
-
-
-    """
-    Method for the quick update of weights (self.w), whenever a negative weight is found
-    """
     def _MultiUpdateInverseHermitian(self, invH, neg_indexes):
+        """
+        Method for the quick update of weights (self.w), whenever a negative weight is found
+        """
         neg_indexes = np.sort(neg_indexes)
         for i in range(np.size(neg_indexes)):
             neg_index = neg_indexes[i] - i
             invH = self._UpdateInverseHermitian(invH, neg_index)
         return invH
 
-
-
-    """
-    Method for the quick update of weights (self.w), whenever a negative weight is found
-    """
     def _UpdateInverseHermitian(self, invH, neg_index):
+        """
+        Method for the quick update of weights (self.w), whenever a negative weight is found
+        """
         if neg_index == np.shape(invH)[1]:
             aux = (invH[0:-1, -1] * invH[-1, 0:-1]) / invH(-1, -1)
             invH_new = invH[:-1, :-1] - aux
