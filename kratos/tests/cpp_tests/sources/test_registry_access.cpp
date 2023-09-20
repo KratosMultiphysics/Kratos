e6e8b18fa74c8e3ab5bdb1e07f23a83548b8aca1
Carlos Roig <roigcarlo@users.noreply.github.com>
2023-09-20 16:24:41 +0200

    Update kratos/tests/cpp_tests/sources/test_registry_access.cpp
    Co-authored-by: Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>

--- kratos/tests/cpp_tests/sources/test_registry_access.cpp
+++ kratos/tests/cpp_tests/sources/test_registry_access.cpp
@@ -7,7 +7,7 @@
 //  License:         BSD License
 //                   Kratos default license: kratos/license.txt
 //
-//  Main authors:    Pooyan Dadvand
+//  Main authors:    Carlos Roig
 //
 //
 
