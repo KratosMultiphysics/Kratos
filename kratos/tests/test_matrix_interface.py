a7d6b7103c5c5b284567b6ee054ba36b090df6d4
matekelemen <kelemen.mate.12@gmail.com>
2023-09-28 10:51:11 +0200

    add test for stepped slice

--- kratos/tests/test_matrix_interface.py
+++ kratos/tests/test_matrix_interface.py
@@ -182,6 +182,17 @@ class TestMatrixInterface(KratosUnittest.TestCase):
                                       [13.0, 14.0]])
         self.assertMatrixAlmostEqual(kratos_matrix, reference_matrix)
 
+        # Test slicing with steps
+        # + ----- +
+        # |  1  3 |
+        # | 13 15 |
+        # + ----- +
+        sliced_numpy_matrix = numpy_matrix[0::3, 1::2]
+        kratos_matrix = KM.Matrix(sliced_numpy_matrix)
+        reference_matrix = KM.Matrix([[ 1.0,  3.0],
+                                      [13.0, 15.0]])
+        self.assertMatrixAlmostEqual(kratos_matrix, reference_matrix)
+
         # Test empty slice
         # ++
         # ++
