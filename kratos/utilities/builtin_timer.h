3ef4a254ab07cd6315e0a3cee8d1c2af53005ce2
Philipp <philipp.bucher@tum.de>
2023-09-30 16:49:45 +0200

    nicely format time

--- kratos/utilities/builtin_timer.h
+++ kratos/utilities/builtin_timer.h
@@ -60,8 +60,24 @@ private:
 /// output stream function
 inline std::ostream& operator << (std::ostream& rOStream, const BuiltinTimer& rThis)
 {
-    rOStream << rThis.ElapsedSeconds() << " [s]";
+    const double elapsed_secs = rThis.ElapsedSeconds();
+
+    int minutes = elapsed_secs / 60;
+    int hours = minutes / 60;
+    minutes = minutes % 60;
+    double seconds = elapsed_secs - hours*3600.0 - minutes*60.0;
+
+    if (hours > 0) {
+        rOStream << hours << " [h] ";
+    }
+    if (minutes > 0) {
+        rOStream << minutes << " [m] ";
+        // remove decimals if over a minute
+        seconds = int(seconds);
+    }
+    rOStream << seconds << " [s]";
     return rOStream;
+
 }
 
 }  // namespace Kratos.
