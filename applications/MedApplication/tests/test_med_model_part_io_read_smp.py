df2e2e9766c2031674317836e7d85b73f6f5f284
Philipp <philipp.bucher@tum.de>
2023-10-01 00:30:20 +0200

    better error in recursive fcts

--- applications/MedApplication/tests/test_med_model_part_io_read_smp.py
+++ applications/MedApplication/tests/test_med_model_part_io_read_smp.py
@@ -188,7 +188,7 @@ class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
     def _check_unique_nodes(self, model_part):
         node_ids: List[int] = get_node_ids(model_part)
         node_ids_unique: Set[int] = set(node_ids)
-        self.assertEqual(len(node_ids), len(node_ids_unique))
+        self.assertEqual(len(node_ids), len(node_ids_unique), msg=f"Name of ModelPart: {model_part.FullName()}")
 
         for smp in model_part.SubModelParts:
             self._check_unique_nodes(smp)
@@ -196,7 +196,7 @@ class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
     def _check_unique_geometries(self, model_part):
         geom_ids: List[int] = get_geometry_ids(model_part)
         geom_ids_unique: Set[int] = set(geom_ids)
-        self.assertEqual(len(geom_ids), len(geom_ids_unique))
+        self.assertEqual(len(geom_ids), len(geom_ids_unique), msg=f"Name of ModelPart: {model_part.FullName()}")
 
         for smp in model_part.SubModelParts:
             self._check_unique_geometries(smp)
@@ -208,7 +208,7 @@ class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
         node_ids: List[int] = get_node_ids(model_part)
 
         # there can be nodes that do not belong to geometries, thus geom_node_ids is subset
-        self.assertTrue(geom_node_ids_unique.issubset(node_ids))
+        self.assertTrue(geom_node_ids_unique.issubset(node_ids), msg=f"Name of ModelPart: {model_part.FullName()}")
 
         for smp in model_part.SubModelParts:
             self._check_nodes_geometries(smp)
