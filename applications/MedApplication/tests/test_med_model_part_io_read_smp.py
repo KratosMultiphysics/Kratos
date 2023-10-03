f2b002baa737104cb7f1824d2bc30bb26abc80f1
Philipp <philipp.bucher@tum.de>
2023-10-03 23:56:00 +0200

    updated subsub test

--- applications/MedApplication/tests/test_med_model_part_io_read_smp.py
+++ applications/MedApplication/tests/test_med_model_part_io_read_smp.py
@@ -186,25 +186,47 @@ class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
         self.assertTrue(model_part.HasSubModelPart("Face_1"))
         self.assertTrue(model_part.HasSubModelPart("Face_2"))
         self.assertTrue(model_part.HasSubModelPart("Edge_1"))
+        self.assertTrue(model_part.HasSubModelPart("boundary"))
 
         smp_face_1: KM.ModelPart = model_part.GetSubModelPart("Face_1")
         smp_face_2: KM.ModelPart = model_part.GetSubModelPart("Face_2")
         smp_edge_1: KM.ModelPart = model_part.GetSubModelPart("Edge_1")
+        smp_boundary: KM.ModelPart = model_part.GetSubModelPart("boundary")
+
+        self.assertTrue(smp_boundary.HasSubModelPart("top"))
+        self.assertTrue(smp_boundary.HasSubModelPart("bottom"))
+        ssmp_boundary_top: KM.ModelPart = smp_boundary.GetSubModelPart("top")
+        ssmp_boundary_bottom: KM.ModelPart = smp_boundary.GetSubModelPart("bottom")
 
         self.assertEqual(smp_face_1.NumberOfNodes(), 15)
         self.assertEqual(smp_face_2.NumberOfNodes(), 15)
         self.assertEqual(smp_edge_1.NumberOfNodes(), 4)
+        self.assertEqual(smp_boundary.NumberOfNodes(), 30)
+        self.assertEqual(ssmp_boundary_top.NumberOfNodes(), 15)
+        self.assertEqual(ssmp_boundary_bottom.NumberOfNodes(), 15)
 
         self.assertEqual(smp_face_1.NumberOfGeometries(), 16)
         self.assertEqual(smp_face_2.NumberOfGeometries(), 16)
         self.assertEqual(smp_edge_1.NumberOfGeometries(), 3)
+        self.assertEqual(smp_boundary.NumberOfGeometries(), 32)
+        self.assertEqual(ssmp_boundary_top.NumberOfGeometries(), 16)
+        self.assertEqual(ssmp_boundary_bottom.NumberOfGeometries(), 16)
 
         smp_face_1_node_ids: Set[int] = set(get_node_ids(smp_face_1))
         smp_face_2_node_ids: Set[int] = set(get_node_ids(smp_face_2))
         smp_edge_1_node_ids: Set[int] = set(get_node_ids(smp_edge_1))
+        smp_boundary_node_ids: Set[int] = set(get_node_ids(smp_boundary))
+        ssmp_boundary_top_node_ids: Set[int] = set(get_node_ids(ssmp_boundary_top))
+        ssmp_boundary_bottom_node_ids: Set[int] = set(get_node_ids(ssmp_boundary_bottom))
 
         # the common edge is the "Edge_1" SubModelPart
         self.assertEqual(smp_face_1_node_ids.intersection(smp_face_2_node_ids), smp_edge_1_node_ids)
+
+        # top and bottom have the same nodes as boundary (since they are its SubSubModelParts)
+        self.assertEqual(smp_boundary_node_ids, ssmp_boundary_top_node_ids.union(ssmp_boundary_bottom_node_ids))
+
+        # top and bottom dont share nodes
+        self.assertEqual(len(ssmp_boundary_top_node_ids.intersection(ssmp_boundary_bottom_node_ids)), 0)
 
         # check node coordinates
         for node in model_part.Nodes:
@@ -247,8 +269,33 @@ class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
             self.assertTrue(0.0 <= node.Z <= 200.0)
             self.assertTrue(0.0 <= node.Z0 <= 200.0)
 
+        for node in ssmp_boundary_top.Nodes:
+            self.assertTrue(0.0 <= node.X <= 200.0)
+            self.assertTrue(0.0 <= node.X0 <= 200.0)
+
+            self.assertTrue(0.0 <= node.Y <= 200.0)
+            self.assertTrue(0.0 <= node.Y0 <= 200.0)
+
+            self.assertAlmostEqual(node.Z, 200.0)
+            self.assertAlmostEqual(node.Z0, 200.0)
+
+        for node in ssmp_boundary_bottom.Nodes:
+            self.assertTrue(0.0 <= node.X <= 200.0)
+            self.assertTrue(0.0 <= node.X0 <= 200.0)
+
+            self.assertTrue(0.0 <= node.Y <= 200.0)
+            self.assertTrue(0.0 <= node.Y0 <= 200.0)
+
+            self.assertAlmostEqual(node.Z, 0.0)
+            self.assertAlmostEqual(node.Z0, 0.0)
+
         # check that the correct geoms (3D triangles) are in the smp
-        for geom in chain(smp_face_1.Geometries, smp_face_2.Geometries):
+        for geom in chain(
+            smp_face_1.Geometries,
+            smp_face_2.Geometries,
+            smp_boundary.Geometries,
+            ssmp_boundary_top.Geometries,
+            ssmp_boundary_bottom.Geometries):
             self.assertIsInstance(geom, KM.Triangle3D3)
 
         # check that the correct geoms (3D lines) are in the smp
