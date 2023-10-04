1135a490503974a4b2c9fad641a6416fe177555a
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2023-10-04 16:36:10 +0200

    Minimal class for prescribed time increments

---
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_prescribed_time_incrementor.cpp
@@ -0,0 +1,33 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+//  Main authors:    Wijtze Pieter Kikstra
+//                   Anne van de Graaf
+//
+
+#include "testing/testing.h"
+#include "custom_workflows/prescribed_time_incrementor.h"
+#include "custom_workflows/time_step_end_state.hpp"
+
+using namespace Kratos;
+
+
+namespace Kratos::Testing
+{
+
+KRATOS_TEST_CASE_IN_SUITE(NoNextTimeStepWhenPrescribedTimeIncrementorHasEmptyList, KratosGeoMechanicsFastSuite)
+{
+    std::vector<double> increments;
+    PrescribedTimeIncrementor incrementor{increments};
+    TimeStepEndState previous_state;
+
+    KRATOS_EXPECT_FALSE(incrementor.WantNextStep(previous_state));
+}
+
+}
