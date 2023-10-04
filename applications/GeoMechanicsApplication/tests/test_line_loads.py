f9e9270867c97f94c5eec338cadcfc0ff9f5f26c
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2023-10-04 15:00:46 +0200

    Renamed another local variable

--- applications/GeoMechanicsApplication/tests/test_line_loads.py
+++ applications/GeoMechanicsApplication/tests/test_line_loads.py
@@ -79,7 +79,7 @@ class KratosGeoMechanicsLineLoadTests(KratosUnittest.TestCase):
         simulation = test_helper.run_kratos(file_path)
 
         bottom_node_ids = [1, 2, 6, 11, 17, 25, 34, 46, 59, 75, 90]
-        reactions_y = [value[1] for value in test_helper.get_nodal_variable(simulation, Kratos.REACTION, bottom_node_ids)]
+        reactions_y = [reaction[1] for reaction in test_helper.get_nodal_variable(simulation, Kratos.REACTION, bottom_node_ids)]
 
         self.assertAlmostEqual(20.0, sum(reactions_y), 4)
 
