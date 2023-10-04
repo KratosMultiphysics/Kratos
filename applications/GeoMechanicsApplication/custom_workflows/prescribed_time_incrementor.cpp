f877adc982f32e6b5b1044fca176c3b694ab63fe
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2023-10-04 16:36:10 +0200

    Minimal class for prescribed time increments

---
+++ applications/GeoMechanicsApplication/custom_workflows/prescribed_time_incrementor.cpp
@@ -0,0 +1,43 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+//  Main authors:    Wijtze Pieter Kikstra
+//                   Anne van de Graaf
+//
+
+#include "prescribed_time_incrementor.h"
+
+
+namespace Kratos
+{
+
+PrescribedTimeIncrementor::PrescribedTimeIncrementor(const std::vector<double>& increments)
+{}
+
+bool PrescribedTimeIncrementor::WantNextStep(const Kratos::TimeStepEndState& rPreviousState) const
+{
+    return false;
+}
+
+bool PrescribedTimeIncrementor::WantRetryStep(std::size_t             CycleNumber,
+                                              const TimeStepEndState& rPreviousState) const
+{
+    return false;
+}
+
+double PrescribedTimeIncrementor::GetIncrement() const
+{
+    return 0.0;
+}
+
+void PrescribedTimeIncrementor::PostTimeStepExecution(const Kratos::TimeStepEndState& rResultantState)
+{
+}
+
+}
