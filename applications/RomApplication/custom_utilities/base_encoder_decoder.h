12c65c7f99f1b23a691cd677785015133aeb51ab
Raul Bravo <rbravo555@gmail.com>
2023-10-05 10:04:07 +0200

    adding stuff from erased class in single file with base class

--- applications/RomApplication/custom_utilities/base_encoder_decoder.h
+++ applications/RomApplication/custom_utilities/base_encoder_decoder.h
@@ -16,22 +16,49 @@
 
 namespace Kratos{
 
+
+    // class RomBasis
+    // {
+    //     public:
+
+    //     KRATOS_CLASS_POINTER_DEFINITION(RomBasis);
+
+    //     ~RomBasis()= default;
+
+    //     void SetNodalBasis(int node_id, Matrix &nodal_basis){
+    //         //mMapPhi[node_id] = the_basis;
+    //         std::shared_ptr<Matrix> pnodal_basis{new Matrix{nodal_basis}};
+    //         mMapPhi[node_id] = pnodal_basis;
+    //         }
+
+    //     std::shared_ptr<Matrix> GetNodalBasis(int node_id){
+    //         return mMapPhi[node_id];
+    //     }
+
+    //     protected:
+    //     //std::unordered_map<int, Matrix> mMapPhi;
+    //     std::unordered_map<int, std::shared_ptr<Matrix>> mMapPhi;
+
+    // };
+
 class BaseEncoderDecoder{
     public:
 
         KRATOS_CLASS_POINTER_DEFINITION(BaseEncoderDecoder);
 
-        BaseEncoderDecoder(Parameters ThisParameters)
-        {
-        // Validate default parameters
-        Parameters default_parameters = Parameters(R"(
-        {
-            "some_default_parameter" : "to_define_later"
-        })" );
+        BaseEncoderDecoder(){}
 
-        ThisParameters.ValidateAndAssignDefaults(default_parameters);
+        // BaseEncoderDecoder(Parameters ThisParameters)
+        // {
+        // // Validate default parameters
+        // Parameters default_parameters = Parameters(R"(
+        // {
+        //     "some_default_parameter" : "to_define_later"
+        // })" );
 
-        }
+        // ThisParameters.ValidateAndAssignDefaults(default_parameters);
+
+        // }
 
         ~BaseEncoderDecoder()= default;
 
@@ -44,11 +71,65 @@ class BaseEncoderDecoder{
         virtual void GetEncoderDerivative(){
         }
 
-        virtual void GetDecoderDerivative(){
+        virtual std::shared_ptr<Matrix> GetDecoderDerivative(int node_id){
+        }
+    };
+
+
+class GlobalLinearEncoderDecoder: public BaseEncoderDecoder{
+    public:
+
+        KRATOS_CLASS_POINTER_DEFINITION(GlobalLinearEncoderDecoder);
+
+        GlobalLinearEncoderDecoder(){}
+
+        // GlobalLinearEncoderDecoder(Parameters ThisParameters): BaseEncoderDecoder(ThisParameters)
+        // {
+        // // Validate default parameters
+        // Parameters default_parameters = Parameters(R"(
+        // {
+        //     "some_default_parameter" : "to_define_later"
+        // })" );
+
+        // ThisParameters.ValidateAndAssignDefaults(default_parameters);
+
+        // }
+
+        ~GlobalLinearEncoderDecoder()= default;
+
+        virtual void Encode() {
         }
 
+        virtual void Decode() {
+        }
 
-        };
+        virtual void GetEncoderDerivative() {
+        }
+
+        virtual std::shared_ptr<Matrix> GetDecoderDerivative(int node_id) {
+            return mMapPhi[node_id];
+        }
+
+        void SetNodalBasis(int node_id, Matrix &nodal_basis){
+            //mMapPhi[node_id] = the_basis;
+            std::shared_ptr<Matrix> pnodal_basis{new Matrix{nodal_basis}};
+            mMapPhi[node_id] = pnodal_basis;
+            }
+
+        // std::shared_ptr<Matrix> GetNodalBasis(int node_id){
+        //     return mMapPhi[node_id];
+        // }
+
+        protected:
+        //std::unordered_map<int, Matrix> mMapPhi;
+        std::unordered_map<int, std::shared_ptr<Matrix>> mMapPhi;
+
+
+    };
+
+
+
+
 }
 
 
