46d7e81f52c448689ce4ae88f94d15f6b79dd302
SADPR <aresdepargas@gmail.com>
2023-10-07 20:17:33 +0200

    Semi-working DEIM and QDEIM

--- applications/RomApplication/python_scripts/discrete_empirical_interpolation_method.py
+++ applications/RomApplication/python_scripts/discrete_empirical_interpolation_method.py
@@ -5,48 +5,119 @@ class QDiscreteEmpiricalInterpolationMethod():
     def __init__(self, DEIM_tolerance=1e-6):
         self.DEIM_tolerance = DEIM_tolerance
         self.P = None  # Interpolation indices
+        self.P_matrix = None  # Matrix constructed from self.P indices
+        self.W = None  # Weight matrix
 
     def SetUp(self, U_k):
+        """Set up the Q-DEIM algorithm with the given matrix U_k."""
         self.U_k = U_k
 
     def Initialize(self):
+        """Initialize the Q-DEIM algorithm by selecting the first index."""
         # Choose the first index by finding the row with the maximum norm
         norms = np.linalg.norm(self.U_k, axis=1)
         index = np.argmax(norms)
         self.P = np.array([index])
 
-    def Calculate(self):
+    def BruntonDEIM(self):
+        """Calculate the DEIM indices using the standard DEIM algorithm."""
         m, n = self.U_k.shape
-        P_matrix = np.zeros((len(self.P), m))
-        P_matrix[np.arange(len(self.P)), self.P] = 1  # Use advanced indexing
-        B = np.linalg.pinv(self.U_k[self.P, :])
-        for _ in range(n - len(self.P)):
-            r = P_matrix @ self.U_k @ B - np.eye(len(self.P))
-            q = np.argmax(np.abs(r))
-            i, j = divmod(q, len(self.P))
-            self.P = np.append(self.P, self.P[i])
+
+        # First DEIM point
+        nmax = np.argmax(np.abs(self.U_k[:, 0]))
+        XI_m = self.U_k[:, 0].reshape(m, 1)
+        z = np.zeros((m, 1))
+        self.P_matrix = np.copy(z)
+        self.P_matrix[nmax, 0] = 1
+        self.P = [nmax]
+
+        # DEIM points 2 to n
+        for jj in range(1, n):
+            c = np.linalg.solve(self.P_matrix.T @ XI_m, self.P_matrix.T @ self.U_k[:, jj].reshape(m, 1))
+            res = self.U_k[:, jj].reshape(m, 1) - XI_m @ c
+            nmax = np.argmax(np.abs(res))
+            XI_m = np.concatenate((XI_m, self.U_k[:, jj].reshape(m, 1)), axis=1)
+            self.P_matrix = np.concatenate((self.P_matrix, z), axis=1)
+            self.P_matrix[nmax, jj] = 1
+            self.P.append(nmax)
+        self.P = np.sort(self.P)  # Ensure self.P is sorted
+        self.__construct_P_matrix()
+        self.compute_W_and_save()
+
+
+    def DEIM(self):
+        """Calculate the DEIM indices using the standard DEIM algorithm."""
+        m, n = self.U_k.shape
+        max_iterations = n - len(self.P)  # Maximum iterations to prevent infinite loop
+        for _ in range(max_iterations):
+            # Construct the P_matrix
             P_matrix = np.zeros((len(self.P), m))
             P_matrix[np.arange(len(self.P)), self.P] = 1  # Use advanced indexing
             B = np.linalg.pinv(self.U_k[self.P, :])
+            r = P_matrix @ self.U_k @ B - np.eye(len(self.P))
+            # Find the index corresponding to the maximum value of the residual r
+            max_index = np.unravel_index(np.argmax(np.abs(r), axis=None), r.shape)
+            self.P = np.append(self.P, max_index[0])
+        self.P = np.sort(self.P)  # Ensure self.P is sorted
+        self.__construct_P_matrix()
+        self.compute_W_and_save()
+
+    def BruntonQDEIM(self):
+        """Calculate the DEIM indices using the Q-DEIM algorithm."""
+        m, n = self.U_k.shape
+
+        # QR factorization with column pivoting
+        Q, R, pivot = scipy_qr(self.U_k, pivoting=True)
+        
+        # Construct the QDEIM matrix P_matrix
+        self.P_matrix = np.zeros((m, n))
+        for jj in range(n):
+            self.P_matrix[pivot[jj], jj] = 1
+
+        # Extract the list of selected indices from the pivot
+        self.P = pivot.tolist()
+        self.P = np.sort(self.P)  # Ensure self.P is sorted
+        self.__construct_P_matrix()
+        self.compute_W_and_save()
 
     def QDEIM(self):
+        """Calculate the DEIM indices using the Q-DEIM algorithm."""
         # QR factorization with column pivoting using SciPy
         Q, R, P = scipy_qr(self.U_k, pivoting=True)
         self.P = P[:self.U_k.shape[1]]
+        self.P = np.sort(self.P)  # Ensure self.P is sorted
+        self.__construct_P_matrix()
+        self.compute_W_and_save()
 
+    def Calculate(self, method="DEIM"):
+        """Calculate the DEIM indices using the specified method."""
+        if method == "DEIM":
+            self.BruntonDEIM()
+        elif method == "QDEIM":
+            self.BruntonQDEIM()
+        else:
+            raise ValueError("Invalid method. Choose either 'DEIM' or 'QDEIM'.")
 
     def Run(self):
-        self.Initialize()
+        """Run the Q-DEIM algorithm."""
+        # self.Initialize()
         self.Calculate()
-        self.QDEIM()
-        return self.P  # Return the selected indices
 
-# Usage
-U_k = np.array([[1, 2], [3, 4], [5, 6]])  # This is an example; replace with your truncated basis
-qdeim = QDiscreteEmpiricalInterpolationMethod(DEIM_tolerance=0.01)
-qdeim.SetUp(U_k)
-selected_indices = qdeim.Run()
-print(f"Selected row indices for Q-DEIM: {selected_indices}")
+    def __construct_P_matrix(self):
+        """Construct the P_matrix from the self.P indices."""
+        m, n = self.U_k.shape
+        self.P_matrix = np.zeros((m, len(self.P)))
+        self.P_matrix[self.P, np.arange(len(self.P))] = 1
+
+    def compute_W_and_save(self):
+        """Compute W matrix and save W and P_matrix."""
+        # Compute W
+        self.W = self.U_k @ np.linalg.pinv(self.P_matrix.T @ self.U_k)
+        
+        # Save W and P_matrix as numpy arrays
+        np.save('W_matrix.npy', self.W)
+        np.save('P_matrix.npy', self.P_matrix)
+        debug = True
 
 
 
