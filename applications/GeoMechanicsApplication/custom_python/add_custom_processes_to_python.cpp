6fe306f476824f7bf2f1a619966a3852b65aabdd
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2023-10-11 11:05:25 +0200

    Renamed a few files and classes as suggested by @carloslubbers
    The header and implementation files that contain the process classes for
    applying a scalar or vector constraint with an optional table have been
    renamed. We now use the singular form ("constraint") rather than the
    plural one ("constraints").
    
    The class names now use singular forms, i.e.
    `ApplyScalarConstraintTableProcess` and
    `ApplyVectorConstraintTableProcess`. All references to the old class
    names have been updated.

--- applications/GeoMechanicsApplication/custom_python/add_custom_processes_to_python.cpp
+++ applications/GeoMechanicsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -41,8 +41,8 @@
 #include "custom_processes/set_absorbing_boundary_parameters_process.hpp"
 #include "custom_processes/set_parameter_field_process.hpp"
 #include "custom_processes/set_multiple_moving_loads.h"
-#include "custom_processes/apply_vector_constraints_table_process.hpp"
-#include "custom_processes/apply_scalar_constraints_table_process.h"
+#include "custom_processes/apply_vector_constraint_table_process.h"
+#include "custom_processes/apply_scalar_constraint_table_process.h"
 
 namespace Kratos {
 namespace Python {
@@ -139,12 +139,12 @@ void  AddCustomProcessesToPython(pybind11::module& m)
         (m, "SetMultipleMovingLoadsProcess")
         .def(py::init < ModelPart&, Parameters>());
 
-    py::class_<ApplyVectorConstraintsTableProcess, ApplyVectorConstraintsTableProcess::Pointer, Process>
-        (m, "ApplyVectorConstraintsTableProcess")
+    py::class_<ApplyVectorConstraintTableProcess, ApplyVectorConstraintTableProcess::Pointer, Process>
+        (m, "ApplyVectorConstraintTableProcess")
         .def(py::init<ModelPart&, const Parameters&>());
 
-    py::class_<ApplyScalarConstraintsTableProcess, ApplyScalarConstraintsTableProcess::Pointer, Process>
-        (m, "ApplyScalarConstraintsTableProcess")
+    py::class_<ApplyScalarConstraintTableProcess, ApplyScalarConstraintTableProcess::Pointer, Process>
+        (m, "ApplyScalarConstraintTableProcess")
         .def(py::init<ModelPart&, const Parameters&>());
 }
 
