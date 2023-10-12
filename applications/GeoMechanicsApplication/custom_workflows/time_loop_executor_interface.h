b832147a200f2de0fe46772e428f1a8ac04db179
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2023-10-12 16:21:25 +0200

    forgotten interface.h

---
+++ applications/GeoMechanicsApplication/custom_workflows/time_loop_executor_interface.h
@@ -0,0 +1,38 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+//  Main authors:    Richard Faasse
+//                   Anne van de Graaf
+//                   Wijtze Pieter Kikstra
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "time_step_end_state.hpp"
+#include "time_incrementor.h"
+#include "strategy_wrapper.hpp"
+
+namespace Kratos
+{
+
+class Process;
+
+class TimeLoopExecutorInterface{
+public :
+    virtual ~TimeLoopExecutorInterface() = default;
+    virtual void SetProcessObservables(const std::vector<std::weak_ptr<Process>>& rProcessObservables) = 0;
+    virtual void SetTimeIncrementor(std::unique_ptr<TimeIncrementor> pTimeIncrementor) = 0;
+    virtual void SetSolverStrategyTimeStepExecutor(std::shared_ptr<StrategyWrapper> pStrategyWrapper) = 0;
+    virtual std::vector<TimeStepEndState> Run(TimeStepEndState EndState) = 0;
+};
+
+}
