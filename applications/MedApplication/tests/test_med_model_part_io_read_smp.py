07bbaca842eed1f7277ab9cd0fb40584dea4a3d3
Philipp Bucher <philipp.bucher@tum.de>
2023-10-17 21:16:59 +0200

    simplification

--- applications/MedApplication/tests/test_med_model_part_io_read_smp.py
+++ applications/MedApplication/tests/test_med_model_part_io_read_smp.py
@@ -2,17 +2,12 @@ import KratosMultiphysics as KM
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 import KratosMultiphysics.MedApplication as KratosMed
+from testing_utilities import MedModelPartIOTestCase, GetMedPath, get_num_geometries_by_type, get_node_ids, get_geom_node_ids
 
 from itertools import chain
-from pathlib import Path
-from typing import Any, Dict, List, Set
+from typing import Any, Dict, Set
 
-
-def GetMedPath(med_path, med_name="mesh.med"):
-    return Path(__file__).absolute().parent / "med_files" / med_path / med_name
-
-
-class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
+class TestMedModelPartIOReadSubModelPart(MedModelPartIOTestCase):
     def test_cube_with_groups(self):
         model: KM.Model = KM.Model()
         model_part: KM.ModelPart = model.CreateModelPart("test")
@@ -310,85 +305,6 @@ class TestMedModelPartIOReadSubModelPart(KratosUnittest.TestCase):
         exp_geoms: Dict[Any, int] = {KM.Tetrahedra3D4: 94, KM.Triangle3D3: 96, KM.Line3D2: 36}
         self.assertEqual(sum(exp_geoms.values()), 226)
         self.assertDictEqual(exp_geoms, get_num_geometries_by_type(model_part))
-
-    def _basic_checks(self, model_part):
-        # check no elements or conditions are created
-        self.assertEqual(model_part.NumberOfElements(), 0)
-        self.assertEqual(model_part.NumberOfConditions(), 0)
-
-        self.assertGreaterEqual(KratosMed.MedTestingUtilities.ComputeLength(model_part), 0.0)
-        self.assertGreaterEqual(KratosMed.MedTestingUtilities.ComputeArea(model_part), 0.0)
-        self.assertGreaterEqual(KratosMed.MedTestingUtilities.ComputeVolume(model_part), 0.0)
-        self.assertGreaterEqual(KratosMed.MedTestingUtilities.ComputeDomainSize(model_part), 0.0)
-
-        # check increasing node Ids
-        for i, node in enumerate(model_part.Nodes):
-            self.assertEqual(node.Id, i + 1)
-
-        # check geometries have correct Ids
-        # Note: Geometries are not ordered, thus cannot check like nodes
-        self.assertEqual(set(get_geometry_ids(model_part)), set(range(1, model_part.NumberOfGeometries() + 1)))
-
-        # check that the entities are unique in the ModelParts
-        self._check_unique_nodes(model_part)
-        self._check_unique_geometries(model_part)
-
-        # check each ModelPart has (at least) the nodes of its geometries
-        self._check_nodes_geometries(model_part)
-
-    def _check_unique_nodes(self, model_part):
-        node_ids: List[int] = get_node_ids(model_part)
-        node_ids_unique: Set[int] = set(node_ids)
-        self.assertEqual(len(node_ids), len(node_ids_unique), msg=f"Name of ModelPart: {model_part.FullName()}")
-
-        for smp in model_part.SubModelParts:
-            self._check_unique_nodes(smp)
-
-    def _check_unique_geometries(self, model_part):
-        geom_ids: List[int] = get_geometry_ids(model_part)
-        geom_ids_unique: Set[int] = set(geom_ids)
-        self.assertEqual(len(geom_ids), len(geom_ids_unique), msg=f"Name of ModelPart: {model_part.FullName()}")
-
-        for smp in model_part.SubModelParts:
-            self._check_unique_geometries(smp)
-
-    def _check_nodes_geometries(self, model_part):
-        geom_node_ids: List[int] = get_geom_node_ids(model_part)
-        geom_node_ids_unique: Set[int] = set(geom_node_ids)
-
-        node_ids: List[int] = get_node_ids(model_part)
-
-        # there can be nodes that do not belong to geometries, thus geom_node_ids is subset
-        self.assertTrue(geom_node_ids_unique.issubset(node_ids), msg=f"Name of ModelPart: {model_part.FullName()}")
-
-        for smp in model_part.SubModelParts:
-            self._check_nodes_geometries(smp)
-
-
-def get_num_geometries_by_type(model_part: KM.ModelPart) -> Dict[Any, int]:
-    geoms_by_type: Dict[Any, int] = {}
-    for geom in model_part.Geometries:
-        type_geom = type(geom)
-        if type_geom not in geoms_by_type:
-            geoms_by_type[type_geom] = 0
-        geoms_by_type[type_geom] += 1
-    return geoms_by_type
-
-
-def get_node_ids(model_part: KM.ModelPart) -> List[int]:
-    return [node.Id for node in model_part.Nodes]
-
-
-def get_geometry_ids(model_part: KM.ModelPart) -> List[int]:
-    return [geom.Id for geom in model_part.Geometries]
-
-
-def get_geom_node_ids(model_part: KM.ModelPart) -> List[int]:
-    node_ids: List[int] = []
-    for geom in model_part.Geometries:
-        for node in geom:
-            node_ids.append(node.Id)
-    return node_ids
 
 
 if __name__ == "__main__":
