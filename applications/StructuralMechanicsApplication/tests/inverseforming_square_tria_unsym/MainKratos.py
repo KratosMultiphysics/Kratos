5500df2be069a495fea3bba7869529e7c06833c6
Juan Carlos Alvarado Morones <q629953@vmcae10169.bmwgroup.net>
2023-10-18 13:41:50 +0200

    added sqauare trias unsymmetric and quads test cases

---
+++ applications/StructuralMechanicsApplication/tests/inverseforming_square_tria_unsym/MainKratos.py
@@ -0,0 +1,45 @@
+import sys
+import time
+import importlib
+
+import KratosMultiphysics
+
+def CreateAnalysisStageWithFlushInstance(cls, global_model, parameters):
+    class AnalysisStageWithFlush(cls):
+
+        def __init__(self, model,project_parameters, flush_frequency=10.0):
+            super().__init__(model,project_parameters)
+            self.flush_frequency = flush_frequency
+            self.last_flush = time.time()
+            sys.stdout.flush()
+
+        def Initialize(self):
+            super().Initialize()
+            sys.stdout.flush()
+
+        def FinalizeSolutionStep(self):
+            super().FinalizeSolutionStep()
+
+            if self.parallel_type == "OpenMP":
+                now = time.time()
+                if now - self.last_flush > self.flush_frequency:
+                    sys.stdout.flush()
+                    self.last_flush = now
+
+    return AnalysisStageWithFlush(global_model, parameters)
+
+if __name__ == "__main__":
+
+    with open("ProjectParameters.json", 'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    analysis_stage_module_name = parameters["analysis_stage"].GetString()
+    analysis_stage_class_name = analysis_stage_module_name.split('.')[-1]
+    analysis_stage_class_name = ''.join(x.title() for x in analysis_stage_class_name.split('_'))
+
+    analysis_stage_module = importlib.import_module(analysis_stage_module_name)
+    analysis_stage_class = getattr(analysis_stage_module, analysis_stage_class_name)
+
+    global_model = KratosMultiphysics.Model()
+    simulation = CreateAnalysisStageWithFlushInstance(analysis_stage_class, global_model, parameters)
+    simulation.Run()
