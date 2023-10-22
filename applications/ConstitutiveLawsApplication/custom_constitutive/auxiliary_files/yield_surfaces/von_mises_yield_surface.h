15be029d9f74cda78c1e42b87cb039e68fedff36
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2023-10-22 21:04:07 +0200

    more

--- applications/ConstitutiveLawsApplication/custom_constitutive/auxiliary_files/yield_surfaces/von_mises_yield_surface.h
+++ applications/ConstitutiveLawsApplication/custom_constitutive/auxiliary_files/yield_surfaces/von_mises_yield_surface.h
@@ -159,8 +159,10 @@ public:
         if (r_material_properties[SOFTENING_TYPE] == static_cast<int>(SofteningType::Exponential)) {
             rAParameter = 1.00 / (fracture_energy * young_modulus / (CharacteristicLength * std::pow(yield_compression, 2)) - 0.5);
             KRATOS_ERROR_IF(rAParameter < 0.0) << "Fracture energy is too low, increase FRACTURE_ENERGY..." << std::endl;
-        } else { // linear
+        } else if (r_material_properties[SOFTENING_TYPE] == static_cast<int>(SofteningType::Linear)) { // linear
             rAParameter = -std::pow(yield_compression, 2) / (2.0 * young_modulus * fracture_energy / CharacteristicLength);
+        } else {
+            rAParameter = 0.0;
         }
     }
 
