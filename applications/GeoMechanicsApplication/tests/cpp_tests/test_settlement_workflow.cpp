f6d565b30527795dba3891c1d714be009ced2927
Richard Faasse <richard.faasse@deltares.nl>
2023-10-25 16:19:38 +0200

    Skipping workflow test temporarily due to discussed pipeline issues

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_settlement_workflow.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_settlement_workflow.cpp
@@ -26,7 +26,7 @@ using namespace Kratos;
 namespace Kratos::Testing
 {
 
-KRATOS_TEST_CASE_IN_SUITE(SettlementWorkflow, KratosGeoMechanicsIntegrationSuite)
+KRATOS_TEST_CASE_IN_SUITE(SettlementWorkflow, KratosGeoMechanicsSkippedSuite)
 {
     const auto working_directory = std::filesystem::path{"./applications/GeoMechanicsApplication/tests/test_settlement_workflow"};
 
