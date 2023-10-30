78d4867d5c98821bda0f120e9463dea1c6ba61fe
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2023-10-30 11:03:22 +0000

    print vtk for specific submodelpart

--- applications/ParticleMechanicsApplication/python_scripts/particle_vtk_output_process.py
+++ applications/ParticleMechanicsApplication/python_scripts/particle_vtk_output_process.py
@@ -15,37 +15,37 @@ class ParticleVtkOutputProcess(KratosMultiphysics.OutputProcess):
     def __init__(self, model: KratosMultiphysics.Model, settings: KratosMultiphysics.Parameters) -> None:
         super().__init__()
 
-        model_part_name = settings["model_part_name"].GetString()
-        self.model_part = model[model_part_name]
+        self.model = model
+        self.settings = settings
 
-        self.TranslateLegacyVariablesAccordingToCurrentStandard(settings)
+        self.full_model_part_name = self.settings["model_part_name"].GetString()
+        main_model_part_name = self.full_model_part_name.split(".",1)[0]
+        self.main_model_part = self.model[main_model_part_name]
 
+        # Change name deprecated settings
+        self.TranslateLegacyVariablesAccordingToCurrentStandard(self.settings)
         # Validate settings using default parameters defined in ParticleVtkOutput process
-        default_settings = KratosParticle.ParticleVtkOutput.GetDefaultParameters()
-        settings.ValidateAndAssignDefaults(default_settings)
-
         # Default settings can be found in "custom_io/particle_vtk_output.cpp"
-        self.vtk_io = KratosParticle.ParticleVtkOutput(self.model_part, settings)
+        default_settings = KratosParticle.ParticleVtkOutput.GetDefaultParameters()
+        self.settings.ValidateAndAssignDefaults(default_settings)
 
-        if settings["save_output_files_in_folder"].GetBool():
-            if self.model_part.GetCommunicator().MyPID() == 0:
-                output_path = settings["output_path"].GetString()
-                if not self.model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
+        if self.settings["save_output_files_in_folder"].GetBool():
+            if self.main_model_part.GetCommunicator().MyPID() == 0:
+                output_path = self.settings["output_path"].GetString()
+                if not self.main_model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
                     kratos_utils.DeleteDirectoryIfExisting(output_path)
-            self.model_part.GetCommunicator().GetDataCommunicator().Barrier()
-
-        self.__controller = KratosMultiphysics.OutputController(model, settings)
+            self.main_model_part.GetCommunicator().GetDataCommunicator().Barrier()
 
         # Print background grid model part
-        if not self.model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
+        if not self.main_model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
             grid_settings = KratosMultiphysics.Parameters()
             grid_settings.AddString("model_part_name","Background_Grid")
-            grid_settings.AddString("file_format",settings["file_format"].GetString())
-            grid_settings.AddDouble("output_precision",settings["output_precision"].GetDouble())
-            grid_settings.AddBool("output_sub_model_parts",settings["output_sub_model_parts"].GetBool())
-            grid_settings.AddString("output_path",settings["output_path"].GetString())
-            grid_settings.AddBool("save_output_files_in_folder",settings["save_output_files_in_folder"].GetBool())
-            background_grid = model["Background_Grid"]
+            grid_settings.AddString("file_format",self.settings["file_format"].GetString())
+            grid_settings.AddDouble("output_precision",self.settings["output_precision"].GetDouble())
+            grid_settings.AddBool("output_sub_model_parts",self.settings["output_sub_model_parts"].GetBool())
+            grid_settings.AddString("output_path",self.settings["output_path"].GetString())
+            grid_settings.AddBool("save_output_files_in_folder",self.settings["save_output_files_in_folder"].GetBool())
+            background_grid = self.model["Background_Grid"]
             KratosMultiphysics.VtkOutput(background_grid, grid_settings).PrintOutput()
 
     # This function can be extended with new deprecated variables as they are generated
@@ -54,34 +54,32 @@ class ParticleVtkOutputProcess(KratosMultiphysics.OutputProcess):
 
         old_name = 'output_frequency'
         new_name = 'output_interval'
-
         if DeprecationManager.HasDeprecatedVariable(context_string, settings, old_name, new_name):
             DeprecationManager.ReplaceDeprecatedVariableName(settings, old_name, new_name)
 
         old_name = 'write_properties_id'
         new_name = 'write_ids'
-
         if DeprecationManager.HasDeprecatedVariable(context_string, settings, old_name, new_name):
             DeprecationManager.ReplaceDeprecatedVariableName(settings, old_name, new_name)
 
         old_name = 'folder_name'
         new_name = 'output_path'
-
         if DeprecationManager.HasDeprecatedVariable(context_string, settings, old_name, new_name):
             DeprecationManager.ReplaceDeprecatedVariableName(settings, old_name, new_name)
 
         old_name = 'gauss_point_results'
         new_name = 'gauss_point_variables_in_elements'
-
         if DeprecationManager.HasDeprecatedVariable(context_string, settings, old_name, new_name):
             DeprecationManager.ReplaceDeprecatedVariableName(settings, old_name, new_name)
 
     def ExecuteBeforeSolutionLoop(self) -> None:
-        if not self.model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
-            self.vtk_io.PrintOutput()
+        vtk_model_part = self.model[self.full_model_part_name]
+        self.vtk_io = KratosParticle.ParticleVtkOutput(vtk_model_part, self.settings)
 
-    def Check(self) -> int:
-        return self.__controller.Check()
+        self.__controller = KratosMultiphysics.OutputController(self.model, self.settings)
+
+        if not self.main_model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
+            self.vtk_io.PrintOutput()
 
     def PrintOutput(self) -> None:
         self.vtk_io.PrintOutput()
