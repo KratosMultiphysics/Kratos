27576a7808d499ab8d3ef2e24f697679dda39603
Vicente Mataix Ferrandiz <vmataix@altair.com>
2023-10-31 18:59:53 +0100

    WIP tree tests

--- kratos/tests/cpp_tests/spatial_containers/test_search_wrapper.cpp
+++ kratos/tests/cpp_tests/spatial_containers/test_search_wrapper.cpp
@@ -251,12 +251,19 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperGeometricalObjectsBinsSearchIsNotInside, 
     KRATOS_EXPECT_FALSE(results[p_outside_point->Id()].IsObjectFound());
 }
 
-// Definition of the geometrical object bins search wrapper
+// Definition of the trees search wrapper
 using SearchWrapperKDTreeElement = SearchWrapper<Tree<KDTreePartition<Bucket<3ul, PointObject<Element>, std::vector<PointObject<Element>::Pointer>>>>>;
+using SearchWrapperOCTreeElement = SearchWrapper<Tree<OCTreePartition<Bucket<3ul, PointObject<Element>, std::vector<PointObject<Element>::Pointer>>>>>;
+using SearchWrapperStaticBinsTreeElement = SearchWrapper<Tree<Bins<3ul, PointObject<Element>, std::vector<PointObject<Element>::Pointer>>>>;
+using SearchWrapperBinsDynamicElement = SearchWrapper<BinsDynamic<3ul, PointObject<Element>, std::vector<PointObject<Element>::Pointer>>>;
 
-/** Checks SearchWrapper works for KDTreeElement search in radius
-*/
-KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchInRadius, KratosCoreFastSuite)
+/**
+ * @brief A function to test tree-based search in a specified radius.
+ * @details This function tests the tree-based search algorithm within a specified radius.
+ * @tparam TSearchWrapper The type of the search wrapper.
+ */
+template<class TSearchWrapper>
+void TestTreeSearchInRadius()
 {
     Model current_model;
 
@@ -265,14 +272,14 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchInRadius, KratosCoreFa
 
     // Generate the search wrapper for bins
     DataCommunicator serial_communicator;
-    SearchWrapperKDTreeElement search_wrapper(r_skin_part.Elements(), serial_communicator);
+    TSearchWrapper search_wrapper(r_skin_part.Elements(), serial_communicator);
 
     // Generate new model part
     ModelPart& r_point_model_part = current_model.CreateModelPart("PointModelPart");
     auto p_point = r_point_model_part.CreateNewNode(1, 0.0, 0.0, 0.0);
     auto& r_array_nodes = r_point_model_part.Nodes();
 
-    SearchWrapperKDTreeElement::ResultContainerVectorType results;
+    typename TSearchWrapper::ResultContainerVectorType results;
 
     // 0.3 radius
     search_wrapper.SearchInRadius(r_array_nodes.begin(), r_array_nodes.end(), 0.3, results);
@@ -287,9 +294,41 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchInRadius, KratosCoreFa
     KRATOS_EXPECT_EQ(results[p_point->Id()].NumberOfGlobalResults(), 12);
 }
 
-/** Checks SearchWrapper works for KDTreeElement search nearest
+/** Checks SearchWrapper works for KDTreeElement search in radius
 */
-KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearestInRadius, KratosCoreFastSuite) 
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchInRadius<SearchWrapperKDTreeElement>();
+}
+
+/** Checks SearchWrapper works for OCTreeElement search in radius
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperOCTreeElementSearchInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchInRadius<SearchWrapperOCTreeElement>();
+}
+
+/** Checks SearchWrapper works for StaticBinsTree search in radius
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperStaticBinsTreeElementSearchInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchInRadius<SearchWrapperStaticBinsTreeElement>();
+}
+
+/** Checks SearchWrapper works for BinsDynamicElement search in radius
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperBinsDynamicElementSearchInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchInRadius<SearchWrapperBinsDynamicElement>();
+}
+
+/**
+ * @brief A function to test tree-based nearest search in a specified radius.
+ * @details This function tests the tree-based nearest search algorithm within a specified radius.
+ * @tparam TSearchWrapper The type of the search wrapper.
+ */
+template<class TSearchWrapper>
+void TestTreeSearchNearestInRadius()
 {
     constexpr double tolerance = 1e-6;
 
@@ -303,7 +342,7 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearestInRadius, Krato
 
     // Generate the search wrapper for bins
     DataCommunicator serial_communicator;
-    SearchWrapperKDTreeElement search_wrapper(r_skin_part.Elements(), serial_communicator);
+    TSearchWrapper search_wrapper(r_skin_part.Elements(), serial_communicator);
 
     double epsilon = 1.0e-6;
 
@@ -312,7 +351,7 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearestInRadius, Krato
     auto p_near_point = r_point_model_part.CreateNewNode(1, epsilon,epsilon,epsilon);
     auto& r_array_nodes = r_point_model_part.Nodes();
 
-    SearchWrapperKDTreeElement::ResultContainerVectorType results;
+    typename TSearchWrapper::ResultContainerVectorType results;
     search_wrapper.SearchNearestInRadius(r_array_nodes.begin(), r_array_nodes.end(), cube_z, results);
 
     KRATOS_EXPECT_EQ(results.NumberOfSearchResults(), 1);
@@ -334,9 +373,41 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearestInRadius, Krato
     KRATOS_EXPECT_EQ(id, 4);
 }
 
-/** Checks SearchWrapper works for KDTreeElement search nearest
+/** Checks SearchWrapper works for KDTreeElement search nearest in radius
 */
-KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearest, KratosCoreFastSuite) 
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearestInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchNearestInRadius<SearchWrapperKDTreeElement>();
+}
+
+/** Checks SearchWrapper works for OCTreeElement search nearest in radius
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperOCTreeElementSearchNearestInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchNearestInRadius<SearchWrapperOCTreeElement>();
+}
+
+/** Checks SearchWrapper works for StaticBinsTree search nearest in radius
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperStaticBinsTreeElementSearchNearestInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchNearestInRadius<SearchWrapperStaticBinsTreeElement>();
+}
+
+/** Checks SearchWrapper works for BinsDynamicElement search nearest in radius
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperBinsDynamicElementSearchNearestInRadius, KratosCoreFastSuite)
+{
+    TestTreeSearchNearestInRadius<SearchWrapperBinsDynamicElement>();
+}
+
+/**
+ * @brief A function to test tree-based nearest search
+ * @details This function tests the tree-based nearest search algorithm
+ * @tparam TSearchWrapper The type of the search wrapper.
+ */
+template<class TSearchWrapper>
+void TestTreeSearchNearest()
 {
     constexpr double tolerance = 1e-6;
 
@@ -350,7 +421,7 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearest, KratosCoreFas
 
     // Generate the search wrapper for bins
     DataCommunicator serial_communicator;
-    SearchWrapperKDTreeElement search_wrapper(r_skin_part.Elements(), serial_communicator);
+    TSearchWrapper search_wrapper(r_skin_part.Elements(), serial_communicator);
 
     double epsilon = 1.0e-6;
 
@@ -359,7 +430,7 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearest, KratosCoreFas
     auto p_near_point = r_point_model_part.CreateNewNode(1, epsilon,epsilon,epsilon);
     auto& r_array_nodes = r_point_model_part.Nodes();
 
-    SearchWrapperKDTreeElement::ResultContainerVectorType results;
+    typename TSearchWrapper::ResultContainerVectorType results;
     search_wrapper.SearchNearest(r_array_nodes.begin(), r_array_nodes.end(), results);
 
     KRATOS_EXPECT_EQ(results.NumberOfSearchResults(), 1);
@@ -376,9 +447,41 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearest, KratosCoreFas
     KRATOS_EXPECT_EQ(id, 4);
 }
 
-/** Checks SearchWrapper works for KDTreeElement empty search nearest 
+/** Checks SearchWrapper works for KDTreeElement search nearest
 */
-KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementEmptySearchNearest, KratosCoreFastSuite) 
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearest, KratosCoreFastSuite)
+{
+    TestTreeSearchNearest<SearchWrapperKDTreeElement>();
+}
+
+/** Checks SearchWrapper works for OCTreeElement search nearest
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperOCTreeElementSearchNearest, KratosCoreFastSuite)
+{
+    TestTreeSearchNearest<SearchWrapperOCTreeElement>();
+}
+
+/** Checks SearchWrapper works for StaticBinsTree search nearest
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperStaticBinsTreeElementSearchNearest, KratosCoreFastSuite)
+{
+    TestTreeSearchNearest<SearchWrapperStaticBinsTreeElement>();
+}
+
+/** Checks SearchWrapper works for BinsDynamicElement search nearest
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperBinsDynamicElementSearchNearest, KratosCoreFastSuite)
+{
+    TestTreeSearchNearest<SearchWrapperBinsDynamicElement>();
+}
+
+/**
+ * @brief A function to test tree-based nearest search (empty)
+ * @details This function tests the tree-based nearest search algorithm (empty)
+ * @tparam TSearchWrapper The type of the search wrapper.
+ */
+template<class TSearchWrapper>
+void TestTreeSearchNearestEmpty()
 {
     Model current_model;
 
@@ -387,18 +490,46 @@ KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementEmptySearchNearest, KratosCo
 
     // Generate the search wrapper for bins
     DataCommunicator serial_communicator;
-    SearchWrapperKDTreeElement search_wrapper(r_skin_part.Elements(), serial_communicator);
+    TSearchWrapper search_wrapper(r_skin_part.Elements(), serial_communicator);
 
     // Generate new model part
     ModelPart& r_point_model_part = current_model.CreateModelPart("PointModelPart");
     auto p_point = r_point_model_part.CreateNewNode(1, 0.0,0.0,0.0);
     auto& r_array_nodes = r_point_model_part.Nodes();
 
-    SearchWrapperKDTreeElement::ResultContainerVectorType results;
+    typename TSearchWrapper::ResultContainerVectorType results;
     search_wrapper.SearchNearest(r_array_nodes.begin(), r_array_nodes.end(), results);
 
     KRATOS_EXPECT_EQ(results.NumberOfSearchResults(), 1);
     KRATOS_EXPECT_FALSE(results[p_point->Id()].IsObjectFound());
 }
+
+/** Checks SearchWrapper works for KDTreeElement search nearest
+*/
+KRATOS_TEST_CASE_IN_SUITE(SearchWrapperKDTreeElementSearchNearestEmpty, KratosCoreFastSuite)
+{
+    TestTreeSearchNearestEmpty<SearchWrapperKDTreeElement>();
+}
+
+// /** Checks SearchWrapper works for OCTreeElement search nearest
+// */
+// KRATOS_TEST_CASE_IN_SUITE(SearchWrapperOCTreeElementSearchNearestEmpty, KratosCoreFastSuite)
+// {
+//     TestTreeSearchNearestEmpty<SearchWrapperOCTreeElement>();
+// }
+
+// /** Checks SearchWrapper works for StaticBinsTree search nearest
+// */
+// KRATOS_TEST_CASE_IN_SUITE(SearchWrapperStaticBinsTreeElementSearchNearestEmpty, KratosCoreFastSuite)
+// {
+//     TestTreeSearchNearestEmpty<SearchWrapperStaticBinsTreeElement>();
+// }
+
+// /** Checks SearchWrapper works for BinsDynamicElement search nearest
+// */
+// KRATOS_TEST_CASE_IN_SUITE(SearchWrapperBinsDynamicElementSearchNearestEmpty, KratosCoreFastSuite)
+// {
+//     TestTreeSearchNearestEmpty<SearchWrapperBinsDynamicElement>();
+// }
 
 } // namespace Kratos::Testing.
