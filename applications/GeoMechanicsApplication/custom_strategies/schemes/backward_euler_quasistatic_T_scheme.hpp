75b2304088104396bf767b3684dfdd6b6ac6b578
Richard Faasse <richard.faasse@deltares.nl>
2023-11-08 14:57:08 +0100

    Clean up the backward euler quasistatic scheme

--- applications/GeoMechanicsApplication/custom_strategies/schemes/backward_euler_quasistatic_T_scheme.hpp
+++ applications/GeoMechanicsApplication/custom_strategies/schemes/backward_euler_quasistatic_T_scheme.hpp
@@ -15,54 +15,43 @@
 #pragma once
 
 // Project includes
+#include "custom_strategies/schemes/newmark_quasistatic_T_scheme.hpp"
 #include "includes/define.h"
 #include "includes/model_part.h"
-#include "utilities/parallel_utilities.h"
 #include "solving_strategies/schemes/scheme.h"
-#include "custom_strategies/schemes/newmark_quasistatic_T_scheme.hpp"
+#include "utilities/parallel_utilities.h"
 
 // Application includes
 #include "geo_mechanics_application_variables.h"
 
-namespace Kratos
-{
+namespace Kratos {
 
-template<class TSparseSpace, class TDenseSpace>
+template <class TSparseSpace, class TDenseSpace>
 
-class BackwardEulerQuasistaticTScheme : public NewmarkQuasistaticTScheme<TSparseSpace,TDenseSpace>
-{
-
+class BackwardEulerQuasistaticTScheme
+    : public NewmarkQuasistaticTScheme<TSparseSpace, TDenseSpace> {
 public:
+    KRATOS_CLASS_POINTER_DEFINITION(BackwardEulerQuasistaticTScheme);
 
-    KRATOS_CLASS_POINTER_DEFINITION( BackwardEulerQuasistaticTScheme );
-
-    using NewmarkQuasistaticTScheme<TSparseSpace,TDenseSpace>::mDeltaTime;
-
-    ///Constructor
-    // ============================================================================================
-    // ============================================================================================
-    BackwardEulerQuasistaticTScheme() :
-        NewmarkQuasistaticTScheme<TSparseSpace,TDenseSpace>(1.0)
-    {}
+    BackwardEulerQuasistaticTScheme()
+        : NewmarkQuasistaticTScheme<TSparseSpace, TDenseSpace>(1.0)
+    {
+    }
 
 protected:
-
-    // ============================================================================================
-    // ============================================================================================
     inline void UpdateVariablesDerivatives(ModelPart& rModelPart) override
     {
         KRATOS_TRY
 
-        //Update DtTemperature
-
-        block_for_each(rModelPart.Nodes(), [this](Node& rNode){
-            const double DeltaTemperature =  rNode.FastGetSolutionStepValue(TEMPERATURE)
-                                           - rNode.FastGetSolutionStepValue(TEMPERATURE, 1);
+        block_for_each(rModelPart.Nodes(), [this](Node& rNode) {
+            const double DeltaTemperature =
+                rNode.FastGetSolutionStepValue(TEMPERATURE) -
+                rNode.FastGetSolutionStepValue(TEMPERATURE, 1);
             rNode.FastGetSolutionStepValue(DT_TEMPERATURE) = DeltaTemperature / mDeltaTime;
         });
 
-        KRATOS_CATCH( "" )
+        KRATOS_CATCH("")
     }
 
 }; // Class BackwardEulerQuasistaticTScheme
-}  // namespace Kratos
+} // namespace Kratos
