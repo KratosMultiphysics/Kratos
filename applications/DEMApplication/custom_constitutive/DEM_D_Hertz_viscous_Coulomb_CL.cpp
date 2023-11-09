ed5797521d36604fd25b7ff3c27c9d91c6c79923
rlrangel <rrangel@cimne.upc.edu>
2023-11-09 20:09:19 +0100

    Disable minoring factor

--- applications/DEMApplication/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.cpp
+++ applications/DEMApplication/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.cpp
@@ -247,11 +247,11 @@ namespace Kratos {
         LocalElasticContactForce[0] = OldLocalElasticContactForce[0] - mKt * LocalDeltDisp[0];
         LocalElasticContactForce[1] = OldLocalElasticContactForce[1] - mKt * LocalDeltDisp[1];
 
-        if (previous_indentation > indentation) {
-            const double minoring_factor = sqrt (indentation / previous_indentation);
-            LocalElasticContactForce[0] = OldLocalElasticContactForce[0] * minoring_factor - mKt * LocalDeltDisp[0];
-            LocalElasticContactForce[1] = OldLocalElasticContactForce[1] * minoring_factor - mKt * LocalDeltDisp[1];
-        }
+        //if (previous_indentation > indentation) {
+        //    const double minoring_factor = sqrt (indentation / previous_indentation);
+        //    LocalElasticContactForce[0] = OldLocalElasticContactForce[0] * minoring_factor - mKt * LocalDeltDisp[0];
+        //    LocalElasticContactForce[1] = OldLocalElasticContactForce[1] * minoring_factor - mKt * LocalDeltDisp[1];
+        //}
 
         AuxElasticShearForce = sqrt(LocalElasticContactForce[0] * LocalElasticContactForce[0] + LocalElasticContactForce[1] * LocalElasticContactForce[1]);
 
