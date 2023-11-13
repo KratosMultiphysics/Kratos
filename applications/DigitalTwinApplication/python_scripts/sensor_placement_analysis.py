47835b5433f40facd15fe0c36541049c54ea0c82
sunethwarna <suneth.warna@gmail.com>
2023-11-13 08:52:12 +0100

    updating sensor placement analysis

--- applications/DigitalTwinApplication/python_scripts/sensor_placement_analysis.py
+++ applications/DigitalTwinApplication/python_scripts/sensor_placement_analysis.py
@@ -3,8 +3,9 @@ import importlib
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.DigitalTwinApplication as KratosDT
 
+from KratosMultiphysics.analysis_stage import AnalysisStage
 from KratosMultiphysics.OptimizationApplication.model_part_controllers.mdpa_model_part_controller import MdpaModelPartController
-from KratosMultiphysics.DigitalTwinApplication.sensor_specification_solvers.sensor_specification_analysis import SensorSpecificationAnalysis
+from KratosMultiphysics.DigitalTwinApplication.sensor_sensitivity_solvers.sensor_sensitivity_analysis import SensorSensitivityAnalysis
 from KratosMultiphysics.DigitalTwinApplication.sensor_placement_algorithms.cosine_similarity_sensor_placement_algorithm import CosineSimilaritySensorPlacementAlgorithm
 
 class SensorPlacementAnalysis:
@@ -27,8 +28,8 @@ class SensorPlacementAnalysis:
 
         self._CreateModelPartControllers()
 
-        self.__list_of_test_analyses: 'list[SensorSpecificationAnalysis]' = self._CreateAnalyses(self.project_parameters["test_analyses"])
-        self.__list_of_sensor_analyses: 'list[SensorSpecificationAnalysis]' = self._CreateAnalyses(self.project_parameters["sensor_analyses"])
+        self.__list_of_test_analyses: 'list[AnalysisStage]' = self._CreateAnalyses(self.project_parameters["test_analyses"])
+        self.__list_of_sensor_analyses: 'list[SensorSensitivityAnalysis]' = self._CreateAnalyses(self.project_parameters["sensor_analyses"])
 
         algorithm_type = self.project_parameters["algorithm_settings"]["type"].GetString()
         if algorithm_type == "cosine_similarity_sensor_placement":
@@ -43,21 +44,21 @@ class SensorPlacementAnalysis:
     def Run(self):
         self.Initialize()
 
-        list(map(SensorSpecificationAnalysis.Run, self.__list_of_test_analyses))
-        list(map(SensorSpecificationAnalysis.Run, self.__list_of_sensor_analyses))
+        list(map(AnalysisStage.Run, self.__list_of_test_analyses))
+        list(map(SensorSensitivityAnalysis.Run, self.__list_of_sensor_analyses))
 
-        specifications_list: 'list[KratosDT.Sensors.SensorSpecification]' = []
+        sensors_list: 'list[KratosDT.Sensors.SensorSpecification]' = []
         for sensor_analysis in self.__list_of_sensor_analyses:
-            specifications_list.extend(sensor_analysis.GetListOfSpecifications())
+            sensors_list.extend(sensor_analysis.GetListOfSensors())
 
-        self.algorithm.Execute(specifications_list)
+        self.algorithm.Execute(sensors_list)
 
     def _CreateModelPartControllers(self):
         for model_part_controller_settings in self.project_parameters["model_parts"]:
             self.__list_of_model_part_controllers.append(MdpaModelPartController(self.model, model_part_controller_settings))
 
-    def _CreateAnalyses(self, parameters: Kratos.Parameters) -> 'list[SensorSpecificationAnalysis]':
-        list_of_analyses: 'list[SensorSpecificationAnalysis]' = []
+    def _CreateAnalyses(self, parameters: Kratos.Parameters) -> 'list[SensorSensitivityAnalysis]':
+        list_of_analyses: 'list[SensorSensitivityAnalysis]' = []
         for analyses_settings in parameters:
 
             analysis_stage_module_name = analyses_settings["analysis_stage"].GetString()
