29cba3b850b83fd339065dde9c44568059f00482
sunethwarna <suneth.warna@gmail.com>
2023-11-15 13:24:46 +0100

    add type

--- applications/DigitalTwinApplication/python_scripts/utilities/expression_utils.py
+++ applications/DigitalTwinApplication/python_scripts/utilities/expression_utils.py
@@ -1,6 +1,11 @@
 import typing
 import KratosMultiphysics as Kratos
 
+ExpressionUnionType = typing.Union[
+                            Kratos.Expression.NodalExpression,
+                            Kratos.Expression.ConditionExpression,
+                            Kratos.Expression.ElementExpression]
+
 def GetContainerExpressionType(data_location: Kratos.Globals.DataLocation) -> 'typing.Union[typing.Type[Kratos.Expression.NodalExpression], typing.Type[Kratos.Expression.ConditionExpression], typing.Type[Kratos.Expression.ElementExpression]]':
     if data_location in [Kratos.Globals.DataLocation.NodeHistorical, Kratos.Globals.DataLocation.NodeNonHistorical]:
         return Kratos.Expression.NodalExpression
@@ -11,7 +16,7 @@ def GetContainerExpressionType(data_location: Kratos.Globals.DataLocation) -> 't
     else:
         raise RuntimeError(f"Unsupported {data_location}.")
 
-def GetContainerExpression(model_part: Kratos.ModelPart, data_location: Kratos.Globals.DataLocation, variable: typing.Any) -> 'typing.Union[Kratos.Expression.NodalExpression, Kratos.Expression.ConditionExpression, Kratos.Expression.ElementExpression]':
+def GetContainerExpression(model_part: Kratos.ModelPart, data_location: Kratos.Globals.DataLocation, variable: typing.Any) -> ExpressionUnionType:
     cexp_type = GetContainerExpressionType(data_location)
     expression = cexp_type(model_part)
     if data_location == Kratos.Globals.DataLocation.NodeHistorical:
