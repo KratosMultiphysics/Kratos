96f5223b38bce4dc48a5ecf9aa198d7f52b252ba
Vicente Mataix Ferándiz <vmataix@altair.com>
2023-11-17 14:54:07 +0000

    Partially revert

--- kratos/solving_strategies/convergencecriterias/residual_criteria.h
+++ kratos/solving_strategies/convergencecriterias/residual_criteria.h
@@ -214,6 +214,7 @@ public:
     void Initialize(ModelPart& rModelPart) override
     {
         BaseType::Initialize(rModelPart);
+        KRATOS_ERROR_IF(rModelPart.IsDistributed() && rModelPart.NumberOfMasterSlaveConstraints() > 0) << "This Criteria does not yet support constraints in MPI!" << std::endl;
     }
 
     /**
