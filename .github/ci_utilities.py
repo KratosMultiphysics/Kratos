7a03070e15c5aa839f48fd65dd1edf3110258b10
Philipp <philipp.bucher@tum.de>
2023-11-19 17:42:42 +0100

    more debug

--- .github/ci_utilities.py
+++ .github/ci_utilities.py
@@ -3,29 +3,29 @@ import subprocess
 from sys import argv
 
 
-def get_files_changed_in_pr() -> list[Path] | None:
+def get_files_changed_in_pr(pr_number: int) -> list[Path] | None:
     try:
-        subprocess.run(
-            ["gh pr view ${{ github.event.pull_request.number }} --json files --jq '.files.[].path'"],
-            check=True,
-        )
         # subprocess.run(
-        #     [
-        #         "gh",
-        #         "pr",
-        #         "view",
-        #         "${{ github.event.pull_request.number }}",
-        #         "--json",
-        #         "files",
-        #         "--jq",
-        #         "'.files.[].path'",
-        #     ],
+        #     ["gh pr view ${{ github.event.pull_request.number }} --json files --jq '.files.[].path'"],
         #     check=True,
         # )
+        subprocess.run(
+            [
+                "gh",
+                "pr",
+                "view",
+                str(pr_number),
+                "--json",
+                "files",
+                "--jq",
+                "'.files.[].path'",
+            ],
+            check=True,
+        )
     except:
         return None
 
 
 if __name__ == "__main__":
     print(argv)
-    get_files_changed_in_pr()
+    get_files_changed_in_pr(argv[1])
