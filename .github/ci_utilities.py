4476a220067623cf6057b93565d5955360934278
Philipp <philipp.bucher@tum.de>
2023-11-19 17:33:05 +0100

    fixed syntax

--- .github/ci_utilities.py
+++ .github/ci_utilities.py
@@ -1,12 +1,25 @@
 from pathlib import Path
 import subprocess
 
+
 def get_files_changed_in_pr() -> list[Path] | None:
     try:
-        subprocess.run(["gh pr view ${{ github.event.pull_request.number }} --json files --jq '.files.[].path'"], check=True)
+        subprocess.run(
+            [
+                "gh",
+                "pr",
+                "view",
+                "${{ github.event.pull_request.number }}",
+                "--json",
+                "files",
+                "--jq",
+                "'.files.[].path'",
+            ],
+            check=True,
+        )
     except:
         return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     get_files_changed_in_pr()
