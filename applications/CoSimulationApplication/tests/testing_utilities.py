a88ff6f91078635003acd64b72ed42b42523e6e9
Philipp <philipp.bucher@tum.de>
2023-11-19 17:21:31 +0100

    test lots files changed

--- applications/CoSimulationApplication/tests/testing_utilities.py
+++
@@ -1,8 +0,0 @@
-
-class DummySolverWrapper(object):
-    '''dummy object used for testing to emulate the behavior of the SolverWrapper'''
-    def __init__(self, interface_data_dict):
-        self.interface_data_dict = interface_data_dict
-
-    def GetInterfaceData(self, data_name):
-        return self.interface_data_dict[data_name]
