7db4a0b00ec5670e39130a45ea3621e4420fa9b4
Philipp <philipp.bucher@tum.de>
2023-11-19 17:30:38 +0100

    create utilities

---
+++ .github/ci_utilities.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import subprocess
+
+def get_files_changed_in_pr() -> list[Path] | None:
+    try:
+        subprocess.run(["gh pr view ${{ github.event.pull_request.number }} --json files --jq '.files.[].path'"], check=True)
+    except:
+        return None
+
+
+if __name__ == '__main__':
+    get_files_changed_in_pr()
