dffd599aa9f136db893bdea2c068a00e4b05e11e
rlrangel <rrangel@cimne.upc.edu>
2023-11-21 21:40:10 +0100

    Cleaning

--- applications/ThermalDEMApplication/custom_utilities/graph_utilities.cpp
+++ applications/ThermalDEMApplication/custom_utilities/graph_utilities.cpp
@@ -22,7 +22,6 @@ namespace Kratos {
     mGraph_ParticleTempAvg               = false;
     mGraph_ParticleTempDev               = false;
     mGraph_ModelTempAvg                  = false;
-    mGraph_ParticleHeatFluxContributions = false;
     mGraph_ParticleHeatGenContributions  = false;
     mGraph_ParticleEnergyContributions   = false;
   }
@@ -47,7 +46,6 @@ namespace Kratos {
     mGraph_ParticleTempAvg               = ParticleTempAvg;
     mGraph_ParticleTempDev               = ParticleTempDev;
     mGraph_ModelTempAvg                  = ModelTempAvg;
-    mGraph_ParticleHeatFluxContributions = ParticleHeatFluxContributions;
     mGraph_ParticleHeatGenContributions  = ParticleHeatGenContributions;
     mGraph_ParticleEnergyContributions   = ParticleEnergyContributions;
 
@@ -92,22 +90,8 @@ namespace Kratos {
       mFile_ModelTempAvg << "3 - AVERAGE MODEL TEMPERATURE";
       mFile_ModelTempAvg << std::endl;
     }
-    if (mGraph_ParticleHeatFluxContributions) {
-      mFile_ParticleHeatFluxContributions.open("graph_flux_contributions.txt", std::ios::out);
-      KRATOS_ERROR_IF_NOT(mFile_ParticleHeatFluxContributions) << "Could not open graph file for heat flux contributions!" << std::endl;
-      mFile_ParticleHeatFluxContributions << "1 - TIME STEP | ";
-      mFile_ParticleHeatFluxContributions << "2 - TIME | ";
-      mFile_ParticleHeatFluxContributions << "3 - DIRECT CONDUCTION | ";
-      mFile_ParticleHeatFluxContributions << "4 - INDIRECT CONDUCTION | ";
-      mFile_ParticleHeatFluxContributions << "5 - RADIATION | ";
-      mFile_ParticleHeatFluxContributions << "6 - GENERATION | ";
-      mFile_ParticleHeatFluxContributions << "7 - CONVECTION | ";
-      mFile_ParticleHeatFluxContributions << "8 - SURFACE PRESCRIBED | ";
-      mFile_ParticleHeatFluxContributions << "9 - VOLUME PRESCRIBED";
-      mFile_ParticleHeatFluxContributions << std::endl;
-    }
     if (mGraph_ParticleHeatGenContributions) {
-      mFile_ParticleHeatGenContributions.open("graph_generation_contributions.txt", std::ios::out);
+      mFile_ParticleHeatGenContributions.open("graph_generation_relative_contributions.txt", std::ios::out);
       KRATOS_ERROR_IF_NOT(mFile_ParticleHeatGenContributions) << "Could not open graph file for heat generation contributions!" << std::endl;
       mFile_ParticleHeatGenContributions << "1 - TIME STEP | ";
       mFile_ParticleHeatGenContributions << "2 - TIME | ";
@@ -152,37 +136,28 @@ namespace Kratos {
       return;
 
     // Initialize results
-    const int num_of_particles                    =  rModelPart.NumberOfElements();
-    int       num_ratio_particles_flux            =  0;
-    int       num_ratio_particles_gen             =  0;
-    double    total_vol                           =  0.0;
+    const int num_of_particles                       =  rModelPart.NumberOfElements();
+    int       num_ratio_particles_gen                =  0;
+    double    total_vol                              =  0.0;
 
-    double    particle_temp_min                   =  DBL_MAX;
-    double    particle_temp_max                   = -DBL_MAX;
-    double    particle_temp_avg                   =  0.0;
-    double    particle_temp_dev                   =  0.0;
-    double    model_temp_avg                      =  0.0;
+    double    particle_temp_min                      =  DBL_MAX;
+    double    particle_temp_max                      = -DBL_MAX;
+    double    particle_temp_avg                      =  0.0;
+    double    particle_temp_dev                      =  0.0;
+    double    model_temp_avg                         =  0.0;
 
-    double    particle_flux_conducdir_ratio_avg   =  0.0;
-    double    particle_flux_conducindir_ratio_avg =  0.0;
-    double    particle_flux_rad_ratio_avg         =  0.0;
-    double    particle_flux_gen_ratio_avg         =  0.0;
-    double    particle_flux_conv_ratio_avg        =  0.0;
-    double    particle_flux_prescsurf_ratio_avg   =  0.0;
-    double    particle_flux_prescvol_ratio_avg    =  0.0;
+    double    particle_gen_slid_pp_ratio_avg         =  0.0;
+    double    particle_gen_slid_pw_ratio_avg         =  0.0;
+    double    particle_gen_roll_pp_ratio_avg         =  0.0;
+    double    particle_gen_roll_pw_ratio_avg         =  0.0;
+    double    particle_gen_damp_pp_ratio_avg         =  0.0;
+    double    particle_gen_damp_pw_ratio_avg         =  0.0;
 
-    double    particle_gen_slid_pp_ratio_avg      =  0.0;
-    double    particle_gen_slid_pw_ratio_avg      =  0.0;
-    double    particle_gen_roll_pp_ratio_avg      =  0.0;
-    double    particle_gen_roll_pw_ratio_avg      =  0.0;
-    double    particle_gen_damp_pp_ratio_avg      =  0.0;
-    double    particle_gen_damp_pw_ratio_avg      =  0.0;
-
-    double    total_energy_potential_gravity      =  0.0;
-    double    total_energy_potential_elastic      =  0.0;
-    double    total_energy_kinetic_translation    =  0.0;
-    double    total_energy_kinetic_rotation       =  0.0;
-    double    total_energy                        =  0.0;
+    double    total_energy_potential_gravity         =  0.0;
+    double    total_energy_potential_elastic         =  0.0;
+    double    total_energy_kinetic_translation       =  0.0;
+    double    total_energy_kinetic_rotation          =  0.0;
+    double    total_energy                           =  0.0;
 
     double    total_accum_energy_dissip_slid         =  0.0;
     double    total_accum_energy_dissip_roll         =  0.0;
@@ -196,9 +171,9 @@ namespace Kratos {
     for (int i = 0; i < num_of_particles; i++) {
       ThermalSphericParticle& particle = dynamic_cast<ThermalSphericParticle&> (*(it+i));
 
-      // Accumulate values
-      const double vol                        = particle.CalculateVolume();
-      const double temp                       = particle.GetGeometry()[0].FastGetSolutionStepValue(TEMPERATURE);
+      const double vol  = particle.CalculateVolume();
+      const double temp = particle.GetGeometry()[0].FastGetSolutionStepValue(TEMPERATURE);
+
       double energy_potential_gravity         = 0.0;
       double energy_potential_elastic         = 0.0;
       double energy_kinetic_translation       = 0.0;
@@ -218,14 +193,22 @@ namespace Kratos {
       particle.Calculate(PARTICLE_INELASTIC_DAMPING_NORMAL_ENERGY,     accum_energy_dissip_damp_normal,  r_process_info);
       particle.Calculate(PARTICLE_INELASTIC_DAMPING_TANGENT_ENERGY,    accum_energy_dissip_damp_tangent, r_process_info);
 
+      const double gen_slid_pp = fabs(particle.mGenerationHeatFlux_slid_particle);
+      const double gen_slid_pw = fabs(particle.mGenerationHeatFlux_slid_wall);
+      const double gen_roll_pp = fabs(particle.mGenerationHeatFlux_roll_particle);
+      const double gen_roll_pw = fabs(particle.mGenerationHeatFlux_roll_wall);
+      const double gen_damp_pp = fabs(particle.mGenerationHeatFlux_damp_particle);
+      const double gen_damp_pw = fabs(particle.mGenerationHeatFlux_damp_wall);
+      const double gen_total   = fabs(particle.mGenerationHeatFlux);
+
       #pragma omp critical
       {
         total_vol += vol;
         if (temp < particle_temp_min) particle_temp_min = temp;
         if (temp > particle_temp_max) particle_temp_max = temp;
-        particle_temp_avg                += temp;
-        particle_temp_dev                += temp * temp;
-        model_temp_avg                   += temp * vol;
+        particle_temp_avg += temp;
+        particle_temp_dev += temp * temp;
+        model_temp_avg    += temp * vol;
 
         total_energy_potential_gravity   += energy_potential_gravity;
         total_energy_potential_elastic   += energy_potential_elastic;
@@ -239,76 +222,23 @@ namespace Kratos {
         total_accum_energy_dissip_damp_normal  += accum_energy_dissip_damp_normal;
         total_accum_energy_dissip_damp_tangent += accum_energy_dissip_damp_tangent;
         total_accum_energy_dissip              += accum_energy_dissip_slid + accum_energy_dissip_roll + accum_energy_dissip_damp;
-      }
 
-      if (mGraph_ParticleHeatFluxContributions) {
-        // Get absolute value of particle heat transfer mechanisms
-        const double flux_conducdir   = fabs(particle.mConductionDirectHeatFlux);
-        const double flux_conducindir = fabs(particle.mConductionIndirectHeatFlux);
-        const double flux_rad         = fabs(particle.mRadiationHeatFlux);
-        const double flux_gen         = fabs(particle.mGenerationHeatFlux);
-        const double flux_conv        = fabs(particle.mConvectionHeatFlux);
-        const double flux_prescsurf   = fabs(particle.mPrescribedHeatFluxSurface);
-        const double flux_prescvol    = fabs(particle.mPrescribedHeatFluxVolume);
-        const double flux_total       = flux_conducdir + flux_conducindir + flux_rad + flux_gen + flux_conv + flux_prescsurf + flux_prescvol;
-
-        // Compute relative contribution of each heat transfer mechanism for current particle
-        if (flux_total != 0.0) {
-          #pragma omp critical
-          {
-            num_ratio_particles_flux++;
-            particle_flux_conducdir_ratio_avg   += flux_conducdir   / flux_total;
-            particle_flux_conducindir_ratio_avg += flux_conducindir / flux_total;
-            particle_flux_rad_ratio_avg         += flux_rad         / flux_total;
-            particle_flux_gen_ratio_avg         += flux_gen         / flux_total;
-            particle_flux_conv_ratio_avg        += flux_conv        / flux_total;
-            particle_flux_prescsurf_ratio_avg   += flux_prescsurf   / flux_total;
-            particle_flux_prescvol_ratio_avg    += flux_prescvol    / flux_total;
-          }
+        if (mGraph_ParticleHeatGenContributions && gen_total != 0.0) {
+          num_ratio_particles_gen++;
+          particle_gen_slid_pp_ratio_avg += gen_slid_pp / gen_total;
+          particle_gen_slid_pw_ratio_avg += gen_slid_pw / gen_total;
+          particle_gen_roll_pp_ratio_avg += gen_roll_pp / gen_total;
+          particle_gen_roll_pw_ratio_avg += gen_roll_pw / gen_total;
+          particle_gen_damp_pp_ratio_avg += gen_damp_pp / gen_total;
+          particle_gen_damp_pw_ratio_avg += gen_damp_pw / gen_total;
         }
       }
-
-      if (mGraph_ParticleHeatGenContributions) {
-        // Get absolute value of particle heat generation mechanisms
-        const double gen_slid_pp = fabs(particle.mGenerationHeatFlux_slid_particle);
-        const double gen_slid_pw = fabs(particle.mGenerationHeatFlux_slid_wall);
-        const double gen_roll_pp = fabs(particle.mGenerationHeatFlux_roll_particle);
-        const double gen_roll_pw = fabs(particle.mGenerationHeatFlux_roll_wall);
-        const double gen_damp_pp = fabs(particle.mGenerationHeatFlux_damp_particle);
-        const double gen_damp_pw = fabs(particle.mGenerationHeatFlux_damp_wall);
-        const double gen_total   = fabs(particle.mGenerationHeatFlux);
-
-        // Compute relative contribution of each heat generation mechanism for current particle
-        if (gen_total != 0.0) {
-          #pragma omp critical
-          {
-            num_ratio_particles_gen++;
-            particle_gen_slid_pp_ratio_avg += gen_slid_pp / gen_total;
-            particle_gen_slid_pw_ratio_avg += gen_slid_pw / gen_total;
-            particle_gen_roll_pp_ratio_avg += gen_roll_pp / gen_total;
-            particle_gen_roll_pw_ratio_avg += gen_roll_pw / gen_total;
-            particle_gen_damp_pp_ratio_avg += gen_damp_pp / gen_total;
-            particle_gen_damp_pw_ratio_avg += gen_damp_pw / gen_total;
-          }
-        }
-      }
-    }
+    } // Loop over particles
 
     // Compute temperature results (avg, dev)
     particle_temp_avg /= num_of_particles;
     model_temp_avg    /= total_vol;
     particle_temp_dev  = sqrt(std::max(0.0, particle_temp_dev / num_of_particles - particle_temp_avg * particle_temp_avg));
-
-    // Compute average of relative contribution of each heat transfer mechanism
-    if (mGraph_ParticleHeatFluxContributions && num_ratio_particles_flux > 0) {
-      particle_flux_conducdir_ratio_avg   /= num_ratio_particles_flux;
-      particle_flux_conducindir_ratio_avg /= num_ratio_particles_flux;
-      particle_flux_rad_ratio_avg         /= num_ratio_particles_flux;
-      particle_flux_gen_ratio_avg         /= num_ratio_particles_flux;
-      particle_flux_conv_ratio_avg        /= num_ratio_particles_flux;
-      particle_flux_prescsurf_ratio_avg   /= num_ratio_particles_flux;
-      particle_flux_prescvol_ratio_avg    /= num_ratio_particles_flux;
-    }
 
     // Compute average of relative contribution of each heat generation mechanism
     if (mGraph_ParticleHeatGenContributions && num_ratio_particles_gen > 0) {
@@ -354,18 +284,6 @@ namespace Kratos {
                          << model_temp_avg
                          << std::endl;
 
-    if (mFile_ParticleHeatFluxContributions.is_open())
-      mFile_ParticleHeatFluxContributions << time_step                           << " "
-                                          << time                                << " "
-                                          << particle_flux_conducdir_ratio_avg   << " "
-                                          << particle_flux_conducindir_ratio_avg << " "
-                                          << particle_flux_rad_ratio_avg         << " "
-                                          << particle_flux_gen_ratio_avg         << " "
-                                          << particle_flux_conv_ratio_avg        << " "
-                                          << particle_flux_prescsurf_ratio_avg   << " "
-                                          << particle_flux_prescvol_ratio_avg
-                                          << std::endl;
-
     if (mFile_ParticleHeatGenContributions.is_open())
       mFile_ParticleHeatGenContributions << time_step                      << " "
                                          << time                           << " "
@@ -408,7 +326,6 @@ namespace Kratos {
     if (mFile_ParticleTempAvg.is_open())               mFile_ParticleTempAvg.close();
     if (mFile_ParticleTempDev.is_open())               mFile_ParticleTempDev.close();
     if (mFile_ModelTempAvg.is_open())                  mFile_ModelTempAvg.close();
-    if (mFile_ParticleHeatFluxContributions.is_open()) mFile_ParticleHeatFluxContributions.close();
     if (mFile_ParticleHeatGenContributions.is_open())  mFile_ParticleHeatGenContributions.close();
     if (mFile_ParticleEnergyContributions.is_open())   mFile_ParticleEnergyContributions.close();
 
