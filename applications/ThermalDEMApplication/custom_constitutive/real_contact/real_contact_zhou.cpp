1da1a009ad95159e7e719022705d1c2d0aacfccc
rlrangel <rrangel@cimne.upc.edu>
2023-12-01 00:34:45 +0100

    Replaced correction factor

--- applications/ThermalDEMApplication/custom_constitutive/real_contact/real_contact_zhou.cpp
+++ applications/ThermalDEMApplication/custom_constitutive/real_contact/real_contact_zhou.cpp
@@ -27,9 +27,31 @@ namespace Kratos {
     const double eff_young_real = particle->ComputeEffectiveYoungReal();
 
     // Adjusted value of contact radius
-    particle->mContactRadiusAdjusted = particle->mContactRadius * pow(eff_young / eff_young_real, 0.2);
+    //particle->mContactRadiusAdjusted = particle->mContactRadius * pow(eff_young / eff_young_real, 0.2);
 
     // Compute adjusted distance/separation from adjusted contact radius
+    //particle->mNeighborDistanceAdjusted   = particle->ComputeDistanceToNeighborAdjusted();
+    //particle->mNeighborSeparationAdjusted = particle->ComputeSeparationToNeighborAdjusted();
+
+    // Porposed
+    if (particle->mNeighborType & PARTICLE_NEIGHBOR) {
+      const double r1           = particle->GetParticleRadius();
+      const double r2           = particle->GetNeighborRadius();
+      const double d_sim        = particle->mNeighborDistance;
+      const double overlap_sim  = r1 + r2 - d_sim;
+      const double overlap_real = pow(eff_young/eff_young_real, 2.0/3.0) * overlap_sim;
+      const double d_real       = r1 + r2 - overlap_real;
+      particle->mContactRadiusAdjusted = sqrt(fabs(r1 * r1 - pow(((r1 * r1 - r2 * r2 + d_real * d_real) / (2.0 * d_real)), 2.0)));
+    }
+    else if (particle->mNeighborType & WALL_NEIGHBOR) {
+      const double r1           = particle->GetParticleRadius();
+      const double d_sim        = particle->mNeighborDistance;
+      const double overlap_sim  = r1 - d_sim;
+      const double overlap_real = pow(eff_young/eff_young_real, 2.0/3.0) * overlap_sim;
+      const double d_real       = r1 - overlap_real;
+      particle->mContactRadiusAdjusted = sqrt(r1 * r1 - d_real * d_real);
+    }
+    
     particle->mNeighborDistanceAdjusted   = particle->ComputeDistanceToNeighborAdjusted();
     particle->mNeighborSeparationAdjusted = particle->ComputeSeparationToNeighborAdjusted();
 
