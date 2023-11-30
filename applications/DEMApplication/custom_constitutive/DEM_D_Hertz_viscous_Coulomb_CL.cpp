b064c2c2a1f2fee7ece1b8ad2f46f5dadb152c63
rlrangel <rrangel@cimne.upc.edu>
2023-11-30 01:33:59 +0100

    Cleaning

--- applications/DEMApplication/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.cpp
+++ applications/DEMApplication/custom_constitutive/DEM_D_Hertz_viscous_Coulomb_CL.cpp
@@ -251,24 +251,20 @@ namespace Kratos {
                                                                             double previous_indentation,
                                                                             double& AuxElasticShearForce,
                                                                             double& MaximumAdmisibleShearForce) {
+        double minoring_factor = 1.0;
+        if (previous_indentation > indentation)
+             minoring_factor = sqrt (indentation / previous_indentation);
 
-        Properties& properties_of_this_contact = element->GetProperties().GetSubProperties(neighbour->GetProperties().Id());
-
-        LocalElasticContactForce[0] = OldLocalElasticContactForce[0] - mKt * LocalDeltDisp[0];
-        LocalElasticContactForce[1] = OldLocalElasticContactForce[1] - mKt * LocalDeltDisp[1];
-
-        if (previous_indentation > indentation) {
-            const double minoring_factor = sqrt (indentation / previous_indentation);
-            LocalElasticContactForce[0] = OldLocalElasticContactForce[0] * minoring_factor - mKt * LocalDeltDisp[0];
-            LocalElasticContactForce[1] = OldLocalElasticContactForce[1] * minoring_factor - mKt * LocalDeltDisp[1];
-        }
+        LocalElasticContactForce[0] = minoring_factor * OldLocalElasticContactForce[0] - mKt * LocalDeltDisp[0];
+        LocalElasticContactForce[1] = minoring_factor * OldLocalElasticContactForce[1] - mKt * LocalDeltDisp[1];
 
         AuxElasticShearForce = sqrt(LocalElasticContactForce[0] * LocalElasticContactForce[0] + LocalElasticContactForce[1] * LocalElasticContactForce[1]);
 
         const double tangential_contact_force_0 = LocalElasticContactForce[0] + ViscoDampingLocalContactForce[0];
         const double tangential_contact_force_1 = LocalElasticContactForce[1] + ViscoDampingLocalContactForce[1];
-        const double ActualTotalShearForce = sqrt(tangential_contact_force_0 * tangential_contact_force_0 + tangential_contact_force_1 * tangential_contact_force_1);
+        const double ActualTotalShearForce      = sqrt(tangential_contact_force_0 * tangential_contact_force_0 + tangential_contact_force_1 * tangential_contact_force_1);
 
+        Properties& properties_of_this_contact = element->GetProperties().GetSubProperties(neighbour->GetProperties().Id());
         const double equiv_tg_of_static_fri_ang = properties_of_this_contact[STATIC_FRICTION];
         MaximumAdmisibleShearForce = normal_contact_force * equiv_tg_of_static_fri_ang;
 
