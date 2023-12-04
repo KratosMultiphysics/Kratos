cc91081fe8f71c350f9bf2772b66cce37cf7d4ae
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2023-12-04 09:38:53 +0100

    Added a unit test for 4-noded (line) micro-climate conditions
    Test whether no errors occur when initializing the solution step.

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_T_micro_climate_flux_condition.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_T_micro_climate_flux_condition.cpp
@@ -165,4 +165,17 @@ KRATOS_TEST_CASE_IN_SUITE(NoErrorWhenInitializingSolutionStepOnThermalMicroClima
     KRATOS_EXPECT_STREQ(error_text.data(), "")
 }
 
+KRATOS_TEST_CASE_IN_SUITE(NoErrorWhenInitializingSolutionStepOnThermalMicroClimateCondition2D4N, KratosGeoMechanicsFastSuite)
+{
+    Model test_model;
+    constexpr auto number_of_nodes = std::size_t{4};
+    auto& r_model_part = CreateDummyModelPartWithNodes(test_model, number_of_nodes);
+    auto  p_properties = CreateDummyConditionProperties(r_model_part);
+    auto  p_condition  = CreateMicroClimateCondition(r_model_part, p_properties);
+
+    const auto error_text = ExecuteInitializeSolutionStep(p_condition, r_model_part.GetProcessInfo());
+
+    KRATOS_EXPECT_STREQ(error_text.data(), "")
+}
+
 }
