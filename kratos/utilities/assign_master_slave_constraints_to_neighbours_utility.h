37240f878d42d71ce260e5280fc3bf354902908a
SADPR <aresdepargas@gmail.com>
2023-12-05 21:12:08 +0100

    Revert "Auto stash before merge of "Kratos_RotatingFrameProcess" and "origin/Kratos_RotatingFrameProcess""
    This reverts commit 5be37fedc1ab27dadab5387178a6f2d58f2e2d63.

--- kratos/utilities/assign_master_slave_constraints_to_neighbours_utility.h
+++ kratos/utilities/assign_master_slave_constraints_to_neighbours_utility.h
@@ -138,29 +138,6 @@ class KRATOS_API(KRATOS_CORE) AssignMasterSlaveConstraintsToNeighboursUtility
       );
 
       /**
-     * @brief Removes duplicate nodes based on their coordinates.
-     * 
-     * This function is intended to filter out nodes with duplicate coordinates 
-     * to avoid errors when using Radial Basis Functions (RBF) that can't handle 
-     * repetitive coordinates. The function updates the provided CloudOfNodesArray 
-     * to only contain unique nodes and also returns a matrix of their coordinates.
-     *
-     * @param CloudOfNodesArray A reference to the container of nodes. This will be updated to only contain unique nodes.
-     * @param rCloudOfNodesCoordinates A reference to a matrix that will be filled with the coordinates of the unique nodes.
-     */
-    void FilterUniqueNodesForRBF(
-        ResultNodesContainerType& CloudOfNodesArray,
-        Matrix& rCloudOfNodesCoordinates
-    );
-
-    void GetDofsAndCoordinatesForCloudOfNodesFlattened(
-        const ResultNodesContainerType& CloudOfNodesArray,
-        const std::vector<std::reference_wrapper<const Kratos::Variable<double>>>& rVariableList,
-        DofPointerVectorType& rCloudOfDofs,
-        Matrix& rCloudOfNodesCoordinates
-    );
-
-      /**
        * @brief Assign Master-Slave Constraints to a set of Nodes.
        * @details Assign Matser-Slave Constraints to a set of Nodes given a radius of influence
        * w.r.t MLS or RBF shape functions.
@@ -178,57 +155,12 @@ class KRATOS_API(KRATOS_CORE) AssignMasterSlaveConstraintsToNeighboursUtility
           double const MinNumOfNeighNodes
       );
 
-      /**
-       * @brief Assign Rotational Master-Slave Constraints to a set of Nodes.
-       * @details This function assigns Master-Slave Constraints to a set of nodes considering rotational effects. 
-       * It takes into account the rotation of the nodes and adjusts the constraints accordingly. The function 
-       * uses MLS or RBF shape functions within a given radius of influence to determine the constraints. 
-       * Additionally, it uses a mapping between ghost nodes and their original counterparts to account for the rotation.
-       * @param pSlaveNodes Nodes to which MasterSlaveConstraints are assigned.
-       * @param Radius Search radius for neighboring nodes.
-       * @param rComputingModelPart Model Part to which MasterSlaveConstraints are applied.
-       * @param rVariableList List of DOFs to assign the MasterSlaveConstraints.
-       * @param MinNumOfNeighNodes Minimum number of neighboring nodes required for the constraint.
-       * @param GhostToOriginalMapping Mapping between ghost nodes and their original counterparts, including the rotation angle.
-       */
-       void AssignRotationalMasterSlaveConstraintsToNodes(
-          NodesContainerType pSlaveNodes,
+      void InterpolateVelocityToNodes(
+          NodesContainerType pDestinationNodes,
           double const Radius,
-          ModelPart& rComputingModelPart,
           const std::vector<std::reference_wrapper<const Kratos::Variable<double>>>& rVariableList,
-          double const MinNumOfNeighNodes,
-          const std::unordered_map<IndexType, std::pair<IndexType, double>>& GhostToOriginalMapping
-       );
-       /**
-       * @brief Adjusts the Master-Slave Constraint for rotational effects.
-       * @details This function modifies the shape functions matrix (weights) of the Master-Slave Constraint 
-       * to account for the rotation of the nodes. Given a rotation angle, it applies the necessary 
-       * trigonometric transformations to the shape functions to ensure that the constraints are correctly 
-       * adjusted for the rotation. This is crucial when dealing with models that have rotational dynamics 
-       * or when considering ghost nodes that represent rotated versions of the original nodes.
-       * @param rotation_angle The angle (in degrees) by which the node has been rotated.
-       * @param shape_matrix The shape functions matrix that needs adjustment for rotation.
-       */
-       void AdjustConstraintForRotation(
-           double rotation_angle,
-           Matrix& shape_matrix
-       );
-
-       void ExtractAnglesFromMapping(
-            const ResultNodesContainerType& Results,
-            const std::unordered_map<IndexType, std::pair<IndexType, double>>& GhostToOriginalMapping,
-            Vector& ListOfAngles
-        );
-
-        void ObtainShapeMatrix(
-            Matrix& shape_matrix_x,
-            Matrix& shape_matrix_y,
-            Matrix& shape_matrix_z,
-            Vector& rN_container,
-            Vector& ListOfAngles
-        );
-
-
+          double const MinNumOfNeighNodes
+      );
 
       ///@}
       ///@name Input and output
