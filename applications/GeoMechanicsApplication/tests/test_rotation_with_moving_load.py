a3ef1f33986620cc99551e93344ee52afdfbb566
Richard Faasse <richard.faasse@deltares.nl>
2023-12-06 14:38:21 +0100

    Made changes to the test such that it fails when rotation is not added to the scheme

---
+++ applications/GeoMechanicsApplication/tests/test_rotation_with_moving_load.py
@@ -0,0 +1,30 @@
+import os
+
+import KratosMultiphysics                as Kratos
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+import test_helper
+
+result_extension = ".post.res"
+
+class KratosGeoMechanicsRotationWithMovingLoadTests(KratosUnittest.TestCase):
+
+    def test_rotation_with_moving_load(self):
+        test_name = "test_rotation_with_moving_load"
+        file_path = test_helper.get_file_path(test_name)
+
+        # run simulation
+        simulation = test_helper.run_kratos(file_path)
+        res_path = os.path.join(file_path, test_name + result_extension)
+
+        reader = test_helper.GiDOutputFileReader()x
+        simulation_output = reader.read_output_from(res_path)
+        displacements = simulation_output["results"]["ROTATION"]
+
+        # Validation for Time = 0.02
+        values_time_0_02 = reader.get_values_at_time(1.0, displacements)
+        value_for_node = reader.get_value_at_node(1, values_time_0_02)
+        # Assert
+        self.assertAlmostEqual(-0.000177858, value_for_node[2])
+
+if __name__ == '__main__':
+    KratosUnittest.main()
