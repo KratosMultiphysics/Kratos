4b81b73f2e8639b23d959c25fae85d4584d53fcf
Philipp <philipp.bucher@tum.de>
2023-12-11 00:04:33 +0100

    fixed tests

--- kratos/tests/test_ci_utilities.py
+++ kratos/tests/test_ci_utilities.py
@@ -3,7 +3,9 @@ from typing import List
 from unittest.mock import patch
 
 import KratosMultiphysics.KratosUnittest as KratosUnittest
-from KratosMultiphysics import ci_utilities
+from KratosMultiphysics.testing import ci_utilities
+
+PATCH_NAME: str = "KratosMultiphysics.testing.ci_utilities.changed_files"
 
 FILES_CHANGED_1: List[Path] = [Path("kratos/linear_solvers/amgcl_solver.h")]
 
@@ -18,7 +20,7 @@ FILES_CHANGED_3: List[Path] = [
 
 class TestCIUtilities(KratosUnittest.TestCase):
     def test_core_changes(self):
-        with patch("ci_utilities.changed_files", return_value=FILES_CHANGED_1):
+        with patch(PATCH_NAME, return_value=FILES_CHANGED_1):
             self.assertCountEqual(ci_utilities.get_changed_applications(), set())
             self.assertTrue(ci_utilities.is_core_changed())
             self.assertFalse(ci_utilities.is_mpi_core_changed())
@@ -26,16 +28,16 @@ class TestCIUtilities(KratosUnittest.TestCase):
             self.assertFalse(ci_utilities.are_only_python_files_changed())
 
     def test_mpi_core_changes(self):
-        with patch("ci_utilities.changed_files", return_value=FILES_CHANGED_2):
-            self.assertListEqual(ci_utilities.get_changed_applications(), set())
+        with patch(PATCH_NAME, return_value=FILES_CHANGED_2):
+            self.assertCountEqual(ci_utilities.get_changed_applications(), set())
             self.assertTrue(ci_utilities.is_core_changed())
             self.assertTrue(ci_utilities.is_mpi_core_changed())
             self.assertCountEqual(ci_utilities.get_changed_files_extensions(), {".py"})
             self.assertTrue(ci_utilities.are_only_python_files_changed())
 
     def test_application_changes(self):
-        with patch("ci_utilities.changed_files", return_value=FILES_CHANGED_3):
-            self.assertListEqual(
+        with patch(PATCH_NAME, return_value=FILES_CHANGED_3):
+            self.assertCountEqual(
                 ci_utilities.get_changed_applications(),
                 {"FluidDynamicsApplication", "LinearSolversApplication", "StructuralMechanicsApplication"},
             )
@@ -43,3 +45,7 @@ class TestCIUtilities(KratosUnittest.TestCase):
             self.assertFalse(ci_utilities.is_mpi_core_changed())
             self.assertCountEqual(ci_utilities.get_changed_files_extensions(), {".cpp", ".h", ".txt"})
             self.assertFalse(ci_utilities.are_only_python_files_changed())
+
+
+if __name__ == "__main__":
+    KratosUnittest.main()
