3cfd240e05d733851bf824b193893fef46a0edf2
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2023-12-19 15:45:18 +0100

    thermal simo ju added

--- applications/ConstitutiveLawsApplication/custom_constitutive/thermal/auxiliary_files/thermal_yield_surfaces/thermal_simo_ju_yield_surface.h
+++ applications/ConstitutiveLawsApplication/custom_constitutive/thermal/auxiliary_files/thermal_yield_surfaces/thermal_simo_ju_yield_surface.h
@@ -59,7 +59,7 @@ class ThermalSimoJuYieldSurface
     /// The type of potential plasticity
     using PlasticPotentialType = TPlasticPotentialType;
 
-    using BaseType = RankineYieldSurface<TPlasticPotentialType>;
+    using BaseType = SimoJuYieldSurface<TPlasticPotentialType>;
     using ThermalVonMisesType = ThermalVonMisesYieldSurface<TPlasticPotentialType>;
 
     /// The Plastic potential already defines the working simension size
@@ -163,39 +163,45 @@ class ThermalSimoJuYieldSurface
         const double CharacteristicLength
         )
     {
-        const Properties& r_material_properties = rValues.GetMaterialProperties();
+        const auto& r_material_properties = rValues.GetMaterialProperties();
 
-        const double fracture_energy = r_material_properties[FRACTURE_ENERGY];
-        const bool has_symmetric_yield_stress = r_material_properties.Has(YIELD_STRESS);
-        const double yield_compression = has_symmetric_yield_stress ? r_material_properties[YIELD_STRESS] : r_material_properties[YIELD_STRESS_COMPRESSION];
-        const double yield_tension = has_symmetric_yield_stress ? r_material_properties[YIELD_STRESS] : r_material_properties[YIELD_STRESS_TENSION];
+        const auto &r_geom = rValues.GetElementGeometry();
+        const auto &r_N = rValues.GetShapeFunctionsValues();
+        const auto &r_process_info = rValues.GetProcessInfo();
+
+        const double fracture_energy = r_material_properties.GetValue(FRACTURE_ENERGY, r_geom, r_N, r_process_info);
+        const double young_modulus   = r_material_properties.GetValue(YOUNG_MODULUS, r_geom, r_N, r_process_info);
+        const double yield_compression = r_material_properties.Has(YIELD_STRESS) ? r_material_properties.GetValue(YIELD_STRESS, r_geom, r_N, r_process_info) : r_material_properties.GetValue(YIELD_STRESS_COMPRESSION, r_geom, r_N, r_process_info);
+        const double yield_tension   = r_material_properties.Has(YIELD_STRESS) ? r_material_properties.GetValue(YIELD_STRESS, r_geom, r_N, r_process_info) : r_material_properties.GetValue(YIELD_STRESS_TENSION, r_geom, r_N, r_process_info);
         const double n = yield_compression / yield_tension;
 
         if (r_material_properties[SOFTENING_TYPE] == static_cast<int>(SofteningType::Exponential)) {
             rAParameter = 1.0 / (fracture_energy * n * n / (CharacteristicLength * std::pow(yield_compression, 2)) - 0.5);
             KRATOS_ERROR_IF(rAParameter < 0.0) << "Fracture energy is too low, increase FRACTURE_ENERGY..." << std::endl;
-        } else { // linear
+        } else if (r_material_properties[SOFTENING_TYPE] == static_cast<int>(SofteningType::Linear)) { // linear
             rAParameter = -std::pow(yield_compression, 2) / (2.0 * fracture_energy * n * n / CharacteristicLength);
+        } else {
+            rAParameter = 0.0;
         }
     }
 
     /**
      * @brief This method calculates the derivative of the plastic potential DG/DS
-     * @param rPredictiveStressVector The stress vector
+     * @param rStressVector The stress vector
      * @param Deviator The deviatoric part of the stress vector
      * @param J2 The second invariant of the Deviator
      * @param rDerivativePlasticPotential The derivative of the plastic potential
      * @param rValues Parameters of the constitutive law
      */
     static void CalculatePlasticPotentialDerivative(
-        const array_1d<double, VoigtSize>& rPredictiveStressVector,
+        const array_1d<double, VoigtSize>& rStressVector,
         const array_1d<double, VoigtSize>& rDeviator,
         const double J2,
         array_1d<double, VoigtSize>& rDerivativePlasticPotential,
         ConstitutiveLaw::Parameters& rValues
         )
     {
-        TPlasticPotentialType::CalculatePlasticPotentialDerivative(rPredictiveStressVector, rDeviator, J2, rDerivativePlasticPotential, rValues);
+        BaseType::CalculatePlasticPotentialDerivative(rStressVector, rDeviator, J2, rDerivativePlasticPotential, rValues);
     }
 
     /**
@@ -226,24 +232,7 @@ class ThermalSimoJuYieldSurface
      */
     static int Check(const Properties& rMaterialProperties)
     {
-        if (!rMaterialProperties.Has(YIELD_STRESS)) {
-            KRATOS_ERROR_IF_NOT(rMaterialProperties.Has(YIELD_STRESS_TENSION)) << "YIELD_STRESS_TENSION is not a defined value" << std::endl;
-            KRATOS_ERROR_IF_NOT(rMaterialProperties.Has(YIELD_STRESS_COMPRESSION)) << "YIELD_STRESS_COMPRESSION is not a defined value" << std::endl;
-
-            const double yield_compression = rMaterialProperties[YIELD_STRESS_COMPRESSION];
-            const double yield_tension = rMaterialProperties[YIELD_STRESS_TENSION];
-
-            KRATOS_ERROR_IF(yield_compression < tolerance) << "Yield stress in compression almost zero or negative, include YIELD_STRESS_COMPRESSION in definition";
-            KRATOS_ERROR_IF(yield_tension < tolerance) << "Yield stress in tension almost zero or negative, include YIELD_STRESS_TENSION in definition";
-        } else {
-            const double yield_stress = rMaterialProperties[YIELD_STRESS];
-
-            KRATOS_ERROR_IF(yield_stress < tolerance) << "Yield stress almost zero or negative, include YIELD_STRESS in definition";
-        }
-        KRATOS_ERROR_IF_NOT(rMaterialProperties.Has(FRACTURE_ENERGY)) << "FRACTURE_ENERGY is not a defined value" << std::endl;
-        KRATOS_ERROR_IF_NOT(rMaterialProperties.Has(YOUNG_MODULUS)) << "YOUNG_MODULUS is not a defined value" << std::endl;
-
-        return TPlasticPotentialType::Check(rMaterialProperties);
+        return BaseType::Check(rMaterialProperties);
     }
 
     /**
@@ -251,7 +240,7 @@ class ThermalSimoJuYieldSurface
      */
     static bool IsWorkingWithTensionThreshold()
     {
-        return false;
+        return BaseType::IsWorkingWithTensionThreshold();
     }
 
     /**
@@ -259,10 +248,10 @@ class ThermalSimoJuYieldSurface
      */
     static double GetScaleFactorTension(const Properties& rMaterialProperties)
     {
-        const double yield_compression = rMaterialProperties.Has(YIELD_STRESS) ? rMaterialProperties[YIELD_STRESS] : rMaterialProperties[YIELD_STRESS_COMPRESSION];
-        const double yield_tension = rMaterialProperties.Has(YIELD_STRESS) ? rMaterialProperties[YIELD_STRESS] : rMaterialProperties[YIELD_STRESS_TENSION];
-        return std::sqrt(rMaterialProperties[YOUNG_MODULUS]) * yield_tension / yield_compression;
+        // The reduction or increase of yield due to temperature is proportional in tension and compression so...
+        return BaseType::GetScaleFactorTension(rMaterialProperties);
     }
+
     ///@}
     ///@name Access
     ///@{
