9f163e1860995e0b5373487aa475aa3453d46d91
uxuech <73470625+uxuech@users.noreply.github.com>
2023-12-29 12:10:09 +0100

    Update applications/FluidDynamicsHydraulicsApplication/custom_utilities/hydraulic_fluid_auxiliary_utilities.cpp
    Co-authored-by: Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>

--- applications/FluidDynamicsHydraulicsApplication/custom_utilities/hydraulic_fluid_auxiliary_utilities.cpp
+++ applications/FluidDynamicsHydraulicsApplication/custom_utilities/hydraulic_fluid_auxiliary_utilities.cpp
@@ -7,7 +7,7 @@
 //  License:         BSD License
 //                   Kratos default license: kratos/license.txt
 //
-//  Main authors:   Uxue Chasco
+//  Main authors:    Uxue Chasco
 //
 
 // System includes
