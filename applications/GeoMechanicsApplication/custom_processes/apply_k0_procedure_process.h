0e505c69b17e42c25cbba640b525fdf4313e4260
Richard Faasse <richard.faasse@deltares.nl>
2024-01-11 13:20:18 +0100

    Formatted the header file

--- applications/GeoMechanicsApplication/custom_processes/apply_k0_procedure_process.h
+++ applications/GeoMechanicsApplication/custom_processes/apply_k0_procedure_process.h
@@ -17,9 +17,9 @@
 #include <iostream>
 
 // Project includes
+#include "includes/element.h"
 #include "includes/model_part.h"
 #include "utilities/math_utils.h"
-#include "includes/element.h"
 
 // Application includes
 #include "geo_mechanics_application_variables.h"
@@ -32,24 +32,19 @@ namespace Kratos
 
 class KRATOS_API(GEO_MECHANICS_APPLICATION) ApplyK0ProcedureProcess : public Process
 {
-  public:
-
+public:
     KRATOS_CLASS_POINTER_DEFINITION(ApplyK0ProcedureProcess);
 
-    ApplyK0ProcedureProcess(ModelPart&  model_part,
-                           const Parameters& );
-
+    ApplyK0ProcedureProcess(ModelPart& model_part, const Parameters&);
     ~ApplyK0ProcedureProcess() override = default;
 
     void ExecuteFinalizeSolutionStep() override;
-
     std::string Info() const override;
 
-  private:
-      ModelPart& mrModelPart;
+private:
+    ModelPart& mrModelPart;
 
-      void CalculateK0Stresses(Element& rElement);
-
+    void CalculateK0Stresses(Element& rElement);
 };
 
-}
+} // namespace Kratos
