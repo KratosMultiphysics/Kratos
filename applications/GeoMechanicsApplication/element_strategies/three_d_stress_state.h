87a8ba98184d6f51a60615e7f31e7e5419163043
Richard Faasse <richard.faasse@deltares.nl>
2024-01-18 16:24:52 +0100

    Remove redundant access specifier

--- applications/GeoMechanicsApplication/element_strategies/three_d_stress_state.h
+++ applications/GeoMechanicsApplication/element_strategies/three_d_stress_state.h
@@ -20,13 +20,11 @@ namespace Kratos
 class ThreeDStressState : public StressStateStrategy
 {
 public:
+    void CalculateBMatrix(Matrix& rB, const Matrix& GradNpT, const Vector&, const Geometry<Node>& rGeometry) override;
     double CalculateIntegrationCoefficient(const Geometry<Node>::IntegrationPointsArrayType& IntegrationPoints,
                                            unsigned int PointNumber,
                                            double detJ,
                                            const Geometry<Node>& rGeometry) override;
-
-public:
-    void CalculateBMatrix(Matrix& rB, const Matrix& GradNpT, const Vector&, const Geometry<Node>& rGeometry) override;
 };
 
 } // namespace Kratos
