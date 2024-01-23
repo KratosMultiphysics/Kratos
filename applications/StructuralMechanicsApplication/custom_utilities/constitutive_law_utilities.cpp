44ce981255dd45e91010b76176355620b3c8fd9d
Mohammad Azizpooryan <mohammad.azizpooryan@upc.edu>
2024-01-23 12:35:34 +0100

    "hello"

--- applications/StructuralMechanicsApplication/custom_utilities/constitutive_law_utilities.cpp
+++ applications/StructuralMechanicsApplication/custom_utilities/constitutive_law_utilities.cpp
@@ -24,7 +24,7 @@ namespace Kratos
 
 /***********************************************************************************/
 /***********************************************************************************/
-
+"HELLO WORLD"
 template<SizeType TVoigtSize>
 void ConstitutiveLawUtilities<TVoigtSize>::CalculateGreenLagrangianStrain(
     const MatrixType& rCauchyTensor,
