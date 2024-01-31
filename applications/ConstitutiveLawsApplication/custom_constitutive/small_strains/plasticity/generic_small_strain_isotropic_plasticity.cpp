2105d4fde53befdde54a17b9c7d536a41351c306
Alejandro <alejandro.cornejo23@gmail.com>
2024-01-31 11:11:54 +0400

    style

--- applications/ConstitutiveLawsApplication/custom_constitutive/small_strains/plasticity/generic_small_strain_isotropic_plasticity.cpp
+++ applications/ConstitutiveLawsApplication/custom_constitutive/small_strains/plasticity/generic_small_strain_isotropic_plasticity.cpp
@@ -358,7 +358,7 @@ void GenericSmallStrainIsotropicPlasticity<TConstLawIntegratorType>::FinalizeMat
             plastic_dissipation, plastic_strain_increment,
             r_constitutive_matrix, plastic_strain, rValues,
             characteristic_length);
-        BaseType::CalculateElasticMatrix( r_constitutive_matrix, rValues);
+        BaseType::CalculateElasticMatrix(r_constitutive_matrix, rValues);
     }
 
     mPlasticDissipation = plastic_dissipation;
