1620b650b203cecf878dc09c6dc0dd1edd3e22bf
Richard Faasse <richard.faasse@deltares.nl>
2024-02-08 14:32:05 +0100

    Fix wrong rename

--- applications/GeoMechanicsApplication/custom_elements/updated_lagrangian_U_Pw_diff_order_element.hpp
+++ applications/GeoMechanicsApplication/custom_elements/updated_lagrangian_U_Pw_diff_order_element.hpp
@@ -67,7 +67,7 @@ public:
     /// The definition of the sizetype
     using SizeType = std::size_t;
     using SmallStrainUPwDiffOrderElement::AssembleUBlockMatrix;
-    using SmallStrainUPwDiffOrderElement::CalculateCauchyStrain;
+    using SmallStrainUPwDiffOrderElement::CalculateGreenLagrangeStrain;
     using SmallStrainUPwDiffOrderElement::CalculateCauchyStrain;
     using SmallStrainUPwDiffOrderElement::CalculateDerivativesOnInitialConfiguration;
     using SmallStrainUPwDiffOrderElement::mConstitutiveLawVector;
