d117cf938c8544b51b7e0142ef169c18e0412600
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2024-02-20 09:31:40 +0100

    test_generalized_newmark_scheme restored with a concrete implementation of the abstract class.

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_generalized_newmark_scheme.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_generalized_newmark_scheme.cpp
@@ -10,27 +10,43 @@
 //  Main authors:    Richard Faasse
 //
 
-//#include "custom_strategies/schemes/generalized_newmark_scheme.hpp"
-#include "custom_strategies/schemes/newmark_quasistatic_Pw_scheme.hpp"
-//#include "geo_mechanics_application_variables.h"
+#include "custom_strategies/schemes/generalized_newmark_scheme.hpp"
 #include "spaces/ublas_space.h"
 #include "testing/testing.h"
 
 using namespace Kratos;
 using SparseSpaceType = UblasSpace<double, CompressedMatrix, Vector>;
-using LocalSpaceType = UblasSpace<double, Matrix, Vector>;
+using LocalSpaceType  = UblasSpace<double, Matrix, Vector>;
 
-namespace Kratos::Testing {
+namespace Kratos::Testing
+{
+
+// We need this class to test all non-abstract functions of the
+// GeneralizedNewmarkScheme class. We cannot use the
+// GeneralizedNewmarkScheme class directly, because it is abstract.
+class ConcreteGeneralizedNewmarkScheme : public GeneralizedNewmarkScheme<SparseSpaceType, LocalSpaceType>
+{
+public:
+    ConcreteGeneralizedNewmarkScheme(const std::vector<FirstOrderScalarVariable>& rFirstOrderScalarVariables,
+                                     double theta)
+        : GeneralizedNewmarkScheme<SparseSpaceType, LocalSpaceType>(rFirstOrderScalarVariables, theta)
+    {
+    }
+
+protected:
+    void UpdateVariablesDerivatives(ModelPart& rModelPart) override
+    {
+        // Intentionally left empty
+    }
+};
 
 KRATOS_TEST_CASE_IN_SUITE(ForInvalidTheta_CheckNewmarkScheme_Throws, KratosGeoMechanicsFastSuite)
 {
     constexpr double invalid_theta = -2.0;
-    //using SchemeType = GeneralizedNewmarkScheme<SparseSpaceType, LocalSpaceType>;
-    using SchemeType = NewmarkQuasistaticPwScheme<SparseSpaceType, LocalSpaceType>;
+    using SchemeType               = ConcreteGeneralizedNewmarkScheme;
 
-    KRATOS_EXPECT_EXCEPTION_IS_THROWN(
-        SchemeType scheme(invalid_theta),
-        "Theta must be larger than zero, but got -2")
+    KRATOS_EXPECT_EXCEPTION_IS_THROWN(SchemeType scheme({}, invalid_theta),
+                                      "Theta must be larger than zero, but got -2")
 }
 
 } // namespace Kratos::Testing
