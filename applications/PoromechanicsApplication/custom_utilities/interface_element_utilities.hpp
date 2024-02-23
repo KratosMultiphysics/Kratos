7bde698179e9e2fde24884385e0b75aed20625cc
xaviertort <xavier.tort.farres@estudiantat.upc.edu>
2024-02-23 14:28:22 +0100

    Create AddInitialInterfaceStresses2D utility

--- applications/PoromechanicsApplication/custom_utilities/interface_element_utilities.hpp
+++ applications/PoromechanicsApplication/custom_utilities/interface_element_utilities.hpp
@@ -17,6 +17,11 @@
 
 // Project includes
 #include "includes/element.h"
+#include "includes/serializer.h"
+#include "includes/constitutive_law.h"
+
+// Application includes
+#include "poromechanics_application_variables.h"
 
 namespace Kratos
 {
@@ -197,6 +202,71 @@ public:
         rPermeabilityMatrix(2,2) = JointWidth*JointWidth/12.0;
     }
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    static inline void AddInitialInterfaceStresses2D(Vector& rStressVector,
+                                                        ConstitutiveLaw::Parameters& rValues,
+                                                        const Element::GeometryType& geometry)
+    {
+        // Obtain necessary variables
+        const Vector& N = rValues.GetShapeFunctionsValues();
+        const unsigned int number_of_nodes = geometry.size();
+        const unsigned int dimension = 2;
+        const unsigned int strainSize = 3;   
+
+        // Create components to initialise the stresses
+        Vector nodal_initial_stress_vector(strainSize);
+        Matrix nodal_initial_stress_tensor(dimension, dimension); 
+        Vector gp_initial_stress_vector(strainSize);
+        noalias(gp_initial_stress_vector) = ZeroVector(strainSize);
+        Vector LocalInitialStress(dimension);
+        noalias(LocalInitialStress) = ZeroVector(dimension);
+
+        // Contribution of each of the nodal values to the corresponding GP
+        for (unsigned int i = 0; i < number_of_nodes; i++) {
+            const Matrix& r_initial_stress_tensor = geometry[i].GetSolutionStepValue(INITIAL_STRESS_TENSOR);
+            for(unsigned int j=0; j < dimension; j++) {
+                for(unsigned int k=0; k < dimension; k++) {
+                    nodal_initial_stress_tensor(j,k) = r_initial_stress_tensor(j,k);
+                }
+            }
+            noalias(nodal_initial_stress_vector) = MathUtils<double>::StressTensorToVector(nodal_initial_stress_tensor);
+
+            for(unsigned int j=0; j < strainSize; j++){
+                gp_initial_stress_vector[j] += N[i] * nodal_initial_stress_vector[j];
+            }
+        }
+
+        // Define mid-plane points for quadrilateral_interface_2d_4
+        array_1d<double, 3> pmid0;
+        array_1d<double, 3> pmid1;
+        noalias(pmid0) = 0.5 * (geometry.GetPoint( 0 ) + geometry.GetPoint( 3 ));
+        noalias(pmid1) = 0.5 * (geometry.GetPoint( 1 ) + geometry.GetPoint( 2 ));
+
+        // Unitary vector in local x direction
+        array_1d<double, 3> Vx;
+        noalias(Vx) = pmid1 - pmid0;
+        double inv_norm_x = 1.0/norm_2(Vx);
+        Vx[0] *= inv_norm_x; // cos
+        Vx[1] *= inv_norm_x; // sin
+
+        // Define and assign the rotation matrix
+        Matrix RotationInterface(dimension,strainSize);
+        RotationInterface(0,0) = -Vx[1]*Vx[0];
+        RotationInterface(0,1) = Vx[1]*Vx[0];
+        RotationInterface(0,2) = Vx[0]*Vx[0] - Vx[1]*Vx[1];
+        RotationInterface(1,0) = Vx[1]*Vx[1];
+        RotationInterface(1,1) = Vx[0]*Vx[0];
+        RotationInterface(1,2) = -2*Vx[1]*Vx[0];
+    
+        // Local stress vector
+        noalias(LocalInitialStress) = prod(RotationInterface, trans(gp_initial_stress_vector));
+    
+        // Update the stress vector
+        rStressVector[0] += LocalInitialStress[0];
+        rStressVector[1] += LocalInitialStress[1];
+
+    }
 }; /* Class InterfaceElementUtilities*/
 } /* namespace Kratos.*/
 
