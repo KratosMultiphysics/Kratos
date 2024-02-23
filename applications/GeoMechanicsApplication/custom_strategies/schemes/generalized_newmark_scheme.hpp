2141b9997f6a4561b4f0e65e2349efecbee7e90b
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2024-02-23 09:32:23 +0100

    To restore the nodal_smoothing

--- applications/GeoMechanicsApplication/custom_strategies/schemes/generalized_newmark_scheme.hpp
+++ applications/GeoMechanicsApplication/custom_strategies/schemes/generalized_newmark_scheme.hpp
@@ -53,6 +53,51 @@ public:
     {
     }
 
+    void FinalizeSolutionStep(ModelPart& rModelPart, TSystemMatrixType& A, TSystemVectorType& Dx, TSystemVectorType& b) override
+    {
+        KRATOS_TRY
+
+        if (rModelPart.GetProcessInfo()[NODAL_SMOOTHING]) {
+            const unsigned int dim = rModelPart.GetProcessInfo()[DOMAIN_SIZE];
+            const auto stress_tensor_size = dim == N_DIM_3D ? STRESS_TENSOR_SIZE_3D : STRESS_TENSOR_SIZE_2D;
+
+            // Clear nodal variables
+            block_for_each(rModelPart.Nodes(), [&stress_tensor_size](Node& rNode) {
+                rNode.FastGetSolutionStepValue(NODAL_AREA) = 0.0;
+                Matrix& r_nodal_stress = rNode.FastGetSolutionStepValue(NODAL_CAUCHY_STRESS_TENSOR);
+                if (r_nodal_stress.size1() != stress_tensor_size)
+                    r_nodal_stress.resize(stress_tensor_size, stress_tensor_size, false);
+                noalias(r_nodal_stress) = ZeroMatrix(stress_tensor_size, stress_tensor_size);
+                rNode.FastGetSolutionStepValue(NODAL_DAMAGE_VARIABLE) = 0.0;
+                rNode.FastGetSolutionStepValue(NODAL_JOINT_AREA)      = 0.0;
+                rNode.FastGetSolutionStepValue(NODAL_JOINT_WIDTH)     = 0.0;
+                rNode.FastGetSolutionStepValue(NODAL_JOINT_DAMAGE)    = 0.0;
+            });
+
+            this->FinalizeSolutionStepActiveEntities(rModelPart, A, Dx, b);
+
+            // Compute smoothed nodal variables
+            block_for_each(rModelPart.Nodes(), [](Node& rNode) {
+                if (const double& nodal_area = rNode.FastGetSolutionStepValue(NODAL_AREA); nodal_area > 1.0e-20) {
+                    const double inv_nodal_area = 1.0 / nodal_area;
+                    rNode.FastGetSolutionStepValue(NODAL_CAUCHY_STRESS_TENSOR) *= inv_nodal_area;
+                    rNode.FastGetSolutionStepValue(NODAL_DAMAGE_VARIABLE) *= inv_nodal_area;
+                }
+
+                if (const double& nodal_joint_area = rNode.FastGetSolutionStepValue(NODAL_JOINT_AREA);
+                    nodal_joint_area > 1.0e-20) {
+                    const double inv_nodal_joint_area = 1.0 / nodal_joint_area;
+                    rNode.FastGetSolutionStepValue(NODAL_JOINT_WIDTH) *= inv_nodal_joint_area;
+                    rNode.FastGetSolutionStepValue(NODAL_JOINT_DAMAGE) *= inv_nodal_joint_area;
+                }
+            });
+        } else {
+            this->FinalizeSolutionStepActiveEntities(rModelPart, A, Dx, b);
+        }
+
+        KRATOS_CATCH("")
+    }
+
 protected:
     inline void SetTimeFactors(ModelPart& rModelPart) override
     {
