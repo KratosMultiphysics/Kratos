d9e9284cb723b3e5a399f6805789164998181057
Vicente Mataix Ferrandiz <vmataix@altair.com>
2024-02-28 15:26:41 +0100

    Adding python test

--- kratos/tests/test_KratosCore.py
+++ kratos/tests/test_KratosCore.py
@@ -90,6 +90,7 @@ import test_controllers
 import test_stl_io
 import test_find_conditions_neighbours_process
 import test_calculate_nodal_distance_to_skin_process
+import test_compute_nodal_gradient_process
 
 # Import modules required for sequential orchestrator test
 from test_sequential_orchestrator import EmptyAnalysisStage
@@ -204,6 +205,7 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_stl_io.TestStlIO]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_find_conditions_neighbours_process.TestFindConditionsNeighboursProcess]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_calculate_nodal_distance_to_skin_process.TestCalculateNodalDistanceToSkinProcessCoarseSphere]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_compute_nodal_gradient_process.TestComputeNodalGradientProcessCoarseSphere]))
 
     if sympy_available:
         smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_sympy_fe_utilities.TestSympyFEUtilities]))
