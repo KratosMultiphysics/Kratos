37ecfd370c6335f9982ac564a28800bfd2b7b159
Alejandro <alejandro.cornejo23@gmail.com>
2024-02-28 11:05:30 +0400

    .

--- kratos/solving_strategies/strategies/line_search_strategy.h
+++ kratos/solving_strategies/strategies/line_search_strategy.h
@@ -308,12 +308,12 @@ public:
         Parameters default_parameters = Parameters(R"(
         {
             "name"                       : "line_search_strategy",
-            "max_line_search_iterations" : 25,
+            "max_line_search_iterations" : 30,
             "first_alpha_value"          : 0.5,
-            "second_alpha_value"         : 1.0,
-            "min_alpha"                  : 0.1,
-            "max_alpha"                  : 2.0,
-            "line_search_tolerance"      : 0.01
+            "second_alpha_value"         : 0.8,
+            "min_alpha"                  : 0.01,
+            "max_alpha"                  : 10.0,
+            "line_search_tolerance"      : 0.001
         })");
 
         // Getting base class default parameters
