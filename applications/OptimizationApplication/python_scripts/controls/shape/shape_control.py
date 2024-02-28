b90ae0c03b6fb6409cb6cf0a6b422b93ac6f62ae
sunethwarna <suneth.warna@gmail.com>
2024-02-28 12:55:36 +0100

    suggested shape control

--- applications/OptimizationApplication/python_scripts/controls/shape/shape_control.py
+++ applications/OptimizationApplication/python_scripts/controls/shape/shape_control.py
@@ -1,83 +1,124 @@
-# ==============================================================================
-#  KratosOptimizationApplication
-#
-#  License:         BSD License
-#                   license: OptimizationApplication/license.txt
-#
-#  Main authors:    Reza Najian Asl, https://github.com/RezaNajian
-#
-# ==============================================================================
+from typing import Optional
 
-import KratosMultiphysics as KM
-import KratosMultiphysics.OptimizationApplication as KOA
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.OptimizationApplication as KratosOA
+from KratosMultiphysics.OptimizationApplication.controls.control import Control
+from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import ContainerExpressionTypes
+from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import SupportedSensitivityFieldVariableTypes
+from KratosMultiphysics.OptimizationApplication.utilities.helper_utilities import IsSameContainerExpression
+from KratosMultiphysics.OptimizationApplication.utilities.model_part_utilities import ModelPartOperation
+from KratosMultiphysics.OptimizationApplication.utilities.component_data_view import ComponentDataView
+from KratosMultiphysics.OptimizationApplication.utilities.optimization_problem import OptimizationProblem
+from KratosMultiphysics.OptimizationApplication.filtering.filtering_factory import Factory as FilterFactory
+from KratosMultiphysics.OptimizationApplication.utilities.logger_utilities import time_decorator
 
-class ShapeControl():
-    def __init__(self, name, model, settings):
-        
-        self.name = name
-        self.type = "shape"
+def Factory(model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem) -> Control:
+    if not parameters.Has("name"):
+        raise RuntimeError(f"VertexMorphingShapeControl instantiation requires a \"name\" in parameters [ parameters = {parameters}].")
+    if not parameters.Has("settings"):
+        raise RuntimeError(f"VertexMorphingShapeControl instantiation requires a \"settings\" in parameters [ parameters = {parameters}].")
+
+    return ShapeControl(parameters["name"].GetString(), model, parameters["settings"], optimization_problem)
+
+class ShapeControl(Control):
+    """Node-based shape control using implicit and explicit Vertex Morphing techniques
+
+    This is filtering-based discrete shape control which parametrizes and controls
+    discrete shell and solid geometries.
+
+    """
+    def __init__(self, name: str, model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem):
+        super().__init__(name)
+
+        self.parameters = parameters
         self.model = model
-        self.settings = settings
-        self.controlling_objects = self.settings["controlling_objects"].GetStringArray() 
-        self.auxiliary_field = False
+        self.optimization_problem = optimization_problem
 
-        self.control_variable_name = "CX"
-        self.control_update_name = "D_CX"
-        self.vector_fields = ["CX","D_CX","D_X"]
-        self.scalar_fields = []
-        
-        if self.project_to_normal:
-            self.vector_fields.append("NORMAL")
-            self.scalar_fields.append("NODAL_AREA")
+        default_settings = Kratos.Parameters("""{
+            "controlled_model_part_names": [],
+            "filter_settings"            : {},
+            "use_mesh_motion_solver"     : true,
+            "mesh_motion_settings"       : {},
+            "output_all_fields"          : false
+        }""")
 
-        if self.auxiliary_field:
-            self.vector_fields.append("AUXILIARY_FIELD")
-        
+        self.parameters.ValidateAndAssignDefaults(default_settings)
 
-        self.vector_fields.append("ADJOINT_DISPLACEMENT")
+        self.output_all_fields = self.parameters["output_all_fields"].GetBool()
 
-        self.output_names = self.vector_fields + self.scalar_fields
+        self.model_part_operation = ModelPartOperation(self.model, ModelPartOperation.OperationType.UNION, f"control_{self.GetName()}", self.parameters["controlled_model_part_names"].GetStringArray(), False)
+        self.model_part: 'Optional[Kratos.ModelPart]' = None
 
-        # add vars
-        for model_part_name in self.controlling_objects:
-            root_model = model_part_name.split(".")[0]
-            for field_name in self.output_names:
-                self.model.GetModelPart(root_model).AddNodalSolutionStepVariable(KM.KratosGlobals.GetVariable(field_name))
+        self.filter = FilterFactory(self.model, self.model_part_operation.GetModelPartFullName(), KratosOA.SHAPE, Kratos.Globals.DataLocation.NodeHistorical, self.parameters["filter_settings"])
 
+    @time_decorator(methodName="GetName")
+    def Initialize(self) -> None:
+        self.filter.Initialize()
 
-    def Initialize(self):
-        # initialize zero the fields
-        for model_part_name in self.controlling_objects:
-            model_part = self.model.GetModelPart(model_part_name)         
-            for node in model_part.Nodes:
-                for vec_field in self.vector_fields:
-                    node.SetSolutionStepValue(KM.KratosGlobals.GetVariable(vec_field), [0.0, 0.0, 0.0])
-                for scala_field in self.scalar_fields:
-                    node.SetSolutionStepValue(KM.KratosGlobals.GetVariable(scala_field), 0)
- 
-    def MapFirstDerivative(self,derivative_variable_name,mapped_derivative_variable_name):
-        raise RuntimeError("ShapeControl:MapFirstDerivative: calling base class function") 
+    def Check(self) -> None:
+        self.filter.Check()
 
-    def Compute(self):
-        raise RuntimeError("ShapeControl:Compute: calling base class function")
+    def Finalize(self) -> None:
+        self.filter.Finalize()
 
-    def Update(self):
-        raise RuntimeError("ShapeControl:Update: calling base class function") 
+    def GetPhysicalKratosVariables(self) -> 'list[SupportedSensitivityFieldVariableTypes]':
+        return [KratosOA.SHAPE]
 
-    def GetVariableName(self):
-        return self.control_variable_name    
+    def GetEmptyField(self) -> ContainerExpressionTypes:
+        field = Kratos.Expression.NodalExpression(self.filter_model_part)
+        Kratos.Expression.LiteralExpressionIO.SetData(field, [0,0,0])
+        return field
 
-    def GetControllingObjects(self):
-        raise RuntimeError("ShapeControl:GetControllingObjects: calling base class function")            
+    def GetControlField(self) -> ContainerExpressionTypes:
+        return self.control_field
 
-    def GetUpdateName(self):
-        return self.control_update_name   
+    def GetPhysicalField(self) -> ContainerExpressionTypes:
+        physical_shape_field = Kratos.Expression.NodalExpression(self.filter_model_part)
+        Kratos.Expression.NodalPositionExpressionIO.Read(physical_shape_field, Kratos.Configuration.Initial)
+        return physical_shape_field
 
-    def GetOutputNames(self):
-        return self.output_names 
+    @time_decorator(methodName="GetName")
+    def MapGradient(self, physical_gradient_variable_container_expression_map: 'dict[SupportedSensitivityFieldVariableTypes, ContainerExpressionTypes]') -> ContainerExpressionTypes:
+        keys = physical_gradient_variable_container_expression_map.keys()
+        if len(keys) != 1:
+            raise RuntimeError(f"Provided more than required gradient fields for control \"{self.GetName()}\". Following are the variables:\n\t" + "\n\t".join([k.Name() for k in keys]))
+        if KratosOA.SHAPE not in keys:
+            raise RuntimeError(f"The required gradient for control \"{self.GetName()}\" w.r.t. {KratosOA.SHAPE.Name()} not found. Followings are the variables:\n\t" + "\n\t".join([k.Name() for k in keys]))
 
-    def GetType(self):
-        return self.type                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-            
+        physical_gradient = physical_gradient_variable_container_expression_map[KratosOA.SHAPE]
+        if not IsSameContainerExpression(physical_gradient, self.GetEmptyField()):
+            raise RuntimeError(f"Gradients for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.filter_model_part.FullName()}, given model part name: {physical_gradient.GetModelPart().FullName()} ]")
 
+        # now filter the field
+        filtered_gradient = self.filter.FilterIntegratedField(physical_gradient)
 
+        return filtered_gradient
+
+    @time_decorator(methodName="GetName")
+    def Update(self, new_control_field: ContainerExpressionTypes) -> bool:
+        if not IsSameContainerExpression(new_control_field, self.GetEmptyField()):
+            raise RuntimeError(f"Updates for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.filter_model_part.FullName()}, given model part name: {new_control_field.GetModelPart().FullName()} ]")
+        if Kratos.Expression.Utils.NormL2(self.control_field - new_control_field) > 1e-15:
+            # update the control SHAPE field
+            control_update = new_control_field - self.control_field
+            self.control_field = new_control_field
+            # now update the physical field
+            self._UpdateAndOutputFields(control_update)
+            # now update the filter
+            self.filter.Update()
+            return True
+        return False
+
+    def _UpdateAndOutputFields(self, control_update: ContainerExpressionTypes) -> None:
+        # compute the shape update
+        shape_update = self.filter.FilterField(control_update)
+
+        # now output the fields
+        un_buffered_data = ComponentDataView(self, self.optimization_problem).GetUnBufferedData()
+        un_buffered_data.SetValue("shape_update", shape_update.Clone(), overwrite=True)
+        if self.output_all_fields:
+            un_buffered_data.SetValue("shape_control", self.control_field.Clone(), overwrite=True)
+            un_buffered_data.SetValue("shape_control_update", control_update.Clone(), overwrite=True)
+
+    def __str__(self) -> str:
+        return f"Control [type = {self.__class__.__name__}, name = {self.GetName()}, filter = {self.filter}, control variable = {KratosOA.SHAPE.Name()}"
