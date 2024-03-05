e627678a60c558fd39308fa99e86eb373f4c801f
sunethwarna <suneth.warna@gmail.com>
2024-03-05 09:40:00 +0100

    fix filter functions

--- applications/OptimizationApplication/custom_utilities/filtering/filter_function.cpp
+++ applications/OptimizationApplication/custom_utilities/filtering/filter_function.cpp
@@ -27,19 +27,20 @@ FilterFunction::FilterFunction(const std::string& rKernelFunctionType)
 
     if (rKernelFunctionType == "gaussian") {
         // Type 1: Gaussian function
-        mFilterFunctional =  [](double radius, double distance) {return std::max(0.0, exp(-(distance*distance)));};
+        // at distance = radius, the filter value will be 1e-8
+        mFilterFunctional =  [](double radius, double distance) { return std::max(0.0, exp(-(8 * 2.3025850929940455 * distance * distance) / (radius * radius))); };
     } else if (rKernelFunctionType == "linear") {
         // Type 2: Linear function
-        mFilterFunctional =  [](double radius, double distance) {return std::max(0.0, (radius - distance) / radius);};
+        mFilterFunctional =  [](double radius, double distance) { return std::max(0.0, (radius - distance) / radius); };
     } else if (rKernelFunctionType == "constant") {
         // Type 3: Constant function
-        mFilterFunctional = [](double radius, double distance) {return 1.0;};
+        mFilterFunctional = [](double radius, double distance) { return 1.0; };
     } else if (rKernelFunctionType == "cosine") {
         // Type 4: Cosine function
-        mFilterFunctional = [](double radius, double distance) {return std::max(0.0, 1-0.5*(1-std::cos(Globals::Pi/radius*distance)));};
+        mFilterFunctional = [](double radius, double distance) { return std::max(0.0, 1 - 0.5 * (1 - std::cos(Globals::Pi / radius * std::min(distance, radius))));};
     } else if (rKernelFunctionType == "quartic") {
         // Type 5: Quartic function
-        mFilterFunctional = [](double radius, double distance) {return std::max(0.0, (pow(distance-radius,4.0)/pow(radius,4.0)));};
+        mFilterFunctional = [](double radius, double distance) { return std::max(0.0, (std::pow(std::min(distance, radius) - radius, 4.0) / std::pow(radius, 4.0))); };
     } else {
         // Throw error message in case of wrong specification
         KRATOS_ERROR << "Specified kernel function of type : "
