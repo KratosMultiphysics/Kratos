8c515af08e3159e24da942fc18cdfe6cdb4fc5cc
sunethwarna <suneth.warna@gmail.com>
2024-03-05 18:09:12 +0100

    fix existing tests

--- applications/OptimizationApplication/tests/filtering/explicit_filters_tests.py
+++ applications/OptimizationApplication/tests/filtering/explicit_filters_tests.py
@@ -108,12 +108,13 @@ class TestExplicitFilterFactory(kratos_unittest.TestCase):
         }""")
         settings["filter_function_type"].SetString(filter_function_type)
         settings["filtering_boundary_conditions"]["damping_function_type"].SetString(damping_function_type)
-        vm_filter = Factory(self.model, "test", KratosOA.SHAPE, Kratos.Globals.DataLocation.NodeHistorical, settings, self.filter_data)
+        vm_filter = Factory(self.model, "test", KratosOA.SHAPE, Kratos.Globals.DataLocation.NodeHistorical, settings)
+        vm_filter.SetComponentDataView(ComponentDataView("test", self.optimization_problem))
         vm_filter.Initialize()
 
         vtu_output = Kratos.VtuOutput(self.model_part, binary_output=Kratos.VtuOutput.ASCII, precision=3)
         step_size = 5e-2
-        for i in range(10):
+        for _ in range(10):
             Kratos.NormalCalculationUtils().CalculateNormals(self.model_part)
             normal_exp = Kratos.Expression.NodalExpression(self.model_part)
             Kratos.Expression.VariableExpressionIO.Read(normal_exp, Kratos.NORMAL, True)
@@ -126,19 +127,19 @@ class TestExplicitFilterFactory(kratos_unittest.TestCase):
             Kratos.Expression.NodalPositionExpressionIO.Read(nodal_coords, Kratos.Configuration.Initial)
             Kratos.Expression.NodalPositionExpressionIO.Write(nodal_coords + filtered_update, Kratos.Configuration.Initial)
             Kratos.Expression.NodalPositionExpressionIO.Write(nodal_coords + filtered_update, Kratos.Configuration.Current)
-            vtu_output.PrintOutput(f"test_{ref_file}_{i}")
 
-        # with kratos_unittest.WorkFolderScope(".", __file__):
-        #     vtu_output.PrintOutput(f"output_{ref_file}")
-        #     params = Kratos.Parameters("""{
-        #         "reference_file_name"   : "explicit_filter_reference_1.vtu.orig",
-        #         "output_file_name"      : "explicit_filter_reference.vtu",
-        #         "remove_output_file"    : true,
-        #         "comparison_type"       : "deterministic"
-        #     }""")
-        #     params["reference_file_name"].SetString(ref_file)
-        #     params["output_file_name"].SetString(f"output_{ref_file}.vtu")
-        #     CompareTwoFilesCheckProcess(params).Execute()
+        vtu_output.PrintOutput(f"output_{ref_file}")
+        with kratos_unittest.WorkFolderScope(".", __file__):
+            vtu_output.PrintOutput(f"output_{ref_file}")
+            params = Kratos.Parameters("""{
+                "reference_file_name"   : "explicit_filter_reference_1.vtu.orig",
+                "output_file_name"      : "explicit_filter_reference.vtu",
+                "remove_output_file"    : true,
+                "comparison_type"       : "deterministic"
+            }""")
+            params["reference_file_name"].SetString(ref_file)
+            params["output_file_name"].SetString(f"output_{ref_file}.vtu")
+            CompareTwoFilesCheckProcess(params).Execute()
 
     def test_FilterCosine(self):
         self.__RunTestCase("cosine", "cosine", "explicit_filter_reference_cosine.vtu.orig")
