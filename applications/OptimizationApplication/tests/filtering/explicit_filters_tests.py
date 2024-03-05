96fe1d0df63cba18950bf4111f455f65de6e8510
sunethwarna <suneth.warna@gmail.com>
2024-03-05 11:49:53 +0100

    add tests

--- applications/OptimizationApplication/tests/filtering/explicit_filters_tests.py
+++ applications/OptimizationApplication/tests/filtering/explicit_filters_tests.py
@@ -2,6 +2,8 @@ import math
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.testing.utilities import ReadModelPart
+from KratosMultiphysics.OptimizationApplication.filtering.filter import Factory
+from KratosMultiphysics.compare_two_files_check_process import CompareTwoFilesCheckProcess
 
 # Import KratosUnittest
 import KratosMultiphysics.KratosUnittest as kratos_unittest
@@ -60,6 +62,64 @@ class TestExplicitFilter(kratos_unittest.TestCase):
         integrated_constant_field = integration_weights * unfiltered_field
         filtered_integrated_data = vm_filter.FilterIntegratedField(integrated_constant_field)
         self.assertAlmostEqual(Kratos.Expression.Utils.NormL2(filtered_integrated_data), math.sqrt(12 * len(entities)), 12)
+
+class TestExplicitFilterFactory(kratos_unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.model = Kratos.Model()
+        cls.model_part = cls.model.CreateModelPart("test")
+        cls.model_part.ProcessInfo[Kratos.DOMAIN_SIZE] = 3
+        cls.model_part.AddNodalSolutionStepVariable(Kratos.NORMAL)
+        with kratos_unittest.WorkFolderScope(".", __file__):
+            ReadModelPart("shell", cls.model_part)
+
+    def test_Reference1(self):
+        settings = Kratos.Parameters("""{
+            "filter_type"               : "explicit_vertex_morphing",
+            "filter_function_type"      : "linear",
+            "max_nodes_in_filter_radius": 100000,
+            "filter_radius_settings":{
+                "filter_radius_type": "constant",
+                "filter_radius"     : 0.5
+            },
+            "filtering_boundary_conditions": {
+                "damping_type"              : "nearest_entity",
+                "damping_function_type"     : "cosine",
+                "damped_model_part_settings": {
+                    "test.DISPLACEMENT_left_edge"  : [ true, false, false],
+                    "test.DISPLACEMENT_top_edge"   : [false,  true, false],
+                    "test.DISPLACEMENT_right_edge" : [false, false,  true],
+                    "test.DISPLACEMENT_bottom_edge": [false,  true,  true]
+                }
+            }
+        }""")
+        vm_filter = Factory(self.model, "test", KratosOA.SHAPE, Kratos.Globals.DataLocation.NodeHistorical, settings)
+        vm_filter.Initialize()
+
+        vtu_output = Kratos.VtuOutput(self.model_part, binary_output=Kratos.VtuOutput.ASCII, precision=3)
+        step_size = 5e-2
+        for _ in range(10):
+            Kratos.NormalCalculationUtils().CalculateNormals(self.model_part)
+            normal_exp = Kratos.Expression.NodalExpression(self.model_part)
+            Kratos.Expression.VariableExpressionIO.Read(normal_exp, Kratos.NORMAL, True)
+
+            update = normal_exp * (step_size / Kratos.Expression.Utils.NormInf(normal_exp))
+            filtered_update = vm_filter.FilterField(update)
+
+            # update the mesh
+            nodal_coords = Kratos.Expression.NodalExpression(self.model_part)
+            Kratos.Expression.NodalPositionExpressionIO.Read(nodal_coords, Kratos.Configuration.Initial)
+            Kratos.Expression.NodalPositionExpressionIO.Write(nodal_coords + filtered_update, Kratos.Configuration.Initial)
+            Kratos.Expression.NodalPositionExpressionIO.Write(nodal_coords + filtered_update, Kratos.Configuration.Current)
+
+        vtu_output.PrintOutput(f"explicit_filter_reference_1")
+        params = Kratos.Parameters("""{
+            "reference_file_name"   : "explicit_filter_reference_1.vtu.orig",
+            "output_file_name"      : "explicit_filter_reference_1.vtu",
+            "remove_output_file"    : true,
+            "comparison_type"       : "deterministic"
+        }""")
+        CompareTwoFilesCheckProcess(params).Execute()
 
 if __name__ == "__main__":
     kratos_unittest.main()
