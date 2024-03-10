0715011c0f29cfc3502576e48af44d838270af24
Chengshun Shang <chengshun.shang1996@gmail.com>
2024-03-10 17:09:49 +0100

    remove Relative Displacement due to rotation

--- applications/DEMApplication/custom_elements/spheric_continuum_particle.cpp
+++ applications/DEMApplication/custom_elements/spheric_continuum_particle.cpp
@@ -296,7 +296,9 @@ namespace Kratos {
             EvaluateDeltaDisplacement(data_buffer, DeltDisp, RelVel, data_buffer.mLocalCoordSystem, data_buffer.mOldLocalCoordSystem, vel, delta_displ);
 
             if (this->Is(DEMFlags::HAS_ROTATION)) {
-                RelativeDisplacementAndVelocityOfContactPointDueToRotationQuaternion(DeltDisp, RelVel, data_buffer.mOldLocalCoordSystem, other_radius, data_buffer.mDt, ang_vel, neighbour_iterator);
+                if (mContinuumConstitutiveLawArray[i]->GetTypeOfLaw() != "parallel_bond_CL"){
+                    RelativeDisplacementAndVelocityOfContactPointDueToRotationQuaternion(DeltDisp, RelVel, data_buffer.mOldLocalCoordSystem, other_radius, data_buffer.mDt, ang_vel, neighbour_iterator);
+                }
             }
 
             RelativeDisplacementAndVelocityOfContactPointDueToOtherReasons(r_process_info, DeltDisp, RelVel, data_buffer.mOldLocalCoordSystem, data_buffer.mLocalCoordSystem, neighbour_iterator);
