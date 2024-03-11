6c68469fec9eb7bc8025155f0c357274c33f5272
Alejandro <alejandro.cornejo23@gmail.com>
2024-03-11 15:16:27 +0400

    minor update in base beam CL

--- applications/StructuralMechanicsApplication/custom_constitutive/beam_constitutive_law.cpp
+++ applications/StructuralMechanicsApplication/custom_constitutive/beam_constitutive_law.cpp
@@ -74,27 +74,25 @@ int BeamConstitutiveLaw::Check(
     const ProcessInfo& rCurrentProcessInfo
 ) const
 {
-    if(YOUNG_MODULUS.Key() == 0 || rMaterialProperties[YOUNG_MODULUS] <= 0.00)
+    if (YOUNG_MODULUS.Key() == 0 || rMaterialProperties[YOUNG_MODULUS] <= 0.0)
     {
         KRATOS_ERROR << "YOUNG_MODULUS has Key zero or invalid value " << std::endl;
     }
 
-    const double& nu = rMaterialProperties[POISSON_RATIO];
-    const bool check = bool( (nu >0.499 && nu<0.501 ) || (nu < -0.999 && nu > -1.01 ) );
+    const double nu = rMaterialProperties[POISSON_RATIO];
+    const bool check = bool((nu > 0.499 && nu < 0.501) || (nu < -0.999 && nu > -1.01));
 
-    if(POISSON_RATIO.Key() == 0 || check==true)
+    if (POISSON_RATIO.Key() == 0 || check==true)
     {
         KRATOS_ERROR << "POISSON_RATIO has Key zero invalid value " << std::endl;
     }
 
-
-    if(DENSITY.Key() == 0 || rMaterialProperties[DENSITY] < 0.00)
+    if (DENSITY.Key() == 0 || rMaterialProperties[DENSITY] < 0.00)
     {
         KRATOS_ERROR << "DENSITY has Key zero or invalid value " << std::endl;
     }
 
     return 0;
-
 }
 
 } // Namespace Kratos
