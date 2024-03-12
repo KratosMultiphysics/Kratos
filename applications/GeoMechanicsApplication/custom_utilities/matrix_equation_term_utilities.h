2d727aad61359f70014dbc6429058bb5ab90f145
Richard Faasse <richard.faasse@deltares.nl>
2024-03-12 16:52:16 +0100

    Added empty utility namespace to start adding the term calculations to

---
+++ applications/GeoMechanicsApplication/custom_utilities/matrix_equation_term_utilities.h
@@ -0,0 +1,18 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+//  Main authors:    Richard Faasse
+//
+
+#pragma once
+
+namespace Kratos::Geo::MatrixEquationTermUtilities
+{
+
+} // namespace Kratos::Geo::MatrixEquationTermUtilities
