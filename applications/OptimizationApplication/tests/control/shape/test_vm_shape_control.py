4eb958ee4f01e7a2893ce3cdfaa68fbc33a26bcb
sunethwarna <suneth.warna@gmail.com>
2024-03-16 10:55:18 +0100

    update tests

--- applications/OptimizationApplication/tests/control/shape/test_vm_shape_control.py
+++ applications/OptimizationApplication/tests/control/shape/test_vm_shape_control.py
@@ -242,7 +242,7 @@ class TestVMShapeControlSolid(TestVMShapeControlBase, kratos_unittest.TestCase):
         control_field = self.explicit_shape_control.GetControlField()
         shape_field = self.explicit_shape_control.GetPhysicalField()
         self.assertAlmostEqual(Kratos.Expression.Utils.NormL2(control_field), 0.6480740698407862, 10)
-        self.assertAlmostEqual(Kratos.Expression.Utils.NormL2(shape_field), 3.1366271299066, 10)
+        self.assertAlmostEqual(Kratos.Expression.Utils.NormL2(shape_field), 3.125699921617557, 10)
 
     def test_UpdateImplicit(self):
         constant_field_value = Kratos.Array3([0.1, 0.1, 0.1])
