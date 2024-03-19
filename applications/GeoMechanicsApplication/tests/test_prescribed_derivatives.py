0ccd576bdcc990aa0e02c1355fd3cae0d82226db
Richard Faasse <richard.faasse@deltares.nl>
2024-03-19 13:47:48 +0100

    Added integration test for prescribed dt_water_pressure

--- applications/GeoMechanicsApplication/tests/test_prescribed_derivatives.py
+++ applications/GeoMechanicsApplication/tests/test_prescribed_derivatives.py
@@ -64,14 +64,37 @@ class KratosGeoMechanicsPrescribedDerivatives(KratosUnittest.TestCase):
         output_data = output_reader.read_output_from(output_file_path)
 
         times = [0.05, 0.1, 0.15, 0.2]
-        expected_x_accelerations = [-0.01, -0.005, -0.003, 0.01]
+        expected_velocities = [-0.01, -0.005, -0.003, 0.01]
         for idx, time in enumerate(times):
-            accelerations = test_helper.GiDOutputFileReader.nodal_values_at_time("VELOCITY", time, output_data,
+            velocities = test_helper.GiDOutputFileReader.nodal_values_at_time("VELOCITY", time, output_data,
                                                                                  node_ids=[1, 2, 3])
-            for acceleration in accelerations:
-                self.assertAlmostEqual(expected_x_accelerations[idx], acceleration[0], 6)
-                self.assertAlmostEqual(0.0, acceleration[1], 6)
-                self.assertAlmostEqual(0.0, acceleration[2], 6)
+            for velocity in velocities:
+                self.assertAlmostEqual(expected_velocities[idx], velocity[0], 6)
+                self.assertAlmostEqual(0.0, velocity[1], 6)
+                self.assertAlmostEqual(0.0, velocity[2], 6)
+
+    def test_prescribed_dt_water_pressure(self):
+        """
+        To be added
+        :return:
+        """
+
+        test_name = 'prescribed_dt_water_pressure'
+        file_path = test_helper.get_file_path(os.path.join('.', 'prescribed_derivative_tests', test_name))
+        test_helper.run_kratos(file_path)
+
+        output_file_path = os.path.join(file_path, test_name + '.post.res')
+        output_reader = test_helper.GiDOutputFileReader()
+        output_data = output_reader.read_output_from(output_file_path)
+
+        times = [0.05, 0.1, 0.15, 0.2]
+        expected_dt_water_pressures = [-0.01, -0.005, -0.003, 0.01]
+        for idx, time in enumerate(times):
+            dt_water_pressures = test_helper.GiDOutputFileReader.nodal_values_at_time("DT_WATER_PRESSURE", time,
+                                                                                      output_data,
+                                                                                      node_ids=[1, 2, 3])
+            for dt_water_pressure in dt_water_pressures:
+                self.assertAlmostEqual(expected_dt_water_pressures[idx], dt_water_pressure, 6)
 
 
 if __name__ == '__main__':
