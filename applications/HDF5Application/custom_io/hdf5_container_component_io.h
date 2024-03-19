2fc9323df207424f69562d6f2db6e8e2c8e01851
sunethwarna <suneth.warna@gmail.com>
2024-03-19 11:25:02 +0100

    documentation

--- applications/HDF5Application/custom_io/hdf5_container_component_io.h
+++ applications/HDF5Application/custom_io/hdf5_container_component_io.h
@@ -57,7 +57,46 @@ public:
     ///@{
 
     /// Constructor.
-    // TODO: Remove the rLegacySuffix once the python side is properly fixed.
+    /**
+     * @brief Construct a new Container Component IO to read and write from kratos containers using variables
+     * @details This class is the inner most class which can be used to read from or write to
+     *          different containers in Kratos. followings are the supported containers.
+     *
+     *               1. Nodal historical data value container
+     *               2. Nodal non-historical data value container
+     *               3. Condition data value container
+     *               4. Element data value container
+     *               5. Condition container with gauss point data values
+     *               6. Element container gauss point data values
+     *               7. Vertex container
+     *
+     *          This supports following variable types:
+     *              - int
+     *              - double
+     *              - array_1d<double, 3>
+     *              - array_1d<double, 4>
+     *              - array_1d<double, 6>
+     *              - array_1d<double, 9>
+     *              - Vector
+     *              - Matrix
+     *
+     *          This also allows writing custom attributes to the HDF5 dataset. These can be anything,
+     *          and should be passed as a @ref Kratos::Parameters object. The attribute keys starting
+     *          with "__" are reserved for the HDF5Application and must not be used as custom attribute
+     *          keys. Followings are list of reserved keys:
+     *              - "__data_dimension"
+     *              - "__data_shape"
+     *              - "__container_type"
+     *              - "__data_name"
+     *              - "__data_location"
+     *              - "__mesh_location"
+     *
+     * @todo Remove the rLegacySuffix once the python side is properly fixed.
+     *
+     * @param Settings          Settings to be used with this Component IO.
+     * @param pFile             HDF5 file to be used for reading and writing components.
+     * @param rLegacySuffix     Suffix used in legacy HDF5 writing. Planned to be removed once python side is fixed.
+     */
     ContainerComponentIO(
         Parameters Settings,
         File::Pointer pFile,
@@ -67,25 +106,67 @@ public:
     ///@name Operations
     ///@{
 
+    /**
+     * @brief Write data from the local container having the type TContainerType to the HDF5 file.
+     * @details This method can be used in either shared memory/distributed memory parallelized architectures.
+     *
+     * @param rModelPart            Model part to get the local container of type TContainerType to get the data.
+     * @param rContainerDataIO      Reading mechanism from where the data will be read from the TContainerType.
+     * @param Attributes            Custom attributes to be written in to the HDF5 dataset.
+     */
     void Write(
         const ModelPart& rModelPart,
         const TContainerIO& rContainerDataIO,
         const Parameters Attributes);
 
+    /**
+     * @brief Read an hdf5 dataset and populate data to the container with type TContainerType.
+     * @details This method can be used in either shared memory/distributed memory parallelized architectures.
+     *          It will synchronize the written data to populate the ghost meshes correctly.
+     *          The returning map will have the component name as the key, and its attributes as values.
+     *
+     * @param rModelPart                            Model part to read the data into from the HDF5 dataset.
+     * @param rContainerDataIO                      Writing mechanism to where the data will be written in rModelPart from the HDF5 dataset.
+     * @return std::map<std::string, Parameters>    A map containing component name as the key and each components' attributes as values.
+     */
     std::map<std::string, Parameters> Read(
         ModelPart& rModelPart,
         const TContainerIO& rContainerDataIO);
 
+    /**
+     * @brief Write data from the passed local data container to the HDF5 dataset.
+     *
+     * @param rLocalContainer       Local container with the data.
+     * @param rContainerDataIO      Reading mechanism specifying where to read the data from the local container.
+     * @param Attributes            Custom attributes to be written in to the HDF5 dataset.
+     */
     void Write(
         const TContainerType& rLocalContainer,
         const TContainerIO& rContainerDataIO,
         const Parameters Attributes);
 
+    /**
+     * @brief Read an hdf5 dataset and populate data to the provided local container.
+     * @details This method can be used in either shared memory/distributed memory parallelized architectures.
+     *          It will synchronize the written data to populate the ghost meshes correctly.
+     *          The returning map will have the component name as the key, and its attributes as values.
+     *
+     * @param rLocalContainer                       Local container with the data.
+     * @param rContainerDataIO                      Writing mechanism specifying where to write the data in the local container.
+     * @param rCommunicator                         Communicator to synchronize and populate ghost meshes.
+     * @return std::map<std::string, Parameters>    A map containing component name as the key and each components' attributes as values.
+     */
     std::map<std::string, Parameters> Read(
         TContainerType& rLocalContainer,
         const TContainerIO& rContainerDataIO,
         Communicator& rCommunicator);
 
+    /**
+     * @brief Reads only the attributes for components specified.
+     * @details This method only reads the attributes for the specified components. Does not read the dataset.
+     *
+     * @return std::map<std::string, Parameters>    A map containing component name as the key and each components' attributes as values.
+     */
     std::map<std::string, Parameters> ReadAttributes();
 
     ///@}
