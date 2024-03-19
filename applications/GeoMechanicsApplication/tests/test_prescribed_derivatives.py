82404d5df1a93cb22ae75a9479bb0737e28d384b
Richard Faasse <richard.faasse@deltares.nl>
2024-03-19 13:38:28 +0100

    Added integration test for prescribed velocity

--- applications/GeoMechanicsApplication/tests/test_prescribed_derivatives.py
+++ applications/GeoMechanicsApplication/tests/test_prescribed_derivatives.py
@@ -49,6 +49,30 @@ class KratosGeoMechanicsPrescribedDerivatives(KratosUnittest.TestCase):
                 self.assertAlmostEqual(0.0, acceleration[1], 6)
                 self.assertAlmostEqual(0.0, acceleration[2], 6)
 
+    def test_prescribed_velocity(self):
+        """
+        To be added
+        :return:
+        """
+
+        test_name = 'prescribed_velocity'
+        file_path = test_helper.get_file_path(os.path.join('.', 'prescribed_derivative_tests', test_name))
+        test_helper.run_kratos(file_path)
+
+        output_file_path = os.path.join(file_path, test_name + '.post.res')
+        output_reader = test_helper.GiDOutputFileReader()
+        output_data = output_reader.read_output_from(output_file_path)
+
+        times = [0.05, 0.1, 0.15, 0.2]
+        expected_x_accelerations = [-0.01, -0.005, -0.003, 0.01]
+        for idx, time in enumerate(times):
+            accelerations = test_helper.GiDOutputFileReader.nodal_values_at_time("VELOCITY", time, output_data,
+                                                                                 node_ids=[1, 2, 3])
+            for acceleration in accelerations:
+                self.assertAlmostEqual(expected_x_accelerations[idx], acceleration[0], 6)
+                self.assertAlmostEqual(0.0, acceleration[1], 6)
+                self.assertAlmostEqual(0.0, acceleration[2], 6)
+
 
 if __name__ == '__main__':
     KratosUnittest.main()
