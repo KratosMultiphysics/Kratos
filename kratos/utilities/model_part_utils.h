fbe5318a4f985c4b6819bf92dab9fe7e74b1c349
Vicente Mataix Ferrándiz <vmataix@altair.com>
2024-03-19 10:43:34 +0100

    In order to satisfy MSVC I removed the explicit instantation because linking problems

--- kratos/utilities/model_part_utils.h
+++ kratos/utilities/model_part_utils.h
@@ -19,15 +19,17 @@
 // External includes
 
 // Project includes
-#include "includes/kratos_export_api.h"
+#include "includes/define.h"
 #include "includes/model_part.h"
 #include "includes/global_variables.h"
+#include "utilities/parallel_utilities.h"
+#include "utilities/reduction_utilities.h"
 
 namespace Kratos
 {
 
 /// @brief Class collecting a set of free-floating utility functions for querying and mutating @ref ModelPart s.
-class KRATOS_API(KRATOS_CORE) ModelPartUtils
+class ModelPartUtils
 {
 public:
     /// @brief Templated interface for getting nodes, elements, conditions or @ref ProcessInfo from a @ref ModelPart.
@@ -114,7 +116,35 @@ public:
         ModelPart::NodesContainerType& rThisNodes,
         PointerVectorSet<TEntity, IndexedObject, std::less<typename IndexedObject::result_type>, std::equal_to<typename IndexedObject::result_type>, typename TEntity::Pointer, std::vector<typename TEntity::Pointer>>& rThisEntities,
         const Properties::Pointer pProperties = nullptr
-        );
+        )
+    {
+        // Some definitions
+        const std::size_t current_size = rThisEntities.size();
+        const std::size_t number_of_entities = rEntitiesConnectivities.size();
+        rThisEntities.reserve(current_size + number_of_entities);
+
+        // We assume that the first entity in the list is the one we want to clone
+        const TEntity& r_clone_entity = CheckEntity<TEntity>(rEntityName);
+        const std::size_t number_of_nodes_per_entity = r_clone_entity.GetGeometry().size();
+
+        // From connectivities generate entities
+        const auto entities_vector = IndexPartition<IndexType>(number_of_entities).for_each<AccumReduction<typename TEntity::Pointer>>([&](const IndexType i) {
+            typename TEntity::NodesArrayType temp_entity_nodes;
+            temp_entity_nodes.reserve(number_of_nodes_per_entity);
+            for(std::size_t j = 0; j < number_of_nodes_per_entity; ++j){
+                const std::size_t node_id = rEntitiesConnectivities[i][j];
+                auto it_node = rThisNodes.find(node_id);
+                KRATOS_ERROR_IF(it_node == rThisNodes.end()) << "Node with Id " << node_id << " does not exist in the nodes array." << std::endl;
+                temp_entity_nodes.push_back(*(it_node.base()));
+            }
+
+            // Add the entity to the list
+            return r_clone_entity.Create(i + 1, temp_entity_nodes, pProperties);
+        });
+
+        // Add the entities to the list
+        rThisEntities.insert(entities_vector.begin(), entities_vector.end());
+    }
 
     /**
      * @brief Generates entities (Elements or Conditions) based on the provided connectivities and adds them to a specified container.
@@ -139,7 +169,71 @@ public:
         ModelPart::NodesContainerType& rThisNodes,
         ModelPart::PropertiesContainerType& rThisProperties,
         PointerVectorSet<TEntity, IndexedObject, std::less<typename IndexedObject::result_type>, std::equal_to<typename IndexedObject::result_type>, typename TEntity::Pointer, std::vector<typename TEntity::Pointer>>& rThisEntities
-        );
+        )
+    {
+        // Some definitions
+        const std::size_t current_size = rThisEntities.size();
+        const std::size_t number_of_entities = rEntitiesConnectivities.size();
+        rThisEntities.reserve(current_size + number_of_entities);
+
+        // We assume that the first entity in the list is the one we want to clone
+        const TEntity& r_clone_entity = CheckEntity<TEntity>(rEntityName);
+        const std::size_t number_of_nodes_per_entity = r_clone_entity.GetGeometry().size();
+
+        // From connectivities generate entities
+        const auto entities_vector = IndexPartition<IndexType>(number_of_entities).for_each<AccumReduction<typename TEntity::Pointer>>([&](const IndexType i) {
+            typename TEntity::NodesArrayType temp_entity_nodes;
+            temp_entity_nodes.reserve(number_of_nodes_per_entity);
+            for(std::size_t j = 0 ; j < number_of_nodes_per_entity; ++j){
+                const std::size_t node_id = rEntitiesConnectivities[i][j];
+                auto it_node = rThisNodes.find(node_id);
+                KRATOS_ERROR_IF(it_node == rThisNodes.end()) << "Node with Id " << node_id << " does not exist in the nodes array." << std::endl;
+                temp_entity_nodes.push_back(*(it_node.base()));
+            }
+
+            // Properties iterator
+            auto it_properties = rThisProperties.find(rPropertiesIds[i]);
+            KRATOS_ERROR_IF(it_properties == rThisProperties.end()) << "Properties with Id " << rPropertiesIds[i] << " does not exist in the properties array." << std::endl;
+            Properties::Pointer p_properties = *(it_properties.base());
+
+            // Add the entity to the list
+            return r_clone_entity.Create(rEntitiesIds[i], temp_entity_nodes, p_properties);
+        });
+
+        // Add the entities to the list
+        rThisEntities.insert(entities_vector.begin(), entities_vector.end());
+    }
+
+private:
+
+    /**
+    * @brief Checks if an entity is registered in Kratos and returns a reference to it.
+    * @details This function checks if a given entity (either an Element or a Condition) is registered in Kratos. If the entity is registered, it returns a constant reference to it. If the entity is not registered, it throws an error with a descriptive message. Template parameter `TEntity` can be either `Element` or `Condition`. The function utilizes compile-time checks to generate appropriate error messages based on the entity type.
+    * @tparam TEntity The type of the entity to check. Must be either `Element` or `Condition`.
+    * @param rEntityName The name of the entity to check.
+    * @return const TEntity& A constant reference to the entity.
+    * @throw Kratos::Exception If the entity is not registered in Kratos. The exception message will specify 
+    * whether the missing entity is an Element or a Condition and remind to check the spelling and registration of the application.
+    */
+    template<class TEntity>
+    static const TEntity& CheckEntity(const std::string& rEntityName)
+    {
+        // Check if the entity is registered in Kratos
+        if(!KratosComponents<TEntity>::Has(rEntityName)) {
+            std::stringstream buffer;
+            if constexpr(std::is_same_v<TEntity, Element>) {
+                buffer << "Element " << rEntityName << " is not registered in Kratos.";
+                buffer << " Please check the spelling of the element name and see if the application which containing it, is registered correctly.";
+            } else if constexpr(std::is_same_v<TEntity, Condition>) {
+                buffer << "Condition " << rEntityName << " is not registered in Kratos.";
+                buffer << " Please check the spelling of the condition name and see if the application which containing it, is registered correctly.";
+            } else {
+                static_assert(!std::is_same_v<TEntity, TEntity>, "Unsupported entity type.");
+            }
+            KRATOS_ERROR << buffer.str() << std::endl;
+        }
+        return KratosComponents<TEntity>::Get(rEntityName);
+    }
 
 }; // class ModelPartUtils
 
