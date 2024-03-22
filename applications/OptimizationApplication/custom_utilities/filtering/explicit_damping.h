962ebcf7519f8809e0846346a0a37445d9f90a7b
sunethwarna <suneth.warna@gmail.com>
2024-03-22 20:50:12 +0100

    minor

--- applications/OptimizationApplication/custom_utilities/filtering/explicit_damping.h
+++ applications/OptimizationApplication/custom_utilities/filtering/explicit_damping.h
@@ -58,34 +58,63 @@ class KRATOS_API(OPTIMIZATION_APPLICATION) ExplicitDamping
     ///@name Operations
     ///@{
 
+    /**
+     * @brief Set the damping radius
+     */
     virtual void SetRadius(const ContainerExpression<TContainerType>& rExpression)
     {
         KRATOS_ERROR << "Calling base class ExplicitDamping::SetRadius. This should be implemented in the derived class.";
     }
 
+    /**
+     * @brief Get the used damping radius expression
+     */
     virtual typename ContainerExpression<TContainerType>::Pointer GetRadius() const
     {
         KRATOS_ERROR << "Calling base class ExplicitDamping::GetRadius. This should be implemented in the derived class.";
         return nullptr;
     }
 
+    /**
+     * @brief Get the stride (number of components) this damping can work on
+     */
     virtual IndexType GetStride() const
     {
         KRATOS_ERROR << "Calling base class ExplicitDamping::GetStride. This should be implemented in the derived class.";
         return 0;
     }
 
+    /**
+     * @brief Get the damped model parts
+     * @details This method returns list of model parts which are damped for each
+     *          component in the stride.
+     */
     virtual std::vector<std::vector<ModelPart*>> GetDampedModelParts() const
     {
         KRATOS_ERROR << "Calling base class ExplicitDamping::GetDampedModelParts. This should be implemented in the derived class.";
         return {};
     }
 
+    /**
+     * @brief Update the damping
+     *
+     */
     virtual void Update()
     {
         KRATOS_ERROR << "Calling base class ExplicitDamping::Update. This should be implemented in the derived class.";
     }
 
+    /**
+     * @brief Apply damping to the given weights.
+     * @details This method applies damping to the passed @ref rWeights. The damped
+     *          weights are stored in the @ref rDampedWeights.
+     *
+     * @param rDampedWeights            Output damped weights.
+     * @param rWeights                  Input filtering weights.
+     * @param Index                     Index of the current damping computation.
+     * @param NumberOfNeighbours        number of neighbours around the Index point.
+     * @param rNeighbours               Neighbour entities around the Index point.
+     */
     virtual void Apply(
         std::vector<std::vector<double>>& rDampedWeights,
         const std::vector<double>& rWeights,
@@ -96,6 +125,11 @@ class KRATOS_API(OPTIMIZATION_APPLICATION) ExplicitDamping
         KRATOS_ERROR << "Calling base class ExplicitDamping::GetFilteredDampedValue. This should be implemented in the derived class.";
     }
 
+    /**
+     * @brief Calculate the damping matrix for the component in stride.
+     * @warning Only to be used in testing.
+     *
+     */
     virtual void CalculateMatrix(
         Matrix& rOutput,
         const IndexType ComponentIndex) const
