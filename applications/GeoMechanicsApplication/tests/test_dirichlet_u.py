1d8dca14e0ec82ddfa4a5296d3cb01853c2a88fd
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2024-03-25 11:32:17 +0100

    Review comments about unused variable and support symbol resolved.

--- applications/GeoMechanicsApplication/tests/test_dirichlet_u.py
+++ applications/GeoMechanicsApplication/tests/test_dirichlet_u.py
@@ -51,7 +51,7 @@ class KratosGeoMechanicsDirichletUTests(KratosUnittest.TestCase):
             self.assertAlmostEqual(time*0.05/2., displacement_8_y, 2)
             total_displacements_8  = test_helper.GiDOutputFileReader.nodal_values_at_time("TOTAL_DISPLACEMENT", time, output_data[stage_nr], [8])[0]
             total_displacement_8_y = total_displacements_8[1]
-            self.assertAlmostEqual(time*0.05/2, displacement_8_y, 2)
+            self.assertAlmostEqual(time*0.05/2, total_displacement_8_y, 2)
 
             # integration point check in element 2, integration point 4 ( uniform stress and strain so an arbitrary choice )
             green_lagrange_strains_2_4    = test_helper.GiDOutputFileReader.element_integration_point_values_at_time("GREEN_LAGRANGE_STRAIN_TENSOR", time, output_data[stage_nr], [2], [3])[0][0]
