1455fd4859d3e05e0cf4a3579cb98c01f7bdf026
Carlos Roig <roigcarlo@gmail.com>
2024-04-02 17:14:23 +0200

    Add utility to autogenerate the test binaries for applications

---
+++ cmake_modules/KratosGTest.cmake
@@ -0,0 +1,8 @@
+# This function automatically configures a given application to build its tests
+macro(kratos_add_gtests application_core_name application_test_sources)
+    include(GoogleTest)
+    add_executable(${application_core_name}Test ${application_test_sources})
+    target_link_libraries(${application_core_name}Test ${application_core_name} GTest::gtest_main GTest::gmock_main)
+    install(TARGETS ${application_core_name}Test)
+    gtest_discover_tests(${application_core_name}Test)
+endmacro(kratos_add_gtests)
