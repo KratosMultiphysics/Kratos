b5a6dab26f0b5293d1e549e51d322aeb0241c34b
roigcarlo <roigcarlo@gmail.com>
2024-04-08 16:53:41 +0200

    Missing linking

--- cmake_modules/KratosGTest.cmake
+++ cmake_modules/KratosGTest.cmake
@@ -2,7 +2,7 @@
 macro(kratos_add_gtests application_core_name application_test_sources)
     include(GoogleTest)
     add_executable(${application_core_name}Test ${application_test_sources})
-    target_link_libraries(${application_core_name}Test ${application_core_name} GTest::gtest_main GTest::gmock_main)
+    target_link_libraries(${application_core_name}Test ${application_core_name} KratosCoreTestUtilities GTest::gtest_main GTest::gmock_main)
     set_target_properties(${application_core_name}Test PROPERTIES COMPILE_DEFINITIONS "KRATOS_TEST_CORE=IMPORT,API")
     install(TARGETS ${application_core_name}Test DESTINATION test)
     gtest_discover_tests(${application_core_name}Test DISCOVERY_MODE PRE_TEST)
