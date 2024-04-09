eadb69121683288c7fb37b762b0ec10de505ec44
IAntonau <igor.antonov92@gmail.com>
2024-04-09 11:28:02 +0200

    [OptApp] Relaxed Gradient Projection Algorithm (#12250)
    * save
    
    * nice=)
    
    * improved version
    
    * adding descriptio
    
    * adding test case
    
    * activate test
    
    * low tolerance
    
    * adding test
    
    * Philips suggestions
    
    * adding utilities & Suneth's suggestions
    
    * moving files around
    
    * final fixes
    
    ---------
    
    Co-authored-by: IAntonau <ihar.antonau@tu-braunschweig.de>
    Co-authored-by: IAntonau <ihar.antonau@tu-bs.de>

--- applications/OptimizationApplication/tests/algorithm_tests/analysis_based_tests/algorithm_relaxed_gradient_projection/test_relaxed_gradient_projection.py
+++ applications/OptimizationApplication/tests/algorithm_tests/analysis_based_tests/algorithm_relaxed_gradient_projection/test_relaxed_gradient_projection.py
@@ -1,13 +1,15 @@
 import KratosMultiphysics as Kratos
-import KratosMultiphysics.KratosUnittest as kratos_unittest
+import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 from KratosMultiphysics.kratos_utilities import DeleteFileIfExisting
 from KratosMultiphysics.OptimizationApplication.optimization_analysis import OptimizationAnalysis
 from KratosMultiphysics.compare_two_files_check_process import CompareTwoFilesCheckProcess
 
-class TestQNBBSteepestDescentAnalysis(kratos_unittest.TestCase):
-    def test_steepest_descent_analysis(self):
-        with kratos_unittest.WorkFolderScope(".", __file__):
+# Temporaly failing
+# @KratosUnittest.skipIf(True, "Temporaly Failing")
+class TestRelaxedGradientProjectionAnalysis(KratosUnittest.TestCase):
+    def test_gradient_projection_analysis(self):
+        with KratosUnittest.WorkFolderScope(".", __file__):
             with open("optimization_parameters.json", "r") as file_input:
                 parameters = Kratos.Parameters(file_input.read())
 
@@ -20,15 +22,17 @@ class TestQNBBSteepestDescentAnalysis(kratos_unittest.TestCase):
                 "reference_file_name"   : "summary_orig.csv",
                 "output_file_name"      : "summary.csv",
                 "remove_output_file"    : true,
-                "comparison_type"       : "deterministic"
+                "comparison_type"       : "csv_file",
+                "tolerance"             : 1e-6,
+                "relative_tolerance"    : 1e-6
             }""")).Execute()
 
     @classmethod
     def tearDownClass(cls) -> None:
-        with kratos_unittest.WorkFolderScope(".", __file__):
+        with KratosUnittest.WorkFolderScope(".", __file__):
             DeleteFileIfExisting("Structure.time")
 
 if __name__ == "__main__":
     Kratos.Tester.SetVerbosity(Kratos.Tester.Verbosity.PROGRESS)  # TESTS_OUTPUTS
-    kratos_unittest.main()
+    KratosUnittest.main()
 
