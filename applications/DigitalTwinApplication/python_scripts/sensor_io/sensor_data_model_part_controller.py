ba896af269bd4b046f53f71f3a2cc6195c3dfb29
sunethwarna <suneth.warna@gmail.com>
2024-04-09 22:14:02 +0200

    add sensor_data_model_part_controller

---
+++ applications/DigitalTwinApplication/python_scripts/sensor_io/sensor_data_model_part_controller.py
@@ -0,0 +1,50 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.DigitalTwinApplication as KratosDT
+from KratosMultiphysics.OptimizationApplication.model_part_controllers.model_part_controller import ModelPartController
+from KratosMultiphysics.OptimizationApplication.utilities.optimization_problem import OptimizationProblem
+from KratosMultiphysics.DigitalTwinApplication.utilities.sensor_io import OpenSensorFile
+from KratosMultiphysics.DigitalTwinApplication.utilities.sensor_utils import GetSensors
+from KratosMultiphysics.OptimizationApplication.utilities.component_data_view import ComponentDataView
+
+def Factory(model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem) -> ModelPartController:
+    if not parameters.Has("settings"):
+        raise RuntimeError(f"SensorDataModelPartController instantiation requires a \"settings\" in parameters [ parameters = {parameters}].")
+    return SensorDataModelPartController(model, parameters["settings"], optimization_problem)
+
+class SensorDataModelPartController(ModelPartController):
+    def __init__(self, model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem):
+        default_settings = Kratos.Parameters("""{
+            "target_model_part_name": "",
+            "sensor_data_file_name" : "",
+            "sensor_data_prefix"    : "",
+            "sensor_model_part_name": "",
+            "list_of_sensors"       : []
+        }""")
+
+        parameters.ValidateAndAssignDefaults(default_settings)
+
+        self.target_model_part_name = model[parameters["target_model_part_name"].GetString()]
+        self.sensor_data_file_name = parameters["sensor_data_file_name"].GetString()
+        self.prefix = parameters["sensor_data_prefix"].GetString()
+
+        sensor_model_part_name = parameters["sensor_model_part_name"].GetString()
+        if model.HasModelPart(sensor_model_part_name):
+            raise RuntimeError(f"The sensor model part name \"{sensor_model_part_name}\" already exists.")
+        self.sensor_model_part = model.CreateModelPart(sensor_model_part_name)
+
+        self.list_of_sensors = GetSensors(self.target_model_part_name, parameters["list_of_sensors"].values())
+
+        # add the list of sensors to optimization problem
+        sensors = ComponentDataView("sensor", optimization_problem)
+        sensors.GetUnBufferedData().SetValue("list_of_sensors", self.list_of_sensors)
+
+    def ImportModelPart(self) -> None:
+        # create nodes for every sensor
+        with OpenSensorFile(self.target_model_part_name, self.sensor_data_file_name, self.prefix, "r") as sensor_io:
+            for i, sensor in enumerate(self.list_of_sensors):
+                sensor_io.Read(sensor)
+                location = sensor.GetLocation()
+                node: Kratos.Node = self.sensor_model_part.CreateNewNode(i + 1, location[0], location[1], location[2])
+
+    def GetModelPart(self) -> Kratos.ModelPart:
+        return self.sensor_model_part
