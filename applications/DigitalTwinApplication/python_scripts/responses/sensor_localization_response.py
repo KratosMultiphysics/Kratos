589f23d36bb44c49490597e946f87d6d6dcca4d9
sunethwarna <suneth.warna@gmail.com>
2024-04-10 15:59:37 +0200

    add sensor localization response

---
+++ applications/DigitalTwinApplication/python_scripts/responses/sensor_localization_response.py
@@ -0,0 +1,91 @@
+from typing import Optional
+
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.OptimizationApplication as KratosOA
+import KratosMultiphysics.DigitalTwinApplication as KratosDT
+from KratosMultiphysics.OptimizationApplication.responses.response_function import ResponseFunction
+from KratosMultiphysics.OptimizationApplication.responses.response_function import SupportedSensitivityFieldVariableTypes
+from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import SupportedSensitivityFieldVariableTypes
+from KratosMultiphysics.OptimizationApplication.utilities.model_part_utilities import ModelPartOperation
+from KratosMultiphysics.OptimizationApplication.utilities.optimization_problem import OptimizationProblem
+from KratosMultiphysics.OptimizationApplication.utilities.component_data_view import ComponentDataView
+
+def Factory(model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem) -> ResponseFunction:
+    if not parameters.Has("name"):
+        raise RuntimeError(f"SensorLocalizationResponse instantiation requires a \"name\" in parameters [ parameters = {parameters}].")
+    if not parameters.Has("settings"):
+        raise RuntimeError(f"SensorLocalizationResponse instantiation requires a \"settings\" in parameters [ parameters = {parameters}].")
+    return SensorLocalizationResponse(parameters["name"].GetString(), model, parameters["settings"], optimization_problem)
+
+
+class SensorLocalizationResponse(ResponseFunction):
+    def __init__(self, name: str, model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem):
+        super().__init__(name)
+
+        default_settings = Kratos.Parameters("""{
+            "evaluated_model_part_names" : [
+                "PLEASE_PROVIDE_A_MODEL_PART_NAME"
+            ],
+            "mask_model_part_name": "",
+            "mask_expression_name": "YOUNG_MODULUS_SENSITIVITY_mask",
+            "p_coefficient"       : 4
+        }""")
+        parameters.ValidateAndAssignDefaults(default_settings)
+
+        self.model = model
+
+        evaluated_model_part_names = parameters["evaluated_model_part_names"].GetStringArray()
+        if len(evaluated_model_part_names) == 0:
+            raise RuntimeError(f"No model parts were provided for SensorLocalizationResponse. [ response name = \"{self.GetName()}\"]")
+
+        self.model_part_operation = ModelPartOperation(self.model, ModelPartOperation.OperationType.UNION, f"response_{self.GetName()}", evaluated_model_part_names, False)
+        self.model_part: Optional[Kratos.ModelPart] = None
+        self.optimization_problem = optimization_problem
+        self.mask_expression_name = parameters["mask_expression_name"].GetString()
+        self.mask_model_part_name = parameters["mask_model_part_name"].GetString()
+        self.p_coefficient = parameters["p_coefficient"].GetDouble()
+
+    def GetImplementedPhysicalKratosVariables(self) -> 'list[SupportedSensitivityFieldVariableTypes]':
+        return [KratosDT.SENSOR_STATUS]
+
+    def Initialize(self) -> None:
+        self.model_part = self.model_part_operation.GetModelPart()
+        self.mask_model_part = self.model[self.mask_model_part_name]
+        self.utils = KratosDT.SensorLocalizationResponseUtils(self.model_part, self.p_coefficient)
+
+    def Check(self) -> None:
+        pass
+
+    def Finalize(self) -> None:
+        pass
+
+    def GetEvaluatedModelPart(self) -> Kratos.ModelPart:
+        if self.model_part is None:
+            raise RuntimeError("Please call SensorLocalizationResponse::Initialize first.")
+        return self.model_part
+
+    def GetAnalysisModelPart(self) -> Kratos.ModelPart:
+        return None
+
+    def CalculateValue(self) -> float:
+        list_of_sensors: 'list[KratosDT.Sensors.Sensor]' = ComponentDataView("sensor", self.optimization_problem).GetUnBufferedData().GetValue("list_of_sensors")
+        list_of_masks = [sensor.GetElementExpression(self.mask_expression_name) for sensor in list_of_sensors]
+        return self.utils.CalculateValue(list_of_masks)
+
+    def CalculateGradient(self, physical_variable_collective_expressions: 'dict[SupportedSensitivityFieldVariableTypes, KratosOA.CollectiveExpression]') -> None:
+        # make everything zeros
+        for physical_variable, collective_expression in physical_variable_collective_expressions.items():
+            for container_expression in collective_expression.GetContainerExpressions():
+                Kratos.Expression.LiteralExpressionIO.SetDataToZero(container_expression, physical_variable)
+
+        for physical_variable, collective_expression in physical_variable_collective_expressions.items():
+            if physical_variable == KratosDT.SENSOR_STATUS:
+                list_of_sensors: 'list[KratosDT.Sensors.Sensor]' = ComponentDataView("sensor", self.optimization_problem).GetUnBufferedData().GetValue("list_of_sensors")
+                list_of_masks = [sensor.GetElementExpression(self.mask_expression_name) for sensor in list_of_sensors]
+                sensitivity = self.utils.CalculateGradient(list_of_masks)
+                collective_expression.GetContainerExpressions()[0].SetExpression(sensitivity.GetExpression())
+            else:
+                raise RuntimeError("Unsupported physical variable.")
+
+    def __str__(self) -> str:
+        return f"Response [type = {self.__class__.__name__}, name = {self.GetName()}, model part name = {self.model_part.FullName()}]"
