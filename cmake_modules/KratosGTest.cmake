9a2089e07267c10a5dbe74c4271cb95a8509c38f
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-04-11 11:26:06 +0200

    Only add a GTest target if there are any unit test source files

--- cmake_modules/KratosGTest.cmake
+++ cmake_modules/KratosGTest.cmake
@@ -6,21 +6,24 @@ macro(kratos_add_gtests)
 	
 	cmake_parse_arguments(KRATOS_ADD_GTEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-	include(GoogleTest)
+	if(KRATOS_ADD_GTEST_SOURCES)
+	    include(GoogleTest)
 
-	if(KRATOS_ADD_GTEST_USE_MPI)
-		set(KTU_MPI "MPI")
+	    if(KRATOS_ADD_GTEST_USE_MPI)
+		    set(KTU_MPI "MPI")
+	    endif()
+
+	    add_executable("${KRATOS_ADD_GTEST_TARGET}Test" ${KRATOS_ADD_GTEST_SOURCES})
+	    target_link_libraries("${KRATOS_ADD_GTEST_TARGET}Test" ${KRATOS_ADD_GTEST_TARGET} "Kratos${KTU_MPI}CoreTestUtilities" GTest::gtest_main GTest::gmock_main)
+	    set_target_properties("${KRATOS_ADD_GTEST_TARGET}Test" PROPERTIES COMPILE_DEFINITIONS "KRATOS_TEST_CORE=IMPORT,API")
+
+	    install(TARGETS ${KRATOS_ADD_GTEST_TARGET}Test DESTINATION test)
+
+	    if(DEFINED KRATOS_ADD_GTEST_WORKING_DIRECTORY)
+		    gtest_discover_tests(${KRATOS_ADD_GTEST_TARGET}Test DISCOVERY_MODE PRE_TEST WORKING_DIRECTORY "${KRATOS_ADD_GTEST_WORKING_DIRECTORY}")
+	    else()
+		    gtest_discover_tests(${KRATOS_ADD_GTEST_TARGET}Test DISCOVERY_MODE PRE_TEST )
+	    endif()
 	endif()
-	
-	add_executable("${KRATOS_ADD_GTEST_TARGET}Test" ${KRATOS_ADD_GTEST_SOURCES})
-	target_link_libraries("${KRATOS_ADD_GTEST_TARGET}Test" ${KRATOS_ADD_GTEST_TARGET} "Kratos${KTU_MPI}CoreTestUtilities" GTest::gtest_main GTest::gmock_main)
-	set_target_properties("${KRATOS_ADD_GTEST_TARGET}Test" PROPERTIES COMPILE_DEFINITIONS "KRATOS_TEST_CORE=IMPORT,API")
 
-	install(TARGETS ${KRATOS_ADD_GTEST_TARGET}Test DESTINATION test)
-
-	if(DEFINED KRATOS_ADD_GTEST_WORKING_DIRECTORY)
-		gtest_discover_tests(${KRATOS_ADD_GTEST_TARGET}Test DISCOVERY_MODE PRE_TEST WORKING_DIRECTORY "${KRATOS_ADD_GTEST_WORKING_DIRECTORY}")
-	else()
-		gtest_discover_tests(${KRATOS_ADD_GTEST_TARGET}Test DISCOVERY_MODE PRE_TEST )
-	endif()
 endmacro(kratos_add_gtests)
