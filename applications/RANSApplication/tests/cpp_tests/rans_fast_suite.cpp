906c5ecabbbc502e1b56a962bf0b41f7173b42ff
Carlos Roig <roigcarlo@gmail.com>
2024-04-12 10:54:05 +0200

    Fixing RANS cpp tests

--- applications/RANSApplication/tests/cpp_tests/rans_fast_suite.cpp
+++ applications/RANSApplication/tests/cpp_tests/rans_fast_suite.cpp
@@ -17,6 +17,7 @@
 // Project includes
 #include "testing/testing.h"
 #include "rans_application.h"
+#include "fluid_dynamics_application.h"
 
 int main(int argc, char* argv[]) 
 {
@@ -25,6 +26,11 @@ int main(int argc, char* argv[])
     Kratos::Testing::mApplicationInitializerList.push_back([](std::vector<Kratos::KratosApplication::Pointer> & rRegisteredApplications, Kratos::Kernel & rKernel) {
       if (!rKernel.IsImported("RANSApplication")) {
         auto pApplication = std::make_shared<Kratos::KratosRANSApplication>();
+        rKernel.ImportApplication(pApplication);
+        rRegisteredApplications.push_back(std::move(pApplication));
+      }
+      if (!rKernel.IsImported("FluidDynamicsApplication")) {
+        auto pApplication = std::make_shared<Kratos::KratosFluidDynamicsApplication>();
         rKernel.ImportApplication(pApplication);
         rRegisteredApplications.push_back(std::move(pApplication));
       }
