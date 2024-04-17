a0f0f9464a90acfbcbbcd86454da1b9266999f6e
sunethwarna <suneth.warna@gmail.com>
2024-04-17 17:16:36 +0200

    renaming application

--- applications/DigitalTwinApplication/python_scripts/sensor_sensitivity_solvers/sensor_sensitivity_analysis.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-import KratosMultiphysics.DigitalTwinApplication as KratosDT
-from KratosMultiphysics.analysis_stage import AnalysisStage
-
-class SensorSensitivityAnalysis(AnalysisStage, abc.ABC):
-    @abc.abstractmethod
-    def GetListOfSensors(self) -> 'list[KratosDT.Sensors.Sensor]':
-        pass
