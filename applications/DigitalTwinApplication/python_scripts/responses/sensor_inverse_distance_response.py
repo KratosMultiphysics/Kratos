8be35ce0aca649dd521c1bd70801f87aba2ea7e5
sunethwarna <suneth.warna@gmail.com>
2024-04-17 08:06:37 +0200

    add sensor inverse distance response

--- applications/DigitalTwinApplication/python_scripts/responses/sensor_inverse_distance_response.py
+++ applications/DigitalTwinApplication/python_scripts/responses/sensor_inverse_distance_response.py
@@ -1,6 +1,4 @@
 from typing import Optional
-from math import exp
-import numpy as np
 
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
@@ -9,24 +7,23 @@ from KratosMultiphysics.OptimizationApplication.responses.response_function impo
 from KratosMultiphysics.OptimizationApplication.responses.response_function import SupportedSensitivityFieldVariableTypes
 from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import SupportedSensitivityFieldVariableTypes
 from KratosMultiphysics.OptimizationApplication.utilities.model_part_utilities import ModelPartOperation
-from KratosMultiphysics.OptimizationApplication.utilities.component_data_view import ComponentDataView
-from KratosMultiphysics.OptimizationApplication.utilities.optimization_problem import OptimizationProblem
 
-def Factory(model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem) -> ResponseFunction:
+def Factory(model: Kratos.Model, parameters: Kratos.Parameters, _) -> ResponseFunction:
     if not parameters.Has("name"):
         raise RuntimeError(f"SensorInverseDistanceResponse instantiation requires a \"name\" in parameters [ parameters = {parameters}].")
     if not parameters.Has("settings"):
         raise RuntimeError(f"SensorInverseDistanceResponse instantiation requires a \"settings\" in parameters [ parameters = {parameters}].")
-    return SensorInverseDistanceResponse(parameters["name"].GetString(), model, parameters["settings"], optimization_problem)
+    return SensorInverseDistanceResponse(parameters["name"].GetString(), model, parameters["settings"])
 
 class SensorInverseDistanceResponse(ResponseFunction):
-    def __init__(self, name: str, model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem):
+    def __init__(self, name: str, model: Kratos.Model, parameters: Kratos.Parameters):
         super().__init__(name)
 
         default_settings = Kratos.Parameters("""{
             "evaluated_model_part_names" : [
                 "PLEASE_PROVIDE_A_MODEL_PART_NAME"
-            ]
+            ],
+            "p_coefficient": 4
         }""")
         parameters.ValidateAndAssignDefaults(default_settings)
 
@@ -38,8 +35,7 @@ class SensorInverseDistanceResponse(ResponseFunction):
 
         self.model_part_operation = ModelPartOperation(self.model, ModelPartOperation.OperationType.UNION, f"response_{self.GetName()}", evaluated_model_part_names, False)
         self.model_part: Optional[Kratos.ModelPart] = None
-
-        self.optimization_problem = optimization_problem
+        self.p_coefficient = parameters["p_coefficient"].GetDouble()
 
     def GetImplementedPhysicalKratosVariables(self) -> 'list[SupportedSensitivityFieldVariableTypes]':
         return [KratosDT.SENSOR_STATUS]
@@ -47,17 +43,8 @@ class SensorInverseDistanceResponse(ResponseFunction):
     def Initialize(self) -> None:
         self.model_part = self.model_part_operation.GetModelPart()
 
-        # get the distance matrix
-        list_of_sensors: 'list[KratosDT.Sensors.Sensor]' = ComponentDataView("sensor", self.optimization_problem).GetUnBufferedData().GetValue("list_of_sensors")
-
-        n = len(list_of_sensors)
-        self.distance_matrix = Kratos.Matrix(n, n, 0)
-        for i in range(n):
-            for j in range(i + 1, n):
-                distance = list_of_sensors[i].GetLocation() - list_of_sensors[j].GetLocation()
-                distance = (distance[0] ** 2 + distance[1] ** 2 + distance[2] ** 2) ** (0.5)
-                self.distance_matrix[i, j] = distance
-                self.distance_matrix[j, i] = distance
+        self.utils = KratosDT.SensorInverseDistanceResponseUtils(self.model_part, self.p_coefficient)
+        self.utils.Initialize()
 
     def Check(self) -> None:
         pass
@@ -74,25 +61,14 @@ class SensorInverseDistanceResponse(ResponseFunction):
         return None
 
     def CalculateValue(self) -> float:
-        value = 0.0
-        for i, node_i in enumerate(self.model_part.Nodes):
-            for j, node_j in enumerate(self.model_part.Nodes):
-                theta_ij = node_i.GetValue(KratosDT.SENSOR_STATUS) * node_j.GetValue(KratosDT.SENSOR_STATUS)
-                value += exp(-theta_ij * self.distance_matrix[i, j])
-        return value
+        return self.utils.CalculateValue()
 
     def CalculateGradient(self, physical_variable_collective_expressions: 'dict[SupportedSensitivityFieldVariableTypes, KratosOA.CollectiveExpression]') -> None:
-        gradient = np.zeros(self.distance_matrix.Size1(), dtype=np.float64)
-        for k, node_k in enumerate(self.model_part.Nodes):
-            theta_k = node_k.GetValue(KratosDT.SENSOR_STATUS)
-            for i, node_i in enumerate(self.model_part.Nodes):
-                theta_i = node_i.GetValue(KratosDT.SENSOR_STATUS)
-                gradient[k] -= 2 * theta_i * self.distance_matrix[i, k] * exp (-theta_i * theta_k * self.distance_matrix[i, k])
-
         for physical_variable, collective_expression in physical_variable_collective_expressions.items():
             for container_expression in collective_expression.GetContainerExpressions():
                 Kratos.Expression.LiteralExpressionIO.SetDataToZero(container_expression, physical_variable)
-        Kratos.Expression.CArrayExpressionIO.Read(physical_variable_collective_expressions[KratosDT.SENSOR_STATUS].GetContainerExpressions()[0], gradient)
+
+        physical_variable_collective_expressions[KratosDT.SENSOR_STATUS].GetContainerExpressions()[0].SetExpression(self.utils.CalculateGradient().GetExpression())
 
     def __str__(self) -> str:
         return f"Response [type = {self.__class__.__name__}, name = {self.GetName()}, model part name = {self.model_part.FullName()}]"
