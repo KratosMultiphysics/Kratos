7af153a2bcd409176bc4f2fa5c5d2dfe6d10d2ed
sunethwarna <suneth.warna@gmail.com>
2024-04-19 19:30:34 +0200

    add philipp's comment

--- applications/SystemIdentificationApplication/tests/test_system_identification.py
+++ applications/SystemIdentificationApplication/tests/test_system_identification.py
@@ -18,7 +18,7 @@ class TestSystemIdentification(UnitTest.TestCase):
 
         data = numpy.loadtxt("auxiliary_files/damaged_problem/measured_data.csv", comments="#", usecols=[0,3,4,5,6], delimiter=",")
         ref_data = numpy.loadtxt("auxiliary_files/damaged_problem/measured_data_ref.csv", comments="#", usecols=[0,3,4,5,6], delimiter=",")
-        self.assertAlmostEqual(numpy.linalg.norm(data - ref_data), 0.0, 9)
+        self.assertTrue(numpy.allclose(data, ref_data, 1e-12, 1e-12))
 
     def test_SystemIdentification(self):
         model = Kratos.Model()
