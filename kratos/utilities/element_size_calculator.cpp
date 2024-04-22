df33d1781a4a7031eaa2546cfdb2ad46f33a88b4
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2024-04-22 13:56:37 +0200

    fix 2.0 multiplier factor

--- kratos/utilities/element_size_calculator.cpp
+++ kratos/utilities/element_size_calculator.cpp
@@ -867,7 +867,7 @@ double ElementSizeCalculator<2,4>::AverageElementSizeDerivative(
 template<>
 double ElementSizeCalculator<2,9>::AverageElementSize(const Geometry<Node >& rGeometry)
 {
-    return rGeometry.Length();
+    return rGeometry.Length() * 2.0;
 }
 
 template<>
@@ -893,9 +893,9 @@ double ElementSizeCalculator<2,9>::AverageElementSizeDerivative(
     const auto detJ = MathUtils<double>::Det2(jacobian);
 
     if (detJ > 0.0) {
-        return 0.5 * detJ_derivative / (std::sqrt(detJ));
+        return detJ_derivative / (std::sqrt(detJ));
     } else if (detJ < 0.0) {
-        return -0.5 * detJ_derivative / (std::sqrt(-detJ));
+        return -detJ_derivative / (std::sqrt(-detJ));
     } else {
         return 0.0;
     }
