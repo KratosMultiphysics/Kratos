275dff40c574beb34ff8c54857fdbd739e052790
ge69vud <manuel.messmer@tum.de>
2024-04-26 10:53:37 +0200

    Add quadrature.

---
+++ external_libraries/queso/quadrature/integration_points_1d/integration_points_factory_1d.cpp
@@ -0,0 +1,207 @@
+// Author: Manuel Meßmer
+// Email: manuel.messmer@tum.de
+
+//// STL includes
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+#include <utility>
+#include <cmath>
+//// Project includes
+#include "quadrature/integration_points_1d/integration_points_factory_1d.h"
+
+namespace queso {
+
+typedef std::size_t SizeType;
+typedef std::vector<std::array<double,2>> Ip1DVectorType;
+typedef Unique<Ip1DVectorType> Ip1DVectorPtrType;
+typedef std::vector<std::vector<std::array<double, 2>>> Ip1DVectorVectorType;
+
+// Public member functions
+Ip1DVectorPtrType IntegrationPointFactory1D::GetGGQ( SizeType PolynomialDegree, SizeType NumberKnotSpans, IntegrationMethodType Method ){
+    if( Method == GGQ_Optimal || Method == GGQ_Reduced1 || Method == GGQ_Reduced2){
+        return GetGGQPoints(PolynomialDegree, NumberKnotSpans, Method);
+    } else
+    {
+        QuESo_ERROR << "Method not available\n";
+    }
+}
+
+Ip1DVectorPtrType IntegrationPointFactory1D::GetGauss( SizeType PolynomialDegree, IntegrationMethodType Method ){
+    switch(Method)
+    {
+        case Gauss:
+            return MakeUnique<Ip1DVectorType>(mGaussLegendrePoints[PolynomialDegree]);
+        case Gauss_Reduced1:
+            return MakeUnique<Ip1DVectorType>(mGaussLegendrePoints[PolynomialDegree-1]);
+        case Gauss_Reduced2:
+            return MakeUnique<Ip1DVectorType>(mGaussLegendrePoints[PolynomialDegree-2]);
+        default:
+            QuESo_ERROR << "Method not available\n";
+            break;
+    }
+}
+
+const std::pair<SizeType, SizeType> IntegrationPointFactory1D::GetSpaceDimension(SizeType PolynomialDegre, IntegrationMethodType Method ){
+    switch(Method)
+    {
+        case GGQ_Optimal:
+            return {2*PolynomialDegre, PolynomialDegre-2};
+        case GGQ_Reduced1:
+            return {2*PolynomialDegre-1, PolynomialDegre-2};
+        case GGQ_Reduced2:
+            return {2*PolynomialDegre-2, PolynomialDegre-2};
+        default:
+            QuESo_ERROR << "Method not available\n";
+            break;
+    }
+}
+
+Ip1DVectorPtrType IntegrationPointFactory1D::GetGGQPoints(SizeType PolynomialDegree, SizeType NumberKnotSpans, IntegrationMethodType Method){
+    const double a = 0.0;
+    const double b = 1.0;
+
+    const auto dimension = GetSpaceDimension(PolynomialDegree, Method);
+
+    const SizeType e = NumberKnotSpans;  // Number of elements
+    const SizeType p = dimension.first;  // Degree
+    const SizeType r = dimension.second; // Continuity
+
+    const SizeType n = (p+1)*2 + (e-1)*(p-r) - p - 1; // Number of dofs
+    const SizeType m = static_cast<SizeType>( std::ceil(n/2.0) );              // Number of quadrature points
+
+    // Get correct base rule points
+    Ip1DVectorVectorPtrType p_base_points{};
+    if( p == 4 && r == 0 ){
+        if( e % 2 == 0 ){
+            p_base_points = S_4_0_base_even;
+        }
+        else {
+            p_base_points = S_4_0_base_odd;
+        }
+    }
+    else if(p == 6 && r == 2){
+        if( e % 2 == 0 ){
+            p_base_points = S_6_2_base_even;
+        }
+        else {
+            p_base_points = S_6_2_base_odd;
+        }
+    }
+    else if( p == 4 && r == 1 ){
+        if( e % 2 == 0 ){
+            int odd = m % 2;
+            p_base_points = mBasePointsReduced2[PolynomialDegree-2][odd];
+        }
+        else {
+            const SizeType odd = m % 2;
+            if( odd ){
+                p_base_points = S_4_1_base_odd;
+            } else {
+                // Requires sepcial rule!!
+                p_base_points = S_4_1_base_even_2;
+            }
+        }
+    }
+    else {
+        const SizeType odd = m % 2;
+        switch(Method)
+        {
+            case GGQ_Optimal:
+                p_base_points = mBasePointsOptimal[PolynomialDegree-2][odd];
+                break;
+            case GGQ_Reduced1:
+                p_base_points = mBasePointsReduced1[PolynomialDegree-2][odd];
+                break;
+            case GGQ_Reduced2:
+                p_base_points = mBasePointsReduced2[PolynomialDegree-2][odd];
+                break;
+            default:
+                QuESo_ERROR << "Method not available 1.\n";
+                break;
+        }
+    }
+
+    SizeType m1 = (*p_base_points)[0].size(); // boundary nodes
+    SizeType m2 = (*p_base_points)[1].size(); // internal nodes
+    SizeType m3 = (*p_base_points)[2].size(); // center nodes
+
+    Ip1DVectorType points(m);
+    if( 2*m == n ){ // For odd number of nodes
+        if( m > 2*m1 ){
+            const Ip1DVectorVectorType& base_points =  *p_base_points;
+            const SizeType z = static_cast<SizeType>( std::ceil(0.5*m) );
+            std::copy_n(base_points[0].begin(), m1, points.begin());
+            SizeType left = static_cast<SizeType>( std::ceil(base_points[0][m1-1][0]) );
+            const SizeType right = static_cast<SizeType>( std::ceil(base_points[1][m2-1][0]) );
+            SizeType ii = m1;
+            while( ii < z ){
+                std::copy_n(base_points[1].begin(), m2, points.begin()+ii);
+                std::for_each(points.begin()+ii, points.begin()+ii+m2, [left](auto& rValue) { rValue[0] +=left;});
+                left += right;
+                ii += m2;
+            }
+            std::reverse_copy(points.begin(), points.begin()+z, points.end()- z );
+            std::for_each(points.end()-z, points.end(), [e](auto& rValue) { rValue[0] = e - rValue[0];});
+        }
+        else {
+            switch(Method)
+            {
+                case GGQ_Optimal:
+                    return MakeUnique<Ip1DVectorType>( (*mPrecomputedPointsOptimal[PolynomialDegree-2])[e-1]);
+                case GGQ_Reduced1:
+                    return MakeUnique<Ip1DVectorType>( (*mPrecomputedPointsReduced1[PolynomialDegree-2])[e-1]);
+                case GGQ_Reduced2:
+                    return MakeUnique<Ip1DVectorType>( (*mPrecomputedPointsReduced2[PolynomialDegree-2])[e-1]);
+                default:
+                    QuESo_ERROR << "Method not available 2.\n";
+                    break;
+            }
+        }
+    }
+    else { // For odd number of nodes
+        if( m > 2*(m1+m3)-1 ){
+            const Ip1DVectorVectorType& base_points =  *p_base_points;
+            const SizeType z = static_cast<SizeType>( std::ceil(0.5*m) );
+            std::copy_n(base_points[0].begin(), m1, points.begin());
+            SizeType left = static_cast<SizeType>( std::ceil(base_points[0].back()[0]) );
+            const SizeType right = static_cast<SizeType>( std::ceil(base_points[1].back()[0]) );
+            SizeType ii = m1;
+            while( ii < z ){
+                std::copy_n(base_points[1].begin(), m2, points.begin()+ii);
+                std::for_each(points.begin()+ii, points.begin()+ii+m2, [left](auto& rValue) { rValue[0] +=left;});
+                left += right;
+                ii += m2;
+            }
+
+            std::copy_n(base_points[2].begin(), m3, points.begin()+z-m3);
+            std::for_each(points.begin()+z-m3, points.begin()+z, [e](auto& rValue) { rValue[0] +=0.5*e;});
+
+            std::reverse_copy(points.begin(), points.begin()+z, points.end()- z );
+            std::for_each(points.end()-z, points.end(), [e](auto& rValue) { rValue[0] = e - rValue[0];});
+        }
+        else {
+            switch(Method)
+            {
+                case GGQ_Optimal:
+                    return MakeUnique<Ip1DVectorType>( (*mPrecomputedPointsOptimal[PolynomialDegree-2])[e-1]);
+                case GGQ_Reduced1:
+                    return MakeUnique<Ip1DVectorType>( (*mPrecomputedPointsReduced1[PolynomialDegree-2])[e-1]);
+                case GGQ_Reduced2:
+                    return MakeUnique<Ip1DVectorType>( (*mPrecomputedPointsReduced2[PolynomialDegree-2])[e-1]);
+                default:
+                    QuESo_ERROR << "Method not available 3.\n";
+                    break;
+            }
+        }
+    }
+
+    // Scale points to desired interval (a,b)
+    const double h = (b-a) / e;
+    std::for_each(points.begin(), points.end(), [a, h](auto& rValue) { rValue[0] = a + h*rValue[0];
+                                                                        rValue[1] *= h; });
+
+    return MakeUnique<Ip1DVectorType>(points);
+}
+
+} // End namespace queso
---
+++ external_libraries/queso/quadrature/integration_points_1d/integration_points_factory_1d.h
@@ -0,0 +1,153 @@
+// Author: Manuel Meßmer
+// Email: manuel.messmer@tum.de
+
+#ifndef INTEGRATION_POINTS_FACTORY_1D_H
+#define INTEGRATION_POINTS_FACTORY_1D_H
+
+//// STL includes
+#include <vector>
+#include <array>
+#include <memory>
+//// Project includes
+#include "includes/parameters.h"
+
+namespace queso {
+
+///@name QuESo Classes
+///@{
+
+////
+/**
+ * @class  IntegrationPointFactory1D
+ * @author Manuel Messmer
+ * @brief  Factory for 1D Integration points for single and multiple knot spans.
+ * @details Available Quadrature rules:
+ *          {Gauss, Gauss_Reduced1, Gauss_Reduced2, GGQ_Optimal, GGQ_Reduced1, GGQ_Reduced2}
+ * @todo Wrap QuESo in namespace and put enum outside the class.
+*/
+class IntegrationPointFactory1D {
+public:
+    ///@name Type Definitions
+    ///@{
+    typedef std::size_t SizeType;
+    typedef std::vector<std::array<double,2>> Ip1DVectorType;
+    typedef Unique<Ip1DVectorType> Ip1DVectorPtrType;
+    typedef std::vector<std::vector<std::array<double, 2>>> Ip1DVectorVectorType;
+    typedef std::shared_ptr<Ip1DVectorVectorType> Ip1DVectorVectorPtrType;
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /// @brief Get Generalized Gaussian Quadrature (GGQ) 1D rules.
+    /// @param PolynomialDegree
+    /// @param NumberKnotSpans
+    /// @param Method options - {GGQ_Optimal, GGQ_Reduced1, GGQ_Reduced2}
+    /// @return IntegrationPoint1DVectorPtrTypes. Points are defined on the interval (0,1).
+    static Ip1DVectorPtrType GetGGQ( SizeType PolynomialDegree, SizeType NumberKnotSpans, IntegrationMethodType Method );
+
+    /// @brief Get standard 1D Gauss-Legendre quadrature rules.
+    /// @param PolynomialDegree
+    /// @param Method options - {Gauss, Gauss_Reduced1, Gauss_Reduced2
+    /// @return IntegrationPoint1DVectorPtrTypes. Points are defined on the interval (0,1).
+    static Ip1DVectorPtrType GetGauss( SizeType PolynomialDegree, IntegrationMethodType Method );
+
+    ///@}
+private:
+
+    /// @name Private operations
+    ///@{
+
+    /// @brief Get integration space dimension for GGQ.
+    /// @param PolynomialDegre
+    /// @param Method options - {GGQ_Optimal, GGQ_Reduced1, GGQ_Reduced2}
+    /// @return std::pair<SizeType, SizeType> {Degree, Continuity}
+    static const std::pair<SizeType, SizeType> GetSpaceDimension(SizeType PolynomialDegre, IntegrationMethodType Method );
+
+    /// @brief   Constructs GGQ rules from precomputed rules.
+    /// @details Algorithm taken from: R. Hiemstra et al. Optimal and reduced quadrature rules for tensor product and hierarchically
+    ///          refined splines in isogeometric analysis. Comput. Methods Appl. Mech. Engrg. 316 (2017) 966–1004,
+    ///          http://dx.doi.org/10.1016/j.cma.2016.10.049
+    /// @param PolynomialDegree
+    /// @param NumberKnotSpans
+    /// @param Method options - {GGQ_Optimal, GGQ_Reduced1, GGQ_Reduced2}
+    /// @return InetgrationPoint1DVectorPtrType
+    static Ip1DVectorPtrType GetGGQPoints(SizeType PolynomialDegree, SizeType NumberKnotSpans, IntegrationMethodType Method);
+    ///@}
+
+    /// @name Private operations
+    ///@{
+
+    /// Standard Gauss Legendre points, index=[p-1]
+    static const Ip1DVectorVectorType mGaussLegendrePoints;
+
+    /// Base points (GGQ Optimal)
+    /// mBasePointsOptimal contains:
+    /// { { S_4_0_base_even, S_4_0_base_odd},
+    ///   { S_6_1_base_even, S_6_1_base_odd},
+    ///   { S_8_2_base_even, S_8_2_base_odd} }
+    static const std::vector<std::array<Ip1DVectorVectorPtrType,2>> mBasePointsOptimal;
+    static const Ip1DVectorVectorPtrType S_4_0_base_even;
+    static const Ip1DVectorVectorPtrType S_4_0_base_odd;
+    static const Ip1DVectorVectorPtrType S_6_1_base_even;
+    static const Ip1DVectorVectorPtrType S_6_1_base_odd;
+    static const Ip1DVectorVectorPtrType S_8_2_base_even;
+    static const Ip1DVectorVectorPtrType S_8_2_base_odd;
+    /// Precomputed points (GGQ Optimal)
+    /// mPrecomputedPointsOptimal:
+    /// {S_4_0_precomputed, S_6_1_precomputed, S_8_2_precomputed}
+    static const std::vector<Ip1DVectorVectorPtrType> mPrecomputedPointsOptimal;
+    static const Ip1DVectorVectorPtrType S_4_0_precomputed;
+    static const Ip1DVectorVectorPtrType S_6_1_precomputed;
+    static const Ip1DVectorVectorPtrType S_8_2_precomputed;
+
+    /// Base points (GGQ Reduced1)
+    /// mBasePointsReduced1 contains:
+    /// { { S_3_0_base_even, S_3_0_base_odd},
+    ///   { S_5_1_base_even, S_5_1_base_odd},
+    ///   { S_7_2_base_even, S_7_2_base_odd} }
+    static const std::vector<std::array<Ip1DVectorVectorPtrType,2>> mBasePointsReduced1;
+    static const Ip1DVectorVectorPtrType S_3_0_base_even;
+    static const Ip1DVectorVectorPtrType S_3_0_base_odd;
+    static const Ip1DVectorVectorPtrType S_5_1_base_even;
+    static const Ip1DVectorVectorPtrType S_5_1_base_odd;
+    static const Ip1DVectorVectorPtrType S_7_2_base_even;
+    static const Ip1DVectorVectorPtrType S_7_2_base_odd;
+    /// Precomputed points (GGQ Reduced1)
+    /// mPrecomputedPointsReduced1:
+    /// {S_3_0_precomputed, S_5_1_precomputed, S_7_2_precomputed}
+    static const std::vector<Ip1DVectorVectorPtrType> mPrecomputedPointsReduced1;
+    static const Ip1DVectorVectorPtrType S_3_0_precomputed;
+    static const Ip1DVectorVectorPtrType S_5_1_precomputed;
+    static const Ip1DVectorVectorPtrType S_7_2_precomputed;
+
+    /// Base points (GGQ Reduced2)
+    /// mBasePointsReduced2:
+    /// { { S_2_0_base_even, S_2_0_base_odd},
+    ///   { S_4_1_base_even, S_4_1_base_odd},
+    ///   { S_6_2_base_even, S_6_2_base_odd} }
+    static const std::vector<std::array<Ip1DVectorVectorPtrType,2>> mBasePointsReduced2;
+    static const Ip1DVectorVectorPtrType S_2_0_base_even;
+    static const Ip1DVectorVectorPtrType S_2_0_base_odd;
+    static const Ip1DVectorVectorPtrType S_4_1_base_even;
+    /// Required for odd element numbers, but even quadrature rules.
+    static const Ip1DVectorVectorPtrType S_4_1_base_even_2;
+    static const Ip1DVectorVectorPtrType S_4_1_base_odd;
+    static const Ip1DVectorVectorPtrType S_6_2_base_even;
+    static const Ip1DVectorVectorPtrType S_6_2_base_odd;
+
+    /// Precomputed points (GGQ Reduced2)
+    /// mPrecomputedPointsReduced2:
+    /// {S_2_0_precomputed, S_4_1_precomputed, S_6_2_precomputed}
+    static const std::vector<Ip1DVectorVectorPtrType> mPrecomputedPointsReduced2;
+    static const Ip1DVectorVectorPtrType S_2_0_precomputed;
+    static const Ip1DVectorVectorPtrType S_4_1_precomputed;
+    static const Ip1DVectorVectorPtrType S_6_2_precomputed;
+
+    ///@}
+
+}; // End IntegrationPointFactory1D class
+///@} // End classes
+} // End namespace queso
+
+#endif // INTEGRATION_POINTS_FACTORY_1D_H
