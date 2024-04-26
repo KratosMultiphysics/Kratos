3d82af9227db8928652f5fe519006e4f6794442e
Richard Faasse <richard.faasse@deltares.nl>
2024-04-26 13:36:04 +0200

    Clean Kratos_stages.py

--- applications/GeoMechanicsApplication/tests/test_settlement_workflow/Kratos_stages.py
+++ applications/GeoMechanicsApplication/tests/test_settlement_workflow/Kratos_stages.py
@@ -5,16 +5,10 @@ from KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis import Geo
 
 if __name__ == "__main__":
 
-    currentWorking = os.getcwd()
-
     # construct parameterfile names of stages to run
-    project_path = r"C:\Users\graaf\OneDrive - Stichting Deltares\Documents\settlement_app\scenario_1\input_files"
-    n_stages = 4
-    parameter_file_names = [os.path.join(project_path, 'ProjectParameters_stage' + str(i + 1) + '.json') for i in
+    n_stages = 1
+    parameter_file_names = ['ProjectParameters_stage' + str(i + 1) + '.json' for i in
                             range(n_stages)]
-
-    # change to project directory
-    os.chdir(project_path)
 
     # setup stages from parameterfiles
     parameters_stages = [None] * n_stages
@@ -28,6 +22,3 @@ if __name__ == "__main__":
     # execute the stages
     for stage in stages:
         stage.Run()
-
-    # back to working directory
-    os.chdir(currentWorking)
