57db926ef7395021697608b63d9385b78d115b61
Carlos Roig <roigcarlo@gmail.com>
2024-04-26 11:16:20 +0200

    Fixing dependency from structural

--- applications/ConstitutiveLawsApplication/tests/cpp_tests/constitutive_laws_fast_suite.cpp
+++ applications/ConstitutiveLawsApplication/tests/cpp_tests/constitutive_laws_fast_suite.cpp
@@ -17,12 +17,19 @@
 // Project includes
 #include "testing/testing.h"
 #include "constitutive_laws_application.h"
+#include "structural_mechanics_application.h"
 
 int main(int argc, char* argv[]) 
 {
     ::testing::InitGoogleTest(&argc, argv);
 
     Kratos::Testing::mApplicationInitializerList.push_back([](std::vector<Kratos::KratosApplication::Pointer> & rRegisteredApplications, Kratos::Kernel & rKernel) {
+      if (!rKernel.IsImported("StructuralMechanicsApplication")) {
+        auto pApplication = std::make_shared<Kratos::KratosStructuralMechanicsApplication>();
+        rKernel.ImportApplication(pApplication);
+        rRegisteredApplications.push_back(std::move(pApplication));
+      }
+
       if (!rKernel.IsImported("ConstitutiveLawsApplication")) {
         auto pApplication = std::make_shared<Kratos::KratosConstitutiveLawsApplication>();
         rKernel.ImportApplication(pApplication);
