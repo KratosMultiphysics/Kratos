82303e4f64cb2c5e0d31f47005706e639c4893da
SADPR <aresdepargas@gmail.com>
2024-05-06 10:20:24 +0200

    Deleting KRATOS_SKIP_TEST

--- applications/RomApplication/tests/cpp_tests/test_lspg_rom_builder_and_solver.cpp
+++ applications/RomApplication/tests/cpp_tests/test_lspg_rom_builder_and_solver.cpp
@@ -204,8 +204,6 @@ KRATOS_TEST_CASE_IN_SUITE(LeastSquaresPetrovGalerkinROMBuilderAndSolver, RomAppl
     const auto dx = BuildAndSolve(model_part, p_scheme, romBnS);
     const auto& dq = model_part.GetValue(ROM_SOLUTION_INCREMENT);
 
-    // KRATOS_SKIP_TEST << "this test is failing randomly in Windows. We will skip it until we find the error" << std::endl;
-
     KRATOS_EXPECT_NEAR(model_part.ElementsBegin()->GetValue(HROM_WEIGHT), 1, 1e-8);
     KRATOS_EXPECT_EQ(romBnS.GetEquationSystemSize(), 3);
 
