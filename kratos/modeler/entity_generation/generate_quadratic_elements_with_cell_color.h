6f3ec9896c89c1605b455a7cb4a08e7011b94b39
Vicente Mataix Ferrandiz <vmataix@altair.com>
2024-05-09 17:33:30 +0200

    Refactor source/header for entity generation

--- kratos/modeler/entity_generation/generate_quadratic_elements_with_cell_color.h
+++ kratos/modeler/entity_generation/generate_quadratic_elements_with_cell_color.h
@@ -29,55 +29,9 @@ public:
 
     ~GenerateQuadraticElementsWithCellColor() override = default;
 
-    Parameters GetDefaultParameters() const override
-    {
-        return Parameters(R"({
-            "type" : "quadratic_elements_with_cell_color",
-            "model_part_name": "Undefined",
-            "color": -1,
-            "properties_id": 1,
-            "generated_entity": "Element3D27N"
-        })");
-    }
+    Parameters GetDefaultParameters() const override;
 
-    void Generate(ModelPart& rModelPart, Parameters parameters) override
-    {
-        InitializeQuadraticData();
-
-        const CartesianMeshColors& r_colors = GetMeshColors();
-        std::size_t element_id = GetStartElementId();
-
-        const int inside_color = parameters["color"].GetInt();
-        std::size_t properties_id = parameters["properties_id"].GetInt();
-        Properties::Pointer p_properties = CreateAndGetProperty(rModelPart, properties_id);
-
-        array_1d<std::size_t, 3> number_of_cells = GetNumberOfCells();
-
-        ModelPart::NodesContainerType new_nodes;
-        ModelPart::ElementsContainerType new_elements;
-
-        auto& r_prototype_element = KratosComponents<Element>::Get(
-            parameters["generated_entity"].GetString());
-
-        Element::NodesArrayType cell_nodes(27);
-
-        for (std::size_t k = 0; k < number_of_cells[2]; k++) {
-            for (std::size_t j = 0; j < number_of_cells[1]; j++) {
-                for (std::size_t i = 0; i < number_of_cells[0]; i++) {
-                    if(std::lround(r_colors.GetElementalColor(i,j,k)) == inside_color){
-                        this->GetQuadraticCellNodes(cell_nodes, rModelPart, new_nodes, i, j, k);
-
-                        //create the new element
-                        Element::Pointer p_element = r_prototype_element.Create(element_id++, cell_nodes, p_properties);
-                        new_elements.push_back(p_element);
-                    }
-                }
-            }
-        }
-
-        AddNodesToModelPart(rModelPart, new_nodes);
-        rModelPart.AddElements(new_elements.begin(), new_elements.end());
-    }
+    void Generate(ModelPart& rModelPart, Parameters parameters) override;
 };
 
 }
--- kratos/modeler/entity_generation/generate_tetrahedral_elements_with_cell_color.h
+++ kratos/modeler/entity_generation/generate_tetrahedral_elements_with_cell_color.h
@@ -17,8 +17,6 @@
 // External includes
 
 // Project includes
-#include "geometries/tetrahedra_3d_4.h"
-
 #include "voxel_mesher_entity_generation.h"
 
 namespace Kratos {
@@ -31,51 +29,9 @@ public:
 
     ~GenerateTetrahedralElementsWithCellColor() override = default;
 
-    Parameters GetDefaultParameters() const override
-    {
-        return Parameters(R"({
-            "type" : "tetrahedral_elements_with_cell_color",
-            "model_part_name": "Undefined",
-            "color": -1,
-            "properties_id": 1,
-            "generated_entity": "Element3D4N"
-        })");
-    }
+    Parameters GetDefaultParameters() const override;
 
-    void Generate(ModelPart& rModelPart, Parameters parameters) override
-    {
-        const CartesianMeshColors& r_colors = GetMeshColors();
-        std::size_t element_id = GetStartElementId();
-
-        const int inside_color = parameters["color"].GetInt();
-        const std::size_t properties_id = parameters["properties_id"].GetInt();
-        Properties::Pointer p_properties = CreateAndGetProperty(rModelPart, properties_id);
-
-        auto& r_prototype_element = KratosComponents<Element>::Get(
-            parameters["generated_entity"].GetString());
-
-        array_1d<std::size_t, 3> number_of_cells = GetNumberOfCells();
-
-        ModelPart::NodesContainerType new_nodes;
-        ModelPart::ElementsContainerType new_elements;
-
-        KRATOS_INFO("Modeler") << "        Generating elements ... " << std::endl;
-        Element::NodesArrayType cell_nodes(8);
-        for (std::size_t k = 0; k < number_of_cells[2]; k++) {
-            for (std::size_t j = 0; j < number_of_cells[1]; j++) {
-                for (std::size_t i = 0; i < number_of_cells[0]; i++) {
-                    if(std::lround(r_colors.GetElementalColor(i,j,k)) == inside_color){
-                        GetLinearCellNodes(cell_nodes, rModelPart, new_nodes, i, j, k);
-                        CreateTetrahedraInCell(new_elements, cell_nodes, element_id, p_properties, r_prototype_element);
-                        element_id += 6;
-                    }
-                }
-            }
-        }
-
-        AddNodesToModelPart(rModelPart, new_nodes);
-        rModelPart.AddElements(new_elements.begin(), new_elements.end());
-    }
+    void Generate(ModelPart& rModelPart, Parameters parameters) override;
 
 private:
 
@@ -84,30 +40,8 @@ private:
         Element::NodesArrayType& rCellNodes,
         const std::size_t StartId,
         Properties::Pointer& pProperties,
-        const Element& rPrototype)
-    {
-        constexpr std::size_t nodes_per_tet = 4;
-        constexpr std::size_t tets_per_cell = 6;
-        using ConnectivityList = std::array<std::array<std::size_t, nodes_per_tet>, tets_per_cell>;
-        static constexpr ConnectivityList local_connectivities {{ { 0,3,6,2 },
-                                                                    { 3,6,7,0 },
-                                                                    { 4,7,6,0 },
-                                                                    { 0,4,5,6 },
-                                                                    { 0,1,2,6 },
-                                                                    { 1,5,6,0 } }};
-
-        for(std::size_t i = 0; i < tets_per_cell; i++) {
-            const auto & connectivty = local_connectivities[i];
-
-            auto geom = Kratos::make_shared<Tetrahedra3D4<Node>>(
-                rCellNodes(connectivty[0]),
-                rCellNodes(connectivty[1]),
-                rCellNodes(connectivty[2]),
-                rCellNodes(connectivty[3]));
-
-            rElements.push_back(rPrototype.Create(StartId+i, geom, pProperties));
-        }
-    }
+        const Element& rPrototype
+        );
 };
 
 }
