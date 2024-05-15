dd7040b0a8367354836d2f31fe1aecb27c783bbb
Gennady Markelov <gennady.markelov@deltates.nl>
2024-05-15 14:10:07 +0200

    used ublas assigment

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_fluid_pressure.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_fluid_pressure.cpp
@@ -10,6 +10,7 @@
 //  Main authors:    Gennady Markelov
 //
 
+#include "boost/numeric/ublas/assignment.hpp"
 #include "custom_retention/retention_law.h"
 #include "includes/checks.h"
 #include "testing/testing.h"
@@ -22,18 +23,10 @@ namespace Kratos::Testing
 KRATOS_TEST_CASE_IN_SUITE(CalculateFluidPressureGivesCorrectResults, KratosGeoMechanicsFastSuite)
 {
     Vector N(5);
-    N(0) = 1.0;
-    N(1) = 2.0;
-    N(2) = 3.0;
-    N(3) = 4.0;
-    N(4) = 5.0;
+    N <<= 1.0, 2.0, 3.0, 4.0, 5.0;
 
     Vector pressure_vector(5);
-    pressure_vector(0) = 0.5;
-    pressure_vector(1) = 0.7;
-    pressure_vector(2) = 0.8;
-    pressure_vector(3) = 0.9;
-    pressure_vector(4) = 0.4;
+    pressure_vector <<= 0.5, 0.7, 0.8, 0.9, 0.4;
 
     auto   fluid_pressure = RetentionLaw::Parameters::CalculateFluidPressure(N, pressure_vector);
     double expected_value = 9.9;
