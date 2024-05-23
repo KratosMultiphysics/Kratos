766fb6170b1fb0faec6544c7404d3343e47d6621
ge69vud <manuel.messmer@tum.de>
2024-05-23 13:42:42 +0200

    Update queso solvers.

--- external_libraries/queso/solvers/nnls.cpp
+++ external_libraries/queso/solvers/nnls.cpp
@@ -1,40 +1,29 @@
 // Author: Manuel Meßmer
 // Email: manuel.messmer@tum.de
 
-//// External includes
-#include "nnls/nnls_impl.h"
 //// STL includes
 #include <iostream>
 #include <algorithm>
+//// External includes
+#include "nnls/nnls_impl.h"
 //// Project includes
 #include "nnls.h"
 
 namespace queso {
 
-double nnls::nnls(MatrixType& A, const VectorType& b, VectorType& x){
+double NNLS::solve(MatrixType& A, VectorType& B, VectorType& X) {
 
     // Get Dimension
-    int m = A.size();
-    int n = (m > 0) ? A[0].size() : 0;
+    int m = B.size();
+    int n = (m > 0) ? A.size()/m : 0;
+    X.resize(n);
 
     // Pointer to doubles
-    double *A_doubles = 0;
-    double *B_doubles = 0;
-
-    // Convert Ublas matrix and vector to a pointer to double.
-    A_doubles = new double[m * n];
-    B_doubles = new double[m];
-    std::vector<double> tmp_vector(m*n);
-    for( int i = 0; i < n; ++i){
-        for( int j = 0; j < m; ++j){
-            A_doubles[i*m+j] = A[j][i];
-        }
-    }
-
-    std::copy(b.begin(), b.end(), B_doubles);
+    double *A_doubles = A.data();
+    double *B_doubles = B.data();
+    double *X_double = X.data();
 
     // Construct buffers
-    double *X = new double[n];
     double *W = new double[n];
     double *ZZ = new double[m];
     int *index = new int[n];
@@ -44,7 +33,8 @@ double nnls::nnls(MatrixType& A, const VectorType& b, VectorType& x){
     int mode = 0;
     int mda = m;
 
-    nnls_(A_doubles, &mda, &m, &n, B_doubles, X, &Rnorm, W, ZZ, index, &mode);
+    // Solve
+    nnls_(A_doubles, &mda, &m, &n, B_doubles, X_double, &Rnorm, W, ZZ, index, &mode);
 
     if( mode == 2 ){
         std::cerr << "THE DIMENSIONS OF THE PROBLEM ARE BAD. EITHER M .LE. 0 OR N .LE. 0." << std::endl;
@@ -54,15 +44,7 @@ double nnls::nnls(MatrixType& A, const VectorType& b, VectorType& x){
         std::cerr << "ITERATION COUNT EXCEEDED.  MORE THAN 3*N ITERATIONS." << std::endl;
     }
 
-    if( x.size() != static_cast<std::size_t>(n) ){
-        x.resize(n);
-    }
-    // Copy results into x
-    std::copy(X, X+n, x.begin());
-
-    delete[] A_doubles;
-    delete[] B_doubles;
-    delete[] X;
+    // Release buffers
     delete[] W;
     delete[] ZZ;
     delete[] index;
--- external_libraries/queso/solvers/nnls.h
+++ external_libraries/queso/solvers/nnls.h
@@ -8,15 +8,42 @@
 #include <vector>
 
 namespace queso {
-namespace nnls {
 
-typedef std::vector<std::vector<double>> MatrixType;
-typedef std::vector<double> VectorType;
+///@name QuESo Classes
+///@{
 
-// Wrapper for nnls solver
-double nnls(MatrixType& A, const VectorType& b, VectorType& x);
 
-} // End Namespace nnls
+/**
+ * @class  NNLS solver
+ * @brief  Wrapper to Non-Negative-Least-Squares-Solver (see: external_libaries/nnls/nnls_impl.h).
+ * @author Manuel Messmer
+**/
+class NNLS {
+public:
+
+    ///@}
+    ///@name Type Definitions
+    ///@{
+
+    typedef std::vector<double> MatrixType;
+    typedef std::vector<double> VectorType;
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /// @brief  Solves ||Ax-b||_L2 as a non-negative least-squares problem. All entries in x are positive.
+    ///         For better performance A and B are not copied. However, this means they are non-const!!
+    /// @param A Matrix as vector form (Serialized matrix, column first)
+    /// @param b RHS
+    /// @param X Solution Vector
+    /// @return Residuum (double)
+    static double solve(MatrixType& A, VectorType& B, VectorType& X);
+
+    ///@}
+
+}; // End Class NNLS
+///@}
 } // End namespace queso
 
 #endif // NNLS_INCLUDE_H
