06823a97ac9e0af49234f41f9aac3dd455c34b36
Richard Faasse <richard.faasse@deltares.nl>
2024-05-30 16:00:38 +0200

    Added test which fails when one of the RHS contributions is removed

---
+++ applications/GeoMechanicsApplication/tests/test_compressibility.py
@@ -0,0 +1,37 @@
+import os
+
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+import test_helper
+
+
+class KratosGeoMechanicsNormalLoadHexaTests(KratosUnittest.TestCase):
+    """
+    This class contains tests for applying normal loads on 3D elements.
+    """
+
+    def test_hexa_8n_normal_load(self):
+        test_name = 'compressibility_tests'
+        file_path = test_helper.get_file_path(test_name)
+        test_helper.run_kratos(file_path)
+
+        output_file_path = os.path.join(file_path, test_name + '.post.res')
+        output_reader = test_helper.GiDOutputFileReader()
+        output_data = output_reader.read_output_from(output_file_path)
+
+        top_node_nbrs = [5, 6, 7, 8]
+        water_pressures_at_top = test_helper.GiDOutputFileReader.nodal_values_at_time("WATER_PRESSURE", 1.0, output_data,
+                                                                                    node_ids=top_node_nbrs)
+        bottom_node_nbrs = [1, 2, 3, 4]
+        water_pressures_at_bottom = test_helper.GiDOutputFileReader.nodal_values_at_time("WATER_PRESSURE", 1.0, output_data,
+                                                                                       node_ids=bottom_node_nbrs)
+
+        expected_water_pressures_at_bottom = [-27.4139, -27.4139, 31.4158, 31.4158]
+
+        for water_pressure_at_top in water_pressures_at_top:
+            self.assertAlmostEqual(water_pressure_at_top, 1.0, 6)
+        for water_pressure_at_bottom, expected_water_pressure_at_bottom in zip(water_pressures_at_bottom, expected_water_pressures_at_bottom):
+            self.assertAlmostEqual(water_pressure_at_bottom, expected_water_pressure_at_bottom, 6)
+
+
+if __name__ == '__main__':
+    KratosUnittest.main()
