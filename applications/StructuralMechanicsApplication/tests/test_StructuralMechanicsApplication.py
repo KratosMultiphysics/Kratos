5411ae97831f2270046c327f1f82b6d615748662
Alejandro <alejandro.cornejo23@gmail.com>
2024-05-31 09:15:57 +0400

    add straight 2d 3N Timoshenko element

--- applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
+++ applications/StructuralMechanicsApplication/tests/test_StructuralMechanicsApplication.py
@@ -98,6 +98,7 @@ from structural_mechanics_test_factory import SimpleMeshMovingTest as TSimpleMes
 ##### NIGHTLY TESTS #####
 # Patch test Small Displacements
 from structural_mechanics_test_factory import TimoshenkoBeam2D2NTest as TTimoshenkoBeam2D2NTest
+from structural_mechanics_test_factory import TimoshenkoBeam2D3NTest as TTimoshenkoBeam2D3NTest
 from structural_mechanics_test_factory import AutomatedInitialVariableProcessTest as TAutomatedInitialVariableProcessTest
 from structural_mechanics_test_factory import SDTwoDShearQuaPatchTest as TSDTwoDShearQuaPatchTest
 from structural_mechanics_test_factory import SDTwoDShearTriPatchTest as TSDTwoDShearTriPatchTest
@@ -344,6 +345,7 @@ def AssembleTestSuites():
     ### Adding Nightly Tests
     # Patch test Small Displacements
     smallSuite.addTest(TTimoshenkoBeam2D2NTest('test_execution'))
+    smallSuite.addTest(TTimoshenkoBeam2D3NTest('test_execution'))
     smallSuite.addTest(TAutomatedInitialVariableProcessTest('test_execution'))
     nightSuite.addTest(TSDTwoDShearQuaPatchTest('test_execution'))
     nightSuite.addTest(TSDTwoDShearTriPatchTest('test_execution'))
