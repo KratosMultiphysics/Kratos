b4749e388765a8645978b64b17dd5a4279429d77
Richard Faasse <richard.faasse@deltares.nl>
2024-06-11 15:21:04 +0200

    More refactoring steps

--- applications/GeoMechanicsApplication/custom_utilities/nodal_extrapolator.cpp
+++ applications/GeoMechanicsApplication/custom_utilities/nodal_extrapolator.cpp
@@ -22,54 +22,68 @@ Matrix NodalExtrapolator::CalculateElementExtrapolationMatrix(GeometryType& rGeo
 {
     CheckIfGeometryIsSupported(rGeometry);
 
-    // Sofar this works for 3, 4, 6 and 8 node planar elements
-    // for 2 and 3 node line elements the extension is straightforward.
-    // for volume elements ( hexa, tetra, wedge ) the midside node interpolation step is more elaborate
-    auto p_low_order_geometry = CreateLowerOrderGeometry(rGeometry);
-    GeometryType* low_order_geometry = p_low_order_geometry ? p_low_order_geometry.get() : &rGeometry;
+    auto          p_lower_order_geometry = CreateLowerOrderGeometry(rGeometry);
+    GeometryType& p_corner_geometry = p_lower_order_geometry ? *p_lower_order_geometry : rGeometry;
 
-    // calculate extrapolation matrix towards corner nodes
-    SizeType number_of_low_order_nodes = low_order_geometry->PointsNumber();
+    Matrix extrapolation_matrix =
+        CalculateExtrapolationMatrixForCornerNodes(rGeometry, IntegrationMethod, p_corner_geometry);
 
-    Vector     N              = ZeroVector(number_of_low_order_nodes);
-    Matrix     quasi_mass_mat = ZeroMatrix(number_of_low_order_nodes, number_of_low_order_nodes);
-    const auto integration_points           = rGeometry.IntegrationPoints(IntegrationMethod);
-    const auto number_of_integration_points = integration_points.size();
-    Matrix     node_coefficient(number_of_low_order_nodes, number_of_integration_points);
-    Vector     determinants_of_jacobian;
+    if (p_lower_order_geometry) {
+        AddRowsForMidsideNodes(rGeometry, extrapolation_matrix);
+    }
+
+    return extrapolation_matrix;
+}
+
+Matrix NodalExtrapolator::CalculateExtrapolationMatrixForCornerNodes(
+    const NodalExtrapolator::GeometryType& rGeometry,
+    GeometryData::IntegrationMethod&       IntegrationMethod,
+    const NodalExtrapolator::GeometryType& rCornerGeometry) const
+{
+    SizeType number_of_corner_nodes = rCornerGeometry.PointsNumber();
+
+    Matrix      quasi_mass_mat     = ZeroMatrix(number_of_corner_nodes, number_of_corner_nodes);
+    const auto& integration_points = rGeometry.IntegrationPoints(IntegrationMethod);
+    const auto  number_of_integration_points = integration_points.size();
+    Matrix      node_coefficient(number_of_corner_nodes, number_of_integration_points);
+    Vector      determinants_of_jacobian;
     determinants_of_jacobian = rGeometry.DeterminantOfJacobian(determinants_of_jacobian, IntegrationMethod);
+
+    const Matrix& shape_functions_values_at_gauss_points =
+        rCornerGeometry.ShapeFunctionsValues(IntegrationMethod);
+
     for (IndexType i_gauss_point = 0; i_gauss_point < number_of_integration_points; ++i_gauss_point) {
-        // local_coordinates --> isoparametric coordinates or for triangles area coordinates
-        const array_1d<double, 3>& r_local_coordinates = integration_points[i_gauss_point].Coordinates();
-        // shape function for this i.p.
-        low_order_geometry->ShapeFunctionsValues(N, r_local_coordinates);
+        const Vector N = row(shape_functions_values_at_gauss_points, i_gauss_point);
         quasi_mass_mat += outer_prod(N, N) * determinants_of_jacobian[i_gauss_point] *
                           integration_points[i_gauss_point].Weight();
         column(node_coefficient, i_gauss_point) =
             N * determinants_of_jacobian[i_gauss_point] * integration_points[i_gauss_point].Weight();
     }
 
-    double MetricDet;
+    double metric_determinant;
     Matrix quasi_mass_mat_inverse;
-    MathUtils<double>::InvertMatrix(quasi_mass_mat, quasi_mass_mat_inverse, MetricDet, -1.);
+    MathUtils<double>::InvertMatrix(quasi_mass_mat, quasi_mass_mat_inverse, metric_determinant, -1.);
 
     auto extrapolation_matrix = Matrix{prod(quasi_mass_mat_inverse, node_coefficient)};
-
-    // add rows for midside nodes if needed ( mean value of corner nodes )
-    if (rGeometry.PointsNumber() > low_order_geometry->PointsNumber()) {
-        extrapolation_matrix.resize(rGeometry.PointsNumber(), extrapolation_matrix.size2());
-        std::size_t n_filled = low_order_geometry->PointsNumber();
-        for (std::size_t i_row = 0; i_row < rGeometry.PointsNumber() - n_filled; ++i_row) {
-            row(extrapolation_matrix, n_filled + i_row) =
-                0.5 * (row(extrapolation_matrix, i_row) + row(extrapolation_matrix, (i_row + 1) % n_filled));
-        }
-    }
-
     return extrapolation_matrix;
+}
+
+void NodalExtrapolator::AddRowsForMidsideNodes(const NodalExtrapolator::GeometryType& rGeometry,
+                                               Matrix& extrapolation_matrix) const
+{
+    const size_t n_filled = extrapolation_matrix.size2();
+    extrapolation_matrix.resize(rGeometry.PointsNumber(), extrapolation_matrix.size2());
+    for (size_t i_row = 0; i_row < rGeometry.PointsNumber() - n_filled; ++i_row) {
+        row(extrapolation_matrix, n_filled + i_row) =
+            0.5 * (row(extrapolation_matrix, i_row) + row(extrapolation_matrix, (i_row + 1) % n_filled));
+    }
 }
 
 std::unique_ptr<NodalExtrapolator::GeometryType> NodalExtrapolator::CreateLowerOrderGeometry(GeometryType& rGeometry) const
 {
+    // Sofar this works for 3, 4, 6 and 8 node planar elements
+    // for 2 and 3 node line elements the extension is straightforward.
+    // for volume elements ( hexa, tetra, wedge ) the midside node interpolation step is more elaborate
     switch (rGeometry.size()) {
     case 6:
         return std::make_unique<Triangle2D3<Node>>(rGeometry(0), rGeometry(1), rGeometry(2));
