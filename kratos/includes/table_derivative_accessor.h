b3767efd4bb5ffa13f6c7660edd2ed07358b3b47
matekelemen <kelemen.mate.12@gmail.com>
2024-06-18 17:04:13 +0200

    add table derivative accessor

---
+++ kratos/includes/table_derivative_accessor.h
@@ -0,0 +1,171 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+
+# pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/accessor.h"
+
+namespace Kratos
+{
+///@addtogroup KratosCore
+///@{
+
+///@name Kratos Classes
+///@{
+
+/**
+ * @class TableDerivativeAccessor
+ * @ingroup Kratos Core
+ * @brief This class looks up a variable from the derivative of a table.
+ * @brief The tables are supposed to relate double <-> double type entities.
+ * @brief The input variable is suposed to be a nodally accesible one (either historical or not)
+ */
+class KRATOS_API(KRATOS_CORE) TableDerivativeAccessor : public Accessor
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Geometry type definition
+    using GeometryType = Geometry<Node>;
+
+    /// Variable type
+    using VariableType = Variable<double>;
+
+    /// BaseType
+    using BaseType = Accessor;
+
+    using DataLocation = Globals::DataLocation;
+
+    using SizeType = std::size_t;
+
+    /// Pointer definition of TableDerivativeAccessor
+    KRATOS_CLASS_POINTER_DEFINITION(TableDerivativeAccessor);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+    TableDerivativeAccessor()
+    {}
+
+    /// Custom constructor
+    TableDerivativeAccessor(VariableType& rInputVariable, const std::string& rInputVariableType = "node_historical")
+        : mpInputVariable(&rInputVariable)
+    {
+        // We initialize the variable type only once
+        if (rInputVariableType == "node_historical") {
+            mInputVariableType = Globals::DataLocation::NodeHistorical;
+        } else if (rInputVariableType == "node_non_historical") {
+            mInputVariableType = Globals::DataLocation::NodeNonHistorical;
+        } else if (rInputVariableType == "element") {
+            mInputVariableType = Globals::DataLocation::Element;
+        } else {
+            KRATOS_ERROR << "The table_input_variable_type is incorrect or not supported. Types available are : 'node_historical', 'node_non_historical' and 'element'" << std::endl;
+        }
+    }
+
+    /// Copy constructor
+    TableDerivativeAccessor(const TableDerivativeAccessor& rOther)
+    : BaseType(rOther),
+        mpInputVariable(rOther.mpInputVariable),
+        mInputVariableType(rOther.mInputVariableType)
+    {}
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief Custom method to retrieve double type properties
+     * @param rVariable The variable considered (double type properties)
+     * @param rProperties The properties considered
+     * @param rGeometry The geometry considered
+     * @param rShapeFunctionVector The shape function of the GP considered
+     * @param rProcessInfo The process info considered
+     * @return The double type properties
+     */
+    double GetValue(
+        const Variable<double>& rVariable,
+        const Properties& rProperties,
+        const GeometryType& rGeometry,
+        const Vector& rShapeFunctionVector,
+        const ProcessInfo& rProcessInfo
+        ) const override;
+
+    /**
+     * @brief This computes a material property according to a certain
+     * nodal Variable<double> table
+     */
+    double GetValueFromTable(
+        const Variable<double>& rIndependentVariable,
+        const Variable<double>& rDependentVariable,
+        const Properties& rProperties,
+        const GeometryType& rGeometry,
+        const Vector& rShapeFunctionVector,
+        const ProcessInfo& rProcessInfo) const;
+
+
+    /**
+     * @brief Returns the member input variable
+     */
+    VariableType& GetInputVariable() const
+    {
+        return *mpInputVariable;
+    }
+
+    // Getting a pointer to the class
+    Accessor::UniquePointer Clone() const override;
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        std::stringstream buffer;
+        buffer << "TableDerivativeAccessor" ;
+
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override {rOStream << "TableDerivativeAccessor";}
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override {rOStream << "TableDerivativeAccessor class";}
+
+    ///@}
+
+private:
+
+    ///@name Member Variables
+    ///@{
+
+    VariableType* mpInputVariable;
+    Globals::DataLocation mInputVariableType = Globals::DataLocation::NodeHistorical; // NodalHistorical by default
+
+    friend class Serializer;
+
+    void save(Serializer &rSerializer) const override;
+
+    void load(Serializer &rSerializer) override;
+
+}; // class
+///@}
+
+///@} addtogroup block
+
+} // namespace Kratos
