2c1e867f1bfa70af952ae4dd0799a613a5d5433e
Richard Faasse <richard.faasse@deltares.nl>
2024-06-19 17:06:26 +0200

    Removed CAUCHY_STRESS_VECTOR, since GiD output writing does not work nicely with writing Vectors it seems. Added TOTAL_STRESS_VECTOR as a nodal solution step value

--- applications/GeoMechanicsApplication/python_scripts/geomechanics_solver.py
+++ applications/GeoMechanicsApplication/python_scripts/geomechanics_solver.py
@@ -358,8 +358,8 @@ class GeoMechanicalSolver(PythonSolver):
         
         # Add integration \ gauss point values that will likely need extrapolating to node
         self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.HYDRAULIC_HEAD)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.CAUCHY_STRESS_VECTOR)
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.CAUCHY_STRESS_TENSOR)
+        self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.TOTAL_STRESS_TENSOR)
         self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.FLUID_FLUX_VECTOR)
 
     def _add_temperature_variables(self):
