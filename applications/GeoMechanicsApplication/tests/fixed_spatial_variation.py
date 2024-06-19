5f1d1127d1a30805d88e1ecae833d3d6ce7538bf
Richard Faasse <richard.faasse@deltares.nl>
2024-06-19 09:08:15 +0200

    Processing review comments

--- applications/GeoMechanicsApplication/tests/fixed_spatial_variation.py
+++ applications/GeoMechanicsApplication/tests/fixed_spatial_variation.py
@@ -22,16 +22,16 @@ class KratosGeoMechanicsFixedSpatialVariationTests(KratosUnittest.TestCase):
         reader = test_helper.GiDOutputFileReader()
         actual_data = reader.read_output_from(result_file_name)
 
-        top_nodes = [4, 5, 13]
+        top_nodes = [3, 4, 7]
         actual_top_nodal_pressures = reader.nodal_values_at_time("WATER_PRESSURE", 1.0, actual_data, top_nodes)
         expected_top_pressures = [-5000, 0.0, -2500]
-        for (actual_pressure, expected_pressure) in zip(actual_top_nodal_pressures, expected_top_pressures):
+        for actual_pressure, expected_pressure in zip(actual_top_nodal_pressures, expected_top_pressures):
             self.assertAlmostEqual(actual_pressure, expected_pressure, 3)
 
-        bottom_nodes = [1, 2, 6]
+        bottom_nodes = [1, 2, 5]
         actual_bottom_nodal_pressures = reader.nodal_values_at_time("WATER_PRESSURE", 1.0, actual_data, bottom_nodes)
-        expected_bottom_pressures = [-30000, -40000, -35000]
-        for (actual_pressure, expected_pressure) in zip(actual_bottom_nodal_pressures, expected_bottom_pressures):
+        expected_bottom_pressures = [-40000, -30000, -35000]
+        for actual_pressure, expected_pressure in zip(actual_bottom_nodal_pressures, expected_bottom_pressures):
             self.assertAlmostEqual(actual_pressure, expected_pressure, 3)
 
 
