a0a560c165272a9c8490e79103eda4f929fc6460
Richard Faasse <richard.faasse@deltares.nl>
2024-06-20 14:08:32 +0200

    Fixed clang-tidy issue: static_cast -> dynamic_cast

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_geo_integration_values_extrapolation_to_nodes_process.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_geo_integration_values_extrapolation_to_nodes_process.cpp
@@ -134,8 +134,8 @@ KRATOS_TEST_CASE_IN_SUITE(TestExtrapolationProcess_ExtrapolatesCorrectlyForConst
     Model model;
     auto& model_part = CreateModelPartWithTwoStubElements(model);
 
-    static_cast<StubElement&>(model_part.Elements()[1]).mIntegrationDoubleValues = {1.0, 1.0, 1.0, 1.0};
-    static_cast<StubElement&>(model_part.Elements()[2]).mIntegrationDoubleValues = {1.0, 1.0, 1.0, 1.0};
+    dynamic_cast<StubElement&>(model_part.Elements()[1]).mIntegrationDoubleValues = {1.0, 1.0, 1.0, 1.0};
+    dynamic_cast<StubElement&>(model_part.Elements()[2]).mIntegrationDoubleValues = {1.0, 1.0, 1.0, 1.0};
 
     auto parameters = Parameters(R"(
     {
@@ -164,8 +164,8 @@ KRATOS_TEST_CASE_IN_SUITE(TestExtrapolationProcess_ExtrapolatesCorrectlyForTwoCo
     Model model;
     auto& model_part = CreateModelPartWithTwoStubElements(model);
 
-    static_cast<StubElement&>(model_part.Elements()[1]).mIntegrationDoubleValues = {1.0, 1.0, 1.0, 1.0};
-    static_cast<StubElement&>(model_part.Elements()[2]).mIntegrationDoubleValues = {2.0, 2.0, 2.0, 2.0};
+    dynamic_cast<StubElement&>(model_part.Elements()[1]).mIntegrationDoubleValues = {1.0, 1.0, 1.0, 1.0};
+    dynamic_cast<StubElement&>(model_part.Elements()[2]).mIntegrationDoubleValues = {2.0, 2.0, 2.0, 2.0};
 
     auto parameters = Parameters(R"(
     {
@@ -197,11 +197,11 @@ KRATOS_TEST_CASE_IN_SUITE(TestExtrapolationProcess_ExtrapolatesCorrectlyForLinea
     auto& model_part = CreateModelPartWithTwoStubElements(model);
 
     // Linear field in x between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[1]).mIntegrationDoubleValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[1]).mIntegrationDoubleValues = {
         -0.57735, 0.57735, 0.57735, -0.57735};
 
     // Linear field in y between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[2]).mIntegrationDoubleValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[2]).mIntegrationDoubleValues = {
         -0.57735, -0.57735, 0.57735, 0.57735};
 
     auto                                            parameters = Parameters(R"(
@@ -233,12 +233,12 @@ KRATOS_TEST_CASE_IN_SUITE(TestExtrapolationProcess_ExtrapolatesMatrixCorrectlyFo
     auto& model_part = CreateModelPartWithTwoStubElements(model);
 
     // Linear field in x between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[1]).mIntegrationMatrixValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[1]).mIntegrationMatrixValues = {
         ScalarMatrix(3, 3, -0.57735), ScalarMatrix(3, 3, 0.57735), ScalarMatrix(3, 3, 0.57735),
         ScalarMatrix(3, 3, -0.57735)};
 
     // Linear field in y between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[2]).mIntegrationMatrixValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[2]).mIntegrationMatrixValues = {
         ScalarMatrix(3, 3, -0.57735), ScalarMatrix(3, 3, -0.57735), ScalarMatrix(3, 3, 0.57735),
         ScalarMatrix(3, 3, 0.57735)};
 
@@ -277,12 +277,12 @@ KRATOS_TEST_CASE_IN_SUITE(TestExtrapolationProcess_ExtrapolatesVectorCorrectlyFo
     auto& model_part = CreateModelPartWithTwoStubElements(model);
 
     // Linear field in x between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[1]).mIntegrationVectorValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[1]).mIntegrationVectorValues = {
         ScalarVector(6, -0.57735), ScalarVector(6, 0.57735), ScalarVector(6, 0.57735),
         ScalarVector(6, -0.57735)};
 
     // Linear field in y between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[2]).mIntegrationVectorValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[2]).mIntegrationVectorValues = {
         ScalarVector(6, -0.57735), ScalarVector(6, -0.57735), ScalarVector(6, 0.57735), ScalarVector(6, 0.57735)};
 
     auto parameters = Parameters(R"(
@@ -320,12 +320,12 @@ KRATOS_TEST_CASE_IN_SUITE(TestExtrapolationProcess_ExtrapolatesArrayCorrectlyFor
     auto& model_part = CreateModelPartWithTwoStubElements(model);
 
     // Linear field in x between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[1]).mIntegrationArrayValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[1]).mIntegrationArrayValues = {
         ScalarVector(3, -0.57735), ScalarVector(3, 0.57735), ScalarVector(3, 0.57735),
         ScalarVector(3, -0.57735)};
 
     // Linear field in y between -1 and 1
-    static_cast<StubElement&>(model_part.Elements()[2]).mIntegrationArrayValues = {
+    dynamic_cast<StubElement&>(model_part.Elements()[2]).mIntegrationArrayValues = {
         ScalarVector(3, -0.57735), ScalarVector(3, -0.57735), ScalarVector(3, 0.57735), ScalarVector(3, 0.57735)};
 
     auto parameters = Parameters(R"(
