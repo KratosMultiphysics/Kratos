376d0702591164504d2f2f5827e8368417fce0c5
Anne van de Graaf <avdg81@users.noreply.github.com>
2024-06-23 13:18:14 +0200

    Custom tangent modulus for linear truss 3D2N (#12464)
    For the setup of the tangent modulus, the `TrussElementLinear3D2N` uses `ReturnTangentModulus1D` from its base class `TrussElement3D2N`. `TrussElementLinear3D2N` intends to use a linear strain measure, `TrussElement3D2N` uses a Green-Lagrange strain measure. The `TrussElement3D2N` base class function `ReturnTangentModulus1D` uses Green-Lagrange strain, hence gives wrong results when used from `TrussElementLinear3D2N` in  combination with a nonlinear constitutive law. This PR corrects that.
    
    Changes include:
    - Extracted the common part of `ReturnTangentModulus1D`, which receives an additional argument representing the truss's strain value.
    - Made the original member function `ReturnTangentModulus1D` `virtual`, to allow for overriding it by class `TrussElementLinear3D2N`.
    - The base class (`TrussElement3D2N`) passes the Green-Lagrange strain to the newly extracted member function, whereas the derived class (`TrussElementLinear3D2N`) passes the linear strain.
    - Added two unit tests to demonstrate the effect of the above behavioral changes.

--- applications/StructuralMechanicsApplication/custom_elements/truss_element_3D2N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/truss_element_3D2N.cpp
@@ -1042,17 +1042,24 @@ void TrussElement3D2N::CalculateLumpedMassVector(
 
 double TrussElement3D2N::ReturnTangentModulus1D(const ProcessInfo& rCurrentProcessInfo)
 {
-    KRATOS_TRY;
+    KRATOS_TRY
+    return ReturnTangentModulus1D(CalculateGreenLagrangeStrain(), rCurrentProcessInfo);
+    KRATOS_CATCH("")
+}
+
+double TrussElement3D2N::ReturnTangentModulus1D(double Strain, const ProcessInfo& rCurrentProcessInfo) const
+{
+    KRATOS_TRY
     double tangent_modulus(0.00);
     Vector strain_vector = ZeroVector(mpConstitutiveLaw->GetStrainSize());
-    strain_vector[0] = CalculateGreenLagrangeStrain();
+    strain_vector[0] = Strain;
 
     ConstitutiveLaw::Parameters Values(GetGeometry(),GetProperties(),rCurrentProcessInfo);
     Values.SetStrainVector(strain_vector);
 
     mpConstitutiveLaw->CalculateValue(Values,TANGENT_MODULUS,tangent_modulus);
     return tangent_modulus;
-    KRATOS_CATCH("");
+    KRATOS_CATCH("")
 }
 
 
