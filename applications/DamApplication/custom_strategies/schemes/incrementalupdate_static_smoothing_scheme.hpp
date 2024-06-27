1efe8c60685933a273375c41241055c3908081e3
xaviertort <xavitort1@gmail.com>
2024-06-27 16:37:17 +0200

    Create Initialize in static smoothing scheme

--- applications/DamApplication/custom_strategies/schemes/incrementalupdate_static_smoothing_scheme.hpp
+++ applications/DamApplication/custom_strategies/schemes/incrementalupdate_static_smoothing_scheme.hpp
@@ -41,6 +41,33 @@ public:
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+    void Initialize(ModelPart& r_model_part) override
+    {
+        KRATOS_TRY
+
+        const int NNodes = static_cast<int>(r_model_part.Nodes().size());
+        ModelPart::NodesContainerType::iterator node_begin = r_model_part.NodesBegin();
+
+        // Initialize INITIAL_STRESS_TENSOR
+        #pragma omp parallel for
+        for(int i = 0; i < NNodes; i++)
+        {
+            ModelPart::NodesContainerType::iterator itNode = node_begin + i;
+
+            Matrix& rInitialStress = itNode->FastGetSolutionStepValue(INITIAL_STRESS_TENSOR);
+            if(rInitialStress.size1() != 3)
+                rInitialStress.resize(3,3,false);
+            noalias(rInitialStress) = ZeroMatrix(3,3);
+        }
+
+        BaseType::mSchemeIsInitialized = true;
+
+        KRATOS_CATCH("")
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     void FinalizeSolutionStep(
         ModelPart& rModelPart,
         TSystemMatrixType& A,
