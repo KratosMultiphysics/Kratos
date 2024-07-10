cf6ba6e17a32343510d35ad797cd654ff52fe775
Guillermo Casas <gcasas@cimne.upc.edu>
2024-07-10 14:09:13 +0200

    Adding python-based NR strategy

---
+++ applications/ConvectionDiffusionApplication/python_scripts/continuation_newton_raphson_strategy_for_shock_capturing.py
@@ -0,0 +1,214 @@
+# Importing the Kratos Library
+import KratosMultiphysics
+
+# Import applications
+import KratosMultiphysics.ConvectionDiffusionApplication as ConvectionDiffusionApplication
+
+# Import base class file
+from KratosMultiphysics.ConvectionDiffusionApplication import convection_diffusion_stationary_solver
+
+# Import python tools
+import numpy as np
+
+class ResidualBasedNewtonRaphsonStrategyPython():
+
+
+    def __init__(self, main_model_part, scheme,
+                 convergence_criterion,
+                 builder_and_solver,
+                 max_iterations,
+                 compute_reactions,
+                 reform_dofs_at_each_step,
+                 move_mesh_flag):
+        self.main_model_part = main_model_part
+        self.scheme = scheme
+        self.convergence_criteria = convergence_criterion
+        self.builder_and_solver = builder_and_solver
+        self.max_iterations = max_iterations
+        self.CalculateReactionsFlag = compute_reactions
+        self.ReformDofSetAtEachStep = reform_dofs_at_each_step
+        self.MoveMeshFlag = move_mesh_flag
+
+        self.builder_and_solver.SetCalculateReactionsFlag(self.CalculateReactionsFlag)
+        self.builder_and_solver.SetReshapeMatrixFlag(self.ReformDofSetAtEachStep)
+
+        self.rebuild_level = 2
+        self.echo_level = 2
+
+        self.space_utils = KratosMultiphysics.UblasSparseSpace()
+
+        self.pA = self.space_utils.CreateEmptyMatrixPointer()
+        self.pDx = self.space_utils.CreateEmptyVectorPointer()
+        self.pb = self.space_utils.CreateEmptyVectorPointer()
+
+        self.A = (self.pA)
+        self.Dx = (self.pDx)
+        self.b = (self.pb)
+
+        #initialize flags
+        self.SolutionStepIsInitialized = False
+        self.InitializeWasPerformed = False
+        self.StiffnessMatrixIsBuilt = False
+        self.UseOldStiffnessInFirstIteration = False
+
+
+        self.NumberOfContinuationIterations = 100
+        self.final_shock_capturing_constant = self.main_model_part.ProcessInfo[ConvectionDiffusionApplication.SHOCK_CAPTURING_INTENSITY]
+
+    def Initialize(self):
+        if (self.InitializeWasPerformed == False):
+            if(self.scheme.SchemeIsInitialized() == False):
+                self.scheme.Initialize(self.main_model_part)
+
+            if (self.scheme.ElementsAreInitialized() == False):
+                self.scheme.InitializeElements(self.main_model_part)
+
+            if (self.scheme.ConditionsAreInitialized() == False):
+                self.scheme.InitializeConditions(self.main_model_part)
+
+            #if (self.convergence_criteria.IsInitialized() == False):
+            self.convergence_criteria.Initialize(self.main_model_part)
+
+            self.InitializeWasPerformed == True
+
+    def Predict(self):
+        self.scheme.Predict(self.main_model_part,self.builder_and_solver.GetDofSet(),self.A,self.Dx,self.b)
+
+    def InitializeSolutionStep(self):
+        if (self.builder_and_solver.GetDofSetIsInitializedFlag() == False or self.ReformDofSetAtEachStep == True):
+            #initialize the list of degrees of freedom to be used
+            self.builder_and_solver.SetUpDofSet(self.scheme,self.main_model_part)
+            #reorder the list of degrees of freedom to identify fixity and system size
+            self.builder_and_solver.SetUpSystem(self.main_model_part)
+            #allocate memory for the system and preallocate the structure of the matrix
+            self.builder_and_solver.ResizeAndInitializeVectors(self.scheme, self.pA,self.pDx,self.pb,self.main_model_part)
+
+            self.A = (self.pA)
+            self.Dx = (self.pDx)
+            self.b = (self.pb)
+
+        self.builder_and_solver.InitializeSolutionStep(self.main_model_part,self.A,self.Dx,self.b)
+        self.scheme.InitializeSolutionStep(self.main_model_part,self.A,self.Dx,self.b)
+        self.builder_and_solver.BuildRHS(self.scheme, self.main_model_part, self.b)
+        self.convergence_criteria.InitializeSolutionStep(self.main_model_part, self.builder_and_solver.GetDofSet(), self.A, self.Dx, self.b)
+
+    def SolveSolutionStep(self):
+        for k in range(self.NumberOfContinuationIterations):
+            if k == self.NumberOfContinuationIterations - 1:
+                self.main_model_part.ProcessInfo[ConvectionDiffusionApplication.SHOCK_CAPTURING_INTENSITY] = self.final_shock_capturing_constant
+            else:
+                self.main_model_part.ProcessInfo[ConvectionDiffusionApplication.SHOCK_CAPTURING_INTENSITY] = self.final_shock_capturing_constant * (1 - np.exp(- 2 * k/self.NumberOfContinuationIterations))
+            self.SolveSolutionStepForFixedContinuationParameter()
+            print(100*'*', 'self.final_shock_capturing_constant', self.main_model_part.ProcessInfo[ConvectionDiffusionApplication.SHOCK_CAPTURING_INTENSITY])
+
+    def SolveSolutionStepForFixedContinuationParameter(self):
+        max_iterations = self.max_iterations
+        dof_set = self.builder_and_solver.GetDofSet()
+        iteration_number = 1
+
+        self.main_model_part.ProcessInfo[KratosMultiphysics.NL_ITERATION_NUMBER] = iteration_number
+
+        self.scheme.InitializeNonLinIteration(self.main_model_part,self.A,self.Dx,self.b)
+        self.convergence_criteria.InitializeNonLinearIteration(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+        is_converged = self.convergence_criteria.PreCriteria(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+        for node in self.main_model_part.Nodes:
+            if node.Id == 44:
+                print(node.GetSolutionStepValue(KratosMultiphysics.TEMPERATURE))
+
+        if (self.rebuild_level > 0 or self.StiffnessMatrixIsBuilt == False):
+            self.space_utils.SetToZeroVector(self.Dx)
+            self.space_utils.SetToZeroVector(self.b)
+            self.space_utils.SetToZeroMatrix(self.A)
+            if (self.UseOldStiffnessInFirstIteration == True):
+                self.builder_and_solver.BuildAndSolveLinearizedOnPreviousIteration(self.scheme,self.main_model_part,self.A,self.Dx,self.b, self.MoveMeshFlag)
+            else:
+                self.builder_and_solver.BuildAndSolve(self.scheme,self.main_model_part,self.A,self.Dx,self.b)
+        else:
+            self.space_utils.SetToZeroVector(self.Dx)
+            self.space_utils.SetToZeroVector(self.b)
+
+            self.builder_and_solver.BuildRHSAndSolve(self.scheme,self.main_model_part,self.A,self.Dx,self.b)
+
+        self.scheme.Update(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+        self.scheme.FinalizeNonLinIteration(self.main_model_part,self.A,self.Dx,self.b)
+        self.convergence_criteria.FinalizeNonLinearIteration(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+        if (is_converged):
+            if (self.convergence_criteria.GetActualizeRHSflag()):
+                self.space_utils.SetToZeroVector(self.b)
+                self.builder_and_solver.BuildRHS(self.scheme,self.main_model_part,self.b)
+
+            is_converged = self.convergence_criteria.PostCriteria(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+
+        while (is_converged == False and iteration_number < max_iterations):
+
+            iteration_number += 1
+
+            self.main_model_part.ProcessInfo[KratosMultiphysics.NL_ITERATION_NUMBER] = iteration_number
+
+            self.scheme.InitializeNonLinIteration(self.main_model_part,self.A,self.Dx,self.b)
+            self.convergence_criteria.InitializeNonLinearIteration(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+            is_converged = self.convergence_criteria.PreCriteria(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+            if (self.space_utils.Size(self.Dx) != 0):
+                if (self.rebuild_level > 1 or self.StiffnessMatrixIsBuilt == False):
+                    self.space_utils.SetToZeroVector(self.Dx)
+                    self.space_utils.SetToZeroVector(self.b)
+                    self.space_utils.SetToZeroMatrix(self.A)
+                    self.builder_and_solver.BuildAndSolve(self.scheme,self.main_model_part,self.A,self.Dx,self.b)
+                else:
+                    self.space_utils.SetToZeroVector(self.Dx)
+                    self.space_utils.SetToZeroVector(self.b)
+                    self.builder_and_solver.BuildRHSAndSolve(self.scheme,self.main_model_part,self.A,self.Dx,self.b)
+
+            self.scheme.Update(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+            self.scheme.FinalizeNonLinIteration(self.main_model_part,self.A,self.Dx,self.b)
+            self.convergence_criteria.FinalizeNonLinearIteration(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+            residual_is_updated = False
+
+            if (is_converged):
+                if (self.convergence_criteria.GetActualizeRHSflag()):
+                    self.space_utils.SetToZeroVector(self.b)
+                    self.builder_and_solver.BuildRHS(self.scheme,self.main_model_part,self.b)
+                    residual_is_updated = True
+                is_converged = self.convergence_criteria.PostCriteria(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+
+        if (iteration_number >= max_iterations):
+            print('NOT CONVERGENCE!!!!!')
+        else:
+            print("Convergence achieved after " + str(iteration_number) + " / " + str(max_iterations) + " iterations")
+
+        return is_converged
+
+    def Check(self):
+        self.main_model_part.Check()
+        self.builder_and_solver.Check(self.main_model_part)
+        self.scheme.Check(self.main_model_part)
+        self.convergence_criteria.Check(self.main_model_part)
+
+    def FinalizeSolutionStep(self):
+        dof_set = self.builder_and_solver.GetDofSet()
+        self.scheme.FinalizeSolutionStep(self.main_model_part,self.A,self.Dx,self.b)
+        self.builder_and_solver.FinalizeSolutionStep(self.main_model_part,self.A,self.Dx,self.b)
+        self.convergence_criteria.FinalizeSolutionStep(self.main_model_part,dof_set,self.A,self.Dx,self.b)
+        self.scheme.Clean()
+        if (self.ReformDofSetAtEachStep == True):
+            self.Clear()
+
+    def Clear(self):
+        self.builder_and_solver.SetDofSetIsInitializedFlag(False)
+        self.builder_and_solver.Clear()
+        self.space_utils.Clear(self.Dx)
+        self.space_utils.Clear(self.b)
+        self.space_utils.Clear(self.A)
+        self.scheme.Clear()
+        self.InitializeWasPerformed = False
+
+    def SetEchoLevel(self, echo_level):
+        self.builder_and_solver.SetEchoLevel(echo_level)
