abf4bbc8a90680c945f916d08e23be65d18663f8
Chengshun Shang <chengshun.shang1996@gmail.com>
2024-07-17 09:50:39 +0200

    minor

--- applications/DEMApplication/custom_constitutive/DEM_parallel_bond_bilinear_damage_mixed_CL.cpp
+++ applications/DEMApplication/custom_constitutive/DEM_parallel_bond_bilinear_damage_mixed_CL.cpp
@@ -142,7 +142,7 @@ void DEM_parallel_bond_bilinear_damage_mixed::CalculateForces(const ProcessInfo&
                 delta_equivalent_0 = delta_at_undamaged_peak_normal * sqrt(1 + beta * beta);
             }
 
-            double deta_equivalent_0_normal = delta_equivalent_0 / sqrt(1 + beta * beta);
+            //double deta_equivalent_0_normal = delta_equivalent_0 / sqrt(1 + beta * beta);
             //double deta_equivalent_0_tangential = beta * deta_equivalent_0_normal;
             double k_bar_equivalent = (kn_el / calculation_area + beta * beta * kt_el / calculation_area) / (1 + beta * beta);
 
