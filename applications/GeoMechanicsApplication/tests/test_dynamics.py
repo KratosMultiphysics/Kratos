c55d77dbdebb9d1ccdaf7948df1739cc6da32033
aronnoordam <aronnoordam@gmail.com>
2024-07-24 13:24:28 +0200

    made strip load test more general

--- applications/GeoMechanicsApplication/tests/test_dynamics.py
+++ applications/GeoMechanicsApplication/tests/test_dynamics.py
@@ -1,8 +1,14 @@
 import os
 import json
+
+import sys
+sys.path.append(r"D:\software_development\Kratos2\bin\Release")
+sys.path.append(r"D:\software_development\Kratos2\bin\Release\libs")
+
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.GeoMechanicsApplication as KratosGeo
+import KratosMultiphysics.StructuralMechanicsApplication as KratosStruct
 import test_helper
 from strip_load_semi_analytical_solution import StripLoad
 
@@ -112,7 +118,7 @@ class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
 
         """
         # set to true if the results should be checked with the analytical solution
-        CHECK_RESULTS = False
+        CHECK_RESULTS = True
 
         test_name = 'test_constant_strip_load_2d'
         file_path = test_helper.get_file_path(os.path.join('.', test_name))
@@ -133,40 +139,50 @@ class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
         # If this test is altered, results between the analytical solution and the calculated solution can be compared
         # below
         if CHECK_RESULTS:
+            # get properties from material file as not all variables are registered in python
+            with open(os.path.join(file_path, "MaterialParameters_stage_1.json")) as fp:
+                material_parameters = json.load(fp)
 
-            # get json output
-            json_process = simulation._GetListOfProcesses()[4]
+            material_variables = material_parameters["properties"]["Material"]["Variables"]
+            process_info = simulation.model.GetModelPart('PorousDomain.porous_computational_model_part').ProcessInfo
 
-            elements = json_process.sub_model_part.Elements
+            top_surface = 10.0  # follows from the mesh
+            line_load_length = 1.0  # follows from the mesh
+            load_value = -1000.0  # follows from table in the mdpa file
+            end_time = process_info.GetValue(Kratos.TIME)
 
+            # initialise semi-analytical solution
+            analytical_solution = StripLoad(material_variables["YOUNG_MODULUS"],
+                                            material_variables["POISSON_RATIO"],
+                                            (1 - material_variables["POROSITY"]) * material_variables["DENSITY_SOLID"],
+                                            load_value)
+
+            # get calculated json output
+            json_model_part = simulation.model.GetModelPart('PorousDomain.json_output')
+
+            elements = json_model_part.Elements
+            x_coords = []
             calculated_vert_stresses = []
             analytical_vert_stresses = []
-
-            porosity = 0.0
-            density_solid = 1020
-
-            line_load_length = 1
-            load_value = -1000
-            end_time = 1.0
-
-            analytical_solution = StripLoad(2.55e3 * 36, 0.25, (1 - porosity) * density_solid, load_value)
-
-            x_coords = []
             for element in elements:
                 # get centroid
                 centroid = element.GetGeometry().Center()
                 x_coord = centroid.X
                 y_coord = centroid.Y
-                depth_check = 10 - y_coord
+                depth_check = top_surface - y_coord
                 x_coords.append(centroid.X)
 
-                vert_stress_analytic = analytical_solution.calculate_vertical_stress(x_coord, depth_check, end_time, line_load_length, load_value)
+                vert_stress_analytic = analytical_solution.calculate_vertical_stress(x_coord,
+                                                                                     depth_check,
+                                                                                     end_time,
+                                                                                     line_load_length,
+                                                                                     load_value)
 
                 analytical_vert_stresses.append(-vert_stress_analytic)
 
                 # get element mean vertical cauchy stress
                 element_stress = element.CalculateOnIntegrationPoints(Kratos.CAUCHY_STRESS_VECTOR,
-                                                                      json_process.sub_model_part.ProcessInfo)
+                                                                      json_model_part.ProcessInfo)
                 vert_stress = test_helper.compute_mean_list([gauss_stress[1] for gauss_stress in element_stress])
 
                 calculated_vert_stresses.append(vert_stress)
