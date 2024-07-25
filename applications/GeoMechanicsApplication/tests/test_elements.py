69eee4094ce924e8610cac8c52b18bbbababfe34
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-07-25 16:41:52 +0200

    Give an expected value a descriptive name

--- applications/GeoMechanicsApplication/tests/test_elements.py
+++ applications/GeoMechanicsApplication/tests/test_elements.py
@@ -227,8 +227,9 @@ class KratosGeoMechanicsElementTypeTests(KratosUnittest.TestCase):
     def assertVerticalStressAtBottomNodes(self, output_data, bottom_node_ids):
         end_time = 1.0
         nodal_stress_tensors = test_helper.GiDOutputFileReader.nodal_values_at_time("NODAL_CAUCHY_STRESS_TENSOR", end_time, output_data, bottom_node_ids)
+        expected_stress_yy = -1e4
         for stress_tensor in nodal_stress_tensors:
-            self.assertAlmostEqual(stress_tensor[1], -1e4)
+            self.assertAlmostEqual(stress_tensor[1], expected_stress_yy)
 
 if __name__ == '__main__':
     KratosUnittest.main()
