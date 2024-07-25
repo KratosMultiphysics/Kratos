ecc4a3510c80ebd38903f465adb85aa13796d23b
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-07-25 12:01:00 +0200

    Check that for all element tests the GiD output file exists
    Also define and pass the bottom node IDs for each test, even though they
    are not being used yet.

--- applications/GeoMechanicsApplication/tests/test_elements.py
+++ applications/GeoMechanicsApplication/tests/test_elements.py
@@ -22,14 +22,15 @@ class KratosGeoMechanicsElementTypeTests(KratosUnittest.TestCase):
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 1, 5]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [5, 7, 9]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
         reader = test_helper.GiDOutputFileReader()
-        output_data = reader.read_output_from(os.path.join(file_path, f"{test_name}.post.res"))
+        output_data = reader.read_output_from(output_file_path)
         end_time = 1.0
-        bottom_node_ids = [5, 7, 9]
         nodal_stress_tensors = test_helper.GiDOutputFileReader.nodal_values_at_time("NODAL_CAUCHY_STRESS_TENSOR", end_time, output_data, bottom_node_ids)
         for stress_tensor in nodal_stress_tensors:
             self.assertAlmostEqual(stress_tensor[1], -1e4)
@@ -39,68 +40,81 @@ class KratosGeoMechanicsElementTypeTests(KratosUnittest.TestCase):
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, "test_triangle_3n.post.res")
         top_node_nbrs = [0, 1, 5]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [5, 7, 9]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_triangle_6n(self):
         test_name = 'test_triangle_6n'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 2, 4, 9, 15]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [17, 18, 22, 24, 25]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_triangle_10n(self):
         test_name = 'test_triangle_10n'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [9, 8, 7, 6]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [1, 2, 3, 4]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_triangle_15n(self):
         test_name = 'test_triangle_15n'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [12, 11, 10, 9, 8]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [1, 2, 3, 4, 5]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_triangle_3n_fic(self):
         test_name = 'test_triangle_3n_fic'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 1, 5]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [5, 7, 9]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_triangle_6n_fic(self):
         test_name = 'test_triangle_6n_fic'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 2, 4, 9, 15]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [17, 18, 22, 24, 25]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_quad_4n(self):
         test_name = 'test_quad_4n'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 1, 5]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [5, 7, 9]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
         reader = test_helper.GiDOutputFileReader()
-        output_data = reader.read_output_from(os.path.join(file_path, f"{test_name}.post.res"))
+        output_data = reader.read_output_from(output_file_path)
         end_time = 1.0
-        bottom_node_ids = [5, 7, 9]
         nodal_stress_tensors = test_helper.GiDOutputFileReader.nodal_values_at_time("NODAL_CAUCHY_STRESS_TENSOR", end_time, output_data, bottom_node_ids)
         for stress_tensor in nodal_stress_tensors:
             self.assertAlmostEqual(stress_tensor[1], -1e4)
@@ -110,34 +124,40 @@ class KratosGeoMechanicsElementTypeTests(KratosUnittest.TestCase):
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 2, 4, 8, 12]
         n_dim = 2
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [14, 15, 18, 20, 21]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_tetra_4n(self):
         test_name = 'test_tetra_4n'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 2, 9, 3, 6, 13, 8, 14, 20]
         n_dim = 3
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [11, 12, 17, 20, 22, 23, 25, 26, 27]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
     def test_tetra_10n(self):
         test_name = 'test_tetra_10n'
         file_path = test_helper.get_file_path(os.path.join('.', test_name + '.gid'))
         simulation = test_helper.run_kratos(file_path)
 
+        output_file_path = os.path.join(file_path, f"{test_name}.post.res")
         top_node_nbrs = [0, 3, 9, 28, 53,
                          1, 6, 13, 32,
                          10, 15, 21, 44, 76,
                          27, 34, 41, 64, 94,
                          51, 56, 74, 95, 110]
         n_dim = 3
-        self.assert_linear_elastic_block(simulation, top_node_nbrs, n_dim)
+        bottom_node_ids = [53, 58, 63, 64, 74, 76, 83, 84, 88, 91, 94, 98, 99, 104, 106, 110, 112, 113, 115, 118, 119, 121, 122, 124, 125]
+        self.assert_linear_elastic_block(simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids)
 
 
-    def assert_linear_elastic_block(self,simulation, top_node_nbrs, n_dim):
+    def assert_linear_elastic_block(self, simulation, output_file_path, top_node_nbrs, n_dim, bottom_node_ids):
         """
         Assert results of a linear elastic block. The sides of the block can move freely in vertical direction and are
         fixed in horizontal direction. The bottom of the block is fixed. On top of the block, a load of 10kN/m2 is
@@ -148,6 +168,8 @@ class KratosGeoMechanicsElementTypeTests(KratosUnittest.TestCase):
         :param n_dim: number of dimensions
         :return:
         """
+        self.assertTrue(os.path.exists(output_file_path))
+
         total_stresses = test_helper.get_total_stress_tensor(simulation)
         total_stresses_xx = [integration_point[0,0] for element in total_stresses for integration_point in element]
         if n_dim >= 2:
