74ab39c87ddaddb4724f3881a150108c38f9afff
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-07-26 16:04:02 +0200

    No failure when at least two coordinates in horizontal direction are given

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_apply_constant_phreatic_multi_line_pressure_process.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_apply_constant_phreatic_multi_line_pressure_process.cpp
@@ -39,10 +39,9 @@ namespace Kratos::Testing
 KRATOS_TEST_CASE_IN_SUITE(ApplyConstantPhreaticMultiLinePressureProcessThrowsWhenLessThanTwoCoordinatesInGravityDirectionAreGiven,
                           KratosGeoMechanicsFastSuiteWithoutKernel)
 {
-    auto  model        = Model{};
-    auto& r_model_part = model.CreateModelPart("foo");
-
-    auto test_parameters = Parameters{R"(
+    auto  model           = Model{};
+    auto& r_model_part    = model.CreateModelPart("foo");
+    auto  test_parameters = Parameters{R"(
             {
                 "model_part_name": "foo",
                 "variable_name": "WATER_PRESSURE",
@@ -66,10 +65,9 @@ KRATOS_TEST_CASE_IN_SUITE(ApplyConstantPhreaticMultiLinePressureProcessThrowsWhe
 KRATOS_TEST_CASE_IN_SUITE(ApplyConstantPhreaticMultiLinePressureProcessThrowsWhenLessThanTwoCoordinatesInHorizontalDirectionAreGiven,
                           KratosGeoMechanicsFastSuiteWithoutKernel)
 {
-    auto  model        = Model{};
-    auto& r_model_part = model.CreateModelPart("foo");
-
-    auto test_parameters = Parameters{R"(
+    auto  model           = Model{};
+    auto& r_model_part    = model.CreateModelPart("foo");
+    auto  test_parameters = Parameters{R"(
             {
                 "model_part_name": "foo",
                 "variable_name": "WATER_PRESSURE",
@@ -111,6 +109,32 @@ KRATOS_TEST_CASE_IN_SUITE(ApplyConstantPhreaticMultiLinePressureProcessDoesNotTh
 
     test_parameters.RemoveValue("y_coordinates");
     test_parameters.AddVector("y_coordinates", ScalarVector{5, 1.0});
+
+    KRATOS_EXPECT_TRUE(CanCreateInstanceOfApplyConstantPhreaticMultiLinePressureProcessWithoutFailure(
+        r_model_part, test_parameters))
+}
+
+KRATOS_TEST_CASE_IN_SUITE(ApplyConstantPhreaticMultiLinePressureProcessDoesNotThrowWhenAtLeastTwoCoordinatesInHorizontalDirectionAreGiven,
+                          KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    auto  model           = Model{};
+    auto& r_model_part    = model.CreateModelPart("foo");
+    auto  test_parameters = Parameters{R"(
+            {
+                "model_part_name": "foo",
+                "variable_name": "WATER_PRESSURE",
+                "x_coordinates": [1.0, 2.0],
+                "y_coordinates": [0.0, 1.0],
+				"z_coordinates": [0.0, 0.0],
+                "gravity_direction": 1,
+                "out_of_plane_direction": 2
+            }  )"};
+
+    KRATOS_EXPECT_TRUE(CanCreateInstanceOfApplyConstantPhreaticMultiLinePressureProcessWithoutFailure(
+        r_model_part, test_parameters))
+
+    test_parameters.RemoveValue("x_coordinates");
+    test_parameters.AddVector("x_coordinates", ScalarVector{5, 1.0});
 
     KRATOS_EXPECT_TRUE(CanCreateInstanceOfApplyConstantPhreaticMultiLinePressureProcessWithoutFailure(
         r_model_part, test_parameters))
