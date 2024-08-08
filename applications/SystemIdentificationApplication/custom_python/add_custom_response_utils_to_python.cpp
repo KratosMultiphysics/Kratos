592ed66da34c6750f09225a46c0bb542ea74539d
sunethwarna <suneth.warna@gmail.com>
2024-08-08 11:32:37 +0530

    expose sens. iso. resp utils to python

--- applications/SystemIdentificationApplication/custom_python/add_custom_response_utils_to_python.cpp
+++ applications/SystemIdentificationApplication/custom_python/add_custom_response_utils_to_python.cpp
@@ -19,6 +19,7 @@
 
 // Application includes
 #include "custom_utilities/response/sensor_distance_summation_response_utils.h"
+#include "custom_utilities/response/sensor_isolation_response_utils.h"
 
 // Include base h
 #include "custom_python/add_custom_response_utils_to_python.h"
@@ -33,7 +34,11 @@ void AddCustomResponseUtilsToPython(pybind11::module& m)
         .def("CalculateValue", &SensorDistanceSummationResponseUtils::CalculateValue, py::arg("model_part"), py::arg("distance_matrix"))
         .def("CalculateGradient", &SensorDistanceSummationResponseUtils::CalculateGradient, py::arg("model_part"), py::arg("distance_matrix"))
         ;
+
+    m.def_submodule("SensorIsolationResponseUtils")
+        .def("CalculateValue", &SensorIsolationResponseUtils::CalculateValue, py::arg("model_part"), py::arg("radius"), py::arg("distance_matrix"))
+        .def("CalculateGradient", &SensorIsolationResponseUtils::CalculateGradient, py::arg("model_part"), py::arg("radius"), py::arg("distance_matrix"))
+        ;
 }
-//
+
 } // namespace Kratos::Python
-//
