dfda00af875ff117636b834024bc0bb7620d2124
Richard Faasse <richard.faasse@deltares.nl>
2024-08-14 15:35:38 +0200

    Fixed unit test such that isoparametric coordinates are used instead of global coordinates

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_geometries/test_interface_geometry.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_geometries/test_interface_geometry.cpp
@@ -67,17 +67,15 @@ KRATOS_TEST_CASE_IN_SUITE(InterfaceGeometry_ReturnsCorrectShapeFunctionValuesInN
 {
     const auto          geometry = LineInterfaceGeometry();
     PointerVector<Node> nodes;
-    auto                node_1 = Kratos::make_intrusive<Node>(1, -1.0, 0.0, 0.0);
-    nodes.push_back(node_1);
-    auto node_2 = Kratos::make_intrusive<Node>(2, 1.0, 0.0, 0.0);
-    nodes.push_back(node_2);
+    nodes.push_back(Kratos::make_intrusive<Node>(1, 0.0, 0.0, 0.0));
+    nodes.push_back(Kratos::make_intrusive<Node>(2, 5.0, 0.0, 0.0));
 
     const auto new_geometry = geometry.Create(1, nodes);
 
-    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(0, *node_1), 1.0);
-    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(1, *node_1), 0.0);
-    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(1, *node_2), 1.0);
-    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(0, *node_2), 0.0);
+    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(0, array_1d<double, 3>{-1.0, 0.0, 0.0}), 1.0);
+    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(1, array_1d<double, 3>{-1.0, 0.0, 0.0}), 0.0);
+    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(1, array_1d<double, 3>{1.0, 0.0, 0.0}), 1.0);
+    KRATOS_EXPECT_DOUBLE_EQ(new_geometry->ShapeFunctionValue(0, array_1d<double, 3>{1.0, 0.0, 0.0}), 0.0);
 }
 
 } // namespace Kratos::Testing
