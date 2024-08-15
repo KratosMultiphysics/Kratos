fd83e4b6ce73706c98f3d46c7972df44e9a938ea
sunethwarna <suneth.warna@gmail.com>
2024-08-15 12:07:08 +0530

    add tests to suite

--- applications/SystemIdentificationApplication/tests/test_SystemIdentificationApplication.py
+++ applications/SystemIdentificationApplication/tests/test_SystemIdentificationApplication.py
@@ -7,6 +7,8 @@ import test_system_identification
 import test_smooth_clamper
 import test_distance_matrix
 import test_mask_utils
+import test_sensor_mask_status
+import test_sensor_mask_status_kd_tree
 import responses.test_sensor_count_response
 import responses.test_sensor_distance_summation_response
 import responses.test_sensor_isolation_response
@@ -21,6 +23,8 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_smooth_clamper.TestSmoothClamper]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_distance_matrix.TestDistanceMatrix]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_mask_utils.TestMaskUtils]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_sensor_mask_status.TestSensorMaskStatus]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_sensor_mask_status_kd_tree.TestSensorMaskStatusKDTree]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_system_identification.TestSystemIdentification]))
 
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([responses.test_sensor_count_response.TestSensorCountResponse]))
