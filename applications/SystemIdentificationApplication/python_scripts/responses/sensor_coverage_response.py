9a7fb912dc24e288eecfa6d4dae8b764f885026a
sunethwarna <suneth.warna@gmail.com>
2024-08-16 17:09:09 +0530

    add sensor coverage response

---
+++ applications/SystemIdentificationApplication/python_scripts/responses/sensor_coverage_response.py
@@ -0,0 +1,84 @@
+from typing import Optional
+
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.OptimizationApplication as KratosOA
+import KratosMultiphysics.SystemIdentificationApplication as KratosSI
+from KratosMultiphysics.OptimizationApplication.responses.response_function import ResponseFunction
+from KratosMultiphysics.OptimizationApplication.responses.response_function import SupportedSensitivityFieldVariableTypes
+from KratosMultiphysics.OptimizationApplication.utilities.union_utilities import SupportedSensitivityFieldVariableTypes
+from KratosMultiphysics.OptimizationApplication.utilities.model_part_utilities import ModelPartOperation
+from KratosMultiphysics.OptimizationApplication.utilities.optimization_problem import OptimizationProblem
+from KratosMultiphysics.OptimizationApplication.utilities.component_data_view import ComponentDataView
+
+def Factory(model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem) -> ResponseFunction:
+    if not parameters.Has("name"):
+        raise RuntimeError(f"SensorCoverageResponse instantiation requires a \"name\" in parameters [ parameters = {parameters}].")
+    if not parameters.Has("settings"):
+        raise RuntimeError(f"SensorCoverageResponse instantiation requires a \"settings\" in parameters [ parameters = {parameters}].")
+    return SensorCoverageResponse(parameters["name"].GetString(), model, parameters["settings"], optimization_problem)
+
+class SensorCoverageResponse(ResponseFunction):
+    def __init__(self, name: str, model: Kratos.Model, parameters: Kratos.Parameters, optimization_problem: OptimizationProblem):
+        super().__init__(name)
+
+        default_settings = Kratos.Parameters("""{
+            "sensor_mask_name"           : "",
+            "echo_level"                 : 0,
+            "evaluated_model_part_names" : [
+                "PLEASE_PROVIDE_A_MODEL_PART_NAME"
+            ]
+        }""")
+        parameters.ValidateAndAssignDefaults(default_settings)
+
+        self.model = model
+        self.sensor_mask_name = parameters["sensor_mask_name"].GetString()
+        self.echo_level = parameters["echo_level"].GetInt()
+
+        evaluated_model_part_names = parameters["evaluated_model_part_names"].GetStringArray()
+        if len(evaluated_model_part_names) == 0:
+            raise RuntimeError(f"No model parts were provided for SensorCoverageResponse. [ response name = \"{self.GetName()}\"]")
+
+        self.model_part_operation = ModelPartOperation(self.model, ModelPartOperation.OperationType.UNION, f"response_{self.GetName()}", evaluated_model_part_names, False)
+        self.model_part: Optional[Kratos.ModelPart] = None
+        self.optimization_problem = optimization_problem
+
+    def GetImplementedPhysicalKratosVariables(self) -> 'list[SupportedSensitivityFieldVariableTypes]':
+        return [KratosSI.SENSOR_STATUS]
+
+    def Initialize(self) -> None:
+        self.model_part = self.model_part_operation.GetModelPart()
+
+        data = ComponentDataView("sensors", self.optimization_problem).GetUnBufferedData()
+        list_of_sensors: 'list[KratosSI.Sensors.Sensor]' = data.GetValue("list_of_sensors")
+        sensor_mask_status_path = "control_updated/sensor_mask_status"
+        if not data.HasValue(sensor_mask_status_path):
+            data.SetValue(sensor_mask_status_path, KratosSI.SensorMaskStatus(self.model_part, [sensor.GetContainerExpression(self.sensor_mask_name) for sensor in list_of_sensors], self.echo_level))
+
+    def Check(self) -> None:
+        pass
+
+    def Finalize(self) -> None:
+        pass
+
+    def GetInfluencingModelPart(self) -> Kratos.ModelPart:
+        if self.model_part is None:
+            raise RuntimeError("Please call SensorCoverageResponse::Initialize first.")
+        return self.model_part
+
+    def CalculateValue(self) -> float:
+        sensor_data = ComponentDataView("sensors", self.optimization_problem)
+        sensor_mask_status: KratosSI.SensorMaskStatus = sensor_data.GetUnBufferedData().GetValue("control_updated/sensor_mask_status")
+        return KratosSI.SensorCoverageResponseUtils.CalculateValue(sensor_mask_status)
+
+    def CalculateGradient(self, physical_variable_collective_expressions: 'dict[SupportedSensitivityFieldVariableTypes, KratosOA.CollectiveExpression]') -> None:
+        # make everything zeros
+        for physical_variable, collective_expression in physical_variable_collective_expressions.items():
+            for container_expression in collective_expression.GetContainerExpressions():
+                Kratos.Expression.LiteralExpressionIO.SetDataToZero(container_expression, physical_variable)
+
+        sensor_data = ComponentDataView("sensors", self.optimization_problem)
+        sensor_mask_status: KratosSI.SensorMaskStatus = sensor_data.GetUnBufferedData().GetValue("control_updated/sensor_mask_status")
+        physical_variable_collective_expressions[KratosSI.SENSOR_STATUS].GetContainerExpressions()[0].SetExpression(KratosSI.SensorCoverageResponseUtils.CalculateGradient(sensor_mask_status).GetExpression())
+
+    def __str__(self) -> str:
+        return f"Response [type = {self.__class__.__name__}, name = {self.GetName()}, model part name = {self.model_part.FullName()}]"
