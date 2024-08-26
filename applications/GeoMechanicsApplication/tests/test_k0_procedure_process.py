6e91d3804bdb2412c399daa01438ad7735f47f96
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2024-08-26 09:39:33 +0200

    added k0 test with ocr from material input.

--- applications/GeoMechanicsApplication/tests/test_k0_procedure_process.py
+++ applications/GeoMechanicsApplication/tests/test_k0_procedure_process.py
@@ -188,6 +188,33 @@ class KratosGeoMechanicsK0ProcedureProcessTests(KratosUnittest.TestCase):
         sig_xy = sig_integrationpoint1_element126[0,1]
         self.assertEqual( sig_xy, 0.0 )
 
+    def test_k0_procedure_k0_nc_ocr(self):
+        """
+        Test to check if CAUCHY_STRESS_XX is correctly derived from CAUCHY_STRESS_YY using K0_NC and
+        OCR
+        """
+
+        test_name = os.path.join("test_k0_procedure_process", "test_k0_procedure_k0_nc_ocr")
+        file_path = test_helper.get_file_path(test_name)
+
+        # run simulation
+        simulation = test_helper.run_kratos(file_path)
+
+        # retrieve Cauchy stress tensor
+        cauchy_stresses = test_helper.get_on_integration_points(simulation,Kratos.CAUCHY_STRESS_TENSOR)
+
+        # compare cauchy_stress_xx = k0 * cauchy_stress_yy, cauchy_stress_xy = 0.
+        k0_nc      = 0.6
+        poisson_ur = 0.
+        ocr        = 1.5
+        k0 = k0_nc * ocr + ( poisson_ur / ( 1.0 - poisson_ur ) ) * ( ocr - 1.0 )
+        sig_integrationpoint1_element1 = cauchy_stresses[0][0]
+        sig_yy = sig_integrationpoint1_element1[1,1]
+        sig_xx = sig_integrationpoint1_element1[0,0]
+        self.assertAlmostEqual( sig_xx, k0*sig_yy )
+        sig_xy = sig_integrationpoint1_element1[0,1]
+        self.assertEqual( sig_xy, 0.0 )
+
     def test_k0_procedure_k0_nc_ocr_field(self):
         """
         Test to check if CAUCHY_STRESS_XX is correctly derived from CAUCHY_STRESS_YY using K0_NC and
