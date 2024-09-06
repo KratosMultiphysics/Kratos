9896aec8793f9d1d802c134c0c87901910b11052
Richard Faasse <richard.faasse@deltares.nl>
2024-09-06 15:42:51 +0200

    Reverted renames in other applications

--- applications/PoromechanicsApplication/custom_conditions/one-phase_flow/U_Pl_face_load_interface_condition.hpp
+++ applications/PoromechanicsApplication/custom_conditions/one-phase_flow/U_Pl_face_load_interface_condition.hpp
@@ -19,7 +19,7 @@
 
 // Application includes
 #include "custom_conditions/one-phase_flow/U_Pl_condition.hpp"
-#include "custom_utilities/interface_element_utilities.h"
+#include "custom_utilities/interface_element_utilities.hpp"
 #include "poromechanics_application_variables.h"
 
 namespace Kratos
