e2b2fc5bb9b879a2c5bb56d8a3e90484679be9c8
aronnoordam <aronnoordam@gmail.com>
2024-09-13 15:00:01 +0200

    clang tidy

--- applications/GeoMechanicsApplication/tests/cpp_tests/test_U_Pw_condition.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_U_Pw_condition.cpp
@@ -16,37 +16,37 @@
 #include "geo_mechanics_fast_suite.h"
 
 using namespace Kratos;
-namespace Kratos::Testing {
 
+namespace Kratos::Testing
+{
 
-        /// <summary>
-        /// Tests the calculation of the left hand side matrix of the default UPwCondition, which should be an empty matrix.
-        /// </summary>
-        /// <param name=""></param>
-        /// <param name=""></param>
-        KRATOS_TEST_CASE_IN_SUITE(CalculateLeftHandSideUPwCondition, KratosGeoMechanicsFastSuite)
-        {
+/// <summary>
+/// Tests the calculation of the left hand side matrix of the default UPwCondition, which should be an empty matrix.
+/// </summary>
+/// <param name=""></param>
+/// <param name=""></param>
+KRATOS_TEST_CASE_IN_SUITE(CalculateLeftHandSideUPwCondition, KratosGeoMechanicsFastSuite)
+{
+    Model current_model;
+    auto& r_model_part = current_model.CreateModelPart("ModelPart", 1);
 
-            Model current_model;
-            auto& r_model_part = current_model.CreateModelPart("ModelPart", 1);	
+    // create geometry as UPwCondition needs a geometry to be initialized
+    auto                              node = r_model_part.CreateNewNode(1, 0.0, 0.0, 0.0);
+    std::vector<ModelPart::IndexType> element_nodes{1};
+    auto p_geometry = r_model_part.CreateNewGeometry("Point2D", element_nodes);
 
-            // create geometry as UPwCondition needs a geometry to be initialized
-            auto node = r_model_part.CreateNewNode(1, 0.0, 0.0, 0.0);
-            std::vector< ModelPart::IndexType> element_nodes{ 1};
-            auto p_geometry = r_model_part.CreateNewGeometry("Point2D", element_nodes);
-            
-            // create UPwCondition
-            auto cond = UPwCondition<2, 2>(1, p_geometry, nullptr);
-            
-            // calculate left hand side matrix
-            Matrix left_hand_side_matrix = ZeroMatrix(6, 6);
-            cond.CalculateLeftHandSide(left_hand_side_matrix, ProcessInfo{});
-            
-            // set expected_results
-            Matrix expected_matrix = ZeroMatrix(0, 0);
-            
-            // compare results
-            KRATOS_CHECK_MATRIX_NEAR(left_hand_side_matrix, expected_matrix, 1.0e-6);
-        }
+    // create UPwCondition
+    auto cond = UPwCondition<2, 2>(1, p_geometry, nullptr);
 
+    // calculate left hand side matrix
+    Matrix left_hand_side_matrix = ZeroMatrix(6, 6);
+    cond.CalculateLeftHandSide(left_hand_side_matrix, ProcessInfo{});
+
+    // set expected_results
+    Matrix expected_matrix = ZeroMatrix(0, 0);
+
+    // compare results
+    KRATOS_CHECK_MATRIX_NEAR(left_hand_side_matrix, expected_matrix, 1.0e-6);
 }
+
+} // namespace Kratos::Testing
