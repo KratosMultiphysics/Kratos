94ea9512f75e2394b4def29837ebaac06aedf25d
aronnoordam <aronnoordam@gmail.com>
2024-09-13 10:08:52 +0200

    cleaned test_dynamics

--- applications/GeoMechanicsApplication/tests/test_dynamics.py
+++ applications/GeoMechanicsApplication/tests/test_dynamics.py
@@ -1,11 +1,9 @@
 import os
 import json
 
-import KratosMultiphysics as Kratos
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import KratosMultiphysics.GeoMechanicsApplication as KratosGeo
 import test_helper
-from strip_load_semi_analytical_solution import StripLoad
 
 
 class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
@@ -33,7 +31,6 @@ class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
         """
         test_name = 'test_1d_wave_prop_drained_soil'
         self.run_wave_through_drained_linear_elastic_soil_test(test_name)
-
 
     def test_wave_through_drained_linear_elastic_soil_constant_mass_damping(self):
         """
@@ -146,7 +143,7 @@ class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
         time step size is increased for faster calculation
         :return:
         """
-        # test_name = 'test_1d_wave_prop_drained_soil_linear_elastic_solver'
+
         file_path = test_helper.get_file_path(os.path.join('.', test_name))
 
         simulation = test_helper.run_kratos(file_path)
@@ -162,7 +159,6 @@ class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
         self.assertVectorAlmostEqual(calculated_result[where][what], expected_result[where][what])
 
         return simulation
-
 
     def test_load_on_block_2d_no_damping(self):
         """
@@ -190,78 +186,6 @@ class KratosGeoMechanicsDynamicsTests(KratosUnittest.TestCase):
         what = "DISPLACEMENT_Y"
         for node in nodes:
             self.assertVectorAlmostEqual(calculated_result[node][what], expected_result[node][what])
-
-
-    def test_constant_strip_load_2d(self):
-        """
-        Tests a constant strip load on a 10m X 10m  block. The block is loaded with a constant strip load of 1kN/m and
-        a length of 1m.
-
-        The solution should roughly follow the semi-analytical solution presented in the following publication:
-        An Introduction to Soil Dynamics , Verruijt A., 2009, Delft University of Technology, Chapter 12.2
-
-
-        """
-        # set to true if the results should be checked with the analytical solution
-        CHECK_RESULTS = False
-
-        test_name = 'test_constant_strip_load_2d'
-        file_path = test_helper.get_file_path(os.path.join('.', test_name))
-
-        simulation = test_helper.run_kratos(file_path)
-
-        if CHECK_RESULTS:
-
-            # get json output
-            json_process = simulation._GetListOfProcesses()[4]
-
-            elements = json_process.sub_model_part.Elements
-
-            calculated_vert_stresses = []
-            analytical_vert_stresses = []
-
-            porosity = 0.0
-            density_solid = 1020
-
-            line_load_length = 1
-            load_value = -1000
-            end_time = 1.0
-
-            analytical_solution = StripLoad(2.55e3 * 36, 0.25, (1 - porosity) * density_solid, load_value)
-
-            x_coords = []
-            for element in elements:
-                # get centroid
-                centroid = element.GetGeometry().Center()
-                x_coord = centroid.X
-                y_coord = centroid.Y
-                depth_check = 10 - y_coord
-                x_coords.append(centroid.X)
-
-                vert_stress_analytic = analytical_solution.calculate_vertical_stress(x_coord, depth_check, end_time,
-                                                                                     line_load_length, load_value)
-
-                analytical_vert_stresses.append(-vert_stress_analytic)
-
-                # get element mean vertical cauchy stress
-                element_stress = element.CalculateOnIntegrationPoints(Kratos.CAUCHY_STRESS_VECTOR,
-                                                                      json_process.sub_model_part.ProcessInfo)
-                vert_stress = test_helper.compute_mean_list([gauss_stress[1] for gauss_stress in element_stress])
-
-                calculated_vert_stresses.append(vert_stress)
-
-            # visualise results if matplotlib is installed
-            try:
-                import matplotlib.pyplot as plt
-                plt.plot(x_coords, analytical_vert_stresses, 'o', color="r")
-                plt.plot(x_coords, calculated_vert_stresses, 'o', color="b")
-
-                plt.legend(["Analytical", "Calculated"])
-
-                plt.show()
-
-            except ImportError:
-                print("Matplotlib not installed. Cannot visualise results")
 
 
 if __name__ == '__main__':
