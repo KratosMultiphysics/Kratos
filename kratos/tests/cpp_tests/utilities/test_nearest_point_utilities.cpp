5a8d25369555b8551a0fde85bbea6d98cd31d79e
Vicente Mataix Ferrandiz <vmataix@altair.com>
2024-09-13 17:41:18 +0200

    Some debugging

--- kratos/tests/cpp_tests/utilities/test_nearest_point_utilities.cpp
+++ kratos/tests/cpp_tests/utilities/test_nearest_point_utilities.cpp
@@ -8,6 +8,7 @@
 //                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Pooyan Dadvand
+//                   Vicente Mataix Ferrandiz
 //
 
 // System includes
@@ -20,7 +21,126 @@
 #include "geometries/triangle_3d_3.h"
 #include "utilities/geometry_utilities/nearest_point_utilities.h"
 
+// Debugging
+#include "input_output/vtk_output.h"
+
 namespace Kratos::Testing {
+
+namespace
+{
+    // This method is for debugging
+    void WriteVTK(ModelPart& rModelPart)
+    {
+        auto volume_vtk_output_parameters = Parameters(R"({
+            "model_part_name"                    : "})"+rModelPart.Name()+R"({",
+            "file_format"                        : "ascii",
+            "output_precision"                   : 8,
+            "output_path"                        : ")"+rModelPart.Name()+R"(",
+            "nodal_data_value_variables"         : [],
+            "nodal_flags"                        : [],
+            "condition_data_value_variables"     : [],
+            "condition_flags"                    : []
+        })");
+        VtkOutput volume_vtk_output(rModelPart, volume_vtk_output_parameters);
+        volume_vtk_output.PrintOutput();
+    }
+}
+
+/**
+ * @brief Finds the nearest point to the given point on a line segment. 
+ * @details It first projects the point into the line. If the projected point is inside the segment boundary 
+ * it returns the projected point. If not it returns the nearest end point of the line.
+ * @tparam Type of the Point 
+ * @tparam TGeometryType The type of the line. Assumes to have [] access and IsInside method
+ * @param rPoint The query point which we want to get nearest point to it on the line
+ * @param rLine The line in which we want to find the nearest point to rPoint
+ * @return The nearest point to rPoint
+ */
+template<class TPointType, class TGeometryType>
+static Point LocalLineNearestPoint(
+    const TPointType& rPoint, 
+    const TGeometryType& rLine
+    )
+{
+    KRATOS_DEBUG_ERROR_IF_NOT(rLine.size() == 2) << "This function only accepts Line2D2 as input" << std::endl;
+    Point result;
+    const Point line_projected_point = GeometricalProjectionUtilities::FastProjectOnLine(rLine, rPoint, result);
+    array_1d<double,3> projected_local;
+    if(rLine.IsInside(result.Coordinates(), projected_local))
+        return result;
+
+    const double distance1 = norm_2(rLine[0] - result);
+    const double distance2 = norm_2(rLine[1] - result);
+
+    result = (distance1 < distance2) ? rLine[0] : rLine[1];
+    return result;
+}
+
+
+/**
+ * @brief Finds the nearest point to the given point on a triangle. 
+ * @details It first projects the point into the triangle surface. If the projected point is inside the triangle 
+ * it returns the projected point. If not it returns the nearest point on the edges of the triangle.
+ * Dividing the plane of the triangle in 7 zones and find the nearest reflecting those zones
+ *
+ *             \       /
+ *              \  6  /
+ *               \   /
+ *                \ /
+ *                 /\ 
+ *                /  \ 
+ *          2    /    \     3
+ *              /   1  \ 
+ *             /        \ 
+ *    _______ /__________\_____________ 
+ *           /            \ 
+ *     5    /       4      \    7
+ *         /                \ 
+ *        /                  \ 
+ * @tparam Type of the Point 
+ * @tparam TGeometryType The type of the triangle. Assumes to have [] access and IsInside method
+ * @param rPoint The query point which we want to get nearest point to it on the line
+ * @param rTriangle The triangle in which we want to find the nearest point to rPoint
+ * @return The nearest point to rPoint
+ */
+template<class TPointType, class TGeometryType>
+static Point LocalTriangleNearestPoint(
+    const TPointType& rPoint,
+    const TGeometryType& rTriangle
+    )
+{
+    constexpr double Tolerance = 1e-12;
+    Point result;
+    array_1d<double,3> local_coordinates = ZeroVector(3);
+    const Point center = rTriangle.Center();
+    const array_1d<double, 3> normal = rTriangle.UnitNormal(local_coordinates);
+    double distance = 0.0;
+    const Point point_projected = GeometricalProjectionUtilities::FastProject( center, rPoint, normal, distance);
+    rTriangle.PointLocalCoordinates(local_coordinates, point_projected);
+    using line_point_type= typename TGeometryType::PointType;
+
+    if(local_coordinates[0] < -Tolerance) { // case 2,5,6
+        if(local_coordinates[1] < -Tolerance) { // case 5
+            result = rTriangle[0];
+        } else if ((local_coordinates[0] + local_coordinates[1]) > (1.0+Tolerance)) { // case 6
+            result = rTriangle[2];
+        } else {
+            result = LocalLineNearestPoint(rPoint, Line3D2<line_point_type>(rTriangle.pGetPoint(0), rTriangle.pGetPoint(2)));
+        }
+    } else if(local_coordinates[1] < -Tolerance) { // case 4,7 (case 5 is already covered in previous if)
+        if ((local_coordinates[0] + local_coordinates[1]) > (1.0+Tolerance)) { // case 7
+            result = rTriangle[1];
+        } else { // case 4
+            result = LocalLineNearestPoint(rPoint, Line3D2<line_point_type>(rTriangle.pGetPoint(0), rTriangle.pGetPoint(1)));
+        }
+    } else if ((local_coordinates[0] + local_coordinates[1]) > (1.0+Tolerance)) { // case 3
+        result = LocalLineNearestPoint(rPoint, Line3D2<line_point_type>(rTriangle.pGetPoint(1), rTriangle.pGetPoint(2)));
+    } else {  // inside
+        result = point_projected;
+    }
+
+    return result;
+}
 
 KRATOS_TEST_CASE_IN_SUITE(LineNearestPoint, KratosCoreFastSuite)
 {
@@ -182,6 +302,42 @@ KRATOS_TEST_CASE_IN_SUITE(TriangleOutOfPlaneNearestPoint, KratosCoreFastSuite)
     scenario = NearestPointUtilities::TriangleNearestPoint(far_left_point, triangle, nearest_point);
     KRATOS_EXPECT_VECTOR_NEAR(nearest_point, Point(0.00, 0.00, 0.00), 1e-6);
     KRATOS_EXPECT_EQ(scenario, TriangleNearestPointLocation::ON_TRIANGLE_EDGE_01);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(TriangleCornerCasesNearestPoint, KratosCoreFastSuite)
+{
+    Model current_model;
+    ModelPart& r_model_part = current_model.CreateModelPart("Main");
+
+    auto p_node_0 = r_model_part.CreateNewNode(0, 0.0150564, 0.0125443, -0.00755447);
+
+    auto p_node_1 = r_model_part.CreateNewNode(1, 0.018141,0.013636,-0.000874817);
+    auto p_node_2 = r_model_part.CreateNewNode(2, 0.0158935,0.0173839,-0.00353975);
+    auto p_node_3 = r_model_part.CreateNewNode(3, 0.0155049,0.02,-0.000874817);
+
+    auto p_cond = r_model_part.CreateNewCondition("SurfaceCondition3D3N", 1, {{1, 2, 3}}, nullptr);
+
+    Point nearest_point = NearestPointUtilities::TriangleNearestPoint(*p_node_0, p_cond->GetGeometry());
+    const double distance = norm_2(nearest_point - *p_node_0);
+    Point local_nearest_point = LocalTriangleNearestPoint(*p_node_0, p_cond->GetGeometry());
+    const double local_distance = norm_2(local_nearest_point - *p_node_0);
+
+    // Debugging
+    KRATOS_WATCH(distance);
+    KRATOS_WATCH(local_distance);
+
+    // Debugging
+    WriteVTK(r_model_part);
+
+    // More debugging
+    ModelPart& r_model_part_solution = current_model.CreateModelPart("Solution");
+    r_model_part_solution.CreateNewNode(1, nearest_point[0], nearest_point[1], nearest_point[2]);
+    WriteVTK(r_model_part_solution);
+    ModelPart& r_model_part_local_solution = current_model.CreateModelPart("LocalSolution");
+    r_model_part_local_solution.CreateNewNode(1, local_nearest_point[0], local_nearest_point[1], local_nearest_point[2]);
+    WriteVTK(r_model_part_local_solution);
+
+    KRATOS_EXPECT_VECTOR_NEAR(nearest_point, local_nearest_point, 1e-6);
 }
 
 } // namespace Kratos::Testing
