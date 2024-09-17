7d25009a1d6511fed8dbe0593357b7f735d37103
Vicente Mataix Ferrándiz <vmataix@altair.com>
2024-09-17 10:46:59 +0200

    Refactor test_KratosCore.py to include test_obj_io

--- kratos/tests/test_KratosCore.py
+++ kratos/tests/test_KratosCore.py
@@ -94,6 +94,7 @@ import test_compute_nodal_gradient_process
 import test_voxel_modeler
 import test_model_part_utils_connectivity_generations
 import test_duplicate_mesh_modeler
+import test_obj_io
 
 # Import modules required for sequential orchestrator test
 from test_sequential_orchestrator import EmptyAnalysisStage
@@ -214,6 +215,7 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_voxel_modeler.TestBoundaryConditionVoxelizer]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_model_part_utils_connectivity_generations.TestModelPartUtilsConnectivityGenerations]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_duplicate_mesh_modeler.TestDuplicateMeshModeler]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_obj_io.TestObjIO]))
 
     if sympy_available:
         smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_sympy_fe_utilities.TestSympyFEUtilities]))
