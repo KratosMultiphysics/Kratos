f1f5a57e4ee85839856320281af185c27ac38c15
Vicente Mataix Ferrándiz <vmataix@altair.com>
2024-09-17 10:44:19 +0200

    Refactor debug_vtk function and add normal calculation in TestObjIO

--- kratos/tests/test_obj_io.py
+++ kratos/tests/test_obj_io.py
@@ -10,7 +10,20 @@ from pathlib import Path
 # To detect the os
 import platform
 
+# Import math module
+import math
+
 def debug_vtk(model_part):
+    """
+    Generates VTK output for the given model part for debugging purposes.
+
+    Parameters:
+    model_part (KratosMultiphysics.ModelPart): The model part to generate VTK output for.
+
+    This function initializes and executes the VTK output process for the provided model part.
+    It sets up the necessary parameters, including the model part name and the nodal data value variables.
+    The VTK output process is then executed through its initialization, pre-solution loop, and solution step phases.
+    """
     from KratosMultiphysics.vtk_output_process import VtkOutputProcess
     parameters = KratosMultiphysics.Parameters("""{
         "model_part_name"            : "",
@@ -18,10 +31,42 @@ def debug_vtk(model_part):
     }
     """)
     parameters["model_part_name"].SetString(model_part.Name)
-    output = VtkOutputProcess(model_part.GetModel(), parameters )
+    output = VtkOutputProcess(model_part.GetModel(), parameters)
     output.ExecuteInitialize()
     output.ExecuteBeforeSolutionLoop()
     output.ExecuteInitializeSolutionStep()
+
+def calculate_analytical_normal(node):
+    """
+    Calculates the analytical normal vector for a given node.
+
+    Parameters:
+    node (KratosMultiphysics.Node): The node for which to calculate the normal vector.
+
+    Returns:
+    KratosMultiphysics.Array3: The normalized vector representing the normal at the node.
+
+    This function computes the normal vector for a node based on its coordinates (X, Y, Z).
+    The normal vector is calculated by normalizing the node's position vector.
+    """
+    norm = math.sqrt(node.X**2 + node.Y**2 + node.Z**2)
+    normal = KratosMultiphysics.Array3([node.X / norm, node.Y / norm, node.Z / norm])
+    return normal
+
+def calculate_norm(array_3d_value):
+    """
+    Calculates the Euclidean norm (magnitude) of a 3D vector.
+
+    Parameters:
+    array_3d_value (list or tuple of float): A 3D vector represented as a list or tuple of three float values.
+
+    Returns:
+    float: The Euclidean norm of the 3D vector.
+
+    This function computes the Euclidean norm of a 3D vector by taking the square root of the sum of the squares
+    of its components.
+    """
+    return math.sqrt(array_3d_value[0]**2 + array_3d_value[1]**2 + array_3d_value[2]**2)
 
 # Define a function to get the file path given a file name
 def GetFilePath(fileName):
@@ -138,6 +183,10 @@ class TestObjIO(KratosUnittest.TestCase):
         mdpa_name = GetFilePath("auxiliar_files_for_python_unittest/mdpa_files/coarse_sphere_skin")
         ReadModelPart(mdpa_name, self.model_part)
 
+        # Compute normals
+        KratosMultiphysics.VariableUtils().SetNonHistoricalVariableToZero(KratosMultiphysics.NORMAL, self.model_part.Nodes)
+        KratosMultiphysics.NormalCalculationUtils().CalculateOnSimplexNonHistorical(self.model_part)
+
         # Compute area of the given mesh
         area_1 = 0.0
         for cond in self.model_part.Conditions:
@@ -152,7 +201,7 @@ class TestObjIO(KratosUnittest.TestCase):
         ReadModelPartFromOBJ(obj_model_part, self.obj_file)
 
         # # Debug
-        # debug_vtk(self.model_part)
+        # debug_vtk(obj_model_part)
 
         # Compute resulting area
         number_of_conditions = self.model_part.NumberOfConditions()
@@ -162,11 +211,18 @@ class TestObjIO(KratosUnittest.TestCase):
             area_2 += geom.Area()
 
         # Assert number of nodes and elements
-        self.assertEqual(obj_model_part.NumberOfNodes(), 384)
+        self.assertEqual(obj_model_part.NumberOfNodes(), self.model_part.NumberOfNodes())
         self.assertEqual(number_of_conditions, obj_model_part.NumberOfElements())
 
         # Assert that the areas match approximately
         self.assertAlmostEqual(area_1, area_2)
+
+        # Check normals results
+        for node in obj_model_part.Nodes:
+            normal = calculate_analytical_normal(node)
+            solution_normal = node.GetValue(KratosMultiphysics.NORMAL)
+            solution_normal /= calculate_norm(solution_normal)
+            self.assertLess(calculate_norm(normal - solution_normal), 0.15)
 
 if __name__ == '__main__':
     # Set the logger severity level and run the KratosUnittest
