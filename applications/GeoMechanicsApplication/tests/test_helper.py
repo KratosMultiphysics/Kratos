061a4b977ae5bd689c6229d6250a5155fe783395
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-09-24 13:33:17 +0200

    Added a rudimentary integration test
    So far, the test comprises a single stage with a single time step. It doesn't include any initial relative displacement nor any initial traction.

--- applications/GeoMechanicsApplication/tests/test_helper.py
+++ applications/GeoMechanicsApplication/tests/test_helper.py
@@ -277,6 +277,32 @@ def get_cauchy_stress_tensor(simulation):
 
     return cauchy_stress_tensors
 
+def get_cauchy_stress_vector(simulation):
+    """
+    Gets cauchy stress vector from kratos simulation
+    :param simulation:
+    :return: cauchy stress vector
+    """
+    model_part = simulation._list_of_output_processes[0].model_part
+    elements = model_part.Elements
+    cauchy_stress_vectors = [element.CalculateOnIntegrationPoints(
+        Kratos.CAUCHY_STRESS_VECTOR, model_part.ProcessInfo) for element in elements]
+
+    return cauchy_stress_vectors
+
+def get_strain_vectors(simulation):
+    """
+    Gets cauchy stress vector from kratos simulation
+    :param simulation:
+    :return: cauchy stress vector
+    """
+    model_part = simulation._list_of_output_processes[0].model_part
+    elements = model_part.Elements
+    strains = [element.CalculateOnIntegrationPoints(
+        Kratos.STRAIN, model_part.ProcessInfo) for element in elements]
+
+    return strains
+
 def get_total_stress_tensor(simulation):
     """
     Gets total stress tensor from kratos simulation
