819845f5f101eb3bd4d34ed41a13cfe1f43f77be
Richard Faasse <richard.faasse@deltares.nl>
2024-09-25 16:56:04 +0200

    Add integration tests for dirichlet and neumann boundary conditions

---
+++ applications/GeoMechanicsApplication/tests/test_line_interface_elements.py
@@ -0,0 +1,79 @@
+import os
+
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+import test_helper
+
+class KratosGeoMechanicsInterfaceElementTests(KratosUnittest.TestCase):
+    """
+    This class contains one test case for a 3+3 line interface element which is loaded
+    in compression as well as in shear.
+    """
+    def setUp(self):
+        # Code here will be placed BEFORE every test in this TestCase.
+        pass
+
+    def tearDown(self):
+        # Code here will be placed AFTER every test in this TestCase.
+        pass
+        
+    def assert_outputs_for_3_plus_3_line_interface_element(self, condition_type):
+        file_path = test_helper.get_file_path(os.path.join('line_interface_test_3+3', f'{condition_type}_single_stage'))
+        simulation = test_helper.run_kratos(file_path)
+
+        displacements = test_helper.get_displacement(simulation)
+
+        # Check the horizontal element
+        shear_load = 667.0
+        shear_stiffness = 1.5e7
+        expected_horizontal_displacement = -shear_load / shear_stiffness
+        normal_load = 333.0
+        normal_stiffness = 3.0e7
+        expected_vertical_displacement = -normal_load / normal_stiffness
+        top_node_indices = [3, 4, 5] # These correspond to nodes 11, 12, 13
+        for index in top_node_indices:
+            self.assertAlmostEqual(displacements[index][0], expected_horizontal_displacement)
+            self.assertAlmostEqual(displacements[index][1], expected_vertical_displacement)
+
+        tractions = test_helper.get_cauchy_stress_vector(simulation)
+        tractions_horizontal_element = tractions[0]
+        for index in range(3):
+            self.assertAlmostEqual(tractions_horizontal_element[index][0], -normal_load)
+            self.assertAlmostEqual(tractions_horizontal_element[index][1], -shear_load)
+
+        relative_displacements = test_helper.get_strain_vectors(simulation)
+        relative_displacements_horizontal_element = relative_displacements[0]
+        for index in range(3):
+            # Notice the first index is the normal displacement (which is vertical for this horizontally oriented
+            # element) and the second index is the shear displacement (which is horizontal for this element)
+            self.assertAlmostEqual(relative_displacements_horizontal_element[index][0], expected_vertical_displacement)
+            self.assertAlmostEqual(relative_displacements_horizontal_element[index][1], expected_horizontal_displacement)
+
+        # Check the vertical element
+        left_node_indices = [6, 7, 8] # These correspond to nodes 21, 22, 23
+        expected_horizontal_displacement = normal_load / normal_stiffness
+        expected_vertical_displacement = -shear_load / shear_stiffness
+        for index in left_node_indices:
+            self.assertAlmostEqual(displacements[index][0], expected_horizontal_displacement)
+            self.assertAlmostEqual(displacements[index][1], expected_vertical_displacement)
+
+        tractions_vertical_element = tractions[1]
+        for index in range(3):
+            self.assertAlmostEqual(tractions_vertical_element[index][0], -normal_load)
+            self.assertAlmostEqual(tractions_vertical_element[index][1], -shear_load)
+
+        relative_displacement_vertical_element = relative_displacements[1]
+        for index in range(3):
+            self.assertAlmostEqual(relative_displacement_vertical_element[index][0], -expected_horizontal_displacement)
+            self.assertAlmostEqual(relative_displacement_vertical_element[index][1], expected_vertical_displacement)
+
+
+    def test_3_plus_3_line_interface_element_with_neumann_conditions(self):
+        self.assert_outputs_for_3_plus_3_line_interface_element('Neumann')
+
+
+    def test_3_plus_3_line_interface_element_with_dirichlet_conditions(self):
+        self.assert_outputs_for_3_plus_3_line_interface_element('Dirichlet')
+
+
+if __name__ == '__main__':
+    KratosUnittest.main()
