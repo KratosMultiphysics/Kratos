edbd503e66f32b29c2e082d6a6ab3a2e3c7668a6
sunethwarna <suneth.warna@gmail.com>
2024-09-30 09:42:13 +0200

    expose to python

--- applications/SystemIdentificationApplication/custom_python/add_custom_utilities_to_python.cpp
+++ applications/SystemIdentificationApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -21,6 +21,7 @@
 
 // Application includes
 #include "custom_utilities/control_utils.h"
+#include "custom_utilities/mask_utils.h"
 #include "custom_utilities/smooth_clamper.h"
 #include "custom_utilities/sensor_generator_utils.h"
 
@@ -45,6 +46,35 @@ void AddSmoothClamper(
         ;
 }
 
+template<class TContainerType>
+void AddMaskUtilsToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+    std::string lower_prefix, upper_prefix;
+    if constexpr(std::is_same_v<TContainerType, ModelPart::NodesContainerType>) {
+        lower_prefix = "nodal";
+        upper_prefix = "Nodal";
+    } else if constexpr(std::is_same_v<TContainerType, ModelPart::ConditionsContainerType>) {
+        lower_prefix = "condition";
+        upper_prefix = "Condition";
+    } else if constexpr(std::is_same_v<TContainerType, ModelPart::ElementsContainerType>) {
+        lower_prefix = "element";
+        upper_prefix = "Element";
+    }
+
+    m.def("GetMaskSize", &MaskUtils::GetMaskSize<TContainerType>, py::arg((lower_prefix + "_mask_expression").c_str()), py::arg("required_minimum_redundancy") = 1);
+    m.def("GetMask", py::overload_cast<const ContainerExpression<TContainerType>&>(&MaskUtils::GetMask<TContainerType>), py::arg((lower_prefix + "_scalar_expression").c_str()));
+    m.def("GetMask", py::overload_cast<const ContainerExpression<TContainerType>&, const double>(&MaskUtils::GetMask<TContainerType>), py::arg((lower_prefix + "_scalar_expression").c_str()), py::arg("threshold"));
+    m.def("GetMaskThreshold", &MaskUtils::GetMaskThreshold<TContainerType>, py::arg((lower_prefix + "_scalar_expression").c_str()));
+    m.def("Union", &MaskUtils::Union<TContainerType>, py::arg((lower_prefix + "_mask_1_expression").c_str()), py::arg((lower_prefix + "_mask_2_expression").c_str()), py::arg("required_minimum_redundancy") = 1);
+    m.def("Intersect", &MaskUtils::Intersect<TContainerType>, py::arg((lower_prefix + "_mask_1_expression").c_str()), py::arg((lower_prefix + "_mask_2_expression").c_str()), py::arg("required_minimum_redundancy") = 1);
+    m.def("Subtract", &MaskUtils::Subtract<TContainerType>, py::arg((lower_prefix + "_mask_1_expression").c_str()), py::arg((lower_prefix + "_mask_2_expression").c_str()), py::arg("required_minimum_redundancy") = 1);
+    m.def("Scale", &MaskUtils::Scale<TContainerType>, py::arg((lower_prefix + "_scalar_expression").c_str()), py::arg((lower_prefix + "_mask_expression").c_str()), py::arg("required_minimum_redundancy") = 1);
+    m.def("ClusterMasks", &MaskUtils::ClusterMasks<TContainerType>, py::arg(("list_of_" + lower_prefix + "_mask_expressions").c_str()), py::arg("required_minimum_redundancy") = 1);
+    m.def("GetMasksDividingReferenceMask", &MaskUtils::GetMasksDividingReferenceMask<TContainerType>, py::arg(("reference_" + lower_prefix + "_mask_expression").c_str()), py::arg(("list_of_" + lower_prefix + "_mask_expressions").c_str()), py::arg("required_minimum_redundancy") = 1);
+}
+
 void AddCustomUtilitiesToPython(pybind11::module& m)
 {
     namespace py = pybind11;
@@ -59,6 +89,11 @@ void AddCustomUtilitiesToPython(pybind11::module& m)
     AddSmoothClamper<ModelPart::NodesContainerType>(m, "Node");
     AddSmoothClamper<ModelPart::ConditionsContainerType>(m, "Condition");
     AddSmoothClamper<ModelPart::ElementsContainerType>(m, "Element");
+
+    auto mask_utils = m.def_submodule("MaskUtils");
+    AddMaskUtilsToPython<ModelPart::NodesContainerType>(mask_utils);
+    AddMaskUtilsToPython<ModelPart::ConditionsContainerType>(mask_utils);
+    AddMaskUtilsToPython<ModelPart::ElementsContainerType>(mask_utils);
 
     auto sensor_generator_utils = m.def_submodule("SensorGeneratorUtils");
     sensor_generator_utils.def("IsPointInGeometry", &SensorGeneratorUtils::IsPointInGeometry, py::arg("point"), py::arg("geometry"));
