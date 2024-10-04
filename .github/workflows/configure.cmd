1cd8145a7d3ac033494d9952a4cb90e2a9b4726a
Carlos Roig <roigcarlo@gmail.com>
2024-10-04 12:35:22 +0200

    Try with system zlib?

--- .github/workflows/configure.cmd
+++ .github/workflows/configure.cmd
@@ -49,7 +49,6 @@ cmake                                                 ^
   -DBOOST_ROOT="%TEMP%\boost"                         ^
   -DCMAKE_CXX_FLAGS="/Od /we4661 /we4804 /WX /wd4996" ^
   -DKRATOS_BUILD_TESTING=OFF                          ^
-  -DFORCE_LOCAL_ZLIB_COMPILATION=ON                   ^
   -DCMAKE_UNITY_BUILD=ON                                    || goto :error
 
 cmake --build "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%" --target all_build -- /property:configuration=%KRATOS_BUILD_TYPE% /p:Platform=x64 || goto :error
