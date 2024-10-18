8e4bfe24ad1ff07be8bcc7489a1904353bc5e507
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2024-10-18 14:54:15 +0200

    First unit tests on create and doflist added.

---
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_geo_steady_state_piping_element.cpp
@@ -0,0 +1,137 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+//  Main authors:    Wijtze Pieter Kikstra
+//
+
+#include "custom_elements/geo_steady_state_Pw_piping_element.h"
+#include "tests/cpp_tests/geo_mechanics_fast_suite.h"
+#include "tests/cpp_tests/test_utilities.h"
+
+// #include <boost/numeric/ublas/assignment.hpp>
+// #include <cstddef>
+
+namespace
+{
+
+using namespace Kratos;
+
+PointerVector<Node> CreateNodes()
+{
+    PointerVector<Node> result;
+    result.push_back(Kratos::make_intrusive<Node>(1, 0.0, 0.0, 0.0));
+    result.push_back(Kratos::make_intrusive<Node>(2, 1.0, 0.0, 0.0));
+    return result;
+}
+
+ModelPart& CreateModelPartWithWaterPressureVariable(Model& rModel)
+{
+    auto& r_result = rModel.CreateModelPart("Main");
+    r_result.AddNodalSolutionStepVariable(WATER_PRESSURE);
+
+    return r_result;
+}
+
+GeoSteadyStatePwPipingElement<2, 2> CreateGeoSteadyStatePwPipingElementWithPWDofs(
+    const Properties::Pointer& rProperties, const Geometry<Node>::Pointer& rGeometry)
+{
+    auto result = GeoSteadyStatePwPipingElement<2, 2>{1, rGeometry, rProperties};
+    for (auto& node : result.GetGeometry()) {
+        node.AddDof(WATER_PRESSURE);
+    }
+
+    return result;
+}
+
+GeoSteadyStatePwPipingElement<2, 2> CreateHorizontalUnitLengthGeoSteadyStatePwPipingElementWithPWDofs(
+    Model& rModel, const Properties::Pointer& rProperties)
+{
+    auto& r_model_part = CreateModelPartWithWaterPressureVariable(rModel);
+
+    PointerVector<Node> nodes;
+    nodes.push_back(r_model_part.CreateNewNode(0, 0.0, 0.0, 0.0));
+    nodes.push_back(r_model_part.CreateNewNode(1, 1.0, 0.0, 0.0));
+    const auto p_geometry = std::make_shared<Line2D2<Node>>(nodes);
+    return CreateGeoSteadyStatePwPipingElementWithPWDofs(rProperties, p_geometry);
+}
+
+} // namespace
+
+namespace Kratos::Testing
+{
+
+using namespace Kratos;
+
+KRATOS_TEST_CASE_IN_SUITE(GeoSteadyStatePwPipingElementIsAnElement, KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    const GeoSteadyStatePwPipingElement<2, 2> element;
+    auto p_casted_element = dynamic_cast<const Element*>(&element);
+    KRATOS_CHECK_NOT_EQUAL(p_casted_element, nullptr);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(GeoSteadyStatePwPipingElementCanCreateInstanceWithGeometryInput,
+                          KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    // Arrange
+    const GeoSteadyStatePwPipingElement<2, 2> element;
+    const auto p_geometry   = std::make_shared<Line2D2<Node>>(CreateNodes());
+    const auto p_properties = std::make_shared<Properties>();
+
+    // Act
+    const auto p_created_element = element.Create(1, p_geometry, p_properties);
+
+    // Assert
+    EXPECT_NE(p_created_element, nullptr);
+    EXPECT_EQ(p_created_element->Id(), 1);
+    EXPECT_NE(p_created_element->pGetGeometry(), nullptr);
+    EXPECT_NE(p_created_element->pGetProperties(), nullptr);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(GeoSteadyStatePwPipingElementCanCreateInstanceWithNodeInput, KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    // Arrange
+    const auto nodes        = CreateNodes();
+    const auto p_properties = std::make_shared<Properties>();
+
+    // The source element needs to have a geometry, otherwise the version of the
+    // Create method with a node input will fail.
+    const auto p_geometry = std::make_shared<Line2D2<Node>>(CreateNodes());
+    const GeoSteadyStatePwPipingElement<2, 2> element(0, p_geometry, p_properties);
+
+    // Act
+    const auto p_created_element = element.Create(1, nodes, p_properties);
+
+    // Assert
+    EXPECT_NE(p_created_element, nullptr);
+    EXPECT_EQ(p_created_element->Id(), 1);
+    EXPECT_NE(p_created_element->pGetGeometry(), nullptr);
+    EXPECT_NE(p_created_element->pGetProperties(), nullptr);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(GeoSteadyStatePwPipingElementReturnsTheExpectedDoFList, KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    // Arrange
+    const auto p_properties = std::make_shared<Properties>();
+
+    Model model;
+    const auto element = CreateHorizontalUnitLengthGeoSteadyStatePwPipingElementWithPWDofs(model, p_properties);
+
+    // Act
+    const auto              dummy_process_info = ProcessInfo{};
+    Element::DofsVectorType degrees_of_freedom;
+    element.GetDofList(degrees_of_freedom, dummy_process_info);
+
+    // Assert
+    KRATOS_EXPECT_EQ(degrees_of_freedom.size(), 2);
+    for (auto dof : degrees_of_freedom) {
+        KRATOS_EXPECT_EQ(dof->GetVariable(), WATER_PRESSURE);
+    }
+}
+
+} // namespace Kratos::Testing
