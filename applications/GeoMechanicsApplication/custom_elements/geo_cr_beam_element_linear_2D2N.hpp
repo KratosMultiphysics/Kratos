944b570ad77f8074cf2e540b11ace3813fca1ca1
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2024-11-03 15:39:57 +0100

    minor in GeomechanicsApp

--- applications/GeoMechanicsApplication/custom_elements/geo_cr_beam_element_linear_2D2N.hpp
+++ applications/GeoMechanicsApplication/custom_elements/geo_cr_beam_element_linear_2D2N.hpp
@@ -18,7 +18,7 @@
 // External includes
 
 // Project includes
-#include "../StructuralMechanicsApplication/custom_elements/cr_beam_element_linear_2D2N.hpp"
+#include "../StructuralMechanicsApplication/custom_elements/beam_elements/cr_beam_element_linear_2D2N.hpp"
 #include "includes/define.h"
 #include "includes/serializer.h"
 #include "includes/variables.h"
