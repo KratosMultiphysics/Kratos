eab33441cadef96a667ccb2f12e677ea001d1a85
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-11-10 17:20:25 +0100

    Avoid hiding members `GetValue` and `SetValue` in class `SmallStrainUMAT2DPlaneStrainLaw`

--- applications/GeoMechanicsApplication/custom_constitutive/small_strain_umat_2D_plane_strain_law.hpp
+++ applications/GeoMechanicsApplication/custom_constitutive/small_strain_umat_2D_plane_strain_law.hpp
@@ -73,8 +73,10 @@ public:
     ConstitutiveLaw::Pointer Clone() const override;
 
     Vector& GetValue(const Variable<Vector>& rThisVariable, Vector& rValue) override;
+    using SmallStrainUMAT3DLaw::GetValue;
 
     void SetValue(const Variable<Vector>& rVariable, const Vector& rValue, const ProcessInfo& rCurrentProcessInfo) override;
+    using SmallStrainUMAT3DLaw::SetValue;
 
     /**
      * @brief Dimension of the law:
