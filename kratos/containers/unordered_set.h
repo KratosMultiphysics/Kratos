ef32496631346b2b3e80101db4ec97efd1bc8f81
Vicente Mataix Ferrándiz <vmataix@altair.com>
2024-11-19 10:20:17 +0100

    [Core] Adding hashed containers (currently STL)

---
+++ kratos/containers/unordered_set.h
@@ -0,0 +1,29 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+#pragma once
+
+// System includes
+#include <unordered_set>
+// TODO: Replace with more suited and advanced implementations
+
+// External includes
+
+// Project includes
+
+namespace Kratos {
+
+template<class T1, class T2>
+using unordered_set = std::unordered_set<T1, T2>;
+
+
+} // namespace Kratos
