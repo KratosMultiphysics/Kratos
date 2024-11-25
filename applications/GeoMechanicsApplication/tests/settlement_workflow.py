022daac31a27399e1f86e1f1feef6fe43ffd1064
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-11-25 16:40:32 +0100

    Added more checks for the C++ route

--- applications/GeoMechanicsApplication/tests/settlement_workflow.py
+++ applications/GeoMechanicsApplication/tests/settlement_workflow.py
@@ -114,9 +114,27 @@ class KratosGeoMechanicsSettlementWorkflowCppRoute(KratosGeoMechanicsSettlementW
 
     def test_d_settlement_workflow(self):
         noop = lambda *args, **kwargs: None
+
+        times_to_check = [1.0, 2.0, 3.0, 3.2]
+        node_ids = [1, 102, 1085, 1442]
+        reader = test_helper.GiDOutputFileReader()
         for i in range(self.number_of_stages):
             status = self.settlement_api.RunStage(self.test_path, self.project_parameters_filenames[i], noop, noop, noop, noop)
             self.assertEqual(status, 0)
+
+            result_file_name = os.path.join(self.test_path, f'test_model_stage{i+1}.post.res')
+            expected_result_file_name = os.path.join(self.test_root, f'test_model_stage{i+1}.post.orig.res')
+
+            actual_data = reader.read_output_from(result_file_name)
+            actual_nodal_values = reader.nodal_values_at_time("DISPLACEMENT", times_to_check[i], actual_data, node_ids)
+
+            expected_data = reader.read_output_from(expected_result_file_name)
+            expected_nodal_values = reader.nodal_values_at_time(
+                "DISPLACEMENT", times_to_check[i], expected_data, node_ids)
+
+            self.assertEqual(len(actual_nodal_values), len(expected_nodal_values))
+            for actual_displacement, expected_displacement in zip(actual_nodal_values, expected_nodal_values):
+                self.assertVectorAlmostEqual(actual_displacement, expected_displacement, 3)
 
 
 if __name__ == '__main__':
