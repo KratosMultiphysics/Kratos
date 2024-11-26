f045bdb4d0fdbdffad6310d005c96c71d96d74aa
Vicente Mataix Ferrándiz <vmataix@altair.com>
2024-11-26 13:18:49 +0100

    Initial commit

---
+++ kratos/includes/system_information.h
@@ -0,0 +1,81 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+
+namespace Kratos
+{
+///@addtogroup KratosCore
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @class SystemInformation
+ * @ingroup KratosCore
+ * @brief This class provides
+ * @author Vicente Mataix Ferrandiz
+ */
+class KRATOS_API(KRATOS_CORE) SystemInformation 
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of SystemInformation
+    KRATOS_CLASS_POINTER_DEFINITION(SystemInformation);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+    SystemInformation()
+    {}
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override
+    {
+        std::stringstream buffer;
+        buffer << "SystemInformation" ;
+
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override {rOStream << "SystemInformation";}
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override {rOStream << "SystemInformation class";}
+
+    ///@}
+}; // class
+///@}
+
+///@} addtogroup block
+
+} // namespace Kratos
---
+++ kratos/sources/system_information.cpp
@@ -0,0 +1,23 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/system_information.h"
+
+namespace Kratos
+{
+
+} // namespace Kratos
