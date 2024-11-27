cf017bf3d739f0a186d9b2b5be403ee03483602f
Alejandro <alejandro.cornejo23@gmail.com>
2024-11-27 14:10:26 +0100

    building a robust Frenet Serret matrix

--- applications/StructuralMechanicsApplication/custom_elements/truss_elements/linear_truss_element_3D.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/truss_elements/linear_truss_element_3D.cpp
@@ -212,15 +212,62 @@ void LinearTrussElement3D<TNNodes>::GetFirstDerivativesShapeFunctionsValues(
 /***********************************************************************************/
 
 template<SizeType TNNodes>
+BoundedMatrix<double, 3, 3> LinearTrussElement3D<TNNodes>::GetFrenetSerretMatrix() const
+{
+    const auto &r_geom = GetGeometry();
+    BoundedMatrix<double, 3, 3> T;
+    T.clear();
+
+    array_1d<double, 3> t;
+    array_1d<double, 3> n;
+    array_1d<double, 3> m;
+
+    // t is the axis of the truss
+    noalias(t) = r_geom[1].GetInitialPosition() - r_geom[0].GetInitialPosition();
+    t /= norm_2(t);
+
+    n.clear();
+    n[1] = 1.0;
+
+    if (norm_2(t-n) <= 1.0e-8) { // colineal, hence we use another aux vector
+        n.clear();
+        n[2] = 1.0;
+    }
+
+    // Gram-Schmidt ortogonalization
+    n = n - inner_prod(t, n) / inner_prod(t, t) * t;
+    n /= norm_2(n);
+
+    noalias(m) = MathUtils<double>::CrossProduct(t, n);
+
+    T(0, 0) = t[0];
+    T(0, 1) = t[1];
+    T(0, 2) = t[2];
+
+    T(1, 0) = n[0];
+    T(1, 1) = n[1];
+    T(1, 2) = n[2];
+
+    T(2, 0) = m[0];
+    T(2, 1) = m[1];
+    T(2, 2) = m[2];
+
+    return T;
+}
+
+/***********************************************************************************/
+/***********************************************************************************/
+
+template<SizeType TNNodes>
 void LinearTrussElement3D<TNNodes>::GetNodalValuesVector(SystemSizeBoundedArrayType& rNodalValues) const
 {
-    // if (rNodalValues.size() != SystemSize)
-    //     rNodalValues.resize(SystemSize, false);
-    // const auto &r_geom = GetGeometry();
+    if (rNodalValues.size() != SystemSize)
+        rNodalValues.resize(SystemSize, false);
+    const auto &r_geom = GetGeometry();
 
     // const double angle = GetAngle();
 
-    // BoundedVector<double, SystemSize> global_values;
+    BoundedVector<double, SystemSize> global_values;
 
     // // We fill the vector with global values
     // for (SizeType i = 0; i < NNodes; ++i) {
