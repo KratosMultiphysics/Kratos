2f775344dfa8ea1e1a7acc4439e4ffaca6dc6adf
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2024-11-29 10:19:15 +0100

    Correct database

--- applications/FluidDynamicsApplication/custom_utilities/weakly_compressible_navier_stokes_data.h
+++ applications/FluidDynamicsApplication/custom_utilities/weakly_compressible_navier_stokes_data.h
@@ -101,8 +101,8 @@ void Initialize(const Element& rElement, const ProcessInfo& rProcessInfo) overri
     this->FillFromHistoricalNodalData(Density,DENSITY,r_geometry);
     this->FillFromHistoricalNodalData(Pressure_OldStep1,PRESSURE,r_geometry,1);
     this->FillFromHistoricalNodalData(Pressure_OldStep2,PRESSURE,r_geometry,2);
+    this->FillFromHistoricalNodalData(SolidFractionVelocity, SOLID_FRACTION_VELOCITY, r_geometry);
     this->FillFromNonHistoricalNodalData(SoundVelocity, SOUND_VELOCITY, r_geometry);
-    this->FillFromNonHistoricalNodalData(SolidFractionVelocity, SOLID_FRACTION_VELOCITY, r_geometry);
     this->FillFromProperties(DynamicViscosity,DYNAMIC_VISCOSITY,r_properties); //TODO: This needs to be retrieved from the EffectiveViscosity of the constitutive law
     this->FillFromProcessInfo(DeltaTime,DELTA_TIME,rProcessInfo);
     this->FillFromProcessInfo(DynamicTau,DYNAMIC_TAU,rProcessInfo);
