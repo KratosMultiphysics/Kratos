005739ecfa067ded579c9fc2a3e0a02e9cc2d300
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2024-12-02 13:52:02 +0100

    Extracted two local helpers for the unit test
    This improves the readability of the unit test.

--- applications/StructuralMechanicsApplication/tests/cpp_tests/test_timoshenko_beam_elastic_constitutive_law.cpp
+++ applications/StructuralMechanicsApplication/tests/cpp_tests/test_timoshenko_beam_elastic_constitutive_law.cpp
@@ -12,48 +12,64 @@
 #include "structural_mechanics_fast_suite.h"
 #include "custom_constitutive/timoshenko_beam_elastic_constitutive_law.h"
 
+namespace
+{
+using namespace Kratos;
+
+Vector MakeStrainVectorForTesting() {
+    constexpr auto number_of_strain_components = 3;
+    auto result = Vector{number_of_strain_components};
+    result[0] = 2.0e-3; // axial strain
+    result[1] = 3.0e-2; // curvature
+    result[2] = 4.0e-3; // shear strain
+    return result;
+}
+
+Properties MakePropertiesForTesting() {
+    auto result = Properties{};
+    result[YOUNG_MODULUS] = 1.0e6;
+    result[POISSON_RATIO] = 0.2;
+    result[CROSS_AREA] = 0.2;
+    result[I33] = 0.5;
+    result[AREA_EFFECTIVE_Y] = 0.3;
+    return result;
+}
+
+}
+
 namespace Kratos::Testing
 {
 
 KRATOS_TEST_CASE_IN_SUITE(TimoshenkoBeamElasticConstitutiveLaw_CalculatesCauchyStressVector, KratosStructuralMechanicsFastSuite) {
     TimoshenkoBeamElasticConstitutiveLaw law;
     ConstitutiveLaw::Parameters parameters;
-    constexpr auto axial_strain = 2.0e-3;
-    constexpr auto curvature = 3.0e-2;
-    constexpr auto shear_strain = 4.0e-3;
-    constexpr auto number_of_strain_components = 3;
-    auto strain_vector = Vector{number_of_strain_components};
-    strain_vector[0] = axial_strain;
-    strain_vector[1] = curvature;
-    strain_vector[2] = shear_strain;
+    auto strain_vector = MakeStrainVectorForTesting();
+    const auto number_of_strain_components = strain_vector.size();
+    const auto axial_strain = strain_vector[0];
+    const auto curvature = strain_vector[1];
+    const auto shear_strain = strain_vector[2];
     parameters.SetStrainVector(strain_vector);
     auto stress_vector = Vector{ZeroVector{number_of_strain_components}};
     parameters.SetStressVector(stress_vector);
 
-    Properties properties;
-    constexpr auto youngs_modulus = 1.0e6;
-    properties[YOUNG_MODULUS] = youngs_modulus;
-    constexpr auto poissons_ratio = 0.2;
-    properties[POISSON_RATIO] = poissons_ratio;
-    constexpr auto area = 0.2;
-    properties[CROSS_AREA] = area;
-    constexpr auto moment_of_inertia = 0.5;
-    properties[I33] = moment_of_inertia;
-    constexpr auto effective_shear_area = 0.3;
-    properties[AREA_EFFECTIVE_Y] = effective_shear_area;
+    auto properties = MakePropertiesForTesting();
+    const auto E = properties[YOUNG_MODULUS];
+    const auto nu = properties[POISSON_RATIO];
+    const auto A = properties[CROSS_AREA];
+    const auto I = properties[I33];
+    const auto As = properties[AREA_EFFECTIVE_Y];
     parameters.SetMaterialProperties(properties);
-    const auto shear_modulus = youngs_modulus / (2.0 * (1.0 + poissons_ratio));
+
+    const auto G = E / (2.0 * (1.0 + nu));
 
     parameters.GetOptions().Set(ConstitutiveLaw::COMPUTE_STRESS, true);
 
-    // We'd like to actually call `CalculateMaterialResponsePK2`, but since that requires Gennady's
-    // changes, we'll use `CalculateMaterialResponseCauchy` for now
     law.CalculateMaterialResponseCauchy(parameters);
 
     auto expected_stress_vector = Vector{number_of_strain_components};
-    expected_stress_vector[0] = youngs_modulus * area * axial_strain;
-    expected_stress_vector[1] = youngs_modulus * moment_of_inertia * curvature;
-    expected_stress_vector[2] = shear_modulus * effective_shear_area * shear_strain;
+    expected_stress_vector[0] = E * A * axial_strain;
+    expected_stress_vector[1] = E * I * curvature;
+    expected_stress_vector[2] = G * As * shear_strain;
 
     constexpr auto relative_tolerance = 1.0e-6;
     KRATOS_EXPECT_VECTOR_RELATIVE_NEAR(parameters.GetStressVector(), expected_stress_vector, relative_tolerance);
