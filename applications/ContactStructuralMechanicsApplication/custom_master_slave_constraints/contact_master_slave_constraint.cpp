3fcee4c656b9b228b5f1660b1e889499864f4b92
Suneth Warnakulasuriya <7856520+sunethwarna@users.noreply.github.com>
2024-12-04 08:21:50 +0100

    fix ContactMechanicsApplication

--- applications/ContactStructuralMechanicsApplication/custom_master_slave_constraints/contact_master_slave_constraint.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_master_slave_constraints/contact_master_slave_constraint.cpp
@@ -1,7 +1,7 @@
 // KRATOS    ______            __             __  _____ __                  __                   __
 //          / ____/___  ____  / /_____ ______/ /_/ ___// /________  _______/ /___  ___________ _/ /
-//         / /   / __ \/ __ \/ __/ __ `/ ___/ __/\__ \/ __/ ___/ / / / ___/ __/ / / / ___/ __ `/ / 
-//        / /___/ /_/ / / / / /_/ /_/ / /__/ /_ ___/ / /_/ /  / /_/ / /__/ /_/ /_/ / /  / /_/ / /  
+//         / /   / __ \/ __ \/ __/ __ `/ ___/ __/\__ \/ __/ ___/ / / / ___/ __/ / / / ___/ __ `/ /
+//        / /___/ /_/ / / / / /_/ /_/ / /__/ /_ ___/ / /_/ /  / /_/ / /__/ /_/ /_/ / /  / /_/ / /
 //        \____/\____/_/ /_/\__/\__,_/\___/\__//____/\__/_/   \__,_/\___/\__/\__,_/_/   \__,_/_/  MECHANICS
 //
 //  License:         BSD License
@@ -90,7 +90,7 @@ MasterSlaveConstraint::Pointer ContactMasterSlaveConstraint::Create(
     const VectorType& rConstantVector
     ) const
 {
-    return Kratos::make_shared<ContactMasterSlaveConstraint>(Id, rMasterDofsVector, rSlaveDofsVector, rRelationMatrix, rConstantVector);
+    return Kratos::make_intrusive<ContactMasterSlaveConstraint>(Id, rMasterDofsVector, rSlaveDofsVector, rRelationMatrix, rConstantVector);
 }
 
 /***********************************************************************************/
@@ -106,7 +106,7 @@ MasterSlaveConstraint::Pointer ContactMasterSlaveConstraint::Create(
     const double Constant
     ) const
 {
-    return Kratos::make_shared<ContactMasterSlaveConstraint>(Id, rMasterNode, rMasterVariable, rSlaveNode, rSlaveVariable, Weight, Constant);
+    return Kratos::make_intrusive<ContactMasterSlaveConstraint>(Id, rMasterNode, rMasterVariable, rSlaveNode, rSlaveVariable, Weight, Constant);
 }
 
 /***********************************************************************************/
