3bbae13e10d20a61cedd972569b533ebeb276474
IAntonau <ihar.antonau@tu-bs.de>
2024-12-10 22:37:29 +0100

    First step

---
+++ applications/OptimizationApplication/python_scripts/base_schema.py
@@ -0,0 +1,40 @@
+import pydantic, json
+from pydantic import BaseModel, Field
+from pydantic import ValidationError
+from pydantic.types import PositiveInt, Literal
+from typing import List, Union
+
+from KratosMultiphysics.OptimizationApplication.schema.schema_controls import ThicknessControl, Control
+from KratosMultiphysics.OptimizationApplication.schema.schema_responses import Response, MassResponse, LinearStrainEnergyResponse
+
+
+class ProblemDataSchema(BaseModel):
+        parallel_type: Literal["OpenMP"] = Field("OpenMP", description="Type of parallelism")
+        echo_level: int = Field(0, description="Verbosity level")
+
+class ModelPartsSchema(BaseModel):
+    class Settings(BaseModel):
+        model_part_name: str = Field(..., description="Name of the model part")
+        domain_size: Literal[2, 3] = Field(..., description="Dimension of the model part")
+        input_filename: str = Field(..., description="Name of the input file")
+    settings: Settings
+
+class BaseSchema(BaseModel):
+    problem_data: ProblemDataSchema = Field(..., description="Problem data")
+    model_parts: List[ModelPartsSchema] = Field(..., description="List of model parts")
+    analyses: list = Field(..., description="List of responses")
+    responses: List[Union[Response, MassResponse, LinearStrainEnergyResponse]] = Field(..., description="List of responses")
+    controls: List[Union[ThicknessControl, Control]] = Field(..., description="List of controls")
+    algorithm_settings: dict = Field(..., description="Algorithm settings")
+    processes: dict = Field(..., description="List of processes")
+
+def ValidateJSON(data):
+    try:
+        valid_data = BaseSchema(**data)
+        print(valid_data)
+    except ValidationError as e:
+        print(e)
+
+    return valid_data
+    
+    
