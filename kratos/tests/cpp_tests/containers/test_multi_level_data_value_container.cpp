ce2ae08304ce25e7541dd13b15000c9ce90d6cb7
Daniel Diez <ddiez@altair.com>
2024-12-10 15:52:49 +0100

    adding vector and matrix test

--- kratos/tests/cpp_tests/containers/test_multi_level_data_value_container.cpp
+++ kratos/tests/cpp_tests/containers/test_multi_level_data_value_container.cpp
@@ -33,7 +33,6 @@ KRATOS_TEST_CASE_IN_SUITE(MultiLevelDataValueContainerSetValue, KratosCoreFastSu
     int dummy;
     MultiLevelDataValueContainer container;
     TestAccesor accesor(3, 4);
-    KRATOS_WATCH("Im starting")
     container.SetValue(TEMPERATURE, accesor, {1,2}, 2.0);
     double value = container.GetValue(TEMPERATURE, accesor, {1,2});
     KRATOS_EXPECT_NEAR(value, 2.0, 1e-12);
@@ -45,8 +44,17 @@ KRATOS_TEST_CASE_IN_SUITE(MultiLevelDataValueContainerSetValue, KratosCoreFastSu
     array_1d<double, 3> array = ZeroVector(3);
     array[0] = 1.0; array[1] = 2.0; array[2] = 3.0;
     container.SetValue(VELOCITY, accesor, {1,2}, array);
-    KRATOS_WATCH(container.GetValue(VELOCITY, accesor, {1,2}));
-    KRATOS_WATCH("Im fine")
+    KRATOS_EXPECT_VECTOR_NEAR(container.GetValue(VELOCITY, accesor, {1,2}), array, 1e-12);
+
+    Vector vector = ZeroVector(3);
+    vector[0] = 1.5; vector[1] = 2.5; vector[2] = 3.5;
+    container.SetValue(STRESSES, accesor, {0,1}, vector);
+    KRATOS_EXPECT_VECTOR_NEAR(container.GetValue(STRESSES, accesor, {0,1}), vector, 1e-12);
+
+    Matrix matrix = ZeroMatrix(3, 3);
+    matrix(0,0) = 5.7;
+    container.SetValue(CAUCHY_STRESS_TENSOR, accesor, {0,1}, matrix);
+    KRATOS_EXPECT_MATRIX_NEAR(container.GetValue(CAUCHY_STRESS_TENSOR, accesor, {0,3}), matrix, 1e-12);
 }
 
 }
