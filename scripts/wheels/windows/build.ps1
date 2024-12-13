622a37abcbb405f99ab5f2122ebbfba6f72fb00e
unknown <roigcarlo@gmail.com>
2024-12-13 09:30:23 +0100

    Bump to 10.2

--- scripts/wheels/windows/build.ps1
+++ scripts/wheels/windows/build.ps1
@@ -1,7 +1,7 @@
 param([System.String]$cotire="OFF")
 
 $pythons = "38", "39", "310", "311", "312"
-$env:kratos_version = "10.1.0"
+$env:kratos_version = "10.2.0"
 
 $kratosRoot = "c:\kratos\kratos"
 $env:kratos_root = $kratosRoot
