a9e8219943f62cef8884581b1380fbcf72508e54
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2024-12-18 10:52:17 +0100

    minor

--- kratos/utilities/type_traits.h
+++ kratos/utilities/type_traits.h
@@ -16,6 +16,7 @@
 
 namespace Kratos {
 
+// TODO: Remove the following when we move to C++20 or higher
 template <class T>
 using BaseType = typename std::remove_cv_t<typename std::remove_reference_t<T>>;
 
