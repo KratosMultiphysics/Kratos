311dc4f7426ebb837cadf84faa63454d7f4c2359
markelov <gennady.markelov@deltares.nl>
2024-12-19 16:24:09 +0100

    added CalculateOnIntegrationPoints(Vector,...

--- applications/StructuralMechanicsApplication/custom_elements/beam_elements/timoshenko_curved_beam_element_2D3N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/beam_elements/timoshenko_curved_beam_element_2D3N.cpp
@@ -678,15 +678,8 @@ void LinearTimoshenkoCurvedBeamElement2D3N::CalculateOnIntegrationPoints(
         const auto &r_geometry = GetGeometry();
 
         ConstitutiveLaw::Parameters cl_values(r_geometry, r_props, rProcessInfo);
-        auto &r_cl_options = cl_values.GetOptions();
-        r_cl_options.Set(ConstitutiveLaw::COMPUTE_STRESS             , true);
-        r_cl_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, false);
-
-        // Let's initialize the cl values
         VectorType strain_vector(StrainSize), stress_vector(StrainSize);
-        strain_vector.clear();
-        cl_values.SetStrainVector(strain_vector);
-        cl_values.SetStressVector(stress_vector);
+        StructuralMechanicsElementUtilities::InitializeConstitutiveLawValuesForStressCalculation(cl_values, strain_vector, stress_vector);
 
         GlobalSizeVector nodal_values, B_b, dNu, dN_theta, N_shape, Nu, N_theta, dN_shape;
         BoundedMatrix<double, 2, 2> frenet_serret;
@@ -785,6 +778,68 @@ void LinearTimoshenkoCurvedBeamElement2D3N::CalculateOnIntegrationPoints(
     }
 }
 
+void LinearTimoshenkoCurvedBeamElement2D3N::CalculateOnIntegrationPoints(
+    const Variable<Vector>& rVariable,
+    std::vector<Vector>& rOutput,
+    const ProcessInfo& rProcessInfo
+    )
+{
+    const auto& r_integration_points = IntegrationPoints(GetIntegrationMethod());
+    rOutput.resize(r_integration_points.size());
+    const auto &r_props = GetProperties();
+
+    if (rVariable == PK2_STRESS_VECTOR) {
+        const auto &r_geometry = GetGeometry();
+
+        ConstitutiveLaw::Parameters cl_values(r_geometry, r_props, rProcessInfo);
+        VectorType strain_vector(StrainSize), stress_vector(StrainSize);
+        StructuralMechanicsElementUtilities::InitializeConstitutiveLawValuesForStressCalculation(cl_values, strain_vector, stress_vector);
+
+        GlobalSizeVector nodal_values, B_b, dNu, dN_theta, N_shape, Nu, N_theta, dN_shape;
+        BoundedMatrix<double, 2, 2> frenet_serret;
+        BoundedVector<double, 2> N_forces, Gamma; // axial and shear forces, strains
+        BoundedMatrix<double, 2, 9> B_s, aux_B_s;
+
+        // Loop over the integration points
+        for (SizeType IP = 0; IP < r_integration_points.size(); ++IP) {
+            const double xi     = r_integration_points[IP].X();
+            const double J      = GetJacobian(xi);
+
+            GetNodalValuesVector(nodal_values);
+
+            const array_3 shape_functions   = GetShapeFunctionsValues(xi);
+            const array_3 d_shape_functions = GetFirstDerivativesShapeFunctionsValues(xi, J);
+
+            GetShapeFunctionsValuesGlobalVectors(shape_functions, N_shape, Nu, N_theta);
+            GetShapeFunctionsValuesGlobalVectors(d_shape_functions, dN_shape, dNu, dN_theta);
+
+            array_3 t, n;
+            GetTangentandTransverseUnitVectors(xi, t, n);
+            noalias(frenet_serret) = GetFrenetSerretMatrix(xi, t, n);
+            noalias(B_b) =  dN_theta;
+
+            // we fill aux_B_s
+            for (IndexType i = 0; i < SystemSize; ++i) {
+                aux_B_s(0, i) = dNu[i] + t[1] * N_theta[i];
+                aux_B_s(1, i) = dN_shape[i] - t[0] * N_theta[i];
+            }
+            noalias(B_s) = prod(frenet_serret, aux_B_s);
+
+            noalias(Gamma) = prod(B_s, nodal_values);
+            strain_vector[0] = Gamma[0]; // axial strain
+            strain_vector[2] = Gamma[1]; // shear strain
+            strain_vector[1] = inner_prod(B_b, nodal_values); // curvature
+
+            mConstitutiveLawVector[IP]->CalculateMaterialResponseCauchy(cl_values);
+            const Vector &r_generalized_stresses = cl_values.GetStressVector();
+
+            rOutput[IP] = r_generalized_stresses;
+            if ( this->GetProperties().Has(BEAM_PRESTRESS_PK2)) {
+                rOutput[IP] += this->GetProperties()[BEAM_PRESTRESS_PK2];
+            }
+        }
+    }
+}
 /***********************************************************************************/
 /***********************************************************************************/
 
