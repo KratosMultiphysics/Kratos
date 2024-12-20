2d676dd16dbee22d4355358a6d2bad7fb27ddd2c
WPK4FEM <129858139+WPK4FEM@users.noreply.github.com>
2024-12-20 15:33:43 +0100

    Geo/remove old K0 materials (#12962)
    * Replaced old K0 laws with linear elasticity
    * GeoLinearElastic3DLaw -> GeoIncrementalLinearElastic3DLaw, registration K0 Laws removed.
    * Removal old K0 constitutive laws
    * redundant material parameters removed

--- applications/GeoMechanicsApplication/geo_mechanics_application.cpp
+++ applications/GeoMechanicsApplication/geo_mechanics_application.cpp
@@ -350,8 +350,6 @@ void KratosGeoMechanicsApplication::Register()
     KRATOS_REGISTER_CONSTITUTIVE_LAW("BilinearCohesive3DLaw", mBilinearCohesive3DLaw)
     KRATOS_REGISTER_CONSTITUTIVE_LAW("BilinearCohesive2DLaw", mBilinearCohesive2DLaw)
 
-    KRATOS_REGISTER_CONSTITUTIVE_LAW("LinearElasticPlaneStrainK02DLaw", mLinearPlaneStrainK0Law)
-    KRATOS_REGISTER_CONSTITUTIVE_LAW("LinearElasticK03DLaw", mElasticIsotropicK03DLaw)
     KRATOS_REGISTER_CONSTITUTIVE_LAW("GeoLinearElasticPlaneStrain2DLaw", mLinearElasticPlaneStrain2DLaw)
     KRATOS_REGISTER_CONSTITUTIVE_LAW("GeoIncrementalLinearElastic3DLaw", mLinearElastic3DLaw)
 
