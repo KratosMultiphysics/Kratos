acc07f58af145688c881ef32b7c499a38d389e04
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2025-01-03 12:05:21 +0100

    Use a non zero material properties Id.

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_constitutive/test_linear_elastic_plane_strain_2D_law.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_constitutive/test_linear_elastic_plane_strain_2D_law.cpp
@@ -13,7 +13,6 @@
 #include "custom_constitutive/incremental_linear_elastic_law.h"
 #include "custom_constitutive/plane_strain.h"
 #include "tests/cpp_tests/geo_mechanics_fast_suite.h"
-#include <tests/cpp_tests/geometries/test_geometry.h>
 
 #include <boost/numeric/ublas/assignment.hpp>
 
@@ -205,25 +204,25 @@ KRATOS_TEST_CASE_IN_SUITE(GeoLinearElasticPlaneStrain2DLawChecksYoungModulusAndP
 {
     auto                        law = CreateLinearElasticPlaneStrainLaw();
     ConstitutiveLaw::Parameters parameters;
-    Properties                  properties;
+    Properties                  properties(3);
     parameters.SetMaterialProperties(properties);
-    ProcessInfo  process_info;
-    GeometryType element_geometry;
+    const auto element_geometry = Geometry<Node>{};
+    const auto process_info     = ProcessInfo{};
     KRATOS_EXPECT_EXCEPTION_IS_THROWN(
         law.Check(properties, element_geometry, process_info),
-        "Error: YOUNG_MODULUS is not available in the parameters of material 0.")
+        "Error: YOUNG_MODULUS is not available in the parameters of material 3.")
     properties.SetValue(YOUNG_MODULUS, -1.0e7);
     KRATOS_EXPECT_EXCEPTION_IS_THROWN(
         law.Check(properties, element_geometry, process_info),
-        "Error: The value of YOUNG_MODULUS (-1e+07) should be positive in material 0.")
+        "Error: The value of YOUNG_MODULUS (-1e+07) should be positive in material 3.")
     properties.SetValue(YOUNG_MODULUS, 1.0e7);
     KRATOS_EXPECT_EXCEPTION_IS_THROWN(
         law.Check(properties, element_geometry, process_info),
-        "Error: POISSON_RATIO is not available in the parameters of material 0.")
+        "Error: POISSON_RATIO is not available in the parameters of material 3.")
     properties.SetValue(POISSON_RATIO, 0.7);
     KRATOS_EXPECT_EXCEPTION_IS_THROWN(
         law.Check(properties, element_geometry, process_info),
-        "Error: The value of POISSON_RATIO (0.7) should be in the range [-1.0, 0.5> in material 0.")
+        "Error: The value of POISSON_RATIO (0.7) should be in the range [-1.0, 0.5> in material 3.")
     properties.SetValue(POISSON_RATIO, 0.25);
     KRATOS_EXPECT_EQ(law.Check(properties, element_geometry, process_info), 0);
 }
