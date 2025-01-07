513772ff19a40db702b65f5cd3e1ecb27134bde0
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-01-07 17:16:00 +0100

    Processed Wijtze Pieter's review suggestions
    - Use a helper function for comparing two lists of numbers.
    - Also include the `"WATER_PRESSURE"` in the master-slave constraints (since we use them with U-Pw elements).
    - Deactivated the $`z`$ component of the prescribed displacements (since we have a plane strain model).

--- applications/GeoMechanicsApplication/tests/test_master_slave_constraints.py
+++ applications/GeoMechanicsApplication/tests/test_master_slave_constraints.py
@@ -8,16 +8,19 @@ class KratosGeoMechanicsMasterSlaveConstraints(KratosUnittest.TestCase):
     def check_x_displacements(self, output_data):
         end_time = 1.0
         loaded_node_ids = [6, 7]
-        actual_displacements = test_helper.GiDOutputFileReader.nodal_values_at_time(
-            "DISPLACEMENT", end_time, output_data, node_ids=loaded_node_ids
-        )
-        expected_x_displacements = [2.0e-2, 2.0e-2]
-        for actual_displacement, expected_x_displacement in zip(
-            actual_displacements, expected_x_displacements
-        ):
-            self.assertAlmostEqual(
-                actual_displacement[0], expected_x_displacement, places=3
+        actual_x_displacements = [
+            displacement_vector[0]
+            for displacement_vector in test_helper.GiDOutputFileReader.nodal_values_at_time(
+                "DISPLACEMENT", end_time, output_data, node_ids=loaded_node_ids
             )
+        ]
+        expected_x_displacements = [2.0e-2, 2.0e-2]
+        self.assertTrue(
+            test_helper.are_iterables_almost_equal(
+                expected_x_displacements, actual_x_displacements
+            ),
+            msg=f"x displacements don't compare equal: expected = {expected_x_displacements}, actual = {actual_x_displacements}",
+        )
 
     def check_displacements_at_tied_nodes(self, output_data):
         end_time = 1.0
@@ -41,14 +44,19 @@ class KratosGeoMechanicsMasterSlaveConstraints(KratosUnittest.TestCase):
     def check_x_reaction_forces(self, output_data):
         end_time = 1.0
         supported_node_ids = [1, 4]
-        actual_reaction_forces = test_helper.GiDOutputFileReader.nodal_values_at_time(
-            "REACTION", end_time, output_data, node_ids=supported_node_ids
-        )
+        actual_x_reaction_forces = [
+            reaction_force_vector[0]
+            for reaction_force_vector in test_helper.GiDOutputFileReader.nodal_values_at_time(
+                "REACTION", end_time, output_data, node_ids=supported_node_ids
+            )
+        ]
         expected_x_reaction_forces = [-5000.0, -5000.0]
-        for actual_reaction_force, expected_x_reaction_force in zip(
-            actual_reaction_forces, expected_x_reaction_forces
-        ):
-            self.assertAlmostEqual(actual_reaction_force[0], expected_x_reaction_force)
+        self.assertTrue(
+            test_helper.are_iterables_almost_equal(
+                expected_x_reaction_forces, actual_x_reaction_forces
+            ),
+            msg=f"x reaction forces don't compare equal: expected = {expected_x_reaction_forces}, actual = {actual_x_reaction_forces}",
+        )
 
     def test_equal_displacements_constraints_at_middle_section(self):
         test_files_path = test_helper.get_file_path("test_master_slave_constraints")
