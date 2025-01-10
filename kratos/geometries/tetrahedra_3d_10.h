21f30e58ff9e6f616a76a4325a22b10b65b16ce7
AniolSala <aniol.sala.95@gmail.com>
2025-01-10 16:09:40 +0100

    Add second derivatives to 3D10N element

--- kratos/geometries/tetrahedra_3d_10.h
+++ kratos/geometries/tetrahedra_3d_10.h
@@ -145,6 +145,10 @@ public:
     /// type as its result.
     using ShapeFunctionsGradientsType = typename BaseType::ShapeFunctionsGradientsType;
 
+    /// A third order tensor to hold shape functions' local second derivatives.
+    /// ShapeFunctionsSecondDerivatives function return this type as its result.
+    using ShapeFunctionsSecondDerivativesType = typename BaseType::ShapeFunctionsSecondDerivativesType;
+
     /// Type of the normal vector used for normal to edges in geometry.
     using NormalType = typename BaseType::NormalType;
 
@@ -742,6 +746,75 @@ public:
         return rResult;
     }
 
+    ShapeFunctionsSecondDerivativesType& ShapeFunctionsSecondDerivatives(
+        ShapeFunctionsSecondDerivativesType& rResult,
+        const CoordinatesArrayType& rPoint) const override
+    {
+        // Check and resize results container
+        if (rResult.size() != this->PointsNumber()) {
+            rResult.resize(this->PointsNumber(), false);
+        }
+
+        for (IndexType i = 0; i < this->PointsNumber(); ++i) {
+            auto& r_DDN_i = rResult[i];
+            if (r_DDN_i.size1() != 3 || r_DDN_i.size2() != 3) {
+                r_DDN_i.resize(3,3, false);
+            }
+        }
+
+        // Node 0
+        rResult[0](0,0) = 4.0; rResult[0](0,1) = 4.0; rResult[0](0,2) = 4.0;
+        rResult[0](1,0) = 4.0; rResult[0](1,1) = 4.0; rResult[0](1,2) = 4.0;
+        rResult[0](2,0) = 4.0; rResult[0](2,1) = 4.0; rResult[0](2,2) = 4.0;
+
+        // Node 1
+        rResult[1](0,0) = 4.0; rResult[1](0,1) = 0.0; rResult[1](0,2) = 0.0;
+        rResult[1](1,0) = 0.0; rResult[1](1,1) = 0.0; rResult[1](1,2) = 0.0;
+        rResult[1](2,0) = 0.0; rResult[1](2,1) = 0.0; rResult[1](2,2) = 0.0;
+
+        // Node 2
+        rResult[2](0,0) = 0.0; rResult[2](0,1) = 0.0; rResult[2](0,2) = 0.0;
+        rResult[2](1,0) = 0.0; rResult[2](1,1) = 4.0; rResult[2](1,2) = 0.0;
+        rResult[2](2,0) = 0.0; rResult[2](2,1) = 0.0; rResult[2](2,2) = 0.0;
+
+        // Node 3
+        rResult[3](0,0) = 0.0; rResult[3](0,1) = 0.0; rResult[3](0,2) = 0.0;
+        rResult[3](1,0) = 0.0; rResult[3](1,1) = 0.0; rResult[3](1,2) = 0.0;
+        rResult[3](2,0) = 0.0; rResult[3](2,1) = 0.0; rResult[3](2,2) = 4.0;
+
+        // Node 4
+        rResult[4](0,0) = -8.0; rResult[4](0,1) = -4.0; rResult[4](0,2) = -4.0;
+        rResult[4](1,0) = -4.0; rResult[4](1,1) = 0.0; rResult[4](1,2) = 0.0;
+        rResult[4](2,0) = -4.0; rResult[4](2,1) = 0.0; rResult[4](2,2) = 0.0;
+
+        // Node 5
+        rResult[5](0,0) = 0.0; rResult[5](0,1) = 4.0; rResult[5](0,2) = 0.0;
+        rResult[5](1,0) = 4.0; rResult[5](1,1) = 0.0; rResult[5](1,2) = 0.0;
+        rResult[5](2,0) = 0.0; rResult[5](2,1) = 0.0; rResult[5](2,2) = 0.0;
+
+        // Node 6
+        rResult[6](0,0) = 0.0; rResult[6](0,1) = -4.0; rResult[6](0,2) = 0.0;
+        rResult[6](1,0) = -4.0; rResult[6](1,1) = -8.0; rResult[6](1,2) = -4.0;
+        rResult[6](2,0) = 0.0; rResult[6](2,1) = -4.0; rResult[6](2,2) = 0.0;
+
+        // Node 7
+        rResult[7](0,0) = 0.0; rResult[7](0,1) = 0.0; rResult[7](0,2) = -4.0;
+        rResult[7](1,0) = 0.0; rResult[7](1,1) = 0.0; rResult[7](1,2) = -4.0;
+        rResult[7](2,0) = -4.0; rResult[7](2,1) = -4.0; rResult[7](2,2) = -8.0;
+
+        // Node 8
+        rResult[8](0,0) = 0.0; rResult[8](0,1) = 0.0; rResult[8](0,2) = 4.0;
+        rResult[8](1,0) = 0.0; rResult[8](1,1) = 0.0; rResult[8](1,2) = 0.0;
+        rResult[8](2,0) = 4.0; rResult[8](2,1) = 0.0; rResult[8](2,2) = 0.0;
+
+        // Node 9
+        rResult[9](0,0) = 0.0; rResult[9](0,1) = 0.0; rResult[9](0,2) = 0.0;
+        rResult[9](1,0) = 0.0; rResult[9](1,1) = 0.0; rResult[9](1,2) = 4.0;
+        rResult[9](2,0) = 0.0; rResult[9](2,1) = 4.0; rResult[9](2,2) = 0.0;
+
+        return rResult;
+    }
+
     /** Tests the intersection of the geometry with
      * a 3D box defined by rLowPoint and rHighPoint.
      * The method is only implemented for simple tets
@@ -754,17 +827,21 @@ public:
     bool HasIntersection(const Point& rLowPoint, const Point& rHighPoint) const override
     {
         // Check if the faces are planar
-        if (this->FacesArePlanar()) {
-            // TODO: Move implementation to GeometryUtils to avoid creating a new tetrahedra
-            return Tetrahedra3D4<TPointType>(
-                this->pGetPoint(0),
-                this->pGetPoint(1),
-                this->pGetPoint(2),
-                this->pGetPoint(3)).HasIntersection(rLowPoint, rHighPoint);
-        } else {
-             KRATOS_ERROR << "\"HasIntersection\" is not implemented for non-planar 10 noded tetrahedra.";
-        }
-        return false;
+        // if (this->FacesArePlanar()) {
+        //     // TODO: Move implementation to GeometryUtils to avoid creating a new tetrahedra
+        //     return Tetrahedra3D4<TPointType>(
+        //         this->pGetPoint(0),
+        //         this->pGetPoint(1),
+        //         this->pGetPoint(2),
+        //         this->pGetPoint(3)).HasIntersection(rLowPoint, rHighPoint);
+        // } else {
+        //      KRATOS_ERROR << "\"HasIntersection\" is not implemented for non-planar 10 noded tetrahedra.";
+        // }
+        return Tetrahedra3D4<TPointType>(
+            this->pGetPoint(0),
+            this->pGetPoint(1),
+            this->pGetPoint(2),
+            this->pGetPoint(3)).HasIntersection(rLowPoint, rHighPoint);
     }
 
     ///@}
@@ -1075,8 +1152,14 @@ private:
     bool FacesArePlanar() const
     {
         constexpr double tol = 1e-6;
-        constexpr std::array<std::array<size_t, 3>, 6> edges{
-            {{0, 1, 4}, {1, 2, 5}, {2, 0, 6}, {0, 3, 7}, {1, 3, 8}, {2, 3, 9}}};
+        constexpr std::array<std::array<size_t, 3>, 6> edges{{
+            {0, 1, 4},
+            {1, 2, 5},
+            {2, 0, 6},
+            {0, 3, 7},
+            {1, 3, 8},
+            {2, 3, 9}}
+        };
         const auto& r_points = this->Points();
         for (const auto& r_edge : edges) {
             const double a = MathUtils<double>::Norm3(r_points[r_edge[0]] - r_points[r_edge[1]]);
