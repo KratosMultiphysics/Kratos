4098bb466c94a4d15a9e62e9ca0a7ff04c63f192
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-01-15 11:04:24 +0100

    minor

--- applications/SystemIdentificationApplication/tests/test_system_identification.py
+++ applications/SystemIdentificationApplication/tests/test_system_identification.py
@@ -33,8 +33,8 @@ class TestSystemIdentification(UnitTest.TestCase):
             "output_file_name"      : "auxiliary_files/summary.csv",
             "remove_output_file"    : true,
             "comparison_type"       : "csv_file",
-            "tolerance"             : 1e-8,
-            "relative_tolerance"    : 1e-8,
+            "tolerance"             : 1e-6,
+            "relative_tolerance"    : 1e-6,
             "dimension"             : 3
         }""")
         CompareTwoFilesCheckProcess(params).Execute()
