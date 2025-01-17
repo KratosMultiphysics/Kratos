fd1ed6de7cfcc198eaa5c1ec051fb156007ce4b2
Richard Faasse <richard.faasse@deltares.nl>
2025-01-17 09:33:57 +0100

    Reformatting

--- applications/GeoMechanicsApplication/dgeoflow_app.cpp
+++ applications/GeoMechanicsApplication/dgeoflow_app.cpp
@@ -17,29 +17,30 @@
 
 using namespace Kratos;
 
-
 int main(int argc, char* argv[])
 {
     if (argc < 3) {
-        std::cerr << "Too few arguments: provide at least the working directory and a project parameters file" << std::endl;
+        std::cerr << "Too few arguments: provide at least the working directory and a project "
+                     "parameters file"
+                  << std::endl;
         return 1;
     }
 
     auto workingDirectory = argv[1];
     auto projectFile      = argv[2];
 
-    auto dummy_log_callback = [](const char*){};
-    auto dummy_report_progress = [](double){};
-    auto dummy_report_textual_progress = [](const char*){};
-    auto never_cancel = [](){ return false; };
+    auto dummy_log_callback            = [](const char*) {};
+    auto dummy_report_progress         = [](double) {};
+    auto dummy_report_textual_progress = [](const char*) {};
+    auto never_cancel                  = []() { return false; };
 
     auto                                           execute = KratosExecute();
     const Kratos::KratosExecute::CriticalHeadInfo  critical_head_info(4.0, 6.0, 0.05);
     const Kratos::KratosExecute::CallBackFunctions call_back_functions(
-        dummy_log_callback, dummy_report_progress,dummy_report_textual_progress, never_cancel);
+        dummy_log_callback, dummy_report_progress, dummy_report_textual_progress, never_cancel);
 
-    const int status = execute.ExecuteFlowAnalysis(workingDirectory, projectFile, critical_head_info,
-                                                   "PorousDomain.0", call_back_functions);
+    const int status = execute.ExecuteFlowAnalysis(
+        workingDirectory, projectFile, critical_head_info, "PorousDomain.0", call_back_functions);
 
     return 0;
 }
