2e2c83d8ca572680d479ef76ac022128b75052ce
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-01-19 16:26:34 +0100

    update Sensor interface

--- applications/SystemIdentificationApplication/custom_sensors/sensor.cpp
+++ applications/SystemIdentificationApplication/custom_sensors/sensor.cpp
@@ -26,13 +26,31 @@ namespace Kratos
 
 Sensor::Sensor(
     const std::string& rName,
-    const Point& rLocation,
+    Node::Pointer pNode,
     const double Weight)
     : mName(rName),
-      mLocation(rLocation),
+      mpNode(pNode),
       mWeight(Weight),
       mSensorValue(0.0)
 {
+}
+
+Parameters Sensor::GetSensorParameters() const
+{
+    Parameters parameters = Parameters(R"(
+    {
+        "name"      : "",
+        "value"     : 0.0,
+        "location"  : [0.0, 0.0, 0.0],
+        "weight"    : 0.0
+    })" );
+
+    parameters["name"].SetString(this->GetName());
+    parameters["value"].SetDouble(this->GetSensorValue());
+    parameters["location"].SetVector(this->GetNode()->Coordinates());
+    parameters["weight"].SetDouble(this->GetWeight());
+
+    return parameters;
 }
 
 std::string Sensor::GetName() const
@@ -40,9 +58,9 @@ std::string Sensor::GetName() const
     return mName;
 }
 
-Point Sensor::GetLocation() const
+Node::Pointer Sensor::GetNode() const
 {
-    return mLocation;
+    return mpNode;
 }
 
 double Sensor::GetWeight() const
@@ -60,135 +78,42 @@ void Sensor::SetSensorValue(const double Value)
     mSensorValue = Value;
 }
 
-template<class TContainerType>
 void Sensor::AddContainerExpression(
     const std::string& rExpressionName,
-    typename ContainerExpression<TContainerType>::Pointer pContainerExpression)
+    ContainerExpressionType pContainerExpression)
 {
     KRATOS_TRY
 
-    if constexpr(std::is_same_v<TContainerType, ModelPart::NodesContainerType>) {
-        const auto p_itr = mNodalExpressions.find(rExpressionName);
-        KRATOS_ERROR_IF_NOT(p_itr == mNodalExpressions.end())
-            << "A nodal expression named \"" << rExpressionName << " already exists.";
-        mNodalExpressions[rExpressionName] = pContainerExpression;
-    } else if constexpr(std::is_same_v<TContainerType, ModelPart::ConditionsContainerType>) {
-        const auto p_itr = mConditionExpressions.find(rExpressionName);
-        KRATOS_ERROR_IF_NOT(p_itr == mConditionExpressions.end())
-            << "A condition expression named \"" << rExpressionName << " already exists.";
-        mConditionExpressions[rExpressionName] = pContainerExpression;
-    } else if constexpr(std::is_same_v<TContainerType, ModelPart::ElementsContainerType>) {
-        const auto p_itr = mElementExpressions.find(rExpressionName);
-        KRATOS_ERROR_IF_NOT(p_itr == mElementExpressions.end())
-            << "A element expression named \"" << rExpressionName << " already exists.";
-        mElementExpressions[rExpressionName] = pContainerExpression;
-    } else {
-        static_assert(std::is_same_v<TContainerType, TContainerType>, "Unsupported container type.");
-    }
+    const auto p_itr = mContainerExpressions.find(rExpressionName);
+    KRATOS_ERROR_IF_NOT(p_itr == mContainerExpressions.end())
+        << "A container expression named \"" << rExpressionName << " already exists.";
+    mContainerExpressions[rExpressionName] = pContainerExpression;
 
     KRATOS_CATCH("");
 }
 
-void Sensor::AddNodalExpression(
-    const std::string& rExpressionName,
-    ContainerExpression<ModelPart::NodesContainerType>::Pointer pNodalExpression)
-{
-    AddContainerExpression<ModelPart::NodesContainerType>(rExpressionName, pNodalExpression);
-}
-
-ContainerExpression<ModelPart::NodesContainerType>::Pointer Sensor::GetNodalExpression(const std::string& rExpressionName) const
+Sensor::ContainerExpressionType Sensor::GetContainerExpression(const std::string& rExpressionName) const
 {
     KRATOS_TRY
 
-    const auto p_itr = mNodalExpressions.find(rExpressionName);
+    const auto p_itr = mContainerExpressions.find(rExpressionName);
 
-    KRATOS_ERROR_IF(p_itr == mNodalExpressions.end())
-        << "A nodal expression named \"" << rExpressionName << " not found.";
+    KRATOS_ERROR_IF(p_itr == mContainerExpressions.end())
+        << "A container expression named \"" << rExpressionName << " not found.";
 
     return p_itr->second;
 
     KRATOS_CATCH("");
 }
 
-std::unordered_map<std::string, ContainerExpression<ModelPart::NodesContainerType>::Pointer> Sensor::GetNodalExpressionsMap() const
+std::unordered_map<std::string, Sensor::ContainerExpressionType> Sensor::GetContainerExpressionsMap() const
 {
-    return mNodalExpressions;
+    return mContainerExpressions;
 }
 
-void Sensor::AddConditionExpression(
-    const std::string& rExpressionName,
-    ContainerExpression<ModelPart::ConditionsContainerType>::Pointer pConditionExpression)
+void Sensor::ClearContainerExpressions()
 {
-    AddContainerExpression<ModelPart::ConditionsContainerType>(rExpressionName, pConditionExpression);
-}
-
-ContainerExpression<ModelPart::ConditionsContainerType>::Pointer Sensor::GetConditionExpression(const std::string& rExpressionName) const
-{
-    KRATOS_TRY
-
-    const auto p_itr = mConditionExpressions.find(rExpressionName);
-
-    KRATOS_ERROR_IF(p_itr == mConditionExpressions.end())
-        << "A condition expression named \"" << rExpressionName << " not found.";
-
-    return p_itr->second;
-
-    KRATOS_CATCH("");
-}
-
-std::unordered_map<std::string, ContainerExpression<ModelPart::ConditionsContainerType>::Pointer> Sensor::GetConditionExpressionsMap() const
-{
-    return mConditionExpressions;
-}
-
-void Sensor::AddElementExpression(
-    const std::string& rExpressionName,
-    ContainerExpression<ModelPart::ElementsContainerType>::Pointer pElementExpression)
-{
-    AddContainerExpression<ModelPart::ElementsContainerType>(rExpressionName, pElementExpression);
-}
-
-ContainerExpression<ModelPart::ElementsContainerType>::Pointer Sensor::GetElementExpression(const std::string& rExpressionName) const
-{
-    KRATOS_TRY
-
-    const auto p_itr = mElementExpressions.find(rExpressionName);
-
-    KRATOS_ERROR_IF(p_itr == mElementExpressions.end())
-        << "A condition expression named \"" << rExpressionName << " not found.";
-
-    return p_itr->second;
-
-    KRATOS_CATCH("");
-}
-
-std::unordered_map<std::string, ContainerExpression<ModelPart::ElementsContainerType>::Pointer> Sensor::GetElementExpressionsMap() const
-{
-    return mElementExpressions;
-}
-
-std::vector<std::string> Sensor::GetDataVariableNames() const
-{
-    std::vector<std::string> result;
-    for (auto i_ptr = this->begin(); i_ptr != this->end(); ++i_ptr) {
-        result.push_back(i_ptr->first->Name());
-    }
-    return result;
-}
-
-void Sensor::ClearNodalExpressions()
-{
-    mNodalExpressions.clear();
-}
-
-void Sensor::ClearConditionExpressions()
-{
-    mConditionExpressions.clear();
-}
-
-void Sensor::ClearElementExpressions()
-{
-    mElementExpressions.clear();
+    mContainerExpressions.clear();
 }
 
 std::string Sensor::Info() const
@@ -205,16 +130,10 @@ void Sensor::PrintInfo(std::ostream& rOStream) const
 
 void Sensor::PrintData(std::ostream& rOStream) const
 {
-    PrintInfo(rOStream);
-    rOStream << "    Location: " << this->GetLocation() << std::endl;
     rOStream << "    Value: " << this->GetSensorValue() << std::endl;
     rOStream << "    Weight: " << this->GetWeight() << std::endl;
-    DataValueContainer::PrintData(rOStream);
+    mpNode->PrintInfo(rOStream);
+    mpNode->PrintData(rOStream);
 }
-
-// template instantiations
-template KRATOS_API(SYSTEM_IDENTIFICATION_APPLICATION) void Sensor::AddContainerExpression<ModelPart::NodesContainerType>(const std::string&, typename ContainerExpression<ModelPart::NodesContainerType>::Pointer);
-template KRATOS_API(SYSTEM_IDENTIFICATION_APPLICATION) void Sensor::AddContainerExpression<ModelPart::ConditionsContainerType>(const std::string&, typename ContainerExpression<ModelPart::ConditionsContainerType>::Pointer);
-template KRATOS_API(SYSTEM_IDENTIFICATION_APPLICATION) void Sensor::AddContainerExpression<ModelPart::ElementsContainerType>(const std::string&, typename ContainerExpression<ModelPart::ElementsContainerType>::Pointer);
 
 } /* namespace Kratos.*/
