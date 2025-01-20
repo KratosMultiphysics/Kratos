ab403a97fcba649b938e59e78d40db3bd5c0e021
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-01-20 11:26:09 +0100

    implement sensor_coverage_resp_utils

---
+++ applications/SystemIdentificationApplication/custom_utilities/response/sensor_coverage_response_utils.h
@@ -0,0 +1,46 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: SystemIdentificationApplication/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/model_part.h"
+#include "expression/container_expression.h"
+
+// Application includes
+#include "custom_utilities/sensor_mask_status.h"
+
+namespace Kratos {
+///@name Kratos Classes
+///@{
+
+class KRATOS_API(SYSTEM_IDENTIFICATION_APPLICATION) SensorCoverageResponseUtils
+{
+public:
+    ///@name Public static operations
+    ///@{
+
+    static double CalculateValue(const SensorMaskStatus& rSensorMaskStatus);
+
+    static ContainerExpression<ModelPart::NodesContainerType> CalculateGradient(const SensorMaskStatus& rSensorMaskStatus);
+
+    ///@}
+};
+
+///@} // Kratos Classes
+
+} /* namespace Kratos.*/
