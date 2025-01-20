3195fbd11b0a69f15d47d1038f477188839ae45d
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2025-01-20 17:39:06 +0100

    find closest material point element or condition

---
+++ applications/MPMApplication/custom_utilities/brute_force_material_point_locator.cpp
@@ -0,0 +1,82 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Nicolo Crescenzio
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "mpm_application_variables.h"
+#include "custom_utilities/brute_force_material_point_locator.h"
+
+namespace Kratos
+{
+
+int BruteForceMaterialPointLocator::FindElement(
+    const Point& rThePoint,
+    const double tolerance) const
+{
+    int found_element_id = -1; // if no element is found this will be returned
+    const auto& r_elements = mrModelPart.Elements();
+    FindObject(r_elements, "Element", rThePoint, found_element_id, tolerance);
+    return found_element_id;
+}
+
+int BruteForceMaterialPointLocator::FindCondition(
+    const Point& rThePoint,
+    const double tolerance) const
+{
+    int found_condition_id = -1; // if no condition is found this will be returned
+    const auto& r_conditions = mrModelPart.Conditions();
+    FindObject(r_conditions, "Condition", rThePoint, found_condition_id, tolerance);
+    return found_condition_id;
+}
+
+template<typename TObjectType>
+void BruteForceMaterialPointLocator::FindObject(
+    const TObjectType& rObjects,
+    const std::string& rObjectName,
+    const Point& rThePoint,
+    int& rObjectId,
+    const double tolerance) const
+{
+    int object_found = 0;
+
+    const int num_objects = rObjects.size();
+
+    KRATOS_WARNING_IF("BruteForceMaterialPointLocator", num_objects == 0)
+        << "No " << rObjectName << " in ModelPart \"" << mrModelPart.Name() << "\"" << std::endl;
+
+    std::vector<array_1d<double, 3>> mp_coord = { ZeroVector(3) };
+
+    double closest_object_distance = std::numeric_limits<double>::max();
+
+    for (auto& r_object : rObjects) {
+        r_object.CalculateOnIntegrationPoints(MP_COORD, mp_coord, mrModelPart.GetProcessInfo());
+
+        const double distance = std::sqrt(
+            std::pow(mp_coord[0][0] - rThePoint.X(),2) +
+            std::pow(mp_coord[0][1] - rThePoint.Y(),2) +
+            std::pow(mp_coord[0][2] - rThePoint.Z(),2));
+
+        if (distance < tolerance && distance < closest_object_distance) {
+            closest_object_distance = distance;
+            object_found = 1;
+            rObjectId = r_object.Id();
+        }
+    }
+
+    KRATOS_WARNING_IF("BruteForceMaterialPointLocator", !object_found)
+        << "No " << rObjectName << " close to point: " << rThePoint << std::endl;
+}
+
+}  // namespace Kratos.
---
+++ applications/MPMApplication/custom_utilities/brute_force_material_point_locator.h
@@ -0,0 +1,147 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Nicolo Crescenzio
+//
+
+#pragma once
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/model_part.h"
+
+namespace Kratos
+{
+
+///@addtogroup MPMApplication
+///@{
+
+///@name Kratos Classes
+///@{
+
+/**
+ * @class BruteForceMaterialPointLocator
+ * @ingroup MPMApplication
+ * @brief Utility class to find a material point entity based on a location
+ * @details Based on the location of a point, the corresponding material point
+ * entity (element or condition) is found and its id is returned
+ */
+class KRATOS_API(MPM_APPLICATION) BruteForceMaterialPointLocator
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of BruteForceMaterialPointLocator
+    KRATOS_CLASS_POINTER_DEFINITION(BruteForceMaterialPointLocator);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Default constructor.
+    explicit BruteForceMaterialPointLocator(ModelPart& rModelPart) : mrModelPart(rModelPart) {}
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * @brief This function finds a material point element based on a location
+     * @param rThePoint the location to search
+     * @param tolerance tolerance for finding closest material point element
+     * @return Id of the found element. -1 if no element was found
+     */
+    int FindElement(
+        const Point& rThePoint,
+        const double tolerance = 1e-6) const;
+
+    /**
+     * @brief This function finds a material point condition based on a location
+     * @param rThePoint the location to search
+     * @param tolerance tolerance for finding closest material point condition
+     * @return Id of the found condition. -1 if no condition was found
+     */
+    int FindCondition(
+        const Point& rThePoint,
+        const double tolerance = 1e-6) const;
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    virtual std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "BruteForceMaterialPointLocator" ;
+        return buffer.str();
+    }
+
+    /// Print information about this object.
+    virtual void PrintInfo(std::ostream& rOStream) const {rOStream << "BruteForceMaterialPointLocator";}
+
+    /// Print object's data.
+    virtual void PrintData(std::ostream& rOStream) const {}
+
+    ///@}
+
+private:
+    ///@name Member Variables
+    ///@{
+
+    ModelPart& mrModelPart;
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    /**
+     * @brief This function finds an object based on a location
+     * @param rObjects the objects to search
+     * @param rObjectType type of the object => "Element"/"Condition"
+     * @param rThePoint the location to search
+     * @param rObjectId Id of the found condition. -1 if no object was found
+     * @param tolerance tolerance local-coordinates for IsInside
+     */
+    template<typename TObjectType>
+    void FindObject(
+        const TObjectType& rObjects,
+        const std::string& rObjectType,
+        const Point& rThePoint,
+        int& rObjectId,
+        const double tolerance) const;
+
+    ///@}
+
+}; // Class BruteForceMaterialPointLocator
+
+///@}
+///@name Input and output
+///@{
+
+/// output stream function
+inline std::ostream& operator << (std::ostream& rOStream,
+                const BruteForceMaterialPointLocator& rThis)
+{
+    rThis.PrintInfo(rOStream);
+    rOStream << std::endl;
+    rThis.PrintData(rOStream);
+
+    return rOStream;
+}
+///@}
+
+///@} addtogroup block
+
+}  // namespace Kratos.
