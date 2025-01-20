96a893da78b8c9f70775f77a01b739e39db5fcc8
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-01-20 10:22:52 +0100

    add sensor mask status kd tree

---
+++ applications/SystemIdentificationApplication/custom_utilities/sensor_mask_status_kd_tree.cpp
@@ -0,0 +1,76 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: DigitalTwinApplication/license.txt
+//
+//  Main authors:    Suneth Warnakulasuriya
+//
+
+// System includes
+
+// External includes
+
+// Project includes
+
+// Application includes
+
+// Include base h
+#include "sensor_mask_status_kd_tree.h"
+
+namespace Kratos {
+///@name Kratos Classes
+///@{
+
+SensorMaskStatusKDTree::SensorMaskStatusKDTree(
+    SensorMaskStatus::Pointer pSensorMaskStatus,
+    const IndexType LeafMaxSize,
+    const IndexType EchoLevel)
+    : mpSensorMaskStatus(pSensorMaskStatus),
+      mLeafMaxSize(LeafMaxSize),
+      mEchoLevel(EchoLevel)
+{
+}
+
+SensorMaskStatus::Pointer SensorMaskStatusKDTree::GetSensorMaskStatus() const
+{
+    return mpSensorMaskStatus;
+}
+
+void SensorMaskStatusKDTree::RadiusSearch(
+    const Vector& rQueryPoint,
+    const double Radius,
+    std::vector<ResultType> &IndicesDistances) const
+{
+    KRATOS_TRY
+
+    mpKDTreeIndex->radiusSearch(rQueryPoint.data().begin(), Radius, IndicesDistances, nanoflann::SearchParameters());
+
+    KRATOS_CATCH("");
+}
+
+void SensorMaskStatusKDTree::Update()
+{
+    KRATOS_TRY
+
+    const Matrix& r_sensor_mask_status = mpSensorMaskStatus->GetMaskStatuses();
+
+    mpKratosMatrixKDTreeAdapter = Kratos::make_unique<KratosMatrixKDTreeAdapter>(r_sensor_mask_status);
+
+    mpKDTreeIndex = Kratos::make_unique<KDTreeIndexType>(
+        r_sensor_mask_status.size2(), *mpKratosMatrixKDTreeAdapter,
+        nanoflann::KDTreeSingleIndexAdaptorParams(mLeafMaxSize, nanoflann::KDTreeSingleIndexAdaptorFlags::None, 0));
+
+    mpKDTreeIndex->buildIndex();
+
+    KRATOS_INFO_IF("SensorMaskStatusKDTree", mEchoLevel > 0)
+        << "Updated sensor mask status kd tree in "
+        << mpSensorMaskStatus->GetSensorModelPart().FullName() << ".";
+
+    KRATOS_CATCH("");
+}
+
+} // namespace Kratos
