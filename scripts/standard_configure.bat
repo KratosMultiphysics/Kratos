4a25d13c108d1bdc3e9c2e27d8f26a4b5ed22e3b
mar0x13 <maramkhlifat@gmail.com>
2025-01-23 12:11:36 +0100

    Merging RM Shell for further testing

--- scripts/standard_configure.bat
+++ scripts/standard_configure.bat
@@ -13,23 +13,23 @@ set CC=cl.exe
 set CXX=cl.exe
 
 rem Set variables
-if not defined KRATOS_SOURCE set KRATOS_SOURCE=%~dp0..
+if not defined KRATOS_SOURCE set KRATOS_SOURCE=C:\Users\Alkhlaifat\Kratos
 if not defined KRATOS_BUILD set KRATOS_BUILD=%KRATOS_SOURCE%/build
 
 rem Warning: In windows this option only works if you run through a terminal with admin privileges
-rem set KRATOS_INSTALL_PYTHON_USING_LINKS=ON
+rem set KRATOS_INSTALL_PYTHON_USING_LINKS=ON 
 
 rem Set basic configuration
 if not defined KRATOS_BUILD_TYPE set KRATOS_BUILD_TYPE=Release
-if not defined BOOST_ROOT set BOOST_ROOT=C:\CompiledLibs\boost_1_67_0
-if not defined PYTHON_EXECUTABLE set PYTHON_EXECUTABLE=C:\Windows\py.exe
+if not defined BOOST_ROOT set BOOST_ROOT=C:\Users\boost_1_87_0
+if not defined PYTHON_EXECUTABLE set PYTHON_EXECUTABLE=C:\Users\Alkhlaifat\AppData\Local\Programs\Python\Python313\python.exe
 
 rem Set applications to compile
 set KRATOS_APP_DIR=applications
 set KRATOS_APPLICATIONS=
 CALL :add_app %KRATOS_APP_DIR%\LinearSolversApplication;
 CALL :add_app %KRATOS_APP_DIR%\StructuralMechanicsApplication;
-CALL :add_app %KRATOS_APP_DIR%\FluidDynamicsApplication;
+CALL :add_app %KRATOS_APP_DIR%\IgaApplication;
 
 rem Clean
 del /F /Q "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%\cmake_install.cmake"
@@ -41,13 +41,14 @@ rem set KRATOS_PARALLEL_BUILD_FLAG=/MP4
 
 rem Configure
 @echo on
-cmake -G"Visual Studio 16 2019" -H"%KRATOS_SOURCE%" -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"          ^
+cmake -G"Visual Studio 17 2022" -H"%KRATOS_SOURCE%" -B"%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%"          ^
 -DUSE_EIGEN_MKL=OFF                                                                                 ^
 -DCMAKE_CXX_FLAGS=" %KRATOS_PARALLEL_BUILD_FLAG% "                                                  ^
+-DCMAKE_INSTALL_PREFIX=C:\Users\Alkhlaifat\kratosv\Lib\site-packages                                ^
 -DKRATOS_GENERATE_PYTHON_STUBS=ON
 
 rem Build
-cmake --build "%KRATOS_BUILD%/%KRATOS_BUILD_TYPE%" --target install -- /property:configuration=%KRATOS_BUILD_TYPE% /p:Platform=x64
+cmake --build "%KRATOS_BUILD%/%KRATOS_BUILD_TYPE%" --target install -- /property:configuration=%KRATOS_BUILD_TYPE% /p:Platform=x64 /p:CL_MPcount=8 /m:1
 goto:eof
 
 rem Function to add apps
