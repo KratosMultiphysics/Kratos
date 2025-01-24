a576aa477a716600ef1144fdc8d25a9d397f1070
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-01-24 20:09:27 +0100

    add sensor localization resputils

---
+++ applications/SystemIdentificationApplication/custom_utilities/response/sensor_localization_response_utils.h
@@ -0,0 +1,88 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         SystemIdentificationApplication/license.txt
+//
+//  Main authors:    Suneth Warnakulasuriya
+//
+
+#pragma once
+
+// System includes
+#include <vector>
+#include <string>
+
+// External includes
+
+// Project includes
+#include "expression/container_expression.h"
+#include "includes/define.h"
+#include "includes/kratos_parameters.h"
+
+// Application includes
+#include "custom_utilities/sensor_mask_status_kd_tree.h"
+
+namespace Kratos {
+///@name Kratos Classes
+///@{
+
+class KRATOS_API(SYSTEM_IDENTIFICATION_APPLICATION) SensorLocalizationResponseUtils
+{
+public:
+    ///@name Type definitions
+    ///@{
+
+    KRATOS_CLASS_POINTER_DEFINITION(SensorLocalizationResponseUtils);
+
+    ///@}
+    ///@name Life cycle
+    ///@{
+
+    SensorLocalizationResponseUtils(
+        SensorMaskStatusKDTree::Pointer pSensorMaskKDTree,
+        const double Beta,
+        const double AllowedDissimilarity);
+
+    ///@}
+    ///@name Public operations
+    ///@{
+
+    double CalculateValue();
+
+    ContainerExpression<ModelPart::NodesContainerType> CalculateGradient() const;
+
+    ContainerExpression<ModelPart::ElementsContainerType> GetClusterSizes() const;
+
+    ///@}
+
+private:
+    ///@name Private member variables
+    ///@{
+
+    SensorMaskStatusKDTree::Pointer mpSensorMaskStatusKDTree;
+
+    const double mBeta;
+
+    const double mAllowedDissimilarity;
+
+    double mNumerator;
+
+    double mDenominator;
+
+    double mValue;
+
+    std::vector<double> mDomainSizeRatio;
+
+    std::vector<double> mClusterSizes;
+
+    std::vector<std::vector<SensorMaskStatusKDTree::ResultType>> mNeighbourData;
+
+    ///@}
+};
+
+///@} // Kratos Classes
+
+} /* namespace Kratos.*/
