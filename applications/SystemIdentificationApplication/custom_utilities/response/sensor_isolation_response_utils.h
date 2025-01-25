f9666a9f5a24ade9ed5b560d1de0aa9fc9229dd9
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-01-25 10:19:34 +0100

    add sensor isolation resp utils

---
+++ applications/SystemIdentificationApplication/custom_utilities/response/sensor_isolation_response_utils.h
@@ -0,0 +1,57 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: SystemIdentificationApplication/license.txt
+//
+//  Main author:     Suneth Warnakulasuriya
+//
+
+#pragma once
+
+// System includes
+
+// Project includes
+#include "includes/define.h"
+#include "includes/model_part.h"
+#include "expression/container_expression.h"
+
+// Application includes
+#include "custom_utilities/distance_matrix.h"
+
+namespace Kratos
+{
+
+///@name Kratos Classes
+///@{
+
+class KRATOS_API(SYSTEM_IDENTIFICATION_APPLICATION) SensorIsolationResponseUtils
+{
+public:
+    ///@name Type definitions
+    ///@{
+
+    using IndexType = std::size_t;
+
+    ///@}
+    ///@name Static operations
+    ///@{
+
+    static double CalculateValue(
+        ModelPart& rModelPart,
+        const double Radius,
+        const DistanceMatrix& rDistanceMatrix);
+
+    static ContainerExpression<ModelPart::NodesContainerType> CalculateGradient(
+        ModelPart& rModelPart,
+        const double Radius,
+        const DistanceMatrix& rDistanceMatrix);
+
+    ///@}
+};
+
+///@}
+}
