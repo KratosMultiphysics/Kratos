67a78a7125aeaff99d68cb9f40f87623f4450134
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2025-01-29 16:22:45 +0100

    Update `python_scripts/assign_initial_velocity_to_material_point_process.py`

--- applications/MPMApplication/python_scripts/assign_initial_velocity_to_material_point_process.py
+++ applications/MPMApplication/python_scripts/assign_initial_velocity_to_material_point_process.py
@@ -1,43 +1,46 @@
 import KratosMultiphysics
 import KratosMultiphysics.MPMApplication as KratosMPM
 
-def Factory(settings, Model):
-    if(not isinstance(settings, KratosMultiphysics.Parameters)):
+def Factory(settings: KratosMultiphysics.Parameters, model: KratosMultiphysics.Model) -> KratosMultiphysics.Process:
+    if not isinstance(model, KratosMultiphysics.Model):
+        raise Exception("expected input shall be a Model object")
+    if not isinstance(settings, KratosMultiphysics.Parameters):
         raise Exception("expected input shall be a Parameters object, encapsulating a json string")
-    return AssignInitialVelocityToMaterialPointProcess(Model, settings["Parameters"])
+    return AssignInitialVelocityToMaterialPointProcess(model, settings["Parameters"])
 
-## All the processes python should be derived from "Process"
 class AssignInitialVelocityToMaterialPointProcess(KratosMultiphysics.Process):
-    def __init__(self, Model, settings ):
+    def __init__(self, Model: KratosMultiphysics.Model, settings: KratosMultiphysics.Parameters) -> None:
         KratosMultiphysics.Process.__init__(self)
 
-        default_settings = KratosMultiphysics.Parameters("""
-            {
-                "model_part_name"      : "please_specify_model_part_name",
-                "variable_name"        : "MP_VELOCITY",
-                "modulus"              : 1.0,
-                "constrained"          : true,
-                "direction"            : [0.0, 0.0, 0.0],
-                "local_axes"           : {}
-            }
-            """)
+        # "modulus" must be a number
+        if settings.Has("modulus") and not settings["modulus"].IsNumber():
+            raise Exception('Parameter "modulus" must be a number')
 
-        # Trick: allow "modulus" and "direction" to be a double or a string value (otherwise the ValidateAndAssignDefaults might fail)
-        if(settings.Has("modulus")):
-            if(settings["modulus"].IsString()):
-                default_settings["modulus"].SetString("0.0")
+        # "direction" must be a vector with three components
+        if settings.Has("direction"):
+            if not settings["direction"].IsVector() or settings["direction"].GetVector().Size() != 3:
+                raise Exception('Parameter "direction" must be a vector of length 3')
 
-        if(settings.Has("direction")):
-            if(settings["direction"].IsString()):
-                default_settings["direction"].SetString("Automatic")
+        # "variable_name" is not requires: velocity is assigned to variable "MP_VELOCITY"
+        if settings.Has("variable_name"):
+            settings.RemoveValue("variable_name")
+            wrn_msg  = 'Parameter "variable_name" has been removed and will be ignored: '
+            wrn_msg += 'initial velocity is assigned to "MP_VELOCITY".'
+            KratosMultiphysics.Logger.PrintWarning("AssignInitialVelocityToMaterialPointProcess", wrn_msg)
 
-        # Detect if variable_name is MP_VELOCITY
-        if(settings.Has("variable_name")):
-            if(settings["variable_name"].GetString() != "MP_VELOCITY"):
-                KratosMultiphysics.Logger.PrintInfo("Warning in apply velocity to material point", "Error in determining variable_name")
-                raise Exception('The assign_initial_velocity_to_material_point_process only accepts \"MP_VELOCITY\" as variable_name.')
+        # This parameter was not used and is removed
+        if settings.Has("constrained"):
+            settings.RemoveValue("constrained")
+            wrn_msg = 'Parameter "constrained" has been removed and will be ignored.'
+            KratosMultiphysics.Logger.PrintWarning("AssignInitialVelocityToMaterialPointProcess", wrn_msg)
 
-        settings.ValidateAndAssignDefaults(default_settings)
+        # This parameter was not used and is removed
+        if settings.Has("local_axes"):
+            settings.RemoveValue("local_axes")
+            wrn_msg = 'Parameter "local_axes" has been removed and will be ignored.'
+            KratosMultiphysics.Logger.PrintWarning("AssignInitialVelocityToMaterialPointProcess", wrn_msg)
+
+        settings.ValidateAndAssignDefaults(self.GetDefaultParameters())
 
         # Get updated model_part
         self.model = Model
@@ -54,6 +57,16 @@ class AssignInitialVelocityToMaterialPointProcess(KratosMultiphysics.Process):
         self.modulus = settings["modulus"].GetDouble()
         self.velocity_direction = settings["direction"].GetVector()
         self.velocity = self.modulus * self.velocity_direction
+
+    @staticmethod
+    def GetDefaultParameters():
+        return KratosMultiphysics.Parameters("""
+            {
+                "model_part_name"      : "please_specify_model_part_name",
+                "modulus"              : 1.0,
+                "direction"            : [0.0, 0.0, 0.0]
+            }
+            """)
 
     def ExecuteBeforeSolutionLoop(self):
         # Assign velocity to MP after solver.Initialize() - only apply once at the beginning!
