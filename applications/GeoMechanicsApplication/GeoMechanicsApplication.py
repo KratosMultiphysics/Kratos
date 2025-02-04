69ddcd25b3ab164413b75988ba80417c41520bb6
markelov <gennady.markelov@deltares.nl>
2025-02-04 15:32:43 +0100

    added check for registration

--- applications/GeoMechanicsApplication/GeoMechanicsApplication.py
+++ applications/GeoMechanicsApplication/GeoMechanicsApplication.py
@@ -8,4 +8,5 @@ application_name = "KratosGeoMechanicsApplication"
 _ImportApplication(application, application_name)
 
 from . import python_registry_lists
-python_registry_utilities.RegisterAll("KratosMultiphysics.GeoMechanicsApplication", python_registry_lists)
+if not KratosMultiphysics.Registry.HasItem("Stages.KratosMultiphysics.GeoMechanicsApplication.GeoMechanicsAnalysis"):
+    python_registry_utilities.RegisterAll("KratosMultiphysics.GeoMechanicsApplication", python_registry_lists)
