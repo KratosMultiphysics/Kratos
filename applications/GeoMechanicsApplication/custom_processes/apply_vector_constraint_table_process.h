ebe986aced7c9fb7c6d60af166f76e63bb4cb180
Richard Faasse <richard.faasse@deltares.nl>
2025-02-06 15:02:16 +0100

    Formatting

--- applications/GeoMechanicsApplication/custom_processes/apply_vector_constraint_table_process.h
+++ applications/GeoMechanicsApplication/custom_processes/apply_vector_constraint_table_process.h
@@ -36,7 +36,7 @@ public:
 
     void ExecuteInitialize() override;
     void ExecuteInitializeSolutionStep() override;
-    void        ExecuteFinalize() override;
+    void ExecuteFinalize() override;
 
     std::string Info() const override;
 
