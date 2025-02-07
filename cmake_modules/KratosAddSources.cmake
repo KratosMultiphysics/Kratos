8f13de10d45d956a44508f1be1c585d2c8148664
Carlos Roig <roigcarlo@gmail.com>
2025-02-07 16:25:04 +0100

    Adding the macro code...

---
+++ cmake_modules/KratosAddSources.cmake
@@ -0,0 +1,31 @@
+# This function automatically configures a given application to build its tests
+macro(kratos_add_sources)
+    set(multiValueArgs SOURCE_DIRS)
+
+    cmake_parse_arguments(KRATOS_ADD_SOURCES "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if(KRATOS_ADD_SOURCES_SOURCE_DIRS)
+
+        FOREACH(subFolder ${KRATOS_ADD_SOURCES_SOURCE_DIRS})
+            FILE(GLOB_RECURSE ${subFolder}Sources "${subFolder}/*.cpp" "${subFolder}/*.c") #all .cpp and .c
+
+            # Auxiliar list
+            SET(AUXILIAR_CORE_SOURCES "")
+            FOREACH(file ${${subFolder}Sources})
+                SET(EXCEPTION_CHECK 0)
+                FOREACH(exception ${LIST_OF_SOURCE_EXCEPTIONS})
+                    IF(${file} STREQUAL ${exception})
+                        SET(EXCEPTION_CHECK 1)
+                    ENDIF(${file} STREQUAL ${exception})
+                ENDFOREACH(exception ${LIST_OF_SOURCE_EXCEPTIONS})
+                IF (${EXCEPTION_CHECK} EQUAL 0)
+                    LIST(APPEND AUXILIAR_CORE_SOURCES ${file})
+                ENDIF(${EXCEPTION_CHECK} EQUAL 0)
+            ENDFOREACH(file ${${subFolder}Sources})
+
+            # Append to final list
+            LIST(APPEND ${ARGV0} ${AUXILIAR_CORE_SOURCES})
+        ENDFOREACH(subFolder ${ListFolders})
+    endif()
+
+endmacro(kratos_add_sources)
