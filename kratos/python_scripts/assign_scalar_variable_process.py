96b892fc15dd2c3884b1ce9db579d849fcd3f561
matekelemen <kelemen.mate.12@gmail.com>
2025-02-16 21:18:46 +0100

    add schema interface to AssignScalarVariableProcess

--- kratos/python_scripts/assign_scalar_variable_process.py
+++ kratos/python_scripts/assign_scalar_variable_process.py
@@ -5,7 +5,7 @@ from KratosMultiphysics.read_csv_table_utility import ReadCsvTableUtility
 
 def Factory(settings, Model):
     if not isinstance(settings, KratosMultiphysics.Parameters):
-        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+        raise Exception(f"Expecting a Parameters object, but got {settings}.")
     return AssignScalarVariableProcess(Model, settings["Parameters"])
 
 class AssignScalarVariableProcess(KratosMultiphysics.Process):
@@ -39,34 +39,9 @@ class AssignScalarVariableProcess(KratosMultiphysics.Process):
         """
         KratosMultiphysics.Process.__init__(self)
 
-        #The value can be a double or a string (function)
-        default_settings = KratosMultiphysics.Parameters("""
-        {
-            "help"            : "This process sets a given scalar value for a certain variable in all the nodes of a submodelpart",
-            "mesh_id"         : 0,
-            "model_part_name" : "please_specify_model_part_name",
-            "variable_name"   : "SPECIFY_VARIABLE_NAME",
-            "interval"        : [0.0, 1e30],
-            "constrained"     : true,
-            "value"           : {},
-            "local_axes"      : {}
-        }
-        """
-        )
-
         # Assign this here since it will change the "interval" prior to validation
         self.interval = KratosMultiphysics.IntervalUtility(settings)
-
-        # Here i do a trick, since i want to allow "value" to be a string or a double value or a sub parameter
-        if settings.Has("value"):
-            if settings["value"].IsString():
-                default_settings["value"].SetString("0.0")
-            if settings["value"].IsNumber():
-                default_settings["value"].SetDouble(0.0)
-        else:
-            default_settings["value"].SetDouble(0.0)
-
-        settings.ValidateAndAssignDefaults(default_settings)
+        settings.ValidateAndAssignDefaults(self.GetSchema())
 
         self.variable = KratosMultiphysics.KratosGlobals.GetVariable(settings["variable_name"].GetString())
         if not isinstance(self.variable, KratosMultiphysics.DoubleVariable) and not isinstance(self.variable, KratosMultiphysics.VectorVariable):
@@ -144,3 +119,80 @@ class AssignScalarVariableProcess(KratosMultiphysics.Process):
                 self.variable_utils.ApplyFixity(self.variable, fixity_status, self.mesh.Nodes)
 
         self.step_is_active = False
+
+    @classmethod
+    def GetSchema(cls) -> KratosMultiphysics.Schema:
+        definition: KratosMultiphysics.Parameters = KratosMultiphysics.Parameters("""{
+            "title" : "AssignScalarVariableToEntitiesProcess",
+            "description" : "A process for setting scalar kratos variables on all nodes of the provided model part.",
+            "type" : "object",
+            "properties" : {
+                "model_part_name" : {
+                    "description" : "Name of the model part to assign variables in.",
+                    "type" : "string",
+                    "pattern" : ".+"
+                },
+                "mesh_id" : {
+                    "type" : "integer",
+                    "default" : 0
+                },
+                "variable_name" : {
+                    "description" : "Name of the variable to set.",
+                    "type" : "string",
+                    "pattern" : "[A-Z0-9_]+"
+                },
+                "interval" : {
+                    "description" : "Populated at runtime."
+                },
+                "constrained" : {
+                    "description" : "Set a Dirichlet condition on the provided variable of the specified model part's nodes if this is true.",
+                    "type" : "boolean",
+                    "default" : true
+                },
+                "value" : {
+                    "description" : "Value to set the specified variable to. The value may be provided as a literal number, a string defining a function of x, y, z, t, X, Y, Z, or an object specifying a CSV file.",
+                    "anyOf": [
+                        {"type" : "bool"},
+                        {"type" : "number"},
+                        {"type" : "string"},
+                        {"type" : "object", "description" : "Populated at runtime."}
+                    ],
+                    "default" : {}
+                },
+                "local_axes" : {
+                    "decription" : "Local coordinate system to compute the value in if it is a string.",
+                    "type" : "object",
+                    "properties" : {
+                        "origin" : {
+                            "description" : "Origin of the local coordinate system.",
+                            "type" : "array",
+                            "minItems" : 3,
+                            "maxItems" : 3,
+                            "items" : "number",
+                            "default" : [0,0,0]
+                        },
+                        "axes" : {
+                            "description" : "3D rotation matrix defining the local basis vectors from the global unit vectors.",
+                            "type" : "array",
+                            "items" : {
+                                "type" : "array",
+                                "minItems" : 3,
+                                "maxItems" : 3
+                            },
+                            "default" : [[1,0,0],[0,1,0],[0,0,1]]
+                        }
+                    },
+                    "default" : {}
+                }
+            },
+            "additionalProperties" : false,
+            "required" : ["model_part_name", "variable_name"]
+        }""")
+
+        # Define the schema when the value refers to a CSV file.
+        definition["properties"]["value"]["anyOf"][3] = ReadCsvTableUtility.GetSchema().GetDefinition()
+
+        # Define the interval's schema.
+        definition["properties"]["interval"] = KratosMultiphysics.IntervalUtility(KratosMultiphysics.Parameters()).GetSchema().GetDefinition()["properties"]["interval"]
+
+        return KratosMultiphysics.Schema(definition)
