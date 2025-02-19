af85366c44c1eddd05b0ebde59af99f724747ead
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2025-02-19 18:50:43 +0100

    Test boilerplate

---
+++ kratos/future/tests/cpp_tests/test_future_linear_solvers.cpp
@@ -0,0 +1,73 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Ruben Zorrilla
+//
+
+// Project includes
+#include "testing/testing.h"
+#include "future/linear_solvers/amgcl_solver.h"
+
+namespace Kratos::Testing
+{
+
+namespace
+{
+    void SetLinearSystem(
+        CsrMatrix<>& rLHS,
+        SystemVector<>& rRHS)
+    {
+        // Set the matrix map and assign it to the provided LHS matrix
+        typename CsrMatrix<double>::MatrixMapType matrix_map;
+        matrix_map[{0, 0}] = 10; matrix_map[{0, 3}] = -2;
+        matrix_map[{1, 1}] = 8; matrix_map[{1, 2}] = -1;
+        matrix_map[{2, 2}] = 5; matrix_map[{2, 1}] = -1; matrix_map[{2, 4}] = -2;
+        matrix_map[{3, 3}] = 7; matrix_map[{3, 0}] = -2; matrix_map[{3, 4}] = -1;
+        matrix_map[{4, 4}] = 6; matrix_map[{4, 2}] = -2; matrix_map[{4, 3}] = -1;
+        rLHS = CsrMatrix<double>(matrix_map);
+
+        // Set the RHS vector
+        DenseVector<double> aux_data(5);
+        aux_data[0] = 3.0;
+        aux_data[1] = 7.0;
+        aux_data[2] = 2.0;
+        aux_data[3] = 5.0;
+        aux_data[4] = 1.0;
+        rRHS = SystemVector<>(aux_data);
+    }
+}
+
+KRATOS_TEST_CASE_IN_SUITE(FutureLinearSolversAmgcl, KratosCoreFutureSuite)
+{
+    // Set up the system to be solved
+    CsrMatrix<> LHS;
+    SystemVector<> RHS;
+    std::cout << "Before set up" << std::endl;
+    SetLinearSystem(LHS, RHS);
+    std::cout << "After set up" << std::endl;
+
+    // Set the linear solver to be tested
+    Parameters amgcl_settings(R"({
+        "solver_type"                    : "AMGCL",
+        "max_iteration"                  : 100,
+        "verbosity"                      : 1,
+        "tolerance"                      : 1e-6
+    })");
+    auto p_linear_solver = Kratos::make_unique<Future::AMGCLSolver<CsrMatrix<>, SystemVector<>>>(amgcl_settings);
+
+    // Solve the problem
+    SystemVector<> sol;
+    p_linear_solver->Solve(LHS, sol, RHS);
+
+    // Check the obtained results
+    KRATOS_WATCH(sol)
+
+}
+
+}
