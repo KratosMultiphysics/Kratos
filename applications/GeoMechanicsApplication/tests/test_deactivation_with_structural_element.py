18ea4eacb934da80e1858d06d126be44387ad8b9
Richard Faasse <richard.faasse@deltares.nl>
2025-02-20 14:06:37 +0100

    Added a readme for the test

--- applications/GeoMechanicsApplication/tests/test_deactivation_with_structural_element.py
+++ applications/GeoMechanicsApplication/tests/test_deactivation_with_structural_element.py
@@ -5,17 +5,15 @@ import test_helper
 import glob
 
 import KratosMultiphysics.GeoMechanicsApplication
-
 from KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis import GeoMechanicsAnalysis
+
 class KratosGeoMechanicsDeactivationWithStructuralTest(KratosUnittest.TestCase):
     """
     This class contains a test for a multistage computation with deactivation of structural elements in the first stage
+    and the use of 'rest' as input type for the second stage.
     """
 
     def test_deactivation_with_structural_element(self):
-        """
-
-        """
         test_name    = 'deactivation_with_structural_element'
         project_path = test_helper.get_file_path(test_name)
 
