0e6780482dd11822d09a8d555ce7dc31fd4c6174
markelov <gennady.markelov@deltares.nl>
2025-03-04 13:40:57 +0100

    changed time step function

--- applications/GeoMechanicsApplication/custom_workflows/adaptive_time_incrementor.cpp
+++ applications/GeoMechanicsApplication/custom_workflows/adaptive_time_incrementor.cpp
@@ -75,25 +75,33 @@ double AdaptiveTimeIncrementor::GetIncrement() const { return mDeltaTime; }
 
 void AdaptiveTimeIncrementor::PostTimeStepExecution(const TimeStepEndState& rResultantState)
 {
-    if (rResultantState.NonConverged() ||
-        (rResultantState.Converged() && (rResultantState.num_of_iterations == mMaxNumOfIterations))) {
+    // mDeltaTime = std::min(mDeltaTime, mMaxDeltaTime);
+    auto conv    = rResultantState.Converged();
+    auto nonconv = rResultantState.NonConverged();
+    if (rResultantState.Converged()) // it is converged also at the beginning of the cycles
+    {
+        // # scale next step if desired
+        if (rResultantState.time < mEndTime) {
+            if (rResultantState.num_of_iterations < mMinNumOfIterations) {
+                // #scale up next step
+                mDeltaTime = std::min(mDeltaTime * mIncreaseFactor, mMaxDeltaTime);
+                constexpr auto fraction_to_avoid_too_small_step = 1.0e-3;
+                if (rResultantState.time + mDeltaTime > mEndTime - fraction_to_avoid_too_small_step * mDeltaTime) {
+                    mDeltaTime = mEndTime - rResultantState.time;
+                }
+            } else if (rResultantState.num_of_iterations == mMaxNumOfIterations) {
+                // # converged, but max_iterations reached, scale down next step
+                mDeltaTime *= mReductionFactor;
+            }
+        }
+    } else {
+        // # scale down step and restart
         mDeltaTime *= mReductionFactor;
-    } else if (rResultantState.Converged() && (rResultantState.num_of_iterations < mMinNumOfIterations)) {
-        mDeltaTime = std::min(mDeltaTime * mIncreaseFactor, mMaxDeltaTime);
+
+        KRATOS_ERROR_IF(mDeltaTime < mMinAllowableDeltaTime.second)
+            << "Delta time (" << mDeltaTime << ") is smaller than minimum allowable value "
+            << mMinAllowableDeltaTime.second << std::endl;
     }
-
-    // Avoid incrementing the time beyond the end time
-    mDeltaTime = std::min(mDeltaTime, mEndTime - rResultantState.time);
-
-    // Avoid very small remaining time steps
-    const auto small_time_increment = 1.E-3 * mDeltaTime;
-    if ((mEndTime - (rResultantState.time + mDeltaTime)) < small_time_increment) {
-        mDeltaTime = mEndTime - rResultantState.time;
-    }
-
-    KRATOS_ERROR_IF(mDeltaTime < mMinAllowableDeltaTime.second)
-        << "Delta time (" << mDeltaTime << ") is smaller than minimum allowable value "
-        << mMinAllowableDeltaTime.second << std::endl;
 }
 
 } // namespace Kratos
