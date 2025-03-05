6fd090471511ac53b73db869c1bff725dfcc43e4
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-03-05 16:44:41 +0100

    Added a generic getter for the cohesion

--- applications/GeoMechanicsApplication/custom_utilities/constitutive_law_utilities.cpp
+++ applications/GeoMechanicsApplication/custom_utilities/constitutive_law_utilities.cpp
@@ -12,6 +12,7 @@
 //
 
 #include "custom_utilities/constitutive_law_utilities.h"
+#include "geo_mechanics_application_variables.h"
 
 namespace Kratos
 {
@@ -41,4 +42,20 @@ void ConstitutiveLawUtilities::SetConstitutiveParameters(ConstitutiveLaw::Parame
     rConstitutiveParameters.SetDeformationGradientF(rF);
     rConstitutiveParameters.SetDeterminantF(detF);
 }
+
+double ConstitutiveLawUtilities::GetCohesion(const Properties& rProperties)
+{
+    // So far, we only support retrieving the cohesion from user-defined material models
+    if (rProperties.Has(UMAT_PARAMETERS) && rProperties.Has(INDEX_OF_UMAT_C_PARAMETER)) {
+        const auto index = rProperties[INDEX_OF_UMAT_C_PARAMETER]; // 1-based index
+        KRATOS_DEBUG_ERROR_IF(index < 1 ||
+                              static_cast<std::size_t>(index) > rProperties[UMAT_PARAMETERS].size())
+            << "Got out-of-bounds INDEX_OF_UMAT_C_PARAMETER (material ID: " << rProperties.Id()
+            << "): " << index << " is not in range [1, " << rProperties[UMAT_PARAMETERS].size() << "]\n";
+        return rProperties[UMAT_PARAMETERS][index - 1];
+    }
+
+    KRATOS_ERROR << "Material " << rProperties.Id() << "does not have a value for the cohesion\n";
+}
+
 } // namespace Kratos
