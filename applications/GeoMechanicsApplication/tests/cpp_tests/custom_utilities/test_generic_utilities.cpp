c22c0a370403f21c86fb94e8ab334cad9c67ac02
mnabideltares <mohamed.nabi@deltares.nl>
2025-03-13 18:37:03 +0100

    Added unit tests for permutations

---
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_utilities/test_generic_utilities.cpp
@@ -0,0 +1,69 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+//  Main authors:    Mohamed Nabi
+//                   Wijtze Pieter Kikstra
+//
+
+#include "custom_utilities/generic_utilities.h"
+#include "tests/cpp_tests/geo_mechanics_fast_suite.h"
+#include "tests/cpp_tests/test_utilities.h"
+#include <boost/numeric/ublas/assignment.hpp>
+
+#include "custom_utilities/math_utilities.h"
+#include "custom_utilities/stress_strain_utilities.h"
+#include "utilities/math_utils.h"
+
+
+
+using namespace Kratos;
+
+namespace Kratos::Testing
+{
+
+KRATOS_TEST_CASE_IN_SUITE(CheckVectorPermutation, KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    // Arrange
+    Vector vector(5);
+    vector <<= 1.0, 2.0, 3.0, 4.0, 5.0;
+    std::vector<int> indices = {4, 3, 2, 1, 0};
+
+    // Act
+    Vector result = GenericUtilities::PermutedVector(vector, indices);
+
+    // Assert
+    Vector expected_result(5);
+    expected_result <<= 5.0, 4.0, 3.0, 2.0, 1.0;
+    KRATOS_EXPECT_VECTOR_EQ(result, expected_result);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(CheckMatrixPermutation, KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    // Arrange
+    Matrix matrix(4, 4);
+    matrix <<= 1.0, 0.0, 0.0, 0.0,
+               0.0, 1.0, 0.0, 0.0,
+               0.0, 0.0, 1.0, 0.0,
+               0.0, 0.0, 0.0, 1.0;
+    std::vector<int> indices = {3, 2, 1, 0};
+
+    // Act
+    Matrix result = GenericUtilities::MatrixWithPermutedColumns(matrix, indices);
+
+    // Assert
+    Matrix expected_result(4, 4);
+    expected_result <<=  0.0, 0.0, 0.0, 1.0,
+                         0.0, 0.0, 1.0, 0.0,
+                         0.0, 1.0, 0.0, 0.0,
+                         1.0, 0.0, 0.0, 0.0;
+    KRATOS_EXPECT_MATRIX_EQ(result, expected_result);
+}
+
+
+} // namespace Kratos::Testing
