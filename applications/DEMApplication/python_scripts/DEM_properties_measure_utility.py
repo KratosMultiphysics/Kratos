a7496067dee02c27c350425ac94e62d1e4aa811b
Chengshun Shang <chengshun.shang1996@gmail.com>
2025-03-13 17:25:27 -0700

    remove "stress_tensor_modulus"

--- applications/DEMApplication/python_scripts/DEM_properties_measure_utility.py
+++ applications/DEMApplication/python_scripts/DEM_properties_measure_utility.py
@@ -306,51 +306,6 @@ class DEMPropertiesMeasureUtility:
             fmt_list = ['%d', '%.6f', '%.6f', '%.6f', '%.6f']
             np.savetxt(os.path.join(self.graphs_path, output_file_name), particle_id_positions_and_radius, fmt=fmt_list, delimiter='\t', comments='')
         
-        if type == "stress_tensor_modulus":
-            
-            if self.DEM_parameters["PostStressStrainOption"].GetBool() and self.DEM_parameters["ContactMeshOption"].GetBool():
-                
-                measure_sphere_volume = 4.0 / 3.0 * math.pi * radius * radius * radius
-                total_tensor        = np.empty((3, 3))
-                total_tensor[:]     = 0.0
-                stress_tensor_modulus = 0.0
-
-                for element in self.contact_model_part.Elements:
-            
-                    x_0 = element.GetNode(0).X
-                    x_1 = element.GetNode(1).X
-                    y_0 = element.GetNode(0).Y
-                    y_1 = element.GetNode(1).Y
-                    z_0 = element.GetNode(0).Z
-                    z_1 = element.GetNode(1).Z
-                    r_0 = element.GetNode(0).GetSolutionStepValue(RADIUS)
-                    r_1 = element.GetNode(1).GetSolutionStepValue(RADIUS)
-                    r   = 0.5 * (r_0 + r_1)
-
-                    center_to_sphere_distance_0 = ((x_0 - center_x)**2 + (y_0 - center_y)**2 + (z_0 - center_z)**2)**0.5
-                    center_to_sphere_distance_1 = ((x_1 - center_x)**2 + (y_1 - center_y)**2 + (z_1 - center_z)**2)**0.5
-
-                    if (center_to_sphere_distance_0 < (radius - r)) or (center_to_sphere_distance_1 < (radius - r)):
-                        
-                        local_contact_force_X = element.GetValue(GLOBAL_CONTACT_FORCE)[0]
-                        local_contact_force_Y = element.GetValue(GLOBAL_CONTACT_FORCE)[1]
-                        local_contact_force_Z = element.GetValue(GLOBAL_CONTACT_FORCE)[2]
-                        contact_force_vector = np.array([local_contact_force_X , local_contact_force_Y, local_contact_force_Z])
-                        vector_l = np.array([x_0 - x_1 , y_0 - y_1, z_0 - z_1])
-                        tensor = np.outer(contact_force_vector, vector_l)
-                        total_tensor += tensor
-
-                total_tensor = total_tensor / measure_sphere_volume
-                
-                stress_tensor_modulus = np.linalg.norm(total_tensor)
-                
-                return stress_tensor_modulus
-            
-            else:
-                
-                raise Exception('The \"PostStressStrainOption\" and \"ContactMeshOption\" in the [ProjectParametersDEM.json] should be [True].')
-            
-        
         if type == "stress_tensor":
             
             if self.DEM_parameters["PostStressStrainOption"].GetBool() and self.DEM_parameters["ContactMeshOption"].GetBool():
