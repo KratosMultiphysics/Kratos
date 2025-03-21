e55fd2c6c0f6dbb623c060b7ab49bb4bb95afab6
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-03-21 11:07:27 +0100

    [Core] Clean up legacy `mesh_id` from processes

--- kratos/python_scripts/assign_vector_variable_to_entities_process.py
+++ kratos/python_scripts/assign_vector_variable_to_entities_process.py
@@ -33,7 +33,6 @@ class AssignVectorVariableToEntitiesProcess(KratosMultiphysics.Process):
         default_settings = KratosMultiphysics.Parameters("""
         {
             "help"                 : "This process assigns a given value (vector) to the entities belonging a certain submodelpart",
-            "mesh_id"              : 0,
             "model_part_name"      : "please_specify_model_part_name",
             "variable_name"        : "SPECIFY_VARIABLE_NAME",
             "interval"             : [0.0, 1e30],
