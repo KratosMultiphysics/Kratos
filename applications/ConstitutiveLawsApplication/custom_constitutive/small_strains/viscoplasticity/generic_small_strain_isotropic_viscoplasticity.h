ead6426a2c2cb4b17c64628c422b000434968150
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2025-03-24 17:01:11 +0100

    clean base CL

--- applications/ConstitutiveLawsApplication/custom_constitutive/small_strains/viscoplasticity/generic_small_strain_isotropic_viscoplasticity.h
+++ applications/ConstitutiveLawsApplication/custom_constitutive/small_strains/viscoplasticity/generic_small_strain_isotropic_viscoplasticity.h
@@ -72,12 +72,6 @@ public:
     /// Counted pointer of GenericSmallStrainIsotropicPlasticity
     KRATOS_CLASS_POINTER_DEFINITION(GenericSmallStrainIsotropicViscoPlasticity);
 
-    // /// The node definition
-    // typedef Node NodeType;
-
-    // /// The geometry definition
-    // typedef Geometry<NodeType> GeometryType;
-
     ///@}
     ///@name Life Cycle
     ///@{
@@ -133,137 +127,6 @@ public:
      */
     void FinalizeMaterialResponseCauchy(ConstitutiveLaw::Parameters &rValues) override;
 
-    // /**
-    //  * @brief Returns whether this constitutive Law has specified variable (double)
-    //  * @param rThisVariable the variable to be checked for
-    //  * @return true if the variable is defined in the constitutive law
-    //  */
-    // bool Has(const Variable<double> &rThisVariable) override;
-
-    // /**
-    //  * @brief Returns whether this constitutive Law has specified variable (Vector)
-    //  * @param rThisVariable the variable to be checked for
-    //  * @return true if the variable is defined in the constitutive law
-    //  */
-    // bool Has(const Variable<Vector> &rThisVariable) override;
-
-    // /**
-    //  * @brief Returns whether this constitutive Law has specified variable (Matrix)
-    //  * @param rThisVariable the variable to be checked for
-    //  * @return true if the variable is defined in the constitutive law
-    //  */
-    // bool Has(const Variable<Matrix> &rThisVariable) override;
-
-    // /**
-    //  * @brief Sets the value of a specified variable (double)
-    //  * @param rVariable the variable to be returned
-    //  * @param rValue new value of the specified variable
-    //  * @param rCurrentProcessInfo the process info
-    //  */
-    // void SetValue(
-    //     const Variable<double> &rThisVariable,
-    //     const double& rValue,
-    //     const ProcessInfo& rCurrentProcessInfo
-    //     ) override;
-
-    // /**
-    //  * @brief Sets the value of a specified variable (Vector)
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue new value of the specified variable
-    //  * @param rCurrentProcessInfo the process info
-    //  */
-    // void SetValue(
-    //     const Variable<Vector> &rThisVariable,
-    //     const Vector& rValue,
-    //     const ProcessInfo& rCurrentProcessInfo
-    //     ) override;
-
-    // /**
-    //  * @brief Returns the value of a specified variable (double)
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue a reference to the returned value
-    //  * @return rValue output: the value of the specified variable
-    //  */
-    // double& GetValue(
-    //     const Variable<double> &rThisVariable,
-    //     double& rValue
-    //     ) override;
-
-    // /**
-    //  * @brief Returns the value of a specified variable (Vector)
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue a reference to the returned value
-    //  * @return rValue output: the value of the specified variable
-    //  */
-    // Vector& GetValue(
-    //     const Variable<Vector> &rThisVariable,
-    //     Vector& rValue
-    //     ) override;
-
-    // /**
-    //  * @brief Returns the value of a specified variable (matrix)
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue a reference to the returned value
-    //  * @return rValue output: the value of the specified variable
-    //  */
-    // Matrix& GetValue(
-    //     const Variable<Matrix>& rThisVariable,
-    //     Matrix& rValue
-    //     ) override;
-
-    // /**
-    //  * @brief Returns the value of a specified variable (double)
-    //  * @param rParameterValues the needed parameters for the CL calculation
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue a reference to the returned value
-    //  * @param rValue output: the value of the specified variable
-    //  */
-    // double& CalculateValue(
-    //     ConstitutiveLaw::Parameters& rParameterValues,
-    //     const Variable<double>& rThisVariable,
-    //     double& rValue) override;
-
-    // /**
-    //  * @brief Returns the value of a specified variable (vector)
-    //  * @param rParameterValues the needed parameters for the CL calculation
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue a reference to the returned value
-    //  * @param rValue output: the value of the specified variable
-    //  */
-    // Vector& CalculateValue(
-    //     ConstitutiveLaw::Parameters& rParameterValues,
-    //     const Variable<Vector>& rThisVariable,
-    //     Vector& rValue
-    //     ) override;
-
-    // /**
-    //  * @brief Returns the value of a specified variable (matrix)
-    //  * @param rParameterValues the needed parameters for the CL calculation
-    //  * @param rThisVariable the variable to be returned
-    //  * @param rValue a reference to the returned value
-    //  * @param rValue output: the value of the specified variable
-    //  */
-    // Matrix& CalculateValue(
-    //     ConstitutiveLaw::Parameters& rParameterValues,
-    //     const Variable<Matrix>& rThisVariable,
-    //     Matrix& rValue
-    //     ) override;
-
-    // /**
-    //  * @brief This function provides the place to perform checks on the completeness of the input.
-    //  * @details It is designed to be called only once (or anyway, not often) typically at the beginning
-    //  * of the calculations, so to verify that nothing is missing from the input or that no common error is found.
-    //  * @param rMaterialProperties The properties of the material
-    //  * @param rElementGeometry The geometry of the element
-    //  * @param rCurrentProcessInfo The current process info instance
-    //  * @return 0 if OK, 1 otherwise
-    //  */
-    // int Check(
-    //     const Properties& rMaterialProperties,
-    //     const GeometryType& rElementGeometry,
-    //     const ProcessInfo& rCurrentProcessInfo
-    //     ) const override;
-
     ///@}
     ///@name Access
     ///@{
@@ -293,14 +156,6 @@ protected:
     ///@}
     ///@name Protected Operators
     ///@{
-
-    // double& GetThreshold() { return mThreshold; }
-    // double& GetPlasticDissipation() { return mPlasticDissipation; }
-    // Vector& GetPlasticStrain() { return mPlasticStrain; }
-
-    // void SetThreshold(const double Threshold) { mThreshold = Threshold; }
-    // void SetPlasticDissipation(const double PlasticDissipation) { mPlasticDissipation = PlasticDissipation; }
-    // void SetPlasticStrain(const array_1d<double, VoigtSize>& rPlasticStrain) { mPlasticStrain = rPlasticStrain; }
 
     ///@}
     ///@name Protected Operations
