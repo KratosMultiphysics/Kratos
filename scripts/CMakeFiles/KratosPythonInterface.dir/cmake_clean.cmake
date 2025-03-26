f98fcc37045e406f9fc0ea791db3fb0cd3891293
Juan I. Camarotti <juanicama7@gmail.com>
2025-03-26 16:11:20 +0100

    Remove files

--- scripts/CMakeFiles/KratosPythonInterface.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/KratosPythonInterface"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/KratosPythonInterface.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
