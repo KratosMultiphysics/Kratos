8a7d988d4a3061c936c92958b054bbbba93442e0
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2025-03-27 16:07:21 +0100

    Tabs

--- kratos/future/linear_solvers/reorderer.h
+++ kratos/future/linear_solvers/reorderer.h
@@ -4,8 +4,8 @@
 //   _|\_\_|  \__,_|\__|\___/ ____/
 //                   Multi-Physics
 //
-//  License:		 BSD License
-//					 Kratos default license: kratos/license.txt
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Pooyan Dadvand
 //                   Ruben Zorrilla
