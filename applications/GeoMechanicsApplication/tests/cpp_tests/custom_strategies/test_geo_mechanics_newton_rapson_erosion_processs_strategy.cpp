f91ed3c78a8e7a8e77ef5e47bf5c23c34a7ccc6f
matekelemen <kelemen.mate.12@gmail.com>
2025-03-28 23:15:10 +0100

    remove unused type alias

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_strategies/test_geo_mechanics_newton_rapson_erosion_processs_strategy.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_strategies/test_geo_mechanics_newton_rapson_erosion_processs_strategy.cpp
@@ -96,8 +96,7 @@ auto SetupPipingStrategy(Model& rModel)
 {
     using SparseSpaceType          = UblasSpace<double, CompressedMatrix, Vector>;
     using LocalSpaceType           = UblasSpace<double, Matrix, Vector>;
-    using LinearSolverBaseType     = LinearSolver<SparseSpaceType, LocalSpaceType>;
-    using LinearSolverType         = SkylineLUFactorizationSolver<SparseSpaceType, LocalSpaceType>;
+    using LinearSolverType         = LinearSolver<SparseSpaceType, LocalSpaceType>;
     using ConvergenceCriteriaType  = ConvergenceCriteria<SparseSpaceType, LocalSpaceType>;
     using MixedGenericCriteriaType = MixedGenericCriteria<SparseSpaceType, LocalSpaceType>;
     using ConvergenceVariableListType = MixedGenericCriteriaType::ConvergenceVariableListType;
@@ -155,12 +154,12 @@ auto SetupPipingStrategy(Model& rModel)
     }  )");
 
     const auto p_builder_and_solver =
-        Kratos::make_shared<ResidualBasedBlockBuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverBaseType>>(nullptr);
+        Kratos::make_shared<ResidualBasedBlockBuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType>>(nullptr);
     const ConvergenceVariableListType      convergence_settings{};
     const ConvergenceCriteriaType::Pointer p_criteria =
         std::make_shared<MixedGenericCriteriaType>(convergence_settings);
 
-    return std::make_unique<MockGeoMechanicsNewtonRaphsonErosionProcessStrategy<SparseSpaceType, LocalSpaceType, LinearSolverBaseType>>(
+    return std::make_unique<MockGeoMechanicsNewtonRaphsonErosionProcessStrategy<SparseSpaceType, LocalSpaceType, LinearSolverType>>(
         r_model_part, nullptr, p_criteria, p_builder_and_solver, p_parameters);
 }
 } // namespace Kratos
