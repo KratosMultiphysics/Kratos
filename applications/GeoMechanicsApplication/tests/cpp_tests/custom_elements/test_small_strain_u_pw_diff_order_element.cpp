db517421ad7d48442bed1ffe140ff331e65c900c
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-03-28 13:47:31 +0100

    Create test elements using a utility function
    Added a utility function that creates a 2D 6-noded U-Pw diff order element.

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_small_strain_u_pw_diff_order_element.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_small_strain_u_pw_diff_order_element.cpp
@@ -16,7 +16,7 @@
 #include "tests/cpp_tests/geo_mechanics_fast_suite.h"
 #include "tests/cpp_tests/stub_constitutive_law.h"
 #include "tests/cpp_tests/test_utilities.h"
-#include "tests/cpp_tests/test_utilities/model_setup_utilities.h"
+#include "tests/cpp_tests/test_utilities/element_setup_utilities.h"
 
 #include <boost/numeric/ublas/assignment.hpp>
 
@@ -26,31 +26,27 @@ namespace Kratos::Testing
 KRATOS_TEST_CASE_IN_SUITE(SmallStrainUPwDiffOrderElement_CalculateShearCapacity, KratosGeoMechanicsFastSuiteWithoutKernel)
 {
     // Arrange
-    constexpr auto element_id   = std::size_t{1};
-    auto           p_properties = std::make_shared<Properties>();
-    p_properties->SetValue(CONSTITUTIVE_LAW, std::make_shared<StubConstitutiveLaw>());
-    p_properties->SetValue(GEO_COHESION, 2.0);
-    p_properties->SetValue(GEO_FRICTION_ANGLE, 0.0);
+    auto p_element = ElementSetupUtilities::Create2D6NDiffOrderElement();
 
-    auto element = SmallStrainUPwDiffOrderElement{
-        element_id, std::make_shared<Triangle2D6<Node>>(ModelSetupUtilities::Create2D6NTriangleGeometry()),
-        p_properties, std::make_unique<PlaneStrainStressState>()};
+    auto& p_properties = p_element->GetProperties();
+    p_properties.SetValue(CONSTITUTIVE_LAW, std::make_shared<StubConstitutiveLaw>());
+    p_properties.SetValue(GEO_COHESION, 2.0);
+    p_properties.SetValue(GEO_FRICTION_ANGLE, 0.0);
+
     const auto dummy_process_info = ProcessInfo{};
-    element.Initialize(dummy_process_info);
+    p_element->Initialize(dummy_process_info);
 
     auto stress_vector = Vector{4};
     stress_vector <<= -1.5, 0.0, 1.5, 0.0;
-    element.SetValuesOnIntegrationPoints(CAUCHY_STRESS_VECTOR,
-                                         std::vector<Vector>{3, stress_vector}, dummy_process_info);
+    p_element->SetValuesOnIntegrationPoints(
+        CAUCHY_STRESS_VECTOR, std::vector<Vector>{3, stress_vector}, dummy_process_info);
 
     // Act
     auto actual_shear_capacity_values = std::vector<double>{};
-    element.CalculateOnIntegrationPoints(GEO_SHEAR_CAPACITY, actual_shear_capacity_values, dummy_process_info);
+    p_element->CalculateOnIntegrationPoints(GEO_SHEAR_CAPACITY, actual_shear_capacity_values, dummy_process_info);
 
     // Assert
-    auto expected_shear_capacity_values = Vector{3};
-    expected_shear_capacity_values <<= 0.75, 0.75, 0.75;
-    KRATOS_EXPECT_VECTOR_NEAR(actual_shear_capacity_values, expected_shear_capacity_values,
+    KRATOS_EXPECT_VECTOR_NEAR(actual_shear_capacity_values, (Vector{ScalarVector{3, 0.75}}),
                               Defaults::absolute_tolerance);
 }
 
