8a010f2011275d46e1c643787256b6a55e477b1f
matekelemen <kelemen.mate.12@gmail.com>
2025-03-29 16:46:07 +0100

    add LinkConstraint

---
+++ applications/StructuralMechanicsApplication/custom_constraints/link_constraint.cpp
@@ -0,0 +1,298 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Mate Kelemen
+//
+
+// Project includes
+#include "custom_constraints/link_constraint.hpp" // LinkConstraint
+#include "includes/define.h"
+#include "includes/process_info.h"
+#include "includes/variables.h" // DISPLACEMENT_X, DISPLACEMENT_Y, DISPLACEMENT_Z
+
+// STL includes
+#include <algorithm> // std::max_element, std::equal, std::transform
+
+
+namespace Kratos {
+
+
+struct LinkConstraint::Impl
+{
+    using ValueType = double;
+
+    static void ComputeRelationMatrix(LinkConstraint::MatrixType& rRelationMatrix,
+                                      LinkConstraint::VectorType& rConstraintGaps,
+                                      const std::array<ValueType,6>& rLastPositions,
+                                      const std::array<ValueType,6>& rLastDisplacements,
+                                      const unsigned Dimensions)
+    {
+        // Make sure we don't try to allocate an underflown unsigned integer.
+        KRATOS_ERROR_IF_NOT(Dimensions);
+
+        // Set output sizes.
+        rRelationMatrix.resize(1, 2 * Dimensions);
+        rConstraintGaps.resize(1);
+        rConstraintGaps[0] = 0.0;
+
+        // Dof order:
+        // {u^0_x, ..., u^0_w, u^1_x, ..., u^1_w}
+        // Where w is the axis with the highest index for the provided dimensions
+        // (e.g.: z-axis in 3 dimensions).
+
+        for (std::size_t i_component=0u; i_component<Dimensions; ++i_component) {
+            rRelationMatrix(0, i_component) = 2 * (rLastPositions[i_component] - rLastPositions[i_component + Dimensions])
+                                              + rLastDisplacements[i_component] - rLastDisplacements[i_component + Dimensions];
+            rRelationMatrix(0, i_component + Dimensions) = -rRelationMatrix(i_component);
+        } // for i_component in range(mDimensions)
+    }
+
+
+    static LinkConstraint::DofPointerVectorType CollectDofs(Node& rFirst,
+                                                            Node& rSecond,
+                                                            const std::size_t Dimensions)
+    {
+        DofPointerVectorType dofs;
+
+        KRATOS_TRY
+        using ValueType = double;
+
+        const std::array<const Variable<ValueType>*,3> displacement_components {
+            &DISPLACEMENT_X,
+            &DISPLACEMENT_Y,
+            &DISPLACEMENT_Z
+        };
+
+        std::array<Node*, 2> nodes {&rFirst, &rSecond};
+        dofs.resize(2 * Dimensions);
+
+        // Collect dofs from nodes in the following order:
+        // {u^0_x, ..., u^0_w, u^1_x, ..., u^1_w}
+        // Where w is the axis with the highest index for the provided dimensions
+        // (e.g.: z-axis in 3 dimensions).
+        for (std::size_t i_dimension=0ul; i_dimension<Dimensions; ++i_dimension) {
+            const auto component_id = displacement_components[i_dimension]->Key();
+
+            for (std::size_t i_node=0ul; i_node<2; ++i_node) {
+                Node& r_node = *nodes[i_node];
+                const auto it_dof = std::find_if(r_node.GetDofs().begin(),
+                                                 r_node.GetDofs().end(),
+                                                 [component_id](auto& rp_dof){
+                                                         return rp_dof->GetVariable().Key() == component_id;
+                                                 });
+                KRATOS_ERROR_IF(it_dof == r_node.GetDofs().end())
+                    << "cannot find DoF " << displacement_components[i_dimension]->Name()
+                    << " in node " << r_node.Id();
+                dofs[i_node * Dimensions + i_dimension] = it_dof->get();
+            } // for i_node in range(2)
+        } // for i_dimension in range(Dimensions)
+        KRATOS_CATCH("")
+
+        return dofs;
+    }
+
+
+    void FetchNodePositions(std::array<ValueType,6>& rPositions,
+                            std::array<ValueType,6>& rDisplacements) const {
+        KRATOS_TRY
+
+        const std::array<const Variable<Impl::ValueType>*,3> displacement_components {
+            &DISPLACEMENT_X,
+            &DISPLACEMENT_Y,
+            &DISPLACEMENT_Z
+        };
+
+        for (std::size_t i_component=0u; i_component<mDimensions; ++i_component) {
+            for (std::size_t i_node=0u; i_node<2; ++i_node) {
+                const std::size_t i_dof = i_component + (i_node ? mDimensions : 0u);
+                rPositions[i_dof] = mNodePair[i_node]->Coordinates()[i_component];
+                rDisplacements[i_dof] = mNodePair[i_node]->GetSolutionStepValue(*displacement_components[i_component]);
+            } // for i_node in range(2)
+        } // for i_component in range(mDimensions)
+
+        KRATOS_CATCH("")
+    }
+
+
+    std::size_t mDimensions;
+
+    bool mIsMeshMoved;
+
+    /// @details MasterSlaveConstraint::GetSlaveDofsVector and MasterSlaveConstraint::GetMasterDofsVector
+    ///          require arrays of mutable Dof pointers, which are only obtainable from mutable nodes,
+    ///          so the nodes pointers stored here cannot be immutable. Risky business.
+    std::array<Node*,2> mNodePair;
+
+    std::array<ValueType,6> mLastPositions;
+
+    std::array<ValueType,6> mLastDisplacements;
+
+    LinkConstraint::MatrixType mRelationMatrix;
+
+    LinkConstraint::VectorType mConstraintGaps;
+}; // struct LinkConstraint::Impl
+
+
+LinkConstraint::LinkConstraint(const IndexType Id,
+                               Node& rFirst,
+                               Node& rSecond,
+                               const std::size_t Dimensions,
+                               bool IsMeshMoved)
+    : MultifreedomConstraint(/*Id               : */Id,
+                             /*rDofs            : */Impl::CollectDofs(rFirst, rSecond, Dimensions),
+                             /*rConstraintLabels: */std::vector<std::size_t> {Id}),
+      mpImpl(new Impl{/*mDimensions         : */Dimensions,
+                      /*mIsMeshMoved        : */IsMeshMoved,
+                      /*mNodePair           : */{&rFirst, &rSecond},
+                      /*mLastPositions      : */{0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+                      /*mLastDisplacements  : */{0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+                      /*mRelationMatrix     : */{},
+                      /*mConstraintGaps     : */{}})
+{
+}
+
+
+LinkConstraint::LinkConstraint(LinkConstraint&& rRhs) noexcept
+    : mpImpl(std::move(rRhs.mpImpl))
+{
+}
+
+
+LinkConstraint::LinkConstraint(const LinkConstraint& rRhs)
+    : mpImpl(new Impl(*rRhs.mpImpl))
+{
+}
+
+
+LinkConstraint& LinkConstraint::operator=(LinkConstraint&& rRhs) noexcept
+{
+    mpImpl = std::move(rRhs.mpImpl);
+    return *this;
+}
+
+
+LinkConstraint& LinkConstraint::operator=(const LinkConstraint& rRhs)
+{
+    mpImpl.reset(new Impl(*rRhs.mpImpl));
+    return *this;
+}
+
+
+LinkConstraint::~LinkConstraint() = default;
+
+
+MasterSlaveConstraint::Pointer LinkConstraint::Clone(IndexType NewId) const
+{
+    KRATOS_TRY
+    auto p_other = LinkConstraint::Pointer(
+        new LinkConstraint(NewId,
+                           *mpImpl->mNodePair.front(),
+                           *mpImpl->mNodePair.back(),
+                           mpImpl->mDimensions,
+                           mpImpl->mIsMeshMoved)
+    );
+    return p_other;
+    KRATOS_CATCH("")
+}
+
+
+void LinkConstraint::Initialize([[maybe_unused]] const ProcessInfo&)
+{
+}
+
+
+void LinkConstraint::InitializeSolutionStep(const ProcessInfo& rProcessInfo)
+{
+    KRATOS_TRY
+    // Store displacements before nonlinear iterations begin.
+    // At this point, displacements are at their last converged
+    // state, when the constraint is supposed to be still satisfied.
+    mpImpl->FetchNodePositions(mpImpl->mLastPositions,
+                               mpImpl->mLastDisplacements);
+
+    if (!mpImpl->mIsMeshMoved)
+        for (std::size_t i_component=0ul; i_component<mpImpl->mLastPositions.size(); ++i_component)
+            mpImpl->mLastPositions[i_component] += mpImpl->mLastDisplacements[i_component];
+
+    this->InitializeNonLinearIteration(rProcessInfo);
+    KRATOS_CATCH("")
+}
+
+
+void LinkConstraint::InitializeNonLinearIteration([[maybe_unused]] const ProcessInfo&)
+{
+    KRATOS_TRY
+    [[maybe_unused]] std::array<Impl::ValueType,6> dummy;
+    mpImpl->FetchNodePositions(dummy, mpImpl->mLastDisplacements);
+    Impl::ComputeRelationMatrix(mpImpl->mRelationMatrix,
+                                mpImpl->mConstraintGaps,
+                                mpImpl->mLastPositions,
+                                mpImpl->mLastDisplacements,
+                                mpImpl->mDimensions);
+    KRATOS_CATCH("")
+}
+
+
+void LinkConstraint::FinalizeNonLinearIteration([[maybe_unused]] const ProcessInfo&)
+{
+    const std::array<const Variable<Impl::ValueType>*,3> displacement_components {
+        &DISPLACEMENT_X,
+        &DISPLACEMENT_Y,
+        &DISPLACEMENT_Z
+    };
+    Impl::ValueType distance = 0;
+    for (unsigned i_component=0u; i_component<mpImpl->mDimensions; ++i_component) {
+        distance += std::pow(mpImpl->mNodePair[0]->Coordinates()[i_component] + mpImpl->mNodePair[0]->GetSolutionStepValue(*displacement_components[i_component])
+                             - mpImpl->mNodePair[1]->Coordinates()[i_component] - mpImpl->mNodePair[1]->GetSolutionStepValue(*displacement_components[i_component]), 2);
+    }
+    std::cout << "LinkConstraint: distance is " << distance << "\n";
+}
+
+
+void LinkConstraint::CalculateLocalSystem(MatrixType& rRelationMatrix,
+                                          VectorType& rConstraintGaps,
+                                          [[maybe_unused]] const ProcessInfo& rProcessInfo) const
+{
+    rRelationMatrix = mpImpl->mRelationMatrix;
+    rConstraintGaps = mpImpl->mConstraintGaps;
+}
+
+
+int LinkConstraint::Check(const ProcessInfo& rProcessInfo) const
+{
+    // Check whatever the base class checks.
+    MasterSlaveConstraint::Check(rProcessInfo);
+
+    // Restrict dimensions to 1, 2, or 3.
+    KRATOS_ERROR_IF_NOT(0 < mpImpl->mDimensions || mpImpl->mDimensions <= 3) << "unsupported dimensions (" << mpImpl->mDimensions << ")";
+
+    // Make sure that the nodes have the necessary Dofs.
+    const std::array<const Variable<Impl::ValueType>*,3> displacement_components {
+        &DISPLACEMENT_X,
+        &DISPLACEMENT_Y,
+        &DISPLACEMENT_Z
+    };
+
+    for (unsigned i_component=0u; i_component<mpImpl->mDimensions; ++i_component) {
+        for (unsigned i_node=0u; i_node<2; ++i_node) {
+            KRATOS_ERROR_IF_NOT(mpImpl->mNodePair[i_node]->HasDofFor(*displacement_components[i_component]))
+                << "node " << mpImpl->mNodePair[i_node]->Id()
+                << " has no Dof for " << displacement_components[i_component]->Name();
+        } // for i_node in range(2)
+    } // for i_component in range(mDimensions)
+
+    // Check for overlapping nodes.
+    constexpr Impl::ValueType tolerance = 1e-16;
+    KRATOS_ERROR_IF(norm_2(mpImpl->mNodePair.front()->Coordinates() - mpImpl->mNodePair.back()->Coordinates()) < tolerance)
+        << "coincident nodes in LinkConstraint";
+
+    return 0;
+}
+
+} // namespace Kratos
---
+++ applications/StructuralMechanicsApplication/custom_constraints/link_constraint.hpp
@@ -0,0 +1,106 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   license: StructuralMechanicsApplication/license.txt
+//
+//  Main authors:    Mate Kelemen
+//
+
+// Project includes
+#include "solving_strategies/builder_and_solvers/p_multigrid/multifreedom_constraint.hpp" // MultifreedomConstraint
+#include "includes/code_location.h" // KRATOS_CODE_LOCATION
+
+// STL includes
+#include <memory> // std::unique_ptr
+
+
+namespace Kratos {
+
+
+class LinkConstraint final : public MultifreedomConstraint
+{
+public:
+    KRATOS_CLASS_POINTER_DEFINITION(LinkConstraint);
+
+    LinkConstraint() noexcept = default;
+
+    LinkConstraint(const IndexType Id,
+                   Node& rFirst,
+                   Node& rSecond,
+                   const std::size_t Dimensions,
+                   bool IsMeshMoved);
+
+    LinkConstraint(LinkConstraint&& rRhs) noexcept;
+
+    LinkConstraint(const LinkConstraint& rRhs);
+
+    LinkConstraint& operator=(LinkConstraint&& rRhs) noexcept;
+
+    LinkConstraint& operator=(const LinkConstraint& rRhs);
+
+    ~LinkConstraint();
+
+    MasterSlaveConstraint::Pointer Clone(IndexType NewId) const override;
+
+    void Initialize(const ProcessInfo& rProcessInfo) override;
+
+    void InitializeSolutionStep(const ProcessInfo& rProcessInfo) override;
+
+    void InitializeNonLinearIteration(const ProcessInfo& rProcessInfo) override;
+
+    void FinalizeNonLinearIteration(const ProcessInfo& rProcessInfo) override;
+
+    void CalculateLocalSystem(MatrixType& rRelationMatrix,
+                              VectorType& rConstraintGaps,
+                              const ProcessInfo& rProcessInfo) const override;
+
+    int Check(const ProcessInfo& rProcessInfo) const override;
+
+    /// @name Unsupported Virtual Functions
+    /// @{
+
+    MasterSlaveConstraint::Pointer Create(IndexType,
+                                          DofPointerVectorType&,
+                                          DofPointerVectorType&,
+                                          const MatrixType&,
+                                          const VectorType&) const override
+    {KRATOS_ERROR << KRATOS_CODE_LOCATION.CleanFunctionName() << " is not supported by LinkConstraint.";}
+
+    MasterSlaveConstraint::Pointer Create(IndexType,
+                                          NodeType&,
+                                          const VariableType&,
+                                          NodeType&,
+                                          const VariableType&,
+                                          const double,
+                                          const double) const override
+    {KRATOS_ERROR << KRATOS_CODE_LOCATION.CleanFunctionName() << " is not supported by LinkConstraint.";}
+
+    void SetDofList(const DofPointerVectorType&,
+                    const DofPointerVectorType&,
+                    const ProcessInfo&) override
+    {KRATOS_ERROR << KRATOS_CODE_LOCATION.CleanFunctionName() << " is not supported by LinkConstraint.";}
+
+    void SetSlaveDofsVector(const DofPointerVectorType&) override
+    {KRATOS_ERROR << KRATOS_CODE_LOCATION.CleanFunctionName() << " is not supported by LinkConstraint.";}
+
+    void SetMasterDofsVector(const DofPointerVectorType&) override
+    {KRATOS_ERROR << KRATOS_CODE_LOCATION.CleanFunctionName() << " is not supported by LinkConstraint.";}
+
+    void SetLocalSystem(const MatrixType&,
+                        const VectorType&,
+                        const ProcessInfo&) override
+    {KRATOS_ERROR << KRATOS_CODE_LOCATION.CleanFunctionName() << " is not supported by LinkConstraint.";}
+
+    /// @}
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> mpImpl;
+}; // class LinkConstraint
+
+
+} // namespace Kratos
