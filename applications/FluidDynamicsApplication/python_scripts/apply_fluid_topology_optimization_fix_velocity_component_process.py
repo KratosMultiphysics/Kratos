88b25f9a6a4f95f862d8267f9bd729d0d53b59c1
Gianmarco Boscolo <gianmarcoboscolo@gmail.com>
2025-03-31 11:47:28 +0200

    Fix Velocity Component Process for FluidTopOpt

---
+++ applications/FluidDynamicsApplication/python_scripts/apply_fluid_topology_optimization_fix_velocity_component_process.py
@@ -0,0 +1,77 @@
+import KratosMultiphysics
+from KratosMultiphysics.assign_scalar_variable_process import AssignScalarVariableProcess
+
+def Factory(settings, Model):
+    if(type(settings) != KratosMultiphysics.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return ApplyFluidTopologyOptimizationFixVelocityComponentProcess(Model, settings["Parameters"])
+
+
+class ApplyFluidTopologyOptimizationFixVelocityComponentProcess(KratosMultiphysics.Process):
+    def __init__(self, Model, settings):
+
+        KratosMultiphysics.Process.__init__(self)
+        
+        default_settings = KratosMultiphysics.Parameters("""
+            {
+                "mesh_id"         : 0,
+                "model_part_name" : "please_specify_model_part_name",
+                "interval"        : [0.0,1e30],
+                "component"       : "Z",
+                "value_physics"   : 0.0,
+                "value_adjoint"   : 0.0
+            }
+            """)
+
+        # Check input data
+        if (settings["model_part_name"].GetString() == ""):
+            raise Exception("Empty inlet model part name string. Set a valid model part name.")
+        if (settings["component"].GetString().upper() not in ["X", "Y", "Z"]):
+            raise Exception("NOt valid component to fix.")
+        settings.ValidateAndAssignDefaults(default_settings)
+
+        mesh_id = settings["mesh_id"].GetInt()
+        model_part_name = settings["model_part_name"].GetString()
+        interval = settings["interval"].PrettyPrintJsonString()
+        component = settings["component"].GetString().upper()
+        value_physics = settings["value_physics"].GetDouble()
+        value_adjoint = settings["value_adjoint"].GetDouble()
+
+        velocity_settings = KratosMultiphysics.Parameters("""
+            {
+                "mesh_id"              : """ + str(mesh_id) + """,
+                "model_part_name"      : \"""" + model_part_name + """\",
+                "variable_name"        : "VELOCITY_""" + component + """\",
+                "interval"             : """ + interval + """,
+                "value"                : """ + str(value_physics) + """,
+                "constrained"          : true,
+                "local_axes"           : {}
+            }
+            """
+            )
+
+        adjoint_velocity_settings = KratosMultiphysics.Parameters("""
+            {
+                "mesh_id"              : """ + str(mesh_id) + """,
+                "model_part_name"      : \"""" + model_part_name + """\",
+                "variable_name"        : "VELOCITY_ADJ_""" + component + """\",
+                "interval"             : """ + interval + """,
+                "value"                : """ + str(value_adjoint) + """,
+                "constrained"          : true,
+                "local_axes"           : {}
+            }
+            """
+            )
+    
+        self.velocity_component_process = AssignScalarVariableProcess(Model, velocity_settings)
+        self.adjoint_velocity_component_process = AssignScalarVariableProcess(Model, adjoint_velocity_settings)
+
+
+    def ExecuteInitializeSolutionStep(self):
+        self.velocity_component_process.ExecuteInitializeSolutionStep()
+        self.adjoint_velocity_component_process.ExecuteInitializeSolutionStep()
+
+
+    def ExecuteFinalizeSolutionStep(self):
+        self.velocity_component_process.ExecuteFinalizeSolutionStep()
+        self.adjoint_velocity_component_process.ExecuteFinalizeSolutionStep()
