02540fb8f14d25fe189f596ae8f6383d8e21ac60
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-04-02 14:46:48 +0200

    Formatted the Python code using `black`

--- applications/GeoMechanicsApplication/python_scripts/run_geo_settlement.py
+++ applications/GeoMechanicsApplication/python_scripts/run_geo_settlement.py
@@ -1,13 +1,21 @@
 import argparse
 import sys
 
+
 def clean_up_api_object(api_object):
     del api_object
 
+
 def _main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--working-dir", help="working directory of the app", default=".")
-    parser.add_argument("project_parameters_file", help="'ProjectParameters.json' files of the stages", nargs="+")
+    parser.add_argument(
+        "-w", "--working-dir", help="working directory of the app", default="."
+    )
+    parser.add_argument(
+        "project_parameters_file",
+        help="'ProjectParameters.json' files of the stages",
+        nargs="+",
+    )
 
     args = parser.parse_args()
 
@@ -19,16 +27,27 @@ def _main() -> int:
     do_not_cancel = lambda: False
 
     import atexit
-    settlement_api = GeoMechanicsApplication.CustomWorkflowFactory.CreateKratosGeoSettlement()
+
+    settlement_api = (
+        GeoMechanicsApplication.CustomWorkflowFactory.CreateKratosGeoSettlement()
+    )
     atexit.register(clean_up_api_object, settlement_api)
 
     for project_parameters_filename in args.project_parameters_file:
-        status = settlement_api.RunStage(args.working_dir, project_parameters_filename, no_logging, no_progress_reporting, no_progress_message, do_not_cancel)
+        status = settlement_api.RunStage(
+            args.working_dir,
+            project_parameters_filename,
+            no_logging,
+            no_progress_reporting,
+            no_progress_message,
+            do_not_cancel,
+        )
         if status != 0:
             print(f"Running analysis stage failed: status = {status}")
             return status
 
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(_main())
