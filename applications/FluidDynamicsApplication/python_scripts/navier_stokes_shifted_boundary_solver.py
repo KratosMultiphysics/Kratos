56d5955a7b6bc0fb7be6b554bb9d86d1419848ca
frawahl <franzi_8.5@web.de>
2025-04-03 15:08:30 +0200

    update sbm solver to handle skin model parts

--- applications/FluidDynamicsApplication/python_scripts/navier_stokes_shifted_boundary_solver.py
+++ applications/FluidDynamicsApplication/python_scripts/navier_stokes_shifted_boundary_solver.py
@@ -1,8 +1,8 @@
 # Importing the Kratos Library
-import KratosMultiphysics
+import KratosMultiphysics as KM
 
 # Import applications
-import KratosMultiphysics.FluidDynamicsApplication as KratosCFD
+import KratosMultiphysics.FluidDynamicsApplication as KM_CFD
 
 # Import base class file
 from KratosMultiphysics.FluidDynamicsApplication.fluid_solver import FluidSolver
@@ -30,7 +30,7 @@ class ShiftedBoundaryFormulation(object):
             model_part.ProcessInfo[variable] = value
 
     def _SetUpShiftedBoundaryWeaklyCompressibleNavierStokes(self, formulation_settings):
-        default_settings = KratosMultiphysics.Parameters(r"""{
+        default_settings = KM.Parameters(r"""{
             "element_type"                 : "shifted_boundary_weakly_compressible_navier_stokes",
             "boundary_model_parts"         : [],
             "enclosed_areas"               : [],
@@ -56,12 +56,12 @@ class ShiftedBoundaryFormulation(object):
             raise Exception(err_msg)
         self.element_integrates_in_time = True
         self.element_has_nodal_properties = True
-        self.historical_nodal_properties_variables_list = [KratosMultiphysics.DENSITY]
-        self.non_historical_nodal_properties_variables_list = [KratosMultiphysics.SOUND_VELOCITY]
+        self.historical_nodal_properties_variables_list = [KM.DENSITY]
+        self.non_historical_nodal_properties_variables_list = [KM.SOUND_VELOCITY]
 
-        self.process_info_data[KratosMultiphysics.DYNAMIC_TAU] = formulation_settings["dynamic_tau"].GetDouble()
-        self.process_info_data[KratosMultiphysics.PENALTY_COEFFICIENT] = formulation_settings["penalty_coefficient"].GetDouble()
-        self.process_info_data[KratosCFD.SLIP_LENGTH] = formulation_settings["slip_length"].GetDouble()
+        self.process_info_data[KM.DYNAMIC_TAU] = formulation_settings["dynamic_tau"].GetDouble()
+        self.process_info_data[KM.PENALTY_COEFFICIENT] = formulation_settings["penalty_coefficient"].GetDouble()
+        self.process_info_data[KM_CFD.SLIP_LENGTH] = formulation_settings["slip_length"].GetDouble()
 
         # Get names of boundary model parts, whether they enclose and area and set post-processing computations for them
         self.skin_model_part_names = formulation_settings["boundary_model_parts"].GetStringArray()
@@ -92,7 +92,7 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
     @classmethod
     def GetDefaultParameters(cls):
         ##settings string in json format
-        default_settings = KratosMultiphysics.Parameters("""
+        default_settings = KM.Parameters("""
         {
             "solver_type": "shifted_boundary_from_defaults",
             "model_part_name": "",
@@ -160,46 +160,71 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
         self.postprocess_pressure = self.shifted_boundary_formulation.postprocess_pressure
         self.boundary_sub_model_part_name = self.shifted_boundary_formulation.boundary_sub_model_part_name
 
-        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Construction of NavierStokesShiftedBoundaryMonolithicSolver finished.")
+
+        # Create a skin model part
+        self.skin_model_parts = []
+        for skin_mp_name in self.skin_model_part_names:
+            self.model.CreateModelPart(skin_mp_name)
+
+        # Create a model part for skin integration points
+        self.skin_point_model_part = self.model.CreateModelPart("SkinPoints")
+
+        KM.Logger.PrintInfo(self.__class__.__name__, "Construction of NavierStokesShiftedBoundaryMonolithicSolver finished.")
 
     def AddVariables(self):
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.PRESSURE)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.VELOCITY)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.ACCELERATION)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.MESH_VELOCITY)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.BODY_FORCE)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.REACTION)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.REACTION_WATER_PRESSURE)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NORMAL)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.EXTERNAL_PRESSURE)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE)              # Use distance variable for node relocation and voting on positive/ negative side
-        self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.EMBEDDED_WET_PRESSURE)          # Post-process variable (stores the fluid nodes pressure and is set to 0 in the structure ones)
-        self.main_model_part.AddNodalSolutionStepVariable(KratosCFD.EMBEDDED_WET_VELOCITY)          # Post-process variable (stores the fluid nodes velocity and is set to 0 in the structure ones)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.PRESSURE)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.VELOCITY)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.ACCELERATION)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.MESH_VELOCITY)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.BODY_FORCE)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.NODAL_AREA)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.REACTION)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.REACTION_WATER_PRESSURE)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.NORMAL)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.EXTERNAL_PRESSURE)
+        self.main_model_part.AddNodalSolutionStepVariable(KM.DISTANCE)              # Use distance variable for node relocation and voting on positive/ negative side
+        self.main_model_part.AddNodalSolutionStepVariable(KM_CFD.EMBEDDED_WET_PRESSURE)          # Post-process variable (stores the fluid nodes pressure and is set to 0 in the structure ones)
+        self.main_model_part.AddNodalSolutionStepVariable(KM_CFD.EMBEDDED_WET_VELOCITY)          # Post-process variable (stores the fluid nodes velocity and is set to 0 in the structure ones)
 
         # Adding variables required for the nodal material properties
         if self.element_has_nodal_properties:
             for variable in self.historical_nodal_properties_variables_list:
                 self.main_model_part.AddNodalSolutionStepVariable(variable)
 
-        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Shifted-boundary fluid solver variables added correctly.")
+        # Add nodal variables to skin model part
+        for skin_mp in self.skin_model_parts:
+            skin_mp.AddNodalSolutionStepVariable(KM.VELOCITY)
+            skin_mp.AddNodalSolutionStepVariable(KM.DISPLACEMENT)
+            skin_mp.AddNodalSolutionStepVariable(KM.POSITIVE_FACE_PRESSURE)
+            skin_mp.AddNodalSolutionStepVariable(KM.NEGATIVE_FACE_PRESSURE)
+            skin_mp.AddNodalSolutionStepVariable(KM.POSITIVE_FACE_FLUID_VELOCITY)
+            skin_mp.AddNodalSolutionStepVariable(KM.NEGATIVE_FACE_FLUID_VELOCITY)
 
-    def AddDofs(self):
-        # Add formulation DOFs and reactions
-        super().AddDofs()
+        # Add nodal variables for skin integration points model part
+        self.skin_point_model_part.AddNodalSolutionStepVariable(KM.POSITIVE_FACE_PRESSURE)
+        self.skin_point_model_part.AddNodalSolutionStepVariable(KM.NEGATIVE_FACE_PRESSURE)
+        self.skin_point_model_part.AddNodalSolutionStepVariable(KM.POSITIVE_FACE_FLUID_VELOCITY)
+        self.skin_point_model_part.AddNodalSolutionStepVariable(KM.NEGATIVE_FACE_FLUID_VELOCITY)
+        self.skin_point_model_part.AddNodalSolutionStepVariable(KM.TRACTION_FROM_FLUID_PRESSURE)
+        self.skin_point_model_part.AddNodalSolutionStepVariable(KM.TRACTION_FROM_FLUID_STRESS)
+
+        KM.Logger.PrintInfo(self.__class__.__name__, "Shifted-boundary fluid solver variables added correctly.")
+
+    def ImportModelPart(self):
+        super(NavierStokesShiftedBoundaryMonolithicSolver, self).ImportModelPart()
 
     def PrepareModelPart(self):
-        if self.main_model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
+        if self.main_model_part.ProcessInfo[KM.IS_RESTARTED]:
             # Delete conditions related to interface utility - the interface utility and conditions will be recreated
             boundary_sub_model_part = self.GetComputingModelPart().GetSubModelPart(self.boundary_sub_model_part_name)
-            KratosMultiphysics.VariableUtils().SetFlag(KratosMultiphysics.TO_ERASE, True, boundary_sub_model_part.Conditions)
-            boundary_sub_model_part.RemoveConditions(KratosMultiphysics.TO_ERASE)
+            KM.VariableUtils().SetFlag(KM.TO_ERASE, True, boundary_sub_model_part.Conditions)
+            boundary_sub_model_part.RemoveConditions(KM.TO_ERASE)
 
-        # Call the base solver PrepareModelPart()
+        # Call the fluid solver PrepareModelPart()
         super(NavierStokesShiftedBoundaryMonolithicSolver, self).PrepareModelPart()
 
         # Set the extra requirements of the shifted-boundary formulation
-        if not self.main_model_part.ProcessInfo[KratosMultiphysics.IS_RESTARTED]:
+        if not self.main_model_part.ProcessInfo[KM.IS_RESTARTED]:
             # Set the shifted-boundary formulation configuration
             self.__SetShiftedBoundaryFormulation()
 
@@ -217,12 +242,25 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
 
         # Deactivate darcy term  #TODO necessary?
         # for ele in self.GetComputingModelPart().Elements:
-        #     ele.SetValue(KratosCFD.RESISTANCE, 0.0)
+        #     ele.SetValue(KM_CFD.RESISTANCE, 0.0)
 
         # Create shifted-boundary meshless interface utility and calculate extension operator requiring nodal and elemental neighbors
         self.__SetUpInterfaceUtility()
 
-        KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Solver initialization finished.")
+        KM.Logger.PrintInfo(self.__class__.__name__, "Solver initialization finished.")
+
+    def AdvanceInTime(self, current_time):
+        new_time = super(NavierStokesShiftedBoundaryMonolithicSolver, self).AdvanceInTime()
+
+        # Clone the solution step data for skin and skin points model parts
+        step = self.main_model_part.ProcessInfo[KM.STEP]
+        for skin_mp in self.skin_model_parts:
+            skin_mp.CloneTimeStep(new_time)
+            skin_mp.ProcessInfo[KM.STEP] = step
+        self.skin_point_model_part.CloneTimeStep(new_time)
+        self.skin_point_model_part.ProcessInfo[KM.STEP] = step
+
+        return new_time
 
     def InitializeSolutionStep(self):
         # Compute the BDF coefficients
@@ -245,24 +283,24 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
         super(NavierStokesShiftedBoundaryMonolithicSolver, self).FinalizeSolutionStep()
 
     def _SetNodalProperties(self):
-        set_density = KratosMultiphysics.DENSITY in self.historical_nodal_properties_variables_list
-        set_sound_velocity = KratosMultiphysics.SOUND_VELOCITY in self.non_historical_nodal_properties_variables_list
+        set_density = KM.DENSITY in self.historical_nodal_properties_variables_list
+        set_sound_velocity = KM.SOUND_VELOCITY in self.non_historical_nodal_properties_variables_list
 
         # Get density and dynamic viscosity from the properties of the first element
         for ele in self.main_model_part.Elements:
             # Get DENSITY from properties
             if set_density:
-                rho = ele.Properties.GetValue(KratosMultiphysics.DENSITY)
+                rho = ele.Properties.GetValue(KM.DENSITY)
                 if rho <= 0.0:
                     raise Exception("DENSITY set to {0} in Properties {1}, positive number expected.".format(rho,ele.Properties.Id))
             # Get SOUND_VELOCITY
             if set_sound_velocity:
-                if ele.Properties.Has(KratosMultiphysics.SOUND_VELOCITY):
-                    sound_velocity = ele.Properties.GetValue(KratosMultiphysics.SOUND_VELOCITY)
+                if ele.Properties.Has(KM.SOUND_VELOCITY):
+                    sound_velocity = ele.Properties.GetValue(KM.SOUND_VELOCITY)
                 else:
                     # NOTE this is the default sound velocity value to deactivate compressibility term of elements
                     sound_velocity = 1.0e+12
-                    KratosMultiphysics.Logger.PrintWarning('No \'SOUND_VELOCITY\' value found in Properties {0}. Setting default value {1}'.format(ele.Properties.Id, sound_velocity))
+                    KM.Logger.PrintWarning('No \'SOUND_VELOCITY\' value found in Properties {0}. Setting default value {1}'.format(ele.Properties.Id, sound_velocity))
                 if sound_velocity <= 0.0:
                     raise Exception("SOUND_VELOCITY set to {0} in Properties {1}, positive number expected.".format(sound_velocity, ele.Properties.Id))
             break
@@ -271,9 +309,9 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
 
         # Transfer the obtained properties to the nodes
         if set_density:
-            KratosMultiphysics.VariableUtils().SetVariable(KratosMultiphysics.DENSITY, rho, self.main_model_part.Nodes)
+            KM.VariableUtils().SetVariable(KM.DENSITY, rho, self.main_model_part.Nodes)
         if set_sound_velocity:
-            KratosMultiphysics.VariableUtils().SetNonHistoricalVariable(KratosMultiphysics.SOUND_VELOCITY, sound_velocity, self.main_model_part.Nodes)
+            KM.VariableUtils().SetNonHistoricalVariable(KM.SOUND_VELOCITY, sound_velocity, self.main_model_part.Nodes)
 
     def __SetShiftedBoundaryFormulation(self):
         # Save the formulation settings in the ProcessInfo
@@ -281,7 +319,7 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
 
     def __SetUpInterfaceUtility(self):
         # Create the boundary elements and MLS basis
-        settings = KratosMultiphysics.Parameters("""{}""")
+        settings = KM.Parameters("""{}""")
         settings.AddEmptyValue("model_part_name").SetString(self.main_model_part.Name + "." + self.GetComputingModelPart().Name)
         settings.AddEmptyValue("boundary_sub_model_part_name").SetString(self.boundary_sub_model_part_name)
         settings.AddEmptyValue("extension_operator_type").SetString(self.settings["formulation"]["extension_operator_type"].GetString())
@@ -289,10 +327,10 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
         settings.AddEmptyValue("sbm_interface_condition_name").SetString(self.sbm_interface_condition_name)
 
         #if self.level_set_type == "point-based":
-        #n_dim = self.main_model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE]
+        #n_dim = self.main_model_part.ProcessInfo[KM.DOMAIN_SIZE]
 
         # Calculate the required neighbors
-        elemental_neighbors_process = KratosMultiphysics.GenericFindElementalNeighboursProcess(self.main_model_part)
+        elemental_neighbors_process = KM.GenericFindElementalNeighboursProcess(self.main_model_part)
         elemental_neighbors_process.Execute()
 
         # Create an interface utility for all skin model part names
@@ -303,13 +341,13 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
             settings.AddEmptyValue("enclosed_area").SetString(enclosed_area)
 
             # Create interface utility
-            sbm_interface_utility = KratosMultiphysics.ShiftedBoundaryPointBasedInterfaceUtility(self.model, settings)
+            sbm_interface_utility = KM.ShiftedBoundaryPointBasedInterfaceUtility(self.model, settings)
             self.sbm_interface_utilities.append(sbm_interface_utility)
-            KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "New shifted-boundary point-based interface utility created for skin model part '" + skin_model_part_name + "'.")
+            KM.Logger.PrintInfo(self.__class__.__name__, "New shifted-boundary point-based interface utility created for skin model part '" + skin_model_part_name + "'.")
 
         if len(self.sbm_interface_utilities) == 1:
             self.sbm_interface_utilities[0].CalculateAndAddPointBasedInterface()
-            KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Extension operators were calculated and interface conditions added.")
+            KM.Logger.PrintInfo(self.__class__.__name__, "Extension operators were calculated and interface conditions added.")
 
         elif len(self.sbm_interface_utilities) > 1:
             # Interface flags should be reset for the volume/ computing model part once before skin model parts are (newly) embedded
@@ -332,13 +370,13 @@ class NavierStokesShiftedBoundaryMonolithicSolver(FluidSolver):
             # NOTE that the same boundary sub model part is being used here for all skin model parts and their utilities to add conditions
             for i_skin, sbm_interface_utility in enumerate(self.sbm_interface_utilities):
                 sbm_interface_utility.CalculateAndAddSkinIntegrationPointConditions()
-                KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Integration point conditions added for skin model part '" + self.skin_model_part_names[i_skin] + "'.")
+                KM.Logger.PrintInfo(self.__class__.__name__, "Integration point conditions added for skin model part '" + self.skin_model_part_names[i_skin] + "'.")
 
             #TODO Search for enclosed volumes and fix the pressure of one node if it has not been fixed yet? (instead of defining enclosed_areas)
             #sbm_interface_utilities[0].FixEnclosedVolumesPressure()
 
-            KratosMultiphysics.Logger.PrintInfo(self.__class__.__name__, "Extension operators were calculated and interface conditions added.")
+            KM.Logger.PrintInfo(self.__class__.__name__, "Extension operators were calculated and interface conditions added.")
 
         else:
-            KratosMultiphysics.Logger.PrintWarning('Shifted-boundary interface utility was not set up because no boundary model part was given.')
+            KM.Logger.PrintWarning('Shifted-boundary interface utility was not set up because no boundary model part was given.')
 
