40f866ee6b7e1505dfc6c24b620c56016ea33d44
markelov <gennady.markelov@deltares.nl>
2025-04-04 10:59:18 +0200

    used value_or

--- applications/GeoMechanicsApplication/custom_utilities/check_utilities.hpp
+++ applications/GeoMechanicsApplication/custom_utilities/check_utilities.hpp
@@ -24,12 +24,14 @@ namespace Kratos
 class CheckUtilities
 {
 public:
-    static void CheckDomainSize(double DomainSize, std::size_t Id, const std::optional<std::string>& PrintName = std::nullopt)
+    static void CheckDomainSize(double                            DomainSize,
+                                std::size_t                       Id,
+                                const std::optional<std::string>& PrintName = std::nullopt)
     {
         constexpr auto min_domain_size = 1.0e-15;
         KRATOS_ERROR_IF(DomainSize < min_domain_size)
-            << (PrintName.has_value() ? PrintName.value() : "DomainSize")
-        << " (" << DomainSize << ") is smaller than " << min_domain_size << " for element " << Id << std::endl;
+            << PrintName.value_or("DomainSize") << " (" << DomainSize << ") is smaller than "
+            << min_domain_size << " for element " << Id << std::endl;
     }
 
 }; /* Class CheckUtilities*/
