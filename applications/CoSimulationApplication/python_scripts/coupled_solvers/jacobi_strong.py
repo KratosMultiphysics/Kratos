2dcb7b501f9c3443fce52383194bb09421d936c8
Susanna Baars <s.baars@tu-bs.de>
2025-04-04 13:48:57 +0200

    Function ComputeLastAcceleratorDataPoint() added.

--- applications/CoSimulationApplication/python_scripts/coupled_solvers/jacobi_strong.py
+++ applications/CoSimulationApplication/python_scripts/coupled_solvers/jacobi_strong.py
@@ -102,6 +102,10 @@ class JacobiStrongCoupledSolver(CoSimulationCoupledSolver):
             is_converged = all([conv_crit.IsConverged() for conv_crit in self.convergence_criteria_list])
 
             if is_converged:
+                # Call the accelerator without performing an update afterwards. This is important if previous time steps are reused in the MVQN or the IQN-ILS method, as otherwise the last data point is not used.
+                for conv_acc in self.convergence_accelerators_list:
+                    conv_acc.ComputeLastAcceleratorDataPoint()
+
                 if self.echo_level > 0:
                     cs_tools.cs_print_info(self._ClassName(), colors.green("### CONVERGENCE WAS ACHIEVED ###"))
                 self.__CommunicateIfTimeStepNeedsToBeRepeated(False)
