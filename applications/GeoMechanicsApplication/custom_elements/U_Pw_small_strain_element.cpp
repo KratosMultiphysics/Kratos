b38b4c2dde1872d3579932516db9cf67c0f9d0c0
markelov208 <146726001+markelov208@users.noreply.github.com>
2025-04-07 21:27:07 +0200

    [GeoMechanicsApplication] Added CheckDomainSize (#13305)
    * added check utility for DomainSize
    
    * corrected error message
    
    * removed accidentally added space
    
    * used nullopt to remove a code smell
    
    * used value_or
    
    * split check_utilities.hpp into header and body
    
    * added unit test
    
    * removed pragma from cpp file

--- applications/GeoMechanicsApplication/custom_elements/U_Pw_small_strain_element.cpp
+++ applications/GeoMechanicsApplication/custom_elements/U_Pw_small_strain_element.cpp
@@ -13,6 +13,7 @@
 
 // Application includes
 #include "custom_elements/U_Pw_small_strain_element.hpp"
+#include "custom_utilities/check_utilities.h"
 #include "custom_utilities/constitutive_law_utilities.h"
 #include "custom_utilities/equation_of_motion_utilities.h"
 #include "custom_utilities/linear_nodal_extrapolator.h"
@@ -57,8 +58,7 @@ int UPwSmallStrainElement<TDim, TNumNodes>::Check(const ProcessInfo& rCurrentPro
     const PropertiesType& r_properties = this->GetProperties();
     const GeometryType&   r_geometry   = this->GetGeometry();
 
-    KRATOS_ERROR_IF(r_geometry.DomainSize() < 1.0e-15)
-        << "DomainSize < 1.0e-15 for the element " << this->Id() << std::endl;
+    CheckUtilities::CheckDomainSize(r_geometry.DomainSize(), this->Id());
 
     // Verify specific properties
     KRATOS_ERROR_IF_NOT(r_properties.Has(IGNORE_UNDRAINED))
