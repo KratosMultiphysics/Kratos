621ab9d792e0df5a7d085d2a845393533a98633a
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-04-14 10:49:26 +0200

    Check that all stress state policies are movable, but not copyable

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_three_dimensional_stress_state.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_three_dimensional_stress_state.cpp
@@ -21,6 +21,7 @@
 
 #include <boost/numeric/ublas/assignment.hpp>
 #include <string>
+#include <type_traits>
 
 using namespace Kratos;
 using namespace std::string_literals;
@@ -83,6 +84,13 @@ KRATOS_TEST_CASE_IN_SUITE(ThreeDimensionalStressState_ReturnsCorrectIntegrationC
     KRATOS_EXPECT_NEAR(calculated_coefficient, 1.0, 1e-5);
 }
 
+KRATOS_TEST_CASE_IN_SUITE(ThreeDimensionalStressState_CannotBeCopiedButItCanBeMoved,
+                          KratosGeoMechanicsFastSuiteWithoutKernel){
+    KRATOS_EXPECT_FALSE(std::is_copy_constructible_v<ThreeDimensionalStressState>)
+        KRATOS_EXPECT_FALSE(std::is_copy_assignable_v<ThreeDimensionalStressState>)
+            KRATOS_EXPECT_TRUE(std::is_move_constructible_v<ThreeDimensionalStressState>)
+                KRATOS_EXPECT_TRUE(std::is_move_assignable_v<ThreeDimensionalStressState>)}
+
 KRATOS_TEST_CASE_IN_SUITE(ThreeDimensionalStressState_GivesCorrectClone, KratosGeoMechanicsFastSuiteWithoutKernel)
 {
     const std::unique_ptr<StressStatePolicy> p_stress_state_policy =
@@ -90,6 +98,7 @@ KRATOS_TEST_CASE_IN_SUITE(ThreeDimensionalStressState_GivesCorrectClone, KratosG
     const auto p_clone = p_stress_state_policy->Clone();
 
     KRATOS_EXPECT_NE(dynamic_cast<ThreeDimensionalStressState*>(p_clone.get()), nullptr);
+    KRATOS_EXPECT_NE(p_clone.get(), p_stress_state_policy.get());
 }
 
 KRATOS_TEST_CASE_IN_SUITE(ThreeDimensionalStressState_CalculateGreenLagrangeStrainReturnsCorrectResults,
