70c9032b11945184cc769605ef0411ebe459bb70
Luz Paz <luzpaz@pm.me>
2025-05-09 19:11:46 -0400

    Fix typos in applications/PfemFluidDynamicsApplication Found via codespell

--- applications/PfemFluidDynamicsApplication/custom_elements/two_step_updated_lagrangian_V_P_implicit_element.h
+++ applications/PfemFluidDynamicsApplication/custom_elements/two_step_updated_lagrangian_V_P_implicit_element.h
@@ -126,7 +126,7 @@ namespace Kratos
 
     // Constructors.
 
-    /// Default constuctor.
+    /// Default constructor.
     /**
      * @param NewId Index number of the new element (optional)
      */
@@ -152,7 +152,7 @@ namespace Kratos
     {
     }
 
-    /// Constuctor using geometry and properties.
+    /// Constructor using geometry and properties.
     /**
      * @param NewId Index of the new element
      * @param pGeometry Pointer to a geometry object
