1470b47a61146ffe1264b498fb15f588612856c9
Luz Paz <luzpaz@pm.me>
2025-05-12 09:07:57 -0400

    Fix typos in applications/StructuralMechanicsApplication Found via codespell

--- applications/StructuralMechanicsApplication/custom_elements/solid_elements/base_solid_element.h
+++ applications/StructuralMechanicsApplication/custom_elements/solid_elements/base_solid_element.h
@@ -228,7 +228,7 @@ public:
     void FinalizeNonLinearIteration(const ProcessInfo& rCurrentProcessInfo) override;
 
     /**
-     * @brief Called at the end of eahc solution step
+     * @brief Called at the end of each solution step
      * @param rCurrentProcessInfo the current process info instance
      */
     void FinalizeSolutionStep(const ProcessInfo& rCurrentProcessInfo) override;
