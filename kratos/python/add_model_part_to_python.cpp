0ec68f76ca5496eed4f34e9d0fb6c21a9654ff01
Ricky Aristio <ricky.aristio@gmail.com>
2025-05-13 10:32:13 +0200

    add number of geometries function

--- kratos/python/add_model_part_to_python.cpp
+++ kratos/python/add_model_part_to_python.cpp
@@ -489,6 +489,11 @@ void ModelPartRemoveConditionsFromAllLevels(ModelPart& rModelPart, Flags identif
 
 // Geometries
 
+ModelPart::SizeType ModelPartNumberOfGeometries1(ModelPart& rModelPart)
+{
+    return rModelPart.NumberOfGeometries();
+}
+
 void ModelPartAddGeometry1(ModelPart& rModelPart, ModelPart::GeometryType::Pointer pNewGeometry)
 {
     rModelPart.AddGeometry(pNewGeometry);
@@ -796,6 +801,7 @@ void AddModelPartToPython(pybind11::module& m)
         .def("NumberOfElements", &ModelPart::NumberOfElements)
         .def("NumberOfConditions", ModelPartNumberOfConditions1)
         .def("NumberOfConditions", &ModelPart::NumberOfConditions)
+        .def("NumberOfGeometries", ModelPartNumberOfGeometries1)
         .def("NumberOfGeometries", &ModelPart::NumberOfGeometries)
         .def("NumberOfMasterSlaveConstraints", ModelPartNumberOfMasterSlaveConstraints1)
         .def("NumberOfMasterSlaveConstraints", &ModelPart::NumberOfMasterSlaveConstraints)
