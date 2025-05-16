55dad50052b5b9196511c4667df11835257660ae
Vicente Mataix Ferrandiz <vmataix@altair.com>
2025-05-16 08:26:57 +0200

    [MestisApplication] Transition changes for constraint support

--- applications/MetisApplication/custom_utilities/legacy_partitioning_utilities.h
+++ applications/MetisApplication/custom_utilities/legacy_partitioning_utilities.h
@@ -8,12 +8,12 @@
 //                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//                   Vicente Mataix Ferrandiz
 //
 
 #pragma once
 
 // System includes
-
 
 // External includes
 #include "metis.h"
@@ -22,18 +22,25 @@
 // Project includes
 #include "includes/io.h"
 
-
 namespace Kratos {
 
 ///@addtogroup MetisApplication
 ///@{
 
+///@}
 ///@name Kratos Classes
 ///@{
 
-/// This class contains legacy versions of utilities used by the metis partitioners.
-/** The new impleementation of these functionalities is part of the MetisPartitioningUtilities.
-*/
+/**
+ * @class LegacyPartitioningUtilities
+ * @ingroup MetisApplication
+ * @brief A utility class for legacy partitioning operations in the MetisApplication.
+ * @details This class provides static methods for performing various partitioning-related operations,
+ * such as calculating domain graphs, dividing nodes, elements, and conditions among partitions,
+ * and converting Kratos format connectivities to CSR format.
+ * @note The new implementation of these functionalities is part of the MetisPartitioningUtilities.
+ * @author Philipp Bucher
+ */
 class KRATOS_API(METIS_APPLICATION) LegacyPartitioningUtilities
 {
 public:
@@ -43,62 +50,160 @@ public:
     /// Pointer definition of LegacyPartitioningUtilities
     KRATOS_CLASS_POINTER_DEFINITION(LegacyPartitioningUtilities);
 
-    using idxtype = idx_t; // from metis
+    /// Type alias for Metis index type.
+    using idxtype = idx_t;
+
+    /// Type alias for partition indices.
     using PartitionIndicesType = std::vector<idxtype>;
+
+    /// Type alias for index type.
+    using IndexType = std::size_t;
+
+    /// Type alias for size type.
     using SizeType = std::size_t;
 
     ///@}
     ///@name Life Cycle
     ///@{
 
-    /// Default constructor.
+    /**
+     * @brief Default constructor is deleted to prevent instantiation.
+     */
     LegacyPartitioningUtilities() = delete;
 
-    /// Copy constructor.
+    /**
+     * @brief Copy constructor is deleted to prevent copying.
+     */
     LegacyPartitioningUtilities(LegacyPartitioningUtilities const& rOther) = delete;
 
     ///@}
     ///@name Operators
     ///@{
 
-    /// Assignment operator.
+    /**
+     * @brief Assignment operator is deleted to prevent assignment.
+     */
     LegacyPartitioningUtilities& operator=(LegacyPartitioningUtilities const& rOther) = delete;
 
     ///@}
     ///@name Operations
     ///@{
 
+    /**
+     * @brief Calculates the domains graph for partitioning.
+     * @param rDomainsGraph The resulting domains graph.
+     * @param NumberOfElements The number of elements in the graph.
+     * @param ElementsConnectivities The connectivities of the elements.
+     * @param NPart The node partitions.
+     * @param EPart The element partitions.
+     */
     static void CalculateDomainsGraph(
         IO::GraphType& rDomainsGraph,
         SizeType NumberOfElements,
         IO::ConnectivitiesContainerType& ElementsConnectivities,
         PartitionIndicesType const& NPart,
-        PartitionIndicesType const&  EPart);
+        PartitionIndicesType const& EPart
+        );
 
+    /**
+     * @brief Divides nodes among partitions.
+     * @details Legacy version not including geometries and constraints.
+     * @param rNodesAllPartitions The resulting nodes for all partitions.
+     * @param rElementsConnectivities The connectivities of the elements.
+     * @param rConditionsConnectivities The connectivities of the conditions.
+     * @param rNodesPartitions The node partitions.
+     * @param rElementsPartitions The element partitions.
+     * @param rConditionsPartitions The condition partitions.
+     */
     static void DividingNodes(
         IO::PartitionIndicesContainerType& rNodesAllPartitions,
-        IO::ConnectivitiesContainerType& ElementsConnectivities,
-        IO::ConnectivitiesContainerType& ConditionsConnectivities,
-        PartitionIndicesType const& NodesPartitions,
-        PartitionIndicesType const& ElementsPartitions,
-        PartitionIndicesType const& ConditionsPartitions);
+        IO::ConnectivitiesContainerType& rElementsConnectivities,
+        IO::ConnectivitiesContainerType& rConditionsConnectivities,
+        const PartitionIndicesType& rNodesPartitions,
+        const PartitionIndicesType& rElementsPartitions,
+        const PartitionIndicesType& rConditionsPartitions
+        );
 
+    /**
+     * @brief Divides nodes among partitions including geometries and constraints.
+     * @param rNodesAllPartitions The resulting nodes for all partitions.
+     * @param rGeometriesAllPartitions The resulting geometries for all partitions.
+     * @param rElementsConnectivities The connectivities of the elements.
+     * @param rConditionsConnectivities The connectivities of the conditions.
+     * @param rMasterSlaveConstraintsAllPartitions The resulting constraints for all partitions.
+     * @param rNodesPartitions The node partitions.
+     * @param rGeometriesPartitions The geometry partitions.
+     * @param rElementsPartitions The element partitions.
+     * @param rConditionsPartitions The condition partitions.
+     * @param rMasterSlaveConstraintsPartitions The constraints partitions.
+     */
+    static void DividingNodes(
+        IO::PartitionIndicesContainerType& rNodesAllPartitions,
+        IO::PartitionIndicesContainerType& rGeometriesAllPartitions,
+        IO::ConnectivitiesContainerType& rElementsConnectivities,
+        IO::ConnectivitiesContainerType& rConditionsConnectivities,
+        IO::PartitionIndicesContainerType& rMasterSlaveConstraintsAllPartitions,
+        const PartitionIndicesType& rNodesPartitions,
+        const PartitionIndicesType& rGeometriesPartitions,
+        const PartitionIndicesType& rElementsPartitions,
+        const PartitionIndicesType& rConditionsPartitions,
+        const PartitionIndicesType& rMasterSlaveConstraintsPartitions
+        );
+
+    /**
+     * @brief Divides elements among partitions.
+     * @param rGeometriesAllPartitions The resulting geometries for all partitions.
+     * @param rGeometriesPartitions The geometry partitions.
+     */
+    static void DividingGeometries(
+        IO::PartitionIndicesContainerType& rGeometriesAllPartitions,
+        const PartitionIndicesType& rGeometriesPartitions
+        );
+
+    /**
+     * @brief Divides elements among partitions.
+     * @param rElementsAllPartitions The resulting elements for all partitions.
+     * @param ElementsPartitions The element partitions.
+     */
     static void DividingElements(
         IO::PartitionIndicesContainerType& rElementsAllPartitions,
-        PartitionIndicesType const& ElementsPartitions);
+        PartitionIndicesType const& ElementsPartitions
+        );
 
+    /**
+     * @brief Divides conditions among partitions.
+     * @param rConditionsAllPartitions The resulting conditions for all partitions.
+     * @param ConditionsPartitions The condition partitions.
+     */
     static void DividingConditions(
         IO::PartitionIndicesContainerType& rConditionsAllPartitions,
-        PartitionIndicesType const& ConditionsPartitions);
+        PartitionIndicesType const& ConditionsPartitions
+        );
 
+    /**
+     * @brief Divides constraints among partitions.
+     * @param rMasterSlaveConstraintsAllPartitions The resulting constraints for all partitions.
+     * @param rMasterSlaveConstraintsPartitions The constraint partitions.
+     */
+    static void DividingMasterSlaveConstraints(
+        IO::PartitionIndicesContainerType& rMasterSlaveConstraintsAllPartitions,
+        const PartitionIndicesType& rMasterSlaveConstraintsPartitions
+        );
+
+    /**
+     * @brief Converts Kratos format connectivities to CSR format.
+     * @param KratosFormatNodeConnectivities The Kratos format node connectivities.
+     * @param NodeIndices The resulting CSR format node indices.
+     * @param NodeConnectivities The resulting CSR format node connectivities.
+     */
     static void ConvertKratosToCSRFormat(
         IO::ConnectivitiesContainerType& KratosFormatNodeConnectivities,
         idxtype** NodeIndices,
-        idxtype** NodeConnectivities);
+        idxtype** NodeConnectivities
+        );
 
     ///@}
-
-}; // Class LegacyPartitioningUtilities
+};
 
 ///@}
 
