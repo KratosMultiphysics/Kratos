0dec081a91be3a9d785bad59918afbea0e018a26
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2025-05-16 11:35:38 +0200

    Remove EXTENDED_GAUSS

--- kratos/integration/integration_info.h
+++ kratos/integration/integration_info.h
@@ -60,7 +60,6 @@ public:
     {
         Default, // Default do nothing integration rule
         GAUSS, // Standard Gauss integration rule
-        EXTENDED_GAUSS, //FIXME: Messy and misleading stuff to be removed in an upcoming PR
         LOBATTO, // Standard Lobatto integration rule
         GRID, // Equally-spaced integration points rule (not suitable for all geometries)
         CUSTOM // Custom integration rule (e.g., integration points coming from QuESo)
