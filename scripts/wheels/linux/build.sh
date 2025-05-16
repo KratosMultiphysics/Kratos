19fc590b559ab2aa2b769b3789e81b7f07356bdc
unknown <roigcarlo@gmail.com>
2025-05-16 09:28:25 +0200

    Bumping version to synch master

--- scripts/wheels/linux/build.sh
+++ scripts/wheels/linux/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PYTHONS=("cp38" "cp39" "cp310" "cp311" "cp12" "cp13")
-export KRATOS_VERSION="10.2.0"
+export KRATOS_VERSION="10.2.2"
 
 BASE_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export KRATOS_ROOT="/workspace/kratos/Kratos"
