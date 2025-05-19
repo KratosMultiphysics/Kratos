f795b4572d14795ca14cad56073841428340e9e0
Richard Faasse <richard.faasse@deltares.nl>
2025-05-19 09:58:39 +0200

    Moved the predict from dynamic to the generalized newmark scheme

--- applications/GeoMechanicsApplication/custom_strategies/schemes/generalized_newmark_scheme.hpp
+++ applications/GeoMechanicsApplication/custom_strategies/schemes/generalized_newmark_scheme.hpp
@@ -27,6 +27,7 @@ public:
     using BaseType          = Scheme<TSparseSpace, TDenseSpace>;
     using TSystemMatrixType = typename BaseType::TSystemMatrixType;
     using TSystemVectorType = typename BaseType::TSystemVectorType;
+    using DofsArrayType         = typename BaseType::DofsArrayType;
 
     GeneralizedNewmarkScheme(const std::vector<FirstOrderScalarVariable>& rFirstOrderScalarVariables,
                              const std::vector<SecondOrderVectorVariable>& rSecondOrderVectorVariables,
@@ -103,6 +104,74 @@ public:
         }
 
         KRATOS_CATCH("")
+    }
+
+        void Predict(ModelPart& rModelPart, DofsArrayType& rDofSet, TSystemMatrixType& A, TSystemVectorType& Dx, TSystemVectorType& b) override
+    {
+        KRATOS_TRY
+
+        PredictVariables(rModelPart);
+        // Update (Angular) Acceleration, (Angular) Velocity and DtPressure
+        this->UpdateVariablesDerivatives(rModelPart);
+
+        KRATOS_CATCH("")
+    }
+
+    void PredictVariables(const ModelPart& rModelPart)
+    {
+        block_for_each(rModelPart.Nodes(), [this](Node& rNode) { PredictVariablesForNode(rNode); });
+    }
+
+    void PredictVariablesForNode(Node& rNode)
+    {
+        for (const auto& r_second_order_vector_variable : this->GetSecondOrderVectorVariables()) {
+            if (!rNode.SolutionStepsDataHas(r_second_order_vector_variable.instance)) continue;
+            PredictVariableForNode(rNode, r_second_order_vector_variable);
+        }
+    }
+
+    void PredictVariableForNode(Node& rNode, const SecondOrderVectorVariable& rSecondOrderVariables)
+    {
+        const std::vector<std::string> components = {"X", "Y", "Z"};
+
+        for (const auto& component : components) {
+            const auto& instance_component = VariablesUtilities::GetComponentFromVectorVariable(
+                rSecondOrderVariables.instance.Name(), component);
+
+            if (!rNode.HasDofFor(instance_component)) continue;
+
+            const auto& first_time_derivative_component = VariablesUtilities::GetComponentFromVectorVariable(
+                rSecondOrderVariables.first_time_derivative.Name(), component);
+            const auto& second_time_derivative_component = VariablesUtilities::GetComponentFromVectorVariable(
+                rSecondOrderVariables.second_time_derivative.Name(), component);
+
+            const double previous_variable = rNode.FastGetSolutionStepValue(instance_component, 1);
+            const double current_first_time_derivative =
+                rNode.FastGetSolutionStepValue(first_time_derivative_component, 0);
+            const double previous_first_time_derivative =
+                rNode.FastGetSolutionStepValue(first_time_derivative_component, 1);
+            const double current_second_time_derivative =
+                rNode.FastGetSolutionStepValue(second_time_derivative_component, 0);
+            const double previous_second_time_derivative =
+                rNode.FastGetSolutionStepValue(second_time_derivative_component, 1);
+            if (rNode.IsFixed(second_time_derivative_component)) {
+                rNode.FastGetSolutionStepValue(instance_component) =
+                    previous_variable + this->GetDeltaTime() * previous_first_time_derivative +
+                    this->GetDeltaTime() * this->GetDeltaTime() *
+                        ((0.5 - this->GetBeta()) * previous_second_time_derivative +
+                         this->GetBeta() * current_second_time_derivative);
+            } else if (rNode.IsFixed(first_time_derivative_component)) {
+                rNode.FastGetSolutionStepValue(instance_component) =
+                    previous_variable +
+                    this->GetDeltaTime() * ((this->GetBeta() / this->GetGamma()) *
+                                                (current_first_time_derivative - previous_first_time_derivative) +
+                                            previous_first_time_derivative);
+            } else if (!rNode.IsFixed(instance_component)) {
+                rNode.FastGetSolutionStepValue(instance_component) =
+                    previous_variable + this->GetDeltaTime() * previous_first_time_derivative +
+                    0.5 * this->GetDeltaTime() * this->GetDeltaTime() * previous_second_time_derivative;
+            }
+        }
     }
 
 protected:
