16fb6cadf914a95c1a2f0672db8f21d0e4bf7b9a
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-05-20 11:48:42 +0200

    Cleaned up class `SmallStrainUDSM2DPlaneStrainLaw`
    - All implementations are now in the `.cpp` file.
    - Removed many redundant comments.
    - Marked a few members `[[nodiscard]]`.

--- applications/GeoMechanicsApplication/custom_constitutive/small_strain_udsm_2D_plane_strain_law.hpp
+++ applications/GeoMechanicsApplication/custom_constitutive/small_strain_udsm_2D_plane_strain_law.hpp
@@ -12,187 +12,35 @@
 
 #pragma once
 
-// System includes
-#include "includes/define.h"
-
-// Project includes
 #include "custom_constitutive/small_strain_udsm_3D_law.hpp"
+#include "includes/define.h"
 
 namespace Kratos
 {
-///@addtogroup ConstitutiveModelsApplication
-///@{
 
-///@name Kratos Globals
-///@{
-
-///@}
-///@name Type Definitions
-///@{
-
-///@}
-///@name  Enum's
-///@{
-
-///@}
-///@name  Functions
-///@{
-
-///@}
-///@name Kratos Classes
-///@{
-
-/// Short class definition.
-/** Detail class definition.
- */
 class KRATOS_API(GEO_MECHANICS_APPLICATION) SmallStrainUDSM2DPlaneStrainLaw : public SmallStrainUDSM3DLaw
 {
 public:
-    // The base class ConstitutiveLaw type definition
     using BaseType = ConstitutiveLaw;
-
-    /// The size type definition
     using SizeType = std::size_t;
 
-    /// Static definition of the dimension
-    static constexpr SizeType Dimension = N_DIM_2D;
-
-    /// Pointer definition of SmallStrainUDSM2DPlaneStrainLaw
     KRATOS_CLASS_POINTER_DEFINITION(SmallStrainUDSM2DPlaneStrainLaw);
 
-    //@}
-    //@name Life Cycle
-    //@{
-
-    /**
-     * @brief Clone method
-     */
     [[nodiscard]] ConstitutiveLaw::Pointer Clone() const override;
 
-    /**
-     * @brief Dimension of the law:
-     */
-    SizeType WorkingSpaceDimension() override { return Dimension; }
+    SizeType WorkingSpaceDimension() override;
 
-    /**
-     * @brief Voigt tensor size:
-     */
     [[nodiscard]] SizeType GetStrainSize() const override;
 
-    ///@}
-    ///@name Inquiry
-    ///@{
+    [[nodiscard]] std::string Info() const override;
 
-    ///@}
-    ///@name Input and output
-    ///@{
-
-    /// Turn back information as a string.
-    std::string Info() const override { return "SmallStrainUDSM2DPlaneStrainLaw"; }
-
-    /// Print information about this object.
-    void PrintInfo(std::ostream& rOStream) const override { rOStream << Info(); }
-
-    /// Print object's data.
-    void PrintData(std::ostream& rOStream) const override
-    {
-        rOStream << "SmallStrainUDSM2DPlaneStrainLaw Data";
-    }
-
-    ///@}
-    ///@name Friends
-    ///@{
-
-    ///@}
-
-protected:
-    ///@name Protected static Member Variables
-    ///@{
-
-    ///@}
-    ///@name Protected member Variables
-    ///@{
-
-    ///@}
-    ///@name Protected Operators
-    ///@{
-
-    ///@}
-    ///@name Protected Operations
-    ///@{
-
-    ///@}
-    ///@name Protected  Access
-    ///@{
-
-    ///@}
-    ///@name Protected Inquiry
-    ///@{
-
-    ///@}
-    ///@name Protected LifeCycle
-    ///@{
-
-    ///@}
+    void PrintInfo(std::ostream& rOStream) const override;
+    void PrintData(std::ostream& rOStream) const override;
 
 private:
-    ///@name Static Member Variables
-    ///@{
-
-    ///@}
-    ///@name Member Variables
-    ///@{
-
-    ///@}
-    ///@name Private Operators
-    ///@{
-
-    ///@}
-    ///@name Private Operations
-    ///@{
-
-    ///@}
-    ///@name Private  Access
-    ///@{
-
-    ///@}
-    ///@name Serialization
-    ///@{
     friend class Serializer;
-
-    void save(Serializer& rSerializer) const override
-    {
-        KRATOS_SERIALIZE_SAVE_BASE_CLASS(rSerializer, ConstitutiveLaw)
-    }
-
-    void load(Serializer& rSerializer) override
-    {
-        KRATOS_SERIALIZE_LOAD_BASE_CLASS(rSerializer, ConstitutiveLaw)
-    }
-
-    ///@}
-    ///@name Private Inquiry
-    ///@{
-
-    ///@}
-    ///@name Un accessible methods
-    ///@{
-
-    ///@}
-
+    void save(Serializer& rSerializer) const override;
+    void load(Serializer& rSerializer) override;
 }; // Class SmallStrainUDSM3DLaw
-
-///@}
-
-///@name Type Definitions
-///@{
-
-///@}
-///@name Input and output
-///@{
-
-///@}
-
-///@} addtogroup block
 
 } // namespace Kratos
