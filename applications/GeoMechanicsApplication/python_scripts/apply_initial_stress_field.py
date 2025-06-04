8877f629ffec2ca7647648d38c76ad6cf8f70109
Richard Faasse <richard.faasse@deltares.nl>
2025-06-04 16:57:21 +0200

    Added python process

---
+++ applications/GeoMechanicsApplication/python_scripts/apply_initial_stress_field.py
@@ -0,0 +1,12 @@
+import KratosMultiphysics as Core
+import KratosMultiphysics.GeoMechanicsApplication as Geo
+
+def Factory(settings, model):
+    if not isinstance(settings, Core.Parameters):
+        raise TypeError("expected input shall be a Parameters object, encapsulating a json string")
+
+    model_part = model[settings["Parameters"]["model_part_name"].GetString()]
+    return Geo.ApplyInitialStressField(model_part, settings["Parameters"])
+
+
+
