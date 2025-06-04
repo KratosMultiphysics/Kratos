ff456b4bdb998bea29c1f68ef6e38c236c20099c
NickNick9 <nicolo.antonelli@upc.edu>
2025-06-04 14:28:05 +0200

    ready for PR

--- applications/IgaApplication/custom_modelers/nurbs_geometry_modeler_sbm.cpp
+++ applications/IgaApplication/custom_modelers/nurbs_geometry_modeler_sbm.cpp
@@ -49,20 +49,36 @@ void NurbsGeometryModelerSbm::CreateAndAddRegularGrid2D(
                                 ? mpModel->GetModelPart(iga_model_part_name)
                                 : mpModel->CreateModelPart(iga_model_part_name);
 
-    // Create the True Model Part -> contains all the true boundary features
-    std::string skin_model_part_name;
-    std::string skin_model_part_inner_initial_name = mParameters["skin_model_part_inner_initial_name"].GetString();
-    std::string skin_model_part_outer_initial_name = mParameters["skin_model_part_outer_initial_name"].GetString();
+    // compute unique_knot_vector_u
+    Vector unique_knot_vector_u(2+(NumKnotSpansU-1));
+    unique_knot_vector_u[0] = mKnotVectorU[0]; 
+    unique_knot_vector_u[NumKnotSpansU] = mKnotVectorU[mKnotVectorU.size()-1];
+    for (SizeType i_knot_insertion = 0; i_knot_insertion < NumKnotSpansU-1; i_knot_insertion++) {
+        unique_knot_vector_u[i_knot_insertion+1] = mInsertKnotsU[i_knot_insertion];
+    }
+    // compute unique_knot_vector_v
+    Vector unique_knot_vector_v(2+(NumKnotSpansV-1));
+    unique_knot_vector_v[0] = mKnotVectorV[0]; 
+    unique_knot_vector_v[NumKnotSpansV] = mKnotVectorV[mKnotVectorV.size()-1];
+    for (SizeType i_knot_insertion = 0; i_knot_insertion < NumKnotSpansV-1; i_knot_insertion++) {
+        unique_knot_vector_v[i_knot_insertion+1] = mInsertKnotsV[i_knot_insertion];
+    }
+    // Set the value of the knot vectors
+    r_iga_model_part.SetValue(KNOT_VECTOR_U, unique_knot_vector_u);
+    r_iga_model_part.SetValue(KNOT_VECTOR_V, unique_knot_vector_v);
 
-    // Create the surrogate sub model parts inner and outer
-    ModelPart& surrogate_sub_model_part_inner = r_iga_model_part.CreateSubModelPart("surrogate_inner");
-    ModelPart& surrogate_sub_model_part_outer = r_iga_model_part.CreateSubModelPart("surrogate_outer");
-
-    // If there is not neither skin_inner nor skin_outer throw a warning since you are using the sbm modeler
+    // If neither skin_inner nor skin_outer exists
     if (!(mParameters.Has("skin_model_part_inner_initial_name") || mParameters.Has("skin_model_part_outer_initial_name"))){
-    
+        
+        Vector knot_step_uv= ZeroVector(2);
+        knot_step_uv[0] = std::abs(unique_knot_vector_u[std::ceil(unique_knot_vector_u.size()/2) +1] - unique_knot_vector_u[std::ceil(unique_knot_vector_u.size()/2)] ) ;
+        knot_step_uv[1] = std::abs(unique_knot_vector_v[std::ceil(unique_knot_vector_v.size()/2) +1] - unique_knot_vector_v[std::ceil(unique_knot_vector_v.size()/2)] ) ;
+
+        // saving the knot span sizes
+        r_iga_model_part.SetValue(KNOT_SPAN_SIZES, knot_step_uv);
+
         // Create the breps for the outer sbm boundary
-        CreateBrepsSbmUtilities<Node, Point> CreateBrepsSbmUtilities(mEchoLevel);
+        CreateBrepsSbmUtilities<Node, Point, false> CreateBrepsSbmUtilities(mEchoLevel);
         CreateBrepsSbmUtilities.CreateSurrogateBoundary(mpSurface, A_uvw, B_uvw, rModelPart);
 
         //TODO: This must be turned to an error once we finish the ongoing SBM BCs development
@@ -70,6 +86,25 @@ void NurbsGeometryModelerSbm::CreateAndAddRegularGrid2D(
                         "in the nurbs_geometry_modeler_sbm in the project paramer json" << std::endl;
         return;
     }
+
+    // Create the True Model Part -> contains all the true boundary features
+    std::string skin_model_part_name;
+
+    // Retrieve skin_model_part_inner_initial_name if it exists
+    std::string skin_model_part_inner_initial_name = "skin_model_part_outer_initial_name";
+    if (mParameters.Has("skin_model_part_inner_initial_name")) {
+        skin_model_part_inner_initial_name = mParameters["skin_model_part_inner_initial_name"].GetString();
+    }
+
+    // Retrieve skin_model_part_outer_initial_name if it exists;
+    std::string skin_model_part_outer_initial_name = "skin_model_part_outer_initial_name";
+    if (mParameters.Has("skin_model_part_outer_initial_name")) {
+        skin_model_part_outer_initial_name = mParameters["skin_model_part_outer_initial_name"].GetString();
+    }
+
+    // Create the surrogate sub model parts inner and outer
+    ModelPart& surrogate_sub_model_part_inner = r_iga_model_part.CreateSubModelPart("surrogate_inner");
+    ModelPart& surrogate_sub_model_part_outer = r_iga_model_part.CreateSubModelPart("surrogate_outer");
     
     if (mParameters.Has("skin_model_part_name"))
         skin_model_part_name = mParameters["skin_model_part_name"].GetString();
@@ -89,27 +124,6 @@ void NurbsGeometryModelerSbm::CreateAndAddRegularGrid2D(
     ModelPart& skin_model_part = mpModel->CreateModelPart(skin_model_part_name);
     skin_model_part.CreateSubModelPart("inner");
     skin_model_part.CreateSubModelPart("outer");
-    
-    
-    // compute unique_knot_vector_u
-    Vector unique_knot_vector_u(2+(NumKnotSpansU-1));
-    unique_knot_vector_u[0] = mKnotVectorU[0]; 
-    unique_knot_vector_u[NumKnotSpansU] = mKnotVectorU[mKnotVectorU.size()-1];
-    for (SizeType i_knot_insertion = 0; i_knot_insertion < NumKnotSpansU-1; i_knot_insertion++) {
-        unique_knot_vector_u[i_knot_insertion+1] = mInsertKnotsU[i_knot_insertion];
-    }
-
-    // compute unique_knot_vector_v
-    Vector unique_knot_vector_v(2+(NumKnotSpansV-1));
-    unique_knot_vector_v[0] = mKnotVectorV[0]; 
-    unique_knot_vector_v[NumKnotSpansV] = mKnotVectorV[mKnotVectorV.size()-1];
-    for (SizeType i_knot_insertion = 0; i_knot_insertion < NumKnotSpansV-1; i_knot_insertion++) {
-        unique_knot_vector_v[i_knot_insertion+1] = mInsertKnotsV[i_knot_insertion];
-    }
-
-    // Set the value of the knot vectors
-    r_iga_model_part.SetValue(KNOT_VECTOR_U, unique_knot_vector_u);
-    r_iga_model_part.SetValue(KNOT_VECTOR_V, unique_knot_vector_v);
 
     // Create the parameters for the SnakeSbmProcess
     Kratos::Parameters snake_parameters;
@@ -130,7 +144,7 @@ void NurbsGeometryModelerSbm::CreateAndAddRegularGrid2D(
     snake_sbm_process.Execute();
 
     // Create the breps for the outer sbm boundary
-    CreateBrepsSbmUtilities<Node, Point> CreateBrepsSbmUtilities(mEchoLevel);
+    CreateBrepsSbmUtilities<Node, Point, true> CreateBrepsSbmUtilities(mEchoLevel);
     CreateBrepsSbmUtilities.CreateSurrogateBoundary(mpSurface, surrogate_sub_model_part_inner, surrogate_sub_model_part_outer, A_uvw, B_uvw, r_iga_model_part);
 }
 
@@ -139,6 +153,25 @@ const Parameters NurbsGeometryModelerSbm::GetDefaultParameters() const
 {
     return Parameters(R"(
     {
+        "echo_level": 1,
+        "model_part_name" : "IgaModelPart",
+        "lower_point_xyz": [0.0, 0.0, 0.0],
+        "upper_point_xyz": [1.0, 1.0, 0.0],
+        "lower_point_uvw": [0.0, 0.0, 0.0],
+        "upper_point_uvw": [1.0, 1.0, 0.0],
+        "polynomial_order" : [2, 2],
+        "number_of_knot_spans" : [10, 10],
+        "lambda_inner": 0.5,
+        "lambda_outer": 0.5,
+        "number_of_inner_loops": 0
+    })");
+}
+
+const Parameters NurbsGeometryModelerSbm::GetValidParameters() const
+{
+    return Parameters(R"(
+    {
+        "echo_level": 1,
         "model_part_name" : "IgaModelPart",
         "lower_point_xyz": [0.0, 0.0, 0.0],
         "upper_point_xyz": [1.0, 1.0, 0.0],
