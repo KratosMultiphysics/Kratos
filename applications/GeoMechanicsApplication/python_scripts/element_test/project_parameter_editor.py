034967de16f5a2148edd79e63e4e4d71a1129eab
Marjan Fathian <marjan.fathian@deltares.nl>
2025-06-15 16:14:14 +0200

    applied review comments

--- applications/GeoMechanicsApplication/python_scripts/element_test/project_parameter_editor.py
+++ applications/GeoMechanicsApplication/python_scripts/element_test/project_parameter_editor.py
@@ -13,16 +13,7 @@ class ProjectParameterEditor:
         with open(self.json_path, 'w') as f:
             f.write(self.raw_text)
 
-    def _update_property(self, property_name, new_value):
-        pattern = rf'("{property_name}"\s*:\s*)([0-9eE+\.\-]+)'
-        replacement = rf'\g<1>{new_value}'
-        self.raw_text, count = re.subn(pattern, replacement, self.raw_text)
-        if count == 0:
-            messagebox.showwarning("Warning", f"Could not find '{property_name}' to update.")
-        else:
-            self._write_back()
-
-    def _update_nested_value(self, module_name, key, new_list):
+    def update_nested_value(self, module_name, key, new_list):
         try:
             data = json.loads(self.raw_text)
 
@@ -43,13 +34,12 @@ class ProjectParameterEditor:
         except Exception as e:
             messagebox.showerror("Error", f"Failed to update stress vector: {e}")
 
-    def update_time_step_properties(self, number_of_step, end_time):
-        new_time_step = end_time / number_of_step
-        self._update_property('time_step', new_time_step)
-
-    def update_end_time_properties(self, end_time):
-        self._update_property('end_time', end_time)
-
-    def update_initial_stress_vector(self,  initial_effective_cell_pressure):
-        new_stress_values = [-initial_effective_cell_pressure] * 3 + [0.0]
-        self._update_nested_value("apply_initial_uniform_stress_field", "value", new_stress_values)
+    def update_property(self, property_name, new_value):
+        pattern = rf'("{property_name}"\s*:\s*)([0-9eE+\.\-]+)'
+        replacement = rf'\g<1>{new_value}'
+        self.raw_text, count = re.subn(pattern, replacement, self.raw_text)
+        if count == 0:
+            messagebox.showwarning("Warning", f"Could not find '{property_name}' to update.")
+        elif count > 1:
+            messagebox.showwarning("Warning", f"Multiple occurrences of '{property_name}' found. Updated all {count}, which may lead to ambiguity.")
+        self._write_back()
