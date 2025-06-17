431340f10617443d4f2e3e98b0cd140a5806ea02
Marjan Fathian <marjan.fathian@deltares.nl>
2025-06-17 17:17:18 +0200

    modified the run_simulation function to include direct shear test + modification of the name of the file

--- applications/GeoMechanicsApplication/python_scripts/element_test/run_simulation.py
+++ applications/GeoMechanicsApplication/python_scripts/element_test/run_simulation.py
@@ -6,20 +6,24 @@ import numpy as np
 from material_editor import MaterialEditor
 from project_parameter_editor import ProjectParameterEditor
 from mdpa_editor import MdpaEditor
-from triaxial import TriaxialTestRunner
-from triaxial_plots import plot_delta_sigma, plot_volumetric_strain, plot_sigma, plot_p_q, plot_mohr_coulomb_circle
+from generic_test_runner import GenericTestRunner
+from plots import (
+    plot_delta_sigma_triaxial, plot_volumetric_vertical_strain_triaxial, plot_principal_stresses_triaxial,
+    plot_p_q_triaxial, plot_mohr_coulomb_triaxial,
+    plot_strain_stress_direct_shear, plot_principal_stresses_direct_shear,
+    plot_p_q_direct_shear, plot_mohr_coulomb_direct_shear
+)
 
-
-def run_triaxial_simulation(dll_path, index, umat_parameters, num_steps, end_time, maximum_strain, initial_effective_cell_pressure,
-                            cohesion_phi_indices=None, axes=None):
-    tmp_folder = tempfile.mkdtemp(prefix="triaxial_")
+def run_simulation(test_type, dll_path, index, umat_parameters, num_steps, end_time, maximum_strain,
+                   initial_effective_cell_pressure, cohesion_phi_indices=None, axes=None):
+    tmp_folder = tempfile.mkdtemp(prefix=f"{test_type}_")
 
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    src_dir = os.path.join(base_dir, "test_triaxial")
+    src_dir = os.path.join(base_dir, f"test_{test_type}")
     files_to_copy = [
         "MaterialParameters.json",
         "ProjectParameters.json",
-        "triaxial.mdpa"
+        "mesh.mdpa"
     ]
 
     for filename in files_to_copy:
@@ -27,7 +31,7 @@ def run_triaxial_simulation(dll_path, index, umat_parameters, num_steps, end_tim
 
     json_file_path = os.path.join(tmp_folder, "MaterialParameters.json")
     project_param_path = os.path.join(tmp_folder, "ProjectParameters.json")
-    mdpa_path = os.path.join(tmp_folder, "triaxial.mdpa")
+    mdpa_path = os.path.join(tmp_folder, "mesh.mdpa")
 
     material_editor = MaterialEditor(json_file_path)
 
@@ -58,9 +62,13 @@ def run_triaxial_simulation(dll_path, index, umat_parameters, num_steps, end_tim
     mdpa_editor.update_end_time(end_time)
     mdpa_editor.update_first_timestep(num_steps, end_time)
 
-    output_files = [os.path.join(tmp_folder, 'gid_output', "triaxial.post.res")]
-    runner = TriaxialTestRunner(output_files, tmp_folder)
-    reshaped_values_by_time, vertical_strain, volumetric_strain, von_mises_stress, mean_effective_stresses = runner.run()
+    if test_type == "direct_shear":
+        mdpa_editor.update_middle_maximum_strain(maximum_strain)
+
+    output_files = [os.path.join(tmp_folder, 'gid_output', "output.post.res")]
+    runner = GenericTestRunner(output_files, tmp_folder)
+    (reshaped_values_by_time, vertical_strain, volumetric_strain, von_mises_stress, mean_effective_stresses,
+     shear_stress_xy, shear_strain_xy) = runner.run()
 
     if cohesion_phi_indices:
         c_idx, phi_idx = cohesion_phi_indices
@@ -90,8 +98,19 @@ def run_triaxial_simulation(dll_path, index, umat_parameters, num_steps, end_tim
         for ax in axes:
             ax.clear()
 
-    plot_delta_sigma(axes[0], vertical_strain, sigma_diff)
-    plot_volumetric_strain(axes[1], vertical_strain, volumetric_strain)
-    plot_sigma(axes[2], sigma1_list, sigma3_list)
-    plot_p_q(axes[3], mean_effective_stresses, von_mises_stress)
-    plot_mohr_coulomb_circle(axes[4], sigma1_list[-1], sigma3_list[-1], cohesion, friction_angle)
+    if test_type == "triaxial":
+        plot_delta_sigma_triaxial(axes[0], vertical_strain, sigma_diff)
+        plot_volumetric_vertical_strain_triaxial(axes[1], vertical_strain, volumetric_strain)
+        plot_principal_stresses_triaxial(axes[2], sigma1_list, sigma3_list)
+        plot_p_q_triaxial(axes[3], mean_effective_stresses, von_mises_stress)
+        plot_mohr_coulomb_triaxial(axes[4], sigma1_list[-1], sigma3_list[-1], cohesion, friction_angle)
+
+
+    elif test_type == "direct_shear":
+        plot_strain_stress_direct_shear(axes[0], shear_strain_xy, shear_stress_xy)
+        plot_principal_stresses_direct_shear(axes[1], sigma1_list, sigma3_list)
+        plot_p_q_direct_shear(axes[2], mean_effective_stresses, von_mises_stress)
+        plot_mohr_coulomb_direct_shear(axes[3], sigma1_list[-1], sigma3_list[-1], cohesion, friction_angle)
+
+    else:
+        raise ValueError(f"Unsupported test_type: {test_type}")
