aa44d5bbd12e0408f66aaa6f52635b34707d0f98
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-06-18 09:38:44 +0200

    minor

--- applications/HDF5Application/custom_utilities/data_type_utilities.h
+++ applications/HDF5Application/custom_utilities/data_type_utilities.h
@@ -130,7 +130,7 @@ template<class TContainerType>
     const auto& key_variable_names_map = block_for_each<MapReduction>(rContainer, map_type(), [](const auto& rEntity, auto& rTLS) -> map_type& {
         const auto& r_data = rEntity.GetData();
         for (auto it = r_data.begin(); it != r_data.end(); ++it) {
-            rTls.emplace(it->first->Key(), it->first->Name());
+            rTLS.emplace(it->first->Key(), it->first->Name());
         }
 
         return rTLS;
