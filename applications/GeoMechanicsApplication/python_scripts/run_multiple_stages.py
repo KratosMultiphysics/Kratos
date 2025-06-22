c8ed314a6ef8c47a8801216de52c6ace44661931
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-06-22 11:15:33 +0200

    Fixed broken integration tests
    After running the geomechanics analysis stages, return a list of run stages.

--- applications/GeoMechanicsApplication/python_scripts/run_multiple_stages.py
+++ applications/GeoMechanicsApplication/python_scripts/run_multiple_stages.py
@@ -14,12 +14,15 @@ def run_stages(project_path, n_stages):
     model = Kratos.Model()
     project_parameters_filenames = [f"ProjectParameters_stage{i + 1}.json" for i in range(n_stages)]
 
+    result = []
     with context_managers.set_cwd_to(project_path):
         for filename in project_parameters_filenames:
             with open(filename, "r") as f:
                 stage_parameters = Kratos.Parameters(f.read())
-            stage = analysis.GeoMechanicsAnalysis(model, stage_parameters)
-            stage.Run()
+            result.append(analysis.GeoMechanicsAnalysis(model, stage_parameters))
+            result[-1].Run()
+
+    return result
 
 
 if __name__ == "__main__":
