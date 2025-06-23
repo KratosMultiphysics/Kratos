599c8b1d6fd48aab39d0948a4ff6274f088affaf
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-06-23 15:01:38 +0200

    add tensor adaptor utils

---
+++ kratos/tensor_adaptors/tensor_adaptor_utils.h
@@ -0,0 +1,65 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Suneth Warnakulasuriya
+//
+
+#pragma once
+
+// System includes
+#include <type_traits>
+
+// External includes
+
+// Project includes
+#include "utilities/data_type_traits.h"
+
+namespace Kratos {
+
+///@name Kratos Classes
+///@{
+
+class TensorAdaptorUtils {
+public:
+    ///@name Public static methods
+    ///@{
+
+    template<class TContainerType, class TContainerIO>
+    static void GetShape(
+        DenseVector<unsigned int>& rShape,
+        const TContainerType& rContainer,
+        const TContainerIO& rContainerIO)
+    {
+        using return_type = typename TContainerIO::ReturnType;
+
+        rShape.resize(DataTypeTraits<return_type>::Dimension + 1);
+
+        return_type dummy_value{};
+        if (!rContainer.empty()) {
+            rContainerIO.GetValue(dummy_value, rContainer.front());
+        }
+
+        DataTypeTraits<return_type>::Shape(dummy_value, rShape.data().begin() + 1, rShape.data().end());
+        rShape[0] = rContainer.size();
+    }
+
+    template<class TIndexType = unsigned int>
+    static TIndexType GetFlatLength(
+        TIndexType const * pShapeBegin,
+        TIndexType const * pShapeEnd)
+    {
+        return std::accumulate(pShapeBegin, pShapeEnd, 1, std::multiplies<TIndexType>{});
+    }
+
+    ///@}
+};
+
+///@}
+
+} // namespace Kratos
