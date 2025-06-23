3fa5ee32d8fb09b55221da022e206f8015e130f5
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-06-23 15:01:58 +0200

    expose to python

--- kratos/python/add_tensor_adaptors_to_python.cpp
+++ kratos/python/add_tensor_adaptors_to_python.cpp
@@ -15,9 +15,11 @@
 // External includes
 
 // Project includes
-#include "tensor_adaptors/tensor_adaptor.h"
 #include "includes/model_part.h"
 #include "includes/define_python.h"
+#include "utilities/container_io_utils.h"
+#include "tensor_adaptors/tensor_adaptor.h"
+#include "tensor_adaptors/variant_variable_tensor_adaptor.h"
 
 // Include base h
 #include "add_tensor_adaptors_to_python.h"
@@ -65,6 +67,27 @@ void AddBaseTensorAdaptor(
     ;
 }
 
+template<class TContainerType>
+void AddContainerBaseTensorAdaptors(
+    pybind11::module& rModule,
+    const std::string& AdaptorNamePrefix)
+{
+    AddBaseTensorAdaptor<TensorAdaptor<TContainerType, int>>(rModule, AdaptorNamePrefix + "IntTensorAdaptor");
+    AddBaseTensorAdaptor<TensorAdaptor<TContainerType, double>>(rModule, AdaptorNamePrefix + "DoubleTensorAdaptor");
+    AddBaseTensorAdaptor<TensorAdaptor<TContainerType, bool>>(rModule, AdaptorNamePrefix + "BoolTensorAdaptor");
+}
+
+template<class TContainerType>
+void AddNonHistoricalTensorAdaptors(
+    pybind11::module& rModule,
+    const std::string& rAdaptorName)
+{
+    using non_historical_variant_variable_ta_type = VariantVariableTensorAdaptor<TContainerType, NonHistoricalIO>;
+    pybind11::class_<non_historical_variant_variable_ta_type, typename non_historical_variant_variable_ta_type::Pointer, typename non_historical_variant_variable_ta_type::BaseType>(rModule, rAdaptorName.c_str())
+        .def(pybind11::init<typename non_historical_variant_variable_ta_type::ContainerType::Pointer, typename non_historical_variant_variable_ta_type::VariableType>(), pybind11::arg("container"), pybind11::arg("variable"));
+
+}
+
 } // namespace Detail
 
 
@@ -73,29 +96,40 @@ void AddTensorAdaptorsToPython(pybind11::module& m)
     auto tensor_adaptor_sub_module = m.def_submodule("TensorAdaptors");
 
     auto node_tensor_adaptors_sub_module = tensor_adaptor_sub_module.def_submodule("NodeTensorAdaptors");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::NodesContainerType, int>>(node_tensor_adaptors_sub_module, "NodeIntTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::NodesContainerType, double>>(node_tensor_adaptors_sub_module, "NodeDoubleTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::NodesContainerType, bool>>(node_tensor_adaptors_sub_module, "NodeBoolTensorAdaptor");
+    Detail::AddContainerBaseTensorAdaptors<ModelPart::NodesContainerType>(node_tensor_adaptors_sub_module, "Node");
 
     auto condition_tensor_adaptors_sub_module = tensor_adaptor_sub_module.def_submodule("ConditionTensorAdaptors");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::ConditionsContainerType, int>>(condition_tensor_adaptors_sub_module, "ConditionIntTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::ConditionsContainerType, double>>(condition_tensor_adaptors_sub_module, "ConditionDoubleTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::ConditionsContainerType, bool>>(condition_tensor_adaptors_sub_module, "ConditionBoolTensorAdaptor");
+    Detail::AddContainerBaseTensorAdaptors<ModelPart::ConditionsContainerType>(condition_tensor_adaptors_sub_module, "Condition");
 
     auto element_tensor_adaptors_sub_module = tensor_adaptor_sub_module.def_submodule("ElementTensorAdaptors");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::ElementsContainerType, int>>(element_tensor_adaptors_sub_module, "ElementIntTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::ElementsContainerType, double>>(element_tensor_adaptors_sub_module, "ElementDoubleTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::ElementsContainerType, bool>>(element_tensor_adaptors_sub_module, "ElementBoolTensorAdaptor");
+    Detail::AddContainerBaseTensorAdaptors<ModelPart::ElementsContainerType>(element_tensor_adaptors_sub_module, "Element");
 
     auto properties_tensor_adaptors_sub_module = tensor_adaptor_sub_module.def_submodule("PropertyTensorAdaptors");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::PropertiesContainerType, int>>(properties_tensor_adaptors_sub_module, "PropertyIntTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::PropertiesContainerType, double>>(properties_tensor_adaptors_sub_module, "PropertyDoubleTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::PropertiesContainerType, bool>>(properties_tensor_adaptors_sub_module, "PropertyBoolTensorAdaptor");
+    Detail::AddContainerBaseTensorAdaptors<ModelPart::PropertiesContainerType>(properties_tensor_adaptors_sub_module, "Property");
 
     auto geometries_tensor_adaptors_sub_module = tensor_adaptor_sub_module.def_submodule("GeometryTensorAdaptors");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::GeometryContainerType, int>>(geometries_tensor_adaptors_sub_module, "GeometryIntTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::GeometryContainerType, double>>(geometries_tensor_adaptors_sub_module, "GeometryDoubleTensorAdaptor");
-    Detail::AddBaseTensorAdaptor<TensorAdaptor<ModelPart::GeometryContainerType, bool>>(geometries_tensor_adaptors_sub_module, "GeometryBoolTensorAdaptor");
+    Detail::AddContainerBaseTensorAdaptors<ModelPart::GeometryContainerType::GeometriesMapType>(geometries_tensor_adaptors_sub_module, "Geometry");
+
+    // adding non historical variable tensor adaptors
+    Detail::AddNonHistoricalTensorAdaptors<ModelPart::NodesContainerType>(node_tensor_adaptors_sub_module, "NodeNonHistoricalVariableTensorAdaptor");
+    Detail::AddNonHistoricalTensorAdaptors<ModelPart::ConditionsContainerType>(condition_tensor_adaptors_sub_module, "ConditionVariableTensorAdaptor");
+    Detail::AddNonHistoricalTensorAdaptors<ModelPart::ElementsContainerType>(element_tensor_adaptors_sub_module, "ElementVariableTensorAdaptor");
+    Detail::AddNonHistoricalTensorAdaptors<ModelPart::PropertiesContainerType>(properties_tensor_adaptors_sub_module, "PropertyVariableTensorAdaptor");
+    Detail::AddNonHistoricalTensorAdaptors<ModelPart::GeometryContainerType::GeometriesMapType>(geometries_tensor_adaptors_sub_module, "GeometryVariableTensorAdaptor");
+
+    // adding non-historical variable tensor adaptors
+    using node_historical_variant_variable_ta_type = VariantVariableTensorAdaptor<ModelPart::NodesContainerType, HistoricalIO, const int>;
+    pybind11::class_<node_historical_variant_variable_ta_type, node_historical_variant_variable_ta_type::Pointer, node_historical_variant_variable_ta_type::BaseType>(node_tensor_adaptors_sub_module, "NodeHistoricalVariableTensorAdaptor")
+        .def(pybind11::init<typename node_historical_variant_variable_ta_type::ContainerType::Pointer, typename node_historical_variant_variable_ta_type::VariableType, const int>(), pybind11::arg("container"), pybind11::arg("variable"), pybind11::arg("step_index"));
+
+    // adding gauss point calculation tensor adaptors
+    using condition_gp_variant_variable_ta_type = VariantVariableTensorAdaptor<ModelPart::ConditionsContainerType, GaussPointIO, const ProcessInfo&>;
+    pybind11::class_<condition_gp_variant_variable_ta_type, condition_gp_variant_variable_ta_type::Pointer, condition_gp_variant_variable_ta_type::BaseType>(condition_tensor_adaptors_sub_module, "ConditionGaussPointVariableTensorAdaptor")
+        .def(pybind11::init<typename condition_gp_variant_variable_ta_type::ContainerType::Pointer, typename condition_gp_variant_variable_ta_type::VariableType, const ProcessInfo&>(), pybind11::arg("container"), pybind11::arg("variable"), pybind11::arg("process_info"));
+
+    using element_gp_variant_variable_ta_type = VariantVariableTensorAdaptor<ModelPart::ElementsContainerType, GaussPointIO, const ProcessInfo&>;
+    pybind11::class_<element_gp_variant_variable_ta_type, element_gp_variant_variable_ta_type::Pointer, element_gp_variant_variable_ta_type::BaseType>(element_tensor_adaptors_sub_module, "ElementGaussPointVariableTensorAdaptor")
+        .def(pybind11::init<typename element_gp_variant_variable_ta_type::ContainerType::Pointer, typename element_gp_variant_variable_ta_type::VariableType, const ProcessInfo&>(), pybind11::arg("container"), pybind11::arg("variable"), pybind11::arg("process_info"));
 
 }
 
