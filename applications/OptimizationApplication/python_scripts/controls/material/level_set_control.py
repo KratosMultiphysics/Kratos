3b8374c032e93f3510d44e40d73feab1d5c90685
Jaanika Kiolein <jaanika.kiolein@gmail.com>
2025-06-27 15:01:03 +0300

    Fixed MapGradient(). Update() unclear.

--- applications/OptimizationApplication/python_scripts/controls/material/level_set_control.py
+++ applications/OptimizationApplication/python_scripts/controls/material/level_set_control.py
@@ -2,6 +2,7 @@ import KratosMultiphysics as Kratos
 import KratosMultiphysics.OptimizationApplication as KratosOA
 from KratosMultiphysics.OptimizationApplication.controls.control import Control
 from KratosMultiphysics.OptimizationApplication.utilities.optimization_problem import OptimizationProblem
+from master.applications.OptimizationApplication.python_scripts.utilities.helper_utilities import IsSameContainerExpression
 from numpy import tanh
 
 from applications.OptimizationApplication.python_scripts.utilities.component_data_view import ComponentDataView
@@ -44,14 +45,8 @@ class LevelSetControl(Control):
         self.output_all_fields = parameters["output_all_fields"].GetBool()
         self.echo_level = parameters["echo_level"].GetInt()
         self.k = parameters["k"].GetDouble()
-        self.young_modulus = parameters["young_modulus"].GetDouble()
-        self.density = parameters["density"].GetDouble()
 
         self.controlled_physical_variables = [Kratos.DENSITY, Kratos.YOUNG_MODULUS]
-
-
-        # self.density_projection = CreateProjection(parameters["density_projection_settings"], self.optimization_problem)
-        # self.young_modulus_projection = CreateProjection(parameters["young_modulus_projection_settings"], self.optimization_problem)
 
         controlled_model_names_parts = parameters["controlled_model_part_names"].GetStringArray()
         if len(controlled_model_names_parts) == 0:
@@ -61,26 +56,18 @@ class LevelSetControl(Control):
 
         self.consider_recursive_property_update = parameters["consider_recursive_property_update"].GetBool()
 
-        # filtering settings
-        ## self.filter = FilterFactory(self.model, self.model_part_operation.GetModelPartFullName(), Kratos.DENSITY, Kratos.Globals.DataLocation.Element, parameters["filter_settings"])
+        
 
     def Initialize(self) -> None:
-        #self.un_buffered_data = ComponentDataView(self, self.optimization_problem).GetUnBufferedData()
+        self.un_buffered_data = ComponentDataView(self, self.optimization_problem).GetUnBufferedData()
 
         # init model parts
         self.model_part = self.model_part_operation.GetModelPart()
 
         # # Creating element specific properties for Youngs Modulus and Density
-        # if not KratosOA.OptAppModelPartUtils.CheckModelPartStatus(self.model_part, "element_specific_properties_created"):
-        #     KratosOA.OptimizationUtils.CreateEntitySpecificPropertiesForContainer(self.model_part, self.model_part.Elements, self.consider_recursive_property_update)
-        #     KratosOA.OptAppModelPartUtils.LogModelPartStatus(self.model_part, "element_specific_properties_created")
-
-        # self.filter.SetComponentDataView(ComponentDataView(self, self.optimization_problem))
-        # self.filter.Initialize()
-
-        # # initialize the projections
-        # self.density_projection.SetProjectionSpaces(self.materials.GetPhi(), self.materials.GetDensities())
-        # self.young_modulus_projection.SetProjectionSpaces(self.materials.GetPhi(), self.materials.GetYoungModulus())
+        if not KratosOA.OptAppModelPartUtils.CheckModelPartStatus(self.model_part, "element_specific_properties_created"):
+            KratosOA.OptimizationUtils.CreateEntitySpecificPropertiesForContainer(self.model_part, self.model_part.Elements, self.consider_recursive_property_update)
+            KratosOA.OptAppModelPartUtils.LogModelPartStatus(self.model_part, "element_specific_properties_created")
 
         # check if the density or Young's modulus is defined
         element: Kratos.Element
@@ -93,6 +80,7 @@ class LevelSetControl(Control):
 
         # # get the control field
         # self.control_phi = self.filter.UnfilterField(self.simp_physical_phi)
+        self.control_phi = self.GetEmptyField()
 
         self._UpdateAndOutputFields(self.GetEmptyField())
 
@@ -115,68 +103,69 @@ class LevelSetControl(Control):
 
     def MapGradient(self, physical_gradient_variable_container_expression_map: 'dict[SupportedSensitivityFieldVariableTypes, ContainerExpressionTypes]') -> ContainerExpressionTypes:
         # get the physical variable
-        physical_control_variable = physical_gradient_variable_container_expression_map.keys()
+        keys = physical_gradient_variable_container_expression_map.keys()
         
         # check if one gradient is provided and is it density/Young's modulus
-        if len(physical_control_variable) != 1:
-            raise RuntimeError(f"Sensitivity w.r.t. DENSITY or YOUNG_MODULUS expected for control \"{self.GetName()}\". \"{len(physical_control_variable)}\" variables provided.")
-        if  Kratos.DENSITY not in physical_control_variable and Kratos.YOUNG_MODULUS not in physical_control_variable:
-            raise RuntimeError(f"The required gradient for control \"{self.GetName()}\" w.r.t. DENSITY or YOUNG_MODULUS not found. Following is the variable: \"{physical_control_variable[0]}\"")
+        if len(keys) != 1:
+            raise RuntimeError(f"Sensitivity w.r.t. DENSITY or YOUNG_MODULUS expected for control \"{self.GetName()}\". \"{len(keys)}\" variables provided.")
+        phys_control_var = next(iter(keys))
+        if  Kratos.DENSITY not in keys and Kratos.YOUNG_MODULUS not in keys:
+            raise RuntimeError(f"The required gradient for control \"{self.GetName()}\" w.r.t. DENSITY or YOUNG_MODULUS not found. Following is the variable:\n \"{phys_control_var.Name()}\"")
 
         # first access the physical partial sensitivity of the response function w.r.t the control variable (rho or E)
-        d_j_d_variable = physical_gradient_variable_container_expression_map[physical_control_variable[0]]
+        d_j_d_variable = physical_gradient_variable_container_expression_map[phys_control_var]
 
         # now compute response function total sensitivity w.r.t. phi
-        ##### here I need to have H'(phi)*rho instead of d_density_d_phi
         ### or use d_density_d_phi as a filter?
-        d_j_d_phi = d_j_d_variable * self.d_young_modulus_d_phi
+        d_variable_d_phi = self._ComputePhysicalVariableGradient(phys_control_var)
+        d_j_d_phi = d_j_d_variable * d_variable_d_phi
 
         return d_j_d_phi
 
     def Update(self, control_field: ContainerExpressionTypes) -> bool:
-        # if not IsSameContainerExpression(control_field, self.GetEmptyField()):
-        #     raise RuntimeError(f"Updates for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.model_part.FullName()}, given model part name: {control_field.GetModelPart().FullName()} ]")
+        # I think here I will get an already updated LSF and I need to use the heavyside to map density and E
 
-        # level-set update: d_phi = ( -v * grad(phi) ) * d_t
-        # get norm of grad(phi)
-        phi_grad_norm = self._ComputeLevelSetGradientNorm(self.control_phi)
+        if not IsSameContainerExpression(control_field, self.GetEmptyField()):
+             raise RuntimeError(f"Updates for the required element container not found for control \"{self.GetName()}\". [ required model part name: {self.model_part.FullName()}, given model part name: {control_field.GetModelPart().FullName()} ]")
 
-        # get design velocity v, ÜKS OSA ON PUUDU
-        design_velocity = - self.d_young_modulus_d_phi
-
-        # get update value
-        update = design_velocity * phi_grad_norm
-
+        update = control_field - self.control_phi
         if Kratos.Expression.Utils.NormL2(update) > 1e-15:
-            self.control_phi = self.control_phi + update
+            self.control_phi = control_field
             self._UpdateAndOutputFields(update)
             return True
+
+        # # level-set update: d_phi = ( -v * grad(phi) ) * d_t
+        # # get norm of grad(phi)
+        # phi_grad_norm = self._ComputeLevelSetGradientNorm(self.control_phi)
+
+        # # get design velocity v, dj/dE missing
+        # design_velocity = - self.d_young_modulus_d_phi
+
+        # # get update value
+        # update = design_velocity * phi_grad_norm
 
         return False
 
     def _UpdateAndOutputFields(self, update: ContainerExpressionTypes) -> None:
         
-        # rho = H(phi)*rho
-        density = 1/2 * (1 + tanh(self.k * self.control_phi)) * self.density
+        # rho = H(phi)*rho_0
+        density = self._ComputeHeaviside(Kratos.DENSITY)
         KratosOA.PropertiesVariableExpressionIO.Write(density, Kratos.DENSITY)
         if self.consider_recursive_property_update:
             KratosOA.OptimizationUtils.UpdatePropertiesVariableWithRootValueRecursively(density.GetContainer(), Kratos.DENSITY)
         self.un_buffered_data.SetValue("DENSITY", density.Clone(), overwrite=True)
 
-        youngs_modulus = 1/2 * (1 + tanh(self.k * self.control_phi)) * self.young_modulus
+        youngs_modulus = self._ComputeHeaviside(Kratos.YOUNG_MODULUS)
         KratosOA.PropertiesVariableExpressionIO.Write(youngs_modulus, Kratos.YOUNG_MODULUS)
         if self.consider_recursive_property_update:
             KratosOA.OptimizationUtils.UpdatePropertiesVariableWithRootValueRecursively(youngs_modulus.GetContainer(), Kratos.YOUNG_MODULUS)
         self.un_buffered_data.SetValue("YOUNG_MODULUS", youngs_modulus.Clone(), overwrite=True)
 
-        # calculate Dirac Delta function
-        d_H_d_phi = self.k / 2 *(1 - pow(tanh(self.k * self.control_phi), 2))
-
         # now calculate the total sensitivities of density w.r.t. phi
-        self.d_density_d_phi = d_H_d_phi * self.density
+        self.d_density_d_phi = self._ComputePhysicalVariableGradient(Kratos.DENSITY)
 
         # now calculate the total sensitivities of young modulus w.r.t. simp_physical_phi
-        self.d_young_modulus_d_phi = d_H_d_phi * self.young_modulus
+        self.d_young_modulus_d_phi = self._ComputePhysicalVariableGradient(Kratos.YOUNG_MODULUS)
 
         # now output the fields
         un_buffered_data = ComponentDataView(self, self.optimization_problem).GetUnBufferedData()
@@ -184,6 +173,11 @@ class LevelSetControl(Control):
         if self.output_all_fields:
             un_buffered_data.SetValue(f"dDENSITY_d{self.GetName()}", self.d_density_d_phi.Clone(), overwrite=True)
             un_buffered_data.SetValue(f"dYOUNG_MODULUS_d{self.GetName()}", self.d_young_modulus_d_phi.Clone(), overwrite=True)
+
+
+
+    def _ComputeHeaviside(self, physical_variable) -> ContainerExpressionTypes:
+        return 1/2 * (1 + tanh(self.k * self.control_phi)) * physical_variable
 
 
     def _ComputeLevelSetGradientNorm(self, LSF: ContainerExpressionTypes) -> ContainerExpressionTypes:
@@ -196,3 +190,15 @@ class LevelSetControl(Control):
 
         # Return the norm expression
         return LFS_grad_norm
+    
+
+    def _ComputePhysicalVariableGradient(self, physical_variable)  -> ContainerExpressionTypes:
+        
+        # calculate Dirac Delta function (derivative of Heaviside function w.r.t. phi)
+        d_H_d_phi = self.k / 2 *(1 - pow(tanh(self.k * self.control_phi), 2))
+
+        # calculate variable sensitivity w.r.t. phi
+        d_var_d_phi = d_H_d_phi * physical_variable
+
+        # return the sensitivity
+        return d_var_d_phi
