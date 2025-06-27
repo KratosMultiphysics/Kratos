ac0a5a31304315c2b681a8680697bd6ad3a82cff
Richard Faasse <richard.faasse@deltares.nl>
2025-06-27 14:22:02 +0200

    Added some explanation to the python test file and renamed a variable for clarity

--- applications/GeoMechanicsApplication/tests/moving_column_with_fixed_pressure_above_phreatic_line.py
+++ applications/GeoMechanicsApplication/tests/moving_column_with_fixed_pressure_above_phreatic_line.py
@@ -7,23 +7,27 @@ class KratosGeoMechanicsMovingColumnWithFixedPressureAbovePhreaticLine(
     KratosUnittest.TestCase
 ):
     """
-    This test class checks the settlement workflow using the C++ route.
+    This test class checks a moving column with fixed pressure above the phreatic line
+    using the C++ route. The column is moving downwards and upwards, crossing the phreatic line.
+    The assertion is done that the water pressure at a specific node switches from a free value
+    (below the phreatic line) to a fixed value (above the phreatic line) and vice versa.
     """
 
     def test_fixed_pressure_above_phreatic_line_via_cpp_workflow(self):
         import KratosMultiphysics.GeoMechanicsApplication.run_geo_settlement as run_geo_settlement
 
-        test_root = test_helper.get_file_path(
+        test_folder = test_helper.get_file_path(
             "moving_column_with_fixed_pressure_above_phreatic_line"
         )
-        status = run_geo_settlement.run_stages(test_root, ["ProjectParameters.json"])
+        status = run_geo_settlement.run_stages(test_folder, ["ProjectParameters.json"])
+        self.assertEqual(status, 0)
+
         reader = test_helper.GiDOutputFileReader()
         output_data = reader.read_output_from(
-            os.path.join(test_root, "output.post.res")
+            os.path.join(test_folder, "output.post.res")
         )
 
         # The Cross-over time is at 21600.0 seconds (the time it takes for the column to move 0.5 m downwards)
-        self.assertEqual(status, 0)
         self.assertAlmostEqual(
             reader.nodal_values_at_time("WATER_PRESSURE", 21600.0, output_data, [27])[
                 0
