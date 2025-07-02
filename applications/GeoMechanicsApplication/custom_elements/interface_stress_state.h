34963a40fed381798a8905d992dbd51d83322594
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-07-02 16:58:18 +0200

    Added new class `PlaneInterfaceStressState`
    The implementation of this class is similar to the one of class `Line2DInterfaceStressState`. Member function `CalculateBMatrix` still needs to be modified; now it's a copy of the implementation of `Line2DInterfaceStressState::CalculateBMatrix`.

--- applications/GeoMechanicsApplication/custom_elements/interface_stress_state.h
+++ applications/GeoMechanicsApplication/custom_elements/interface_stress_state.h
@@ -36,4 +36,23 @@ private:
     void load(Serializer&) override;
 };
 
+class KRATOS_API(GEO_MECHANICS_APPLICATION) PlaneInterfaceStressState final : public StressStatePolicy
+{
+public:
+    [[nodiscard]] Matrix CalculateBMatrix(const Matrix&, const Vector& rN, const Geometry<Node>& rGeometry) const override;
+    [[noreturn]] Vector CalculateGreenLagrangeStrain(const Matrix& rDeformationGradient) const override;
+    [[nodiscard]] std::unique_ptr<StressStatePolicy> Clone() const override;
+    [[nodiscard]] const Vector&                      GetVoigtVector() const override;
+    [[nodiscard]] SizeType                           GetVoigtSize() const override;
+    [[noreturn]] SizeType                            GetStressTensorSize() const override;
+
+private:
+    static const Vector VoigtVectorInterface3D;
+    static Vector       DefineInterfaceVoigtVector();
+
+    friend class Serializer;
+    void save(Serializer&) const override;
+    void load(Serializer&) override;
+};
+
 } // namespace Kratos
