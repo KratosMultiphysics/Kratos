43a34a9e79bae535cc6cf4c94b6bcabf833ca5ce
Lluis de Miguel <lmiguel@cimne.upc.edu>
2025-07-10 16:49:22 +0200

    Display the 5 smallest and 5 largest values.

--- applications/LaserDrillingApplication/data_analysis/check_sample_size.py
+++ applications/LaserDrillingApplication/data_analysis/check_sample_size.py
@@ -25,11 +25,11 @@ if __name__ == "__main__":
         print(f"{file.stem}  ---  {i}")
         data = clean_data(read_single_bore(file))
 
-        min_x_dict[file.stem] = data[:, 0].min()
-        max_x_dict[file.stem] = data[:, 0].max() 
+        min_x_dict[file.stem] = float(data[:, 0].min())
+        max_x_dict[file.stem] = float(data[:, 0].max())
 
-        min_y_dict[file.stem] = data[:, 1].min()
-        max_y_dict[file.stem] = data[:, 1].max()
+        min_y_dict[file.stem] = float(data[:, 1].min())
+        max_y_dict[file.stem] = float(data[:, 1].max())
 
     global_min_x_key = min(min_x_dict, key=min_x_dict.get)
     global_max_x_key = max(max_x_dict, key=max_x_dict.get)
@@ -44,5 +44,21 @@ if __name__ == "__main__":
     print(f"min_y={min_y_dict[global_min_y_key]} at {global_min_y_key}")
     print(f"max_y={max_y_dict[global_max_y_key]} at {global_max_y_key}")
 
+
+    sorted_min_x = sorted(min_x_dict.items(), key=lambda x: x[1])
+    sorted_max_x = sorted(max_x_dict.items(), key=lambda x: x[1])
+    
+    sorted_min_y = sorted(min_y_dict.items(), key=lambda x: x[1])
+    sorted_max_y = sorted(max_y_dict.items(), key=lambda x: x[1])
+
+    print("Sorted min x")
+    print(sorted_min_x[:5])
+    print("Sorted max x")
+    print(sorted_max_x[-5:])
+
+    print("Sorted min y")
+    print(sorted_min_y[:5])
+    print("Sorted max y")
+    print(sorted_max_y[-5:])
 
 
