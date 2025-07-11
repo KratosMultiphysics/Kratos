006e728d59db0125ab5a603cc7a41e1a26712fad
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-07-11 13:19:08 +0200

    update container io utils

--- kratos/utilities/container_io_utils.h
+++ kratos/utilities/container_io_utils.h
@@ -15,11 +15,15 @@
 // System includes
 #include <vector>
 
+// External includes
+#include <span/span.hpp>
+
 // Project includes
 #include "includes/define.h"
 #include "containers/flags.h"
 #include "includes/model_part.h"
 #include "utilities/data_type_traits.h"
+#include "utilities/string_utilities.h"
 
 // Application includes
 
@@ -300,6 +304,45 @@ void CopyToContiguousArray(
     KRATOS_CATCH("");
 }
 
+
+
+template<class TDataType, class TContainerType, class TIntegerType, class TGetterType>
+void CopyToContiguousArrayNew(
+    const TContainerType& rContainer,
+    const Kratos::span<typename DataTypeTraits<TDataType>::PrimitiveType>& rDataSpan,
+    TIntegerType const * pShapeBegin,
+    TIntegerType const * pShapeEnd,
+    const TGetterType& rGetter)
+{
+    KRATOS_TRY
+
+    using value_type_traits = DataTypeTraits<TDataType>;
+
+    KRATOS_ERROR_IF_NOT(pShapeBegin[0] == rContainer.size())
+        << "First dimension of the  shape mismatch with the container size [ "
+        << " container size = " << rContainer.size() << ", first dimension of the shape = " << pShapeBegin[0] << " ].\n";
+
+    KRATOS_ERROR_IF_NOT(DataTypeTraits<TDataType>::IsValidShape(pShapeBegin + 1, pShapeEnd))
+        << "Invalid data shape provided. [ data shape provided = ["
+        << StringUtilities::JoinValues(pShapeBegin + 1, pShapeEnd, ",")
+        << "], max possible sizes in each dimension  = "
+        << DataTypeTraits<TDataType>::Shape(TDataType{}) << " ].\n";
+
+    KRATOS_ERROR_IF_NOT(rDataSpan.size() == pShapeBegin[0] * DataTypeTraits<TDataType>::Size(pShapeBegin + 1, pShapeEnd))
+        << "The span size mismatch with required size from the given shape [ "
+        << "span size = " << rDataSpan.size() << ", shape = [" << StringUtilities::JoinValues(pShapeBegin, pShapeEnd, ",") << "] ].\n";
+
+    const auto stride = value_type_traits::Size(pShapeBegin + 1, pShapeEnd);
+
+    IndexPartition<unsigned int>(rContainer.size()).for_each(TDataType{}, [&rContainer,  &rGetter, pShapeBegin, pShapeEnd, stride, rDataSpan](const auto Index, auto& rTLS) {
+        rGetter(rTLS, *(rContainer.begin() + Index));
+        auto p_subrange_begin = rDataSpan.data() + Index * stride;
+        value_type_traits::CopyToContiguousData(p_subrange_begin, rTLS, pShapeBegin + 1, pShapeEnd);
+    });
+
+    KRATOS_CATCH("");
+}
+
 template<class TContainerType, class TContainerDataIO, class TIteratorType>
 void CopyFromContiguousDataArray(
     TContainerType& rContainer,
@@ -326,6 +369,49 @@ void CopyFromContiguousDataArray(
         auto p_subrange_begin = pBegin + Index * stride;
         value_type_traits::CopyFromContiguousData(rTLS, p_subrange_begin, pShapeBegin + 1, pShapeEnd);
         rContainerDataIO.SetValue(rTLS, *(rContainer.begin() + Index));
+    });
+
+    KRATOS_CATCH("");
+}
+
+template<class TDataType, class TContainerType, class TSpanType, class TIntegerType, class TSetterType>
+void CopyFromContiguousDataArrayNew(
+    TContainerType& rContainer,
+    const TSpanType& rDataSpan,
+    TIntegerType const * pShapeBegin,
+    TIntegerType const * pShapeEnd,
+    const TSetterType& rSetter)
+{
+    KRATOS_TRY
+
+    using value_type_traits = DataTypeTraits<TDataType>;
+
+    KRATOS_ERROR_IF_NOT(pShapeBegin[0] == rContainer.size())
+        << "First dimension of the  shape mismatch with the container size [ "
+        << " container size = " << rContainer.size() << ", first dimension of the shape = " << pShapeBegin[0] << " ].\n";
+
+    KRATOS_ERROR_IF_NOT(DataTypeTraits<TDataType>::IsValidShape(pShapeBegin + 1, pShapeEnd))
+        << "Invalid data shape provided. [ data shape provided = ["
+        << StringUtilities::JoinValues(pShapeBegin + 1, pShapeEnd, ",")
+        << "], max possible sizes in each dimension  = "
+        << DataTypeTraits<TDataType>::Shape(TDataType{}) << " ].\n";
+
+    KRATOS_ERROR_IF_NOT(rDataSpan.size() == pShapeBegin[0] * DataTypeTraits<TDataType>::Size(pShapeBegin + 1, pShapeEnd))
+        << "The span size mismatch with required size from the given shape [ "
+        << "span size = " << rDataSpan.size() << ", shape = [" << StringUtilities::JoinValues(pShapeBegin, pShapeEnd, ",") << "] ].\n";
+
+    const auto stride = value_type_traits::Size(pShapeBegin + 1, pShapeEnd);
+
+    TDataType dummy_value{};
+    if constexpr(DataTypeTraits<TDataType>::Dimension > 0) {
+        // skip for all the primitive types which does not need reshaping.
+        value_type_traits::Reshape(dummy_value, &*(pShapeBegin + 1), &*(pShapeBegin) + std::distance(pShapeBegin, pShapeEnd));
+    }
+
+    IndexPartition<unsigned int>(rContainer.size()).for_each(dummy_value, [&rContainer, &rSetter, rDataSpan, pShapeBegin, pShapeEnd, stride](const auto Index, auto& rTLS) {
+        auto p_subrange_begin = rDataSpan.data() + Index * stride;
+        value_type_traits::CopyFromContiguousData(rTLS, p_subrange_begin, pShapeBegin + 1, pShapeEnd);
+        rSetter(rTLS, *(rContainer.begin() + Index));
     });
 
     KRATOS_CATCH("");
