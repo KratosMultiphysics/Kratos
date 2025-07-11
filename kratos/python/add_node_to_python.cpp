e71f7621aede8e88b8f8ea921d355215dce6c05e
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-07-11 16:16:26 +0200

    Missing

--- kratos/python/add_node_to_python.cpp
+++ kratos/python/add_node_to_python.cpp
@@ -61,7 +61,6 @@ void NodeAddDof(Node& rThisNode, TVariableType const& rThisVariable)
 template<class TVariableType> inline
 void NodeAddDofwithReaction(Node& rThisNode, TVariableType const& rThisVariable,  TVariableType const& rReactionVariable)
 {
-// 	      KRATOS_WATCH(rReactionVariable);
     rThisNode.pAddDof(rThisVariable, rReactionVariable);
 }
 
@@ -85,9 +84,9 @@ template< class TBinderType, typename TContainerType, typename TVariableType > v
         binder.def("HasSolutionStepValue", [](const TContainerType& node, const TVariableType& rV){return node.SolutionStepsDataHas(rV);} );
         binder.def("SetSolutionStepValue",  [](TContainerType& node, const TVariableType& rV,const typename TVariableType::Type rValue){node.GetSolutionStepValue(rV) = rValue;} );
         binder.def("SetSolutionStepValue",  [](TContainerType& node,
-                                               const TVariableType& rV,
-                                               typename TContainerType::IndexType SolutionStepIndex,
-                                               const typename TVariableType::Type rValue){node.GetSolutionStepValue(rV, SolutionStepIndex) = rValue;} );
+                                              const TVariableType& rV,
+                                              typename TContainerType::IndexType SolutionStepIndex,
+                                              const typename TVariableType::Type rValue){node.GetSolutionStepValue(rV, SolutionStepIndex) = rValue;} );
         binder.def("GetSolutionStepValue", [](TContainerType& node, const TVariableType& rV){return node.GetSolutionStepValue(rV);} );
         binder.def("GetSolutionStepValue", [](TContainerType& node, const TVariableType& rV, typename TContainerType::IndexType SolutionStepIndex ){return node.GetSolutionStepValue(rV, SolutionStepIndex);} );
     }
@@ -141,7 +140,7 @@ void  AddNodeToPython(pybind11::module& m)
     node_binder.def("SolutionStepsDataHas", &NodeSolutionStepsDataHas<Variable<DenseMatrix<double> > >);
     node_binder.def("__str__", PrintObject<Node>);
     node_binder.def("OverwriteSolutionStepData", &Node::OverwriteSolutionStepData);
-    node_binder.def("GetInitialPosition", [](Node& rNode) { return rNode.GetInitialPosition(););
+    node_binder.def("GetInitialPosition", [](Node& rNode) { return rNode.GetInitialPosition(); });
     node_binder.def("SetInitialPosition", [](Node& rNode, const double X0, const double Y0, const double Z0) { rNode.SetInitialPosition(X0, Y0, Z0); });
     node_binder.def("SetInitialPosition", [](Node& rNode, const Point& rPoint) { rNode.SetInitialPosition(rPoint); });
     node_binder.def_property("X0",
@@ -158,5 +157,4 @@ void  AddNodeToPython(pybind11::module& m)
     PointerVectorSetPythonInterface<MeshType::NodesContainerType>().CreateInterface(m,"NodesArray");
 }
 
-}  // namespace Kratos::Python.
-
+}  // namespace Kratos::Python
