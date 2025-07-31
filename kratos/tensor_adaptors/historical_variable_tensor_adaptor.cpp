302a2e1baba524670c0462aad655240f58a57baf
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-07-31 20:02:36 +0200

    fix error

--- kratos/tensor_adaptors/historical_variable_tensor_adaptor.cpp
+++ kratos/tensor_adaptors/historical_variable_tensor_adaptor.cpp
@@ -98,12 +98,12 @@ HistoricalVariableTensorAdaptor::HistoricalVariableTensorAdaptor(
       mStepIndex(StepIndex)
 {
     // now check whether the given storage is compatible with the variable.
-    std::visit([this](auto pVariable) {
+    std::visit([this, &rOther](auto pVariable) {
         using data_type = typename BareType<decltype(*pVariable)>::Type;
         const auto& r_data_shape = this->mpStorage->DataShape();
         KRATOS_ERROR_IF_NOT(DataTypeTraits<data_type>::IsValidShape(r_data_shape.data().begin(), r_data_shape.data().begin() + r_data_shape.size()))
             << "The data storage within the tensor data is not compatible with the " << pVariable->Name()
-            << "[ tensor data = " << this->mpStorage->Info() << " ].\n";
+            << " [ origin data shape = " << rOther.DataShape() << ", tensor adaptor = " << *this << " ].\n";
 
     }, mpVariable);
 
