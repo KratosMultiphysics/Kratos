b5597118ec4330b54db8e784e7a005f10930ee00
Lluis de Miguel <lmiguel@cimne.upc.edu>
2025-08-06 01:56:27 +0200

    Add comment

--- applications/LaserDrillingApplication/data_analysis/batch_ellipse_extraction.py
+++ applications/LaserDrillingApplication/data_analysis/batch_ellipse_extraction.py
@@ -5,7 +5,15 @@ Extract ellipses from a folder with files of geometry point clouds
 import argparse
 from pathlib import Path
 import json
-from data_manipulation_functions import read_single_bore, clean_data, remove_surface_and_outliers, subsample_data, calculate_slice_bounds, calculate_slices, compute_ellipses
+from data_manipulation_functions import (
+    read_single_bore,
+    clean_data,
+    remove_surface_and_outliers,
+    subsample_data,
+    calculate_slice_bounds,
+    calculate_slices,
+    compute_ellipses,
+)
 from plotting_functions import plot_individual_slices_grid
 
 
@@ -31,13 +39,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process multiple bore measurements to extract ellipses.")
 
     parser.add_argument("--input-folder", required=True, help="Path to a folder containing the data files.")
-    parser.add_argument("--parameters-file", required=True, help="Path of the file that contains the parameters to be used")
+    parser.add_argument(
+        "--parameters-file", required=True, help="Path of the file that contains the parameters to be used"
+    )
 
-    parser.add_argument("--output-file", required=True, help="Path to the output JSON file (note that it will be overwritten).")
-    parser.add_argument("--output-failures", required=True, help="Path to the output JSON file that conatins the list of data files that failed and the reason.")
-    parser.add_argument("--output-plots", required=False, help="Path to the output folder were the plots will be saved. If it is not specfied, plots are not generated.")
-
-
+    parser.add_argument(
+        "--output-file", required=True, help="Path to the output JSON file (note that it will be overwritten)."
+    )
+    parser.add_argument(
+        "--output-failures",
+        required=True,
+        help="Path to the output JSON file that conatins the list of data files that failed and the reason.",
+    )
+    parser.add_argument(
+        "--output-plots",
+        required=False,
+        help="Path to the output folder were the plots will be saved. If it is not specfied, plots are not generated.",
+    )
 
     parser.add_argument("--material", required=True, help="Material name to associate with all experiments")
 
@@ -54,7 +72,6 @@ if __name__ == "__main__":
 
     input_data_folder = Path(folderpath)
 
-    
     # Read parameters
     with open(parameters_file, "r") as params_file:
         params = json.load(params_file)
@@ -69,10 +86,14 @@ if __name__ == "__main__":
 
         max_points = params["max_points"]  # Max number of points to use for entire dataset
         subsample_points = params["subsample_points"]  # Toggle for random subsampling of entire dataset
-        slice_thickness = params["slice_thickness"] # Thickness of each slice in um
-        deep_outlier_quantile_threshold = params["deep_outlier_quantile_threshold"]  # Lower quantile for discarding deep outliers. Set it to None to not discard any.
-        
-        shallow_z_threshold = params["shallow_z_threshold"] # Discard points shallower than this z (closer to surface). Set it to None to not discard any.
+        slice_thickness = params["slice_thickness"]  # Thickness of each slice in um
+        deep_outlier_quantile_threshold = params[
+            "deep_outlier_quantile_threshold"
+        ]  # Lower quantile for discarding deep outliers. Set it to None to not discard any.
+
+        shallow_z_threshold = params[
+            "shallow_z_threshold"
+        ]  # Discard points shallower than this z (closer to surface). Set it to None to not discard any.
         random_seed = params["random_seed"]  # Seed for reproducibility
         spline_order = params["spline_order"]  # Spline interpolation order
         n_spline_points = params["n_spline_points"]  # Number of points in the splines
@@ -80,23 +101,26 @@ if __name__ == "__main__":
         print("Missing parameters in the parameters JSON file.")
         exit(-1)
 
-    failed_files = {} # Dictionary of files that failed for some reason
+    failed_files = {}  # Dictionary of files that failed for some reason
     number_of_files = sum(1 for _ in input_data_folder.glob("*.dat"))
     print(f"{number_of_files=}")
 
     experiments = {}
     for i, file in enumerate(input_data_folder.glob("*.dat")):
         filename = file.name
-        print(f"Analyzing file {filename}: {i+1}/{number_of_files}")
+        print(f"Analyzing file {filename}: {i + 1}/{number_of_files}")
 
         power, pulses, identifier = parse_filename(file)
-        
-
 
         # ==== Read and Clean the Data ====
         data_raw = read_single_bore(file)
         data = clean_data(data_raw)
-        data, surface_outliers, deep_outliers = remove_surface_and_outliers(data, shallow_z_threshold, deep_outlier_quantile_threshold)
+
+        # ALIGN THE DATA TO THE XY PLANE
+
+        data, surface_outliers, deep_outliers = remove_surface_and_outliers(
+            data, shallow_z_threshold, deep_outlier_quantile_threshold
+        )
 
         # ==== Random subsample after filtering ====
         if subsample_points and max_points < len(data):
@@ -114,8 +138,8 @@ if __name__ == "__main__":
         try:
             slices = calculate_slices(data, slice_bounds)
         except ValueError:
-            # Emtpy slice, handle gracefully so that the script can 
-            # move on to the next data file. Note the file that is 
+            # Emtpy slice, handle gracefully so that the script can
+            # move on to the next data file. Note the file that is
             # failing for manual review later.
             failed_files[filename] = "Empty slice"
             print("Error in file" + filename)
@@ -128,22 +152,25 @@ if __name__ == "__main__":
             print("Error in file" + filename)
             continue
 
-        experiment = {
-        "material": material,
-        "power": power,
-        "pulses": pulses,
-        "id": identifier,
-        "ellipses" : ellipses
-        }
-        
-        
+        experiment = {"material": material, "power": power, "pulses": pulses, "id": identifier, "ellipses": ellipses}
+
         experiments[filename] = experiment
-        
+
         # Make the plot and export it
         if output_plots_path is not None:
             plot_path = Path(output_plots_path, file.stem)
-            plot_individual_slices_grid(slices, slice_bounds, centroids=None, ellipses=ellipses, sample_limits=sample_limits, plot_centroids=False, plot_ellipses_in_slices=True, filename=filename, save_path=plot_path)
-    
+            plot_individual_slices_grid(
+                slices,
+                slice_bounds,
+                centroids=None,
+                ellipses=ellipses,
+                sample_limits=sample_limits,
+                plot_centroids=False,
+                plot_ellipses_in_slices=True,
+                filename=filename,
+                save_path=plot_path,
+            )
+
     # Export the JSON with the ellipses
     with open(output_file, "w") as fout:
         json.dump(experiments, fout, indent=2)
@@ -156,5 +183,3 @@ if __name__ == "__main__":
         print("The following files failed:" + str(failed_files))
     else:
         print("No files failed")
-
-
