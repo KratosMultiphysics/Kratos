b2cecb2a929969ce5d00397d942416d235877367
matekelemen <kelemen.mate.12@gmail.com>
2025-08-11 16:33:29 +0200

    remove Reorderer from the template argument list of AMGCLSolver

--- applications/ContactStructuralMechanicsApplication/tests/cpp_tests/linear_solvers/test_mixedulm_linear_solver.cpp
+++ applications/ContactStructuralMechanicsApplication/tests/cpp_tests/linear_solvers/test_mixedulm_linear_solver.cpp
@@ -1,7 +1,7 @@
 // KRATOS    ______            __             __  _____ __                  __                   __
 //          / ____/___  ____  / /_____ ______/ /_/ ___// /________  _______/ /___  ___________ _/ /
-//         / /   / __ \/ __ \/ __/ __ `/ ___/ __/\__ \/ __/ ___/ / / / ___/ __/ / / / ___/ __ `/ / 
-//        / /___/ /_/ / / / / /_/ /_/ / /__/ /_ ___/ / /_/ /  / /_/ / /__/ /_/ /_/ / /  / /_/ / /  
+//         / /   / __ \/ __ \/ __/ __ `/ ___/ __/\__ \/ __/ ___/ / / / ___/ __/ / / / ___/ __ `/ /
+//        / /___/ /_/ / / / / /_/ /_/ / /__/ /_ ___/ / /_/ /  / /_/ / /__/ /_/ /_/ / /  / /_/ / /
 //        \____/\____/_/ /_/\__/\__,_/\___/\__//____/\__/_/   \__,_/\___/\__/\__,_/_/   \__,_/_/  MECHANICS
 //
 //  License:         BSD License
@@ -44,7 +44,7 @@ using LocalSpaceType = UblasSpace<double, Matrix, Vector>;
 using ReordererType = Reorderer<SparseSpaceType, LocalSpaceType>;
 using DirectSolverType = DirectSolver<SparseSpaceType, LocalSpaceType, ReordererType>;
 using LinearSolverType = LinearSolver<SparseSpaceType, LocalSpaceType>;
-using AMGCLSolverType = AMGCLSolver<SparseSpaceType, LocalSpaceType, ReordererType>;
+using AMGCLSolverType = AMGCLSolver<SparseSpaceType, LocalSpaceType>;
 using SkylineLUFactorizationSolverType = SkylineLUFactorizationSolver<SparseSpaceType, LocalSpaceType, ReordererType>;
 using PreconditionerType = Preconditioner<SparseSpaceType, LocalSpaceType>;
 using MixedULMLinearSolverType = MixedULMLinearSolver<SparseSpaceType, LocalSpaceType, PreconditionerType, ReordererType>;
