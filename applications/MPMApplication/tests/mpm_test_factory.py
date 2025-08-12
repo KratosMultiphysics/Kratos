18ebd9f4a3aa7c7ed88bcef760e809bfc1b673a1
V. Singer <veronika.singer@tum.de>
2025-08-12 14:38:22 +0200

    add additional test to check lagrange condition and condition values as well as json check results for elements and conditions

--- applications/MPMApplication/tests/mpm_test_factory.py
+++ applications/MPMApplication/tests/mpm_test_factory.py
@@ -110,6 +110,9 @@ class PenaltyImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(
 class LagrangeImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(MPMTestFactory):
     file_name = "beam_tests/hyperelastic_cantilever_beam/lagrange_self_weight_load_2D_quad_test"
 
+class LagrangeCenterImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(MPMTestFactory):
+    file_name = "beam_tests/hyperelastic_cantilever_beam/lagrange_center_self_weight_load_2D_quad_test"
+
 ### Perturbed Lagrange Imposition Tests
 class PerturbedLagrangeImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(MPMTestFactory):
     file_name = "beam_tests/hyperelastic_cantilever_beam/perturbed_lagrange_self_weight_load_2D_quad_test"
