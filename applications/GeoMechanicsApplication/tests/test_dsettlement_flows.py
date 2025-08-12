6970dc0742679e7bf7c47dcbadbdac6c951ff285
Richard Faasse <richard.faasse@deltares.nl>
2025-08-12 15:07:56 +0200

    Added assertion after one hundred days

--- applications/GeoMechanicsApplication/tests/test_dsettlement_flows.py
+++ applications/GeoMechanicsApplication/tests/test_dsettlement_flows.py
@@ -17,10 +17,17 @@ class KratosGeoMechanicsDSettlementTests(KratosUnittest.TestCase):
         status = run_geo_settlement.run_stages(project_path, self.project_parameters_filenames)
         self.assertEqual(status, 0)
         reader=test_helper.GiDOutputFileReader()
+        output_data=reader.read_output_from(os.path.join(project_path, "stage3.post.res"))
+        actual_settlement_after_one_hundred_days=reader.nodal_values_at_time("TOTAL_DISPLACEMENT", 100.0, output_data, [12])[0][1]
+        expected_settlement_after_one_hundred_days=-1.75393
         output_data=reader.read_output_from(os.path.join(project_path, "stage5.post.res"))
-        actual_total_y_displacement_after_ten_thousand_days=reader.nodal_values_at_time("TOTAL_DISPLACEMENT", 10000.0, output_data, [12])[0][1]
+        self.assertAlmostEqual(actual_settlement_after_one_hundred_days, expected_settlement_after_one_hundred_days, 4)
+        actual_settlement_after_ten_thousand_days=reader.nodal_values_at_time("TOTAL_DISPLACEMENT", 10000.0, output_data, [12])[0][1]
 
-        self.assertAlmostEqual(actual_total_y_displacement_after_ten_thousand_days, -6.4576, 4)
+        expected_settlement_after_ten_thousand_days = -6.4576
+        self.assertAlmostEqual(actual_settlement_after_ten_thousand_days, expected_settlement_after_ten_thousand_days, 4)
+
+        
         os.chdir(cwd)
 
 
