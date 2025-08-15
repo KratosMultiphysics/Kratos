0010802d7a80597f0554f8c6632c1c754aafa37a
matekelemen <kelemen.mate.12@gmail.com>
2025-08-15 12:13:36 +0200

    fix matrix adaptor lifetime bug

--- kratos/linear_solvers/amgcl_solver_impl.cpp
+++ kratos/linear_solvers/amgcl_solver_impl.cpp
@@ -13,6 +13,9 @@
 #include "linear_solvers/amgcl_solver_impl.hpp" // AMGCLAdaptor
 #include "spaces/ublas_space.h" // TUblasSparseSpace, TUblasDenseSpace
 
+// STD includes
+#include <optional>
+
 
 namespace Kratos {
 
@@ -21,25 +24,38 @@ template <class TValue>
 struct AMGCLAdaptor<TUblasSparseSpace<TValue>>
 {
     template <int BlockSize>
-    auto MakeMatrixAdaptor(const typename TUblasSparseSpace<TValue>::MatrixType& rMatrix) const
+    auto MakeMatrixAdaptor(const typename TUblasSparseSpace<TValue>::MatrixType& rMatrix)
     {
         if constexpr (BlockSize == 1) {
+            KRATOS_TRY
             return amgcl::adapter::zero_copy(rMatrix.size1(),
                                              rMatrix.index1_data().begin(),
                                              rMatrix.index2_data().begin(),
                                              rMatrix.value_data().begin());
+            KRATOS_CATCH("")
         } else {
-            using BackendMatrix = amgcl::static_matrix<
+            using BlockType = amgcl::static_matrix<
                 TValue,
                 BlockSize,
                 BlockSize
             >;
-            return amgcl::adapter::block_matrix<BackendMatrix>(
-                std::tuple_cat(
-                    std::make_tuple(rMatrix.size1()),
-                    std::tie(rMatrix.index1_data(), rMatrix.index2_data(), rMatrix.value_data())
-                )
-            );
+
+            KRATOS_TRY
+            // amgcl::adapter::block_matrix constructs a class
+            // that stores a reference to the "matrix" passed
+            // into it, which in this case means the tuple
+            // defined below. We need to keep it alive until
+            // the hierarchy construction finishes, hence the
+            // convoluted member variable.
+            mIntermediateAdaptor.emplace(std::tuple_cat(
+                std::tuple<std::size_t>(rMatrix.size1()),
+                std::tie(rMatrix.index1_data(),
+                         rMatrix.index2_data(),
+                         rMatrix.value_data())
+            ));
+
+            return amgcl::adapter::block_matrix<BlockType>(mIntermediateAdaptor.value());
+            KRATOS_CATCH("")
         }
     }
 
@@ -54,6 +70,16 @@ struct AMGCLAdaptor<TUblasSparseSpace<TValue>>
         KRATOS_ERROR_IF(rVector.empty());
         return &*rVector.begin();
     }
+
+private:
+    using TMatrix = typename TUblasSparseSpace<TValue>::MatrixType;
+
+    std::optional<std::tuple<
+        std::size_t,
+        const typename TMatrix::index_array_type&,
+        const typename TMatrix::index_array_type&,
+        const typename TMatrix::value_array_type&
+    >> mIntermediateAdaptor;
 }; // struct AMGCLAdaptor
 
 
