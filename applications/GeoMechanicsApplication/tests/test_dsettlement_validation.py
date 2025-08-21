2e42746b68ce44a38836a8c73063a47381c49a6a
mnabideltares <mohamed.nabi@deltares.nl>
2025-08-21 23:26:11 +0200

    Added 2 test cases

--- applications/GeoMechanicsApplication/tests/test_dsettlement_validation.py
+++ applications/GeoMechanicsApplication/tests/test_dsettlement_validation.py
@@ -62,5 +62,99 @@ class KratosGeoMechanicsDSettlementValidationTests(KratosUnittest.TestCase):
         os.chdir(original_working_dir)
 
 
+    def test_settlement_consolidation(self):
+        """
+        This test validates the settlement of a fully saturated column under uniform load.
+        The test runs multiple stages of a settlement simulation and checks the
+        settlement values at specific times against expected results.
+        The expected settlement values are based on an analytical solution.
+        """
+        test_name = "consolidation_uniform_load"
+        test_root = "dsettlement"
+        project_path = test_helper.get_file_path(os.path.join(test_root, test_name))
+
+        cwd = os.getcwd()
+        os.chdir(project_path)
+
+        import KratosMultiphysics.GeoMechanicsApplication.run_geo_settlement as run_geo_settlement
+
+        n_stages = 3
+        project_parameters_filenames = [
+            f"ProjectParameters_stage{i+1}.json" for i in range(n_stages)
+        ]
+        status = run_geo_settlement.run_stages(
+            project_path, project_parameters_filenames
+        )
+        self.assertEqual(status, 0)
+
+        reader = test_helper.GiDOutputFileReader()
+
+        output_data = reader.read_output_from(
+            os.path.join(project_path, "stage3.post.res")
+        )
+        
+        nodes = [3, 15, 16, 17, 18, 4]
+        expected_pressure = [0.0, -18524.3, -24096.0, -33456.2, -42816.6, -50000.0]
+        expected_total_stress = [-5332.28, -24858.1, -52279.1, -70030.9, -89823.8, -109627.0]
+        
+        for i in range(6):
+            actual_pressure = reader.nodal_values_at_time("WATER_PRESSURE", 8726.4, output_data, [nodes[i]])[0]
+            self.assertAlmostEqual(actual_pressure, expected_pressure[i], 4)
+        
+            actual_total_stress = reader.nodal_values_at_time(
+                "TOTAL_STRESS_TENSOR", 8726.4, output_data, [nodes[i]])[0][1]
+            self.assertAlmostEqual(actual_total_stress, expected_total_stress[i], 4)
+        
+
+        os.chdir(cwd)
+
+
+    def test_settlement_consolidation_fine_mesh(self):
+        """
+        This test validates the settlement of a fully saturated column under uniform load.
+        The test runs multiple stages of a settlement simulation and checks the
+        settlement values at specific times against expected results.
+        The expected settlement values are based on an analytical solution.
+        """
+        test_name = "consolidation_uniform_load_fine_mesh"
+        test_root = "dsettlement"
+        project_path = test_helper.get_file_path(os.path.join(test_root, test_name))
+
+        cwd = os.getcwd()
+        os.chdir(project_path)
+
+        import KratosMultiphysics.GeoMechanicsApplication.run_geo_settlement as run_geo_settlement
+
+        n_stages = 3
+        project_parameters_filenames = [
+            f"ProjectParameters_stage{i+1}.json" for i in range(n_stages)
+        ]
+        status = run_geo_settlement.run_stages(
+            project_path, project_parameters_filenames
+        )
+        self.assertEqual(status, 0)
+
+        reader = test_helper.GiDOutputFileReader()
+
+        output_data = reader.read_output_from(
+            os.path.join(project_path, "stage3.post.res")
+        )
+        
+        nodes = [87, 154, 266, 390, 516, 641]
+        expected_pressure = [0.0, -16382.0, -24416.0, -33477.7, -42701.9, -50000.0]
+        expected_total_stress = [-10324.4, -30053.2, -50021.0, -70014.2, -89998.3, -110028.0]
+        
+        for i in range(6):
+            actual_pressure = reader.nodal_values_at_time("WATER_PRESSURE", 8726.4, output_data, [nodes[i]])[0]
+            self.assertAlmostEqual(actual_pressure, expected_pressure[i], 4)
+        
+            actual_total_stress = reader.nodal_values_at_time(
+                "TOTAL_STRESS_TENSOR", 8726.4, output_data, [nodes[i]])[0][1]
+            self.assertAlmostEqual(actual_total_stress, expected_total_stress[i], 4)
+        
+
+        os.chdir(cwd)
+
+
 if __name__ == "__main__":
     KratosUnittest.main()
