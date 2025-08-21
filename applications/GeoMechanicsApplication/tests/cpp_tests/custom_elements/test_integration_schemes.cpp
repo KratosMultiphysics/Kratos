e39c49063873b811bb8506051c07ad4808ca7d5c
Richard Faasse <richard.faasse@deltares.nl>
2025-08-21 14:58:42 +0200

    Formatting

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_integration_schemes.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_integration_schemes.cpp
@@ -50,7 +50,7 @@ double SumOfWeights(const Geo::IntegrationPointVectorType& rIntegrationPoints)
     auto weights = std::vector<double>{};
     weights.reserve(rIntegrationPoints.size());
     std::ranges::transform(rIntegrationPoints, std::back_inserter(weights),
-                   [](const auto& rIntegrationPoint) { return rIntegrationPoint.Weight(); });
+                           [](const auto& rIntegrationPoint) { return rIntegrationPoint.Weight(); });
     return std::accumulate(weights.cbegin(), weights.cend(), 0.0);
 }
 
