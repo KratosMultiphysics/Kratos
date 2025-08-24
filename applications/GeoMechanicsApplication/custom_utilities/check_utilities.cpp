eb828fca5abdd88ac4cb11385c2cdadddbf99241
gennady.markelov <gennady.markelov@mail.com>
2025-08-24 15:28:43 +0200

    changed CheckHasNodalSolutionStepData

--- applications/GeoMechanicsApplication/custom_utilities/check_utilities.cpp
+++ applications/GeoMechanicsApplication/custom_utilities/check_utilities.cpp
@@ -25,15 +25,24 @@ void CheckUtilities::CheckDomainSize(double DomainSize, std::size_t Id, const st
         << min_domain_size << " for element " << Id << std::endl;
 }
 
-void CheckUtilities::CheckHasNodalSolutionStepData(const Geometry<Node>&            rGeometry, 
+void CheckUtilities::CheckHasNodalSolutionStepData(const Geometry<Node>& rGeometry, 
     const Geo::ConstVariableDataRefs& rVariableRefs)
 {
-    for (const auto& r_node : rGeometry) {
-        for (const auto& r_variable_ref : rVariableRefs) {
-            if (!r_node.SolutionStepsDataHas(r_variable_ref.get()))
-                KRATOS_ERROR << "Missing variable " << r_variable_ref.get().Name() << " on node "
-                             << r_node.Id() << std::endl;
+    auto has_all_variables = [&](const auto& node) {
+        return std::all_of(rVariableRefs.begin(), rVariableRefs.end(), [&node](const auto& variable_ref) {
+            return node.SolutionStepsDataHas(variable_ref.get());
+        });
+    };
+
+    if (!std::all_of(rGeometry.begin(), rGeometry.end(), has_all_variables)) {
+        for (const auto& r_node : rGeometry) {
+            for (const auto& r_variable_ref : rVariableRefs) {
+                if (!r_node.SolutionStepsDataHas(r_variable_ref.get()))
+                    KRATOS_INFO("Missing variable ") << r_variable_ref.get().Name()
+                                 << " on node " << r_node.Id() << std::endl;
+            }
         }
+        KRATOS_ERROR << "Missing variables on nodes" << std::endl;
     }
 }
 
