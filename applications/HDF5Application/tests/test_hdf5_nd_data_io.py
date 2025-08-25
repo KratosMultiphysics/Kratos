282650742b035d58289c35f74743e62d52c4e5b6
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-08-25 15:33:18 +0200

    add empty rank test

--- applications/HDF5Application/tests/test_hdf5_nd_data_io.py
+++ applications/HDF5Application/tests/test_hdf5_nd_data_io.py
@@ -31,7 +31,7 @@ class TestNDDataIO(UnitTest.TestCase):
         # DeleteFileIfExisting("test_nd_data_io.h5")
 
     def test_BoolNDData(self):
-        numpy_array = np.random.random((self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10)) > 0.5
+        numpy_array = np.random.random((self.data_communicator.Rank() + 10, 5, 6)) > 0.5
         write_nd_data = Kratos.BoolNDData(numpy_array, copy=False)
         write_attributes = Kratos.Parameters("""{"custom_attrib": "custom_value"}""")
 
@@ -45,7 +45,7 @@ class TestNDDataIO(UnitTest.TestCase):
         self.assertTrue(np.all(read_nd_data.to_numpy() == numpy_array))
 
     def test_UIntNDData(self):
-        numpy_array = (np.random.random((self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10)) * 255.0).astype(np.uint8)
+        numpy_array = (np.random.random((self.data_communicator.Rank() + 10, 5, 6)) * 255.0).astype(np.uint8)
         write_nd_data = Kratos.UIntNDData(numpy_array, copy=False)
         write_attributes = Kratos.Parameters("""{"custom_attrib": "custom_value"}""")
 
@@ -59,7 +59,7 @@ class TestNDDataIO(UnitTest.TestCase):
         self.assertTrue(np.all(read_nd_data.to_numpy() == numpy_array))
 
     def test_IntNDData(self):
-        numpy_array = (np.random.random((self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10)) * 1000).astype(np.int32)
+        numpy_array = (np.random.random((self.data_communicator.Rank() + 10, 5, 6)) * 1000).astype(np.int32)
         write_nd_data = Kratos.IntNDData(numpy_array, copy=False)
         write_attributes = Kratos.Parameters("""{"custom_attrib": "custom_value"}""")
 
@@ -73,7 +73,21 @@ class TestNDDataIO(UnitTest.TestCase):
         self.assertTrue(np.all(read_nd_data.to_numpy() == numpy_array))
 
     def test_FloatNDData(self):
-        numpy_array = (np.random.random((self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10, self.data_communicator.Rank() + 10)) * 1000).astype(np.float64)
+        numpy_array = (np.random.random((self.data_communicator.Rank() + 10, 5, 6)) * 1000).astype(np.float64)
+        write_nd_data = Kratos.DoubleNDData(numpy_array, copy=False)
+        write_attributes = Kratos.Parameters("""{"custom_attrib": "custom_value"}""")
+
+        nd_data_io = KratosHDF5.NDDataIO(Kratos.Parameters("""{"prefix": "/nd_data/"}"""), self.h5_file)
+        nd_data_io.Write("uint_test", write_nd_data, write_attributes)
+
+        read_nd_data, read_attributes = nd_data_io.Read("uint_test")
+
+        self.assertTrue(isinstance(read_nd_data, Kratos.DoubleNDData))
+        self.assertTrue(read_attributes.IsEquivalentTo(write_attributes))
+        self.assertTrue(np.all(read_nd_data.to_numpy() == numpy_array))
+
+    def test_EmptyRankNDData(self):
+        numpy_array = (np.random.random(((self.data_communicator.Rank() + 1) % 3, 5, 6)) * 1000).astype(np.float64)
         write_nd_data = Kratos.DoubleNDData(numpy_array, copy=False)
         write_attributes = Kratos.Parameters("""{"custom_attrib": "custom_value"}""")
 
