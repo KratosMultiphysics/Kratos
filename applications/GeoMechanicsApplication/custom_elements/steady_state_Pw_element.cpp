42bc26f1ed55d2fa2415db4e240cbbb8e67724fe
gennady.markelov <gennady.markelov@mail.com>
2025-08-29 06:06:10 +0200

    used non-const CheckProperties in SteadyStatePwElement

--- applications/GeoMechanicsApplication/custom_elements/steady_state_Pw_element.cpp
+++ applications/GeoMechanicsApplication/custom_elements/steady_state_Pw_element.cpp
@@ -53,8 +53,7 @@ int SteadyStatePwElement<TDim, TNumNodes>::Check(const ProcessInfo& rCurrentProc
         r_geometry, {std::cref(WATER_PRESSURE), std::cref(VOLUME_ACCELERATION)});
     CheckUtilities::CheckHasDofs(r_geometry, {std::cref(WATER_PRESSURE)});
 
-    const CheckProperties check_properties("material properties", r_properties,
-                                           CheckProperties::Bounds::AllExclusive);
+    CheckProperties check_properties("material properties", r_properties, CheckProperties::Bounds::AllExclusive);
     check_properties.Check(DENSITY_WATER);
     check_properties.Check(POROSITY);
     check_properties.Check(DYNAMIC_VISCOSITY);
