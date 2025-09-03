51c011ade1da931a822cc2e80c0cf9458ef59dff
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-09-03 16:29:08 +0200

    Renamed a few function parameters
    Also reformatted a Python script using `black`.

--- applications/GeoMechanicsApplication/python_scripts/geo_plot_utilities.py
+++ applications/GeoMechanicsApplication/python_scripts/geo_plot_utilities.py
@@ -2,13 +2,26 @@ import matplotlib.pyplot as plt
 import pathlib
 
 
-def _make_plot(series_collection, figure_filename, xlabel=None, ylabel=None, yaxis_inverted=False, xscale=None):
+def _make_plot(
+    data_series_collection,
+    plot_file_path,
+    xlabel=None,
+    ylabel=None,
+    yaxis_inverted=False,
+    xscale=None,
+):
     figure, axes = plt.subplots(layout="constrained")
     if xscale is not None:
         axes.set_xscale(xscale)
 
-    for series in series_collection:
-        axes.plot(series.x_values, series.y_values, label=series.label, linestyle=series.linestyle, marker=series.marker)
+    for series in data_series_collection:
+        axes.plot(
+            series.x_values,
+            series.y_values,
+            label=series.label,
+            linestyle=series.linestyle,
+            marker=series.marker,
+        )
     axes.grid()
     axes.grid(which="minor", color="0.9")
     axes.yaxis.set_inverted(yaxis_inverted)
@@ -16,17 +29,26 @@ def _make_plot(series_collection, figure_filename, xlabel=None, ylabel=None, yax
         axes.set_xlabel(xlabel)
     if ylabel is not None:
         axes.set_ylabel(ylabel)
-    figure.legend(loc='outside center right')
+    figure.legend(loc="outside center right")
 
-    if isinstance(figure_filename, pathlib.Path):
-        figure_filename = str(figure_filename.resolve())
-    print(f"Saving plot to {figure_filename}")
-    plt.savefig(figure_filename)
-
-
-def plot_settlement_results(series_collection, figure_filename):
-    _make_plot(series_collection, figure_filename, xlabel='Time [day]', ylabel='Settlement [m]', yaxis_inverted=True, xscale='log')
+    if isinstance(plot_file_path, pathlib.Path):
+        plot_file_path = str(plot_file_path.resolve())
+    print(f"Saving plot to {plot_file_path}")
+    plt.savefig(plot_file_path)
 
 
-def make_stress_plot(series_collection, figure_filename):
-    _make_plot(series_collection, figure_filename, xlabel='Stress [kPa]', ylabel='y [m]')
+def plot_settlement_results(data_series_collection, plot_file_path):
+    _make_plot(
+        data_series_collection,
+        plot_file_path,
+        xlabel="Time [day]",
+        ylabel="Settlement [m]",
+        yaxis_inverted=True,
+        xscale="log",
+    )
+
+
+def make_stress_plot(data_series_collection, plot_file_path):
+    _make_plot(
+        data_series_collection, plot_file_path, xlabel="Stress [kPa]", ylabel="y [m]"
+    )
