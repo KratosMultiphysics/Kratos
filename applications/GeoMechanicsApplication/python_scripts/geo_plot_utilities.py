2fb987bbecb8404bfef3be91f5d7cedf00edbdf2
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-09-03 16:25:02 +0200

    Extracted some common code

--- applications/GeoMechanicsApplication/python_scripts/geo_plot_utilities.py
+++ applications/GeoMechanicsApplication/python_scripts/geo_plot_utilities.py
@@ -2,35 +2,31 @@ import matplotlib.pyplot as plt
 import pathlib
 
 
-def plot_settlement_results(series_collection, figure_filename):
+def _make_plot(series_collection, figure_filename, xlabel=None, ylabel=None, yaxis_inverted=False, xscale=None):
     figure, axes = plt.subplots(layout="constrained")
-    axes.set_xscale('log')
+    if xscale is not None:
+        axes.set_xscale(xscale)
+
     for series in series_collection:
         axes.plot(series.x_values, series.y_values, label=series.label, linestyle=series.linestyle, marker=series.marker)
     axes.grid()
     axes.grid(which="minor", color="0.9")
-    axes.yaxis.set_inverted(True)
-    axes.set_xlabel('Time [day]')
-    axes.set_ylabel('Settlement [m]')
+    axes.yaxis.set_inverted(yaxis_inverted)
+    if xlabel is not None:
+        axes.set_xlabel(xlabel)
+    if ylabel is not None:
+        axes.set_ylabel(ylabel)
     figure.legend(loc='outside center right')
 
     if isinstance(figure_filename, pathlib.Path):
         figure_filename = str(figure_filename.resolve())
-    print(f"Save plot to {figure_filename}")
+    print(f"Saving plot to {figure_filename}")
     plt.savefig(figure_filename)
+
+
+def plot_settlement_results(series_collection, figure_filename):
+    _make_plot(series_collection, figure_filename, xlabel='Time [day]', ylabel='Settlement [m]', yaxis_inverted=True, xscale='log')
 
 
 def make_stress_plot(series_collection, figure_filename):
-    figure, axes = plt.subplots(layout="constrained")
-    for series in series_collection:
-        axes.plot(series.x_values, series.y_values, label=series.label, linestyle=series.linestyle, marker=series.marker)
-    axes.grid()
-    axes.grid(which="minor", color="0.9")
-    axes.set_xlabel('Stress [kPa]')
-    axes.set_ylabel('y [m]')
-    figure.legend(loc='outside center right')
-
-    if isinstance(figure_filename, pathlib.Path):
-        figure_filename = str(figure_filename.resolve())
-    print(f"Save plot to {figure_filename}")
-    plt.savefig(figure_filename)
+    _make_plot(series_collection, figure_filename, xlabel='Stress [kPa]', ylabel='y [m]')
