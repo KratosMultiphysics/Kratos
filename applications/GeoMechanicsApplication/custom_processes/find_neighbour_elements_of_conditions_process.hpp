e28829f9809e2090bbd305839f5c944a82d7b1d7
Richard Faasse <richard.faasse@deltares.nl>
2025-09-08 16:51:59 +0200

    Removed redundant comments in the header

--- applications/GeoMechanicsApplication/custom_processes/find_neighbour_elements_of_conditions_process.hpp
+++ applications/GeoMechanicsApplication/custom_processes/find_neighbour_elements_of_conditions_process.hpp
@@ -8,18 +8,17 @@
 //  License:         geo_mechanics_application/license.txt
 //
 //  Main authors:    Vahid Galavi
+//                   Richard Faasse
 //
 
 #pragma once
 
-// Project includes
 #include "includes/model_part.h"
 #include "processes/process.h"
 
 namespace Kratos
 {
-///@name Type Definitions
-///@{
+
 using hashmap = std::unordered_multimap<std::vector<std::size_t>,
                                         std::vector<Condition::Pointer>,
                                         KeyHasherRange<std::vector<std::size_t>>,
@@ -28,44 +27,18 @@ using hashmap2 = std::unordered_multimap<std::vector<std::size_t>,
                                         std::vector<std::size_t>,
                                         KeyHasherRange<std::vector<std::size_t>>,
                                         KeyComparorRange<std::vector<std::size_t>>>;
-///@}
-///@name Kratos Classes
-///@{
 
-/**
- * @class FindNeighbourElementsOfConditionsProcess
- * @ingroup Kratos.GeoMechanicsApplication
- * @brief Finds list of elements attached to conditions.
- * @author Vahid Galavi
- */
 class KRATOS_API(GEO_MECHANICS_APPLICATION) FindNeighbourElementsOfConditionsProcess : public Process
 {
 public:
-    ///@name Type Definitions
-    ///@{
-
-    /// Pointer definition of Process
     KRATOS_CLASS_POINTER_DEFINITION(FindNeighbourElementsOfConditionsProcess);
 
-    /// The definition of the index type
     using IndexType = std::size_t;
-
-    /// Definition of the node type
     using NodeType = Node;
-
-    // Definition of the geometry
     using GeometryType = Geometry<NodeType>;
 
-    ///@}
-    ///@name Life Cycle
-    ///@{
-
-    /// Constructor for FindNeighbourElementsOfConditionsProcess Process
-    /**
-     * @param rModelPart The model part to check.
-     */
     explicit FindNeighbourElementsOfConditionsProcess(ModelPart& rModelPart)
-        : Process(), mrModelPart(rModelPart)
+        : mrModelPart(rModelPart)
     {
     }
 
@@ -73,56 +46,17 @@ public:
     FindNeighbourElementsOfConditionsProcess(const FindNeighbourElementsOfConditionsProcess&) = delete;
     ~FindNeighbourElementsOfConditionsProcess() override = default;
 
-    ///@}
-    ///@name Operators
-    ///@{
-
-    ///@}
-    ///@name Operations
-    ///@{
-
-
-    /// Finds neighbour elements of conditions
     void Execute() override;
 
-    ///@}
-    ///@name Access
-    ///@{
-
-    ///@}
-    ///@name Inquiry
-    ///@{
-
-    ///@}
-    ///@name Input and output
-    ///@{
-
-    /// Turn back information as a string.
     [[nodiscard]] std::string Info() const override
     {
         return "FindNeighbourElementsOfConditionsProcess";
     }
 
-    /// Print object's data.
     void PrintData(std::ostream& rOStream) const override { this->PrintInfo(rOStream); }
 
-    ///@}
 private:
-    ///@name Static Member Variables
-    ///@{
-    bool FindPermutations(std::vector<std::size_t> elements_boundary_node_ids, std::vector<std::size_t> condition_node_ids) const;
-    bool FindPermutationsQuadratic(std::vector<std::size_t> elements_boundary_node_ids,
-                                   std::vector<std::size_t> condition_node_ids) const;
-
-    ///@}
-    ///@name Member Variables
-    ///@{
-
     ModelPart& mrModelPart;
-
-    ///@}
-    ///@name Private Operations
-    ///@{
 
     [[nodiscard]] bool AllConditionsAreVisited() const;
 
@@ -137,23 +71,13 @@ private:
         const hashmap2&                             FacesMapSorted,
         Element&                                   rElement,
         const Geometry<Node>::GeometriesArrayType& rBoundaryGeometries) const;
+    bool FindPermutations(std::vector<std::size_t> elements_boundary_node_ids, std::vector<std::size_t> condition_node_ids) const;
+    bool FindPermutationsQuadratic(std::vector<std::size_t> elements_boundary_node_ids,
+                                   std::vector<std::size_t> condition_node_ids) const;
+};
 
-    ///@}
-}; // Class Process
-
-///@}
-
-///@name Type Definitions
-///@{
-
-///@}
-///@name Input and output
-///@{
-
-/// input stream function
 inline std::istream& operator>>(std::istream& rIStream, FindNeighbourElementsOfConditionsProcess& rThis);
 
-/// output stream function
 inline std::ostream& operator<<(std::ostream& rOStream, const FindNeighbourElementsOfConditionsProcess& rThis)
 {
     rThis.PrintInfo(rOStream);
@@ -162,7 +86,5 @@ inline std::ostream& operator<<(std::ostream& rOStream, const FindNeighbourEleme
 
     return rOStream;
 }
-
-///@}
 
 } // namespace Kratos
