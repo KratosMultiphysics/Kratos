6b6c8d5bd029aecd99b859e2ccc7290916dc3c4c
Carlos Roig <roigcarlo@gmail.com>
2025-09-09 11:24:14 +0200

    Updating existing processes

--- kratos/processes/apply_ray_casting_process.h
+++ kratos/processes/apply_ray_casting_process.h
@@ -50,8 +50,10 @@ public:
     /// Pointer definition of ApplyRayCastingProcess
     KRATOS_CLASS_POINTER_DEFINITION(ApplyRayCastingProcess);
 
-    KRATOS_REGISTRY_ADD_TEMPLATE_PROTOTYPE("Processes.KratosMultiphysics", Process, ApplyRayCastingProcess, TDim)
-    KRATOS_REGISTRY_ADD_TEMPLATE_PROTOTYPE("Processes.All", Process, ApplyRayCastingProcess, TDim)
+    using RegistryBaseType = Process;
+    using RegistryClassType = ApplyRayCastingProcess<TDim>;
+    KRATOS_REGISTRY_ADD_TEMPLATE_PROTOTYPE("Processes.KratosMultiphysics", Process, RegistryClassType)
+    KRATOS_REGISTRY_ADD_TEMPLATE_PROTOTYPE("Processes.All", Process, RegistryClassType)
 
     //TODO: These using statements have been included to make the old functions able to compile. It is still pending to update them.
     using ConfigurationType = Internals::DistanceSpatialContainersConfigure;
