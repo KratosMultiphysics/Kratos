3bb74a82fcc6dff4cb3f93ab2714ae4325ae0eb5
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2025-09-09 14:59:33 +0200

    Replace `TANGENTIAL_PENALTY_FACTOR` with `TANGENTIAL_PENALTY_COEFFICIENT` for consistency

--- applications/MPMApplication/custom_utilities/material_point_generator_utility.cpp
+++ applications/MPMApplication/custom_utilities/material_point_generator_utility.cpp
@@ -546,15 +546,15 @@ namespace Kratos::MaterialPointGeneratorUtility
 
         }
 
-        // Scale TANGENTIAL_PENALTY_FACTOR by NODAL_AREA (in effect incorporating the integration weight)
+        // Scale TANGENTIAL_PENALTY_COEFFICIENT by NODAL_AREA (in effect incorporating the integration weight)
         block_for_each(rBackgroundGridModelPart.Nodes(), [&](Node& rNode)
         {
             const Node& rConstNode = rNode; // const Node reference to avoid issues with previously unset GetValue()
             double modified_factor;
 
             if (rNode.Is(SLIP)){
-                modified_factor = rConstNode.GetValue(NODAL_AREA) * rConstNode.GetValue(TANGENTIAL_PENALTY_FACTOR);
-                rNode.SetValue(TANGENTIAL_PENALTY_FACTOR, modified_factor);
+                modified_factor = rConstNode.GetValue(NODAL_AREA) * rConstNode.GetValue(TANGENTIAL_PENALTY_COEFFICIENT);
+                rNode.SetValue(TANGENTIAL_PENALTY_COEFFICIENT, modified_factor);
             }
         });
 
