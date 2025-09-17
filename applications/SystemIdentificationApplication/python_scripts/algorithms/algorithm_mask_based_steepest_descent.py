65e5106f7094b48d9ea040875bd228a5416e34ca
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-09-17 11:34:34 +0200

    use mask smoothing

--- applications/SystemIdentificationApplication/python_scripts/algorithms/algorithm_mask_based_steepest_descent.py
+++ applications/SystemIdentificationApplication/python_scripts/algorithms/algorithm_mask_based_steepest_descent.py
@@ -37,15 +37,34 @@ class AlgorithmMaskBasedSteepestDescent(AlgorithmSteepestDescent):
         super().__init__(model, parameters, optimization_problem)
         self.mask_name = self.parameters["settings"]["mask_name"].GetString()
 
+    @time_decorator()
+    def ComputeControlUpdate(self, alpha, list_of_gradients):
+        search_direction = self.algorithm_data.GetBufferedData()["search_direction"].Clone()
+        response: DamageDetectionResponse = self._objective.GetReponse()
+        control: MaterialPropertiesControl = self.master_control.GetListOfControls()[0]
+
+        # now we compute the modified search direction
+        update = search_direction * 0.0
+        for i, (filtered_mask, mapped_sensor_grad) in enumerate(list_of_gradients):
+            search_direction = -KratosOA.CollectiveExpression([control.filter.ForwardFilterField(mapped_sensor_grad) * filtered_mask])
+            sensor_update = KratosOA.ExpressionUtils.Scale(search_direction, alpha)
+            sensor = response.list_of_sensors[i]
+            self.algorithm_data.GetBufferedData()[f"{response.list_of_sensors[i].GetName()}_search_direction"] = search_direction.Clone()
+            self.algorithm_data.GetBufferedData()[f"{response.list_of_sensors[i].GetName()}_update"] = sensor_update.Clone()
+            sensor.AddContainerExpression("search_direction", search_direction.GetContainerExpressions()[0])
+            sensor.AddContainerExpression("update", update.GetContainerExpressions()[0])
+            update = KratosOA.ExpressionUtils.Collapse(update + sensor_update)
+
+        self.algorithm_data.GetBufferedData()["control_field_update"] = update.Clone()
 
     @time_decorator()
     def Solve(self):
+        response: DamageDetectionResponse = self._objective.GetReponse()
+        control: MaterialPropertiesControl = self.master_control.GetListOfControls()[0]
+        smooth_masks_list = KratosSI.MaskUtils.SmoothenMasks([sensor.GetContainerExpression(self.mask_name) for sensor in response.list_of_sensors], 3e-2)
         while not self.converged:
             with OptimizationAlgorithmTimeLogger("AlgorithmSteepestDescent",self._optimization_problem.GetStep()):
                 self._InitializeIteration()
-
-                response: DamageDetectionResponse = self._objective.GetReponse()
-                control: MaterialPropertiesControl = self.master_control.GetListOfControls()[0]
 
                 control.skip_forward_filter = True
 
@@ -63,8 +82,12 @@ class AlgorithmMaskBasedSteepestDescent(AlgorithmSteepestDescent):
                     self.algorithm_data.GetBufferedData()["abs_obj[%]"] = obj_info["abs_change [%]"]
 
                 obj_grad = self._control_field.Clone() * 0.0
-                for sensor in response.list_of_sensors:
+                list_of_masked_sensor_gradients = []
+                for i, sensor in enumerate(response.list_of_sensors):
                     mask = sensor.GetContainerExpression(self.mask_name)
+                    mask = Kratos.Expression.ElementExpression(self._control_field.GetContainerExpressions()[0].GetModelPart())
+                    Kratos.Expression.LiteralExpressionIO.SetData(mask, 1.0)
+                    filtered_mask = mask.Clone()
 
                     # clear all the sensors
                     response.damage_response_function.Clear()
@@ -75,20 +98,28 @@ class AlgorithmMaskBasedSteepestDescent(AlgorithmSteepestDescent):
                     control.GetEmptyField().GetModelPart().ProcessInfo[KratosSI.SENSOR_NAME] = sensor.GetName()
 
                     response.CalculateGradient({Kratos.YOUNG_MODULUS: raw_sensor_grad})
-                    mapped_sensor_grad = control.MapGradient({Kratos.YOUNG_MODULUS: raw_sensor_grad.GetContainerExpressions()[0]})
-                    modified_sensor_grad = control.filter.ForwardFilterField(mapped_sensor_grad) * mask
-                    obj_grad += KratosOA.CollectiveExpression([modified_sensor_grad])
+                    masked_sensor_grad = raw_sensor_grad * KratosOA.CollectiveExpression([filtered_mask])
+                    mapped_sensor_grad = control.MapGradient({Kratos.YOUNG_MODULUS: masked_sensor_grad.GetContainerExpressions()[0]})
+                    list_of_masked_sensor_gradients.append((filtered_mask, mapped_sensor_grad))
+                    obj_grad += KratosOA.CollectiveExpression([mapped_sensor_grad])
+
+                    sensor.AddContainerExpression("mask", mask)
+                    sensor.AddContainerExpression("filtered_mask", filtered_mask)
+                    sensor.AddContainerExpression("raw_gradient", raw_sensor_grad.GetContainerExpressions()[0])
+                    sensor.AddContainerExpression("mapped_sensor_grad", mapped_sensor_grad)
+                    sensor.AddContainerExpression("masked_sensor_grad", masked_sensor_grad.GetContainerExpressions()[0])
 
                     self.algorithm_data.GetBufferedData()[f"{sensor.GetName()}_mask"] = mask.Clone()
+                    self.algorithm_data.GetBufferedData()[f"{sensor.GetName()}_filtered_mask"] = filtered_mask.Clone()
                     self.algorithm_data.GetBufferedData()[f"{sensor.GetName()}_raw_gradient"] = raw_sensor_grad.Clone()
                     self.algorithm_data.GetBufferedData()[f"{sensor.GetName()}_mapped_gradient"] = mapped_sensor_grad.Clone()
-                    self.algorithm_data.GetBufferedData()[f"{sensor.GetName()}_modified_sensor_gradient"] = modified_sensor_grad.Clone()
+                    self.algorithm_data.GetBufferedData()[f"{sensor.GetName()}_masked_sensor_grad"] = masked_sensor_grad.Clone()
 
                 self.ComputeSearchDirection(obj_grad)
 
                 alpha = self._line_search_method.ComputeStep()
 
-                self.ComputeControlUpdate(alpha)
+                self.ComputeControlUpdate(alpha, list_of_masked_sensor_gradients)
 
                 self._FinalizeIteration()
 
