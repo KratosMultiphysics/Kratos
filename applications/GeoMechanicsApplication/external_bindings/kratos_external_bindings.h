8b3acf7795465b119db89fd8120208d8de6fc5a9
Richard Faasse <richard.faasse@deltares.nl>
2025-09-22 16:14:49 +0200

    Removed remaining `__stdcall`

--- applications/GeoMechanicsApplication/external_bindings/kratos_external_bindings.h
+++ applications/GeoMechanicsApplication/external_bindings/kratos_external_bindings.h
@@ -11,26 +11,26 @@ class KRATOS_API(GEO_MECHANICS_APPLICATION) ExternalBindings
     static KratosExecute* KratosExecute_CreateInstance();
 
     static int execute_flow_analysis(KratosExecute* instance,
-                              const char*    workingDirectory,
-                              const char*    projectFile,
-                              double         minCriticalHead,
-                              double         maxCriticalHead,
-                              double         stepCriticalHead,
-                              const char*    criticalHeadBoundaryModelPartName,
-                              void __stdcall logCallback(const char*),
-                              void __stdcall reportProgress(double),
-                              void __stdcall reportTextualProgress(const char*),
-                              bool __stdcall shouldCancel());
+                                     const char*    workingDirectory,
+                                     const char*    projectFile,
+                                     double         minCriticalHead,
+                                     double         maxCriticalHead,
+                                     double         stepCriticalHead,
+                                     const char*    criticalHeadBoundaryModelPartName,
+                                     void           logCallback(const char*),
+                                     void           reportProgress(double),
+                                     void           reportTextualProgress(const char*),
+                                     bool           shouldCancel());
 
     static KratosGeoSettlement* KratosGeoSettlement_CreateInstance();
 
     static int runSettlementStage(KratosGeoSettlement* instance,
-                           const char*          workingDirectory,
-                           const char*          projectFileName,
-                           void __stdcall logCallback(const char*),
-                           void __stdcall reportProgress(double),
-                           void __stdcall reportTextualProgress(const char*),
-                           bool __stdcall shouldCancel());
+                                  const char*          workingDirectory,
+                                  const char*          projectFileName,
+                                  void                 logCallback(const char*),
+                                  void                 reportProgress(double),
+                                  void                 reportTextualProgress(const char*),
+                                  bool                 shouldCancel());
 };
 
 } // namespace Kratos
