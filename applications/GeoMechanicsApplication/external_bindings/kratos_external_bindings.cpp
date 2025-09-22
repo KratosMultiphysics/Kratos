3b10e13348e9f757da3513f2311117a87628dea0
Richard Faasse <richard.faasse@deltares.nl>
2025-09-22 14:48:13 +0200

    Changed export mechanism of external bindings to kratos default KRATOS_API mechanism

--- applications/GeoMechanicsApplication/external_bindings/kratos_external_bindings.cpp
+++ applications/GeoMechanicsApplication/external_bindings/kratos_external_bindings.cpp
@@ -1,50 +1,50 @@
-#define EXPORT __declspec(dllexport)
 #include "kratos_external_bindings.h"
 #include "custom_workflows/custom_workflow_factory.h"
 
-extern "C" {
+#include "custom_workflows/dgeoflow.h"
+#include "custom_workflows/dgeosettlement.h"
 
-#if defined(KRATOS_COMPILED_IN_WINDOWS)
 
-EXPORT Kratos::KratosExecute* KratosExecute_CreateInstance() { return new Kratos::KratosExecute(); }
-
-EXPORT int __stdcall execute_flow_analysis(Kratos::KratosExecute* instance,
-                                           const char*            workingDirectory,
-                                           const char*            projectFile,
-                                           double                 minCriticalHead,
-                                           double                 maxCriticalHead,
-                                           double                 stepCriticalHead,
-                                           const char*            criticalHeadBoundaryModelPartName,
-                                           void __stdcall logCallback(const char*),
-                                           void __stdcall reportProgress(double),
-                                           void __stdcall reportTextualProgress(const char*),
-                                           bool __stdcall shouldCancel())
+namespace Kratos
 {
-    const Kratos::KratosExecute::CriticalHeadInfo critical_head_info(
+
+KratosExecute* ExternalBindings::KratosExecute_CreateInstance() { return new KratosExecute(); }
+
+int ExternalBindings::execute_flow_analysis(KratosExecute* instance,
+                          const char*            workingDirectory,
+                          const char*            projectFile,
+                          double                 minCriticalHead,
+                          double                 maxCriticalHead,
+                          double                 stepCriticalHead,
+                          const char*            criticalHeadBoundaryModelPartName,
+                          void                   logCallback(const char*),
+                          void                   reportProgress(double),
+                          void                   reportTextualProgress(const char*),
+                          bool                   shouldCancel())
+{
+    const KratosExecute::CriticalHeadInfo critical_head_info(
         minCriticalHead, maxCriticalHead, stepCriticalHead);
-    const Kratos::KratosExecute::CallBackFunctions call_back_functions(
+    const KratosExecute::CallBackFunctions call_back_functions(
         logCallback, reportProgress, reportTextualProgress, shouldCancel);
 
     return instance->ExecuteFlowAnalysis(workingDirectory, projectFile, critical_head_info,
                                          criticalHeadBoundaryModelPartName, call_back_functions);
 }
 
-EXPORT Kratos::KratosGeoSettlement* KratosGeoSettlement_CreateInstance()
+KratosGeoSettlement* ExternalBindings::KratosGeoSettlement_CreateInstance()
 {
-    return Kratos::CustomWorkflowFactory::CreateKratosGeoSettlement();
+    return CustomWorkflowFactory::CreateKratosGeoSettlement();
 }
 
-EXPORT int __stdcall runSettlementStage(Kratos::KratosGeoSettlement* instance,
-                                        const char*                  workingDirectory,
-                                        const char*                  projectFileName,
-                                        void __stdcall logCallback(const char*),
-                                        void __stdcall reportProgress(double),
-                                        void __stdcall reportTextualProgress(const char*),
-                                        bool __stdcall shouldCancel())
+int ExternalBindings::runSettlementStage(KratosGeoSettlement* instance,
+                       const char*                  workingDirectory,
+                       const char*                  projectFileName,
+                       void                         logCallback(const char*),
+                       void                         reportProgress(double),
+                       void                         reportTextualProgress(const char*),
+                       bool                         shouldCancel())
 {
     return instance->RunStage(workingDirectory, projectFileName, logCallback, reportProgress,
                               reportTextualProgress, shouldCancel);
 }
-
-#endif
 }
--- applications/GeoMechanicsApplication/external_bindings/kratos_external_bindings.h
+++ applications/GeoMechanicsApplication/external_bindings/kratos_external_bindings.h
@@ -1,4 +1,36 @@
-#include "custom_workflows/dgeoflow.h"
-#include "custom_workflows/dgeosettlement.h"
-#include <iostream>
-#include <stdlib.h>
+#include "includes/kratos_export_api.h"
+
+namespace Kratos
+{
+
+class KratosGeoSettlement;
+class KratosExecute;
+
+class KRATOS_API(GEO_MECHANICS_APPLICATION) ExternalBindings
+{
+    KratosExecute* KratosExecute_CreateInstance();
+
+    int execute_flow_analysis(KratosExecute* instance,
+                              const char*    workingDirectory,
+                              const char*    projectFile,
+                              double         minCriticalHead,
+                              double         maxCriticalHead,
+                              double         stepCriticalHead,
+                              const char*    criticalHeadBoundaryModelPartName,
+                              void __stdcall logCallback(const char*),
+                              void __stdcall reportProgress(double),
+                              void __stdcall reportTextualProgress(const char*),
+                              bool __stdcall shouldCancel());
+
+    KratosGeoSettlement* KratosGeoSettlement_CreateInstance();
+
+    int runSettlementStage(KratosGeoSettlement* instance,
+                           const char*          workingDirectory,
+                           const char*          projectFileName,
+                           void __stdcall logCallback(const char*),
+                           void __stdcall reportProgress(double),
+                           void __stdcall reportTextualProgress(const char*),
+                           bool __stdcall shouldCancel());
+};
+
+} // namespace Kratos
