6b675f62ac1956147b13b059c67c16ab218af2bb
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2025-09-23 13:48:30 +0200

    3rd stage with reset displacement

--- applications/GeoMechanicsApplication/tests/test_dirichlet_u_constant.py
+++ applications/GeoMechanicsApplication/tests/test_dirichlet_u_constant.py
@@ -11,14 +11,16 @@ class KratosGeoMechanicsDirichletUConstantTests(KratosUnittest.TestCase):
     def test_dirichlet_u_constant(self):
         """
         Single element compression test. The incremental elastic material should show
-        constant total_strain_yy and stress_yy over the steps, the incremental_strain_yy should be 0 from step 2
+        constant total_strain_yy and stress_yy over the steps, the incremental_strain_yy should be 0 in step 2 of every stage
         H = 1 [m]
         Young's modulus E = 10E+03 [N/m^2]
-        compress -0.1 [m] in 1 steps and keep constant the next step
+        stage 0: compress -0.1 [m] in 1 steps and keep constant the next step
+        stage 1: elongate  0.2 [m] in 1 steps and keep constant the next step
+        stage 2: compress -0.1 [m] in 1 steps and keep constant the next step reset_displacement=true
         """
         test_name    = 'dirichlet_u_constant'
         project_path = test_helper.get_file_path(test_name)
-        n_stages     = 2
+        n_stages     = 3
         run_multiple_stages.run_stages(project_path, n_stages)
 
         output_data = []
@@ -74,6 +76,29 @@ class KratosGeoMechanicsDirichletUConstantTests(KratosUnittest.TestCase):
             cauchy_stresses_1_4_yy = cauchy_stresses_1_4[1]
             self.assertAlmostEqual(E*0.1, cauchy_stresses_1_4_yy, 2)
 
+        stage_nr = 2
+        for time in [2.5, 3.0]:
+            # displacement of a top node
+            displacements_3  = test_helper.GiDOutputFileReader.nodal_values_at_time("DISPLACEMENT", time, output_data[stage_nr], [3])[0]
+            displacement_3_y = displacements_3[1]
+            self.assertAlmostEqual(-0.1, displacement_3_y, 2)
+            total_displacements_3  = test_helper.GiDOutputFileReader.nodal_values_at_time("TOTAL_DISPLACEMENT", time, output_data[stage_nr], [3])[0]
+            total_displacement_3_y = total_displacements_3[1]
+            self.assertAlmostEqual(-0.1, total_displacement_3_y, 2)
+            incremental_displacements_3  = test_helper.GiDOutputFileReader.nodal_values_at_time("INCREMENTAL_DISPLACEMENT", time, output_data[stage_nr], [3])[0]
+            incremental_displacement_3_y = incremental_displacements_3[1]
+            # the increment has nonzero value in the first step time < 1.6 avoids an exact double comparison like time == 1.5
+            expected_incr = -0.1 if time < 2.6 else 0.0
+            self.assertAlmostEqual(expected_incr, incremental_displacement_3_y, 2)
+
+            # integration point check in element 1, integration point 4 ( uniform stress and strain so an arbitrary choice )
+            green_lagrange_strains_1_4    = test_helper.GiDOutputFileReader.element_integration_point_values_at_time("GREEN_LAGRANGE_STRAIN_TENSOR", time, output_data[stage_nr], [1], [3])[0][0]
+            green_lagrange_strains_1_4_yy = green_lagrange_strains_1_4[1]
+            # total eps_yy = 0.0, but Green Lagrange strain is based on stage displacement here so -0.1
+            self.assertAlmostEqual(-0.1, green_lagrange_strains_1_4_yy, 2)
+            cauchy_stresses_1_4    = test_helper.GiDOutputFileReader.element_integration_point_values_at_time("CAUCHY_STRESS_TENSOR", time, output_data[stage_nr], [1], [3])[0][0]
+            cauchy_stresses_1_4_yy = cauchy_stresses_1_4[1]
+            self.assertAlmostEqual(0.0, cauchy_stresses_1_4_yy, 2)
 
 if __name__ == '__main__':
     KratosUnittest.main()
