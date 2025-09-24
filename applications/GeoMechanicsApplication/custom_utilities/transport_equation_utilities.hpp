b53ad378f4b0a3169d9d81adf1ae4e23e0ba0b88
markelov <gennady.markelov@deltares.nl>
2025-09-24 23:15:25 +0200

    used KRATOS_ERROR_IF

--- applications/GeoMechanicsApplication/custom_utilities/transport_equation_utilities.hpp
+++ applications/GeoMechanicsApplication/custom_utilities/transport_equation_utilities.hpp
@@ -61,10 +61,9 @@ public:
 
         Vector temp_vector(rB.size2());
         noalias(temp_vector) = prod(trans(rB), rVoigtVector);
-        if (rCouplingMatrix.size1() != rB.size2()) {
-            std::cout << "rCouplingMatrix.size1(): " << rCouplingMatrix.size1() << std::endl;
-            std::cout << "rB.size2(): " << rB.size2() << std::endl;
-        }
+        KRATOS_ERROR_IF(rCouplingMatrix.size1() != rB.size2()) <<
+            " Inconsistent sizes: rCouplingMatrix.size1(): " << rCouplingMatrix.size1() <<
+            " rB.size2(): " << rB.size2() << std::endl;
         noalias(rCouplingMatrix) = multiplier * outer_prod(temp_vector, rNp);
     }
 
