9a001e13ae6f5280277affea5f8294ccca7daf4f
Richard Faasse <richard.faasse@deltares.nl>
2025-09-24 15:18:07 +0200

    Made test more complex, since RHS actually needs to be the difference between internal forces
    * Also removed now redundant test cases

--- applications/GeoMechanicsApplication/custom_strategies/schemes/load_stepping_scheme.hpp
+++ applications/GeoMechanicsApplication/custom_strategies/schemes/load_stepping_scheme.hpp
@@ -26,6 +26,31 @@ public:
                                   const ProcessInfo& CurrentProcessInfo) override
     {
         rCurrentElement.Calculate(INTERNAL_FORCES_VECTOR, RHS_Contribution, CurrentProcessInfo);
+        RHS_Contribution -= mInternalForcesAtStartByElementId.at(rCurrentElement.GetId());
     }
+
+    void InitializeSolutionStep(ModelPart& rModelPart,
+                                GeoMechanicsTimeIntegrationScheme<TSparseSpace, TDenseSpace>::TSystemMatrixType& rA,
+                                GeoMechanicsTimeIntegrationScheme<TSparseSpace, TDenseSpace>::TSystemVectorType& rDx,
+                                GeoMechanicsTimeIntegrationScheme<TSparseSpace, TDenseSpace>::TSystemVectorType& rB) override
+    {
+        GeoMechanicsStaticScheme<TSparseSpace, TDenseSpace>::InitializeSolutionStep(rModelPart, rA, rDx, rB);
+
+        if (!mIsInitialized) {
+            for (auto& rElement : rModelPart.Elements()) {
+
+                mInternalForcesAtStartByElementId.insert({rElement.GetId(), Vector{}});
+                rElement.Calculate(INTERNAL_FORCES_VECTOR,
+                                   mInternalForcesAtStartByElementId[rElement.GetId()],
+                                   rModelPart.GetProcessInfo());
+            }
+            mIsInitialized = true;
+        }
+    }
+
+
+private:
+    std::map<std::size_t, Vector> mInternalForcesAtStartByElementId;
+    bool mIsInitialized = false;
 };
 }
--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_strategies/test_load_stepping_scheme.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_strategies/test_load_stepping_scheme.cpp
@@ -14,64 +14,36 @@
 #include "custom_strategies/schemes/load_stepping_scheme.hpp"
 #include "spaces/ublas_space.h"
 #include "tests/cpp_tests/geo_mechanics_fast_suite.h"
-
+#include "boost/numeric/ublas/assignment.hpp"
 namespace Kratos::Testing
 {
 
 using SparseSpaceType = UblasSpace<double, CompressedMatrix, Vector>;
 using LocalSpaceType  = UblasSpace<double, Matrix, Vector>;
 
-KRATOS_TEST_CASE_IN_SUITE(CanCreateLoadSteppingScheme, KratosGeoMechanicsFastSuiteWithoutKernel)
-{
-    LoadSteppingScheme<SparseSpaceType, LocalSpaceType> scheme;
-}
-
-KRATOS_TEST_CASE_IN_SUITE(LoadSteppingSchemeCanCalculateRHSContribution, KratosGeoMechanicsFastSuiteWithoutKernel)
-{
-    LoadSteppingScheme<SparseSpaceType, LocalSpaceType> scheme;
-    Element                                             element;
-
-    Matrix                   LHS_Contribution;
-    Vector                   RHS_Contribution;
-    std::vector<std::size_t> EquationId;
-    ProcessInfo              CurrentProcessInfo;
-
-    scheme.CalculateRHSContribution(element, RHS_Contribution, EquationId, CurrentProcessInfo);
-}
-
-KRATOS_TEST_CASE_IN_SUITE(LoadSteppingSchemeCanInitializeSolutionStep, KratosGeoMechanicsFastSuiteWithoutKernel)
-{
-    LoadSteppingScheme<SparseSpaceType, LocalSpaceType> scheme;
-    Element                                             element;
-
-    Matrix                   LHS_Contribution;
-    Vector                   RHS_Contribution;
-    std::vector<std::size_t> EquationId;
-    ProcessInfo              CurrentProcessInfo;
-
-    Model            model;
-    auto&            model_part = model.CreateModelPart("Main");
-    CompressedMatrix A;
-    Vector           Dx;
-    Vector           b;
-
-    scheme.InitializeSolutionStep(model_part, A, Dx, b);
-}
-
 class MockElementForLoadSteppingScheme : public Element
 {
 public:
     void Calculate(const Variable<Vector>& rVariable, Vector& Output, const ProcessInfo& rCurrentProcessInfo) override
     {
-        if (rVariable == INTERNAL_FORCES_VECTOR) Output = ScalarVector(10, 1);
+        if (rVariable == INTERNAL_FORCES_VECTOR) Output = mInternalForces;
     }
+
+    void SetInternalForces(const Vector& rInternalForces)
+    {
+        mInternalForces = rInternalForces;
+    }
+
+private:
+    Vector mInternalForces;
 };
 
-KRATOS_TEST_CASE_IN_SUITE(LoadSteppingSchemeRHSIsEqualToCurrentInternalForcesAtStartOfStage,
+KRATOS_TEST_CASE_IN_SUITE(LoadSteppingSchemeRHSAtStartOfStageIsEqualToCurrentInternalForcesAtStartMinusInternalForcesAtStart,
                           KratosGeoMechanicsFastSuiteWithoutKernel)
 {
     LoadSteppingScheme<SparseSpaceType, LocalSpaceType> scheme;
-    MockElementForLoadSteppingScheme                    element;
+    auto                    element = Kratos::make_intrusive<MockElementForLoadSteppingScheme>();
+    element->SetId(1);
 
     ProcessInfo CurrentProcessInfo;
     CurrentProcessInfo[TIME]       = 0.0;
@@ -81,15 +53,23 @@ KRATOS_TEST_CASE_IN_SUITE(LoadSteppingSchemeRHSIsEqualToCurrentInternalForcesAtS
 
     Model            model;
     auto&            model_part = model.CreateModelPart("Main");
+    model_part.AddElement(element);
     CompressedMatrix A;
     Vector           Dx;
     Vector           b;
 
+    Vector internal_forces_at_start_of_stage(4);
+    internal_forces_at_start_of_stage <<= 1.0, 2.0, 3.0, 4.0; // Change later to new creation function
+    element->SetInternalForces(internal_forces_at_start_of_stage);
     scheme.InitializeSolutionStep(model_part, A, Dx, b);
     Vector actual_right_hand_side;
-    scheme.CalculateRHSContribution(element, actual_right_hand_side, EquationId, CurrentProcessInfo);
 
-    KRATOS_EXPECT_VECTOR_RELATIVE_NEAR(actual_right_hand_side, Vector{ScalarVector(10, 1)}, 1e-6);
+    Vector difference{ScalarVector(4, 0.1)};
+    Vector current_internal_forces = internal_forces_at_start_of_stage + difference;
+    element->SetInternalForces(current_internal_forces);
+    scheme.CalculateRHSContribution(*element, actual_right_hand_side, EquationId, CurrentProcessInfo);
+
+    KRATOS_EXPECT_VECTOR_RELATIVE_NEAR(actual_right_hand_side, difference, 1e-6);
 }
 
 } // namespace Kratos::Testing
