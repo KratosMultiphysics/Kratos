f358f05b39e0bc3a93bab31d17f5b9f8fd9d2efd
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-09-30 09:43:16 +0200

    Removed variable `NODAL_JOINT_AREA` at the Python side

--- applications/GeoMechanicsApplication/python_scripts/geomechanics_solver.py
+++ applications/GeoMechanicsApplication/python_scripts/geomechanics_solver.py
@@ -374,7 +374,6 @@ class GeoMechanicalSolver(PythonSolver):
 
     def _add_smoothing_variables(self):
         self.main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
-        self.main_model_part.AddNodalSolutionStepVariable(GeoMechanicsApplication.NODAL_JOINT_AREA)
 
     def _add_dynamic_dofs(self):
         # For being consistent for Serial and Trilinos
