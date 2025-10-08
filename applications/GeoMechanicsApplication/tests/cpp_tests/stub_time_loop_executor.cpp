bc1a5588aebe125abdcf5f1084c6b528765b0454
Richard Faasse <richard.faasse@deltares.nl>
2025-10-08 16:45:50 +0200

    Formatting

--- applications/GeoMechanicsApplication/tests/cpp_tests/stub_time_loop_executor.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/stub_time_loop_executor.cpp
@@ -11,10 +11,10 @@
 //
 
 #include "stub_time_loop_executor.h"
-#include "geo_mechanics_fast_suite.h"
-#include "processes/process.h"
 #include "custom_workflows/strategy_wrapper.hpp"
 #include "custom_workflows/time_incrementor.h"
+#include "geo_mechanics_fast_suite.h"
+#include "processes/process.h"
 
 namespace Kratos
 {
