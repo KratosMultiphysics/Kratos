87f73a01bfb2303bb3dc97b2d1b0679718307329
Lluis de Miguel <lmiguel@cimne.upc.edu>
2025-10-09 17:07:51 +0200

    Minor

--- applications/LaserDrillingApplication/python_scripts/laserdrilling_analysis.py
+++ applications/LaserDrillingApplication/python_scripts/laserdrilling_analysis.py
@@ -39,18 +39,18 @@ if __name__ == "__main__":
     from sys import argv
 
     if len(argv) > 2:
-        err_msg =  'Too many input arguments!\n'
-        err_msg += 'Use this script in the following way:\n'
-        err_msg += '- With default ProjectParameters (read from "ProjectParameters.json"):\n'
-        err_msg += '    "python3 laserdrilling_analysis.py"\n'
-        err_msg += '- With custom ProjectParameters:\n'
-        err_msg += '    "python3 laserdrilling_analysis.py CustomProjectParameters.json"\n'
+        err_msg = "Too many input arguments!\n"
+        err_msg += "Use this script in the following way:\n"
+        err_msg += '- With default ProjectParameters (read from "parameters/ProjectParameters.json"):\n'
+        err_msg += '    "python3 MainKratos.py"\n'
+        err_msg += "- With custom ProjectParameters:\n"
+        err_msg += '    "python3 MainKratos.py PathToCustomProjectParameters.json"\n'
         raise Exception(err_msg)
 
     if len(argv) == 2: # ProjectParameters is being passed from outside
         project_parameters_file_name = argv[1]
     else: # using default name
-        project_parameters_file_name = "ProjectParameters.json"
+        project_parameters_file_name = "parameters/ProjectParameters.json"
 
     with open(project_parameters_file_name,'r') as parameter_file:
         parameters = KratosMultiphysics.Parameters(parameter_file.read())
