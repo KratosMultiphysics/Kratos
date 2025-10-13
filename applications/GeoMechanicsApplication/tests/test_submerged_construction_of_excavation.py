c0f7f1293131f47bf8c1779e53169c1ea5e590bc
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-10-13 13:33:35 +0200

    Started to write a Python test script
    So far, it only runs the initial stage (with the K0 procedure), and it only checks the global vertical equilibrium.

---
+++ applications/GeoMechanicsApplication/tests/test_submerged_construction_of_excavation.py
@@ -0,0 +1,53 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+import KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis as analysis
+import KratosMultiphysics.GeoMechanicsApplication.context_managers as context_managers
+import test_helper
+
+import os
+
+
+class KratosGeoMechanicsSubmergedConstructionOfExcavation(KratosUnittest.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.unsaturated_unit_weight_of_clay = 16.0e3  # N/m3
+        self.saturated_unit_weight_of_clay = 17.0e3  # N/m3
+        self.saturated_unit_weight_of_sand = 20.0e3  # N/m3
+        self.model_width = 65.0  # m
+        self.unsaturated_clay_layer_thickness = 2.0  # m
+        self.saturated_clay_layer_thickness = 18.0  # m
+        self.saturated_sand_layer_thickness = 30.0  # m
+
+    def total_reaction_y_from_output_data(self, output_data, time, node_ids):
+        reactions = test_helper.GiDOutputFileReader.nodal_values_at_time("REACTION", time, output_data, node_ids=node_ids)
+        return sum([reaction[1] for reaction in reactions])
+
+    def calculate_expected_sum_of_vertical_reaction_forces(self):
+        return self.model_width * (self.unsaturated_clay_layer_thickness * self.unsaturated_unit_weight_of_clay +
+                                   self.saturated_clay_layer_thickness * self.saturated_unit_weight_of_clay +
+                                   self.saturated_sand_layer_thickness * self.saturated_unit_weight_of_sand)
+
+    def test_run_simulation(self):
+        project_path = test_helper.get_file_path("submerged_construction_of_excavation")
+        project_parameters_filenames = ["1_Initial_stage.json"]
+
+        with context_managers.set_cwd_to(project_path):
+            model = Kratos.Model()
+            for filename in project_parameters_filenames:
+                with open(filename, "r") as f:
+                    stage_parameters = Kratos.Parameters(f.read())
+                analysis.GeoMechanicsAnalysis(model, stage_parameters).Run()
+
+        # Check vertical reaction forces
+        output_reader = test_helper.GiDOutputFileReader()
+        output_data = output_reader.read_output_from(os.path.join(project_path, "1_Initial_stage.post.res"))
+        time = -1.0
+        bottom_node_ids = [1, 4, 10, 25, 43, 69, 97, 133, 174, 218, 269, 318, 377, 434, 500, 572, 648, 732, 820, 909, 1000, 1101, 1206, 1316, 1433, 1561, 1695, 1829, 1965, 2110, 2258, 2406, 2559, 2725, 2896, 3058, 3238, 3423, 3620, 3818, 4022, 4233, 4455, 4739, 5151, 5563, 5959, 6281, 6607, 6924, 7236, 7541, 7831]
+        rel_tolerance = 0.005
+        self.assertAlmostEqual(self.total_reaction_y_from_output_data(output_data, time, bottom_node_ids),
+                               self.calculate_expected_sum_of_vertical_reaction_forces(), places=None, delta=rel_tolerance*self.calculate_expected_sum_of_vertical_reaction_forces())
+
+
+if __name__ == "__main__":
+    KratosUnittest.main()
