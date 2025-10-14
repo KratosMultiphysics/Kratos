98eaa7cafb1c0c41b4cbf4b2d007cfc3a51ddb43
Juan Ignacio Camarotti <juanicama7@gmail.com>
2025-10-14 10:13:49 +0200

    Finishing the implementation of BeamMapperLocalSystem

--- applications/MappingApplication/custom_mappers/beam_mapper.cpp
+++ applications/MappingApplication/custom_mappers/beam_mapper.cpp
@@ -274,52 +274,14 @@ void BeamMapperInterfaceInfo::ComputeRotationMatrix()
 
 }
 
-
+// This function does not do nothing for the beam mapper
 void BeamMapperLocalSystem::CalculateAll(MatrixType& rLocalMappingMatrix,
                     EquationIdVectorType& rOriginIds,
                     EquationIdVectorType& rDestinationIds,
                     MapperLocalSystem::PairingStatus& rPairingStatus) const
 {
-    if (mInterfaceInfos.size() > 0) {
-        rPairingStatus = MapperLocalSystem::PairingStatus::InterfaceInfoFound;
-
-        if (rDestinationIds.size() != 1) rDestinationIds.resize(1);
-
-        std::vector<int> nearest_neighbor_id;
-        double nearest_neighbor_distance;
-        mInterfaceInfos[0]->GetValue(nearest_neighbor_id, MapperInterfaceInfo::InfoType::Dummy);
-        mInterfaceInfos[0]->GetValue(nearest_neighbor_distance, MapperInterfaceInfo::InfoType::Dummy);
-        rOriginIds = nearest_neighbor_id;
-
-        for (std::size_t i=1; i<mInterfaceInfos.size(); ++i) {
-            // no check if this InterfaceInfo is an approximation is necessary
-            // bcs this does not exist for BeamMapper
-            double distance;
-            mInterfaceInfos[i]->GetValue(distance, MapperInterfaceInfo::InfoType::Dummy);
-
-            if (distance < nearest_neighbor_distance) {
-                nearest_neighbor_distance = distance;
-                mInterfaceInfos[i]->GetValue(nearest_neighbor_id, MapperInterfaceInfo::InfoType::Dummy);
-                rOriginIds = nearest_neighbor_id;
-            } else if (distance == nearest_neighbor_distance) {
-                mInterfaceInfos[i]->GetValue(nearest_neighbor_id, MapperInterfaceInfo::InfoType::Dummy);
-                rOriginIds.insert(rOriginIds.end(), nearest_neighbor_id.begin(), nearest_neighbor_id.end()); // appending to end
-            }
-        }
-
-        if (rLocalMappingMatrix.size1() != 1 || rLocalMappingMatrix.size2() != rOriginIds.size()) {
-            rLocalMappingMatrix.resize(1, rOriginIds.size(), false);
-        }
-        const double mapping_weight = 1.0/rOriginIds.size();
-        for (IndexType i=0; i<rOriginIds.size(); ++i) {
-            rLocalMappingMatrix(0,i) = mapping_weight;
-        }
-
-        KRATOS_DEBUG_ERROR_IF_NOT(mpNode) << "Members are not initialized!" << std::endl;
-        rDestinationIds[0] = mpNode->GetValue(INTERFACE_EQUATION_ID);
-    } else {
-        ResizeToZero(rLocalMappingMatrix, rOriginIds, rDestinationIds, rPairingStatus);
-    }
+    KRATOS_WARNING("BeamMapperLocalSystem")
+        << "CalculateAll() was called, but is not implemented for BeamMapperLocalSystem." << std::endl;
 }
 
 void BeamMapperLocalSystem::PairingInfo(std::ostream& rOStream, const int EchoLevel) const
@@ -327,8 +289,11 @@ void BeamMapperLocalSystem::PairingInfo(std::ostream& rOStream, const int EchoLe
     KRATOS_DEBUG_ERROR_IF_NOT(mpNode) << "Members are not initialized!" << std::endl;
 
     rOStream << "BeamMapperLocalSystem based on " << mpNode->Info();
-    if (EchoLevel > 3) {
+    if (EchoLevel > 1) {
         rOStream << " at Coordinates " << Coordinates()[0] << " | " << Coordinates()[1] << " | " << Coordinates()[2];
+        if (mPairingStatus == MapperLocalSystem::PairingStatus::Approximation) {
+            mpNode->SetValue(PAIRING_STATUS, 0);
+        } 
     }
 }
 
