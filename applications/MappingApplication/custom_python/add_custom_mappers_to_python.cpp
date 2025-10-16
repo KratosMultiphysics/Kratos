2c6453b2fe2bc7f3e39457a030241733d05798d9
Juan Ignacio Camarotti <juanicama7@gmail.com>
2025-10-16 15:04:32 +0200

    Correcting  a bug in the python bindings

--- applications/MappingApplication/custom_python/add_custom_mappers_to_python.cpp
+++ applications/MappingApplication/custom_python/add_custom_mappers_to_python.cpp
@@ -38,7 +38,7 @@ void  AddCustomMappersToPython(pybind11::module& m)
     const std::string beam_mapper_name("BeamMapper");
     py::class_< BeamMapperType, typename BeamMapperType::Pointer, Mapper<MapperDefinitions::SparseSpaceType, MapperDefinitions::DenseSpaceType>>(m, beam_mapper_name.c_str())
         .def("Map", py::overload_cast<const Variable<array_1d<double, 3>>&, const Variable<array_1d<double, 3>>&, const Variable<array_1d<double, 3>>&, Flags>(&BeamMapperType::Map), py::arg("origin_displacement_variable"), py::arg("origin_rotation_variable"), py::arg("destination_displacement_variable"), py::arg("mapping_options") = Kratos::Flags())
-        .def("InverseMap", py::overload_cast<const Variable<array_1d<double, 3>>&, const Variable<array_1d<double, 3>>&, const Variable<array_1d<double, 3>>&, Flags>(&BeamMapperType::Map), py::arg("origin_force_variable"), py::arg("origin_moment_variable"), py::arg("destination_force_variable"), py::arg("mapping_options") = Kratos::Flags())
+        .def("InverseMap", py::overload_cast<const Variable<array_1d<double, 3>>&, const Variable<array_1d<double, 3>>&, const Variable<array_1d<double, 3>>&, Flags>(&BeamMapperType::InverseMap), py::arg("origin_force_variable"), py::arg("origin_moment_variable"), py::arg("destination_force_variable"), py::arg("mapping_options") = Kratos::Flags())
         ;
 }
 
