5baae1b0a721d4a6811448ad5ac3ce8ceeae039c
Juan Ignacio Camarotti <juanicama7@gmail.com>
2025-10-16 15:08:59 +0200

    Adding the tests for the beam mapper

--- applications/MappingApplication/tests/test_beam_mapper.py
+++ applications/MappingApplication/tests/test_beam_mapper.py
@@ -1,13 +1,15 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
 import KratosMultiphysics as KM
-import blade_beam_mapping_test
+import test_blade_beam_mapping
 
-class BeamBladeMapping(blade_beam_mapping_test.BladeMappingTests):
+class BeamBladeMapping(test_blade_beam_mapping.BladeMappingTests):
     @classmethod
     def setUpClass(cls):
         mapper_params = KM.Parameters("""{
             "mapper_type": "beam_mapper",
+            "search_iterations"        : 30,
+            "use_corotation": true,
             "echo_level" : 0
         }""")
         super(BeamBladeMapping, cls).setUpMapper(mapper_params)
