e344117c81a6bd216857e0107c83d2e6162a0fc2
Juan Ignacio Camarotti <juanicama7@gmail.com>
2025-10-17 11:28:59 +0200

    Adding new parameters to the mapper

--- applications/MappingApplication/custom_mappers/radial_basis_function_mapper.h
+++ applications/MappingApplication/custom_mappers/radial_basis_function_mapper.h
@@ -208,7 +208,8 @@ private:
                 "solver_type": "skyline_lu_factorization"
             },
             "aerodynamic_panel_solver_settings": {
-                "project_origin_nodes_to_destination_domain_panel_solver": false
+                "project_origin_nodes_to_destination_domain_panel_solver": false,
+                "map_structural_displacements_to_panels_angles_of_attack": false
             }
         })");
     }
