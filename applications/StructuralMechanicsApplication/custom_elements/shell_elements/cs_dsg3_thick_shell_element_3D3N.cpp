01de9ff1300650e5a6a9401ae08150c58e972186
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2025-10-23 17:32:49 +0200

    adding std::

--- applications/StructuralMechanicsApplication/custom_elements/shell_elements/cs_dsg3_thick_shell_element_3D3N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/shell_elements/cs_dsg3_thick_shell_element_3D3N.cpp
@@ -505,7 +505,7 @@ void CSDSG3ThickShellElement3D3N<IS_COROTATIONAL>::CalculateBTriangle(
     TTu *= (1.0 / A4);
 
     const double beta0 = 0.5 * (1.0 - 4.0 * std::pow(GetMaterialProperty<double>(POISSON_RATIO, GetProperties()), 2));
-    noalias(aux3_by_3) = (1.5 * sqrt(beta0)) * prod(trans(Q), trans(Te));
+    noalias(aux3_by_3) = (1.5 * std::sqrt(beta0)) * prod(trans(Q), trans(Te));
     noalias(B_m_high_order) = trans(prod(TTu, aux3_by_3)) / aux_prod;
 
     // We assemble into the global one
