17bc2beba5dac4bf1f0a88c7a2544f9334a7bb72
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2025-10-28 17:09:09 +0100

    Cleaner file path string setup

--- applications/GeoMechanicsApplication/tests/test_dsettlement_validation.py
+++ applications/GeoMechanicsApplication/tests/test_dsettlement_validation.py
@@ -201,7 +201,7 @@ class KratosGeoMechanicsDSettlementValidationTests(KratosUnittest.TestCase):
 
         n_stages = 5
         project_parameters_filenames = [
-            f"../common/ProjectParameters_stage{i+1}.json" for i in range(n_stages)
+            os.path.join("..", "common", f"ProjectParameters_stage{i+1}.json") for i in range(n_stages)
         ]
         status = run_geo_settlement.run_stages(
             project_path, project_parameters_filenames
@@ -608,7 +608,7 @@ class KratosGeoMechanicsDSettlementValidationTests(KratosUnittest.TestCase):
 
         n_stages = 5
         project_parameters_filenames = [
-            f"..\common\ProjectParameters_stage{i+1}.json" for i in range(n_stages)
+            os.path.join("..", "common", f"ProjectParameters_stage{i+1}.json") for i in range(n_stages)
         ]
         status = run_geo_settlement.run_stages(
             project_path, project_parameters_filenames
@@ -984,7 +984,7 @@ class KratosGeoMechanicsDSettlementValidationTests(KratosUnittest.TestCase):
 
         n_stages = 5
         project_parameters_filenames = [
-            f"..\common\ProjectParameters_stage{i+1}.json" for i in range(n_stages)
+            os.path.join("..", "common", f"ProjectParameters_stage{i+1}.json") for i in range(n_stages)
         ]
         status = run_geo_settlement.run_stages(
             project_path, project_parameters_filenames
