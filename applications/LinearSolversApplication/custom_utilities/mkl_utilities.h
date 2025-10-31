7def1c1d8d1cc9d4b2a21f429f84dc69da35cc3e
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-10-31 16:15:23 +0100

    Move to source file

--- applications/LinearSolversApplication/custom_utilities/mkl_utilities.h
+++ applications/LinearSolversApplication/custom_utilities/mkl_utilities.h
@@ -12,9 +12,6 @@
 
 #pragma once
 
-// External includes
-#include <mkl.h>
-
 namespace Kratos
 {
 
@@ -47,36 +44,7 @@ public:
      * @note This method only performs validation and issues warnings; it does NOT modify the MKL thread count. Use EnsureMKLThreadConsistency() or SetMKLThreadCount() to apply corrections.
      * @return true if the thread configuration is consistent, false if adjustment is needed
      */
-    static bool CheckThreadNumber(const int NumberOfMKLThreads)
-    {
-        const int number_of_threads_mkl = mkl_get_max_threads();
-        if (NumberOfMKLThreads > 0) { // Manual setting
-            if (number_of_threads_mkl != NumberOfMKLThreads) {
-                KRATOS_WARNING("EigenPardisoLUSolver") << "The number of threads in MKL is: " << NumberOfMKLThreads << " instead of " << number_of_threads_mkl << std::endl;
-                return false;
-            }
-            return true;
-        } else { // NumberOfMKLThreads <= 0, for Minimal or Consistent
-            const int number_of_threads_used = ParallelUtilities::GetNumThreads();
-            if (static_cast<int>(MKLThreadSetting::Minimal) == NumberOfMKLThreads) {
-                const int min_threads = std::min(number_of_threads_mkl, number_of_threads_used);
-                if (number_of_threads_mkl != min_threads) {
-                    KRATOS_WARNING("EigenPardisoLUSolver") << "The number of threads in MKL is: " << number_of_threads_mkl << " instead of minimal: " << min_threads << std::endl;
-                    return false;
-                }
-                return true;
-            } else if (static_cast<int>(MKLThreadSetting::Consistent) == NumberOfMKLThreads) {
-                if (number_of_threads_mkl > number_of_threads_used) {
-                    KRATOS_WARNING("EigenPardisoLUSolver") << "The number of threads in MKL is: " << number_of_threads_mkl << " which is greater than the number of threads used by the application: " << number_of_threads_used << std::endl;
-                    return false;
-                }
-                return true;
-            } else {
-                KRATOS_ERROR << "Invalid MKL thread setting: " << NumberOfMKLThreads << std::endl;
-            }
-        }
-        return true;
-    }
+    static bool CheckThreadNumber(const int NumberOfMKLThreads);
 
     /**
      * @brief Sets the number of threads for MKL (Intel Math Kernel Library) operations.
@@ -90,23 +58,8 @@ public:
      * @note This method modifies the global MKL thread count using mkl_set_num_threads()
      * @note Thread count is only set once per execution (controlled by CheckThreadNumber())
      */
-    static void SetMKLThreadCount(const int NumberOfMKLThreads)
-    {
-        if (!CheckThreadNumber(NumberOfMKLThreads)) {
-            int number_of_threads_used;
-            if (NumberOfMKLThreads > 0) {
-                number_of_threads_used = NumberOfMKLThreads;
-            } else if (static_cast<int>(MKLThreadSetting::Minimal) == NumberOfMKLThreads) {
-                const int number_of_threads_mkl = mkl_get_max_threads();
-                number_of_threads_used = std::min(number_of_threads_mkl, number_of_threads_used);
-            } else if (static_cast<int>(MKLThreadSetting::Consistent) == NumberOfMKLThreads) {
-                number_of_threads_used = ParallelUtilities::GetNumThreads();
-            } else {
-                KRATOS_ERROR << "Invalid MKL thread setting: " << NumberOfMKLThreads << std::endl;
-            }
-            mkl_set_num_threads(number_of_threads_used);
-        }
-    }
+    static void SetMKLThreadCount(const int NumberOfMKLThreads);
+
 };
 
 } // namespace Kratos
