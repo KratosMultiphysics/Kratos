167f6e4ccae497a57601702bb307f228e98b5e41
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-11-04 13:57:01 +0100

    Add MKL thread configuration to Pardiso solvers: Set default parameters and validate thread settings

--- applications/LinearSolversApplication/custom_solvers/eigen_pardiso_llt_solver.h
+++ applications/LinearSolversApplication/custom_solvers/eigen_pardiso_llt_solver.h
@@ -19,6 +19,7 @@
 #include "spaces/ublas_space.h"
 #include "includes/ublas_interface.h"
 #include "includes/ublas_complex_interface.h"
+#include "custom_utilities/mkl_utilities.h"
 
 namespace Kratos {
 
@@ -41,6 +42,27 @@ public:
 
     void Initialize(Parameters settings)
     {
+        // Set default parameters
+        if (!settings.Has("num_threads_mkl")) {
+            settings.AddEmptyValue("num_threads_mkl").SetString("do_nothing"); // Default to 0 (do nothing)
+        }
+
+        // Configure number of threads for MKL Pardiso solver
+        int number_of_mkl_threads = 0;
+        if (settings["num_threads_mkl"].IsNumber()) {
+            number_of_mkl_threads = settings["num_threads_mkl"].GetInt();
+        } else if (settings["num_threads_mkl"].GetString() == "minimal") {
+            number_of_mkl_threads = static_cast<int>(MKLUtilities::MKLThreadSetting::Minimal);
+        } else if (settings["num_threads_mkl"].GetString() == "consistent") {
+            number_of_mkl_threads = static_cast<int>(MKLUtilities::MKLThreadSetting::Consistent);
+        } else if (settings["num_threads_mkl"].GetString() == "do_nothing") {
+            number_of_mkl_threads = static_cast<int>(MKLUtilities::MKLThreadSetting::Do_nothing);
+        } else {
+            KRATOS_ERROR << "Invalid value for 'num_threads_mkl': " << settings["num_threads_mkl"].GetString() << ". Accepted values are 'minimal', 'consistent', or an integer." << std::endl;
+        }
+
+        // Ensure the number of threads in MKL is the same considered for other operations
+        MKLUtilities::SetMKLThreadCount(number_of_mkl_threads);
     }
 
     bool Compute(Eigen::Map<const SparseMatrix> a)
