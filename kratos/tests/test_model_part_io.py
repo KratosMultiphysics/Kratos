2be9e53cea0c66adacd2b14530089db011ab13c9
Máté Kelemen <44344022+matekelemen@users.noreply.github.com>
2025-11-06 10:21:49 +0100

    delete mdpa output before writing again

--- kratos/tests/test_model_part_io.py
+++ kratos/tests/test_model_part_io.py
@@ -317,6 +317,8 @@ class TestModelPartIO(KratosUnittest.TestCase):
 
         # Writing a second time should yield the same results.
         # (unless writing is allowed to mutate the model part because we're not const-correct ...)
+        KratosUtils.DeleteFileIfExisting(GetFilePath("test_model_part_io_write_mesh_only.out.mdpa"))
+        KratosUtils.DeleteFileIfExisting(GetFilePath("test_model_part_io_write_mesh_only.out.time"))
         model_part_io.WriteModelPart(model_part)
         value = filecmp.cmp(GetFilePath("auxiliar_files_for_python_unittest/mdpa_files/test_model_part_io_write.mdpa"), GetFilePath("test_model_part_io_write.out.mdpa"))
         self.assertTrue(value)
