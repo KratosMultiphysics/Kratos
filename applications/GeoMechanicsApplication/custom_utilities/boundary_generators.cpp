865e3308003647cce4cfaa6f234a169c80a966b8
Richard Faasse <richard.faasse@deltares.nl>
2025-11-11 17:01:03 +0100

    Made boundary generators more robust, to account for the core geometries being incomplete.

--- applications/GeoMechanicsApplication/custom_utilities/boundary_generators.cpp
+++ applications/GeoMechanicsApplication/custom_utilities/boundary_generators.cpp
@@ -23,12 +23,38 @@ Geometry<Node>::GeometriesArrayType PointsGenerator::operator()(const Geometry<N
 
 Geometry<Node>::GeometriesArrayType EdgesGenerator::operator()(const Geometry<Node>& rGeometry) const
 {
-    return rGeometry.GenerateEdges();
+    try {
+        return rGeometry.GenerateEdges();
+    }
+    catch (const Exception& e) {
+        // Since some of the surface geometries in core do not implement GenerateEdges, but we
+        // know how to recover if the local dimension is 1, we do so here.
+        if (rGeometry.LocalSpaceDimension() == 1) {
+            Geometry<Node>::GeometriesArrayType result;
+            result.push_back(std::make_shared<Geometry<Node>>(rGeometry));
+            return result;
+        }
+
+        throw;
+    }
 }
 
 Geometry<Node>::GeometriesArrayType FacesGenerator::operator()(const Geometry<Node>& rGeometry) const
 {
-    return rGeometry.GenerateFaces();
+    try {
+        return rGeometry.GenerateFaces();
+    }
+    catch (const Exception& e) {
+        // Since some of the surface geometries in core do not implement GenerateFaces, but we
+        // know how to recover if the local dimension is 2, we do so here.
+        if (rGeometry.LocalSpaceDimension() == 2) {
+            Geometry<Node>::GeometriesArrayType result;
+            result.push_back(std::make_shared<Geometry<Node>>(rGeometry));
+            return result;
+        }
+
+        throw;
+    }
 }
 
 } // namespace Kratos
