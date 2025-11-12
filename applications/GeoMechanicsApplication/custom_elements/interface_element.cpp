4027f43def7a752c10a882caaead7d56195a55af
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2025-11-12 13:35:24 +0100

    Implemented looking at nodes shared with interfaces neighbour element.

--- applications/GeoMechanicsApplication/custom_elements/interface_element.cpp
+++ applications/GeoMechanicsApplication/custom_elements/interface_element.cpp
@@ -190,8 +190,7 @@ void InterfaceElement::Initialize(const ProcessInfo& rCurrentProcessInfo)
         mConstitutiveLaws.push_back(GetProperties()[CONSTITUTIVE_LAW]->Clone());
     }
     // Only interpolate when neighbouring elements that provide nodal stresses were found
-    if (this->Has(NEIGHBOUR_ELEMENTS))
-        InterpolateNodalStressesToIntegrationPointTractions();
+    if (this->Has(NEIGHBOUR_ELEMENTS)) InterpolateNodalStressesToIntegrationPointTractions();
     for (auto i = std::size_t{0}; i < mConstitutiveLaws.size(); ++i) {
         mConstitutiveLaws[i]->InitializeMaterial(GetProperties(), GetGeometry(),
                                                  shape_function_values_at_integration_points[i]);
@@ -322,28 +321,39 @@ void InterfaceElement::ApplyRotationToBMatrix(Matrix& rBMatrix, const Matrix& rR
 
 void InterfaceElement::InterpolateNodalStressesToIntegrationPointTractions() const
 {
-    auto&               r_geometry = GetGeometry();
+    auto neighbour_elements = this->GetValue(NEIGHBOUR_ELEMENTS);
+    // so far pick the first ( this may have to change when an interface is between 2 soils
+    auto&      r_neighbour_geometry = neighbour_elements[0].GetGeometry();
+    const auto neighbour_nodes = GeometryUtilities::GetNodeIdsFromGeometry(r_neighbour_geometry);
+
+    // interpolate nodal stresses on a chosen side
+    auto& r_interface_geometry = GetGeometry();
+    const auto interface_element_nodes = GeometryUtilities::GetNodeIdsFromGeometry(r_interface_geometry);
+    // check which side is shared with the neighbour geometry, by checking for the first nodes of the first side
+    auto in_first_side = std::find(neighbour_nodes.begin(), neighbour_nodes.end(),
+                                   interface_element_nodes[0]) != neighbour_nodes.end();
+
+    std::size_t start_index = in_first_side ? 0 : interface_element_nodes.size() / 2;
     std::vector<Vector> nodal_stresses;
-    for (auto& r_node : r_geometry) {
-        nodal_stresses.push_back(r_node.FastGetSolutionStepValue(CAUCHY_STRESS_VECTOR));
+    for (auto i = 0; i < interface_element_nodes.size() / 2; ++i) {
+        nodal_stresses.push_back(r_interface_geometry[start_index + i].FastGetSolutionStepValue(CAUCHY_STRESS_VECTOR));
     }
 
-    // interpolate nodal stresses on first side
     std::size_t integration_point_index = 0;
     for (const auto& r_integration_point : mIntegrationScheme->GetIntegrationPoints()) {
-        // interpolate first interface side
+        // interpolate on interface
         auto integration_point_stress                = Vector{ZeroVector(nodal_stresses[0].size())};
         auto integration_point_shape_function_values = Vector{};
-        r_geometry.ShapeFunctionsValues(integration_point_shape_function_values, r_integration_point);
-        for (std::size_t node = 0; node < GetGeometry().PointsNumber() / 2; ++node) {
-            integration_point_stress += integration_point_shape_function_values[node] * nodal_stresses[node];
+        r_interface_geometry.ShapeFunctionsValues(integration_point_shape_function_values, r_integration_point);
+        for (std::size_t i = 0; i < GetGeometry().PointsNumber() / 2; ++i) {
+            integration_point_stress += integration_point_shape_function_values[i] * nodal_stresses[i];
         }
 
         auto rotation_matrix = Matrix{ZeroMatrix{3}};
-        if (r_geometry.LocalSpaceDimension() == 1) {
+        if (r_interface_geometry.LocalSpaceDimension() == 1) {
             auto two_d_rotation_matrix = mfpCalculateRotationMatrix(GetGeometry(), r_integration_point);
             GeoElementUtilities::AssembleUUBlockMatrix(rotation_matrix, two_d_rotation_matrix);
-            rotation_matrix(2, 2) = 1.0;
+            rotation_matrix(2,2) = 1.0;
         } else {
             rotation_matrix = mfpCalculateRotationMatrix(GetGeometry(), r_integration_point);
         }
@@ -353,18 +363,16 @@ void InterfaceElement::InterpolateNodalStressesToIntegrationPointTractions() con
             integration_point_stress_tensor, rotation_matrix);
 
         // extract normal and shear components to form initial traction
-        Vector traction_vector;
-        if (r_geometry.LocalSpaceDimension() == 1) {
-            traction_vector    = ZeroVector(VOIGT_SIZE_2D_INTERFACE);
-            traction_vector(0) = integration_point_local_stress_tensor(1, 1);
-            traction_vector(1) = integration_point_local_stress_tensor(0, 1);
+        auto traction_vector = Vector(ZeroVector(mConstitutiveLaws[0]->GetStrainSize()));
+        if (r_interface_geometry.LocalSpaceDimension() == 1) {
+            traction_vector[0] = integration_point_local_stress_tensor(1, 1);
+            traction_vector[1] = integration_point_local_stress_tensor(0, 1);
         } else {
-            traction_vector    = ZeroVector(VOIGT_SIZE_3D_INTERFACE);
-            traction_vector(0) = integration_point_local_stress_tensor(2, 2);
-            traction_vector(1) = integration_point_local_stress_tensor(0, 2);
-            traction_vector(2) = integration_point_local_stress_tensor(1, 2);
+            traction_vector[0] = integration_point_local_stress_tensor(2, 2);
+            traction_vector[1] = integration_point_local_stress_tensor(0, 2);
+            traction_vector[2] = integration_point_local_stress_tensor(1, 2);
         }
-        auto initial_state =
+        const auto initial_state =
             make_intrusive<InitialState>(traction_vector, InitialState::InitialImposingType::STRESS_ONLY);
         mConstitutiveLaws[integration_point_index]->SetInitialState(initial_state);
         ++integration_point_index;
--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_interface_element.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/test_interface_element.cpp
@@ -18,6 +18,7 @@
 #include "custom_elements/interface_stress_state.h"
 #include "custom_geometries/interface_geometry.h"
 #include "geo_mechanics_application_variables.h"
+#include "test_setup_utilities/element_setup_utilities.h"
 #include "tests/cpp_tests/geo_mechanics_fast_suite.h"
 #include "tests/cpp_tests/test_utilities.h"
 
@@ -1305,17 +1306,23 @@ KRATOS_TEST_CASE_IN_SUITE(LineInterfaceElement_InterpolatesNodalStresses, Kratos
     PointerVector<Node> nodes;
     nodes.push_back(r_model_part.CreateNewNode(0, 0.0, 0.0, 0.0));
     nodes.push_back(r_model_part.CreateNewNode(1, 1.0, 0.0, 0.0));
-    nodes.push_back(r_model_part.CreateNewNode(2, 0.0, 0.0, 0.0));
-    nodes.push_back(r_model_part.CreateNewNode(3, 1.0, 0.0, 0.0));
+    // create a neighbour element, properties are irrelevant, it will have stresses on its nodes.
+    auto neighbour_element = ElementSetupUtilities::Create2D2NElement(nodes, p_properties);
+
+    nodes.push_back(r_model_part.CreateNewNode(2, 0.0, 1.0, 0.0));
+    nodes.push_back(r_model_part.CreateNewNode(3, 1.0, 1.0, 0.0));
+    //second_neighbour_nodes.push_back(r_model_part.pGetNode(2));
     const auto p_geometry = std::make_shared<LineInterfaceGeometry2D2Plus2Noded>(nodes);
-    auto element = CreateInterfaceElementWithUDofs<Interface2D>(p_properties, p_geometry);
-    Variable<GlobalPointersVector<Element>>::Type neighbours;
+    auto       element    = CreateInterfaceElementWithUDofs<Interface2D>(p_properties, p_geometry);
+    Variable<GlobalPointersVector<Element>>::Type neighbours{neighbour_element};
     element.SetValue(NEIGHBOUR_ELEMENTS, neighbours);
-    auto r_geometry = element.GetGeometry();
-    for (auto& r_node : r_geometry) {
+    // provide stresses on nodes of neighbour element
+    auto r_neighbour_geometry = neighbour_element->GetGeometry();
+    for (auto& r_node : r_neighbour_geometry) {
         auto   x = r_node.GetInitialPosition().X();
+        auto   y = r_node.GetInitialPosition().Y();
         Vector nodal_stress{4};
-        nodal_stress <<= x, 10.0 + x, 78.0, 5.0 + x;
+        nodal_stress <<= x + y, 10.0 + x + y, 78.0, 5.0 + x + y;
         r_node.FastGetSolutionStepValue(CAUCHY_STRESS_VECTOR) = nodal_stress;
     }
 
@@ -1328,6 +1335,7 @@ KRATOS_TEST_CASE_IN_SUITE(LineInterfaceElement_InterpolatesNodalStresses, Kratos
 
     // Assert
     auto expected_traction_vector = Vector{2};
+    // answers for 1st side
     expected_traction_vector <<= 10.0, 5.0;
     KRATOS_EXPECT_VECTOR_RELATIVE_NEAR(actual_traction_vectors[0], expected_traction_vector, Defaults::relative_tolerance)
     expected_traction_vector <<= 11.0, 6.0;
