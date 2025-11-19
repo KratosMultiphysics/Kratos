426f67c6b1d873efc3eac4c2bc0ffac2afd50b47
Richard Faasse <richard.faasse@deltares.nl>
2025-11-19 14:41:27 +0100

    Added test file for the new integration test
    The assertion is still commented out, since the functionality is not yet fully implemented (BDD)

---
+++ applications/GeoMechanicsApplication/tests/interface_prestress.py
@@ -0,0 +1,28 @@
+import os
+
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+from KratosMultiphysics.GeoMechanicsApplication.gid_output_file_reader import GiDOutputFileReader
+import KratosMultiphysics.GeoMechanicsApplication.run_multiple_stages as run_multiple_stages
+import test_helper
+
+class KratosGeoMechanicsInterfacePreStressTests(KratosUnittest.TestCase):
+    def test_interface_pre_stress(self):
+        test_name    = 'test_interface_prestress'
+        project_path = test_helper.get_file_path(test_name)
+        n_stages     = 2
+        run_multiple_stages.run_stages(project_path, n_stages)
+
+        reader = GiDOutputFileReader()
+        output_data_stage_2 = reader.read_output_from(os.path.join(project_path, 'gid_output', f'interface_prestress_test_Stage_2.post.res'))
+
+        nodal_displacements = reader.nodal_values_at_time('DISPLACEMENT', 1, output_data_stage_2)
+
+        for nodal_displacement in nodal_displacements:
+            y_displacement = nodal_displacement[1]
+            # The following assertion is commented out, since the process doesn't exist yet.
+            # self.assertAlmostEqual(y_displacement, 0, places=6)
+
+
+
+if __name__ == '__main__':
+    KratosUnittest.main()
