8d2fd7dbcdef875410e6c5867fcdcc5aa43bdb23
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-11-19 11:18:14 +0100

    Expect that no neighbours are found when the interface model part is empty

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_processes/test_find_neighbours_of_interfaces_process.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_processes/test_find_neighbours_of_interfaces_process.cpp
@@ -11,6 +11,7 @@
 //
 
 #include "custom_processes/find_neighbours_of_interfaces_process.h"
+#include "test_setup_utilities/model_setup_utilities.h"
 #include "tests/cpp_tests/geo_mechanics_fast_suite.h"
 
 namespace Kratos::Testing
@@ -20,6 +21,23 @@ KRATOS_TEST_CASE_IN_SUITE(FindNeighboursOfInterfacesProcess_IsAProcess, KratosGe
 {
     const auto process = FindNeighboursOfInterfacesProcess{};
     EXPECT_NE(dynamic_cast<const Process*>(&process), nullptr);
+}
+
+KRATOS_TEST_CASE_IN_SUITE(FindNeighboursOfInterfacesProcess_FindsNoNeighboursWhenAdjacentModelPartIsEmpty,
+                          KratosGeoMechanicsFastSuiteWithoutKernel)
+{
+    // Arrange
+    auto  model = Model{};
+    auto& r_model_part_with_interface_element =
+        ModelSetupUtilities::CreateModelPartWithASingle3D6NInterfaceElement(model);
+    model.CreateModelPart("EmptyModelPart");
+    auto process = FindNeighboursOfInterfacesProcess{};
+
+    // Act
+    process.ExecuteInitialize();
+
+    // Assert
+    EXPECT_EQ(r_model_part_with_interface_element.Elements().front().GetValue(NEIGHBOUR_ELEMENTS).size(), 0);
 }
 
 } // namespace Kratos::Testing
