087379067cb47fc6291ae5a0caa59f69f1147a9f
Andi Katili <andi.mkatili@gmail.com>
2025-11-24 14:57:21 +0100

    renaming test name for clarity

--- applications/MPMApplication/tests/mpm_test_factory.py
+++ applications/MPMApplication/tests/mpm_test_factory.py
@@ -110,7 +110,7 @@ class PenaltyImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(
 class LagrangeImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(MPMTestFactory):
     file_name = "beam_tests/hyperelastic_cantilever_beam/lagrange_self_weight_load_2D_quad_test"
 
-class LagrangeCenterImpositionBeamCantileverStaticHyperelasticSelfWeightLoad2DQuadTest(MPMTestFactory):
+class LagrangeCenterImpositionBeamCantileverQuasiStaticHyperelasticSelfWeightLoad2DQuadTest(MPMTestFactory):
     file_name = "beam_tests/hyperelastic_cantilever_beam/lagrange_center_self_weight_load_2D_quad_test"
 
 ### Perturbed Lagrange Imposition Tests
