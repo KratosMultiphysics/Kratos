4246ba58ac9fdb36a11b5282f1576e341478b851
Richard Faasse <richard.faasse@deltares.nl>
2025-11-28 16:26:09 +0100

    Transfered test_axisymmetric_U_Pw_normal_face_load_condition.cpp

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_conditions/test_axisymmetric_U_Pw_normal_face_load_condition.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_conditions/test_axisymmetric_U_Pw_normal_face_load_condition.cpp
@@ -12,17 +12,18 @@
 
 #include "containers/model.h"
 #include "custom_conditions/axisymmetric_U_Pw_normal_face_load_condition.hpp"
+#include "geometries/line_2d_2.h"
+#include "geometries/line_2d_3.h"
 #include "geometries/line_2d_4.h"
 #include "geometries/line_2d_5.h"
-#include "tests/cpp_tests/geo_mechanics_fast_suite.h"
+#include "tests/cpp_tests/geo_mechanics_fast_suite_without_kernel.h"
 
 using namespace Kratos;
 
 namespace Kratos::Testing
 {
 
-KRATOS_TEST_CASE_IN_SUITE(AxisymmetricUPwNormalFaceLoadCondition_GetIntegrationMethodForAllRegisteredElements,
-                          KratosGeoMechanicsFastSuiteWithoutKernel)
+TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, AxisymmetricUPwNormalFaceLoadCondition_GetIntegrationMethodForAllRegisteredElements)
 {
     const auto          p_properties = std::make_shared<Properties>();
     PointerVector<Node> nodes;
@@ -33,28 +34,28 @@ KRATOS_TEST_CASE_IN_SUITE(AxisymmetricUPwNormalFaceLoadCondition_GetIntegrationM
     auto p_axisymmetric_U_Pw_normal_face_load_condition_2D2N =
         make_intrusive<AxisymmetricUPwNormalFaceLoadCondition<2, 2>>(
             1, std::make_shared<Line2D2<Node>>(nodes), p_properties);
-    KRATOS_EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D2N->GetIntegrationMethod(),
+    EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D2N->GetIntegrationMethod(),
                      GeometryData::IntegrationMethod::GI_GAUSS_2);
 
     nodes.push_back(make_intrusive<Node>(3, 1.0, 1.0, 0.0));
     auto p_axisymmetric_U_Pw_normal_face_load_condition_2D3N =
         make_intrusive<AxisymmetricUPwNormalFaceLoadCondition<2, 3>>(
             1, std::make_shared<Line2D3<Node>>(nodes), p_properties);
-    KRATOS_EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D3N->GetIntegrationMethod(),
+    EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D3N->GetIntegrationMethod(),
                      GeometryData::IntegrationMethod::GI_GAUSS_2);
 
     nodes.push_back(make_intrusive<Node>(4, 0.5, 0.0, 0.0));
     auto p_axisymmetric_U_Pw_normal_face_load_condition_2D4N =
         make_intrusive<AxisymmetricUPwNormalFaceLoadCondition<2, 4>>(
             1, std::make_shared<Line2D4<Node>>(nodes), p_properties);
-    KRATOS_EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D4N->GetIntegrationMethod(),
+    EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D4N->GetIntegrationMethod(),
                      GeometryData::IntegrationMethod::GI_GAUSS_3);
 
     nodes.push_back(make_intrusive<Node>(5, 1.0, 0.5, 0.0));
     auto p_axisymmetric_U_Pw_normal_face_load_condition_2D5N =
         make_intrusive<AxisymmetricUPwNormalFaceLoadCondition<2, 5>>(
             1, std::make_shared<Line2D5<Node>>(nodes), p_properties);
-    KRATOS_EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D5N->GetIntegrationMethod(),
+    EXPECT_EQ(p_axisymmetric_U_Pw_normal_face_load_condition_2D5N->GetIntegrationMethod(),
                      GeometryData::IntegrationMethod::GI_GAUSS_5);
 }
 
