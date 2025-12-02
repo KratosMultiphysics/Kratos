92ed23910adb386bf5d639caee145f7b82029fdc
Richard Faasse <richard.faasse@deltares.nl>
2025-12-02 16:39:32 +0100

    Transferred test_fluid_pressure.cpp

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_utilities/test_fluid_pressure.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_utilities/test_fluid_pressure.cpp
@@ -12,15 +12,14 @@
 
 #include "boost/numeric/ublas/assignment.hpp"
 #include "custom_utilities/transport_equation_utilities.hpp"
-#include "includes/checks.h"
-#include "tests/cpp_tests/geo_mechanics_fast_suite.h"
+#include "tests/cpp_tests/geo_mechanics_fast_suite_without_kernel.h"
 
 using namespace Kratos;
 
 namespace Kratos::Testing
 {
 
-KRATOS_TEST_CASE_IN_SUITE(CalculateFluidPressureGivesCorrectResults, KratosGeoMechanicsFastSuiteWithoutKernel)
+TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, CalculateFluidPressureGivesCorrectResults)
 {
     Vector N(5);
     N <<= 1.0, 2.0, 3.0, 4.0, 5.0;
@@ -30,7 +29,7 @@ KRATOS_TEST_CASE_IN_SUITE(CalculateFluidPressureGivesCorrectResults, KratosGeoMe
 
     auto fluid_pressure = GeoTransportEquationUtilities::CalculateFluidPressure(N, pressure_vector);
     double expected_value = 9.9;
-    KRATOS_CHECK_NEAR(fluid_pressure, expected_value, 1e-12);
+    EXPECT_NEAR(fluid_pressure, expected_value, 1e-12);
 }
 
 } // namespace Kratos::Testing
