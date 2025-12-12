5c9c6bf6ccfbb182d454c8c16fa1afc6df38226f
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2025-12-12 16:17:10 +0100

    Working version

--- kratos/future/containers/linear_operator.h
+++ kratos/future/containers/linear_operator.h
@@ -29,7 +29,26 @@ namespace Kratos::Future
 ///@name Kratos Classes
 ///@{
 
+namespace
+{
+    // Lazy wrapper to prevent instantiation unless selected
+    template<typename... Ts>
+    struct matrix_type_or_void
+    {
+        using type = void;
+        using p_type = void*;
+    };
+
+    template<typename T0, typename... Ts>
+    struct matrix_type_or_void<T0, Ts...>
+    {
+        using type = T0;
+        using p_type = typename T0::Pointer;
+    };
+}
+
 /**
+ * @brief
  * @class LinearOperator
  * @ingroup KratosCore
  * @brief Auxiliary container to store a linear operator
@@ -43,18 +62,17 @@ namespace Kratos::Future
  *  - `Apply()`: performs the product \( y = A(x) \)
  *  - `ApplyTranspose()`: performs the product \( y = A^T(x) \)
  *
- * This structure can be used to represent:
- *  - Explicit matrices (via wrapping)
- *  - Matrix-free operators (e.g. preconditioners, Schur complements)
- *  - Transpose operations
- *
+ * This structure can be used to:
+ *  - Represent explicit CSR matrices (via wrapping)
+ *  - Derive matrix-free operators (e.g. preconditioners, Schur complements)
  * @tparam TSystemVectorType The system vector type used in the Kratos linear algebra backend
+ * @tparam Ts Optional parameter pack representing the sparse matrix type (e.g., CsrMatrix<double>).
  */
-template <class TSystemVectorType, typename... TSparseMatrixType>
+template <class TSystemVectorType, typename... Ts>
 class LinearOperator
 {
 
-static_assert(sizeof...(TSparseMatrixType) < 2, "TSparseMatrixType must be either a single type representing the sparse matrix type or empty to indicate a matrix-free operator.");
+static_assert(sizeof...(Ts) < 2, "Ts must be either a single type representing the sparse matrix type or empty to indicate a matrix-free operator.");
 
 public:
     ///@name Type Definitions
@@ -63,9 +81,14 @@ public:
     /// Pointer definition of LinearOperator
     KRATOS_CLASS_POINTER_DEFINITION(LinearOperator);
 
-    static constexpr bool IsCsr = sizeof...(TSparseMatrixType) == 1;
+    /// Sparse matrix type (if any) or void
+    using TSparseMatrixType = typename matrix_type_or_void<Ts...>::type;
 
-    using TSparseMatrixTypePointer = std::conditional_t<IsCsr, typename std::tuple_element<0, std::tuple<TSparseMatrixType...>>::type::Pointer, void*>;
+    /// Sparse matrix pointer type (if any) or void*
+    using TSparseMatrixTypePointer = typename matrix_type_or_void<Ts...>::p_type;
+
+    /// Boolean indicating whether a CSR matrix type is provided
+    static constexpr bool kIsMatrixFree = std::is_void_v<TSparseMatrixType>;
 
     ///@}
     ///@name Life Cycle
@@ -78,30 +101,40 @@ public:
     LinearOperator() = default;
 
     /**
-     * @brief Constructor from row/column sizes and function objects.
+     * @brief Constructor from row/column sizes.
      * @param NumRows Number of rows of the operator
      * @param NumCols Number of columns of the operator
      */
     LinearOperator(
         std::size_t NumRows,
-        std::size_t NumCols) requires(!IsCsr)
+        std::size_t NumCols) requires(kIsMatrixFree)
         : mNumRows(NumRows)
         , mNumCols(NumCols)
     {
     }
 
     /**
-     * @brief Constructor from a pair of (rows, cols) and function objects.
+     * @brief Constructor from a pair of (rows, cols).
      * @param Size Pair containing {NumRows, NumCols}
      */
-    LinearOperator(std::pair<std::size_t, std::size_t> Size) requires(!IsCsr)
+    LinearOperator(std::pair<std::size_t, std::size_t> Size) requires(kIsMatrixFree)
         : mNumRows(std::get<0>(Size))
         , mNumCols(std::get<1>(Size))
     {
     }
 
-    LinearOperator(TSparseMatrixType... Args) requires(IsCsr)
+    /**
+     * @brief Constructor from a CSR matrix.
+     * Constructs a LinearOperator that wraps the provided CSR matrix.
+     * Note that this constructor is only enabled when a CSR matrix type is provided.
+     * @param rA Reference to the CSR matrix
+     */
+    LinearOperator(Ts& ...rA) requires(!kIsMatrixFree)
     {
+        auto& r_A = std::get<0>(std::forward_as_tuple(rA...)); // Extract the CSR matrix from the parameter pack
+        mpCsrMatrix = std::make_shared<TSparseMatrixType>(r_A);
+        mNumRows = mpCsrMatrix->size1();
+        mNumCols = mpCsrMatrix->size2();
     }
 
     /// Deleted copy constructor (non-copyable)
@@ -137,6 +170,13 @@ public:
         const TSystemVectorType& rX,
         TSystemVectorType& rY)
     {
+        if constexpr (!kIsMatrixFree) {
+            // Apply the CSR matrix
+            mpCsrMatrix->SpMV(rX, rY);
+        } else {
+            // Matrix-free application (to be implemented by the user)
+            KRATOS_ERROR << "Matrix-free Apply() must be implemented in derived classes." << std::endl;
+        }
     }
 
     /**
@@ -149,6 +189,13 @@ public:
         const TSystemVectorType& rX,
         TSystemVectorType& rY)
     {
+        if constexpr (!kIsMatrixFree) {
+            // Apply the transpose of the CSR matrix
+            mpCsrMatrix->TransposeSpMV(rX, rY);
+        } else {
+            // Matrix-free transpose application (to be implemented by the user)
+            KRATOS_ERROR << "Matrix-free ApplyTranspose() must be implemented in derived classes." << std::endl;
+        }
     }
 
     /**
@@ -159,6 +206,7 @@ public:
     {
         mNumRows = 0;
         mNumCols = 0;
+        mpCsrMatrix = nullptr;
     }
 
     ///@}
@@ -205,6 +253,15 @@ public:
         return mNumCols;
     }
 
+    /**
+     * @brief Check if the operator is matrix-free.
+     * @return True if the operator is matrix-free, false if it wraps a CSR matrix
+     */
+    bool IsMatrixFree() const
+    {
+        return kIsMatrixFree;
+    }
+
     ///@}
 
 private:
@@ -214,6 +271,9 @@ private:
 
     /// Number of columns of the operator
     std::size_t mNumCols = 0;
+
+    /// Pointer to the CSR matrix (if applicable)
+    TSparseMatrixTypePointer mpCsrMatrix = nullptr;
 
 }; // class LinearOperator
 
--- kratos/future/tests/cpp_tests/test_future_linear_operator.cpp
+++ kratos/future/tests/cpp_tests/test_future_linear_operator.cpp
@@ -18,40 +18,29 @@
 namespace Kratos::Testing
 {
 
-// namespace
-// {
-//     std::size_t SetLinearSystem(
-//         CsrMatrix<>& rLHS,
-//         SystemVector<>& rRHS)
-//     {
-//         // Set the matrix map and assign it to the provided LHS matrix
-//         typename CsrMatrix<double>::MatrixMapType matrix_map;
-//         matrix_map[{0, 0}] = 10; matrix_map[{0, 3}] = -2;
-//         matrix_map[{1, 1}] = 8; matrix_map[{1, 2}] = -1;
-//         matrix_map[{2, 2}] = 5; matrix_map[{2, 1}] = -1; matrix_map[{2, 4}] = -2;
-//         matrix_map[{3, 3}] = 7; matrix_map[{3, 0}] = -2; matrix_map[{3, 4}] = -1;
-//         matrix_map[{4, 4}] = 6; matrix_map[{4, 2}] = -2; matrix_map[{4, 3}] = -1;
-//         rLHS = CsrMatrix<double>(matrix_map);
-
-//         // Set the RHS vector
-//         DenseVector<double> aux_data(5);
-//         aux_data[0] = 3.0;
-//         aux_data[1] = 7.0;
-//         aux_data[2] = 2.0;
-//         aux_data[3] = 5.0;
-//         aux_data[4] = 1.0;
-//         rRHS = SystemVector<>(aux_data);
-
-//         // Return the system size
-//         return rRHS.size();
-//     }
-// }
-
-KRATOS_TEST_CASE_IN_SUITE(FutureLinearOperator, KratosCoreFutureSuite)
+KRATOS_TEST_CASE_IN_SUITE(LinearOperatorEmpty, KratosCoreFutureSuite)
 {
     // Set up the linear operator
     Future::LinearOperator<SystemVector<>> linear_operator(std::make_pair(5,5));
 
+    // Check linear operator features
+    KRATOS_EXPECT_EQ(linear_operator.Rows(), 5);
+    KRATOS_EXPECT_EQ(linear_operator.Cols(), 5);
+    KRATOS_EXPECT_TRUE(linear_operator.IsMatrixFree());
+
+    // Check that applying the base operator raises an error
+    const SystemVector<> input_vector(5);
+    SystemVector<> output_vector(5);
+    KRATOS_EXPECT_EXCEPTION_IS_THROWN(
+        linear_operator.Apply(input_vector, output_vector),
+        "Matrix-free Apply() must be implemented in derived classes.");
+    KRATOS_EXPECT_EXCEPTION_IS_THROWN(
+        linear_operator.ApplyTranspose(input_vector, output_vector),
+        "Matrix-free ApplyTranspose() must be implemented in derived classes.");
+}
+
+KRATOS_TEST_CASE_IN_SUITE(LinearOperatorWithCsr, KratosCoreFutureSuite)
+{
     // Set the input and output vectors
     DenseVector<double> aux_data(5);
     aux_data[0] = 3.0;
@@ -61,30 +50,34 @@ KRATOS_TEST_CASE_IN_SUITE(FutureLinearOperator, KratosCoreFutureSuite)
     aux_data[4] = 1.0;
     SystemVector<> input_vector(aux_data);
     SystemVector<> output_vector(5);
+    output_vector.SetValue(0.0);
 
-    //
+    // Set up the CSR matrix
+    typename CsrMatrix<double>::MatrixMapType matrix_map;
+    matrix_map[{0, 0}] = 10; matrix_map[{0, 3}] = -2;
+    matrix_map[{1, 1}] = 8; matrix_map[{1, 2}] = -1;
+    matrix_map[{2, 2}] = 5; matrix_map[{2, 1}] = -1; matrix_map[{2, 4}] = -2;
+    matrix_map[{3, 3}] = 7; matrix_map[{3, 0}] = -2; matrix_map[{3, 4}] = -1;
+    matrix_map[{4, 4}] = 6; matrix_map[{4, 2}] = -2; matrix_map[{4, 3}] = -1;
+    CsrMatrix<double> csr_matrix(matrix_map);
+
+    // Set up the linear operator from the CSR matrix
+    Future::LinearOperator<SystemVector<>, CsrMatrix<double>> linear_operator(csr_matrix);
+
+    // Apply the linear operator to an input vector
     linear_operator.Apply(input_vector, output_vector);
 
+    // Check linear operator features
+    KRATOS_EXPECT_EQ(linear_operator.Rows(), 5);
+    KRATOS_EXPECT_EQ(linear_operator.Cols(), 5);
+    KRATOS_EXPECT_FALSE(linear_operator.IsMatrixFree());
 
-
-
-
-    // CsrMatrix<> LHS;
-    // SystemVector<> RHS;
-    // const std::size_t system_size = SetLinearSystem(LHS, RHS);
-
-    // // Set the linear solver to be tested
-    // Parameters skyline_lu_settings(R"({
-    // })");
-    // auto p_linear_solver = Kratos::make_unique<Future::SkylineLUFactorizationSolver<CsrMatrix<>, SystemVector<>>>(skyline_lu_settings);
-
-    // // Solve the problem
-    // SystemVector<> sol(system_size);
-    // p_linear_solver->Solve(LHS, sol, RHS);
-
-    // // Check the obtained results
-    // std::vector<double> ref_sol = {0.487946221604, 0.979601298099, 0.836810384794, 0.93973110802, 0.602225312935};
-    // KRATOS_EXPECT_VECTOR_NEAR(sol.data(), ref_sol, 1e-12);
+    // Check the output values
+    KRATOS_EXPECT_NEAR(output_vector[0], 20.0, 1e-12);
+    KRATOS_EXPECT_NEAR(output_vector[1], 54.0, 1e-12);
+    KRATOS_EXPECT_NEAR(output_vector[2], 1.0, 1e-12);
+    KRATOS_EXPECT_NEAR(output_vector[3], 28.0, 1e-12);
+    KRATOS_EXPECT_NEAR(output_vector[4], -3.0, 1e-12);
 }
 
 }
