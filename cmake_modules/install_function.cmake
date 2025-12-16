6dae4e927dca0dfbbd70ce7339e0f0eef60dbbdd
Carlos Roig <roigcarlo@gmail.com>
2025-12-16 11:16:54 +0100

    Making test installable as symlinks

--- cmake_modules/install_function.cmake
+++ cmake_modules/install_function.cmake
@@ -22,6 +22,41 @@ macro(kratos_python_install_directory INSTALL_PYTHON_USING_LINKS origin_dir dest
     endif(${INSTALL_PYTHON_USING_LINKS} MATCHES ON )
 endmacro(kratos_python_install_directory)
 
+# This function install a symlinc to an entire directory
+macro(kratos_python_install_tests)
+    set(options INSTALL_PYTHON_USING_LINKS)
+    set(oneValueArgs DIRECTORY DESTINATION)
+    set(multiValueArgs EXCLUSION_PATTERNS)
+
+    cmake_parse_arguments(TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    # Install the destination directory (required for the file structure)
+    install(DIRECTORY DESTINATION ${CMAKE_INSTALL_PREFIX}/${TEST_DESTINATION})
+
+    # Recursively find all files in the origin directory
+    message("(D)===> ${TEST_DIRECTORY}")
+    file(GLOB_RECURSE SRC_FILES RELATIVE ${TEST_DIRECTORY} ${TEST_DIRECTORY}/* )
+
+    # Filter the file list based on the exclusion patterns
+    foreach(pattern IN LISTS TEST_EXCLUSION_PATTERNS)
+        list(FILTER SRC_FILES EXCLUDE REGEX "${pattern}")
+    endforeach()
+
+    # Handle installation based on symlink preference
+    if(${INSTALL_PYTHON_USING_LINKS} MATCHES ON )
+        foreach(f ${SRC_FILES})
+            get_filename_component(barename ${f} NAME)
+            get_filename_component(relpath ${f} DIRECTORY)
+            install_symlink(${TEST_DIRECTORY}/${f} ${CMAKE_INSTALL_PREFIX}/${TEST_DESTINATION}/${relpath} ${barename} )
+        endforeach(f ${SRC_FILES})
+    else()
+        foreach(f ${SRC_FILES})
+            get_filename_component(relpath ${f} DIRECTORY)
+            install(FILES ${TEST_DIRECTORY}/${f} DESTINATION ${CMAKE_INSTALL_PREFIX}/${TEST_DESTINATION}/${relpath} )
+        endforeach(f ${SRC_FILES})
+    endif()
+endmacro(kratos_python_install_tests)
+
 macro(kratos_python_install INSTALL_PYTHON_USING_LINKS origin_file destination_file)
     get_filename_component(destination_dir ${destination_file} DIRECTORY )
     get_filename_component(destination_filename ${destination_file} NAME )
