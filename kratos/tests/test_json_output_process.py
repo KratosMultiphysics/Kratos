7fcdfbe5fe80b88a671c45eb3dbf91a08260db2d
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2025-12-19 09:27:35 +0100

    Let each test case show which test vectors are being set
    This eases to understand the test assertions.

--- kratos/tests/test_json_output_process.py
+++ kratos/tests/test_json_output_process.py
@@ -2,12 +2,9 @@ import KratosMultiphysics
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 from KratosMultiphysics.json_output_process import JsonOutputProcess
 
-from pathlib import Path
+import itertools
 import json
-
-
-def make_scalar_vector_from(value):
-    return [float(value)] * 3
+from pathlib import Path
 
 
 class TestJsonOutputProcess(KratosUnittest.TestCase):
@@ -25,9 +22,9 @@ class TestJsonOutputProcess(KratosUnittest.TestCase):
         for node_id, x, y, z in node_ids_and_coordinates:
             self.model_part.CreateNewNode(node_id, x, y, z)
 
-    def add_vectors_to_nodes_of_model_part(self, vector_variable):
-        for node in self.model_part.Nodes:
-            node.SetSolutionStepValue(vector_variable, make_scalar_vector_from(node.Id))
+    def add_vectors_to_nodes_of_model_part(self, vector_variable, nodal_vectors):
+        for node, vector in zip(self.model_part.Nodes, nodal_vectors):
+            node.SetSolutionStepValue(vector_variable, vector)
 
     def produce_json_output_file(self, process_settings):
         output_process = JsonOutputProcess(self.model, process_settings)
@@ -44,7 +41,10 @@ class TestJsonOutputProcess(KratosUnittest.TestCase):
         vector_variable_for_testing = KratosMultiphysics.EXTERNAL_FORCES_VECTOR
         self.model_part.AddNodalSolutionStepVariable(vector_variable_for_testing)
         self.add_three_nodes_to_model_part()
-        self.add_vectors_to_nodes_of_model_part(vector_variable_for_testing)
+        test_vectors = [[1.0] * 3, [2.0] * 3, [3.0] * 3]
+        self.add_vectors_to_nodes_of_model_part(
+            vector_variable_for_testing, test_vectors
+        )
 
         output_file_path = (
             Path(__file__).resolve().parent / "external_forces_output.json"
@@ -62,21 +62,23 @@ class TestJsonOutputProcess(KratosUnittest.TestCase):
         with open(output_file_path, "r") as output_file:
             output_data = json.load(output_file)
 
-        for node in self.model_part.Nodes:
+        for node, vector in zip(self.model_part.Nodes, test_vectors):
             vector_variable_results_for_all_time_steps = output_data[f"NODE_{node.Id}"][
                 vector_variable_for_testing.Name()
             ]
             self.assertEqual(len(vector_variable_results_for_all_time_steps), 1)
             self.assertVectorAlmostEqual(
-                vector_variable_results_for_all_time_steps[0],
-                make_scalar_vector_from(node.Id),
+                vector_variable_results_for_all_time_steps[0], vector
             )
 
     def test_output_of_resultant_vector_variable(self):
         vector_variable_for_testing = KratosMultiphysics.EXTERNAL_FORCES_VECTOR
         self.model_part.AddNodalSolutionStepVariable(vector_variable_for_testing)
         self.add_three_nodes_to_model_part()
-        self.add_vectors_to_nodes_of_model_part(vector_variable_for_testing)
+        test_vectors = [[1.0] * 3, [2.0] * 3, [3.0] * 3]
+        self.add_vectors_to_nodes_of_model_part(
+            vector_variable_for_testing, test_vectors
+        )
 
         output_file_path = (
             Path(__file__).resolve().parent / "resultant_external_forces_output.json"
@@ -101,7 +103,7 @@ class TestJsonOutputProcess(KratosUnittest.TestCase):
         self.assertEqual(len(resultant_vector_variable_results_for_all_time_steps), 1)
         self.assertVectorAlmostEqual(
             resultant_vector_variable_results_for_all_time_steps[0],
-            [1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 3.0, 3.0, 3.0],
+            list(itertools.chain(*test_vectors)),
         )
 
 
