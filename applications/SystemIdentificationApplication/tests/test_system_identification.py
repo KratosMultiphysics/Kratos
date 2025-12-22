457c3278813aa17810206234ab66bfc834eef025
Talhah Ansari <talhah.ansari@tum.de>
2025-12-22 03:17:27 +0100

    Making full system identification test run on single thread for tighter testing

--- applications/SystemIdentificationApplication/tests/test_system_identification.py
+++ applications/SystemIdentificationApplication/tests/test_system_identification.py
@@ -1,3 +1,6 @@
+import subprocess
+import os
+import sys
 import numpy
 import KratosMultiphysics as Kratos
 import KratosMultiphysics.KratosUnittest as UnitTest
@@ -10,50 +13,66 @@ class TestSystemIdentification(UnitTest.TestCase):
     def test_DamagedSystem(self):
         self.addCleanup(DeleteFileIfExisting, "auxiliary_files/damaged_problem/measured_data.csv")
 
-        model = Kratos.Model()
-        with open("auxiliary_files/damaged_problem/damaged_project_parameters.json", "r") as file_input:
-            params = Kratos.Parameters(file_input.read())
-        analysis = StructuralMechanicsAnalysis(model, params)
-        analysis.Run()
+        env = os.environ.copy()
+        env["OMP_NUM_THREADS"] = "1"
+        env["OPENBLAS_NUM_THREADS"] = "1"
+        env["MKL_NUM_THREADS"] = "1"
+        env["NUMEXPR_NUM_THREADS"] = "1"
+
+        subprocess.run(
+            [sys.executable, "auxiliary_files/damaged_problem/MainKratos.py"],
+            env=env,
+            check=True
+        )
 
         data = numpy.loadtxt("auxiliary_files/damaged_problem/measured_data.csv", comments="#", usecols=[0,3,4,5,6], delimiter=",")
         ref_data = numpy.loadtxt("auxiliary_files/damaged_problem/measured_data_ref.csv", comments="#", usecols=[0,3,4,5,6], delimiter=",")
-        self.assertTrue(numpy.allclose(data, ref_data, 1e-12, 1e-12))
+        self.assertTrue(numpy.allclose(data, ref_data, 1e-16, 1e-16))
 
     def test_SystemIdentification(self):
-        model = Kratos.Model()
-        with open("auxiliary_files/system_identification/optimization_parameters.json", "r") as file_input:
-            params = Kratos.Parameters(file_input.read())
+        env = os.environ.copy()
+        env["OMP_NUM_THREADS"] = "1"
+        env["OPENBLAS_NUM_THREADS"] = "1"
+        env["MKL_NUM_THREADS"] = "1"
+        env["NUMEXPR_NUM_THREADS"] = "1"
 
-        analysis = OptimizationAnalysis(model, params)
-        analysis.Run()
+        subprocess.run(
+            [sys.executable, "auxiliary_files/system_identification/MainKratos.py"],
+            env=env,
+            check=True
+        )
 
         params = Kratos.Parameters("""{
             "reference_file_name"   : "auxiliary_files/system_identification_summary_ref.csv",
             "output_file_name"      : "auxiliary_files/summary.csv",
             "remove_output_file"    : true,
             "comparison_type"       : "csv_file",
-            "tolerance"             : 1e-6,
-            "relative_tolerance"    : 1e-6,
+            "tolerance"             : 1e-16,
+            "relative_tolerance"    : 1e-16,
             "dimension"             : 3
         }""")
         CompareTwoFilesCheckProcess(params).Execute()
 
     def test_SystemIdentificationPNorm(self):
-        model = Kratos.Model()
-        with open("auxiliary_files/system_identification_p_norm/optimization_parameters.json", "r") as file_input:
-            params = Kratos.Parameters(file_input.read())
+        env = os.environ.copy()
+        env["OMP_NUM_THREADS"] = "1"
+        env["OPENBLAS_NUM_THREADS"] = "1"
+        env["MKL_NUM_THREADS"] = "1"
+        env["NUMEXPR_NUM_THREADS"] = "1"
 
-        analysis = OptimizationAnalysis(model, params)
-        analysis.Run()
+        subprocess.run(
+            [sys.executable, "auxiliary_files/system_identification_p_norm/MainKratos.py"],
+            env=env,
+            check=True
+        )
 
         params = Kratos.Parameters("""{
             "reference_file_name"   : "auxiliary_files/system_identification_p_norm_summary_ref.csv",
             "output_file_name"      : "auxiliary_files/summary_p_norm.csv",
             "remove_output_file"    : true,
             "comparison_type"       : "csv_file",
-            "tolerance"             : 1e-6,
-            "relative_tolerance"    : 1e-6,
+            "tolerance"             : 1e-16,
+            "relative_tolerance"    : 1e-16,
             "dimension"             : 3
         }""")
         CompareTwoFilesCheckProcess(params).Execute()
