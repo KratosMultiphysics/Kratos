b8e7ab47b534526e3e5594498e2d8108b80610b5
Riccardo Rossi <rrossi@cimne.upc.edu>
2026-01-04 22:47:59 +0100

    removing index tensor adaptor which should be part of a different pr

--- kratos/python/add_tensor_adaptors_to_python.cpp
+++ kratos/python/add_tensor_adaptors_to_python.cpp
@@ -34,7 +34,7 @@
 #include "tensor_adaptors/equation_ids_tensor_adaptor.h"
 #include "tensor_adaptors/gauss_point_variable_tensor_adaptor.h"
 #include "tensor_adaptors/node_position_tensor_adaptor.h"
-#include "tensor_adaptors/index_tensor_adaptor.h"
+
 #include "tensor_adaptors/geometries_tensor_adaptor.h"
 
 // Include base h
@@ -155,12 +155,7 @@ void AddTensorAdaptorsToPython(pybind11::module& m)
         .def(py::init<const NodePositionTensorAdaptor::BaseType&, Globals::Configuration, const bool>(), py::arg("tensor_adaptor"), py::arg("configuration"), py::arg("copy") = true)
         ;
 
-    py::class_<IndexTensorAdaptor, IndexTensorAdaptor::Pointer, IndexTensorAdaptor::BaseType>(tensor_adaptor_sub_module, "IndexTensorAdaptor")
-        .def(py::init<ModelPart::GeometryContainerType::Pointer>(), py::arg("container"))
-        .def(py::init<ModelPart::ElementsContainerType::Pointer>(), py::arg("container"))
-        .def(py::init<ModelPart::ConditionsContainerType::Pointer>(), py::arg("container"))
-        .def(py::init<const IndexTensorAdaptor::BaseType&, const bool>(), py::arg("tensor_adaptor"), py::arg("copy") = true)
-        ;
+
 
     using GeometriesTensorAdaptorType = GeometriesTensorAdaptor;
     py::class_<GeometriesTensorAdaptorType, GeometriesTensorAdaptorType::Pointer, GeometriesTensorAdaptorType::BaseType> geometries_tensor_adaptor(tensor_adaptor_sub_module, "GeometriesTensorAdaptor");
