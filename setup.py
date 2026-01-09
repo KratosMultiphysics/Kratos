0dbbe6de0762b37cf49a0a79583f4f8722c27323
aronnoordam <aronnoordam@gmail.com>
2026-01-09 09:51:50 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ def set_install_requirements():
     """
     Creates kratos requirements list
     """
-    release_tag = "test_release0.1.2"
-    kratos_version = "10.3.1.3"
+    release_tag = "test_release0.1.3"
+    kratos_version = "10.3.1.4"
     python_version_part = ""
     platform_part = ""
 
