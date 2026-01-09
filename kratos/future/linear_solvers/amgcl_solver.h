de88ab8d23498f19f6702acf4ab15d6199ad940f
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2026-01-09 11:32:29 +0100

    Fix autorship

--- kratos/future/linear_solvers/amgcl_solver.h
+++ kratos/future/linear_solvers/amgcl_solver.h
@@ -100,7 +100,7 @@ void KRATOS_API(KRATOS_CORE) AMGCLSolve(
  * @details Created by Denis Deminov: https://github.com/ddemidov/amgcl
  * AMGCL is a header-only C++ library for solving large sparse linear systems with algebraic multigrid (AMG) method. AMG is one of the most effective iterative methods for solution of equation systems arising, for example, from discretizing PDEs on unstructured grids. The method can be used as a black-box solver for various computational problems, since it does not require any information about the underlying geometry. AMG is often used not as a standalone solver but as a preconditioner within an iterative solver (e.g. Conjugate Gradients, BiCGStab, or GMRES).
  * AMGCL builds the AMG hierarchy on a CPU and then transfers it to one of the provided backends. This allows for transparent acceleration of the solution phase with help of OpenCL, CUDA, or OpenMP technologies. Users may provide their own backends which enables tight integration between AMGCL and the user code.
- * @author Denis Demidov
+ * @author Pooyan Dadvand
  * @author Riccardo Rossi
  * @author Ruben Zorrilla
  */
