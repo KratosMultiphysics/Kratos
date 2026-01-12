e43089c55a9e1cf76e11df51ecfe8ae582d8d11f
Carlos Roig <roigcarlo@gmail.com>
2026-01-12 16:05:08 +0100

    Restore schedule(guided, 512) nowait

--- kratos/future/solving_strategies/schemes/implicit_scheme.h
+++ kratos/future/solving_strategies/schemes/implicit_scheme.h
@@ -374,7 +374,7 @@ public:
             TLSType aux_tls;
 
             // Assemble elements
-            # pragma omp for
+            # pragma omp for schedule(guided, 512) nowait
             for (int k = 0; k < n_elems; ++k) {
                 // Calculate local LHS and RHS contributions
                 auto it_elem = elems_begin + k;
@@ -388,7 +388,7 @@ public:
             }
 
             // Assemble conditions
-            # pragma omp for
+            # pragma omp for schedule(guided, 512) nowait
             for (int k = 0; k < n_conds; ++k) {
                 // Calculate local LHS and RHS contributions
                 auto it_cond = conds_begin + k;
@@ -923,7 +923,7 @@ public:
             #pragma omp parallel
             {
                 TLSType aux_tls;
-                
+
                 // Auxiliary set to store the inactive constraints slave DOFs (required by the block build)
                 std::unordered_set<IndexType> auxiliar_inactive_slave_dofs;
 
