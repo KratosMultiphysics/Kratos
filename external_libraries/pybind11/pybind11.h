a45b583e70795a5007ccea0a5be26edf77692434
Vicente Mataix Ferrandiz <vmataix@altair.com>
2026-01-12 09:03:17 +0100

    Revert "Testing alternative solution"
    This reverts commit 935af4a1434885545088fcc0b19b7e73202af1b6.

--- external_libraries/pybind11/pybind11.h
+++ external_libraries/pybind11/pybind11.h
@@ -10,8 +10,12 @@
 
 #pragma once
 
+// See Issue #5956. This fixes compilation failure with MSVC 17.12 (v14.44) and C++20:
+// '_BACKUP_ITERATOR_DEBUG_LEVEL' undeclared when including span header from STL.
 #if defined(_MSC_VER)
-#    include <yvals_core.h>  // Ensures all iterator debug macros are defined
+#    ifndef _BACKUP_ITERATOR_DEBUG_LEVEL
+#        define _BACKUP_ITERATOR_DEBUG_LEVEL 0
+#    endif
 #endif
 
 #include "detail/class.h"
