8b5e4a76d18a3d0f42b2a48a1f60cdac5c7f1d7a
Carlos Roig <roigcarlo@gmail.com>
2026-01-20 17:10:47 +0100

    Adding header

--- kratos/tests/test_utilities/serializer_testing_utilities.h
+++ kratos/tests/test_utilities/serializer_testing_utilities.h
@@ -1,3 +1,17 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+//
+
+
 #include "includes/serializer.h"
 
 namespace Kratos::Testing
