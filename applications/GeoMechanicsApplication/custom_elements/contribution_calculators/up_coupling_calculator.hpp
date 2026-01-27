3260de5948d8e9750fe016b140a4cbd2621f8d77
Richard Faasse <richard.faasse@deltares.nl>
2026-01-27 13:13:54 +0100

    Added input provider to the constructor of the calculator + reformatting with clang-format

--- applications/GeoMechanicsApplication/custom_elements/contribution_calculators/up_coupling_calculator.hpp
+++ applications/GeoMechanicsApplication/custom_elements/contribution_calculators/up_coupling_calculator.hpp
@@ -17,50 +17,50 @@
 
 namespace Kratos
 {
-    template <unsigned int NumberOfRows, unsigned int NumberOfColumns>
-    class UPCouplingCalculator : public ContributionCalculator<NumberOfRows, NumberOfColumns>
-    {
-    public:
-        struct InputProvider
+template <unsigned int NumberOfRows, unsigned int NumberOfColumns>
+class UPCouplingCalculator : public ContributionCalculator<NumberOfRows, NumberOfColumns>
+{
+public:
+    struct InputProvider {
+        InputProvider(std::function<const Matrix&()>       GetNpContainer,
+                      Geo::BMatricesGetter                 GetBMatrices,
+                      std::function<Vector()>              GetVoigtVector,
+                      Geo::IntegrationCoefficientsGetter   GetIntegrationCoefficients,
+                      std::function<std::vector<double>()> GetBiotCoefficients,
+                      std::function<std::vector<double>()> GetBishopCoefficients)
+            : GetNpContainer(std::move(GetNpContainer)),
+              GetBMatrices(std::move(GetBMatrices)),
+              GetVoigtVector(std::move(GetVoigtVector)),
+              GetIntegrationCoefficients(std::move(GetIntegrationCoefficients)),
+              GetBiotCoefficients(std::move(GetBiotCoefficients)),
+              GetBishopCoefficients(std::move(GetBishopCoefficients))
         {
-            InputProvider(std::function<const Matrix&()> GetNpContainer, Geo::BMatricesGetter GetBMatrices,
-                          std::function<Vector()> GetVoigtVector,
-                          Geo::IntegrationCoefficientsGetter GetIntegrationCoefficients,
-                          std::function<std::vector<double>()> GetBiotCoefficients,
-                          std::function<std::vector<double>()> GetBishopCoefficients) :
-                GetNpContainer(std::move(GetNpContainer)), GetBMatrices(std::move(GetBMatrices)),
-                GetVoigtVector(std::move(GetVoigtVector)),
-                GetIntegrationCoefficients(std::move(GetIntegrationCoefficients)),
-                GetBiotCoefficients(std::move(GetBiotCoefficients)),
-                GetBishopCoefficients(std::move(GetBishopCoefficients))
-            {
-            }
-
-            std::function<const Matrix&()> GetNpContainer;
-            Geo::BMatricesGetter GetBMatrices;
-            std::function<Vector()> GetVoigtVector;
-            Geo::IntegrationCoefficientsGetter GetIntegrationCoefficients;
-            std::function<std::vector<double>()> GetBiotCoefficients;
-            std::function<std::vector<double>()> GetBishopCoefficients;
-        };
-
-
-        using BaseType = ContributionCalculator<NumberOfRows, NumberOfColumns>;
-
-        std::optional<typename BaseType::LHSMatrixType> LHSContribution() override
-        {
-            return {};
         }
 
-        typename BaseType::RHSVectorType RHSContribution() override
-        {
-            return {};
-        }
-
-        std::pair<std::optional<typename BaseType::LHSMatrixType>,
-                  typename BaseType::RHSVectorType> LocalSystemContribution() override
-        {
-            return {LHSContribution(), RHSContribution()};
-        };
+        std::function<const Matrix&()>       GetNpContainer;
+        Geo::BMatricesGetter                 GetBMatrices;
+        std::function<Vector()>              GetVoigtVector;
+        Geo::IntegrationCoefficientsGetter   GetIntegrationCoefficients;
+        std::function<std::vector<double>()> GetBiotCoefficients;
+        std::function<std::vector<double>()> GetBishopCoefficients;
     };
-}
+
+    explicit UPCouplingCalculator(InputProvider CouplingInputProvider)
+        : mInputProvider{std::move(CouplingInputProvider)}
+    {
+    }
+
+    using BaseType = ContributionCalculator<NumberOfRows, NumberOfColumns>;
+
+    std::optional<typename BaseType::LHSMatrixType> LHSContribution() override { return {}; }
+
+    typename BaseType::RHSVectorType RHSContribution() override { return {}; }
+
+    std::pair<std::optional<typename BaseType::LHSMatrixType>, typename BaseType::RHSVectorType> LocalSystemContribution() override
+    {
+        return {LHSContribution(), RHSContribution()};
+    };
+
+    InputProvider mInputProvider;
+};
+} // namespace Kratos
--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_up_coupling_calculator.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_up_coupling_calculator.cpp
@@ -15,44 +15,24 @@
 
 namespace Kratos::Testing
 {
-    TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestCanCreateUPCouplingCalculator)
-    {
-        constexpr auto number_of_u_dof = 4;
-        constexpr auto number_of_pw_dof = 2;
-        UPCouplingCalculator<number_of_u_dof, number_of_pw_dof> coupling_calculator;
-    }
 
-    TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestCanCreateUpCouplingInputProvider)
-    {
-        constexpr auto number_of_u_dof = 4;
-        constexpr auto number_of_pw_dof = 2;
+TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestCanCreateUpCouplingCalculatorWithInputProvider)
+{
+    constexpr auto number_of_u_dof  = 4;
+    constexpr auto number_of_pw_dof = 2;
 
-        auto get_b_matrices = []()
-        {
-            return std::vector<Matrix>{};
-        };
-        auto get_integration_coefficients = []()
-        {
-            return std::vector<double>{};
-        };
-        const auto np_container = Matrix{};
-        auto get_np_container = [&np_container]() -> const Matrix& { return np_container; };
-        auto get_biot_coefficients = []()
-        {
-            return std::vector<double>{};
-        };
-        auto get_bishop_coefficients = []()
-        {
-            return std::vector<double>{};
-        };
-        auto get_voigt_vector = []()
-        {
-            return Vector{};
-        };
+    auto       get_b_matrices               = []() { return std::vector<Matrix>{}; };
+    auto       get_integration_coefficients = []() { return std::vector<double>{}; };
+    const auto np_container                 = Matrix{};
+    auto       get_np_container      = [&np_container]() -> const Matrix& { return np_container; };
+    auto       get_biot_coefficients = []() { return std::vector<double>{}; };
+    auto       get_bishop_coefficients = []() { return std::vector<double>{}; };
+    auto       get_voigt_vector        = []() { return Vector{}; };
 
+    UPCouplingCalculator<number_of_u_dof, number_of_pw_dof>::InputProvider input_provider(
+        get_np_container, get_b_matrices, get_voigt_vector, get_integration_coefficients,
+        get_biot_coefficients, get_bishop_coefficients);
 
-        UPCouplingCalculator<number_of_u_dof, number_of_pw_dof>::InputProvider input_provider(
-            get_np_container, get_b_matrices, get_voigt_vector, get_integration_coefficients, get_biot_coefficients,
-            get_bishop_coefficients);
-    }
+    UPCouplingCalculator<number_of_u_dof, number_of_pw_dof> coupling_calculator(input_provider);
 }
+} // namespace Kratos::Testing
