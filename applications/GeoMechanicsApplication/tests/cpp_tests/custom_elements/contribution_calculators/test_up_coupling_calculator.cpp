430bed7e8c252ab3012a68d8509cc1f64f08ec79
Richard Faasse <richard.faasse@deltares.nl>
2026-01-27 14:27:05 +0100

    Added a test case with a matrix that's checked by hand

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_up_coupling_calculator.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_up_coupling_calculator.cpp
@@ -102,4 +102,56 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestUPCouplingMatrixContributio
     KRATOS_CHECK_MATRIX_NEAR(calculated_coupling_matrix, expected_coupling_matrix, Defaults::absolute_tolerance);
 }
 
+TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestUPCouplingMatrixContributionSmaller)
+{
+    constexpr std::size_t size_D = 2;
+    constexpr std::size_t size_N = 2;
+
+    // clang-format off
+    const auto b_matrix = UblasUtilities::CreateMatrix(
+                {{1.0,  2.0, 3.0, 4.0},
+                 {5.0,  6.0, 7.0, 8.0},
+                 {9.0, 10.0,11.0,12.0},
+                 {13.0,14.0,15.0,16.0}});
+    // clang-format on
+
+    const auto   voigt_vector            = UblasUtilities::CreateVector({1.0, 1.0, 1.0, 0.0});
+    Vector       n_p                     = UblasUtilities::CreateVector({1.0, 2.0});
+    const double integration_coefficient = 0.5;
+    const double biot_coefficient        = 2.0;
+    const double bishop_coefficient      = 0.1;
+
+    BoundedMatrix<double, size_D * size_N, size_N> expected_coupling_matrix;
+    // clang-format off
+    // Checked by hand
+    expected_coupling_matrix = UblasUtilities::CreateMatrix(
+    {{1.5, 3.0},
+     {1.8, 3.6},
+     {2.1, 4.2},
+     {2.4, 4.8}});
+    // clang-format on
+
+    constexpr auto number_of_u_dof  = 4;
+    constexpr auto number_of_pw_dof = 2;
+
+    auto get_b_matrices               = [b_matrix]() { return std::vector{b_matrix}; };
+    auto get_integration_coefficients = [integration_coefficient]() {
+        return std::vector{integration_coefficient};
+    };
+    const auto np_container     = UblasUtilities::CreateMatrix({{1.0, 2.0}});
+    auto       get_np_container = [&np_container]() -> const Matrix& { return np_container; };
+    auto get_biot_coefficients  = [biot_coefficient]() { return std::vector{biot_coefficient}; };
+    auto get_bishop_coefficients = [bishop_coefficient]() { return std::vector{bishop_coefficient}; };
+    auto get_voigt_vector = [voigt_vector]() { return voigt_vector; };
+
+    UPCouplingCalculator<number_of_u_dof, number_of_pw_dof>::InputProvider input_provider(
+        get_np_container, get_b_matrices, get_voigt_vector, get_integration_coefficients,
+        get_biot_coefficients, get_bishop_coefficients);
+
+    UPCouplingCalculator<number_of_u_dof, number_of_pw_dof> coupling_calculator(input_provider);
+
+    const auto calculated_coupling_matrix = coupling_calculator.LHSContribution().value();
+    KRATOS_CHECK_MATRIX_NEAR(calculated_coupling_matrix, expected_coupling_matrix, Defaults::absolute_tolerance);
+}
+
 } // namespace Kratos::Testing
