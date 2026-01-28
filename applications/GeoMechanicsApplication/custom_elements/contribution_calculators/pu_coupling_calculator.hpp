c893e3077fc0dde0cd36fd35802c92aca605d89f
Richard Faasse <richard.faasse@deltares.nl>
2026-01-28 08:23:23 +0100

    Added functionality + test for RHS calculation

--- applications/GeoMechanicsApplication/custom_elements/contribution_calculators/pu_coupling_calculator.hpp
+++ applications/GeoMechanicsApplication/custom_elements/contribution_calculators/pu_coupling_calculator.hpp
@@ -29,7 +29,7 @@ public:
                       Geo::IntegrationCoefficientsGetter   GetIntegrationCoefficients,
                       std::function<std::vector<double>()> GetBiotCoefficients,
                       std::function<std::vector<double>()> GetDegreesOfSaturation,
-                      std::function<Vector()>              GetNodalWaterPressures,
+                      std::function<Vector()>              GetNodalVelocities,
                       std::function<double()>              GetVelocityCoefficient)
             : GetNpContainer(std::move(GetNpContainer)),
               GetBMatrices(std::move(GetBMatrices)),
@@ -37,7 +37,7 @@ public:
               GetIntegrationCoefficients(std::move(GetIntegrationCoefficients)),
               GetBiotCoefficients(std::move(GetBiotCoefficients)),
               GetDegreesOfSaturation(std::move(GetDegreesOfSaturation)),
-              GetNodalWaterPressures(std::move(GetNodalWaterPressures)),
+              GetNodalVelocities(std::move(GetNodalVelocities)),
               GetVelocityCoefficient(std::move(GetVelocityCoefficient))
         {
         }
@@ -48,7 +48,7 @@ public:
         Geo::IntegrationCoefficientsGetter   GetIntegrationCoefficients;
         std::function<std::vector<double>()> GetBiotCoefficients;
         std::function<std::vector<double>()> GetDegreesOfSaturation;
-        std::function<Vector()>              GetNodalWaterPressures;
+        std::function<Vector()>              GetNodalVelocities;
         std::function<double()>              GetVelocityCoefficient;
     };
 
@@ -63,13 +63,13 @@ public:
     {
         // The NumberOfColumns and NumberOfRows are swapped because the coupling matrix is
         // transposed before returning it.
-        Matrix result       = ZeroMatrix(NumberOfColumns, NumberOfRows);
-        const auto                       b_matrices   = mInputProvider.GetBMatrices();
-        const auto                       voigt_vector = mInputProvider.GetVoigtVector();
-        const auto integration_coefficients           = mInputProvider.GetIntegrationCoefficients();
-        const auto biot_coefficients                  = mInputProvider.GetBiotCoefficients();
-        const auto bishop_coefficients                = mInputProvider.GetDegreesOfSaturation();
-        const auto np_container                       = mInputProvider.GetNpContainer();
+        Matrix     result                   = ZeroMatrix(NumberOfColumns, NumberOfRows);
+        const auto b_matrices               = mInputProvider.GetBMatrices();
+        const auto voigt_vector             = mInputProvider.GetVoigtVector();
+        const auto integration_coefficients = mInputProvider.GetIntegrationCoefficients();
+        const auto biot_coefficients        = mInputProvider.GetBiotCoefficients();
+        const auto bishop_coefficients      = mInputProvider.GetDegreesOfSaturation();
+        const auto np_container             = mInputProvider.GetNpContainer();
         for (int i = 0; i < mInputProvider.GetBMatrices().size(); ++i) {
             Matrix coupling_contribution(NumberOfColumns, NumberOfRows);
             GeoTransportEquationUtilities::CalculateCouplingMatrix(
@@ -86,10 +86,7 @@ public:
         return CalculateCouplingMatrix();
     }
 
-    typename BaseType::RHSVectorType RHSContribution() override
-    {
-        return {};
-    }
+    typename BaseType::RHSVectorType RHSContribution() override { return prod(CalculateCouplingMatrix(), mInputProvider.GetNodalVelocities()); }
 
     std::pair<std::optional<typename BaseType::LHSMatrixType>, typename BaseType::RHSVectorType> LocalSystemContribution() override
     {
--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_coupling_calculators.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_coupling_calculators.cpp
@@ -10,13 +10,14 @@
 //  Main authors:    Richard Faasse
 //
 
-#include "custom_elements/contribution_calculators/up_coupling_calculator.hpp"
 #include "custom_elements/contribution_calculators/pu_coupling_calculator.hpp"
+#include "custom_elements/contribution_calculators/up_coupling_calculator.hpp"
 #include "custom_utilities/transport_equation_utilities.hpp"
 #include "custom_utilities/ublas_utilities.h"
 #include "includes/checks.h"
 #include "tests/cpp_tests/geo_mechanics_fast_suite_without_kernel.h"
 #include "tests/cpp_tests/test_utilities.h"
+
 namespace Kratos::Testing
 {
 
@@ -135,10 +136,14 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestUPCouplingVectorContributio
     auto get_integration_coefficients = [integration_coefficient]() {
         return std::vector{integration_coefficient, integration_coefficient};
     };
-    const auto np_container     = UblasUtilities::CreateMatrix({{1.0, 2.0}, {1.0, 2.0}});
-    auto       get_np_container = [&np_container]() -> const Matrix& { return np_container; };
-    auto get_biot_coefficients  = [biot_coefficient]() { return std::vector{biot_coefficient, biot_coefficient}; };
-    auto get_bishop_coefficients = [bishop_coefficient]() { return std::vector{bishop_coefficient, bishop_coefficient}; };
+    const auto np_container          = UblasUtilities::CreateMatrix({{1.0, 2.0}, {1.0, 2.0}});
+    auto       get_np_container      = [&np_container]() -> const Matrix& { return np_container; };
+    auto       get_biot_coefficients = [biot_coefficient]() {
+        return std::vector{biot_coefficient, biot_coefficient};
+    };
+    auto get_bishop_coefficients = [bishop_coefficient]() {
+        return std::vector{bishop_coefficient, bishop_coefficient};
+    };
     auto get_voigt_vector    = [voigt_vector]() { return voigt_vector; };
     auto get_fluid_pressures = []() { return UblasUtilities::CreateVector({1.0, 2.0}); };
 
@@ -165,7 +170,7 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestPUCouplingMatrixContributio
     const auto   voigt_vector            = UblasUtilities::CreateVector({1.0, 1.0, 1.0, 0.0});
     const double integration_coefficient = 0.5;
     const double biot_coefficient        = 2.0;
-    const double degree_of_saturation      = 0.1;
+    const double degree_of_saturation    = 0.1;
 
     // clang-format off
     // Checked by hand
@@ -181,13 +186,15 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestPUCouplingMatrixContributio
     auto get_integration_coefficients = [integration_coefficient]() {
         return std::vector{integration_coefficient};
     };
-    const auto np_container     = UblasUtilities::CreateMatrix({{1.0, 2.0}});
-    auto       get_np_container = [&np_container]() -> const Matrix& { return np_container; };
-    auto get_biot_coefficients  = [biot_coefficient]() { return std::vector{biot_coefficient}; };
-    auto get_degrees_of_saturation = [degree_of_saturation]() { return std::vector{degree_of_saturation}; };
-    auto get_voigt_vector    = [voigt_vector]() { return voigt_vector; };
-    auto get_fluid_pressures = []() { return UblasUtilities::CreateVector({1.0, 2.0}); };
-    auto get_velocity_coefficient = []() {return 2.0;};
+    const auto np_container        = UblasUtilities::CreateMatrix({{1.0, 2.0}});
+    auto       get_np_container    = [&np_container]() -> const Matrix& { return np_container; };
+    auto get_biot_coefficients     = [biot_coefficient]() { return std::vector{biot_coefficient}; };
+    auto get_degrees_of_saturation = [degree_of_saturation]() {
+        return std::vector{degree_of_saturation};
+    };
+    auto get_voigt_vector         = [voigt_vector]() { return voigt_vector; };
+    auto get_fluid_pressures      = []() { return UblasUtilities::CreateVector({1.0, 2.0}); };
+    auto get_velocity_coefficient = []() { return 2.0; };
 
     PUCouplingCalculator<number_of_pw_dof, number_of_u_dof>::InputProvider input_provider(
         get_np_container, get_b_matrices, get_voigt_vector, get_integration_coefficients,
@@ -197,6 +204,49 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestPUCouplingMatrixContributio
 
     const auto calculated_coupling_matrix = coupling_calculator.LHSContribution().value();
     KRATOS_CHECK_MATRIX_NEAR(calculated_coupling_matrix, expected_coupling_matrix, Defaults::absolute_tolerance);
+}
+
+TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestPUCouplingVectorContribution)
+{
+    constexpr auto number_of_u_dof  = 4;
+    constexpr auto number_of_pw_dof = 2;
+
+    // clang-format off
+    const auto b_matrix = UblasUtilities::CreateMatrix(
+                {{1.0,  2.0, 3.0, 4.0},
+                 {5.0,  6.0, 7.0, 8.0},
+                 {9.0, 10.0,11.0,12.0},
+                 {13.0,14.0,15.0,16.0}});
+    // clang-format on
+
+    const auto   voigt_vector            = UblasUtilities::CreateVector({1.0, 1.0, 1.0, 0.0});
+    const double integration_coefficient = 0.5;
+    const double biot_coefficient        = 2.0;
+    const double bishop_coefficient      = 0.1;
+
+    // Checked by hand
+    const auto expected_coupling_vector = UblasUtilities::CreateVector({42.0, 84.0});
+
+    auto get_b_matrices               = [b_matrix]() { return std::vector{b_matrix}; };
+    auto get_integration_coefficients = [integration_coefficient]() {
+        return std::vector{integration_coefficient};
+    };
+    const auto np_container     = UblasUtilities::CreateMatrix({{1.0, 2.0}});
+    auto       get_np_container = [&np_container]() -> const Matrix& { return np_container; };
+    auto get_biot_coefficients  = [biot_coefficient]() { return std::vector{biot_coefficient}; };
+    auto get_bishop_coefficients = [bishop_coefficient]() { return std::vector{bishop_coefficient}; };
+    auto get_voigt_vector    = [voigt_vector]() { return voigt_vector; };
+    auto get_velocities = []() { return UblasUtilities::CreateVector({1.0, 2.0, 3.0, 4.0}); };
+    auto get_velocity_coefficient = []() { return 2.0; };
+
+    PUCouplingCalculator<number_of_pw_dof, number_of_u_dof>::InputProvider input_provider(
+        get_np_container, get_b_matrices, get_voigt_vector, get_integration_coefficients,
+        get_biot_coefficients, get_bishop_coefficients, get_velocities, get_velocity_coefficient);
+
+    PUCouplingCalculator<number_of_pw_dof, number_of_u_dof> coupling_calculator(input_provider);
+
+    const auto calculated_coupling_vector = coupling_calculator.RHSContribution();
+    KRATOS_CHECK_VECTOR_NEAR(calculated_coupling_vector, expected_coupling_vector, Defaults::absolute_tolerance);
 }
 
 } // namespace Kratos::Testing
