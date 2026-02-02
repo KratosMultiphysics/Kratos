a8179e79c61624c5ec4c2b9e05f6f1fea3608046
Richard Faasse <richard.faasse@deltares.nl>
2026-02-02 14:46:19 +0100

    Simplified tests somewhat

--- applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_stiffness_calculator.cpp
+++ applications/GeoMechanicsApplication/tests/cpp_tests/custom_elements/contribution_calculators/test_stiffness_calculator.cpp
@@ -23,7 +23,7 @@ Kratos::ProcessInfo GetProcessInfo() { return Kratos::ProcessInfo(); }
 
 using namespace Kratos;
 
-class MockLaw : public Kratos::ConstitutiveLaw
+class MockLaw : public ConstitutiveLaw
 {
 public:
     KRATOS_CLASS_POINTER_DEFINITION(MockLaw);
@@ -32,9 +32,9 @@ public:
     {
     }
 
-    Kratos::Matrix& CalculateValue(Parameters& rParameterValues,
-                                   const Kratos::Variable<boost::numeric::ublas::matrix<double>>& rThisVariable,
-                                   Kratos::Matrix& rValue) override
+    Matrix& CalculateValue(Parameters& rParameterValues,
+                                   const Variable<Matrix>& rThisVariable,
+                                   Matrix& rValue) override
     {
         rValue = mConstitutiveMatrix;
         return mConstitutiveMatrix;
@@ -59,7 +59,7 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestsStiffnessContribution)
     constexpr std::size_t number_of_u_dof = 4;
 
     Geo::IntegrationCoefficientsGetter integration_coefficients_getter = []() {
-        return std::vector{1.0, 2.0};
+        return std::vector{0.7, 0.3};
     };
     const auto b = UblasUtilities::CreateMatrix({{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}});
     Geo::BMatricesGetter     b_matrices_getter = [b]() { return std::vector{b, b}; };
@@ -88,7 +88,6 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestsStiffnessContribution)
     // integration point and summing the results.
     auto expected_stiffness_matrix = UblasUtilities::CreateMatrix(
         {{1605, 1902, 2199, 2496}, {1854, 2196, 2538, 2880}, {2103, 2490, 2877, 3264}, {2352, 2784, 3216, 3648}});
-    expected_stiffness_matrix*=3.0; // Sum of the integration weights
     KRATOS_CHECK_MATRIX_NEAR(actual_stiffness_matrix, expected_stiffness_matrix, 1e-4)
 }
 
@@ -97,12 +96,12 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestsStiffnessForceContribution
     constexpr std::size_t number_of_u_dof = 4;
 
     Geo::IntegrationCoefficientsGetter integration_coefficients_getter = []() {
-        return std::vector{1.0, 2.0};
+        return std::vector{0.7, 0.3};
     };
     const auto b = UblasUtilities::CreateMatrix({{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}});
     Geo::BMatricesGetter     b_matrices_getter = [b]() { return std::vector{b, b}; };
     Geo::StrainVectorsGetter strains_getter    = []() {
-        return std::vector{Vector{4, 0.5}, Vector{4, 0.6}};
+        return std::vector{Vector{3, 0.5}, Vector{3, 0.6}};
     };
 
     const auto constitutive_matrix = UblasUtilities::CreateMatrix({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
@@ -125,7 +124,6 @@ TEST_F(KratosGeoMechanicsFastSuiteWithoutKernel, TestsStiffnessForceContribution
     // The expected matrix is obtained by calculating - B^T * stress vector * weight for each
     // integration point and summing the results.
     auto expected_stiffness_force = UblasUtilities::CreateVector({-38, -44, -50, -56});
-    expected_stiffness_force*=3.0; // Sum of the integration weights
     KRATOS_CHECK_VECTOR_NEAR(actual_stiffness_force, expected_stiffness_force, 1e-4)
 }
 
