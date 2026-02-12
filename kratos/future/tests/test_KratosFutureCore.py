39bcd4899be097476420b0b577a141e5aa4dd257
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2026-02-12 08:28:39 +0100

    Remove no longer needed EigenvalueSystem

--- kratos/future/tests/test_KratosFutureCore.py
+++ kratos/future/tests/test_KratosFutureCore.py
@@ -6,7 +6,6 @@ import KratosMultiphysics.KratosUnittest as KratosUnittest
 
 # Import the tests or test_classes to create the suites
 import test_vtu_output
-import test_eigenvalue_system
 import test_linear_system
 import test_sparse_matrix_linear_operator
 
@@ -27,7 +26,6 @@ def AssembleTestSuites():
 
     # Create a test suite with the selected tests (Small tests):
     smallSuite = suites['small']
-    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_eigenvalue_system.TestEigenvalueSystem]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_linear_system.TestLinearSystem]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_sparse_matrix_linear_operator.TestSparseMatrixLinearOperator]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_vtu_output.TestVtuOutput2D]))
