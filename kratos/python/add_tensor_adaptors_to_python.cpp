b4e34d6ee277893ed4c4334530689b4c02fbbda7
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2026-02-13 15:32:08 +0100

    expose to python

--- kratos/python/add_tensor_adaptors_to_python.cpp
+++ kratos/python/add_tensor_adaptors_to_python.cpp
@@ -36,6 +36,7 @@
 #include "tensor_adaptors/geometries_tensor_adaptor.h"
 #include "tensor_adaptors/tensor_adaptor.h"
 #include "tensor_adaptors/variable_tensor_adaptor.h"
+#include "tensor_adaptors/geometry_metrics_tensor_adaptor.h"
 
 // Include base h
 #include "add_tensor_adaptors_to_python.h"
@@ -298,6 +299,27 @@ void AddTensorAdaptorsToPython(pybind11::module& m)
           .def(py::init<const ConnectivityIdsTensorAdaptor::BaseType&, const bool>(),
                py::arg("tensor_adaptor"),
                py::arg("copy") = true);
+
+     py::class_<GeometryMetricsTensorAdaptor, GeometryMetricsTensorAdaptor::Pointer, GeometryMetricsTensorAdaptor::BaseType> geometric_tensor_adaptor(tensor_adaptor_sub_module, "GeometryMetricsTensorAdaptor");
+
+     py::enum_<GeometryMetricsTensorAdaptor::DatumType>(geometric_tensor_adaptor,"DatumType")
+          .value("DomainSize", GeometryMetricsTensorAdaptor::DatumType::DomainSize)
+          .export_values();
+
+     geometric_tensor_adaptor
+          .def(py::init<const GeometryMetricsTensorAdaptor::BaseType&, GeometryMetricsTensorAdaptor::DatumType, const bool>(),
+               py::arg("tensor_adaptor"),
+               py::arg("datum"),
+               py::arg("copy") = true)
+          .def(py::init<ModelPart::GeometryContainerType::Pointer, GeometryMetricsTensorAdaptor::DatumType>(),
+               py::arg("container"),
+               py::arg("datum"))
+          .def(py::init<ModelPart::ConditionsContainerType::Pointer, GeometryMetricsTensorAdaptor::DatumType>(),
+               py::arg("container"),
+               py::arg("datum"))
+          .def(py::init<ModelPart::ElementsContainerType::Pointer, GeometryMetricsTensorAdaptor::DatumType>(),
+               py::arg("container"),
+               py::arg("datum"));
 }
 
 } // namespace Kratos::Python.
