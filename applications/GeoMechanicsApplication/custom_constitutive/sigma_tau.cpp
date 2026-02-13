b992b2235fbe8df99adbf642022cef440638ceb8
Anne van de Graaf <avdg81@users.noreply.github.com>
2026-02-13 14:42:05 +0100

    [GeoMechanicsApplication] Defined addition operators for class `SigmaTau` (#14199)
    - Defined and unit-tested `operator+=` as well as `operator+` for objects of type `SigmaTau`. Code that adds two traction vectors has been updated accordingly.
    - Renamed several local variables in the unit tests of class `SigmaTau`.

--- applications/GeoMechanicsApplication/custom_constitutive/sigma_tau.cpp
+++ applications/GeoMechanicsApplication/custom_constitutive/sigma_tau.cpp
@@ -31,4 +31,16 @@ double SigmaTau::Tau() const { return mValues[1]; }
 
 double& SigmaTau::Tau() { return mValues[1]; }
 
+SigmaTau& SigmaTau::operator+=(const SigmaTau& rRhsTraction)
+{
+    std::ranges::transform(mValues, rRhsTraction.mValues, mValues.begin(), std::plus{});
+    return *this;
+}
+
+SigmaTau operator+(SigmaTau LhsTraction, const SigmaTau& rRhsTraction)
+{
+    LhsTraction += rRhsTraction;
+    return LhsTraction;
+}
+
 } // namespace Kratos::Geo
