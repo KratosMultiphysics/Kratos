4ab56264101057581976acbaaf4fedd3b25d662a
Richard Faasse <richard.faasse@deltares.nl>
2026-02-16 14:21:18 +0100

    Added some details to the benchmark

--- applications/GeoMechanicsApplication/benchmarks/builder_and_solver_benchmark.cpp
+++ applications/GeoMechanicsApplication/benchmarks/builder_and_solver_benchmark.cpp
@@ -10,15 +10,23 @@
 //  Main authors:    Richard Faasse
 //
 
+#include "linear_solvers/linear_solver.h"
+#include "solving_strategies/builder_and_solvers/residualbased_block_builder_and_solver.h"
+
 #include <benchmark/benchmark.h>
 
 
 namespace Kratos
 {
-
+using SparseSpaceType   = UblasSpace<double, CompressedMatrix, Vector>;
+using DenseSpaceType    = UblasSpace<double, Matrix, Vector>;
+using LinearSolverType = LinearSolver<SparseSpaceType, DenseSpaceType>;
 
 void BenchmarkBuildRHS(benchmark::State& rState)
 {
+    ResidualBasedBlockBuilderAndSolver<SparseSpaceType, DenseSpaceType,LinearSolverType> builder_and_solver();
+
+
     for (auto _ : rState) {
         double test = 0.0;
         test *= 2;
