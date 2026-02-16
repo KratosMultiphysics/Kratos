a4ebbeb45d42aa2fa090c4c6f5d24fb08d563089
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2026-02-16 09:37:50 +0100

    adding code

--- applications/StructuralMechanicsApplication/custom_elements/beam_elements/non_linear_timoshenko_beam_element_2D2N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/beam_elements/non_linear_timoshenko_beam_element_2D2N.cpp
@@ -200,13 +200,91 @@ void NonLinearTimoshenkoBeamElement2D2N::CalculateRightHandSide(
 /***********************************************************************************/
 
 void NonLinearTimoshenkoBeamElement2D2N::CalculateAll(
-    MatrixType& rLeftHandSideMatrix,
-    VectorType& rRightHandSideVector,
-    const ProcessInfo& rCurrentProcessInfo,
+    MatrixType& rLHS,
+    VectorType& rRHS,
+    const ProcessInfo& rProcessInfo,
     const bool ComputeLHS,
     const bool ComputeRHS
     )
 {
+    KRATOS_TRY
+
+    const auto &r_props = GetProperties();
+    const auto &r_geometry = GetGeometry();
+    const SizeType number_of_nodes = r_geometry.size();
+    const SizeType mat_size = GetDoFsPerNode() * number_of_nodes;
+    const SizeType strain_size = mConstitutiveLawVector[0]->GetStrainSize();
+
+
+    if (ComputeLHS) {
+        if (rLHS.size1() != mat_size || rLHS.size2() != mat_size) {
+            rLHS.resize(mat_size, mat_size, false);
+        }
+        noalias(rLHS) = ZeroMatrix(mat_size, mat_size);
+    }
+
+    if (ComputeRHS) {
+        if (rRHS.size() != mat_size) {
+            rRHS.resize(mat_size, false);
+        }
+        noalias(rRHS) = ZeroVector(mat_size);
+    }
+
+    const auto& r_integration_points = IntegrationPoints(GetIntegrationMethod());
+
+    ConstitutiveLaw::Parameters cl_values(r_geometry, r_props, rProcessInfo);
+    auto &r_cl_options = cl_values.GetOptions();
+    r_cl_options.Set(ConstitutiveLaw::COMPUTE_STRESS, true);
+    r_cl_options.Set(ConstitutiveLaw::COMPUTE_CONSTITUTIVE_TENSOR, true);
+
+    const double length = CalculateLength(); // Reference length
+    const double Phi = StructuralMechanicsElementUtilities::CalculatePhi(r_props, length);
+    const double J = 0.5 * length;
+    const double area = GetCrossArea();
+
+    // Let's initialize the cl values
+    VectorType strain_vector(strain_size), stress_vector(strain_size);
+    MatrixType constitutive_matrix(strain_size, strain_size);
+    strain_vector.clear();
+    cl_values.SetStrainVector(strain_vector);
+    cl_values.SetStressVector(stress_vector);
+    cl_values.SetConstitutiveMatrix(constitutive_matrix);
+
+    VectorType nodal_values(mat_size);
+    GetNodalValuesVector(nodal_values);
+
+    VectorType Ntheta(mat_size), dNtheta(mat_size);
+    VectorType Nu(mat_size), dNu(mat_size);
+    VectorType Nv(mat_size), dNv(mat_size);
+
+    MatrixType B(strain_size, mat_size);
+    double theta, du, dv;
+
+    // Loop over the integration points
+    for (SizeType IP = 0; IP < r_integration_points.size(); ++IP) {
+
+        const auto local_body_forces = GetLocalAxesBodyForce(*this, r_integration_points, IP);
+
+        const double xi     = r_integration_points[IP].X();
+        const double weight = r_integration_points[IP].Weight();
+        const double jacobian_weight = weight * J;
+
+        // Let's fill the shape functions and their derivatives
+        GetNu0ShapeFunctionsValues(Nu, length, Phi, xi); // u
+        GetFirstDerivativesNu0ShapeFunctionsValues(dNu, length, Phi, xi);
+
+        GetNThetaShapeFunctionsValues(Ntheta, length, Phi, xi); // rotation
+        GetFirstDerivativesNThetaShapeFunctionsValues(dNtheta, length, Phi, xi);
+
+        GetShapeFunctionsValues(Nv, length, Phi, xi); // v
+        GetFirstDerivativesShapeFunctionsValues(dNv, length, Phi, xi);
+
+
+
+
+    }
+
+    KRATOS_CATCH("");
 
 }
 
