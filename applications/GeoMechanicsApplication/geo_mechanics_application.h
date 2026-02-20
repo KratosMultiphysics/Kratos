717c04505e759de6a037a34ed08082898fc04724
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2026-02-20 13:29:12 +0100

    Use a nonmember `static` variable for the calculation contributions of the U-Pw interface element

--- applications/GeoMechanicsApplication/geo_mechanics_application.h
+++ applications/GeoMechanicsApplication/geo_mechanics_application.h
@@ -133,6 +133,9 @@
 namespace Kratos
 {
 
+static const auto UPwInterfaceElementContributions =
+    std::vector{CalculationContribution::Stiffness, CalculationContribution::UPCoupling};
+
 ///@name Kratos Globals
 ///@{
 
@@ -516,30 +519,32 @@ private:
         0, Kratos::make_shared<HexahedraInterface3D8<NodeType>>(Element::GeometryType::PointsArrayType(8)),
         std::make_unique<ThreeDimensionalStressState>()};
 
-    const std::vector<CalculationContribution> interface_contributions =
-        std::vector{CalculationContribution::Stiffness, CalculationContribution::UPCoupling};
-
     const UPwInterfaceElement mUPwLineInterfacePlaneStrainElement2Plus2N{
         0, Kratos::make_shared<InterfaceGeometry<Line2D2<NodeType>>>(Element::GeometryType::PointsArrayType(4)),
-        std::make_unique<Line2DInterfaceStressState>(), IsDiffOrderElement::No, interface_contributions};
+        std::make_unique<Line2DInterfaceStressState>(), IsDiffOrderElement::No, UPwInterfaceElementContributions};
     const UPwInterfaceElement mUPwLineInterfacePlaneStrainElement3Plus3N{
         0, Kratos::make_shared<InterfaceGeometry<Line2D3<NodeType>>>(Element::GeometryType::PointsArrayType(6)),
-        std::make_unique<Line2DInterfaceStressState>(), IsDiffOrderElement::No, interface_contributions};
+        std::make_unique<Line2DInterfaceStressState>(), IsDiffOrderElement::No, UPwInterfaceElementContributions};
     const UPwInterfaceElement mUPwSurfaceInterfaceElement3Plus3N{
         0, Kratos::make_shared<InterfaceGeometry<Triangle3D3<NodeType>>>(Element::GeometryType::PointsArrayType(6)),
-        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No, interface_contributions};
+        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No,
+        UPwInterfaceElementContributions};
     const UPwInterfaceElement mUPwSurfaceInterfaceElement4Plus4N{
         0, Kratos::make_shared<InterfaceGeometry<Quadrilateral3D4<NodeType>>>(Element::GeometryType::PointsArrayType(8)),
-        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No, interface_contributions};
+        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No,
+        UPwInterfaceElementContributions};
     const UPwInterfaceElement mUPwSurfaceInterfaceElement6Plus6N{
         0, Kratos::make_shared<InterfaceGeometry<Triangle3D6<NodeType>>>(Element::GeometryType::PointsArrayType(12)),
-        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No, interface_contributions};
+        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No,
+        UPwInterfaceElementContributions};
     const UPwInterfaceElement mUPwSurfaceInterfaceElement8Plus8N{
         0, Kratos::make_shared<InterfaceGeometry<Quadrilateral3D8<NodeType>>>(Element::GeometryType::PointsArrayType(16)),
-        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No, interface_contributions};
+        std::make_unique<SurfaceInterfaceStressState>(), IsDiffOrderElement::No,
+        UPwInterfaceElementContributions};
     const UPwInterfaceElement mUPwLineInterfacePlaneStrainDiffOrderElement3Plus3N{
         0, Kratos::make_shared<InterfaceGeometry<Line2D3<NodeType>>>(Element::GeometryType::PointsArrayType(6)),
-        std::make_unique<Line2DInterfaceStressState>(), IsDiffOrderElement::Yes, interface_contributions};
+        std::make_unique<Line2DInterfaceStressState>(), IsDiffOrderElement::Yes,
+        UPwInterfaceElementContributions};
 
     // Updated-Lagrangian elements:
     const UPwUpdatedLagrangianElement<2, 3> mUPwUpdatedLagrangianElement2D3N{
