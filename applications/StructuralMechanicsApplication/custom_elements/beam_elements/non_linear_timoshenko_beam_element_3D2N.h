774285d9e4a9af1516debad05641f8b56acac965
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2026-02-23 15:32:42 +0100

    add quaternion

--- applications/StructuralMechanicsApplication/custom_elements/beam_elements/non_linear_timoshenko_beam_element_3D2N.h
+++ applications/StructuralMechanicsApplication/custom_elements/beam_elements/non_linear_timoshenko_beam_element_3D2N.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "timoshenko_beam_element_3D2N.h"
+#include "utilities/quaternion.h"
 
 namespace Kratos
 {
@@ -114,8 +115,20 @@ public:
         const bool ComputeRHS
         );
 
+        // void CalculateOnIntegrationPoints(const Variable<double>& rVariable, std::vector<double>& rOutput, const ProcessInfo& rProcessInfo) override;
 
-    // void CalculateOnIntegrationPoints(const Variable<double>& rVariable, std::vector<double>& rOutput, const ProcessInfo& rProcessInfo) override;
+private:
+    /**
+     * @brief This quaternion is located at the centre of the beam and
+     * it will be updated at any time that the rotations are updated.
+     * It is used to compute the rotation matrix that transforms the local internal forces to the global frame, and also to compute the local deformation of the beam.
+     * It is initialized as the identity quaternion, and then it is updated at each iteration using the incremental rotations of the nodes.
+     * The use of a quaternion allows to avoid singularities and to have a more robust update of the rotation matrix, especially for large rotations.
+     * The use of a quaternion also allows to have a more efficient update of the rotation matrix.
+     */
+    Quaternion<double> mQuaternion = Quaternion<double>::Identity();
+
+
 };
 
 } // namespace Kratos
