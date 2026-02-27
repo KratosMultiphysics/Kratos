c30feb33c19bc56dbea3542bb529fa40d8894d8c
Richard Faasse <56549273+rfaasse@users.noreply.github.com>
2026-02-27 08:44:35 +0100

    Update configure.cmd

--- .github/workflows/configure.cmd
+++ .github/workflows/configure.cmd
@@ -53,7 +53,7 @@ cmake                                                 ^
   -DFORCE_LOCAL_ZLIB_COMPILATION=ON                   ^
   -DCMAKE_UNITY_BUILD=ON                                    || goto :error
 
-cmake --build "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%" --target all install || goto :error
+cmake --build "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%" && cmake --install "%KRATOS_BUILD%\%KRATOS_BUILD_TYPE%" --config %KRATOS_BUILD_TYPE% || goto :error
 
 goto :EOF
 
