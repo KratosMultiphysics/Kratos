e51d7ec07d208bcc9cae970373c1141f1ecc7480
markelov <gennady.markelov@deltares.nl>
2026-03-10 15:17:18 +0100

    corrected material/project parameters and added common folder

--- applications/GeoMechanicsApplication/tests/c_phi_reduction_process.py
+++ applications/GeoMechanicsApplication/tests/c_phi_reduction_process.py
@@ -1,7 +1,9 @@
 import os
+import KratosMultiphysics as Kratos
 import KratosMultiphysics.KratosUnittest as KratosUnittest
+import KratosMultiphysics.GeoMechanicsApplication.context_managers as context_managers
+import KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis as analysis
 from KratosMultiphysics.GeoMechanicsApplication.gid_output_file_reader import GiDOutputFileReader
-import KratosMultiphysics.GeoMechanicsApplication.run_multiple_stages as run_multiple_stages
 
 import test_helper
 
@@ -9,11 +11,27 @@ class KratosGeoMechanicsCPhiReductionProcess(KratosUnittest.TestCase):
     """
     This class contains benchmark tests which are checked with the original solution
     """
+
     @staticmethod
-    def _get_displacement_vectors(file_path):
-        run_multiple_stages.run_stages(file_path, 2)
+    def _run_stages_with_common_parameters(case_path, common_path, n_stages):
+        model = Kratos.Model()
+        with context_managers.set_cwd_to(case_path):
+            for stage_index in range(1, n_stages + 1):
+                project_parameters_file = os.path.join(
+                    common_path, f"ProjectParameters_stage{stage_index}.json"
+                )
+                with open(project_parameters_file, "r") as parameter_file:
+                    stage_parameters = Kratos.Parameters(parameter_file.read())
+                analysis.GeoMechanicsAnalysis(model, stage_parameters).Run()
+
+    @staticmethod
+    def _get_displacement_vectors(case_path):
+        common_path = os.path.join(os.path.dirname(case_path), "common")
+        KratosGeoMechanicsCPhiReductionProcess._run_stages_with_common_parameters(
+            case_path, common_path, 2
+        )
         reader = GiDOutputFileReader()
-        actual_data = reader.read_output_from(os.path.join(file_path, "stage2.post.res"))
+        actual_data = reader.read_output_from(os.path.join(case_path, "stage2.post.res"))
         node_ids = [57, 151, 235]
         return reader.nodal_values_at_time("DISPLACEMENT", 0.2, actual_data, node_ids)
 
