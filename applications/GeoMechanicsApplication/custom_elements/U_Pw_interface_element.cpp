c9d9ef66a2c4e9c31fef05c2804f9688d9287381
markelov <gennady.markelov@deltares.nl>
2026-03-13 22:33:15 +0100

    added checks

--- applications/GeoMechanicsApplication/custom_elements/U_Pw_interface_element.cpp
+++ applications/GeoMechanicsApplication/custom_elements/U_Pw_interface_element.cpp
@@ -14,6 +14,7 @@
 
 #include "custom_geometries/interface_geometry.hpp"
 #include "custom_retention/retention_law_factory.h"
+#include "custom_utilities/check_utilities.hpp"
 #include "custom_utilities/constitutive_law_utilities.h"
 #include "custom_utilities/dof_utilities.hpp"
 #include "custom_utilities/element_utilities.hpp"
@@ -506,7 +507,27 @@ int UPwInterfaceElement::Check(const ProcessInfo& rCurrentProcessInfo) const
             << "Number of integration points (" << mpIntegrationScheme->GetNumberOfIntegrationPoints()
             << ") and constitutive laws (" << mConstitutiveLaws.size() << ") do not match.\n";
 
-        const auto r_properties  = GetProperties();
+        const auto r_properties = GetProperties();
+
+        if (!GetIgnoreUndrained(r_properties)) {
+            const auto check_properties =
+                CheckProperties(r_properties, "property", Id(), CheckProperties::Bounds::AllInclusive);
+
+            const auto has_permeability_contribution =
+                std::ranges::find(mContributions, CalculationContribution::Permeability) !=
+                mContributions.end();
+            const auto has_fluid_body_flow_contribution =
+                std::ranges::find(mContributions, CalculationContribution::FluidBodyFlow) !=
+                mContributions.end();
+            if (has_permeability_contribution || has_fluid_body_flow_contribution) {
+                check_properties.Check(TRANSVERSAL_PERMEABILITY);
+                check_properties.Check(DYNAMIC_VISCOSITY);
+
+                if (has_fluid_body_flow_contribution) {
+                    check_properties.Check(DENSITY_WATER);
+                }
+            }
+        }
         const auto expected_size = mpStressStatePolicy->GetVoigtSize();
         ConstitutiveLawUtilities::CheckStrainSize(r_properties, expected_size, Id());
 
