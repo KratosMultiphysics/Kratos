77a0cc63137dec99bf64b53dcbab7736fa09ecb2
qn-tang <qining.tang@tum.de>
2026-03-13 16:36:24 +0100

    preliminary output

--- applications/IgaApplication/python_scripts/iga_vtk_output_process.py
+++ applications/IgaApplication/python_scripts/iga_vtk_output_process.py
@@ -4,6 +4,9 @@ import KratosMultiphysics.IgaApplication
 import KratosMultiphysics.kratos_utilities
 
 VTK_QUAD = 9
+VERSION = (2, 4)
+GLOBAL_TYPE = 'PartitionedDataSetCollection'
+SUB_TYPE = "UnstructuredGrid"
 
 try:
     import h5py as h5
@@ -39,9 +42,6 @@ class IgaVTKOutputProcess(KratosMultiphysics.Process):
 
         ## Get the model part
         self.model_part = model[self.params["model_part_name"].GetString()]
-        print('---------------MODEL PART------------------')
-        print(self.model_part)
-
 
         ## Get the geometry
         self.brep_surface_ids = [
@@ -53,10 +53,6 @@ class IgaVTKOutputProcess(KratosMultiphysics.Process):
         if not self.output_file_name.endswith(".vtkhdf"):
             self.output_file_name += ".vtkhdf"
         
-        # The geometries are not stored in the model part, but they are in the model
-        self.brep_surface = self.model_part.GetGeometry(self.brep_surface_ids[0])
-        print(self.brep_surface)
-
         # self.nodal_results_scalar, self.nodal_results_vector = \
         #     CreateVariablesListFromInput(self.params["nodal_results"])
 
@@ -102,19 +98,31 @@ class IgaVTKOutputProcess(KratosMultiphysics.Process):
         else:
             label = self.printed_step_count
 
-        #TODO:
+        #TODO: write vtkhdf output
         file = h5.File(self.output_file_name, "w")
-        root = file.create_group("VTKHDF")
-        root.attrs["Version"] = (2, 4)
-        root.attrs["Type"] = "PartotopmedDataSetCollection"
+        root = file.create_group("VTKHDF", track_order=True)
+        root.attrs["Version"] = VERSION
+        root.attrs["Type"] = GLOBAL_TYPE
 
-        # for brep_id in self.brep_surface_ids:
-        #     geometry = self.model_part.GetGeometry(brep_id)
-        #     knots_u = geometry.KnotsU()
-        #     knots_v = geometry.KnotsV()
+        assembly = root.create_group("Assembly", track_order=True)
 
+        # Output subgroup for each brep
+        index = 0
+        for brep_id in self.brep_surface_ids:
+            brep_surface = self.model_part.GetGeometry(brep_id)
+            group_name = f"brep_surface_{brep_id}"
+            surface_group = root.create_group(group_name)
+            surface_group.attrs["Version"] = VERSION
+            surface_group.attrs["Type"] = SUB_TYPE
+            self.__write_out_surface(surface_group, brep_surface)
 
+            # ---- Assembly ----
+            assem_group = assembly.create_group(f"brep_{brep_id}")
+            #assem_group.attrs['Index'] = index
+            assem_group[group_name] = h5.SoftLink(f"/VTKHDF/{group_name}")
+            assem_group[group_name].attrs['Index'] = index
 
+            index += 1
 
         # Schedule next output
         if self.output_frequency > 0.0: # Note: if == 0, we'll just always print
@@ -132,10 +140,68 @@ class IgaVTKOutputProcess(KratosMultiphysics.Process):
         else:
             return ( self.step_count >= self.next_output )
 
-    def __write_out_surface(self, grid_points) -> h5.Group:
-        None
+    def __write_out_surface(self, surface_group:h5.Group, brep_surface:KM.BrepSurface):
+        knots_u = brep_surface.KnotsU()
+        knots_v = brep_surface.KnotsV()
 
+        num_u = len(knots_u)
+        num_v = len(knots_v)
+        num_cells = (num_u-1) * (num_v-1)
+        num_points = num_u * num_v
 
+        # get the Points
+        grid_points = KM.Matrix(num_points, 3)
+        for j, v in enumerate(knots_v):
+            for i, u in enumerate(knots_u):
+                X = KM.Array3()
+                X[0] = 0; X[1] = 0; X[2] = 0
+                local_coord = KM.Array3()
+                local_coord[0] = u; local_coord[1] = v; local_coord[2] = 0.0
+                X = brep_surface.GlobalCoordinates(local_coord)
+                print(X)
+
+                # Column-major indexing
+                idx = i + j * len(knots_u)
+                grid_points[idx, 0] = local_coord[0]
+                grid_points[idx, 1] = local_coord[1]
+                grid_points[idx, 2] = local_coord[2]
+        
+        # get the Types
+        cell_types = [VTK_QUAD] * num_cells
+
+        # get the Connectivity & Offsets
+        connectivity = KM.Vector(num_cells * 4)  # 4 nodes per quad
+        offsets = KM.Vector(num_cells+1)
+        cell_idx = 0
+        conn_idx = 0
+        for j in range(num_v - 1):
+            for i in range(num_u - 1):
+                # Column-major indices
+                n0 = i     + j * num_u
+                n1 = (i+1) + j * num_u
+                n2 = (i+1) + (j+1) * num_u
+                n3 = i     + (j+1) * num_u
+
+                # Fill connectivity
+                connectivity[conn_idx    ] = n0
+                connectivity[conn_idx + 1] = n1
+                connectivity[conn_idx + 2] = n2
+                connectivity[conn_idx + 3] = n3
+
+                offsets[cell_idx] = conn_idx  
+                conn_idx += 4
+                cell_idx += 1
+
+        offsets[cell_idx] = conn_idx
+
+        # write to hdf5 group
+        surface_group.create_dataset("Points", data=grid_points, dtype="f")
+        surface_group.create_dataset("Connectivity", data=connectivity, dtype="i8")
+        surface_group.create_dataset("Offsets", data=offsets, dtype="i8")
+        surface_group.create_dataset("Types", data=cell_types, dtype="uint8")
+        surface_group.create_dataset("NumberOfPoints", data=(num_points,), dtype="i8")
+        surface_group.create_dataset("NumberOfCells", data=(num_cells,), dtype="i8")
+        surface_group.create_dataset("NumberOfConnectivityIds", data=(len(connectivity),), dtype="i8")
 
 def GetPrettyTime(time):
     pretty_time = "{0:.12g}".format(time)
