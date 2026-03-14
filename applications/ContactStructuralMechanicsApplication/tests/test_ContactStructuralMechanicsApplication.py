98ee14f06f29fadf05d4db0d8ff17b5c4605df9c
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@siemens.com>
2026-03-14 18:39:38 +0100

    Reorganize test cases: move MPC contact tests from smallSuite to nightlySuite

--- applications/ContactStructuralMechanicsApplication/tests/test_ContactStructuralMechanicsApplication.py
+++ applications/ContactStructuralMechanicsApplication/tests/test_ContactStructuralMechanicsApplication.py
@@ -250,14 +250,6 @@ def AssembleTestSuites():
         smallSuite.addTest(TPenaltyThresholdSlipHyperSimplePatchFrictionalTestContact('test_execution'))
 
         # MPC contact test
-        smallSuite.addTest(TTwoDSimplestPatchMatchingTestContact('test_execution'))
-        smallSuite.addTest(TTwoDSimplestWithFrictionPatchMatchingTestContact('test_execution'))
-        if has_CL_application:
-            smallSuite.addTest(TThreeDSimplestPatchMatchingTestContact('test_execution'))
-            smallSuite.addTest(TThreeDSimplestWithFrictionPatchMatchingTestContact('test_execution'))
-        smallSuite.addTest(TThreeDSimplestPatchMatchingSlopeTestContact('test_execution'))
-        smallSuite.addTest(TThreeDPatchMatchingTestContact('test_execution'))
-        smallSuite.addTest(TThreeDPatchNotMatchingTestContact('test_execution'))
 
         ### END SMALL SUITE ###
 
@@ -329,6 +321,14 @@ def AssembleTestSuites():
         nightlySuite.addTest(TPenaltyHyperSimplePatchFrictionalStickTestContact('test_execution'))
 
         # MPC contact test
+        nightlySuite.addTest(TTwoDSimplestPatchMatchingTestContact('test_execution'))
+        nightlySuite.addTest(TTwoDSimplestWithFrictionPatchMatchingTestContact('test_execution'))
+        if has_CL_application:
+            nightlySuite.addTest(TThreeDSimplestPatchMatchingTestContact('test_execution'))
+            nightlySuite.addTest(TThreeDSimplestWithFrictionPatchMatchingTestContact('test_execution'))
+        nightlySuite.addTest(TThreeDSimplestPatchMatchingSlopeTestContact('test_execution'))
+        nightlySuite.addTest(TThreeDPatchMatchingTestContact('test_execution'))
+        nightlySuite.addTest(TThreeDPatchNotMatchingTestContact('test_execution'))
         nightlySuite.addTest(TBeamAxilSimpleContactTest('test_execution'))
         nightlySuite.addTest(TBeamContactTest('test_execution'))
         nightlySuite.addTest(TBeamContactWithTyingTest('test_execution'))
