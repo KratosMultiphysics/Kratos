449339456ac2e5fada57e013a13d0fb9479d5cfd
NickNick9 <nicolo.antonelli@upc.edu>
2026-03-17 21:25:50 +0100

    Splitting a 3D condition [delete printing]

--- applications/IgaApplication/custom_processes/snake_sbm_process.cpp
+++ applications/IgaApplication/custom_processes/snake_sbm_process.cpp
@@ -2115,7 +2115,6 @@ void SnakeSbmProcess::SnakeStep3D(
         )
         {
             isSplitted = true;
-            KRATOS_INFO("::[SnakeSBMUtilities]::") << "SnakeStep :: Splitting a 3D condition" << std::endl;
 
             // Midpoints
             double x12 = (rConditionCoord[0][0] + rConditionCoord[0][1]) / 2.0;
