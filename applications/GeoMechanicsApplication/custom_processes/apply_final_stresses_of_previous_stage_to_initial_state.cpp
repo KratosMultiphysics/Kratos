3d7f121286c11fdbd6c33be1c4e7093c932ddaec
Richard Faasse <richard.faasse@deltares.nl>
2026-03-23 15:06:53 +0100

    Fix thread safety issue in geo process

--- applications/GeoMechanicsApplication/custom_processes/apply_final_stresses_of_previous_stage_to_initial_state.cpp
+++ applications/GeoMechanicsApplication/custom_processes/apply_final_stresses_of_previous_stage_to_initial_state.cpp
@@ -36,7 +36,7 @@ ApplyFinalStressesOfPreviousStageToInitialState::ApplyFinalStressesOfPreviousSta
 void ApplyFinalStressesOfPreviousStageToInitialState::ExecuteInitialize()
 {
     for (const auto& r_model_part : mrModelParts) {
-        block_for_each(r_model_part.get().Elements(), [&r_model_part, this](Element& rElement) {
+        for (auto& rElement : r_model_part.get().Elements()) {
             std::vector<Vector> stresses_on_integration_points;
             rElement.CalculateOnIntegrationPoints(PK2_STRESS_VECTOR, stresses_on_integration_points,
                                                   r_model_part.get().GetProcessInfo());
@@ -50,7 +50,7 @@ void ApplyFinalStressesOfPreviousStageToInitialState::ExecuteInitialize()
 
             CheckRetrievedElementData(constitutive_laws, stresses_on_integration_points, rElement.GetId());
             mStressesByElementId[rElement.GetId()] = stresses_on_integration_points;
-        });
+        }
     }
 }
 
