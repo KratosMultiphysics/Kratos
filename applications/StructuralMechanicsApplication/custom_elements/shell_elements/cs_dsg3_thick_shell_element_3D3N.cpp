46971d7561e2978462df780edf02fbf6ee965859
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2026-04-01 15:31:33 +0200

    update local axes in dsg shell

--- applications/StructuralMechanicsApplication/custom_elements/shell_elements/cs_dsg3_thick_shell_element_3D3N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/shell_elements/cs_dsg3_thick_shell_element_3D3N.cpp
@@ -299,15 +299,18 @@ void CSDSG3ThickShellElement3D3N<IS_COROTATIONAL>::CalculateRotationMatrixGlobal
     }
 
     if (this->Has(LOCAL_AXIS_1)) {
+        array_3 edge_12 = aux_1 - aux_0;
+        array_3 edge_13 = aux_2 - aux_0;
+        array_3 normal = MathUtils<double>::CrossProduct(edge_12, edge_13);
+        const double norm_normal = norm_2(normal);
+
+        KRATOS_DEBUG_ERROR_IF_NOT(norm_normal > 0.0) << "Zero area for CSDSG3ThickShellElement3D3N " << this->Id() << std::endl;
+        normal /= norm_normal;
         noalias(v1) = this->GetValue(LOCAL_AXIS_1); // We assume that the user has set a unit vector
-        noalias(v2) = aux_2 - aux_0;
-        v2 -= inner_prod(v1, v2) * v1; // v2 orthogonal to v1
+        noalias(v2) = MathUtils<double>::CrossProduct(normal, v1);
         const double norm_v2 = norm_2(v2);
-        if (norm_v2 <= 1.0e-8) { // colineal
-            noalias(v2) = aux_1 - aux_0;
-            v2 -= inner_prod(v1, v2) * v1; // v2 orthogonal to v1
-        }
-        v2 /= norm_2(v2);
+        KRATOS_DEBUG_ERROR_IF_NOT(norm_v2 > 0.0) << "Zero length local axis 2 for CSDSG3ThickShellElement3D3N " << this->Id() << std::endl;
+        v2 /= norm_v2;
     } else {
         noalias(v1) = aux_1 - aux_0;
         const double norm_v1 = norm_2(v1);
