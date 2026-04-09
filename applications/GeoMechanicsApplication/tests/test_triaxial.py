adc5bc2e0314a82bbea8fa05dbd9d8d54876aeb9
Sabine-van-Dijk <sabine.vandijk@deltares.nl>
2026-04-09 11:26:40 +0200

    Add test triaxial

---
+++ applications/GeoMechanicsApplication/tests/test_triaxial.py
@@ -0,0 +1,8 @@
+from kratos.python_scripts import KratosUnittest
+
+class KratosGeoMechanicsTriaxialTests(KratosUnittest.TestCase):
+    def test_triaxial(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    KratosUnittest.main()
