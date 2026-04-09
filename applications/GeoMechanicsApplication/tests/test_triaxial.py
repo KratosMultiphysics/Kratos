816d73a180b483fed7d6133e96ad28aa6941f54c
Sabine-van-Dijk <sabine.vandijk@deltares.nl>
2026-04-09 11:35:56 +0200

    Added running of the test triaxial in the test script

--- applications/GeoMechanicsApplication/tests/test_triaxial.py
+++ applications/GeoMechanicsApplication/tests/test_triaxial.py
@@ -1,8 +1,11 @@
+from applications.GeoMechanicsApplication.tests import test_helper
 from kratos.python_scripts import KratosUnittest
 
 class KratosGeoMechanicsTriaxialTests(KratosUnittest.TestCase):
     def test_triaxial(self):
-        self.assertTrue(True)
+        test_name = 'test_triaxial'
+        file_path = test_helper.get_file_path(test_name)
+        simulation = test_helper.run_kratos(file_path)
 
 if __name__ == '__main__':
     KratosUnittest.main()
