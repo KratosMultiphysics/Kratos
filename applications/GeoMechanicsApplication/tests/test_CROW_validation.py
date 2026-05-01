7c1ab9afecf90f9a41122df2ff0e05b50843b103
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2026-05-01 17:44:17 +0200

    Made Python test scripts more similar
    The name of the simulation file now also needs to be passed.

--- applications/GeoMechanicsApplication/tests/test_CROW_validation.py
+++ applications/GeoMechanicsApplication/tests/test_CROW_validation.py
@@ -163,13 +163,11 @@ class KratosGeoMechanicsCrowValidation(KratosUnittest.TestCase):
             "final_equilibrium": {"end_time": 1.0, "base_name": "3_Final_equilibrium"},
         }
 
-    def run_simulation_and_checks(self, relative_test_path):
-        project_path = test_helper.get_file_path(
-            Path("crow_validation") / relative_test_path
-        )
+    def run_simulation_and_checks(self, relative_test_path, analysis_filename):
+        project_path = test_helper.get_file_path(Path("crow_validation") / relative_test_path)
 
         with context_managers.set_cwd_to(project_path):
-            with open(Path("..") / ".." / "common" / "construct_at_once.json", 'r') as parameter_file:
+            with open(Path("..") / ".." / "common" / analysis_filename, 'r') as parameter_file:
                 project_parameters = Kratos.Parameters(parameter_file.read())
                 project = Project(project_parameters)
                 orchestrator_reg_entry = Kratos.Registry[project.GetSettings()["orchestrator"]["name"].GetString()]
@@ -494,7 +492,7 @@ class KratosGeoMechanicsCrowValidation(KratosUnittest.TestCase):
         )
 
     def test_simulation_without_excavation(self):
-        self.run_simulation_and_checks(Path("linear_elastic") / "construct_at_once")
+        self.run_simulation_and_checks(Path("linear_elastic") / "construct_at_once", "construct_at_once.json")
 
 
 if __name__ == "__main__":
