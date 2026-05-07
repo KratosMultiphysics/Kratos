e3ad70406649293fbb979ba71b6d9773af68658a
Vicente Mataix Ferrándiz <vmataix@altair.com>
2026-05-07 14:14:49 +0200

    refactor: streamline assembly finalization in trilinos_block_builder_and_solver

--- applications/TrilinosApplication/custom_strategies/builder_and_solvers/trilinos_block_builder_and_solver.h
+++ applications/TrilinosApplication/custom_strategies/builder_and_solvers/trilinos_block_builder_and_solver.h
@@ -784,12 +784,6 @@ public:
 
         ConstructMasterSlaveConstraintsStructure(rModelPart);
 
-        // Finalize assembly after all structure is built
-        TSparseSpace::GlobalAssemble(*rpA);
-        if (!TSparseSpace::IsNull(mpT)) {
-            TSparseSpace::GlobalAssemble(*mpT);
-        }
-
         KRATOS_CATCH("")
     }
 
@@ -1365,6 +1359,11 @@ protected:
             // Reset flag
             mConstraintsAssembled = false;
 
+            // Finalize assembly after all structure is built
+            if (!TSparseSpace::IsNull(mpT)) {
+                TSparseSpace::GlobalAssemble(*mpT);
+            }
+
             STOP_TIMER("ConstraintsRelationMatrixStructure", 0)
         }
     }
@@ -1599,6 +1598,9 @@ protected:
             BaseType::mEquationSystemSize,
             mpMap
         );
+
+        // Finalize assembly after all structure is built
+        TSparseSpace::GlobalAssemble(*rpA);
 
         STOP_TIMER("MatrixStructure", 0)
     }
