19f130f32d30569eb186c0367fdda1b3999590b8
DESKTOP-EARHLAT\Alejandro <alejandro.cornejo23@gmail.com>
2026-05-08 10:31:15 +0200

    move to cpp

--- applications/ContactStructuralMechanicsApplication/custom_processes/prepare_linear_constraints_for_quadratic_contact_process.h
+++ applications/ContactStructuralMechanicsApplication/custom_processes/prepare_linear_constraints_for_quadratic_contact_process.h
@@ -7,7 +7,7 @@
 //  License:         BSD License
 //                   license: ContactStructuralMechanicsApplication/license.txt
 //
-//  Main authors:    Your Name
+//  Main authors:    Alejandro Cornejo
 //
 
 #pragma once
@@ -95,63 +95,7 @@ public:
      * @brief This function is designed for being called at the beginning of the computations
      * right after reading the model and the groups
      */
-    void ExecuteInitialize() override
-    {
-        int max_condition_id = 1000; // TODO take the actual max id from the model part
-
-        for (auto& r_node : mrModelPart.Nodes()) {
-            r_node.Set(VISITED, false);
-        }
-
-        std::vector<std::vector<std::size_t>> master_slave_nodes_ids = {{0, 1, 3}, {1, 2, 4}, {2, 0, 5}};
-        MasterSlaveConstraint::DofPointerVectorType master_dofs(2);
-        MasterSlaveConstraint::DofPointerVectorType slave_dofs(1);
-
-        Matrix relation_matrix(1, 2);
-        relation_matrix(0, 0) = 0.5;
-        relation_matrix(0, 1) = 0.5;
-        Vector constant_vector(2);
-        constant_vector.clear();
-
-        for (auto& r_geom : mrModelPart.Geometries()) {
-
-            IndexType count = 0;
-            const IndexType number_of_points = r_geom.PointsNumber();
-            for (IndexType it_node = 0; it_node < number_of_points; ++it_node) {
-                if (r_geom[it_node].IsDefined(INTERFACE))
-                    if (r_geom[it_node].Is(INTERFACE))
-                        ++count;
-            }
-
-            if (count == 6) { // quadratic triangles
-                for (IndexType edge = 0; edge < 3; ++edge) {
-                    if (r_geom[master_slave_nodes_ids[edge][2]].IsNot(VISITED)) {
-                        master_dofs[0] = r_geom[master_slave_nodes_ids[edge][0]].pGetDof(DISPLACEMENT_X);
-                        master_dofs[1] = r_geom[master_slave_nodes_ids[edge][1]].pGetDof(DISPLACEMENT_X);
-                        slave_dofs[0]  = r_geom[master_slave_nodes_ids[edge][2]].pGetDof(DISPLACEMENT_X);
-                        mrModelPart.CreateNewMasterSlaveConstraint("LinearMasterSlaveConstraint", max_condition_id, master_dofs,
-                            slave_dofs, relation_matrix, constant_vector);
-                        ++max_condition_id;
-                        master_dofs[0] = r_geom[master_slave_nodes_ids[edge][0]].pGetDof(DISPLACEMENT_Y);
-                        master_dofs[1] = r_geom[master_slave_nodes_ids[edge][1]].pGetDof(DISPLACEMENT_Y);
-                        slave_dofs[0]  = r_geom[master_slave_nodes_ids[edge][2]].pGetDof(DISPLACEMENT_Y);
-                        mrModelPart.CreateNewMasterSlaveConstraint("LinearMasterSlaveConstraint", max_condition_id, master_dofs,
-                            slave_dofs, relation_matrix, constant_vector);
-                        ++max_condition_id;
-    
-                        master_dofs[0] = r_geom[master_slave_nodes_ids[edge][0]].pGetDof(DISPLACEMENT_Z);
-                        master_dofs[1] = r_geom[master_slave_nodes_ids[edge][1]].pGetDof(DISPLACEMENT_Z);
-                        slave_dofs[0] = r_geom[master_slave_nodes_ids[edge][2]].pGetDof(DISPLACEMENT_Z);
-                        mrModelPart.CreateNewMasterSlaveConstraint("LinearMasterSlaveConstraint", max_condition_id, master_dofs,
-                            slave_dofs, relation_matrix, constant_vector);
-                        ++max_condition_id;
-
-                        r_geom[master_slave_nodes_ids[edge][2]].Set(VISITED, true);
-                    }
-                }
-            }
-        }
-    }
+    void ExecuteInitialize() override;
 
     /**
      * @brief This function is designed for being execute once before the solution loop but after
