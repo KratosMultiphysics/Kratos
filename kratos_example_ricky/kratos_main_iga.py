56dbcd8b3687f30af4ddb2854da7952eb82dc39a
watf-dev <wataru.fukuda.530@gmail.com>
2026-05-18 12:19:51 +0200

    add

---
+++ kratos_example_ricky/kratos_main_iga.py
@@ -0,0 +1,11 @@
+import KratosMultiphysics
+import KratosMultiphysics.IgaApplication
+from KratosMultiphysics.StructuralMechanicsApplication.structural_mechanics_analysis import StructuralMechanicsAnalysis
+
+if __name__ == "__main__":
+    with open("ProjectParameters.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = StructuralMechanicsAnalysis(model, parameters)
+    simulation.Run()
