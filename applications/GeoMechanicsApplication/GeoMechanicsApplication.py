faf4fd27e6a52f3243cc4d7e0db5d06645acd8ab
markelov <gennady.markelov@deltares.nl>
2026-05-20 23:53:30 +0200

    updated _required_bindings

--- applications/GeoMechanicsApplication/GeoMechanicsApplication.py
+++ applications/GeoMechanicsApplication/GeoMechanicsApplication.py
@@ -7,19 +7,84 @@ import KratosGeoMechanicsApplication as _KratosGeoMechanicsApplication
 # Explicitly export required bindings by name.
 _required_bindings = (
     "KratosGeoMechanicsApplication",
-    "ProcessUtilities",
+    "KratosExecuteCallBackFunctions",
+    "KratosExecuteCriticalHeadInfo",
+
+    "BackwardEulerQuasistaticPwScheme",
+    "GeneralizedNewmarkTScheme",
+    "GeoLoadSteppingScheme",
+    "GeoStaticScheme",
+    "GeoMechanicsNewtonRaphsonStrategy",
+    "GeoMechanicsNewtonRaphsonErosionProcessStrategy",
+    "NewmarkDynamicUPwScheme",
+    "NewmarkQuasistaticDampedUPwScheme",
+    "NewmarkQuasistaticPwScheme",
+    "NewmarkQuasistaticUPwScheme",
     "CustomWorkflowFactory",
+    "DeactivateConditionsOnInactiveElements",
+    "ProcessUtilities",
+    "ResidualBasedBlockBuilderAndSolverWithMassAndDamping",
+    "NodeUtilities",
+
+    "ApplyCPhiReductionProcess",
+    "ApplyFinalStressesOfPreviousStageToInitialState",
+    "ApplyExcavationProcess",
+    "ApplyInitialUniformStressField",
+    "ApplyK0ProcedureProcess",
+    "ApplyNormalLoadTableProcess",
+    "ApplyScalarConstraintTableProcess",
+    "ApplyVectorConstraintTableProcess",
+    "BackwardEulerQuasistaticUPwScheme",
+    "BackwardEulerTScheme",
+    "CalculateIncrementalMotionProcess",
+    "CalculateTotalMotionProcess",
+    "FindNeighbourElementsOfConditionsProcess",
+    "FindNeighboursOfInterfacesProcess",
+    "GeoExtrapolateIntegrationPointValuesToNodesProcess",
+    "SetAbsorbingBoundaryParametersProcess",
+    "SetMultipleMovingLoadsProcess",
+    "SetParameterFieldProcess",
+
+    "AIR_HUMIDITY",
+    "AIR_TEMPERATURE",
+    "DT_PRESSURE_COEFFICIENT",
+    "DT_TEMPERATURE",
+    "DT_TEMPERATURE_COEFFICIENT",
+    "DT_WATER_PRESSURE",
+    "FLUID_FLUX_VECTOR",
+    "GEO_PLASTICITY_STATUS",
+    "GEO_RELATIVE_DISPLACEMENT_VECTOR",
+    "GEO_EFFECTIVE_TRACTION_VECTOR",
+    "HYDRAULIC_HEAD",
+    "INCREMENTAL_DISPLACEMENT",
+    "INCREMENTAL_ROTATION",
+    "IS_CONVERGED",
+    "LOCAL_STRESS_VECTOR",
+    "NORMAL_HEAT_FLUX",
+    "NORMAL_FLUID_FLUX",
+    "PIPE_ACTIVE",
+    "PIPE_ELEMENT_LENGTH",
+    "PRECIPITATION",
+    "SOLAR_RADIATION",
+    "TIME_UNIT_CONVERTER",
+    "TOTAL_DISPLACEMENT",
+    "TOTAL_DISPLACEMENT_X",
+    "TOTAL_DISPLACEMENT_Y",
+    "TOTAL_DISPLACEMENT_Z",
+    "TOTAL_ROTATION",
+    "TOTAL_ROTATION_X",
+    "TOTAL_ROTATION_Y",
+    "TOTAL_ROTATION_Z",
+    "TOTAL_STRESS_TENSOR",
+    "VELOCITY_COEFFICIENT",
+    "WIND_SPEED",
+
     "UMAT_PARAMETERS",
 )
 for _name in _required_bindings:
     globals()[_name] = getattr(_KratosGeoMechanicsApplication, _name)
 
 KratosGeoMechanicsApplication = _KratosGeoMechanicsApplication.KratosGeoMechanicsApplication
-
-# Re-export all public bindings from the extension module without wildcard imports.
-for _name in dir(_KratosGeoMechanicsApplication):
-    if not _name.startswith("_"):
-        globals()[_name] = getattr(_KratosGeoMechanicsApplication, _name)
 
 application = KratosGeoMechanicsApplication()
 application_name = "KratosGeoMechanicsApplication"
