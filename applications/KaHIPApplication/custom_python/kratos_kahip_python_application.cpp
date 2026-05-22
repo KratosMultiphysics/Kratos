74c8ea663e1a67d96a56dac05959995dad3167e7
Vicente Mataix Ferrándiz <vicente.mataix@polytechnique.edu>
2026-05-22 08:48:22 +0200

    Add Python bindings for KaHIP application

---
+++ applications/KaHIPApplication/custom_python/kratos_kahip_python_application.cpp
@@ -0,0 +1,41 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Vicente Mataix Ferrandiz
+//
+
+// System includes
+
+#if defined(KRATOS_PYTHON)
+// External includes
+#include <pybind11/pybind11.h>
+
+// Project includes
+#include "includes/define_python.h"
+#include "kahip_application.h"
+#include "custom_python/add_custom_processes_to_python.h"
+
+namespace Kratos::Python {
+
+PYBIND11_MODULE(KratosKaHIPApplication, m)
+{
+    namespace py = pybind11;
+
+    py::class_<KratosKaHIPApplication,
+               KratosKaHIPApplication::Pointer,
+               KratosApplication>(m, "KratosKaHIPApplication")
+        .def(py::init<>())
+        ;
+
+    AddCustomProcessesToPython(m);
+}
+
+} // namespace Kratos::Python
+
+#endif // KRATOS_PYTHON defined
