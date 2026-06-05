09af01bee340d9464135fa9122164d02dbabfd14
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@siemens.com>
2026-06-05 16:05:01 +0200

    Add tests for CartesianMeshGeneratorModeler functionality

--- kratos/tests/test_KratosCore.py
+++ kratos/tests/test_KratosCore.py
@@ -100,6 +100,7 @@ import test_obj_io
 import test_import_obj_modeler
 import test_vectorized_interpolation
 import test_clean_up_problematic_triangles_modeler
+import test_cartesian_mesh_generator_modeler
 import test_tetrahedral_mesh_orientation_check
 import test_nd_data
 import test_tensor_adaptors
@@ -230,6 +231,7 @@ def AssembleTestSuites():
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_import_obj_modeler.TestImportOBJModeler]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_vectorized_interpolation.TestVectorizedInterpolation]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_clean_up_problematic_triangles_modeler.TestCleanUpProblematicTrianglesModeler]))
+    smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_cartesian_mesh_generator_modeler.TestCartesianMeshGeneratorModeler]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_tetrahedral_mesh_orientation_check.TestTetrahedralMeshOrientationCheck]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_nd_data.TestNDData]))
     smallSuite.addTests(KratosUnittest.TestLoader().loadTestsFromTestCases([test_tensor_adaptors.TestTensorAdaptors]))
