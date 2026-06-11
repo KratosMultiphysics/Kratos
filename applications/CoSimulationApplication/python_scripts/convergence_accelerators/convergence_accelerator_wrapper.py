37741d18860454578af93b749946b8e550d332ab
Juan Ignacio Camarotti <juanicama7@gmail.com>
2026-06-11 09:31:22 +0200

    new interface for 2 field convergence accelerators

--- applications/CoSimulationApplication/python_scripts/convergence_accelerators/convergence_accelerator_wrapper.py
+++ applications/CoSimulationApplication/python_scripts/convergence_accelerators/convergence_accelerator_wrapper.py
@@ -20,24 +20,58 @@ class ConvergenceAcceleratorWrapper:
                  settings: KratosMultiphysics.Parameters,
                  interface_data_dict: "dict[str,CouplingInterfaceData]",
                  parent_coupled_solver_data_communicator: KratosMultiphysics.DataCommunicator):
-        self.interface_data = interface_data_dict[settings["data_name"].GetString()]
-        self.residual_computation = CreateResidualComputation(settings, interface_data_dict)
+        # self.interface_data = interface_data_dict[settings["data_name"].GetString()]
+        # self.residual_computation = CreateResidualComputation(settings, interface_data_dict)
+        self.is_two_field_accelerator = settings.Has("data_names")
+
+        if self.is_two_field_accelerator:
+            if settings["data_names"].size() != 2:
+                raise Exception('"data_names" must contain exactly two entries!')
+
+            self.data_name_1 = settings["data_names"][0].GetString()
+            self.data_name_2 = settings["data_names"][1].GetString()
+
+            self.interface_data_1 = interface_data_dict[self.data_name_1]
+            self.interface_data_2 = interface_data_dict[self.data_name_2]
+
+            if self.interface_data_1.Size() != self.interface_data_2.Size():
+                raise Exception(
+                    f'Two-field accelerator requires equal data sizes. '
+                    f'{self.data_name_1}: {self.interface_data_1.Size()}, '
+                    f'{self.data_name_2}: {self.interface_data_2.Size()}'
+                )
+
+            self.interface_data = self.interface_data_1
+
+        else:
+            self.interface_data = interface_data_dict[settings["data_name"].GetString()]
+            self.residual_computation = CreateResidualComputation(settings, interface_data_dict)
 
         # Remove extra entries from accelerator parameters
-        for key in ("data_name", "solver", "residual_computation"):
+        for key in ("data_name", "data_names", "solver", "residual_computation"):
             if settings.Has(key):
                 settings.RemoveValue(key)
 
         self.conv_acc = CreateConvergenceAccelerator(settings)
         self.data_communicator = parent_coupled_solver_data_communicator
 
+        self.executing_rank = False
+        self.gather_scatter_required = False
+
         if self.interface_data.IsDefinedOnThisRank():
             conv_acc_supports_dist_data = self.conv_acc.SupportsDistributedData()
-            self.executing_rank = conv_acc_supports_dist_data or (self.interface_data.GetModelPart().GetCommunicator().MyPID() == 0)
-            self.gather_scatter_required = self.interface_data.IsDistributed() and not conv_acc_supports_dist_data
+            self.executing_rank = conv_acc_supports_dist_data or (
+                self.interface_data.GetModelPart().GetCommunicator().MyPID() == 0
+            )
+            self.gather_scatter_required = (
+                self.interface_data.IsDistributed() and not conv_acc_supports_dist_data
+            )
+
             if self.gather_scatter_required:
                 self.data_comm = self.interface_data.GetModelPart().GetCommunicator().GetDataCommunicator()
-                self.sizes_from_ranks = np.cumsum(self.data_comm.GatherInts([self.interface_data.Size()], 0))
+                self.sizes_from_ranks = np.cumsum(
+                    self.data_comm.GatherInts([self.interface_data.Size()], 0)
+                )
 
     def Initialize(self):
         self.conv_acc.Initialize()
@@ -52,10 +86,16 @@ class ConvergenceAcceleratorWrapper:
         self.conv_acc.FinalizeSolutionStep()
 
     def InitializeNonLinearIteration(self):
+        # if self.interface_data.IsDefinedOnThisRank():
+        #     # Saving the previous data for the computation of the residual
+        #     # and the computation of the solution update
+        #     self.input_data = self.interface_data.GetData()
         if self.interface_data.IsDefinedOnThisRank():
-            # Saving the previous data for the computation of the residual
-            # and the computation of the solution update
-            self.input_data = self.interface_data.GetData()
+            if self.is_two_field_accelerator:
+                self.input_data_1 = self.interface_data_1.GetData()
+                self.input_data_2 = self.interface_data_2.GetData()
+            else:
+                self.input_data = self.interface_data.GetData()
 
         self.conv_acc.InitializeNonLinearIteration()
 
@@ -63,6 +103,49 @@ class ConvergenceAcceleratorWrapper:
         self.conv_acc.FinalizeNonLinearIteration()
 
     def ComputeAndApplyUpdate(self):
+        if self.is_two_field_accelerator:
+            if not self.interface_data_1.IsDefinedOnThisRank():
+                return
+
+            residual_1 = self.interface_data_1.GetData() - self.input_data_1
+            residual_2 = self.interface_data_2.GetData() - self.input_data_2
+
+            input_data_1_for_acc = self.input_data_1
+            input_data_2_for_acc = self.input_data_2
+
+            if self.gather_scatter_required:
+                residual_1 = np.array(np.concatenate(self.data_comm.GathervDoubles(residual_1, 0)))
+                residual_2 = np.array(np.concatenate(self.data_comm.GathervDoubles(residual_2, 0)))
+
+                input_data_1_for_acc = np.array(np.concatenate(self.data_comm.GathervDoubles(input_data_1_for_acc, 0)))
+                input_data_2_for_acc = np.array(np.concatenate(self.data_comm.GathervDoubles(input_data_2_for_acc, 0)))
+
+            if self.executing_rank:
+                delta_1, delta_2 = self.conv_acc.UpdateSolution(
+                    (residual_1, residual_2),
+                    (input_data_1_for_acc, input_data_2_for_acc)
+                )
+
+                updated_data_1 = input_data_1_for_acc + delta_1
+                updated_data_2 = input_data_2_for_acc + delta_2
+
+            if self.gather_scatter_required:
+                if self.executing_rank:
+                    data_to_scatter_1 = np.split(updated_data_1, self.sizes_from_ranks[:-1])
+                    data_to_scatter_2 = np.split(updated_data_2, self.sizes_from_ranks[:-1])
+                else:
+                    data_to_scatter_1 = []
+                    data_to_scatter_2 = []
+
+                updated_data_1 = self.data_comm.ScattervDoubles(data_to_scatter_1, 0)
+                updated_data_2 = self.data_comm.ScattervDoubles(data_to_scatter_2, 0)
+
+            self.interface_data_1.SetData(updated_data_1)
+            self.interface_data_2.SetData(updated_data_2)
+
+            return
+
+        # original single-field implementation continues below
         if not self.interface_data.IsDefinedOnThisRank(): return
 
         residual = self.residual_computation.ComputeResidual(self.input_data)
