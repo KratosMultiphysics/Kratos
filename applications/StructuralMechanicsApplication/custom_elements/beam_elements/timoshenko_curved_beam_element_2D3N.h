fbf915bb21558212617b106b44a4892c70e92580
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2026-06-19 15:43:31 +0200

    Removed unused, but recently introduced function.

--- applications/StructuralMechanicsApplication/custom_elements/beam_elements/timoshenko_curved_beam_element_2D3N.h
+++ applications/StructuralMechanicsApplication/custom_elements/beam_elements/timoshenko_curved_beam_element_2D3N.h
@@ -180,14 +180,6 @@ public:
     }
 
     /**
-    * @brief Indicates the amount of DoFs per node (u0, v, theta)
-    */
-    virtual IndexType GetDoFsPerNode() const
-    {
-        return 3;
-    }
-
-    /**
     * element can be integrated using the GP provided by the geometry or custom ones
     * by default, the base element will use the standard integration provided by the geom
     * @return bool to select if use/not use GPs given by the geometry
