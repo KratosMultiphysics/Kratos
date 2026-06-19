34bd48562f2c8e3ea61d03d7797f443294f52f12
Wijtze Pieter Kikstra <wijtzepieter.kikstra@deltares.nl>
2026-06-19 17:10:21 +0200

    improved error message ( corrected class name )

--- applications/GeoMechanicsApplication/custom_constitutive/p_q_theta.hpp
+++ applications/GeoMechanicsApplication/custom_constitutive/p_q_theta.hpp
@@ -38,7 +38,7 @@ public:
         auto first = std::begin(rValues);
         auto last  = std::end(rValues);
         KRATOS_DEBUG_ERROR_IF(std::distance(first, last) != msVectorSize)
-            << "Cannot construct a PQ instance: expected " << msVectorSize << " values, but got "
+            << "Cannot construct a PQTheta instance: expected " << msVectorSize << " values, but got "
             << std::distance(first, last) << " value(s)\n";
 
         std::copy(first, last, mValues.begin());
