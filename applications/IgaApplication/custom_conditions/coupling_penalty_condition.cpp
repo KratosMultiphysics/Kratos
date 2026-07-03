d2e4dc6e6710593ef0b830741fe010d3f5b41731
Ricky Aristio <ricky.aristio@gmail.com>
2026-07-03 08:25:16 +0200

    fix the test and the penalty rotation flag

--- applications/IgaApplication/custom_conditions/coupling_penalty_condition.cpp
+++ applications/IgaApplication/custom_conditions/coupling_penalty_condition.cpp
@@ -30,7 +30,6 @@ namespace Kratos
     {
         KRATOS_TRY
         const double penalty = GetProperties()[PENALTY_FACTOR];
-        const double penalty_rotation = GetProperties()[PENALTY_ROTATION_FACTOR];
 
         const auto& r_geometry_master = GetGeometry().GetGeometryPart(0);
         const auto& r_geometry_slave = GetGeometry().GetGeometryPart(1);
@@ -101,11 +100,13 @@ namespace Kratos
 
             // Differential area
             double penalty_integration = penalty * integration_points[point_number].Weight() * determinant_jacobian_vector[point_number];
-            double penalty_rotation_integration = penalty_rotation * integration_points[point_number].Weight() * determinant_jacobian_vector[point_number];
             
             // Rotation coupling
-            if (Is(IgaFlags::FIX_ROTATION_X))
+            if (Is(IgaFlags::FIX_ROTATION_X) || Is(IgaFlags::FIX_ROTATION_Y) || Is(IgaFlags::FIX_ROTATION_Z))
             {
+                const double penalty_rotation = GetProperties()[PENALTY_ROTATION_FACTOR];
+                double penalty_rotation_integration = penalty_rotation * integration_points[point_number].Weight() * determinant_jacobian_vector[point_number];
+
                 Vector phi_r = ZeroVector(mat_size);
                 Matrix phi_rs = ZeroMatrix(mat_size, mat_size);
                 array_1d<double, 2> diff_phi;
