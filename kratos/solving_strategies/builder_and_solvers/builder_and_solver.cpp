51bae99efae366d86bb9b781445e22d6090d4f5b
Vicente Mataix Ferrándiz <vmataix@altair.com>
2026-07-09 17:41:34 +0200

    Refactor identity matrix creation to use CSR arrays for compatibility with both uBLAS and Eigen backends

--- kratos/solving_strategies/builder_and_solvers/builder_and_solver.cpp
+++ kratos/solving_strategies/builder_and_solvers/builder_and_solver.cpp
@@ -1,37 +1,23 @@
 // Project includes
 #include "solving_strategies/builder_and_solvers/builder_and_solver.h" // BuilderAndSolver
-#include "spaces/ublas_space.h" // TUblasSparseSpace, TUblasDenseSpace
 #include "spaces/default_spaces.h" // TDefaultSparseSpace, TDefaultDenseSpace
 #include "linear_solvers/linear_solver.h" // LinearSolver
-#ifdef KRATOS_USE_EIGEN_BACKEND
-#include "linear_solvers/linear_solver_eigen.h" // Already-exported LinearSolver<TEigenSparseSpace<double>, TUblasDenseSpace<double>>
-#endif
 
 
 namespace Kratos {
 
 
-template class KRATOS_API(KRATOS_CORE) BuilderAndSolver<TUblasSparseSpace<double>,
-                                                        TUblasDenseSpace<double>,
-                                                        LinearSolver<TUblasSparseSpace<double>,
-                                                                     TUblasDenseSpace<double>>>;
-
-template class KRATOS_API(KRATOS_CORE) BuilderAndSolver<TUblasSparseSpace<float>,
-                                                        TUblasDenseSpace<double>,
-                                                        LinearSolver<TUblasSparseSpace<float>,
-                                                                     TUblasDenseSpace<double>>>;
-
-#ifdef KRATOS_USE_EIGEN_BACKEND
-// With the Eigen backend the default sparse space (used e.g. by
-// ResidualBasedBlockBuilderAndSolver<DefaultSparseSpaceType, ...> in
-// add_strategies_to_python.cpp and in the uBLAS/Eigen parity tests) differs
-// from the uBLAS one explicitly instantiated above, so it needs its own
-// explicit instantiation to be importable from other binaries/DLLs.
+// Instantiated on the configure-time selected linear-algebra backend only:
+// the two backends are mutually exclusive for the real sparse spaces.
 template class KRATOS_API(KRATOS_CORE) BuilderAndSolver<TDefaultSparseSpace<double>,
                                                         TDefaultDenseSpace<double>,
                                                         LinearSolver<TDefaultSparseSpace<double>,
                                                                      TDefaultDenseSpace<double>>>;
-#endif
+
+template class KRATOS_API(KRATOS_CORE) BuilderAndSolver<TDefaultSparseSpace<float>,
+                                                        TDefaultDenseSpace<double>,
+                                                        LinearSolver<TDefaultSparseSpace<float>,
+                                                                     TDefaultDenseSpace<double>>>;
 
 
 } // namespace Kratos
