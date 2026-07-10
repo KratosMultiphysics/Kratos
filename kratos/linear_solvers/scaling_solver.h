4bb1a92043f00ce5f116d1776070a95171f573f6
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@siemens.com>
2026-07-10 15:23:31 +0200

    Revert "Update kratos/linear_solvers/scaling_solver.h"
    This reverts commit 6c356c7e31e90245ae5e246c5d09efc35575f782.

--- kratos/linear_solvers/scaling_solver.h
+++ kratos/linear_solvers/scaling_solver.h
@@ -93,7 +93,7 @@ public:
     using IndexIteratorType = decltype(std::declval<SparseMatrixType&>().index1_data().begin());
 
     /// Definition of the const index iterator type
-    using ConstIndexIteratorType = std::decltype(std::declval<const SparseMatrixType&>().index1_data().begin());
+    using ConstIndexIteratorType = decltype(std::declval<const SparseMatrixType&>().index1_data().begin());
 
     /// Definition of the value iterator type
     using ValueIteratorType = std::decltype(std::declval<SparseMatrixType&>().value_data().begin());
