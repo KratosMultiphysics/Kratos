483b336c1754a5c5ca6eb536cede8fb45ae9fc41
Vicente Mataix Ferrandiz <vmataix@altair.com>
2026-07-27 22:31:57 +0200

    Improve registry

--- applications/MeshioPlusPlusApplication/python_registry_lists.py
+++ applications/MeshioPlusPlusApplication/python_registry_lists.py
@@ -3,6 +3,7 @@ from typing import List
 python_modelers_to_be_registered: List[str] = [
     "modelers.meshio_input_modeler.MeshioInputModeler",
     "modelers.meshio_operation_modeler.MeshioOperationModeler",
+    "modelers.meshio_interpolate_modeler.MeshioInterpolateModeler",
 ]
 
 python_operations_to_be_registered: List[str] = []
