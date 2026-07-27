de5c26c1fc162d1a94e5140e66079375caf3e667
Ricky Aristio <ricky.aristio@gmail.com>
2026-07-27 13:07:49 +0200

    add initial flattening test

--- applications/StructuralMechanicsApplication/tests/test_patch_test_cutting_pattern.py
+++ applications/StructuralMechanicsApplication/tests/test_patch_test_cutting_pattern.py
@@ -138,6 +138,77 @@ class TestPatchTestCuttingPattern(KratosUnittest.TestCase):
             self.assertAlmostEqual(node.GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Y), 0.0)
             self.assertAlmostEqual(node.GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT_Z), expected_z)
 
+    def test_initial_flattening_utility_per_part_mean_normal(self):
+        current_model = KratosMultiphysics.Model()
+        mp = current_model.CreateModelPart("per_part_flattening")
+        mp.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, 3)
+        self._add_variables(mp)
+        self._apply_material_properties(mp)
+
+        node_coords = {
+            1: (0.0, 0.0, 0.0),
+            2: (1.0, 0.0, 0.0),
+            3: (0.0, 1.0, 1.0),
+            4: (2.0, 0.0, 0.0),
+            5: (1.0, 1.0, -1.0),
+        }
+        for nid, (x, y, z) in node_coords.items():
+            mp.CreateNewNode(nid, x, y, z)
+        self._add_dofs(mp)
+
+        element_name = "MembraneCuttingPatternElement3D3N"
+        elem_a = mp.CreateNewElement(element_name, 1, [1, 2, 3], mp.GetProperties()[1])
+        elem_b = mp.CreateNewElement(element_name, 2, [2, 4, 5], mp.GetProperties()[1])
+
+        part_a = mp.CreateSubModelPart("PartA")
+        part_a.AddNodes([1, 2, 3])
+        part_a.AddElements([1])
+
+        part_b = mp.CreateSubModelPart("PartB")
+        part_b.AddNodes([2, 4, 5])
+        part_b.AddElements([2])
+
+        settings = KratosMultiphysics.Parameters("""{
+            "projection_type" : "planar_mean_normal",
+            "sub_model_parts" : ["PartA", "PartB"],
+            "echo_level"      : 0
+        }""")
+        StructuralMechanicsApplication.InitialFlatteningUtility.Execute(mp, settings)
+
+        def unit_normal(p0, p1, p2):
+            v1 = [p1[i] - p0[i] for i in range(3)]
+            v2 = [p2[i] - p0[i] for i in range(3)]
+            cross = [
+                v1[1] * v2[2] - v1[2] * v2[1],
+                v1[2] * v2[0] - v1[0] * v2[2],
+                v1[0] * v2[1] - v1[1] * v2[0],
+            ]
+            norm = math.sqrt(sum(c * c for c in cross))
+            return [c / norm for c in cross]
+
+        normal_a = unit_normal(node_coords[1], node_coords[2], node_coords[3])
+        normal_b = unit_normal(node_coords[2], node_coords[4], node_coords[5])
+        shared_sum = [normal_a[i] + normal_b[i] for i in range(3)]
+        shared_norm = math.sqrt(sum(c * c for c in shared_sum))
+        normal_shared = [c / shared_norm for c in shared_sum]
+
+        def expected_proj(pos, normal):
+            dot = sum(pos[i] * normal[i] for i in range(3))
+            return [-dot * normal[i] for i in range(3)]
+
+        expected = {
+            1: expected_proj(node_coords[1], normal_a),
+            2: expected_proj(node_coords[2], normal_shared),
+            3: expected_proj(node_coords[3], normal_a),
+            4: expected_proj(node_coords[4], normal_b),
+            5: expected_proj(node_coords[5], normal_b),
+        }
+
+        for node_id, expected_disp in expected.items():
+            disp = mp.Nodes[node_id].GetSolutionStepValue(KratosMultiphysics.DISPLACEMENT)
+            for i in range(3):
+                self.assertAlmostEqual(disp[i], expected_disp[i], places=10)
+
 
 if __name__ == '__main__':
     KratosUnittest.main()
