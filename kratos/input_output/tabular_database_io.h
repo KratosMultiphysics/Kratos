0f3c429b121e29cb32e02cb1f5db038dc0a56793
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2026-08-13 12:52:24 +0200

    change database name

--- kratos/input_output/tabular_database_io.h
+++ kratos/input_output/tabular_database_io.h
@@ -25,35 +25,33 @@ namespace Kratos
 ///@name Kratos Classes
 ///@{
 
-class KRATOS_API(KRATOS_CORE) TransientDatabaseIO
+class KRATOS_API(KRATOS_CORE) TabularDatabaseIO
 {
 public:
     ///@name Type definitions
     ///@{
 
-    KRATOS_CLASS_POINTER_DEFINITION(TransientDatabaseIO);
+    KRATOS_CLASS_POINTER_DEFINITION(TabularDatabaseIO);
 
     ///@}
     ///@name Life cycle
     ///@{
 
-    virtual ~TransientDatabaseIO() = default;
-
-    ///@}
-    ///@name Public operations
-    ///@{
-
-    virtual void Initialize() {}
-
-    virtual void Finalize() {}
+    virtual ~TabularDatabaseIO() = default;
 
     ///@}
     ///@name Input / output
     ///@{
 
+    virtual std::vector<int> GetRowIds() const
+    {
+        KRATOS_ERROR << "Please implement this method in the derived class.";
+        return std::vector<int>{};
+    }
+
     virtual void Read(
         bool& rValue,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -61,7 +59,7 @@ public:
 
     virtual void Read(
         int& rValue,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -69,7 +67,7 @@ public:
 
     virtual void Read(
         double& rValue,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -77,7 +75,7 @@ public:
 
     virtual void Read(
         std::string& rValue,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -85,7 +83,7 @@ public:
 
     virtual void Write(
         const bool Value,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -93,7 +91,7 @@ public:
 
     virtual void Write(
         const int Value,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -101,7 +99,7 @@ public:
 
     virtual void Write(
         const double Value,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
@@ -109,14 +107,14 @@ public:
 
     virtual void Write(
         const std::string& rValue,
-        const int Step,
+        const int RowId,
         const std::string& rKey)
     {
         KRATOS_ERROR << "Please implement this method in the derived class.";
     }
 
     /// Turn back information as a string.
-    virtual std::string Info() const { return "TransientDatabaseIO"; }
+    virtual std::string Info() const { return "TabularDatabaseIO"; }
 
     /// Print information about this object.
     virtual void PrintInfo(std::ostream& rOStream) const { rOStream << Info(); }
@@ -131,7 +129,7 @@ public:
 ///@name Input and output
 ///@{
 
-inline std::ostream& operator << (std::ostream& rOStream, const TransientDatabaseIO& rThis)
+inline std::ostream& operator << (std::ostream& rOStream, const TabularDatabaseIO& rThis)
 {
     rThis.PrintInfo(rOStream);
     rOStream << std::endl;
