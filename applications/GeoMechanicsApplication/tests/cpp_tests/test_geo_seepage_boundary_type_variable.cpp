e56440766aab4ba5384ab3d1c595b45716a770b5
Richard Faasse <richard.faasse@deltares.nl>
2026-08-25 14:53:07 +0200

    Add GEO_SEEPAGE_BOUNDARY_TYPE variable

---
+++ applications/GeoMechanicsApplication/tests/cpp_tests/test_geo_seepage_boundary_type_variable.cpp
@@ -0,0 +1,32 @@
+// KRATOS___
+//     //   ) )
+//    //         ___      ___
+//   //  ____  //___) ) //   ) )
+//  //    / / //       //   / /
+// ((____/ / ((____   ((___/ /  MECHANICS
+//
+//  License:         geo_mechanics_application/license.txt
+//
+
+#include "geo_mechanics_application_variables.h"
+#include "includes/condition.h"
+#include "includes/kratos_components.h"
+#include "tests/cpp_tests/geo_mechanics_fast_suite.h"
+
+using namespace Kratos;
+
+namespace Kratos::Testing
+{
+
+KRATOS_TEST_CASE_IN_SUITE(GeoSeepageConditionBoundaryTypeVariableIsRegistered, KratosGeoMechanicsFastSuite)
+{
+    KRATOS_EXPECT_TRUE(KratosComponents<Variable<std::string>>::Has("GEO_SEEPAGE_BOUNDARY_TYPE"))
+}
+
+KRATOS_TEST_CASE_IN_SUITE(GeoSeepageConditionBoundaryTypeVariableHasExpectedName, KratosGeoMechanicsFastSuite)
+{
+    KRATOS_EXPECT_EQ(GEO_SEEPAGE_BOUNDARY_TYPE.Name(), "GEO_SEEPAGE_BOUNDARY_TYPE");
+}
+
+} // namespace Kratos::Testing
+
