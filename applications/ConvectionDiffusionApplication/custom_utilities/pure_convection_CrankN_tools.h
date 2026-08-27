60433baca7823009ee162a053f7d4fbeec8b4ca2
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@siemens.com>
2026-08-27 17:32:04 +0200

    Remove unused variable tot_nnz in pure_convection tools and CrankN utilities

--- applications/ConvectionDiffusionApplication/custom_utilities/pure_convection_CrankN_tools.h
+++ applications/ConvectionDiffusionApplication/custom_utilities/pure_convection_CrankN_tools.h
@@ -55,7 +55,6 @@ public:
         //mDofSet.resize(0);
         mDofSet.reserve(model_part.Nodes().size() );
 
-        int tot_nnz=0;
         //int tot_row=0;
         for (auto it=model_part.NodesBegin(); it!=model_part.NodesEnd(); ++it)
         {
@@ -63,7 +62,6 @@ public:
             {
                 mDofSet.push_back( it->pGetDof(rScalarVar) );
                 //tot_row += 1;
-                tot_nnz +=  (it->GetValue(NEIGHBOUR_NODES)).size()+1;
             }
         }
 
