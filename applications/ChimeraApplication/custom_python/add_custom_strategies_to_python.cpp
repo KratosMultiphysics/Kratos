aa57229fce8dfa2fdc868627e7ab4ed3d9fd91c1
Vicente Mataix Ferrandiz <vmataix@altair.com>
2026-08-31 08:09:02 +0200

    Refactor space type definitions in ChimeraApplication to use TDefaultSparseSpace and TDefaultDenseSpace for improved consistency in Python bindings

--- applications/ChimeraApplication/custom_python/add_custom_strategies_to_python.cpp
+++ applications/ChimeraApplication/custom_python/add_custom_strategies_to_python.cpp
@@ -24,6 +24,7 @@
 
 
 #include "spaces/ublas_space.h"
+#include "spaces/default_spaces.h"
 
 // Strategies
 #include "custom_utilities/solver_settings.h"
@@ -46,8 +47,8 @@ namespace Python
 void  AddCustomStrategiesToPython(pybind11::module& m)
 {
     namespace py = pybind11;
-    typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
-    typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
+    typedef TDefaultSparseSpace<double> SparseSpaceType;
+    typedef TDefaultDenseSpace<double> LocalSpaceType;
 
     // Base types
     typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
