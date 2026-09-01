307520a9464f466db21f7f6adf5271d3dba8e6cf
Vicente Mataix Ferrándiz <vicente.mataix-ferrandiz@siemens.com>
2026-09-01 17:20:13 +0200

    [FSIApp] Replace hardcoded uBLAS space types with configurable default space types

--- applications/FSIApplication/tests/cpp_tests/test_convergence_accelerators.cpp
+++ applications/FSIApplication/tests/cpp_tests/test_convergence_accelerators.cpp
@@ -18,7 +18,7 @@
 // Project includes
 #include "includes/define.h"
 #include "includes/global_variables.h"
-#include "spaces/ublas_space.h"
+#include "spaces/default_spaces.h"
 #include "testing/testing.h"
 
 // Application includes
@@ -30,8 +30,8 @@
 namespace Kratos {
 	namespace Testing {
 
-        typedef UblasSpace<double, Matrix, Vector> DenseSpaceType;
-        typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
+        typedef TDefaultDenseSpace<double> DenseSpaceType;
+        typedef TDefaultSparseSpace<double> SparseSpaceType;
         typedef typename ConvergenceAccelerator<SparseSpaceType, DenseSpaceType>::UniquePointer ConvAccPointerType;
 
         /**
