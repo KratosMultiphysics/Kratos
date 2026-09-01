ebfce5db5771c546d342dc4f04b6a63eb58517b3
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2026-09-01 11:03:17 +0200

    adding missing entry

--- applications/ContactStructuralMechanicsApplication/custom_conditions/mortar_contact_condition.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_conditions/mortar_contact_condition.cpp
@@ -351,7 +351,7 @@ void MortarContactCondition<TDim, TNumNodes, TFrictional, TNormalVariation, TNum
 
         if constexpr (TNormalVariation) DerivativesUtilitiesType::CalculateDeltaNormalMaster(derivative_data.DeltaNormalMaster, r_master_geometry);
 
-        const bool dual_LM =  DerivativesUtilitiesType::CalculateAeAndDeltaAe(r_slave_geometry, r_normal_slave, r_master_geometry, derivative_data, general_variables, consider_normal_variation, conditions_points_slave, this_integration_method, GetAxisymmetricCoefficient(general_variables));
+        const bool dual_LM =  DerivativesUtilitiesType::CalculateAeAndDeltaAe(r_slave_geometry, r_normal_slave, r_master_geometry, r_normal_master, derivative_data, general_variables, consider_normal_variation, conditions_points_slave, this_integration_method, GetAxisymmetricCoefficient(general_variables));
 
         PointerVector< PointType > points_array(TDim); // The points are stored as local coordinates, we calculate the global coordinates of this points
         array_1d<BelongType, TDim> belong_array;
