0d6bb06728ead1a6663c5d06d762ed163224d3d9
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2026-09-18 10:20:58 +0200

    revert to local model part search

--- applications/OptimizationApplication/python_scripts/processes/optimization_problem_field_output_process.py
+++ applications/OptimizationApplication/python_scripts/processes/optimization_problem_field_output_process.py
@@ -102,3 +102,23 @@ class OptimizationProblemFieldOutputProcess(Kratos.OutputProcess):
     def _CreateTensorAdaptorOutput(self, _: TensorAdaptorData) -> TensorAdaptorOutput:
         raise NotImplementedError("_CreateTensorAdaptorOutput needs to be implemented in the derived class")
 
+    def _GetModelPart(self, container) -> Kratos.ModelPart:
+        def get_model_part(container, model_part: Kratos.ModelPart):
+            if container in [model_part.Nodes, model_part.Conditions, model_part.Elements]:
+                return model_part
+
+            for sub_model_part_name in model_part.GetSubModelPartNames():
+                root_model_part = get_model_part(container, model_part.GetSubModelPart(sub_model_part_name))
+                if root_model_part is not None:
+                    return root_model_part
+
+            return None
+
+        for model_part_name in self.model.GetModelPartNames():
+            root_model_part = get_model_part(container, self.model[model_part_name])
+            if root_model_part is not None:
+                return root_model_part
+
+        raise RuntimeError(f"No model part contains the provided container.")
+
+
--- applications/OptimizationApplication/python_scripts/processes/optimization_problem_vtu_output_process.py
+++ applications/OptimizationApplication/python_scripts/processes/optimization_problem_vtu_output_process.py
@@ -91,8 +91,4 @@ class OptimizationProblemVtuOutputProcess(OptimizationProblemFieldOutputProcess)
         )
 
     def _CreateTensorAdaptorOutput(self, tensor_adaptor_data: TensorAdaptorData) -> TensorAdaptorOutput:
-        model_part = Kratos.ModelPartUtils.GetModelPart(self.model,tensor_adaptor_data.GetContainer())
-        if model_part is not None:
-            return TensorAdaptorVtuOutput(Kratos.ModelPartUtils.GetModelPart(self.model,tensor_adaptor_data.GetContainer()), self.parameters.Clone(), self.optimization_problem)
-        else:
-            raise RuntimeError("A model part is not found for the given tensor adaptor. Either remove the \"optimization_problem_vtu_output_process\" or check the tensor adaptor.")
+        return TensorAdaptorVtuOutput(self._GetModelPart(tensor_adaptor_data.GetContainer()), self.parameters.Clone(), self.optimization_problem)
