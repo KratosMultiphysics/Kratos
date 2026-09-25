78b3a29b58de4736ae07434ed4c6aac54e68d4c0
Vicente Mataix Ferrándiz <tote1989@gmail.com>
2026-09-25 15:12:53 +0200

    [TrilinosApplication] Refactor TrilinosApplication to use TDefaultDenseSpace instead of UblasSpace for local space types

--- applications/TrilinosApplication/custom_factories/trilinos_linear_solver_factory.h
+++ applications/TrilinosApplication/custom_factories/trilinos_linear_solver_factory.h
@@ -18,7 +18,7 @@
 // External includes
 
 // Project includes
-#include "includes/define.h"
+#include "spaces/default_spaces.h"
 #include "trilinos_space.h"
 #include "factories/linear_solver_factory.h"
 
@@ -105,7 +105,7 @@ inline std::ostream& operator << (std::ostream& rOStream,
 void RegisterTrilinosLinearSolvers();
 
 typedef TrilinosSpace<Epetra_FECrsMatrix, Epetra_FEVector> TrilinosSparseSpaceType;
-typedef UblasSpace<double, Matrix, Vector> TrilinosLocalSpaceType;
+typedef TDefaultDenseSpace<double> TrilinosLocalSpaceType;
 
 typedef LinearSolverFactory<TrilinosSparseSpaceType,  TrilinosLocalSpaceType> TrilinosLinearSolverFactoryType;
 
